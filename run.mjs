85ebf6067cfe42d698e228a5eb0f958d1ca95306
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-16 16:20:09 -0400

    progress

--- run.mjs
+++ run.mjs
@@ -1,6 +1,7 @@
 let {argv} = process;
 let remaining = argv.slice(2);
 let first = remaining[0];
+let args = remaining.slice(1);
 let src = 'src';
 let extension = 'mjs';
 let extension_marker = '.';
