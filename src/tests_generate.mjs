554a5c0529b9375a9363061dc077aa685ba0562c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 18:54:19 -0400

    2024-03-17T22:54:19.636Z

--- src/tests_generate.mjs
+++ src/tests_generate.mjs
@@ -1,3 +1,5 @@
-export function tests_generate(function_name, args_values_list) {
+import { string_split_comma } from "./string_split_comma.mjs";
 
+export function tests_generate(function_name, args_values_list) {
+    let args_values = string_split_comma(args_values_list);
 }
