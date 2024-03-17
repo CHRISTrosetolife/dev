30c099b3f9de9419aedaad40af6a2fb177a09c17
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 14:07:05 -0400

    2024-03-17T18:07:05.172Z

--- src/git_acp.mjs
+++ src/git_acp.mjs
@@ -1,6 +1,6 @@
 import { command_line } from "./command_line.mjs";
 
-export async function git_acp() {
+export async function git_acp(commit_message) {
     let commands = [
         'add *',
         `commit -m "${new Date().toISOString()}"`,
