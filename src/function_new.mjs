fa09ec66d209fa9b3cb571b80537c6ab01b9328d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 14:27:22 -0400

    2024-03-17T18:27:22.793Z

--- src/function_new.mjs
+++ src/function_new.mjs
@@ -1,3 +1,5 @@
-export function function_new( ) {
+import { function_new_generic } from "./function_new_generic.mjs";
 
+export async function function_new(function_name) {
+    await function_new_generic(function_name, '');
 }
