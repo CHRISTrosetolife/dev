300e10057cd2c3d6a28de97f7ad0bb72114a3ece
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 19:03:56 -0400

    2024-03-17T23:03:56.251Z

--- src/run.mjs
+++ src/run.mjs
@@ -1,22 +1,8 @@
-import path from 'path';
+import { function_run } from './function_run.mjs';
 import { process_argv_remaining } from './process_argv_remaining.mjs';
-import { function_extension } from './function_extension.mjs';
 export async function run() {
     let remaining = process_argv_remaining();
     let first = remaining[0];
     let args = remaining.slice(1);
     return await function_run(first, args);
-}
-
-async function function_run(first, args) {
-    let extension = function_extension();
-    let extension_marker = '.';
-    let folder_current = '.';
-    let first_with_extension = [first, extension_marker, extension].join('');
-    let function_path_second = path.join(folder_current, first_with_extension);
-    let function_path = './' + function_path_second;
-    let imported = await import(function_path);
-    let imported_function = imported[first];
-    let result = await imported_function(...args);
-    return result;
 }
