14a57354e1dcf09f29f64c913741d6a1d68161a2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 18:31:08 -0400

    2024-03-17T22:31:07.264Z  function_new_args range count

--- src/string_starts_with.mjs
+++ src/string_starts_with.mjs
@@ -4,4 +4,7 @@ export function string_starts_with(input, prefix) {
     if (input_length < prefix_length) {
         return false;
     }
+    for (let i of range(prefix_length)) {
+        
+    }
 }
