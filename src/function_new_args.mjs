b7c79a576e57188f585aa48f738d2d4f0be72cef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 14:26:30 -0400

    2024-03-17T18:26:30.193Z

--- src/function_new_args.mjs
+++ src/function_new_args.mjs
@@ -1,4 +1,4 @@
-import { file_new } from './file_new.mjs';
+import { function_new_generic } from "./function_new_generic.mjs";
 
 export async function function_new_args(function_name, args) {
     let args_split = args.split(',');
@@ -6,9 +6,4 @@ export async function function_new_args(function_name, args) {
     await function_new_generic(function_name, args_joined);
 }
 
-async function function_new_generic(function_name, args_joined) {
-    let contents = `export function ${function_name}(${args_joined}) {
 
-}`;
-    await file_new(`./src/${function_name}.mjs`, contents);
-}
--- src/function_new_generic.mjs
+++ src/function_new_generic.mjs
@@ -1,4 +1,8 @@
+import { file_new } from './file_new.mjs';
 
-export function function_new_generic(function_name, args_joined) {
+export async function function_new_generic(function_name, args_joined) {
+    let contents = `export function ${function_name}(${args_joined}) {
 
+}`;
+    await file_new(`./src/${function_name}.mjs`, contents);
 }
