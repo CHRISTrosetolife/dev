acb0d3f3795ddc0cc55f0f2d26d189295d535bad
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 19:04:21 -0400

    2024-03-17T23:04:21.423Z

--- src/tests_generate.mjs
+++ src/tests_generate.mjs
@@ -2,4 +2,6 @@ import { string_split_comma } from "./string_split_comma.mjs";
 
 export function tests_generate(function_name, args_values_list) {
     let args_values = string_split_comma(args_values_list);
+    let mapped = list_map(args_values, av => function_run(av, []))
+    return mapped
 }
