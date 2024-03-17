8d9b0f90d210604cdf5babf4192408f4f0dd79b0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 13:36:14 -0400

    2024-03-17T17:36:14.182Z

---
+++ src/run.mjs
@@ -0,0 +1,18 @@
+import path from 'path';
+export async function run() {
+    let {argv} = process;
+    let remaining = argv.slice(2);
+    let first = remaining[0];
+    let args = remaining.slice(1);
+    let src = 'src';
+    let extension = 'mjs';
+    let extension_marker = '.';
+    let folder_current = '.';
+    let first_with_extension = [first,extension_marker,extension].join('');
+    let function_path_second = path.join(folder_current,src,first_with_extension)
+    let function_path = './' + function_path_second;
+    let imported = await import(function_path);
+    let imported_function = imported[first];
+    let result = await imported_function(...args);
+    console.log(result);
+}
