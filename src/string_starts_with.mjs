bbf4fcc582b4110fa14c74563b36b40334e39ed0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 18:29:59 -0400

    2024-03-17T22:29:59.056Z

--- src/string_starts_with.mjs
+++ src/string_starts_with.mjs
@@ -1,7 +1,7 @@
 export function string_starts_with(input, prefix) {
     let input_length = string_length(input);
     let prefix_length = string_length(prefix);
-    if (input < prefix_length) {
+    if (input_length < prefix_length) {
         return false;
     }
 }
