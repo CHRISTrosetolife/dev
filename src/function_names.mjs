285fa0de260bc31b8981a5d6bc8d5bd393400aaa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 18:22:24 -0400

    2024-03-17T22:22:23.977Z

--- src/function_names.mjs
+++ src/function_names.mjs
@@ -1,9 +1,12 @@
 import {glob} from 'glob';
 export async function function_names() {
     let prefix = `src/`;
-    let files = await glob(`${prefix}**/*.mjs`);
+    let suffix = '.mjs';
+    let files = await glob(`${prefix}**/*${suffix}`);
     let mapped = list_map(files, file_path => {
         let mapped2 = string_prefix_without(file_path, prefix);
+        let mapped3 = string_suffix_without(mapped2, suffix);
+        return mapped3;
     })
-    return files;
+    return mapped;
 }
