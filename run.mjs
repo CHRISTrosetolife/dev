1a13d8178ca410c1afe0106bcfedfbae6b9a02c3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 13:37:26 -0400

    2024-03-17T17:37:26.409Z

--- run.mjs
+++ run.mjs
@@ -1,19 +1,2 @@
-import path from 'path';
-async function run() {
-    let {argv} = process;
-    let remaining = argv.slice(2);
-    let first = remaining[0];
-    let args = remaining.slice(1);
-    let src = 'src';
-    let extension = 'mjs';
-    let extension_marker = '.';
-    let folder_current = '.';
-    let first_with_extension = [first,extension_marker,extension].join('');
-    let function_path_second = path.join(folder_current,src,first_with_extension)
-    let function_path = './' + function_path_second;
-    let imported = await import(function_path);
-    let imported_function = imported[first];
-    let result = await imported_function(...args);
-    console.log(result);
-}
+import { run } from './src/run.mjs';
 run();
--- src/run.mjs
+++ src/run.mjs
@@ -4,12 +4,11 @@ export async function run() {
     let remaining = argv.slice(2);
     let first = remaining[0];
     let args = remaining.slice(1);
-    let src = 'src';
     let extension = 'mjs';
     let extension_marker = '.';
     let folder_current = '.';
     let first_with_extension = [first,extension_marker,extension].join('');
-    let function_path_second = path.join(folder_current,src,first_with_extension)
+    let function_path_second = path.join(folder_current,first_with_extension)
     let function_path = './' + function_path_second;
     let imported = await import(function_path);
     let imported_function = imported[first];
