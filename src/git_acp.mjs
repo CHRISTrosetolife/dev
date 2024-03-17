caa43986f0dd8cd2e1a231373590c9a34b54e72b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 13:26:45 -0400

    progress

---
+++ src/git_acp.mjs
@@ -0,0 +1,9 @@
+import { exec } from "child_process";
+
+export function git_acp(command) {
+    let commands = [
+        'add *',
+        `commit -m "${new Date().toISOString()}"`,
+        'push'
+    ]
+}
