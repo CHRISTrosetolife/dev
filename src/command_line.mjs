bdad13278fb14d1120bb982d4cc64958b6eb71cb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 13:27:32 -0400

    progress

--- src/command_line.mjs
+++ src/command_line.mjs
@@ -1,7 +1,7 @@
 import { exec } from "child_process";
 
-export function command_line(command) {
-    return new Promise((resolve) => {
+export async function command_line(command) {
+    return await new Promise((resolve) => {
         exec(command, (error, stdout, stderr) => {
             resolve({error, stdout, stderr});
         });
--- src/git_acp.mjs
+++ src/git_acp.mjs
@@ -1,4 +1,5 @@
 import { exec } from "child_process";
+import { command_line } from "./command_line.mjs";
 
 export function git_acp(command) {
     let commands = [
@@ -6,4 +7,7 @@ export function git_acp(command) {
         `commit -m "${new Date().toISOString()}"`,
         'push'
     ];
+    for (let command of commands) {
+        command_line
+    }
 }
