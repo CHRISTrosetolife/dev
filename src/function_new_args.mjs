3816bdcbc1dbe6739608940c1b155d1444d2fe40
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 14:14:46 -0400

    2024-03-17T18:14:46.638Z

--- src/function_new_args.mjs
+++ src/function_new_args.mjs
@@ -1,6 +1,6 @@
 import { file_new } from './file_new.mjs';
 
-export async function function_new(function_name, args) {
+export async function function_new_args(function_name, args) {
     let args_split = args.split(',');
     let contents = `
 export function ${function_name}(${args_split.join(', ')}) {
