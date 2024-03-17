86c7462e32e9c839d81b6f17ebe472c24e350551
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 19:01:49 -0400

    2024-03-17T23:01:48.899Z

--- src/run.mjs
+++ src/run.mjs
@@ -5,16 +5,18 @@ export async function run() {
     let remaining = process_argv_remaining();
     let first = remaining[0];
     let args = remaining.slice(1);
+    return await function_run(first, args);
+}
+
+async function function_run(first, args) {
     let extension = function_extension();
     let extension_marker = '.';
     let folder_current = '.';
-    let first_with_extension = [first,extension_marker,extension].join('');
-    let function_path_second = path.join(folder_current,first_with_extension)
+    let first_with_extension = [first, extension_marker, extension].join('');
+    let function_path_second = path.join(folder_current, first_with_extension);
     let function_path = './' + function_path_second;
     let imported = await import(function_path);
     let imported_function = imported[first];
     let result = await imported_function(...args);
     return result;
-
-    
 }
