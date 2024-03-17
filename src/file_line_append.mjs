8612b0b03fb9023dc07c5431b9d4fbc11453eb3c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 13:03:31 -0400

    progress

--- src/file_line_append.mjs
+++ src/file_line_append.mjs
@@ -1,4 +1,4 @@
 import fs from 'fs';
 export async function file_line_append(file_new_name, line) {
-    await fs.promises.appendFile(file_new_name, '')
+    await fs.promises.appendFile(file_new_name, '');
 }
