c7c68bb0c43d3567a7675ae81858765a40616b90
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 16:42:00 -0400

    2024-03-17T20:42:00.459Z

--- src/function_open.mjs
+++ src/function_open.mjs
@@ -1,3 +1,7 @@
-export function function_open(function_name) {
+import { file_open } from "./file_open.mjs";
+import { function_name_to_path } from "./function_name_to_path.mjs";
 
+export async function function_open(function_name) {
+    const file_path = function_name_to_path(function_name);
+    await file_open(file_path);
 }
