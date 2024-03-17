0bdb874a28d6e994d41dfb5ed81a32e83be62512
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 17:14:19 -0400

    2024-03-17T21:14:19.541Z

--- src/function_search.mjs
+++ src/function_search.mjs
@@ -1,5 +1,5 @@
 import {string_split_comma} from './string_split_comma.mjs';
 export async function function_search(query) {
     let terms = string_split_comma(query);
-    let function_names = await function_names_get();
+    let function_names = await function_names();
 }
