01a03f683464252bd658e4529926910f9098c576
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 14:31:12 -0400

    2024-03-17T18:31:11.959Z

--- src/run_git.mjs
+++ src/run_git.mjs
@@ -1,8 +1,10 @@
 import { run } from "./run.mjs";
 import { git_acp_message } from "./git_acp_message.mjs";
+import { process_argv_remaining } from "./process_argv_remaining.mjs";
 
 export async function run_git() {
     await run();
-    let commit_message = '';
+    let remaining = process_argv_remaining();
+    let commit_message = remaining.join(' ');
     await git_acp_message(commit_message);
 }
