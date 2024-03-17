ec7d470f4a28b6d5818bd18cc9b836a81a250cd2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 14:01:23 -0400

    2024-03-17T18:01:22.873Z

--- src/function_new.mjs
+++ src/function_new.mjs
@@ -1,9 +1,9 @@
 import { file_new } from './file_new.mjs';
 
 export async function function_new(function_name, args) {
-    let split = args.split(',');
+    let args_split = args.split(',');
     let contents = `
-export function ${function_name}(${split.join(', ')}) {
+export function ${function_name}(${args_split.join(', ')}) {
 
 }`
     await file_new(`./src/${function_name}`, contents);
