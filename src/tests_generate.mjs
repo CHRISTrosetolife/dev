e3ddab189ae8a235b46b18b4dc06d159f50d217a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 19:06:41 -0400

    2024-03-17T23:06:41.699Z

--- src/tests_generate.mjs
+++ src/tests_generate.mjs
@@ -1,9 +1,9 @@
 import { string_split_comma } from "./string_split_comma.mjs";
-import { list_map } from "./list_map.mjs";
 import { function_run } from "./function_run.mjs";
+import { list_map_async } from "./list_map_async.mjs";
 
 export function tests_generate(function_name, args_values_list) {
     let args_values = string_split_comma(args_values_list);
-    let mapped = list_map(args_values, async av => await function_run(av, []))
+    let mapped = list_map_async(args_values, async av => await function_run(av, []))
     return mapped
 }
