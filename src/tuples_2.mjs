9679582c087954e7b3bcdc34e7d9865d5d2bfb7e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 06:10:54 -0400

    2024-03-18T10:10:53.878Z

--- src/tuples_2.mjs
+++ src/tuples_2.mjs
@@ -6,7 +6,7 @@ import { equal } from "./equal.mjs";
 import { list_get } from "./list_get.mjs";
 
 export function tuples_2(names_pair) {
-    let args_values = string_split_comma(args_values_list);
+    let args_values = string_split_comma(names_pair);
     let mapped = list_map_async(args_values, async av => await function_run(av, []))
     assert(equal(list_length(mapped), 2));
     let m1 = list_get(mapped, 0);
