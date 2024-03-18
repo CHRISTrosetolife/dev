2c5636bb7b307f2953141ce703f926e4c74521e4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 07:55:54 -0400

    2024-03-18T11:55:53.878Z

--- src/tests_generate.mjs
+++ src/tests_generate.mjs
@@ -5,9 +5,9 @@ export async function tests_generate(function_name, args_values_get) {
     let args_values = await function_run(args_values_get, []);
     await list_counter_async(async count => {
         for (let args of args_values) {
+            let c = count();
             let result = await function_run(function_name, args);
             console.log(list_concat(args, [result]));
-            let c = count();
         }
     })
 }
