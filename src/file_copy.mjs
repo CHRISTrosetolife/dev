90e77920f6eed13906e0eafa7d899074eed98521
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 11:27:12 -0400

    2024-03-18T15:27:12.342Z  noop

--- src/file_copy.mjs
+++ src/file_copy.mjs
@@ -1,3 +1,4 @@
-export function file_copy(file_copy) {
-
+import fs from 'fs';
+export async function file_copy(file_name_from, file_name_to) {
+    await fs.promises.copyFile(file_name_from, file_name_to);
 }
