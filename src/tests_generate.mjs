752f826a9e2964cbed4a6e5e92446c7edbc7def0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 07:35:14 -0400

    2024-03-18T11:35:14.062Z  function_new_args list_concat a,b

--- src/tests_generate.mjs
+++ src/tests_generate.mjs
@@ -9,6 +9,7 @@ export async function tests_generate(function_name, args_values_get) {
     let args_values = await function_run(args_values_get, []);
     for (let args of args_values) {
         let result = await function_run(function_name, args);
+        
         console.log({args, result})
     }
 }
