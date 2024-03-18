15d2ca2535eeaccbbd9b65fbf5e56f2bf095c337
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 07:11:14 -0400

    2024-03-18T11:11:13.716Z

--- src/tuples_2.mjs
+++ src/tuples_2.mjs
@@ -4,6 +4,7 @@ import { list_map_async } from "./list_map_async.mjs";
 import { assert } from "./assert.mjs";
 import { equal } from "./equal.mjs";
 import { list_get } from "./list_get.mjs";
+import { list_adder } from "./list_adder.mjs";
 
 export function tuples_2(names_pair) {
     let args_values = string_split_comma(names_pair);
@@ -11,4 +12,11 @@ export function tuples_2(names_pair) {
     assert(equal(list_length(mapped), 2));
     let m1 = list_get(mapped, 0);
     let m2 = list_get(mapped, 1);
+    return list_adder(la => {
+        for (let m1_i of m1) {
+            for (let m2_i of m2) {
+                la([m1_i,m2_i])
+            }
+        }
+    })
 }
