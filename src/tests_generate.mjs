4771b9c6950e4e4a46b9d006e5de37c98c741f32
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 15:27:13 -0400

    2024-03-18T19:27:13.495Z  noop

--- src/tests_generate.mjs
+++ src/tests_generate.mjs
@@ -4,6 +4,8 @@ import { list_counter_async } from "./list_counter_async.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 import { assert } from "./assert.mjs";
 import { equal } from "./equal.mjs";
+import { list_map } from "./list_map.mjs";
+import { assert } from "./assert.mjs";
 
 export async function tests_generate(function_name, args_values_get) {
     let args_values = await function_run(args_values_get, []);
@@ -13,7 +15,11 @@ export async function tests_generate(function_name, args_values_get) {
             let result = await function_run(function_name, args);
             console.log(c.toString(), list_concat(args, [result]));
             let result_name = 'result';
-            console.log({args,a:args.join(', ')})
+            let string_delimeter = "'";
+            for (let arg of args) {
+                assert(!string_includes(arg, string_delimeter));
+            }
+            let args_mapped = list_map(args, arg => `${string_delimeter}${arg}${string_delimeter}`)
             await function_new_generic(`${function_name}_test_${c}`, ``, `    let ${result_name} = ${function_name}(${args.join(', ')});
     ${assert.name}(${equal.name}(${result_name}, ${result}))`, false, [assert.name, equal.name])
         }
