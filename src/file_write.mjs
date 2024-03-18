ba699e1986fb4921221af29c42a208993191ca2f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 14:23:53 -0400

    2024-03-18T18:23:52.815Z  noop

--- src/file_write.mjs
+++ src/file_write.mjs
@@ -1,4 +1,4 @@
 import fs from 'fs';
-export async function file_read(file_name) {
-    return await fs.promises.readFile(file_name, 'utf-8');
+export async function file_read(file_name, data) {
+    return await fs.promises.writeFile(file_name, data, 'utf-8');
 }
