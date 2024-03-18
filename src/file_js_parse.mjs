62524248789f1821972e0894238dd51bfee600fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 11:48:32 -0400

    2024-03-18T15:48:32.006Z  noop

--- src/file_js_parse.mjs
+++ src/file_js_parse.mjs
@@ -1,3 +1,5 @@
-export function file_js_parse(file_name) {
-
+export async function file_js_parse(file_name) {
+    let code = await file_read(file_name);
+    let parsed = js_parse(code);
+    return parsed;
 }
--- src/function_parse.mjs
+++
@@ -1,5 +0,0 @@
-import acorn from 'acorn';
-export function function_parse(function_name) {
-    let text = file_read
-    acorn.parse("1 + 1", {ecmaVersion: 2020})
-}
