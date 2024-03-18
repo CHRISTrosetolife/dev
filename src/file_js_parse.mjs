8f93720cec8272fd8dfa9904c7bc2841829cfa56
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 13:21:47 -0400

    2024-03-18T17:21:47.114Z  noop

--- src/file_js_parse.mjs
+++ src/file_js_parse.mjs
@@ -1,3 +1,5 @@
+import {file_read} from './file_read.mjs';
+import {js_parse} from './js_parse.mjs';
 export async function file_js_parse(file_name) {
     let code = await file_read(file_name);
     let parsed = js_parse(code);
