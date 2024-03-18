024bd888917ca79e06476145da81c618ce5bdacc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 07:10:02 -0400

    2024-03-18T11:10:02.003Z

--- src/list_adder.mjs
+++ src/list_adder.mjs
@@ -1,3 +1,7 @@
-export function list_adder(lambda) {
+import { list_add } from "./list_add.mjs";
 
+export function list_adder(lambda) {
+    let result = [];
+    lambda(item => list_add(result, item));
+    return result;
 }
