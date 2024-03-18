e244bf9f369c6ab2f1f9cbb526e409d3c6162215
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 15:20:16 -0400

    2024-03-18T19:20:16.300Z  noop

--- src/function_new_generic.mjs
+++ src/function_new_generic.mjs
@@ -6,11 +6,12 @@ import { js_code_import } from './js_code_import.mjs';
 import { list_concat } from './list_concat.mjs';
 
 export async function function_new_generic(function_name, args_string, body_string, open, imports) {
-    let contents = `export function ${function_name}(${args_string}) {
+    let contents_function = `export function ${function_name}(${args_string}) {
 ${body_string}
 }`;
     let mapped = list_map(imports, js_code_import);
-    let concat = list_concat(mapped, [contents])
+    let concat = list_concat(mapped, [contents_function])
+    let contents = list_join(concat, newline())
     const file_path = function_name_to_path(function_name);
     await file_new(file_path, contents);
     if (open) {
