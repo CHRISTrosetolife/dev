2f545672291074dcaa56d299e1ceb877737328a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 07:15:56 -0400

    2024-03-18T11:15:55.973Z

--- src/tuples_2.mjs
+++ src/tuples_2.mjs
@@ -7,9 +7,9 @@ import { list_get } from "./list_get.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { list_length } from "./list_length.mjs";
 
-export function tuples_2(names_pair) {
+export async function tuples_2(names_pair) {
     let args_values = string_split_comma(names_pair);
-    let mapped = list_map_async(args_values, async av => await function_run(av, []))
+    let mapped = await list_map_async(args_values, async av => await function_run(av, []))
     assert(equal(list_length(mapped), 2));
     let m1 = list_get(mapped, 0);
     let m2 = list_get(mapped, 1);
