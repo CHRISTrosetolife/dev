a660aac738100f774408d2dc673a2811d185ebd5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 11:14:05 -0400

    2024-03-18T15:14:05.369Z

--- src/function_import_add.mjs
+++ src/function_import_add.mjs
@@ -1,3 +1,5 @@
-export function function_import_add(function_name, import_name) {
+import { function_name_to_path } from "./function_name_to_path.mjs";
 
+export function function_import_add(function_name, import_name) {
+    let function_path = function_name_to_path(function_name);
 }
