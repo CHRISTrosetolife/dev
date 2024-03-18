9797cc6d26911b6c85cddbc15421a9ca8c3d952b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 13:36:38 -0400

    2024-03-18T17:36:38.599Z  noop

--- src/file_js_identifier_rename.mjs
+++ src/file_js_identifier_rename.mjs
@@ -2,12 +2,15 @@ import { file_js_parse } from "./file_js_parse.mjs";
 import { visit } from "./visit.mjs";
 import { object_values } from "./object_values.mjs";
 import { js_node_is } from "./js_node_is.mjs";
+import { list_is } from "./list_is.mjs";
 export async function file_js_identifier_rename(file_path, identifier_from, identifier_to) {
     let parsed = await file_js_parse(file_path);
     visit(parsed, n => {
         if (js_node_is(n)) {
-            console.log('here')
             return object_values(n);
+        }
+        if (list_is(n)) {
+            return n;
         }
         return []
     }, n => n.type === 'Identifier', v => {
