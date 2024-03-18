07623dd552baa3c7d4448ec9db35046b63e71d6e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 06:10:46 -0400

    2024-03-18T10:10:46.058Z

--- src/tuples_2.mjs
+++ src/tuples_2.mjs
@@ -1,3 +1,13 @@
-export function tuples_2(names_pair) {
+import { string_split_comma } from "./string_split_comma.mjs";
+import { function_run } from "./function_run.mjs";
+import { list_map_async } from "./list_map_async.mjs";
+import { assert } from "./assert.mjs";
+import { equal } from "./equal.mjs";
+import { list_get } from "./list_get.mjs";
 
+export function tuples_2(names_pair) {
+    let args_values = string_split_comma(args_values_list);
+    let mapped = list_map_async(args_values, async av => await function_run(av, []))
+    assert(equal(list_length(mapped), 2));
+    let m1 = list_get(mapped, 0);
 }
