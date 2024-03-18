6fdaea9cf61a2dd259e55abdeb633415c2879aa0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 08:13:07 -0400

    2024-03-18T12:13:07.187Z

--- src/function_new_generic.mjs
+++ src/function_new_generic.mjs
@@ -2,12 +2,14 @@ import { file_new } from './file_new.mjs';
 import { file_open } from './file_open.mjs';
 import { function_name_to_path } from './function_name_to_path.mjs';
 
-export async function function_new_generic(function_name, args_string, body_string) {
+export async function function_new_generic(function_name, args_string, body_string, open) {
     let contents = `export function ${function_name}(${args_string}) {
 ${body_string}
 }`;
     const file_path = function_name_to_path(function_name);
     await file_new(file_path, contents);
-    await file_open(file_path);
+    if (open) {
+        await file_open(file_path);
+    }
 }
 
