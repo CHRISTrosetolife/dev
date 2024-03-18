7540f1ed038bdf8abdd48a1a2b7e535650289946
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 13:37:58 -0400

    2024-03-18T17:37:58.060Z  noop

--- src/js_node_is.mjs
+++ src/js_node_is.mjs
@@ -1,4 +1,7 @@
 import {object_property_exists} from './object_property_exists.mjs';
 export function js_node_is(candidate) {
+    if (null_is(candidate)) {
+        
+    }
     return object_property_exists(candidate, 'type');
 }
