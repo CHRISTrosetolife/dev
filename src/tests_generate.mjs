b0711ae508923114924a5caa5ec3c14d65f65010
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-17 20:35:27 -0400

    2024-03-18T00:35:26.521Z  function_new_args list_length list

--- src/tests_generate.mjs
+++ src/tests_generate.mjs
@@ -1,9 +1,11 @@
 import { string_split_comma } from "./string_split_comma.mjs";
 import { function_run } from "./function_run.mjs";
 import { list_map_async } from "./list_map_async.mjs";
+import { assert } from "./assert.mjs";
 
 export function tests_generate(function_name, args_values_list) {
     let args_values = string_split_comma(args_values_list);
     let mapped = list_map_async(args_values, async av => await function_run(av, []))
+    assert
     return mapped
 }
