9ececf3a8a01bcc800cc2a6e5fb771c8b4ec881b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 16:45:30 -0400

    2024-03-18T20:45:30.334Z  noop

--- src/tests_generate.mjs
+++ src/tests_generate.mjs
@@ -9,13 +9,22 @@ import { string_includes } from "./string_includes.mjs";
 import { range } from "./range.mjs";
 import { add_1 } from "./add_1.mjs";
 import { list_random_item } from "./list_random_item.mjs";
+import { json_to } from "./json_to.mjs";
 
 export async function tests_generate(function_name, args_values_get) {
     let args_values = await function_run(args_values_get, []);
+    let existing = {};
     let max = 10;
     for (let i of range(max)) {
         let index = add_1(i);
-        let args = list_random_item(args_values)
+        let attempts = 100;
+        for (let j of range(attempts)) {
+            let args = list_random_item(args_values);
+            let args_json = json_to(args);
+            if (object_property_exists(existing, args_json)) {
+                continue;
+            }
+        }
     }
     await list_counter_async(async count => {
         for (let args of args_values) {
