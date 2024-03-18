c27c032e79c3a4032b572fdfa3dc3f3d3d51cdce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 08:07:45 -0400

    2024-03-18T12:07:45.337Z

--- src/tests_generate.mjs
+++ src/tests_generate.mjs
@@ -2,6 +2,8 @@ import { function_run } from "./function_run.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { list_counter_async } from "./list_counter_async.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
+import { assert } from "./assert.mjs";
+import { equal } from "./equal.mjs";
 
 export async function tests_generate(function_name, args_values_get) {
     let args_values = await function_run(args_values_get, []);
@@ -10,7 +12,8 @@ export async function tests_generate(function_name, args_values_get) {
             let c = count();
             let result = await function_run(function_name, args);
             console.log(c.toString(), list_concat(args, [result]));
-            function_new_generic()
+            function_new_generic(`${function_name}_test_${c}`, ``, `let result = ${function_name}(${args.join(', ')});
+${assert.name}(${equal.name}(result, ${result}))`)
         }
     })
 }
