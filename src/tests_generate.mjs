df830a1637d0d184327fa71b1ffdb66eccaef3d0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 07:56:18 -0400

    2024-03-18T11:56:17.842Z

--- src/tests_generate.mjs
+++ src/tests_generate.mjs
@@ -1,5 +1,6 @@
 import { function_run } from "./function_run.mjs";
 import { list_concat } from "./list_concat.mjs";
+import { list_counter_async } from "./list_counter_async.mjs";
 
 export async function tests_generate(function_name, args_values_get) {
     let args_values = await function_run(args_values_get, []);
@@ -7,7 +8,7 @@ export async function tests_generate(function_name, args_values_get) {
         for (let args of args_values) {
             let c = count();
             let result = await function_run(function_name, args);
-            console.log(list_concat(args, [result]));
+            console.log(list_concat(args, [result,c]));
         }
     })
 }
