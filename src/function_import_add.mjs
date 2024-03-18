68035ea9db4dec723431fe690f2058db343c4b1f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 14:52:39 -0400

    2024-03-18T18:52:38.780Z  noop

--- src/function_import_add.mjs
+++ src/function_import_add.mjs
@@ -1,7 +1,8 @@
 import { function_name_to_path } from "./function_name_to_path.mjs";
+import { file_line_prepend } from "./file_line_prepend.mjs";
 
-export function function_import_add(function_name, import_name) {
+export async function function_import_add(function_name, import_name) {
     let function_path = function_name_to_path(function_name);
     let import_code = `import { ${import_name} } from "./${import_name}.mjs";`
-    
+    await file_line_prepend(function_path, import_code)
 }
