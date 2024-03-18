9414652fa7af2f8343dda424cd9b3e17563395d4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 07:36:14 -0400

    2024-03-18T11:36:13.824Z

--- src/tests_generate.mjs
+++ src/tests_generate.mjs
@@ -1,15 +1,10 @@
-import { string_split_comma } from "./string_split_comma.mjs";
 import { function_run } from "./function_run.mjs";
-import { list_map_async } from "./list_map_async.mjs";
-import { assert } from "./assert.mjs";
-import { equal } from "./equal.mjs";
-import { list_get } from "./list_get.mjs";
+import { list_concat } from "./list_concat.mjs";
 
 export async function tests_generate(function_name, args_values_get) {
     let args_values = await function_run(args_values_get, []);
     for (let args of args_values) {
         let result = await function_run(function_name, args);
-        
-        console.log({args, result})
+        console.log(list_concat(args, [result]));
     }
 }
