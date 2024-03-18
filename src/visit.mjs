12dd7b9cc77bb13489f4b7ae6f8c5fe739bad72f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 13:25:48 -0400

    2024-03-18T17:25:48.412Z  noop

--- src/visit.mjs
+++ src/visit.mjs
@@ -1,3 +1,8 @@
 export function visit(root, children_get, filter, each) {
     let children = children_get(root);
+    for (let node of children) {
+        if (filter(node)) {
+            each({node});
+        }
+    }
 }
