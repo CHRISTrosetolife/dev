41273d95c3caacdfa473e523c92815a9dc375690
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 07:55:31 -0400

    2024-03-18T11:55:31.392Z

--- src/list_counter_async.mjs
+++ src/list_counter_async.mjs
@@ -1,3 +1,8 @@
-export function list_counter_async(lambda) {
-
+export async function list_counter_async(lambda) {
+    let result = 0;
+    await lambda(() => {
+        result = result + 1;
+        return result;
+    });
+    return result;
 }
--- src/tests_generate.mjs
+++ src/tests_generate.mjs
@@ -3,8 +3,11 @@ import { list_concat } from "./list_concat.mjs";
 
 export async function tests_generate(function_name, args_values_get) {
     let args_values = await function_run(args_values_get, []);
-    for (let args of args_values) {
-        let result = await function_run(function_name, args);
-        console.log(list_concat(args, [result]));
-    }
+    await list_counter_async(async count => {
+        for (let args of args_values) {
+            let result = await function_run(function_name, args);
+            console.log(list_concat(args, [result]));
+            let c = count();
+        }
+    })
 }
