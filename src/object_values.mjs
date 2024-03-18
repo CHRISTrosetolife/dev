24d9a3c2b889b7fbaae69e4ac36b37e538186d17
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 13:19:28 -0400

    2024-03-18T17:19:28.316Z  noop

--- src/object_values.mjs
+++ src/object_values.mjs
@@ -1,3 +1,4 @@
+import {object_properties} from './object_properties.mjs';
 export function object_values(object) {
     let properties = object_properties(object);
     return list_map(properties, p => object_property_get(object, p));
