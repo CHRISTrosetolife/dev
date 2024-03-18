5ee41bfbade58a5a0098aac52c3ec564980bcadc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-18 11:19:17 -0400

    2024-03-18T15:19:17.067Z

--- src/function_copy.mjs
+++ src/function_copy.mjs
@@ -1,3 +1,6 @@
-export function function_copy(fn_name_from, fn_name_to) {
+import { function_name_to_path } from "./function_name_to_path.mjs";
 
+export function function_copy(fn_name_from, fn_name_to) {
+    let fn_path_from = function_name_to_path(fn_name_from);
+    let fn_path_to = function_name_to_path(fn_name_to);
 }
