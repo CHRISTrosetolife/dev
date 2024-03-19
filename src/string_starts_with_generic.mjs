39d14ec62b2dc6b64d084f1a08373607858e9f9f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 12:57:23 -0400

    2024-03-19T16:57:22.606Z  function_copy string_starts_with string_starts_with_generic

---
+++ src/string_starts_with_generic.mjs
@@ -0,0 +1,17 @@
+import {string_length} from './string_length.mjs';
+import {range} from './range.mjs';
+import {string_get} from './string_get.mjs';
+export function string_starts_with_generic(input, prefix) {
+  let string_get_lambda = string_get;
+  let input_length = string_length(input);
+  let prefix_length = string_length(prefix);
+  if (input_length < prefix_length) {
+    return false;
+  }
+  for (let i of range(prefix_length)) {
+    if (string_get_lambda(input, i) !== string_get_lambda(prefix, i)) {
+      return false;
+    }
+  }
+  return true;
+}
