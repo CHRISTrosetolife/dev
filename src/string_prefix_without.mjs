348d9add7f4bc832b765f2a81026f3d1ac5e9942
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 14:00:27 -0400

    2024-03-19T18:00:27.548Z  noop

--- src/string_prefix_without.mjs
+++ src/string_prefix_without.mjs
@@ -1,5 +1,6 @@
 import { string_length } from "./string_length.mjs";
 import { string_skip } from "./string_skip.mjs";
+import { assert } from "./assert.mjs";
 
 export function string_prefix_without(input, prefix) {
     assert(string_starts_with(input, prefix));
