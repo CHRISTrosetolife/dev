92211c33514a0d7e01de8fc2dcf6640ac753f79d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 15:00:59 -0400

    2024-03-19T19:00:59.695Z  function_names

--- src/function_names.mjs
+++ src/function_names.mjs
@@ -6,6 +6,7 @@ export async function function_names() {
     let prefix = `src\\`;
     let suffix = '.mjs';
     let files = await glob(`${prefix}**\\*${suffix}`);
+    console.log({files})
     let mapped = list_map(files, file_path => {
         let mapped2 = string_prefix_without(file_path, prefix);
         let mapped3 = string_suffix_without(mapped2, suffix);
