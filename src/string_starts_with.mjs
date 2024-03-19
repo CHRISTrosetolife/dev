5cfdd10b636927344ba6c2ef3b5bc102c97ec472
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 13:24:13 -0400

    2024-03-19T17:24:13.493Z  noop

--- src/string_starts_with.mjs
+++ src/string_starts_with.mjs
@@ -1,4 +1,5 @@
 import {string_get} from './string_get.mjs';
+import {string_starts_with_generic} from './string_starts_with_generic.mjs';
 export function string_starts_with(input, prefix) {
     let string_get_lambda = string_get;
     return string_starts_with_generic(string_get_lambda, input, prefix);
