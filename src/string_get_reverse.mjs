9370f34db168109bd2cdd397a45d1b3207455656
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 14:08:10 -0400

    2024-03-19T18:08:10.174Z  noop

--- src/string_get_reverse.mjs
+++ src/string_get_reverse.mjs
@@ -1,3 +1,6 @@
+import { string_length } from "./string_length.mjs";
+
 export function string_get_reverse(input, index) {
-  return input[index];
+    let length = string_length(input);
+  return input[length - 1 - index];
 }
