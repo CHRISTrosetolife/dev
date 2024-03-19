886ad6b2b4637f1eedb0df2a703deac4efd39dbd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 12:54:59 -0400

    2024-03-19T16:54:58.790Z  noop

--- src/string_starts_with.mjs
+++ src/string_starts_with.mjs
@@ -2,6 +2,7 @@ import {string_length} from './string_length.mjs';
 import {range} from './range.mjs';
 import {string_get} from './string_get.mjs';
 export function string_starts_with(input, prefix) {
+    let string_get_lambda = string_get;
     let input_length = string_length(input);
     let prefix_length = string_length(prefix);
     if (input_length < prefix_length) {
