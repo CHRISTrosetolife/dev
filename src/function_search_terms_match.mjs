e593110fc8e9699ea2a2a431d0e2e8546ed39bf8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 12:13:37 -0400

    2024-03-19T16:13:36.920Z  noop

--- src/function_search_terms_match.mjs
+++ src/function_search_terms_match.mjs
@@ -3,8 +3,9 @@ import {list_includes} from './list_includes.mjs';
 export function function_search_terms_match(function_name, terms) {
     let fn_name_parts = function_name_to_parts(function_name);
     for (let t of terms) {
-        if (list_includes(fn_name_parts, t)) {
-            
+        if (!list_includes(fn_name_parts, t)) {
+            return false;
         }
     }
+    return true;
 }
