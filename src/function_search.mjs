fa1161be45ed56c9d0a131a2607642e3ebf0d741
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 11:50:29 -0400

    2024-03-19T15:50:28.749Z  noop

--- src/function_search.mjs
+++ src/function_search.mjs
@@ -1,6 +1,10 @@
 import {string_split_comma} from './string_split_comma.mjs';
 import {function_names} from './function_names.mjs';
+import {list_filter} from './list_filter.mjs';
 export async function function_search(query) {
     let terms = string_split_comma(query);
     let fn_names = await function_names();
+    list_filter(fn_names, fn_name => {
+
+    })
 }
