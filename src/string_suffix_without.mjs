4ac7d4f3d4832ad7f78e207338ab09d1c3c64ac6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 14:01:45 -0400

    2024-03-19T18:01:45.313Z  noop

--- src/string_suffix_without.mjs
+++ src/string_suffix_without.mjs
@@ -1,9 +1,9 @@
 import {string_length} from "./string_length.mjs";
 import {string_skip} from "./string_skip.mjs";
 import {assert} from "./assert.mjs";
-import {string_starts_with} from "./string_starts_with.mjs";
+import {string_ends_with} from "./string_ends_with.mjs";
 export function string_suffix_without(input, prefix) {
-  assert(string_starts_with(input, prefix));
+  assert(string_ends_with(input, prefix));
   let prefix_length = string_length(prefix);
   return string_skip(input, prefix_length);
 }
