e9c6b3eab5fd942c68a12fc4bc2cc9072b093c06
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 16:59:01 -0400

    2024-03-19T20:59:00.812Z  noop

--- src/string_take.mjs
+++ src/string_take.mjs
@@ -3,6 +3,6 @@ import { string_length } from "./string_length.mjs";
 
 export function string_take(input, count) {
     let length = string_length(input);
-    assert(count >= length);
+    assert(count <= length);
   return input.substring(0, count);
 }
