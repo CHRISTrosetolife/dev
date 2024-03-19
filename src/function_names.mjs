8cb2052edac91e49e30041aec64bc4b71f9b5dbc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 16:53:59 -0400

    2024-03-19T20:53:58.659Z  function_new_args string_replace input,from,to

--- src/function_names.mjs
+++ src/function_names.mjs
@@ -3,11 +3,12 @@ import {list_map} from './list_map.mjs';
 import {string_prefix_without} from './string_prefix_without.mjs';
 import {string_suffix_without} from './string_suffix_without.mjs';
 export async function function_names() {
-    let prefix = `src\\`;
+    let prefix = `src/`;
     let suffix = '.mjs';
     let files = await glob(`${prefix}**/*${suffix}`);
     console.log({files})
     let mapped = list_map(files, file_path => {
+        let replaced = string_replace(file_path, '\\', '/');
         let mapped2 = string_prefix_without(file_path, prefix);
         let mapped3 = string_suffix_without(mapped2, suffix);
         return mapped3;
