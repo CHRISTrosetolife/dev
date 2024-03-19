0234bd82cf15053de761121fcd8a84bb35d07dc5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 11:53:13 -0400

    2024-03-19T15:53:12.548Z  function_copy string_split_comma string_split_underscore

--- src/function_search.mjs
+++ src/function_search.mjs
@@ -5,6 +5,9 @@ export async function function_search(query) {
     let terms = string_split_comma(query);
     let fn_names = await function_names();
     list_filter(fn_names, fn_name => {
-
+        let fn_name_parts = string_split(fn_name, '_');
+        for (let t of terms) {
+            if ()
+        }
     })
 }
