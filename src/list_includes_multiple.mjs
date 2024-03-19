0aeaaa587c1ec20bced933ac58acb0e3aa64bc60
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 17:01:32 -0400

    2024-03-19T21:01:31.762Z  function_search list

--- src/list_includes_multiple.mjs
+++ src/list_includes_multiple.mjs
@@ -1,6 +1,5 @@
 import {list_includes} from './list_includes.mjs';
 export function list_includes_multiple(list, items_to_include) {
-    console.log({list, items_to_include})
     for (let item of items_to_include) {
         if (!list_includes(list, item)) {
             return false;
