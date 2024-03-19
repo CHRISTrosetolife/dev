675fbf68b065ab1acb2c6f1704b7c1531fbc0ea8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 14:54:39 -0400

    2024-03-19T18:54:39.572Z  noop

---
+++ src/string_ends_with_test_1.mjs
@@ -0,0 +1,6 @@
+import { assert } from "./assert.mjs";
+import { equal } from "./equal.mjs";
+export function string_ends_with_test_1() {
+    let result = string_ends_with('ba', 'ba');
+    assert(equal(result, true))
+}
