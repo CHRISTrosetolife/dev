b82bb551574eb3b55337c01ae537654badb7f74e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 12:37:59 -0400

    2024-03-19T16:37:58.833Z  noop

--- src/function_search_terms_match.mjs
+++ src/function_search_terms_match.mjs
@@ -1,11 +1,6 @@
 import {function_name_to_parts} from './function_name_to_parts.mjs';
-import {list_includes} from './list_includes.mjs';
+import {list_includes_multiple} from './list_includes_multiple.mjs';
 export function function_search_terms_match(function_name, terms) {
     let fn_name_parts = function_name_to_parts(function_name);
-    for (let t of terms) {
-        if (!list_includes(fn_name_parts, t)) {
-            return false;
-        }
-    }
-    return true;
+    return list_includes_multiple(terms, fn_name_parts)
 }
