85d1b1c07061d5864e3cbe85da08be66f70a6004
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 10:58:29 -0400

    2024-03-19T14:58:29.235Z  noop

--- src/tests_generate.mjs
+++ src/tests_generate.mjs
@@ -6,6 +6,7 @@ import { list_random_item } from "./list_random_item.mjs";
 import { json_to } from "./json_to.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { tests_generate_single } from "./tests_generate_single.mjs";
+import { object_property_set } from "./object_property_set.mjs";
 
 export async function tests_generate(function_name, args_values_get) {
     let args_values = await function_run(args_values_get, []);
@@ -18,6 +19,7 @@ export async function tests_generate(function_name, args_values_get) {
             let args = list_random_item(args_values);
             let args_json = json_to(args);
             if (!object_property_exists(existing, args_json)) {
+                object_property_set(existing, args_json, true);
                 await tests_generate_single(function_name, args, test_number);
                 break;
             }
