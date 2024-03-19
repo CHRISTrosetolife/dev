a1c0dbbb4d4dbe3ddf8315bb95582121cff3cb57
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 12:09:45 -0400

    2024-03-19T16:09:45.250Z  noop

--- src/function_search.mjs
+++ src/function_search.mjs
@@ -1,7 +1,6 @@
 import {string_split_comma} from './string_split_comma.mjs';
 import {function_names} from './function_names.mjs';
 import {list_filter} from './list_filter.mjs';
-import {function_name_to_parts} from './function_name_to_parts.mjs';
 export async function function_search(query) {
     let terms = string_split_comma(query);
     let fn_names = await function_names();
@@ -9,7 +8,7 @@ export async function function_search(query) {
         let fn_name_parts = function_name_to_parts(fn_name);
         for (let t of terms) {
             if (list_includes(fn_name_parts, t)) {
-                
+
             }
         }
     })
--- src/function_search_terms_match.mjs
+++ src/function_search_terms_match.mjs
@@ -1,3 +1,9 @@
+import {function_name_to_parts} from './function_name_to_parts.mjs';
 export function function_search_terms_match(function_name, terms) {
-
+    let fn_name_parts = function_name_to_parts(fn_name);
+    for (let t of terms) {
+        if (list_includes(fn_name_parts, t)) {
+            
+        }
+    }
 }
