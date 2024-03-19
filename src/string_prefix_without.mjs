205e48edfb79b7975b938847e6af3ffeb3b22f8b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 13:14:41 -0400

    2024-03-19T17:14:41.158Z  noop

--- src/string_prefix_without.mjs
+++ src/string_prefix_without.mjs
@@ -1,7 +1,7 @@
 import { string_length } from "./string_length.mjs"
 
 export function string_prefix_without(input, prefix) {
-    assert(string_starts_with(input,prefix))
-    let prefix_length = string_length(prefix)
+    assert(string_starts_with(input,prefix));
+    let prefix_length = string_length(prefix);
     return
 }
