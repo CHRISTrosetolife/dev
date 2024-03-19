b0dfe542cbcfc291cd3bfd43bd4dfc500176de31
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 14:57:52 -0400

    2024-03-19T18:57:51.684Z  noop

--- src/string_take.mjs
+++ src/string_take.mjs
@@ -1,3 +1,8 @@
+import { assert } from "./assert.mjs";
+import { string_length } from "./string_length.mjs";
+
 export function string_take(input, count) {
+    let length = string_length(input);
+    assert(count >= length);
   return input.substring(0, count);
 }
