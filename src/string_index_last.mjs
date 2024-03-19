b5e8c34fa1cc3c877e44834ce2f0672ee1f391e9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 14:09:43 -0400

    2024-03-19T18:09:42.986Z  noop

--- src/string_index_last.mjs
+++ src/string_index_last.mjs
@@ -1,3 +1,7 @@
-export function string_index_last(input) {
+import { string_length } from "./string_length.mjs";
 
+export function string_index_last(input) {
+    let length = string_length(input);
+    let index_last = length - 1;
+    return index_last;
 }
