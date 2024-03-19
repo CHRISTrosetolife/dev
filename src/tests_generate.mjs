a8e4295b4e0589e4276b267705bfb186c0a604f9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-19 10:49:56 -0400

    2024-03-19T14:49:56.293Z  noop

--- src/tests_generate.mjs
+++ src/tests_generate.mjs
@@ -12,13 +12,13 @@ export async function tests_generate(function_name, args_values_get) {
     let existing = {};
     let max = 10;
     for (let test_index of range(max)) {
-        let c = add_1(test_index);
+        let test_number = add_1(test_index);
         let attempts = 100;
         for (let j of range(attempts)) {
             let args = list_random_item(args_values);
             let args_json = json_to(args);
             if (!object_property_exists(existing, args_json)) {
-                await tests_generate_single(function_name, args, c);
+                await tests_generate_single(function_name, args, test_number);
                 break;
             }
         }
