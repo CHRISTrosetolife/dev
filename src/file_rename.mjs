6afc618453324175faee55fb1dd13ae491557d17
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-20 12:20:11 -0400

    2024-03-20T16:20:11.191Z  noop

--- src/file_rename.mjs
+++ src/file_rename.mjs
@@ -1,6 +1,4 @@
 import fs from 'fs';
-import { file_open } from './file_open.mjs';
-export async function file_copy(file_name_from, file_name_to) {
-    await fs.promises.copyFile(file_name_from, file_name_to);
-    await file_open(file_name_to);
+export async function file_rename(file_name_from, file_name_to) {
+  await fs.promises.rename(file_name_from, file_name_to);
 }
