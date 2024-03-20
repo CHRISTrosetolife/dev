7e8935b775dc1637d7fd57a8c0f204eb856d1b07
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-20 12:19:50 -0400

    2024-03-20T16:19:50.552Z  function_copy file_copy file_rename

---
+++ src/file_rename.mjs
@@ -0,0 +1,6 @@
+import fs from 'fs';
+import { file_open } from './file_open.mjs';
+export async function file_copy(file_name_from, file_name_to) {
+    await fs.promises.copyFile(file_name_from, file_name_to);
+    await file_open(file_name_to);
+}
