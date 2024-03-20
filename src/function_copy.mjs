d4d53c16f00908dc13e47105de935285e7214057
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-20 12:16:35 -0400

    2024-03-20T16:16:35.071Z  noop

--- src/function_copy.mjs
+++ src/function_copy.mjs
@@ -1,11 +1,5 @@
 import { file_copy } from "./file_copy.mjs";
-import { function_name_to_path } from "./function_name_to_path.mjs";
-import { file_js_identifier_rename } from "./file_js_identifier_rename.mjs";
-
 export async function function_copy(fn_name_from, fn_name_to) {
     let lambda = file_copy;
-    let fn_path_from = function_name_to_path(fn_name_from);
-    let fn_path_to = function_name_to_path(fn_name_to);
-    await file_copy(fn_path_from, fn_path_to);
-    await file_js_identifier_rename(fn_path_to, fn_name_from, fn_name_to)
+    function_copy_generic(lambda, fn_name_from, fn_name_to)
 }
