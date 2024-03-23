4c1d0bc351c7fe0546693a21f6d4c8fed424a587
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-23 17:39:59 -0400

    2024-03-23T21:39:59.504Z  noop

--- src/string_combine_multiple.mjs
+++ src/string_combine_multiple.mjs
@@ -1,3 +1,9 @@
-export function string_combine_multiple(list) {
+import { string_combine } from "./string_combine.mjs";
 
+export function string_combine_multiple(list) {
+    let result = '';
+    for (let l of list) {
+        result = string_combine(result, l);
+    }
+    return result;
 }
