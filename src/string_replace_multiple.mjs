a9915d75f94555b3ebdefd80374d059624803508
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-23 16:19:21 -0400

    2024-03-23T20:19:20.857Z  noop

--- src/string_replace_multiple.mjs
+++ src/string_replace_multiple.mjs
@@ -1,3 +1,6 @@
+import {string_replace} from './string_replace.mjs'
 export function string_replace_multiple(input, froms, to) {
-
+    for (let from of froms) {
+        input = string_replace(input, from, to)
+    }
 }
