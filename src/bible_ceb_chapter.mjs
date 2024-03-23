de46eadba9da73734d54169f2ad77c8ad5c6db16
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-23 15:47:40 -0400

    2024-03-23T19:47:40.372Z  noop

--- src/bible_ceb_chapter.mjs
+++ src/bible_ceb_chapter.mjs
@@ -5,6 +5,7 @@ import {html_parse} from './html_parse.mjs'
 import {string_split} from './string_split.mjs'
 import {list_map} from './list_map.mjs'
 import {string_trim} from './string_trim.mjs'
+import {list_get} from './list_get.mjs'
 export async function bible_ceb_chapter(chapter_name) {
     let folder = '.\\gitignore\\cebulb_html';
     let chapter_path = string_combine(chapter_name, '.htm')
@@ -15,5 +16,10 @@ export async function bible_ceb_chapter(chapter_name) {
     let rawText = verses.rawText;
     let split = string_split(rawText, '  ');
     let mapped = list_map(split, string_trim)
+    let mapped2 = list_map(mapped, m => {
+        let split = string_split(m, '&#160;');
+        let verse_number = list_get(split, 0);
+        let text = list_get(split, 1);
+    })
     return mapped;
 }
