2521d0d9dcdaf901a9e4aea957a4280b3e65757c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-23 15:38:04 -0400

    2024-03-23T19:38:03.936Z  bible_ceb_chapter JAS01

--- src/bible_ceb_chapter.mjs
+++ src/bible_ceb_chapter.mjs
@@ -2,11 +2,19 @@ import { file_read } from './file_read.mjs';
 import {path_join} from './path_join.mjs'
 import {string_combine} from './string_combine.mjs'
 import {html_parse} from './html_parse.mjs'
+import {list_map} from './list_map.mjs'
 export async function bible_ceb_chapter(chapter_name) {
     let folder = '.\\gitignore\\cebulb_html';
     let chapter_path = string_combine(chapter_name, '.htm')
     let joined = path_join([folder, chapter_path]);
     let read = await file_read(joined)
     let parsed = html_parse(read)
-    return parsed.querySelectorAll('.verse');
+    let verses = parsed.querySelectorAll('.verse');
+    list_map(verses, v => {
+        let id = v.id;
+        let text = v.text;
+        return {
+            id,text
+        }
+    })
 }
