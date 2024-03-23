0e4f65d53131b6ffa9e5d6a3c21a20dd7a23820b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-23 15:18:51 -0400

    2024-03-23T19:18:51.358Z  bible_ceb_chapter JAS01

--- src/bible_ceb_chapter.mjs
+++ src/bible_ceb_chapter.mjs
@@ -3,6 +3,6 @@ import {string_combine} from './string_combine.mjs'
 export function bible_ceb_chapter(chapter_name) {
     let folder = '.\\gitignore\\cebulb_html';
     let chapter_path = string_combine(chapter_name, '.html')
-    let joined = path_join([folder, chapter_name]);
+    let joined = path_join([folder, chapter_path]);
     return joined;
 }
