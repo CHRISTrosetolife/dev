58598ec4276d2cbe40365a054f0c2b2ffb30e59c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-23 16:07:53 -0400

    2024-03-23T20:07:52.815Z  noop

--- src/list_adder_unique.mjs
+++ src/list_adder_unique.mjs
@@ -1,3 +1,9 @@
 export function list_adder_unique(lambda) {
-
+    let result = [];
+    lambda(item => {
+        if (!list_includes(result, item)) {
+            list_add(result, item);
+        }
+    });
+    return result;
 }
