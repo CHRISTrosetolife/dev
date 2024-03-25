0e1a333981f2d19c378f5ccc5446aa13500d0305
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-25 17:25:59 -0400

    2024-03-25T21:25:59.020Z  noop

--- src/element.mjs
+++
@@ -1,4 +0,0 @@
-export function element(parent, tag_name) {
-    let result = document.createElement(tag_name);
-    parent.appendChild(result);
-}
--- src/html_element.mjs
+++ src/html_element.mjs
@@ -1,3 +1,5 @@
 export function html_element(parent, tag_name) {
-
+    let result = document.createElement(tag_name);
+    parent.appendChild(result);
+    return result
 }
