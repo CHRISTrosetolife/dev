68bc60435a6d907f9414e6104e343317fb42e70e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-25 18:07:44 -0400

    2024-03-25T22:07:44.061Z  noop

--- src/ceb_learn.mjs
+++ src/ceb_learn.mjs
@@ -2,6 +2,8 @@ import { ceb_translations } from "./ceb_translations.mjs";
 import { list_map } from "./list_map.mjs";
 import { object_properties } from "./object_properties.mjs";
 import { object_property_get } from "./object_property_get.mjs";
+import { string_length } from "./string_length.mjs";
+import { list_sort } from "./list_sort.mjs";
 
 export function ceb_learn() {
     let root = document.body;
@@ -14,4 +16,6 @@ export function ceb_learn() {
             value: object_property_get(translations, key),
         }
     })
+    list_sort(mapped, item => string_length(item.key));
+    console.log({mapped})
 }
