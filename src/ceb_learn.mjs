73890bb8cefd72f7cac01d2a842fc3345cf3cb95
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-25 18:01:00 -0400

    2024-03-25T22:01:00.127Z  function_search object

--- src/ceb_learn.mjs
+++ src/ceb_learn.mjs
@@ -1,4 +1,7 @@
+import { ceb_translations } from "./ceb_translations.mjs";
+
 export function ceb_learn() {
     let root = document.body;
     let button = html_element(root, 'button');
+    let translations = ceb_translations()
 }
