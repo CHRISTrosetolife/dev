da499f3fd217fa7df57ffb27d54679dd4411594d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-25 18:03:35 -0400

    2024-03-25T22:03:35.393Z  noop

--- src/ceb_learn.mjs
+++ src/ceb_learn.mjs
@@ -1,9 +1,17 @@
 import { ceb_translations } from "./ceb_translations.mjs";
+import { list_map } from "./list_map.mjs";
 import { object_properties } from "./object_properties.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 
 export function ceb_learn() {
     let root = document.body;
     let button = html_element(root, 'button');
     let translations = ceb_translations();
     let keys = object_properties(translations);
+    let mapped = list_map(keys, key => {
+        return {
+            key,
+            value: object_property_get(translations, key),
+        }
+    })
 }
