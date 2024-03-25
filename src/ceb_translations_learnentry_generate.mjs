2098223df5461a4f8f9a06377a2abc8d5713c4f9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-25 13:52:33 -0400

    2024-03-25T17:52:33.130Z  noop

--- src/ceb_translations_learnentry_generate.mjs
+++ src/ceb_translations_learnentry_generate.mjs
@@ -1,4 +1,5 @@
-export function ceb_translations_learnentry_generate() {
+export async function ceb_translations_learnentry_generate() {
     let folder = `/translations/ceb/learnentry/letters/`;
     let extension = `.txt`;
+    await folder_read(folder, extension)
 }
