9adc7654e2c82ce809544a6cb50597c7486cb329
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-26 15:16:42 -0400

    2024-03-26T19:16:41.238Z  deploy

--- src/ceb_learn.mjs
+++ src/ceb_learn.mjs
@@ -17,10 +17,12 @@ export function ceb_learn() {
     let button1 = html_element(root, 'div');
     html_style(button1, { 'font-size': button_font_size });
     html_style(button1, { 'font-weight': 'bold' });
+    html_style(button1, { 'text-align': 'center' });
     html_style_width_full(button1);
     let button2 = html_element(root, 'div');
     html_style(button2, { 'font-size': button_font_size });
     html_style(button2, { 'font-weight': 'bold' });
+    html_style(button2, { 'text-align': 'center' });
     html_style_width_full(button2);
     let button_previous = html_element(root, 'button');
     html_style(button_previous, { 'font-size': button_font_size });
