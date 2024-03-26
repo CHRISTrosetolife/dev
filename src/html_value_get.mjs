014be06267154199de8764768f122a1d0cc15667
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-26 15:27:07 -0400

    2024-03-26T19:27:06.150Z  noop

--- src/html_value_get.mjs
+++ src/html_value_get.mjs
@@ -1,6 +1,5 @@
-import { object_property_set } from "./object_property_set.mjs";
-
-export function html_value_set(component, value) {
-    let {element} = component;
-    object_property_set(element, 'value', value);
+import {object_property_get} from "./object_property_get.mjs";
+export function html_value_get(component, value) {
+  let {element} = component;
+  return object_property_get(element, 'value');
 }
