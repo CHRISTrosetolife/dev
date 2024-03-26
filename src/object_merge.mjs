bd1f621ebf435b4b2843af8f6c907e3351815b27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-26 13:23:45 -0400

    2024-03-26T17:23:43.255Z  function_search set

--- src/object_merge.mjs
+++ src/object_merge.mjs
@@ -1,3 +1,5 @@
-export function object_merge(to, from) {
+import { object_properties } from "./object_properties.mjs";
 
+export function object_merge(to, from) {
+    let from_properties = object_properties(from);
 }
