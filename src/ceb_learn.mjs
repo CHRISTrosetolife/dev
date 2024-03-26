a23b59b4a528f826bc710c9f7f36daab6f389424
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-26 13:39:18 -0400

    2024-03-26T17:39:18.273Z  noop

--- src/ceb_learn.mjs
+++ src/ceb_learn.mjs
@@ -8,16 +8,22 @@ import { html_element } from "./html_element.mjs";
 import { list_get } from "./list_get.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
 import { html_style } from "./html_style.mjs";
+import { html_style_width_full } from "./html_style_width_full.mjs";
 
 export function ceb_learn() {
     let root = document.body;
+    let button_font_size = '10vh';
     let button1 = html_element(root, 'button');
+    html_style(button1, { 'font-size': button_font_size });
     html_style_width_full(button1);
     let button2 = html_element(root, 'button');
+    html_style(button1, { 'font-size': button_font_size });
     html_style_width_full(button2);
     let button_previous = html_element(root, 'button');
+    html_style(button_previous, { 'font-size': button_font_size });
     html_inner_set(button_previous, 'previous');
     let button_next = html_element(root, 'button');
+    html_style(button_next, { 'font-size': button_font_size });
     html_inner_set(button_next, 'next');
     let translations = ceb_translations();
     let keys = object_properties(translations);
