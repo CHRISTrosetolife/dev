5d31360a0ea29ccc4712592da3b78c89e4506e08
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-26 13:30:20 -0400

    2024-03-26T17:30:19.817Z  noop

--- src/ceb_learn.mjs
+++ src/ceb_learn.mjs
@@ -5,6 +5,8 @@ import { object_property_get } from "./object_property_get.mjs";
 import { string_length } from "./string_length.mjs";
 import { list_sort } from "./list_sort.mjs";
 import { html_element } from "./html_element.mjs";
+import { list_get } from "./list_get.mjs";
+import { html_inner_set } from "./html_inner_set.mjs";
 
 export function ceb_learn() {
     let root = document.body;
@@ -19,5 +21,8 @@ export function ceb_learn() {
         }
     })
     list_sort(mapped, item => string_length(item.key));
-    console.log({mapped})
+    let index = 0;
+    let item = list_get(mapped, index)
+    html_inner_set(button1, item.key);
+    html_inner_set(button2, item.value);
 }
