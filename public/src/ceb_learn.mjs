233ea7fe4a883030bcc3208c2170180028caf12b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-03-26 13:31:31 -0400

    2024-03-26T17:31:31.186Z  deploy

--- public/src/ceb_learn.mjs
+++ public/src/ceb_learn.mjs
@@ -5,11 +5,16 @@ import { object_property_get } from "./object_property_get.mjs";
 import { string_length } from "./string_length.mjs";
 import { list_sort } from "./list_sort.mjs";
 import { html_element } from "./html_element.mjs";
+import { list_get } from "./list_get.mjs";
+import { html_inner_set } from "./html_inner_set.mjs";
+import { html_style } from "./html_style.mjs";
 
 export function ceb_learn() {
-    // test
     let root = document.body;
-    let button = html_element(root, 'button');
+    let button1 = html_element(root, 'button');
+    html_style(button1, {'width': '100%'});
+    let button2 = html_element(root, 'button');
+    html_style(button2, {'width': '100%'});
     let translations = ceb_translations();
     let keys = object_properties(translations);
     let mapped = list_map(keys, key => {
@@ -19,5 +24,8 @@ export function ceb_learn() {
         }
     })
     list_sort(mapped, item => string_length(item.key));
-    console.log({mapped})
+    let index = 0;
+    let item = list_get(mapped, index)
+    html_inner_set(button1, item.key);
+    html_inner_set(button2, item.value);
 }
