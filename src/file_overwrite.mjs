dcf02ecc2cf842944ffa06a62dacc31d510a0567
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 18:06:35 -0400

    2024-04-02T22:06:35.166Z  noop

--- src/file_overwrite.mjs
+++ src/file_overwrite.mjs
@@ -1,3 +1,4 @@
-export function file_overwrite(file_name, data) {
-
+import fs from 'fs';
+export async function file_overwrite(file_name, data) {
+    return await fs.promises.writeFile(file_name, data, 'utf-8');
 }
