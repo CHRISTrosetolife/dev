3e8565f8657c43209d96bdcdd4bd39f5d20e4922
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 17:07:31 -0400

    2024-04-02T21:07:31.437Z  noop

--- src/run_git.mjs
+++ src/run_git.mjs
@@ -1,5 +1,5 @@
 import { run } from "./run.mjs";
-import { git_acp_message } from "./git_acp_message.mjs";
+import { git_ac_message } from "./git_ac_message.mjs";
 import { process_argv_remaining } from "./process_argv_remaining.mjs";
 
 export async function run_git() {
@@ -7,6 +7,6 @@ export async function run_git() {
     let remaining = process_argv_remaining();
     remaining.splice(0, 0, ' ')
     let commit_message = remaining.join(' ');
-    await git_acp_message(commit_message);
+    await git_ac_message(commit_message);
     return result;
 }
