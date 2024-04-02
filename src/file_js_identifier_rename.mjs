7aa50a2d9d719832e2fbdcef8850a16c78f0ea38
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 17:49:49 -0400

    2024-04-02T21:49:48.762Z  noop

--- src/file_js_identifier_rename.mjs
+++ src/file_js_identifier_rename.mjs
@@ -2,7 +2,7 @@ import {js_identifier_rename} from "./js_identifier_rename.mjs";
 import {file_js_transform} from "./file_js_transform.mjs";
 export async function file_js_identifier_rename(file_path, identifier_from, identifier_to) {
   let lambda = js_identifier_rename
-  await file_js_transform(lambda, file_path, identifier_from, identifier_to);
+  await file_js_transform(lambda, file_path, [identifier_from, identifier_to]);
 }
 
 
