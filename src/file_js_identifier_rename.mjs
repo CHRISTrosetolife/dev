f525646d9c37e7c318c9cdc547c524b5d8245696
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 17:42:34 -0400

    2024-04-02T21:42:34.462Z  noop

--- src/file_js_identifier_rename.mjs
+++ src/file_js_identifier_rename.mjs
@@ -1,12 +1,5 @@
 import {file_js_parse} from "./file_js_parse.mjs";
-import {visit} from "./visit.mjs";
-import {object_values} from "./object_values.mjs";
-import {js_node_is} from "./js_node_is.mjs";
-import {list_is} from "./list_is.mjs";
-import {null_is} from "./null_is.mjs";
-import {equal} from "./equal.mjs";
-import {object_property_get} from "./object_property_get.mjs";
-import {object_property_set} from "./object_property_set.mjs";
+import {js_identifier_rename} from "./js_identifier_rename.mjs";
 import {file_js_unparse} from "./file_js_unparse.mjs";
 export async function file_js_identifier_rename(file_path, identifier_from, identifier_to) {
   let ast = await file_js_parse(file_path);
@@ -14,20 +7,4 @@ export async function file_js_identifier_rename(file_path, identifier_from, iden
   await file_js_unparse(file_path, ast);
 }
 
-function js_identifier_rename(ast, identifier_from, identifier_to) {
-  visit(ast, n => {
-    if (js_node_is(n)) {
-      return object_values(n);
-    }
-    if (list_is(n)) {
-      return n;
-    }
-    return [];
-  }, n => !null_is(n) && n.type === 'Identifier', v => {
-    let { node } = v;
-    let name = object_property_get(node, 'name');
-    if (equal(name, identifier_from)) {
-      object_property_set(node, 'name', identifier_to);
-    }
-  });
-}
+
