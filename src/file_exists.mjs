c714780501cf1630ad7a8820f18e0f050101e54a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 17:17:59 -0400

    2024-04-02T21:17:59.512Z  noop

--- src/file_exists.mjs
+++ src/file_exists.mjs
@@ -1,3 +1,13 @@
 export function file_exists(file_name) {
-
+    return new Promise((resolve, reject)=> {
+        fs.stat(file_name, function(err, stat) {
+            if (err == null) {
+                resolve(true);
+            } else if (err.code === 'ENOENT') {
+                resolve(false);
+            } else {
+                reject(err);
+            }
+        });
+    })
 }
