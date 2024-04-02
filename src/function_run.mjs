b31d252e8b77a6e738203a2ddb1ce689612af482
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 17:24:39 -0400

    2024-04-02T21:24:39.646Z  noop

--- src/function_run.mjs
+++ src/function_run.mjs
@@ -1,14 +1,14 @@
 import path from 'path';
 import { function_extension } from './function_extension.mjs';
-export async function function_run(first, args) {
+export async function function_run(function_name, args) {
     let extension = function_extension();
     let extension_marker = '.';
     let folder_current = '.';
-    let first_with_extension = [first, extension_marker, extension].join('');
+    let first_with_extension = [function_name, extension_marker, extension].join('');
     let function_path_second = path.join(folder_current, first_with_extension);
     let function_path = './' + function_path_second;
     let imported = await import(function_path);
-    let imported_function = imported[first];
+    let imported_function = imported[function_name];
     let result = await imported_function(...args);
     return result;
 }
