f78be052aef854bf912bed7de02f3b6d1ba4413e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 17:06:54 -0400

    2024-04-02T21:06:54.254Z  noop

--- src/git_ac_message.mjs
+++ src/git_ac_message.mjs
@@ -1,3 +1,10 @@
-export function git_ac_message(commit_message) {
-
+import { command_line_git } from "./command_line_git.mjs";
+export async function git_ac_message(commit_message) {
+    let commands = [
+        'add *',
+        `commit -m "${new Date().toISOString()}${commit_message}"`,
+    ];
+    for (let command of commands) {
+        await command_line_git(command);
+    }
 }
--- src/git_acp_message.mjs
+++ src/git_acp_message.mjs
@@ -1,15 +1,6 @@
 import { command_line_git } from "./command_line_git.mjs";
+import { git_ac_message } from "./git_ac_message.mjs";
 export async function git_acp_message(commit_message) {
     await git_ac_message(commit_message);
     await command_line_git('push');
-}
-
-async function git_ac_message(commit_message) {
-    let commands = [
-        'add *',
-        `commit -m "${new Date().toISOString()}${commit_message}"`,
-    ];
-    for (let command of commands) {
-        await command_line_git(command);
-    }
 }
