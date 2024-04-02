c4d1c7f9f2adde0a42bfe0b7efb11929b3d1dea9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 17:08:33 -0400

    2024-04-02T21:08:33.242Z  noop

--- src/git_acp_message.mjs
+++ src/git_acp_message.mjs
@@ -2,5 +2,9 @@ import { command_line_git } from "./command_line_git.mjs";
 import { git_ac_message } from "./git_ac_message.mjs";
 export async function git_acp_message(commit_message) {
     await git_ac_message(commit_message);
+    await git_push();
+}
+async function git_push() {
     await command_line_git('push');
 }
+
