e9093a455d2fe7884c715d2caa9428a111226150
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 18:03:37 -0400

    2024-04-02T22:03:37.002Z  noop

--- src/function_transform.mjs
+++ src/function_transform.mjs
@@ -3,9 +3,9 @@ import { function_name_to_path } from "./function_name_to_path.mjs";
 import { function_import } from "./function_import.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 import { string_combine } from "./string_combine.mjs";
-export function function_transform(lambda_name, function_name, args) {
+export async function function_transform(lambda_name, function_name, args) {
     let concated = string_combine('js_', lambda_name)
-    let lambda = function_import(concated)
+    let lambda = await function_import(concated)
     let args_split = string_split_comma(args);
     let file_path = function_name_to_path(function_name)
     file_js_transform(lambda, file_path, args_split)
