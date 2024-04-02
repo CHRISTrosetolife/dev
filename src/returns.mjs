846bb2a7bdc039ccb0d7b52a8f712e563e663315
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 17:26:42 -0400

    2024-04-02T21:26:41.984Z  noop

--- src/returns.mjs
+++ src/returns.mjs
@@ -1,5 +1,8 @@
 
+import { assert } from './assert.mjs';
+import { equal } from './equal.mjs';
 import function_run from './function_run.mjs';
 export async function returns(function_name, expected, args) {
     let actual = await function_run(function_name, args)
+    assert(equal(actual, expected));
 }
