6f2b54eb9d585149118f7d7760fbac9863bc67ef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 17:52:50 -0400

    2024-04-02T21:52:50.062Z  function_open function_run

--- src/function_transform.mjs
+++ src/function_transform.mjs
@@ -1,3 +1,7 @@
-export function function_transform(lambda, function_name, args) {
+import { file_js_transform } from "./file_js_transform.mjs";
+import { function_name_to_path } from "./function_name_to_path.mjs";
 
+export function function_transform(lambda_name, function_name, args) {
+    let file_path = function_name_to_path(function_name)
+    file_js_transform(lambda, file_path, args)
 }
