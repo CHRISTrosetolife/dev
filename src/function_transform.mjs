4e3f3c61241bfef01635ddbd8b50691668223a9f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 17:54:54 -0400

    2024-04-02T21:54:54.092Z  noop

--- src/function_transform.mjs
+++ src/function_transform.mjs
@@ -2,6 +2,7 @@ import { file_js_transform } from "./file_js_transform.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 
 export function function_transform(lambda_name, function_name, args) {
+    let lambda = function_import(lambda_name)
     let file_path = function_name_to_path(function_name)
     file_js_transform(lambda, file_path, args)
 }
