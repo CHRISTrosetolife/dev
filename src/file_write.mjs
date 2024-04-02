f79de711f08b7ea1c3af160a2c211d8ac92898d9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 17:19:21 -0400

    2024-04-02T21:19:21.136Z  noop

--- src/file_write.mjs
+++ src/file_write.mjs
@@ -2,7 +2,6 @@ import fs from 'fs';
 import assert from './assert.mjs';
 import file_exists from './file_exists.mjs';
 export async function file_write(file_name, data) {
-    if (false)
-        assert(file_exists());
+    assert(!file_exists(file_name));
     return await fs.promises.writeFile(file_name, data, 'utf-8');
 }
