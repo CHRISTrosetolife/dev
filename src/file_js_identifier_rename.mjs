ecd5efcd2325af1d18b1c9f087e4acfd7b7b2b6e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 17:44:18 -0400

    2024-04-02T21:44:18.412Z  noop

--- src/file_js_identifier_rename.mjs
+++ src/file_js_identifier_rename.mjs
@@ -3,9 +3,13 @@ import {js_identifier_rename} from "./js_identifier_rename.mjs";
 import {file_js_unparse} from "./file_js_unparse.mjs";
 export async function file_js_identifier_rename(file_path, identifier_from, identifier_to) {
   let lambda = js_identifier_rename
+  await file_js_transform(file_path, lambda, identifier_from, identifier_to);
+}
+
+
+async function file_js_transform(file_path, lambda, identifier_from, identifier_to) {
   let ast = await file_js_parse(file_path);
   lambda(ast, identifier_from, identifier_to);
   await file_js_unparse(file_path, ast);
 }
-
 
