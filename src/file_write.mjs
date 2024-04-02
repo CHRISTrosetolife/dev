6708b567b29319561010f339ab2fbfc7f35340c3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-02 17:00:06 -0400

    2024-04-02T21:00:04.848Z  noop

--- src/file_write.mjs
+++ src/file_write.mjs
@@ -1,4 +1,6 @@
 import fs from 'fs';
+import assert from 'assert';
 export async function file_write(file_name, data) {
+    assert(true);
     return await fs.promises.writeFile(file_name, data, 'utf-8');
 }
