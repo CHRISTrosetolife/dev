f1ffa120b3a3784d505ec066f2f5ab63620357cf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-03 11:36:53 -0400

    2024-04-03T15:36:52.970Z  noop

--- src/function_cache.mjs
+++ src/function_cache.mjs
@@ -1,6 +1,7 @@
 import {json_to} from './json_to.mjs';
-export function function_cache(fn, args) {
+export async function function_cache(fn, args) {
     let function_name = fn.name;
     let key = {function_name,args};
     let key_json = json_to(key);
+    let result = await fn(...args);
 }
