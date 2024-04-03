8ce2a90a026d4eaf6ca6796c1336d6ab92cceba9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-03 11:28:05 -0400

    2024-04-03T15:28:05.510Z  noop

--- src/function_cache.mjs
+++ src/function_cache.mjs
@@ -1,4 +1,5 @@
+import {json_to} from './json_to.mjs';
 export function function_cache(function_name, args) {
     let key = {function_name,args};
-    let key_json = 0;
+    let key_json = json_to(key);
 }
