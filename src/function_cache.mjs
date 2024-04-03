1a6b5498f04f2444e707d142f687fd42439f9405
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-03 12:01:57 -0400

    2024-04-03T16:01:57.146Z  noop

--- src/function_cache.mjs
+++ src/function_cache.mjs
@@ -6,11 +6,13 @@ import { string_combine } from './string_combine.mjs';
 import { file_exists } from './file_exists.mjs';
 import { file_read } from './file_read.mjs';
 import { file_write } from './file_write.mjs';
+import { string_encoded_to } from './string_encoded_to.mjs';
 export async function function_cache(fn, args) {
     let function_name = fn.name;
     let key = {function_name,args};
     let key_json = json_to(key);
-    let file_name = string_combine(key_json, '.json')
+    let key_encoded = string_encoded_to(key);
+    let file_name = string_combine(key_encoded, '.json')
     let file_path = path_join([folder_gitignore(), 'cache', file_name]); 
     let result;
     if (await file_exists(file_path)) {
