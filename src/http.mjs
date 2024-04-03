1809e9abbe0c89218010ae3b918f32c304371714
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-03 13:53:26 -0400

    2024-04-03T17:53:25.702Z  noop

--- src/http.mjs
+++ src/http.mjs
@@ -1,5 +1,8 @@
 import fetch from 'node-fetch';
+import { sleep } from './sleep.mjs';
+import { integer_random } from './integer_random.mjs';
 export async function http(url) {
+    await sleep(integer_random(5000, 8000));
     let response = await fetch(url);
     let body = await response.text();
     return body;
