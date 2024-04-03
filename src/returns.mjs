32e205013404135b15c28c9ff2fbfb1aad7481fc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-03 11:51:54 -0400

    2024-04-03T15:51:54.185Z  noop

--- src/returns.mjs
+++ src/returns.mjs
@@ -1,6 +1,6 @@
 import {assert_boolean} from './assert_boolean.mjs';
 import {equal} from './equal.mjs';
-export async function returns(fn, expected, args) {
+export function returns(fn, expected, args) {
     let actual = fn(...args);
     console.log({actual})
     let e = equal(actual, expected);
