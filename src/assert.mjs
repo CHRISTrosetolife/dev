af01d633edc58e1e7aafd86f2b9c6fbc51883ef6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-03 10:50:44 -0400

    2024-04-03T14:50:44.259Z  noop

--- src/assert.mjs
+++ src/assert.mjs
@@ -1,4 +1,4 @@
 import { returns } from "./returns.mjs";
 export function assert(fn, args) {
-    returns(fn, true, args)
+    returns(fn, true, args);
 }
--- src/assert_not.mjs
+++ src/assert_not.mjs
@@ -1,3 +1,4 @@
+import { returns } from "./returns.mjs";
 export function assert_not(fn, args) {
-
+    returns(fn, false, args);
 }
