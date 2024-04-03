97c2d5f71730e934969be0f6b4ad740712dacbf5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-03 10:16:39 -0400

    2024-04-03T14:16:39.208Z  noop

--- src/file_js_unparse.mjs
+++ src/file_js_unparse.mjs
@@ -2,8 +2,5 @@ import {file_overwrite} from './file_overwrite.mjs';
 import {js_unparse} from './js_unparse.mjs';
 export async function file_js_unparse(file_name, ast) {
     let code = js_unparse(ast);
-    console.log({
-        code
-    });
     await file_overwrite(file_name, code);
 }
--- src/function_transform_all.mjs
+++ src/function_transform_all.mjs
@@ -3,9 +3,6 @@ import {function_transform} from "./function_transform.mjs";
 export async function function_transform_all(lambda_name, args) {
     let function_names_result = await function_names();
     for (let function_name of function_names_result) {
-        console.log({
-            function_name
-        });
         await function_transform(lambda_name, function_name, args);
     }
 }
