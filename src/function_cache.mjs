26e1ed9de2e1d24d918ee8266e8d727b6cd390f4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-03 11:43:37 -0400

    2024-04-03T15:43:36.905Z  noop

--- src/function_cache.mjs
+++ src/function_cache.mjs
@@ -1,8 +1,10 @@
 import {json_to} from './json_to.mjs';
+import {json_from} from './json_from.mjs';
 import {path_join} from './path_join.mjs';
 import {folder_gitignore} from './folder_gitignore.mjs';
 import { string_combine } from './string_combine.mjs';
 import { file_exists } from './file_exists.mjs';
+import { file_read } from '../public/src/file_read.mjs';
 export async function function_cache(fn, args) {
     let function_name = fn.name;
     let key = {function_name,args};
@@ -11,7 +13,9 @@ export async function function_cache(fn, args) {
     let file_path = path_join([folder_gitignore(), 'cache', file_name]); 
     let result;
     if (await file_exists(file_path)) {
-        
+        let json = await file_read(file_path);
+        result = json_from(json);
+        return result;
     }
     result = await fn(...args);
 }
