2aa864c9ae97cb48d18c1c97705c43311f481c3f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-03 10:51:16 -0400

    2024-04-03T14:51:16.338Z  noop

--- src/file_write.mjs
+++ src/file_write.mjs
@@ -1,7 +1,7 @@
-import {assert_boolean} from './assert_boolean.mjs';
+import {assert_not} from './assert_not.mjs';
 import {file_exists} from './file_exists.mjs';
 import {file_overwrite} from './file_overwrite.mjs';
 export async function file_write(file_name, data) {
-    assert_boolean(!file_exists(file_name));
+    assert_not(file_exists, file_name);
     return await file_overwrite(file_name, data);
 }
