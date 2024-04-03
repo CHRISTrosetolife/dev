4bd0239de34e85757bfe18aa4c3d5f9b5ec99408
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-03 11:51:29 -0400

    2024-04-03T15:51:28.875Z  noop

--- src/returns.mjs
+++ src/returns.mjs
@@ -2,6 +2,7 @@ import {assert_boolean} from './assert_boolean.mjs';
 import {equal} from './equal.mjs';
 export async function returns(fn, expected, args) {
     let actual = fn(...args);
+    console.log({actual})
     let e = equal(actual, expected);
     assert_boolean(e);
 }
