b64bf40d38728cb7aed3f6bd4509e71dd69c31d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-13 16:32:59 -0400

    2024-04-13T20:32:59.723Z  bible_ceb_chapter JAS01

--- src/ceb_definition.mjs
+++ src/ceb_definition.mjs
@@ -3,10 +3,11 @@ import {http_cache} from './http_cache.mjs';
 import {html_parse} from './html_parse.mjs';
 import { list_map } from './list_map.mjs';
 import { object_property_get } from './object_property_get.mjs';
-import { string_ends_with } from './string_ends_with.mjs';
+import { string_starts_with } from './string_starts_with.mjs';
 import { string_suffix_without } from './string_suffix_without.mjs';
 import { string_trim } from './string_trim.mjs';
 import { list_unique } from './list_unique.mjs';
+import { list_filter } from './list_filter.mjs';
 export async function ceb_definition(word) {
     let url = string_combine_multiple([
         'https://www.binisaya.com/node/21?search=binisaya&word=',
@@ -15,8 +16,12 @@ export async function ceb_definition(word) {
     ]);
     let html = await http_cache(url);
     let parsed = html_parse(html)
-    let q = parsed.querySelectorAll('a[href^="https:\/\/www.binisaya.com\/english\/"]');
-    let mapped = list_map(q, i => object_property_get(i, 'text'));
+    let q = parsed.querySelectorAll('a[href]');
+    let filtered = list_filter(q, i => {
+        let href = i.getAttribute('href');
+        return string_starts_with(href, 'https://www.binisaya.com/english/')
+    })
+    let mapped = list_map(filtered, i => object_property_get(i, 'text'));
     let mapped3 = list_map(mapped, i => string_trim(i));
     let u = list_unique(mapped3);
     return u
