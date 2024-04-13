963818e56521646c60d8aac140ab9b3e2889dcae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-13 16:02:19 -0400

    2024-04-13T20:02:19.432Z  noop

--- src/ceb_definition.mjs
+++ src/ceb_definition.mjs
@@ -1,9 +1,9 @@
 import {string_combine_multiple} from './string_combine_multiple.mjs';
 import {http_cache} from './http_cache.mjs';
-export async function ceb_definition(w) {
+export async function ceb_definition(word) {
     let url = string_combine_multiple([
         'https://www.binisaya.com/node/21?search=binisaya&word=',
-        w,
+        word,
         '&Search=Search'
     ]);
     return url;
