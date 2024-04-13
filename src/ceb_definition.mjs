6e3f2f3cbc26eff46b3eac8ae853643339bcb5b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-13 16:18:19 -0400

    2024-04-13T20:18:18.882Z  function_search trim

--- src/ceb_definition.mjs
+++ src/ceb_definition.mjs
@@ -3,6 +3,8 @@ import {http_cache} from './http_cache.mjs';
 import {html_parse} from './html_parse.mjs';
 import { list_map } from './list_map.mjs';
 import { object_property_get } from './object_property_get.mjs';
+import { string_ends_with } from './string_ends_with.mjs';
+import { string_suffix_without } from './string_suffix_without.mjs';
 export async function ceb_definition(word) {
     let url = string_combine_multiple([
         'https://www.binisaya.com/node/21?search=binisaya&word=',
@@ -14,5 +16,21 @@ export async function ceb_definition(word) {
     let q = parsed.querySelectorAll('td[halign="left"]');
     let mapped = list_map(q, i => object_property_get(i, 'text'));
     let endings = ['(n.)']
+    let mapped2 = list_map(mapped, i => {
+        for (let e of endings) {
+            if (string_ends_with(i, e)) {
+                return string_suffix_without(i, e);
+            }
+        }
+        return i;
+    });
+    let mapped3 = list_map(mapped2, i => {
+        for (let e of endings) {
+            if (string_ends_with(i, e)) {
+                return string_suffix_without(i, e);
+            }
+        }
+        return i;
+    });
     return mapped
 }
