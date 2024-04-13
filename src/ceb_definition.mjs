943b0833aeba844284e285034df11325c08fb1fc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-13 16:12:18 -0400

    2024-04-13T20:12:17.725Z  ceb_definition dios

--- src/ceb_definition.mjs
+++ src/ceb_definition.mjs
@@ -1,6 +1,8 @@
 import {string_combine_multiple} from './string_combine_multiple.mjs';
 import {http_cache} from './http_cache.mjs';
 import {html_parse} from './html_parse.mjs';
+import { list_map } from './list_map.mjs';
+import { object_property_get } from './object_property_get.mjs';
 export async function ceb_definition(word) {
     let url = string_combine_multiple([
         'https://www.binisaya.com/node/21?search=binisaya&word=',
@@ -10,5 +12,5 @@ export async function ceb_definition(word) {
     let html = await http_cache(url);
     let parsed = html_parse(html)
     let q = parsed.querySelectorAll('td[halign="left"]');
-    return q;
+    return list_map(q, i => object_property_get(i, 'rawText'));
 }
