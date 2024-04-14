5bdb8d31ee4ebf6db9375ecba8518bd4d27a047f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-14 12:23:11 -0400

    2024-04-14T16:23:11.561Z  deploy

--- public/src/app_bible.mjs
+++ public/src/app_bible.mjs
@@ -1,5 +1,27 @@
+import { assert } from './assert.mjs';
 import { bible_data_jas01 } from './bible_data_jas01.mjs';
+import { equal_by } from './equal_by.mjs';
+import { html_element } from './html_element.mjs';
+import { list_length } from './list_length.mjs';
+import { range } from './range.mjs';
+import { html_inner_set } from './html_inner_set.mjs';
+import { list_get } from './list_get.mjs';
+import { object_property_get } from './object_property_get.mjs';
+import { list_join } from './list_join.mjs';
 export function app_bible() {
+    let root = document.body;
     let data = bible_data_jas01();
-    console.log({data})
+    let {eng} = data;
+    let {ceb} = data;
+    assert(equal_by, [eng, ceb, list_length])
+    let length = list_length(eng);
+    for (let i of range(length)) {
+        let verse = html_element(root, 'div');
+        let first = html_element(verse, 'div');
+        let eng_i = list_get(eng, i);
+        let eng_tokens = object_property_get(eng_i, 'tokens');
+        let first_text = list_join(eng_tokens, ' ')
+        html_inner_set(first, first_text);
+        let second = html_element(verse, 'div');
+    }
 }
