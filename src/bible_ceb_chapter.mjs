b2bc1b76c5a41568a2e13b3894b9c24b7053b9c5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-13 20:44:02 -0400

    2024-04-14T00:44:02.675Z  noop

--- src/bible_ceb_chapter.mjs
+++ src/bible_ceb_chapter.mjs
@@ -21,8 +21,8 @@ import {object_property_set} from './object_property_set.mjs';
 export async function bible_ceb_chapter(chapter_name) {
     let parsed_bsb = await bible_chapter_parsed('engbsb_html', chapter_name);
     let parsed_ceb = await bible_chapter_parsed('cebulb_html', chapter_name);
-    let verses = parsed_ceb.querySelector('.p');
-    let rawText = verses.rawText;
+    let verses_ceb = parsed_ceb.querySelector('.p');
+    let rawText = verses_ceb.rawText;
     let split = string_split(rawText, '  ');
     let mapped = list_map(split, string_trim);
     let mapped2 = list_map(mapped, m => {
