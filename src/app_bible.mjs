8024ac5c93fd8d9e6165332e7d2e97f3bc33c37b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-14 12:15:26 -0400

    2024-04-14T16:15:25.988Z  noop

--- src/app_bible.mjs
+++ src/app_bible.mjs
@@ -1,6 +1,10 @@
+import { assert } from './assert.mjs';
 import { bible_data_jas01 } from './bible_data_jas01.mjs';
+import { equal_by } from './equal_by.mjs';
+import { list_length } from './list_length.mjs';
 export function app_bible() {
     let data = bible_data_jas01();
     let {eng} = data;
     let {ceb} = data;
+    assert(equal_by, [eng, ceb, list_length])
 }
