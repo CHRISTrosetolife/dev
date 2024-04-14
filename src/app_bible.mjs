c3227a6cdf16ebe4ceab7ca162e49b436ef8f8bd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-14 12:10:00 -0400

    2024-04-14T16:09:59.886Z  deploy

--- src/app_bible.mjs
+++ src/app_bible.mjs
@@ -1,4 +1,4 @@
-import { bible_data_jas01 } from 'bible_data_jas01.mjs';
+import { bible_data_jas01 } from './bible_data_jas01.mjs';
 export function app_bible() {
     let data = bible_data_jas01();
     console.log({data})
