735b10488bbd41f7d0d24601df5e4b9b518f238f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-14 12:14:52 -0400

    2024-04-14T16:14:52.323Z  noop

--- src/app_bible.mjs
+++ src/app_bible.mjs
@@ -1,5 +1,6 @@
 import { bible_data_jas01 } from './bible_data_jas01.mjs';
 export function app_bible() {
     let data = bible_data_jas01();
-    console.log({data})
+    let {eng} = data;
+    let {ceb} = data;
 }
