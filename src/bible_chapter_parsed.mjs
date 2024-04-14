e57fba86c51e26e67d53ca200798198ad6304158
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-13 20:40:42 -0400

    2024-04-14T00:40:41.768Z  noop

--- src/bible_chapter_parsed.mjs
+++ src/bible_chapter_parsed.mjs
@@ -1,3 +1,9 @@
-export function bible_chapter_parsed(bible_folder, chapter_name) {
-
+export async function bible_chapter_parsed(bible_folder, chapter_name) {
+    let folder_gitignore_result = folder_gitignore();
+    let folder = path_join([folder_gitignore_result, bible_folder]);
+    let chapter_path = string_combine(chapter_name, '.htm');
+    let joined = path_join([folder, chapter_path]);
+    let read = await file_read(joined);
+    let parsed = html_parse(read);
+    return parsed;
 }
