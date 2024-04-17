2b75f7eebf3a67e2fb25258cd9aa5b81d08b4453
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-16 22:42:30 -0400

    2024-04-17T02:42:29.636Z  noop

--- public/src/js_imports_add.mjs
+++ public/src/js_imports_add.mjs
@@ -1,8 +1,15 @@
 import {list_adder} from "./list_adder.mjs";
 import {js_visit} from "./js_visit.mjs";
+import {js_visit_node} from "./js_visit_node.mjs";
 export async function js_imports_add(ast) {
     let existing = list_adder(la => {
-
+        js_visit_node(ast, 'ImportSpecifier', v => {
+            let {node} = v;
+            let {imported} = node;
+            if (imported.type === 'Identifier') {
+                la(imported.name)
+            }
+        });
     })
     let names = await function_names();
     js_visit(ast, v => {
