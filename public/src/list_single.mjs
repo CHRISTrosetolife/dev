085c03688a9273cf24c45ff187498aedb514532a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 16:29:38 -0400

    2024-04-17T20:29:37.279Z  noop

--- public/src/list_single.mjs
+++ public/src/list_single.mjs
@@ -1,7 +1,9 @@
 import {list_length} from './list_length.mjs';
 import {equal} from './equal.mjs';
 import {assert} from './assert.mjs';
+import {list_first} from './list_first.mjs';
 export function list_single(list) {
     let length = list_length(list);
     assert(equal, [length, 1])
+    return list_first(list);
 }
