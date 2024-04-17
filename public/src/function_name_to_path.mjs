f89bc7f3b7067ca9ed30cf4e3b48157ffd6bdab1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 17:33:20 -0400

    2024-04-17T21:33:19.560Z  function_transform js_imports_add function_name_to_path a

--- public/src/function_name_to_path.mjs
+++ public/src/function_name_to_path.mjs
@@ -1,7 +1,9 @@
+import {folder_path_src} from "./folder_path_src.mjs";
 import {folder_name_public} from "./folder_name_public.mjs";
 import {folder_name_src} from './folder_name_src.mjs';
 import {function_extension} from './function_extension.mjs';
 export function function_name_to_path(function_name) {
+    if (false) folder_path_src;
     let a = `${folder_name_public()}/${folder_name_src()}/`;
     return `./${a}${function_name}.${function_extension()}`;
 }
