54148ab0b4244e9ffc5855b66dded2c1bbe57274
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-16 22:30:56 -0400

    2024-04-17T02:30:55.449Z  function_transform js_node_types js_node_types .

--- public/src/js_node_types.mjs
+++ public/src/js_node_types.mjs
@@ -1,11 +1,11 @@
-import { list_adder_unique } from './list_adder_unique.mjs';
-import { js_visit } from './js_visit.mjs';
+import {list_adder_unique} from './list_adder_unique.mjs';
+import {js_visit} from './js_visit.mjs';
 export function js_node_types(ast) {
     list_adder_unique(la => {
         js_visit(ast, lambda);
         function lambda(v) {
             let {node} = v;
-            la (node.type);
+            la(node.type);
         }
-    })
+    });
 }
