f2bf5c53408138be563ea5d72830978fbd5e3ecc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 16:03:46 -0400

    2024-04-17T20:03:44.003Z  function_transform js_imports_add js_imports_add .

--- public/src/js_imports_add.mjs
+++ public/src/js_imports_add.mjs
@@ -4,12 +4,14 @@ import {js_imports_existing} from "./js_imports_existing.mjs";
 import {list_intersect} from "./list_intersect.mjs";
 import {function_names} from "./function_names.mjs";
 export async function js_imports_add(ast) {
-    let existing = js_imports_existing(ast)
+    let existing = js_imports_existing(ast);
     let names = await function_names();
-    let identifiers = js_identifiers(ast)
+    let identifiers = js_identifiers(ast);
     let identifier_fns = list_intersect(identifiers, names);
-    console.log({identifiers,identifier_fns})
-    // identifiers that are not existing , that are in names
+    console.log({
+        identifiers,
+        identifier_fns
+    });
     js_visit(ast, v => {
         let {node} = v;
     });
