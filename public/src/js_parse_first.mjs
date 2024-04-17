b00adce8ce38a09fb79263537e70ca3230b3b448
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 17:00:44 -0400

    2024-04-17T21:00:43.741Z  noop

--- public/src/js_parse_first.mjs
+++ public/src/js_parse_first.mjs
@@ -1,3 +1,8 @@
+import {js_parse} from "./js_parse.mjs";
+import {list_first} from "./list_first.mjs";
 export function js_parse_first(code) {
-
+    let parsed = js_parse(code);
+    let { body } = parsed;
+    let first = list_first(body);
+    return first;
 }
