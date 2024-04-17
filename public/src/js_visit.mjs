ef2319555d8f6ea53b9d0ab85ef2500e1ee86ce2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-16 22:23:38 -0400

    2024-04-17T02:23:37.910Z  function_copy js_visit_node js_visit

---
+++ public/src/js_visit.mjs
@@ -0,0 +1,28 @@
+import {visit} from "./visit.mjs";
+import {object_values} from "./object_values.mjs";
+import {js_node_is} from "./js_node_is.mjs";
+import {list_is} from "./list_is.mjs";
+import {null_is} from "./null_is.mjs";
+import {undefined_is} from "./undefined_is.mjs";
+export function js_visit_node(ast, type, lambda) {
+    js_visit(ast, lambda_outer);
+    function lambda_outer(v) {
+        let {node} = v;
+        if (node.type !== type) {
+            return;
+        }
+        lambda(v);
+    }
+}
+function js_visit(ast, lambda_outer) {
+    visit(ast, n => {
+        if (js_node_is(n)) {
+            return object_values(n);
+        }
+        if (list_is(n)) {
+            return n;
+        }
+        return [];
+    }, n => !null_is(n) && !undefined_is(n), lambda_outer);
+}
+
