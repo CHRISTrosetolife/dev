df0ac1d75cec434e66064ee372a8f9272e7d53b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 17:19:19 -0400

    2024-04-17T21:19:18.479Z  function_transform js_imports_add js_code_import .

--- public/src/js_code_import.mjs
+++ public/src/js_code_import.mjs
@@ -1,4 +1,8 @@
+import {js_code_statement} from "./js_code_statement.mjs";
 export function js_code_import(import_name) {
-    let import_code = js_code_statement(`import { ${import_name} } from "./${import_name}.mjs"`);
+    if (false) {
+        let import_code = js_code_statement(`import { ${import_name} } from "./${import_name}.mjs"`);
+    }
+    let import_code = `import { ${import_name} } from "./${import_name}.mjs";`;
     return import_code;
 }
