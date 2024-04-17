bd6e1e6b35356f0e494c30d6096a97737c6f493d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 17:42:16 -0400

    2024-04-17T21:42:13.410Z  function_transform js_imports_add app_bible .

--- public/src/app_bible.mjs
+++ public/src/app_bible.mjs
@@ -1,32 +1,35 @@
-import { assert } from './assert.mjs';
-import { bible_data_jas01 } from './bible_data_jas01.mjs';
-import { equal_by } from './equal_by.mjs';
-import { html_element } from './html_element.mjs';
-import { list_length } from './list_length.mjs';
-import { range } from './range.mjs';
-import { html_inner_set } from './html_inner_set.mjs';
-import { list_get } from './list_get.mjs';
-import { object_property_get } from './object_property_get.mjs';
-import { noop } from './noop.mjs';
-import { html_style } from './html_style.mjs';
-import { html_on_click } from './html_on_click.mjs';
-import { string_case_lower } from './string_case_lower.mjs';
-import { bible_token_normalize } from './bible_token_normalize.mjs';
+import {html_document_body} from "./html_document_body.mjs";
+import {assert} from './assert.mjs';
+import {bible_data_jas01} from './bible_data_jas01.mjs';
+import {equal_by} from './equal_by.mjs';
+import {html_element} from './html_element.mjs';
+import {list_length} from './list_length.mjs';
+import {range} from './range.mjs';
+import {html_inner_set} from './html_inner_set.mjs';
+import {list_get} from './list_get.mjs';
+import {object_property_get} from './object_property_get.mjs';
+import {noop} from './noop.mjs';
+import {html_style} from './html_style.mjs';
+import {html_on_click} from './html_on_click.mjs';
+import {string_case_lower} from './string_case_lower.mjs';
+import {bible_token_normalize} from './bible_token_normalize.mjs';
 export function app_bible() {
     let root = html_document_body();
-    let root_component = {element:root}
-    html_style(root_component, { 
+    let root_component = {
+        element: root
+    };
+    html_style(root_component, {
         'font-family': 'Sans-Serif',
         'font-size': '5vh',
-        'margin': '0',
+        'margin': '0'
     });
     let top = html_element(root_component, 'div');
-    html_style(top, { 
+    html_style(top, {
         'height': '75vh',
-        'overflow-y': 'scroll',
+        'overflow-y': 'scroll'
     });
     let bottom = html_element(root_component, 'div');
-    html_style(bottom, { 
+    html_style(bottom, {
         'height': '25vh',
         'overflow-y': 'scroll',
         'background-color': 'lightgreen'
@@ -35,7 +38,7 @@ export function app_bible() {
     let {eng} = data;
     let {ceb} = data;
     let {definitions} = data;
-    assert(equal_by, [eng, ceb, list_length])
+    assert(equal_by, [eng, ceb, list_length]);
     let length = list_length(eng);
     for (let i of range(length)) {
         let verse = html_element(top, 'div');
@@ -45,38 +48,35 @@ export function app_bible() {
         let ceb_i = list_get(ceb, i);
         let ceb_tokens = object_property_get(ceb_i, 'tokens');
         let verse_number = object_property_get(eng_i, 'verse_number');
-        let verse_number_element = html_element(first, 'span')
+        let verse_number_element = html_element(first, 'span');
         html_inner_set(verse_number_element, verse_number);
-        html_style(verse_number_element, { 
-            'font-weight': 'bold',
+        html_style(verse_number_element, {
+            'font-weight': 'bold'
         });
         html_spaced_tokens(first, eng_tokens, noop);
         let second = html_element(verse, 'div');
-        html_style(second, { 
-            'color': 'green',
+        html_style(second, {
+            'color': 'green'
         });
         html_spaced_tokens(second, ceb_tokens, (token_element, token) => {
             html_on_click(token_element, () => {
-                html_inner_set(bottom, '')
+                html_inner_set(bottom, '');
                 let n = bible_token_normalize(token);
-                let d = object_property_get(definitions, n)
+                let d = object_property_get(definitions, n);
                 html_spaced_tokens(bottom, d, () => {
                     let spacer = html_element(bottom, 'span');
                     html_inner_set(spacer, ',');
                 });
-            })
+            });
         });
     }
 }
-
-
-
 function html_spaced_tokens(parent, tokens, each) {
     for (let token of tokens) {
         let spacer = html_element(parent, 'span');
         html_inner_set(spacer, ' ');
         let token_element = html_element(parent, 'span');
         html_inner_set(token_element, token);
-        each(token_element, token)
+        each(token_element, token);
     }
 }
