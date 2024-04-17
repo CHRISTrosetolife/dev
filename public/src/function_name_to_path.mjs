bcf049cac1e54d20fa129fea0cd0a617902fa9c4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 17:31:17 -0400

    2024-04-17T21:31:16.809Z  noop

--- public/src/function_name_to_path.mjs
+++ public/src/function_name_to_path.mjs
@@ -2,5 +2,6 @@ import {folder_name_public} from "./folder_name_public.mjs";
 import {folder_name_src} from './folder_name_src.mjs';
 import {function_extension} from './function_extension.mjs';
 export function function_name_to_path(function_name) {
-    return `./${folder_name_public()}/${folder_name_src()}/${function_name}.${function_extension()}`;
+    let a = `${folder_name_public()}/${folder_name_src()}/`;
+    return `./${a}${function_name}.${function_extension()}`;
 }
