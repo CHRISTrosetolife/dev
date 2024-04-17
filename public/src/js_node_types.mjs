83d3b7f05fa9920f7e8b42a88c7c346f82015308
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-16 22:27:56 -0400

    2024-04-17T02:27:55.322Z  function_search adder

--- public/src/js_node_types.mjs
+++ public/src/js_node_types.mjs
@@ -1,3 +1,11 @@
 export function js_node_types(ast) {
-
+    list_adder()
+    js_visit(ast, lambda_outer);
+    function lambda_outer(v) {
+        let {node} = v;
+        if (node.type !== type) {
+            return;
+        }
+        lambda(v);
+    }
 }
