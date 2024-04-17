95a58e6116d6908bae972bf03c1e5ca60e72d4a2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 16:35:31 -0400

    2024-04-17T20:35:30.305Z  noop

--- public/src/js_export_single.mjs
+++ public/src/js_export_single.mjs
@@ -1,3 +1,6 @@
-export function js_export_single(ast) {
+import { js_exports } from "./js_exports.mjs";
 
+export function js_export_single(ast) {
+    let exports = js_exports(ast);
+    return list_single(exports);
 }
