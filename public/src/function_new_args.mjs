38281d8664df20a5102de2f1da2c30878761f8c9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-16 21:32:44 -0400

    2024-04-17T01:32:43.797Z  noop

--- public/src/function_new_args.mjs
+++ public/src/function_new_args.mjs
@@ -3,5 +3,6 @@ import {string_split_comma} from "./string_split_comma.mjs";
 export async function function_new_args(function_name, args) {
     let args_split = string_split_comma(args);
     let args_joined = args_split.join(', ');
-    await function_new_generic(function_name, args_joined, '', true, []);
+    await function_new_generic(
+        function_name, args_joined, '', true, []);
 }
