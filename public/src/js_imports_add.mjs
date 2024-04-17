5284e99778188938620a0c72a055811d69654da8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 16:39:01 -0400

    2024-04-17T20:38:59.936Z  noop

--- public/src/js_imports_add.mjs
+++ public/src/js_imports_add.mjs
@@ -3,7 +3,9 @@ import {js_imports_existing} from "./js_imports_existing.mjs";
 import {list_intersect} from "./list_intersect.mjs";
 import {function_names} from "./function_names.mjs";
 import {list_difference} from "./list_difference.mjs";
+import {js_export_single} from "./js_export_single.mjs";
 export async function js_imports_add(ast) {
+    let e = js_export_single(ast)
     let existing = js_imports_existing(ast);
     let names = await function_names();
     let identifiers = js_identifiers(ast);
