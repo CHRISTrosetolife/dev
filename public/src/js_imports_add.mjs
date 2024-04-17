75eb4120652b943d86bf1e7a89bd44da6fec2354
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-16 22:17:10 -0400

    2024-04-17T02:17:10.571Z  noop

--- public/src/js_imports_add.mjs
+++ public/src/js_imports_add.mjs
@@ -1,3 +1,14 @@
+import {equal} from "./equal.mjs";
+import {object_property_get} from "./object_property_get.mjs";
+import {object_property_set} from "./object_property_set.mjs";
+import {js_visit_identifiers} from "./js_visit_identifiers.mjs";
 export async function js_imports_add(ast) {
     let names = await function_names();
+    js_visit_identifiers(ast, v => {
+        let {node} = v;
+        let name = object_property_get(node, 'name');
+        if (equal(name, identifier_from)) {
+            object_property_set(node, 'name', identifier_to);
+        }
+    });
 }
