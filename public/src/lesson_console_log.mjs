8fca61a7976091dfaf33565e6358986a497348dc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 19:10:07 -0400

    2024-04-17T23:10:01.461Z  function_transform js_outside_move lesson_console_log .

--- public/src/lesson_console_log.mjs
+++ public/src/lesson_console_log.mjs
@@ -1,21 +1,17 @@
 export function lesson_console_log() {
     return {
         description: 'outputting a string to the console',
-        screens: [
-            parent => {
-                let text = 'a computer program has three parts :';
-                html_span_text(parent, text);
-                ' (1) an input , (2) a process and (3) an output'
-            }
-        ]
-    }
+        screens: [parent => {
+            let text = 'a computer program has three parts :';
+            html_span_text(parent, text);
+            ' (1) an input , (2) a process and (3) an output';
+        }]
+    };
 }
-
 function html_span_text(parent, text) {
     let a = html_span(parent);
     html_inner_set(a, text);
 }
-
 function html_span(parent) {
     return html_element(parent, 'span');
 }
