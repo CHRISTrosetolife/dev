c8ef9e099bf43427af0a1905962a2dff2324b8a5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-16 22:43:58 -0400

    2024-04-17T02:43:57.753Z  function_transform js_node_type js_imports_existing js_imports_existing

--- public/src/js_imports_existing.mjs
+++ public/src/js_imports_existing.mjs
@@ -4,6 +4,9 @@ export async function js_imports_existing(ast) {
     let existing = list_adder(la => {
         js_visit_node(ast, 'ImportSpecifier', v => {
             let {node} = v;
+            console.log({
+                node
+            });
             let {imported} = node;
             if (imported.type === 'Identifier') {
                 la(imported.name);
