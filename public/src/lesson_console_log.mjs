c91000a6208af0147c6ff7e810852e45b6e6e08c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 18:08:07 -0400

    2024-04-17T22:08:06.750Z  noop

--- public/src/lesson_console_log.mjs
+++ public/src/lesson_console_log.mjs
@@ -2,7 +2,10 @@ export function lesson_console_log() {
     return {
         description: 'outputting a string to the console',
         screens: [
-            'a computer program has three parts : (1) an input , (2) a process and (3) an output'
+            parent => {
+                html_element(parent, 'span')
+                'a computer program has three parts : (1) an input , (2) a process and (3) an output'
+            }
         ]
     }
 }
