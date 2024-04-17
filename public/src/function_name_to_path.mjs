42509d31046a20670612d46ba0ba8d23f49559cd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 17:30:16 -0400

    2024-04-17T21:30:14.216Z  function_transform js_imports_add function_name_to_path .

--- public/src/function_name_to_path.mjs
+++ public/src/function_name_to_path.mjs
@@ -1,5 +1,7 @@
+import {folder_name_public} from "./folder_name_public.mjs";
 import {folder_name_src} from './folder_name_src.mjs';
 import {function_extension} from './function_extension.mjs';
 export function function_name_to_path(function_name) {
+    if (false) folder_name_public();
     return `./public/${folder_name_src()}/${function_name}.${function_extension()}`;
 }
