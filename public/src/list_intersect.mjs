97f6c1da8602b175b89626396b47cf89a0f69132
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 15:51:37 -0400

    2024-04-17T19:51:36.439Z  noop

--- public/src/list_intersect.mjs
+++ public/src/list_intersect.mjs
@@ -1,3 +1,12 @@
-export function list_intersect(a, b) {
+import { list_adder } from "./list_adder.mjs";
+import { list_includes } from "./list_includes.mjs";
 
+export function list_intersect(a, b) {
+    return list_adder(la => {
+        for (let element of a) {
+            if (list_includes(b, element)) {
+                la(element)
+            }
+        }
+    })
 }
