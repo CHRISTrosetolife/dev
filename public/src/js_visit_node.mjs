92d89cfbed99e411abe1293de73f53d725a55492
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-16 22:22:24 -0400

    2024-04-17T02:22:23.787Z  noop

--- public/src/js_visit_node.mjs
+++ public/src/js_visit_node.mjs
@@ -13,7 +13,7 @@ export function js_visit_node(ast, type, lambda) {
             return n;
         }
         return [];
-    }, n => !null_is(n) && !undefined_is(n), v => {
+    }, n => !null_is(n) && !undefined_is(n), function (v) {
         let {node} = v;
         if (node.type !== type) {
             return;
