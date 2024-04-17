60a18836e985c87d3bc42afc09b12461741526ef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 15:44:46 -0400

    2024-04-17T19:44:45.557Z  noop

--- public/src/js_imports_add.mjs
+++ public/src/js_imports_add.mjs
@@ -5,6 +5,7 @@ export async function js_imports_add(ast) {
     let existing = js_imports_existing(ast)
     let names = await function_names();
     let identifiers = js_identifiers(ast)
+    // identifiers that are not existing , that are in names
     js_visit(ast, v => {
         let {node} = v;
     });
