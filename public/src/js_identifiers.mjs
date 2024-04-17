51508d69954fc3b209b648b6f7660097c0265064
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 15:41:31 -0400

    2024-04-17T19:41:30.215Z  noop

--- public/src/js_identifiers.mjs
+++ public/src/js_identifiers.mjs
@@ -1,6 +1,10 @@
 import {js_visit_identifiers} from './js_visit_identifiers.mjs'
+import { list_adder } from './list_adder.mjs'
 export function js_identifiers(ast) {
-    js_visit_identifiers(ast, v => {
-        
+    return list_adder(la => {
+        js_visit_identifiers(ast, v => {
+            let {node} = v;
+            la(node);
+        })
     })
 }
