7167d10933f46d208a617a9ed4349dd069813226
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 17:05:30 -0400

    2024-04-17T21:05:29.274Z  noop

--- public/src/file_js_transform.mjs
+++ public/src/file_js_transform.mjs
@@ -2,7 +2,7 @@ import {file_js_parse} from "./file_js_parse.mjs";
 import {file_js_unparse} from "./file_js_unparse.mjs";
 export async function file_js_transform(lambda, file_path, args) {
     let ast = await file_js_parse(file_path);
-    let result = lambda(ast, ...args);
+    let result = await lambda(ast, ...args);
     await file_js_unparse(file_path, ast);
     return result;
 }
