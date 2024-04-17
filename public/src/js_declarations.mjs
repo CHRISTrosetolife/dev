5fb6381d3f621bc95623f5865899ed4c2d148f1f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 16:19:22 -0400

    2024-04-17T20:19:20.312Z  function_copy js_identifiers js_declarations

---
+++ public/src/js_declarations.mjs
@@ -0,0 +1,10 @@
+import {js_visit_identifiers} from './js_visit_identifiers.mjs';
+import {list_adder_unique} from './list_adder_unique.mjs';
+export function js_identifiers(ast) {
+    return list_adder_unique(la => {
+        js_visit_identifiers(ast, v => {
+            let {node} = v;
+            la(node.name);
+        });
+    });
+}
