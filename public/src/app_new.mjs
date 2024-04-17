8fa6d6855debd62fd962899262197dba40760d67
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 17:27:19 -0400

    2024-04-17T21:27:18.277Z  noop

--- public/src/app_new.mjs
+++ public/src/app_new.mjs
@@ -1,5 +1,19 @@
 import {js_code_import} from "./js_code_import.mjs";
 export function app_new(name) {
-    js_code_import(name);
-    js_code_call(name);
+    let html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>${name}</title>
+      </head>
+      <body>
+        <script type="module">
+          ${js_code_import(name)}
+          ${js_code_call(name)};
+        </script>
+      </body>
+    </html>        
+`;
 }
