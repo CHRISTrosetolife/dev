acd2bf88912273f3486581179b490c2d5ec00354
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-16 21:48:22 -0400

    2024-04-17T01:48:21.088Z  noop

--- public/src/js_visit_identifiers.mjs
+++ public/src/js_visit_identifiers.mjs
@@ -1,3 +1,23 @@
+import {visit} from "./visit.mjs";
+import {object_values} from "./object_values.mjs";
+import {js_node_is} from "./js_node_is.mjs";
+import {list_is} from "./list_is.mjs";
+import {null_is} from "./null_is.mjs";
+import {undefined_is} from "./undefined_is.mjs";
 export function js_visit_identifiers(ast, lambda) {
-
+    visit(ast, n => {
+        if (js_node_is(n)) {
+            return object_values(n);
+        }
+        if (list_is(n)) {
+            return n;
+        }
+        return [];
+    }, n => !null_is(n) && !undefined_is(n), v => {
+        let {node} = v;
+        if (node.type !== 'Identifier') {
+            return;
+        }
+        lambda(v);
+    });
 }
