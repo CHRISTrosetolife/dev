ab6902687d91ac003d1e4cec490e91752268346e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 17:34:34 -0400

    2024-04-17T21:34:33.674Z  noop

--- public/src/folder_path_src.mjs
+++ public/src/folder_path_src.mjs
@@ -2,4 +2,5 @@ import {folder_name_src} from "./folder_name_src.mjs";
 import {folder_name_public} from "./folder_name_public.mjs";
 export function folder_path_src() {
     let a = `${folder_name_public()}/${folder_name_src()}/`;
+    return a;
 }
--- public/src/function_name_to_path.mjs
+++ public/src/function_name_to_path.mjs
@@ -3,7 +3,5 @@ import {folder_name_public} from "./folder_name_public.mjs";
 import {folder_name_src} from './folder_name_src.mjs';
 import {function_extension} from './function_extension.mjs';
 export function function_name_to_path(function_name) {
-    if (false) folder_path_src;
-    let a = `${folder_name_public()}/${folder_name_src()}/`;
-    return `./${a}${function_name}.${function_extension()}`;
+    return `./${folder_path_src()}${function_name}.${function_extension()}`;
 }
