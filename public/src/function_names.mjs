de0870e95179c7c8fdef93a34c0859e44288045b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 17:35:04 -0400

    2024-04-17T21:35:03.332Z  noop

--- public/src/function_names.mjs
+++ public/src/function_names.mjs
@@ -4,6 +4,7 @@ import {string_suffix_without} from './string_suffix_without.mjs';
 import {string_replace} from './string_replace.mjs';
 import {folder_read} from './folder_read.mjs';
 export async function function_names() {
+    if ( false) folder_path_src()
     let prefix = `public/src/`;
     let suffix = '.mjs';
     let files = await folder_read(prefix, suffix);
