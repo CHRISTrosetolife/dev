0443b1639d7ac8bc2e4c46f753d20384049d5eec
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 16:20:48 -0400

    2024-04-17T20:20:46.523Z  function_transform js_node_type js_declarations .

--- public/src/js_declarations.mjs
+++ public/src/js_declarations.mjs
@@ -1,8 +1,8 @@
-import {js_visit} from './js_visit.mjs';
+import {js_visit_node} from './js_visit_node.mjs';
 import {list_adder_unique} from './list_adder_unique.mjs';
 export function js_declarations(ast) {
     return list_adder_unique(la => {
-        js_visit(ast, 'ExportNamedDeclaration', v => {
+        js_visit_node(ast, 'ExportNamedDeclaration', v => {
             let {node} = v;
             la(node.name);
         });
