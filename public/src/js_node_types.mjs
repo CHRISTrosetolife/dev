787387fa4eb287497206b141e14dffbf18c64fee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-16 22:31:06 -0400

    2024-04-17T02:31:06.047Z  function_transform js_node_types js_node_types .

--- public/src/js_node_types.mjs
+++ public/src/js_node_types.mjs
@@ -1,7 +1,7 @@
 import {list_adder_unique} from './list_adder_unique.mjs';
 import {js_visit} from './js_visit.mjs';
 export function js_node_types(ast) {
-    list_adder_unique(la => {
+    return list_adder_unique(la => {
         js_visit(ast, lambda);
         function lambda(v) {
             let {node} = v;
