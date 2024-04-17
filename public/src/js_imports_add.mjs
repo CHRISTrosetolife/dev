911dd9924618edae47697c5ac7fb1c976d8f6556
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 16:02:41 -0400

    2024-04-17T20:02:40.673Z  noop

--- public/src/js_imports_add.mjs
+++ public/src/js_imports_add.mjs
@@ -2,6 +2,7 @@ import {js_identifiers} from "./js_identifiers.mjs";
 import {js_visit} from "./js_visit.mjs";
 import {js_imports_existing} from "./js_imports_existing.mjs";
 import {list_intersect} from "./list_intersect.mjs";
+import {function_names} from "./function_names.mjs";
 export async function js_imports_add(ast) {
     let existing = js_imports_existing(ast)
     let names = await function_names();
