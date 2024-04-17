5e4dff63d6214f62ef98a6e3fe7813006b2cc868
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-16 22:44:36 -0400

    2024-04-17T02:44:35.693Z  function_transform js_node_type js_imports_existing js_imports_existing

--- public/src/js_visit_node.mjs
+++ public/src/js_visit_node.mjs
@@ -3,6 +3,9 @@ export function js_visit_node(ast, type, lambda) {
     js_visit(ast, lambda_outer);
     function lambda_outer(v) {
         let {node} = v;
+        console.log({
+            node
+        });
         if (node.type !== type) {
             return;
         }
