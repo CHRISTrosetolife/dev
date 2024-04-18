e607e1a1e1237f545347300e16fc21372d447664
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 20:33:44 -0400

    2024-04-18T00:33:44.151Z  function_transform js_outside_move lesson_console_log .

--- public/src/js_outside_move.mjs
+++ public/src/js_outside_move.mjs
@@ -1,5 +1,5 @@
 import {js_exports} from "./js_exports.mjs";
 export function js_outside_move(ast) {
-    let exports = js_exports(ast);
-    console.log({exports})
+    let {body} = ast;
+    console.log({body})
 }
