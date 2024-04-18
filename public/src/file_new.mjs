f8222246316dd5c263535ac0566638ab3a7b9245
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 22:24:48 -0400

    2024-04-18T02:24:47.617Z  function_transform js_imports_add file_new .

--- public/src/file_new.mjs
+++ public/src/file_new.mjs
@@ -1,7 +1,8 @@
+import {assert_not_async} from "./assert_not_async.mjs";
 import {assert_async} from "./assert_async.mjs";
 import {file_exists} from "./file_exists.mjs";
 import fs from 'fs';
 export async function file_new(file_new_name, contents) {
-    await assert_async(file_exists, [file_new_name]);
+    await assert_not_async(file_exists, [file_new_name]);
     await fs.promises.writeFile(file_new_name, contents);
 }
