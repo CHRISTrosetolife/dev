c5777915e6211fdfd588b937eb4faf3f580c4c44
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 20:40:43 -0400

    2024-04-18T00:40:41.822Z  function_transform js_imports_add assert_async .

--- public/src/assert_async.mjs
+++ public/src/assert_async.mjs
@@ -1,4 +1,5 @@
-import { returns } from "./returns.mjs";
-export function assert(fn, args) {
-    returns(fn, true, args);
+import {returns_async} from "./returns_async.mjs";
+import {returns} from "./returns.mjs";
+export async function assert_async(fn, args) {
+    await returns_async(fn, true, args);
 }
