4f0af4f7cc691149947c5c6d01b183f1b9491daa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 20:34:55 -0400

    2024-04-18T00:34:55.163Z  noop

--- public/src/js_outside_move.mjs
+++ public/src/js_outside_move.mjs
@@ -1,5 +1,6 @@
 import {js_exports} from "./js_exports.mjs";
 export function js_outside_move(ast) {
     let {body} = ast;
+    let declarations = list_filter(body, b => b.type === 'FunctionDeclaration')
     console.log({body})
 }
