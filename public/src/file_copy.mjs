38f7a1871aa3a46fe53205cd864e10af103ddd21
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 20:43:08 -0400

    2024-04-18T00:43:07.782Z  noop

--- public/src/file_copy.mjs
+++ public/src/file_copy.mjs
@@ -1,6 +1,9 @@
 import fs from 'fs';
 import {file_open} from './file_open.mjs';
+import { assert_not_async } from './assert_not_async.mjs';
+import { file_exists } from './file_exists.mjs';
 export async function file_copy(file_name_from, file_name_to) {
+    await assert_not_async(file_exists, [file_name_to])
     await fs.promises.copyFile(file_name_from, file_name_to);
     await file_open(file_name_to);
 }
