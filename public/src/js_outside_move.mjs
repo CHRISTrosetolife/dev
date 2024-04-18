b58c7b342d6f5062d11e048e3f98ac200e074d37
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-17 20:35:34 -0400

    2024-04-18T00:35:32.517Z  function_transform js_imports_add js_outside_move .

--- public/src/js_outside_move.mjs
+++ public/src/js_outside_move.mjs
@@ -1,6 +1,9 @@
+import {list_filter} from "./list_filter.mjs";
 import {js_exports} from "./js_exports.mjs";
 export function js_outside_move(ast) {
     let {body} = ast;
-    let declarations = list_filter(body, b => b.type === 'FunctionDeclaration')
-    console.log({body})
+    let declarations = list_filter(body, b => b.type === 'FunctionDeclaration');
+    console.log({
+        declarations
+    });
 }
