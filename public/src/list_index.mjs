4952950cdbffd3790eba563f52ae8e9bac63a5c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 06:58:20 -0400

    2024-04-19T10:58:20.254Z  function_transform js_imports_add list_index .

--- public/src/list_index.mjs
+++ public/src/list_index.mjs
@@ -1,3 +1,6 @@
+import {assert_not} from "./assert_not.mjs";
 export function list_index(list, element) {
     let index = list.indexOf(element);
+    assert_not(number_less_than, [index, 0]);
+    return index;
 }
