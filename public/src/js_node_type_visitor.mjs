78065748bee40efb721d1166e7eb1969e8d0a1b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 16:44:44 -0400

    2024-04-19T20:44:43.324Z  function_copy js_node_type js_node_type_visitor

---
+++ public/src/js_node_type_visitor.mjs
@@ -0,0 +1,13 @@
+import {list_adder_unique} from './list_adder_unique.mjs';
+import {js_visit} from './js_visit.mjs';
+export function js_node_type(ast, type) {
+    return list_adder_unique(la => {
+        js_visit(ast, lambda);
+        function lambda(v) {
+            let {node} = v;
+            if (node.type === type) {
+                la(node);
+            }
+        }
+    });
+}
