1a5b0d168ba1ed67e97a9e8ae200db8dda59faac
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 18:27:08 -0400

    2024-04-19T22:27:07.411Z  function_transform js_imports_add js_object_init_functionize a

--- public/src/js_object_init_functionize.mjs
+++ public/src/js_object_init_functionize.mjs
@@ -1,10 +1,16 @@
+import {list_adder_unique} from "./list_adder_unique.mjs";
 import {js_visit_node} from "./js_visit_node.mjs";
 export function js_object_init_functionize(ast) {
-    js_visit_node(ast, 'VariableDeclarator', v => {
-        let {node} = v;
-        let {init} = node;
-        console.log({
-            init
+    list_adder_unique(la => {
+        js_visit_node(ast, 'VariableDeclarator', v => {
+            let {node} = v;
+            let {init} = node;
+            if (init.type === 'ObjectExpression') {
+                la(v);
+            }
+            console.log({
+                init
+            });
         });
     });
 }
