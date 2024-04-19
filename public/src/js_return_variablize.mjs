7b834944a0dc8701c0461eec1991b4a2b6f8026d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 16:45:34 -0400

    2024-04-19T20:45:34.232Z  function_transform js_outside_move js_return_variablize .

--- public/src/js_return_variablize.mjs
+++ public/src/js_return_variablize.mjs
@@ -1,3 +1,4 @@
+import {js_node_type_visitor} from "./js_node_type_visitor.mjs";
 import {js_node_type} from "./js_node_type.mjs";
 import {list_adder_unique} from "./list_adder_unique.mjs";
 import {returns} from "./returns.mjs";
@@ -5,7 +6,6 @@ import {js_node_types} from "./js_node_types.mjs";
 import {js_visit_node} from "./js_visit_node.mjs";
 import {list_adder} from "./list_adder.mjs";
 export function js_return_variablize(ast) {
-    let return_statements = js_node_type(ast, 'ReturnStatement');
-    return_statements;
+    let return_statements = js_node_type_visitor(ast, 'ReturnStatement');
     return js_node_types(ast);
 }
