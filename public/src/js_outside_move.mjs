ee7ffccc6a371cd790dbb0dc73268bdd04e29d78
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 07:12:24 -0400

    2024-04-19T11:12:23.793Z  function_transform js_imports_add js_outside_move .

--- public/src/js_outside_move.mjs
+++ public/src/js_outside_move.mjs
@@ -1,9 +1,11 @@
+import {list_remove} from "./list_remove.mjs";
 import {js_parse} from "./js_parse.mjs";
 import {js_unparse} from "./js_unparse.mjs";
 import {function_new_generic} from "./function_new_generic.mjs";
 import {js_declaration_to_name} from "./js_declaration_to_name.mjs";
 import {list_filter} from "./list_filter.mjs";
 import {js_exports} from "./js_exports.mjs";
+import {list_copy} from "./list_copy.mjs";
 export async function js_outside_move(ast) {
     let {body} = ast;
     let declarations = list_filter(body, b => b.type === 'FunctionDeclaration');
@@ -13,11 +15,7 @@ export async function js_outside_move(ast) {
         let {body: body2} = body;
         let parsed = js_parse(``);
         parsed.body = body2;
-        console.log({
-            function_name,
-            d: js_unparse(parsed)
-        });
-        continue;
         await function_new_generic(function_name, '', '', false, []);
+        list_remove(body, declaration);
     }
 }
--- public/src/list_copy.mjs
+++ public/src/list_copy.mjs
@@ -1,3 +1,3 @@
 export function list_copy(list) {
-
+    return list_skip(list, 0);
 }
