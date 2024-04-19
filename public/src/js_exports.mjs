b658dd4056dd62097d673382c8687b77ff0cd810
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 16:40:43 -0400

    2024-04-19T20:40:42.464Z  function_transform js_outside_move js_exports .

--- public/src/js_exports.mjs
+++ public/src/js_exports.mjs
@@ -2,11 +2,5 @@ import {js_node_type} from "./js_node_type.mjs";
 import {js_visit_node} from './js_visit_node.mjs';
 import {list_adder_unique} from './list_adder_unique.mjs';
 export function js_exports(ast) {
-    return list_adder_unique(la => {
-        js_visit_node(ast, 'ExportNamedDeclaration', v => {
-            let {node} = v;
-            la(node);
-        });
-    });
     return js_node_type(ast, 'ExportNamedDeclaration');
 }
