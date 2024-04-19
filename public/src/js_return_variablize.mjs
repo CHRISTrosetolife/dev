e7900d0b56d580abe82c067b6af2e7542ff7947f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 18:02:23 -0400

    2024-04-19T22:02:22.354Z  function_transform js_imports_add js_return_variablize .

--- public/src/js_return_variablize.mjs
+++ public/src/js_return_variablize.mjs
@@ -7,16 +7,16 @@ import {returns} from "./returns.mjs";
 import {js_node_types} from "./js_node_types.mjs";
 import {js_visit_node} from "./js_visit_node.mjs";
 import {list_adder} from "./list_adder.mjs";
-import { list_index } from "./list_index.mjs";
-import { js_code_statement } from "./js_code_statement.mjs";
-import { js_parse } from "./js_parse.mjs";
-import { js_identifiers } from "./js_identifiers.mjs";
-import { string_combine } from "./string_combine.mjs";
-import { list_includes } from "./list_includes.mjs";
-import { js_parse_first } from "./js_parse_first.mjs";
-import { error } from "./error.mjs";
-import { js_unparse } from "./js_unparse.mjs";
-import { list_first } from "./list_first.mjs";
+import {list_index} from "./list_index.mjs";
+import {js_code_statement} from "./js_code_statement.mjs";
+import {js_parse} from "./js_parse.mjs";
+import {js_identifiers} from "./js_identifiers.mjs";
+import {string_combine} from "./string_combine.mjs";
+import {list_includes} from "./list_includes.mjs";
+import {js_parse_first} from "./js_parse_first.mjs";
+import {error} from "./error.mjs";
+import {js_unparse} from "./js_unparse.mjs";
+import {list_first} from "./list_first.mjs";
 export function js_return_variablize(ast) {
     let return_statements = js_node_type_visitor(ast, 'ReturnStatement');
     for (let r of return_statements) {
@@ -37,19 +37,18 @@ export function js_return_variablize(ast) {
             }
         }
         const code = js_code_statement(`let ${variable_name} = 0`);
-        let parsed = js_parse_first(code)
+        let parsed = js_parse_first(code);
         parent.splice(index, 0, parsed);
-        let {declarations} = parsed
-        let first = list_first(declarations)
-        first.init = node.argument
+        let {declarations} = parsed;
+        let first = list_first(declarations);
+        first.init = node.argument;
         let parsed2 = js_parse_expression(variable_name);
-        node.argument = parsed2
+        node.argument = parsed2;
     }
 }
 function js_parse_expression(variable_name) {
     const identifier_code = js_code_statement(variable_name);
     let parsed2 = js_parse_first(identifier_code);
-    let { expression } = parsed2;
+    let {expression} = parsed2;
     return parsed2;
 }
-
