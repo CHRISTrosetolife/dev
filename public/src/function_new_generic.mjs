48f4de7c06aa82a13d553e01d0bb183bf6227e63
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 07:18:38 -0400

    2024-04-19T11:18:37.937Z  noop

--- public/src/function_new_generic.mjs
+++ public/src/function_new_generic.mjs
@@ -6,6 +6,8 @@ import {js_code_import} from './js_code_import.mjs';
 import {list_concat} from './list_concat.mjs';
 import {list_join} from './list_join.mjs';
 import {newline} from './newline.mjs';
+import { js_parse } from './js_parse.mjs';
+import { js_unparse } from './js_unparse.mjs';
 export async function function_new_generic(function_name, args_string, body_string, open, imports) {
     let contents_function = `export function ${function_name}(${args_string}) {
 ${body_string}
@@ -13,8 +15,10 @@ ${body_string}
     let mapped = list_map(imports, js_code_import);
     let concat = list_concat(mapped, [contents_function]);
     let contents = list_join(concat, newline());
+    let parsed = js_parse(contents);
+    let unparsed = js_unparse(parsed);
     let file_path = function_name_to_path(function_name);
-    await file_new(file_path, contents);
+    await file_new(file_path, unparsed);
     if (open) {
         await file_open(file_path);
     }
