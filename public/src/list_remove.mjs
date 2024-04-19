f09a8508eac85c10953ec0ec6df2fe71c48ff45d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 07:35:15 -0400

    2024-04-19T11:35:15.296Z  noop

--- public/src/list_remove.mjs
+++ public/src/list_remove.mjs
@@ -5,6 +5,8 @@ import { number_less_than } from "./number_less_than.mjs";
 export function list_remove(list, element) {
     let index = list_index(list, element);
     assert_not(number_less_than, [0, index])
+    console.log({list})
     list.splice(index, 1);
+    console.log({list})
     assert_not(list_includes, [list, element])
 }
