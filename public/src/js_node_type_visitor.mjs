0d4b59817ee1716b52608cd2f035318bc45a1f0a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 16:45:12 -0400

    2024-04-19T20:45:11.590Z  noop

--- public/src/js_node_type_visitor.mjs
+++ public/src/js_node_type_visitor.mjs
@@ -1,12 +1,12 @@
 import {list_adder_unique} from './list_adder_unique.mjs';
 import {js_visit} from './js_visit.mjs';
-export function js_node_type(ast, type) {
+export function js_node_type_visitor(ast, type) {
     return list_adder_unique(la => {
         js_visit(ast, lambda);
         function lambda(v) {
             let {node} = v;
             if (node.type === type) {
-                la(node);
+                la(v);
             }
         }
     });
