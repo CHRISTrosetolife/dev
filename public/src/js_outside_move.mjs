488bdef71bfe61145226900cb5392aa46020b19b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-18 20:49:31 -0400

    2024-04-19T00:49:30.816Z  function_transform js_imports_add js_outside_move .

--- public/src/js_outside_move.mjs
+++ public/src/js_outside_move.mjs
@@ -1,3 +1,4 @@
+import {js_parse} from "./js_parse.mjs";
 import {js_unparse} from "./js_unparse.mjs";
 import {function_new_generic} from "./function_new_generic.mjs";
 import {js_declaration_to_name} from "./js_declaration_to_name.mjs";
@@ -8,10 +9,12 @@ export async function js_outside_move(ast) {
     let declarations = list_filter(body, b => b.type === 'FunctionDeclaration');
     for (let declaration of declarations) {
         let function_name = js_declaration_to_name(declaration);
-        let {body} = declaration
+        let {body} = declaration;
+        let parsed = js_parse(``);
+        parsed.body = body;
         console.log({
             function_name,
-            d: js_unparse(body)
+            d: js_unparse(parsed)
         });
         continue;
         await function_new_generic(function_name, '', '', false, []);
