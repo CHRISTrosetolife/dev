d0c0dd45d853b64413becc891bd2400fce7a1717
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 07:38:43 -0400

    2024-04-19T11:38:43.304Z  noop

---
+++ public/src/html_span_text.mjs
@@ -0,0 +1,4 @@
+export function html_span_text(parent, text) {
+    let a = html_span(parent);
+    html_inner_set(a, text);
+}
--- public/src/list_remove.mjs
+++ public/src/list_remove.mjs
@@ -3,9 +3,8 @@ import { list_includes } from "./list_includes.mjs";
 import {list_index} from "./list_index.mjs";
 import { number_less_than } from "./number_less_than.mjs";
 export function list_remove(list, element) {
-    console.log({list, element})
     let index = list_index(list, element);
-    assert_not(number_less_than, [0, index])
+    assert_not(number_less_than, [index, 0])
     list.splice(index, 1);
     assert_not(list_includes, [list, element])
 }
