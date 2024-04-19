2e5fb16dad339dcc07c58ae092065fc4d47d9144
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 18:25:45 -0400

    2024-04-19T22:25:45.239Z  function_transform js_object_init_functionize lesson_console_log a

--- public/src/js_object_init_functionize.mjs
+++ public/src/js_object_init_functionize.mjs
@@ -2,8 +2,9 @@ import {js_visit_node} from "./js_visit_node.mjs";
 export function js_object_init_functionize(ast) {
     js_visit_node(ast, 'VariableDeclarator', v => {
         let {node} = v;
+        let {init} = node;
         console.log({
-            node
+            init
         });
     });
 }
