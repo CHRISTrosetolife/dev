610de48d6795265e2334c562418508cd08973cc5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 07:58:56 -0400

    2024-04-19T11:58:56.107Z  noop

--- public/src/lesson_console_log.mjs
+++ public/src/lesson_console_log.mjs
@@ -4,8 +4,8 @@ export function lesson_console_log() {
         screens: [parent => {
             let text = 'a computer program has three parts :';
             html_span_text(parent, text);
-            ' (1) an input ,'; 
-            '(2) a process and ';
+            '(1) an input ,'; 
+            '(2) a process and';
             '(3) an output';
         }]
     };
