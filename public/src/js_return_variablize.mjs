5cf349bb0047910b8542148cc7ed3d976e7583e6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 16:23:11 -0400

    2024-04-19T20:23:10.788Z  noop

--- public/src/js_return_variablize.mjs
+++ public/src/js_return_variablize.mjs
@@ -1,4 +1,12 @@
 import {js_node_types} from "./js_node_types.mjs";
+import { js_visit_node } from "./js_visit_node.mjs";
+import { list_adder } from "./list_adder.mjs";
 export function js_return_variablize(ast) {
+    let returns = list_adder_unique(la => {
+        js_visit_node(ast, 'ReturnStatement', v => {
+            let {node} = v;
+            la(node);
+        })
+    })
     return js_node_types(ast);
 }
