ac3e8a14a5c7d0f21ec0e2faae1a3c8283498223
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 18:27:56 -0400

    2024-04-19T22:27:56.148Z  noop

--- public/src/js_object_init_functionize.mjs
+++ public/src/js_object_init_functionize.mjs
@@ -1,7 +1,7 @@
 import {list_adder_unique} from "./list_adder_unique.mjs";
 import {js_visit_node} from "./js_visit_node.mjs";
 export function js_object_init_functionize(ast) {
-    list_adder_unique(la => {
+    let vs = list_adder_unique(la => {
         js_visit_node(ast, 'VariableDeclarator', v => {
             let {node} = v;
             let {init} = node;
@@ -13,4 +13,7 @@ export function js_object_init_functionize(ast) {
             });
         });
     });
+    for (let v of vs) {
+
+    }
 }
