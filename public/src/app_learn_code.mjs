8a0b0be9f47f70e1dd5e125c0fff540f2c3ebbc2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 08:17:25 -0400

    2024-04-19T12:17:25.492Z  noop

--- public/src/app_learn_code.mjs
+++ public/src/app_learn_code.mjs
@@ -1,4 +1,6 @@
 import {html_document_body} from "./html_document_body.mjs";
 export function app_learn_code() {
     let root = html_document_body();
+    let lesson = lesson_console_log();
+    let {screens} = lesson;
 }
