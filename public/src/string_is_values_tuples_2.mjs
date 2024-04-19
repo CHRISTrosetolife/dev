b5c84288486cb6d9c3a01066859c7203b54646fd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 07:29:38 -0400

    2024-04-19T11:29:37.448Z  function_transform js_imports_add string_is_values_tuples_2 .

--- public/src/string_is_values_tuples_2.mjs
+++ public/src/string_is_values_tuples_2.mjs
@@ -1,7 +1,8 @@
+import {list_join_comma} from "./list_join_comma.mjs";
 import {tuples_2} from "./tuples_2.mjs";
 import {string_is_values} from "./string_is_values.mjs";
-import { list_join } from "./list_join.mjs";
+import {list_join} from "./list_join.mjs";
 export function string_is_values_tuples_2() {
     let s = string_is_values.name;
-    return tuples_2(list_join([s, s],','));
+    return tuples_2(list_join_comma([s, s]));
 }
