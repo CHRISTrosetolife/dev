6ffb24b08e18c22fe184fde1f297e4ed1dec5b13
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 17:17:06 -0400

    2024-04-19T21:17:05.647Z  noop

--- public/src/js_return_variablize.mjs
+++ public/src/js_return_variablize.mjs
@@ -10,18 +10,21 @@ import {list_adder} from "./list_adder.mjs";
 import { list_index } from "./list_index.mjs";
 import { js_code_statement } from "./js_code_statement.mjs";
 import { js_parse } from "./js_parse.mjs";
+import { js_identifiers } from "./js_identifiers.mjs";
 export function js_return_variablize(ast) {
     let return_statements = js_node_type_visitor(ast, 'ReturnStatement');
     for (let r of return_statements) {
-        let {parent} = r;
+        let {parent,node} = r;
         assert(list_is, [parent]);
-        let index = list_index(parent, r);
+        let index = list_index(parent, node);
+        let identifiers = js_identifiers(ast);
         let code = js_code_statement(`let ${variable_name} = 0;`)
         let parsed = js_parse(code);
         if (0)
         parent.splice(index, 0, s);
         console.log({
-            r
+            r,
+            parsed
         });
     }
 }
