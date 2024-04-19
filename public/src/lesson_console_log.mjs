516eb16932aa9d535a867ff2c36b0a82104e6935
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-19 08:14:37 -0400

    2024-04-19T12:14:36.856Z  noop

--- public/src/lesson_console_log.mjs
+++ public/src/lesson_console_log.mjs
@@ -7,10 +7,18 @@ export function lesson_console_log() {
             let text = 'a computer program has three parts :';
             html_span_text(parent, text);
             let ol = html_element(parent, 'ol');
-            let li = html_element(ol, 'li');
-            html_inner_set(li, '(1) an input ,');
+            html_li_text(ol, '(1) an input ,');
             '(2) a process and';
             '(3) an output';
         }]
     };
 }
+function html_li_text(ol, text) {
+    let li = html_li(ol);
+    html_inner_set(li, text);
+}
+
+function html_li(ol) {
+    return html_element(ol, 'li');
+}
+
