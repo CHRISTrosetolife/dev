e3081ede6fc097615e630fcb78553c394de400f5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 17:17:09 -0400

    2024-04-20T21:17:09.715Z  noop

--- public/src/js_object_init_functionize.mjs
+++ public/src/js_object_init_functionize.mjs
@@ -5,6 +5,7 @@ import {js_code_call} from "./js_code_call.mjs";
 import {js_code_statement} from "./js_code_statement.mjs";
 import {list_adder_unique} from "./list_adder_unique.mjs";
 import {js_visit_node} from "./js_visit_node.mjs";
+import { list_add } from "./list_add.mjs";
 export function js_object_init_functionize(ast) {
     let vs = list_adder_unique(la => {
         js_visit_node(ast, 'VariableDeclarator', v => {
@@ -25,6 +26,8 @@ export function js_object_init_functionize(ast) {
             let call = js_parse_first(code);
             let {expression} = call;
             let {arguments:args}= expression
+            list_add(args, key)
+            list_add(args, value)
             console.log({
                 call
             });
