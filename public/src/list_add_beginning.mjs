63746abb7df39ee3c0cb8af587c142351161ec7c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 18:15:27 -0400

    2024-04-20T22:15:27.610Z  noop

--- public/src/list_add_beginning.mjs
+++ public/src/list_add_beginning.mjs
@@ -1,6 +1,4 @@
 import {list_insert} from "./list_insert.mjs";
 export function list_add_beginning(list, element) {
-    list.splice(0, 0, element);
-    return;
     list_insert(list, 0, element);
 }
