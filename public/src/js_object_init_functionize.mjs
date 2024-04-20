356f79dfb01d30fcf9df0499491e8b398e3db972
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 18:06:59 -0400

    2024-04-20T22:06:59.483Z  function_transform js_object_init_functionize lesson_console_log a

--- public/src/js_object_init_functionize.mjs
+++ public/src/js_object_init_functionize.mjs
@@ -41,6 +41,8 @@ export function js_object_init_functionize(ast) {
             list_add(args, key_string);
             list_add(args, value);
         }
-        console.log(variable_declaration);
+        let init_new = js_parse_expression('{}');
+        object_property_set(node, 'init', init_new)
+        console.log(node);
     }
 }
