a43b9c4ddbd2949429867c80a40bfb824422150e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 18:15:22 -0400

    2024-04-20T22:15:21.719Z  function_transform js_imports_add list_add_beginning a

--- public/src/list_add_beginning.mjs
+++ public/src/list_add_beginning.mjs
@@ -1,3 +1,6 @@
+import {list_insert} from "./list_insert.mjs";
 export function list_add_beginning(list, element) {
     list.splice(0, 0, element);
+    return;
+    list_insert(list, 0, element);
 }
