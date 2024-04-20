46a00aefd9ab9a3e0ee03d35167c608d9f4898af
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 17:37:25 -0400

    2024-04-20T21:37:25.670Z  function_transform js_imports_add list_get_end a

--- public/src/list_get_end.mjs
+++ public/src/list_get_end.mjs
@@ -1,1 +1,4 @@
-export function list_get_end(list, index) {}
+import {list_index_last} from "./list_index_last.mjs";
+export function list_get_end(list, index) {
+    let index_end = list_index_last(list);
+}
