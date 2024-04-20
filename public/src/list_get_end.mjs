daed67a67cdf360bfcd7df25b0761faa0ad4b9d5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 17:38:01 -0400

    2024-04-20T21:38:01.609Z  noop

--- public/src/list_get_end.mjs
+++ public/src/list_get_end.mjs
@@ -1,4 +1,7 @@
+import { list_get } from "./list_get.mjs";
 import {list_index_last} from "./list_index_last.mjs";
 export function list_get_end(list, index) {
     let index_end = list_index_last(list);
+    let index_actual = index_end - index;
+    return list_get(list, index_actual)
 }
