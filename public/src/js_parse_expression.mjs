6fb0695a1f960e8f57d3054368c1b10f9872ad0c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 18:09:22 -0400

    2024-04-20T22:09:22.705Z  noop

--- public/src/js_parse_expression.mjs
+++ public/src/js_parse_expression.mjs
@@ -4,5 +4,5 @@ export function js_parse_expression(variable_name) {
     const identifier_code = js_code_statement(variable_name);
     let parsed2 = js_parse_first(identifier_code);
     let {expression} = parsed2;
-    return parsed2;
+    return expression;
 }
