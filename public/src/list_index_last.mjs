2e607995ec73e59a76c41eba7d407736582b7f58
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 17:36:47 -0400

    2024-04-20T21:36:47.237Z  function_transform js_imports_add list_index_last a

--- public/src/list_index_last.mjs
+++ public/src/list_index_last.mjs
@@ -1,5 +1,5 @@
 import {string_length} from "./string_length.mjs";
-export function string_index_last(input) {
+export function list_index_last(input) {
     let length = string_length(input);
     let index_last = length - 1;
     return index_last;
