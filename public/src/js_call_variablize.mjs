bb9ff2e6aceb3eea8dc690592e86956cf575fe02
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-21 10:14:17 -0400

    2024-04-21T14:14:17.318Z  function_transform js_outside_move js_call_variablize a

--- public/src/js_call_variablize.mjs
+++ public/src/js_call_variablize.mjs
@@ -1,9 +1,16 @@
+import {js_variablize} from "./js_variablize.mjs";
+import {js_name_unique_v} from "./js_name_unique_v.mjs";
+import {list_index} from "./list_index.mjs";
+import {list_is} from "./list_is.mjs";
+import {assert} from "./assert.mjs";
+import {equal} from "./equal.mjs";
+import {list_get_end} from "./list_get_end.mjs";
 import {js_node_type_visitor} from "./js_node_type_visitor.mjs";
 import {js_node_type} from "./js_node_type.mjs";
 import {js_node_types} from "./js_node_types.mjs";
-import { list_includes } from "./list_includes.mjs";
+import {list_includes} from "./list_includes.mjs";
 export function js_call_variablize(ast) {
-    let move_types = ['ArrayExpression']
+    let move_types = ['ArrayExpression'];
     let vs = js_node_type_visitor(ast, 'CallExpression');
     for (let v of vs) {
         let {node} = v;
@@ -13,18 +20,20 @@ export function js_call_variablize(ast) {
             let {type} = arg;
             if (list_includes(move_types, type)) {
                 let ancestor = list_get_end(stack, 2);
-                let {type:ancestor_type} =ancestor;
+                let {type: ancestor_type} = ancestor;
                 if (equal(ancestor_type, 'BlockStatement')) {
-                    let body = list_get_end(stack, 1)
-                    assert(list_is, [body])
-                    let es = list_get_end(stack, 0)
-                    let {type:es_type} = es;
-                    assert(equal, [es_type, 'ExpressionStatement'])
-                    let index = list_index(body, es)
+                    let body = list_get_end(stack, 1);
+                    assert(list_is, [body]);
+                    let es = list_get_end(stack, 0);
+                    let {type: es_type} = es;
+                    assert(equal, [es_type, 'ExpressionStatement']);
+                    let index = list_index(body, es);
                     let variable_name = js_name_unique_v(ast);
-                    let parsed2 = js_variablize(ast, body, es, init);
+                    let parsed2 = js_variablize(ast, body, es, arg);
                 }
-                console.log({stack})
+                console.log({
+                    stack
+                });
             }
         }
     }
