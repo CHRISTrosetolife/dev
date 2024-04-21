cd7bc00c32be7e1f58641d9acb85936aec5f48fc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 21:32:27 -0400

    2024-04-21T01:32:27.371Z  function_transform js_outside_move object_replace a

--- public/src/object_replace.mjs
+++ public/src/object_replace.mjs
@@ -1,3 +1,4 @@
+import {object_properties_delete} from "./object_properties_delete.mjs";
 import {object_merge} from "./object_merge.mjs";
 export function object_replace(original, replacement) {
     object_properties_delete(original);
