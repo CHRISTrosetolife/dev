4a78d3fe38395588df2f1e7015495ac63f2640ea
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 20:48:04 -0400

    2024-04-21T00:48:04.635Z  noop

--- public/src/js_function_name_add.mjs
+++ public/src/js_function_name_add.mjs
@@ -5,8 +5,9 @@ import {js_visit_node} from "./js_visit_node.mjs";
 export function js_function_name_add(ast) {
     let name = js_declaration_single_name(ast);
     let prefix = string_combine(name, '_');
-    js_name_unique(ast, prefix);
     js_visit_node(ast, 'FunctionExpression', v => {
         let {node} = v;
+        let u = js_name_unique(ast, prefix);
+        console.log({node,u})
     });
 }
