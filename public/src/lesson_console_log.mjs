6743ec6b2ca7feab777e071c30ac20884de4287f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-21 12:37:02 -0400

    2024-04-21T16:37:02.564Z  noop

--- public/src/lesson_console_log.mjs
+++ public/src/lesson_console_log.mjs
@@ -9,8 +9,7 @@ import {html_span_text} from "./html_span_text.mjs";
 export function lesson_console_log() {
     let v1 = object_new();
     object_property_set(v1, 'description', 'outputting a string to the console');
-    let item = lesson_console_log_intro;
-    let v2 = list_1(item);
+    let v2 = list_1(lesson_console_log_intro);
     object_property_set(v1, 'screens', v2);
     return v1;
     ;
