a018a25c43ffbf708b66c4821f80816784f3ce90
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 20:57:57 -0400

    2024-04-21T00:57:57.042Z  noop

--- public/src/js_function_move_outside.mjs
+++ public/src/js_function_move_outside.mjs
@@ -1,6 +1,16 @@
+import { equal } from "./equal.mjs";
 import {js_visit_node} from "./js_visit_node.mjs";
 export function js_function_move_outside(ast, function_name) {
     js_visit_node(ast, 'FunctionExpression', v => {
+        let {node} = v;
+        let {id} = node;
+        if (equal(id, null)) {
+            return;
+        }
+        let {name} = id;
+        if (!equal(name, function_name)) {
+            return;
+        }
         let {stack} = v;
         console.log({
             stack
