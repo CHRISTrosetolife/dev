cd575f43d7352e9ddd25861a792dbc68e8bb0a0c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 21:19:22 -0400

    2024-04-21T01:19:21.806Z  function_transform js_imports_add js_function_move_outside a

--- public/src/js_function_move_outside.mjs
+++ public/src/js_function_move_outside.mjs
@@ -1,4 +1,5 @@
-import { equal } from "./equal.mjs";
+import {object_merge} from "./object_merge.mjs";
+import {equal} from "./equal.mjs";
 import {js_visit_node} from "./js_visit_node.mjs";
 export function js_function_move_outside(ast, function_name) {
     js_visit_node(ast, 'FunctionExpression', v => {
@@ -11,9 +12,6 @@ export function js_function_move_outside(ast, function_name) {
         if (!equal(name, function_name)) {
             return;
         }
-        let {parent} = v;
-        console.log({
-            parent
-        });
+        let outside = object_merge();
     });
 }
