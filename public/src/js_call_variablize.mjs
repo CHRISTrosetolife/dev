479f4f19e53ff39196223778bf067f4e10b575c0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-21 08:24:05 -0400

    2024-04-21T12:24:05.384Z  function_transform js_call_variablize lesson_console_log a

--- public/src/js_call_variablize.mjs
+++ public/src/js_call_variablize.mjs
@@ -2,7 +2,7 @@ import {js_node_type_visitor} from "./js_node_type_visitor.mjs";
 import {js_node_type} from "./js_node_type.mjs";
 import {js_node_types} from "./js_node_types.mjs";
 export function js_call_variablize(ast) {
-    return js_node_type_visitor(ast, 'CallExpression', v => {
+    js_node_type_visitor(ast, 'CallExpression', v => {
         let {node} = v;
         let {arguments: args} = node;
         console.log({
