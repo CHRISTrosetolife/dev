8a500deecf38c24f03e4ac8dfd32bd620a9b0ff2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 20:49:31 -0400

    2024-04-21T00:49:31.173Z  function_transform js_imports_add js_function_name_add a

--- public/src/js_function_name_add.mjs
+++ public/src/js_function_name_add.mjs
@@ -1,15 +1,16 @@
+import {object_property_set} from "./object_property_set.mjs";
 import {js_name_unique} from "./js_name_unique.mjs";
 import {string_combine} from "./string_combine.mjs";
 import {js_declaration_single_name} from "./js_declaration_single_name.mjs";
 import {js_visit_node} from "./js_visit_node.mjs";
-import { js_parse_expression } from "./js_parse_expression.mjs";
+import {js_parse_expression} from "./js_parse_expression.mjs";
 export function js_function_name_add(ast) {
     let name = js_declaration_single_name(ast);
     let prefix = string_combine(name, '_');
     js_visit_node(ast, 'FunctionExpression', v => {
         let {node} = v;
         let u = js_name_unique(ast, prefix);
-        let id = js_parse_expression(u)
-        object_property_set(node, 'id', id)
+        let id = js_parse_expression(u);
+        object_property_set(node, 'id', id);
     });
 }
