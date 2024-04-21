61a759703e11faab6bf0da2048645aee2e5a6a33
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 22:19:50 -0400

    2024-04-21T02:19:49.913Z  function_transform js_function_move_outside lesson_console_log lesson_console_log_intro

--- public/src/lesson_console_log.mjs
+++ public/src/lesson_console_log.mjs
@@ -1,3 +1,4 @@
+import {lesson_console_log_intro} from "./lesson_console_log_intro.mjs";
 import {object_property_set} from "./object_property_set.mjs";
 import {html_li_text} from "./html_li_text.mjs";
 import {html_element} from "./html_element.mjs";
@@ -6,14 +7,7 @@ import {html_span_text} from "./html_span_text.mjs";
 export function lesson_console_log() {
     let v1 = {};
     object_property_set(v1, 'description', 'outputting a string to the console');
-    object_property_set(v1, 'screens', [function lesson_console_log_intro(parent) {
-        let text = 'a computer program has three parts :';
-        html_span_text(parent, text);
-        let ol = html_element(parent, 'ol');
-        html_li_text(ol, 'an input ,');
-        html_li_text(ol, 'a process and');
-        html_li_text(ol, 'an output');
-    }]);
+    object_property_set(v1, 'screens', [lesson_console_log_intro]);
     return v1;
     ;
 }
