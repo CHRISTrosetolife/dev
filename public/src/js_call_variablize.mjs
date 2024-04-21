e8c06b9964039cffc9e803334318473acaafcab7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-21 08:21:50 -0400

    2024-04-21T12:21:50.624Z  function_transform js_call_variablize lesson_console_log a

--- public/src/js_call_variablize.mjs
+++ public/src/js_call_variablize.mjs
@@ -3,8 +3,9 @@ import {js_node_types} from "./js_node_types.mjs";
 export function js_call_variablize(ast) {
     return js_node_type(ast, 'CallExpression', v => {
         let {node} = v;
+        let {arguments:args} = node;
         console.log({
-            node
+            args
         });
     });
 }
