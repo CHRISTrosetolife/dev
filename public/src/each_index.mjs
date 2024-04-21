fbbc7e9bf18b3feabc95db0b5248065d65a6916f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-21 10:18:10 -0400

    2024-04-21T14:18:10.361Z  noop

--- public/src/each_index.mjs
+++ public/src/each_index.mjs
@@ -1,1 +1,7 @@
-export function each_index(list, lambda) {}
+export function each_index(list, lambda) {
+    let index = 0;
+    for (let element of list) {
+        lambda(element, index);
+        index++;
+    }
+}
