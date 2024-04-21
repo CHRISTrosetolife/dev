f1ee0270ae41dda1ccc806f3a42d1ed59b79af1f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-21 08:10:04 -0400

    2024-04-21T12:10:02.450Z  function_transform js_outside_move lesson_console_log a

--- public/src/lesson_console_log.mjs
+++ public/src/lesson_console_log.mjs
@@ -1,3 +1,4 @@
+import {object_new} from "./object_new.mjs";
 import {lesson_console_log_intro} from "./lesson_console_log_intro.mjs";
 import {object_property_set} from "./object_property_set.mjs";
 import {html_li_text} from "./html_li_text.mjs";
@@ -5,7 +6,7 @@ import {html_element} from "./html_element.mjs";
 import {html_inner_set} from "./html_inner_set.mjs";
 import {html_span_text} from "./html_span_text.mjs";
 export function lesson_console_log() {
-    let v1 = {};
+    let v1 = object_new();
     object_property_set(v1, 'description', 'outputting a string to the console');
     object_property_set(v1, 'screens', [lesson_console_log_intro]);
     return v1;
