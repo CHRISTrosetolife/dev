a80dcbd4278d5eb87ceb19e22fe97caae414fcfe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-21 08:25:41 -0400

    2024-04-21T12:25:41.083Z  noop

--- public/src/js_call_variablize.mjs
+++ public/src/js_call_variablize.mjs
@@ -2,6 +2,7 @@ import {js_node_type_visitor} from "./js_node_type_visitor.mjs";
 import {js_node_type} from "./js_node_type.mjs";
 import {js_node_types} from "./js_node_types.mjs";
 export function js_call_variablize(ast) {
+    let move_types = ['ArrayExpression']
     let vs = js_node_type_visitor(ast, 'CallExpression');
     for (let v of vs) {
         let {node} = v;
