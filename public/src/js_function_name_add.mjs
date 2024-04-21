cc37b0f7faed2852df601878b3e92078c13d9a0f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 20:41:43 -0400

    2024-04-21T00:41:42.825Z  function_transform js_imports_add js_function_name_add a

--- public/src/js_function_name_add.mjs
+++ public/src/js_function_name_add.mjs
@@ -1,1 +1,6 @@
-export function js_function_name_add(ast) {}
+import {js_visit_node} from "./js_visit_node.mjs";
+export function js_function_name_add(ast) {
+    js_visit_node(ast, 'FunctionExpression', v => {
+        let {node} = v;
+    });
+}
