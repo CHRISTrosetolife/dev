0c6fee544169b863e25be09cfa27fd8a60494337
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 20:35:26 -0400

    2024-04-21T00:35:25.805Z  function_transform js_imports_add js_arrow_to_function a

--- public/src/js_arrow_to_function.mjs
+++ public/src/js_arrow_to_function.mjs
@@ -1,1 +1,4 @@
-export function js_arrow_to_function(ast) {}
+import {js_node_types} from "./js_node_types.mjs";
+export function js_arrow_to_function(ast) {
+    console.log(js_node_types(ast));
+}
