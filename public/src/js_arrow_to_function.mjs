f4e4e245490e481f735ce4d44a03da30c6f6ebd7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 20:45:16 -0400

    2024-04-21T00:45:16.352Z  function_transform js_imports_add js_arrow_to_function a

--- public/src/js_arrow_to_function.mjs
+++ public/src/js_arrow_to_function.mjs
@@ -1,9 +1,11 @@
+import {js_name_unique} from "./js_name_unique.mjs";
 import {object_property_set} from "./object_property_set.mjs";
 import {js_visit_node} from "./js_visit_node.mjs";
 import {js_node_types} from "./js_node_types.mjs";
 export function js_arrow_to_function(ast) {
     js_visit_node(ast, 'ArrowFunctionExpression', v => {
         let {node} = v;
+        js_name_unique(ast, prefix);
         object_property_set(node, 'type', 'FunctionExpression');
     });
 }
