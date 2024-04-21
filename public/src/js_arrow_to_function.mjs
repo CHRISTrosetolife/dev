89794e5b8c02fe5770961c276f42a47087f7fcde
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 20:46:38 -0400

    2024-04-21T00:46:38.358Z  noop

--- public/src/js_arrow_to_function.mjs
+++ public/src/js_arrow_to_function.mjs
@@ -3,8 +3,10 @@ import {js_name_unique} from "./js_name_unique.mjs";
 import {object_property_set} from "./object_property_set.mjs";
 import {js_visit_node} from "./js_visit_node.mjs";
 import {js_node_types} from "./js_node_types.mjs";
+import { string_combine } from "./string_combine.mjs";
 export function js_arrow_to_function(ast) {
     let name = js_declaration_single_name(ast);
+    let prefix = string_combine(name, '_')
     js_visit_node(ast, 'ArrowFunctionExpression', v => {
         let {node} = v;
         js_name_unique(ast, prefix);
