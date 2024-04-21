d701f25bb635c7773f1eda21d378ffd3404e43ba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 20:56:20 -0400

    2024-04-21T00:56:20.366Z  function_transform js_imports_add js_function_move_outside a

--- public/src/js_function_move_outside.mjs
+++ public/src/js_function_move_outside.mjs
@@ -1,1 +1,9 @@
-export function js_function_move_outside(ast, function_name) {}
+import {js_visit_node} from "./js_visit_node.mjs";
+export function js_function_move_outside(ast, function_name) {
+    js_visit_node(ast, 'FunctionExpression', v => {
+        let {stack} = v;
+        console.log({
+            stack
+        });
+    });
+}
