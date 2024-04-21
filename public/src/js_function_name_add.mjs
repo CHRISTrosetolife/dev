4adfb2bf4b52a3e5850e84b0cf462c4996c9901c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 20:47:41 -0400

    2024-04-21T00:47:40.458Z  function_transform js_imports_add js_function_name_add a

--- public/src/js_function_name_add.mjs
+++ public/src/js_function_name_add.mjs
@@ -1,5 +1,11 @@
+import {js_name_unique} from "./js_name_unique.mjs";
+import {string_combine} from "./string_combine.mjs";
+import {js_declaration_single_name} from "./js_declaration_single_name.mjs";
 import {js_visit_node} from "./js_visit_node.mjs";
 export function js_function_name_add(ast) {
+    let name = js_declaration_single_name(ast);
+    let prefix = string_combine(name, '_');
+    js_name_unique(ast, prefix);
     js_visit_node(ast, 'FunctionExpression', v => {
         let {node} = v;
     });
