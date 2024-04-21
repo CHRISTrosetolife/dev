fff1784279c5440cc53853c98bba7e31190fad21
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-20 21:32:42 -0400

    2024-04-21T01:32:42.367Z  function_transform js_outside_move object_properties_delete a

--- public/src/object_properties_delete.mjs
+++ public/src/object_properties_delete.mjs
@@ -1,3 +1,4 @@
+import {object_property_delete} from "./object_property_delete.mjs";
 import {object_properties} from "./object_properties.mjs";
 export function object_properties_delete(object) {
     for (let property of object_properties(object)) {
