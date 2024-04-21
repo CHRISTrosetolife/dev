a82213a43945f48fed34162c1deda49bffdd04e7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-21 14:21:21 -0400

    2024-04-21T18:21:21.376Z  noop

--- public/src/each_reverse.mjs
+++ public/src/each_reverse.mjs
@@ -2,7 +2,11 @@ import { list_index_last } from "./list_index_last.mjs";
 
 export function each_reverse(list) {
     let index_last = list_index_last(list);
-    for (let i = index_last ; i >= 0 ; i--) {
+    for (let i = index_last ; i >= list_index_first() ; i--) {
 
     }
 }
+function list_index_first() {
+    return 0;
+}
+
