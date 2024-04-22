2a39da7deea1de074b8bee23eb9d6ae655a922c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-22 15:35:35 -0400

    2024-04-22T19:35:34.856Z  function_transform js_empty_statements_remove sandbox a

--- public/src/js_empty_statements_remove.mjs
+++ public/src/js_empty_statements_remove.mjs
@@ -1,4 +1,8 @@
 import {js_node_type_visitor} from "./js_node_type_visitor.mjs";
 export function js_empty_statements_remove(ast) {
     let vs = js_node_type_visitor(ast, 'EmptyStatement');
+    for (let v of vs) {
+        let {parent} = v;
+        console.log({parent})
+    }
 }
