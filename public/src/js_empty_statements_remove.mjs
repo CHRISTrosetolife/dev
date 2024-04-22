ef08512af07f008d0655a820d4c9525ffd227d1a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-22 15:35:55 -0400

    2024-04-22T19:35:54.725Z  function_transform js_outside_move js_empty_statements_remove a

--- public/src/js_empty_statements_remove.mjs
+++ public/src/js_empty_statements_remove.mjs
@@ -1,8 +1,13 @@
+import {list_is} from "./list_is.mjs";
+import {assert} from "./assert.mjs";
 import {js_node_type_visitor} from "./js_node_type_visitor.mjs";
 export function js_empty_statements_remove(ast) {
     let vs = js_node_type_visitor(ast, 'EmptyStatement');
     for (let v of vs) {
         let {parent} = v;
-        console.log({parent})
+        assert(list_is, [parent]);
+        console.log({
+            parent
+        });
     }
 }
