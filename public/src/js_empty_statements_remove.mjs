bdd890cdba92bb180dc04e19d90285d94ef69672
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-22 15:34:42 -0400

    2024-04-22T19:34:42.264Z  function_transform js_outside_move js_empty_statements_remove a

--- public/src/js_empty_statements_remove.mjs
+++ public/src/js_empty_statements_remove.mjs
@@ -1,1 +1,4 @@
-export function js_empty_statements_remove(ast) {}
+import {js_node_type_visitor} from "./js_node_type_visitor.mjs";
+export function js_empty_statements_remove(ast) {
+    let vs = js_node_type_visitor(ast, 'EmptyStatement');
+}
