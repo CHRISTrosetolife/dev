497b7f07fbb1b9d1af856ef827ad389b57e3c170
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 15:53:45 -0400

    2024-04-24T19:53:45.635Z  function_transform js_imports_add app_learn_code

--- public/src/app_learn_code.mjs
+++ public/src/app_learn_code.mjs
@@ -1,3 +1,4 @@
+import {lesson_console_log} from "./lesson_console_log.mjs";
 import {list_first} from "./list_first.mjs";
 import {object_property_get} from "./object_property_get.mjs";
 import {html_document_body} from "./html_document_body.mjs";
@@ -5,5 +6,6 @@ export function app_learn_code() {
     let root = html_document_body();
     let lesson = lesson_console_log();
     let screens = object_property_get(lesson, 'screens');
-    let screen_first = list_first(screens);;
+    let screen_first = list_first(screens);
+    ;
 }
