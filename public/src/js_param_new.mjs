b3f2afa320786d22ffd9aef7e5a644e707d46ce3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 16:46:13 -0400

    2024-04-24T20:46:13.092Z  noop

--- public/src/js_param_new.mjs
+++ public/src/js_param_new.mjs
@@ -1,1 +1,7 @@
-export function js_param_new(ast, param_name, default_value) {}
+import { js_node_type } from "./js_node_type.mjs";
+import { list_filter } from "./list_filter.mjs";
+
+export function js_param_new(ast, param_name, default_value) {
+    let nodes = js_node_type(ast, 'CallExpression');
+    let filtered = list_filter(nodes, )
+}
