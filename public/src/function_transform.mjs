becc37a2e7d519e4f2116854e407506d3556bbea
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 18:05:40 -0400

    2024-04-24T22:05:40.444Z  noop

--- public/src/function_transform.mjs
+++ public/src/function_transform.mjs
@@ -10,6 +10,6 @@ import { equal } from "./equal.mjs";
 export async function function_transform(lambda_name, function_name) {
     let args = arguments;
     let length = list_length(args);
-    assert(equal(length, 2))
+    assert(equal,[length, 2])
     return function_transform_args(lambda_name, function_name, 'a');
 }
