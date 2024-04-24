c1612f693bbaaa918499a95fc94197ba488b07a7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 17:39:26 -0400

    2024-04-24T21:39:25.777Z  noop

--- public/src/function_new_generic.mjs
+++ public/src/function_new_generic.mjs
@@ -9,7 +9,8 @@ import {newline} from './newline.mjs';
 import {js_parse} from './js_parse.mjs';
 import {js_unparse} from './js_unparse.mjs';
 export async function function_new_generic(function_name, args_string, body_string, open, imports, async_is) {
-    let contents_function = `export function ${function_name}(${args_string}) {
+    let space = async_is ? ` async ` : ' ';
+    let contents_function = `export${space}function ${function_name}(${args_string}) {
 ${body_string}
 }`;
     let mapped = list_map(imports, js_code_import);
