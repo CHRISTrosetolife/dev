7d17801b00147ca82d69a8258a64231a560bf090
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 16:52:16 -0400

    2024-04-24T20:52:16.386Z  noop

--- public/src/js_param_new.mjs
+++ public/src/js_param_new.mjs
@@ -1,3 +1,4 @@
+import { equal } from "./equal.mjs";
 import { js_node_type } from "./js_node_type.mjs";
 import { list_filter } from "./list_filter.mjs";
 
@@ -5,6 +6,10 @@ export function js_param_new(ast, function_name, param_name, default_value) {
     let nodes = js_node_type(ast, 'CallExpression');
     for (let node of nodes) {
         let{callee} = node;
+        let {type} = callee;
+        if (!equal(type, 'Identifier')) {
+            continue;
+        }
         console.log({callee})
     }
     let filtered = list_filter(nodes, )
