4847484d9b65608de211040e75552e1380705dfb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 15:38:48 -0400

    2024-04-24T19:38:48.113Z  function_transform js_imports_add js_call_append

--- public/src/js_call_append.mjs
+++ public/src/js_call_append.mjs
@@ -1,3 +1,5 @@
+import {file_exists} from "./file_exists.mjs";
+import {function_name_to_path} from "./function_name_to_path.mjs";
 import {js_code_declare_assign} from "./js_code_declare_assign.mjs";
 import {string_split_comma} from "./string_split_comma.mjs";
 import {js_call} from "./js_call.mjs";
@@ -5,7 +7,8 @@ import {js_parse_expression} from "./js_parse_expression.mjs";
 import {list_add} from "./list_add.mjs";
 import {list_map} from "./list_map.mjs";
 import {object_property_set} from "./object_property_set.mjs";
-export function js_call_append(ast, fn_name, args, result_name) {
+import {js_imports_add_specified} from "./js_imports_add_specified.mjs";
+export async function js_call_append(ast, fn_name, args, result_name) {
     let {body} = ast;
     let args_list = string_split_comma(args);
     let mapped = list_map(args_list, js_parse_expression);
@@ -13,4 +16,8 @@ export function js_call_append(ast, fn_name, args, result_name) {
     let parsed = js_code_declare_assign(result_name);
     object_property_set(parsed, 'init', call);
     list_add(body, parsed);
+    let path = function_name_to_path(fn_name);
+    if (await file_exists(path)) {
+        js_imports_add_specified(ast, [fn_name]);
+    }
 }
