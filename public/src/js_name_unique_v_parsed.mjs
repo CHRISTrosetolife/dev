4452efba4c5ca452ce03fd62e82b20ab016d3c6f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-23 20:52:18 -0400

    2024-04-24T00:52:17.757Z  function_transform js_outside_move js_name_unique_v_parsed

--- public/src/js_name_unique_v_parsed.mjs
+++ public/src/js_name_unique_v_parsed.mjs
@@ -1,10 +1,10 @@
+import {js_code_declare_assign} from "./js_code_declare_assign.mjs";
 import {js_parse_first} from "./js_parse_first.mjs";
 import {js_code_statement} from "./js_code_statement.mjs";
 import {js_name_unique_v} from "./js_name_unique_v.mjs";
 export function js_name_unique_v_parsed(ast) {
     let variable_name = js_name_unique_v(ast);
-    const code = js_code_statement(`let ${variable_name} = 0`);
-    let parsed = js_parse_first(code);
+    let parsed = js_code_declare_assign(variable_name);
     return {
         parsed,
         variable_name
