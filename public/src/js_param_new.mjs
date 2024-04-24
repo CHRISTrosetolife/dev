640e9c2540732569d538f5dc96cc6506b7447835
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 17:30:41 -0400

    2024-04-24T21:30:41.271Z  function_transform js_imports_add js_param_new

--- public/src/js_param_new.mjs
+++ public/src/js_param_new.mjs
@@ -1,14 +1,14 @@
-import { equal } from "./equal.mjs";
-import { js_export_single } from "./js_export_single.mjs";
-import { js_node_type } from "./js_node_type.mjs";
-import { js_parse_expression } from "./js_parse_expression.mjs";
-import { list_add } from "./list_add.mjs";
-import { list_filter } from "./list_filter.mjs";
-
+import {js_declaration_single} from "./js_declaration_single.mjs";
+import {equal} from "./equal.mjs";
+import {js_export_single} from "./js_export_single.mjs";
+import {js_node_type} from "./js_node_type.mjs";
+import {js_parse_expression} from "./js_parse_expression.mjs";
+import {list_add} from "./list_add.mjs";
+import {list_filter} from "./list_filter.mjs";
 export function js_param_new(ast, function_name, param_name, default_value_string) {
     let nodes = js_node_type(ast, 'CallExpression');
     for (let node of nodes) {
-        let{callee} = node;
+        let {callee} = node;
         let {type} = callee;
         if (!equal(type, 'Identifier')) {
             continue;
@@ -17,9 +17,12 @@ export function js_param_new(ast, function_name, param_name, default_value_strin
         if (!equal(name, function_name)) {
             continue;
         }
-        let {arguments:args} = node;
+        let {arguments: args} = node;
         let default_value = js_parse_expression(default_value_string);
         list_add(args, default_value);
     }
-    let e = js_export_single(ast);
+    let declaration = js_declaration_single(ast);
+    console.log({
+        declaration
+    });
 }
