5dc27107d4e78ec82bdb8c0fc5c72dd723c43732
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 16:51:17 -0400

    2024-04-24T20:51:17.213Z  noop

--- public/src/js_param_new.mjs
+++ public/src/js_param_new.mjs
@@ -3,6 +3,9 @@ import { list_filter } from "./list_filter.mjs";
 
 export function js_param_new(ast, function_name, param_name, default_value) {
     let nodes = js_node_type(ast, 'CallExpression');
-    console.log({nodes})
+    for (let node of nodes) {
+        let{callee} = node;
+        console.log({callee})
+    }
     let filtered = list_filter(nodes, )
 }
