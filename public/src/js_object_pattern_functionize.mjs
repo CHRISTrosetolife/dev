f2bb3c6a84eefaee384849a6feff9642df4b285e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-23 21:02:49 -0400

    2024-04-24T01:02:48.794Z  noop

--- public/src/js_object_pattern_functionize.mjs
+++ public/src/js_object_pattern_functionize.mjs
@@ -39,6 +39,8 @@ export function js_object_pattern_functionize(ast) {
             let parsed = js_code_declare_assign(value);
             let key_string = js_identifier_to_expression(key);
             let call = js_call(object_property_get.name, [variable_name, key_string]);
+            
+            return;
             js_variable_declaration_init(parsed, call);
             console.log({
                 key,
