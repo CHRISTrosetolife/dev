8258678e9b3545edccd17ba40a3cf43915ba7875
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 15:34:22 -0400

    2024-04-24T19:34:21.830Z  noop

--- public/src/js_call_append.mjs
+++ public/src/js_call_append.mjs
@@ -1,3 +1,7 @@
+import { js_call } from "./js_call.mjs";
+
 export function js_call_append(ast, fn_name, args, result_name) {
     let {body} = ast;
+    let mapped 
+    let call = js_call(fn_name, mapped)
 }
