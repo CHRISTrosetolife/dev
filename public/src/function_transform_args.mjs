7abb86095f397a9347898e9c98251e38e4522a5f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 16:41:24 -0400

    2024-04-24T20:41:23.693Z  function_open js_outside_move

--- public/src/function_transform_args.mjs
+++ public/src/function_transform_args.mjs
@@ -5,7 +5,11 @@ import {string_split_comma} from "./string_split_comma.mjs";
 import {string_combine} from "./string_combine.mjs";
 export async function function_transform_args(lambda_name, function_name, args) {
     let args_split = string_split_comma(args);
+    return await function_transform_args_split(lambda_name, function_name, args_split);
+}
+async function function_transform_args_split(lambda_name, function_name, args_split) {
     let lambda = await function_import(lambda_name);
     let file_path = function_name_to_path(function_name);
     return await file_js_transform(lambda, file_path, args_split);
 }
+
