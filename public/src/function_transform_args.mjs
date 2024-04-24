db754098807a5d1d9b33e477cf534de81e419014
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 17:46:43 -0400

    2024-04-24T21:46:43.055Z  function_transform js_outside_move function_transform_args

--- public/src/function_transform_args.mjs
+++ public/src/function_transform_args.mjs
@@ -1,3 +1,4 @@
+import {function_transform_args_split} from "./function_transform_args_split.mjs";
 import {file_js_transform} from "./file_js_transform.mjs";
 import {function_name_to_path} from "./function_name_to_path.mjs";
 import {function_import} from "./function_import.mjs";
@@ -6,9 +7,4 @@ import {string_combine} from "./string_combine.mjs";
 export async function function_transform_args(lambda_name, function_name, args) {
     let args_split = string_split_comma(args);
     return await function_transform_args_split(lambda_name, function_name, args_split);
-}
-async function function_transform_args_split(lambda_name, function_name, args_split) {
-    let lambda = await function_import(lambda_name);
-    let file_path = function_name_to_path(function_name);
-    return await file_js_transform(lambda, file_path, args_split);
 }
