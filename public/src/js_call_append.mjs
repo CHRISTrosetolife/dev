26605187d6cf8b9f2c202c844a4422a429f984a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 15:37:17 -0400

    2024-04-24T19:37:17.443Z  function_transform js_imports_add js_call_append

--- public/src/js_call_append.mjs
+++ public/src/js_call_append.mjs
@@ -1,7 +1,16 @@
-import { js_call } from "./js_call.mjs";
-
+import {js_code_declare_assign} from "./js_code_declare_assign.mjs";
+import {string_split_comma} from "./string_split_comma.mjs";
+import {js_call} from "./js_call.mjs";
+import {js_parse_expression} from "./js_parse_expression.mjs";
+import {list_add} from "./list_add.mjs";
+import {list_map} from "./list_map.mjs";
+import {object_property_set} from "./object_property_set.mjs";
 export function js_call_append(ast, fn_name, args, result_name) {
     let {body} = ast;
-    let mapped 
-    let call = js_call(fn_name, mapped)
+    let args_list = string_split_comma(args);
+    let mapped = list_map(args_list, js_parse_expression);
+    let call = js_call(fn_name, mapped);
+    let parsed = js_code_declare_assign(result_name);
+    object_property_set(parsed, 'init', call);
+    list_add(body, parsed);
 }
