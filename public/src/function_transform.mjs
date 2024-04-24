8dde9d2a63b0f473ff7307f56601e2e47f3b9f07
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 18:04:28 -0400

    2024-04-24T22:04:28.742Z  noop

--- public/src/function_transform.mjs
+++ public/src/function_transform.mjs
@@ -4,6 +4,12 @@ import {function_name_to_path} from "./function_name_to_path.mjs";
 import {function_import} from "./function_import.mjs";
 import {string_split_comma} from "./string_split_comma.mjs";
 import {string_combine} from "./string_combine.mjs";
+import { list_length } from "./list_length.mjs";
+import { assert } from "./assert.mjs";
+import { equal } from "./equal.mjs";
 export async function function_transform(lambda_name, function_name) {
+    let args = arguments;
+    let length = list_length(args);
+    assert(equal(length, 2))
     return function_transform_args(lambda_name, function_name, 'a');
 }
