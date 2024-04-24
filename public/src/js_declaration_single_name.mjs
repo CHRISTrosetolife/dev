98bdd1110357c0a28bfb4aa74849aef479077d03
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 16:57:25 -0400

    2024-04-24T20:57:25.080Z  noop

--- public/src/js_declaration_single_name.mjs
+++ public/src/js_declaration_single_name.mjs
@@ -1,9 +1,14 @@
 import {js_export_single} from "./js_export_single.mjs";
 export function js_declaration_single_name(ast) {
-    let e = js_export_single(ast);
-    let {declaration:d} = e;
-    let declaration = d;
+    let declaration = js_declaration_single(ast);
     let {id} = declaration;
     let name = id.name;
     return name;
 }
+function js_declaration_single(ast) {
+    let e = js_export_single(ast);
+    let { declaration: d } = e;
+    let declaration = d;
+    return declaration;
+}
+
