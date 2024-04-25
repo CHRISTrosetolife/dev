67eaef7f9577f7896a9a685dbd732a89f9452b5f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-25 17:26:22 -0400

    2024-04-25T21:26:22.283Z  noop

--- public/src/lesson_console_log_process_computer_program.mjs
+++ public/src/lesson_console_log_process_computer_program.mjs
@@ -2,13 +2,19 @@ import {html_li_text} from "./html_li_text.mjs";
 import {html_ul} from "./html_ul.mjs";
 import {html_p_text} from "./html_p_text.mjs";
 import {html_hr} from "./html_hr.mjs";
+import { html_element } from "./html_element.mjs";
+import { html_inner_set } from "./html_inner_set.mjs";
 export function lesson_console_log_process_computer_program(parent) {
-    html_p_text(parent, 'computer programs may have inputs');
-    html_p_text(parent, 'computer programs may have outputs');
-    html_p_text(parent, 'we could say what the computer program does is a process');
+    html_p_text(parent, 'a computer runs a computer program');
+    html_p_text(parent, 'when a computer program is ran , it may have output');
+    html_p_text(parent, 'we could say :');
+    let ul1 = html_ul(parent)
+    html_li_text(ul1, 'when a computer runs a computer program , the running is a process');
+    html_li_text(ul1, 'the computer program is an input to the running process');
+    html_li_text(ul1, 'any output from the computer program is an output of the running process');
     html_hr(parent);
-    html_p_text(parent, 'our first computer program will :');
-    let ul = html_ul(parent)
-    html_li_text(ul, 'output a message');
-    html_li_text(ul, 'not have an input');
+    html_p_text(parent, 'our first computer program will output a message');
+    html_p_text(parent, 'and here is our first computer program :');
+    let code = html_element(parent, 'pre')
+    html_inner_set(code, `console.log('hello')`)
 }
