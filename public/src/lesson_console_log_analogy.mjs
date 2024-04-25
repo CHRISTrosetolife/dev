b6a516ea02afe129be37bd40bc635e8a4bca40a7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 21:31:00 -0400

    2024-04-25T01:31:00.522Z  noop

--- public/src/lesson_console_log_analogy.mjs
+++ public/src/lesson_console_log_analogy.mjs
@@ -1,6 +1,5 @@
 import {html_p_text} from "./html_p_text.mjs";
 import {html_element} from "./html_element.mjs";
 export function lesson_console_log_analogy(parent) {
-    let text = 'a computer program has three parts :';
-    html_p_text(parent, text);
+    html_p_text(parent, 'a computer program has three parts :');
 }
