f348aba9bf384dc77cb034af96ca23881779492a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 21:29:01 -0400

    2024-04-25T01:29:01.295Z  function_copy lesson_console_log_intro lesson_console_log_analogy

---
+++ public/src/lesson_console_log_analogy.mjs
@@ -0,0 +1,11 @@
+import {html_li_text} from "./html_li_text.mjs";
+import {html_element} from "./html_element.mjs";
+import {html_span_text} from "./html_span_text.mjs";
+export function lesson_console_log_intro(parent) {
+    let text = 'a computer program has three parts :';
+    html_span_text(parent, text);
+    let ol = html_element(parent, 'ol');
+    html_li_text(ol, 'an input ,');
+    html_li_text(ol, 'a process and');
+    html_li_text(ol, 'an output');
+}
