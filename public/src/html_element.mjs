34e5d3bf787c4c1259f8c8dacffb6e121f9ecced
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-25 18:12:34 -0400

    2024-04-25T22:12:34.206Z  function_transform js_outside_move html_element

--- public/src/html_element.mjs
+++ public/src/html_element.mjs
@@ -1,6 +1,9 @@
+import {html_style_default} from "./html_style_default.mjs";
+import {html_style} from "./html_style.mjs";
 export function html_element(parent, tag_name) {
     let {element} = parent;
     let result = document.createElement(tag_name);
+    html_style(result, html_style_default());
     element.appendChild(result);
     return {
         element: result
