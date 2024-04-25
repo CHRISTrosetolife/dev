055aa8c41ea9e9a48b056b468b29fd00c0e5d0e0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-25 08:13:30 -0400

    2024-04-25T12:13:30.069Z  function_transform js_outside_move lesson_console_log_analogy

---
+++ public/src/lesson_console_log_analogy_1.mjs
@@ -0,0 +1,10 @@
+import {html_img_width_full} from "./html_img_width_full.mjs";
+import {html_p_text} from "./html_p_text.mjs";
+export function lesson_console_log_analogy_1(parent) {
+    html_p_text(parent, 'suppose there is paper');
+    html_img_width_full(parent, 'paper.jpg');
+    html_p_text(parent, 'and suppose someone folds the paper into origami');
+    html_img_width_full(parent, 'paper_folding.jpg');
+    html_p_text(parent, 'then afterward there is folded paper origami');
+    html_img_width_full(parent, 'paper_folded.jpg');
+}
