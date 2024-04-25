e501e82efd9d804e3ef5a7ada246b007e3182be7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-25 17:29:11 -0400

    2024-04-25T21:29:11.459Z  noop

--- public/src/lesson_console_log_process_computer_program.mjs
+++ public/src/lesson_console_log_process_computer_program.mjs
@@ -6,7 +6,7 @@ import { html_element } from "./html_element.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
 export function lesson_console_log_process_computer_program(parent) {
     html_p_text(parent, 'a computer runs a computer program');
-    html_p_text(parent, 'when a computer program is ran , it may have output');
+    html_p_text(parent, 'when a computer program is ran , the computer program may have output');
     html_p_text(parent, 'we could say :');
     let ul1 = html_ul(parent)
     html_li_text(ul1, 'when a computer runs a computer program , the running is a process');
