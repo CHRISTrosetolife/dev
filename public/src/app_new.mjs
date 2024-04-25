c99073102348fc9bc33ef5f7e4fd4576646e9b54
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 20:50:54 -0400

    2024-04-25T00:50:53.889Z  function_transform js_outside_move app_new

--- public/src/app_new.mjs
+++ public/src/app_new.mjs
@@ -1,10 +1,18 @@
+import {js_code_import_path} from "./js_code_import_path.mjs";
+import {folder_current} from "./folder_current.mjs";
+import {app_prefix} from "./app_prefix.mjs";
 import {function_new} from "./function_new.mjs";
 import {file_write} from "./file_write.mjs";
 import {folder_name_public} from "./folder_name_public.mjs";
 import {path_join} from "./path_join.mjs";
 import {js_code_call} from "./js_code_call.mjs";
 import {js_code_import} from "./js_code_import.mjs";
+import {string_combine} from "./string_combine.mjs";
+import {file_overwrite} from "./file_overwrite.mjs";
 export async function app_new(name) {
+    let prefix = app_prefix();
+    let name_prefixed = string_combine(prefix, name);
+    let import_path = `${folder_current()}/${folder_src()}/`;
     let html = `
     <!DOCTYPE html>
     <html>
@@ -15,13 +23,13 @@ export async function app_new(name) {
       </head>
       <body>
         <script type="module">
-          ${js_code_import(name)}
-          ${js_code_call(name)}
+          ${js_code_import_path(import_path, import_name)}
+          ${js_code_call(name_prefixed)}
         </script>
       </body>
     </html>        
 `;
     let output_path = path_join([folder_name_public(), `${name}.html`]);
-    await file_write(output_path, html);
-    await function_new(name);
+    await file_overwrite(output_path, html);
+    await function_new(name_prefixed);
 }
---
+++ public/src/app_prefix.mjs
@@ -0,0 +1,3 @@
+export function app_prefix() {
+    return 'app_';
+}
