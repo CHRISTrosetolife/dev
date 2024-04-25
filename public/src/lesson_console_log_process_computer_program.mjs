77b7e5fd6b4a974393444d9c6180adfadf9f0725
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-25 17:17:00 -0400

    2024-04-25T21:16:59.794Z  noop

--- public/src/lesson_console_log_process_computer_program.mjs
+++ public/src/lesson_console_log_process_computer_program.mjs
@@ -7,6 +7,8 @@ export function lesson_console_log_process_computer_program(parent) {
     html_p_text(parent, 'computer programs may have outputs');
     html_p_text(parent, 'we could say what the computer program does is a process');
     html_hr(parent);
-    html_p_text(parent, 'the first computer programs we look at will output a message');
-    html_p_text(parent, 'the first computer programs we look at will not have an input');
+    html_p_text(parent, 'the first computer programs we look at will :');
+    let ul = html_ul(parent)
+    html_li_text(parent, 'output a message');
+    html_li_text(parent, 'not have an input');
 }
