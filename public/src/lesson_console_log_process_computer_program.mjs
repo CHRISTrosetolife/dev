03255ecb58c8e6dade351b2004cccc39614e6925
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-25 17:42:51 -0400

    2024-04-25T21:42:51.702Z  noop

--- public/src/lesson_console_log_process_computer_program.mjs
+++ public/src/lesson_console_log_process_computer_program.mjs
@@ -18,19 +18,16 @@ export function lesson_console_log_process_computer_program(parent) {
     html_p_text(parent, 'our first computer program will output a message :');
     let input_container = html_div(parent)
     html_p_text(input_container, 'input ( computer program ) :');
-    let code = html_element(input_container, 'div', `console.log('hello');`)
+    let code = html_p(input_container, `console.log('hello');`)
     html_style(code, {
         'color':'white',
         'background-color':'black',
-        'padding': '0.5rem',
-        'border-radius': '0.5rem',
     })
     html_style(code, {
         'padding': '0.5rem',
         'border-radius': '0.5rem',
         'font-family': 'monospace'
     })
-    html_inner_set(code, )
     html_p_text(parent, 'output ( from computer program ) :');
     let output = html_element(parent, 'pre')
     html_inner_set(output, `hello`)
