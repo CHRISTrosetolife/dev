bd69669a8b1e1947bdac8960e38d8943c30b418c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 20:45:58 -0400

    2024-04-25T00:45:57.754Z  noop

--- public/src/js_code_import.mjs
+++ public/src/js_code_import.mjs
@@ -1,6 +1,7 @@
 import {js_code_statement} from "./js_code_statement.mjs";
+import { string_combine } from "./string_combine.mjs";
 export function js_code_import(import_name) {
-    let import_path = './'
-    let import_code = js_code_statement(`import { ${import_name} } from "${import_path}${import_name}.mjs"`);
+    let import_path = string_combine('./', import_name)
+    let import_code = js_code_statement(`import { ${import_name} } from "${import_path}.mjs"`);
     return import_code;
 }
