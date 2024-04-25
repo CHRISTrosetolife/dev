29ef516ee8e57c7fc717b0dcaf8147e1cfe7b4d1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-25 16:17:56 -0400

    2024-04-25T20:17:56.049Z  noop

--- public/src/app_learn_code.mjs
+++ public/src/app_learn_code.mjs
@@ -12,6 +12,7 @@ export function app_learn_code() {
     let screens = object_property_get(lesson, 'screens');
     let index = 0;
     let screen = list_get(list, index);
+    html_inner_set(root, '');
     screen(root);
     let button_next = html_button_width_full_text(root, 'next');
     let button_previous = html_button_width_full_text(root, 'previous');
