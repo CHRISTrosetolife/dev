62bc7eff40987bccab2bcdd625dae236bc9c1768
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 21:33:48 -0400

    2024-04-25T01:33:48.113Z  noop

--- public/src/lesson_console_log_analogy.mjs
+++ public/src/lesson_console_log_analogy.mjs
@@ -1,6 +1,8 @@
 import {html_p_text} from "./html_p_text.mjs";
 import {html_element} from "./html_element.mjs";
+import { object_property_set } from "./object_property_set.mjs";
 export function lesson_console_log_analogy(parent) {
     html_p_text(parent, 'suppose there is a sheet of paper');
     let i1 = html_element(parent, 'img')
+    object_property_set(i1, 'src', img_path)
 }
