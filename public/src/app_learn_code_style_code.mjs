11d28d216a7d7e78320457d1f213de8984cd113c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-25 17:48:28 -0400

    2024-04-25T21:48:28.057Z  function_transform js_outside_move app_learn_code_style_code

--- public/src/app_learn_code_style_code.mjs
+++ public/src/app_learn_code_style_code.mjs
@@ -1,9 +1,7 @@
+import {app_learn_code_style_rounded_padded} from "./app_learn_code_style_rounded_padded.mjs";
 import {html_style} from "./html_style.mjs";
 export function app_learn_code_style_code(code) {
-    html_style(code, {
-        'padding': '0.5rem',
-        'border-radius': '0.5rem',
-    });
+    app_learn_code_style_rounded_padded(code);
     html_style(code, {
         'padding': '0.5rem',
         'border-radius': '0.5rem',
