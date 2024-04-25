71635ab75c58c424ba67212f70a7a73539bc044c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-25 18:14:04 -0400

    2024-04-25T22:14:04.670Z  noop

--- public/src/html_element.mjs
+++ public/src/html_element.mjs
@@ -2,10 +2,10 @@ import {html_style_default} from "./html_style_default.mjs";
 import {html_style} from "./html_style.mjs";
 export function html_element(parent, tag_name) {
     let {element} = parent;
-    let result = document.createElement(tag_name);
-    html_style(result, html_style_default());
-    element.appendChild(result);
+    let e = document.createElement(tag_name);
+    html_style(e, html_style_default());
+    element.appendChild(e);
     return {
-        element: result
+        element: e
     };
 }
