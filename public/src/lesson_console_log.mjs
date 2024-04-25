a9b6b55f04df40276f87e1aecabfcb84a1541362
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-25 17:15:37 -0400

    2024-04-25T21:15:37.166Z  function_transform js_outside_move lesson_console_log

--- public/src/lesson_console_log.mjs
+++ public/src/lesson_console_log.mjs
@@ -1,15 +1,17 @@
+import {lesson_console_log_process_computer_program} from "./lesson_console_log_process_computer_program.mjs";
 import {list_add} from "./list_add.mjs";
 import {array_new} from "./array_new.mjs";
 import {object_new} from "./object_new.mjs";
 import {object_property_set} from "./object_property_set.mjs";
-import { lesson_console_log_analogy_paper } from "./lesson_console_log_analogy_paper.mjs";
-import { lesson_console_log_analogy_addition } from "./lesson_console_log_analogy_addition.mjs";
+import {lesson_console_log_analogy_paper} from "./lesson_console_log_analogy_paper.mjs";
+import {lesson_console_log_analogy_addition} from "./lesson_console_log_analogy_addition.mjs";
 export function lesson_console_log() {
     let v1 = object_new();
     object_property_set(v1, 'description', 'outputting a string to the console');
     let v2 = array_new();
     list_add(v2, lesson_console_log_analogy_paper);
     list_add(v2, lesson_console_log_analogy_addition);
+    list_add(v2, lesson_console_log_process_computer_program);
     object_property_set(v1, 'screens', v2);
     return v1;
 }
