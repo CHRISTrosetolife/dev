7bd8ece9148c8dd11776e2852c38a7c8fdfd7681
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-25 16:15:33 -0400

    2024-04-25T20:15:32.741Z  noop

--- public/src/app_learn_code.mjs
+++ public/src/app_learn_code.mjs
@@ -3,11 +3,13 @@ import {lesson_console_log} from "./lesson_console_log.mjs";
 import {list_first} from "./list_first.mjs";
 import {object_property_get} from "./object_property_get.mjs";
 import {html_document_body} from "./html_document_body.mjs";
+import { html_inner_set } from "./html_inner_set.mjs";
 export function app_learn_code() {
     let root = html_document_body();
     let lesson = lesson_console_log();
     let screens = object_property_get(lesson, 'screens');
     let screen_first = list_first(screens);
     screen_first(root);
-    html_button_width_full(root);
+    let button_next = html_button_width_full_text(root, 'next');
+    let button_previous = html_button_width_full_text(root, 'previous');
 }
