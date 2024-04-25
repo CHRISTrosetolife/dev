b2f5b0404dff42099405ea64ba3a981ad0c029c4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 21:47:00 -0400

    2024-04-25T01:46:59.966Z  function_transform js_outside_move lesson_console_log_intro

---
+++ public/src/html_ol.mjs
@@ -0,0 +1,4 @@
+import {html_element} from "./html_element.mjs";
+export function html_ol(parent) {
+    return html_element(parent, 'ol');
+}
--- public/src/lesson_console_log_intro.mjs
+++ public/src/lesson_console_log_intro.mjs
@@ -1,9 +1,10 @@
+import {html_ol} from "./html_ol.mjs";
 import {html_li_text} from "./html_li_text.mjs";
 import {html_element} from "./html_element.mjs";
 import {html_span_text} from "./html_span_text.mjs";
 export function lesson_console_log_intro(parent) {
     html_span_text(parent, 'a computer program has three parts :');
-    let ol = html_element(parent, 'ol');
+    let ol = html_ol(parent);
     html_li_text(ol, 'an input ,');
     html_li_text(ol, 'a process and');
     html_li_text(ol, 'an output');
