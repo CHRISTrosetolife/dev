a06991f5ad6bf638728f1657d1b71803d54ad318
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-25 16:16:21 -0400

    2024-04-25T20:16:20.816Z  function_transform js_outside_move app_learn_code

--- public/src/app_learn_code.mjs
+++ public/src/app_learn_code.mjs
@@ -1,14 +1,16 @@
+import {html_button_width_full_text} from "./html_button_width_full_text.mjs";
+import {list_get} from "./list_get.mjs";
 import {html_button_width_full} from "./html_button_width_full.mjs";
 import {lesson_console_log} from "./lesson_console_log.mjs";
 import {list_first} from "./list_first.mjs";
 import {object_property_get} from "./object_property_get.mjs";
 import {html_document_body} from "./html_document_body.mjs";
-import { html_inner_set } from "./html_inner_set.mjs";
+import {html_inner_set} from "./html_inner_set.mjs";
 export function app_learn_code() {
     let root = html_document_body();
     let lesson = lesson_console_log();
     let screens = object_property_get(lesson, 'screens');
-    let screen_first = list_first(screens);
+    let screen_first = list_get(list, 0);
     screen_first(root);
     let button_next = html_button_width_full_text(root, 'next');
     let button_previous = html_button_width_full_text(root, 'previous');
