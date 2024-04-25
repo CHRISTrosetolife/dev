e1b8836337c6fec7f7a13df9cd957193a4ef9ef5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 20:41:00 -0400

    2024-04-25T00:41:00.234Z  noop

--- public/src/file_js_transform.mjs
+++ public/src/file_js_transform.mjs
@@ -1,9 +1,10 @@
+import {js_empty_statements_remove} from "./js_empty_statements_remove.mjs";
 import {file_js_parse} from "./file_js_parse.mjs";
 import {file_js_unparse} from "./file_js_unparse.mjs";
 export async function file_js_transform(lambda, file_path, args) {
     let ast = await file_js_parse(file_path);
     let result = await lambda(ast, ...args);
-    if (false) js_empty_statements_remove(ast)
+    js_empty_statements_remove(ast);
     await file_js_unparse(file_path, ast);
     return result;
 }
