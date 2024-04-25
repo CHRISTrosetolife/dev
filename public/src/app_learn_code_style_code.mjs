cf259436a712a7d4661eb05a77bfc1c5773707f9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-25 17:48:34 -0400

    2024-04-25T21:48:34.397Z  noop

--- public/src/app_learn_code_style_code.mjs
+++ public/src/app_learn_code_style_code.mjs
@@ -3,8 +3,6 @@ import {html_style} from "./html_style.mjs";
 export function app_learn_code_style_code(code) {
     app_learn_code_style_rounded_padded(code);
     html_style(code, {
-        'padding': '0.5rem',
-        'border-radius': '0.5rem',
         'font-family': 'monospace'
     });
 }
