6cb213b8ff7566a72e84c29f89dae93d5b6557af
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 21:32:33 -0400

    2024-04-25T01:32:33.689Z  noop

--- public/src/lesson_console_log_analogy.mjs
+++ public/src/lesson_console_log_analogy.mjs
@@ -2,5 +2,5 @@ import {html_p_text} from "./html_p_text.mjs";
 import {html_element} from "./html_element.mjs";
 export function lesson_console_log_analogy(parent) {
     html_p_text(parent, 'suppose there is a sheet of paper');
-    html_element(parent, 'img')
+    let i1 = html_element(parent, 'img')
 }
