4461d5ce18dfda216b1b508efa5a3a01965dad5e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 22:04:02 -0400

    2024-04-25T02:04:01.808Z  noop

--- public/src/lesson_console_log_analogy.mjs
+++ public/src/lesson_console_log_analogy.mjs
@@ -21,4 +21,12 @@ export function lesson_console_log_analogy(parent) {
     let li1 = html_li(ol);
     html_span_text(li1, 'the folding of the paper into origami is a ');
     html_strong_text(li1, 'process');
+    let li2 = html_li(ol);
+    html_span_text(li2, 'the unfolded paper is the ');
+    html_strong_text(li2, 'input');
+    html_span_text(li2, ' to the folding process');
+    let li3 = html_li(ol);
+    html_span_text(li3, 'the folded paper is the ');
+    html_strong_text(li3, 'output');
+    html_span_text(li3, ' of the folding process');
 }
