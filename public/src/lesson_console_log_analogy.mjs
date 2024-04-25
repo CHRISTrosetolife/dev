cb3ab3209928be3131a941c2827a4627810bfa87
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 21:41:41 -0400

    2024-04-25T01:41:41.131Z  noop

--- public/src/lesson_console_log_analogy.mjs
+++ public/src/lesson_console_log_analogy.mjs
@@ -5,8 +5,8 @@ import {object_property_set} from "./object_property_set.mjs";
 import {string_combine} from "./string_combine.mjs";
 export function lesson_console_log_analogy(parent) {
     html_p_text(parent, 'suppose there is a sheet of paper');
-    let i1 = html_element(parent, 'img');
     let img_name = 'paper.jpg';
+    let i1 = html_element(parent, 'img');
     let img_path = string_combine('./img/', img_name);
     let {element} = i1;
     object_property_set(element, 'src', img_path);
