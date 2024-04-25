cd259b1aa8c37712f9d563670afae038c56423b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 21:44:39 -0400

    2024-04-25T01:44:39.114Z  noop

--- public/src/lesson_console_log_analogy.mjs
+++ public/src/lesson_console_log_analogy.mjs
@@ -5,6 +5,10 @@ import {html_element} from "./html_element.mjs";
 import {object_property_set} from "./object_property_set.mjs";
 import {string_combine} from "./string_combine.mjs";
 export function lesson_console_log_analogy(parent) {
-    html_p_text(parent, 'suppose there is a sheet of paper');
+    html_p_text(parent, 'suppose there is paper');
     html_img_width_full(parent, 'paper.jpg');
+    html_p_text(parent, 'and suppose someone folds the paper into origami');
+    html_img_width_full(parent, 'paper_folding.jpg');
+    html_p_text(parent, 'then afterward there is folded paper origami');
+    html_img_width_full(parent, 'paper_folded.jpg');
 }
