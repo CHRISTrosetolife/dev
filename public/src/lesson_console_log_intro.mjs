5d4a3a10d00d8514c6a46d72cdcb08407811b688
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 21:45:56 -0400

    2024-04-25T01:45:56.404Z  noop

--- public/src/lesson_console_log_intro.mjs
+++ public/src/lesson_console_log_intro.mjs
@@ -2,8 +2,7 @@ import {html_li_text} from "./html_li_text.mjs";
 import {html_element} from "./html_element.mjs";
 import {html_span_text} from "./html_span_text.mjs";
 export function lesson_console_log_intro(parent) {
-    let text = 'a computer program has three parts :';
-    html_span_text(parent, text);
+    html_span_text(parent, 'a computer program has three parts :');
     let ol = html_element(parent, 'ol');
     html_li_text(ol, 'an input ,');
     html_li_text(ol, 'a process and');
