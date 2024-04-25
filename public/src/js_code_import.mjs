3472619332624d6bb9d35054d02def4f4dc92a2d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-24 20:46:52 -0400

    2024-04-25T00:46:52.684Z  function_transform js_outside_move js_code_import

--- public/src/js_code_import.mjs
+++ public/src/js_code_import.mjs
@@ -1,8 +1,8 @@
+import {js_code_import_path} from "./js_code_import_path.mjs";
 import {js_code_statement} from "./js_code_statement.mjs";
-import { string_combine } from "./string_combine.mjs";
+import {string_combine} from "./string_combine.mjs";
 export function js_code_import(import_name) {
     const import_path = './';
-    let import_path_combined = string_combine(import_path, import_name)
-    let import_code = js_code_statement(`import { ${import_name} } from "${import_path_combined}.mjs"`);
+    let import_code = js_code_import_path(import_path, import_name);
     return import_code;
 }
