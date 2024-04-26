d28497714f2e210ab26fbb83f5a30348c4492db6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 18:35:10 -0400

    2024-04-26T22:35:10.332Z  function_transform js_outside_move lesson_console_log_quiz

--- public/src/lesson_console_log_quiz.mjs
+++ public/src/lesson_console_log_quiz.mjs
@@ -1,5 +1,7 @@
+import {integer_random} from "./integer_random.mjs";
 import {string_split_space} from "./string_split_space.mjs";
 export function lesson_console_log_quiz() {
     let sentence = 'for GOD so loved the world that HE gave HIS only SON that whoever believes in HIM shall not perish but have eternal life';
     let words = string_split_space(sentence);
+    let count = integer_random(1, 3);
 }
