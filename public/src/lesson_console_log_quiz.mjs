c197845b681e3d66d0e8cce8bba363bfac8a0c8c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 18:59:58 -0400

    2024-04-26T22:59:57.661Z  function_transform js_outside_move lesson_console_log_quiz

--- public/src/lesson_console_log_quiz.mjs
+++ public/src/lesson_console_log_quiz.mjs
@@ -1,3 +1,6 @@
+import {app_learn_code_code_part_titled_code} from "./app_learn_code_code_part_titled_code.mjs";
+import {app_learn_code_code_container} from "./app_learn_code_code_container.mjs";
+import {lesson_console_log_quiz_get} from "./lesson_console_log_quiz_get.mjs";
 import {list_join} from "./list_join.mjs";
 import {subtract} from "./subtract.mjs";
 import {list_slice} from "./list_slice.mjs";
@@ -9,5 +12,4 @@ export function lesson_console_log_quiz(parent) {
     let source = lesson_console_log_quiz_get();
     let container = app_learn_code_code_container(parent);
     app_learn_code_code_part_titled_code(container, `console.log('hello');`);
-    
 }
