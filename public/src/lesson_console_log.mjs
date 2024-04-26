68ef5dcb163f32c95ac32bdb1f43ce6aa38a2490
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 14:11:09 -0400

    2024-04-26T18:11:09.240Z  noop

--- public/src/lesson_console_log.mjs
+++ public/src/lesson_console_log.mjs
@@ -9,17 +9,20 @@ import {lesson_console_log_analogy_paper} from "./lesson_console_log_analogy_pap
 import {lesson_console_log_analogy_addition} from "./lesson_console_log_analogy_addition.mjs";
 import { lesson_console_log_program_first } from "./lesson_console_log_program_first.mjs";
 import { lesson_console_log_strings } from "./lesson_console_log_strings.mjs";
+import { lesson_console_log_analogy_addition_symbol } from "./lesson_console_log_analogy_addition_symbol.mjs";
 export function lesson_console_log() {
     let v1 = object_new();
     object_property_set(v1, 'description', 'outputting a string to the console');
     let v2 = array_new();
     list_add(v2, lesson_console_log_analogy_paper);
     list_add(v2, lesson_console_log_analogy_addition);
+    list_add(v2, lesson_console_log_analogy_addition_symbol);
     list_add(v2, lesson_console_log_process_named);
     list_add(v2, lesson_console_log_javascript);
     list_add(v2, lesson_console_log_strings);
     list_add(v2, lesson_console_log_process_computer_program);
     list_add(v2, lesson_console_log_program_first);
+    
     object_property_set(v1, 'screens', v2);
     return v1;
 }
--- public/src/lesson_console_log_analogy_addition.mjs
+++ public/src/lesson_console_log_analogy_addition.mjs
@@ -18,13 +18,4 @@ export function lesson_console_log_analogy_addition(parent) {
     html_li_text(ol1, 'the adding of the two numbers together is a process');
     html_li_text(ol1, 'the two numbers are the inputs to the addition process');
     html_li_text(ol1, 'the sum of the two numbers is the output of the addition process');
-    html_hr(parent);
-    html_p_text(parent, 'suppose the letters "a", "b" and "c" represent numbers');
-    html_p_text(parent, 'suppose "c" is the sum of "a" and "b"');
-    html_p_text(parent, 'then here is the equation : a + b = c');
-    html_p_text(parent, 'we could say :');
-    let ol2 = html_ul(parent);
-    html_li_text(ol2, 'the addition sign ( "+" ) is a process');
-    html_li_text(ol2, '"a" and "b" are the inputs to the "+" process');
-    html_li_text(ol2, '"c" is the output of the "+" process');
 }
