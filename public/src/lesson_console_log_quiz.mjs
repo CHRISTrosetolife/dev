24c71cfbeec805839d37e03b1091d122712ade4a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 19:04:17 -0400

    2024-04-26T23:04:17.507Z  function_transform js_outside_move lesson_console_log_quiz

--- public/src/lesson_console_log_quiz.mjs
+++ public/src/lesson_console_log_quiz.mjs
@@ -1,3 +1,5 @@
+import {html_button_width_full_text_click} from "./html_button_width_full_text_click.mjs";
+import {html_hr} from "./html_hr.mjs";
 import {app_learn_code_code_part_titled_code} from "./app_learn_code_code_part_titled_code.mjs";
 import {app_learn_code_code_container} from "./app_learn_code_code_container.mjs";
 import {lesson_console_log_quiz_get} from "./lesson_console_log_quiz_get.mjs";
@@ -8,19 +10,18 @@ import {add} from "./add.mjs";
 import {integer_random} from "./integer_random.mjs";
 import {list_length} from "./list_length.mjs";
 import {string_split_space} from "./string_split_space.mjs";
-import { range } from "./range.mjs";
+import {range} from "./range.mjs";
 export function lesson_console_log_quiz(parent) {
-    let source = lesson_console_log_quiz_get();
     let container = app_learn_code_code_container(parent);
     app_learn_code_code_part_titled_code(container, source);
     html_hr(container);
     let choices_count = 4;
     let r = range(choices_count);
+    let choices = list_new();
     for (let c of r) {
         for (let i of range(100)) {
-
+            let source = lesson_console_log_quiz_get();
         }
     }
-    let button = html_button_width_full_text_click(root, 'previous', function on_click() {
-    });
+    let button = html_button_width_full_text_click(root, 'previous', function on_click() {});
 }
