830e9400dec8dcec2759bc1d87688aa28599c13b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 19:09:58 -0400

    2024-04-26T23:09:57.072Z  function_transform js_outside_move lesson_console_log

--- public/src/lesson_console_log.mjs
+++ public/src/lesson_console_log.mjs
@@ -1,3 +1,4 @@
+import {lesson_console_log_quiz} from "./lesson_console_log_quiz.mjs";
 import {lesson_console_log_javascript} from "./lesson_console_log_javascript.mjs";
 import {lesson_console_log_process_named} from "./lesson_console_log_process_named.mjs";
 import {lesson_console_log_process_computer_program} from "./lesson_console_log_process_computer_program.mjs";
@@ -7,9 +8,9 @@ import {object_new} from "./object_new.mjs";
 import {object_property_set} from "./object_property_set.mjs";
 import {lesson_console_log_analogy_paper} from "./lesson_console_log_analogy_paper.mjs";
 import {lesson_console_log_analogy_addition} from "./lesson_console_log_analogy_addition.mjs";
-import { lesson_console_log_program_first } from "./lesson_console_log_program_first.mjs";
-import { lesson_console_log_strings } from "./lesson_console_log_strings.mjs";
-import { lesson_console_log_analogy_addition_symbol } from "./lesson_console_log_analogy_addition_symbol.mjs";
+import {lesson_console_log_program_first} from "./lesson_console_log_program_first.mjs";
+import {lesson_console_log_strings} from "./lesson_console_log_strings.mjs";
+import {lesson_console_log_analogy_addition_symbol} from "./lesson_console_log_analogy_addition_symbol.mjs";
 export function lesson_console_log() {
     let v1 = object_new();
     object_property_set(v1, 'description', 'outputting a string to the console');
@@ -23,7 +24,6 @@ export function lesson_console_log() {
     list_add(v2, lesson_console_log_process_computer_program);
     list_add(v2, lesson_console_log_program_first);
     list_add(v2, lesson_console_log_quiz);
-    
     object_property_set(v1, 'screens', v2);
     return v1;
 }
