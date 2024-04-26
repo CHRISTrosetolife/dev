ce7b4e8c5909ed3be651dcd8b62c15fc910d1ae3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 18:56:37 -0400

    2024-04-26T22:56:37.353Z  noop

--- public/src/lesson_console_log_quiz.mjs
+++ public/src/lesson_console_log_quiz.mjs
@@ -5,16 +5,6 @@ import {add} from "./add.mjs";
 import {integer_random} from "./integer_random.mjs";
 import {list_length} from "./list_length.mjs";
 import {string_split_space} from "./string_split_space.mjs";
-export function lesson_console_log_quiz() {
-    let sentence = 'for GOD so loved the world that HE gave HIS only SON that whoever believes in HIM shall not perish but have eternal life';
-    let words = string_split_space(sentence);
-    let word_count = integer_random(2, 4);
-    let length = list_length(words);
-    let high = subtract(length, word_count);
-    let index = integer_random(0, high);
-    let end = add(index, word_count);
-    let sliced = list_slice(words, index, end);
-    let message = list_join(sliced, ' ');
-    let code = `console.log('${message}');`
-    return code;
+export function lesson_console_log_quiz(parent) {
+    
 }
--- public/src/lesson_console_log_quiz_get.mjs
+++ public/src/lesson_console_log_quiz_get.mjs
@@ -5,7 +5,7 @@ import {add} from "./add.mjs";
 import {integer_random} from "./integer_random.mjs";
 import {list_length} from "./list_length.mjs";
 import {string_split_space} from "./string_split_space.mjs";
-export function lesson_console_log_quiz() {
+export function lesson_console_log_quiz_get() {
     let sentence = 'for GOD so loved the world that HE gave HIS only SON that whoever believes in HIM shall not perish but have eternal life';
     let words = string_split_space(sentence);
     let word_count = integer_random(2, 4);
@@ -15,6 +15,6 @@ export function lesson_console_log_quiz() {
     let end = add(index, word_count);
     let sliced = list_slice(words, index, end);
     let message = list_join(sliced, ' ');
-    let code = `console.log('${message}');`
+    let code = `console.log('${message}');`;
     return code;
 }
