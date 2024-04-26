7e1e233140a490987d70d772c890deeed70490e6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 19:20:18 -0400

    2024-04-26T23:20:17.989Z  noop

--- public/src/lesson_console_log_quiz.mjs
+++ public/src/lesson_console_log_quiz.mjs
@@ -46,9 +46,7 @@ export function lesson_console_log_quiz(parent) {
     });
     let s = subtract(list_length(choices), 1);
     let correct_index = integer_random(0, s);
-    let correct = list_get(choices, correct_index);
-    let {source} = correct;
-    console.log({correct, s})
+    let source = list_get(choices, correct_index);
     app_learn_code_code_part_titled_code(container, source);
     html_hr(container);
     for (let m of mapped) {
