64de38be2404542a3867d0d8414e4769d6b4f0f7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 18:56:00 -0400

    2024-04-26T22:55:59.779Z  function_copy lesson_console_log_quiz lesson_console_log_quiz_get

---
+++ public/src/lesson_console_log_quiz_get.mjs
@@ -0,0 +1,20 @@
+import {list_join} from "./list_join.mjs";
+import {subtract} from "./subtract.mjs";
+import {list_slice} from "./list_slice.mjs";
+import {add} from "./add.mjs";
+import {integer_random} from "./integer_random.mjs";
+import {list_length} from "./list_length.mjs";
+import {string_split_space} from "./string_split_space.mjs";
+export function lesson_console_log_quiz() {
+    let sentence = 'for GOD so loved the world that HE gave HIS only SON that whoever believes in HIM shall not perish but have eternal life';
+    let words = string_split_space(sentence);
+    let word_count = integer_random(2, 4);
+    let length = list_length(words);
+    let high = subtract(length, word_count);
+    let index = integer_random(0, high);
+    let end = add(index, word_count);
+    let sliced = list_slice(words, index, end);
+    let message = list_join(sliced, ' ');
+    let code = `console.log('${message}');`
+    return code;
+}
