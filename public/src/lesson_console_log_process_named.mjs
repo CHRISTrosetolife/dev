ad99c4afbbd10cd15142e80c6b76af6eb90b3039
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 12:11:19 -0400

    2024-04-26T16:11:19.423Z  noop

--- public/src/lesson_console_log_process_named.mjs
+++ public/src/lesson_console_log_process_named.mjs
@@ -38,11 +38,10 @@ export function lesson_console_log_process_named(parent) {
     html_style_alternate(parent, html_p, [noop, html_style_monospace], ['in javascript a semicolon ( ', ';', ' ) is a separator']);
     html_hr(parent);
     html_style_alternate(parent, html_p, [noop, html_style_bold], ['here are some examples of a ', 'string', ' ( a string is text like letters and symbols ) in javascript :']);
-    const code = '\'hello\'';
-    app_learn_code_code_part_contrast(parent, code);
-    html_li_text_monospace(ol4, '\'strings can have spaces\'');
-    html_li_text_monospace(ol4, '\'strings can have numbers like : 123\'');
-    html_li_text_monospace(ol4, '\'strings can have numbers like 123\'');
+    app_learn_code_code_part_contrast(parent, '\'hello\'');
+    app_learn_code_code_part_contrast(ol4, '\'strings can have spaces\'');
+    app_learn_code_code_part_contrast(ol4, '\'strings can have numbers like : 123\'');
+    app_learn_code_code_part_contrast(ol4, '\'strings can have numbers like 123\'');
     html_style_alternate(parent, html_p, [noop, html_style_monospace], ['notice that the beginning of a string has a quote ( ', '\'', ' ) and so does the end of a string']);
 }
 function app_learn_code_code_part_contrast(parent, code) {
