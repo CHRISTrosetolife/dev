7d0f5803582b04d084a3c08e19531d4271edd572
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 19:23:10 -0400

    2024-04-26T23:23:09.655Z  function_transform js_outside_move app_learn_code_code_part_titled

---
+++ public/src/app_learn_code_code_part_title.mjs
@@ -0,0 +1,12 @@
+import {html_spacer_vertical} from "./html_spacer_vertical.mjs";
+import {html_style_margin_none} from "./html_style_margin_none.mjs";
+import {html_style} from "./html_style.mjs";
+import {html_p_text} from "./html_p_text.mjs";
+export function app_learn_code_code_part_title(parent, title_text) {
+    let title = html_p_text(parent, title_text);
+    html_style(title, {
+        'font-size': '2.3vh'
+    });
+    html_style_margin_none(title);
+    html_spacer_vertical(parent);
+}
--- public/src/app_learn_code_code_part_titled.mjs
+++ public/src/app_learn_code_code_part_titled.mjs
@@ -1,3 +1,4 @@
+import {app_learn_code_code_part_title} from "./app_learn_code_code_part_title.mjs";
 import {app_learn_code_code_part} from "./app_learn_code_code_part.mjs";
 import {app_learn_code_style_code} from "./app_learn_code_style_code.mjs";
 import {html_style} from "./html_style.mjs";
@@ -5,12 +6,7 @@ import {html_spacer_vertical} from "./html_spacer_vertical.mjs";
 import {html_style_margin_none} from "./html_style_margin_none.mjs";
 import {html_p_text} from "./html_p_text.mjs";
 export function app_learn_code_code_part_titled(parent, title_text, source_code, background_color) {
-    let title = html_p_text(parent, title_text);
-    html_style(title, {
-        'font-size': '2.3vh'
-    });
-    html_style_margin_none(title);
-    html_spacer_vertical(parent);
+    app_learn_code_code_part_title(parent, title_text);
     let code = app_learn_code_code_part(parent, source_code, background_color);
     return {
         code
