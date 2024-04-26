458f44e8f861c3b93699f78d1c3094ea87e913fc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 12:07:48 -0400

    2024-04-26T16:07:48.535Z  function_transform js_outside_move app_learn_code_code_part_titled

--- public/src/app_learn_code_code_part_titled.mjs
+++ public/src/app_learn_code_code_part_titled.mjs
@@ -1,3 +1,4 @@
+import {app_learn_code_code_part} from "./app_learn_code_code_part.mjs";
 import {app_learn_code_style_code} from "./app_learn_code_style_code.mjs";
 import {html_style} from "./html_style.mjs";
 import {html_spacer_vertical} from "./html_spacer_vertical.mjs";
@@ -7,13 +8,7 @@ export function app_learn_code_code_part_titled(parent, title_text, source_code,
     let title = html_p_text(parent, title_text);
     html_style_margin_none(title);
     html_spacer_vertical(parent);
-    let code = html_p_text(parent, source_code);
-    app_learn_code_style_code(code);
-    html_style_margin_none(code);
-    html_style(code, {
-        'background-color': background_color,
-        'word-break': 'break-all'
-    });
+    let code = app_learn_code_code_part(parent, source_code, background_color);
     return {
         code
     };
