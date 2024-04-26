9ce1e52212e5e1fee6709f4231a99b5f8f82466b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 18:33:48 -0400

    2024-04-26T22:33:47.813Z  function_transform js_outside_move lesson_console_log_quiz

--- public/src/lesson_console_log_quiz.mjs
+++ public/src/lesson_console_log_quiz.mjs
@@ -1,4 +1,5 @@
+import {string_split_space} from "./string_split_space.mjs";
 export function lesson_console_log_quiz() {
-    let sentence = 'for GOD so loved the world that HE gave HIS only SON that whoever believes in HIM shall not perish but have eternal life'
-    let words = string_split(sentence, ' ')
+    let sentence = 'for GOD so loved the world that HE gave HIS only SON that whoever believes in HIM shall not perish but have eternal life';
+    let words = string_split_space(sentence);
 }
