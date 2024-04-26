01399c397bcc658ca9a50f27b9803632d88bc98b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 13:59:38 -0400

    2024-04-26T17:59:38.123Z  noop

--- public/src/lesson_console_log_strings.mjs
+++ public/src/lesson_console_log_strings.mjs
@@ -18,10 +18,6 @@ import {list_get} from "./list_get.mjs";
 import {html_style_bold} from "./html_style_bold.mjs";
 import {html_spacer_vertical} from "./html_spacer_vertical.mjs";
 export function lesson_console_log_strings(parent) {
-    html_style_alternate(parent, html_p, [noop, html_style_bold], ['in computer programming different symbols can be used as a ', 'separator', ' so that the computer can tell two things apart']);
-    html_style_alternate(parent, html_p, [noop, html_style_bold], ['we will be learning a computer programming language called ', 'javascript']);
-    html_style_alternate(parent, html_p, [noop, html_style_monospace], ['in javascript a semicolon ( ', ';', ' ) is a separator']);
-    html_hr(parent);
     html_style_alternate(parent, html_p, [noop, html_style_bold], ['here are some examples of ', 'strings', ' ( a string is text , like letters and symbols ) in javascript :']);
     app_learn_code_code_part_contrast(parent, '\'hello\'');
     html_spacer_vertical(parent);
