c51f85427badc40fb6ebbf001c7423fcb37ff457
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 18:33:11 -0400

    2024-04-26T22:33:10.921Z  function_search space

--- public/src/lesson_console_log_quiz.mjs
+++ public/src/lesson_console_log_quiz.mjs
@@ -1,1 +1,4 @@
-export function lesson_console_log_quiz() {}
+export function lesson_console_log_quiz() {
+    let sentence = 'for GOD so loved the world that HE gave HIS only SON that whoever believes in HIM shall not perish but have eternal life'
+    let words = string_split(sentence, ' ')
+}
