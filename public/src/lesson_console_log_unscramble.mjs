2f04fd9dd3add18ce8f8ba3e3ff7ee280c4017e8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 21:58:43 -0400

    2024-04-27T01:58:43.280Z  function_transform js_outside_move lesson_console_log_unscramble

--- public/src/lesson_console_log_unscramble.mjs
+++ public/src/lesson_console_log_unscramble.mjs
@@ -1,9 +1,12 @@
+import {app_learn_code_eval_messages_to_string} from "./app_learn_code_eval_messages_to_string.mjs";
+import {app_learn_code_eval} from "./app_learn_code_eval.mjs";
 import {js_tokenize} from "./js_tokenize.mjs";
 import {html_p_text} from "./html_p_text.mjs";
-import { lesson_console_log_quiz_get } from "./lesson_console_log_quiz_get.mjs";
+import {lesson_console_log_quiz_get} from "./lesson_console_log_quiz_get.mjs";
 export function lesson_console_log_unscramble(parent) {
     let source = lesson_console_log_quiz_get();
     let messages = app_learn_code_eval(source);
+    let joined = app_learn_code_eval_messages_to_string(messages);
     html_p_text(parent, 'below is another quiz');
     html_p_text(parent, 'write a program that outputs : hello');
     for (let token of js_tokenize('function test(){}')) {
