e83fbbd5ce5c7ebfe02fcc5e619318f64b32334b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 18:19:14 -0400

    2024-04-27T22:19:13.492Z  noop

--- public/src/lesson_simple.mjs
+++ public/src/lesson_simple.mjs
@@ -5,10 +5,13 @@ import {list_add} from "./list_add.mjs";
 import {array_new} from "./array_new.mjs";
 import {object_property_set} from "./object_property_set.mjs";
 import {object_new} from "./object_new.mjs";
-export function lesson_simple(source_get, example_before, description, example_message) {
+export function lesson_simple(source_get, example_before, description, example_message, screens_before) {
     let v1 = object_new();
     object_property_set(v1, 'description', description);
     let v2 = array_new();
+    for (let s of screens_before) {
+        list_add(v2, s);
+    }
     list_add(v2, lesson_log_number_example(source_get, example_message, example_before));
     list_add(v2, app_learn_code_quiz(source_get));
     list_add(v2, app_learn_code_unscramble(source_get));
