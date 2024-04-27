747c82c440fe5801687b9a6cd51791fcae68f8cd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 17:03:17 -0400

    2024-04-27T21:03:16.666Z  function_transform js_outside_move lesson_add

--- public/src/lesson_add.mjs
+++ public/src/lesson_add.mjs
@@ -1,3 +1,4 @@
+import {html_p} from "./html_p.mjs";
 import {app_learn_code_unscramble} from "./app_learn_code_unscramble.mjs";
 import {lesson_log_number_quiz} from "./lesson_log_number_quiz.mjs";
 import {app_learn_code_quiz} from "./app_learn_code_quiz.mjs";
@@ -15,17 +16,17 @@ import {lesson_console_log_program_first} from "./lesson_console_log_program_fir
 import {lesson_console_log_strings} from "./lesson_console_log_strings.mjs";
 import {lesson_console_log_analogy_addition_symbol} from "./lesson_console_log_analogy_addition_symbol.mjs";
 import {lesson_log_number_explain} from "./lesson_log_number_explain.mjs";
-import { html_style_alternate } from "./html_style_alternate.mjs";
-import { html_p_text } from "./html_p_text.mjs";
-import { noop } from "./noop.mjs";
-import { html_style_bold } from "./html_style_bold.mjs";
-import { html_style_monospace } from "./html_style_monospace.mjs";
+import {html_style_alternate} from "./html_style_alternate.mjs";
+import {html_p_text} from "./html_p_text.mjs";
+import {noop} from "./noop.mjs";
+import {html_style_bold} from "./html_style_bold.mjs";
+import {html_style_monospace} from "./html_style_monospace.mjs";
 export function lesson_add() {
     let v1 = object_new();
     object_property_set(v1, 'description', 'outputting a number to the console');
     let v2 = array_new();
     list_add(v2, lesson_log_number_example(lesson_log_number_quiz, 'adding two numbers together', parent => {
-        html_style_alternate(parent, html_p_text, [noop, html_style_monospace], ['two numbers can be added together using the plus ( ', '+', ' ) sign'])
+        html_style_alternate(parent, html_p, [noop, html_style_monospace], ['two numbers can be added together using the plus ( ', '+', ' ) sign']);
     }));
     list_add(v2, app_learn_code_quiz(lesson_log_number_quiz));
     list_add(v2, app_learn_code_unscramble(lesson_log_number_quiz));
