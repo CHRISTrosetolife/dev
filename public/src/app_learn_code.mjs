0d7bf643db815d2fa02ceb0af9443bb44c210b4a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 10:38:46 -0400

    2024-04-27T14:38:46.277Z  noop

--- public/src/app_learn_code.mjs
+++ public/src/app_learn_code.mjs
@@ -29,7 +29,7 @@ export function app_learn_code() {
     });
     let lesson_creates = [lesson_console_log]
     for (let lesson_create of lesson_creates) {
-
+        let lesson = lesson_create();
     }
     let screens = object_property_get(lesson, 'screens');
     let index_last = list_index_last(screens);
