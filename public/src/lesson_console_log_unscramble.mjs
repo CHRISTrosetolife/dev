610ccd3326abff4192ec0459016436386fd1b896
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 21:40:06 -0400

    2024-04-27T01:40:05.526Z  function_transform js_outside_move lesson_console_log_unscramble

--- public/src/lesson_console_log_unscramble.mjs
+++ public/src/lesson_console_log_unscramble.mjs
@@ -1,1 +1,4 @@
-export function lesson_console_log_unscramble(parent) {}
+import {html_p_text} from "./html_p_text.mjs";
+export function lesson_console_log_unscramble(parent) {
+    html_p_text(parent, 'write a program that outputs : hello');
+}
