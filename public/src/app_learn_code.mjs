a8daeac40b5d93802b89d00d62c75fde7376bf42
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 17:13:29 -0400

    2024-04-27T21:13:28.985Z  function_transform js_outside_move app_learn_code

--- public/src/app_learn_code.mjs
+++ public/src/app_learn_code.mjs
@@ -1,3 +1,4 @@
+import {lesson_subtract} from "./lesson_subtract.mjs";
 import {lesson_add} from "./lesson_add.mjs";
 import {lesson_log_number} from "./lesson_log_number.mjs";
 import {list_add_multiple} from "./list_add_multiple.mjs";
@@ -35,6 +36,7 @@ export function app_learn_code() {
     list_add(lesson_creates, lesson_console_log);
     list_add(lesson_creates, lesson_log_number);
     list_add(lesson_creates, lesson_add);
+    list_add(lesson_creates, lesson_subtract);
     let screens = [];
     for (let lesson_create of lesson_creates) {
         let lesson = lesson_create();
--- public/src/lesson_subtract.mjs
+++ public/src/lesson_subtract.mjs
@@ -23,11 +23,11 @@ import {html_p_text} from "./html_p_text.mjs";
 import {noop} from "./noop.mjs";
 import {html_style_bold} from "./html_style_bold.mjs";
 import {html_style_monospace} from "./html_style_monospace.mjs";
-export function lesson_add() {
-    const example_message = 'adding two numbers together';
-    const description = 'outputting a number to the console';
+export function lesson_subtract() {
+    const example_message = 'subtracting two numbers';
+    const description = example_message;
     return lesson_simple(lesson_add_quiz, example_before, description, example_message);
     function example_before(parent) {
-        html_style_alternate(parent, html_p, [noop, html_style_monospace], ['two numbers can be added together using the plus ( ', '+', ' ) sign']);
+        html_style_alternate(parent, html_p, [noop, html_style_monospace], ['one number can be subtracted from the other using the minus ( ', '-', ' ) sign']);
     }
 }
