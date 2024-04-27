ebcd7291ea56398fa2afaf57166808d8d61d7185
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 16:59:17 -0400

    2024-04-27T20:59:16.589Z  noop

--- public/src/lesson_add_quiz.mjs
+++ public/src/lesson_add_quiz.mjs
@@ -4,6 +4,7 @@ import {random_50_50} from "./random_50_50.mjs";
 import {integer_random} from "./integer_random.mjs";
 export function lesson_add_quiz() {
     let a = integer_random_digit_single();
+    let b = integer_random_digit_single();
     let value = integer_random(0, 999999);
     if (random_50_50()) {
         value = value / list_random_item([10, 100, 1000]);
