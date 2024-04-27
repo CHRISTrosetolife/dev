19426a5482c0a013b1eb5571eec56ba65bc113c9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 18:33:20 -0400

    2024-04-27T22:33:19.468Z  noop

--- public/src/app_learn_code_code_part_titled_code.mjs
+++ public/src/app_learn_code_code_part_titled_code.mjs
@@ -1,9 +1,13 @@
 import {app_learn_code_code_background} from "./app_learn_code_code_background.mjs";
 import {app_learn_code_style_code_color} from "./app_learn_code_style_code_color.mjs";
 import {app_learn_code_code_part_titled} from "./app_learn_code_code_part_titled.mjs";
+import { html_style } from "./html_style.mjs";
 export function app_learn_code_code_part_titled_code(container, source) {
     let result = app_learn_code_code_part_titled(container, 'code :', source, app_learn_code_code_background());
     let {code} = result;
     app_learn_code_style_code_color(code);
+    html_style(code, {
+        'white-space': 'pre'
+    })
     return result;
 }
