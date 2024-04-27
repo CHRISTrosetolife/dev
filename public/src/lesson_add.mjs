30ebe726a1b40a91e8ca58503a71e880e712ab05
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 17:03:33 -0400

    2024-04-27T21:03:32.561Z  function_transform js_outside_move lesson_add

--- public/src/lesson_add.mjs
+++ public/src/lesson_add.mjs
@@ -1,3 +1,4 @@
+import {lesson_add_quiz} from "./lesson_add_quiz.mjs";
 import {html_p} from "./html_p.mjs";
 import {app_learn_code_unscramble} from "./app_learn_code_unscramble.mjs";
 import {lesson_log_number_quiz} from "./lesson_log_number_quiz.mjs";
@@ -25,11 +26,11 @@ export function lesson_add() {
     let v1 = object_new();
     object_property_set(v1, 'description', 'outputting a number to the console');
     let v2 = array_new();
-    list_add(v2, lesson_log_number_example(lesson_log_number_quiz, 'adding two numbers together', parent => {
+    list_add(v2, lesson_log_number_example(lesson_add_quiz, 'adding two numbers together', parent => {
         html_style_alternate(parent, html_p, [noop, html_style_monospace], ['two numbers can be added together using the plus ( ', '+', ' ) sign']);
     }));
-    list_add(v2, app_learn_code_quiz(lesson_log_number_quiz));
-    list_add(v2, app_learn_code_unscramble(lesson_log_number_quiz));
+    list_add(v2, app_learn_code_quiz(lesson_add_quiz));
+    list_add(v2, app_learn_code_unscramble(lesson_add_quiz));
     object_property_set(v1, 'screens', v2);
     return v1;
 }
