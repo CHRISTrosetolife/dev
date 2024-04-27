02150f5d25669400d32ec55836ab4ba6b0cedcd0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 22:01:14 -0400

    2024-04-27T02:01:14.293Z  noop

--- public/src/lesson_console_log_unscramble.mjs
+++ public/src/lesson_console_log_unscramble.mjs
@@ -3,6 +3,7 @@ import {app_learn_code_eval} from "./app_learn_code_eval.mjs";
 import {js_tokenize} from "./js_tokenize.mjs";
 import {html_p_text} from "./html_p_text.mjs";
 import {lesson_console_log_quiz_get} from "./lesson_console_log_quiz_get.mjs";
+import { list_map } from "./list_map.mjs";
 export function lesson_console_log_unscramble(parent) {
     let source = lesson_console_log_quiz_get();
     let messages = app_learn_code_eval(source);
@@ -14,5 +15,9 @@ export function lesson_console_log_unscramble(parent) {
         for (let token of js_tokenize(source)) {
             la(token);
         }
+    })
+    let mapped = list_map(tokens, t => {
+        let {type} = t;
+        let {label} = type;
     })
 }
