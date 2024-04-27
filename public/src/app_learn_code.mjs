7afa3721d72d6490066ad6146b23244c697fe278
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 17:18:41 -0400

    2024-04-27T21:18:40.709Z  function_transform js_outside_move app_learn_code

--- public/src/app_learn_code.mjs
+++ public/src/app_learn_code.mjs
@@ -1,3 +1,4 @@
+import {lesson_multiply} from "./lesson_multiply.mjs";
 import {lesson_subtract} from "./lesson_subtract.mjs";
 import {lesson_add} from "./lesson_add.mjs";
 import {lesson_log_number} from "./lesson_log_number.mjs";
@@ -37,6 +38,7 @@ export function app_learn_code() {
     list_add(lesson_creates, lesson_log_number);
     list_add(lesson_creates, lesson_add);
     list_add(lesson_creates, lesson_subtract);
+    list_add(lesson_creates, lesson_multiply);
     let screens = [];
     for (let lesson_create of lesson_creates) {
         let lesson = lesson_create();
