8e674c046bc4783cd0551e0fce45ef05745de63f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 16:59:33 -0400

    2024-04-27T20:59:33.239Z  noop

--- public/src/lesson_add_quiz.mjs
+++ public/src/lesson_add_quiz.mjs
@@ -5,12 +5,5 @@ import {integer_random} from "./integer_random.mjs";
 export function lesson_add_quiz() {
     let a = integer_random_digit_single();
     let b = integer_random_digit_single();
-    let value = integer_random(0, 999999);
-    if (random_50_50()) {
-        value = value / list_random_item([10, 100, 1000]);
-    }
-    if (random_50_50()) {
-        value = value * -1;
-    }
-    return `console.log(${value});`;
+    return `console.log(${a} + ${b});`;
 }
