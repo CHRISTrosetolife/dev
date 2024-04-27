67428e6a864310ad13fc64919af391bffbb1d39f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 21:49:22 -0400

    2024-04-27T01:49:21.811Z  noop

--- public/src/lesson_console_log_unscramble.mjs
+++ public/src/lesson_console_log_unscramble.mjs
@@ -1,4 +1,6 @@
 import {html_p_text} from "./html_p_text.mjs";
+import { js_parse } from "./js_parse.mjs";
 export function lesson_console_log_unscramble(parent) {
     html_p_text(parent, 'write a program that outputs : hello');
+    js_parse('test')
 }
