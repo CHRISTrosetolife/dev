dcdb8083025b016d59e0d316e8b08378d3bc0581
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 22:51:36 -0400

    2024-04-27T02:51:35.337Z  function_transform js_outside_move lesson_console_log_unscramble

--- public/src/lesson_console_log_unscramble.mjs
+++ public/src/lesson_console_log_unscramble.mjs
@@ -1,9 +1,13 @@
+import {app_learn_code_code_background} from "./app_learn_code_code_background.mjs";
+import {html_span_text} from "./html_span_text.mjs";
+import {app_learn_code_code_part_generic} from "./app_learn_code_code_part_generic.mjs";
+import {list_adder} from "./list_adder.mjs";
 import {app_learn_code_eval_messages_to_string} from "./app_learn_code_eval_messages_to_string.mjs";
 import {app_learn_code_eval} from "./app_learn_code_eval.mjs";
 import {js_tokenize} from "./js_tokenize.mjs";
 import {html_p_text} from "./html_p_text.mjs";
 import {lesson_console_log_quiz_get} from "./lesson_console_log_quiz_get.mjs";
-import { list_map } from "./list_map.mjs";
+import {list_map} from "./list_map.mjs";
 export function lesson_console_log_unscramble(parent) {
     let source = lesson_console_log_quiz_get();
     let messages = app_learn_code_eval(source);
@@ -15,14 +19,13 @@ export function lesson_console_log_unscramble(parent) {
         for (let token of js_tokenize(source)) {
             la(token);
         }
-    })
+    });
     let mapped = list_map(tokens, t => {
         let {type} = t;
         let {label} = type;
-        return label
-    })
+        return label;
+    });
     for (let m of mapped) {
-        app_learn_code_code_part_generic(html_span_text, parent, source_code, background_color);
-
+        app_learn_code_code_part_generic(html_span_text, parent, m, app_learn_code_code_background());
     }
 }
