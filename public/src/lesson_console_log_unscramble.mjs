1590fda4ebcbb9e121b8bfc4b25c6c098d5c34e6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 21:53:01 -0400

    2024-04-27T01:53:00.763Z  function_transform js_outside_move lesson_console_log_unscramble

--- public/src/lesson_console_log_unscramble.mjs
+++ public/src/lesson_console_log_unscramble.mjs
@@ -1,6 +1,7 @@
+import {js_tokenize} from "./js_tokenize.mjs";
 import {html_p_text} from "./html_p_text.mjs";
-import { js_parse } from "./js_parse.mjs";
 export function lesson_console_log_unscramble(parent) {
+    html_p_text(parent, 'below is another quiz');
     html_p_text(parent, 'write a program that outputs : hello');
-    js_parse('test')
+    js_tokenize('test');
 }
