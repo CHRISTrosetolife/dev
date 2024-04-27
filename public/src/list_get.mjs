f9b34bb697325f3a86eb8fed45c29bdd88ae7ee4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 07:53:52 -0400

    2024-04-27T11:53:52.247Z  function_transform js_outside_move list_get

--- public/src/list_get.mjs
+++ public/src/list_get.mjs
@@ -1,7 +1,7 @@
-import { list_index_last } from "./list_index_last.mjs";
-import { number_less_than } from "./number_less_than.mjs";
-
+import {assert} from "./assert.mjs";
+import {list_index_last} from "./list_index_last.mjs";
+import {number_less_than} from "./number_less_than.mjs";
 export function list_get(list, index) {
-    assert(number_less_than, [index, list_index_last(list)])
+    if (0) assert(number_less_than, [index, list_index_last(list)]);
     return list[index];
 }
