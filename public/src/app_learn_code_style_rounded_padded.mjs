88ea8a3c9f592ae1f537a5e638d57a435eca45db
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 22:56:55 -0400

    2024-04-27T02:56:55.432Z  noop

--- public/src/app_learn_code_style_rounded_padded.mjs
+++ public/src/app_learn_code_style_rounded_padded.mjs
@@ -1,7 +1,8 @@
 import {html_style} from "./html_style.mjs";
 export function app_learn_code_style_rounded_padded(code) {
+    let units = 2;
     html_style(code, {
-        'padding': 0.2 + 'rem',
+        'padding': (0.1 * units) + 'rem',
         'border-radius': '0.5rem'
     });
 }
