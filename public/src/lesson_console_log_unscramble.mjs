35413196cf3dffe5fd3a7c98d00520a9307aebd8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-26 21:55:06 -0400

    2024-04-27T01:55:05.922Z  noop

--- public/src/lesson_console_log_unscramble.mjs
+++ public/src/lesson_console_log_unscramble.mjs
@@ -3,5 +3,7 @@ import {html_p_text} from "./html_p_text.mjs";
 export function lesson_console_log_unscramble(parent) {
     html_p_text(parent, 'below is another quiz');
     html_p_text(parent, 'write a program that outputs : hello');
-    console.log(js_tokenize('test'));
+    for (let token of js_tokenize('test')) {
+        console.log();
+    }
 }
