d6d698e141d66302be214862b4943c79a791c498
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 07:53:59 -0400

    2024-04-27T11:53:59.415Z  noop

--- public/src/list_get.mjs
+++ public/src/list_get.mjs
@@ -2,6 +2,6 @@ import {assert} from "./assert.mjs";
 import {list_index_last} from "./list_index_last.mjs";
 import {number_less_than} from "./number_less_than.mjs";
 export function list_get(list, index) {
-    if (0) assert(number_less_than, [index, list_index_last(list)]);
+    assert(number_less_than, [index, list_index_last(list)]);
     return list[index];
 }
