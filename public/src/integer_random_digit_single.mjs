054235f84195c86ba863bdbd02ffbcc7ca74672a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 16:59:05 -0400

    2024-04-27T20:59:04.842Z  function_transform js_outside_move lesson_add_quiz

---
+++ public/src/integer_random_digit_single.mjs
@@ -0,0 +1,4 @@
+import {integer_random} from "./integer_random.mjs";
+export function integer_random_digit_single() {
+    return integer_random(-9, 9);
+}
--- public/src/lesson_add_quiz.mjs
+++ public/src/lesson_add_quiz.mjs
@@ -1,7 +1,9 @@
+import {integer_random_digit_single} from "./integer_random_digit_single.mjs";
 import {list_random_item} from "./list_random_item.mjs";
 import {random_50_50} from "./random_50_50.mjs";
 import {integer_random} from "./integer_random.mjs";
-export function lesson_log_number_quiz() {
+export function lesson_add_quiz() {
+    let a = integer_random_digit_single();
     let value = integer_random(0, 999999);
     if (random_50_50()) {
         value = value / list_random_item([10, 100, 1000]);
