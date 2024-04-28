f4869dd38b87393b02295f1dc4981da5e333fd12
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-28 15:08:15 -0400

    2024-04-28T19:08:13.883Z  function_imports_add html_merge

--- public/src/html_merge.mjs
+++ public/src/html_merge.mjs
@@ -1,8 +1,11 @@
+import {object_properties} from "./object_properties.mjs";
 import {object_merge} from "./object_merge.mjs";
+import {object_property_get} from "./object_property_get.mjs";
 export function html_merge(component, to_merge) {
     let {element} = component;
     let ps = object_properties(to_merge);
     for (let p of ps) {
-        
+        let value = object_property_get(to_merge, p);
+        element.setAttribute(p, value);
     }
 }
