e5dc7983e3c4ea4b30f68266f222d2fccc48d210
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-28 09:42:11 -0400

    2024-04-28T13:42:11.095Z  function_imports_add html_button

--- public/src/html_button.mjs
+++ public/src/html_button.mjs
@@ -1,8 +1,9 @@
+import {html_style_button_default} from "./html_style_button_default.mjs";
 import {html_style_default} from "./html_style_default.mjs";
 import {html_style} from "./html_style.mjs";
 import {html_element} from "./html_element.mjs";
 export function html_button(root) {
     let result = html_element(root, 'button');
-    html_style(result, html_style_default());
+    html_style(result, html_style_button_default());
     return result;
 }
