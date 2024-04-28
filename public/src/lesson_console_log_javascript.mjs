c2e237987886749412ea7c0184882e50b77c98d8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 21:29:49 -0400

    2024-04-28T01:29:49.334Z  noop

--- public/src/lesson_console_log_javascript.mjs
+++ public/src/lesson_console_log_javascript.mjs
@@ -19,9 +19,13 @@ import {html_style_bold} from "./html_style_bold.mjs";
 import {html_spacer_vertical} from "./html_spacer_vertical.mjs";
 export function lesson_console_log_javascript(parent) {
     html_style_alternate(parent, html_p, [noop, html_style_bold], [
-        'in computer programming different symbols can be used as a ', 'separator', ' so that the computer can tell two things apart']);
-    html_style_alternate(parent, html_p, [noop, html_style_bold], [
         'we will be learning a computer programming language called ', 'javascript']);
+    html_style_alternate(parent, html_p, [noop, html_style_bold], [
+        'in javascript , a ', 'statement', ' is analogous to a sentence in english']);
     html_style_alternate(parent, html_p, [noop, html_style_monospace], [
-        'in javascript a semicolon ( ', ';', ' ) is a separator']);
+        'at the end of a statement there should be a semicolon ( ', ';', ' )']);
+    html_style_alternate(parent, html_p, [noop, html_style_bold], [
+        'in computer programming different symbols can be used as a ', 'separator', ' so that the computer can tell two things apart']);
+    html_style_alternate(parent, html_p, [noop, html_style_monospace], [
+    'in javascript a semicolon ( ', ';', ' ) is a separator that separates statements from other statements']);
 }
