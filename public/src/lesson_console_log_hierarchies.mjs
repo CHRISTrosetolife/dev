ff24bd82fd81da6a83403821ab59843f7db2fbb8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 21:25:29 -0400

    2024-04-28T01:25:29.172Z  function_transform js_outside_move lesson_statements_two

---
+++ public/src/lesson_console_log_hierarchies.mjs
@@ -0,0 +1,16 @@
+import {html_style_bold} from "./html_style_bold.mjs";
+import {noop} from "./noop.mjs";
+import {html_p} from "./html_p.mjs";
+import {html_style_alternate} from "./html_style_alternate.mjs";
+import {html_p_text} from "./html_p_text.mjs";
+export function lesson_console_log_hierarchies(parent) {
+    html_p_text(parent, 'in english , there are letters');
+    html_p_text(parent, 'words have letters');
+    html_p_text(parent, 'phrases have words');
+    html_p_text(parent, 'sentences have phrases');
+    html_p_text(parent, 'paragraphs have sentences');
+    html_p_text(parent, 'and so on');
+    html_style_alternate(parent, html_p, [noop, html_style_bold], ['this is a ', 'hierarchy']);
+    html_style_alternate(parent, html_p, [noop, html_style_bold], ['a hierarchy can also be called a ', 'tree']);
+    html_p_text(parent, 'trees are very important in computer programming');
+}
--- public/src/lesson_statements_two.mjs
+++ public/src/lesson_statements_two.mjs
@@ -2,23 +2,6 @@ import {lesson_console_log_quiz} from "./lesson_console_log_quiz.mjs";
 import {lesson_simple} from "./lesson_simple.mjs";
 import {lesson_add_quiz} from "./lesson_add_quiz.mjs";
 import {html_p} from "./html_p.mjs";
-import {app_learn_code_unscramble} from "./app_learn_code_unscramble.mjs";
-import {lesson_log_number_quiz} from "./lesson_log_number_quiz.mjs";
-import {app_learn_code_quiz} from "./app_learn_code_quiz.mjs";
-import {lesson_log_number_example} from "./lesson_log_number_example.mjs";
-import {lesson_console_log_javascript} from "./lesson_console_log_javascript.mjs";
-import {lesson_console_log_process_named} from "./lesson_console_log_process_named.mjs";
-import {lesson_console_log_process_computer_program} from "./lesson_console_log_process_computer_program.mjs";
-import {list_add} from "./list_add.mjs";
-import {array_new} from "./array_new.mjs";
-import {object_new} from "./object_new.mjs";
-import {object_property_set} from "./object_property_set.mjs";
-import {lesson_console_log_analogy_paper} from "./lesson_console_log_analogy_paper.mjs";
-import {lesson_console_log_analogy_addition} from "./lesson_console_log_analogy_addition.mjs";
-import {lesson_console_log_program_first} from "./lesson_console_log_program_first.mjs";
-import {lesson_console_log_strings} from "./lesson_console_log_strings.mjs";
-import {lesson_console_log_analogy_addition_symbol} from "./lesson_console_log_analogy_addition_symbol.mjs";
-import {lesson_log_number_explain} from "./lesson_log_number_explain.mjs";
 import {html_style_alternate} from "./html_style_alternate.mjs";
 import {html_p_text} from "./html_p_text.mjs";
 import {noop} from "./noop.mjs";
@@ -30,17 +13,7 @@ export function lesson_statements_two() {
     return lesson_simple(() => {
         return `${lesson_console_log_quiz()}
 ${lesson_console_log_quiz()}`;
-    }, example_before, description, example_message, [parent => {
-        html_p_text(parent, 'in english , there are letters');
-        html_p_text(parent, 'words have letters');
-        html_p_text(parent, 'phrases have words');
-        html_p_text(parent, 'sentences have phrases');
-        html_p_text(parent, 'paragraphs have sentences');
-        html_p_text(parent, 'and so on');
-        html_style_alternate(parent, html_p, [noop, html_style_bold], ['this is a ', 'hierarchy']);
-        html_style_alternate(parent, html_p, [noop, html_style_bold], ['a hierarchy can also be called a ', 'tree']);
-        html_p_text(parent, 'trees are very important in computer programming');
-    }]);
+    }, example_before, description, example_message, []);
     function example_before(parent) {
         html_style_alternate(parent, html_p, [noop, html_style_bold], ['in javascript , a ', 'statement', ' is analogous to a sentence in english']);
         html_style_alternate(parent, html_p, [noop, html_style_monospace], ['at the end of a statement there should be a semicolon ( ', ';', ' )']);
