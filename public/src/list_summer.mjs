48fa2f018ad5d9989bb4d0691597e5fed65f40b0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-28 11:20:36 -0400

    2024-04-28T15:20:35.934Z  noop

--- public/src/list_summer.mjs
+++ public/src/list_summer.mjs
@@ -1,6 +1,7 @@
+import { add } from "./add.mjs";
 import {list_add} from "./list_add.mjs";
-export function list_adder(lambda) {
-    let result = [];
-    lambda(item => list_add(result, item));
+export function list_summer(lambda) {
+    let result = 0;
+    lambda(item => result = add(result, item));
     return result;
 }
