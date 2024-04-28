e1639e7b296edc75e4b9be1fb4a743c4dc1a1d82
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-28 11:38:13 -0400

    2024-04-28T15:38:13.224Z  noop

--- public/src/list_add_multiple.mjs
+++ public/src/list_add_multiple.mjs
@@ -1,6 +1,6 @@
 import {list_add} from "./list_add.mjs";
-export function list_add_multiple(list, screens) {
-    for (let s of list) {
-        list_add(screens, s);
+export function list_add_multiple(list, other) {
+    for (let s of other) {
+        list_add(list, s);
     }
 }
