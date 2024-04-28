b922fd47dad9707ea1534a6f4d122c6d6162eab6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-28 10:12:56 -0400

    2024-04-28T14:12:56.279Z  noop

--- public/src/app_learn_code_style_success.mjs
+++ public/src/app_learn_code_style_success.mjs
@@ -1,6 +1,6 @@
 import {html_style} from "./html_style.mjs";
-export function app_learn_code_style_success(button) {
-    html_style(button, {
+export function app_learn_code_style_success(component) {
+    html_style(component, {
         'background-color': 'lightgreen',
         'border-color': 'MediumSeaGreen'
     });
