8364b4e8524d1424fbcc209627e61e7cddaf73ec
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-28 11:21:03 -0400

    2024-04-28T15:21:02.922Z  function_copy list_summer list_sum

---
+++ public/src/list_sum.mjs
@@ -0,0 +1,7 @@
+import { add } from "./add.mjs";
+import {list_add} from "./list_add.mjs";
+export function list_summer(lambda) {
+    let result = 0;
+    lambda(item => result = add(result, item));
+    return result;
+}
