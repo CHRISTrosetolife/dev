8196eaf813b8ba0ed139dc4d19b48f4f17cddef0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-28 11:24:45 -0400

    2024-04-28T15:24:45.281Z  noop

--- public/src/list_sum.mjs
+++ public/src/list_sum.mjs
@@ -1,6 +1,6 @@
-import { add } from "./add.mjs";
+import {add} from "./add.mjs";
 import {list_add} from "./list_add.mjs";
-export function list_summer(lambda) {
+export function list_sum(lambda) {
     let result = 0;
     lambda(item => result = add(result, item));
     return result;
