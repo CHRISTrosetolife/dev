e84912102cc2e66d34b42a3f67bcd2050b469eb7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-28 09:26:26 -0400

    2024-04-28T13:26:26.171Z  function_imports_add lesson_add_quiz

--- public/src/lesson_add_quiz.mjs
+++ public/src/lesson_add_quiz.mjs
@@ -1,9 +1,10 @@
+import {integer_random_digit_single_positive} from "./integer_random_digit_single_positive.mjs";
 import {integer_random_digit_single} from "./integer_random_digit_single.mjs";
 import {list_random_item} from "./list_random_item.mjs";
 import {random_50_50} from "./random_50_50.mjs";
 import {integer_random} from "./integer_random.mjs";
 export function lesson_add_quiz() {
-    let a = integer_random_digit_single();
-    let b = integer_random_digit_single();
+    let a = integer_random_digit_single_positive();
+    let b = integer_random_digit_single_positive();
     return `console.log(${a} + ${b});`;
 }
