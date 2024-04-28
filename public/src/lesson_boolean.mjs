e5b7deeae026322175c5ae4ceefe275078b49bf0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-28 15:57:29 -0400

    2024-04-28T19:57:28.472Z  function_search random

--- public/src/lesson_boolean.mjs
+++ public/src/lesson_boolean.mjs
@@ -31,7 +31,9 @@ export function lesson_boolean() {
     return lesson_simple(function () {
         let a = integer_random_digit_single_positive();
         let b = integer_random_digit_single_positive();
-        return `console.log(${a} / ${b});`;
+        return `console.log();
+console.log();
+console.log();`;
     }, example_before, description, example_message, []);
     function example_before(parent) {
         html_style_alternate(parent, html_p, [noop, html_style_monospace], ['one number can be divided by the other using the forward slash ( ', '/', ' ) symbol']);
