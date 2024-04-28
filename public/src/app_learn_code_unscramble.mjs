4735bae6bf8a0771fc9d5780cb1d8051b982cfe0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-27 21:14:56 -0400

    2024-04-28T01:14:56.069Z  noop

--- public/src/app_learn_code_unscramble.mjs
+++ public/src/app_learn_code_unscramble.mjs
@@ -95,7 +95,8 @@ export function app_learn_code_unscramble(source_get) {
                 html_style(part, {
                     margin: html_style_units(3),
                     display: 'inline-block',
-                    cursor: 'pointer'
+                    cursor: 'pointer',
+                    'min-width': html_style_units(3)
                 });
                 html_on_click(part, () => {
                     let current = list_get(answer, current_index);
