6ebb941bda07289a9538c88b21f1bc6bbb0ee3d9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-28 14:31:41 -0400

    2024-04-28T18:31:40.497Z  noop

--- public/src/lesson_console_log_hierarchies.mjs
+++ public/src/lesson_console_log_hierarchies.mjs
@@ -3,6 +3,9 @@ import {noop} from "./noop.mjs";
 import {html_p} from "./html_p.mjs";
 import {html_style_alternate} from "./html_style_alternate.mjs";
 import {html_p_text} from "./html_p_text.mjs";
+import { html_element } from "./html_element.mjs";
+import { html_style_width_full } from "./html_style_width_full.mjs";
+import { html_inner_set } from "./html_inner_set.mjs";
 export async function lesson_console_log_hierarchies(parent) {
     html_p_text(parent, 'in english , there are letters');
     html_p_text(parent, 'words have letters');
@@ -14,5 +17,10 @@ export async function lesson_console_log_hierarchies(parent) {
     html_style_alternate(parent, html_p, [noop, html_style_bold], ['a hierarchy can also be called a ', 'tree']);
     html_p_text(parent, 'trees are very important in computer programming');
     html_p_text(parent, 'here is an example of a tree or hierarchy :');
+    let t = html_element(parent, 'table');
+    html_style_width_full(t);
+    let tr1 = html_element(t, 'tr');
+    let td_phrase = html_element(tr1, 'td');
+    html_inner_set(td_phrase, 'JESUS is LORD')
 
 }
