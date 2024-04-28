d885d30106a75f34323514472a5e995f691141a6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-28 13:43:09 -0400

    2024-04-28T17:43:08.697Z  noop

--- public/src/app_learn_code.mjs
+++ public/src/app_learn_code.mjs
@@ -44,6 +44,7 @@ import {add} from "./add.mjs";
 import {html_spacer_vertical} from "./html_spacer_vertical.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { html_style_bold } from "./html_style_bold.mjs";
+import { lesson_statements_three } from "./lesson_statements_three.mjs";
 export function app_learn_code() {
     let root = html_document_body();
     html_style(root, {
@@ -67,6 +68,7 @@ export function app_learn_code() {
     list_add(lesson_creates, lesson_multiply);
     list_add(lesson_creates, lesson_divide);
     list_add(lesson_creates, lesson_statements_two);
+    list_add(lesson_creates, lesson_statements_three);
     let lesson_screens = [];
     let lessons = [];
     for (let lesson_create of lesson_creates) {
--- public/src/lesson_statements_three.mjs
+++ public/src/lesson_statements_three.mjs
@@ -7,15 +7,15 @@ import {html_p_text} from "./html_p_text.mjs";
 import {noop} from "./noop.mjs";
 import {html_style_bold} from "./html_style_bold.mjs";
 import {html_style_monospace} from "./html_style_monospace.mjs";
-export function lesson_statements_two() {
-    const description = 'two console.log statements';
-    const example_message = 'with two statements';
+export function lesson_statements_three() {
+    const description = 'three console.log statements';
+    const example_message = 'with three statements';
     return lesson_simple(() => {
         return `${lesson_console_log_quiz()}
+${lesson_console_log_quiz()}
 ${lesson_console_log_quiz()}`;
     }, example_before, description, example_message, []);
     function example_before(parent) {
-        html_p_text(parent, 'statements are ran one after the other');
-        html_p_text(parent, 'if there are two statements , the first statement will run and then the second statement will run');
+        html_p_text(parent, 'if there are three statements , the first statement will run and then the second statement will run and then the third statement will run');
     }
 }
