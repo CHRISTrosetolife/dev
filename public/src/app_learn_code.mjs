5d8dc2b8ae26406b5cbbf70707753652cfbb22c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-28 11:32:16 -0400

    2024-04-28T15:32:16.118Z  function_transform js_outside_move app_learn_code

--- public/src/app_learn_code.mjs
+++ public/src/app_learn_code.mjs
@@ -1,3 +1,4 @@
+import {add_1} from "./add_1.mjs";
 import {html_style_button_default} from "./html_style_button_default.mjs";
 import {lesson_statements_two} from "./lesson_statements_two.mjs";
 import {lesson_multiply} from "./lesson_multiply.mjs";
@@ -28,15 +29,16 @@ import {html_style} from "./html_style.mjs";
 import {object_merge} from "./object_merge.mjs";
 import {list_add} from "./list_add.mjs";
 import {lesson_divide} from "./lesson_divide.mjs";
-import { app_learn_code_style_rounded_padded_style } from "./app_learn_code_style_rounded_padded_style.mjs";
-import { html_hr } from "./html_hr.mjs";
-import { string_combine } from "./string_combine.mjs";
-import { noop } from "./noop.mjs";
-import { html_p_text } from "./html_p_text.mjs";
-import { list_index } from "./list_index.mjs";
-import { list_take } from "./list_take.mjs";
-import { list_sum } from "./list_sum.mjs";
-import { each_index } from "./each_index.mjs";
+import {app_learn_code_style_rounded_padded_style} from "./app_learn_code_style_rounded_padded_style.mjs";
+import {html_hr} from "./html_hr.mjs";
+import {string_combine} from "./string_combine.mjs";
+import {noop} from "./noop.mjs";
+import {html_p_text} from "./html_p_text.mjs";
+import {list_index} from "./list_index.mjs";
+import {list_take} from "./list_take.mjs";
+import {list_sum} from "./list_sum.mjs";
+import {each_index} from "./each_index.mjs";
+import {add} from "./add.mjs";
 export function app_learn_code() {
     let root = html_document_body();
     html_style(root, {
@@ -46,12 +48,11 @@ export function app_learn_code() {
         'font-size': '3vh'
     });
     object_merge(html_style_button_default(), app_learn_code_style_rounded_padded_style());
-    object_merge(html_style_button_default(),     {
+    object_merge(html_style_button_default(), {
         'margin': '0.5vh',
         'background-color': 'lightblue',
-        'border' : 'solid 0.5vh dodgerblue'
+        'border': 'solid 0.5vh dodgerblue'
     });
-
     let lesson_creates = [];
     list_add(lesson_creates, lesson_console_log);
     list_add(lesson_creates, lesson_log_number);
@@ -70,10 +71,10 @@ export function app_learn_code() {
     }
     function refresh_main() {
         html_clear(root);
-        html_p_text(root, 'choose a lesson : ')
+        html_p_text(root, 'choose a lesson : ');
         for (let lesson of lessons) {
             let {description} = lesson;
-            html_button_width_full_text_click(root, description, () => refresh_lesson(lesson))
+            html_button_width_full_text_click(root, description, () => refresh_lesson(lesson));
         }
     }
     refresh_main();
@@ -82,18 +83,17 @@ export function app_learn_code() {
     function refresh_lesson(lesson) {
         html_clear(root);
         let lesson_index = list_index(lessons, lesson);
-        let previous = list_take(lessons, lesson_index)
+        let previous = list_take(lessons, lesson_index);
         let index_first = list_sum(s => {
             for (let p of previous) {
                 let {screens} = p;
-                s(list_length(screens))
+                s(list_length(screens));
             }
-        })
+        });
         let {screens} = lesson;
         each_index(screens, (screen, index) => {
-            html_button_width_full_text_click(root, string_combine('screen ' , add_1(index)), () => refresh_lesson(lesson))
-
-        })
+            html_button_width_full_text_click(root, string_combine('screen ', add_1(index)), () => refresh_lesson_screen(add(index_first, index)));
+        });
     }
     function refresh_lesson_screen(index) {
         html_clear(root);
@@ -102,9 +102,9 @@ export function app_learn_code() {
         });
         let lesson_screen = list_get(lesson_screens, index);
         lesson_screen(root);
-        html_hr(root)
+        html_hr(root);
         let button_next = html_button_width_full_text_click(root, 'next', function on_click() {
-            refresh_lesson_screen(add_1(index))
+            refresh_lesson_screen(add_1(index));
         });
         let button_previous = html_button_width_full_text_click(root, 'previous', function on_click() {
             refresh_lesson_screen(subtract_1(index));
