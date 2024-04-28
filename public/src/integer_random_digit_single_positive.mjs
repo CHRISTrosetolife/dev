60ea2396e14cc581aeef9a3f70b5cdc5485992cc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-28 09:24:53 -0400

    2024-04-28T13:24:53.166Z  function_imports_add integer_random_digit_single_positive

--- public/src/integer_random_digit_single_positive.mjs
+++ public/src/integer_random_digit_single_positive.mjs
@@ -1,1 +1,4 @@
-export function integer_random_digit_single_positive() {}
+import {integer_random} from "./integer_random.mjs";
+export function integer_random_digit_single_positive() {
+    return integer_random(1, 9);
+}
