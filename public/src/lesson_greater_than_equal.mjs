0180a3f5724b6eb96b7c72bbabd217a1f6c94e6f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-29 18:03:34 -0400

    2024-04-29T22:03:33.371Z  function_copy lesson_less_than_equal lesson_greater_than_equal

---
+++ public/src/lesson_greater_than_equal.mjs
@@ -0,0 +1,52 @@
+import {lesson_comparisons_source_get_get} from "./lesson_comparisons_source_get_get.mjs";
+import {integer_random_digit_single_positive} from "./integer_random_digit_single_positive.mjs";
+import {integer_random_digit_single} from "./integer_random_digit_single.mjs";
+import {lesson_simple} from "./lesson_simple.mjs";
+import {lesson_add_quiz} from "./lesson_add_quiz.mjs";
+import {html_p} from "./html_p.mjs";
+import {app_learn_code_unscramble} from "./app_learn_code_unscramble.mjs";
+import {lesson_log_number_quiz} from "./lesson_log_number_quiz.mjs";
+import {app_learn_code_quiz} from "./app_learn_code_quiz.mjs";
+import {lesson_log_number_example} from "./lesson_log_number_example.mjs";
+import {lesson_console_log_javascript} from "./lesson_console_log_javascript.mjs";
+import {lesson_console_log_process_named} from "./lesson_console_log_process_named.mjs";
+import {lesson_console_log_process_computer_program} from "./lesson_console_log_process_computer_program.mjs";
+import {list_add} from "./list_add.mjs";
+import {array_new} from "./array_new.mjs";
+import {object_new} from "./object_new.mjs";
+import {object_property_set} from "./object_property_set.mjs";
+import {lesson_console_log_analogy_paper} from "./lesson_console_log_analogy_paper.mjs";
+import {lesson_console_log_analogy_addition} from "./lesson_console_log_analogy_addition.mjs";
+import {lesson_console_log_program_first} from "./lesson_console_log_program_first.mjs";
+import {lesson_console_log_strings} from "./lesson_console_log_strings.mjs";
+import {lesson_console_log_analogy_addition_symbol} from "./lesson_console_log_analogy_addition_symbol.mjs";
+import {lesson_log_number_explain} from "./lesson_log_number_explain.mjs";
+import {html_style_alternate} from "./html_style_alternate.mjs";
+import {html_p_text} from "./html_p_text.mjs";
+import {noop} from "./noop.mjs";
+import {html_style_bold} from "./html_style_bold.mjs";
+import {html_style_monospace} from "./html_style_monospace.mjs";
+import {random_50_50} from "./random_50_50.mjs";
+import {list_map} from "./list_map.mjs";
+import {list_join} from "./list_join.mjs";
+import {integer_random} from "./integer_random.mjs";
+import {equal} from "./equal.mjs";
+import {html_ul} from "./html_ul.mjs";
+import {html_li_text} from "./html_li_text.mjs";
+export function lesson_less_than_equal() {
+    const description = 'less than or equal to ( <= )';
+    const example_message = 'outputting whether or not a number is greater than another number';
+    return lesson_simple(lesson_comparisons_source_get_get('<='), example_before, description, example_message, [screen_before1]);
+    function screen_before1(parent) {
+        html_p_text(parent, '"less than or equal to" is almost the same as "less than"');
+        html_p_text(parent, 'the only difference is when the two numbers being compared are the same');
+        html_p_text(parent, 'if "a" and "b" are equal then "a" is less than or equal to "b"');
+        html_p_text(parent, 'whereas if "a" and "b" are equal then "a" is not less than "b"');
+        html_p_text(parent, 'here are some examples :');
+        let ul1 = html_ul(parent);
+        html_li_text(ul1, '3 <= 5 is true');
+        html_li_text(ul1, '6 <= 4 is false');
+        html_li_text(ul1, '2 <= 2 is true');
+    }
+    function example_before(parent) {}
+}
