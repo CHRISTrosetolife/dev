6142660a497ead395beb9587d1d9a13f921c5979
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-29 15:57:49 -0400

    2024-04-29T19:57:48.447Z  function_transform js_outside_move html_style_button_default_border

--- public/src/html_style_button_default_border.mjs
+++ public/src/html_style_button_default_border.mjs
@@ -1,11 +1,7 @@
-import { string_combine } from "./string_combine.mjs";
-
+import {html_style_button_default_border_color} from "./html_style_button_default_border_color.mjs";
+import {string_combine} from "./string_combine.mjs";
 export function html_style_button_default_border() {
     return {
         'border': string_combine('solid 0.5vh ', html_style_button_default_border_color())
     };
 }
-function html_style_button_default_border_color() {
-    return 'dodgerblue';
-}
-
---
+++ public/src/html_style_button_default_border_color.mjs
@@ -0,0 +1,3 @@
+export function html_style_button_default_border_color() {
+    return 'dodgerblue';
+}
