fca98b046049fba19dfc05035097e18d72b34e98
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-29 15:57:41 -0400

    2024-04-29T19:57:40.653Z  noop

--- public/src/html_style_button_default_border.mjs
+++ public/src/html_style_button_default_border.mjs
@@ -2,6 +2,10 @@ import { string_combine } from "./string_combine.mjs";
 
 export function html_style_button_default_border() {
     return {
-        'border': string_combine('solid 0.5vh ', 'dodgerblue')
+        'border': string_combine('solid 0.5vh ', html_style_button_default_border_color())
     };
 }
+function html_style_button_default_border_color() {
+    return 'dodgerblue';
+}
+
