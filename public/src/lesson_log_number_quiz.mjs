681cef4fb758f998af1b9ae67adb1bfc8d98101c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-29 18:31:00 -0400

    2024-04-29T22:30:59.662Z  function_transform js_outside_move lesson_log_number_quiz

--- public/src/lesson_log_number_quiz.mjs
+++ public/src/lesson_log_number_quiz.mjs
@@ -1,13 +1,8 @@
+import {lesson_log_number_quiz_value} from "./lesson_log_number_quiz_value.mjs";
 import {list_random_item} from "./list_random_item.mjs";
 import {random_50_50} from "./random_50_50.mjs";
 import {integer_random} from "./integer_random.mjs";
 export function lesson_log_number_quiz() {
-    let value = integer_random(0, 999999);
-    if (random_50_50()) {
-        value = value / list_random_item([10, 100, 1000]);
-    }
-    if (random_50_50()) {
-        value = value * -1;
-    }
+    let value = lesson_log_number_quiz_value();
     return `console.log(${value});`;
 }
