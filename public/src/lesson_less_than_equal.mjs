713de8423c817796dfd5d7e28fa29090d18d9ad0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-29 17:44:48 -0400

    2024-04-29T21:44:48.096Z  noop

--- public/src/lesson_less_than_equal.mjs
+++ public/src/lesson_less_than_equal.mjs
@@ -38,11 +38,11 @@ export function lesson_less_than_equal() {
     const example_message = 'outputting whether or not a number is greater than another number';
     return lesson_simple(lesson_comparisons_source_get_get('<='), example_before, description, example_message, [screen_before1]);
     function screen_before1(parent) {
-        html_text(parent, '"less than or equal to" is almost the same as "less than"');
-        html_text(parent, 'the only difference is when the two numbers being compared are the same');
-        html_text(parent, 'if "a" and "b" are equal then "a" is less than or equal to "b"');
-        html_text(parent, 'whereas if "a" and "b" are equal then "a" is not less than "b"');
-        html_text(parent, 'here are some examples :');
+        html_p_text(parent, '"less than or equal to" is almost the same as "less than"');
+        html_p_text(parent, 'the only difference is when the two numbers being compared are the same');
+        html_p_text(parent, 'if "a" and "b" are equal then "a" is less than or equal to "b"');
+        html_p_text(parent, 'whereas if "a" and "b" are equal then "a" is not less than "b"');
+        html_p_text(parent, 'here are some examples :');
         let ul1 = html_ul(parent);
         html_li_text(ul1, '3 <= 5 is true');
         html_li_text(ul1, '6 <= 4 is false');
