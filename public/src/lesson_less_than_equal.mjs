284390d23d5b8b224e42952f9dbd6742450d7179
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-29 17:38:52 -0400

    2024-04-29T21:38:52.254Z  noop

--- public/src/lesson_less_than_equal.mjs
+++ public/src/lesson_less_than_equal.mjs
@@ -33,19 +33,19 @@ import {integer_random} from "./integer_random.mjs";
 import {equal} from "./equal.mjs";
 import {html_ul} from "./html_ul.mjs";
 import {html_li_text} from "./html_li_text.mjs";
-export function lesson_greater_than() {
-    const description = 'greater than ( > )';
+export function lesson_less_than_equal() {
+    const description = 'less than or equal to ( <= )';
     const example_message = 'outputting whether or not a number is greater than another number';
-    return lesson_simple(lesson_comparisons_source_get_get('>'), example_before, description, example_message, [screen_before1]);
+    return lesson_simple(lesson_comparisons_source_get_get('<='), example_before, description, example_message, [screen_before1]);
     function screen_before1(parent) {
         html_style_alternate(parent, html_p, [noop, html_style_bold], ['if "a" and "b" are numbers and "a" is less than "b" then "b" is ', 'greater than', ' "a"']);
         html_p_text(parent, 'like less than , if "a" and "b" are equal to each other , then "a" is not greater than "b"');
         html_p_text(parent, 'we can write "a" is greater than "b" as : a > b');
         html_p_text(parent, 'here are some examples :');
         let ul = html_ul(parent);
-        html_li_text(ul, '6 > 4 is true');
-        html_li_text(ul, '3 > 5 is false');
-        html_li_text(ul, '2 > 2 is false');
+        html_li_text(ul, '3 <= 5 is true');
+        html_li_text(ul, '6 <= 4 is false');
+        html_li_text(ul, '2 <= 2 is true');
     }
     function example_before(parent) {}
 }
