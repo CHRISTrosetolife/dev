da395a9e4894c34418eeba42c5cd6b377751e914
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-29 18:33:30 -0400

    2024-04-29T22:33:29.727Z  function_transform js_outside_move lesson_console_log_quiz

--- public/src/lesson_console_log_quiz.mjs
+++ public/src/lesson_console_log_quiz.mjs
@@ -1,3 +1,4 @@
+import {lesson_console_log_quiz_value} from "./lesson_console_log_quiz_value.mjs";
 import {list_join} from "./list_join.mjs";
 import {subtract} from "./subtract.mjs";
 import {list_slice} from "./list_slice.mjs";
@@ -6,15 +7,7 @@ import {integer_random} from "./integer_random.mjs";
 import {list_length} from "./list_length.mjs";
 import {string_split_space} from "./string_split_space.mjs";
 export function lesson_console_log_quiz() {
-    let sentence = 'for GOD so loved the world that HE gave HIS only SON that whoever believes in HIM shall not perish but have eternal life';
-    let words = string_split_space(sentence);
-    let word_count = integer_random(2, 4);
-    let length = list_length(words);
-    let high = subtract(length, word_count);
-    let index = integer_random(0, high);
-    let end = add(index, word_count);
-    let sliced = list_slice(words, index, end);
-    let message = list_join(sliced, ' ');
+    let message = lesson_console_log_quiz_value();
     let code = `console.log('${message}');`;
     return code;
 }
