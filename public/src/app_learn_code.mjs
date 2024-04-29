2f018fe6127e8c9e2e470b17fa0591139adc7b9e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-29 15:16:58 -0400

    2024-04-29T19:16:56.038Z  noop

--- public/src/app_learn_code.mjs
+++ public/src/app_learn_code.mjs
@@ -85,7 +85,7 @@ export function app_learn_code() {
         window.scrollTo({
             top: 0
         });
-        let settings = html_button_width_full_text_click(root, 'settings', () => {});
+        let settings = html_button_width_full_text_click(root, '⚙️ settings', () => {});
         html_p_text(root, 'if you want to start from beginning , choose "begin" : ');
         let begin = html_button_width_full_text_click(root, 'begin', async () => await refresh_lesson_screen(0));
         html_style(begin, {
