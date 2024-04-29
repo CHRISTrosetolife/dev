e2508698fa7bddbfefa1663271dfa2b7bb2e407e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-29 18:25:58 -0400

    2024-04-29T22:25:58.125Z  noop

--- public/src/lesson_equal.mjs
+++ public/src/lesson_equal.mjs
@@ -57,6 +57,6 @@ export function lesson_equal() {
     }
     function example_before(parent) {
         html_style_alternate(parent, html_p, [noop, html_style_monospace], 
-        ['in javascript to check if two number are equal or not , instead of using one equal sign ( ', '=', ' ) use three equal signs ( ', '===', ' )']);
+        ['in javascript to check if two numbers are equal or not , instead of using one equal sign ( ', '=', ' ) use three equal signs ( ', '===', ' )']);
     }
 }
