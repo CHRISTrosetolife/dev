6f483141e97d3374c7bc5ca63316a2ac7d2361aa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-29 16:25:36 -0400

    2024-04-29T20:25:35.698Z  noop

--- public/src/app_learn_code.mjs
+++ public/src/app_learn_code.mjs
@@ -61,8 +61,14 @@ import {string_length} from "./string_length.mjs";
 import { string_split } from "./string_split.mjs";
 export function app_learn_code() {
     let hash = window.location.hash;
+    if (greater_than(a, b)) {
+        
+    }
     let hash_no_tag = string_prefix_without(hash, '#')
     let split = string_split(hash, '+')
+    for (let s of split) {
+        console.log({s})
+    }
     let root = html_document_body();
     html_style(root, {
         'font-family': 'sans-serif'
@@ -206,3 +212,7 @@ export function app_learn_code() {
         });
     }
 }
+function greater_than(a, b) {
+    return a > b;
+}
+
