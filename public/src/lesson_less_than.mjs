9a4ff530f10010060eedab2cf82e6d423a05be1d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-28 22:21:07 -0400

    2024-04-29T02:21:06.638Z  noop

--- public/src/lesson_less_than.mjs
+++ public/src/lesson_less_than.mjs
@@ -30,16 +30,34 @@ import {list_map} from "./list_map.mjs";
 import {list_join} from "./list_join.mjs";
 import { integer_random } from "./integer_random.mjs";
 import { equal } from "./equal.mjs";
+import { html_ul } from "./html_ul.mjs";
+import { html_li_text } from "./html_li_text.mjs";
 export function lesson_less_than() {
     const description = 'less than ( < )';
     const example_message = 'outputting whether or not a number is less than another number';
     return lesson_simple(source_get, example_before, description, example_message, 
-        [screen_before]);
-    function screen_before(parent) {
+        [screen_before1,screen_before2,screen_before3]);
+    function screen_before1(parent) {
         html_p_text(parent, 
             'if "a" and "b" are numbers , then either "a" is less than "b" or "a" is not less than "b"');
         html_p_text(parent, 'we can write "a" is less than "b" as : a < b');
         html_p_text(parent, 'either a < b is true or a < b is false');
+        html_p_text(parent, 'for example :');
+        let ul = html_ul(parent)
+        html_li_text(parent, '1 < 2 is true');
+        html_li_text(parent, '3 < 5 is true');
+        html_li_text(parent, '6 < 4 is false');
+    }
+    function screen_before2(parent) {
+        html_p_text(parent, 
+            'if "a" and "b" are equal to each other , then "a" is not less than "b"');
+        html_p_text(parent, 'for example :');
+        let ul = html_ul(parent)
+        html_li_text(ul, '8 < 8 is false');
+        html_li_text(ul, '0 < 0 is false');
+        html_li_text(ul, '-2 < -2 is false');
+    }
+    function screen_before3(parent) {
         html_style_alternate(parent, html_p, [noop, html_style_monospace], 
             ['in javascript , to check if a number is less than another number or not , use the less than ( ', '<', ' ) sign']);
         html_style_alternate(parent, html_p, [noop, html_style_monospace], 
