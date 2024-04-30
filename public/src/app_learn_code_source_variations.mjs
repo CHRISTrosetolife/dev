b5d3a33ff6cc220d231e0e61586f77bdcdf55f64
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 16:41:40 -0400

    2024-04-30T20:41:38.738Z  noop

--- public/src/app_learn_code_source_variations.mjs
+++ public/src/app_learn_code_source_variations.mjs
@@ -3,6 +3,7 @@ import { equal } from "./equal.mjs";
 import {js_node_type} from "./js_node_type.mjs";
 import {js_parse} from "./js_parse.mjs";
 import { lesson_less_than_equal } from "./lesson_less_than_equal.mjs";
+import { list_first } from "./list_first.mjs";
 import { list_includes } from "./list_includes.mjs";
 import { list_length } from "./list_length.mjs";
 import { list_map } from "./list_map.mjs";
@@ -20,4 +21,5 @@ export function app_learn_code_source_variations(source) {
     }
     // code needs modification to exceed limit
     assert(lesson_less_than_equal, [length, 1])
+    let first = list_first(filtered)
 }
