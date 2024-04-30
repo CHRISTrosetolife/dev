6be65ba141d3a70512680281f7ec8b6a9a540a5d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 17:09:46 -0400

    2024-04-30T21:09:46.466Z  noop

--- public/src/js_parse.mjs
+++ public/src/js_parse.mjs
@@ -1,15 +1,10 @@
 import {js_parse_options} from "./js_parse_options.mjs";
-let a = await import_or_get('acorn', 'acorn');
-async function import_or_get(library_name_to_import, variable_name_to_get) {
-    let a;
-    if (typeof window === 'undefined') {
-        a = await import(library_name_to_import);
-    } else {
-        a = eval(variable_name_to_get);
-    }
-    return a;
+let a;
+if (typeof window === 'undefined') {
+    a = await import('acorn')
+} else {
+    a = acorn;
 }
-
 export function js_parse(code) {
     return a.parse(code, js_parse_options());
 }
