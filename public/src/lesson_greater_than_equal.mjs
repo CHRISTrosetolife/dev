90f055952df4f7e9dd0c3d96677e4b2d6faeabb0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-29 22:19:32 -0400

    2024-04-30T02:19:32.398Z  noop

--- public/src/lesson_greater_than_equal.mjs
+++ public/src/lesson_greater_than_equal.mjs
@@ -38,7 +38,7 @@ export function lesson_greater_than_equal() {
     const example_message = 'outputting whether or not a number is greater than or equal to another number';
     return lesson_simple(lesson_comparisons_source_get_get('<='), example_before, description, example_message, [screen_before1]);
     function screen_before1(parent) {
-        html_p_text(parent, 'as "less than or equal to" was for "less than" so "greater than or equal to" is for "greater than"');
+        html_p_text(parent, 'as "less than or equal to" ( ≤ ) was for "less than" ( < ) so "greater than or equal to" ( ≥ ) is for "greater than" ( > )');
         html_p_text(parent, 'here are some examples :');
         let ul1 = html_ul(parent);
         html_li_text(ul1, '3 ≥ 5 is false');
