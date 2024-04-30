f76de848d76c9b809bb98c1497d8ba1e6fabbc8b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 17:06:09 -0400

    2024-04-30T21:06:08.854Z  noop

--- public/src/js_parse.mjs
+++ public/src/js_parse.mjs
@@ -1,7 +1,8 @@
 import {js_parse_options} from "./js_parse_options.mjs";
 let a;
 if (typeof window === 'undefined') {
-    a = await import('acorn')
+    const library_name = 'acorn';
+    a = await import(library_name)
 } else {
     a = acorn;
 }
