244c5c0e7ecef58d1f0e6b9b4acdd9bc59437109
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 16:37:05 -0400

    2024-04-30T20:37:05.154Z  noop

--- public/src/app_learn_code_source_variations.mjs
+++ public/src/app_learn_code_source_variations.mjs
@@ -3,6 +3,7 @@ import {js_node_type} from "./js_node_type.mjs";
 import {js_parse} from "./js_parse.mjs";
 import { lesson_less_than_equal } from "./lesson_less_than_equal.mjs";
 import { list_includes } from "./list_includes.mjs";
+import { list_length } from "./list_length.mjs";
 import { list_map } from "./list_map.mjs";
 export function app_learn_code_source_variations(source) {
     let parsed = js_parse(source);
@@ -12,7 +13,7 @@ export function app_learn_code_source_variations(source) {
         let {operator} = be;
         return list_includes(operators, operator)
     })
-    
+    let length = list_length(filtered);
     // needs modification to exceed limit
     assert(lesson_less_than_equal, [length, 1])
 }
