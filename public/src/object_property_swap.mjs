51b647c0e4f76f3d416ca095782623cf0ab672fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 16:45:26 -0400

    2024-04-30T20:45:26.250Z  noop

--- public/src/object_property_swap.mjs
+++ public/src/object_property_swap.mjs
@@ -1,4 +1,5 @@
 import {object_property_get} from "./object_property_get.mjs";
 export function object_property_swap(object, property_a, property_b) {
     let temp = object_property_get(object, property_a);
+    let value_b = object_property_get(object, property_b);
 }
