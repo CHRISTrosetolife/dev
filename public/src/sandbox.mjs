c30cb8e7258a14aa78c854ff3d1e10573bf6793e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 16:18:02 -0400

    2024-04-30T20:18:02.028Z  noop

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,7 +1,4 @@
 import {object_property_get} from "./object_property_get.mjs";
 export function sandbox() {
-    let v1 = b;
-    let c = object_property_get(v1, 'a');
-    a();
-    ;
+    return a + b;
 }
