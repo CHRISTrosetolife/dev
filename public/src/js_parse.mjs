e8cae83c1ab3c38c44fed2dadedcadaafa4da076
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 16:31:04 -0400

    2024-04-30T20:31:04.395Z  noop

--- public/src/js_parse.mjs
+++ public/src/js_parse.mjs
@@ -1,8 +1,8 @@
 import {js_parse_options} from "./js_parse_options.mjs";
 let acorn_imported;
 if (undefined_is(window)) {
-    acorn = await import('acorn')
+    acorn_imported = await import('acorn')
 }
 export function js_parse(code) {
-    return acorn.parse(code, js_parse_options());
+    return (acorn || acorn_imported).parse(code, js_parse_options());
 }
