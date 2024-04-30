cac0b81832331c8a9f35c098fce35475ffdd3fce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 16:40:26 -0400

    2024-04-30T20:40:25.530Z  function_transform js_imports_add json_copy

--- public/src/json_copy.mjs
+++ public/src/json_copy.mjs
@@ -1,1 +1,6 @@
-export function json_copy(input) {}
+import {json_from} from "./json_from.mjs";
+import {json_to} from "./json_to.mjs";
+export function json_copy(input) {
+    let json = json_to(input);
+    return json_from(json);
+}
