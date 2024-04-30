1a237633bc7b0439e9681b7df44b78400f9ffdbf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 17:15:28 -0400

    2024-04-30T21:15:28.247Z  function_transform js_imports_add js_unparse

--- public/src/js_unparse.mjs
+++ public/src/js_unparse.mjs
@@ -1,13 +1,5 @@
 import {import_multi} from "./import_multi.mjs";
-let a;
-if (typeof window === 'undefined') {
-    a = await import('astring');
-} else {
-    a = astring;
-}
-if (false) {
-    let a = await import_multi('astring', 'astring');
-}
+let a = await import_multi('astring', 'astring');
 let {generate} = a;
 export function js_unparse(ast) {
     return generate(ast, {
