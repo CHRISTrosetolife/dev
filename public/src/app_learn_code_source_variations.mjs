462537b82e75e580459790b63e07f4edab24ebae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 16:33:34 -0400

    2024-04-30T20:33:33.223Z  function_transform js_imports_add app_learn_code_source_variations

--- public/src/app_learn_code_source_variations.mjs
+++ public/src/app_learn_code_source_variations.mjs
@@ -1,1 +1,4 @@
-export function app_learn_code_source_variations(source) {}
+import {js_parse} from "./js_parse.mjs";
+export function app_learn_code_source_variations(source) {
+    let parsed = js_parse(source);
+}
