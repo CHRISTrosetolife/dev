0800db71d0bc3a9bba058a79e288eefa26643f28
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 18:00:18 -0400

    2024-04-30T22:00:18.393Z  noop

--- public/src/list_equal.mjs
+++ public/src/list_equal.mjs
@@ -1,9 +1,13 @@
 import {equal} from "./equal.mjs";
 import {list_length} from "./list_length.mjs";
+import { range } from "./range.mjs";
 export function list_equal(a, b) {
     let a_length = list_length(a);
     let b_length = list_length(b);
     if (!equal(a_length, b_length)) {
         return false;
+    }
+    for (let i of range(a_length)) {
+        
     }
 }
