28cee2b02a05b9d9bfa98ef524e99a4e3bf5f2ed
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 17:14:01 -0400

    2024-04-30T21:13:59.586Z  function_transform js_outside_move js_parse

--- public/src/js_parse.mjs
+++ public/src/js_parse.mjs
@@ -1,10 +1,6 @@
+import {import_multi} from "./import_multi.mjs";
 import {js_parse_options} from "./js_parse_options.mjs";
-let a;
-if (typeof window === 'undefined') {
-    a = await import('acorn');
-} else {
-    a = acorn;
-}
+let a = await import_multi('acorn', 'acorn');
 export function js_parse(code) {
     return a.parse(code, js_parse_options());
 }
