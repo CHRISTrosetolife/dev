cc8642299f848c068081b3bd67ae2ae595ca7433
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 16:30:21 -0400

    2024-04-30T20:30:20.748Z  function_search undefined

--- public/src/js_parse.mjs
+++ public/src/js_parse.mjs
@@ -1,5 +1,8 @@
 import {js_parse_options} from "./js_parse_options.mjs";
-let acorn = await import('acorn')
+let acorn_imported;
+if (undefined_is(window)) {
+    acorn = await import('acorn')
+}
 export function js_parse(code) {
     return acorn.parse(code, js_parse_options());
 }
