63472143c6fe565b1a58345fded757c2430e266f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 17:04:04 -0400

    2024-04-30T21:04:03.704Z  noop

--- public/src/js_unparse.mjs
+++ public/src/js_unparse.mjs
@@ -1,9 +1,11 @@
 let a;
 if (typeof window === 'undefined') {
     a = await import('astring')
+} else {
+    a = astring;
 }
+let {generate} = a;
 export function js_unparse(ast) {
-    let {generate} = a;
     return generate(ast, {
         indent: '    '
     });
