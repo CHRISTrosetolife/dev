7a16e8e1a8b7b01dae1f04874783b1d139532b2a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 16:59:37 -0400

    2024-04-30T20:59:36.606Z  function_transform js_imports_add app_learn_code_source_variations

--- public/src/app_learn_code_source_variations.mjs
+++ public/src/app_learn_code_source_variations.mjs
@@ -1,26 +1,24 @@
-import { assert } from "./assert.mjs";
-import { equal } from "./equal.mjs";
+import {assert} from "./assert.mjs";
+import {equal} from "./equal.mjs";
 import {js_node_type} from "./js_node_type.mjs";
 import {js_parse} from "./js_parse.mjs";
-import { lesson_less_than_equal } from "./lesson_less_than_equal.mjs";
-import { list_first } from "./list_first.mjs";
-import { list_includes } from "./list_includes.mjs";
-import { list_length } from "./list_length.mjs";
-import { list_map } from "./list_map.mjs";
+import {lesson_less_than_equal} from "./lesson_less_than_equal.mjs";
+import {list_first} from "./list_first.mjs";
+import {list_includes} from "./list_includes.mjs";
+import {list_length} from "./list_length.mjs";
+import {list_map} from "./list_map.mjs";
 export function app_learn_code_source_variations(source) {
     let parsed = js_parse(source);
     let bes = js_node_type(parsed, 'BinaryExpression');
-    let operators = ['+']
+    let operators = ['+'];
     let filtered = list_map(bes, be => {
         let {operator} = be;
-        return list_includes(operators, operator)
-    })
+        return list_includes(operators, operator);
+    });
     let length = list_length(filtered);
     if (equal(length, 0)) {
-        return [source]
+        return [source];
     }
-    // code needs modification to exceed limit
-    assert(lesson_less_than_equal, [length, 1])
-    let first = list_first(filtered)
-    
+    assert(lesson_less_than_equal, [length, 1]);
+    let first = list_first(filtered);
 }
