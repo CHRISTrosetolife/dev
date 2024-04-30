2668263392e32dc9b0deb3de1e16d45a3ca9c6ae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 18:00:05 -0400

    2024-04-30T22:00:03.902Z  function_transform js_outside_move list_equal

--- public/src/list_equal.mjs
+++ public/src/list_equal.mjs
@@ -1,5 +1,9 @@
+import {equal} from "./equal.mjs";
 import {list_length} from "./list_length.mjs";
 export function list_equal(a, b) {
     let a_length = list_length(a);
     let b_length = list_length(b);
+    if (!equal(a_length, b_length)) {
+        return false;
+    }
 }
