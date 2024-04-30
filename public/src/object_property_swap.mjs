1272b6321244722663b882c6b068d84df150b901
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 16:45:01 -0400

    2024-04-30T20:44:59.939Z  function_transform js_imports_add object_property_swap

--- public/src/object_property_swap.mjs
+++ public/src/object_property_swap.mjs
@@ -1,1 +1,4 @@
-export function object_property_swap(object, property_a, property_b) {}
+import {object_property_get} from "./object_property_get.mjs";
+export function object_property_swap(object, property_a, property_b) {
+    let temp = object_property_get(object, property_a);
+}
