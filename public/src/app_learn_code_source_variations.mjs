ed52e69d4af80659aae08e4612fa541adcc54ebb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-04-30 16:34:08 -0400

    2024-04-30T20:34:07.463Z  app_learn_code_source_variations a+b

--- public/src/app_learn_code_source_variations.mjs
+++ public/src/app_learn_code_source_variations.mjs
@@ -2,5 +2,5 @@ import {js_node_type} from "./js_node_type.mjs";
 import {js_parse} from "./js_parse.mjs";
 export function app_learn_code_source_variations(source) {
     let parsed = js_parse(source);
-    js_node_type(parsed);
+    return js_node_type(parsed, 'BinaryExpression');
 }
