7f25186972fb77faf4abb95c04f0f2dd4437eaa7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 16:20:14 -0400

    2024-05-01T20:20:12.548Z  function_transform js_outside_move app_memorize

--- public/src/app_memorize.mjs
+++ public/src/app_memorize.mjs
@@ -1,12 +1,10 @@
+import {storage_url} from "./storage_url.mjs";
 import {http_get} from "./http_get.mjs";
 import {html_document_body} from "./html_document_body.mjs";
 export async function app_memorize() {
     let root = html_document_body();
     let data = await http_get(storage_url(`bible/engbsb/JHN19.json`));
-    console.log({data})
+    console.log({
+        data
+    });
 }
-function storage_url(file_path) {
-    let replaced = string_replace(file_path, '/', '%2F')
-    return `https://firebasestorage.googleapis.com/v0/b/letjesusbeexalted.appspot.com/o/${}?alt=media/`;
-}
-
