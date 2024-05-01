6f87025fa553b4febcd4074882023af474173b6a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 13:45:54 -0400

    2024-05-01T17:45:53.021Z  noop

--- public/src/file_write_json.mjs
+++ public/src/file_write_json.mjs
@@ -3,5 +3,4 @@ import {json_to} from "./json_to.mjs";
 export async function file_write_json(file_path, object) {
     let json = json_to(object);
     await file_write(file_path, json);
-    return json;
 }
