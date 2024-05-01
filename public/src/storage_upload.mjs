3e3edd1d3347a539f21d4ad188bdf7bdb3f298c2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 13:12:04 -0400

    2024-05-01T17:12:03.350Z  noop

--- public/src/storage_upload.mjs
+++ public/src/storage_upload.mjs
@@ -1,11 +1,11 @@
 import {file_read_json} from "./file_read_json.mjs";
 import admin from "firebase-admin";
+let service_account = await file_read_json('../firebase.json');
+admin.initializeApp({
+    credential: admin.credential.cert(service_account),
+    storageBucket: "gs://letJESUSbeexalted.appspot.com"
+});
+let bucket = admin.storage().bucket();
 export async function storage_upload(file_path) {
-    let service_account = await file_read_json('../firebase.json');
-    admin.initializeApp({
-        credential: admin.credential.cert(service_account),
-        storageBucket: "gs://letJESUSbeexalted.appspot.com"
-    });
-    let bucket = admin.storage().bucket();
-    bucket.upload(file_path);
+    await bucket.upload(file_path);
 }
