bc508c24a33bab7619ee5486c4e31f707de737f4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 16:18:53 -0400

    2024-05-01T20:18:52.352Z  noop

--- public/src/app_memorize.mjs
+++ public/src/app_memorize.mjs
@@ -2,6 +2,10 @@ import {http_get} from "./http_get.mjs";
 import {html_document_body} from "./html_document_body.mjs";
 export async function app_memorize() {
     let root = html_document_body();
-    let data = await http_get('https://firebasestorage.googleapis.com/v0/b/letjesusbeexalted.appspot.com/o/bible%2Fengbsb%2FJHN19.json?alt=media');
+    let data = await http_get(storage_url());
     console.log({data})
 }
+function storage_url() {
+    return 'https://firebasestorage.googleapis.com/v0/b/letjesusbeexalted.appspot.com/o/bible%2Fengbsb%2FJHN19.json?alt=media';
+}
+
