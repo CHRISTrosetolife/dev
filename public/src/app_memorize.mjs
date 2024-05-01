839b1d12c1f1cac766ef04cfc8a2fcf58b909193
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 16:23:19 -0400

    2024-05-01T20:23:18.353Z  function_transform js_outside_move app_memorize

--- public/src/app_memorize.mjs
+++ public/src/app_memorize.mjs
@@ -1,9 +1,11 @@
+import {bible_engbsb_storage_path_file} from "./bible_engbsb_storage_path_file.mjs";
 import {storage_url} from "./storage_url.mjs";
 import {http_get} from "./http_get.mjs";
 import {html_document_body} from "./html_document_body.mjs";
 export async function app_memorize() {
     let root = html_document_body();
-    let data = await http_get(storage_url(`bible/engbsb/JHN19.json`));
+    let file_path = bible_engbsb_storage_path_file('JHN19');
+    let data = await http_get(storage_url(file_path));
     console.log({
         data
     });
