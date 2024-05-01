079227522bd64d45c338c654163b84a7769cdc65
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 16:04:32 -0400

    2024-05-01T20:04:31.035Z  noop

--- public/src/http_get.mjs
+++ public/src/http_get.mjs
@@ -1,6 +1,6 @@
 import {equal} from "./equal.mjs";
 import {assert} from "./assert.mjs";
-export function http_get(url) {
+export async function http_get(url) {
     let result = await axios.get(url);
     let {status, data} = result;
     assert(equal, [status, 200]);
