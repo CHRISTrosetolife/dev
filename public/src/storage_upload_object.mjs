5805214e079b1b08e3f048bc393fecc59207928d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 13:52:49 -0400

    2024-05-01T17:52:48.514Z  noop

--- public/src/storage_upload_object.mjs
+++ public/src/storage_upload_object.mjs
@@ -1,9 +1,8 @@
 import {uuid} from "./uuid.mjs";
 import {file_write_json} from "./file_write_json.mjs";
 import {file_name_json} from "./file_name_json.mjs";
-import crypto from 'crypto';
 export async function storage_upload_object(object, destination) {
-    let uuid = uuid();
-    let file_name = file_name_json(uuid);
+    let u = uuid();
+    let file_name = file_name_json(u);
     await file_write_json(file_path, object);
 }
