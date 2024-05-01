e1f90dc1a143062fb5c4f0dd2d844947c5cacc08
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 17:33:21 -0400

    2024-05-01T21:33:20.079Z  noop

--- public/src/app_memorize.mjs
+++ public/src/app_memorize.mjs
@@ -9,6 +9,7 @@ import {html_span} from "./html_span.mjs";
 import {html_span_text} from "./html_span_text.mjs";
 import {html_strong_text} from "./html_strong_text.mjs";
 import {html_style_sans_serif} from "./html_style_sans_serif.mjs";
+import { each_index } from "./each_index.mjs";
 export async function app_memorize() {
     let root = html_document_body();
     html_style_sans_serif(root);
@@ -17,16 +18,18 @@ export async function app_memorize() {
     let verses = await http_get(storage_url(file_path));
     let verse_index = 0;
     let word_index = 0;
-    for (let verse of verses) {
-        let section = html_element(root, 'section');
-        let {tokens, verse_number} = verse;
-        html_strong_text(section, verse_number);
-        for (let token of tokens) {
-            html_span_text(section, ' ');
-            let token_element = html_span_text(section, token);
-            if (and(token, verse))
+    each_index(verses, (verse, i) => {
+        for (let verse of verses) {
+            let section = html_element(root, 'section');
+            let {tokens, verse_number} = verse;
+            html_strong_text(section, verse_number);
+            for (let token of tokens) {
+                html_span_text(section, ' ');
+                let token_element = html_span_text(section, token);
+                and(equal(), verse);
+            }
         }
-    }
+    })
 }
 function and(token, verse) {
     return token && verse;
