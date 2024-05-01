db988b0e3c4f4299835f29fe877e5e467755f229
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 13:09:31 -0400

    2024-05-01T17:09:29.960Z  function_transform js_outside_move storage_upload

--- public/src/storage_upload.mjs
+++ public/src/storage_upload.mjs
@@ -1,7 +1,8 @@
+import {file_read_json} from "./file_read_json.mjs";
 import {file_read} from "./file_read.mjs";
 import admin from "firebase-admin";
 export async function storage_upload(file_path) {
-    await file_read('../firebase.json');
+    let service_account = await file_read_json('../firebase.json');
     var serviceAccount = require("path/to/serviceAccountKey.json");
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount)
