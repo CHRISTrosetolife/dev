9672dbd0ff559dce0fc991808f1e72e5274fdf08
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 17:36:46 -0400

    2024-05-01T21:36:44.057Z  noop

--- public/src/app_learn_code_code_container.mjs
+++ public/src/app_learn_code_code_container.mjs
@@ -5,6 +5,6 @@ import {html_div} from "./html_div.mjs";
 export function app_learn_code_code_container(parent) {
     let container = html_div(parent);
     app_learn_code_style_rounded_padded(container);
-    html_style_background_color(container);
+    html_style_background_color(container, 'lightgray');
     return container;
 }
