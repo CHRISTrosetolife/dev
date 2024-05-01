583288e56ee0d5c2940c474d41abe5cf05b9aa0c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 15:37:03 -0400

    2024-05-01T19:37:03.080Z  noop

--- public/src/bible_engbsb_chapter_upload.mjs
+++ public/src/bible_engbsb_chapter_upload.mjs
@@ -1,6 +1,10 @@
 import {storage_upload_object} from "./storage_upload_object.mjs";
 import {bible_engbsb_chapter} from "./bible_engbsb_chapter.mjs";
+import { string_combine } from "./string_combine.mjs";
+import { file_name_json } from "./file_name_json.mjs";
 export async function bible_engbsb_chapter_upload(chapter_name) {
     let data = bible_engbsb_chapter(chapter_name);
-    storage_upload_object(data);
+    const file_name = file_name_json(chapter_name);
+    const destination = string_combine('bible/engbsb/', file_name);
+    storage_upload_object(data, destination);
 }
