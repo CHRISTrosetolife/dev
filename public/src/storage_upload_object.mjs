74576074914bf64cd7a79e756d577c803832a8f0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 13:52:28 -0400

    2024-05-01T17:52:26.996Z  function_transform js_outside_move storage_upload_object

--- public/src/storage_upload_object.mjs
+++ public/src/storage_upload_object.mjs
@@ -1,8 +1,9 @@
+import {uuid} from "./uuid.mjs";
 import {file_write_json} from "./file_write_json.mjs";
 import {file_name_json} from "./file_name_json.mjs";
 import crypto from 'crypto';
 export async function storage_upload_object(object, destination) {
-    let uuid = crypto.randomUUID();
+    let uuid = uuid();
     let file_name = file_name_json(uuid);
     await file_write_json(file_path, object);
 }
