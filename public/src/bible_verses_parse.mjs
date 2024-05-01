7483266958b60239a3f37976daf5f9586c6810ae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 15:07:10 -0400

    2024-05-01T19:07:09.825Z  noop

--- public/src/bible_verses_parse.mjs
+++ public/src/bible_verses_parse.mjs
@@ -5,6 +5,9 @@ import {list_map} from "./list_map.mjs";
 import {string_split} from "./string_split.mjs";
 export function bible_verses_parse(rawText) {
     let split = string_split(rawText, '&#160;');
+    each_pairs(split, (previous, current) => {
+
+    })
     let mapped2 = list_map(split, m => {
         console.log({m})
         let split = string_split(m, '&#160;');
