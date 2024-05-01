099589bfc2477257f8ceae90824c09454b041497
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 15:05:44 -0400

    2024-05-01T19:05:42.807Z  noop

--- public/src/each_pairs.mjs
+++ public/src/each_pairs.mjs
@@ -1,1 +1,10 @@
-export function each_pairs(list, lambda) {}
+import { add_1 } from "./add_1.mjs";
+import { list_index_last } from "./list_index_last.mjs";
+import { range } from "./range.mjs";
+
+export function each_pairs(list, lambda) {
+    let last_index = list_index_last(list);
+    for (let i of range(last_index)) {
+        let i_next = add_1(i);
+    }
+}
