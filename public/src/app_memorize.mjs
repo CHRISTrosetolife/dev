6e6faef374161deac768fc377f14883644fc364b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 16:27:55 -0400

    2024-05-01T20:27:54.876Z  noop

--- public/src/app_memorize.mjs
+++ public/src/app_memorize.mjs
@@ -4,6 +4,8 @@ import {http_get} from "./http_get.mjs";
 import {html_document_body} from "./html_document_body.mjs";
 import { html_element } from "./html_element.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
+import { html_span } from "./html_span.mjs";
+import { html_span_text } from "./html_span_text.mjs";
 export async function app_memorize() {
     let root = html_document_body();
     let file_path = bible_engbsb_storage_path_file('JHN19');
@@ -14,6 +16,10 @@ export async function app_memorize() {
     for (let verse of verses) {
         let section = html_element(root, 'section');
         let {verse_tokens,verse_number} = verses;
-        html_inner_set()
+        html_span_text(section, verse_number);
+        for (let token of verse_tokens) {
+            html_span_text(section, ' ');
+            html_span_text(section, token);
+        }
     }
 }
