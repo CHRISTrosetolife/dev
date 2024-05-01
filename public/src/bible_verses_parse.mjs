2c0bf628d2fc8e89bbeb908ab94cee74e606a6ef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 15:03:03 -0400

    2024-05-01T19:03:00.707Z  bible_engbsb_chapter JHN19

--- public/src/bible_verses_parse.mjs
+++ public/src/bible_verses_parse.mjs
@@ -4,10 +4,11 @@ import {string_trim} from "./string_trim.mjs";
 import {list_map} from "./list_map.mjs";
 import {string_split} from "./string_split.mjs";
 export function bible_verses_parse(rawText) {
-    let split = string_split(rawText, '  ');
+    let split = string_split(rawText, '&#160;');
+    return split
     let mapped = list_map(split, string_trim);
-    console.log({mapped})
     let mapped2 = list_map(mapped, m => {
+        console.log({m})
         let split = string_split(m, '&#160;');
         let verse_number = list_get(split, 0);
         let text = list_get(split, 1);
