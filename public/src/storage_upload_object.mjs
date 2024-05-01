2ab26e423b26caef2838adefb1645d4f3a056fe5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 13:55:36 -0400

    2024-05-01T17:55:35.167Z  noop

--- public/src/storage_upload_object.mjs
+++ public/src/storage_upload_object.mjs
@@ -6,9 +6,9 @@ import {file_write_json} from "./file_write_json.mjs";
 import {file_name_json} from "./file_name_json.mjs";
 import {storage_upload_file} from "./storage_upload_file.mjs";
 export async function storage_upload_object(object, destination) {
+    let json = json_to(object);
     let u = uuid();
     let file_path = file_name_json(u);
-    let json = json_to(object);
     await file_write(file_path, json);
     storage_upload_file(file_name, destination);
     await file_delete(file_path);
