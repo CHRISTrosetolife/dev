cf7f7a45bf9f95b519ae47b9038acda4bbd77f2e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 16:01:42 -0400

    2024-05-01T20:01:40.462Z  noop

--- public/src/app_memorize.mjs
+++ public/src/app_memorize.mjs
@@ -3,4 +3,5 @@ import {html_document_body} from "./html_document_body.mjs";
 export function app_memorize() {
     let root = html_document_body();
     let data = http_get('https://firebasestorage.googleapis.com/v0/b/letjesusbeexalted.appspot.com/o/bible%2Fengbsb%2FJHN19.json?alt=media');
+    console.log({data})
 }
