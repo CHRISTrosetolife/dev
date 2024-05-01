717e15f8c1e8d9a833ba054742533174c7bb0551
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 16:24:08 -0400

    2024-05-01T20:24:08.074Z  noop

--- public/src/storage_url.mjs
+++ public/src/storage_url.mjs
@@ -1,5 +1,5 @@
 import {string_replace} from "./string_replace.mjs";
 export function storage_url(file_path) {
     let replaced = string_replace(file_path, '/', '%2F');
-    return `https://firebasestorage.googleapis.com/v0/b/letjesusbeexalted.appspot.com/o/${replaced}?alt=media/`;
+    return `https://firebasestorage.googleapis.com/v0/b/letjesusbeexalted.appspot.com/o/${replaced}?alt=media`;
 }
