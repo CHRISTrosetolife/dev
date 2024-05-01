106df5ddeefa359c947f134d56f40b1d9f023106
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 13:55:27 -0400

    2024-05-01T17:55:26.590Z  function_transform js_outside_move storage_upload_object

--- public/src/storage_upload_object.mjs
+++ public/src/storage_upload_object.mjs
@@ -1,3 +1,5 @@
+import {file_write} from "./file_write.mjs";
+import {json_to} from "./json_to.mjs";
 import {file_delete} from "./file_delete.mjs";
 import {uuid} from "./uuid.mjs";
 import {file_write_json} from "./file_write_json.mjs";
@@ -5,8 +7,9 @@ import {file_name_json} from "./file_name_json.mjs";
 import {storage_upload_file} from "./storage_upload_file.mjs";
 export async function storage_upload_object(object, destination) {
     let u = uuid();
-    let file_name = file_name_json(u);
-    await file_write_json(file_path, object);
+    let file_path = file_name_json(u);
+    let json = json_to(object);
+    await file_write(file_path, json);
     storage_upload_file(file_name, destination);
     await file_delete(file_path);
 }
