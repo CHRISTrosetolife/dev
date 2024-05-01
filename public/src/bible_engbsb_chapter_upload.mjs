6c8a2a233b02db7951d946abfd41637301d3a9b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 15:52:27 -0400

    2024-05-01T19:52:25.580Z  function_open bible_engbsb_chapter_upload

--- public/src/bible_engbsb_chapter_upload.mjs
+++ public/src/bible_engbsb_chapter_upload.mjs
@@ -4,6 +4,7 @@ import { string_combine } from "./string_combine.mjs";
 import { file_name_json } from "./file_name_json.mjs";
 export async function bible_engbsb_chapter_upload(chapter_name) {
     let data = bible_engbsb_chapter(chapter_name);
+    return data
     let file_name = file_name_json(chapter_name);
     let destination = string_combine('bible/engbsb/', file_name);
     await storage_upload_object(data, destination);
