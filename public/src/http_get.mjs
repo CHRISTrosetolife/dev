23e00517e0297fdbde8f8d649e013e56d0e4b9fe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 16:03:23 -0400

    2024-05-01T20:03:22.080Z  noop

--- public/src/http_get.mjs
+++ public/src/http_get.mjs
@@ -1,3 +1,8 @@
+import { assert } from "./assert.mjs";
+
 export function http_get(url) {
-    return axios.get(url)
+    let result = axios.get(url)
+    let {status,data} = result;
+    assert(equal, [status, 200])
+    return data;
 }
