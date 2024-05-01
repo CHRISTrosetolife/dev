46831c09e24add884d4ca81d9a433af1b9fe1b27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 13:00:17 -0400

    2024-05-01T17:00:15.187Z  noop

--- public/src/storage_upload.mjs
+++ public/src/storage_upload.mjs
@@ -1,1 +1,12 @@
-export function storage_upload(file_path) {}
+export function storage_upload(file_path) {
+    var admin = require("firebase-admin");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "gs://learnfirebase-39b36.appspot.com"
+});
+
+var bucket = admin.storage().bucket();
+
+bucket.upload("/Users/j.milanmethodius/Desktop/hapi screenshots/demoimage.jpeg")
+}
