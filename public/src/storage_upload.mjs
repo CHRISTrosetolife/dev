561a4009b06398fee26108139129ae9c94ac15e4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-01 13:05:52 -0400

    2024-05-01T17:05:48.200Z  function_imports_add storage_upload

--- public/src/storage_upload.mjs
+++ public/src/storage_upload.mjs
@@ -1,11 +1,15 @@
+import {file_read} from "./file_read.mjs";
 import admin from "firebase-admin";
-export function storage_upload(file_path) {
+export async function storage_upload(file_path) {
+    file_read();
+    var serviceAccount = require("path/to/serviceAccountKey.json");
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
         storageBucket: "gs://letJESUSbeexalted.appspot.com"
     });
-
     var bucket = admin.storage().bucket();
-
-    bucket.upload("/Users/j.milanmethodius/Desktop/hapi screenshots/demoimage.jpeg")
-    }
+    bucket.upload("/Users/j.milanmethodius/Desktop/hapi screenshots/demoimage.jpeg");
+}
