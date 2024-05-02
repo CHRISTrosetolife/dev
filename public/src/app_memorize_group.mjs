77448b0530d6b7065f4665249d2206d45bafd9a4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-02 11:28:11 -0400

    2024-05-02T15:28:09.641Z  noop

--- public/src/app_memorize_group.mjs
+++ public/src/app_memorize_group.mjs
@@ -1,8 +1,22 @@
+import { add } from "./add.mjs";
+import { divide } from "./divide.mjs";
+import { list_length } from "./list_length.mjs";
+import { list_take } from "./list_take.mjs";
+import { mod } from "./mod.mjs";
 import {range} from "./range.mjs";
 export function app_memorize_group(count) {
     let indices = range(count);
+    let sub = 2
     return indices
     function recursive(result, list) {
-
+        let length = list_length(list);
+        let half = divide(length, sub);
+        let half_floor = floor(half);
+        let remainder = mod(length, sub);
+        let first = list_take(list, add(half_floor, remainder))
     }
 }
+function floor(value) {
+    return Math.floor(value);
+}
+
