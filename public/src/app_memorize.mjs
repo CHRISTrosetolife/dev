c8a49294e0ba0e53b50cf7b9767f0986a6442045
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-02 14:04:13 -0400

    2024-05-02T18:04:12.539Z  noop

--- public/src/app_memorize.mjs
+++ public/src/app_memorize.mjs
@@ -67,7 +67,7 @@ export async function app_memorize() {
     let groups = app_memorize_group(verses_length);
     let patterns = [['1'], ['0', '1', '1'], ['1','0'], ['0', '1'], ['0']];
     let patterns_length = list_length(patterns);
-    let pattern_index = 0;
+    let pattern_index;
     let group_current;
     let verse_index;
     let token_index;
