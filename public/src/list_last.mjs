0be629139625dec0d500c8c9b2623eb2d1200f3f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-02 11:50:20 -0400

    2024-05-02T15:50:19.682Z  noop

--- public/src/list_last.mjs
+++ public/src/list_last.mjs
@@ -1,5 +1,5 @@
 import {list_index_last} from "./list_index_last.mjs";
 import {list_get} from "./list_get.mjs";
-export function list_last(split2) {
-    return list_get(split2, list_index_last(split2));
+export function list_last(list) {
+    return list_get(list, list_index_last(list));
 }
