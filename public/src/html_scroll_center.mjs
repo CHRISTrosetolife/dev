79f530cadde85c6fb10bbc6f39a407350441ebff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-02 09:38:54 -0400

    2024-05-02T13:38:53.554Z  noop

--- public/src/html_scroll_center.mjs
+++ public/src/html_scroll_center.mjs
@@ -1,6 +1,6 @@
 export function html_scroll_center(component) {
     let {element} = component
-    current_element.scrollIntoView({
+    element.scrollIntoView({
         behavior: 'auto',
         block: 'center',
         inline: 'center'
