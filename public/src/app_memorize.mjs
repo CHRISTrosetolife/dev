3d7b2090857b0874fb32b57ddf84be2c518e3373
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-02 17:03:30 -0400

    2024-05-02T21:03:29.410Z  noop

--- public/src/app_memorize.mjs
+++ public/src/app_memorize.mjs
@@ -89,7 +89,7 @@ export async function app_memorize() {
     function group_current_set(g) {
         group_current = g;
         if (equal(list_length(group_current), 1)) {
-            patterns = ['1', '011', '10', '01', '0', '0'];
+            patterns = ['1', '110', '10', '01', '0', '0'];
         } else {
             patterns = ['10', '0'];
         }
