73d121a8501ff4eddb1cf61e5bf881ab9a43fb05
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-03 16:31:14 -0400

    2024-05-03T20:31:13.623Z  noop

--- public/src/app_learn_code_random_identifier.mjs
+++ public/src/app_learn_code_random_identifier.mjs
@@ -3,7 +3,9 @@ import {string_combine} from "./string_combine.mjs";
 import {range} from "./range.mjs";
 import {integer_random} from "./integer_random.mjs";
 export function app_learn_code_random_identifier() {
-    let count = integer_random(1, 3);
+    let count = integer_random(1, 1);
+    'would need to exclude identifier names that cannot be used such as "if"'
+    'therefore setting to 1'
     let name = '';
     for (let i of range(count)) {
         name = string_combine(name, string_random_letter());
