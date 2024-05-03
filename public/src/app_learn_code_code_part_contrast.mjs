d6352d9c66dd4a01cec9c514813e6dbe3b47dcb4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-03 15:08:45 -0400

    2024-05-03T19:08:44.944Z  noop

--- public/src/app_learn_code_code_part_contrast.mjs
+++ public/src/app_learn_code_code_part_contrast.mjs
@@ -4,6 +4,5 @@ import {app_learn_code_code_part} from "./app_learn_code_code_part.mjs";
 export function app_learn_code_code_part_contrast(parent, code) {
     let component = app_learn_code_code_part(parent, code, 'black');
     app_learn_code_style_code_color(component);
-    let y_margin = '0.5vh';
-    html_style_margin_y(component, y_margin);
+    html_style_margin_y(component, '0.5dvh');
 }
