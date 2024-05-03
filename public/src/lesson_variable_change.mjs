52622bfcdfbcfded758307c225f1862737d1f3cd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-03 17:12:01 -0400

    2024-05-03T21:12:00.497Z  noop

--- public/src/lesson_variable_change.mjs
+++ public/src/lesson_variable_change.mjs
@@ -73,7 +73,7 @@ ${app_learn_code_log(name)}`;
         value_string = string_to(value);
         html_style_alternate_monospace(parent, ['here is code that will set the value of a variable named ', name, ' to a value of ', value, ' :']);
         app_learn_code_code_part_contrast(parent, js_code_statement_assign(name, value));
-        html_style_alternate_monospace(parent, ['if ', name, ' already has a value , then the value of ', name, ' will be changed to ', value]);
+        html_style_alternate_monospace(parent, ['if ', name, ' already has a value , and ',name,' does not already have a value of ',value,' , then the value of ', name, ' will be changed to ', value]);
         '= can be used to change the value of a variable';
         html_style_alternate_monospace(parent, ['remember , here is code that outputs twice because there are two ', 'console.log', ' statements :']);
     }
