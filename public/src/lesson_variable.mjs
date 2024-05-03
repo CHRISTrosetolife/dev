2493bda0fa220967607a27e679ab382965ce3596
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-03 15:11:06 -0400

    2024-05-03T19:11:05.121Z  noop

--- public/src/lesson_variable.mjs
+++ public/src/lesson_variable.mjs
@@ -79,14 +79,13 @@ console.log(${name});`;
         html_style_alternate(ul, html_li, [noop, html_style_monospace], ['underscores ( ', '_', ' )']);
         html_style_alternate(ul, html_li, [noop, html_style_monospace], ['dollar signs ( ', '$', ' )']);
         html_p_text(parent, 'here are some valid variable names :');
-        let ul2 = html_ul(parent);
-        app_learn_code_code_part_contrast(ul2, 'y');
-        app_learn_code_code_part_contrast(ul2, 'Y');
-        app_learn_code_code_part_contrast(ul2, '_');
-        app_learn_code_code_part_contrast(ul2, '$');
-        app_learn_code_code_part_contrast(ul2, '$$');
-        app_learn_code_code_part_contrast(ul2, '__$');
-        app_learn_code_code_part_contrast(ul2, 'd8$_');
+        app_learn_code_code_part_contrast(parent, 'y');
+        app_learn_code_code_part_contrast(parent, 'Y');
+        app_learn_code_code_part_contrast(parent, '_');
+        app_learn_code_code_part_contrast(parent, '$');
+        app_learn_code_code_part_contrast(parent, '$$');
+        app_learn_code_code_part_contrast(parent, '__$');
+        app_learn_code_code_part_contrast(parent, 'd8$_');
     }
     function screen4(parent) {
         'variable names cannot begin with a number';
