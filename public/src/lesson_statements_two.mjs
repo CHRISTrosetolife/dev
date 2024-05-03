1f8e3a4e616da8c4c96eefd4f4fa3900d1792fd9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-03 16:16:10 -0400

    2024-05-03T20:16:10.007Z  noop

--- public/src/lesson_statements_two.mjs
+++ public/src/lesson_statements_two.mjs
@@ -11,9 +11,7 @@ import {html_style_monospace} from "./html_style_monospace.mjs";
 export function lesson_statements_two() {
     const description = 'two console.log statements';
     const example_message = 'with two statements';
-    return lesson_simple(() => {
-        return lesson_statements_two_quiz();
-    }, example_before, description, example_message, []);
+    return lesson_simple(lesson_statements_two_quiz, example_before, description, example_message, []);
     function example_before(parent) {
         html_p_text(parent, 'statements are ran one after the other');
         html_p_text(parent, 'if there are two statements , the first statement will run and then the second statement will run');
