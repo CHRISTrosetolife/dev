46df3d742ff596df45d49c7cd012d4062bde4576
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-02 20:41:59 -0400

    2024-05-03T00:41:58.169Z  noop

--- public/src/list_concat_multiple.mjs
+++ public/src/list_concat_multiple.mjs
@@ -1,1 +1,8 @@
-export function list_concat_multiple(lists) {}
+import { list_add_multiple } from "./list_add_multiple.mjs";
+export function list_concat_multiple(lists) {
+    let result = [];
+    for (let list of lists) {
+        list_add_multiple(result, list);
+    }
+    return result;
+}
