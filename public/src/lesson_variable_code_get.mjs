16bf2728d037aa63e40ed71132a010a532286e8f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-03 17:01:23 -0400

    2024-05-03T21:01:22.838Z  function_transform js_outside_move lesson_variable_code_get

--- public/src/lesson_variable_code_get.mjs
+++ public/src/lesson_variable_code_get.mjs
@@ -1,9 +1,10 @@
+import {js_code_statement_let} from "./js_code_statement_let.mjs";
 import {app_learn_code_log} from "./app_learn_code_log.mjs";
 import {js_code_statement_assign} from "./js_code_statement_assign.mjs";
 import {js_code_statement} from "./js_code_statement.mjs";
 import {js_code_assign} from "./js_code_assign.mjs";
 export function lesson_variable_code_get(name, value) {
-    return `let ${name};
+    return `${js_code_statement_let(name)}
 ${js_code_statement_assign(name, value)}
 ${app_learn_code_log(name)}`;
 }
