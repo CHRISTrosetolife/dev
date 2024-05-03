08575ff3588afac9c3c3f7cdcd8893cd8ecf8ae0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-02 20:44:50 -0400

    2024-05-03T00:44:49.284Z  keyboard_keys

--- public/src/keyboard_keys.mjs
+++ public/src/keyboard_keys.mjs
@@ -1,3 +1,8 @@
+import { keyboard_keys_rows } from "./keyboard_keys_rows.mjs";
+import { list_concat_multiple } from "./list_concat_multiple.mjs";
+
 export function keyboard_keys() {
-    return ['qwertyuiop', 'asdfghjkl', 'zxcvbnm'];
+    let ks = keyboard_keys_rows();
+    let concat = list_concat_multiple(ks);
+    return concat
 }
