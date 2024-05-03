c07be4cdb775ab897b0d0f3303623f20c5864074
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-03 16:56:29 -0400

    2024-05-03T20:56:28.568Z  function_transform js_outside_move js_code_declare_assign

--- public/src/js_code_declare_assign.mjs
+++ public/src/js_code_declare_assign.mjs
@@ -1,8 +1,9 @@
+import {js_code_assign} from "./js_code_assign.mjs";
 import {js_parse_first} from "./js_parse_first.mjs";
 import {js_code_statement} from "./js_code_statement.mjs";
 export function js_code_declare_assign(variable_name) {
     const value = `0`;
-    const code = js_code_statement(`let ${`${variable_name} = ${value}`}`);
+    const code = js_code_statement(`let ${js_code_assign(variable_name, value)}`);
     let parsed = js_parse_first(code);
     return parsed;
 }
