3424d604a3a282ca8914a5d57fc05703d7c46f99
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-03 17:01:05 -0400

    2024-05-03T21:01:04.203Z  function_transform js_outside_move js_code_declare_assign

---
+++ public/src/js_code_statement_let.mjs
@@ -0,0 +1,5 @@
+import {js_code_let} from "./js_code_let.mjs";
+import {js_code_statement} from "./js_code_statement.mjs";
+export function js_code_statement_let(declare) {
+    return js_code_statement(js_code_let(declare));
+}
