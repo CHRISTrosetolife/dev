5786f8f34918ffc7d4279324d6b47f00344c34bf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-03 15:08:33 -0400

    2024-05-03T19:08:32.845Z  function_transform js_outside_move app_learn_code_code_part_contrast

--- public/src/app_learn_code_code_part_contrast.mjs
+++ public/src/app_learn_code_code_part_contrast.mjs
@@ -1,6 +1,9 @@
+import {html_style_margin_y} from "./html_style_margin_y.mjs";
 import {app_learn_code_style_code_color} from "./app_learn_code_style_code_color.mjs";
 import {app_learn_code_code_part} from "./app_learn_code_code_part.mjs";
 export function app_learn_code_code_part_contrast(parent, code) {
     let component = app_learn_code_code_part(parent, code, 'black');
     app_learn_code_style_code_color(component);
+    let y_margin = '0.5vh';
+    html_style_margin_y(component, y_margin);
 }
