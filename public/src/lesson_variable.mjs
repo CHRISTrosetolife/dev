109ce34dd660546334b96d0e3a7184205139f232
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-02 20:33:49 -0400

    2024-05-03T00:33:47.113Z  function_copy lesson_log_boolean lesson_variable

---
+++ public/src/lesson_variable.mjs
@@ -0,0 +1,35 @@
+import {html_style_monospace} from "./html_style_monospace.mjs";
+import {html_p} from "./html_p.mjs";
+import {html_style_alternate} from "./html_style_alternate.mjs";
+import {lesson_add_quiz} from "./lesson_add_quiz.mjs";
+import {lesson_simple} from "./lesson_simple.mjs";
+import {app_learn_code_unscramble} from "./app_learn_code_unscramble.mjs";
+import {lesson_log_number_quiz} from "./lesson_log_number_quiz.mjs";
+import {app_learn_code_quiz} from "./app_learn_code_quiz.mjs";
+import {lesson_log_number_example} from "./lesson_log_number_example.mjs";
+import {lesson_console_log_javascript} from "./lesson_console_log_javascript.mjs";
+import {lesson_console_log_process_named} from "./lesson_console_log_process_named.mjs";
+import {lesson_console_log_process_computer_program} from "./lesson_console_log_process_computer_program.mjs";
+import {list_add} from "./list_add.mjs";
+import {array_new} from "./array_new.mjs";
+import {object_new} from "./object_new.mjs";
+import {object_property_set} from "./object_property_set.mjs";
+import {lesson_console_log_analogy_paper} from "./lesson_console_log_analogy_paper.mjs";
+import {lesson_console_log_analogy_addition} from "./lesson_console_log_analogy_addition.mjs";
+import {lesson_console_log_program_first} from "./lesson_console_log_program_first.mjs";
+import {lesson_console_log_strings} from "./lesson_console_log_strings.mjs";
+import {lesson_console_log_analogy_addition_symbol} from "./lesson_console_log_analogy_addition_symbol.mjs";
+import {lesson_log_number_explain} from "./lesson_log_number_explain.mjs";
+import {noop} from "./noop.mjs";
+import { random_50_50 } from "./random_50_50.mjs";
+export function lesson_log_boolean() {
+    let example_message = 'outputting a boolean to the console';
+    let description = example_message;
+    return lesson_simple(function () {
+        return `console.log(${random_50_50()});
+console.log(${random_50_50()});`;
+    }, example_before, description, example_message, []);
+    function example_before(parent) {
+        html_style_alternate(parent, html_p, [noop, html_style_monospace], ['boolean ', 'true', ' and ', 'false', ' can be used in code :'])
+    }
+}
