f160e9db8b43fbaaaaa9abde38b063aa2ac9ee4c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-03 17:00:12 -0400

    2024-05-03T21:00:11.634Z  noop

--- public/src/js_code_declare_assign.mjs
+++ public/src/js_code_declare_assign.mjs
@@ -3,7 +3,8 @@ import {js_parse_first} from "./js_parse_first.mjs";
 import {js_code_statement} from "./js_code_statement.mjs";
 export function js_code_declare_assign(variable_name) {
     const value = `0`;
-    const code = js_code_statement(`let ${js_code_assign(variable_name, value)}`);
+    const declare = js_code_assign(variable_name, value);
+    const code = js_code_statement(`let ${declare}`);
     let parsed = js_parse_first(code);
     return parsed;
 }
