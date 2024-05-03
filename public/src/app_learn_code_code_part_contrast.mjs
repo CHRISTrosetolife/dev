0122fab131bc2e04d55c868dc7c55d7c59191a0d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-03 16:28:52 -0400

    2024-05-03T20:28:51.657Z  function_transform js_outside_move app_learn_code_code_part_contrast

--- public/src/app_learn_code_code_part_contrast.mjs
+++ public/src/app_learn_code_code_part_contrast.mjs
@@ -1,3 +1,4 @@
+import {html_style_pre_wrap} from "./html_style_pre_wrap.mjs";
 import {html_style_margin_y} from "./html_style_margin_y.mjs";
 import {app_learn_code_style_code_color} from "./app_learn_code_style_code_color.mjs";
 import {app_learn_code_code_part} from "./app_learn_code_code_part.mjs";
@@ -5,4 +6,5 @@ export function app_learn_code_code_part_contrast(parent, code) {
     let component = app_learn_code_code_part(parent, code, 'black');
     app_learn_code_style_code_color(component);
     html_style_margin_y(component, '0.5dvh');
+    html_style_pre_wrap(component);
 }
