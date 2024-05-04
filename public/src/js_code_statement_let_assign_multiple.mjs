8b96d215c3dd64c801f95acb0121712bd3127962
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-04 17:30:35 -0400

    2024-05-04T21:30:34.196Z  noop

--- public/src/js_code_statement_let_assign_multiple.mjs
+++ public/src/js_code_statement_let_assign_multiple.mjs
@@ -6,13 +6,18 @@ import { list_length } from "./list_length.mjs";
 import { assert } from "./assert.mjs";
 import { equal } from "./equal.mjs";
 import { range } from "./range.mjs";
+import { list_add } from "./list_add.mjs";
+import { list_get } from "./list_get.mjs";
+import { array_new } from "./array_new.mjs";
 export function js_code_statement_let_assign_multiple(names, values) {
     let length_n = list_length(names);
     let length_v = list_length(values);
     assert(equal, [length_n, length_v])
-    let list = [js_code_assign(name_a, a), js_code_assign(name_b, b)];
+    let list = array_new();
     for (let i of range(length_n)) {
-        
+        let n = list_get(names, i);
+        let v = list_get(values, v);
+        list_add(list, js_code_assign(n, v))
     }
     return js_code_statement_let(list_join(list, ', '));
 }
