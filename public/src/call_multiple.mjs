f97b17e84b9b3ccb664a2dc5385a86f5147a3c87
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-04 17:02:15 -0400

    2024-05-04T21:02:14.112Z  function_transform js_outside_move lesson_variables_three

--- public/src/call_multiple.mjs
+++ public/src/call_multiple.mjs
@@ -1,1 +1,10 @@
-export function call_multiple(fn) {}
+import { list_adder } from "./list_adder.mjs";
+import { range } from "./range.mjs";
+
+export function call_multiple(fn, times) {
+    return list_adder(la => {
+        for (let i of range(times)) {
+            la(fn())
+        }
+    })
+}
--- public/src/lesson_variables_three.mjs
+++ public/src/lesson_variables_three.mjs
@@ -1,3 +1,4 @@
+import {call_multiple} from "./call_multiple.mjs";
 import {app_learn_code_unique} from "./app_learn_code_unique.mjs";
 import {app_learn_code_log_add} from "./app_learn_code_log_add.mjs";
 import {integer_random_digit_single_positive} from "./integer_random_digit_single_positive.mjs";
@@ -69,8 +70,7 @@ export function lesson_variables_three() {
     let description = 'three variables';
     let example_message = string_combine('with ', description);
     return lesson_simple(function () {
-        let a = integer_random_digit_single_positive();
-        let b = integer_random_digit_single_positive();
+        let [a, b, c] = call_multiple(integer_random_digit_single_positive, 3);
         let name_a = app_learn_code_random_identifier();
         let name_b = app_learn_code_unique(app_learn_code_random_identifier, [name_a]);
         return `${js_code_statement_let_assign(name_a, a)}
