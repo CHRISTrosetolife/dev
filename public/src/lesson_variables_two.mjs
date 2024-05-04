178854f67a9bb4f9bc1d7a29c173a335bfa08cbf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-04 17:38:37 -0400

    2024-05-04T21:38:36.921Z  noop

--- public/src/lesson_variables_two.mjs
+++ public/src/lesson_variables_two.mjs
@@ -73,7 +73,8 @@ export function lesson_variables_two() {
     return lesson_simple(function () {
         let count = 2;
         let [a, b] = call_multiple(integer_random_digit_single_positive, count);
-        let [name_a, name_b] = app_learn_code_unique_multiple(app_learn_code_random_identifier, count);
+        let names = app_learn_code_unique_multiple(app_learn_code_random_identifier, count);
+        let [name_a, name_b] = names;
         return `${js_code_statement_let_assign(name_a, a)}
 ${js_code_statement_let_assign(name_b, b)}
 ${app_learn_code_log_add(name_a, name_b)}`;
