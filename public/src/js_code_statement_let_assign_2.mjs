16869629afce544dddf645289a57216250d9eeb4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-04 17:25:57 -0400

    2024-05-04T21:25:56.517Z  noop

--- public/src/js_code_statement_let_assign_2.mjs
+++ public/src/js_code_statement_let_assign_2.mjs
@@ -2,5 +2,5 @@ import {js_code_assign} from "./js_code_assign.mjs";
 import {list_join_comma} from "./list_join_comma.mjs";
 import {js_code_statement_let} from "./js_code_statement_let.mjs";
 export function js_code_statement_let_assign_2(name_a, a, name_b, b) {
-    return js_code_statement_let(list_join_comma(js_code_assign(name_a, a), js_code_assign(name_b, b)));
+    return js_code_statement_let(list_join_comma([js_code_assign(name_a, a), js_code_assign(name_b, b)]));
 }
