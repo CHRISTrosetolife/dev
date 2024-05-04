102c034684495201705b25fa9f008829083d7698
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-04 15:02:33 -0400

    2024-05-04T19:02:32.722Z  noop

--- public/src/app_learn_code_source_variations.mjs
+++ public/src/app_learn_code_source_variations.mjs
@@ -36,7 +36,7 @@ export function app_learn_code_source_variations(source) {
         let b_split = string_split_empty(base2);
         let b_split_length = list_length(b_split);
         for (let n of range(length)) {
-            if (less_than(less_than(n, b_split_length))) {
+            if (less_than(n, b_split_length)) {
                 console.log(list_get(b_split, n));
             } else {
                 console.log('here');
