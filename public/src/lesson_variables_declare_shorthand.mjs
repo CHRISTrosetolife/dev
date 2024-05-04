55073ea8061fa3f9855d9d833c560f139905d3b4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-04 17:20:45 -0400

    2024-05-04T21:20:44.347Z  noop

--- public/src/lesson_variables_declare_shorthand.mjs
+++ public/src/lesson_variables_declare_shorthand.mjs
@@ -82,6 +82,8 @@ ${app_learn_code_log_add(name_a, name_b)}`;
         let [a, b] = call_multiple(integer_random_digit_single_positive, count);
         let [name_a, name_b] = app_learn_code_unique_multiple(app_learn_code_random_identifier, count);
         'here is some code :';
+        `${js_code_statement_let_assign(name_a, a)}
+${js_code_statement_let_assign(name_b, b)}`;
         'that code can be shortened to the following :';
         js_code_statement_let_assign_2(name_a, a, name_b, b)
     }
