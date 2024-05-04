7f126f2454751da4c62e912c2e66df8cba93e540
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-04 13:07:56 -0400

    2024-05-04T17:07:55.312Z  noop

--- public/src/js_code_statement_let_assign.mjs
+++ public/src/js_code_statement_let_assign.mjs
@@ -1,5 +1,5 @@
 import {js_code_assign} from "./js_code_assign.mjs";
 import {js_code_statement_let} from "./js_code_statement_let.mjs";
 export function js_code_statement_let_assign(name, value) {
-    js_code_statement_let(js_code_assign(name, value));
+    return js_code_statement_let(js_code_assign(name, value));
 }
