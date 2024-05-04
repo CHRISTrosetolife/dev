a52d82ae309d0955449a61447496fa558c50602e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-04 13:34:56 -0400

    2024-05-04T17:34:55.782Z  noop

--- public/src/lesson_variables_two.mjs
+++ public/src/lesson_variables_two.mjs
@@ -76,7 +76,7 @@ export function lesson_variables_two() {
         return `${js_code_statement_let_assign(name_a, a)}
 ${js_code_statement_let_assign(name_b, b)}
 ${app_learn_code_log_add(name_a, name_b)}`;
-    }, example_before, description, example_message, [screen1]);
+    }, example_before, description, example_message, []);
     function example_before(parent) {
         html_p_text(parent, 'a program can have more than one variable');
     }
