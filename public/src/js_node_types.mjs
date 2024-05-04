eb119651e4cf0a8f288daa9240695492baafaaa7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-04 15:53:54 -0400

    2024-05-04T19:53:52.825Z  function_imports_add js_node_types

--- public/src/js_node_types.mjs
+++ public/src/js_node_types.mjs
@@ -1,3 +1,4 @@
+import {undefined_not_is} from "./undefined_not_is.mjs";
 import {list_adder_unique} from './list_adder_unique.mjs';
 import {js_visit} from './js_visit.mjs';
 export function js_node_types(ast) {
@@ -5,7 +6,10 @@ export function js_node_types(ast) {
         js_visit(ast, lambda);
         function lambda(v) {
             let {node} = v;
-            la(node.type);
+            let {type} = node;
+            if (undefined_not_is(type)) {
+                la(type);
+            }
         }
     });
 }
