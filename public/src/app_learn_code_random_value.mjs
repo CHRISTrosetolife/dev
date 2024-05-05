b5dccc67972e4af7971ff8a33aff0c94da873c7c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-05 18:07:29 -0400

    2024-05-05T22:07:27.675Z  noop

--- public/src/app_learn_code_random_value.mjs
+++ public/src/app_learn_code_random_value.mjs
@@ -3,11 +3,12 @@ import {random_50_50} from "./random_50_50.mjs";
 import {integer_random_digit_single} from "./integer_random_digit_single.mjs";
 import {equal} from "./equal.mjs";
 import {integer_random} from "./integer_random.mjs";
+import { integer_random_digit_single_positive } from "./integer_random_digit_single_positive.mjs";
 export function app_learn_code_random_value() {
     let value;
     let r = integer_random(1, 3);
     if (equal(r, 1)) {
-        value = integer_random_digit_single();
+        value = integer_random_digit_single_positive();
     } else if (equal(r, 2)) {
         value = random_50_50();
     } else {
