14119fe318d2b0620936d7974a418db88669e9f8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-05 16:09:41 -0400

    2024-05-05T20:09:40.181Z  noop

--- public/src/js_unparse.mjs
+++ public/src/js_unparse.mjs
@@ -3,6 +3,5 @@ import {import_multi} from "./import_multi.mjs";
 let a = await import_multi('astring', 'astring');
 let {generate} = a;
 export function js_unparse(ast) {
-    let indent = '    ';
-    return js_unparse_indent(ast, indent);
+    return js_unparse_indent(ast, '    ');
 }
