a852d4fc3fa20577739a41c06386f23f168c9346
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-05 12:19:51 -0400

    2024-05-05T16:19:50.120Z  noop

--- public/src/app_learn_code_random_identifier.mjs
+++ public/src/app_learn_code_random_identifier.mjs
@@ -3,17 +3,11 @@ import {string_combine} from "./string_combine.mjs";
 import {range} from "./range.mjs";
 import {integer_random} from "./integer_random.mjs";
 import { list_remove } from "./list_remove.mjs";
+import { assert } from "./assert.mjs";
+import { equal } from "./equal.mjs";
+import { list_length } from "./list_length.mjs";
 export function app_learn_code_random_identifier() {
-    let ks = keyboard_keys();
-    list_remove(ks, 'l');
-    'the letter l looks like an i and a 1 so removing to to avoid visual ambiguity'
-    let count = integer_random(1, 1);
-    'would need to exclude identifier names that cannot be used such as "if"'
-    'therefore setting to 1'
-    let name = '';
-    for (let i of range(count)) {
-        const l = string_random_letter();
-        name = string_combine(name, l);
-    }
-    return name;
+    let list = app_learn_code_random_identifiers(1);
+    let length = list_length(list);
+    assert(equal, [length, 1]);
 }
