2225c2355f7d54d0584be4365e6be903bb292240
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-05 13:56:40 -0400

    2024-05-05T17:56:38.217Z  function_transform js_outside_move html_img_width_full

--- public/src/html_img_width_full.mjs
+++ public/src/html_img_width_full.mjs
@@ -1,12 +1,10 @@
+import {html_img} from "./html_img.mjs";
 import {html_style} from "./html_style.mjs";
 import {object_property_set} from "./object_property_set.mjs";
 import {string_combine} from "./string_combine.mjs";
 import {html_element} from "./html_element.mjs";
 export function html_img_width_full(parent, img_name) {
-    let i1 = html_element(parent, 'img');
-    let img_path = string_combine('./img/', img_name);
-    let {element} = i1;
-    object_property_set(element, 'src', img_path);
+    let i1 = html_img(parent, img_name);
     html_style(i1, {
         'width': '100%'
     });
