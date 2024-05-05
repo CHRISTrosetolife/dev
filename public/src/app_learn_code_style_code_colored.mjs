d1e49cc9db30f9ff366a75d5485c6f613493afad
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-05 14:29:22 -0400

    2024-05-05T18:29:21.724Z  noop

--- public/src/app_learn_code_style_code_colored.mjs
+++ public/src/app_learn_code_style_code_colored.mjs
@@ -4,8 +4,8 @@ import {html_style_background_color} from "./html_style_background_color.mjs";
 import {app_learn_code_style_code_color} from "./app_learn_code_style_code_color.mjs";
 import { html_style_button_default_border } from "./html_style_button_default_border.mjs";
 import { html_style_button_default_border_color } from "./html_style_button_default_border_color.mjs";
+import { html_style_button_default_background_color } from "./html_style_button_default_background_color.mjs";
 export function app_learn_code_style_code_colored(component) {
-    app_learn_code_style_code_color(component);
-    html_style_background_color(component,  html_style_button_default_border_color());
+    html_style_background_color(component,  html_style_button_default_background_color());
     app_learn_code_style_code(component);
 }
