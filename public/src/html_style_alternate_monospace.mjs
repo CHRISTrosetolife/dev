b1eee4ae586bc0a8823d6236cda06923bf9e8670
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-05 14:19:14 -0400

    2024-05-05T18:19:12.469Z  noop

--- public/src/html_style_alternate_monospace.mjs
+++ public/src/html_style_alternate_monospace.mjs
@@ -3,7 +3,7 @@ import {noop} from "./noop.mjs";
 import {html_p} from "./html_p.mjs";
 import {html_style_alternate} from "./html_style_alternate.mjs";
 export function html_style_alternate_monospace(parent, list) {
-    html_style_alternate(parent, html_p, [noop, html_style_monospace], list);
+    html_style_alternate(parent, html_p, [noop, html_style_code], list);
 }
 function html_style_code(component) {
     app_learn_code_style_code_color(component);
