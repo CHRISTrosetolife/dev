e49705b995903989782a8cb2539329e82b193f0f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-05 14:22:20 -0400

    2024-05-05T18:22:19.154Z  function_transform js_outside_move app_learn_code_random_identifiers

--- public/src/app_learn_code_random_identifiers.mjs
+++ public/src/app_learn_code_random_identifiers.mjs
@@ -1,10 +1,11 @@
+import {keyboard_keys} from "./keyboard_keys.mjs";
 import {string_random_letter} from "./string_random_letter.mjs";
 import {string_combine} from "./string_combine.mjs";
 import {range} from "./range.mjs";
 import {integer_random} from "./integer_random.mjs";
 import {list_remove} from "./list_remove.mjs";
-import { list_scramble } from "./list_scramble.mjs";
-import { list_take } from "./list_take.mjs";
+import {list_scramble} from "./list_scramble.mjs";
+import {list_take} from "./list_take.mjs";
 export function app_learn_code_random_identifiers(count) {
     let ks = keyboard_keys();
     'the letter l looks like an i and a 1 so removing to to avoid visual ambiguity';
