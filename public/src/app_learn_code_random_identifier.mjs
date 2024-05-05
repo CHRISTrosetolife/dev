ac0b9a758b130458bf4043c3b49a6e0053fbff48
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-05 12:20:05 -0400

    2024-05-05T16:20:04.564Z  function_imports_add app_learn_code_random_identifier

--- public/src/app_learn_code_random_identifier.mjs
+++ public/src/app_learn_code_random_identifier.mjs
@@ -1,11 +1,12 @@
+import {app_learn_code_random_identifiers} from "./app_learn_code_random_identifiers.mjs";
 import {string_random_letter} from "./string_random_letter.mjs";
 import {string_combine} from "./string_combine.mjs";
 import {range} from "./range.mjs";
 import {integer_random} from "./integer_random.mjs";
-import { list_remove } from "./list_remove.mjs";
-import { assert } from "./assert.mjs";
-import { equal } from "./equal.mjs";
-import { list_length } from "./list_length.mjs";
+import {list_remove} from "./list_remove.mjs";
+import {assert} from "./assert.mjs";
+import {equal} from "./equal.mjs";
+import {list_length} from "./list_length.mjs";
 export function app_learn_code_random_identifier() {
     let list = app_learn_code_random_identifiers(1);
     let length = list_length(list);
