430d977bf383af22c23ce5788d71f9155e942abb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-05 12:18:32 -0400

    2024-05-05T16:18:31.308Z  noop

--- public/src/app_learn_code_random_identifiers.mjs
+++ public/src/app_learn_code_random_identifiers.mjs
@@ -2,14 +2,18 @@ import {string_random_letter} from "./string_random_letter.mjs";
 import {string_combine} from "./string_combine.mjs";
 import {range} from "./range.mjs";
 import {integer_random} from "./integer_random.mjs";
-import { list_remove } from "./list_remove.mjs";
-export function app_learn_code_random_identifier() {
+import {list_remove} from "./list_remove.mjs";
+import { list_scramble } from "./list_scramble.mjs";
+import { list_take } from "./list_take.mjs";
+export function app_learn_code_random_identifiers(count) {
     let ks = keyboard_keys();
     list_remove(ks, 'l');
-    'the letter l looks like an i and a 1 so removing to to avoid visual ambiguity'
+    'the letter l looks like an i and a 1 so removing to to avoid visual ambiguity';
+    list_scramble(ks);
+    return list_take(count);
     let count = integer_random(1, 1);
-    'would need to exclude identifier names that cannot be used such as "if"'
-    'therefore setting to 1'
+    'would need to exclude identifier names that cannot be used such as "if"';
+    'therefore setting to 1';
     let name = '';
     for (let i of range(count)) {
         const l = string_random_letter();
