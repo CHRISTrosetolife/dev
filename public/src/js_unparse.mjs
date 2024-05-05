59578fdb557a6223fb063c734f172fbcd2e32f09
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-05 16:10:01 -0400

    2024-05-05T20:10:00.590Z  noop

--- public/src/js_unparse.mjs
+++ public/src/js_unparse.mjs
@@ -1,7 +1,4 @@
 import {js_unparse_indent} from "./js_unparse_indent.mjs";
-import {import_multi} from "./import_multi.mjs";
-let a = await import_multi('astring', 'astring');
-let {generate} = a;
 export function js_unparse(ast) {
     return js_unparse_indent(ast, '    ');
 }
--- public/src/js_unparse_indent.mjs
+++ public/src/js_unparse_indent.mjs
@@ -1,3 +1,6 @@
+import {import_multi} from "./import_multi.mjs";
+let a = await import_multi('astring', 'astring');
+let {generate} = a;
 export function js_unparse_indent(ast, indent) {
     return generate(ast, {
         indent: indent
