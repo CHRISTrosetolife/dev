dc22f2e4c09ce8120c3204e525cc8ee35f5a8da2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-05 14:20:24 -0400

    2024-05-05T18:20:23.704Z  noop

--- public/src/html_style_alternate_monospace.mjs
+++ public/src/html_style_alternate_monospace.mjs
@@ -2,9 +2,11 @@ import {html_style_monospace} from "./html_style_monospace.mjs";
 import {noop} from "./noop.mjs";
 import {html_p} from "./html_p.mjs";
 import {html_style_alternate} from "./html_style_alternate.mjs";
+import { html_style_background_color } from "./html_style_background_color.mjs";
 export function html_style_alternate_monospace(parent, list) {
     html_style_alternate(parent, html_p, [noop, html_style_code], list);
 }
 function html_style_code(component) {
     app_learn_code_style_code_color(component);
+    html_style_background_color(component, app_learn_code_style_code_color_background())
 }
