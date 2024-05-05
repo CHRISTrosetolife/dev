b94ffc6734ec77654504c85f997dca71aaabb462
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-04 21:09:13 -0400

    2024-05-05T01:09:12.752Z  noop

--- public/src/lesson_variables_declare_shorthand.mjs
+++ public/src/lesson_variables_declare_shorthand.mjs
@@ -81,8 +81,11 @@ export function lesson_variables_declare_shorthand() {
         let [name_a, name_b] = names;
         return `${js_code_statement_let_assign_multiple(names, values)}
 ${app_learn_code_log_add(name_a, name_b)}`;
-    }, example_before, description, example_message, [screen1]);
+    }, example_before, description, example_message, [screen1, screen2]);
     function screen1(parent) {
+
+    }
+    function screen2(parent) {
         let values = call_multiple(integer_random_digit_single_positive, count);
         let [a, b] = values;
         let names = app_learn_code_unique_multiple(app_learn_code_random_identifier, count);
