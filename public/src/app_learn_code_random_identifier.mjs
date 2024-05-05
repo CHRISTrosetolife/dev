7091fc86f82e408818e3f1c70fc4545cfd9f153f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-05 12:15:56 -0400

    2024-05-05T16:15:56.009Z  function_search shuffle

--- public/src/app_learn_code_random_identifier.mjs
+++ public/src/app_learn_code_random_identifier.mjs
@@ -2,7 +2,11 @@ import {string_random_letter} from "./string_random_letter.mjs";
 import {string_combine} from "./string_combine.mjs";
 import {range} from "./range.mjs";
 import {integer_random} from "./integer_random.mjs";
+import { list_remove } from "./list_remove.mjs";
 export function app_learn_code_random_identifier() {
+    let ks = keyboard_keys();
+    list_remove(ks, 'l');
+    'the letter l looks like an i and a 1 so removing to to avoid visual ambiguity'
     let count = integer_random(1, 1);
     'would need to exclude identifier names that cannot be used such as "if"'
     'therefore setting to 1'
