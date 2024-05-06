3e97559481671a1c09caa6044a4519f5978b86bf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 15:25:22 -0400

    2024-05-06T19:25:21.167Z  function_transform js_imports_add js_imports_fix

--- public/src/js_imports_fix.mjs
+++ public/src/js_imports_fix.mjs
@@ -1,1 +1,6 @@
-export function js_imports_fix(ast) {}
+import {js_imports_remove} from "./js_imports_remove.mjs";
+import {js_imports_add} from "./js_imports_add.mjs";
+export async function js_imports_fix(ast) {
+    await js_imports_add(ast);
+    js_imports_remove(ast);
+}
