df7519d3dccb8a62b46b5ce535f723d3b63961fc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 15:15:12 -0400

    2024-05-06T19:15:10.317Z  function_transform js_imports_remove js_imports_remove

--- public/src/js_imports_remove.mjs
+++ public/src/js_imports_remove.mjs
@@ -1,14 +1,10 @@
 import {list_get_end} from "./list_get_end.mjs";
 import {list_single} from "./list_single.mjs";
-import {list_length_1} from "./list_length_1.mjs";
 import {js_imports_existing_named} from "./js_imports_existing_named.mjs";
 import {list_difference} from "./list_difference.mjs";
 import {js_identifiers} from "./js_identifiers.mjs";
 import {js_export_single} from "./js_export_single.mjs";
 import {js_imports_existing} from "./js_imports_existing.mjs";
-import {noop} from "./noop.mjs";
-import {assert} from "./assert.mjs";
-import {list_length} from "./list_length.mjs";
 import {list_remove} from "./list_remove.mjs";
 export function js_imports_remove(ast) {
     let existing = js_imports_existing(ast);
@@ -20,8 +16,7 @@ export function js_imports_remove(ast) {
         let existing2 = js_imports_existing_named(ast, e);
         let e2 = list_single(existing2);
         let {stack} = e2;
-        let ancestor = list_get_end(stack, 2);
-        console.log(ancestor);
-        list_remove(body, e2);
+        let ancestor = list_get_end(stack, 1);
+        list_remove(body, ancestor);
     }
 }
