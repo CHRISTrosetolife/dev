2b04be7f7ebb161f67f060dabb6cdca7a19d71d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 18:18:44 -0400

    2024-05-06T22:18:42.490Z  noop

--- public/src/folder_read.mjs
+++ public/src/folder_read.mjs
@@ -1,5 +1,7 @@
-import {import_node} from './import_node.mjs';let {glob} = await import_node('glob');
+import {import_node} from './import_node.mjs';
+let g = await import_node('glob');
 export async function folder_read(folder, file_extension) {
+    let {glob} = g;
     let files = await glob(`${folder}**/*${file_extension}`);
     return files;
 }
