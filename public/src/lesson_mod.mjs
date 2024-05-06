bfb939cee371ad56335bdb48164e00a9dc6044e9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 07:14:34 -0400

    2024-05-06T11:14:33.007Z  noop

--- public/src/lesson_mod.mjs
+++ public/src/lesson_mod.mjs
@@ -122,8 +122,9 @@ export function lesson_mod() {
     }
     function screen3(parent) {
         let divisor = 2;
-        html_p_text(parent, string_combine('in the table below are the quotient and remainders for a divisor of ', divisor))
-        html_p_text(parent, 'in the table below :')
+        html_p_text(parent, string_combine('in the table below are quotient and remainders for a divisor of ', divisor))
+        html_p_text(parent, 'pay attention to the remainder')
+        html_p_text(parent, 'here are what the letters mean :')
         let ul = html_ul(parent);
         html_li_text(ul, 'a is the dividend')
         html_li_text(ul,'b is the divisor')
