f7c7ed6c683c19134883df763339f8e4005c723f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 18:08:13 -0400

    2024-05-06T22:08:13.197Z  noop

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,8 +1,12 @@
 import { functions_source } from "./functions_source.mjs";
+import { html_document_body } from "./html_document_body.mjs";
+import { html_inner_set } from "./html_inner_set.mjs";
 import { http_post } from "./http_post.mjs";
 import { server_port } from "./server_port.mjs";
 
 export async function app_code() {
+    let root = html_document_body()
     let result = function_run(functions_source.name, [])
     console.log({result})
+    html_inner_set(root, result)
 }
