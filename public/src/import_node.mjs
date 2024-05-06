ba8ee37a6d30f03f66ae39c6d21dbf8e26036ce0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 18:13:56 -0400

    2024-05-06T22:13:55.538Z  noop

--- public/src/import_node.mjs
+++ public/src/import_node.mjs
@@ -1,9 +1,7 @@
-export async function import_multi(libary_to_import, variable_to_eval) {
+export async function import_node(libary_to_import, variable_to_eval) {
     let a;
     if (typeof window === 'undefined') {
         a = await import(libary_to_import);
-    } else {
-        a = eval(variable_to_eval);
     }
     return a;
 }
