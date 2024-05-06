7b481329f8f61054e74a7caced618816e18cb7c7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 15:01:11 -0400

    2024-05-06T19:01:08.834Z  noop

--- public/src/js_imports_existing_named.mjs
+++ public/src/js_imports_existing_named.mjs
@@ -1,12 +1,14 @@
 import {list_adder} from "./list_adder.mjs";
 import {js_visit_node} from "./js_visit_node.mjs";
-export function js_imports_existing(ast) {
+import { equal } from "./equal.mjs";
+export function js_imports_existing_named(ast, name) {
     let existing = list_adder(la => {
         js_visit_node(ast, 'ImportSpecifier', v => {
             let {node} = v;
             let {imported} = node;
             if (imported.type === 'Identifier') {
-                la(imported.name);
+                if (equal(imported.name, name))
+                la(v);
             }
         });
     });
