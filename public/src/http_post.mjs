122d9c57af8a75dffae658f9c48f5088e08e6129
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 16:12:19 -0400

    2024-05-06T20:12:18.019Z  function_transform js_outside_move http_post

--- public/src/http_post.mjs
+++ public/src/http_post.mjs
@@ -1,6 +1,4 @@
 import {http_data_get} from "./http_data_get.mjs";
-import {equal} from "./equal.mjs";
-import {assert} from "./assert.mjs";
 export async function http_post(url, body) {
     let result = await axios.post(url, body);
     return http_data_get(result);
