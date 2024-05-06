90769f6cb215cbca14a286c51020580a9afc579b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 18:14:09 -0400

    2024-05-06T22:14:08.804Z  noop

--- public/src/import_node.mjs
+++ public/src/import_node.mjs
@@ -1,4 +1,4 @@
-export async function import_node(libary_to_import, variable_to_eval) {
+export async function import_node(libary_to_import) {
     let a;
     if (typeof window === 'undefined') {
         a = await import(libary_to_import);
