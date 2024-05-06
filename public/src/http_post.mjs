533401d869dca08e9d975322b3a94b4488ddd2e5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 16:09:09 -0400

    2024-05-06T20:09:08.102Z  function_copy http_get http_post

---
+++ public/src/http_post.mjs
@@ -0,0 +1,8 @@
+import {equal} from "./equal.mjs";
+import {assert} from "./assert.mjs";
+export async function http_get(url) {
+    let result = await axios.get(url);
+    let {status, data} = result;
+    assert(equal, [status, 200]);
+    return data;
+}
