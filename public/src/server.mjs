030f4b5aac79ad6dfd7299f1a90798d1fa254ca2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 16:08:42 -0400

    2024-05-06T20:08:41.566Z  noop

--- public/src/server.mjs
+++ public/src/server.mjs
@@ -2,17 +2,11 @@ import {server_port} from "./server_port.mjs";
 import http from 'http';
 export function server() {
     let server = http.createServer(function (request, response) {
+        let read = request.read();
         response.writeHead(200, {
             "Content-Type": "application/json"
         });
-        response.write()
-        if (request.method == "GET") {
-            response.end("received GET request.");
-        } else if (request.method == "POST") {
-            response.end("received POST request.");
-        } else {
-            response.end("Undefined request .");
-        }
+        res.end(read);
     });
     server.listen(server_port());
     console.log("Server running on port 8000");
