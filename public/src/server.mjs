90a10f4669ec635269f12f9366860ad4c37dc572
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 16:31:12 -0400

    2024-05-06T20:31:09.727Z  noop

--- public/src/server.mjs
+++ public/src/server.mjs
@@ -1,15 +1,26 @@
 import {server_port} from "./server_port.mjs";
-import http from 'http';
+import express from 'express';
 export function server() {
-    let server = http.createServer(function (request, response) {
-        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-        response.writeHead(200, {
-            "Content-Type": "application/json",
-            'Access-Control-Allow-Origin': '*'
-        });
-        console.log({request})
-        response.end(read);
-    });
-    server.listen(server_port());
-    console.log("server running on port 8000");
+    const app = express()
+    const port = server_port()
+    
+    app.get('/', (req, res) => {
+      res.send('Hello World!')
+    })
+    
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
+
+    // let server = http.createServer(function (request, response) {
+    //     response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    //     response.writeHead(200, {
+    //         "Content-Type": "application/json",
+    //         'Access-Control-Allow-Origin': '*'
+    //     });
+    //     console.log({request})
+    //     response.end(read);
+    // });
+    // server.listen();
+    // console.log("server running on port 8000");
 }
