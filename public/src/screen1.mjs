1bbf1d00004f3867b5c60f7563298ef1ebc278e2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 14:30:01 -0400

    2024-05-06T18:30:00.435Z  noop

--- public/src/screen1.mjs
+++
@@ -1,8 +0,0 @@
-import {html_style_alternate_bold} from "./html_style_alternate_bold.mjs";
-import {html_p_text} from "./html_p_text.mjs";
-export function screen1(parent) {
-    html_p_text(parent, 'here is an example of division in math : a / b');
-    html_style_alternate_bold(parent, ['when a number ( like "a" ) is divided by another number ( like "b" ) , the first number is called the ', 'dividend']);
-    html_style_alternate_bold(parent, ['the second number is called the ', 'divisor']);
-    html_p_text(parent, 'for example in 7 / 4 the dividend is 7 and the divisor is 4');
-}
--- public/src/screen2.mjs
+++
@@ -1,8 +0,0 @@
-import {html_p_text} from "./html_p_text.mjs";
-export function screen2(parent) {
-    html_p_text(parent, 'after the divisor divides the dividend , there is a quotient and a remainder');
-    html_p_text(parent, 'the following formula will give the answer to the division with a decimal point :');
-    html_p_text(parent, 'quotient + ( remainder / divisor )');
-    html_p_text(parent, 'for example 7 / 4 has a quotient of 1 and a remainder of 3');
-    html_p_text(parent, '1 + ( 3 / 4 ) = 1 + 0.75 = 1.75');
-}
