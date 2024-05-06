8510e39ff9e5393bd693746c64053e3693339652
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 17:16:45 -0400

    2024-05-06T21:16:42.214Z  function_open function_run

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -2,8 +2,10 @@ import { http_post } from "./http_post.mjs";
 import { server_port } from "./server_port.mjs";
 
 export async function app_code() {
+    const function_name = 'functions_source';
+    const args = [];
     let result = await http_post(
         `http://localhost:${server_port()}/`, 
-        { function_name: 'functions_source', args: [] })
+        { function_name: function_name, args: args })
     console.log({result})
 }
