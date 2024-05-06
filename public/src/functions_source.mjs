02778c9aaa3f869f9eb3fb7ce31b813e9c11d37b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 16:56:15 -0400

    2024-05-06T20:56:14.222Z  function_search file

--- public/src/functions_source.mjs
+++ public/src/functions_source.mjs
@@ -1,6 +1,8 @@
 import { function_names } from "./function_names.mjs";
+import { list_map } from "./list_map.mjs";
 
 export async function functions_source() {
     let names = await function_names()
+    let mapped = list_map(names, )
     return names;
 }
