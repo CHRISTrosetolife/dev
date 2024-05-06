4648b00f8f9fcf3cbbd96e2e03de9d2059119004
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 15:05:35 -0400

    2024-05-06T19:05:33.660Z  function_imports_add js_imports_remove

--- public/src/js_imports_remove.mjs
+++ public/src/js_imports_remove.mjs
@@ -1,9 +1,10 @@
+import {js_imports_existing_named} from "./js_imports_existing_named.mjs";
 import {list_difference} from "./list_difference.mjs";
 import {js_identifiers} from "./js_identifiers.mjs";
 import {js_export_single} from "./js_export_single.mjs";
 import {js_imports_existing} from "./js_imports_existing.mjs";
 import {noop} from "./noop.mjs";
-import { assert } from "./assert.mjs";
+import {assert} from "./assert.mjs";
 export function js_imports_remove(ast) {
     let existing = js_imports_existing(ast);
     let export_ = js_export_single(ast);
@@ -11,7 +12,7 @@ export function js_imports_remove(ast) {
     let extra = list_difference(existing, used);
     for (let e of extra) {
         let existing2 = js_imports_existing_named(ast, e);
-        assert(list_length_1,[existing]);
+        assert(list_length_1, [existing]);
     }
     return {
         existing,
