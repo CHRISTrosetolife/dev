2828c11b6b1b29f69e560af060748d67384d0023
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 17:17:42 -0400

    2024-05-06T21:17:40.833Z  function_transform js_imports_fix function_run

--- public/src/function_run.mjs
+++ public/src/function_run.mjs
@@ -1,6 +1,16 @@
+import {server_port} from "./server_port.mjs";
+import {http_post} from "./http_post.mjs";
 import {function_import} from './function_import.mjs';
 export async function function_run(function_name, args) {
-    let imported_function = await function_import(function_name);
-    let result = await imported_function(...args);
+    let result;
+    if (typeof document === 'undefined') {
+        let imported_function = await function_import(function_name);
+        result = await imported_function(...args);
+    } else {
+        result = await http_post(`http://localhost:${server_port()}/`, {
+            function_name,
+            args
+        });
+    }
     return result;
 }
