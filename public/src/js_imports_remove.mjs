9581449c76274c8ab6829b9b6c8f9509603bd805
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 15:11:44 -0400

    2024-05-06T19:11:42.489Z  function_transform js_outside_move js_imports_remove

--- public/src/js_imports_remove.mjs
+++ public/src/js_imports_remove.mjs
@@ -1,3 +1,4 @@
+import {list_get_end} from "./list_get_end.mjs";
 import {list_single} from "./list_single.mjs";
 import {list_length_1} from "./list_length_1.mjs";
 import {js_imports_existing_named} from "./js_imports_existing_named.mjs";
@@ -8,16 +9,18 @@ import {js_imports_existing} from "./js_imports_existing.mjs";
 import {noop} from "./noop.mjs";
 import {assert} from "./assert.mjs";
 import {list_length} from "./list_length.mjs";
-import { list_remove } from "./list_remove.mjs";
+import {list_remove} from "./list_remove.mjs";
 export function js_imports_remove(ast) {
     let existing = js_imports_existing(ast);
     let export_ = js_export_single(ast);
     let used = js_identifiers(export_);
     let extra = list_difference(existing, used);
-    let {body} = ast
+    let {body} = ast;
     for (let e of extra) {
         let existing2 = js_imports_existing_named(ast, e);
         let e2 = list_single(existing2);
-        list_remove(body,e2)
+        console.log(e2);
+        list_remove(body, e2);
+        list_get_end();
     }
 }
