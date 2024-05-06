2d2e20d725ac162d8e95589147909c9c1920566d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 14:31:00 -0400

    2024-05-06T18:30:58.274Z  function_imports_add js_imports_remove

--- public/src/js_imports_remove.mjs
+++ public/src/js_imports_remove.mjs
@@ -1,5 +1,7 @@
+import {js_export_single} from "./js_export_single.mjs";
 import {js_imports_existing} from "./js_imports_existing.mjs";
 export function js_imports_remove(ast) {
     let existing = js_imports_existing(ast);
+    let e = js_export_single(ast);
     return existing;
 }
