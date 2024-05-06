3b0b57529ed8e02c2218b413165ada26c095cd2e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-05 20:41:21 -0400

    2024-05-06T00:41:20.837Z  noop

--- public/src/lesson_if_variable.mjs
+++ public/src/lesson_if_variable.mjs
@@ -86,7 +86,6 @@ export function lesson_if_variable() {
         let operator = list_random_item(operators);
         let operators_equality = ['===', '!=='];
         let values = call_multiple(integer_random_digit_single_positive, 2);
-        if (false)list_sort(values, identity);
         let [a, b] = values;
         if (list_includes(operators_equality, operator) && random_50_50()) {
             b = a;
