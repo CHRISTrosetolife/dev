2d5c69a13262c369db0e714e70d2719f763d413d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 18:05:58 -0400

    2024-05-06T22:05:56.459Z  function_imports_add server

--- public/src/server.mjs
+++ public/src/server.mjs
@@ -1,9 +1,12 @@
+import {command_line} from "./command_line.mjs";
 import {json_to} from "./json_to.mjs";
 import {function_run} from "./function_run.mjs";
 import {json_from} from "./json_from.mjs";
 import {server_port} from "./server_port.mjs";
 import express from 'express';
 import cors from 'cors';
+import {function_run_json} from "./function_run_json.mjs";
+import {string_replace} from "./string_replace.mjs";
 export function server() {
     let app = express();
     let port = server_port();
@@ -18,9 +21,10 @@ export function server() {
             body
         });
         let {function_name, args} = body;
-        let result = await function_run(function_name, args);
+        let args_json = json_to(args);
+        let replaced = string_replace(args_json, '\'', '\'\'');
+        let result = await command_line(`node run_git.mjs ${function_run_json.name} ${function_name} '${replaced}`);
         res.end(json_to(result));
-        command_line(`node run_git.mjs ${function_name}`)
     });
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`);
