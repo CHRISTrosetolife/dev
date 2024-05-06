281e37629e9f35cda0f9943746a2c567334a3f4e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 16:50:00 -0400

    2024-05-06T20:49:59.491Z  noop

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -2,5 +2,5 @@ import { http_post } from "./http_post.mjs";
 import { server_port } from "./server_port.mjs";
 
 export async function app_code() {
-    await http_post(`http://localhost:${server_port()}/`, {test:123})
+    await http_post(`http://localhost:${server_port()}/`, { function_name: functions_source, args: [] })
 }
