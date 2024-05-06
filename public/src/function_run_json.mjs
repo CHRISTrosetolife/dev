e26811e8caa7231df76b5bfb2b8a965af78b2cf6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 18:29:37 -0400

    2024-05-06T22:29:36.135Z  function_imports_add function_run_json

--- public/src/function_run_json.mjs
+++ public/src/function_run_json.mjs
@@ -1,9 +1,9 @@
-import { function_run } from "./function_run.mjs";
-import { json_from } from "./json_from.mjs";
-import { json_to } from "./json_to.mjs";
-
+import {file_overwrite} from "./file_overwrite.mjs";
+import {function_run} from "./function_run.mjs";
+import {json_from} from "./json_from.mjs";
+import {json_to} from "./json_to.mjs";
 export async function function_run_json(function_name, args_json_string) {
-    let {args} = json_from(args_json_string)
-    await file_overwrite('log.txt', args_json_string)
-    return await function_run(function_name,args)
+    await file_overwrite('log.txt', args_json_string);
+    let {args} = json_from(args_json_string);
+    return await function_run(function_name, args);
 }
