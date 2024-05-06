68fc6cf9bcf34abc6ad22e2d7b09723a68adb23a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 18:28:50 -0400

    2024-05-06T22:28:49.118Z  server

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -3,6 +3,8 @@ import {functions_source} from "./functions_source.mjs";
 import {html_document_body} from "./html_document_body.mjs";
 import {html_inner_set} from "./html_inner_set.mjs";
 import {http_post} from "./http_post.mjs";
+import { json_from } from "./json_from.mjs";
+import { json_to } from "./json_to.mjs";
 import {server_port} from "./server_port.mjs";
 export async function app_code() {
     let root = html_document_body();
@@ -10,5 +12,5 @@ export async function app_code() {
     console.log({
         result
     });
-    html_inner_set(root, result);
+    html_inner_set(root, json_to(result));
 }
--- public/src/function_run_json.mjs
+++ public/src/function_run_json.mjs
@@ -1,7 +1,9 @@
 import { function_run } from "./function_run.mjs";
 import { json_from } from "./json_from.mjs";
+import { json_to } from "./json_to.mjs";
 
 export async function function_run_json(function_name, args_json_string) {
     let {args} = json_from(args_json_string)
+    await file_overwrite('log.txt', args_json_string)
     return await function_run(function_name,args)
 }
