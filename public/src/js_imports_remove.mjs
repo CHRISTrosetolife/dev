da2796f0298efa8019909539ce7d004eb40f0458
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 14:36:44 -0400

    2024-05-06T18:36:43.205Z  noop

--- public/src/js_imports_remove.mjs
+++ public/src/js_imports_remove.mjs
@@ -3,5 +3,6 @@ import {js_imports_existing} from "./js_imports_existing.mjs";
 export function js_imports_remove(ast) {
     let existing = js_imports_existing(ast);
     let e = js_export_single(ast);
+    let used = js_identifiers(ast);
     return e;
 }
