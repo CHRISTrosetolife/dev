05e5fac568165c7c8dc3808ce7c0cebaa6da94c7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 16:48:51 -0400

    2024-05-06T20:48:49.932Z  noop

--- public/src/functions_source.mjs
+++ public/src/functions_source.mjs
@@ -1,1 +1,5 @@
-export function functions_source() {}
+import { function_names } from "./function_names.mjs";
+
+export async function functions_source() {
+    await names = await function_names()
+}
