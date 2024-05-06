feb5e00885374744c9e80871079a6b0928954af9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 07:48:46 -0400

    2024-05-06T11:48:45.159Z  noop

--- public/src/lesson_mod.mjs
+++ public/src/lesson_mod.mjs
@@ -116,7 +116,7 @@ export function lesson_mod() {
     }
     function screen2(parent) {
         html_p_text(parent, 'after the divisor divides the dividend , there is a quotient and a remainder');
-        html_p_text(parent, 'this formula will give the answer with a decimal point : quotient + ( remainder / divisor )');
+        html_p_text(parent, 'this formula will give the answer to the division with a decimal point : quotient + ( remainder / divisor )');
         html_p_text(parent, 'for example 7 / 4 has a quotient of 1 and a remainder of 3');
         html_p_text(parent, '1 + ( 3 / 4 ) = 1 + 0.75 = 1.75');
     }
