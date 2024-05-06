cb8bc033bd2cad0a86452c7ead61ad82924de0fd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 16:24:14 -0400

    2024-05-06T20:24:13.446Z  server

--- public/src/server.mjs
+++ public/src/server.mjs
@@ -2,6 +2,7 @@ import {server_port} from "./server_port.mjs";
 import http from 'http';
 export function server() {
     let server = http.createServer(function (request, response) {
+        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         let read = request.read();
         response.writeHead(200, {
             "Content-Type": "application/json",
