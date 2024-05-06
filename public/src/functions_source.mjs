fd8b0edb5e148bfa2eb37a16c5e1e1faab7753b1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 16:48:58 -0400

    2024-05-06T20:48:57.014Z  noop

--- public/src/functions_source.mjs
+++ public/src/functions_source.mjs
@@ -1,5 +1,5 @@
 import { function_names } from "./function_names.mjs";
 
 export async function functions_source() {
-    await names = await function_names()
+    let names = await function_names()
 }
