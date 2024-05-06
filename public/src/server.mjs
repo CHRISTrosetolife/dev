e83237985ecf5770a1ebb39bdd5ebbc7bc3d15fa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 16:44:54 -0400

    2024-05-06T20:44:53.036Z  function_transform js_imports_fix server

--- public/src/server.mjs
+++ public/src/server.mjs
@@ -1,36 +1,20 @@
+import {json_from} from "./json_from.mjs";
 import {server_port} from "./server_port.mjs";
 import express from 'express';
 import cors from 'cors';
 export function server() {
-    const app = express()
-    const port = server_port()
-
-    let corsOptions = { 
-        origin : ['http://localhost:8080'], 
-     } 
-       
-     app.use(cors(corsOptions)) 
-     app.use(express.json());
-     
+    const app = express();
+    const port = server_port();
+    let corsOptions = {
+        origin: ['http://localhost:8080']
+    };
+    app.use(cors(corsOptions));
+    app.use(express.json());
     app.post('/', async (req, res) => {
-        let json = req.body
-        console.log(json)
-      res.send(json)
-    })
-    
+        let {body} = req;
+        let unparsed = json_from(body);
+    });
     app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
-    })
-
-    // let server = http.createServer(function (request, response) {
-    //     response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    //     response.writeHead(200, {
-    //         "Content-Type": "application/json",
-    //         'Access-Control-Allow-Origin': '*'
-    //     });
-    //     console.log({request})
-    //     response.end(read);
-    // });
-    // server.listen();
-    // console.log("server running on port 8000");
+        console.log(`Example app listening on port ${port}`);
+    });
 }
