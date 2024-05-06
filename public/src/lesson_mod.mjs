264ee42de6044fd170aa6e8c61740b8915c86105
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 08:26:40 -0400

    2024-05-06T12:26:36.535Z  function_transform js_outside_move lesson_mod

--- public/src/lesson_mod.mjs
+++ public/src/lesson_mod.mjs
@@ -1,3 +1,7 @@
+import {mod} from "./mod.mjs";
+import {screen3} from "./screen3.mjs";
+import {screen2} from "./screen2.mjs";
+import {screen1} from "./screen1.mjs";
 import {floor} from "./floor.mjs";
 import {app_learn_code_log_delimit} from "./app_learn_code_log_delimit.mjs";
 import {list_join_newline} from "./list_join_newline.mjs";
@@ -85,12 +89,12 @@ import {lesson_console_log_quiz} from "./lesson_console_log_quiz.mjs";
 import {list_map} from "./list_map.mjs";
 import {html_element} from "./html_element.mjs";
 import {html_inner_set} from "./html_inner_set.mjs";
-import { html_style_width_full } from "./html_style_width_full.mjs";
-import { html_style_centered } from "./html_style_centered.mjs";
-import { app_learn_code_style_rounded_padded } from "./app_learn_code_style_rounded_padded.mjs";
-import { list_index_last } from "./list_index_last.mjs";
-import { html_style_background_color } from "./html_style_background_color.mjs";
-import { each_index } from "./each_index.mjs";
+import {html_style_width_full} from "./html_style_width_full.mjs";
+import {html_style_centered} from "./html_style_centered.mjs";
+import {app_learn_code_style_rounded_padded} from "./app_learn_code_style_rounded_padded.mjs";
+import {list_index_last} from "./list_index_last.mjs";
+import {html_style_background_color} from "./html_style_background_color.mjs";
+import {each_index} from "./each_index.mjs";
 export function lesson_mod() {
     let description = 'remainder ( % ) 2';
     let example_message = 'outputting the remainder of a number with a divisor of 2';
@@ -100,13 +104,12 @@ export function lesson_mod() {
         return list_join_newline(lines);
     }, example_before, description, example_message, [screen1, screen2, screen3]);
     function example_before(parent) {
-        html_style_alternate_monospace(parent, ['in javascript , to get the remainder of ','dividend',' use ','%',' like this : ','dividend % divisor'])
+        html_style_alternate_monospace(parent, ['in javascript , to get the remainder of ', 'dividend', ' use ', '%', ' like this : ', 'dividend % divisor']);
     }
-}
     function screen1(parent) {
         html_p_text(parent, 'here is an example of division in math : a / b');
-        html_style_alternate_bold(parent, ['when a number ( like "a" ) is divided by another number ( like "b" ) , the first number is called the ','dividend']);
-        html_style_alternate_bold(parent, ['the second number is called the ','divisor']);
+        html_style_alternate_bold(parent, ['when a number ( like "a" ) is divided by another number ( like "b" ) , the first number is called the ', 'dividend']);
+        html_style_alternate_bold(parent, ['the second number is called the ', 'divisor']);
         html_p_text(parent, 'for example in 7 / 4 the dividend is 7 and the divisor is 4');
     }
     function screen2(parent) {
@@ -118,61 +121,58 @@ export function lesson_mod() {
     }
     function screen3(parent) {
         let divisor = 2;
-        html_p_text(parent, string_combine('in the table below are some quotient and remainders for a divisor of ', divisor))
-        html_p_text(parent, 'pay attention to the remainder')
-        html_p_text(parent, 'here is what the letters mean :')
+        html_p_text(parent, string_combine('in the table below are some quotient and remainders for a divisor of ', divisor));
+        html_p_text(parent, 'pay attention to the remainder');
+        html_p_text(parent, 'here is what the letters mean :');
         let ul = html_ul(parent);
-        html_li_text(ul, 'a is the dividend')
-        html_li_text(ul,'b is the divisor')
-        html_li_text(ul,'q is the quotient')
-        html_li_text(ul,'r is the remainder')
+        html_li_text(ul, 'a is the dividend');
+        html_li_text(ul, 'b is the divisor');
+        html_li_text(ul, 'q is the quotient');
+        html_li_text(ul, 'r is the remainder');
         let table = html_element(parent, 'table');
-        html_style_width_full(table)
+        html_style_width_full(table);
         let thead = html_element(table, 'thead');
         let tr_head = html_element(thead, 'tr');
         let columns_head = ['a', 'b', 'a / b', 'q', 'r'];
         each_index(columns_head, (c, c_index) => {
             let th = html_element(tr_head, 'th');
             html_inner_set(th, c);
-            let index_last = list_index_last(columns_head)
-            app_learn_code_style_rounded_padded(th)
+            let index_last = list_index_last(columns_head);
+            app_learn_code_style_rounded_padded(th);
             let color;
             if (equal(c_index, index_last)) {
-                color = '#FFEB3B'
+                color = '#FFEB3B';
             } else {
-                color = '#29B6F6'
+                color = '#29B6F6';
             }
-            html_style_background_color(th, color)
-        })
+            html_style_background_color(th, color);
+        });
         let tbody = html_element(table, 'tbody');
         each_index(range(11), (n, n_index) => {
             let even = equal(mod(n_index, divisor), 0);
             let tr_body = html_element(tbody, 'tr');
             let quotient = n / divisor;
             let columns_body = [n, divisor, quotient, floor(quotient), n % divisor];
-            let index_last = list_index_last(columns_body)
+            let index_last = list_index_last(columns_body);
             each_index(columns_body, (c, c_index) => {
                 let divisor = 2;
                 let td = html_element(tr_body, 'td');
                 html_inner_set(td, c);
-                html_style_centered(td)
-                app_learn_code_style_rounded_padded(td)
+                html_style_centered(td);
+                app_learn_code_style_rounded_padded(td);
                 let color;
                 if (equal(c_index, index_last)) {
                     color = even ? '#F9E79F' : '#F7DC6F';
                 } else {
                     color = even ? '#D6EAF8' : '#AED6F1';
                 }
-                html_style_background_color(td, color)
-            })
-        })
-        html_hr(parent)
-        html_p_text(parent, 'did you notice how the remainder alternates between 0 and 1 ?')
-        html_p_text(parent, 'did you notice that when the number is even the remainder is 0 ?')
-        html_p_text(parent, 'did you notice that when the number is odd the remainder is 1 ?')
-        html_p_text(parent, 'the remainder can be used to check whether or not a number is even or odd')
+                html_style_background_color(td, color);
+            });
+        });
+        html_hr(parent);
+        html_p_text(parent, 'did you notice how the remainder alternates between 0 and 1 ?');
+        html_p_text(parent, 'did you notice that when the number is even the remainder is 0 ?');
+        html_p_text(parent, 'did you notice that when the number is odd the remainder is 1 ?');
+        html_p_text(parent, 'the remainder can be used to check whether a number is even or odd');
     }
-function mod(index, divisor) {
-    return index % divisor;
 }
-
