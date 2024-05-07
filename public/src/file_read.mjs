e731f48b90ebce742d98b908895bdcbb252c8772
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 15:51:24 -0400

    2024-05-07T19:51:24.255Z  noop

--- public/src/file_read.mjs
+++ public/src/file_read.mjs
@@ -1,4 +1,11 @@
-import {import_node} from './import_node.mjs';let fs = await import_node('fs');
+import { global_get } from './global_get.mjs';
+import {import_node} from './import_node.mjs';import { object_property_get } from './object_property_get.mjs';
+import { web_is } from './web_is.mjs';
+let fs = await import_node('fs');
 export async function file_read(file_name) {
+    if (web_is()) {
+        let {files} = global_get()
+        return object_property_get(files, file_name)
+    }
     return await fs.promises.readFile(file_name, 'utf-8');
 }
