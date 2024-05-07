6757e5738f16ac62a9fbbbf2348efc66982ed5c9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 07:55:39 -0400

    2024-05-07T11:55:39.169Z  noop

--- public/src/html_style_button_default_border.mjs
+++ public/src/html_style_button_default_border.mjs
@@ -3,6 +3,6 @@ import {string_combine} from "./string_combine.mjs";
 export function html_style_button_default_border() {
     let color = html_style_button_default_border_color();
     return {
-        'border': string_combine('solid 0.5vh ', color)
+        'border': string_combine('solid 0.5dvh ', color)
     };
 }
