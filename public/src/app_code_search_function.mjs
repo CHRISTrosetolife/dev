48a09a3245cf89389478d3a84fad98371dbeba66
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 22:58:43 -0400

    2024-05-07T02:58:42.934Z  noop

--- public/src/app_code_search_function.mjs
+++ public/src/app_code_search_function.mjs
@@ -9,9 +9,11 @@ import {html_style_width_full} from "./html_style_width_full.mjs";
 import {html_focus} from "./html_focus.mjs";
 import {html_element} from "./html_element.mjs";
 import {html_document_body} from "./html_document_body.mjs";
+import { html_clear } from "./html_clear.mjs";
 export function app_code_search_function(context) {
     let {files} = context;
     let root = html_document_body();
+    html_clear(root)
     let input = html_element(root, 'input');
     html_focus(input);
     html_style_width_full(input);
@@ -20,8 +22,13 @@ export function app_code_search_function(context) {
     let paths = object_properties(files);
     for (let p of paths) {
         let name = function_path_to_name(p);
-        let b = html_button_width_full_text_click(root, name, () => {});
+        let b = html_button_width_full_text_click(root, name, () => {
+            app_code_edit(context)
+        });
         html_style_margin_x(b, '0');
         html_style_word_break_all(b);
     }
+}
+function app_code_edit(context) {
+
 }
