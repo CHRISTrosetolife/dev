61ff11b261522ecc36679346200e30985e75b526
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 13:10:30 -0400

    2024-05-07T17:10:30.383Z  function_transform js_imports_fix functions_source_set

--- public/src/each_object_async.mjs
+++ public/src/each_object_async.mjs
@@ -1,9 +1,9 @@
 import {object_property_get} from "./object_property_get.mjs";
 import {object_properties} from "./object_properties.mjs";
-export function each_object(object, lambda) {
+export async function each_object_async(object, lambda) {
     let keys = object_properties(object);
     for (let key of keys) {
         let value = object_property_get(object, key);
-        lambda(key, value);
+        await lambda(key, value);
     }
 }
--- public/src/functions_source_set.mjs
+++ public/src/functions_source_set.mjs
@@ -1,10 +1,10 @@
+import {each_object_async} from "./each_object_async.mjs";
 import {object_property_get} from "./object_property_get.mjs";
-import {each_object} from "./each_object.mjs";
 import {equal_not} from "./equal_not.mjs";
 import {function_contents} from "./function_contents.mjs";
 export async function functions_source_set(contents) {
     let existing = await function_contents();
-    each_object(contents, (file_path, contents_new) => {
+    each_object_async(contents, (file_path, contents_new) => {
         let contents_existing = object_property_get(existing, file_path);
         if (equal_not(contents_new, contents_existing)) {}
     });
