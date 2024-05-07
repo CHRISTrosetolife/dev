978e1f050713ced5b0bb664d9dd0bd8403d73183
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 07:45:29 -0400

    2024-05-07T11:45:29.042Z  noop

--- public/src/app_code_edit.mjs
+++ public/src/app_code_edit.mjs
@@ -3,14 +3,12 @@ import {html_div} from "./html_div.mjs";
 import {html_document_body_clear} from "./html_document_body_clear.mjs";
 import {js_parse} from "./js_parse.mjs";
 import {object_property_get} from "./object_property_get.mjs";
+import { html_inner_set } from "./html_inner_set.mjs";
 export function app_code_edit(context, file_path) {
     let root = html_document_body_clear();
     let container = html_div(root);
     let {files} = context;
     let contents = object_property_get(files, file_path);
     let ast = js_parse(contents);
-    let d= js_declaration_single(ast);
-    console.log({
-        d
-    });
+    html_inner_set(container,ast.type)
 }
