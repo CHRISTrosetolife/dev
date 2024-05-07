954d93df2b50783f984fe6315f747243e812069e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 14:19:35 -0400

    2024-05-07T18:19:34.992Z  function_transform js_imports_fix server

--- public/src/server.mjs
+++ public/src/server.mjs
@@ -8,7 +8,7 @@ import {function_run_json} from "./function_run_json.mjs";
 import {string_replace} from "./string_replace.mjs";
 import {run} from "./run.mjs";
 import {file_overwrite} from "./file_overwrite.mjs";
-import { file_read } from "./file_read.mjs";
+import {file_read} from "./file_read.mjs";
 import bodyParser from "body-parser";
 export function server() {
     let app = express();
@@ -17,8 +17,13 @@ export function server() {
         origin: ['http://localhost:8080']
     };
     app.use(cors(options));
-    app.use(express.json({limit: '50mb'}));
-    app.use(bodyParser.urlencoded({limit: '50mb', extended:true}));
+    app.use(express.json({
+        limit: '50mb'
+    }));
+    app.use(bodyParser.urlencoded({
+        limit: '50mb',
+        extended: true
+    }));
     app.post('/', async (req, res) => {
         let {body} = req;
         let {function_name, args} = body;
@@ -30,13 +35,15 @@ export function server() {
         await uuid_file(async file_path_input => {
             await uuid_file(async file_path_output => {
                 await file_overwrite(file_path_input, args_json);
-            let command = `node ${run.name}.mjs ${function_run_json.name} ${function_name} ${file_path_input} ${file_path_output}`;
-            let r = await command_line(command);
-            console.log({r})
-            let contents = await file_read(file_path_output)
-            res.end(contents);
-        })
-    })
+                let command = `node ${run.name}.mjs ${function_run_json.name} ${function_name} ${file_path_input} ${file_path_output}`;
+                let r = await command_line(command);
+                console.log({
+                    r
+                });
+                let contents = await file_read(file_path_output);
+                res.end(contents);
+            });
+        });
     });
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`);
