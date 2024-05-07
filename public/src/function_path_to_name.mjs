fb24e7be6545d1aed4de5e2d2c708cb1ab03ba32
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 22:05:21 -0400

    2024-05-07T02:05:21.558Z  noop

--- public/src/function_path_to_name.mjs
+++ public/src/function_path_to_name.mjs
@@ -1,1 +1,9 @@
-export function function_path_to_name(function_path) {}
+import { function_path_prefix } from "./function_path_prefix.mjs";
+import { string_prefix_without } from "./string_prefix_without.mjs";
+import { string_suffix_without } from "./string_suffix_without.mjs";
+
+export function function_path_to_name(function_path) {
+    function_path = string_prefix_without(function_path, function_path_prefix())
+    function_path = string_suffix_without(function_path, string_suffix_without())
+    return function_path;
+}
