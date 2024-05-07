c11786263f10cc34e2ff69389daa63ac0c375541
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 20:14:15 -0400

    2024-05-07T00:14:13.948Z  noop

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,5 +1,6 @@
 import {function_run} from "./function_run.mjs";
 import {functions_source} from "./functions_source.mjs";
+import { html_button_width_full_text } from "./html_button_width_full_text.mjs";
 import {html_document_body} from "./html_document_body.mjs";
 import {html_inner_set} from "./html_inner_set.mjs";
 import { html_style_button_default_initialize } from "./html_style_button_default_initialize.mjs";
@@ -15,6 +16,6 @@ export async function app_code() {
     let result = await function_run(functions_source.name, []);
     let paths = object_properties(result);
     for (let p of paths) {
-        
+        html_button_width_full_text(root, p);
     }
 }
--- public/src/html_button_width_full_text.mjs
+++ public/src/html_button_width_full_text.mjs
@@ -1,7 +1,7 @@
 import {html_inner_set} from "./html_inner_set.mjs";
 import {html_button_width_full} from "./html_button_width_full.mjs";
-export function html_button_width_full_text(root, button_text) {
-    let button_previous = html_button_width_full(root);
+export function html_button_width_full_text(parent, button_text) {
+    let button_previous = html_button_width_full(parent);
     html_inner_set(button_previous, button_text);
     return button_previous;
 }
