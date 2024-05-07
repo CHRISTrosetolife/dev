684823299e4f20ca103ae8e931ce91dc2df4f9a4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 08:43:14 -0400

    2024-05-07T12:43:13.639Z  noop

--- public/src/app_code_edit_recursive.mjs
+++ public/src/app_code_edit_recursive.mjs
@@ -12,34 +12,31 @@ import { list_single } from "./list_single.mjs";
 import { equal } from "./equal.mjs";
 export function app_code_edit_recursive(node, parent) {
     let {type} = node;
-    switch (type) {
-        case 'Program':
-            let {body} = node;
-            for (let b of body) {
-                let child = html_div(parent);
-                app_learn_code_style_rounded_padded(child);
-                html_style_background_color(child, 'hsl(0, 0%, 20%)');
-                html_style_font_color(child, 'white');
-                html_style(child, {
-                    'margin': '0.3dvh'
-                });
-                app_code_edit_recursive(b, child);
-            }
-            break;
-        case 'ImportDeclaration':
-            html_style_word_break_all(parent);
-            let {specifiers} = node;
-            let specifier = list_single(specifiers);
-            let {local,imported} = specifier;
-            for (let a of [local,imported]) {
-                let {type} = a;
-                assert(equal, [type, 'Identifier'])
-            }
-            let {name:local_name} = local;
-            let {name:imported_name} = imported;
-            html_inner_set(parent, json_to(specifiers));
-            break;
-        default:
-            html_inner_set(parent, json_to(node));
+    if (equal(type, 'Program')) {
+        let {body} = node;
+        for (let b of body) {
+            let child = html_div(parent);
+            app_learn_code_style_rounded_padded(child);
+            html_style_background_color(child, 'hsl(0, 0%, 20%)');
+            html_style_font_color(child, 'white');
+            html_style(child, {
+                'margin': '0.3dvh'
+            });
+            app_code_edit_recursive(b, child);
+        }
+    } else if (equal(type, 'ImportDeclaration')) {
+        html_style_word_break_all(parent);
+        let {specifiers} = node;
+        let specifier = list_single(specifiers);
+        let {local,imported} = specifier;
+        for (let a of [local,imported]) {
+            let {type} = a;
+            assert(equal, [type, 'Identifier'])
+        }
+        let {name:local_name} = local;
+        let {name:imported_name} = imported;
+        html_inner_set(parent, json_to(specifiers));
+    } else {
+        html_inner_set(parent, json_to(node));
     }
 }
