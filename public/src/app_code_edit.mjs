53f3d5897aab6d56d113cdb3b54a6121022bde92
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 09:48:49 -0400

    2024-05-07T13:48:48.605Z  function_transform js_imports_fix app_code_edit

--- public/src/app_code_edit.mjs
+++ public/src/app_code_edit.mjs
@@ -1,3 +1,4 @@
+import {app_code_search_function} from "./app_code_search_function.mjs";
 import {html_spellcheck_none} from "./html_spellcheck_none.mjs";
 import {html_style_border_box} from "./html_style_border_box.mjs";
 import {html_style_height_full} from "./html_style_height_full.mjs";
@@ -7,20 +8,23 @@ import {html_element} from "./html_element.mjs";
 import {html_inner_set} from "./html_inner_set.mjs";
 import {html_style_width_full} from "./html_style_width_full.mjs";
 import {html_style} from "./html_style.mjs";
-import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
-import { html_button } from "./html_button.mjs";
-import { html_on_click } from "./html_on_click.mjs";
+import {html_button_width_full_text_click} from "./html_button_width_full_text_click.mjs";
+import {html_button} from "./html_button.mjs";
+import {html_on_click} from "./html_on_click.mjs";
 export function app_code_edit(context, file_path) {
     let root = html_document_body_clear();
     html_style(root, {
         'display': 'flex',
         'flex-direction': 'column'
-    })
-    let b = html_button(root)
-    html_inner_set(b, 'menu')
+    });
+    let b = html_button(root);
+    html_inner_set(b, 'menu');
     html_on_click(b, () => {
-
-    })
+        let root = html_document_body_clear();
+        html_button_width_full_text_click(root, 'search', () => {
+            app_code_search_function(context);
+        });
+    });
     let {files} = context;
     let contents = object_property_get(files, file_path);
     let ta = html_element(root, 'textarea');
