4410ec8447c2a994993a85a662f1129a2029c767
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 22:17:14 -0400

    2024-05-07T02:17:14.349Z  function_transform js_outside_move app_learn_code_code_part_generic

--- public/src/app_learn_code_code_part_generic.mjs
+++ public/src/app_learn_code_code_part_generic.mjs
@@ -1,14 +1,12 @@
-import {html_style} from "./html_style.mjs";
+import {html_style_word_break_all} from "./html_style_word_break_all.mjs";
 import {html_style_margin_none} from "./html_style_margin_none.mjs";
 import {app_learn_code_style_code} from "./app_learn_code_style_code.mjs";
-import { html_style_background_color } from "./html_style_background_color.mjs";
+import {html_style_background_color} from "./html_style_background_color.mjs";
 export function app_learn_code_code_part_generic(create, parent, source_code, background_color) {
     let code = create(parent, source_code);
     app_learn_code_style_code(code);
     html_style_margin_none(code);
-    html_style(code, {
-        'word-break': 'break-all'
-    });
-    html_style_background_color(background_color)
+    html_style_word_break_all(code);
+    html_style_background_color(background_color);
     return code;
 }
