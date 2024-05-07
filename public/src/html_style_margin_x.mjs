990946d74e852198b8cf62cae17c8b5e669ef04e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 22:36:59 -0400

    2024-05-07T02:36:58.836Z  noop

--- public/src/html_style_margin_x.mjs
+++ public/src/html_style_margin_x.mjs
@@ -1,5 +1,5 @@
 import {html_style} from "./html_style.mjs";
-export function html_style_margin_y(component, x_margin) {
+export function html_style_margin_x(component, x_margin) {
     html_style(component, {
         'margin-left': x_margin,
         'margin-right': x_margin
