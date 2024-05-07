07589e8c7aab4c158bd1aaad1a03632f9af80db2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 23:24:12 -0400

    2024-05-07T03:24:11.788Z  function_search js_export

--- public/src/app_code_edit.mjs
+++ public/src/app_code_edit.mjs
@@ -1,9 +1,11 @@
 import {html_div} from "./html_div.mjs";
 import {html_document_body_clear} from "./html_document_body_clear.mjs";
+import { js_parse } from "./js_parse.mjs";
 import {object_property_get} from "./object_property_get.mjs";
 export function app_code_edit(context, file_path) {
     let root = html_document_body_clear();
     let container = html_div(root);
     let {files} = context;
     let contents = object_property_get(files, file_path);
+    let parsed = js_parse(contents);
 }
