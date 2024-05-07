0dba22b1becb9a23f57ada39e76de4667ae4b71c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 15:53:50 -0400

    2024-05-07T19:53:50.464Z  function_auto function_auto

--- public/src/function_auto.mjs
+++ public/src/function_auto.mjs
@@ -1,8 +1,7 @@
-import {js_imports_fix} from "./js_imports_fix.mjs";
-import {js_outside_move} from "./js_outside_move.mjs";
+import {function_auto_transforms} from "./function_auto_transforms.mjs";
 import {function_transform} from "./function_transform.mjs";
 export async function function_auto(function_name) {
-    let transforms = [js_outside_move, js_imports_fix];
+    let transforms = function_auto_transforms();
     for (let t of transforms) {
         await function_transform(t.name, function_name);
     }
