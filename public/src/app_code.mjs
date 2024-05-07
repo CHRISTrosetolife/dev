4befae7ab271a270130097a01f3444df8bcbee98
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 22:11:01 -0400

    2024-05-07T02:11:00.939Z  noop

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -6,8 +6,8 @@ import {html_document_body} from "./html_document_body.mjs";
 import {html_style_default_initialize} from "./html_style_default_initialize.mjs";
 import {object_properties} from "./object_properties.mjs";
 export async function app_code() {
-    let root = html_document_body();
     html_style_default_initialize();
+    let root = html_document_body();
     let result = await function_run(functions_source.name, []);
     let paths = object_properties(result);
     for (let p of paths) {
