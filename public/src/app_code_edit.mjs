14ec941ee6089bc6bf69d7423c4cadc0e820a728
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 08:33:51 -0400

    2024-05-07T12:33:50.926Z  function_transform js_imports_fix app_code_edit

--- public/src/app_code_edit.mjs
+++ public/src/app_code_edit.mjs
@@ -1,14 +1,7 @@
-import {html_style_font_color} from "./html_style_font_color.mjs";
-import {html_style_margin_default_value} from "./html_style_margin_default_value.mjs";
-import {html_style_default_border_value} from "./html_style_default_border_value.mjs";
-import {app_learn_code_style_rounded_padded} from "./app_learn_code_style_rounded_padded.mjs";
-import {json_to} from "./json_to.mjs";
 import {html_div} from "./html_div.mjs";
 import {html_document_body_clear} from "./html_document_body_clear.mjs";
 import {js_parse} from "./js_parse.mjs";
 import {object_property_get} from "./object_property_get.mjs";
-import {html_inner_set} from "./html_inner_set.mjs";
-import {html_style} from "./html_style.mjs";
 import {html_style_background_color} from "./html_style_background_color.mjs";
 export function app_code_edit(context, file_path) {
     let root = html_document_body_clear();
@@ -24,7 +17,7 @@ export function app_code_edit(context, file_path) {
 function app_code_edit_recursive(node, parent) {
     let {type} = node;
     switch (type) {
-        case 'Program': 
+        case 'Program':
             let {body} = node;
             for (let b of body) {
                 let child = html_div(parent);
@@ -32,12 +25,13 @@ function app_code_edit_recursive(node, parent) {
                 html_style_background_color(child, 'hsl(0, 0%, 20%)');
                 html_style_font_color(child, 'white');
                 html_style(child, {
-                    'margin':'0.3dvh'
+                    'margin': '0.3dvh'
                 });
                 app_code_edit_recursive(b, child);
             }
             break;
         case 'ImportDeclaration':
+            html_style_word_break_all(parent);
             let {specifiers} = node;
             html_inner_set(parent, json_to(specifiers));
             break;
