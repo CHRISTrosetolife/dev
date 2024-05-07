0356cda2ecef0b0209f7a27325fcf84d2a24ed0b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 23:29:41 -0400

    2024-05-07T03:29:41.273Z  function_transform js_imports_fix app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,4 +1,7 @@
+import {app_code_edit} from "./app_code_edit.mjs";
+import {html_hash} from "./html_hash.mjs";
 import {app_code_search_function} from "./app_code_search_function.mjs";
+import {function_name_to_path} from "./function_name_to_path.mjs";
 import {function_run} from "./function_run.mjs";
 import {functions_source} from "./functions_source.mjs";
 import {html_style_default_initialize} from "./html_style_default_initialize.mjs";
@@ -8,5 +11,11 @@ export async function app_code() {
     let context = {
         files
     };
+    html_hash({
+        'function_name': function_name => {
+            let p = function_name_to_path(function_name);
+            app_code_edit(context, p);
+        }
+    });
     app_code_search_function(context);
 }
