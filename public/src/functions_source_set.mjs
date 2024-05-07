93529552576c3495463c58a493b6df2c2b1d786e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 13:09:41 -0400

    2024-05-07T17:09:41.213Z  noop

--- public/src/functions_source_set.mjs
+++ public/src/functions_source_set.mjs
@@ -1,11 +1,13 @@
+import { each_object } from "./each_object.mjs";
+import { equal_not } from "./equal_not.mjs";
 import {function_contents} from "./function_contents.mjs";
 import {object_property_set} from "./object_property_set.mjs";
-export async function functions_source_get() {
-    let mapped = await function_contents();
-    let result = {};
-    for (let fc of mapped) {
-        let {file_path, contents} = fc;
-        object_property_set(result, file_path, contents);
-    }
-    return result;
+export async function functions_source_set(contents) {
+    let existing = await function_contents();
+    each_object(contents, (file_path, contents_new) => {
+        let contents_existing = object_property_get(existing, file_path);
+        if (equal_not(contents_new, contents_existing)) {
+
+        }
+    })
 }
