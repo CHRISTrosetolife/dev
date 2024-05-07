8af012e5903f675821e0cd0821bcd94dd570d5a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 12:14:26 -0400

    2024-05-07T16:14:25.813Z  noop

--- public/src/file_exists.mjs
+++ public/src/file_exists.mjs
@@ -1,10 +1,14 @@
 import {global_get} from "./global_get.mjs";
 import {web_is} from './web_is.mjs';
 import {import_node} from './import_node.mjs';
+import { object_properties } from "./object_properties.mjs";
+import { list_includes } from "./list_includes.mjs";
 let fs = await import_node('fs');
 export function file_exists(file_name) {
     if (web_is()) {
         let {files} = global_get();
+        let keys = object_properties(files);
+        return list_includes(keys, file_name);
     }
     return new Promise((resolve, reject) => {
         fs.stat(file_name, function (err, stat) {
