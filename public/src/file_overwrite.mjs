6b163b59b61d2df391336109e4e8f32ec36fca27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 12:16:24 -0400

    2024-05-07T16:16:23.858Z  function_transform js_imports_add file_overwrite

--- public/src/file_overwrite.mjs
+++ public/src/file_overwrite.mjs
@@ -1,4 +1,12 @@
-import {import_node} from './import_node.mjs';let fs = await import_node('fs');
+import {global_get} from "./global_get.mjs";
+import {web_is} from "./web_is.mjs";
+import {import_node} from './import_node.mjs';
+import {object_property_set} from './object_property_set.mjs';
+let fs = await import_node('fs');
 export async function file_overwrite(file_name, data) {
     return await fs.promises.writeFile(file_name, data, 'utf-8');
+    if (web_is()) {
+        let {files} = global_get();
+        object_property_set(files, file_name, data);
+    } else {}
 }
