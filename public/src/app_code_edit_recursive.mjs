d34ad38ae518d723de305b10fe0cd1dbd6af0fff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 08:35:39 -0400

    2024-05-07T12:35:38.973Z  noop

--- public/src/app_code_edit_recursive.mjs
+++ public/src/app_code_edit_recursive.mjs
@@ -6,6 +6,9 @@ import {html_style_font_color} from "./html_style_font_color.mjs";
 import {html_style_background_color} from "./html_style_background_color.mjs";
 import {app_learn_code_style_rounded_padded} from "./app_learn_code_style_rounded_padded.mjs";
 import {html_div} from "./html_div.mjs";
+import { assert } from "./assert.mjs";
+import { list_length_1 } from "./list_length_1.mjs";
+import { list_single } from "./list_single.mjs";
 export function app_code_edit_recursive(node, parent) {
     let {type} = node;
     switch (type) {
@@ -25,6 +28,7 @@ export function app_code_edit_recursive(node, parent) {
         case 'ImportDeclaration':
             html_style_word_break_all(parent);
             let {specifiers} = node;
+            let specifier = list_single(specifiers);
             html_inner_set(parent, json_to(specifiers));
             break;
         default:
