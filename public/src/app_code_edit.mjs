2d40a714d43009bf0c53b6239542d2dd239bf533
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 13:56:06 -0400

    2024-05-07T17:56:06.052Z  noop

--- public/src/app_code_edit.mjs
+++ public/src/app_code_edit.mjs
@@ -54,6 +54,7 @@ export function app_code_edit(file_path) {
             'save functions': async () => {
                 let {files} = global_get();
                 await function_run(functions_source_set.name, json_to(files));
+                console.log('here')
             }
         };
         each_object(choices, lambda);
