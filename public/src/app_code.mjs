131d9f9ec3a5ffad66d6d19c463f7702c0590199
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 18:26:46 -0400

    2024-05-07T22:26:45.924Z  function_auto app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -11,6 +11,7 @@ import {html_hash} from "./html_hash.mjs";
 import {app_code_search_function} from "./app_code_search_function.mjs";
 import {function_name_to_path} from "./function_name_to_path.mjs";
 import {html_style_default_initialize} from "./html_style_default_initialize.mjs";
+import {app_code_backable} from "./app_code_backable.mjs";
 export async function app_code() {
     let global = global_get();
     object_property_set(global, 'back_stack', []);
@@ -26,10 +27,10 @@ export async function app_code() {
         html_hash({
             'function_name': function_name => {
                 let p = function_name_to_path(function_name);
-                app_code_edit(p);
+                app_code_backable(() => app_code_edit(p));
             }
         });
     } else {
-        app_code_search_function();
+        app_code_backable(() => app_code_search_function());
     }
 }
