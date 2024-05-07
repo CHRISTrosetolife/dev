2e50a61a0ce909af1d9dcd628905d3495f773a18
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 23:24:26 -0400

    2024-05-07T03:24:25.861Z  function_transform js_imports_fix app_code_edit

--- public/src/app_code_edit.mjs
+++ public/src/app_code_edit.mjs
@@ -1,6 +1,7 @@
+import {js_export_single} from "./js_export_single.mjs";
 import {html_div} from "./html_div.mjs";
 import {html_document_body_clear} from "./html_document_body_clear.mjs";
-import { js_parse } from "./js_parse.mjs";
+import {js_parse} from "./js_parse.mjs";
 import {object_property_get} from "./object_property_get.mjs";
 export function app_code_edit(context, file_path) {
     let root = html_document_body_clear();
@@ -8,4 +9,5 @@ export function app_code_edit(context, file_path) {
     let {files} = context;
     let contents = object_property_get(files, file_path);
     let parsed = js_parse(contents);
+    let e = js_export_single(parsed);
 }
