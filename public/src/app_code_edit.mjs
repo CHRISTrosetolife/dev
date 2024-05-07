eaf6f68cf3538dcc566bdfbe83312466684aa946
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 18:28:15 -0400

    2024-05-07T22:28:15.163Z  function_auto app_code_edit

--- public/src/app_code_edit.mjs
+++ public/src/app_code_edit.mjs
@@ -1,3 +1,4 @@
+import {app_code_back} from "./app_code_back.mjs";
 import {app_code_backable} from "./app_code_backable.mjs";
 import {html_button_text_click} from "./html_button_text_click.mjs";
 import {function_auto} from "./function_auto.mjs";
@@ -51,7 +52,7 @@ export function app_code_edit(file_path) {
         app_code_backable(() => {
             let root = html_document_body_clear();
             let choices = {
-                'back': back,
+                'back': app_code_back,
                 'search': () => {
                     app_code_backable(() => app_code_search_function());
                 },
