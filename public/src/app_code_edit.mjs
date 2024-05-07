2eea8b02e133523acfbc99fe7c3214e3cec7e22c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 23:17:26 -0400

    2024-05-07T03:17:26.344Z  noop

--- public/src/app_code_edit.mjs
+++ public/src/app_code_edit.mjs
@@ -1,9 +1,11 @@
 import {html_document_body_clear} from "./html_document_body_clear.mjs";
 import {html_inner_set} from "./html_inner_set.mjs";
+import { html_style_monospace } from "./html_style_monospace.mjs";
 import {object_property_get} from "./object_property_get.mjs";
 export function app_code_edit(context, file_path) {
     let root = html_document_body_clear();
     let {files} = context;
     let contents = object_property_get(files, file_path);
     html_inner_set(root, contents);
+    html_style_monospace(root)
 }
