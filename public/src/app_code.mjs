91b52ef1f4f98f48647ed59c7668e9056087b467
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 22:14:05 -0400

    2024-05-07T02:14:05.441Z  noop

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -3,11 +3,13 @@ import {function_run} from "./function_run.mjs";
 import {functions_source} from "./functions_source.mjs";
 import {html_button_width_full_text} from "./html_button_width_full_text.mjs";
 import {html_document_body} from "./html_document_body.mjs";
+import { html_element } from "./html_element.mjs";
 import {html_style_default_initialize} from "./html_style_default_initialize.mjs";
 import {object_properties} from "./object_properties.mjs";
 export async function app_code() {
     html_style_default_initialize();
     let root = html_document_body();
+    html_element(root, 'input')
     let result = await function_run(functions_source.name, []);
     let paths = object_properties(result);
     for (let p of paths) {
