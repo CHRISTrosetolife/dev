a74f45190b071c66524cd6f7741d8fd12e6c1728
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 12:18:20 -0400

    2024-05-07T16:18:19.885Z  noop

--- public/src/file_write.mjs
+++ public/src/file_write.mjs
@@ -2,11 +2,6 @@ import {assert_not_async} from './assert_not_async.mjs';
 import {file_exists} from './file_exists.mjs';
 import {file_overwrite} from './file_overwrite.mjs';
 export async function file_write(file_name, data) {
-    if (web_is()) {
-        let {files} = global_get();
-        let keys = object_properties(files);
-        return list_includes(keys, file_name);
-    }
     await assert_not_async(file_exists, [file_name]);
     return await file_overwrite(file_name, data);
 }
