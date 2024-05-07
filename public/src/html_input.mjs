126f34d711b549d6664e6da2e88deddf7de96a0c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 12:22:50 -0400

    2024-05-07T16:22:49.971Z  function_transform js_imports_fix html_input

--- public/src/html_input.mjs
+++ public/src/html_input.mjs
@@ -1,4 +1,7 @@
+import {html_style_border_box} from "./html_style_border_box.mjs";
 import {html_element} from "./html_element.mjs";
 export function html_input(root) {
-    return html_element(root, 'input');
+    let input = html_element(root, 'input');
+    html_style_border_box(input);
+    return input;
 }
