d7dd9650b31caed115f7f367390dd8c2bd033074
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 12:51:12 -0400

    2024-05-07T16:51:11.790Z  noop

--- public/src/file_overwrite.mjs
+++ public/src/file_overwrite.mjs
@@ -5,6 +5,7 @@ import {object_property_set} from './object_property_set.mjs';
 import { file_read } from "./file_read.mjs";
 import { equal_not } from "./equal_not.mjs";
 import { file_exists } from "./file_exists.mjs";
+import { equal } from "./equal.mjs";
 let fs = await import_node('fs');
 export async function file_overwrite(file_name, data) {
     if (web_is()) {
@@ -13,7 +14,7 @@ export async function file_overwrite(file_name, data) {
     } else {
         if (await file_exists(file_name)) {
             let existing = await file_read(file_name);
-            if (equal_not(data, existing)) {
+            if (equal(data, existing)) {
                 return;
             }
         }
