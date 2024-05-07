04a7967f8eab7e6d164a778a55ce9a229ae95c3e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 12:57:43 -0400

    2024-05-07T16:57:42.813Z  function_transform js_outside_move functions_source

---
+++ public/src/function_contents.mjs
@@ -0,0 +1,15 @@
+import {promise_all} from "./promise_all.mjs";
+import {file_read} from "./file_read.mjs";
+import {list_map} from "./list_map.mjs";
+import {function_paths} from "./function_paths.mjs";
+export async function function_contents() {
+    let paths = await function_paths();
+    let mapped = list_map(paths, async file_path => {
+        return {
+            file_path,
+            contents: await file_read(file_path)
+        };
+    });
+    await promise_all(mapped);
+    return mapped;
+}
--- public/src/functions_source.mjs
+++ public/src/functions_source.mjs
@@ -1,17 +1,7 @@
-import {function_paths} from "./function_paths.mjs";
-import {file_read} from "./file_read.mjs";
-import {promise_all} from "./promise_all.mjs";
-import {list_map} from "./list_map.mjs";
+import {function_contents} from "./function_contents.mjs";
 import {object_property_set} from "./object_property_set.mjs";
 export async function functions_source() {
-    let paths = await function_paths();
-    let mapped = list_map(paths, async file_path => {
-        return {
-            file_path,
-            contents: await file_read(file_path)
-        };
-    });
-    await promise_all(mapped);
+    let mapped = await function_contents();
     let result = {};
     for (let fc of mapped) {
         let {file_path, contents} = await fc;
