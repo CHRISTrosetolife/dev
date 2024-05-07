af0c609b7c9aab7d973e042d55ddbd48945d8390
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 08:52:52 -0400

    2024-05-07T12:52:51.746Z  function_transform js_imports_fix app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,3 +1,4 @@
+import {html_style_height_full} from "./html_style_height_full.mjs";
 import {html_style_margin_none} from "./html_style_margin_none.mjs";
 import {html_document_body} from "./html_document_body.mjs";
 import {html_hash_exists} from "./html_hash_exists.mjs";
@@ -8,12 +9,11 @@ import {function_name_to_path} from "./function_name_to_path.mjs";
 import {function_run} from "./function_run.mjs";
 import {functions_source} from "./functions_source.mjs";
 import {html_style_default_initialize} from "./html_style_default_initialize.mjs";
-import { html_style } from "./html_style.mjs";
 export async function app_code() {
     html_style_default_initialize();
     let root = html_document_body();
     html_style_margin_none(root);
-    html_style_height_full(root)
+    html_style_height_full(root);
     let files = await function_run(functions_source.name, []);
     let context = {
         files
