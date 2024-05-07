a8070f6091ff608c010e2efe39b4c216b6c3327a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 15:21:45 -0400

    2024-05-07T19:21:45.283Z  noop

--- public/src/server.mjs
+++ public/src/server.mjs
@@ -49,8 +49,7 @@ export function server() {
                 });
                 let contents = await file_read(file_path_output);
                 let {result} = contents
-                error('a')
-                res.end(result);
+                res.end(json_to(result));
             });
         });
     });
