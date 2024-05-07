8fae2db59eec6f4a2b675bb2796238bbab589b96
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 15:55:28 -0400

    2024-05-07T19:55:27.363Z  function_auto function_import

--- public/src/function_import.mjs
+++ public/src/function_import.mjs
@@ -1,4 +1,6 @@
-import {import_node} from './import_node.mjs';let path = await import_node('path');
+import {path_join} from "./path_join.mjs";
+import {folder_current} from "./folder_current.mjs";
+let path = await import_node('path');
 import {function_extension} from './function_extension.mjs';
 export async function function_import(function_name) {
     let extension = function_extension();
@@ -6,6 +8,7 @@ export async function function_import(function_name) {
     let folder_current = '.';
     let first_with_extension = [function_name, extension_marker, extension].join('');
     let function_path_second = path.join(folder_current, first_with_extension);
+    if (0) path_join;
     let function_path = './' + function_path_second;
     let imported = await import(function_path);
     let imported_function = imported[function_name];
