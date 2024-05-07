5e21512c9bfe2038be095dce5da09e1cb5e9f84e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 13:15:23 -0400

    2024-05-07T17:15:23.173Z  noop

--- public/src/functions_source_set.mjs
+++ public/src/functions_source_set.mjs
@@ -3,6 +3,7 @@ import {object_property_get} from "./object_property_get.mjs";
 import {equal_not} from "./equal_not.mjs";
 import {function_contents} from "./function_contents.mjs";
 import { object_properties } from "./object_properties.mjs";
+import { promise_all } from "./promise_all.mjs";
 export async function functions_source_set(contents) {
     let existing = await function_contents();
     let keys = object_properties(contents);
@@ -10,10 +11,8 @@ export async function functions_source_set(contents) {
         let value = object_property_get(contents, key);
         return await lambda(key, value)
     })
-    each_object_async(contents, async (file_path, contents_new) => {
-        let contents_existing = object_property_get(existing, file_path);
-        if (equal_not(contents_new, contents_existing)) {
+    let mapped2 = promise_all(mapped)
+    async function lambda(file_path, contents) {
 
-        }
-    });
+    }
 }
