4c6d7b6889bc2c9303e58f637fe90dbc74afd56e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 22:03:11 -0400

    2024-05-07T02:03:11.313Z  function_transform js_outside_move function_name_to_path

---
+++ public/src/function_path_prefix.mjs
@@ -0,0 +1,4 @@
+import {folder_path_src} from "./folder_path_src.mjs";
+export function function_path_prefix() {
+    return `./${folder_path_src()}`;
+}
---
+++ public/src/function_path_suffix.mjs
@@ -0,0 +1,4 @@
+import {function_extension} from "./function_extension.mjs";
+export function function_path_suffix() {
+    return `.${function_extension()}`;
+}
