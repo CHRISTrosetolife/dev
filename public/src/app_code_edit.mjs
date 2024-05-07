650d18c058bfacda37dc9cdd077685bb68942841
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 09:01:21 -0400

    2024-05-07T13:01:21.453Z  noop

--- public/src/app_code_edit.mjs
+++ public/src/app_code_edit.mjs
@@ -5,11 +5,13 @@ import {object_property_get} from "./object_property_get.mjs";
 import {html_element} from "./html_element.mjs";
 import {html_inner_set} from "./html_inner_set.mjs";
 import {html_style_width_full} from "./html_style_width_full.mjs";
+import { html_style } from "./html_style.mjs";
 export function app_code_edit(context, file_path) {
     let root = html_document_body_clear();
     let {files} = context;
     let contents = object_property_get(files, file_path);
     let ta = html_element(root, 'textarea');
+    html_style(ta,{'display': 'block'})
     html_style_border_box(ta);
     html_inner_set(ta, contents);
     html_style_width_full(ta);
