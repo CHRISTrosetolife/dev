5ce7db5a19c5cc0f1d6b051d1e30d05b3479cae8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 12:32:28 -0400

    2024-05-07T16:32:28.397Z  noop

--- public/src/command_line.mjs
+++ public/src/command_line.mjs
@@ -1,5 +1,8 @@
-import {exec} from "child_process";
+
+import {import_node} from "./import_node.mjs";
 export async function command_line(command) {
+    let c = await import_node("child_process")
+    let  {exec}  = c;
     return await new Promise(resolve => {
         exec(command, (error, stdout, stderr) => {
             resolve({
