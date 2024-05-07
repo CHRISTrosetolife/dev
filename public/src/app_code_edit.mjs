f951881990550e5108c9e806de8c59bf24fb3b88
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 08:49:18 -0400

    2024-05-07T12:49:17.988Z  function_transform js_imports_fix app_code_edit

--- public/src/app_code_edit.mjs
+++ public/src/app_code_edit.mjs
@@ -1,17 +1,11 @@
-import {app_code_edit_recursive} from "./app_code_edit_recursive.mjs";
-import {html_div} from "./html_div.mjs";
 import {html_document_body_clear} from "./html_document_body_clear.mjs";
-import {js_parse} from "./js_parse.mjs";
 import {object_property_get} from "./object_property_get.mjs";
-import {html_style_background_color} from "./html_style_background_color.mjs";
+import {html_element} from "./html_element.mjs";
+import {html_inner_set} from "./html_inner_set.mjs";
 export function app_code_edit(context, file_path) {
     let root = html_document_body_clear();
-    let container = html_div(root);
-    html_style_background_color(container, 'black');
     let {files} = context;
     let contents = object_property_get(files, file_path);
-    let ast = js_parse(contents);
-    let node = ast;
-    let parent = container;
-    
+    let ta = html_element(root, 'textarea');
+    html_inner_set(ta, contents);
 }
