14ac66e6b7c170414cd6803d9b47099687ebb076
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 11:54:13 -0400

    2024-05-07T15:54:12.691Z  function_transform js_outside_move import_node

--- public/src/import_node.mjs
+++ public/src/import_node.mjs
@@ -1,6 +1,7 @@
+import {web_not_is} from "./web_not_is.mjs";
 export async function import_node(libary_to_import) {
     let a;
-    if (typeof window === 'undefined') {
+    if (web_not_is()) {
         a = await import(libary_to_import);
     }
     return a;
