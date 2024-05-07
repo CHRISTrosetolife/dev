2d102af22b0167f4b0858a2fef01ef6d07c0ab25
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 12:03:36 -0400

    2024-05-07T16:03:36.571Z  file_exists ./public/src/global_get.mjs

--- public/src/returns_async.mjs
+++ public/src/returns_async.mjs
@@ -2,6 +2,7 @@ import {assert_boolean} from './assert_boolean.mjs';
 import {equal} from './equal.mjs';
 export async function returns_async(fn, expected, args) {
     let actual = await fn(...args);
+    console.log({actual})
     let e = equal(actual, expected);
     assert_boolean(e);
 }
