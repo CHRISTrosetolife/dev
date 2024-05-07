1e5766c8f3c80644943034a478c962ffe5d4c6d9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 22:00:28 -0400

    2024-05-07T02:00:28.266Z  function_transform js_imports_fix function_name_to_path

--- public/src/function_name_to_path.mjs
+++ public/src/function_name_to_path.mjs
@@ -1,5 +1,6 @@
 import {folder_path_src} from "./folder_path_src.mjs";
 import {function_extension} from './function_extension.mjs';
+import {string_combine_multiple} from './string_combine_multiple.mjs';
 export function function_name_to_path(function_name) {
-    return `./${folder_path_src()}${function_name}.${function_extension()}`;
+    return string_combine_multiple([`./${folder_path_src()}`, function_name, `.${function_extension()}`]);
 }
