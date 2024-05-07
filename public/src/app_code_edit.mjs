3e0b6bb2371c3994c4ef7bcf4042240692da3dff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 23:16:32 -0400

    2024-05-07T03:16:31.889Z  noop

--- public/src/app_code_edit.mjs
+++ public/src/app_code_edit.mjs
@@ -1,4 +1,9 @@
+import { html_inner_set } from "./html_inner_set.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+
 export function app_code_edit(context, file_path) {
     let root = html_document_body_clear();
     let {files} = context;
+    let contents = object_property_get(files, file_path)
+    html_inner_set(root, contents)
 }
