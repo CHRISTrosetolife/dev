f1785b571f58ea38626dcddac1a73a0f27612edc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 14:36:22 -0400

    2024-05-07T18:36:11.893Z  noop

--- public/src/app_code_edit.mjs
+++ public/src/app_code_edit.mjs
@@ -53,7 +53,7 @@ export function app_code_edit(file_path) {
             },
             'save functions': async () => {
                 let {files} = global_get();
-                await function_run(functions_source_set.name, [json_to(files)]);
+                await function_run(functions_source_set.name, [files]);
                 console.log('here')
             }
         };
