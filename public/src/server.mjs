342cc7d7e3ffd23f3bc6e28e001cdca3a6dc598c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-06 21:09:35 -0400

    2024-05-07T01:09:34.875Z  function_transform js_imports_fix server

--- public/src/server.mjs
+++ public/src/server.mjs
@@ -1,15 +1,13 @@
-import {run_git} from "./run_git.mjs";
+import {uuid_file} from "./uuid_file.mjs";
 import {command_line} from "./command_line.mjs";
 import {json_to} from "./json_to.mjs";
-import {function_run} from "./function_run.mjs";
-import {json_from} from "./json_from.mjs";
 import {server_port} from "./server_port.mjs";
 import express from 'express';
 import cors from 'cors';
 import {function_run_json} from "./function_run_json.mjs";
 import {string_replace} from "./string_replace.mjs";
-import { run } from "./run.mjs";
-import { file_overwrite } from "./file_overwrite.mjs";
+import {run} from "./run.mjs";
+import {file_overwrite} from "./file_overwrite.mjs";
 export function server() {
     let app = express();
     let port = server_port();
@@ -21,16 +19,21 @@ export function server() {
     app.post('/', async (req, res) => {
         let {body} = req;
         let {function_name, args} = body;
-        let args_json = json_to({args});
+        let args_json = json_to({
+            args
+        });
         let replaced = string_replace(args_json, '\'', '\'\'');
         replaced = string_replace(replaced, '"', '\\"');
+        uuid_file;
         let command = `node ${run.name}.mjs ${function_run_json.name} ${function_name} '${replaced}'`;
         let {stdout} = await command_line(command);
-        await file_overwrite('log.txt', stdout)
-        stdout = string_replace(stdout, '\n', '')
+        await file_overwrite('log.txt', stdout);
+        stdout = string_replace(stdout, '\n', '');
         let parsed = eval(stdout);
         let json = json_to(parsed);
-        console.log({json})
+        console.log({
+            json
+        });
         res.end(json);
     });
     app.listen(port, () => {
