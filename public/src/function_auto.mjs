6d56b5b65e8f1e39feaff906bb0a96cb4e13cb0a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 15:46:30 -0400

    2024-05-07T19:46:30.615Z  function_transform js_imports_fix function_auto

--- public/src/function_auto.mjs
+++ public/src/function_auto.mjs
@@ -1,5 +1,9 @@
-import {js_imports_add} from "./js_imports_add.mjs";
+import {js_imports_fix} from "./js_imports_fix.mjs";
+import {js_outside_move} from "./js_outside_move.mjs";
 import {function_transform} from "./function_transform.mjs";
-export async function function_imports_add(function_name) {
-    await function_transform(js_imports_add.name, function_name);
+export async function function_auto(function_name) {
+    let transforms = [js_outside_move, js_imports_fix];
+    for (let t of transforms) {
+        await function_transform(t.name, function_name);
+    }
 }
