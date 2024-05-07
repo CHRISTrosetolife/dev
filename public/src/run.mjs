1dae716016705a04a26cf40fb676ca30032902d9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 14:25:49 -0400

    2024-05-07T18:25:48.521Z  noop

--- public/src/run.mjs
+++ public/src/run.mjs
@@ -1,8 +1,10 @@
 import {function_run} from './function_run.mjs';
+import { list_first } from './list_first.mjs';
+import { list_skip } from './list_skip.mjs';
 import {process_argv_remaining} from './process_argv_remaining.mjs';
 export async function run() {
     let remaining = process_argv_remaining();
-    let first = remaining[0];
-    let args = remaining.slice(1);
+    let first = list_first(remaining);
+    let args = list_skip(remaining, 1)
     return await function_run(first, args);
 }
