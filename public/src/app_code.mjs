5ef9405465d9081becc2ed20b2f0fc1909d76c5a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 15:16:54 -0400

    2024-05-07T19:16:54.050Z  function_transform js_outside_move app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,4 +1,4 @@
-import {global_get} from "./global_get.mjs";
+import {app_code_download} from "./app_code_download.mjs";
 import {html_root} from "./html_root.mjs";
 import {html_style_height_full} from "./html_style_height_full.mjs";
 import {html_style_margin_none} from "./html_style_margin_none.mjs";
@@ -8,10 +8,7 @@ import {app_code_edit} from "./app_code_edit.mjs";
 import {html_hash} from "./html_hash.mjs";
 import {app_code_search_function} from "./app_code_search_function.mjs";
 import {function_name_to_path} from "./function_name_to_path.mjs";
-import {function_run} from "./function_run.mjs";
 import {html_style_default_initialize} from "./html_style_default_initialize.mjs";
-import {object_property_set} from "./object_property_set.mjs";
-import { functions_source_get } from "./functions_source_get.mjs";
 export async function app_code() {
     html_style_default_initialize();
     let root = html_document_body();
@@ -20,8 +17,7 @@ export async function app_code() {
     for (let e of [html, root]) {
         html_style_height_full(e);
     }
-    let files = await function_run(functions_source_get.name, []);
-    object_property_set(global_get(), 'files', files);
+    await app_code_download();
     if (html_hash_exists()) {
         html_hash({
             'function_name': function_name => {
---
+++ public/src/app_code_download.mjs
@@ -0,0 +1,8 @@
+import {global_get} from "./global_get.mjs";
+import {object_property_set} from "./object_property_set.mjs";
+import {functions_source_get} from "./functions_source_get.mjs";
+import {function_run} from "./function_run.mjs";
+export async function app_code_download() {
+    let files = await function_run(functions_source_get.name, []);
+    object_property_set(global_get(), 'files', files);
+}
