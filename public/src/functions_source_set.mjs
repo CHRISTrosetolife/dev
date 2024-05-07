9a3359837ad56bb31ced85253dbbbe2125776256
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 13:09:48 -0400

    2024-05-07T17:09:47.417Z  function_transform js_imports_fix functions_source_set

--- public/src/functions_source_set.mjs
+++ public/src/functions_source_set.mjs
@@ -1,13 +1,11 @@
-import { each_object } from "./each_object.mjs";
-import { equal_not } from "./equal_not.mjs";
+import {object_property_get} from "./object_property_get.mjs";
+import {each_object} from "./each_object.mjs";
+import {equal_not} from "./equal_not.mjs";
 import {function_contents} from "./function_contents.mjs";
-import {object_property_set} from "./object_property_set.mjs";
 export async function functions_source_set(contents) {
     let existing = await function_contents();
     each_object(contents, (file_path, contents_new) => {
         let contents_existing = object_property_get(existing, file_path);
-        if (equal_not(contents_new, contents_existing)) {
-
-        }
-    })
+        if (equal_not(contents_new, contents_existing)) {}
+    });
 }
