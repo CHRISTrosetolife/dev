5e86e02b406ab27c1347bc81c5bae08eb99bc0af
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 08:01:34 -0400

    2024-05-07T12:01:33.857Z  function_transform js_outside_move html_style_button_default_initialize

--- public/src/html_style_button_default_initialize.mjs
+++ public/src/html_style_button_default_initialize.mjs
@@ -1,3 +1,4 @@
+import {html_style_margin_default_value} from "./html_style_margin_default_value.mjs";
 import {html_style_button_default_background_color} from "./html_style_button_default_background_color.mjs";
 import {html_style_button_default_border} from "./html_style_button_default_border.mjs";
 import {app_learn_code_style_rounded_padded_style} from "./app_learn_code_style_rounded_padded_style.mjs";
@@ -10,9 +11,4 @@ export function html_style_button_default_initialize() {
         'background-color': html_style_button_default_background_color()
     });
     object_merge(html_style_button_default(), html_style_button_default_border());
-}
-function html_style_margin_default_value() {
-    return {
-        'margin': '0.5dvh'
-    };
 }
---
+++ public/src/html_style_margin_default_value.mjs
@@ -0,0 +1,5 @@
+export function html_style_margin_default_value() {
+    return {
+        'margin': '0.5dvh'
+    };
+}
