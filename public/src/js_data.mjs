6bd23c88c2b5d3f0234af9946b8d6deb1003b35c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 17:16:06 -0400

    2024-05-08T21:16:05.254Z  function_open app_learn_code

--- public/src/js_data.mjs
+++ public/src/js_data.mjs
@@ -1,4 +1,7 @@
+import {file_exists} from "./file_exists.mjs";
 import {js_identifiers} from "./js_identifiers.mjs";
 export function js_data(ast) {
     return js_identifiers(ast);
+    let data_path = 'data.json';
+    if (!file_exists(data_path)) {}
 }
