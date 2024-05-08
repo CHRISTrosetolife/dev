35d7be34fe73a2c7884060459a58b54fcb661a0d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 17:46:58 -0400

    2024-05-08T21:46:57.949Z  function_copy lesson_mod_even lesson_if_else_nested_if

---
+++ public/src/lesson_if_else_nested_if.mjs
@@ -0,0 +1,22 @@
+import {html_style_alternate_monospace_short} from "./html_style_alternate_monospace_short.mjs";
+import {list_join_newline} from "./list_join_newline.mjs";
+import {lesson_simple} from "./lesson_simple.mjs";
+import {integer_random_digit_single_positive} from "./integer_random_digit_single_positive.mjs";
+import {js_code_statement_let_assign} from "./js_code_statement_let_assign.mjs";
+import {app_learn_code_log_delimit} from "./app_learn_code_log_delimit.mjs";
+export function lesson_if_else_nested_if() {
+    let description = 'checking if a number is even';
+    let example_message = 'outputting whether or not a number is even';
+    return lesson_simple(function () {
+        let a = integer_random_digit_single_positive();
+        let lines = [js_code_statement_let_assign('is_even', `${a} % 2 === 0`), `if (is_even) {`, app_learn_code_log_delimit('number is even'), `} else {`, app_learn_code_log_delimit('number is odd'), `}`];
+        return list_join_newline(lines);
+    }, example_before, description, example_message, [screen1]);
+    function example_before(parent) {}
+    function screen1(parent) {
+        html_style_alternate_monospace_short(parent, 'suppose `n` is a number in javascipt');
+        html_style_alternate_monospace_short(parent, 'if `n % 2` is `0` , then `n` is an even number');
+        html_style_alternate_monospace_short(parent, 'if `n % 2 === 0` is `true` , then `n` is an even number');
+        html_style_alternate_monospace_short(parent, 'otherwise , `n` is an odd number');
+    }
+}
