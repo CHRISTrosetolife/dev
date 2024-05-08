f1036a3e944b1da5d0725a5d15d7001fd55a6a49
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 17:21:48 -0400

    2024-05-08T21:21:47.808Z  noop

--- public/src/lesson_mod_even.mjs
+++ public/src/lesson_mod_even.mjs
@@ -32,6 +32,7 @@ export function lesson_mod_even() {
     }, example_before, description, example_message, [screen1, screen2, screen3]);
     function example_before(parent) {
         'suppose n is a number in javascipt'
+        'if n % 2 is true , then n is an even number'
         html_style_alternate_monospace(parent, ['in javascript , to get the remainder of ', 'dividend', ' use ', '%', ' like this : ', 'dividend % divisor']);
     }
     function screen1(parent) {
