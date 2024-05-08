f2278fb045b3ac9be15e3741adadcb9b76b4d899
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 18:08:34 -0400

    2024-05-08T22:08:34.423Z  noop

--- public/src/html_style_alternate_short.mjs
+++ public/src/html_style_alternate_short.mjs
@@ -3,7 +3,7 @@ import {noop} from "./noop.mjs";
 import {html_p} from "./html_p.mjs";
 import {html_style_alternate} from "./html_style_alternate.mjs";
 import {html_style_alternate_split} from "./html_style_alternate_split.mjs";
-export function html_style_alternate_short(parent, message) {
+export function html_style_alternate_short(parent, create, patterns, message) {
     let split = html_style_alternate_split(message);
-    html_style_alternate(parent, html_p, [noop, app_learn_code_style_code_colored], split);
+    html_style_alternate(parent, create, patterns, split);
 }
