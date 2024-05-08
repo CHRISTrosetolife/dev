dcea015e3b471649990c65868989d0c8f6724187
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 18:08:11 -0400

    2024-05-08T22:08:11.205Z  noop

--- public/src/html_style_alternate_short.mjs
+++ public/src/html_style_alternate_short.mjs
@@ -5,5 +5,5 @@ import {html_style_alternate} from "./html_style_alternate.mjs";
 import {html_style_alternate_split} from "./html_style_alternate_split.mjs";
 export function html_style_alternate_short(parent, message) {
     let split = html_style_alternate_split(message);
-    html_style_alternate(parent, html_p, [noop, app_learn_code_style_code_colored], list);
+    html_style_alternate(parent, html_p, [noop, app_learn_code_style_code_colored], split);
 }
