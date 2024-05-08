61e74d73049194b10bf807ea6779081442b09eb8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 18:08:01 -0400

    2024-05-08T22:08:01.008Z  function_auto html_style_alternate_short

--- public/src/html_style_alternate_short.mjs
+++ public/src/html_style_alternate_short.mjs
@@ -1,6 +1,9 @@
+import {app_learn_code_style_code_colored} from "./app_learn_code_style_code_colored.mjs";
+import {noop} from "./noop.mjs";
+import {html_p} from "./html_p.mjs";
+import {html_style_alternate} from "./html_style_alternate.mjs";
 import {html_style_alternate_split} from "./html_style_alternate_split.mjs";
-import {html_style_alternate_monospace} from "./html_style_alternate_monospace.mjs";
 export function html_style_alternate_short(parent, message) {
     let split = html_style_alternate_split(message);
-    html_style_alternate_monospace(parent, split);
+    html_style_alternate(parent, html_p, [noop, app_learn_code_style_code_colored], list);
 }
