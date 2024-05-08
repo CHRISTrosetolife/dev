0c9057c81156f94886a8a5952c4168332605bbeb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 18:04:27 -0400

    2024-05-08T22:04:27.626Z  noop

--- public/src/lesson_if_else_nested_if.mjs
+++ public/src/lesson_if_else_nested_if.mjs
@@ -5,25 +5,17 @@ import {lesson_simple} from "./lesson_simple.mjs";
 import {js_code_statement_let_assign} from "./js_code_statement_let_assign.mjs";
 import {app_learn_code_log_delimit} from "./app_learn_code_log_delimit.mjs";
 import {app_learn_code_log} from "./app_learn_code_log.mjs";
+import { string_combine } from "./string_combine.mjs";
 export function lesson_if_else_nested_if() {
     let description = 'nested if';
-    let example_message = 'outputting whether or not a number is even';
+    let example_message = string_combine('with a ', description);
     return lesson_simple(function () {
         let a = integer_random_digit_single();
         let lines = [js_code_statement_let_assign('is_positive', `${a} > 0`), js_code_statement_let_assign('is_even', `${a} % 2 === 0`), app_learn_code_log(a), `if (is_positive) {`, app_learn_code_log_delimit(`number is positive`), `if (is_even) {`, app_learn_code_log_delimit('number is positive and even'), `}`, `}`];
         return list_join_newline(lines);
-    }, example_before, description, example_message, [screen1]);
+    }, example_before, description, example_message, []);
     function example_before(parent) {
         'an `if` can be inside another `if`';
         'this is called a nested `if`'
-        'for now we assume the code in the parenthesis of an `if` returns `true` or `false`'
-        'the code inside the braces of the `if` on the inside only runs if the code in parenthesis of the `if` on the outside is `true`';
-
-    }
-    function screen1(parent) {
-        html_style_alternate_monospace_short(parent, 'suppose `n` is a number in javascipt');
-        html_style_alternate_monospace_short(parent, 'if `n % 2` is `0` , then `n` is an even number');
-        html_style_alternate_monospace_short(parent, 'if `n % 2 === 0` is `true` , then `n` is an even number');
-        html_style_alternate_monospace_short(parent, 'otherwise , `n` is an odd number');
     }
 }
