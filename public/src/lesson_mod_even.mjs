71f97b03d25a03187b9fe8f689d4eb2bcf0d6df5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 17:41:26 -0400

    2024-05-08T21:41:25.858Z  noop

--- public/src/lesson_mod_even.mjs
+++ public/src/lesson_mod_even.mjs
@@ -4,12 +4,13 @@ import {app_learn_code_log} from "./app_learn_code_log.mjs";
 import {lesson_simple} from "./lesson_simple.mjs";
 import {integer_random_digit_single_positive} from "./integer_random_digit_single_positive.mjs";
 import {js_code_statement_let_assign} from "./js_code_statement_let_assign.mjs";
+import { app_learn_code_log_delimit } from "./app_learn_code_log_delimit.mjs";
 export function lesson_mod_even() {
     let description = 'checking if a number is even';
     let example_message = 'outputting whether or not a number is even';
     return lesson_simple(function () {
         let a = integer_random_digit_single_positive();
-        let lines = [js_code_statement_let_assign('is_even', a),`if (is_even) {`, app_learn_code_log('number is even'), `} else {`, app_learn_code_log('number is odd'), `}`];
+        let lines = [js_code_statement_let_assign('is_even', a),`if (is_even) {`, app_learn_code_log_delimit('number is even'), `} else {`, app_learn_code_log_delimit('number is odd'), `}`];
         return list_join_newline(lines);
     }, example_before, description, example_message, [screen1]);
     function example_before(parent) {}
