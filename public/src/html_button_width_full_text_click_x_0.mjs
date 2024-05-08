8ac682468b4152e3b91cb1f20b73d4f4926ca69d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-07 20:40:46 -0400

    2024-05-08T00:40:45.778Z  noop

--- public/src/html_button_width_full_text_click_x_0.mjs
+++ public/src/html_button_width_full_text_click_x_0.mjs
@@ -3,4 +3,5 @@ import {html_button_width_full_text_click} from "./html_button_width_full_text_c
 export function html_button_width_full_text_click_x_0(root, key, value) {
     let b = html_button_width_full_text_click(root, key, value);
     html_style_margin_x_0(b);
+    return b;
 }
