90d383a8b23d9650a4c8a115db982b77f1f7c760
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 13:35:52 -0400

    2024-05-08T17:35:51.907Z  function_auto js_code_call

--- public/src/js_code_call.mjs
+++ public/src/js_code_call.mjs
@@ -1,5 +1,5 @@
-import {js_code_statement} from "./js_code_statement.mjs";
+import {js_code_call_args} from "./js_code_call_args.mjs";
 export function js_code_call(function_name) {
-    let code = js_code_statement(`${function_name}()`);
+    let code = js_code_call_args(function_name, []);
     return code;
 }
