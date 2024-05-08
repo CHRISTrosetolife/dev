e1914f495bbdee76f2e64fe9059f7571c887802c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 17:27:40 -0400

    2024-05-08T21:27:40.043Z  noop

--- public/src/lesson_mod_even.mjs
+++ public/src/lesson_mod_even.mjs
@@ -32,8 +32,7 @@ export function lesson_mod_even() {
         return list_join_newline(lines);
     }, example_before, description, example_message, [screen1, screen2, screen3]);
     function example_before(parent) {
-        let message = 'suppose `n` is a number in javascipt';
-        html_style_alternate_monospace_short(parent, message);
+        html_style_alternate_monospace_short(parent, 'suppose `n` is a number in javascipt');
         'if `n % 2` is `0` , then `n` is an even number';
         'if `n % 2 === 0` is true , then `n` is an even number';
         'otherwise , `n` is an odd number';
