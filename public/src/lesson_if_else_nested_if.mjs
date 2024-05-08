6b36e3f02776218bb1e460e50bcd149196e33da3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 17:58:02 -0400

    2024-05-08T21:58:01.926Z  function_auto lesson_if_else_nested_if

--- public/src/lesson_if_else_nested_if.mjs
+++ public/src/lesson_if_else_nested_if.mjs
@@ -1,30 +1,21 @@
+import {integer_random_digit_single} from "./integer_random_digit_single.mjs";
 import {html_style_alternate_monospace_short} from "./html_style_alternate_monospace_short.mjs";
 import {list_join_newline} from "./list_join_newline.mjs";
 import {lesson_simple} from "./lesson_simple.mjs";
-import {integer_random_digit_single_positive} from "./integer_random_digit_single_positive.mjs";
 import {js_code_statement_let_assign} from "./js_code_statement_let_assign.mjs";
 import {app_learn_code_log_delimit} from "./app_learn_code_log_delimit.mjs";
-import { app_learn_code_log } from "./app_learn_code_log.mjs";
+import {app_learn_code_log} from "./app_learn_code_log.mjs";
 export function lesson_if_else_nested_if() {
     let description = 'nested if';
     let example_message = 'outputting whether or not a number is even';
     return lesson_simple(function () {
-        let a = integer_random_digit_single_positive();
-        let lines = [
-        js_code_statement_let_assign('is_positive', `${a} > 0`),
-        js_code_statement_let_assign('is_even', `${a} % 2 === 0`),
-        app_learn_code_log(a), 
-        `if (is_positive) {`,
-            app_learn_code_log_delimit(`number is positive`), 
-            `if (is_even) {`, 
-                app_learn_code_log_delimit('number is even'), 
-            `}`,
-        `}`];
+        let a = integer_random_digit_single();
+        let lines = [js_code_statement_let_assign('is_positive', `${a} > 0`), js_code_statement_let_assign('is_even', `${a} % 2 === 0`), app_learn_code_log(a), `if (is_positive) {`, app_learn_code_log_delimit(`number is positive`), `if (is_even) {`, app_learn_code_log_delimit('number is even'), `}`, `}`];
         return list_join_newline(lines);
     }, example_before, description, example_message, [screen1]);
     function example_before(parent) {
-        'an `if` can be inside another `if`'
-        'then there '
+        'an `if` can be inside another `if`';
+        'then there ';
     }
     function screen1(parent) {
         html_style_alternate_monospace_short(parent, 'suppose `n` is a number in javascipt');
