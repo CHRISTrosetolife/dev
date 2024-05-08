d9e963f24230821bf57eb276dffe454e350c10e5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 13:36:45 -0400

    2024-05-08T17:36:44.828Z  noop

--- public/src/app_code_edit.mjs
+++ public/src/app_code_edit.mjs
@@ -89,7 +89,7 @@ export function app_code_edit(file_path) {
             let transform = html_button_width_full_text_click_x_0(root, '', () => {
                 let sources = object_values(files);
                 let code = `(() => {
-                    ${}
+                    ${js_code_call_args(function_transform_args_split.name, [])}
                     ${list_join(sources, newline())}
                 })();`
             });
