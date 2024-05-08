e9664da77de7905797ac9809bfca08ad3ef01fe5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 13:48:59 -0400

    2024-05-08T17:48:59.559Z  noop

--- public/src/function_transform_args_split.mjs
+++ public/src/function_transform_args_split.mjs
@@ -4,5 +4,6 @@ import {function_import} from "./function_import.mjs";
 export async function function_transform_args_split(lambda_name, function_name, args) {
     let lambda = await function_import(lambda_name);
     let file_path = function_name_to_path(function_name);
+    console.log({file_path, function_name})
     return await file_js_transform(lambda, file_path, args);
 }
