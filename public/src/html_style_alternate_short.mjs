82d08f82c2b9bd470418ccdf169028d14b4bb474
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 18:08:37 -0400

    2024-05-08T22:08:35.994Z  function_auto html_style_alternate_short

--- public/src/html_style_alternate_short.mjs
+++ public/src/html_style_alternate_short.mjs
@@ -1,6 +1,3 @@
-import {app_learn_code_style_code_colored} from "./app_learn_code_style_code_colored.mjs";
-import {noop} from "./noop.mjs";
-import {html_p} from "./html_p.mjs";
 import {html_style_alternate} from "./html_style_alternate.mjs";
 import {html_style_alternate_split} from "./html_style_alternate_split.mjs";
 export function html_style_alternate_short(parent, create, patterns, message) {
