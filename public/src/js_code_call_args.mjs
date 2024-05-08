f660aae78d60549c5870a66ed199efd72616397c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 13:35:34 -0400

    2024-05-08T17:35:34.673Z  noop

--- public/src/js_code_call_args.mjs
+++ public/src/js_code_call_args.mjs
@@ -1,5 +1,6 @@
 import {js_code_statement} from "./js_code_statement.mjs";
-export function js_code_call(function_name) {
-    let code = js_code_statement(`${function_name}()`);
+import { list_join } from "./list_join.mjs";
+export function js_code_call_args(function_name, args) {
+    let code = js_code_statement(`${function_name}(${list_join(args, ', ')})`);
     return code;
 }
