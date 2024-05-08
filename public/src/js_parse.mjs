c202da03324be9019063653ce5548806199553bd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 13:57:38 -0400

    2024-05-08T17:57:38.465Z  noop

--- public/src/js_parse.mjs
+++ public/src/js_parse.mjs
@@ -1,6 +1,11 @@
 import {import_multi} from "./import_multi.mjs";
+import { import_node } from "./import_node.mjs";
 import {js_parse_options} from "./js_parse_options.mjs";
-let a = await import_multi('acorn', 'acorn');
+import { web_is } from "./web_is.mjs";
+let a = await import_node('acorn');
 export function js_parse(code) {
+    if (web_is()) {
+        a = acorn;
+    }
     return a.parse(code, js_parse_options());
 }
--- public/src/js_unparse_indent.mjs
+++ public/src/js_unparse_indent.mjs
@@ -1,7 +1,12 @@
 import {import_multi} from "./import_multi.mjs";
-let a = await import_multi('astring', 'astring');
-let {generate} = a;
+import { import_node } from "./import_node.mjs";
+import { web_is } from "./web_is.mjs";
+let a = await import_node('astring');
 export function js_unparse_indent(ast, indent) {
+    if (web_is()) {
+        a = astring;
+    }
+    let {generate} = a;
     return generate(ast, {
         indent: indent
     });
