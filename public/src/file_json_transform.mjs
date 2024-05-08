9a8bb6018073fb42d472c15764525c1944458143
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 16:30:13 -0400

    2024-05-08T20:30:12.694Z  noop

--- public/src/file_json_transform.mjs
+++ public/src/file_json_transform.mjs
@@ -1,8 +1,8 @@
 import {file_write_json} from "./file_write_json.mjs";
 import {file_read_json} from "./file_read_json.mjs";
 export async function file_json_transform(lambda, file_path, args) {
-    let ast = await file_read_json(file_path);
-    let result = await lambda(ast, ...args);
-    await file_write_json(file_path, ast);
+    let obj = await file_read_json(file_path);
+    let result = await lambda(obj, ...args);
+    await file_write_json(file_path, obj);
     return result;
 }
