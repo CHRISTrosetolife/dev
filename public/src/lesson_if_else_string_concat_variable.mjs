202c0bc1ac756f657c8dfe0ab351652b37bd5aca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-09 15:10:16 -0400

    2024-05-09T19:10:16.132Z  noop

--- public/src/lesson_if_else_string_concat_variable.mjs
+++ public/src/lesson_if_else_string_concat_variable.mjs
@@ -3,13 +3,13 @@ import {html_style_alternate_monospace_short} from "./html_style_alternate_monos
 import {lesson_simple} from "./lesson_simple.mjs";
 import {string_combine} from "./string_combine.mjs";
 export function lesson_if_else_string_concat_variable() {
-    let description = 'three strings combined';
+    let description = 'string combined a variable';
     let example_message = string_combine('with ', description);
     return lesson_simple(function () {
         let concat_count = 3;
         return lesson_if_else_string_concat_quiz(concat_count);
     }, example_before, description, example_message, []);
     function example_before(parent) {
-        html_style_alternate_monospace_short(parent, 'three strings can be concatentated using multiple `+');
+        html_style_alternate_monospace_short(parent, 'strings can be concatentated with a variable');
     }
 }
