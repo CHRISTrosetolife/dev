484378f25b97700da804258a490c8725aa05d52c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-09 12:41:57 -0400

    2024-05-09T16:41:57.120Z  noop

--- public/src/lesson_if_else_if_else_inside_else.mjs
+++ public/src/lesson_if_else_if_else_inside_else.mjs
@@ -15,7 +15,16 @@ export function lesson_if_else_if_else_inside_else() {
     let example_message = string_combine('with an ', description);
     return lesson_simple(function () {
         let a = integer_random_digit_single();
-        let lines = [app_learn_code_log(a), `if (${a} < 0) {`, `if (${a} % 2 === 0) {`, app_learn_code_log_delimit('negative and even'), `} else {`, app_learn_code_log_delimit(`negative and odd`), `}`, `}`];
+        let lines = [
+            app_learn_code_log(a), 
+            `if (${a} < 0) {`, 
+            `if (${a} % 2 === 0) {`, 
+            app_learn_code_log_delimit('negative and even'), 
+            `} else {`,
+            app_learn_code_log_delimit(`negative and odd`), 
+            `}`, 
+            `}`
+        ];        
         return list_join_newline(lines);
     }, example_before, description, example_message, []);
     function example_before(parent) {
