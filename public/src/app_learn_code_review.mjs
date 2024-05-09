a2d5795d5cf6dbd761920f4722ed69c6ee435284
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 21:08:04 -0400

    2024-05-09T01:08:03.809Z  noop

--- public/src/app_learn_code_review.mjs
+++ public/src/app_learn_code_review.mjs
@@ -1,8 +1,10 @@
 import { list_map } from "./list_map.mjs";
+import { list_scramble } from "./list_scramble.mjs";
 
 export function app_learn_code_review(lessons) {
     return function app_learn_code_review_inner(parent) {
         let quizzes = list_map(lessons, object_property_get(lesson, 'quiz'))
+        list_scramble(quizzes);
         app_learn_code_quiz_inner_refresh
     }
 }
