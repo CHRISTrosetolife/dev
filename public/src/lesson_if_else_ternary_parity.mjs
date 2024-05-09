45b2ca1080b66a02f3cf34f39858b22545be299b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-09 18:33:17 -0400

    2024-05-09T22:33:16.875Z  function_copy lesson_if_else_ternary_two lesson_if_else_ternary_parity

---
+++ public/src/lesson_if_else_ternary_parity.mjs
@@ -0,0 +1,23 @@
+import {js_code_ternary} from "./js_code_ternary.mjs";
+import {app_learn_code_log_join_plus} from "./app_learn_code_log_join_plus.mjs";
+import {lesson_simple} from "./lesson_simple.mjs";
+import {string_combine} from "./string_combine.mjs";
+import {list_join_newline} from "./list_join_newline.mjs";
+import {js_code_statement_let_assign} from "./js_code_statement_let_assign.mjs";
+import {string_delimit} from "./string_delimit.mjs";
+import {integer_random_digit_single} from "./integer_random_digit_single.mjs";
+import {js_code_statement_assign} from "./js_code_statement_assign.mjs";
+import {app_learn_code_log} from "./app_learn_code_log.mjs";
+import {js_code_statement_let} from "./js_code_statement_let.mjs";
+import {html_p_text} from "./html_p_text.mjs";
+import {app_learn_code_code_part_contrast} from "./app_learn_code_code_part_contrast.mjs";
+export function lesson_if_else_ternary_parity() {
+    let description = 'two ternaries';
+    let example_message = string_combine('with ', description);
+    return lesson_simple(function () {
+        let n = integer_random_digit_single();
+        let lines = [app_learn_code_log(n), js_code_statement_let_assign('positive', js_code_ternary(`${n} <= 0`, string_delimit('not '), string_delimit(''))), app_learn_code_log_join_plus(['positive', string_delimit('positive')]), js_code_statement_let_assign('even', js_code_ternary(`${n} % 2 === 0`, string_delimit('even'), string_delimit('odd'))), app_learn_code_log('even')];
+        return list_join_newline(lines);
+    }, example_before, description, example_message, []);
+    function example_before(parent) {}
+}
