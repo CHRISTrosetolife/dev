221c34877aa3361c11a19f9cbd94c4ae34b7f154
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 21:08:29 -0400

    2024-05-09T01:08:29.235Z  function_auto app_learn_code_review

--- public/src/app_learn_code_review.mjs
+++ public/src/app_learn_code_review.mjs
@@ -1,10 +1,13 @@
-import { list_map } from "./list_map.mjs";
-import { list_scramble } from "./list_scramble.mjs";
-
+import {app_learn_code_quiz_inner_refresh} from "./app_learn_code_quiz_inner_refresh.mjs";
+import {object_property_get} from "./object_property_get.mjs";
+import {list_map} from "./list_map.mjs";
+import {list_scramble} from "./list_scramble.mjs";
 export function app_learn_code_review(lessons) {
     return function app_learn_code_review_inner(parent) {
-        let quizzes = list_map(lessons, object_property_get(lesson, 'quiz'))
+        let quizzes = list_map(lessons, object_property_get(lesson, 'quiz'));
         list_scramble(quizzes);
-        app_learn_code_quiz_inner_refresh
-    }
+        function quiz_next() {
+            app_learn_code_quiz_inner_refresh;
+        }
+    };
 }
