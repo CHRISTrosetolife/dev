7b40d6e5eb85f52d3d5436316d1dec3b70de1413
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-09 16:19:56 -0400

    2024-05-09T20:19:56.683Z  noop

--- public/src/lesson_if_else_string_concat_if.mjs
+++ public/src/lesson_if_else_string_concat_if.mjs
@@ -9,6 +9,10 @@ import { js_code_let } from "./js_code_let.mjs";
 import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
 import { string_delimit } from "./string_delimit.mjs";
 import { integer_random_digit_single } from "./integer_random_digit_single.mjs";
+import { js_code_statement_assign } from "./js_code_statement_assign.mjs";
+import { app_learn_code_log_delimit } from "./app_learn_code_log_delimit.mjs";
+import { app_learn_code_log } from "./app_learn_code_log.mjs";
+import { list_join } from "./list_join.mjs";
 export function lesson_if_else_string_concat_if() {
     let description = 'string combined with two variables';
     let example_message = string_combine('with ', description);
@@ -18,7 +22,10 @@ export function lesson_if_else_string_concat_if() {
             js_code_statement_let_assign('before', string_delimit('number is')), 
             js_code_statement_let_assign('middle', string_delimit(' ')),
             js_code_statement_let_assign('after', string_delimit('negative')),
-            ``
+            `if (${n} >= 0) {`,
+            js_code_statement_assign('middle', ' not '),
+            `}`,
+            app_learn_code_log(list_join(['before', 'middle', 'after'], ' + '))
         ]
         return lesson_if_else_string_concat_variables_quiz(2);
     }, example_before, description, example_message, [screen1]);
