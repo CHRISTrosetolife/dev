9bb762436f8e760e9e7dda22c62b15a9d1d0e322
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-09 15:11:05 -0400

    2024-05-09T19:11:05.244Z  function_auto lesson_if_else_string_concat_variable

--- public/src/lesson_if_else_string_concat_variable.mjs
+++ public/src/lesson_if_else_string_concat_variable.mjs
@@ -1,4 +1,6 @@
-import {lesson_if_else_string_concat_quiz} from "./lesson_if_else_string_concat_quiz.mjs";
+import {list_join} from "./list_join.mjs";
+import {app_learn_code_log} from "./app_learn_code_log.mjs";
+import {lesson_if_else_string_concat_quiz_parts} from "./lesson_if_else_string_concat_quiz_parts.mjs";
 import {html_style_alternate_monospace_short} from "./html_style_alternate_monospace_short.mjs";
 import {lesson_simple} from "./lesson_simple.mjs";
 import {string_combine} from "./string_combine.mjs";
@@ -7,7 +9,8 @@ export function lesson_if_else_string_concat_variable() {
     let example_message = string_combine('with ', description);
     return lesson_simple(function () {
         let concat_count = 3;
-        return lesson_if_else_string_concat_quiz(concat_count);
+        let mapped = lesson_if_else_string_concat_quiz_parts(concat_count);
+        return app_learn_code_log(list_join(mapped, ' + '));
     }, example_before, description, example_message, []);
     function example_before(parent) {
         html_style_alternate_monospace_short(parent, 'strings can be concatentated with a variable');
