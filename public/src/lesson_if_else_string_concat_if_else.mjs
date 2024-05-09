9ebac16338a62a4039f79227c4ac1fcb1d882b48
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-09 18:06:42 -0400

    2024-05-09T22:06:42.075Z  noop

--- public/src/lesson_if_else_string_concat_if_else.mjs
+++ public/src/lesson_if_else_string_concat_if_else.mjs
@@ -16,8 +16,7 @@ export function lesson_if_else_string_concat_if_else() {
             js_code_statement_let_assign('before', string_delimit('')), 
             app_learn_code_log(n), 
             `if (${n} >= 0) {`, 
-            js_code_statement_assign('before', 
-            string_delimit('not ')), 
+            js_code_statement_assign('before', string_delimit('not ')), 
             `} else {`, 
             `}`, 
             app_learn_code_log_join_plus(['before', string_delimit('negative')])
