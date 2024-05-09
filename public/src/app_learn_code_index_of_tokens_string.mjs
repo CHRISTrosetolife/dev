cf2de01b8e34b73637ebe1257867be4c811602b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-09 11:41:10 -0400

    2024-05-09T15:41:09.887Z  app_learn_code_index_of_tokens_string aacbc a,b,c

--- public/src/app_learn_code_index_of_tokens_string.mjs
+++ public/src/app_learn_code_index_of_tokens_string.mjs
@@ -5,7 +5,7 @@ import {app_learn_code_index_of_tokens} from "./app_learn_code_index_of_tokens.m
 import {string_split_comma} from "./string_split_comma.mjs";
 export function app_learn_code_index_of_tokens_string(input, tokens_string) {
     let tokens = string_split_comma(tokens_string);
-    tokens = ["let", "is_positive", "=", "-", "7", ">", 0, ";", "let", "is_even", "=", "-", "7", "%", "2", "===", "0", ";"];
+    tokens = ["let", "is_positive", "=", "-", 7, ">", 0, ";", "let", "is_even", "=", "-", 7, "%", 2, "===", 0, ";"];
     input = `let is_positive = -7 > 0;
     let is_even = -7 % 2 === 0;
     console.log(-7);
