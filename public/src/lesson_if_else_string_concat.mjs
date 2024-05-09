82c554886c24f53c224d2581a544f4260048e081
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-09 13:28:52 -0400

    2024-05-09T17:28:52.733Z  noop

--- public/src/lesson_if_else_string_concat.mjs
+++ public/src/lesson_if_else_string_concat.mjs
@@ -8,13 +8,20 @@ import {list_scramble} from "./list_scramble.mjs";
 import {list_take} from "./list_take.mjs";
 import {list_map} from "./list_map.mjs";
 import {string_delimit} from "./string_delimit.mjs";
+import { list_set } from "./list_set.mjs";
+import { list_skip } from "./list_skip.mjs";
+import { list_length } from "./list_length.mjs";
 export function lesson_if_else_string_concat() {
     let description = 'two strings combined';
     let example_message = string_combine('with ', description);
     return lesson_simple(function () {
         let words = lesson_console_log_quiz_words();
+        let wl = list_length(words)
+        let remain
         list_scramble(words);
         let w = list_take(words, 2);
+        list_skip()
+        list_set(w, 0, string_combine());
         let mapped = list_map(w, string_delimit);
         let [a, b] = mapped;
         return app_learn_code_log(`${a} + ${b}`);
