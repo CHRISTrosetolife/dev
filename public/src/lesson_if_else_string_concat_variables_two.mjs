e8ffc0ea81568ff21834564bef085db37c6ac3b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-09 15:39:28 -0400

    2024-05-09T19:39:27.864Z  noop

--- public/src/lesson_if_else_string_concat_variables_two.mjs
+++ public/src/lesson_if_else_string_concat_variables_two.mjs
@@ -9,8 +9,7 @@ export function lesson_if_else_string_concat_variables_two() {
     let description = 'string combined with two variables';
     let example_message = string_combine('with ', description);
     return lesson_simple(function () {
-        let variable_count = 2;
-        return lesson_if_else_string_concat_variables_quiz(variable_count);
+        return lesson_if_else_string_concat_variables_quiz(2);
     }, example_before, description, example_message, [screen1]);
     function example_before(parent) {
         html_style_alternate_monospace_short(parent, 'strings can be concatentated with multiple variables');
