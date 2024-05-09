a680e8e47f8010ce2cc4ae786c846446a51c3c24
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-09 11:30:08 -0400

    2024-05-09T15:30:08.275Z  function_auto app_learn_code_index_of_tokens_string

--- public/src/app_learn_code_index_of_tokens_string.mjs
+++ public/src/app_learn_code_index_of_tokens_string.mjs
@@ -1,1 +1,6 @@
-export function app_learn_code_index_of_tokens_string(input, tokens_string) {}
+import {app_learn_code_index_of_tokens} from "./app_learn_code_index_of_tokens.mjs";
+import {string_split_comma} from "./string_split_comma.mjs";
+export function app_learn_code_index_of_tokens_string(input, tokens_string) {
+    let tokens = string_split_comma(tokens_string);
+    return app_learn_code_index_of_tokens(input, tokens);
+}
