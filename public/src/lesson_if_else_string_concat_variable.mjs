82df1c50e8f69a94f268b6d8dc392501dcf9fff9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-09 15:13:50 -0400

    2024-05-09T19:13:50.139Z  noop

--- public/src/lesson_if_else_string_concat_variable.mjs
+++ public/src/lesson_if_else_string_concat_variable.mjs
@@ -5,13 +5,15 @@ import {html_style_alternate_monospace_short} from "./html_style_alternate_monos
 import {lesson_simple} from "./lesson_simple.mjs";
 import {string_combine} from "./string_combine.mjs";
 import { list_random_index } from "./list_random_index.mjs";
+import { list_join_newline } from "./list_join_newline.mjs";
 export function lesson_if_else_string_concat_variable() {
     let description = 'string combined a variable';
     let example_message = string_combine('with ', description);
     return lesson_simple(function () {
         let parts = lesson_if_else_string_concat_quiz_parts(2);
         let parts_variable_index = list_random_index(parts)
-        return app_learn_code_log(list_join(parts, ' + '));
+        let lines = [app_learn_code_log(list_join(parts, ' + '))];
+        return list_join_newline(lines)
     }, example_before, description, example_message, []);
     function example_before(parent) {
         html_style_alternate_monospace_short(parent, 'strings can be concatentated with a variable');
