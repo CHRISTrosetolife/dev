7889be41e8f37df2a18f5627992e1b266b38a160
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 21:20:47 -0400

    2024-05-09T01:20:47.218Z  noop

--- public/src/app_learn_code_review.mjs
+++ public/src/app_learn_code_review.mjs
@@ -6,9 +6,12 @@ import {list_scramble} from "./list_scramble.mjs";
 import {list_pop} from "./list_pop.mjs";
 import {list_empty_not_is} from "./list_empty_not_is.mjs";
 import {noop} from "./noop.mjs";
+import { html_p_text } from "./html_p_text.mjs";
+import { string_combine } from "./string_combine.mjs";
 export function app_learn_code_review(lessons) {
+    let description = 'module review';
     return {
-        description: 'module review',
+        description,
         screens: [function app_learn_code_review_inner(parent) {
             let div = html_div(parent);
             let quizzes = list_map(lessons, lesson => object_property_get(lesson, 'quiz'));
@@ -19,6 +22,8 @@ export function app_learn_code_review(lessons) {
                 app_learn_code_quiz_inner_refresh(div, quiz, undefined, () => {
                     if (list_empty_not_is(quizzes)) {
                         quiz_next();
+                    } else {
+                        html_p_text(string_combine('you completed the ',description))
                     }
                 }, noop);
             }
