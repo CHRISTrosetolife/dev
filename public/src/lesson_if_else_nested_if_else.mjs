fbcd5db72e408f7acc697cc49fb0daa9a9a3318c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-09 12:30:28 -0400

    2024-05-09T16:30:27.683Z  function_copy lesson_if_else_nested_if lesson_if_else_nested_if_else

---
+++ public/src/lesson_if_else_nested_if_else.mjs
@@ -0,0 +1,25 @@
+import {html_style_alternate_short_p} from "./html_style_alternate_short_p.mjs";
+import {html_style_monospace_bold} from "./html_style_monospace_bold.mjs";
+import {integer_random_digit_single} from "./integer_random_digit_single.mjs";
+import {html_style_alternate_monospace_short} from "./html_style_alternate_monospace_short.mjs";
+import {list_join_newline} from "./list_join_newline.mjs";
+import {lesson_simple} from "./lesson_simple.mjs";
+import {js_code_statement_let_assign} from "./js_code_statement_let_assign.mjs";
+import {app_learn_code_log_delimit} from "./app_learn_code_log_delimit.mjs";
+import {app_learn_code_log} from "./app_learn_code_log.mjs";
+import {string_combine} from "./string_combine.mjs";
+import {noop} from "./noop.mjs";
+import {html_style_bold} from "./html_style_bold.mjs";
+export function lesson_if_else_nested_if_else() {
+    let description = 'nested `if';
+    let example_message = string_combine('with a ', description);
+    return lesson_simple(function () {
+        let a = integer_random_digit_single();
+        let lines = [app_learn_code_log(a), `if (${a} > 0) {`, app_learn_code_log_delimit(`positive`), `if (${a} % 2 === 0) {`, app_learn_code_log_delimit('positive and even'), `}`, `}`];
+        return list_join_newline(lines);
+    }, example_before, description, example_message, []);
+    function example_before(parent) {
+        html_style_alternate_monospace_short(parent, 'an `if` can be inside another `if`');
+        html_style_alternate_short_p(parent, [noop, html_style_bold, html_style_monospace_bold], 'this is called a `nested `if');
+    }
+}
