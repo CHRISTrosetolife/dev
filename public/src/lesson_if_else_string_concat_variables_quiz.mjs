ef6e6cce821601a3036cc4e1ecb2f2d025426ab8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-09 16:12:36 -0400

    2024-05-09T20:12:36.726Z  noop

--- public/src/lesson_if_else_string_concat_variables_quiz.mjs
+++ public/src/lesson_if_else_string_concat_variables_quiz.mjs
@@ -14,6 +14,7 @@ import {list_scramble} from "./list_scramble.mjs";
 import {list_indices} from "./list_indices.mjs";
 import {add_1} from "./add_1.mjs";
 import {lesson_if_else_string_concat_quiz_parts} from "./lesson_if_else_string_concat_quiz_parts.mjs";
+import { js_code_statement_let_assign_multiple } from "./js_code_statement_let_assign_multiple.mjs";
 export function lesson_if_else_string_concat_variables_quiz(variable_count) {
     let parts = lesson_if_else_string_concat_quiz_parts(add_1(variable_count));
     let indices = list_indices(parts);
@@ -21,13 +22,15 @@ export function lesson_if_else_string_concat_variables_quiz(variable_count) {
     let taken = list_take(indices, variable_count);
     let taken_names = app_learn_code_random_identifiers(variable_count);
     list_sort_string(taken_names, identity);
-    let lines = [];
+    let values = [];
     each_index(taken, (t, index) => {
         let parts_at_variable_index = list_get(parts, t);
         let name = list_get(taken_names, index);
         list_set(parts, t, name);
-        list_add(lines, js_code_statement_let_assign(name, parts_at_variable_index));
+        list_add(values, parts_at_variable_index);
     });
+    let lines = [];
+    list_add(lines, js_code_statement_let_assign_multiple(taken_names, values));
     list_add(lines, app_learn_code_log(list_join(parts, ' + ')));
     return list_join_newline(lines);
 }
