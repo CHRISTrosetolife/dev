3cb572ec91687ccf247ff9051030e5e33bbb0dee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-09 18:11:08 -0400

    2024-05-09T22:11:07.581Z  function_copy lesson_if_else_string_concat_if_else lesson_if_else_ternary

---
+++ public/src/lesson_if_else_ternary.mjs
@@ -0,0 +1,20 @@
+import {app_learn_code_log_join_plus} from "./app_learn_code_log_join_plus.mjs";
+import {lesson_simple} from "./lesson_simple.mjs";
+import {string_combine} from "./string_combine.mjs";
+import {list_join_newline} from "./list_join_newline.mjs";
+import {js_code_statement_let_assign} from "./js_code_statement_let_assign.mjs";
+import {string_delimit} from "./string_delimit.mjs";
+import {integer_random_digit_single} from "./integer_random_digit_single.mjs";
+import {js_code_statement_assign} from "./js_code_statement_assign.mjs";
+import {app_learn_code_log} from "./app_learn_code_log.mjs";
+import {js_code_statement_let} from "./js_code_statement_let.mjs";
+export function lesson_if_else_ternary() {
+    let description = 'string concatentation with an `if` `else';
+    let example_message = string_combine('with ', description);
+    return lesson_simple(function () {
+        let n = integer_random_digit_single();
+        let lines = [app_learn_code_log(n), js_code_statement_let('before'), `if (${n} <= 0) {`, js_code_statement_assign('before', string_delimit('not ')), `} else {`, js_code_statement_assign('before', string_delimit('')), `}`, app_learn_code_log_join_plus(['before', string_delimit('positive')])];
+        return list_join_newline(lines);
+    }, example_before, description, example_message, []);
+    function example_before(parent) {}
+}
