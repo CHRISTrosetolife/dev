fda7e96c8a16e43abe0b572c1a33f36f8a0dcf7b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-08 21:04:30 -0400

    2024-05-09T01:04:30.288Z  function_auto app_learn_code_quiz_inner

--- public/src/app_learn_code_quiz_inner.mjs
+++ public/src/app_learn_code_quiz_inner.mjs
@@ -1,36 +1,15 @@
+import {app_learn_code_quiz_inner_refresh} from "./app_learn_code_quiz_inner_refresh.mjs";
 import {html_style_display_block} from "./html_style_display_block.mjs";
 import {html_style_display_none} from "./html_style_display_none.mjs";
-import {app_learn_code_range_retry} from "./app_learn_code_range_retry.mjs";
 import {app_learn_code_answer_correct} from "./app_learn_code_answer_correct.mjs";
-import {list_get} from "./list_get.mjs";
-import {list_random_index} from "./list_random_index.mjs";
-import {app_learn_code_style_success} from "./app_learn_code_style_success.mjs";
-import {equal} from "./equal.mjs";
-import {list_any} from "./list_any.mjs";
-import {app_learn_code_eval_to_string} from "./app_learn_code_eval_to_string.mjs";
-import {html_style_monospace} from "./html_style_monospace.mjs";
-import {html_disable} from "./html_disable.mjs";
-import {html_style} from "./html_style.mjs";
-import {html_button_width_full_text_click} from "./html_button_width_full_text_click.mjs";
-import {each_index} from "./each_index.mjs";
-import {app_learn_code_code_part_title_output} from "./app_learn_code_code_part_title_output.mjs";
-import {app_learn_code_code_part_title} from "./app_learn_code_code_part_title.mjs";
-import {html_hr} from "./html_hr.mjs";
-import {app_learn_code_code_part_titled_code} from "./app_learn_code_code_part_titled_code.mjs";
-import {list_add} from "./list_add.mjs";
-import {array_new} from "./array_new.mjs";
-import {range} from "./range.mjs";
-import {app_learn_code_code_container} from "./app_learn_code_code_container.mjs";
 import {html_p_text} from "./html_p_text.mjs";
-import {html_clear} from "./html_clear.mjs";
 import {html_div} from "./html_div.mjs";
-import {list_sort_string} from "./list_sort_string.mjs";
 export function app_learn_code_quiz_inner(parent, source_get) {
     let previous;
     let div = html_div(parent);
     refresh(false);
     function refresh(refreshed) {
-        previous = app_learn_code_quiz_inner(div, source_get, previous, on_success, container_bottom_hide);
+        previous = app_learn_code_quiz_inner_refresh(div, source_get, previous, on_success, container_bottom_hide);
         let container_bottom = html_div(div);
         if (!refreshed) {
             container_bottom_hide();
@@ -50,55 +29,3 @@ export function app_learn_code_quiz_inner(parent, source_get) {
         }
     }
 }
-function app_learn_code_quiz_inner(parent, source_get, previous_source, on_answer_right, on_answer_wrong) {
-    html_clear(parent);
-    html_p_text(parent, 'below is a quiz');
-    html_p_text(parent, 'choose the correct output for the code');
-    let container = app_learn_code_code_container(parent);
-    let choices_count = 4;
-    let r = range(choices_count);
-    let choices = array_new();
-    for (let c of r) {
-        for (let i of app_learn_code_range_retry()) {
-            let source = source_get();
-            let answer = app_learn_code_eval_to_string(source);
-            if (list_any(choices, c => equal(c.answer, answer))) {
-                continue;
-            }
-            if (equal(source, previous_source)) {
-                continue;
-            }
-            list_add(choices, {
-                source,
-                answer
-            });
-            break;
-        }
-    }
-    list_sort_string(choices, c => c.answer);
-    let correct_index = list_random_index(choices);
-    let { source } = list_get(choices, correct_index);
-    previous_source = source;
-    app_learn_code_code_part_titled_code(container, source);
-    html_hr(container);
-    app_learn_code_code_part_title(container, app_learn_code_code_part_title_output());
-    each_index(choices, (c, index) => {
-        let { answer } = c;
-        let button = html_button_width_full_text_click(container, answer, function on_click() {
-            if (index === correct_index) {
-                app_learn_code_style_success(button);
-                setTimeout(on_answer_right, 200);
-            } else {
-                on_answer_wrong();
-                html_disable(button);
-                html_style(button, {
-                    'background-color': 'salmon',
-                    'border-color': 'tomato'
-                });
-            }
-        });
-        html_style_monospace(button);
-    });
-    return previous_source;
-}
-
