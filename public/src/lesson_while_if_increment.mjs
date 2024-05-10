6299c227779b9e70c587c6b82a4c5e454106c69b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-10 16:02:38 -0400

    2024-05-10T20:02:38.389Z  function_auto lesson_while_if_increment

--- public/src/lesson_while_if_increment.mjs
+++ public/src/lesson_while_if_increment.mjs
@@ -6,17 +6,20 @@ import { lesson_simple } from "./lesson_simple.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { app_learn_code_log } from "./app_learn_code_log.mjs";
-import { integer_random_digit_single_positive } from "./integer_random_digit_single_positive.mjs";
 import { list_add } from "./list_add.mjs";
 import { range } from "./range.mjs";
 import { integer_random } from "./integer_random.mjs";
+import { subtract } from "./subtract.mjs";
 export function lesson_while_if_increment() {
   let description = "multiple increments";
   let example_message = string_combine("with an ", description);
   return lesson_simple(
     function () {
       let name = app_learn_code_random_identifier();
-      let value = '1';
+      let iterations = 3;
+      let max = 10;
+      let offset = integer_random(0, subtract(max, iterations));
+      let value = "integer_random_digit_single_positive()";
       let lines = [js_code_statement_let_assign(name, value)];
       for (let i of range(integer_random(2, 3))) {
         list_add(lines, js_code_statement_increment(name));
