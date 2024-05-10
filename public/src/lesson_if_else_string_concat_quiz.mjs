3e3aa1d5764b826415a8ecd08ca910dc624ea60f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-10 16:15:46 -0400

    2024-05-10T20:15:46.450Z  function_auto lesson_if_else_string_concat_quiz

--- public/src/lesson_if_else_string_concat_quiz.mjs
+++ public/src/lesson_if_else_string_concat_quiz.mjs
@@ -1,7 +1,6 @@
+import { app_learn_code_log_join_plus } from "./app_learn_code_log_join_plus.mjs";
 import { lesson_if_else_string_concat_quiz_parts } from "./lesson_if_else_string_concat_quiz_parts.mjs";
-import { app_learn_code_log } from "./app_learn_code_log.mjs";
-import { list_join } from "./list_join.mjs";
 export function lesson_if_else_string_concat_quiz(concat_count) {
   let mapped = lesson_if_else_string_concat_quiz_parts(concat_count);
-  return app_learn_code_log(list_join(mapped, " + "));
+  return app_learn_code_log_join_plus(mapped);
 }
