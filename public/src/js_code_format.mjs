fdeb4aee95444048e4296536db1a29524ac78029
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-10 14:08:38 -0400

    2024-05-10T18:08:37.828Z  noop

--- public/src/js_code_format.mjs
+++ public/src/js_code_format.mjs
@@ -1,5 +1,5 @@
 
 import * as prettier from "prettier";
 export async function js_code_format(code) {
-    return await prettier.format(code);
+    return await prettier.format(code, { parser: "babel" });
 }
