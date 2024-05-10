5b70e2c969287704c92adca533b254e64be62200
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-10 14:04:29 -0400

    2024-05-10T18:04:29.237Z  noop

--- public/src/file_js_unparse.mjs
+++ public/src/file_js_unparse.mjs
@@ -1,6 +1,8 @@
 import {file_overwrite} from './file_overwrite.mjs';
 import {js_unparse} from './js_unparse.mjs';
+import * as prettier from "prettier";
 export async function file_js_unparse(file_name, ast) {
     let code = js_unparse(ast);
-    await file_overwrite(file_name, code);
+    let prettied = await prettier.format(code);
+    await file_overwrite(file_name, prettied);
 }
