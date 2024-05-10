00be71e71b704f83a1aaebb3926ecf6c69f8ff64
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-10 13:40:56 -0400

    2024-05-10T17:40:56.380Z  function_auto lesson_log_number

--- public/src/lesson_log_number.mjs
+++ public/src/lesson_log_number.mjs
@@ -1,9 +1,9 @@
-import {lesson_add_quiz} from "./lesson_add_quiz.mjs";
+import {lesson_log_number_quiz} from "./lesson_log_number_quiz.mjs";
 import {lesson_simple} from "./lesson_simple.mjs";
 import {lesson_log_number_explain} from "./lesson_log_number_explain.mjs";
 export function lesson_log_number() {
     let example_message = 'outputting a number to the console';
     let description = example_message;
-    return lesson_simple(lesson_add_quiz, example_before, description, example_message, [lesson_log_number_explain]);
+    return lesson_simple(lesson_log_number_quiz, example_before, description, example_message, [lesson_log_number_explain]);
     function example_before(parent) {}
 }
