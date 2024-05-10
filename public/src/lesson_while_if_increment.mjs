a1dac8c1cb9b08f3172607e58b4a972432681cb5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-10 16:11:01 -0400

    2024-05-10T20:11:01.146Z  noop

--- public/src/lesson_while_if_increment.mjs
+++ public/src/lesson_while_if_increment.mjs
@@ -9,8 +9,8 @@ import { app_learn_code_log } from "./app_learn_code_log.mjs";
 import { integer_random } from "./integer_random.mjs";
 import { subtract } from "./subtract.mjs";
 export function lesson_while_if_increment() {
-  let description = "multiple increments";
-  let example_message = string_combine("with an ", description);
+  let description = "increment inside `if`";
+  let example_message = string_combine("with an increment inside of an `if`");
   return lesson_simple(
     function () {
       let name = app_learn_code_random_identifier();
