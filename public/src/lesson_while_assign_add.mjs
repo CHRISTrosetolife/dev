71aaf0e2e09327607e22d37565ab737479684636
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-10 14:20:04 -0400

    2024-05-10T18:20:04.020Z  function_auto lesson_while_assign_add

--- public/src/lesson_while_assign_add.mjs
+++ public/src/lesson_while_assign_add.mjs
@@ -6,11 +6,7 @@ import { list_join_newline } from "./list_join_newline.mjs";
 import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
 import { string_delimit } from "./string_delimit.mjs";
 import { integer_random_digit_single } from "./integer_random_digit_single.mjs";
-import { js_code_statement_assign } from "./js_code_statement_assign.mjs";
 import { app_learn_code_log } from "./app_learn_code_log.mjs";
-import { js_code_statement_let } from "./js_code_statement_let.mjs";
-import { html_p_text } from "./html_p_text.mjs";
-import { app_learn_code_code_part_contrast } from "./app_learn_code_code_part_contrast.mjs";
 export function lesson_while_assign_add() {
   let description = "two ternaries";
   let example_message = string_combine("with ", description);
