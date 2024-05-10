7f83764011ce00a2c2910fb600dcff946516ec87
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-10 15:58:22 -0400

    2024-05-10T19:58:22.699Z  function_auto lesson_while_increment_multiple

--- public/src/lesson_while_increment_multiple.mjs
+++ public/src/lesson_while_increment_multiple.mjs
@@ -1,14 +1,12 @@
+import { html_style_alternate_monospace_short } from "./html_style_alternate_monospace_short.mjs";
 import { js_code_statement_increment } from "./js_code_statement_increment.mjs";
 import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
 import { app_learn_code_random_identifier } from "./app_learn_code_random_identifier.mjs";
-import { app_learn_code_code_part_contrast } from "./app_learn_code_code_part_contrast.mjs";
-import { js_code_statement_add_assign } from "./js_code_statement_add_assign.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { app_learn_code_log } from "./app_learn_code_log.mjs";
 import { integer_random_digit_single_positive } from "./integer_random_digit_single_positive.mjs";
-import { html_p_text } from "./html_p_text.mjs";
 import { list_add } from "./list_add.mjs";
 import { range } from "./range.mjs";
 import { integer_random } from "./integer_random.mjs";
@@ -20,7 +18,7 @@ export function lesson_while_increment_multiple() {
       let name = app_learn_code_random_identifier();
       let value = integer_random_digit_single_positive();
       let lines = [js_code_statement_let_assign(name, value)];
-      for (let i of range(integer_random(2,4))) {
+      for (let i of range(integer_random(2, 4))) {
         list_add(lines, js_code_statement_increment(name));
       }
       list_add(lines, app_learn_code_log(name));
@@ -29,19 +27,12 @@ export function lesson_while_increment_multiple() {
     example_before,
     description,
     example_message,
-    [screen1],
+    [],
   );
   function example_before(parent) {
-    'a variable may be incremented ( `++` ) multiple times'
-  }
-  function screen1(parent) {
-    html_p_text(parent, "here is some code :");
-    let a = "a";
-    app_learn_code_code_part_contrast(
+    html_style_alternate_monospace_short(
       parent,
-      js_code_statement_add_assign(a, "1"),
+      "a variable may be incremented ( `++` ) multiple times",
     );
-    html_p_text(parent, "that code can be shortened to :");
-    app_learn_code_code_part_contrast(parent, js_code_statement_increment(a));
   }
 }
