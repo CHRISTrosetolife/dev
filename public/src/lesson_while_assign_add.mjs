f8d3300234e54b8c6007dbd80ab343f365bc334b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-10 14:29:25 -0400

    2024-05-10T18:29:24.773Z  function_auto lesson_while_assign_add

--- public/src/lesson_while_assign_add.mjs
+++ public/src/lesson_while_assign_add.mjs
@@ -1,13 +1,9 @@
 import { app_learn_code_code_part_contrast } from "./app_learn_code_code_part_contrast.mjs";
 import { js_code_statement_add_assign } from "./js_code_statement_add_assign.mjs";
-import { js_code_ternary } from "./js_code_ternary.mjs";
-import { app_learn_code_log_join_plus } from "./app_learn_code_log_join_plus.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
-import { string_delimit } from "./string_delimit.mjs";
-import { integer_random_digit_single } from "./integer_random_digit_single.mjs";
 import { app_learn_code_log } from "./app_learn_code_log.mjs";
 import { js_code_statement_assign } from "./js_code_statement_assign.mjs";
 import { app_learn_code_random_identifier } from "./app_learn_code_random_identifier.mjs";
@@ -18,11 +14,12 @@ export function lesson_while_assign_add() {
   let example_message = string_combine("with an ", description);
   return lesson_simple(
     function () {
-      let a = app_learn_code_random_identifier();
-      let values = call_multiple(integer_random_digit_single_positive, 2);
+      let n = app_learn_code_random_identifier();
+      let [a, b] = call_multiple(integer_random_digit_single_positive, 2);
       let lines = [
-        js_code_statement_add_assign(a, b),
-        app_learn_code_log("even"),
+        js_code_statement_let_assign(n, b),
+        js_code_statement_add_assign(n, a),
+        app_learn_code_log(n),
       ];
       return list_join_newline(lines);
     },
@@ -34,10 +31,16 @@ export function lesson_while_assign_add() {
   function example_before(parent) {}
   function screen1(parent) {
     "here is some code";
-    app_learn_code_code_part_contrast(parent, js_code_statement_assign("a", "a + b"));
+    app_learn_code_code_part_contrast(
+      parent,
+      js_code_statement_assign("a", "a + b"),
+    );
     ("that code can be shortened to :");
     let a = "a";
     let b = "b";
-    app_learn_code_code_part_contrast(parent, js_code_statement_add_assign(a, b));
+    app_learn_code_code_part_contrast(
+      parent,
+      js_code_statement_add_assign(a, b),
+    );
   }
 }
