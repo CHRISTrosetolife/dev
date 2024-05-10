0858591a75242f65ac00b9b93b2a077afb5bf81f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-10 14:30:04 -0400

    2024-05-10T18:30:03.979Z  noop

--- public/src/app_learn_code_modules.mjs
+++ public/src/app_learn_code_modules.mjs
@@ -52,6 +52,7 @@ import { lesson_if_else_string_empty } from "./lesson_if_else_string_empty.mjs";
 import { lesson_if_else_ternary } from "./lesson_if_else_ternary.mjs";
 import { lesson_if_else_ternary_two } from "./lesson_if_else_ternary_two.mjs";
 import { lesson_if_else_ternary_parity } from "./lesson_if_else_ternary_parity.mjs";
+import { lesson_while_assign_add } from "./lesson_while_assign_add.mjs";
 export function app_learn_code_modules() {
   let modules = [];
   let module_current;
@@ -116,6 +117,7 @@ export function app_learn_code_modules() {
   list_add(module_current, lesson_if_else_ternary_parity);
   list_add(module_current, lesson_if_else_ternary_two);
   module_current = module_new(modules, "`while` loops");
+  list_add(module_current, lesson_while_assign_add);
   return modules;
   function module_new(modules, module_name) {
     let module_current = [];
