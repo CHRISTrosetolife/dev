ffcff9aff2079bd0efd6ca41ae8e72745b252431
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-11 12:35:55 -0400

    2024-05-11T16:35:55.510Z  function_auto lesson_while_while

--- public/src/lesson_while_while.mjs
+++ public/src/lesson_while_while.mjs
@@ -1,24 +1,17 @@
+import { lesson_while_generic_1 } from "./lesson_while_generic_1.mjs";
 import { html_style_alternate_monospace_short_multiple } from "./html_style_alternate_monospace_short_multiple.mjs";
 import { js_code_statement_increment } from "./js_code_statement_increment.mjs";
 import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
-import { app_learn_code_random_identifier } from "./app_learn_code_random_identifier.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { app_learn_code_log } from "./app_learn_code_log.mjs";
-import { integer_random } from "./integer_random.mjs";
-import { subtract } from "./subtract.mjs";
 import { string_combine } from "./string_combine.mjs";
 export function lesson_while_while() {
   let description = "`while` loop";
   let example_message = string_combine("with a ", description);
   return lesson_simple(
     function () {
-      let name = app_learn_code_random_identifier();
-      let iterations = integer_random(2, 3);
-      let max_value = 10;
-      let offset = integer_random(0, subtract(max_value, iterations));
-      let value = 1 + offset;
-      let loop_end = iterations + offset;
+      let { name, value, loop_end } = lesson_while_generic_1();
       let lines = [
         js_code_statement_let_assign(name, value),
         `while (${name} <= ${loop_end}) {`,
