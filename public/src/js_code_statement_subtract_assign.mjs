fede7d5a288a627d3dfb6fdad902c0f549796fca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-11 12:04:52 -0400

    2024-05-11T16:04:51.856Z  function_auto lesson_while_assign_subtract

--- public/src/js_code_statement_subtract_assign.mjs
+++ public/src/js_code_statement_subtract_assign.mjs
@@ -1,4 +1,4 @@
 import { js_code_statement } from "./js_code_statement.mjs";
 export function js_code_statement_subtract_assign(a, b) {
-  return js_code_statement(`${a} += ${b}`);
+  return js_code_statement(`${a} -= ${b}`);
 }
--- public/src/lesson_while_assign_subtract.mjs
+++ public/src/lesson_while_assign_subtract.mjs
@@ -1,7 +1,7 @@
+import { js_code_statement_subtract_assign } from "./js_code_statement_subtract_assign.mjs";
 import { lesson_screen_variables_numbers_sorted } from "./lesson_screen_variables_numbers_sorted.mjs";
 import { app_learn_code_random_identifiers } from "./app_learn_code_random_identifiers.mjs";
 import { app_learn_code_code_part_contrast } from "./app_learn_code_code_part_contrast.mjs";
-import { js_code_statement_add_assign } from "./js_code_statement_add_assign.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
@@ -26,11 +26,11 @@ export function lesson_while_assign_subtract() {
       let [a, b] = names;
       let values = call_multiple(integer_random_digit_single_positive, 2);
       let [v_a, v_b] = values;
-      list_set(values, 0, add(v_a, v_b))
+      list_set(values, 0, add(v_a, v_b));
       list_sort(values, identity);
       let lines = [
         js_code_statement_let_assign_multiple(names, values),
-        js_code_statement_add_assign(a, b),
+        js_code_statement_subtract_assign(a, b),
         app_learn_code_log(a),
       ];
       return list_join_newline(lines);
@@ -45,14 +45,14 @@ export function lesson_while_assign_subtract() {
     html_p_text(parent, "here is some code :");
     app_learn_code_code_part_contrast(
       parent,
-      js_code_statement_assign("a", "a + b"),
+      js_code_statement_assign("a", "a - b"),
     );
     html_p_text(parent, "that code can be shortened to :");
     let a = "a";
     let b = "b";
     app_learn_code_code_part_contrast(
       parent,
-      js_code_statement_add_assign(a, b),
+      js_code_statement_subtract_assign(a, b),
     );
   }
 }
