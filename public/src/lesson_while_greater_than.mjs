8dcb7c190898f5cbbda099b7dd2996d2814799d5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-11 16:09:53 -0400

    2024-05-11T20:09:53.381Z  function_auto lesson_while_greater_than

--- public/src/lesson_while_greater_than.mjs
+++ public/src/lesson_while_greater_than.mjs
@@ -1,23 +1,22 @@
+import { js_code_statement_decrement } from "./js_code_statement_decrement.mjs";
 import { lesson_while_generic_1 } from "./lesson_while_generic_1.mjs";
-import { subtract_1 } from "./subtract_1.mjs";
 import { html_style_alternate_monospace_short_multiple } from "./html_style_alternate_monospace_short_multiple.mjs";
-import { js_code_statement_increment } from "./js_code_statement_increment.mjs";
 import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { app_learn_code_log } from "./app_learn_code_log.mjs";
 import { string_combine } from "./string_combine.mjs";
 export function lesson_while_greater_than() {
-  let description = "`while` loop with less than ( `<` )";
+  let description = "reverse `while` loop with greater than ( `>` )";
   let example_message = string_combine("with a ", description);
   return lesson_simple(
     function () {
       let { name, value, loop_end } = lesson_while_generic_1();
       let lines = [
-        js_code_statement_let_assign(name, subtract_1(value)),
-        `while (${name} < ${loop_end}) {`,
+        js_code_statement_let_assign(name, loop_end),
+        `while (${name} > ${value}) {`,
         app_learn_code_log(`'inside ' + ${name}`),
-        js_code_statement_increment(name),
+        js_code_statement_decrement(name),
         `}`,
         app_learn_code_log(`'after ' + ${name}`),
       ];
