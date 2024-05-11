ddb93838b6933a63f6caf1490cf6faac75414241
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-11 18:09:12 -0400

    2024-05-11T22:09:11.911Z  function_auto lesson_for_while

--- public/src/lesson_for_while.mjs
+++ public/src/lesson_for_while.mjs
@@ -31,7 +31,7 @@ export function lesson_for_while() {
   function example_before(parent) {}
   function screen1(parent) {
     html_p_text(parent, "here is some code :");
-    let lines = [`while (a) {`, `// b``}`];
+    let lines = [`while (a) {`, `// b`, `}`];
     app_learn_code_code_part_contrast(parent, list_join_newline(lines));
     html_p_text(parent, "that code can be rewritten to the following :");
     let lines2 = [`for ( ; a; ) {`, `// b``}`];
