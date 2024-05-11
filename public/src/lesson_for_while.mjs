90c8b65d60394588309ad4a166d0f7d34a5eb52d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-11 18:01:54 -0400

    2024-05-11T22:01:52.999Z  function_copy lesson_while_while lesson_for_while

---
+++ public/src/lesson_for_while.mjs
@@ -0,0 +1,39 @@
+import { lesson_while_generic_1 } from "./lesson_while_generic_1.mjs";
+import { html_style_alternate_monospace_short_multiple } from "./html_style_alternate_monospace_short_multiple.mjs";
+import { js_code_statement_increment } from "./js_code_statement_increment.mjs";
+import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
+import { lesson_simple } from "./lesson_simple.mjs";
+import { list_join_newline } from "./list_join_newline.mjs";
+import { app_learn_code_log } from "./app_learn_code_log.mjs";
+import { string_combine } from "./string_combine.mjs";
+export function lesson_for_while() {
+  let description = "`while` loop";
+  let example_message = string_combine("with a ", description);
+  return lesson_simple(
+    function () {
+      let { name, value, loop_end } = lesson_while_generic_1();
+      let lines = [
+        js_code_statement_let_assign(name, value),
+        `while (${name} <= ${loop_end}) {`,
+        app_learn_code_log(`'inside ' + ${name}`),
+        js_code_statement_increment(name),
+        `}`,
+        app_learn_code_log(`'after ' + ${name}`),
+      ];
+      return list_join_newline(lines);
+    },
+    example_before,
+    description,
+    example_message,
+    [screen1],
+  );
+  function example_before(parent) {}
+  function screen1(parent) {
+    html_style_alternate_monospace_short_multiple(parent, [
+      "an `if` is ran only once",
+      "a `while` is similar to an `if`",
+      "a `while` is an `if` that runs over and over",
+      "a `while` will be ran until what is in between the parenthesis is `false`",
+    ]);
+  }
+}
