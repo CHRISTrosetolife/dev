f5678f520a7541f1e104a8e0bc3e9bcf1be7727c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-10 20:10:16 -0400

    2024-05-11T00:10:15.704Z  function_auto lesson_while_add_two

--- public/src/lesson_while_add_two.mjs
+++ public/src/lesson_while_add_two.mjs
@@ -1,5 +1,5 @@
+import { js_code_statement_add_assign } from "./js_code_statement_add_assign.mjs";
 import { html_style_alternate_monospace_short_multiple } from "./html_style_alternate_monospace_short_multiple.mjs";
-import { js_code_statement_increment } from "./js_code_statement_increment.mjs";
 import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
 import { app_learn_code_random_identifier } from "./app_learn_code_random_identifier.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
@@ -8,6 +8,7 @@ import { app_learn_code_log } from "./app_learn_code_log.mjs";
 import { integer_random } from "./integer_random.mjs";
 import { subtract } from "./subtract.mjs";
 import { string_combine } from "./string_combine.mjs";
+import { multiply } from "./multiply.mjs";
 export function lesson_while_add_two() {
   let description = "`while` loop";
   let example_message = string_combine("with a ", description);
@@ -15,15 +16,17 @@ export function lesson_while_add_two() {
     function () {
       let name = app_learn_code_random_identifier();
       let iterations = integer_random(2, 3);
+      let increment = integer_random(2, 3);
+      let product = multiply(iterations, increment);
       let max_value = 10;
-      let offset = integer_random(0, subtract(max_value, iterations));
+      let offset = integer_random(0, subtract(max_value, product));
       let value = 1 + offset;
-      let loop_end = iterations + offset;
+      let loop_end = product + offset;
       let lines = [
         js_code_statement_let_assign(name, value),
         `while (${name} <= ${loop_end}) {`,
         app_learn_code_log(`'inside ' + ${name}`),
-        js_code_statement_increment(name),
+        js_code_statement_add_assign(name, increment),
         `}`,
         app_learn_code_log(`'after ' + ${name}`),
       ];
