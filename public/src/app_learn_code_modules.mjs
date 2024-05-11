4f86aab621174a31544d3dc8f3d97f71d74f0e4a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-11 16:10:13 -0400

    2024-05-11T20:10:13.520Z  noop

--- public/src/app_learn_code_modules.mjs
+++ public/src/app_learn_code_modules.mjs
@@ -68,6 +68,7 @@ import { lesson_while_less_than } from "./lesson_while_less_than.mjs";
 import { lesson_while_add_two } from "./lesson_while_add_two.mjs";
 import { lesson_while_assign_subtract } from "./lesson_while_assign_subtract.mjs";
 import { lesson_while_if_decrement_two } from "./lesson_while_if_decrement_two.mjs";
+import { lesson_while_greater_than } from "./lesson_while_greater_than.mjs";
 export function app_learn_code_modules() {
   let modules = [];
   let module_current;
@@ -149,6 +150,7 @@ export function app_learn_code_modules() {
   list_add(module_current, lesson_while_if_decrement);
   list_add(module_current, lesson_while_if_decrement_two);
   list_add(module_current, lesson_while_reverse);
+  list_add(module_current, lesson_while_greater_than);
   return modules;
   function module_new(modules, module_name) {
     let module_current = [];
