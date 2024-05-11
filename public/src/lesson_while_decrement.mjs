086ab92db658677100d97423064408f22afdb1f5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-11 12:09:55 -0400

    2024-05-11T16:09:55.630Z  noop

--- public/src/lesson_while_decrement.mjs
+++ public/src/lesson_while_decrement.mjs
@@ -10,8 +10,8 @@ import { app_learn_code_log } from "./app_learn_code_log.mjs";
 import { integer_random_digit_single_positive } from "./integer_random_digit_single_positive.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 export function lesson_while_decrement() {
-  let description = "increment ( `++` )";
-  let example_message = string_combine("with an ", description);
+  let description = "decrement ( `--` )";
+  let example_message = string_combine("with a ", description);
   return lesson_simple(
     function () {
       let name = app_learn_code_random_identifier();
