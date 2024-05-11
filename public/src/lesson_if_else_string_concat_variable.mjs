02867c0b00e42b0ef2effd04c1021ffbd76b9e9b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-11 10:55:00 -0400

    2024-05-11T14:55:00.447Z  noop

--- public/src/lesson_if_else_string_concat_variable.mjs
+++ public/src/lesson_if_else_string_concat_variable.mjs
@@ -2,12 +2,18 @@ import { lesson_if_else_string_concat_variables_quiz } from "./lesson_if_else_st
 import { html_style_alternate_monospace_short } from "./html_style_alternate_monospace_short.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
 import { string_combine } from "./string_combine.mjs";
+import { list_sort_string } from "./list_sort_string.mjs";
+import { identity } from "./identity.mjs";
+import { list_map } from "./list_map.mjs";
+import { string_delimit } from "./string_delimit.mjs";
 export function lesson_if_else_string_concat_variable() {
   let description = "string combined a variable";
   let example_message = string_combine("with ", description);
   return lesson_simple(
     function () {
-      
+      let strings = app_learn_code_random_identifiers(2);
+      list_sort_string(strings, identity);
+      let mapped = list_map(strings, string_delimit)
       return lesson_if_else_string_concat_variables_quiz(1);
     },
     example_before,
