2a39bcb78b7639c558435580916de29209f29183
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-11 19:36:56 -0400

    2024-05-11T23:36:55.958Z  function_auto lesson_for_less_than

--- public/src/app_learn_code_modules.mjs
+++ public/src/app_learn_code_modules.mjs
@@ -72,6 +72,7 @@ import { lesson_while_add_two } from "./lesson_while_add_two.mjs";
 import { lesson_while_assign_subtract } from "./lesson_while_assign_subtract.mjs";
 import { lesson_while_if_decrement_two } from "./lesson_while_if_decrement_two.mjs";
 import { lesson_while_greater_than } from "./lesson_while_greater_than.mjs";
+import { lesson_for_less_than } from "./lesson_for_less_than.mjs";
 export function app_learn_code_modules() {
   let modules = [];
   let module_current;
@@ -158,6 +159,8 @@ export function app_learn_code_modules() {
   list_add(module_current, lesson_for_while);
   list_add(module_current, lesson_for_while_2);
   list_add(module_current, lesson_for);
+  list_add(module_current, lesson_for_less_than);
+  
   return modules;
   function module_new(modules, module_name) {
     let module_current = [];
