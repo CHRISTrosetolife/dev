6e32eff945f8da6a23eacb7e5bc8249730fc0d60
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-11 12:32:08 -0400

    2024-05-11T16:32:08.071Z  noop

--- public/src/lesson_while_add_two.mjs
+++ public/src/lesson_while_add_two.mjs
@@ -12,8 +12,7 @@ export function lesson_while_add_two() {
   let example_message = string_combine("with `while` loop with `+=`");
   return lesson_simple(
     function () {
-      let increment = integer_random(2, 3);
-      let { name, value, loop_end } = lesson_while_generic(increment);
+      let { name, value, loop_end } = lesson_while_generic(integer_random(2, 3));
       let lines = [
         js_code_statement_let_assign(name, value),
         `while (${name} <= ${loop_end}) {`,
