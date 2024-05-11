9ffa90605f7de9bede8ca1bb059dd70cdc124205
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-11 12:33:11 -0400

    2024-05-11T16:33:11.101Z  function_auto lesson_while_less_than

--- public/src/lesson_while_less_than.mjs
+++ public/src/lesson_while_less_than.mjs
@@ -1,26 +1,23 @@
+import { subtract_1 } from "./subtract_1.mjs";
+import { lesson_while_generic } from "./lesson_while_generic.mjs";
 import { html_style_alternate_monospace_short_multiple } from "./html_style_alternate_monospace_short_multiple.mjs";
 import { js_code_statement_increment } from "./js_code_statement_increment.mjs";
 import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
-import { app_learn_code_random_identifier } from "./app_learn_code_random_identifier.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { app_learn_code_log } from "./app_learn_code_log.mjs";
 import { integer_random } from "./integer_random.mjs";
-import { subtract } from "./subtract.mjs";
 import { string_combine } from "./string_combine.mjs";
 export function lesson_while_less_than() {
   let description = "`while` loop with less than ( `<` )";
   let example_message = string_combine("with a ", description);
   return lesson_simple(
     function () {
-      let name = app_learn_code_random_identifier();
-      let iterations = integer_random(2, 3) + 1;
-      let max_value = 10;
-      let offset = integer_random(0, subtract(max_value, iterations));
-      let value = 1 + offset;
-      let loop_end = iterations + offset;
+      let { name, value, loop_end } = lesson_while_generic(
+        integer_random(2, 3),
+      );
       let lines = [
-        js_code_statement_let_assign(name, value),
+        js_code_statement_let_assign(name, subtract_1(value)),
         `while (${name} < ${loop_end}) {`,
         app_learn_code_log(`'inside ' + ${name}`),
         js_code_statement_increment(name),
@@ -37,11 +34,11 @@ export function lesson_while_less_than() {
   function example_before(parent) {}
   function screen1(parent) {
     html_style_alternate_monospace_short_multiple(parent, [
-      'a `while` loop can use a `<` instead of `<=`',
-      'if a `while` loop says `while (a <= 10)` then if `a` is `10` then the `while` loop will run',
-      'remember , `10 === 10` is `true`',
-      'if a `while` loop says `while (a < 10)` then if `a` is `10` then the `while` loop will not run',
-      'remember , `10 < 10` is `false`'
+      "a `while` loop can use a `<` instead of `<=`",
+      "if a `while` loop says `while (a <= 10)` then if `a` is `10` then the `while` loop will run",
+      "remember , `10 === 10` is `true`",
+      "if a `while` loop says `while (a < 10)` then if `a` is `10` then the `while` loop will not run",
+      "remember , `10 < 10` is `false`",
     ]);
   }
 }
