bfa2481331707fd481399e44c9cde1610a263b6c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-11 12:17:04 -0400

    2024-05-11T16:17:04.026Z  function_auto app_learn_code_modules

--- public/src/app_learn_code_modules.mjs
+++ public/src/app_learn_code_modules.mjs
@@ -1,3 +1,4 @@
+import { lesson_while_decrement_multiple } from "./lesson_while_decrement_multiple.mjs";
 import { lesson_while_decrement } from "./lesson_while_decrement.mjs";
 import { lesson_while_if_increment_two } from "./lesson_while_if_increment_two.mjs";
 import { lesson_while_increment_multiple } from "./lesson_while_increment_multiple.mjs";
@@ -141,6 +142,7 @@ export function app_learn_code_modules() {
   module_current = module_new(modules, "reverse `while` loops");
   list_add(module_current, lesson_while_assign_subtract);
   list_add(module_current, lesson_while_decrement);
+  list_add(module_current, lesson_while_decrement_multiple);
   return modules;
   function module_new(modules, module_name) {
     let module_current = [];
