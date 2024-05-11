35ede41ec69464e3fc99d8f882c4385b1794ba1b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-11 11:08:23 -0400

    2024-05-11T15:08:22.941Z  function_auto lesson_if_else_string_concat_variable

--- public/src/lesson_if_else_string_concat_variable.mjs
+++ public/src/lesson_if_else_string_concat_variable.mjs
@@ -1,44 +1,15 @@
-import { list_last } from "./list_last.mjs";
-import { list_join_newline } from "./list_join_newline.mjs";
-import { app_learn_code_log_join_plus } from "./app_learn_code_log_join_plus.mjs";
-import { js_code_statement_let_assign_multiple } from "./js_code_statement_let_assign_multiple.mjs";
-import { app_learn_code_random_identifiers } from "./app_learn_code_random_identifiers.mjs";
+import { lesson_if_else_string_concat_variables_quiz } from "./lesson_if_else_string_concat_variables_quiz.mjs";
 import { html_style_alternate_monospace_short } from "./html_style_alternate_monospace_short.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
 import { string_combine } from "./string_combine.mjs";
-import { list_sort_string } from "./list_sort_string.mjs";
-import { identity } from "./identity.mjs";
-import { list_map } from "./list_map.mjs";
-import { string_delimit } from "./string_delimit.mjs";
-import { list_add } from "./list_add.mjs";
-import { list_scramble } from "./list_scramble.mjs";
 import { app_learn_code_code_part_contrast } from "./app_learn_code_code_part_contrast.mjs";
 import { html_p_text } from "./html_p_text.mjs";
-import { add_1 } from "./add_1.mjs";
-import { list_take } from "./list_take.mjs";
 export function lesson_if_else_string_concat_variable() {
   let description = "string combined a variable";
   let example_message = string_combine("with ", description);
   return lesson_simple(
     function () {
-      let variable_count = 1;
-      let strings = app_learn_code_random_identifiers(add_1(variable_count));
-      list_sort_string(strings, identity);
-      let mapped = list_map(strings, string_delimit);
-      let mapped_taken = list_take(mapped, variable_count);
-      let names = app_learn_code_random_identifiers(variable_count);
-      let parts = [list_last(mapped)];
-      for (let name of names) {
-        list_add(parts, name);
-      }
-      list_scramble(parts);
-      let lines = [];
-      list_add(
-        lines,
-        js_code_statement_let_assign_multiple(names, mapped_taken),
-      );
-      list_add(lines, app_learn_code_log_join_plus(parts));
-      return list_join_newline(lines);
+      return lesson_if_else_string_concat_variables_quiz(1);
     },
     example_before,
     description,
