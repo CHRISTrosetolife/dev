3d2da3b971aed23cf2e82a95e77b0fd63f4e213e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-11 12:31:59 -0400

    2024-05-11T16:31:59.306Z  function_auto lesson_while_add_two

--- public/src/lesson_while_add_two.mjs
+++ public/src/lesson_while_add_two.mjs
@@ -1,27 +1,19 @@
+import { lesson_while_generic } from "./lesson_while_generic.mjs";
 import { html_style_alternate_monospace_short } from "./html_style_alternate_monospace_short.mjs";
 import { js_code_statement_add_assign } from "./js_code_statement_add_assign.mjs";
 import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
-import { app_learn_code_random_identifier } from "./app_learn_code_random_identifier.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { app_learn_code_log } from "./app_learn_code_log.mjs";
 import { integer_random } from "./integer_random.mjs";
-import { subtract } from "./subtract.mjs";
 import { string_combine } from "./string_combine.mjs";
-import { multiply } from "./multiply.mjs";
 export function lesson_while_add_two() {
   let description = "`while` loop with `+=` `2` or `3`";
   let example_message = string_combine("with `while` loop with `+=`");
   return lesson_simple(
     function () {
       let increment = integer_random(2, 3);
-      let name = app_learn_code_random_identifier();
-      let iterations = integer_random(2, 3) - 1;
-      let product = multiply(iterations, increment);
-      let max_value = 10;
-      let offset = integer_random(0, subtract(max_value, product));
-      let value = 1 + offset;
-      let loop_end = product + value;
+      let { name, value, loop_end } = lesson_while_generic(increment);
       let lines = [
         js_code_statement_let_assign(name, value),
         `while (${name} <= ${loop_end}) {`,
---
+++ public/src/lesson_while_generic.mjs
@@ -0,0 +1,18 @@
+import { subtract } from "./subtract.mjs";
+import { multiply } from "./multiply.mjs";
+import { integer_random } from "./integer_random.mjs";
+import { app_learn_code_random_identifier } from "./app_learn_code_random_identifier.mjs";
+export function lesson_while_generic(increment) {
+  let name = app_learn_code_random_identifier();
+  let iterations = integer_random(2, 3) - 1;
+  let product = multiply(iterations, increment);
+  let max_value = 10;
+  let offset = integer_random(0, subtract(max_value, product));
+  let value = 1 + offset;
+  let loop_end = product + value;
+  return {
+    name,
+    value,
+    loop_end,
+  };
+}
