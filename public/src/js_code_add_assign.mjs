6ff7b187ad80a7be9648b4461c0fb1a69a7aa8d7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-12 17:19:15 -0400

    2024-05-12T21:19:15.490Z  function_auto js_code_statement_add_assign

---
+++ public/src/js_code_add_assign.mjs
@@ -0,0 +1,3 @@
+export function js_code_add_assign(a, b) {
+  return `${a} += ${b}`;
+}
--- public/src/js_code_statement_add_assign.mjs
+++ public/src/js_code_statement_add_assign.mjs
@@ -1,4 +1,5 @@
+import { js_code_add_assign } from "./js_code_add_assign.mjs";
 import { js_code_statement } from "./js_code_statement.mjs";
 export function js_code_statement_add_assign(a, b) {
-  return js_code_statement(`${a} += ${b}`);
+  return js_code_statement(js_code_add_assign(a, b));
 }
