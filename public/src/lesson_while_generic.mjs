0ee6d7dbfc44349ac843b028c8c3728eef8e6c8d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-12 17:24:42 -0400

    2024-05-12T21:24:41.923Z  noop

--- public/src/lesson_while_generic.mjs
+++ public/src/lesson_while_generic.mjs
@@ -5,8 +5,8 @@ import { integer_random } from "./integer_random.mjs";
 import { app_learn_code_random_identifier } from "./app_learn_code_random_identifier.mjs";
 export function lesson_while_generic(increment) {
   let name = app_learn_code_random_identifier();
-  let iterations = integer_random(2, 3) - 1;
-  let product = multiply(iterations, increment);
+  let iterations_less_1 = integer_random(2, 3) - 1;
+  let product = multiply(iterations_less_1, increment);
   let max_value = 10;
   let offset = integer_random(0, subtract_1(subtract(max_value, product)));
   let value = 1 + offset;
