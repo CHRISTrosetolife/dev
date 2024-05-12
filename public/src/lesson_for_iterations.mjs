9cb48e6331d7dcff1489d3133d3c65a03b9944c7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-12 12:09:02 -0400

    2024-05-12T16:09:02.546Z  noop

--- public/src/lesson_for_iterations.mjs
+++ public/src/lesson_for_iterations.mjs
@@ -17,7 +17,7 @@ export function lesson_for_iterations() {
     function () {
       let { name, value, loop_end } = lesson_while_generic_1();
       let lines = [
-        js_code_statement_let_assign("counter"),
+        js_code_statement_let_assign("counter", '0'),
         js_code_statement_let(name),
         `for (${js_code_statement_assign(name, value)} ${name} <= ${loop_end}; ${js_code_increment(name)}) {`,
         js_code_statement_increment("counter"),
