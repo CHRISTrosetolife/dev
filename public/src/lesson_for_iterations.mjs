02661e80855d118f6fc09785a3243ebc4c424362
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-12 12:12:33 -0400

    2024-05-12T16:12:33.577Z  function_auto lesson_for_iterations

--- public/src/lesson_for_iterations.mjs
+++ public/src/lesson_for_iterations.mjs
@@ -1,9 +1,8 @@
+import { html_style_alternate_monospace_short_multiple } from "./html_style_alternate_monospace_short_multiple.mjs";
 import { js_code_statement_assign } from "./js_code_statement_assign.mjs";
 import { js_code_statement_let } from "./js_code_statement_let.mjs";
 import { js_code_statement_increment } from "./js_code_statement_increment.mjs";
 import { js_code_increment } from "./js_code_increment.mjs";
-import { app_learn_code_code_part_contrast } from "./app_learn_code_code_part_contrast.mjs";
-import { html_p_text } from "./html_p_text.mjs";
 import { lesson_while_generic_1 } from "./lesson_while_generic_1.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
@@ -17,7 +16,7 @@ export function lesson_for_iterations() {
     function () {
       let { name, value, loop_end } = lesson_while_generic_1();
       let lines = [
-        js_code_statement_let_assign("counter", '0'),
+        js_code_statement_let_assign("counter", "0"),
         js_code_statement_let(name),
         `for (${js_code_statement_assign(name, value)} ${name} <= ${loop_end}; ${js_code_increment(name)}) {`,
         js_code_statement_increment("counter"),
@@ -32,13 +31,11 @@ export function lesson_for_iterations() {
     example_message,
     [screen1],
   );
-  function example_before(parent) {}
-  function screen1(parent) {
-    html_p_text(parent, "here is some code :");
-    let lines = [`a;`, `while (b) {`, `// c`, `d;`, `}`];
-    app_learn_code_code_part_contrast(parent, list_join_newline(lines));
-    html_p_text(parent, "that code can be rewritten as :");
-    let lines2 = [`for (a; b; d) {`, `// c`, `}`];
-    app_learn_code_code_part_contrast(parent, list_join_newline(lines2));
+  function example_before(parent) {
+    html_style_alternate_monospace_short_multiple(parent, [
+      "in this lesson there will be a variable named `counter`",
+      "`counter` will increment by `1` each time the code inside the braces of the `for` loop runs",
+      "after the `for` loop runs , the value of `counter` will be the total number of times the code ran inside the braces of the `for` loop",
+    ]);
   }
 }
