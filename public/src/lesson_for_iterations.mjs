e66adb9791305ba35dfd45c15076d4b9ad676a70
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-12 12:06:15 -0400

    2024-05-12T16:06:15.169Z  function_auto lesson_for_iterations

--- public/src/lesson_for_iterations.mjs
+++ public/src/lesson_for_iterations.mjs
@@ -1,6 +1,5 @@
+import { js_code_statement_increment } from "./js_code_statement_increment.mjs";
 import { js_code_increment } from "./js_code_increment.mjs";
-import { js_code_statement_assign } from "./js_code_statement_assign.mjs";
-import { js_code_statement_let } from "./js_code_statement_let.mjs";
 import { app_learn_code_code_part_contrast } from "./app_learn_code_code_part_contrast.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { lesson_while_generic_1 } from "./lesson_while_generic_1.mjs";
@@ -8,6 +7,7 @@ import { lesson_simple } from "./lesson_simple.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { app_learn_code_log } from "./app_learn_code_log.mjs";
 import { string_combine } from "./string_combine.mjs";
+import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
 export function lesson_for_iterations() {
   let description = "`for` loop";
   let example_message = string_combine("with a ", description);
@@ -15,11 +15,12 @@ export function lesson_for_iterations() {
     function () {
       let { name, value, loop_end } = lesson_while_generic_1();
       let lines = [
-        js_code_statement_let(name),
-        `for (${js_code_statement_assign(name, value)} ${name} <= ${loop_end}; ${js_code_increment(name)}) {`,
-        app_learn_code_log(`'inside ' + ${name}`),
+        js_code_statement_let_assign("counter"),
+        `for (${js_code_statement_let_assign(name, value)} ${name} <= ${loop_end}; ${js_code_increment(name)}) {`,
+        js_code_statement_increment("counter"),
+        app_learn_code_log(`'inside ' + ${name} + ' ' + counter`),
         `}`,
-        app_learn_code_log(`'after ' + ${name}`),
+        app_learn_code_log(`'after ' + ${name} + ' ' + counter`),
       ];
       return list_join_newline(lines);
     },
