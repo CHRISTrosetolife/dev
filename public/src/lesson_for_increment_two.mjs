2d49a478108c28460333a5ef3b50ca0281da914c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-12 17:17:40 -0400

    2024-05-12T21:17:40.352Z  function_auto lesson_for_increment_two

--- public/src/lesson_for_increment_two.mjs
+++ public/src/lesson_for_increment_two.mjs
@@ -1,21 +1,22 @@
+import { lesson_while_generic } from "./lesson_while_generic.mjs";
+import { integer_random } from "./integer_random.mjs";
 import { html_style_alternate_monospace_short } from "./html_style_alternate_monospace_short.mjs";
 import { subtract_1 } from "./subtract_1.mjs";
-import { js_code_increment } from "./js_code_increment.mjs";
 import { js_code_statement_assign } from "./js_code_statement_assign.mjs";
 import { js_code_statement_let } from "./js_code_statement_let.mjs";
-import { lesson_while_generic_1 } from "./lesson_while_generic_1.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { app_learn_code_log } from "./app_learn_code_log.mjs";
 export function lesson_for_increment_two() {
-  let description = "`for` loop with `<`";
-  let example_message = "with a `for` loop with a `<`";
+  let description = "`for` loop with `+=` `2` or `3`";
+  let example_message = "with `for` loop with `+=`";
   return lesson_simple(
     function () {
-      let { name, value, loop_end } = lesson_while_generic_1();
+      const increment = integer_random(2, 3);
+      let { name, value, loop_end } = lesson_while_generic(increment);
       let lines = [
         js_code_statement_let(name),
-        `for (${js_code_statement_assign(name, subtract_1(value))} ${name} < ${loop_end}; ${js_code_increment(name)}) {`,
+        `for (${js_code_statement_assign(name, subtract_1(value))} ${name} < ${loop_end}; ${js_code_add_assign(name, increment)}) {`,
         app_learn_code_log(`'inside ' + ${name}`),
         `}`,
         app_learn_code_log(`'after ' + ${name}`),
