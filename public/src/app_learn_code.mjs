57148c4ef33f3a4deba79816d11439fa360d175f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-13 07:46:11 -0400

    2024-05-13T11:46:11.247Z  noop

--- public/src/app_learn_code.mjs
+++ public/src/app_learn_code.mjs
@@ -131,7 +131,7 @@ export function app_learn_code() {
     html_style(begin, {
       "font-size": string_combine(
         multiply(default_font_size, 1.2).toString(),
-        "vh",
+        "dvh",
       ),
     });
     html_style_bold(begin);
