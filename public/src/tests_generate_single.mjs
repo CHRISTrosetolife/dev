422585fbb097ad25f4574c4b5f25dec24a95e4bd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-16 08:41:06 -0400

    2024-05-16T12:41:05.143Z  function_auto tests_generate_single

--- public/src/tests_generate_single.mjs
+++ public/src/tests_generate_single.mjs
@@ -1,10 +1,10 @@
+import { equal_json } from "./equal_json.mjs";
 import { json_to } from "./json_to.mjs";
 import { string_is } from "./string_is.mjs";
 import { string_delimit } from "./string_delimit.mjs";
 import { log } from "./log.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
-import { equal } from "./equal.mjs";
 import { list_map } from "./list_map.mjs";
 import { assert_boolean } from "./assert_boolean.mjs";
 import { string_includes } from "./string_includes.mjs";
@@ -22,12 +22,16 @@ export async function tests_generate_single(function_name, args, test_number) {
     if (string_is(arg)) {
       return string_delimit(arg);
     }
-    error()
+    error();
   });
-  console.log({args_mapped})
-    const body_string = `    let ${result_name} = ${function_name}(${args_mapped.join(", ")});
+  console.log({
+    args_mapped,
+  });
+  const body_string = `    let ${result_name} = ${function_name}(${args_mapped.join(", ")});
     ${assert_boolean.name}(${equal_json.name}(${result_name}, ${json_to(result)}))`;
-    console.log({body_string})
+  console.log({
+    body_string,
+  });
   await function_new_generic(
     `${function_name}_test_${test_number}`,
     ``,
