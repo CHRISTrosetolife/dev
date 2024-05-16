c3446ad25b4d2f17c10199d06ec6303d04161630
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-16 09:08:36 -0400

    2024-05-16T13:08:35.132Z  sandbox

--- public/src/logic_parse_test_2.mjs
+++ public/src/logic_parse_test_2.mjs
@@ -2,18 +2,14 @@ import {equal_json} from "./equal_json.mjs";
 import {assert_boolean} from "./assert_boolean.mjs";
 import {logic_parse} from "./logic_parse.mjs";
 export function logic_parse_test_2() {
-    let result = logic_parse('a(b(c()))');
+    let result = logic_parse('a(b())');
     assert_boolean(equal_json(result, {
         "name": "a",
         "type": "function",
         "args": [{
             "name": "b",
             "type": "function",
-            "args": [{
-                "name": "c",
-                "type": "function",
-                "args": []
-            }]
+            "args": []
         }]
     }));
 }
