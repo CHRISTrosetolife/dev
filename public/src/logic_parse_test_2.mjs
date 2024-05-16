e0b9d53746130820b4e902ad0b17c143d10cabf1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-16 08:44:20 -0400

    2024-05-16T12:44:19.623Z  sandbox

---
+++ public/src/logic_parse_test_2.mjs
@@ -0,0 +1,11 @@
+import {equal_json} from "./equal_json.mjs";
+import {assert_boolean} from "./assert_boolean.mjs";
+import {logic_parse} from "./logic_parse.mjs";
+export function logic_parse_test_2() {
+    let result = logic_parse('a(b())');
+    assert_boolean(equal_json(result, {
+        "name": "ab",
+        "type": "function",
+        "args": []
+    }));
+}
