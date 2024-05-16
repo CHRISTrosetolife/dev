bdae2d28e4df222c9be8a37e367390955a9dc518
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-16 09:41:53 -0400

    2024-05-16T13:41:53.527Z  noop

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,16 +1,17 @@
 import { tests_generate_single } from "./tests_generate_single.mjs";
 import { logic_parse } from "./logic_parse.mjs";
+import { add_1 } from "./add_1.mjs";
 export async function sandbox() {
   let inputs = [
     "a()",
     "a(b())",
     "a(b(c()))",
-    "a(b(c(),d()))",
-    "a(b(c(),d(e())))",
-    "a(b(c(),d(e(),f())))",
-    "a(b(c(),d(e(),f(),g())))",
+    "a(b(),c()))",
+    "a(b(),c(d())))",
+    "a(b(),c(d(),e())))",
+    "a(b(),c(d(),e(),f())))",
   ];
   await each_index_async(inputs, (input, index) => {
-    tests_generate_single(logic_parse.name, ["a(b(),c(d(),e(),f()))"], 7);
+    tests_generate_single(logic_parse.name, ["a(b(),c(d(),e(),f()))"], add_1(index));
   });
 }
