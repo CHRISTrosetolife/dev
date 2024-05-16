9f8d6fed078738ce9c9a152fae8951ef56482507
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-15 22:07:57 -0400

    2024-05-16T02:07:56.044Z  function_auto logic_parse

--- public/src/logic_parse.mjs
+++ public/src/logic_parse.mjs
@@ -1,8 +1,10 @@
-import { range } from "./range.mjs";
-import { string_length } from "./string_length.mjs";
+import { string_combine } from "./string_combine.mjs";
+import { string_letters_is } from "./string_letters_is.mjs";
 export function logic_parse(input) {
-  let identifier = ''
+  let identifier = "";
   for (let c of input) {
-    console.log(c)
+    if (string_letters_is(c)) {
+      identifier = string_combine(identifier, c);
+    }
   }
 }
