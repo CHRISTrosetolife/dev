4993e37d8ec1b122e2bd2bca944828be9a210f72
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-16 08:46:22 -0400

    2024-05-16T12:46:21.796Z  noop

--- public/src/logic_parse.mjs
+++ public/src/logic_parse.mjs
@@ -1,19 +1,22 @@
 import { string_combine } from "./string_combine.mjs";
 import { string_letters_is } from "./string_letters_is.mjs";
 export function logic_parse(input) {
-  let result;
-  let identifier = "";
-  for (let c of input) {
-    if (string_letters_is(c)) {
-      identifier = string_combine(identifier, c);
-    } else if (c === "(") {
-      result = {
-        name: identifier,
-        type: "function",
-        args: [],
-      };
-    } else if (c === ")") {
-      return result;
+  return logic_parse_recursive(input)
+  function logic_parse_recursive(input) {
+    let result;
+    let identifier = "";
+    for (let c of input) {
+      if (string_letters_is(c)) {
+        identifier = string_combine(identifier, c);
+      } else if (c === "(") {
+        result = {
+          name: identifier,
+          type: "function",
+          args: [],
+        };
+      } else if (c === ")") {
+        return result;
+      }
     }
   }
 }
