f8699b8eab7d0c574a4e98e85879a0a89138c607
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-16 12:15:51 -0400

    2024-05-16T16:15:51.505Z  sandbox

---
+++ public/src/logic_parse_test_8.mjs
@@ -0,0 +1,17 @@
+import {equal_json} from "./equal_json.mjs";
+import {assert_boolean} from "./assert_boolean.mjs";
+import {logic_parse} from "./logic_parse.mjs";
+export function logic_parse_test_8() {
+    let result = logic_parse('a(b)');
+    assert_boolean(equal_json(result, {
+        "type": "call",
+        "callee": {
+            "type": "identifier",
+            "name": "a"
+        },
+        "args": [{
+            "type": "identifier",
+            "name": "b"
+        }]
+    }));
+}
