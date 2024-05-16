3e068e3b9004b0217f3eb8fc6b5923e9e6574ca1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-16 09:40:38 -0400

    2024-05-16T13:40:37.578Z  noop

--- public/src/each_index_async.mjs
+++ public/src/each_index_async.mjs
@@ -1,7 +1,7 @@
-export function each_index_async(list, lambda) {
+export async function each_index_async(list, lambda) {
   let index = 0;
   for (let element of list) {
-    lambda(element, index);
+    await lambda(element, index);
     index++;
   }
 }
