c3d2a4801f20a8db1d3f5dfd9560bde374be76a3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-16 10:00:33 -0400

    2024-05-16T14:00:31.792Z  function_auto logic_parse

--- public/src/logic_parse.mjs
+++ public/src/logic_parse.mjs
@@ -1,3 +1,5 @@
+import { equal_0 } from "./equal_0.mjs";
+import { log } from "./log.mjs";
 import { add_1 } from "./add_1.mjs";
 import { assert } from "./assert.mjs";
 import { equal } from "./equal.mjs";
@@ -10,24 +12,32 @@ import { string_length } from "./string_length.mjs";
 import { string_letters_is } from "./string_letters_is.mjs";
 export function logic_parse(input) {
   let index = 0;
-  let r = logic_parse_recursive(input, index)
-  let {result,next} = r;
-  console.log({input,r})
-  assert(equal, [next, string_length(input)])
+  let r = logic_parse_recursive(input, index);
+  let { result, next } = r;
+  console.log({
+    input,
+    r,
+  });
+  assert(equal, [next, string_length(input)]);
   return result;
   function logic_parse_recursive(input, index) {
-    console.log({input,index})
+    console.log({
+      input,
+      index,
+    });
     let open = false;
     let result;
     let identifier = "";
     while (number_less_than(index, string_length(input))) {
       let c = string_get(input, index);
-      console.log({c})
+      console.log({
+        c,
+      });
       if (string_letters_is(c)) {
         identifier = string_combine(identifier, c);
-        index = add_1(index)
-      } else if (c === "(" || c === ',') {
-        if (c === '(') {
+        index = add_1(index);
+      } else if (c === "(" || c === ",") {
+        if (c === "(") {
           open = true;
           result = {
             type: "function",
@@ -35,23 +45,34 @@ export function logic_parse(input) {
             args: [],
           };
         }
-        let {args} = result
-        let r = logic_parse_recursive(input, add_1(index))
-        console.log({r})
-        let {result:child} =r;
-        let {type:child_type} = child;
-        if (child_type !== 'empty') {
-          list_add(args,child)
+        let { args } = result;
+        let r = logic_parse_recursive(input, add_1(index));
+        console.log({
+          r,
+        });
+        let { result: child } = r;
+        let { type: child_type } = child;
+        if (child_type !== "empty") {
+          list_add(args, child);
         }
-        let {next} = r;
-        index = next
+        let { next } = r;
+        index = next;
       } else if (c === ")") {
         if (open) {
-          return {result,next:add_1(index)};
+          return {
+            result,
+            next: add_1(index),
+          };
         }
-        return {result:{type:'empty'}, next:index}
+        assert(equal_0, [string_length(identifier)]);
+        return {
+          result: {
+            type: "empty",
+          },
+          next: index,
+        };
       }
     }
-    error()
+    error();
   }
 }
