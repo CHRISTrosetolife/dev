4d39ac21cc089b034f89457ceb52e228133093c8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-15 21:55:56 -0400

    2024-05-16T01:55:54.952Z  function_auto string_letter_is

--- public/src/string_letter_is.mjs
+++ public/src/string_letter_is.mjs
@@ -1,3 +1,4 @@
+import { string_letters_is } from "./string_letters_is.mjs";
 export function string_letter_is(str) {
-    return str.length === 1 && str.match(/[a-z]/i);
+  return str.length === 1 && string_letters_is(str);
 }
---
+++ public/src/string_letters_is.mjs
@@ -0,0 +1,3 @@
+export function string_letters_is(str) {
+  return str.match(/[a-z]/i);
+}
