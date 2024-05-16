d5b8c8a04dfadbef194d6315f01f1cdbef4a1427
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-15 22:06:26 -0400

    2024-05-16T02:06:26.058Z  logic_parse a()

--- public/src/logic_parse.mjs
+++ public/src/logic_parse.mjs
@@ -1,8 +1,8 @@
-import { log } from "./log.mjs";
+import { range } from "./range.mjs";
 import { string_length } from "./string_length.mjs";
 export function logic_parse(input) {
-  let length = string_length(input);
+  let identifier = ''
   for (let c of input) {
-    console.log(c);
+    console.log(c)
   }
 }
