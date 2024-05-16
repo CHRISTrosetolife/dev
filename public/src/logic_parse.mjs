57f5aaf35682d7c646093cac2845712d9bfc6535
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-16 08:56:20 -0400

    2024-05-16T12:56:20.195Z  noop

--- public/src/logic_parse.mjs
+++ public/src/logic_parse.mjs
@@ -1,4 +1,5 @@
 import { add_1 } from "./add_1.mjs";
+import { error } from "./error.mjs";
 import { list_add } from "./list_add.mjs";
 import { number_less_than } from "./number_less_than.mjs";
 import { string_combine } from "./string_combine.mjs";
@@ -12,7 +13,7 @@ export function logic_parse(input) {
     let result;
     let identifier = "";
     while (number_less_than(index, string_length(input))) {
-      let c = string_get(index);
+      let c = string_get(input, index);
       if (string_letters_is(c)) {
         identifier = string_combine(identifier, c);
       } else if (c === "(") {
@@ -30,6 +31,8 @@ export function logic_parse(input) {
       } else if (c === ")") {
         return {result,next:add_1(index)};
       }
+      index = add_1(index)
     }
+    error()
   }
 }
