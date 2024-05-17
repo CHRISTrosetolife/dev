faeaf0faf943c5a320248a1ff3a3c389abd50973
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-17 15:10:15 -0400

    2024-05-17T19:10:15.152Z  function_auto lesson_new

--- public/src/lesson_new.mjs
+++ public/src/lesson_new.mjs
@@ -10,7 +10,6 @@ import { string_combine } from "./string_combine.mjs";
 import { list_add } from "./list_add.mjs";
 import { js_imports_add } from "./js_imports_add.mjs";
 import { js_unparse } from "./js_unparse.mjs";
-import { file_open } from "./file_open.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function lesson_new(name, previous) {
   const prefix = "lesson_";
@@ -39,5 +38,4 @@ export async function lesson_new(name, previous) {
     file_path,
     [],
   );
-  if (0) await file_open(lesson_name);
 }
