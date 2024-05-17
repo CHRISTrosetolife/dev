b3e17224d3d92d6ca6eb200d42d71d74fc00d610
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-17 14:50:55 -0400

    2024-05-17T18:50:54.768Z  noop

--- public/src/file_transform.mjs
+++ public/src/file_transform.mjs
@@ -1,8 +1,8 @@
 import { file_overwrite } from "./file_overwrite.mjs";
 import { file_read } from "./file_read.mjs";
 export async function file_transform(lambda, file_path, args) {
-  let obj = await file_read(file_path);
-  let result = await lambda(obj, ...args);
-  await file_overwrite(file_path, obj);
+  let text = await file_read(file_path);
+  let result = await lambda(text, ...args);
+  await file_overwrite(file_path, text);
   return result;
 }
--- public/src/lesson_new.mjs
+++ public/src/lesson_new.mjs
@@ -1,5 +1,5 @@
 import { string_replace } from "./string_replace.mjs";
 import { file_transform } from "./file_transform.mjs";
 export function lesson_new(name) {
-  file_transform((before) => string_replace(), file_path);
+  file_transform((before) => string_replace(), file_path, []);
 }
