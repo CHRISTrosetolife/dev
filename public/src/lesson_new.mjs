e64276f5fd650dd4d09a279b6493f35b49e727d0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-17 14:58:35 -0400

    2024-05-17T18:58:34.494Z  function_auto lesson_new

--- public/src/lesson_new.mjs
+++ public/src/lesson_new.mjs
@@ -1,3 +1,4 @@
+import { function_auto_transforms } from "./function_auto_transforms.mjs";
 import { js_code_statement_call_args } from "./js_code_statement_call_args.mjs";
 import { js_parse } from "./js_parse.mjs";
 import { js_code_statement_call } from "./js_code_statement_call.mjs";
@@ -7,7 +8,7 @@ import { marker } from "./marker.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { list_add } from "./list_add.mjs";
 export function lesson_new(name) {
-  let lesson_name = string_combine('lesson_', name)
+  let lesson_name = string_combine("lesson_", name);
   file_transform(
     (before) => {
       let name = marker.name;
@@ -15,9 +16,17 @@ export function lesson_new(name) {
       let after = string_replace(
         before,
         search,
-        string_combine(js_code_statement_call_args(list_add.name, ['module_current', lesson_name]), search),
+        string_combine(
+          js_code_statement_call_args(list_add.name, [
+            "module_current",
+            lesson_name,
+          ]),
+          search,
+        ),
       );
       let parsed = js_parse(after);
+      for (let t of function_auto_transforms()) {
+      }
     },
     file_path,
     [],
