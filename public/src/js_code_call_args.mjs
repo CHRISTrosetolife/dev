bb4e04c7114b1910c7933bc0cba37e45cf4ada2a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-17 15:14:30 -0400

    2024-05-17T19:14:30.102Z  lesson_new_modules_add object_properties_two

--- public/src/js_code_call_args.mjs
+++ public/src/js_code_call_args.mjs
@@ -1,6 +1,6 @@
 import { js_code_statement } from "./js_code_statement.mjs";
 import { list_join } from "./list_join.mjs";
 export function js_code_call_args(function_name, args) {
-  let code = js_code_statement(`${function_name}(${list_join(args, ", ")})`);
+  let code = `${function_name}(${list_join(args, ", ")})`;
   return code;
 }
