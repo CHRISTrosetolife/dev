36695dba5080eb98459ce167959d83240385ec4b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 17:06:49 -0400

    2024-05-18T21:06:48.771Z  noop

--- public/src/app_yyy8Uu.mjs
+++ public/src/app_yyy8Uu.mjs
@@ -7,6 +7,8 @@ import { html_button_width_full_text_click_next } from "./html_button_width_full
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
+import { html_inner_set } from "./html_inner_set.mjs";
+import { json_to } from "./json_to.mjs";
 export async function app_yyy8Uu() {
   html_style_default_initialize();
   let root = html_document_body();
@@ -14,7 +16,11 @@ export async function app_yyy8Uu() {
   async function refresh_chapter(index) {
     html_clear_scroll_top(root);
     let file_path = yyy8Uu_storage_path(index);
-    let verses = await http_get(storage_url(file_path));
+    let chapter = await http_get(storage_url(file_path));
+    let {
+        english,
+        latin,} = chapter;
+    html_inner_set(root, json_to(chapter))
     html_hr(root);
     let button_next = html_button_width_full_text_click_next(
       root,
