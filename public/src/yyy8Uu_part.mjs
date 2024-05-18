c866fabd0e111079d452866d9df08f4204c6d732
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 15:42:26 -0400

    2024-05-18T19:42:26.552Z  noop

--- public/src/yyy8Uu_part.mjs
+++ public/src/yyy8Uu_part.mjs
@@ -1,5 +1,7 @@
 import { yyy8Uu_parts } from "./yyy8Uu_parts.mjs";
 import { list_get } from "./list_get.mjs";
 export async function yyy8Uu_part(index, index2) {
-  return[ list_get(await yyy8Uu_parts(index), index2)];
+    const parts = await yyy8Uu_parts(index);
+    console.log({parts, index, index2})
+  return[ list_get(parts, index2)];
 }
