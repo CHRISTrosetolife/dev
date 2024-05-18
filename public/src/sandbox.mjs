2203b502ee32d3254bbff5c94cedf44b6fe54b41
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 14:46:18 -0400

    2024-05-18T18:46:17.924Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -3,17 +3,16 @@ import { yyy8Uu_file_paths } from "./yyy8Uu_file_paths.mjs";
 import { yyy8Uu_parts } from "./yyy8Uu_parts.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { yyy8Uu_file_path_to_parts } from "./yyy8Uu_file_path_to_parts.mjs";
-import { log } from "./log.mjs";
-import { file_overwrite } from "./file_overwrite.mjs";
-import { list_join } from "./list_join.mjs";
 export async function sandbox() {
-  let index = 0;
-  let parts = await gcloud_translate_cache("la", "en", yyy8Uu_parts.name, [index]);
-
-  await file_overwrite('log.txt', list_join(parts, '\n\n'))
+  let list = await yyy8Uu_parts(index);
+  await each_index_async(list, async (file_path, index) => {
+    let parts = await gcloud_translate_cache("la", "en", yyy8Uu_parts.name, [
+      index,
+    ]);
+  });
   return;
   let result = await yyy8Uu_parts(index);
-  return result;
+  return list;
   return;
   let filtered = await yyy8Uu_file_paths();
   await each_index_async(filtered, async (file_path, index) => {
