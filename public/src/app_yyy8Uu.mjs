2d259bb5c89d6afab32a81664148950961dbdf37
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 16:46:49 -0400

    2024-05-18T20:46:49.144Z  function_auto app_yyy8Uu

--- public/src/app_yyy8Uu.mjs
+++ public/src/app_yyy8Uu.mjs
@@ -1,6 +1,10 @@
+import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
+import { html_document_body } from "./html_document_body.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_yyy8Uu() {
   html_style_default_initialize();
+  let root = html_document_body();
   async function refresh_chapter(index) {
+    html_clear_scroll_top(root);
   }
 }
