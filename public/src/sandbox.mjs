b190ef6bd5eb7782896149fde0a6c04ccb102c31
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 09:13:32 -0400

    2024-05-18T13:13:32.344Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -2,14 +2,16 @@ import { log } from "./log.mjs";
 import { TranslationServiceClient } from "@google-cloud/translate";
 export async function sandbox() {
   let text = "hello";
+  let sourceLanguageCode = "en";
+  let targetLanguageCode = "es";
   const translationClient = new TranslationServiceClient();
   async function translateText() {
     const request = {
       parent: `projects/letjesusbeexalted/locations/global`,
       contents: [text],
       mimeType: "text/plain",
-      sourceLanguageCode: "en",
-      targetLanguageCode: "es",
+      sourceLanguageCode: sourceLanguageCode,
+      targetLanguageCode: targetLanguageCode,
     };
     const [response] = await translationClient.translateText(request);
     for (const translation of response.translations) {
