3e666e84b7ff4c9b2d74b6a2a161f6a227dc33dc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 13:23:39 -0400

    2024-05-18T17:23:39.455Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -4,7 +4,7 @@ import { yyy8Uu_file_path_to_parts } from "./yyy8Uu_file_path_to_parts.mjs";
 export async function sandbox() {
   let index = 0;
   let filtered = await yyy8Uu_file_paths();
- return await yyy8Uu_file_path_to_parts(filtered, index);
+  return await yyy8Uu_file_path_to_parts(filtered, index);
   return;
   await each_index_async(filtered, async (file_path, index) => {
     await yyy8Uu_file_path_to_parts(filtered, index);
