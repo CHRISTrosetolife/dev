c202b64942c01bda2a1dbdc41e6efd6727ae5cc6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 16:47:33 -0400

    2024-05-18T20:47:33.306Z  function_auto app_yyy8Uu

--- public/src/app_yyy8Uu.mjs
+++ public/src/app_yyy8Uu.mjs
@@ -1,3 +1,5 @@
+import { add_1 } from "./add_1.mjs";
+import { html_button_width_full_text_click_next } from "./html_button_width_full_text_click_next.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
@@ -6,5 +8,11 @@ export function app_yyy8Uu() {
   let root = html_document_body();
   async function refresh_chapter(index) {
     html_clear_scroll_top(root);
+    let button_next = html_button_width_full_text_click_next(
+      root,
+      async function next_on_click() {
+        await refresh_lesson_screen(add_1(index));
+      },
+    );
   }
 }
