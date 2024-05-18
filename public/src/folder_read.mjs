89da7f78bf677fc5ffb4fa8ce0852868d648d5dc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 11:36:50 -0400

    2024-05-18T15:36:50.334Z  sandbox

--- public/src/folder_read.mjs
+++ public/src/folder_read.mjs
@@ -1,6 +1,8 @@
+import { string_replace } from "./string_replace.mjs";
 import { import_node } from "./import_node.mjs";
 let g = await import_node("glob");
 export async function folder_read(folder, file_extension) {
+  folder = string_replace(folder, "\\", "/");
   let { glob } = g;
   let files = await glob(`${folder}**/*${file_extension}`);
   return files;
