98a9621156e83963522d456a73a4b2a4e7b7586c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 11:46:36 -0400

    2024-05-18T15:46:35.864Z  function_auto xml_parse

--- public/src/xml_parse.mjs
+++ public/src/xml_parse.mjs
@@ -1,1 +1,5 @@
-export function xml_parse(input_string) {}
+import { XMLParser } from "fast-xml-parser";
+export function xml_parse(input_string) {
+  const parser = new XMLParser();
+  return parser.parse(input_string);
+}
