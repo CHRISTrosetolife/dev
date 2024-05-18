55c96b552582fc5cd91226d5827d2e600a3eb315
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 11:36:55 -0400

    2024-05-18T15:36:54.967Z  sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,6 @@
 import { string_replace } from "./string_replace.mjs";
 import { folder_read } from "./folder_read.mjs";
 export async function sandbox() {
-  let path = "C:\\Users\\JESUS\\Downloads\\yyy8Uu-master\\yyy8Uu-master\\";
+  let path = "C:\\Users\\JESUS\\Downloads\\yyy8Uu-master\\yyy8Uu-master";
   return await folder_read(path, ".xml");
 }
