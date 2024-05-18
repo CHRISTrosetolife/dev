9557c37a3830bd0fcce2b334322da58aa1f9b125
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 11:33:58 -0400

    2024-05-18T15:33:58.125Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,5 @@
+import { folder_read } from "./folder_read.mjs";
 export async function sandbox() {
   let path = "C:\\Users\\JESUS\\Downloads\\yyy8Uu-master\\yyy8Uu-master";
+  return await folder_read(path);
 }
