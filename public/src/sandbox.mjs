3443fe29228a5ff02e038d79ae58e314448c51cd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 13:15:00 -0400

    2024-05-18T17:14:59.988Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,10 +1,11 @@
+import { yyy8Uu_path_base } from "./yyy8Uu_path_base.mjs";
 import { yyy8Uu_file_path_to_parts } from "./yyy8Uu_file_path_to_parts.mjs";
 import { string_includes } from "./string_includes.mjs";
 import { folder_read } from "./folder_read.mjs";
 import { list_filter } from "./list_filter.mjs";
 export async function sandbox() {
   let index = 0;
-  let path = "C:\\Users\\JESUS\\Downloads\\yyy8Uu-master\\yyy8Uu-master";
+  let path = yyy8Uu_path_base();
   let files = await folder_read(path, ".xml");
   let filtered = list_filter(files, (f) => string_includes(f, "\\cod-"));
   return await yyy8Uu_file_path_to_parts(filtered, index);
---
+++ public/src/yyy8Uu_path_base.mjs
@@ -0,0 +1,3 @@
+export function yyy8Uu_path_base() {
+  return "C:\\Users\\JESUS\\Downloads\\yyy8Uu-master\\yyy8Uu-master";
+}
