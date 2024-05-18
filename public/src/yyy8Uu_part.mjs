51c77deb90a1fbdf9b4ff9499f12074db932ea8d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 15:20:03 -0400

    2024-05-18T19:20:03.128Z  noop

--- public/src/yyy8Uu_part.mjs
+++ public/src/yyy8Uu_part.mjs
@@ -1,5 +1,5 @@
 import { yyy8Uu_parts } from "./yyy8Uu_parts.mjs";
 import { list_get } from "./list_get.mjs";
 export async function yyy8Uu_part(index, index2) {
-  return list_get(await yyy8Uu_parts(index), index2);
+  return[ list_get(await yyy8Uu_parts(index), index2)];
 }
