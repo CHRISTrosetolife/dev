9f0315ea84482fdf5d568c0a1f079cd23668f51c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 08:19:55 -0400

    2024-05-18T12:19:54.872Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,20 +1,4 @@
-import { each_index_async } from "./each_index_async.mjs";
-import { tests_generate_single } from "./tests_generate_single.mjs";
-import { logic_parse } from "./logic_parse.mjs";
-import { add_1 } from "./add_1.mjs";
+import { logic_parse_tests_generate } from "./logic_parse_tests_generate.mjs";
 export async function sandbox() {
-  let inputs = [
-    "a()",
-    "a(b())",
-    "a(b(c()))",
-    "a(b(),c())",
-    "a(b(),c(d()))",
-    "a(b(),c(d(),e()))",
-    "a(b(),c(d(),e(),f()))",
-    "a(b)",
-    "a(b,c)",
-  ];
-  await each_index_async(inputs, async (input, index) => {
-    await tests_generate_single(logic_parse.name, [input], add_1(index));
-  });
+  await logic_parse_tests_generate();
 }
