9f6508180a5552e540ab86341db81c592230910e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 11:33:01 -0400

    2024-05-18T15:33:01.133Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,22 +1,3 @@
-import { list_map } from "./list_map.mjs";
-import { TranslationServiceClient } from "@google-cloud/translate";
 export async function sandbox() {
-  let text = "hello";
-  let sourceLanguageCode = "en";
-  let targetLanguageCode = "es";
-  const translationClient = new TranslationServiceClient();
-  async function translateText() {
-    const request = {
-      parent: `projects/letjesusbeexalted/locations/global`,
-      contents: [text],
-      mimeType: "text/plain",
-      sourceLanguageCode: sourceLanguageCode,
-      targetLanguageCode: targetLanguageCode,
-    };
-    const [response] = await translationClient.translateText(request);
-    let { translations } = response;
-    let result = list_map(translations, (t) => t.translatedText);
-    return result;
-  }
-  return await translateText();
+  let path = "C:\\Users\\JESUS\\Downloads\\yyy8Uu-master\\yyy8Uu-master";
 }
