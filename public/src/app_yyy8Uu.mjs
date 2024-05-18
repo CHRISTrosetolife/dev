fdf19a5f061545bab6901beb0ee905ee23419354
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 17:12:42 -0400

    2024-05-18T21:12:42.381Z  function_auto app_yyy8Uu

--- public/src/app_yyy8Uu.mjs
+++ public/src/app_yyy8Uu.mjs
@@ -1,3 +1,4 @@
+import { html_style_font_color } from "./html_style_font_color.mjs";
 import { storage_url } from "./storage_url.mjs";
 import { http_get } from "./http_get.mjs";
 import { yyy8Uu_storage_path } from "./yyy8Uu_storage_path.mjs";
@@ -7,8 +8,6 @@ import { html_button_width_full_text_click_next } from "./html_button_width_full
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-import { html_inner_set } from "./html_inner_set.mjs";
-import { json_to } from "./json_to.mjs";
 import { range } from "./range.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { list_get } from "./list_get.mjs";
@@ -24,11 +23,11 @@ export async function app_yyy8Uu() {
     let { english, latin } = chapter;
     let indices = range(list_length(english));
     for (let i of indices) {
-        const l = list_get(latin, i);
-        let latin_p = html_p_text(root, l)
-        html_style_font_color
-        html_p_text(root, list_get(english, i))
-        html_hr(root);
+      const l = list_get(latin, i);
+      let latin_p = html_p_text(root, l);
+      html_style_font_color;
+      html_p_text(root, list_get(english, i));
+      html_hr(root);
     }
     html_hr(root);
     let button_next = html_button_width_full_text_click_next(
