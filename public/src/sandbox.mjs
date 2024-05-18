fa605e57149ce58a6e89e3b43cee1f580054dd9c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 13:55:30 -0400

    2024-05-18T17:55:30.674Z  noop

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -4,13 +4,13 @@ import { yyy8Uu_parts } from "./yyy8Uu_parts.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { yyy8Uu_file_path_to_parts } from "./yyy8Uu_file_path_to_parts.mjs";
 import { log } from "./log.mjs";
+import { file_overwrite } from "./file_overwrite.mjs";
+import { list_join } from "./list_join.mjs";
 export async function sandbox() {
   let index = 0;
   let parts = await gcloud_translate_cache("la", "en", yyy8Uu_parts.name, [index]);
-  for (let part of parts) {
-    log(part);
-    log('')
-  }
+
+  await file_overwrite('log.txt', list_join(parts, '\n\n'))
   return;
   let result = await yyy8Uu_parts(index);
   return result;
