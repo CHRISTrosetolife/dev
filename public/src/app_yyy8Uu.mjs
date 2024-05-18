e192c1e6f7591e135f38071d41b1f3313c92e642
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 16:47:51 -0400

    2024-05-18T20:47:51.553Z  function_auto app_yyy8Uu

--- public/src/app_yyy8Uu.mjs
+++ public/src/app_yyy8Uu.mjs
@@ -3,9 +3,10 @@ import { html_button_width_full_text_click_next } from "./html_button_width_full
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-export function app_yyy8Uu() {
+export async function app_yyy8Uu() {
   html_style_default_initialize();
   let root = html_document_body();
+  await refresh_chapter(0);
   async function refresh_chapter(index) {
     html_clear_scroll_top(root);
     let button_next = html_button_width_full_text_click_next(
