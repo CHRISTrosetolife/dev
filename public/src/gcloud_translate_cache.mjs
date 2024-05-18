b5cd03eef581eeff95c72c95f0df902565671ca9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 13:36:20 -0400

    2024-05-18T17:36:20.097Z  function_auto gcloud_translate_cache

--- public/src/gcloud_translate_cache.mjs
+++ public/src/gcloud_translate_cache.mjs
@@ -1,6 +1,7 @@
-import { http } from "./http.mjs";
+import { sandbox_texts } from "./sandbox_texts.mjs";
 import { function_cache } from "./function_cache.mjs";
 import { gcloud_translate } from "./gcloud_translate.mjs";
 export async function gcloud_translate_cache(url) {
+  return await gcloud_translate("en", "es", sandbox_texts.name, ["john"]);
   return await function_cache(gcloud_translate, [url]);
 }
