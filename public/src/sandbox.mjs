5320783bab04a7fe3a1beaeb8b0de348ae099c5e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 12:45:11 -0400

    2024-05-18T16:45:11.336Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -12,25 +12,26 @@ import { list_single } from "./list_single.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { string_trim } from "./string_trim.mjs";
 import { string_empty_not_is } from "./string_empty_not_is.mjs";
+import { list_get } from "./list_get.mjs";
 export async function sandbox() {
+  let index = 0;
   let path = "C:\\Users\\JESUS\\Downloads\\yyy8Uu-master\\yyy8Uu-master";
   let files = await folder_read(path, ".xml");
   let filtered = list_filter(files, (f) => string_includes(f, "\\cod-"));
-  for (let f of filtered) {
-    let input_string = await file_read(f);
-    let parsed = html_parse(input_string);
-    let teis = parsed.getElementsByTagName("TEI");
-    assert(equal, [list_length(teis), 1]);
-    let bodies = list_single(teis).getElementsByTagName("body");
-    assert(equal, [list_length(bodies), 1]);
-    let divs = list_single(bodies).getElementsByTagName("div");
-    assert(equal, [list_length(divs), 1]);
-    let div = list_single(divs);
-    let { childNodes } = div;
-    let parts = list_map(childNodes, (c) => object_property_get(c, "rawText"));
-    let normalized = list_map(parts, string_whitespace_normalize);
-    let trimmed = list_map(normalized, string_trim);
-    let non_empty = list_filter(trimmed, string_empty_not_is);
-    return non_empty;
-  }
+  let f = list_get(filtered, index);
+  let input_string = await file_read(f);
+  let parsed = html_parse(input_string);
+  let teis = parsed.getElementsByTagName("TEI");
+  assert(equal, [list_length(teis), 1]);
+  let bodies = list_single(teis).getElementsByTagName("body");
+  assert(equal, [list_length(bodies), 1]);
+  let divs = list_single(bodies).getElementsByTagName("div");
+  assert(equal, [list_length(divs), 1]);
+  let div = list_single(divs);
+  let { childNodes } = div;
+  let parts = list_map(childNodes, (c) => object_property_get(c, "rawText"));
+  let normalized = list_map(parts, string_whitespace_normalize);
+  let trimmed = list_map(normalized, string_trim);
+  let non_empty = list_filter(trimmed, string_empty_not_is);
+  return non_empty;
 }
