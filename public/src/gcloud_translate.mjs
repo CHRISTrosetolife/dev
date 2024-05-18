0af71cb04c758ec5e5baaeeb3ea244769cd91095
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 13:27:21 -0400

    2024-05-18T17:27:21.396Z  function_auto gcloud_translate

--- public/src/gcloud_translate.mjs
+++ public/src/gcloud_translate.mjs
@@ -1,23 +1,27 @@
+import { function_run } from "./function_run.mjs";
 import { list_map } from "./list_map.mjs";
 import { TranslationServiceClient } from "@google-cloud/translate";
 export async function gcloud_translate(
   sourceLanguageCode,
   targetLanguageCode,
   mimeType,
-  texts,
+  function_name,
+  args,
 ) {
-  "mimeTypes:";
-  "text/plain";
-  "text/html";
+  let contents = await function_run(function_name, args);
+  ("mimeTypes:");
+  ("text/plain");
+  ("text/html");
   const translationClient = new TranslationServiceClient();
   const request = {
     parent: `projects/letjesusbeexalted/locations/global`,
-    contents: text,
+    contents,
     mimeType,
     sourceLanguageCode: sourceLanguageCode,
     targetLanguageCode: targetLanguageCode,
   };
   const responses = await translationClient.translateText(request);
+  list_map(responses);
   let { translations } = response;
   let result = list_map(translations, (t) => t.translatedText);
   return result;
--- public/src/gcloud_translate_cache.mjs
+++ public/src/gcloud_translate_cache.mjs
@@ -1,5 +1,6 @@
 import { http } from "./http.mjs";
 import { function_cache } from "./function_cache.mjs";
+import { gcloud_translate } from "./gcloud_translate.mjs";
 export async function gcloud_translate_cache(url) {
-  return await function_cache(http, [url]);
+  return await function_cache(gcloud_translate, [url]);
 }
