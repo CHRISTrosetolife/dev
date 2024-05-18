b057468514d040d924a4ccc5ed5d54ca672c44e2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-17 20:45:38 -0400

    2024-05-18T00:45:29.894Z  function_auto lesson_object_properties_two

--- public/src/lesson_object_properties_two.mjs
+++ public/src/lesson_object_properties_two.mjs
@@ -1,3 +1,4 @@
+import { js_code_statement_assign } from "./js_code_statement_assign.mjs";
 import { js_code_dot } from "./js_code_dot.mjs";
 import { integer_random_digit_single_positive_multiple_sorted } from "./integer_random_digit_single_positive_multiple_sorted.mjs";
 import { app_learn_code_random_identifiers_sorted } from "./app_learn_code_random_identifiers_sorted.mjs";
@@ -7,7 +8,6 @@ import { lesson_simple } from "./lesson_simple.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { app_learn_code_log } from "./app_learn_code_log.mjs";
 import { string_combine } from "./string_combine.mjs";
-import { js_code_assign } from "./js_code_assign.mjs";
 export function lesson_object_properties_two() {
   let description = "object with two properties";
   let example_message = string_combine("with an ", description);
@@ -17,8 +17,8 @@ export function lesson_object_properties_two() {
       let [d, e] = integer_random_digit_single_positive_multiple_sorted(2);
       let lines = [
         js_code_statement_let_assign(a, js_code_braces()),
-        js_code_assign(js_code_dot(a, b), d),
-        js_code_assign(js_code_dot(a, c), e),
+        js_code_statement_assign(js_code_dot(a, b), d),
+        js_code_statement_assign(js_code_dot(a, c), e),
         app_learn_code_log(`${js_code_dot(a, b)} + ' ' + ${js_code_dot(a, c)}`),
       ];
       return list_join_newline(lines);
