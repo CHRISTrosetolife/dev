5fe2123519ee71dff357893869d5a045dcb3b166
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-17 20:32:04 -0400

    2024-05-18T00:32:04.269Z  noop

--- public/src/lesson_object_properties_two.mjs
+++ public/src/lesson_object_properties_two.mjs
@@ -3,12 +3,14 @@ import { lesson_simple } from "./lesson_simple.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { app_learn_code_log } from "./app_learn_code_log.mjs";
 import { string_combine } from "./string_combine.mjs";
+import { list_sort_string } from "./list_sort_string.mjs";
+import { identity } from "./identity.mjs";
 export function lesson_object_properties_two() {
   let description = "object with two properties";
   let example_message = string_combine("with an ", description);
   return lesson_simple(
     function () {
-      let identifiers = app_learn_code_random_identifiers(3);
+      let identifiers = app_learn_code_random_identifiers(3);list_sort_string(identifiers, identity)
       let lines = [app_learn_code_log(`counter`)];
       return list_join_newline(lines);
     },
