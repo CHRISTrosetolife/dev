77e5aecf5d7db68ea9ba299fdfa89c32097fb112
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 16:40:44 -0400

    2024-05-18T20:40:43.676Z  noop

--- public/src/html_style_default_font_size.mjs
+++ public/src/html_style_default_font_size.mjs
@@ -3,6 +3,6 @@ import { html_style_default } from "./html_style_default.mjs";
 import { object_merge } from "./object_merge.mjs";
 export function html_style_default_font_size(default_font_size) {
   object_merge(html_style_default(), {
-    "font-size": string_combine(default_font_size.toString(), "dvh"),
+    "font-size": string_combine(default_font_size.toString(), "vh"),
   });
 }
