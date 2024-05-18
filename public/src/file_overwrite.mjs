983f96cfb7e451f8c69c411cecd482900ceb4cf6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 13:48:13 -0400

    2024-05-18T17:48:13.717Z  function_auto file_overwrite

--- public/src/file_overwrite.mjs
+++ public/src/file_overwrite.mjs
@@ -1,19 +1,14 @@
+import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
 import { global_get } from "./global_get.mjs";
 import { web_is } from "./web_is.mjs";
-import { import_node } from "./import_node.mjs";
 import { object_property_set } from "./object_property_set.mjs";
-import { folder_new } from "./folder_new.mjs";
 export async function file_overwrite(file_name, data) {
   if (web_is()) {
     let { files } = global_get();
     object_property_set(files, file_name, data);
   } else {
     let fs = await import("fs");
-    let path = await import("path");
-    let parent = path.dirname(file_name);
-    if (!await file_exists(parent)) {
-        await folder_new(parent);
-    }
+    await folder_parent_exists_ensure(file_name);
     return await fs.promises.writeFile(file_name, data, "utf-8");
   }
 }
