6c76592819bad892bfe9b9f16984188b16430938
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 08:35:09 -0400

    2024-05-18T12:35:09.666Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,19 @@
-import { logic_parse_tests_generate } from "./logic_parse_tests_generate.mjs";
+import { log } from "./log.mjs";
+const { TranslationServiceClient } = require("@google-cloud/translate");
 export async function sandbox() {
-  await logic_parse_tests_generate();
+  const translationClient = new TranslationServiceClient();
+  async function translateText() {
+    const request = {
+      parent: `projects/${projectId}/locations/${location}`,
+      contents: [text],
+      mimeType: "text/plain",
+      sourceLanguageCode: "en",
+      targetLanguageCode: "sr-Latn",
+    };
+    const [response] = await translationClient.translateText(request);
+    for (const translation of response.translations) {
+      console.log(`Translation: ${translation.translatedText}`);
+    }
+  }
+  translateText();
 }
