366811f759f19670ba0ebdc5d52828d53e794dc9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 15:43:29 -0400

    2024-05-18T19:43:28.926Z  function_auto yyy8Uu_part

--- public/src/yyy8Uu_part.mjs
+++ public/src/yyy8Uu_part.mjs
@@ -1,7 +1,6 @@
 import { yyy8Uu_parts } from "./yyy8Uu_parts.mjs";
 import { list_get } from "./list_get.mjs";
 export async function yyy8Uu_part(index, index2) {
-    const parts = await yyy8Uu_parts(index);
-    console.log({parts, index, index2})
-  return[ list_get(parts, index2)];
+  const parts = await yyy8Uu_parts(index);
+  return [list_get(parts, index2)];
 }
