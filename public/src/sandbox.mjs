88ae19fb55f57cb81b3b8dfea78dc9969f98a0a5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 13:37:28 -0400

    2024-05-18T17:37:27.892Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,11 +1,11 @@
+import { gcloud_translate_cache } from "./gcloud_translate_cache.mjs";
 import { sandbox_texts } from "./sandbox_texts.mjs";
-import { gcloud_translate } from "./gcloud_translate.mjs";
 import { yyy8Uu_file_paths } from "./yyy8Uu_file_paths.mjs";
 import { yyy8Uu_parts } from "./yyy8Uu_parts.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { yyy8Uu_file_path_to_parts } from "./yyy8Uu_file_path_to_parts.mjs";
 export async function sandbox() {
-  return await gcloud_translate("en", "es", sandbox_texts.name, ["john"]);
+  return await gcloud_translate_cache("en", "es", sandbox_texts.name, ["john"]);
   let index = 0;
   let result = await yyy8Uu_parts(index);
   return result;
