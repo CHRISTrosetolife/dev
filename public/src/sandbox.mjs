ecfaada6a712b77cfb6ee731ed3b679dca6147ea
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 11:41:52 -0400

    2024-05-18T15:41:52.603Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,10 +1,11 @@
+import { string_includes } from "./string_includes.mjs";
 import { log } from "./log.mjs";
 import { folder_read } from "./folder_read.mjs";
 import { list_filter } from "./list_filter.mjs";
 export async function sandbox() {
   let path = "C:\\Users\\JESUS\\Downloads\\yyy8Uu-master\\yyy8Uu-master";
   let files = await folder_read(path, ".xml");
-  let filtered = list_filter(files, "\\cod-");
+  let filtered = list_filter(files, (f) => string_includes(f, "\\cod-"));
   for (let f of filtered) {
     log(f);
     return;
