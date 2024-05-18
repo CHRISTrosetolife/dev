88e7357ee391ded6b9b58b5ba078c91213c04897
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 16:41:19 -0400

    2024-05-18T20:41:19.488Z  function_auto app_yyy8Uu

--- public/src/app_yyy8Uu.mjs
+++ public/src/app_yyy8Uu.mjs
@@ -1,1 +1,4 @@
-export function app_yyy8Uu() {}
+import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
+export function app_yyy8Uu() {
+  html_style_default_initialize();
+}
