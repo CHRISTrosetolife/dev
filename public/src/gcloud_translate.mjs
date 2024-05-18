250e3ef071979094736b25deb1b1a34beec97cf0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-18 09:17:58 -0400

    2024-05-18T13:17:58.438Z  function_auto gcloud_translate

--- public/src/gcloud_translate.mjs
+++ public/src/gcloud_translate.mjs
@@ -1,22 +1,20 @@
 import { list_map } from "./list_map.mjs";
 import { TranslationServiceClient } from "@google-cloud/translate";
-export async function gcloud_translate() {
-  let text = "hello";
-  let sourceLanguageCode = "en";
-  let targetLanguageCode = "es";
+export async function gcloud_translate(
+  sourceLanguageCode,
+  targetLanguageCode,
+  text,
+) {
   const translationClient = new TranslationServiceClient();
-  async function translateText() {
-    const request = {
-      parent: `projects/letjesusbeexalted/locations/global`,
-      contents: [text],
-      mimeType: "text/plain",
-      sourceLanguageCode: sourceLanguageCode,
-      targetLanguageCode: targetLanguageCode,
-    };
-    const [response] = await translationClient.translateText(request);
-    let { translations } = response;
-    let result = list_map(translations, (t) => t.translatedText);
-    return result;
-  }
-  return await translateText();
+  const request = {
+    parent: `projects/letjesusbeexalted/locations/global`,
+    contents: [text],
+    mimeType: "text/plain",
+    sourceLanguageCode: sourceLanguageCode,
+    targetLanguageCode: targetLanguageCode,
+  };
+  const [response] = await translationClient.translateText(request);
+  let { translations } = response;
+  let result = list_map(translations, (t) => t.translatedText);
+  return result;
 }
