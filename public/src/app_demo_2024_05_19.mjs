967ad6ad90d2d79def98ff021bf3a4659a814204
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-19 16:15:52 -0400

    2024-05-19T20:15:50.855Z  function_auto app_demo_2024_05_19

--- public/src/app_demo_2024_05_19.mjs
+++ public/src/app_demo_2024_05_19.mjs
@@ -1,3 +1,5 @@
+import { html_style_alternate_monospace_short_multiple } from "./html_style_alternate_monospace_short_multiple.mjs";
+import { html_style_alternate_monospace_short } from "./html_style_alternate_monospace_short.mjs";
 import { html_img_limited_centered } from "./html_img_limited_centered.mjs";
 import { html_buttons_next_previous } from "./html_buttons_next_previous.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
@@ -9,7 +11,14 @@ export function app_demo_2024_05_19() {
   let index_last = 0;
   let screens = [
     (parent) => {
+      html_style_alternate_monospace_short;
+      ("`string_combine` is a function that concatenate two strings");
       html_img_limited_centered(parent, "demo\\2024_05_19\\1.png");
+      html_style_alternate_monospace_short_multiple;
+      ("any function can be ran from the command line using `run.mjs`");
+      ("just pass in any arguments like `hello` and `' world` like this:");
+      ("this speeds up developing a new function");
+      html_img_limited_centered(parent, "demo\\2024_05_19\\2.png");
     },
   ];
   refresh_lesson_screen(index);
