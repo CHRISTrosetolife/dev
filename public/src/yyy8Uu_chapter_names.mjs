8efe05458f454fef7082bf6a4ea4442e44bbdb2a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-19 09:54:24 -0400

    2024-05-19T13:54:24.618Z  yyy8Uu_chapter_names

--- public/src/yyy8Uu_chapter_names.mjs
+++ public/src/yyy8Uu_chapter_names.mjs
@@ -5,6 +5,6 @@ export async function yyy8Uu_chapter_names() {
   return await yyy8Uu_symlinks(each_symlink);
   async function each_symlink(s) {
     let dirname = path_dirname(s);
-    return path_parse_base(dirname);
+    return path_parse_base(s);
   }
 }
