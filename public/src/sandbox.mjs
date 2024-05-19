b2af9deee7d7f7e20d59f9a6cf8111507a8251ba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-19 12:22:37 -0400

    2024-05-19T16:22:36.256Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -9,46 +9,33 @@ import { yyy8Uu_parts } from "./yyy8Uu_parts.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { yyy8Uu_file_path_to_parts } from "./yyy8Uu_file_path_to_parts.mjs";
 import { list_single } from "./list_single.mjs";
-import { list_first } from "./list_first.mjs";
 export async function sandbox() {
   if (false) {
-    let firsts = await list_adder_async(async (la_outer) => {
-      for (let index of range(16)) {
-        let latin = await yyy8Uu_parts(index);
-        let english = await list_adder_async(async (la) => {
-          await each_index_async(latin, async (part, index2) => {
-            let translateds = await gcloud_translate_cache(
-              "la",
-              "en",
-              yyy8Uu_part.name,
-              [index, index2],
-            );
-            let translated = list_single(translateds);
-            la(translated);
-          });
-        });
-        la_outer(list_first(english));
-        if (false) {
-          let destination = yyy8Uu_storage_path(index);
-          await storage_upload_object(
-            {
-              english,
-              latin,
-            },
-            destination,
+    for (let index of range(16)) {
+      let latin = await yyy8Uu_parts(index);
+      let english = await list_adder_async(async (la) => {
+        await each_index_async(latin, async (part, index2) => {
+          let translateds = await gcloud_translate_cache(
+            "la",
+            "en",
+            yyy8Uu_part.name,
+            [index, index2],
           );
-        }
+          let translated = list_single(translateds);
+          la(translated);
+        });
+      });
+      if (false) {
+        let destination = yyy8Uu_storage_path(index);
+        await storage_upload_object(
+          {
+            english,
+            latin,
+          },
+          destination,
+        );
       }
-    });
-    return firsts;
-    let destination = yyy8Uu_storage_path("index");
-    await storage_upload_object(
-      {
-        firsts,
-      },
-      destination,
-    );
-    return;
+    }
   }
   let filtered = await yyy8Uu_translate();
   return;
