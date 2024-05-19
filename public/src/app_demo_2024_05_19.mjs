f3bdfd396f6b1f76a01fa0a1f113ebbbc5e15db6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-19 16:32:12 -0400

    2024-05-19T20:32:12.328Z  function_search short

--- public/src/app_demo_2024_05_19.mjs
+++ public/src/app_demo_2024_05_19.mjs
@@ -41,6 +41,9 @@ export function app_demo_2024_05_19() {
         "suppose we make a change to `noop` :",
       ]);
       html_img_limited_centered(parent, "demo\\2024_05_19\\4.png");
+      html_style_alternate_monospace_short_multiple(parent, [
+        "then , if we change `run.mjs` to `run_git.mjs` ...",
+      ]);
       html_img_limited_centered(parent, "demo\\2024_05_19\\5.png");
     },
   ];
