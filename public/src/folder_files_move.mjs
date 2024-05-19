e12260f543ea20ee924f264126d2c701abf5b5c5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-19 16:00:01 -0400

    2024-05-19T19:59:59.334Z  function_auto folder_files_move

--- public/src/folder_files_move.mjs
+++ public/src/folder_files_move.mjs
@@ -1,9 +1,9 @@
+import { path_parse_base } from "./path_parse_base.mjs";
 import { file_rename } from "./file_rename.mjs";
 import { add_1 } from "./add_1.mjs";
 import { string_to } from "./string_to.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { path_join } from "./path_join.mjs";
-import { path_extname } from "./path_extname.mjs";
 import { path_dirname } from "./path_dirname.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { folder_read } from "./folder_read.mjs";
@@ -11,7 +11,7 @@ export async function folder_files_move(input_directory, file_extension) {
   let file_paths = await folder_read(input_directory, file_extension);
   await each_index_async(file_paths, async (file_path, index) => {
     let dirname = path_dirname(file_path);
-    let extension = path_extname(file_path);
+    let file_name = path_parse_base(file_path);
     let file_path_new = path_join([
       dirname,
       string_combine(string_to(add_1(index)), extension),
