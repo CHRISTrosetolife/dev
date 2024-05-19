36a888637fb62983254196f2f34aa715d94a11a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-19 16:10:59 -0400

    2024-05-19T20:10:56.979Z  function_auto app_demo_2024_05_19

--- public/src/app_demo_2024_05_19.mjs
+++ public/src/app_demo_2024_05_19.mjs
@@ -1,3 +1,4 @@
+import { html_img_limited_centered } from "./html_img_limited_centered.mjs";
 import { html_buttons_next_previous } from "./html_buttons_next_previous.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
@@ -8,6 +9,7 @@ export function app_demo_2024_05_19() {
   refresh_lesson_screen(index);
   async function refresh_lesson_screen(index) {
     html_clear_scroll_top(root);
+    html_img_limited_centered();
     html_buttons_next_previous(root, refresh_lesson_screen, index, index_last);
   }
 }
