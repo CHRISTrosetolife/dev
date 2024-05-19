0979421017f8b4d2623c24b611948d7b49f1f764
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-19 15:53:20 -0400

    2024-05-19T19:53:19.857Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,19 +1,24 @@
-import { file_rename } from "./file_rename.mjs";
 import { yyy8Uu_translate_upload } from "./yyy8Uu_translate_upload.mjs";
 import { yyy8Uu_translate } from "./yyy8Uu_translate.mjs";
 import { yyy8Uu_parts } from "./yyy8Uu_parts.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { yyy8Uu_file_path_to_parts } from "./yyy8Uu_file_path_to_parts.mjs";
 import { folder_read } from "./folder_read.mjs";
+import { path_dirname } from "./path_dirname.mjs";
+import { path_extname } from "./path_extname.mjs";
+import { log } from "./log.mjs";
+import { path_join } from "./path_join.mjs";
+import { string_combine } from "./string_combine.mjs";
 export async function sandbox() {
   let file_paths = await folder_read(
     "C:\\Users\\JESUS\\Pictures\\Screenshots",
     "",
   );
-  for (let file_path of file_paths) {
-    continue;
-    file_rename();
-  }
+  await each_index_async(file_paths, async (file_path, index) => {
+    let dirname = path_dirname(file_path);
+    let extension = path_extname(file_path);
+    log(path_join([dirname, string_combine()]));
+  });
   return;
   if (false) {
     await yyy8Uu_translate_upload();
