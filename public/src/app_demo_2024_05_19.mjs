0ff949d13de1680f503aa3a826b3bc2ac811cbc5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-19 16:12:49 -0400

    2024-05-19T20:12:49.391Z  function_auto app_demo_2024_05_19

--- public/src/app_demo_2024_05_19.mjs
+++ public/src/app_demo_2024_05_19.mjs
@@ -2,14 +2,20 @@ import { html_img_limited_centered } from "./html_img_limited_centered.mjs";
 import { html_buttons_next_previous } from "./html_buttons_next_previous.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
+import { list_get } from "./list_get.mjs";
 export function app_demo_2024_05_19() {
   let root = html_style_default_initialize();
   let index = 0;
   let index_last = 0;
+  let screens = [
+    (parent) => {
+      html_img_limited_centered(parent, "demo\\2024_05_19\\1.png");
+    },
+  ];
   refresh_lesson_screen(index);
   async function refresh_lesson_screen(index) {
     html_clear_scroll_top(root);
-    html_img_limited_centered();
+    list_get(screens, 0)(root);
     html_buttons_next_previous(root, refresh_lesson_screen, index, index_last);
   }
 }
