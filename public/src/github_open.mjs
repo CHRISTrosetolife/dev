6db5f64c222eefe8171c4850ae319e14320fec05
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-19 14:24:41 -0400

    2024-05-19T18:24:40.809Z  function_auto github_open

--- public/src/github_open.mjs
+++ public/src/github_open.mjs
@@ -1,1 +1,5 @@
-export function github_open(function_name) {}
+import { function_name_to_path } from "./function_name_to_path.mjs";
+export function github_open(function_name) {
+  let function_path = function_name_to_path(function_name);
+  let url = `https://github.com/CHRISTrosetolife/dev/blob/master/`;
+}
