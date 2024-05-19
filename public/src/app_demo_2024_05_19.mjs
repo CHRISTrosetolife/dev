d2fc47fd663770edc8f68478bbb4e2887c85ae75
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-19 16:39:04 -0400

    2024-05-19T20:39:03.866Z  noop

--- public/src/app_demo_2024_05_19.mjs
+++ public/src/app_demo_2024_05_19.mjs
@@ -53,6 +53,7 @@ export function app_demo_2024_05_19() {
       html_img_limited_centered(parent, "demo\\2024_05_19\\8.png");
     },
     (parent) => {
+        
       html_img_limited_centered(parent, "demo\\2024_05_19\\9.png");
     },
   ];
