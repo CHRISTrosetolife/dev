a2acf7276a7be431503b6cfbcd525d31e7f1c525
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-19 16:10:03 -0400

    2024-05-19T20:10:02.957Z  function_auto app_demo_2024_05_19

--- public/src/app_demo_2024_05_19.mjs
+++ public/src/app_demo_2024_05_19.mjs
@@ -2,9 +2,9 @@ import { html_buttons_next_previous } from "./html_buttons_next_previous.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_demo_2024_05_19() {
-  html_style_default_initialize();
+  let root = html_style_default_initialize();
   let index = 0;
-  refresh_lesson_screen(index)
+  refresh_lesson_screen(index);
   async function refresh_lesson_screen(index) {
     html_clear_scroll_top(root);
     html_buttons_next_previous(root, refresh_lesson_screen, index, index_last);
