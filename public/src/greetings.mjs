d0ccabbfbc43ffd5f4ec9f6180e873495ed28a1a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-19 15:07:27 -0400

    2024-05-19T19:07:27.222Z  function_auto greetings

--- public/src/greetings.mjs
+++ public/src/greetings.mjs
@@ -1,3 +1,5 @@
+import { list_random_item } from "./list_random_item.mjs";
 export function greetings(name) {
-  return "hello " + name;
+  let greeting_word = list_random_item(["hello", "hey", "howdy"]);
+  return greeting_word + " " + name;
 }
