2064493f15fb4eea64efe20198d03ef03dd25c3d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-19 09:54:48 -0400

    2024-05-19T13:54:47.896Z  function_auto yyy8Uu_chapter_names

--- public/src/yyy8Uu_chapter_names.mjs
+++ public/src/yyy8Uu_chapter_names.mjs
@@ -1,10 +1,8 @@
 import { path_parse_base } from "./path_parse_base.mjs";
 import { yyy8Uu_symlinks } from "./yyy8Uu_symlinks.mjs";
-import { path_dirname } from "./path_dirname.mjs";
 export async function yyy8Uu_chapter_names() {
   return await yyy8Uu_symlinks(each_symlink);
   async function each_symlink(s) {
-    let dirname = path_dirname(s);
     return path_parse_base(s);
   }
 }
