673a11f419d5448b6af1e3806e0056513878a5aa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-19 15:55:52 -0400

    2024-05-19T19:55:51.929Z  function_auto sandbox

---
+++ public/src/folder_files_rename_incrementing.mjs
@@ -0,0 +1,24 @@
+import { file_rename } from "./file_rename.mjs";
+import { add_1 } from "./add_1.mjs";
+import { string_to } from "./string_to.mjs";
+import { string_combine } from "./string_combine.mjs";
+import { path_join } from "./path_join.mjs";
+import { path_extname } from "./path_extname.mjs";
+import { path_dirname } from "./path_dirname.mjs";
+import { each_index_async } from "./each_index_async.mjs";
+import { folder_read } from "./folder_read.mjs";
+export async function folder_files_rename_incrementing(
+  input_directory,
+  file_extension,
+) {
+  let file_paths = await folder_read(input_directory, file_extension);
+  await each_index_async(file_paths, async (file_path, index) => {
+    let dirname = path_dirname(file_path);
+    let extension = path_extname(file_path);
+    let file_path_new = path_join([
+      dirname,
+      string_combine(string_to(add_1(index)), extension),
+    ]);
+    await file_rename(file_path, file_path_new);
+  });
+}
--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,29 +1,13 @@
-import { file_rename } from "./file_rename.mjs";
-import { add_1 } from "./add_1.mjs";
-import { string_to } from "./string_to.mjs";
+import { folder_files_rename_incrementing } from "./folder_files_rename_incrementing.mjs";
 import { yyy8Uu_translate_upload } from "./yyy8Uu_translate_upload.mjs";
 import { yyy8Uu_translate } from "./yyy8Uu_translate.mjs";
 import { yyy8Uu_parts } from "./yyy8Uu_parts.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { yyy8Uu_file_path_to_parts } from "./yyy8Uu_file_path_to_parts.mjs";
-import { folder_read } from "./folder_read.mjs";
-import { path_dirname } from "./path_dirname.mjs";
-import { path_extname } from "./path_extname.mjs";
-import { path_join } from "./path_join.mjs";
-import { string_combine } from "./string_combine.mjs";
 export async function sandbox() {
   const input_directory = "C:\\Users\\JESUS\\Pictures\\Screenshots";
   const file_extension = ".png";
-  let file_paths = await folder_read(input_directory, file_extension);
-  await each_index_async(file_paths, async (file_path, index) => {
-    let dirname = path_dirname(file_path);
-    let extension = path_extname(file_path);
-    let file_path_new = path_join([
-      dirname,
-      string_combine(string_to(add_1(index)), extension),
-    ]);
-    await file_rename(file_path, file_path_new);
-  });
+  await folder_files_rename_incrementing(input_directory, file_extension);
   return;
   if (false) {
     await yyy8Uu_translate_upload();
