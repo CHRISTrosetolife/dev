4c62789b64dc326241fb043b468fdcb3e3fcd414
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-20 16:15:16 -0400

    2024-05-20T20:15:14.610Z  function_auto lesson_object_properties_shorthand_two

--- public/src/lesson_object_properties_shorthand_two.mjs
+++ public/src/lesson_object_properties_shorthand_two.mjs
@@ -1,3 +1,5 @@
+import { equal_by } from "./equal_by.mjs";
+import { integer_random_digit_single_positive_multiple } from "./integer_random_digit_single_positive_multiple.mjs";
 import { js_code_object_property } from "./js_code_object_property.mjs";
 import { app_learn_code_code_part_contrast } from "./app_learn_code_code_part_contrast.mjs";
 import { html_p_text } from "./html_p_text.mjs";
@@ -12,6 +14,9 @@ import { lesson_simple } from "./lesson_simple.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { app_learn_code_log } from "./app_learn_code_log.mjs";
 import { string_combine } from "./string_combine.mjs";
+import { list_skip } from "./list_skip.mjs";
+import { list_first } from "./list_first.mjs";
+import { assert } from "./assert.mjs";
 export function lesson_object_properties_shorthand_two() {
   let description = "object with two properties shorthand";
   let example_message = string_combine("with an ", description);
@@ -32,12 +37,17 @@ export function lesson_object_properties_shorthand_two() {
   );
   function example_before(parent) {}
   function screen1(parent) {
-    let [a, b] = app_learn_code_random_identifiers_sorted(2);
-    let c = integer_random_digit_single_positive();
+    let identifiers = app_learn_code_random_identifiers_sorted(3);
+    let a = list_first(identifiers);
+    let names = list_skip(identifiers, 1);
+    let values = integer_random_digit_single_positive_multiple(2);
+    let result = [];
+    assert(equal_by, []);
     html_p_text(parent, "here is some code :");
     let lines = [
       js_code_statement_let_assign(a, js_code_braces()),
-      js_code_statement_assign(js_code_dot(a, b), c),
+      js_code_statement_assign(js_code_dot(a, b), d),
+      js_code_statement_assign(js_code_dot(a, c), e),
     ];
     app_learn_code_code_part_contrast(parent, list_join_newline(lines));
     html_p_text(parent, "that code can be shortened to :");
