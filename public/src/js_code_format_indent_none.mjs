56dc292d8acabdf1dd38f80e4313cb6da19408a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-20 15:53:18 -0400

    2024-05-20T19:53:18.464Z  function_auto js_code_format_indent_none

--- public/src/js_code_format_indent_none.mjs
+++ public/src/js_code_format_indent_none.mjs
@@ -1,1 +1,6 @@
-export function js_code_format_indent_none(code) {}
+import { js_unparse_indent_none } from "./js_unparse_indent_none.mjs";
+import { js_parse } from "./js_parse.mjs";
+export function js_code_format_indent_none(code) {
+  let parsed = js_parse(code);
+  return js_unparse_indent_none(parsed);
+}
