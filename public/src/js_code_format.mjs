becf0fce80a1b544ffdb959213b792f9023ea7fd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-20 15:51:41 -0400

    2024-05-20T19:51:40.709Z  function_auto js_code_format

--- public/src/js_code_format.mjs
+++ public/src/js_code_format.mjs
@@ -1,7 +1,6 @@
-import {js_unparse} from "./js_unparse.mjs";
-import {js_parse} from "./js_parse.mjs";
-export function js_code_format(source) {
-    let parsed = js_parse(source);
-    source = js_unparse(parsed);
-    return source;
+import * as prettier from "prettier";
+export async function js_code_format(code) {
+  return await prettier.format(code, {
+    parser: "babel",
+  });
 }
