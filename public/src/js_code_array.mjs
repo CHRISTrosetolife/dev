8044d581924cca3d4bdf3609a6e40fad7eea86dd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-20 18:10:22 -0400

    2024-05-20T22:10:21.929Z  function_search sorted

--- public/src/js_code_array.mjs
+++ public/src/js_code_array.mjs
@@ -1,3 +1,5 @@
-export function js_code_array() {
-  return `[]`;
+import { list_join } from "./list_join.mjs";
+
+export function js_code_array(contents_list) {
+  return `[ ${list_join(contents_list, ', ')} ]`;
 }
