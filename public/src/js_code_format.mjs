2b9bb62ad36301cd3a043f373f67e006ae362bca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-20 15:51:16 -0400

    2024-05-20T19:51:15.891Z  function_auto js_code_format

--- public/src/js_code_format.mjs
+++ public/src/js_code_format.mjs
@@ -1,7 +1,7 @@
-import { js_unparse } from "./js_unparse.mjs";
-import { js_parse } from "./js_parse.mjs";
+import {js_unparse} from "./js_unparse.mjs";
+import {js_parse} from "./js_parse.mjs";
 export function js_code_format(source) {
-  let parsed = js_parse(source);
-  source = js_unparse(parsed);
-  return source;
+    let parsed = js_parse(source);
+    source = js_unparse(parsed);
+    return source;
 }
