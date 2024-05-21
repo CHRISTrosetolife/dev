ee8c860240eddd9fa2b0f74cd1313351cadce1e5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-21 13:28:45 -0400

    2024-05-21T17:28:45.598Z  noop

--- public/src/logic_step_substitution_tests_generate.mjs
+++ public/src/logic_step_substitution_tests_generate.mjs
@@ -4,15 +4,6 @@ import { tests_generate_single } from "./tests_generate_single.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 export async function logic_step_substitution_tests_generate() {
   let inputs = [
-    "a()",
-    "a(b())",
-    "a(b(c()))",
-    "a(b(),c())",
-    "a(b(),c(d()))",
-    "a(b(),c(d(),e()))",
-    "a(b(),c(d(),e(),f()))",
-    "a(b)",
-    "a(b,c)",
   ];
   await each_index_async(inputs, async (input, index) => {
     await tests_generate_single(logic_parse.name, [input], add_1(index));
