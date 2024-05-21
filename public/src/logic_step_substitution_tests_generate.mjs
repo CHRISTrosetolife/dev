87d85fc20e84ac78dfb31291936b1c323b491f68
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-21 16:03:12 -0400

    2024-05-21T20:03:11.991Z  logic_step_substitution_tests_generate

--- public/src/logic_step_substitution_tests_generate.mjs
+++ public/src/logic_step_substitution_tests_generate.mjs
@@ -5,7 +5,7 @@ import { each_index_async } from "./each_index_async.mjs";
 import { tests_generate_single_generic } from "./tests_generate_single_generic.mjs";
 import { logic_parse } from "./logic_parse.mjs";
 export async function logic_step_substitution_tests_generate() {
-  let inputs = [["all(x,equal(x,x))", "zero"]];
+  let inputs = [["all(x,equal(x,x))", "zero"],["all(x,equal(x,x))", "and(and(true,false),true)"]];
   await each_index_async(inputs, async (input, index) => {
     await tests_generate_single_generic(
       logic_step_substitution.name,
