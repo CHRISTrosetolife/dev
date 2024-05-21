c8d17947a49b596264ec04995362ae500a0894d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-21 14:08:30 -0400

    2024-05-21T18:08:29.985Z  function_copy logic_parse_tests_generate logic_unparse_tests_generate

---
+++ public/src/logic_unparse_tests_generate.mjs
@@ -0,0 +1,11 @@
+import { logic_parse_tests } from "./logic_parse_tests.mjs";
+import { add_1 } from "./add_1.mjs";
+import { logic_parse } from "./logic_parse.mjs";
+import { tests_generate_single } from "./tests_generate_single.mjs";
+import { each_index_async } from "./each_index_async.mjs";
+export async function logic_unparse_tests_generate() {
+  let inputs = logic_parse_tests();
+  await each_index_async(inputs, async (input, index) => {
+    await tests_generate_single(logic_parse.name, [input], add_1(index));
+  });
+}
