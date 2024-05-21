f17923c03bf92d3a58d3d186c11aba534b019a7e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-20 21:03:01 -0400

    2024-05-21T01:03:01.616Z  function_auto tests_generate

--- public/src/tests_generate.mjs
+++ public/src/tests_generate.mjs
@@ -1,16 +1,13 @@
+import { function_new_generic } from "./function_new_generic.mjs";
 import { string_includes } from "./string_includes.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { function_names } from "./function_names.mjs";
+import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function tests_generate() {
   let names = await function_names();
   let filtered = list_filter(names, (n) => string_includes(n, "_test_"));
-  await function_new_generic(
-    function_name,
-    "",
-    "",
-    false,
-    [],
-    false,
-  );
+  let function_name = "tests";
+  let function_path = function_name_to_path(function_name);
+  await function_new_generic(function_name, "", "", false, [], false);
   return filtered;
 }
