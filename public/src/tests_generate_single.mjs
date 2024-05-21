266fa7d8a65c3a2ceeac915f35144eee9fe48f35
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-21 14:13:29 -0400

    2024-05-21T18:13:29.484Z  logic_unparse_tests_generate

--- public/src/tests_generate_single.mjs
+++ public/src/tests_generate_single.mjs
@@ -1,7 +1,8 @@
 import { tests_generate_single_generic } from "./tests_generate_single_generic.mjs";
 import { identity } from "./identity.mjs";
+import { list_map } from "./list_map.mjs";
 export async function tests_generate_single(function_name, args, test_number) {
-  let args_mappers = [identity, identity];
+  let args_mappers = list_map(args, a => identity);
   let result_mapper = identity;
   await tests_generate_single_generic(
     function_name,
