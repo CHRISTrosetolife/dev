2aff3fafe19048dd62fe5d1abd55d721b7bac90d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-20 21:02:26 -0400

    2024-05-21T01:02:25.975Z  function_search delete

--- public/src/tests_generate.mjs
+++ public/src/tests_generate.mjs
@@ -4,5 +4,13 @@ import { function_names } from "./function_names.mjs";
 export async function tests_generate() {
   let names = await function_names();
   let filtered = list_filter(names, (n) => string_includes(n, "_test_"));
+  await function_new_generic(
+    function_name,
+    "",
+    "",
+    false,
+    [],
+    false,
+  );
   return filtered;
 }
