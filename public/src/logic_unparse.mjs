ab3ced932acb6ade2769439c2debc34d04b946d3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-21 14:07:34 -0400

    2024-05-21T18:07:33.806Z  function_auto logic_unparse

--- public/src/logic_unparse.mjs
+++ public/src/logic_unparse.mjs
@@ -1,6 +1,7 @@
 import { assert } from "./assert.mjs";
 import { equal } from "./equal.mjs";
 import { list_join } from "./list_join.mjs";
+import { list_map } from "./list_map.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_is } from "./string_is.mjs";
 export function logic_unparse(parse) {
@@ -11,7 +12,8 @@ export function logic_unparse(parse) {
     return name;
   }
   assert(equal(type, "call"));
-  let { callee } = parse;
+  let { callee, args } = parse;
+  let mapped = list_map(args, logic_unparse);
   return string_combine_multiple([
     logic_unparse(callee),
     "(",
