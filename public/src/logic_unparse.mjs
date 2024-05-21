5e17522b319b4b9549751452feae850f32693bef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-21 14:07:12 -0400

    2024-05-21T18:07:12.409Z  function_auto logic_unparse

--- public/src/logic_unparse.mjs
+++ public/src/logic_unparse.mjs
@@ -1,5 +1,7 @@
 import { assert } from "./assert.mjs";
 import { equal } from "./equal.mjs";
+import { list_join } from "./list_join.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_is } from "./string_is.mjs";
 export function logic_unparse(parse) {
   let { type } = parse;
@@ -10,5 +12,10 @@ export function logic_unparse(parse) {
   }
   assert(equal(type, "call"));
   let { callee } = parse;
-  return logic_unparse(callee);
+  return string_combine_multiple([
+    logic_unparse(callee),
+    "(",
+    list_join(args, ","),
+    ")",
+  ]);
 }
