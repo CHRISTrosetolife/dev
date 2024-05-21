c26ea0362e01a1ad74dcfe620aa536e4f7d93c4a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-21 14:03:36 -0400

    2024-05-21T18:03:35.948Z  function_new_args logic_unparse parse

--- public/src/logic_step_substitution_tests_generate.mjs
+++ public/src/logic_step_substitution_tests_generate.mjs
@@ -3,13 +3,17 @@ import { tests } from "./tests.mjs";
 import { add_1 } from "./add_1.mjs";
 import { tests_generate_single } from "./tests_generate_single.mjs";
 import { each_index_async } from "./each_index_async.mjs";
+import { tests_generate_single_generic } from "./tests_generate_single_generic.mjs";
+import { logic_parse } from "./logic_parse.mjs";
 export async function logic_step_substitution_tests_generate() {
   let inputs = [["all(x,equal(x,x))", "zero"]];
   await each_index_async(inputs, async (input, index) => {
-    await tests_generate_single(
+    await tests_generate_single_generic(
       logic_step_substitution.name,
       input,
       add_1(index),
+      [logic_parse],
+      logic_unparse
     );
   });
 }
---
+++ public/src/logic_unparse.mjs
@@ -0,0 +1,1 @@
+export function logic_unparse(parse) {}
