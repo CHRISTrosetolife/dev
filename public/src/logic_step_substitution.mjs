dd0b459cd5b6430ac58ca30d22e077b268c23214
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-21 14:37:09 -0400

    2024-05-21T18:37:09.619Z  function_auto logic_step_substitution

--- public/src/logic_step_substitution.mjs
+++ public/src/logic_step_substitution.mjs
@@ -2,13 +2,13 @@ import { assert } from "./assert.mjs";
 import { equal } from "./equal.mjs";
 import { list_length } from "./list_length.mjs";
 export function logic_step_substitution(statement, substitution) {
-  let { type, args } = statement;
-  assert(equal, [type, "call"]);
   let {
+    type,
     callee: { type: callee_type, name },
+    args: [variable, inner],
   } = statement;
+  assert(equal, [type, "call"]);
   assert(equal, [callee_type, "identifier"]);
   assert(equal, [name, "all"]);
   assert(equal, [list_length(args), 2]);
-  let [variable] = args;
 }
