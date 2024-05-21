3b75c65b858d1e119cd87131975c71d23fc3df1b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-20 21:01:38 -0400

    2024-05-21T01:01:38.577Z  function_auto_run tests_generate

--- public/src/tests_generate.mjs
+++ public/src/tests_generate.mjs
@@ -1,6 +1,8 @@
+import { string_includes } from "./string_includes.mjs";
+import { list_filter } from "./list_filter.mjs";
 import { function_names } from "./function_names.mjs";
 export async function tests_generate() {
   let names = await function_names();
-  let filtered=list_filter(names,n=>string_contains(n,'_test_'))
+  let filtered = list_filter(names, (n) => string_includes(n, "_test_"));
   return filtered;
 }
