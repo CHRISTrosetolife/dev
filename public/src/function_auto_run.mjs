18e228dc6165a2ff5609dcc9e8d1f416ec83f9d5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-20 21:01:21 -0400

    2024-05-21T01:01:21.048Z  function_auto function_auto_run

--- public/src/function_auto_run.mjs
+++ public/src/function_auto_run.mjs
@@ -1,1 +1,6 @@
-export function function_auto_run(function_name) {}
+import { function_run } from "./function_run.mjs";
+import { function_auto } from "./function_auto.mjs";
+export async function function_auto_run(function_name) {
+  await function_auto(function_name);
+  return await function_run(function_name, []);
+}
