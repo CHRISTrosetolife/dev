df1ad4b0b96054a267ff6d8b04a306c8957d815c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-21 14:40:39 -0400

    2024-05-21T18:40:39.712Z  function_auto logic_step_substitution

--- public/src/logic_step_substitution.mjs
+++ public/src/logic_step_substitution.mjs
@@ -2,6 +2,7 @@ import { logic_copy } from "./logic_copy.mjs";
 import { assert } from "./assert.mjs";
 import { equal } from "./equal.mjs";
 import { list_length } from "./list_length.mjs";
+import { string_is } from "./string_is.mjs";
 export function logic_step_substitution(statement, substitution) {
   let copy = logic_copy(statement);
   let {
@@ -15,4 +16,5 @@ export function logic_step_substitution(statement, substitution) {
   assert(equal, [list_length(args), 2]);
   let { type: variable_type, name: variable_name } = variable;
   assert(equal, [variable_type, "identifier"]);
+  assert(string_is, [variable_name]);
 }
