8d4bde1210895dda131d4aedb914e1e5339ba054
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-21 14:34:04 -0400

    2024-05-21T18:34:04.417Z  function_auto logic_step_substitution

--- public/src/logic_step_substitution.mjs
+++ public/src/logic_step_substitution.mjs
@@ -3,4 +3,8 @@ import { equal } from "./equal.mjs";
 export function logic_step_substitution(statement, substitution) {
   let { type } = statement;
   assert(equal, [type, "call"]);
+  let {
+    callee: { type: callee_type },
+  } = statement;
+  assert(equal, [callee_type, "identifier"]);
 }
