661f1c33b618d41b034cffa66d614a1fca4fde91
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-20 21:00:34 -0400

    2024-05-21T01:00:34.231Z  function_new_args function_auto_run function_name

--- public/src/tests_generate.mjs
+++ public/src/tests_generate.mjs
@@ -1,5 +1,6 @@
 import { function_names } from "./function_names.mjs";
 export async function tests_generate() {
   let names = await function_names();
-  return names;
+  let filtered=list_filter(names,n=>string_contains(n,'_test_'))
+  return filtered;
 }
