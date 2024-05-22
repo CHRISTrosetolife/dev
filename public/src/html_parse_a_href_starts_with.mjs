6a2efd2cd5a2cf0e7e33f91f9845da598c8639a3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 15:23:06 -0400

    2024-05-22T19:23:05.825Z  sandbox

--- public/src/html_parse_a_href_starts_with.mjs
+++ public/src/html_parse_a_href_starts_with.mjs
@@ -5,11 +5,12 @@ import { string_starts_with } from "./string_starts_with.mjs";
 import { list_filter } from "./list_filter.mjs";
 export function html_parse_a_href_starts_with(parsed, prefix) {
   let q = parsed.querySelectorAll("a[href]");
-  let filtered = list_filter(q, (i) => {
-    let href = i.getAttribute("href");
+  let mapped2 = list_map(q, 
+    i => i.getAttribute("href"))
+  let filtered = list_filter(mapped2, (href) => {
     return string_starts_with(href, prefix);
   });
-  console.log({q})
+  console.log({mapped2})
   let mapped = list_map(filtered, (i) => object_property_get(i, "text"));
   let mapped3 = list_map(mapped, (i) => string_trim(i));
   return mapped3;
