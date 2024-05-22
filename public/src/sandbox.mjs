c0e7aff7de41aef32308e838313fac32dea5da75
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 16:26:50 -0400

    2024-05-22T20:26:50.765Z  function_auto_run sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,24 +1,26 @@
+import { list_adder_async } from "./list_adder_async.mjs";
 import { list_length } from "./list_length.mjs";
 import { equal } from "./equal.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_single } from "./list_single.mjs";
 import { ceb_dictionary_page_each } from "./ceb_dictionary_page_each.mjs";
-import { log } from "./log.mjs";
 import { list_map } from "./list_map.mjs";
 import { assert } from "./assert.mjs";
 export async function sandbox() {
-  await ceb_dictionary_page_each((v) => {
-    let { url, letter, page_number, parsed } = v;
-    if (letter === "a" && page_number === 1) {
-      let tables = parsed.querySelectorAll(".table-responsive");
-      let div = list_single(tables);
-      let rows = div.querySelectorAll("tr");
-      for (let r of rows) {
-        let columns = r.querySelectorAll("td");
-        assert(equal, [list_length(columns), 2]);
-        let texts = list_map(columns, (c) => object_property_get(c, "text"));
+  return await list_adder_async(async (la) => {
+    await ceb_dictionary_page_each((v) => {
+      let { letter, page_number, parsed } = v;
+      if (letter === "a" && page_number === 1) {
+        let tables = parsed.querySelectorAll(".table-responsive");
+        let div = list_single(tables);
+        let rows = div.querySelectorAll("tr");
+        for (let r of rows) {
+          let columns = r.querySelectorAll("td");
+          assert(equal, [list_length(columns), 2]);
+          let texts = list_map(columns, (c) => object_property_get(c, "text"));
+          la(texts);
+        }
       }
-      log(list_length(rows));
-    }
+    });
   });
 }
