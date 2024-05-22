ebc5e24463dd3b714e5ba94eff879e34640cc36f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 18:02:26 -0400

    2024-05-22T22:02:26.507Z  function_auto ceb_dictionary_words

--- public/src/ceb_dictionary_words.mjs
+++ public/src/ceb_dictionary_words.mjs
@@ -1,3 +1,4 @@
+import { list_remove } from "./list_remove.mjs";
 import { string_empty_not_is } from "./string_empty_not_is.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { each_object } from "./each_object.mjs";
@@ -15,6 +16,7 @@ import { list_add } from "./list_add.mjs";
 import { list_sort_string } from "./list_sort_string.mjs";
 import { list_first } from "./list_first.mjs";
 import { string_length } from "./string_length.mjs";
+import { list_includes } from "./list_includes.mjs";
 export async function ceb_dictionary_words() {
   let lookup = {};
   await ceb_dictionary_page_each((v) => {
@@ -37,6 +39,9 @@ export async function ceb_dictionary_words() {
   });
   let list = list_adder((la) => {
     each_object(lookup, (key, value) => {
+      if (list_includes(value, key)) {
+        list_remove;
+      }
       la([key, value]);
     });
   });
