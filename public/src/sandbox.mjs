17564165d1001af31b2353809bca0b04a51741bc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 15:29:44 -0400

    2024-05-22T19:29:44.092Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { add_1 } from "./add_1.mjs";
 import { html_parse_a_href_starts_with } from "./html_parse_a_href_starts_with.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { folder_files_move } from "./folder_files_move.mjs";
@@ -6,13 +7,18 @@ import { yyy8Uu_translate } from "./yyy8Uu_translate.mjs";
 import { yyy8Uu_parts } from "./yyy8Uu_parts.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { yyy8Uu_file_path_to_parts } from "./yyy8Uu_file_path_to_parts.mjs";
-import { string_combine } from "./string_combine.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function sandbox() {
-  let prefix = 'https://www.learnentry.com/english-to-cebuano/dictionary/words-start-with-a?page=';
-  let page_number = `1`;
-  let parsed = await html_cache_parse(string_combine_multiple([prefix, page_number]));
-  let mapped3 = html_parse_a_href_starts_with(parsed, prefix);
+  let page_number = 1;
+  while (true) {
+    let prefix =
+      "https://www.learnentry.com/english-to-cebuano/dictionary/words-start-with-a?page=";
+    let parsed = await html_cache_parse(
+      string_combine_multiple([prefix, page_number]),
+    );
+    let mapped3 = html_parse_a_href_starts_with(parsed, prefix);
+    let next = add_1(page_number);
+  }
   return mapped3;
   const input_directory = "C:\\Users\\JESUS\\Pictures\\Screenshots";
   let output_directory = "C:\\Users\\JESUS\\dev\\public\\img\\demo\\2024_05_19";
