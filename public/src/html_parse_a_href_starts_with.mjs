57e72ed786c54ba997690516a03104e33e1919f2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 15:28:51 -0400

    2024-05-22T19:28:51.631Z  function_auto html_parse_a_href_starts_with

--- public/src/html_parse_a_href_starts_with.mjs
+++ public/src/html_parse_a_href_starts_with.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { string_trim } from "./string_trim.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_map } from "./list_map.mjs";
@@ -6,11 +7,13 @@ import { list_filter } from "./list_filter.mjs";
 export function html_parse_a_href_starts_with(parsed, prefix) {
   let q = parsed.querySelectorAll("a[href]");
   let filtered = list_filter(q, (i) => {
-    let href = i.getAttribute("href")
-    console.log({href,prefix})
+    let href = i.getAttribute("href");
+    console.log({
+      href,
+      prefix,
+    });
     return string_starts_with(href, prefix);
   });
-  console.log({filtered})
   let mapped = list_map(filtered, (i) => object_property_get(i, "text"));
   let mapped3 = list_map(mapped, (i) => string_trim(i));
   return mapped3;
