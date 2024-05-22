01294a576436c288396d7e0ffdee3b3339e668c0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 17:43:22 -0400

    2024-05-22T21:43:21.865Z  function_auto sandbox

--- public/src/ceb_dictionary_words.mjs
+++ public/src/ceb_dictionary_words.mjs
@@ -19,7 +19,7 @@ export async function ceb_dictionary_words() {
         assert(equal, [list_length(columns), 2]);
         let texts = list_map(columns, (c) => object_property_get(c, "text"));
         let mapped = list_map(texts, string_case_lower);
-        la(texts);
+        la(mapped);
       }
     });
   });
--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,5 @@
 import { ceb_dictionary_words } from "./ceb_dictionary_words.mjs";
-import { list_sort_string } from "./list_sort_string.mjs";
 export async function sandbox() {
   let dictionary = await ceb_dictionary_words();
-  return dictionary
+  return dictionary;
 }
