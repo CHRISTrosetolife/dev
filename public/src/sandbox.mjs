f6e82b8a9cbc7d62d6215c24734c864f1c23372f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 16:22:03 -0400

    2024-05-22T20:22:03.014Z  function_auto_run sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,8 +1,11 @@
+import { list_length } from "./list_length.mjs";
+import { equal } from "./equal.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_single } from "./list_single.mjs";
 import { ceb_dictionary_page_each } from "./ceb_dictionary_page_each.mjs";
 import { log } from "./log.mjs";
 import { list_map } from "./list_map.mjs";
+import { assert } from "./assert.mjs";
 export async function sandbox() {
   await ceb_dictionary_page_each((v) => {
     let { url, letter, page_number, parsed } = v;
@@ -10,6 +13,10 @@ export async function sandbox() {
       let tables = parsed.querySelectorAll(".table-responsive");
       let div = list_single(tables);
       let rows = div.querySelectorAll("tr");
+      for (let r of rows) {
+        let columns = r.querySelectorAll("td");
+        assert(equal, [list_length(columns), 2]);
+      }
       log(list_map(rows, (r) => object_property_get(r, "text")));
     }
   });
