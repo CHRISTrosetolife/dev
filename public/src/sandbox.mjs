e0f2761cf37d28c027261ce853a5615f047d7e84
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 16:33:04 -0400

    2024-05-22T20:33:04.021Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,4 @@
 import { ceb_dictionary_words } from "./ceb_dictionary_words.mjs";
 export async function sandbox() {
-  return await ceb_dictionary_words();
+  let dictionary = await ceb_dictionary_words();
 }
