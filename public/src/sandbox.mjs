79c55b99e550301ebd108552e742aef2f3918393
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 16:18:10 -0400

    2024-05-22T20:18:10.237Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { assert } from "./assert.mjs";
 import { ceb_dictionary_page_each } from "./ceb_dictionary_page_each.mjs";
 import { log } from "./log.mjs";
 export async function sandbox() {
@@ -5,6 +6,8 @@ export async function sandbox() {
     let { url, letter, page_number, parsed } = v;
     if (letter === "a" && page_number === 1) {
       log(url);
+      let tables = parsed.querySelectorAll(".table-responsive");
+      assert(list_length_is_1, [tables]);
     }
   });
 }
