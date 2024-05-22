48cbffc0a27bb0d567e70d4854fe0ea9fb0912dd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 16:21:01 -0400

    2024-05-22T20:21:01.132Z  function_auto_run sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,13 +1,16 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { list_single } from "./list_single.mjs";
 import { ceb_dictionary_page_each } from "./ceb_dictionary_page_each.mjs";
 import { log } from "./log.mjs";
+import { list_map } from "./list_map.mjs";
 export async function sandbox() {
   await ceb_dictionary_page_each((v) => {
     let { url, letter, page_number, parsed } = v;
     if (letter === "a" && page_number === 1) {
-      log(url);
       let tables = parsed.querySelectorAll(".table-responsive");
       let div = list_single(tables);
+      let rows = div.querySelectorAll("tr");
+      log(list_map(rows, (r) => object_property_get(r, "text")));
     }
   });
 }
