6cb5c32b7cdd2440aa8daf24329c203d343d0baa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 17:44:13 -0400

    2024-05-22T21:44:13.477Z  function_auto_run sandbox

--- public/src/ceb_dictionary_words.mjs
+++ public/src/ceb_dictionary_words.mjs
@@ -7,6 +7,7 @@ import { list_length } from "./list_length.mjs";
 import { list_single } from "./list_single.mjs";
 import { ceb_dictionary_page_each } from "./ceb_dictionary_page_each.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
+import { string_trim } from "./string_trim.mjs";
 export async function ceb_dictionary_words() {
   return await list_adder_async(async (la) => {
     await ceb_dictionary_page_each((v) => {
@@ -19,7 +20,8 @@ export async function ceb_dictionary_words() {
         assert(equal, [list_length(columns), 2]);
         let texts = list_map(columns, (c) => object_property_get(c, "text"));
         let mapped = list_map(texts, string_case_lower);
-        la(mapped);
+        let mapped2 = list_map(mapped, string_trim);
+        la(mapped2);
       }
     });
   });
