2fe087a8abfc718d134e5b57be18c2e700b26ccb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 18:02:43 -0400

    2024-05-23T22:02:43.036Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -5,6 +5,7 @@ import { ceb_definition } from "./ceb_definition.mjs";
 import { list_take } from "./list_take.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_map } from "./list_map.mjs";
+import { each_async } from "./each_async.mjs";
 export async function sandbox() {
   if (0) return await ceb_definition("kamo");
   let limit = 75;
@@ -14,6 +15,7 @@ export async function sandbox() {
   let group = list_take(atoms, group_count);
   let atom = list_first(group);
   let mapped = list_map(atom, list_first);
+  await each_async(mapped, async (m) => {});
   return mapped;
   let group_index = 0;
   if (0) await storage_upload_object(group, ceb_group_path(group_index));
