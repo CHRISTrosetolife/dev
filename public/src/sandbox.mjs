662bc45a142a35c98a22f80dd3eaee283086bdf9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 17:09:42 -0400

    2024-05-23T21:09:42.327Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,10 +1,8 @@
-import { ceb_atom_count } from "./ceb_atom_count.mjs";
 import { ceb_bible_words_definitions_atoms } from "./ceb_bible_words_definitions_atoms.mjs";
 import { ceb_definition } from "./ceb_definition.mjs";
 import { list_length } from "./list_length.mjs";
 export async function sandbox() {
   if (0) return await ceb_definition("kamo");
-  let atom_count = ceb_atom_count();
   let limit = 75;
   let skip = 0;
   let atoms = await ceb_bible_words_definitions_atoms(skip, limit);
