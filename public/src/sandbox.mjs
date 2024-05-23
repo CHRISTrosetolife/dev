0833c824723de86b33beb1b177838643a393215c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 08:04:01 -0400

    2024-05-23T12:04:01.208Z  noop

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -6,7 +6,7 @@ import { list_length } from "./list_length.mjs";
 export async function sandbox() {
   let words = await ceb_bible_words();
   let length = list_length(words);
-  each_index_async(words, async (word, index) => {
+  await each_index_async(words, async (word, index) => {
     await ceb_definition(word);
     log({
       word,
