48922c35519b4aa2e299c2758b17e148e8c572a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 22:13:59 -0400

    2024-05-23T02:13:58.892Z  function_auto_run sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,5 @@
+import { string_skip } from "./string_skip.mjs";
+import { string_index } from "./string_index.mjs";
 import { string_split_space } from "./string_split_space.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { string_whitespace_normalize } from "./string_whitespace_normalize.mjs";
@@ -8,8 +10,8 @@ export async function sandbox() {
   let url = "https://www.ccel.org/ccel/b/bible/ceb_p/cache/ceb_p.txt";
   let text = await http_cache(url);
   const verse_1 = "^1";
-  let index = text.indexOf(verse_1);
-  return index;
+  let index = string_index(text, verse_1);
+  text = string_skip(text, index);
   let replace = "_,;:!?.'\"()¶/`^|0123456789";
   let replace_list = string_split_empty(replace);
   const replacement = " ";
---
+++ public/src/string_index.mjs
@@ -0,0 +1,3 @@
+export function string_index(text, verse_1) {
+  return text.indexOf(verse_1);
+}
