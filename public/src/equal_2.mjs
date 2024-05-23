bb6f87350de0b1a85e1987ce26efeb719260e136
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 15:20:09 -0400

    2024-05-23T19:20:09.024Z  function_auto list_length_2

--- public/src/equal_2.mjs
+++ public/src/equal_2.mjs
@@ -1,4 +1,4 @@
 import { equal } from "./equal.mjs";
 export function equal_2(length) {
-  return equal(length, 1);
+  return equal(length, 2);
 }
--- public/src/list_length_2.mjs
+++ public/src/list_length_2.mjs
@@ -1,3 +1,4 @@
+import { equal_2 } from "./equal_2.mjs";
 import { list_length } from "./list_length.mjs";
 export function list_length_2(list) {
   let length = list_length(list);
