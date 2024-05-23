ea0d90f5056ff900b4fc046f910cf3acdac218d0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 17:57:22 -0400

    2024-05-23T21:57:21.781Z  function_auto file_overwrite

--- public/src/file_overwrite.mjs
+++ public/src/file_overwrite.mjs
@@ -1,5 +1,4 @@
 import { file_overwrite_generic } from "./file_overwrite_generic.mjs";
 export async function file_overwrite(file_name, data) {
-  const encoding = "utf-8";
-  return await file_overwrite_generic(file_name, data, encoding);
+  return await file_overwrite_generic(file_name, data, "utf-8");
 }
