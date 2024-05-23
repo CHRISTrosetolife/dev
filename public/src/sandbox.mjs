49cf597870cce9b9224bf65ca9a91ad662d3022c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 15:57:31 -0400

    2024-05-23T19:57:31.112Z  sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -6,12 +6,12 @@ import { ceb_definition } from "./ceb_definition.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export async function sandbox() {
-  if (10) return await ceb_definition("kamo");
+  if (0) return await ceb_definition("kamo");
   let t = await ceb_bible_words_top(75);
   t = list_skip(t, 32);
   let lookup = {};
   await each_index_async(t, async (bible_word, index) => {
-    if (0)
+    if (10)
       log({
         bible_word,
         index,
