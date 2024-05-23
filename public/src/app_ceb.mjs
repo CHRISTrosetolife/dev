0687b56ef791018282eacc8a5b85d038c07481ff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 17:19:35 -0400

    2024-05-23T21:19:34.765Z  function_auto app_ceb

--- public/src/app_ceb.mjs
+++ public/src/app_ceb.mjs
@@ -1,6 +1,9 @@
+import { log } from "./log.mjs";
 import { http_storage } from "./http_storage.mjs";
 import { ceb_group_path } from "./ceb_group_path.mjs";
-export function app_ceb() {
-  ceb_group_path(group_index);
-  http_storage;
+export async function app_ceb() {
+  let group = await http_storage(ceb_group_path(group_index));
+  log({
+    group,
+  });
 }
