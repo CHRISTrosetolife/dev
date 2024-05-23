35d2676149ab392e0195d91422394e7c974b6b47
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 22:41:53 -0400

    2024-05-23T02:41:53.138Z  function_auto_run sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,34 +1,4 @@
-import { string_count } from "./string_count.mjs";
-import { list_map_property } from "./list_map_property.mjs";
-import { string_take } from "./string_take.mjs";
-import { string_trim } from "./string_trim.mjs";
-import { string_skip } from "./string_skip.mjs";
-import { string_index } from "./string_index.mjs";
-import { string_split_space } from "./string_split_space.mjs";
-import { string_case_lower } from "./string_case_lower.mjs";
-import { string_whitespace_normalize } from "./string_whitespace_normalize.mjs";
-import { string_replace_multiple } from "./string_replace_multiple.mjs";
-import { string_split_empty } from "./string_split_empty.mjs";
-import { http_cache } from "./http_cache.mjs";
+import { ceb_bible_words } from "./ceb_bible_words.mjs";
 export async function sandbox() {
-  let url = "https://www.ccel.org/ccel/b/bible/ceb_p/cache/ceb_p.txt";
-  let text = await http_cache(url);
-  const verse_1 = "^1";
-  let index = string_index(text, verse_1);
-  text = string_skip(text, index);
-  let end = "This document is from the Christian Classics Ethereal";
-  let index_end = string_index(text, end);
-  text = string_take(text, index_end);
-  let replace = "_,;:!?.'\"()¶/`^|0123456789";
-  let replace_list = string_split_empty(replace);
-  const replacement = " ";
-  text = string_replace_multiple(text, replace_list, replacement);
-  text = string_whitespace_normalize(text);
-  text = string_case_lower(text);
-  text = string_trim(text);
-  let text_split = string_split_space(text);
-  let list = string_count(text_split);
-  const property_name = "word";
-  let mapped = list_map_property(list, property_name);
-  return mapped;
+  return await ceb_bible_words();
 }
