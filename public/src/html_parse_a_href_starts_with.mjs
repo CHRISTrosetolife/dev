f8ab92cd11c9a6764cbffdccfadacb7ac1434d85
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 08:07:23 -0400

    2024-05-23T12:07:23.771Z  function_auto html_parse_a_href_starts_with

--- public/src/html_parse_a_href_starts_with.mjs
+++ public/src/html_parse_a_href_starts_with.mjs
@@ -1,16 +1,5 @@
-import { log } from "./log.mjs";
-import { string_trim } from "./string_trim.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { list_map } from "./list_map.mjs";
-import { string_starts_with } from "./string_starts_with.mjs";
-import { list_filter } from "./list_filter.mjs";
+import { html_parse_a_href_starts_with_text } from "./html_parse_a_href_starts_with_text.mjs";
 export function html_parse_a_href_starts_with(parsed, prefix) {
-  let q = parsed.querySelectorAll("a[href]");
-  let filtered = list_filter(q, (i) => {
-    let href = i.getAttribute("href");
-    return string_starts_with(href, prefix);
-  });
-  let mapped = list_map(filtered, (i) => object_property_get(i, "text"));
-  let mapped3 = list_map(mapped, (i) => string_trim(i));
+  let mapped3 = html_parse_a_href_starts_with_text(parsed, prefix);
   return mapped3;
 }
---
+++ public/src/html_parse_a_href_starts_with_text.mjs
@@ -0,0 +1,15 @@
+import { string_trim } from "./string_trim.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { list_map } from "./list_map.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
+import { list_filter } from "./list_filter.mjs";
+export function html_parse_a_href_starts_with_text(parsed, prefix) {
+  let q = parsed.querySelectorAll("a[href]");
+  let filtered = list_filter(q, (i) => {
+    let href = i.getAttribute("href");
+    return string_starts_with(href, prefix);
+  });
+  let mapped = list_map(filtered, (i) => object_property_get(i, "text"));
+  let mapped3 = list_map(mapped, (i) => string_trim(i));
+  return mapped3;
+}
