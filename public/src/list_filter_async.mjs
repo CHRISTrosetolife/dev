72e2cc206286f69a5ca963d1ace25932f2806f12
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 14:03:09 -0400

    2024-05-23T18:03:09.112Z  function_auto list_filter_async

--- public/src/list_filter_async.mjs
+++ public/src/list_filter_async.mjs
@@ -1,4 +1,6 @@
 import { list_adder_async } from "./list_adder_async.mjs";
 export async function list_filter_async(list, filter) {
-  return await list_adder_async(async (la) => {});
+  return await list_adder_async(async (la) => {
+    each_async(list, async (l) => {});
+  });
 }
