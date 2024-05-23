094f9fa8b613f909afbe80faabbb397fcc20c9ee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 15:35:15 -0400

    2024-05-23T19:35:15.315Z  function_auto_run sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,4 @@
-import { ceb_bible_words_100 } from "./ceb_bible_words_100.mjs";
+import { ceb_bible_words_top } from "./ceb_bible_words_top.mjs";
 import { log } from "./log.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { ceb_definition } from "./ceb_definition.mjs";
@@ -6,7 +6,7 @@ import { object_property_set } from "./object_property_set.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export async function sandbox() {
   if (0) return await ceb_definition("ila");
-  let t = await ceb_bible_words_100();
+  let t = await ceb_bible_words_top(250);
   for (let w of t) {
     log(w);
   }
