241169da8932b1e856812768ab2ffa6a2249bd7b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 16:20:27 -0400

    2024-05-23T20:20:27.613Z  function_auto ceb_definition

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -6,11 +6,14 @@ import { ceb_definition } from "./ceb_definition.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { list_take } from "./list_take.mjs";
+import { object_property_exists_not } from "./object_property_exists_not.mjs";
+import { list_add } from "./list_add.mjs";
 export async function sandbox() {
   if (0) return await ceb_definition("kamo");
   let t = await ceb_bible_words_top(75);
   t = list_skip(t, 32);
-  let lookup = {};
+  let existing = {};
+  let result = [];
   await each_index_async(t, async (bible_word, index) => {
     if (0)
       log({
@@ -18,8 +21,12 @@ export async function sandbox() {
         index,
       });
     let { word, definitions } = await ceb_definition(bible_word);
-    if (list_empty_not_is(definitions)) {
-      object_property_set(lookup, word, definitions);
+    if (object_property_exists_not(existing, word)) {
+
+        object_property_set(existing, word, true);
+        if (list_empty_not_is(definitions)) {
+list_add(result, {word,definitions})
+        }
     }
     if(0)
     log({word,definitions})
