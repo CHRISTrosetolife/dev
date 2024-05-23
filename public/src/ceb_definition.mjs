6a4f5c188b6ef989d4e8efc2a6460c4a281b74e6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 08:06:40 -0400

    2024-05-23T12:06:39.994Z  function_auto ceb_definition

--- public/src/ceb_definition.mjs
+++ public/src/ceb_definition.mjs
@@ -2,6 +2,7 @@ import { html_cache_parse } from "./html_cache_parse.mjs";
 import { html_parse_a_href_starts_with } from "./html_parse_a_href_starts_with.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { list_unique } from "./list_unique.mjs";
+import { string_combine } from "./string_combine.mjs";
 export async function ceb_definition(word) {
   let url = string_combine_multiple([
     "https://www.binisaya.com/node/21?search=binisaya&word=",
@@ -9,7 +10,9 @@ export async function ceb_definition(word) {
     "&Search=Search",
   ]);
   let parsed = await html_cache_parse(url);
-  let prefix = "http://www.binisaya.com/english/";
+  let prefix = "http://www.binisaya.com/";
+  let prefix_2 = string_combine(prefix, "cebuano/");
+  let prefix_1 = string_combine(prefix, "english/");
   let mapped3 = html_parse_a_href_starts_with(parsed, prefix);
   let u = list_unique(mapped3);
   return u;
