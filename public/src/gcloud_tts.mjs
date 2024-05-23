2a8e0f0aa6fa06424a1bbf2840fac82eed3a6c32
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 17:54:27 -0400

    2024-05-23T21:54:27.011Z  function_auto gcloud_tts

--- public/src/gcloud_tts.mjs
+++ public/src/gcloud_tts.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import textToSpeech from "@google-cloud/text-to-speech";
 export async function gcloud_tts(language_code, text) {
   const client = new textToSpeech.TextToSpeechClient();
@@ -17,5 +16,4 @@ export async function gcloud_tts(language_code, text) {
   const [response] = await client.synthesizeSpeech(request);
   const writeFile = util.promisify(fs.writeFile);
   await writeFile("output.mp3", response.audioContent, "binary");
-  console.log("Audio content written to file: output.mp3");
 }
