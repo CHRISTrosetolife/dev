da7ae0f39255165bbd86b4a114915c92af5b3218
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 17:12:49 -0400

    2024-05-23T21:12:49.601Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,7 +1,6 @@
 import { storage_upload_object } from "./storage_upload_object.mjs";
 import { ceb_bible_words_definitions_atoms } from "./ceb_bible_words_definitions_atoms.mjs";
 import { ceb_definition } from "./ceb_definition.mjs";
-import { list_length } from "./list_length.mjs";
 import { list_take } from "./list_take.mjs";
 export async function sandbox() {
   if (0) return await ceb_definition("kamo");
@@ -10,9 +9,6 @@ export async function sandbox() {
   let group_count = 8;
   let atoms = await ceb_bible_words_definitions_atoms(skip, limit);
   let group = list_take(atoms, group_count);
-  storage_upload_object;
-  return {
-    atoms,
-    c: list_length(atoms),
-  };
+  await storage_upload_object(group, "/ceb/group/1.json");
+  return group;
 }
