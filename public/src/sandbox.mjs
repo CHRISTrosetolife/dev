fb9ea61c4e8b5e326b5a7152249823757e2738cc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 15:46:26 -0400

    2024-05-23T19:46:25.695Z  function_auto_run sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { list_skip } from "./list_skip.mjs";
 import { ceb_bible_words_top } from "./ceb_bible_words_top.mjs";
 import { log } from "./log.mjs";
 import { each_index_async } from "./each_index_async.mjs";
@@ -6,11 +7,8 @@ import { object_property_set } from "./object_property_set.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export async function sandbox() {
   if (0) return await ceb_definition("ila");
-  let t = await ceb_bible_words_top(250);
-  for (let w of t) {
-    log(w);
-  }
-  return;
+  let t = await ceb_bible_words_top(75);
+  t = list_skip(t, 32);
   let lookup = {};
   await each_index_async(t, async (bible_word, index) => {
     if (0)
