0878bb6dfa310bfa707517adde9b71f588d0b8d3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 22:02:08 -0400

    2024-05-23T02:02:08.733Z  function_auto_run sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,5 @@
+import { identity } from "./identity.mjs";
+import { list_sort_string } from "./list_sort_string.mjs";
 import { list_join_empty } from "./list_join_empty.mjs";
 import { string_symbols } from "./string_symbols.mjs";
 import { http_cache } from "./http_cache.mjs";
@@ -5,5 +7,7 @@ export async function sandbox() {
   let url = "https://www.ccel.org/ccel/b/bible/ceb_p/cache/ceb_p.txt";
   let text = await http_cache(url);
   let symbols = string_symbols(text);
+  list_sort_string(symbols, identity);
   let joined = list_join_empty(symbols);
+  return joined;
 }
