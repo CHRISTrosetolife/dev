bd15e208681fcfb1c98fa54f769af8d42ff0ad0c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 17:52:37 -0400

    2024-05-23T21:52:35.752Z  function_auto gcloud_tts

--- public/src/gcloud_tts.mjs
+++ public/src/gcloud_tts.mjs
@@ -1,1 +1,20 @@
-export function gcloud_tts() {}
+import { log } from "./log.mjs";
+export async function gcloud_tts() {
+  const text = "hello, world!";
+  const request = {
+    input: {
+      text: text,
+    },
+    voice: {
+      languageCode: "en-US",
+      ssmlGender: "NEUTRAL",
+    },
+    audioConfig: {
+      audioEncoding: "MP3",
+    },
+  };
+  const [response] = await client.synthesizeSpeech(request);
+  const writeFile = util.promisify(fs.writeFile);
+  await writeFile("output.mp3", response.audioContent, "binary");
+  console.log("Audio content written to file: output.mp3");
+}
