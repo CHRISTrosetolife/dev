1e8d83f12aaa9a78a76a4c1e9cf80d9e99a0748a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 22:00:42 -0400

    2024-05-23T02:00:42.448Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,11 +1,10 @@
+import { each } from "./each.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 import { http_cache } from "./http_cache.mjs";
 export async function sandbox() {
   let url = "https://www.ccel.org/ccel/b/bible/ceb_p/cache/ceb_p.txt";
   let text = await http_cache(url);
   let symbols = list_adder_unique((la) => {
-    for (let c of text) {
-      la(c);
-    }
+    each(text, la);
   });
 }
