cc456ffb378781035d8dcdaed65af129503d6117
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 14:11:22 -0400

    2024-05-23T18:11:22.358Z  sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -5,8 +5,8 @@ import { ceb_definition } from "./ceb_definition.mjs";
 import { ceb_bible_words } from "./ceb_bible_words.mjs";
 import { list_length } from "./list_length.mjs";
 export async function sandbox() {
-    if (false)
-    return await ceb_definition('sa')
+    if (true)
+    return await ceb_definition('jehova')
   let words = await ceb_bible_words();
   let length = list_length(words);
   let t = list_take(words, 32);
