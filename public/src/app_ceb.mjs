beaec65a64b06d833e9da4d8a29c0cbd42659fd5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 17:19:12 -0400

    2024-05-23T21:19:11.757Z  function_auto app_ceb

--- public/src/app_ceb.mjs
+++ public/src/app_ceb.mjs
@@ -1,4 +1,6 @@
+import { http_storage } from "./http_storage.mjs";
 import { ceb_group_path } from "./ceb_group_path.mjs";
 export function app_ceb() {
   ceb_group_path(group_index);
+  http_storage;
 }
