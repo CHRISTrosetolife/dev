1651e9cddb6a314659c8d8ddd023badd99f15112
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 19:35:03 -0400

    2024-05-23T23:35:03.585Z  function_auto html_audio

--- public/src/html_audio.mjs
+++ public/src/html_audio.mjs
@@ -1,10 +1,3 @@
-import { object_property_set } from "./object_property_set.mjs";
-import { string_combine } from "./string_combine.mjs";
-import { html_element } from "./html_element.mjs";
-export function html_audio(parent, img_name) {
-  let i1 = html_element(parent, "img");
-  let img_path = string_combine("./img/", img_name);
-  let { element } = i1;
-  object_property_set(element, "src", img_path);
-  return i1;
+export function html_audio(src) {
+  return new Audio(src).play();
 }
