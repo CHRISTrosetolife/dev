6a815cf8d5b52f20d25fce655dc925d08b37497a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 14:04:01 -0400

    2024-05-23T18:04:00.949Z  function_auto each_async

--- public/src/each_async.mjs
+++ public/src/each_async.mjs
@@ -1,7 +1,5 @@
 export async function each_async(list, lambda) {
-  let index = 0;
   for (let element of list) {
-    await lambda(element, index);
-    index++;
+    await lambda(element);
   }
 }
