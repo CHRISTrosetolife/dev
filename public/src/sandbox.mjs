fbae6b4384511829bb7694e05333299492bc9e38
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 16:25:57 -0400

    2024-05-23T20:25:57.656Z  function_auto_run sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -6,7 +6,7 @@ import { object_property_get } from "./object_property_get.mjs";
 export async function sandbox() {
   if (0) return await ceb_definition("kamo");
   let limit = 75;
-  let skip = 32;
+  let skip = 0;
   let ds = await ceb_bible_words_definitions(skip, limit);
   let pairs = list_adder((la) =>
     each(ds, (w) =>
