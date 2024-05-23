e37ec10370fd65970772cdad55c24f00fbe97d07
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 22:06:15 -0400

    2024-05-23T02:06:15.073Z  function_auto_run sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,5 @@
+import { string_replace_multiple } from "./string_replace_multiple.mjs";
 import { string_split_empty } from "./string_split_empty.mjs";
-import { string_replace } from "./string_replace.mjs";
 import { identity } from "./identity.mjs";
 import { list_sort_string } from "./list_sort_string.mjs";
 import { list_join_empty } from "./list_join_empty.mjs";
@@ -13,6 +13,7 @@ export async function sandbox() {
   let joined = list_join_empty(symbols);
   let replace = "_,;:!?.'\"()¶/`^|0123456789";
   let replace_list = string_split_empty(replace);
-  let replaced = string_replace();
-  return joined;
+  const replacement = " ";
+  text = string_replace_multiple(text, replace_list, replacement);
+  return text;
 }
