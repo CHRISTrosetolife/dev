01d588c65e25d8bb0b76d7e2b2e11b265f636068
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 17:29:09 -0400

    2024-05-23T21:29:08.710Z  function_auto_run sandbox

--- public/src/app_ceb.mjs
+++ public/src/app_ceb.mjs
@@ -1,10 +1,10 @@
 import { log } from "./log.mjs";
 import { http_storage } from "./http_storage.mjs";
 import { ceb_group_path } from "./ceb_group_path.mjs";
+import { list_first } from "./list_first.mjs";
 export async function app_ceb() {
   let group_index = 0;
   let group = await http_storage(ceb_group_path(group_index));
-  log({
-    group,
-  });
+  let atom = list_first(group);
+  let copy=list_copy(atom);list_scramble(copy);log({copy})
 }
