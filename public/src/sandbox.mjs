7a00e91423756bc9f744dec7f90e7beee4181a46
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 22:00:54 -0400

    2024-05-23T02:00:54.386Z  function_auto_run sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -4,7 +4,5 @@ import { http_cache } from "./http_cache.mjs";
 export async function sandbox() {
   let url = "https://www.ccel.org/ccel/b/bible/ceb_p/cache/ceb_p.txt";
   let text = await http_cache(url);
-  let symbols = list_adder_unique((la) => {
-    each(text, la);
-  });
+  let symbols = list_adder_unique((la) => each(text, la));
 }
