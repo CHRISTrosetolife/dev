aa2bdaa92213a4548127ba388d485a24e77c6b0a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 08:10:05 -0400

    2024-05-23T12:10:04.897Z  function_auto ceb_definition

--- public/src/ceb_definition.mjs
+++ public/src/ceb_definition.mjs
@@ -1,7 +1,6 @@
 import { html_parse_a_href_starts_with_text } from "./html_parse_a_href_starts_with_text.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { list_unique } from "./list_unique.mjs";
 import { string_combine } from "./string_combine.mjs";
 export async function ceb_definition(word) {
   let url = string_combine_multiple([
@@ -13,7 +12,6 @@ export async function ceb_definition(word) {
   let prefix = "http://www.binisaya.com/";
   let prefix_1 = string_combine(prefix, "cebuano/");
   let prefix_2 = string_combine(prefix, "english/");
-  let mapped3 = html_parse_a_href_starts_with_text(parsed, prefix);
-  let u = list_unique(mapped3);
-  return u;
+  let mapped3 = html_parse_a_href_starts_with_text(parsed, prefix_1);
+  return mapped3;
 }
