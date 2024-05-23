2a2190f5ef694170142b268cd99015951d5412a2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-22 21:59:45 -0400

    2024-05-23T01:59:45.780Z  function_search symbol

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,5 @@
 import { http_cache } from "./http_cache.mjs";
 export async function sandbox() {
   let url = "https://www.ccel.org/ccel/b/bible/ceb_p/cache/ceb_p.txt";
-  return await http_cache(url);
+  let text = await http_cache(url);return list_adder_unique(la=>{for (let c of text) {la(c)}})
 }
