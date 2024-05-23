0566c95c85c5c678f6a941da625d205de8ea32ec
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 17:32:20 -0400

    2024-05-23T21:32:19.415Z  function_auto app_ceb

--- public/src/app_ceb.mjs
+++ public/src/app_ceb.mjs
@@ -1,3 +1,4 @@
+import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { html_buttons_next_previous } from "./html_buttons_next_previous.mjs";
 import { list_scramble } from "./list_scramble.mjs";
 import { list_copy } from "./list_copy.mjs";
@@ -5,6 +6,7 @@ import { http_storage } from "./http_storage.mjs";
 import { ceb_group_path } from "./ceb_group_path.mjs";
 import { list_first } from "./list_first.mjs";
 export async function app_ceb() {
+  let root = html_style_default_initialize();
   let group_index = 0;
   let group = await http_storage(ceb_group_path(group_index));
   let atom = list_first(group);
