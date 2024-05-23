1383b493ae5dfa31ae8c10eb3b5b5a4fa5cd246b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 14:03:24 -0400

    2024-05-23T18:03:24.745Z  function_auto list_filter_async

--- public/src/list_filter_async.mjs
+++ public/src/list_filter_async.mjs
@@ -1,6 +1,10 @@
 import { list_adder_async } from "./list_adder_async.mjs";
 export async function list_filter_async(list, filter) {
   return await list_adder_async(async (la) => {
-    each_async(list, async (l) => {});
+    each_async(list, async (item) => {
+      if (await filter(item)) {
+        la(item);
+      }
+    });
   });
 }
