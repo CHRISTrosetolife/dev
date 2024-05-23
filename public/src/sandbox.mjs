a08533bea033a12514ee8a79803cafdb964e8043
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 16:23:04 -0400

    2024-05-23T20:23:04.236Z  function_auto_run sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,42 +1,8 @@
-import { list_skip } from "./list_skip.mjs";
-import { ceb_bible_words_top } from "./ceb_bible_words_top.mjs";
-import { log } from "./log.mjs";
-import { each_index_async } from "./each_index_async.mjs";
+import { ceb_bible_words_definitions } from "./ceb_bible_words_definitions.mjs";
 import { ceb_definition } from "./ceb_definition.mjs";
-import { object_property_set } from "./object_property_set.mjs";
-import { list_empty_not_is } from "./list_empty_not_is.mjs";
-import { object_property_exists_not } from "./object_property_exists_not.mjs";
-import { list_add } from "./list_add.mjs";
 export async function sandbox() {
   if (0) return await ceb_definition("kamo");
   let limit = 75;
   let skip = 32;
-  let t = await ceb_bible_words_top(limit);
-  t = list_skip(t, skip);
-  let existing = {};
-  let result = [];
-  await each_index_async(t, async (bible_word, index) => {
-    if (0)
-      log({
-        bible_word,
-        index,
-      });
-    let { word, definitions } = await ceb_definition(bible_word);
-    if (object_property_exists_not(existing, word)) {
-      object_property_set(existing, word, true);
-      if (list_empty_not_is(definitions)) {
-        list_add(result, {
-          word,
-          definitions,
-        });
-      }
-    }
-    if (0)
-      log({
-        word,
-        definitions,
-      });
-  });
-  if (0) return;
-  return result;
+  return await ceb_bible_words_definitions(skip, limit);
 }
