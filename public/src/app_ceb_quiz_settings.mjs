61d03a94a62c84ba7153c4e6b2fe399267bcafd9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 06:35:37 -0400

    2024-05-24T10:35:37.538Z  function_auto app_ceb

--- public/src/app_ceb_quiz_settings.mjs
+++ public/src/app_ceb_quiz_settings.mjs
@@ -4,7 +4,7 @@ import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function app_ceb_quiz_settings(atom) {
   let chunk_sizes = [3, 2, 1, 1];
-  if (10) chunk_sizes = [1,1];
+  if (0) chunk_sizes = [1,1];
   let settings_choices = list_adder((la) =>
     each(chunk_sizes, (chunk_size) =>
       each([true, false], (forwards) => {
