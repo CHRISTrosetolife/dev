5a19ac953926c54869d6da95061fe268f4ea0adc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 05:55:46 -0400

    2024-05-24T09:55:45.941Z  function_auto_run sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -21,28 +21,32 @@ export async function sandbox() {
   let group_count = 8;
   let atoms = await ceb_bible_words_definitions_atoms(skip, limit);
   let group = list_take(atoms, group_count);
-  let atom = list_first(group);
-  let mapped = list_map(atom, list_first);
-  let language_code = ceb_audio_language_code();
-  let voices = ceb_audio_voices();
-  await each_async(mapped, async (text) => {
-    each_index_async(voices, async (voice, voice_index) => {
-      let file_path = ceb_audio_path(voice_index, text);
-      let output_path = folder_gitignore_path(file_path);
-      log(output_path);
-      let { created } = await gcloud_tts(
-        language_code,
-        voice,
-        text,
-        output_path,
-      );
-      if (created) {
-        await storage_upload_file(output_path, file_path);
-        log("uploaded");
-      }
+  let taken = list_take(group, 2);
+  await each_async(taken, async (atom) => {
+    log({
+      atom,
+    });
+    let mapped = list_map(atom, list_first);
+    let language_code = ceb_audio_language_code();
+    let voices = ceb_audio_voices();
+    await each_async(mapped, async (text) => {
+      each_index_async(voices, async (voice, voice_index) => {
+        let file_path = ceb_audio_path(voice_index, text);
+        let output_path = folder_gitignore_path(file_path);
+        log(output_path);
+        let { created } = await gcloud_tts(
+          language_code,
+          voice,
+          text,
+          output_path,
+        );
+        if (created) {
+          await storage_upload_file(output_path, file_path);
+          log("uploaded");
+        }
+      });
     });
   });
-  return mapped;
   let group_index = 0;
   if (0) await storage_upload_object(group, ceb_group_path(group_index));
   return group;
