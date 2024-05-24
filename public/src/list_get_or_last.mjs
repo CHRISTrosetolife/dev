cde7ed4efd13b6af4aaba981ac0e0ceb3c51a609
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 18:27:40 -0400

    2024-05-24T22:27:40.385Z  function_auto list_get_or_last

--- public/src/list_get_or_last.mjs
+++ public/src/list_get_or_last.mjs
@@ -2,12 +2,12 @@ import { list_get } from "./list_get.mjs";
 import { list_last } from "./list_last.mjs";
 import { list_index_last } from "./list_index_last.mjs";
 import { greater_than } from "./greater_than.mjs";
-export function list_get_or_last(right, group) {
+export function list_get_or_last(list, index) {
   let gr;
-  if (greater_than(right, list_index_last(group))) {
-    gr = list_last(group);
+  if (greater_than(index, list_index_last(list))) {
+    gr = list_last(list);
   } else {
-    gr = list_get(group, right);
+    gr = list_get(list, index);
   }
   return gr;
 }
