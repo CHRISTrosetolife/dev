2259b79b569d63b0f51d7cc4eae2b3d4b42544da
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 20:49:20 -0400

    2024-05-24T00:49:19.656Z  function_auto html_style_click_width_min

--- public/src/html_style_click_width_min.mjs
+++ public/src/html_style_click_width_min.mjs
@@ -1,7 +1,10 @@
-import { html_style_units } from "./html_style_units.mjs";
+import { html_style_default_font_size_value } from "./html_style_default_font_size_value.mjs";
+import { html_style_default_font_size_value_get } from "./html_style_default_font_size_value_get.mjs";
 import { html_style } from "./html_style.mjs";
 export function html_style_click_width_min(part) {
   html_style(part, {
-    "min-width": html_style_units(15),
+    "min-width": html_style_default_font_size_value_get(
+      html_style_default_font_size_value(),
+    ),
   });
 }
