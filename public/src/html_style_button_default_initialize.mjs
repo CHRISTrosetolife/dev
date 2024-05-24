33c386e300f53a80ab79c43e9a3a607f9563c26c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-23 21:11:13 -0400

    2024-05-24T01:11:13.006Z  function_auto html_style_button_default_initialize

--- public/src/html_style_button_default_initialize.mjs
+++ public/src/html_style_button_default_initialize.mjs
@@ -1,17 +1,11 @@
-import { html_style_margin_default_value } from "./html_style_margin_default_value.mjs";
+import { html_style_default_border_margin } from "./html_style_default_border_margin.mjs";
 import { html_style_button_default_background_color } from "./html_style_button_default_background_color.mjs";
-import { html_style_button_default_border } from "./html_style_button_default_border.mjs";
-import { app_learn_code_style_rounded_padded_style } from "./app_learn_code_style_rounded_padded_style.mjs";
 import { html_style_button_default } from "./html_style_button_default.mjs";
 import { object_merge } from "./object_merge.mjs";
 export function html_style_button_default_initialize() {
-  object_merge(
-    html_style_button_default(),
-    app_learn_code_style_rounded_padded_style(),
-  );
-  object_merge(html_style_button_default(), html_style_margin_default_value());
+  let d = html_style_button_default();
+  html_style_default_border_margin(d);
   object_merge(html_style_button_default(), {
     "background-color": html_style_button_default_background_color(),
   });
-  object_merge(html_style_button_default(), html_style_button_default_border());
 }
---
+++ public/src/html_style_default_border_margin.mjs
@@ -0,0 +1,9 @@
+import { html_style_button_default_border } from "./html_style_button_default_border.mjs";
+import { html_style_margin_default_value } from "./html_style_margin_default_value.mjs";
+import { app_learn_code_style_rounded_padded_style } from "./app_learn_code_style_rounded_padded_style.mjs";
+import { object_merge } from "./object_merge.mjs";
+export function html_style_default_border_margin(d) {
+  object_merge(d, app_learn_code_style_rounded_padded_style());
+  object_merge(d, html_style_margin_default_value());
+  object_merge(d, html_style_button_default_border());
+}
