88a7ea7836eaae1f6d3c59aa8e43ba9c403e648f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 14:52:24 -0400

    2024-05-24T18:52:24.589Z  function_auto list_scramble

--- public/src/list_scramble.mjs
+++ public/src/list_scramble.mjs
@@ -1,13 +1,12 @@
 import { list_set } from "./list_set.mjs";
 import { list_get } from "./list_get.mjs";
 import { integer_random } from "./integer_random.mjs";
-import { list_length } from "./list_length.mjs";
-import { range } from "./range.mjs";
 import { list_index_last } from "./list_index_last.mjs";
 export function list_scramble(scrambled) {
-  let mapped_index_last = list_index_last(scrambled);
-  for (let i of range(list_length(scrambled))) {
-    let j = integer_random(i, mapped_index_last);
+  let low = 0;
+  let high = list_index_last(scrambled);
+  for (let i = low; i <= high; i++) {
+    let j = integer_random(i, high);
     let temp = list_get(scrambled, j);
     list_set(scrambled, j, list_get(scrambled, i));
     list_set(scrambled, i, temp);
