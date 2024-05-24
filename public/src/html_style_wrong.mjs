4dfaf9656324339d3b536ba7e7d498447b9166d7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 15:13:34 -0400

    2024-05-24T19:13:33.938Z  function_auto html_style_wrong

--- public/src/html_style_wrong.mjs
+++ public/src/html_style_wrong.mjs
@@ -1,8 +1,10 @@
 import { html_style_background_color } from "./html_style_background_color.mjs";
 import { html_style } from "./html_style.mjs";
-export function html_style_wrong(keyboard_button) {
-  html_style(keyboard_button, {
+import { html_style_font_color } from "./html_style_font_color.mjs";
+export function html_style_wrong(button) {
+  html_style(button, {
     "border-color": "red",
   });
-  html_style_background_color(keyboard_button, "salmon");
+  html_style_background_color(button, "salmon");
+  html_style_font_color(button, "white");
 }
