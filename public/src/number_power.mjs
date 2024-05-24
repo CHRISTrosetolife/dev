3d6fb0f10000a7995cd84ac8c01e1a5a514086c2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 17:48:07 -0400

    2024-05-24T21:48:07.693Z  function_auto number_power_2

---
+++ public/src/number_power.mjs
@@ -0,0 +1,3 @@
+export function number_power(newLocal, filtered_length) {
+  return Math.pow(newLocal, filtered_length);
+}
--- public/src/number_power_2.mjs
+++ public/src/number_power_2.mjs
@@ -1,3 +1,5 @@
+import { number_power } from "./number_power.mjs";
 export function number_power_2(filtered_length) {
-  return Math.pow(2, filtered_length);
+  const newLocal = 2;
+  return number_power(newLocal, filtered_length);
 }
