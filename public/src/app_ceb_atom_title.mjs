f43dcc5188cb9e5a2fb220aa18084bb090ca7ac9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 17:41:44 -0400

    2024-05-24T21:41:44.480Z  function_auto app_ceb_atom_title

--- public/src/app_ceb_atom_title.mjs
+++ public/src/app_ceb_atom_title.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { list_last } from "./list_last.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_join_colon_spaces } from "./list_join_colon_spaces.mjs";
@@ -6,10 +5,6 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_delimit_backtick } from "./string_delimit_backtick.mjs";
 import { list_map_nested } from "./list_map_nested.mjs";
 export function app_ceb_atom_title(atom_left, atom_right) {
-  log({
-    atom_left,
-    atom_right,
-  });
   let l = list_first(atom_left);
   let r = list_last(atom_right);
   let mapped = list_map_nested([l, r], string_delimit_backtick);
