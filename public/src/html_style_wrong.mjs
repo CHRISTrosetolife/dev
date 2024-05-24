4203d8660081177c0080afaedee4a9bb92582a70
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 15:13:37 -0400

    2024-05-24T19:13:37.621Z  function_auto html_style_wrong

--- public/src/html_style_wrong.mjs
+++ public/src/html_style_wrong.mjs
@@ -1,10 +1,8 @@
 import { html_style_background_color } from "./html_style_background_color.mjs";
 import { html_style } from "./html_style.mjs";
-import { html_style_font_color } from "./html_style_font_color.mjs";
 export function html_style_wrong(button) {
   html_style(button, {
     "border-color": "red",
   });
   html_style_background_color(button, "salmon");
-  html_style_font_color(button, "white");
 }
