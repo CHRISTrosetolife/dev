8e173794a6633b19fe9ca2eff4bdab7b4e3d3838
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 05:20:37 -0400

    2024-05-24T09:20:37.639Z  function_auto app_ceb

--- public/src/app_ceb.mjs
+++ public/src/app_ceb.mjs
@@ -1,7 +1,6 @@
 import { range_list } from "./range_list.mjs";
 import { app_ceb_word_english } from "./app_ceb_word_english.mjs";
 import { list_after } from "./list_after.mjs";
-import { log } from "./log.mjs";
 import { number_is } from "./number_is.mjs";
 import { list_random_item } from "./list_random_item.mjs";
 import { html_element_style } from "./html_element_style.mjs";
@@ -83,20 +82,23 @@ export async function app_ceb() {
     let pairs_other = list_copy(atom);
     list_remove(pairs_other, pair);
     let [cebuano, english] = pair;
+    let answer;
     if (forwards) {
       app_ceb_word_button(root, cebuano);
+      answer = english;
     } else {
       app_ceb_word_english(root, english);
+      answer = cebuano;
     }
-    let answer = html_p_text(root, "?");
-    let style = html_element_style(answer);
+    let answer_element = html_p_text(root, "?");
+    let style = html_element_style(answer_element);
     html_style_default_border_margin(style);
-    html_style_width_full(answer);
-    html_style(answer, {
+    html_style_width_full(answer_element);
+    html_style(answer_element, {
       "border-color": "lightblue",
     });
     let index = 0;
-    let correct_choices = string_chunk(english, chunk_size);
+    let correct_choices = string_chunk(answer, chunk_size);
     let pair_other = list_random_item(pairs_other);
     let english_other = list_second(pair_other);
     let other_choices = string_chunk(english_other, chunk_size);
@@ -110,33 +112,26 @@ export async function app_ceb() {
           index = add_1(index);
           const last_is = greater_than_equal(
             multiply(index, chunk_size),
-            string_length(english),
+            string_length(answer),
           );
           let last = last_is ? "" : "?";
           const take_count = number_min(
             multiply(index, chunk_size),
-            string_length(english),
+            string_length(answer),
           );
-          log({
-            index,
-            chunk_size,
-            english,
-            last,
-            take_count,
-          });
           html_inner_set(
-            answer,
-            string_combine(string_take(english, take_count), last),
+            answer_element,
+            string_combine(string_take(answer, take_count), last),
           );
           if (last_is) {
-            app_learn_code_style_success(answer);
+            app_learn_code_style_success(answer_element);
             html_style_display_none(button);
           }
           app_learn_code_style_success(button);
           app_learn_code_correct_timeout(() => {
             html_style_display_none(button);
             if (last_is) {
-              app_learn_code_style_success(answer);
+              app_learn_code_style_success(answer_element);
               let after = list_after(settings_choices, settings);
               refresh_quiz(after);
             }
