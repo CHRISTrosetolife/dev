145f9bdfdb275c4d8bdaddbbb99230c22e3d0484
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 15:10:01 -0400

    2024-05-24T19:10:00.823Z  function_auto html_button

--- public/src/html_button.mjs
+++ public/src/html_button.mjs
@@ -1,8 +1,7 @@
-import { html_style_button_default_value } from "./html_style_button_default_value.mjs";
-import { html_style } from "./html_style.mjs";
+import { html_style_button_default } from "./html_style_button_default.mjs";
 import { html_element } from "./html_element.mjs";
 export function html_button(root) {
   let result = html_element(root, "button");
-  html_style(result, html_style_button_default_value());
+  html_style_button_default(result);
   return result;
 }
--- public/src/html_style_button_default.mjs
+++ public/src/html_style_button_default.mjs
@@ -1,4 +1,5 @@
-let d = {};
-export function html_style_button_default_value() {
-  return d;
+import { html_style_button_default_value } from "./html_style_button_default_value.mjs";
+import { html_style } from "./html_style.mjs";
+export function html_style_button_default(result) {
+  html_style(result, html_style_button_default_value());
 }
