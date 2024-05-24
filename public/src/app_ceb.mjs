90a97424d3752240ecc4808c5cdfca5db20d7950
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 15:07:02 -0400

    2024-05-24T19:07:02.782Z  function_auto app_memorize

--- public/src/app_ceb.mjs
+++ public/src/app_ceb.mjs
@@ -162,7 +162,7 @@ export async function app_ceb() {
           });
         } else {
           list_add(settings_choices, object_copy_shallow(settings));
-          button
+          html_style_wrong(button);
         }
       });
       html_style_click_width_min(button);
