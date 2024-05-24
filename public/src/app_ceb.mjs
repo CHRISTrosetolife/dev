2dd1b6ef3a23d4d7049f11c6504ada5f3f563bea
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 16:14:59 -0400

    2024-05-24T20:14:59.488Z  function_auto app_ceb

--- public/src/app_ceb.mjs
+++ public/src/app_ceb.mjs
@@ -85,7 +85,7 @@ export async function app_ceb() {
     html_clear_scroll_top(root);
     each_range(level_size, (i) => {
       let { left, right } = position;
-      divide(add_1(subtract(right, left)), 2);
+      divide(add_1(subtract(right, left)), level_size);
       let atom_i = list_get(group, i);
       let mapped = list_map_nested(atom_i, string_delimit_backtick);
       let position_text = string_combine_multiple([
