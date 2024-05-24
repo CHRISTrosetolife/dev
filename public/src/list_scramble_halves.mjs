dcb4a0507656d10e9095bc7bc2ca14124ea878fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 14:55:23 -0400

    2024-05-24T18:55:23.245Z  function_auto list_scramble_halves

--- public/src/list_scramble_halves.mjs
+++ public/src/list_scramble_halves.mjs
@@ -1,7 +1,10 @@
+import { floor } from "./floor.mjs";
 import { list_scramble_from } from "./list_scramble_from.mjs";
 import { list_index_last } from "./list_index_last.mjs";
+import { divide } from "./divide.mjs";
 export function list_scramble_halves(scrambled) {
   let low = 0;
   let high = list_index_last(scrambled);
+  let mid = floor(divide(high, 2));
   list_scramble_from(low, high, scrambled);
 }
