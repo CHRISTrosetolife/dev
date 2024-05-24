ad5901c6c5439fd1842907e6b4790c7e98643aa5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 05:43:39 -0400

    2024-05-24T09:43:39.100Z  function_auto html_audio

--- public/src/html_audio.mjs
+++ public/src/html_audio.mjs
@@ -1,11 +1,11 @@
 export async function html_audio(src) {
-    const a = new Audio(src);
-    let result = new Promise(resolve=>{
-        myAudio.addEventListener("ended", function(){
-            myAudio.currentTime = 0;
-            resolve()
-       });
-    })
+  const a = new Audio(src);
+  let result = new Promise((resolve) => {
+    myAudio.addEventListener("ended", function () {
+      myAudio.currentTime = 0;
+      resolve();
+    });
+  });
   a.play();
   return await result;
 }
