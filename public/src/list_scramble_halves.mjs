8349b3d7238805a407462eee65279fab2cba390d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 14:54:06 -0400

    2024-05-24T18:54:06.085Z  function_copy list_scramble list_scramble_halves

---
+++ public/src/list_scramble_halves.mjs
@@ -0,0 +1,7 @@
+import { list_scramble_from } from "./list_scramble_from.mjs";
+import { list_index_last } from "./list_index_last.mjs";
+export function list_scramble_halves(scrambled) {
+  let low = 0;
+  let high = list_index_last(scrambled);
+  list_scramble_from(low, high, scrambled);
+}
