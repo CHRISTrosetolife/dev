26a621d7cce4bda49d32c7b91fe57ff3641b6f05
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 20:41:48 -0400

    2024-05-25T00:41:48.034Z  function_auto function_auto_run_args

--- public/src/function_auto_run_args.mjs
+++ public/src/function_auto_run_args.mjs
@@ -1,6 +1,8 @@
 import { function_run } from "./function_run.mjs";
 import { function_auto } from "./function_auto.mjs";
-export async function function_auto_run_args(function_name) {
+import { string_split_comma } from "./string_split_comma.mjs";
+export async function function_auto_run_args(function_name, args_string) {
+  let args = string_split_comma(args_string);
   await function_auto(function_name);
-  return await function_run(function_name, []);
+  return await function_run(function_name, args);
 }
