60c9871becf542094e85c7b933ca66534aa29ee4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 21:06:56 -0400

    2024-05-25T01:06:56.577Z  function_auto_run gcloud_tts

--- public/src/gcloud_tts.mjs
+++ public/src/gcloud_tts.mjs
@@ -3,13 +3,16 @@ import { file_overwrite_generic } from "./file_overwrite_generic.mjs";
 import textToSpeech from "@google-cloud/text-to-speech";
 import { file_exists } from "./file_exists.mjs";
 export async function gcloud_tts(language_code, voice, text, output_path) {
+  const client = new textToSpeech.TextToSpeechClient();
+  const [result] = await client.listVoices({});
+  const voices = result.voices;
+  return;
   if (await file_exists(output_path)) {
     return {
       created: false,
     };
   }
   let { code, male } = language_code;
-  const client = new textToSpeech.TextToSpeechClient();
   log(textToSpeech.listVoices());
   const request = {
     input: {
