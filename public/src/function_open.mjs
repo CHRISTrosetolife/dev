26590cab61794513d8eadfdacddde2ebe01d303e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-25 12:28:21 -0400

    2024-05-25T16:28:20.647Z  function_auto function_open

--- public/src/function_open.mjs
+++ public/src/function_open.mjs
@@ -1,6 +1,9 @@
+import { assert_async } from "./assert_async.mjs";
+import { file_exists } from "./file_exists.mjs";
 import { file_open } from "./file_open.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function function_open(function_name) {
   const file_path = function_name_to_path(function_name);
+  await assert_async(file_exists, [file_path]);
   await file_open(file_path);
 }
