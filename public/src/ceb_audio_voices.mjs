02d85f261e8fc8cd522d5ce3188c800240cfa0da
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 21:03:20 -0400

    2024-05-25T01:03:19.744Z  function_auto ceb_audio_voices

--- public/src/ceb_audio_voices.mjs
+++ public/src/ceb_audio_voices.mjs
@@ -1,3 +1,12 @@
 export function ceb_audio_voices() {
-  return [{code:"Standard-D",male:true}, {code:"Standard-A",male:false}]};
+  return [
+    {
+      code: "Standard-D",
+      male: true,
+    },
+    {
+      code: "Standard-A",
+      male: false,
+    },
+  ];
 }
