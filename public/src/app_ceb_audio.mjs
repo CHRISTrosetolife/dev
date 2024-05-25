eb29c5e305b4b0df7e5f45e1bc7aa983a8ae000d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 20:50:24 -0400

    2024-05-25T00:50:23.931Z  function_auto app_ceb_audio

--- public/src/app_ceb_audio.mjs
+++ public/src/app_ceb_audio.mjs
@@ -1,7 +1,20 @@
+import { ceb_audio_voices } from "./ceb_audio_voices.mjs";
 import { storage_url } from "./storage_url.mjs";
 import { html_audio } from "./html_audio.mjs";
 import { ceb_audio_path } from "./ceb_audio_path.mjs";
+import { global_get } from "./global_get.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
+import { greater_than_equal } from "./greater_than_equal.mjs";
+import { subtract_1 } from "./subtract_1.mjs";
+import { list_length } from "./list_length.mjs";
 export async function app_ceb_audio(cebuano) {
-  let file_path = ceb_audio_path(0, cebuano);
+  let voices = ceb_audio_voices();
+  let length = list_length(voices);
+  let g = global_get();
+  let counts = object_property_initialize(g, app_ceb_audio.name, {});
+  let count = object_property_initialize(counts, cebuano, 0);
+  if (greater_than_equal(count, subtract_1(length))) {
+  }
+  let file_path = ceb_audio_path(index, cebuano);
   return await html_audio(storage_url(file_path));
 }
