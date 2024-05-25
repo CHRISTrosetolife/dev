4108e00102183574febb1200ec030154bf476573
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 21:23:19 -0400

    2024-05-25T01:23:18.842Z  function_auto sandbox

---
+++ public/src/ceb_audio_upload.mjs
@@ -0,0 +1,29 @@
+import { storage_upload_file } from "./storage_upload_file.mjs";
+import { gcloud_tts } from "./gcloud_tts.mjs";
+import { log } from "./log.mjs";
+import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
+import { ceb_audio_path } from "./ceb_audio_path.mjs";
+import { each_index_async } from "./each_index_async.mjs";
+import { ceb_audio_voices } from "./ceb_audio_voices.mjs";
+import { ceb_audio_language_code } from "./ceb_audio_language_code.mjs";
+export async function ceb_audio_upload(text) {
+  let language_code = ceb_audio_language_code();
+  let voices = ceb_audio_voices();
+  await each_index_async(voices, async (voice, voice_index) => {
+    let file_path = ceb_audio_path(voice_index, text);
+    let output_path = folder_gitignore_path(file_path);
+    log(output_path);
+    let { code, male } = voice;
+    let { created } = await gcloud_tts(
+      language_code,
+      code,
+      male ? "MALE" : "FEMALE",
+      text,
+      output_path,
+    );
+    if (created) {
+      await storage_upload_file(output_path, file_path);
+      log("uploaded");
+    }
+  });
+}
--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,9 +1,5 @@
+import { ceb_audio_upload } from "./ceb_audio_upload.mjs";
 import { ceb_group_size } from "./ceb_group_size.mjs";
-import { ceb_audio_path } from "./ceb_audio_path.mjs";
-import { ceb_audio_voices } from "./ceb_audio_voices.mjs";
-import { ceb_audio_language_code } from "./ceb_audio_language_code.mjs";
-import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
-import { gcloud_tts } from "./gcloud_tts.mjs";
 import { ceb_group_path } from "./ceb_group_path.mjs";
 import { storage_upload_object } from "./storage_upload_object.mjs";
 import { ceb_bible_words_definitions_atoms } from "./ceb_bible_words_definitions_atoms.mjs";
@@ -12,8 +8,6 @@ import { list_first } from "./list_first.mjs";
 import { list_map } from "./list_map.mjs";
 import { each_async } from "./each_async.mjs";
 import { log } from "./log.mjs";
-import { storage_upload_file } from "./storage_upload_file.mjs";
-import { each_index_async } from "./each_index_async.mjs";
 export async function sandbox() {
   let limit = 75;
   let skip = 0;
@@ -27,25 +21,7 @@ export async function sandbox() {
     });
     let mapped = list_map(atom, list_first);
     await each_async(mapped, async (text) => {
-      let language_code = ceb_audio_language_code();
-      let voices = ceb_audio_voices();
-      await each_index_async(voices, async (voice, voice_index) => {
-        let file_path = ceb_audio_path(voice_index, text);
-        let output_path = folder_gitignore_path(file_path);
-        log(output_path);
-        let { code, male } = voice;
-        let { created } = await gcloud_tts(
-          language_code,
-          code,
-          male ? "MALE" : "FEMALE",
-          text,
-          output_path,
-        );
-        if (created) {
-          await storage_upload_file(output_path, file_path);
-          log("uploaded");
-        }
-      });
+      await ceb_audio_upload(text);
     });
   });
   let group_index = 0;
