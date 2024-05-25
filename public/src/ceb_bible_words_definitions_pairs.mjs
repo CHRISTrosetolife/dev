c0a5e0f24562183c90432a3c6dac49845c160289
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-25 12:29:08 -0400

    2024-05-25T16:29:08.520Z  noop

--- public/src/ceb_bible_words_definitions_pairs.mjs
+++ public/src/ceb_bible_words_definitions_pairs.mjs
@@ -3,9 +3,9 @@ import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { ceb_bible_words_definitions } from "./ceb_bible_words_definitions.mjs";
 export async function ceb_bible_words_definitions_pairs(skip, limit) {
-  let ds = await ceb_bible_words_definitions(skip, limit);
+  let definitions = await ceb_bible_words_definitions(skip, limit);
   let pairs = list_adder((la) =>
-    each(ds, (w) =>
+    each(definitions, (w) =>
       each(object_property_get(w, "definitions"), (d) =>
         la([object_property_get(w, "word"), d]),
       ),
