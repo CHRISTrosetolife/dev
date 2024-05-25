3e0efe1952f80ff57272d9c8eff25657e51bf910
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 21:13:05 -0400

    2024-05-25T01:13:05.569Z  function_auto_run_args gcloud_tts fil-PH,Standard-D,sa,gitignore/test4.mp3

--- public/src/gcloud_tts.mjs
+++ public/src/gcloud_tts.mjs
@@ -1,32 +1,21 @@
-import { log } from "./log.mjs";
 import { file_overwrite_generic } from "./file_overwrite_generic.mjs";
 import textToSpeech from "@google-cloud/text-to-speech";
 import { file_exists } from "./file_exists.mjs";
-import { list_filter } from "./list_filter.mjs";
-import { string_starts_with } from "./string_starts_with.mjs";
 export async function gcloud_tts(language_code, voice, text, output_path) {
-  const client = new textToSpeech.TextToSpeechClient();
-  const [result] = await client.listVoices({});
-  let voices = result.voices;
-  voices = list_filter(voices, (v) => string_starts_with(v.name, "fil-"));
-  log({
-    voices,
-  });
-  return;
   if (await file_exists(output_path)) {
     return {
       created: false,
     };
   }
-  let { code, male } = language_code;
-  log(textToSpeech.listVoices());
+  const client = new textToSpeech.TextToSpeechClient();
+  let { code, male } = voice;
   const request = {
     input: {
       text: text,
     },
     voice: {
-      languageCode: code,
-      voice,
+      languageCode: language_code,
+      voice: code,
       ssmlGender: male ? "MALE" : "FEMALE",
     },
     audioConfig: {
