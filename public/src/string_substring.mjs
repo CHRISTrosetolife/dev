4f0cc8b702e77b1ff39b98d62621197b4186a7bf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-25 18:24:12 -0400

    2024-05-25T22:24:12.161Z  function_auto string_take

---
+++ public/src/string_substring.mjs
@@ -0,0 +1,3 @@
+export function string_substring(input, start, end) {
+  return input.substring(start, end);
+}
--- public/src/string_take.mjs
+++ public/src/string_take.mjs
@@ -1,7 +1,8 @@
+import { string_substring } from "./string_substring.mjs";
 import { assert_boolean } from "./assert_boolean.mjs";
 import { string_length } from "./string_length.mjs";
 export function string_take(input, count) {
   let length = string_length(input);
   assert_boolean(count <= length);
-  return input.substring(0, count);
+  return string_substring(input, 0, count);
 }
