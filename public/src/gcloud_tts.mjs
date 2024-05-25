fbe03509ddde87099a747780d868ae1f700676a2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 21:18:30 -0400

    2024-05-25T01:18:29.946Z  function_auto_run_args gcloud_tts fil-PH,Standard-C,MALE,sa,gitignore/test7.mp3

--- public/src/gcloud_tts.mjs
+++ public/src/gcloud_tts.mjs
@@ -1,6 +1,7 @@
 import { file_overwrite_generic } from "./file_overwrite_generic.mjs";
 import textToSpeech from "@google-cloud/text-to-speech";
 import { file_exists } from "./file_exists.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function gcloud_tts(
   language_code,
   voice,
@@ -20,7 +21,7 @@ export async function gcloud_tts(
     },
     voice: {
       languageCode: language_code,
-      voice,
+      voice: string_combine_multiple([language_code, "-", voice]),
       ssmlGender: ssml_gender,
     },
     audioConfig: {
