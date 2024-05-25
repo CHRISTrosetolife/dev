2a19fb2bca0d43349d8a1ee956aba17d6b50cf10
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-24 20:23:12 -0400

    2024-05-25T00:23:12.066Z  function_auto ceb_definition

--- public/src/ceb_definition.mjs
+++ public/src/ceb_definition.mjs
@@ -145,6 +145,9 @@ export async function ceb_definition(word) {
     }
     return d;
   });
+  log({
+    definitions,
+  });
   definitions = await list_filter_async(definitions, async (d) => {
     let url = string_combine(prefix_2, d);
     url = string_replace(url, " ", "+");
