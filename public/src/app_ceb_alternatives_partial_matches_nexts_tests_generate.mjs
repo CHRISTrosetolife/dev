ccecd90f6ff718210af3a97a8826c97d4cfc4b82
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-26 10:07:04 -0400

    2024-05-26T14:07:03.790Z  function_auto_run app_ceb_alternatives_partial_matches_nexts_tests_generate

--- public/src/app_ceb_alternatives_partial_matches_nexts_tests_generate.mjs
+++ public/src/app_ceb_alternatives_partial_matches_nexts_tests_generate.mjs
@@ -3,32 +3,15 @@ import { range } from "./range.mjs";
 import { tests_generate_single } from "./tests_generate_single.mjs";
 import { add_1 } from "./add_1.mjs";
 import { each_index_async } from "./each_index_async.mjs";
-import { each } from "./each.mjs";
 import { ceiling } from "./ceiling.mjs";
 import { divide } from "./divide.mjs";
-import { string_length } from "./string_length.mjs";
-import { list_adder } from "./list_adder.mjs";
-import { list_map } from "./list_map.mjs";
 export async function app_ceb_alternatives_partial_matches_nexts_tests_generate() {
   let f = app_ceb_alternatives_partial_matches_nexts;
-  return f("from", 1, 0, ["for", "front", "at"]);
-  let answer = "from";
-  let length = string_length(answer);
-  let list = list_adder((la) =>
-    each([1, 2, 3], (chunk_size) => {
-      each(range(ceiling(divide(add_1(length), chunk_size))), (index) => {
-        la({
-          answer,
-          chunk_size,
-          index,
-        });
-      });
-    }),
-  );
-  let inputs = list_map(list, (a) => {
-    let { answer, chunk_size, index } = a;
-    return [answer, chunk_size, index];
-  });
+  const alternatives = ["for", "form", "front", "at"];
+  const answer = "from";
+  const chunk_size = 1;
+  return f(answer, chunk_size, 0, alternatives);
+  range(ceiling(divide(add_1(length), chunk_size)));
   await each_index_async(inputs, async (input, index) => {
     await tests_generate_single(f.name, input, add_1(index));
   });
