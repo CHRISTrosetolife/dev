9daf4475b67b8fa289d204df6b11f5597f47135f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-26 10:09:15 -0400

    2024-05-26T14:09:15.148Z  function_auto_run app_ceb_alternatives_partial_matches_nexts_tests_generate

--- public/src/app_ceb_alternatives_partial_matches_nexts_tests_generate.mjs
+++ public/src/app_ceb_alternatives_partial_matches_nexts_tests_generate.mjs
@@ -5,13 +5,19 @@ import { add_1 } from "./add_1.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { ceiling } from "./ceiling.mjs";
 import { divide } from "./divide.mjs";
+import { list_adder } from "./list_adder.mjs";
+import { string_length } from "./string_length.mjs";
 export async function app_ceb_alternatives_partial_matches_nexts_tests_generate() {
   let f = app_ceb_alternatives_partial_matches_nexts;
   let alternatives = ["for", "form", "front", "at"];
   let answer = "from";
+  let length = string_length(answer);
   let chunk_size = 1;
-  return f(answer, chunk_size, 0, alternatives);
-  range(ceiling(divide(add_1(length), chunk_size)));
+  return list_adder(
+    (la) => range(ceiling(divide(add_1(length), chunk_size))),
+    (index) => la(f(answer, chunk_size, 0, alternatives)),
+  );
+  return;
   await each_index_async(inputs, async (input, index) => {
     await tests_generate_single(f.name, input, add_1(index));
   });
