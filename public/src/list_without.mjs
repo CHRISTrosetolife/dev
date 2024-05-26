ae9048fd1dff35702be782bde925dfae5938f163
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-25 20:34:39 -0400

    2024-05-26T00:34:38.923Z  function_auto list_without

--- public/src/list_without.mjs
+++ public/src/list_without.mjs
@@ -1,7 +1,5 @@
-import { list_remove } from "./list_remove.mjs";
-import { list_copy } from "./list_copy.mjs";
+import { list_filter } from "./list_filter.mjs";
+import { equal_not } from "./equal_not.mjs";
 export function list_without(atom, pair) {
-  let pairs_other = list_copy(atom);
-  list_remove(pairs_other, pair);
-  return pairs_other;
+  return list_filter(atom, (a) => equal_not(a, pair));
 }
