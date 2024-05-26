116acbda176ba1f5d38f52ad7eb2005a1f3730ec
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-26 10:02:36 -0400

    2024-05-26T14:02:36.507Z  function_auto app_ceb_alternatives_partial_matches_nexts

--- public/src/app_ceb_alternatives_partial_matches_nexts.mjs
+++ public/src/app_ceb_alternatives_partial_matches_nexts.mjs
@@ -1,7 +1,8 @@
+import { app_ceb_correct_get } from "./app_ceb_correct_get.mjs";
+import { string_chunk } from "./string_chunk.mjs";
 import { equal_not } from "./equal_not.mjs";
 import { list_length } from "./list_length.mjs";
 import { greater_than_equal } from "./greater_than_equal.mjs";
-import { log } from "./log.mjs";
 import { number_min } from "./number_min.mjs";
 import { add } from "./add.mjs";
 import { string_substring } from "./string_substring.mjs";
@@ -18,9 +19,8 @@ export function app_ceb_alternatives_partial_matches_nexts(
   chunk_size,
   index,
   alternatives,
-  correct_choices,
-  correct_get,
 ) {
+  let correct_choices = string_chunk(answer, chunk_size);
   let answer_partial = app_ceb_answer_partial(answer, chunk_size, index);
   let alternatives_partial_matches = list_filter(alternatives, (a) =>
     and(
@@ -41,13 +41,7 @@ export function app_ceb_alternatives_partial_matches_nexts(
         add(index, number_min(chunk_size, string_length(a) - index)),
       ),
   );
-  log({
-    answer_partial,
-    index,
-    alternatives_partial_matches,
-    alternatives_partial_matches_nexts,
-  });
-  let correct;
+  let correct = app_ceb_correct_get(index);
   if (greater_than_equal(index, list_length(correct_choices))) {
     correct = null;
   } else {
