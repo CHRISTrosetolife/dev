cca7c2ecb75bb24b50697104358304e0c981fb80
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-26 09:51:42 -0400

    2024-05-26T13:51:42.113Z  function_auto_run app_ceb_answer_partial_tests_generate

---
+++ public/src/app_ceb_answer_partial_test_5.mjs
@@ -0,0 +1,7 @@
+import {equal_json} from "./equal_json.mjs";
+import {assert_boolean} from "./assert_boolean.mjs";
+import {app_ceb_answer_partial} from "./app_ceb_answer_partial.mjs";
+export function app_ceb_answer_partial_test_5() {
+    let result = app_ceb_answer_partial('from', 2, 0);
+    assert_boolean(equal_json(result, ""));
+}
---
+++ public/src/app_ceb_answer_partial_test_6.mjs
@@ -0,0 +1,7 @@
+import {equal_json} from "./equal_json.mjs";
+import {assert_boolean} from "./assert_boolean.mjs";
+import {app_ceb_answer_partial} from "./app_ceb_answer_partial.mjs";
+export function app_ceb_answer_partial_test_6() {
+    let result = app_ceb_answer_partial('from', 2, 1);
+    assert_boolean(equal_json(result, "fr"));
+}
