5fd5a7bf2a1be32857569b2a6901af1aea2eb0fd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-26 09:43:05 -0400

    2024-05-26T13:43:05.456Z  function_auto app_ceb_answer_partial_tests_generate

--- public/src/app_ceb_answer_partial_tests_generate.mjs
+++ public/src/app_ceb_answer_partial_tests_generate.mjs
@@ -1,9 +1,7 @@
 import { tests_generate_single } from "./tests_generate_single.mjs";
 import { app_ceb_answer_partial } from "./app_ceb_answer_partial.mjs";
 import { add_1 } from "./add_1.mjs";
-import { logic_parse } from "./logic_parse.mjs";
 import { each_index_async } from "./each_index_async.mjs";
-import { identity } from "./identity.mjs";
 export async function app_ceb_answer_partial_tests_generate() {
   let answer = "from";
   await each_index_async(inputs, async (input, index) => {
@@ -11,8 +9,6 @@ export async function app_ceb_answer_partial_tests_generate() {
       app_ceb_answer_partial.name,
       [input],
       add_1(index),
-      [logic_parse],
-      identity,
     );
   });
 }
