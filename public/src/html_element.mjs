9d480d416e639219eb2225b09ab139a21a6259c5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-26 17:27:07 -0400

    2024-05-26T21:27:06.851Z  function_auto html_element

--- public/src/html_element.mjs
+++ public/src/html_element.mjs
@@ -1,5 +1,6 @@
 import { html_style_default } from "./html_style_default.mjs";
 import { html_style } from "./html_style.mjs";
+import { html_style_font_color } from "./html_style_font_color.mjs";
 export function html_element(parent, tag_name) {
   let { element } = parent;
   let e = document.createElement(tag_name);
@@ -7,6 +8,7 @@ export function html_element(parent, tag_name) {
   let result = {
     element: e,
   };
+  html_style_font_color(result, "black");
   html_style(result, html_style_default());
   return result;
 }
