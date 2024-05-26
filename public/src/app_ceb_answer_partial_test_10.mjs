b769c1389a507d69f80a333fdc2c3d21b09c85a3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-26 09:52:52 -0400

    2024-05-26T13:52:52.476Z  function_auto_run app_ceb_answer_partial_tests_generate

--- public/src/app_ceb_answer_partial_test_10.mjs
+++ public/src/app_ceb_answer_partial_test_10.mjs
@@ -1,7 +1,7 @@
 import {equal_json} from "./equal_json.mjs";
 import {assert_boolean} from "./assert_boolean.mjs";
 import {app_ceb_answer_partial} from "./app_ceb_answer_partial.mjs";
-export function app_ceb_answer_partial_test_5() {
-    let result = app_ceb_answer_partial('from', 1, 4);
-    assert_boolean(equal_json(result, "from"));
+export function app_ceb_answer_partial_test_10() {
+    let result = app_ceb_answer_partial('from', 3, 1);
+    assert_boolean(equal_json(result, "fro"));
 }
--- public/src/app_ceb_answer_partial_test_8.mjs
+++ public/src/app_ceb_answer_partial_test_8.mjs
@@ -2,6 +2,6 @@ import {equal_json} from "./equal_json.mjs";
 import {assert_boolean} from "./assert_boolean.mjs";
 import {app_ceb_answer_partial} from "./app_ceb_answer_partial.mjs";
 export function app_ceb_answer_partial_test_8() {
-    let result = app_ceb_answer_partial('from', 3, 1);
-    assert_boolean(equal_json(result, "fro"));
+    let result = app_ceb_answer_partial('from', 2, 2);
+    assert_boolean(equal_json(result, "from"));
 }
