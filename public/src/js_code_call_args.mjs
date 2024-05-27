d8d8c235388ca31227a73cd40dc6f1558a4bf8d9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-27 13:59:08 -0400

    2024-05-27T17:59:08.575Z  function_auto js_code_call_args

--- public/src/js_code_call_args.mjs
+++ public/src/js_code_call_args.mjs
@@ -1,6 +1,5 @@
-import { js_code_statement } from "./js_code_statement.mjs";
 import { list_join } from "./list_join.mjs";
-export function js_code_call_args(function_name, args) {
-  let code = `${function_name}(${list_join(args, ", ")})`;
+export function js_code_call_args(function_name, args_code) {
+  let code = `${function_name}(${list_join(args_code, ", ")})`;
   return code;
 }
