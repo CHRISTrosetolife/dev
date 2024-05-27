653187076099bf9e240565b77f1b049b9a10d20e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-27 13:09:48 -0400

    2024-05-27T17:09:48.680Z  function_auto app_dev_screen_add

--- public/src/app_dev_screen_add.mjs
+++ public/src/app_dev_screen_add.mjs
@@ -4,10 +4,14 @@ import { app_list_add } from "./app_list_add.mjs";
 import { list_add } from "./list_add.mjs";
 import { js_code_statement_call_args } from "./js_code_statement_call_args.mjs";
 import { app_learn_code_modules } from "./app_learn_code_modules.mjs";
+import { js_code_call } from "./js_code_call.mjs";
 export async function app_dev_screen_add(name) {
   let screen_name = app_dev_screen_name(name);
   function_new(screen_name);
   await app_list_add(app_learn_code_modules, function inserted_code() {
-    return js_code_statement_call_args(list_add.name, ["result", screen_name]);
+    return js_code_statement_call_args(list_add.name, [
+      "result",
+      js_code_call(screen_name),
+    ]);
   });
 }
