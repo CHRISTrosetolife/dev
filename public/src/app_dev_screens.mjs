71e179e36e3ed3bac8759a4cc31edaf28340afe6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-27 14:02:59 -0400

    2024-05-27T18:02:59.266Z  app_dev_screen_add overview

--- public/src/app_dev_screens.mjs
+++ public/src/app_dev_screens.mjs
@@ -1,6 +1,9 @@
-import { marker } from "./marker.mjs";
+import {app_dev_screen_overview} from "./app_dev_screen_overview.mjs";
+import {list_add} from "./list_add.mjs";
+import {marker} from "./marker.mjs";
 export function app_dev_screens() {
-  let result = [];
-  marker();
-  return result;
+    let result = [];
+    list_add(result, app_dev_screen_overview());
+    marker();
+    return result;
 }
