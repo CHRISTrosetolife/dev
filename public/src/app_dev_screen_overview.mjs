a18261d3e322e8f2a0687aa06016f382a063f4e4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-27 14:00:53 -0400

    2024-05-27T18:00:52.825Z  function_auto app_dev_screens

--- public/src/app_dev_screen_overview.mjs
+++
@@ -1,1 +0,0 @@
-export function app_dev_screen_overview() {}
--- public/src/app_dev_screens.mjs
+++ public/src/app_dev_screens.mjs
@@ -1,9 +1,6 @@
 import { marker } from "./marker.mjs";
-import { app_dev_screen_overview } from "./app_dev_screen_overview.mjs";
-import { list_add } from "./list_add.mjs";
 export function app_dev_screens() {
   let result = [];
-  list_add(result, app_dev_screen_overview());
   marker();
   return result;
 }
