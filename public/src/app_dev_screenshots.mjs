f8a090edc9906016f30a9127eb8111cf67b8949a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 09:45:52 -0400

    2024-05-28T13:45:52.133Z  function_auto app_dev_screenshots

--- public/src/app_dev_screenshots.mjs
+++ public/src/app_dev_screenshots.mjs
@@ -1,6 +1,11 @@
 import { folder_files_move } from "./folder_files_move.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function app_dev_screenshots(screen_name) {
   let folder_path = "C:\\Users\\JESUS\\Pictures\\Screenshots";
-  let file_extension='.png'
-  await folder_files_move(folder_path, file_extension);
+  let file_extension = ".png";
+  await folder_files_move(
+    folder_path,
+    file_extension,
+    string_combine_multiple([".\\public\\img\\"]),
+  );
 }
