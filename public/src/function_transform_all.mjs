7c6ae0ea18d5d521f3fec967201d797bf28a200f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 14:15:10 -0400

    2024-05-28T18:15:10.384Z  function_auto function_transform_all

--- public/src/function_transform_all.mjs
+++ public/src/function_transform_all.mjs
@@ -1,4 +1,4 @@
 import { function_transform_all_args } from "./function_transform_all_args.mjs";
 export async function function_transform_all(lambda_name, args) {
-  return function_transform_all_args(lambda_name, "a");
+  return await function_transform_all_args(lambda_name, "a");
 }
