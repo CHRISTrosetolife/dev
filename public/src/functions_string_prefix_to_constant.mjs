9decbb9993c47bec0deef11dbd2dbf4bb8a24381
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 17:18:36 -0400

    2024-05-28T21:18:35.609Z  function_open data_update

--- public/src/functions_string_prefix_to_constant.mjs
+++ public/src/functions_string_prefix_to_constant.mjs
@@ -1,6 +1,12 @@
+import { log } from "./log.mjs";
 import { data_functions } from "./data_functions.mjs";
 import { each_object } from "./each_object.mjs";
 export async function functions_string_prefix_to_constant() {
   let functions = await data_functions();
-  each_object(functions, (function_name, details) => {});
+  each_object(functions, (function_name, details) => {
+    let { literals } = details;
+    log({
+      literals,
+    });
+  });
 }
