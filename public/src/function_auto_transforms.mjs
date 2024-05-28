1f721dfc36b936a87aebcd2d9cff2a5ef48d000c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 12:53:47 -0400

    2024-05-28T16:53:47.426Z  function_auto function_auto_transforms

--- public/src/function_auto_transforms.mjs
+++ public/src/function_auto_transforms.mjs
@@ -1,5 +1,7 @@
+import { js_const_to_let } from "./js_const_to_let.mjs";
 import { js_imports_fix } from "./js_imports_fix.mjs";
 import { js_outside_move } from "./js_outside_move.mjs";
 export function function_auto_transforms() {
+  if (0) js_const_to_let;
   return [js_outside_move, js_imports_fix];
 }
