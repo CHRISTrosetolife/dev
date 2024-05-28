bc9b2249e5c4b3584f36912ad1e23abab4b83fea
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 09:50:27 -0400

    2024-05-28T13:50:27.453Z  function_auto app_dev_screenshots

--- public/src/app_dev_screenshots.mjs
+++ public/src/app_dev_screenshots.mjs
@@ -1,3 +1,4 @@
+import { folder_files_rename_incrementing } from "./folder_files_rename_incrementing.mjs";
 import { app_name } from "./app_name.mjs";
 import { app_dev } from "./app_dev.mjs";
 import { folder_files_move } from "./folder_files_move.mjs";
@@ -11,5 +12,6 @@ export async function app_dev_screenshots(screen_name) {
     "\\",
     screen_name,
   ]);
+  folder_files_rename_incrementing;
   await folder_files_move(folder_path, file_extension, target_path);
 }
