f17dbe86c3563416b90c907cf07f79c394564dd2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 19:51:54 -0400

    2024-05-28T23:51:54.215Z  function_auto app_dev_screen_command_line

--- public/src/app_dev_screen_command_line.mjs
+++ public/src/app_dev_screen_command_line.mjs
@@ -1,9 +1,12 @@
+import { html_img } from "./html_img.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_command_line() {
   return {
     name: function command_line_name(root) {
       return html_span_text(root, "command_line");
     },
-    screen: function command_line_screen(root) {},
+    screen: function command_line_screen(root) {
+      html_img;
+    },
   };
 }
