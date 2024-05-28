01717dbce0a571e8f70e91aee69d0232d72bb9bc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 13:00:47 -0400

    2024-05-28T17:00:47.368Z  function_auto file_overwrite_json

--- public/src/file_overwrite_json.mjs
+++ public/src/file_overwrite_json.mjs
@@ -1,6 +1,6 @@
-import { file_write } from "./file_write.mjs";
+import { file_overwrite } from "./file_overwrite.mjs";
 import { json_to } from "./json_to.mjs";
 export async function file_overwrite_json(file_path, object) {
   let json = json_to(object);
-  await file_write(file_path, json);
+  await file_overwrite(file_path, json);
 }
