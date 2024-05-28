ecc216cb720ce59498ca7380ae68a89c2ef5ede9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 12:54:08 -0400

    2024-05-28T16:54:08.199Z  function_auto js_const_to_let

--- public/src/js_const_to_let.mjs
+++ public/src/js_const_to_let.mjs
@@ -1,7 +1,7 @@
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_const_to_let(ast) {
   js_visit_node(ast, "VariableDeclaration", (v) => {
-    const { node } = v;
+    let { node } = v;
     if (node.kind === "const") {
       node.kind = "let";
     }
