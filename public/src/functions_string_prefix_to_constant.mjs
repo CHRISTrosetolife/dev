ce57e058e797390e969d8ffdf8bcd53e18a11ebc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 17:25:16 -0400

    2024-05-28T21:25:16.302Z  function_auto functions_string_prefix_to_constant

--- public/src/functions_string_prefix_to_constant.mjs
+++ public/src/functions_string_prefix_to_constant.mjs
@@ -2,12 +2,15 @@ import { data_functions } from "./data_functions.mjs";
 import { each_object } from "./each_object.mjs";
 import { string_is } from "./string_is.mjs";
 import { each } from "./each.mjs";
-export async function functions_string_prefix_to_constant() {
+import { string_starts_with } from "./string_starts_with.mjs";
+export async function functions_string_prefix_to_constant(prefix) {
   let functions = await data_functions();
   each_object(functions, (function_name, details) => {
     let { literals } = details;
     each(literals, (literal) => {
       if (string_is(literal)) {
+        if (string_starts_with(literal, prefix)) {
+        }
       }
     });
   });
