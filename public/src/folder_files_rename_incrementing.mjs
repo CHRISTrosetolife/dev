9f78fddf1a47340c4ac844c57bc0ff1b94fb2e1e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 09:49:26 -0400

    2024-05-28T13:49:26.094Z  function_auto folder_files_rename_incrementing

--- public/src/folder_files_rename_incrementing.mjs
+++ public/src/folder_files_rename_incrementing.mjs
@@ -1,5 +1,5 @@
+import { add } from "./add.mjs";
 import { file_rename } from "./file_rename.mjs";
-import { add_1 } from "./add_1.mjs";
 import { string_to } from "./string_to.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { path_join } from "./path_join.mjs";
@@ -11,13 +11,14 @@ export async function folder_files_rename_incrementing(
   input_directory,
   file_extension,
 ) {
+  let starting = 1;
   let file_paths = await folder_read(input_directory, file_extension);
   await each_index_async(file_paths, async (file_path, index) => {
     let dirname = path_dirname(file_path);
     let extension = path_extname(file_path);
     let file_path_new = path_join([
       dirname,
-      string_combine(string_to(add_1(index)), extension),
+      string_combine(string_to(add(starting, index)), extension),
     ]);
     await file_rename(file_path, file_path_new);
   });
