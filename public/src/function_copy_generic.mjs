6fbbf305beec279ec34e95a40351f9937c4d1df8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 14:18:50 -0400

    2024-05-28T18:18:50.539Z  function_auto function_copy_generic

--- public/src/function_copy_generic.mjs
+++ public/src/function_copy_generic.mjs
@@ -9,6 +9,6 @@ export async function function_copy_generic(
   let fn_path_from = function_name_to_path(fn_name_from);
   let fn_path_to = function_name_to_path(fn_name_to);
   await file_lambda(fn_path_from, fn_path_to);
-  await function_data(fn_path_to);
   await file_js_identifier_rename(fn_path_to, fn_name_from, fn_name_to);
+  await function_data(fn_path_to);
 }
