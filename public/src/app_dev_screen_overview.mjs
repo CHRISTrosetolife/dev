442bbf082bc2fadc7ee612dfd61aeb8e018841bc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-27 20:35:17 -0400

    2024-05-28T00:35:17.397Z  function_auto app_dev_screen_overview

--- public/src/app_dev_screen_overview.mjs
+++ public/src/app_dev_screen_overview.mjs
@@ -1,3 +1,4 @@
+import { html_p_text } from "./html_p_text.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_overview() {
   return {
@@ -5,7 +6,10 @@ export function app_dev_screen_overview() {
       return html_span_text(root, "overview");
     },
     screen: function overview_screen(root) {
-      "to keep the programming simple , functions are the basic building block , rather than classes or object-oriented programming";
+      html_p_text(
+        root,
+        "to keep the programming simple , functions are the basic building block , rather than classes or object-oriented programming",
+      );
     },
   };
 }
