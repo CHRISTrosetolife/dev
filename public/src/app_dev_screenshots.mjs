26b6415a6851165b211ace8bb442a00b9a3a7973
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 09:45:18 -0400

    2024-05-28T13:45:18.095Z  string_slashes_escape public\img

--- public/src/app_dev_screenshots.mjs
+++ public/src/app_dev_screenshots.mjs
@@ -1,5 +1,6 @@
 import { folder_files_move } from "./folder_files_move.mjs";
 export async function app_dev_screenshots(screen_name) {
   let folder_path = "C:\\Users\\JESUS\\Pictures\\Screenshots";
-  await folder_files_move();
+  let file_extension='.png'
+  await folder_files_move(folder_path, file_extension);
 }
