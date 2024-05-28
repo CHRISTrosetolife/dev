133a27996bc26f626a269fe795d42829c1ac5f61
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 14:28:17 -0400

    2024-05-28T18:28:16.709Z  function_auto js_data

--- public/src/data_update_single.mjs
+++ public/src/data_update_single.mjs
@@ -1,6 +1,7 @@
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
+import { list_unique } from "./list_unique.mjs";
 export function data_update_single(ast, data) {
   let declaration = js_declaration_single(ast);
   let {
@@ -10,4 +11,7 @@ export function data_update_single(ast, data) {
   let functions = object_property_initialize(data, "functions", {});
   let f = object_property_initialize(functions, name, {});
   object_property_set(f, "async", async);
+  return;
+  let literals = js_node_type(filtered_n, "Literal");
+  let u =list_unique(literals)
 }
