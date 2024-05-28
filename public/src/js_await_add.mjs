ced3fa5349f6c9bfba7fcd50dab7d1525b7be517
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 14:10:44 -0400

    2024-05-28T18:10:44.404Z  function_auto js_await_add

--- public/src/js_await_add.mjs
+++ public/src/js_await_add.mjs
@@ -1,3 +1,4 @@
+import { assert } from "./assert.mjs";
 import { each_object } from "./each_object.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -7,6 +8,7 @@ import { js_visit_node } from "./js_visit_node.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { counter } from "./counter.mjs";
+import { equal_1 } from "./equal_1.mjs";
 export async function js_await_add(ast) {
   let data = await file_read_json(data_path());
   js_visit_node(ast, "CallExpression", (v) => {
@@ -32,6 +34,7 @@ export async function js_await_add(ast) {
                 }
               });
             });
+            assert(equal_1, [count]);
           }
         }
       }
