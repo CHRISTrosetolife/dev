260fe8011e2bcc772b54dba9906968bccdf6757d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 13:34:11 -0400

    2024-05-28T17:34:11.101Z  function_auto js_await_add

--- public/src/js_await_add.mjs
+++ public/src/js_await_add.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { data_path } from "./data_path.mjs";
 import { file_read_json } from "./file_read_json.mjs";
 import { log } from "./log.mjs";
@@ -11,7 +12,8 @@ export async function js_await_add(ast) {
     if (callee.type === "Identifier") {
       let { name } = callee;
       let { functions } = data;
-      if (object_property_exists(data, name)) {
+      if (object_property_exists(functions, name)) {
+        let f = object_property_get(functions, name);
       }
     }
     log({
