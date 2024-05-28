7d07d5b474624b0ad9c59822bb067494a4c00bc6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 19:49:58 -0400

    2024-05-28T23:49:58.458Z  function_auto file_rename

--- public/src/file_rename.mjs
+++ public/src/file_rename.mjs
@@ -1,5 +1,6 @@
-import { import_node } from "./import_node.mjs";
+import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
 let fs = await import_node("fs");
 export async function file_rename(file_name_from, file_name_to) {
+  await folder_parent_exists_ensure(file_name_to);
   await fs.promises.rename(file_name_from, file_name_to);
 }
