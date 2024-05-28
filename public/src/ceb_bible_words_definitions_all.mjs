132f2186c65a98f2b8280ba66e13e7407baec752
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 09:07:01 -0400

    2024-05-28T13:07:01.411Z  function_auto ceb_bible_words_definitions_all

--- public/src/ceb_bible_words_definitions_all.mjs
+++ public/src/ceb_bible_words_definitions_all.mjs
@@ -1,38 +1,8 @@
-import { list_add } from "./list_add.mjs";
-import { list_empty_not_is } from "./list_empty_not_is.mjs";
-import { object_property_set } from "./object_property_set.mjs";
-import { object_property_exists_not } from "./object_property_exists_not.mjs";
-import { ceb_definition } from "./ceb_definition.mjs";
-import { log } from "./log.mjs";
-import { each_index_async } from "./each_index_async.mjs";
-import { list_skip } from "./list_skip.mjs";
-import { ceb_bible_words_top } from "./ceb_bible_words_top.mjs";
+import { ceb_bible_words_definitions_get } from "./ceb_bible_words_definitions_get.mjs";
+import { ceb_bible_words } from "./ceb_bible_words.mjs";
 export async function ceb_bible_words_definitions_all() {
-    let words = await ceb_bible_words();
+  let words = await ceb_bible_words();
   let existing = {};
-  let result = [];
-  await each_index_async(words, async (bible_word, index) => {
-    if (0)
-      log({
-        bible_word,
-        index,
-      });
-    let { word, definitions } = await ceb_definition(bible_word);
-    if (object_property_exists_not(existing, word)) {
-      object_property_set(existing, word, true);
-      if (list_empty_not_is(definitions)) {
-        list_add(result, {
-          word,
-          definitions,
-        });
-      }
-    }
-    if (0)
-      log({
-        word,
-        definitions,
-      });
-  });
-  if (0) return;
+  let result = await ceb_bible_words_definitions_get(t);
   return result;
 }
