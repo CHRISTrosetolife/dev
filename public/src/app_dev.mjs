8799513a7b6ae9793d98c7c7393f1060473e840b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-27 20:37:12 -0400

    2024-05-28T00:37:11.981Z  function_auto app_dev

--- public/src/app_dev.mjs
+++ public/src/app_dev.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { html_buttons_next_previous } from "./html_buttons_next_previous.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
@@ -7,11 +6,15 @@ import { app_dev_screens } from "./app_dev_screens.mjs";
 import { each_index } from "./each_index.mjs";
 import { list_get } from "./list_get.mjs";
 import { list_index_last } from "./list_index_last.mjs";
+import { html_span_text } from "./html_span_text.mjs";
+import { string_combine } from "./string_combine.mjs";
+import { add_1 } from "./add_1.mjs";
 export function app_dev() {
   let root = html_style_default_initialize();
   let screens = app_dev_screens();
   each_index(screens, (s, index) => {
     let b = html_button_width_full_text_click(root, "", () => screen(index));
+    html_span_text(b, string_combine(add_1(index), ". "));
     s.name(b);
   });
   function screen(index) {
