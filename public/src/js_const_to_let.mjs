20baad335536c56955d5f438a5c1d00dd1353646
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 12:52:14 -0400

    2024-05-28T16:52:14.007Z  function_auto js_const_to_let

--- public/src/js_const_to_let.mjs
+++ public/src/js_const_to_let.mjs
@@ -1,1 +1,4 @@
-export function js_const_to_let(ast) {}
+import { js_visit_node } from "./js_visit_node.mjs";
+export function js_const_to_let(ast) {
+  js_visit_node(ast, "VariableDeclaration", (v) => {});
+}
