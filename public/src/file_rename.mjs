4a7841471491eb0a7720a057f4aff79a52edbd6c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 19:51:17 -0400

    2024-05-28T23:51:17.185Z  app_dev_screenshots command_line

--- public/src/file_rename.mjs
+++ public/src/file_rename.mjs
@@ -1,4 +1,5 @@
 import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
+import { import_node } from "./import_node.mjs";
 let fs = await import_node("fs");
 export async function file_rename(file_name_from, file_name_to) {
   await folder_parent_exists_ensure(file_name_to);
