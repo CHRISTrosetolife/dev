6d151cfa853f141b59590adb4522360f37a49e97
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 13:30:25 -0400

    2024-05-28T17:30:25.610Z  function_transform js_await_add app_dev_screenshots

--- public/src/js_await_add.mjs
+++ public/src/js_await_add.mjs
@@ -2,7 +2,7 @@ import { log } from "./log.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_await_add(ast) {
   js_visit_node(ast, "CallExpression", (v) => {
-    let { node } = v;
+    let { node } = v;if (node.callee.type==='Identifier'){}
     log({
       node,
     });
