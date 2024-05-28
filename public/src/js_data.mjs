1ab9d7a53c377f41bd860681fdcd2aa36b0e6953
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 13:01:18 -0400

    2024-05-28T17:01:18.579Z  function_transform js_data js_data

--- public/src/js_data.mjs
+++ public/src/js_data.mjs
@@ -11,6 +11,7 @@ export async function js_data(ast) {
   await file_json_transform(
     () => {
       let declaration = js_declaration_single(ast);
+      let { async } = declaration;
       log({
         declaration,
       });
