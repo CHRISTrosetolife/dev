d68b5e4376fae33d0a1400e6769daae0b03749d4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 19:53:45 -0400

    2024-05-28T23:53:45.009Z  function_auto app_dev_screen_name

--- public/src/app_dev_screen_name.mjs
+++ public/src/app_dev_screen_name.mjs
@@ -1,7 +1,8 @@
+import { string_combine } from "./string_combine.mjs";
 import { app_dev } from "./app_dev.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_dev_screen_name(name) {
-  const s = "_screen_";
-  let lesson_name = string_combine_multiple([app_dev.name, s, name]);
+  let s = "_screen_";
+  let prefix = string_combine(app_dev.name, s);
+  let lesson_name = string_combine(prefix, name);
   return lesson_name;
 }
