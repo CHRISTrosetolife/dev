6267f4f298d081a8512f57e9a2dc7d58308ce6a3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-27 20:23:37 -0400

    2024-05-28T00:23:36.888Z  function_auto app_dev

--- public/src/app_dev.mjs
+++ public/src/app_dev.mjs
@@ -1,3 +1,7 @@
+import { list_length } from "./list_length.mjs";
+import { list_index_last } from "./list_index_last.mjs";
+import { greater_than } from "./greater_than.mjs";
+import { html_buttons_next_previous } from "./html_buttons_next_previous.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
@@ -10,6 +14,18 @@ export function app_dev() {
     let b = html_button_width_full_text_click(root, "", () => {
       html_clear_scroll_top(root);
       s.screen(root);
+      html_buttons_next_previous(
+        root,
+        (pair_index) => {
+          if (greater_than(pair_index, list_index_last(concat))) {
+            refresh_node();
+          } else {
+            refresh_pair(pair_index);
+          }
+        },
+        pair_index,
+        list_length(concat),
+      );
     });
     s.name(b);
   });
