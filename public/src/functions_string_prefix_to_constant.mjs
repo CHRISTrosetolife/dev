1d395d20f6437efce8c649e261c222268b88048b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 17:36:25 -0400

    2024-05-28T21:36:24.964Z  function_auto_run_args functions_string_prefix_to_constant C:\Users\JESUS"

--- public/src/functions_string_prefix_to_constant.mjs
+++ public/src/functions_string_prefix_to_constant.mjs
@@ -1,15 +1,21 @@
+import { log } from "./log.mjs";
 import { data_functions } from "./data_functions.mjs";
 import { each_object } from "./each_object.mjs";
 import { string_is } from "./string_is.mjs";
 import { each } from "./each.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
+import { assert } from "./assert.mjs";
 export async function functions_string_prefix_to_constant(prefix) {
+  assert(string_is, [prefix]);
   let functions = await data_functions();
   each_object(functions, (function_name, details) => {
     let { literals } = details;
     each(literals, (literal) => {
       if (string_is(literal)) {
         if (string_starts_with(literal, prefix)) {
+          log({
+            function_name,
+          });
         }
       }
     });
