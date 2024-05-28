8765975589cf4c604014b4e631612e198dc90528
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 09:48:25 -0400

    2024-05-28T13:48:24.722Z  function_auto app_dev_screenshots

--- public/src/app_dev_screenshots.mjs
+++ public/src/app_dev_screenshots.mjs
@@ -5,14 +5,11 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function app_dev_screenshots(screen_name) {
   let folder_path = "C:\\Users\\JESUS\\Pictures\\Screenshots";
   let file_extension = ".png";
-  await folder_files_move(
-    folder_path,
-    file_extension,
-    string_combine_multiple([
-      ".\\public\\img\\",
-      app_name(app_dev),
-      "\\",
-      screen_name,
-    ]),
-  );
+  const target_path = string_combine_multiple([
+    ".\\public\\img\\",
+    app_name(app_dev),
+    "\\",
+    screen_name,
+  ]);
+  await folder_files_move(folder_path, file_extension, target_path);
 }
