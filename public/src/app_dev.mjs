236c04b09e42769a941d4ababf6eb313673acaf5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-27 20:26:02 -0400

    2024-05-28T00:26:02.181Z  function_auto app_dev

--- public/src/app_dev.mjs
+++ public/src/app_dev.mjs
@@ -1,32 +1,22 @@
 import { list_length } from "./list_length.mjs";
-import { list_index_last } from "./list_index_last.mjs";
-import { greater_than } from "./greater_than.mjs";
 import { html_buttons_next_previous } from "./html_buttons_next_previous.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { app_dev_screens } from "./app_dev_screens.mjs";
-import { each } from "./each.mjs";
+import { each_index } from "./each_index.mjs";
+import { list_get } from "./list_get.mjs";
 export function app_dev() {
   let root = html_style_default_initialize();
   let screens = app_dev_screens();
-  each(screens, (s) => {
-    let b = html_button_width_full_text_click(root, "", () => {
-      html_clear_scroll_top(root);
-      s.screen(root);
-      html_buttons_next_previous(
-        root,
-        (pair_index) => {
-          if (greater_than(pair_index, list_index_last(concat))) {
-            refresh_node();
-          } else {
-            refresh_pair(pair_index);
-          }
-        },
-        pair_index,
-        list_length(concat),
-      );
-    });
+  each_index(screens, (s, index) => {
+    let b = html_button_width_full_text_click(root, "", () => screen(index));
     s.name(b);
   });
+  function screen(index) {
+    html_clear_scroll_top(root);
+    let s = list_get(screens, index);
+    s.screen(root);
+    html_buttons_next_previous(root, screen, index, list_length(screens));
+  }
 }
