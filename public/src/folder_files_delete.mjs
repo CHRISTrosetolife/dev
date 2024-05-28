2686c4b7e543bf749d62943e1cf85cea088f43b6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 09:14:47 -0400

    2024-05-28T13:14:47.611Z  function_auto folder_files_delete

--- public/src/folder_files_delete.mjs
+++ public/src/folder_files_delete.mjs
@@ -1,6 +1,5 @@
 import { each_async } from "./each_async.mjs";
 import { file_delete } from "./file_delete.mjs";
-import { path_parse_base } from "./path_parse_base.mjs";
 import { folder_read } from "./folder_read.mjs";
 export async function folder_files_delete(
   input_directory,
@@ -9,7 +8,6 @@ export async function folder_files_delete(
 ) {
   let file_paths = await folder_read(input_directory, file_extension);
   await each_async(file_paths, async (file_path) => {
-    let file_name = path_parse_base(file_path);
     await file_delete(file_path);
   });
 }
