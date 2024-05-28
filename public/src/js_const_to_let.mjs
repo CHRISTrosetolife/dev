50fcb27ea826890eea06209f52c97d992bb87ebb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 12:53:04 -0400

    2024-05-28T16:53:04.536Z  function_auto js_const_to_let

--- public/src/js_const_to_let.mjs
+++ public/src/js_const_to_let.mjs
@@ -2,5 +2,8 @@ import { js_visit_node } from "./js_visit_node.mjs";
 export function js_const_to_let(ast) {
   js_visit_node(ast, "VariableDeclaration", (v) => {
     let { node } = v;
+    if (node.kind === "const") {
+      node.kind = "let";
+    }
   });
 }
