2541e7aa0ccb3b11034685249a4d2d15006eb206
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 13:29:43 -0400

    2024-05-28T17:29:43.456Z  function_auto js_await_add

--- public/src/js_await_add.mjs
+++ public/src/js_await_add.mjs
@@ -1,9 +1,9 @@
 import { log } from "./log.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_await_add(ast) {
-  js_visit_node(ast, "CallExpression", (v) =>
+  js_visit_node(ast, "CallExpression", (v) => {
     log({
       v,
-    }),
-  );
+    });
+  });
 }
