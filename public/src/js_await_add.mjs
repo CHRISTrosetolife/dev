3ea90220746022cb04ee65fbc85ad78fc3d9ead6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 13:31:25 -0400

    2024-05-28T17:31:25.028Z  function_auto js_await_add

--- public/src/js_await_add.mjs
+++ public/src/js_await_add.mjs
@@ -1,6 +1,9 @@
+import { data_path } from "./data_path.mjs";
+import { file_read_json } from "./file_read_json.mjs";
 import { log } from "./log.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
-export function js_await_add(ast) {
+export async function js_await_add(ast) {
+  let data = await file_read_json(data_path());
   js_visit_node(ast, "CallExpression", (v) => {
     let { node } = v;
     if (node.callee.type === "Identifier") {
