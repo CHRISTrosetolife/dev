143e5ada0a8cd65f034179d83f28b6a2c8282212
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 14:58:08 -0400

    2024-05-28T18:58:08.197Z  function_auto list_before

--- public/src/list_before.mjs
+++ public/src/list_before.mjs
@@ -1,6 +1,6 @@
 import { list_offset } from "./list_offset.mjs";
-import { add_1 } from "./add_1.mjs";
+import { subtract_1 } from "./subtract_1.mjs";
 export function list_before(settings_choices, settings) {
-  let after = list_offset(settings_choices, settings, add_1);
+  let after = list_offset(settings_choices, settings, subtract_1);
   return after;
 }
