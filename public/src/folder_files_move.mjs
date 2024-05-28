466328cd2fbf9ee7fef88e7160a09a6418b884c4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 09:13:54 -0400

    2024-05-28T13:13:54.148Z  function_auto folder_files_move

--- public/src/folder_files_move.mjs
+++ public/src/folder_files_move.mjs
@@ -1,7 +1,7 @@
+import { each_async } from "./each_async.mjs";
 import { path_parse_base } from "./path_parse_base.mjs";
 import { file_rename } from "./file_rename.mjs";
 import { path_join } from "./path_join.mjs";
-import { each_index_async } from "./each_index_async.mjs";
 import { folder_read } from "./folder_read.mjs";
 export async function folder_files_move(
   input_directory,
@@ -9,7 +9,7 @@ export async function folder_files_move(
   output_directory,
 ) {
   let file_paths = await folder_read(input_directory, file_extension);
-  await each_index_async(file_paths, async (file_path, index) => {
+  await each_async(file_paths, async (file_path) => {
     let file_name = path_parse_base(file_path);
     let file_path_new = path_join([output_directory, file_name]);
     await file_rename(file_path, file_path_new);
