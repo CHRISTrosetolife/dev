d6021a37d5ba096b2919f87357f1f5dadb1f730f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 09:28:23 -0400

    2024-05-28T13:28:23.145Z  function_auto app_dev_screenshots

--- public/src/app_dev_screenshots.mjs
+++ public/src/app_dev_screenshots.mjs
@@ -1,3 +1,5 @@
-export function app_dev_screenshots(screen_name) {
+import { folder_files_move } from "./folder_files_move.mjs";
+export async function app_dev_screenshots(screen_name) {
   let folder_path = "C:\\Users\\JESUS\\Pictures\\Screenshots";
+  await folder_files_move();
 }
