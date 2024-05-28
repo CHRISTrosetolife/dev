701bae3cdbab40f654699869258f51b4879fa24b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 14:35:58 -0400

    2024-05-28T18:35:57.774Z  data_update

--- public/src/data_update_single.mjs
+++ public/src/data_update_single.mjs
@@ -3,6 +3,7 @@ import { js_node_type } from "./js_node_type.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
+import { list_map_property } from "./list_map_property.mjs";
 export function data_update_single(ast, data) {
   let declaration = js_declaration_single(ast);
   let {
@@ -13,6 +14,7 @@ export function data_update_single(ast, data) {
   let f = object_property_initialize(functions, name, {});
   object_property_set(f, "async", async);
   let literals = js_node_type(ast, "Literal");
-  let lookup = string_count_lookup(literals);
+  let mapped =list_map_property(literals,'value')
+  let lookup = string_count_lookup(mapped);
   object_property_set(f, "literals", lookup);
 }
