a28b5d5abcf2e154e027fec7a067574462539e73
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 13:20:28 -0400

    2024-05-28T17:20:28.567Z  function_copy js_data data_update

--- public/src/data_update.mjs
+++ public/src/data_update.mjs
@@ -1,13 +1,8 @@
-import { object_property_set } from "./object_property_set.mjs";
-import { object_property_initialize } from "./object_property_initialize.mjs";
-import { js_declaration_single } from "./js_declaration_single.mjs";
-export function data_update(ast, data) {
-  let declaration = js_declaration_single(ast);
-  let {
-    async,
-    id: { name },
-  } = declaration;
-  let functions = object_property_initialize(data, "functions", {});
-  let f = object_property_initialize(functions, name, {});
-  object_property_set(f, "async", async);
+import { data_update_single } from "./data_update_single.mjs";
+import { data_path } from "./data_path.mjs";
+import { file_json_transform_exists } from "./file_json_transform_exists.mjs";
+export async function data_update(ast) {
+  await file_json_transform_exists(data_path(), (data) =>
+    data_update_single(ast, data),
+  );
 }
