2b4b3b0eca3cf1fd5bf9210dd8daab558c28c9a2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 09:15:55 -0400

    2024-05-28T13:15:51.764Z  function_auto folder_files_move

--- public/src/folder_files_move.mjs
+++ public/src/folder_files_move.mjs
@@ -1,15 +1,13 @@
-import { each_async } from "./each_async.mjs";
+import { folder_read_each } from "./folder_read_each.mjs";
 import { path_parse_base } from "./path_parse_base.mjs";
 import { file_rename } from "./file_rename.mjs";
 import { path_join } from "./path_join.mjs";
-import { folder_read } from "./folder_read.mjs";
 export async function folder_files_move(
   input_directory,
   file_extension,
   output_directory,
 ) {
-  let file_paths = await folder_read(input_directory, file_extension);
-  await each_async(file_paths, lambda);
+  await folder_read_each(input_directory, file_extension, lambda);
   async function lambda(file_path) {
     let file_name = path_parse_base(file_path);
     let file_path_new = path_join([output_directory, file_name]);
---
+++ public/src/folder_read_each.mjs
@@ -0,0 +1,10 @@
+import { each_async } from "./each_async.mjs";
+import { folder_read } from "./folder_read.mjs";
+export async function folder_read_each(
+  input_directory,
+  file_extension,
+  lambda,
+) {
+  let file_paths = await folder_read(input_directory, file_extension);
+  await each_async(file_paths, lambda);
+}
