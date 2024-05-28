81e34bee1de28301ced06f6a603fe6306b0ba12a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 09:46:43 -0400

    2024-05-28T13:46:42.881Z  function_new_args app_name app_function

--- public/src/app_dev_screenshots.mjs
+++ public/src/app_dev_screenshots.mjs
@@ -1,3 +1,4 @@
+import { app_dev } from "./app_dev.mjs";
 import { folder_files_move } from "./folder_files_move.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function app_dev_screenshots(screen_name) {
@@ -6,6 +7,6 @@ export async function app_dev_screenshots(screen_name) {
   await folder_files_move(
     folder_path,
     file_extension,
-    string_combine_multiple([".\\public\\img\\"]),
+    string_combine_multiple([".\\public\\img\\",app_dev.name]),
   );
 }
