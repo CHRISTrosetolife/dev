6c634bc68abb562ab8fcc7dc508a72d7c62e5cb7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 12:48:27 -0400

    2024-05-28T16:48:26.714Z  function_auto app_dev_screenshots

--- public/src/app_dev_screenshots.mjs
+++ public/src/app_dev_screenshots.mjs
@@ -1,9 +1,11 @@
+import { assert } from "./assert.mjs";
 import { folder_read } from "./folder_read.mjs";
 import { folder_files_rename_incrementing } from "./folder_files_rename_incrementing.mjs";
 import { app_name } from "./app_name.mjs";
 import { app_dev } from "./app_dev.mjs";
 import { folder_files_move } from "./folder_files_move.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { list_empty_is } from "./list_empty_is.mjs";
 export async function app_dev_screenshots(screen_name) {
   let folder_path = "C:\\Users\\JESUS\\Pictures\\Screenshots";
   let file_extension = ".png";
@@ -14,7 +16,8 @@ export async function app_dev_screenshots(screen_name) {
     screen_name,
   ]);
   let target_files = await folder_read(target_path, file_extension);
-  return target_files;
-  folder_files_rename_incrementing;
+  assert(list_empty_is, [target_files]);
+  let starting = 1;
+  folder_files_rename_incrementing(folder_path, file_extension, starting);
   await folder_files_move(folder_path, file_extension, target_path);
 }
