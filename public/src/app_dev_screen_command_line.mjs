c1200a60263aae15732020393d8e7433bc1ec50e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 19:56:15 -0400

    2024-05-28T23:56:14.995Z  function_auto app_dev_screen_command_line

--- public/src/app_dev_screen_command_line.mjs
+++ public/src/app_dev_screen_command_line.mjs
@@ -1,10 +1,10 @@
+import { path_join } from "./path_join.mjs";
 import { app_dev } from "./app_dev.mjs";
 import { app_name } from "./app_name.mjs";
 import { app_dev_screen_prefix } from "./app_dev_screen_prefix.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
 import { html_img } from "./html_img.mjs";
 import { html_span_text } from "./html_span_text.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_dev_screen_command_line() {
   let name = string_prefix_without(
     app_dev_screen_command_line.name,
@@ -15,10 +15,7 @@ export function app_dev_screen_command_line() {
       return html_span_text(root, "command_line");
     },
     screen: function command_line_screen(root) {
-      html_img(
-        root,
-        string_combine_multiple([app_name(app_dev), name, "1.png"]),
-      );
+      html_img(root, path_join([app_name(app_dev), name, "1.png"]));
     },
   };
 }
