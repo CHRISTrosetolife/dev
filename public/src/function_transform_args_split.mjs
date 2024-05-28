e028340ea60ed3d0d7afffe35889721bfc264059
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 13:54:05 -0400

    2024-05-28T17:54:04.938Z  function_auto function_transform_args_split

--- public/src/function_transform_args_split.mjs
+++ public/src/function_transform_args_split.mjs
@@ -1,12 +1,16 @@
 import { file_js_transform } from "./file_js_transform.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { function_import } from "./function_import.mjs";
+import { string_split_comma } from "./string_split_comma.mjs";
 export async function function_transform_args_split(
-  lambda_name,
+  lambda_names,
   function_name,
   args,
 ) {
-  let lambda = await function_import(lambda_name);
+  if (0) {
+    let split = string_split_comma(lambda_names);
+  }
+  let lambda = await function_import(lambda_names);
   let file_path = function_name_to_path(function_name);
   return await file_js_transform(lambda, file_path, args);
 }
