7aef0985c8afb3a2b42bfc49bf6bd8896a21409c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 12:55:48 -0400

    2024-05-28T16:55:48.108Z  function_auto js_data

--- public/src/js_data.mjs
+++ public/src/js_data.mjs
@@ -1,8 +1,6 @@
 import { file_exists } from "./file_exists.mjs";
-import { js_identifiers } from "./js_identifiers.mjs";
-export function js_data(ast) {
-  return js_identifiers(ast);
+export async function js_data(ast) {
   let data_path = "data.json";
-  if (!file_exists(data_path)) {
+  if (!(await file_exists(data_path))) {
   }
 }
