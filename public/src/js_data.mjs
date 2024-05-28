f067979ec2ed07e34074afc6f7744ef2f031427f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 13:03:49 -0400

    2024-05-28T17:03:49.632Z  function_transform js_data js_data

--- public/src/js_data.mjs
+++ public/src/js_data.mjs
@@ -3,22 +3,23 @@ import { file_json_transform } from "./file_json_transform.mjs";
 import { file_write_json } from "./file_write_json.mjs";
 import { file_exists } from "./file_exists.mjs";
 import { log } from "./log.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
+import { object_property_set } from "./object_property_set.mjs";
 export async function js_data(ast) {
   let data_path = "data.json";
   if (!(await file_exists(data_path))) {
     await file_write_json(data_path, {});
   }
   await file_json_transform(
-    () => {
+    (data) => {
       let declaration = js_declaration_single(ast);
       let {
         async,
         id: { name },
       } = declaration;
-      log({
-        name,
-        async,
-      });
+      let functions = object_property_initialize(data, "functions", []);
+      let f = object_property_initialize(functions, name, {});
+      object_property_set(f, "async", async);
     },
     data_path,
     [],
