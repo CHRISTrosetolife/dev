c5250b3a43e1bd1ad6489aba151c90e85960cd46
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-27 20:20:25 -0400

    2024-05-28T00:20:25.248Z  function_auto app_dev

--- public/src/app_dev.mjs
+++ public/src/app_dev.mjs
@@ -1,12 +1,13 @@
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { app_dev_screens } from "./app_dev_screens.mjs";
 import { each } from "./each.mjs";
-import { html_button_text_click } from "./html_button_text_click.mjs";
 import { noop } from "./noop.mjs";
 export function app_dev() {
   let root = html_style_default_initialize();
   let screens = app_dev_screens();
   each(screens, (s) => {
-    return html_button_text_click(root, s.name, noop);
+    let b = html_button_width_full_text_click(root, "", noop);
+    s.name(b);
   });
 }
