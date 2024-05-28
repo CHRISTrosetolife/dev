8d3aeb178583ef711ba43b18cf62de88557ce67c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 19:56:36 -0400

    2024-05-28T23:56:36.349Z  function_auto app_dev_screen_command_line

--- public/src/app_dev_screen_command_line.mjs
+++ public/src/app_dev_screen_command_line.mjs
@@ -1,9 +1,9 @@
+import { html_img_limited_centered } from "./html_img_limited_centered.mjs";
 import { path_join } from "./path_join.mjs";
 import { app_dev } from "./app_dev.mjs";
 import { app_name } from "./app_name.mjs";
 import { app_dev_screen_prefix } from "./app_dev_screen_prefix.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
-import { html_img } from "./html_img.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_command_line() {
   let name = string_prefix_without(
@@ -15,7 +15,10 @@ export function app_dev_screen_command_line() {
       return html_span_text(root, "command_line");
     },
     screen: function command_line_screen(root) {
-      html_img(root, path_join([app_name(app_dev), name, "1.png"]));
+      html_img_limited_centered(
+        root,
+        path_join([app_name(app_dev), name, "1.png"]),
+      );
     },
   };
 }
