d1813e03e45e3990be811c661e8e1cc83b015298
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 14:12:47 -0400

    2024-05-28T18:12:47.429Z  function_auto function_auto_transforms

--- public/src/function_auto_transforms.mjs
+++ public/src/function_auto_transforms.mjs
@@ -1,6 +1,8 @@
+import { js_await_add } from "./js_await_add.mjs";
 import { js_const_to_let } from "./js_const_to_let.mjs";
 import { js_imports_fix } from "./js_imports_fix.mjs";
 import { js_outside_move } from "./js_outside_move.mjs";
 export function function_auto_transforms() {
+  if (0) js_await_add;
   return [js_const_to_let, js_outside_move, js_imports_fix];
 }
