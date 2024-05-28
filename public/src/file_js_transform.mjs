71f68a7511aad56eb63f179150f2b4ff382d6fc3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 14:02:14 -0400

    2024-05-28T18:02:14.147Z  function_auto file_js_transform

--- public/src/file_js_transform.mjs
+++ public/src/file_js_transform.mjs
@@ -4,7 +4,6 @@ import { file_js_unparse } from "./file_js_unparse.mjs";
 import { list_length_1 } from "./list_length_1.mjs";
 import { list_single } from "./list_single.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
-import { log } from "./log.mjs";
 export async function file_js_transform(lambdas, file_path, args) {
   let ast = await file_js_parse(file_path);
   let results = await list_adder_async(async (la) => {
