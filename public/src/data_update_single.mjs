4c048c2fb1923765e1e9367398a56061e434648f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 14:32:28 -0400

    2024-05-28T18:32:27.761Z  function_search count

--- public/src/data_update_single.mjs
+++ public/src/data_update_single.mjs
@@ -1,10 +1,8 @@
-import { list_sort } from "./list_sort.mjs";
 import { js_node_type } from "./js_node_type.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
-import { list_unique } from "./list_unique.mjs";
-import { identity } from "./identity.mjs";
+import { each } from "./each.mjs";
 export function data_update_single(ast, data) {
   let declaration = js_declaration_single(ast);
   let {
@@ -15,7 +13,8 @@ export function data_update_single(ast, data) {
   let f = object_property_initialize(functions, name, {});
   object_property_set(f, "async", async);
   let literals = js_node_type(filtered_n, "Literal");
-  let u = list_unique(literals);
-  list_sort(u, identity);
+  let u = {};
+  each(literals, (l) => object_property_set(u, l, 0));
+  return;
   object_property_set(f, "literals", u);
 }
