8d081d5e718d0f80fddf962d4d73086cab827ff4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 17:16:32 -0400

    2024-05-28T21:16:32.087Z  function_auto functions_string_prefix_to_constant

--- public/src/functions_string_prefix_to_constant.mjs
+++ public/src/functions_string_prefix_to_constant.mjs
@@ -1,5 +1,4 @@
-import { data_path } from "./data_path.mjs";
-import { file_read_json } from "./file_read_json.mjs";
+import { data_functions } from "./data_functions.mjs";
 export async function functions_string_prefix_to_constant() {
-  let data = await file_read_json(data_path());
+  let functions = await data_functions();
 }
