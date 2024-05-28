d4318b9b251c8bc488e1ebb9ff98060ff0a023f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 13:18:51 -0400

    2024-05-28T17:18:51.074Z  function_auto js_data

--- public/src/js_data.mjs
+++ public/src/js_data.mjs
@@ -1,6 +1,8 @@
+import { data_path } from "./data_path.mjs";
 import { data_update } from "./data_update.mjs";
 import { file_json_transform_exists } from "./file_json_transform_exists.mjs";
 export async function js_data(ast) {
-  let data_path = "data.json";
-  await file_json_transform_exists(data_path, (data) => data_update(ast, data));
+  await file_json_transform_exists(data_path(), (data) =>
+    data_update(ast, data),
+  );
 }
