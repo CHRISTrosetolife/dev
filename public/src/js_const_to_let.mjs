3cfe906ef284b21dd41e6d118cad59e775c40a31
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 12:52:22 -0400

    2024-05-28T16:52:21.770Z  function_auto js_const_to_let

--- public/src/js_const_to_let.mjs
+++ public/src/js_const_to_let.mjs
@@ -1,4 +1,6 @@
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_const_to_let(ast) {
-  js_visit_node(ast, "VariableDeclaration", (v) => {});
+  js_visit_node(ast, "VariableDeclaration", (v) => {
+    let { node } = v;
+  });
 }
