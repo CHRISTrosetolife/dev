29b7343b6442290922505167a8075f0c45d2189d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 15:23:54 -0400

    2024-05-29T19:23:53.764Zfunction_auto spawn

--- public/src/spawn.mjs
+++ public/src/spawn.mjs
@@ -2,7 +2,7 @@ import { error } from "./error.mjs";
 import { import_node } from "./import_node.mjs";
 export async function spawn(command, options) {
   let c = await import_node("child_process");
-  let { exec } = c;
+  let { spawn } = c;
   return await new Promise((resolve) => {
     exec(command, options, (error, stdout, stderr) => {
       resolve({
