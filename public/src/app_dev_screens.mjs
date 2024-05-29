35ca75ac6ebce77a10b121d4ca5904c92b9249a6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 22:13:05 -0400

    2024-05-29T02:13:04.789Z  function_search dev

--- public/src/app_dev_screens.mjs
+++ public/src/app_dev_screens.mjs
@@ -4,8 +4,8 @@ import {list_add} from "./list_add.mjs";
 import {marker} from "./marker.mjs";
 export function app_dev_screens() {
     let result = [];
-    list_add(result, app_dev_screen_overview());
-    list_add(result, app_dev_screen_command_line());
+    list_add(result, app_dev_screen_overview);
+    list_add(result, app_dev_screen_command_line);
     marker();
     return result;
 }
