aaaea238a91aeec4929811ca7964579dd0e0e64c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 14:53:48 -0400

    2024-05-29T18:53:47.048Zfunction_auto beep

--- public/src/beep.mjs
+++ public/src/beep.mjs
@@ -1,12 +1,12 @@
-import { log } from "./log.mjs";
 import tone from "tonegenerator";
 import Speaker from "speaker";
 export function beep() {
-  log({
-    b,
+  var tonedata = tone({
+    freq: 440,
+    lengthInSecs: 2.0,
+    volume: 30,
+    rate: 44100,
+    shape: "triangle",
+    Int16Array: true,
   });
-  b();
-  b(2);
-  b(3, 1000);
-  b([1000, 500, 2000]);
 }
