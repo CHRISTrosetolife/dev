8cd54aea28bf177a0c1a85d5b2999213026e92b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 19:55:57 -0400

    2024-05-29T23:55:56.950Zfunction_auto file_js_transform

--- public/src/file_js_transform.mjs
+++ public/src/file_js_transform.mjs
@@ -3,18 +3,14 @@ import { file_js_parse } from "./file_js_parse.mjs";
 import { file_js_unparse } from "./file_js_unparse.mjs";
 import { list_length_1 } from "./list_length_1.mjs";
 import { list_single } from "./list_single.mjs";
-import { list_adder_async } from "./list_adder_async.mjs";
 export async function file_js_transform(lambdas, file_path, args) {
   let ast = await file_js_parse(file_path);
-  let results = await list_adder_async(async (la) => {
-    await each_async(lambdas, async (lambda) => {
-      let result = await lambda(ast, ...args);
-      la(result);
-    });
+  await each_async(lambdas, async (lambda) => {
+    await lambda(ast, ...args);
   });
-  await file_js_unparse(file_path, ast);
-  if (list_length_1(results)) {
-    return list_single(results);
+  let result = await file_js_unparse(file_path, ast);
+  if (list_length_1(result)) {
+    return list_single(result);
   }
-  return results;
+  return result;
 }
