58f552a11bd396ae0888abba8da84153c5cabcc9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 15:23:48 -0400

    2024-05-29T19:23:42.253Z  function_copy command_line_generic spawn

---
+++ public/src/spawn.mjs
@@ -0,0 +1,15 @@
+import { error } from "./error.mjs";
+import { import_node } from "./import_node.mjs";
+export async function spawn(command, options) {
+  let c = await import_node("child_process");
+  let { exec } = c;
+  return await new Promise((resolve) => {
+    exec(command, options, (error, stdout, stderr) => {
+      resolve({
+        error,
+        stdout,
+        stderr,
+      });
+    });
+  });
+}
