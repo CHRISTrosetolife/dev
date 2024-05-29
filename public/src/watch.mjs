5f023655f4284ad28225c271f5eded14e18db813
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 13:47:59 -0400

    2024-05-29T17:47:58.730Z  function_auto_run watch

--- public/src/watch.mjs
+++ public/src/watch.mjs
@@ -7,25 +7,22 @@ import chokidar from "chokidar";
 import { string_replace } from "./string_replace.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { object_property_set } from "./object_property_set.mjs";
-import { object_property_exists } from "./object_property_exists.mjs";
 export async function watch() {
-  let processing = {};
-  let watcher = chokidar
-    .watch(folder_path_src())
-    .on("all", async (event, path) => {
-      if (event === "change") {
-        log("start");
-        path = string_replace(path, "\\", "/");
-        path = string_combine("./", path);
-        if (object_property_exists(processing, path)) {
-          return;
-        }
-        log("continue");
-        object_property_set(processing, path, true);
-        let funcion_name = function_path_to_name(path);
-        await function_auto(funcion_name);
-        object_property_delete(processing, path);
-        log("stop");
-      }
-    });
+  let watcher = start();
+  function start() {
+    return chokidar.watch(folder_path_src()).on("all", on_watch);
+  }
+  async function on_watch(event, path) {
+    if (event === "change") {
+      await watcher.close();
+      log("start");
+      path = string_replace(path, "\\", "/");
+      path = string_combine("./", path);
+      object_property_set(processing, path, true);
+      let funcion_name = function_path_to_name(path);
+      await function_auto(funcion_name);
+      object_property_delete(processing, path);
+      log("stop");
+    }
+  }
 }
