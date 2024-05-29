a9779459832c30658ae6afbd5775a82114005b88
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 20:35:45 -0400

    2024-05-29T00:35:44.761Z  function_auto sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -17,7 +17,7 @@ import { each_object } from "./each_object.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { list_add } from "./list_add.mjs";
 export async function sandbox() {
-  let limit = 75;
+  let limit = 150;
   let skip = 0;
   let group_count = ceb_group_size();
   let { atoms, definitions: definitions_all } =
