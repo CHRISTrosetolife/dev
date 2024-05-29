f1c2446033531d2a1856e4d4bf6ece3376872c53
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 13:55:43 -0400

    2024-05-29T17:55:42.735Z  function_auto watch

--- public/src/watch.mjs
+++ public/src/watch.mjs
@@ -1,3 +1,4 @@
+import { object_property_set } from "./object_property_set.mjs";
 import { log } from "./log.mjs";
 import { function_auto } from "./function_auto.mjs";
 import { function_path_to_name } from "./function_path_to_name.mjs";
@@ -6,6 +7,8 @@ import chokidar from "chokidar";
 import { string_replace } from "./string_replace.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { file_read } from "./file_read.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 export async function watch() {
   let cache = {};
   let watcher = start();
@@ -18,12 +21,19 @@ export async function watch() {
       await watcher.close();
       path = string_replace(path, "\\", "/");
       path = string_combine("./", path);
+      let before = await file_read(path);
+      if (object_property_exists(cache, path)) {
+        if (before === object_property_get(cache, path)) {
+          return;
+        }
+      }
       log({
         path,
       });
-      let current = await file_read(path);
       let funcion_name = function_path_to_name(path);
       await function_auto(funcion_name);
+      let after = await file_read(path);
+      object_property_set(cache, path, after);
       start();
     }
   }
