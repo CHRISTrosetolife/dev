30541ac8a6c15a70d52100b248a3861c6121862f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 20:11:52 -0400

    2024-05-29T00:11:51.710Z  function_auto app_dev_screen_overview

--- public/src/app_dev_screen_overview.mjs
+++ public/src/app_dev_screen_overview.mjs
@@ -1,4 +1,4 @@
-import { html_p_text } from "./html_p_text.mjs";
+import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_overview() {
   return {
@@ -6,10 +6,9 @@ export function app_dev_screen_overview() {
       return html_span_text(root, "overview");
     },
     screen: function overview_screen(root) {
-        html_p_text_multiple(
-        root,
-        ["to keep the programming simple , functions are the basic building block , rather than classes or object-oriented programming"],
-      );
+      html_p_text_multiple(root, [
+        "to keep the programming simple , functions are the basic building block , rather than classes or object-oriented programming",
+      ]);
     },
   };
 }
