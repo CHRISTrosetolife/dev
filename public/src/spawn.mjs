be8e78b9d7a9b69b6fc97fef6b3422878b5c8b2c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 15:24:39 -0400

    2024-05-29T19:24:39.529Zfunction_auto spawn

--- public/src/spawn.mjs
+++ public/src/spawn.mjs
@@ -3,6 +3,9 @@ import { import_node } from "./import_node.mjs";
 export async function spawn() {
   let c = await import_node("child_process");
   let { spawn: s } = c;
+  s("powershell.exe", {
+    detached: true,
+  });
   return await new Promise((resolve) => {
     exec(command, options, (error, stdout, stderr) => {
       resolve({
