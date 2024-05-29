c47ce5287530becd1146f15bfe70f55845bfc3f0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 19:51:33 -0400

    2024-05-29T23:51:32.082Zfunction_auto function_auto

---
+++ public/src/function_auto_return.mjs
@@ -0,0 +1,12 @@
+import { function_data } from "./function_data.mjs";
+import { function_transform } from "./function_transform.mjs";
+import { list_join_comma } from "./list_join_comma.mjs";
+import { list_map_property } from "./list_map_property.mjs";
+import { function_auto_transforms } from "./function_auto_transforms.mjs";
+export async function function_auto_return(function_name) {
+  let transforms = function_auto_transforms();
+  let names = list_map_property(transforms, "name");
+  let joined = list_join_comma(names);
+  await function_transform(joined, function_name);
+  await function_data(function_name);
+}
