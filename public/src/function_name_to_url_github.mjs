6781359101fef8c326ca145309c89900e52e7a1b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-28 20:07:37 -0400

    2024-05-29T00:07:37.028Z  function_auto github_open

---
+++ public/src/function_name_to_url_github.mjs
@@ -0,0 +1,8 @@
+import { string_prefix_without } from "./string_prefix_without.mjs";
+import { function_name_to_path } from "./function_name_to_path.mjs";
+export function function_name_to_url_github(function_name) {
+  let function_path = function_name_to_path(function_name);
+  let trimmed = string_prefix_without(function_path, "./");
+  let url = `https://github.com/CHRISTrosetolife/dev/blob/master/${trimmed}`;
+  return url;
+}
--- public/src/github_open.mjs
+++ public/src/github_open.mjs
@@ -1,9 +1,6 @@
+import { function_name_to_url_github } from "./function_name_to_url_github.mjs";
 import { command_line } from "./command_line.mjs";
-import { string_prefix_without } from "./string_prefix_without.mjs";
-import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function github_open(function_name) {
-  let function_path = function_name_to_path(function_name);
-  let trimmed = string_prefix_without(function_path, "./");
-  let url = `https://github.com/CHRISTrosetolife/dev/blob/master/${trimmed}`;
+  let url = function_name_to_url_github(function_name);
   await command_line(`start msedge ${url}`);
 }
