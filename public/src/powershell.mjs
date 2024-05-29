1fd0b846f982adde3998271e8da25126c46594d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 14:38:58 -0400

    2024-05-29T18:38:55.998Zfunction_auto powershell

--- public/src/powershell.mjs
+++ public/src/powershell.mjs
@@ -1,5 +1,7 @@
 import { command_line_generic } from "./command_line_generic.mjs";
 export async function powershell(command) {
-  let options = {};
+  let options = {
+    shell: "powershell.exe",
+  };
   return await command_line_generic(command, options);
 }
