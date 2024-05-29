84f253fb72f54f7feb729767d45bf3df117a6090
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 13:26:17 -0400

    2024-05-29T17:26:17.136Z  function_auto watch

--- public/src/watch.mjs
+++ public/src/watch.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { function_auto } from "./function_auto.mjs";
 import { function_path_to_name } from "./function_path_to_name.mjs";
 import { folder_path_src } from "./folder_path_src.mjs";
@@ -5,6 +6,9 @@ import chokidar from "chokidar";
 export async function watch() {
   chokidar.watch(folder_path_src()).on("all", async (event, path) => {
     if (event === "change") {
+      log({
+        path,
+      });
       let funcion_name = function_path_to_name(path);
       await function_auto(funcion_name);
     }
