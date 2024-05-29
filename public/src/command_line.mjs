d9ec408e06c7e5c09c89b74a13657d8157a468e3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 14:36:57 -0400

    2024-05-29T18:36:56.974Zfunction_auto command_line

--- public/src/command_line.mjs
+++ public/src/command_line.mjs
@@ -3,17 +3,14 @@ import { import_node } from "./import_node.mjs";
 import { exec } from "child_process";
 export async function command_line(command) {
   let c = await import_node("child_process");
+  let options = {};
   return await new Promise((resolve) => {
-    exec(
-      command,
-      (error, stdout, stderr) => {
-        resolve({
-          error,
-          stdout,
-          stderr,
-        });
-      },
-      {},
-    );
+    exec(command, options, (error, stdout, stderr) => {
+      resolve({
+        error,
+        stdout,
+        stderr,
+      });
+    });
   });
 }
