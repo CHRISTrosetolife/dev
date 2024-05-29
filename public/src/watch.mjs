f9506d08a3be4fe21385b645416d6bda8474a916
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 13:24:44 -0400

    2024-05-29T17:24:44.308Z  function_auto watch

--- public/src/watch.mjs
+++ public/src/watch.mjs
@@ -1,10 +1,12 @@
+import { function_auto } from "./function_auto.mjs";
 import { function_path_to_name } from "./function_path_to_name.mjs";
 import { folder_path_src } from "./folder_path_src.mjs";
 import chokidar from "chokidar";
-export function watch() {
-  chokidar.watch(folder_path_src()).on("all", (event, path) => {
+export async function watch() {
+  chokidar.watch(folder_path_src()).on("all", async (event, path) => {
     if (event === "change") {
       let funcion_name = function_path_to_name(path);
+      await function_auto(funcion_name);
     }
   });
 }
