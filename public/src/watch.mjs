629c85697588b940d56f4528a51854a9c0cd15c3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 13:48:26 -0400

    2024-05-29T17:48:24.469Z  function_auto_run watch

--- public/src/watch.mjs
+++ public/src/watch.mjs
@@ -1,12 +1,10 @@
 import { log } from "./log.mjs";
-import { object_property_delete } from "./object_property_delete.mjs";
 import { function_auto } from "./function_auto.mjs";
 import { function_path_to_name } from "./function_path_to_name.mjs";
 import { folder_path_src } from "./folder_path_src.mjs";
 import chokidar from "chokidar";
 import { string_replace } from "./string_replace.mjs";
 import { string_combine } from "./string_combine.mjs";
-import { object_property_set } from "./object_property_set.mjs";
 export async function watch() {
   let watcher = start();
   function start() {
@@ -18,11 +16,10 @@ export async function watch() {
       log("start");
       path = string_replace(path, "\\", "/");
       path = string_combine("./", path);
-      object_property_set(processing, path, true);
       let funcion_name = function_path_to_name(path);
       await function_auto(funcion_name);
-      object_property_delete(processing, path);
       log("stop");
+      start();
     }
   }
 }
