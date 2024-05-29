c2e0ba05067bef74029706aca423e2ae63dd7345
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 13:22:19 -0400

    2024-05-29T17:22:18.926Z  watch

--- public/src/watch.mjs
+++ public/src/watch.mjs
@@ -2,21 +2,7 @@ import { error } from "./error.mjs";
 import { log } from "./log.mjs";
 import chokidar from "chokidar";
 export function watch() {
-  var watcher = chokidar.watch("file or dir", {
-    ignored: /^\./,
-    persistent: true,
-  });
-  watcher
-    .on("add", function (path) {
-      console.log("File", path, "has been added");
-    })
-    .on("change", function (path) {
-      console.log("File", path, "has been changed");
-    })
-    .on("unlink", function (path) {
-      console.log("File", path, "has been removed");
-    })
-    .on("error", function (error) {
-      console.error("Error happened", error);
-    });
+    chokidar.watch('.').on('all', (event, path) => {
+        console.log(event, path);
+      });
 }
