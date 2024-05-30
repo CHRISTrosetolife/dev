0252fecae30f9c8fa2df61d95fa9c2b73de316b6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 20:13:37 -0400

    2024-05-30T00:13:36.350Z  function_auto_run watch

--- public/src/watch.mjs
+++ public/src/watch.mjs
@@ -28,6 +28,10 @@ export async function watch() {
       let c = object_property_get(cache, path);
       let { contents, processing } = c;
       if (processing) {
+        console.log({
+          path,
+          c,
+        });
         return;
       }
       object_property_set(c, "processing", true);
