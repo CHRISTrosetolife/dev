c852c3186989b2e4711721d567a858fe108afcd4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 16:38:13 -0400

    2024-05-30T20:38:12.423Zfunction_auto_return function_import

--- public/src/function_import.mjs
+++ public/src/function_import.mjs
@@ -1,3 +1,4 @@
+import { function_name_to_file_name } from "./function_name_to_file_name.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_join_empty } from "./list_join_empty.mjs";
 import { folder_current_prefix_combine } from "./folder_current_prefix_combine.mjs";
@@ -17,6 +18,7 @@ export async function function_import(function_name) {
     first_with_extension,
   ]);
   let function_path = folder_current_prefix_combine(function_path_second);
+  function_name_to_file_name(function_name);
   let imported = await import(function_path);
   let imported_function = object_property_get(imported, function_name);
   return imported_function;
