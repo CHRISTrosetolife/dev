119e559c1726c9338a5f797f2fe8061ec8c4e4e3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 11:13:29 -0400

    2024-05-30T15:13:28.618Zfunction_auto_return app_dev_screen_command_line

--- public/src/app_dev_screen_command_line.mjs
+++ public/src/app_dev_screen_command_line.mjs
@@ -111,6 +111,7 @@ export function app_dev_screen_command_line() {
           imported,
         });
         let fn = object_property_get(imported, function_name);
+        await fn(...remaining);
         function run_error(message) {
           app_learn_code_style_code_error(result);
           html_inner_set(result, message);
