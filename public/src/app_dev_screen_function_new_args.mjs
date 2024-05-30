d73b0a8425b489a6637ddd679f8b22acd764f48c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 13:27:59 -0400

    2024-05-30T17:27:59.271Zfunction_auto_return app_dev_screen_function_new_args

--- public/src/app_dev_screen_function_new_args.mjs
+++ public/src/app_dev_screen_function_new_args.mjs
@@ -1,8 +1,11 @@
-import { html_span_text } from "./html_span_text.mjs";
+import { html_style_alternate_monospace_short_span } from "./html_style_alternate_monospace_short_span.mjs";
 export function app_dev_screen_function_new_args() {
   return {
-    name: function function_new_args_name(root) {
-      return html_span_text(root, "function_new_args");
+    name: function function_new_name(root) {
+      return html_style_alternate_monospace_short_span(
+        root,
+        "add new `function` with parameters",
+      );
     },
     screen: function function_new_args_screen(root) {},
   };
