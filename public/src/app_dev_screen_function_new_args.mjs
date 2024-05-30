adbd0e1e4bda23c08a012abf22bd20c877f165d6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 13:34:50 -0400

    2024-05-30T17:34:50.051Zfunction_auto_return app_dev_screen_function_new_args

--- public/src/app_dev_screen_function_new_args.mjs
+++ public/src/app_dev_screen_function_new_args.mjs
@@ -1,5 +1,4 @@
 import { html_style_alternate_monospace_short_multiple } from "./html_style_alternate_monospace_short_multiple.mjs";
-import { app_dev_screen_function_new } from "./app_dev_screen_function_new.mjs";
 import { app_dev_screen_img } from "./app_dev_screen_img.mjs";
 import { app_learn_code_style_code_colored } from "./app_learn_code_style_code_colored.mjs";
 import { noop } from "./noop.mjs";
@@ -26,12 +25,12 @@ export function app_dev_screen_function_new_args() {
         "here is the command :",
       ]);
       let i = 1;
-      app_dev_screen_img(app_dev_screen_function_new, root, i++);
+      app_dev_screen_img(app_dev_screen_function_new_args, root, i++);
       html_p_text_multiple(root, [
         "the parameters should be separated by commas with no spaces",
         "here is the result :",
       ]);
-      app_dev_screen_img(app_dev_screen_function_new, root, i++);
+      app_dev_screen_img(app_dev_screen_function_new_args, root, i++);
     },
   };
 }
