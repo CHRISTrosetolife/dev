387413b07242b8f5e5401494cd3964203eb3c234
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 20:26:35 -0400

    2024-05-30T00:26:33.976Zfunction_auto_return js_assign_to_let

--- public/src/js_assign_to_let.mjs
+++ public/src/js_assign_to_let.mjs
@@ -1,12 +1,13 @@
 import { log } from "./log.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
-import { js_parse } from "./js_parse.mjs";
 export function js_assign_to_let(ast) {
   js_visit_node(ast, "ExpressionStatement", (v) => {
     let { node } = v;
     let { expression } = node;
     let { type, left } = expression;
-    if (type === "AssignmentExpression") {if (left.type==='Identifier'){}
+    if (type === "AssignmentExpression") {
+      if (left.type === "Identifier") {
+      }
       log({
         left,
       });
