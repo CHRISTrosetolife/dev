d957add927abe61b99ea9bfe57ade5c5e48ef84b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 11:06:06 -0400

    2024-05-30T15:06:05.579Zfunction_auto_return function_name_to_path

--- public/src/function_name_to_path.mjs
+++ public/src/function_name_to_path.mjs
@@ -2,6 +2,10 @@ import { function_path_suffix } from "./function_path_suffix.mjs";
 import { function_path_prefix } from "./function_path_prefix.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function function_name_to_path(function_name) {
+  let file_name = string_combine_multiple([
+    function_name,
+    function_path_suffix(),
+  ]);
   let result = string_combine_multiple([
     function_path_prefix(),
     function_name,
