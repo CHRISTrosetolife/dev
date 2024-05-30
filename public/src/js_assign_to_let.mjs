84ca660032c4ceee251f53fa4351c0d1998e8ae0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 20:23:45 -0400

    2024-05-30T00:23:44.166Z  function_transform js_assign_to_let js_assign_to_let

--- public/src/js_assign_to_let.mjs
+++ public/src/js_assign_to_let.mjs
@@ -4,7 +4,7 @@ export function js_assign_to_let(ast) {
   js_visit_node(ast, "ExpressionStatement", (v) => {
     let { node } = v;
     let { expression } = node;
-    let { type,left } = expression;
+    let { type, left } = expression;
     if (type === "AssignmentExpression") {
       log({
         left,
