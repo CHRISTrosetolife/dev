27f84b5a464d1b39634a968bdc6b02c88681db65
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 09:55:08 -0400

    2024-05-30T13:55:08.642Z  function_transform js_assign_to_let js_assign_to_let

--- public/src/js_assign_to_let.mjs
+++ public/src/js_assign_to_let.mjs
@@ -60,10 +60,9 @@ export function js_assign_to_let(ast) {
                   la(m_name);
                 } else if (equal(m_type, "ObjectPattern")) {
                   let { properties } = m;
-                  return;
                   let keys = list_map_property(properties, "key");
                   log({
-                    properties,
+                    keys,
                   });
                 }
               });
