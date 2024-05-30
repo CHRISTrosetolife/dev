9fba2b489ff2aa438b1848c2a081c96b2dd33932
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 11:14:35 -0400

    2024-05-30T15:14:35.624Zfunction_auto_return app_dev_screen_command_line

--- public/src/app_dev_screen_command_line.mjs
+++ public/src/app_dev_screen_command_line.mjs
@@ -72,12 +72,12 @@ export function app_dev_screen_command_line() {
         root,
         "here is a sandbox to try out running a function from the command-line :",
       );
-      let h = html_textarea(root);
-      html_style_width_full(h);
-      html_attribute_set(h, "rows", 2);
-      html_attribute_set(h, "spellcheck", "false");
+      let textarea = html_textarea(root);
+      html_style_width_full(textarea);
+      html_attribute_set(textarea, "rows", 2);
+      html_attribute_set(textarea, "spellcheck", "false");
       html_inner_set(
-        h,
+        textarea,
         string_combine_multiple([prefix, string_combine.name, " left right"]),
       );
       html_button_width_full_text_click(root, "💻 run", run_click);
@@ -89,7 +89,7 @@ export function app_dev_screen_command_line() {
         relative,
       });
       async function run_click() {
-        let text = html_value_get(h);
+        let text = html_value_get(textarea);
         if (string_starts_with_not(text, prefix)) {
           let message = "must begin with : " + string_delimit(prefix);
           run_error(message);
@@ -110,7 +110,7 @@ export function app_dev_screen_command_line() {
         }
         let fn = object_property_get(imported, function_name);
         let result = await fn(...remaining);
-        html_value_set(h, result);
+        html_value_set(textarea, result);
         function run_error(message) {
           app_learn_code_style_code_error(result_component);
           html_inner_set(result_component, message);
