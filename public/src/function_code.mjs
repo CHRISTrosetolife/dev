1d4a618502fb7ca598cf88b7adbf2428394f6978
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 17:29:50 -0400

    2024-05-30T21:29:50.266Zfunction_auto_return function_code

--- public/src/function_code.mjs
+++ public/src/function_code.mjs
@@ -1,5 +1,10 @@
+import { folder_current_prefix_combine } from "./folder_current_prefix_combine.mjs";
 import { function_name_to_file_name } from "./function_name_to_file_name.mjs";
 import { file_read } from "./file_read.mjs";
 export async function function_code(function_name_choice) {
-  return await file_read(function_name_to_file_name(function_name_choice));
+  return await file_read(
+    folder_current_prefix_combine(
+      function_name_to_file_name(function_name_choice),
+    ),
+  );
 }
