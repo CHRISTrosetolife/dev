88c1a2385816914d0088f1f95fdb0f016227caad
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 15:26:24 -0400

    2024-05-30T19:26:24.384Zfunction_auto_return app_dev_screen_git_auto

--- public/src/app_dev_screen_git_auto.mjs
+++ public/src/app_dev_screen_git_auto.mjs
@@ -1,3 +1,4 @@
+import { run } from "./run.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_git_auto() {
   return {
@@ -7,9 +8,11 @@ export function app_dev_screen_git_auto() {
     screen: function git_auto_screen(root) {
       "suppose we want to create a `function` named `greetings_to_the_world";
       "suppose we also want to commit this new `function` to git";
-      "then we can change `run.mjs` to `run_git.mjs` and run `function_new";
-      "suppose we add a body to `greetings_to_the_world";
-      "if `run.mjs` is changed to `run_git.mjs` , then the `function` will be ran and then a git commit will be created :";
+      "then we can change `" +
+        run.name +
+        ".mjs` to `run_git.mjs` and run `function_new";
+      ("suppose we add a body to `greetings_to_the_world");
+      ("if `run.mjs` is changed to `run_git.mjs` , then the `function` will be ran and then a git commit will be created :");
     },
   };
 }
