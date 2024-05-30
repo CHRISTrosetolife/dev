b92c5bd0aa7891d6f5dc680596d6c44943965c0f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 13:19:06 -0400

    2024-05-30T17:19:06.306Zfunction_auto_return html_style_alternate_short_function_name

--- public/src/html_style_alternate_short_function_name.mjs
+++ public/src/html_style_alternate_short_function_name.mjs
@@ -1,12 +1,6 @@
+import { html_style_alternate_short_p } from "./html_style_alternate_short_p.mjs";
 import { html_style_function_name } from "./html_style_function_name.mjs";
 import { noop } from "./noop.mjs";
-import { html_p } from "./html_p.mjs";
-import { html_style_alternate_short } from "./html_style_alternate_short.mjs";
 export function html_style_alternate_short_function_name(root, message) {
-  html_style_alternate_short(
-    root,
-    html_p,
-    [noop, html_style_function_name],
-    message,
-  );
+  html_style_alternate_short_p(root, [noop, html_style_function_name], message);
 }
