c6b2230d5fee76785eddce79fa8c5235847c7f07
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 17:11:56 -0400

    2024-05-30T21:11:52.934Zfunction_auto_return file_open

--- public/src/file_open.mjs
+++ public/src/file_open.mjs
@@ -1,4 +1,6 @@
+import { web_is } from "./web_is.mjs";
 import { command_line } from "./command_line.mjs";
 export async function file_open(file_name) {
+  if (web_is()) return;
   await command_line(`code ${file_name}`);
 }
