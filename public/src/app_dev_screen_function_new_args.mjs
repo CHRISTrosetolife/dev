57765f64a04e9d418ea13055c13c63ed393f5d8e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 13:32:04 -0400

    2024-05-30T17:32:04.300Zfunction_auto_return app_dev_screen_function_new_args

--- public/src/app_dev_screen_function_new_args.mjs
+++ public/src/app_dev_screen_function_new_args.mjs
@@ -1,3 +1,4 @@
+import { html_style_alternate_monospace_short_multiple } from "./html_style_alternate_monospace_short_multiple.mjs";
 import { app_dev_screen_function_new } from "./app_dev_screen_function_new.mjs";
 import { app_dev_screen_img } from "./app_dev_screen_img.mjs";
 import { app_learn_code_style_code_colored } from "./app_learn_code_style_code_colored.mjs";
@@ -19,8 +20,10 @@ export function app_dev_screen_function_new_args() {
         [html_style_function_name, noop, app_learn_code_style_code_colored],
         "function_new` adds a new `function` with parameters",
       );
-      ("suppose we want to add a `function` named `greet_person` with two parameters : `first_name` and `last_name`");
-      ("here is the command :");
+      html_style_alternate_monospace_short_multiple(root, [
+        "suppose we want to add a `function` named `greet_person` with two parameters : `first_name` and `last_name`",
+        "here is the command :",
+      ]);
       let i = 1;
       app_dev_screen_img(app_dev_screen_function_new, root, i++);
     },
