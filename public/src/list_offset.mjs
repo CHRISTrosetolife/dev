153ffa010d398df4a12dcbe9c67b26e31c54ff36
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 21:38:45 -0400

    2024-05-30T01:38:45.194Zfunction_auto_return list_offset

--- public/src/list_offset.mjs
+++ public/src/list_offset.mjs
@@ -1,7 +1,8 @@
 import { list_get } from "./list_get.mjs";
 import { list_index } from "./list_index.mjs";
 export function list_offset(settings_choices, settings, offset) {
+  let fn = list_get;
   let settings_index = list_index(settings_choices, settings);
-  let after = list_get(settings_choices, offset(settings_index));
+  let after = fn(settings_choices, offset(settings_index));
   return after;
 }
