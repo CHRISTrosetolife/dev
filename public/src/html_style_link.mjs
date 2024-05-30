55b4268e0d6e463c05cf29c6552e19a80ccdcf07
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 15:50:07 -0400

    2024-05-30T19:50:06.923Zfunction_auto_return html_style_link

--- public/src/html_style_link.mjs
+++ public/src/html_style_link.mjs
@@ -1,5 +1,4 @@
 import { html_clear_inner_get } from "./html_clear_inner_get.mjs";
-import { html_style_font_color } from "./html_style_font_color.mjs";
 import { app_learn_code_style_code_colored } from "./app_learn_code_style_code_colored.mjs";
 import { html_a } from "./html_a.mjs";
 import { function_name_to_url_github } from "./function_name_to_url_github.mjs";
@@ -8,5 +7,4 @@ export function html_style_link(e) {
   let url = function_name_to_url_github(text);
   let a = html_a(e, text, url);
   app_learn_code_style_code_colored(a);
-  html_style_font_color(a, "darkblue");
 }
