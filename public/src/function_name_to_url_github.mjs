ce742c6d135ddd17a92e53c81f47f1016eafe30f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 15:41:00 -0400

    2024-05-30T19:40:59.908Zfunction_auto_return function_name_to_url_github

--- public/src/function_name_to_url_github.mjs
+++ public/src/function_name_to_url_github.mjs
@@ -4,6 +4,6 @@ import { function_name_to_path } from "./function_name_to_path.mjs";
 export function function_name_to_url_github(function_name) {
   let function_path = function_name_to_path(function_name);
   let trimmed = string_prefix_without(function_path, folder_current_prefix());
-  let url = `${"https://github.com/CHRISTrosetolife/dev"}/blob/master/${trimmed}`;
+  let url = `${"https://github.com/CHRISTrosetolife/dev/"}blob/master/${trimmed}`;
   return url;
 }
