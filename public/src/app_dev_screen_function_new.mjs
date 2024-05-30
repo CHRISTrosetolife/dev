d2f1dfcaa71b17d2e4933aed02e77a71937bdfd3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 13:24:14 -0400

    2024-05-30T17:24:11.997Zfunction_auto_return app_dev_screen_function_new

--- public/src/app_dev_screen_function_new.mjs
+++ public/src/app_dev_screen_function_new.mjs
@@ -1,3 +1,4 @@
+import { html_style_alternate_monospace_short_span } from "./html_style_alternate_monospace_short_span.mjs";
 import { html_style_alternate_short_function_name } from "./html_style_alternate_short_function_name.mjs";
 import { html_style_alternate_monospace_short_multiple } from "./html_style_alternate_monospace_short_multiple.mjs";
 import { app_learn_code_style_code_colored } from "./app_learn_code_style_code_colored.mjs";
@@ -5,11 +6,13 @@ import { html_style_function_name } from "./html_style_function_name.mjs";
 import { noop } from "./noop.mjs";
 import { html_style_alternate_short_p } from "./html_style_alternate_short_p.mjs";
 import { app_dev_screen_img } from "./app_dev_screen_img.mjs";
-import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_function_new() {
   return {
     name: function function_new_name(root) {
-      return html_span_text(root, "`function` that adds a new `function`");
+      return html_style_alternate_monospace_short_span(
+        root,
+        "`function` that adds a new `function`",
+      );
     },
     screen: function function_new_screen(root) {
       html_style_alternate_short_p(
