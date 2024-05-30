bc318aa56f910d4d324e6c9beb6a68321bf9d656
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 12:47:16 -0400

    2024-05-30T16:47:16.484Zfunction_auto_return app_dev_screen_git_auto

--- public/src/app_dev_screen_git_auto.mjs
+++ public/src/app_dev_screen_git_auto.mjs
@@ -2,7 +2,7 @@ import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_git_auto() {
   return {
     name: function git_auto_name(root) {
-      return html_span_text(root, "git_auto");
+      return html_span_text(root, "automatically commit to git");
     },
     screen: function git_auto_screen(root) {},
   };
