027a93faf48372b930fceb6caacc5964b38b157c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 15:37:15 -0400

    2024-05-30T19:37:14.710Zfunction_auto_return js_code_format

--- public/src/js_code_format.mjs
+++ public/src/js_code_format.mjs
@@ -1,6 +1,6 @@
 import { import_node } from "./import_node.mjs";
 export async function js_code_format(code) {
-  let pretter = await import_node("prettier");
+  let prettier = await import_node("prettier");
   return await prettier.format(code, {
     parser: "babel",
   });
