8679170ccffbeb47c2ba023dec28f75c93b31e04
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 16:55:03 -0400

    2024-05-30T20:55:02.927Zfunction_auto_return function_import

--- public/src/function_import.mjs
+++ public/src/function_import.mjs
@@ -1,7 +1,8 @@
 import { function_name_to_file_name } from "./function_name_to_file_name.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function function_import(function_name) {
-  let function_path = function_name_to_file_name(function_name);
+  let function_file_name = function_name_to_file_name(function_name);
+  let function_path = function_file_name;
   let imported = await import(function_path);
   let imported_function = object_property_get(imported, function_name);
   return imported_function;
