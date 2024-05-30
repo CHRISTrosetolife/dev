0e55c3daed8545edb5ec502d9c8ad2c4fde9da3c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 15:50:15 -0400

    2024-05-30T19:50:14.618Zfunction_auto_return html_style_link

--- public/src/html_style_link.mjs
+++ public/src/html_style_link.mjs
@@ -1,8 +1,6 @@
 import { html_clear_inner_get } from "./html_clear_inner_get.mjs";
 import { html_a } from "./html_a.mjs";
-import { function_name_to_url_github } from "./function_name_to_url_github.mjs";
-export function html_style_link(e) {
+export function html_style_link(e, url) {
   let text = html_clear_inner_get(e);
-  let url = function_name_to_url_github(text);
   let a = html_a(e, text, url);
 }
