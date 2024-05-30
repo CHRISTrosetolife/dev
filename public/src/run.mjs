a7df643a66ed299eb3c992fbd329ef218a87604c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 09:47:39 -0400

    2024-05-30T13:47:39.055Zfunction_auto_return run

--- public/src/run.mjs
+++ public/src/run.mjs
@@ -3,8 +3,8 @@ import { list_first } from "./list_first.mjs";
 import { list_skip } from "./list_skip.mjs";
 import { process_argv_remaining } from "./process_argv_remaining.mjs";
 export async function run() {
-  let remaining = process_argv_remaining();
-  let first = list_first(remaining);
-  let args = list_skip(remaining, 1);
-  return await function_run(first, args);
+  let r = process_argv_remaining();
+  let first = list_first(r);
+  let remaining = list_skip(r, 1);
+  return await function_run(first, remaining);
 }
