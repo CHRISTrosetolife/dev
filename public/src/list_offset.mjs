f26996fbf0c57ae1f60cd079c245878b3c119bce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 21:42:19 -0400

    2024-05-30T01:42:18.466Zfunction_auto_return list_offset

--- public/src/list_offset.mjs
+++ public/src/list_offset.mjs
@@ -1,6 +1,6 @@
 import { list_offset_generic } from "./list_offset_generic.mjs";
 import { list_get } from "./list_get.mjs";
 export function list_offset(list, item, offset) {
-  let fn = (list, index, default_value) => list_get(list, index);
+  let fn = list_get;
   return list_offset_generic(fn, list, item, offset);
 }
