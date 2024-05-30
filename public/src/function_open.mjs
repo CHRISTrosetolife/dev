0f2fba777f82e6921dca41c60bc7e2ee986a4ae4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 17:11:34 -0400

    2024-05-30T21:11:33.881Zfunction_auto_return function_open

--- public/src/function_open.mjs
+++ public/src/function_open.mjs
@@ -1,10 +1,8 @@
-import { web_is } from "./web_is.mjs";
 import { assert_async } from "./assert_async.mjs";
 import { file_exists } from "./file_exists.mjs";
 import { file_open } from "./file_open.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function function_open(function_name) {
-  if (web_is()) return;
   let file_path = function_name_to_path(function_name);
   await assert_async(file_exists, [file_path]);
   await file_open(file_path);
