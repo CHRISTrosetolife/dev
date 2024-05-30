d52d8f5095141cc66b229e9718216c434554e6db
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 12:52:36 -0400

    2024-05-30T16:52:34.356Zfunction_auto_return app_dev_screen_git_auto

--- public/src/app_dev_screen_git_auto.mjs
+++ public/src/app_dev_screen_git_auto.mjs
@@ -4,6 +4,8 @@ export function app_dev_screen_git_auto() {
     name: function git_auto_name(root) {
       return html_span_text(root, "automatically commit to git");
     },
-    screen: function git_auto_screen(root) {},
+    screen: function git_auto_screen(root) {
+      "if `run.mjs` is changed to `run_git.mjs` , then the `function` will be ran and then a git commit will be created :";
+    },
   };
 }
