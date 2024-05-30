948671ec33d8879163dd4c5933cfa3e28ca7828e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 16:36:20 -0400

    2024-05-30T20:36:19.789Z  watch

--- public/src/function_import.mjs
+++ public/src/function_import.mjs
@@ -7,10 +7,8 @@ export async function function_import(function_name) {
   let extension = function_extension();
   let extension_marker = ".";
   let folder_current = ".";
-  if (0) list_join_empty;
-  let first_with_extension = [function_name, extension_marker, extension].join(
-    "",
-  );
+  if (0) ;
+  let first_with_extension = list_join_empty([function_name, extension_marker, extension])
   let function_path_second = path_join([folder_current, first_with_extension]);
   let function_path = folder_current_prefix_combine(function_path_second);
   let imported = await import(function_path);
