3711d8246d0565478270ce0f2059fbf8114fc728
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 20:10:44 -0400

    2024-05-30T00:10:44.013Zfunction_auto_return js_assign_to_let

--- public/src/js_assign_to_let.mjs
+++ public/src/js_assign_to_let.mjs
@@ -1,5 +1,6 @@
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_assign_to_let(ast) {
+  a = 1;
   js_visit_node(ast, "ExpressionStatement", (v) => {
     let { node } = v;
   });
