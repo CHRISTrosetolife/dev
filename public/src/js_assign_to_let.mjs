775c54eee76aaaae9d173f49e2861f75562a76b7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 20:29:31 -0400

    2024-05-30T00:29:31.229Zfunction_auto_return js_assign_to_let

--- public/src/js_assign_to_let.mjs
+++ public/src/js_assign_to_let.mjs
@@ -1,6 +1,7 @@
 import { js_code_declare_assign } from "./js_code_declare_assign.mjs";
 import { log } from "./log.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
+import { list_single } from "./list_single.mjs";
 export function js_assign_to_let(ast) {
   js_visit_node(ast, "ExpressionStatement", (v) => {
     let { node } = v;
@@ -10,6 +11,8 @@ export function js_assign_to_let(ast) {
       if (left.type === "Identifier") {
         let { name } = left;
         let parsed = js_code_declare_assign(name);
+        let { declarations } = parsed;
+        let d = list_single(parsed);
         log({
           parsed,
         });
