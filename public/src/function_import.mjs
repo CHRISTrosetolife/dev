e711a32407eb558ddee760b3744f8546661456be
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 11:11:02 -0400

    2024-05-30T15:11:00.315Zfunction_auto_return folder_current_prefix_combine

--- public/src/function_import.mjs
+++ public/src/function_import.mjs
@@ -1,7 +1,7 @@
+import { folder_current_prefix_combine } from "./folder_current_prefix_combine.mjs";
 import { path_join } from "./path_join.mjs";
 import { folder_current } from "./folder_current.mjs";
 import { function_extension } from "./function_extension.mjs";
-import { folder_current_prefix } from "./folder_current_prefix.mjs";
 export async function function_import(function_name) {
   let extension = function_extension();
   let extension_marker = ".";
@@ -10,7 +10,7 @@ export async function function_import(function_name) {
     "",
   );
   let function_path_second = path_join([folder_current, first_with_extension]);
-  let function_path = folder_current_prefix() + function_path_second;
+  let function_path = folder_current_prefix_combine(function_path_second);
   let imported = await import(function_path);
   let imported_function = imported[function_name];
   return imported_function;
