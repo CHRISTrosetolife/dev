2445236bf8f5c8c16a275e9de121a34435f9fd96
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 13:06:35 -0400

    2024-05-30T17:06:34.637Zfunction_auto_return app_dev_screen_function_new

--- public/src/app_dev_screen_function_new.mjs
+++ public/src/app_dev_screen_function_new.mjs
@@ -2,7 +2,7 @@ import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_function_new() {
   return {
     name: function function_new_name(root) {
-      return html_span_text(root, "function_new");
+      return html_span_text(root, "`function` that adds a new `function`");
     },
     screen: function function_new_screen(root) {},
   };
