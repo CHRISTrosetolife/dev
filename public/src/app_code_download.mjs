cd2993422175408f4fd6d6d65e29aebce8d2f981
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 17:02:11 -0400

    2024-05-30T21:02:11.297Zfunction_auto_return app_code_download

--- public/src/app_code_download.mjs
+++ public/src/app_code_download.mjs
@@ -1,8 +1,7 @@
-import { global_get } from "./global_get.mjs";
-import { object_property_set } from "./object_property_set.mjs";
+import { global_files_initialize } from "./global_files_initialize.mjs";
 import { functions_source_get } from "./functions_source_get.mjs";
 import { function_run } from "./function_run.mjs";
 export async function app_code_download() {
   let files = await function_run(functions_source_get.name, []);
-  object_property_set(global_get(), "files", files);
+  global_files_initialize(files);
 }
