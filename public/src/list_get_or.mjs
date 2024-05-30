815f1058625913246c206a6b3cd7825839594b1a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 21:37:36 -0400

    2024-05-30T01:37:34.360Z  function_copy list_get_or_last list_get_or

---
+++ public/src/list_get_or.mjs
@@ -0,0 +1,6 @@
+import { list_get_or_generic } from "./list_get_or_generic.mjs";
+import { list_last } from "./list_last.mjs";
+export function list_get_or(list, index) {
+  let fn = list_last;
+  return list_get_or_generic(fn, list, index);
+}
