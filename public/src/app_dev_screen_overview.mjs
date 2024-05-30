fcbff742d82840a89989ecc450f1d5ed1091e4a6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 16:02:18 -0400

    2024-05-30T20:02:17.961Zfunction_auto_return app_dev_screen_overview

--- public/src/app_dev_screen_overview.mjs
+++ public/src/app_dev_screen_overview.mjs
@@ -8,6 +8,7 @@ export function app_dev_screen_overview() {
     screen: function overview_screen(root) {
       html_style_alternate_monospace_short_multiple(root, [
         "to keep the programming simple , `function`s are the basic building block , rather than classes or object-oriented programming",
+        "one goal was that a recent college or bootcamp graduate could get started quickly",
       ]);
     },
   };
