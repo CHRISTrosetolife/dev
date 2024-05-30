976098a791e1ea354543dd355e5ef09acd67b544
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 21:42:51 -0400

    2024-05-30T01:42:50.684Zfunction_auto_return list_after_or

--- public/src/list_after_or.mjs
+++ public/src/list_after_or.mjs
@@ -2,9 +2,9 @@ import { list_offset_generic } from "./list_offset_generic.mjs";
 import { list_get_or } from "./list_get_or.mjs";
 import { list_offset } from "./list_offset.mjs";
 import { add_1 } from "./add_1.mjs";
-export function list_after_or(settings_choices, settings, default_value) {
+export function list_after_or(list, item, default_value) {
   let fn = (list, index) => list_get_or(list, index, default_value);
   return list_offset_generic(list_get_or, list, item, offset);
-  let after = list_offset(settings_choices, settings, add_1);
+  let after = list_offset(list, item, add_1);
   return after;
 }
