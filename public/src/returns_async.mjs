0eb60d045cc479be0972327886630ad8c58444ef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 17:36:13 -0400

    2024-05-30T21:36:12.554Zfunction_auto_return returns_async

--- public/src/returns_async.mjs
+++ public/src/returns_async.mjs
@@ -1,6 +1,8 @@
+import { assert_message_error } from "./assert_message_error.mjs";
 import { assert_boolean } from "./assert_boolean.mjs";
 import { equal } from "./equal.mjs";
 export async function returns_async(fn, expected, args) {
+  let message = assert_message_error();
   let actual = await fn(...args);
   let e = equal(actual, expected);
   assert_boolean(e);
