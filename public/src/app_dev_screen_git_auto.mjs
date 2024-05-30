e51f0b963fec2e840f1395fddd64001bff6d92f8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 15:28:18 -0400

    2024-05-30T19:28:17.767Zfunction_auto_return app_dev_screen_git_auto

--- public/src/app_dev_screen_git_auto.mjs
+++ public/src/app_dev_screen_git_auto.mjs
@@ -1,3 +1,4 @@
+import { html_style_alternate_monospace_short_multiple } from "./html_style_alternate_monospace_short_multiple.mjs";
 import { run_git } from "./run_git.mjs";
 import { run } from "./run.mjs";
 import { html_span_text } from "./html_span_text.mjs";
@@ -8,16 +9,17 @@ export function app_dev_screen_git_auto() {
       return html_span_text(root, "automatically commit to git");
     },
     screen: function git_auto_screen(root) {
-      "suppose we want to create a `function` named `greetings_to_the_world";
-      "suppose we also want to commit this new `function` to git";
-      "then we can change `" +
-        run.name +
-        ".mjs` to `" +
-        run_git.name +
-        ".mjs` and run `" +
-        function_new.name +
-        "` :";
-      ("suppose we add a body to `greetings_to_the_world");
+      html_style_alternate_monospace_short_multiple(root, [
+        "suppose we want to create a `function` named `greetings_to_the_world",
+        "suppose we also want to commit this new `function` to git",
+        "then we can change `" +
+          run.name +
+          ".mjs` to `" +
+          run_git.name +
+          ".mjs` and run `" +
+          function_new.name +
+          "` :",
+      ])("suppose we add a body to `greetings_to_the_world");
       "if `" +
         run.name +
         ".mjs` is changed to `" +
