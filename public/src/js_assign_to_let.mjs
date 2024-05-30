f2aac2b6e6f3fe1abd675d3f6d654e5d6f11fd09
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 20:11:29 -0400

    2024-05-30T00:11:28.881Zfunction_auto_return js_assign_to_let

--- public/src/js_assign_to_let.mjs
+++ public/src/js_assign_to_let.mjs
@@ -1,7 +1,11 @@
+import { log } from "./log.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_assign_to_let(ast) {
   js_visit_node(ast, "ExpressionStatement", (v) => {
-    let { node } = v;log({node})//
+    let { node } = v;
+    log({
+      node,
+    });
   });
   if (0) a = 1;
 }
