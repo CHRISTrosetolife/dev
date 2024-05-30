13a5dffcbb5ab3ed3e159f0244dd9206ad9c8da5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 16:01:03 -0400

    2024-05-30T20:01:02.533Zfunction_auto_return app_dev_screen_overview

--- public/src/app_dev_screen_overview.mjs
+++ public/src/app_dev_screen_overview.mjs
@@ -1,4 +1,4 @@
-import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
+import { html_style_alternate_monospace_short_multiple } from "./html_style_alternate_monospace_short_multiple.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_overview() {
   return {
@@ -6,7 +6,7 @@ export function app_dev_screen_overview() {
       return html_span_text(root, "overview");
     },
     screen: function overview_screen(root) {
-      html_p_text_multiple(root, [
+      html_style_alternate_monospace_short_multiple(root, [
         "to keep the programming simple , functions are the basic building block , rather than classes or object-oriented programming",
       ]);
     },
