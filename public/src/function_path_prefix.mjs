cfe64e45fe5d8a403825245c3724e3102e10f90a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 17:31:31 -0400

    2024-05-30T21:31:31.424Zfunction_auto_return function_path_prefix

--- public/src/function_path_prefix.mjs
+++ public/src/function_path_prefix.mjs
@@ -1,4 +1,5 @@
+import { folder_current_prefix } from "./folder_current_prefix.mjs";
 import { folder_path_src } from "./folder_path_src.mjs";
 export function function_path_prefix() {
-  return `./${folder_path_src()}`;
+  return `${folder_current_prefix()}${folder_path_src()}`;
 }
