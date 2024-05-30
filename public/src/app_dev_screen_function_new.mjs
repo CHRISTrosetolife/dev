f448d2e4f473a55e508a59300dce60d393d405e9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 13:19:31 -0400

    2024-05-30T17:19:31.190Zfunction_auto_return app_dev_screen_function_new

--- public/src/app_dev_screen_function_new.mjs
+++ public/src/app_dev_screen_function_new.mjs
@@ -1,4 +1,6 @@
-import { html_style_alternate_short_function_name } from "./html_style_alternate_short_function_name.mjs";
+import { html_style_function_name } from "./html_style_function_name.mjs";
+import { noop } from "./noop.mjs";
+import { html_style_alternate_short_p } from "./html_style_alternate_short_p.mjs";
 import { app_dev_screen_img } from "./app_dev_screen_img.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_function_new() {
@@ -7,8 +9,9 @@ export function app_dev_screen_function_new() {
       return html_span_text(root, "`function` that adds a new `function`");
     },
     screen: function function_new_screen(root) {
-      html_style_alternate_short_function_name(
+      html_style_alternate_short_p(
         root,
+        [noop, html_style_function_name],
         "`function_new` adds a new `function`",
       );
       let i = 1;
