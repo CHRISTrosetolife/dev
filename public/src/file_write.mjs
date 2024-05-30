eb8ea47f8129e505fcb2dc57b614c4bd6319ef0f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 17:38:21 -0400

    2024-05-30T21:38:20.001Zfunction_auto_return file_write

--- public/src/file_write.mjs
+++ public/src/file_write.mjs
@@ -1,7 +1,12 @@
-import { assert_not_async } from "./assert_not_async.mjs";
+import { assert_not_message_async } from "./assert_not_message_async.mjs";
 import { file_exists } from "./file_exists.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function file_write(file_name, data) {
-  await assert_not_async(file_exists, [file_name]);
+  await assert_not_message_async(
+    file_exists,
+    [file_name],
+    string_combine_multiple(["file already exists"]),
+  );
   return await file_overwrite(file_name, data);
 }
