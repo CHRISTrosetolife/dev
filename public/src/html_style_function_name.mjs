01a6858b834c1144511e549d970e6c4a6461bf75
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 15:49:04 -0400

    2024-05-30T19:49:04.362Zfunction_auto_return html_style_function_name

--- public/src/html_style_function_name.mjs
+++ public/src/html_style_function_name.mjs
@@ -1,4 +1,3 @@
-import { html_style_font_color } from "./html_style_font_color.mjs";
 import { app_learn_code_style_code_colored } from "./app_learn_code_style_code_colored.mjs";
 import { html_a } from "./html_a.mjs";
 import { function_name_to_url_github } from "./function_name_to_url_github.mjs";
@@ -10,5 +9,4 @@ export function html_style_function_name(e) {
   let url = function_name_to_url_github(text);
   let a = html_a(e, text, url);
   app_learn_code_style_code_colored(a);
-  html_style_font_color(a, "darkblue");
 }
