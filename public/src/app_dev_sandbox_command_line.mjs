a4afcca4362e5596af7f68384390c5ee4abaed10
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 16:32:15 -0400

    2024-05-30T20:32:14.882Zfunction_auto_return app_dev_sandbox_command_line

--- public/src/app_dev_sandbox_command_line.mjs
+++ public/src/app_dev_sandbox_command_line.mjs
@@ -55,6 +55,7 @@ export async function app_dev_sandbox_command_line(
     let without = string_prefix_without(text, prefix);
     let parts = string_split_space(without);
     let { first: function_name, remaining } = list_first_remaining(parts);
+    let on_error = run_error;
     let file_name = folder_current_prefix_combine(
       function_name_to_file_name(function_name),
     );
