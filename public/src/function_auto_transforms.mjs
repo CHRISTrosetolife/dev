8ac9273e5a191277bff0e79be2d947adf787ea71
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-29 20:36:11 -0400

    2024-05-30T00:36:11.136Zfunction_auto_return function_auto_transforms

--- public/src/function_auto_transforms.mjs
+++ public/src/function_auto_transforms.mjs
@@ -1,7 +1,14 @@
+import { js_assign_to_let } from "./js_assign_to_let.mjs";
 import { js_await_add } from "./js_await_add.mjs";
 import { js_const_to_let } from "./js_const_to_let.mjs";
 import { js_imports_fix } from "./js_imports_fix.mjs";
 import { js_outside_move } from "./js_outside_move.mjs";
 export function function_auto_transforms() {
-  return [js_const_to_let, js_await_add, js_outside_move, js_imports_fix];
+  return [
+    js_const_to_let,
+    js_assign_to_let,
+    js_await_add,
+    js_outside_move,
+    js_imports_fix,
+  ];
 }
