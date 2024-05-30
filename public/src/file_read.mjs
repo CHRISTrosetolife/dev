583aec2b78d3f585e370d825a5ab2f3bbec94d2c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 17:32:35 -0400

    2024-05-30T21:32:32.238Zfunction_auto_return file_read

--- public/src/file_read.mjs
+++ public/src/file_read.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { global_get } from "./global_get.mjs";
 import { import_node } from "./import_node.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -7,10 +6,6 @@ export async function file_read(file_name) {
   if (web_is()) {
     let g = global_get();
     let { files } = g;
-    log({
-      file_name,
-      files,
-    });
     return object_property_get(files, file_name);
   }
   let fs = await import_node("fs");
