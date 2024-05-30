a14f0c2d9662817da0b6f279bc2f8b5ce4b5d018
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 15:50:38 -0400

    2024-05-30T19:50:38.266Zfunction_auto_return html_style_link

--- public/src/html_style_link.mjs
+++ public/src/html_style_link.mjs
@@ -1,6 +1,8 @@
 import { html_clear_inner_get } from "./html_clear_inner_get.mjs";
 import { html_a } from "./html_a.mjs";
-export function html_style_link(e, url) {
-  let text = html_clear_inner_get(e);
-  let a = html_a(e, text, url);
+export function html_style_link(url) {
+  return (e) => {
+    let text = html_clear_inner_get(e);
+    html_a(e, text, url);
+  };
 }
