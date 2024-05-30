7edaa65ea4e8a93e65f6171f5c5a5b18348559d8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 08:34:16 -0400

    2024-05-30T12:34:16.214Zfunction_auto_return js_assign_to_let

--- public/src/js_assign_to_let.mjs
+++ public/src/js_assign_to_let.mjs
@@ -42,11 +42,11 @@ export function js_assign_to_let(ast) {
             list_adder((la) =>
               each(filtered, (f) => {
                 let { declarations } = f;
+                log({
+                  filtered,
+                });
               }),
             );
-            log({
-              filtered,
-            });
           }
         });
         log(js_unparse(parsed));
