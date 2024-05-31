0688dddba143a0de0fcefd8195a4fa9e547f3d2e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 08:36:02 -0400

    2024-05-31T12:36:01.608Zfunction_auto_return app_dev_screen_function_new

--- public/src/app_dev_screen_function_new.mjs
+++ public/src/app_dev_screen_function_new.mjs
@@ -1,5 +1,5 @@
+import { app_dev_sandbox_command_line_result_remaining_first_function } from "./app_dev_sandbox_command_line_result_remaining_first_function.mjs";
 import { funcion_name_new } from "./funcion_name_new.mjs";
-import { function_code } from "./function_code.mjs";
 import { app_dev_sandbox_command_line } from "./app_dev_sandbox_command_line.mjs";
 import { function_new } from "./function_new.mjs";
 import { html_style_alternate_monospace_short_span } from "./html_style_alternate_monospace_short_span.mjs";
@@ -10,7 +10,6 @@ import { html_style_function_name } from "./html_style_function_name.mjs";
 import { noop } from "./noop.mjs";
 import { html_style_alternate_short_p } from "./html_style_alternate_short_p.mjs";
 import { app_dev_screen_img } from "./app_dev_screen_img.mjs";
-import { list_first } from "./list_first.mjs";
 export function app_dev_screen_function_new() {
   return {
     name: function function_new_name(root) {
