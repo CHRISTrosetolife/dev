c28507ce3ad967f18f1d208b90e1063c5279f646
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 14:48:42 -0400

    2024-05-31T18:48:41.523Zfunction_auto_return app_dev_sandbox_result_show

--- public/src/app_dev_sandbox_result.mjs
+++ public/src/app_dev_sandbox_result.mjs
@@ -9,18 +9,15 @@ import { object_merge } from "./object_merge.mjs";
 export function app_dev_sandbox_result(root) {
   let result_component = app_learn_code_code_part_contrast(root, "");
   html_style_display_none(result_component);
-  object_merge(result_component, {
-    show: function app_dev_sandbox_result_show() {
-      html_style_display_block(result_component);
-      app_learn_code_code_background_set(result_component);
-    },
-    error: function app_dev_sandbox_result_error(message) {
-      app_learn_code_style_code_error(result_component);
-      html_inner_set(result_component, message);
-    },
-    text_set: function app_dev_sandbox_result_text_set(text) {
-      html_inner_set(result_component, text);
-    },
-  });
   return result_component;
-}
+}     function app_dev_sandbox_result_show() {
+    html_style_display_block(result_component);
+    app_learn_code_code_background_set(result_component);
+  }
+   function app_dev_sandbox_result_error(message) {
+    app_learn_code_style_code_error(result_component);
+    html_inner_set(result_component, message);
+  }
+   function app_dev_sandbox_result_text_set(text) {
+    html_inner_set(result_component, text);
+  }
---
+++ public/src/app_dev_sandbox_result_text_set.mjs
@@ -0,0 +1,4 @@
+import { html_inner_set } from "./html_inner_set.mjs";
+export function app_dev_sandbox_result_text_set(text) {
+  html_inner_set(result_component, text);
+}
