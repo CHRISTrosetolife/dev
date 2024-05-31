9d203d72309ea29c35ab73dbbdbbea090c6c36d9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 14:45:03 -0400

    2024-05-31T18:45:02.392Zfunction_auto_return app_dev_sandbox_command_line

--- public/src/app_dev_sandbox_command_line.mjs
+++ public/src/app_dev_sandbox_command_line.mjs
@@ -4,7 +4,6 @@ import { app_dev_sandbox_message } from "./app_dev_sandbox_message.mjs";
 import { html_textarea_code } from "./html_textarea_code.mjs";
 import { function_import } from "./function_import.mjs";
 import { html_style_alternate_monospace_short } from "./html_style_alternate_monospace_short.mjs";
-import { app_learn_code_style_code_error } from "./app_learn_code_style_code_error.mjs";
 import { list_first_remaining } from "./list_first_remaining.mjs";
 import { string_split_space } from "./string_split_space.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
@@ -76,9 +75,5 @@ export async function app_dev_sandbox_command_line(
     }
     let result = await result_get(fn_result, remaining);
     html_inner_set(result_component, result);
-    function run_error(message) {
-      app_learn_code_style_code_error(result_component);
-      html_inner_set(result_component, message);
-    }
   }
 }
