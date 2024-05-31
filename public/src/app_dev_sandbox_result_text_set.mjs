a8d0d5e58fbbc0588ca39ce7baf37cefc9a342a2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 14:50:56 -0400

    2024-05-31T18:50:55.234Zfunction_auto_return app_dev_sandbox_result_text_set

--- public/src/app_dev_sandbox_result_text_set.mjs
+++ public/src/app_dev_sandbox_result_text_set.mjs
@@ -1,4 +1,4 @@
 import { html_inner_set } from "./html_inner_set.mjs";
-export function app_dev_sandbox_result_text_set(text) {
+export function app_dev_sandbox_result_text_set(result_component, text) {
   html_inner_set(result_component, text);
 }
