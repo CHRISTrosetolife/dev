5c0186f7d434cc9de891a1997989412390262519
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 14:50:45 -0400

    2024-05-31T18:50:44.957Zfunction_auto_return app_dev_sandbox_result_error

--- public/src/app_dev_sandbox_result_error.mjs
+++ public/src/app_dev_sandbox_result_error.mjs
@@ -1,6 +1,6 @@
 import { html_inner_set } from "./html_inner_set.mjs";
 import { app_learn_code_style_code_error } from "./app_learn_code_style_code_error.mjs";
-export function app_dev_sandbox_result_error(message) {
+export function app_dev_sandbox_result_error(result_component, message) {
   app_learn_code_style_code_error(result_component);
   html_inner_set(result_component, message);
 }
