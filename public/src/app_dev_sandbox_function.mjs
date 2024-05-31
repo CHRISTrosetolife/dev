fcb68d42d72b22b970cb3beb6733949cfbeb5cfd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 14:33:49 -0400

    2024-05-31T18:33:47.844Zfunction_auto_return app_dev_sandbox_function

--- public/src/app_dev_sandbox_function.mjs
+++ public/src/app_dev_sandbox_function.mjs
@@ -1,1 +1,19 @@
-export function app_dev_sandbox_function() {}
+import { html_attribute_set } from "./html_attribute_set.mjs";
+import { html_textarea_code } from "./html_textarea_code.mjs";
+import { app_dev_sandbox_message } from "./app_dev_sandbox_message.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { html_style_alternate_monospace_short } from "./html_style_alternate_monospace_short.mjs";
+import { html_hr } from "./html_hr.mjs";
+export function app_dev_sandbox_function() {
+  html_hr(root);
+  html_style_alternate_monospace_short(
+    root,
+    string_combine_multiple([
+      app_dev_sandbox_message(),
+      try_out_message,
+      " from the command-line :",
+    ]),
+  );
+  let textarea = html_textarea_code(root);
+  html_attribute_set(textarea, "rows", 2);
+}
