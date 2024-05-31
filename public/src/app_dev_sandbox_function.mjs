37247c33fdd90c0ae257f35930b0315fc137cf66
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 15:13:24 -0400

    2024-05-31T19:13:24.238Zfunction_auto_return app_dev_sandbox_function

--- public/src/app_dev_sandbox_function.mjs
+++ public/src/app_dev_sandbox_function.mjs
@@ -25,8 +25,9 @@ export function app_dev_sandbox_function(
   html_value_set(textarea, code_initial);
   html_button_run(root, async () => {
     app_dev_sandbox_result_show(result_component);
+    let result;
     try {
-      await run_click(html_value_get(textarea));
+      result = await run_click(html_value_get(textarea));
     } catch (e) {
       app_dev_sandbox_result_error(result_component, e);
     }
