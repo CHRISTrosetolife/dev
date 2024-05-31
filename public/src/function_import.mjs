3ea358ce4d204d4281a944e36e0b0c5c99cd217c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 21:09:58 -0400

    2024-05-31T01:09:57.705Zfunction_auto_return function_import

--- public/src/function_import.mjs
+++ public/src/function_import.mjs
@@ -11,20 +11,20 @@ import { js_declaration_single } from "./js_declaration_single.mjs";
 import { js_unparse } from "./js_unparse.mjs";
 export async function function_import(function_name) {
   let function_file_name = function_name_to_file_name(function_name);
-  let function_path = string_combine(
+  let function_file_name_path = string_combine(
     folder_current_prefix(),
     function_file_name,
   );
   let i = 1;
   log("a" + i++);
   if (web_is()) {
-    log("a" + function_path);
+    log("a" + function_file_name_path);
     let g = global_get();
     let { files } = g;
     log(files);
-    if (object_property_exists(files, function_path)) {
+    if (object_property_exists(files, function_file_name_path)) {
       log("a" + i++);
-      let code = object_property_get(files, function_path);
+      let code = object_property_get(files, function_file_name_path);
       let ast = js_parse(code);
       let d = js_declaration_single(ast);
       let unparsed = js_unparse(d);
@@ -32,7 +32,7 @@ export async function function_import(function_name) {
       return eval(unparsed);
     }
   }
-  let imported = await import(function_path);
+  let imported = await import(function_file_name_path);
   let imported_function = object_property_get(imported, function_name);
   return imported_function;
 }
