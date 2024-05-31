aa7bb7e336e78d01006fe50bb0da4ccf168b9835
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 14:50:37 -0400

    2024-05-31T18:50:35.726Zfunction_auto_return app_dev_sandbox_result_show

--- public/src/app_dev_sandbox_result_show.mjs
+++ public/src/app_dev_sandbox_result_show.mjs
@@ -1,6 +1,6 @@
 import { app_learn_code_code_background_set } from "./app_learn_code_code_background_set.mjs";
 import { html_style_display_block } from "./html_style_display_block.mjs";
-export function app_dev_sandbox_result_show() {
+export function app_dev_sandbox_result_show(result_component) {
   html_style_display_block(result_component);
   app_learn_code_code_background_set(result_component);
 }
