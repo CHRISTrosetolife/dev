fdf94127dbb618bff3d0f0b9c035921a889b23cc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 20:19:31 -0400

    2024-05-31T00:19:31.041Zfunction_auto_return app_dev_screen_imports_add

--- public/src/app_dev_screen_imports_add.mjs
+++ public/src/app_dev_screen_imports_add.mjs
@@ -2,7 +2,7 @@ import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_imports_add() {
   return {
     name: function imports_add_name(root) {
-      return html_span_text(root, "imports_add");
+      return html_span_text(root, "add missing `import`s");
     },
     screen: function imports_add_screen(root) {},
   };
