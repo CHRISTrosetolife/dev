f6a8cb00db02697eaa73c950c70d8d22f0533536
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 15:33:42 -0400

    2024-05-31T19:33:41.892Zfunction_auto_return js_param_new

--- public/src/js_param_new.mjs
+++ public/src/js_param_new.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { js_declaration_single_name } from "./js_declaration_single_name.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
 import { equal } from "./equal.mjs";
@@ -13,8 +14,13 @@ export async function js_param_new(
   param_name,
   default_value_string,
 ) {
-    assert(string_is,[param_name]);
-    assert(string_is,[default_value_string]);
+  log({
+    function_name,
+    param_name,
+    default_value_string,
+  });
+  assert(string_is, [param_name]);
+  assert(string_is, [default_value_string]);
   let needs_imports_add = false;
   let nodes = js_node_type(ast, "CallExpression");
   for (let node of nodes) {
