7016a5b0400a8efd246fa1392ee35b6b65a5bcaf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 20:35:04 -0400

    2024-05-31T00:35:03.936Zfunction_auto_return app_dev_screen_imports_add

--- public/src/app_dev_screen_imports_add.mjs
+++ public/src/app_dev_screen_imports_add.mjs
@@ -1,3 +1,5 @@
+import { js_imports_add } from "./js_imports_add.mjs";
+import { function_transform } from "./function_transform.mjs";
 import { app_dev_screen_img } from "./app_dev_screen_img.mjs";
 import { html_style_alternate_monospace_short_span } from "./html_style_alternate_monospace_short_span.mjs";
 export function app_dev_screen_imports_add() {
@@ -12,7 +14,11 @@ export function app_dev_screen_imports_add() {
       "if we have a `function` that calls another `function` ...";
       let i = 1;
       app_dev_screen_img(app_dev_screen_imports_add, root, i++);
-      ("... and we run `function_transform` and pass in `js_imports_add` and the name of the function ...");
+      "... and we run `" +
+        function_transform.name +
+        "` and pass in `" +
+        js_imports_add +
+        "` and the name of the function ...";
       app_dev_screen_img(app_dev_screen_imports_add, root, i++);
       ("... then any missing `import`s will be added");
       app_dev_screen_img(app_dev_screen_imports_add, root, i++);
