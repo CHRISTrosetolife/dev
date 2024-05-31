1369ee9dc3acf4c9e43a8ac6536403cd3b8fb092
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 16:00:12 -0400

    2024-05-31T20:00:11.041Zfunction_auto_return html_button_run

--- public/src/html_button_run.mjs
+++ public/src/html_button_run.mjs
@@ -1,11 +1,11 @@
+import { html_button_width_full_text_click_alternate_short } from "./html_button_width_full_text_click_alternate_short.mjs";
 import { string_empty_not_is } from "./string_empty_not_is.mjs";
-import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { string_combine } from "./string_combine.mjs";
 export function html_button_run(root, run_click, message) {
   if (string_empty_not_is(message)) {
     message = string_combine(" : ", message);
   }
-  html_button_width_full_text_click(
+  html_button_width_full_text_click_alternate_short(
     root,
     string_combine("💻 run", message),
     run_click,
