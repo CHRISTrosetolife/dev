20896f568f8805bb8af0b59db6267262f6fb6db7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 15:37:43 -0400

    2024-05-31T19:37:42.403Z  watch

--- public/src/function_transform_all_args.mjs
+++ public/src/function_transform_all_args.mjs
@@ -2,7 +2,7 @@ import { function_names } from "./function_names.mjs";
 import { function_transform_args } from "./function_transform_args.mjs";
 export async function function_transform_all_args(lambda_name, args) {
   let function_names_result = await function_names();
-  for (let function_name of function_names_result) {
+  for (let function_name of function_names_result) {log({function_name})
     await function_transform_args(lambda_name, function_name, args);
   }
 }
