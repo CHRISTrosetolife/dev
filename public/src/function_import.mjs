67dfa87222ddb6d3cc13481a1764fe3d7029617a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 20:58:21 -0400

    2024-05-31T00:58:20.932Zfunction_auto_return function_import

--- public/src/function_import.mjs
+++ public/src/function_import.mjs
@@ -5,6 +5,7 @@ import { function_name_to_file_name } from "./function_name_to_file_name.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { web_is } from "./web_is.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
+import { js_parse } from "./js_parse.mjs";
 export async function function_import(function_name) {
   let function_file_name = function_name_to_file_name(function_name);
   let function_path = string_combine(
@@ -16,6 +17,7 @@ export async function function_import(function_name) {
     let { files } = g;
     if (object_property_exists(files, function_path)) {
       let code = object_property_get(files, function_path);
+      let ast = js_parse(code);
     }
   }
   let imported = await import(function_path);
