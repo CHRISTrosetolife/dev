befdcf39ad092d14b16c83a135a58e64a600ea3a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 14:04:31 -0400

    2024-05-31T18:04:31.203Zfunction_auto_return app_dev_sandbox_command_line

--- public/src/app_dev_sandbox_command_line.mjs
+++ public/src/app_dev_sandbox_command_line.mjs
@@ -82,6 +82,7 @@ export async function app_dev_sandbox_command_line(
       fn_result = await fn(...remaining);
     } catch (e) {
       run_error(e);
+      throw e;
       return;
     }
     log({
