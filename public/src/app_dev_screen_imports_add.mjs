313fc35cf21dfd19bf9312db133e85946e06b35b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-30 20:19:35 -0400

    2024-05-31T00:19:35.362Zfunction_auto_return app_dev_screen_imports_add

--- public/src/app_dev_screen_imports_add.mjs
+++ public/src/app_dev_screen_imports_add.mjs
@@ -1,8 +1,11 @@
-import { html_span_text } from "./html_span_text.mjs";
+import { html_style_alternate_monospace_short_span } from "./html_style_alternate_monospace_short_span.mjs";
 export function app_dev_screen_imports_add() {
   return {
     name: function imports_add_name(root) {
-      return html_span_text(root, "add missing `import`s");
+      return html_style_alternate_monospace_short_span(
+        root,
+        "add missing `import`s",
+      );
     },
     screen: function imports_add_screen(root) {},
   };
