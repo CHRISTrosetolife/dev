653d33e47f673c1561682880e6451458376de46b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-01 13:54:52 -0400

    2024-06-01T17:54:52.044Zfunction_auto_return file_js_transform

--- public/src/file_js_transform.mjs
+++ public/src/file_js_transform.mjs
@@ -4,10 +4,12 @@ import { file_js_parse } from "./file_js_parse.mjs";
 import { file_js_unparse } from "./file_js_unparse.mjs";
 export async function file_js_transform(lambdas, file_path, args) {
   let ast = await file_js_parse(file_path);
-  list_adder_async;
-  await each_async(lambdas, async (lambda) => {
-    await lambda(ast, ...args);
-  });
+  await list_adder_async(
+    async (la) =>
+      await each_async(lambdas, async (lambda) => {
+        await lambda(ast, ...args);
+      }),
+  );
   let result = await file_js_unparse(file_path, ast);
   return result;
 }
