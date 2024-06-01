3856593e1a86a98777db1c206604635339c84de8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-01 10:20:55 -0400

    2024-06-01T14:20:55.215Zfunction_auto_return app_dev_screen_imports_remove

--- public/src/app_dev_screen_imports_remove.mjs
+++ public/src/app_dev_screen_imports_remove.mjs
@@ -1,9 +1,12 @@
+import { html_style_alternate_monospace_short_span } from "./html_style_alternate_monospace_short_span.mjs";
 import { app_dev_screen_img } from "./app_dev_screen_img.mjs";
-import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_imports_remove() {
   return {
     name: function imports_remove_name(root) {
-      return html_span_text(root, "imports_remove");
+      return html_style_alternate_monospace_short_span(
+        root,
+        "remove unused `import`s",
+      );
     },
     screen: function imports_remove_screen(root) {
       let i = 1;
