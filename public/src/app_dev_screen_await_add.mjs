33b2b16a98c64ee3b694669ececed87d98aaed3c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-01 15:03:46 -0400

    2024-06-01T19:03:46.132Zfunction_auto_return app_dev_screen_await_add

--- public/src/app_dev_screen_await_add.mjs
+++ public/src/app_dev_screen_await_add.mjs
@@ -78,7 +78,10 @@ export function app_dev_screen_await_add() {
         "... then `await` will automatically be added and the `function` will be made `async` :",
       );
       app_dev_screen_img(app_dev_screen_await_add, root, i++);
-      ("once this function is automatically ran on every file save , then await and async are automatically added as you code");
+      html_style_alternate_monospace_short(
+        root,
+        "once this function is automatically ran on every file save , then await and async are automatically added as you code",
+      );
     },
   };
 }
