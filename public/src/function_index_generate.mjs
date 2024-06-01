4482dd8d592627c5cda182c49d204f8a964bac61
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 20:29:57 -0400

    2024-06-01T00:29:57.409Zfunction_auto_return function_index_generate

--- public/src/function_index_generate.mjs
+++ public/src/function_index_generate.mjs
@@ -1,8 +1,8 @@
-import { data_path } from "./data_path.mjs";
 import { file_read } from "./file_read.mjs";
+let p = data_path();
 export async function function_index_generate() {
   let name = "function_index";
-  let d = await file_read(data_path());
+  let d = await file_read(p);
   let a = 1;
   a = 2;
 }
