cae0bba84ce188430ea3d0e534526cc8b70ab9c7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-01 11:53:54 -0400

    2024-06-01T15:53:54.459Zfunction_auto_return app_dev_screen_imports_remove

--- public/src/app_dev_screen_imports_remove.mjs
+++ public/src/app_dev_screen_imports_remove.mjs
@@ -5,14 +5,7 @@ import { app_dev_sandbox_function } from "./app_dev_sandbox_function.mjs";
 import { file_write } from "./file_write.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { js_code_format } from "./js_code_format.mjs";
-import { string_delimit } from "./string_delimit.mjs";
-import { list_map } from "./list_map.mjs";
-import { js_code_array } from "./js_code_array.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { js_code_call_args } from "./js_code_call_args.mjs";
-import { log } from "./log.mjs";
-import { js_code_statement_call_args } from "./js_code_statement_call_args.mjs";
-import { list_join_space } from "./list_join_space.mjs";
 import { js_code_export_function_declare } from "./js_code_export_function_declare.mjs";
 import { function_name_new } from "./function_name_new.mjs";
 import { app_dev_p_transformer } from "./app_dev_p_transformer.mjs";
@@ -46,13 +39,7 @@ export function app_dev_screen_imports_remove() {
       let contents_function = js_code_export_function_declare(
         function_name_unique,
         "",
-        list_join_space([
-          js_code_statement_call_args(log.name, [
-            js_code_call_args(string_combine_multiple.name, [
-              js_code_array(list_map(["log", " ", "message"], string_delimit)),
-            ]),
-          ]),
-        ]),
+        "",
         false,
       );
       let contents = await js_code_format(contents_function);
