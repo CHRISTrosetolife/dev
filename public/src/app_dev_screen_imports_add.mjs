805c09cc82004c342abbb2c9cfd50b85619780bf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-01 13:07:08 -0400

    2024-06-01T17:07:08.061Zfunction_auto_return app_dev_screen_imports_add

--- public/src/app_dev_screen_imports_add.mjs
+++ public/src/app_dev_screen_imports_add.mjs
@@ -12,7 +12,6 @@ import { js_imports_add } from "./js_imports_add.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { app_dev_screen_img } from "./app_dev_screen_img.mjs";
 import { html_style_alternate_monospace_short_span } from "./html_style_alternate_monospace_short_span.mjs";
-import { list_join_space } from "./list_join_space.mjs";
 import { js_code_call_args } from "./js_code_call_args.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { file_write } from "./file_write.mjs";
@@ -45,11 +44,9 @@ export function app_dev_screen_imports_add() {
       let contents_function = js_code_export_function_declare(
         function_name_unique,
         "",
-        list_join_space([
-          js_code_statement_call_args(log.name, [
-            js_code_call_args(string_combine_multiple.name, [
-              js_code_array(list_map(["log", " ", "message"], string_delimit)),
-            ]),
+        js_code_statement_call_args(log.name, [
+          js_code_call_args(string_combine_multiple.name, [
+            js_code_array(list_map(["log", " ", "message"], string_delimit)),
           ]),
         ]),
         false,
