30d82d8f9acb3a4a088e6731790e8f62c59720a5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-01 13:19:01 -0400

    2024-06-01T17:19:00.758Zfunction_auto_return app_dev_p_transformer_generic

--- public/src/app_dev_p_transformer_generic.mjs
+++ public/src/app_dev_p_transformer_generic.mjs
@@ -1,6 +1,6 @@
 import { html_style_alternate_short_function_name } from "./html_style_alternate_short_function_name.mjs";
 import { function_transform } from "./function_transform.mjs";
-export function app_dev_p_transformer_generic(prefix, transformer, root) {
+export function app_dev_p_transformer_generic(root, transformer, prefix) {
   let message =
     prefix +
     " we run `" +
