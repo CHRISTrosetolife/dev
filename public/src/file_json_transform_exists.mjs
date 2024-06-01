76051132497d9d34ef82cde6199116dd27165ba6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-01 15:10:12 -0400

    2024-06-01T19:10:12.193Zfunction_auto_return file_json_transform_exists

--- public/src/file_json_transform_exists.mjs
+++ public/src/file_json_transform_exists.mjs
@@ -1,9 +1,9 @@
 import { file_json_transform } from "./file_json_transform.mjs";
 import { file_write_json } from "./file_write_json.mjs";
 import { file_exists } from "./file_exists.mjs";
-export async function file_json_transform_exists(data_path, lambda) {
-  if (!(await file_exists(data_path))) {
-    await file_write_json(data_path, {});
+export async function file_json_transform_exists(file_path, lambda) {
+  if (!(await file_exists(file_path))) {
+    await file_write_json(file_path, {});
   }
-  await file_json_transform(lambda, data_path, []);
+  await file_json_transform(lambda, file_path, []);
 }
