0f97e0a4c145e6a5f1c550ad1bf47b4cd3e9c68a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-01 11:52:05 -0400

    2024-06-01T15:52:04.525Zfunction_auto_return app_dev_screen_imports_remove

--- public/src/app_dev_screen_imports_remove.mjs
+++ public/src/app_dev_screen_imports_remove.mjs
@@ -1,6 +1,8 @@
+import { app_dev_p_transformer } from "./app_dev_p_transformer.mjs";
 import { html_style_alternate_monospace_short } from "./html_style_alternate_monospace_short.mjs";
 import { html_style_alternate_monospace_short_span } from "./html_style_alternate_monospace_short_span.mjs";
 import { app_dev_screen_img } from "./app_dev_screen_img.mjs";
+import { js_imports_remove } from "./js_imports_remove.mjs";
 export function app_dev_screen_imports_remove() {
   return {
     name: function imports_remove_name(root) {
@@ -16,6 +18,7 @@ export function app_dev_screen_imports_remove() {
         "if we have an `import` that is not being used ...",
       );
       app_dev_screen_img(app_dev_screen_imports_remove, root, i++);
+      app_dev_p_transformer(root, js_imports_remove.name);
       app_dev_screen_img(app_dev_screen_imports_remove, root, i++);
       app_dev_screen_img(app_dev_screen_imports_remove, root, i++);
     },
