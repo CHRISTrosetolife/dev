27a2ac18853a9a130c745ccc797bc574f0092091
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-01 07:58:20 -0400

    2024-06-01T11:58:19.821Zfunction_auto_return uuid_file

--- public/src/uuid_file.mjs
+++ public/src/uuid_file.mjs
@@ -2,7 +2,7 @@ import { file_delete } from "./file_delete.mjs";
 import { file_name_json } from "./file_name_json.mjs";
 import { uuid } from "./uuid.mjs";
 export async function uuid_file(lambda) {
-  let u = uuid();
+  let u = await uuid();
   let file_path = file_name_json(u);
   await lambda(file_path);
   await file_delete(file_path);
