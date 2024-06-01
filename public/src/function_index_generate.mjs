823d0677ee03fa346a8b1beeaf02365d5d7cda21
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 20:41:39 -0400

    2024-06-01T00:41:38.909Zfunction_auto_return function_index_generate

--- public/src/function_index_generate.mjs
+++ public/src/function_index_generate.mjs
@@ -1,7 +1,9 @@
 import { file_read_json } from "./file_read_json.mjs";
 import { data_path } from "./data_path.mjs";
+import { object_properties } from "./object_properties.mjs";
 export async function function_index_generate() {
   let name = "function_index";
   let d = await file_read_json(data_path());
   let { files } = d;
+  let file_names = object_properties(files);
 }
