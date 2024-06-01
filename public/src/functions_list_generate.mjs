efe58c59b195aef93fe06a4e0447644f98198eb4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 20:47:40 -0400

    2024-06-01T00:47:39.601Zfunction_auto_return functions_list_generate

--- public/src/functions_list_generate.mjs
+++ public/src/functions_list_generate.mjs
@@ -1,8 +1,5 @@
 import { js_code_statement_return } from "./js_code_statement_return.mjs";
 import { function_names } from "./function_names.mjs";
-import { file_read_json } from "./file_read_json.mjs";
-import { data_path } from "./data_path.mjs";
-import { object_properties } from "./object_properties.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 import { js_code_array } from "./js_code_array.mjs";
 import { list_map } from "./list_map.mjs";
@@ -10,10 +7,8 @@ import { string_delimit } from "./string_delimit.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
 export async function functions_list_generate() {
   let name = "functions_list";
-  let d = await file_read_json(data_path());
-  let { functions } = d;
-  let function_names = object_properties(functions);
-  let delimited = list_map(function_names, string_delimit);
+  let f = await function_names();
+  let delimited = list_map(f, string_delimit);
   await function_new_generic(
     name,
     "",
