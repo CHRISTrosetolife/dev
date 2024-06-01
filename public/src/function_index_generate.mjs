cbace100d889c71e3dd6b8486f4c9b2b351cd90c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 20:43:42 -0400

    2024-06-01T00:43:42.360Zfunction_auto_return function_index_generate

--- public/src/function_index_generate.mjs
+++ public/src/function_index_generate.mjs
@@ -1,11 +1,13 @@
+import { js_code_statement_return } from "./js_code_statement_return.mjs";
 import { function_names } from "./function_names.mjs";
 import { file_read_json } from "./file_read_json.mjs";
 import { data_path } from "./data_path.mjs";
 import { object_properties } from "./object_properties.mjs";
+import { function_new_generic } from "./function_new_generic.mjs";
 export async function function_index_generate() {
   let name = "function_index";
   let d = await file_read_json(data_path());
   let { functions } = d;
   let function_names = object_properties(functions);
-  return function_names;
+  await function_new_generic(name, "", js_code_statement_return);
 }
