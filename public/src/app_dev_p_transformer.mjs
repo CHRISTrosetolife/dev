88e360fc5aa5484634487b0a5e1d7c1b621039b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-01 13:19:24 -0400

    2024-06-01T17:19:24.564Zfunction_auto_return app_dev_p_transformer

--- public/src/app_dev_p_transformer.mjs
+++ public/src/app_dev_p_transformer.mjs
@@ -1,5 +1,4 @@
 import { app_dev_p_transformer_generic } from "./app_dev_p_transformer_generic.mjs";
 export function app_dev_p_transformer(root, transformer) {
-  let prefix = "... and";
-  app_dev_p_transformer_generic(root, transformer, prefix);
+  app_dev_p_transformer_generic(root, transformer, "... and");
 }
