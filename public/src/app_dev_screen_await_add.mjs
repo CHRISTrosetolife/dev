1579407216d4080b67859ce00524454a6a835823
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-01 13:46:44 -0400

    2024-06-01T17:46:43.810Zfunction_auto_return app_dev_screen_await_add

--- public/src/app_dev_screen_await_add.mjs
+++ public/src/app_dev_screen_await_add.mjs
@@ -1,3 +1,5 @@
+import { string_combine } from "./string_combine.mjs";
+import { html_style_alternate_short_function_name } from "./html_style_alternate_short_function_name.mjs";
 import { app_dev_screen_img } from "./app_dev_screen_img.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_await_add() {
@@ -8,6 +10,12 @@ export function app_dev_screen_await_add() {
     screen: function await_add_screen(root) {
       let i = 1;
       app_dev_screen_img(app_dev_screen_await_add, root, i++);
+      html_style_alternate_short_function_name(
+        root,
+        "`" +
+          string_combine.name +
+          "` is a function that concatenates two strings",
+      );
     },
   };
 }
