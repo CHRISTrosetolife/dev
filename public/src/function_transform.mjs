95edbf97d0a50d14b6ed63451edfd1babbbe6970
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-01 13:57:32 -0400

    2024-06-01T17:57:31.768Zfunction_auto_return function_transform

--- public/src/function_transform.mjs
+++ public/src/function_transform.mjs
@@ -1,4 +1,8 @@
 import { function_transform_return } from "./function_transform_return.mjs";
 export async function function_transform(lambda_names, function_name) {
-  await function_transform_return(lambda_names, function_name);
+  let { results } = await function_transform_return(
+    lambda_names,
+    function_name,
+  );
+  return results;
 }
