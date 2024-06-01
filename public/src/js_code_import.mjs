359a09d656490c57af6e4ec0d62fd93feeea92f3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-01 11:54:42 -0400

    2024-06-01T15:54:41.756Zfunction_auto_return js_code_import

--- public/src/js_code_import.mjs
+++ public/src/js_code_import.mjs
@@ -1,7 +1,7 @@
 import { folder_current } from "./folder_current.mjs";
 import { js_code_import_path } from "./js_code_import_path.mjs";
 export function js_code_import(import_name) {
-  const import_path = `${folder_current()}/`;
+  let import_path = `${folder_current()}/`;
   let import_code = js_code_import_path(import_path, import_name);
   return import_code;
 }
