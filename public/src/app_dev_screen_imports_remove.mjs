f03be2bc3a82e10fb2ebff9592728bc98ec5786f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-01 11:53:19 -0400

    2024-06-01T15:53:18.014Z  watch

--- public/src/app_dev_screen_imports_remove.mjs
+++ public/src/app_dev_screen_imports_remove.mjs
@@ -25,6 +25,38 @@ export function app_dev_screen_imports_remove() {
         "... then any unused `import`s will be removed",
       );
       app_dev_screen_img(app_dev_screen_imports_remove, root, i++);
+      let function_name_unique =  function_name_new("add_missing_imports");
+      let contents_function = js_code_export_function_declare(
+        function_name_unique,
+        "",
+        list_join_space([
+          js_code_statement_call_args(log.name, [
+            js_code_call_args(string_combine_multiple.name, [
+              js_code_array(list_map(["log", " ", "message"], string_delimit)),
+            ]),
+          ]),
+        ]),
+        false,
+      );
+      let contents =  js_code_format(contents_function);
+      let file_path = function_name_to_path(function_name_unique);
+       file_write(file_path, contents);
+       app_dev_sandbox_function(
+        root,
+        function_name_unique,
+        "adding missing `import`s",
+        string_delimit_backtick(
+          string_combine_multiple([
+            function_transform.name,
+            " ",
+            js_imports_add.name,
+            " ",
+            function_name_unique,
+          ]),
+        ),
+        async () =>
+          await function_transform(js_imports_add.name, function_name_unique),
+      );
     },
   };
 }
