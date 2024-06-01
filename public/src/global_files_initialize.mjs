b785f307d20cef906873ce0632985060a304b245
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-05-31 20:58:32 -0400

    2024-06-01T00:58:31.868Zfunction_auto_return global_files_initialize

--- public/src/global_files_initialize.mjs
+++ public/src/global_files_initialize.mjs
@@ -2,4 +2,5 @@ import { global_get } from "./global_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 export function global_files_initialize(files) {
   object_property_set(global_get(), "files", files);
+  return global_get();
 }
