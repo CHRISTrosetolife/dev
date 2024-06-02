3cab36a8bc519f63da1dec08059102b6bddf0530
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:37:28 -0400

    2024-06-02T19:37:28.296Zfunction_auto_return html_parse_visit_tag

--- public/src/html_parse_visit_tag.mjs
+++ public/src/html_parse_visit_tag.mjs
@@ -2,6 +2,9 @@ import { html_parse_visit } from "./html_parse_visit.mjs";
 export function html_parse_visit_tag(root, tag_name, lambda2) {
   let lambda = (v) => {
     let { node } = v;
+    if (node.type !== "tag") {
+      return;
+    }
     let { name } = node;
     if (name === tag_name) {
       lambda2(v);
