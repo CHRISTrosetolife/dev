3a7447859a2b14853788ca26f8927b2a35e9a950
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:40:27 -0400

    2024-06-02T18:40:27.142Zfunction_auto_return html_parse_a_href_starts_with

--- public/src/html_parse_a_href_starts_with.mjs
+++ public/src/html_parse_a_href_starts_with.mjs
@@ -3,7 +3,6 @@ import { list_adder } from "./list_adder.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { list_filter } from "./list_filter.mjs";
-import { object_property_exists } from "./object_property_exists.mjs";
 export function html_parse_a_href_starts_with(element, prefix) {
   let q = list_adder((la) =>
     html_parse_visit_tag_attribute(element, "a", "href", (v) => {
@@ -11,11 +10,7 @@ export function html_parse_a_href_starts_with(element, prefix) {
       la(node);
     }),
   );
-  let f2 = list_filter(q, (f) => {
-    let { attribs } = f;
-    return object_property_exists(attribs, "href");
-  });
-  let filtered = list_filter(f2, (i) => {
+  let filtered = list_filter(q, (i) => {
     let href = html_parse_href(i);
     return string_starts_with(href, prefix);
   });
