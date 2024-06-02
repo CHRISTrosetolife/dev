7d233a32410d303902ef693f001107682c6deb2e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:37:53 -0400

    2024-06-02T18:37:53.693Zfunction_auto_return html_parse_a_href_starts_with

--- public/src/html_parse_a_href_starts_with.mjs
+++ public/src/html_parse_a_href_starts_with.mjs
@@ -1,9 +1,13 @@
+import { html_parse_visit_tag_attribute } from "./html_parse_visit_tag_attribute.mjs";
+import { list_adder } from "./list_adder.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 export function html_parse_a_href_starts_with(element, prefix) {
-  let q = element("a[href]");
+  let q = list_adder((la) =>
+    html_parse_visit_tag_attribute(element, "a[href]"),
+  );
   let f2 = list_filter(q, (f) => {
     let { attribs } = f;
     return object_property_exists(attribs, "href");
