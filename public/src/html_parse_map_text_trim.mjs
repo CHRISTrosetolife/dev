b340a224001f871b9ebe07821cf62ddf67d6f1e7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:51:00 -0400

    2024-06-02T18:50:59.736Zfunction_auto_return html_parse_map_text_trim

--- public/src/html_parse_map_text_trim.mjs
+++ public/src/html_parse_map_text_trim.mjs
@@ -1,10 +1,11 @@
+import { string_trim } from "./string_trim.mjs";
 import { list_map } from "./list_map.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function html_parse_map_text_trim(list) {
   assert_arguments_length(arguments, 1);
   let mapped = list_map(list, (m) => {
-    returnstring_trim(object_property_get(m, "data"));
+    return string_trim(object_property_get(m, "data"));
   });
   return mapped;
 }
