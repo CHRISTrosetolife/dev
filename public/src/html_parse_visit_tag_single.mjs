0106b0d2fabefc00bcbb53f8a2112939d45f8313
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:47:32 -0400

    2024-06-02T19:47:32.599Zfunction_auto_return html_parse_visit_tag_single

--- public/src/html_parse_visit_tag_single.mjs
+++ public/src/html_parse_visit_tag_single.mjs
@@ -1,7 +1,7 @@
 import { list_single } from "./list_single.mjs";
 import { html_parse_visit_tag_list } from "./html_parse_visit_tag_list.mjs";
-export function html_parse_visit_tag_single(teis_single, tag_name) {
-  let r = html_parse_visit_tag_list(teis_single, tag_name);
+export function html_parse_visit_tag_single(node, tag_name) {
+  let r = html_parse_visit_tag_list(node, tag_name);
   let s = list_single(r);
   return s;
 }
