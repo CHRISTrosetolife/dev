cd339abb9b802911228c03aadaa4871842ee4fdf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:42:21 -0400

    2024-06-02T19:42:21.048Zfunction_auto_return yyy8Uu_file_path_to_parts

--- public/src/yyy8Uu_file_path_to_parts.mjs
+++ public/src/yyy8Uu_file_path_to_parts.mjs
@@ -19,7 +19,7 @@ export async function yyy8Uu_file_path_to_parts(file_path, index) {
   let f = list_get(file_path, index);
   let input_string = await file_read(f);
   let parsed = html_parse(input_string);
-  let teis = html_parse_visit_tag_list(parsed, "TEI");
+  let teis = html_parse_visit_tag_list(parsed, "tei");
   log({
     teis,
   });
