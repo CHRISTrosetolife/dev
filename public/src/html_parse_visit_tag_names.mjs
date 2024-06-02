a7c6d20364f2546cd44c0e94ada4bedbd73ea7bc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:40:04 -0400

    2024-06-02T19:40:03.713Zfunction_auto_return html_parse_visit_tag_names

--- public/src/html_parse_visit_tag_names.mjs
+++ public/src/html_parse_visit_tag_names.mjs
@@ -1,14 +1,11 @@
-import { html_parse_visit } from "./html_parse_visit.mjs";
+import { html_parse_visit_tags } from "./html_parse_visit_tags.mjs";
 export function html_parse_visit_tag_names(root, tag_name, lambda2) {
   let lambda = (v) => {
     let { node } = v;
-    if (node.type !== "tag") {
-      return;
-    }
     let { name } = node;
     if (name === tag_name) {
       lambda2(v);
     }
   };
-  html_parse_visit(root, lambda);
+  html_parse_visit_tags(root, lambda);
 }
