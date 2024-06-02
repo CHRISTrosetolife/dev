c4eb4c42d65ebe0e21accc05d347066bc774775a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 18:06:05 -0400

    2024-06-02T22:06:04.874Zfunction_auto_return storage_local_set

--- public/src/storage_local_set.mjs
+++ public/src/storage_local_set.mjs
@@ -1,7 +1,7 @@
 import { json_from } from "./json_from.mjs";
 import { storage_local_key } from "./storage_local_key.mjs";
-export function storage_local_set(fn_namespace, key) {
-  let json = localStorage.getItem(storage_local_key(fn_namespace, key));
+export function storage_local_set(fn_namespace, key, value) {
   let item = json_from(json);
+  let json = localStorage.setItem(storage_local_key(fn_namespace, key), value);
   return item;
 }
