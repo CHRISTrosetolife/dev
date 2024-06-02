00e02efee396f4deb4c7b9b79f0f037176746e6c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:20:16 -0400

    2024-06-02T18:20:16.186Zfunction_auto_return visit

--- public/src/visit.mjs
+++ public/src/visit.mjs
@@ -1,11 +1,13 @@
 import { log } from "./log.mjs";
 import { each } from "./each.mjs";
 import { list_concat } from "./list_concat.mjs";
+import { list_is } from "./list_is.mjs";
 export function visit(root, children_get, filter, each, stack) {
   let children = children_get(root);
-  log({
-    children,
-  });
+  if (!list_is(children))
+    log({
+      children,
+    });
   for (let node of children) {
     if (filter(node)) {
       each({
