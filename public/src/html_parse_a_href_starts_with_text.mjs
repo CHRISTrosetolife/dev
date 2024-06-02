9f43c4026f9d11474901688e12930954fb8cdd6e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 12:36:12 -0400

    2024-06-02T16:36:12.108Zfunction_auto_return html_parse_a_href_starts_with_text

--- public/src/html_parse_a_href_starts_with_text.mjs
+++ public/src/html_parse_a_href_starts_with_text.mjs
@@ -1,9 +1,7 @@
-import { list_map } from "./list_map.mjs";
-import { list_map_property_text_trim } from "./list_map_property_text_trim.mjs";
+import { html_parse_map_text_trim } from "./html_parse_map_text_trim.mjs";
 import { html_parse_a_href_starts_with } from "./html_parse_a_href_starts_with.mjs";
 export function html_parse_a_href_starts_with_text(parsed, prefix) {
   let filtered = html_parse_a_href_starts_with(parsed, prefix);
-  let mapped3 = html_parse_map_text_trim(parsed,filtered);
+  let mapped3 = html_parse_map_text_trim(parsed, filtered);
   return mapped3;
 }
-
