e94f0da8d7838dbd05c3346865a92d8ccb5cf29f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:51:27 -0400

    2024-06-02T19:51:27.548Zfunction_auto_return yyy8Uu_file_path_to_parts

--- public/src/yyy8Uu_file_path_to_parts.mjs
+++ public/src/yyy8Uu_file_path_to_parts.mjs
@@ -27,7 +27,7 @@ export async function yyy8Uu_file_path_to_parts(file_path, index) {
   });
   assert(equal, [list_length(teis), 1]);
   let teis_single = list_single(teis);
-  let s = html_parse_visit_tag_single(teis_single, "text");
+  let text = html_parse_visit_tag_single(teis_single, "text");
   log({
     a: html_parse_visit_tag_names(teis_single),
     b: s.children,
