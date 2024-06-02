bf5839b0e0d6c9c85d9abedaa2761605679e56cb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:31:29 -0400

    2024-06-02T19:31:28.824Zfunction_auto_return html_cache_parse

--- public/src/html_cache_parse.mjs
+++ public/src/html_cache_parse.mjs
@@ -2,6 +2,6 @@ import { html_parse } from "./html_parse.mjs";
 import { http_cache } from "./http_cache.mjs";
 export async function html_cache_parse(url) {
   let html = await http_cache(url);
-  let parsed = html_parse(html);
+  let root = html_parse(html);
   return root;
 }
