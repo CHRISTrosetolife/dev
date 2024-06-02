717b316e31cb3a1de581dafc2397a43149fb314e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:42:12 -0400

    2024-06-02T18:42:11.914Zfunction_auto_return assert_arguments_length

--- public/src/assert_arguments_length.mjs
+++ public/src/assert_arguments_length.mjs
@@ -1,5 +1,6 @@
 import { equal } from "./equal.mjs";
 import { assert } from "./assert.mjs";
 export function assert_arguments_length(args, length) {
+  assert(equal, [arguments.length, 2]);
   assert(equal, [args.length, length]);
 }
