e86d2da3427a31554ac62e6d94ff0ad1eb0f148c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 12:39:10 -0400

    2024-06-02T16:39:10.787Z  function_search text,trim

--- public/src/list_map_property_text_trim.mjs
+++
@@ -1,6 +0,0 @@
-import { string_trim } from "./string_trim.mjs";
-import { list_map } from "./list_map.mjs";
-export function list_map_property_text_trim(filtered) {
-  let mapped = list_map(filtered, (i) => string_trim(i.text()));
-  return mapped;
-}
