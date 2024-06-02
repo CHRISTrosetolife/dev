53d980dd8b454a722f1b3dedf5d8f1691b7f4534
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 12:24:05 -0400

    2024-06-02T16:24:05.246Zfunction_auto_return ceb_form1

--- public/src/ceb_form1.mjs
+++ public/src/ceb_form1.mjs
@@ -1,6 +1,6 @@
 import { list_single } from "./list_single.mjs";
 export function ceb_form1(parsed) {
-  let q = parsed.querySelectorAll("form[name=form1]");
+  let q = parsed("form[name=form1]");
   let q_single = list_single(q);
   let { childNodes: q_children } = q_single;
   return q_children;
