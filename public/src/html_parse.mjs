c1bc7acee7fdc5195e7047af30d635402ed7454f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:31:52 -0400

    2024-06-02T19:31:52.362Zfunction_auto_return html_parse

--- public/src/html_parse.mjs
+++ public/src/html_parse.mjs
@@ -1,7 +1,7 @@
 import { parse } from "node-html-parser";
 import * as cheerio from "cheerio";
 export function html_parse(input) {
-  parsed cheerio.load(input);
+  let parsed = cheerio.load(input);
   let [root] = parsed("html");
   return parse(input);
 }
