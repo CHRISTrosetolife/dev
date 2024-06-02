8920c7e9dd490804d3dd3294d556632e8ff6f436
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 13:05:49 -0400

    2024-06-02T17:05:48.743Zfunction_auto_return html_parse_a_href_starts_with

--- public/src/html_parse_a_href_starts_with.mjs
+++ public/src/html_parse_a_href_starts_with.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { list_to } from "./list_to.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
@@ -6,6 +7,9 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function html_parse_a_href_starts_with(parsed, element, prefix) {
   assert_arguments_length(arguments, 3);
   let q = list_to(parsed(element).find("a[href]"));
+  log({
+    q,
+  });
   let filtered = list_filter(q, (i) => {
     let href = html_parse_href(i);
     return string_starts_with(href, prefix);
