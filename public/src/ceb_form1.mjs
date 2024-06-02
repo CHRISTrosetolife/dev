8c722f5a7f4511e08d2d07cd8c93d7faa2f6e31f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:16:38 -0400

    2024-06-02T18:16:38.342Zfunction_auto_return ceb_form1

--- public/src/ceb_form1.mjs
+++ public/src/ceb_form1.mjs
@@ -1,8 +1,9 @@
+import { log } from "./log.mjs";
 import { js_visit } from "./js_visit.mjs";
 import { list_single } from "./list_single.mjs";
-export function ceb_form1(parsed) {
-  js_visit;
-  let q = parsed("form[name=form1]");
+export function ceb_form1(root) {
+  js_visit(root, log);
+  let q = root("form[name=form1]");
   let q_single = list_single(q);
   let { childNodes: q_children } = q_single;
   return q_children;
