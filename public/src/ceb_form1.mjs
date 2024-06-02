c84528b8146884cee4b5016769904144417a06d0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:18:32 -0400

    2024-06-02T18:18:32.004Zfunction_auto_return ceb_form1

--- public/src/ceb_form1.mjs
+++ public/src/ceb_form1.mjs
@@ -1,10 +1,11 @@
+import { tautology } from "./tautology.mjs";
 import { visit } from "./visit.mjs";
 import { log } from "./log.mjs";
 import { list_single } from "./list_single.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function ceb_form1(root) {
   log(root);
-  visit(root, (n) => object_property_get(n, "children"));
+  visit(root, (n) => object_property_get(n, "children"), tautology);
   let q = root("form[name=form1]");
   let q_single = list_single(q);
   let { childNodes: q_children } = q_single;
