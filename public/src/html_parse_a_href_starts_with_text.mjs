440cc5cc40d5d14757301b622ca1bea1f31bb693
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 12:34:54 -0400

    2024-06-02T16:34:54.415Zfunction_auto_return html_parse_a_href_starts_with_text

--- public/src/html_parse_a_href_starts_with_text.mjs
+++ public/src/html_parse_a_href_starts_with_text.mjs
@@ -1,7 +1,9 @@
+import { list_map } from "./list_map.mjs";
 import { list_map_property_text_trim } from "./list_map_property_text_trim.mjs";
 import { html_parse_a_href_starts_with } from "./html_parse_a_href_starts_with.mjs";
 export function html_parse_a_href_starts_with_text(parsed, prefix) {
   let filtered = html_parse_a_href_starts_with(parsed, prefix);
+  let mapped = list_map(filtered, (f) => parsed(f));
   let mapped3 = list_map_property_text_trim(filtered);
   return mapped3;
 }
