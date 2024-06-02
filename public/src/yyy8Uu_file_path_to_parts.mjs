b548e5ebb3212552994adc0520105babe2de9bed
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:34:51 -0400

    2024-06-02T19:34:51.320Zfunction_auto_return yyy8Uu_file_path_to_parts

--- public/src/yyy8Uu_file_path_to_parts.mjs
+++ public/src/yyy8Uu_file_path_to_parts.mjs
@@ -19,10 +19,10 @@ export async function yyy8Uu_file_path_to_parts(file_path, index) {
   let f = list_get(file_path, index);
   let input_string = await file_read(f);
   let parsed = html_parse(input_string);
-  log({
-    parsed,
-  });
   let teis = html_parse_visit_tag_list(parsed, "TEI");
+  log({
+    teis,
+  });
   assert(equal, [list_length(teis), 1]);
   let bodies = html_parse_visit_tag_list(list_single(teis), "body");
   assert(equal, [list_length(bodies), 1]);
