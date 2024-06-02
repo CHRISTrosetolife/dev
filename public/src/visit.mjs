e5e083ad8e270eb944a41618607bb797e343a1fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:20:35 -0400

    2024-06-02T18:20:33.995Z  watch

--- public/src/visit.mjs
+++ public/src/visit.mjs
@@ -5,7 +5,7 @@ import { list_is } from "./list_is.mjs";
 export function visit(root, children_get, filter, each, stack) {
   let children = children_get(root);
   if (!list_is(children))
-    log({
+    log({root,
       children,
     });
   for (let node of children) {
