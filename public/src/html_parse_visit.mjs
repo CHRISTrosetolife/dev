91dc2387e4ffed6d44532d32435b7778e638fedc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:45:08 -0400

    2024-06-02T18:45:07.783Zfunction_auto_return html_parse_visit

--- public/src/html_parse_visit.mjs
+++ public/src/html_parse_visit.mjs
@@ -1,3 +1,4 @@
+import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { tautology } from "./tautology.mjs";
 import { visit } from "./visit.mjs";
@@ -5,7 +6,10 @@ export function html_parse_visit(root, lambda) {
   visit(
     root,
     (n) => {
-      return object_property_get(n, "children") || [];
+      if (object_property_exists_not(n, "children")) {
+        return [];
+      }
+      return object_property_get(n, "children");
     },
     tautology,
     lambda,
