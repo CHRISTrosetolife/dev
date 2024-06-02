9e212f74a315ee4e996ac10cb5969562d295da4e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:35:03 -0400

    2024-06-02T18:35:02.825Zfunction_auto_return html_parse_a_href_starts_with

--- public/src/html_parse_a_href_starts_with.mjs
+++ public/src/html_parse_a_href_starts_with.mjs
@@ -1,10 +1,9 @@
-import { list_to } from "./list_to.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 export function html_parse_a_href_starts_with(element, prefix) {
-  let q = list_to(element("a[href]"));
+  let q = element("a[href]");
   let f2 = list_filter(q, (f) => {
     let { attribs } = f;
     return object_property_exists(attribs, "href");
