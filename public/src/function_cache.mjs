28b514aefca11cde1986e8ff63a19d983f125f12
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 12:13:55 -0400

    2024-06-02T16:13:55.326Zfunction_auto_return function_cache_path

--- public/src/function_cache.mjs
+++ public/src/function_cache.mjs
@@ -1,22 +1,10 @@
-import { file_name_json } from "./file_name_json.mjs";
+import { function_cache_path } from "./function_cache_path.mjs";
 import { file_write_json } from "./file_write_json.mjs";
 import { file_read_json } from "./file_read_json.mjs";
-import { json_to } from "./json_to.mjs";
-import { path_join } from "./path_join.mjs";
-import { folder_gitignore } from "./folder_gitignore.mjs";
 import { file_exists } from "./file_exists.mjs";
-import { string_encoded_to } from "./string_encoded_to.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function function_cache(fn, args) {
-  let function_name = fn.name;
-  let key = {
-    function_name,
-    args,
-  };
-  let key_json = json_to(key);
-  let key_encoded = string_encoded_to(key_json);
-  let file_name = file_name_json(key_encoded);
-  let file_path = path_join([folder_gitignore(), "cache", file_name]);
+  let file_path = function_cache_path(fn, args);
   let result;
   let object;
   if (await file_exists(file_path)) {
