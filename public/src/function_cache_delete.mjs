7e4f9cca826db188fdc2329d1b90d5d515625dfc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 12:16:33 -0400

    2024-06-02T16:16:32.912Zfunction_auto_return function_cache_delete

--- public/src/function_cache_delete.mjs
+++ public/src/function_cache_delete.mjs
@@ -1,6 +1,8 @@
 import { function_cache_path } from "./function_cache_path.mjs";
 import { file_delete } from "./file_delete.mjs";
-export async function function_cache_delete(function_name, args) {
+import { string_split_comma } from "./string_split_comma.mjs";
+export async function function_cache_delete(function_name, args_string) {
+  string_split_comma(args_string);
   let file_path = function_cache_path(function_name, args);
   await file_delete(file_path);
 }
