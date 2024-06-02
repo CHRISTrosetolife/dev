4f74f45646c067c77040fb0e3e2be487224a18ed
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:54:47 -0400

    2024-06-02T18:54:47.258Zfunction_auto_return html_parse_parent

--- public/src/html_parse_parent.mjs
+++ public/src/html_parse_parent.mjs
@@ -1,4 +1,8 @@
+import { log } from "./log.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function html_parse_parent(parent) {
+  log({
+    parent,
+  });
   return object_property_get(parent, "parentNode");
 }
