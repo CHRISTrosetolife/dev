d38eb4a3e913def7b2a0d95f8f9314e3a17023f1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:09:18 -0400

    2024-06-02T19:09:17.992Zfunction_auto_return folder_gitignore_path

--- public/src/folder_gitignore_path.mjs
+++ public/src/folder_gitignore_path.mjs
@@ -1,7 +1,7 @@
 import { path_join } from "./path_join.mjs";
 import { folder_gitignore } from "./folder_gitignore.mjs";
-export function folder_gitignore_path(bible_folder) {
+export function folder_gitignore_path(file_path) {
   let folder_gitignore_result = folder_gitignore();
-  let folder = path_join([folder_gitignore_result, bible_folder]);
+  let folder = path_join([folder_gitignore_result, file_path]);
   return folder;
 }
