d606bc794a20bb0d9fab2389c8515ea087e63f39
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 12:38:10 -0400

    2024-06-02T16:38:10.326Zfunction_auto_return list_map_property_text_trim

--- public/src/list_map_property_text_trim.mjs
+++ public/src/list_map_property_text_trim.mjs
@@ -1,9 +1,6 @@
-import { object_property_get } from "./object_property_get.mjs";
 import { string_trim } from "./string_trim.mjs";
 import { list_map } from "./list_map.mjs";
 export function list_map_property_text_trim(filtered) {
-  let mapped = list_map(filtered, (i) =>
-    string_trim(object_property_get(i, "text")()),
-  );
+  let mapped = list_map(filtered, (i) => string_trim(i.text()));
   return mapped;
 }
--- public/src/property_text_trim.mjs
+++
@@ -1,5 +0,0 @@
-import { object_property_get } from "./object_property_get.mjs";
-import { string_trim } from "./string_trim.mjs";
-export function property_text_trim(i) {
-  return string_trim(object_property_get(i, "text")());
-}
