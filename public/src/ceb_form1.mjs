d52d23a2629b824491c137f31f9b0d756868b960
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:22:25 -0400

    2024-06-02T18:22:25.511Zfunction_auto_return ceb_form1

--- public/src/ceb_form1.mjs
+++ public/src/ceb_form1.mjs
@@ -4,7 +4,7 @@ import { list_single } from "./list_single.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function ceb_form1(root) {
   let lambda = (n) => {
-    let name = n;
+    let { name } = n;
   };
   visit(
     root,
