4e1d383593c7ced05b48ed67ac8699a77527ef25
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:06:34 -0400

    2024-06-02T19:06:34.647Zfunction_auto_return http_get

--- public/src/http_get.mjs
+++ public/src/http_get.mjs
@@ -1,5 +1,7 @@
 import { http_data_get } from "./http_data_get.mjs";
+import { import_multi } from "./import_multi.mjs";
 export async function http_get(url) {
+  let axios = await import_multi("axios", "axios");
   let result = await axios.get(url);
   return http_data_get(result);
 }
