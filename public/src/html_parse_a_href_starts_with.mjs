a2f63691f90290529bb729e937fe21637d1921b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 13:16:33 -0400

    2024-06-02T17:16:33.330Zfunction_auto_return html_parse_a_href_starts_with

--- public/src/html_parse_a_href_starts_with.mjs
+++ public/src/html_parse_a_href_starts_with.mjs
@@ -1,19 +1,12 @@
-import { error } from "./error.mjs";
 import { list_to } from "./list_to.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
-import { log } from "./log.mjs";
 export function html_parse_a_href_starts_with(parsed, element, prefix) {
   assert_arguments_length(arguments, 3);
-  let p = parsed(element);
-  log({
-    e: element("a[href=]"),
-  });
-  error();
-  let q = list_to(p("a"));
+  let q = list_to(element("a[href]"));
   let f2 = list_filter(q, (f) => object_property_exists(f.attrs, "href"));
   let filtered = list_filter(f2, (i) => {
     let href = html_parse_href(i);
