cc5d896a68defb8c8fcc3e6cd4908627977bc93d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:39:37 -0400

    2024-06-02T19:39:37.589Zfunction_auto_return html_parse_visit_tag

--- public/src/html_parse_visit_tag.mjs
+++ public/src/html_parse_visit_tag.mjs
@@ -1,11 +1,11 @@
 import { html_parse_visit_tags } from "./html_parse_visit_tags.mjs";
-export function html_parse_visit_tag(root, tag_name, lambda2) {
-  html_parse_visit_tags(root, lambda);
-  function lambda(v) {
+export function html_parse_visit_tag(root, tag_name, lambda) {
+  html_parse_visit_tags(root, lambda_inner);
+  function lambda_inner(v) {
     let { node } = v;
     let { name } = node;
     if (name === tag_name) {
-      lambda2(v);
+      lambda(v);
     }
   }
 }
