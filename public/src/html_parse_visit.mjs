51c2d0818317929ac4b85ecc69c805881c9676b6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:45:34 -0400

    2024-06-02T18:45:34.005Zfunction_auto_return html_parse_visit

--- public/src/html_parse_visit.mjs
+++ public/src/html_parse_visit.mjs
@@ -8,7 +8,8 @@ export function html_parse_visit(root, lambda) {
     (n) => {
       let property_name = "children";
       if (object_property_exists_not(n, property_name)) {
-        return [];
+        let default_value = [];
+        return default_value;
       }
       return object_property_get(n, property_name);
     },
