47c784e6c12c458be2f8ecd0520ed439fa340924
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:16:30 -0400

    2024-06-02T19:16:30.015Zfunction_auto_return html_parse_map_text_trim

--- public/src/html_parse_map_text_trim.mjs
+++ public/src/html_parse_map_text_trim.mjs
@@ -4,7 +4,7 @@ import { list_map } from "./list_map.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function html_parse_map_text_trim(list) {
   assert_arguments_length(arguments, 1);
-  let mapped = list_map(list, html_parse_text);
-  let m2 = list_map(mapped, string_trim);
-  return mapped;
+  let m = list_map(list, html_parse_text);
+  let m2 = list_map(m, string_trim);
+  return m2;
 }
