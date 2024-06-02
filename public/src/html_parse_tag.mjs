155f2f0022c0e8895267a82bcde4a9e1c27944a0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:55:59 -0400

    2024-06-02T18:55:58.710Zfunction_auto_return html_parse_tag

--- public/src/html_parse_tag.mjs
+++ public/src/html_parse_tag.mjs
@@ -1,5 +1,5 @@
 import { equal } from "./equal.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function html_parse_tag(parent, tag_name) {
-  return equal(object_property_get(parent, "rawTagName"), tag_name);
+  return equal(object_property_get(parent, "name"), tag_name);
 }
