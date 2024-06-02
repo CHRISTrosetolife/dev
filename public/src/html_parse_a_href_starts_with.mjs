696cfed1b66a9139da1f2ffe740ff54fcabf20a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:13:39 -0400

    2024-06-02T19:13:38.836Zfunction_auto_return html_parse_a_href_starts_with

--- public/src/html_parse_a_href_starts_with.mjs
+++ public/src/html_parse_a_href_starts_with.mjs
@@ -4,15 +4,15 @@ import { html_parse_href } from "./html_parse_href.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { list_filter } from "./list_filter.mjs";
 export function html_parse_a_href_starts_with(element, prefix) {
-  let q = list_adder((la) =>
+  let found = list_adder((la) =>
     html_parse_visit_tag_attribute(element, "a", "href", (v) => {
       let { node } = v;
       la(node);
     }),
   );
-  let filtered = list_filter(q, (i) => {
+  let filtered2 = list_filter(found, (i) => {
     let href = html_parse_href(i);
     return string_starts_with(href, prefix);
   });
-  return filtered;
+  return filtered2;
 }
