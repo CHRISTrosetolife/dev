c296b8e5f529c062a529ac8b86868aa5f2a9c086
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:38:16 -0400

    2024-06-02T19:38:16.352Zfunction_auto_return html_parse_visit_tag

--- public/src/html_parse_visit_tag.mjs
+++ public/src/html_parse_visit_tag.mjs
@@ -5,10 +5,13 @@ export function html_parse_visit_tag(root, tag_name, lambda2) {
     if (node.type !== "tag") {
       return;
     }
+    lambda3(node, v);
+  };
+  html_parse_visit(root, lambda);
+  function lambda3(node, v) {
     let { name } = node;
     if (name === tag_name) {
       lambda2(v);
     }
-  };
-  html_parse_visit(root, lambda);
+  }
 }
