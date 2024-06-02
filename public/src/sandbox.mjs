dde289af6984197b5206659ed835a52e325eef3e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:10:16 -0400

    2024-06-02T19:10:16.264Zfunction_auto_return sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -19,12 +19,12 @@ import { each_object } from "./each_object.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { list_add } from "./list_add.mjs";
 import { http_get } from "./http_get.mjs";
-import { file_write } from "./file_write.mjs";
+import { file_write_json } from "./file_write_json.mjs";
 export async function sandbox() {
   let group_index = 0;
   let storage_path = ceb_group_path(group_index);
   let result = await http_get(storage_url(storage_path));
-  await file_write(folder_gitignore_path(storage_path));
+  await file_write_json(folder_gitignore_path(storage_path), result);
   return;
   let limit = 150;
   let skip = 0;
