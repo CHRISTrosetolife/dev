85964f9a9794b375663a9bd7c40a0bcd2509a9af
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 10:22:09 -0400

    2024-06-02T14:22:08.497Z  watch

--- public/src/data_path.mjs
+++ public/src/data_path.mjs
@@ -3,6 +3,10 @@ import { folder_current_prefix_combine } from "./folder_current_prefix_combine.m
 import { path_join } from "./path_join.mjs";
 export function data_path() {
   return folder_current_prefix_combine(
-    path_join([folder_name_public(), "data.json"]),
+    path_join([folder_name_public(), data_file_name()]),
   );
 }
+function data_file_name() {
+    return "data.json";
+}
+
