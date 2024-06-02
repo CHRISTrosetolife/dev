1ca9dedf420c1ece1968f3ba188eb2037517870f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:23:27 -0400

    2024-06-02T19:23:26.979Zfunction_auto_return ceb_bible_words_definitions_get

--- public/src/ceb_bible_words_definitions_get.mjs
+++ public/src/ceb_bible_words_definitions_get.mjs
@@ -5,9 +5,11 @@ import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { ceb_definition } from "./ceb_definition.mjs";
 import { log } from "./log.mjs";
 import { each_index_async } from "./each_index_async.mjs";
+import { list_length } from "./list_length.mjs";
 export async function ceb_bible_words_definitions_get(t) {
   let existing = {};
   let result = [];
+  let t_length = list_length(t);
   await each_index_async(t, async (bible_word, index) => {
     if (10)
       log({
