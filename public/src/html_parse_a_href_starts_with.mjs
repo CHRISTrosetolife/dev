e7680012a2d1d131113799624549e45a344ca3ca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 13:07:31 -0400

    2024-06-02T17:07:31.191Zfunction_auto_return html_parse_a_href_starts_with

--- public/src/html_parse_a_href_starts_with.mjs
+++ public/src/html_parse_a_href_starts_with.mjs
@@ -1,16 +1,14 @@
-import { log } from "./log.mjs";
 import { list_to } from "./list_to.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
 export function html_parse_a_href_starts_with(parsed, element, prefix) {
   assert_arguments_length(arguments, 3);
-  let q = list_to(parsed(element).find("a[href]"));
-  log({
-    q,
-  });
-  let filtered = list_filter(q, (i) => {
+  let q = list_to(parsed(element).find("a"));
+  let f2 = list_filter(q, object_property_exists(f.attrs, "href"));
+  let filtered = list_filter(f2, (i) => {
     let href = html_parse_href(i);
     return string_starts_with(href, prefix);
   });
