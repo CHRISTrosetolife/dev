02df16d3c87ca4e9a9be5ae63ba9821104def586
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:41:39 -0400

    2024-06-02T19:41:38.290Zfunction_auto_return html_parse_visit_tag_names

--- public/src/html_parse_visit_tag_names.mjs
+++ public/src/html_parse_visit_tag_names.mjs
@@ -1,7 +1,7 @@
 import { html_parse_visit_tags } from "./html_parse_visit_tags.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 export function html_parse_visit_tag_names(root) {
-  return list_adder_unique((la) => {
+  let result = list_adder_unique((la) => {
     let lambda = (v) => {
       let { node } = v;
       let { name } = node;
