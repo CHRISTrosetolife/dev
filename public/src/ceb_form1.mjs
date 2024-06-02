3a94762a12918f75d62e0b9dec7b38f02f8b32d0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 14:22:08 -0400

    2024-06-02T18:22:08.236Zfunction_auto_return ceb_form1

--- public/src/ceb_form1.mjs
+++ public/src/ceb_form1.mjs
@@ -4,6 +4,7 @@ import { list_single } from "./list_single.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { noop } from "./noop.mjs";
 export function ceb_form1(root) {
+  let lambda = (n) => {};
   visit(
     root,
     (n) => object_property_get(n, "children") || [],
