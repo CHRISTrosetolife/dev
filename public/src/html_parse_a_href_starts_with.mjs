b1c50b8c7e4490623d4c833514047f45ca846a40
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 13:18:15 -0400

    2024-06-02T17:18:13.922Z  watch

--- public/src/html_parse_a_href_starts_with.mjs
+++ public/src/html_parse_a_href_starts_with.mjs
@@ -6,11 +6,11 @@ import { list_filter } from "./list_filter.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 export function html_parse_a_href_starts_with(element, prefix) {
   let q = list_to(element("a[href]"));
-  let f2 = list_filter(q, (f) => {
+  let f2 = list_filter(q, (f) => {let {attribs}=f
     log({
-      f,
+      f,attribs
     });
-    return object_property_exists(f.attribs, "href");
+    return object_property_exists(attribs, "href");
   });
   let filtered = list_filter(f2, (i) => {
     let href = html_parse_href(i);
