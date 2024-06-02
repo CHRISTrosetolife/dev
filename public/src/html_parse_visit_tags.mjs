511670d8e3915a51a4a619e768534ca54dbe1c5e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 15:39:52 -0400

    2024-06-02T19:39:52.330Zfunction_auto_return html_parse_visit_tags

--- public/src/html_parse_visit_tags.mjs
+++ public/src/html_parse_visit_tags.mjs
@@ -1,11 +1,11 @@
 import { html_parse_visit } from "./html_parse_visit.mjs";
-export function html_parse_visit_tags(root, lambda3) {
+export function html_parse_visit_tags(root, lambda) {
   let lambda_inner = (v) => {
     let { node } = v;
     if (node.type !== "tag") {
       return;
     }
-    lambda3(v);
+    lambda(v);
   };
   html_parse_visit(root, lambda_inner);
 }
