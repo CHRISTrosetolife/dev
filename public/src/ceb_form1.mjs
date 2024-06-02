04380398b9ae8c5bebbb110caa11626c267e1470
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-02 12:06:45 -0400

    2024-06-02T16:06:45.326Zfunction_auto_return ceb_form1

--- public/src/ceb_form1.mjs
+++ public/src/ceb_form1.mjs
@@ -1,7 +1,7 @@
 import { log } from "./log.mjs";
 import { list_single } from "./list_single.mjs";
 export function ceb_form1(parsed) {
-  let q2 = parsed.querySelectorAll("div#center");
+  let q2 = parsed.querySelectorAll("div[id=center]");
   log({
     q2,
   });
