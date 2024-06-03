a862e45215783325349336786ea6c1426e8383a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:59:35 -0400

    2024-06-03T19:59:35.570Zfunction_auto_return html_parse_visit_attribute_lambda

--- public/src/html_parse_visit_attribute_lambda.mjs
+++ public/src/html_parse_visit_attribute_lambda.mjs
@@ -1,4 +1,3 @@
-import { equal } from "./equal.mjs";
 import { html_parse_visit_attribute } from "./html_parse_visit_attribute.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function html_parse_visit_attribute_lambda(
@@ -11,7 +10,7 @@ export function html_parse_visit_attribute_lambda(
     let { node } = v;
     let { attribs } = node;
     let actual = object_property_get(attribs, attribute_name);
-    if (equal(actual, value)) {
+    if (attribute_lambda(actual)) {
       lambda(v);
     }
   }
