29cae5909b1a75cd74687dc22e5b46b1725a0da1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 16:54:22 -0400

    2024-06-03T20:54:21.680Zfunction_auto_return bible_verses_parse

--- public/src/bible_verses_parse.mjs
+++ public/src/bible_verses_parse.mjs
@@ -9,7 +9,6 @@ import { list_filter } from "./list_filter.mjs";
 import { list_length } from "./list_length.mjs";
 import { list_first } from "./list_first.mjs";
 import { html_parse_text } from "./html_parse_text.mjs";
-import { list_second } from "./list_second.mjs";
 import { list_get } from "./list_get.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
@@ -27,7 +26,6 @@ export function bible_verses_parse(verses) {
     let first = list_first(children);
     let { data } = first;
     assert(equal, [data, " "]);
-    let verse_number = html_parse_text(list_second(children));
     let right = list_get(children, 2);
     assert(equal, [right.type, "text"]);
     let right_text = object_property_get(right, "data");
