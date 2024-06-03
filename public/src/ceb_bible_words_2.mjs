63c99aa541c90b42e238d88c0536969821b46059
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:18:39 -0400

    2024-06-03T19:18:39.461Zfunction_auto_return ceb_bible_words_2

--- public/src/ceb_bible_words_2.mjs
+++ public/src/ceb_bible_words_2.mjs
@@ -1,10 +1,10 @@
+import { html_cache_parse } from "./html_cache_parse.mjs";
 import { url_secure_w3 } from "./url_secure_w3.mjs";
 import { string_count_words } from "./string_count_words.mjs";
 import { string_combine } from "./string_combine.mjs";
-import { http_cache } from "./http_cache.mjs";
 export async function ceb_bible_words_2() {
   let url = string_combine(url_secure_w3(), "talibon.com/bible/bible.html");
-  let text = await http_cache(url);
+  let text = await html_cache_parse(url);
   return text;
   let mapped = string_count_words(text_split);
   return mapped;
