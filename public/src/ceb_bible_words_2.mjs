0fc586491345695238ce6c6c3b9149d45fe280dd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:20:35 -0400

    2024-06-03T19:20:35.086Zfunction_auto_return ceb_bible_words_2

--- public/src/ceb_bible_words_2.mjs
+++ public/src/ceb_bible_words_2.mjs
@@ -5,12 +5,15 @@ import { html_cache_parse } from "./html_cache_parse.mjs";
 import { url_secure_w3 } from "./url_secure_w3.mjs";
 import { string_count_words } from "./string_count_words.mjs";
 import { string_combine } from "./string_combine.mjs";
+import { string_ends_with } from "./string_ends_with.mjs";
 export async function ceb_bible_words_2() {
   let url = string_combine(url_secure_w3(), "talibon.com/bible/bible.html");
   let root = await html_cache_parse(url);
   html_parse_visit_tag(root, "a", (v) => {
     let { node } = v;
     let href = html_parse_href(node);
+    if (string_ends_with(href, ".html")) {
+    }
     log(href);
   });
   return;
