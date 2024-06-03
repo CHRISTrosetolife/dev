425985cedce1ced8c975ad421951fa2e6d9cf3a0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:50:00 -0400

    2024-06-03T19:50:00.266Zfunction_auto_return bible_chapter_parsed

--- public/src/bible_chapter_parsed.mjs
+++ public/src/bible_chapter_parsed.mjs
@@ -1,3 +1,4 @@
+import { html_cache_parse } from "./html_cache_parse.mjs";
 import { path_join } from "./path_join.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { url_secure } from "./url_secure.mjs";
@@ -11,7 +12,7 @@ export async function bible_chapter_parsed(bible_folder, chapter_name) {
   ]);
   let chapter_path = string_combine(chapter_name, ".htm");
   let joined = path_join([url_base, chapter_path]);
-  let parsed = await http_cache_parse(joined);
+  let parsed = await html_cache_parse(joined);
   let notemarks = parsed.querySelectorAll(".notemark");
   for (let n of notemarks) {
     n.remove();
