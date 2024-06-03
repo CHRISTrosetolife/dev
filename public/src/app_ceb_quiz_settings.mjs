87785dc486543a072fa0ca33537b506a7c658412
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 13:13:57 -0400

    2024-06-03T17:13:57.376Zfunction_auto_return app_ceb_quiz_settings

--- public/src/app_ceb_quiz_settings.mjs
+++ public/src/app_ceb_quiz_settings.mjs
@@ -1,3 +1,4 @@
+import { list_scramble } from "./list_scramble.mjs";
 import { list_scramble_halves } from "./list_scramble_halves.mjs";
 import { list_copy } from "./list_copy.mjs";
 import { each } from "./each.mjs";
@@ -5,6 +6,8 @@ import { list_adder } from "./list_adder.mjs";
 import { list_concat_multiple } from "./list_concat_multiple.mjs";
 export function app_ceb_quiz_settings(atoms, chunk_sizes) {
   let concat = list_concat_multiple(atoms);
+  let copy = list_copy(concat);
+  list_scramble(copy);
   let settings_choices = list_adder((la) =>
     each(chunk_sizes, (chunk_size) =>
       each([true, false], (forwards) => {
