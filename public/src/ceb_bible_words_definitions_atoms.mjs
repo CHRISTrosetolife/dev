2eedb82d9698d53296996becb94bc2b7c1b8cb62
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 13:30:18 -0400

    2024-06-03T17:30:18.516Zfunction_auto_return ceb_bible_words_definitions_atoms

--- public/src/ceb_bible_words_definitions_atoms.mjs
+++ public/src/ceb_bible_words_definitions_atoms.mjs
@@ -56,6 +56,9 @@ export async function ceb_bible_words_definitions_atoms(skip, limit) {
       });
       if (equal(list_length(atom_result), atom_count)) {
         la(atom_result);
+        log({
+          atom_result,
+        });
         previous = atom_result;
       } else {
         if (0)
