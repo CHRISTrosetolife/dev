5ef20e93620d7dfe65e6f655f69944bc20f99cd8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 16:09:12 -0400

    2024-06-03T20:09:11.287Zfunction_auto_return bible_engbsb_chapter

--- public/src/bible_engbsb_chapter.mjs
+++ public/src/bible_engbsb_chapter.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { html_parse_text } from "./html_parse_text.mjs";
 import { list_adder_visit } from "./list_adder_visit.mjs";
 import { html_parse_visit_attribute_values } from "./html_parse_visit_attribute_values.mjs";
@@ -17,9 +16,6 @@ export async function bible_engbsb_chapter(chapter_name) {
       list_adder_visit(la),
     ),
   );
-  log({
-    verses_bsb,
-  });
   let mapped6 = list_map(verses_bsb, html_parse_text);
   let joined = list_join(mapped6, "");
   let eng = bible_verses_parse(joined);
