49d901019881ca38da273098680f0fce5b377bc4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:56:38 -0400

    2024-06-03T19:56:37.849Z  function_copy html_parse_visit_attribute html_parse_visit_attribute_value

---
+++ public/src/html_parse_visit_attribute_value.mjs
@@ -0,0 +1,12 @@
+import { html_parse_visit } from "./html_parse_visit.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
+export function html_parse_visit_attribute_value(root, attribute_name, lambda) {
+  function lambda_inner(v) {
+    let { node } = v;
+    let { attribs } = node;
+    if (object_property_exists(attribs, attribute_name)) {
+      lambda(v);
+    }
+  }
+  html_parse_visit(root, lambda_inner);
+}
