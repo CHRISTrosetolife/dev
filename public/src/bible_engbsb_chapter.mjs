ca381e3c27eb5cbd9aab35424a18221e0b7ad1f7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 16:05:41 -0400

    2024-06-03T20:05:40.807Zfunction_auto_return bible_engbsb_chapter

--- public/src/bible_engbsb_chapter.mjs
+++ public/src/bible_engbsb_chapter.mjs
@@ -1,3 +1,4 @@
+import { list_adder_visit } from "./list_adder_visit.mjs";
 import { html_parse_visit_attribute_values } from "./html_parse_visit_attribute_values.mjs";
 import { bible_verses_parse } from "./bible_verses_parse.mjs";
 import { list_join } from "./list_join.mjs";
@@ -7,10 +8,12 @@ import { list_adder } from "./list_adder.mjs";
 export async function bible_engbsb_chapter(chapter_name) {
   let root = await bible_chapter_parsed("engbsb_html", chapter_name);
   let verses_bsb = list_adder((la) =>
-    html_parse_visit_attribute_values(root, "class", ["m", "pc"], (v) => {
-      let { node } = v;
-      la(node);
-    }),
+    html_parse_visit_attribute_values(
+      root,
+      "class",
+      ["m", "pc"],
+      list_adder_visit(la),
+    ),
   );
   let mapped6 = list_map(verses_bsb, (v) => v.rawText);
   let joined = list_join(mapped6, "");
