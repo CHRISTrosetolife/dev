1724def98abe6214f100ece0383be963dac56ceb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 14:05:52 -0400

    2024-06-03T18:05:51.763Zfunction_auto_return app_ceb

--- public/src/app_ceb.mjs
+++ public/src/app_ceb.mjs
@@ -389,6 +389,7 @@ export async function app_ceb() {
   async function refresh_pair_list() {
     html_clear_scroll_top_centered(root);
     let concat = atoms_slice_concat();
+    let lookup = {};
     let [cebuano, english] = pair;
     app_ceb_word_button(root, cebuano);
     app_ceb_word_english(root, english);
