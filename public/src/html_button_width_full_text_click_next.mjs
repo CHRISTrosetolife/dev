247370db6f497cbf15b0025a0e9adfe4b3843bee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 14:15:02 -0400

    2024-06-03T18:15:02.309Zfunction_auto_return html_button_width_full_text_click_next

--- public/src/html_button_width_full_text_click_next.mjs
+++ public/src/html_button_width_full_text_click_next.mjs
@@ -1,5 +1,10 @@
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+import { string_combine } from "./string_combine.mjs";
 export function html_button_width_full_text_click_next(root, next_on_click) {
   let prefix = "";
-  return html_button_width_full_text_click(root, "➡️ next", next_on_click);
+  return html_button_width_full_text_click(
+    root,
+    string_combine(prefix, "➡️ next"),
+    next_on_click,
+  );
 }
