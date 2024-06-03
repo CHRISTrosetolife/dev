beff14d6b2904176d1eac73d312c3323fe9bb0fa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:56:58 -0400

    2024-06-03T19:56:57.998Zfunction_auto_return html_parse_visit_attribute_value

--- public/src/html_parse_visit_attribute_value.mjs
+++ public/src/html_parse_visit_attribute_value.mjs
@@ -1,6 +1,11 @@
 import { html_parse_visit_attribute } from "./html_parse_visit_attribute.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
-export function html_parse_visit_attribute_value(root, attribute_name, lambda) {
+export function html_parse_visit_attribute_value(
+  root,
+  attribute_name,
+  value,
+  lambda,
+) {
   function lambda_inner(v) {
     let { node } = v;
     let { attribs } = node;
