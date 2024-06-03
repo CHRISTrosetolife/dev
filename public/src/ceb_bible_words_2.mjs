0d0e487216e9fb8c51c063a156d01e38e4fc4028
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:20:55 -0400

    2024-06-03T19:20:54.639Zfunction_auto_return ceb_bible_words_2

--- public/src/ceb_bible_words_2.mjs
+++ public/src/ceb_bible_words_2.mjs
@@ -1,21 +1,23 @@
 import { html_parse_href } from "./html_parse_href.mjs";
-import { log } from "./log.mjs";
 import { html_parse_visit_tag } from "./html_parse_visit_tag.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { url_secure_w3 } from "./url_secure_w3.mjs";
 import { string_count_words } from "./string_count_words.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { string_ends_with } from "./string_ends_with.mjs";
+import { list_adder } from "./list_adder.mjs";
 export async function ceb_bible_words_2() {
   let url = string_combine(url_secure_w3(), "talibon.com/bible/bible.html");
   let root = await html_cache_parse(url);
-  html_parse_visit_tag(root, "a", (v) => {
-    let { node } = v;
-    let href = html_parse_href(node);
-    if (string_ends_with(href, ".html")) {
-    }
-    log(href);
-  });
+  list_adder((la) =>
+    html_parse_visit_tag(root, "a", (v) => {
+      let { node } = v;
+      let href = html_parse_href(node);
+      if (string_ends_with(href, ".html")) {
+        la(href);
+      }
+    }),
+  );
   return;
   let mapped = string_count_words(text_split);
   return mapped;
