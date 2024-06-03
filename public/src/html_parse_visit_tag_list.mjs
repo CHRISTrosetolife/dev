4e597c0de5dc20cb94c3f1724d58e939b6f35f00
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 16:06:00 -0400

    2024-06-03T20:06:00.331Zfunction_auto_return html_parse_visit_tag_list

--- public/src/html_parse_visit_tag_list.mjs
+++ public/src/html_parse_visit_tag_list.mjs
@@ -1,10 +1,8 @@
+import { list_adder_visit } from "./list_adder_visit.mjs";
 import { html_parse_visit_tag } from "./html_parse_visit_tag.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function html_parse_visit_tag_list(parsed, tag_name) {
   return list_adder((la) => {
-    html_parse_visit_tag(parsed, tag_name, (v) => {
-      let { node } = v;
-      la(node);
-    });
+    html_parse_visit_tag(parsed, tag_name, list_adder_visit(la));
   });
 }
