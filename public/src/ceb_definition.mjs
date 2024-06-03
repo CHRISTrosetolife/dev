70eadda840bba22780411e5dbd93c81930e8e806
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 18:21:22 -0400

    2024-06-03T22:21:22.485Zfunction_auto_return ceb_definition

--- public/src/ceb_definition.mjs
+++ public/src/ceb_definition.mjs
@@ -221,10 +221,6 @@ export async function ceb_definition(word) {
     let url = string_combine(prefix_2, d);
     url = string_whitespace_normalize(url);
     url = string_replace(url, " ", "+");
-    log({
-      a: "t",
-      url,
-    });
     let { children: children2 } = await ceb_html_cache_parse_form1(url);
     let as = list_filter(children2, (c) => html_parse_tag(c, "a"));
     let mapped5 = list_map(as, html_parse_href);
