dde6354946a9c33fe9823d4dd726d022f125aab0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:59:51 -0400

    2024-06-03T19:59:51.434Zfunction_auto_return html_parse_visit_attribute_value

--- public/src/html_parse_visit_attribute_value.mjs
+++ public/src/html_parse_visit_attribute_value.mjs
@@ -1,5 +1,5 @@
+import { html_parse_visit_attribute_lambda } from "./html_parse_visit_attribute_lambda.mjs";
 import { equal } from "./equal.mjs";
-import { html_parse_visit_attribute } from "./html_parse_visit_attribute.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function html_parse_visit_attribute_value(
   root,
@@ -15,5 +15,5 @@ export function html_parse_visit_attribute_value(
       lambda(v);
     }
   }
-  html_parse_visit_attribute(root, attribute_name, lambda_inner);
+  html_parse_visit_attribute_lambda(root, attribute_name, lambda_inner);
 }
