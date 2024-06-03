5e8aba15643efdccb23e8c0da548d23927bdb351
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 14:17:57 -0400

    2024-06-03T18:17:56.894Zfunction_auto_return app_ceb

--- public/src/app_ceb.mjs
+++ public/src/app_ceb.mjs
@@ -148,9 +148,7 @@ export async function app_ceb() {
         j++;
       });
     }
-    html_button_width_full_text_click(root, "📃 review", () => {
-      refresh_pair_list(0);
-    });
+    html_button_width_full_text_click(root, "📃 review", refresh_pair_list);
     if (left === right) {
       html_button_width_full_text_click(
         root,
