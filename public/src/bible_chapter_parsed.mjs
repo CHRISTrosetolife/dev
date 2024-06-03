c1b69508d2070241e86cc20c87f62554fab5ff6d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:47:10 -0400

    2024-06-03T19:47:10.679Zfunction_auto_return bible_chapter_parsed

--- public/src/bible_chapter_parsed.mjs
+++ public/src/bible_chapter_parsed.mjs
@@ -3,10 +3,11 @@ import { file_read } from "./file_read.mjs";
 import { path_join } from "./path_join.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { html_parse } from "./html_parse.mjs";
+import { url_secure } from "./url_secure.mjs";
 export async function bible_chapter_parsed(bible_folder, chapter_name) {
   let folder = folder_gitignore_path(bible_folder);
   let chapter_path = string_combine(chapter_name, ".htm");
-  ("https://ebible.org/cebulb/");
+  string_combine(url_secure(), "ebible.org/cebulb/");
   let joined = path_join([folder, chapter_path]);
   let read = await file_read(joined);
   let parsed = html_parse(read);
