1bf4dc11d9096ecfdb0a36103828a34586215888
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 16:58:26 -0400

    2024-06-03T20:58:26.182Zfunction_auto_return bible_verses_parse

--- public/src/bible_verses_parse.mjs
+++ public/src/bible_verses_parse.mjs
@@ -1,12 +1,6 @@
 import { object_property_get_or } from "./object_property_get_or.mjs";
-import { log } from "./log.mjs";
-import { equal } from "./equal.mjs";
 import { string_split_space } from "./string_split_space.mjs";
-import { assert } from "./assert.mjs";
-import { list_length } from "./list_length.mjs";
-import { list_first } from "./list_first.mjs";
 import { html_parse_text } from "./html_parse_text.mjs";
-import { list_get } from "./list_get.mjs";
 import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { undefined_not_is } from "./undefined_not_is.mjs";
@@ -37,21 +31,6 @@ export function bible_verses_parse(verses) {
           }
         }
       });
-      assert(equal, [list_length(children), 3]);
-      let first = list_first(children);
-      let { data } = first;
-      assert(equal, [data, " "]);
-      let right = list_get(children, 2);
-      assert(equal, [right.type, "text"]);
-      let s2 = string_split_space(right_text);
-      let result = {
-        verse_number,
-        tokens,
-      };
-      log({
-        result,
-      });
-      return result;
     }),
   );
 }
