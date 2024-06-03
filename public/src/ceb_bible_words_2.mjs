c0c412d3ad01f31b3686e3434ff1a314322151b0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:22:06 -0400

    2024-06-03T19:22:05.708Zfunction_auto_return ceb_bible_words_2

--- public/src/ceb_bible_words_2.mjs
+++ public/src/ceb_bible_words_2.mjs
@@ -3,12 +3,15 @@ import { html_parse_visit_tag } from "./html_parse_visit_tag.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { url_secure_w3 } from "./url_secure_w3.mjs";
 import { string_count_words } from "./string_count_words.mjs";
-import { string_combine } from "./string_combine.mjs";
 import { string_ends_with } from "./string_ends_with.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { each_async } from "./each_async.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function ceb_bible_words_2() {
-  let url = string_combine(url_secure_w3(), "talibon.com/bible/bible.html");
+  let url = string_combine_multiple([
+    url_secure_w3(),
+    "talibon.com/bible/bible.html",
+  ]);
   let root = await html_cache_parse(url);
   let hrefs = list_adder((la) =>
     html_parse_visit_tag(root, "a", (v) => {
