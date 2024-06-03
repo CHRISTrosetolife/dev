84626214178b4661fc7b7c9956e5f4fe6f40d063
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:56:08 -0400

    2024-06-03T19:56:07.793Zfunction_auto_return html_parse_visit_attribute

--- public/src/html_parse_visit_attribute.mjs
+++ public/src/html_parse_visit_attribute.mjs
@@ -1,11 +1,6 @@
 import { html_parse_visit } from "./html_parse_visit.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
-export function html_parse_visit_attribute(
-  root,
-  tag_name,
-  attribute_name,
-  lambda,
-) {
+export function html_parse_visit_attribute(root, attribute_name, lambda) {
   function lambda_inner(v) {
     let { node } = v;
     let { attribs } = node;
