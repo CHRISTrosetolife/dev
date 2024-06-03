5a184ea0c16649118c6cd5c13b2e985f125c6918
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 16:57:46 -0400

    2024-06-03T20:57:46.073Zfunction_auto_return bible_verses_parse

--- public/src/bible_verses_parse.mjs
+++ public/src/bible_verses_parse.mjs
@@ -12,6 +12,7 @@ import { list_adder } from "./list_adder.mjs";
 import { undefined_not_is } from "./undefined_not_is.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
 import { string_whitespace_normalize } from "./string_whitespace_normalize.mjs";
+import { string_trim } from "./string_trim.mjs";
 export function bible_verses_parse(verses) {
   list_adder((la) =>
     each(verses, (v) => {
@@ -26,7 +27,7 @@ export function bible_verses_parse(verses) {
         }
         if (undefined_not_is(verse_number)) {
           if (c.type === "text") {
-            let n = string_whitespace_normalize(c.data);
+            let n = string_trim(string_whitespace_normalize(c.data));
             list_add_multiple(tokens);
           }
         }
