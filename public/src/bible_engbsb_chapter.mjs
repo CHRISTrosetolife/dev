039d56e234480bf796539c1306bc7d62902a29f4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 16:16:47 -0400

    2024-06-03T20:16:46.930Zfunction_auto_return bible_engbsb_chapter

--- public/src/bible_engbsb_chapter.mjs
+++ public/src/bible_engbsb_chapter.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { html_parse_text } from "./html_parse_text.mjs";
 import { list_adder_visit } from "./list_adder_visit.mjs";
 import { html_parse_visit_attribute_values } from "./html_parse_visit_attribute_values.mjs";
@@ -19,7 +18,6 @@ export async function bible_engbsb_chapter(chapter_name) {
   );
   let mapped6 = list_map(verses_bsb, html_parse_text);
   let joined = list_join(mapped6, "");
-  log(mapped6);
   let eng = bible_verses_parse(joined);
   return eng;
 }
