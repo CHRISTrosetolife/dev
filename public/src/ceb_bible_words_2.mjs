edcf553d4a75659e8f6f4e225d768d7ad0a09828
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:23:08 -0400

    2024-06-03T19:23:07.755Zfunction_auto_return ceb_bible_words_2

--- public/src/ceb_bible_words_2.mjs
+++ public/src/ceb_bible_words_2.mjs
@@ -24,7 +24,9 @@ export async function ceb_bible_words_2() {
       }
     }),
   );
-  await each_async(hrefs, (href) => {});
+  await each_async(hrefs, async (href) => {
+    await html_cache_parse(url);
+  });
   return hrefs;
   let mapped = string_count_words(text_split);
   return mapped;
