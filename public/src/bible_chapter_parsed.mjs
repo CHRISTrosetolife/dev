fc9d55edb5e7175a745d81ee6ef580da12dd4f63
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:48:06 -0400

    2024-06-03T19:48:06.515Zfunction_auto_return bible_chapter_parsed

--- public/src/bible_chapter_parsed.mjs
+++ public/src/bible_chapter_parsed.mjs
@@ -1,4 +1,3 @@
-import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 import { file_read } from "./file_read.mjs";
 import { path_join } from "./path_join.mjs";
 import { string_combine } from "./string_combine.mjs";
@@ -6,10 +5,14 @@ import { html_parse } from "./html_parse.mjs";
 import { url_secure } from "./url_secure.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function bible_chapter_parsed(bible_folder, chapter_name) {
-  let folder = folder_gitignore_path(bible_folder);
+  let url_base = string_combine_multiple([
+    url_secure(),
+    "ebible.org/",
+    bible_folder,
+    "/",
+  ]);
   let chapter_path = string_combine(chapter_name, ".htm");
-  string_combine_multiple([url_secure(), "ebible.org/", bible_folder, "/"]);
-  let joined = path_join([folder, chapter_path]);
+  let joined = path_join([url_base, chapter_path]);
   let read = await file_read(joined);
   let parsed = html_parse(read);
   let notemarks = parsed.querySelectorAll(".notemark");
