ed967c729fdbaef6471d30d5c09b405bd3bcde6b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 16:01:17 -0400

    2024-06-03T20:01:16.641Zfunction_auto_return bible_chapter_parsed

--- public/src/bible_chapter_parsed.mjs
+++ public/src/bible_chapter_parsed.mjs
@@ -1,4 +1,4 @@
-import { html_parse_visit_tag_attribute } from "./html_parse_visit_tag_attribute.mjs";
+import { html_parse_visit_attribute_value } from "./html_parse_visit_attribute_value.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { path_join } from "./path_join.mjs";
 import { string_combine } from "./string_combine.mjs";
@@ -15,7 +15,7 @@ export async function bible_chapter_parsed(bible_folder, chapter_name) {
   let chapter_path = string_combine(chapter_name, ".htm");
   let joined = path_join([url_base, chapter_path]);
   let root = await html_cache_parse(joined);
-  html_parse_visit_tag_attribute(root, "class", "notemark", (v) => {
+  html_parse_visit_attribute_value(root, "class", "notemark", (v) => {
     let { node } = v;
     let { parent } = node;
     let { children } = parent;
