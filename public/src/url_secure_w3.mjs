f59c8bfd14654944a1655d3018859fbde1ff1628
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:14:53 -0400

    2024-06-03T19:14:53.519Zfunction_auto_return url_secure_w3

--- public/src/url_secure_w3.mjs
+++ public/src/url_secure_w3.mjs
@@ -1,3 +1,5 @@
+import { url_secure } from "./url_secure.mjs";
+import { string_combine } from "./string_combine.mjs";
 export function url_secure_w3() {
-  return "https://";
+  return string_combine(url_secure(), "www.");
 }
