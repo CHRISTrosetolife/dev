2e299c8be6bd48fa647f5e35fbcb85befa69d4bb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:08:05 -0400

    2024-06-03T19:08:04.953Zfunction_auto_return app_ceb_word_button

--- public/src/app_ceb_word_button.mjs
+++ public/src/app_ceb_word_button.mjs
@@ -2,6 +2,6 @@ import { app_ceb_word_button_audio_none } from "./app_ceb_word_button_audio_none
 import { app_ceb_audio } from "./app_ceb_audio.mjs";
 export async function app_ceb_word_button(root, cebuano) {
   let button = app_ceb_word_button_audio_none(root, cebuano);
-  await app_ceb_audio(cebuano);
+  (await app_ceb_audio(cebuano))();
   return button;
 }
