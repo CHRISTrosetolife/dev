3df8f8298ba5f06f388c4fbc57947e904c03ddad
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 16:59:29 -0400

    2024-06-03T20:59:29.303Zfunction_auto_return bible_verses_parse

--- public/src/bible_verses_parse.mjs
+++ public/src/bible_verses_parse.mjs
@@ -7,14 +7,13 @@ import { undefined_not_is } from "./undefined_not_is.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
 import { string_whitespace_normalize } from "./string_whitespace_normalize.mjs";
 import { string_trim } from "./string_trim.mjs";
-import { object_property_exists } from "./object_property_exists.mjs";
 export function bible_verses_parse(verses) {
   list_adder((la) =>
     each(verses, (v) => {
       let { children } = v;
       let verse_number, tokens;
       each(children, (c) => {
-        if (object_property_exists(c, "attribs")) {
+        if (c.type === "tag") {
           let { attribs } = c;
           if (object_property_get_or(attribs, "class", "") === "verse") {
             verse_number = html_parse_text(c);
