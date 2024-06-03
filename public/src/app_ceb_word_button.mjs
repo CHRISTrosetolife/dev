8f9a40f95914532b0426430167a37e49def32e7d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 14:19:30 -0400

    2024-06-03T18:19:29.195Zfunction_auto_return app_ceb_word_button_audio_none

--- public/src/app_ceb_word_button.mjs
+++ public/src/app_ceb_word_button.mjs
@@ -1,16 +1,7 @@
-import { app_ceb_word_style } from "./app_ceb_word_style.mjs";
+import { app_ceb_word_button_audio_none } from "./app_ceb_word_button_audio_none.mjs";
 import { app_ceb_audio } from "./app_ceb_audio.mjs";
-import { string_combine } from "./string_combine.mjs";
-import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
-export function app_ceb_word_button(root, cebuano) {
-  let button = html_button_width_full_text_click(
-    root,
-    string_combine("👂", cebuano),
-    async () => {
-      return await app_ceb_audio(cebuano);
-    },
-  );
-  app_ceb_word_style(button);
-  app_ceb_audio(cebuano);
+export async function app_ceb_word_button(root, cebuano) {
+  let button = app_ceb_word_button_audio_none(root, cebuano);
+  await app_ceb_audio(cebuano);
   return button;
 }
