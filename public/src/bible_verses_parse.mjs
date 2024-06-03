268428d7df09480815c4c42685500ec930e0afc3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 16:59:18 -0400

    2024-06-03T20:59:17.830Zfunction_auto_return bible_verses_parse

--- public/src/bible_verses_parse.mjs
+++ public/src/bible_verses_parse.mjs
@@ -7,21 +7,24 @@ import { undefined_not_is } from "./undefined_not_is.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
 import { string_whitespace_normalize } from "./string_whitespace_normalize.mjs";
 import { string_trim } from "./string_trim.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
 export function bible_verses_parse(verses) {
   list_adder((la) =>
     each(verses, (v) => {
       let { children } = v;
       let verse_number, tokens;
       each(children, (c) => {
-        let { attribs } = c;
-        if (object_property_get_or(attribs, "class", "") === "verse") {
-          verse_number = html_parse_text(c);
-          tokens = [];
-          la({
-            verse_number,
-            tokens,
-          });
-          return;
+        if (object_property_exists(c, "attribs")) {
+          let { attribs } = c;
+          if (object_property_get_or(attribs, "class", "") === "verse") {
+            verse_number = html_parse_text(c);
+            tokens = [];
+            la({
+              verse_number,
+              tokens,
+            });
+            return;
+          }
         }
         if (undefined_not_is(verse_number)) {
           if (c.type === "text") {
