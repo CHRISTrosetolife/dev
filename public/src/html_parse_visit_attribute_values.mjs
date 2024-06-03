1322c72d1999c009c4a20f3428964517417da3ec
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:58:30 -0400

    2024-06-03T19:58:30.393Zfunction_auto_return html_parse_visit_attribute_values

---
+++ public/src/html_parse_visit_attribute_values.mjs
@@ -0,0 +1,19 @@
+import { equal } from "./equal.mjs";
+import { html_parse_visit_attribute } from "./html_parse_visit_attribute.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+export function html_parse_visit_attribute_values(
+  root,
+  attribute_name,
+  value,
+  lambda,
+) {
+  function lambda_inner(v) {
+    let { node } = v;
+    let { attribs } = node;
+    let actual = object_property_get(attribs, attribute_name);
+    if (equal(actual, value)) {
+      lambda(v);
+    }
+  }
+  html_parse_visit_attribute(root, attribute_name, lambda_inner);
+}
