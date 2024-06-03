2619c6ca7e2dea3137abec7a1924e79ab446ab22
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 13:20:25 -0400

    2024-06-03T17:20:25.114Zfunction_auto_return ceb_bible_words_definitions_atoms

--- public/src/ceb_bible_words_definitions_atoms.mjs
+++ public/src/ceb_bible_words_definitions_atoms.mjs
@@ -32,7 +32,7 @@ export async function ceb_bible_words_definitions_atoms(skip, limit) {
         let next;
         for (let p of pairs) {
           let c = false;
-          let lists = [atom_result, previous];
+          let lists = [atom_result];
           each(lists, (list) => {
             for (let eq of [list_first, list_second]) {
               if (list_any(list, (a) => equal_by(a, p, eq))) {
