4854f16a277f4890a4e0405a0a3ea2878c37037d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-03 15:08:26 -0400

    2024-06-03T19:08:23.496Z  watch

--- public/src/app_ceb_word_button.mjs
+++ public/src/app_ceb_word_button.mjs
@@ -1,7 +1,7 @@
 import { app_ceb_word_button_audio_none } from "./app_ceb_word_button_audio_none.mjs";
 import { app_ceb_audio } from "./app_ceb_audio.mjs";
-export async function app_ceb_word_button(root, cebuano) {
+export function app_ceb_word_button(root, cebuano) {
   let button = app_ceb_word_button_audio_none(root, cebuano);
-  (await app_ceb_audio(cebuano))();
+  (()=> app_ceb_audio(cebuano))();
   return button;
 }
