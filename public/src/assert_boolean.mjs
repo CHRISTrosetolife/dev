f6df94c8551db6591f09c1e833d9795be73c8d97
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 12:16:26 -0400

    2024-06-04T16:16:26.252Zfunction_auto_return assert_boolean

--- public/src/assert_boolean.mjs
+++ public/src/assert_boolean.mjs
@@ -1,6 +1,5 @@
 import { assert_message_error } from "./assert_message_error.mjs";
 import { assert_boolean_message } from "./assert_boolean_message.mjs";
 export function assert_boolean(condition) {
-  let message = assert_message_error();
-  assert_boolean_message(condition, message);
+  assert_boolean_message(condition, assert_message_error);
 }
