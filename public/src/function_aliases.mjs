d702827d1a02077afb1ab1ab7a60b86e73a3acd6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 11:58:47 -0400

    2024-06-04T15:58:44.616Z  watch

--- public/src/function_aliases.mjs
+++ public/src/function_aliases.mjs
@@ -1,4 +1,6 @@
+import { object_property_initialize } from "./object_property_initialize.mjs";
+
 export function function_aliases() {
   let result = {};
-  function alias_add(fn, aliases) {}
+  function alias_add(fn, aliases) {each(aliases,a=>a)}
 }
