258f55bca306139f0e24113103553553bc5ba97f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 13:40:40 -0400

    2024-06-04T17:40:39.724Zfunction_auto_return function_aliases_generate

--- public/src/function_aliases_generate.mjs
+++ public/src/function_aliases_generate.mjs
@@ -1,28 +1,21 @@
-import { sandbox } from "./sandbox.mjs";
-import { function_open } from "./function_open.mjs";
-import { function_copy } from "./function_copy.mjs";
-import { watch } from "./watch.mjs";
-import { function_search } from "./function_search.mjs";
-import { function_auto } from "./function_auto.mjs";
-import { assert } from "./assert.mjs";
-import { each } from "./each.mjs";
-import { object_property_exists_not } from "./object_property_exists_not.mjs";
-import { object_property_set } from "./object_property_set.mjs";
-export function function_aliases_generate() {
-  let result = {};
-  function alias_add(fn, aliases) {
-    let { name } = fn;
-    each(aliases, (a) => {
-      assert(object_property_exists_not, [result, a]);
-      object_property_set(result, a, name);
-    });
-  }
-  alias_add(function_aliases_generate, ["al"]);
-  alias_add(function_auto, ["a"]);
-  alias_add(function_copy, ["c"]);
-  alias_add(function_open, ["o"]);
-  alias_add(function_search, ["s"]);
-  alias_add(sandbox, ["sb"]);
-  alias_add(watch, ["w"]);
-  return result;
+import { file_overwrite } from "./file_overwrite.mjs";
+import { js_code_array } from "./js_code_array.mjs";
+import { js_code_statement_return } from "./js_code_statement_return.mjs";
+import { function_new_generic } from "./function_new_generic.mjs";
+import { string_delimit } from "./string_delimit.mjs";
+import { list_map } from "./list_map.mjs";
+import { function_names } from "./function_names.mjs";
+export async function function_aliases_generate() {
+  let name = "functions_list";
+  let f = await function_names();
+  let delimited = list_map(f, string_delimit);
+  await function_new_generic(
+    name,
+    "",
+    js_code_statement_return(js_code_array(delimited)),
+    false,
+    [],
+    false,
+    file_overwrite,
+  );
 }
