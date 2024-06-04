e58151a8d1b4f4b0b4b67ce182803a9d81dbdbf1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 11:19:50 -0400

    2024-06-04T15:19:49.687Zfunction_auto_return bible_books

--- public/src/bible_books.mjs
+++ public/src/bible_books.mjs
@@ -1,3 +1,4 @@
+import { list_single } from "./list_single.mjs";
 import { html_parse_visit_attribute_value_list } from "./html_parse_visit_attribute_value_list.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { bible_url_base } from "./bible_url_base.mjs";
@@ -5,4 +6,5 @@ export async function bible_books(bible_folder) {
   let url_base = bible_url_base(bible_folder);
   let root = await html_cache_parse(url_base);
   let vnavs = html_parse_visit_attribute_value_list(root, "class", "vnav");
+  let vnav = list_single(vnavs);
 }
