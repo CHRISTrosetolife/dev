fdce523793d7c9ef8ed98fa4dfa76e1a622847f4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 12:04:06 -0400

    2024-06-04T16:04:06.050Zfunction_auto_return function_aliases

--- public/src/function_aliases.mjs
+++ public/src/function_aliases.mjs
@@ -3,6 +3,8 @@ import { object_property_set } from "./object_property_set.mjs";
 export function function_aliases() {
   let result = {};
   function alias_add(fn, aliases) {
-    each(aliases, (a) => object_property_set(result, a, fn.name));
+    each(aliases, (a) => {
+      object_property_set(result, a, fn.name);
+    });
   }
 }
