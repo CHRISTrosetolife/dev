6297c5d3e9d095bc6d5859833f31c719afd8ee43
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 11:37:48 -0400

    2024-06-04T15:37:47.438Zfunction_auto_return ceb_bible_words_2

--- public/src/ceb_bible_words_2.mjs
+++ public/src/ceb_bible_words_2.mjs
@@ -1,10 +1,8 @@
-import { html_parse_href } from "./html_parse_href.mjs";
-import { html_parse_visit_tag } from "./html_parse_visit_tag.mjs";
+import { html_parse_a_hrefs } from "./html_parse_a_hrefs.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { url_secure_w3 } from "./url_secure_w3.mjs";
 import { string_count_words } from "./string_count_words.mjs";
 import { string_ends_with } from "./string_ends_with.mjs";
-import { list_adder } from "./list_adder.mjs";
 import { each_async } from "./each_async.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_combine } from "./string_combine.mjs";
@@ -18,8 +16,7 @@ export async function ceb_bible_words_2() {
   let root = await html_cache_parse(url);
   let hrefs = html_parse_a_hrefs(root, function condition(href) {
     return string_ends_with(href, ".html");
-  });//
-  
+  });
   hrefs = list_map(hrefs, (href) => string_combine(url_base, href));
   await each_async(hrefs, async (href) => {
     await html_cache_parse(href);
@@ -28,14 +25,3 @@ export async function ceb_bible_words_2() {
   let mapped = string_count_words(text_split);
   return mapped;
 }
-function html_parse_a_hrefs(root, condition) {
-    return list_adder((la) => html_parse_visit_tag(root, "a", (v) => {
-        let { node } = v;
-        let href = html_parse_href(node);
-        if (condition(href)) {
-            la(href);
-        }
-    })
-    );
-}
-
