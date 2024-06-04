ced7da0fbb2a8482e1d6999b056d48fd4013090a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 12:58:58 -0400

    2024-06-04T16:58:56.475Zfunction_auto_return function_aliases

--- public/src/function_aliases.mjs
+++ public/src/function_aliases.mjs
@@ -1,3 +1,4 @@
+import { function_open } from "./function_open.mjs";
 import { function_copy } from "./function_copy.mjs";
 import { watch } from "./watch.mjs";
 import { function_search } from "./function_search.mjs";
@@ -17,6 +18,7 @@ export function function_aliases() {
   }
   alias_add(function_auto, ["a"]);
   alias_add(function_copy, ["c"]);
+  alias_add(function_open, ["o"]);
   alias_add(function_search, ["s"]);
   alias_add(watch, ["w"]);
   return result;
