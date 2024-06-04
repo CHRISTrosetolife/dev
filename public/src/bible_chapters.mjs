1115dbea29f502b91a027c1326741cff1eabc07b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 11:31:30 -0400

    2024-06-04T15:31:30.316Zfunction_auto_return bible_chapters

--- public/src/bible_chapters.mjs
+++ public/src/bible_chapters.mjs
@@ -1,3 +1,6 @@
-export function bible_chapters(bible_folder, book_name) {
-    let url_base = bible_url_base(bible_folder);
-    let root =  html_cache_parse(url_base);}
+import { html_cache_parse } from "./html_cache_parse.mjs";
+import { bible_url_base } from "./bible_url_base.mjs";
+export async function bible_chapters(bible_folder, book_name) {
+  let url_base = bible_url_base(bible_folder);
+  let root = await html_cache_parse(url_base);
+}
