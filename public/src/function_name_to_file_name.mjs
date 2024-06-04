e810a2292ccde4249a737b28eddc96e5540bbab2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 13:01:31 -0400

    2024-06-04T17:01:31.577Zfunction_auto_return function_name_to_file_name

--- public/src/function_name_to_file_name.mjs
+++ public/src/function_name_to_file_name.mjs
@@ -1,5 +1,7 @@
+import { function_unalias } from "./function_unalias.mjs";
 import { function_path_suffix } from "./function_path_suffix.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function function_name_to_file_name(function_name) {
+  function_name = function_unalias(function_name);
   return string_combine_multiple([function_name, function_path_suffix()]);
 }
