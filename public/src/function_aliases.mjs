4f62eb08cfee786c0e8f6dcc9bb5d2f254e5ea4e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 13:44:06 -0400

    2024-06-04T17:44:05.309Z  function_aliases_generate

--- public/src/function_aliases.mjs
+++ public/src/function_aliases.mjs
@@ -1,3 +1,11 @@
 export function function_aliases() {
-  return {};
+  return {
+    al: "function_aliases",
+    a: "function_auto",
+    c: "function_copy",
+    o: "function_open",
+    s: "function_search",
+    sb: "sandbox",
+    w: "watch",
+  };
 }
