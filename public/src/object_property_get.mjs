86ef4675705cafabac8b8b4f2f72fdfdb45d1c45
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 14:01:19 -0400

    2024-06-04T18:01:18.669Zfunction_auto_return object_property_get

--- public/src/object_property_get.mjs
+++ public/src/object_property_get.mjs
@@ -2,5 +2,6 @@ import { assert } from "./assert.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 export function object_property_get(object, property_name) {
   assert(object_property_exists, [object, property_name]);
-  return object[property_name];
+  let result = object[property_name];
+  return result;
 }
