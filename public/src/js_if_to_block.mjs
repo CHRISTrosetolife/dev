3bb3711a87e2173a02569a7ad1d269ac0da84937
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 11:05:51 -0400

    2024-06-04T15:05:50.737Zfunction_auto_return js_if_to_block

--- public/src/js_if_to_block.mjs
+++ public/src/js_if_to_block.mjs
@@ -1,9 +1,7 @@
-import { visit_log_node } from "./visit_log_node.mjs";
 import { each } from "./each.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function js_if_to_block(ast) {
-  return js_visit_node(ast, "BlockStatement", visit_log_node());
   return js_visit_node(ast, "IfStatement", (v) => {
     let { node } = v;
     let properties = ["consequent", "alternate"];
