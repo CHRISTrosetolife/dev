f3509cf655933b1a513624ba518bf19bb17a16a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 12:04:20 -0400

    2024-06-04T16:04:19.276Zfunction_auto_return function_aliases

--- public/src/function_aliases.mjs
+++ public/src/function_aliases.mjs
@@ -1,9 +1,12 @@
+import { assert } from "./assert.mjs";
 import { each } from "./each.mjs";
+import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 export function function_aliases() {
   let result = {};
   function alias_add(fn, aliases) {
     each(aliases, (a) => {
+      assert(object_property_exists_not, [result, a]);
       object_property_set(result, a, fn.name);
     });
   }
