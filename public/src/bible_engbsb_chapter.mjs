c74c3979ec3e627fbad5fa85b43b1016318d2090
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 11:22:52 -0400

    2024-06-04T15:22:50.012Zfunction_auto_return bible_engbsb_chapter

--- public/src/bible_engbsb_chapter.mjs
+++ public/src/bible_engbsb_chapter.mjs
@@ -6,10 +6,11 @@ import { list_adder } from "./list_adder.mjs";
 export async function bible_engbsb_chapter(chapter_name) {
   let root = await bible_chapter_parsed("engbsb", chapter_name);
   let attribute_values = ["m", "pc", "pmo", "q", "li"];
+  let attribute_name = "class";
   let verses = list_adder((la) =>
     html_parse_visit_attribute_values(
       root,
-      "class",
+      attribute_name,
       attribute_values,
       list_adder_visit(la),
     ),
