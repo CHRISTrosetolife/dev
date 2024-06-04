6f8a3d2a0a1575df59022ebc68950bc2a9154ecf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 12:06:41 -0400

    2024-06-04T16:06:40.268Zfunction_auto_return run

--- public/src/run.mjs
+++ public/src/run.mjs
@@ -1,8 +1,10 @@
+import { function_aliases } from "./function_aliases.mjs";
 import { list_first_remaining } from "./list_first_remaining.mjs";
 import { function_run } from "./function_run.mjs";
 import { process_argv_remaining } from "./process_argv_remaining.mjs";
 export async function run() {
   let r = process_argv_remaining();
   let { first, remaining } = list_first_remaining(r);
+  let aliases = function_aliases();
   return await function_run(first, remaining);
 }
