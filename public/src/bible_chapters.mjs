07e60422fc1e3fc547570bf64bed9d9b2e4f7aed
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 11:32:21 -0400

    2024-06-04T15:32:21.562Zfunction_auto_return bible_chapters

--- public/src/bible_chapters.mjs
+++ public/src/bible_chapters.mjs
@@ -1,6 +1,8 @@
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { bible_url_base } from "./bible_url_base.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function bible_chapters(bible_folder, book_name) {
   let url_base = bible_url_base(bible_folder);
+  let url = string_combine_multiple(url_base, book_name);
   let root = await html_cache_parse(url_base);
 }
