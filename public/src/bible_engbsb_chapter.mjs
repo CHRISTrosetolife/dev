4b81cffa473c33eba60a2d2ab3b3188f354170d4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 12:24:45 -0400

    2024-06-04T16:24:45.227Zfunction_auto_return bible_engbsb_chapter

--- public/src/bible_engbsb_chapter.mjs
+++ public/src/bible_engbsb_chapter.mjs
@@ -3,7 +3,7 @@ import { bible_verses_parse } from "./bible_verses_parse.mjs";
 import { bible_chapter_parsed } from "./bible_chapter_parsed.mjs";
 export async function bible_engbsb_chapter(chapter_name) {
   let root = await bible_chapter_parsed("engbsb", chapter_name);
-  let attribute_values = ["m", "pc", "pmo", "q", "li"];
+  let attribute_values = ["m", "pc", "pmo", "q", "li", "q2"];
   let verses = html_parse_visit_classes_list(root, attribute_values);
   let eng = bible_verses_parse(verses);
   return eng;
