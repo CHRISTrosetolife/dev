683964abf813fa4fcff97e35970d2bf700823a78
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 12:22:26 -0400

    2024-06-04T16:22:25.827Zfunction_auto_return assert_message

--- public/src/assert_message.mjs
+++ public/src/assert_message.mjs
@@ -1,4 +1,4 @@
-import { returns } from "./returns.mjs";
-export function assert_message(fn, args) {
-  returns(fn, true, args);
+import { returns_message } from "./returns_message.mjs";
+export function assert_message(fn, args, message_get) {
+  returns_message(fn, true, args, message_get);
 }
