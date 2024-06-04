831ae784fd11f89116c6b7b9d7eac567a8c3fb11
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 11:13:36 -0400

    2024-06-04T15:13:36.003Zfunction_auto_return bible_chapter_parsed

--- public/src/bible_chapter_parsed.mjs
+++ public/src/bible_chapter_parsed.mjs
@@ -1,7 +1,7 @@
+import { bible_url_base } from "./bible_url_base.mjs";
 import { html_parse_visit_attribute_value } from "./html_parse_visit_attribute_value.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { string_combine } from "./string_combine.mjs";
-import { url_secure } from "./url_secure.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { list_remove } from "./list_remove.mjs";
 export async function bible_chapter_parsed(bible_folder, chapter_name) {
@@ -17,12 +17,3 @@ export async function bible_chapter_parsed(bible_folder, chapter_name) {
   });
   return root;
 }
-function bible_url_base(bible_folder) {
-    return string_combine_multiple([
-        url_secure(),
-        "ebible.org/",
-        bible_folder,
-        "/",
-    ]);
-}
-
