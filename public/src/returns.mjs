66f82806ba847870ee8ef6c5f4829aebe0a6d724
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 12:14:54 -0400

    2024-06-04T16:14:53.701Zfunction_auto_return returns

--- public/src/returns.mjs
+++ public/src/returns.mjs
@@ -1,7 +1,8 @@
-import { assert_boolean } from "./assert_boolean.mjs";
+import { assert_boolean_message } from "./assert_boolean_message.mjs";
 import { equal } from "./equal.mjs";
+import { json_to } from "./json_to.mjs";
 export function returns(fn, expected, args) {
   let actual = fn(...args);
   let e = equal(actual, expected);
-  assert_boolean(e);
+  assert_boolean_message(e, json_to(args));
 }
