35e54efc2dec01a0ab0cfad2f136d6ecca07ed0f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 11:18:06 -0400

    2024-06-04T15:18:06.460Zfunction_auto_return bible_ceb_chapter

--- public/src/bible_ceb_chapter.mjs
+++ public/src/bible_ceb_chapter.mjs
@@ -1,6 +1,4 @@
-import { list_adder_visit } from "./list_adder_visit.mjs";
-import { html_parse_visit_attribute_value } from "./html_parse_visit_attribute_value.mjs";
-import { list_adder } from "./list_adder.mjs";
+import { html_parse_visit_attribute_value_list } from "./html_parse_visit_attribute_value_list.mjs";
 import { bible_engbsb_chapter } from "./bible_engbsb_chapter.mjs";
 import { bible_verses_parse } from "./bible_verses_parse.mjs";
 import { bible_chapter_parsed } from "./bible_chapter_parsed.mjs";
@@ -17,9 +15,13 @@ import { js_code_return } from "./js_code_return.mjs";
 export async function bible_ceb_chapter(chapter_name) {
   let eng = await bible_engbsb_chapter(chapter_name);
   let parsed_ceb = await bible_chapter_parsed("cebulb", chapter_name);
-  let attribute_name = "class";//
+  let attribute_name = "class";
   let attribute_value = "p";
-  let verses_ceb = html_parse_visit_attribute_value_list(parsed_ceb, attribute_name, attribute_value);
+  let verses_ceb = html_parse_visit_attribute_value_list(
+    parsed_ceb,
+    attribute_name,
+    attribute_value,
+  );
   let ceb = bible_verses_parse(verses_ceb);
   assert(equal_by, [eng, ceb, list_length]);
   let symbols = [",", "1", "2", ".", ";", "“", "”", ":"];
@@ -50,5 +52,4 @@ export async function bible_ceb_chapter(chapter_name) {
   let body_string = js_code_return(json_to(data));
   let fn_name = `bible_data_${string_case_lower(chapter_name)}`;
   return data;
-}//
-
+}
