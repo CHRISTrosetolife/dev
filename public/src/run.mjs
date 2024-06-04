6d5e5d22c077d318b1d59b1ef3b8baa1a012fe13
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 12:07:08 -0400

    2024-06-04T16:07:07.804Zfunction_auto_return run

--- public/src/run.mjs
+++ public/src/run.mjs
@@ -2,9 +2,14 @@ import { function_aliases } from "./function_aliases.mjs";
 import { list_first_remaining } from "./list_first_remaining.mjs";
 import { function_run } from "./function_run.mjs";
 import { process_argv_remaining } from "./process_argv_remaining.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 export async function run() {
   let r = process_argv_remaining();
   let { first, remaining } = list_first_remaining(r);
   let aliases = function_aliases();
+  if (object_property_exists(aliases, first)) {
+    first = object_property_get(aliases, first);
+  }
   return await function_run(first, remaining);
 }
