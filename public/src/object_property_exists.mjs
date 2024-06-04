b6b3d61efc6b1b425209ddda382a70d164a53032
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 13:49:50 -0400

    2024-06-04T17:49:48.905Zfunction_auto_return object_property_exists

--- public/src/object_property_exists.mjs
+++ public/src/object_property_exists.mjs
@@ -1,8 +1,3 @@
-import { log } from "./log.mjs";
 export function object_property_exists(object, property_name) {
-  log({
-    object,
-    property_name,
-  });
-  return Object.hasOwnProperty.bind(object)(property_name);
+  return property_name in object;
 }
