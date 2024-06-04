827a58a5bb53fec170efa5626f151746edcfdafb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 12:04:31 -0400

    2024-06-04T16:04:30.294Zfunction_auto_return function_aliases

--- public/src/function_aliases.mjs
+++ public/src/function_aliases.mjs
@@ -5,6 +5,7 @@ import { object_property_set } from "./object_property_set.mjs";
 export function function_aliases() {
   let result = {};
   function alias_add(fn, aliases) {
+    let name = fn;
     each(aliases, (a) => {
       assert(object_property_exists_not, [result, a]);
       object_property_set(result, a, fn.name);
