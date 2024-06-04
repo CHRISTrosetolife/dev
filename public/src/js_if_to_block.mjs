0e1af34caa499af91b8f71327661374714fcc1ac
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 11:03:07 -0400

    2024-06-04T15:03:06.971Zfunction_auto_return js_if_to_block

--- public/src/js_if_to_block.mjs
+++ public/src/js_if_to_block.mjs
@@ -1,6 +1,7 @@
-import { visit_log_node } from "./visit_log_node.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_if_to_block(ast) {
-  return js_visit_node(ast, "IfStatement", visit_log_node());
+  return js_visit_node(ast, "IfStatement", (v) => {
+    let node = v;
+  });
   if (0) 1;
 }
