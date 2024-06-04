2b44bf5fa1ef2749f9511e35bc85641100517772
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 11:04:25 -0400

    2024-06-04T15:04:24.915Zfunction_auto_return js_if_to_block

--- public/src/js_if_to_block.mjs
+++ public/src/js_if_to_block.mjs
@@ -1,10 +1,13 @@
 import { each } from "./each.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 export function js_if_to_block(ast) {
   return js_visit_node(ast, "IfStatement", (v) => {
     let { node } = v;
     let properties = ["consequent", "alternate"];
-    each(properties, (p) => {});
+    each(properties, (p) => {
+      let value = object_property_get(node, p);
+    });
   });
   if (0) 1;
 }
