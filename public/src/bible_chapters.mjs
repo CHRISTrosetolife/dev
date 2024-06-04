b00326c09171dad9d51824a304b30e0893d8eb47
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 11:39:19 -0400

    2024-06-04T15:39:19.343Zfunction_auto_return bible_chapters

--- public/src/bible_chapters.mjs
+++ public/src/bible_chapters.mjs
@@ -3,12 +3,14 @@ import { html_cache_parse } from "./html_cache_parse.mjs";
 import { bible_url_base } from "./bible_url_base.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
+import { string_prefix_without } from "./string_prefix_without.mjs";
 export async function bible_chapters(bible_folder, book_name) {
   let url_base = bible_url_base(bible_folder);
   let url = string_combine_multiple([url_base, book_name, ".htm"]);
   let root = await html_cache_parse(url);
   let hrefs = html_parse_a_hrefs(root, function condition(href) {
     if (string_starts_with(href, book_name)) {
+      let without = string_prefix_without(href, book_name);
     }
   });
   return hrefs;
