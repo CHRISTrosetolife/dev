c521fcfc9db058cc9f0478114c34fa74c1294a16
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-04 13:39:23 -0400

    2024-06-04T17:39:19.688Z  c function_aliases function_aliases_generate

---
+++ public/src/function_aliases_generate.mjs
@@ -0,0 +1,28 @@
+import { sandbox } from "./sandbox.mjs";
+import { function_open } from "./function_open.mjs";
+import { function_copy } from "./function_copy.mjs";
+import { watch } from "./watch.mjs";
+import { function_search } from "./function_search.mjs";
+import { function_auto } from "./function_auto.mjs";
+import { assert } from "./assert.mjs";
+import { each } from "./each.mjs";
+import { object_property_exists_not } from "./object_property_exists_not.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+export function function_aliases_generate() {
+  let result = {};
+  function alias_add(fn, aliases) {
+    let { name } = fn;
+    each(aliases, (a) => {
+      assert(object_property_exists_not, [result, a]);
+      object_property_set(result, a, name);
+    });
+  }
+  alias_add(function_aliases_generate, ["al"]);
+  alias_add(function_auto, ["a"]);
+  alias_add(function_copy, ["c"]);
+  alias_add(function_open, ["o"]);
+  alias_add(function_search, ["s"]);
+  alias_add(sandbox, ["sb"]);
+  alias_add(watch, ["w"]);
+  return result;
+}
