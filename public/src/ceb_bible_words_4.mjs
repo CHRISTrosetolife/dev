4350aabf97bf76aa68d975b92d701ddbeaeb2611
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 15:23:31 -0400

    2024-06-05T19:23:30.425Zfunction_auto_return ceb_bible_words_4

--- public/src/ceb_bible_words_4.mjs
+++ public/src/ceb_bible_words_4.mjs
@@ -1,13 +1,13 @@
 import { html_parse_visit_class_single } from "./html_parse_visit_class_single.mjs";
 import { log } from "./log.mjs";
 import { each_async } from "./each_async.mjs";
-import { string_starts_with_curry } from "./string_starts_with_curry.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { html_parse_a_hrefs } from "./html_parse_a_hrefs.mjs";
 import { url_secure_w3 } from "./url_secure_w3.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { list_length } from "./list_length.mjs";
+import { tautology } from "./tautology.mjs";
 export async function ceb_bible_words_4() {
   let url_base = string_combine_multiple([
     url_secure_w3(),
@@ -16,7 +16,7 @@ export async function ceb_bible_words_4() {
   let root_bible = await html_cache_parse(url_base);
   let newLocal = "ym-grid linearize-level-2";
   let s = html_parse_visit_class_single(root_bible, newLocal);
-  let hrefs = html_parse_a_hrefs(s, string_starts_with_curry("B"));
+  let hrefs = html_parse_a_hrefs(s, tautology);
   return list_length(hrefs);
   await each_async(hrefs, async (href) => {
     log({
