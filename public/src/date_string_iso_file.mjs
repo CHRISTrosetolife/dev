8abe3069f775a3b9de13e718f0e5e2bace537064
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 16:58:50 -0400

    2024-06-05T20:58:45.434Zfunction_auto_return date_string_iso_file

--- public/src/date_string_iso_file.mjs
+++ public/src/date_string_iso_file.mjs
@@ -1,6 +1,8 @@
+import { string_replace } from "./string_replace.mjs";
 import { string_replace_multiple } from "./string_replace_multiple.mjs";
 import { date_string_iso } from "./date_string_iso.mjs";
 export function date_string_iso_file() {
   let d = date_string_iso();
   d = string_replace_multiple(date_string_iso, [":", "."], "_");
+  d = string_replace(d, "Z", "");
 }
