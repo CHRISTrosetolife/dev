488dd1db41241f566003f5841010d8cc0b43f6de
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 17:19:53 -0400

    2024-06-05T21:19:52.672Zfunction_auto_return assert_not_async

--- public/src/assert_not_async.mjs
+++ public/src/assert_not_async.mjs
@@ -1,5 +1,10 @@
+import { json_to } from "./json_to.mjs";
 import { assert_not_message_async } from "./assert_not_message_async.mjs";
-import { assert_message_error } from "./assert_message_error.mjs";
 export async function assert_not_async(fn, args) {
-  await assert_not_message_async(fn, args, assert_message_error);
+  await assert_not_message_async(fn, args, () =>
+    json_to({
+      fn: fn.name,
+      args,
+    }),
+  );
 }
