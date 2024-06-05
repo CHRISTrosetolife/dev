f6731c0570e7ec2693884cfde4598618f58cc7d4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 17:19:31 -0400

    2024-06-05T21:19:30.464Zfunction_auto_return returns_async

--- public/src/returns_async.mjs
+++ public/src/returns_async.mjs
@@ -1,5 +1,11 @@
 import { returns_message_async } from "./returns_message_async.mjs";
-import { assert_message_error } from "./assert_message_error.mjs";
+import { json_to } from "./json_to.mjs";
 export async function returns_async(fn, expected, args) {
-  await returns_message_async(fn, expected, args, assert_message_error);
+  await returns_message_async(fn, expected, args, () =>
+    json_to({
+      fn: fn.name,
+      expected,
+      args,
+    }),
+  );
 }
