03a05b91a70a0c98dae0da62e32da0237aef0bba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 17:46:25 -0400

    2024-06-05T21:46:23.848Zfunction_auto_return ceb_bible_words_3

--- public/src/ceb_bible_words_3.mjs
+++ public/src/ceb_bible_words_3.mjs
@@ -1,3 +1,5 @@
+import { html_parse_text } from "./html_parse_text.mjs";
+import { list_map } from "./list_map.mjs";
 import { html_parse_visit_tag_list } from "./html_parse_visit_tag_list.mjs";
 import { log } from "./log.mjs";
 import { each_async } from "./each_async.mjs";
@@ -21,5 +23,6 @@ export async function ceb_bible_words_3() {
     let url = string_combine(url_base, href);
     let root = await html_cache_parse(url);
     let t = html_parse_visit_tag_list(root, "table");
+    log(list_map(t, html_parse_text));
   });
 }
