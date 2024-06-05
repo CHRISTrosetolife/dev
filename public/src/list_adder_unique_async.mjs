7daf9a069252ebc304205179f98cbe6bd1708918
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 10:41:48 -0400

    2024-06-05T14:41:47.934Zfunction_auto_return list_adder_unique_async

--- public/src/list_adder_unique_async.mjs
+++ public/src/list_adder_unique_async.mjs
@@ -1,8 +1,8 @@
 import { list_includes_not } from "./list_includes_not.mjs";
 import { list_add } from "./list_add.mjs";
-export function list_adder_unique_async(lambda) {
+export async function list_adder_unique_async(lambda) {
   let result = [];
-  lambda((item) => {
+  await lambda((item) => {
     if (list_includes_not(result, item)) {
       list_add(result, item);
     }
