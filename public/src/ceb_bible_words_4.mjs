33a150b7d68b01e6103533307261bcb04e21aa77
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 15:36:55 -0400

    2024-06-05T19:36:54.707Zfunction_auto_return ceb_bible_words_4

--- public/src/ceb_bible_words_4.mjs
+++ public/src/ceb_bible_words_4.mjs
@@ -27,6 +27,6 @@ export async function ceb_bible_words_4() {
     });
     let url = string_combine(url_base, href);
     let root_book = await html_cache_parse(url);
-    html_parse_visit_class_list;
+    let chapter_elements = html_parse_visit_class_list(root_book, "chap");
   });
 }
