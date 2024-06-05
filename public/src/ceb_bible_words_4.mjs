a7803b64adb6681dfaf466f6a0031f49f6673a06
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 15:23:18 -0400

    2024-06-05T19:23:18.265Zfunction_auto_return ceb_bible_words_4

--- public/src/ceb_bible_words_4.mjs
+++ public/src/ceb_bible_words_4.mjs
@@ -7,6 +7,7 @@ import { html_parse_a_hrefs } from "./html_parse_a_hrefs.mjs";
 import { url_secure_w3 } from "./url_secure_w3.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_combine } from "./string_combine.mjs";
+import { list_length } from "./list_length.mjs";
 export async function ceb_bible_words_4() {
   let url_base = string_combine_multiple([
     url_secure_w3(),
@@ -15,8 +16,8 @@ export async function ceb_bible_words_4() {
   let root_bible = await html_cache_parse(url_base);
   let newLocal = "ym-grid linearize-level-2";
   let s = html_parse_visit_class_single(root_bible, newLocal);
-  return s;
-  let hrefs = html_parse_a_hrefs(root_bible, string_starts_with_curry("B"));
+  let hrefs = html_parse_a_hrefs(s, string_starts_with_curry("B"));
+  return list_length(hrefs);
   await each_async(hrefs, async (href) => {
     log({
       href,
