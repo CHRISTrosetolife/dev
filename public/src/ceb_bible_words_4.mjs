ac122615b69d66b8e424aad7238efd411f45a1f3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 15:24:09 -0400

    2024-06-05T19:24:09.145Zfunction_auto_return ceb_bible_words_4

--- public/src/ceb_bible_words_4.mjs
+++ public/src/ceb_bible_words_4.mjs
@@ -7,6 +7,7 @@ import { url_secure_w3 } from "./url_secure_w3.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { tautology } from "./tautology.mjs";
+import { list_remove } from "./list_remove.mjs";
 export async function ceb_bible_words_4() {
   let url_base = string_combine_multiple([
     url_secure_w3(),
@@ -16,6 +17,7 @@ export async function ceb_bible_words_4() {
   let newLocal = "ym-grid linearize-level-2";
   let s = html_parse_visit_class_single(root_bible, newLocal);
   let hrefs = html_parse_a_hrefs(s, tautology);
+  list_remove(hrefs, "index_en.htm#1");
   return hrefs;
   await each_async(hrefs, async (href) => {
     log({
