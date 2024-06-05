f8ac4c935a962743465a9d11cb74227d012a8be9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 15:22:07 -0400

    2024-06-05T19:22:06.171Zfunction_auto_return ceb_bible_words_4

--- public/src/ceb_bible_words_4.mjs
+++ public/src/ceb_bible_words_4.mjs
@@ -14,9 +14,8 @@ export async function ceb_bible_words_4() {
     "wordproject.org/bibles/ceb/",
   ]);
   let root_bible = await html_cache_parse(url_base);
-  list_single(
-    html_parse_visit_class_list(root_bible, "ym-grid linearize-level-2"),
-  );
+  let newLocal = "ym-grid linearize-level-2";
+  list_single(html_parse_visit_class_list(root_bible, newLocal));
   return;
   let hrefs = html_parse_a_hrefs(root_bible, string_starts_with_curry("B"));
   await each_async(hrefs, async (href) => {
