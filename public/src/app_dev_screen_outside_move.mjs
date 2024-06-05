7919a4704bc7afe8d459b22d3cbb11c15586c064
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 16:16:39 -0400

    2024-06-05T20:16:37.278Zfunction_auto_return app_dev_screens

---
+++ public/src/app_dev_screen_outside_move.mjs
@@ -0,0 +1,13 @@
+import { app_dev_screen_img } from "./app_dev_screen_img.mjs";
+import { html_span_text } from "./html_span_text.mjs";
+export function app_dev_screen_outside_move() {
+  return {
+    name: function outside_move_name(root) {
+      return html_span_text(root, "outside_move");
+    },
+    screen: function outside_move_screen(root) {
+      let i = 1;
+      app_dev_screen_img(app_dev_screen_outside_move, root, i++);
+    },
+  };
+}
