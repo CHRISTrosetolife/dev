6619fa48a122953542a59790f1cf712f854330f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 10:25:37 -0400

    2024-06-05T14:25:36.839Zfunction_auto_return html_parse_visit_attribute_value_list

--- public/src/html_parse_visit_attribute_value_list.mjs
+++ public/src/html_parse_visit_attribute_value_list.mjs
@@ -2,11 +2,13 @@ import { list_adder_visit } from "./list_adder_visit.mjs";
 import { html_parse_visit_attribute_value } from "./html_parse_visit_attribute_value.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { assert } from "./assert.mjs";
+import { list_is } from "./list_is.mjs";
 export function html_parse_visit_attribute_value_list(
   node,
   attribute_name,
   attribute_value,
 ) {
+  let a = !list_is(node);
   assert(list_not_is, [node]);
   return list_adder((la) =>
     html_parse_visit_attribute_value(
