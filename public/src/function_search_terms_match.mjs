fbc34d1e1a2932549b70a0131a7e81233d5aa56f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 15:10:28 -0400

    2024-06-05T19:10:28.083Zfunction_auto_return function_search_terms_match

--- public/src/function_search_terms_match.mjs
+++ public/src/function_search_terms_match.mjs
@@ -1,6 +1,5 @@
 import { function_name_to_parts } from "./function_name_to_parts.mjs";
-import { list_includes_multiple } from "./list_includes_multiple.mjs";
 export function function_search_terms_match(function_name, terms) {
   let fn_name_parts = function_name_to_parts(function_name);
-  return list_includes_multiple(fn_name_parts, terms);
+  list_all;
 }
