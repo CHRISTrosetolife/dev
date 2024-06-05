b6b0b7b6cb5b85f5d58e12817b3c0a999d57c602
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 16:40:39 -0400

    2024-06-05T20:40:38.796Zfunction_auto_return folder_files_move

--- public/src/folder_files_move.mjs
+++ public/src/folder_files_move.mjs
@@ -9,7 +9,7 @@ export async function folder_files_move(
 ) {
   let lambda = file_rename;
   await folder_read_each(input_directory, file_extension, lambda);
-  async function lambda(file_path) {
+  async function lambda_each(file_path) {
     let file_name = path_parse_base(file_path);
     let file_path_new = path_join([output_directory, file_name]);
     await file_rename(file_path, file_path_new);
