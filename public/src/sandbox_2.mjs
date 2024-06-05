fdade0f91307f115f7bb5be6dc8ac622b74773d6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 16:21:23 -0400

    2024-06-05T20:21:22.894Z  noop

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -3,5 +3,12 @@ export async function sandbox_2() {
   let a = 1;
   let b = 2;
   let c = 3;
-  log(a + b * c);
+  log(sandbox_add_multiply(a, b, c));
 }
+function sandbox_add_multiply(a, b, c) {
+    return a + sandbox_multiply(b, c);
+}
+function sandbox_multiply(b, c) {
+    return b * c;
+}
+
