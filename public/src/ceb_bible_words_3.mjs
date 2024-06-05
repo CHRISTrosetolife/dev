2fa26b403369c00a3062e094195bc7d1fcd33ea7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 18:06:43 -0400

    2024-06-05T22:06:42.847Zfunction_auto_return ceb_bible_words_3

--- public/src/ceb_bible_words_3.mjs
+++ public/src/ceb_bible_words_3.mjs
@@ -1,11 +1,9 @@
 import { bible_ceb_3_chapter } from "./bible_ceb_3_chapter.mjs";
 import { bible_ceb_3_hrefs } from "./bible_ceb_3_hrefs.mjs";
-import { bible_ceb_3_url_base } from "./bible_ceb_3_url_base.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 export async function ceb_bible_words_3() {
   let hrefs = await bible_ceb_3_hrefs();
   await each_index_async(hrefs, async (href_, index) => {
-    let url_base = bible_ceb_3_url_base();
-    await bible_ceb_3_chapter(index, url_base);
+    await bible_ceb_3_chapter(index);
   });
 }
