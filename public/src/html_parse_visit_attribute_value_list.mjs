b83c090769cc05aee1f7026f03c6da1eafb2b68c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 10:25:58 -0400

    2024-06-05T14:25:57.462Zfunction_auto_return html_parse_visit_attribute_value_list

--- public/src/html_parse_visit_attribute_value_list.mjs
+++ public/src/html_parse_visit_attribute_value_list.mjs
@@ -1,14 +1,11 @@
-import { list_not_is } from "./list_not_is.mjs";
 import { list_adder_visit } from "./list_adder_visit.mjs";
 import { html_parse_visit_attribute_value } from "./html_parse_visit_attribute_value.mjs";
 import { list_adder } from "./list_adder.mjs";
-import { assert } from "./assert.mjs";
 export function html_parse_visit_attribute_value_list(
   node,
   attribute_name,
   attribute_value,
 ) {
-  assert(list_not_is, [node]);
   return list_adder((la) =>
     html_parse_visit_attribute_value(
       node,
