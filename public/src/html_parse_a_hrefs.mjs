ae88aca3af3aef69b2109cb89273e7c8ff82e957
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 15:00:50 -0400

    2024-06-05T19:00:49.154Zfunction_auto_return html_parse_a_hrefs

--- public/src/html_parse_a_hrefs.mjs
+++ public/src/html_parse_a_hrefs.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
 import { html_parse_visit_tag } from "./html_parse_visit_tag.mjs";
 import { list_adder } from "./list_adder.mjs";
@@ -6,6 +7,9 @@ export function html_parse_a_hrefs(root, condition) {
     html_parse_visit_tag(root, "a", (v) => {
       let { node } = v;
       let href = html_parse_href(node);
+      log({
+        href,
+      });
       if (condition(href)) {
         la(href);
       }
