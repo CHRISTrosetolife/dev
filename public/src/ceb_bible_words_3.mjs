cfb5bb8afe0eb2b3a5b693dbea893e0b41648194
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 14:55:43 -0400

    2024-06-05T18:55:43.034Zfunction_auto_return ceb_bible_words_3

--- public/src/ceb_bible_words_3.mjs
+++ public/src/ceb_bible_words_3.mjs
@@ -1,1 +1,8 @@
-export function ceb_bible_words_3() {}
+import { url_secure_w3 } from "./url_secure_w3.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function ceb_bible_words_3() {
+  let url_base = string_combine_multiple([
+    url_secure_w3(),
+    "talibon.com/bible/",
+  ]);
+}
