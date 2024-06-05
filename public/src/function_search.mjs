f41f34f5e0857c5f941c91c4ad828da842e16655
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 15:09:05 -0400

    2024-06-05T19:09:04.932Zfunction_auto_return function_search

--- public/src/function_search.mjs
+++ public/src/function_search.mjs
@@ -3,6 +3,8 @@ import { string_split_comma } from "./string_split_comma.mjs";
 import { function_names } from "./function_names.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { function_search_terms_match } from "./function_search_terms_match.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function function_search(query) {
   let terms = string_split_comma(query);
   let fn_names = await function_names();
@@ -10,6 +12,8 @@ export async function function_search(query) {
     return function_search_terms_match(fn_name, terms);
   });
   let result = {};
-  each(matches, (m) => {});
+  each(matches, (m) => {
+    object_property_set(result, m, function_name_to_path(m));
+  });
   return matches;
 }
