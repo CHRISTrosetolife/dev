e6dd7ae13d0fef0ec11a6d552a5c542cee397b06
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 10:41:12 -0400

    2024-06-05T14:41:11.374Z  c list_adder_unique list_adder_unique_async

---
+++ public/src/list_adder_unique_async.mjs
@@ -0,0 +1,11 @@
+import { list_includes } from "./list_includes.mjs";
+import { list_add } from "./list_add.mjs";
+export function list_adder_unique_async(lambda) {
+  let result = [];
+  lambda((item) => {
+    if (!list_includes(result, item)) {
+      list_add(result, item);
+    }
+  });
+  return result;
+}
