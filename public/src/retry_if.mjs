93c5c6ddd777104cb6f8fddca2b4efc79c2c0224
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 13:33:24 -0400

    2024-06-05T17:33:23.843Zfunction_auto_return retry_if

--- public/src/retry_if.mjs
+++ public/src/retry_if.mjs
@@ -2,13 +2,15 @@ import { string_to } from "./string_to.mjs";
 import { string_includes } from "./string_includes.mjs";
 import { list_any } from "./list_any.mjs";
 import { log } from "./log.mjs";
+import { list_map } from "./list_map.mjs";
 export function retry_if(list) {
   return function retry_if_inner(e) {
     let s = string_to(e);
+    let m = list_map(list, (i) => string_includes(s, i));
     log({
       e,
       s,
     });
-    return list_any(list, (i) => string_includes(s, i));
+    return list_any();
   };
 }
