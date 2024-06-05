ee22a9a1964e6d21be1ed8f2a17d201ff98b6884
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 17:16:14 -0400

    2024-06-05T21:16:14.042Zfunction_auto_return file_copy_generic

--- public/src/file_copy_generic.mjs
+++ public/src/file_copy_generic.mjs
@@ -1,3 +1,4 @@
+import { assert_not_message_async } from "./assert_not_message_async.mjs";
 import { file_open } from "./file_open.mjs";
 import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
 import { import_node } from "./import_node.mjs";
@@ -6,9 +7,8 @@ import { object_property_get } from "./object_property_get.mjs";
 import { global_get } from "./global_get.mjs";
 import { web_is } from "./web_is.mjs";
 import { file_exists } from "./file_exists.mjs";
-import { assert_not_async } from "./assert_not_async.mjs";
 export async function file_copy_generic(file_name_to, file_name_from, open) {
-  await assert_not_async(file_exists, [file_name_to]);
+  await assert_not_message_async(file_exists, [file_name_to]);
   if (web_is()) {
     let { files } = global_get();
     let existing = object_property_get(files, file_name_from);
