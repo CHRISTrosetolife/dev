c7b1c9de3320cc111cfd60928d747c5437295988
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 17:20:45 -0400

    2024-06-05T21:20:44.510Zfunction_auto_return file_copy

--- public/src/file_copy.mjs
+++ public/src/file_copy.mjs
@@ -1,5 +1,5 @@
 import { file_copy_generic } from "./file_copy_generic.mjs";
 export async function file_copy(file_name_from, file_name_to) {
   let open = true;
-  await file_copy_generic(file_name_to, file_name_from, open);
+  await file_copy_generic(file_name_from, file_name_to, open);
 }
