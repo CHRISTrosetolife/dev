0c4cbbaae2440234bff513aad22997374c7c0043
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 10:26:13 -0400

    2024-06-05T14:26:13.303Zfunction_auto_return html_parse_visit

--- public/src/html_parse_visit.mjs
+++ public/src/html_parse_visit.mjs
@@ -1,7 +1,10 @@
+import { list_not_is } from "./list_not_is.mjs";
+import { assert } from "./assert.mjs";
 import { object_property_get_or } from "./object_property_get_or.mjs";
 import { tautology } from "./tautology.mjs";
 import { visit } from "./visit.mjs";
 export function html_parse_visit(root, lambda) {
+  assert(list_not_is, [node]);
   visit(
     root,
     (n) => object_property_get_or(n, "children", []),
