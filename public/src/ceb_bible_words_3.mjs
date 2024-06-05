72b188a17a604df65d49f1ff9bd2a2c2c2baa8d7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 14:59:45 -0400

    2024-06-05T18:59:45.396Zfunction_auto_return ceb_bible_words_3

--- public/src/ceb_bible_words_3.mjs
+++ public/src/ceb_bible_words_3.mjs
@@ -1,3 +1,4 @@
+import { list_length } from "./list_length.mjs";
 import { string_starts_with_curry } from "./string_starts_with_curry.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { html_parse_a_hrefs } from "./html_parse_a_hrefs.mjs";
@@ -10,4 +11,5 @@ export async function ceb_bible_words_3() {
   ]);
   let root_bible = await html_cache_parse(url_base);
   let hrefs = html_parse_a_hrefs(root_bible, string_starts_with_curry("B"));
+  return list_length(hrefs);
 }
