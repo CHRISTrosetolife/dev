6ba31ef560989092557376af0bac57c5ee6cb949
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 15:18:39 -0400

    2024-06-05T19:18:38.882Zfunction_auto_return function_search_terms_match

--- public/src/function_search_terms_match.mjs
+++ public/src/function_search_terms_match.mjs
@@ -1,5 +1,5 @@
 import { string_includes_curry } from "./string_includes_curry.mjs";
 import { list_all } from "./list_all.mjs";
 export function function_search_terms_match(function_name, terms) {
-  list_all(terms, string_includes_curry(function_name));
+  return list_all(terms, string_includes_curry(function_name));
 }
