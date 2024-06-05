6fe84f9e2db54f77197d062b7d76c291c5a8cee1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 17:05:53 -0400

    2024-06-05T21:05:52.348Zfunction_auto_return number_max_list_or

--- public/src/number_max_list_or.mjs
+++ public/src/number_max_list_or.mjs
@@ -1,1 +1,6 @@
-export function number_max_list_or(list) {}
+import { list_empty_is } from "./list_empty_is.mjs";
+export function number_max_list_or(list, value_if_empty) {
+  if (list_empty_is(list)) {
+    return value_if_empty;
+  }
+}
