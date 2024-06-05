cccb50798832d8614393047f146af838ff0db79c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 15:03:41 -0400

    2024-06-05T19:03:41.049Zfunction_auto_return html_parse_a_hrefs

--- public/src/html_parse_a_hrefs.mjs
+++ public/src/html_parse_a_hrefs.mjs
@@ -3,6 +3,7 @@ import { log } from "./log.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
 import { html_parse_visit_tag } from "./html_parse_visit_tag.mjs";
 import { list_adder } from "./list_adder.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
 export function html_parse_a_hrefs(root, condition) {
   return list_adder((la) =>
     html_parse_visit_tag(root, "a", (v) => {
@@ -11,9 +12,11 @@ export function html_parse_a_hrefs(root, condition) {
         t: html_parse_text(node),
       });
       let attribs = node;
-      let href = html_parse_href(node);
-      if (condition(href)) {
-        la(href);
+      if (object_property_exists(attribs, "href")) {
+        let href = html_parse_href(node);
+        if (condition(href)) {
+          la(href);
+        }
       }
     }),
   );
