08e4b897b341081fe730831aa2576ebebfd1e18b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 16:41:49 -0400

    2024-06-05T20:41:49.064Zfunction_auto_return folder_files_copy

--- public/src/folder_files_copy.mjs
+++ public/src/folder_files_copy.mjs
@@ -1,11 +1,11 @@
+import { file_copy } from "./file_copy.mjs";
 import { folder_files_move_generic } from "./folder_files_move_generic.mjs";
-import { file_rename } from "./file_rename.mjs";
 export async function folder_files_copy(
   input_directory,
   file_extension,
   output_directory,
 ) {
-  let lambda = file_rename;
+  let lambda = file_copy;
   await folder_files_move_generic(
     input_directory,
     file_extension,
