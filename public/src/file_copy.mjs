01637521523186514b4d720c8eaf1e20082cc072
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 17:09:06 -0400

    2024-06-05T21:09:04.177Zfunction_auto_return file_copy

--- public/src/file_copy.mjs
+++ public/src/file_copy.mjs
@@ -1,25 +1,5 @@
-import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
-import { import_node } from "./import_node.mjs";
-import { file_open } from "./file_open.mjs";
-import { assert_not_async } from "./assert_not_async.mjs";
-import { file_exists } from "./file_exists.mjs";
-import { web_is } from "./web_is.mjs";
-import { global_get } from "./global_get.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { object_property_set } from "./object_property_set.mjs";
+import { file_copy_generic } from "./file_copy_generic.mjs";
 export async function file_copy(file_name_from, file_name_to) {
   let open = true;
-  await assert_not_async(file_exists, [file_name_to]);
-  if (web_is()) {
-    let { files } = global_get();
-    let existing = object_property_get(files, file_name_from);
-    object_property_set(files, file_name_to, existing);
-  } else {
-    let fs = await import_node("fs");
-    await folder_parent_exists_ensure(file_name_to);
-    await fs.promises.copyFile(file_name_from, file_name_to);
-    if (open) {
-      await file_open(file_name_to);
-    }
-  }
+  await file_copy_generic(file_name_to, file_name_from, open);
 }
