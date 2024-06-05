daf6cda3205e5d9bdc5ed63600e27d038e853a7e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 15:46:33 -0400

    2024-06-05T19:46:32.616Zfunction_auto_return path_join

--- public/src/path_join.mjs
+++ public/src/path_join.mjs
@@ -1,5 +1,8 @@
 import { list_join } from "./list_join.mjs";
+import { list_map } from "./list_map.mjs";
+import { string_replace } from "./string_replace.mjs";
 export function path_join(paths) {
   let joined = list_join(paths, "/");
+  let mapped = list_map(joined, (m) => string_replace());
   return joined;
 }
