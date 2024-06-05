a3edd9e2cd55a716d418e39bac1a007068d088c6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 14:57:50 -0400

    2024-06-05T18:57:49.832Zfunction_auto_return ceb_bible_words_3

--- public/src/ceb_bible_words_3.mjs
+++ public/src/ceb_bible_words_3.mjs
@@ -1,10 +1,12 @@
+import { html_cache_parse } from "./html_cache_parse.mjs";
 import { html_parse_a_hrefs } from "./html_parse_a_hrefs.mjs";
 import { url_secure_w3 } from "./url_secure_w3.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-export function ceb_bible_words_3() {
+export async function ceb_bible_words_3() {
   let url_base = string_combine_multiple([
     url_secure_w3(),
     "htmlbible.com/sacrednamebiblecom/cebuano/",
   ]);
-  html_parse_a_hrefs;
+  let root_bible = await html_cache_parse(url_base);
+  html_parse_a_hrefs();
 }
