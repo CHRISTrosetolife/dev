1355386321a24afe640f7d6f7be036268427561c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 17:20:58 -0400

    2024-06-05T21:20:57.765Zfunction_auto_return file_copy_generic

--- public/src/file_copy_generic.mjs
+++ public/src/file_copy_generic.mjs
@@ -1,4 +1,3 @@
-import { assert } from "./assert.mjs";
 import { file_open } from "./file_open.mjs";
 import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
 import { import_node } from "./import_node.mjs";
@@ -8,9 +7,7 @@ import { global_get } from "./global_get.mjs";
 import { web_is } from "./web_is.mjs";
 import { file_exists } from "./file_exists.mjs";
 import { assert_not_async } from "./assert_not_async.mjs";
-import { undefined_not_is } from "./undefined_not_is.mjs";
 export async function file_copy_generic(file_name_from, file_name_to, open) {
-  assert(undefined_not_is, [file_name_to]);
   await assert_not_async(file_exists, [file_name_to]);
   if (web_is()) {
     let { files } = global_get();
