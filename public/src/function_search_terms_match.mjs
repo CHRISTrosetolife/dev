12bb7c2f8410fa5927690f553cf1a773f9f6fc55
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 15:11:55 -0400

    2024-06-05T19:11:55.221Zfunction_auto_return function_search_terms_match

--- public/src/function_search_terms_match.mjs
+++ public/src/function_search_terms_match.mjs
@@ -1,6 +1,5 @@
+import { string_includes } from "./string_includes.mjs";
 import { list_all } from "./list_all.mjs";
-import { function_name_to_parts } from "./function_name_to_parts.mjs";
 export function function_search_terms_match(function_name, terms) {
-  let fn_name_parts = function_name_to_parts(function_name);
-  list_all;
+  list_all(terms, (term) => string_includes(function_name, term));
 }
