363afa431f2ae53393d19047c20db490ba687a5a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 13:30:34 -0400

    2024-06-05T17:30:32.921Z  function_aliases_generate

--- public/src/function_aliases.mjs
+++ public/src/function_aliases.mjs
@@ -1,6 +1,6 @@
 export function function_aliases() {
   return {
-    al: "function_aliases",
+    al: "function_aliases_generate",
     a: "function_auto",
     c: "function_copy",
     o: "function_open",
