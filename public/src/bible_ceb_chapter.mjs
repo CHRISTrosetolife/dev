480a71f5e64243a738c7144f9bef50ca944d4b85
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 14:43:24 -0400

    2024-06-05T18:43:23.951Zfunction_auto_return bible_ceb_chapter

--- public/src/bible_ceb_chapter.mjs
+++ public/src/bible_ceb_chapter.mjs
@@ -36,9 +36,12 @@ export async function bible_ceb_chapter(chapter_name) {
         verse_number: "21",
         tokens: [],
       });
-      list_sort_string(eng, object_property_get_curry("verse_number"));
+      missing_is = true;
     }
   });
+  if (missing_is) {
+    list_sort_string(eng, object_property_get_curry("verse_number"));
+  }
   let eng_verse_numbers = list_map_property(eng, "verse_number");
   let parsed_ceb = await bible_chapter_parsed("cebulb", chapter_name);
   let verses_ceb = html_parse_visit_class_list(parsed_ceb, "p");
