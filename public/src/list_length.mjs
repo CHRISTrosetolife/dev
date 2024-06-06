4593b2cde8649cfcf9c26a6f111fe7b78e1cdf17
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 22:33:33 -0400

    2024-06-06T02:33:33.685Zfunction_auto_return list_length

--- public/src/list_length.mjs
+++ public/src/list_length.mjs
@@ -1,5 +1,8 @@
+import { assert } from "./assert.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
+import { list_is } from "./list_is.mjs";
 export function list_length(list) {
   assert_arguments_length(arguments, 1);
+  assert(list_is, [list]);
   return list.length;
 }
