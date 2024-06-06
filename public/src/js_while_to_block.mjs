222664e1c635bfd93d52e1c382456a3a3be6dc32
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 14:59:40 -0400

    2024-06-06T18:59:38.883Z  w

--- public/src/js_while_to_block.mjs
+++ public/src/js_while_to_block.mjs
@@ -1,10 +1,8 @@
-import { log } from "./log.mjs";
 import { js_to_block_generic } from "./js_to_block_generic.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_while_to_block(ast) {
   return js_visit_node(ast, "WhileStatement", (v) => {
     let { node } = v;
     js_to_block_generic(node, ["body"]);
-    log(node.body);
   });
 }
