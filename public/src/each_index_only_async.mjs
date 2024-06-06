e0266e4cace9403b837803033a9b00b0a089f1ec
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 20:46:43 -0400

    2024-06-06T00:46:41.935Zfunction_auto_return each_index_only_async

--- public/src/each_index_only_async.mjs
+++ public/src/each_index_only_async.mjs
@@ -1,7 +1,7 @@
 export async function each_index_only_async(list, lambda) {
   let index = 0;
   for (let element of list) {
-    await lambda(element, index);
+    await lambda(index);
     index++;
   }
 }
