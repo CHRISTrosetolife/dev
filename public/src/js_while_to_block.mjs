f797cc7621ef576b9f84862b13389663a9b1eb9a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 14:44:33 -0400

    2024-06-06T18:44:33.251Zfunction_auto_return js_while_to_block

---
+++ public/src/js_while_to_block.mjs
@@ -0,0 +1,24 @@
+import { object_property_set } from "./object_property_set.mjs";
+import { each } from "./each.mjs";
+import { js_visit_node } from "./js_visit_node.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+export function js_while_to_block(ast) {
+  return js_visit_node(ast, "IfStatement", (v) => {
+    let { node } = v;
+    let properties = ["consequent", "alternate"];
+    each(properties, (p) => {
+      let value = object_property_get(node, p);
+      if (value === null) {
+        return;
+      }
+      let { type } = value;
+      if (type !== "BlockStatement") {
+        let value_new = {
+          type: "BlockStatement",
+          body: [value],
+        };
+        object_property_set(node, p, value_new);
+      }
+    });
+  });
+}
