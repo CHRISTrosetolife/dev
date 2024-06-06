39f01871989afa454f8c125ab62afb40b4f72072
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 21:01:27 -0400

    2024-06-06T01:01:26.473Zfunction_auto_return bible_ceb_4_chapter

--- public/src/bible_ceb_4_chapter.mjs
+++ public/src/bible_ceb_4_chapter.mjs
@@ -1,3 +1,4 @@
+import { html_parse_visit_attribute_value_single } from "./html_parse_visit_attribute_value_single.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { path_dirname } from "./path_dirname.mjs";
@@ -6,7 +7,6 @@ import { list_get } from "./list_get.mjs";
 import { ceb_bible_4_chapters_hrefs } from "./ceb_bible_4_chapters_hrefs.mjs";
 import { bible_ceb_4_hrefs_books } from "./bible_ceb_4_hrefs_books.mjs";
 import { bible_ceb_4_url_base } from "./bible_ceb_4_url_base.mjs";
-import { html_parse_visit_attribute_value } from "./html_parse_visit_attribute_value.mjs";
 export async function bible_ceb_4_chapter(book_index, chapter_index) {
   let url_base = bible_ceb_4_url_base();
   let book_hrefs = await bible_ceb_4_hrefs_books(url_base);
@@ -16,5 +16,9 @@ export async function bible_ceb_4_chapter(book_index, chapter_index) {
   let url_chapter = path_join([path_dirname(href_book), href_chapter]);
   url_chapter = string_combine(url_base, url_chapter);
   let root_chapter = await html_cache_parse(url_chapter);
-  html_parse_visit_attribute_value(root_chapter, "id", "textBody");
+  let tb = html_parse_visit_attribute_value_single(
+    root_chapter,
+    "id",
+    "textBody",
+  );
 }
