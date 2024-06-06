c0aa46fd48afb0ffa4e4b85b89787f1ef0a9e3d0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 15:00:36 -0400

    2024-06-06T19:00:35.058Zfunction_auto_return string_ends_with_multiple

--- public/src/string_ends_with_multiple.mjs
+++ public/src/string_ends_with_multiple.mjs
@@ -1,5 +1,5 @@
 import { string_ends_with } from "./string_ends_with.mjs";
 import { list_any } from "./list_any.mjs";
-export function string_ends_with_multiple(input, prefixes) {
-  list_any(prefixes, (prefix) => string_ends_with(input, prefix));
+export function string_ends_with_multiple(input, suffixes) {
+  list_any(suffixes, (prefix) => string_ends_with(input, prefix));
 }
