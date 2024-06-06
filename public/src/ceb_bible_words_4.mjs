2c7ffd2d84913b6139d040a850d0bcbc5cb171c2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 20:45:30 -0400

    2024-06-06T00:45:29.598Zfunction_auto_return ceb_bible_words_4

--- public/src/ceb_bible_words_4.mjs
+++ public/src/ceb_bible_words_4.mjs
@@ -1,11 +1,9 @@
 import { bible_ceb_4_chapter } from "./bible_ceb_4_chapter.mjs";
 import { ceb_bible_4_chapters_hrefs } from "./ceb_bible_4_chapters_hrefs.mjs";
 import { bible_ceb_4_hrefs_books } from "./bible_ceb_4_hrefs_books.mjs";
-import { bible_ceb_4_url_base } from "./bible_ceb_4_url_base.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 export async function ceb_bible_words_4() {
-  let url_base = bible_ceb_4_url_base();
-  let book_hrefs = await bible_ceb_4_hrefs_books(url_base);
+  let book_hrefs = await bible_ceb_4_hrefs_books();
   await each_index_async(book_hrefs, async (href_book_, book_index) => {
     let hrefs_chapters = await ceb_bible_4_chapters_hrefs(
       book_hrefs,
