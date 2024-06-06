1792777eca8c48bfbc433d3f8fb85a4c408c628c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 14:02:28 -0400

    2024-06-06T18:02:28.027Zfunction_auto_return ceb_bible_words_2

--- public/src/ceb_bible_words_2.mjs
+++ public/src/ceb_bible_words_2.mjs
@@ -17,9 +17,7 @@ export async function ceb_bible_words_2() {
           let verses = await bible_ceb_2_book(book_index);
           each(verses, (v) =>
             each(v.tokens, (t) => {
-              each(string_symbols(t), (s) => {
-                las(s);
-              });
+              each(string_symbols(t), las);
               law(t);
             }),
           );
