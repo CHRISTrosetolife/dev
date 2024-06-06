a9b95eebfaea24c1ed900c2d83c70074aa7e69ab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 14:49:59 -0400

    2024-06-06T18:49:58.687Zfunction_auto_return js_node_type

--- public/src/js_node_type.mjs
+++ public/src/js_node_type.mjs
@@ -1,15 +1,14 @@
-import { log } from "./log.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 import { js_visit } from "./js_visit.mjs";
 export function js_node_type(ast, type) {
-  return list_adder_unique((la) => {
+  let result = list_adder_unique((la) => {
     js_visit(ast, lambda);
     function lambda(v) {
       let { node } = v;
       if (node.type === type) {
-        log("here");
         la(node);
       }
     }
   });
+  return result;
 }
