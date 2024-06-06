cd05e50dc8d94cad699493fe14c70c689b5d7926
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 21:15:14 -0400

    2024-06-06T01:15:13.124Zfunction_auto_return bible_ceb_4_chapter

--- public/src/bible_ceb_4_chapter.mjs
+++ public/src/bible_ceb_4_chapter.mjs
@@ -47,7 +47,6 @@ export async function bible_ceb_4_chapter(book_index, chapter_index) {
           tokens,
         });
         verse_number = null;
-        let a = b !== null;
       }
       if (type === "tag") {
         let { attribs } = c;
