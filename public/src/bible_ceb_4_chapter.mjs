ce51950d3f08de9121a12dd92a823851aa0bce4d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 12:32:20 -0400

    2024-06-06T16:32:19.789Zfunction_auto_return bible_ceb_4_chapter

--- public/src/bible_ceb_4_chapter.mjs
+++ public/src/bible_ceb_4_chapter.mjs
@@ -1,3 +1,4 @@
+import { bible_ceb_4_chapter_url } from "./bible_ceb_4_chapter_url.mjs";
 import { log } from "./log.mjs";
 import { null_not_is } from "./null_not_is.mjs";
 import { assert } from "./assert.mjs";
@@ -8,24 +9,11 @@ import { each } from "./each.mjs";
 import { html_parse_visit_tag_single } from "./html_parse_visit_tag_single.mjs";
 import { html_parse_visit_attribute_value_single } from "./html_parse_visit_attribute_value_single.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
-import { string_combine } from "./string_combine.mjs";
-import { path_dirname } from "./path_dirname.mjs";
-import { path_join } from "./path_join.mjs";
-import { list_get } from "./list_get.mjs";
-import { ceb_bible_4_chapters_hrefs } from "./ceb_bible_4_chapters_hrefs.mjs";
-import { bible_ceb_4_hrefs_books } from "./bible_ceb_4_hrefs_books.mjs";
-import { bible_ceb_4_url_base } from "./bible_ceb_4_url_base.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { string_split_space } from "./string_split_space.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function bible_ceb_4_chapter(book_index, chapter_index) {
-  let url_base = bible_ceb_4_url_base();
-  let book_hrefs = await bible_ceb_4_hrefs_books(url_base);
-  let hrefs_chapters = await ceb_bible_4_chapters_hrefs(book_hrefs, book_index);
-  let href_chapter = list_get(hrefs_chapters, chapter_index);
-  let href_book = list_get(book_hrefs, book_index);
-  let url_chapter = path_join([path_dirname(href_book), href_chapter]);
-  url_chapter = string_combine(url_base, url_chapter);
+  let url_chapter = await bible_ceb_4_chapter_url(book_index, chapter_index);
   log({
     url_chapter,
   });
