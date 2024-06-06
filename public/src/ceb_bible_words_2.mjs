3ad61b6ffdfe999ea68bb862df4acd5289ebaedf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 14:01:07 -0400

    2024-06-06T18:01:06.914Zfunction_auto_return ceb_bible_words_2

--- public/src/ceb_bible_words_2.mjs
+++ public/src/ceb_bible_words_2.mjs
@@ -9,19 +9,21 @@ import { list_sort_string } from "./list_sort_string.mjs";
 import { identity } from "./identity.mjs";
 export async function ceb_bible_words_2() {
   let book_hrefs = await bible_ceb_2_books_hrefs();
-  let symbols = await list_adder_unique_async(
-    async (la) =>
-      await each_index_only_async(book_hrefs, async (book_index) => {
-        let verses = await bible_ceb_2_book(book_index);
-        each(verses, (v) =>
-          each(v.tokens, (t) =>
-            each(string_symbols(t), (s) => {
-              la(s);
-            }),
-          ),
-        );
-      }),
-  );
+  let symbols = await list_adder_unique_async(async (la) => {
+    let symbols = await list_adder_unique_async(
+      async (la) =>
+        await each_index_only_async(book_hrefs, async (book_index) => {
+          let verses = await bible_ceb_2_book(book_index);
+          each(verses, (v) =>
+            each(v.tokens, (t) =>
+              each(string_symbols(t), (s) => {
+                la(s);
+              }),
+            ),
+          );
+        }),
+    );
+  });
   list_sort_string(symbols, identity);
   return symbols;
   let mapped = string_count_words(text_split);
