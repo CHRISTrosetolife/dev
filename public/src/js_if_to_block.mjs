5df416aa07247f0390e49a2006eea42552457cf3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 14:58:12 -0400

    2024-06-06T18:58:11.417Zfunction_auto_return js_if_to_block

--- public/src/js_if_to_block.mjs
+++ public/src/js_if_to_block.mjs
@@ -1,26 +1,9 @@
-import { object_property_set } from "./object_property_set.mjs";
-import { equal } from "./equal.mjs";
-import { assert } from "./assert.mjs";
-import { each } from "./each.mjs";
+import { js_to_block_generic } from "./js_to_block_generic.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 export function js_if_to_block(ast) {
   return js_visit_node(ast, "IfStatement", (v) => {
     let { node } = v;
     let properties = ["consequent", "alternate"];
-    each(properties, (p) => {
-      let value = object_property_get(node, p);
-      if (value === null) {
-        return;
-      }
-      let { type } = value;
-      if (type !== "BlockStatement") {
-        let value_new = {
-          type: "BlockStatement",
-          body: [value],
-        };
-        object_property_set(node, p, value_new);
-      }
-    });
+    js_to_block_generic(node, properties);
   });
 }
