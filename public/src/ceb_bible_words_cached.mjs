4c3ff35119d38a7cb64986ddab768658e0bc5053
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 16:46:32 -0400

    2024-06-06T20:46:31.625Zfunction_auto_return ceb_bible_words_cached

--- public/src/ceb_bible_words_cached.mjs
+++ public/src/ceb_bible_words_cached.mjs
@@ -1,21 +1,5 @@
-import { list_adder_multiple_async } from "./list_adder_multiple_async.mjs";
-import { list_map } from "./list_map.mjs";
-import { range_1 } from "./range_1.mjs";
-import { string_count_words } from "./string_count_words.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { each_async } from "./each_async.mjs";
-import { function_run } from "./function_run.mjs";
+import { ceb_bible_words } from "./ceb_bible_words.mjs";
+import { function_cache } from "./function_cache.mjs";
 export async function ceb_bible_words_cached() {
-  let count = 5;
-  let fn_names = list_map(range_1(count), (i) =>
-    string_combine_multiple([ceb_bible_words_cached.name, "_", i]),
-  );
-  let all = await list_adder_multiple_async(async (la) => {
-    await each_async(fn_names, async (fn_name) => {
-      let words = await function_run(fn_name, []);
-      la(words);
-    });
-  });
-  let mapped = string_count_words(all);
-  return mapped;
+  await function_cache(ceb_bible_words);
 }
