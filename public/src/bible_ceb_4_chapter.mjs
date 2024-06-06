d8a0e8c84af077b5a90338368bd4989da21f9644
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 20:45:12 -0400

    2024-06-06T00:45:11.628Zfunction_auto_return bible_ceb_4_chapter

---
+++ public/src/bible_ceb_4_chapter.mjs
@@ -0,0 +1,18 @@
+import { html_cache_parse } from "./html_cache_parse.mjs";
+import { string_combine } from "./string_combine.mjs";
+import { path_dirname } from "./path_dirname.mjs";
+import { path_join } from "./path_join.mjs";
+import { list_get } from "./list_get.mjs";
+import { ceb_bible_4_chapters_hrefs } from "./ceb_bible_4_chapters_hrefs.mjs";
+import { bible_ceb_4_hrefs_books } from "./bible_ceb_4_hrefs_books.mjs";
+import { bible_ceb_4_url_base } from "./bible_ceb_4_url_base.mjs";
+export async function bible_ceb_4_chapter(book_index, chapter_index) {
+  let url_base = bible_ceb_4_url_base();
+  let book_hrefs = await bible_ceb_4_hrefs_books(url_base);
+  let hrefs_chapters = await ceb_bible_4_chapters_hrefs(book_hrefs, book_index);
+  let href_chapter = list_get(hrefs_chapters, chapter_index);
+  let href_book = list_get(book_hrefs, book_index);
+  let url_chapter = path_join([path_dirname(href_book), href_chapter]);
+  url_chapter = string_combine(url_base, url_chapter);
+  let root_chapter = await html_cache_parse(url_chapter);
+}
