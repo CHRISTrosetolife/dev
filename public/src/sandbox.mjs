5aa943f32eb9743cb2bc969a32e9fe5a3929b106
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 12:32:38 -0400

    2024-06-06T16:32:37.756Zfunction_auto_return sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -53,6 +53,9 @@ export async function sandbox() {
         each(b, (v) => {
           let { tokens } = v;
           if (list_any(tokens, string_includes_curry("Ã±"))) {
+            log({
+              url_chapter,
+            });
           }
         });
         return;
