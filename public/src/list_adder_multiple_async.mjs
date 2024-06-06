7b1cd9a7040665b02ed1ec52af2015fefcc97a5b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 16:23:27 -0400

    2024-06-06T20:23:26.599Zfunction_auto_return list_adder_multiple_async

--- public/src/list_adder_multiple_async.mjs
+++ public/src/list_adder_multiple_async.mjs
@@ -1,6 +1,6 @@
 import { list_add_multiple } from "./list_add_multiple.mjs";
 export async function list_adder_multiple_async(lambda) {
   let result = [];
-  await lambda((item) => list_add_multiple(result, item));
+  await lambda((items) => list_add_multiple(result, items));
   return result;
 }
