4507ce558f60d98d0bb1714ecbdecdc3105c62bc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 14:33:54 -0400

    2024-06-06T18:33:53.941Zfunction_auto_return ceb_bible_words_5

--- public/src/ceb_bible_words_5.mjs
+++ public/src/ceb_bible_words_5.mjs
@@ -5,34 +5,28 @@ import { bible_books } from "./bible_books.mjs";
 import { list_adder_unique_async } from "./list_adder_unique_async.mjs";
 import { each } from "./each.mjs";
 import { string_symbols } from "./string_symbols.mjs";
-import { bible_ceb_2_book } from "./bible_ceb_2_book.mjs";
-import { each_index_only_async } from "./each_index_only_async.mjs";
 import { bible_ceb_2_books_hrefs } from "./bible_ceb_2_books_hrefs.mjs";
 import { string_count_words } from "./string_count_words.mjs";
 import { list_sort_string } from "./list_sort_string.mjs";
 import { identity } from "./identity.mjs";
 export async function ceb_bible_words_5() {
-  await each_async(await bible_books("engbsb"), async (book_name) => {
-    let chapters = await bible_chapters("engbsb", book_name);
-    await each_async(chapters, async (chapter_name) => {
-      let verses = await bible_ceb_chapter(chapter_name);
-    });
-  });
   let book_hrefs = await bible_ceb_2_books_hrefs();
   let symbols;
   let words = await list_adder_unique_async(async (law) => {
-    symbols = await list_adder_unique_async(
-      async (las) =>
-        await each_index_only_async(book_hrefs, async (book_index) => {
-          let verses = await bible_ceb_2_book(book_index);
+    symbols = await list_adder_unique_async(async (las) => {
+      await each_async(await bible_books("engbsb"), async (book_name) => {
+        let chapters = await bible_chapters("engbsb", book_name);
+        await each_async(chapters, async (chapter_name) => {
+          let verses = await bible_ceb_chapter(chapter_name);
           each(verses, (v) =>
             each(v.tokens, (t) => {
               each(string_symbols(t), las);
               law(t);
             }),
           );
-        }),
-    );
+        });
+      });
+    });
   });
   list_sort_string(symbols, identity);
   if (0) {
