188816ed48fb7e2d62a4822b0f19c45edc67d9f8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 21:06:03 -0400

    2024-06-06T01:06:02.342Zfunction_auto_return bible_ceb_4_chapter

--- public/src/bible_ceb_4_chapter.mjs
+++ public/src/bible_ceb_4_chapter.mjs
@@ -27,7 +27,7 @@ export async function bible_ceb_4_chapter(book_index, chapter_index) {
   let p = html_parse_visit_tag_single(tb, "p");
   let { children } = p;
   each(children, (c) => {
-    let type = c;
+    let { type } = c;
   });
   error();
 }
