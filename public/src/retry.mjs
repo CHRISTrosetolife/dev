d0cc632f1d7f79ff02e901b8230a486e1441d380
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 09:53:21 -0400

    2024-06-06T13:53:21.359Zfunction_auto_return retry

--- public/src/retry.mjs
+++ public/src/retry.mjs
@@ -1,3 +1,4 @@
+import { sleep } from "./sleep.mjs";
 import { error } from "./error.mjs";
 export async function retry(retries, lambda, retry_if_lambda) {
   let delay = 10;
@@ -7,6 +8,7 @@ export async function retry(retries, lambda, retry_if_lambda) {
       return await lambda();
     } catch (e) {
       if (retry_if_lambda(e)) {
+        await sleep(delay * 1000);
         continue;
       }
       throw e;
