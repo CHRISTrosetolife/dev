101c491f17d72382371fde5321e014afdb057b66
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 16:46:42 -0400

    2024-06-06T20:46:42.382Zfunction_auto_return ceb_bible_words_cached

--- public/src/ceb_bible_words_cached.mjs
+++ public/src/ceb_bible_words_cached.mjs
@@ -1,5 +1,5 @@
 import { ceb_bible_words } from "./ceb_bible_words.mjs";
 import { function_cache } from "./function_cache.mjs";
 export async function ceb_bible_words_cached() {
-  await function_cache(ceb_bible_words);
+  return await function_cache(ceb_bible_words, []);
 }
