8b6e8b4ccc47525a9d4bb2f7ea0d54a7411dd3a3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 14:57:31 -0400

    2024-06-06T18:57:31.066Zfunction_auto_return js_while_to_block

--- public/src/js_while_to_block.mjs
+++ public/src/js_while_to_block.mjs
@@ -1,7 +1,7 @@
 import { js_to_block_generic } from "./js_to_block_generic.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_while_to_block(ast) {
-  return js_visit_node(ast, "IfStatement", (v) => {
+  return js_visit_node(ast, "WhileStatement", (v) => {
     let { node } = v;
     js_to_block_generic(node, ["body"]);
   });
