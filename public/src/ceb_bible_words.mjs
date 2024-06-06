d014d42559dad7ca3378d23c89f32b386019ce52
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 16:18:32 -0400

    2024-06-06T20:18:31.559Zfunction_auto_return ceb_bible_words

--- public/src/ceb_bible_words.mjs
+++ public/src/ceb_bible_words.mjs
@@ -1,8 +1,11 @@
+import { range } from "./range.mjs";
 import { ceb_bible_words_1 } from "./ceb_bible_words_1.mjs";
 import { string_count_words } from "./string_count_words.mjs";
+import { list_map } from "./list_map.mjs";
+import { add_1 } from "./add_1.mjs";
 export async function ceb_bible_words() {
   let count = 5;
-  range_1(count);
+  list_map(range(count), add_1);
   let text_split = await ceb_bible_words_1();
   let mapped = string_count_words(text_split);
   return mapped;
