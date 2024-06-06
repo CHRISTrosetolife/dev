5e0509001d0c8672f381d13b5b9f803408181511
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 22:42:19 -0400

    2024-06-06T02:42:18.989Zfunction_auto_return sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -61,7 +61,7 @@ export async function sandbox() {
           });
           assert(equal, [va, vb]);
           function normalize(l) {
-            l = list_map(l, (e) => string_replace_multiple(e, ",", ""));
+            l = list_map(l, (e) => string_replace_multiple(e, ",", "-"));
             l = list_filter(l, string_empty_not_is);
             return l;
           }
