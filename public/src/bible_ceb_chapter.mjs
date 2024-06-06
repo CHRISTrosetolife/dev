f6fb81877b07d9a2198587550e14aebf875ec5e5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 20:34:16 -0400

    2024-06-06T00:34:15.560Zfunction_auto_return bible_ceb_chapter

--- public/src/bible_ceb_chapter.mjs
+++ public/src/bible_ceb_chapter.mjs
@@ -41,6 +41,7 @@ export async function bible_ceb_chapter(chapter_name) {
     ACT15: ["34"],
     ACT24: ["7"],
     ACT28: ["29"],
+    ROM16: ["24"],
   };
   let missing_is = false;
   each_object(missing, (chapter_name_missing, verse_numbers) => {
