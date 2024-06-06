d47eedbaf0df522eaf2d838417843bb44e0c1e93
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 14:57:23 -0400

    2024-06-06T18:57:21.858Zfunction_auto_return js_to_block_generic

---
+++ public/src/js_to_block_generic.mjs
@@ -0,0 +1,19 @@
+import { object_property_set } from "./object_property_set.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { each } from "./each.mjs";
+export function js_to_block_generic(node, properties) {
+  each(properties, (p) => {
+    let value = object_property_get(node, p);
+    if (value === null) {
+      return;
+    }
+    let { type } = value;
+    if (type !== "BlockStatement") {
+      let value_new = {
+        type: "BlockStatement",
+        body: [value],
+      };
+      object_property_set(node, p, value_new);
+    }
+  });
+}
