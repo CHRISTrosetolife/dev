27033766686413cc22e2fa62b5403dba3915f92c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 14:46:20 -0400

    2024-06-06T18:46:19.432Zfunction_auto_return function_transform_return

--- public/src/function_transform_return.mjs
+++ public/src/function_transform_return.mjs
@@ -5,7 +5,7 @@ import { assert } from "./assert.mjs";
 import { list_length } from "./list_length.mjs";
 export async function function_transform_return(lambda_names, function_name) {
   let args = list_to(arguments);
-  let length = list_length();
+  let length = list_length(args);
   assert(equal, [length, 2]);
   return await function_transform_args(lambda_names, function_name, "a");
 }
