b22155284f7fdeb380ae98618aa1735817fcdbd6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 13:53:43 -0400

    2024-06-06T17:53:42.876Zfunction_auto_return bible_ceb_2_book

--- public/src/bible_ceb_2_book.mjs
+++ public/src/bible_ceb_2_book.mjs
@@ -49,7 +49,7 @@ export async function bible_ceb_2_book(book_index) {
             "+2",
           );
           let chapter_name_text = html_parse_text(chapter_name_element);
-          if (chapter_name_text === "Back to Cebuano Bible Options") {
+          if (chapter_name_text !== "Back to Cebuano Bible Options") {
             chapter_name = string_prefix_without(chapter_name_text, "CAPITLO ");
           }
         }
