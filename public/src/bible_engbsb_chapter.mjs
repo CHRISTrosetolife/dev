336febe856c99f639d7456340bacbb68200a2163
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 17:01:31 -0400

    2024-06-06T21:01:30.719Zfunction_auto_return bible_engbsb_chapter

--- public/src/bible_engbsb_chapter.mjs
+++ public/src/bible_engbsb_chapter.mjs
@@ -1,15 +1,5 @@
-import { html_parse_visit_classes_list } from "./html_parse_visit_classes_list.mjs";
-import { bible_verses_parse } from "./bible_verses_parse.mjs";
-import { bible_chapter_parsed } from "./bible_chapter_parsed.mjs";
+import { bible_chapter } from "./bible_chapter.mjs";
 export async function bible_engbsb_chapter(chapter_name) {
   let bible_folder = "engbsb";
   return await bible_chapter(bible_folder, chapter_name);
 }
-async function bible_chapter(bible_folder, chapter_name) {
-    let root = await bible_chapter_parsed(bible_folder, chapter_name);
-    let attribute_values = ["m", "pc", "pmo", "q", "li", "q2", "li2", "d"];
-    let verses = html_parse_visit_classes_list(root, attribute_values);
-    let eng = bible_verses_parse(verses);
-    return eng;
-}
-
