51efbb260353bb475decdf74f7055ad37eb5fac7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-05 20:44:34 -0400

    2024-06-06T00:44:33.658Zfunction_auto_return ceb_bible_4_chapters_hrefs

---
+++ public/src/ceb_bible_4_chapters_hrefs.mjs
@@ -0,0 +1,22 @@
+import { string_take } from "./string_take.mjs";
+import { string_index } from "./string_index.mjs";
+import { html_parse_href } from "./html_parse_href.mjs";
+import { list_map } from "./list_map.mjs";
+import { html_parse_visit_class_list } from "./html_parse_visit_class_list.mjs";
+import { html_cache_parse } from "./html_cache_parse.mjs";
+import { string_combine } from "./string_combine.mjs";
+import { bible_ceb_4_url_base } from "./bible_ceb_4_url_base.mjs";
+import { list_get } from "./list_get.mjs";
+export async function ceb_bible_4_chapters_hrefs(book_hrefs, book_index) {
+  let href_book = list_get(book_hrefs, book_index);
+  let url_base = bible_ceb_4_url_base();
+  let url = string_combine(url_base, href_book);
+  let root_book = await html_cache_parse(url);
+  let chapter_elements = html_parse_visit_class_list(root_book, "chap");
+  let hrefs_chapters = list_map(chapter_elements, html_parse_href);
+  hrefs_chapters = list_map(hrefs_chapters, (h) => {
+    let i = string_index(h, "#");
+    return string_take(h, i);
+  });
+  return hrefs_chapters;
+}
--- public/src/ceb_bible_words_4.mjs
+++ public/src/ceb_bible_words_4.mjs
@@ -1,30 +1,20 @@
+import { ceb_bible_4_chapters_hrefs } from "./ceb_bible_4_chapters_hrefs.mjs";
 import { list_get } from "./list_get.mjs";
 import { bible_ceb_4_hrefs_books } from "./bible_ceb_4_hrefs_books.mjs";
 import { bible_ceb_4_url_base } from "./bible_ceb_4_url_base.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { path_join } from "./path_join.mjs";
 import { path_dirname } from "./path_dirname.mjs";
-import { string_take } from "./string_take.mjs";
-import { string_index } from "./string_index.mjs";
-import { html_parse_href } from "./html_parse_href.mjs";
-import { html_parse_visit_class_list } from "./html_parse_visit_class_list.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { string_combine } from "./string_combine.mjs";
-import { list_map } from "./list_map.mjs";
 export async function ceb_bible_words_4() {
   let url_base = bible_ceb_4_url_base();
   let book_hrefs = await bible_ceb_4_hrefs_books(url_base);
   await each_index_async(book_hrefs, async (href_book_, book_index) => {
-    let href_book = list_get(book_hrefs, book_index);
-    let url_base = bible_ceb_4_url_base();
-    let url = string_combine(url_base, href_book);
-    let root_book = await html_cache_parse(url);
-    let chapter_elements = html_parse_visit_class_list(root_book, "chap");
-    let hrefs_chapters = list_map(chapter_elements, html_parse_href);
-    hrefs_chapters = list_map(hrefs_chapters, (h) => {
-      let i = string_index(h, "#");
-      return string_take(h, i);
-    });
+    let hrefs_chapters = await ceb_bible_4_chapters_hrefs(
+      book_hrefs,
+      book_index,
+    );
     await each_index_async(
       hrefs_chapters,
       async (href_chapter_, chapter_index) => {
