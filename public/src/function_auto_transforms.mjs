f625873542ce92f71710f7056672a6327fedd32a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-06 14:45:38 -0400

    2024-06-06T18:45:37.367Zfunction_auto_return function_auto_transforms

--- public/src/function_auto_transforms.mjs
+++ public/src/function_auto_transforms.mjs
@@ -1,3 +1,4 @@
+import { js_while_to_block } from "./js_while_to_block.mjs";
 import { js_if_to_block } from "./js_if_to_block.mjs";
 import { js_assign_to_let } from "./js_assign_to_let.mjs";
 import { js_await_add } from "./js_await_add.mjs";
@@ -7,6 +8,7 @@ import { js_outside_move } from "./js_outside_move.mjs";
 export function function_auto_transforms() {
   return [
     js_if_to_block,
+    js_while_to_block,
     js_const_to_let,
     js_assign_to_let,
     js_await_add,
