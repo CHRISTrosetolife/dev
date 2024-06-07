d4ec2cd1a50278a24199fc2b8ad16e09280bca92
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 14:09:28 -0400

    2024-06-07T18:09:27.634Zfunction_auto_return app_dev_sandbox_function

--- public/src/app_dev_sandbox_function.mjs
+++ public/src/app_dev_sandbox_function.mjs
@@ -15,6 +15,7 @@ import { html_hr } from "./html_hr.mjs";
 import { html_value_get } from "./html_value_get.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
+import { list_length } from "./list_length.mjs";
 export async function app_dev_sandbox_function(
   root,
   function_name,
@@ -28,6 +29,8 @@ export async function app_dev_sandbox_function(
     root,
     string_combine_multiple([app_dev_sandbox_message(), try_out_message]),
   );
+  if (list_length(function_names) > 1) {
+  }
   each(function_names, (fn) => {});
   let value_initial = await function_read(function_name);
   let textarea = html_textarea_code(root);
