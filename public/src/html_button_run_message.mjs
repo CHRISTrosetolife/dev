0cd65faa597d907be872ee3bb707a08a7c4b8e7e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 15:39:28 -0400

    2024-06-07T19:39:28.330Zfunction_auto_return html_button_run_message

--- public/src/html_button_run_message.mjs
+++ public/src/html_button_run_message.mjs
@@ -1,7 +1,7 @@
 import { list_add } from "./list_add.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function html_button_run_message(message) {
-  let list = ["💻 run", message];
+  let list = ["💻 run"];
   list_add(list, message);
   return string_combine_multiple(list);
 }
