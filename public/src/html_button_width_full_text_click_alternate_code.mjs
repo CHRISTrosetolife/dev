07291fbc4ca2e2bc24578c5f6b1a7e9662f53899
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 15:33:59 -0400

    2024-06-07T19:33:56.847Zfunction_auto_return html_button_width_full_text_click_alternate_code

--- public/src/html_button_width_full_text_click_alternate_code.mjs
+++ public/src/html_button_width_full_text_click_alternate_code.mjs
@@ -1,7 +1,4 @@
-import { app_learn_code_style_code_colored } from "./app_learn_code_style_code_colored.mjs";
-import { noop } from "./noop.mjs";
-import { html_span } from "./html_span.mjs";
-import { html_style_alternate } from "./html_style_alternate.mjs";
+import { html_style_alternate_monospace_short_span } from "./html_style_alternate_monospace_short_span.mjs";
 import { html_style_alternate_split } from "./html_style_alternate_split.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function html_button_width_full_text_click_alternate_code(
@@ -11,11 +8,6 @@ export function html_button_width_full_text_click_alternate_code(
 ) {
   let b = html_button_width_full_text_click(root, "", on_click);
   let split = html_style_alternate_split(description);
-  html_style_alternate(
-    b,
-    html_span,
-    [noop, app_learn_code_style_code_colored],
-    split,
-  );
+  html_style_alternate_monospace_short_span(b, split);
   return b;
 }
