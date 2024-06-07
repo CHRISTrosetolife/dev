923112872dbe89f2e75bf07562862972fa90b463
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 10:15:34 -0400

    2024-06-07T14:15:33.846Zfunction_auto_return function_path_to_name

--- public/src/function_path_to_name.mjs
+++ public/src/function_path_to_name.mjs
@@ -1,12 +1,8 @@
-import { log } from "./log.mjs";
 import { function_path_prefix } from "./function_path_prefix.mjs";
 import { function_path_suffix } from "./function_path_suffix.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
 export function function_path_to_name(function_path) {
-  log({
-    function_path,
-  });
   function_path = string_prefix_without(function_path, function_path_prefix());
   function_path = string_suffix_without(function_path, function_path_suffix());
   return function_path;
