cd45669164f24dd057a70fbc9756ed906713f900
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 09:50:40 -0400

    2024-06-07T13:50:40.204Zfunction_auto_return function_names_new

--- public/src/function_names_new.mjs
+++ public/src/function_names_new.mjs
@@ -1,8 +1,11 @@
+import { range } from "./range.mjs";
+import { each_async } from "./each_async.mjs";
 import { function_name_new } from "./function_name_new.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
 export async function function_names_new(function_name_base, count) {
   await list_adder_async(async (la) => {
-    each_range_async;
+    await each_async(range, lambda);
+    function lambda() {}
     la(await function_name_new(function_name_base));
   });
 }
