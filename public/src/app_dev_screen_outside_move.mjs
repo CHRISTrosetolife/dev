c929911b9b8e8f0e77314f147cb554e69d9ea189
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 09:39:41 -0400

    2024-06-07T13:39:40.726Zfunction_auto_return app_dev_screen_outside_move

--- public/src/app_dev_screen_outside_move.mjs
+++ public/src/app_dev_screen_outside_move.mjs
@@ -1,3 +1,5 @@
+import { app_dev_p_transformer } from "./app_dev_p_transformer.mjs";
+import { js_outside_move } from "./js_outside_move.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { app_dev_screen_img } from "./app_dev_screen_img.mjs";
 import { html_span_text } from "./html_span_text.mjs";
@@ -10,6 +12,8 @@ export function app_dev_screen_outside_move() {
       let i = 1;
       html_p_text(root, "if we have functions in the module scope ...");
       app_dev_screen_img(app_dev_screen_outside_move, root, i++);
+      let fn = js_outside_move;
+      app_dev_p_transformer(root, fn);
       app_dev_screen_img(app_dev_screen_outside_move, root, i++);
       app_dev_screen_img(app_dev_screen_outside_move, root, i++);
       app_dev_screen_img(app_dev_screen_outside_move, root, i++);
