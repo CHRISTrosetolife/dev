760b5416fe20db2f58dbe3e27e4ec769230a15f5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 14:18:39 -0400

    2024-06-07T18:18:38.586Zfunction_auto_return html_button_width_full

--- public/src/html_button_width_full.mjs
+++ public/src/html_button_width_full.mjs
@@ -1,7 +1,7 @@
 import { html_style_width_full } from "./html_style_width_full.mjs";
 import { html_button } from "./html_button.mjs";
-export function html_button_width_full(root) {
-  let b = html_button(root);
+export function html_button_width_full(parent) {
+  let b = html_button(parent);
   html_style_width_full(b);
   return b;
 }
