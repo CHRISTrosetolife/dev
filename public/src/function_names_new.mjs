e7865243e787d1a54104bf8bd88aadb5c9495234
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 09:50:07 -0400

    2024-06-07T13:50:07.138Zfunction_auto_return function_names_new

--- public/src/function_names_new.mjs
+++ public/src/function_names_new.mjs
@@ -1,7 +1,7 @@
 import { function_name_new } from "./function_name_new.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
 export async function function_names_new(function_name_base, count) {
-  await list_adder_async(async (la) =>
-    la(await function_name_new(function_name_base)),
-  );
+  await list_adder_async(async (la) => {
+    la(await function_name_new(function_name_base));
+  });
 }
