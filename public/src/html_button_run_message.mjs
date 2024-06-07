640c69f4313280a826ee67e684950af35cfdfd64
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 15:39:14 -0400

    2024-06-07T19:39:12.424Zfunction_auto_return html_button_run_message

--- public/src/html_button_run_message.mjs
+++ public/src/html_button_run_message.mjs
@@ -1,5 +1,7 @@
+import { list_add } from "./list_add.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function html_button_run_message(message) {
   let list = ["💻 run", message];
+  list_add(list, message);
   return string_combine_multiple(list);
 }
