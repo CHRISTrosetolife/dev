3df063a4786bda92d3dfcf16399418344587f342
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 15:35:04 -0400

    2024-06-07T19:35:03.521Zfunction_auto_return html_button_width_full_text_click_alternate_code

--- public/src/html_button_width_full_text_click_alternate_code.mjs
+++ public/src/html_button_width_full_text_click_alternate_code.mjs
@@ -1,5 +1,4 @@
 import { html_style_alternate_monospace_short_span } from "./html_style_alternate_monospace_short_span.mjs";
-import { html_style_alternate_split } from "./html_style_alternate_split.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function html_button_width_full_text_click_alternate_code(
   root,
@@ -7,7 +6,6 @@ export function html_button_width_full_text_click_alternate_code(
   on_click,
 ) {
   let b = html_button_width_full_text_click(root, "", on_click);
-  let split = html_style_alternate_split(description);
-  html_style_alternate_monospace_short_span(b, split);
+  html_style_alternate_monospace_short_span(b, description);
   return b;
 }
