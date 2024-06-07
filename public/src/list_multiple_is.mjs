319e89129f201725b5589c03c6e90e64fd116138
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 14:16:36 -0400

    2024-06-07T18:16:35.115Zfunction_auto_return list_multiple_is

--- public/src/list_multiple_is.mjs
+++ public/src/list_multiple_is.mjs
@@ -1,5 +1,5 @@
 import { list_length } from "./list_length.mjs";
 import { greater_than_equal } from "./greater_than_equal.mjs";
-export function list_multiple_is(split_d) {
-  return greater_than_equal(list_length(split_d), 2);
+export function list_multiple_is(list) {
+  return greater_than_equal(list_length(list), 2);
 }
