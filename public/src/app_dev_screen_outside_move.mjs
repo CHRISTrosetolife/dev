bd228d49943779f202f1665d156401b49480c810
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 10:02:47 -0400

    2024-06-07T14:02:46.702Zfunction_auto_return app_dev_screen_outside_move

--- public/src/app_dev_screen_outside_move.mjs
+++ public/src/app_dev_screen_outside_move.mjs
@@ -1,3 +1,4 @@
+import { multiply } from "./multiply.mjs";
 import { js_code_function_declare } from "./js_code_function_declare.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
@@ -40,7 +41,11 @@ export function app_dev_screen_outside_move() {
       app_dev_screen_img(app_dev_screen_outside_move, root, i++);
       app_dev_screen_img(app_dev_screen_outside_move, root, i++);
       let [fn_a, fn_b, fn_c] = await function_name_new("outside_move", 3);
-      js_code_function_declare(fn_c, "a", "");
+      js_code_function_declare(
+        fn_c,
+        "a,b",
+        js_code_statement_call_args(multiply.name, [100]),
+      );
       let contents_a = js_code_export_function_declare(
         fn_a,
         "",
