a7a5a91109662ea766fd7c759169a1330b41f4c2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 14:38:07 -0400

    2024-06-07T18:38:03.303Zfunction_auto_return app_dev_sandbox_function_multiple

--- public/src/app_dev_sandbox_function_multiple.mjs
+++ public/src/app_dev_sandbox_function_multiple.mjs
@@ -16,7 +16,6 @@ import { html_textarea_code } from "./html_textarea_code.mjs";
 import { function_read } from "./function_read.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
 import { each } from "./each.mjs";
-import { list_multiple_is } from "./list_multiple_is.mjs";
 import { app_dev_sandbox_message } from "./app_dev_sandbox_message.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_style_alternate_monospace_short } from "./html_style_alternate_monospace_short.mjs";
@@ -25,7 +24,7 @@ import { object_property_exists } from "./object_property_exists.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function app_dev_sandbox_function_multiple(
   root,
-  function_name_choices,
+  multiple_is,
   function_name,
   try_out_message,
   run_message,
@@ -36,7 +35,7 @@ export async function app_dev_sandbox_function_multiple(
     root,
     string_combine_multiple([app_dev_sandbox_message(), try_out_message, " :"]),
   );
-  if (list_multiple_is(function_name_choices)) {
+  if (multiple_is) {
     let select = html_select(root);
     html_style_button_default(select);
     html_style_width_full(select);
