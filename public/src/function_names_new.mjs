8d9cf33862a1f0000165dabef488c9b91b08206c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 09:51:04 -0400

    2024-06-07T13:51:02.558Zfunction_auto_return each_range_async

--- public/src/function_names_new.mjs
+++ public/src/function_names_new.mjs
@@ -4,8 +4,12 @@ import { function_name_new } from "./function_name_new.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
 export async function function_names_new(function_name_base, count) {
   await list_adder_async(async (la) => {
-    await each_async(range(count), lambda);
+    await each_range_async(count, lambda);
     function lambda() {}
     la(await function_name_new(function_name_base));
   });
 }
+async function each_range_async(count, lambda) {
+    await each_async(range(count), lambda);
+}
+
