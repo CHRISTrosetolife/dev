d6ab75a9f3fb71c8506100998ea5cd147f46939c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 10:17:25 -0400

    2024-06-07T14:17:24.578Zfunction_auto_return function_names

--- public/src/function_names.mjs
+++ public/src/function_names.mjs
@@ -1,3 +1,4 @@
+import { string_ends_with } from "./string_ends_with.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { function_path_suffix } from "./function_path_suffix.mjs";
 import { function_paths } from "./function_paths.mjs";
@@ -16,7 +17,7 @@ export async function function_names() {
   if (web_is()) {
     let { files } = global_get();
     let function_paths = object_properties(files);
-    list_filter(function_paths);
+    list_filter(function_paths, (fp) => string_ends_with(fp, suffix));
     let result = list_map(function_paths, function_path_to_name);
     return result;
   }
