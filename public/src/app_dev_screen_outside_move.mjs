b2bdce2b76cc4e1643e101dab542b8a2616944d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 09:46:17 -0400

    2024-06-07T13:46:17.125Zfunction_auto_return app_dev_screen_outside_move

--- public/src/app_dev_screen_outside_move.mjs
+++ public/src/app_dev_screen_outside_move.mjs
@@ -1,3 +1,16 @@
+import { function_transform } from "./function_transform.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { string_delimit_backtick } from "./string_delimit_backtick.mjs";
+import { app_dev_sandbox_function } from "./app_dev_sandbox_function.mjs";
+import { file_write } from "./file_write.mjs";
+import { function_name_to_path } from "./function_name_to_path.mjs";
+import { js_code_format } from "./js_code_format.mjs";
+import { js_code_import } from "./js_code_import.mjs";
+import { string_combine } from "./string_combine.mjs";
+import { sleep } from "./sleep.mjs";
+import { js_code_statement_call_args } from "./js_code_statement_call_args.mjs";
+import { js_code_export_function_declare } from "./js_code_export_function_declare.mjs";
+import { function_name_new } from "./function_name_new.mjs";
 import { html_style_alternate_monospace_short } from "./html_style_alternate_monospace_short.mjs";
 import { app_dev_p_transformer } from "./app_dev_p_transformer.mjs";
 import { js_outside_move } from "./js_outside_move.mjs";
@@ -8,7 +21,7 @@ export function app_dev_screen_outside_move() {
     name: function outside_move_name(root) {
       return html_span_text(root, "outside_move");
     },
-    screen: function outside_move_screen(root) {
+    screen: async function outside_move_screen(root) {
       let i = 1;
       html_style_alternate_monospace_short(
         root,
@@ -25,6 +38,32 @@ export function app_dev_screen_outside_move() {
       app_dev_screen_img(app_dev_screen_outside_move, root, i++);
       app_dev_screen_img(app_dev_screen_outside_move, root, i++);
       app_dev_screen_img(app_dev_screen_outside_move, root, i++);
+      let function_name_unique = await function_name_new("async_await_add");
+      let contents_function = js_code_export_function_declare(
+        function_name_unique,
+        "",
+        js_code_statement_call_args(sleep.name, [100]),
+        false,
+      );
+      let code = string_combine(js_code_import(sleep.name), contents_function);
+      let contents = await js_code_format(code);
+      let file_path = function_name_to_path(function_name_unique);
+      await file_write(file_path, contents);
+      await app_dev_sandbox_function(
+        root,
+        function_name_unique,
+        "adding `async` and `await`",
+        string_delimit_backtick(
+          string_combine_multiple([
+            function_transform.name,
+            " ",
+            fn.name,
+            " ",
+            function_name_unique,
+          ]),
+        ),
+        async () => await function_transform(fn.name, function_name_unique),
+      );
     },
   };
 }
