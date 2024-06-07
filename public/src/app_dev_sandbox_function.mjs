901ad8f9cddd0350edc2bc79b0fe01d2406fd26d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-07 14:37:47 -0400

    2024-06-07T18:37:44.642Zfunction_auto_return app_dev_sandbox_function

--- public/src/app_dev_sandbox_function.mjs
+++ public/src/app_dev_sandbox_function.mjs
@@ -1,5 +1,4 @@
 import { app_dev_sandbox_function_multiple } from "./app_dev_sandbox_function_multiple.mjs";
-import { function_names } from "./function_names.mjs";
 export async function app_dev_sandbox_function(
   root,
   function_name,
@@ -7,10 +6,9 @@ export async function app_dev_sandbox_function(
   run_message,
   run_click,
 ) {
-  let function_names = [function_name];
   return await app_dev_sandbox_function_multiple(
     root,
-    function_names,
+    false,
     function_name,
     try_out_message,
     run_message,
