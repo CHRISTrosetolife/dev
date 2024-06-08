973d86d88ff3c7f3b783eca2ccc8f89d71bd4ac1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-08 17:21:44 -0400

    2024-06-08T21:21:43.791Zfunction_auto_return yyy8Uu_translate_upload

--- public/src/yyy8Uu_translate_upload.mjs
+++ public/src/yyy8Uu_translate_upload.mjs
@@ -1,6 +1,3 @@
-import { list_take } from "./list_take.mjs";
-import { error } from "./error.mjs";
-import { equal_by } from "./equal_by.mjs";
 import { log } from "./log.mjs";
 import { each_index_only_async } from "./each_index_only_async.mjs";
 import { yyy8Uu_file_paths } from "./yyy8Uu_file_paths.mjs";
@@ -11,8 +8,6 @@ import { yyy8Uu_part } from "./yyy8Uu_part.mjs";
 import { gcloud_translate_cache } from "./gcloud_translate_cache.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
 import { yyy8Uu_parts } from "./yyy8Uu_parts.mjs";
-import { assert } from "./assert.mjs";
-import { list_length } from "./list_length.mjs";
 export async function yyy8Uu_translate_upload() {
   let filtered = await yyy8Uu_file_paths();
   await each_index_only_async(filtered, async (index) => {
@@ -37,12 +32,6 @@ export async function yyy8Uu_translate_upload() {
         la(translated);
       });
     });
-    assert(equal_by, [latin, english, list_length]);
-    log({
-      latin: list_take(latin, 3),
-      english: list_take(english, 3),
-    });
-    error();
     let destination = yyy8Uu_storage_path(index);
     if (0) {
       await storage_upload_object(
