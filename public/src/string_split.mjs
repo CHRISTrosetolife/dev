fbff4dfa44ff886e70b2d1dd18b4f0f62f976479
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-08 17:26:32 -0400

    2024-06-08T21:26:32.155Zfunction_auto_return string_split

--- public/src/string_split.mjs
+++ public/src/string_split.mjs
@@ -1,3 +1,5 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function string_split(input, delimeter) {
+  assert_arguments_length(2);
   return input.split(delimeter);
 }
