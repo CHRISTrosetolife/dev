b3b6afa081bc8723a370ffb43a0df9d7190b7f27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-08 13:34:04 -0400

    2024-06-08T17:34:04.328Zfunction_auto_return yyy8Uu_translate

--- public/src/yyy8Uu_translate.mjs
+++ public/src/yyy8Uu_translate.mjs
@@ -3,7 +3,6 @@ import { yyy8Uu_part } from "./yyy8Uu_part.mjs";
 import { gcloud_translate_cache } from "./gcloud_translate_cache.mjs";
 import { yyy8Uu_parts } from "./yyy8Uu_parts.mjs";
 import { log } from "./log.mjs";
-import { each_index_async } from "./each_index_async.mjs";
 import { yyy8Uu_file_paths } from "./yyy8Uu_file_paths.mjs";
 export async function yyy8Uu_translate() {
   let filtered = await yyy8Uu_file_paths();
@@ -12,7 +11,7 @@ export async function yyy8Uu_translate() {
       index,
     });
     let parts = await yyy8Uu_parts(index);
-    await each_index_async(parts, async (part, index2) => {
+    await each_index_only_async(parts, async (index2) => {
       log({
         index,
         index2,
