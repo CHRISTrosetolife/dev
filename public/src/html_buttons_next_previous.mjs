00dab0ca5955f23a5339f6abe86ca6c6fc0f9b6f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 12:42:30 -0400

    2024-06-09T16:42:30.246Zfunction_auto_return html_buttons_next_previous

--- public/src/html_buttons_next_previous.mjs
+++ public/src/html_buttons_next_previous.mjs
@@ -1,30 +1,6 @@
-import { html_button_disable } from "./html_button_disable.mjs";
-import { html_spacer_vertical_2 } from "./html_spacer_vertical_2.mjs";
-import { equal } from "./equal.mjs";
-import { subtract_1 } from "./subtract_1.mjs";
-import { html_button_width_full_text_click_previous } from "./html_button_width_full_text_click_previous.mjs";
-import { add_1 } from "./add_1.mjs";
-import { html_button_width_full_text_click_next } from "./html_button_width_full_text_click_next.mjs";
+import { html_buttons_next_previous_hr_none } from "./html_buttons_next_previous_hr_none.mjs";
 import { html_hr } from "./html_hr.mjs";
 export function html_buttons_next_previous(root, on_click, index, index_last) {
   html_hr(root);
-  let button_next = html_button_width_full_text_click_next(
-    root,
-    async function next_on_click() {
-      await on_click(add_1(index));
-    },
-  );
-  let button_previous = html_button_width_full_text_click_previous(
-    root,
-    async function previous_on_click() {
-      await on_click(subtract_1(index));
-    },
-  );
-  if (equal(index, 0)) {
-    html_button_disable(button_previous);
-  }
-  if (equal(index, index_last)) {
-    html_button_disable(button_next);
-  }
-  html_spacer_vertical_2(root);
+  html_buttons_next_previous_hr_none(root, on_click, index, index_last);
 }
