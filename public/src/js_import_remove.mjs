5dbdd2bb228735440941d3371d61a6b5bb02c281
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 14:37:29 -0400

    2024-06-09T18:37:29.296Zfunction_auto_return js_import_remove

--- public/src/js_import_remove.mjs
+++ public/src/js_import_remove.mjs
@@ -6,14 +6,14 @@ import { js_identifiers } from "./js_identifiers.mjs";
 import { js_export_single } from "./js_export_single.mjs";
 import { js_imports_existing } from "./js_imports_existing.mjs";
 import { list_remove } from "./list_remove.mjs";
-export function js_import_remove(ast, name) {
+export function js_import_remove(ast) {
   let existing = js_imports_existing(ast);
-  let existing2 = js_imports_existing_named(ast, name);
-  let { body } = ast;
   let export_ = js_export_single(ast);
   let used = js_identifiers(export_);
   let extra = list_difference(existing, used);
+  let { body } = ast;
   for (let e of extra) {
+    let existing2 = js_imports_existing_named(ast, e);
     let e2 = list_single(existing2);
     let { stack } = e2;
     let ancestor = list_get_end(stack, 1);
