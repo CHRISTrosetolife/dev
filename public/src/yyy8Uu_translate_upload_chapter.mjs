e41d6b2d08c5db2cebbeaea7357bd51b186821b0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 12:02:03 -0400

    2024-06-09T16:02:02.616Zfunction_auto_return yyy8Uu_translate_upload_chapter

--- public/src/yyy8Uu_translate_upload_chapter.mjs
+++ public/src/yyy8Uu_translate_upload_chapter.mjs
@@ -17,7 +17,7 @@ export async function yyy8Uu_translate_upload_chapter(chapter_index) {
   let latin = await yyy8Uu_parts(chapter_index);
   let latin_length = list_length(latin);
   let english_length = await yyy8Uu_parts_english_count(chapter_index);
-  await list_adder_async(async (la) => {
+  let english = await list_adder_async(async (la) => {
     await each_range_async(english_length, async (index2) => {
       let translateds = await gcloud_translate_cache(
         yyy8Uu_language_from(),
@@ -30,7 +30,7 @@ export async function yyy8Uu_translate_upload_chapter(chapter_index) {
     });
   });
   let destination = yyy8Uu_storage_path(chapter_index);
-  if (10) {
+  if (0) {
     await storage_upload_object(
       {
         english,
