714ecfed825c093bf7e332d526eebaac7c098ff8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 15:07:26 -0400

    2024-06-09T19:07:26.405Zfunction_auto_return data_remove

--- public/src/data_remove.mjs
+++ public/src/data_remove.mjs
@@ -3,13 +3,13 @@ import { list_remove_if_exists } from "./list_remove_if_exists.mjs";
 import { each_object } from "./each_object.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
-export function data_remove(data, name) {
+export function data_remove(data, function_name) {
   let functions = object_property_initialize(data, "functions", {});
-  if (object_property_exists(functions, name)) {
-    object_property_delete(functions, name);
+  if (object_property_exists(functions, function_name)) {
+    object_property_delete(functions, function_name);
   }
   let identifiers = object_property_initialize(data, "identifiers", {});
   each_object(identifiers, (id, id_functions) => {
-    list_remove_if_exists(id_functions, name);
+    list_remove_if_exists(id_functions, function_name);
   });
 }
