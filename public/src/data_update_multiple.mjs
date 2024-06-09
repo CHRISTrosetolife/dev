312393247ab6a7e0c1c50140f9d846e4ffadcdd4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 15:09:08 -0400

    2024-06-09T19:09:07.790Zfunction_auto_return data_update_multiple

--- public/src/data_update_multiple.mjs
+++ public/src/data_update_multiple.mjs
@@ -4,7 +4,8 @@ import { data_path } from "./data_path.mjs";
 import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
 export async function data_update_multiple(fps) {
   await file_delete_if_exists(data_path());
-  await file_json_transform_exists(data_path(), lambda);async function lambda(data) {
+  await file_json_transform_exists(data_path(), lambda);
+  async function lambda(data) {
     await data_update_multiple_transform(fps, data);
   }
 }
