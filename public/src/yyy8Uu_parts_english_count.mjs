a640e0d7f6278d9881b6d73df3276235c3eddcdd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 11:01:43 -0400

    2024-06-09T15:01:43.262Zfunction_auto_return yyy8Uu_parts_english_count

--- public/src/yyy8Uu_parts_english_count.mjs
+++ public/src/yyy8Uu_parts_english_count.mjs
@@ -1,3 +1,5 @@
+import { yyy8Uu_language_to } from "./yyy8Uu_language_to.mjs";
+import { yyy8Uu_language_from } from "./yyy8Uu_language_from.mjs";
 import { gcloud_translate } from "./gcloud_translate.mjs";
 import { file_exists_not } from "./file_exists_not.mjs";
 import { yyy8Uu_part } from "./yyy8Uu_part.mjs";
@@ -6,6 +8,8 @@ export async function yyy8Uu_parts_english_count(index) {
   let index2 = 0;
   while (true) {
     let fcp = function_cache_path(gcloud_translate.name, [
+      yyy8Uu_language_from(),
+      yyy8Uu_language_to(),
       yyy8Uu_part.name,
       [index, index2],
     ]);
