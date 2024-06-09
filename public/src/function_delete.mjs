4e8084339d483a89828847cc9f92837aedc5bdf8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 15:38:46 -0400

    2024-06-09T19:38:46.192Zfunction_auto_return function_delete

--- public/src/function_delete.mjs
+++ public/src/function_delete.mjs
@@ -1,6 +1,13 @@
+import { data_update_multiple_transform } from "./data_update_multiple_transform.mjs";
+import { data_remove } from "./data_remove.mjs";
+import { data_transform } from "./data_transform.mjs";
 import { file_delete } from "./file_delete.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function function_delete(function_name) {
   let function_path = function_name_to_path(function_name);
   await file_delete(function_path);
+  await data_transform(async function lambda(data) {
+    data_remove(data, fn_name_from);
+    await data_update_multiple_transform(fps, data);
+  });
 }
