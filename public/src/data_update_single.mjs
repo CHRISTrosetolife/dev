2aaee8e1f3da4b4850d45d6af53695efd8d7b6a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 14:26:08 -0400

    2024-06-09T18:26:08.356Zfunction_auto_return data_update_single

--- public/src/data_update_single.mjs
+++ public/src/data_update_single.mjs
@@ -29,6 +29,7 @@ export function data_update_single(ast, data) {
   let identifiers = object_property_initialize(data, "identifiers", {});
   each_object(identifiers, (id, id_functions) => {
     if (list_includes(ids, id)) {
+    } else {
     }
   });
   each(ids, (id) => {
