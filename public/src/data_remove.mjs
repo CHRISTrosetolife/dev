acf7815e1868f2fadc91c0d74098f522bfb966ba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 15:06:47 -0400

    2024-06-09T19:06:47.238Zfunction_auto_return data_remove

--- public/src/data_remove.mjs
+++ public/src/data_remove.mjs
@@ -1,18 +1,8 @@
 import { object_property_delete } from "./object_property_delete.mjs";
-import { each } from "./each.mjs";
 import { list_remove_if_exists } from "./list_remove_if_exists.mjs";
 import { each_object } from "./each_object.mjs";
-import { list_add_if_exists_not } from "./list_add_if_exists_not.mjs";
-import { js_identifiers } from "./js_identifiers.mjs";
-import { string_delimit_if } from "./string_delimit_if.mjs";
-import { js_node_type } from "./js_node_type.mjs";
-import { object_property_set } from "./object_property_set.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
-import { list_map_property } from "./list_map_property.mjs";
-import { list_unique } from "./list_unique.mjs";
-import { list_sort_string } from "./list_sort_string.mjs";
-import { list_includes } from "./list_includes.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 export function data_remove(ast, data) {
   let declaration = js_declaration_single(ast);
@@ -24,21 +14,7 @@ export function data_remove(ast, data) {
   if (object_property_exists(functions, name)) {
     object_property_delete(functions, name);
   }
-  let f = object_property_initialize(functions, name, {});
-  object_property_set(f, "async", async);
-  let literals = js_node_type(ast, "Literal");
-  let mapped = list_map_property(literals, "value");
-  let u = list_unique(mapped);
-  list_sort_string(u, string_delimit_if);
-  object_property_set(f, "literals", u);
-  let ids = js_identifiers(ast);
-  let identifiers = object_property_initialize(data, "identifiers", {});
-  each(ids, (id) => object_property_initialize(identifiers, id, []));
   each_object(identifiers, (id, id_functions) => {
-    if (list_includes(ids, id)) {
-      list_add_if_exists_not(id_functions, name);
-    } else {
-      list_remove_if_exists(id_functions, name);
-    }
+    list_remove_if_exists(id_functions, name);
   });
 }
