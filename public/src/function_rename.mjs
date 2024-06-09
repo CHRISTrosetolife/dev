d7a6a08fe9855db76a94fa048459ed9baf67cdf7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 14:32:30 -0400

    2024-06-09T18:32:30.024Zfunction_auto_return function_rename

--- public/src/function_rename.mjs
+++ public/src/function_rename.mjs
@@ -2,8 +2,10 @@ import { data_identifiers } from "./data_identifiers.mjs";
 import { data_update_multiple } from "./data_update_multiple.mjs";
 import { file_copy } from "./file_copy.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 export async function function_rename(fn_name_from, fn_name_to) {
   let identifiers = await data_identifiers();
+  object_property_get(identifiers, fn_name_from);
   let fn_path_from = function_name_to_path(fn_name_from);
   let fn_path_to = function_name_to_path(fn_name_to);
   await file_copy(fn_path_from, fn_path_to);
