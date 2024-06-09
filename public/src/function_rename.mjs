e492449064928586513364c693b1a4c964e7d227
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 14:43:55 -0400

    2024-06-09T18:43:54.713Zfunction_auto_return function_rename

--- public/src/function_rename.mjs
+++ public/src/function_rename.mjs
@@ -1,8 +1,8 @@
+import { file_rename } from "./file_rename.mjs";
 import { function_transform_args_split_lambda } from "./function_transform_args_split_lambda.mjs";
 import { js_import_remove_ast } from "./js_import_remove_ast.mjs";
 import { data_identifiers } from "./data_identifiers.mjs";
 import { data_update_multiple } from "./data_update_multiple.mjs";
-import { file_copy } from "./file_copy.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { list_remove } from "./list_remove.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -22,7 +22,7 @@ export async function function_rename(fn_name_from, fn_name_to) {
   );
   let fn_path_from = function_name_to_path(fn_name_from);
   let fn_path_to = function_name_to_path(fn_name_to);
-  await file_copy(fn_path_from, fn_path_to);
+  await file_rename(fn_path_from, fn_path_to);
   await data_update_multiple(fps);
   js_import_remove_ast;
 }
