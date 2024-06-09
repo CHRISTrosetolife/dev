432713215193abe1d3bf7176c9be4fe7491bd439
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 10:53:51 -0400

    2024-06-09T14:53:50.925Zfunction_auto_return file_exists

--- public/src/file_exists.mjs
+++ public/src/file_exists.mjs
@@ -3,21 +3,23 @@ import { web_is } from "./web_is.mjs";
 import { import_node } from "./import_node.mjs";
 import { object_properties } from "./object_properties.mjs";
 import { list_includes } from "./list_includes.mjs";
-let fs = await import_node("fs");
-export function file_exists(file_name) {
+export async function file_exists(file_name) {
   if (web_is()) {
     let { files } = global_get();
     let keys = object_properties(files);
     return list_includes(keys, file_name);
   }
-  return new Promise((resolve, reject) => {
+  let fs = await import_node("fs");
+  return await new Promise((resolve, reject) => {
     fs.stat(file_name, function (err, stat) {
       if (err == null) {
         resolve(true);
-      } else if (err.code === "ENOENT") {
-        resolve(false);
       } else {
-        reject(err);
+        if (err.code === "ENOENT") {
+          resolve(false);
+        } else {
+          reject(err);
+        }
       }
     });
   });
