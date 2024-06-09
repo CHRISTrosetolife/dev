4f133c2dd315365aebbeee204fb7778ba9732c48
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 10:56:48 -0400

    2024-06-09T14:56:47.723Zfunction_auto_return yyy8Uu_translate_upload

--- public/src/yyy8Uu_translate_upload.mjs
+++ public/src/yyy8Uu_translate_upload.mjs
@@ -9,7 +9,6 @@ import { yyy8Uu_storage_path } from "./yyy8Uu_storage_path.mjs";
 import { list_single } from "./list_single.mjs";
 import { yyy8Uu_part } from "./yyy8Uu_part.mjs";
 import { gcloud_translate_cache } from "./gcloud_translate_cache.mjs";
-import { list_adder_async } from "./list_adder_async.mjs";
 import { yyy8Uu_parts } from "./yyy8Uu_parts.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_length_2 } from "./list_length_2.mjs";
@@ -21,23 +20,20 @@ export async function yyy8Uu_translate_upload() {
       index,
     });
     let latin = await yyy8Uu_parts(index);
-    let english = await list_adder_async(async (la) => {
-      let count = await yyy8Uu_parts_english_count(index);
-      if (0) {
-        log({
-          index,
-          index2: count,
-        });
-      }
-      let translateds = await gcloud_translate_cache(
-        "la",
-        "en",
-        yyy8Uu_part.name,
-        [index, count],
-      );
-      let translated = list_single(translateds);
-      la(translated);
-    });
+    let count = await yyy8Uu_parts_english_count(index);
+    if (0) {
+      log({
+        index,
+        index2: count,
+      });
+    }
+    let translateds = await gcloud_translate_cache(
+      "la",
+      "en",
+      yyy8Uu_part.name,
+      [index, count],
+    );
+    let translated = list_single(translateds);
     let f = list_first(english);
     let f_split = string_split_space(f);
     if (list_length_2(f_split)) {
