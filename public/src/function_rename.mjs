5d1d3d57e89d528a55d6e35654c9973707abd670
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 14:15:43 -0400

    2024-06-09T18:15:43.271Zfunction_auto_return function_rename

--- public/src/function_rename.mjs
+++ public/src/function_rename.mjs
@@ -1,7 +1,9 @@
+import { function_data } from "./function_data.mjs";
 import { file_copy } from "./file_copy.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function function_rename(fn_name_from, fn_name_to) {
   let fn_path_from = function_name_to_path(fn_name_from);
   let fn_path_to = function_name_to_path(fn_name_to);
   await file_copy(fn_path_from, fn_path_to);
+  await function_data(fn_name_to);
 }
