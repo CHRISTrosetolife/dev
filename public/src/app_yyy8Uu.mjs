c3cce14ab85fa2ed0a7ff5ddda4a69bf4063d153
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 12:41:17 -0400

    2024-06-09T16:41:17.127Zfunction_auto_return app_yyy8Uu

--- public/src/app_yyy8Uu.mjs
+++ public/src/app_yyy8Uu.mjs
@@ -1,3 +1,4 @@
+import { html_buttons_next_previous } from "./html_buttons_next_previous.mjs";
 import { html_button_disable } from "./html_button_disable.mjs";
 import { log } from "./log.mjs";
 import { html_spacer_vertical_2 } from "./html_spacer_vertical_2.mjs";
@@ -96,6 +97,7 @@ export async function app_yyy8Uu() {
       chapter_index,
     });
     html_clear_scroll_top(root);
+    html_buttons_next_previous();
     buttons_navigation();
     html_hr(root);
     html_button_width_full_text_click(
