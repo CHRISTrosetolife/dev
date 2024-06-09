f57c5e27e78c33ff00add80db639f59b947ff2eb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 12:21:16 -0400

    2024-06-09T16:21:15.869Zfunction_auto_return yyy8Uu_translate_upload_chapter

--- public/src/yyy8Uu_translate_upload_chapter.mjs
+++ public/src/yyy8Uu_translate_upload_chapter.mjs
@@ -1,3 +1,4 @@
+import { list_remove_at } from "./list_remove_at.mjs";
 import { assert } from "./assert.mjs";
 import { integer_parse } from "./integer_parse.mjs";
 import { error } from "./error.mjs";
@@ -42,7 +43,9 @@ export async function yyy8Uu_translate_upload_chapter(chapter_index) {
   assert(greater_than_equal, [english_length, latin_length]);
   let less = english_length - latin_length;
   assert(less_than_equal, [less, 2]);
-  while (number_less_than(list_length(english), latin_length)) {}
+  while (number_less_than(list_length(english), latin_length)) {
+    list_remove_at(english, 0);
+  }
   return;
   let destination = yyy8Uu_storage_path(chapter_index);
   if (10) {
