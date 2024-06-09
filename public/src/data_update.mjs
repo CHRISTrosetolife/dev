7dd4b58722edc569d9c4fada1bf6a0ffd3397ccb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 14:16:55 -0400

    2024-06-09T18:16:54.993Zfunction_auto_return data_update

--- public/src/data_update.mjs
+++ public/src/data_update.mjs
@@ -6,8 +6,8 @@ import { file_json_transform_exists } from "./file_json_transform_exists.mjs";
 import { each_async } from "./each_async.mjs";
 import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
 export async function data_update() {
-  await file_delete_if_exists(data_path());
   let fps = await function_paths();
+  await file_delete_if_exists(data_path());
   await file_json_transform_exists(data_path(), async (data) => {
     await each_async(fps, async (fp) => {
       let ast = await file_js_parse(fp);
