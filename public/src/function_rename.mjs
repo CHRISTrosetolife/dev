98043ef3c404c8eb19eff406ae5d28239a6d32e0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 14:14:00 -0400

    2024-06-09T18:13:59.967Zfunction_auto_return function_rename

--- public/src/function_rename.mjs
+++ public/src/function_rename.mjs
@@ -2,5 +2,5 @@ import { file_rename } from "./file_rename.mjs";
 import { function_copy_generic } from "./function_copy_generic.mjs";
 export async function function_rename(fn_name_from, fn_name_to) {
   let lambda = file_rename;
-  function_copy_generic(lambda, fn_name_from, fn_name_to);
+  await function_copy_generic(lambda, fn_name_from, fn_name_to);
 }
