3c5bbbb47989507dcf87db821469aac2b9a5b29c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 10:58:52 -0400

    2024-06-09T14:58:52.520Zfunction_auto_return yyy8Uu_parts_english_count

--- public/src/yyy8Uu_parts_english_count.mjs
+++ public/src/yyy8Uu_parts_english_count.mjs
@@ -1,10 +1,11 @@
+import { file_exists } from "./file_exists.mjs";
 import { yyy8Uu_part } from "./yyy8Uu_part.mjs";
 import { function_cache_path } from "./function_cache_path.mjs";
 export async function yyy8Uu_parts_english_count(index) {
   let index2 = 0;
   while (true) {
     let fcp = function_cache_path(yyy8Uu_part.name, [index, index2]);
-    if (await file_exists_not(fcp)) {
+    if (await file_exists(fcp)) {
       break;
     }
     index2++;
