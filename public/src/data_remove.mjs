b93b75afb45d68f939082d15bf3493f95fc8db95
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 15:07:20 -0400

    2024-06-09T19:07:20.448Zfunction_auto_return data_remove

--- public/src/data_remove.mjs
+++ public/src/data_remove.mjs
@@ -2,14 +2,8 @@ import { object_property_delete } from "./object_property_delete.mjs";
 import { list_remove_if_exists } from "./list_remove_if_exists.mjs";
 import { each_object } from "./each_object.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
-import { js_declaration_single } from "./js_declaration_single.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
-export function data_remove(ast, data) {
-  let declaration = js_declaration_single(ast);
-  let {
-    async,
-    id: { name },
-  } = declaration;
+export function data_remove(data, name) {
   let functions = object_property_initialize(data, "functions", {});
   if (object_property_exists(functions, name)) {
     object_property_delete(functions, name);
