ec1169a26f15240702fa5595134226f407d64a05
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 15:10:04 -0400

    2024-06-09T19:10:04.436Zfunction_auto_return data_update_multiple

--- public/src/data_update_multiple.mjs
+++ public/src/data_update_multiple.mjs
@@ -1,8 +1,7 @@
 import { data_transform } from "./data_transform.mjs";
 import { data_update_multiple_transform } from "./data_update_multiple_transform.mjs";
 export async function data_update_multiple(fps) {
-  await data_transform(lambda);
-  async function lambda(data) {
+  await data_transform(async function lambda(data) {
     await data_update_multiple_transform(fps, data);
-  }
+  });
 }
