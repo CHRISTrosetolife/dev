edb49df3fc601b46b86b6d582f14abb1f45065a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 14:57:34 -0400

    2024-06-09T18:57:34.621Zfunction_auto_return function_rename

--- public/src/function_rename.mjs
+++ public/src/function_rename.mjs
@@ -18,7 +18,11 @@ export async function function_rename(fn_name_from, fn_name_to) {
   await file_rename(fn_path_from, fn_path_to);
   let existing = object_property_get(identifiers, fn_name_from);
   list_remove(existing, fn_name_to);
-  await function_transform_args_split_lambda();
+  await function_transform_args_split_lambda(
+    fn_name_to,
+    [js_identifier_rename],
+    [fn_name_from, fn_name_to],
+  );
   await each_async(existing, async (e) => {
     let file_path = function_name_to_path(e);
     let ast = await file_js_parse(file_path);
