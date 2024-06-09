bc787a69494f4c9daa583c4b726055e9e90d8e40
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 11:02:15 -0400

    2024-06-09T15:02:14.925Zfunction_auto_return yyy8Uu_translate_upload

--- public/src/yyy8Uu_translate_upload.mjs
+++ public/src/yyy8Uu_translate_upload.mjs
@@ -24,6 +24,7 @@ export async function yyy8Uu_translate_upload() {
     let english_length = await yyy8Uu_parts_english_count(index);
     if (10) {
       if (equal_not(latin_length, english_length)) {
+        let less = english_length - 2;
         log({
           l: latin_length,
           e: english_length,
