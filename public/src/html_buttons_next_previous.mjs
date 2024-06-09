f1859c52c4c063a101878201b59ae4c5f2fb6327
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 12:45:50 -0400

    2024-06-09T16:45:50.292Zfunction_auto_return html_buttons_next_previous

--- public/src/html_buttons_next_previous.mjs
+++ public/src/html_buttons_next_previous.mjs
@@ -1,6 +1,8 @@
+import { html_spacer_vertical_2 } from "./html_spacer_vertical_2.mjs";
 import { html_buttons_next_previous_hr_none } from "./html_buttons_next_previous_hr_none.mjs";
 import { html_hr } from "./html_hr.mjs";
 export function html_buttons_next_previous(root, on_click, index, index_last) {
   html_hr(root);
   html_buttons_next_previous_hr_none(root, on_click, index, index_last);
+  html_spacer_vertical_2(root);
 }
