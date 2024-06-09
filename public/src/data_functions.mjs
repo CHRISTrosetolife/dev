041648cfa75fdbaa2557a3eb44b9feda44f66875
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 14:31:09 -0400

    2024-06-09T18:31:09.047Zfunction_auto_return data_functions

--- public/src/data_functions.mjs
+++ public/src/data_functions.mjs
@@ -2,7 +2,6 @@ import { data_path } from "./data_path.mjs";
 import { file_read_json } from "./file_read_json.mjs";
 export async function data_functions() {
   let data = await file_read_json(data_path());
-  let { functions: fs } = data;
-  let functions = fs;
+  let { functions } = data;
   return functions;
 }
