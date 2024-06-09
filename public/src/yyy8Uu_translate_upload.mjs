847750764f0d01dace3b72c09c39013471f8fe39
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-09 12:01:17 -0400

    2024-06-09T16:01:17.566Zfunction_auto_return yyy8Uu_translate_upload

--- public/src/yyy8Uu_translate_upload.mjs
+++ public/src/yyy8Uu_translate_upload.mjs
@@ -3,7 +3,5 @@ import { each_index_only_async } from "./each_index_only_async.mjs";
 import { yyy8Uu_file_paths } from "./yyy8Uu_file_paths.mjs";
 export async function yyy8Uu_translate_upload() {
   let filtered = await yyy8Uu_file_paths();
-  await each_index_only_async(filtered, async (chapter_index) => {
-    await yyy8Uu_translate_upload_chapter(chapter_index);
-  });
+  await each_index_only_async(filtered, yyy8Uu_translate_upload_chapter);
 }
