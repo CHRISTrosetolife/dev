7b6719fec4fae0a306eda19b9ebe232e1654abbf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-10 15:21:14 -0400

    2024-06-10T19:21:14.274Zfunction_auto_return app_dev_p_transformer_generic

--- public/src/app_dev_p_transformer_generic.mjs
+++ public/src/app_dev_p_transformer_generic.mjs
@@ -1,7 +1,6 @@
 import { html_style_function_name } from "./html_style_function_name.mjs";
 import { noop } from "./noop.mjs";
 import { html_style_alternate_short_p } from "./html_style_alternate_short_p.mjs";
-import { html_style_alternate_short_function_name } from "./html_style_alternate_short_function_name.mjs";
 import { function_transform } from "./function_transform.mjs";
 export function app_dev_p_transformer_generic(root, transformer, prefix) {
   let message =
@@ -12,5 +11,4 @@ export function app_dev_p_transformer_generic(root, transformer, prefix) {
     transformer.name +
     "` and the name of the `function` ...";
   html_style_alternate_short_p(root, [noop, html_style_function_name], message);
-  html_style_alternate_short_function_name(root, message);
 }
