a17c81714d15a13356ab3894dc8888005f7ce65e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-10 15:40:01 -0400

    2024-06-10T19:40:00.675Zfunction_auto_return js_code_declare_assign

--- public/src/js_code_declare_assign_0.mjs
+++ public/src/js_code_declare_assign_0.mjs
@@ -1,10 +1,5 @@
-import { js_code_statement_let } from "./js_code_statement_let.mjs";
-import { js_code_assign } from "./js_code_assign.mjs";
-import { js_parse_first } from "./js_parse_first.mjs";
+import { js_code_declare_assign } from "./js_code_declare_assign.mjs";
 export function js_code_declare_assign_0(variable_name) {
   let value = `0`;
-  let declare = js_code_assign(variable_name, value);
-  let code = js_code_statement_let(declare);
-  let parsed = js_parse_first(code);
-  return parsed;
+  return js_code_declare_assign(variable_name, value);
 }
