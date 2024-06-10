bb7d1bc6e931dbb32b395d2060590e619bc79465
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-10 15:30:21 -0400

    2024-06-10T19:30:20.793Z  w

--- public/src/data_transform.mjs
+++ public/src/data_transform.mjs
@@ -2,6 +2,5 @@ import { file_json_transform_exists } from "./file_json_transform_exists.mjs";
 import { data_path } from "./data_path.mjs";
 import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
 export async function data_transform(lambda) {
-  await file_delete_if_exists(data_path());
   await file_json_transform_exists(data_path(), lambda);
 }
--- public/src/data_update.mjs
+++ public/src/data_update.mjs
@@ -2,5 +2,6 @@ import { data_update_multiple } from "./data_update_multiple.mjs";
 import { function_paths } from "./function_paths.mjs";
 export async function data_update() {
   let fps = await function_paths();
+  await file_delete_if_exists(data_path());
   await data_update_multiple(fps);
 }
