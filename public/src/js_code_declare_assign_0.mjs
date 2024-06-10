992770cf440e34074a47ff6bd2b4a143f2de4f9a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-10 15:41:55 -0400

    2024-06-10T19:41:54.960Zfunction_auto_return js_code_declare_assign_0

--- public/src/js_code_declare_assign_0.mjs
+++ public/src/js_code_declare_assign_0.mjs
@@ -1,5 +1,8 @@
+import { js_parse_first } from "./js_parse_first.mjs";
 import { js_code_declare_assign } from "./js_code_declare_assign.mjs";
 export function js_code_declare_assign_0(variable_name) {
   let value = `0`;
-  return js_code_declare_assign(variable_name, value);
+  let code = js_code_declare_assign(variable_name, value);
+  let parsed = js_parse_first(code);
+  return parsed;
 }
