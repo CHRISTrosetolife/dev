f071b722550c5aabe766bdaf8672d6459950cdba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-10 15:10:13 -0400

    2024-06-10T19:10:13.132Zfunction_auto_return app_dev_screen_assign_to_let

--- public/src/app_dev_screen_assign_to_let.mjs
+++ public/src/app_dev_screen_assign_to_let.mjs
@@ -1,3 +1,5 @@
+import { app_dev_p_transformer } from "./app_dev_p_transformer.mjs";
+import { js_assign_to_let } from "./js_assign_to_let.mjs";
 import { app_dev_screen_img } from "./app_dev_screen_img.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_assign_to_let() {
@@ -9,6 +11,8 @@ export function app_dev_screen_assign_to_let() {
       let i = 1;
       app_dev_screen_img(app_dev_screen_assign_to_let, root, i++);
       ("if a variable is assigned a value ...");
+      let fn = js_assign_to_let;
+      app_dev_p_transformer(root, fn);
     },
   };
 }
