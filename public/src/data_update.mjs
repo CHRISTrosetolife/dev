2ca4df90eb625d1a13b33b60452860b35ae84a1e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-10 15:30:26 -0400

    2024-06-10T19:30:26.193Zfunction_auto_return data_update

--- public/src/data_update.mjs
+++ public/src/data_update.mjs
@@ -1,3 +1,5 @@
+import { data_path } from "./data_path.mjs";
+import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
 import { data_update_multiple } from "./data_update_multiple.mjs";
 import { function_paths } from "./function_paths.mjs";
 export async function data_update() {
