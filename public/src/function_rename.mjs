f8306b8cff14eeac32ddbc5e2170ddee0e7221c3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-10 15:24:49 -0400

    2024-06-10T19:24:48.040Zfunction_auto_return function_rename

--- public/src/function_rename.mjs
+++ public/src/function_rename.mjs
@@ -15,12 +15,17 @@ import { object_property_get } from "./object_property_get.mjs";
 import { each_async } from "./each_async.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { list_map } from "./list_map.mjs";
+import { log } from "./log.mjs";
 export async function function_rename(fn_name_from, fn_name_to) {
   let identifiers = await data_identifiers();
   let fn_path_from = function_name_to_path(fn_name_from);
   let fn_path_to = function_name_to_path(fn_name_to);
   await file_rename(fn_path_from, fn_path_to);
   let existing = object_property_get(identifiers, fn_name_from);
+  log({
+    existing,
+    fn_name_from,
+  });
   list_remove(existing, fn_name_from);
   await function_transform_args_split_lambda(
     fn_name_to,
