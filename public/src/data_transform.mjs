56df22616588e621bad7b0fa7dfa86b8d772a88d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-10 15:30:31 -0400

    2024-06-10T19:30:30.156Zfunction_auto_return data_transform

--- public/src/data_transform.mjs
+++ public/src/data_transform.mjs
@@ -1,6 +1,5 @@
 import { file_json_transform_exists } from "./file_json_transform_exists.mjs";
 import { data_path } from "./data_path.mjs";
-import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
 export async function data_transform(lambda) {
   await file_json_transform_exists(data_path(), lambda);
 }
