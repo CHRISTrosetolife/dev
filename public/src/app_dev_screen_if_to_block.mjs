79ba39831780d51bd875a2bf0fd920e6fb040923
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 12:25:19 -0400

    2024-06-11T16:25:18.844Zfunction_auto_return app_dev_screen_if_to_block

--- public/src/app_dev_screen_if_to_block.mjs
+++ public/src/app_dev_screen_if_to_block.mjs
@@ -1,3 +1,4 @@
+import { html_style_code } from "./html_style_code.mjs";
 import { noop } from "./noop.mjs";
 import { html_cycle_code } from "./html_cycle_code.mjs";
 import { html_cycle_code_span } from "./html_cycle_code_span.mjs";
@@ -12,13 +13,18 @@ export function app_dev_screen_if_to_block() {
       return html_cycle_code_span(root, "changing `if` statements to blocks");
     },
     screen: function if_to_block_screen(root) {
-      html_cycle_p(root, [
-        noop,
-        html_style_link(
-          `https://google.github.io/styleguide/jsguide.html#formatting-braces-all`,
-        ),
+      html_cycle_p(
+        root,
+        [
+          noop,
+          html_style_link(
+            `https://google.github.io/styleguide/jsguide.html#formatting-braces-all`,
+          ),
+          noop,
+          html_style_code,
+        ],
         "the `google javascript style guide` , for example , generally requires that `if` statements be blocks",
-      ]);
+      );
       html_cycle_code(
         root,
         "if there is an `if` statement that is not a block ...",
