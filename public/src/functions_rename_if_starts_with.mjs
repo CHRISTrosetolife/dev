f5d3e3e5e5453c4cba6c53941cde6b4245d4c81d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 12:02:43 -0400

    2024-06-11T16:02:43.310Zfunction_auto_return functions_rename_if_starts_with

--- public/src/functions_rename_if_starts_with.mjs
+++ public/src/functions_rename_if_starts_with.mjs
@@ -1,1 +1,4 @@
-export function functions_rename_if_starts_with(prefix_before, prefix_after) {function_rename}
+import { function_rename } from "./function_rename.mjs";
+export function functions_rename_if_starts_with(prefix_before, prefix_after) {
+  function_rename;
+}
