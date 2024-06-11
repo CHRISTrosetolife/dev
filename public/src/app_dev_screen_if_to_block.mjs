3c734abec4d9b6a5d4d5d1fe8c3d28952b65b113
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 12:28:51 -0400

    2024-06-11T16:28:50.703Zfunction_auto_return app_dev_screen_if_to_block

--- public/src/app_dev_screen_if_to_block.mjs
+++ public/src/app_dev_screen_if_to_block.mjs
@@ -1,3 +1,15 @@
+import { function_transform } from "./function_transform.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { string_delimit_backtick } from "./string_delimit_backtick.mjs";
+import { app_dev_sandbox_function } from "./app_dev_sandbox_function.mjs";
+import { file_write } from "./file_write.mjs";
+import { function_name_to_path } from "./function_name_to_path.mjs";
+import { js_code_format } from "./js_code_format.mjs";
+import { js_code_statement_declare_assign } from "./js_code_statement_declare_assign.mjs";
+import { js_code_statement_assign } from "./js_code_statement_assign.mjs";
+import { list_join_empty } from "./list_join_empty.mjs";
+import { js_code_export_function_declare } from "./js_code_export_function_declare.mjs";
+import { function_name_new } from "./function_name_new.mjs";
 import { html_style_code } from "./html_style_code.mjs";
 import { noop } from "./noop.mjs";
 import { html_cycle_code } from "./html_cycle_code.mjs";
@@ -12,7 +24,7 @@ export function app_dev_screen_if_to_block() {
     name: function if_to_block_name(root) {
       return html_cycle_code_span(root, "changing `if` statements to blocks");
     },
-    screen: function if_to_block_screen(root) {
+    screen: async function if_to_block_screen(root) {
       html_cycle_p(
         root,
         [
@@ -39,6 +51,36 @@ export function app_dev_screen_if_to_block() {
         "... then the `if` statement will change to a block :",
       );
       app_dev_screen_img(app_dev_screen_if_to_block, root, i++);
+      let function_name_unique = await function_name_new("assign_to_let");
+      let contents_function = js_code_export_function_declare(
+        function_name_unique,
+        "a",
+        list_join_empty([
+          js_code_statement_assign("a", 1),
+          js_code_statement_declare_assign("b", 2),
+          js_code_statement_assign("b", 3),
+          js_code_statement_assign("c", 4),
+        ]),
+        false,
+      );
+      let contents = await js_code_format(contents_function);
+      let file_path = function_name_to_path(function_name_unique);
+      await file_write(file_path, contents);
+      await app_dev_sandbox_function(
+        root,
+        function_name_unique,
+        "adding `let`",
+        string_delimit_backtick(
+          string_combine_multiple([
+            function_transform.name,
+            " ",
+            fn.name,
+            " ",
+            function_name_unique,
+          ]),
+        ),
+        async () => await function_transform(fn.name, function_name_unique),
+      );
     },
   };
 }
