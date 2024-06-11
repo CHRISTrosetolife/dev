7a8120f37601bb5f5533cb0123963bf3f1984478
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 13:11:34 -0400

    2024-06-11T17:11:34.092Zfunction_auto_after function_transform_return

--- public/src/function_transform_return.mjs
+++ public/src/function_transform_return.mjs
@@ -1,5 +1,5 @@
+import { function_transform_args_return } from "./function_transform_args_return.mjs";
 import { list_to } from "./list_to.mjs";
-import { function_transform_args } from "./function_transform_args.mjs";
 import { equal } from "./equal.mjs";
 import { assert } from "./assert.mjs";
 import { list_length } from "./list_length.mjs";
@@ -7,5 +7,5 @@ export async function function_transform_return(lambda_names, function_name) {
   let args = list_to(arguments);
   let length = list_length(args);
   assert(equal, [length, 2]);
-  return await function_transform_args(lambda_names, function_name, "a");
+  return await function_transform_args_return(lambda_names, function_name, "a");
 }
