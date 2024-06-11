d85ff0a56599dc753c8a2e98f08a635fbefdf430
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 12:12:19 -0400

    2024-06-11T16:12:19.027Zfunction_auto_return app_ceb

--- public/src/html_audio.mjs
+++ public/src/html_audio.mjs
@@ -1,5 +1,5 @@
 export async function html_audio(src) {
-  const a = new Audio(src);
+  let a = new Audio(src);
   let result = new Promise((resolve) => {
     a.addEventListener("ended", function () {
       a.currentTime = 0;
