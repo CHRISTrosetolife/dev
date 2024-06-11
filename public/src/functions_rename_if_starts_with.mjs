7f7bb25661c8cd750dc60999afa2f188a34d66a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 12:03:48 -0400

    2024-06-11T16:03:48.578Zfunction_auto_return functions_rename_if_starts_with

--- public/src/functions_rename_if_starts_with.mjs
+++ public/src/functions_rename_if_starts_with.mjs
@@ -1,12 +1,15 @@
 import { each_async } from "./each_async.mjs";
 import { function_names } from "./function_names.mjs";
 import { function_rename } from "./function_rename.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
 export async function functions_rename_if_starts_with(
   prefix_before,
   prefix_after,
 ) {
   let fns = await function_names();
   await each_async(fns, (fn) => {
+    if (string_starts_with(prefix_before)) {
+    }
     function_rename;
   });
 }
