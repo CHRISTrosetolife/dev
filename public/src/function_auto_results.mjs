142c33bd8b3d00f61712e829ef6d4b9981fe9e5d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 13:23:42 -0400

    2024-06-11T17:23:41.244Z  c function_auto_after function_auto_results

---
+++ public/src/function_auto_results.mjs
@@ -0,0 +1,9 @@
+import { function_auto_return } from "./function_auto_return.mjs";
+import { assert } from "./assert.mjs";
+import { string_is } from "./string_is.mjs";
+export async function function_auto_results(function_name) {
+  let r = await function_auto_return(function_name);
+  let { after } = r;
+  assert(string_is, [after]);
+  return after;
+}
