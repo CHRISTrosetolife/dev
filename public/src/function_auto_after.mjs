fc253503b6557b91151be842c2f65f965f896fa8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 13:13:31 -0400

    2024-06-11T17:13:30.506Zfunction_auto_after function_auto_after

--- public/src/function_auto_after.mjs
+++ public/src/function_auto_after.mjs
@@ -2,10 +2,13 @@ import { function_transform_return } from "./function_transform_return.mjs";
 import { list_join_comma } from "./list_join_comma.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 import { function_auto_transforms } from "./function_auto_transforms.mjs";
+import { assert } from "./assert.mjs";
+import { string_is } from "./string_is.mjs";
 export async function function_auto_after(function_name) {
   let transforms = function_auto_transforms();
   let names = list_map_property(transforms, "name");
   let joined = list_join_comma(names);
   let { after } = await function_transform_return(joined, function_name);
+  assert(string_is, [after]);
   return after;
 }
