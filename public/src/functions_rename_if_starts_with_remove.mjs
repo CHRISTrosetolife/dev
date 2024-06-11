f857472168d473a2c203dcc6792cc9df5552568f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 12:09:16 -0400

    2024-06-11T16:09:15.834Zfunction_auto_return functions_rename_if_starts_with_remove

--- public/src/functions_rename_if_starts_with_remove.mjs
+++ public/src/functions_rename_if_starts_with_remove.mjs
@@ -5,13 +5,13 @@ import { string_combine } from "./string_combine.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 export async function functions_rename_if_starts_with_remove(
-  prefix_before,
+  prefix,
   prefix_after,
 ) {
   let fns = await function_names();
   await each_async(fns, async (fn) => {
-    if (string_starts_with(fn, prefix_before)) {
-      let without = string_prefix_without(fn, prefix_before);
+    if (string_starts_with(fn, prefix)) {
+      let without = string_prefix_without(fn, prefix);
       let fn_new = string_combine(prefix_after, without);
       await function_rename(fn, fn_new);
     }
