479f56027eb97abe420852623e1bc05e454efc36
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 13:00:40 -0400

    2024-06-11T17:00:40.321Zfunction_auto_return object_property_get

--- public/src/object_property_get.mjs
+++ public/src/object_property_get.mjs
@@ -1,13 +1,10 @@
 import { assert_message_json } from "./assert_message_json.mjs";
-import { json_to } from "./json_to.mjs";
 import { undefined_not_is } from "./undefined_not_is.mjs";
 export function object_property_get(object, property_name) {
   let result = object[property_name];
-  assert_message_json(undefined_not_is, [result], () =>
-    json_to({
-      object,
-      property_name,
-    }),
-  );
+  assert_message_json(undefined_not_is, [result], () => ({
+    object,
+    property_name,
+  }));
   return result;
 }
