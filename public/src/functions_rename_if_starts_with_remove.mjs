1d0eeb154adad1e6742f8a0616377b96ec42fc50
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 12:19:15 -0400

    2024-06-11T16:19:14.495Z  w

--- public/src/functions_rename_if_starts_with_remove.mjs
+++ public/src/functions_rename_if_starts_with_remove.mjs
@@ -1,3 +1,4 @@
+import { string_includes } from "./string_includes.mjs";
 import { string_replace } from "./string_replace.mjs";
 import { each_async } from "./each_async.mjs";
 import { function_names } from "./function_names.mjs";
@@ -7,9 +8,10 @@ export async function functions_rename_if_starts_with_remove(prefix, removal) {
   let fns = await function_names();
   await each_async(fns, async (fn) => {
     if (string_starts_with(fn, prefix)) {
-        if (string_includes(fn,removal)){
-      let fn_new = string_replace(fn, removal, "");
-      await function_rename(fn, fn_new);}
+      if (string_includes(fn, removal)) {
+        let fn_new = string_replace(fn, removal, "");
+        await function_rename(fn, fn_new);
+      }
     }
   });
 }
