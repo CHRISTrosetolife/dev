b626e47a81ae52f0f2344cdd367f473ce56f6983
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 11:54:50 -0400

    2024-06-11T15:54:50.196Zfunction_auto_return html_style_alternate_monospace_short

--- public/src/html_style_alternate_monospace_short.mjs
+++ public/src/html_style_alternate_monospace_short.mjs
@@ -3,4 +3,5 @@ import { html_style_alternate_monospace } from "./html_style_alternate_monospace
 export function html_style_alternate_monospace_short(parent, message) {
   let split = html_style_alternate_split(message);
   html_style_alternate_monospace(parent, split);
+  html_style_alternate_p;
 }
