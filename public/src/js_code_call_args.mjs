234b9459b7e0cae75bfcd6d6eb6e68b8f5573029
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 12:32:11 -0400

    2024-06-11T16:32:11.178Zfunction_auto_return js_code_call_args

--- public/src/js_code_call_args.mjs
+++ public/src/js_code_call_args.mjs
@@ -1,5 +1,5 @@
 import { list_join_comma_space } from "./list_join_comma_space.mjs";
-export function js_code_call_args(function_name, args_code) {
-  let code = `${function_name}(${list_join_comma_space(args_code)})`;
+export function js_code_call_args(function_name, args_code_list) {
+  let code = `${function_name}(${list_join_comma_space(args_code_list)})`;
   return code;
 }
