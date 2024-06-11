121578b5d8f7d24ffec843e02fd13557982ec4be
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 13:11:19 -0400

    2024-06-11T17:11:18.833Zfunction_auto_after function_transform_args

--- public/src/function_transform_args.mjs
+++ public/src/function_transform_args.mjs
@@ -1,15 +1,13 @@
-import { function_transform_args_split } from "./function_transform_args_split.mjs";
-import { string_split_comma } from "./string_split_comma.mjs";
+import { function_transform_args_return } from "./function_transform_args_return.mjs";
 export async function function_transform_args(
   lambda_names,
   function_name,
   args,
 ) {
-  let args_split = string_split_comma(args);
-  let r = await function_transform_args_split(
+  let r = await function_transform_args_return(
+    args,
     lambda_names,
     function_name,
-    args_split,
   );
   let { results } = r;
   return results;
---
+++ public/src/function_transform_args_return.mjs
@@ -0,0 +1,15 @@
+import { function_transform_args_split } from "./function_transform_args_split.mjs";
+import { string_split_comma } from "./string_split_comma.mjs";
+export async function function_transform_args_return(
+  args,
+  lambda_names,
+  function_name,
+) {
+  let args_split = string_split_comma(args);
+  let r = await function_transform_args_split(
+    lambda_names,
+    function_name,
+    args_split,
+  );
+  return r;
+}
