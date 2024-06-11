67c64066a608f089d34b7ead0edcf5cfc84fa5f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 09:46:11 -0400

    2024-06-11T13:46:10.813Zfunction_auto_return app_dev_screen_if_to_block

--- public/src/app_dev_screen_if_to_block.mjs
+++ public/src/app_dev_screen_if_to_block.mjs
@@ -1,3 +1,4 @@
+import { html_style_alternate_monospace_short } from "./html_style_alternate_monospace_short.mjs";
 import { app_dev_p_transformer } from "./app_dev_p_transformer.mjs";
 import { js_if_to_block } from "./js_if_to_block.mjs";
 import { html_style_alternate_monospace_short_span } from "./html_style_alternate_monospace_short_span.mjs";
@@ -11,7 +12,10 @@ export function app_dev_screen_if_to_block() {
       );
     },
     screen: function if_to_block_screen(root) {
-      "if there is an `if` statement ...";
+      html_style_alternate_monospace_short(
+        root,
+        "if there is an `if` statement ...",
+      );
       let i = 1;
       app_dev_screen_img(app_dev_screen_if_to_block, root, i++);
       let fn = js_if_to_block;
