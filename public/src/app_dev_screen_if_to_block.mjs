b250367724f265948cc7dc6e467a76a40ba8a217
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-11 09:38:44 -0400

    2024-06-11T13:38:43.825Zfunction_auto_return app_dev_screen_if_to_block

--- public/src/app_dev_screen_if_to_block.mjs
+++ public/src/app_dev_screen_if_to_block.mjs
@@ -3,7 +3,7 @@ import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_if_to_block() {
   return {
     name: function if_to_block_name(root) {
-      return html_span_text(root, "if_to_block");
+      return html_span_text(root, "changing `if` statements to blocks");
     },
     screen: function if_to_block_screen(root) {
       let i = 1;
