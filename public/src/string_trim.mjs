fb8ce9e72cadf3ab07ab30a3b066e8af43bf6973
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 12:51:58 -0400

    2024-06-12T16:51:54.966Z  w

--- public/src/string_trim.mjs
+++ public/src/string_trim.mjs
@@ -2,7 +2,7 @@ import { string_take_less_1 } from "./string_take_less_1.mjs";
 import { string_ends_with_multiple } from "./string_ends_with_multiple.mjs";
 import { string_skip_1 } from "./string_skip_1.mjs";
 import { string_starts_with_multiple } from "./string_starts_with_multiple.mjs";
-export function string_trim(t, symbols) {
+export function string_trim(t, symbols) {symbols=list_to(symbols);
   while (string_starts_with_multiple(t, symbols)) {
     t = string_skip_1(t);
   }
