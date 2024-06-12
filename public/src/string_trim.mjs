05e40d8343d0a174bf3b6f9b6dd54ff5f538f3a5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 12:50:13 -0400

    2024-06-12T16:50:13.151Z  string_trim abddc ca

--- public/src/string_trim.mjs
+++ public/src/string_trim.mjs
@@ -3,10 +3,10 @@ import { string_ends_with_multiple } from "./string_ends_with_multiple.mjs";
 import { string_skip_1 } from "./string_skip_1.mjs";
 import { string_starts_with_multiple } from "./string_starts_with_multiple.mjs";
 export function string_trim(t, symbols) {
-  while (string_starts_with_multiple(symbols, t)) {
+  while (string_starts_with_multiple(t, symbols)) {
     t = string_skip_1(t);
   }
-  while (string_ends_with_multiple(symbols, t)) {
+  while (string_ends_with_multiple( t, symbols)) {
     t = string_take_less_1(t);
   }
   return t;
