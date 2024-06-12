977d5987411d1a77bb958e5991d7c8cf0366fc27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 15:51:15 -0400

    2024-06-12T19:51:15.211Zfunction_auto_after arc_sandbox

--- public/src/arc_sandbox.mjs
+++ public/src/arc_sandbox.mjs
@@ -1,6 +1,5 @@
-import { list_all } from "./list_all.mjs";
+import { list_rectangular_is } from "./list_rectangular_is.mjs";
 import { assert_message } from "./assert_message.mjs";
-import { equal } from "./equal.mjs";
 import { each } from "./each.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { folder_downloads_repository } from "./folder_downloads_repository.mjs";
@@ -8,8 +7,6 @@ import { folder_read } from "./folder_read.mjs";
 import { file_read_json } from "./file_read_json.mjs";
 import { assert } from "./assert.mjs";
 import { list_is } from "./list_is.mjs";
-import { list_length } from "./list_length.mjs";
-import { list_first } from "./list_first.mjs";
 export async function arc_sandbox() {
   let training = string_combine(
     folder_downloads_repository("ARC-AGI"),
@@ -24,9 +21,7 @@ export async function arc_sandbox() {
       let { input, output } = t;
       each([input, output], (io) => {
         assert(list_is, [io]);
-        let first = list_first(io);
-        let length = list_length(first);
-        let same = list_all(io, (row) => equal(list_length(row), length));
+        list_rectangular_is(io);
         assert_message();
       });
     });
