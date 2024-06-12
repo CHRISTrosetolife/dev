f35c73871a6b1b52977c70bdd185d9108354f296
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 16:35:46 -0400

    2024-06-12T20:35:45.997Zfunction_auto_after arc_sandbox

--- public/src/arc_sandbox.mjs
+++ public/src/arc_sandbox.mjs
@@ -46,7 +46,7 @@ export async function arc_sandbox() {
           let p = integer_parse(key);
           if (p !== 0) {
             let coordinates = arc_coordinates(io, p);
-            let xs = list_map(coordinates, "x");
+            let xs = list_unique(list_map(coordinates, "x"));
             let ys = list_map(coordinates, "y");
             log({
               coordinates,
