fae54d81aa7159a97276610789d2c156efb2ed0b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 12:01:25 -0400

    2024-06-12T16:01:25.049Zfunction_auto_after ceb_bible_words_3

--- public/src/ceb_bible_words_3.mjs
+++ public/src/ceb_bible_words_3.mjs
@@ -1,60 +1,22 @@
-import { strng_trim } from "./strng_trim.mjs";
 import { each_index_only_async } from "./each_index_only_async.mjs";
 import { identity } from "./identity.mjs";
 import { list_sort_string } from "./list_sort_string.mjs";
-import { string_empty_not_is } from "./string_empty_not_is.mjs";
-import { string_symbols } from "./string_symbols.mjs";
 import { each } from "./each.mjs";
 import { list_adder_unique_async } from "./list_adder_unique_async.mjs";
 import { bible_ceb_3_chapter } from "./bible_ceb_3_chapter.mjs";
 import { bible_ceb_3_hrefs } from "./bible_ceb_3_hrefs.mjs";
 export async function ceb_bible_words_3() {
-  let symbols = [
-    "-",
-    ",",
-    ";",
-    ":",
-    "!",
-    "?",
-    ".",
-    '"',
-    "(",
-    ")",
-    "¶",
-    "/",
-    "`",
-    "|",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "7",
-    "8",
-    "9",
-  ];
-  let characters;
   let words = await list_adder_unique_async(async (law) => {
-    characters = await list_adder_unique_async(async (las) => {
-      let hrefs = await bible_ceb_3_hrefs();
-      await each_index_only_async(hrefs, async (index) => {
-        let verses = await bible_ceb_3_chapter(index);
-        each(verses, (v) =>
-          each(v.tokens, (t) => {
-            each(string_symbols(t), las);
-            t = strng_trim(t, symbols);
-            if (string_empty_not_is(t)) {
-              law(t);
-            }
-          }),
-        );
-      });
+    let hrefs = await bible_ceb_3_hrefs();
+    await each_index_only_async(hrefs, async (index) => {
+      let verses = await bible_ceb_3_chapter(index);
+      each(verses, (v) =>
+        each(v.tokens, (t) => {
+          law(t);
+        }),
+      );
     });
   });
   list_sort_string(characters, identity);
-  if (0) {
-    return characters;
-  }
   return words;
 }
