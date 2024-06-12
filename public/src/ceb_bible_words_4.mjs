834ed24a9474050ddba7313a27eb0ac7946deb09
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 12:02:09 -0400

    2024-06-12T16:02:08.899Zfunction_auto_after ceb_bible_words_4

--- public/src/ceb_bible_words_4.mjs
+++ public/src/ceb_bible_words_4.mjs
@@ -1,8 +1,3 @@
-import { strng_trim } from "./strng_trim.mjs";
-import { identity } from "./identity.mjs";
-import { list_sort_string } from "./list_sort_string.mjs";
-import { string_empty_not_is } from "./string_empty_not_is.mjs";
-import { string_symbols } from "./string_symbols.mjs";
 import { each } from "./each.mjs";
 import { list_adder_unique_async } from "./list_adder_unique_async.mjs";
 import { each_index_only_async } from "./each_index_only_async.mjs";
@@ -10,58 +5,22 @@ import { bible_ceb_4_chapter } from "./bible_ceb_4_chapter.mjs";
 import { ceb_bible_4_chapters_hrefs } from "./ceb_bible_4_chapters_hrefs.mjs";
 import { bible_ceb_4_hrefs_books } from "./bible_ceb_4_hrefs_books.mjs";
 export async function ceb_bible_words_4() {
-  let symbols = [
-    "-",
-    ",",
-    ";",
-    ":",
-    "!",
-    "?",
-    ".",
-    '"',
-    "(",
-    ")",
-    "¶",
-    "/",
-    "`",
-    "|",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "7",
-    "8",
-    "9",
-  ];
-  let characters;
-  let words = await list_adder_unique_async(async (law) => {
-    characters = await list_adder_unique_async(async (las) => {
-      let book_hrefs = await bible_ceb_4_hrefs_books();
-      await each_index_only_async(book_hrefs, async (book_index) => {
-        let hrefs_chapters = await ceb_bible_4_chapters_hrefs(
-          book_hrefs,
-          book_index,
+  let words = await list_adder_unique_async(async (la) => {
+    let book_hrefs = await bible_ceb_4_hrefs_books();
+    await each_index_only_async(book_hrefs, async (book_index) => {
+      let hrefs_chapters = await ceb_bible_4_chapters_hrefs(
+        book_hrefs,
+        book_index,
+      );
+      await each_index_only_async(hrefs_chapters, async (chapter_index) => {
+        let verses = await bible_ceb_4_chapter(book_index, chapter_index);
+        each(verses, (v) =>
+          each(v.tokens, (t) => {
+            la(t);
+          }),
         );
-        await each_index_only_async(hrefs_chapters, async (chapter_index) => {
-          let verses = await bible_ceb_4_chapter(book_index, chapter_index);
-          each(verses, (v) =>
-            each(v.tokens, (t) => {
-              each(string_symbols(t), las);
-              t = strng_trim(t, symbols);
-              if (string_empty_not_is(t)) {
-                law(t);
-              }
-            }),
-          );
-        });
       });
     });
   });
-  list_sort_string(characters, identity);
-  if (0) {
-    return characters;
-  }
   return words;
 }
