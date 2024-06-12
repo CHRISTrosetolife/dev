abed7fe3dc18127e1f29793e93e4db7a9be18790
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 15:29:29 -0400

    2024-06-12T19:29:29.073Zfunction_auto_after arc_sandbox

--- public/src/arc_sandbox.mjs
+++ public/src/arc_sandbox.mjs
@@ -6,5 +6,5 @@ export async function arc_sandbox() {
     folder_downloads_repository("ARC-AGI"),
     "data\\training",
   );
-  return await folder_read(training);
+  return await folder_read(training, ".json");
 }
