73b98f857bec718ea7972c8e833158a50852f4d4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 15:21:51 -0400

    2024-06-12T19:21:48.412Zfunction_auto_after folder_user_downloads

--- public/src/folder_user_downloads.mjs
+++ public/src/folder_user_downloads.mjs
@@ -1,1 +1,5 @@
-export function folder_user_downloads() {}
+import { folder_user } from "./folder_user.mjs";
+import { string_combine } from "./string_combine.mjs";
+export function folder_user_downloads() {
+  return string_combine(folder_user(), "Downloads\\");
+}
