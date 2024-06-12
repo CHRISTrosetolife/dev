b785ca6751cdb9b0d669cf7954e9c5e2746652ab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 12:45:10 -0400

    2024-06-12T16:45:09.765Zfunction_auto_after string_symbols

--- public/src/string_symbols.mjs
+++ public/src/string_symbols.mjs
@@ -4,6 +4,7 @@ import { list_adder_unique } from "./list_adder_unique.mjs";
 import { list_join_empty } from "./list_join_empty.mjs";
 import { list_sort_string } from "./list_sort_string.mjs";
 export function string_symbols(text) {
+  let list = [text];
   let s = list_adder_unique((la) => each(text, la));
   list_sort_string(s, identity);
   return list_join_empty(s);
