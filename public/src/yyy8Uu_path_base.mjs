b3d0417ab38de5db9aaa6f115d5b9302c6865ca1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 15:22:01 -0400

    2024-06-12T19:22:01.250Zfunction_auto_after yyy8Uu_path_base

--- public/src/yyy8Uu_path_base.mjs
+++ public/src/yyy8Uu_path_base.mjs
@@ -1,8 +1,8 @@
-import { folder_user } from "./folder_user.mjs";
+import { folder_user_downloads } from "./folder_user_downloads.mjs";
 import { string_combine } from "./string_combine.mjs";
 export function yyy8Uu_path_base() {
   return string_combine(
-    folder_user(),
+    folder_user_downloads(),
     "Downloads\\yyy8Uu-master\\yyy8Uu-master\\",
   );
 }
