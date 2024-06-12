9a262825a0a1822903793608f9e5c54f769fa2ec
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 15:26:24 -0400

    2024-06-12T19:26:24.334Zfunction_auto_after arc_sandbox

--- public/src/arc_sandbox.mjs
+++ public/src/arc_sandbox.mjs
@@ -1,1 +1,4 @@
-export function arc_sandbox() {}
+import { folder_downloads_repository } from "./folder_downloads_repository.mjs";
+export function arc_sandbox() {
+  folder_downloads_repository;
+}
