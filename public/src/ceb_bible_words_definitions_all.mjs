5d728a1cfc2637fff30d42566a22430367b59dce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 12:55:06 -0400

    2024-06-12T16:55:01.093Zfunction_auto_after ceb_bible_words_definitions_all

--- public/src/ceb_bible_words_definitions_all.mjs
+++ public/src/ceb_bible_words_definitions_all.mjs
@@ -1,11 +1,12 @@
-import { string_symbols_multiple } from "./string_symbols_multiple.mjs";
+import { string_trim } from "./string_trim.mjs";
 import { ceb_bible_words_cache } from "./ceb_bible_words_cache.mjs";
 import { string_count_words } from "./string_count_words.mjs";
 import { ceb_bible_words_definitions_get } from "./ceb_bible_words_definitions_get.mjs";
+import { list_map } from "./list_map.mjs";
 export async function ceb_bible_words_definitions_all() {
   let all = await ceb_bible_words_cache();
-  let symbols = "";
-  return string_symbols_multiple(all);
+  let symbols = "_-–—,;:!?.'‘’\"“”()[]}¶/`¯|€0123456789";
+  all = list_map(all, (a) => string_trim(a, symbols));
   let c = string_count_words(all);
   let result = await ceb_bible_words_definitions_get(c);
   return result;
