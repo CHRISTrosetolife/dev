06ba4791100ff3efde60336d92bf447fdff0025e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 12:27:55 -0400

    2024-06-12T16:27:50.414Z  w

--- public/src/function_cache_delete_args.mjs
+++ public/src/function_cache_delete_args.mjs
@@ -3,6 +3,10 @@ import { file_delete } from "./file_delete.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 export async function function_cache_delete_args(function_name, args_string) {
   let args = string_split_comma(args_string);
-  let file_path = function_cache_path(function_name, args);
-  await file_delete(file_path);
+  await function_cache_delete_generic(function_name, args);
 }
+async function function_cache_delete_generic(function_name, args) {
+    let file_path = function_cache_path(function_name, args);
+    await file_delete(file_path);
+}
+
