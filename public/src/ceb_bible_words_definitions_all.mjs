9fc68405588dff75773958664babec89c3523dae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 12:53:45 -0400

    2024-06-12T16:53:43.208Zfunction_auto_after ceb_bible_words_definitions_all

--- public/src/ceb_bible_words_definitions_all.mjs
+++ public/src/ceb_bible_words_definitions_all.mjs
@@ -4,6 +4,7 @@ import { string_count_words } from "./string_count_words.mjs";
 import { ceb_bible_words_definitions_get } from "./ceb_bible_words_definitions_get.mjs";
 export async function ceb_bible_words_definitions_all() {
   let all = await ceb_bible_words_cache();
+  let symbols = "";
   return string_symbols_multiple(all);
   let c = string_count_words(all);
   let result = await ceb_bible_words_definitions_get(c);
