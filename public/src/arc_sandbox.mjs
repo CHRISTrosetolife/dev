43f07763b49d71b09cee576a6cf0cc8011734b71
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 15:30:31 -0400

    2024-06-12T19:30:31.132Zfunction_auto_after arc_sandbox

--- public/src/arc_sandbox.mjs
+++ public/src/arc_sandbox.mjs
@@ -1,19 +1,17 @@
 import { error } from "./error.mjs";
-import { log } from "./log.mjs";
 import { each } from "./each.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { folder_downloads_repository } from "./folder_downloads_repository.mjs";
 import { folder_read } from "./folder_read.mjs";
+import { file_read_json } from "./file_read_json.mjs";
 export async function arc_sandbox() {
   let training = string_combine(
     folder_downloads_repository("ARC-AGI"),
     "data\\training",
   );
   let files = await folder_read(training, ".json");
-  each(files, (file) => {
-    log({
-      file,
-    });
+  each(files, async (file) => {
+    let o = await file_read_json(file);
     error();
   });
 }
