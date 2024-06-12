f14c86c9939d0edecf7a61b642889a3baa3d9a44
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 16:12:03 -0400

    2024-06-12T20:12:02.693Zfunction_auto_after each

--- public/src/each.mjs
+++ public/src/each.mjs
@@ -1,4 +1,6 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function each(list, lambda) {
+  assert_arguments_length(arguments, 2);
   for (let item of list) {
     lambda(item);
   }
