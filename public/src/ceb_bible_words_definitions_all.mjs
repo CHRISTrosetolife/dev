72748a0bbb867d2832313e5233a0634be1e5b17a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 12:47:21 -0400

    2024-06-12T16:47:20.005Zfunction_auto_after ceb_bible_words_definitions_all

--- public/src/ceb_bible_words_definitions_all.mjs
+++ public/src/ceb_bible_words_definitions_all.mjs
@@ -1,8 +1,10 @@
+import { string_symbols_multiple } from "./string_symbols_multiple.mjs";
 import { ceb_bible_words_cache } from "./ceb_bible_words_cache.mjs";
 import { string_count_words } from "./string_count_words.mjs";
 import { ceb_bible_words_definitions_get } from "./ceb_bible_words_definitions_get.mjs";
 export async function ceb_bible_words_definitions_all() {
   let all = await ceb_bible_words_cache();
+  return string_symbols_multiple(all);
   let c = string_count_words(all);
   let result = await ceb_bible_words_definitions_get(c);
   return result;
