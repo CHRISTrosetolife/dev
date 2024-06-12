9a82b92aff83e8902a801afce7b3f0080ed764d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 12:27:57 -0400

    2024-06-12T16:27:56.757Zfunction_auto_after function_cache_delete_args

---
+++ public/src/function_cache_delete_generic.mjs
@@ -0,0 +1,6 @@
+import { file_delete } from "./file_delete.mjs";
+import { function_cache_path } from "./function_cache_path.mjs";
+export async function function_cache_delete_generic(function_name, args) {
+  let file_path = function_cache_path(function_name, args);
+  await file_delete(file_path);
+}
