fe1c284393a174891d213eca0992f8e18ed09086
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 15:23:10 -0400

    2024-06-12T19:23:10.167Zfunction_auto_after yyy8Uu_path_base

--- public/src/yyy8Uu_path_base.mjs
+++ public/src/yyy8Uu_path_base.mjs
@@ -1,10 +1,8 @@
 import { folder_user_downloads } from "./folder_user_downloads.mjs";
 import { string_combine } from "./string_combine.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function yyy8Uu_path_base() {
   let repository_name = "yyy8Uu";
   let m = string_combine(repository_name, "-master\\");
-  return string_combine(
-    folder_user_downloads(),
-    "yyy8Uu-master\\yyy8Uu-master\\",
-  );
+  return string_combine_multiple([folder_user_downloads(), m, m]);
 }
