7890663d5ae633102d03b581b5b1df9d2d162d4c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 11:59:07 -0400

    2024-06-12T15:59:07.199Zfunction_auto_after ceb_bible_words_3

--- public/src/ceb_bible_words_3.mjs
+++ public/src/ceb_bible_words_3.mjs
@@ -1,11 +1,8 @@
+import { strng_trim } from "./strng_trim.mjs";
 import { each_index_only_async } from "./each_index_only_async.mjs";
 import { identity } from "./identity.mjs";
 import { list_sort_string } from "./list_sort_string.mjs";
 import { string_empty_not_is } from "./string_empty_not_is.mjs";
-import { string_take_less_1 } from "./string_take_less_1.mjs";
-import { string_ends_with_multiple } from "./string_ends_with_multiple.mjs";
-import { string_skip } from "./string_skip.mjs";
-import { string_starts_with_multiple } from "./string_starts_with_multiple.mjs";
 import { string_symbols } from "./string_symbols.mjs";
 import { each } from "./each.mjs";
 import { list_adder_unique_async } from "./list_adder_unique_async.mjs";
@@ -46,12 +43,7 @@ export async function ceb_bible_words_3() {
         each(verses, (v) =>
           each(v.tokens, (t) => {
             each(string_symbols(t), las);
-            while (string_starts_with_multiple(symbols, t)) {
-              t = string_skip(t, 1);
-            }
-            while (string_ends_with_multiple(symbols, t)) {
-              t = string_take_less_1(t);
-            }
+            t = strng_trim(t, symbols);
             if (string_empty_not_is(t)) {
               law(t);
             }
