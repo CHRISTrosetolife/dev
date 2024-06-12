8272d8a2b829d5c77f243b6db04aa072c261a483
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 15:51:52 -0400

    2024-06-12T19:51:52.104Zfunction_auto_after list_rectangular_is

--- public/src/list_rectangular_is.mjs
+++ public/src/list_rectangular_is.mjs
@@ -1,8 +1,11 @@
+import { list_is } from "./list_is.mjs";
+import { assert } from "./assert.mjs";
 import { equal } from "./equal.mjs";
 import { list_all } from "./list_all.mjs";
 import { list_length } from "./list_length.mjs";
 import { list_first } from "./list_first.mjs";
 export function list_rectangular_is(io) {
+  assert(list_is, [io]);
   let first = list_first(io);
   let length = list_length(first);
   let same = list_all(io, (row) => equal(list_length(row), length));
