53d54896f99bbfa34988b11b41bc2268894c71d3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 15:26:05 -0400

    2024-06-12T19:26:04.885Zfunction_auto_after yyy8Uu_path_base

--- public/src/yyy8Uu_path_base.mjs
+++ public/src/yyy8Uu_path_base.mjs
@@ -1,5 +1,4 @@
 import { folder_downloads_repository } from "./folder_downloads_repository.mjs";
 export function yyy8Uu_path_base() {
-  let repository_name = "yyy8Uu";
-  return folder_downloads_repository(repository_name);
+  return folder_downloads_repository("yyy8Uu");
 }
