2f9d2475a12b29acffa9a0c47f7206c71eca0c13
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-12 12:46:34 -0400

    2024-06-12T16:46:32.869Zfunction_auto_after string_symbols

--- public/src/string_symbols.mjs
+++ public/src/string_symbols.mjs
@@ -1,4 +1,4 @@
 import { string_symbols_multiple } from "./string_symbols_multiple.mjs";
 export function string_symbols(text) {
-  return string_symbols_multiple(list);
+  return string_symbols_multiple([text]);
 }
