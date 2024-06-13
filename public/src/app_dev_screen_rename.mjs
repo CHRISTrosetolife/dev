788493f823c2ae908093f2fc133c4c81a6c6bba1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 11:48:05 -0400

    2024-06-13T15:48:04.812Zfunction_auto_after app_dev_screen_rename

--- public/src/app_dev_screen_rename.mjs
+++ public/src/app_dev_screen_rename.mjs
@@ -3,7 +3,10 @@ import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_rename() {
   return {
     name: function rename_name(root) {
-      return html_span_text(root, "rename");
+      return html_span_text(
+        root,
+        "renaming a `function` or multiple `function`s at once",
+      );
     },
     screen: function rename_screen(root) {
       let i = 1;
