0374737265cbc4ba7ad32c46751fa767b5170f53
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 14:02:38 -0400

    2024-06-13T18:02:37.821Zfunction_auto_after html_style_font_size

--- public/src/html_style_font_size.mjs
+++ public/src/html_style_font_size.mjs
@@ -1,5 +1,5 @@
 import { html_style_font_size_object } from "./html_style_font_size_object.mjs";
 import { html_style } from "./html_style.mjs";
-export function html_style_font_size(c, font_size_bigger) {
-  html_style(c, html_style_font_size_object(font_size_bigger));
+export function html_style_font_size(c, size) {
+  html_style(c, html_style_font_size_object(size));
 }
