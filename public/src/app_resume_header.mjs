703dd4b9bbc8a1cb4aa6208cd646b80711e7e121
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 13:55:55 -0400

    2024-06-13T17:55:55.471Zfunction_auto_after app_resume_header

--- public/src/app_resume_header.mjs
+++ public/src/app_resume_header.mjs
@@ -1,9 +1,13 @@
+import { html_style_font_size } from "./html_style_font_size.mjs";
+import { html_style_font_size_property } from "./html_style_font_size_property.mjs";
 import { html_style_default_font_size_value } from "./html_style_default_font_size_value.mjs";
 import { html_style_centered } from "./html_style_centered.mjs";
 import { html_style_bold } from "./html_style_bold.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 export function app_resume_header(root, header) {
   let h = html_p_text(root, header);
+  html_style_font_size_property();
+  html_style_font_size(h, font_size_bigger);
   html_style_bold(h);
   html_style_centered(h);
   html_style_default_font_size_value();
