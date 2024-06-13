d8c29b30e3519eb6ffede6373d3ad9c3db7fbc81
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 13:15:50 -0400

    2024-06-13T17:15:50.069Zfunction_auto_after app_resume

--- public/src/app_resume.mjs
+++ public/src/app_resume.mjs
@@ -15,6 +15,7 @@ export function app_resume() {
     root,
     html_replace_nbsp("1612 Orlando Circle South"),
   );
+  html_span_text(root, " ⚫ ");
   let address2 = html_span_text(
     root,
     html_replace_nbsp("Jacksonville, Florida 32207"),
