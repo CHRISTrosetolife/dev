94fba0ac6e67b8ffac7fb033d7d32bb9d5ae934e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 19:58:54 -0400

    2024-06-13T23:58:53.846Zfunction_auto_after string_suffix_without

--- public/src/string_suffix_without.mjs
+++ public/src/string_suffix_without.mjs
@@ -2,9 +2,9 @@ import { string_length } from "./string_length.mjs";
 import { string_take } from "./string_take.mjs";
 import { assert_boolean } from "./assert_boolean.mjs";
 import { string_ends_with } from "./string_ends_with.mjs";
-export function string_suffix_without(input, prefix) {
-  assert_boolean(string_ends_with(input, prefix));
-  let prefix_length = string_length(prefix);
+export function string_suffix_without(input, suffix) {
+  assert_boolean(string_ends_with(input, suffix));
+  let prefix_length = string_length(suffix);
   let input_length = string_length(input);
   let difference = input_length - prefix_length;
   return string_take(input, difference);
