2828c3a30d66c4913d459a0ea8a3f42bbc73fc32
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 13:07:33 -0400

    2024-06-13T17:07:33.490Zfunction_auto_after html_style_email

--- public/src/html_style_email.mjs
+++ public/src/html_style_email.mjs
@@ -1,4 +1,4 @@
 import { html_style_link_prefix } from "./html_style_link_prefix.mjs";
-export function html_style_email(phone_number_text) {
-  return html_style_link_prefix("tel", phone_number_text);
+export function html_style_email(email_address) {
+  return html_style_link_prefix("mailto", email_address);
 }
