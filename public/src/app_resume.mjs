35b009a956c7951ab43bc1b38e19418953738259
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 12:53:00 -0400

    2024-06-13T16:53:00.490Zfunction_auto_after app_resume

--- public/src/app_resume.mjs
+++ public/src/app_resume.mjs
@@ -2,5 +2,5 @@ import { html_p_text } from "./html_p_text.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_resume() {
   let root = html_style_default_initialize();
-  html_p_text("JARED MATHIS");
+  let full_name = html_p_text("JARED MATHIS");
 }
