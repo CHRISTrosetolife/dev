eeb23f0db65a0715db79ba79b8589b04bc640376
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 13:51:28 -0400

    2024-06-13T17:51:28.593Zfunction_auto_after html_style_default_font_size

--- public/src/html_style_default_font_size.mjs
+++ public/src/html_style_default_font_size.mjs
@@ -1,8 +1,9 @@
-import { html_style_default_font_size_value_get } from "./html_style_default_font_size_value_get.mjs";
+import { html_style_font_size_object } from "./html_style_font_size_object.mjs";
 import { html_style_default } from "./html_style_default.mjs";
 import { object_merge } from "./object_merge.mjs";
 export function html_style_default_font_size(default_font_size) {
-  object_merge(html_style_default(), {
-    "font-size": html_style_default_font_size_value_get(default_font_size),
-  });
+  object_merge(
+    html_style_default(),
+    html_style_font_size_object(default_font_size),
+  );
 }
---
+++ public/src/html_style_font_size_object.mjs
@@ -0,0 +1,6 @@
+import { html_style_default_font_size_value_get } from "./html_style_default_font_size_value_get.mjs";
+export function html_style_font_size_object(default_font_size) {
+  return {
+    "font-size": html_style_default_font_size_value_get(default_font_size),
+  };
+}
