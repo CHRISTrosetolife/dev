f8a895d783ad61b1d275cfe0379a39139bd1aade
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 12:55:47 -0400

    2024-06-13T16:55:47.462Zfunction_auto_after app_resume

--- public/src/app_resume.mjs
+++ public/src/app_resume.mjs
@@ -1,3 +1,4 @@
+import { html_span_text } from "./html_span_text.mjs";
 import { html_style_centered } from "./html_style_centered.mjs";
 import { html_style_bold } from "./html_style_bold.mjs";
 import { html_p_text } from "./html_p_text.mjs";
@@ -7,4 +8,5 @@ export function app_resume() {
   let full_name = html_p_text(root, "JARED MATHIS");
   html_style_bold(full_name);
   html_style_centered(full_name);
+  html_span_text(root, "+1 (904) 314-4052");
 }
