3a0dc560fc0c24cae2cdb920ab66a789afa2a6d5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 13:12:57 -0400

    2024-06-13T17:12:56.944Zfunction_auto_after app_resume

--- public/src/app_resume.mjs
+++ public/src/app_resume.mjs
@@ -5,13 +5,20 @@ import { html_style_bold } from "./html_style_bold.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { html_span_text } from "./html_span_text.mjs";
+import { html_replace_nbsp } from "./html_replace_nbsp.mjs";
 export function app_resume() {
   let root = html_style_default_initialize();
   let full_name = html_p_text(root, "JARED MATHIS");
   html_style_bold(full_name);
   html_style_centered(full_name);
-  let address1 = html_span_text(root, "1612 Orlando Circle South");
-  let address2 = html_span_text(root, "Jacksonville, Florida 32207");
+  let address1 = html_span_text(
+    root,
+    html_replace_nbsp("1612 Orlando Circle South"),
+  );
+  let address2 = html_span_text(
+    root,
+    html_replace_nbsp("Jacksonville, Florida 32207"),
+  );
   let phone = html_phone(root, "+1 (904) 314-4052");
   html_email(root, "JESUSrosetolife@protonmail.com");
 }
