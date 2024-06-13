abc645c3975ab87f7ff858e86cc64e9e6b24c1ac
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 19:50:06 -0400

    2024-06-13T23:50:06.240Zfunction_auto_after files_rename_if_ends_with

--- public/src/files_rename_if_ends_with.mjs
+++ public/src/files_rename_if_ends_with.mjs
@@ -1,1 +1,4 @@
-export function files_rename_if_ends_with(input_path, suffix) {}
+import { folder_read } from "./folder_read.mjs";
+export async function files_rename_if_ends_with(input_path, suffix) {
+  let files = await folder_read(input_path);
+}
