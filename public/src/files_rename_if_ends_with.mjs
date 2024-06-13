4df380c62afcfb7293b0c64c846f71a141a5594c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 19:51:08 -0400

    2024-06-13T23:51:08.171Zfunction_auto_after files_rename_if_ends_with

--- public/src/files_rename_if_ends_with.mjs
+++ public/src/files_rename_if_ends_with.mjs
@@ -2,7 +2,11 @@ import { each_async } from "./each_async.mjs";
 import { file_rename } from "./file_rename.mjs";
 import { folder_read } from "./folder_read.mjs";
 import { string_ends_with } from "./string_ends_with.mjs";
-export async function files_rename_if_ends_with(input_path, suffix) {
+export async function files_rename_if_ends_with(
+  input_path,
+  suffix_old,
+  suffix_new,
+) {
   let files = await folder_read(input_path);
   await each_async(files, async (file) => {
     if (!string_ends_with(suffix)) {
