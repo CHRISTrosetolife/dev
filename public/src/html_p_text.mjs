dc49bab04dc304c300cc66fb7a6af4cb9dca6491
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 12:53:47 -0400

    2024-06-13T16:53:47.013Zfunction_auto_after html_p_text

--- public/src/html_p_text.mjs
+++ public/src/html_p_text.mjs
@@ -1,6 +1,7 @@
 import { html_p } from "./html_p.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
 export function html_p_text(parent, text) {
+  arguments_assert_length(arguments, 2);
   let p = html_p(parent);
   html_inner_set(p, text);
   return p;
