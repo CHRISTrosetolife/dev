db26552a31a5b5d67c986b5078ca0003ea5ce7d7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 13:26:34 -0400

    2024-06-13T17:26:34.081Z  c app_resume_header app_resume_header_3

---
+++ public/src/app_resume_header_3.mjs
@@ -0,0 +1,8 @@
+import { html_style_centered } from "./html_style_centered.mjs";
+import { html_style_bold } from "./html_style_bold.mjs";
+import { html_p_text } from "./html_p_text.mjs";
+export function app_resume_header_3(root, header) {
+  let full_name = html_p_text(root, header);
+  html_style_bold(full_name);
+  html_style_centered(full_name);
+}
