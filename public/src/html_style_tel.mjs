02b53c1f173feabf078743dd915d5e1bafaeefc1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 13:06:43 -0400

    2024-06-13T17:06:43.012Zfunction_auto_after html_style_tel

--- public/src/html_style_tel.mjs
+++ public/src/html_style_tel.mjs
@@ -1,7 +1,8 @@
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_style_link } from "./html_style_link.mjs";
 export function html_style_tel(phone_number_text) {
+  let prefix = "tel";
   return html_style_link(
-    string_combine_multiple(["tel", ":", phone_number_text]),
+    string_combine_multiple([prefix, ":", phone_number_text]),
   );
 }
