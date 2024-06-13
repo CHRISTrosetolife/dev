97dcefc24a081b3974e17032c68d7ebf2fb59aa1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 19:55:30 -0400

    2024-06-13T23:55:29.961Zfunction_auto_after string_ends_with

--- public/src/string_ends_with.mjs
+++ public/src/string_ends_with.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { string_get_reverse } from "./string_get_reverse.mjs";
 import { string_starts_with_generic } from "./string_starts_with_generic.mjs";
 export function string_ends_with(input, prefix) {
+  assert_arguments_length(arguments, 2);
   let string_get_lambda = string_get_reverse;
   return string_starts_with_generic(string_get_lambda, input, prefix);
 }
