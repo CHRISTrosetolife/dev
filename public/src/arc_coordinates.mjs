91ffdf932e9405a76d5e704faf84f36653581b47
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 07:12:48 -0400

    2024-06-13T11:12:47.808Zfunction_auto_after arc_coordinates

--- public/src/arc_coordinates.mjs
+++ public/src/arc_coordinates.mjs
@@ -1,10 +1,10 @@
 import { each_index } from "./each_index.mjs";
 import { list_adder } from "./list_adder.mjs";
-export function arc_coordinates(io, p) {
+export function arc_coordinates(io, color) {
   return list_adder((la) => {
     each_index(io, (row, y) => {
       each_index(row, (c, x) => {
-        if (c === p) {
+        if (c === color) {
           la({
             y,
             x,
