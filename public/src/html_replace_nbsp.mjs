59c1ed4df8472920764a8f32daee4706152260a3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 13:18:51 -0400

    2024-06-13T17:18:50.838Zfunction_auto_after html_replace_nbsp

--- public/src/html_replace_nbsp.mjs
+++ public/src/html_replace_nbsp.mjs
@@ -1,5 +1,6 @@
 import { string_replace } from "./string_replace.mjs";
 export function html_replace_nbsp(s) {
   s = string_replace(s, " ", "&nbsp;");
+  s = string_replace(s, "-", "&#8209;");
   return s;
 }
