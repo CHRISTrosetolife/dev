4a835a46028281056668809b55d74eeb426f4a91
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 11:54:18 -0400

    2024-06-13T15:54:18.074Zfunction_auto_after app_dev_screen_rename

--- public/src/app_dev_screen_rename.mjs
+++ public/src/app_dev_screen_rename.mjs
@@ -24,6 +24,10 @@ export function app_dev_screen_rename() {
         root,
         "the file is renamed , all identifiers in all files are renamed , and all `import`s are updated to match the new file name",
       );
+      html_cycle_code(
+        root,
+        "it is easy to write a `function` that renames multiple functions at once :",
+      );
     },
   };
 }
