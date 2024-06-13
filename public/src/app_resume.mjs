ef23d400dc5eb54a6b47b905c33a8330f99d3558
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 13:22:09 -0400

    2024-06-13T17:22:09.747Zfunction_auto_after app_resume

--- public/src/app_resume.mjs
+++ public/src/app_resume.mjs
@@ -9,8 +9,7 @@ import { html_span_text } from "./html_span_text.mjs";
 import { html_p } from "./html_p.mjs";
 export function app_resume() {
   let root = html_style_default_initialize();
-  let header = "JARED MATHIS";
-  app_resume_header(root, header);
+  app_resume_header(root, "JARED MATHIS");
   let row = html_p(root);
   html_style_centered(row);
   let address1 = html_span_text(
