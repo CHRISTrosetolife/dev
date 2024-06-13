6de503265eca8a4fd6157e9d67108c9f07ee1e66
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 13:07:08 -0400

    2024-06-13T17:07:08.371Zfunction_auto_after html_style_tel

--- public/src/html_style_tel.mjs
+++ public/src/html_style_tel.mjs
@@ -1,5 +1,4 @@
 import { html_style_link_prefix } from "./html_style_link_prefix.mjs";
 export function html_style_tel(phone_number_text) {
-  let prefix = "tel";
-  return html_style_link_prefix(prefix, phone_number_text);
+  return html_style_link_prefix("tel", phone_number_text);
 }
