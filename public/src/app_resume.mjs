3cacb0823f627b9852e00074d11880b480383020
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 14:07:45 -0400

    2024-06-13T18:07:44.754Zfunction_auto_after app_resume

--- public/src/app_resume.mjs
+++ public/src/app_resume.mjs
@@ -24,7 +24,7 @@ export function app_resume() {
   app_resume_header_2(root, "Projects");
   app_resume_header_3(root, "Development Tools");
   app_resume_header_3(root, "Learn to Code");
-  app_resume_header_3(root, "Learn Cebuano");
+  app_resume_header_3(root, "Learn Cebuano Language");
   app_resume_header_3(root, "Memorizing Bible");
   app_resume_header_2(root, "Skills");
   app_resume_header_2(root, "Professional Experience");
