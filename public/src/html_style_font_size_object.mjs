e73d4560ae3b5593c0b1154bccccad2c9fbba0f5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 13:55:44 -0400

    2024-06-13T17:55:44.029Zfunction_auto_after html_style_font_size_property

--- public/src/html_style_font_size_object.mjs
+++ public/src/html_style_font_size_object.mjs
@@ -1,6 +1,8 @@
+import { html_style_font_size_property } from "./html_style_font_size_property.mjs";
 import { html_style_default_font_size_value_get } from "./html_style_default_font_size_value_get.mjs";
 export function html_style_font_size_object(default_font_size) {
   return {
-    ["font-size"]: html_style_default_font_size_value_get(default_font_size),
+    [html_style_font_size_property()]:
+      html_style_default_font_size_value_get(default_font_size),
   };
 }
---
+++ public/src/html_style_font_size_property.mjs
@@ -0,0 +1,3 @@
+export function html_style_font_size_property() {
+  return "font-size";
+}
