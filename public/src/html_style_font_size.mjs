1cddd9fc265fc584922e8c656b55670606629388
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 14:02:34 -0400

    2024-06-13T18:02:33.825Zfunction_auto_after html_style_font_size

--- public/src/html_style_font_size.mjs
+++ public/src/html_style_font_size.mjs
@@ -1,5 +1,5 @@
 import { html_style_font_size_object } from "./html_style_font_size_object.mjs";
 import { html_style } from "./html_style.mjs";
-export function html_style_font_size(begin, font_size_bigger) {
-  html_style(begin, html_style_font_size_object(font_size_bigger));
+export function html_style_font_size(c, font_size_bigger) {
+  html_style(c, html_style_font_size_object(font_size_bigger));
 }
