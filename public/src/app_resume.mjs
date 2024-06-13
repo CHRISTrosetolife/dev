e337daa48ecb9349cef3912ee06e59c9f1e3de17
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 12:51:47 -0400

    2024-06-13T16:51:46.955Zfunction_auto_after app_resume

--- public/src/app_resume.mjs
+++ public/src/app_resume.mjs
@@ -1,1 +1,4 @@
-export function app_resume() {}
+import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
+export function app_resume() {
+  let root = html_style_default_initialize();
+}
