6d2fa048884583ae2362a9f4e7a5b679d6a57f4f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 13:18:24 -0400

    2024-06-13T17:18:24.572Zfunction_auto_after html_replace_nbsp

--- public/src/html_replace_nbsp.mjs
+++ public/src/html_replace_nbsp.mjs
@@ -1,4 +1,4 @@
 import { string_replace } from "./string_replace.mjs";
-export function html_replace_nbsp(phone_number) {
-  return string_replace(phone_number, " ", "&nbsp;");
+export function html_replace_nbsp(s) {
+  s = string_replace(s, " ", "&nbsp;");
 }
