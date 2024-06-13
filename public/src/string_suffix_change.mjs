ae24a1c66fb2c183c616bada4b37cf350507b70f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 19:56:53 -0400

    2024-06-13T23:56:53.045Zfunction_auto_after string_suffix_change

--- public/src/string_suffix_change.mjs
+++ public/src/string_suffix_change.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
 export function string_suffix_change(input, suffix_old, suffix_new) {
+  assert_arguments_length(arguments, 3);
   let without = string_suffix_without(input, suffix_old);
   let output = string_combine(without, suffix_new);
   return output;
