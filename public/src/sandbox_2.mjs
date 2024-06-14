9cbaba9eb4586d3b5b1aa2fa84423786fc1f119f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 10:13:43 -0400

    2024-06-14T14:13:43.085Zfunction_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,6 +1,22 @@
-import { log } from "./log.mjs";
+import fs from "fs";
+import { PNG } from "pngjs";
 export async function sandbox_2() {
-  if (false) {
-    log("code");
-  }
+  fs.createReadStream("in.png")
+    .pipe(
+      new PNG({
+        filterType: 4,
+      }),
+    )
+    .on("parsed", function () {
+      for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+          var idx = (this.width * y + x) << 2;
+          this.data[idx] = 255 - this.data[idx];
+          this.data[idx + 1] = 255 - this.data[idx + 1];
+          this.data[idx + 2] = 255 - this.data[idx + 2];
+          this.data[idx + 3] = this.data[idx + 3] >> 1;
+        }
+      }
+      this.pack().pipe(fs.createWriteStream("out.png"));
+    });
 }
