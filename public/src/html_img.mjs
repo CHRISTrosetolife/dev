441010723036c5a4ef109b54bc04defb4434a0ee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 15:08:26 -0400

    2024-06-14T19:08:26.133Zfunction_auto_after html_img

--- public/src/html_img.mjs
+++ public/src/html_img.mjs
@@ -1,9 +1,9 @@
 import { object_property_set } from "./object_property_set.mjs";
-import { string_combine } from "./string_combine.mjs";
 import { html_element } from "./html_element.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function html_img(parent, img_name) {
   let i1 = html_element(parent, "img");
-  let img_path = string_combine("./img/", img_name);
+  let img_path = string_combine_multiple(["./img/", img_name]);
   let { element } = i1;
   object_property_set(element, "src", img_path);
   return i1;
