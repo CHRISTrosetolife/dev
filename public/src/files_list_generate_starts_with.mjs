763fcc44ad7a1c4459203d493c6b7360874d8a68
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 14:59:07 -0400

    2024-06-14T18:59:06.633Zfunction_auto_after files_list_generate_starts_with

--- public/src/files_list_generate_starts_with.mjs
+++ public/src/files_list_generate_starts_with.mjs
@@ -1,3 +1,4 @@
+import { path_parse_name } from "./path_parse_name.mjs";
 import { folder_read } from "./folder_read.mjs";
 import { generate_list_generic } from "./generate_list_generic.mjs";
 import { list_map } from "./list_map.mjs";
@@ -10,6 +11,7 @@ export async function files_list_generate_starts_with(
 ) {
   let files = await folder_read(input_path, file_extension);
   let mapped = list_map(files, (f) => {
+    path_parse_name();
     return string_starts_with(f, prefix);
   });
   await generate_list_generic(mapped, name);
