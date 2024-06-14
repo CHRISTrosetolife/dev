0f6a47aed92b8241bbed02d20c034e4dcb2da8d7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 13:49:12 -0400

    2024-06-14T17:49:12.158Zfunction_auto_after range_list

--- public/src/range_list.mjs
+++ public/src/range_list.mjs
@@ -1,5 +1,5 @@
 import { list_size } from "./list_size.mjs";
 import { range } from "./range.mjs";
-export function range_list(atom) {
-  return range(list_size(atom));
+export function range_list(list) {
+  return range(list_size(list));
 }
