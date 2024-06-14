89a87737bf2fa72c3560f9de3f676ffcf1109b0a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 16:36:53 -0400

    2024-06-14T20:36:50.094Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -123,7 +123,7 @@ export function app_gs() {
                 ),
               ),
             );
-            timeout_set(walk, 150);
+            timeout_set(walk, 175);
           }
         }
       });
