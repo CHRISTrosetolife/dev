d0f5145305d64c760e0ba157bbdc10d2fe4f9524
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 13:00:12 -0400

    2024-06-14T17:00:12.077Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -6,7 +6,7 @@ import { each_range } from "./each_range.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_style_background_color } from "./html_style_background_color.mjs";
 import { html_div } from "./html_div.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { html_style_width_full } from "./html_style_width_full.mjs";
 export function app_gs() {
   let root = html_style_default_initialize();
   html_style(root, {
@@ -15,11 +15,8 @@ export function app_gs() {
   html_style_background_color(root, "black");
   let rows = 20;
   let columns = 20;
-  let min_tiles = 10;
-  let tile_size_css = (w, h) =>
-    string_combine_multiple(["min(", w, "dvw,", h, "dvh)"]);
   let map = html_div(root);
-  html_style_width(map, tile_size_css());
+  html_style_width_full(map);
   html_style(map, {
     overflow: "hidden",
   });
@@ -27,7 +24,7 @@ export function app_gs() {
     let row = html_div(map);
     each_range(columns, (c) => {
       let image = html_img(row, game_img_base(r));
-      html_style_width(image, tile_size_css(min_tiles, min_tiles));
+      html_style_width(image, "min(10dvw,10dvh)");
     });
   });
 }
