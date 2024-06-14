cf141d0451a9bed2474398be422ac7274d24723e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 14:55:28 -0400

    2024-06-14T18:55:28.081Zfunction_auto_after generate_list_generic

--- public/src/generate_list_generic.mjs
+++ public/src/generate_list_generic.mjs
@@ -4,9 +4,8 @@ import { js_code_statement_return } from "./js_code_statement_return.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 import { string_delimit } from "./string_delimit.mjs";
 import { list_map } from "./list_map.mjs";
-import { list_get } from "./list_get.mjs";
-export async function generate_list_generic(list_get, name) {
-  let f = await list_get();
+export async function generate_list_generic(list_lambda, name) {
+  let f = await list_lambda();
   let delimited = list_map(f, string_delimit);
   await function_new_generic(
     name,
