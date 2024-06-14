73395c18e7baf6f7a5d957c60a2c7ff44b900cc2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 12:29:10 -0400

    2024-06-14T16:29:09.927Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,10 +1,10 @@
+import { html_style_width } from "./html_style_width.mjs";
 import { game_img_base } from "./game_img_base.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { html_img } from "./html_img.mjs";
 import { each_range } from "./each_range.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_style_background_color } from "./html_style_background_color.mjs";
-import { html_style_width_full } from "./html_style_width_full.mjs";
 export function app_gs() {
   let root = html_style_default_initialize();
   html_style(root, {
@@ -14,6 +14,6 @@ export function app_gs() {
   let tile_size = 32;
   each_range(30, (index) => {
     let image = html_img(root, game_img_base(index));
-    html_style_width(image,)
+    html_style_width(image);
   });
 }
