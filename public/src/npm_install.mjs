672c38b177349429a1ff33f92af5558780fa7ff9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 20:17:27 -0400

    2024-06-14T00:17:27.405Zfunction_auto_after npm_install

--- public/src/npm_install.mjs
+++ public/src/npm_install.mjs
@@ -1,5 +1,4 @@
 import { npm_install_generic } from "./npm_install_generic.mjs";
 export async function npm_install(package_name) {
-  let suffix = "";
-  return await npm_install_generic(package_name, suffix);
+  return await npm_install_generic(package_name, "");
 }
