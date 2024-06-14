ab8e39bf31fc0b9d5db598c596e7f4810465357f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 10:04:06 -0400

    2024-06-14T14:04:06.137Zfunction_auto_after files_rename_if_ends_with

--- public/src/files_rename_if_ends_with.mjs
+++ public/src/files_rename_if_ends_with.mjs
@@ -16,7 +16,7 @@ export async function files_rename_if_ends_with(
 ) {
   await files_rename_generic(input_path, file_extension, (b_old) => {
     if (!string_ends_with(b_old, suffix_old)) {
-      return;
+      return b_old;
     }
     let b_new = string_suffix_change(b_old, suffix_old, suffix_new);
     return b_new;
