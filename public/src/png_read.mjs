a81d5564e1223da13a5042077bdee1b670af3deb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 10:38:55 -0400

    2024-06-14T14:38:54.970Zfunction_auto_after png_read

---
+++ public/src/png_read.mjs
@@ -0,0 +1,14 @@
+export async function png_read(path_in) {
+  return await new Promise((resolve) => {
+    fs.createReadStream(path_in)
+      .pipe(
+        new PNG({
+          filterType: 4,
+        }),
+      )
+      .on("parsed", async function () {
+        let image = this;
+        resolve(image);
+      });
+  });
+}
--- public/src/png_transform.mjs
+++ public/src/png_transform.mjs
@@ -1,18 +1,8 @@
+import { png_read } from "./png_read.mjs";
 import { png_overwrite } from "./png_overwrite.mjs";
 import fs from "fs";
 export async function png_transform(path_in, path_out, transform) {
-  let image = await new Promise((resolve) => {
-    fs.createReadStream(path_in)
-      .pipe(
-        new PNG({
-          filterType: 4,
-        }),
-      )
-      .on("parsed", async function () {
-        let image = this;
-        resolve(image);
-      });
-  });
+  let image = await png_read(path_in);
   transform(image);
   await png_overwrite(path_out, image);
 }
