80ae62237848b652ced74af34dc6a68aae66dfbd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 20:10:24 -0400

    2024-06-14T00:10:24.316Zfunction_auto_after files_rename_map

--- public/src/files_rename_map.mjs
+++ public/src/files_rename_map.mjs
@@ -1,9 +1,9 @@
+import { path_base_change } from "./path_base_change.mjs";
 import { path_parse_base } from "./path_parse_base.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { each_async } from "./each_async.mjs";
 import { file_rename } from "./file_rename.mjs";
 import { folder_read } from "./folder_read.mjs";
-import { path_join } from "./path_join.mjs";
 import { function_import } from "./function_import.mjs";
 export async function files_rename_map(
   input_path,
@@ -19,7 +19,7 @@ export async function files_rename_map(
     if (b === b_new) {
       return;
     }
-    let file_name_new = path_join([d, b_new]);
+    let file_name_new = path_base_change(file_name_old, b_new);
     await file_rename(file_name_old, file_name_new);
   });
 }
