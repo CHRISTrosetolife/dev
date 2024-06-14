770e2863bd304757c09934e3015b3fcc802e4372
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 15:25:21 -0400

    2024-06-14T19:25:20.263Zfunction_auto_after game_img_path_pipoya

--- public/src/game_img_path_pipoya_rpg_32.mjs
+++ public/src/game_img_path_pipoya_rpg_32.mjs
@@ -1,4 +1,5 @@
+import { game_img_path_pipoya } from "./game_img_path_pipoya.mjs";
 import { string_combine } from "./string_combine.mjs";
 export function game_img_path_pipoya_rpg_32() {
-  return string_combine("pipoya\\", "rpg_32\\");
+  return string_combine(game_img_path_pipoya(), "rpg_32\\");
 }
