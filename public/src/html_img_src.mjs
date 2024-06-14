e3b2bb7b8118adb91d2fdc33bf784ec2547d2a9c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 16:19:14 -0400

    2024-06-14T20:19:14.343Zfunction_auto_after html_img_src

--- public/src/html_img_src.mjs
+++ public/src/html_img_src.mjs
@@ -1,13 +1,13 @@
 import { object_property_set } from "./object_property_set.mjs";
 import { folder_img_name } from "./folder_img_name.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-export function html_img_src(img_name, i1) {
+export function html_img_src(component, img_name) {
   let img_path = string_combine_multiple([
     "./",
     folder_img_name(),
     "/",
     img_name,
   ]);
-  let { element } = i1;
+  let { element } = component;
   object_property_set(element, "src", img_path);
 }
