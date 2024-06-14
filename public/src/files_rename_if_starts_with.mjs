923b12daffcd296016ba37e77e05bd520ce3dc47
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 10:05:04 -0400

    2024-06-14T14:05:03.855Zfunction_auto_after files_rename_if_starts_with

--- public/src/files_rename_if_starts_with.mjs
+++ public/src/files_rename_if_starts_with.mjs
@@ -1,12 +1,7 @@
 import { files_rename_generic } from "./files_rename_generic.mjs";
-import { path_base_change } from "./path_base_change.mjs";
 import { string_prefix_change } from "./string_prefix_change.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
-import { path_parse_base } from "./path_parse_base.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
-import { each_async } from "./each_async.mjs";
-import { file_rename } from "./file_rename.mjs";
-import { folder_read } from "./folder_read.mjs";
 export async function files_rename_if_starts_with(
   input_path,
   file_extension,
@@ -20,15 +15,5 @@ export async function files_rename_if_starts_with(
     }
     let b_new = string_prefix_change(b_old, prefix_old, prefix_new);
     return b_new;
-  });
-  let files = await folder_read(input_path, file_extension);
-  await each_async(files, async (file_name_old) => {
-    let b_old = path_parse_base(file_name_old);
-    if (!string_starts_with(b_old, prefix_old)) {
-      return;
-    }
-    let b_new = string_prefix_change(b_old, prefix_old, prefix_new);
-    let file_name_new = path_base_change(file_name_old, b_new);
-    await file_rename(file_name_old, file_name_new);
   });
 }
