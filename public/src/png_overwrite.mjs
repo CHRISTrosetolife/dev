2ba87220e0a408c4bc1a0643610263483e4bc914
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 10:32:09 -0400

    2024-06-14T14:32:09.126Zfunction_auto_after png_overwrite

--- public/src/png_overwrite.mjs
+++ public/src/png_overwrite.mjs
@@ -1,5 +1,5 @@
-export function png_overwrite(output_file_path, image) {
-  new Promise((resolve) => {
+export async function png_overwrite(output_file_path, image) {
+  await new Promise((resolve) => {
     image
       .pack()
       .pipe(fs.createWriteStream(output_file_path))
