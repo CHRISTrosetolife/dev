0895ed62da70a1736ae9898124dc698f9075b8a7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 11:50:49 -0400

    2024-06-14T15:50:48.870Zfunction_auto_after sandbox_2

---
+++ public/src/png_slice_tiles.mjs
@@ -0,0 +1,10 @@
+import { png_slice_generic } from "./png_slice_generic.mjs";
+import { number_is } from "./number_is.mjs";
+import { assert } from "./assert.mjs";
+import { integer_parse } from "./integer_parse.mjs";
+export async function png_slice_tiles(path_in, tile_size) {
+  tile_size = integer_parse(tile_size);
+  assert(number_is, tile_size);
+  let tile_size_use = true;
+  await png_slice_generic(path_in, -1, -1, tile_size_use, tile_size);
+}
--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,15 +1,8 @@
-import { png_slice_generic } from "./png_slice_generic.mjs";
+import { png_slice_tiles } from "./png_slice_tiles.mjs";
 export async function sandbox_2() {
   let path_in = "public\\img\\shubibubi\\animals\\";
   let rows_count = 5;
   let columns_count = 4;
   let tile_size = 32;
-  let tile_size_use = true;
-  await png_slice_generic(
-    path_in,
-    rows_count,
-    columns_count,
-    tile_size_use,
-    tile_size,
-  );
+  await png_slice_tiles(path_in, tile_size);
 }
