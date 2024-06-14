1d9086c0652e13a6e0c8baf5607d20da0c224563
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 15:05:38 -0400

    2024-06-14T19:05:38.009Zfunction_auto_after game_img_pipoya_rpg_32_lists_generate

--- public/src/game_img_pipoya_rpg_32_lists_generate.mjs
+++ public/src/game_img_pipoya_rpg_32_lists_generate.mjs
@@ -1,8 +1,9 @@
+import { game_img_path_pipoya_rpg_32 } from "./game_img_path_pipoya_rpg_32.mjs";
 import { files_list_generate_starts_with } from "./files_list_generate_starts_with.mjs";
 import { each_async } from "./each_async.mjs";
 export async function game_img_pipoya_rpg_32_lists_generate() {
   let prefixes = ["cat", "dog", "male", "female", "soldier"];
-  await each_async(prefixes, (prefix) => {
-    files_list_generate_starts_with;
+  await each_async(prefixes, async (prefix) => {
+    await files_list_generate_starts_with(game_img_path_pipoya_rpg_32());
   });
 }
