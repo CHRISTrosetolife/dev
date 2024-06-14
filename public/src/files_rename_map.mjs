ebed519503151a8e5d31519c23af057947500244
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 20:09:09 -0400

    2024-06-14T00:09:08.539Zfunction_auto_after files_rename_map

--- public/src/files_rename_map.mjs
+++ public/src/files_rename_map.mjs
@@ -1,4 +1,3 @@
-import { string_prefix_change } from "./string_prefix_change.mjs";
 import { path_parse_base } from "./path_parse_base.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { each_async } from "./each_async.mjs";
@@ -16,7 +15,7 @@ export async function files_rename_map(
   let files = await folder_read(input_path, file_extension);
   await each_async(files, async (file_name_old) => {
     let b = path_parse_base(file_name_old);
-    let b_new = string_prefix_change(b, prefix_old, prefix_new);
+    let b_new = mapper(b);
     let file_name_new = path_join([d, b_new]);
     await file_rename(file_name_old, file_name_new);
   });
