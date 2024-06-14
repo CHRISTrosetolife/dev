e3822637c8af0f24aa931d5c626fb9ea89ae39df
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 16:19:25 -0400

    2024-06-14T20:19:24.731Zfunction_auto_after html_img

--- public/src/html_img.mjs
+++ public/src/html_img.mjs
@@ -2,6 +2,6 @@ import { html_img_src } from "./html_img_src.mjs";
 import { html_element } from "./html_element.mjs";
 export function html_img(parent, img_name) {
   let component = html_element(parent, "img");
-  html_img_src(img_name, component);
+  html_img_src(component, img_name);
   return component;
 }
