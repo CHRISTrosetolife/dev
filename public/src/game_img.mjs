5b2d94e29447a155b0dfe56ae6030b2cb1cdff36
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 15:57:02 -0400

    2024-06-14T19:57:00.980Zfunction_auto_after game_img

--- public/src/game_img.mjs
+++ public/src/game_img.mjs
@@ -3,12 +3,13 @@ import { game_tile_units_css } from "./game_tile_units_css.mjs";
 import { html_style_width } from "./html_style_width.mjs";
 import { html_img } from "./html_img.mjs";
 export function game_img(map, img_url, r, c, z_index) {
-  let overlay = html_img(map, img_url);
-  html_style_width(overlay, game_tile_units_css(1));
-  html_style(overlay, {
+  let image = html_img(map, img_url);
+  html_style_width(image, game_tile_units_css(1));
+  html_style(image, {
     position: "absolute",
     "z-index": z_index,
     left: game_tile_units_css(c),
     top: game_tile_units_css(r),
   });
+  return image;
 }
