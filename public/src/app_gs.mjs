a71d22315626de8b8fbb52a4ad48843314acc23e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 16:32:01 -0400

    2024-06-14T20:32:01.303Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -107,6 +107,7 @@ export function app_gs() {
           );
           timeout_set(() => {
             if (player.walk_offset === 0) {
+              player.walk_offset = -player.walk_previous;
             } else {
             }
             html_img_src(
