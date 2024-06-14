5028458c38d03c9a4bc5b8e1108b4df78a447646
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 11:41:19 -0400

    2024-06-14T15:41:18.178Zfunction_auto_after folder_read

--- public/src/folder_read.mjs
+++ public/src/folder_read.mjs
@@ -5,6 +5,7 @@ import { identity } from "./identity.mjs";
 import { import_node } from "./import_node.mjs";
 import { assert } from "./assert.mjs";
 export async function folder_read(folder, file_extension) {
+  let recursive = true;
   let g = await import_node("glob");
   assert(string_is, [file_extension]);
   folder = string_replace(folder, "\\", "/");
