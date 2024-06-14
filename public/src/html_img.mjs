96103dbd44d5145e8e4fe787860b83fb7e1f79b0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 15:09:10 -0400

    2024-06-14T19:09:10.085Zfunction_auto_after html_img

--- public/src/html_img.mjs
+++ public/src/html_img.mjs
@@ -4,7 +4,12 @@ import { html_element } from "./html_element.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function html_img(parent, img_name) {
   let i1 = html_element(parent, "img");
-  let img_path = string_combine_multiple(["./", folder_img_name(), img_name]);
+  let img_path = string_combine_multiple([
+    "./",
+    folder_img_name(),
+    "/",
+    img_name,
+  ]);
   let { element } = i1;
   object_property_set(element, "src", img_path);
   return i1;
