3b2909c586f3cbba68420f931d277ccbb5034bb0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 12:19:40 -0400

    2024-06-14T16:19:40.277Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -3,4 +3,6 @@ import { html_img } from "./html_img.mjs";
 export function app_gs() {
   let root = html_style_default_initialize();
   html_img(root, "pipoya\\rpg_32\\sliced\\base_0.png");
+  html_img(root, "pipoya\\rpg_32\\sliced\\base_1.png");
+  html_img(root, "pipoya\\rpg_32\\sliced\\base_2.png");
 }
