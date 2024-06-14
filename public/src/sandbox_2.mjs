f977fb1f83becddebde254dcea3c5a2f12f22885
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 10:41:37 -0400

    2024-06-14T14:41:37.372Zfunction_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -3,6 +3,7 @@ import { png_read } from "./png_read.mjs";
 import { png_transform } from "./png_transform.mjs";
 export async function sandbox_2() {
   let path_in = "public\\img\\shubibubi\\animals\\bunny.png";
+  let tile_size = 32;
   let d = path_dirname(path_in);
   let i = await png_read(path_in);
   await png_transform(path_in, path_out, transform);
