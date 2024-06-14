da2360bad6942cf272630be4eb7f82eb09478edb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 11:31:56 -0400

    2024-06-14T15:31:55.817Zfunction_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -53,9 +53,9 @@ export async function sandbox_2() {
               );
             });
           });
-          image.width = width_unit;
-          image.height = height_unit;
           if (0) {
+            image.width = width_unit;
+            image.height = height_unit;
             each_index(image_out, (value, a) => {
               image[a] = value;
             });
