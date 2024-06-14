cd69ee6bf0d8d905ddf7100e1fd17b6898fdf35f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 11:52:57 -0400

    2024-06-14T15:52:56.857Zfunction_auto_after function_copy

--- public/src/function_copy.mjs
+++ public/src/function_copy.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { file_copy } from "./file_copy.mjs";
 import { function_copy_generic } from "./function_copy_generic.mjs";
 export async function function_copy(fn_name_from, fn_name_to) {
+  assert_arguments_length(arguments, 2);
   let lambda = file_copy;
   await function_copy_generic(lambda, fn_name_from, fn_name_to);
 }
