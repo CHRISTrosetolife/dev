e20257f40ea85448560d718e3f373bc98739561f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 16:25:53 -0400

    2024-06-14T20:25:52.716Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -23,6 +23,7 @@ import { each_range } from "./each_range.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_style_background_color } from "./html_style_background_color.mjs";
 import { html_div } from "./html_div.mjs";
+import { timeout_set } from "./timeout_set.mjs";
 export function app_gs() {
   let root = html_style_default_initialize();
   html_style(root, {
@@ -101,6 +102,7 @@ export function app_gs() {
               list_index(character_indices, direction),
             ),
           );
+          timeout_set(() => {}, 50);
         }
       });
       if (integer_random(1, 8) === 1) {
