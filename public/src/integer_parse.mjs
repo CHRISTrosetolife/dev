7ae38f96ecb1f50ed4085ba0a0af9ffc34c96e23
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 11:52:01 -0400

    2024-06-14T15:52:01.360Zfunction_auto_after integer_parse_try

--- public/src/integer_parse.mjs
+++ public/src/integer_parse.mjs
@@ -1,7 +1,8 @@
+import { integer_parse_try } from "./integer_parse_try.mjs";
 import { number_is } from "./number_is.mjs";
 import { assert } from "./assert.mjs";
 export function integer_parse(verse_number) {
-  let result = parseInt(verse_number, 10);
+  let result = integer_parse_try(verse_number);
   assert(number_is, result);
   return result;
 }
---
+++ public/src/integer_parse_try.mjs
@@ -0,0 +1,3 @@
+export function integer_parse_try(verse_number) {
+  return parseInt(verse_number, 10);
+}
