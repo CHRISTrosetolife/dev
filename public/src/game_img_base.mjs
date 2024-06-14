df276a09bc69aa7098e8d46fb3678620a4d637e0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 15:03:41 -0400

    2024-06-14T19:03:40.776Zfunction_auto_after game_img_base

--- public/src/game_img_base.mjs
+++ public/src/game_img_base.mjs
@@ -1,4 +1,9 @@
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function game_img_base(i) {
-  return string_combine_multiple(["pipoya\\rpg_32\\sliced\\base_", i, ".png"]);
+  return string_combine_multiple([
+    "pipoya\\rpg_32\\",
+    "sliced\\base_",
+    i,
+    ".png",
+  ]);
 }
