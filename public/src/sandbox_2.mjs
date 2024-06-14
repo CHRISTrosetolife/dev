cf96efad6b1b7ae0815cc9787cdcd248b0564f42
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 10:50:37 -0400

    2024-06-14T14:50:37.155Zfunction_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,11 +1,24 @@
+import { mod } from "./mod.mjs";
+import { equal } from "./equal.mjs";
+import { assert } from "./assert.mjs";
+import { png_read } from "./png_read.mjs";
+import { path_dirname } from "./path_dirname.mjs";
 import { png_transform } from "./png_transform.mjs";
 import { folder_read } from "./folder_read.mjs";
+import { each_async } from "./each_async.mjs";
 export async function sandbox_2() {
   let path_in = "public\\img\\shubibubi\\animals\\";
   let tile_size = 32;
   let rows_count = 5;
   let columns_count = 4;
   let files = await folder_read(path_in, ".png");
+  await each_async(files, async (file) => {
+    let d = path_dirname(file);
+    let i = await png_read(file);
+    let { height, width } = i;
+    assert(equal, [mod(width, columns_count), 0]);
+    assert(equal, [mod(height, rows_count), 0]);
+  });
   return;
   await png_transform(path_in, path_out, transform);
   function transform(image) {
