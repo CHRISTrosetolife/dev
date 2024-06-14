86e80fd5aa6df8bb91e407d7e38c3a7d05f371d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 16:02:57 -0400

    2024-06-14T20:02:53.857Zfunction_auto_after game_img_style

--- public/src/game_img_style.mjs
+++ public/src/game_img_style.mjs
@@ -5,7 +5,7 @@ import { html_style_height } from "./html_style_height.mjs";
 export function game_img_style(image, r, c, z_index) {
   let unit = game_tile_units_css(1);
   html_style_width(image, unit);
-  html_style_height;
+  html_style_height(image, unit);
   html_style(image, {
     position: "absolute",
     "z-index": z_index,
