c07d6a9fc4d8caad58877e09f12381da4c66b80e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 11:29:30 -0400

    2024-06-14T15:29:29.962Zfunction_auto_after png_overwrite

--- public/src/png_overwrite.mjs
+++ public/src/png_overwrite.mjs
@@ -1,5 +1,7 @@
+import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
 import fs from "fs";
 export async function png_overwrite(output_file_path, image) {
+  folder_parent_exists_ensure;
   await new Promise((resolve) => {
     image
       .pack()
