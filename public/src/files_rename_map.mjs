d88a1ca8d0c5182dab2c53fa360586ee21e77598
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 20:09:01 -0400

    2024-06-14T00:09:01.064Zfunction_auto_after files_rename_map

--- public/src/files_rename_map.mjs
+++ public/src/files_rename_map.mjs
@@ -1,6 +1,4 @@
 import { string_prefix_change } from "./string_prefix_change.mjs";
-import { path_dirname } from "./path_dirname.mjs";
-import { string_starts_with } from "./string_starts_with.mjs";
 import { path_parse_base } from "./path_parse_base.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { each_async } from "./each_async.mjs";
@@ -18,10 +16,6 @@ export async function files_rename_map(
   let files = await folder_read(input_path, file_extension);
   await each_async(files, async (file_name_old) => {
     let b = path_parse_base(file_name_old);
-    if (!string_starts_with(b, prefix_old)) {
-      return;
-    }
-    let d = path_dirname(file_name_old);
     let b_new = string_prefix_change(b, prefix_old, prefix_new);
     let file_name_new = path_join([d, b_new]);
     await file_rename(file_name_old, file_name_new);
