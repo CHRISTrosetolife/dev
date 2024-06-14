c3e0c3dc4a17e5d7c7b2359abf061e9bb4fa95b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 20:17:12 -0400

    2024-06-14T00:17:11.767Zfunction_auto_after npm_install

--- public/src/npm_install.mjs
+++ public/src/npm_install.mjs
@@ -3,6 +3,6 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function npm_install(package_name) {
   let suffix = "";
   return await command_line(
-    string_combine_multiple(["npm install ", package_name, "@latest"]),
+    string_combine_multiple(["npm install ", package_name, "@latest", suffix]),
   );
 }
