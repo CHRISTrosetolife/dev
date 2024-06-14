cd0003490af4f5ddf7c94edcd9b1631d9ef45c87
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 15:58:45 -0400

    2024-06-14T19:58:44.434Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,3 +1,4 @@
+import { game_img_style } from "./game_img_style.mjs";
 import { log } from "./log.mjs";
 import { html_on_click } from "./html_on_click.mjs";
 import { subtract_1 } from "./subtract_1.mjs";
@@ -47,6 +48,8 @@ export function app_gs() {
       let grass = game_grass_weight();
       let index = list_random_index_weighted(grass);
       let tile = game_img(map, game_img_base(index), r, c, 0);
+      let clicker = html_div(map);
+      game_img_style(image, r, c, z_index);
       html_on_click(tile, () => log("here"));
       if (integer_random(1, 8) === 1) {
         game_img(
