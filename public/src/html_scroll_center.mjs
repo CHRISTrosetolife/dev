fdca57e9ac684effcc91efefe7f5026435c003cb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 18:26:08 -0400

    2024-06-14T22:26:08.333Zfunction_auto_after html_scroll_center

--- public/src/html_scroll_center.mjs
+++ public/src/html_scroll_center.mjs
@@ -1,9 +1,9 @@
 export function html_scroll_center(component) {
   let { element } = component;
-    const options = {
-        behavior: "auto",
-        block: "center",
-        inline: "center",
-    };//
+  let options = {
+    behavior: "auto",
+    block: "center",
+    inline: "center",
+  };
   element.scrollIntoView(options);
 }
