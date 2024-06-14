8f7965d097fdd8c44e819397cd565688c036fe8e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 11:53:38 -0400

    2024-06-14T15:53:37.900Zfunction_auto_after png_slice_counts

--- public/src/png_slice_counts.mjs
+++ public/src/png_slice_counts.mjs
@@ -1,5 +1,6 @@
 import { png_slice_generic } from "./png_slice_generic.mjs";
 export async function png_slice_counts(path_in, rows_count, columns_count) {
-  rows_count, columns_count;
+  rows_count;
+  columns_count;
   await png_slice_generic(path_in, rows_count, columns_count, false, -1);
 }
