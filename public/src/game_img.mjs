c534e0884dd658a9ddd31d0a4f763fb798504848
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 15:58:06 -0400

    2024-06-14T19:58:05.816Zfunction_auto_after game_img

--- public/src/game_img.mjs
+++ public/src/game_img.mjs
@@ -2,6 +2,6 @@ import { game_img_style } from "./game_img_style.mjs";
 import { html_img } from "./html_img.mjs";
 export function game_img(map, img_url, r, c, z_index) {
   let image = html_img(map, img_url);
-  game_img_style(image, z_index, c, r);
+  game_img_style(image, r, c, z_index);
   return image;
 }
