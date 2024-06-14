893715361bf2991a94965fed757035ad0de9915b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 10:35:16 -0400

    2024-06-14T14:35:16.070Zfunction_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -11,16 +11,19 @@ export async function sandbox_2() {
     )
     .on("parsed", async function () {
       let image = this;
-      image.height = image.height / 4;
-      for (var y = 0; y < image.height; y++) {
-        for (var x = 0; x < image.width; x++) {
-          var idx = (image.width * y + x) << 2;
-          image.data[idx] = 255 - image.data[idx];
-          image.data[idx + 1] = 255 - image.data[idx + 1];
-          image.data[idx + 2] = 255 - image.data[idx + 2];
-          image.data[idx + 3] = image.data[idx + 3] >> 1;
-        }
-      }
+      transform(image);
       await png_overwrite(output_file_path, image);
     });
+  function transform(image) {
+    image.height = image.height / 4;
+    for (var y = 0; y < image.height; y++) {
+      for (var x = 0; x < image.width; x++) {
+        var idx = (image.width * y + x) << 2;
+        image.data[idx] = 255 - image.data[idx];
+        image.data[idx + 1] = 255 - image.data[idx + 1];
+        image.data[idx + 2] = 255 - image.data[idx + 2];
+        image.data[idx + 3] = image.data[idx + 3] >> 1;
+      }
+    }
+  }
 }
