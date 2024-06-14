09099d182c7d8a83f3bf5535af27113526a1d18b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 10:38:33 -0400

    2024-06-14T14:38:33.619Zfunction_auto_after png_transform

--- public/src/png_transform.mjs
+++ public/src/png_transform.mjs
@@ -1,17 +1,18 @@
 import { png_overwrite } from "./png_overwrite.mjs";
 import fs from "fs";
 export async function png_transform(path_in, path_out, transform) {
-  let image = await new Promise((resolve) => {});
-  fs.createReadStream(path_in)
-    .pipe(
-      new PNG({
-        filterType: 4,
-      }),
-    )
-    .on("parsed", async function () {
-      let image = this;
-      resolve(image);
-    });
+  let image = await new Promise((resolve) => {
+    fs.createReadStream(path_in)
+      .pipe(
+        new PNG({
+          filterType: 4,
+        }),
+      )
+      .on("parsed", async function () {
+        let image = this;
+        resolve(image);
+      });
+  });
   transform(image);
   await png_overwrite(path_out, image);
 }
