00a3add597f234f7dcec9627d93e9439d65f4518
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 13:49:36 -0400

    2024-06-14T17:49:35.814Zfunction_auto_after range

--- public/src/range.mjs
+++ public/src/range.mjs
@@ -2,6 +2,7 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { list_add } from "./list_add.mjs";
 export function range(count) {
   assert_arguments_length(arguments, 1);
+  let offset = 0;
   let result = [];
   for (let i = 0; i < count; i++) {
     list_add(result, i);
