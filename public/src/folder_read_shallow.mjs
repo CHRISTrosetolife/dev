cc73a0228ac62065f1a035b22c4585e5f9d78145
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 11:44:11 -0400

    2024-06-14T15:44:10.780Z  c folder_read folder_read_shallow

---
+++ public/src/folder_read_shallow.mjs
@@ -0,0 +1,6 @@
+import { folder_read_generic } from "./folder_read_generic.mjs";
+export async function folder_read_shallow(folder, file_extension) {
+  let recursive = true;
+  let files = await folder_read_generic(folder, file_extension, recursive);
+  return files;
+}
