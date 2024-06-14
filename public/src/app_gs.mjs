0f792b2740f6bbe822c181a9aefef19882d70822
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 16:50:12 -0400

    2024-06-14T20:50:11.965Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -130,9 +130,9 @@ export function app_gs() {
                 ),
               ),
             );
-            let row_offset =
+            let row_delta =
               direction === "up" ? -1 : direction === "down" ? 1 : 0;
-            let column_offset =
+            let column_delta =
               direction === "left" ? -1 : direction === "right" ? 1 : 0;
             game_img_position(
               player_overlay,
