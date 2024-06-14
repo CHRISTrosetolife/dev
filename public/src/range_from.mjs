cb6c888a4dc9f171c24ac73ace704937161a0498
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 13:51:50 -0400

    2024-06-14T17:51:50.397Zfunction_auto_after range_from

---
+++ public/src/range_from.mjs
@@ -0,0 +1,7 @@
+import { range_generic } from "./range_generic.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
+export function range_from(count) {
+  assert_arguments_length(arguments, 1);
+  let offset = 0;
+  return range_generic(count, offset);
+}
