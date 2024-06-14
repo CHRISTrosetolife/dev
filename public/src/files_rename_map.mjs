8e90b91b1ecc51ec330e85ada722f00b0da2235c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 20:08:49 -0400

    2024-06-14T00:08:48.839Zfunction_auto_after files_rename_map

--- public/src/files_rename_map.mjs
+++ public/src/files_rename_map.mjs
@@ -7,12 +7,14 @@ import { each_async } from "./each_async.mjs";
 import { file_rename } from "./file_rename.mjs";
 import { folder_read } from "./folder_read.mjs";
 import { path_join } from "./path_join.mjs";
+import { function_import } from "./function_import.mjs";
 export async function files_rename_map(
   input_path,
   file_extension,
   mapper_function_name,
 ) {
   assert_arguments_length(arguments, 4);
+  let mapper = await function_import(mapper_function_name);
   let files = await folder_read(input_path, file_extension);
   await each_async(files, async (file_name_old) => {
     let b = path_parse_base(file_name_old);
