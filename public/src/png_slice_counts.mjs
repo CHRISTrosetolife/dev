53fab03a38d44d979bbdd8b48bce520cd03bbca2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 11:53:48 -0400

    2024-06-14T15:53:48.185Zfunction_auto_after png_slice_counts

--- public/src/png_slice_counts.mjs
+++ public/src/png_slice_counts.mjs
@@ -1,6 +1,7 @@
+import { integer_parse } from "./integer_parse.mjs";
 import { png_slice_generic } from "./png_slice_generic.mjs";
 export async function png_slice_counts(path_in, rows_count, columns_count) {
-  rows_count;
-  columns_count;
+  rows_count = integer_parse(rows_count);
+  columns_count = integer_parse(columns_count);
   await png_slice_generic(path_in, rows_count, columns_count, false, -1);
 }
