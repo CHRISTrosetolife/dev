6aaeb3ec99a42ec84f6ea7fd4aa4e3410f593ca1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 14:58:39 -0400

    2024-06-14T18:58:38.659Zfunction_auto_after files_list_generate_starts_with

--- public/src/files_list_generate_starts_with.mjs
+++ public/src/files_list_generate_starts_with.mjs
@@ -1,5 +1,7 @@
 import { folder_read } from "./folder_read.mjs";
 import { generate_list_generic } from "./generate_list_generic.mjs";
+import { list_map } from "./list_map.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
 export async function files_list_generate_starts_with(
   input_path,
   file_extension,
@@ -7,5 +9,6 @@ export async function files_list_generate_starts_with(
   name,
 ) {
   let files = await folder_read(input_path, file_extension);
+  let mapped = list_map(file, (f) => string_starts_with(f, prefix));
   await generate_list_generic(name);
 }
