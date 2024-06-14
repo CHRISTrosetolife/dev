0702a0db73455d9f2adae2f4a36af5cfa020c370
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 10:05:41 -0400

    2024-06-14T14:05:41.201Zfunction_auto_after files_rename_replace

--- public/src/files_rename_replace.mjs
+++ public/src/files_rename_replace.mjs
@@ -1,3 +1,6 @@
+import { string_suffix_change } from "./string_suffix_change.mjs";
+import { string_ends_with } from "./string_ends_with.mjs";
+import { files_rename_generic } from "./files_rename_generic.mjs";
 import { path_base_change } from "./path_base_change.mjs";
 import { path_parse_base } from "./path_parse_base.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
@@ -12,6 +15,13 @@ export async function files_rename_replace(
   replace_to,
 ) {
   assert_arguments_length(arguments, 4);
+  await files_rename_generic(input_path, file_extension, (b_old) => {
+    if (!string_ends_with(b_old, suffix_old)) {
+      return b_old;
+    }
+    let b_new = string_suffix_change(b_old, suffix_old, suffix_new);
+    return b_new;
+  });
   let files = await folder_read(input_path, file_extension);
   await each_async(files, async (file_name_old) => {
     let b = path_parse_base(file_name_old);
