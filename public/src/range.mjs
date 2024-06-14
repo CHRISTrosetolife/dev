a823f91da93134b211f4bb3ed6fa623f8197f062
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 13:50:12 -0400

    2024-06-14T17:50:11.514Zfunction_auto_after range

--- public/src/range.mjs
+++ public/src/range.mjs
@@ -1,3 +1,4 @@
+import { add } from "./add.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { list_add } from "./list_add.mjs";
 export function range(count) {
@@ -5,7 +6,7 @@ export function range(count) {
   let offset = 0;
   let result = [];
   for (let i = 0; i < count; i++) {
-    list_add(result, number_add(i, offset));
+    list_add(result, add(i, offset));
   }
   return result;
 }
