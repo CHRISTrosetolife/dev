40995d06b2dec0a487545528eadcc858ee9ca0f5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 12:16:50 -0400

    2024-06-14T16:16:49.532Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,4 +1,6 @@
+import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { html_img } from "./html_img.mjs";
 export function app_gs() {
+  let root = html_style_default_initialize();
   html_img(root);
 }
