8f3f097f5fa9cea00d8cab7048494e69fd85e412
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 13:53:14 -0400

    2024-06-14T17:53:14.170Zfunction_auto_after range_from

--- public/src/range_from.mjs
+++ public/src/range_from.mjs
@@ -1,7 +1,10 @@
 import { range_generic } from "./range_generic.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { add_1 } from "./add_1.mjs";
+import { integer_parse } from "./integer_parse.mjs";
 export function range_from(from, to) {
   assert_arguments_length(arguments, 2);
+  to = integer_parse(to);
+  from = integer_parse(from);
   return range_generic(add_1(to - from), from);
 }
