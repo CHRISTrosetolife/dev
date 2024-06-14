ac41e1ef4515fe9a19fd842d1e5aa5fed3c426a7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 15:22:43 -0400

    2024-06-14T19:22:42.564Zfunction_auto_after game_img_character

--- public/src/game_img_character.mjs
+++ public/src/game_img_character.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { game_img_path_pipoya_rpg_32 } from "./game_img_path_pipoya_rpg_32.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function game_img_character(name, index) {
+  assert_arguments_length(arguments, 2);
   return string_combine_multiple([
     game_img_path_pipoya_rpg_32(),
     "sliced\\",
