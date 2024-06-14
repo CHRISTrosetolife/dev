a40675777c454efbd12b47b6481b1b492e9bd186
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 15:01:32 -0400

    2024-06-14T19:01:31.704Zfunction_auto_after files_list_generate_starts_with

--- public/src/files_list_generate_starts_with.mjs
+++ public/src/files_list_generate_starts_with.mjs
@@ -3,12 +3,14 @@ import { folder_read_shallow } from "./folder_read_shallow.mjs";
 import { path_parse_name } from "./path_parse_name.mjs";
 import { generate_list_generic } from "./generate_list_generic.mjs";
 import { list_map } from "./list_map.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function files_list_generate_starts_with(
   input_path,
   file_extension,
   prefix,
   name,
 ) {
+  assert_arguments_length(arguments, 4);
   let files = await folder_read_shallow(input_path, file_extension);
   let m = list_map(files, path_parse_name);
   let filtered = list_filter_starts_with(m, prefix);
