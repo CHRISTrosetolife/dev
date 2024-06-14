28b2a2e2e66fb8a2efeca12e7b889b48f6ed1e36
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 12:34:47 -0400

    2024-06-14T16:34:46.671Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -5,12 +5,14 @@ import { html_img } from "./html_img.mjs";
 import { each_range } from "./each_range.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_style_background_color } from "./html_style_background_color.mjs";
+import { html_div } from "./html_div.mjs";
 export function app_gs() {
   let root = html_style_default_initialize();
   html_style(root, {
     margin: 0,
   });
   html_style_background_color(root, "black");
+  let map = html_div(root);
   each_range(30, (index) => {
     let image = html_img(root, game_img_base(index));
     html_style_width(image, "min(10dvw,10dvh)");
