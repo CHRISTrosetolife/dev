d166921169d7b86694a7305202df9763e3b3dc26
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 11:43:35 -0400

    2024-06-14T15:43:35.626Zfunction_auto_after folder_read_generic

--- public/src/folder_read.mjs
+++ public/src/folder_read.mjs
@@ -1,25 +1,6 @@
-import { string_is } from "./string_is.mjs";
-import { string_replace } from "./string_replace.mjs";
-import { list_sort_string } from "./list_sort_string.mjs";
-import { identity } from "./identity.mjs";
-import { import_node } from "./import_node.mjs";
-import { assert } from "./assert.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { folder_read_generic } from "./folder_read_generic.mjs";
 export async function folder_read(folder, file_extension) {
   let recursive = true;
-  let g = await import_node("glob");
-  assert(string_is, [file_extension]);
-  folder = string_replace(folder, "\\", "/");
-  folder += "/";
-  let { glob } = g;
-  let files = await glob(
-    string_combine_multiple([
-      folder,
-      recursive ? "**" : "",
-      "/*",
-      file_extension,
-    ]),
-  );
-  list_sort_string(files, identity);
+  let files = await folder_read_generic(folder, file_extension, recursive);
   return files;
 }
---
+++ public/src/folder_read_generic.mjs
@@ -0,0 +1,24 @@
+import { identity } from "./identity.mjs";
+import { list_sort_string } from "./list_sort_string.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { string_replace } from "./string_replace.mjs";
+import { string_is } from "./string_is.mjs";
+import { assert } from "./assert.mjs";
+import { import_node } from "./import_node.mjs";
+export async function folder_read_generic(folder, file_extension, recursive) {
+  let g = await import_node("glob");
+  assert(string_is, [file_extension]);
+  folder = string_replace(folder, "\\", "/");
+  folder += "/";
+  let { glob } = g;
+  let files = await glob(
+    string_combine_multiple([
+      folder,
+      recursive ? "**" : "",
+      "/*",
+      file_extension,
+    ]),
+  );
+  list_sort_string(files, identity);
+  return files;
+}
