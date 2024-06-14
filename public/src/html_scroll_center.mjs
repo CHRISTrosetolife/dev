833caa38e6c53bf6ab690da7c837806769a0e900
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 18:26:26 -0400

    2024-06-14T22:26:23.771Zfunction_auto_after html_scroll_center

--- public/src/html_scroll_center.mjs
+++ public/src/html_scroll_center.mjs
@@ -1,9 +1,12 @@
+import { object_merge } from "./object_merge.mjs";
 export function html_scroll_center(component) {
+  let additional = {};
   let { element } = component;
   let options = {
     behavior: "auto",
     block: "center",
     inline: "center",
   };
+  object_merge(options, additional);
   element.scrollIntoView(options);
 }
