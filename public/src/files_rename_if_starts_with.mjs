d372f25c4b5334987e0733012b71c5bcad3dea26
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-13 20:01:07 -0400

    2024-06-14T00:01:06.982Z  c files_rename_if_ends_with files_rename_if_starts_with

---
+++ public/src/files_rename_if_starts_with.mjs
@@ -0,0 +1,28 @@
+import { string_suffix_change } from "./string_suffix_change.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
+import { each_async } from "./each_async.mjs";
+import { file_rename } from "./file_rename.mjs";
+import { folder_read } from "./folder_read.mjs";
+import { string_ends_with } from "./string_ends_with.mjs";
+import { string_suffix_without } from "./string_suffix_without.mjs";
+export async function files_rename_if_starts_with(
+  input_path,
+  file_extension,
+  suffix_old,
+  suffix_new,
+) {
+  assert_arguments_length(arguments, 4);
+  let files = await folder_read(input_path, file_extension);
+  await each_async(files, async (file_name_old) => {
+    file_name_old = string_suffix_without(file_name_old, file_extension);
+    if (!string_ends_with(file_name_old, suffix_old)) {
+      return;
+    }
+    let file_name_new = string_suffix_change(
+      file_name_old,
+      suffix_old,
+      suffix_new,
+    );
+    await file_rename(file_name_old, file_name_new);
+  });
+}
