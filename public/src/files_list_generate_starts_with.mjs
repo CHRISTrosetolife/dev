3448a7b32e57ad35e16a70af788ed7c5721ebc08
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 14:57:26 -0400

    2024-06-14T18:57:25.607Zfunction_auto_after files_list_generate_starts_with

--- public/src/files_list_generate_starts_with.mjs
+++ public/src/files_list_generate_starts_with.mjs
@@ -1,6 +1,5 @@
 import { generate_list_generic } from "./generate_list_generic.mjs";
-import { function_names } from "./function_names.mjs";
 export async function files_list_generate_starts_with() {
   let name = "functions_list";
-  await generate_list_generic(function_names, name);
+  await generate_list_generic(name);
 }
