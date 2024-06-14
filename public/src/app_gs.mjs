7a054c50e6319a5ff4f45f23d99b5d3fc8899877
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 12:34:01 -0400

    2024-06-14T16:34:00.538Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -13,6 +13,6 @@ export function app_gs() {
   html_style_background_color(root, "black");
   each_range(30, (index) => {
     let image = html_img(root, game_img_base(index));
-    html_style_width(image, "10dvw");
+    html_style_width(image, "ming(10dvw,10dvh)");
   });
 }
