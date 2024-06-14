4d6b932df3285883eba3e50afda51347f599453e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 17:46:37 -0400

    2024-06-14T21:46:37.016Zfunction_auto_after html_style_width

--- public/src/html_style_width.mjs
+++ public/src/html_style_width.mjs
@@ -1,6 +1,9 @@
+import { each } from "./each.mjs";
 import { html_style } from "./html_style.mjs";
 export function html_style_width(component, value) {
-  html_style(component, {
-    width: value,
-  });
+  each(["min-width", "max-width"], (w) =>
+    html_style(component, {
+      [w]: value,
+    }),
+  );
 }
