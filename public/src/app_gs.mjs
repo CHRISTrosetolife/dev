c396d0a98225d89a2668c47a62c1f77848f2229c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 12:23:24 -0400

    2024-06-14T16:23:23.870Zfunction_auto_after game_img_base

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,13 +1,8 @@
+import { game_img_base } from "./game_img_base.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { html_img } from "./html_img.mjs";
 import { each_range } from "./each_range.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_gs() {
   let root = html_style_default_initialize();
-  each_range(30, (i) =>
-    html_img(
-      root,
-      string_combine_multiple(["pipoya\\rpg_32\\sliced\\base_", i, ".png"]),
-    ),
-  );
+  each_range(30, (i) => html_img(root, game_img_base(i)));
 }
---
+++ public/src/game_img_base.mjs
@@ -0,0 +1,4 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function game_img_base(i) {
+  return string_combine_multiple(["pipoya\\rpg_32\\sliced\\base_", i, ".png"]);
+}
