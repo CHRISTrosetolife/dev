c430c3d3e6f2b49d5a79c501659efaefeebc1b7b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 13:39:10 -0400

    2024-06-14T17:39:09.813Zfunction_auto_after integer_random

--- public/src/integer_random.mjs
+++ public/src/integer_random.mjs
@@ -1,4 +1,5 @@
+import { random } from "./random.mjs";
 import { floor } from "./floor.mjs";
 export function integer_random(low, high) {
-  return floor(Math.random() * (high - low + 1) + low);
+  return floor(random() * (high - low + 1) + low);
 }
