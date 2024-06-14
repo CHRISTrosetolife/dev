9bbd52051a38f91f60cec30f8e17f9d1db51db01
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 11:53:32 -0400

    2024-06-14T15:53:32.633Zfunction_auto_after png_slice_counts

--- public/src/png_slice_counts.mjs
+++ public/src/png_slice_counts.mjs
@@ -1,7 +1,5 @@
 import { png_slice_generic } from "./png_slice_generic.mjs";
-import { integer_parse } from "./integer_parse.mjs";
-export async function png_slice_counts(path_in, tile_size) {
-  tile_size = integer_parse(tile_size);
-  let tile_size_use = true;
-  await png_slice_generic(path_in, -1, -1, tile_size_use, tile_size);
+export async function png_slice_counts(path_in, rows_count, columns_count) {
+  rows_count, columns_count;
+  await png_slice_generic(path_in, rows_count, columns_count, false, -1);
 }
