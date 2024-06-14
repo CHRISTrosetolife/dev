f782c96028b4af9d6ddfa718594bd3a684019336
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 13:21:55 -0400

    2024-06-14T17:21:54.884Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -33,7 +33,7 @@ export function app_gs() {
     let row = html_div(map);
     html_style_height(row, tile_units_css(1));
     each_range(columns, (c) => {
-      let grass = [5, 5, 1, 1];
+      let grass = [5, 5, 10, 1];
       let index = list_random_index_weighted(grass);
       let image = html_img(row, game_img_base(index));
       html_style_width(image, tile_units_css(1));
