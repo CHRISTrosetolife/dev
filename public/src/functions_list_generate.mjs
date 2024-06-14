b5215e40a0341ab8609074001cea4bbdf2cade89
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 14:55:10 -0400

    2024-06-14T18:55:09.731Zfunction_auto_after functions_list_generate

--- public/src/functions_list_generate.mjs
+++ public/src/functions_list_generate.mjs
@@ -1,8 +1,6 @@
 import { generate_list_generic } from "./generate_list_generic.mjs";
-import { list_get } from "./list_get.mjs";
 import { function_names } from "./function_names.mjs";
 export async function functions_list_generate() {
   let name = "functions_list";
-  let list_get = function_names;
-  await generate_list_generic(list_get, name);
+  await generate_list_generic(function_names, name);
 }
