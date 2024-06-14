7f6585faf84075cce84d517966b99f47eabf853f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 15:01:06 -0400

    2024-06-14T19:01:05.260Zfunction_auto_after list_filter_starts_with

--- public/src/list_filter_starts_with.mjs
+++ public/src/list_filter_starts_with.mjs
@@ -1,5 +1,5 @@
 import { string_starts_with } from "./string_starts_with.mjs";
 import { list_filter } from "./list_filter.mjs";
-export function list_filter_starts_with(mapped5, prefix_1) {
-  return list_filter(mapped5, (m) => string_starts_with(m, prefix_1));
+export function list_filter_starts_with(list, prefix) {
+  return list_filter(list, (item) => string_starts_with(item, prefix));
 }
