3e70ff726a1a5607414568e5e0fc72e1517139fe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 23:08:53 -0400

    2024-06-15T03:08:52.466Zfunction_auto_after graph_neighbors

--- public/src/graph_neighbors.mjs
+++ public/src/graph_neighbors.mjs
@@ -1,7 +1,8 @@
+import { list_without } from "./list_without.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_includes } from "./list_includes.mjs";
 import { list_filter } from "./list_filter.mjs";
 export function graph_neighbors(edges, v) {
   let filtered = list_filter(edges, (e) => list_includes(e, v));
-  list_map(filtered);
+  list_map(filtered, (f) => list_without(f, v));
 }
