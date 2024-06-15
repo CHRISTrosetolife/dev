72d45338da16aaef351b9636fc72e47292ee3787
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 19:56:27 -0400

    2024-06-15T23:56:26.538Zfunction_auto_after js_node_types

--- public/src/js_node_types.mjs
+++ public/src/js_node_types.mjs
@@ -1,3 +1,5 @@
+import { js_node_is } from "./js_node_is.mjs";
+import { assert } from "./assert.mjs";
 import { log } from "./log.mjs";
 import { undefined_not_is } from "./undefined_not_is.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
@@ -5,9 +7,7 @@ import { js_visit } from "./js_visit.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function js_node_types(ast) {
   assert_arguments_length(arguments, 1);
-  log({
-    ast,
-  });
+  assert(js_node_is, [ast]);
   return list_adder_unique((la) => {
     js_visit(ast, lambda);
     function lambda(v) {
