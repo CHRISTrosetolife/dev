3f6f205cb845e68f76dedc496d22a55829c0d916
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 16:12:14 -0400

    2024-06-15T20:12:13.642Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -16,7 +16,7 @@ export async function app_gs() {
   let tiles = list_map(map.tiles, async (tile) => {
     let b = map.player;
     let distance = abs(tile.x - b.x) + abs(tile.y + b.y);
-    if (distance <= 3) {
+    if (distance > 5) {
       return;
     }
     await app_gs_map_cell(map, map_c, player_overlay, tile);
