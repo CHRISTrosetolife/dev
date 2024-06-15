7798685779eb83a46d47e66c74c79889a051e70b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 22:51:25 -0400

    2024-06-15T02:51:24.801Zfunction_auto_after app_gs_map_cell

--- public/src/app_gs_map_cell.mjs
+++ public/src/app_gs_map_cell.mjs
@@ -1,3 +1,4 @@
+import { list_all } from "./list_all.mjs";
 import { app_gs_at_single } from "./app_gs_at_single.mjs";
 import { graph_path_shortest } from "./graph_path_shortest.mjs";
 import { app_gs_overlays_any_wall } from "./app_gs_overlays_any_wall.mjs";
@@ -28,6 +29,9 @@ export function app_gs_map_cell(map, map_c, player_overlay, tile) {
     if (w) {
       log("wall");
       return;
+    }
+    if (0) {
+      list_all([a, b], (ab) => !app_gs_overlays_any_wall(map, ab));
     }
     let from = app_gs_at_single(map.tiles, map.player);
     let p = graph_path_shortest(
