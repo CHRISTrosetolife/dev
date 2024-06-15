fecb01813c84cb9b6ffd5d67574884287a97eb72
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 15:10:09 -0400

    2024-06-15T19:10:08.845Zfunction_auto_after terminal

--- public/src/terminal.mjs
+++ public/src/terminal.mjs
@@ -27,9 +27,6 @@ export async function terminal() {
       keys: ["ctrl", "v"],
       action: async () => {
         let value = await clipboard_paste();
-        log({
-          value,
-        });
         each(string_split_empty(value), (c) => {
           keyboard_type(c);
         });
