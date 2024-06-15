1ba27992856d1469665c82923de720513a261ade
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 08:51:53 -0400

    2024-06-15T12:51:52.908Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,3 +1,4 @@
+import { promise_all } from "./promise_all.mjs";
 import { app_gs_map_cell } from "./app_gs_map_cell.mjs";
 import { app_gs_style_default_initialize } from "./app_gs_style_default_initialize.mjs";
 import { app_gs_map_html } from "./app_gs_map_html.mjs";
@@ -13,5 +14,6 @@ export function app_gs() {
   let tiles = list_map(map.tiles, async (tile) => {
     await app_gs_map_cell(map, map_c, player_overlay, tile);
   });
+  promise_all;
   html_scroll_center_smooth(player_overlay);
 }
