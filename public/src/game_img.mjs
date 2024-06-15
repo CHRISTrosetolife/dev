bf3ad03834b3e11f0df9a3b1db6df4f2540c15b4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 22:03:10 -0400

    2024-06-15T02:03:10.341Zfunction_auto_after game_img

--- public/src/game_img.mjs
+++ public/src/game_img.mjs
@@ -1,9 +1,9 @@
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { game_img_style } from "./game_img_style.mjs";
 import { html_img } from "./html_img.mjs";
-export function game_img(map, img_url, o, z_index) {
+export function game_img(map, img_url, y_x, z_index) {
   assert_arguments_length(arguments, 4);
   let image = html_img(map, img_url);
-  game_img_style(image, o.y, o.x, z_index);
+  game_img_style(image, y_x.y, y_x.x, z_index);
   return image;
 }
