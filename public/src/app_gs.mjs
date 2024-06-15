228cae06ddb3d08296ccd860f2e474694ca16341
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 20:50:51 -0400

    2024-06-15T00:50:50.735Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,26 +1,17 @@
-import { app_gs_walk } from "./app_gs_walk.mjs";
+import { app_gs_map_onclick } from "./app_gs_map_onclick.mjs";
+import { app_gs_z_indexes } from "./app_gs_z_indexes.mjs";
 import { game_character_index } from "./game_character_index.mjs";
 import { game_character_indices } from "./game_character_indices.mjs";
 import { app_gs_map_new } from "./app_gs_map_new.mjs";
-import { html_data_set } from "./html_data_set.mjs";
 import { html_scroll_center_smooth } from "./html_scroll_center_smooth.mjs";
-import { each } from "./each.mjs";
 import { list_index } from "./list_index.mjs";
-import { game_img_style } from "./game_img_style.mjs";
-import { html_on_click } from "./html_on_click.mjs";
-import { subtract_1 } from "./subtract_1.mjs";
 import { game_img } from "./game_img.mjs";
-import { divide } from "./divide.mjs";
-import { floor } from "./floor.mjs";
 import { game_img_list_male } from "./game_img_list_male.mjs";
 import { game_img_character } from "./game_img_character.mjs";
 import { game_tile_units_css } from "./game_tile_units_css.mjs";
 import { list_random_item } from "./list_random_item.mjs";
-import { game_grass_weight } from "./game_grass_weight.mjs";
-import { list_random_index_weighted } from "./list_random_index_weighted.mjs";
 import { html_style_height } from "./html_style_height.mjs";
 import { html_style_width } from "./html_style_width.mjs";
-import { game_img_base } from "./game_img_base.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { each_range } from "./each_range.mjs";
 import { html_style } from "./html_style.mjs";
@@ -59,61 +50,3 @@ export function app_gs() {
   });
   html_scroll_center_smooth(player_overlay);
 }
-function app_gs_z_indexes() {
-    return ["tile", "overlay", "player", "clicker"];
-}
-
-function app_gs_map_onclick(map, map_c, player_overlay, r, c) {
-    let z_indexes = app_gs_z_indexes();
-    let clicker = html_div(map_c);
-    each(map.overlays, (o) => {
-        let { id, x, y } = o;
-        if (x !== c) {
-            return;
-        }
-        if (y !== r) {
-            return;
-        }
-        html_data_set(clicker, "overlay", id);
-        game_img(
-            map_c,
-            game_img_base(id),
-            r,
-            c,
-            list_index(z_indexes, "overlay")
-        );
-    });
-    let grass = game_grass_weight();
-    let index = list_random_index_weighted(grass);
-    game_img(
-        map_c,
-        game_img_base(index),
-        r,
-        c,
-        list_index(z_indexes, "tile")
-    );
-    game_img_style(clicker, r, c, list_index(z_indexes, "clicker"));
-    html_on_click(clicker, async () => {
-        let direction = null;
-        if (r === map.player.y) {
-            if (c > map.player.x) {
-                direction = "right";
-            }
-            if (c < map.player.x) {
-                direction = "left";
-            }
-        }
-        if (c === map.player.x) {
-            if (r > map.player.y) {
-                direction = "down";
-            }
-            if (r < map.player.y) {
-                direction = "up";
-            }
-        }
-        if (direction !== null) {
-            await app_gs_walk(player_overlay, map.player, direction, r, c);
-        }
-    });
-}
-
