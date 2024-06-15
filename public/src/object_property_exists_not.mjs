660524487de244bbfcd568317b1b8454879ad16b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 23:06:03 -0400

    2024-06-15T03:06:02.411Zfunction_auto_after object_property_exists_not

--- public/src/object_property_exists_not.mjs
+++ public/src/object_property_exists_not.mjs
@@ -1,5 +1,7 @@
 import { object_property_exists } from "./object_property_exists.mjs";
 import { not } from "./not.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function object_property_exists_not(lookup, right) {
+  assert_arguments_length(arguments, 2);
   return not(object_property_exists(lookup, right));
 }
