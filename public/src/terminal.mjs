ceae35ac6a667f4ab15bd8c89ac430b4bddd7cd3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 10:32:13 -0400

    2024-06-15T14:32:13.102Zfunction_auto_after terminal

--- public/src/terminal.mjs
+++ public/src/terminal.mjs
@@ -1,11 +1,14 @@
+import { string_empty_not_is } from "./string_empty_not_is.mjs";
 import { string_split_space } from "./string_split_space.mjs";
 import { run_git_ac } from "./run_git_ac.mjs";
 import { run_git } from "./run_git.mjs";
 import { log } from "./log.mjs";
 import readline_sync from "readline-sync";
+import { list_filter } from "./list_filter.mjs";
 export async function terminal() {
   let input = readline_sync.question(`> `);
   let tokens = string_split_space(input);
+  tokens = list_filter(tokens, string_empty_not_is);
   log(await run_git());
   await run_git_ac();
 }
