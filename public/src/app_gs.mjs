21572540e55ad634c7766418a0fcb9cdd9095103
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 22:01:14 -0400

    2024-06-15T02:01:13.667Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,19 +1,17 @@
+import { each } from "./each.mjs";
 import { app_gs_map_cell } from "./app_gs_map_cell.mjs";
 import { app_gs_style_default_initialize } from "./app_gs_style_default_initialize.mjs";
 import { app_gs_map_html } from "./app_gs_map_html.mjs";
 import { app_gs_overlay_player } from "./app_gs_overlay_player.mjs";
 import { app_gs_map_new } from "./app_gs_map_new.mjs";
 import { html_scroll_center_smooth } from "./html_scroll_center_smooth.mjs";
-import { each_range } from "./each_range.mjs";
 export function app_gs() {
   let root = app_gs_style_default_initialize();
   let map = app_gs_map_new();
   let map_c = app_gs_map_html(root, map);
   let player_overlay = app_gs_overlay_player(map_c, map);
-  each_range(map.rows, (r) => {
-    each_range(map.columns, (c) =>
-      app_gs_map_cell(map, map_c, player_overlay, r, c),
-    );
+  each(map.tiles, (tile) => {
+    app_gs_map_cell(map, map_c, player_overlay, tile);
   });
   html_scroll_center_smooth(player_overlay);
 }
