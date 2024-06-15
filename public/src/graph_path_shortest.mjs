5f53c4904e54ffa95668a40a5f211bf15dbf221c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 22:20:34 -0400

    2024-06-15T02:20:34.078Zfunction_auto_after graph_path_shortest

--- public/src/graph_path_shortest.mjs
+++ public/src/graph_path_shortest.mjs
@@ -1,5 +1,7 @@
-import { each } from "./each.mjs";
+import { each_index } from "./each_index.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function graph_path_shortest(vertices, edge_lambda) {
-  list_adder((la) => each(vertices, (a) => each(vertices, (b) => {})));
+  list_adder((la) =>
+    each_index(vertices, (a, ai) => each_index(vertices, (b, bi) => {})),
+  );
 }
