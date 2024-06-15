835b776f28a2fb6ef483656aa53d5e0033899a39
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 20:41:28 -0400

    2024-06-15T00:41:27.993Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,13 +1,7 @@
-import { list_pop } from "./list_pop.mjs";
-import { list_shuffle } from "./list_shuffle.mjs";
-import { list_adder } from "./list_adder.mjs";
-import { list_add } from "./list_add.mjs";
-import { ceiling } from "./ceiling.mjs";
+import { map_new } from "./map_new.mjs";
 import { html_data_set } from "./html_data_set.mjs";
 import { html_scroll_center_smooth } from "./html_scroll_center_smooth.mjs";
 import { abs } from "./abs.mjs";
-import { number_max } from "./number_max.mjs";
-import { game_tiles_min } from "./game_tiles_min.mjs";
 import { html_img_src_wait } from "./html_img_src_wait.mjs";
 import { app_gs_sleep_time } from "./app_gs_sleep_time.mjs";
 import { each_range_async } from "./each_range_async.mjs";
@@ -25,8 +19,6 @@ import { floor } from "./floor.mjs";
 import { game_img_list_male } from "./game_img_list_male.mjs";
 import { game_img_character } from "./game_img_character.mjs";
 import { game_tile_units_css } from "./game_tile_units_css.mjs";
-import { list_concat } from "./list_concat.mjs";
-import { range_from } from "./range_from.mjs";
 import { list_random_item } from "./list_random_item.mjs";
 import { game_grass_weight } from "./game_grass_weight.mjs";
 import { list_random_index_weighted } from "./list_random_index_weighted.mjs";
@@ -41,48 +33,16 @@ import { html_div } from "./html_div.mjs";
 import { sleep } from "./sleep.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { object_merge } from "./object_merge.mjs";
 export function app_gs() {
   let root = html_style_default_initialize();
   html_style(root, {
     margin: 0,
   });
   html_style_background_color(root, "black");
-  let tiles_min = game_tiles_min();
-  let rows = number_max(20, tiles_min * 2);
-  let columns = rows;
-  let map_overlays = [];
-  let map = {
-    overlays: map_overlays,
-    rows,
-    columns,
-  };
-  let total = rows * columns;
-  let map_overlays_count = ceiling(total / 8);
-  let overlays = list_concat(range_from(40, 42), range_from(48, 57));
-  let tiles = list_adder((la) =>
-    each_range(rows, (y) =>
-      each_range(columns, (x) =>
-        la({
-          y,
-          x,
-        }),
-      ),
-    ),
-  );
-  list_shuffle(tiles);
-  each_range(map_overlays_count, (i) => {
-    let t = list_pop(tiles);
-    let id = list_random_item(overlays);
-    let o = {
-      id,
-    };
-    object_merge(o, t);
-    list_add(map_overlays, o);
-  });
+  let map = map_new();
   let player = {};
-  player.y = floor(divide(subtract_1(rows), 2));
-  player.x = floor(divide(subtract_1(columns), 2));
+  player.y = floor(divide(subtract_1(map.rows), 2));
+  player.x = floor(divide(subtract_1(map.columns), 2));
   player.walk_offset = 0;
   player.walk_previous = 1;
   let character_indices = {
@@ -92,8 +52,8 @@ export function app_gs() {
     up: 10,
   };
   let map_c = html_div(root);
-  html_style_width(map_c, game_tile_units_css(columns));
-  html_style_height(map_c, game_tile_units_css(rows));
+  html_style_width(map_c, game_tile_units_css(map.columns));
+  html_style_height(map_c, game_tile_units_css(map.rows));
   html_style(map_c, {
     overflow: "hidden",
     position: "relative",
@@ -111,11 +71,11 @@ export function app_gs() {
     player.x,
     list_index(z_indexes, "player"),
   );
-  each_range(rows, (r) => {
+  each_range(map.rows, (r) => {
     html_style_height(map_c, game_tile_units_css(1));
-    each_range(columns, (c) => {
+    each_range(map.columns, (c) => {
       let clicker = html_div(map_c);
-      each(map_overlays, (o) => {
+      each(map.overlays, (o) => {
         let { id, x, y } = o;
         if (x !== c) {
           return;
