7a71dd9037ca078631f5fd6f52608fd3384fe547
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 20:44:36 -0400

    2024-06-15T00:44:36.276Zfunction_auto_after game_character_index

--- public/src/game_character_index.mjs
+++ public/src/game_character_index.mjs
@@ -1,5 +1,7 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { game_character_indices } from "./game_character_indices.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function game_character_index(direction) {
+  assert_arguments_length(arguments, 1);
   return object_property_get(game_character_indices(), direction);
 }
