09dedc57403e2c7f547356b601181e425689fb9c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 22:55:38 -0400

    2024-06-15T02:55:36.969Zfunction_auto_after app_gs_adjacent

--- public/src/app_gs_adjacent.mjs
+++ public/src/app_gs_adjacent.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { abs } from "./abs.mjs";
 import { list_any } from "./list_any.mjs";
 export function app_gs_adjacent(a, b) {
@@ -11,10 +10,6 @@ export function app_gs_adjacent(a, b) {
       let [i, j] = both;
       let d1 = abs(a[i] - b[i]);
       let d2 = abs(a[j] - b[j]);
-      log({
-        d1,
-        d2,
-      });
       return d1 === 1 && d2 === 0;
     },
   );
