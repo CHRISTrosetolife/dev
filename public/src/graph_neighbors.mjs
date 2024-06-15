09a3a54d1e80393f95069779f0eb45b331c487d4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 23:08:33 -0400

    2024-06-15T03:08:33.038Zfunction_auto_after graph_neighbors

--- public/src/graph_neighbors.mjs
+++ public/src/graph_neighbors.mjs
@@ -2,6 +2,6 @@ import { list_map } from "./list_map.mjs";
 import { list_includes } from "./list_includes.mjs";
 import { list_filter } from "./list_filter.mjs";
 export function graph_neighbors(edges, v) {
-  let f = list_filter(edges, (e) => list_includes(e, v));
-  list_map(f);
+  let filtered = list_filter(edges, (e) => list_includes(e, v));
+  list_map(filtered);
 }
