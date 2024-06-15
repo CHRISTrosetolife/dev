62b54503c04ccde49f882e5a10ea2f01ae2e94e4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 08:50:46 -0400

    2024-06-15T12:50:46.425Zfunction_auto_after html_img_src_wait

--- public/src/html_img_src_wait.mjs
+++ public/src/html_img_src_wait.mjs
@@ -1,6 +1,6 @@
 import { html_on } from "./html_on.mjs";
 import { html_img_src } from "./html_img_src.mjs";
-export async function html_img_src_wait(player_overlay, img_url) {
-  html_img_src(player_overlay, img_url);
-  await new Promise((resolve) => html_on(player_overlay, "load", resolve));
+export async function html_img_src_wait(img, img_url) {
+  html_img_src(img, img_url);
+  await new Promise((resolve) => html_on(img, "load", resolve));
 }
