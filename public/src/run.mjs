3fcc76c3768f3dc40045cf230529aeaece861ecd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 10:30:55 -0400

    2024-06-15T14:30:54.884Zfunction_auto_after run

--- public/src/run.mjs
+++ public/src/run.mjs
@@ -2,5 +2,5 @@ import { run_tokens } from "./run_tokens.mjs";
 import { process_argv_remaining } from "./process_argv_remaining.mjs";
 export async function run() {
   let r = process_argv_remaining();
-  return run_tokens(r);
+  return await run_tokens(r);
 }
