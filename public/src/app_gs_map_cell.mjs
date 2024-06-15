7ee30824bf6fcdc230bcc967736cc3bbd8f99dfe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 21:02:01 -0400

    2024-06-15T01:02:00.654Zfunction_auto_after app_gs_map_cell

--- public/src/app_gs_map_cell.mjs
+++ public/src/app_gs_map_cell.mjs
@@ -1,3 +1,4 @@
+import { list_adder } from "./list_adder.mjs";
 import { app_gs_overlays_at } from "./app_gs_overlays_at.mjs";
 import { app_gs_walk } from "./app_gs_walk.mjs";
 import { html_on_click } from "./html_on_click.mjs";
@@ -10,7 +11,6 @@ import { game_img } from "./game_img.mjs";
 import { html_data_set } from "./html_data_set.mjs";
 import { html_div } from "./html_div.mjs";
 import { app_gs_z_indexes } from "./app_gs_z_indexes.mjs";
-import { log } from "./log.mjs";
 export function app_gs_map_cell(map, map_c, player_overlay, r, c) {
   let z_indexes = app_gs_z_indexes();
   let clicker = html_div(map_c);
@@ -20,12 +20,7 @@ export function app_gs_map_cell(map, map_c, player_overlay, r, c) {
   game_img(map_c, game_img_base(index), r, c, list_index(z_indexes, "tile"));
   game_img_style(clicker, r, c, list_index(z_indexes, "clicker"));
   html_on_click(clicker, async () => {
-    app_gs_overlays_at(map, r, c, (o) => {
-      let { id } = o;
-      log({
-        id,
-      });
-    });
+    list_adder((la) => app_gs_overlays_at(map, r, c, la));
     let direction = null;
     if (r === map.player.y) {
       if (c > map.player.x) {
