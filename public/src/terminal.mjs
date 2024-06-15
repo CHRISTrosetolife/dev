dc4272cf1a981ea105995f2a9bbcd80e73dfbbbb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 10:22:42 -0400

    2024-06-15T14:22:41.786Zfunction_auto_after terminal

--- public/src/terminal.mjs
+++ public/src/terminal.mjs
@@ -4,7 +4,6 @@ export function terminal() {
   let rl = readline.createInterface({
     input: process.stdin,
   });
-  return;
   rl.question(`What's your name?`, (name) => {
     log(`Hi ${name}!`);
     rl.close();
