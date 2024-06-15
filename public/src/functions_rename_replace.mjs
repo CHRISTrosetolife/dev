d13f5c0ab91e654d788f72c5cabafecaa8d21350
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 20:24:06 -0400

    2024-06-15T00:24:06.488Zfunction_auto_after functions_rename_replace

--- public/src/functions_rename_replace.mjs
+++ public/src/functions_rename_replace.mjs
@@ -1,12 +1,15 @@
 import { functions_rename_generic } from "./functions_rename_generic.mjs";
-import { string_prefix_change } from "./string_prefix_change.mjs";
-import { string_starts_with } from "./string_starts_with.mjs";
-export async function functions_rename_replace(prefix_before, prefix_after) {
+import { string_includes } from "./string_includes.mjs";
+import { string_replace } from "./string_replace.mjs";
+export async function functions_rename_replace(
+  replacement_before,
+  replacement_after,
+) {
   await functions_rename_generic(fn_new_get);
   function fn_new_get(fn) {
     let fn_new = fn;
-    if (string_starts_with(fn, prefix_before)) {
-      fn_new = string_prefix_change(fn, prefix_before, prefix_after);
+    if (string_includes(fn, replacement_before)) {
+      fn_new = string_replace(fn, replacement_before, replacement_after);
     }
     return fn_new;
   }
