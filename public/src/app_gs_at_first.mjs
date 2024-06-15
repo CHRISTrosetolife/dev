d5105c5d003c9b0bdbb451231a3c93ee7bcf6975
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 22:28:10 -0400

    2024-06-15T02:28:09.894Zfunction_auto_after app_gs_at_first

--- public/src/app_gs_at_first.mjs
+++ public/src/app_gs_at_first.mjs
@@ -1,8 +1,8 @@
-import { list_first } from "./list_first.mjs";
+import { list_single } from "./list_single.mjs";
 import { app_gs_at_generic } from "./app_gs_at_generic.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function app_gs_at_first(b, a) {
   let ts = list_adder((la) => app_gs_at_generic(b, a, la));
-  let t = list_first(t);
+  let t = list_single(ts);
   return t;
 }
