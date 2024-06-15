e7d39d23a608d35158aca51d3a23c123f90d9096
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 23:09:19 -0400

    2024-06-15T03:09:19.461Zfunction_auto_after graph_neighbors

--- public/src/graph_neighbors.mjs
+++ public/src/graph_neighbors.mjs
@@ -2,7 +2,9 @@ import { list_without } from "./list_without.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_includes } from "./list_includes.mjs";
 import { list_filter } from "./list_filter.mjs";
+import { list_single } from "./list_single.mjs";
 export function graph_neighbors(edges, v) {
   let filtered = list_filter(edges, (e) => list_includes(e, v));
   let mapped = list_map(filtered, (f) => list_without(f, v));
+  let mapped2 = list_map(mapped, list_single);
 }
