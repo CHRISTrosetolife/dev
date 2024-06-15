8c24b572799703bc3819e2c04f2703a44ab7e524
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 16:34:14 -0400

    2024-06-15T20:34:14.426Zfunction_auto_after app_index

--- public/src/app_index.mjs
+++ public/src/app_index.mjs
@@ -1,8 +1,13 @@
+import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 import { window_open } from "./window_open.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_index() {
   let root = html_style_default_initialize();
+  html_clear_scroll_top_centered(root);
+  html_button_width_full_text_click(root, "learn", () => {
+    window_open("gs.html");
+  });
   html_button_width_full_text_click(root, "game", () => {
     window_open("gs.html");
   });
