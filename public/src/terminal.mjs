701761cc67fb51aac915411f9de6d103ffb4f8bd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 10:29:59 -0400

    2024-06-15T14:29:58.656Zfunction_auto_after terminal

--- public/src/terminal.mjs
+++ public/src/terminal.mjs
@@ -1,6 +1,7 @@
+import { run_git } from "./run_git.mjs";
 import { log } from "./log.mjs";
 import readline_sync from "readline-sync";
 export function terminal() {
   let name = readline_sync.question(`> `);
-  log(`Hi ${name}!`);
+  log(run_git);
 }
