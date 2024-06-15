b9f142111463cb46c74a93cf1865cc75098b547d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 20:25:17 -0400

    2024-06-15T00:25:16.764Z  noop

--- public/src/functions_rename_generic.mjs
+++ public/src/functions_rename_generic.mjs
@@ -4,7 +4,7 @@ import { function_names } from "./function_names.mjs";
 export async function functions_rename_generic(fn_new_get) {
   let fns = await function_names();
   await each_async(fns, async (fn) => {
-    let fn_new = fn_new_get(fn);
+    let fn_new = await fn_new_get(fn);
     if (fn_new !== fn) {
       await function_rename(fn, fn_new);
     }
