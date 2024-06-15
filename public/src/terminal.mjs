2ea895371334346d9c26a4887a462c82abf1600c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 10:31:52 -0400

    2024-06-15T14:31:51.194Zfunction_auto_after terminal

--- public/src/terminal.mjs
+++ public/src/terminal.mjs
@@ -3,7 +3,7 @@ import { run_git } from "./run_git.mjs";
 import { log } from "./log.mjs";
 import readline_sync from "readline-sync";
 export async function terminal() {
-  let name = readline_sync.question(`> `);
+  let input = readline_sync.question(`> `);
   log(await run_git());
   await run_git_ac();
 }
