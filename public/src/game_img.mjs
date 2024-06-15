ea7ba16fb432cae3e7b20862c9a7628f416923f5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 22:02:43 -0400

    2024-06-15T02:02:43.382Zfunction_auto_after game_img

--- public/src/game_img.mjs
+++ public/src/game_img.mjs
@@ -1,7 +1,7 @@
 import { game_img_style } from "./game_img_style.mjs";
 import { html_img } from "./html_img.mjs";
-export function game_img(map, img_url, r, c, z_index) {
+export function game_img(map, img_url, o, z_index) {
   let image = html_img(map, img_url);
-  game_img_style(image, r, c, z_index);
+  game_img_style(image, o.y, o.x, z_index);
   return image;
 }
