1272374ed25b57f40a3f639360433f842c4443fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 22:59:41 -0400

    2024-06-15T02:59:40.518Zfunction_auto_after graph_path_shortest

--- public/src/graph_path_shortest.mjs
+++ public/src/graph_path_shortest.mjs
@@ -21,11 +21,15 @@ export function graph_path_shortest(vertices, edge_lambda, from, to) {
       }),
     ),
   );
+  let previous = null;
   let remaining = [from];
   let visited = [];
   while (list_empty_not_is(remaining)) {
     let current = list_remove_first(remaining);
-    list_add(visited, current);
+    list_add(visited, {
+      current,
+      previous,
+    });
     la(current);
     if (current === to) {
       break;
