2164594c5e7bb3c79ad42584c2872c44a4d0222b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 22:39:05 -0400

    2024-06-15T02:39:02.671Zfunction_auto_after app_gs_adjacent

--- public/src/app_gs_adjacent.mjs
+++ public/src/app_gs_adjacent.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { abs } from "./abs.mjs";
 import { list_any } from "./list_any.mjs";
 export function app_gs_adjacent(a, b) {
@@ -8,9 +7,7 @@ export function app_gs_adjacent(a, b) {
       ["y", "x"],
     ],
     (both) => {
-      log({
-        both,
-      })[(i, j)] = both;
+      [i, j] = both;
       abs(a[i] - b[i]) === 1 && abs(a[j] - b[j]) === 0;
     },
   );
