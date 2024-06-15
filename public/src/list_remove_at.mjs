ec1201f86adec850201be158442491be34cf36f2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 22:35:32 -0400

    2024-06-15T02:35:31.756Zfunction_auto_after list_remove_at

--- public/src/list_remove_at.mjs
+++ public/src/list_remove_at.mjs
@@ -1,4 +1,4 @@
 import { list_remove_multiple_at } from "./list_remove_multiple_at.mjs";
 export function list_remove_at(list, index) {
-  list_remove_multiple_at(list, index, 1);
+  return list_remove_multiple_at(list, index, 1);
 }
