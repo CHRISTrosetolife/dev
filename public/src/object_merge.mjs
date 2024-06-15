79b045afabd530c278ac899c93a80b4ea6375893
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 08:48:43 -0400

    2024-06-15T12:48:43.298Zfunction_auto_after object_merge

--- public/src/object_merge.mjs
+++ public/src/object_merge.mjs
@@ -2,6 +2,6 @@ import { object_merge_properties } from "./object_merge_properties.mjs";
 import { object_properties } from "./object_properties.mjs";
 export function object_merge(to, from) {
   let from_properties = object_properties(from);
-  object_merge_properties(from, to, from_properties);
+  object_merge_properties(to, from, from_properties);
   return to;
 }
