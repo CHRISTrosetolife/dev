a327ffd147dcd14b18824a38017ac7ab34699169
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 16:52:48 -0400

    2024-06-15T20:52:48.460Zfunction_auto_after git_acp

--- public/src/git_acp.mjs
+++ public/src/git_acp.mjs
@@ -1,7 +1,7 @@
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { git_acp_message } from "./git_acp_message.mjs";
 export async function git_acp() {
-  assert_arguments_length(arguments, 1);
+  assert_arguments_length(arguments, 0);
   let commit_message = "";
   await git_acp_message(commit_message);
 }
