d966c307213eec9373016437ab6454300d2ff6da
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 22:49:57 -0400

    2024-06-15T02:49:56.121Zfunction_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,8 +1,13 @@
-import { png_slice_tiles } from "./png_slice_tiles.mjs";
+import { app_gs_adjacent } from "./app_gs_adjacent.mjs";
 export async function sandbox_2() {
-  let path_in = "public\\img\\shubibubi\\animals\\";
-  let rows_count = 5;
-  let columns_count = 4;
-  let tile_size = 32;
-  await png_slice_tiles(path_in, tile_size);
+  return app_gs_adjacent(
+    {
+      x: 0,
+      y: 0,
+    },
+    {
+      x: 1,
+      y: 2,
+    },
+  );
 }
