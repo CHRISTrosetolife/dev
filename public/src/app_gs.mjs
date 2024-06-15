3a832da263b4dc0485f9e434972d5f5df1706ff3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 20:53:32 -0400

    2024-06-15T00:53:32.484Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -4,8 +4,6 @@ import { app_gs_overlay_player } from "./app_gs_overlay_player.mjs";
 import { app_gs_map_onclick } from "./app_gs_map_onclick.mjs";
 import { app_gs_map_new } from "./app_gs_map_new.mjs";
 import { html_scroll_center_smooth } from "./html_scroll_center_smooth.mjs";
-import { game_tile_units_css } from "./game_tile_units_css.mjs";
-import { html_style_height } from "./html_style_height.mjs";
 import { each_range } from "./each_range.mjs";
 export function app_gs() {
   let root = app_gs_style_default_initialize();
@@ -13,7 +11,6 @@ export function app_gs() {
   let map_c = app_gs_map_html(root, map);
   let player_overlay = app_gs_overlay_player(map_c, map);
   each_range(map.rows, (r) => {
-    html_style_height(map_c, game_tile_units_css(1));
     each_range(map.columns, (c) =>
       app_gs_map_onclick(map, map_c, player_overlay, r, c),
     );
