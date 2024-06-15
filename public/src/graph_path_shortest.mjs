6f76e63345c72e06d6264984f5d6f5ddf38148d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 22:30:50 -0400

    2024-06-15T02:30:50.374Zfunction_auto_after graph_path_shortest

--- public/src/graph_path_shortest.mjs
+++ public/src/graph_path_shortest.mjs
@@ -2,6 +2,7 @@ import { list_filter } from "./list_filter.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { each_index } from "./each_index.mjs";
 import { list_adder } from "./list_adder.mjs";
+import { list_includes } from "./list_includes.mjs";
 export function graph_path_shortest(vertices, edge_lambda, from, to) {
   assert_arguments_length(arguments, 4);
   let edges = list_adder((la) =>
@@ -18,5 +19,5 @@ export function graph_path_shortest(vertices, edge_lambda, from, to) {
   );
   let current = from;
   let visited = [];
-  list_filter(edges);
+  list_filter(edges, (e) => list_includes(e, current));
 }
