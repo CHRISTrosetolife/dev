2d1abdc8cd85f2d512ccd7180a572945c80cbd15
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 10:34:05 -0400

    2024-06-15T14:34:05.051Zfunction_auto_after terminal

--- public/src/terminal.mjs
+++ public/src/terminal.mjs
@@ -7,10 +7,12 @@ import { log } from "./log.mjs";
 import readline_sync from "readline-sync";
 import { list_filter } from "./list_filter.mjs";
 export async function terminal() {
-  let input = readline_sync.question(`> `);
-  let tokens = string_split_space(input);
-  tokens = list_filter(tokens, string_empty_not_is);
-  let result = await run_tokens(tokens);
-  log(result);
+  while (true) {
+    let input = readline_sync.question(`> `);
+    let tokens = string_split_space(input);
+    tokens = list_filter(tokens, string_empty_not_is);
+    let result = await run_tokens(tokens);
+    log(result);
+  }
   each([run_git_ac], (f) => f());
 }
