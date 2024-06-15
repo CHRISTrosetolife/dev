9c90be19121eafe560fecedd5bdecdbc029bd009
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 10:29:38 -0400

    2024-06-15T14:29:37.880Zfunction_auto_after terminal

--- public/src/terminal.mjs
+++ public/src/terminal.mjs
@@ -1,6 +1,6 @@
 import { log } from "./log.mjs";
 import readline_sync from "readline-sync";
 export function terminal() {
-  let name = readline_sync.question(`What's your name?`);
+  let name = readline_sync.question(`> `);
   log(`Hi ${name}!`);
 }
