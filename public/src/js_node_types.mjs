3811bac6c3c0566b8da681ba1fa0dcfdf24d96b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 19:53:58 -0400

    2024-06-15T23:53:57.616Zfunction_auto_after js_node_types

--- public/src/js_node_types.mjs
+++ public/src/js_node_types.mjs
@@ -1,7 +1,9 @@
 import { undefined_not_is } from "./undefined_not_is.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 import { js_visit } from "./js_visit.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function js_node_types(ast) {
+  assert_arguments_length(arguments, 1);
   return list_adder_unique((la) => {
     js_visit(ast, lambda);
     function lambda(v) {
