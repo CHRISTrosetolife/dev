efcdbfdcf33dba7591836b22ba12a9503caa8c4b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 10:21:58 -0400

    2024-06-15T14:21:58.166Zfunction_auto_after terminal

--- public/src/terminal.mjs
+++ public/src/terminal.mjs
@@ -3,7 +3,6 @@ import readline from "readline";
 export function terminal() {
   let rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout,
   });
   rl.question(`What's your name?`, (name) => {
     log(`Hi ${name}!`);
