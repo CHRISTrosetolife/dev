e3d0fc98923f4313d239fd58be2c54665f5040ed
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 20:22:08 -0400

    2024-06-15T00:22:07.589Zfunction_auto_after functions_rename_if_starts_with

--- public/src/functions_rename_if_starts_with.mjs
+++ public/src/functions_rename_if_starts_with.mjs
@@ -1,19 +1,11 @@
+import { functions_rename_generic } from "./functions_rename_generic.mjs";
 import { string_prefix_change } from "./string_prefix_change.mjs";
-import { each_async } from "./each_async.mjs";
-import { function_names } from "./function_names.mjs";
-import { function_rename } from "./function_rename.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 export async function functions_rename_if_starts_with(
   prefix_before,
   prefix_after,
 ) {
-  let fns = await function_names();
-  await each_async(fns, async (fn) => {
-    let fn_new = fn_new_get(fn);
-    if (fn_new !== fn) {
-      await function_rename(fn, fn_new);
-    }
-  });
+  await functions_rename_generic(fn_new_get);
   function fn_new_get(fn) {
     let fn_new = fn;
     if (string_starts_with(fn, prefix_before)) {
