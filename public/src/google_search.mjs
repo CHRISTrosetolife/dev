015170fb26a441aa358753cc85e85f5e43c9e8a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 19:49:18 -0400

    2024-06-15T23:49:18.209Zfunction_auto_after google_search

--- public/src/google_search.mjs
+++ public/src/google_search.mjs
@@ -1,13 +1,11 @@
-import { log } from "./log.mjs";
+import { string_to_url } from "./string_to_url.mjs";
 import { url_secure_w3 } from "./url_secure_w3.mjs";
 import { browser_open } from "./browser_open.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function google_search(query) {
   assert_arguments_length(arguments, 1);
-  log({
-    query,
-  });
+  query = string_to_url(query);
   await browser_open(
     string_combine_multiple([url_secure_w3(), "google.com/search?q=", query]),
   );
