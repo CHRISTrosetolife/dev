a17579051459a3cbf828c2c3311ed279d0f3cb3b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 16:34:27 -0400

    2024-06-15T20:34:27.323Zfunction_auto_after app_index

--- public/src/app_index.mjs
+++ public/src/app_index.mjs
@@ -5,7 +5,7 @@ import { html_style_default_initialize } from "./html_style_default_initialize.m
 export function app_index() {
   let root = html_style_default_initialize();
   html_clear_scroll_top_centered(root);
-  html_button_width_full_text_click(root, "learn", () => {
+  html_button_width_full_text_click(root, "learn code", () => {
     window_open("learn_code.html");
   });
   html_button_width_full_text_click(root, "game", () => {
