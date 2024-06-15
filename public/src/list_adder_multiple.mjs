ac5fd4a57653dc6a9dfc9b5c9546e38f76c77263
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 09:06:59 -0400

    2024-06-15T13:06:59.480Zfunction_auto_after list_adder_multiple

--- public/src/list_adder_multiple.mjs
+++ public/src/list_adder_multiple.mjs
@@ -1,6 +1,8 @@
 import { list_add } from "./list_add.mjs";
+import { list_map } from "./list_map.mjs";
+import { range } from "./range.mjs";
 export function list_adder_multiple(count, lambda) {
-  let result = [];
+  let result = list_map(range(count), (i) => []);
   lambda((item) => list_add(result, item));
   return result;
 }
