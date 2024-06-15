c59b44c34926f5025f062a6c401c30645abea44c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 19:52:08 -0400

    2024-06-15T23:52:08.295Zfunction_auto_after terminal

--- public/src/terminal.mjs
+++ public/src/terminal.mjs
@@ -77,7 +77,7 @@ export async function terminal() {
       action: (key) => keyboard_type(key.name),
     },
     {
-      keys: string_split_empty("_'"),
+      keys: string_split_empty("_',"),
       action: (key) => keyboard_type(key.sequence),
     },
   ];
