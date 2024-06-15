284c27b1ece7f0ec4c25cf19d1f1d297349d8b7e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 16:34:46 -0400

    2024-06-15T20:34:45.743Zfunction_auto_after app_index

--- public/src/app_index.mjs
+++ public/src/app_index.mjs
@@ -5,10 +5,10 @@ import { html_style_default_initialize } from "./html_style_default_initialize.m
 export function app_index() {
   let root = html_style_default_initialize();
   html_clear_scroll_top_centered(root);
-  html_button_width_full_text_click(root, "learn code", () => {
+  html_button_width_full_text_click(root, "learn code javascript", () => {
     window_open("learn_code.html");
   });
-  html_button_width_full_text_click(root, "learn cebuano language", () => {
+  html_button_width_full_text_click(root, "learn language cebuano", () => {
     window_open("learn_code.html");
   });
   html_button_width_full_text_click(root, "game", () => {
