6fa98e57b13059a9948511038cfe8387aeedffb7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 16:33:38 -0400

    2024-06-15T20:33:37.845Zfunction_auto_after app_index

--- public/src/app_index.mjs
+++ public/src/app_index.mjs
@@ -1,8 +1,9 @@
+import { window_open } from "./window_open.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_index() {
   let root = html_style_default_initialize();
   html_button_width_full_text_click(root, "game", () => {
-    quizzes_start([2]);
+    window_open();
   });
 }
