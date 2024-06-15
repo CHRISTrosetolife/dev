8c387fee08a9781c33a30d71da297d37ec0fee5e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 16:51:23 -0400

    2024-06-15T20:51:22.556Zfunction_auto_after google_search

--- public/src/google_search.mjs
+++ public/src/google_search.mjs
@@ -1,4 +1,8 @@
+import { url_secure_w3 } from "./url_secure_w3.mjs";
 import { browser_open } from "./browser_open.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function google_search(query) {
-  await browser_open("https://www.google.com/search?q=");
+  await browser_open(
+    string_combine_multiple([url_secure_w3(), "google.com/search?q="]),
+  );
 }
