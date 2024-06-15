74a9094e32ffd341d0554c68cd089e2918555cb8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 10:14:48 -0400

    2024-06-15T14:14:47.249Zfunction_auto_after terminal

--- public/src/terminal.mjs
+++ public/src/terminal.mjs
@@ -1,1 +1,12 @@
-export function terminal() {}
+import { log } from "./log.mjs";
+let readline = require("node:readline");
+export function terminal() {
+  let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question(`What's your name?`, (name) => {
+    console.log(`Hi ${name}!`);
+    rl.close();
+  });
+}
