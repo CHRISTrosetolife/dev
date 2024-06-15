32c6e1b94b69f1309dda9059fbd0f1b961312d7b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 16:32:30 -0400

    2024-06-15T20:32:29.749Zfunction_auto_after app_index

--- public/src/app_index.mjs
+++ public/src/app_index.mjs
@@ -1,1 +1,4 @@
-export function app_index() {}
+import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
+export function app_index() {
+  let root = html_style_default_initialize();
+}
