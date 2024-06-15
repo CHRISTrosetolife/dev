f484ed1aa4a7c04af410b7d622fc2135ee0b8327
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 16:03:36 -0400

    2024-06-15T20:03:36.210Zfunction_auto_after game_tile_units_css

--- public/src/game_tile_units_css.mjs
+++ public/src/game_tile_units_css.mjs
@@ -1,7 +1,6 @@
-import { game_tiles_min } from "./game_tiles_min.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function game_tile_units_css(tiles_count) {
-  let tiles_min = game_tiles_min();
-  let v = (100 / tiles_min) * tiles_count;
-  return string_combine_multiple(["min(", v, "dvw,", v, "dvh)"]);
+  let tiles_max = 17;
+  let v = (100 / tiles_max) * tiles_count;
+  return string_combine_multiple(["max(", v, "dvw,", v, "dvh)"]);
 }
