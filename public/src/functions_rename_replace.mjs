3a74c0bd4904f9b7eb2dd6aa532bea2818b58c52
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-14 20:24:15 -0400

    2024-06-15T00:24:14.763Zfunction_auto_after functions_rename_replace

--- public/src/functions_rename_replace.mjs
+++ public/src/functions_rename_replace.mjs
@@ -1,3 +1,4 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { functions_rename_generic } from "./functions_rename_generic.mjs";
 import { string_includes } from "./string_includes.mjs";
 import { string_replace } from "./string_replace.mjs";
@@ -5,6 +6,7 @@ export async function functions_rename_replace(
   replacement_before,
   replacement_after,
 ) {
+  assert_arguments_length(arguments, 2);
   await functions_rename_generic(fn_new_get);
   function fn_new_get(fn) {
     let fn_new = fn;
