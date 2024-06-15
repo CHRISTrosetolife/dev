5328b74bd3307757ecfd7eea815dd0751dd521d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 10:45:11 -0400

    2024-06-15T14:45:10.458Zfunction_auto_after terminal

--- public/src/terminal.mjs
+++ public/src/terminal.mjs
@@ -9,7 +9,7 @@ import chalk from "chalk";
 import { list_filter } from "./list_filter.mjs";
 export async function terminal() {
   while (true) {
-    let input = readline_sync.question(chalk.blueBright(`♱✟♱ `));
+    let input = readline_sync.question(chalk.blueBright(`✝ `));
     let tokens = string_split_space(input);
     tokens = list_filter(tokens, string_empty_not_is);
     try {
