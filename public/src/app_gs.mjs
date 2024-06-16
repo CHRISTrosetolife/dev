2af65ed7acf3c19d9b16b0c2c250cb83e8a08a40
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 14:23:26 -0400

    2024-06-16T18:23:25.991Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,3 +1,4 @@
+import { noop } from "./noop.mjs";
 import { html_scroll_center_smooth } from "./html_scroll_center_smooth.mjs";
 import { html_style } from "./html_style.mjs";
 import { app_gs_map_render } from "./app_gs_map_render.mjs";
@@ -5,8 +6,10 @@ import { app_gs_style_default_initialize } from "./app_gs_style_default_initiali
 import { app_gs_map_html } from "./app_gs_map_html.mjs";
 import { app_gs_overlay_player } from "./app_gs_overlay_player.mjs";
 import { app_gs_map_new } from "./app_gs_map_new.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export async function app_gs() {
   let root = app_gs_style_default_initialize();
+  html_button_width_full_text_click(root, "menu", noop);
   html_style(root, {
     overflow: "hidden",
   });
