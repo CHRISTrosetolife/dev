c143c2af448fdef4e852fcd102e471d00c45f223
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 11:54:38 -0400

    2024-06-16T15:54:38.219Zfunction_auto_after app_gs_map_render

--- public/src/app_gs_map_render.mjs
+++ public/src/app_gs_map_render.mjs
@@ -1,4 +1,3 @@
-import { list_concat_multiple } from "./list_concat_multiple.mjs";
 import { range_from } from "./range_from.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
 import { list_remove_all } from "./list_remove_all.mjs";
@@ -9,7 +8,6 @@ import { floor } from "./floor.mjs";
 import { ceiling } from "./ceiling.mjs";
 import { number_max } from "./number_max.mjs";
 import { game_tiles_max } from "./game_tiles_max.mjs";
-import { promise_all } from "./promise_all.mjs";
 import { each_async } from "./each_async.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 export async function app_gs_map_render(
@@ -26,28 +24,21 @@ export async function app_gs_map_render(
   let h_tiles = ceiling(h / tile_size_px);
   let w_extend = floor(w_tiles / 2);
   let h_extend = floor(h_tiles / 2);
-  let rendered = {};
   let tiles_new = {};
   await each_async(coordinates, async (c) => {
     let { x, y } = c;
     await each_async(range_from(y - h_extend, y + h_extend), async (ye) => {
-      let rows = object_property_initialize(rendered, ye, {});
+      let rows = object_property_initialize(tiles_new, ye, {});
       await each_async(range_from(x - w_extend, x + w_extend), async (xe) => {
         if (rows[xe]) {
           return;
         }
         let tile = map.tiles[ye][xe];
         let component = await app_gs_map_cell(map, map_c, player_overlay, tile);
-        la({
-          tile,
-          component,
-        });
-        rows[xe] = true;
+        tiles_new[xe] = component;
       });
     });
   });
-  tiles_new = await promise_all(tiles_new);
-  tiles_new = list_concat_multiple(tiles_new);
   each(map.html, html_remove);
   list_remove_all(map.html);
   list_add_multiple(map.html, tiles_new);
