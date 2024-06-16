5f113bb32569844d84bfdba2969e926dc89a162e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 10:56:47 -0400

    2024-06-16T14:56:46.722Zfunction_auto_after js_assert_arguments_length

--- public/src/js_assert_arguments_length.mjs
+++ public/src/js_assert_arguments_length.mjs
@@ -4,10 +4,10 @@ import { list_first } from "./list_first.mjs";
 export function js_assert_arguments_length(ast) {
   let d = js_declaration_single(ast);
   let { body: body_d } = d;
-  if (list_empty_is(body_d)) {
+  let { body: body_block } = body_d;
+  if (list_empty_is(body_block)) {
     return;
   }
-  let { body: body_block } = body_d;
   let first = list_first(body_block);
   return first;
 }
