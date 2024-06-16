4bb2ecdea1454a5a030729dd66a0af415b955cc4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 10:00:19 -0400

    2024-06-16T14:00:18.829Zfunction_auto_after app_gs_overlays_at_list

--- public/src/app_gs_overlays_at_list.mjs
+++ public/src/app_gs_overlays_at_list.mjs
@@ -1,5 +1,5 @@
 import { app_gs_overlays_at } from "./app_gs_overlays_at.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function app_gs_overlays_at_list(map, tile) {
-  return list_adder((la) => app_gs_overlays_at(map, tile, la));
+  return list_adder((la) => app_gs_overlays_at(tile, la));
 }
