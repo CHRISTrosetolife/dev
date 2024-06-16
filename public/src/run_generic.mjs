cfd276bf0c1015a8280027081e4897c1464e30d4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 14:38:49 -0400

    2024-06-16T18:38:48.366Zfunction_auto_after run_generic

--- public/src/run_generic.mjs
+++ public/src/run_generic.mjs
@@ -1,6 +1,6 @@
 import { log } from "./log.mjs";
 import { undefined_not_is } from "./undefined_not_is.mjs";
-export async function run_generic() {
+export async function run_generic(fn) {
   Error.stackTraceLimit = Infinity;
   let result = await fn();
   if (undefined_not_is(result)) {
