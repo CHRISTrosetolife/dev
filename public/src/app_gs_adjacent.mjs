602452a42494a3912746b785ac0fcd321397429a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 20:19:11 -0400

    2024-06-16T00:19:10.618Zfunction_auto_after app_gs_adjacent

--- public/src/app_gs_adjacent.mjs
+++ public/src/app_gs_adjacent.mjs
@@ -1,24 +1,12 @@
+import { list_yx } from "./list_yx.mjs";
+import { list_xy } from "./list_xy.mjs";
 import { abs } from "./abs.mjs";
 import { list_any } from "./list_any.mjs";
 export function app_gs_adjacent(a, b) {
-  return list_any(
-    [
-      list_xy(),
-      list_yx(),
-    ],
-    (both) => {
-      let [i, j] = both;
-      let d1 = abs(a[i] - b[i]);
-      let d2 = abs(a[j] - b[j]);
-      return d1 === 1 && d2 === 0;
-    },
-  );
-}
-
-function list_yx() {
-    return ["y", "x"];
-}
-
-function list_xy() {
-    return ["x", "y"];
+  return list_any([list_xy(), list_yx()], (both) => {
+    let [i, j] = both;
+    let d1 = abs(a[i] - b[i]);
+    let d2 = abs(a[j] - b[j]);
+    return d1 === 1 && d2 === 0;
+  });
 }
---
+++ public/src/list_xy.mjs
@@ -0,0 +1,3 @@
+export function list_xy() {
+  return ["x", "y"];
+}
