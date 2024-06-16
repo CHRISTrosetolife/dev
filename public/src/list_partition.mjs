1943df87a013067f8efb2d9f3369c38c250837c9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 09:41:17 -0400

    2024-06-16T13:41:16.725Zfunction_auto_after list_partition

--- public/src/list_partition.mjs
+++ public/src/list_partition.mjs
@@ -1,8 +1,8 @@
 import { each } from "./each.mjs";
 import { list_adder_multiple } from "./list_adder_multiple.mjs";
-export function list_partition(map, predicate) {
+export function list_partition(list, predicate) {
   return list_adder_multiple(2, (la) => {
-    each(map.tiles, (t) => {
+    each(list, (t) => {
       if (predicate(t)) {
         la(0, t);
       } else {
