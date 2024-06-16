bd42ae9d2ecaa9fce8cc846d0383435419795fe8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 14:38:29 -0400

    2024-06-16T18:38:27.221Z  terminal

--- run_git.mjs
+++ run_git.mjs
@@ -1,5 +1,9 @@
 import { run_git } from "./public/src/run_git.mjs";
 import { log } from "./public/src/log.mjs";
+import { undefined_not_is } from "./public/src/undefined_not_is.mjs";
+let fn = run_git;
 Error.stackTraceLimit = Infinity;
-let result = await run_git();
-log(result);
+let result = await fn();
+if (undefined_not_is(result)) {
+  log(result);
+}
