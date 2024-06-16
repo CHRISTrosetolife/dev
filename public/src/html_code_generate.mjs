1e282cf979c55be782f93a8f87b4868b31c94f93
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 18:53:01 -0400

    2024-06-16T22:53:00.990Zfunction_auto_after html_code_generate

--- public/src/html_code_generate.mjs
+++ public/src/html_code_generate.mjs
@@ -1,6 +1,7 @@
 import { log } from "./log.mjs";
 import { html_parse } from "./html_parse.mjs";
 import { list_size } from "./list_size.mjs";
+import { list_single } from "./list_single.mjs";
 export function html_code_generate(input) {
   '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />';
   log({
@@ -8,5 +9,6 @@ export function html_code_generate(input) {
   });
   let p = html_parse(input);
   let { children } = p;
+  let c = list_single(children);
   return list_size(children);
 }
