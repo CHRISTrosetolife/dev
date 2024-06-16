08d833ea1ba298b9eb65198223875a1b23d66869
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 11:03:15 -0400

    2024-06-16T15:03:14.772Zfunction_auto_after js_assert_arguments_length

--- public/src/js_assert_arguments_length.mjs
+++ public/src/js_assert_arguments_length.mjs
@@ -1,5 +1,6 @@
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
+import { js_parse_expression } from "./js_parse_expression.mjs";
 import { list_empty_is } from "./list_empty_is.mjs";
 import { list_first } from "./list_first.mjs";
 export function js_assert_arguments_length(ast) {
@@ -29,5 +30,5 @@ export function js_assert_arguments_length(ast) {
   if (name !== assert_arguments_length.name) {
     return;
   }
-  expression.arguments = [];
+  expression.arguments = [js_parse_expression("arguments")];
 }
