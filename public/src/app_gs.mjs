9954ba17ed0addcd79da17f023b9911555ffbd11
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 21:11:48 -0400

    2024-06-16T01:11:48.467Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -24,7 +24,7 @@ export async function app_gs() {
   let h = window.innerHeight;
   let tile_size_px = number_max(h, w) / limit;
   let w_tiles = ceiling(w / tile_size_px);
-  let h_tiles = ceiling(w / tile_size_px);
+  let h_tiles = ceiling(h / tile_size_px);
   let tiles = list_map(map.tiles, async (tile) => {
     let b = map.player;
     let visible = list_all(list_xy(), (xy) => abs(tile[xy] - b[xy]) <= limit);
