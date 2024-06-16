8a67c9c89fbbc38c9358f5cd4dc72e845643bfcd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 20:04:27 -0400

    2024-06-16T00:04:27.118Z  terminal

--- public/src/js_if_to_block.mjs
+++
@@ -1,9 +0,0 @@
-import { js_to_block_generic } from "./js_to_block_generic.mjs";
-import { js_visit_node } from "./js_visit_node.mjs";
-export function js_if_to_block(ast) {
-  return js_visit_node(ast, "IfStatement", (v) => {
-    let { node } = v;
-    let properties = ["consequent", "alternate"];
-    js_to_block_generic(node, properties);
-  });
-}
