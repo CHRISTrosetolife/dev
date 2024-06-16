d3a94b3a9d14049c95267158e3b4d957ea5a1c34
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 18:27:47 -0400

    2024-06-16T22:27:46.959Zfunction_auto_after function_import

--- public/src/function_import.mjs
+++ public/src/function_import.mjs
@@ -1,8 +1,10 @@
+import { function_unalias } from "./function_unalias.mjs";
 import { folder_current_prefix } from "./folder_current_prefix.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { function_name_to_file_name } from "./function_name_to_file_name.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function function_import(function_name) {
+  function_name = function_unalias(function_name);
   let function_file_name = function_name_to_file_name(function_name);
   let function_file_name_path = string_combine(
     folder_current_prefix(),
