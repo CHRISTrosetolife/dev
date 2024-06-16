64f37655d04d17be4900989fd703ab867e6cedee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 14:30:25 -0400

    2024-06-16T18:30:24.752Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -11,9 +11,8 @@ import { html_button_width_full_text_click } from "./html_button_width_full_text
 export async function app_gs() {
   let root = app_gs_style_default_initialize();
   html_button_width_full_text_click(root, "menu", noop);
-  let h = html_document_head()(
-    '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />',
-  );
+  let h = html_document_head();
+  ('<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />');
   html_style(root, {
     overflow: "hidden",
   });
