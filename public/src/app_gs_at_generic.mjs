cdf72f636c8294fa262576c1d2b432ba55f869ff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 09:32:32 -0400

    2024-06-16T13:32:31.956Zfunction_auto_after app_gs_at_generic

--- public/src/app_gs_at_generic.mjs
+++ public/src/app_gs_at_generic.mjs
@@ -1,14 +1,4 @@
-import { each } from "./each.mjs";
-export function app_gs_at_generic(list, a, lambda_overlay) {
-  each(list, (b) => {
-    let { x } = b;
-    if (x !== a.x) {
-      return;
-    }
-    let { y } = b;
-    if (y !== a.y) {
-      return;
-    }
-    lambda_overlay(b);
-  });
+export function app_gs_at_generic(grid, a, lambda_overlay) {
+  grid[a.y][a.x];
+  lambda_overlay(b);
 }
