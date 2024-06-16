e4d0df0a79f65a7a11653c7116c9d7675c0dd6ab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 14:38:35 -0400

    2024-06-16T18:38:34.746Zfunction_auto_after run_generic

--- public/src/run_generic.mjs
+++ public/src/run_generic.mjs
@@ -1,1 +1,9 @@
-export function run_generic() {}
+import { log } from "./log.mjs";
+import { undefined_not_is } from "./undefined_not_is.mjs";
+export function run_generic() {
+  Error.stackTraceLimit = Infinity;
+  let result = fn();
+  if (undefined_not_is(result)) {
+    log(result);
+  }
+}
