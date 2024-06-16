f8257854348e67836e025548f61c2cf18a1fc027
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 10:27:17 -0400

    2024-06-16T14:27:17.200Zfunction_auto_after terminal

--- public/src/terminal.mjs
+++ public/src/terminal.mjs
@@ -57,7 +57,6 @@ export async function terminal() {
     {
       keys: ["backspace"],
       action: (key) => {
-        return;
         let b = buffer_get();
         list_pop(b);
         log_clear();
