b3bfbaa3e1ef9e54c8ebf9ec3df1f1a058c4b564
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 10:44:08 -0400

    2024-06-16T14:44:07.623Zfunction_auto_after assert_arguments_length

--- public/src/assert_arguments_length.mjs
+++ public/src/assert_arguments_length.mjs
@@ -1,9 +1,10 @@
 import { equal } from "./equal.mjs";
 import { assert } from "./assert.mjs";
 import { assert_message } from "./assert_message.mjs";
-export function assert_arguments_length(args, length) {
+export function assert_arguments_length(args, expected) {
   assert(equal, [arguments.length, 2]);
-  assert_message(equal, [args.length, length], () => ({
+  let actual = args.length;
+  assert_message(equal, [actual, expected], () => ({
     message: "expecting different argument count",
   }));
 }
