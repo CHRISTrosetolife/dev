6c8933bb4278f0a84e062d6577ad388834e8df0e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 10:02:28 -0400

    2024-06-16T14:02:27.670Zfunction_auto_after app_gs_map_cell

--- public/src/app_gs_map_cell.mjs
+++ public/src/app_gs_map_cell.mjs
@@ -1,3 +1,4 @@
+import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { app_gs_map_render } from "./app_gs_map_render.mjs";
 import { html_img_wait } from "./html_img_wait.mjs";
@@ -8,7 +9,6 @@ import { app_gs_at_single } from "./app_gs_at_single.mjs";
 import { graph_path_shortest } from "./graph_path_shortest.mjs";
 import { app_gs_overlays_any_wall } from "./app_gs_overlays_any_wall.mjs";
 import { app_gs_adjacent } from "./app_gs_adjacent.mjs";
-import { app_gs_overlays_at } from "./app_gs_overlays_at.mjs";
 import { app_gs_walk } from "./app_gs_walk.mjs";
 import { html_on_click } from "./html_on_click.mjs";
 import { game_img_style } from "./game_img_style.mjs";
@@ -25,7 +25,7 @@ export async function app_gs_map_cell(map, map_c, player_overlay, tile) {
   let z_indexes = app_gs_z_indexes();
   let clicker = html_div(map_c);
   let overlays = list_adder((la) =>
-    app_gs_overlays_at(tile, function lambda_overlay(o) {
+    each(tile.overlays, function lambda_overlay(o) {
       let { id: o_id } = o;
       html_data_set(clicker, "overlay", o_id);
       let i = game_img(
