a32648186c4464114e300704c0a3fd1cce527a55
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 22:18:17 -0400

    2024-06-16T02:18:15.790Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,5 +1,3 @@
-import { html_style_visible } from "./html_style_visible.mjs";
-import { html_style_hidden } from "./html_style_hidden.mjs";
 import { html_scroll_center } from "./html_scroll_center.mjs";
 import { app_gs_map_render } from "./app_gs_map_render.mjs";
 import { sleep } from "./sleep.mjs";
@@ -10,7 +8,6 @@ import { app_gs_overlay_player } from "./app_gs_overlay_player.mjs";
 import { app_gs_map_new } from "./app_gs_map_new.mjs";
 export async function app_gs() {
   let root = app_gs_style_default_initialize();
-  html_style_hidden(root);
   let map = app_gs_map_new();
   let map_c = app_gs_map_html(root, map);
   let player_overlay = app_gs_overlay_player(map_c, map);
@@ -23,6 +20,5 @@ export async function app_gs() {
   );
   await promise_all(tiles_new);
   await sleep(10);
-  html_style_visible(root);
   html_scroll_center(player_overlay);
 }
