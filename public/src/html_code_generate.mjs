8699209eff5e4510b837c9b6ccad776a9c8adc79
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 18:55:27 -0400

    2024-06-16T22:55:27.141Zfunction_auto_after html_code_generate

--- public/src/html_code_generate.mjs
+++ public/src/html_code_generate.mjs
@@ -1,7 +1,7 @@
 import { log } from "./log.mjs";
 import { html_parse } from "./html_parse.mjs";
 import { list_single } from "./list_single.mjs";
-import { json_to } from "./json_to.mjs";
+import { object_properties } from "./object_properties.mjs";
 export function html_code_generate(input) {
   '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />';
   log({
@@ -11,5 +11,5 @@ export function html_code_generate(input) {
   let { children } = p;
   let c = list_single(children);
   let { attribs } = c;
-  return json_to(attribs);
+  return object_properties(c);
 }
