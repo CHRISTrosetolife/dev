669caded44f790b117c38d04255fab96724cbf79
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 15:13:26 -0400

    2024-06-16T19:13:26.284Zfunction_auto_after on_keypress_log

--- public/src/on_keypress_log.mjs
+++ public/src/on_keypress_log.mjs
@@ -1,10 +1,12 @@
+import { sleep } from "./sleep.mjs";
 import { log } from "./log.mjs";
 import { on_keypress } from "./on_keypress.mjs";
-export function on_keypress_log() {
+export async function on_keypress_log() {
   on_keypress((chunk, key) =>
     log({
       chunk,
       key,
     }),
   );
+  await sleep(100000);
 }
