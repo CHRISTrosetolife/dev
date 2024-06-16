e55e822b4168584fbc28a05cdbded60bdbea6503
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 09:40:26 -0400

    2024-06-16T13:40:25.703Zfunction_auto_after app_gs_map_render

--- public/src/app_gs_map_render.mjs
+++ public/src/app_gs_map_render.mjs
@@ -53,7 +53,7 @@ export async function app_gs_map_render(
             );
             let visible = d1 <= w_extend && d2 <= h_extend;
             if (visible) {
-              return await app_gs_map_cell(map, map_c, player_overlay, tile);
+              let c = await app_gs_map_cell(map, map_c, player_overlay, tile);
             }
           }),
       ),
