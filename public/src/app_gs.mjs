d10440872fe3e0c1399e033edaf0dbaa3651606b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 21:05:54 -0400

    2024-06-16T01:05:54.017Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,3 +1,4 @@
+import { game_tile_units_css } from "./game_tile_units_css.mjs";
 import { list_xy } from "./list_xy.mjs";
 import { list_all } from "./list_all.mjs";
 import { game_tiles_max } from "./game_tiles_max.mjs";
@@ -19,6 +20,7 @@ export async function app_gs() {
   let player_overlay = app_gs_overlay_player(map_c, map);
   let limit = ceiling(game_tiles_max());
   let existing = [];
+  game_tile_units_css;
   let tiles = list_map(map.tiles, async (tile) => {
     let b = map.player;
     let visible = list_all(list_xy(), (xy) => abs(tile[xy] - b[xy]) <= limit);
