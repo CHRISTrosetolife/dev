28cb9616034daa7c7c85997705b8ad1ea6bd104f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 10:00:23 -0400

    2024-06-16T14:00:23.155Zfunction_auto_after app_gs_map_cell

--- public/src/app_gs_map_cell.mjs
+++ public/src/app_gs_map_cell.mjs
@@ -25,7 +25,7 @@ export async function app_gs_map_cell(map, map_c, player_overlay, tile) {
   let z_indexes = app_gs_z_indexes();
   let clicker = html_div(map_c);
   let overlays = list_adder((la) =>
-    app_gs_overlays_at(map, tile, function lambda_overlay(o) {
+    app_gs_overlays_at(tile, function lambda_overlay(o) {
       let { id: o_id } = o;
       html_data_set(clicker, "overlay", o_id);
       let i = game_img(
