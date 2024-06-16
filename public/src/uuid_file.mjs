8cf0f901c4e394080e5e5b3d928d41be042138fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 18:18:52 -0400

    2024-06-16T22:18:51.561Zfunction_auto_after uuid_file

--- public/src/uuid_file.mjs
+++ public/src/uuid_file.mjs
@@ -1,9 +1,12 @@
-import { file_delete } from "./file_delete.mjs";
+import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
 import { file_name_json } from "./file_name_json.mjs";
 import { uuid } from "./uuid.mjs";
 export async function uuid_file(lambda) {
   let u = await uuid();
   let file_path = file_name_json(u);
-  await lambda(file_path);
-  await file_delete(file_path);
+  try {
+    await lambda(file_path);
+  } finally {
+    await file_delete_if_exists(file_path);
+  }
 }
