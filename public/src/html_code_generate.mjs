53ad80b5d800616fe8524dca18c98249b1250e08
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 18:51:12 -0400

    2024-06-16T22:51:11.515Zfunction_auto_after html_code_generate

--- public/src/html_code_generate.mjs
+++ public/src/html_code_generate.mjs
@@ -1,6 +1,7 @@
 import { log } from "./log.mjs";
 import { html_parse } from "./html_parse.mjs";
 export function html_code_generate(input) {
+  '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />';
   log({
     input,
   });
