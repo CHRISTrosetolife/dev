06f62cba58b33ce8734edfdcf3fd4a91f8dda5aa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 10:04:36 -0400

    2024-06-16T14:04:35.989Z  terminal

--- public/src/app_gs_overlays_at_list.mjs
+++
@@ -1,5 +0,0 @@
-import { each } from "./each.mjs";
-import { list_adder } from "./list_adder.mjs";
-export function app_gs_overlays_at_list(tile) {
-  return list_adder((la) => each(tile.overlays, la));
-}
