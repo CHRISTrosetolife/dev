64e4b7f8271b3b4dabc54378ca2d43869d1a2f78
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 10:51:23 -0400

    2024-06-16T14:51:22.906Zfunction_auto_after js_assert_arguments_length

--- public/src/js_assert_arguments_length.mjs
+++ public/src/js_assert_arguments_length.mjs
@@ -1,1 +1,4 @@
-export function js_assert_arguments_length(ast) {}
+import { js_declaration_single } from "./js_declaration_single.mjs";
+export function js_assert_arguments_length(ast) {
+  let d = js_declaration_single(ast);
+}
