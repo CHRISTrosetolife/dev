64b3cba99a4d135df8cb9516378c99f53526de7c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 10:42:55 -0400

    2024-06-16T14:42:55.035Zfunction_auto_after function_auto

--- public/src/function_auto.mjs
+++ public/src/function_auto.mjs
@@ -1,4 +1,6 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { function_auto_return } from "./function_auto_return.mjs";
 export async function function_auto(function_name) {
+  assert_arguments_length(arguments, 1);
   await function_auto_return(function_name);
 }
