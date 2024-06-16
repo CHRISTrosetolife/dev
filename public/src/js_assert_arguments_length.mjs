fe25f879468e1f92e4c6423edf97018c9e254eb2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 10:59:47 -0400

    2024-06-16T14:59:46.885Zfunction_auto_after js_assert_arguments_length

--- public/src/js_assert_arguments_length.mjs
+++ public/src/js_assert_arguments_length.mjs
@@ -20,6 +20,6 @@ export function js_assert_arguments_length(ast) {
   if (type_expression !== "CallExpression") {
     return;
   }
-  let callee = expression;
+  let { callee } = expression;
   return first;
 }
