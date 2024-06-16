678e8566fe121f0b282e4381b6eafe644d997a6c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 10:28:59 -0400

    2024-06-16T14:28:59.232Zfunction_auto_after terminal

--- public/src/terminal.mjs
+++ public/src/terminal.mjs
@@ -61,7 +61,7 @@ export async function terminal() {
         list_pop(b);
         log_clear();
         log_write(prompt_get());
-        each(b, log_write);
+        each(b, keyboard_write);
       },
     },
     {
