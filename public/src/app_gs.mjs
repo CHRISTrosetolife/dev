e74ecbdb65fc6cf45899e6edc3b92bf53e011f39
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 21:20:14 -0400

    2024-06-16T01:20:14.402Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -12,6 +12,7 @@ import { app_gs_overlay_player } from "./app_gs_overlay_player.mjs";
 import { app_gs_map_new } from "./app_gs_map_new.mjs";
 import { html_scroll_center_smooth } from "./html_scroll_center_smooth.mjs";
 import { list_map } from "./list_map.mjs";
+import { each } from "./each.mjs";
 export async function app_gs() {
   let root = app_gs_style_default_initialize();
   let map = app_gs_map_new();
@@ -34,6 +35,7 @@ export async function app_gs() {
       await app_gs_map_cell(map, map_c, player_overlay, tile);
     }
   });
+  each(existing, (t) => {});
   await promise_all(tiles);
   await sleep(0);
   html_scroll_center_smooth(player_overlay);
