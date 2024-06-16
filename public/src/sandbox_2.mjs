ee464ddab1965c6ee37b46293dce96b21a2abe25
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 11:08:21 -0400

    2024-06-16T15:08:21.359Z  terminal

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,4 +1,4 @@
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function sandbox_2() {
-  assert_arguments_length();
+  assert_arguments_length(arguments, 0);
 }
