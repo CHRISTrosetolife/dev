554008094027090abe68f777d3199b7f483d10d1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 20:04:13 -0400

    2024-06-16T00:04:12.784Zfunction_auto_after function_transform_args_return

--- public/src/function_transform_args_return.mjs
+++ public/src/function_transform_args_return.mjs
@@ -1,3 +1,4 @@
+import { null_is } from "./null_is.mjs";
 import { function_transform_args_split } from "./function_transform_args_split.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 export async function function_transform_args_return(
@@ -5,7 +6,7 @@ export async function function_transform_args_return(
   function_name,
   args,
 ) {
-  let args_split = string_split_comma(args);
+  let args_split = null_is(args) ? [] : string_split_comma(args);
   let r = await function_transform_args_split(
     lambda_names,
     function_name,
