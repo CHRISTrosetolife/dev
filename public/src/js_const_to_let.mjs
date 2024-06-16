b8f83ddfa665ad67e9721e038163a129559c40a6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-15 21:08:14 -0400

    2024-06-16T01:08:13.926Zfunction_auto_after js_const_to_let

--- public/src/js_const_to_let.mjs
+++ public/src/js_const_to_let.mjs
@@ -1,8 +1,9 @@
 import { js_visit_node } from "./js_visit_node.mjs";
+import { list_includes } from "./list_includes.mjs";
 export function js_const_to_let(ast) {
   js_visit_node(ast, "VariableDeclaration", (v) => {
     let { node } = v;
-    if (node.kind === "const") {
+    if (list_includes(["const", "var"], node.kind)) {
       node.kind = "let";
     }
   });
