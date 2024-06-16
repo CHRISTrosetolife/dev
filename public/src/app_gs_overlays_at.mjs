7bc6cd066ec479bcbbba908b3a9cb3a462b6da92
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 09:59:03 -0400

    2024-06-16T13:59:03.315Zfunction_auto_after app_gs_overlays_at

--- public/src/app_gs_overlays_at.mjs
+++ public/src/app_gs_overlays_at.mjs
@@ -1,5 +1,5 @@
 import { app_gs_at_generic } from "./app_gs_at_generic.mjs";
-export function app_gs_overlays_at(map, a, lambda_overlay) {
+export function app_gs_overlays_at(map, tile, lambda_overlay) {
   let list = map.overlays;
-  app_gs_at_generic(list, a, lambda_overlay);
+  app_gs_at_generic(list, tile, lambda_overlay);
 }
