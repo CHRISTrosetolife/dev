01a77922efbc29045036e28ace28fc78f4b9763d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 18:49:00 -0400

    2024-06-16T22:49:00.196Zfunction_auto_after html_code_generate

--- public/src/html_code_generate.mjs
+++ public/src/html_code_generate.mjs
@@ -1,10 +1,10 @@
 import { log } from "./log.mjs";
 import { html_parse } from "./html_parse.mjs";
-import { object_properties } from "./object_properties.mjs";
 export function html_code_generate(input) {
   log({
     input,
   });
   let p = html_parse(input);
-  return object_properties(p);
+  let { attribs } = node;
+  return attribs;
 }
