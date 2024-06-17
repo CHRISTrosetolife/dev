969c4930eda21b68058a3885bf69bf321c588c85
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 18:07:37 -0400

    2024-06-17T22:07:37.195Zfunction_auto_after file_delete

--- public/src/file_delete.mjs
+++ public/src/file_delete.mjs
@@ -1,4 +1,5 @@
-let fs = await import_node("fs");
+import { import_node } from "./import_node.mjs";
 export async function file_delete(file_name) {
+  let fs = await import_node("fs");
   return await fs.promises.rm(file_name);
 }
