9963b1d85dd24336e8377469466e4c4462d769f5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 21:27:08 -0400

    2024-06-17T01:27:08.230Zfunction_auto_after list_filter_indices

--- public/src/list_filter_indices.mjs
+++ public/src/list_filter_indices.mjs
@@ -2,9 +2,12 @@ import { log } from "./log.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { list_map_index } from "./list_map_index.mjs";
+import { assert } from "./assert.mjs";
+import { list_includes } from "./list_includes.mjs";
 export function list_filter_indices(list, predicate) {
   let mapped = list_map_index(list, (h, index) => {
     let include = predicate(h);
+    assert(list_includes, [[true, false], include]);
     return {
       include: include,
       index,
