289d6c371b85bee1d38c157b555438908ce750c8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 00:00:40 -0400

    2024-06-17T04:00:39.847Zfunction_auto_after ceb_bible_words_definitions_get

--- public/src/ceb_bible_words_definitions_get.mjs
+++ public/src/ceb_bible_words_definitions_get.mjs
@@ -1,5 +1,3 @@
-import { list_map_property } from "./list_map_property.mjs";
-import { list_sum } from "./list_sum.mjs";
 import { each_async } from "./each_async.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
@@ -7,8 +5,6 @@ import { object_property_set } from "./object_property_set.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { ceb_definition } from "./ceb_definition.mjs";
 export async function ceb_bible_words_definitions_get(words_ceb, words_eng) {
-  let eng_total = list_sum(list_map_property(words_eng, "count"));
-  let ceb_total = list_sum(list_map_property(words_ceb, "count"));
   let existing = {};
   let result = [];
   await each_async(words_ceb, async (word_ceb) => {
