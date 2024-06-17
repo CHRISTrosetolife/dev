298fb959509fc01868a62d7687748cf8aa16049d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 20:53:38 -0400

    2024-06-17T00:53:37.472Zfunction_auto_after terminal_data_history_clear

--- public/src/terminal_data_history_clear.mjs
+++ public/src/terminal_data_history_clear.mjs
@@ -1,12 +1,9 @@
-import { list_size } from "./list_size.mjs";
-import { list_pop } from "./list_pop.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { terminal_data_transform } from "./terminal_data_transform.mjs";
 export async function terminal_data_history_clear() {
   await terminal_data_transform(function lambda(d) {
     let history = object_property_initialize(d, "history", []);
-    let r = list_pop(history);
-    d.history_index = list_size(history);
+    list_delete_all(history);
     return r;
   });
 }
