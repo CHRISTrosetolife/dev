b3bb2a843834f7f6322bac6f2fe75cce43381b44
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 23:24:31 -0400

    2024-06-17T03:24:30.801Zfunction_auto_after ceb_bible_words_count

---
+++ public/src/bible_words_count_generic.mjs
@@ -0,0 +1,13 @@
+import { string_count_words } from "./string_count_words.mjs";
+import { string_empty_not_is } from "./string_empty_not_is.mjs";
+import { list_filter } from "./list_filter.mjs";
+import { string_case_lower } from "./string_case_lower.mjs";
+import { string_trim } from "./string_trim.mjs";
+import { list_map } from "./list_map.mjs";
+export function bible_words_count_generic(all, symbols) {
+  all = list_map(all, (a) => string_trim(a, symbols));
+  all = list_map(all, string_case_lower);
+  all = list_filter(all, string_empty_not_is);
+  let c = string_count_words(all);
+  return c;
+}
--- public/src/ceb_bible_words_count.mjs
+++ public/src/ceb_bible_words_count.mjs
@@ -1,16 +1,7 @@
-import { string_count_words } from "./string_count_words.mjs";
-import { string_empty_not_is } from "./string_empty_not_is.mjs";
-import { list_filter } from "./list_filter.mjs";
-import { string_case_lower } from "./string_case_lower.mjs";
-import { string_trim } from "./string_trim.mjs";
-import { list_map } from "./list_map.mjs";
+import { bible_words_count_generic } from "./bible_words_count_generic.mjs";
 import { ceb_bible_words_cache } from "./ceb_bible_words_cache.mjs";
 export async function ceb_bible_words_count() {
   let all = await ceb_bible_words_cache();
   let symbols = "_-–—,;:!?.'‘’\"“”()[]}¶/`¯|€0123456789";
-  all = list_map(all, (a) => string_trim(a, symbols));
-  all = list_map(all, string_case_lower);
-  all = list_filter(all, string_empty_not_is);
-  let c = string_count_words(all);
-  return c;
+  return bible_words_count_generic(all, symbols);
 }
