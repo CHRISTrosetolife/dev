9602234563d51000d94706aee4efc85e145f9307
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 06:38:40 -0400

    2024-06-17T10:38:38.470Zfunction_auto_after ceb_bible_words_definitions_map

--- public/src/ceb_bible_words_definitions_map.mjs
+++ public/src/ceb_bible_words_definitions_map.mjs
@@ -1,3 +1,4 @@
+import { list_size } from "./list_size.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
@@ -7,6 +8,7 @@ import { ceb_bible_words_count } from "./ceb_bible_words_count.mjs";
 export async function ceb_bible_words_definitions_map() {
   let words_ceb = await ceb_bible_words_count();
   let result = {};
+  let s = list_size(words_ceb);
   await each_async(words_ceb, async (word_ceb) => {
     let { word, definitions } = await ceb_definition(word_ceb.word);
     if (object_property_exists_not(result, word)) {
