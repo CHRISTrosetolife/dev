68d2a29ae400606638e6cfa9622a0121937464e0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 23:53:56 -0400

    2024-06-17T03:53:56.208Zfunction_auto_after ceb_bible_words_definitions_get

--- public/src/ceb_bible_words_definitions_get.mjs
+++ public/src/ceb_bible_words_definitions_get.mjs
@@ -1,13 +1,13 @@
+import { each_async } from "./each_async.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { ceb_definition } from "./ceb_definition.mjs";
-import { each_index_async } from "./each_index_async.mjs";
 export async function ceb_bible_words_definitions_get(words_ceb, words_eng) {
   let existing = {};
   let result = [];
-  await each_index_async(words_ceb, async (bible_word, index) => {
+  await each_async(words_ceb, async (bible_word) => {
     let { word, definitions } = await ceb_definition(bible_word);
     if (object_property_exists_not(existing, word)) {
       object_property_set(existing, word, true);
