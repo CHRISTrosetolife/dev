10306468dc8518ba1b80bd49737c0b2d905ac615
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 21:37:31 -0400

    2024-06-17T01:37:31.190Zfunction_auto_after html_code_generate

--- public/src/html_code_generate.mjs
+++ public/src/html_code_generate.mjs
@@ -15,8 +15,7 @@ export function html_code_generate(tag_name, input) {
   let { attribs } = c;
   [
     js_code_statement_call_args(html_element.name, ["parent", tag_name]),
-    js_code_statement_call_args(html_element.name, ["parent", tag_name]),
-    html_attribute_set,
+    html_attribute_set(html_element.name, ["parent", tag_name]),
   ];
   return attribs;
   return object_properties(c);
