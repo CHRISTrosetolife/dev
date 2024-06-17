ab4f947afa61fad7763213082fe5b7c7981e7618
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:33:36 -0400

    2024-06-17T16:33:36.374Zfunction_auto_after function_cache_new_run

--- public/src/function_cache_new_run.mjs
+++ public/src/function_cache_new_run.mjs
@@ -1,7 +1,9 @@
 import { log } from "./log.mjs";
 import { function_cache_new } from "./function_cache_new.mjs";
+import { function_run } from "./function_run.mjs";
 export async function function_cache_new_run(function_name) {
   let r = await function_cache_new(function_name);
   log(r);
   let { cache_name } = r;
+  await function_run(cache_name, []);
 }
