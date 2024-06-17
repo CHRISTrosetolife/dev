853315df3e15291e2259eda89335f2797aa03ed7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 17:28:31 -0400

    2024-06-17T21:28:30.585Zfunction_auto_after js_param_new

--- public/src/js_param_new.mjs
+++ public/src/js_param_new.mjs
@@ -7,12 +7,14 @@ import { list_add } from "./list_add.mjs";
 import { js_imports_add } from "./js_imports_add.mjs";
 import { assert } from "./assert.mjs";
 import { string_is } from "./string_is.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function js_param_new(
   ast,
   function_name,
   param_name,
   default_value_string,
 ) {
+  assert_arguments_length(arguments, 4);
   assert(string_is, [param_name]);
   assert(string_is, [default_value_string]);
   let needs_imports_add = false;
