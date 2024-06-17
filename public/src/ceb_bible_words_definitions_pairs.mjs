4e869ab00273fe93c89d802884c2f1696fbe8a67
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:58:08 -0400

    2024-06-17T16:58:08.220Zfunction_auto_after ceb_bible_words_definitions_pairs

--- public/src/ceb_bible_words_definitions_pairs.mjs
+++ public/src/ceb_bible_words_definitions_pairs.mjs
@@ -2,10 +2,8 @@ import { ceb_bible_words_score } from "./ceb_bible_words_score.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
-import { ceb_bible_words_definitions } from "./ceb_bible_words_definitions.mjs";
 import { bible_words_eng_score } from "./bible_words_eng_score.mjs";
-export async function ceb_bible_words_definitions_pairs(skip, limit) {
-  let definitions = await ceb_bible_words_definitions(skip, limit);
+export async function ceb_bible_words_definitions_pairs() {
   let ceb_scores = await ceb_bible_words_score();
   let eng_scores = await bible_words_eng_score();
   let pairs = list_adder((la) =>
