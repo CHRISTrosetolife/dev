9cc829c63604ad033395737b2c35a49a3f4df371
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 21:51:38 -0400

    2024-06-17T01:51:37.766Zfunction_auto_after html_code_generate

--- public/src/html_code_generate.mjs
+++ public/src/html_code_generate.mjs
@@ -5,18 +5,12 @@ import { js_code_statement_declare_assign } from "./js_code_statement_declare_as
 import { html_attribute_set } from "./html_attribute_set.mjs";
 import { js_code_statement_call_args } from "./js_code_statement_call_args.mjs";
 import { html_parse_visit_tag_single } from "./html_parse_visit_tag_single.mjs";
-import { log } from "./log.mjs";
 import { html_parse } from "./html_parse.mjs";
-import { object_properties } from "./object_properties.mjs";
 import { html_element } from "./html_element.mjs";
 import { list_add } from "./list_add.mjs";
 import { each_object } from "./each_object.mjs";
 import { list_concat } from "./list_concat.mjs";
 export function html_code_generate(tag_name, input) {
-  '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />';
-  log({
-    input,
-  });
   let p = html_parse(input);
   let c = html_parse_visit_tag_single(p, tag_name);
   let { attribs } = c;
@@ -34,6 +28,5 @@ export function html_code_generate(tag_name, input) {
     );
     list_add(statements, s);
   });
-  return attribs;
-  return object_properties(c);
+  return statements;
 }
