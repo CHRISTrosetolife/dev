5c795b3a49d03f7bc1d5006aace13ed6c044f1d9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:43:22 -0400

    2024-06-17T16:43:21.218Zfunction_auto_after bible_words_eng_score

--- public/src/bible_words_eng_score.mjs
+++ public/src/bible_words_eng_score.mjs
@@ -1,8 +1,7 @@
-import { list_last } from "./list_last.mjs";
 import { bible_words_score_generic } from "./bible_words_score_generic.mjs";
 import { bible_words_eng_count_cache } from "./bible_words_eng_count_cache.mjs";
 export async function bible_words_eng_score() {
   let w = await bible_words_eng_count_cache();
   bible_words_score_generic(w);
-  return list_last(w);
+  return w;
 }
