fac7bed7bbca7c6f0fe7b02ba242865b55cc36de
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:54:46 -0400

    2024-06-17T16:54:46.234Zfunction_auto_after ceb_bible_word_pair_score

--- public/src/ceb_bible_word_pair_score.mjs
+++ public/src/ceb_bible_word_pair_score.mjs
@@ -1,4 +1,8 @@
 import { assert } from "./assert.mjs";
+import { list_first } from "./list_first.mjs";
+import { list_second } from "./list_second.mjs";
 export function ceb_bible_word_pair_score(pair) {
   assert(list_length_2, [pair]);
+  let f = list_first(pair);
+  let s = list_second(pair);
 }
