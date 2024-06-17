13a6d52758dc390f27184a52ecaec104344dcd9f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 00:32:59 -0400

    2024-06-17T04:32:58.994Zfunction_auto_after ceb_bible_words_definitions_map

--- public/src/ceb_bible_words_definitions_map.mjs
+++ public/src/ceb_bible_words_definitions_map.mjs
@@ -1,1 +1,4 @@
-export function ceb_bible_words_definitions_map() {}
+import { ceb_bible_words_count } from "./ceb_bible_words_count.mjs";
+export async function ceb_bible_words_definitions_map() {
+  let words_ceb = await ceb_bible_words_count();
+}
