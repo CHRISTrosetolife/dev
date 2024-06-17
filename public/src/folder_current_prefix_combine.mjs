73fbd61fb369cd6d802201cb1cbee3f9b1cfe725
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 18:05:17 -0400

    2024-06-17T22:05:09.920Zfunction_auto_after folder_current_prefix_combine

---
+++ public/src/folder_current_prefix_combine.mjs
@@ -0,0 +1,4 @@
+import { folder_current_prefix } from "./folder_current_prefix.mjs";
+export function folder_current_prefix_combine(function_path_second) {
+  return folder_current_prefix() + function_path_second;
+}
