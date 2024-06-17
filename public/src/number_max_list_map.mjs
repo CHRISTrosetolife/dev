17731bc2b55bea806cc86e8c524b685929788d06
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:26:43 -0400

    2024-06-17T16:26:42.197Zfunction_auto_after number_max_list_map

--- public/src/number_max_list_map.mjs
+++ public/src/number_max_list_map.mjs
@@ -1,5 +1,5 @@
 import { list_map } from "./list_map.mjs";
 export function number_max_list_map(list, mapper) {
   let mapped = list_map(list, mapper);
-  return Math.max(...list);
+  return Math.max(...mapped);
 }
