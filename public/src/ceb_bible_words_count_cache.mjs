6cf1114ab653fd6d611824a9aa0dd61ca934e638
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 07:35:47 -0400

    2024-06-17T11:35:47.504Zfunction_auto_after ceb_bible_words_count_cache

--- public/src/ceb_bible_words_count_cache.mjs
+++ public/src/ceb_bible_words_count_cache.mjs
@@ -1,5 +1,5 @@
 import { ceb_bible_words_count } from "./ceb_bible_words_count.mjs";
 import { function_cache } from "./function_cache.mjs";
 export async function ceb_bible_words_count_cache() {
-  await function_cache(ceb_bible_words_count, []);
+  return await function_cache(ceb_bible_words_count, []);
 }
