d3910136c53a163e0ac36596ffa382ee6c5891d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 13:02:13 -0400

    2024-06-17T17:02:13.129Zfunction_auto_after ceb_bible_words_definitions_pairs

--- public/src/ceb_bible_words_definitions_pairs.mjs
+++ public/src/ceb_bible_words_definitions_pairs.mjs
@@ -1,3 +1,4 @@
+import { assert } from "./assert.mjs";
 import { string_match_best_find_index } from "./string_match_best_find_index.mjs";
 import { ceb_bible_words_score } from "./ceb_bible_words_score.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -6,6 +7,7 @@ import { list_adder } from "./list_adder.mjs";
 import { bible_words_eng_score } from "./bible_words_eng_score.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 import { list_get } from "./list_get.mjs";
+import { number_is } from "./number_is.mjs";
 export async function ceb_bible_words_definitions_pairs() {
   let ceb_scores = await ceb_bible_words_score();
   let pairs = list_adder((la) =>
@@ -24,7 +26,8 @@ export async function ceb_bible_words_definitions_pairs() {
     let { ceb, eng } = pair;
     let index = string_match_best_find_index(eng, eng_words);
     let eng_score = list_get(eng_scores, index);
-    let score = eng_score;
+    let { score } = eng_score;
+    assert(number_is(eng_score));
   });
   return {
     pairs,
