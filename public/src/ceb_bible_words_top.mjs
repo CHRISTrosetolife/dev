7d8db10ccf2553a550fa8ccc845e36fc1511b76a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 06:52:48 -0400

    2024-06-17T10:52:46.376Zfunction_auto_after ceb_bible_words_top

--- public/src/ceb_bible_words_top.mjs
+++ public/src/ceb_bible_words_top.mjs
@@ -1,7 +1,7 @@
-import { ceb_bible_words_count } from "./ceb_bible_words_count.mjs";
+import { ceb_bible_words_count_cache } from "./ceb_bible_words_count_cache.mjs";
 import { list_take } from "./list_take.mjs";
 export async function ceb_bible_words_top(count) {
-  let words = await ceb_bible_words_count();
+  let words = await ceb_bible_words_count_cache();
   let t = list_take(words, count);
   return t;
 }
