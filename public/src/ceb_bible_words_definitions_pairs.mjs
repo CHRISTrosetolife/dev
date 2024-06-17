a13a273689876ea59068e2db9fa97da03669a392
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 13:01:08 -0400

    2024-06-17T17:01:07.899Zfunction_auto_after ceb_bible_words_definitions_pairs

--- public/src/ceb_bible_words_definitions_pairs.mjs
+++ public/src/ceb_bible_words_definitions_pairs.mjs
@@ -11,8 +11,8 @@ export async function ceb_bible_words_definitions_pairs() {
     each(ceb_scores, (w) =>
       each(object_property_get(w, "definitions"), (d) =>
         la({
-          eng: object_property_get(w, "word"),
-          ceb: d,
+          ceb: object_property_get(w, "word"),
+          eng: d,
         }),
       ),
     ),
