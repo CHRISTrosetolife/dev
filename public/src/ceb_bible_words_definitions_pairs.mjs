6ce8459dce3c5499a0100388491ab2b3b82f1bce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 17:18:32 -0400

    2024-06-17T21:18:31.860Zfunction_auto_after ceb_bible_words_definitions_pairs

--- public/src/ceb_bible_words_definitions_pairs.mjs
+++ public/src/ceb_bible_words_definitions_pairs.mjs
@@ -6,12 +6,13 @@ import { list_map } from "./list_map.mjs";
 export async function ceb_bible_words_definitions_pairs() {
   let ceb_scores = await ceb_bible_words_score();
   let pairs = await ceb_bible_words_definitions_pairs_compute_cache();
-  return {
-    pairs: list_map(pairs, (p) =>
-      list_map(["ceb", "eng"], (language) =>
-        object_property_get(p[language], "word"),
-      ),
+  let concise = list_map(pairs, (p) =>
+    list_map(["ceb", "eng"], (language) =>
+      object_property_get(p[language], "word"),
     ),
+  );
+  return {
+    pairs: concise,
     definitions: list_map(ceb_scores, (w) =>
       object_properties_new(w, ["word", "definitions"]),
     ),
