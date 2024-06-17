954c00d0ea7d3433db1509942758f5bdd3a6d563
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 07:40:37 -0400

    2024-06-17T11:40:37.227Zfunction_auto_after list_sum

--- public/src/list_sum.mjs
+++ public/src/list_sum.mjs
@@ -1,6 +1,8 @@
 import { add } from "./add.mjs";
 export function list_sum(lambda) {
   let result = 0;
-  lambda((item) => (result = add(result, item)));
+  lambda((item) => {
+    result = add(result, item);
+  });
   return result;
 }
