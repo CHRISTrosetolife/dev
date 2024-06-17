1b5f7419c6f581edc6b7670ea6c1ef1c301a3173
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 18:15:12 -0400

    2024-06-17T22:15:11.220Zfunction_auto_after function_cache_refresh

--- public/src/function_cache_refresh.mjs
+++ public/src/function_cache_refresh.mjs
@@ -1,8 +1,8 @@
+import { function_cache_delete_if_exists } from "./function_cache_delete_if_exists.mjs";
 import { function_cache_name } from "./function_cache_name.mjs";
-import { function_cache_delete } from "./function_cache_delete.mjs";
 import { function_run } from "./function_run.mjs";
 export async function function_cache_refresh(function_name) {
-  await function_cache_delete(function_name);
+  await function_cache_delete_if_exists(function_name);
   let cache_name = function_cache_name(function_name);
   return await function_run(cache_name, []);
 }
