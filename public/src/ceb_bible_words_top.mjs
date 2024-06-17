8335841ece5a99d91848b550967e509ee9b22eb0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 16:43:37 -0400

    2024-06-17T20:43:36.362Zfunction_auto_after ceb_bible_words_top

--- public/src/ceb_bible_words_top.mjs
+++ public/src/ceb_bible_words_top.mjs
@@ -1,7 +1,9 @@
-import { ceb_bible_words_count_cache } from "./ceb_bible_words_count_cache.mjs";
+import { ceb_bible_words_score } from "./ceb_bible_words_score.mjs";
+import { list_sort_property } from "./list_sort_property.mjs";
 import { list_take } from "./list_take.mjs";
 export async function ceb_bible_words_top(count) {
-  let words = await ceb_bible_words_count_cache();
+  let ceb_scores = await ceb_bible_words_score();
+  list_sort_property(ceb_scores, "score");
   let t = list_take(words, count);
   return t;
 }
