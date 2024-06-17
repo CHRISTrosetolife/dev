35f39a772747e7137fa766314692a142b638ac82
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 14:16:03 -0400

    2024-06-17T18:16:02.500Zfunction_auto_after ceb_bible_words_definitions_pairs_compute

--- public/src/ceb_bible_words_definitions_pairs_compute.mjs
+++ public/src/ceb_bible_words_definitions_pairs_compute.mjs
@@ -39,8 +39,8 @@ export async function ceb_bible_words_definitions_pairs_compute() {
       });
     }
     let { ceb, eng } = pair;
-    let match = string_match_best_find(eng.word, eng_words);
-    let { index } = match;
+    let m = string_match_best_find(eng.word, eng_words);
+    let { index, match } = m;
     eng.score = list_get(eng_scores, index).score;
     assert(number_is, [eng.score]);
     let scores = list_map_property([eng, ceb], "score");
