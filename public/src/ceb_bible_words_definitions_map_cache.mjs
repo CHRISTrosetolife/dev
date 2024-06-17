28672f8610c3c5d5c8ac0dcac822008122eb4ee7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 00:39:55 -0400

    2024-06-17T04:39:54.529Zfunction_auto_after ceb_bible_words_definitions_map_cache

--- public/src/ceb_bible_words_definitions_map_cache.mjs
+++ public/src/ceb_bible_words_definitions_map_cache.mjs
@@ -1,5 +1,5 @@
 import { ceb_bible_words_definitions_map } from "./ceb_bible_words_definitions_map.mjs";
 import { function_cache } from "./function_cache.mjs";
 export async function ceb_bible_words_definitions_map_cache() {
-  await function_cache(ceb_bible_words_definitions_map, []);
+  return await function_cache(ceb_bible_words_definitions_map, []);
 }
