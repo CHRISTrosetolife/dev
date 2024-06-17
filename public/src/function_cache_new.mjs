12e09e262161be39970af9ab83ef2636efeb8cd2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 06:48:11 -0400

    2024-06-17T10:48:10.431Zfunction_auto_after function_cache_new

--- public/src/function_cache_new.mjs
+++ public/src/function_cache_new.mjs
@@ -1,3 +1,4 @@
+import { js_code_await } from "./js_code_await.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 import { js_code_brackets } from "./js_code_brackets.mjs";
 import { function_cache } from "./function_cache.mjs";
@@ -8,10 +9,12 @@ export async function function_cache_new(function_name) {
   await function_new_generic(
     string_combine(function_name, "_cache"),
     "",
-    js_code_statement_call_args(function_cache.name, [
-      function_name,
-      js_code_brackets(),
-    ]),
+    js_code_await(
+      js_code_statement_call_args(function_cache.name, [
+        function_name,
+        js_code_brackets(),
+      ]),
+    ),
     false,
     [],
     false,
