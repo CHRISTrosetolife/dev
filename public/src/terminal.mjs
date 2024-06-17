d5e9c76987a2484bdb08e6da36a59fbaf7ddb024
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 20:38:46 -0400

    2024-06-17T00:38:46.415Z  on_keypress_log

--- public/src/terminal.mjs
+++ public/src/terminal.mjs
@@ -249,15 +249,7 @@ export async function terminal() {
     });
   }
   async function history_index_previous() {
-    return await terminal_data_transform(function (d) {
-      if (!number_is(d.history_index)) {
-        return;
-      }
-      d.history_index = lambda(d.history_index);
-      let i = d.history_index;
-      let history = object_property_initialize(d, "history", []);
-      return list_get(history, i);
-    });
+    return await terminal_index_history_transform(lambda);
     function lambda(index) {
       return subtract_1(index);
     }
@@ -379,3 +371,15 @@ export async function terminal() {
     return tokens;
   }
 }
+async function terminal_index_history_transform(lambda) {
+    return await terminal_data_transform(function (d) {
+        if (!number_is(d.history_index)) {
+            return;
+        }
+        d.history_index = lambda(d.history_index);
+        let i = d.history_index;
+        let history = object_property_initialize(d, "history", []);
+        return list_get(history, i);
+    });
+}
+
---
+++ public/src/terminal_index_history_transform.mjs
@@ -0,0 +1,15 @@
+import { list_get } from "./list_get.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
+import { number_is } from "./number_is.mjs";
+import { terminal_data_transform } from "./terminal_data_transform.mjs";
+export async function terminal_index_history_transform(lambda) {
+  return await terminal_data_transform(function (d) {
+    if (!number_is(d.history_index)) {
+      return;
+    }
+    d.history_index = lambda(d.history_index);
+    let i = d.history_index;
+    let history = object_property_initialize(d, "history", []);
+    return list_get(history, i);
+  });
+}
