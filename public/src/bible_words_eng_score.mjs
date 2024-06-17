885c46bb8f527cc4552c88c57d29b7df9ede11b3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:37:28 -0400

    2024-06-17T16:37:27.561Zfunction_auto_after bible_words_eng_score

--- public/src/bible_words_eng_score.mjs
+++ public/src/bible_words_eng_score.mjs
@@ -1,1 +1,6 @@
-export function bible_words_eng_score() {}
+import { bible_words_score_generic } from "./bible_words_score_generic.mjs";
+import { bible_words_eng_count_cache } from "./bible_words_eng_count_cache.mjs";
+export function bible_words_eng_score() {
+  bible_words_eng_count_cache();
+  bible_words_score_generic(definitions);
+}
