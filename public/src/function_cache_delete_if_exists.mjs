f029dd01b7d58bddb682e181ca40936d5ae6c1c6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 17:27:08 -0400

    2024-06-17T21:27:08.299Zfunction_auto_after function_cache_delete_if_exists

--- public/src/function_cache_delete_if_exists.mjs
+++ public/src/function_cache_delete_if_exists.mjs
@@ -1,6 +1,6 @@
-import { file_delete } from "./file_delete.mjs";
+import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
 import { function_cache_path } from "./function_cache_path.mjs";
 export async function function_cache_delete_if_exists(function_name) {
   let file_path = function_cache_path(function_name, args);
-  await file_delete(file_path);
+  await file_delete_if_exists(file_path);
 }
