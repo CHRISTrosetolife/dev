42cff08c55f1411fd0ffcd6160b4c4231fc831e8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 18:28:51 -0400

    2024-06-17T22:28:50.612Zfunction_auto_after list_to_lookup_value_property

---
+++ public/src/list_to_lookup_value_property.mjs
@@ -0,0 +1,18 @@
+import { object_property_get } from "./object_property_get.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+import { each } from "./each.mjs";
+export function list_to_lookup_value_property(
+  list,
+  key_property_name,
+  value_property_name,
+) {
+  let result = {};
+  each(list, (item) =>
+    object_property_set(
+      result,
+      object_property_get(item, key_property_name),
+      object_property_get(item, value_property_name),
+    ),
+  );
+  return result;
+}
