f0aea13433e919ea8620ecd217ee4bcfbc0cf20d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 23:53:39 -0400

    2024-06-17T03:53:38.815Zfunction_auto_after ceb_bible_words_definitions_get

--- public/src/ceb_bible_words_definitions_get.mjs
+++ public/src/ceb_bible_words_definitions_get.mjs
@@ -4,21 +4,12 @@ import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { ceb_definition } from "./ceb_definition.mjs";
-import { log } from "./log.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 export async function ceb_bible_words_definitions_get(words_ceb, words_eng) {
   let existing = {};
   let result = [];
   let words_ceb_size = list_size(words_ceb);
   await each_index_async(words_ceb, async (bible_word, index) => {
-    if (10) {
-      log({
-        t_length: words_ceb_size,
-        index,
-        bible_word,
-        index,
-      });
-    }
     let { word, definitions } = await ceb_definition(bible_word);
     if (object_property_exists_not(existing, word)) {
       object_property_set(existing, word, true);
@@ -28,12 +19,6 @@ export async function ceb_bible_words_definitions_get(words_ceb, words_eng) {
           definitions,
         });
       }
-    }
-    if (10) {
-      log({
-        word,
-        definitions,
-      });
     }
   });
   return result;
