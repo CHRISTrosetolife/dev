be2c7ebbe796e089e4492e573d1058164dc932e3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 16:46:34 -0400

    2024-06-17T20:46:33.860Zfunction_auto_after ceb_bible_words_top

--- public/src/ceb_bible_words_top.mjs
+++ public/src/ceb_bible_words_top.mjs
@@ -1,15 +1,18 @@
-import { log } from "./log.mjs";
-import { each } from "./each.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 import { ceb_bible_words_score } from "./ceb_bible_words_score.mjs";
 import { list_sort_property } from "./list_sort_property.mjs";
 import { list_take } from "./list_take.mjs";
 import { list_reverse } from "./list_reverse.mjs";
+import { file_overwrite } from "./file_overwrite.mjs";
+import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
+import { string_combine } from "./string_combine.mjs";
 export async function ceb_bible_words_top(count) {
   let words = await ceb_bible_words_score();
   list_sort_property(words, "score");
   list_reverse(words);
   let t = list_take(words, count);
   let m = list_map_property(t, "word");
-  each(m, log);
+  await file_overwrite(
+    folder_gitignore_path(string_combine(ceb_bible_words_top.name, ".txt")),
+  );
 }
