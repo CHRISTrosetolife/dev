6f0fd037c31dceb22f94d48216dcf8688c52aa43
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:40:09 -0400

    2024-06-17T16:40:08.532Zfunction_auto_after bible_words_eng_score

--- public/src/bible_words_eng_score.mjs
+++ public/src/bible_words_eng_score.mjs
@@ -1,6 +1,6 @@
 import { bible_words_score_generic } from "./bible_words_score_generic.mjs";
 import { bible_words_eng_count_cache } from "./bible_words_eng_count_cache.mjs";
-export function bible_words_eng_score() {
-  let w = bible_words_eng_count_cache();
+export async function bible_words_eng_score() {
+  let w = await bible_words_eng_count_cache();
   bible_words_score_generic(definitions);
 }
