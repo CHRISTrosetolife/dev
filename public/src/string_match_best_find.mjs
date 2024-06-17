5d3a6d59704d0af9b17c1a516d4b6413848c4c39
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 14:10:53 -0400

    2024-06-17T18:10:53.377Zfunction_auto_after string_match_best_find

--- public/src/string_match_best_find.mjs
+++ public/src/string_match_best_find.mjs
@@ -3,5 +3,7 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function string_match_best_find(search, choices) {
   assert_arguments_length(arguments, 2);
   let matches = ss.findBestMatch(search, choices);
-  return matches.bestMatchIndex;
+  return {
+    index: matches.bestMatchIndex,
+  };
 }
