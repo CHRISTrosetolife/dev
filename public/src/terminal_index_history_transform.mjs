6becac196f0ef748b6a3d881df10cd971466a180
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 21:08:27 -0400

    2024-06-17T01:08:26.802Zfunction_auto_after terminal_index_history_transform

--- public/src/terminal_index_history_transform.mjs
+++ public/src/terminal_index_history_transform.mjs
@@ -2,6 +2,7 @@ import { list_get } from "./list_get.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { number_is } from "./number_is.mjs";
 import { terminal_data_transform } from "./terminal_data_transform.mjs";
+import { list_size } from "./list_size.mjs";
 export async function terminal_index_history_transform(lambda) {
   return await terminal_data_transform(function (d) {
     if (!number_is(d.history_index)) {
@@ -10,6 +11,9 @@ export async function terminal_index_history_transform(lambda) {
     d.history_index = lambda(d.history_index);
     let i = d.history_index;
     let history = object_property_initialize(d, "history", []);
+    if (i >= list_size(history)) {
+      return;
+    }
     let r = list_get(history, i);
     return r;
   });
