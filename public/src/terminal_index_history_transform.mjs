7292810497321180f57927a49513e19a1a497453
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 17:43:23 -0400

    2024-06-17T21:43:22.193Zfunction_auto_after terminal_index_history_transform

--- public/src/terminal_index_history_transform.mjs
+++ public/src/terminal_index_history_transform.mjs
@@ -10,7 +10,9 @@ export async function terminal_index_history_transform(lambda) {
     }
     let history = object_property_initialize(d, "history", []);
     d.history_index = lambda(d.history_index);
-    if (d.history_index >= list_size(history)) {
+    let s = list_size(history);
+    if (d.history_index >= s) {
+      d.history_index = s;
       return;
     }
     let r = list_get(history, d.history_index);
