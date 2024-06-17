0e544c28235b60e74eebbfcab79af48a81a6350b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 14:14:05 -0400

    2024-06-17T18:14:05.085Zfunction_auto_after string_match_best_find

--- public/src/string_match_best_find.mjs
+++ public/src/string_match_best_find.mjs
@@ -4,7 +4,7 @@ export function string_match_best_find(search, choices) {
   assert_arguments_length(arguments, 2);
   let matches = ss.findBestMatch(search, choices);
   return {
-    match: bestMatch,
+    match: matches.bestMatch,
     index: matches.bestMatchIndex,
   };
 }
