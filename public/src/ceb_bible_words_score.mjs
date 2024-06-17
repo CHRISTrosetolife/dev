5c42ed056c51a42eb34188369ad6c3cd0a8abd11
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 07:41:48 -0400

    2024-06-17T11:41:47.907Zfunction_auto_after ceb_bible_words_score

--- public/src/ceb_bible_words_score.mjs
+++ public/src/ceb_bible_words_score.mjs
@@ -1,10 +1,13 @@
+import { each } from "./each.mjs";
 import { list_summer } from "./list_summer.mjs";
 import { ceb_bible_words_count_cache } from "./ceb_bible_words_count_cache.mjs";
 import { bible_words_eng_cache } from "./bible_words_eng_cache.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 export async function ceb_bible_words_score() {
   let words_ceb = await ceb_bible_words_count_cache();
-  let ceb_total = list_summer(list_map_property(words_ceb, "count"));
+  let ceb_total = list_summer((s) =>
+    each(list_map_property(words_ceb, "count")),
+  );
   return ceb_total;
   let words_eng = await bible_words_eng_cache();
   let eng_total = list_summer(list_map_property(words_eng, "count"));
