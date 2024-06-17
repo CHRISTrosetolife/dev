c889e6b765ad5f09845bd7a6a1f4c960679589b7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 21:30:35 -0400

    2024-06-17T01:30:34.488Zfunction_auto_after list_remove_multiple_at

--- public/src/list_remove_multiple_at.mjs
+++ public/src/list_remove_multiple_at.mjs
@@ -1,4 +1,3 @@
-import { list_includes } from "./list_includes.mjs";
 import { list_get } from "./list_get.mjs";
 import { number_less_than } from "./number_less_than.mjs";
 import { assert_not } from "./assert_not.mjs";
@@ -6,6 +5,5 @@ export function list_remove_multiple_at(list, index, count) {
   assert_not(number_less_than, [index, 0]);
   let element = list_get(list, index);
   list.splice(index, count);
-  assert_not(list_includes, [list, element]);
   return element;
 }
