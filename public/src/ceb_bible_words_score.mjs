3d901e8848a72b81e5c370e716b13d99e0fbd1c2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 00:01:40 -0400

    2024-06-17T04:01:39.998Zfunction_auto_after ceb_bible_words_score

--- public/src/ceb_bible_words_score.mjs
+++ public/src/ceb_bible_words_score.mjs
@@ -1,6 +1,8 @@
+import { bible_words_eng_cache } from "./bible_words_eng_cache.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 import { list_sum } from "./list_sum.mjs";
-export function ceb_bible_words_score() {
+export async function ceb_bible_words_score() {
+  let words_eng = await bible_words_eng_cache();
   let eng_total = list_sum(list_map_property(words_eng, "count"));
   let ceb_total = list_sum(list_map_property(words_ceb, "count"));
 }
