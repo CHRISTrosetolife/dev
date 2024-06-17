54b21f8fe35b6a9adde79f328fe2eef6d1fce23d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 17:49:03 -0400

    2024-06-17T21:49:03.472Zfunction_auto_after function_transform_all_args

--- public/src/function_transform_all_args.mjs
+++ public/src/function_transform_all_args.mjs
@@ -1,7 +1,9 @@
 import { log } from "./log.mjs";
 import { function_names } from "./function_names.mjs";
 import { function_transform_args } from "./function_transform_args.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function function_transform_all_args(lambda_name, args) {
+  assert_arguments_length(arguments, 2);
   let function_names_result = await function_names();
   for (let function_name of function_names_result) {
     log({
