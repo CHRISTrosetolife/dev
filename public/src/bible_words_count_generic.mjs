69497dc1918eb0dced8ce763eb8423347561442b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 23:28:55 -0400

    2024-06-17T03:28:54.983Zfunction_auto_after bible_words_count_generic

--- public/src/bible_words_count_generic.mjs
+++ public/src/bible_words_count_generic.mjs
@@ -1,4 +1,4 @@
-import { string_count_words } from "./string_count_words.mjs";
+import { string_count } from "./string_count.mjs";
 import { string_empty_not_is } from "./string_empty_not_is.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
@@ -8,6 +8,6 @@ export function bible_words_count_generic(all, symbols) {
   all = list_map(all, (a) => string_trim(a, symbols));
   all = list_map(all, string_case_lower);
   all = list_filter(all, string_empty_not_is);
-  let c = string_count_words(all);
+  let c = string_count(all);
   return c;
 }
