cad0063ff0511131990f233ebd728bb4bb4a47c0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 20:24:49 -0400

    2024-06-17T00:24:48.815Zfunction_auto_after file_json_transform_exists

--- public/src/file_json_transform_exists.mjs
+++ public/src/file_json_transform_exists.mjs
@@ -5,5 +5,5 @@ export async function file_json_transform_exists(file_path, lambda) {
   if (!(await file_exists(file_path))) {
     await file_write_json(file_path, {});
   }
-  await file_json_transform(lambda, file_path, []);
+  return await file_json_transform(lambda, file_path, []);
 }
