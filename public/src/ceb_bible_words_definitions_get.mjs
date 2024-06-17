e4cbdc967fa5081c7f31b09f0fe5aa32cedcf025
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 23:56:36 -0400

    2024-06-17T03:56:35.728Zfunction_auto_after ceb_bible_words_definitions_get

--- public/src/ceb_bible_words_definitions_get.mjs
+++ public/src/ceb_bible_words_definitions_get.mjs
@@ -1,18 +1,14 @@
-import { list_sum } from "./list_sum.mjs";
 import { each_async } from "./each_async.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { ceb_definition } from "./ceb_definition.mjs";
-import { list_map_property } from "./list_map_property.mjs";
 export async function ceb_bible_words_definitions_get(words_ceb, words_eng) {
   let existing = {};
   let result = [];
-  let eng_total = list_sum(list_map_property(words_eng, "count"));
-  let ceb_total = list_sum(list_map_property(words_ceb, "count"));
-  await each_async(words_ceb, async (bible_word) => {
-    let { word, definitions } = await ceb_definition(bible_word.word);
+  await each_async(words_ceb, async (word_ceb) => {
+    let { word, definitions } = await ceb_definition(word_ceb.word);
     if (object_property_exists_not(existing, word)) {
       object_property_set(existing, word, true);
       if (list_empty_not_is(definitions)) {
