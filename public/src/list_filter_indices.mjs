6df34153bb7a71f41a329cae3409bec21fc579fa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 21:26:21 -0400

    2024-06-17T01:26:20.641Zfunction_auto_after list_filter_indices

--- public/src/list_filter_indices.mjs
+++ public/src/list_filter_indices.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { list_map_index } from "./list_map_index.mjs";
@@ -7,6 +8,9 @@ export function list_filter_indices(list, predicate) {
       include: predicate(h),
       index,
     };
+  });
+  log({
+    mapped,
   });
   let filtered = list_filter(mapped, (m) => m.include === true);
   let mapped2 = list_map(filtered, (f) => f.index);
