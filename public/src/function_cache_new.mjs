1917aed45026ede16edc481a8fbb28157f1b809b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 07:37:21 -0400

    2024-06-17T11:37:21.029Zfunction_auto_after function_cache_new

--- public/src/function_cache_new.mjs
+++ public/src/function_cache_new.mjs
@@ -1,3 +1,4 @@
+import { js_code_return } from "./js_code_return.mjs";
 import { js_code_call_args } from "./js_code_call_args.mjs";
 import { js_code_await } from "./js_code_await.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
@@ -9,11 +10,13 @@ export async function function_cache_new(function_name) {
   await function_new_generic(
     string_combine(function_name, "_cache"),
     "",
-    js_code_await(
-      js_code_call_args(function_cache.name, [
-        function_name,
-        js_code_brackets(),
-      ]),
+    js_code_return(
+      js_code_await(
+        js_code_call_args(function_cache.name, [
+          function_name,
+          js_code_brackets(),
+        ]),
+      ),
     ),
     false,
     [],
