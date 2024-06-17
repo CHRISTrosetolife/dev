82da227e3422028aea62c9a2fd2e9ac3860542fd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 13:12:45 -0400

    2024-06-17T17:12:44.677Zfunction_auto_after list_sum

--- public/src/list_sum.mjs
+++ public/src/list_sum.mjs
@@ -1,1 +1,5 @@
-export function list_sum(list) {}
+import { each } from "./each.mjs";
+import { summation } from "./summation.mjs";
+export function list_sum(list) {
+  return summation((s) => each(list, s));
+}
