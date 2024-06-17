7d625d04311771868f4dc16aad86deda66167440
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 18:29:46 -0400

    2024-06-17T22:29:45.994Zfunction_auto_after ceb_bible_words_score

--- public/src/ceb_bible_words_score.mjs
+++ public/src/ceb_bible_words_score.mjs
@@ -1,18 +1,17 @@
+import { list_to_lookup_value_property } from "./list_to_lookup_value_property.mjs";
 import { summation } from "./summation.mjs";
 import { bible_words_score_generic } from "./bible_words_score_generic.mjs";
 import { ceb_bible_words_definitions_map_cache } from "./ceb_bible_words_definitions_map_cache.mjs";
 import { ceb_bible_words_definitions_all_cache } from "./ceb_bible_words_definitions_all_cache.mjs";
 import { each } from "./each.mjs";
 import { ceb_bible_words_count_cache } from "./ceb_bible_words_count_cache.mjs";
-import { object_property_set } from "./object_property_set.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { object_merge } from "./object_merge.mjs";
 export async function ceb_bible_words_score() {
   let words_ceb = await ceb_bible_words_count_cache();
-  let lookup = {};
-  each(words_ceb, (d) => object_property_set(lookup, d.word, d.count));
+  let lookup = list_to_lookup_value_property(words_ceb, "word", "count");
   let map = await ceb_bible_words_definitions_map_cache();
   let definitions = await ceb_bible_words_definitions_all_cache();
   each(definitions, (d) => {
