e8505bd7a74fa5c4d7c77ace1ad2d2a787a4ec36
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 20:53:16 -0400

    2024-06-17T00:53:16.392Zfunction_auto_after terminal_data_history_clear

--- public/src/terminal_data_history_clear.mjs
+++ public/src/terminal_data_history_clear.mjs
@@ -1,1 +1,12 @@
-export function terminal_data_history_clear() {}
+import { list_size } from "./list_size.mjs";
+import { list_pop } from "./list_pop.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
+import { terminal_data_transform } from "./terminal_data_transform.mjs";
+export async function terminal_data_history_clear() {
+  await terminal_data_transform(function lambda(d) {
+    let history = object_property_initialize(d, "history", []);
+    let r = list_pop(history);
+    d.history_index = list_size(history);
+    return r;
+  });
+}
