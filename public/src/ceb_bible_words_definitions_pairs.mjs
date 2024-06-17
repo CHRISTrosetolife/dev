590d4b740e106ba3a96ba7579ddfbcbee1a01bf5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 13:18:25 -0400

    2024-06-17T17:18:24.739Zfunction_auto_after ceb_bible_words_definitions_pairs

--- public/src/ceb_bible_words_definitions_pairs.mjs
+++ public/src/ceb_bible_words_definitions_pairs.mjs
@@ -38,7 +38,7 @@ export async function ceb_bible_words_definitions_pairs() {
   });
   list_sort_property(pairs, "score");
   return {
-    pairs: list_map(pairs, object_properties_new(w, ["word", "score"])),
+    pairs: list_map(pairs, object_properties_new(w, ["word", "definitions"])),
     definitions,
   };
 }
