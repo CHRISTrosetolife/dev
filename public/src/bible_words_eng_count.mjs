8de70ab6e7f28ed7695c3d718376b4b206ee81a1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 23:24:40 -0400

    2024-06-17T03:24:39.955Zfunction_auto_after bible_words_eng_count

--- public/src/bible_words_eng_count.mjs
+++ public/src/bible_words_eng_count.mjs
@@ -1,16 +1,8 @@
+import { bible_words_count_generic } from "./bible_words_count_generic.mjs";
 import { bible_words_eng_cache } from "./bible_words_eng_cache.mjs";
-import { string_count_words } from "./string_count_words.mjs";
-import { string_empty_not_is } from "./string_empty_not_is.mjs";
-import { list_filter } from "./list_filter.mjs";
-import { string_case_lower } from "./string_case_lower.mjs";
-import { string_trim } from "./string_trim.mjs";
-import { list_map } from "./list_map.mjs";
 export async function bible_words_eng_count() {
   let all = await bible_words_eng_cache();
   let symbols = "-–,;:!?.…·'‘’“”()[]{}/&`►◄0123456789";
-  all = list_map(all, (a) => string_trim(a, symbols));
-  all = list_map(all, string_case_lower);
-  all = list_filter(all, string_empty_not_is);
-  let c = string_count_words(all);
+  return bible_words_count_generic(all, symbols);
   return c;
 }
