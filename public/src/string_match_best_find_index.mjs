0d2eeb8551264d2b3597e07f48ec5b03eab500ba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:53:05 -0400

    2024-06-17T16:53:04.604Zfunction_auto_after string_match_best_find_index

--- public/src/string_match_best_find_index.mjs
+++ public/src/string_match_best_find_index.mjs
@@ -1,5 +1,5 @@
 import ss from "string-similarity";
 export function string_match_best_find_index(search, choices) {
-  let matches = ss.findBestMatch("healed", ["edward", "sealed", "theatre"]);
-  return matches;
+  let matches = ss.findBestMatch(search, choices);
+  return matches.bestMatchIndex;
 }
