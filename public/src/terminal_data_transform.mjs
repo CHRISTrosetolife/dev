aee13699e9a254cb53bcf972c166f68f4e97745d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 20:24:55 -0400

    2024-06-17T00:24:54.507Zfunction_auto_after terminal_data_transform

--- public/src/terminal_data_transform.mjs
+++ public/src/terminal_data_transform.mjs
@@ -2,7 +2,7 @@ import { terminal_data_path } from "./terminal_data_path.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { file_json_transform_exists } from "./file_json_transform_exists.mjs";
 export async function terminal_data_transform(lambda) {
-  await file_json_transform_exists(
+  return await file_json_transform_exists(
     (d) => {
       let history = object_property_initialize(d, "history", []);
       lambda(history);
