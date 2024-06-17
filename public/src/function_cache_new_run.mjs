ebadc15fcecb8f916df3e8cd93a37a16f4eaab25
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:32:18 -0400

    2024-06-17T16:32:17.987Zfunction_auto_after function_cache_new_run

--- public/src/function_cache_new_run.mjs
+++ public/src/function_cache_new_run.mjs
@@ -1,1 +1,4 @@
-export function function_cache_new_run() {}
+import { function_cache_new } from "./function_cache_new.mjs";
+export async function function_cache_new_run() {
+  await function_cache_new(function_name);
+}
