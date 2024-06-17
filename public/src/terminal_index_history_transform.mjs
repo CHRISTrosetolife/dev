d8fe31b97705dad85bd6ebdd624f307bd1abf22e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 21:07:42 -0400

    2024-06-17T01:07:41.619Zfunction_auto_after terminal_index_history_transform

--- public/src/terminal_index_history_transform.mjs
+++ public/src/terminal_index_history_transform.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { list_get } from "./list_get.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { number_is } from "./number_is.mjs";
@@ -12,9 +11,6 @@ export async function terminal_index_history_transform(lambda) {
     let i = d.history_index;
     let history = object_property_initialize(d, "history", []);
     let r = list_get(history, i);
-    log({
-      r,
-    });
     return r;
   });
 }
