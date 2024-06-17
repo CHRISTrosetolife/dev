411eb002d8d93d616d3f6caabec56f7dbe59f5d8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 20:30:47 -0400

    2024-06-17T00:30:47.002Zfunction_auto_after terminal_history_transform

--- public/src/terminal_history_transform.mjs
+++ public/src/terminal_history_transform.mjs
@@ -2,12 +2,9 @@ import { terminal_data_path } from "./terminal_data_path.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { file_json_transform_exists } from "./file_json_transform_exists.mjs";
 export async function terminal_history_transform(lambda) {
-  return await file_json_transform_exists(
-    terminal_data_path(),
-    (d) => {
-      let history = object_property_initialize(d, "history", []);
-      return lambda(history);
-    },
-    [],
-  );
+  return await file_json_transform_exists(terminal_data_path(), lambda, []);
+  function lambda(d) {
+    let history = object_property_initialize(d, "history", []);
+    return lambda(history);
+  }
 }
