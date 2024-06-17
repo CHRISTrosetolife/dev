9be03fb5142131fb810b0fa63a76155820b18cfd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 00:01:19 -0400

    2024-06-17T04:01:19.060Zfunction_auto_after ceb_bible_words_score

--- public/src/ceb_bible_words_score.mjs
+++ public/src/ceb_bible_words_score.mjs
@@ -1,1 +1,6 @@
-export function ceb_bible_words_score() {}
+import { list_map_property } from "./list_map_property.mjs";
+import { list_sum } from "./list_sum.mjs";
+export function ceb_bible_words_score() {
+  let eng_total = list_sum(list_map_property(words_eng, "count"));
+  let ceb_total = list_sum(list_map_property(words_ceb, "count"));
+}
