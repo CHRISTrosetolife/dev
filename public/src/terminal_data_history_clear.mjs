de14849748056f39c743672120c0857340f3a346
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 20:54:02 -0400

    2024-06-17T00:54:02.363Zfunction_auto_after terminal_data_history_clear

--- public/src/terminal_data_history_clear.mjs
+++ public/src/terminal_data_history_clear.mjs
@@ -1,8 +1,9 @@
+import { list_remove_all } from "./list_remove_all.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { terminal_data_transform } from "./terminal_data_transform.mjs";
 export async function terminal_data_history_clear() {
   await terminal_data_transform(function lambda(d) {
     let history = object_property_initialize(d, "history", []);
-    list_delete_all(history);
+    list_remove_all(history);
   });
 }
