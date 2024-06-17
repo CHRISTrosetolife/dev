cb9526ff1e27fd9a3280127b2c21c0ed7b09c56c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:58:39 -0400

    2024-06-17T16:58:39.008Zfunction_auto_after ceb_bible_words_definitions_pairs

--- public/src/ceb_bible_words_definitions_pairs.mjs
+++ public/src/ceb_bible_words_definitions_pairs.mjs
@@ -7,7 +7,7 @@ export async function ceb_bible_words_definitions_pairs() {
   let ceb_scores = await ceb_bible_words_score();
   let eng_scores = await bible_words_eng_score();
   let pairs = list_adder((la) =>
-    each(definitions, (w) =>
+    each(ceb_scores, (w) =>
       each(object_property_get(w, "definitions"), (d) =>
         la([object_property_get(w, "word"), d]),
       ),
