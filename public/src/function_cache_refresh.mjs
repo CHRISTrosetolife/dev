63108bd7f4d0d9b599eb0772a2ddd2198dced268
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 13:56:52 -0400

    2024-06-17T17:56:51.368Zfunction_auto_after function_cache_refresh

--- public/src/function_cache_refresh.mjs
+++ public/src/function_cache_refresh.mjs
@@ -1,4 +1,4 @@
 import { function_cache_delete } from "./function_cache_delete.mjs";
 export async function function_cache_refresh(function_name) {
-  await function_cache_delete();
+  await function_cache_delete(function_name);
 }
