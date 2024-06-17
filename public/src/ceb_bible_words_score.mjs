cd8d4aa74bcfa39d6ed280ff3ae77164e7cb5de2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:16:10 -0400

    2024-06-17T16:16:09.959Zfunction_auto_after ceb_bible_words_score

--- public/src/ceb_bible_words_score.mjs
+++ public/src/ceb_bible_words_score.mjs
@@ -3,7 +3,6 @@ import { ceb_bible_words_definitions_all_cache } from "./ceb_bible_words_definit
 import { each } from "./each.mjs";
 import { list_summer } from "./list_summer.mjs";
 import { ceb_bible_words_count_cache } from "./ceb_bible_words_count_cache.mjs";
-import { bible_words_eng_cache } from "./bible_words_eng_cache.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -35,7 +34,5 @@ export async function ceb_bible_words_score() {
       count,
     });
   });
-  return words_ceb;
-  let words_eng = await bible_words_eng_cache();
-  let eng_total = list_summer(list_map_property(words_eng, "count"));
+  return definitions;
 }
