cde38f1ea428fd65f043dcc389f6a1b0d444bf99
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 21:35:20 -0400

    2024-06-17T01:35:20.299Z  t

--- public/src/html_code_generate.mjs
+++ public/src/html_code_generate.mjs
@@ -2,7 +2,7 @@ import { html_parse_visit_tag_single } from "./html_parse_visit_tag_single.mjs";
 import { log } from "./log.mjs";
 import { html_parse } from "./html_parse.mjs";
 import { object_properties } from "./object_properties.mjs";
-export function html_code_generate(input) {
+export function html_code_generate(tag_name, input) {
   '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />';
   log({
     input,
