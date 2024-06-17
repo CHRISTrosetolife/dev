a421eb4a78054c1149df8533fb8994126b1b16b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 20:55:30 -0400

    2024-06-17T00:55:30.394Zfunction_auto_after file_json_transform

--- public/src/file_json_transform.mjs
+++ public/src/file_json_transform.mjs
@@ -1,7 +1,9 @@
+import { json_from } from "./json_from.mjs";
+import { file_read } from "./file_read.mjs";
 import { file_overwrite_json } from "./file_overwrite_json.mjs";
-import { file_read_json } from "./file_read_json.mjs";
 export async function file_json_transform(lambda, file_path, args) {
-  let obj = await file_read_json(file_path);
+  let json = await file_read(file_path);
+  let object = json_from(json);
   let result = await lambda(obj, ...args);
   await file_overwrite_json(file_path, obj);
   return result;
