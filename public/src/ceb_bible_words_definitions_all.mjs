af70d3c838ab764a020b6a2c88fc48b4c48fa023
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 23:51:58 -0400

    2024-06-17T03:51:58.328Zfunction_auto_after ceb_bible_words_definitions_all

--- public/src/ceb_bible_words_definitions_all.mjs
+++ public/src/ceb_bible_words_definitions_all.mjs
@@ -1,7 +1,7 @@
 import { ceb_bible_words_count } from "./ceb_bible_words_count.mjs";
 import { ceb_bible_words_definitions_get } from "./ceb_bible_words_definitions_get.mjs";
 export async function ceb_bible_words_definitions_all() {
-  let c = await ceb_bible_words_count();
+  let words_ceb = await ceb_bible_words_count();
   let result = await ceb_bible_words_definitions_get(c);
   return result;
 }
