6f38a631e9c959e731d948fbf0beeea9bc8db613
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:53:17 -0400

    2024-06-17T16:53:17.109Zfunction_auto_after string_match_best_find_index

--- public/src/string_match_best_find_index.mjs
+++ public/src/string_match_best_find_index.mjs
@@ -1,5 +1,7 @@
 import ss from "string-similarity";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function string_match_best_find_index(search, choices) {
+  assert_arguments_length(arguments, 2);
   let matches = ss.findBestMatch(search, choices);
   return matches.bestMatchIndex;
 }
