984dd2091b15191aaf1cdb16139db5c6672fcaed
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 17:51:03 -0400

    2024-06-17T21:50:47.682Zfunction_auto_after ceb_audio_path

---
+++ public/src/ceb_audio_voices.mjs
@@ -0,0 +1,20 @@
+export function ceb_audio_voices() {
+  return [
+    {
+      code: "Standard-D",
+      male: true,
+    },
+    {
+      code: "Standard-A",
+      male: false,
+    },
+    {
+      code: "Standard-C",
+      male: true,
+    },
+    {
+      code: "Standard-B",
+      male: false,
+    },
+  ];
+}
