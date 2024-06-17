8c81f50bba04ff39c781b9365b35607982f9bb98
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:55:24 -0400

    2024-06-17T16:55:24.333Zfunction_auto_after ceb_bible_word_pair_score

--- public/src/ceb_bible_word_pair_score.mjs
+++ public/src/ceb_bible_word_pair_score.mjs
@@ -1,7 +1,7 @@
 import { assert } from "./assert.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_second } from "./list_second.mjs";
-export function ceb_bible_word_pair_score(pair) {
+export function ceb_bible_word_pair_score(pair, words_eng_scores) {
   assert(list_length_2, [pair]);
   let f = list_first(pair);
   let s = list_second(pair);
