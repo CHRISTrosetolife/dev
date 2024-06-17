45936eeca9ed4ca1137cf8c34601608754ea3adc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 07:01:44 -0400

    2024-06-17T11:01:40.845Zfunction_auto_after html_document_head

--- public/src/html_document_head.mjs
+++ public/src/html_document_head.mjs
@@ -1,5 +1,8 @@
+import { assert } from "./assert.mjs";
+import { undefined_not_is } from "./undefined_not_is.mjs";
 export function html_document_head() {
   let h = document.getElementsByTagName("head")[0];
+  assert(undefined_not_is, [h]);
   return {
     element: h,
   };
