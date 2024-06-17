ef2f4ed24bf99f23636be9ed07b8d38ed1f2d846
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 06:38:50 -0400

    2024-06-17T10:38:48.397Zfunction_auto_after ceb_bible_words_definitions_map

--- public/src/ceb_bible_words_definitions_map.mjs
+++ public/src/ceb_bible_words_definitions_map.mjs
@@ -1,15 +1,15 @@
+import { each_index_async } from "./each_index_async.mjs";
 import { list_size } from "./list_size.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { ceb_definition } from "./ceb_definition.mjs";
-import { each_async } from "./each_async.mjs";
 import { ceb_bible_words_count } from "./ceb_bible_words_count.mjs";
 export async function ceb_bible_words_definitions_map() {
   let words_ceb = await ceb_bible_words_count();
   let result = {};
   let s = list_size(words_ceb);
-  await each_async(words_ceb, async (word_ceb) => {
+  await each_index_async(words_ceb, async (word_ceb) => {
     let { word, definitions } = await ceb_definition(word_ceb.word);
     if (object_property_exists_not(result, word)) {
       if (list_empty_not_is(definitions)) {
