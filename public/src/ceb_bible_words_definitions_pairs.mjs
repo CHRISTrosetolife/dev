026e1b6425ccac987900d57edac94797f8ca40bd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 13:13:49 -0400

    2024-06-17T17:13:49.216Zfunction_auto_after ceb_bible_words_definitions_pairs

--- public/src/ceb_bible_words_definitions_pairs.mjs
+++ public/src/ceb_bible_words_definitions_pairs.mjs
@@ -10,6 +10,7 @@ import { list_map_property } from "./list_map_property.mjs";
 import { list_get } from "./list_get.mjs";
 import { number_is } from "./number_is.mjs";
 import { object_merge_properties } from "./object_merge_properties.mjs";
+import { list_sort } from "./list_sort.mjs";
 export async function ceb_bible_words_definitions_pairs() {
   let ceb_scores = await ceb_bible_words_score();
   let pairs = list_adder((la) =>
@@ -34,6 +35,7 @@ export async function ceb_bible_words_definitions_pairs() {
     let scores = list_map_property([eng, ceb], "score");
     pair.score = list_sum(scores);
   });
+  list_sort(pairs, (p) => p.score);
   return {
     pairs,
     definitions,
