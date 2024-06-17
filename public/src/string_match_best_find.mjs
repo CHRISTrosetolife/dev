906db82b5fc3ab9f3f9ddffbd01a20caab84fd6e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:51:30 -0400

    2024-06-17T16:51:30.515Zfunction_auto_after string_match_best_find

--- public/src/string_match_best_find.mjs
+++ public/src/string_match_best_find.mjs
@@ -1,8 +1,5 @@
+import ss from "string-similarity";
 export function string_match_best_find(search, choices) {
-  let matches = stringSimilarity.findBestMatch("healed", [
-    "edward",
-    "sealed",
-    "theatre",
-  ]);
+  let matches = ss.findBestMatch("healed", ["edward", "sealed", "theatre"]);
   return matches;
 }
