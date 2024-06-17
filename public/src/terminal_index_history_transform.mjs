39c35acc6ce76dfc8ff0f90dfcf688333950d562
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 17:38:39 -0400

    2024-06-17T21:38:39.094Zfunction_auto_after terminal_index_history_transform

--- public/src/terminal_index_history_transform.mjs
+++ public/src/terminal_index_history_transform.mjs
@@ -12,9 +12,8 @@ export async function terminal_index_history_transform(lambda) {
       return;
     }
     d.history_index = lambda(d.history_index);
-    let i = d.history_index;
     let history = object_property_initialize(d, "history", []);
-    let r = list_get(history, i);
+    let r = list_get(history, d.history_index);
     return r;
   });
 }
