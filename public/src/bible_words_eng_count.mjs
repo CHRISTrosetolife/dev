4772154e6f5b45df3399374eab541d84d89d70a7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-16 23:23:22 -0400

    2024-06-17T03:23:21.606Zfunction_auto_after bible_words_eng_count

--- public/src/bible_words_eng_count.mjs
+++ public/src/bible_words_eng_count.mjs
@@ -1,3 +1,4 @@
+import { bible_words_eng_cache } from "./bible_words_eng_cache.mjs";
 import { string_symbols_multiple } from "./string_symbols_multiple.mjs";
 import { string_count_words } from "./string_count_words.mjs";
 import { string_empty_not_is } from "./string_empty_not_is.mjs";
@@ -5,9 +6,8 @@ import { list_filter } from "./list_filter.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { string_trim } from "./string_trim.mjs";
 import { list_map } from "./list_map.mjs";
-import { ceb_bible_words_cache } from "./ceb_bible_words_cache.mjs";
 export async function bible_words_eng_count() {
-  let all = await ceb_bible_words_cache();
+  let all = await bible_words_eng_cache();
   return string_symbols_multiple(all);
   let symbols = "_-–—,;:!?.'‘’\"“”()[]}¶/`¯|€0123456789";
   all = list_map(all, (a) => string_trim(a, symbols));
