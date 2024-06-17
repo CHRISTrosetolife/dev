65fddc78beea36901f79abbca9f0a890dd810148
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-17 12:30:05 -0400

    2024-06-17T16:30:05.390Zfunction_auto_after number_max_list_map_property

--- public/src/number_max_list_map_property.mjs
+++ public/src/number_max_list_map_property.mjs
@@ -1,5 +1,7 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { number_max_list_map } from "./number_max_list_map.mjs";
 export function number_max_list_map_property(definitions, property) {
-  number_max_list_map(definitions, (d) => object_property_get(d, property));
+  return number_max_list_map(definitions, (d) =>
+    object_property_get(d, property),
+  );
 }
