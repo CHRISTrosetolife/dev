145161a782339b974a91d108785aab0e42580f5e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 13:44:37 -0400

    2024-06-18T17:44:36.964Zfunction_auto_after graph_path_shortest_neighbors

--- public/src/graph_path_shortest.mjs
+++ public/src/graph_path_shortest.mjs
@@ -1,19 +1,8 @@
-import { list_shuffle } from "./list_shuffle.mjs";
-import { list_reverse } from "./list_reverse.mjs";
-import { list_map } from "./list_map.mjs";
-import { list_includes_not } from "./list_includes_not.mjs";
-import { object_property_exists_not } from "./object_property_exists_not.mjs";
-import { list_remove_first } from "./list_remove_first.mjs";
-import { list_empty_not_is } from "./list_empty_not_is.mjs";
-import { list_add_multiple } from "./list_add_multiple.mjs";
+import { graph_path_shortest_neighbors } from "./graph_path_shortest_neighbors.mjs";
 import { graph_neighbors } from "./graph_neighbors.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { each_index } from "./each_index.mjs";
 import { list_adder } from "./list_adder.mjs";
-import { object_property_set } from "./object_property_set.mjs";
-import { list_index } from "./list_index.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { list_filter } from "./list_filter.mjs";
 export function graph_path_shortest(vertices, edge_lambda, from, to) {
   assert_arguments_length(arguments, 4);
   let edges = list_adder((la) =>
@@ -29,44 +18,5 @@ export function graph_path_shortest(vertices, edge_lambda, from, to) {
     ),
   );
   let neighbors_get = (v) => graph_neighbors(edges, v);
-  let remaining = [
-    {
-      current: from,
-      previous: null,
-    },
-  ];
-  let visited = {};
-  let result = null;
-  while (list_empty_not_is(remaining)) {
-    let r = list_remove_first(remaining);
-    let { current } = r;
-    object_property_set(visited, list_index(vertices, current), r);
-    if (current === to) {
-      result = list_adder((la) => {
-        let c = current;
-        while (c !== null) {
-          la(c);
-          let i = list_index(vertices, c);
-          let v = object_property_get(visited, i);
-          c = v.previous;
-        }
-      });
-      list_reverse(result);
-      break;
-    }
-    let neighbors = neighbors_get(current);
-    let neighbors_new = list_filter(
-      neighbors,
-      (n) =>
-        object_property_exists_not(visited, list_index(vertices, n)) &&
-        list_includes_not(remaining, n),
-    );
-    list_shuffle(neighbors_new);
-    let mapped = list_map(neighbors_new, (n) => ({
-      current: n,
-      previous: current,
-    }));
-    list_add_multiple(remaining, mapped);
-  }
-  return result;
+  return graph_path_shortest_neighbors(from, vertices, to, neighbors_get);
 }
