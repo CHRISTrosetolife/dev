21014fd4c7f74c352740db1430b2b1157b63f3b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 11:58:56 -0400

    2024-06-18T15:58:55.753Zfunction_auto_after audio_language

--- public/src/audio_language.mjs
+++ public/src/audio_language.mjs
@@ -1,5 +1,4 @@
 import { function_run } from "./function_run.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function audio_language(language) {
   let function_name = string_combine_multiple(
@@ -7,6 +6,5 @@ export async function audio_language(language) {
     "_",
     language,
   );
-  await function_run(function_name, []);
-  return object_property_get(choices, language);
+  return await function_run(function_name, []);
 }
