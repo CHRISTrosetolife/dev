2cb970b5fdb7f1c92860ca9a970b875a913c372f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 18:07:38 -0400

    2024-06-18T22:07:38.200Zfunction_auto_after performance_start

--- public/src/performance_start.mjs
+++ public/src/performance_start.mjs
@@ -1,5 +1,5 @@
 import { performance_next } from "./performance_next.mjs";
-export function performance_start() {
+export function performance_start(function_name) {
   let p = {};
   p.times = [];
   performance_next(p);
