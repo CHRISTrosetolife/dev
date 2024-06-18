c7a61325b7eb8ec7253d46e22bd49aaa7c362e5f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 18:11:48 -0400

    2024-06-18T22:11:47.849Zfunction_auto_after performance_start

--- public/src/performance_start.mjs
+++ public/src/performance_start.mjs
@@ -1,7 +1,9 @@
 import { global_get } from "./global_get.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
 import { performance_next } from "./performance_next.mjs";
 export function performance_start(category) {
   let g = global_get();
+  object_property_initialize(g, performance_start.name, {});
   let p = {};
   p.times = [];
   performance_next(p);
