caa1467ea62a0d97fd7983e3a160f2bd3dd87010
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 11:59:51 -0400

    2024-06-18T15:59:50.885Zfunction_auto_after audio_path

--- public/src/audio_path.mjs
+++ public/src/audio_path.mjs
@@ -2,9 +2,9 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { audio_language } from "./audio_language.mjs";
 import { gcloud_audio_path } from "./gcloud_audio_path.mjs";
 import { list_get } from "./list_get.mjs";
-export function audio_path(language, voice_index, text) {
+export async function audio_path(language, voice_index, text) {
   assert_arguments_length(arguments, 3);
-  let { language_code, voices } = audio_language(language);
+  let { language_code, voices } = await audio_language(language);
   let { code } = list_get(voices, voice_index);
   let file_path = gcloud_audio_path(language_code, text, code);
   return file_path;
