d9c40f41617b5c0a30418e9416b031680abe1794
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 18:17:15 -0400

    2024-06-18T22:17:15.217Zfunction_auto_after each_pairs

--- public/src/each_pairs.mjs
+++ public/src/each_pairs.mjs
@@ -1,13 +1,14 @@
+import { each_pairs_index } from "./each_pairs_index.mjs";
 import { add_1 } from "./add_1.mjs";
 import { list_get } from "./list_get.mjs";
 import { list_index_last } from "./list_index_last.mjs";
 import { range } from "./range.mjs";
 export function each_pairs(list, lambda) {
+  each_pairs_index(list, (current, next, index) => lambda(current, next));
   let last_index = list_index_last(list);
   for (let i of range(last_index)) {
     let i_next = add_1(i);
     let current = list_get(list, i);
     let next = list_get(list, i_next);
-    lambda(current, next);
   }
 }
