c73f58c55c9e26e1e8b07e49c27406628b1e8d07
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 18:31:38 -0400

    2024-06-18T22:31:37.819Zfunction_auto_after graph_path_shortest_neighbors

--- public/src/graph_path_shortest_neighbors.mjs
+++ public/src/graph_path_shortest_neighbors.mjs
@@ -32,7 +32,7 @@ export function graph_path_shortest_neighbors(id_get, neighbors_get, from, to) {
         let c = current;
         while (c !== null) {
           la(c);
-          let i = id_get(c);
+          let i = c.id;
           let v = object_property_get(visited, i);
           c = v.previous;
         }
