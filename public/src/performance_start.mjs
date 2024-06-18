cdcc576bcc7a421449be02f18da06087c03e7d57
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 18:10:52 -0400

    2024-06-18T22:10:51.551Zfunction_auto_after performance_start

--- public/src/performance_start.mjs
+++ public/src/performance_start.mjs
@@ -1,5 +1,7 @@
+import { global_get } from "./global_get.mjs";
 import { performance_next } from "./performance_next.mjs";
 export function performance_start(category) {
+  global_get();
   let p = {};
   p.times = [];
   performance_next(p);
