8ad0fb94214b795bff5c43359c6fa0d231e4f67e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 12:08:59 -0400

    2024-06-18T16:08:59.204Zfunction_auto_after string_combine_multiple

--- public/src/string_combine_multiple.mjs
+++ public/src/string_combine_multiple.mjs
@@ -1,7 +1,9 @@
 import { assert } from "./assert.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { list_is } from "./list_is.mjs";
 import { string_combine } from "./string_combine.mjs";
 export function string_combine_multiple(list) {
+  assert_arguments_length(arguments, 1);
   assert(list_is, [list]);
   let result = "";
   for (let l of list) {
