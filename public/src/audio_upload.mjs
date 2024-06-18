e190dd8a6100a9db686e47357c6909f39200fd66
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 12:11:05 -0400

    2024-06-18T16:11:04.959Zfunction_auto_after audio_upload

--- public/src/audio_upload.mjs
+++ public/src/audio_upload.mjs
@@ -1,3 +1,5 @@
+import { string_is } from "./string_is.mjs";
+import { assert } from "./assert.mjs";
 import { audio_path } from "./audio_path.mjs";
 import { audio_language } from "./audio_language.mjs";
 import { storage_upload_file } from "./storage_upload_file.mjs";
@@ -6,6 +8,7 @@ import { log } from "./log.mjs";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 export async function audio_upload(language, text) {
+  assert(string_is, [text]);
   let { language_code, voices } = await audio_language(language);
   await each_index_async(voices, async (voice, voice_index) => {
     let file_path = await audio_path(language, voice_index, text);
