e022bbb22e7ac46b1ff6c493eb2e8b7f58071a6d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 11:58:02 -0400

    2024-06-18T15:58:01.656Zfunction_auto_after audio_language

--- public/src/audio_language.mjs
+++ public/src/audio_language.mjs
@@ -1,6 +1,12 @@
 import { audio_language_ceb } from "./audio_language_ceb.mjs";
 import { object_property_get } from "./object_property_get.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function audio_language(language) {
+  let function_name = string_combine_multiple(
+    audio_language.name,
+    "_",
+    language,
+  );
   let choices = {
     ceb: () => audio_language_ceb(),
   };
