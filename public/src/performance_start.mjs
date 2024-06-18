bacf5e0302378f5bf17568e49271f7cd57948ea7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 18:11:53 -0400

    2024-06-18T22:11:52.715Zfunction_auto_after performance_start

--- public/src/performance_start.mjs
+++ public/src/performance_start.mjs
@@ -3,7 +3,7 @@ import { object_property_initialize } from "./object_property_initialize.mjs";
 import { performance_next } from "./performance_next.mjs";
 export function performance_start(category) {
   let g = global_get();
-  object_property_initialize(g, performance_start.name, {});
+  let categories = object_property_initialize(g, performance_start.name, {});
   let p = {};
   p.times = [];
   performance_next(p);
