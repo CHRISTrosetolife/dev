ead3bd392cf8321344cd6f2b7b1e95aa56dd37dc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 11:46:29 -0400

    2024-06-18T15:46:29.384Zfunction_auto_after ceb_audio_upload

--- public/src/ceb_audio_upload.mjs
+++ public/src/ceb_audio_upload.mjs
@@ -1,14 +1,12 @@
-import { audio_voices_ceb } from "./audio_voices_ceb.mjs";
+import { audio_language_ceb } from "./audio_language_ceb.mjs";
 import { storage_upload_file } from "./storage_upload_file.mjs";
 import { gcloud_tts } from "./gcloud_tts.mjs";
 import { log } from "./log.mjs";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 import { ceb_audio_path } from "./ceb_audio_path.mjs";
 import { each_index_async } from "./each_index_async.mjs";
-import { ceb_audio_language_code } from "./ceb_audio_language_code.mjs";
 export async function ceb_audio_upload(text) {
-  let language_code = ceb_audio_language_code();
-  let voices = audio_voices_ceb();
+  let { language_code, voices } = audio_language_ceb();
   await each_index_async(voices, async (voice, voice_index) => {
     let file_path = ceb_audio_path(voice_index, text);
     let output_path = folder_gitignore_path(file_path);
