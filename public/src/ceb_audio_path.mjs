bed4dec6233198d96d8ad931da4ec13d72736135
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 11:56:54 -0400

    2024-06-18T15:56:54.221Zfunction_auto_after ceb_audio_path

--- public/src/ceb_audio_path.mjs
+++ public/src/ceb_audio_path.mjs
@@ -1,8 +1,8 @@
-import { audio_language_ceb } from "./audio_language_ceb.mjs";
+import { audio_language } from "./audio_language.mjs";
 import { gcloud_audio_path } from "./gcloud_audio_path.mjs";
 import { list_get } from "./list_get.mjs";
 export function ceb_audio_path(voice_index, text) {
-  let { language_code, voices } = audio_language_ceb();
+  let { language_code, voices } = audio_language("ceb");
   let { code } = list_get(voices, voice_index);
   let file_path = gcloud_audio_path(language_code, text, code);
   return file_path;
