94884cd2eb0b6ac7b93fa7988d637f3dba0c7684
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 14:18:37 -0400

    2024-06-18T18:18:36.712Zfunction_auto_after graph_path_shortest_neighbors

--- public/src/graph_path_shortest_neighbors.mjs
+++ public/src/graph_path_shortest_neighbors.mjs
@@ -6,7 +6,6 @@ import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { list_reverse } from "./list_reverse.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_adder } from "./list_adder.mjs";
-import { list_index } from "./list_index.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { list_size } from "./list_size.mjs";
 import { list_get } from "./list_get.mjs";
@@ -41,7 +40,7 @@ export function graph_path_shortest_neighbors(id_get, neighbors_get, from, to) {
     let neighbors = neighbors_get(current);
     list_shuffle(neighbors);
     each(neighbors, (n) => {
-      let en = object_property_exists_not(visited, list_index(vertices, n));
+      let en = object_property_exists_not(visited, id_get(n));
       let lin = list_includes_not(remaining, n);
       if (en && lin) {
         let m = {
