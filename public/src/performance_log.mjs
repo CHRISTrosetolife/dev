b5006b61799b04bb0cbe2660f3d437eeac500232
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 18:10:35 -0400

    2024-06-18T22:10:34.520Zfunction_auto_after performance_log

--- public/src/performance_log.mjs
+++ public/src/performance_log.mjs
@@ -1,9 +1,7 @@
 import { log } from "./log.mjs";
 import { each_pairs } from "./each_pairs.mjs";
 import { list_adder } from "./list_adder.mjs";
-import { performance_next } from "./performance_next.mjs";
-export function performance_log(p) {
-  performance_next(p);
+export function performance_log() {
   let r = list_adder((la) => {
     each_pairs(p.times, (a, b) => {
       let d = b.time - a.time;
