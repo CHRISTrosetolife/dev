adae660f1c640eac463f8f5ea117b7bdbc7ec832
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 12:05:03 -0400

    2024-06-18T16:05:03.353Zfunction_auto_after audio_language_ceb

--- public/src/audio_language_ceb.mjs
+++ public/src/audio_language_ceb.mjs
@@ -1,1 +1,23 @@
-export function audio_language_ceb() {}
+export function audio_language_ceb() {
+  return {
+    language_code: "fil-PH",
+    voices: [
+      {
+        code: "Standard-D",
+        male: true,
+      },
+      {
+        code: "Standard-A",
+        male: false,
+      },
+      {
+        code: "Standard-C",
+        male: true,
+      },
+      {
+        code: "Standard-B",
+        male: false,
+      },
+    ],
+  };
+}
