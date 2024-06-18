0c44075d42070018dcaf29e209676254f36e5bdd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 11:56:22 -0400

    2024-06-18T15:56:21.951Zfunction_auto_after audio_language_ceb

--- public/src/audio_language_ceb.mjs
+++ public/src/audio_language_ceb.mjs
@@ -1,6 +1,4 @@
-import { audio_voices_ceb } from "./audio_voices_ceb.mjs";
 export function audio_language_ceb() {
-  let voices = audio_voices_ceb();
   return {
     language_code: "fil-PH",
     voices: [
