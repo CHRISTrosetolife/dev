65df597c1ee80b323b424c054eb024666d8bbbfa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 12:05:10 -0400

    2024-06-18T16:05:10.108Z  t

---
+++ public/src/audio_language_en.mjs
@@ -0,0 +1,23 @@
+export function audio_language_en() {
+  return {
+    language_code: "fil-PH",
+    voices: [
+      {
+        code: "Standard-D",
+        male: true,
+      },
+      {
+        code: "Standard-A",
+        male: false,
+      },
+      {
+        code: "Standard-C",
+        male: true,
+      },
+      {
+        code: "Standard-B",
+        male: false,
+      },
+    ],
+  };
+}
