75a996a7f653be540abd273cda4d6f9199d56e79
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 11:49:44 -0400

    2024-06-18T15:49:44.524Zfunction_auto_after audio_path

--- public/src/audio_path.mjs
+++ public/src/audio_path.mjs
@@ -1,7 +1,9 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { audio_language } from "./audio_language.mjs";
 import { gcloud_audio_path } from "./gcloud_audio_path.mjs";
 import { list_get } from "./list_get.mjs";
 export function audio_path(language, voice_index, text) {
+  assert_arguments_length(arguments, 3);
   let { language_code, voices } = audio_language(language);
   let { code } = list_get(voices, voice_index);
   let file_path = gcloud_audio_path(language_code, text, code);
