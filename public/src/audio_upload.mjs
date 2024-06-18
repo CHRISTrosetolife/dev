6340f5bf89c88e0ca57701aaa87dc75e3b423c05
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 11:51:08 -0400

    2024-06-18T15:51:07.730Zfunction_auto_after audio_upload

--- public/src/audio_upload.mjs
+++ public/src/audio_upload.mjs
@@ -5,10 +5,10 @@ import { gcloud_tts } from "./gcloud_tts.mjs";
 import { log } from "./log.mjs";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 import { each_index_async } from "./each_index_async.mjs";
-export async function audio_upload(text) {
-  let { language_code, voices } = audio_language("ceb");
+export async function audio_upload(language, text) {
+  let { language_code, voices } = audio_language(language);
   await each_index_async(voices, async (voice, voice_index) => {
-    let file_path = audio_path("ceb", voice_index, text);
+    let file_path = audio_path(language, voice_index, text);
     let output_path = folder_gitignore_path(file_path);
     let { code, male } = voice;
     let { created } = await gcloud_tts(
