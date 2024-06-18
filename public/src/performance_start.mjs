1cf19bda879fff463fb76e53b37442e00630fe60
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 18:07:52 -0400

    2024-06-18T22:07:51.471Zfunction_auto_after performance_start

--- public/src/performance_start.mjs
+++ public/src/performance_start.mjs
@@ -1,5 +1,5 @@
 import { performance_next } from "./performance_next.mjs";
-export function performance_start(function_name) {
+export function performance_start(category) {
   let p = {};
   p.times = [];
   performance_next(p);
