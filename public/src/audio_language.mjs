08f65a1f0b6fc34a39d517f3686bb2164b2306e7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 11:47:45 -0400

    2024-06-18T15:47:45.355Zfunction_auto_after audio_language

--- public/src/audio_language.mjs
+++ public/src/audio_language.mjs
@@ -1,1 +1,6 @@
-export function audio_language() {}
+import { audio_language_ceb } from "./audio_language_ceb.mjs";
+export function audio_language(language) {
+  let choices = {
+    ceb: () => audio_language_ceb(),
+  };
+}
