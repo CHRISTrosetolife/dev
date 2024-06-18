524fd707612ea4dc11d4568ea800e9d58d84d3a3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 18:18:25 -0400

    2024-06-18T22:18:24.737Zfunction_auto_after performance_log

--- public/src/performance_log.mjs
+++ public/src/performance_log.mjs
@@ -2,8 +2,6 @@ import { each_pairs_index } from "./each_pairs_index.mjs";
 import { each } from "./each.mjs";
 import { performance_start_categories } from "./performance_start_categories.mjs";
 import { log } from "./log.mjs";
-import { each_pairs } from "./each_pairs.mjs";
-import { list_adder } from "./list_adder.mjs";
 import { each_object } from "./each_object.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 export function performance_log() {
@@ -16,12 +14,6 @@ export function performance_log() {
         object_property_initialize();
         la(d);
       });
-    });
-  });
-  let r = list_adder((la) => {
-    each_pairs(p.times, (a, b) => {
-      let d = b - a;
-      la(d);
     });
   });
   log(r);
