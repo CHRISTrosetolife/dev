45ea0c66578d90811b3a68307eb81e353dee7f53
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 13:11:58 -0400

    2024-06-18T17:11:57.935Zfunction_auto_after app_gs_map_cell

--- public/src/app_gs_map_cell.mjs
+++ public/src/app_gs_map_cell.mjs
@@ -20,6 +20,7 @@ import { app_gs_z_indexes } from "./app_gs_z_indexes.mjs";
 import { list_skip } from "./list_skip.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { list_concat_multiple } from "./list_concat_multiple.mjs";
+import { list_add } from "./list_add.mjs";
 export function app_gs_map_cell(map, map_c, player_overlay, tile) {
   let z_indexes = app_gs_z_indexes();
   let clicker = html_div(map_c);
@@ -47,6 +48,9 @@ export function app_gs_map_cell(map, map_c, player_overlay, tile) {
   html_on_click(clicker, async () => {
     let p = {};
     p.times = [];
+    list_add(p.times, {
+      time: new Date(),
+    });
     let w = app_gs_overlays_any_wall(tile);
     if (w) {
       return;
