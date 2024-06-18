b205e81d847695256950746eeee8694eddc6e8fe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 11:46:24 -0400

    2024-06-18T15:46:23.942Zfunction_auto_after ceb_audio_path

--- public/src/ceb_audio_path.mjs
+++ public/src/ceb_audio_path.mjs
@@ -1,10 +1,8 @@
-import { audio_voices_ceb } from "./audio_voices_ceb.mjs";
-import { ceb_audio_language_code } from "./ceb_audio_language_code.mjs";
+import { audio_language_ceb } from "./audio_language_ceb.mjs";
 import { gcloud_audio_path } from "./gcloud_audio_path.mjs";
 import { list_get } from "./list_get.mjs";
 export function ceb_audio_path(voice_index, text) {
-  let language_code = ceb_audio_language_code();
-  let voices = audio_voices_ceb();
+  let { language_code, voices } = audio_language_ceb();
   let { code } = list_get(voices, voice_index);
   let file_path = gcloud_audio_path(language_code, text, code);
   return file_path;
