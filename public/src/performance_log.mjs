86f787f95d7ca655a7250f67f87d089f6a7ce3d3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 18:13:35 -0400

    2024-06-18T22:13:34.748Zfunction_auto_after performance_log

--- public/src/performance_log.mjs
+++ public/src/performance_log.mjs
@@ -2,8 +2,10 @@ import { performance_start_categories } from "./performance_start_categories.mjs
 import { log } from "./log.mjs";
 import { each_pairs } from "./each_pairs.mjs";
 import { list_adder } from "./list_adder.mjs";
+import { each_object } from "./each_object.mjs";
 export function performance_log() {
   let categories = performance_start_categories();
+  each_object(categories, (category, ps) => {});
   let r = list_adder((la) => {
     each_pairs(p.times, (a, b) => {
       let d = b - a;
