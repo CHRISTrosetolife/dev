1638d34cc1de7df851d43de75893c92e4ccfd8d0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 09:06:24 -0400

    2024-06-19T13:06:23.426Zfunction_auto_after app_language_group_path

--- public/src/app_language_group_path.mjs
+++ public/src/app_language_group_path.mjs
@@ -1,3 +1,5 @@
-export function app_language_group_path(group_index) {
-  return `ceb/group/${group_index}.json`;
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
+export function app_language_group_path(from, to, group_index) {
+  assert_arguments_length(arguments, 3);
+  return `${from}/to/${to}/group/${group_index}.json`;
 }
