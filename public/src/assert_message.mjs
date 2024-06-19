cac90e0986178caddc7c253c29cf22940f459c2e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 10:12:38 -0400

    2024-06-19T14:12:38.144Zfunction_auto_after assert_message

--- public/src/assert_message.mjs
+++ public/src/assert_message.mjs
@@ -1,7 +1,12 @@
 import { assert_message_string } from "./assert_message_string.mjs";
 import { json_to } from "./json_to.mjs";
+import { string_length } from "./string_length.mjs";
+import { string_take } from "./string_take.mjs";
 export function assert_message(fn, args, message_get) {
   return assert_message_string(fn, args, () => {
     let j = json_to(message_get());
+    if (string_length(j > 1000)) {
+      return string_take(j, 1000);
+    }
   });
 }
