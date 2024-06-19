41118a8e00b6dc6dda8c411bcef9bb32b08b1c8b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 10:22:38 -0400

    2024-06-19T14:22:36.102Zfunction_auto_after app_language_word_button_audio_none

--- public/src/app_language_word_button_audio_none.mjs
+++ public/src/app_language_word_button_audio_none.mjs
@@ -2,7 +2,9 @@ import { app_language_word_style } from "./app_language_word_style.mjs";
 import { app_language_audio } from "./app_language_audio.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function app_language_word_button_audio_none(root, langauge, text) {
+  assert_arguments_length(arguments, 3);
   let button = html_button_width_full_text_click(
     root,
     string_combine("👂", text),
