fdae94f556476af8455a083df0396c134a0c2950
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 10:25:43 -0400

    2024-06-19T14:25:39.322Zfunction_auto_after app_language_word_button

--- public/src/app_language_word_button.mjs
+++ public/src/app_language_word_button.mjs
@@ -1,7 +1,9 @@
 import { app_language_word_button_audio_none } from "./app_language_word_button_audio_none.mjs";
 import { app_language_audio } from "./app_language_audio.mjs";
-export function app_language_word_button(root, cebuano) {
-  let button = app_language_word_button_audio_none(root, cebuano);
-  (async () => await app_language_audio(cebuano))();
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
+export function app_language_word_button(root, language, text) {
+  assert_arguments_length(arguments, 3);
+  let button = app_language_word_button_audio_none(root, language, text);
+  (async () => await app_language_audio(text))();
   return button;
 }
