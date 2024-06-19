536cd300247aae03e72d426aec382e594f1c4dbc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 17:41:11 -0400

    2024-06-19T21:41:10.949Zfunction_auto_after command_line_generic

--- public/src/command_line_generic.mjs
+++ public/src/command_line_generic.mjs
@@ -2,11 +2,14 @@ import { list_first_remaining } from "./list_first_remaining.mjs";
 import { log_error } from "./log_error.mjs";
 import { log } from "./log.mjs";
 import { import_node } from "./import_node.mjs";
+import { string_split_space } from "./string_split_space.mjs";
 export async function command_line_generic(command, options) {
   let c = await import_node("child_process");
   let { spawn } = c;
   return await new Promise((resolve) => {
-    let { first, remaining } = list_first_remaining(command);
+    let { first, remaining } = list_first_remaining(
+      string_split_space(command),
+    );
     let child = spawn(first, remaining);
     child.stdout.setEncoding("utf8");
     child.stdout.on("data", function (data) {
