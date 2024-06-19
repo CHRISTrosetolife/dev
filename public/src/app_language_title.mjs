3c51da99fef5c531004c3e7b03e2fcd7d26e219f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 11:11:47 -0400

    2024-06-19T15:11:47.051Zfunction_auto_after app_language_title

--- public/src/app_language_title.mjs
+++ public/src/app_language_title.mjs
@@ -4,7 +4,9 @@ import { app_language_atom_title } from "./app_language_atom_title.mjs";
 import { list_get_or_last } from "./list_get_or_last.mjs";
 import { list_get } from "./list_get.mjs";
 import { storage_local_get } from "./storage_local_get.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function app_language_title(context) {
+  assert_arguments_length(arguments, 1);
   let { left, right } = storage_local_get(context.app_fn, "position");
   let gl = list_get(context.group, left);
   let gr = list_get_or_last(context.group, right);
