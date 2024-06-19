b3cd7b79720c2820fe9ecc71ca948172460623bc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 10:50:30 -0400

    2024-06-19T14:50:29.893Zfunction_auto_after app_language_text

--- public/src/app_language_text.mjs
+++ public/src/app_language_text.mjs
@@ -1,5 +1,7 @@
 import { audio_language } from "./audio_language.mjs";
 import { function_run_plugin } from "./function_run_plugin.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 export async function app_language_text(language, text) {
   let texts = await function_run_plugin(audio_language, language);
+  return object_property_get(texts, text);
 }
