c209a709026743239b5a4ca3071eb5cc6703eded
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 00:08:31 -0400

    2024-06-19T04:08:30.045Zfunction_auto_after log

--- public/src/log.mjs
+++ public/src/log.mjs
@@ -1,5 +1,7 @@
+import { object_property_initialize } from "./object_property_initialize.mjs";
 import { global_get } from "./global_get.mjs";
 export function log(message) {
   let g = global_get();
+  object_property_initialize(g, log.name, []);
   console.log(message);
 }
