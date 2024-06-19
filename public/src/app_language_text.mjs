fa4117bf7ae2007dcd14945b7944e85730bdc4f9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 10:49:34 -0400

    2024-06-19T14:49:33.391Zfunction_auto_after app_language_text

--- public/src/app_language_text.mjs
+++ public/src/app_language_text.mjs
@@ -1,1 +1,5 @@
-export function app_language_text(language, text) {}
+import { audio_language } from "./audio_language.mjs";
+import { function_run_plugin } from "./function_run_plugin.mjs";
+export async function app_language_text(language, text) {
+  return await function_run_plugin(audio_language, language);
+}
