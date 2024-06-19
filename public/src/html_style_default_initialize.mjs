ba2cf7e5471dddcdb71e8850670916a92ef940b9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 21:13:50 -0400

    2024-06-19T01:13:50.002Zfunction_auto_after html_style_default_initialize

--- public/src/html_style_default_initialize.mjs
+++ public/src/html_style_default_initialize.mjs
@@ -1,3 +1,6 @@
+import { html_style_border_box_get } from "./html_style_border_box_get.mjs";
+import { html_style_default } from "./html_style_default.mjs";
+import { object_merge } from "./object_merge.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { html_style_button_default_initialize } from "./html_style_button_default_initialize.mjs";
 import { html_style_default_font_size_value } from "./html_style_default_font_size_value.mjs";
@@ -6,6 +9,7 @@ import { html_style_sans_serif } from "./html_style_sans_serif.mjs";
 export function html_style_default_initialize() {
   let root = html_document_body();
   html_style_sans_serif(root);
+  object_merge(html_style_default(), html_style_border_box_get());
   html_style_default_font_size(html_style_default_font_size_value());
   html_style_button_default_initialize();
   return root;
