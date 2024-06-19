ce5f617a81ccb9fd2d63de0d67ae471293e9f6d6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 17:31:31 -0400

    2024-06-19T21:31:30.176Zfunction_auto_after command_line_generic

--- public/src/command_line_generic.mjs
+++ public/src/command_line_generic.mjs
@@ -1,10 +1,11 @@
+import { spawn } from "./spawn.mjs";
 import { error } from "./error.mjs";
 import { import_node } from "./import_node.mjs";
 export async function command_line_generic(command, options) {
   let c = await import_node("child_process");
-  let { exec } = c;
-  return await new Promise((resolve) => {
-    exec(command, options, (error, stdout, stderr) => {
+  let { spawn } = c;
+  return await new Promise(async (resolve) => {
+    await spawn(command, options, (error, stdout, stderr) => {
       resolve({
         error,
         stdout,
