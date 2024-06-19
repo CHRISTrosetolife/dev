1caa3fbf066f381d9c7b1c85feeab35b6a1beb47
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 10:49:16 -0400

    2024-06-19T14:49:13.707Zfunction_auto_after audio_language

--- public/src/audio_language.mjs
+++ public/src/audio_language.mjs
@@ -1,4 +1,4 @@
 import { function_run_plugin } from "./function_run_plugin.mjs";
 export async function audio_language(language) {
-  return function_run_plugin(audio_language, language);
+  return await function_run_plugin(audio_language, language);
 }
