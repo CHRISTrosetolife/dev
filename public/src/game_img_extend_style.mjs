f9e5df5dec1a2e9578a4f01fa6185490666bfe92
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 20:34:03 -0400

    2024-06-19T00:34:02.022Zfunction_auto_after game_img_extend_style

--- public/src/game_img_extend_style.mjs
+++ public/src/game_img_extend_style.mjs
@@ -1,7 +1,6 @@
 import { subtract } from "./subtract.mjs";
 import { game_img_position } from "./game_img_position.mjs";
 import { html_style } from "./html_style.mjs";
-import { game_img_style_transition } from "./game_img_style_transition.mjs";
 import { html_style_height } from "./html_style_height.mjs";
 import { game_tile_units_css } from "./game_tile_units_css.mjs";
 import { html_style_width } from "./html_style_width.mjs";
@@ -15,7 +14,6 @@ export function game_img_extend_style(
 ) {
   html_style_width(image, game_tile_units_css(1 + 2 * x_extend));
   html_style_height(image, game_tile_units_css(1 + 2 * y_extend));
-  game_img_style_transition(image);
   html_style(image, {
     position: "absolute",
     "z-index": z_index,
