1d239e83a5bdb8ed5a15e78f724e203731d3d921
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 20:43:48 -0400

    2024-06-19T00:43:48.383Zfunction_auto_after app_gs_map_extends

--- public/src/app_gs_map_extends.mjs
+++ public/src/app_gs_map_extends.mjs
@@ -5,10 +5,10 @@ export function app_gs_map_extends() {
   let w = window.innerWidth;
   let h = window.innerHeight;
   let size_px = game_tile_size_px();
-  let x_count = ceiling(w / size_px);
-  let y_count = ceiling(h / size_px);
-  let x_extend = floor(x_count / 2);
-  let y_extend = floor(y_count / 2);
+  let x_count = w / size_px;
+  let y_count = h / size_px;
+  let x_extend = floor(ceiling(x_count) / 2);
+  let y_extend = floor(ceiling(y_count) / 2);
   return {
     y_extend,
     x_extend,
