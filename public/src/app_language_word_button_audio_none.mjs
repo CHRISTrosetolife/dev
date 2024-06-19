6d022b163f6e53eded87602fdf1475d75d114626
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 10:22:08 -0400

    2024-06-19T14:22:07.869Zfunction_auto_after app_language_word_button_audio_none

--- public/src/app_language_word_button_audio_none.mjs
+++ public/src/app_language_word_button_audio_none.mjs
@@ -2,7 +2,7 @@ import { app_language_word_style } from "./app_language_word_style.mjs";
 import { app_language_audio } from "./app_language_audio.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
-export function app_language_word_button_audio_none(root, cebuano) {
+export function app_language_word_button_audio_none(root, langauge, cebuano) {
   let button = html_button_width_full_text_click(
     root,
     string_combine("👂", cebuano),
