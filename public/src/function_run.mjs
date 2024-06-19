a20b82224a2a42f5b398cb76dfbccd5b85709425
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 08:22:35 -0400

    2024-06-19T12:22:34.827Zfunction_auto_after function_run

--- public/src/function_run.mjs
+++ public/src/function_run.mjs
@@ -4,14 +4,16 @@ import { function_import } from "./function_import.mjs";
 import { web_is } from "./web_is.mjs";
 export async function function_run(function_name, args) {
   let result;
-  if (web_is()) {
-    result = await http_post(`http://localhost:${server_port()}/`, {
-      function_name,
-      args,
-    });
-  } else {
-    let imported_function = await function_import(function_name);
-    result = await imported_function(...args);
+  if (0) {
+    if (web_is()) {
+      result = await http_post(`http://localhost:${server_port()}/`, {
+        function_name,
+        args,
+      });
+      return;
+    }
   }
+  let imported_function = await function_import(function_name);
+  result = await imported_function(...args);
   return result;
 }
