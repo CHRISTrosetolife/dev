585053965f1d4bcab3225708f21ef77bd98837bf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 17:00:49 -0400

    2024-06-19T21:00:48.695Zfunction_auto_after app_language_refresh_node

--- public/src/app_language_refresh_node.mjs
+++ public/src/app_language_refresh_node.mjs
@@ -28,17 +28,13 @@ import { app_ceb } from "./app_ceb.mjs";
 import { storage_local_get } from "./storage_local_get.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 export async function app_language_refresh_node(
-  root,
   context,
-  group,
-  invert,
   refresh_node,
-  language_fluent,
   refresh_review,
   refresh_learn,
   quizzes_start,
 ) {
-  let a = context;
+  let { root, group, invert, language_fluent } = context;
   let level_size = app_language_level_size();
   html_clear_scroll_top_centered(root);
   app_language_title(context);
