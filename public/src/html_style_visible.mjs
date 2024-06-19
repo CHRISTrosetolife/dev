2492d881de4cd5965a075dfa34c2ee36037cd9c9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 23:30:54 -0400

    2024-06-19T03:30:53.379Zfunction_auto_after html_style_visible

--- public/src/html_style_visible.mjs
+++ public/src/html_style_visible.mjs
@@ -1,6 +1,6 @@
 import { html_style } from "./html_style.mjs";
-export function html_style_visible(spacer2) {
-  html_style(spacer2, {
+export function html_style_visible(component) {
+  html_style(component, {
     visibility: "visible",
   });
 }
