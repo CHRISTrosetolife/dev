7da843f67b52285012ef2dbb1043556daad500a7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 23:33:03 -0400

    2024-06-19T03:33:03.071Zfunction_auto_after app_gs_overlay_player_direction

--- public/src/app_gs_overlay_player_direction.mjs
+++ public/src/app_gs_overlay_player_direction.mjs
@@ -1,8 +1,8 @@
 import { log } from "./log.mjs";
 import { html_style_hidden } from "./html_style_hidden.mjs";
 import { html_style_visible } from "./html_style_visible.mjs";
-import { each } from "./each.mjs";
 import { game_character_direction_index } from "./game_character_direction_index.mjs";
+import { each_index } from "./each_index.mjs";
 export function app_gs_overlay_player_direction(
   player_c,
   direction,
@@ -13,8 +13,8 @@ export function app_gs_overlay_player_direction(
     di,
   });
   let { player_directions } = player_c;
-  each(player_directions, (pd) => {
-    if (pd === di) {
+  each_index(player_directions, (pd, index) => {
+    if (index === di) {
       html_style_visible(pd);
     } else {
       html_style_hidden(pd);
