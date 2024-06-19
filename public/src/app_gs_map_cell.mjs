ae6ad8e90ff588b965bbfbd3cf8fd2c4789b7530
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 20:53:38 -0400

    2024-06-19T00:53:36.373Zfunction_auto_after app_gs_map_cell

--- public/src/app_gs_map_cell.mjs
+++ public/src/app_gs_map_cell.mjs
@@ -32,6 +32,7 @@ import { list_concat } from "./list_concat.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { equal_by } from "./equal_by.mjs";
 import { html_style_background_color } from "./html_style_background_color.mjs";
+import { html_remove } from "./html_remove.mjs";
 export function app_gs_map_cell(map, map_c, player_c, tile) {
   let z_indexes = app_gs_z_indexes();
   let clicker = html_div(map_c);
@@ -91,7 +92,9 @@ export function app_gs_map_cell(map, map_c, player_c, tile) {
         subtract(map.player.y, (y_count - 1) / 2),
         subtract(map.player.x, (x_count - 1) / 2),
       );
-      html_button_width_full_text_click(menu, "close menu", () => {});
+      html_button_width_full_text_click(menu, "close menu", () => {
+        html_remove(menu);
+      });
       return;
     }
     app_gs_map_render(map, map_c, path, player_c);
