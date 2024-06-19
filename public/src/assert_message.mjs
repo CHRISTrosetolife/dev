aaae9848df460dca64c7018e186ca212649f81a7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 10:12:23 -0400

    2024-06-19T14:12:22.370Zfunction_auto_after assert_message

--- public/src/assert_message.mjs
+++ public/src/assert_message.mjs
@@ -2,6 +2,6 @@ import { assert_message_string } from "./assert_message_string.mjs";
 import { json_to } from "./json_to.mjs";
 export function assert_message(fn, args, message_get) {
   return assert_message_string(fn, args, () => {
-    json_to(message_get());
+    let j = json_to(message_get());
   });
 }
