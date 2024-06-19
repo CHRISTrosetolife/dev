452d4b2d8b2b9d93a04fe399ea2f79b8794db990
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 10:05:54 -0400

    2024-06-19T14:05:54.407Zfunction_auto_after sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { list_to_lookup_value_property } from "./list_to_lookup_value_property.mjs";
 import { object_list_invert } from "./object_list_invert.mjs";
 import { app_language_group_size } from "./app_language_group_size.mjs";
@@ -51,6 +52,10 @@ export async function sandbox() {
       invert: true,
     },
   ];
+  log({
+    profiles,
+  });
+  return;
   await each_async(profiles, async (profile) => {
     let group = list_take(atoms, group_count);
     await each_async(group, async (atom) => {
