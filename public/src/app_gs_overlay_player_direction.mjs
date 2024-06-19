6f0e6331c79f7c8700bc964468fb1faa64a5395f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 23:29:17 -0400

    2024-06-19T03:29:16.774Zfunction_auto_after app_gs_overlay_player_direction

--- public/src/app_gs_overlay_player_direction.mjs
+++ public/src/app_gs_overlay_player_direction.mjs
@@ -1,1 +1,4 @@
-export function app_gs_overlay_player_direction(player_c, direction, offset) {}
+import { game_character_direction_index } from "./game_character_direction_index.mjs";
+export function app_gs_overlay_player_direction(player_c, direction, offset) {
+  let di = game_character_direction_index(direction, player.walk_offset);
+}
