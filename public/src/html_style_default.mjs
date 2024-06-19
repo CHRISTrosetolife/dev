ed3dda64515de6551f41ed89cf960259ac878fbb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 21:12:11 -0400

    2024-06-19T01:12:11.090Zfunction_auto_after html_style_default

--- public/src/html_style_default.mjs
+++ public/src/html_style_default.mjs
@@ -1,4 +1,5 @@
-let d = {};
+import { global_get } from "./global_get.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
 export function html_style_default() {
-  return d;
+  return object_property_initialize(global_get(), html_style_default.name, {});
 }
