d7c0f1a366791c316be6a2de7cb478917ab4ebaa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 00:59:28 -0400

    2024-06-19T04:59:27.791Zfunction_auto_after app_gs_map_cell

--- public/src/app_gs_map_cell.mjs
+++ public/src/app_gs_map_cell.mjs
@@ -82,9 +82,9 @@ export function app_gs_map_cell(map, map_c, player_c, tile) {
       await app_gs_walk(player_c, map.player, tile);
     });
     log("app_gs_map_cell 3");
-    app_gs_map_render(map, map_c, [map.player], player_c);
-    log("app_gs_map_cell 4");
     await html_scroll_center_smooth(player_c);
+    log("app_gs_map_cell 4");
+    app_gs_map_render(map, map_c, [map.player], player_c);
     log("app_gs_map_cell 5");
   });
   return list_concat([tile_c, clicker], overlays);
