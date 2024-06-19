346150938ad533ab2701420075a4bcb9e6ba2987
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 22:03:41 -0400

    2024-06-19T02:03:40.499Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,10 +1,10 @@
+import { app_gs_map_render_all } from "./app_gs_map_render_all.mjs";
 import { object_merge } from "./object_merge.mjs";
 import { html_style_button_default_value } from "./html_style_button_default_value.mjs";
 import { html_hostname } from "./html_hostname.mjs";
 import { html_scale_none } from "./html_scale_none.mjs";
 import { html_scroll_center_smooth } from "./html_scroll_center_smooth.mjs";
 import { html_style } from "./html_style.mjs";
-import { app_gs_map_render } from "./app_gs_map_render.mjs";
 import { app_gs_style_default_initialize } from "./app_gs_style_default_initialize.mjs";
 import { app_gs_map_html } from "./app_gs_map_html.mjs";
 import { app_gs_overlay_player } from "./app_gs_overlay_player.mjs";
@@ -26,6 +26,6 @@ export function app_gs() {
   let map_c = app_gs_map_html(root, map);
   let player_overlay = app_gs_overlay_player(map_c, map);
   map.html = [];
-  app_gs_map_render(map, map_c, [map.player], player_overlay);
+  app_gs_map_render_all(map, map_c, player_overlay);
   html_scroll_center_smooth(player_overlay);
 }
