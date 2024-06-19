0a9df6423d4844c6e9d1e86e09f731bb1223cef6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 23:37:29 -0400

    2024-06-19T03:37:29.239Zfunction_auto_after app_gs_map_cell

--- public/src/app_gs_map_cell.mjs
+++ public/src/app_gs_map_cell.mjs
@@ -81,7 +81,7 @@ export function app_gs_map_cell(map, map_c, player_c, tile) {
     await each_async(list_skip(path, 1), async (tile) => {
       await app_gs_walk(player_c, map.player, tile);
     });
-    await html_scroll_center_smooth(player_c);
+    await html_scroll_center_smooth(player_c.first);
     app_gs_map_render(map, map_c, [map.player], player_c);
   });
   return list_concat([tile_c, clicker], overlays);
