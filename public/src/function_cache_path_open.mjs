b775c37732f80b4848d3ba2640d3ddb5c599c876
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 12:14:51 -0400

    2024-06-19T16:14:50.746Zfunction_auto_after function_cache_path_open

--- public/src/function_cache_path_open.mjs
+++ public/src/function_cache_path_open.mjs
@@ -3,4 +3,5 @@ import { string_split_comma } from "./string_split_comma.mjs";
 export function function_cache_path_open(fn, args_string) {
   let args = string_split_comma(args_string);
   let p = function_cache_path(fn, args);
+  p;
 }
