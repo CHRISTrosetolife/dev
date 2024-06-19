5c826a69924df2a42914d2b2b47f4bf3872f78a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 10:17:52 -0400

    2024-06-19T14:17:52.466Zfunction_auto_after sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -70,7 +70,11 @@ export async function sandbox() {
       });
     });
     let words = list_adder_unique((la) =>
-      each(atoms, (a) => each(a, (pair) => la(list_first(pair)))),
+      each(atoms, (a) =>
+        each(a, (pair) => {
+          la(list_first(pair));
+        }),
+      ),
     );
     let definitions = {};
     each(words, (w) => {
