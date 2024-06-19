422e930d1c66d05f58002c83292fd3d667a8e682
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 23:57:01 -0400

    2024-06-19T03:56:59.915Zfunction_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,9 +1,9 @@
+import { html_style_overflow_hidden } from "./html_style_overflow_hidden.mjs";
 import { object_merge } from "./object_merge.mjs";
 import { html_style_button_default_value } from "./html_style_button_default_value.mjs";
 import { html_hostname } from "./html_hostname.mjs";
 import { html_scale_none } from "./html_scale_none.mjs";
 import { html_scroll_center_smooth } from "./html_scroll_center_smooth.mjs";
-import { html_style } from "./html_style.mjs";
 import { app_gs_map_render } from "./app_gs_map_render.mjs";
 import { app_gs_style_default_initialize } from "./app_gs_style_default_initialize.mjs";
 import { app_gs_map_html } from "./app_gs_map_html.mjs";
@@ -19,9 +19,7 @@ export async function app_gs() {
   if (html_hostname() !== "localhost") {
     html_scale_none();
   }
-  html_style(root, {
-    overflow: "hidden",
-  });
+  html_style_overflow_hidden(root);
   let map = app_gs_map_new();
   let map_c = app_gs_map_html(root, map);
   let player_c = app_gs_overlay_player(map_c, map);
---
+++ public/src/html_style_overflow_hidden.mjs
@@ -0,0 +1,6 @@
+import { html_style } from "./html_style.mjs";
+export function html_style_overflow_hidden(root) {
+  html_style(root, {
+    overflow: "hidden",
+  });
+}
