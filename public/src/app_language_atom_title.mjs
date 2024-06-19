35237de4992dc2705903973492eb3cc9213fed4d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 17:24:58 -0400

    2024-06-19T21:24:57.144Zfunction_auto_after app_language_atom_title

--- public/src/app_language_atom_title.mjs
+++ public/src/app_language_atom_title.mjs
@@ -20,7 +20,7 @@ export function app_language_atom_title(group, atom_left, atom_right, invert) {
   let text = string_combine_multiple([
     "words ",
     list_join_colon_spaces((invert ? list_last : list_first)(mapped)),
-    " through ",
+    " - ",
     list_join_colon_spaces((invert ? list_first : list_last)(mapped)),
     "<br>( words ",
     add_1(multiply(list_index(group, atom_left), atom_count)),
