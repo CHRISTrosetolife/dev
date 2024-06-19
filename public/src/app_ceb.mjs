488170d7d7974dc1aaf935669e45d9b5515e90a4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 09:47:34 -0400

    2024-06-19T13:47:33.971Zfunction_auto_after app_ceb

--- public/src/app_ceb.mjs
+++ public/src/app_ceb.mjs
@@ -1,7 +1,4 @@
 import { app_language } from "./app_language.mjs";
 export async function app_ceb() {
-  let from = "ceb";
-  let to = "en";
-  let app_fn = app_ceb;
-  await app_language(app_fn, from, to);
+  await app_language(app_ceb, "ceb", "en");
 }
