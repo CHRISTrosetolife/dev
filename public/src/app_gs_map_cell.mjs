7134aa4858c569fa11985f8edae057e9c23155ba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 21:46:31 -0400

    2024-06-19T01:46:31.239Zfunction_auto_after app_gs_map_cell

--- public/src/app_gs_map_cell.mjs
+++ public/src/app_gs_map_cell.mjs
@@ -1,10 +1,4 @@
-import { app_gs_screen_size } from "./app_gs_screen_size.mjs";
-import { subtract } from "./subtract.mjs";
-import { game_img_position } from "./game_img_position.mjs";
-import { html_style } from "./html_style.mjs";
-import { html_style_height } from "./html_style_height.mjs";
-import { game_tile_units_css } from "./game_tile_units_css.mjs";
-import { html_style_width } from "./html_style_width.mjs";
+import { app_gs_menu } from "./app_gs_menu.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { list_xy } from "./list_xy.mjs";
 import { list_all } from "./list_all.mjs";
@@ -26,15 +20,12 @@ import { game_img_base } from "./game_img_base.mjs";
 import { game_img } from "./game_img.mjs";
 import { html_data_set } from "./html_data_set.mjs";
 import { html_div } from "./html_div.mjs";
-import { app_gs_z_indexes } from "./app_gs_z_indexes.mjs";
 import { list_skip } from "./list_skip.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { equal_by } from "./equal_by.mjs";
-import { html_style_background_color } from "./html_style_background_color.mjs";
 import { html_remove } from "./html_remove.mjs";
 export function app_gs_map_cell(map, map_c, player_c, tile) {
-  let z_indexes = app_gs_z_indexes();
   let clicker = html_div(map_c);
   let overlays = list_adder((la) =>
     each(tile.overlays, function lambda_overlay(o) {
@@ -78,23 +69,7 @@ export function app_gs_map_cell(map, map_c, player_c, tile) {
         ),
       )
     ) {
-      let menu = html_div(map_c);
-      html_style(menu, {
-        padding: game_tile_units_css(0.25),
-      });
-      html_style_background_color(menu, "rgba(1,1,1,0.5)");
-      let { x_count, y_count } = app_gs_screen_size();
-      html_style_width(menu, game_tile_units_css(x_count));
-      html_style_height(menu, game_tile_units_css(y_count));
-      html_style(menu, {
-        position: "absolute",
-        "z-index": list_index(z_indexes, "menu"),
-      });
-      game_img_position(
-        menu,
-        subtract(map.player.y, (y_count - 1) / 2),
-        subtract(map.player.x, (x_count - 1) / 2),
-      );
+      let menu = app_gs_menu(map_c, map);
       html_button_width_full_text_click(menu, "back", () => {
         html_remove(menu);
       });
---
+++ public/src/app_gs_menu.mjs
@@ -0,0 +1,32 @@
+import { subtract } from "./subtract.mjs";
+import { game_img_position } from "./game_img_position.mjs";
+import { list_index } from "./list_index.mjs";
+import { html_style_height } from "./html_style_height.mjs";
+import { html_style_width } from "./html_style_width.mjs";
+import { app_gs_screen_size } from "./app_gs_screen_size.mjs";
+import { html_style_background_color } from "./html_style_background_color.mjs";
+import { game_tile_units_css } from "./game_tile_units_css.mjs";
+import { html_style } from "./html_style.mjs";
+import { html_div } from "./html_div.mjs";
+import { app_gs_z_indexes } from "./app_gs_z_indexes.mjs";
+export function app_gs_menu(map_c, map) {
+  let z_indexes = app_gs_z_indexes();
+  let menu = html_div(map_c);
+  html_style(menu, {
+    padding: game_tile_units_css(0.25),
+  });
+  html_style_background_color(menu, "rgba(1,1,1,0.5)");
+  let { x_count, y_count } = app_gs_screen_size();
+  html_style_width(menu, game_tile_units_css(x_count));
+  html_style_height(menu, game_tile_units_css(y_count));
+  html_style(menu, {
+    position: "absolute",
+    "z-index": list_index(z_indexes, "menu"),
+  });
+  game_img_position(
+    menu,
+    subtract(map.player.y, (y_count - 1) / 2),
+    subtract(map.player.x, (x_count - 1) / 2),
+  );
+  return menu;
+}
