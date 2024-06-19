2a982c9ec8096c349fdbd3ce7e3fa5f19e407ee6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 20:24:16 -0400

    2024-06-19T00:24:15.875Zfunction_auto_after game_img_extend_style

--- public/src/game_img_style.mjs
+++ public/src/game_img_style.mjs
@@ -1,20 +1,6 @@
-import { add } from "./add.mjs";
-import { game_img_style_transition } from "./game_img_style_transition.mjs";
-import { game_img_position } from "./game_img_position.mjs";
-import { html_style } from "./html_style.mjs";
-import { game_tile_units_css } from "./game_tile_units_css.mjs";
-import { html_style_width } from "./html_style_width.mjs";
-import { html_style_height } from "./html_style_height.mjs";
-import { subtract } from "./subtract.mjs";
+import { game_img_extend_style } from "./game_img_extend_style.mjs";
 export function game_img_style(image, y, x, z_index) {
   let x_extend = 0;
   let y_extend = 0;
-  html_style_width(image, add(game_tile_units_css(1 + 2 * x_extend)));
-  html_style_height(image, add(game_tile_units_css(1 + 2 * y_extend)));
-  game_img_style_transition(image);
-  html_style(image, {
-    position: "absolute",
-    "z-index": z_index,
-  });
-  game_img_position(image, subtract(y, y_extend), subtract(x, x_extend));
+  game_img_extend_style(image, z_index, y, x, y_extend, x_extend);
 }
