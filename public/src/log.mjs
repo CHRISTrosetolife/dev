5a07691b0865538a398cad63980377161a0d607f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 00:11:17 -0400

    2024-06-19T04:11:16.917Zfunction_auto_after log

--- public/src/log.mjs
+++ public/src/log.mjs
@@ -1,9 +1,7 @@
 import { log_logs } from "./log_logs.mjs";
 import { list_add } from "./list_add.mjs";
-import { global_get } from "./global_get.mjs";
 export function log(message) {
-  let g = global_get();
-  let logs = log_logs(g);
+  let logs = log_logs();
   list_add(logs, message);
   console.log(message);
 }
