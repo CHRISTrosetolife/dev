397bfdf1117fa48f10e042bca84e4274914ad29e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 21:51:27 -0400

    2024-06-19T01:51:25.670Zfunction_auto_after html_scroll_center_smooth

--- public/src/html_scroll_center_smooth.mjs
+++ public/src/html_scroll_center_smooth.mjs
@@ -1,10 +1,5 @@
 import { html_scroll_center_generic } from "./html_scroll_center_generic.mjs";
 export function html_scroll_center_smooth(component) {
-  let additional = {
-    behavior: "smooth",
-  };
-  html_scroll_center_generic(component, additional);
-  return;
   let vh = window.innerHeight;
   let vw = window.innerWidth;
   let e = player_overlay.element;
@@ -13,4 +8,10 @@ export function html_scroll_center_smooth(component) {
     top: e.offsetTop - vh / 2 + e.height / 2,
     behavior: "smooth",
   });
+  return;
+  let additional = {
+    behavior: "smooth",
+  };
+  html_scroll_center_generic(component, additional);
+  return;
 }
