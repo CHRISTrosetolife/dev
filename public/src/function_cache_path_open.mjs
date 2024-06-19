4f9e19a4dded92b784b00422f1a38f61cdb41bb8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 16:52:33 -0400

    2024-06-19T20:52:31.790Z  w

--- public/src/function_cache_path_open.mjs
+++ public/src/function_cache_path_open.mjs
@@ -2,7 +2,7 @@ import { file_open } from "./file_open.mjs";
 import { function_cache_path } from "./function_cache_path.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 export async function function_cache_path_open(fn, args_string) {
-  let args = string_split_comma(args_string);
+  let args = string_empty_is(args_string) ? [] : string_split_comma(args_string);
   return args
   let p = function_cache_path(fn, args);
   await file_open(p);
