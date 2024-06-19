103abd0332bde39b982be80953eee086d6c75657
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 16:16:49 -0400

    2024-06-19T20:16:49.472Z

--- public/src/function_open.mjs
+++ public/src/function_open.mjs
@@ -1,3 +1,4 @@
+import { unawait } from "./unawait.mjs";
 import { assert_async } from "./assert_async.mjs";
 import { file_exists } from "./file_exists.mjs";
 import { file_open } from "./file_open.mjs";
@@ -5,5 +6,5 @@ import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function function_open(function_name) {
   let file_path = function_name_to_path(function_name);
   await assert_async(file_exists, [file_path]);
-  unawait(()=> file_open(file_path));
+  unawait(async () => await file_open(file_path));
 }
