600b2074a050801ec440ff0904003720a15ba146
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 10:13:08 -0400

    2024-06-19T14:13:07.420Zfunction_auto_after assert_message

--- public/src/assert_message.mjs
+++ public/src/assert_message.mjs
@@ -5,8 +5,10 @@ import { string_take } from "./string_take.mjs";
 export function assert_message(fn, args, message_get) {
   return assert_message_string(fn, args, () => {
     let j = json_to(message_get());
-    if (string_length(j > 1000)) {
-      return string_take(j, 1000);
+    let limit = 1000;
+    if (string_length(j > limit)) {
+      return string_take(j, limit);
     }
+    return j;
   });
 }
