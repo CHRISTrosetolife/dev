c7ac3f1dee7106b384bb7d33afc3bc36e3f326dc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 12:21:51 -0400

    2024-06-19T16:21:50.549Zfunction_auto_after string_take_reverse

---
+++ public/src/string_take_reverse.mjs
@@ -0,0 +1,8 @@
+import { string_substring } from "./string_substring.mjs";
+import { assert_boolean } from "./assert_boolean.mjs";
+import { string_length } from "./string_length.mjs";
+export function string_take_reverse(input, count) {
+  let length = string_length(input);
+  assert_boolean(count <= length);
+  return string_substring(input, 0, count);
+}
