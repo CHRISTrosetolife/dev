93ac2da40fbc4600827c016077ce630df267baa1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 16:59:54 -0400

    2024-06-19T20:59:53.915Zfunction_auto_after app_language

--- public/src/app_language.mjs
+++ public/src/app_language.mjs
@@ -23,7 +23,6 @@ import { list_get } from "./list_get.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 import { list_index_last } from "./list_index_last.mjs";
 import { storage_local_initialize } from "./storage_local_initialize.mjs";
-import { app_language_level_size } from "./app_language_level_size.mjs";
 import { app_language_group_path } from "./app_language_group_path.mjs";
 import { http_storage } from "./http_storage.mjs";
 import { html_style_default_font_size } from "./html_style_default_font_size.mjs";
@@ -57,7 +56,6 @@ export async function app_language(
     definitions,
     inverted,
   });
-  let level_size = app_language_level_size();
   storage_local_initialize(app_fn, "position", {
     left: 0,
     right: list_index_last(group),
