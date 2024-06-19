735cb32cef155b1c147e9c6a98fb9cd94d98fa04
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 17:03:09 -0400

    2024-06-19T21:03:09.077Zfunction_auto_after app_language_quizzes_start

--- public/src/app_language_quizzes_start.mjs
+++ public/src/app_language_quizzes_start.mjs
@@ -1,14 +1,8 @@
 import { app_language_atoms_slice } from "./app_language_atoms_slice.mjs";
 import { list_first } from "./list_first.mjs";
 import { app_language_quiz_settings } from "./app_language_quiz_settings.mjs";
-export function app_language_quizzes_start(
-  app_fn,
-  group,
-  context,
-  chunk_sizes,
-  refresh_quiz,
-) {
-  let a = context;
+export function app_language_quizzes_start(context, chunk_sizes, refresh_quiz) {
+  let { app_fn, group } = context;
   let atoms = app_language_atoms_slice(app_fn, group);
   context.settings_choices = app_language_quiz_settings(atoms, chunk_sizes);
   refresh_quiz(list_first(context.settings_choices));
