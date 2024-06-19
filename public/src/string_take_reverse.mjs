9aa886799e5006b900dedb967d359040fb49f185
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 12:23:38 -0400

    2024-06-19T16:23:38.272Zfunction_auto_after string_take_reverse

--- public/src/string_take_reverse.mjs
+++ public/src/string_take_reverse.mjs
@@ -1,4 +1,4 @@
 import { string_skip } from "./string_skip.mjs";
 export function string_take_reverse(input, count) {
-  return string_skip(input, length - count - 1);
+  return string_skip(input, length - count);
 }
