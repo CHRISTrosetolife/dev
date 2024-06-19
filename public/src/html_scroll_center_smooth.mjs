c6d8d0933619a349ceac55a9d2fe5edf8be2fff3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 21:51:45 -0400

    2024-06-19T01:51:45.156Zfunction_auto_after html_scroll_center_smooth

--- public/src/html_scroll_center_smooth.mjs
+++ public/src/html_scroll_center_smooth.mjs
@@ -2,7 +2,7 @@ import { html_scroll_center_generic } from "./html_scroll_center_generic.mjs";
 export function html_scroll_center_smooth(component) {
   let vh = window.innerHeight;
   let vw = window.innerWidth;
-  let e = player_overlay.element;
+  let e = component.element;
   window.scrollTo({
     left: e.offsetLeft - vw / 2 + e.width / 2,
     top: e.offsetTop - vh / 2 + e.height / 2,
