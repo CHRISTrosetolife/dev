80189cafe13cd25d8bd3db20b9f5f80608bca861
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 09:28:39 -0400

    2024-06-19T13:28:39.388Zfunction_auto_after list_find

--- public/src/list_find.mjs
+++ public/src/list_find.mjs
@@ -1,1 +1,6 @@
-export function list_find(list, predicate) {}
+import { list_single } from "./list_single.mjs";
+import { list_filter } from "./list_filter.mjs";
+export function list_find(list, predicate) {
+  let filtered = list_filter(definitions_list, (d) => d.word === w);
+  let ds = list_single(filtered).definitions;
+}
