5dd36b3c38ae90bf1b02eb3794af5e160cf40908
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 22:06:37 -0400

    2024-06-19T02:06:36.816Zfunction_auto_after app_gs_map_render_all

--- public/src/app_gs_map_render_all.mjs
+++ public/src/app_gs_map_render_all.mjs
@@ -1,11 +1,9 @@
-import { app_gs_at_single } from "./app_gs_at_single.mjs";
-import { range_from } from "./range_from.mjs";
-import { each } from "./each.mjs";
+import { each_range } from "./each_range.mjs";
 import { app_gs_map_cell } from "./app_gs_map_cell.mjs";
 export function app_gs_map_render_all(map, map_component, player_overlay) {
-  each(range_from(y - y_extend, y + y_extend), (ye) => {
-    each(range_from(x - x_extend, x + x_extend), (xe) => {
-      let tile = app_gs_at_single;
+  each_range(map.y_size, (y) => {
+    each_range(map.x_size, (x) => {
+      let tile = map.tiles[y][x];
       app_gs_map_cell(map, map_component, player_overlay, tile);
     });
   });
