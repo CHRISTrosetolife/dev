6675dcc5d559e1ba4c15fbd4cb09359b4fd51f41
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 23:31:09 -0400

    2024-06-19T03:31:08.707Zfunction_auto_after app_gs_overlay_player_direction

--- public/src/app_gs_overlay_player_direction.mjs
+++ public/src/app_gs_overlay_player_direction.mjs
@@ -1,3 +1,4 @@
+import { html_style_hidden } from "./html_style_hidden.mjs";
 import { html_style_visible } from "./html_style_visible.mjs";
 import { each } from "./each.mjs";
 import { game_character_direction_index } from "./game_character_direction_index.mjs";
@@ -10,7 +11,9 @@ export function app_gs_overlay_player_direction(
   let { player_directions } = player_c;
   each(player_directions, (pd) => {
     if (pd === di) {
-      html_style_visible();
+      html_style_visible(pd);
+    } else {
+      html_style_hidden(pd);
     }
   });
 }
