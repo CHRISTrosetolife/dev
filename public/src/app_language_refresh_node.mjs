d7111a1d2eff2f2e0f1ee309d5f6c46167328585
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 11:11:41 -0400

    2024-06-19T15:11:38.738Zfunction_auto_after app_language_refresh_node

--- public/src/app_language_refresh_node.mjs
+++ public/src/app_language_refresh_node.mjs
@@ -28,7 +28,6 @@ import { storage_local_get } from "./storage_local_get.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 export async function app_language_refresh_node(
   root,
-  app_language_title,
   level_size,
   group,
   invert,
