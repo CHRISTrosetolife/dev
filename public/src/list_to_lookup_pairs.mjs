b37311f319f8a91ba02a2790578e4a586bdcd97c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 09:34:23 -0400

    2024-06-19T13:34:23.370Zfunction_auto_after list_to_lookup_pairs

--- public/src/list_to_lookup_pairs.mjs
+++ public/src/list_to_lookup_pairs.mjs
@@ -2,9 +2,9 @@ import { list_second } from "./list_second.mjs";
 import { list_first } from "./list_first.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { each } from "./each.mjs";
-export function list_to_lookup_pairs(skipped_pairs_split) {
+export function list_to_lookup_pairs(pairs) {
   let lookup = {};
-  each(skipped_pairs_split, (s) =>
+  each(pairs, (s) =>
     object_property_set(lookup, list_first(s), list_second(s)),
   );
   return lookup;
