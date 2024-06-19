ba3a4f901ebbc9b89c1a73d7fceb7b43a367ef96
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-18 23:20:27 -0400

    2024-06-19T03:20:27.086Zfunction_auto_after app_gs_overlay_player

--- public/src/app_gs_overlay_player.mjs
+++ public/src/app_gs_overlay_player.mjs
@@ -1,3 +1,4 @@
+import { list_adder } from "./list_adder.mjs";
 import { game_character_direction_index } from "./game_character_direction_index.mjs";
 import { each } from "./each.mjs";
 import { game_character_indices } from "./game_character_indices.mjs";
@@ -11,17 +12,19 @@ import { range_from } from "./range_from.mjs";
 export function app_gs_overlay_player(map_c, map) {
   let z_indexes = app_gs_z_indexes();
   let directions = object_properties(game_character_indices());
-  each(directions, (direction) => {
-    each(range_from(-1, 1), (walk_offset) => {
-      let player_c = game_img(
-        map_c,
-        game_img_character(
-          map.player.character,
-          game_character_direction_index(direction, walk_offset),
-        ),
-        map.player,
-        list_index(z_indexes, "player"),
-      );
+  list_adder((la) => {
+    each(directions, (direction) => {
+      each(range_from(-1, 1), (walk_offset) => {
+        let player_c = game_img(
+          map_c,
+          game_img_character(
+            map.player.character,
+            game_character_direction_index(direction, walk_offset),
+          ),
+          map.player,
+          list_index(z_indexes, "player"),
+        );
+      });
     });
   });
   game_img_style_transition(player_c);
