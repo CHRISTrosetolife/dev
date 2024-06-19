2e7f1be61d745f74a234a7183aec0dd900176bfb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 10:12:10 -0400

    2024-06-19T14:12:10.183Zfunction_auto_after object_property_get

--- public/src/object_property_get.mjs
+++ public/src/object_property_get.mjs
@@ -3,8 +3,8 @@ import { undefined_not_is } from "./undefined_not_is.mjs";
 export function object_property_get(object, property_name) {
   let result = object[property_name];
   assert_message(undefined_not_is, [result], () => ({
-    object,
     property_name,
+    object,
   }));
   return result;
 }
