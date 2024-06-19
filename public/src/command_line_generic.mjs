02f23c6e53bb666c5c00fd09fc1f23a1c1ee37cb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-19 17:34:48 -0400

    2024-06-19T21:34:48.082Zfunction_auto_after command_line_generic

--- public/src/command_line_generic.mjs
+++ public/src/command_line_generic.mjs
@@ -1,8 +1,26 @@
+import { log } from "./log.mjs";
 import { import_node } from "./import_node.mjs";
 export async function command_line_generic(command, options) {
   let c = await import_node("child_process");
   let { spawn } = c;
   return await new Promise((resolve) => {
     spawn(command);
+    let scriptOutput = "";
+    child.stdout.setEncoding("utf8");
+    child.stdout.on("data", function (data) {
+      console.log("stdout: " + data);
+      data = data.toString();
+      scriptOutput += data;
+    });
+    child.stderr.setEncoding("utf8");
+    child.stderr.on("data", function (data) {
+      console.log("stderr: " + data);
+      data = data.toString();
+      scriptOutput += data;
+    });
+    child.on("close", function (code) {
+      console.log("closing code: " + code);
+      console.log("Full output of script: ", scriptOutput);
+    });
   });
 }
