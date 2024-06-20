d5bbee7ff5fb4dab958686e8e59326515e443417
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 14:19:49 -0400

    2024-06-20T18:19:47.239Z function_auto_after html_element

--- public/src/html_element.mjs
+++ public/src/html_element.mjs
@@ -2,7 +2,9 @@ import { html_id } from "./html_id.mjs";
 import { html_style_font_color_default_set } from "./html_style_font_color_default_set.mjs";
 import { html_style_default } from "./html_style_default.mjs";
 import { html_style } from "./html_style.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function html_element(parent, tag_name) {
+  assert_arguments_length(arguments, 2);
   let { element } = parent;
   let e = document.createElement(tag_name);
   element.appendChild(e);
