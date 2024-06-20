c48ffb9dd2f59a3cd6013b5fba64f510a32c243e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 13:42:13 -0400

    2024-06-20T17:42:13.073Z function_auto_after app_gs_tiles

--- public/src/app_gs_tiles.mjs
+++ public/src/app_gs_tiles.mjs
@@ -1,1 +1,11 @@
-export function app_gs_tiles() {}
+import { list_index } from "./list_index.mjs";
+import { game_img_base } from "./game_img_base.mjs";
+import { game_img } from "./game_img.mjs";
+export function app_gs_tiles() {
+  let tile_c = game_img(
+    map_c,
+    game_img_base(id),
+    tile,
+    list_index(z_indexes, "tile"),
+  );
+}
