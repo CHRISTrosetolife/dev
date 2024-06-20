ad78c29c69360d2dd65b23ac24248ba58b0bf2f0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 17:51:55 -0400

    2024-06-20T21:51:54.582Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -1,6 +1,9 @@
+import { string_starts_with } from "./string_starts_with.mjs";
 import { js_visit_identifiers } from "./js_visit_identifiers.mjs";
 export function js_dollar(ast) {
   js_visit_identifiers(ast, (v) => {
     let { node } = v;
+    if (string_starts_with("$")) {
+    }
   });
 }
