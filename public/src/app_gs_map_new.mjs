42204ab7269079af90d761f21365e2fcbd4aeef6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 16:22:07 -0400

    2024-06-20T20:22:07.315Z function_auto_after app_gs_map_new

--- public/src/app_gs_map_new.mjs
+++ public/src/app_gs_map_new.mjs
@@ -118,6 +118,20 @@ export function app_gs_map_new() {
         overlay_id = 9;
       }
     }
+    if (x_left && y_top) {
+      if (x_even === y_even) {
+        overlay_id = 4;
+      } else {
+        overlay_id = 5;
+      }
+    }
+    if (x_left && y_bottom) {
+      if (x_even === y_even) {
+        overlay_id = 4;
+      } else {
+        overlay_id = 5;
+      }
+    }
     overlay_add(tile, overlay_id);
   });
   list_shuffle(inside);
