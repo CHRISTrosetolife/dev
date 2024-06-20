976500b1325de47f4a723c5eb70cef26665f6b23
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 05:00:34 -0400

    2024-06-20T09:00:33.601Zfunction_auto_after file_open

--- public/src/file_open.mjs
+++ public/src/file_open.mjs
@@ -1,10 +1,11 @@
+import { command_line_generic } from "./command_line_generic.mjs";
 import { web_is } from "./web_is.mjs";
 import { command_line } from "./command_line.mjs";
 export async function file_open(file_name) {
   if (web_is()) {
     return;
   }
-  await command_line(`code ${file_name}`);
+  await command_line_generic(`code ${file_name}`, {});
   return;
   await command_line(`start notepad++ ${file_name}`);
 }
