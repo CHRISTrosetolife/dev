1d551287fddb0c08102924dc63640c94363e34fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 05:13:30 -0400

    2024-06-20T09:13:29.791Zfunction_auto_after command_line_generic

--- public/src/command_line.mjs
+++ public/src/command_line.mjs
@@ -1,29 +1,5 @@
-import { log_error } from "./log_error.mjs";
-import { log } from "./log.mjs";
-import { string_split_space } from "./string_split_space.mjs";
-import { list_first_remaining } from "./list_first_remaining.mjs";
-import { import_node } from "./import_node.mjs";
+import { command_line_generic } from "./command_line_generic.mjs";
 export async function command_line(command) {
   let silent = false;
-  let c = await import_node("child_process");
-  let { spawn } = c;
-  return await new Promise((resolve) => {
-    let { first, remaining } = list_first_remaining(
-      string_split_space(command),
-    );
-    let child = spawn(first, remaining);
-    if (!silent) {
-      child.stdout.setEncoding("utf8");
-      child.stdout.on("data", function (data) {
-        log(data);
-      });
-      child.stderr.setEncoding("utf8");
-      child.stderr.on("data", function (data) {
-        log_error(data);
-      });
-    }
-    child.on("close", function (code) {
-      resolve();
-    });
-  });
+  return await command_line_generic(command, silent);
 }
---
+++ public/src/command_line_generic.mjs
@@ -0,0 +1,28 @@
+import { log_error } from "./log_error.mjs";
+import { log } from "./log.mjs";
+import { string_split_space } from "./string_split_space.mjs";
+import { list_first_remaining } from "./list_first_remaining.mjs";
+import { import_node } from "./import_node.mjs";
+export async function command_line_generic(command, silent) {
+  let c = await import_node("child_process");
+  let { spawn } = c;
+  return await new Promise((resolve) => {
+    let { first, remaining } = list_first_remaining(
+      string_split_space(command),
+    );
+    let child = spawn(first, remaining);
+    if (!silent) {
+      child.stdout.setEncoding("utf8");
+      child.stdout.on("data", function (data) {
+        log(data);
+      });
+      child.stderr.setEncoding("utf8");
+      child.stderr.on("data", function (data) {
+        log_error(data);
+      });
+    }
+    child.on("close", function (code) {
+      resolve();
+    });
+  });
+}
