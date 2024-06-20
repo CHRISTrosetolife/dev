e68a29eb30f49472f624931bbffc682c22e47a33
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 13:58:39 -0400

    2024-06-20T17:58:38.770Z function_auto_after game_img_style

--- public/src/game_img_style.mjs
+++ public/src/game_img_style.mjs
@@ -1,6 +1,6 @@
 import { game_img_extend_style } from "./game_img_extend_style.mjs";
-export function game_img_style(image, y, x, z_index) {
+export function game_img_style(image, y, x, z_index_name) {
   let x_extend = 0;
   let y_extend = 0;
-  game_img_extend_style(image, y, x, z_index, y_extend, x_extend);
+  game_img_extend_style(image, y, x, z_index_name, y_extend, x_extend);
 }
