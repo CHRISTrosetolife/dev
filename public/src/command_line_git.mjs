2e449f48fdf0f17e1939680cfaf040fa491fd531
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 07:48:56 -0400

    2024-06-20T11:48:55.977Zfunction_auto_after command_line_git

--- public/src/command_line_git.mjs
+++ public/src/command_line_git.mjs
@@ -1,5 +1,9 @@
+import { log } from "./log.mjs";
 import { command_line_silent } from "./command_line_silent.mjs";
 export async function command_line_git(args) {
   let command = `git ${args}`;
+  log({
+    command,
+  });
   await command_line_silent(command);
 }
