5f7bd51fc0a2863503b4bf5696da1def4f79c81e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 18:37:16 -0400

    2024-06-20T22:37:15.669Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -1,9 +1,9 @@
-import { js_parse_first } from "./js_parse_first.mjs";
 import { object_replace } from "./object_replace.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { js_visit_identifiers } from "./js_visit_identifiers.mjs";
+import { js_parse_first_function } from "./js_parse_first_function.mjs";
 export function js_dollar(ast) {
   js_visit_identifiers(ast, (v) => {
     let { node } = v;
@@ -20,7 +20,7 @@ export function js_dollar(ast) {
         object_replace(node, e);
       }
       if (remaining === "r") {
-        let e = js_parse_first("return");
+        let e = js_parse_first_function("return");
         let { parent } = v;
         if (parent.type === "ExpressionStatement") {
           object_replace(parent, e);
