2c6f4a42da4d356cb8cfef3def1f3dec9358aa4b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 18:01:52 -0400

    2024-06-20T22:01:51.896Z function_auto_after log_error_write

--- public/src/log_error_write.mjs
+++ public/src/log_error_write.mjs
@@ -1,6 +1,6 @@
+import { log_write } from "./log_write.mjs";
 import { log_error_generic } from "./log_error_generic.mjs";
-import { log } from "./log.mjs";
 export function log_error_write(message) {
-  let fn = log;
+  let fn = log_write;
   log_error_generic(fn, message);
 }
