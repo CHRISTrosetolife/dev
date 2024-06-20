ae66d88dd385fcb6d369cfb7692cade7a49d4701
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 08:48:16 -0400

    2024-06-20T12:48:15.972Z function_auto_after html_scrollable_hide

---
+++ public/src/html_id.mjs
@@ -0,0 +1,15 @@
+import { object_property_set } from "./object_property_set.mjs";
+import { html_attribute_set } from "./html_attribute_set.mjs";
+import { number_to_letters } from "./number_to_letters.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
+import { html_scrollable_hide } from "./html_scrollable_hide.mjs";
+import { global_function } from "./global_function.mjs";
+export function html_id(map_c) {
+  let data = global_function(html_scrollable_hide);
+  let id_next = object_property_initialize(data, "id_next", 0);
+  let id = number_to_letters(id_next);
+  html_attribute_set(map_c, "id", id);
+  id_next++;
+  object_property_set(data, "id_next", id_next);
+  return id;
+}
--- public/src/html_scrollable_hide.mjs
+++ public/src/html_scrollable_hide.mjs
@@ -1,18 +1,9 @@
-import { object_property_set } from "./object_property_set.mjs";
-import { global_function } from "./global_function.mjs";
-import { number_to_letters } from "./number_to_letters.mjs";
+import { html_id } from "./html_id.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
 import { html_element } from "./html_element.mjs";
-import { html_attribute_set } from "./html_attribute_set.mjs";
-import { object_property_initialize } from "./object_property_initialize.mjs";
 export function html_scrollable_hide(map_c) {
-  let data = global_function(html_scrollable_hide);
-  let id_next = object_property_initialize(data, "id_next", 0);
-  let id = number_to_letters(id_next);
-  html_attribute_set(map_c, "id", id);
-  id_next++;
-  object_property_set(data, "id_next", id_next);
+  let id = html_id(map_c);
   let style = html_element(map_c, "style");
   html_inner_set(
     style,
