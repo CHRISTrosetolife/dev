55073b23fc3756ab0a14f7c2ff584fc453c14c4e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 14:46:34 -0400

    2024-06-20T18:46:32.597Z function_auto_after app_sandbox

--- public/src/app_sandbox.mjs
+++ public/src/app_sandbox.mjs
@@ -1,4 +1,3 @@
-import { html_p_text } from "./html_p_text.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { app_gs_tiles } from "./app_gs_tiles.mjs";
 import { html_style } from "./html_style.mjs";
@@ -7,16 +6,15 @@ import { html_style_centered } from "./html_style_centered.mjs";
 import { html_div } from "./html_div.mjs";
 export function app_sandbox() {
   let root = html_document_body();
-  html_style(root, {
+  let container_outer = html_div(root);
+  html_style(container_outer, {
     "-webkit-box-align": "center",
     "-webkit-box-pack": "center",
     display: "-webkit-box",
   });
-  let container_outer = html_div(root);
   html_style_centered(container_outer);
   html_style_width_full(container_outer);
-  html_p_text(container_outer, "test");
-  let container_inner = html_div(root);
+  let container_inner = html_div(container_outer);
   html_style(container_inner, {
     position: "relative",
   });
