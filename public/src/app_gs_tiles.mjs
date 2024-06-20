25ac056423369a118f60bde51f85d61980bfadf0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 15:02:12 -0400

    2024-06-20T19:01:59.620Z function_auto_after app_gs_tiles

--- public/src/app_gs_tiles.mjs
+++ public/src/app_gs_tiles.mjs
@@ -14,6 +14,6 @@ export function app_gs_tiles(root) {
       html_br(container);
     }
     let image = html_img(container, game_img_base(i));
-    html_style_background_color(container_inner, "black");
+    html_style_background_color(image, "black");
   });
 }
