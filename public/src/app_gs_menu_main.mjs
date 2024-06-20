6fa520d64aa8f57bfd12c6286fcd751b7cd1f023
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 14:59:36 -0400

    2024-06-20T18:59:35.009Z function_auto_after app_gs_menu_main

--- public/src/app_gs_menu_main.mjs
+++ public/src/app_gs_menu_main.mjs
@@ -25,7 +25,7 @@ export function app_gs_menu_main(menu_overlay) {
     html_style_centered(container_outer);
     html_style_width_full(container_outer);
     let container_inner = html_div(container_outer);
-    html_style_background_color();
+    html_style_background_color(container_inner, "black");
     html_style(container_inner, {
       position: "relative",
     });
