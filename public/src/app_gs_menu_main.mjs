70cfd2a9c4834b55a8220074e9f5d294723a6a5e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 15:18:07 -0400

    2024-06-20T19:18:06.126Z function_auto_after app_gs_menu_main

--- public/src/app_gs_menu_main.mjs
+++ public/src/app_gs_menu_main.mjs
@@ -26,6 +26,6 @@ export function app_gs_menu_main(menu_overlay, map_c, map) {
     });
     html_style_centered(container);
     html_style_width_full(container);
-    app_gs_tiles(menu_overlay, container, map);
+    app_gs_tiles(menu_overlay, container, map_c, map);
   });
 }
