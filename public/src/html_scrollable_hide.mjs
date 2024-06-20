e701e70ece7bb0260dffdc4ac76f5c0f9c9d5f15
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 08:48:21 -0400

    2024-06-20T12:48:20.869Z function_auto_after html_scrollable_hide

--- public/src/html_scrollable_hide.mjs
+++ public/src/html_scrollable_hide.mjs
@@ -1,9 +1,7 @@
-import { html_id } from "./html_id.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
 import { html_element } from "./html_element.mjs";
 export function html_scrollable_hide(map_c) {
-  let id = html_id(map_c);
   let style = html_element(map_c, "style");
   html_inner_set(
     style,
