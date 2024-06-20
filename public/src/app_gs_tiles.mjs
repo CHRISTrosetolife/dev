4da2a527039cd5593d59b5fc09678245dfa13a4e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 14:56:25 -0400

    2024-06-20T18:56:23.978Z function_auto_after html_style_line_height_none

--- public/src/app_gs_tiles.mjs
+++ public/src/app_gs_tiles.mjs
@@ -1,4 +1,4 @@
-import { html_style } from "./html_style.mjs";
+import { html_style_line_height_none } from "./html_style_line_height_none.mjs";
 import { html_div } from "./html_div.mjs";
 import { html_br } from "./html_br.mjs";
 import { html_img } from "./html_img.mjs";
@@ -6,9 +6,7 @@ import { each_range } from "./each_range.mjs";
 import { game_img_base } from "./game_img_base.mjs";
 export function app_gs_tiles(root) {
   let container = html_div(root);
-  html_style(container, {
-    "line-height": 0,
-  });
+  html_style_line_height_none(container);
   let columns = 8;
   each_range(1063, (i) => {
     if (i > 0 && i % columns === 0) {
---
+++ public/src/html_style_line_height_none.mjs
@@ -0,0 +1,6 @@
+import { html_style } from "./html_style.mjs";
+export function html_style_line_height_none(container) {
+  html_style(container, {
+    "line-height": 0,
+  });
+}
