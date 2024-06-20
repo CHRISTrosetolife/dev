91ed5bd0fa53c48b77d586f1627baec3811b655e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 14:19:59 -0400

    2024-06-20T18:19:55.129Z function_auto_after app_gs_menu_main

--- public/src/app_gs_menu_main.mjs
+++ public/src/app_gs_menu_main.mjs
@@ -1,10 +1,10 @@
+import { html_div } from "./html_div.mjs";
 import { html_style } from "./html_style.mjs";
 import { app_gs_tiles } from "./app_gs_tiles.mjs";
 import { app_gs_menu_system_logs } from "./app_gs_menu_system_logs.mjs";
 import { html_remove } from "./html_remove.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { html_clear } from "./html_clear.mjs";
-import { html_element } from "./html_element.mjs";
 export function app_gs_menu_main(menu_overlay) {
   html_clear(menu_overlay);
   html_button_width_full_text_click(menu_overlay, "back", () => {
@@ -18,7 +18,7 @@ export function app_gs_menu_main(menu_overlay) {
     html_button_width_full_text_click(menu_overlay, "back", () => {
       app_gs_menu_main(menu_overlay);
     });
-    let container = html_element(menu_overlay);
+    let container = html_div(menu_overlay);
     html_style(container, {
       position: "relative",
     });
