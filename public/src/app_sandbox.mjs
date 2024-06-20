c1365fd57a6bc8e1e56a123acaa7ea52cd7ae8e9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 14:39:56 -0400

    2024-06-20T18:39:55.803Z function_auto_after app_sandbox

--- public/src/app_sandbox.mjs
+++ public/src/app_sandbox.mjs
@@ -1,1 +1,15 @@
-export function app_sandbox() {}
+import { app_gs_tiles } from "./app_gs_tiles.mjs";
+import { html_style } from "./html_style.mjs";
+import { html_style_width_full } from "./html_style_width_full.mjs";
+import { html_style_centered } from "./html_style_centered.mjs";
+import { html_div } from "./html_div.mjs";
+export function app_sandbox() {
+  let container_outer = html_div(menu_overlay);
+  html_style_centered(container_outer);
+  html_style_width_full(container_outer);
+  let container_inner = html_div(container_outer);
+  html_style(container_inner, {
+    position: "relative",
+  });
+  app_gs_tiles(container_inner);
+}
