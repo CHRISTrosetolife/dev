bcbc73d6d79ef12d36d100667ec7b225749cfab4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 18:26:32 -0400

    2024-06-20T22:26:32.310Z function_auto_after js_parse_first

--- public/src/js_parse_first.mjs
+++ public/src/js_parse_first.mjs
@@ -1,14 +1,12 @@
-import { log } from "./log.mjs";
 import { js_code_function_declare } from "./js_code_function_declare.mjs";
 import { js_parse } from "./js_parse.mjs";
 import { list_first } from "./list_first.mjs";
 export function js_parse_first(code) {
   let f = js_code_function_declare("a", "", code);
-  log({
-    f,
-  });
   let parsed = js_parse(f);
   let { body } = parsed;
   let first = list_first(body);
-  return first;
+  let { body: fd_body } = first;
+  let fd_body_first = list_first(fd_body);
+  return fd_body_first;
 }
