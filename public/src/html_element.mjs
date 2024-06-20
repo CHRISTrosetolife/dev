3cf9c1e4833e6adbb2617159e6887fe6b347bfb4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 08:48:47 -0400

    2024-06-20T12:48:46.554Z function_auto_after html_element

--- public/src/html_element.mjs
+++ public/src/html_element.mjs
@@ -1,3 +1,4 @@
+import { html_id } from "./html_id.mjs";
 import { html_style_font_color_default_set } from "./html_style_font_color_default_set.mjs";
 import { html_style_default } from "./html_style_default.mjs";
 import { html_style } from "./html_style.mjs";
@@ -10,5 +11,6 @@ export function html_element(parent, tag_name) {
   };
   html_style_font_color_default_set(result);
   html_style(result, html_style_default());
+  let id = html_id(result);
   return result;
 }
