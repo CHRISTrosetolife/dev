3ae8755647aa15f44b3b924cd0206f6ff94eb69f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 09:36:33 -0400

    2024-06-20T13:36:32.900Z function_auto_after html_id

--- public/src/html_id.mjs
+++ public/src/html_id.mjs
@@ -1,3 +1,5 @@
+import { html_attribute_get } from "./html_attribute_get.mjs";
+import { html_attribute_has } from "./html_attribute_has.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { html_attribute_set } from "./html_attribute_set.mjs";
 import { number_to_letters } from "./number_to_letters.mjs";
@@ -5,6 +7,9 @@ import { object_property_initialize } from "./object_property_initialize.mjs";
 import { html_scrollable_hide } from "./html_scrollable_hide.mjs";
 import { global_function } from "./global_function.mjs";
 export function html_id(component) {
+  if (html_attribute_has(component, "id")) {
+    return html_attribute_get(component, "id");
+  }
   let data = global_function(html_scrollable_hide);
   let id_next = object_property_initialize(data, "id_next", 0);
   let id = number_to_letters(id_next);
