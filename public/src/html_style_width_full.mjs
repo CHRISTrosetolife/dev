a7e284eec1abfa2372a9bb6c00d76ef38111530a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 15:49:25 -0400

    2024-06-20T19:49:25.088Z function_auto_after html_style_width_full

--- public/src/html_style_width_full.mjs
+++ public/src/html_style_width_full.mjs
@@ -1,7 +1,5 @@
 import { html_style_width } from "./html_style_width.mjs";
-import { html_style_border_box } from "./html_style_border_box.mjs";
 export function html_style_width_full(component) {
-  html_style_border_box(component);
   let value = "100%";
   html_style_width(component, value);
 }
