a48815d1e528524c5b86f9b09cff01d46a09b9cb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 18:13:23 -0400

    2024-06-20T22:13:23.454Z function_auto_after app_gs_map_new

--- public/src/app_gs_map_new.mjs
+++ public/src/app_gs_map_new.mjs
@@ -29,6 +29,7 @@ import { object_merge_properties } from "./object_merge_properties.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_concat_multiple } from "./list_concat_multiple.mjs";
 import { list_filter } from "./list_filter.mjs";
+import { subtract } from "./subtract.mjs";
 export function app_gs_map_new() {
   let { y_extend, x_extend } = app_gs_map_extends();
   let border_thickness_y = y_extend;
@@ -164,6 +165,7 @@ export function app_gs_map_new() {
     each(list_xy(), (xy) => {
       object_property_change(copy, xy, () => {
         let delata = integer_random(1, 2);
+        [add, subtract];
         return;
       });
     });
