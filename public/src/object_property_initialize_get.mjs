77574051c45c7fbe13e7ca215c14bae443041e4a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 13:46:59 -0400

    2024-06-20T17:46:59.031Z function_auto_after object_property_initialize_get

--- public/src/object_property_initialize_get.mjs
+++ public/src/object_property_initialize_get.mjs
@@ -3,7 +3,7 @@ import { object_property_get } from "./object_property_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 export function object_property_initialize_get(lookup, key, initial) {
   if (object_property_exists_not(lookup, key)) {
-    object_property_set(lookup, key, initial);
+    object_property_set(lookup, key, initial());
   }
   return object_property_get(lookup, key);
 }
