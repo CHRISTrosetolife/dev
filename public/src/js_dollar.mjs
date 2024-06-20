2eef146b6278fabd1d9b2e756c0dc8508981a991
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 17:53:45 -0400

    2024-06-20T21:53:45.066Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { js_visit_identifiers } from "./js_visit_identifiers.mjs";
@@ -8,6 +9,9 @@ export function js_dollar(ast) {
     let prefix = "$";
     if (string_starts_with(name, prefix)) {
       let remaining = string_prefix_without(name, prefix);
+      log({
+        remaining,
+      });
     }
   });
 }
