1b9fadc18ff9d36312a0cf035b0eca372db6855b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 18:01:59 -0400

    2024-06-20T22:01:58.385Z function_auto_after command_line_generic

--- public/src/command_line_generic.mjs
+++ public/src/command_line_generic.mjs
@@ -1,5 +1,5 @@
+import { log_error_write } from "./log_error_write.mjs";
 import { log_write } from "./log_write.mjs";
-import { log_error } from "./log_error.mjs";
 import { string_split_space } from "./string_split_space.mjs";
 import { list_first_remaining } from "./list_first_remaining.mjs";
 import { import_node } from "./import_node.mjs";
@@ -18,7 +18,7 @@ export async function command_line_generic(command, silent) {
       });
       child.stderr.setEncoding("utf8");
       child.stderr.on("data", function (data) {
-        log_error(data);
+        log_error_write(data);
       });
     }
     child.on("close", function (code) {
