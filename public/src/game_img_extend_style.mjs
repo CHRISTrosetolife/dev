1cd967811f3a19c487fe84462a4e51dae734a170
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 14:00:51 -0400

    2024-06-20T18:00:50.486Z function_auto_after game_img_extend_style

--- public/src/game_img_extend_style.mjs
+++ public/src/game_img_extend_style.mjs
@@ -13,9 +13,9 @@ export function game_img_extend_style(
   y_extend,
   x_extend,
 ) {
+  let height_tiles = 1 + 2 * y_extend;
   let width_tiles = 1 + 2 * x_extend;
   html_style_width(image, game_tile_units_css(width_tiles));
-  let height_tiles = 1 + 2 * y_extend;
   html_style_height(image, game_tile_units_css(height_tiles));
   html_style(image, {
     position: "absolute",
