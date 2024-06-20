cf1efe88ef72fad61c63f7bfb3263489c3a01cf0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 09:43:02 -0400

    2024-06-20T13:43:02.137Z function_auto_after html_attribute_get

--- public/src/html_attribute_get.mjs
+++ public/src/html_attribute_get.mjs
@@ -1,4 +1,8 @@
+import { log } from "./log.mjs";
 export function html_attribute_get(component, name) {
   let { element } = component;
+  log({
+    element,
+  });
   element.getAttribute(name);
 }
