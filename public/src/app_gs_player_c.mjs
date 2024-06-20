41a28a94b865b643570eaed0b64f5146508c6836
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 18:57:01 -0400

    2024-06-20T22:57:01.236Z function_auto_after app_gs_player_c

--- public/src/app_gs_player_c.mjs
+++ public/src/app_gs_player_c.mjs
@@ -1,16 +1,6 @@
-import { game_img_style_transition } from "./game_img_style_transition.mjs";
-import { game_character_index } from "./game_character_index.mjs";
-import { game_img_character } from "./game_img_character.mjs";
-import { game_img } from "./game_img.mjs";
+import { app_gs_player_c_generic } from "./app_gs_player_c_generic.mjs";
 export function app_gs_player_c(map_c, map) {
   let y_x = map.player;
   let character = map.player.character;
-  let player_c = game_img(
-    map_c,
-    game_img_character(character, game_character_index("down")),
-    y_x,
-    "player",
-  );
-  game_img_style_transition(player_c);
-  return player_c;
+  return app_gs_player_c_generic(map_c, character, y_x);
 }
---
+++ public/src/app_gs_player_c_generic.mjs
@@ -0,0 +1,14 @@
+import { game_img_style_transition } from "./game_img_style_transition.mjs";
+import { game_character_index } from "./game_character_index.mjs";
+import { game_img_character } from "./game_img_character.mjs";
+import { game_img } from "./game_img.mjs";
+export function app_gs_player_c_generic(map_c, character, y_x) {
+  let player_c = game_img(
+    map_c,
+    game_img_character(character, game_character_index("down")),
+    y_x,
+    "player",
+  );
+  game_img_style_transition(player_c);
+  return player_c;
+}
