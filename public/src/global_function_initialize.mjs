4f866898d85ac63bcefc623738861f92db10ef7f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 13:47:32 -0400

    2024-06-20T17:47:32.367Z function_auto_after global_function_initialize

--- public/src/global_function_initialize.mjs
+++ public/src/global_function_initialize.mjs
@@ -1,7 +1,7 @@
-import { object_property_initialize } from "./object_property_initialize.mjs";
+import { object_property_initialize_get } from "./object_property_initialize_get.mjs";
 import { global_get } from "./global_get.mjs";
 export function global_function_initialize(fn, value) {
   let g = global_get();
-  let counts = object_property_initialize(g, fn.name, value);
+  let counts = object_property_initialize_get(g, fn.name, value);
   return counts;
 }
