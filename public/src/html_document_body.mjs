41e78f9389b5e9de3a0fca3dd4422a8eebb0401b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 09:37:30 -0400

    2024-06-20T13:37:29.658Z function_auto_after html_document_body

--- public/src/html_document_body.mjs
+++ public/src/html_document_body.mjs
@@ -1,6 +1,8 @@
+import { html_id_generate } from "./html_id_generate.mjs";
 export function html_document_body() {
   let result = {
     element: document.body,
   };
+  html_id_generate(result);
   return result;
 }
