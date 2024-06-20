26fa4edfcc0ac798406149eed313f095b30a50c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 04:59:11 -0400

    2024-06-20T08:59:10.699Zfunction_auto_after powershell

--- public/src/powershell.mjs
+++ public/src/powershell.mjs
@@ -1,18 +1,7 @@
-import { error } from "./error.mjs";
-import { import_node } from "./import_node.mjs";
+import { command_line_generic } from "./command_line_generic.mjs";
 export async function powershell(command) {
   let options = {
     shell: "powershell.exe",
   };
-  let c = await import_node("child_process");
-  let { exec } = c;
-  return await new Promise((resolve) => {
-    exec(command, options, (error, stdout, stderr) => {
-      resolve({
-        error,
-        stdout,
-        stderr,
-      });
-    });
-  });
+  return await command_line_generic(command, options);
 }
