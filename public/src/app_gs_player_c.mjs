284672eef1fd2a5fb9a740ef10461e9db8d258ba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 18:57:15 -0400

    2024-06-20T22:57:14.770Z function_auto_after app_gs_player_c

--- public/src/app_gs_player_c.mjs
+++ public/src/app_gs_player_c.mjs
@@ -1,6 +1,4 @@
 import { app_gs_player_c_generic } from "./app_gs_player_c_generic.mjs";
 export function app_gs_player_c(map_c, map) {
-  let y_x = map.player;
-  let character = map.player.character;
-  return app_gs_player_c_generic(map_c, character, y_x);
+  return app_gs_player_c_generic(map_c, map.player.character, map.player);
 }
