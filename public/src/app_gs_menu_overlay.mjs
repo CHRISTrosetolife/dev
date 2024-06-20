d554c0393a5a755eca772f3fef864b558e5fec2e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 15:13:18 -0400

    2024-06-20T19:13:14.307Z function_auto_after app_gs_map_cell

--- public/src/app_gs_menu_overlay.mjs
+++ public/src/app_gs_menu_overlay.mjs
@@ -5,15 +5,15 @@ import { html_style_background_color } from "./html_style_background_color.mjs";
 import { game_tile_units_css } from "./game_tile_units_css.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_div } from "./html_div.mjs";
-export function app_gs_menu_overlay(map_c, map) {
+export function app_gs_menu_overlay(map_c, coordinates) {
   let menu = html_div(map_c);
   html_style(menu, {
     padding: game_tile_units_css(0.25),
   });
   html_style_background_color(menu, "rgba(1,1,1,0.25)");
   let { x_count, y_count } = app_gs_screen_size();
-  let position_y = subtract(map.player.y, (y_count - 1) / 2);
-  let position_x = subtract(map.player.x, (x_count - 1) / 2);
+  let position_y = subtract(coordinates.y, (y_count - 1) / 2);
+  let position_x = subtract(coordinates.x, (x_count - 1) / 2);
   game_img_style_generic(
     menu,
     y_count,
