9f12ad78a3cacd1f7f87290be7aaa5471baf4ecf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 13:50:23 -0400

    2024-06-20T17:50:22.656Z function_auto_after app_gs_z_index

--- public/src/app_gs_z_index.mjs
+++ public/src/app_gs_z_index.mjs
@@ -1,5 +1,5 @@
 import { app_gs_z_indexes } from "./app_gs_z_indexes.mjs";
 import { list_index } from "./list_index.mjs";
 export function app_gs_z_index(z_index_name) {
-  return list_index(app_gs_z_indexes(), "tile");
+  return list_index(app_gs_z_indexes(), z_index_name);
 }
