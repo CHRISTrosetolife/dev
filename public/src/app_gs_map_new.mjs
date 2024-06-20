1477a7597538028ca59111b5a8202f38f70666df
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 16:27:33 -0400

    2024-06-20T20:27:33.208Z function_auto_after app_gs_map_new

--- public/src/app_gs_map_new.mjs
+++ public/src/app_gs_map_new.mjs
@@ -127,7 +127,7 @@ export function app_gs_map_new() {
     }
     if (x_left && y_bottom) {
       if (x_even === y_even) {
-        overlay_id = 4;
+        overlay_id = 33;
       } else {
         overlay_id = 9;
       }
