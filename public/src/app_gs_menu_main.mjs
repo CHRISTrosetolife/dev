4868b6e53f3b182700307aff24d78e261306ed35
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 14:17:23 -0400

    2024-06-20T18:17:22.831Z function_auto_after app_gs_menu_main

--- public/src/app_gs_menu_main.mjs
+++ public/src/app_gs_menu_main.mjs
@@ -1,3 +1,4 @@
+import { app_gs_tiles } from "./app_gs_tiles.mjs";
 import { app_gs_menu_system_logs } from "./app_gs_menu_system_logs.mjs";
 import { html_remove } from "./html_remove.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
@@ -9,5 +10,8 @@ export function app_gs_menu_main(menu_overlay) {
   });
   html_button_width_full_text_click(menu_overlay, "system logs", () => {
     app_gs_menu_system_logs(menu_overlay);
+  });
+  html_button_width_full_text_click(menu_overlay, "tiles", () => {
+    app_gs_tiles(menu_overlay);
   });
 }
