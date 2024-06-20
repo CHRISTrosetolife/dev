3e44526bbe3e4dd2f85d942d0c587e4f6e204611
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 18:30:53 -0400

    2024-06-20T22:30:52.956Z

--- public/src/app_gs_at_single.mjs
+++
@@ -1,8 +0,0 @@
-import { app_gs_map_at } from "./app_gs_map_at.mjs";
-import { list_single } from "./list_single.mjs";
-import { list_adder } from "./list_adder.mjs";
-export function app_gs_at_single(grid, a) {
-  let ts = list_adder((la) => app_gs_map_at(grid, a, la));
-  let t = list_single(ts);
-  return t;
-}
