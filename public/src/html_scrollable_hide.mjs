8b8c04ba3761334e00538147b99bf76b81d27895
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 08:38:38 -0400

    2024-06-20T12:38:37.860Z function_auto_after html_scrollable_hide

--- public/src/html_scrollable_hide.mjs
+++ public/src/html_scrollable_hide.mjs
@@ -6,8 +6,7 @@ import { html_inner_set } from "./html_inner_set.mjs";
 import { html_element } from "./html_element.mjs";
 import { html_attribute_set } from "./html_attribute_set.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
-let id = 1;
-export async function html_scrollable_hide(map_c) {
+export function html_scrollable_hide(map_c) {
   let data = global_function(html_scrollable_hide);
   let id_next = object_property_initialize(data, "id_next", 0);
   html_attribute_set(map_c, "id", number_to_letters(id_next));
