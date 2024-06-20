a12ca386248064f80f72ab94e26422ff4ae56fb3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 14:53:04 -0400

    2024-06-20T18:53:03.335Z function_auto_after app_gs_tiles

--- public/src/app_gs_tiles.mjs
+++ public/src/app_gs_tiles.mjs
@@ -1,3 +1,4 @@
+import { html_style } from "./html_style.mjs";
 import { html_div } from "./html_div.mjs";
 import { html_br } from "./html_br.mjs";
 import { html_img } from "./html_img.mjs";
@@ -5,6 +6,9 @@ import { each_range } from "./each_range.mjs";
 import { game_img_base } from "./game_img_base.mjs";
 export function app_gs_tiles(root) {
   let container = html_div(root);
+  html_style(container, {
+    "line-height": 0,
+  });
   let columns = 8;
   each_range(1063, (i) => {
     if (i > 0 && i % columns === 0) {
