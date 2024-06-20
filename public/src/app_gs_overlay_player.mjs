c2c5149b5fd8719b3aa987e1afff5b449a50620d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 13:57:45 -0400

    2024-06-20T17:57:44.166Z function_auto_after app_gs_overlay_player

--- public/src/app_gs_overlay_player.mjs
+++ public/src/app_gs_overlay_player.mjs
@@ -1,4 +1,3 @@
-import { app_gs_z_index } from "./app_gs_z_index.mjs";
 import { game_character_indices } from "./game_character_indices.mjs";
 import { game_img_style_transition } from "./game_img_style_transition.mjs";
 import { game_character_index } from "./game_character_index.mjs";
@@ -10,7 +9,7 @@ export function app_gs_overlay_player(map_c, map) {
     map_c,
     game_img_character(map.player.character, game_character_index("down")),
     map.player,
-    app_gs_z_index("player"),
+    "player",
   );
   game_img_style_transition(player_c);
   return player_c;
