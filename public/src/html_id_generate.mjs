b5b78248ad51c139f674cf569c4cc641772efb86
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 09:38:14 -0400

    2024-06-20T13:38:13.910Z

--- public/src/html_id_generate.mjs
+++
@@ -1,8 +0,0 @@
-import { object_merge } from "./object_merge.mjs";
-import { html_id } from "./html_id.mjs";
-export function html_id_generate(result) {
-  let id = html_id(result);
-  object_merge(result, {
-    id,
-  });
-}
