3833abbd23d6e6fa6cf8ab60e5bcf9dd5060f091
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 14:40:08 -0400

    2024-06-20T18:40:07.971Z function_auto_after app_sandbox

--- public/src/app_sandbox.mjs
+++ public/src/app_sandbox.mjs
@@ -1,9 +1,11 @@
+import { html_document_body } from "./html_document_body.mjs";
 import { app_gs_tiles } from "./app_gs_tiles.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_style_width_full } from "./html_style_width_full.mjs";
 import { html_style_centered } from "./html_style_centered.mjs";
 import { html_div } from "./html_div.mjs";
 export function app_sandbox() {
+  let body = html_document_body();
   let container_outer = html_div(menu_overlay);
   html_style_centered(container_outer);
   html_style_width_full(container_outer);
