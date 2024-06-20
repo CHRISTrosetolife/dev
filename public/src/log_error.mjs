009ea81802cdd65250d09e50353253cc02c1c5dc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 18:01:28 -0400

    2024-06-20T22:01:27.714Z function_auto_after log_error

--- public/src/log_error.mjs
+++ public/src/log_error.mjs
@@ -2,5 +2,5 @@ import { chalk } from "./chalk.mjs";
 import { log } from "./log.mjs";
 export function log_error(message) {
   let fn = log;
-  log(chalk().redBright(message));
+  fn(chalk().redBright(message));
 }
