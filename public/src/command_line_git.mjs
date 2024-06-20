fb748ed26542cb9caa97df5c806d115e01978731
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 05:13:53 -0400

    2024-06-20T09:13:52.858Zfunction_auto_after command_line_git

--- public/src/command_line_git.mjs
+++ public/src/command_line_git.mjs
@@ -1,4 +1,5 @@
 import { command_line_generic } from "./command_line_generic.mjs";
 export async function command_line_git(args) {
-  await command_line_generic(`git ${args}`, true);
+  let command = `git ${args}`;
+  await command_line_generic(command, true);
 }
