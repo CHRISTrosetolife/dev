f8893cf183bc1134257d4d765710691e7299939e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 18:36:48 -0400

    2024-06-20T22:36:47.708Z

---
+++ public/src/js_parse_first_function.mjs
@@ -0,0 +1,13 @@
+import { js_code_function_declare } from "./js_code_function_declare.mjs";
+import { js_parse } from "./js_parse.mjs";
+import { list_first } from "./list_first.mjs";
+export function js_parse_first_function(code) {
+  let f = js_code_function_declare("a", "", code);
+  let parsed = js_parse(f);
+  let { body } = parsed;
+  let fd = list_first(body);
+  let { body: block_statement } = fd;
+  let { body: bs_body } = block_statement;
+  let bs_body_first = list_first(bs_body);
+  return bs_body_first;
+}
