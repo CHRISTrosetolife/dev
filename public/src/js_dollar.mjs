d2636d2a67108babfd56a9181267c99092019149
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 17:52:31 -0400

    2024-06-20T21:52:30.684Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -1,3 +1,4 @@
+import { string_prefix_without } from "./string_prefix_without.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { js_visit_identifiers } from "./js_visit_identifiers.mjs";
 export function js_dollar(ast) {
@@ -5,6 +6,7 @@ export function js_dollar(ast) {
     let { node } = v;
     let { name } = node;
     if (string_starts_with(node, "$")) {
+      let remaining = string_prefix_without;
     }
   });
 }
