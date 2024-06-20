bf7cfd09320d7c3f6669a7b335643d0a20af5297
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 14:08:17 -0400

    2024-06-20T18:08:17.172Z function_auto_after app_gs_tiles

--- public/src/app_gs_tiles.mjs
+++ public/src/app_gs_tiles.mjs
@@ -1,8 +1,10 @@
+import { html_document_body } from "./html_document_body.mjs";
 import { game_img_base } from "./game_img_base.mjs";
 import { game_img } from "./game_img.mjs";
 export function app_gs_tiles() {
+  let root = html_document_body();
   game_img(
-    map_c,
+    root,
     game_img_base(0),
     {
       x: 0,
