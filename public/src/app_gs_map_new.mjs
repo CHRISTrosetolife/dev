cffc1cfa2a96392914f0fda346300631eaca0d64
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 18:08:00 -0400

    2024-06-20T22:08:00.300Z function_auto_after app_gs_map_new

--- public/src/app_gs_map_new.mjs
+++ public/src/app_gs_map_new.mjs
@@ -156,7 +156,9 @@ export function app_gs_map_new() {
     overlay_add(tile, id);
   });
   let npc_count = 1;
-  each_range(npc_count, () => {});
+  each_range(npc_count, () => {
+    object_merge(map.player, spawn);
+  });
   map.player = {};
   object_merge(map.player, spawn);
   map.player.walk_offset = 0;
