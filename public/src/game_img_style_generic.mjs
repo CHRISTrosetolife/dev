dead5019c87763670273d4ee8f26c006d8f1a37f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 14:04:26 -0400

    2024-06-20T18:04:25.888Z function_auto_after game_img_style_generic

--- public/src/game_img_style_generic.mjs
+++ public/src/game_img_style_generic.mjs
@@ -5,18 +5,18 @@ import { html_style_width } from "./html_style_width.mjs";
 import { game_tile_units_css } from "./game_tile_units_css.mjs";
 import { html_style_height } from "./html_style_height.mjs";
 export function game_img_style_generic(
-  image,
+  component,
   height_tiles,
   width_tiles,
   z_index_name,
   y_position,
   x_position,
 ) {
-  html_style_height(image, game_tile_units_css(height_tiles));
-  html_style_width(image, game_tile_units_css(width_tiles));
-  html_style(image, {
+  html_style_height(component, game_tile_units_css(height_tiles));
+  html_style_width(component, game_tile_units_css(width_tiles));
+  html_style(component, {
     position: "absolute",
     "z-index": app_gs_z_index(z_index_name),
   });
-  game_img_position(image, y_position, x_position);
+  game_img_position(component, y_position, x_position);
 }
