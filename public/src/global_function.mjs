9a13ac33d7cbb5166e44bc5633021a18672a7488
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 13:54:24 -0400

    2024-06-20T17:54:23.787Z function_auto_after global_function

--- public/src/global_function.mjs
+++ public/src/global_function.mjs
@@ -1,5 +1,4 @@
 import { global_function_initialize } from "./global_function_initialize.mjs";
 export function global_function(fn) {
-  let value = {};
-  return global_function_initialize(fn, () => value);
+  return global_function_initialize(fn, () => ({}));
 }
