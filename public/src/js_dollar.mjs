cbedd66c8b13b629ae1b9b5de92f7e74f219f256
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 15:31:04 -0400

    2024-06-21T19:31:03.972Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -97,6 +97,7 @@ export function js_dollar(ast) {
       let objection_prefix = "o";
       if (string_starts_with(remaining, objection_prefix)) {
         remaining = string_prefix_without(remaining, scm_prefix);
+        let count = 1;
         if (string_empty_not_is(remaining)) {
           let count = integer_parse(remaining);
         }
