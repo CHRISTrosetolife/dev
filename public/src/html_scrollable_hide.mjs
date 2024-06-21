8be3df6e641c815375e317a672646397ed82ca5b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 14:10:32 -0400

    2024-06-21T18:10:31.684Z function_auto_after html_scrollable_hide

--- public/src/html_scrollable_hide.mjs
+++ public/src/html_scrollable_hide.mjs
@@ -2,13 +2,9 @@ import { html_id } from "./html_id.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
 import { html_element } from "./html_element.mjs";
-import { log } from "./log.mjs";
 export function html_scrollable_hide(root, map_c) {
   let style = html_element(root, "style");
   let id = html_id(root);
-  log({
-    id,
-  });
   html_inner_set(
     style,
     `#${id}::-webkit-scrollbar {
