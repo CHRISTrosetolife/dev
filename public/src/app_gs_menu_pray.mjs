19e88a46120194dbf0bf30d3fff5dc182fd42839
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 09:58:36 -0400

    2024-06-21T13:58:36.185Z function_auto_after app_gs_menu_pray

--- public/src/app_gs_menu_pray.mjs
+++ public/src/app_gs_menu_pray.mjs
@@ -3,6 +3,7 @@ import { app_gs_menu_main } from "./app_gs_menu_main.mjs";
 import { html_button_width_full_text_click_back } from "./html_button_width_full_text_click_back.mjs";
 import { html_clear } from "./html_clear.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { list_random_item } from "./list_random_item.mjs";
 export function app_gs_menu_pray(menu_overlay, map_c, map) {
   html_clear(menu_overlay);
   html_button_width_full_text_click_back(menu_overlay, () =>
@@ -10,7 +11,7 @@ export function app_gs_menu_pray(menu_overlay, map_c, map) {
   );
   let prayer = string_combine_multiple([
     "🙏 God, please ",
-    "bless",
+    list_random_item(["bless"]),
     " my next 👥 conversation, in the name of Jesus, amen.",
   ]);
   html_button_width_full_text_click(menu_overlay, prayer, () => {
