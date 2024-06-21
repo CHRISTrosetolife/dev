f0bcc925ec0baea9661158b8fc0962dddc811d9e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 21:52:35 -0400

    2024-06-21T01:52:34.284Z function_auto_after app_gs_overlay_wall_is

--- public/src/app_gs_overlay_wall_is.mjs
+++ public/src/app_gs_overlay_wall_is.mjs
@@ -1,5 +1,7 @@
 import { app_gs_overlays_wall } from "./app_gs_overlays_wall.mjs";
 import { list_includes } from "./list_includes.mjs";
 export function app_gs_overlay_wall_is(o) {
-  return list_includes(app_gs_overlays_wall(), o.id);
+  if (o.type === "base") {
+    return list_includes(app_gs_overlays_wall(), o.id);
+  }
 }
