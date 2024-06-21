7abc69ea443ad3dec385ab5128d80f61d34f688d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 15:31:09 -0400

    2024-06-21T19:31:08.646Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -99,7 +99,7 @@ export function js_dollar(ast) {
         remaining = string_prefix_without(remaining, scm_prefix);
         let count = 1;
         if (string_empty_not_is(remaining)) {
-          let count = integer_parse(remaining);
+          count = integer_parse(remaining);
         }
         let e = js_parse_expression("{text:'',count:1}");
         object_replace(node, e);
