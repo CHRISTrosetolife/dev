4e1c2aab169134f3609791d22f5ecf7d4481515a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 09:46:29 -0400

    2024-06-21T13:46:28.656Z function_auto_after app_gs_menu_main

--- public/src/app_gs_menu_main.mjs
+++ public/src/app_gs_menu_main.mjs
@@ -9,13 +9,9 @@ export function app_gs_menu_main(menu_overlay, map_c, map) {
   html_button_width_full_text_click_back(menu_overlay, () => {
     html_remove(menu_overlay);
   });
-  html_button_width_full_text_click(
-    menu_overlay,
-    "🙏 pray for conversation",
-    () => {
-      html_remove(menu_overlay);
-    },
-  );
+  html_button_width_full_text_click(menu_overlay, "🙏 pray", () => {
+    html_remove(menu_overlay);
+  });
   html_button_width_full_text_click(menu_overlay, "💻 system logs", () => {
     app_gs_menu_system_logs(menu_overlay);
   });
