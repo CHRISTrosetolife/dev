5366d6e2e2ee68f929c994aee4f9f99ec2cd185c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 14:05:11 -0400

    2024-06-21T18:05:10.773Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -1,6 +1,4 @@
-import { equal_2 } from "./equal_2.mjs";
-import { app_gs_phrase_exclaim_or_period } from "./app_gs_phrase_exclaim_or_period.mjs";
-import { random_50_50 } from "./random_50_50.mjs";
+import { app_gs_conversation_opening } from "./app_gs_conversation_opening.mjs";
 import { html_style_units } from "./html_style_units.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_div } from "./html_div.mjs";
@@ -8,123 +6,15 @@ import { app_gs_overlay_speech } from "./app_gs_overlay_speech.mjs";
 import { app_gs_div_rounded_transparent } from "./app_gs_div_rounded_transparent.mjs";
 import { html_style_font_color } from "./html_style_font_color.mjs";
 import { html_p_text } from "./html_p_text.mjs";
-import { app_gs_phrase_it_is } from "./app_gs_phrase_it_is.mjs";
-import { app_gs_phrase_i_am } from "./app_gs_phrase_i_am.mjs";
-import { list_join_space } from "./list_join_space.mjs";
 import { html_remove } from "./html_remove.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { html_style_rounded_padded } from "./html_style_rounded_padded.mjs";
-import { app_gs_phrase_very } from "./app_gs_phrase_very.mjs";
-import { list_random_item } from "./list_random_item.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { app_gs_menu_overlay } from "./app_gs_menu_overlay.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
-import { list_add } from "./list_add.mjs";
 export function app_gs_conversation(map_c, map, npc) {
   npc.conversation_count++;
-  let greeting = string_combine_multiple([
-    list_random_item(["Hello", "Hey", "Greetings", "Hi"]),
-    ", ",
-    map.player.name,
-    "!",
-  ]);
-  let opening = ["👋", greeting];
-  if (npc.conversation_count >= 2) {
-    if (random_50_50() || equal_2(npc.conversation_count)) {
-      list_add(
-        opening,
-        string_combine_multiple([
-          list_random_item([
-            string_combine_multiple([
-              app_gs_phrase_it_is(),
-              " ",
-              list_random_item(["good", "great", "wonderful", "nice"]),
-            ]),
-            string_combine_multiple([
-              app_gs_phrase_i_am(),
-              " ",
-              list_random_item(["happy", "glad"]),
-            ]),
-          ]),
-          " to ",
-          list_random_item(["talk to", "see"]),
-          " you again",
-          app_gs_phrase_exclaim_or_period(),
-        ]),
-      );
-    }
-  } else {
-    let introduction = string_combine_multiple([
-      list_random_item([
-        string_combine_multiple([app_gs_phrase_i_am(), " called"]),
-        "My name is",
-        "I go by",
-        string_combine_multiple([
-          list_random_item(["They", "You can", "Please"]),
-          " call me",
-        ]),
-      ]),
-      " ",
-      npc.name,
-      ".",
-    ]);
-    list_add(opening, introduction);
-    if (random_50_50()) {
-      let nice_to_meet = string_combine_multiple([
-        list_random_item([
-          string_combine_multiple([
-            app_gs_phrase_it_is(),
-            " ",
-            list_random_item([
-              "my pleasure",
-              string_combine(
-                app_gs_phrase_very(),
-                list_random_item(["wonderful", "nice"]),
-              ),
-            ]),
-          ]),
-          string_combine_multiple([
-            app_gs_phrase_i_am(),
-            " ",
-            list_random_item([
-              "happy",
-              "glad",
-              string_combine(app_gs_phrase_very(), "pleased"),
-            ]),
-          ]),
-        ]),
-        " to ",
-        list_random_item(["meet you", "make your acquaintance"]),
-        app_gs_phrase_exclaim_or_period(),
-      ]);
-      list_add(opening, nice_to_meet);
-    }
-  }
-  list_add(
-    opening,
-    string_combine_multiple([
-      "What",
-      list_random_item([
-        string_combine_multiple([list_random_item(["'", " i"]), "s up"]),
-        string_combine_multiple([
-          " ",
-          list_random_item([
-            string_combine_multiple([
-              "is on your ",
-              list_random_item(["heart", "mind"]),
-            ]),
-            string_combine_multiple([
-              list_random_item(["do you want", "would you like"]),
-              " to talk about",
-            ]),
-          ]),
-        ]),
-      ]),
-      "?",
-    ]),
-  );
-  let text = string_combine_multiple([list_join_space(opening)]);
+  let text = app_gs_conversation_opening(map, npc);
   let menu_overlay = app_gs_menu_overlay(map_c, map);
   let div_npc = app_gs_overlay_speech(menu_overlay);
   let npc_speaking = html_p_text(div_npc, string_combine(npc.name, " says :"));
---
+++ public/src/app_gs_conversation_opening.mjs
@@ -0,0 +1,116 @@
+import { list_join_space } from "./list_join_space.mjs";
+import { app_gs_phrase_very } from "./app_gs_phrase_very.mjs";
+import { string_combine } from "./string_combine.mjs";
+import { app_gs_phrase_exclaim_or_period } from "./app_gs_phrase_exclaim_or_period.mjs";
+import { app_gs_phrase_i_am } from "./app_gs_phrase_i_am.mjs";
+import { app_gs_phrase_it_is } from "./app_gs_phrase_it_is.mjs";
+import { list_add } from "./list_add.mjs";
+import { equal_2 } from "./equal_2.mjs";
+import { random_50_50 } from "./random_50_50.mjs";
+import { list_random_item } from "./list_random_item.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function app_gs_conversation_opening(map, npc) {
+  let greeting = string_combine_multiple([
+    list_random_item(["Hello", "Hey", "Greetings", "Hi"]),
+    ", ",
+    map.player.name,
+    "!",
+  ]);
+  let opening = ["👋", greeting];
+  if (npc.conversation_count >= 2) {
+    if (random_50_50() || equal_2(npc.conversation_count)) {
+      list_add(
+        opening,
+        string_combine_multiple([
+          list_random_item([
+            string_combine_multiple([
+              app_gs_phrase_it_is(),
+              " ",
+              list_random_item(["good", "great", "wonderful", "nice"]),
+            ]),
+            string_combine_multiple([
+              app_gs_phrase_i_am(),
+              " ",
+              list_random_item(["happy", "glad"]),
+            ]),
+          ]),
+          " to ",
+          list_random_item(["talk to", "see"]),
+          " you again",
+          app_gs_phrase_exclaim_or_period(),
+        ]),
+      );
+    }
+  } else {
+    let introduction = string_combine_multiple([
+      list_random_item([
+        string_combine_multiple([app_gs_phrase_i_am(), " called"]),
+        "My name is",
+        "I go by",
+        string_combine_multiple([
+          list_random_item(["They", "You can", "Please"]),
+          " call me",
+        ]),
+      ]),
+      " ",
+      npc.name,
+      ".",
+    ]);
+    list_add(opening, introduction);
+    if (random_50_50()) {
+      let nice_to_meet = string_combine_multiple([
+        list_random_item([
+          string_combine_multiple([
+            app_gs_phrase_it_is(),
+            " ",
+            list_random_item([
+              "my pleasure",
+              string_combine(
+                app_gs_phrase_very(),
+                list_random_item(["wonderful", "nice"]),
+              ),
+            ]),
+          ]),
+          string_combine_multiple([
+            app_gs_phrase_i_am(),
+            " ",
+            list_random_item([
+              "happy",
+              "glad",
+              string_combine(app_gs_phrase_very(), "pleased"),
+            ]),
+          ]),
+        ]),
+        " to ",
+        list_random_item(["meet you", "make your acquaintance"]),
+        app_gs_phrase_exclaim_or_period(),
+      ]);
+      list_add(opening, nice_to_meet);
+    }
+  }
+  list_add(
+    opening,
+    string_combine_multiple([
+      "What",
+      list_random_item([
+        string_combine_multiple([list_random_item(["'", " i"]), "s up"]),
+        string_combine_multiple([
+          " ",
+          list_random_item([
+            string_combine_multiple([
+              "is on your ",
+              list_random_item(["heart", "mind"]),
+            ]),
+            string_combine_multiple([
+              list_random_item(["do you want", "would you like"]),
+              " to talk about",
+            ]),
+          ]),
+        ]),
+      ]),
+      "?",
+    ]),
+  );
+  let text = string_combine_multiple([list_join_space(opening)]);
+  return text;
+}
