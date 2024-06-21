b7caebe499c9a3856093e1cb8cbc66b148f7e07b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 09:15:57 -0400

    2024-06-21T13:15:57.028Z function_auto_after html_data_set

--- public/src/html_data_set.mjs
+++ public/src/html_data_set.mjs
@@ -1,5 +1,5 @@
 import { string_combine } from "./string_combine.mjs";
 import { html_attribute_set } from "./html_attribute_set.mjs";
-export function html_data_set(clicker, data_name, base_id) {
-  html_attribute_set(clicker, string_combine("data-", data_name), base_id);
+export function html_data_set(component, data_name, data_value) {
+  html_attribute_set(component, string_combine("data-", data_name), data_value);
 }
