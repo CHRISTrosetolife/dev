e187de92c12a2b3cc69c15260db326753f8f5583
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 22:17:54 -0400

    2024-06-21T02:17:53.885Z function_auto_after list_find_property

--- public/src/list_find_property.mjs
+++ public/src/list_find_property.mjs
@@ -1,7 +1,10 @@
 import { list_single } from "./list_single.mjs";
 import { list_filter_property } from "./list_filter_property.mjs";
+import { list_size_1 } from "./list_size_1.mjs";
 export function list_find_property(list, property_name, value) {
   let npcs = list_filter_property(list, property_name, value);
-  let npc = list_single(npcs);
-  return npc;
+  if (list_size_1(npcs)) {
+    let npc = list_single(npcs);
+    return npc;
+  }
 }
