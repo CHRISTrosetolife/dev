ae95e063998576b7ec6823ea4e03aa14fb63c84a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 12:48:53 -0400

    2024-06-21T16:48:52.933Z function_auto_after app_gs_div_rounded_transparent

--- public/src/app_gs_div_rounded_transparent.mjs
+++ public/src/app_gs_div_rounded_transparent.mjs
@@ -1,3 +1,4 @@
+import { html_style_rounded_padded } from "./html_style_rounded_padded.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_style_background_color } from "./html_style_background_color.mjs";
 import { game_tile_units_css } from "./game_tile_units_css.mjs";
@@ -6,12 +7,12 @@ import { html_div } from "./html_div.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function app_gs_div_rounded_transparent(parent, color, percent) {
   assert_arguments_length(arguments, 3);
-  let menu = html_div(parent);
-  html_style(menu, {
+  let div = html_div(parent);
+  html_style(div, {
     padding: game_tile_units_css(0.25),
   });
   html_style_background_color(
-    menu,
+    div,
     string_combine_multiple([
       "color-mix(in srgb, ",
       color,
@@ -20,5 +21,6 @@ export function app_gs_div_rounded_transparent(parent, color, percent) {
       "%, transparent)",
     ]),
   );
-  return menu;
+  html_style_rounded_padded(div);
+  return div;
 }
