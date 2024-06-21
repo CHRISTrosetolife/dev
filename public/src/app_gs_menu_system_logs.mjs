7910584ff67f85dcd7d3bba8a691173fd8bbf99e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 09:44:17 -0400

    2024-06-21T13:44:16.663Z function_auto_after app_gs_menu_system_logs

--- public/src/app_gs_menu_system_logs.mjs
+++ public/src/app_gs_menu_system_logs.mjs
@@ -3,11 +3,11 @@ import { html_button_width_full_text } from "./html_button_width_full_text.mjs";
 import { each } from "./each.mjs";
 import { log_logs } from "./log_logs.mjs";
 import { app_gs_menu_main } from "./app_gs_menu_main.mjs";
-import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { html_clear } from "./html_clear.mjs";
+import { html_button_width_full_text_click_back } from "./html_button_width_full_text_click_back.mjs";
 export function app_gs_menu_system_logs(menu_overlay) {
   html_clear(menu_overlay);
-  html_button_width_full_text_click(menu_overlay, "back", () =>
+  html_button_width_full_text_click_back(menu_overlay, () =>
     app_gs_menu_main(menu_overlay),
   );
   let logs = log_logs();
