8a5c7239b4399c12ccb937cd4be8ebc89661dd52
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 14:31:07 -0400

    2024-06-21T18:31:06.871Z   t

--- public/src/js_name_unique.mjs
+++ public/src/js_name_unique.mjs
@@ -2,7 +2,8 @@ import { list_includes } from "./list_includes.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { js_identifiers } from "./js_identifiers.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
-export function js_name_unique(ast, prefix) {assert_arguments_length()
+export function js_name_unique(ast, prefix) {
+  assert_arguments_length(arguments, 2);
   let identifiers = js_identifiers(ast);
   let i = 1;
   let variable_name;
