bf0624b7ff414dd026bc9edf98980fe98c36ffcc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 12:31:22 -0400

    2024-06-21T16:31:22.187Z function_auto_after app_gs_div_rounded_alpha

--- public/src/app_gs_div_rounded_alpha.mjs
+++ public/src/app_gs_div_rounded_alpha.mjs
@@ -2,7 +2,7 @@ import { html_style_background_color_alpha } from "./html_style_background_color
 import { game_tile_units_css } from "./game_tile_units_css.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_div } from "./html_div.mjs";
-export function app_gs_div_rounded_alpha(parent, rgb) {
+export function app_gs_div_rounded_alpha(parent, rgb, alpha) {
   let menu = html_div(parent);
   html_style(menu, {
     padding: game_tile_units_css(0.25),
