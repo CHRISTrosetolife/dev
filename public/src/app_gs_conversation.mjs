c84a8f074b1f71a39a1acd457b88de8b11ec0529
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 12:33:08 -0400

    2024-06-21T16:33:07.642Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -69,8 +69,8 @@ export function app_gs_conversation(map_c, map, npc) {
   let div = html_div(menu_overlay);
   let npc_speaking = html_p_text(div, string_combine(npc.name, " says :"));
   html_style_font_color(npc_speaking, "lightblue");
-  let npc_speech = app_gs_div_rounded_alpha(parent, "0,0,0", 0.25);
-  html_inner_set(div, npc_speech);
+  let npc_speech = app_gs_div_rounded_alpha(div, "0,0,0", 0.25);
+  html_inner_set(div, text);
   html_style_background_color_black_alpha(div, 0.51);
   html_style_centered(div);
   html_style_rounded_padded(div);
