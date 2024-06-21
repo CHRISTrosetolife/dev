3b83e968ff89bd8fe255ee4e60f8cc3baf018f7f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 12:13:06 -0400

    2024-06-21T16:13:06.165Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -64,6 +64,7 @@ export function app_gs_conversation(map_c, map, npc) {
   ]);
   let div = html_div(menu_overlay);
   html_p_text(div, text);
+  html_p_text(div, string_combine(npc.name, " says :"));
   html_style_background_color_black_alpha(div, 0.51);
   html_style_centered(div);
   html_style_rounded_padded(div);
