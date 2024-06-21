6dda4f20d78e2ba7c096dadf9b09984fec946ad6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 09:58:23 -0400

    2024-06-21T13:58:23.502Z function_auto_after app_gs_menu_pray

--- public/src/app_gs_menu_pray.mjs
+++ public/src/app_gs_menu_pray.mjs
@@ -2,13 +2,17 @@ import { html_button_width_full_text_click } from "./html_button_width_full_text
 import { app_gs_menu_main } from "./app_gs_menu_main.mjs";
 import { html_button_width_full_text_click_back } from "./html_button_width_full_text_click_back.mjs";
 import { html_clear } from "./html_clear.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_gs_menu_pray(menu_overlay, map_c, map) {
   html_clear(menu_overlay);
   html_button_width_full_text_click_back(menu_overlay, () =>
     app_gs_menu_main(menu_overlay, map_c, map),
   );
-  let prayer =
-    "🙏 God, please bless my next 👥 conversation, in the name of Jesus, amen.";
+  let prayer = string_combine_multiple([
+    "🙏 God, please ",
+    "bless",
+    " my next 👥 conversation, in the name of Jesus, amen.",
+  ]);
   html_button_width_full_text_click(menu_overlay, prayer, () => {
     map.player.pray.conversation = true;
   });
