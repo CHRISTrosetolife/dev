dddae0ee5d9bdc0df82cd49a054a58e0516d06c3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 15:54:06 -0400

    2024-06-21T19:54:05.335Z function_auto_after google_search_lucky

--- public/src/google_search_lucky.mjs
+++ public/src/google_search_lucky.mjs
@@ -1,8 +1,20 @@
-import { google_search_generic } from "./google_search_generic.mjs";
+import { browser_open } from "./browser_open.mjs";
+import { log } from "./log.mjs";
+import { url_secure_w3 } from "./url_secure_w3.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function google_search_lucky(query) {
   assert_arguments_length(arguments, 1);
-  let lucky = true;
   ("https://duckduckgo.com/?q=!ducky+");
-  await google_search_generic(query, lucky);
+  let url = string_combine_multiple([
+    url_secure_w3(),
+    "google.com/search?",
+    lucky_url,
+    "q=",
+    query,
+  ]);
+  log({
+    url,
+  });
+  await browser_open(url);
 }
