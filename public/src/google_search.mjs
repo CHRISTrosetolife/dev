47d3a13ed98471f68ffa90da238f1bf9f040b6ad
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 15:48:41 -0400

    2024-06-21T19:48:41.482Z function_auto_after google_search

--- public/src/google_search.mjs
+++ public/src/google_search.mjs
@@ -5,9 +5,9 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function google_search(query) {
   assert_arguments_length(arguments, 1);
-  let luck = false;
+  let lucky = false;
   query = string_to_url(query);
-  let lucky_url = "btnI=1&";
+  let lucky_url = lucky ? "btnI=1&" : "";
   await browser_open(
     string_combine_multiple([
       url_secure_w3(),
