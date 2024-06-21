cad888e3beeb7189e60f5bffbaa701c3a0a29e34
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 21:46:42 -0400

    2024-06-21T01:46:42.358Z function_auto_after app_gs_overlays_any_wall

--- public/src/app_gs_overlays_any_wall.mjs
+++ public/src/app_gs_overlays_any_wall.mjs
@@ -3,9 +3,13 @@ import { list_any } from "./list_any.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { equal_by } from "./equal_by.mjs";
+import { object_properties_new } from "./object_properties_new.mjs";
+import { list_xy } from "./list_xy.mjs";
 export function app_gs_overlays_any_wall(map, tile) {
   assert_arguments_length(arguments, 2);
   let w = list_any(tile.overlays, app_gs_overlay_wall_is);
-  list_filter(map.overlays, (o) => equal_by(o, tile));
+  list_filter(map.overlays, (o) =>
+    equal_by(o, tile, (a) => object_properties_new(a, list_xy())),
+  );
   return w;
 }
