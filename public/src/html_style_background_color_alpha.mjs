01b71ff9af0e2cfc1583a5122c9b6afed53f5124
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 12:46:16 -0400

    2024-06-21T16:46:15.315Z   t

--- public/src/html_style_background_color_alpha.mjs
+++
@@ -1,8 +0,0 @@
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { html_style_background_color } from "./html_style_background_color.mjs";
-export function html_style_background_color_alpha(menu, rgb, alpha) {
-  html_style_background_color(
-    menu,
-    string_combine_multiple(["rgba(", rgb, ",", alpha, ")"]),
-  );
-}
