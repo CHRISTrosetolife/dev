a21eb88c7a04bae6bf60f5bd534e42040740ae0e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 11:16:46 -0400

    2024-06-21T15:16:45.643Z function_auto_after js_code_call_args

--- public/src/js_code_call_args.mjs
+++ public/src/js_code_call_args.mjs
@@ -1,7 +1,7 @@
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { list_join_comma_space } from "./list_join_comma_space.mjs";
 export function js_code_call_args(function_name, args_code_list) {
-  assert_arguments_length;
+  assert_arguments_length(arguments, 2);
   let code = `${function_name}(${list_join_comma_space(args_code_list)})`;
   return code;
 }
