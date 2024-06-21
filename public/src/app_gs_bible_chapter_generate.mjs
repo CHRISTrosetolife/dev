43c81368b007f9b94fefeaf4b82f64d2d67652df
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 14:22:46 -0400

    2024-06-21T18:22:45.617Z function_auto_after app_gs_bible_chapter_generate

--- public/src/app_gs_bible_chapter_generate.mjs
+++ public/src/app_gs_bible_chapter_generate.mjs
@@ -7,7 +7,6 @@ import { list_map } from "./list_map.mjs";
 import { function_open } from "./function_open.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
-import { generate_list_generic } from "./generate_list_generic.mjs";
 import { bible_chapter } from "./bible_chapter.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
 export async function app_gs_bible_chapter_generate(chapter_name) {
@@ -18,7 +17,6 @@ export async function app_gs_bible_chapter_generate(chapter_name) {
     "_generate",
   );
   let name = string_combine_multiple([prefix, chapter_name]);
-  await generate_list_generic(verses, name);
   let delimited = list_map(list, string_delimit);
   await function_new_generic(
     name,
