ddc32d534aa6d0cee62d03412ca2116a6a1d1c30
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 13:29:27 -0400

    2024-06-21T17:29:26.829Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -76,7 +76,7 @@ export function js_dollar(ast) {
           );
           let index = list_index(parent, node);
           let next_index = index + 1;
-          list_map(range(count));
+          list_map(range(count), () => list_remove_at(parent, next_index));
           let next = list_remove_at(parent, next_index);
           list_add(list_first(e.arguments).elements, next);
           object_replace(node, e);
