9f6a0220341996754a5a26f6b29bfd6232b149b3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 13:35:52 -0400

    2024-06-21T17:35:52.378Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -100,9 +100,7 @@ export function app_gs_conversation(map_c, map, npc) {
     string_combine_multiple([
       "What ",
       list_random_item([
-        $s2,
-        "is on your ",
-        "heart",
+        string_combine_multiple(["is on your ", "heart"]),
         string_combine_multiple([
           list_random_item(["do you want", "would you like"]),
           " to talk about",
