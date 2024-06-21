84f2832f11aa1f872291b64aa02f0c116b1132ae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 12:04:02 -0400

    2024-06-21T16:04:02.133Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -47,10 +47,7 @@ export function app_gs_conversation(map_c, map, npc) {
         ]),
         app_gs_phrase_i_am(),
         " ",
-        list_random_item([
-          "happy",
-          string_combine([app_gs_phrase_very(), "pleased"]),
-        ]),
+        string_combine([app_gs_phrase_very(), "pleased"]),
       ],
     ),
     " to meet you!",
