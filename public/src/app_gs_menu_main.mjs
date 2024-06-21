ce1c56daf91e2ee285087f98f0a038ff0ab839cf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 09:44:47 -0400

    2024-06-21T13:44:47.218Z function_auto_after app_gs_menu_main

--- public/src/app_gs_menu_main.mjs
+++ public/src/app_gs_menu_main.mjs
@@ -9,7 +9,7 @@ export function app_gs_menu_main(menu_overlay, map_c, map) {
   html_button_width_full_text_click_back(menu_overlay, () => {
     html_remove(menu_overlay);
   });
-  html_button_width_full_text_click(menu_overlay, "pray", () => {
+  html_button_width_full_text_click(menu_overlay, "🙏 pray", () => {
     html_remove(menu_overlay);
   });
   html_button_width_full_text_click(menu_overlay, "💻 system logs", () => {
