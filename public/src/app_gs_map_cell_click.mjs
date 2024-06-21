41ea18516e976e140b0238312b0325e07e7cca1d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 10:10:50 -0400

    2024-06-21T14:10:49.884Z function_auto_after app_gs_map_cell_click

--- public/src/app_gs_map_cell_click.mjs
+++ public/src/app_gs_map_cell_click.mjs
@@ -16,6 +16,7 @@ import { app_gs_menu_main } from "./app_gs_menu_main.mjs";
 import { app_gs_menu_overlay } from "./app_gs_menu_overlay.mjs";
 import { app_gs_map_neighbors_get } from "./app_gs_map_neighbors_get.mjs";
 import { app_gs_overlays_any_wall } from "./app_gs_overlays_any_wall.mjs";
+import { html_style_centered } from "./html_style_centered.mjs";
 export async function app_gs_map_cell_click(map, map_c, player_c, tile) {
   let npc = list_find_property_or(tile.overlays, "type", "npc", null);
   if (null_is(npc)) {
@@ -48,6 +49,7 @@ export async function app_gs_map_cell_click(map, map_c, player_c, tile) {
       app_gs_walk_direction(npc_c, npc, player);
       let menu_overlay = app_gs_menu_overlay(map_c, map);
       let p = html_p_text(menu_overlay, "hello");
+      html_style_centered(p);
       html_button_width_full_text_click(
         menu_overlay,
         "politely end the conversation after praying",
