e63a11b6259eaa943583c92c975471fa70d7a1fc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 14:28:31 -0400

    2024-06-21T18:28:31.013Z function_auto_after js_code_arrow_block_args

--- public/src/js_code_arrow_block_args.mjs
+++ public/src/js_code_arrow_block_args.mjs
@@ -1,5 +1,11 @@
 import { list_join_comma_space } from "./list_join_comma_space.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function js_code_arrow_block_args(names, body_string) {
-  return string_combine_multiple(["(", list_join_comma_space(names), ")=>{}"]);
+  return string_combine_multiple([
+    "(",
+    list_join_comma_space(names),
+    ")=>{",
+    body_string,
+    "}",
+  ]);
 }
