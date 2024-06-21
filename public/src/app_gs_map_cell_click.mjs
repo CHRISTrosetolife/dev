0ab51e0282a8c98d67f76fa716403dd18d38031f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 22:19:01 -0400

    2024-06-21T02:19:00.448Z

--- public/src/app_gs_map_cell_click.mjs
+++ public/src/app_gs_map_cell_click.mjs
@@ -1,14 +1,14 @@
+import { list_find_property_or } from "./list_find_property_or.mjs";
 import { null_is } from "./null_is.mjs";
 import { app_gs_walk_path } from "./app_gs_walk_path.mjs";
 import { app_gs_xy_equal } from "./app_gs_xy_equal.mjs";
 import { app_gs_map_path } from "./app_gs_map_path.mjs";
-import { list_find_property } from "./list_find_property.mjs";
 import { app_gs_menu_main } from "./app_gs_menu_main.mjs";
 import { app_gs_menu_overlay } from "./app_gs_menu_overlay.mjs";
 import { app_gs_map_neighbors_get } from "./app_gs_map_neighbors_get.mjs";
 import { app_gs_overlays_any_wall } from "./app_gs_overlays_any_wall.mjs";
 export async function app_gs_map_cell_click(map, map_c, player_c, tile) {
-  let npc = list_find_property(tile.overlays, "type", "npc", null);
+  let npc = list_find_property_or(tile.overlays, "type", "npc", null);
   if (null_is(npc)) {
     let walls = app_gs_overlays_any_wall(tile);
     if (!walls) {
