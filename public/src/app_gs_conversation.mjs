83513fb9a8a8aea51ff438e4d252fddb019a9b9e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 12:24:26 -0400

    2024-06-21T16:24:23.651Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -74,7 +74,7 @@ export function app_gs_conversation(map_c, map, npc) {
   html_style_centered(div);
   html_style_rounded_padded(div);
   let hr = html_hr(div);
-  html_style_background_color(hr);
+  html_style_background_color(hr, "black");
   let prompt = html_p_text(div, "What would you like to do?");
   html_style_font_color(prompt, "lightgreen");
   html_button_width_full_text_click(
