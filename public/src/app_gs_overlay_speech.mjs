97e134ae9cbe0df6bcce47fb3d1973cdf5c10a15
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 12:53:30 -0400

    2024-06-21T16:53:29.352Z function_auto_after app_gs_overlay_speech

--- public/src/app_gs_overlay_speech.mjs
+++ public/src/app_gs_overlay_speech.mjs
@@ -1,4 +1,9 @@
+import { html_style_rounded_padded } from "./html_style_rounded_padded.mjs";
+import { html_style_centered } from "./html_style_centered.mjs";
 import { app_gs_div_rounded_transparent } from "./app_gs_div_rounded_transparent.mjs";
 export function app_gs_overlay_speech(menu_overlay) {
   let result = app_gs_div_rounded_transparent(menu_overlay, "black", 51);
+  html_style_centered(div_npc);
+  html_style_rounded_padded(div_npc);
+  return result;
 }
