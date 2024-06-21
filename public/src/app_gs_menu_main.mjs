8eb80ea7d49da4fda1e993e54b950350dadabebe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 09:53:07 -0400

    2024-06-21T13:53:07.246Z function_auto_after app_gs_menu_main

--- public/src/app_gs_menu_main.mjs
+++ public/src/app_gs_menu_main.mjs
@@ -16,7 +16,7 @@ export function app_gs_menu_main(menu_overlay, map_c, map) {
     );
     html_button_width_full_text_click(
       menu_overlay,
-      "God, please bless my next 👥 conversation, in the name of Jesus, amen.",
+      "🙏 God, please bless my next 👥 conversation, in the name of Jesus, amen.",
       () => {
         app_gs_menu_system_logs(menu_overlay);
       },
