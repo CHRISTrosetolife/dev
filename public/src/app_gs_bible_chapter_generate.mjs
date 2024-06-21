620a224e7f3eb30e285fc127cc5bfd6aee9adcb3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 14:19:53 -0400

    2024-06-21T18:19:53.187Z function_auto_after app_gs_bible_chapter_generate

--- public/src/app_gs_bible_chapter_generate.mjs
+++ public/src/app_gs_bible_chapter_generate.mjs
@@ -4,5 +4,5 @@ import { bible_chapter } from "./bible_chapter.mjs";
 export async function app_gs_bible_chapter_generate(chapter_name) {
   let verses = await bible_chapter("engbsb", chapter_name);
   chapter_name = string_case_lower(chapter_name);
-  await generate_list_generic(filtered, name);
+  await generate_list_generic(verses, name);
 }
