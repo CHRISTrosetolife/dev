bb1b7b2b97ed37a33a0b4c01917b7ddf9ed710eb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 15:53:55 -0400

    2024-06-21T19:53:54.021Z function_auto_after google_search_lucky

--- public/src/google_search_lucky.mjs
+++ public/src/google_search_lucky.mjs
@@ -3,5 +3,6 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function google_search_lucky(query) {
   assert_arguments_length(arguments, 1);
   let lucky = true;
+  ("https://duckduckgo.com/?q=!ducky+");
   await google_search_generic(query, lucky);
 }
