6848da644f8155d99e6b246d83520f36b3ee26db
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 15:18:45 -0400

    2024-06-21T19:18:45.245Z function_auto_after js_code_arrow_block

--- public/src/js_code_arrow_block.mjs
+++ public/src/js_code_arrow_block.mjs
@@ -1,4 +1,4 @@
 import { js_code_arrow_block_args } from "./js_code_arrow_block_args.mjs";
 export function js_code_arrow_block() {
-  return js_code_arrow_block_args([]);
+  return js_code_arrow_block_args([], "");
 }
