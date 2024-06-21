3afb4ef2d443dc2d6a8bd682d31a2d34dece45b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 10:57:59 -0400

    2024-06-21T14:57:58.779Z function_auto_after app_gs_map_cell_click

--- public/src/app_gs_map_cell_click.mjs
+++ public/src/app_gs_map_cell_click.mjs
@@ -77,7 +77,8 @@ export async function app_gs_map_cell_click(map, map_c, player_c, tile) {
             [3, 1],
             [
               string_combine_multiple([
-                "It is ",
+                "It is",
+                " ",
                 app_gs_phrase_very(),
                 list_random_item(["wonderful", "nice"]),
               ]),
