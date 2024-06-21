aa1e9a7661b5dd4d9aa13bb1fd311ebd0d7d800a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 10:55:32 -0400

    2024-06-21T14:55:31.627Z function_auto_after list_random_item_weighted

--- public/src/list_random_item_weighted.mjs
+++ public/src/list_random_item_weighted.mjs
@@ -1,4 +1,6 @@
+import { list_get } from "./list_get.mjs";
 import { list_random_index_weighted } from "./list_random_index_weighted.mjs";
 export function list_random_item_weighted(weights, items) {
   let index = list_random_index_weighted(weights);
+  return list_get(items, index);
 }
