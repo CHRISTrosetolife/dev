bcb81690a5d5c07df89c25636be8942474058b8f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 09:12:23 -0400

    2024-06-21T13:12:22.222Z

---
+++ public/src/app_gs_map_html_at.mjs
@@ -0,0 +1,5 @@
+import { app_gs_map_at_generic } from "./app_gs_map_at_generic.mjs";
+export function app_gs_map_html_at(map, coordinates) {
+  let grid = map.tiles;
+  return app_gs_map_at_generic(grid, coordinates);
+}
