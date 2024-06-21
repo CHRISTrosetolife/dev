4aa3d8adf031aab2f3896d1419b58fc99a3e316a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 10:55:19 -0400

    2024-06-21T14:55:19.366Z function_auto_after list_random_item_weighted

--- public/src/list_random_item_weighted.mjs
+++ public/src/list_random_item_weighted.mjs
@@ -1,1 +1,4 @@
-export function list_random_item_weighted(weights, items) {}
+import { list_random_index_weighted } from "./list_random_index_weighted.mjs";
+export function list_random_item_weighted(weights, items) {
+  list_random_index_weighted(weights);
+}
