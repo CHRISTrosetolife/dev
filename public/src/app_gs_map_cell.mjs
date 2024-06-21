266578c934494d50462c37743a4407c54209454b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 21:56:21 -0400

    2024-06-21T01:56:20.366Z function_auto_after app_gs_map_cell

--- public/src/app_gs_map_cell.mjs
+++ public/src/app_gs_map_cell.mjs
@@ -1,29 +1,14 @@
+import { app_gs_map_cell_click } from "./app_gs_map_cell_click.mjs";
 import { app_gs_player_c_generic } from "./app_gs_player_c_generic.mjs";
-import { app_gs_map_at } from "./app_gs_map_at.mjs";
-import { app_gs_menu_main } from "./app_gs_menu_main.mjs";
-import { app_gs_menu_overlay } from "./app_gs_menu_overlay.mjs";
-import { list_xy } from "./list_xy.mjs";
-import { list_all } from "./list_all.mjs";
-import { app_gs_map_tile_id } from "./app_gs_map_tile_id.mjs";
-import { app_gs_map_neighbors_get } from "./app_gs_map_neighbors_get.mjs";
-import { graph_path_shortest_neighbors } from "./graph_path_shortest_neighbors.mjs";
 import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
-import { app_gs_map_render } from "./app_gs_map_render.mjs";
-import { html_scroll_center_smooth } from "./html_scroll_center_smooth.mjs";
-import { each_async } from "./each_async.mjs";
-import { app_gs_overlays_any_wall } from "./app_gs_overlays_any_wall.mjs";
-import { app_gs_walk } from "./app_gs_walk.mjs";
 import { html_on_click } from "./html_on_click.mjs";
 import { game_img_style } from "./game_img_style.mjs";
 import { game_img_base } from "./game_img_base.mjs";
 import { game_img } from "./game_img.mjs";
 import { html_data_set } from "./html_data_set.mjs";
 import { html_div } from "./html_div.mjs";
-import { list_skip } from "./list_skip.mjs";
 import { list_concat } from "./list_concat.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { equal_by } from "./equal_by.mjs";
 export function app_gs_map_cell(map, map_c, player_c, tile) {
   let clicker = html_div(map_c);
   let overlays = list_adder((la) =>
@@ -42,35 +27,7 @@ export function app_gs_map_cell(map, map_c, player_c, tile) {
   let tile_c = game_img(map_c, game_img_base(id), tile, "tile");
   game_img_style(clicker, tile.y, tile.x, "clicker");
   html_on_click(clicker, async () => {
-    let w = app_gs_overlays_any_wall(tile);
-    if (!w) {
-      let from = app_gs_map_at(map, map.player);
-      let path = graph_path_shortest_neighbors(
-        app_gs_map_tile_id,
-        (v) => {
-          return app_gs_map_neighbors_get(map, v);
-        },
-        from,
-        tile,
-      );
-      if (
-        list_all(list_xy(), (xy) =>
-          equal_by(map.player, tile, (coordinates) =>
-            object_property_get(coordinates, xy),
-          ),
-        )
-      ) {
-        let menu_overlay = app_gs_menu_overlay(map_c, map);
-        app_gs_menu_main(menu_overlay, map_c, map);
-      } else {
-        app_gs_map_render(map, map_c, path, player_c);
-        await each_async(list_skip(path, 1), async (tile) => {
-          await app_gs_walk(player_c, map.player, tile);
-        });
-        await html_scroll_center_smooth(player_c);
-        app_gs_map_render(map, map_c, [map.player], player_c);
-      }
-    }
+    await app_gs_map_cell_click(map, map_c, player_c, tile);
   });
   return list_concat([tile_c, clicker], overlays);
 }
---
+++ public/src/app_gs_map_cell_click.mjs
@@ -0,0 +1,47 @@
+import { html_scroll_center_smooth } from "./html_scroll_center_smooth.mjs";
+import { app_gs_walk } from "./app_gs_walk.mjs";
+import { list_skip } from "./list_skip.mjs";
+import { each_async } from "./each_async.mjs";
+import { app_gs_map_render } from "./app_gs_map_render.mjs";
+import { app_gs_menu_main } from "./app_gs_menu_main.mjs";
+import { app_gs_menu_overlay } from "./app_gs_menu_overlay.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { equal_by } from "./equal_by.mjs";
+import { list_xy } from "./list_xy.mjs";
+import { list_all } from "./list_all.mjs";
+import { app_gs_map_neighbors_get } from "./app_gs_map_neighbors_get.mjs";
+import { app_gs_map_tile_id } from "./app_gs_map_tile_id.mjs";
+import { graph_path_shortest_neighbors } from "./graph_path_shortest_neighbors.mjs";
+import { app_gs_map_at } from "./app_gs_map_at.mjs";
+import { app_gs_overlays_any_wall } from "./app_gs_overlays_any_wall.mjs";
+export async function app_gs_map_cell_click(map, map_c, player_c, tile) {
+  let w = app_gs_overlays_any_wall(tile);
+  if (!w) {
+    let from = app_gs_map_at(map, map.player);
+    let path = graph_path_shortest_neighbors(
+      app_gs_map_tile_id,
+      (v) => {
+        return app_gs_map_neighbors_get(map, v);
+      },
+      from,
+      tile,
+    );
+    if (
+      list_all(list_xy(), (xy) =>
+        equal_by(map.player, tile, (coordinates) =>
+          object_property_get(coordinates, xy),
+        ),
+      )
+    ) {
+      let menu_overlay = app_gs_menu_overlay(map_c, map);
+      app_gs_menu_main(menu_overlay, map_c, map);
+    } else {
+      app_gs_map_render(map, map_c, path, player_c);
+      await each_async(list_skip(path, 1), async (tile) => {
+        await app_gs_walk(player_c, map.player, tile);
+      });
+      await html_scroll_center_smooth(player_c);
+      app_gs_map_render(map, map_c, [map.player], player_c);
+    }
+  }
+}
