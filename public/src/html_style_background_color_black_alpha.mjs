2b9ce87cf8aaa41d5d31e4bc68f58a5052228ff5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 12:28:34 -0400

    2024-06-21T16:28:33.023Z function_auto_after html_style_background_color_black_alpha

--- public/src/html_style_background_color_black_alpha.mjs
+++ public/src/html_style_background_color_black_alpha.mjs
@@ -1,8 +1,9 @@
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_style_background_color } from "./html_style_background_color.mjs";
 export function html_style_background_color_black_alpha(menu, alpha) {
+  let rgb = "1,1,1";
   html_style_background_color(
     menu,
-    string_combine_multiple(["rgba(", "1,1,1", ",", alpha, ")"]),
+    string_combine_multiple(["rgba(", rgb, ",", alpha, ")"]),
   );
 }
