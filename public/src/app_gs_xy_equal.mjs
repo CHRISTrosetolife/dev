4054841f93745b621ff4e30e2d0cb9c7b0440b47
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 22:15:09 -0400

    2024-06-21T02:15:09.040Z function_auto_after app_gs_xy_equal

--- public/src/app_gs_xy_equal.mjs
+++ public/src/app_gs_xy_equal.mjs
@@ -1,6 +1,13 @@
+import { object_property_get } from "./object_property_get.mjs";
+import { list_all } from "./list_all.mjs";
 import { list_xy } from "./list_xy.mjs";
 import { object_properties_new } from "./object_properties_new.mjs";
 import { equal_by } from "./equal_by.mjs";
 export function app_gs_xy_equal(o, tile) {
+  list_all(list_xy(), (xy) =>
+    equal_by(map.player, tile, (coordinates) =>
+      object_property_get(coordinates, xy),
+    ),
+  );
   return equal_by(o, tile, (a) => object_properties_new(a, list_xy()));
 }
