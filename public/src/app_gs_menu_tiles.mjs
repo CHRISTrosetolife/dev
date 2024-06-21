cc6378443f6d474fe55a0c0d321b634617dc53b4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 09:44:02 -0400

    2024-06-21T13:44:02.270Z function_auto_after app_gs_menu_tiles

--- public/src/app_gs_menu_tiles.mjs
+++ public/src/app_gs_menu_tiles.mjs
@@ -1,14 +1,14 @@
+import { html_button_width_full_text_click_back } from "./html_button_width_full_text_click_back.mjs";
 import { app_gs_tiles } from "./app_gs_tiles.mjs";
 import { html_style_width_full } from "./html_style_width_full.mjs";
 import { html_style_centered } from "./html_style_centered.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_div } from "./html_div.mjs";
 import { app_gs_menu_main } from "./app_gs_menu_main.mjs";
-import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { html_clear } from "./html_clear.mjs";
 export function app_gs_menu_tiles(menu_overlay, map_c, map) {
   html_clear(menu_overlay);
-  html_button_width_full_text_click(menu_overlay, "back", () => {
+  html_button_width_full_text_click_back(menu_overlay, () => {
     app_gs_menu_main(menu_overlay);
   });
   let container = html_div(menu_overlay);
