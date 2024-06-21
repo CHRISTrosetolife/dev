e00bb66d18c90772dfcbf60811536bad7fa6f03f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 15:48:56 -0400

    2024-06-21T19:48:55.534Z function_auto_after google_search

--- public/src/google_search.mjs
+++ public/src/google_search.mjs
@@ -1,20 +1,7 @@
-import { string_to_url } from "./string_to_url.mjs";
-import { url_secure_w3 } from "./url_secure_w3.mjs";
-import { browser_open } from "./browser_open.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { google_search_generic } from "./google_search_generic.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function google_search(query) {
   assert_arguments_length(arguments, 1);
   let lucky = false;
-  query = string_to_url(query);
-  let lucky_url = lucky ? "btnI=1&" : "";
-  await browser_open(
-    string_combine_multiple([
-      url_secure_w3(),
-      "google.com/search?",
-      lucky_url,
-      "q=",
-      query,
-    ]),
-  );
+  query = await google_search_generic(query, lucky);
 }
---
+++ public/src/google_search_generic.mjs
@@ -0,0 +1,18 @@
+import { url_secure_w3 } from "./url_secure_w3.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { browser_open } from "./browser_open.mjs";
+import { string_to_url } from "./string_to_url.mjs";
+export async function google_search_generic(query, lucky) {
+  query = string_to_url(query);
+  let lucky_url = lucky ? "btnI=1&" : "";
+  await browser_open(
+    string_combine_multiple([
+      url_secure_w3(),
+      "google.com/search?",
+      lucky_url,
+      "q=",
+      query,
+    ]),
+  );
+  return query;
+}
