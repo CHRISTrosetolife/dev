aff76f8b6f4f33a8ffd7fd4f49cbc04c3e61dc10
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 14:18:53 -0400

    2024-06-21T18:18:52.785Z function_auto_after app_gs_bible_chapter_generate

--- public/src/app_gs_bible_chapter_generate.mjs
+++ public/src/app_gs_bible_chapter_generate.mjs
@@ -1,4 +1,3 @@
-import { bible_chapter } from "./bible_chapter.mjs";
 export async function app_gs_bible_chapter_generate(chapter_name) {
-  await bible_chapter(chapter_name);
+  await bible_chapter_engbsb(chapter_name);
 }
