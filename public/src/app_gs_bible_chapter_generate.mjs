e2cdc9704b94e8f64590e75b7fce5a5716eebd99
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 15:38:13 -0400

    2024-06-21T19:38:12.885Z function_auto_after app_gs_bible_chapter_generate

--- public/src/app_gs_bible_chapter_generate.mjs
+++ public/src/app_gs_bible_chapter_generate.mjs
@@ -34,7 +34,9 @@ export async function app_gs_bible_chapter_generate(chapter_name) {
           let { argument } = last;
           let { elements } = argument;
           each(elements, (element) => {
-            log({});
+            log({
+              element,
+            });
           });
         },
       ],
