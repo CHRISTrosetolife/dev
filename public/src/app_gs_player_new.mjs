6366958b657e1ab26585beaa81b70875d9603d1f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 09:36:09 -0400

    2024-06-21T13:36:08.561Z function_auto_after app_gs_player_new

--- public/src/app_gs_player_new.mjs
+++ public/src/app_gs_player_new.mjs
@@ -5,4 +5,7 @@ export function app_gs_player_new(map) {
   map.player.walk_offset = 0;
   map.player.walk_previous = 1;
   map.player.character = list_random_item(game_img_list_male());
+  map.player.pray = {
+    conversation: false,
+  };
 }
