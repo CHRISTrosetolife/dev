0b1cf2787c7052186cf01db522d75e8162d633b4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 22:18:55 -0400

    2024-06-21T02:18:55.011Z function_auto_after app_gs_map_cell_click

--- public/src/app_gs_map_cell_click.mjs
+++ public/src/app_gs_map_cell_click.mjs
@@ -10,10 +10,6 @@ import { app_gs_overlays_any_wall } from "./app_gs_overlays_any_wall.mjs";
 export async function app_gs_map_cell_click(map, map_c, player_c, tile) {
   let npc = list_find_property(tile.overlays, "type", "npc", null);
   if (null_is(npc)) {
-    let neighbors = app_gs_map_neighbors_get(map, npc);
-    let { path } = app_gs_map_path(map, neighbors);
-    await app_gs_walk_path(map, map_c, player_c, path);
-  } else {
     let walls = app_gs_overlays_any_wall(tile);
     if (!walls) {
       let { path } = app_gs_map_path(map, [tile]);
@@ -24,5 +20,9 @@ export async function app_gs_map_cell_click(map, map_c, player_c, tile) {
         await app_gs_walk_path(map, map_c, player_c, path);
       }
     }
+  } else {
+    let neighbors = app_gs_map_neighbors_get(map, npc);
+    let { path } = app_gs_map_path(map, neighbors);
+    await app_gs_walk_path(map, map_c, player_c, path);
   }
 }
