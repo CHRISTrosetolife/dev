3ff132cacba337a42e7335bff22c73e8a22a6437
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 14:21:49 -0400

    2024-06-21T18:21:49.121Z function_auto_after app_gs_bible_chapter_generate

--- public/src/app_gs_bible_chapter_generate.mjs
+++ public/src/app_gs_bible_chapter_generate.mjs
@@ -1,3 +1,4 @@
+import { function_open } from "./function_open.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { generate_list_generic } from "./generate_list_generic.mjs";
@@ -12,4 +13,5 @@ export async function app_gs_bible_chapter_generate(chapter_name) {
   );
   let name = string_combine_multiple([prefix, chapter_name]);
   await generate_list_generic(verses, name);
+  await function_open(name);
 }
