61c3e100c96e97f5e539fc96035139ed28f64ec8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 12:44:44 -0400

    2024-06-21T16:44:42.799Z

--- public/src/html_style_background_color_black_alpha.mjs
+++
@@ -1,4 +0,0 @@
-import { html_style_background_color_alpha } from "./html_style_background_color_alpha.mjs";
-export function html_style_background_color_black_alpha(menu, alpha) {
-  html_style_background_color_alpha(menu, "1,1,1", alpha);
-}
