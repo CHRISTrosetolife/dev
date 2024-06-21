57ad3b9e7d51ac10ac432a56d66aa5adc2e4fc0b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 22:28:10 -0400

    2024-06-21T02:28:09.528Z function_auto_after app_gs_overlays_wall

--- public/src/app_gs_overlays_wall.mjs
+++ public/src/app_gs_overlays_wall.mjs
@@ -1,5 +1,5 @@
 import { list_concat_multiple } from "./list_concat_multiple.mjs";
 import { range_from } from "./range_from.mjs";
 export function app_gs_overlays_wall() {
-  return list_concat_multiple([range_from(8, 9), range_from(40, 42)]);
+  return list_concat_multiple([[8, 9, 16, 17, 32, 33], range_from(40, 42)]);
 }
