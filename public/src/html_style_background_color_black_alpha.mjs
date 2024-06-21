86a6732f66c8896c51148ed98604d711f8e78b3f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 12:29:13 -0400

    2024-06-21T16:29:12.025Z function_auto_after html_style_background_color_black_alpha

--- public/src/html_style_background_color_black_alpha.mjs
+++ public/src/html_style_background_color_black_alpha.mjs
@@ -1,5 +1,4 @@
 import { html_style_background_color_alpha } from "./html_style_background_color_alpha.mjs";
 export function html_style_background_color_black_alpha(menu, alpha) {
-  let rgb = "1,1,1";
-  html_style_background_color_alpha(menu, rgb, alpha);
+  html_style_background_color_alpha(menu, "1,1,1", alpha);
 }
