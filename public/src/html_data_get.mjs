ff802765e0cbe60939350207b63bf99889bf0b36
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 09:17:05 -0400

    2024-06-21T13:17:04.940Z function_auto_after html_data_get

---
+++ public/src/html_data_get.mjs
@@ -0,0 +1,5 @@
+import { html_data_attribute } from "./html_data_attribute.mjs";
+import { html_attribute_set } from "./html_attribute_set.mjs";
+export function html_data_get(component, data_name, data_value) {
+  html_attribute_set(component, html_data_attribute(data_name), data_value);
+}
