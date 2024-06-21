4b4794ceaa0225f39a8e3eab52585826c5222800
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 15:16:20 -0400

    2024-06-21T19:16:19.678Z function_auto_after function_delete_if_exists

--- public/src/function_delete_if_exists.mjs
+++ public/src/function_delete_if_exists.mjs
@@ -3,7 +3,8 @@ import { file_exists } from "./file_exists.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function function_delete_if_exists(function_name) {
   let function_path = function_name_to_path(function_name);
-  if (await file_exists(function_path)) {
+  let e = await file_exists(function_path);
+  if (e) {
     await function_delete(function_name);
   }
 }
