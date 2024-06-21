7f5f9109c7f271b5b7573db6dc49bcdec51e8b01
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 12:23:46 -0400

    2024-06-21T16:23:44.043Z function_auto_after html_style_background_color

--- public/src/html_style_background_color.mjs
+++ public/src/html_style_background_color.mjs
@@ -1,5 +1,7 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { html_style } from "./html_style.mjs";
 export function html_style_background_color(container, color) {
+  assert_arguments_length;
   html_style(container, {
     "background-color": color,
   });
