675dd81065174f133df4f1be5cc73d83b4a4e483
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 15:15:52 -0400

    2024-06-21T19:15:51.995Z function_auto_after app_gs_bible_chapter_generate

--- public/src/app_gs_bible_chapter_generate.mjs
+++ public/src/app_gs_bible_chapter_generate.mjs
@@ -13,7 +13,7 @@ import { string_suffix_without } from "./string_suffix_without.mjs";
 import { json_to } from "./json_to.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 export async function app_gs_bible_chapter_generate(chapter_name) {
-  if (await function_exists_not()) {
+  if (await function_exists()) {
   }
   let verses = await bible_chapter("engbsb", chapter_name);
   chapter_name = string_case_lower(chapter_name);
