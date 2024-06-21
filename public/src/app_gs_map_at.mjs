a4e88d00618fc5bd659ab00a25d2c87acedbf91f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 09:11:49 -0400

    2024-06-21T13:11:48.483Z function_auto_after app_gs_map_at

--- public/src/app_gs_map_at.mjs
+++ public/src/app_gs_map_at.mjs
@@ -1,4 +1,5 @@
 export function app_gs_map_at(map, coordinates) {
-  let tile = map.tiles[coordinates.y][coordinates.x];
+  let grid = map.tiles;
+  let tile = grid[coordinates.y][coordinates.x];
   return tile;
 }
