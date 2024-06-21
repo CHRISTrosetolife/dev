9a789d986c0f0f57371cd26fc1e4eb685149fc77
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-20 21:43:08 -0400

    2024-06-21T01:43:08.484Z function_auto_after app_gs_overlays_any_wall

--- public/src/app_gs_overlays_any_wall.mjs
+++ public/src/app_gs_overlays_any_wall.mjs
@@ -3,7 +3,6 @@ import { list_any } from "./list_any.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function app_gs_overlays_any_wall(map, tile) {
   assert_arguments_length(arguments, 2);
-  let os = tile.overlays;
-  let w = list_any(os, app_gs_overlay_wall_is);
+  let w = list_any(tile.overlays, app_gs_overlay_wall_is);
   return w;
 }
