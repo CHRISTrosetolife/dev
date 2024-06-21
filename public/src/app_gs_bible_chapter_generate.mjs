fb550d561a3f9c379e52efb887bd3abaa8c58de5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 14:29:53 -0400

    2024-06-21T18:29:53.079Z   t

--- public/src/app_gs_bible_chapter_generate.mjs
+++ public/src/app_gs_bible_chapter_generate.mjs
@@ -1,4 +1,4 @@
-import { exit } from "./exit.mjs";
+import { each } from "./each.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
 import { js_code_array } from "./js_code_array.mjs";
 import { js_code_statement_return } from "./js_code_statement_return.mjs";
@@ -18,7 +18,7 @@ export async function app_gs_bible_chapter_generate(chapter_name) {
     "generate",
   );
   let name = string_combine_multiple([prefix, chapter_name]);
-  exit();
+  each((NaN) => {});
   let jsons = list_map(verses, json_to);
   await function_new_generic(
     name,
