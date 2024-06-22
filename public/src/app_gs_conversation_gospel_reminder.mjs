d635cb1c5fd04bb1a2e9668ca2d72f7c9e4ac4e5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 10:00:49 -0400

    2024-06-22T14:00:48.889Z function_auto_after app_gs_conversation_gospel_reminder

--- public/src/app_gs_conversation_gospel_reminder.mjs
+++ public/src/app_gs_conversation_gospel_reminder.mjs
@@ -6,21 +6,24 @@ import { list_random_item } from "./list_random_item.mjs";
 import { app_gs_phrase_thanks } from "./app_gs_phrase_thanks.mjs";
 import { app_gs_phrase_worship_multiple_or_empty } from "./app_gs_phrase_worship_multiple_or_empty.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { app_gs_phrase_combine } from "./app_gs_phrase_combine.mjs";
+import { app_gs_phrase_combine_multiple } from "./app_gs_phrase_combine_multiple.mjs";
 export function app_gs_conversation_gospel_reminder(menu_overlay, npc) {
-  let text = app_gs_phrase_combine(app_gs_phrase_worship_multiple_or_empty(), [
-    string_combine_multiple([
-      app_gs_phrase_thanks(),
-      " for ",
-      list_random_item(["the reminder", "reminding me"]),
+  let text = app_gs_phrase_combine_multiple(
+    app_gs_phrase_worship_multiple_or_empty(),
+    [
+      string_combine_multiple([
+        app_gs_phrase_thanks(),
+        " for ",
+        list_random_item(["the reminder", "reminding me"]),
+        string_random_or_empty(
+          string_combine_multiple([" of ", app_gs_phrase_the_gospel()]),
+        ),
+        "!",
+      ]),
       string_random_or_empty(
-        string_combine_multiple([" of ", app_gs_phrase_the_gospel()]),
+        string_combine_multiple([" ", app_gs_phrase_doxology()]),
       ),
-      "!",
-    ]),
-    string_random_or_empty(
-      string_combine_multiple([" ", app_gs_phrase_doxology()]),
-    ),
-  ]);
+    ],
+  );
   app_gs_conversation_npc_root(menu_overlay, npc, text);
 }
