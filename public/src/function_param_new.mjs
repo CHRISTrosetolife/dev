b7628d3579006af3e353f87b8005c84b82016a30
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 15:32:33 -0400

    2024-06-22T19:32:33.004Z function_auto_after function_param_new

--- public/src/function_param_new.mjs
+++ public/src/function_param_new.mjs
@@ -4,6 +4,7 @@ import { each_async } from "./each_async.mjs";
 import { function_names } from "./function_names.mjs";
 export async function function_param_new() {
   let dis = await data_identifiers();
+  return dis;
   let fns = await function_names();
   await each_async(fns, async (fn) => {
     let fn_new = await fn_new_get(fn);
