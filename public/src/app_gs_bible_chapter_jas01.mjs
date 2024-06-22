a2d9e3687d31203ac8da65c91cf64abf47ef236d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 13:37:51 -0400

    2024-06-22T17:37:50.861Z function_auto_after app_gs_bible_chapter_jas01

--- public/src/app_gs_bible_chapter_jas01.mjs
+++ public/src/app_gs_bible_chapter_jas01.mjs
@@ -297,7 +297,7 @@ export function app_gs_bible_chapter_jas01() {
       sermons: [],
       questions: [
         {
-          text: "",
+          text: "If someone hears the word of God but does not obey it, what can they be compared to?",
           count: 1,
         },
       ],
