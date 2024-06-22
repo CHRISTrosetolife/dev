68d95f99a9fae29edd8493f5e7b24a276721813c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 10:11:09 -0400

    2024-06-22T14:11:06.653Z function_auto_after app_gs_phrase_combine_multiple

--- public/src/app_gs_phrase_combine_multiple.mjs
+++ public/src/app_gs_phrase_combine_multiple.mjs
@@ -1,5 +1,7 @@
 import { app_gs_phrase_combine } from "./app_gs_phrase_combine.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { list_concat_multiple } from "./list_concat_multiple.mjs";
 export function app_gs_phrase_combine_multiple(list_of_lists) {
+  assert_arguments_length(arguments, 1);
   return app_gs_phrase_combine(list_concat_multiple(list_of_lists));
 }
