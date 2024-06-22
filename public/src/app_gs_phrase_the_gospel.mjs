26cd7be4fa06ceeaa2b3ec6ac8d0c980a8d24daa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 09:35:05 -0400

    2024-06-22T13:35:05.080Z   t

--- public/src/app_gs_phrase_the_gospel.mjs
+++ public/src/app_gs_phrase_the_gospel.mjs
@@ -1,7 +1,7 @@
 import { string_random_or_empty } from "./string_random_or_empty.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { list_random_item } from "./list_random_item.mjs";
-export function app_gs_phrase_the() {
+export function app_gs_phrase_the_gospel() {
   return list_random_item([
     string_combine_multiple([
       "the ",
