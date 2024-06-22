a7dc89fc57d95b31fb2305244eb2532198abf838
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 15:35:24 -0400

    2024-06-22T19:35:24.344Z function_auto_after function_param_new

--- public/src/function_param_new.mjs
+++ public/src/function_param_new.mjs
@@ -1,3 +1,4 @@
+import { js_param_new } from "./js_param_new.mjs";
 import { function_transform_args_split_lambda } from "./function_transform_args_split_lambda.mjs";
 import { data_identifiers } from "./data_identifiers.mjs";
 import { each_async } from "./each_async.mjs";
@@ -6,6 +7,6 @@ export async function function_param_new(function_name) {
   let dis = await data_identifiers();
   let fns = object_property_get(dis, function_name);
   await each_async(fns, async (fn) => {
-    await function_transform_args_split_lambda();
+    await function_transform_args_split_lambda(fn, [js_param_new]);
   });
 }
