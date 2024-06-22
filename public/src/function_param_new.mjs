2a87c23737b1ecbbbcace8f63c2ce4d7a6f230dc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 15:33:31 -0400

    2024-06-22T19:33:31.308Z function_auto_after function_param_new

--- public/src/function_param_new.mjs
+++ public/src/function_param_new.mjs
@@ -2,8 +2,10 @@ import { data_identifiers } from "./data_identifiers.mjs";
 import { function_rename } from "./function_rename.mjs";
 import { each_async } from "./each_async.mjs";
 import { function_names } from "./function_names.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 export async function function_param_new(function_name) {
   let dis = await data_identifiers();
+  object_property_get(function_name);
   return dis;
   let fns = await function_names();
   await each_async(fns, async (fn) => {
