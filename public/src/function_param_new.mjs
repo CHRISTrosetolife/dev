f7211654ff16095e7701b0c40e18f1e02dd1620e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 15:35:47 -0400

    2024-06-22T19:35:46.950Z function_auto_after function_param_new

--- public/src/function_param_new.mjs
+++ public/src/function_param_new.mjs
@@ -3,7 +3,11 @@ import { function_transform_args_split_lambda } from "./function_transform_args_
 import { data_identifiers } from "./data_identifiers.mjs";
 import { each_async } from "./each_async.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-export async function function_param_new(function_name) {
+export async function function_param_new(
+  function_name,
+  param_name,
+  default_value_string,
+) {
   let dis = await data_identifiers();
   let fns = object_property_get(dis, function_name);
   await each_async(fns, async (fn) => {
