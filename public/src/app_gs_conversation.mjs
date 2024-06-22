081c8737fcceecbeeb3c29299e455f71be900961
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 16:11:49 -0400

    2024-06-22T20:11:47.831Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -57,7 +57,7 @@ export function app_gs_conversation(map_c, map, npc) {
       string_random_or_empty(
         string_combine_multiple([
           app_gs_phrase_let_us(),
-          "talk ",
+          " talk ",
           string_random_or_empty("some time"),
           "later",
         ]),
