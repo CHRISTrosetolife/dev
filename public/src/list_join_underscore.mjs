36acf94af4210a39ffd5ceea98cc0875857c2ae8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 17:51:56 -0400

    2024-06-22T21:51:55.612Z function_auto_after list_join_underscore

--- public/src/list_join_underscore.mjs
+++ public/src/list_join_underscore.mjs
@@ -1,4 +1,4 @@
 import { list_join } from "./list_join.mjs";
-export function list_join_underscore(m) {
-  return list_join(m, "_");
+export function list_join_underscore(list) {
+  return list_join(list, "_");
 }
