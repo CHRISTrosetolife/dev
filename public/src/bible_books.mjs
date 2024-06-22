2077294e36924ea243e3af4b60c75c8d007818a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 18:24:04 -0400

    2024-06-22T22:24:03.346Z function_auto_after bible_books

--- public/src/bible_books.mjs
+++ public/src/bible_books.mjs
@@ -1,17 +1,9 @@
+import { bible_books_html_elements } from "./bible_books_html_elements.mjs";
 import { list_map } from "./list_map.mjs";
-import { html_parse_visit_classes_list } from "./html_parse_visit_classes_list.mjs";
-import { html_parse_visit_class_list } from "./html_parse_visit_class_list.mjs";
-import { list_single } from "./list_single.mjs";
-import { html_cache_parse } from "./html_cache_parse.mjs";
-import { bible_url_base } from "./bible_url_base.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
 import { string_take } from "./string_take.mjs";
 export async function bible_books(bible_folder) {
-  let url_base = bible_url_base(bible_folder);
-  let root = await html_cache_parse(url_base);
-  let vnavs = html_parse_visit_class_list(root, "vnav");
-  let vnav = list_single(vnavs);
-  let book_elements = html_parse_visit_classes_list(vnav, ["oo", "nn"]);
+  let book_elements = await bible_books_html_elements(bible_folder);
   let book_chapter_ones = list_map(book_elements, html_parse_href);
   let book_prefixes = list_map(book_chapter_ones, (s) => string_take(s, 3));
   return book_prefixes;
---
+++ public/src/bible_books_html_elements.mjs
@@ -0,0 +1,13 @@
+import { html_parse_visit_classes_list } from "./html_parse_visit_classes_list.mjs";
+import { list_single } from "./list_single.mjs";
+import { html_parse_visit_class_list } from "./html_parse_visit_class_list.mjs";
+import { html_cache_parse } from "./html_cache_parse.mjs";
+import { bible_url_base } from "./bible_url_base.mjs";
+export async function bible_books_html_elements(bible_folder) {
+  let url_base = bible_url_base(bible_folder);
+  let root = await html_cache_parse(url_base);
+  let vnavs = html_parse_visit_class_list(root, "vnav");
+  let vnav = list_single(vnavs);
+  let book_elements = html_parse_visit_classes_list(vnav, ["oo", "nn"]);
+  return book_elements;
+}
