09eef95c7d7da20fb3253a295d12dc6a76999b3c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 10:14:02 -0400

    2024-06-22T14:13:59.424Z function_auto_after app_gs_phrase_jesus

--- public/src/app_gs_phrase_jesus.mjs
+++ public/src/app_gs_phrase_jesus.mjs
@@ -1,3 +1,4 @@
+import { list_random_item } from "./list_random_item.mjs";
 import { string_random_or_empty } from "./string_random_or_empty.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_gs_phrase_jesus() {
@@ -7,9 +8,11 @@ export function app_gs_phrase_jesus() {
     string_random_or_empty(
       string_combine_multiple([
         ", the ",
-        string_combine_multiple([
-          "Son of God,",
-          string_random_or_empty(" the Father"),
+        list_random_item([
+          string_combine_multiple([
+            "Son of God,",
+            string_random_or_empty(" the Father"),
+          ]),
         ]),
       ]),
     ),
