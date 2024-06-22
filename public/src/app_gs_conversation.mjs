01c61a3ae3e93664d077f9b2737c357f2738945c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 15:59:35 -0400

    2024-06-22T19:59:35.529Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -26,12 +26,14 @@ export function app_gs_conversation(map_c, map, npc) {
           string_random_or_empty(" to me"),
         ),
         ". ",
-        string_combine_multiple([
-          app_gs_phrase_let_us(),
-          "talk ",
-          string_random_or_empty("some time"),
-          "later",
-        ]),
+        string_random_or_empty(
+          string_combine_multiple([
+            app_gs_phrase_let_us(),
+            "talk ",
+            string_random_or_empty("some time"),
+            "later",
+          ]),
+        ),
       ]),
     );
   } else {
