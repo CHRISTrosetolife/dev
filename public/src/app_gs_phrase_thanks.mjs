0c981260f9dbd1e9d41a82765249e9a54f820436
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 09:37:38 -0400

    2024-06-22T13:37:34.863Z function_auto_after app_gs_phrase_thanks

--- public/src/app_gs_phrase_thanks.mjs
+++ public/src/app_gs_phrase_thanks.mjs
@@ -1,7 +1,8 @@
 import { string_random_or_empty } from "./string_random_or_empty.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { list_random_item } from "./list_random_item.mjs";
 export function app_gs_phrase_thanks() {
-  return string_combine_multiple([
+  return list_random_item([
     string_combine_multiple(["Thanks", string_random_or_empty(" a lot")]),
     string_combine_multiple([
       "Thank you",
