a09aed65adde4871c674d481afb6752bd63b2bb0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 15:32:05 -0400

    2024-06-22T19:32:04.889Z function_auto_after function_param_new

---
+++ public/src/function_param_new.mjs
@@ -0,0 +1,12 @@
+import { function_rename } from "./function_rename.mjs";
+import { each_async } from "./each_async.mjs";
+import { function_names } from "./function_names.mjs";
+export async function function_param_new(fn_new_get) {
+  let fns = await function_names();
+  await each_async(fns, async (fn) => {
+    let fn_new = await fn_new_get(fn);
+    if (fn_new !== fn) {
+      await function_rename(fn, fn_new);
+    }
+  });
+}
