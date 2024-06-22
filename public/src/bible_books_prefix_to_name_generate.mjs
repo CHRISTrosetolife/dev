75cab18731c19828648d0d69fa4419e3ff42f33e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 18:25:27 -0400

    2024-06-22T22:25:27.006Z function_auto_after bible_books_prefix_to_name_generate

--- public/src/bible_books_prefix_to_name_generate.mjs
+++ public/src/bible_books_prefix_to_name_generate.mjs
@@ -1,1 +1,4 @@
-export function bible_books_prefix_to_name_generate() {}
+import { bible_books_html_elements } from "./bible_books_html_elements.mjs";
+export async function bible_books_prefix_to_name_generate() {
+  let book_elements = await bible_books_html_elements(bible_folder);
+}
