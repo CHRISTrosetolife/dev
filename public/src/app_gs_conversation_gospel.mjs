d991bf77f0bf2e178937277b889e7e11ac19caca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 15:41:35 -0400

    2024-06-22T19:41:34.674Z   t

--- public/src/app_gs_conversation_gospel.mjs
+++ public/src/app_gs_conversation_gospel.mjs
@@ -1,3 +1,4 @@
+import { error } from "./error.mjs";
 import { app_gs_conversation_witness } from "./app_gs_conversation_witness.mjs";
 import { app_gs_conversation_gospel_reminder } from "./app_gs_conversation_gospel_reminder.mjs";
 import { app_gs_conversation_gospel_convert } from "./app_gs_conversation_gospel_convert.mjs";
@@ -7,7 +8,7 @@ export function app_gs_conversation_gospel(menu_overlay, npc, map) {
   if (npc.christian) {
     app_gs_conversation_gospel_reminder(menu_overlay, npc);
   } else if (npc.objection_count === 0) {
-    app_gs_conversation_gospel_convert(menu_overlay, npc);
+    app_gs_conversation_gospel_convert(menu_overlay, npc, error());
   } else {
     app_gs_conversation_witness(menu_overlay, npc, map);
   }
