aa71178a13d3cb5c190bc2828398b8538139755e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 17:51:51 -0400

    2024-06-22T21:51:50.990Z function_auto_after app_gs_conversation_witness

--- public/src/app_gs_conversation_witness.mjs
+++ public/src/app_gs_conversation_witness.mjs
@@ -39,7 +39,7 @@ export function app_gs_conversation_witness(menu_overlay, npc, map) {
   let objectables = list_adder((la) =>
     each_index(verses, (verse, index) => {
       each(verse.objections, (objection) => {
-        la(list_join_underscore());
+        la(list_join_underscore([]));
       });
     }),
   );
