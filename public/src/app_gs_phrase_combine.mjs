4110ace9d593ccac8bc185b73c6176bfce381290
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 10:00:42 -0400

    2024-06-22T14:00:41.510Z function_auto_after app_gs_phrase_combine

--- public/src/app_gs_phrase_combine.mjs
+++ public/src/app_gs_phrase_combine.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 import { string_empty_not_is } from "./string_empty_not_is.mjs";
 export function app_gs_phrase_combine(list) {
+  assert_arguments_length(arguments, 1);
   return list_join_space(list_filter(list, string_empty_not_is));
 }
