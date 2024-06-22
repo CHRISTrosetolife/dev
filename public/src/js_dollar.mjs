84fbb7118304b9fd1294a061eadb3b2a802a81b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 10:29:48 -0400

    2024-06-22T14:29:48.192Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -88,6 +88,18 @@ export function js_dollar(ast) {
           object_replace(node, e);
         }
       }
+      let objection_prefix = "o";
+      if (string_starts_with(remaining, objection_prefix)) {
+        remaining = string_prefix_without(remaining, objection_prefix);
+        let count = 1;
+        if (string_empty_not_is(remaining)) {
+          count = integer_parse(remaining);
+        }
+        let e = js_parse_expression(
+          string_combine_multiple(["{text:'',count:", count, "}"]),
+        );
+        object_replace(node, e);
+      }
       if (remaining === "r") {
         let e = js_parse_first_function("return");
         if (parent.type === "ExpressionStatement") {
@@ -116,18 +128,6 @@ export function js_dollar(ast) {
           });
           object_replace(node, e);
         }
-      }
-      let objection_prefix = "o";
-      if (string_starts_with(remaining, objection_prefix)) {
-        remaining = string_prefix_without(remaining, objection_prefix);
-        let count = 1;
-        if (string_empty_not_is(remaining)) {
-          count = integer_parse(remaining);
-        }
-        let e = js_parse_expression(
-          string_combine_multiple(["{text:'',count:", count, "}"]),
-        );
-        object_replace(node, e);
       }
     }
   });
