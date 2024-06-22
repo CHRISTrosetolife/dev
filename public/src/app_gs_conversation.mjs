ea0996cd6fff4d362452853c2d799faf873c4f86
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 16:01:50 -0400

    2024-06-22T20:01:50.269Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -17,17 +17,19 @@ export function app_gs_conversation(map_c, map, npc) {
     let sentences = [
       string_combine_multiple([
         "I need",
-        string_combine_multiple([
-          " some time",
-          string_random_or_empty(" to myself"),
-          " to think",
-          string_random_or_empty(
-            " about what ",
-            app_gs_phrase_you_have(),
-            list_random_item(["said", "spoken"]),
-            string_random_or_empty(" to me"),
-          ),
-          ".",
+        list_random_item([
+          string_combine_multiple([
+            " some time",
+            string_random_or_empty(" to myself"),
+            " to think",
+            string_random_or_empty(
+              " about what ",
+              app_gs_phrase_you_have(),
+              list_random_item(["said", "spoken"]),
+              string_random_or_empty(" to me"),
+            ),
+            ".",
+          ]),
         ]),
       ]),
       string_random_or_empty(
