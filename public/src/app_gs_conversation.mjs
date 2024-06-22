5b3ebac08a76ba8a104b0a07cfd50f28ca436593
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 20:20:07 -0400

    2024-06-22T00:20:06.455Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -35,7 +35,9 @@ export function app_gs_conversation(map_c, map, npc) {
       } else {
         html_clear(menu_overlay);
         let verses = app_gs_bible_chapter_jas01();
-        each(verses, (item) => {});
+        each(verses, (item) => {
+          item;
+        });
         let objectables = list_filter(verses, (v) =>
           list_empty_not_is(v.objections),
         );
