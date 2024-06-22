cc15cdcf5cdcf51723e2d200f4e1d2d1a1d574b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 09:45:36 -0400

    2024-06-22T13:45:35.824Z function_auto_after app_gs_conversation_gospel_convert

--- public/src/app_gs_conversation_gospel_convert.mjs
+++ public/src/app_gs_conversation_gospel_convert.mjs
@@ -1,3 +1,4 @@
+import { list_concat } from "./list_concat.mjs";
 import { app_gs_phrase_combine } from "./app_gs_phrase_combine.mjs";
 import { app_gs_phrase_doxology_or_empty } from "./app_gs_phrase_doxology_or_empty.mjs";
 import { app_gs_phrase_the_gospel } from "./app_gs_phrase_the_gospel.mjs";
@@ -14,93 +15,95 @@ export function app_gs_conversation_gospel_convert(menu_overlay, npc) {
   app_gs_conversation_speech_npc(
     menu_overlay,
     npc,
-    app_gs_phrase_combine([
-      app_gs_phrase_worship_multiple_or_empty(),
-      app_gs_phrase_doxology_or_empty(),
-      list_random_item([
-        string_combine_multiple([
-          "You have ",
-          string_random_or_empty(
-            string_combine_multiple([
-              list_random_item(["fully", "completely"]),
-              " ",
-            ]),
-          ),
-          list_random_item(["convinced", "persuaded"]),
-          " me",
-          app_gs_phrase_exclaim_or_period(),
-        ]),
-        string_combine_multiple([
-          "Thank you for ",
-          list_random_item([
-            string_combine_multiple([
-              "sharing ",
-              app_gs_phrase_the_gospel(),
-              " with me",
-            ]),
-            string_combine_multiple([
-              list_random_item([
-                "telling",
-                "taking the time to tell",
-                "persuading",
-                "convincing",
+    app_gs_phrase_combine(
+      list_concat([
+        app_gs_phrase_worship_multiple_or_empty(),
+        app_gs_phrase_doxology_or_empty(),
+        list_random_item([
+          string_combine_multiple([
+            "You have ",
+            string_random_or_empty(
+              string_combine_multiple([
+                list_random_item(["fully", "completely"]),
+                " ",
               ]),
-              " me about ",
-              app_gs_phrase_the_gospel(),
+            ),
+            list_random_item(["convinced", "persuaded"]),
+            " me",
+            app_gs_phrase_exclaim_or_period(),
+          ]),
+          string_combine_multiple([
+            "Thank you for ",
+            list_random_item([
+              string_combine_multiple([
+                "sharing ",
+                app_gs_phrase_the_gospel(),
+                " with me",
+              ]),
+              string_combine_multiple([
+                list_random_item([
+                  "telling",
+                  "taking the time to tell",
+                  "persuading",
+                  "convincing",
+                ]),
+                " me about ",
+                app_gs_phrase_the_gospel(),
+              ]),
             ]),
           ]),
         ]),
-      ]),
-      string_combine_multiple([
-        string_random_or_empty("Now "),
-        "I believe ",
-        list_random_item([
-          string_combine_multiple(["in ", app_gs_phrase_jesus()]),
-          string_combine_multiple([
-            "that ",
-            app_gs_phrase_jesus(),
-            " ",
-            list_random_item([
-              string_combine_multiple([
-                string_random_or_empty(
-                  string_combine_multiple([
-                    "died",
-                    string_random_or_empty(", was buried"),
-                    " and ",
-                  ]),
-                ),
-                "rose to life",
-                string_random_or_empty(
-                  string_combine_multiple([
-                    " with ",
-                    list_random_item(["all my", "my whole"]),
-                    " heart",
-                  ]),
-                ),
-              ]),
-              string_combine_multiple([
-                "is ",
-                list_random_item([
-                  string_combine_multiple([
-                    "King",
-                    string_random_or_empty(" of kings"),
-                  ]),
-                  string_combine_multiple([
-                    "Lord",
-                    string_random_or_empty(" of lords"),
-                  ]),
-                  "alive",
-                  "reigning from on high",
-                  string_combine_multiple([
-                    "my ",
-                    list_random_item([
-                      string_combine_multiple([
-                        "Savior",
-                        string_random_or_empty(" and Lord"),
-                      ]),
-                      string_combine_multiple([
-                        "Lord",
-                        string_random_or_empty(" and Savior"),
+        string_combine_multiple([
+          string_random_or_empty("Now "),
+          "I believe ",
+          list_random_item([
+            string_combine_multiple(["in ", app_gs_phrase_jesus()]),
+            string_combine_multiple([
+              "that ",
+              app_gs_phrase_jesus(),
+              " ",
+              list_random_item([
+                string_combine_multiple([
+                  string_random_or_empty(
+                    string_combine_multiple([
+                      "died",
+                      string_random_or_empty(", was buried"),
+                      " and ",
+                    ]),
+                  ),
+                  "rose to life",
+                  string_random_or_empty(
+                    string_combine_multiple([
+                      " with ",
+                      list_random_item(["all my", "my whole"]),
+                      " heart",
+                    ]),
+                  ),
+                ]),
+                string_combine_multiple([
+                  "is ",
+                  list_random_item([
+                    string_combine_multiple([
+                      "King",
+                      string_random_or_empty(" of kings"),
+                    ]),
+                    string_combine_multiple([
+                      "Lord",
+                      string_random_or_empty(" of lords"),
+                    ]),
+                    "alive",
+                    "reigning from on high",
+                    string_combine_multiple([
+                      "my ",
+                      list_random_item([
+                        string_combine_multiple([
+                          "Savior",
+                          string_random_or_empty(" and Lord"),
+                        ]),
+                        string_combine_multiple([
+                          "Lord",
+                          string_random_or_empty(" and Savior"),
+                        ]),
                       ]),
                     ]),
                   ]),
@@ -108,10 +111,10 @@ export function app_gs_conversation_gospel_convert(menu_overlay, npc) {
               ]),
             ]),
           ]),
+          "!",
         ]),
-        "!",
       ]),
-    ]),
+    ),
   );
   app_gs_conversation_root(menu_overlay, npc);
 }
