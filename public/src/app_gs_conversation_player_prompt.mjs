bf63098985003e7ee94fd3bb058eccc531ac08c0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 21:29:56 -0400

    2024-06-22T01:29:54.647Z function_auto_after app_gs_conversation_player_prompt

--- public/src/app_gs_conversation_player_prompt.mjs
+++ public/src/app_gs_conversation_player_prompt.mjs
@@ -1,9 +1,13 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_style_font_color } from "./html_style_font_color.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { app_gs_overlay_speech } from "./app_gs_overlay_speech.mjs";
 export function app_gs_conversation_player_prompt(menu_overlay, additional) {
   let div_player = app_gs_overlay_speech(menu_overlay);
-  let prompt = html_p_text(div_player, $s1,"What would you like to do?");
+  let prompt = html_p_text(
+    div_player,
+    string_combine_multiple(["What would you like to do?"]),
+  );
   html_style_font_color(prompt, "lightgreen");
   return div_player;
 }
