51511bf6fb14a631ccfce605e692d9cd98bce724
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 21:35:02 -0400

    2024-06-22T01:35:01.780Z function_auto_after app_gs_conversation_gospel

--- public/src/app_gs_conversation_gospel.mjs
+++ public/src/app_gs_conversation_gospel.mjs
@@ -47,7 +47,9 @@ export function app_gs_conversation_gospel(menu_overlay, npc) {
             " me",
             app_gs_phrase_exclaim_or_period(),
           ]),
-          "Thank you for telling me about Jesus",
+          "Thank you for ",
+          "telling",
+          " me about Jesus",
         ]),
         " ",
         string_combine_multiple([
