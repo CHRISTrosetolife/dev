6f03169b2793543e495d981e7a3d8e6d69b62b19
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 21:31:06 -0400

    2024-06-22T01:31:03.581Z function_auto_after app_gs_conversation_root

--- public/src/app_gs_conversation_root.mjs
+++ public/src/app_gs_conversation_root.mjs
@@ -4,7 +4,7 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { app_gs_conversation_player_prompt } from "./app_gs_conversation_player_prompt.mjs";
 export function app_gs_conversation_root(menu_overlay, npc) {
-  let div_player = app_gs_conversation_player_prompt(menu_overlay);
+  let div_player = app_gs_conversation_player_prompt(menu_overlay, "");
   html_button_width_full_text_click(
     div_player,
     string_combine_multiple([
