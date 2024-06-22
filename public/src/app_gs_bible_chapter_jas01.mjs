ea5d16a4ead1183cbdd8f29f49f8348871a24933
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 14:12:15 -0400

    2024-06-22T18:12:15.271Z function_auto_after app_gs_bible_chapter_jas01

--- public/src/app_gs_bible_chapter_jas01.mjs
+++ public/src/app_gs_bible_chapter_jas01.mjs
@@ -327,7 +327,8 @@ export function app_gs_bible_chapter_jas01() {
       objections: [],
       sermons: [
         {
-          sermon: "let us bridle our tongues",
+          sermon:
+            "let us bridle our tongues`those who consider themselves religious, but do not bridle their tongues deceive their own hearts",
           count: 2,
         },
       ],
