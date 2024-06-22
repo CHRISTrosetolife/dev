47717775c449a8ab66688996e6756d221586f8a1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 09:50:28 -0400

    2024-06-22T13:50:27.688Z function_auto_after app_gs_conversation_gospel

--- public/src/app_gs_conversation_gospel.mjs
+++ public/src/app_gs_conversation_gospel.mjs
@@ -1,20 +1,6 @@
+import { app_gs_conversation_witness } from "./app_gs_conversation_witness.mjs";
 import { app_gs_conversation_gospel_reminder } from "./app_gs_conversation_gospel_reminder.mjs";
-import { app_gs_conversation_npc } from "./app_gs_conversation_npc.mjs";
 import { app_gs_conversation_gospel_convert } from "./app_gs_conversation_gospel_convert.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
-import { app_gs_conversation_player_prompt } from "./app_gs_conversation_player_prompt.mjs";
-import { list_join_space } from "./list_join_space.mjs";
-import { list_map_property } from "./list_map_property.mjs";
-import { list_last } from "./list_last.mjs";
-import { list_first } from "./list_first.mjs";
-import { list_slice } from "./list_slice.mjs";
-import { list_random_item } from "./list_random_item.mjs";
-import { list_empty_not_is } from "./list_empty_not_is.mjs";
-import { list_filter } from "./list_filter.mjs";
-import { object_merge } from "./object_merge.mjs";
-import { each_index } from "./each_index.mjs";
-import { app_gs_bible_chapter_jas01 } from "./app_gs_bible_chapter_jas01.mjs";
 import { html_clear } from "./html_clear.mjs";
 export function app_gs_conversation_gospel(menu_overlay, npc) {
   html_clear(menu_overlay);
@@ -23,50 +9,6 @@ export function app_gs_conversation_gospel(menu_overlay, npc) {
   } else if (npc.objection_count === 0) {
     app_gs_conversation_gospel_convert(menu_overlay, npc);
   } else {
-    let book_name = "James";
-    let chapter = "1";
-    let verses = app_gs_bible_chapter_jas01();
-    each_index(verses, (item, index) => {
-      object_merge(item, {
-        index,
-        book_name,
-        chapter,
-      });
-    });
-    let objectables = list_filter(verses, (v) =>
-      list_empty_not_is(v.objections),
-    );
-    let objectable = list_random_item(objectables);
-    let { index } = objectable;
-    let objection = list_random_item(objectable.objections);
-    let { count, text: object_text } = objection;
-    let answer_verses = list_slice(verses, index, index + count);
-    let first = list_first(answer_verses);
-    let last = list_last(answer_verses);
-    let answer_texts = list_map_property(answer_verses, "text");
-    let answer = list_join_space(answer_texts);
-    app_gs_conversation_npc(menu_overlay, npc, object_text);
-    let div_player = app_gs_conversation_player_prompt(
-      menu_overlay,
-      string_combine_multiple(["Tell ", npc.name, ": "]),
-    );
-    html_button_width_full_text_click(
-      div_player,
-      string_combine_multiple([
-        "📖 ",
-        first.book_name,
-        " ",
-        first.chapter,
-        ":",
-        first.verse_number,
-        first !== last ? "-" + last.verse_number : "",
-        " - ",
-        answer,
-      ]),
-      () => {
-        npc.objection_count--;
-        app_gs_conversation_gospel(menu_overlay, npc);
-      },
-    );
+    app_gs_conversation_witness(menu_overlay, npc);
   }
 }
