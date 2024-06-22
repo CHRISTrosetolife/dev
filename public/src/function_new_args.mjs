92ecc7c96e90703775dc2a0bb4eecff28716f961
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 17:56:47 -0400

    2024-06-22T21:56:46.945Z function_auto_after function_new_args

--- public/src/function_new_args.mjs
+++ public/src/function_new_args.mjs
@@ -1,8 +1,10 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { file_write } from "./file_write.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 import { list_join } from "./list_join.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 export async function function_new_args(function_name, args) {
+  assert_arguments_length(arguments, 2);
   let args_split = string_split_comma(args);
   let args_joined = list_join(args_split, ", ");
   await function_new_generic(
