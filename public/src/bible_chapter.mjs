26c095dcc0354ac4efda59772e0b7bca9083e523
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 10:33:05 -0400

    2024-06-22T14:33:04.813Z function_auto_after bible_chapter

--- public/src/bible_chapter.mjs
+++ public/src/bible_chapter.mjs
@@ -2,7 +2,9 @@ import { string_case_upper } from "./string_case_upper.mjs";
 import { bible_verses_parse } from "./bible_verses_parse.mjs";
 import { html_parse_visit_classes_list } from "./html_parse_visit_classes_list.mjs";
 import { bible_chapter_parsed } from "./bible_chapter_parsed.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function bible_chapter(bible_folder, chapter_name) {
+  assert_arguments_length(arguments, 2);
   chapter_name = string_case_upper(chapter_name);
   let root = await bible_chapter_parsed(bible_folder, chapter_name);
   let attribute_values = ["m", "pc", "pmo", "q", "li", "q2", "li2", "d"];
