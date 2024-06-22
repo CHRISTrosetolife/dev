0d104a25a978d0da95a9f8e1ee58cd353349d0d8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 21:28:07 -0400

    2024-06-22T01:28:07.039Z function_auto_after string_random_or_empty

--- public/src/string_random_or_empty.mjs
+++ public/src/string_random_or_empty.mjs
@@ -1,4 +1,6 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { random_50_50 } from "./random_50_50.mjs";
 export function string_random_or_empty(s) {
+  assert_arguments_length(arguments, 1);
   return random_50_50() ? "" : s;
 }
