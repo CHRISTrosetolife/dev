94b365b42984dd09c6e6b7a6a6af980c6475d3c5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 15:49:00 -0400

    2024-06-22T19:49:00.406Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -3,6 +3,7 @@ import { app_gs_conversation_opening } from "./app_gs_conversation_opening.mjs";
 import { app_gs_menu_overlay } from "./app_gs_menu_overlay.mjs";
 export function app_gs_conversation(map_c, map, npc) {
   let menu_overlay = app_gs_menu_overlay(map_c, map);
+  map.player.conversation_count++;
   npc.conversation_count++;
   if (map.player.conversation_count < npc.conversation_wait_until) {
   }
