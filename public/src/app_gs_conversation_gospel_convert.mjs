f139ccdca7bd4d8c3929c3efa0bf1874e4a51d27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 09:45:52 -0400

    2024-06-22T13:45:52.145Z function_auto_after app_gs_conversation_gospel_convert

--- public/src/app_gs_conversation_gospel_convert.mjs
+++ public/src/app_gs_conversation_gospel_convert.mjs
@@ -16,8 +16,8 @@ export function app_gs_conversation_gospel_convert(menu_overlay, npc) {
     menu_overlay,
     npc,
     app_gs_phrase_combine(
-      list_concat([
-        app_gs_phrase_worship_multiple_or_empty(),
+      list_concat(app_gs_phrase_worship_multiple_or_empty(), [
+        ,
         app_gs_phrase_doxology_or_empty(),
         list_random_item([
           string_combine_multiple([
