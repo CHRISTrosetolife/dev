1ac1821f555ddf4830b9acd7f1937b96cb4b7fca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 18:07:38 -0400

    2024-06-22T22:07:29.122Z function_auto_after number_pad

--- public/src/number_pad.mjs
+++ public/src/number_pad.mjs
@@ -1,3 +1,5 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function number_pad(input, padding_count) {
+  assert_arguments_length(arguments, 2);
   return input.toString().padStart(padding_count, "0");
 }
