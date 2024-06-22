a3d7aa669137d96bbb370925f281a6f01368744e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 16:07:47 -0400

    2024-06-22T20:07:46.567Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -40,7 +40,8 @@ export function app_gs_conversation(map_c, map, npc) {
           " to ",
           list_random_item(["think", "ponder"]),
           string_random_or_empty(
-            " about what ",
+            " about ",
+            "what ",
             app_gs_phrase_you_have(),
             list_random_item(["said", "spoken"]),
             string_random_or_empty(" to me"),
