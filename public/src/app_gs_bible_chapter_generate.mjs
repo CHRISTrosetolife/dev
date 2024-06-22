c7eff4802a453ff1576071ad27b50928af096565
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 10:22:51 -0400

    2024-06-22T14:22:50.500Z function_auto_after app_gs_bible_chapter_generate

--- public/src/app_gs_bible_chapter_generate.mjs
+++ public/src/app_gs_bible_chapter_generate.mjs
@@ -1,3 +1,4 @@
+import { list_all } from "./list_all.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { error } from "./error.mjs";
 import { log } from "./log.mjs";
@@ -41,6 +42,7 @@ export async function app_gs_bible_chapter_generate(chapter_name) {
               properties,
               (p) => p.key.type === "Identifier",
             );
+            list_all(properties, (p) => p.key.type !== "Identifier" || p.ke);
             log({
               existing,
             });
