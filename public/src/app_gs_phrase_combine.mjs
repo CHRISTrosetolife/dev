dc258545936035e7f6c5222052ebd42091756d0c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 09:44:00 -0400

    2024-06-22T13:44:00.400Z function_auto_after app_gs_phrase_combine

--- public/src/app_gs_phrase_combine.mjs
+++ public/src/app_gs_phrase_combine.mjs
@@ -1,1 +1,5 @@
-export function app_gs_phrase_combine() {}
+import { list_filter } from "./list_filter.mjs";
+import { list_join_space } from "./list_join_space.mjs";
+export function app_gs_phrase_combine() {
+  return list_join_space(list_filter());
+}
