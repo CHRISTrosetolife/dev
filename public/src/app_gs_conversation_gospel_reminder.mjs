3a1dfb5cd1ed3f45b25a4e1ece4d173998cf8e0e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 10:10:18 -0400

    2024-06-22T14:10:18.373Z function_auto_after app_gs_conversation_gospel_reminder

--- public/src/app_gs_conversation_gospel_reminder.mjs
+++ public/src/app_gs_conversation_gospel_reminder.mjs
@@ -7,38 +7,33 @@ import { app_gs_phrase_the_gospel } from "./app_gs_phrase_the_gospel.mjs";
 import { string_random_or_empty } from "./string_random_or_empty.mjs";
 import { list_random_item } from "./list_random_item.mjs";
 import { app_gs_phrase_thanks } from "./app_gs_phrase_thanks.mjs";
-import { app_gs_phrase_worship_multiple_or_empty } from "./app_gs_phrase_worship_multiple_or_empty.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { list_concat_multiple } from "./list_concat_multiple.mjs";
 import { app_gs_phrase_combine } from "./app_gs_phrase_combine.mjs";
 export function app_gs_conversation_gospel_reminder(menu_overlay, npc) {
-  let list = list_concat_multiple([
-    app_gs_phrase_worship_multiple_or_empty(),
-    [
+  let list = [
+    string_combine_multiple([
+      app_gs_phrase_thanks(),
+      " for ",
+      list_random_item(["a reminder", "reminding me"]),
+      string_random_or_empty(
+        string_combine_multiple([" of ", app_gs_phrase_the_gospel()]),
+      ),
+      "!",
+    ]),
+    string_random_or_empty(
       string_combine_multiple([
-        app_gs_phrase_thanks(),
-        " for ",
-        list_random_item(["a reminder", "reminding me"]),
-        string_random_or_empty(
-          string_combine_multiple([" of ", app_gs_phrase_the_gospel()]),
-        ),
+        list_random_item(["Yes", "Indeed"]),
+        ", ",
+        app_gs_phrase_jesus(),
+        " ",
+        app_gs_phrase_gospel_verbs(),
         "!",
       ]),
-      string_random_or_empty(
-        string_combine_multiple([
-          list_random_item(["Yes", "Indeed"]),
-          ", ",
-          app_gs_phrase_jesus(),
-          " ",
-          app_gs_phrase_gospel_verbs(),
-          "!",
-        ]),
-      ),
-      string_random_or_empty(
-        string_combine_multiple([" ", app_gs_phrase_doxology()]),
-      ),
-    ],
-  ]);
+    ),
+    string_random_or_empty(
+      string_combine_multiple([" ", app_gs_phrase_doxology()]),
+    ),
+  ];
   list_shuffle(list);
   app_gs_conversation_npc_root(menu_overlay, npc, app_gs_phrase_combine(list));
 }
