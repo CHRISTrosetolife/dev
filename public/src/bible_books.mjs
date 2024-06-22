234ee85041f63c5b17edb8ab666478bc184cc46f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 18:23:28 -0400

    2024-06-22T22:23:27.193Z function_auto_after bible_books

--- public/src/bible_books.mjs
+++ public/src/bible_books.mjs
@@ -6,14 +6,12 @@ import { html_cache_parse } from "./html_cache_parse.mjs";
 import { bible_url_base } from "./bible_url_base.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
 import { string_take } from "./string_take.mjs";
-import { html_parse_text } from "./html_parse_text.mjs";
 export async function bible_books(bible_folder) {
   let url_base = bible_url_base(bible_folder);
   let root = await html_cache_parse(url_base);
   let vnavs = html_parse_visit_class_list(root, "vnav");
   let vnav = list_single(vnavs);
   let book_elements = html_parse_visit_classes_list(vnav, ["oo", "nn"]);
-  return list_map(book_elements, html_parse_text);
   let book_chapter_ones = list_map(book_elements, html_parse_href);
   let book_prefixes = list_map(book_chapter_ones, (s) => string_take(s, 3));
   return book_prefixes;
