ee43e6635172a0482bf4b8cd8692f4ada8bf5727
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 18:06:30 -0400

    2024-06-22T22:06:29.520Z function_auto_after app_gs_bible_chapter_prefix

--- public/src/app_gs_bible_chapter_prefix.mjs
+++ public/src/app_gs_bible_chapter_prefix.mjs
@@ -1,4 +1,4 @@
 import { string_suffix_without } from "./string_suffix_without.mjs";
 export function app_gs_bible_chapter_prefix() {
-  return string_suffix_without(app_gs_bible_chapter_prefix.name, "generate");
+  return string_suffix_without(app_gs_bible_chapter_prefix.name, "prefix");
 }
