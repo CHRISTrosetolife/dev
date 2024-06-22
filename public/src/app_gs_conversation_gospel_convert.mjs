cf76f4ebba53e90dcfa2fe80effa7cb0542182c6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 15:41:44 -0400

    2024-06-22T19:41:43.902Z function_auto_after app_gs_conversation_gospel_convert

--- public/src/app_gs_conversation_gospel_convert.mjs
+++ public/src/app_gs_conversation_gospel_convert.mjs
@@ -1,4 +1,3 @@
-import { error } from "./error.mjs";
 import { app_gs_phrase_i_am } from "./app_gs_phrase_i_am.mjs";
 import { app_gs_phrase_convinced } from "./app_gs_phrase_convinced.mjs";
 import { app_gs_phrase_thanks } from "./app_gs_phrase_thanks.mjs";
@@ -115,6 +114,6 @@ export function app_gs_conversation_gospel_convert(menu_overlay, npc, map) {
         ]),
       ]),
     ]),
-    error(),
+    map,
   );
 }
