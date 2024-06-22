7fc665b613d93e04531f1147b56953385cac50af
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 10:42:10 -0400

    2024-06-22T14:42:10.425Z function_auto_after bible_verses_parse

--- public/src/bible_verses_parse.mjs
+++ public/src/bible_verses_parse.mjs
@@ -11,8 +11,8 @@ import { list_add_multiple } from "./list_add_multiple.mjs";
 import { string_whitespace_normalize } from "./string_whitespace_normalize.mjs";
 export function bible_verses_parse(verses) {
   let result = list_adder((la) => {
+    let verse_number, tokens;
     each(verses, (v) => {
-      let verse_number, tokens;
       log(html_parse_text(v));
       each(children, (c) => {
         if (c.type === "tag") {
