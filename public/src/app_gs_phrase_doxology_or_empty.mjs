baa0d57aff769c837dff85b9006721921423024e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 09:42:37 -0400

    2024-06-22T13:42:36.652Z function_auto_after app_gs_phrase_doxology_or_empty

--- public/src/app_gs_phrase_doxology_or_empty.mjs
+++ public/src/app_gs_phrase_doxology_or_empty.mjs
@@ -1,8 +1,5 @@
 import { app_gs_phrase_doxology } from "./app_gs_phrase_doxology.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_random_or_empty } from "./string_random_or_empty.mjs";
 export function app_gs_phrase_doxology_or_empty() {
-  return string_random_or_empty(
-    string_combine_multiple([app_gs_phrase_doxology(), " "]),
-  );
+  return string_random_or_empty(app_gs_phrase_doxology());
 }
