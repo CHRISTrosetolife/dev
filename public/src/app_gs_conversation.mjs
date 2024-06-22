7ce8507032fc7d62b0ce14b5eb2029b5d51fda2c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 15:57:46 -0400

    2024-06-22T19:57:45.819Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -21,7 +21,7 @@ export function app_gs_conversation(map_c, map, npc) {
         string_random_or_empty(
           " about what ",
           app_gs_phrase_you_have(),
-          list_random_item(["said", "have spoken"]),
+          list_random_item(["said", "spoken"]),
           string_random_or_empty(" to me"),
         ),
       ]),
