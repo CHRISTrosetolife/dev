3ff300cd9612a8aacf1fbeab25a49d75edffafd4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 15:54:38 -0400

    2024-06-22T19:54:37.917Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -1,3 +1,4 @@
+import { app_gs_conversation_npc_end } from "./app_gs_conversation_npc_end.mjs";
 import { app_gs_conversation_npc_opening_text } from "./app_gs_conversation_npc_opening_text.mjs";
 import { app_gs_conversation_npc_opening } from "./app_gs_conversation_npc_opening.mjs";
 import { app_gs_menu_overlay } from "./app_gs_menu_overlay.mjs";
@@ -6,6 +7,7 @@ export function app_gs_conversation(map_c, map, npc) {
   map.player.conversation_count++;
   npc.conversation_count++;
   if (map.player.conversation_count < npc.conversation_wait_until) {
+    app_gs_conversation_npc_end(menu_overlay, npc, npc_text);
   } else {
     let text = app_gs_conversation_npc_opening_text(map, npc);
     app_gs_conversation_npc_opening(menu_overlay, npc, text, map);
