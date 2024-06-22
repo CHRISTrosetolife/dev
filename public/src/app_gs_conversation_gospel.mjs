090daf7ee06f167dde0162dec454389fbe06ca3e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 09:27:01 -0400

    2024-06-22T13:27:01.036Z function_auto_after app_gs_conversation_gospel

--- public/src/app_gs_conversation_gospel.mjs
+++ public/src/app_gs_conversation_gospel.mjs
@@ -24,6 +24,7 @@ export function app_gs_conversation_gospel(menu_overlay, npc) {
   html_clear(menu_overlay);
   if (npc.christian) {
     app_gs_conversation_speech_npc(menu_overlay, npc, "");
+    app_gs_conversation_root(menu_overlay, npc);
   } else if (npc.objection_count === 0) {
     npc.christian = true;
     app_gs_conversation_speech_npc(
