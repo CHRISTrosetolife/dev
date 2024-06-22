bc97a032f3431a6f1e3f8143622fbfbd9cf67eb9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 20:37:04 -0400

    2024-06-22T00:37:04.558Z function_auto_after app_gs_conversation_gospel

--- public/src/app_gs_conversation_gospel.mjs
+++ public/src/app_gs_conversation_gospel.mjs
@@ -30,7 +30,7 @@ export function app_gs_conversation_gospel(menu_overlay, npc) {
           "Praise be to God",
         ]),
         "! ",
-        "I believe in Jesus",
+        "I believe in Jesus!",
       ]),
     );
   } else {
