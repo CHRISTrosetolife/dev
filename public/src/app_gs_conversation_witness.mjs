088b2d7602f3d6684c3d2d276b01e79a6629ead5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 14:45:52 -0400

    2024-06-22T18:45:52.257Z function_auto_after app_gs_conversation_witness

--- public/src/app_gs_conversation_witness.mjs
+++ public/src/app_gs_conversation_witness.mjs
@@ -52,6 +52,7 @@ export function app_gs_conversation_witness(menu_overlay, npc) {
   );
   let choices = [choice_wrong, choice_correct];
   list_shuffle(choices);
+  each(list, (item2) => {});
   html_button_width_full_text_click(
     div_player,
     choice_correct.answer_text,
