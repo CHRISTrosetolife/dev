684554e665bceeac099e4e216193f8012c5d2187
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-21 20:50:42 -0400

    2024-06-22T00:50:41.578Z function_auto_after app_gs_conversation_gospel

--- public/src/app_gs_conversation_gospel.mjs
+++ public/src/app_gs_conversation_gospel.mjs
@@ -28,7 +28,7 @@ export function app_gs_conversation_gospel(menu_overlay, npc) {
           "Amen",
           "Alleluia",
           "Hallelujah",
-          "Praise the Lord",
+          string_combine_multiple(["Praise the ", "Lord"]),
           "Thank You, Jesus",
           string_combine_multiple([
             "Praise be to ",
