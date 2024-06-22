b458447ca9ca381631f047d0c2a48a5b58d099c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 09:42:21 -0400

    2024-06-22T13:42:20.998Z function_auto_after app_gs_phrase_worship_multiple_or_empty

--- public/src/app_gs_phrase_worship_multiple_or_empty.mjs
+++ public/src/app_gs_phrase_worship_multiple_or_empty.mjs
@@ -2,10 +2,8 @@ import { app_gs_phrase_worship } from "./app_gs_phrase_worship.mjs";
 import { string_random_or_empty } from "./string_random_or_empty.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_gs_phrase_worship_multiple_or_empty() {
-  return string_combine_multiple([
-    string_random_or_empty("Amen! "),
-    string_random_or_empty(
-      string_combine_multiple([app_gs_phrase_worship(), " "]),
-    ),
-  ]);
+  return [
+    string_random_or_empty("Amen!"),
+    string_random_or_empty(string_combine_multiple([app_gs_phrase_worship()])),
+  ];
 }
