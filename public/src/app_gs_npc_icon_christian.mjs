ba153210e44e62b4841865b6bd3f3ca53af15d1c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 13:59:54 -0400

    2024-06-23T17:59:54.139Z function_auto_after app_gs_npc_icon_christian

--- public/src/app_gs_npc_icon_christian.mjs
+++ public/src/app_gs_npc_icon_christian.mjs
@@ -1,3 +1,6 @@
+import { game_tile_units_css } from "./game_tile_units_css.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { html_style_filter } from "./html_style_filter.mjs";
 import { app_gs_npc_icon_generic } from "./app_gs_npc_icon_generic.mjs";
 export function app_gs_npc_icon_christian(c_blur, c) {
   app_gs_npc_icon_generic(
@@ -5,5 +8,13 @@ export function app_gs_npc_icon_christian(c_blur, c) {
     c,
     "icons/cross.svg",
     "invert(80%) sepia(52%) saturate(474%) hue-rotate(48deg) brightness(103%) contrast(102%)",
+  );
+  html_style_filter(
+    c_blur,
+    string_combine_multiple([
+      "invert(100%) sepia(100%) saturate(1%) hue-rotate(144deg) brightness(102%) contrast(101%) blur(",
+      game_tile_units_css(1 / 46),
+      ")",
+    ]),
   );
 }
