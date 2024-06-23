d8860269f9d966aed8a1dff744f8f7567af47414
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 13:30:27 -0400

    2024-06-23T17:30:26.604Z function_auto_after app_gs_npc_icon_unmet

--- public/src/app_gs_npc_icon_unmet.mjs
+++ public/src/app_gs_npc_icon_unmet.mjs
@@ -1,4 +1,4 @@
-import { html_style } from "./html_style.mjs";
+import { html_style_filter } from "./html_style_filter.mjs";
 import { html_img_src } from "./html_img_src.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function app_gs_npc_icon_unmet(c_blur, c) {
@@ -6,7 +6,5 @@ export function app_gs_npc_icon_unmet(c_blur, c) {
   html_img_src(c, "icons/question.svg");
   let filter =
     "invert(11%) sepia(100%) saturate(5663%) hue-rotate(239deg) brightness(105%) contrast(113%)";
-  html_style(c, {
-    filter,
-  });
+  html_style_filter(c, filter);
 }
---
+++ public/src/html_style_filter.mjs
@@ -0,0 +1,6 @@
+import { html_style } from "./html_style.mjs";
+export function html_style_filter(c, filter) {
+  html_style(c, {
+    filter,
+  });
+}
