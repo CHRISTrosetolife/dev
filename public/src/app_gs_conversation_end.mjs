236b93956228ec390918db50f885c894fb6de3ea
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 16:30:31 -0400

    2024-06-23T20:30:31.202Z function_auto_after app_gs_conversation_end

--- public/src/app_gs_conversation_end.mjs
+++ public/src/app_gs_conversation_end.mjs
@@ -7,7 +7,7 @@ export function app_gs_conversation_end(div_player, menu_overlay, npc, map) {
     div_player,
     "👋 Politely end the conversation after 🙏 praying",
     () => {
-      let npc_c = app_gs_map_html_at_find_multiple(map, npc, [
+      let [c_blur, c] = app_gs_map_html_at_find_multiple(map, npc, [
         "npc_overlay_blur",
         "npc_overlay",
       ]);
