8db9eaa511bf1e871cab8f00e3b76d8b37529aa3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 14:26:45 -0400

    2024-06-23T18:26:44.058Z function_auto_after search_open

--- public/src/search_open.mjs
+++ public/src/search_open.mjs
@@ -1,10 +1,9 @@
 import { list_sort } from "./list_sort.mjs";
-import { list_adder } from "./list_adder.mjs";
 import { function_search } from "./function_search.mjs";
 import { object_properties } from "./object_properties.mjs";
 export async function search_open(query) {
   let results = await function_search(query);
   let properties = object_properties(results);
-  list_sort(properties, string_size);
-  list_adder((la) => {});
+  let first = list_sort(properties, string_size);
+  return results;
 }
