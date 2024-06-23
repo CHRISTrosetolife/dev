020de4c8963c37b9dfcbcfcfe5e6edc1a0202619
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 13:15:05 -0400

    2024-06-23T17:15:03.614Z function_auto_after app_gs_npc_icon_wait

--- public/src/app_gs_npc_icon_wait.mjs
+++ public/src/app_gs_npc_icon_wait.mjs
@@ -1,7 +1,7 @@
 import { html_style } from "./html_style.mjs";
 import { html_img_src } from "./html_img_src.mjs";
 export function app_gs_npc_icon_wait(c) {
-  html_img_src(c, "icons/cross.svg");
+  html_img_src(c, "icons/x.svg");
   html_style(c, {
     filter:
       "invert(33%) sepia(69%) saturate(6226%) hue-rotate(349deg) brightness(101%) contrast(113%)",
