3bc7c12e7ef487d007b50983c48d23a8b80d5869
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 13:45:16 -0400

    2024-06-23T17:45:16.229Z function_auto_after app_gs_npc_icon_christian

--- public/src/app_gs_npc_icon_christian.mjs
+++ public/src/app_gs_npc_icon_christian.mjs
@@ -1,5 +1,5 @@
 import { app_gs_npc_icon_generic } from "./app_gs_npc_icon_generic.mjs";
-export function app_gs_npc_icon_christian(c) {
+export function app_gs_npc_icon_christian(c_blur, c) {
   app_gs_npc_icon_generic(
     c_blur,
     c,
