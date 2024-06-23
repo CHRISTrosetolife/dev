30dfce399ca71a37529c33b5bf59c91568760fa2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 17:08:42 -0400

    2024-06-23T21:08:41.994Z function_auto_after app_gs_npc_icon_unconverted

--- public/src/app_gs_npc_icon_unconverted.mjs
+++ public/src/app_gs_npc_icon_unconverted.mjs
@@ -2,4 +2,5 @@ import { app_gs_npc_icon_green } from "./app_gs_npc_icon_green.mjs";
 export function app_gs_npc_icon_unconverted(c_blur, c) {
   let icon_name = "exclaim";
   app_gs_npc_icon_green(c_blur, c, icon_name);
+  ("invert(99%) sepia(98%) saturate(7493%) hue-rotate(12deg) brightness(118%) contrast(106%)");
 }
