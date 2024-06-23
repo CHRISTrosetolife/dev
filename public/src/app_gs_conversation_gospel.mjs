e88d54e29e5dbb7de7f88d1b96d09c3500995c68
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 20:43:25 -0400

    2024-06-23T00:43:22.086Z function_auto_after app_gs_conversation_gospel

--- public/src/app_gs_conversation_gospel.mjs
+++ public/src/app_gs_conversation_gospel.mjs
@@ -2,13 +2,14 @@ import { app_gs_conversation_witness } from "./app_gs_conversation_witness.mjs";
 import { app_gs_conversation_gospel_reminder } from "./app_gs_conversation_gospel_reminder.mjs";
 import { app_gs_conversation_gospel_convert } from "./app_gs_conversation_gospel_convert.mjs";
 import { html_clear } from "./html_clear.mjs";
-export function app_gs_conversation_gospel(menu_overlay, npc, map) {
+import { list_empty_is } from "./list_empty_is.mjs";
+export async function app_gs_conversation_gospel(menu_overlay, npc, map) {
   html_clear(menu_overlay);
   if (npc.christian) {
     app_gs_conversation_gospel_reminder(menu_overlay, npc, map);
-  } else if (npc.objection_count === 0) {
+  } else if (list_empty_is(npc.objections)) {
     app_gs_conversation_gospel_convert(menu_overlay, npc, map);
   } else {
-    app_gs_conversation_witness(menu_overlay, npc, map);
+    await app_gs_conversation_witness(menu_overlay, npc, map);
   }
 }
