a0395380af28178e2e17abb6effe8dfd6e79501a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 16:25:46 -0400

    2024-06-23T20:25:46.529Z function_auto_after app_gs_conversation_end

--- public/src/app_gs_conversation_end.mjs
+++ public/src/app_gs_conversation_end.mjs
@@ -1,3 +1,4 @@
+import { app_gs_map_html_at_find } from "./app_gs_map_html_at_find.mjs";
 import { app_gs_npc_icon_update } from "./app_gs_npc_icon_update.mjs";
 import { html_remove } from "./html_remove.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
@@ -6,6 +7,7 @@ export function app_gs_conversation_end(div_player, menu_overlay, npc) {
     div_player,
     "👋 Politely end the conversation after 🙏 praying",
     () => {
+      let npc_c = app_gs_map_html_at_find(map, npc, "npc");
       app_gs_npc_icon_update(map, npc, c_blur, c);
       html_remove(menu_overlay);
     },
