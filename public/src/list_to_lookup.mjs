14de59ad86de2413d1605ee6d92defdd1aabff16
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 20:22:50 -0400

    2024-06-23T00:22:50.414Z function_auto_after list_to_lookup

--- public/src/list_to_lookup.mjs
+++ public/src/list_to_lookup.mjs
@@ -1,6 +1,8 @@
 import { object_property_set } from "./object_property_set.mjs";
 import { each } from "./each.mjs";
+import { identity } from "./identity.mjs";
 export function list_to_lookup(list, mapper_key) {
+  let mapped_value = identity;
   let lookup = {};
   each(list, (w) => {
     object_property_set(lookup, mapper_key(w), w);
