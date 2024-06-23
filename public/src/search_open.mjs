edb9a05daa674084ad0091f1571f37333f12cb1f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 14:25:12 -0400

    2024-06-23T18:25:11.374Z function_auto_after search_open

--- public/src/search_open.mjs
+++ public/src/search_open.mjs
@@ -1,6 +1,9 @@
+import { each_object } from "./each_object.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { function_search } from "./function_search.mjs";
 export async function search_open(query) {
   let results = await function_search(query);
-  list_adder((la) => {});
+  list_adder((la) => {
+    each_object(object, (key, value) => {});
+  });
 }
