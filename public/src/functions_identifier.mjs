e1a5b6fbb67be387ee0dd015db95ef5e61180724
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 15:58:04 -0400

    2024-06-23T19:58:04.270Z function_auto_after functions_identifier

--- public/src/functions_identifier.mjs
+++ public/src/functions_identifier.mjs
@@ -1,4 +1,4 @@
 import { data_identifiers } from "./data_identifiers.mjs";
 export async function functions_identifier(name) {
-  return await data_identifiers();
+  let ids = await data_identifiers();
 }
