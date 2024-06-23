aafc19a8e8465bddd3c25f1edcc80020fd10b839
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 20:24:41 -0400

    2024-06-23T00:24:40.981Z function_auto_after bible_books_prefix_to_name_generate

--- public/src/bible_books_prefix_to_name_generate.mjs
+++ public/src/bible_books_prefix_to_name_generate.mjs
@@ -1,3 +1,7 @@
+import { file_overwrite } from "./file_overwrite.mjs";
+import { js_code_array } from "./js_code_array.mjs";
+import { js_code_statement_return } from "./js_code_statement_return.mjs";
+import { function_new_generic } from "./function_new_generic.mjs";
 import { list_to_lookup_value } from "./list_to_lookup_value.mjs";
 import { bible_books_html_element_to_prefix } from "./bible_books_html_element_to_prefix.mjs";
 import { bible_books_html_elements } from "./bible_books_html_elements.mjs";
@@ -8,5 +12,14 @@ export async function bible_books_prefix_to_name_generate() {
     book_elements,
     bible_books_html_element_to_prefix,
     html_parse_text,
+  );
+  await function_new_generic(
+    name,
+    "",
+    js_code_statement_return(js_code_array(delimited)),
+    false,
+    [],
+    false,
+    file_overwrite,
   );
 }
