7696d1f6ed13f3dcb9da58f8939f15a828e82294
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 20:19:34 -0400

    2024-06-23T00:19:33.585Z function_auto_after bible_books

--- public/src/bible_books.mjs
+++ public/src/bible_books.mjs
@@ -4,7 +4,7 @@ import { html_parse_href } from "./html_parse_href.mjs";
 import { string_take } from "./string_take.mjs";
 export async function bible_books(bible_folder) {
   let book_elements = await bible_books_html_elements(bible_folder);
-  let book_chapter_ones = list_map(book_elements, html_parse_href);
+  let book_chapter_ones = list_map(book_elements, (e) => html_parse_href(e));
   let book_prefixes = list_map(book_chapter_ones, (s) => string_take(s, 3));
   return book_prefixes;
 }
