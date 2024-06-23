2a8ae94a490fd70514100f4675c5188782d3d576
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 16:29:33 -0400

    2024-06-23T20:29:32.599Z function_auto_after app_gs_map_html_at_find_multiple

--- public/src/app_gs_map_html_at_find_multiple.mjs
+++ public/src/app_gs_map_html_at_find_multiple.mjs
@@ -1,7 +1,7 @@
 import { html_data_get } from "./html_data_get.mjs";
 import { list_find } from "./list_find.mjs";
 import { app_gs_map_html_at } from "./app_gs_map_html_at.mjs";
-export function app_gs_map_html_at_find_multiple(map, npc, data_type) {
+export function app_gs_map_html_at_find_multiple(map, npc, data_types) {
   let tile_cs = app_gs_map_html_at(map, npc);
   let npc_c = list_find(
     tile_cs,
