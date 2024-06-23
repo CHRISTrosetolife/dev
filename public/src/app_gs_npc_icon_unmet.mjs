e7b26f23cc2406c9b61eaa9772ef737a86b1dd61
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 14:09:45 -0400

    2024-06-23T18:09:41.936Z function_auto_after app_gs_npc_icon_unmet

--- public/src/app_gs_npc_icon_unmet.mjs
+++ public/src/app_gs_npc_icon_unmet.mjs
@@ -1,3 +1,4 @@
+import { app_gs_npc_icon_blur } from "./app_gs_npc_icon_blur.mjs";
 import { app_gs_npc_icon_generic } from "./app_gs_npc_icon_generic.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function app_gs_npc_icon_unmet(c_blur, c) {
@@ -6,4 +7,5 @@ export function app_gs_npc_icon_unmet(c_blur, c) {
   let filter =
     "invert(11%) sepia(100%) saturate(5663%) hue-rotate(239deg) brightness(105%) contrast(113%)";
   app_gs_npc_icon_generic(c_blur, c, img_url, filter);
+  app_gs_npc_icon_blur(c_blur, true);
 }
