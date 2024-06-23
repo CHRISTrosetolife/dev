170d2e0aa79eceebc3fded4a5f141400b29dfb17
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 20:22:23 -0400

    2024-06-23T00:22:22.905Z function_auto_after list_to_lookup

--- public/src/list_to_lookup.mjs
+++ public/src/list_to_lookup.mjs
@@ -1,8 +1,8 @@
 import { object_property_set } from "./object_property_set.mjs";
 import { each } from "./each.mjs";
-export function list_to_lookup(ceb_scores, mapper_key) {
+export function list_to_lookup(list, mapper_key) {
   let lookup = {};
-  each(ceb_scores, (w) => {
+  each(list, (w) => {
     object_property_set(lookup, mapper_key(w), w);
   });
   return lookup;
