c203c33c3a00d19e3fdfaf5176e1d13212ed3f7f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 18:14:44 -0400

    2024-06-23T22:14:43.617Z function_auto_after html_scrollable_hide

--- public/src/html_scrollable_hide.mjs
+++ public/src/html_scrollable_hide.mjs
@@ -3,6 +3,9 @@ import { html_style } from "./html_style.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
 import { html_element } from "./html_element.mjs";
 export function html_scrollable_hide(root, map_c) {
+  html_style(root, {
+    overflow: "none",
+  });
   let style = html_element(root, "style");
   let id = html_id(root);
   html_inner_set(
