bed9a12d39d4806cfde172f9ee76474447130f16
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 09:05:59 -0400

    2024-06-23T13:05:59.011Z function_auto_after list_without

--- public/src/list_without.mjs
+++ public/src/list_without.mjs
@@ -1,5 +1,5 @@
 import { list_filter } from "./list_filter.mjs";
 import { equal_not } from "./equal_not.mjs";
-export function list_without(atom, pair) {
-  return list_filter(atom, (a) => equal_not(a, pair));
+export function list_without(list, item) {
+  return list_filter(list, (a) => equal_not(a, item));
 }
