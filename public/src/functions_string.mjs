4f10921f1cb3c05390a0b8d22da5f9d626066735
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 16:23:59 -0400

    2024-06-23T20:23:59.299Z function_auto_after functions_string

--- public/src/functions_string.mjs
+++ public/src/functions_string.mjs
@@ -1,10 +1,10 @@
+import { data_literal_strings } from "./data_literal_strings.mjs";
 import { function_names_to_lookup } from "./function_names_to_lookup.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { data_identifiers } from "./data_identifiers.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function functions_string(name) {
   assert_arguments_length(arguments, 1);
-  let ids = await data_identifiers();
+  let ids = await data_literal_strings();
   let matches = object_property_get(ids, name);
   return function_names_to_lookup(matches);
 }
