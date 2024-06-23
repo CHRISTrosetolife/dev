da7a26c3dbbaa27835edfeca315a48c615187937
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 16:02:59 -0400

    2024-06-23T20:02:59.202Z function_auto_after list_to_lookup_property

--- public/src/list_to_lookup_property.mjs
+++ public/src/list_to_lookup_property.mjs
@@ -1,6 +1,6 @@
 import { list_to_lookup } from "./list_to_lookup.mjs";
-export function list_to_lookup_property(ceb_scores, property_name) {
-  let lookup = list_to_lookup(ceb_scores, mapper);
+export function list_to_lookup_property(list, property_name) {
+  let lookup = list_to_lookup(list, mapper);
   function mapper(w) {
     return w[property_name];
   }
