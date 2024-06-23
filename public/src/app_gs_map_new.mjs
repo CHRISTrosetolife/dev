2c5cfe4b0ef60c53717904b23d3b9f109932ad8e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 09:11:34 -0400

    2024-06-23T13:11:33.605Z function_auto_after app_gs_map_new

--- public/src/app_gs_map_new.mjs
+++ public/src/app_gs_map_new.mjs
@@ -1,3 +1,4 @@
+import { list_size } from "./list_size.mjs";
 import { app_gs_objection_ids } from "./app_gs_objection_ids.mjs";
 import { app_gs_npc_new } from "./app_gs_npc_new.mjs";
 import { app_gs_tile_overlay_add } from "./app_gs_tile_overlay_add.mjs";
@@ -159,7 +160,7 @@ export function app_gs_map_new() {
   app_gs_player_new(map);
   object_merge(map.player, spawn);
   let objection_ids = app_gs_objection_ids();
-  let npc_count = 1;
+  let npc_count = list_size(objection_ids);
   each_range(npc_count, () => {
     app_gs_npc_new(map, inside, objection_ids);
   });
