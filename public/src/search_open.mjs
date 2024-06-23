a316a4cac6ee027f15a4e6e277a3a57d2ef12481
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 14:26:22 -0400

    2024-06-23T18:26:21.435Z function_auto_after search_open

--- public/src/search_open.mjs
+++ public/src/search_open.mjs
@@ -1,8 +1,10 @@
+import { list_sort } from "./list_sort.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { function_search } from "./function_search.mjs";
 import { object_properties } from "./object_properties.mjs";
 export async function search_open(query) {
   let results = await function_search(query);
   let properties = object_properties();
+  list_sort(properties, string_size);
   list_adder((la) => {});
 }
