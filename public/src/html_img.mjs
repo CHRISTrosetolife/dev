bce443ed10343ebc3b864c0b41f8ad5bef6fdb40
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 12:48:39 -0400

    2024-06-23T16:48:38.438Z function_auto_after html_img

--- public/src/html_img.mjs
+++ public/src/html_img.mjs
@@ -1,7 +1,7 @@
+import { html_img_element } from "./html_img_element.mjs";
 import { html_img_src } from "./html_img_src.mjs";
-import { html_element } from "./html_element.mjs";
 export function html_img(parent, img_name) {
-  let component = html_element(parent, "img");
+  let component = html_img_element(parent);
   html_img_src(component, img_name);
   return component;
 }
---
+++ public/src/html_img_element.mjs
@@ -0,0 +1,4 @@
+import { html_element } from "./html_element.mjs";
+export function html_img_element(parent) {
+  return html_element(parent, "img");
+}
