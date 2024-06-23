93d84546b87cb552f7f08865b8c242d648e0c3c6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 14:09:25 -0400

    2024-06-23T18:09:22.579Z function_auto_after app_gs_npc_icon_wait

--- public/src/app_gs_npc_icon_wait.mjs
+++ public/src/app_gs_npc_icon_wait.mjs
@@ -1,3 +1,4 @@
+import { app_gs_npc_icon_blur } from "./app_gs_npc_icon_blur.mjs";
 import { app_gs_npc_icon_generic } from "./app_gs_npc_icon_generic.mjs";
 export function app_gs_npc_icon_wait(c_blur, c) {
   app_gs_npc_icon_generic(
@@ -6,4 +7,5 @@ export function app_gs_npc_icon_wait(c_blur, c) {
     "icons/x.svg",
     "invert(33%) sepia(69%) saturate(6226%) hue-rotate(349deg) brightness(101%) contrast(113%)",
   );
+  app_gs_npc_icon_blur(c_blur, false);
 }
