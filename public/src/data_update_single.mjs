df66c4b5f7b5a810cac0445566fed56ca6135633
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 16:16:48 -0400

    2024-06-23T20:16:48.330Z function_auto_after data_update_single

--- public/src/data_update_single.mjs
+++ public/src/data_update_single.mjs
@@ -4,7 +4,6 @@ import { list_remove_if_exists } from "./list_remove_if_exists.mjs";
 import { each_object } from "./each_object.mjs";
 import { list_add_if_exists_not } from "./list_add_if_exists_not.mjs";
 import { js_identifiers } from "./js_identifiers.mjs";
-import { string_delimit_if } from "./string_delimit_if.mjs";
 import { js_node_type } from "./js_node_type.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
@@ -14,6 +13,7 @@ import { list_unique } from "./list_unique.mjs";
 import { list_sort_string } from "./list_sort_string.mjs";
 import { list_includes } from "./list_includes.mjs";
 import { string_is } from "./string_is.mjs";
+import { identity } from "./identity.mjs";
 export function data_update_single(ast, data) {
   let declaration = js_declaration_single(ast);
   let {
@@ -27,7 +27,7 @@ export function data_update_single(ast, data) {
   let mapped = list_map_property(ast_literals, "value");
   let filtered = list_filter(mapped, string_is);
   let u = list_unique(filtered);
-  list_sort_string(u, string_delimit_if);
+  list_sort_string(u, identity);
   let literals = object_property_initialize(data, "literal_strings", {});
   each(literals, (id) => object_property_initialize(identifiers, id, []));
   object_property_set(f, "literals", u);
