49332cdb12e0408cc483959c64291d5832cbe74b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 17:09:06 -0400

    2024-06-23T21:09:05.456Z function_auto_after app_gs_npc_icon_unconverted

--- public/src/app_gs_npc_icon_unconverted.mjs
+++ public/src/app_gs_npc_icon_unconverted.mjs
@@ -1,6 +1,13 @@
-import { app_gs_npc_icon_green } from "./app_gs_npc_icon_green.mjs";
+import { app_gs_npc_icon_blur } from "./app_gs_npc_icon_blur.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { app_gs_npc_icon_generic } from "./app_gs_npc_icon_generic.mjs";
 export function app_gs_npc_icon_unconverted(c_blur, c) {
   let icon_name = "exclaim";
-  app_gs_npc_icon_green(c_blur, c, icon_name);
-  ("invert(99%) sepia(98%) saturate(7493%) hue-rotate(12deg) brightness(118%) contrast(106%)");
+  app_gs_npc_icon_generic(
+    c_blur,
+    c,
+    string_combine_multiple(["icons/", icon_name, ".svg"]),
+    "invert(99%) sepia(98%) saturate(7493%) hue-rotate(12deg) brightness(118%) contrast(106%)",
+  );
+  app_gs_npc_icon_blur(c_blur, false);
 }
