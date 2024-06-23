d075a755e57f415aa87ec52b938e6f37559a1343
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 16:21:39 -0400

    2024-06-23T20:21:39.075Z function_auto_after data_update_single

--- public/src/data_update_single.mjs
+++ public/src/data_update_single.mjs
@@ -38,7 +38,7 @@ export function data_update_single(ast, data) {
     let literals = object_property_initialize(data, item.key, {});
     each(item.ast_list, (s) => object_property_initialize(literals, s, []));
     each_object(literals, (s, s_functions) => {
-      if (list_includes(u, s)) {
+      if (list_includes(item.ast_list, s)) {
         list_add_if_exists_not(s_functions, name);
       } else {
         list_remove_if_exists(s_functions, name);
