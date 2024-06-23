275bb25853101c69a2f3d97b96b8f713c3dc82b7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 12:45:12 -0400

    2024-06-23T16:45:11.190Z function_auto_after app_gs_map_cell_click

--- public/src/app_gs_map_cell_click.mjs
+++ public/src/app_gs_map_cell_click.mjs
@@ -1,3 +1,4 @@
+import { app_gs_map_html_at_find } from "./app_gs_map_html_at_find.mjs";
 import { html_button_width_full_text_click_back } from "./html_button_width_full_text_click_back.mjs";
 import { app_gs_div_rounded_transparent } from "./app_gs_div_rounded_transparent.mjs";
 import { app_gs_overlay_speech } from "./app_gs_overlay_speech.mjs";
@@ -5,9 +6,6 @@ import { app_gs_conversation } from "./app_gs_conversation.mjs";
 import { html_remove } from "./html_remove.mjs";
 import { app_gs_sleep_time_face } from "./app_gs_sleep_time_face.mjs";
 import { sleep } from "./sleep.mjs";
-import { html_data_get } from "./html_data_get.mjs";
-import { list_find } from "./list_find.mjs";
-import { app_gs_map_html_at } from "./app_gs_map_html_at.mjs";
 import { app_gs_walk_direction } from "./app_gs_walk_direction.mjs";
 import { list_find_property_or } from "./list_find_property_or.mjs";
 import { null_is } from "./null_is.mjs";
@@ -40,11 +38,7 @@ export async function app_gs_map_cell_click(map, map_c, player_c, tile) {
       let { path } = app_gs_map_path(map, neighbors);
       await app_gs_walk_path(map, map_c, player_c, path);
       let data_type = "npc";
-      let tile_cs = app_gs_map_html_at(map, npc);
-      let npc_c = list_find(
-        tile_cs,
-        (tile_c) => html_data_get(tile_c, "type") === data_type,
-      );
+      let npc_c = app_gs_map_html_at_find(map, npc, data_type);
       let sleep_time = app_gs_sleep_time_face();
       await sleep(sleep_time);
       app_gs_walk_direction(player_c, player, npc);
---
+++ public/src/app_gs_map_html_at_find.mjs
@@ -0,0 +1,11 @@
+import { html_data_get } from "./html_data_get.mjs";
+import { list_find } from "./list_find.mjs";
+import { app_gs_map_html_at } from "./app_gs_map_html_at.mjs";
+export function app_gs_map_html_at_find(map, npc, data_type) {
+  let tile_cs = app_gs_map_html_at(map, npc);
+  let npc_c = list_find(
+    tile_cs,
+    (tile_c) => html_data_get(tile_c, "type") === data_type,
+  );
+  return npc_c;
+}
