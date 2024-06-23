60a79608ce18ab5080ce4206f673e70eded238da
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 15:56:33 -0400

    2024-06-23T19:56:32.973Z   t

--- public/src/app_gs_conversation_end.mjs
+++ public/src/app_gs_conversation_end.mjs
@@ -1,7 +1,7 @@
 import { app_gs_npc_icon_update } from "./app_gs_npc_icon_update.mjs";
 import { html_remove } from "./html_remove.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
-export function app_gs_conversation_end(div_player, menu_overlay) {
+export function app_gs_conversation_end(div_player, menu_overlay, npc) {
   html_button_width_full_text_click(
     div_player,
     "👋 Politely end the conversation after 🙏 praying",
--- public/src/app_gs_conversation_npc_opening.mjs
+++ public/src/app_gs_conversation_npc_opening.mjs
@@ -1,3 +1,4 @@
+import { error } from "./error.mjs";
 import { app_gs_conversation_end } from "./app_gs_conversation_end.mjs";
 import { app_gs_conversation_gospel } from "./app_gs_conversation_gospel.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
@@ -18,5 +19,5 @@ export function app_gs_conversation_npc_opening(menu_overlay, npc, text, map) {
       app_gs_conversation_gospel(menu_overlay, npc, map);
     },
   );
-  app_gs_conversation_end(div_player, menu_overlay);
+  app_gs_conversation_end(div_player, menu_overlay, error());
 }
