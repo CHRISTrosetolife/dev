a1c400d882afbd371933498cf65653fa1c5381de
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 20:23:12 -0400

    2024-06-23T00:23:12.234Z function_auto_after bible_books_prefix_to_name_generate

--- public/src/bible_books_prefix_to_name_generate.mjs
+++ public/src/bible_books_prefix_to_name_generate.mjs
@@ -1,10 +1,10 @@
+import { list_to_lookup_value } from "./list_to_lookup_value.mjs";
 import { bible_books_html_element_to_prefix } from "./bible_books_html_element_to_prefix.mjs";
 import { bible_books_html_elements } from "./bible_books_html_elements.mjs";
 import { html_parse_text } from "./html_parse_text.mjs";
-import { list_to_lookup } from "./list_to_lookup.mjs";
 export async function bible_books_prefix_to_name_generate() {
   let book_elements = await bible_books_html_elements("engbsb");
-  return list_to_lookup(book_elements, (b) => ({
+  return list_to_lookup_value(book_elements, (b) => ({
     prefix: bible_books_html_element_to_prefix(b),
     name: html_parse_text(b),
   }));
