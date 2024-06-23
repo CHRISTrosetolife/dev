6ce9374a7edc59d5b2099380891a6dc682f53b32
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 16:23:40 -0400

    2024-06-23T20:23:40.140Z function_auto_after data_literal_strings

--- public/src/data_literal_strings.mjs
+++ public/src/data_literal_strings.mjs
@@ -2,6 +2,6 @@ import { data_path } from "./data_path.mjs";
 import { file_read_json } from "./file_read_json.mjs";
 export async function data_literal_strings() {
   let data = await file_read_json(data_path());
-  let { identifiers } = data;
-  return identifiers;
+  let { literal_strings } = data;
+  return literal_strings;
 }
