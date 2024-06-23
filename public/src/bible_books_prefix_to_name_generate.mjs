ea90d93f334e87ac2def9d0fb22917401890873a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 20:18:44 -0400

    2024-06-23T00:18:44.088Z function_auto_after bible_books_prefix_to_name_generate

--- public/src/bible_books_prefix_to_name_generate.mjs
+++ public/src/bible_books_prefix_to_name_generate.mjs
@@ -1,5 +1,6 @@
+import { list_map } from "./list_map.mjs";
 import { bible_books_html_elements } from "./bible_books_html_elements.mjs";
 export async function bible_books_prefix_to_name_generate() {
   let book_elements = await bible_books_html_elements("engbsb");
-  return;
+  return list_map(book_elements, (b) => {});
 }
