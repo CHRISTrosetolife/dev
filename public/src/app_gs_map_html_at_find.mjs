a0836dad6980306f10e57aa437b206493aceb0c7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 16:31:37 -0400

    2024-06-23T20:31:36.586Z function_auto_after app_gs_map_html_at_find

--- public/src/app_gs_map_html_at_find.mjs
+++ public/src/app_gs_map_html_at_find.mjs
@@ -1,4 +1,6 @@
 import { app_gs_map_html_at_find_multiple } from "./app_gs_map_html_at_find_multiple.mjs";
+import { list_single } from "./list_single.mjs";
 export function app_gs_map_html_at_find(map, npc, data_type) {
   let results = app_gs_map_html_at_find_multiple(map, npc, [data_type]);
+  return list_single(results);
 }
