213b893219be74e4f279eda89a0a8387fc39ed97
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 09:37:39 -0400

    2024-06-23T13:37:38.558Z function_auto_after app_gs_map_cell

--- public/src/app_gs_map_cell.mjs
+++ public/src/app_gs_map_cell.mjs
@@ -18,13 +18,14 @@ export function app_gs_map_cell(map, map_c, player_c, tile) {
         let { id: o_id } = o;
         html_data_set(clicker, "overlay", o_id);
         let overlay_c = game_img(map_c, game_img_base(o_id), tile, "overlay");
+        la(overlay_c);
       } else if (o.type === "npc") {
         let overlay_c = app_gs_player_c_generic(map_c, o.character, o);
         html_data_set(overlay_c, "type", "npc");
+        la(overlay_c);
       } else {
         error();
       }
-      la(overlay_c);
     }),
   );
   let { id } = tile;
