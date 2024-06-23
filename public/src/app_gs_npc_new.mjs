51929e0f0801113e43870ee719140e1295452357
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 20:42:14 -0400

    2024-06-23T00:42:12.386Z function_auto_after app_gs_npc_new

--- public/src/app_gs_npc_new.mjs
+++ public/src/app_gs_npc_new.mjs
@@ -8,7 +8,9 @@ import { app_gs_tile_overlay_add } from "./app_gs_tile_overlay_add.mjs";
 import { list_pop } from "./list_pop.mjs";
 import { integer_random } from "./integer_random.mjs";
 export function app_gs_npc_new(map, tile_choices, objection_ids) {
-  list_map(range(integer_random(1, 3)), (i) => list_pop(objection_ids));
+  let objections = list_map(range(integer_random(1, 3)), (i) =>
+    list_pop(objection_ids),
+  );
   let tile = list_pop(tile_choices);
   let npc = app_gs_tile_overlay_add(tile, "npc");
   npc.character = list_random_item(game_img_list_male());
