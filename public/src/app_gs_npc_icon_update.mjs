7fed57600c5222fef4da7510da85be1843b0504d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 16:38:56 -0400

    2024-06-23T20:38:55.587Z function_auto_after app_gs_npc_icon_update

--- public/src/app_gs_npc_icon_update.mjs
+++ public/src/app_gs_npc_icon_update.mjs
@@ -3,10 +3,10 @@ import { app_gs_npc_icon_wait } from "./app_gs_npc_icon_wait.mjs";
 import { app_gs_npc_conversation_wait } from "./app_gs_npc_conversation_wait.mjs";
 import { app_gs_npc_icon_christian } from "./app_gs_npc_icon_christian.mjs";
 import { app_gs_npc_icon_unmet } from "./app_gs_npc_icon_unmet.mjs";
-export function app_gs_npc_icon_update(map, o, c_blur, c) {
-  if (o.conversation_count === 0) {
+export function app_gs_npc_icon_update(map, npc, c_blur, c) {
+  if (npc.conversation_count === 0) {
     app_gs_npc_icon_unmet(c_blur, c);
-  } else if (o.christian) {
+  } else if (npc.christian) {
     app_gs_npc_icon_christian(c_blur, c);
   } else if (app_gs_npc_conversation_wait(map, npc)) {
     app_gs_npc_icon_wait(c_blur, c);
