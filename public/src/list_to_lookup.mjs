61c29edd1f96aa7a445ddd992dfcc4628fb79a2d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 20:22:17 -0400

    2024-06-23T00:22:17.503Z function_auto_after list_to_lookup

--- public/src/list_to_lookup.mjs
+++ public/src/list_to_lookup.mjs
@@ -1,9 +1,9 @@
 import { object_property_set } from "./object_property_set.mjs";
 import { each } from "./each.mjs";
-export function list_to_lookup(ceb_scores, mapper) {
+export function list_to_lookup(ceb_scores, mapper_key) {
   let lookup = {};
   each(ceb_scores, (w) => {
-    object_property_set(lookup, mapper(w), w);
+    object_property_set(lookup, mapper_key(w), w);
   });
   return lookup;
 }
