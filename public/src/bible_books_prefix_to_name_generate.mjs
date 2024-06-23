c4776fe8201701cc7f4c30f88e1c01c56140c272
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 20:29:18 -0400

    2024-06-23T00:29:18.156Z function_auto_after bible_books_prefix_to_name_generate

--- public/src/bible_books_prefix_to_name_generate.mjs
+++ public/src/bible_books_prefix_to_name_generate.mjs
@@ -7,6 +7,7 @@ import { list_to_lookup_value } from "./list_to_lookup_value.mjs";
 import { bible_books_html_element_to_prefix } from "./bible_books_html_element_to_prefix.mjs";
 import { bible_books_html_elements } from "./bible_books_html_elements.mjs";
 import { html_parse_text } from "./html_parse_text.mjs";
+import { each_object } from "./each_object.mjs";
 export async function bible_books_prefix_to_name_generate() {
   let book_elements = await bible_books_html_elements("engbsb");
   let lookup = list_to_lookup_value(
@@ -17,6 +18,7 @@ export async function bible_books_prefix_to_name_generate() {
   let r = {
     type: "ObjectExpression",
   };
+  each_object(lookup, (key, value) => {});
   await function_new_generic(
     string_suffix_without(bible_books_prefix_to_name_generate, "_generate"),
     "",
