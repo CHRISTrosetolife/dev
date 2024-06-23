3f0161441cfa566c72addf8dc6afdc61d4231b1d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 13:44:23 -0400

    2024-06-23T17:44:23.456Z function_auto_after app_gs_npc_icon_unmet

--- public/src/app_gs_npc_icon_unmet.mjs
+++ public/src/app_gs_npc_icon_unmet.mjs
@@ -5,10 +5,10 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function app_gs_npc_icon_unmet(c_blur, c) {
   assert_arguments_length(arguments, 2);
   let img_url = "icons/question.svg";
+  let filter =
+    "invert(11%) sepia(100%) saturate(5663%) hue-rotate(239deg) brightness(105%) contrast(113%)";
   each([c_blur, c], (item) => {
     html_img_src(item, img_url);
   });
-  let filter =
-    "invert(11%) sepia(100%) saturate(5663%) hue-rotate(239deg) brightness(105%) contrast(113%)";
   html_style_filter(c, filter);
 }
