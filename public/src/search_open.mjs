f079fbdad20dd3a5317a0741964d2db8f17df1bb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 14:22:01 -0400

    2024-06-23T18:22:00.370Z function_auto_after search_open

--- public/src/search_open.mjs
+++ public/src/search_open.mjs
@@ -1,4 +1,4 @@
 import { function_search } from "./function_search.mjs";
 export async function search_open(query) {
-  await function_search(query);
+  let results = await function_search(query);
 }
