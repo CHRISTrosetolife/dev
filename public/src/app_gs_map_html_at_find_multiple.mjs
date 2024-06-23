6cbd95a4792ea0494b1705313c4fb1825a800f77
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 16:31:13 -0400

    2024-06-23T20:31:12.750Z function_auto_after app_gs_map_html_at_find_multiple

--- public/src/app_gs_map_html_at_find_multiple.mjs
+++ public/src/app_gs_map_html_at_find_multiple.mjs
@@ -1,10 +1,10 @@
+import { list_filter } from "./list_filter.mjs";
 import { html_data_get } from "./html_data_get.mjs";
-import { list_find } from "./list_find.mjs";
 import { app_gs_map_html_at } from "./app_gs_map_html_at.mjs";
 import { list_includes } from "./list_includes.mjs";
 export function app_gs_map_html_at_find_multiple(map, npc, data_types) {
   let tile_cs = app_gs_map_html_at(map, npc);
-  let results = list_find(tile_cs, (tile_c) =>
+  let results = list_filter(tile_cs, (tile_c) =>
     list_includes(data_types, html_data_get(tile_c, "type")),
   );
   return results;
