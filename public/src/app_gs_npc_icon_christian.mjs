3741a8dfc4664d56141da854621704574128aa5a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 13:45:10 -0400

    2024-06-23T17:45:09.522Z function_auto_after app_gs_npc_icon_christian

--- public/src/app_gs_npc_icon_christian.mjs
+++ public/src/app_gs_npc_icon_christian.mjs
@@ -1,9 +1,9 @@
-import { html_style } from "./html_style.mjs";
-import { html_img_src } from "./html_img_src.mjs";
+import { app_gs_npc_icon_generic } from "./app_gs_npc_icon_generic.mjs";
 export function app_gs_npc_icon_christian(c) {
-  html_img_src(c, "icons/cross.svg");
-  html_style(c, {
-    filter:
-      "invert(80%) sepia(52%) saturate(474%) hue-rotate(48deg) brightness(103%) contrast(102%)",
-  });
+  app_gs_npc_icon_generic(
+    c_blur,
+    c,
+    "icons/cross.svg",
+    "invert(80%) sepia(52%) saturate(474%) hue-rotate(48deg) brightness(103%) contrast(102%)",
+  );
 }
