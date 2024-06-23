a76432f4ac7cb07641a78e8fdba2c9dfecb3e73c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-22 20:41:59 -0400

    2024-06-23T00:41:56.326Z function_auto_after app_gs_npc_new

--- public/src/app_gs_npc_new.mjs
+++ public/src/app_gs_npc_new.mjs
@@ -1,11 +1,14 @@
+import { range } from "./range.mjs";
+import { list_map } from "./list_map.mjs";
 import { list_remove } from "./list_remove.mjs";
 import { bible_names_men } from "./bible_names_men.mjs";
 import { game_img_list_male } from "./game_img_list_male.mjs";
 import { list_random_item } from "./list_random_item.mjs";
 import { app_gs_tile_overlay_add } from "./app_gs_tile_overlay_add.mjs";
 import { list_pop } from "./list_pop.mjs";
+import { integer_random } from "./integer_random.mjs";
 export function app_gs_npc_new(map, tile_choices, objection_ids) {
-  let objection_id = list_pop(objection_ids);
+  list_map(range(integer_random(1, 3)), (i) => list_pop(objection_ids));
   let tile = list_pop(tile_choices);
   let npc = app_gs_tile_overlay_add(tile, "npc");
   npc.character = list_random_item(game_img_list_male());
