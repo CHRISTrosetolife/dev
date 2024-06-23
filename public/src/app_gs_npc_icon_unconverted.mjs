d072d4d6f6ae365d6242b580917016706e0256bd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-23 17:01:14 -0400

    2024-06-23T21:01:11.856Z function_auto_after app_gs_npc_icon_unconverted

--- public/src/app_gs_npc_icon_unconverted.mjs
+++ public/src/app_gs_npc_icon_unconverted.mjs
@@ -1,5 +1,5 @@
 import { app_gs_npc_icon_green } from "./app_gs_npc_icon_green.mjs";
 export function app_gs_npc_icon_unconverted(c_blur, c) {
-  let icon_name = "cross";
+  let icon_name = "exclaim";
   app_gs_npc_icon_green(c_blur, c, icon_name);
 }
