6582261d6e47d1459d266b428b5fcc21fd8604f2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 12:46:51 -0400

    2024-06-24T16:46:51.423Z function_auto_after app_gs_map_html

--- public/src/app_gs_map_html.mjs
+++ public/src/app_gs_map_html.mjs
@@ -4,11 +4,11 @@ import { game_tile_units_css } from "./game_tile_units_css.mjs";
 import { html_style_width } from "./html_style_width.mjs";
 import { html_div } from "./html_div.mjs";
 export function app_gs_map_html(context) {
-  let map_c = html_div(root);
-  html_style_width(map_c, game_tile_units_css(map.x_size));
-  html_style_height(map_c, game_tile_units_css(map.y_size));
+  let map_c = html_div(context.root);
+  html_style_width(context.map_c, game_tile_units_css(context.map.x_size));
+  html_style_height(context.map_c, game_tile_units_css(context.map.y_size));
   if (0) {
-    html_style(map_c, {
+    html_style(context.map_c, {
       position: "relative",
       display: "inline",
     });
