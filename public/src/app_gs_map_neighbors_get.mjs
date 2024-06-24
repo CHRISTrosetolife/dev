45c24814530daca7c7a25b639beb378f25380140
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 14:04:53 -0400

    2024-06-24T18:04:53.332Z function_auto_after app_gs_map_neighbors_get

--- public/src/app_gs_map_neighbors_get.mjs
+++ public/src/app_gs_map_neighbors_get.mjs
@@ -11,11 +11,11 @@ export function app_gs_map_neighbors_get(context, x_y) {
     list_add(neighbors, context.map.tiles[y][x - 1]);
   }
   let y1 = y + 1;
-  if (y1 < map.y_size) {
+  if (y1 < context.map.y_size) {
     list_add(neighbors, context.map.tiles[y1][x]);
   }
   let x1 = x + 1;
-  if (x1 < map.x_size) {
+  if (x1 < context.map.x_size) {
     list_add(neighbors, context.map.tiles[y][x1]);
   }
   neighbors = list_filter(neighbors, (n) => !app_gs_overlays_any_wall(n));
