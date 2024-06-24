59e0011feb0fdcb8be53505040877608945a0602
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 12:41:11 -0400

    2024-06-24T16:41:11.150Z function_auto_after app_gs_player_c

--- public/src/app_gs_player_c.mjs
+++ public/src/app_gs_player_c.mjs
@@ -1,4 +1,9 @@
 import { app_gs_character_c } from "./app_gs_character_c.mjs";
-export function app_gs_player_c(map_c, map) {
-  return app_gs_character_c(map_c, map.player.character, map.player, "player");
+export function app_gs_player_c(map_c, game) {
+  return app_gs_character_c(
+    map_c,
+    game.player.character,
+    game.player,
+    "player",
+  );
 }
