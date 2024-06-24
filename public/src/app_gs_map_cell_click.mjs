2e75165aeaa2bfa64a271577890686f29204deb1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 12:52:56 -0400

    2024-06-24T16:52:56.054Z function_auto_after app_gs_map_cell_click

--- public/src/app_gs_map_cell_click.mjs
+++ public/src/app_gs_map_cell_click.mjs
@@ -18,7 +18,7 @@ import { app_gs_menu_overlay } from "./app_gs_menu_overlay.mjs";
 import { app_gs_map_neighbors_get } from "./app_gs_map_neighbors_get.mjs";
 import { app_gs_overlays_any_wall } from "./app_gs_overlays_any_wall.mjs";
 import { html_p_text } from "./html_p_text.mjs";
-export async function app_gs_map_cell_click(game, map_c, player_c, tile) {
+export async function app_gs_map_cell_click(context, tile) {
   let map = app_gs_map(game);
   let npc = list_find_property_or(tile.overlays, "type", "npc", null);
   if (null_is(npc)) {
