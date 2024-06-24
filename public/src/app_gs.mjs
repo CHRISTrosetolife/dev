b0ed46038ad09e70f981fd90a2d51721d6a9ebb9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 07:51:41 -0400

    2024-06-24T11:51:40.676Z function_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -14,6 +14,7 @@ export async function app_gs() {
   history.scrollRestoration = "manual";
   let root = app_gs_style_default_initialize();
   let game = storage_local_initialize_lambda(app_gs.name, "game", () => {
+    game = {};
     let map = app_gs_map_new();
     let game = {
       maps: [map],
