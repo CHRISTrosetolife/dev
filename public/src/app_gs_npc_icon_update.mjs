2c33c6689423ff8270a02a0dab97ccfe72678435
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 12:51:54 -0400

    2024-06-24T16:51:54.128Z function_auto_after app_gs_npc_icon_update

--- public/src/app_gs_npc_icon_update.mjs
+++ public/src/app_gs_npc_icon_update.mjs
@@ -1,11 +1,9 @@
-import { app_gs_map } from "./app_gs_map.mjs";
 import { app_gs_npc_icon_unconverted } from "./app_gs_npc_icon_unconverted.mjs";
 import { app_gs_npc_icon_unknown } from "./app_gs_npc_icon_unknown.mjs";
 import { app_gs_npc_icon_wait } from "./app_gs_npc_icon_wait.mjs";
 import { app_gs_npc_conversation_wait } from "./app_gs_npc_conversation_wait.mjs";
 import { app_gs_npc_icon_christian } from "./app_gs_npc_icon_christian.mjs";
-export function app_gs_npc_icon_update(game, npc, c_blur, c) {
-  let map = app_gs_map(game);
+export function app_gs_npc_icon_update(context, npc, c_blur, c) {
   if (app_gs_npc_conversation_wait(map, npc)) {
     app_gs_npc_icon_wait(c_blur, c);
   } else if (npc.christian) {
