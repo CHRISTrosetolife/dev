7639f673051967fe9584e5796c78bcff5963bd53
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 14:39:15 -0400

    2024-06-24T18:39:15.034Z function_auto_after app_gs_load

--- public/src/app_gs_load.mjs
+++ public/src/app_gs_load.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
 import { html_scroll_center_smooth } from "./html_scroll_center_smooth.mjs";
 import { app_gs_map_render } from "./app_gs_map_render.mjs";
@@ -9,6 +10,7 @@ import { object_merge } from "./object_merge.mjs";
 import { app_gs_player_new } from "./app_gs_player_new.mjs";
 import { app_gs } from "./app_gs.mjs";
 import { storage_local_initialize_lambda } from "./storage_local_initialize_lambda.mjs";
+import { html_remove } from "./html_remove.mjs";
 export async function app_gs_load(context) {
   let game = storage_local_initialize_lambda(app_gs, "game", () => {
     let game = {};
@@ -28,7 +30,9 @@ export async function app_gs_load(context) {
   });
   context.map = context.game.maps[context.game.player.map];
   let keys = ["map_c", "player_c"];
-  each(list, (item) => {});
+  each(keys, (item) => {
+    html_remove(object_property_get(object, property_name));
+  });
   context.map_c = app_gs_map_html(context);
   html_scrollable_hide(context);
   context.player_c = app_gs_player_c(context);
