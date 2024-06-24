e28f09cb7c536fd908f817bfa83ba5cac0a4e5cd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 14:36:10 -0400

    2024-06-24T18:36:09.675Z function_auto_after app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,17 +1,10 @@
+import { app_gs_load } from "./app_gs_load.mjs";
 import { storage_local_remove } from "./storage_local_remove.mjs";
-import { app_gs_player_new } from "./app_gs_player_new.mjs";
-import { storage_local_initialize_lambda } from "./storage_local_initialize_lambda.mjs";
-import { app_gs_player_c } from "./app_gs_player_c.mjs";
-import { html_scrollable_hide } from "./html_scrollable_hide.mjs";
 import { object_merge } from "./object_merge.mjs";
 import { html_style_button_default_value } from "./html_style_button_default_value.mjs";
 import { html_hostname } from "./html_hostname.mjs";
 import { html_scale_none } from "./html_scale_none.mjs";
-import { html_scroll_center_smooth } from "./html_scroll_center_smooth.mjs";
-import { app_gs_map_render } from "./app_gs_map_render.mjs";
 import { app_gs_style_default_initialize } from "./app_gs_style_default_initialize.mjs";
-import { app_gs_map_html } from "./app_gs_map_html.mjs";
-import { app_gs_map_new } from "./app_gs_map_new.mjs";
 export async function app_gs() {
   history.scrollRestoration = "manual";
   let context = {};
@@ -30,27 +23,5 @@ export async function app_gs() {
   if (0) {
     storage_local_remove(app_gs, "game");
   }
-  let game = storage_local_initialize_lambda(app_gs, "game", () => {
-    let game = {};
-    let player = app_gs_player_new();
-    object_merge(game, {
-      player,
-    });
-    let map = app_gs_map_new(game);
-    object_merge(player, map.spawn);
-    object_merge(game, {
-      maps: [map],
-    });
-    return game;
-  });
-  object_merge(context, {
-    game,
-  });
-  context.map = context.game.maps[context.game.player.map];
-  context.map_c = app_gs_map_html(context);
-  html_scrollable_hide(context);
-  context.player_c = app_gs_player_c(context);
-  context.html = [];
-  app_gs_map_render(context, [context.game.player]);
-  await html_scroll_center_smooth(context.player_c);
+  await app_gs_load(context);
 }
---
+++ public/src/app_gs_load.mjs
@@ -0,0 +1,35 @@
+import { html_scroll_center_smooth } from "./html_scroll_center_smooth.mjs";
+import { app_gs_map_render } from "./app_gs_map_render.mjs";
+import { app_gs_player_c } from "./app_gs_player_c.mjs";
+import { html_scrollable_hide } from "./html_scrollable_hide.mjs";
+import { app_gs_map_html } from "./app_gs_map_html.mjs";
+import { app_gs_map_new } from "./app_gs_map_new.mjs";
+import { object_merge } from "./object_merge.mjs";
+import { app_gs_player_new } from "./app_gs_player_new.mjs";
+import { app_gs } from "./app_gs.mjs";
+import { storage_local_initialize_lambda } from "./storage_local_initialize_lambda.mjs";
+export async function app_gs_load(context) {
+  let game = storage_local_initialize_lambda(app_gs, "game", () => {
+    let game = {};
+    let player = app_gs_player_new();
+    object_merge(game, {
+      player,
+    });
+    let map = app_gs_map_new(game);
+    object_merge(player, map.spawn);
+    object_merge(game, {
+      maps: [map],
+    });
+    return game;
+  });
+  object_merge(context, {
+    game,
+  });
+  context.map = context.game.maps[context.game.player.map];
+  context.map_c = app_gs_map_html(context);
+  html_scrollable_hide(context);
+  context.player_c = app_gs_player_c(context);
+  context.html = [];
+  app_gs_map_render(context, [context.game.player]);
+  await html_scroll_center_smooth(context.player_c);
+}
