51f7bf604739569edf1996ad8a55b093b7b4ecdd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 12:55:46 -0400

    2024-06-24T16:55:45.410Z function_auto_after app_gs_menu_main

--- public/src/app_gs_menu_main.mjs
+++ public/src/app_gs_menu_main.mjs
@@ -5,7 +5,7 @@ import { app_gs_menu_system_logs } from "./app_gs_menu_system_logs.mjs";
 import { html_remove } from "./html_remove.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { html_clear } from "./html_clear.mjs";
-export function app_gs_menu_main(menu_overlay, map_c, map) {
+export function app_gs_menu_main(context, menu_overlay) {
   html_clear(menu_overlay);
   html_button_width_full_text_click_back(menu_overlay, () => {
     html_remove(menu_overlay);
