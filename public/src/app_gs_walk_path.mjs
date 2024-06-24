c4f62c1b4c477458f9a0134740b7c30686151ea2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 12:59:39 -0400

    2024-06-24T16:59:39.394Z function_auto_after app_gs_walk_path

--- public/src/app_gs_walk_path.mjs
+++ public/src/app_gs_walk_path.mjs
@@ -6,7 +6,7 @@ import { app_gs_map_render } from "./app_gs_map_render.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function app_gs_walk_path(context, path) {
   assert_arguments_length(arguments, 2);
-  app_gs_map_render(map, map_c, path, player_c);
+  app_gs_map_render(context, path);
   await each_async(list_skip(path, 1), async (tile) => {
     await app_gs_walk(player_c, map.player, tile);
   });
