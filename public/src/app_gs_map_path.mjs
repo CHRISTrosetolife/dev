cd9a878fd90cbcca370f491f3987c08995fb1480
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 12:53:35 -0400

    2024-06-24T16:53:35.112Z function_auto_after app_gs_map_path

--- public/src/app_gs_map_path.mjs
+++ public/src/app_gs_map_path.mjs
@@ -2,7 +2,7 @@ import { app_gs_map_at } from "./app_gs_map_at.mjs";
 import { app_gs_map_neighbors_get } from "./app_gs_map_neighbors_get.mjs";
 import { app_gs_map_tile_id } from "./app_gs_map_tile_id.mjs";
 import { graph_path_shortest_neighbors } from "./graph_path_shortest_neighbors.mjs";
-export function app_gs_map_path(map, to_any) {
+export function app_gs_map_path(context, to_any) {
   let from = app_gs_map_at(map, map.player);
   return graph_path_shortest_neighbors(
     app_gs_map_tile_id,
