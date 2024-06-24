74da4521edff7ba73c487ecfe051ec98990e5cbc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 14:23:23 -0400

    2024-06-24T18:23:22.523Z function_auto_after app_gs_npc_new

--- public/src/app_gs_npc_new.mjs
+++ public/src/app_gs_npc_new.mjs
@@ -9,6 +9,7 @@ export function app_gs_npc_new(game, tile_choices, objections) {
   let tile = list_pop(tile_choices);
   let npc = app_gs_tile_overlay_add(tile, "npc");
   npc.character = list_random_item(game_img_list_male());
+  npc.direction = "down";
   let choices = bible_names_men();
   list_remove(choices, game.player.name);
   npc.name = list_random_item(choices);
