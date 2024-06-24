2398e9106714a691ab633e4d5c224d25fafe1ee1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 14:01:47 -0400

    2024-06-24T18:01:46.624Z function_auto_after app_gs_menu_overlay

--- public/src/app_gs_menu_overlay.mjs
+++ public/src/app_gs_menu_overlay.mjs
@@ -3,8 +3,8 @@ import { app_gs_div_rounded_transparent } from "./app_gs_div_rounded_transparent
 import { game_img_style_generic } from "./game_img_style_generic.mjs";
 import { subtract } from "./subtract.mjs";
 import { app_gs_screen_size } from "./app_gs_screen_size.mjs";
-export function app_gs_menu_overlay(context) {
-  let menu = app_gs_div_rounded_transparent(context.map_c, "black", 25);
+export function app_gs_menu_overlay(parent) {
+  let menu = app_gs_div_rounded_transparent(parent, "black", 25);
   let { x_count, y_count } = app_gs_screen_size();
   let position_y = subtract(context.game.player.y, (y_count - 1) / 2);
   let position_x = subtract(context.game.player.x, (x_count - 1) / 2);
