13dceea5a9195f87185c5b703aa1fe4d5b0740f9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 07:51:16 -0400

    2024-06-24T11:51:16.282Z function_auto_after app_gs_player_new

--- public/src/app_gs_player_new.mjs
+++ public/src/app_gs_player_new.mjs
@@ -1,14 +1,14 @@
 import { bible_names_men } from "./bible_names_men.mjs";
 import { game_img_list_male } from "./game_img_list_male.mjs";
 import { list_random_item } from "./list_random_item.mjs";
-export function app_gs_player_new(map) {
-  map.player = {};
-  map.player.walk_offset = 0;
-  map.player.walk_previous = 1;
-  map.player.character = list_random_item(game_img_list_male());
-  map.player.name = list_random_item(bible_names_men());
-  map.player.pray = {
+export function app_gs_player_new(game) {
+  game.player = {};
+  game.player.walk_offset = 0;
+  game.player.walk_previous = 1;
+  game.player.character = list_random_item(game_img_list_male());
+  game.player.name = list_random_item(bible_names_men());
+  game.player.pray = {
     conversation: false,
   };
-  map.player.conversation_count = 0;
+  game.player.conversation_count = 0;
 }
