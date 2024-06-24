68864e057c580af0535d382eb5991f1a189f55a1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 14:12:48 -0400

    2024-06-24T18:12:48.154Z function_auto_after app_gs_walk

--- public/src/app_gs_walk.mjs
+++ public/src/app_gs_walk.mjs
@@ -18,32 +18,32 @@ export async function app_gs_walk(context, destination) {
   assert_arguments_length(arguments, 2);
   let direction = app_gs_walk_direction(
     context.player_c,
-    context.player,
+    context.game.player,
     destination,
   );
   let steps_count =
-    abs(context.player.y - destination.y) +
-    abs(context.player.x - destination.x);
+    abs(context.game.player.y - destination.y) +
+    abs(context.game.player.x - destination.x);
   await each_range_async(steps_count, async () => {
     let animate_count = 2;
     await each_range_async(animate_count, async (ac) => {
       let step_count = add_1(ac);
-      if (context.player.walk_offset === 0) {
-        context.player.walk_offset = -context.player.walk_previous;
-        context.player.walk_previous = context.player.walk_offset;
+      if (context.game.player.walk_offset === 0) {
+        context.game.player.walk_offset = -context.game.player.walk_previous;
+        context.game.player.walk_previous = context.game.player.walk_offset;
       } else {
-        context.player.walk_offset = 0;
+        context.game.player.walk_offset = 0;
       }
       let di = game_character_direction_index(
         direction,
-        context.player.walk_offset,
+        context.game.player.walk_offset,
       );
       html_img_src(player_c, game_img_character(context.player.character, di));
       let delta = game_direction_to_delta(direction);
       game_img_position(
-        context.player_c,
-        context.player.y + (delta.y / animate_count) * step_count,
-        context.player.x + (delta.x / animate_count) * step_count,
+        context.game.player_c,
+        context.game.player.y + (delta.y / animate_count) * step_count,
+        context.game.player.x + (delta.x / animate_count) * step_count,
       );
       let sleep_time = app_gs_sleep_time_walk();
       await sleep(sleep_time);
@@ -51,7 +51,7 @@ export async function app_gs_walk(context, destination) {
     let delta = game_direction_to_delta(direction);
     each(list_xy(), (xy) => {
       let value =
-        object_property_get(context.player, xy) +
+        object_property_get(context.game.player, xy) +
         object_property_get(delta, xy);
       object_property_set(context.game.player, xy, value);
     });
