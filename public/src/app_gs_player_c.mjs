dff285027ab35c026650d4f13387b9a1b65f12a6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 12:47:54 -0400

    2024-06-24T16:47:54.397Z function_auto_after app_gs_player_c

--- public/src/app_gs_player_c.mjs
+++ public/src/app_gs_player_c.mjs
@@ -1,5 +1,7 @@
 import { app_gs_character_c } from "./app_gs_character_c.mjs";
-export function app_gs_player_c(map_c, game) {
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
+export function app_gs_player_c(context) {
+  assert_arguments_length(arguments, 1);
   return app_gs_character_c(
     map_c,
     game.player.character,
