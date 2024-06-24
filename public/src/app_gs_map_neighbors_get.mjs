a6d70141154fc61cbb7cf223dd0c7471a5d72374
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 13:44:50 -0400

    2024-06-24T17:44:49.971Z function_auto_after app_gs_map_neighbors_get

--- public/src/app_gs_map_neighbors_get.mjs
+++ public/src/app_gs_map_neighbors_get.mjs
@@ -1,22 +1,22 @@
 import { app_gs_overlays_any_wall } from "./app_gs_overlays_any_wall.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { list_add } from "./list_add.mjs";
-export function app_gs_map_neighbors_get(map, x_y) {
+export function app_gs_map_neighbors_get(context, x_y) {
   let { x, y } = x_y;
   let neighbors = [];
   if (y >= 1) {
-    list_add(neighbors, map.tiles[y - 1][x]);
+    list_add(neighbors, context.map.tiles[y - 1][x]);
   }
   if (x >= 1) {
-    list_add(neighbors, map.tiles[y][x - 1]);
+    list_add(neighbors, context.map.tiles[y][x - 1]);
   }
   let y1 = y + 1;
   if (y1 < map.y_size) {
-    list_add(neighbors, map.tiles[y1][x]);
+    list_add(neighbors, context.map.tiles[y1][x]);
   }
   let x1 = x + 1;
   if (x1 < map.x_size) {
-    list_add(neighbors, map.tiles[y][x1]);
+    list_add(neighbors, context.map.tiles[y][x1]);
   }
   neighbors = list_filter(neighbors, (n) => !app_gs_overlays_any_wall(n));
   return neighbors;
