8021faf8dc5e94239fa18b979f5c1d7321fb403b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 12:54:32 -0400

    2024-06-24T16:54:30.850Z function_auto_after app_gs_map_cell_click

--- public/src/app_gs_map_cell_click.mjs
+++ public/src/app_gs_map_cell_click.mjs
@@ -26,7 +26,7 @@ export async function app_gs_map_cell_click(context, tile) {
     if (!walls) {
       let { path } = app_gs_map_path(context, [tile]);
       if (app_gs_xy_equal(context.game.player, tile)) {
-        let menu_overlay = app_gs_menu_overlay(map_c, map);
+        let menu_overlay = app_gs_menu_overlay(context);
         app_gs_menu_main(menu_overlay, map_c, map);
       } else {
         await app_gs_walk_path(map, map_c, player_c, path);
