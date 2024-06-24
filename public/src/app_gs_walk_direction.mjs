41470197a878c6bf7529bbd699d42ccaba324e48
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 13:01:12 -0400

    2024-06-24T17:01:11.962Z function_auto_after app_gs_walk_direction

--- public/src/app_gs_walk_direction.mjs
+++ public/src/app_gs_walk_direction.mjs
@@ -4,12 +4,12 @@ import { html_img_src } from "./html_img_src.mjs";
 import { null_not_is } from "./null_not_is.mjs";
 import { assert } from "./assert.mjs";
 import { app_gs_direction } from "./app_gs_direction.mjs";
-export function app_gs_walk_direction(player_c, character, destination) {
+export function app_gs_walk_direction(character_c, character, destination) {
   let direction = app_gs_direction(character, destination);
   assert(null_not_is, [direction]);
   character.direction = direction;
   html_img_src(
-    player_c,
+    character_c,
     game_img_character(character.character, game_character_index(direction)),
   );
   return direction;
