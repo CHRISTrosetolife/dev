18877963d0aef7fd0c588376cfc59a76b4928c37
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 12:47:29 -0400

    2024-06-24T16:47:29.417Z function_auto_after html_scrollable_hide

--- public/src/html_scrollable_hide.mjs
+++ public/src/html_scrollable_hide.mjs
@@ -3,19 +3,19 @@ import { html_style } from "./html_style.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
 import { html_element } from "./html_element.mjs";
 export function html_scrollable_hide(root, map_c) {
-  html_style(root, {
+  html_style(context.root, {
     overflow: "hidden",
   });
   return;
-  let style = html_element(root, "style");
-  let id = html_id(root);
+  let style = html_element(context.root, "style");
+  let id = html_id(context.root);
   html_inner_set(
     style,
     `#${id}::-webkit-scrollbar {
   display: none;
 }`,
   );
-  html_style(map_c, {
+  html_style(context.map_c, {
     overflow: "scroll",
     "scrollbar-width": "none",
     "-ms-overflow-style": "none",
