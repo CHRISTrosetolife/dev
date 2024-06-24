22dc97d26bf8915499526eea51035c9228388ed8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 13:49:07 -0400

    2024-06-24T17:49:07.285Z function_auto_after app_gs_conversation_npc_opening

--- public/src/app_gs_conversation_npc_opening.mjs
+++ public/src/app_gs_conversation_npc_opening.mjs
@@ -4,7 +4,13 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { app_gs_conversation_player_prompt } from "./app_gs_conversation_player_prompt.mjs";
 import { app_gs_conversation_npc } from "./app_gs_conversation_npc.mjs";
-export function app_gs_conversation_npc_opening(menu_overlay, npc, text, map) {
+export function app_gs_conversation_npc_opening(
+  context,
+  menu_overlay,
+  npc,
+  text,
+  map,
+) {
   app_gs_conversation_npc(menu_overlay, npc, text);
   let div_player = app_gs_conversation_player_prompt(menu_overlay, "");
   html_button_width_full_text_click(
