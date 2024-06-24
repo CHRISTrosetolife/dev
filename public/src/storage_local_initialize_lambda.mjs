98aa7c5d2916ce07bc99a53dfcc37a6241f98c89
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 07:42:50 -0400

    2024-06-24T11:42:50.159Z function_auto_after storage_local_initialize_lambda

--- public/src/storage_local_initialize_lambda.mjs
+++ public/src/storage_local_initialize_lambda.mjs
@@ -1,7 +1,7 @@
 import { storage_local_set } from "./storage_local_set.mjs";
 import { storage_local_exists_not } from "./storage_local_exists_not.mjs";
-export function storage_local_initialize_lambda(fn_namespace, key, value) {
+export function storage_local_initialize_lambda(fn_namespace, key, value_get) {
   if (storage_local_exists_not(fn_namespace, key)) {
-    storage_local_set(fn_namespace, key, value);
+    storage_local_set(fn_namespace, key, value_get());
   }
 }
