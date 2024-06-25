fad8015283733444daf2881a6f9ec8921a61e2ef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-24 21:35:18 -0400

    2024-06-25T01:35:18.367Z function_auto_after sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -57,7 +57,7 @@ export async function sandbox() {
     let split = string_split_space(reference);
     assert(list_size_2, [split]);
     let book_name = list_first(split);
-    let book = list_find_property_or(bible, book_name, {
+    let book = list_find_property_or(bible, "book_name", book_name, {
       book_name,
     });
     let chapter_verse = list_second(split);
