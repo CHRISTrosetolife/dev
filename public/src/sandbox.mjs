9de71cc0ea5cf3266769589d5b696ba3d7825cf7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 12:02:03 -0400

    2024-06-25T16:02:02.728Z   t

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,3 @@
-import { list_take_reverse } from "./list_take_reverse.mjs";
 import { bible_interlinear_cache } from "./bible_interlinear_cache.mjs";
 import { string_split_empty } from "./string_split_empty.mjs";
 import { list_map } from "./list_map.mjs";
@@ -25,7 +24,7 @@ import { assert } from "./assert.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function sandbox() {
   let books = await bible_interlinear_cache();
-  $r, list_take_reverse(books, 27);
+  return;
   return;
   list_map(csv_lines, (line) => {
     let split = string_split_empty(input);
