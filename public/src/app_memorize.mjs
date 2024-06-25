35ba7e75bfa68b17dc51c1052d52658d9067df5b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 15:56:04 -0400

    2024-06-25T19:56:04.560Z function_auto_after app_memorize

--- public/src/app_memorize.mjs
+++ public/src/app_memorize.mjs
@@ -335,7 +335,7 @@ export async function app_memorize() {
       }
       for (let errored_key of errored_keys) {
         html_style(errored_key, html_style_button_default_value());
-        button_keyboard_stylize(errored_key, button_height);
+        button_keyboard_stylize(errored_key);
       }
     } else {
       context.mistakes = true;
