140b23c428b4bd9265f29e054eb3bff4afc0ad0c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 14:59:39 -0400

    2024-06-25T18:59:38.814Z function_auto_after app_gs_conversation

--- public/src/app_gs_conversation.mjs
+++ public/src/app_gs_conversation.mjs
@@ -8,7 +8,8 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { assert } from "./assert.mjs";
 export function app_gs_conversation(context, npc) {
   assert_arguments_length(arguments, 2);
-  assert(greater_than_equal, [context.game.player.energy.conversation, 1]);
+  context.game.player.energy = context.game.player;
+  assert(greater_than_equal, [conversation, 1]);
   let menu_overlay = app_gs_menu_overlay(context);
   context.game.player.conversation_count++;
   npc.conversation_count++;
