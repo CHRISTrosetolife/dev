985025a3882006873858bbadad63eebe882a5792
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 16:34:52 -0400

    2024-06-25T20:34:51.964Z function_auto_after app_memorize

--- public/src/app_memorize.mjs
+++ public/src/app_memorize.mjs
@@ -1,3 +1,5 @@
+import { html_attribute_set } from "./html_attribute_set.mjs";
+import { html_element } from "./html_element.mjs";
 import { app_memorize_frame } from "./app_memorize_frame.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { html_style } from "./html_style.mjs";
@@ -12,5 +14,18 @@ export async function app_memorize() {
   object_merge(context, {
     root,
   });
+  let c = html_element(parent, "script");
+  html_attribute_set(
+    c,
+    "src",
+    "https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js",
+  );
+  html_attribute_set(
+    c,
+    "integrity",
+    "sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==",
+  );
+  html_attribute_set(c, "crossorigin", "anonymous");
+  html_attribute_set(c, "referrerpolicy", "no-referrer");
   await app_memorize_frame(context);
 }
