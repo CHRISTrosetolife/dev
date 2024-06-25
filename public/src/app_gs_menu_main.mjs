ed41cc063e20d9c57bed19fc370ccadef8a86f97
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 15:13:26 -0400

    2024-06-25T19:13:26.183Z function_auto_after app_gs_menu_main

--- public/src/app_gs_menu_main.mjs
+++ public/src/app_gs_menu_main.mjs
@@ -17,6 +17,15 @@ export function app_gs_menu_main(context, menu_overlay) {
   html_button_width_full_text_click(menu_overlay, "🙏 pray", () => {
     app_gs_menu_pray(context, menu_overlay);
   });
+  html_button_width_full_text_click(
+    menu_overlay,
+    "📖 study bible",
+    async () => {
+      app_gs_game_delete();
+      await app_gs_load(context);
+      html_remove(menu_overlay);
+    },
+  );
   html_button_width_full_text_click(menu_overlay, "🌱 new game", async () => {
     app_gs_game_delete();
     await app_gs_load(context);
