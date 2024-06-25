1259049171291c0ff1195e70155558c5bd2d8a67
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 14:55:35 -0400

    2024-06-25T18:55:34.705Z   t

--- public/src/app_gs_npc_icon_update.mjs
+++ public/src/app_gs_npc_icon_update.mjs
@@ -1,10 +1,10 @@
+import { app_gs_npc_conversation_wait_is } from "./app_gs_npc_conversation_wait_is.mjs";
 import { app_gs_npc_icon_unconverted } from "./app_gs_npc_icon_unconverted.mjs";
 import { app_gs_npc_icon_unknown } from "./app_gs_npc_icon_unknown.mjs";
 import { app_gs_npc_icon_wait } from "./app_gs_npc_icon_wait.mjs";
-import { app_gs_npc_conversation_wait } from "./app_gs_npc_conversation_wait.mjs";
 import { app_gs_npc_icon_christian } from "./app_gs_npc_icon_christian.mjs";
 export function app_gs_npc_icon_update(context, npc, c_blur, c) {
-  if (app_gs_npc_conversation_wait(context, npc)) {
+  if (app_gs_npc_conversation_wait_is(context, npc)) {
     app_gs_npc_icon_wait(c_blur, c);
   } else if (npc.christian) {
     app_gs_npc_icon_christian(c_blur, c);
