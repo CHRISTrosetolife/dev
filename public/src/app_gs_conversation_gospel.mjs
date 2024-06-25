ed0c8d6aa60c644a8897ab3591d4e5be6165a9c2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 13:49:26 -0400

    2024-06-25T17:49:25.806Z function_auto_after app_gs_conversation_gospel

--- public/src/app_gs_conversation_gospel.mjs
+++ public/src/app_gs_conversation_gospel.mjs
@@ -9,7 +9,7 @@ export async function app_gs_conversation_gospel(context, menu_overlay, npc) {
     app_gs_conversation_reminder(context, menu_overlay, npc);
   } else {
     npc.gospel_share = true;
-    player.gospel_share_count++;
+    context.game.player.gospel_share_count++;
     if (list_empty_is(npc.objections)) {
       app_gs_conversation_convert(context, menu_overlay, npc);
     } else {
