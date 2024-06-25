7b948536592b819be98da3c986b886679db4d81d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 16:25:05 -0400

    2024-06-25T20:25:04.790Z function_auto_after app_memorize

--- public/src/app_memorize.mjs
+++ public/src/app_memorize.mjs
@@ -1,22 +1,6 @@
-import { app_memorize_refresh_memorize } from "./app_memorize_refresh_memorize.mjs";
-import { app_memorize_on_keydown } from "./app_memorize_on_keydown.mjs";
-import { app_memorize_group_current_set } from "./app_memorize_group_current_set.mjs";
-import { app_memorize_group_to_range_string } from "./app_memorize_group_to_range_string.mjs";
+import { app_memorize_frame } from "./app_memorize_frame.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-import { list_size } from "./list_size.mjs";
-import { html_hash } from "./html_hash.mjs";
-import { app_memorize_group } from "./app_memorize_group.mjs";
-import { string_case_lower } from "./string_case_lower.mjs";
-import { bible_engbsb_storage_path_file } from "./bible_engbsb_storage_path_file.mjs";
-import { storage_url } from "./storage_url.mjs";
-import { http_get } from "./http_get.mjs";
 import { html_style } from "./html_style.mjs";
-import { list_get } from "./list_get.mjs";
-import { list_first } from "./list_first.mjs";
-import { list_index } from "./list_index.mjs";
-import { list_map } from "./list_map.mjs";
-import { string_replace } from "./string_replace.mjs";
-import { html_on } from "./html_on.mjs";
 import { object_merge } from "./object_merge.mjs";
 export async function app_memorize() {
   let context = {};
@@ -28,38 +12,5 @@ export async function app_memorize() {
   object_merge(context, {
     root,
   });
-  let file_path = bible_engbsb_storage_path_file("JHN19");
-  let verses = await http_get(storage_url(file_path));
-  object_merge(context, {
-    verses,
-  });
-  let verses_length = list_size(context.verses);
-  let groups = app_memorize_group(verses_length);
-  object_merge(context, {
-    groups,
-  });
-  context.button_height = 7;
-  app_memorize_group_current_set(context, list_first(context.groups));
-  html_hash({
-    verses: (value) => {
-      let mapped = list_map(context.groups, (g) =>
-        app_memorize_group_to_range_string(context, g),
-      );
-      let mapped2 = list_map(mapped, (m) => string_replace(m, " ", ""));
-      let index = list_index(mapped2, value);
-      let g = list_get(context.groups, index);
-      app_memorize_group_current_set(context, g);
-    },
-    pattern: (value) => {
-      context.pattern_index = list_index(context.patterns, value);
-    },
-  });
-  app_memorize_refresh_memorize(context);
-  html_on(root, "keydown", (e) => {
-    let { keyCode } = e;
-    let c = String.fromCharCode(keyCode);
-    let c_lower = string_case_lower(c);
-    app_memorize_on_keydown(context, c_lower);
-  });
-  context.errored_keys = [];
+  await app_memorize_frame(context, root);
 }
---
+++ public/src/app_memorize_frame.mjs
@@ -0,0 +1,54 @@
+import { app_memorize_on_keydown } from "./app_memorize_on_keydown.mjs";
+import { string_case_lower } from "./string_case_lower.mjs";
+import { html_on } from "./html_on.mjs";
+import { app_memorize_refresh_memorize } from "./app_memorize_refresh_memorize.mjs";
+import { list_get } from "./list_get.mjs";
+import { list_index } from "./list_index.mjs";
+import { string_replace } from "./string_replace.mjs";
+import { app_memorize_group_to_range_string } from "./app_memorize_group_to_range_string.mjs";
+import { list_map } from "./list_map.mjs";
+import { html_hash } from "./html_hash.mjs";
+import { list_first } from "./list_first.mjs";
+import { app_memorize_group_current_set } from "./app_memorize_group_current_set.mjs";
+import { app_memorize_group } from "./app_memorize_group.mjs";
+import { list_size } from "./list_size.mjs";
+import { object_merge } from "./object_merge.mjs";
+import { storage_url } from "./storage_url.mjs";
+import { http_get } from "./http_get.mjs";
+import { bible_engbsb_storage_path_file } from "./bible_engbsb_storage_path_file.mjs";
+export async function app_memorize_frame(context, root) {
+  let file_path = bible_engbsb_storage_path_file("JHN19");
+  let verses = await http_get(storage_url(file_path));
+  object_merge(context, {
+    verses,
+  });
+  let verses_length = list_size(context.verses);
+  let groups = app_memorize_group(verses_length);
+  object_merge(context, {
+    groups,
+  });
+  context.button_height = 7;
+  app_memorize_group_current_set(context, list_first(context.groups));
+  html_hash({
+    verses: (value) => {
+      let mapped = list_map(context.groups, (g) =>
+        app_memorize_group_to_range_string(context, g),
+      );
+      let mapped2 = list_map(mapped, (m) => string_replace(m, " ", ""));
+      let index = list_index(mapped2, value);
+      let g = list_get(context.groups, index);
+      app_memorize_group_current_set(context, g);
+    },
+    pattern: (value) => {
+      context.pattern_index = list_index(context.patterns, value);
+    },
+  });
+  app_memorize_refresh_memorize(context);
+  html_on(root, "keydown", (e) => {
+    let { keyCode } = e;
+    let c = String.fromCharCode(keyCode);
+    let c_lower = string_case_lower(c);
+    app_memorize_on_keydown(context, c_lower);
+  });
+  context.errored_keys = [];
+}
