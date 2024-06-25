e930d676edb732f80df7045bb0a93a722ddb538d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 16:49:06 -0400

    2024-06-25T20:49:04.476Z function_auto_after app_memorize

---
+++ public/src/html_script_axios.mjs
@@ -0,0 +1,18 @@
+import { html_attribute_set } from "./html_attribute_set.mjs";
+import { html_element } from "./html_element.mjs";
+export function html_script_axios(root) {
+  let c = html_element(root, "script");
+  html_attribute_set(
+    c,
+    "src",
+    "https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js",
+  );
+  html_attribute_set(
+    c,
+    "integrity",
+    "sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==",
+  );
+  html_attribute_set(c, "crossorigin", "anonymous");
+  html_attribute_set(c, "referrerpolicy", "no-referrer");
+  return c;
+}
