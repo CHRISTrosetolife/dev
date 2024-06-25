5bba10052b2a5b4dbda4614263dbd6b07a3a2dff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 12:28:21 -0400

    2024-06-25T16:28:20.994Z function_auto_after js_code_arrow_block_args_async

--- public/src/js_code_arrow_block_args_async.mjs
+++ public/src/js_code_arrow_block_args_async.mjs
@@ -1,13 +1,6 @@
+import { js_code_arrow_block_args } from "./js_code_arrow_block_args.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
-import { list_join_comma_space } from "./list_join_comma_space.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function js_code_arrow_block_args_async(names, body_string) {
   assert_arguments_length(arguments, 2);
-  return string_combine_multiple([
-    "(",
-    list_join_comma_space(names),
-    ")=>{",
-    body_string,
-    "}",
-  ]);
+  return "async " + js_code_arrow_block_args(names, body_string);
 }
