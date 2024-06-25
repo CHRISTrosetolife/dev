576d900002cd14115bd95c5a6d697348468e0a96
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 16:46:32 -0400

    2024-06-25T20:46:29.715Z function_auto_after html_load_wait

--- public/src/html_load_wait.mjs
+++ public/src/html_load_wait.mjs
@@ -1,4 +1,5 @@
-export async function html_load_wait() {
+export async function html_load_wait(component) {
+  let element = component;
   await new Promise(function (resolve) {
     window.addEventListener("load", function on_load() {
       window.removeEventListener(on_load);
