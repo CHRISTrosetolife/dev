dbc7ef61c881908af7a1cc306c9a6891504612b7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 15:12:30 -0400

    2024-06-25T19:12:30.103Z function_auto_after app_gs_map_cell_click

--- public/src/app_gs_map_cell_click.mjs
+++ public/src/app_gs_map_cell_click.mjs
@@ -24,9 +24,8 @@ export async function app_gs_map_cell_click(context, tile) {
     let menu_overlay = app_gs_menu_overlay(context);
     app_gs_menu_main(context, menu_overlay);
     return;
-  } else {
-    let { path } = app_gs_map_path(context, [tile]);
-    await app_gs_walk_path(context, path);
-    app_gs_save(context);
   }
+  let { path } = app_gs_map_path(context, [tile]);
+  await app_gs_walk_path(context, path);
+  app_gs_save(context);
 }
