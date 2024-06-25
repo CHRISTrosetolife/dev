a28c9a7c0e690fdc6acc4c187faec26dbcd9a4ec
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 12:17:40 -0400

    2024-06-25T16:17:39.867Z function_auto_after sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -29,7 +29,7 @@ export async function sandbox() {
   let new_testament_books = list_take_reverse(books, 27);
   return new_testament_books;
   list_adder((la) => {
-    each(new_testament_books, (item) => {});
+    each(new_testament_books, (book) => {});
   });
   return;
   list_map(csv_lines, (line) => {
