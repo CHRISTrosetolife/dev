6b0604339d0ef8121456ce20e484b5cd44b5af1b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 16:45:23 -0400

    2024-06-25T20:45:22.891Z function_auto_after html_load_wait

--- public/src/html_load_wait.mjs
+++ public/src/html_load_wait.mjs
@@ -1,5 +1,7 @@
 export async function html_load_wait() {
   await new Promise(function (resolve) {
-    window.addEventListener("load", resolve);
+    window.addEventListener("load", function on_load() {
+      resolve();
+    });
   });
 }
