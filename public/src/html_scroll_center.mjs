99039d97dd4d22cabdc633240ef7b1f47d9d1a8b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-25 17:46:06 -0400

    2024-06-25T21:46:05.230Z function_auto_after html_scroll_center

--- public/src/html_scroll_center.mjs
+++ public/src/html_scroll_center.mjs
@@ -2,7 +2,7 @@ import { html_scroll_center_generic } from "./html_scroll_center_generic.mjs";
 export function html_scroll_center(component) {
   let additional = {
     block: "nearest",
-    block: "nearest",
+    inline: "nearest",
   };
   html_scroll_center_generic(component, additional);
 }
