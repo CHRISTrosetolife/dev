e451a44ed92c94d506e28ab3d5e0bb675e5d131a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-26 13:04:42 -0400

    2024-06-26T17:04:41.883Z function_auto_after ceb_bible_words_definitions_pairs_find

--- public/src/ceb_bible_words_definitions_pairs_find.mjs
+++ public/src/ceb_bible_words_definitions_pairs_find.mjs
@@ -1,4 +1,4 @@
 import { ceb_bible_words_definitions_pairs_compute } from "./ceb_bible_words_definitions_pairs_compute.mjs";
 export async function ceb_bible_words_definitions_pairs_find(query) {
-  await ceb_bible_words_definitions_pairs_compute();
+  let pairs = await ceb_bible_words_definitions_pairs_compute();
 }
