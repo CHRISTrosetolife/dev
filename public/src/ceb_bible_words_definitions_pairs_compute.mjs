cb0dad504775838c2cb8dc874c48a06866c75b08
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-26 13:02:34 -0400

    2024-06-26T17:02:33.609Z function_auto_after ceb_bible_words_definitions_pairs_compute

--- public/src/ceb_bible_words_definitions_pairs_compute.mjs
+++ public/src/ceb_bible_words_definitions_pairs_compute.mjs
@@ -53,6 +53,7 @@ export async function ceb_bible_words_definitions_pairs_compute() {
     }
     assert(number_is, [eng.score]);
     let [eng_score, ceb_score] = list_map_property([eng, ceb], "score");
+    let n = 2;
     pair.score = number_power(
       number_power(eng_score, 2) + number_power(ceb_score, 2),
       1 / 2,
