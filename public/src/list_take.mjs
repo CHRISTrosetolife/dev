d7f20cc76e119bb57459d0fa276431e21e80c11c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-26 14:38:04 -0400

    2024-06-26T18:38:03.453Z function_auto_after list_take

--- public/src/list_take.mjs
+++ public/src/list_take.mjs
@@ -1,3 +1,7 @@
+import { assert } from "./assert.mjs";
+import { greater_than_equal } from "./greater_than_equal.mjs";
+import { list_size } from "./list_size.mjs";
 export function list_take(list, count) {
+  assert(greater_than_equal, [count, list_size(list)]);
   return list.slice(0, count);
 }
