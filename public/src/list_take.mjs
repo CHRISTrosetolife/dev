cf2a538d324d820cc863dcf28ae94b47ad24f8b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-26 14:40:11 -0400

    2024-06-26T18:40:10.762Z function_auto_after list_take_soft

--- public/src/list_take.mjs
+++ public/src/list_take.mjs
@@ -1,7 +1,8 @@
+import { list_take_soft } from "./list_take_soft.mjs";
 import { assert } from "./assert.mjs";
 import { less_than_equal } from "./less_than_equal.mjs";
 import { list_size } from "./list_size.mjs";
 export function list_take(list, count) {
   assert(less_than_equal, [count, list_size(list)]);
-  return list.slice(0, count);
+  return list_take_soft(list, count);
 }
---
+++ public/src/list_take_soft.mjs
@@ -0,0 +1,3 @@
+export function list_take_soft(list, count) {
+  return list.slice(0, count);
+}
