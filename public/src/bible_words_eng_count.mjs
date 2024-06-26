786a0b2d1d75585a72c78653b0eff93c829321fe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-26 12:52:05 -0400

    2024-06-26T16:52:04.934Z function_auto_after bible_words_eng_count

--- public/src/bible_words_eng_count.mjs
+++ public/src/bible_words_eng_count.mjs
@@ -1,7 +1,9 @@
+import { string_symbols } from "./string_symbols.mjs";
 import { bible_words_count_generic } from "./bible_words_count_generic.mjs";
 import { bible_words_eng_cache } from "./bible_words_eng_cache.mjs";
 export async function bible_words_eng_count() {
   let all = await bible_words_eng_cache();
+  return string_symbols(all);
   let symbols = "-–,;:!?.…·'‘’“”()[]{}/&`►◄0123456789";
   return bible_words_count_generic(all, symbols);
 }
