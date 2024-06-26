14afa4342c8812afb0779a7239b500f0fd1d1596
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-26 13:14:16 -0400

    2024-06-26T17:14:15.999Z function_auto_after ceb_bible_words_definitions_pairs_find

--- public/src/ceb_bible_words_definitions_pairs_find.mjs
+++ public/src/ceb_bible_words_definitions_pairs_find.mjs
@@ -1,24 +1,21 @@
 import { log } from "./log.mjs";
-import { list_adder } from "./list_adder.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
 import { each_index } from "./each_index.mjs";
 import { ceb_bible_words_definitions_pairs_compute } from "./ceb_bible_words_definitions_pairs_compute.mjs";
 export async function ceb_bible_words_definitions_pairs_find(query) {
   let pairs = await ceb_bible_words_definitions_pairs_compute();
-  return list_adder((la) => {
-    each_index(pairs, (pair, index) => {
-      each(["eng", "ceb"], (language) => {
-        if (
-          object_property_get(object_property_get(pair, language), "word") ===
-          query
-        ) {
-          log({})({
-            pair,
-            index,
-          });
-        }
-      });
+  each_index(pairs, (pair, index) => {
+    each(["eng", "ceb"], (language) => {
+      if (
+        object_property_get(object_property_get(pair, language), "word") ===
+        query
+      ) {
+        log({
+          pair,
+          index,
+        });
+      }
     });
   });
 }
