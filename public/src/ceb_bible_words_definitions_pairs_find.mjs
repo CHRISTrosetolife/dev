5359713623a2563dc23f7ab93160eedd0f0f858d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-26 13:05:26 -0400

    2024-06-26T17:05:25.978Z function_auto_after ceb_bible_words_definitions_pairs_find

--- public/src/ceb_bible_words_definitions_pairs_find.mjs
+++ public/src/ceb_bible_words_definitions_pairs_find.mjs
@@ -4,6 +4,6 @@ import { ceb_bible_words_definitions_pairs_compute } from "./ceb_bible_words_def
 export async function ceb_bible_words_definitions_pairs_find(query) {
   let pairs = await ceb_bible_words_definitions_pairs_compute();
   each_index(pairs, (pair, index) => {
-    each(list, (item) => {});
+    each(["eng", "ceb"], (language) => {});
   });
 }
