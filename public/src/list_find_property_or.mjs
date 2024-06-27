018e3d1b7b777d7fd91232e38a5065a46c78ccee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-27 17:46:41 -0400

    2024-06-27T21:46:41.477Z function_auto_after list_find_property_or

--- public/src/list_find_property_or.mjs
+++ public/src/list_find_property_or.mjs
@@ -2,6 +2,7 @@ import { list_single } from "./list_single.mjs";
 import { list_filter_property } from "./list_filter_property.mjs";
 import { list_size_1 } from "./list_size_1.mjs";
 export function list_find_property_or(list, property_name, value, or_value) {
+  let use_or = true;
   let npcs = list_filter_property(list, property_name, value);
   if (list_size_1(npcs)) {
     let npc = list_single(npcs);
