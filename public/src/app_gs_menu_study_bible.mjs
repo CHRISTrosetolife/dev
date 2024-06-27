2b1d01280157fae6e8ad52d123b06737b8d012d0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-27 15:43:39 -0400

    2024-06-27T19:43:38.919Z function_auto_after app_gs_menu_study_bible

--- public/src/app_gs_menu_study_bible.mjs
+++ public/src/app_gs_menu_study_bible.mjs
@@ -3,6 +3,7 @@ import { app_gs_overlay_speech_text } from "./app_gs_overlay_speech_text.mjs";
 import { error } from "./error.mjs";
 import { app_memorize_frame } from "./app_memorize_frame.mjs";
 import { html_clear } from "./html_clear.mjs";
+import { list_size } from "./list_size.mjs";
 export async function app_gs_menu_study_bible(menu_overlay) {
   html_clear(menu_overlay);
   await app_memorize_frame({
@@ -24,6 +25,7 @@ export async function app_gs_menu_study_bible(menu_overlay) {
       let { tokens } = a;
       let { player } = context.game;
       let { energy } = player;
+      energy.conversation += list_size(tokens);
       html_clear(menu_overlay);
       app_gs_overlay_speech_text("You gained");
       html_remove(menu_overlay);
