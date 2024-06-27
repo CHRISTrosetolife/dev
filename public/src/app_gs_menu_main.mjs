47c0740fd0f5b93661b0e9f80768cf95bce1fb03
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-27 15:41:06 -0400

    2024-06-27T19:41:05.906Z function_auto_after app_gs_menu_main

--- public/src/app_gs_menu_main.mjs
+++ public/src/app_gs_menu_main.mjs
@@ -41,7 +41,7 @@ export function app_gs_menu_main(context, menu_overlay) {
           },
         },
         group_next: (a) => {
-          let tokens = a;
+          let { tokens } = a;
           html_clear(menu_overlay);
           app_gs_overlay_speech_text(menu_overlay);
           html_remove(menu_overlay);
