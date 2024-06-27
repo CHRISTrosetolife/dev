f3a56b85ae8debe46c5824387c6766ac41209323
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-27 15:41:44 -0400

    2024-06-27T19:41:43.070Z function_auto_after app_gs_menu_study_bible

--- public/src/app_gs_menu_main.mjs
+++ public/src/app_gs_menu_main.mjs
@@ -1,6 +1,4 @@
-import { app_gs_overlay_speech_text } from "./app_gs_overlay_speech_text.mjs";
-import { error } from "./error.mjs";
-import { app_memorize_frame } from "./app_memorize_frame.mjs";
+import { app_gs_menu_study_bible } from "./app_gs_menu_study_bible.mjs";
 import { app_gs_load } from "./app_gs_load.mjs";
 import { app_gs_game_delete } from "./app_gs_game_delete.mjs";
 import { app_gs_menu_pray } from "./app_gs_menu_pray.mjs";
@@ -24,30 +22,7 @@ export function app_gs_menu_main(context, menu_overlay) {
     menu_overlay,
     "📖 study bible",
     async () => {
-      html_clear(menu_overlay);
-      await app_memorize_frame({
-        settings: false,
-        root: menu_overlay,
-        style: {
-          text: {
-            color: "white",
-          },
-          spacer2: {
-            color: "lightgreen",
-            "background-color": "lightgreen",
-          },
-          error: {
-            color: "salmon",
-          },
-        },
-        group_next: (a) => {
-          let { tokens } = a;
-          html_clear(menu_overlay);
-          app_gs_overlay_speech_text(menu_overlay);
-          html_remove(menu_overlay);
-        },
-        short: true,
-      });
+      await app_gs_menu_study_bible(menu_overlay);
     },
   );
   html_button_width_full_text_click(menu_overlay, "🌱 new game", async () => {
---
+++ public/src/app_gs_menu_study_bible.mjs
@@ -0,0 +1,31 @@
+import { html_remove } from "./html_remove.mjs";
+import { app_gs_overlay_speech_text } from "./app_gs_overlay_speech_text.mjs";
+import { error } from "./error.mjs";
+import { app_memorize_frame } from "./app_memorize_frame.mjs";
+import { html_clear } from "./html_clear.mjs";
+export async function app_gs_menu_study_bible(menu_overlay) {
+  html_clear(menu_overlay);
+  await app_memorize_frame({
+    settings: false,
+    root: menu_overlay,
+    style: {
+      text: {
+        color: "white",
+      },
+      spacer2: {
+        color: "lightgreen",
+        "background-color": "lightgreen",
+      },
+      error: {
+        color: "salmon",
+      },
+    },
+    group_next: (a) => {
+      let { tokens } = a;
+      html_clear(menu_overlay);
+      app_gs_overlay_speech_text(menu_overlay);
+      html_remove(menu_overlay);
+    },
+    short: true,
+  });
+}
