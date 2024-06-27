70a6b4eb1327db074744133bf2a2f411da3151a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-27 17:36:50 -0400

    2024-06-27T21:36:50.382Z function_auto_after app_gs_menu_study_bible

--- public/src/app_gs_menu_study_bible.mjs
+++ public/src/app_gs_menu_study_bible.mjs
@@ -47,7 +47,7 @@ export async function app_gs_menu_study_bible(menu_overlay) {
           "Would you like to study the 📖 bible some more?",
         ]),
       );
-      app_memorize_group_next;
+      app_memorize_group_next();
       html_button_width_full_text_click(
         menu_overlay,
         "📖 Yes, study the bible more",
