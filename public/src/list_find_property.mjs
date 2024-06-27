eaee6fa3adce3f2843849d11c760a37856590650
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-27 17:47:50 -0400

    2024-06-27T21:47:50.000Z function_auto_after list_find_property

--- public/src/list_find_property.mjs
+++ public/src/list_find_property.mjs
@@ -1,10 +1,4 @@
 import { list_find_property_or_generic } from "./list_find_property_or_generic.mjs";
 export function list_find_property(list, property_name, value, or_value) {
-  return list_find_property_or_generic(
-    list,
-    property_name,
-    value,
-    or_value,
-    true,
-  );
+  return list_find_property_or_generic(list, property_name, value, null, false);
 }
