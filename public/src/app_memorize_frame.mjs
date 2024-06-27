a6b025904bed2f1f339fca2b6f3e3f6c6c3663ff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-27 18:05:20 -0400

    2024-06-27T22:05:19.939Z function_auto_after app_memorize_frame

--- public/src/app_memorize_frame.mjs
+++ public/src/app_memorize_frame.mjs
@@ -1,4 +1,4 @@
-import { object_property_get } from "./object_property_get.mjs";
+import { object_property_get_or } from "./object_property_get_or.mjs";
 import { app_memorize_groups_get } from "./app_memorize_groups_get.mjs";
 import { app_memorize_on_keydown } from "./app_memorize_on_keydown.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
@@ -15,7 +15,6 @@ import { storage_url } from "./storage_url.mjs";
 import { http_get } from "./http_get.mjs";
 import { bible_engbsb_storage_path_file } from "./bible_engbsb_storage_path_file.mjs";
 import { html_document_body } from "./html_document_body.mjs";
-import { object_property_exists } from "./object_property_exists.mjs";
 export async function app_memorize_frame(context) {
   let file_path = bible_engbsb_storage_path_file("JHN19");
   let verses = await http_get(storage_url(file_path));
@@ -28,12 +27,11 @@ export async function app_memorize_frame(context) {
     groups,
   });
   context.button_height = 7;
-  let group;
-  if (object_property_exists(context, "group_current")) {
-    group = object_property_get(context, "group_current");
-  } else {
-    group = list_first(context.groups);
-  }
+  let group = object_property_get_or(
+    context,
+    "group_current",
+    list_first(context.groups),
+  );
   html_hash({
     verses: (value) => {
       let g = app_memorize_groups_get(context, value);
