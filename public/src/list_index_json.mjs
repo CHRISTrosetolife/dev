9a580e6a1dcab35177d980d7cb29e780d0c36f3b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 11:41:04 -0400

    2024-06-28T15:41:03.452Z function_auto_after list_index_json

--- public/src/list_index_json.mjs
+++ public/src/list_index_json.mjs
@@ -1,6 +1,8 @@
+import { each_index } from "./each_index.mjs";
 import { number_less_than } from "./number_less_than.mjs";
 import { assert_not } from "./assert_not.mjs";
 export function list_index_json(list, element) {
+  each_index(list2, (item, index2) => {});
   let index = list.indexOf(element);
   assert_not(number_less_than, [index, 0]);
   return index;
