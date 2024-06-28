ebcc0e48aa150801b6171ca816cbe90457f783b4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 11:41:48 -0400

    2024-06-28T15:41:47.944Z function_auto_after list_index_json

--- public/src/list_index_json.mjs
+++ public/src/list_index_json.mjs
@@ -1,9 +1,13 @@
+import { equal_json } from "./equal_json.mjs";
 import { each_index } from "./each_index.mjs";
 import { number_less_than } from "./number_less_than.mjs";
 import { assert_not } from "./assert_not.mjs";
 export function list_index_json(list, element) {
   let result;
-  each_index(list, (item, index) => {});
+  each_index(list, (item, index) => {
+    if (equal_json(item, element)) {
+    }
+  });
   let index = list.indexOf(element);
   assert_not(number_less_than, [index, 0]);
   return index;
