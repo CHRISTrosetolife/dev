e3c21feacc764a85779d98df7a096c32d8b2c7da
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 12:14:28 -0400

    2024-06-28T16:14:25.154Z function_auto_after app_gs_menu_study_bible

--- public/src/app_gs_menu_study_bible.mjs
+++ public/src/app_gs_menu_study_bible.mjs
@@ -111,6 +111,7 @@ export async function app_gs_menu_study_bible(menu_overlay, context) {
       );
     },
   };
+  let { game } = context;
   if (game.memorize.group_current) {
     object_merge(memorize_context, {
       group_current: game.memorize.group_current,
