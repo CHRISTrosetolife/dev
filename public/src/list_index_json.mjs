c5431bc98fbdce7532420bf676c0c241ff8c2c76
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 11:42:08 -0400

    2024-06-28T15:42:08.006Z function_auto_after list_index_json

--- public/src/list_index_json.mjs
+++ public/src/list_index_json.mjs
@@ -3,7 +3,7 @@ import { each_index } from "./each_index.mjs";
 import { number_less_than } from "./number_less_than.mjs";
 import { assert_not } from "./assert_not.mjs";
 export function list_index_json(list, element) {
-  let result;
+  let result = -1;
   each_index(list, (item, index) => {
     if (equal_json(item, element)) {
       result = index;
