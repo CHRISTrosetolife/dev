c5d26dcde5e084c634e86519e0257ad709113efb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-27 20:28:17 -0400

    2024-06-28T00:28:16.718Z function_auto_after app_gs_menu_study_bible_group

--- public/src/app_gs_menu_study_bible_group.mjs
+++ public/src/app_gs_menu_study_bible_group.mjs
@@ -3,6 +3,7 @@ import { app_memorize_group_to_range_string } from "./app_memorize_group_to_rang
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function app_gs_menu_study_bible_group(menu_overlay, g) {
+  let extra = "";
   html_button_width_full_text_click(
     menu_overlay,
     string_combine_multiple([
