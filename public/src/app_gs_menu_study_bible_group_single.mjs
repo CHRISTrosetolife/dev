54e2c5a528006b8646890277fcd5a16e219c0084
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-27 20:32:06 -0400

    2024-06-28T00:32:05.887Z function_auto_after app_gs_menu_study_bible

---
+++ public/src/app_gs_menu_study_bible_group_single.mjs
@@ -0,0 +1,5 @@
+import { app_gs_menu_study_bible_group } from "./app_gs_menu_study_bible_group.mjs";
+export function app_gs_menu_study_bible_group_single(menu_overlay, g) {
+  let extra = "";
+  app_gs_menu_study_bible_group(menu_overlay, g, extra, context);
+}
