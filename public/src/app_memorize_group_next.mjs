2567c85c8aed9b510989afc153ebd20f5581df7d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 11:42:37 -0400

    2024-06-28T15:42:37.377Z function_auto_after app_memorize_group_next

--- public/src/app_memorize_group_next.mjs
+++ public/src/app_memorize_group_next.mjs
@@ -1,10 +1,10 @@
+import { list_index_json } from "./list_index_json.mjs";
 import { list_get } from "./list_get.mjs";
 import { add_1 } from "./add_1.mjs";
-import { list_index } from "./list_index.mjs";
 import { list_size } from "./list_size.mjs";
 export function app_memorize_group_next(context) {
   let { groups } = context;
-  let group_current_index = list_index(groups, context.group_current);
+  let group_current_index = list_index_json(groups, context.group_current);
   let group_next_index = add_1(group_current_index);
   if (group_next_index >= list_size(groups)) {
     return null;
