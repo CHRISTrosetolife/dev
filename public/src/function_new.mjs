58dd0172526b7041739abc0dd41e1b5b2f03d35a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 11:59:16 -0400

    2024-06-28T15:59:15.948Z function_auto_after function_new

--- public/src/function_new.mjs
+++ public/src/function_new.mjs
@@ -1,6 +1,7 @@
 import { file_write } from "./file_write.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 export async function function_new(function_name) {
+  assert_arguments_size();
   let open = true;
   await function_new_generic(
     function_name,
