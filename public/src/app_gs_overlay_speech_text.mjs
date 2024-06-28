db7a76ec482ce23efeff7b5cd346574cbce2634e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-27 20:34:16 -0400

    2024-06-28T00:34:15.441Z function_auto_after app_gs_overlay_speech_text

--- public/src/app_gs_overlay_speech_text.mjs
+++ public/src/app_gs_overlay_speech_text.mjs
@@ -1,7 +1,9 @@
 import { html_style_font_color_white } from "./html_style_font_color_white.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { app_gs_overlay_speech } from "./app_gs_overlay_speech.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function app_gs_overlay_speech_text(menu_overlay, text) {
+  assert_arguments_length;
   let overlay_speech = app_gs_overlay_speech(menu_overlay);
   let p = html_p_text(overlay_speech, text);
   html_style_font_color_white(p);
