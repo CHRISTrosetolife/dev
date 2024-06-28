9a64a606250f378d09c79784991203a2bd8963d5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 11:59:53 -0400

    2024-06-28T15:59:52.571Z function_auto_after list_map_sum

--- public/src/list_map_sum.mjs
+++ public/src/list_map_sum.mjs
@@ -1,4 +1,6 @@
+import { list_sum } from "./list_sum.mjs";
 import { list_map } from "./list_map.mjs";
 export function list_map_sum(list, mapper) {
   let m = list_map(list, mapper);
+  return list_sum(m);
 }
