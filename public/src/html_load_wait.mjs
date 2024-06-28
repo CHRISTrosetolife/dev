cd032b0bcf55202f2586c560435b4739fbc2b4e7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-27 20:56:18 -0400

    2024-06-28T00:56:17.933Z function_auto_after html_load_wait

--- public/src/html_load_wait.mjs
+++ public/src/html_load_wait.mjs
@@ -3,7 +3,6 @@ import { html_on_remove } from "./html_on_remove.mjs";
 export async function html_load_wait(component) {
   await new Promise(function (resolve) {
     let type = "load";
-    let { element } = component;
     html_on(component, type, function on_load() {
       html_on_remove(component, type, on_load);
       resolve();
