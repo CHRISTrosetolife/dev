78c03d7769af3ba858781dc10bf1e0f248b1e717
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 20:50:01 -0400

    2024-06-29T00:50:00.444Z function_auto_after app_record

--- public/src/app_record.mjs
+++ public/src/app_record.mjs
@@ -1,4 +1,6 @@
+import { log } from "./log.mjs";
 import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.mjs";
 export async function app_record() {
   let verses = await bible_engbsb_storage_http_get("bible");
+  log({});
 }
