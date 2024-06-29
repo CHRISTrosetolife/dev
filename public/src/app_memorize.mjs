845c6a81dcf2f606894828e76cf36739bd3410a4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 20:52:12 -0400

    2024-06-29T00:52:12.111Z function_auto_after app_memorize

--- public/src/app_memorize.mjs
+++ public/src/app_memorize.mjs
@@ -1,5 +1,4 @@
 import { html_script_axios } from "./html_script_axios.mjs";
-import { html_load_wait } from "./html_load_wait.mjs";
 import { app_memorize_frame } from "./app_memorize_frame.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { html_style } from "./html_style.mjs";
@@ -16,7 +15,6 @@ export async function app_memorize() {
   object_merge(context, {
     root,
   });
-  let c = html_script_axios(root);
-  await html_load_wait(c);
+  let c = await html_script_axios(root);
   await app_memorize_frame(context);
 }
