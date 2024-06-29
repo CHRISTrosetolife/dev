24e6d9a14a92ece970388d7d5413de10e71a8d50
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-29 17:07:18 -0400

    2024-06-29T21:07:17.364Z function_auto_after app_record_verse

--- public/src/app_record_verse.mjs
+++ public/src/app_record_verse.mjs
@@ -4,6 +4,7 @@ import { html_span_text } from "./html_span_text.mjs";
 import { list_find_property } from "./list_find_property.mjs";
 import { app_record_verses } from "./app_record_verses.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export async function app_record_verse(
   context,
   book_code,
@@ -19,4 +20,5 @@ export async function app_record_verse(
   html_style_bold(vn);
   html_span_text(root, " ");
   html_span_text(root, list_join_space(tokens));
+  html_button_width_full_text_click(root, "start recording");
 }
