e28ebfadbc79890bb1e0d1e256d06a824151da8f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 20:49:51 -0400

    2024-06-29T00:49:51.273Z function_auto_after app_record

--- public/src/app_record.mjs
+++ public/src/app_record.mjs
@@ -1,1 +1,4 @@
-export function app_record() {}
+import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.mjs";
+export async function app_record() {
+  let verses = await bible_engbsb_storage_http_get(chapter_code);
+}
