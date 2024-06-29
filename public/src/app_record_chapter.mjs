c42df7c2d51f6d4cbb0edef006b09b767942af45
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-29 17:01:54 -0400

    2024-06-29T21:01:53.824Z function_auto_after app_record_chapter

--- public/src/app_record_chapter.mjs
+++ public/src/app_record_chapter.mjs
@@ -1,3 +1,4 @@
+import { object_property_get_or_async } from "./object_property_get_or_async.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 import { html_style_bold } from "./html_style_bold.mjs";
@@ -7,11 +8,14 @@ import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+import { object_property_get_or } from "./object_property_get_or.mjs";
 export async function app_record_chapter(context, book_code, chapter) {
   let { root } = context;
   let chapter_code = string_combine_multiple([book_code, chapter]);
+  await object_property_get_or_async();
   if (object_property_exists_not(context, chapter_code)) {
     let verses = await bible_engbsb_storage_http_get(chapter_code);
+    object_property_get_or;
   }
   html_clear_scroll_top(root);
   each(verses, (verse) => {
