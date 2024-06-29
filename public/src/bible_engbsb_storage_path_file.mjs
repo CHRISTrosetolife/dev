825864fc1d4be268456be96881ee2608d3fcb3c9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 20:49:10 -0400

    2024-06-29T00:49:09.939Z function_auto_after bible_engbsb_storage_path_file

--- public/src/bible_engbsb_storage_path_file.mjs
+++ public/src/bible_engbsb_storage_path_file.mjs
@@ -1,7 +1,7 @@
 import { file_name_json } from "./file_name_json.mjs";
 import { bible_engbsb_storage_path } from "./bible_engbsb_storage_path.mjs";
 import { string_combine } from "./string_combine.mjs";
-export function bible_engbsb_storage_path_file(chapter_name) {
-  let file_name = file_name_json(chapter_name);
+export function bible_engbsb_storage_path_file(key) {
+  let file_name = file_name_json(key);
   return string_combine(bible_engbsb_storage_path(), file_name);
 }
