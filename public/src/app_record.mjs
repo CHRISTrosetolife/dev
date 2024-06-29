87020a90ffdc0260175c3cc6a0be810882589a3b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 20:56:03 -0400

    2024-06-29T00:56:02.276Z function_auto_after app_record

--- public/src/app_record.mjs
+++ public/src/app_record.mjs
@@ -7,7 +7,7 @@ import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.m
 export async function app_record() {
   let root = html_style_default_initialize();
   await html_script_axios(root);
-  let books = await bible_engbsb_storage_http_get("books");
+  let { books } = await bible_engbsb_storage_http_get("books");
   each(books, (book) => {
     html_button_text_click(root, book.book_code, noop);
   });
