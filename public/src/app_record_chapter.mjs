7b6022caf090f6cd6c72a7e82e909db4e37b205a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-29 17:02:57 -0400

    2024-06-29T21:02:57.235Z function_auto_after app_record_verses

--- public/src/app_record_chapter.mjs
+++ public/src/app_record_chapter.mjs
@@ -1,20 +1,13 @@
-import { object_property_get_or_async } from "./object_property_get_or_async.mjs";
+import { app_record_verses } from "./app_record_verses.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 import { html_style_bold } from "./html_style_bold.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { each } from "./each.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
-import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export async function app_record_chapter(context, book_code, chapter) {
   let { root } = context;
-  let chapter_code = string_combine_multiple([book_code, chapter]);
-  let verses = await object_property_get_or_async(
-    context,
-    chapter_code,
-    async () => await bible_engbsb_storage_http_get(chapter_code),
-  );
+  let verses = await app_record_verses(context, book_code, chapter);
   html_clear_scroll_top(root);
   each(verses, (verse) => {
     let { tokens, verse_number } = verse;
---
+++ public/src/app_record_verses.mjs
@@ -0,0 +1,12 @@
+import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.mjs";
+import { object_property_get_or_async } from "./object_property_get_or_async.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export async function app_record_verses(context, book_code, chapter) {
+  let chapter_code = string_combine_multiple([book_code, chapter]);
+  let verses = await object_property_get_or_async(
+    context,
+    chapter_code,
+    async () => await bible_engbsb_storage_http_get(chapter_code),
+  );
+  return verses;
+}
