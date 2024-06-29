b4cf9f2277e5a9f593c8914c310676400034242a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 20:55:13 -0400

    2024-06-29T00:55:13.339Z function_auto_after app_record

--- public/src/app_record.mjs
+++ public/src/app_record.mjs
@@ -1,3 +1,4 @@
+import { each } from "./each.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { html_script_axios } from "./html_script_axios.mjs";
 import { log } from "./log.mjs";
@@ -6,6 +7,7 @@ export async function app_record() {
   let root = html_style_default_initialize();
   await html_script_axios(root);
   let books = await bible_engbsb_storage_http_get("books");
+  each(list, (item) => {});
   log({
     books,
   });
