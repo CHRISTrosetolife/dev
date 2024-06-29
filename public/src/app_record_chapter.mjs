7e0a427edc8c4310e9c5907384a05cbe63a9d545
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-29 16:26:52 -0400

    2024-06-29T20:26:52.219Z function_auto_after app_record_chapter

--- public/src/app_record_chapter.mjs
+++ public/src/app_record_chapter.mjs
@@ -1,11 +1,12 @@
+import { noop } from "./noop.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 import { html_style_bold } from "./html_style_bold.mjs";
 import { html_span_text } from "./html_span_text.mjs";
-import { html_p } from "./html_p.mjs";
 import { each } from "./each.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export async function app_record_chapter(context, book_code, chapter) {
   let { root } = context;
   let chapter_code = string_combine_multiple([book_code, chapter]);
@@ -13,7 +14,7 @@ export async function app_record_chapter(context, book_code, chapter) {
   html_clear_scroll_top(root);
   each(verses, (verse) => {
     let { tokens, verse_number } = verse;
-    let p = html_p(root);
+    let p = html_button_width_full_text_click(root, "", noop);
     let vn = html_span_text(p, verse_number);
     html_style_bold(vn);
     html_span_text(p, " ");
