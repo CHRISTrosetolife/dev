30de8b2417e7aeedfe727cfd451e38f5f09a714b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 20:55:41 -0400

    2024-06-29T00:55:40.181Z function_auto_after app_record

--- public/src/app_record.mjs
+++ public/src/app_record.mjs
@@ -1,17 +1,14 @@
+import { noop } from "./noop.mjs";
 import { html_button_text_click } from "./html_button_text_click.mjs";
 import { each } from "./each.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { html_script_axios } from "./html_script_axios.mjs";
-import { log } from "./log.mjs";
 import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.mjs";
 export async function app_record() {
   let root = html_style_default_initialize();
   await html_script_axios(root);
   let books = await bible_engbsb_storage_http_get("books");
   each(books, (book) => {
-    html_button_text_click();
-  });
-  log({
-    books,
+    html_button_text_click(root, book.book_code, noop);
   });
 }
