2cde5adf84a1fa181f84efb6818c0fe14c8ba303
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-28 20:32:15 -0400

    2024-06-29T00:32:13.609Z function_auto_after bible_books_chapters

--- public/src/bible_books_chapters.mjs
+++ public/src/bible_books_chapters.mjs
@@ -2,7 +2,7 @@ import { bible_chapter } from "./bible_chapter.mjs";
 import { bible_chapters } from "./bible_chapters.mjs";
 import { bible_books } from "./bible_books.mjs";
 import { each_async } from "./each_async.mjs";
-export async function bible_books_chapters() {
+export async function bible_books_chapters(bible_folder) {
   await each_async(await bible_books(bible_folder), async (book_name) => {
     let chapters = await bible_chapters(bible_folder, book_name);
     await each_async(chapters, async (chapter_name) => {
