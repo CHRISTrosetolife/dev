5df598bb8836990745e71e04edca6bf5986c13fc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-29 17:03:47 -0400

    2024-06-29T21:03:47.112Z function_auto_after app_record_chapter

--- public/src/app_record_chapter.mjs
+++ public/src/app_record_chapter.mjs
@@ -14,7 +14,7 @@ export async function app_record_chapter(context, book_code, chapter) {
     let { tokens, verse_number } = verse;
     let p = html_button_width_full_text_click(root, "", async () => {
       let verses = await app_record_verses(context, book_code, chapter);
-      verse = list_find_property(verses, "verse_number", verse_number);
+      let verse = list_find_property(verses, "verse_number", verse_number);
       let vn = html_span_text(p, verse_number);
       html_style_bold(vn);
       html_span_text(p, " ");
