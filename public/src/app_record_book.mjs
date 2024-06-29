c0234113fad6010742d407c5f08d2d22ce4f3cb7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-29 16:23:57 -0400

    2024-06-29T20:23:56.655Z function_auto_after app_record_book

--- public/src/app_record_book.mjs
+++ public/src/app_record_book.mjs
@@ -1,10 +1,5 @@
+import { app_record_chapter } from "./app_record_chapter.mjs";
 import { list_find_property_or } from "./list_find_property_or.mjs";
-import { list_join_space } from "./list_join_space.mjs";
-import { html_style_bold } from "./html_style_bold.mjs";
-import { html_span_text } from "./html_span_text.mjs";
-import { html_p } from "./html_p.mjs";
-import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_button_text_click } from "./html_button_text_click.mjs";
 import { each } from "./each.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
@@ -15,17 +10,7 @@ export function app_record_book(context, book_code) {
   html_clear_scroll_top(root);
   each(book.chapters, (chapter) => {
     html_button_text_click(root, chapter, async () => {
-      let chapter_code = string_combine_multiple([book_code, chapter]);
-      let verses = await bible_engbsb_storage_http_get(chapter_code);
-      html_clear_scroll_top(root);
-      each(verses, (verse) => {
-        let { tokens, verse_number } = verse;
-        let p = html_p(root);
-        let vn = html_span_text(p, verse_number);
-        html_style_bold(vn);
-        html_span_text(p, " ");
-        html_span_text(p, list_join_space(tokens));
-      });
+      await app_record_chapter(context, book_code, chapter);
     });
   });
 }
