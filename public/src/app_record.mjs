bab1319276a7f019b9b12fac4fc369ffaeaa8ee0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-29 16:02:20 -0400

    2024-06-29T20:02:19.483Z function_auto_after app_record

--- public/src/app_record.mjs
+++ public/src/app_record.mjs
@@ -17,6 +17,7 @@ export async function app_record() {
         html_button_text_click(root, chapter, async () => {
           let chapter_code = string_combine_multiple([book_code, chapter]);
           let verses = await bible_engbsb_storage_http_get(chapter_code);
+          html_clear_scroll_top(root);
         });
       });
     });
