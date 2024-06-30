4dadf7752b2bfa839a6f5d3ae43d544b53bb9c76
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 16:19:42 -0400

    2024-06-30T20:19:42.511Z function_auto_after firebase_login

--- public/src/firebase_login.mjs
+++ public/src/firebase_login.mjs
@@ -1,14 +1,6 @@
-import { error } from "./error.mjs";
 import { global_get } from "./global_get.mjs";
-export function firebase_login() {
+export async function firebase_login() {
   let { firebase } = global_get();
   let { auth } = firebase;
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      let user = userCredential.user;
-    })
-    .catch((error) => {
-      let errorCode = error.code;
-      let errorMessage = error.message;
-    });
+  let user = await signInWithEmailAndPassword(auth, email, password);
 }
