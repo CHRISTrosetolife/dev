f42120971b4696c3298210ac6b3f21532ac989cc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 15:20:16 -0400

    2024-06-30T19:20:16.081Z function_auto_after app_record

--- public/src/app_record.mjs
+++ public/src/app_record.mjs
@@ -13,7 +13,14 @@ import { object_merge } from "./object_merge.mjs";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 export async function app_record() {
-  let firebaseConfig = {};
+  let firebaseConfig = {
+    apiKey: "AIzaSyBzIDxtdV44GoO3NH_a6KSw7iJ9Oa-trQU",
+    authDomain: "letjesusbeexalted.firebaseapp.com",
+    projectId: "letjesusbeexalted",
+    storageBucket: "letjesusbeexalted.appspot.com",
+    messagingSenderId: "1088010912651",
+    appId: "1:1088010912651:web:99fe9a26960793a702658e",
+  };
   let app = initializeApp(firebaseConfig);
   let auth = getAuth(app);
   let root = html_style_default_initialize();
