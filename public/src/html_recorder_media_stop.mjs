ea803c82da903845280167b9b79f21e1642812c7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 14:44:42 -0400

    2024-06-30T18:44:41.439Z function_auto_after html_recorder_media_stop

--- public/src/html_recorder_media_stop.mjs
+++ public/src/html_recorder_media_stop.mjs
@@ -6,13 +6,15 @@ export async function html_recorder_media_stop(mr) {
   let { media_recorder, chunks } = mr;
   media_recorder.stop();
   let type = "stop";
-  html_event_listener(media_recorder, type, async function on_stop() {
-    html_event_listener_remove(element, type, on_stop);
-    let blob = new Blob(chunks, {
-      type: chunks[0].type,
+  return await new Promise((resolve) => {
+    html_event_listener(media_recorder, type, async function on_stop() {
+      html_event_listener_remove(element, type, on_stop);
+      let blob = new Blob(chunks, {
+        type: chunks[0].type,
+      });
+      let blob_mp3 = await html_wav_to_mp3(blob);
+      list_remove_all(chunks);
+      return blob_mp3;
     });
-    let blob_mp3 = await html_wav_to_mp3(blob);
-    list_remove_all(chunks);
-    return blob_mp3;
   });
 }
