4ac313b6f38d229fdfb34a58dc949fe9d6379c56
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 14:32:21 -0400

    2024-06-30T18:32:20.703Z   t

--- public/src/html_recorder_media_start.mjs
+++
@@ -1,7 +0,0 @@
-import { log } from "./log.mjs";
-export function html_recorder_media_start(mr) {
-  let { media_recorder } = mr;
-  media_recorder.start();
-  log(media_recorder.state);
-  log("recorder started");
-}
