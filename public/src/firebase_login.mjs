4b42f985dc6e39c47ac5a8b4b31d036bab65f714
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 16:19:49 -0400

    2024-06-30T20:19:48.674Z function_auto_after firebase_login

--- public/src/firebase_login.mjs
+++ public/src/firebase_login.mjs
@@ -2,5 +2,6 @@ import { global_get } from "./global_get.mjs";
 export async function firebase_login() {
   let { firebase } = global_get();
   let { auth } = firebase;
-  let user = await signInWithEmailAndPassword(auth, email, password);
+  let { user } = await signInWithEmailAndPassword(auth, email, password);
+  return user;
 }
