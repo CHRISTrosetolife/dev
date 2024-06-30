a0795b88cfa8c7def8d781776ab2dbc4c5520118
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 14:42:46 -0400

    2024-06-30T18:42:45.386Z function_auto_after html_on

---
+++ public/src/html_event_listener.mjs
@@ -0,0 +1,3 @@
+export function html_event_listener(element, type, lambda) {
+  element.addEventListener(type, lambda);
+}
--- public/src/html_on.mjs
+++ public/src/html_on.mjs
@@ -1,4 +1,5 @@
+import { html_event_listener } from "./html_event_listener.mjs";
 export function html_on(component, type, lambda) {
   let { element } = component;
-  element.addEventListener(type, lambda);
+  html_event_listener(element, type, lambda);
 }
