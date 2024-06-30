06a3472e9a1527e73a9317d862c13f16be33eda3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 12:00:58 -0400

    2024-06-30T16:00:57.643Z function_auto_after app_record

--- public/src/app_record.mjs
+++ public/src/app_record.mjs
@@ -11,9 +11,9 @@ import { html_script_axios } from "./html_script_axios.mjs";
 import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.mjs";
 import { object_merge } from "./object_merge.mjs";
 export async function app_record() {
-  await html_script_extendable_media_recorder();
-  let context = {};
   let root = html_style_default_initialize();
+  await html_script_extendable_media_recorder(root);
+  let context = {};
   object_merge(context, {
     root,
   });
