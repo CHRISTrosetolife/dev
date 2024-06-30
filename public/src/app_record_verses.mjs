7c93c45410e02139e9baad36f3a88812ad314319
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 17:23:16 -0400

    2024-06-30T21:23:16.350Z function_auto_after app_record_verses

--- public/src/app_record_verses.mjs
+++ public/src/app_record_verses.mjs
@@ -1,8 +1,10 @@
+import { global_function_initialize_async } from "./global_function_initialize_async.mjs";
 import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.mjs";
 import { object_property_get_or_async } from "./object_property_get_or_async.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function app_record_verses(context, book_code, chapter) {
   let chapter_code = string_combine_multiple([book_code, chapter]);
+  await global_function_initialize_async();
   let verses = await object_property_get_or_async(
     context,
     chapter_code,
