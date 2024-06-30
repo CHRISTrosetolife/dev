addfd8a09c5e9bcfccde9d9aadbdfdadfa322ddb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 13:49:12 -0400

    2024-06-30T17:49:12.305Z function_auto_after html_recorder_media_record

--- public/src/html_recorder_media_record.mjs
+++ public/src/html_recorder_media_record.mjs
@@ -1,4 +1,6 @@
+import { html_recorder_media_start } from "./html_recorder_media_start.mjs";
 export function html_recorder_media_record(mr) {
   let AudioContext = window.AudioContext || window.webkitAudioContext;
   let audioContext = new AudioContext();
+  html_recorder_media_start(mr);
 }
