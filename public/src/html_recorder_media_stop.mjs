d6f73e9c2ff34d3482165763119a5dee691f1021
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 13:54:33 -0400

    2024-06-30T17:54:32.301Z function_auto_after html_recorder_media_stop

--- public/src/html_recorder_media_stop.mjs
+++ public/src/html_recorder_media_stop.mjs
@@ -1,4 +1,4 @@
-import { list_clear } from "./list_clear.mjs";
+import { list_remove_all } from "./list_remove_all.mjs";
 import { log } from "./log.mjs";
 export async function html_recorder_media_stop(mr) {
   let { media_recorder, chunks } = mr;
@@ -9,6 +9,6 @@ export async function html_recorder_media_stop(mr) {
     type: chunks[0].type,
   });
   let blob_mp3 = await html_wav_to_mp3(blob);
-  list_clear(chunks);
+  list_remove_all(chunks);
   return blob_mp3;
 }
