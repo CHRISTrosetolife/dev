e2ca71427a63b8668fde9702bce8865f0412f578
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 14:08:22 -0400

    2024-06-30T18:08:22.083Z function_auto_after app_record_verse

--- public/src/app_record_verse.mjs
+++ public/src/app_record_verse.mjs
@@ -34,9 +34,9 @@ export async function app_record_verse(
     let clipContainer = html_element(root, "article");
     let clipLabel = html_p_text(clipContainer, clipName);
     let audio = html_element(clipContainer, "audio");
+    html_attribute_set(audio, "controls", "");
     let audioURL = window.URL.createObjectURL(blob);
     audio.src = audioURL;
-    html_attribute_set(audio, "controls", "");
     let deleteButton = html_button_width_full_text_click(
       clipContainer,
       "Delete",
