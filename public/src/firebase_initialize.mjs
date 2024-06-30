1c51761eb43ea5b4b9b3b3f387b2c3df183c77b1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 15:29:51 -0400

    2024-06-30T19:29:50.795Z function_auto_after firebase_initialize

--- public/src/firebase_initialize.mjs
+++ public/src/firebase_initialize.mjs
@@ -4,21 +4,22 @@ import { object_merge } from "./object_merge.mjs";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 export function firebase_initialize() {
-  let firebaseConfig = {
-    apiKey: "AIzaSyBzIDxtdV44GoO3NH_a6KSw7iJ9Oa-trQU",
-    authDomain: "letjesusbeexalted.firebaseapp.com",
-    projectId: "letjesusbeexalted",
-    storageBucket: "letjesusbeexalted.appspot.com",
-    messagingSenderId: "1088010912651",
-    appId: "1:1088010912651:web:99fe9a26960793a702658e",
-  };
-  let app = initializeApp(firebaseConfig);
-  let auth = getAuth(app);
-  object_merge(global_get(), {
-    firebase: {
-      app,
-      auth,
-    },
+  global_function_initialize(firebase_initialize, () => {
+    let firebaseConfig = {
+      apiKey: "AIzaSyBzIDxtdV44GoO3NH_a6KSw7iJ9Oa-trQU",
+      authDomain: "letjesusbeexalted.firebaseapp.com",
+      projectId: "letjesusbeexalted",
+      storageBucket: "letjesusbeexalted.appspot.com",
+      messagingSenderId: "1088010912651",
+      appId: "1:1088010912651:web:99fe9a26960793a702658e",
+    };
+    let app = initializeApp(firebaseConfig);
+    let auth = getAuth(app);
+    object_merge(global_get(), {
+      firebase: {
+        app,
+        auth,
+      },
+    });
   });
-  global_function_initialize();
 }
