77b9d804581bfeaf944b0f91588078621ea7da9a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 13:41:31 -0400

    2024-06-30T17:41:31.483Z function_auto_after html_recorder_media

--- public/src/html_recorder_media.mjs
+++ public/src/html_recorder_media.mjs
@@ -1,17 +1,15 @@
-import { log } from "./log.mjs";
+import { error } from "./error.mjs";
 export async function html_recorder_media() {
   let port = await connect();
   await register(port);
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    log("getUserMedia supported.");
-    let stream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-    });
-    let mediaRecorder = new MediaRecorder(stream, {
-      mimeType: "audio/wav",
-    });
-    return mediaRecorder;
-  } else {
-    log("getUserMedia not supported on your browser!");
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    error("getUserMedia not supported on your browser!");
   }
+  let stream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+  });
+  let mediaRecorder = new MediaRecorder(stream, {
+    mimeType: "audio/wav",
+  });
+  return mediaRecorder;
 }
