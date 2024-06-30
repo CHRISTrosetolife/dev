d3890b9b7cbec09aeb034d8e3c0dfbaa5dd2c671
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 17:22:26 -0400

    2024-06-30T21:22:25.603Z function_auto_after object_property_initialize_get_async

--- public/src/object_property_initialize_get_async.mjs
+++ public/src/object_property_initialize_get_async.mjs
@@ -1,9 +1,13 @@
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
-export function object_property_initialize_get_async(lookup, key, initial) {
+export async function object_property_initialize_get_async(
+  lookup,
+  key,
+  initial,
+) {
   if (object_property_exists_not(lookup, key)) {
-    object_property_set(lookup, key, initial());
+    object_property_set(lookup, key, await initial());
   }
   return object_property_get(lookup, key);
 }
