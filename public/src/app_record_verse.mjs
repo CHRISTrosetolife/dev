8f5354f68c10496eb0b9381650d450d6bdc9a561
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 18:10:31 -0400

    2024-06-30T22:10:30.429Z function_auto_after app_record_verse

--- public/src/app_record_verse.mjs
+++ public/src/app_record_verse.mjs
@@ -41,7 +41,7 @@ export async function app_record_verse(
   });
   cancel = html_button_width_full_text_click(
     root,
-    "⏺️ cancel recording",
+    "🚫 cancel recording",
     async () => {
       each(recording, html_style_display_none);
       let blob = await html_recorder_media_stop(context.mr);
