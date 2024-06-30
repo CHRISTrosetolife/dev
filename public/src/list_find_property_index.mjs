264a44ac4a3dda726cb5a748a8c6c7f053f64f66
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 17:46:53 -0400

    2024-06-30T21:46:51.621Z function_auto_after list_find_property_index

--- public/src/list_find_property_index.mjs
+++ public/src/list_find_property_index.mjs
@@ -1,1 +1,6 @@
-export function list_find_property_index(verses, property_name, target_value) {}
+import { list_single } from "./list_single.mjs";
+import { list_filter_property_index } from "./list_filter_property_index.mjs";
+export function list_find_property_index(verses, property_name, target_value) {
+  let indices = list_filter_property_index(verses, property_name, target_value);
+  let index = list_single(indices);
+}
