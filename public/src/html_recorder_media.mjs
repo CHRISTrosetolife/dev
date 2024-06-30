e2a60b36a281c479d6c9871449f66f0d4b7c83ab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 13:40:54 -0400

    2024-06-30T17:40:54.355Z function_auto_after html_recorder_media

--- public/src/html_recorder_media.mjs
+++ public/src/html_recorder_media.mjs
@@ -4,15 +4,12 @@ export async function html_recorder_media() {
   await register(port);
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     log("getUserMedia supported.");
-    navigator.mediaDevices
-      .getUserMedia({
-        audio: true,
-      })
-      .then(function (stream) {
-        let mediaRecorder = new MediaRecorder(stream, {
-          mimeType: "audio/wav",
-        });
-      });
+    let stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+    });
+    let mediaRecorder = new MediaRecorder(stream, {
+      mimeType: "audio/wav",
+    });
   } else {
     log("getUserMedia not supported on your browser!");
   }
