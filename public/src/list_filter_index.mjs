a113a73aaf67245e692f6b397259d05303c581a5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 17:42:52 -0400

    2024-06-30T21:42:52.135Z function_auto_after list_filter_index

--- public/src/list_filter_index.mjs
+++ public/src/list_filter_index.mjs
@@ -1,7 +1,8 @@
 import { list_map_index } from "./list_map_index.mjs";
 export function list_filter_index(list, filter) {
-  let mapped = list_map_index(list, (item, index) => {
-    item, index;
-  });
+  let mapped = list_map_index(list, (item, index) => ({
+    item,
+    index,
+  }));
   return list.filter((item, index) => filter(item, index));
 }
