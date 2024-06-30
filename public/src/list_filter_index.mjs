2f81e032e0798589c59f162c468c210251b0bab3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 17:42:11 -0400

    2024-06-30T21:42:10.980Z function_auto_after list_filter_index

--- public/src/list_filter_index.mjs
+++ public/src/list_filter_index.mjs
@@ -1,3 +1,5 @@
+import { list_map } from "./list_map.mjs";
 export function list_filter_index(list, filter) {
+  let mapped = list_map(list, () => {});
   return list.filter((item, index) => filter(item, index));
 }
