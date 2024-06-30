8a85529ea8611a51b27648b02a63f874e7e95729
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 16:35:14 -0400

    2024-06-30T20:35:13.491Z function_auto_after firebase_auth

--- public/src/firebase_auth.mjs
+++ public/src/firebase_auth.mjs
@@ -1,1 +1,5 @@
-export function firebase_auth() {}
+import { global_get } from "./global_get.mjs";
+export function firebase_auth() {
+  let { firebase } = global_get();
+  let { auth } = firebase;
+}
