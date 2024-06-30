96baa63a6afc037a9985831483d45354ac358618
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 13:41:01 -0400

    2024-06-30T17:41:00.835Z function_auto_after html_recorder_media

--- public/src/html_recorder_media.mjs
+++ public/src/html_recorder_media.mjs
@@ -10,6 +10,7 @@ export async function html_recorder_media() {
     let mediaRecorder = new MediaRecorder(stream, {
       mimeType: "audio/wav",
     });
+    return mediaRecorder;
   } else {
     log("getUserMedia not supported on your browser!");
   }
