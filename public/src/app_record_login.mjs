522ac3375def7548eeea7cb0edfefd97d55a2c7b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 16:20:27 -0400

    2024-06-30T20:20:27.314Z function_auto_after app_record_login

--- public/src/app_record_login.mjs
+++ public/src/app_record_login.mjs
@@ -1,3 +1,4 @@
+import { firebase_login } from "./firebase_login.mjs";
 import { html_attribute_set } from "./html_attribute_set.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { html_input_width_full } from "./html_input_width_full.mjs";
@@ -5,5 +6,7 @@ export function app_record_login(root) {
   let email = html_input_width_full(root);
   let password = html_input_width_full(root);
   html_attribute_set(password, "type", "password");
-  html_button_width_full_text_click(root, "login", () => {});
+  html_button_width_full_text_click(root, "login", async () => {
+    await firebase_login(email, password);
+  });
 }
