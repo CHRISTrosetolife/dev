999beffaa2f1285ad12776af686c4b939afdc4d1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 18:01:10 -0400

    2024-06-30T22:01:10.130Z function_auto_after list_find_property_next_property

--- public/src/list_find_property_next_property.mjs
+++ public/src/list_find_property_next_property.mjs
@@ -1,11 +1,11 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { list_find_property_next } from "./list_find_property_next.mjs";
 export function list_find_property_next_property(
-  verses,
+  list,
   property_name,
   target_value,
 ) {
-  let verse_next = list_find_property_next(verses, property_name, target_value);
+  let verse_next = list_find_property_next(list, property_name, target_value);
   let verse_number_next = object_property_get(verse_next, property_name);
   return verse_next;
 }
