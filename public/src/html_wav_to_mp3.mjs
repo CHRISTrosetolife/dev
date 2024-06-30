7125d694ade4e6630b83c9d6b4d96ef38c3d30b0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 13:59:19 -0400

    2024-06-30T17:59:18.249Z function_auto_after html_wav_to_mp3

--- public/src/html_wav_to_mp3.mjs
+++ public/src/html_wav_to_mp3.mjs
@@ -1,1 +1,37 @@
-export function html_wav_to_mp3() {}
+import { error } from "./error.mjs";
+export function html_wav_to_mp3() {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    reader.onload = function () {
+      let arrayBuffer = this.result;
+      let wavDecoder = breezystacklamejs.WavHeader.readHeader(
+        new DataView(arrayBuffer),
+      );
+      let wavSamples = new Int16Array(
+        arrayBuffer,
+        wavDecoder.dataOffset,
+        wavDecoder.dataLen / 2,
+      );
+      let mp3Encoder = new breezystacklamejs.Mp3Encoder(
+        wavDecoder.channels,
+        wavDecoder.sampleRate,
+        128,
+      );
+      let mp3Buffer = mp3Encoder.encodeBuffer(wavSamples);
+      let mp3Data = mp3Encoder.flush();
+      let mp3BufferWithHeader = new Uint8Array(
+        mp3Buffer.length + mp3Data.length,
+      );
+      mp3BufferWithHeader.set(mp3Buffer, 0);
+      mp3BufferWithHeader.set(mp3Data, mp3Buffer.length);
+      let mp3Blob = new Blob([mp3BufferWithHeader], {
+        type: "audio/mp3",
+      });
+      resolve(mp3Blob);
+    };
+    reader.onerror = function (error) {
+      reject(error);
+    };
+    reader.readAsArrayBuffer(wavBlob);
+  });
+}
