4e70dd8ac4b9180e934a9093891cce78b0be8e22
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-06-30 14:32:05 -0400

    2024-06-30T18:32:05.140Z function_auto_after html_recorder_media_stop

--- public/src/html_recorder_media_stop.mjs
+++ public/src/html_recorder_media_stop.mjs
@@ -1,11 +1,8 @@
 import { html_wav_to_mp3 } from "./html_wav_to_mp3.mjs";
 import { list_remove_all } from "./list_remove_all.mjs";
-import { log } from "./log.mjs";
 export async function html_recorder_media_stop(mr) {
   let { media_recorder, chunks } = mr;
   media_recorder.stop();
-  log(media_recorder.state);
-  log("recorder stopped");
   let blob = new Blob(chunks, {
     type: chunks[0].type,
   });
