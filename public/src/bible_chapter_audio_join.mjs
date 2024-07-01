f77beae8ae333a6242dcaa0cbe46c4dd88d8abe7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 18:24:13 -0400

    2024-07-01T22:24:12.668Z function_auto_after bible_chapter_audio_join

--- public/src/bible_chapter_audio_join.mjs
+++ public/src/bible_chapter_audio_join.mjs
@@ -5,8 +5,6 @@ import { file_read_wav } from "./file_read_wav.mjs";
 import { each_index } from "./each_index.mjs";
 import { list_threshold_index_reverse } from "./list_threshold_index_reverse.mjs";
 import { list_threshold_index } from "./list_threshold_index.mjs";
-import { bible_chapter_audio_to_wav_path } from "./bible_chapter_audio_to_wav_path.mjs";
-import { string_suffix_change } from "./string_suffix_change.mjs";
 import { list_add } from "./list_add.mjs";
 import { object_merge } from "./object_merge.mjs";
 export async function bible_chapter_audio_join(bible_folder, chapter_name) {
@@ -14,11 +12,6 @@ export async function bible_chapter_audio_join(bible_folder, chapter_name) {
   await each_async(downloads, async (download) => {
     let { path } = download;
     let { trimmed } = path;
-    let path_trimmed = string_suffix_change(
-      wav,
-      bible_chapter_audio_to_wav_path(),
-      "trimmed.wav",
-    );
     let w = await file_read_wav(wav);
     let { samples, fmt } = w;
     let samples_out = [];
