7e5b02b12d426bde9dc4053c84f8ddf49d48a656
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 15:55:00 -0400

    2024-07-01T19:54:59.626Z function_auto_after bible_chapter_audio_to_wav

--- public/src/bible_chapter_audio_to_wav.mjs
+++ public/src/bible_chapter_audio_to_wav.mjs
@@ -11,6 +11,7 @@ export async function bible_chapter_audio_to_wav(bible_folder, chapter_name) {
   );
   let first = list_first(file_names);
   let folder = path_dirname(first);
+  log({});
   await new Promise((resolve, reject) => {
     ffmpeg(first)
       .toFormat("wav")
