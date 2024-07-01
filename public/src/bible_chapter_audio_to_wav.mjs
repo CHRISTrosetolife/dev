1ab9c216d0cbe77ca906a9d7e8edf33de7f12fb9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 16:06:39 -0400

    2024-07-01T20:06:38.797Z function_auto_after bible_chapter_audio_to_wav

--- public/src/bible_chapter_audio_to_wav.mjs
+++ public/src/bible_chapter_audio_to_wav.mjs
@@ -2,7 +2,6 @@ import { audio_to_wav } from "./audio_to_wav.mjs";
 import { path_dirname } from "./path_dirname.mjs";
 import { bible_chapter_audio_download } from "./bible_chapter_audio_download.mjs";
 import { list_first } from "./list_first.mjs";
-import ffmpeg from "fluent-ffmpeg";
 import { path_join } from "./path_join.mjs";
 export async function bible_chapter_audio_to_wav(bible_folder, chapter_name) {
   let file_paths = await bible_chapter_audio_download(
