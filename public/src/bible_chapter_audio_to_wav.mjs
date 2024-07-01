c81d311495cb3b883c4f52bba2ba3f2e46112c83
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 16:04:34 -0400

    2024-07-01T20:04:34.427Z function_auto_after bible_chapter_audio_to_wav

--- public/src/bible_chapter_audio_to_wav.mjs
+++ public/src/bible_chapter_audio_to_wav.mjs
@@ -1,10 +1,10 @@
+import { assert_async } from "./assert_async.mjs";
 import { path_dirname } from "./path_dirname.mjs";
 import { log } from "./log.mjs";
 import { bible_chapter_audio_download } from "./bible_chapter_audio_download.mjs";
 import { list_first } from "./list_first.mjs";
 import ffmpeg from "fluent-ffmpeg";
 import { path_join } from "./path_join.mjs";
-import { assert } from "./assert.mjs";
 import { file_exists } from "./file_exists.mjs";
 export async function bible_chapter_audio_to_wav(bible_folder, chapter_name) {
   let file_paths = await bible_chapter_audio_download(
@@ -17,8 +17,8 @@ export async function bible_chapter_audio_to_wav(bible_folder, chapter_name) {
   } = first;
   let path_input = path_system;
   let folder = path_dirname(path_input);
-  await new Promise((resolve, reject) => {
-    assert(file_exists, [path_input]);
+  await new Promise(async (resolve, reject) => {
+    await assert_async(file_exists, [path_input]);
     ffmpeg(path_input)
       .toFormat("wav")
       .on("error", (err) => {
