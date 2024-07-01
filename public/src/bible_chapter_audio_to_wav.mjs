15a6261f959484248685fa86703ea783bfa9cd79
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 18:30:04 -0400

    2024-07-01T22:30:04.332Z function_auto_after bible_chapter_audio_to_wav

--- public/src/bible_chapter_audio_to_wav.mjs
+++ public/src/bible_chapter_audio_to_wav.mjs
@@ -1,3 +1,4 @@
+import { bible_chapter_audio_download_folder_name } from "./bible_chapter_audio_download_folder_name.mjs";
 import { bible_chapter_audio_download_prefix } from "./bible_chapter_audio_download_prefix.mjs";
 import { bible_chapter_audio_to_wav_path } from "./bible_chapter_audio_to_wav_path.mjs";
 import { object_merge } from "./object_merge.mjs";
@@ -8,7 +9,7 @@ import { bible_chapter_audio_download } from "./bible_chapter_audio_download.mjs
 import { path_join } from "./path_join.mjs";
 export async function bible_chapter_audio_to_wav(bible_folder, chapter_name) {
   let downloads = await bible_chapter_audio_download(
-    bible_folder,
+    bible_chapter_audio_download_folder_name(),
     chapter_name,
   );
   await each_async(downloads, async (download) => {
