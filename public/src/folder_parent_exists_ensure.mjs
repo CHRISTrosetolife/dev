546ed5ef63b6ff5e99e74e8804560aac6e25c6df
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 15:52:33 -0400

    2024-07-01T19:52:31.821Z function_auto_after folder_parent_exists_ensure

--- public/src/folder_parent_exists_ensure.mjs
+++ public/src/folder_parent_exists_ensure.mjs
@@ -1,8 +1,9 @@
+import { path_dirname } from "./path_dirname.mjs";
 import { folder_new } from "./folder_new.mjs";
 import { file_exists } from "./file_exists.mjs";
 export async function folder_parent_exists_ensure(file_name) {
   let path = await import("path");
-  let parent = path.dirname(file_name);
+  let parent = path_dirname(file_name);
   if (!(await file_exists(parent))) {
     await folder_parent_exists_ensure(parent);
     await folder_new(parent);
