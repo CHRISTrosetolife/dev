de557d4a72d26b009d0025e8b278360478cba896
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 17:04:28 -0400

    2024-07-01T21:04:28.263Z function_auto_after bible_chapter_audio_trim

--- public/src/bible_chapter_audio_trim.mjs
+++ public/src/bible_chapter_audio_trim.mjs
@@ -19,6 +19,7 @@ export async function bible_chapter_audio_trim(bible_folder, chapter_name) {
   each_index(samples, (sample, index) => {
     let max = list_last(maxes);
     if (sample > max.value) {
+      max_add(maxes, sample, index);
     }
   });
   function max_add(maxes, value, index) {
