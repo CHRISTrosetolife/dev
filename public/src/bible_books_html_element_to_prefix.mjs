f9a37a1b6a2f3665c3446e75733f574fa19dbe8c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 14:42:30 -0400

    2024-07-01T18:42:29.773Z function_auto_after bible_books_html_element_to_prefix

--- public/src/bible_books_html_element_to_prefix.mjs
+++ public/src/bible_books_html_element_to_prefix.mjs
@@ -1,5 +1,6 @@
 import { html_parse_href } from "./html_parse_href.mjs";
 import { string_take } from "./string_take.mjs";
 export function bible_books_html_element_to_prefix(e) {
-  return string_take(html_parse_href(e), 3);
+  let href = html_parse_href(e);
+  return string_take(href, 3);
 }
