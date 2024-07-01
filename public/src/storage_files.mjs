94857e524ed6a3bdaf902a60a24fe1614d666888
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 13:44:26 -0400

    2024-07-01T17:44:25.650Z function_auto_after storage_files

--- public/src/storage_files.mjs
+++ public/src/storage_files.mjs
@@ -2,7 +2,7 @@ import { storage_bucket } from "./storage_bucket.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { error } from "./error.mjs";
 export async function storage_files(prefix) {
-  if (string_starts_with(destination, "/")) {
+  if (string_starts_with(prefix, "/")) {
     error();
   }
   let bucket = await storage_bucket();
