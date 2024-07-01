d6cb33958bfd39257e06f874cee9e344532322fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 17:34:00 -0400

    2024-07-01T21:33:59.719Z function_auto_after each_reverse

--- public/src/each_reverse.mjs
+++ public/src/each_reverse.mjs
@@ -1,10 +1,4 @@
-import { list_index_first } from "./list_index_first.mjs";
-import { list_get } from "./list_get.mjs";
-import { list_index_last } from "./list_index_last.mjs";
+import { each_index_reverse } from "./each_index_reverse.mjs";
 export function each_reverse(list, lambda) {
-  let index_last = list_index_last(list);
-  for (let i = index_last; i >= list_index_first(); i--) {
-    let element = list_get(list, i);
-    lambda(element);
-  }
+  each_index_reverse(list, (item, index) => lambda(item));
 }
