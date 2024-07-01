1b16f0dd8d7d220c860a5b097acd97493e29747c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 18:26:12 -0400

    2024-07-01T22:26:12.262Z function_auto_after list_add_multiple

--- public/src/list_add_multiple.mjs
+++ public/src/list_add_multiple.mjs
@@ -1,6 +1,6 @@
 import { list_add } from "./list_add.mjs";
 export function list_add_multiple(list, other) {
-  for (let s of other) {
-    list_add(list, s);
+  for (let o of other) {
+    list_add(list, o);
   }
 }
