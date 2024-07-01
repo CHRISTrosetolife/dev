ec93ce0b70ce33eaf87862fc4a92e0bd4c008d35
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 13:43:18 -0400

    2024-07-01T17:43:17.337Z   t

---
+++ public/src/storage_files.mjs
@@ -0,0 +1,12 @@
+import { storage_bucket } from "./storage_bucket.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
+import { error } from "./error.mjs";
+export async function storage_files(file_path, destination) {
+  if (string_starts_with(destination, "/")) {
+    error();
+  }
+  let bucket = await storage_bucket();
+  await bucket.upload(file_path, {
+    destination,
+  });
+}
