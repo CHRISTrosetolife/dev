36db977c9a87c434683436b47b35d1c017cd5f51
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 16:43:23 -0400

    2024-07-01T20:43:22.916Z function_auto_after bible_chapter_audio_trim

--- public/src/bible_chapter_audio_trim.mjs
+++ public/src/bible_chapter_audio_trim.mjs
@@ -1,24 +1,6 @@
-import { object_merge } from "./object_merge.mjs";
-import { each_async } from "./each_async.mjs";
-import { audio_to_wav } from "./audio_to_wav.mjs";
-import { path_dirname } from "./path_dirname.mjs";
-import { bible_chapter_audio_download } from "./bible_chapter_audio_download.mjs";
-import { path_join } from "./path_join.mjs";
+import { bible_chapter_audio_to_wav } from "./bible_chapter_audio_to_wav.mjs";
+import { list_first } from "./list_first.mjs";
 export async function bible_chapter_audio_trim(bible_folder, chapter_name) {
-  let downloads = await bible_chapter_audio_download(
-    bible_folder,
-    chapter_name,
-  );
-  await each_async(downloads, async (download) => {
-    let { path } = download;
-    let { system: path_system } = path;
-    let path_input = path_system;
-    let folder = path_dirname(path_input);
-    let path_output = path_join([folder, "last.wav"]);
-    object_merge(path, {
-      wav: path_output,
-    });
-    await audio_to_wav(path_input, path_output);
-  });
-  return downloads;
+  let downloads = await bible_chapter_audio_to_wav(bible_folder, chapter_name);
+  let first = list_first(downloads);
 }
