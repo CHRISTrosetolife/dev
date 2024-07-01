794ad37f5a0127b2721be6130fe877a89d37f266
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 14:20:56 -0400

    2024-07-01T18:20:55.795Z function_auto_after storage_files_download

--- public/src/storage_files_download.mjs
+++ public/src/storage_files_download.mjs
@@ -3,10 +3,14 @@ import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 import { list_first } from "./list_first.mjs";
 import { path_join } from "./path_join.mjs";
+import { file_exists } from "./file_exists.mjs";
 export async function storage_files_download(prefix) {
   let files = await storage_files(prefix);
   let f = list_first(files);
   let destination = folder_gitignore_path(path_join(["firebase", f.name]));
+  if (await file_exists(destination)) {
+    return;
+  }
   await folder_parent_exists_ensure(destination);
   return f.download({
     destination,
