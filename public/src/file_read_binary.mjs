a0c0e66490490e1ae6dbf6572aaa659bb5b4ba65
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 16:52:30 -0400

    2024-07-01T20:52:29.684Z function_auto_after file_read_binary

--- public/src/file_read_binary.mjs
+++ public/src/file_read_binary.mjs
@@ -1,5 +1,6 @@
-import { file_read_generic } from "./file_read_generic.mjs";
+import { import_node } from "./import_node.mjs";
 export async function file_read_binary(file_name) {
-  let encoding = "binary";
-  return await file_read_generic(file_name, encoding);
+  let fs = await import_node("fs");
+  let buffer = await fs.promises.readFile(wav);
+  return buffer;
 }
