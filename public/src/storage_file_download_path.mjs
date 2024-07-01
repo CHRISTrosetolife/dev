f1792d9a4e648634c1f3fcecdaeb5830f89cdf31
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 18:47:38 -0400

    2024-07-01T22:47:38.465Z function_auto_after storage_file_download_path

--- public/src/storage_file_download_path.mjs
+++ public/src/storage_file_download_path.mjs
@@ -1,5 +1,6 @@
 import { path_join } from "./path_join.mjs";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 export function storage_file_download_path(firebase_file) {
-  return folder_gitignore_path(path_join(["firebase", firebase_file.name]));
+  let file_name = firebase_file.name;
+  return folder_gitignore_path(path_join(["firebase", file_name]));
 }
