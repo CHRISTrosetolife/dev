c781b850234fa6b4c48438597dcc36db52edec56
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 16:42:15 -0400

    2024-07-01T20:42:15.289Z   t

---
+++ public/src/file_read_binary.mjs
@@ -0,0 +1,13 @@
+import { file_read_generic } from "./file_read_generic.mjs";
+import { global_get } from "./global_get.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { web_is } from "./web_is.mjs";
+export async function file_read_binary(file_name) {
+  if (web_is()) {
+    let g = global_get();
+    let { files } = g;
+    return object_property_get(files, file_name);
+  }
+  let encoding = "utf-8";
+  return await file_read_generic(file_name, encoding);
+}
