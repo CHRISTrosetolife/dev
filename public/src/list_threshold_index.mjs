9bc5528390c8c3a7fc7e7d9e7d9c6ad8a8cc7848
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 17:32:45 -0400

    2024-07-01T21:32:44.766Z function_auto_after list_threshold_index

--- public/src/list_threshold_index.mjs
+++ public/src/list_threshold_index.mjs
@@ -1,11 +1,6 @@
+import { list_threshold_index_generic } from "./list_threshold_index_generic.mjs";
 import { each_index } from "./each_index.mjs";
 export function list_threshold_index(list, threshold) {
   let lambda_each_index = each_index;
-  let index_first = null;
-  lambda_each_index(list, (item, index) => {
-    if (item >= threshold) {
-      index_first = index;
-      return true;
-    }
-  });
+  list_threshold_index_generic(lambda_each_index, list, threshold);
 }
---
+++ public/src/list_threshold_index_generic.mjs
@@ -0,0 +1,13 @@
+export function list_threshold_index_generic(
+  lambda_each_index,
+  list,
+  threshold,
+) {
+  let index_first = null;
+  lambda_each_index(list, (item, index) => {
+    if (item >= threshold) {
+      index_first = index;
+      return true;
+    }
+  });
+}
