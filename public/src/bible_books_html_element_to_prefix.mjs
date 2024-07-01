fe8fe7520625874a88216bb297434ffb438a971e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 14:43:09 -0400

    2024-07-01T18:43:08.656Z function_auto_after bible_chapter_audio_download

---
+++ public/src/bible_books_html_element_to_prefix.mjs
@@ -0,0 +1,6 @@
+import { bible_chapter_code_to_book } from "./bible_chapter_code_to_book.mjs";
+import { html_parse_href } from "./html_parse_href.mjs";
+export function bible_books_html_element_to_prefix(e) {
+  let href = html_parse_href(e);
+  return bible_chapter_code_to_book(href);
+}
