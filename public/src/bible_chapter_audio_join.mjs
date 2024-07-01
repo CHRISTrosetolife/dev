c238ef4c5fee31ef3ea0d416823de0d16128c961
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 18:26:27 -0400

    2024-07-01T22:26:26.930Z function_auto_after bible_chapter_audio_join

--- public/src/bible_chapter_audio_join.mjs
+++ public/src/bible_chapter_audio_join.mjs
@@ -1,14 +1,10 @@
+import { list_add_multiple } from "./list_add_multiple.mjs";
 import { equal_json } from "./equal_json.mjs";
 import { assert } from "./assert.mjs";
 import { bible_chapter_audio_trim } from "./bible_chapter_audio_trim.mjs";
 import { each_async } from "./each_async.mjs";
 import { file_overwrite_wav } from "./file_overwrite_wav.mjs";
 import { file_read_wav } from "./file_read_wav.mjs";
-import { each_index } from "./each_index.mjs";
-import { list_threshold_index_reverse } from "./list_threshold_index_reverse.mjs";
-import { list_threshold_index } from "./list_threshold_index.mjs";
-import { list_add } from "./list_add.mjs";
-import { object_merge } from "./object_merge.mjs";
 export async function bible_chapter_audio_join(bible_folder, chapter_name) {
   let downloads = await bible_chapter_audio_trim(bible_folder, chapter_name);
   let fmt_first = null;
@@ -23,16 +19,7 @@ export async function bible_chapter_audio_join(bible_folder, chapter_name) {
     } else {
       assert(equal_json, [fmt, fmt_first]);
     }
-    let first = list_threshold_index(samples, 400);
-    let last = list_threshold_index_reverse(samples, 5000);
-    each_index(samples, (item, index) => {
-      if (first <= index && index <= last) {
-        list_add(samples_out, item);
-      }
-    });
-    object_merge(path, {
-      trimmed: path_trimmed,
-    });
+    list_add_multiple(samples_out, samples);
   });
   await file_overwrite_wav(fmt, samples_out, path_trimmed);
   return downloads;
