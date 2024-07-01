cb8c9493bd0dde3930e0c295bb5061b40bd33864
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 14:06:55 -0400

    2024-07-01T18:06:54.898Z function_auto_after storage_bucket

--- public/src/storage_bucket.mjs
+++ public/src/storage_bucket.mjs
@@ -1,13 +1,16 @@
+import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import admin from "firebase-admin";
 import { file_read_json } from "./file_read_json.mjs";
 import { global_function_initialize } from "./global_function_initialize.mjs";
 export async function storage_bucket() {
   let service_account = await file_read_json("../firebase.json");
   let g = global_function_initialize(storage_bucket, {});
-  admin.initializeApp({
-    credential: admin.credential.cert(service_account),
-    storageBucket: "gs://letjesusbeexalted.appspot.com",
-  });
+  if (object_property_exists_not(g, "initialized")) {
+    admin.initializeApp({
+      credential: admin.credential.cert(service_account),
+      storageBucket: "gs://letjesusbeexalted.appspot.com",
+    });
+  }
   let bucket = admin.storage().bucket();
   return bucket;
 }
