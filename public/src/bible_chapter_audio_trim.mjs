eb5f12a0d750b14b7b43bc22dafb0f79805994f4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 17:23:32 -0400

    2024-07-01T21:23:32.061Z function_auto_after bible_chapter_audio_trim

--- public/src/bible_chapter_audio_trim.mjs
+++ public/src/bible_chapter_audio_trim.mjs
@@ -24,6 +24,7 @@ export async function bible_chapter_audio_trim(bible_folder, chapter_name) {
   let w = new WaveFile(await file_read_binary(wav));
   let { fmt } = w;
   let samples = w.getSamples();
+  return typeof samples;
   let maxes = [];
   let max_index = 0;
   max_add(maxes, list_first(samples), max_index);
