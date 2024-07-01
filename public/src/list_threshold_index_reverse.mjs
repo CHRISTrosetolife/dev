be0d9de3d147c1d073fb0ec4607dbf8f72dd8704
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 17:39:27 -0400

    2024-07-01T21:39:25.592Z function_auto_after list_threshold_index_reverse

--- public/src/list_threshold_index_reverse.mjs
+++ public/src/list_threshold_index_reverse.mjs
@@ -2,5 +2,5 @@ import { each_index_reverse } from "./each_index_reverse.mjs";
 import { list_threshold_index_generic } from "./list_threshold_index_generic.mjs";
 export function list_threshold_index_reverse(list, threshold) {
   let lambda_each_index = each_index_reverse;
-  list_threshold_index_generic(lambda_each_index, list, threshold);
+  return list_threshold_index_generic(lambda_each_index, list, threshold);
 }
