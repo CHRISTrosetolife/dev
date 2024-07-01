2fb3f1485dd68d6c96383c08bfc8da826daee06f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 15:50:48 -0400

    2024-07-01T19:50:48.226Z function_auto_after bible_chapter_audio_to_wav

--- public/src/bible_chapter_audio_to_wav.mjs
+++ public/src/bible_chapter_audio_to_wav.mjs
@@ -1,4 +1,7 @@
 import { bible_chapter_audio_download } from "./bible_chapter_audio_download.mjs";
 export async function bible_chapter_audio_to_wav(bible_folder, chapter_name) {
-  return await bible_chapter_audio_download(bible_folder, chapter_name);
+  let file_names = await bible_chapter_audio_download(
+    bible_folder,
+    chapter_name,
+  );
 }
