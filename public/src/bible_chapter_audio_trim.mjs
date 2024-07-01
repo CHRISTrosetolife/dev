0eea3b8064f928fc8f448b6463c5e2f94e5c9ab8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 16:47:51 -0400

    2024-07-01T20:47:51.317Z function_auto_after bible_chapter_audio_trim

--- public/src/bible_chapter_audio_trim.mjs
+++ public/src/bible_chapter_audio_trim.mjs
@@ -1,12 +1,13 @@
 import { file_read_binary } from "./file_read_binary.mjs";
 import { bible_chapter_audio_to_wav } from "./bible_chapter_audio_to_wav.mjs";
 import { list_first } from "./list_first.mjs";
-import { WaveFile } from "wavefile";
+import wavefile from "wavefile";
 export async function bible_chapter_audio_trim(bible_folder, chapter_name) {
   let downloads = await bible_chapter_audio_to_wav(bible_folder, chapter_name);
   let download = list_first(downloads);
   let { path } = download;
   let { wav } = path;
+  let { WaveFile } = wavefile;
   let w = new WaveFile(await file_read_binary(wav));
   return w;
 }
