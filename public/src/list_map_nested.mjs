55f805b9fc1e504c0632576b3ebd387e187fe996
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 14:49:10 -0400

    2024-07-01T18:49:10.545Z function_auto_after list_map_nested

--- public/src/list_map_nested.mjs
+++ public/src/list_map_nested.mjs
@@ -1,4 +1,4 @@
 import { list_map } from "./list_map.mjs";
-export function list_map_nested(list, mapper) {
-  return list_map(list, (a) => list_map(a, mapper));
+export function list_map_nested(list_of_lists, mapper) {
+  return list_map(list_of_lists, (a) => list_map(a, mapper));
 }
