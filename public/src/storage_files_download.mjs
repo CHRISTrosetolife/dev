ba900e599d3f79b87b509544523e7548a860716b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 14:01:42 -0400

    2024-07-01T18:01:42.130Z function_auto_after storage_files_download

--- public/src/storage_files_download.mjs
+++ public/src/storage_files_download.mjs
@@ -2,5 +2,5 @@ import { storage_files } from "./storage_files.mjs";
 import { storage_bucket } from "./storage_bucket.mjs";
 export async function storage_files_download(prefix) {
   let bucket = await storage_bucket();
-  await storage_files();
+  await storage_files(prefix);
 }
