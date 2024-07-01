b50477ab31e2300a3a015427b08e63f72ecdb910
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 14:17:58 -0400

    2024-07-01T18:17:58.075Z function_auto_after storage_files_names

---
+++ public/src/storage_files.mjs
@@ -0,0 +1,17 @@
+import { list_sort_string } from "./list_sort_string.mjs";
+import { list_single } from "./list_single.mjs";
+import { storage_bucket } from "./storage_bucket.mjs";
+import { error } from "./error.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
+export async function storage_files(prefix, result) {
+  if (string_starts_with(prefix, "/")) {
+    error();
+  }
+  let bucket = await storage_bucket();
+  let data = await bucket.getFiles({
+    prefix,
+  });
+  let files = list_single(data);
+  list_sort_string(result, (f) => f.name);
+  return files;
+}
--- public/src/storage_files_names.mjs
+++ public/src/storage_files_names.mjs
@@ -1,19 +1,7 @@
-import { storage_bucket } from "./storage_bucket.mjs";
-import { string_starts_with } from "./string_starts_with.mjs";
-import { error } from "./error.mjs";
-import { list_single } from "./list_single.mjs";
+import { storage_files } from "./storage_files.mjs";
 import { list_map_property } from "./list_map_property.mjs";
-import { list_sort_string } from "./list_sort_string.mjs";
 export async function storage_files_names(prefix) {
-  if (string_starts_with(prefix, "/")) {
-    error();
-  }
-  let bucket = await storage_bucket();
-  let data = await bucket.getFiles({
-    prefix,
-  });
-  let files = list_single(data);
-  list_sort_string(result, (f) => f.name);
+  let files = await storage_files(prefix, result);
   let result = list_map_property(files, "name");
   return result;
 }
