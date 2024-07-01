e63a8829ee716d0e1f9beb87f73ac39551798709
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 13:42:37 -0400

    2024-07-01T17:42:36.788Z function_auto_after storage_upload_file

--- public/src/storage_upload_file.mjs
+++ public/src/storage_upload_file.mjs
@@ -1,17 +1,12 @@
+import { storage_bucket } from "./storage_bucket.mjs";
 import admin from "firebase-admin";
-import { file_read_json } from "./file_read_json.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { error } from "./error.mjs";
 export async function storage_upload_file(file_path, destination) {
   if (string_starts_with(destination, "/")) {
     error();
   }
-  let service_account = await file_read_json("../firebase.json");
-  admin.initializeApp({
-    credential: admin.credential.cert(service_account),
-    storageBucket: "gs://letjesusbeexalted.appspot.com",
-  });
-  let bucket = admin.storage().bucket();
+  let bucket = await storage_bucket();
   await bucket.upload(file_path, {
     destination,
   });
