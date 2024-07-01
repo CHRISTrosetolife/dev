1b731fb98bc1218f8c84a141a5a69ce47abe5831
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 13:47:38 -0400

    2024-07-01T17:47:38.468Z function_auto_after storage_files

--- public/src/storage_files.mjs
+++ public/src/storage_files.mjs
@@ -3,6 +3,8 @@ import { string_starts_with } from "./string_starts_with.mjs";
 import { error } from "./error.mjs";
 import { list_single } from "./list_single.mjs";
 import { list_map_property } from "./list_map_property.mjs";
+import { list_sort_string } from "./list_sort_string.mjs";
+import { identity } from "./identity.mjs";
 export async function storage_files(prefix) {
   if (string_starts_with(prefix, "/")) {
     error();
@@ -13,5 +15,6 @@ export async function storage_files(prefix) {
   });
   let files = list_single(data);
   let result = list_map_property(files, "name");
+  list_sort_string(result, identity);
   return result;
 }
