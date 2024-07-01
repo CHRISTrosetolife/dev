5a7bd208394a08e44341063bf703cd3d634277c6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 13:42:23 -0400

    2024-07-01T17:42:22.880Z function_auto_after storage_bucket

--- public/src/storage_bucket.mjs
+++ public/src/storage_bucket.mjs
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 import { file_read_json } from "./file_read_json.mjs";
-export async function storage_bucket(file_path, destination) {
+export async function storage_bucket() {
   let service_account = await file_read_json("../firebase.json");
   admin.initializeApp({
     credential: admin.credential.cert(service_account),
