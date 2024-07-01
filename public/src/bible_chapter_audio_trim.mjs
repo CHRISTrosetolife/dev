572bfdfa1defdf44db3ac19a7ade39cd74277ee2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 17:42:50 -0400

    2024-07-01T21:42:50.548Z function_auto_after bible_chapter_audio_trim

--- public/src/bible_chapter_audio_trim.mjs
+++ public/src/bible_chapter_audio_trim.mjs
@@ -25,9 +25,8 @@ export async function bible_chapter_audio_trim(bible_folder, chapter_name) {
   let { fmt } = w;
   let samples = list_to(w.getSamples());
   let samples_out = [];
-  let threshold = 400;
-  let first = list_threshold_index(samples, threshold);
-  let last = list_threshold_index_reverse(samples, threshold);
+  let first = list_threshold_index(samples, 400);
+  let last = list_threshold_index_reverse(samples, 1000);
   each_index(samples, (item, index) => {
     if (first <= index && index <= last) {
       list_add(samples_out, item);
