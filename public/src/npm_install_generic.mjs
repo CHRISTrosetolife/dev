5ac4ec50397d36df476fcd4fdeaa7d776208a662
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 15:45:42 -0400

    2024-07-01T19:45:41.792Z function_auto_after npm_install_generic

--- public/src/npm_install_generic.mjs
+++ public/src/npm_install_generic.mjs
@@ -1,7 +1,7 @@
+import { command_line_exec } from "./command_line_exec.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { command_line } from "./command_line.mjs";
 export async function npm_install_generic(package_name, suffix) {
-  return await command_line(
+  return await command_line_exec(
     string_combine_multiple(["npm install ", package_name, "@latest", suffix]),
   );
 }
