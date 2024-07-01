181517b16e1911a9735ca0253f65f951a851b46c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 15:51:26 -0400

    2024-07-01T19:51:25.903Z function_auto_after bible_chapter_audio_to_wav

--- public/src/bible_chapter_audio_to_wav.mjs
+++ public/src/bible_chapter_audio_to_wav.mjs
@@ -1,5 +1,6 @@
 import { bible_chapter_audio_download } from "./bible_chapter_audio_download.mjs";
 import { list_first } from "./list_first.mjs";
+import ffmpeg from "fluent-ffmpeg";
 export async function bible_chapter_audio_to_wav(bible_folder, chapter_name) {
   let file_names = await bible_chapter_audio_download(
     bible_folder,
