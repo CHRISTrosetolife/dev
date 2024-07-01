15c8abef4c4a6eb6d5021b4baa137d3a81e8f36a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 17:32:35 -0400

    2024-07-01T21:32:34.672Z function_auto_after list_threshold_index

--- public/src/list_threshold_index.mjs
+++ public/src/list_threshold_index.mjs
@@ -2,7 +2,7 @@ import { each_index } from "./each_index.mjs";
 export function list_threshold_index(list, threshold) {
   let lambda_each_index = each_index;
   let index_first = null;
-  each_index(list, (item, index) => {
+  lambda_each_index(list, (item, index) => {
     if (item >= threshold) {
       index_first = index;
       return true;
