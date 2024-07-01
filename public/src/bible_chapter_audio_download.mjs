ecd9bc40e436a705786001e579d4352459c4e165
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 14:37:00 -0400

    2024-07-01T18:37:00.163Z function_auto_after bible_chapter_audio_download

--- public/src/bible_chapter_audio_download.mjs
+++ public/src/bible_chapter_audio_download.mjs
@@ -1,1 +1,4 @@
-export function bible_chapter_audio_download(bible_folder) {}
+import { folder_audio_bible } from "./folder_audio_bible.mjs";
+export function bible_chapter_audio_download(bible_folder) {
+  folder_audio_bible();
+}
