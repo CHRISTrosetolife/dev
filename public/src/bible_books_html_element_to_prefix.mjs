6b74c38171c456c9c3ef8a26c8a05a806db202bb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 14:43:58 -0400

    2024-07-01T18:43:57.412Z   t

--- public/src/bible_books_html_element_to_prefix.mjs
+++ public/src/bible_books_html_element_to_prefix.mjs
@@ -1,6 +1,6 @@
-import { bible_chapter_name_to_book } from "./bible_chapter_name_to_book.mjs";
+import { bible_chapter_name_to_book_code } from "./bible_chapter_name_to_book_code.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
 export function bible_books_html_element_to_prefix(e) {
   let href = html_parse_href(e);
-  return bible_chapter_name_to_book(href);
+  return bible_chapter_name_to_book_code(href);
 }
--- public/src/bible_chapter_name_to_book_code.mjs
+++ public/src/bible_chapter_name_to_book_code.mjs
@@ -1,4 +1,4 @@
 import { string_take } from "./string_take.mjs";
-export function bible_chapter_name_to_book(href) {
+export function bible_chapter_name_to_book_code(href) {
   return string_take(href, 3);
 }
