9c35bab8ef45e0c82ec3f877c4f3140d9ddc2b03
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 15:54:49 -0400

    2024-07-01T19:54:48.650Z function_auto_after bible_chapter_audio_to_wav

--- public/src/bible_chapter_audio_to_wav.mjs
+++ public/src/bible_chapter_audio_to_wav.mjs
@@ -15,14 +15,14 @@ export async function bible_chapter_audio_to_wav(bible_folder, chapter_name) {
     ffmpeg(first)
       .toFormat("wav")
       .on("error", (err) => {
-        console.log("An error occurred: " + err.message);
+        log("An error occurred: " + err.message);
         reject(err);
       })
       .on("progress", (progress) => {
-        console.log("Processing: " + progress.targetSize + " KB converted");
+        log("Processing: " + progress.targetSize + " KB converted");
       })
       .on("end", () => {
-        console.log("Processing finished !");
+        log("Processing finished !");
         resolve();
       })
       .save(path_join([folder, "last.wav"]));
