8f4d8887fff18f4f7d51e87e783d0048e8b63ca1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 13:41:15 -0400

    2024-07-01T17:41:15.140Z function_auto_after storage_upload_file

--- public/src/storage_upload_file.mjs
+++ public/src/storage_upload_file.mjs
@@ -3,15 +3,15 @@ import { file_read_json } from "./file_read_json.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { error } from "./error.mjs";
 export async function storage_upload_file(file_path, destination) {
+  if (string_starts_with(destination, "/")) {
+    error();
+  }
   let service_account = await file_read_json("../firebase.json");
   admin.initializeApp({
     credential: admin.credential.cert(service_account),
     storageBucket: "gs://letjesusbeexalted.appspot.com",
   });
   let bucket = admin.storage().bucket();
-  if (string_starts_with(destination, "/")) {
-    error();
-  }
   await bucket.upload(file_path, {
     destination,
   });
