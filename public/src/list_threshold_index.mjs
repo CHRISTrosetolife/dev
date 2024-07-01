8d28aa5887c5abe95fe09987aedf0f45db03d1c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-01 17:39:19 -0400

    2024-07-01T21:39:18.564Z function_auto_after list_threshold_index

--- public/src/list_threshold_index.mjs
+++ public/src/list_threshold_index.mjs
@@ -2,5 +2,5 @@ import { list_threshold_index_generic } from "./list_threshold_index_generic.mjs
 import { each_index } from "./each_index.mjs";
 export function list_threshold_index(list, threshold) {
   let lambda_each_index = each_index;
-  list_threshold_index_generic(lambda_each_index, list, threshold);
+  return list_threshold_index_generic(lambda_each_index, list, threshold);
 }
