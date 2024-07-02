0cd49b999565e5383a498e4780019c59e26ee9e4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:21:26 -0400

    2024-07-02T17:21:25.489Z function_auto_after js_param_move_first

--- public/src/js_param_move_first.mjs
+++ public/src/js_param_move_first.mjs
@@ -7,10 +7,15 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function js_param_move_first(ast, function_name, param_index) {
   assert_arguments_length(arguments, 3);
   assert(string_is, [param_index]);
-  js_param_generic(ast, function_name, function each_caller(args) {
-    list_add(args, default_value);
-  });
-  js_param_actual(ast, function_name, (params) => {
-    list_add(params, param_new);
-  });
+  js_param_generic(
+    ast,
+    function_name,
+    function each_caller(args) {
+      list_add(args, default_value);
+    },
+    (params) => {
+      list_add(params, param_new);
+    },
+  );
+  js_param_actual(ast, function_name);
 }
