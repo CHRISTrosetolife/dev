be7ef60b147fbf8454e6954971affc048a666f8b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 15:36:33 -0400

    2024-07-02T19:36:32.484Z function_auto_after bible_engbsb_book_upload

--- public/src/bible_engbsb_book_upload.mjs
+++ public/src/bible_engbsb_book_upload.mjs
@@ -1,1 +1,4 @@
-export function bible_engbsb_book_upload(book_name) {}
+import { bible_engbsb_chapter_upload } from "./bible_engbsb_chapter_upload.mjs";
+export function bible_engbsb_book_upload(book_name) {
+  bible_engbsb_chapter_upload;
+}
