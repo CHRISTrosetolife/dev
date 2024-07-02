32844fdb7f6b2df61e8f12ed3f32c67fd904792c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 16:38:08 -0400

    2024-07-02T20:38:07.835Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -16,5 +16,5 @@ export async function sandbox_2() {
   ctx.lineTo(50 + text.width, 102);
   ctx.stroke();
   let buffer = canvas.toBuffer("image/png");
-  await file_overwrite_binary(folder_gitignore_path("test.png"));
+  await file_overwrite_binary(folder_gitignore_path("test.png"), buffer);
 }
