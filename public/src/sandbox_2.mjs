7f483674757d3654b5b74ca2729338cae4faa29c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 18:42:21 -0400

    2024-07-02T22:42:21.454Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -89,7 +89,7 @@ export async function sandbox_2() {
   let height_total = list_property_sum(lines, "height");
   assert(less_than_equal, [height_total, canvas_height]);
   each(lines, (line) => {
-    ctx.fillText(reference, 0, 215);
+    ctx.fillText(line.text, 0, 215);
   });
   let buffer = canvas.toBuffer("image/png");
   let output_path = folder_gitignore_path("test.png");
