37f132a263aab08d3af2d81d0018cfdfa5b1dc85
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:50:48 -0400

    2024-07-02T17:50:47.655Z function_auto_after bible_chapter_audio_download_prefix

--- public/src/bible_chapter_audio_download_prefix.mjs
+++ public/src/bible_chapter_audio_download_prefix.mjs
@@ -1,8 +1,10 @@
+import { string_trim_front } from "./string_trim_front.mjs";
 import { bible_chapter_name_parse } from "./bible_chapter_name_parse.mjs";
 import { folder_audio_bible } from "./folder_audio_bible.mjs";
 import { path_join } from "./path_join.mjs";
 export function bible_chapter_audio_download_prefix(folder_name, chapter_name) {
   let { book_code, chapter_code } = bible_chapter_name_parse(chapter_name);
+  let chapter_number = string_trim_front(chapter_code, "0");
   return path_join([
     folder_audio_bible(),
     folder_name,
