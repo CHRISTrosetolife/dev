e2dca205a084b1e53de02893256381303cfa5c00
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 15:47:16 -0400

    2024-07-02T19:47:16.524Z function_auto_after bible_engbsb_book_upload

--- public/src/bible_engbsb_book_upload.mjs
+++ public/src/bible_engbsb_book_upload.mjs
@@ -1,13 +1,8 @@
 import { each_async } from "./each_async.mjs";
-import { each_range } from "./each_range.mjs";
 import { bible_chapters } from "./bible_chapters.mjs";
 import { bible_engbsb_chapter_upload } from "./bible_engbsb_chapter_upload.mjs";
-import { list_remove_first } from "./list_remove_first.mjs";
 export async function bible_engbsb_book_upload(book_name) {
   let chapters = await bible_chapters("engbsb", book_name);
-  each_range(2, (item) => {
-    list_remove_first(chapters);
-  });
   await each_async(chapters, async (chapter) => {
     await bible_engbsb_chapter_upload(chapter);
   });
