d65e35a658928e1017e326c2d0189314f763bc4f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 10:26:54 -0400

    2024-07-02T14:26:52.415Z function_auto_after storage_upload_file

--- public/src/storage_upload_file.mjs
+++ public/src/storage_upload_file.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { storage_bucket } from "./storage_bucket.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { error } from "./error.mjs";
@@ -9,4 +10,5 @@ export async function storage_upload_file(file_path, destination) {
   await bucket.upload(file_path, {
     destination,
   });
+  log({});
 }
