55e2127391a173d5dc6f702e33ffc0726762f2d9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:40:33 -0400

    2024-07-02T17:40:33.011Z function_auto_after js_param_new_at

--- public/src/js_param_new_at.mjs
+++ public/src/js_param_new_at.mjs
@@ -1,3 +1,6 @@
+import { number_is } from "./number_is.mjs";
+import { assert } from "./assert.mjs";
+import { integer_parse } from "./integer_parse.mjs";
 import { list_insert } from "./list_insert.mjs";
 import { js_param_new_generic } from "./js_param_new_generic.mjs";
 export async function js_param_new_at(
@@ -6,6 +9,8 @@ export async function js_param_new_at(
   param_name,
   default_value_string,
 ) {
+  let param_index = integer_parse(param_index);
+  assert(number_is, [param_index]);
   await js_param_new_generic(
     ast,
     function_name,
