e062f7d032db58b7ca28d9da257140913a203269
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 15:41:36 -0400

    2024-07-02T19:41:35.799Z function_auto_after list_remove_first

--- public/src/list_remove_first.mjs
+++ public/src/list_remove_first.mjs
@@ -1,4 +1,4 @@
 import { list_remove_at } from "./list_remove_at.mjs";
-export function list_remove_first(english) {
-  return list_remove_at(english, 0);
+export function list_remove_first(list) {
+  return list_remove_at(list, 0);
 }
