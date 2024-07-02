93dd87b8122a8e961f138b844020f75586ba1748
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 17:43:11 -0400

    2024-07-02T21:43:11.160Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -62,7 +62,7 @@ export async function sandbox_2() {
       }
     });
   });
-  list_map(tokens);
+  list_map(tokens, ctx.measureText);
   ctx.fillText(reference, 0, 215);
   let buffer = canvas.toBuffer("image/png");
   let output_path = folder_gitignore_path("test.png");
