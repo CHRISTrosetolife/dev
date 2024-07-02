b49b1bf211a6f8d4f259dbe7a7143b4334f79aca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:46:48 -0400

    2024-07-02T17:46:47.491Z function_auto_after list_index_last

--- public/src/list_index_last.mjs
+++ public/src/list_index_last.mjs
@@ -4,6 +4,6 @@ import { list_size } from "./list_size.mjs";
 export function list_index_last(list) {
   let length = list_size(list);
   let index_last = length - 1;
-  assert(greater_than_equal, [index_last, list]);
+  assert(greater_than_equal, [index_last, 0]);
   return index_last;
 }
