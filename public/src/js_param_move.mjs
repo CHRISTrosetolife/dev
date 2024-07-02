47483d8cd9ff30a764492c47c8c469e5b26b9ebf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:32:51 -0400

    2024-07-02T17:32:51.291Z function_auto_after js_param_move

--- public/src/js_param_move.mjs
+++ public/src/js_param_move.mjs
@@ -4,8 +4,13 @@ import { assert } from "./assert.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { integer_parse } from "./integer_parse.mjs";
 import { number_is } from "./number_is.mjs";
-export async function js_param_move(ast, function_name, param_index) {
-  assert_arguments_length(arguments, 3);
+export async function js_param_move(
+  ast,
+  function_name,
+  param_index,
+  param_index_new,
+) {
+  assert_arguments_length(arguments, 4);
   param_index = integer_parse(param_index);
   assert(number_is, [param_index]);
   js_param_generic(
