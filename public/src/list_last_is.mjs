98c64c4d67e86ee5bed6e8840c338fc8ac829932
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 14:18:05 -0400

    2024-07-02T18:18:05.272Z function_auto_after list_last_is

--- public/src/list_last_is.mjs
+++ public/src/list_last_is.mjs
@@ -1,5 +1,5 @@
 import { list_index_last } from "./list_index_last.mjs";
 import { list_index } from "./list_index.mjs";
 export function list_last_is(verses, verse) {
-  return list_index(verse) === list_index_last(verses);
+  return list_index(verses, verse) === list_index_last(verses);
 }
