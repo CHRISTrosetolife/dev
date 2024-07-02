804db3f6ea2b8aec3e0e026439fd558a5c782877
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:21:45 -0400

    2024-07-02T17:21:44.578Z function_auto_after js_param_actual

--- public/src/js_param_actual.mjs
+++ public/src/js_param_actual.mjs
@@ -1,12 +1,1 @@
-import { js_declaration_single } from "./js_declaration_single.mjs";
-import { equal } from "./equal.mjs";
-import { js_declaration_single_name } from "./js_declaration_single_name.mjs";
-export function js_param_actual(ast, function_name, lambda_if_match) {
-  let name = js_declaration_single_name(ast);
-  if (!equal(name, function_name)) {
-    return;
-  }
-  let declaration = js_declaration_single(ast);
-  let { params } = declaration;
-  lambda_if_match(params);
-}
+export function js_param_actual(ast, function_name, lambda_if_match) {}
