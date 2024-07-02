dc58b8f3851a1025c612b94e2711b9e168d8e894
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:18:31 -0400

    2024-07-02T17:18:31.485Z function_auto_after js_param_move_first

--- public/src/js_param_move_first.mjs
+++ public/src/js_param_move_first.mjs
@@ -2,23 +2,17 @@ import { js_param_actual } from "./js_param_actual.mjs";
 import { js_param_existing_each } from "./js_param_existing_each.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { list_add } from "./list_add.mjs";
-import { js_imports_add } from "./js_imports_add.mjs";
 import { assert } from "./assert.mjs";
 import { string_is } from "./string_is.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function js_param_move_first(ast, function_name, param_name) {
   assert_arguments_length(arguments, 3);
   assert(string_is, [param_name]);
-  assert(string_is, [default_value_string]);
-  let needs_imports_add = false;
   js_param_existing_each(ast, function_name, function lambda(args) {
     let default_value = js_parse_expression(default_value_string);
     list_add(args, default_value);
-    needs_imports_add = true;
+    let needs_imports_add = true;
   });
-  if (needs_imports_add) {
-    await js_imports_add(ast);
-  }
   js_param_actual(ast, function_name, (params) => {
     let param_new = js_parse_expression(param_name);
     list_add(params, param_new);
