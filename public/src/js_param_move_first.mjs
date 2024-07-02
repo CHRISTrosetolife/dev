6388980c342b4a515b7e15ab8fa3b7ffc021f5d8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:16:31 -0400

    2024-07-02T17:16:31.228Z function_auto_after js_param_move_first

--- public/src/js_param_move_first.mjs
+++ public/src/js_param_move_first.mjs
@@ -1,7 +1,5 @@
-import { js_declaration_single_name } from "./js_declaration_single_name.mjs";
-import { js_declaration_single } from "./js_declaration_single.mjs";
-import { equal } from "./equal.mjs";
-import { js_node_type } from "./js_node_type.mjs";
+import { js_param_actual } from "./js_param_actual.mjs";
+import { js_param_existing_each } from "./js_param_existing_each.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { list_add } from "./list_add.mjs";
 import { js_imports_add } from "./js_imports_add.mjs";
@@ -13,31 +11,15 @@ export async function js_param_move_first(ast, function_name, param_name) {
   assert(string_is, [param_name]);
   assert(string_is, [default_value_string]);
   let needs_imports_add = false;
-  let nodes = js_node_type(ast, "CallExpression");
-  for (let node of nodes) {
-    let { callee } = node;
-    let { type } = callee;
-    if (!equal(type, "Identifier")) {
-      continue;
-    }
-    let { name } = callee;
-    if (!equal(name, function_name)) {
-      continue;
-    }
-    let { arguments: args } = node;
+  js_param_existing_each(ast, function_name, function lambda(args) {
     let default_value = js_parse_expression(default_value_string);
     list_add(args, default_value);
     needs_imports_add = true;
-  }
+  });
   if (needs_imports_add) {
     await js_imports_add(ast);
   }
-  let name = js_declaration_single_name(ast);
-  if (!equal(name, function_name)) {
-    return;
-  }
-  let declaration = js_declaration_single(ast);
-  let { params } = declaration;
+  let params = js_param_actual(ast, function_name);
   let param_new = js_parse_expression(param_name);
   list_add(params, param_new);
 }
