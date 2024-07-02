88fd3c7e92be2a997f98fb576664751e62874fdd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 16:49:24 -0400

    2024-07-02T20:49:23.468Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,3 +1,4 @@
+import { file_read_binary } from "./file_read_binary.mjs";
 import { file_overwrite_binary } from "./file_overwrite_binary.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { createCanvas, loadImage } from "canvas";
@@ -5,6 +6,7 @@ import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 export async function sandbox_2() {
   assert_arguments_length(arguments, 0);
   let canvas = createCanvas(1080, 1920);
+  await file_read_binary();
   loadImage();
   let ctx = canvas.getContext("2d");
   ctx.font = "30px Arial";
