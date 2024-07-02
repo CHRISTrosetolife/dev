a87c5789710a364303ee326122b38a240bceecf9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 16:36:48 -0400

    2024-07-02T20:36:47.682Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,4 +1,15 @@
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
-export async function sandbox_2(a) {
-  assert_arguments_length(arguments, 1);
+export async function sandbox_2() {
+  assert_arguments_length(arguments, 0);
+  let canvas = createCanvas(200, 200);
+  let ctx = canvas.getContext("2d");
+  ctx.font = "30px Impact";
+  ctx.rotate(0.1);
+  ctx.fillText("Awesome!", 50, 100);
+  let text = ctx.measureText("Awesome!");
+  ctx.strokeStyle = "rgba(0,0,0,0.5)";
+  ctx.beginPath();
+  ctx.lineTo(50, 102);
+  ctx.lineTo(50 + text.width, 102);
+  ctx.stroke();
 }
