260d269386fbd072868063e9ca60bbe1a77979f7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 17:16:17 -0400

    2024-07-02T21:16:16.780Z function_auto_after bible_book_name

--- public/src/bible_book_name.mjs
+++ public/src/bible_book_name.mjs
@@ -2,6 +2,9 @@ import { object_property_get } from "./object_property_get.mjs";
 import { string_case_upper } from "./string_case_upper.mjs";
 import { bible_books_prefix_to_name } from "./bible_books_prefix_to_name.mjs";
 export function bible_book_name(book_id) {
-  object_property_get(object, property_name);
-  return bible_books_prefix_to_name()[string_case_upper(book_id)];
+  return object_property_get(
+    bible_books_prefix_to_name(),
+    string_case_upper(book_id),
+  );
+  return [];
 }
