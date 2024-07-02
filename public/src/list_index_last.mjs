ed4a191e3bb420c05b4cf2dc074309db1b26c71c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:46:41 -0400

    2024-07-02T17:46:40.910Z function_auto_after list_index_last

--- public/src/list_index_last.mjs
+++ public/src/list_index_last.mjs
@@ -1,6 +1,9 @@
+import { assert } from "./assert.mjs";
+import { greater_than_equal } from "./greater_than_equal.mjs";
 import { list_size } from "./list_size.mjs";
 export function list_index_last(list) {
   let length = list_size(list);
   let index_last = length - 1;
+  assert(greater_than_equal, [index_last, list]);
   return index_last;
 }
