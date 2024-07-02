fac9ad02ab4c23d97f93ed74c15a6bdb92ba55e2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:23:14 -0400

    2024-07-02T17:23:13.829Z function_auto_after js_param_move_first

--- public/src/js_param_move_first.mjs
+++ public/src/js_param_move_first.mjs
@@ -3,8 +3,10 @@ import { list_add } from "./list_add.mjs";
 import { assert } from "./assert.mjs";
 import { string_is } from "./string_is.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
+import { integer_parse } from "./integer_parse.mjs";
 export async function js_param_move_first(ast, function_name, param_index) {
   assert_arguments_length(arguments, 3);
+  param_index = integer_parse(param_index);
   assert(string_is, [param_index]);
   js_param_generic(
     ast,
