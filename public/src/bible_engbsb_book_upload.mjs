4a5422420a58fe7c5c029abc601600d7ee085e90
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 15:42:48 -0400

    2024-07-02T19:42:47.367Z function_auto_after bible_engbsb_book_upload

--- public/src/bible_engbsb_book_upload.mjs
+++ public/src/bible_engbsb_book_upload.mjs
@@ -8,6 +8,7 @@ export async function bible_engbsb_book_upload(book_name) {
   each_range(2, (item) => {
     list_remove_first(chapters);
   });
-  await each_async(list, async (item2) => {});
-  await bible_engbsb_chapter_upload(chapters);
+  await each_async(chapters, async (chapter) => {
+    await bible_engbsb_chapter_upload(chapter);
+  });
 }
