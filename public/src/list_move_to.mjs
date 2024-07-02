8dc08635899ab4c389676a4cde9c59e2806181a6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:32:32 -0400

    2024-07-02T17:32:31.730Z function_auto_after list_move_to

--- public/src/list_move_to.mjs
+++ public/src/list_move_to.mjs
@@ -1,6 +1,6 @@
 import { list_insert } from "./list_insert.mjs";
 import { list_remove_at } from "./list_remove_at.mjs";
-export function list_move_to(list, param_index) {
-  let item = list_remove_at(list, param_index);
-  list_insert(list, item);
+export function list_move_to(list, index_from, index_to) {
+  let item = list_remove_at(list, index_from);
+  list_insert(list, index_to, item);
 }
