8f067978556ffa884873239047042410395c6a71
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:37:44 -0400

    2024-07-02T17:37:43.708Z   t

--- public/src/js_param_new.mjs
+++ public/src/js_param_new.mjs
@@ -2,9 +2,9 @@ import { js_param_new_generic } from "./js_param_new_generic.mjs";
 import { list_add } from "./list_add.mjs";
 export async function js_param_new(
   ast,
-  default_value_string,
   function_name,
   param_name,
+  default_value_string,
 ) {
   await js_param_new_generic(
     ast,
