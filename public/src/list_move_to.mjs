7ddc9bbee2c05fa45bfc1a166a2eeffebf8155ff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:32:15 -0400

    2024-07-02T17:32:15.018Z function_auto_after list_move_to

--- public/src/list_move_to.mjs
+++ public/src/list_move_to.mjs
@@ -1,6 +1,6 @@
-import { list_add_beginning } from "./list_add_beginning.mjs";
+import { list_insert } from "./list_insert.mjs";
 import { list_remove_at } from "./list_remove_at.mjs";
 export function list_move_to(list, param_index) {
   let item = list_remove_at(list, param_index);
-  list_add_beginning(list, item);
+  list_insert(list, item);
 }
