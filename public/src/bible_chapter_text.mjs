6c4411b91a59b0a1bd396eb3f816758b707a66d3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 19:17:17 -0400

    2024-07-02T23:17:16.576Z function_auto_after bible_chapter_text

--- public/src/bible_chapter_text.mjs
+++ public/src/bible_chapter_text.mjs
@@ -1,4 +1,4 @@
 import { bible_chapter } from "./bible_chapter.mjs";
 export async function bible_chapter_text(bible_folder, chapter_name) {
-  await bible_chapter(bible_folder, chapter_name);
+  let verses = await bible_chapter(bible_folder, chapter_name);
 }
