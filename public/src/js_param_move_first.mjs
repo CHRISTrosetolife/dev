bd02338922caf20fd9a12d6f05e3421745b73a64
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:19:27 -0400

    2024-07-02T17:19:26.725Z function_auto_after js_param_move_first

--- public/src/js_param_move_first.mjs
+++ public/src/js_param_move_first.mjs
@@ -5,14 +5,14 @@ import { list_add } from "./list_add.mjs";
 import { assert } from "./assert.mjs";
 import { string_is } from "./string_is.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
-export async function js_param_move_first(ast, function_name, param_name) {
+export async function js_param_move_first(ast, function_name, param_index) {
   assert_arguments_length(arguments, 3);
-  assert(string_is, [param_name]);
+  assert(string_is, [param_index]);
   js_param_existing_each(ast, function_name, function lambda(args) {
     list_add(args, default_value);
   });
   js_param_actual(ast, function_name, (params) => {
-    let param_new = js_parse_expression(param_name);
+    let param_new = js_parse_expression(param_index);
     list_add(params, param_new);
   });
 }
