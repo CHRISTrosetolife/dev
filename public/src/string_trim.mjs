cc257a6b8b96be1085d94c368bbf648abf4604b9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:50:20 -0400

    2024-07-02T17:50:20.142Z   t

--- public/src/string_trim.mjs
+++ public/src/string_trim.mjs
@@ -3,7 +3,7 @@ import { list_to } from "./list_to.mjs";
 import { string_take_less_1 } from "./string_take_less_1.mjs";
 import { string_ends_with_multiple } from "./string_ends_with_multiple.mjs";
 export function string_trim(t, symbols) {
-  t = string_trim_front(symbols, t);
+  t = string_trim_front(t, symbols);
   symbols = list_to(symbols);
   while (string_ends_with_multiple(t, symbols)) {
     t = string_take_less_1(t);
--- public/src/string_trim_front.mjs
+++ public/src/string_trim_front.mjs
@@ -1,7 +1,7 @@
 import { string_skip_1 } from "./string_skip_1.mjs";
 import { string_starts_with_multiple } from "./string_starts_with_multiple.mjs";
 import { list_to } from "./list_to.mjs";
-export function string_trim_front(symbols, t) {
+export function string_trim_front(t, symbols) {
   symbols = list_to(symbols);
   while (string_starts_with_multiple(t, symbols)) {
     t = string_skip_1(t);
