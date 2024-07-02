8b75d8d1bfbe5b07a2c762d0a869ec6368fc7d0f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 17:17:27 -0400

    2024-07-02T21:17:27.145Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -11,8 +11,8 @@ import { number_max } from "./number_max.mjs";
 export async function sandbox_2() {
   assert_arguments_length(arguments, 0);
   let book_id = "MAT";
-  let chapter = 1;
-  let verse_number = 1;
+  let chapter = "1";
+  let verse_number = "1";
   let book_name = bible_book_name(book_id);
   bible_reference(book_name, chapter, verse_number);
   let width = 1080;
