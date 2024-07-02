be510df02b165b92e5dc52fe057290cac5285cd7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 16:59:17 -0400

    2024-07-02T20:59:17.013Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -21,7 +21,7 @@ export async function sandbox_2() {
     bigger,
     bigger,
   );
-  ctx.fillStyle = "rgba(225,225,225,0.5)";
+  ctx.fillStyle = "rgba(0,0,0,0.5)";
   ctx.fillRect(0, 0, width, 1920);
   ctx.font = "300px Arial";
   ctx.fillText("Awesome!", 0, 0);
