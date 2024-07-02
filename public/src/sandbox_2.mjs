86523c87ec25987ae584f0df2b888913f3078b13
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 16:51:07 -0400

    2024-07-02T20:51:07.006Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -10,7 +10,7 @@ export async function sandbox_2() {
   let image = await loadImage(data);
   ctx.drawImage(image, 50, 0, 70, 70);
   let ctx = canvas.getContext("2d");
-  ctx.font = "30px Arial";
+  ctx.font = "300px Arial";
   ctx.fillText("Awesome!", 0, 0);
   let text = ctx.measureText("Awesome!");
   let buffer = canvas.toBuffer("image/png");
