f8e9aaf6226bfdee87f3646ca1044389493c0254
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:19:23 -0400

    2024-07-02T17:19:22.927Z function_auto_after function_param_move_first

--- public/src/function_param_move_first.mjs
+++ public/src/function_param_move_first.mjs
@@ -2,14 +2,14 @@ import { js_param_move_first } from "./js_param_move_first.mjs";
 import { data_identifiers_each } from "./data_identifiers_each.mjs";
 import { function_transform_args_split_lambda } from "./function_transform_args_split_lambda.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
-export async function function_param_move_first(function_name, param_name) {
+export async function function_param_move_first(function_name, param_index) {
   assert_arguments_length(arguments, 2);
   await data_identifiers_each(function_name, lambda);
   async function lambda(fn_name) {
     await function_transform_args_split_lambda(
       fn_name,
       [js_param_move_first],
-      [function_name, param_name],
+      [function_name, param_index],
     );
   }
 }
