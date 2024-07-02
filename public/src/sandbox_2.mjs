a8ce5ed717ce2a70347d0d4a8707e04f2a308ac9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 16:50:53 -0400

    2024-07-02T20:50:52.148Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -7,7 +7,7 @@ export async function sandbox_2() {
   assert_arguments_length(arguments, 0);
   let canvas = createCanvas(1080, 1920);
   let data = await file_read_binary("./img/bible_green.png");
-  loadImage(data);
+  let image = await loadImage(data);
   let ctx = canvas.getContext("2d");
   ctx.font = "30px Arial";
   ctx.fillText("Awesome!", 0, 0);
