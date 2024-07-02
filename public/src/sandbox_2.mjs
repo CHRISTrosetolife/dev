22f5985ed5298cc0c495448613193d3c0604acbf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 17:31:36 -0400

    2024-07-02T21:31:35.553Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -45,7 +45,7 @@ export async function sandbox_2() {
   ctx.font = "300px Arial";
   ctx.fillStyle = "white";
   let text = ctx.measureText(reference);
-  let width = text;
+  let { width } = text;
   log({
     text,
   });
