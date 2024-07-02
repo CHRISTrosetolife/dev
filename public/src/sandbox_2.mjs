c3941f48423106b29c254bff5b58709d4cc471ff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 16:41:19 -0400

    2024-07-02T20:41:18.733Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -4,7 +4,7 @@ import { createCanvas } from "canvas";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 export async function sandbox_2() {
   assert_arguments_length(arguments, 0);
-  let canvas = createCanvas(200, 200);
+  let canvas = createCanvas(1080, 1920);
   let ctx = canvas.getContext("2d");
   ctx.font = "30px Impact";
   ctx.rotate(0.1);
