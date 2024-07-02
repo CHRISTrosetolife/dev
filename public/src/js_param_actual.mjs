1fc5536ed55344343d3559d69517b91fe3d40ced
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 13:17:51 -0400

    2024-07-02T17:17:51.093Z function_auto_after js_param_actual

--- public/src/js_param_actual.mjs
+++ public/src/js_param_actual.mjs
@@ -1,7 +1,7 @@
 import { js_declaration_single } from "./js_declaration_single.mjs";
 import { equal } from "./equal.mjs";
 import { js_declaration_single_name } from "./js_declaration_single_name.mjs";
-export function js_param_actual(ast, function_name) {
+export function js_param_actual(ast, function_name, lambda) {
   let name = js_declaration_single_name(ast);
   if (!equal(name, function_name)) {
     return;
