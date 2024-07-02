048bdec607cafdbecab19f733c7c73f3e827f67b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 18:48:16 -0400

    2024-07-02T22:48:16.186Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -96,7 +96,7 @@ export async function sandbox_2() {
     let padding = line_height_to_padding(line);
     let offset = list_map_sum(
       list_take(lines, index + 1),
-      (line) => line.height + line_height_to_padding_double(line),
+      (line) => line.height + line_height_to_padding_double(line.height),
     );
     ctx.fillText(line.text, padding, offset);
   });
@@ -104,10 +104,10 @@ export async function sandbox_2() {
   let output_path = folder_gitignore_path("test.png");
   await file_overwrite_binary(output_path, buffer);
   await file_open(output_path);
-  function line_height_to_padding_double(line) {
-    return 2 * line_height_to_padding(line.height);
+  function line_height_to_padding_double(height) {
+    return 2 * line_height_to_padding(height);
   }
-  function line_height_to_padding(line) {
-    return line.height / 10;
+  function line_height_to_padding(height) {
+    return height / 10;
   }
 }
