7e8ee6c81c93f327b84332c116f8c7bea3e9bce5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 14:43:14 -0400

    2024-07-03T18:43:14.426Z function_auto_after bible_chapter_video

--- public/src/bible_chapter_video.mjs
+++ public/src/bible_chapter_video.mjs
@@ -1,5 +1,7 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { bible_chapter_videos } from "./bible_chapter_videos.mjs";
 import video_stitch from "video-stitch";
+import { list_map } from "./list_map.mjs";
 export async function bible_chapter_video(
   project_name,
   bible_folder,
@@ -10,6 +12,7 @@ export async function bible_chapter_video(
     bible_folder,
     chapter_name,
   );
+  let m = list_map(videos, (v) => object_property_get(object, property_name));
   return new Promise((resolve) => {
     video_stitch
       .concat({
