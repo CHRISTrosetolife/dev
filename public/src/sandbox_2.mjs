c0c102f3bda17617f632846613654663b68b683b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 11:39:24 -0400

    2024-07-03T15:39:23.021Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,6 +1,6 @@
 import { bible_image_path } from "./bible_image_path.mjs";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
-import videoshow from "videoshow";
+import { videoshow } from "videoshow";
 export function sandbox_2() {
   return new Promise((resolve, reject) => {
     videoshow([bible_image_path()])
