0923408d21275e037a8a2b8a78edddcc2789470f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 11:35:11 -0400

    2024-07-03T15:35:11.270Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,1 +1,6 @@
-export function sandbox_2() {}
+export function sandbox_2() {
+  videoshow(["image1.jpg", "image2.jpg", "image3.jpg"])
+    .save("video.mp4")
+    .on("error", function () {})
+    .on("end", function () {});
+}
