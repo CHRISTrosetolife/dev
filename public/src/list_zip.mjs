e86f97e6a53c40e620dc18e10223c31828e01d22
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 12:07:27 -0400

    2024-07-03T16:07:26.570Z function_auto_after list_zip

--- public/src/list_zip.mjs
+++ public/src/list_zip.mjs
@@ -1,3 +1,4 @@
+import { list_adder } from "./list_adder.mjs";
 import { equal_by } from "./equal_by.mjs";
 import { assert } from "./assert.mjs";
 import { each_pairs } from "./each_pairs.mjs";
@@ -5,6 +6,7 @@ import { list_map } from "./list_map.mjs";
 import { list_size } from "./list_size.mjs";
 export function list_zip(lists, lambda) {
   let sizes = list_map(lists, list_size);
+  let compares = list_adder((la) => {});
   each_pairs(sizes, (a, b) => {
     assert(equal_by, [a, b], list_size);
   });
