a9a5331dae4538fe850a553479ca87dd9eb0441a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 12:22:33 -0400

    2024-07-03T16:22:33.255Z function_auto_after list_zip

--- public/src/list_zip.mjs
+++ public/src/list_zip.mjs
@@ -1,3 +1,4 @@
+import { range } from "./range.mjs";
 import { true_is } from "./true_is.mjs";
 import { assert } from "./assert.mjs";
 import { lists_sizes_equal } from "./lists_sizes_equal.mjs";
@@ -8,7 +9,7 @@ import { list_get } from "./list_get.mjs";
 export function list_zip(lists) {
   let same_sizes = lists_sizes_equal(lists);
   assert(true_is, [same_sizes]);
-  return list_map(list_size(list_first(lists)), (index) => {
+  return list_map(range(list_size(list_first(lists))), (index) => {
     let item = list_map(lists, (list) => list_get(list, index));
     return item;
   });
