20bbb23102eda399af31136d3fca63678179fcd3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 15:48:38 -0400

    2024-07-03T19:48:37.378Z function_auto_after bible_chapter_images

--- public/src/bible_chapter_images.mjs
+++ public/src/bible_chapter_images.mjs
@@ -7,6 +7,6 @@ export async function bible_chapter_images(bible_folder, chapter_name) {
   let verses = await bible_chapter(bible_folder, chapter_name);
   return await list_map_async(verses, async (verse) => {
     let { verse_number } = verse;
-    await bible_verse_images(bible_folder, chapter_name, verse_number);
+    return await bible_verse_images(bible_folder, chapter_name, verse_number);
   });
 }
