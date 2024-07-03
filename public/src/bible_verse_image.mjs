2b50d16562f27c9c24892bd9f094adc84ac11bb9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 15:46:57 -0400

    2024-07-03T19:46:56.363Z function_auto_after bible_verse_image

--- public/src/bible_verse_image.mjs
+++ public/src/bible_verse_image.mjs
@@ -61,103 +61,104 @@ export async function bible_verse_image(
     path: result_path,
   };
   let hv = list_second(hvs);
-  await each_async(list, async (item) => {});
-  let path_image = path_join([
-    output_path_folder,
-    verse_number,
-    string_combine_multiple([object_property_get(hv, "name"), ".png"]),
-  ]);
-  object_merge(result_path, {
-    [hv]: path_image,
-  });
-  if (await file_exists(path_image)) {
-    result;
-  }
-  log({
-    match,
-  });
-  let tokens = object_property_get(match, "tokens");
-  let book_name = bible_book_name(book_code);
-  let reference = bible_reference(book_name, chapter_code, verse_number);
-  list_add_beginning(tokens, reference);
-  let canvas_width = object_property_get(hv, "width");
-  let canvas_height = object_property_get(hv, "height");
-  let bigger = number_max(canvas_width, canvas_height);
-  let canvas = createCanvas(canvas_width, canvas_height);
-  let ctx = canvas.getContext("2d");
-  let data = await file_read_binary(bible_image_path());
-  let image = await loadImage(data);
-  ctx.drawImage(
-    image,
-    -(bigger - canvas_width) / 2,
-    -(bigger - canvas_height) / 2,
-    bigger,
-    bigger,
-  );
-  ctx.fillStyle = "rgba(0,0,0,0.65)";
-  ctx.fillRect(0, 0, canvas_width, canvas_height);
-  ctx.fillStyle = "white";
-  let font_size_px_max = 300;
-  let font_size_px = font_size_px_max;
-  each(tokens, (token) => {
-    each_range_reverse(font_size_px + 1, (i) => {
-      font_size_px = i;
-      ctx.font = string_combine_multiple([font_size_px, "px Arial"]);
-      let measured = ctx.measureText(token);
-      let { width, actualBoundingBoxAscent: height } = measured;
-      let padding = line_height_to_padding_double(height);
-      if (width <= canvas_width - padding) {
-        return true;
-      }
+  await each_async(hvs, async (hv) => {
+    let path_image = path_join([
+      output_path_folder,
+      verse_number,
+      string_combine_multiple([object_property_get(hv, "name"), ".png"]),
+    ]);
+    object_merge(result_path, {
+      [hv]: path_image,
     });
-  });
-  let lines = null;
-  let offset_height = 0;
-  each_range_reverse(font_size_px + 1, (i) => {
-    lines = [];
-    font_size_px = i;
-    ctx.font = string_combine_multiple([font_size_px, "px Arial"]);
-    let index_current = 0;
-    while (index_current < list_size(tokens)) {
-      each_range_reverse(list_size(tokens) - index_current, (count) => {
-        let sliced = list_slice(
-          tokens,
-          index_current,
-          index_current + count + 1,
-        );
-        let sliced_text = list_join_space(sliced);
-        let measured = ctx.measureText(sliced_text);
+    if (await file_exists(path_image)) {
+      result;
+    }
+    log({
+      match,
+    });
+    let tokens = object_property_get(match, "tokens");
+    let book_name = bible_book_name(book_code);
+    let reference = bible_reference(book_name, chapter_code, verse_number);
+    list_add_beginning(tokens, reference);
+    let canvas_width = object_property_get(hv, "width");
+    let canvas_height = object_property_get(hv, "height");
+    let bigger = number_max(canvas_width, canvas_height);
+    let canvas = createCanvas(canvas_width, canvas_height);
+    let ctx = canvas.getContext("2d");
+    let data = await file_read_binary(bible_image_path());
+    let image = await loadImage(data);
+    ctx.drawImage(
+      image,
+      -(bigger - canvas_width) / 2,
+      -(bigger - canvas_height) / 2,
+      bigger,
+      bigger,
+    );
+    ctx.fillStyle = "rgba(0,0,0,0.65)";
+    ctx.fillRect(0, 0, canvas_width, canvas_height);
+    ctx.fillStyle = "white";
+    let font_size_px_max = 300;
+    let font_size_px = font_size_px_max;
+    each(tokens, (token) => {
+      each_range_reverse(font_size_px + 1, (i) => {
+        font_size_px = i;
+        ctx.font = string_combine_multiple([font_size_px, "px Arial"]);
+        let measured = ctx.measureText(token);
         let { width, actualBoundingBoxAscent: height } = measured;
         let padding = line_height_to_padding_double(height);
         if (width <= canvas_width - padding) {
-          list_add(lines, {
-            height,
-            text: sliced_text,
-          });
-          index_current += count + 1;
           return true;
         }
       });
-    }
-    let height_total = list_map_sum(lines, line_to_height_padded);
-    if (less_than_equal(height_total, canvas_height)) {
-      offset_height = (canvas_height - height_total) / 2;
-      return true;
+    });
+    let lines = null;
+    let offset_height = 0;
+    each_range_reverse(font_size_px + 1, (i) => {
+      lines = [];
+      font_size_px = i;
+      ctx.font = string_combine_multiple([font_size_px, "px Arial"]);
+      let index_current = 0;
+      while (index_current < list_size(tokens)) {
+        each_range_reverse(list_size(tokens) - index_current, (count) => {
+          let sliced = list_slice(
+            tokens,
+            index_current,
+            index_current + count + 1,
+          );
+          let sliced_text = list_join_space(sliced);
+          let measured = ctx.measureText(sliced_text);
+          let { width, actualBoundingBoxAscent: height } = measured;
+          let padding = line_height_to_padding_double(height);
+          if (width <= canvas_width - padding) {
+            list_add(lines, {
+              height,
+              text: sliced_text,
+            });
+            index_current += count + 1;
+            return true;
+          }
+        });
+      }
+      let height_total = list_map_sum(lines, line_to_height_padded);
+      if (less_than_equal(height_total, canvas_height)) {
+        offset_height = (canvas_height - height_total) / 2;
+        return true;
+      }
+    });
+    each_index(lines, (line, index) => {
+      let padding = line_height_to_padding(line.height);
+      let offset_line = list_map_sum(
+        list_take(lines, index + 1),
+        line_to_height_padded,
+      );
+      ctx.fillText(line.text, padding, offset_line - padding + offset_height);
+    });
+    let buffer = canvas.toBuffer("image/png");
+    await file_overwrite_binary(path_image, buffer);
+    if (0) {
+      await file_open(path_image);
     }
   });
-  each_index(lines, (line, index) => {
-    let padding = line_height_to_padding(line.height);
-    let offset_line = list_map_sum(
-      list_take(lines, index + 1),
-      line_to_height_padded,
-    );
-    ctx.fillText(line.text, padding, offset_line - padding + offset_height);
-  });
-  let buffer = canvas.toBuffer("image/png");
-  await file_overwrite_binary(path_image, buffer);
-  if (0) {
-    await file_open(path_image);
-  }
   function line_to_height_padded(line) {
     return line.height + line_height_to_padding_double(line.height);
   }
