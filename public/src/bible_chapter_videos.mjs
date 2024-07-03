414c1dd8f234b461c2d429b3b776320a5f48efbd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 11:46:13 -0400

    2024-07-03T15:46:13.040Z function_auto_after bible_chapter_videos

--- public/src/bible_chapter_videos.mjs
+++ public/src/bible_chapter_videos.mjs
@@ -3,7 +3,7 @@ import { bible_image_path } from "./bible_image_path.mjs";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 import videoshow from "videoshow";
 export async function bible_chapter_videos(bible_folder, chapter_name) {
-  await bible_chapter_images(bible_folder, chapter_name);
+  let verses = await bible_chapter_images(bible_folder, chapter_name);
   return new Promise((resolve, reject) => {
     videoshow([bible_image_path()])
       .save(folder_gitignore_path("video.mp4"))
