ff7805c47a0354c4b0d89e16a564302cbcc7ff73
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 12:15:59 -0400

    2024-07-03T16:15:58.613Z function_auto_after string_take

--- public/src/string_take.mjs
+++ public/src/string_take.mjs
@@ -1,8 +1,9 @@
 import { string_substring } from "./string_substring.mjs";
 import { string_length } from "./string_length.mjs";
+import { assert_boolean_message } from "./assert_boolean_message.mjs";
 export function string_take(input, count) {
   let length = string_length(input);
-  assert_message_json(count <= length, () => ({
+  assert_boolean_message(count <= length, () => ({
     input,
     count,
     length,
