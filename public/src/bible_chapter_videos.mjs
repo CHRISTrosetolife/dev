49d27cf8d1875bee9ece8cc1a26c187693efd1d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 12:24:52 -0400

    2024-07-03T16:24:52.406Z function_auto_after bible_chapter_videos

--- public/src/bible_chapter_videos.mjs
+++ public/src/bible_chapter_videos.mjs
@@ -2,7 +2,6 @@ import { list_zip } from "./list_zip.mjs";
 import { bible_chapter_audio_trim } from "./bible_chapter_audio_trim.mjs";
 import { bible_chapter_folder_parent_gitignore } from "./bible_chapter_folder_parent_gitignore.mjs";
 import { bible_chapter_images } from "./bible_chapter_images.mjs";
-import { bible_image_path } from "./bible_image_path.mjs";
 import videoshow from "videoshow";
 import { path_join } from "./path_join.mjs";
 import { list_first } from "./list_first.mjs";
@@ -26,7 +25,7 @@ export async function bible_chapter_videos(
   let [image, audio] = list_first(zipped);
   return z;
   return new Promise((resolve, reject) => {
-    videoshow([bible_image_path()])
+    videoshow([image.path.vertical])
       .audio(audio.path.trimmed)
       .save(path_join([output_path_folder, "video.mp4"]))
       .on("error", function (e) {
