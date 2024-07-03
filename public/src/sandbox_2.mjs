b05ae38d827739c4d8f4f868222a36b0661ac64f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 11:37:55 -0400

    2024-07-03T15:37:54.824Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,7 +1,8 @@
 import { bible_image_path } from "./bible_image_path.mjs";
+import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 export function sandbox_2() {
   videoshow([bible_image_path()])
-    .save("video.mp4")
+    .save(folder_gitignore_path("video.mp4"))
     .on("error", function () {})
     .on("end", function () {});
 }
