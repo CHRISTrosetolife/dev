254cd59a3fc6cfddbb3fe10b2bca1b0cc782823b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 14:41:41 -0400

    2024-07-03T18:41:41.046Z function_auto_after bible_chapter_video

--- public/src/bible_chapter_video.mjs
+++ public/src/bible_chapter_video.mjs
@@ -1,1 +1,4 @@
-export function bible_chapter_video(project_name, bible_folder, chapter_name) {}
+import video_stitch from "video-stitch";
+export function bible_chapter_video(project_name, bible_folder, chapter_name) {
+  video_stitch.concat;
+}
