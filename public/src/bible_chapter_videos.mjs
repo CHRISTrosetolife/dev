41291354aec676ec830fca0195ac9e6536b1b180
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 11:51:01 -0400

    2024-07-03T15:51:00.558Z function_auto_after bible_chapter_videos

--- public/src/bible_chapter_videos.mjs
+++ public/src/bible_chapter_videos.mjs
@@ -1,8 +1,14 @@
+import { bible_chapter_folder_parent_gitignore } from "./bible_chapter_folder_parent_gitignore.mjs";
 import { bible_chapter_images } from "./bible_chapter_images.mjs";
 import { bible_image_path } from "./bible_image_path.mjs";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 import videoshow from "videoshow";
 export async function bible_chapter_videos(bible_folder, chapter_name) {
+  let output_path_folder = bible_chapter_folder_parent_gitignore(
+    "img",
+    bible_folder,
+    chapter_name,
+  );
   let verses = await bible_chapter_images(bible_folder, chapter_name);
   return new Promise((resolve, reject) => {
     videoshow([bible_image_path()])
