aedfd221220a8f0de9c7dad4ed5b3cd618694ebd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 11:51:25 -0400

    2024-07-03T15:51:25.232Z function_auto_after bible_chapter_videos

--- public/src/bible_chapter_videos.mjs
+++ public/src/bible_chapter_videos.mjs
@@ -1,8 +1,8 @@
 import { bible_chapter_folder_parent_gitignore } from "./bible_chapter_folder_parent_gitignore.mjs";
 import { bible_chapter_images } from "./bible_chapter_images.mjs";
 import { bible_image_path } from "./bible_image_path.mjs";
-import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 import videoshow from "videoshow";
+import { path_join } from "./path_join.mjs";
 export async function bible_chapter_videos(bible_folder, chapter_name) {
   let output_path_folder = bible_chapter_folder_parent_gitignore(
     "video",
@@ -12,7 +12,7 @@ export async function bible_chapter_videos(bible_folder, chapter_name) {
   let verses = await bible_chapter_images(bible_folder, chapter_name);
   return new Promise((resolve, reject) => {
     videoshow([bible_image_path()])
-      .save(folder_gitignore_path("video.mp4"))
+      .save(path_join([output_path_folder, "video.mp4"]))
       .on("error", function (e) {
         reject(e);
       })
