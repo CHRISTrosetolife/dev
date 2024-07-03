64265e53960c386a29895ed759005b6e8a8585bc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 11:53:34 -0400

    2024-07-03T15:53:33.846Z function_auto_after bible_chapter_images

--- public/src/bible_chapter_images.mjs
+++ public/src/bible_chapter_images.mjs
@@ -39,10 +39,7 @@ export async function bible_chapter_images(bible_folder, chapter_name) {
   let verses = await bible_chapter(bible_folder, chapter_name);
   return await list_map_async(verses, async (verse) => {
     let { verse_number } = verse;
-    let path_image = path_join([
-      output_path_folder,
-      string_combine_multiple([verse_number, ".png"]),
-    ]);
+    let path_image = path_join([output_path_folder, verse_number, ".png"]);
     let result = {
       path_image,
     };
