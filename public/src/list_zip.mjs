9edd6707788fdd1199ba917b08e44fde4112ddce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 12:05:59 -0400

    2024-07-03T16:05:58.615Z function_auto_after list_zip

--- public/src/list_zip.mjs
+++ public/src/list_zip.mjs
@@ -1,1 +1,5 @@
-export function list_zip(lists, lambda) {}
+import { list_map } from "./list_map.mjs";
+import { list_size } from "./list_size.mjs";
+export function list_zip(lists, lambda) {
+  let sizes = list_map(lists, list_size);
+}
