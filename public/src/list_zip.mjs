6d4566af1d96fe5016d07e9a117245b6e6df3472
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 12:11:22 -0400

    2024-07-03T16:11:21.506Z function_auto_after list_zip

--- public/src/list_zip.mjs
+++ public/src/list_zip.mjs
@@ -4,8 +4,12 @@ import { assert } from "./assert.mjs";
 import { lists_sizes_equal } from "./lists_sizes_equal.mjs";
 import { list_size } from "./list_size.mjs";
 import { list_first } from "./list_first.mjs";
+import { list_map } from "./list_map.mjs";
+import { list_get } from "./list_get.mjs";
 export function list_zip(lists, lambda) {
   let same_sizes = lists_sizes_equal(lists);
   assert(true_is, [same_sizes]);
-  each_range(list_size(list_first(lists)), (index) => {});
+  each_range(list_size(list_first(lists)), (index) => {
+    let item = list_map(lists, list_get());
+  });
 }
