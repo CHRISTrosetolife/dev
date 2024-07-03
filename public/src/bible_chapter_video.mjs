d58792316d96c7b289d4fbd2002d70d72a8d84fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 15:56:18 -0400

    2024-07-03T19:56:17.594Z function_auto_after bible_chapter_video

--- public/src/bible_chapter_video.mjs
+++ public/src/bible_chapter_video.mjs
@@ -1,3 +1,4 @@
+import { each_async } from "./each_async.mjs";
 import { bible_image_resolutions } from "./bible_image_resolutions.mjs";
 import { path_resolve } from "./path_resolve.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
@@ -14,6 +15,7 @@ export async function bible_chapter_video(
   chapter_name,
 ) {
   let hvs = bible_image_resolutions();
+  await each_async(list, async (item) => {});
   let hv_name = object_property_get(hv, "name");
   let output_path_folder = bible_chapter_folder_parent_gitignore(
     "video",
