37b8d6a000cfa82f5b65e21860c6971071d1cced
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 14:45:37 -0400

    2024-07-03T18:45:37.157Z function_auto_after bible_chapter_video

--- public/src/bible_chapter_video.mjs
+++ public/src/bible_chapter_video.mjs
@@ -1,9 +1,11 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { file_exists } from "./file_exists.mjs";
 import { bible_chapter_folder_parent_gitignore } from "./bible_chapter_folder_parent_gitignore.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { bible_chapter_videos } from "./bible_chapter_videos.mjs";
 import video_stitch from "video-stitch";
 import { list_map } from "./list_map.mjs";
+import { path_join } from "./path_join.mjs";
 export async function bible_chapter_video(
   project_name,
   bible_folder,
@@ -14,6 +16,10 @@ export async function bible_chapter_video(
     bible_folder,
     chapter_name,
   );
+  let output_path = path_join([
+    output_path_folder,
+    string_combine_multiple([hv, ".mp4"]),
+  ]);
   if (await file_exists(output_path_folder)) {
     return;
   }
