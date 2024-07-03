ffd382f6e808a415804f382fafd264dbd7f841c5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 12:18:52 -0400

    2024-07-03T16:18:50.246Z   t

--- public/src/bible_chapter_folder.mjs
+++ public/src/bible_chapter_folder.mjs
@@ -1,7 +1,7 @@
 import { string_trim_front } from "./string_trim_front.mjs";
 import { bible_chapter_name_parse } from "./bible_chapter_name_parse.mjs";
 import { path_join } from "./path_join.mjs";
-export function bible_chapter_folder(chapter_name, folder_name) {
+export function bible_chapter_folder(folder_name, chapter_name) {
   let { book_code, chapter_code } = bible_chapter_name_parse(chapter_name);
   let chapter_number = string_trim_front(chapter_code, "0");
   let p = path_join([folder_name, book_code, chapter_number, "/"]);
