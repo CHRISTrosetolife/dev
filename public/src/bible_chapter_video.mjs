e6bc07b15211cbac2c7081636a54e586b66a7b7f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 15:56:29 -0400

    2024-07-03T19:56:29.205Z function_auto_after bible_chapter_video

--- public/src/bible_chapter_video.mjs
+++ public/src/bible_chapter_video.mjs
@@ -15,36 +15,37 @@ export async function bible_chapter_video(
   chapter_name,
 ) {
   let hvs = bible_image_resolutions();
-  await each_async(list, async (item) => {});
-  let hv_name = object_property_get(hv, "name");
-  let output_path_folder = bible_chapter_folder_parent_gitignore(
-    "video",
-    bible_folder,
-    chapter_name,
-  );
-  let output_path = path_join([
-    output_path_folder,
-    string_combine_multiple([hv_name, ".mp4"]),
-  ]);
-  if (await file_exists(output_path)) {
-    return;
-  }
-  let videos = await bible_chapter_videos(
-    project_name,
-    bible_folder,
-    chapter_name,
-  );
-  let m = list_map(videos, (v) => ({
-    fileName: path_resolve(
-      object_property_get(object_property_get(v, "path"), hv_name),
-    ),
-  }));
-  await video_stitch
-    .concat({
-      silent: false,
-      overwrite: true,
-    })
-    .clips(m)
-    .output(output_path)
-    .concat();
+  await each_async(hvs, async (hv) => {
+    let hv_name = object_property_get(hv, "name");
+    let output_path_folder = bible_chapter_folder_parent_gitignore(
+      "video",
+      bible_folder,
+      chapter_name,
+    );
+    let output_path = path_join([
+      output_path_folder,
+      string_combine_multiple([hv_name, ".mp4"]),
+    ]);
+    if (await file_exists(output_path)) {
+      return;
+    }
+    let videos = await bible_chapter_videos(
+      project_name,
+      bible_folder,
+      chapter_name,
+    );
+    let m = list_map(videos, (v) => ({
+      fileName: path_resolve(
+        object_property_get(object_property_get(v, "path"), hv_name),
+      ),
+    }));
+    await video_stitch
+      .concat({
+        silent: false,
+        overwrite: true,
+      })
+      .clips(m)
+      .output(output_path)
+      .concat();
+  });
 }
