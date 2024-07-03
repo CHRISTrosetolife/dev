ea9e24c6337a11f73b2641f3146717ef313d5c82
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 11:38:29 -0400

    2024-07-03T15:38:28.819Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,8 +1,10 @@
 import { bible_image_path } from "./bible_image_path.mjs";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 export function sandbox_2() {
-  videoshow([bible_image_path()])
-    .save(folder_gitignore_path("video.mp4"))
-    .on("error", function () {})
-    .on("end", function () {});
+  return new Promise((resolve, reject) => {
+    videoshow([bible_image_path()])
+      .save(folder_gitignore_path("video.mp4"))
+      .on("error", function () {})
+      .on("end", function () {});
+  });
 }
