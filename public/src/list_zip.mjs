4c8ed2809c75ea5cba0eba816b91e3b988dcf37b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-03 12:09:04 -0400

    2024-07-03T16:09:04.456Z function_auto_after list_zip

--- public/src/list_zip.mjs
+++ public/src/list_zip.mjs
@@ -1,15 +1,5 @@
-import { list_adder } from "./list_adder.mjs";
-import { equal_by } from "./equal_by.mjs";
-import { each_pairs } from "./each_pairs.mjs";
-import { list_size } from "./list_size.mjs";
-import { list_all } from "./list_all.mjs";
+import { lists_sizes_equal } from "./lists_sizes_equal.mjs";
 export function list_zip(lists, lambda) {
-  let compares = list_adder((la) => {
-    each_pairs(lists, (a, b) => {
-      let e = equal_by(a, b, list_size);
-      la(e);
-    });
-  });
-  let same_sizes = list_all(compares, (c) => c === true);
+  let same_sizes = lists_sizes_equal(lists);
   same_sizes;
 }
---
+++ public/src/lists_sizes_equal.mjs
@@ -0,0 +1,15 @@
+import { list_all } from "./list_all.mjs";
+import { list_size } from "./list_size.mjs";
+import { equal_by } from "./equal_by.mjs";
+import { each_pairs } from "./each_pairs.mjs";
+import { list_adder } from "./list_adder.mjs";
+export function lists_sizes_equal(lists) {
+  let compares = list_adder((la) => {
+    each_pairs(lists, (a, b) => {
+      let e = equal_by(a, b, list_size);
+      la(e);
+    });
+  });
+  let same_sizes = list_all(compares, (c) => c === true);
+  return same_sizes;
+}
