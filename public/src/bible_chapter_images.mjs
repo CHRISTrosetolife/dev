91957f4f12b90788432a4656cfa073f5eeaed70e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-02 21:04:38 -0400

    2024-07-03T01:04:38.326Z function_auto_after bible_chapter_images

--- public/src/bible_chapter_images.mjs
+++ public/src/bible_chapter_images.mjs
@@ -29,10 +29,9 @@ import { list_size } from "./list_size.mjs";
 import { list_take } from "./list_take.mjs";
 export async function bible_chapter_images(bible_folder, chapter_name) {
   assert_arguments_length(arguments, 2);
-  let prefix = path_join([
-    "img",
-    bible_chapter_folder(bible_folder, chapter_name),
-  ]);
+  let prefix = folder_gitignore_path(
+    path_join(["img", bible_chapter_folder(bible_folder, chapter_name)]),
+  );
   return prefix;
   let { book_code, chapter_code } = bible_chapter_name_parse(chapter_name);
   let verses = await bible_chapter(bible_folder, chapter_name);
