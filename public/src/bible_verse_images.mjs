4b14ead19e817e58a0b6eb2823fc05b5b64913cc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-04 11:09:39 -0400

    2024-07-04T15:09:38.658Z function_auto_after bible_verse_images

--- public/src/bible_verse_images.mjs
+++ public/src/bible_verse_images.mjs
@@ -65,9 +65,8 @@ export async function bible_verse_images(
     object_merge(result_path, {
       [hv_name]: path_image,
     });
-    await list_all_async(paths_image, file_exists);
     if (!overwrite) {
-      if (await file_exists(path_image)) {
+      if (await list_all_async(paths_image, file_exists)) {
         return;
       }
     }
