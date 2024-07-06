0667f1143715742384ddee172744358ebab5961f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-06 16:09:23 -0400

    2024-07-06T20:09:22.320Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,6 +1,7 @@
+import { range } from "./range.mjs";
 import { each_async } from "./each_async.mjs";
 import { bible_chapter_video } from "./bible_chapter_video.mjs";
 export async function sandbox_2() {
-  await each_async(list, async (item) => {});
+  await each_async(range(5), async (item) => {});
   await bible_chapter_video("calm", "engbsb", "MAT01");
 }
