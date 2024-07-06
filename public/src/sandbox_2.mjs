8df48c7e0ee66aa427b92c0a4db0975c1d133cb5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-06 16:08:56 -0400

    2024-07-06T20:08:55.600Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,4 +1,4 @@
 import { bible_chapter_video } from "./bible_chapter_video.mjs";
 export async function sandbox_2() {
-  await bible_chapter_video();
+  await bible_chapter_video("calm", "engbsb", "MAT01");
 }
