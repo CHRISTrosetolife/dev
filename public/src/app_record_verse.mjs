90cb3734ba493eba14a35b6d25cb318385768c4d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-06 14:54:02 -0400

    2024-07-06T18:54:01.772Z function_auto_after app_record_verse

--- public/src/app_record_verse.mjs
+++ public/src/app_record_verse.mjs
@@ -66,7 +66,7 @@ export async function app_record_verse(
           let chapter_index = list_index(chapters, chapter);
           let chapter_index_next = chapter_index + 1;
           let chapter_next = list_get(chapters, chapter_index_next);
-          let verses_next = await app_record_verses(book_code, chapter);
+          let verses_next = await app_record_verses(book_code, chapter_next);
         }
         html_clear_scroll_top(root);
         return;
