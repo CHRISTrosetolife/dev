01b71d8f919defb320749293e0242358637fe6c9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-06 14:48:50 -0400

    2024-07-06T18:48:49.908Z function_auto_after app_record_verse

--- public/src/app_record_verse.mjs
+++ public/src/app_record_verse.mjs
@@ -58,6 +58,7 @@ export async function app_record_verse(
       if (list_last_is(verses, verse)) {
         let { books } = context;
         let book = list_find_property_or(books, "book_code", book_code);
+        book.chapters;
         html_clear_scroll_top(root);
         return;
       }
