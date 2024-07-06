a899e1b5f13e6ccceb7d8731b49110a7b3010bb4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-06 16:09:48 -0400

    2024-07-06T20:09:48.006Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,8 +1,8 @@
-import { range } from "./range.mjs";
+import { range_1 } from "./range_1.mjs";
 import { each_async } from "./each_async.mjs";
 import { bible_chapter_video } from "./bible_chapter_video.mjs";
 export async function sandbox_2() {
-  await each_async(range(5), async (item) => {
+  await each_async(range_1(5), async (item) => {
     await bible_chapter_video("calm", "engbsb", "MAT0" + (item + 1));
   });
 }
