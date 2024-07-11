9afd3bb039feae4a51385e1f06f65abdbe87c9fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-11 10:34:11 -0400

    2024-07-11T14:34:07.491Z function_auto_after app_record_book

--- public/src/app_record_book.mjs
+++ public/src/app_record_book.mjs
@@ -1,9 +1,11 @@
+import { html_button_width_full_text_click_home } from "./html_button_width_full_text_click_home.mjs";
 import { app_record_chapter } from "./app_record_chapter.mjs";
 import { list_find_property_or } from "./list_find_property_or.mjs";
 import { html_button_text_click } from "./html_button_text_click.mjs";
 import { each } from "./each.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 export function app_record_book(context, book_code) {
+  html_button_width_full_text_click_home;
   let { books } = context;
   let book = list_find_property_or(books, "book_code", book_code);
   let { root } = context;
