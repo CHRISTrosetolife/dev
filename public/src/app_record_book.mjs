6709c5380bc65381a5c2cb77b46c7c084c7ff850
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-11 10:52:14 -0400

    2024-07-11T14:52:13.636Z function_auto_after app_record_book

--- public/src/app_record_book.mjs
+++ public/src/app_record_book.mjs
@@ -6,10 +6,10 @@ import { each } from "./each.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 export function app_record_book(context, book_code) {
   let { root } = context;
-  html_button_width_full_text_click_home;
+  html_clear_scroll_top(root);
+  html_button_width_full_text_click_home(root);
   let { books } = context;
   let book = list_find_property_or(books, "book_code", book_code);
-  html_clear_scroll_top(root);
   each(book.chapters, (chapter) => {
     html_button_text_click(root, chapter, async () => {
       await app_record_chapter(context, book_code, chapter);
