2d34df2ef596b59031a7c986bad92cb9d31a01fd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-12 09:59:14 -0400

    2024-07-12T13:59:07.158Z function_auto_after list_index_next

--- public/src/list_index_next.mjs
+++ public/src/list_index_next.mjs
@@ -3,6 +3,6 @@ import { list_index } from "./list_index.mjs";
 export function list_index_next(chapters, chapter) {
   let lambda = add_1;
   let chapter_index = list_index(chapters, chapter);
-  let chapter_index_next = add_1(chapter_index);
+  let chapter_index_next = lambda(chapter_index);
   return chapter_index_next;
 }
