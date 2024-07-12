b94eced9453b0b7b6e83649f41011cfebe56285a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-12 10:01:27 -0400

    2024-07-12T14:01:18.642Z c list_index_next list_index_previous

---
+++ public/src/list_index_previous.mjs
@@ -0,0 +1,5 @@
+import { list_index_change } from "./list_index_change.mjs";
+import { add_1 } from "./add_1.mjs";
+export function list_index_previous(list, item) {
+  return list_index_change(list, item, add_1);
+}
