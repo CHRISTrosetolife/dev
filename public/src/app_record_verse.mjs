149e2c25610c158e3a1d0fb0e8c5e40d0f138b36
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-12 09:51:33 -0400

    2024-07-12T13:51:31.203Z function_auto_after app_record_verse

--- public/src/app_record_verse.mjs
+++ public/src/app_record_verse.mjs
@@ -53,7 +53,7 @@ export async function app_record_verse(
   html_style_bold(vn);
   html_span_text(root, " ");
   html_span_text(root, list_join_space(tokens));
-  let start, save, cancel, restart, recording, previous;
+  let start, previous, save, cancel, restart, recording, recording_not;
   start = html_button_width_full_text_click(root, "⏺️ start recording", () => {
     html_style_display_none(start);
     html_recorder_media_start(context.mr);
@@ -107,7 +107,7 @@ export async function app_record_verse(
     },
   );
   recording = [save, restart, cancel];
-  let recording_not = [];
+  recording_not = [];
   each(recording, html_style_display_none);
   async function chapter_next() {
     let { books } = context;
