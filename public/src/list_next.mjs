4f2721f03615f3bd8a1b52ca7497c799d84818cc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-12 10:20:52 -0400

    2024-07-12T14:20:51.949Z function_auto_after list_next

--- public/src/list_next.mjs
+++ public/src/list_next.mjs
@@ -2,6 +2,6 @@ import { list_get } from "./list_get.mjs";
 import { list_index_next } from "./list_index_next.mjs";
 export function list_next(list, item) {
   let item_index_next = list_index_next(list, item);
-  let tem_next = list_get(list, chapter_index_next);
+  let item_next = list_get(list, item_index_next);
   return item_next;
 }
