56a2bcf3ab8bb210ea4545946fbe25940b0a3fe1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-12 10:01:37 -0400

    2024-07-12T14:01:25.925Z function_auto_after list_index_previous

--- public/src/list_index_previous.mjs
+++ public/src/list_index_previous.mjs
@@ -1,5 +1,5 @@
+import { subtract_1 } from "./subtract_1.mjs";
 import { list_index_change } from "./list_index_change.mjs";
-import { add_1 } from "./add_1.mjs";
-export function list_index_previous(list, item) {
-  return list_index_change(list, item, add_1);
+export function list_index_next(list, item) {
+  return list_index_change(list, item, subtract_1);
 }
