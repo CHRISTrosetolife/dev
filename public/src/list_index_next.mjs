46fed11c269527e501ccf984df10abb4ab3e0d41
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-12 09:58:07 -0400

    2024-07-12T13:57:58.179Z function_auto_after list_index_next

--- public/src/list_index_next.mjs
+++ public/src/list_index_next.mjs
@@ -1,6 +1,7 @@
+import { add_1 } from "./add_1.mjs";
 import { list_index } from "./list_index.mjs";
 export function list_index_next(chapters, chapter) {
   let chapter_index = list_index(chapters, chapter);
-  let chapter_index_next = chapter_index + 1;
+  let chapter_index_next = add_1(chapter_index);
   return chapter_index_next;
 }
