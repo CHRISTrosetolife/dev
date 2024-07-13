7f3eb301e0dca1fb2d569ee0e26bc60a69ca3ab9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-13 13:24:53 -0400

    2024-07-13T17:24:53.506Z function_auto_after audio_to_video

--- public/src/audio_to_video.mjs
+++ public/src/audio_to_video.mjs
@@ -3,9 +3,13 @@ import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
 import videoshow from "videoshow";
 import { getAudioDurationInSeconds } from "get-audio-duration";
 export async function audio_to_video(path_audio, image_path, path_output) {
+  log({
+    path_audio,
+    image_path,
+    path_output,
+  });
   await folder_parent_exists_ensure(path_output);
   let audio_duration = await getAudioDurationInSeconds(path_audio);
-  log({});
   await new Promise((resolve, reject) => {
     videoshow([image_path], {
       disableFadeOut: true,
