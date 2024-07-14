fd91849b3bd1d63f24c1b5cfa62f7e93a35c0c31
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-14 13:02:04 -0400

    2024-07-14T17:02:04.253Z function_auto_after audio_to_video

--- public/src/audio_to_video.mjs
+++ public/src/audio_to_video.mjs
@@ -1,11 +1,11 @@
 import { import_node } from "./import_node.mjs";
 import { log } from "./log.mjs";
 import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
-import { getAudioDurationInSeconds } from "get-audio-duration";
 import { assert_async } from "./assert_async.mjs";
 import { file_exists } from "./file_exists.mjs";
 export async function audio_to_video(path_audio, image_path, path_output) {
   let videoshow = await import_node("videoshow");
+  let { getAudioDurationInSeconds } = await import_node("get-audio-duration");
   log({
     path_audio,
     image_path,
