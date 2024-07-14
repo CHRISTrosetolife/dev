b5333713d00c423328b3fc1c3033d9b6d901f36c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-14 13:08:21 -0400

    2024-07-14T17:08:21.369Z function_auto_after yyy8Uu_file_path_to_parts

--- public/src/yyy8Uu_file_path_to_parts.mjs
+++ public/src/yyy8Uu_file_path_to_parts.mjs
@@ -15,7 +15,7 @@ import { list_single } from "./list_single.mjs";
 export async function yyy8Uu_file_path_to_parts(file_path, index) {
   let f = list_get(file_path, index);
   let input_string = await file_read(f);
-  let parsed = html_parse(input_string);
+  let parsed = await html_parse(input_string);
   let tei = html_parse_visit_tag_single(parsed, "tei");
   let text = html_parse_visit_tag_single(tei, "text");
   let { children: children_text } = text;
@@ -29,12 +29,13 @@ export async function yyy8Uu_file_path_to_parts(file_path, index) {
   let non_empty = list_filter(trimmed, string_empty_not_is);
   let mapped = list_map(non_empty, (p) => string_replace(p, "&amp;", "&"));
   for (let part of mapped) {
-    if (0)
+    if (0) {
       if (part.match(/.*&[a-z]+;.*/g)) {
         log(file_path);
         log(part);
         error();
       }
+    }
   }
   return mapped;
 }
