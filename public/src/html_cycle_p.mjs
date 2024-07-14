76afe8fd81033108ef26111cbac618b7e295de5e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-14 12:30:45 -0400

    2024-07-14T16:30:45.048Z function_auto_after html_cycle_p

--- public/src/html_cycle_p.mjs
+++ public/src/html_cycle_p.mjs
@@ -1,7 +1,9 @@
 import { html_cycle_split } from "./html_cycle_split.mjs";
 import { html_cycle_list } from "./html_cycle_list.mjs";
 import { html_p } from "./html_p.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function html_cycle_p(parent, patterns, message) {
+  assert_arguments_length(arguments, 3);
   let split = html_cycle_split(message);
   html_cycle_list(parent, html_p, patterns, split);
 }
