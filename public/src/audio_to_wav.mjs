b4c1e01de2d9c00a32c2129779ea1b5c18084254
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-14 13:11:08 -0400

    2024-07-14T17:11:06.968Z function_auto_after audio_to_wav

--- public/src/audio_to_wav.mjs
+++ public/src/audio_to_wav.mjs
@@ -1,10 +1,12 @@
 import { file_exists } from "./file_exists.mjs";
 import { assert_async } from "./assert_async.mjs";
 import ffmpeg from "fluent-ffmpeg";
+import { import_node } from "./import_node.mjs";
 export async function audio_to_wav(path_input, path_output) {
   if (await file_exists(path_output)) {
     return;
   }
+  let ffmpeg = await import_node("fluent-ffmpeg");
   await new Promise(async (resolve, reject) => {
     await assert_async(file_exists, [path_input]);
     ffmpeg(path_input)
