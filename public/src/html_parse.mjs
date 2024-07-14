dffd37e3e6ca89fab501f87872ee135e97087b2a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-14 13:08:01 -0400

    2024-07-14T17:08:01.196Z function_auto_after html_parse

--- public/src/html_parse.mjs
+++ public/src/html_parse.mjs
@@ -1,6 +1,8 @@
 import * as cheerio from "cheerio";
 import { object_property_get } from "./object_property_get.mjs";
-export function html_parse(input) {
+import { import_node } from "./import_node.mjs";
+export async function html_parse(input) {
+  let cheerio = await import_node("cheerio");
   let parsed = cheerio.load(input);
   return object_property_get(parsed.root(), "0");
 }
