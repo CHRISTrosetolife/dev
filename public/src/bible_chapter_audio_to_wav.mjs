64090b04538e42b33042670f1240cfc3416e5939
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-14 13:18:05 -0400

    2024-07-14T17:18:05.150Z function_auto_after bible_chapter_audio_to_wav

--- public/src/bible_chapter_audio_to_wav.mjs
+++ public/src/bible_chapter_audio_to_wav.mjs
@@ -1,8 +1,8 @@
+import { path_dirname_async } from "./path_dirname_async.mjs";
 import { bible_chapter_audio_to_wav_path } from "./bible_chapter_audio_to_wav_path.mjs";
 import { object_merge } from "./object_merge.mjs";
 import { each_async } from "./each_async.mjs";
 import { audio_to_wav } from "./audio_to_wav.mjs";
-import { path_dirname } from "./path_dirname.mjs";
 import { bible_chapter_audio_download } from "./bible_chapter_audio_download.mjs";
 import { path_join } from "./path_join.mjs";
 export async function bible_chapter_audio_to_wav(
@@ -19,7 +19,7 @@ export async function bible_chapter_audio_to_wav(
     let { path } = download;
     let { system: path_system } = path;
     let path_input = path_system;
-    let folder = path_dirname(path_input);
+    let folder = await path_dirname_async(path_input);
     let path_output = path_join([folder, bible_chapter_audio_to_wav_path()]);
     object_merge(path, {
       wav: path_output,
