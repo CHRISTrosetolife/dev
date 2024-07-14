90c14298985a62808ca2497e4f06b382350d9359
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-14 13:17:38 -0400

    2024-07-14T17:17:38.255Z function_auto_after path_dirname_async

--- public/src/path_dirname_async.mjs
+++ public/src/path_dirname_async.mjs
@@ -1,4 +1,6 @@
 import path from "path";
-export function path_dirname_async(file_path) {
+import { import_node } from "./import_node.mjs";
+export async function path_dirname_async(file_path) {
+  let path = await import_node("path");
   return path.dirname(file_path);
 }
