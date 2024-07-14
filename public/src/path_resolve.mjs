bccb14cee683563d0074778635d8ce2d883abc2a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-14 13:13:51 -0400

    2024-07-14T17:13:50.954Z function_auto_after path_resolve

--- public/src/path_resolve.mjs
+++ public/src/path_resolve.mjs
@@ -1,6 +1,5 @@
-import path from "path";
 import { import_node } from "./import_node.mjs";
 export async function path_resolve(file_path) {
-  await import_node("path");
+  let path = await import_node("path");
   return path.resolve(file_path);
 }
