15288233e16f0f6499b029476be7833f0b9f8465
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-14 12:04:18 -0400

    2024-07-14T16:04:17.985Z function_auto_after url_secure_w3

--- public/src/url_secure_w3.mjs
+++ public/src/url_secure_w3.mjs
@@ -1,5 +1,7 @@
 import { url_secure } from "./url_secure.mjs";
 import { string_combine } from "./string_combine.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function url_secure_w3() {
+  assert_arguments_length(arguments, 0);
   return string_combine(url_secure(), "www.");
 }
