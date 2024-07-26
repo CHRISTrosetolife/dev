58383ba1a28e3781adbc02e68a0f9a024969f6d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-25 21:01:43 -0400

    2024-07-26T01:01:42.966Z function_auto_after keyboard_near

--- public/src/keyboard_near.mjs
+++ public/src/keyboard_near.mjs
@@ -1,4 +1,6 @@
+import { keyboard_keys_rows } from "./keyboard_keys_rows.mjs";
 import { equal } from "./equal.mjs";
 export function keyboard_near(a, b) {
+  keyboard_keys_rows();
   return equal(a, b);
 }
