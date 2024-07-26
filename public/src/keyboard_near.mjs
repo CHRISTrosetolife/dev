6b25453a3a9399490a9492d03f5f44176d435a0e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-25 21:12:57 -0400

    2024-07-26T01:12:56.794Z function_auto_after keyboard_near_lookup

--- public/src/keyboard_near.mjs
+++ public/src/keyboard_near.mjs
@@ -1,42 +1,6 @@
-import { each } from "./each.mjs";
-import { each_index } from "./each_index.mjs";
-import { each_pairs } from "./each_pairs.mjs";
-import { keyboard_keys_rows } from "./keyboard_keys_rows.mjs";
+import { keyboard_near_lookup } from "./keyboard_near_lookup.mjs";
 import { equal } from "./equal.mjs";
-import { list_size } from "./list_size.mjs";
-import { list_get } from "./list_get.mjs";
-import { object_property_initialize } from "./object_property_initialize.mjs";
-import { list_add } from "./list_add.mjs";
 export function keyboard_near(a, b) {
-  let lookup = {};
-  let r = keyboard_keys_rows();
-  each_pairs(r, (c, d) => {
-    let c_size = list_size(c);
-    let d_size = list_size(d);
-    let difference = c_size - d_size;
-    each_index(d, (d_item, d_index) => {
-      let difference_1 = difference === 1;
-      let c_right = list_get(c, d_index + 1);
-      let c_lrs = [c_right];
-      if (difference_1) {
-        let c_left = list_get(c, d_index);
-        list_add(c_lrs, c_left);
-      }
-      let c_right_nears = object_property_initialize(lookup, c_right, []);
-      let c_lr_nears = [c_right_nears];
-      if (difference_1) {
-        let c_left_nears = object_property_initialize(lookup, c_left, []);
-        list_add(c_lr_nears, c_left_nears);
-      }
-      each(c_lr_nears, (c_nears) => {
-        list_add(c_nears, d_item);
-      });
-      let d_item_nears = object_property_initialize(lookup, c_right, []);
-      each(c_lrs, (c_lr) => {
-        list_add(d_item_nears, c_lr);
-      });
-    });
-  });
-  return lookup;
+  return keyboard_near_lookup();
   return equal(a, b);
 }
---
+++ public/src/keyboard_near_lookup.mjs
@@ -0,0 +1,40 @@
+import { each } from "./each.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
+import { list_add } from "./list_add.mjs";
+import { list_get } from "./list_get.mjs";
+import { each_index } from "./each_index.mjs";
+import { list_size } from "./list_size.mjs";
+import { each_pairs } from "./each_pairs.mjs";
+import { keyboard_keys_rows } from "./keyboard_keys_rows.mjs";
+export function keyboard_near_lookup() {
+  let lookup = {};
+  let r = keyboard_keys_rows();
+  each_pairs(r, (c, d) => {
+    let c_size = list_size(c);
+    let d_size = list_size(d);
+    let difference = c_size - d_size;
+    each_index(d, (d_item, d_index) => {
+      let difference_1 = difference === 1;
+      let c_right = list_get(c, d_index + 1);
+      let c_lrs = [c_right];
+      if (difference_1) {
+        let c_left = list_get(c, d_index);
+        list_add(c_lrs, c_left);
+      }
+      let c_right_nears = object_property_initialize(lookup, c_right, []);
+      let c_lr_nears = [c_right_nears];
+      if (difference_1) {
+        let c_left_nears = object_property_initialize(lookup, c_left, []);
+        list_add(c_lr_nears, c_left_nears);
+      }
+      each(c_lr_nears, (c_nears) => {
+        list_add(c_nears, d_item);
+      });
+      let d_item_nears = object_property_initialize(lookup, c_right, []);
+      each(c_lrs, (c_lr) => {
+        list_add(d_item_nears, c_lr);
+      });
+    });
+  });
+  return lookup;
+}
