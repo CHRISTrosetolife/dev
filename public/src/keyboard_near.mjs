caedfcdbd4299d5456687ba5a440e54f9e27236a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-25 21:27:42 -0400

    2024-07-26T01:27:42.275Z function_auto_after keyboard_near

--- public/src/keyboard_near.mjs
+++ public/src/keyboard_near.mjs
@@ -2,5 +2,5 @@ import { keyboard_near_lookup } from "./keyboard_near_lookup.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_includes } from "./list_includes.mjs";
 export function keyboard_near(a, b) {
-  list_includes(object_property_get(keyboard_near_lookup(), a), b);
+  return list_includes(object_property_get(keyboard_near_lookup(), a), b);
 }
