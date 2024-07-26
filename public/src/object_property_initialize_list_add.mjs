7de7c2da4bc9fef8584fec26c84d061dab8cfa7c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-25 21:27:13 -0400

    2024-07-26T01:27:13.122Z function_auto_after object_property_initialize_list_add

--- public/src/object_property_initialize_list_add.mjs
+++ public/src/object_property_initialize_list_add.mjs
@@ -1,5 +1,5 @@
-import { object_property_initialize } from "./object_property_initialize.mjs";
+import { object_property_initialize_list } from "./object_property_initialize_list.mjs";
 import { list_add } from "./list_add.mjs";
 export function object_property_initialize_list_add(lookup, r1, r2) {
-  list_add(object_property_initialize(lookup, r1, []), r2);
+  list_add(object_property_initialize_list(lookup, r1), r2);
 }
