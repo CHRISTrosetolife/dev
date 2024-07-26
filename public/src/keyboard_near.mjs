27c8833308e61ca31224f1b7fd037bfe71070928
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-25 21:26:19 -0400

    2024-07-26T01:26:19.353Z function_auto_after keyboard_near

--- public/src/keyboard_near.mjs
+++ public/src/keyboard_near.mjs
@@ -1,5 +1,6 @@
+import { keyboard_near_lookup } from "./keyboard_near_lookup.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_includes } from "./list_includes.mjs";
 export function keyboard_near(a, b) {
-  list_includes(object_property_get(object, property_name));
+  list_includes(object_property_get(keyboard_near_lookup(), a), b);
 }
