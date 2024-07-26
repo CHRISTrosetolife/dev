2550ada8e783da86471d905aad4517121eb746b7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-25 21:26:47 -0400

    2024-07-26T01:26:45.729Z r object_property_initialize_ist object_property_initialize_list

--- public/src/object_property_initialize_list.mjs
+++ public/src/object_property_initialize_list.mjs
@@ -1,4 +1,4 @@
 import { object_property_initialize } from "./object_property_initialize.mjs";
-export function object_property_initialize_ist(lookup, c_right) {
+export function object_property_initialize_list(lookup, c_right) {
   return object_property_initialize(lookup, c_right, []);
 }
