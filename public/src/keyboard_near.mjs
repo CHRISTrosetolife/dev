2c9b5d5ad1391a2360e51c55b88674e4f411a14c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-25 21:04:14 -0400

    2024-07-26T01:04:14.276Z function_auto_after keyboard_near

--- public/src/keyboard_near.mjs
+++ public/src/keyboard_near.mjs
@@ -1,8 +1,11 @@
 import { each_pairs } from "./each_pairs.mjs";
 import { keyboard_keys_rows } from "./keyboard_keys_rows.mjs";
 import { equal } from "./equal.mjs";
+import { list_size } from "./list_size.mjs";
 export function keyboard_near(a, b) {
   let r = keyboard_keys_rows();
-  each_pairs(r, (c, d) => {});
+  each_pairs(r, (c, d) => {
+    let c_size = list_size(c);
+  });
   return equal(a, b);
 }
