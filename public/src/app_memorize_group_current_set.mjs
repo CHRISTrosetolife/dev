aa60c988c570fffe708f0609b9896542542d79a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-26 11:33:37 -0400

    2024-07-26T15:33:37.191Z function_auto_after app_memorize_group_current_set

--- public/src/app_memorize_group_current_set.mjs
+++ public/src/app_memorize_group_current_set.mjs
@@ -2,6 +2,7 @@ import { each_range } from "./each_range.mjs";
 import { list_size } from "./list_size.mjs";
 import { equal } from "./equal.mjs";
 import { list_add } from "./list_add.mjs";
+import { list_concat } from "./list_concat.mjs";
 export function app_memorize_group_current_set(context, g) {
   context.group_current = g;
   let repeats = [];
@@ -9,20 +10,12 @@ export function app_memorize_group_current_set(context, g) {
     list_add(repeats, "0");
   });
   if (equal(list_size(context.group_current), 1)) {
-    context.patterns = [
-      "1",
-      "110",
-      "10",
-      "01",
-      "01001",
-      "001",
-      "0",
-      "0",
-      "0",
-      "0",
-    ];
+    context.patterns = list_concat(
+      ["1", "110", "10", "01", "01001", "001"],
+      repeats,
+    );
   } else {
-    context.patterns = ["1", "10", "0", "0", "0", "0"];
+    context.patterns = list_concat(["1", "10"], repeats);
   }
   context.patterns_length = list_size(context.patterns);
   context.verse_index = 0;
