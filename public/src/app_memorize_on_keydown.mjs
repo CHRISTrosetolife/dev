c9a470d23f784fb287d5171eb706ca1dc8cad3ba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-28 13:36:23 -0400

    2024-07-28T17:36:21.889Z   w

--- public/src/app_memorize_on_keydown.mjs
+++ public/src/app_memorize_on_keydown.mjs
@@ -36,6 +36,8 @@ export function app_memorize_on_keydown(context, key) {
       }
     }
     context.token_index++;
+    let current_token = list_get(tokens, context.token_index);
+    let letter_first = (string_letter_first(current_token));
     let tokens_length = list_size(tokens);
     if (greater_than_equal(context.token_index, tokens_length)) {
       context.verse_index++;
