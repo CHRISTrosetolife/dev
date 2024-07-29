01493dcd0258190b7b1ac32d5bc10423172fb91d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-29 13:50:23 -0400

    2024-07-29T17:50:23.223Z function_auto_after sermon_new

--- public/src/sermon_new.mjs
+++ public/src/sermon_new.mjs
@@ -1,3 +1,4 @@
+import { file_open } from "./file_open.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { path_join } from "./path_join.mjs";
 import { file_write } from "./file_write.mjs";
@@ -5,4 +6,5 @@ export async function sermon_new(name) {
   let file_name = string_combine(name, ".txt");
   let sermon_path = path_join(["./sermons/", file_name]);
   await file_write(sermon_path, "TODO");
+  await file_open(sermon_path);
 }
