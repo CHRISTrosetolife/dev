2acebb64290f70f466b09da0135799a621c511d1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-29 13:49:44 -0400

    2024-07-29T17:49:44.204Z function_auto_after sermon_new

--- public/src/sermon_new.mjs
+++ public/src/sermon_new.mjs
@@ -2,7 +2,7 @@ import { path_join } from "./path_join.mjs";
 import { file_write } from "./file_write.mjs";
 export async function sermon_new(name) {
   let file_name = string_concat(name, ".txt");
-  let sermon_path = path_join(["./sermons/", name]);
+  let sermon_path = path_join(["./sermons/", file_name]);
   return sermon_path;
   await file_write(sermon_path);
 }
