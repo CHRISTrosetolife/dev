cace440b37720dedb0f1248d8cc34da0cafdc540
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-31 15:16:57 -0400

    2024-07-31T19:16:56.855Z function_auto_after app_memorize_group_current_set

--- public/src/app_memorize_group_current_set.mjs
+++ public/src/app_memorize_group_current_set.mjs
@@ -6,12 +6,12 @@ import { list_concat } from "./list_concat.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 export function app_memorize_group_current_set(context, g) {
   let save = object_property_initialize(context, "save", {});
-  context.save.group_current = g;
+  save.group_current = g;
   let repeats = [];
   each_range(5, (item) => {
     list_add(repeats, "0");
   });
-  if (equal(list_size(context.save.group_current), 1)) {
+  if (equal(list_size(save.group_current), 1)) {
     context.patterns = list_concat(
       ["1", "110", "10", "01", "01001", "001"],
       repeats,
