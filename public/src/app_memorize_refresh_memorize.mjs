72c19189371e2966aadb7a9a5112be541ec254cd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-31 16:53:40 -0400

    2024-07-31T20:53:38.940Z function_auto_after app_memorize_refresh_memorize

--- public/src/app_memorize_refresh_memorize.mjs
+++ public/src/app_memorize_refresh_memorize.mjs
@@ -50,6 +50,7 @@ export function app_memorize_refresh_memorize(context) {
   let verses_element = html_div(root);
   let keyboard_height = app_memorize_keyboard_height(context);
   let offset = add(keyboard_height, context.settings ? 7 : 0);
+  let recent_element_offset = 6;
   let height_max = subtract(100, offset);
   html_style(verses_element, {
     "max-height": number_to_dvh(height_max),
