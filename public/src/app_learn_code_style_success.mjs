c425ad084485daca74dc91cb37e24ba4a9a8d34d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-31 16:17:06 -0400

    2024-07-31T20:17:05.291Z function_auto_after app_learn_code_style_success

--- public/src/app_learn_code_style_success.mjs
+++ public/src/app_learn_code_style_success.mjs
@@ -1,9 +1,5 @@
-import { log } from "./log.mjs";
 import { html_style } from "./html_style.mjs";
 export function app_learn_code_style_success(component) {
-  log({
-    component,
-  });
   html_style(component, {
     "background-color": "lightgreen",
     "border-color": "MediumSeaGreen",
