e0a23b5d28a2e920335bdb8b6e223ff67381848d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-31 16:23:06 -0400

    2024-07-31T20:23:04.821Z function_auto_after app_memorize_button_keyboard_stylize

--- public/src/app_memorize_button_keyboard_stylize.mjs
+++ public/src/app_memorize_button_keyboard_stylize.mjs
@@ -1,9 +1,11 @@
+import { html_style_button_default } from "./html_style_button_default.mjs";
 import { number_to_dvh } from "./number_to_dvh.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_style_margin_default } from "./html_style_margin_default.mjs";
 import { html_style_centered } from "./html_style_centered.mjs";
 import { number_to_dvw } from "./number_to_dvw.mjs";
 export function app_memorize_button_keyboard_stylize(context, button) {
+  html_style_button_default(button);
   let b_width = number_to_dvw(8);
   html_style_centered(button);
   html_style_margin_default(button);
