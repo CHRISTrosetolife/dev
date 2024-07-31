ed0f044af504dfa5c64c74d347b214a245f2fd7e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-31 15:35:38 -0400

    2024-07-31T19:35:37.341Z function_auto_after app_memorize

--- public/src/app_memorize.mjs
+++ public/src/app_memorize.mjs
@@ -1,9 +1,11 @@
+import { storage_local_initialize } from "./storage_local_initialize.mjs";
 import { html_script_axios } from "./html_script_axios.mjs";
 import { app_memorize_frame } from "./app_memorize_frame.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { html_style } from "./html_style.mjs";
 import { object_merge } from "./object_merge.mjs";
 export async function app_memorize() {
+  storage_local_initialize();
   let context = {
     style: {},
   };
