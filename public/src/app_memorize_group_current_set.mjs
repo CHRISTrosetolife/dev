f5b075dd7b91edcdf9dad15f4253c4a695370d9e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-07-31 15:49:43 -0400

    2024-07-31T19:49:42.233Z function_auto_after app_memorize_group_current_set

--- public/src/app_memorize_group_current_set.mjs
+++ public/src/app_memorize_group_current_set.mjs
@@ -1,3 +1,4 @@
+import { equal_json } from "./equal_json.mjs";
 import { app_memorize_save_get } from "./app_memorize_save_get.mjs";
 import { app_memorize_save } from "./app_memorize_save.mjs";
 import { each_range } from "./each_range.mjs";
@@ -9,7 +10,7 @@ import { object_property_exists } from "./object_property_exists.mjs";
 export function app_memorize_group_current_set(context, g) {
   let save = app_memorize_save_get(context);
   if (
-    json_equal(save.group_current, g) &&
+    equal_json(save.group_current, g) &&
     object_property_exists(save, "pattern_index")
   ) {
     save.pattern_index = save.pattern_index;
