020397c27d2c43d2c87f764923ffdd40917ac453
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-01 15:31:53 -0400

    2024-08-01T19:31:52.750Z function_auto_after app_language_refresh_node

--- public/src/app_language_refresh_node.mjs
+++ public/src/app_language_refresh_node.mjs
@@ -110,7 +110,7 @@ export async function app_language_refresh_node(context) {
       root,
       string_combine_multiple([
         j++,
-        string_combine_multiple([". 📝 quiz ( ", "🔴", " "]),
+        string_combine_multiple([". 📝 quiz ( ", item.emoji, " "]),
         await app_language_text(language_fluent, "hard"),
         " )",
       ]),
