f09f60cbffe68bff891171cc820a8d0e82eaacce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-02 18:54:21 -0400

    2024-08-02T22:54:19.782Z function_auto_after storage_upload_file

--- public/src/storage_upload_file.mjs
+++ public/src/storage_upload_file.mjs
@@ -3,7 +3,7 @@ import { storage_bucket } from "./storage_bucket.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { error } from "./error.mjs";
 export async function storage_upload_file(file_path, destination) {
-  destination = string_replace(file_path, "\\", "/");
+  destination = string_replace(destination, "\\", "/");
   if (string_starts_with(destination, "/")) {
     error();
   }
