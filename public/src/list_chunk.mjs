c42641d2928975e8208e98d83521ea3408ad2975
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-03 18:18:21 -0400

    2024-08-03T22:18:20.729Z function_auto_after list_chunk

--- public/src/list_chunk.mjs
+++ public/src/list_chunk.mjs
@@ -1,7 +1,7 @@
+import { each_index } from "./each_index.mjs";
 import { list_adder } from "./list_adder.mjs";
-import { each } from "./each.mjs";
 export function list_chunk(list, chunk_size) {
   return list_adder((la) => {
-    each(list, (item) => {});
+    each_index(list, (item, index) => {});
   });
 }
