5d3da1386805f9209b048e070bbf5590387beecc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-03 18:19:05 -0400

    2024-08-03T22:19:05.432Z function_auto_after list_chunk

--- public/src/list_chunk.mjs
+++ public/src/list_chunk.mjs
@@ -2,6 +2,7 @@ import { each_index } from "./each_index.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function list_chunk(list, chunk_size) {
   return list_adder((la) => {
+    let chunk;
     each_index(list, (item, index) => {
       if (index % chunk_size === 0) {
       }
