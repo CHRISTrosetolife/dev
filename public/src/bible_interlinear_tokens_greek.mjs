951f49344d956da2fb5907e53cd317c762d1e5eb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-07 17:27:23 -0400

    2024-08-07T21:27:22.677Z function_auto_after bible_interlinear_tokens_greek

--- public/src/bible_interlinear_tokens_greek.mjs
+++ public/src/bible_interlinear_tokens_greek.mjs
@@ -1,5 +1,4 @@
 import { list_adder_unique } from "./list_adder_unique.mjs";
-import { log } from "./log.mjs";
 import { bible_interlinear_each_token } from "./bible_interlinear_each_token.mjs";
 import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
 export async function bible_interlinear_tokens_greek() {
@@ -7,9 +6,7 @@ export async function bible_interlinear_tokens_greek() {
   list_adder_unique((la) => {
     bible_interlinear_each_token(new_testament_books, lambda);
     function lambda(token) {
-      log({
-        token,
-      });
+      la(token.word);
     }
   });
 }
