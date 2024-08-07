d38edf1a37566fa5e442fec8882ed48de2501592
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-07 17:28:57 -0400

    2024-08-07T21:28:56.575Z function_auto_after bible_interlinear_words_greek_audio_upload

--- public/src/bible_interlinear_words_greek_audio_upload.mjs
+++ public/src/bible_interlinear_words_greek_audio_upload.mjs
@@ -1,1 +1,4 @@
-export function bible_interlinear_words_greek_audio_upload() {}
+import { bible_interlinear_words_greek } from "./bible_interlinear_words_greek.mjs";
+export async function bible_interlinear_words_greek_audio_upload() {
+  let words = await bible_interlinear_words_greek();
+}
