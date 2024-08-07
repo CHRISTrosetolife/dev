355e5285dfed19df5cbabe416510b53dd9032eab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-07 17:32:49 -0400

    2024-08-07T21:32:46.313Z function_auto_after audio_language_el

---
+++ public/src/audio_language_el.mjs
@@ -0,0 +1,23 @@
+export function audio_language_el() {
+  return {
+    language_code: "en-US",
+    voices: [
+      {
+        code: "Standard-J",
+        male: true,
+      },
+      {
+        code: "Standard-H",
+        male: false,
+      },
+      {
+        code: "Standard-I",
+        male: true,
+      },
+      {
+        code: "Standard-F",
+        male: false,
+      },
+    ],
+  };
+}
