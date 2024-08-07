3feb4bb00271b8b9ce62141abda8c22b3ffbe373
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-07 13:41:03 -0400

    2024-08-07T17:41:03.183Z function_auto_after ceb_audio_download

--- public/src/ceb_audio_download.mjs
+++ public/src/ceb_audio_download.mjs
@@ -1,5 +1,17 @@
+import { promise_all } from "./promise_all.mjs";
+import { log } from "./log.mjs";
+import { list_map } from "./list_map.mjs";
+import { list_chunk } from "./list_chunk.mjs";
+import { each_async } from "./each_async.mjs";
 import { ceb_bible_words_definitions_all_cache } from "./ceb_bible_words_definitions_all_cache.mjs";
 export async function ceb_audio_download() {
   let from = "ceb";
   let words = await ceb_bible_words_definitions_all_cache();
+  await each_async(list_chunk(words, 20), async (chunk) => {
+    let mapped = list_map(chunk, async (word) => {
+      log({});
+    });
+    await promise_all(mapped);
+    log("chunk finished");
+  });
 }
