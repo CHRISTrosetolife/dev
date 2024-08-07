89400a5c1ce6aa3c2bce062ebf135350ee0f64b1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-07 17:51:33 -0400

    2024-08-07T21:51:32.884Z function_auto_after bible_interlinear_words_greek_audio_upload

--- public/src/bible_interlinear_words_greek_audio_upload.mjs
+++ public/src/bible_interlinear_words_greek_audio_upload.mjs
@@ -1,7 +1,7 @@
+import { each_index_async } from "./each_index_async.mjs";
 import { log } from "./log.mjs";
 import { promise_all } from "./promise_all.mjs";
 import { list_chunk } from "./list_chunk.mjs";
-import { each_async } from "./each_async.mjs";
 import { string_filter } from "./string_filter.mjs";
 import { keyboard_greek } from "./keyboard_greek.mjs";
 import { audio_upload } from "./audio_upload.mjs";
@@ -17,7 +17,7 @@ export async function bible_interlinear_words_greek_audio_upload() {
   let lambda = (character) => string_includes(alphabet, character);
   let m2 = list_map(m1, (word) => string_filter(word, lambda));
   let chunks = list_chunk(m2, 20);
-  await each_async(chunks, async (chunk) => {
+  await each_index_async(chunks, async (chunk) => {
     let m3 = list_map(chunk, async (word) => {
       await audio_upload(language_code, word);
     });
