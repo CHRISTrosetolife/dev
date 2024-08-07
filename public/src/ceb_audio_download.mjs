ec9e806a68576f102ca9070a69db31708c99783f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-07 13:39:34 -0400

    2024-08-07T17:39:33.044Z function_auto_after ceb_audio_download

--- public/src/ceb_audio_download.mjs
+++ public/src/ceb_audio_download.mjs
@@ -1,1 +1,4 @@
-export function ceb_audio_download() {}
+import { ceb_bible_words_definitions_all_cache } from "./ceb_bible_words_definitions_all_cache.mjs";
+export async function ceb_audio_download() {
+  let words = await ceb_bible_words_definitions_all_cache();
+}
