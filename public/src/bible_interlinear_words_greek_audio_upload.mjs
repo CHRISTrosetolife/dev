b7f96ed892393e09a5ba1f975cd7aff6c68bf3e4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-07 17:29:49 -0400

    2024-08-07T21:29:49.075Z function_auto_after bible_interlinear_words_greek_audio_upload

--- public/src/bible_interlinear_words_greek_audio_upload.mjs
+++ public/src/bible_interlinear_words_greek_audio_upload.mjs
@@ -5,5 +5,5 @@ import { list_take } from "./list_take.mjs";
 export async function bible_interlinear_words_greek_audio_upload() {
   let words = await bible_interlinear_words_greek();
   let mapped = list_map(words, string_case_lower);
-  return list_take(mapped, 10);
+  return list_take(words, 10);
 }
