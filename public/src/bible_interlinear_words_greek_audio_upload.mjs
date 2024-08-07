2c6371c0b9a111bace3a1a4daf1bb063dde23b7f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-07 17:46:28 -0400

    2024-08-07T21:46:27.690Z function_auto_after bible_interlinear_words_greek_audio_upload

--- public/src/bible_interlinear_words_greek_audio_upload.mjs
+++ public/src/bible_interlinear_words_greek_audio_upload.mjs
@@ -7,6 +7,7 @@ import { audio_upload } from "./audio_upload.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { list_map } from "./list_map.mjs";
 import { bible_interlinear_words_greek } from "./bible_interlinear_words_greek.mjs";
+import { list_take } from "./list_take.mjs";
 import { list_first } from "./list_first.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
 import { string_includes } from "./string_includes.mjs";
@@ -17,7 +18,7 @@ export async function bible_interlinear_words_greek_audio_upload() {
   let mapped = list_map(words, string_case_lower);
   let lambda = (character) => string_includes(alphabet, character);
   let m2 = list_map(mapped, (word) => string_filter(word, lambda));
-  m2 = await audio_upload(language_code, list_first(mapped));
+  m2 = list_take(m2, 10);
   await each_async(list, async (item) => {
     await audio_upload(language_code, list_first(mapped));
   });
@@ -26,5 +27,5 @@ export async function bible_interlinear_words_greek_audio_upload() {
     string_symbols_multiple(m2),
   );
   return;
-  return x;
+  return;
 }
