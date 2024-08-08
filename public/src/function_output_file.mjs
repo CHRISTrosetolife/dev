e981667eec4e8df8e553b6ae4ab1de9cd7638716
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-08 17:23:41 -0400

    2024-08-08T21:23:28.289Z function_auto_after function_output_file

--- public/src/function_output_file.mjs
+++ public/src/function_output_file.mjs
@@ -1,6 +1,9 @@
+import { path_join } from "./path_join.mjs";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 import { function_run } from "./function_run.mjs";
 export async function function_output_file(function_name) {
   let result = await function_run(function_name, []);
-  let output_path = folder_gitignore_path();
+  let output_path = folder_gitignore_path(
+    path_join([function_output_file.name]),
+  );
 }
