0114d3aa659cdaa0b3cfc6130e7f735fd769902b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-08 17:13:04 -0400

    2024-08-08T21:12:55.140Z function_auto_after ceb_definitions_change

--- public/src/ceb_definitions_change.mjs
+++ public/src/ceb_definitions_change.mjs
@@ -1,1 +1,5 @@
-export function ceb_definitions_change() {}
+import { ceb_bible_words_definitions_pairs_compute } from "./ceb_bible_words_definitions_pairs_compute.mjs";
+import { function_cache_refresh } from "./function_cache_refresh.mjs";
+export async function ceb_definitions_change() {
+  await function_cache_refresh(ceb_bible_words_definitions_pairs_compute);
+}
