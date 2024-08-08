1377acc69bf7cd2101294936571b42adfd17daeb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-08 17:24:07 -0400

    2024-08-08T21:24:01.125Z function_auto_after function_output_file

--- public/src/function_output_file.mjs
+++ public/src/function_output_file.mjs
@@ -1,9 +1,14 @@
+import { date_string_iso_file } from "./date_string_iso_file.mjs";
 import { path_join } from "./path_join.mjs";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 import { function_run } from "./function_run.mjs";
 export async function function_output_file(function_name) {
   let result = await function_run(function_name, []);
   let output_path = folder_gitignore_path(
-    path_join([function_output_file.name]),
+    path_join([
+      function_output_file.name,
+      function_output_file,
+      date_string_iso_file(),
+    ]),
   );
 }
