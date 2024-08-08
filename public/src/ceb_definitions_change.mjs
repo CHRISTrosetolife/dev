d836001e094c7b9cc292f5d76810aceb2b3f9685
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-08 18:52:53 -0400

    2024-08-08T22:52:45.027Z function_auto_after ceb_definitions_change

--- public/src/ceb_definitions_change.mjs
+++ public/src/ceb_definitions_change.mjs
@@ -1,7 +1,12 @@
+import { ceb_bible_words_definitions_all } from "./ceb_bible_words_definitions_all.mjs";
 import { app_ceb_upload } from "./app_ceb_upload.mjs";
 import { ceb_bible_words_definitions_pairs_compute } from "./ceb_bible_words_definitions_pairs_compute.mjs";
 import { function_cache_refresh } from "./function_cache_refresh.mjs";
 export async function ceb_definitions_change() {
+  let fns = [
+    ceb_bible_words_definitions_all,
+    ceb_bible_words_definitions_pairs_compute,
+  ];$ea
   await function_cache_refresh(ceb_bible_words_definitions_pairs_compute.name);
   await app_ceb_upload();
 }
