b486adc85b404e33dc2cf67ae02df557c18bf33f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-08 17:09:55 -0400

    2024-08-08T21:09:49.066Z function_auto_after function_alias_expand

--- public/src/function_alias_expand.mjs
+++ public/src/function_alias_expand.mjs
@@ -1,4 +1,5 @@
+import { function_aliases } from "./function_aliases.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-export function function_alias_expand() {
-  return object_property_get(object, property_name);
+export function function_alias_expand(alias) {
+  return object_property_get(function_aliases(), alias);
 }
