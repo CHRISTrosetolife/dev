2a109f641664d45a5b910a725f8fa684d0d5c918
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-08 17:31:52 -0400

    2024-08-08T21:31:49.107Z function_auto_after function_output_file

--- public/src/function_output_file.mjs
+++ public/src/function_output_file.mjs
@@ -2,6 +2,7 @@ import { date_string_iso_file } from "./date_string_iso_file.mjs";
 import { path_join } from "./path_join.mjs";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 import { function_run } from "./function_run.mjs";
+import { file_write } from "./file_write.mjs";
 export async function function_output_file(function_name) {
   let output_path = folder_gitignore_path(
     path_join([
@@ -10,6 +11,7 @@ export async function function_output_file(function_name) {
       date_string_iso_file() + ".txt",
     ]),
   );
-  return output_path;
   let result = await function_run(function_name, []);
+  await file_write();
+  return output_path;
 }
