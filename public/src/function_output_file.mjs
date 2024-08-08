7dbd52725a16d168321a0fe15b1e3e7b9efab803
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-08 17:21:41 -0400

    2024-08-08T21:21:31.255Z function_auto_after function_output_file

--- public/src/function_output_file.mjs
+++ public/src/function_output_file.mjs
@@ -1,4 +1,4 @@
 import { function_run } from "./function_run.mjs";
 export async function function_output_file(function_name) {
-  await function_run(function_name);
+  await function_run(function_name, []);
 }
