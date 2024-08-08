049f1dfa0d745611b41a2813474110448bd769c6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-08 17:13:09 -0400

    2024-08-08T21:13:06.026Z function_auto_after ceb_definitions_change

--- public/src/ceb_definitions_change.mjs
+++ public/src/ceb_definitions_change.mjs
@@ -1,5 +1,5 @@
 import { ceb_bible_words_definitions_pairs_compute } from "./ceb_bible_words_definitions_pairs_compute.mjs";
 import { function_cache_refresh } from "./function_cache_refresh.mjs";
 export async function ceb_definitions_change() {
-  await function_cache_refresh(ceb_bible_words_definitions_pairs_compute);
+  await function_cache_refresh(ceb_bible_words_definitions_pairs_compute.name);
 }
