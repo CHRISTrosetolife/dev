ea2c802d3089a52e5dc283ac0f40932feed1afb3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-07 21:00:16 -0400

    2024-08-08T01:00:14.070Z function_auto_after bible_interlinear_words_greek_audio_upload

--- public/src/bible_interlinear_words_greek_audio_upload.mjs
+++ public/src/bible_interlinear_words_greek_audio_upload.mjs
@@ -11,22 +11,26 @@ import { list_map } from "./list_map.mjs";
 import { bible_interlinear_words_greek } from "./bible_interlinear_words_greek.mjs";
 import { string_includes } from "./string_includes.mjs";
 export async function bible_interlinear_words_greek_audio_upload() {
-  let alphabet = keyboard_greek();
-  let language_code = "el";
-  let words = await bible_interlinear_words_greek();
-  let m1 = list_map(words, string_case_lower);
-  let lambda = (character) => string_includes(alphabet, character);
-  let m2 = list_map(m1, (word) => string_filter(word, lambda));
-  let chunks = list_chunk(m2, 20);
-  let chunks_size = list_size(chunks);
-  await each_index_async(chunks, async (chunk, index) => {
-    let m3 = list_map(chunk, async (word) => {
-      await audio_upload(language_code, word);
+  try {
+    let alphabet = keyboard_greek();
+    let language_code = "el";
+    let words = await bible_interlinear_words_greek();
+    let m1 = list_map(words, string_case_lower);
+    let lambda = (character) => string_includes(alphabet, character);
+    let m2 = list_map(m1, (word) => string_filter(word, lambda));
+    let chunks = list_chunk(m2, 20);
+    let chunks_size = list_size(chunks);
+    await each_index_async(chunks, async (chunk, index) => {
+      let m3 = list_map(chunk, async (word) => {
+        await audio_upload(language_code, word);
+      });
+      await promise_all(m3);
+      log({
+        chunks_size,
+        index,
+      });
     });
-    await promise_all(m3);
-    log({
-      chunks_size,
-      index,
-    });
-  });
+  } catch (e) {
+    await bible_interlinear_words_greek_audio_upload();
+  }
 }
