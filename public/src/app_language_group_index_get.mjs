eeb0c342fa61476cb8dd69b5d1f34726c03d01b1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 17:03:39 -0400

    2024-08-09T21:03:29.878Z function_auto_after app_language_group_index_get

--- public/src/app_language_group_index_get.mjs
+++ public/src/app_language_group_index_get.mjs
@@ -1,6 +1,6 @@
 import { app_language_group_path } from "./app_language_group_path.mjs";
 import { http_storage } from "./http_storage.mjs";
-export async function app_language_group_get(context, group_index) {
+export async function app_language_group_index_get(context, group_index) {
   return await http_storage(
     app_language_group_path(
       context.language_learn,
