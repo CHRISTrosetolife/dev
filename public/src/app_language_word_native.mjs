7443ac70c5b1d327dd2014df8556ea4bdbdac599
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 18:40:53 -0400

    2024-08-09T22:40:53.300Z function_auto_after app_language_word_native

--- public/src/app_language_word_native.mjs
+++ public/src/app_language_word_native.mjs
@@ -3,6 +3,6 @@ import { html_style_bold } from "./html_style_bold.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 export function app_language_word_native(root, text) {
   let p = html_p_text(root, text);
-  html_span_text();
+  html_span_text(p, text);
   html_style_bold(p);
 }
