ffaebb0c63a7391e2edc8c27ae77dceea7589243
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 18:26:39 -0400

    2024-08-09T22:26:37.511Z function_auto_after html_css_generate

--- public/src/html_css_generate.mjs
+++ public/src/html_css_generate.mjs
@@ -1,5 +1,7 @@
+import { string_suffix_without } from "./string_suffix_without.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 export async function html_css_generate(name, html_css_code) {
+  let prefix = string_suffix_without(html_css_generate.name, "_generate");
   await function_new_generic(
     function_name,
     args_string,
