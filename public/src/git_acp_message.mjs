5d9c4ba30baa344068a948aada13dd66b47c1f62
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 12:41:41 -0400

    2024-08-09T16:41:36.552Z function_auto_after git_acp_message

--- public/src/git_acp_message.mjs
+++ public/src/git_acp_message.mjs
@@ -1,6 +1,8 @@
+import { list_add } from "./list_add.mjs";
 import { git_push } from "./git_push.mjs";
 import { git_ac_message } from "./git_ac_message.mjs";
 export async function git_acp_message(commit_message) {
   let result = await git_ac_message(commit_message);
   let p_result = await git_push();
+  list_add(p_result);
 }
