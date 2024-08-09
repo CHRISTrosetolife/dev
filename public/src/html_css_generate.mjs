4923374f8e91eea44fdbda2366502fe8da814616
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 18:33:22 -0400

    2024-08-09T22:33:22.395Z function_auto_after html_css_generate

--- public/src/html_css_generate.mjs
+++ public/src/html_css_generate.mjs
@@ -1,5 +1,4 @@
 import { function_names_to_lookup } from "./function_names_to_lookup.mjs";
-import { fn_name } from "./fn_name.mjs";
 import { html_code_generate } from "./html_code_generate.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
@@ -7,13 +6,13 @@ import { function_name_generated } from "./function_name_generated.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 export async function html_css_generate(name, html_css_code) {
   let body_string = await html_code_generate("link", html_css_code);
-  let fn_name = string_combine_multiple([
+  let fn_new_name = string_combine_multiple([
     function_name_generated(html_css_generate),
     "_",
     name,
   ]);
   await function_new_generic(
-    fn_name,
+    name,
     "",
     body_string,
     false,
@@ -21,5 +20,5 @@ export async function html_css_generate(name, html_css_code) {
     false,
     file_overwrite,
   );
-  return function_names_to_lookup([fn_name]);
+  return function_names_to_lookup([name]);
 }
