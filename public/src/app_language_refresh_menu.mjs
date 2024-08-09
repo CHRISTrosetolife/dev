7ea2460e9c4dc2c3cd0bd0100852083eda7eb3f8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 16:56:11 -0400

    2024-08-09T20:56:04.592Z function_auto_after app_language_refresh_menu

--- public/src/app_language_refresh_menu.mjs
+++ public/src/app_language_refresh_menu.mjs
@@ -1,3 +1,4 @@
+import { add_1 } from "./add_1.mjs";
 import { app_language_group_get } from "./app_language_group_get.mjs";
 import { app_language_group_index_changed } from "./app_language_group_index_changed.mjs";
 import { app_language_position_initial } from "./app_language_position_initial.mjs";
@@ -11,15 +12,19 @@ export async function app_language_refresh_menu(context) {
   html_clear_scroll_top_centered(root);
   let { count } = await app_language_group_get(context, "index");
   await each_range_async(count, async (item) => {
-    html_button_width_full_text_click(root, item, async () => {
-      storage_local_set(context.app_fn, "group_index", item);
-      await app_language_group_index_changed(context);
-      storage_local_set(
-        context.app_fn,
-        "position",
-        app_language_position_initial(context),
-      );
-      await app_language_refresh_node(context);
-    });
+    html_button_width_full_text_click(
+      root,
+      "group " + add_1(item),
+      async () => {
+        storage_local_set(context.app_fn, "group_index", item);
+        await app_language_group_index_changed(context);
+        storage_local_set(
+          context.app_fn,
+          "position",
+          app_language_position_initial(context),
+        );
+        await app_language_refresh_node(context);
+      },
+    );
   });
 }
