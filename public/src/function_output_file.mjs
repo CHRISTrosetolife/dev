3f1ca1014eed574d7c85c7b9b4e9958b4845066d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 07:50:11 -0400

    2024-08-09T11:50:03.786Z function_auto_after function_output_file

--- public/src/function_output_file.mjs
+++ public/src/function_output_file.mjs
@@ -5,8 +5,9 @@ import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 import { function_run } from "./function_run.mjs";
 export async function function_output_file(function_name) {
   let f_path = path_join([function_name, date_string_iso_file() + ".txt"]);
+  let fn = function_output_file;
   let output_path = folder_gitignore_path(
-    path_join([function_output_file.name, f_path]),
+    path_join([fn.name, f_path]),
   );
   let result = await function_run(function_name, []);
   await file_overwrite_json(output_path, result);
