6387f51a913c04c29965ec7fcc0dd8c1fdd3b1b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 18:29:33 -0400

    2024-08-09T22:29:32.785Z function_auto_after html_css_generate

--- public/src/html_css_generate.mjs
+++ public/src/html_css_generate.mjs
@@ -1,14 +1,18 @@
+import { file_overwrite } from "./file_overwrite.mjs";
+import { js_unparse } from "./js_unparse.mjs";
+import { js_code_statement_return } from "./js_code_statement_return.mjs";
+import { function_name_generated } from "./function_name_generated.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 export async function html_css_generate(name, html_css_code) {
   let prefix = string_suffix_without(html_css_generate.name, "_generate");
   await function_new_generic(
-    function_name,
-    args_string,
-    body_string,
-    open,
-    imports,
-    async_is,
-    writer,
+    function_name_generated(html_css_generate),
+    "",
+    js_code_statement_return(js_unparse(r)),
+    false,
+    [],
+    false,
+    file_overwrite,
   );
 }
