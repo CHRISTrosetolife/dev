bd8384f66135d6203bb659177beb0b1b2c2209cf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 15:24:23 -0400

    2024-08-09T19:24:21.589Z function_auto_after app_language_refresh_menu

--- public/src/app_language_refresh_menu.mjs
+++ public/src/app_language_refresh_menu.mjs
@@ -1,8 +1,11 @@
+import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 import { each_range_async } from "./each_range_async.mjs";
 import { app_language_refresh_review } from "./app_language_refresh_review.mjs";
 import { app_language_text } from "./app_language_text.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export async function app_language_refresh_menu(context) {
+  let { root, language_fluent } = context;
+  html_clear_scroll_top_centered(root);
   await each_range_async(2, async (item) => {
     html_button_width_full_text_click(
       root,
