6dba08d53e2f5aa0a31a3b3e1de7c5e2dfeca835
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 07:55:22 -0400

    2024-08-09T11:55:11.071Z function_auto_after storage_upload_string

--- public/src/storage_upload_string.mjs
+++ public/src/storage_upload_string.mjs
@@ -2,7 +2,7 @@ import { uuid_file } from "./uuid_file.mjs";
 import { storage_upload_file } from "./storage_upload_file.mjs";
 import { file_write } from "./file_write.mjs";
 export async function storage_upload_string(string, destination) {
-  await uuid_file(async (file_path) => {
+  await uuid_file(storage_upload_string, async (file_path) => {
     await file_write(file_path, string);
     await storage_upload_file(file_path, destination);
   });
