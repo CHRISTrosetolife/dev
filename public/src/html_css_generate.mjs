239ae2337cb829dc46f57f206a8c8554d462f834
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 18:30:47 -0400

    2024-08-09T22:30:46.855Z function_auto_after html_css_generate

--- public/src/html_css_generate.mjs
+++ public/src/html_css_generate.mjs
@@ -1,8 +1,6 @@
 import { html_code_generate } from "./html_code_generate.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
-import { js_unparse } from "./js_unparse.mjs";
-import { js_code_statement_return } from "./js_code_statement_return.mjs";
 import { function_name_generated } from "./function_name_generated.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 export async function html_css_generate(name, html_css_code) {
@@ -14,7 +12,7 @@ export async function html_css_generate(name, html_css_code) {
       name,
     ]),
     "",
-    js_code_statement_return(js_unparse(r)),
+    body_string,
     false,
     [],
     false,
