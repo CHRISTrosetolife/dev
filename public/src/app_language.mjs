c8c1928ee31674e8e90f0bf75cb90c3b771c90c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 14:58:27 -0400

    2024-08-09T18:58:23.628Z function_auto_after app_language

--- public/src/app_language.mjs
+++ public/src/app_language.mjs
@@ -1,8 +1,7 @@
+import { app_language_group_index_set } from "./app_language_group_index_set.mjs";
 import { app_language_refresh_node } from "./app_language_refresh_node.mjs";
 import { list_index_last } from "./list_index_last.mjs";
 import { storage_local_initialize } from "./storage_local_initialize.mjs";
-import { app_language_group_path } from "./app_language_group_path.mjs";
-import { http_storage } from "./http_storage.mjs";
 import { html_style_default_font_size } from "./html_style_default_font_size.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
@@ -27,14 +26,7 @@ export async function app_language(
   html_style_default_font_size(3.5);
   let group_index = storage_local_initialize(app_fn, "group_index", 0);
   group_index = 1;
-  let { group, definitions, inverted } = await http_storage(
-    app_language_group_path(language_learn, language_fluent, group_index),
-  );
-  object_merge(context, {
-    group,
-    definitions,
-    inverted,
-  });
+  await app_language_group_index_set(context, group_index);
   storage_local_initialize(app_fn, "position", {
     left: 0,
     right: list_index_last(context.group),
---
+++ public/src/app_language_group_index_set.mjs
@@ -0,0 +1,17 @@
+import { object_merge } from "./object_merge.mjs";
+import { app_language_group_path } from "./app_language_group_path.mjs";
+import { http_storage } from "./http_storage.mjs";
+export async function app_language_group_index_set(context, group_index) {
+  let { group, definitions, inverted } = await http_storage(
+    app_language_group_path(
+      context.language_learn,
+      context.language_fluent,
+      group_index,
+    ),
+  );
+  object_merge(context, {
+    group,
+    definitions,
+    inverted,
+  });
+}
