57d949ed1d9891a976b7efe0222369aef106296b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 18:33:54 -0400

    2024-08-09T22:33:53.480Z html_css_generate flags '<link   rel=stylesheet   href=https://cdn.jsdelivr.net/gh/lipis/flag-icons@7.2.3/css/flag-icons.min.css />'

---
+++ public/src/html_css_flags.mjs
@@ -0,0 +1,11 @@
+import { html_attribute_set } from "./html_attribute_set.mjs";
+import { html_element } from "./html_element.mjs";
+export function html_css_flags() {
+  let c = html_element(parent, "link");
+  html_attribute_set(c, "rel", "stylesheet");
+  html_attribute_set(
+    c,
+    "href",
+    "https://cdn.jsdelivr.net/gh/lipis/flag-icons@7.2.3/css/flag-icons.min.css",
+  );
+}
