54788743258f3f55ab06daab6fc56ca77e7c5217
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 15:23:35 -0400

    2024-08-09T19:23:31.836Z function_auto_after app_language_refresh_menu

--- public/src/app_language_refresh_menu.mjs
+++ public/src/app_language_refresh_menu.mjs
@@ -1,9 +1,9 @@
-import { each_async } from "./each_async.mjs";
+import { each_range_async } from "./each_range_async.mjs";
 import { app_language_refresh_review } from "./app_language_refresh_review.mjs";
 import { app_language_text } from "./app_language_text.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export async function app_language_refresh_menu(context) {
-  await each_async(list, async (item) => {});
+  await each_range_async(list, async (item) => {});
   html_button_width_full_text_click(
     root,
     "📃 " + (await app_language_text(language_fluent, "review")),
