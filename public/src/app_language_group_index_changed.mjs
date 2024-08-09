fc209b0c22b325cce9aeb7e0bb8638a445e64fbd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 16:38:39 -0400

    2024-08-09T20:38:38.421Z function_auto_after app_language_group_index_changed

--- public/src/app_language_group_index_changed.mjs
+++ public/src/app_language_group_index_changed.mjs
@@ -3,11 +3,12 @@ import { object_merge } from "./object_merge.mjs";
 import { app_language_group_path } from "./app_language_group_path.mjs";
 import { http_storage } from "./http_storage.mjs";
 export async function app_language_group_index_changed(context) {
+  let group_index = storage_local_initialize(context.app_fn, "group_index");
   let { group, definitions, inverted } = await http_storage(
     app_language_group_path(
       context.language_learn,
       context.language_fluent,
-      storage_local_initialize(context.app_fn, "group_index"),
+      group_index,
     ),
   );
   object_merge(context, {
