f4fd278f70500f4f78b21fce8e2205532608826e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 15:46:50 -0400

    2024-08-09T19:46:42.832Z function_auto_after app_language_refresh_menu

--- public/src/app_language_refresh_menu.mjs
+++ public/src/app_language_refresh_menu.mjs
@@ -1,14 +1,14 @@
+import { app_language_refresh_node } from "./app_language_refresh_node.mjs";
 import { app_language_group_index_set } from "./app_language_group_index_set.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 import { each_range_async } from "./each_range_async.mjs";
-import { app_language_refresh_review } from "./app_language_refresh_review.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export async function app_language_refresh_menu(context) {
   let { root, language_fluent } = context;
   html_clear_scroll_top_centered(root);
   await each_range_async(2, async (item) => {
     html_button_width_full_text_click(root, item, async () => {
-      app_language_refresh_review(context);
+      await app_language_refresh_node(context);
       await app_language_group_index_set(context, group_index);
     });
   });
