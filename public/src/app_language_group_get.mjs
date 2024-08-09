654c8cfdfe85bc137d01f6d07c4dc0e2d949ce97
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 16:39:57 -0400

    2024-08-09T20:39:52.532Z function_auto_after app_language_group_index_changed

---
+++ public/src/app_language_group_get.mjs
@@ -0,0 +1,11 @@
+import { app_language_group_path } from "./app_language_group_path.mjs";
+import { http_storage } from "./http_storage.mjs";
+export async function app_language_group_get(context, group_index) {
+  return await http_storage(
+    app_language_group_path(
+      context.language_learn,
+      context.language_fluent,
+      group_index,
+    ),
+  );
+}
--- public/src/app_language_group_index_changed.mjs
+++ public/src/app_language_group_index_changed.mjs
@@ -1,16 +1,9 @@
+import { app_language_group_get } from "./app_language_group_get.mjs";
 import { storage_local_initialize } from "./storage_local_initialize.mjs";
 import { object_merge } from "./object_merge.mjs";
-import { app_language_group_path } from "./app_language_group_path.mjs";
-import { http_storage } from "./http_storage.mjs";
 export async function app_language_group_index_changed(context) {
   let group_index = storage_local_initialize(context.app_fn, "group_index");
-  let result = await http_storage(
-    app_language_group_path(
-      context.language_learn,
-      context.language_fluent,
-      group_index,
-    ),
-  );
+  let result = await app_language_group_get(context, group_index);
   let { group, definitions, inverted } = result;
   object_merge(context, {
     group,
