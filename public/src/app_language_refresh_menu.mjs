adfef3c40b72a1589efd51d21d26e37f3cdb8dee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 15:55:51 -0400

    2024-08-09T19:55:46.986Z function_auto_after app_language_refresh_menu

--- public/src/app_language_refresh_menu.mjs
+++ public/src/app_language_refresh_menu.mjs
@@ -1,3 +1,4 @@
+import { app_language_position_initial } from "./app_language_position_initial.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
 import { app_language_refresh_node } from "./app_language_refresh_node.mjs";
 import { app_language_group_index_set } from "./app_language_group_index_set.mjs";
@@ -11,6 +12,11 @@ export async function app_language_refresh_menu(context) {
     html_button_width_full_text_click(root, item, async () => {
       storage_local_set(context.app_fn, "group_index", item);
       await app_language_group_index_set(context, item);
+      storage_local_set(
+        context.app_fn,
+        "position",
+        app_language_position_initial(context),
+      );
       await app_language_refresh_node(context);
     });
   });
