6ca62e6d7739131645e4267313c1ea581402bf8a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 19:46:04 -0400

    2024-08-09T23:45:54.346Z function_auto_after app_language_refresh_menu

--- public/src/app_language_refresh_menu.mjs
+++ public/src/app_language_refresh_menu.mjs
@@ -7,7 +7,7 @@ import { html_button_width_full_text_click } from "./html_button_width_full_text
 export async function app_language_refresh_menu(context) {
   let { root } = context;
   html_clear_scroll_top_centered(root);
-  let { count } = await app_language_group_get(context, "index");
+  let { groups } = await app_language_group_get(context, "index");
   await each_range_async(count, async (group_index) => {
     html_button_width_full_text_click(
       root,
