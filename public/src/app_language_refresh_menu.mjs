67b24228381f12532c47e5b6a6e9ba52e3f8c41a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 15:46:18 -0400

    2024-08-09T19:46:03.570Z function_auto_after app_language_refresh_menu

--- public/src/app_language_refresh_menu.mjs
+++ public/src/app_language_refresh_menu.mjs
@@ -1,18 +1,15 @@
+import { app_language_group_index_set } from "./app_language_group_index_set.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 import { each_range_async } from "./each_range_async.mjs";
 import { app_language_refresh_review } from "./app_language_refresh_review.mjs";
-import { app_language_text } from "./app_language_text.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export async function app_language_refresh_menu(context) {
   let { root, language_fluent } = context;
   html_clear_scroll_top_centered(root);
   await each_range_async(2, async (item) => {
-    html_button_width_full_text_click(
-      root,
-      "📃 " + (await app_language_text(language_fluent, "review")),
-      () => {
-        app_language_refresh_review(context);
-      },
-    );
+    html_button_width_full_text_click(root, item, async () => {
+      app_language_refresh_review(context);
+      await app_language_group_index_set(context, group_index);
+    });
   });
 }
