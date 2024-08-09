99987aad197984d446ed49a548f9337bd403cf1b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 18:32:34 -0400

    2024-08-09T22:32:34.159Z function_auto_after html_css_generate

--- public/src/html_css_generate.mjs
+++ public/src/html_css_generate.mjs
@@ -1,3 +1,4 @@
+import { fn_name } from "./fn_name.mjs";
 import { html_code_generate } from "./html_code_generate.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
@@ -5,12 +6,13 @@ import { function_name_generated } from "./function_name_generated.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 export async function html_css_generate(name, html_css_code) {
   let body_string = await html_code_generate("link", html_css_code);
+  let fn_name = string_combine_multiple([
+    function_name_generated(html_css_generate),
+    "_",
+    name,
+  ]);
   await function_new_generic(
-    string_combine_multiple([
-      function_name_generated(html_css_generate),
-      "_",
-      name,
-    ]),
+    fn_name,
     "",
     body_string,
     false,
