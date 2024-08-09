e28c111616cb13b700961dc91b9b5e8b0c0af215
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 12:46:08 -0400

    2024-08-09T16:46:03.874Z function_auto_after ceb_definitions_change

--- public/src/ceb_definitions_change.mjs
+++ public/src/ceb_definitions_change.mjs
@@ -1,11 +1,7 @@
-import { each_async } from "./each_async.mjs";
 import { ceb_bible_words_definitions_all } from "./ceb_bible_words_definitions_all.mjs";
 import { app_ceb_upload } from "./app_ceb_upload.mjs";
 import { function_cache_refresh } from "./function_cache_refresh.mjs";
 export async function ceb_definitions_change() {
-  let fns = [ceb_bible_words_definitions_all];
-  await each_async(fns, async (fn) => {
-    await function_cache_refresh(fn.name);
-  });
+  await function_cache_refresh(ceb_bible_words_definitions_all.name);
   await app_ceb_upload();
 }
