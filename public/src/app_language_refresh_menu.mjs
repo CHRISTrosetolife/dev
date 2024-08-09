362c58acb248d914d29707428eefdb5d690718f2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 19:46:20 -0400

    2024-08-09T23:46:13.591Z function_auto_after app_language_refresh_menu

--- public/src/app_language_refresh_menu.mjs
+++ public/src/app_language_refresh_menu.mjs
@@ -1,14 +1,14 @@
+import { each_index_async } from "./each_index_async.mjs";
 import { app_language_group_get } from "./app_language_group_get.mjs";
 import { app_language_group_index_set } from "./app_language_group_index_set.mjs";
 import { add_1 } from "./add_1.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
-import { each_range_async } from "./each_range_async.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export async function app_language_refresh_menu(context) {
   let { root } = context;
   html_clear_scroll_top_centered(root);
   let { groups } = await app_language_group_get(context, "index");
-  await each_range_async(count, async (group_index) => {
+  await each_index_async(count, async (group_index) => {
     html_button_width_full_text_click(
       root,
       "group " + add_1(group_index),
