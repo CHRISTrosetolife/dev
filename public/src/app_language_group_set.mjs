2fdba5ccaff5cbb022d50d43db6ce6ebc633f8df
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 16:58:14 -0400

    2024-08-09T20:58:12.912Z function_auto_after app_language_refresh_menu

---
+++ public/src/app_language_group_set.mjs
@@ -0,0 +1,14 @@
+import { app_language_refresh_node } from "./app_language_refresh_node.mjs";
+import { app_language_position_initial } from "./app_language_position_initial.mjs";
+import { app_language_group_index_changed } from "./app_language_group_index_changed.mjs";
+import { storage_local_set } from "./storage_local_set.mjs";
+export async function app_language_group_set(context, group_index_new) {
+  storage_local_set(context.app_fn, "group_index", group_index_new);
+  await app_language_group_index_changed(context);
+  storage_local_set(
+    context.app_fn,
+    "position",
+    app_language_position_initial(context),
+  );
+  await app_language_refresh_node(context);
+}
--- public/src/app_language_refresh_menu.mjs
+++ public/src/app_language_refresh_menu.mjs
@@ -1,9 +1,6 @@
+import { app_language_group_set } from "./app_language_group_set.mjs";
 import { add_1 } from "./add_1.mjs";
 import { app_language_group_get } from "./app_language_group_get.mjs";
-import { app_language_group_index_changed } from "./app_language_group_index_changed.mjs";
-import { app_language_position_initial } from "./app_language_position_initial.mjs";
-import { storage_local_set } from "./storage_local_set.mjs";
-import { app_language_refresh_node } from "./app_language_refresh_node.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 import { each_range_async } from "./each_range_async.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
@@ -11,19 +8,12 @@ export async function app_language_refresh_menu(context) {
   let { root } = context;
   html_clear_scroll_top_centered(root);
   let { count } = await app_language_group_get(context, "index");
-  await each_range_async(count, async (item) => {
+  await each_range_async(count, async (group_index) => {
     html_button_width_full_text_click(
       root,
-      "group " + add_1(item),
+      "group " + add_1(group_index),
       async () => {
-        storage_local_set(context.app_fn, "group_index", item);
-        await app_language_group_index_changed(context);
-        storage_local_set(
-          context.app_fn,
-          "position",
-          app_language_position_initial(context),
-        );
-        await app_language_refresh_node(context);
+        await app_language_group_set(context, group_index);
       },
     );
   });
