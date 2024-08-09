d393aca92de929fe9bbd71079fe067b87fc26951
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 12:43:46 -0400

    2024-08-09T16:43:43.289Z function_auto_after command_line_exec

--- public/src/command_line_exec.mjs
+++ public/src/command_line_exec.mjs
@@ -1,4 +1,4 @@
 import { command_line_exec_generic } from "./command_line_exec_generic.mjs";
 export async function command_line_exec(command) {
-  await command_line_exec_generic(command, {});
+  return await command_line_exec_generic(command, {});
 }
