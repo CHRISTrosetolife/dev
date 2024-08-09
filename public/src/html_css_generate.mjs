64aa50fc8294bee63345a727cbcb10aec629b23b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 18:25:54 -0400

    2024-08-09T22:25:52.755Z function_auto_after html_css_generate

--- public/src/html_css_generate.mjs
+++ public/src/html_css_generate.mjs
@@ -1,1 +1,12 @@
-export function html_css_generate(name, html_css_code) {}
+import { function_new_generic } from "./function_new_generic.mjs";
+export async function html_css_generate(name, html_css_code) {
+  await function_new_generic(
+    function_name,
+    args_string,
+    body_string,
+    open,
+    imports,
+    async_is,
+    writer,
+  );
+}
