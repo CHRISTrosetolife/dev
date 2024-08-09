26834db0f8c3c47cabe60a59465720d00b0359d9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 07:54:49 -0400

    2024-08-09T11:54:41.982Z function_auto_after uuid_file

--- public/src/uuid_file.mjs
+++ public/src/uuid_file.mjs
@@ -5,7 +5,7 @@ import { uuid } from "./uuid.mjs";
 export async function uuid_file(fn, lambda) {
   let u = await uuid();
   let file_name = file_name_json(u);
-  let file_path = folder_gitignore_path_function(uuid_file, file_name);
+  let file_path = folder_gitignore_path_function(fn, file_name);
   try {
     await lambda(file_name);
   } finally {
