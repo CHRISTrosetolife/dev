8a401a51ee835803cc17b0b19d353270e9b54ddf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 17:09:14 -0400

    2024-08-09T21:09:06.613Z function_auto_after app_language_group_get

---
+++ public/src/app_language_group_index_changed.mjs
@@ -0,0 +1,12 @@
+import { app_language_group_get } from "./app_language_group_get.mjs";
+import { object_merge } from "./object_merge.mjs";
+export async function app_language_group_index_changed(context) {
+  let group_index = await app_language_group_get(context);
+  let result = await app_language_group_get(context, group_index);
+  let { group, definitions, inverted } = result;
+  object_merge(context, {
+    group,
+    definitions,
+    inverted,
+  });
+}
