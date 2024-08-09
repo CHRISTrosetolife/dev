8ddae54ef1b36ff15585e0a2968a8a383ed1cf1b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-09 07:54:11 -0400

    2024-08-09T11:54:04.322Z function_auto_after uuid_file

--- public/src/uuid_file.mjs
+++ public/src/uuid_file.mjs
@@ -1,12 +1,14 @@
+import { folder_gitignore_path_function } from "./folder_gitignore_path_function.mjs";
 import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
 import { file_name_json } from "./file_name_json.mjs";
 import { uuid } from "./uuid.mjs";
 export async function uuid_file(lambda) {
   let u = await uuid();
-  let file_path = file_name_json(u);
+  let file_name = file_name_json(u);
+  let file_path = folder_gitignore_path_function(uuid_file, file_name);
   try {
-    await lambda(file_path);
+    await lambda(file_name);
   } finally {
-    await file_delete_if_exists(file_path);
+    await file_delete_if_exists(file_name);
   }
 }
