ea00ff75596d88b700acd1908665fb17db6a46e7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 17:08:35 -0400

    2024-08-10T21:08:34.376Z function_auto_after bible_interlinear_words_greek_audio_upload

--- public/src/bible_interlinear_words.mjs
+++ public/src/bible_interlinear_words.mjs
@@ -1,6 +1,6 @@
 import { list_adder_unique } from "./list_adder_unique.mjs";
 import { bible_interlinear_each_token } from "./bible_interlinear_each_token.mjs";
-export async function bible_interlinear_words_greek(books_get) {
+export async function bible_interlinear_words(books_get) {
   let books = await books_get();
   return list_adder_unique((la) => {
     bible_interlinear_each_token(books, lambda);
--- public/src/bible_interlinear_words_greek_audio_upload.mjs
+++ public/src/bible_interlinear_words_greek_audio_upload.mjs
@@ -1,11 +1,11 @@
+import { bible_interlinear_words } from "./bible_interlinear_words.mjs";
 import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
 import { bible_interlinear_words_audio_upload_generic } from "./bible_interlinear_words_audio_upload_generic.mjs";
 import { keyboard_greek } from "./keyboard_greek.mjs";
-import { bible_interlinear_words_greek } from "./bible_interlinear_words_greek.mjs";
 export async function bible_interlinear_words_greek_audio_upload() {
   let alphabet = keyboard_greek();
   let language_code = "el";
-  let words = await bible_interlinear_words_greek(bible_interlinear_cache_new);
+  let words = await bible_interlinear_words(bible_interlinear_cache_new);
   await bible_interlinear_words_audio_upload_generic(
     words,
     alphabet,
