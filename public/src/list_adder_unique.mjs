4d0a6fc2b0a4ba3ee753c25189004a5072b42a1e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 17:45:07 -0400

    2024-08-10T21:45:05.273Z function_auto_after list_adder_unique

--- public/src/list_adder_unique.mjs
+++ public/src/list_adder_unique.mjs
@@ -1,15 +1,12 @@
-import { log } from "./log.mjs";
+import { add } from "./add.mjs";
 import { list_add } from "./list_add.mjs";
 export function list_adder_unique(lambda) {
-  let included = new WeakMap();
+  let included = new Set();
   let result = [];
   lambda((item) => {
     if (!included.has(item)) {
       list_add(result, item);
-      log({
-        item,
-      });
-      included.set(item, true);
+      included.add(item);
     }
   });
   return result;
