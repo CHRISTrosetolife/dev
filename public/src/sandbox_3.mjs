cc9fe412c63bc801734360185eefbd64a558cc31
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 13:56:53 -0400

    2024-08-10T17:56:48.875Z sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -4,9 +4,14 @@ export function sandbox_3() {
   log({
     text,
   });
-  text.send("9043144052", "A sample text message!", undefined, function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
+  text.sendText(
+    "9043144052",
+    "A sample text message!",
+    undefined,
+    function (err) {
+      if (err) {
+        console.log(err);
+      }
+    },
+  );
 }
