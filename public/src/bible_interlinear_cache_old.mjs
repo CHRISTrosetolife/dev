366c403d7341026369d438cdc0edb979a1c627b6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 17:05:12 -0400

    2024-08-10T21:05:12.338Z function_auto_after bible_interlinear_cache_old

--- public/src/bible_interlinear_cache_old.mjs
+++ public/src/bible_interlinear_cache_old.mjs
@@ -1,7 +1,8 @@
-import { list_take_reverse } from "./list_take_reverse.mjs";
+import { list_size } from "./list_size.mjs";
+import { list_take } from "./list_take.mjs";
 import { bible_interlinear_cache } from "./bible_interlinear_cache.mjs";
 export async function bible_interlinear_cache_old() {
   let books = await bible_interlinear_cache();
-  let new_testament_books = list_take_reverse(books, 27);
+  let new_testament_books = list_take(books, list_size(books) - 27);
   return new_testament_books;
 }
