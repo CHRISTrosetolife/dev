ad6cb04e009d346955daa5367f4438ace3548727
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 17:40:05 -0400

    2024-08-10T21:40:03.366Z function_auto_after list_adder_unique

--- public/src/list_adder_unique.mjs
+++ public/src/list_adder_unique.mjs
@@ -6,6 +6,7 @@ export function list_adder_unique(lambda) {
   lambda((item) => {
     if (!list_includes(result, item)) {
       list_add(result, item);
+      included.set(item, true);
     }
   });
   return result;
