366850f60a1874721883a95908bb6854a1cb3b62
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 17:14:37 -0400

    2024-08-10T21:14:36.933Z function_auto_after bible_interlinear_words_audio_upload_generic

--- public/src/bible_interlinear_words_audio_upload_generic.mjs
+++ public/src/bible_interlinear_words_audio_upload_generic.mjs
@@ -1,7 +1,5 @@
+import { repeat } from "./repeat.mjs";
 import { bible_interlinear_words } from "./bible_interlinear_words.mjs";
-import { promise_all } from "./promise_all.mjs";
-import { audio_upload } from "./audio_upload.mjs";
-import { each_log_async } from "./each_log_async.mjs";
 import { list_chunk } from "./list_chunk.mjs";
 import { string_filter } from "./string_filter.mjs";
 import { string_includes } from "./string_includes.mjs";
@@ -18,13 +16,8 @@ export async function bible_interlinear_words_audio_upload_generic(
   let lambda = (character) => string_includes(alphabet, character);
   let m2 = list_map(m1, (word) => string_filter(word, lambda));
   let chunks = list_chunk(m2, 20);
+  await repeat(() => {});
   try {
-    await each_log_async(chunks, async (chunk) => {
-      let m3 = list_map(chunk, async (word) => {
-        await audio_upload(language_code, word);
-      });
-      await promise_all(m3);
-    });
   } catch (e) {
     await bible_interlinear_words_audio_upload_generic(
       words,
