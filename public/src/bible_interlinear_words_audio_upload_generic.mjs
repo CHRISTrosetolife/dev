fa92dfd42667a04d53d795d86f9f2e22cf32f93f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 17:03:59 -0400

    2024-08-10T21:03:56.713Z function_auto_after bible_interlinear_words_greek_audio_upload

---
+++ public/src/bible_interlinear_words_audio_upload_generic.mjs
@@ -0,0 +1,32 @@
+import { promise_all } from "./promise_all.mjs";
+import { audio_upload } from "./audio_upload.mjs";
+import { each_log_async } from "./each_log_async.mjs";
+import { list_chunk } from "./list_chunk.mjs";
+import { string_filter } from "./string_filter.mjs";
+import { string_includes } from "./string_includes.mjs";
+import { string_case_lower } from "./string_case_lower.mjs";
+import { list_map } from "./list_map.mjs";
+export async function bible_interlinear_words_audio_upload_generic(
+  words,
+  alphabet,
+  language_code,
+) {
+  try {
+    let m1 = list_map(words, string_case_lower);
+    let lambda = (character) => string_includes(alphabet, character);
+    let m2 = list_map(m1, (word) => string_filter(word, lambda));
+    let chunks = list_chunk(m2, 20);
+    await each_log_async(chunks, async (chunk) => {
+      let m3 = list_map(chunk, async (word) => {
+        await audio_upload(language_code, word);
+      });
+      await promise_all(m3);
+    });
+  } catch (e) {
+    await bible_interlinear_words_audio_upload_generic(
+      words,
+      alphabet,
+      language_code,
+    );
+  }
+}
--- public/src/bible_interlinear_words_greek_audio_upload.mjs
+++ public/src/bible_interlinear_words_greek_audio_upload.mjs
@@ -1,36 +1,13 @@
-import { list_size } from "./list_size.mjs";
-import { each_index_async } from "./each_index_async.mjs";
-import { log } from "./log.mjs";
-import { promise_all } from "./promise_all.mjs";
-import { list_chunk } from "./list_chunk.mjs";
-import { string_filter } from "./string_filter.mjs";
+import { bible_interlinear_words_audio_upload_generic } from "./bible_interlinear_words_audio_upload_generic.mjs";
 import { keyboard_greek } from "./keyboard_greek.mjs";
-import { audio_upload } from "./audio_upload.mjs";
-import { string_case_lower } from "./string_case_lower.mjs";
-import { list_map } from "./list_map.mjs";
 import { bible_interlinear_words_greek } from "./bible_interlinear_words_greek.mjs";
-import { string_includes } from "./string_includes.mjs";
 export async function bible_interlinear_words_greek_audio_upload() {
-  try {
-    let alphabet = keyboard_greek();
-    let language_code = "el";
-    let words = await bible_interlinear_words_greek();
-    let m1 = list_map(words, string_case_lower);
-    let lambda = (character) => string_includes(alphabet, character);
-    let m2 = list_map(m1, (word) => string_filter(word, lambda));
-    let chunks = list_chunk(m2, 20);
-    let chunks_size = list_size(chunks);
-    await each_index_async(chunks, async (chunk, index) => {
-      let m3 = list_map(chunk, async (word) => {
-        await audio_upload(language_code, word);
-      });
-      await promise_all(m3);
-      log({
-        chunks_size,
-        index,
-      });
-    });
-  } catch (e) {
-    await bible_interlinear_words_greek_audio_upload();
-  }
+  let alphabet = keyboard_greek();
+  let language_code = "el";
+  let words = await bible_interlinear_words_greek();
+  await bible_interlinear_words_audio_upload_generic(
+    words,
+    alphabet,
+    language_code,
+  );
 }
