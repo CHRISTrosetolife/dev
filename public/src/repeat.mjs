bc41b409cc50aa9bf58241e7c3dc583a16473e6b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 17:13:14 -0400

    2024-08-10T21:13:13.463Z function_auto_after repeat

--- public/src/repeat.mjs
+++ public/src/repeat.mjs
@@ -1,16 +1,7 @@
 import { bible_interlinear_words_audio_upload_generic } from "./bible_interlinear_words_audio_upload_generic.mjs";
-import { promise_all } from "./promise_all.mjs";
-import { audio_upload } from "./audio_upload.mjs";
-import { list_map } from "./list_map.mjs";
-import { each_log_async } from "./each_log_async.mjs";
 export async function repeat(lambda) {
   try {
-    await each_log_async(chunks, async (chunk) => {
-      let m3 = list_map(chunk, async (word) => {
-        await audio_upload(language_code, word);
-      });
-      await promise_all(m3);
-    });
+    await lambda();
   } catch (e) {
     await bible_interlinear_words_audio_upload_generic(
       words,
