9526dbc19382bd1bdafcdd53d9c090247480f9aa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 17:40:21 -0400

    2024-08-10T21:40:20.546Z function_auto_after list_adder_unique

--- public/src/list_adder_unique.mjs
+++ public/src/list_adder_unique.mjs
@@ -1,10 +1,9 @@
-import { list_includes } from "./list_includes.mjs";
 import { list_add } from "./list_add.mjs";
 export function list_adder_unique(lambda) {
   let included = new WeakMap();
   let result = [];
   lambda((item) => {
-    if (!list_includes(result, item)) {
+    if (!included.has(item)) {
       list_add(result, item);
       included.set(item, true);
     }
