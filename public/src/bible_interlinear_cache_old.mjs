9a5e5863bc2a50719f01d67935d046638b3c1972
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 17:05:21 -0400

    2024-08-10T21:05:20.575Z function_auto_after bible_interlinear_cache_old

--- public/src/bible_interlinear_cache_old.mjs
+++ public/src/bible_interlinear_cache_old.mjs
@@ -3,6 +3,6 @@ import { list_take } from "./list_take.mjs";
 import { bible_interlinear_cache } from "./bible_interlinear_cache.mjs";
 export async function bible_interlinear_cache_old() {
   let books = await bible_interlinear_cache();
-  let new_testament_books = list_take(books, list_size(books) - 27);
-  return new_testament_books;
+  let old_testament_books = list_take(books, list_size(books) - 27);
+  return old_testament_books;
 }
