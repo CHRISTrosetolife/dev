739365831d9dc00d43e4d3f06d642d30b866caf9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 18:02:24 -0400

    2024-08-10T22:02:23.643Z function_auto_after bible_interlinear_words_greek_audio_upload

--- public/src/bible_interlinear_words_greek_audio_upload.mjs
+++ public/src/bible_interlinear_words_greek_audio_upload.mjs
@@ -3,12 +3,12 @@ import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
 import { bible_interlinear_words_audio_upload_generic } from "./bible_interlinear_words_audio_upload_generic.mjs";
 import { keyboard_greek } from "./keyboard_greek.mjs";
 export async function bible_interlinear_words_greek_audio_upload() {
-  let alphabet = alphabet_get();
+  let alphabet = keyboard_greek();
   let filter = (character) => string_includes(alphabet, character);
   let language_code = "el";
   await bible_interlinear_words_audio_upload_generic(
     bible_interlinear_cache_new,
-    keyboard_greek,
+    filter,
     language_code,
   );
 }
