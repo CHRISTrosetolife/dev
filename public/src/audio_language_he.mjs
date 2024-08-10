df085cd0a5c43bdac08e910316b6c944182acd24
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 17:30:01 -0400

    2024-08-10T21:30:00.523Z function_auto_after audio_language_he

---
+++ public/src/audio_language_he.mjs
@@ -0,0 +1,15 @@
+export function audio_language_he() {
+  return {
+    language_code: "el-GR",
+    voices: [
+      {
+        code: "Wavenet-A",
+        male: false,
+      },
+      {
+        code: "Standard-A",
+        male: false,
+      },
+    ],
+  };
+}
