5efdb23910afb494fbc2ba6fae5cabcdd90c8801
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 17:10:51 -0400

    2024-08-10T21:10:50.901Z function_auto_after bible_interlinear_words_audio_upload_generic

--- public/src/bible_interlinear_words_audio_upload_generic.mjs
+++ public/src/bible_interlinear_words_audio_upload_generic.mjs
@@ -1,3 +1,5 @@
+import { bible_interlinear_cache_old } from "./bible_interlinear_cache_old.mjs";
+import { bible_interlinear_words } from "./bible_interlinear_words.mjs";
 import { promise_all } from "./promise_all.mjs";
 import { audio_upload } from "./audio_upload.mjs";
 import { each_log_async } from "./each_log_async.mjs";
@@ -7,10 +9,11 @@ import { string_includes } from "./string_includes.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { list_map } from "./list_map.mjs";
 export async function bible_interlinear_words_audio_upload_generic(
-  words,
+  books_get,
   alphabet,
   language_code,
 ) {
+  let words = await bible_interlinear_words(bible_interlinear_cache_old);
   try {
     let m1 = list_map(words, string_case_lower);
     let lambda = (character) => string_includes(alphabet, character);
