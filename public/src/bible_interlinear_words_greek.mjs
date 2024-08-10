3aa94978671d7e3b9cdb186489ad14f775ab4216
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 17:08:03 -0400

    2024-08-10T21:08:02.778Z function_auto_after bible_interlinear_words_greek

--- public/src/bible_interlinear_words_greek.mjs
+++ public/src/bible_interlinear_words_greek.mjs
@@ -1,7 +1,7 @@
 import { list_adder_unique } from "./list_adder_unique.mjs";
 import { bible_interlinear_each_token } from "./bible_interlinear_each_token.mjs";
 import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
-export async function bible_interlinear_words_greek() {
+export async function bible_interlinear_words_greek(books_get) {
   let books = await bible_interlinear_cache_new();
   return list_adder_unique((la) => {
     bible_interlinear_each_token(books, lambda);
