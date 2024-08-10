5f4f20660867b12d0da1523c5e3c4a7258132f3b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 17:55:20 -0400

    2024-08-10T21:55:19.157Z function_auto_after bible_interlinear_words_hebrew_audio_upload

--- public/src/bible_interlinear_words_hebrew_audio_upload.mjs
+++ public/src/bible_interlinear_words_hebrew_audio_upload.mjs
@@ -1,7 +1,12 @@
+import { list_filter } from "./list_filter.mjs";
+import { bible_interlinear_words } from "./bible_interlinear_words.mjs";
 import { bible_interlinear_cache_old } from "./bible_interlinear_cache_old.mjs";
 import { bible_interlinear_words_audio_upload_generic } from "./bible_interlinear_words_audio_upload_generic.mjs";
 import { keyboard_greek } from "./keyboard_greek.mjs";
+import { string_includes } from "./string_includes.mjs";
 export async function bible_interlinear_words_hebrew_audio_upload() {
+  let words = await bible_interlinear_words(bible_interlinear_cache_old);
+  return list_filter(words, (w) => string_includes(w, " "));
   let language_code = "he";
   await bible_interlinear_words_audio_upload_generic(
     bible_interlinear_cache_old,
