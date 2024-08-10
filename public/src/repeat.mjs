3e89ed5c9711193aaf95516db5f2afa57c4a896a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 17:13:58 -0400

    2024-08-10T21:13:58.171Z function_auto_after repeat

--- public/src/repeat.mjs
+++ public/src/repeat.mjs
@@ -1,12 +1,16 @@
 import { bible_interlinear_words_audio_upload_generic } from "./bible_interlinear_words_audio_upload_generic.mjs";
 export async function repeat(lambda) {
-  try {
-    await lambda();
-  } catch (e) {
-    await bible_interlinear_words_audio_upload_generic(
-      words,
-      alphabet,
-      language_code,
-    );
+  let success = false;
+  while (!success) {
+    try {
+      await lambda();
+      success = true;
+    } catch (e) {
+      await bible_interlinear_words_audio_upload_generic(
+        words,
+        alphabet,
+        language_code,
+      );
+    }
   }
 }
