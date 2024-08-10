d1c9f92c46eba0abd4195980fee5ad5bd154e742
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-10 13:55:34 -0400

    2024-08-10T17:55:32.943Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,7 +1,7 @@
 import { log } from "./log.mjs";
 import text from "textbelt";
 export function sandbox_3() {
-  text.send("9491234567", "A sample text message!", undefined, function (err) {
+  text.send("9043144052", "A sample text message!", undefined, function (err) {
     if (err) {
       console.log(err);
     }
