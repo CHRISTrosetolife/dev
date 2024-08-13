9f4b99028f0768c2283758c7ceaeaeb24735d579
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 17:39:06 -0400

    2024-08-13T21:39:06.038Z function_auto_after list_offset

--- public/src/list_offset.mjs
+++ public/src/list_offset.mjs
@@ -1,4 +1,5 @@
+import { equal } from "./equal.mjs";
 import { list_offset_by } from "./list_offset_by.mjs";
 export function list_offset(list, item, offset) {
-  return list_offset_by();
+  return list_offset_by(list, item, offset, equal);
 }
