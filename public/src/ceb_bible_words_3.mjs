e995359e9782de8ecbe178d37b927eb9d38c138a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:36:48 -0400

    2024-08-13T17:36:47.683Z function_auto_after ceb_bible_words_3

--- public/src/ceb_bible_words_3.mjs
+++ public/src/ceb_bible_words_3.mjs
@@ -9,7 +9,7 @@ export async function ceb_bible_words_3(args) {
     let hrefs = await bible_ceb_3_hrefs();
     await each_index_async(hrefs, async (href, index) => {
       if (args.new) {
-        if (string_substring(href, 3)) {
+        if (string_substring(href, 1, 3)) {
         }
       }
       let verses = await bible_ceb_3_chapter(index);
