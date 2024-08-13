c349178db2f6b20d2958a768d83a8dc2d743efb3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 14:42:29 -0400

    2024-08-13T18:42:28.249Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,12 +1,7 @@
-import { list_find_property } from "./list_find_property.mjs";
 import { ceb_bible_words_score } from "./ceb_bible_words_score.mjs";
 import { each } from "./each.mjs";
-import { list_unique } from "./list_unique.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 import { list_take } from "./list_take.mjs";
-import { promise_all } from "./promise_all.mjs";
-import { list_map } from "./list_map.mjs";
-import { ceb_definition } from "./ceb_definition.mjs";
 import { log } from "./log.mjs";
 import { ceb_bible_words_count_cache_new } from "./ceb_bible_words_count_cache_new.mjs";
 import { list_includes } from "./list_includes.mjs";
@@ -17,15 +12,11 @@ export async function sandbox_3() {
   let words = list_map_property(taken, "word");
   let scored = await ceb_bible_words_score();
   each(scored, (s) => {
-    let word = s;
+    let { word } = s;
     if (list_includes(words, s.word)) {
-      log({});
+      log({
+        word,
+      });
     }
-    let match = list_find_property(scored, "word", word);
   });
-  let mapped = list_map(words, ceb_definition);
-  let unawaited = await promise_all(mapped);
-  let m2 = list_map_property(unawaited, "word");
-  let us = list_unique(m2);
-  each(us, log);
 }
