62f35dfcac17a8aff5679d308becd9e59e28a9b6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 17:33:53 -0400

    2024-08-13T21:33:48.679Z c list_index list_index_generic

---
+++ public/src/list_index_generic.mjs
@@ -0,0 +1,7 @@
+import { number_less_than } from "./number_less_than.mjs";
+import { assert_not } from "./assert_not.mjs";
+export function list_index_generic(list, element) {
+  let index = list.indexOf(element);
+  assert_not(number_less_than, [index, 0]);
+  return index;
+}
