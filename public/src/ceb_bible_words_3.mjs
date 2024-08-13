776637c7abe624beccc57ca1814e6ed4ecf9bba0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:35:34 -0400

    2024-08-13T17:35:34.179Z function_auto_after ceb_bible_words_3

--- public/src/ceb_bible_words_3.mjs
+++ public/src/ceb_bible_words_3.mjs
@@ -3,12 +3,13 @@ import { each } from "./each.mjs";
 import { list_adder_unique_async } from "./list_adder_unique_async.mjs";
 import { bible_ceb_3_chapter } from "./bible_ceb_3_chapter.mjs";
 import { bible_ceb_3_hrefs } from "./bible_ceb_3_hrefs.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
 export async function ceb_bible_words_3(args) {
   let words = await list_adder_unique_async(async (la) => {
     let hrefs = await bible_ceb_3_hrefs();
     await each_index_async(hrefs, async (href, index) => {
       if (args.new) {
-        if (href) {
+        if (string_starts_with(href, "B000")) {
         }
       }
       let verses = await bible_ceb_3_chapter(index);
