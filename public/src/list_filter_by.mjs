3d8b5734e0137e55827d349805396f1fdc766254
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 17:50:47 -0400

    2024-08-13T21:50:46.606Z de list_filter_by

--- public/src/list_filter_by.mjs
+++
@@ -1,8 +0,0 @@
-import { each } from "./each.mjs";
-import { list_adder } from "./list_adder.mjs";
-export function list_filter_by(list, filter) {
-  return list_adder((la) => {
-    each(list2, (item2) => {});
-  });
-  return list.filter((item) => filter(item));
-}
