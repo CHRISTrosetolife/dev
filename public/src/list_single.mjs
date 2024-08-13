96b6577f49f2120ec4efe5804a30e026b1f24345
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:06:50 -0400

    2024-08-13T17:06:48.950Z function_auto_after list_single

--- public/src/list_single.mjs
+++ public/src/list_single.mjs
@@ -1,7 +1,7 @@
+import { assert_message } from "./assert_message.mjs";
 import { list_size_1 } from "./list_size_1.mjs";
-import { assert } from "./assert.mjs";
 import { list_first } from "./list_first.mjs";
 export function list_single(list) {
-  assert(list_size_1, [list]);
+  assert_message(list_size_1, [list]);
   return list_first(list);
 }
