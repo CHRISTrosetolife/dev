fbeaf711adc0af05a5536a36140947ec8fd812c9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 11:11:10 -0400

    2024-08-13T15:11:09.800Z function_auto_after list_map_async

--- public/src/list_map_async.mjs
+++ public/src/list_map_async.mjs
@@ -1,7 +1,9 @@
+import { list_adder_async } from "./list_adder_async.mjs";
 import { each_async } from "./each_async.mjs";
 import { list_add } from "./list_add.mjs";
 export async function list_map_async(list, mapper) {
   let result = [];
+  await list_adder_async(async (la) => {});
   await each_async(list, async (l) => {
     let mapped = mapper(l);
     let waited = await mapped;
