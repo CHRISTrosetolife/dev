6e7d46cc2a033965f07b2de698067331d3a2639f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 17:05:02 -0400

    2024-08-13T21:05:01.664Z function_auto_after list_after

--- public/src/list_after.mjs
+++ public/src/list_after.mjs
@@ -1,6 +1,6 @@
 import { list_offset } from "./list_offset.mjs";
 import { add_1 } from "./add_1.mjs";
-export function list_after(settings_choices, settings) {
-  let after = list_offset(settings_choices, settings, add_1);
+export function list_after(list, item) {
+  let after = list_offset(list, item, add_1);
   return after;
 }
