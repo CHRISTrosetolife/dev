0c481b817671a320d8cbf9cf93c03f26e5a36e95
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:04:16 -0400

    2024-08-13T17:04:14.811Z function_auto_after list_filter_properties

--- public/src/list_filter_properties.mjs
+++ public/src/list_filter_properties.mjs
@@ -1,5 +1,5 @@
 import { object_properties_match } from "./object_properties_match.mjs";
 import { list_filter } from "./list_filter.mjs";
-export function list_filter_properties(indexed, matcher) {
-  return list_filter(indexed, (i) => object_properties_match(matcher, i));
+export function list_filter_properties(list, matcher) {
+  return list_filter(list, (item) => object_properties_match(matcher, item));
 }
