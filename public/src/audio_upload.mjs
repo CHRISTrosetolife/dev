54cb7938a81b910bf3a590b41ebcc4aa622f2e43
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 11:17:10 -0400

    2024-08-13T15:17:09.328Z function_auto_after audio_upload

--- public/src/audio_upload.mjs
+++ public/src/audio_upload.mjs
@@ -1,3 +1,4 @@
+import { list_map_index_async } from "./list_map_index_async.mjs";
 import { audio_upload_file } from "./audio_upload_file.mjs";
 import { string_is } from "./string_is.mjs";
 import { assert } from "./assert.mjs";
@@ -6,12 +7,11 @@ import { audio_language } from "./audio_language.mjs";
 import { gcloud_tts } from "./gcloud_tts.mjs";
 import { log } from "./log.mjs";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
-import { each_index_async } from "./each_index_async.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function audio_upload(language, text) {
   assert(string_is, [text]);
   let { language_code, voices } = await audio_language(language);
-  await each_index_async(voices, async (voice, voice_index) => {
+  await list_map_index_async(voices, async (voice, voice_index) => {
     let file_path = await audio_path(language, voice_index, text);
     let output_path = folder_gitignore_path(file_path);
     let { code, male } = voice;
