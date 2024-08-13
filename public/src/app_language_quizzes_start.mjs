ab96092894c6538201b4ecf8b25ee4c077aec659
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 17:03:15 -0400

    2024-08-13T21:03:14.778Z function_auto_after app_language_quizzes_start

--- public/src/app_language_quizzes_start.mjs
+++ public/src/app_language_quizzes_start.mjs
@@ -1,3 +1,4 @@
+import { storage_local_get } from "./storage_local_get.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
 import { app_language_refresh_quiz } from "./app_language_refresh_quiz.mjs";
 import { app_language_atoms_slice } from "./app_language_atoms_slice.mjs";
@@ -7,6 +8,11 @@ export async function app_language_quizzes_start(context, chunk_sizes) {
   let { app_fn, group } = context;
   let atoms = app_language_atoms_slice(app_fn, group);
   storage_local_set(
+    context.app_fn,
+    "settings_choices",
+    app_language_quiz_settings(atoms, chunk_sizes),
+  );
+  storage_local_get(
     context.app_fn,
     "settings_choices",
     app_language_quiz_settings(atoms, chunk_sizes),
