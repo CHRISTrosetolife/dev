fb7be487fc52c96573d1befa833dbcf35869086d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 14:25:26 -0400

    2024-08-13T18:25:21.337Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,3 +1,4 @@
+import { list_take } from "./list_take.mjs";
 import { promise_all } from "./promise_all.mjs";
 import { list_map } from "./list_map.mjs";
 import { each_range_async } from "./each_range_async.mjs";
@@ -8,6 +9,7 @@ import { ceb_bible_words_count_cache_new } from "./ceb_bible_words_count_cache_n
 export async function sandbox_3() {
   let words = await ceb_bible_words_count_cache_new();
   let count = 500;
+  let taken = list_take(words, count);
   let mapped = list_map(words, ceb_definition);
   let unawaited = await promise_all(mapped);
   await each_range_async(count, async (i) => {
