88ef0b16937e56baec562afdfe3f5678c294f023
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:55:00 -0400

    2024-08-13T17:54:59.687Z function_auto_after ceb_bible_words_count

--- public/src/ceb_bible_words_count.mjs
+++ public/src/ceb_bible_words_count.mjs
@@ -1,7 +1,7 @@
 import { bible_words_count_generic } from "./bible_words_count_generic.mjs";
 import { ceb_bible_words_cache } from "./ceb_bible_words_cache.mjs";
-export async function ceb_bible_words_count() {
-  let all = await ceb_bible_words_cache();
+export async function ceb_bible_words_count(args) {
+  let all = await ceb_bible_words_cache(args);
   let symbols = "_-–—,;:!?.'‘’\"“”()[]}¶/`¯|€0123456789";
   return bible_words_count_generic(all, symbols);
 }
