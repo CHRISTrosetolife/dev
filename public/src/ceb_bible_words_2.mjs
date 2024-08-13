a103b43a17d9d6fcf39d08c59600e4691d46270b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 12:03:25 -0400

    2024-08-13T16:03:24.366Z function_auto_after ceb_bible_words_2

--- public/src/ceb_bible_words_2.mjs
+++ public/src/ceb_bible_words_2.mjs
@@ -9,7 +9,11 @@ import { bible_ceb_2_books_hrefs } from "./bible_ceb_2_books_hrefs.mjs";
 export async function ceb_bible_words_2(args) {
   let book_hrefs = await bible_ceb_2_books_hrefs();
   if (args.new) {
-    each_index(book_hrefs, (bh, index) => {});
+    let columns = 7;
+    each_index(book_hrefs, (bh, index) => {
+      if (index % columns === column) {
+      }
+    });
     list_adder((la) => {});
     book_hrefs = list_take_bible_books_new(book_hrefs);
   }
