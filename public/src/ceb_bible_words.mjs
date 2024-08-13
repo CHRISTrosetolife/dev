5c63d775e5defa748ab3fee1f112d98f20f256ce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:54:12 -0400

    2024-08-13T17:54:11.714Z function_auto_after ceb_bible_words

--- public/src/ceb_bible_words.mjs
+++ public/src/ceb_bible_words.mjs
@@ -5,7 +5,6 @@ import { list_map } from "./list_map.mjs";
 import { range_1 } from "./range_1.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { each_async } from "./each_async.mjs";
-import { function_run } from "./function_run.mjs";
 export async function ceb_bible_words(args) {
   let count = 5;
   let fn_names = list_map(range_1(count), (i) =>
@@ -13,8 +12,7 @@ export async function ceb_bible_words(args) {
   );
   let all = await list_adder_multiple_async(async (la) => {
     await each_async(fn_names, async (fn_name) => {
-      await function_cache(ceb_bible_words, []);
-      let words = await function_run(fn_name, [args]);
+      let words = await function_cache(fn_name, [args]);
       la(words);
     });
   });
