e543f3380f9e4feb2396864b8e4c920f48383ff3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 14:39:16 -0400

    2024-08-13T18:39:15.713Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -9,10 +9,10 @@ import { log } from "./log.mjs";
 import { ceb_bible_words_count_cache_new } from "./ceb_bible_words_count_cache_new.mjs";
 export async function sandbox_3() {
   let count = 1000;
-  let words = await ceb_bible_words_count_cache_new();
-  let taken = list_take(words, count);
-  let m3 = list_map_property(taken, "word");
-  let mapped = list_map(m3, ceb_definition);
+  let wcs = await ceb_bible_words_count_cache_new();
+  let taken = list_take(wcs, count);
+  let words = list_map_property(taken, "word");
+  let mapped = list_map(words, ceb_definition);
   let unawaited = await promise_all(mapped);
   let m2 = list_map_property(unawaited, "word");
   let us = list_unique(m2);
