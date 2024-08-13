fcb287c587694fcd18615e0fecf6aca328fe43f4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 11:58:31 -0400

    2024-08-13T15:58:30.563Z function_auto_after ceb_bible_words_2

--- public/src/ceb_bible_words_2.mjs
+++ public/src/ceb_bible_words_2.mjs
@@ -1,3 +1,4 @@
+import { list_take_bible_books_new } from "./list_take_bible_books_new.mjs";
 import { list_adder_unique_async } from "./list_adder_unique_async.mjs";
 import { each } from "./each.mjs";
 import { bible_ceb_2_book } from "./bible_ceb_2_book.mjs";
@@ -5,6 +6,9 @@ import { each_index_only_async } from "./each_index_only_async.mjs";
 import { bible_ceb_2_books_hrefs } from "./bible_ceb_2_books_hrefs.mjs";
 export async function ceb_bible_words_2() {
   let book_hrefs = await bible_ceb_2_books_hrefs();
+  if (args.new) {
+    book_hrefs = list_take_bible_books_new(book_hrefs);
+  }
   let words = await list_adder_unique_async(async (law) => {
     await each_index_only_async(book_hrefs, async (book_index) => {
       let verses = await bible_ceb_2_book(book_index);
