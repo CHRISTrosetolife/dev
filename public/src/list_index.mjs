a22666c7d948160b15d0afedb1ae8a812d3c5bc0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 17:36:20 -0400

    2024-08-13T21:36:18.134Z r list_index_generic list_index_by

--- public/src/list_index.mjs
+++ public/src/list_index.mjs
@@ -1,5 +1,5 @@
-import { list_index_generic } from "./list_index_generic.mjs";
+import { list_index_by } from "./list_index_by.mjs";
 import { equal } from "./equal.mjs";
 export function list_index(list, element) {
-  return list_index_generic(list, element, equal);
+  return list_index_by(list, element, equal);
 }
