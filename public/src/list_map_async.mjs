2a4212b657ca39454a3ab7b3032ab7caae6c5cde
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 11:11:34 -0400

    2024-08-13T15:11:33.580Z function_auto_after list_map_async

--- public/src/list_map_async.mjs
+++ public/src/list_map_async.mjs
@@ -1,12 +1,11 @@
 import { list_adder_async } from "./list_adder_async.mjs";
 import { each_async } from "./each_async.mjs";
-import { list_add } from "./list_add.mjs";
 export async function list_map_async(list, mapper) {
   return await list_adder_async(async (la) => {
     await each_async(list, async (l) => {
       let mapped = mapper(l);
       let waited = await mapped;
-      list_add(result, waited);
+      la(waited);
     });
   });
 }
