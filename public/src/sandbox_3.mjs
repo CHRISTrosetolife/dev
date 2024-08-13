f5932671d72e9ec622d9d939394642ce9ecb1565
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 11:56:26 -0400

    2024-08-13T15:56:24.458Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,6 +1,7 @@
 import { ceb_bible_words_1 } from "./ceb_bible_words_1.mjs";
 import text from "textbelt";
 export async function sandbox_3() {
-  return;
-  await ceb_bible_words_1([]);
+  return await ceb_bible_words_1({
+    new: true,
+  });
 }
