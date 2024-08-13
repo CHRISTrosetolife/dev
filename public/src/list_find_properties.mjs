45077af9651d709b31bb1e5bf08523d031f57a3e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:03:59 -0400

    2024-08-13T17:03:59.333Z function_auto_after list_find_properties

--- public/src/list_find_properties.mjs
+++ public/src/list_find_properties.mjs
@@ -3,4 +3,5 @@ import { list_filter_properties } from "./list_filter_properties.mjs";
 export function list_find_properties(indexed, matcher) {
   let matches = list_filter_properties(indexed, matcher);
   let match = list_single(matches);
+  return match;
 }
