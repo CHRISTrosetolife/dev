24401cb226ac42feafa25b23f524f2c0f39e9993
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 14:42:51 -0400

    2024-08-13T18:42:50.855Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -6,7 +6,7 @@ import { log } from "./log.mjs";
 import { ceb_bible_words_count_cache_new } from "./ceb_bible_words_count_cache_new.mjs";
 import { list_includes } from "./list_includes.mjs";
 export async function sandbox_3() {
-  let count = 1000;
+  let count = 500;
   let wcs = await ceb_bible_words_count_cache_new();
   let taken = list_take(wcs, count);
   let words = list_map_property(taken, "word");
