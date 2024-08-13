9b1d475ca216cb76cb7eb2a2b0f44fbc2377ac4d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 14:11:30 -0400

    2024-08-13T18:11:29.534Z function_auto_after ceb_bible_words_2

--- public/src/ceb_bible_words_2.mjs
+++ public/src/ceb_bible_words_2.mjs
@@ -1,8 +1,8 @@
+import { each_async } from "./each_async.mjs";
 import { list_take_bible_books_new } from "./list_take_bible_books_new.mjs";
 import { list_adder_unique_async } from "./list_adder_unique_async.mjs";
 import { each } from "./each.mjs";
 import { bible_ceb_2_book } from "./bible_ceb_2_book.mjs";
-import { each_index_only_async } from "./each_index_only_async.mjs";
 import { bible_ceb_2_books_hrefs } from "./bible_ceb_2_books_hrefs.mjs";
 export async function ceb_bible_words_2(args) {
   let book_hrefs = await bible_ceb_2_books_hrefs();
@@ -10,8 +10,8 @@ export async function ceb_bible_words_2(args) {
     book_hrefs = list_take_bible_books_new(book_hrefs);
   }
   let words = await list_adder_unique_async(async (law) => {
-    await each_index_only_async(book_hrefs, async (book_index) => {
-      let verses = await bible_ceb_2_book(book_index);
+    await each_async(book_hrefs, async (book_href) => {
+      let verses = await bible_ceb_2_book(book_href);
       each(verses, (v) =>
         each(v.tokens, (t) => {
           law(t);
