fb40537055238cf3deec056367c167c50f804ef1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 17:37:29 -0400

    2024-08-13T21:37:29.141Z function_auto_after list_offset_generic

--- public/src/list_offset_generic.mjs
+++ public/src/list_offset_generic.mjs
@@ -1,6 +1,6 @@
-import { list_index } from "./list_index.mjs";
+import { list_index_by } from "./list_index_by.mjs";
 export function list_offset_generic(fn, list, item, offset, lambda_equal) {
-  let settings_index = list_index(list, item);
+  let settings_index = list_index_by(list, item, lambda_equal);
   let after = fn(list, offset(settings_index));
   return after;
 }
