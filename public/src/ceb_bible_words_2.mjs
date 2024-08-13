96ae96db0235ad26bc1a76325d34b8a6f58bad95
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 12:04:28 -0400

    2024-08-13T16:04:26.085Z function_auto_after ceb_bible_words_2

--- public/src/ceb_bible_words_2.mjs
+++ public/src/ceb_bible_words_2.mjs
@@ -11,7 +11,7 @@ export async function ceb_bible_words_2(args) {
   let book_hrefs = await bible_ceb_2_books_hrefs();
   if (args.new) {
     let columns = 7;
-    list_adder((la) => {
+    return list_adder((la) => {
       each_range(columns, (column) => {
         each_index(book_hrefs, (bh, index) => {
           if (index % columns === column) {
