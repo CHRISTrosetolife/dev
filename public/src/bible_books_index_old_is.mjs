cf1cf2dc62ada1cc23874bf2c511dc85b0a32ae4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:50:49 -0400

    2024-08-13T17:50:48.428Z function_auto_after bible_books_index_old_is

--- public/src/bible_books_index_old_is.mjs
+++ public/src/bible_books_index_old_is.mjs
@@ -1,6 +1,6 @@
 import { bible_books_old_count } from "./bible_books_old_count.mjs";
 export function bible_books_index_old_is(parsed) {
   let old_count = bible_books_old_count();
-  let old_is = parsed <= old_count;
+  let old_is = parsed < old_count;
   return old_is;
 }
