6d417aca8651450cc66efbb79b59bf3ffc554773
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 11:46:38 -0400

    2024-08-13T15:46:37.969Z function_auto_after app_ceb_upload

--- public/src/app_ceb_upload.mjs
+++ public/src/app_ceb_upload.mjs
@@ -23,98 +23,94 @@ import { object_list_invert } from "./object_list_invert.mjs";
 import { ceb_bible_words_definitions_atoms } from "./ceb_bible_words_definitions_atoms.mjs";
 import { app_language_group_size } from "./app_language_group_size.mjs";
 export async function app_ceb_upload() {
-  try {
-    let from = "ceb";
-    let to = "en";
-    let audio_only = false;
-    let audio_upload_run = true;
-    let group_upload = false;
-    let group_local_compare_to_new = false;
-    let group_count = app_language_group_size();
-    let { atoms, definitions, inverted } =
-      await ceb_bible_words_definitions_atoms();
-    let groups = list_chunk(atoms, group_count);
-    let profiles = [
-      {
-        definitions,
-        from,
-        to,
-        invert: false,
-      },
-      {
-        definitions: inverted,
-        to: from,
-        from: to,
-        invert: true,
-      },
-    ];
-    await each_async(profiles, async (profile) => {
-      await each_index_async(groups, async (group, group_index) => {
-        await each_async(group, async (atom) => {
-          await each_async(atom, async (pair) => {
-            if (profile.invert) {
-              list_reverse(pair);
-            }
-          });
-        });
-        if (audio_upload_run) {
-          await each_async(list_chunk(group, 20), async (chunk) => {
-            let mapped = list_map(chunk, async (atom) => {
-              let createds = await list_map_async(atom, async (pair) => {
-                let b = list_first(pair);
-                let r = await audio_upload(profile.from, b);
-                return r;
-              });
-              return list_any_created(createds);
-            });
-            let createds = await promise_all(mapped);
-            if (object_property_get(list_any_created(createds), "created")) {
-              log("chunk finished");
-            }
-          });
-        }
-        if (audio_only) {
-          return;
-        }
-        let words = list_adder_unique((la) =>
-          each(group, (a) =>
-            each(a, (pair) => {
-              la(list_first(pair));
-            }),
-          ),
-        );
-        let definitions = {};
-        each(words, (w) => {
-          let ds = object_property_get(profile.definitions, w);
-          object_property_set(definitions, w, ds);
-        });
-        let inverted = object_list_invert(definitions);
-        let result_new = {
-          group,
-          definitions,
-          inverted,
-        };
-        if (group_upload) {
-          let existing_path = await app_language_group_upload(
-            profile,
-            group_index,
-            result_new,
-          );
-          if (group_local_compare_to_new) {
-            let existing = await file_read_json(existing_path);
-            assert(equal_json, [result_new, existing]);
-            return group;
+  let from = "ceb";
+  let to = "en";
+  let audio_only = false;
+  let audio_upload_run = true;
+  let group_upload = false;
+  let group_local_compare_to_new = false;
+  let group_count = app_language_group_size();
+  let { atoms, definitions, inverted } =
+    await ceb_bible_words_definitions_atoms();
+  let groups = list_chunk(atoms, group_count);
+  let profiles = [
+    {
+      definitions,
+      from,
+      to,
+      invert: false,
+    },
+    {
+      definitions: inverted,
+      to: from,
+      from: to,
+      invert: true,
+    },
+  ];
+  await each_async(profiles, async (profile) => {
+    await each_index_async(groups, async (group, group_index) => {
+      await each_async(group, async (atom) => {
+        await each_async(atom, async (pair) => {
+          if (profile.invert) {
+            list_reverse(pair);
           }
+        });
+      });
+      if (audio_upload_run) {
+        await each_async(list_chunk(group, 20), async (chunk) => {
+          let mapped = list_map(chunk, async (atom) => {
+            let createds = await list_map_async(atom, async (pair) => {
+              let b = list_first(pair);
+              let r = await audio_upload(profile.from, b);
+              return r;
+            });
+            return list_any_created(createds);
+          });
+          let createds = await promise_all(mapped);
+          if (object_property_get(list_any_created(createds), "created")) {
+            log("chunk finished");
+          }
+        });
+      }
+      if (audio_only) {
+        return;
+      }
+      let words = list_adder_unique((la) =>
+        each(group, (a) =>
+          each(a, (pair) => {
+            la(list_first(pair));
+          }),
+        ),
+      );
+      let definitions = {};
+      each(words, (w) => {
+        let ds = object_property_get(profile.definitions, w);
+        object_property_set(definitions, w, ds);
+      });
+      let inverted = object_list_invert(definitions);
+      let result_new = {
+        group,
+        definitions,
+        inverted,
+      };
+      if (group_upload) {
+        let existing_path = await app_language_group_upload(
+          profile,
+          group_index,
+          result_new,
+        );
+        if (group_local_compare_to_new) {
+          let existing = await file_read_json(existing_path);
+          assert(equal_json, [result_new, existing]);
+          return group;
         }
-      });
-      await app_language_group_upload(profile, "index", {
-        groups: list_map(groups, (g) => ({
-          first: list_first_nested(g),
-          last: list_last_nested(g),
-        })),
-      });
+      }
     });
-  } catch (e) {
-    await app_ceb_upload();
-  }
+    await app_language_group_upload(profile, "index", {
+      groups: list_map(groups, (g) => ({
+        first: list_first_nested(g),
+        last: list_last_nested(g),
+      })),
+    });
+  });
 }
