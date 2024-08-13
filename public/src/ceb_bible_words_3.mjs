18e6b8291fad513b9f905e8cc5958c558bd95137
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:43:30 -0400

    2024-08-13T17:43:29.570Z function_auto_after ceb_bible_words_3

--- public/src/ceb_bible_words_3.mjs
+++ public/src/ceb_bible_words_3.mjs
@@ -7,13 +7,13 @@ import { bible_ceb_3_chapter } from "./bible_ceb_3_chapter.mjs";
 import { bible_ceb_3_hrefs } from "./bible_ceb_3_hrefs.mjs";
 import { string_substring } from "./string_substring.mjs";
 export async function ceb_bible_words_3(args) {
-  let old_count = bible_books_old_count();
   let words = await list_adder_unique_async(async (la) => {
     let hrefs = await bible_ceb_3_hrefs();
     await each_index_async(hrefs, async (href, index) => {
       if (args.new) {
         let unparsed = string_substring(href, 1, 3);
         let parsed = integer_parse(unparsed);
+        let old_count = bible_books_old_count();
         let old_is = parsed <= old_count;
         if (old_is) {
           return;
