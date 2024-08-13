8be45384a218f13ef2f39e9a842a7638fb862f25
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:56:06 -0400

    2024-08-13T17:56:06.232Z function_auto_after ceb_bible_words_definitions_all

--- public/src/ceb_bible_words_definitions_all.mjs
+++ public/src/ceb_bible_words_definitions_all.mjs
@@ -1,7 +1,9 @@
 import { ceb_bible_words_count_cache } from "./ceb_bible_words_count_cache.mjs";
 import { ceb_bible_words_definitions_get } from "./ceb_bible_words_definitions_get.mjs";
 export async function ceb_bible_words_definitions_all() {
-  let words_ceb = await ceb_bible_words_count_cache();
+  let words_ceb = await ceb_bible_words_count_cache({
+    new: false,
+  });
   let result = await ceb_bible_words_definitions_get(words_ceb);
   return result;
 }
