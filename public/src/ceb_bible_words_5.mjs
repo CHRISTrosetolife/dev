e5e7fca2fdb58b4ca9b01ab9ea65d7cbdd956d5c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:46:33 -0400

    2024-08-13T17:46:32.718Z function_auto_after ceb_bible_words_5

--- public/src/ceb_bible_words_5.mjs
+++ public/src/ceb_bible_words_5.mjs
@@ -7,7 +7,7 @@ import { bible_chapters } from "./bible_chapters.mjs";
 import { bible_books } from "./bible_books.mjs";
 import { list_adder_unique_async } from "./list_adder_unique_async.mjs";
 import { each } from "./each.mjs";
-export async function ceb_bible_words_5() {
+export async function ceb_bible_words_5(args) {
   let words = await list_adder_unique_async(async (la) => {
     await each_index_async(
       await bible_books("engbsb"),
