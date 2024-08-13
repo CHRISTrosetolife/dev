808ded1d10608c73bed93768f69be3ec64f16c1c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 17:39:32 -0400

    2024-08-13T21:39:31.850Z function_auto_after list_after_by

--- public/src/list_after_by.mjs
+++ public/src/list_after_by.mjs
@@ -1,6 +1,6 @@
-import { list_offset } from "./list_offset.mjs";
+import { list_offset_by } from "./list_offset_by.mjs";
 import { add_1 } from "./add_1.mjs";
 export function list_after_by(list, item) {
-  let after = list_offset(list, item, add_1);
+  let after = list_offset_by(list, item, add_1);
   return after;
 }
