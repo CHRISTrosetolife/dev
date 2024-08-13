dfceee3d5b8e6d41d2c37fdc78d393b01df31ea2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:43:08 -0400

    2024-08-13T17:43:06.399Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,6 +1,6 @@
-import { ceb_bible_words_4 } from "./ceb_bible_words_4.mjs";
+import { ceb_bible_words_3 } from "./ceb_bible_words_3.mjs";
 export async function sandbox_3() {
-  return await ceb_bible_words_4({
+  return await ceb_bible_words_3({
     new: true,
   });
 }
