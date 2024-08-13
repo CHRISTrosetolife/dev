f2816ab92f123d23ca147983daf8f51634cef391
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 17:51:26 -0400

    2024-08-13T21:51:25.500Z function_auto_after list_without_by

--- public/src/list_without_by.mjs
+++ public/src/list_without_by.mjs
@@ -1,5 +1,5 @@
+import { equal } from "./equal.mjs";
 import { list_filter } from "./list_filter.mjs";
-import { equal_not } from "./equal_not.mjs";
 export function list_without_by(list, item) {
-  return list_filter(list, (a) => equal_not(a, item));
+  return list_filter(list, (a) => !equal(a, item));
 }
