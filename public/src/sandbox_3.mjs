5cef92181623bec6542e7087671a7f6acd1b89d6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 14:29:14 -0400

    2024-08-13T18:29:13.751Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,3 +1,4 @@
+import { list_unique } from "./list_unique.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 import { list_take } from "./list_take.mjs";
@@ -14,6 +15,7 @@ export async function sandbox_3() {
   let mapped = list_map(m3, ceb_definition);
   let unawaited = await promise_all(mapped);
   let m2 = list_map_property(unawaited, "word");
+  let us = list_unique(m2);
   await each_index_async(m2, async (m, i) => {
     log(i + 1 + ". " + m);
   });
