742a1232b5acf5949440f83be3fcfc8bf83b792c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 17:35:39 -0400

    2024-08-13T21:35:39.236Z function_auto_after list_index_generic

--- public/src/list_index_generic.mjs
+++ public/src/list_index_generic.mjs
@@ -1,7 +1,7 @@
 import { each_index } from "./each_index.mjs";
 import { number_less_than } from "./number_less_than.mjs";
 import { assert_not } from "./assert_not.mjs";
-export function list_index_generic(list, element) {
+export function list_index_generic(list, element, lambda_equal) {
   let index = -1;
   each_index(list, (candidate, candidate_index) => {
     if (lambda_equal(candidate, element)) {
