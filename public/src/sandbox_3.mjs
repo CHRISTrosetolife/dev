ed8678001e5dfbd3ebc1aa645e55e35c85ffb4c4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 14:17:32 -0400

    2024-08-13T18:17:31.661Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,6 +1,11 @@
+import { ceb_bible_words_2 } from "./ceb_bible_words_2.mjs";
+import { function_cache } from "./function_cache.mjs";
 import { ceb_bible_words_5 } from "./ceb_bible_words_5.mjs";
 export async function sandbox_3() {
   return await ceb_bible_words_5({
+    new: true,
+  });
+  return await function_cache(ceb_bible_words_2, {
     new: true,
   });
 }
