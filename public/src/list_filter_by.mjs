bab642325a793008ec7ae9130696a51d5b4e68f5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 17:50:24 -0400

    2024-08-13T21:50:23.335Z function_auto_after list_filter_by

--- public/src/list_filter_by.mjs
+++ public/src/list_filter_by.mjs
@@ -1,5 +1,8 @@
+import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function list_filter_by(list, filter) {
-  return list_adder((la) => {});
+  return list_adder((la) => {
+    each(list2, (item2) => {});
+  });
   return list.filter((item) => filter(item));
 }
