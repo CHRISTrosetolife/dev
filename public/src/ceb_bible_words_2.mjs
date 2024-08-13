dae10cefbb65afd5ce4fd50f6653a92d0d73760e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:28:46 -0400

    2024-08-13T17:28:45.600Z function_auto_after ceb_bible_words_2

--- public/src/ceb_bible_words_2.mjs
+++ public/src/ceb_bible_words_2.mjs
@@ -1,61 +1,11 @@
-import { object_property_get } from "./object_property_get.mjs";
-import { list_find_properties } from "./list_find_properties.mjs";
-import { list_get } from "./list_get.mjs";
-import { each_range } from "./each_range.mjs";
-import { list_adder } from "./list_adder.mjs";
 import { list_take_bible_books_new } from "./list_take_bible_books_new.mjs";
 import { list_adder_unique_async } from "./list_adder_unique_async.mjs";
 import { each } from "./each.mjs";
 import { bible_ceb_2_book } from "./bible_ceb_2_book.mjs";
 import { each_index_only_async } from "./each_index_only_async.mjs";
 import { bible_ceb_2_books_hrefs } from "./bible_ceb_2_books_hrefs.mjs";
-import { list_size } from "./list_size.mjs";
 export async function ceb_bible_words_2(args) {
   let book_hrefs = await bible_ceb_2_books_hrefs();
-  let columns = 7;
-  let rows = 10;
-  let book_hrefs_size = list_size(book_hrefs);
-  let columns_full = columns - 1;
-  let columns_full_count = columns_full * rows;
-  let rows_full = book_hrefs_size - columns_full_count;
-  let rows_missing = rows - rows_full;
-  let indexed = list_adder((la) => {
-    let index = 0;
-    each_range(rows_full, (row) => {
-      each_range(columns, (column) => {
-        la({
-          row,
-          column,
-          href: list_get(book_hrefs, index),
-        });
-        index++;
-      });
-    });
-    each_range(rows_missing, (row_missing) => {
-      each_range(columns_full, (column) => {
-        la({
-          row: row_missing + rows_full,
-          column,
-          href: list_get(book_hrefs, index),
-        });
-        index++;
-      });
-    });
-  });
-  book_hrefs = list_adder((la) => {
-    each_range(columns, (column) => {
-      each_range(rows, (row) => {
-        if (column === columns - 1 && row >= rows_full) {
-          return;
-        }
-        let m = list_find_properties(indexed, {
-          row,
-          column,
-        });
-        la(object_property_get(m, "href"));
-      });
-    });
-  });
   if (args.new) {
     list_take_bible_books_new(book_hrefs);
   }
