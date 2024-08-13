b0e116ed5d3d0c00f3a0f1bf27f721fa53d808de
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 17:50:11 -0400

    2024-08-13T21:50:10.922Z function_auto_after list_filter_by

--- public/src/list_filter_by.mjs
+++ public/src/list_filter_by.mjs
@@ -1,5 +1,5 @@
 import { list_adder } from "./list_adder.mjs";
 export function list_filter_by(list, filter) {
-  return list_adder();
+  return list_adder((la) => {});
   return list.filter((item) => filter(item));
 }
