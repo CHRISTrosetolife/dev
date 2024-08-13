58e1a887caffb774c5634738e309ec653d171551
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:29:25 -0400

    2024-08-13T17:29:24.500Z function_auto_after ceb_bible_words_2

--- public/src/ceb_bible_words_2.mjs
+++ public/src/ceb_bible_words_2.mjs
@@ -7,7 +7,7 @@ import { bible_ceb_2_books_hrefs } from "./bible_ceb_2_books_hrefs.mjs";
 export async function ceb_bible_words_2(args) {
   let book_hrefs = await bible_ceb_2_books_hrefs();
   if (args.new) {
-    list_take_bible_books_new(book_hrefs);
+    book_hrefs = list_take_bible_books_new(book_hrefs);
   }
   return book_hrefs;
   let words = await list_adder_unique_async(async (law) => {
