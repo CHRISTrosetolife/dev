c09b2dff18d70c8d9025def609b4b701c9b11c34
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 11:16:50 -0400

    2024-08-13T15:16:49.527Z function_auto_after list_map_index_async

--- public/src/list_map_index_async.mjs
+++ public/src/list_map_index_async.mjs
@@ -3,7 +3,7 @@ import { list_adder_async } from "./list_adder_async.mjs";
 export async function list_map_index_async(list, mapper) {
   return await list_adder_async(async (la) => {
     await each_index_async(list, async (l, index) => {
-      let mapped = mapper(l);
+      let mapped = mapper(l, index);
       let waited = await mapped;
       la(waited);
     });
