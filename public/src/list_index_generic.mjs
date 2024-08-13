8e6ddada5cf72dfdfaf60e17aa56a95e904d9840
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 17:34:57 -0400

    2024-08-13T21:34:56.926Z function_auto_after list_index_generic

--- public/src/list_index_generic.mjs
+++ public/src/list_index_generic.mjs
@@ -3,7 +3,10 @@ import { number_less_than } from "./number_less_than.mjs";
 import { assert_not } from "./assert_not.mjs";
 export function list_index_generic(list, element) {
   let index = -1;
-  each_index(list2, (item, index2) => {});
+  each_index(list, (item, candidate_index) => {
+    if (lambda_equal(element)) {
+    }
+  });
   assert_not(number_less_than, [index, 0]);
   return index;
 }
