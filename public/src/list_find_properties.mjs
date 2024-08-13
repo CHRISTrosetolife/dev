81b92cc22960c9142664bdd652337a55a955af89
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:08:49 -0400

    2024-08-13T17:08:48.581Z function_auto_after list_find_properties

--- public/src/list_find_properties.mjs
+++ public/src/list_find_properties.mjs
@@ -1,7 +1,12 @@
 import { list_single_message } from "./list_single_message.mjs";
 import { list_filter_properties } from "./list_filter_properties.mjs";
+import { json_to } from "./json_to.mjs";
 export function list_find_properties(list, matcher) {
   let matches = list_filter_properties(list, matcher);
-  let match = list_single_message(matches, () => {});
+  let match = list_single_message(matches, () =>
+    json_to({
+      matcher,
+    }),
+  );
   return match;
 }
