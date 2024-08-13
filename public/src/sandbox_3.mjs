b9cbf24504ed13540398006a0bb5085f1fba81ba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 11:41:27 -0400

    2024-08-13T15:41:26.083Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,9 +1,5 @@
 import { list_any_created } from "./list_any_created.mjs";
 import text from "textbelt";
 export function sandbox_3() {
-  return list_any_created([
-    {
-      created: false,
-    },
-  ]);
+  return list_any_created([]);
 }
