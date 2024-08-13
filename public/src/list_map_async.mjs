29291390b276aadd64628c7611a358885cea2866
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 11:10:53 -0400

    2024-08-13T15:10:53.083Z function_auto_after list_map_async

--- public/src/list_map_async.mjs
+++ public/src/list_map_async.mjs
@@ -2,11 +2,10 @@ import { each_async } from "./each_async.mjs";
 import { list_add } from "./list_add.mjs";
 export async function list_map_async(list, mapper) {
   let result = [];
-  await each_async(list2, async (item) => {});
-  for (let l of list) {
+  await each_async(list, async (l) => {
     let mapped = mapper(l);
     let waited = await mapped;
     list_add(result, waited);
-  }
+  });
   return result;
 }
