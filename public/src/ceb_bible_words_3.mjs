42dd3d556f38104e744c43b160a16ef21a48b2b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:32:00 -0400

    2024-08-13T17:31:59.583Z function_auto_after ceb_bible_words_3

--- public/src/ceb_bible_words_3.mjs
+++ public/src/ceb_bible_words_3.mjs
@@ -1,4 +1,4 @@
-import { each_index_only_async } from "./each_index_only_async.mjs";
+import { each_index_async } from "./each_index_async.mjs";
 import { each } from "./each.mjs";
 import { list_adder_unique_async } from "./list_adder_unique_async.mjs";
 import { bible_ceb_3_chapter } from "./bible_ceb_3_chapter.mjs";
@@ -6,8 +6,10 @@ import { bible_ceb_3_hrefs } from "./bible_ceb_3_hrefs.mjs";
 export async function ceb_bible_words_3(args) {
   let words = await list_adder_unique_async(async (la) => {
     let hrefs = await bible_ceb_3_hrefs();
-    await each_index_only_async(hrefs, async (index) => {
+    await each_index_async(hrefs, async (href, index) => {
       if (args.new) {
+        if (href) {
+        }
       }
       let verses = await bible_ceb_3_chapter(index);
       each(verses, (v) =>
