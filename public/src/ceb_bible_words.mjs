40e4f16c3a02a9d2d4d0ffbb54a9bfb53c2ede59
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:53:24 -0400

    2024-08-13T17:53:24.089Z function_auto_after ceb_bible_words

--- public/src/ceb_bible_words.mjs
+++ public/src/ceb_bible_words.mjs
@@ -1,10 +1,11 @@
+import { fn_name } from "./fn_name.mjs";
 import { list_adder_multiple_async } from "./list_adder_multiple_async.mjs";
 import { list_map } from "./list_map.mjs";
 import { range_1 } from "./range_1.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { each_async } from "./each_async.mjs";
 import { function_run } from "./function_run.mjs";
-export async function ceb_bible_words() {
+export async function ceb_bible_words(args) {
   let count = 5;
   let fn_names = list_map(range_1(count), (i) =>
     string_combine_multiple([ceb_bible_words.name, "_", i]),
