c3dbc378f08a1dd9fcc18ac54c8929b2c3737d19
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 13:30:13 -0400

    2024-08-13T17:30:12.669Z function_auto_after ceb_bible_words_3

--- public/src/ceb_bible_words_3.mjs
+++ public/src/ceb_bible_words_3.mjs
@@ -3,7 +3,7 @@ import { each } from "./each.mjs";
 import { list_adder_unique_async } from "./list_adder_unique_async.mjs";
 import { bible_ceb_3_chapter } from "./bible_ceb_3_chapter.mjs";
 import { bible_ceb_3_hrefs } from "./bible_ceb_3_hrefs.mjs";
-export async function ceb_bible_words_3() {
+export async function ceb_bible_words_3(args) {
   let words = await list_adder_unique_async(async (la) => {
     let hrefs = await bible_ceb_3_hrefs();
     await each_index_only_async(hrefs, async (index) => {
