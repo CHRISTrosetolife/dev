75ab239631b3e7a16a4003d1b1451254d5c44fcc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 14:44:35 -0400

    2024-08-13T18:44:33.915Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -3,7 +3,6 @@ import { ceb_bible_words_score } from "./ceb_bible_words_score.mjs";
 import { each } from "./each.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 import { list_take } from "./list_take.mjs";
-import { log } from "./log.mjs";
 import { ceb_bible_words_count_cache_new } from "./ceb_bible_words_count_cache_new.mjs";
 import { list_includes } from "./list_includes.mjs";
 export async function sandbox_3() {
@@ -12,11 +11,11 @@ export async function sandbox_3() {
   let taken = list_take(wcs, count);
   let words = list_map_property(taken, "word");
   let scored = await ceb_bible_words_score();
-  list_adder((la) => {
+  let result = list_adder((la) => {
     each(scored, (s) => {
       let { word } = s;
       if (list_includes(words, s.word)) {
-        log(word);
+        la(word);
       }
     });
   });
