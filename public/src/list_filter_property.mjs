3488911cee43776ec02cb82484df8eb6c4fdc1a5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-13 12:58:58 -0400

    2024-08-13T16:58:57.359Z function_auto_after list_filter_property

--- public/src/list_filter_property.mjs
+++ public/src/list_filter_property.mjs
@@ -1,8 +1,8 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { equal } from "./equal.mjs";
 import { list_filter } from "./list_filter.mjs";
-export function list_filter_property(mapped4, property_name, value) {
-  return list_filter(mapped4, (m) =>
+export function list_filter_property(list, property_name, value) {
+  return list_filter(list, (m) =>
     equal(object_property_get(m, property_name), value),
   );
 }
