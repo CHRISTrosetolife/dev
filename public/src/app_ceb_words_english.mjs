ec19d86aa6d839393c3d1e72a5c29d743c6fccdd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-17 17:02:12 -0400

    2024-08-17T21:02:11.381Z function_auto_after app_ceb_words_english

--- public/src/app_ceb_words_english.mjs
+++ public/src/app_ceb_words_english.mjs
@@ -1,13 +1,8 @@
-import { each } from "./each.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { ceb_bible_words_definitions_atoms_cache } from "./ceb_bible_words_definitions_atoms_cache.mjs";
 import { object_properties } from "./object_properties.mjs";
-import { object_property_set } from "./object_property_set.mjs";
 export async function app_ceb_words_english() {
   let { inverted } = await ceb_bible_words_definitions_atoms_cache();
   let english = object_properties(inverted);
-  each(english, (e) => {
-    object_property_set(inverted, e, null);
-  });
   return list_join_newline(english);
 }
