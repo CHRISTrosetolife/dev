bf1610ceaf9c83e47f88dea33d7488c38fb7e683
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-17 15:53:41 -0400

    2024-08-17T19:53:40.430Z function_auto_after list_find_last

--- public/src/list_find_last.mjs
+++ public/src/list_find_last.mjs
@@ -1,7 +1,7 @@
-import { list_single } from "./list_single.mjs";
+import { list_last } from "./list_last.mjs";
 import { list_filter } from "./list_filter.mjs";
 export function list_find_last(list, predicate) {
   let filtered = list_filter(list, predicate);
-  let s = list_single(filtered);
+  let s = list_last(filtered);
   return s;
 }
