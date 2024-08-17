fba61a6200b0ba9e1c84a26d5be3e6e0bc54f709
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-17 17:00:45 -0400

    2024-08-17T21:00:44.436Z function_auto_after function_output_file

--- public/src/function_output_file.mjs
+++ public/src/function_output_file.mjs
@@ -1,9 +1,11 @@
+import { file_overwrite } from "./file_overwrite.mjs";
 import { folder_gitignore_path_function } from "./folder_gitignore_path_function.mjs";
 import { file_overwrite_json } from "./file_overwrite_json.mjs";
 import { date_string_iso_file } from "./date_string_iso_file.mjs";
 import { path_join } from "./path_join.mjs";
 import { function_run } from "./function_run.mjs";
 export async function function_output_file(function_name) {
+  let lambda_file_overwrite = file_overwrite;
   let f_path = path_join([function_name, date_string_iso_file() + ".txt"]);
   let output_path = folder_gitignore_path_function(
     function_output_file,
