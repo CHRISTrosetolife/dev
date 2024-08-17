f4733e46cf09f3b6ae320be19399b3d1e62c9da6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-17 16:38:40 -0400

    2024-08-17T20:38:40.180Z function_auto_after html_button_width_full_text_click_previous

--- public/src/html_button_width_full_text_click_previous.mjs
+++ public/src/html_button_width_full_text_click_previous.mjs
@@ -1,12 +1,9 @@
 import { html_button_previous_text } from "./html_button_previous_text.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
-export function html_button_width_full_text_click_previous(
-  root,
-  previous_on_click,
-) {
+export function html_button_width_full_text_click_previous(root, on_click) {
   return html_button_width_full_text_click(
     root,
     html_button_previous_text(),
-    previous_on_click,
+    on_click,
   );
 }
