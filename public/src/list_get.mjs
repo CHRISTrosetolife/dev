72ebb47dea06b41a507316c5466f325a78f66acc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-17 15:55:13 -0400

    2024-08-17T19:55:12.963Z function_auto_after list_get

--- public/src/list_get.mjs
+++ public/src/list_get.mjs
@@ -1,7 +1,7 @@
+import { less_than } from "./less_than.mjs";
 import { list_size } from "./list_size.mjs";
 import { assert } from "./assert.mjs";
-import { number_less_than } from "./number_less_than.mjs";
 export function list_get(list, index) {
-  assert(number_less_than, [index, list_size(list)]);
+  assert(less_than, [index, list_size(list)]);
   return list[index];
 }
