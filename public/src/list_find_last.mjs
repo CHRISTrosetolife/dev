41b9b7d7dc6845036fae85a949924c8564212f44
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-17 16:03:58 -0400

    2024-08-17T20:03:57.686Z a app_language_refresh_node

--- public/src/list_find_last.mjs
+++ public/src/list_find_last.mjs
@@ -1,6 +1,5 @@
 import { list_last } from "./list_last.mjs";
 import { list_filter } from "./list_filter.mjs";
-import { list_last } from "./list_last.mjs";
 export function list_find_last(list, predicate) {
   let filtered = list_filter(list, predicate);
   let s = list_last(filtered);
