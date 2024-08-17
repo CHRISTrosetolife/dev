f2db8803c4c3eafdf22b4a60b72b3af0c75c9d54
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-17 15:37:49 -0400

    2024-08-17T19:37:48.950Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -127,7 +127,9 @@ export function js_dollar(ast) {
         object_replace(node, e);
       }
       if (remaining === "el") {
-        log({});
+        log({
+          stack,
+        });
       }
       if (remaining === "eo") {
         let object = js_name_unique(ast, "object");
