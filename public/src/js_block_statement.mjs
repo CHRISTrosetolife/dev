4128037127fa5151bd2fa2a7913640d37521b0c2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-17 16:34:30 -0400

    2024-08-17T20:34:30.349Z function_auto_after js_to_block_generic

---
+++ public/src/js_block_statement.mjs
@@ -0,0 +1,6 @@
+export function js_block_statement(body) {
+  return {
+    type: "BlockStatement",
+    body: body,
+  };
+}
--- public/src/js_to_block_generic.mjs
+++ public/src/js_to_block_generic.mjs
@@ -1,3 +1,4 @@
+import { js_block_statement } from "./js_block_statement.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
@@ -13,10 +14,7 @@ export function js_to_block_generic(node, properties, except) {
         return;
       }
       let body = [value];
-      let value_new = {
-        type: "BlockStatement",
-        body: body,
-      };
+      let value_new = js_block_statement(body);
       object_property_set(node, p, value_new);
     }
   });
