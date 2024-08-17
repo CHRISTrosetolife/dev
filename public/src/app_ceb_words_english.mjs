255144511936de06b27138b7fce0615afc867cc3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-17 16:56:15 -0400

    2024-08-17T20:56:13.500Z function_auto_after app_ceb_words_english

--- public/src/app_ceb_words_english.mjs
+++ public/src/app_ceb_words_english.mjs
@@ -1,1 +1,5 @@
-export function app_ceb_words_english() {}
+import { ceb_bible_words_definitions_atoms_cache } from "./ceb_bible_words_definitions_atoms_cache.mjs";
+export async function app_ceb_words_english() {
+  let { atoms, definitions, inverted } =
+    await ceb_bible_words_definitions_atoms_cache();
+}
