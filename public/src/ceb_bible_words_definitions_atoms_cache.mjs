f8b63f70021b5305fa185bc0f25dfd4033231898
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-17 16:55:15 -0400

    2024-08-17T20:55:14.593Z fc ceb_bible_words_definitions_atoms

---
+++ public/src/ceb_bible_words_definitions_atoms_cache.mjs
@@ -0,0 +1,5 @@
+import { ceb_bible_words_definitions_atoms } from "./ceb_bible_words_definitions_atoms.mjs";
+import { function_cache } from "./function_cache.mjs";
+export async function ceb_bible_words_definitions_atoms_cache() {
+  return await function_cache(ceb_bible_words_definitions_atoms, []);
+}
