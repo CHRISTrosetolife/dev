7c55f89878e56d02a9ec7c22b1f6b596a8a36b1f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-17 16:58:45 -0400

    2024-08-17T20:58:44.665Z function_auto_after app_ceb_words_english

--- public/src/app_ceb_words_english.mjs
+++ public/src/app_ceb_words_english.mjs
@@ -1,7 +1,8 @@
+import { list_join_newline } from "./list_join_newline.mjs";
 import { ceb_bible_words_definitions_atoms_cache } from "./ceb_bible_words_definitions_atoms_cache.mjs";
 import { object_properties } from "./object_properties.mjs";
 export async function app_ceb_words_english() {
   let { inverted } = await ceb_bible_words_definitions_atoms_cache();
   let english = object_properties(inverted);
-  return english;
+  return list_join_newline(english);
 }
