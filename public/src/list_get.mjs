d3f4d921e54954ae764d95146d9608eecb2d4bb2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-17 15:59:52 -0400

    2024-08-17T19:59:51.313Z function_auto_after list_get

--- public/src/list_get.mjs
+++ public/src/list_get.mjs
@@ -1,7 +1,9 @@
+import { greater_than_equal } from "./greater_than_equal.mjs";
 import { less_than } from "./less_than.mjs";
 import { list_size } from "./list_size.mjs";
 import { assert } from "./assert.mjs";
 export function list_get(list, index) {
+  assert(greater_than_equal, [index, 0]);
   assert(less_than, [index, list_size(list)]);
   return list[index];
 }
