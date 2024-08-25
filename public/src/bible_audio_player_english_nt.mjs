4f41dbc0371da58bfe892f5f1b1549bc918fe3d8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-24 22:08:44 -0400

    2024-08-25T02:08:44.420Z function_auto_after bible_audio_player_english_nt

--- public/src/bible_audio_player_english_nt.mjs
+++ public/src/bible_audio_player_english_nt.mjs
@@ -2,5 +2,5 @@ import { folder_files_copy } from "./folder_files_copy.mjs";
 export async function bible_audio_player_english_nt() {
   let p = "..\\bible\\english\\ENGESVN2DA";
   let output = "E:\\";
-  await folder_files_copy(p, ".mp3");
+  await folder_files_copy(p, ".mp3", output);
 }
