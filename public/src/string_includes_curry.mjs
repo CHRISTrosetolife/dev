b7899663f2da13d8e0c130a226398f5b5bad5702
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-25 17:02:58 -0400

    2024-08-25T21:02:57.742Z function_auto_after string_includes_curry

--- public/src/string_includes_curry.mjs
+++ public/src/string_includes_curry.mjs
@@ -1,7 +1,7 @@
 import { string_includes } from "./string_includes.mjs";
-export function string_includes_curry(function_name) {
-  return (term) => {
-    let result = string_includes(function_name, term);
+export function string_includes_curry(item) {
+  return (search) => {
+    let result = string_includes(item, search);
     return result;
   };
 }
