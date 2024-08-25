9019e2d144313e3280b1232b351873dd9c420a8e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-25 17:07:12 -0400

    2024-08-25T21:07:12.299Z function_auto_after bible_audio_player_english_psalms_proverbs

--- public/src/bible_audio_player_english_psalms_proverbs.mjs
+++ public/src/bible_audio_player_english_psalms_proverbs.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { list_any } from "./list_any.mjs";
 import { string_includes } from "./string_includes.mjs";
 import { bible_audio_player_english_generic } from "./bible_audio_player_english_generic.mjs";
@@ -5,5 +6,6 @@ export async function bible_audio_player_english_psalms_proverbs() {
   let download_folder = "ENGESVO2DA";
   let filter = (file_name) =>
     list_any(["Psalms", "Proverbs"], string_includes(file_name));
+  log({});
   await bible_audio_player_english_generic(download_folder, filter);
 }
