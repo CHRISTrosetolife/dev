a33f287534d152c27166ffd964ebfe132839b0cf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-25 17:04:27 -0400

    2024-08-25T21:04:26.453Z function_auto_after folder_read_each_generic

--- public/src/folder_read_each_generic.mjs
+++ public/src/folder_read_each_generic.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { each_async } from "./each_async.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { folder_read } from "./folder_read.mjs";
@@ -9,5 +10,6 @@ export async function folder_read_each_generic(
 ) {
   let file_paths = await folder_read(input_directory, file_extension);
   let filtered = list_filter(file_paths, filter);
+  log({});
   await each_async(filtered, lambda);
 }
