b9f05eec83303bee21bb17bd062cbbf915a73f2d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-25 16:54:18 -0400

    2024-08-25T20:54:17.780Z function_auto_after folder_files_move_generic

--- public/src/folder_files_move_generic.mjs
+++ public/src/folder_files_move_generic.mjs
@@ -1,6 +1,6 @@
-import { folder_files_copy_generic } from "./folder_files_copy_generic.mjs";
 import { path_join } from "./path_join.mjs";
 import { path_parse_base } from "./path_parse_base.mjs";
+import { folder_read_each } from "./folder_read_each.mjs";
 export async function folder_files_move_generic(
   input_directory,
   file_extension,
@@ -8,7 +8,7 @@ export async function folder_files_move_generic(
   lambda,
   filter,
 ) {
-  await folder_files_copy_generic(input_directory, file_extension, lambda_each);
+  await folder_read_each(input_directory, file_extension, lambda_each);
   async function lambda_each(file_path) {
     let file_name = path_parse_base(file_path);
     let file_path_new = path_join([output_directory, file_name]);
