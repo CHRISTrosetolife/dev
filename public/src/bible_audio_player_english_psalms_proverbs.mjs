a022236df688cbf0bdaea350efac9f5f645f52ca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-25 17:02:31 -0400

    2024-08-25T21:02:30.854Z function_auto_after bible_audio_player_english_psalms_proverbs

--- public/src/bible_audio_player_english_psalms_proverbs.mjs
+++ public/src/bible_audio_player_english_psalms_proverbs.mjs
@@ -2,6 +2,6 @@ import { string_includes } from "./string_includes.mjs";
 import { bible_audio_player_english_generic } from "./bible_audio_player_english_generic.mjs";
 export async function bible_audio_player_english_psalms_proverbs() {
   let download_folder = "ENGESVN2DA";
-  let filter = (file_name) => string_includes();
+  let filter = (file_name) => string_includes(file_name);
   await bible_audio_player_english_generic(download_folder, filter);
 }
