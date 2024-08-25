8651902a43676cb037a82d00e0e3c9cbec23e044
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-25 16:53:26 -0400

    2024-08-25T20:53:25.091Z function_auto_after folder_files_copy

--- public/src/folder_files_copy.mjs
+++ public/src/folder_files_copy.mjs
@@ -6,6 +6,7 @@ export async function folder_files_copy(
   file_extension,
   output_directory,
 ) {
+  let filter = tautology;
   let lambda = async (from, to) => await file_copy_generic(from, to, false);
   await folder_files_move_generic(
     input_directory,
