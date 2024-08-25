ecc2a59113ff04b43e26b0a6046857509a407294
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-25 16:51:41 -0400

    2024-08-25T20:51:40.699Z function_auto_after folder_read_each

--- public/src/folder_read_each.mjs
+++ public/src/folder_read_each.mjs
@@ -1,3 +1,4 @@
+import { tautology } from "./tautology.mjs";
 import { each_async } from "./each_async.mjs";
 import { folder_read } from "./folder_read.mjs";
 export async function folder_read_each(
@@ -5,6 +6,7 @@ export async function folder_read_each(
   file_extension,
   lambda,
 ) {
+  let filter = tautology;
   let file_paths = await folder_read(input_directory, file_extension);
   await each_async(file_paths, lambda);
 }
