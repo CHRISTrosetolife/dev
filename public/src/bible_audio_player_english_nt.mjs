fb773f680d2dc940b062170f83193ddfcc767e27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-25 16:57:34 -0400

    2024-08-25T20:57:33.945Z function_auto_after bible_audio_player_english_nt

--- public/src/bible_audio_player_english_nt.mjs
+++ public/src/bible_audio_player_english_nt.mjs
@@ -1,8 +1,10 @@
+import { tautology } from "./tautology.mjs";
 import { folder_files_copy_generic } from "./folder_files_copy_generic.mjs";
 import { path_join } from "./path_join.mjs";
 export async function bible_audio_player_english_nt() {
   let download_folder = "ENGESVN2DA";
+  let filter = tautology;
   let p = path_join(["..\\bible\\english", download_folder]);
   let output = "E:\\";
-  await folder_files_copy_generic(p, ".mp3", output);
+  await folder_files_copy_generic(p, ".mp3", output, filter);
 }
