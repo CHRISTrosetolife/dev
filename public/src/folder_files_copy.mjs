d7786df1a4f24886dbfde0821903bf137824bc1f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-25 16:53:44 -0400

    2024-08-25T20:53:44.260Z function_auto_after folder_files_copy

--- public/src/folder_files_copy.mjs
+++ public/src/folder_files_copy.mjs
@@ -1,18 +1,15 @@
+import { folder_files_copy_generic } from "./folder_files_copy_generic.mjs";
 import { tautology } from "./tautology.mjs";
-import { file_copy_generic } from "./file_copy_generic.mjs";
-import { folder_files_move_generic } from "./folder_files_move_generic.mjs";
 export async function folder_files_copy(
   input_directory,
   file_extension,
   output_directory,
 ) {
   let filter = tautology;
-  let lambda = async (from, to) => await file_copy_generic(from, to, false);
-  await folder_files_move_generic(
+  await folder_files_copy_generic(
     input_directory,
     file_extension,
     output_directory,
-    lambda,
     filter,
   );
 }
---
+++ public/src/folder_files_copy_generic.mjs
@@ -0,0 +1,17 @@
+import { folder_files_move_generic } from "./folder_files_move_generic.mjs";
+import { file_copy_generic } from "./file_copy_generic.mjs";
+export async function folder_files_copy_generic(
+  input_directory,
+  file_extension,
+  output_directory,
+  filter,
+) {
+  let lambda = async (from, to) => await file_copy_generic(from, to, false);
+  await folder_files_move_generic(
+    input_directory,
+    file_extension,
+    output_directory,
+    lambda,
+    filter,
+  );
+}
