8c0f1cc34b41572bf21acc735b27d3a7ebc50f7d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-08-25 16:57:50 -0400

    2024-08-25T20:57:49.970Z function_auto_after bible_audio_player_english_nt

---
+++ public/src/bible_audio_player_english_generic.mjs
@@ -0,0 +1,10 @@
+import { folder_files_copy_generic } from "./folder_files_copy_generic.mjs";
+import { path_join } from "./path_join.mjs";
+export async function bible_audio_player_english_generic(
+  download_folder,
+  filter,
+) {
+  let p = path_join(["..\\bible\\english", download_folder]);
+  let output = "E:\\";
+  await folder_files_copy_generic(p, ".mp3", output, filter);
+}
--- public/src/bible_audio_player_english_nt.mjs
+++ public/src/bible_audio_player_english_nt.mjs
@@ -1,10 +1,7 @@
+import { bible_audio_player_english_generic } from "./bible_audio_player_english_generic.mjs";
 import { tautology } from "./tautology.mjs";
-import { folder_files_copy_generic } from "./folder_files_copy_generic.mjs";
-import { path_join } from "./path_join.mjs";
 export async function bible_audio_player_english_nt() {
   let download_folder = "ENGESVN2DA";
   let filter = tautology;
-  let p = path_join(["..\\bible\\english", download_folder]);
-  let output = "E:\\";
-  await folder_files_copy_generic(p, ".mp3", output, filter);
+  await bible_audio_player_english_generic(download_folder, filter);
 }
