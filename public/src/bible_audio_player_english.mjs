432e8c30b16acc24106d4ea22a5522a335beee50
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-02 13:42:11 -0400

    2024-09-02T17:42:10.663Z function_auto_after bible_audio_player_english

--- public/src/bible_audio_player_english.mjs
+++ public/src/bible_audio_player_english.mjs
@@ -3,5 +3,6 @@ import { path_join } from "./path_join.mjs";
 export async function bible_audio_player_english(download_folder, filter) {
   let p = path_join(["..\\bible\\english", download_folder]);
   let output = "E:\\";
+  output = "..\\bible\\english\\test";
   await folder_files_copy_generic(p, ".mp3", output, filter);
 }
