8715c36117dd5bbbf497d6cdad3357527f28edd0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-02 13:33:33 -0400

    2024-09-02T17:33:33.370Z function_auto_after bible_audio_player_proverbs

--- public/src/bible_audio_player_proverbs.mjs
+++ public/src/bible_audio_player_proverbs.mjs
@@ -13,7 +13,7 @@ export async function bible_audio_player_proverbs() {
   let filter = (file_name) =>
     string_includes(file_name, "_____") &&
     string_includes(file_name, "Proverbs");
-  await each_async(download_folders_fcbh, async (download_folders) => {
+  await each_async(download_folders_fcbh, async (download_folder) => {
     await bible_audio_player_english(download_folder, filter);
   });
 }
