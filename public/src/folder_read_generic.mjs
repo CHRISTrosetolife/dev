4120318810ec18f01f07806f645d6358b4950934
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-02 13:24:55 -0400

    2024-09-02T17:24:54.726Z function_auto_after folder_read_generic

--- public/src/folder_read_generic.mjs
+++ public/src/folder_read_generic.mjs
@@ -1,3 +1,4 @@
+import { each_async } from "./each_async.mjs";
 import { log } from "./log.mjs";
 import { identity } from "./identity.mjs";
 import { list_sort_string } from "./list_sort_string.mjs";
@@ -12,8 +13,9 @@ export async function folder_read_generic(folder, file_extension, recursive) {
   assert(string_is, [file_extension]);
   folder = string_replace(folder, "\\", "/");
   folder += "/";
-  function traverseDir(dir) {
+  async function traverseDir(dir) {
     let files = fs.readdir(dir);
+    await each_async(list, async (item) => {});
     files.forEach((file) => {
       let fullPath = path.join(dir, file);
       if (fs.lstatSync(fullPath).isDirectory()) {
