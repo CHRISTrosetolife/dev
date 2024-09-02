0a70f73fee957bf2332b5614cb9793cac4024845
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-02 13:45:52 -0400

    2024-09-02T17:45:52.204Z function_auto_after bible_audio_player_proverbs

--- public/src/bible_audio_player_proverbs.mjs
+++ public/src/bible_audio_player_proverbs.mjs
@@ -13,7 +13,6 @@ export async function bible_audio_player_proverbs() {
   let download_folders_fcbh = [
     "ENGESVO2DA",
     "ENGESVN2DA",
-    "ENGNLTO2DA",
     "ENGNRSO2DA",
     "EN1WEBO2DA",
   ];
@@ -27,6 +26,7 @@ export async function bible_audio_player_proverbs() {
   });
   let download_folders_regular = [
     "ENGCEVO2DA",
+    "ENGNLTO2DA",
     "niv_mp3_bible_new_international_version",
     "rsv",
     "NHEB_NT_audio",
