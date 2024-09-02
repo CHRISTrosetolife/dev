74daf05593e624690e77172cefda741fa3a4b941
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-02 13:05:02 -0400

    2024-09-02T17:04:59.907Z function_auto_after bible_audio_player_copy

--- public/src/bible_audio_player_esv_nt.mjs
+++ public/src/bible_audio_player_esv_nt.mjs
@@ -1,7 +1,7 @@
-import { bible_audio_player_esv_generic } from "./bible_audio_player_esv_generic.mjs";
+import { bible_audio_player_copy } from "./bible_audio_player_copy.mjs";
 import { tautology } from "./tautology.mjs";
 export async function bible_audio_player_esv_nt() {
   let download_folder = "ENGESVN2DA";
   let filter = tautology;
-  await bible_audio_player_esv_generic(download_folder, filter);
+  await bible_audio_player_copy(download_folder, filter);
 }
