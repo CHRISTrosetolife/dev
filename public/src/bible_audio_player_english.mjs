0130f9ed604006f526017cd95386813036a42cd8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-02 13:09:45 -0400

    2024-09-02T17:09:45.112Z function_auto_after bible_audio_player_esv_psalms_proverbs

---
+++ public/src/bible_audio_player_english.mjs
@@ -0,0 +1,7 @@
+import { folder_files_copy_generic } from "./folder_files_copy_generic.mjs";
+import { path_join } from "./path_join.mjs";
+export async function bible_audio_player_english(download_folder, filter) {
+  let p = path_join(["..\\bible\\english", download_folder]);
+  let output = "E:\\";
+  await folder_files_copy_generic(p, ".mp3", output, filter);
+}
