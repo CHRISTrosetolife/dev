e328b15dc839cae3c401e9e4d0173ce935035885
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-02 13:36:41 -0400

    2024-09-02T17:36:41.234Z function_auto_after object_invert

--- public/src/object_invert.mjs
+++ public/src/object_invert.mjs
@@ -1,15 +1,9 @@
-import { list_add } from "./list_add.mjs";
-import { object_property_initialize } from "./object_property_initialize.mjs";
-import { each } from "./each.mjs";
+import { object_property_set } from "./object_property_set.mjs";
 import { each_object } from "./each_object.mjs";
 export function object_invert(object) {
   let inverted = {};
   each_object(object, (key, value) => {
-    $s;
-    each(value, (value) => {
-      let list_inverted = object_property_initialize(inverted, value, []);
-      list_add(list_inverted, key);
-    });
+    object_property_set(inverted, value, key);
   });
   return inverted;
 }
