8aa98e21248ff9f79792e32e7fc00cbd00070398
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 05:13:08 -0400

    2024-09-03T09:13:08.292Z function_auto_after ceb_stem_test

--- public/src/ceb_stem_test.mjs
+++ public/src/ceb_stem_test.mjs
@@ -2,5 +2,5 @@ import { each_object } from "./each_object.mjs";
 import { ceb_bible_words_definitions_map_cache } from "./ceb_bible_words_definitions_map_cache.mjs";
 export async function ceb_stem_test() {
   let map = await ceb_bible_words_definitions_map_cache();
-  each_object(object, (key, value) => {});
+  each_object(object, (stem, inputs) => {});
 }
