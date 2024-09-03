6fdb77f84e26d54a7f09931c5cc94464f6f3d794
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 05:16:27 -0400

    2024-09-03T09:16:26.696Z function_auto_after ceb_stem

--- public/src/ceb_stem.mjs
+++ public/src/ceb_stem.mjs
@@ -1,9 +1,11 @@
 import { string_suffix_without } from "./string_suffix_without.mjs";
 import { string_ends_with } from "./string_ends_with.mjs";
+import { list_add } from "./list_add.mjs";
 export function ceb_stem(word) {
   let result = [word];
   let suffix = "ng";
   if (string_ends_with(word, suffix)) {
     let r = string_suffix_without(word, suffix);
+    list_add(result, r);
   }
 }
