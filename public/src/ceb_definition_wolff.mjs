e045d10bbb9ce0418e0e920670f84db819ca35e5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 07:13:54 -0400

    2024-09-03T11:13:53.724Z function_auto_after ceb_definition_wolff

--- public/src/ceb_definition_wolff.mjs
+++ public/src/ceb_definition_wolff.mjs
@@ -1,4 +1,4 @@
 import { ceb_definition_wolff_context } from "./ceb_definition_wolff_context.mjs";
-export async function ceb_definition_wolff(context, word) {
-  await ceb_definition_wolff_context();
+export async function ceb_definition_wolff(word) {
+  let context = await ceb_definition_wolff_context();
 }
