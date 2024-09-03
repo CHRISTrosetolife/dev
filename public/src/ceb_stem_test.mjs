46377f4282f381d0d13dad731e94b49eb9858c96
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 05:56:05 -0400

    2024-09-03T09:56:05.293Z function_auto_after ceb_stem_test

--- public/src/ceb_stem_test.mjs
+++ public/src/ceb_stem_test.mjs
@@ -1,4 +1,3 @@
-import { list_remove } from "./list_remove.mjs";
 import { list_includes } from "./list_includes.mjs";
 import { ceb_stem } from "./ceb_stem.mjs";
 import { each } from "./each.mjs";
@@ -9,10 +8,10 @@ export async function ceb_stem_test() {
   let map = await ceb_bible_words_definitions_map_cache();
   let removes = ["juda", "moises"];
   each_object(map, (stem, inputs) => {
-    each(removes, (r) => {
-      list_remove(map, r);
-    });
     each(inputs, (input) => {
+      if (list_includes(removes)) {
+        return;
+      }
       let choices = ceb_stem(input);
       assert_message(list_includes, [choices, stem], () => ({
         input,
