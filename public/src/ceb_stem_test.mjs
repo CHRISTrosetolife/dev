7d20955dd8e8099816c8e12d25bbdf955b1226e7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 07:05:12 -0400

    2024-09-03T11:05:11.832Z function_auto_after ceb_stem_test

--- public/src/ceb_stem_test.mjs
+++ public/src/ceb_stem_test.mjs
@@ -6,6 +6,7 @@ import { each_object } from "./each_object.mjs";
 import { ceb_bible_words_definitions_map_cache } from "./ceb_bible_words_definitions_map_cache.mjs";
 import { assert_message } from "./assert_message.mjs";
 export async function ceb_stem_test() {
+  let context = {};
   ceb_definition_wolff();
   let map = await ceb_bible_words_definitions_map_cache();
   let removes = ["juda", "moises"];
