f3b7660d38ed6e3de82e0638078afc9e39692273
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 05:11:42 -0400

    2024-09-03T09:11:41.699Z function_auto_after ceb_stem_test

--- public/src/ceb_stem_test.mjs
+++ public/src/ceb_stem_test.mjs
@@ -1,1 +1,4 @@
-export function ceb_stem_test() {}
+import { ceb_bible_words_definitions_map_cache } from "./ceb_bible_words_definitions_map_cache.mjs";
+export async function ceb_stem_test() {
+  await ceb_bible_words_definitions_map_cache();
+}
