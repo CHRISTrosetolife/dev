3da5562f402a3878ea0fcb4faf7adf8b9303d480
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 05:47:55 -0400

    2024-09-03T09:47:54.493Z function_auto_after ceb_stem

--- public/src/ceb_stem.mjs
+++ public/src/ceb_stem.mjs
@@ -38,7 +38,7 @@ export function ceb_stem(word) {
       list_add(result, r);
     }
   });
-  let spelling_ou = ["kung", "gayod", "tibuok"];
+  let spelling_ou = ["kung", "gayod", "tibuok", "hangtod"];
   each(result, (r) => {
     let f = list_filter(spelling_ou, (s) => {
       if (string_length(s) !== string_length(r)) {
