305607a2f9870b37858244f1e06f8bd350c79871
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 05:19:09 -0400

    2024-09-03T09:19:09.244Z function_auto_after ceb_stem

--- public/src/ceb_stem.mjs
+++ public/src/ceb_stem.mjs
@@ -2,6 +2,7 @@ import { object_property_get } from "./object_property_get.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
 import { string_ends_with } from "./string_ends_with.mjs";
 import { list_add } from "./list_add.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
 export function ceb_stem(word) {
   let result = [word];
   let suffix = "ng";
@@ -12,7 +13,7 @@ export function ceb_stem(word) {
   let replacements = {
     jehova: "hehoba",
   };
-  if (object_property_has(replacements, word)) {
+  if (object_property_exists(replacements, word)) {
     let r = object_property_get(replacements, word);
     list_add(result, r);
   }
