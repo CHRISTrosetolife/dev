c81fade12169bd05f81aff725799d83bf03030d7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 05:15:51 -0400

    2024-09-03T09:15:50.926Z function_auto_after ceb_stem

--- public/src/ceb_stem.mjs
+++ public/src/ceb_stem.mjs
@@ -1,6 +1,8 @@
+import { string_suffix_without } from "./string_suffix_without.mjs";
 import { string_ends_with } from "./string_ends_with.mjs";
 export function ceb_stem(word) {
   let suffix = "ng";
   if (string_ends_with(word, suffix)) {
+    string_suffix_without(word, suffix);
   }
 }
