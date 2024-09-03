d5f4da4673442268524204870a01657e23b3802a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 05:56:23 -0400

    2024-09-03T09:56:23.191Z function_auto_after list_includes

--- public/src/list_includes.mjs
+++ public/src/list_includes.mjs
@@ -1,3 +1,5 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function list_includes(list, item) {
+  assert_arguments_length(arguments, 2);
   return list.includes(item);
 }
