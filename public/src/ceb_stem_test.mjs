7043223d667f55d081e2cd2887ca63e842438f93
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 13:58:07 -0400

    2024-09-03T17:58:07.292Z function_auto_after ceb_stem_test

--- public/src/ceb_stem_test.mjs
+++ public/src/ceb_stem_test.mjs
@@ -9,13 +9,14 @@ import { each } from "./each.mjs";
 import { each_object } from "./each_object.mjs";
 import { ceb_bible_words_definitions_map_cache } from "./ceb_bible_words_definitions_map_cache.mjs";
 import { assert_message } from "./assert_message.mjs";
+import { html_parse_text } from "./html_parse_text.mjs";
 export async function ceb_stem_test() {
   let data = await ceb_dictionary_data();
   each_object(data, (letter, parsed) => {
     let body = html_parse_visit_tag_single(parsed, "body");
     each(body.children, (c) => {
       log({
-        c,
+        c: html_parse_text(c),
       });
     });
     html_parse_visit_tag(body, "p", (p) => {
