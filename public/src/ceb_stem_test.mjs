76650f48a6e56848408149a3d22778d703aff6a5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 07:23:08 -0400

    2024-09-03T11:23:08.087Z function_auto_after ceb_stem_test

--- public/src/ceb_stem_test.mjs
+++ public/src/ceb_stem_test.mjs
@@ -6,7 +6,7 @@ import { each_object } from "./each_object.mjs";
 import { ceb_bible_words_definitions_map_cache } from "./ceb_bible_words_definitions_map_cache.mjs";
 import { assert_message } from "./assert_message.mjs";
 export async function ceb_stem_test() {
-  let context = await ceb_dictionary_data();
+  let data = await ceb_dictionary_data();
   let map = await ceb_bible_words_definitions_map_cache();
   let removes = ["juda", "moises"];
   each_object(map, (stem, inputs) => {
