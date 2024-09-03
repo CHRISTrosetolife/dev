8d13300476b5d26a72f5982cee77d57f6c4368f7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 07:10:17 -0400

    2024-09-03T11:10:16.643Z function_auto_after global_function_initialize_async

--- public/src/global_function_initialize_async.mjs
+++ public/src/global_function_initialize_async.mjs
@@ -2,10 +2,10 @@ import { object_property_initialize_get_async } from "./object_property_initiali
 import { global_get } from "./global_get.mjs";
 export async function global_function_initialize_async(fn, value_get) {
   let g = global_get();
-  let counts = await object_property_initialize_get_async(
+  let result = await object_property_initialize_get_async(
     g,
     fn.name,
     value_get,
   );
-  return counts;
+  return result;
 }
