619114fc4eed8927caff7aedcf3cdb8a269d426e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 07:12:33 -0400

    2024-09-03T11:12:32.472Z function_auto_after ceb_definition_wolff_context

--- public/src/ceb_definition_wolff_context.mjs
+++ public/src/ceb_definition_wolff_context.mjs
@@ -1,1 +1,14 @@
-export function ceb_definition_wolff_context() {}
+import { app_gs_z_indexes } from "./app_gs_z_indexes.mjs";
+import { global_function_initialize } from "./global_function_initialize.mjs";
+export function ceb_definition_wolff_context() {
+  return global_function_initialize(app_gs_z_indexes, () => [
+    "tile",
+    "overlay",
+    "npc",
+    "npc_overlay_blur",
+    "npc_overlay",
+    "player",
+    "clicker",
+    "menu",
+  ]);
+}
