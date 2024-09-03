fcd4e9822a11c4056c552b4c64100d452c881583
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 05:11:47 -0400

    2024-09-03T09:11:46.866Z function_auto_after ceb_stem_test

--- public/src/ceb_stem_test.mjs
+++ public/src/ceb_stem_test.mjs
@@ -1,4 +1,4 @@
 import { ceb_bible_words_definitions_map_cache } from "./ceb_bible_words_definitions_map_cache.mjs";
 export async function ceb_stem_test() {
-  await ceb_bible_words_definitions_map_cache();
+  let map = await ceb_bible_words_definitions_map_cache();
 }
