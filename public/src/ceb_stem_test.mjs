6de2b4ef1f49e9372191f4413d8de17a4945191f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 14:01:33 -0400

    2024-09-03T18:01:32.748Z function_auto_after ceb_stem_test

--- public/src/ceb_stem_test.mjs
+++ public/src/ceb_stem_test.mjs
@@ -11,11 +11,13 @@ import { ceb_bible_words_definitions_map_cache } from "./ceb_bible_words_definit
 import { assert_message } from "./assert_message.mjs";
 import { list_second } from "./list_second.mjs";
 import { html_parse_text } from "./html_parse_text.mjs";
+import { list_get } from "./list_get.mjs";
 export async function ceb_stem_test() {
   let data = await ceb_dictionary_data();
   each_object(data, (letter, parsed) => {
     let body = html_parse_visit_tag_single(parsed, "body");
-    let s = list_second(body.children);
+    let b2 = list_second(body.children);
+    let b3 = list_get(b2, 3);
     each(s.children, (c) => {
       log({
         c: html_parse_text(c),
