a4c3ab96ce71b3ac3dbe867202e8009d4b5eefd8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 05:19:50 -0400

    2024-09-03T09:19:50.029Z function_auto_after ceb_stem

--- public/src/ceb_stem.mjs
+++ public/src/ceb_stem.mjs
@@ -1,3 +1,5 @@
+import { string_prefix_without } from "./string_prefix_without.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
 import { string_ends_with } from "./string_ends_with.mjs";
@@ -8,6 +10,11 @@ export function ceb_stem(word) {
   let suffix = "ng";
   if (string_ends_with(word, suffix)) {
     let r = string_suffix_without(word, suffix);
+    list_add(result, r);
+  }
+  let prefix = "ka";
+  if (string_starts_with(word, prefix)) {
+    let r = string_prefix_without(word, prefix);
     list_add(result, r);
   }
   let replacements = {
