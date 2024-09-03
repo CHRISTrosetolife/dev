7a264c17340ceaa855cc2f78bebe93ec361e05b1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 07:12:36 -0400

    2024-09-03T11:12:35.821Z function_auto_after ceb_definition_wolff_context

--- public/src/ceb_definition_wolff_context.mjs
+++ public/src/ceb_definition_wolff_context.mjs
@@ -1,7 +1,7 @@
+import { global_function_initialize_async } from "./global_function_initialize_async.mjs";
 import { app_gs_z_indexes } from "./app_gs_z_indexes.mjs";
-import { global_function_initialize } from "./global_function_initialize.mjs";
-export function ceb_definition_wolff_context() {
-  return global_function_initialize(app_gs_z_indexes, () => [
+export async function ceb_definition_wolff_context() {
+  return await global_function_initialize_async(app_gs_z_indexes, () => [
     "tile",
     "overlay",
     "npc",
