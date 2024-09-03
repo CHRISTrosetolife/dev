ab4117c8599163fdc2d5e5fb9cc385fc361b7643
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 05:17:16 -0400

    2024-09-03T09:17:16.375Z function_auto_after ceb_stem_test

--- public/src/ceb_stem_test.mjs
+++ public/src/ceb_stem_test.mjs
@@ -1,5 +1,5 @@
+import { list_includes } from "./list_includes.mjs";
 import { ceb_stem } from "./ceb_stem.mjs";
-import { equal } from "./equal.mjs";
 import { each } from "./each.mjs";
 import { each_object } from "./each_object.mjs";
 import { ceb_bible_words_definitions_map_cache } from "./ceb_bible_words_definitions_map_cache.mjs";
@@ -8,7 +8,7 @@ export async function ceb_stem_test() {
   let map = await ceb_bible_words_definitions_map_cache();
   each_object(map, (stem, inputs) => {
     each(inputs, (input) => {
-      assert_message(equal, [ceb_stem(input), stem], () => ({
+      assert_message(list_includes, [ceb_stem(input), stem], () => ({
         input,
         stem,
       }));
