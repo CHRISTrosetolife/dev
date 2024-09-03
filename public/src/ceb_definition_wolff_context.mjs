9976407ac090e9e20a747493c471d88792d23704
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-03 07:13:03 -0400

    2024-09-03T11:13:02.731Z function_auto_after ceb_definition_wolff_context

--- public/src/ceb_definition_wolff_context.mjs
+++ public/src/ceb_definition_wolff_context.mjs
@@ -1,7 +1,29 @@
+import { object_property_set } from "./object_property_set.mjs";
+import { file_html_parse } from "./file_html_parse.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { string_case_upper } from "./string_case_upper.mjs";
+import { each_async } from "./each_async.mjs";
+import { keyboard_keys } from "./keyboard_keys.mjs";
 import { global_function_initialize_async } from "./global_function_initialize_async.mjs";
 export async function ceb_definition_wolff_context() {
   return await global_function_initialize_async(
     ceb_definition_wolff_context,
-    async () => {},
+    async () => {
+      let letters = {};
+      let keys = keyboard_keys();
+      await each_async(keys, async (key) => {
+        let key_upper = string_case_upper(key);
+        let p = string_combine_multiple([
+          "..\\phildict-master\\Data\\Wolff\\WCED-",
+          key_upper,
+          ".tei",
+        ]);
+        let parsed = await file_html_parse(p);
+        object_property_set(letters, key, parsed);
+      });
+      let context = {
+        letters,
+      };
+    },
   );
 }
