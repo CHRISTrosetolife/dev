cf5a0cd5793c44e10dfd9d5842714df1612667e2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 13:46:47 -0400

    2024-09-05T17:46:46.917Z function_auto_after bible_audio_player_unzip

--- public/src/bible_audio_player_unzip.mjs
+++ public/src/bible_audio_player_unzip.mjs
@@ -1,3 +1,5 @@
+import { path_parse_name } from "./path_parse_name.mjs";
+import { folder_read_shallow } from "./folder_read_shallow.mjs";
 import { each_async } from "./each_async.mjs";
 import { folder_read_shallow_names } from "./folder_read_shallow_names.mjs";
 import { folder_read_shallow_folders_bases } from "./folder_read_shallow_folders_bases.mjs";
@@ -7,8 +9,10 @@ export async function bible_audio_player_unzip() {
   let p = bible_audio_player_english_path();
   let extension = ".zip";
   let zip_names = await folder_read_shallow_names(p, extension);
+  let zips = await folder_read_shallow(p, extension);
   let folder_names = await folder_read_shallow_folders_bases(p);
-  await each_async(zip_names, async (zip_name) => {
+  await each_async(zips, async (z) => {
+    let zip_name = path_parse_name(z);
     if (list_includes(folder_names, zip_name)) {
       return;
     }
