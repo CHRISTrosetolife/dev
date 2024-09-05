03af5d79da2e23c0ea3b53357e455d803821cc0a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 19:36:28 -0400

    2024-09-05T23:36:28.280Z function_auto_after bible_audio_player_english_path

--- public/src/bible_audio_player_english_path.mjs
+++ public/src/bible_audio_player_english_path.mjs
@@ -1,4 +1,5 @@
 import { path_join } from "./path_join.mjs";
 export function bible_audio_player_english_path() {
-  return path_join(["..\\bible", "english"]);
+  let name = "english";
+  return path_join(["..\\bible", name]);
 }
