a088d1ac98883a3e64381255d26c4bf5a4c6e076
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 15:16:49 -0400

    2024-09-05T19:16:49.001Z function_auto_after bible_audio_player_proverbs

--- public/src/bible_audio_player_proverbs.mjs
+++ public/src/bible_audio_player_proverbs.mjs
@@ -28,6 +28,7 @@ export async function bible_audio_player_proverbs() {
     "ENGESVO1DA",
     "ENGESHN1DA",
     "BBE_OldTestamentAudio_India",
+    "ENGESVO1DA",
   ];
   let download_folders_regular_computer_voice = [
     "NHEB_NT_audio",
