42d80b588d258b1c78aa05a189149acbeccfbab8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 19:41:08 -0400

    2024-09-05T23:41:07.814Z function_auto_after bible_audio_player_cebuano

--- public/src/bible_audio_player_cebuano.mjs
+++ public/src/bible_audio_player_cebuano.mjs
@@ -12,6 +12,7 @@ export async function bible_audio_player_cebuano() {
   let p = bible_audio_player_cebuano_path();
   await folder_unzip(p);
   let folders = ["CEBRPVN1DA", "CEBRPVN2DA"];
+  await each_async(list, async (item) => {});
   await bible_audio_player_generic(p, download_folder, filter);
   let book = "Proverbs";
   let lookup = await bible_books_names_lookup("engbsb");
