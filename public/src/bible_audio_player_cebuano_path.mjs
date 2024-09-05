60229638c20cf9aa0d651e12d08ce1ba18dc8670
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 19:37:06 -0400

    2024-09-05T23:37:05.278Z function_auto_after bible_audio_player_cebuano_path

--- public/src/bible_audio_player_cebuano_path.mjs
+++ public/src/bible_audio_player_cebuano_path.mjs
@@ -1,5 +1,5 @@
 import { bible_audio_player_path } from "./bible_audio_player_path.mjs";
 export function bible_audio_player_cebuano_path() {
-  let name = "english";
+  let name = "cebuano";
   return bible_audio_player_path(name);
 }
