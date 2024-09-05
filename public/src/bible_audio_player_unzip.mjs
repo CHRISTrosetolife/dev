48992d067cd6709864238a45b8c66bfebff9df82
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 13:44:50 -0400

    2024-09-05T17:44:49.612Z r folder_read_shallow_folders_names folder_read_shallow_folders_bases

--- public/src/bible_audio_player_unzip.mjs
+++ public/src/bible_audio_player_unzip.mjs
@@ -1,12 +1,12 @@
+import { folder_read_shallow_folders_bases } from "./folder_read_shallow_folders_bases.mjs";
 import { path_parse_name } from "./path_parse_name.mjs";
 import { list_map } from "./list_map.mjs";
-import { folder_read_shallow_folders_names } from "./folder_read_shallow_folders_names.mjs";
 import { folder_read_shallow } from "./folder_read_shallow.mjs";
 import { bible_audio_player_english_path } from "./bible_audio_player_english_path.mjs";
 export async function bible_audio_player_unzip() {
   let p = bible_audio_player_english_path();
   let zips = await folder_read_shallow(p, ".zip");
   let zip_names = list_map(zips, path_parse_name);
-  let folder_names = await folder_read_shallow_folders_names(p);
+  let folder_names = await folder_read_shallow_folders_bases(p);
   return folder_names;
 }
--- public/src/folder_read_shallow_folders_bases.mjs
+++ public/src/folder_read_shallow_folders_bases.mjs
@@ -1,7 +1,7 @@
 import { path_parse_base } from "./path_parse_base.mjs";
 import { list_map } from "./list_map.mjs";
 import { folder_read_shallow_folders } from "./folder_read_shallow_folders.mjs";
-export async function folder_read_shallow_folders_names(p) {
+export async function folder_read_shallow_folders_bases(p) {
   let folders = await folder_read_shallow_folders(p);
   let names = list_map(folders, path_parse_base);
   return names;
