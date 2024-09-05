bcc1be8fc98524f72bfd359240ceb2a68ee5ae4f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 13:41:09 -0400

    2024-09-05T17:41:08.412Z function_auto_after bible_audio_player_unzip

--- public/src/bible_audio_player_unzip.mjs
+++ public/src/bible_audio_player_unzip.mjs
@@ -1,9 +1,10 @@
 import { list_filter_async } from "./list_filter_async.mjs";
 import { folder_read_shallow } from "./folder_read_shallow.mjs";
 import { bible_audio_player_english_path } from "./bible_audio_player_english_path.mjs";
+import { folder_is } from "./folder_is.mjs";
 export async function bible_audio_player_unzip() {
   let p = bible_audio_player_english_path();
   let zips = await folder_read_shallow(p, ".zip");
   let all = await folder_read_shallow(p, "");
-  let folders = list_filter_async;
+  let folders = await list_filter_async(all, folder_is);
 }
