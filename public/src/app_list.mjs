726984681771579a2ed4d12ce64925384d561125
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 19:48:29 -0400

    2024-09-05T23:48:28.716Z function_auto_after app_list

--- public/src/app_list.mjs
+++ public/src/app_list.mjs
@@ -1,1 +1,4 @@
-export function app_list() {}
+import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
+export function app_list() {
+  let body = html_style_default_initialize();
+}
