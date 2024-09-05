637439ca916102254a934a855270434e5b1c4ed3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 19:36:56 -0400

    2024-09-05T23:36:54.650Z c bible_audio_player_english_path bible_audio_player_cebuano_path

---
+++ public/src/bible_audio_player_cebuano_path.mjs
@@ -0,0 +1,5 @@
+import { bible_audio_player_path } from "./bible_audio_player_path.mjs";
+export function bible_audio_player_cebuano_path() {
+  let name = "english";
+  return bible_audio_player_path(name);
+}
