5c21141416d9664d45b1abea4f0f0b9afe932b31
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 13:40:17 -0400

    2024-09-05T17:40:16.377Z function_auto_after folder_is

--- public/src/folder_is.mjs
+++ public/src/folder_is.mjs
@@ -1,4 +1,6 @@
-export async function folder_is(fs, path_full) {
+import { import_node } from "./import_node.mjs";
+export async function folder_is(path_full) {
+  let fs = await import_node("fs");
   let stat = await fs.promises.lstat(path_full);
   let path_folder_is = stat.isDirectory();
   return path_folder_is;
