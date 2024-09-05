df5ba673137a988d5126a500023e62d659fd11bb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 19:41:17 -0400

    2024-09-05T23:41:16.573Z function_auto_after bible_audio_player_cebuano

--- public/src/bible_audio_player_cebuano.mjs
+++ public/src/bible_audio_player_cebuano.mjs
@@ -12,8 +12,9 @@ export async function bible_audio_player_cebuano() {
   let p = bible_audio_player_cebuano_path();
   await folder_unzip(p);
   let folders = ["CEBRPVN1DA", "CEBRPVN2DA"];
-  await each_async(list, async (item) => {});
-  await bible_audio_player_generic(p, download_folder, filter);
+  await each_async(folders, async (download_folder) => {
+    await bible_audio_player_generic(p, download_folder, filter);
+  });
   let book = "Proverbs";
   let lookup = await bible_books_names_lookup("engbsb");
   let inverted = object_invert(lookup);
