3f49a4a19d481f5e35f32399f64f021f91303e16
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 19:48:56 -0400

    2024-09-05T23:48:55.789Z function_auto_after app_new

--- public/src/app_new.mjs
+++ public/src/app_new.mjs
@@ -1,10 +1,20 @@
+import { file_write } from "./file_write.mjs";
+import { function_new_generic } from "./function_new_generic.mjs";
 import { app_html_generate } from "./app_html_generate.mjs";
 import { app_prefix } from "./app_prefix.mjs";
-import { function_new } from "./function_new.mjs";
 import { string_combine } from "./string_combine.mjs";
 export async function app_new(name) {
   await app_html_generate(name);
   let prefix = app_prefix();
   let name_prefixed = string_combine(prefix, name);
-  await function_new(name_prefixed);
+  let open = true;
+  await function_new_generic(
+    name_prefixed,
+    "",
+    "",
+    open,
+    [],
+    false,
+    file_write,
+  );
 }
