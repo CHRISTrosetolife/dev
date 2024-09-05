f87dc91c6a554c60be3fe05e3eb236fe9f11fa5e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 16:49:21 -0400

    2024-09-05T20:49:15.803Z function_auto_after bible_audio_player_proverbs

--- public/src/bible_audio_player_proverbs.mjs
+++ public/src/bible_audio_player_proverbs.mjs
@@ -1,4 +1,3 @@
-import { command_line } from "./command_line.mjs";
 import { string_includes_curry_right } from "./string_includes_curry_right.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { bible_books_names_lookup } from "./bible_books_names_lookup.mjs";
@@ -8,7 +7,6 @@ import { bible_audio_player_english } from "./bible_audio_player_english.mjs";
 import { string_includes } from "./string_includes.mjs";
 export async function bible_audio_player_proverbs() {
   let book = "Proverbs";
-  await command_line("format e:/Q ");
   let lookup = await bible_books_names_lookup("engbsb");
   let inverted = object_invert(lookup);
   let acronym = object_property_get(inverted, book);
