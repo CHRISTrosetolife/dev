86895f0f4bd2d8922dd849c5772243a5f7dc719b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 19:55:58 -0400

    2024-09-05T23:55:58.399Z function_auto_after app_list

--- public/src/app_list.mjs
+++ public/src/app_list.mjs
@@ -1,3 +1,4 @@
+import { html_value_get } from "./html_value_get.mjs";
 import { html_button_width_full_text_click_next } from "./html_button_width_full_text_click_next.mjs";
 import { html_style_width_full } from "./html_style_width_full.mjs";
 import { html_textarea } from "./html_textarea.mjs";
@@ -6,5 +7,7 @@ export function app_list() {
   let body = html_style_default_initialize();
   let t = html_textarea(body);
   html_style_width_full(t);
-  html_button_width_full_text_click_next(body, () => {});
+  html_button_width_full_text_click_next(body, () => {
+    let value = html_value_get(input);
+  });
 }
