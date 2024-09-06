7a7f88ab86903065053e095cf72d2b0acf70d725
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 20:21:33 -0400

    2024-09-06T00:21:32.825Z function_auto_after app_ceb_words_english_without_bible

--- public/src/app_ceb_words_english_without_bible.mjs
+++ public/src/app_ceb_words_english_without_bible.mjs
@@ -1,12 +1,8 @@
 import { bible_words_eng_symbols } from "./bible_words_eng_symbols.mjs";
 import { bible_words_map } from "./bible_words_map.mjs";
-import { list_map } from "./list_map.mjs";
 import { bible_words_eng_cache } from "./bible_words_eng_cache.mjs";
-import { string_case_lower } from "./string_case_lower.mjs";
 export async function app_ceb_words_english_without_bible() {
   let all = await bible_words_eng_cache();
   all = bible_words_map(all, bible_words_eng_symbols());
-  list_map(engs, (e) => {
-    let lower = string_case_lower(e);
-  });
+  return all;
 }
