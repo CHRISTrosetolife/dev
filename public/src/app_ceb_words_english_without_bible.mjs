05bfdb5e3d0ace70cc2c0734f176a4aad44dd810
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 20:24:26 -0400

    2024-09-06T00:24:25.733Z function_auto_after app_ceb_words_english_without_bible

--- public/src/app_ceb_words_english_without_bible.mjs
+++ public/src/app_ceb_words_english_without_bible.mjs
@@ -4,7 +4,7 @@ import { bible_words_eng_symbols } from "./bible_words_eng_symbols.mjs";
 import { bible_words_map } from "./bible_words_map.mjs";
 import { bible_words_eng_cache } from "./bible_words_eng_cache.mjs";
 export async function app_ceb_words_english_without_bible() {
-  return await app_ceb_words_english();
+  let cebs = await app_ceb_words_english();
   let all = await bible_words_eng_cache();
   all = bible_words_map(all, bible_words_eng_symbols());
   let lookup = list_to_lookup(all);
