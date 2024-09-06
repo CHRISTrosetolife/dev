e563fc4ee75252bcb00022bcba610604ddaead96
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 20:19:08 -0400

    2024-09-06T00:19:08.250Z function_auto_after app_ceb_words_english_without_bible

--- public/src/app_ceb_words_english_without_bible.mjs
+++ public/src/app_ceb_words_english_without_bible.mjs
@@ -1,4 +1,4 @@
 import { bible_words_eng_cache } from "./bible_words_eng_cache.mjs";
 export async function app_ceb_words_english_without_bible() {
-  return await bible_words_eng_cache();
+  let engs = await bible_words_eng_cache();
 }
