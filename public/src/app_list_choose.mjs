118e056e2e8579a0e9fddbdc38b990d7dc9ed371
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 20:53:48 -0400

    2024-09-06T00:53:48.260Z function_auto_after app_list_choose

--- public/src/app_list_choose.mjs
+++ public/src/app_list_choose.mjs
@@ -1,3 +1,4 @@
+import { html_hr } from "./html_hr.mjs";
 import { app_list_root } from "./app_list_root.mjs";
 import { storage_local_remove } from "./storage_local_remove.mjs";
 import { log } from "./log.mjs";
@@ -11,10 +12,11 @@ import { storage_local_get } from "./storage_local_get.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 export function app_list_choose(body) {
   html_clear_scroll_top_centered(body);
-  let b = html_button_width_full_text_click(body, "clear list", () => {
+  html_button_width_full_text_click(body, "clear list", () => {
     storage_local_remove(app_list, "list");
     app_list_root(body);
   });
+  html_hr(body);
   let split = storage_local_get(app_list, "list");
   let chosen = {};
   each_index(split, (line, index) => {
