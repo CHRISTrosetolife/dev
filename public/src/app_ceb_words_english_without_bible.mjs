410ef0a67b90e3f7a5b9452291f332d4a88930ba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 20:19:19 -0400

    2024-09-06T00:19:18.773Z function_auto_after app_ceb_words_english_without_bible

--- public/src/app_ceb_words_english_without_bible.mjs
+++ public/src/app_ceb_words_english_without_bible.mjs
@@ -1,4 +1,6 @@
+import { list_map } from "./list_map.mjs";
 import { bible_words_eng_cache } from "./bible_words_eng_cache.mjs";
 export async function app_ceb_words_english_without_bible() {
   let engs = await bible_words_eng_cache();
+  list_map(engs, () => {});
 }
