ca7bd7058eb24a8296d1c813ce7181775323743b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 20:49:52 -0400

    2024-09-06T00:49:51.909Z function_auto_after app_list

--- public/src/app_list.mjs
+++ public/src/app_list.mjs
@@ -1,24 +1,10 @@
-import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
+import { app_list_root } from "./app_list_root.mjs";
 import { app_list_choose } from "./app_list_choose.mjs";
 import { storage_local_exists } from "./storage_local_exists.mjs";
-import { storage_local_set } from "./storage_local_set.mjs";
-import { string_split_newline } from "./string_split_newline.mjs";
-import { html_value_get } from "./html_value_get.mjs";
-import { html_button_width_full_text_click_next } from "./html_button_width_full_text_click_next.mjs";
-import { html_style_width_full } from "./html_style_width_full.mjs";
-import { html_textarea } from "./html_textarea.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_list() {
   let body = html_style_default_initialize();
-  html_clear_scroll_top_centered(body);
-  let t = html_textarea(body);
-  html_style_width_full(t);
-  html_button_width_full_text_click_next(body, () => {
-    let value = html_value_get(t);
-    let split = string_split_newline(value);
-    storage_local_set(app_list, "list", split);
-    app_list_choose(body);
-  });
+  app_list_root(body);
   if (storage_local_exists(app_list, "list")) {
     app_list_choose(body);
   }
---
+++ public/src/app_list_root.mjs
@@ -0,0 +1,20 @@
+import { app_list_choose } from "./app_list_choose.mjs";
+import { app_list } from "./app_list.mjs";
+import { storage_local_set } from "./storage_local_set.mjs";
+import { string_split_newline } from "./string_split_newline.mjs";
+import { html_value_get } from "./html_value_get.mjs";
+import { html_button_width_full_text_click_next } from "./html_button_width_full_text_click_next.mjs";
+import { html_style_width_full } from "./html_style_width_full.mjs";
+import { html_textarea } from "./html_textarea.mjs";
+import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
+export function app_list_root(body) {
+  html_clear_scroll_top_centered(body);
+  let t = html_textarea(body);
+  html_style_width_full(t);
+  html_button_width_full_text_click_next(body, () => {
+    let value = html_value_get(t);
+    let split = string_split_newline(value);
+    storage_local_set(app_list, "list", split);
+    app_list_choose(body);
+  });
+}
