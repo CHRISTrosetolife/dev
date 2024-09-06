08051628f6c0c7d1b6f6e2ecd0318c93ee1f9fea
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 20:20:57 -0400

    2024-09-06T00:20:55.884Z function_auto_after app_ceb_words_english_without_bible

--- public/src/app_ceb_words_english_without_bible.mjs
+++ public/src/app_ceb_words_english_without_bible.mjs
@@ -1,8 +1,10 @@
+import { bible_words_map } from "./bible_words_map.mjs";
 import { list_map } from "./list_map.mjs";
 import { bible_words_eng_cache } from "./bible_words_eng_cache.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 export async function app_ceb_words_english_without_bible() {
-  let engs = await bible_words_eng_cache();
+  let all = await bible_words_eng_cache();
+  all = bible_words_map(all, symbols);
   list_map(engs, (e) => {
     let lower = string_case_lower(e);
   });
