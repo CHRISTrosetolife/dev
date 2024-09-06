770b7c80aafca35470a1b6fc38bafda3e020d61e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-05 20:33:44 -0400

    2024-09-06T00:33:43.969Z function_auto_after app_ceb_words_english_without_bible

--- public/src/app_ceb_words_english_without_bible.mjs
+++ public/src/app_ceb_words_english_without_bible.mjs
@@ -1,5 +1,5 @@
+import { app_ceb_words_english_list } from "./app_ceb_words_english_list.mjs";
 import { list_to_lookup } from "./list_to_lookup.mjs";
-import { app_ceb_words_english } from "./app_ceb_words_english.mjs";
 import { bible_words_eng_symbols } from "./bible_words_eng_symbols.mjs";
 import { bible_words_map } from "./bible_words_map.mjs";
 import { bible_words_eng_cache } from "./bible_words_eng_cache.mjs";
@@ -7,7 +7,7 @@ import { list_filter } from "./list_filter.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { identity } from "./identity.mjs";
 export async function app_ceb_words_english_without_bible() {
-  let cebs = await app_ceb_words_english();
+  let cebs = await app_ceb_words_english_list();
   let all = await bible_words_eng_cache();
   all = bible_words_map(all, bible_words_eng_symbols());
   let lookup = list_to_lookup(all, identity);
