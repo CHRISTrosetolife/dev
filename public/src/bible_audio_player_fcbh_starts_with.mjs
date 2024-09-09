04bf60096b774c74940c0b446579863e66b81421
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 08:11:46 -0400

    2024-09-09T12:11:45.825Z function_auto_after bible_audio_player_fcbh_starts_with

---
+++ public/src/bible_audio_player_fcbh_starts_with.mjs
@@ -0,0 +1,7 @@
+import { string_starts_with } from "./string_starts_with.mjs";
+import { path_parse_base } from "./path_parse_base.mjs";
+export function bible_audio_player_fcbh_starts_with(file_path) {
+  let file_name = path_parse_base(file_path);
+  let e = string_starts_with(file_name, "B" + n);
+  return e;
+}
