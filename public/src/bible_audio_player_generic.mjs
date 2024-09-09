27c7a790c4e1730eb4562d8d4ef502165591139a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 16:50:17 -0400

    2024-09-09T20:50:17.000Z function_auto_after bible_audio_player_generic

--- public/src/bible_audio_player_generic.mjs
+++ public/src/bible_audio_player_generic.mjs
@@ -1,4 +1,3 @@
-import { identity } from "./identity.mjs";
 import { folder_files_copy_generic } from "./folder_files_copy_generic.mjs";
 import { path_join } from "./path_join.mjs";
 export async function bible_audio_player_generic(
@@ -11,5 +10,5 @@ export async function bible_audio_player_generic(
   if (10) {
     output = "..\\bible\\english\\test";
   }
-  await folder_files_copy_generic(joined, ".mp3", output, filter, identity);
+  await folder_files_copy_generic(joined, ".mp3", output, filter, (to) => {});
 }
