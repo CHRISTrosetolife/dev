442e13bccce1d3fa36d686b7446a81c7e65282f5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 08:30:45 -0400

    2024-09-09T12:30:44.452Z function_auto_after bible_audio_player_proverbs

--- public/src/bible_audio_player_proverbs.mjs
+++ public/src/bible_audio_player_proverbs.mjs
@@ -1,6 +1,5 @@
-import { bible_audio_player_range } from "./bible_audio_player_range.mjs";
+import { bible_audio_player_book } from "./bible_audio_player_book.mjs";
 export async function bible_audio_player_proverbs() {
   let book_from = "Proverbs";
-  let book_to = book_from;
-  await bible_audio_player_range(book_from, book_to);
+  await bible_audio_player_book(book_from);
 }
