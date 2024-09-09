4639fc3ad4a0a00189be0c7899603a8a0affa84e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 12:43:46 -0400

    2024-09-09T16:43:45.553Z function_auto_after bible_audio_player_fcbh_starts_with

--- public/src/bible_audio_player_fcbh_starts_with.mjs
+++ public/src/bible_audio_player_fcbh_starts_with.mjs
@@ -1,16 +1,7 @@
-import { string_skip } from "./string_skip.mjs";
+import { path_parse_base_starts_with } from "./path_parse_base_starts_with.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
-import { log } from "./log.mjs";
-import { path_parse_base } from "./path_parse_base.mjs";
 export function bible_audio_player_fcbh_starts_with(file_path, n) {
-  let file_name = path_parse_base(file_path);
-  let without_first = string_skip(file_name, 1);
-  if (10) {
-    log({
-      without_first,
-      n,
-    });
-  }
+  let file_name = path_parse_base_starts_with(file_path);
   let e = string_starts_with(without_first, n);
   return e;
 }
