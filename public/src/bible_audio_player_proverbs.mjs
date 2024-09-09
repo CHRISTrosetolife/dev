70bbaf4faa0f937eaaec25811091e67eb2de11b3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 07:30:01 -0400

    2024-09-09T11:30:00.237Z function_auto_after bible_audio_player_proverbs

--- public/src/bible_audio_player_proverbs.mjs
+++ public/src/bible_audio_player_proverbs.mjs
@@ -1,15 +1,9 @@
 import { string_includes_curry_right } from "./string_includes_curry_right.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { bible_books_names_lookup } from "./bible_books_names_lookup.mjs";
-import { object_invert } from "./object_invert.mjs";
 import { each_async } from "./each_async.mjs";
 import { bible_audio_player_english } from "./bible_audio_player_english.mjs";
 import { string_includes } from "./string_includes.mjs";
 export async function bible_audio_player_proverbs() {
   let book = "Proverbs";
-  let lookup = await bible_books_names_lookup("engbsb");
-  let inverted = object_invert(lookup);
-  let acronym = object_property_get(inverted, book);
   let download_folders_fcbh = ["ENGESVO2DA", "ENGESVN2DA"];
   await each_async(download_folders_fcbh, async (download_folder) => {
     await bible_audio_player_english(
