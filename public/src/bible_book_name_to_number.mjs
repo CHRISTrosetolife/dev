241c3900ab9ef809e564d2933eb53bdac7694eed
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 07:51:10 -0400

    2024-09-09T11:51:10.262Z function_auto_after bible_book_name_to_number

--- public/src/bible_book_name_to_number.mjs
+++ public/src/bible_book_name_to_number.mjs
@@ -1,5 +1,4 @@
 import { list_index } from "./list_index.mjs";
-import { number_pad } from "./number_pad.mjs";
 import { bible_books } from "./bible_books.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { bible_books_names_lookup_inverted } from "./bible_books_names_lookup_inverted.mjs";
@@ -7,6 +6,6 @@ export async function bible_book_name_to_number(bible_folder, book) {
   let inverted = await bible_books_names_lookup_inverted(bible_folder);
   let acronym = object_property_get(inverted, book);
   let books = await bible_books(bible_folder);
-  let n = number_pad(list_index(books, acronym) + 1, 2);
+  let n = list_index(books, acronym) + 1;
   return n;
 }
