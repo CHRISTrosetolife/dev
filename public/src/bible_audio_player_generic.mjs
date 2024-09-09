927281e8ca287f6023b3a3ac23a5263138aa65f0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 17:13:09 -0400

    2024-09-09T21:13:07.123Z function_auto_after bible_audio_player_generic

--- public/src/bible_audio_player_generic.mjs
+++ public/src/bible_audio_player_generic.mjs
@@ -2,6 +2,7 @@ import { uuid } from "./uuid.mjs";
 import { folder_files_copy_generic } from "./folder_files_copy_generic.mjs";
 import { path_join } from "./path_join.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function bible_audio_player_generic(
   path_language,
   download_folder,
@@ -20,7 +21,12 @@ export async function bible_audio_player_generic(
     filter,
     async (to) => {
       let without = string_suffix_without(to, extension);
-      let with_id = without + "_" + (await uuid()) + extension;
+      let with_id = string_combine_multiple([
+        without,
+        "_",
+        await uuid(),
+        extension,
+      ]);
       return with_id;
     },
   );
