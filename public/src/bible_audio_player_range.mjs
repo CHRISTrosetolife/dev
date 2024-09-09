0f2a9c6fc1c8320771b408063d1ee4f1faa3a3e5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 12:32:09 -0400

    2024-09-09T16:32:08.590Z function_auto_after bible_audio_player_range

--- public/src/bible_audio_player_range.mjs
+++ public/src/bible_audio_player_range.mjs
@@ -32,7 +32,9 @@ export async function bible_audio_player_range(book_from, book_to) {
     (n) => n - bible_books_old_count(),
   );
   let numbers_fcbh = list_map(numbers_nt_filtered, (n) => {
-    n - bible_books_old_count();
+    if (0) {
+      n - bible_books_old_count();
+    }
   });
   let numbers_nt_s = list_map_number_pad_2(numbers_nt);
   let download_folders_fcbh_underscore = ["ENGESVO2DA", "ENGESVN2DA"];
