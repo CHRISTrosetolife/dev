76966eb9c6d52772806d3322a181dbf115eee5ba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 12:07:30 -0400

    2024-09-09T16:07:29.729Z function_auto_after number_pad_2

--- public/src/app_gs_bible_chapter_name.mjs
+++ public/src/app_gs_bible_chapter_name.mjs
@@ -1,5 +1,5 @@
-import { number_pad } from "./number_pad.mjs";
+import { number_pad_2 } from "./number_pad_2.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_gs_bible_chapter_name(book_id, chapter) {
-  return string_combine_multiple([book_id, number_pad(chapter, 2)]);
+  return string_combine_multiple([book_id, number_pad_2(chapter)]);
 }
---
+++ public/src/number_pad_2.mjs
@@ -0,0 +1,4 @@
+import { number_pad } from "./number_pad.mjs";
+export function number_pad_2(chapter) {
+  return number_pad(chapter, 2);
+}
