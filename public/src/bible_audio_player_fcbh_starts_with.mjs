2091158e2529d05a59b908ac66c6d8c3878171c6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 12:22:02 -0400

    2024-09-09T16:22:00.873Z function_auto_after bible_audio_player_fcbh_starts_with

--- public/src/bible_audio_player_fcbh_starts_with.mjs
+++ public/src/bible_audio_player_fcbh_starts_with.mjs
@@ -1,6 +1,16 @@
-import { path_parse_base_starts_with_generic } from "./path_parse_base_starts_with_generic.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
+import { log } from "./log.mjs";
+import { path_parse_base } from "./path_parse_base.mjs";
 export function bible_audio_player_fcbh_starts_with(file_path, n) {
   let prefix = "B";
-  let e = path_parse_base_starts_with_generic(file_path, prefix, n);
+  let file_name = path_parse_base(file_path);
+  let p = prefix + n;
+  if (0) {
+    log({
+      file_name,
+      p,
+    });
+  }
+  let e = string_starts_with(file_name, p);
   return e;
 }
