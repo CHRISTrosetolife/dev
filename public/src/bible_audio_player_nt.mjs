c226c027512219d7c6a6bcc96dacd92c8313a517
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 08:29:09 -0400

    2024-09-09T12:29:09.079Z function_auto_after bible_audio_player_nt

--- public/src/bible_audio_player_nt.mjs
+++ public/src/bible_audio_player_nt.mjs
@@ -1,1 +1,4 @@
-export function bible_audio_player_nt() {}
+import { bible_audio_player_range } from "./bible_audio_player_range.mjs";
+export async function bible_audio_player_nt() {
+  await bible_audio_player_range();
+}
