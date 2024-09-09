b17d71a70cf915dc8962e1aa743dbf22f3fda85b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 08:12:21 -0400

    2024-09-09T12:12:21.256Z function_auto_after bible_audio_player_fcbh_starts_with

--- public/src/bible_audio_player_fcbh_starts_with.mjs
+++ public/src/bible_audio_player_fcbh_starts_with.mjs
@@ -1,6 +1,6 @@
 import { string_starts_with } from "./string_starts_with.mjs";
 import { path_parse_base } from "./path_parse_base.mjs";
-export function bible_audio_player_fcbh_starts_with(file_path) {
+export function bible_audio_player_fcbh_starts_with(file_path, n) {
   let file_name = path_parse_base(file_path);
   let e = string_starts_with(file_name, "B" + n);
   return e;
