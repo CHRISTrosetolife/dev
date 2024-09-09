c565d292b60d2005f9701f5eb246d1826d2a9224
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 08:29:59 -0400

    2024-09-09T12:29:59.500Z function_auto_after bible_audio_player_nt

--- public/src/bible_audio_player_nt.mjs
+++ public/src/bible_audio_player_nt.mjs
@@ -1,4 +1,4 @@
 import { bible_audio_player_range } from "./bible_audio_player_range.mjs";
 export async function bible_audio_player_nt() {
-  await bible_audio_player_range();
+  await bible_audio_player_range("Matthew", "Revelation");
 }
