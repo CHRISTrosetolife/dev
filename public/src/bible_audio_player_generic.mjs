8f0195d43cc14434edbdd94939de166bdd809bee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 16:58:24 -0400

    2024-09-09T20:58:24.294Z function_auto_after bible_audio_player_generic

--- public/src/bible_audio_player_generic.mjs
+++ public/src/bible_audio_player_generic.mjs
@@ -20,7 +20,7 @@ export async function bible_audio_player_generic(
     filter,
     async (to) => {
       let without = string_suffix_without(to, extension);
-      let with_id = without + (await uuid()) + extension;
+      let with_id = without + "_" + (await uuid()) + extension;
       return with_id;
     },
   );
