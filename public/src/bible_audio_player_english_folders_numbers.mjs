37b347b9c8d0057ee2f9e9c982982d883be2035e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 12:15:35 -0400

    2024-09-09T16:15:34.526Z function_auto_after bible_audio_player_english_folders_numbers

--- public/src/bible_audio_player_english_folders_numbers.mjs
+++ public/src/bible_audio_player_english_folders_numbers.mjs
@@ -1,18 +1,15 @@
 import { bible_audio_player_english } from "./bible_audio_player_english.mjs";
 import { each_async } from "./each_async.mjs";
 export async function bible_audio_player_english_folders_numbers(
-  download_folders_fcbh_underscore,
+  download_folders,
   numbers_s,
   filter,
 ) {
-  await each_async(
-    download_folders_fcbh_underscore,
-    async (download_folder) => {
-      await each_async(numbers_s, async (n) => {
-        await bible_audio_player_english(download_folder, (file_path) =>
-          filter(file_path, n),
-        );
-      });
-    },
-  );
+  await each_async(download_folders, async (download_folder) => {
+    await each_async(numbers_s, async (n) => {
+      await bible_audio_player_english(download_folder, (file_path) =>
+        filter(file_path, n),
+      );
+    });
+  });
 }
