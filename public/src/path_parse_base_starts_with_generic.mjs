0e2a88b486b7e7bcdeb34ac325dab057614e4bb3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-09 12:21:46 -0400

    2024-09-09T16:21:43.188Z function_auto_after path_parse_base_starts_with_generic

--- public/src/path_parse_base_starts_with_generic.mjs
+++ public/src/path_parse_base_starts_with_generic.mjs
@@ -4,10 +4,12 @@ import { path_parse_base } from "./path_parse_base.mjs";
 export function path_parse_base_starts_with_generic(file_path, prefix, n) {
   let file_name = path_parse_base(file_path);
   let p = prefix + n;
-  log({
-    file_name,
-    p,
-  });
+  if (0) {
+    log({
+      file_name,
+      p,
+    });
+  }
   let e = string_starts_with(file_name, p);
   return e;
 }
