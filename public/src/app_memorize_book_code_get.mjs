7e7ec4425806e8eaae1b568d1363d7d2ffca0717
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 13:49:01 -0400

    2024-09-10T17:49:00.511Z function_auto_after app_memorize_book_code_get

--- public/src/app_memorize_book_code_get.mjs
+++ public/src/app_memorize_book_code_get.mjs
@@ -2,6 +2,5 @@ import { app_memorize_save_get } from "./app_memorize_save_get.mjs";
 export function app_memorize_book_code_get(context) {
   let save = app_memorize_save_get(context);
   let { book_code } = save;
-  let b = book_code;
-  return b;
+  return book_code;
 }
