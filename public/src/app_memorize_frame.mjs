866816211211e3ce1c5c10efdb0c0b939c86b9d1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 12:00:26 -0400

    2024-09-10T16:00:25.568Z function_auto_after app_memorize_frame

--- public/src/app_memorize_frame.mjs
+++ public/src/app_memorize_frame.mjs
@@ -1,3 +1,4 @@
+import { object_property_initialize_get } from "./object_property_initialize_get.mjs";
 import { app_memorize_save_get } from "./app_memorize_save_get.mjs";
 import { app_memorize_save } from "./app_memorize_save.mjs";
 import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.mjs";
@@ -17,6 +18,7 @@ import { list_size } from "./list_size.mjs";
 import { object_merge } from "./object_merge.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 export async function app_memorize_frame(context) {
+  object_property_initialize_get();
   let chapter_code = "JHN19";
   let verses = await bible_engbsb_storage_http_get(chapter_code);
   object_merge(context, {
