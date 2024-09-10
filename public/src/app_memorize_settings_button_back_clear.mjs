1456d44dbc3c6aee42243064bc4f111ab102a130
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 11:50:56 -0400

    2024-09-10T15:50:55.509Z function_auto_after app_memorize_settings_button_back_clear

--- public/src/app_memorize_settings_button_back_clear.mjs
+++ public/src/app_memorize_settings_button_back_clear.mjs
@@ -3,5 +3,5 @@ import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 export function app_memorize_settings_button_back_clear(context) {
   let { root } = context;
   html_clear_scroll_top(root);
-  app_memorize_settings_button_back(root, context);
+  app_memorize_settings_button_back(context);
 }
