8e31874ea2886ff027203e6f97715675ac3704c8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 12:12:44 -0400

    2024-09-10T16:12:43.986Z function_auto_after app_memorize_frame

--- public/src/app_memorize_frame.mjs
+++ public/src/app_memorize_frame.mjs
@@ -1,55 +1,10 @@
-import { object_property_initialize_get } from "./object_property_initialize_get.mjs";
-import { app_memorize_save_get } from "./app_memorize_save_get.mjs";
-import { app_memorize_save } from "./app_memorize_save.mjs";
-import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.mjs";
 import { html_on_remove } from "./html_on_remove.mjs";
-import { object_property_get_or } from "./object_property_get_or.mjs";
-import { app_memorize_groups_get } from "./app_memorize_groups_get.mjs";
 import { app_memorize_on_keydown } from "./app_memorize_on_keydown.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { html_on } from "./html_on.mjs";
 import { app_memorize_refresh_memorize } from "./app_memorize_refresh_memorize.mjs";
-import { list_index } from "./list_index.mjs";
-import { html_hash } from "./html_hash.mjs";
-import { list_first } from "./list_first.mjs";
-import { app_memorize_group_current_set } from "./app_memorize_group_current_set.mjs";
-import { app_memorize_group } from "./app_memorize_group.mjs";
-import { list_size } from "./list_size.mjs";
-import { object_merge } from "./object_merge.mjs";
 import { html_document_body } from "./html_document_body.mjs";
-import { string_combine } from "./string_combine.mjs";
 export async function app_memorize_frame(context) {
-  let save = app_memorize_save_get(context);
-  let book_code = object_property_initialize_get(save, "book_code", "JHN");
-  let chapter = object_property_initialize_get(save, "chapter", "19");
-  app_memorize_save(context);
-  let chapter_code = string_combine(book_code, chapter);
-  let verses = await bible_engbsb_storage_http_get(chapter_code);
-  object_merge(context, {
-    verses,
-  });
-  let verses_length = list_size(context.verses);
-  let groups = app_memorize_group(verses_length);
-  object_merge(context, {
-    groups,
-  });
-  context.button_height = 7;
-  let group = object_property_get_or(
-    save,
-    "group_current",
-    list_first(context.groups),
-  );
-  html_hash({
-    verses: (value) => {
-      group = app_memorize_groups_get(context, value);
-    },
-    pattern: (value) => {
-      let save = app_memorize_save_get(context);
-      save.pattern_index = list_index(context.patterns, value);
-      app_memorize_save(context);
-    },
-  });
-  app_memorize_group_current_set(context, group);
   app_memorize_refresh_memorize(context);
   let type = "keydown";
   html_on(html_document_body(), type, on_keydown);
