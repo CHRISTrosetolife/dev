50f33e8294d3be0dade463730bcb6f35ced77826
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 12:09:48 -0400

    2024-09-10T16:09:47.614Z r object_properties_delete object_property_delete_all

--- public/src/object_property_delete_all.mjs
+++ public/src/object_property_delete_all.mjs
@@ -1,6 +1,6 @@
 import { object_property_delete } from "./object_property_delete.mjs";
 import { object_properties } from "./object_properties.mjs";
-export function object_properties_delete(object) {
+export function object_property_delete_all(object) {
   for (let property of object_properties(object)) {
     object_property_delete(object, property);
   }
--- public/src/object_replace.mjs
+++ public/src/object_replace.mjs
@@ -1,6 +1,6 @@
-import { object_properties_delete } from "./object_properties_delete.mjs";
+import { object_property_delete_all } from "./object_property_delete_all.mjs";
 import { object_merge } from "./object_merge.mjs";
 export function object_replace(original, replacement) {
-  object_properties_delete(original);
+  object_property_delete_all(original);
   object_merge(original, replacement);
 }
