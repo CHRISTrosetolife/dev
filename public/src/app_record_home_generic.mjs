e57a603a200ce3131015547a9ca63ec094e1a842
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 13:57:14 -0400

    2024-09-10T17:57:14.406Z function_auto_after app_record_home_generic

--- public/src/app_record_home_generic.mjs
+++ public/src/app_record_home_generic.mjs
@@ -12,8 +12,8 @@ export function app_record_home_generic(
   lambda_before(root);
   each(books, (book) => {
     let { book_code } = book;
-    html_button_text_click(root, book_code, function () {
-      app_record_home_on_click(context, book_code);
+    html_button_text_click(root, book_code, async function () {
+      await app_record_home_on_click(context, book_code);
     });
   });
 }
