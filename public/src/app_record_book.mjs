f0631c0e21f304b2e577aaca13c50b65faac1d88
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 11:55:36 -0400

    2024-09-10T15:55:36.267Z function_auto_after app_record_book

--- public/src/app_record_book.mjs
+++ public/src/app_record_book.mjs
@@ -1,3 +1,4 @@
+import { noop } from "./noop.mjs";
 import { app_record_home_button } from "./app_record_home_button.mjs";
 import { app_record_chapter } from "./app_record_chapter.mjs";
 import { list_find_property_or } from "./list_find_property_or.mjs";
@@ -5,6 +6,7 @@ import { html_button_text_click } from "./html_button_text_click.mjs";
 import { each } from "./each.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 export function app_record_book(context, book_code) {
+  let lambda_top = noop;
   let app_record_book_on_click = app_record_chapter;
   let { root } = context;
   html_clear_scroll_top(root);
