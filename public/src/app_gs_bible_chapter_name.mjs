1a48c5c89d5d9fff485e9119192cda09b1976ecb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 13:36:10 -0400

    2024-09-10T17:36:10.330Z function_auto_after app_gs_bible_chapter_name

--- public/src/app_gs_bible_chapter_name.mjs
+++ public/src/app_gs_bible_chapter_name.mjs
@@ -5,6 +5,7 @@ export function app_gs_bible_chapter_name(book_id, chapter) {
   if (book_code === "PSA") {
     chapter = number_pad(chapter, 3);
   } else {
+    chapter = number_pad_2(chapter);
   }
   return string_combine_multiple([book_id, number_pad_2(chapter)]);
 }
