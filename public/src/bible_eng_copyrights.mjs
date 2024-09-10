6d651df9898116061b8933ccd83ff2dc88f0464a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 18:03:19 -0400

    2024-09-10T22:03:19.442Z function_auto_after bible_eng_copyrights

--- public/src/bible_eng_copyrights.mjs
+++ public/src/bible_eng_copyrights.mjs
@@ -1,7 +1,9 @@
+import { html_cache_parse } from "./html_cache_parse.mjs";
 import { bible_url_htm } from "./bible_url_htm.mjs";
 import { bible_eng_versions_each } from "./bible_eng_versions_each.mjs";
 export async function bible_eng_copyrights() {
   await bible_eng_versions_each(async function lambda(bible_folder) {
     let url = bible_url_htm(bible_folder, "copyright");
+    let root = await html_cache_parse(url);
   });
 }
