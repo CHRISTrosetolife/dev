4d250690b7c5268ae9c82041da9c9451023efcb6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 11:39:37 -0400

    2024-09-10T15:39:34.047Z function_auto_after app_memorize

--- public/src/app_memorize.mjs
+++ public/src/app_memorize.mjs
@@ -1,3 +1,4 @@
+import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.mjs";
 import { log } from "./log.mjs";
 import { storage_local_initialize } from "./storage_local_initialize.mjs";
 import { html_script_axios } from "./html_script_axios.mjs";
@@ -14,6 +15,10 @@ export async function app_memorize() {
     style: {},
     save,
   };
+  let { books } = await bible_engbsb_storage_http_get("books");
+  object_merge(context, {
+    books,
+  });
   context.settings = true;
   let root = html_style_default_initialize();
   html_style(root, {
