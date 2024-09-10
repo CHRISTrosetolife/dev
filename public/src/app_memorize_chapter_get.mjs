09c651451b43fe1e06d4bcc61acb6ea37ad1813b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 13:49:23 -0400

    2024-09-10T17:49:19.330Z function_auto_after app_memorize_chapter_get

--- public/src/app_memorize_chapter_get.mjs
+++ public/src/app_memorize_chapter_get.mjs
@@ -1,6 +1,6 @@
 import { app_memorize_save_get } from "./app_memorize_save_get.mjs";
 export function app_memorize_chapter_get(context) {
   let save = app_memorize_save_get(context);
-  let { book_code } = save;
-  return book_code;
+  let { chapter } = save;
+  return chapter;
 }
