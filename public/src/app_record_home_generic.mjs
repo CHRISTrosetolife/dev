f245acba909ef7274a54b93ea6a80446f237a83d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 13:44:54 -0400

    2024-09-10T17:44:50.560Z function_auto_after app_record_home_generic

--- public/src/app_record_home_generic.mjs
+++ public/src/app_record_home_generic.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { html_button_text_click } from "./html_button_text_click.mjs";
 import { each } from "./each.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
@@ -13,6 +14,7 @@ export function app_record_home_generic(
   each(books, (book) => {
     let { book_code } = book;
     html_button_text_click(root, book_code, function () {
+      log("here2");
       app_record_home_on_click(context, book_code);
     });
   });
