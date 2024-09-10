1d73710efc79bfa67ff27263cd9cc5065ee2fe0c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 18:01:51 -0400

    2024-09-10T22:01:50.725Z function_auto_after bible_eng_copyrights

--- public/src/bible_eng_copyrights.mjs
+++ public/src/bible_eng_copyrights.mjs
@@ -1,13 +1,7 @@
-import { bible_chapter } from "./bible_chapter.mjs";
-import { each_async } from "./each_async.mjs";
 import { bible_books_chapters } from "./bible_books_chapters.mjs";
 import { bible_eng_versions_each } from "./bible_eng_versions_each.mjs";
 export async function bible_eng_copyrights() {
   await bible_eng_versions_each(async function lambda(bible_folder) {
     let chapters = await bible_books_chapters(bible_folder);
-    await each_async(chapters, async (c) => {
-      let { chapter_code } = c;
-      la(await bible_chapter(bible_folder, chapter_code));
-    });
   });
 }
