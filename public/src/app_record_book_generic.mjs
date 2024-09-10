ea2d6470c69fc046494620b266dcec65b00c57a7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 13:43:50 -0400

    2024-09-10T17:43:49.200Z function_auto_after app_record_book_generic

--- public/src/app_record_book_generic.mjs
+++ public/src/app_record_book_generic.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { html_button_text_click } from "./html_button_text_click.mjs";
 import { each } from "./each.mjs";
 import { list_find_property_or } from "./list_find_property_or.mjs";
@@ -16,12 +15,9 @@ export function app_record_book_generic(
   app_record_home_button(context);
   let { books } = context;
   let book = list_find_property_or(books, "book_code", book_code);
-  log("here32");
   each(book.chapters, (chapter) => {
     html_button_text_click(root, chapter, async () => {
-      log("here2");
       await app_record_book_on_click(context, book_code, chapter);
     });
   });
-  debugger;
 }
