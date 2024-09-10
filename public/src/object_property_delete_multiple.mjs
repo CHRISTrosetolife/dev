d44ca6e22972cb08f6fdc5bf560cb8b4712b58b1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 12:09:16 -0400

    2024-09-10T16:09:15.390Z function_auto_after object_property_delete_multiple

--- public/src/object_property_delete_multiple.mjs
+++ public/src/object_property_delete_multiple.mjs
@@ -1,7 +1,7 @@
 import { object_property_delete } from "./object_property_delete.mjs";
 import { each } from "./each.mjs";
-export function object_property_delete_multiple(save, properties) {
+export function object_property_delete_multiple(object, properties) {
   each(properties, (property) => {
-    object_property_delete(save, property);
+    object_property_delete(object, property);
   });
 }
