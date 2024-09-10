ba604583f63e09cbf57ff5793910b542e8258806
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 12:11:20 -0400

    2024-09-10T16:11:19.282Z function_auto_after app_memorize_chapter_set

--- public/src/app_memorize_chapter_set.mjs
+++ public/src/app_memorize_chapter_set.mjs
@@ -1,3 +1,4 @@
+import { app_memorize_save } from "./app_memorize_save.mjs";
 import { app_memorize_save_get } from "./app_memorize_save_get.mjs";
 import { object_property_delete_multiple } from "./object_property_delete_multiple.mjs";
 import { object_merge } from "./object_merge.mjs";
@@ -8,4 +9,5 @@ export function app_memorize_chapter_set(context, chapter) {
   });
   let properties = ["group_current", "pattern_index"];
   object_property_delete_multiple(save, properties);
+  app_memorize_save(context);
 }
