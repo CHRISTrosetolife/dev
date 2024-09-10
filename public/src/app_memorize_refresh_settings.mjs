2c730573d0d453831e7eeedb960ce62fc52a507b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 13:51:39 -0400

    2024-09-10T17:51:38.388Z function_auto_after app_memorize_refresh_settings

--- public/src/app_memorize_refresh_settings.mjs
+++ public/src/app_memorize_refresh_settings.mjs
@@ -25,7 +25,7 @@ export function app_memorize_refresh_settings(context) {
   let { root } = context;
   html_clear_scroll_top(root);
   let save = app_memorize_save_get(context);
-  html_button_width_full_text_click_back(root, "back", async () => {
+  html_button_width_full_text_click_back(root, async () => {
     await app_memorize_refresh_memorize(context);
   });
   let book_code = app_memorize_book_code_get(context);
