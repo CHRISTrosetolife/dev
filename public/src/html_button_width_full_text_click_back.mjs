cbbba19a9b2c35b406053a1423f43f308c4a5125
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 11:42:34 -0400

    2024-09-10T15:42:33.732Z function_auto_after html_button_width_full_text_click_back

--- public/src/html_button_width_full_text_click_back.mjs
+++ public/src/html_button_width_full_text_click_back.mjs
@@ -1,7 +1,4 @@
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
-export function html_button_width_full_text_click_back(
-  menu_overlay,
-  back_on_click,
-) {
-  html_button_width_full_text_click(menu_overlay, "⬅️ back", back_on_click);
+export function html_button_width_full_text_click_back(parent, back_on_click) {
+  html_button_width_full_text_click(parent, "⬅️ back", back_on_click);
 }
