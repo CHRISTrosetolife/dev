9cf896d123094f19384a8a85b9a38c9c209cae1b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 13:46:17 -0400

    2024-09-10T17:46:16.364Z function_auto_after app_memorize_refresh_settings_chapter

--- public/src/app_memorize_refresh_settings_chapter.mjs
+++ public/src/app_memorize_refresh_settings_chapter.mjs
@@ -1,11 +1,11 @@
 import { app_memorize_refresh_memorize } from "./app_memorize_refresh_memorize.mjs";
 import { app_memorize_chapter_set } from "./app_memorize_chapter_set.mjs";
-import { log } from "./log.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { app_memorize_settings_button_back } from "./app_memorize_settings_button_back.mjs";
 import { app_record_book_generic } from "./app_record_book_generic.mjs";
-export function app_memorize_refresh_settings_chapter(context, root) {
+export function app_memorize_refresh_settings_chapter(context) {
+  let root = context;
   let { book_code } = context.save;
   app_record_book_generic(
     context,
@@ -22,7 +22,6 @@ export function app_memorize_refresh_settings_chapter(context, root) {
     },
     book_code,
     async (context, book_code, chapter) => {
-      log("here");
       app_memorize_chapter_set(context, chapter);
       await app_memorize_refresh_memorize(context);
     },
