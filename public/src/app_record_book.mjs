5608187f4b3113db9982861d0f63bdcdd926d20a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 13:53:00 -0400

    2024-09-10T17:52:59.888Z function_auto_after app_record_book

--- public/src/app_record_book.mjs
+++ public/src/app_record_book.mjs
@@ -1,8 +1,8 @@
+import { app_record_home_button } from "./app_record_home_button.mjs";
 import { app_record_book_generic } from "./app_record_book_generic.mjs";
-import { noop } from "./noop.mjs";
 import { app_record_chapter } from "./app_record_chapter.mjs";
 export function app_record_book(context, book_code) {
-  let lambda_top = noop;
+  let lambda_top = () => app_record_home_button(context);
   let app_record_book_on_click = app_record_chapter;
   app_record_book_generic(
     context,
