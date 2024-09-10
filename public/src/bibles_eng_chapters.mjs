366411551b8e47a605d121f9da20808613a056f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 18:00:50 -0400

    2024-09-10T22:00:50.428Z function_auto_after bibles_eng_chapters

--- public/src/bibles_eng_chapters.mjs
+++ public/src/bibles_eng_chapters.mjs
@@ -6,12 +6,13 @@ import { each_async } from "./each_async.mjs";
 export async function bibles_eng_chapters() {
   return await list_adder_async(async (la) => {
     let bible_folders = bible_eng_versions();
-    await each_async(bible_folders, async (bible_folder) => {
+    await each_async(bible_folders, lambda);
+    async function lambda(bible_folder) {
       let chapters = await bible_books_chapters(bible_folder);
       await each_async(chapters, async (c) => {
         let { chapter_code } = c;
         la(await bible_chapter(bible_folder, chapter_code));
       });
-    });
+    }
   });
 }
