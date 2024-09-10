2fe98b8ef4bb2d5f1516e47821d0574cdf778bff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 12:11:14 -0400

    2024-09-10T16:11:13.405Z function_auto_after app_memorize_refresh_settings

--- public/src/app_memorize_refresh_settings.mjs
+++ public/src/app_memorize_refresh_settings.mjs
@@ -40,7 +40,6 @@ export function app_memorize_refresh_settings(context) {
             book_code,
           });
           app_memorize_chapter_set(context, "1");
-          app_memorize_save(context);
         },
       );
     },
