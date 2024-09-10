d070c6c505ac2c4b16d3be2ac3984ee88862448b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 18:01:04 -0400

    2024-09-10T22:01:04.079Z function_auto_after bibles_eng_chapters

---
+++ public/src/bible_eng_versions_each.mjs
@@ -0,0 +1,6 @@
+import { each_async } from "./each_async.mjs";
+import { bible_eng_versions } from "./bible_eng_versions.mjs";
+export async function bible_eng_versions_each(lambda) {
+  let bible_folders = bible_eng_versions();
+  await each_async(bible_folders, lambda);
+}
--- public/src/bibles_eng_chapters.mjs
+++ public/src/bibles_eng_chapters.mjs
@@ -1,12 +1,11 @@
+import { bible_eng_versions_each } from "./bible_eng_versions_each.mjs";
 import { bible_books_chapters } from "./bible_books_chapters.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
-import { bible_eng_versions } from "./bible_eng_versions.mjs";
 import { bible_chapter } from "./bible_chapter.mjs";
 import { each_async } from "./each_async.mjs";
 export async function bibles_eng_chapters() {
   return await list_adder_async(async (la) => {
-    let bible_folders = bible_eng_versions();
-    await each_async(bible_folders, lambda);
+    await bible_eng_versions_each(lambda);
     async function lambda(bible_folder) {
       let chapters = await bible_books_chapters(bible_folder);
       await each_async(chapters, async (c) => {
