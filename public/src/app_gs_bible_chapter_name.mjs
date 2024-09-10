3416df596260907db4295e98c34566ef2c850d83
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-10 13:35:55 -0400

    2024-09-10T17:35:54.943Z function_auto_after app_gs_bible_chapter_name

--- public/src/app_gs_bible_chapter_name.mjs
+++ public/src/app_gs_bible_chapter_name.mjs
@@ -1,5 +1,9 @@
+import { number_pad } from "./number_pad.mjs";
 import { number_pad_2 } from "./number_pad_2.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_gs_bible_chapter_name(book_id, chapter) {
+  if (book_code === "PSA") {
+    chapter = number_pad();
+  }
   return string_combine_multiple([book_id, number_pad_2(chapter)]);
 }
