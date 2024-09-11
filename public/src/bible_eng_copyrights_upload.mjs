7a289064c12f0821602406175e46836c0200eb3a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 14:30:59 -0400

    2024-09-11T18:30:58.744Z function_auto_after bible_storage_path_copyrights

--- public/src/bible_eng_copyrights_upload.mjs
+++ public/src/bible_eng_copyrights_upload.mjs
@@ -1,8 +1,8 @@
+import { bible_storage_path_copyrights } from "./bible_storage_path_copyrights.mjs";
 import { bible_eng_copyrights } from "./bible_eng_copyrights.mjs";
 import { storage_upload_object } from "./storage_upload_object.mjs";
-import { bible_storage_path } from "./bible_storage_path.mjs";
 export async function bible_eng_copyrights_upload() {
   let copyrights = await bible_eng_copyrights();
-  let destination = bible_storage_path("copyrights");
+  let destination = bible_storage_path_copyrights();
   await storage_upload_object(copyrights, destination);
 }
---
+++ public/src/bible_storage_path_copyrights.mjs
@@ -0,0 +1,4 @@
+import { bible_storage_path } from "./bible_storage_path.mjs";
+export function bible_storage_path_copyrights() {
+  return bible_storage_path("copyrights");
+}
