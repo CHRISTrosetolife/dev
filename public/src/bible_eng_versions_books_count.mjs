d9035a02dd8563808905d27fd7ec5516ee7cf63e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 18:40:25 -0400

    2024-09-11T22:40:25.299Z function_auto_after bible_eng_versions_books_count

--- public/src/bible_eng_versions_books_count.mjs
+++ public/src/bible_eng_versions_books_count.mjs
@@ -2,11 +2,13 @@ import { bible_books } from "./bible_books.mjs";
 import { log } from "./log.mjs";
 import { bible_book_upload_all } from "./bible_book_upload_all.mjs";
 import { bible_eng_versions_each } from "./bible_eng_versions_each.mjs";
+import { list_size } from "./list_size.mjs";
 export async function bible_eng_versions_books_count() {
   await bible_eng_versions_each(async (version_code) => {
     let books = await bible_books(bible_folder);
+    let count = list_size(books);
     log({
-      c: books,
+      count,
     });
     if (version_code === "engbsb") {
       return;
