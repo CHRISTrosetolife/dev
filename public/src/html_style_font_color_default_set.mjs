41c5e4d54678d432f65760529ded2d6dbcde3dab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 12:22:13 -0400

    2024-09-11T16:22:11.354Z function_auto_after html_style_font_color_default_set

--- public/src/html_style_font_color_default_set.mjs
+++ public/src/html_style_font_color_default_set.mjs
@@ -1,5 +1,5 @@
 import { html_style_font_color_default } from "./html_style_font_color_default.mjs";
 import { html_style_font_color } from "./html_style_font_color.mjs";
-export function html_style_font_color_default_set(result) {
-  html_style_font_color(result, html_style_font_color_default());
+export function html_style_font_color_default_set(component) {
+  html_style_font_color(component, html_style_font_color_default());
 }
