c8886aa920e094baf45c205a027e1fbc7257b547
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 09:52:31 -0400

    2024-09-11T13:52:31.244Z function_auto_after bible_engbsb_storage_path

--- public/src/bible_engbsb_storage_path.mjs
+++ public/src/bible_engbsb_storage_path.mjs
@@ -1,4 +1,5 @@
+import { bible_storage_path } from "./bible_storage_path.mjs";
 import { string_combine } from "./string_combine.mjs";
 export function bible_engbsb_storage_path() {
-  return string_combine("bible/", "engbsb/");
+  return string_combine(bible_storage_path(), "engbsb/");
 }
---
+++ public/src/bible_storage_path.mjs
@@ -0,0 +1,3 @@
+export function bible_storage_path() {
+  return "bible/";
+}
