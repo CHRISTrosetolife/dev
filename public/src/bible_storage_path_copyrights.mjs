e0470eaa72a404d0c74d60fc98532c3907436976
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 14:51:28 -0400

    2024-09-11T18:51:27.928Z function_auto_after bible_storage_path_copyrights

--- public/src/bible_storage_path_copyrights.mjs
+++ public/src/bible_storage_path_copyrights.mjs
@@ -1,9 +1,7 @@
-import { bible_storage_path_version } from "./bible_storage_path_version.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { file_name_json } from "./file_name_json.mjs";
 import { bible_storage_path } from "./bible_storage_path.mjs";
 export function bible_storage_path_copyrights() {
-  let file_name = file_name_json(key);
-  return string_combine(bible_storage_path_version(version_code), file_name);
-  return bible_storage_path("copyrights");
+  let file_name = file_name_json("copyrights");
+  return string_combine(bible_storage_path(), file_name);
 }
