f90ad39bca7c85bd82cd578712842bdb4c30cb78
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 07:21:57 -0400

    2024-09-11T11:21:57.127Z function_auto_after html_parse_text

--- public/src/html_parse_text.mjs
+++ public/src/html_parse_text.mjs
@@ -2,9 +2,9 @@ import { list_join_empty } from "./list_join_empty.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { html_parse_visit } from "./html_parse_visit.mjs";
 import { list_adder } from "./list_adder.mjs";
-export function html_parse_text(m) {
+export function html_parse_text(element) {
   let parts = list_adder((la) =>
-    html_parse_visit(m, (v) => {
+    html_parse_visit(element, (v) => {
       let { node } = v;
       let { type } = node;
       if (type === "text") {
