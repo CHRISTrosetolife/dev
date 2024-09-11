04d5a47c1acb8a63e54eabf249725f6d92ac078a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 07:28:47 -0400

    2024-09-11T11:28:47.163Z function_auto_after bible_copyright

--- public/src/bible_copyright.mjs
+++ public/src/bible_copyright.mjs
@@ -51,7 +51,7 @@ export async function bible_copyright(bible_folder) {
   let filtered3 = list_filter(first_children, (c) =>
     list_any(texts, (t) => string_includes(html_parse_text(c), t)),
   );
-  each(filtered3, (f) => list_remove_multiple(f, first_children));
+  list_remove_multiple(first_children, filtered3);
   log({
     text: html_parse_text(first),
     first: html_parse_children(first),
