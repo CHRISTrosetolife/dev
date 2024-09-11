93d3410a13c082a044553afaf76c138af250b092
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 06:51:37 -0400

    2024-09-11T10:51:35.702Z function_auto_after string_date_is

--- public/src/string_date_is.mjs
+++ public/src/string_date_is.mjs
@@ -1,7 +1,9 @@
+import { log } from "./log.mjs";
 import { integer_parse_try } from "./integer_parse_try.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 import { list_all } from "./list_all.mjs";
 export function string_date_is(trimmed) {
-  let newLocal = string_split_comma(trimmed);
-  return list_all(newLocal, (s) => integer_parse_try(s) !== null);
+  let split = string_split_comma(trimmed);
+  log({});
+  return list_all(split, (s) => integer_parse_try(s) !== null);
 }
