3dfd8ac05c7c1e253ab6ac66c61940bf35200e1c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 15:46:28 -0400

    2024-09-11T19:46:27.045Z function_auto_after bible_eng_versions_upload

--- public/src/bible_eng_versions_upload.mjs
+++ public/src/bible_eng_versions_upload.mjs
@@ -1,1 +1,4 @@
-export function bible_eng_versions_upload() {}
+import { bible_eng_versions_each } from "./bible_eng_versions_each.mjs";
+export async function bible_eng_versions_upload() {
+  await bible_eng_versions_each();
+}
