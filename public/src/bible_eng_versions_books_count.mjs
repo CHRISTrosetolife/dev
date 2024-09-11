f18e35f7439ac735d79564e4bad99320c3fee2ff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 18:39:20 -0400

    2024-09-11T22:39:19.832Z function_auto_after bible_eng_versions_books_count

--- public/src/bible_eng_versions_books_count.mjs
+++ public/src/bible_eng_versions_books_count.mjs
@@ -1,1 +1,10 @@
-export function bible_eng_versions_books_count() {}
+import { bible_book_upload_all } from "./bible_book_upload_all.mjs";
+import { bible_eng_versions_each } from "./bible_eng_versions_each.mjs";
+export async function bible_eng_versions_books_count() {
+  await bible_eng_versions_each(async (version_code) => {
+    if (version_code === "engbsb") {
+      return;
+    }
+    await bible_book_upload_all(version_code);
+  });
+}
