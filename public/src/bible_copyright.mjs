b0d14e654b9ed1bb7ff7536284a6e7bc03b57a5e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 06:35:04 -0400

    2024-09-11T10:35:02.008Z function_auto_after bible_copyright

--- public/src/bible_copyright.mjs
+++ public/src/bible_copyright.mjs
@@ -1,6 +1,6 @@
+import { list_second } from "./list_second.mjs";
 import { list_size_2 } from "./list_size_2.mjs";
 import { assert } from "./assert.mjs";
-import { log } from "./log.mjs";
 import { html_parse_visit_class_list } from "./html_parse_visit_class_list.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { bible_url_htm } from "./bible_url_htm.mjs";
@@ -8,8 +8,6 @@ export async function bible_copyright(bible_folder) {
   let url = bible_url_htm(bible_folder, "copyright");
   let root = await html_cache_parse(url);
   let mains = html_parse_visit_class_list(root, "main");
-  assert(list_size_2);
-  log({
-    mains,
-  });
+  assert(list_size_2, [mains]);
+  let main = list_second(mains);
 }
