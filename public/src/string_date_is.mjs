a952d231b2ab8c62bcd5437900888142e52ce976
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 06:54:05 -0400

    2024-09-11T10:54:05.155Z function_auto_after string_date_is

--- public/src/string_date_is.mjs
+++ public/src/string_date_is.mjs
@@ -1,10 +1,10 @@
-import { string_split } from "./string_split.mjs";
+import { string_split_dash } from "./string_split_dash.mjs";
 import { list_size } from "./list_size.mjs";
 import { log } from "./log.mjs";
 import { integer_parse_try } from "./integer_parse_try.mjs";
 import { list_all } from "./list_all.mjs";
 export function string_date_is(trimmed) {
-  let split = string_split(trimmed, "-");
+  let split = string_split_dash(trimmed);
   log({
     split,
   });
---
+++ public/src/string_split_dash.mjs
@@ -0,0 +1,4 @@
+import { string_split } from "./string_split.mjs";
+export function string_split_dash(trimmed) {
+  return string_split(trimmed, "-");
+}
