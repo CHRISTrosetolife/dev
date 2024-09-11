3983c29bea448c1f87011402fb2196a212ccbaeb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 09:54:00 -0400

    2024-09-11T13:54:00.137Z function_auto_after storage_upload_object_bible_engbsb

--- public/src/storage_upload_object_bible_engbsb.mjs
+++ public/src/storage_upload_object_bible_engbsb.mjs
@@ -1,5 +1,5 @@
 import { bible_engbsb_storage_path_file } from "./bible_engbsb_storage_path_file.mjs";
-export async function storage_upload_object_bible_engbsb(chapter_name, data) {
-  let destination = bible_engbsb_storage_path_file(chapter_name);
+export async function storage_upload_object_bible_engbsb(key, data) {
+  let destination = bible_engbsb_storage_path_file(key);
   await storage_upload_object_bible_engbsb(data, destination);
 }
