bba3a7f2db1433b53928464623a9a9e4bc06b3cb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 07:08:14 -0400

    2024-09-11T11:08:13.581Z function_auto_after html_parse

--- public/src/html_parse.mjs
+++ public/src/html_parse.mjs
@@ -1,4 +1,4 @@
 import { html_parse_parsed } from "./html_parse_parsed.mjs";
 export async function html_parse(input) {
-  return await html_parse_parsed(input);
+  let root = await html_parse_parsed(input);
 }
