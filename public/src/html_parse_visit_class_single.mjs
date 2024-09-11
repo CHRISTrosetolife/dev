3d8f9599434bc79f738201347b4b741b797bb078
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 07:01:04 -0400

    2024-09-11T11:01:04.422Z function_auto_after html_parse_visit_class_single

--- public/src/html_parse_visit_class_single.mjs
+++ public/src/html_parse_visit_class_single.mjs
@@ -1,8 +1,9 @@
+import { list_size } from "./list_size.mjs";
 import { log } from "./log.mjs";
 import { html_parse_visit_class_list } from "./html_parse_visit_class_list.mjs";
 import { list_single } from "./list_single.mjs";
 export function html_parse_visit_class_single(root_bible, class_name) {
   let list = html_parse_visit_class_list(root_bible, class_name);
-  log({});
+  log(list_size(list));
   return list_single(list);
 }
