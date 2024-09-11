741f37a11ace47a93317c77f0d3fc34523c04737
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 07:00:52 -0400

    2024-09-11T11:00:51.734Z function_auto_after html_parse_visit_class_single

--- public/src/html_parse_visit_class_single.mjs
+++ public/src/html_parse_visit_class_single.mjs
@@ -1,5 +1,6 @@
 import { html_parse_visit_class_list } from "./html_parse_visit_class_list.mjs";
 import { list_single } from "./list_single.mjs";
 export function html_parse_visit_class_single(root_bible, class_name) {
-  return list_single(html_parse_visit_class_list(root_bible, class_name));
+  let list = html_parse_visit_class_list(root_bible, class_name);
+  return list_single(list);
 }
