786eb6fcf4e861ee3d9127f0fbc7b719ddd19542
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 14:32:08 -0400

    2024-09-11T18:32:07.542Z function_auto_after bible_storage_version_http_get

--- public/src/bible_storage_version_http_get.mjs
+++ public/src/bible_storage_version_http_get.mjs
@@ -1,11 +1,10 @@
-import { storage_url } from "./storage_url.mjs";
-import { http_get } from "./http_get.mjs";
+import { http_storage_get } from "./http_storage_get.mjs";
 import { bible_storage_path_file_version } from "./bible_storage_path_file_version.mjs";
 export async function bible_storage_version_http_get(
   chapter_code,
   version_code,
 ) {
   let file_path = bible_storage_path_file_version(chapter_code, version_code);
-  let verses = await http_get(storage_url(file_path));
+  let verses = await http_storage_get(file_path);
   return verses;
 }
---
+++ public/src/http_storage_get.mjs
@@ -0,0 +1,5 @@
+import { storage_url } from "./storage_url.mjs";
+import { http_get } from "./http_get.mjs";
+export async function http_storage_get(file_path) {
+  return await http_get(storage_url(file_path));
+}
