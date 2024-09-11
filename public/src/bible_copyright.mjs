b002eed6d55621a7f8f5b275e94b45db1d6a6bb8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 06:42:16 -0400

    2024-09-11T10:42:15.683Z function_auto_after bible_copyright

--- public/src/bible_copyright.mjs
+++ public/src/bible_copyright.mjs
@@ -1,3 +1,4 @@
+import { list_remove } from "./list_remove.mjs";
 import { list_copy } from "./list_copy.mjs";
 import { html_parse_tag } from "./html_parse_tag.mjs";
 import { list_filter } from "./list_filter.mjs";
@@ -22,7 +23,9 @@ export async function bible_copyright(bible_folder) {
   let filtered = list_copy(children);
   each(["h1", "h2", "div", "ul"], (tag) => {
     let tags = list_filter(children, (c) => html_parse_tag(c, tag));
-    each(tags, (tag) => {});
+    each(tags, (tag) => {
+      list_remove(filtered, tag);
+    });
   });
   return children;
 }
