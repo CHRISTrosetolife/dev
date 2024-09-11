b74e6f9bee1c16434c4cbd879851208edb555583
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 14:23:18 -0400

    2024-09-11T18:23:18.090Z function_auto_after bible_engbsb_storage_path

--- public/src/bible_engbsb_storage_path.mjs
+++ public/src/bible_engbsb_storage_path.mjs
@@ -1,5 +1,5 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { bible_storage_path } from "./bible_storage_path.mjs";
-import { string_combine } from "./string_combine.mjs";
 export function bible_engbsb_storage_path() {
-  return string_combine(bible_storage_path(), "engbsb/");
+  return string_combine_multiple([bible_storage_path(), "engbsb/"]);
 }
