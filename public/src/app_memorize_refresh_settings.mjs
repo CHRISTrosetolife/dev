3c14049a2e28337a0f3b4bba69d261a5546ed320
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 14:35:35 -0400

    2024-09-11T18:35:34.726Z function_auto_after app_memorize_refresh_settings

--- public/src/app_memorize_refresh_settings.mjs
+++ public/src/app_memorize_refresh_settings.mjs
@@ -53,7 +53,9 @@ export function app_memorize_refresh_settings(context) {
           let destination = bible_storage_path_copyrights();
           let copyrights = await http_storage_get(destination);
           let { copyright } = object_property_get(copyrights, version_code);
-          await app_memorize_refresh_memorize_load(context);
+          if (string_includes_not()) {
+            await app_memorize_refresh_memorize_load(context);
+          }
         });
       });
     },
