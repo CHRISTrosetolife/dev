804c2728a5fd9b9b65b7d02fdf99f44fb1a9ab91
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 10:05:19 -0400

    2024-09-11T14:05:18.048Z function_auto_after app_memorize_update_colors

--- public/src/app_memorize_update_colors.mjs
+++ public/src/app_memorize_update_colors.mjs
@@ -1,10 +1,12 @@
+import { app_memorize_save_get } from "./app_memorize_save_get.mjs";
 import { html_style_hidden } from "./html_style_hidden.mjs";
 import { undefined_not_is } from "./undefined_not_is.mjs";
 import { html_style_visible } from "./html_style_visible.mjs";
 import { html_scroll_center } from "./html_scroll_center.mjs";
 import { list_get } from "./list_get.mjs";
 export function app_memorize_update_colors(context) {
-  let current_verse = list_get(context.verse_elements, context.verse_index);
+  app_memorize_save_get();
+  let current_verse = list_get(context.verse_elements, save.verse_index);
   let { verse_element, token_objects } = current_verse;
   let current_token = list_get(token_objects, context.token_index);
   html_scroll_center(verse_element);
