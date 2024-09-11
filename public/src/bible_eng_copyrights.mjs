bfe4db906907f795a1254a035a53beb6d043b7a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 09:57:10 -0400

    2024-09-11T13:57:10.271Z function_auto_after bible_eng_copyrights_upload

---
+++ public/src/bible_eng_copyrights.mjs
@@ -0,0 +1,12 @@
+import { object_property_set } from "./object_property_set.mjs";
+import { bible_copyright } from "./bible_copyright.mjs";
+import { bible_eng_versions_each } from "./bible_eng_versions_each.mjs";
+export async function bible_eng_copyrights() {
+  let copyrights = {};
+  await bible_eng_versions_each(async function lambda(bible_folder) {
+    let c = await bible_copyright(bible_folder);
+    object_property_set(copyrights, bible_folder, c);
+    return c;
+  });
+  return copyrights;
+}
--- public/src/bible_eng_copyrights_upload.mjs
+++ public/src/bible_eng_copyrights_upload.mjs
@@ -1,15 +1,8 @@
+import { bible_eng_copyrights } from "./bible_eng_copyrights.mjs";
 import { storage_upload_object } from "./storage_upload_object.mjs";
 import { bible_storage_path } from "./bible_storage_path.mjs";
-import { bible_eng_versions_each } from "./bible_eng_versions_each.mjs";
-import { bible_copyright } from "./bible_copyright.mjs";
-import { object_property_set } from "./object_property_set.mjs";
 export async function bible_eng_copyrights_upload() {
-  let copyrights = {};
-  await bible_eng_versions_each(async function lambda(bible_folder) {
-    let c = await bible_copyright(bible_folder);
-    object_property_set(copyrights, bible_folder, c);
-    return c;
-  });
+  let copyrights = await bible_eng_copyrights();
   let destination = bible_storage_path("copyrights");
   await storage_upload_object(copyrights, destination);
 }
