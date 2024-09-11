de9f5b28633096117af87e582ffee13435b0bc92
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 12:15:56 -0400

    2024-09-11T16:15:55.442Z function_auto_after html_style_font_color_gray

--- public/src/html_button_disable.mjs
+++ public/src/html_button_disable.mjs
@@ -1,10 +1,10 @@
+import { html_style_font_color_gray } from "./html_style_font_color_gray.mjs";
 import { html_style_border_color } from "./html_style_border_color.mjs";
 import { html_style_background_color } from "./html_style_background_color.mjs";
-import { html_style_font_color } from "./html_style_font_color.mjs";
 import { html_disable } from "./html_disable.mjs";
 export function html_button_disable(button) {
   html_disable(button);
-  html_style_font_color(button, "gray");
+  html_style_font_color_gray(button);
   html_style_background_color(button, "#d8ecf3");
   html_style_border_color(button, "lightblue");
 }
---
+++ public/src/html_style_font_color_gray.mjs
@@ -0,0 +1,4 @@
+import { html_style_font_color } from "./html_style_font_color.mjs";
+export function html_style_font_color_gray(button) {
+  html_style_font_color(button, "gray");
+}
