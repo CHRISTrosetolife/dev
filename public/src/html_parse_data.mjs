cab889b100b8ffe4983f9d1909571cf2d15fec5f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 07:30:57 -0400

    2024-09-11T11:30:57.326Z function_auto_after html_parse_text

---
+++ public/src/html_parse_data.mjs
@@ -0,0 +1,7 @@
+import { object_property_get } from "./object_property_get.mjs";
+export function html_parse_data(node) {
+  let { type } = node;
+  if (type === "text") {
+    let d = object_property_get(node, "data");
+  }
+}
--- public/src/html_parse_text.mjs
+++ public/src/html_parse_text.mjs
@@ -1,16 +1,12 @@
+import { html_parse_data } from "./html_parse_data.mjs";
 import { list_join_empty } from "./list_join_empty.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { html_parse_visit } from "./html_parse_visit.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function html_parse_text(element) {
   let parts = list_adder((la) =>
     html_parse_visit(element, (v) => {
       let { node } = v;
-      let { type } = node;
-      if (type === "text") {
-        let d = object_property_get(node, "data");
-        la(d);
-      }
+      html_parse_data(node, la);
     }),
   );
   return list_join_empty(parts);
