204786f1d134a32d2aedc184635301fe1ee6616d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 10:01:44 -0400

    2024-09-11T14:01:43.494Z function_auto_after app_memorize_on_keydown

--- public/src/app_memorize_on_keydown.mjs
+++ public/src/app_memorize_on_keydown.mjs
@@ -57,7 +57,7 @@ export async function app_memorize_on_keydown(context, key) {
     let tokens_length = list_size(tokens);
     if (greater_than_equal(context.token_index, tokens_length)) {
       save.verse_index++;
-      save.token_index = 0;
+      context.token_index = 0;
       app_memorize_save(context);
     }
     let group_current_length = list_size(context.save.group_current);
