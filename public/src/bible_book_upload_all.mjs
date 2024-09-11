66d0e96e8cca88648d4fd6bfd6d8b6f582bb91c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 15:45:55 -0400

    2024-09-11T19:45:54.994Z function_auto_after bible_engbsb_book_upload_all

---
+++ public/src/bible_book_upload_all.mjs
@@ -0,0 +1,12 @@
+import { bible_engbsb_book_upload } from "./bible_engbsb_book_upload.mjs";
+import { log } from "./log.mjs";
+import { bible_books } from "./bible_books.mjs";
+import { each_async } from "./each_async.mjs";
+export async function bible_book_upload_all(version_code) {
+  await each_async(await bible_books(version_code), async (item) => {
+    log({
+      item,
+    });
+    await bible_engbsb_book_upload(item);
+  });
+}
--- public/src/bible_engbsb_book_upload_all.mjs
+++ public/src/bible_engbsb_book_upload_all.mjs
@@ -1,13 +1,5 @@
-import { log } from "./log.mjs";
-import { bible_books } from "./bible_books.mjs";
-import { bible_engbsb_book_upload } from "./bible_engbsb_book_upload.mjs";
-import { each_async } from "./each_async.mjs";
+import { bible_book_upload_all } from "./bible_book_upload_all.mjs";
 export async function bible_engbsb_book_upload_all() {
   let version_code = "engbsb";
-  await each_async(await bible_books(version_code), async (item) => {
-    log({
-      item,
-    });
-    await bible_engbsb_book_upload(item);
-  });
+  await bible_book_upload_all(version_code);
 }
