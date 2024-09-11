51bc326716b1a6f1cb2c47b3cff2cfb311d6d3c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 09:59:53 -0400

    2024-09-11T13:59:53.236Z function_auto_after app_memorize_on_keydown

--- public/src/app_memorize_on_keydown.mjs
+++ public/src/app_memorize_on_keydown.mjs
@@ -22,7 +22,7 @@ import { string_case_lower } from "./string_case_lower.mjs";
 import { list_get } from "./list_get.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
 export async function app_memorize_on_keydown(context, key) {
-  let j = list_get(context.save.group_current, context.verse_index);
+  let j = list_get(context.save.group_current, save.verse_index);
   let current_verse = list_get(context.verses, j);
   let { tokens } = current_verse;
   let current_token = list_get(tokens, context.token_index);
