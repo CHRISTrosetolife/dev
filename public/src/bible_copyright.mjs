040ee0e05f524b805d332f676dab4f1c67b43ffd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 07:51:19 -0400

    2024-09-11T11:51:18.594Z function_auto_after bible_copyright

--- public/src/bible_copyright.mjs
+++ public/src/bible_copyright.mjs
@@ -12,7 +12,6 @@ import { list_map } from "./list_map.mjs";
 import { string_empty_not_is } from "./string_empty_not_is.mjs";
 import { html_parse_text } from "./html_parse_text.mjs";
 import { list_remove } from "./list_remove.mjs";
-import { list_copy } from "./list_copy.mjs";
 import { html_parse_tag } from "./html_parse_tag.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { each } from "./each.mjs";
@@ -35,7 +34,6 @@ export async function bible_copyright(bible_folder) {
   let name = html_parse_visit_tag_single(main, "h1");
   let title = html_parse_visit_tag_single(main, "h2");
   let children = html_parse_children(main);
-  let filtered = list_copy(children);
   each(["h1", "h2", "div"], (tag) => {
     let tags = list_filter(children, (c) => html_parse_tag(c, tag));
     html_remove_multiple(tags);
