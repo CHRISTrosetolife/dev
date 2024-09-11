ecd9d0401cdeec8f848ef8f1ccdfba00ea5004b3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 07:32:03 -0400

    2024-09-11T11:32:03.313Z function_auto_after html_parse_text

--- public/src/html_parse_text.mjs
+++ public/src/html_parse_text.mjs
@@ -3,13 +3,13 @@ import { list_join_empty } from "./list_join_empty.mjs";
 import { html_parse_visit } from "./html_parse_visit.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function html_parse_text(element) {
-  html_parse_data(element);
+  let d = html_parse_data(element);
   let parts = list_adder((la) =>
     html_parse_visit(element, (v) => {
       let { node } = v;
-      let d = html_parse_data(node);
-      if (d !== null) {
-        la(d);
+      let d2 = html_parse_data(node);
+      if (d2 !== null) {
+        la(d2);
       }
     }),
   );
