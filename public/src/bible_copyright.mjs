5d9a6ffafacf9d549006b85179b5ab437cce9c56
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 09:14:59 -0400

    2024-09-11T13:14:58.579Z function_auto_after bible_copyright

--- public/src/bible_copyright.mjs
+++ public/src/bible_copyright.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { html_parse_visit_tag_list } from "./html_parse_visit_tag_list.mjs";
 import { list_remove_multiple } from "./list_remove_multiple.mjs";
 import { list_any } from "./list_any.mjs";
@@ -29,9 +28,7 @@ export async function bible_copyright(bible_folder) {
   assert(list_size_2, [mains]);
   let main = list_second(mains);
   let { attribs } = main;
-  log({
-    main,
-  });
+  object_property_all_delete(attribs);
   let name = html_parse_visit_tag_single(main, "h1");
   let h2s = html_parse_visit_tag_list(main, "h2");
   let children = html_parse_children(main);
