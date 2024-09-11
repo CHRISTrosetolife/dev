54e76153d40ed873f4a55b0f79eab0aba964bd0c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 06:53:41 -0400

    2024-09-11T10:53:40.976Z function_auto_after string_date_is

--- public/src/string_date_is.mjs
+++ public/src/string_date_is.mjs
@@ -1,9 +1,10 @@
+import { string_split } from "./string_split.mjs";
 import { list_size } from "./list_size.mjs";
 import { log } from "./log.mjs";
 import { integer_parse_try } from "./integer_parse_try.mjs";
 import { list_all } from "./list_all.mjs";
 export function string_date_is(trimmed) {
-  let split = string_split_hyphen(trimmed);
+  let split = string_split(trimmed, "-");
   log({
     split,
   });
