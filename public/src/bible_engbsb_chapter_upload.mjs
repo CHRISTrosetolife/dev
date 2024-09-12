be497669f7be5907d249d905f605c064fc87b2c0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-12 17:51:15 -0400

    2024-09-12T21:51:15.025Z de bible_engbsb_chapter_upload

--- public/src/bible_engbsb_chapter_upload.mjs
+++
@@ -1,5 +0,0 @@
-import { bible_chapter_upload } from "./bible_chapter_upload.mjs";
-export async function bible_engbsb_chapter_upload(chapter_name) {
-  let bible_folder = "engbsb";
-  await bible_chapter_upload(bible_folder, chapter_name);
-}
