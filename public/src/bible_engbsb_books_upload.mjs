6ddb878f24b431a1677a2debf6cc20372f075c4f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-12 17:50:50 -0400

    2024-09-12T21:50:49.490Z de bible_engbsb_books_upload

--- public/src/bible_engbsb_books_upload.mjs
+++
@@ -1,7 +0,0 @@
-import { bible_books_upload } from "./bible_books_upload.mjs";
-import { assert_arguments_length } from "./assert_arguments_length.mjs";
-export async function bible_engbsb_books_upload() {
-  assert_arguments_length(arguments, 0);
-  let bible_folder = "engbsb";
-  await bible_books_upload(bible_folder);
-}
