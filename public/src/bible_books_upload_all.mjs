a50a892916b9a21ca72653ad2d25414c746176dc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-12 17:54:43 -0400

    2024-09-12T21:54:43.189Z function_auto_after bible_books_upload_all

--- public/src/bible_books_upload_all.mjs
+++ public/src/bible_books_upload_all.mjs
@@ -1,7 +1,7 @@
 import { bible_books_upload } from "./bible_books_upload.mjs";
 import { bible_books_each } from "./bible_books_each.mjs";
 import { list_includes } from "./list_includes.mjs";
-export async function bible_books_upload_all(bible_folder) {
+export async function bible_books_upload_all() {
   await bible_books_each(bible_folder, lambda);
   async function lambda(book_name) {
     if (list_includes(["engbsb", "engBBE"])) {
