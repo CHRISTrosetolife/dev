51b72d7ea3805ffb38ab54a4b3d7c693507be05d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-12 17:39:09 -0400

    2024-09-12T21:39:09.228Z function_auto_after app_memorize_refresh_memorize_load

--- public/src/app_memorize_refresh_memorize_load.mjs
+++ public/src/app_memorize_refresh_memorize_load.mjs
@@ -29,7 +29,8 @@ export async function app_memorize_refresh_memorize_load(context) {
   });
   let book = list_find_property_or(books, "book_code", null);
   if (book === null) {
-    book_code = object_property_get(list_first(books), "book_code");
+    let f = list_first(books);
+    book_code = object_property_get(f, "book_code");
   }
   app_memorize_save(context);
   let load = true;
