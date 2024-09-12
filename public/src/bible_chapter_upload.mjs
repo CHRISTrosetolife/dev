01ca83550d262d47d7a5211ad2ee24dcf1b4a9d6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-11 21:36:59 -0400

    2024-09-12T01:36:59.142Z function_auto_after bible_chapter_upload

--- public/src/bible_chapter_upload.mjs
+++ public/src/bible_chapter_upload.mjs
@@ -3,6 +3,6 @@ import { bible_storage_path_file_version } from "./bible_storage_path_file_versi
 import { bible_chapter } from "./bible_chapter.mjs";
 export async function bible_chapter_upload(bible_folder, chapter_name) {
   let data = await bible_chapter(bible_folder, chapter_name);
-  let destination = bible_storage_path_file_version(key, bible_folder);
+  let destination = bible_storage_path_file_version(chapter_name, bible_folder);
   await storage_upload_object(data, destination);
 }
