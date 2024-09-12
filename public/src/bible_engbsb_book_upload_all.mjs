628a24ea7909de8082213613a115cfbfa0afb54d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-12 17:51:36 -0400

    2024-09-12T21:51:35.463Z de bible_engbsb_book_upload_all

--- public/src/bible_engbsb_book_upload_all.mjs
+++
@@ -1,5 +0,0 @@
-import { bible_book_upload_all } from "./bible_book_upload_all.mjs";
-export async function bible_engbsb_book_upload_all() {
-  let version_code = "engbsb";
-  await bible_book_upload_all(version_code);
-}
