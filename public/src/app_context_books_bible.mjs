d99d3170392358528f4daab4225c9fe12a0bbe9f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-12 17:33:10 -0400

    2024-09-12T21:33:09.570Z function_auto_after app_context_books_bible

--- public/src/app_context_books_bible.mjs
+++ public/src/app_context_books_bible.mjs
@@ -1,7 +1,8 @@
+import { app_memorize_version_code_get } from "./app_memorize_version_code_get.mjs";
 import { bible_storage_version_http_get } from "./bible_storage_version_http_get.mjs";
 import { object_merge } from "./object_merge.mjs";
 export async function app_context_books_bible(context) {
-  let version_code = "engbsb";
+  let version_code = app_memorize_version_code_get(context);
   let { books } = await bible_storage_version_http_get("books", version_code);
   object_merge(context, {
     books,
