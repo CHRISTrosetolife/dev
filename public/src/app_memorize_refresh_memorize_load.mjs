3e4b859b689f0c4b652d695ea4ab9f5efc73465c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-12 17:36:37 -0400

    2024-09-12T21:36:36.360Z function_auto_after app_memorize_refresh_memorize_load

--- public/src/app_memorize_refresh_memorize_load.mjs
+++ public/src/app_memorize_refresh_memorize_load.mjs
@@ -23,7 +23,9 @@ export async function app_memorize_refresh_memorize_load(context) {
   let book_code = object_property_initialize(save, "book_code", "JHN");
   let chapter = object_property_initialize(save, "chapter", "19");
   let books = object_property_get(context, "books");
-  log({});
+  log({
+    books,
+  });
   app_memorize_save(context);
   let load = true;
   let chapter_code = app_gs_bible_chapter_name(book_code, chapter);
