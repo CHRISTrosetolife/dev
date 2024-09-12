1c137ac3c0fc0b8b93c0794d1089f2e3834117f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-12 18:00:35 -0400

    2024-09-12T22:00:34.966Z function_auto_after ceb_bible_words_5

--- public/src/ceb_bible_words_5.mjs
+++ public/src/ceb_bible_words_5.mjs
@@ -1,9 +1,8 @@
+import { bible_chapters_each } from "./bible_chapters_each.mjs";
 import { bible_books_index_old_is } from "./bible_books_index_old_is.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { log } from "./log.mjs";
 import { bible_ceb_chapter } from "./bible_ceb_chapter.mjs";
-import { each_async } from "./each_async.mjs";
-import { bible_chapters } from "./bible_chapters.mjs";
 import { bible_books } from "./bible_books.mjs";
 import { list_adder_unique_async } from "./list_adder_unique_async.mjs";
 import { each } from "./each.mjs";
@@ -21,8 +20,7 @@ export async function ceb_bible_words_5(args) {
         log({
           book_name,
         });
-        let chapters = await bible_chapters("engbsb", book_name);
-        await each_async(chapters, async (chapter_name) => {
+        await bible_chapters_each("engbsb", book_name, async (chapter_name) => {
           let { ceb } = await bible_ceb_chapter(chapter_name);
           each(ceb, (v) =>
             each(v.tokens, (t) => {
