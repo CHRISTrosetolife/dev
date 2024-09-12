fb54a78e4c0756c1e581a8f22ae8443868ac6f04
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-12 17:42:15 -0400

    2024-09-12T21:42:11.182Z function_auto_after list_find_property_or

--- public/src/list_find_property_or.mjs
+++ public/src/list_find_property_or.mjs
@@ -1,5 +1,7 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { list_find_property_or_generic } from "./list_find_property_or_generic.mjs";
 export function list_find_property_or(list, property_name, value, or_value) {
+  assert_arguments_length(arguments, 4);
   return list_find_property_or_generic(
     list,
     property_name,
