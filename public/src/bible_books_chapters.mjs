8355a8f50099650c38f2961041041384579b3546
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-12 17:59:36 -0400

    2024-09-12T21:59:34.657Z function_auto_after bible_books_chapters

--- public/src/bible_books_chapters.mjs
+++ public/src/bible_books_chapters.mjs
@@ -4,13 +4,16 @@ import { list_adder_async } from "./list_adder_async.mjs";
 export async function bible_books_chapters(bible_folder) {
   return await list_adder_async(async (la) => {
     await bible_books_each(async (book_code) => {
-      function lambda(chapter_code) {
-        la({
-          book_code,
-          chapter_code,
-        });
-      }
-      await bible_chapters_each(bible_folder, book_code, lambda);
+      await bible_chapters_each(
+        bible_folder,
+        book_code,
+        function lambda(chapter_code) {
+          la({
+            book_code,
+            chapter_code,
+          });
+        },
+      );
     });
   });
 }
