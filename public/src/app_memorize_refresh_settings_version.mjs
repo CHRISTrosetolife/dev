36af8294c053c4aef511d7da1063b0dd62d83bea
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-13 11:34:20 -0400

    2024-09-13T15:34:19.578Z function_auto_after app_memorize_refresh_settings

---
+++ public/src/app_memorize_refresh_settings_version.mjs
@@ -0,0 +1,21 @@
+import { app_memorize_save_get } from "./app_memorize_save_get.mjs";
+import { app_context_books_bible } from "./app_context_books_bible.mjs";
+import { app_memorize_save } from "./app_memorize_save.mjs";
+import { object_property_delete } from "./object_property_delete.mjs";
+import { object_merge } from "./object_merge.mjs";
+import { app_memorize_refresh_settings } from "./app_memorize_refresh_settings.mjs";
+import { app_memorize_refresh_memorize_load } from "./app_memorize_refresh_memorize_load.mjs";
+export async function app_memorize_refresh_settings_version(
+  context,
+  version_code,
+) {
+  let save = app_memorize_save_get(context);
+  object_merge(save, {
+    version_code,
+  });
+  object_property_delete(context, "chapter_code");
+  app_memorize_save(context);
+  await app_context_books_bible(context);
+  await app_memorize_refresh_memorize_load(context);
+  await app_memorize_refresh_settings(context);
+}
