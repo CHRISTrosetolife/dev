27709978962663a3fc9f01eeb9594e63b35c377f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-13 11:52:15 -0400

    2024-09-13T15:52:14.823Z function_auto_after object_property_get_or

--- public/src/object_property_get_or.mjs
+++ public/src/object_property_get_or.mjs
@@ -1,11 +1,11 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
-export function object_property_get_or(n, property_name, default_value) {
+export function object_property_get_or(o, property_name, default_value) {
   let result;
-  if (object_property_exists_not(n, property_name)) {
+  if (object_property_exists_not(o, property_name)) {
     result = default_value;
   } else {
-    result = object_property_get(n, property_name);
+    result = object_property_get(o, property_name);
   }
   return result;
 }
