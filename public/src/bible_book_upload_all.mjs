14b810d3621ff1b0ef064f390855151014960c86
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-13 16:53:35 -0400

    2024-09-13T20:53:35.248Z function_auto_after bible_book_upload_all

--- public/src/bible_book_upload_all.mjs
+++ public/src/bible_book_upload_all.mjs
@@ -6,7 +6,7 @@ export async function bible_book_upload_all(bible_folder) {
   let books = await bible_books(bible_folder);
   await bible_books_each(bible_folder, lambda);
   async function lambda(book_name) {
-    if (list_index_before(book_name, "1CH")) {
+    if (list_index_before(books, book_name, "1CH")) {
       return;
     }
     await bible_book_upload(bible_folder, book_name);
