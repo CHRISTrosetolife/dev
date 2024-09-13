6a51bb7d6984cec57a7bd5e18532687a60953ddb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-13 17:04:56 -0400

    2024-09-13T21:04:56.108Z function_auto_after app_memorize_refresh_memorize_load

--- public/src/app_memorize_refresh_memorize_load.mjs
+++ public/src/app_memorize_refresh_memorize_load.mjs
@@ -37,7 +37,7 @@ export async function app_memorize_refresh_memorize_load(context) {
   );
   book_code = object_property_get(book, "book_code");
   object_merge(context, {
-    books,
+    book_code,
   });
   let chapters = object_property_get(book, "chapters");
   if (list_includes_not(chapters, chapter)) {
