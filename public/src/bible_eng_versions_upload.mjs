f597dcd51143c9ac6db1428c2f237b2d8103d3c3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-12 20:34:16 -0400

    2024-09-13T00:34:16.011Z function_auto_after bible_eng_versions_upload

--- public/src/bible_eng_versions_upload.mjs
+++ public/src/bible_eng_versions_upload.mjs
@@ -5,9 +5,10 @@ import { bible_eng_versions_each } from "./bible_eng_versions_each.mjs";
 export async function bible_eng_versions_upload() {
   let bible_folders = bible_eng_versions();
   await bible_eng_versions_each(async (bible_folder) => {
+    let target = "eng-rv";
     if (
       list_index(bible_folders, bible_folder) <
-      list_index(bible_folders, "eng-rv")
+      list_index(bible_folders, target)
     ) {
       return;
     }
