7106a94d656bdebfde18ffc437abb561070a6e79
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-13 11:27:11 -0400

    2024-09-13T15:27:08.374Z function_auto_after app_memorize_refresh_memorize_load

--- public/src/app_memorize_refresh_memorize_load.mjs
+++ public/src/app_memorize_refresh_memorize_load.mjs
@@ -79,7 +79,9 @@ export async function app_memorize_refresh_memorize_load(context) {
     });
     context.button_height = 7;
     let group_first = list_first(context.groups);
-    log({});
+    log({
+      group_first,
+    });
     let group = object_property_get_or(save, "group_current", group_first);
     log({
       groups,
