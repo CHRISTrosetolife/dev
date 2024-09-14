4740ad672046c5e023167d12846a5b26fe7b28ae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 11:26:45 -0400

    2024-09-14T15:26:44.933Z function_auto_after bible_versions_list_english

--- public/src/bible_versions_list_english.mjs
+++ public/src/bible_versions_list_english.mjs
@@ -1,11 +1,13 @@
+import { list_unique } from "./list_unique.mjs";
 import { string_starts_with_curry } from "./string_starts_with_curry.mjs";
 import { html_cache_parse_hrefs } from "./html_cache_parse_hrefs.mjs";
 import { bible_ebible_url_combine } from "./bible_ebible_url_combine.mjs";
 export async function bible_versions_list_english() {
   let url = bible_ebible_url_combine("download.php");
-  let book_hrefs = await html_cache_parse_hrefs(
+  let hrefs = await html_cache_parse_hrefs(
     url,
     string_starts_with_curry("details.php?id=eng"),
   );
-  return book_hrefs;
+  let u = list_unique(hrefs);
+  return hrefs;
 }
