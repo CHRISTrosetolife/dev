9b13ad56995f8322fd62868215512e8ed53fb661
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-13 20:05:02 -0400

    2024-09-14T00:05:01.410Z function_auto_after html_style_success_if

--- public/src/html_style_success_if.mjs
+++ public/src/html_style_success_if.mjs
@@ -1,6 +1,6 @@
 import { html_style_success } from "./html_style_success.mjs";
-export function html_style_success_if(version_code, actual, b) {
-  if (version_code === actual) {
+export function html_style_success_if(actual, expected, b) {
+  if (actual === expected) {
     html_style_success(b);
   }
 }
