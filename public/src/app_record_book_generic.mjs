cc3e2abb01b7841e7f2af9cedaf26b176154a0c2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-13 20:09:00 -0400

    2024-09-14T00:08:59.639Z function_auto_after app_record_book_generic

--- public/src/app_record_book_generic.mjs
+++ public/src/app_record_book_generic.mjs
@@ -1,6 +1,6 @@
+import { list_find_property } from "./list_find_property.mjs";
 import { html_button_text_click } from "./html_button_text_click.mjs";
 import { each } from "./each.mjs";
-import { list_find_property_or } from "./list_find_property_or.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 export function app_record_book_generic(
   context,
@@ -12,7 +12,7 @@ export function app_record_book_generic(
   html_clear_scroll_top(root);
   lambda_top();
   let { books } = context;
-  let book = list_find_property_or(books, "book_code", book_code);
+  let book = list_find_property(books, "book_code", book_code);
   each(book.chapters, (chapter) => {
     html_button_text_click(root, chapter, async () => {
       await app_record_book_on_click(context, book_code, chapter);
