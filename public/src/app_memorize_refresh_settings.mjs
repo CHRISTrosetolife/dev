bf33507ea667e8a958c6e1b54863db758b5c0700
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-13 20:11:44 -0400

    2024-09-14T00:11:43.863Z function_auto_after app_memorize_refresh_settings

--- public/src/app_memorize_refresh_settings.mjs
+++ public/src/app_memorize_refresh_settings.mjs
@@ -155,7 +155,7 @@ export async function app_memorize_refresh_settings(context) {
           app_memorize_group_current_set(context, g, true);
           await app_memorize_refresh_settings(context);
         });
-        html_style_success_if(b, chapter_selected, chapter);
+        html_style_success_if(b, save.group_current, g);
       }
     },
   );
