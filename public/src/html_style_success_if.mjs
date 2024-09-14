b89f8e007d978b39919eccaec4d6027c906211ac
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-13 20:12:21 -0400

    2024-09-14T00:12:20.093Z function_auto_after html_style_success_if

--- public/src/html_style_success_if.mjs
+++ public/src/html_style_success_if.mjs
@@ -1,6 +1,7 @@
+import { equal_json } from "./equal_json.mjs";
 import { html_style_success } from "./html_style_success.mjs";
 export function html_style_success_if(button, expected, actual) {
-  if (actual === expected) {
+  if (equal_json(actual, expected)) {
     html_style_success(button);
   }
 }
