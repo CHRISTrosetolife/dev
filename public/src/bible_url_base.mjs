3d35b20dbf60e56f62fdf619eca00d80435e5c6b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 09:46:16 -0400

    2024-09-14T13:46:16.013Z function_auto_after bible_url_base

--- public/src/bible_url_base.mjs
+++ public/src/bible_url_base.mjs
@@ -1,5 +1,8 @@
 import { bible_ebible_url } from "./bible_ebible_url.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function bible_url_base(bible_folder) {
-  return string_combine_multiple([bible_ebible_url(), bible_folder, "/"]);
+  return string_combine_multiple([
+    string_combine_multiple([bible_ebible_url(), bible_folder]),
+    "/",
+  ]);
 }
