dedbcf54c11b949400a1b743f59bc6b1a2fe1e3f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 11:37:10 -0400

    2024-09-14T15:37:10.378Z function_auto_after html_parse_visit_tag_single

--- public/src/html_parse_visit_tag_single.mjs
+++ public/src/html_parse_visit_tag_single.mjs
@@ -1,10 +1,10 @@
-import { html_parse_outer } from "./html_parse_outer.mjs";
+import { html_parse_text } from "./html_parse_text.mjs";
 import { list_single_message } from "./list_single_message.mjs";
 import { html_parse_visit_tag_list } from "./html_parse_visit_tag_list.mjs";
 export function html_parse_visit_tag_single(node, tag_name) {
   let r = html_parse_visit_tag_list(node, tag_name);
   let s = list_single_message(r, () => ({
-    node: html_parse_outer(node),
+    node_text: html_parse_text(node),
   }));
   return s;
 }
