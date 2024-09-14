c6523445212bb66319dec12b4c9a07484d2634d7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 11:51:19 -0400

    2024-09-14T15:51:19.320Z function_auto_after bible_eng_copyrights

--- public/src/bible_eng_copyrights.mjs
+++ public/src/bible_eng_copyrights.mjs
@@ -2,6 +2,7 @@ import { bible_eng_versions_all_each } from "./bible_eng_versions_all_each.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { bible_copyright } from "./bible_copyright.mjs";
 export async function bible_eng_copyrights() {
+  let lambda_each = bible_eng_versions_all_each;
   let copyrights = {};
   await bible_eng_versions_all_each(async function lambda(bible_folder) {
     let c = await bible_copyright(bible_folder);
