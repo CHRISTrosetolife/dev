09a7e421857a0c7b64c2fe742e76f7cb8a3be4a5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 09:47:54 -0400

    2024-09-14T13:47:53.673Z function_auto_after bible_versions_list_english

--- public/src/bible_versions_list_english.mjs
+++ public/src/bible_versions_list_english.mjs
@@ -1,6 +1,6 @@
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { bible_ebible_url_combine } from "./bible_ebible_url_combine.mjs";
 export async function bible_versions_list_english() {
-  let joined = bible_ebible_url_combine("download.php");
-  let root = await html_cache_parse(joined);
+  let c = bible_ebible_url_combine("download.php");
+  let root = await html_cache_parse(c);
 }
