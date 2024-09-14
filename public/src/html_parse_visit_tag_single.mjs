b199ea897caa441cbe26f590e8015e69d7173a0c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 11:36:08 -0400

    2024-09-14T15:36:07.605Z function_auto_after html_parse_visit_tag_single

--- public/src/html_parse_visit_tag_single.mjs
+++ public/src/html_parse_visit_tag_single.mjs
@@ -3,8 +3,8 @@ import { list_single_message } from "./list_single_message.mjs";
 import { html_parse_visit_tag_list } from "./html_parse_visit_tag_list.mjs";
 export function html_parse_visit_tag_single(node, tag_name) {
   let r = html_parse_visit_tag_list(node, tag_name);
-  let s = list_single_message(r, {
+  let s = list_single_message(r, () => ({
     node: html_parse_outer(node),
-  });
+  }));
   return s;
 }
