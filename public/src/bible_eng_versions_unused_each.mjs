4a51d1eaaab54d40060e7fd6bd6eb652964291be
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 11:50:13 -0400

    2024-09-14T15:50:12.896Z function_auto_after bible_eng_versions_unused_each

--- public/src/bible_eng_versions_unused_each.mjs
+++ public/src/bible_eng_versions_unused_each.mjs
@@ -3,8 +3,8 @@ import { bible_eng_versions } from "./bible_eng_versions.mjs";
 import { bible_eng_versions_all } from "./bible_eng_versions_all.mjs";
 import { each_async } from "./each_async.mjs";
 export async function bible_eng_versions_unused_each(lambda) {
-  let bible_folders_all = await bible_eng_versions_all();
+  let bible_folders = await bible_eng_versions_all();
   let bible_folders_used = await bible_eng_versions();
-  list_remove_multiple(bible_folders_all, bible_folders_used);
-  await each_async(bible_folders_all, lambda);
+  list_remove_multiple(bible_folders, bible_folders_used);
+  await each_async(bible_folders, lambda);
 }
