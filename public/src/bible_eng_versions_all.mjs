f28e871fa6296d6d2c4a25c93c05ef44c6697773
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 11:29:32 -0400

    2024-09-14T15:29:31.997Z r bible_versions_list_english bible_eng_versions_all

--- public/src/bible_eng_versions_all.mjs
+++ public/src/bible_eng_versions_all.mjs
@@ -5,7 +5,7 @@ import { list_unique } from "./list_unique.mjs";
 import { string_starts_with_curry } from "./string_starts_with_curry.mjs";
 import { html_cache_parse_hrefs } from "./html_cache_parse_hrefs.mjs";
 import { bible_ebible_url_combine } from "./bible_ebible_url_combine.mjs";
-export async function bible_versions_list_english() {
+export async function bible_eng_versions_all() {
   let url = bible_ebible_url_combine("download.php");
   let prefix = "details.php?id=";
   let hrefs = await html_cache_parse_hrefs(
--- public/src/bible_eng_versions_all_each.mjs
+++ public/src/bible_eng_versions_all_each.mjs
@@ -1,6 +1,6 @@
-import { bible_versions_list_english } from "./bible_versions_list_english.mjs";
+import { bible_eng_versions_all } from "./bible_eng_versions_all.mjs";
 import { each_async } from "./each_async.mjs";
 export async function bible_eng_versions_all_each(lambda) {
-  let bible_folders = await bible_versions_list_english();
+  let bible_folders = await bible_eng_versions_all();
   await each_async(bible_folders, lambda);
 }
