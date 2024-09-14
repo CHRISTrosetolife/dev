e23e228b330f377b140b4202207ac20c54b80b39
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 11:27:52 -0400

    2024-09-14T15:27:51.721Z function_auto_after bible_versions_list_english

--- public/src/bible_versions_list_english.mjs
+++ public/src/bible_versions_list_english.mjs
@@ -1,3 +1,5 @@
+import { string_prefix_without } from "./string_prefix_without.mjs";
+import { list_map } from "./list_map.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { list_unique } from "./list_unique.mjs";
 import { string_starts_with_curry } from "./string_starts_with_curry.mjs";
@@ -11,5 +13,6 @@ export async function bible_versions_list_english() {
     string_starts_with_curry(string_combine_multiple([prefix, "eng"])),
   );
   let u = list_unique(hrefs);
+  let m = list_map(u, (s) => string_prefix_without(s, prefix));
   return u;
 }
