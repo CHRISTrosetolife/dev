223689f6109b591441077ebaaceb0587100e54e9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 11:22:03 -0400

    2024-09-14T15:22:01.065Z function_auto_after bible_ceb_3_hrefs

--- public/src/bible_ceb_3_hrefs.mjs
+++ public/src/bible_ceb_3_hrefs.mjs
@@ -1,10 +1,11 @@
+import { html_cache_parse_hrefs } from "./html_cache_parse_hrefs.mjs";
 import { string_starts_with_curry } from "./string_starts_with_curry.mjs";
-import { html_parse_a_hrefs } from "./html_parse_a_hrefs.mjs";
-import { html_cache_parse } from "./html_cache_parse.mjs";
 import { bible_ceb_3_url_base } from "./bible_ceb_3_url_base.mjs";
 export async function bible_ceb_3_hrefs() {
   let url_base = bible_ceb_3_url_base();
-  let root_bible = await html_cache_parse(url_base);
-  let hrefs = html_parse_a_hrefs(root_bible, string_starts_with_curry("B"));
+  let hrefs = await html_cache_parse_hrefs(
+    url_base,
+    string_starts_with_curry("B"),
+  );
   return hrefs;
 }
