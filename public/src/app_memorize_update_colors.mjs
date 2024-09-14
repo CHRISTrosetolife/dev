07029984bd446d655ac9a38e97212b853e7d2e18
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-13 20:16:03 -0400

    2024-09-14T00:15:56.443Z function_auto_after app_memorize_update_colors

--- public/src/app_memorize_update_colors.mjs
+++ public/src/app_memorize_update_colors.mjs
@@ -20,8 +20,7 @@ export function app_memorize_update_colors(context) {
   let { previous_token_element } = context;
   if (undefined_not_is(previous_token_element)) {
     html_style_visible(previous_token_element);
-    let attribute_name = "error";
-    if (!html_data_has(previous_token_element, attribute_name)) {
+    if (!html_data_has(previous_token_element, "error")) {
       html_style_font_color_default_set(previous_token_element);
     }
   }
