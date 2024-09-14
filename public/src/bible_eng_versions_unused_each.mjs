5d1a55c5924834979ce894d2536c6382ab797842
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 11:49:18 -0400

    2024-09-14T15:49:17.579Z function_auto_after bible_eng_versions_unused_each

--- public/src/bible_eng_versions_unused_each.mjs
+++ public/src/bible_eng_versions_unused_each.mjs
@@ -1,6 +1,6 @@
 import { bible_eng_versions_all } from "./bible_eng_versions_all.mjs";
 import { each_async } from "./each_async.mjs";
 export async function bible_eng_versions_unused_each(lambda) {
-  let bible_folders = await bible_eng_versions_all();
-  await each_async(bible_folders, lambda);
+  let bible_folders_all = await bible_eng_versions_all();
+  await each_async(bible_folders_all, lambda);
 }
