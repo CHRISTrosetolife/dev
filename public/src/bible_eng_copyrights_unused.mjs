daa2c3bac3913ab21aa4b111be5d953f969822ea
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 11:52:03 -0400

    2024-09-14T15:52:02.351Z function_auto_after bible_eng_copyrights_unused

--- public/src/bible_eng_copyrights_unused.mjs
+++ public/src/bible_eng_copyrights_unused.mjs
@@ -1,6 +1,6 @@
+import { bible_eng_versions_unused_each } from "./bible_eng_versions_unused_each.mjs";
 import { bible_eng_copyrights_generic } from "./bible_eng_copyrights_generic.mjs";
-import { bible_eng_versions_all_each } from "./bible_eng_versions_all_each.mjs";
 export async function bible_eng_copyrights_unused() {
-  let lambda_each = bible_eng_versions_all_each;
+  let lambda_each = bible_eng_versions_unused_each;
   return await bible_eng_copyrights_generic(lambda_each);
 }
