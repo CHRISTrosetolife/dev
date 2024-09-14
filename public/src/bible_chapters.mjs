39e79ed57433f3cb836c75bc90c5e9516bd352eb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 11:23:11 -0400

    2024-09-14T15:23:10.737Z function_auto_after bible_chapters

--- public/src/bible_chapters.mjs
+++ public/src/bible_chapters.mjs
@@ -1,16 +1,14 @@
+import { html_cache_parse_hrefs } from "./html_cache_parse_hrefs.mjs";
 import { bible_url_htm } from "./bible_url_htm.mjs";
 import { list_map } from "./list_map.mjs";
 import { integer_parse } from "./integer_parse.mjs";
-import { html_parse_a_hrefs } from "./html_parse_a_hrefs.mjs";
-import { html_cache_parse } from "./html_cache_parse.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
 export async function bible_chapters(bible_folder, book_name) {
   let url = bible_url_htm(bible_folder, book_name);
-  let root = await html_cache_parse(url);
-  let hrefs = html_parse_a_hrefs(root, condition);
   let extension = ".htm";
+  let hrefs = await html_cache_parse_hrefs(url, condition);
   function condition(href) {
     if (string_starts_with(href, book_name)) {
       let without = string_prefix_without(href, book_name);
