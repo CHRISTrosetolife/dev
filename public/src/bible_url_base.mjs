14300b960947f98c4a230d30d28b6225a293ad39
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 09:45:32 -0400

    2024-09-14T13:45:30.841Z function_auto_after bible_url_base

--- public/src/bible_url_base.mjs
+++ public/src/bible_url_base.mjs
@@ -2,8 +2,7 @@ import { url_secure } from "./url_secure.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function bible_url_base(bible_folder) {
   return string_combine_multiple([
-    url_secure(),
-    "ebible.org/",
+    string_combine_multiple([url_secure(), "ebible.org/"]),
     bible_folder,
     "/",
   ]);
