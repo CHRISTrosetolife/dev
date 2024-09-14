fdd7bcee2e759e8a1753cb010e37c4d432378c66
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-14 07:59:32 -0400

    2024-09-14T11:59:32.139Z function_auto_after bible_book_prefix_to_number

--- public/src/bible_book_prefix_to_number.mjs
+++ public/src/bible_book_prefix_to_number.mjs
@@ -1,12 +1,15 @@
+import { string_starts_with_curry } from "./string_starts_with_curry.mjs";
+import { list_find } from "./list_find.mjs";
 import { list_index } from "./list_index.mjs";
 import { bible_books } from "./bible_books.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { bible_books_names_lookup_inverted } from "./bible_books_names_lookup_inverted.mjs";
 import { object_properties } from "./object_properties.mjs";
-export async function bible_book_prefix_to_number(bible_folder, book) {
+export async function bible_book_prefix_to_number(bible_folder, book_prefix) {
   let inverted = await bible_books_names_lookup_inverted(bible_folder);
   let keys = object_properties(inverted);
-  let acronym = object_property_get(inverted, book);
+  list_find(keys, string_starts_with_curry(book_prefix));
+  let acronym = object_property_get(inverted, book_prefix);
   let books = await bible_books(bible_folder);
   let n = list_index(books, acronym) + 1;
   return n;
