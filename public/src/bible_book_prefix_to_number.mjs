28627db8bfaf99341ded18866860bdcdcab99c2f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-15 16:24:10 -0400

    2024-09-15T20:24:10.288Z function_auto_after bible_book_prefix_to_number

--- public/src/bible_book_prefix_to_number.mjs
+++ public/src/bible_book_prefix_to_number.mjs
@@ -5,11 +5,13 @@ import { string_starts_with_curry } from "./string_starts_with_curry.mjs";
 import { list_find } from "./list_find.mjs";
 import { bible_books_names_lookup_inverted } from "./bible_books_names_lookup_inverted.mjs";
 import { object_properties } from "./object_properties.mjs";
+import { string_case_lower } from "./string_case_lower.mjs";
 export async function bible_book_prefix_to_number(bible_folder, book_prefix) {
   let inverted = await bible_books_names_lookup_inverted(bible_folder);
   let keys = object_properties(inverted);
   let keys_lower = list_map_lower(keys);
-  let book = list_find(keys, string_starts_with_curry(book_prefix));
+  book_prefix = string_case_lower(book_prefix);
+  let book = list_find(keys_lower, string_starts_with_curry(book_prefix));
   log({
     book,
   });
