4562f97b76e817afe0450c5a6903b3c5fc746dff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 14:49:17 -0400

    2024-09-16T18:49:14.950Z function_auto_after bible_eng_versions_download

--- public/src/bible_eng_versions_download.mjs
+++ public/src/bible_eng_versions_download.mjs
@@ -1,12 +1,12 @@
+import { bible_eng_versions } from "./bible_eng_versions.mjs";
 import { each_log_async } from "./each_log_async.mjs";
 import { folder_unzip } from "./folder_unzip.mjs";
 import { path_encoded_parent } from "./path_encoded_parent.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { bible_ebible_url_combine } from "./bible_ebible_url_combine.mjs";
 import { http_file } from "./http_file.mjs";
-import { bible_eng_versions_new } from "./bible_eng_versions_new.mjs";
 export async function bible_eng_versions_download() {
-  let bible_folders = bible_eng_versions_new();
+  let bible_folders = bible_eng_versions();
   await each_log_async(bible_folders, async (bible_folder) => {
     let url = bible_ebible_url_combine(
       string_combine_multiple(["Scriptures/", bible_folder, "_html.zip"]),
