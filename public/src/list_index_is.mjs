22b2abfee0bd25afc6560ddbc1a7260106275f23
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 19:46:28 -0400

    2024-09-16T23:46:27.473Z function_auto_after list_index_is

---
+++ public/src/list_index_is.mjs
@@ -0,0 +1,6 @@
+import { list_size } from "./list_size.mjs";
+import { less_than } from "./less_than.mjs";
+import { greater_than_equal } from "./greater_than_equal.mjs";
+export function list_index_is(index, list) {
+  return greater_than_equal(index, 0) && less_than(index, list_size(list));
+}
