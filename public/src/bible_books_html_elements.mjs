55e99cba2ec01b199c2ac8f57878e2fcea929e0f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 16:07:44 -0400

    2024-09-16T20:07:44.110Z function_auto_after bible_books_html_elements

--- public/src/bible_books_html_elements.mjs
+++ public/src/bible_books_html_elements.mjs
@@ -1,13 +1,9 @@
-import { bible_file } from "./bible_file.mjs";
+import { bible_file_html_parse } from "./bible_file_html_parse.mjs";
 import { html_parse_visit_classes_list } from "./html_parse_visit_classes_list.mjs";
 import { list_single } from "./list_single.mjs";
 import { html_parse_visit_class_list } from "./html_parse_visit_class_list.mjs";
-import { html_cache_parse } from "./html_cache_parse.mjs";
-import { bible_url_base } from "./bible_url_base.mjs";
 export async function bible_books_html_elements(bible_folder) {
-  let url_base = bible_url_base(bible_folder);
-  let html = await bible_file(bible_folder, chapter_name);
-  let root = await html_cache_parse(url_base);
+  let root = await bible_file_html_parse(bible_folder, "index");
   let vnavs = html_parse_visit_class_list(root, "vnav");
   let vnav = list_single(vnavs);
   let book_elements = html_parse_visit_classes_list(vnav, ["oo", "nn"]);
