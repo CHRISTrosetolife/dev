6693ab7a3838f69255b48df0b5ae8d31057b0972
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 14:35:22 -0400

    2024-09-16T18:35:21.495Z function_auto_after path_encoded

--- public/src/path_encoded.mjs
+++ public/src/path_encoded.mjs
@@ -3,6 +3,9 @@ import { path_join } from "./path_join.mjs";
 import { string_encoded_to } from "./string_encoded_to.mjs";
 export function path_encoded(folder_name, file_name) {
   let file_name_encoded = string_encoded_to(file_name);
-  let joined = path_join([folder_gitignore(), folder_name, file_name_encoded]);
+  let joined = path_join([
+    path_join([folder_gitignore(), folder_name]),
+    file_name_encoded,
+  ]);
   return joined;
 }
