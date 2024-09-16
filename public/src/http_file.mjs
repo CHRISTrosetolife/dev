505e39b0711075219280966bd6f4162dd7491aae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 08:56:44 -0400

    2024-09-16T12:56:43.611Z function_auto_after http_file

--- public/src/http_file.mjs
+++ public/src/http_file.mjs
@@ -8,9 +8,9 @@ export async function http_file(url) {
   let file_path_output = path_json_encoded(http_file.name, {
     url,
   });
+  await folder_parent_exists_ensure(file_path_output);
   let response = await http_response(url);
   let streamPipeline = promisify(pipeline);
-  await folder_parent_exists_ensure(file_path_output);
   await streamPipeline(response.body, createWriteStream(file_path_output));
   return file_path_output;
 }
