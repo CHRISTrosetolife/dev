e8abc5df4e127387771cd060be0ce0dac691293e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 09:00:14 -0400

    2024-09-16T13:00:13.441Z function_auto_after file_name_json

--- public/src/file_name_json.mjs
+++ public/src/file_name_json.mjs
@@ -1,4 +1,4 @@
 import { string_combine } from "./string_combine.mjs";
-export function file_name_json(key_encoded) {
-  return string_combine(key_encoded, ".json");
+export function file_name_json(without_extension) {
+  return string_combine(without_extension, ".json");
 }
