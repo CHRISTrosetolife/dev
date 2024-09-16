fa8af357f685634cdbbfbce9abad01e9dbbc3f4f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 16:14:24 -0400

    2024-09-16T20:14:23.608Z function_auto_after bible_zip_url

--- public/src/bible_zip_url.mjs
+++ public/src/bible_zip_url.mjs
@@ -1,6 +1,9 @@
+import { string_is } from "./string_is.mjs";
+import { assert } from "./assert.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { bible_ebible_url_combine } from "./bible_ebible_url_combine.mjs";
 export function bible_zip_url(bible_folder) {
+  assert(string_is, [bible_folder]);
   return bible_ebible_url_combine(
     string_combine_multiple(["Scriptures/", bible_folder, "_html.zip"]),
   );
