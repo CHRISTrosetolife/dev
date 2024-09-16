0c768d1391a100e48040f1d0f41f9177fd34c66f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 14:34:25 -0400

    2024-09-16T18:34:24.146Z function_auto_after bible_eng_versions_download

--- public/src/bible_eng_versions_download.mjs
+++ public/src/bible_eng_versions_download.mjs
@@ -1,3 +1,4 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { bible_ebible_url_combine } from "./bible_ebible_url_combine.mjs";
 import { http_file } from "./http_file.mjs";
 import { each_async } from "./each_async.mjs";
@@ -5,7 +6,7 @@ import { bible_eng_versions_new } from "./bible_eng_versions_new.mjs";
 export async function bible_eng_versions_download() {
   let bible_folders = bible_eng_versions_new();
   await each_async(bible_folders, async (bible_folder) => {
-    bible_ebible_url_combine("Scriptures/eng-lxx2012_html.zip");
+    bible_ebible_url_combine(string_combine_multiple(["Scriptures/", ".zip"]));
     await http_file();
   });
 }
