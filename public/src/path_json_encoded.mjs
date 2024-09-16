4a4a3936ea5fb5b1a21720380ba7d49589338447
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 09:03:37 -0400

    2024-09-16T13:03:36.765Z function_auto_after path_json_encoded

--- public/src/path_json_encoded.mjs
+++ public/src/path_json_encoded.mjs
@@ -1,10 +1,8 @@
 import { folder_gitignore } from "./folder_gitignore.mjs";
 import { path_join } from "./path_join.mjs";
 import { string_encoded_to } from "./string_encoded_to.mjs";
-import { json_to } from "./json_to.mjs";
-export function path_json_encoded(folder_name, key) {
-  let key_json = json_to(key);
-  let key_encoded = string_encoded_to(key_json);
-  let joined = path_join([folder_gitignore(), folder_name, key_encoded]);
+export function path_json_encoded(folder_name, file_name) {
+  let file_name_encoded = string_encoded_to(file_name);
+  let joined = path_join([folder_gitignore(), folder_name, file_name_encoded]);
   return joined;
 }
