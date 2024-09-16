066a46010cd8b3d0ef0800f20d2add37271dd540
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 14:28:20 -0400

    2024-09-16T18:28:19.783Z function_auto_after unzip

--- public/src/unzip.mjs
+++ public/src/unzip.mjs
@@ -3,14 +3,14 @@ import { folder_exists_ensure } from "./folder_exists_ensure.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { log } from "./log.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
-export async function unzip(z_path) {
+export async function unzip(path_input) {
   let extension = ".zip";
-  let output_path = string_suffix_without(z_path, extension);
+  let output_path = string_suffix_without(path_input, extension);
   log(string_combine_multiple(["unzipping to ", output_path]));
   await folder_exists_ensure(output_path);
   let command = string_combine_multiple([
     'tar -xf "',
-    z_path,
+    path_input,
     '" -C "',
     output_path,
     '"',
