2bed4f6bb5aa77b93ad338da820210de07699ffe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 15:47:41 -0400

    2024-09-16T19:47:40.572Z function_auto_after bible_chapter_path

--- public/src/bible_chapter_path.mjs
+++ public/src/bible_chapter_path.mjs
@@ -1,11 +1,14 @@
+import { string_combine } from "./string_combine.mjs";
 import { http_file } from "./http_file.mjs";
 import { bible_zip_url } from "./bible_zip_url.mjs";
 import { unzip } from "./unzip.mjs";
 import { http_file_path } from "./http_file_path.mjs";
+import { path_join } from "./path_join.mjs";
 export async function bible_chapter_path(bible_folder, chapter_name) {
   let url = bible_zip_url(bible_folder);
   await http_file(url);
   let file_path_output = http_file_path(url);
   let output_path = await unzip(file_path_output);
+  path_join([output_path, string_combine(chapter_name, ".htm")]);
   return output_path;
 }
