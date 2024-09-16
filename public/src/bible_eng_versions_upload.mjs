dbac566069da0536116471eba48bcfddf2a9413f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 19:17:27 -0400

    2024-09-16T23:17:27.062Z function_auto_after bible_eng_versions_upload

--- public/src/bible_eng_versions_upload.mjs
+++ public/src/bible_eng_versions_upload.mjs
@@ -1,10 +1,8 @@
-import { bible_eng_versions_new } from "./bible_eng_versions_new.mjs";
-import { each_async } from "./each_async.mjs";
+import { bible_eng_versions_each } from "./bible_eng_versions_each.mjs";
 import { bible_books_upload } from "./bible_books_upload.mjs";
 import { bible_book_upload_all } from "./bible_book_upload_all.mjs";
 export async function bible_eng_versions_upload() {
-  let bible_folders = bible_eng_versions_new();
-  await each_async(bible_folders, async (bible_folder) => {
+  await bible_eng_versions_each(bible_folders, async (bible_folder) => {
     await bible_books_upload(bible_folder);
     await bible_book_upload_all(bible_folder);
   });
