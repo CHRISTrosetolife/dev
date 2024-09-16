33cfa6a2baa76b55c40eff49dec9aeef877daad2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 16:10:59 -0400

    2024-09-16T20:10:59.323Z function_auto_after bible_chapters

--- public/src/bible_chapters.mjs
+++ public/src/bible_chapters.mjs
@@ -1,13 +1,11 @@
 import { html_parse_a_hrefs } from "./html_parse_a_hrefs.mjs";
 import { bible_file_html_parse } from "./bible_file_html_parse.mjs";
-import { bible_url_htm } from "./bible_url_htm.mjs";
 import { list_map } from "./list_map.mjs";
 import { integer_parse } from "./integer_parse.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
 export async function bible_chapters(bible_folder, book_name) {
-  let url = bible_url_htm(bible_folder, book_name);
   let extension = ".htm";
   let root = await bible_file_html_parse(bible_folder, book_name);
   let hrefs = html_parse_a_hrefs(root, condition);
