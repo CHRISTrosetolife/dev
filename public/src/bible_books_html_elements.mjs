dbd5f2c36cc3045234aedcb1c9c2c26241e9391d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 16:14:05 -0400

    2024-09-16T20:14:05.213Z function_auto_after bible_books_html_elements

--- public/src/bible_books_html_elements.mjs
+++ public/src/bible_books_html_elements.mjs
@@ -1,11 +1,8 @@
-import { string_is } from "./string_is.mjs";
-import { assert } from "./assert.mjs";
 import { bible_file_html_parse } from "./bible_file_html_parse.mjs";
 import { html_parse_visit_classes_list } from "./html_parse_visit_classes_list.mjs";
 import { list_single } from "./list_single.mjs";
 import { html_parse_visit_class_list } from "./html_parse_visit_class_list.mjs";
 export async function bible_books_html_elements(bible_folder) {
-  assert(string_is, [bible_folder]);
   let root = await bible_file_html_parse(bible_folder, "index");
   let vnavs = html_parse_visit_class_list(root, "vnav");
   let vnav = list_single(vnavs);
