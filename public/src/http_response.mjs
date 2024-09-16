afc4ad77f3629b9f14070550099abad61c93bb87
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 14:38:41 -0400

    2024-09-16T18:38:40.580Z function_auto_after http_response

--- public/src/http_response.mjs
+++ public/src/http_response.mjs
@@ -1,3 +1,5 @@
+import { json_to } from "./json_to.mjs";
+import { error } from "./error.mjs";
 import { integer_random } from "./integer_random.mjs";
 import { sleep } from "./sleep.mjs";
 import { retry_if } from "./retry_if.mjs";
@@ -12,7 +14,13 @@ export async function http_response(url) {
     retry_if(["ECONNRESET", "ENOTFOUND", "ETIMEDOUT", "EHOSTUNREACH"]),
   );
   if (!response.ok) {
-    throw error_json();
+    let statusText = response;
+    throw error(
+      json_to({
+        url,
+        response,
+      }),
+    );
   }
   async function lambda() {
     await sleep(integer_random(5000, 8000));
