ae1ce0d1dbecacd1c0612b6965cbc01def1863aa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 08:48:31 -0400

    2024-09-16T12:48:30.979Z function_auto_after http_file

--- public/src/http_file.mjs
+++ public/src/http_file.mjs
@@ -1,4 +1,5 @@
 import { http_response } from "./http_response.mjs";
-export async function http_file() {
+export async function http_file(url, file_path_output) {
   let response = await http_response(url);
+  await streamPipeline(response.body, createWriteStream(file_path_output));
 }
