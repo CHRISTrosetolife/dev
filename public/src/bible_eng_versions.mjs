cf67929367480c1e990545ed34d2d17e60f166c4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 19:59:12 -0400

    2024-09-16T23:59:10.896Z function_auto_after bible_eng_versions

--- public/src/bible_eng_versions.mjs
+++ public/src/bible_eng_versions.mjs
@@ -1,6 +1,6 @@
+import { list_sort_string } from "./list_sort_string.mjs";
 import { string_replace } from "./string_replace.mjs";
 import { bible_eng_versions_new } from "./bible_eng_versions_new.mjs";
-import { list_sort } from "./list_sort.mjs";
 import { list_concat } from "./list_concat.mjs";
 export function bible_eng_versions() {
   let a = [
@@ -28,6 +28,6 @@ export function bible_eng_versions() {
   ];
   let b = bible_eng_versions_new();
   let c = list_concat(a, b);
-  list_sort(c, (s) => string_replace(s, "-", ""));
+  list_sort_string(c, (s) => string_replace(s, "-", ""));
   return c;
 }
