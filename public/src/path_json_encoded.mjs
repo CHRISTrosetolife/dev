ac24a4d03520ca83d507db58c67784c7f3ac9dbf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 09:00:22 -0400

    2024-09-16T13:00:20.833Z function_auto_after path_json_encoded

--- public/src/path_json_encoded.mjs
+++ public/src/path_json_encoded.mjs
@@ -1,12 +1,10 @@
 import { folder_gitignore } from "./folder_gitignore.mjs";
 import { path_join } from "./path_join.mjs";
-import { file_name_json } from "./file_name_json.mjs";
 import { string_encoded_to } from "./string_encoded_to.mjs";
 import { json_to } from "./json_to.mjs";
 export function path_json_encoded(folder_name, key) {
   let key_json = json_to(key);
   let key_encoded = string_encoded_to(key_json);
   let joined = path_join([folder_gitignore(), folder_name, key_encoded]);
-  let file_path = file_name_json(joined);
   return file_path;
 }
