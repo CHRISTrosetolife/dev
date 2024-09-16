dc596f47c4193918ba1ef1f94e800ea2a634c536
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 15:43:54 -0400

    2024-09-16T19:43:53.580Z function_auto_after http_file

--- public/src/http_file.mjs
+++ public/src/http_file.mjs
@@ -4,9 +4,13 @@ import { http_response } from "./http_response.mjs";
 import { createWriteStream } from "node:fs";
 import { pipeline } from "node:stream";
 import { promisify } from "node:util";
+import { file_exists } from "./file_exists.mjs";
 export async function http_file(url) {
   let streamPipeline = promisify(pipeline);
   let file_path_output = http_file_path(url);
+  if (file_exists) {
+    return;
+  }
   await folder_parent_exists_ensure(file_path_output);
   let response = await http_response(url);
   await streamPipeline(response.body, createWriteStream(file_path_output));
