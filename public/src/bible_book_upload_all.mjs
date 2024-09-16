002a9da9cb51f6081274ba18ed26fe27cd65cca7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 19:17:58 -0400

    2024-09-16T23:17:57.772Z function_auto_after bible_book_upload_all

--- public/src/bible_book_upload_all.mjs
+++ public/src/bible_book_upload_all.mjs
@@ -1,8 +1,6 @@
-import { bible_books } from "./bible_books.mjs";
 import { bible_books_each } from "./bible_books_each.mjs";
 import { bible_book_upload } from "./bible_book_upload.mjs";
 export async function bible_book_upload_all(bible_folder) {
-  let books = await bible_books(bible_folder);
   await bible_books_each(bible_folder, lambda);
   async function lambda(book_name) {
     await bible_book_upload(bible_folder, book_name);
