095bc0dfc9468538d491ea31ef204c4e86d12835
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 19:46:37 -0400

    2024-09-16T23:46:36.840Z pm list_index_is 0 1

--- public/src/list_get.mjs
+++ public/src/list_get.mjs
@@ -4,7 +4,7 @@ import { less_than } from "./less_than.mjs";
 import { list_size } from "./list_size.mjs";
 import { assert } from "./assert.mjs";
 export function list_get(list, index) {
-  let valid = list_index_is(index, list);
+  let valid = list_index_is(list, index);
   assert(greater_than_equal, [index, 0]);
   assert(less_than, [index, list_size(list)]);
   return list[index];
