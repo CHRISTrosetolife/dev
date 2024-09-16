df07217ec679d334291a6700559395cdd387decc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 08:55:15 -0400

    2024-09-16T12:55:14.926Z function_auto_after http_file

--- public/src/http_file.mjs
+++ public/src/http_file.mjs
@@ -3,12 +3,12 @@ import { http_response } from "./http_response.mjs";
 import { createWriteStream } from "node:fs";
 import { pipeline } from "node:stream";
 import { promisify } from "node:util";
-export async function http_file(url, file_path_output) {
-  let file_path = path_json_encoded(http_file.name, {
+export async function http_file(url) {
+  let file_path_output = path_json_encoded(http_file.name, {
     url,
   });
   let response = await http_response(url);
   let streamPipeline = promisify(pipeline);
-  await streamPipeline(response.body, createWriteStream(file_path));
-  return file_path;
+  await streamPipeline(response.body, createWriteStream(file_path_output));
+  return file_path_output;
 }
