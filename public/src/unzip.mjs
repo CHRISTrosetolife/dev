ef36f2ed49f43a5f9cbd81d345682278505d21b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 15:46:05 -0400

    2024-09-16T19:46:04.626Z function_auto_after unzip

--- public/src/unzip.mjs
+++ public/src/unzip.mjs
@@ -3,8 +3,12 @@ import { command_line } from "./command_line.mjs";
 import { folder_exists_ensure } from "./folder_exists_ensure.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { log } from "./log.mjs";
+import { file_exists } from "./file_exists.mjs";
 export async function unzip(path_input) {
   let output_path = unzip_folder(path_input);
+  if (await file_exists(output_path)) {
+    return;
+  }
   log(string_combine_multiple(["unzipping to ", output_path]));
   await folder_exists_ensure(output_path);
   let command = string_combine_multiple([
