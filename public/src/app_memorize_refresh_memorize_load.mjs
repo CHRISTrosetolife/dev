b0d87dc80e6bb778a78678d9516a206481685bda
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 19:49:29 -0400

    2024-09-16T23:49:23.221Z function_auto_after app_memorize_refresh_memorize_load

--- public/src/app_memorize_refresh_memorize_load.mjs
+++ public/src/app_memorize_refresh_memorize_load.mjs
@@ -78,9 +78,11 @@ export async function app_memorize_refresh_memorize_load(context) {
     let { verse_index, token_index } = save;
     verse_index = list_index_ensure(verses, verse_index);
     let verse = list_get(verses, verse_index);
+    token_index = list_index_ensure(verse, token_index);
     object_merge(context, {
       verses,
       verse_index,
+      token_index,
     });
     let verses_length = list_size(context.verses);
     let groups = app_memorize_group(verses_length);
