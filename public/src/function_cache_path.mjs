8244e1775bac88c88c6bfd55eebc8a0e06ed2963
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 09:00:31 -0400

    2024-09-16T13:00:28.830Z function_auto_after function_cache_path

--- public/src/function_cache_path.mjs
+++ public/src/function_cache_path.mjs
@@ -1,9 +1,11 @@
+import { file_name_json } from "./file_name_json.mjs";
 import { path_json_encoded } from "./path_json_encoded.mjs";
 export function function_cache_path(function_name, args) {
   let key = {
     function_name,
     args,
   };
-  let file_path = path_json_encoded("cache", key);
+  let joined = path_json_encoded("cache", key);
+  let file_path = file_name_json(joined);
   return file_path;
 }
