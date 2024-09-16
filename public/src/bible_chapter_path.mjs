e6e4c3a9b5ef2e37ce3cd67102bb655442c872de
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 15:41:49 -0400

    2024-09-16T19:41:48.663Z function_auto_after bible_chapter_path

--- public/src/bible_chapter_path.mjs
+++ public/src/bible_chapter_path.mjs
@@ -1,1 +1,4 @@
-export function bible_chapter_path(bible_folder, chapter_name) {}
+import { http_file_path } from "./http_file_path.mjs";
+export function bible_chapter_path(bible_folder, chapter_name) {
+  let file_path_output = http_file_path(url);
+}
