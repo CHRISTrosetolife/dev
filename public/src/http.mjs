4957abd24e6edff7accb7eca6d7e70b19aefabcf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 08:47:36 -0400

    2024-09-16T12:47:35.994Z function_auto_after http

--- public/src/http.mjs
+++ public/src/http.mjs
@@ -1,24 +1,6 @@
-import { retry_if } from "./retry_if.mjs";
-import { retry } from "./retry.mjs";
-import { sleep } from "./sleep.mjs";
-import { integer_random } from "./integer_random.mjs";
-import { import_node } from "./import_node.mjs";
+import { http_response } from "./http_response.mjs";
 export async function http(url) {
-  let f = await import_node("node-fetch");
-  let { default: fetch } = f;
-  let response = await retry(
-    4,
-    lambda,
-    retry_if(["ECONNRESET", "ENOTFOUND", "ETIMEDOUT", "EHOSTUNREACH"]),
-  );
-  if (!response.ok) {
-    throw new Error(`unexpected response ${response.statusText}`);
-  }
-  async function lambda() {
-    await sleep(integer_random(5000, 8000));
-    let response = await fetch(url);
-    return response;
-  }
+  let response = await http_response(url);
   let body = await response.text();
   return body;
 }
---
+++ public/src/http_response.mjs
@@ -0,0 +1,23 @@
+import { integer_random } from "./integer_random.mjs";
+import { sleep } from "./sleep.mjs";
+import { retry_if } from "./retry_if.mjs";
+import { retry } from "./retry.mjs";
+import { import_node } from "./import_node.mjs";
+export async function http_response(url) {
+  let f = await import_node("node-fetch");
+  let { default: fetch } = f;
+  let response = await retry(
+    4,
+    lambda,
+    retry_if(["ECONNRESET", "ENOTFOUND", "ETIMEDOUT", "EHOSTUNREACH"]),
+  );
+  if (!response.ok) {
+    throw new Error(`unexpected response ${response.statusText}`);
+  }
+  async function lambda() {
+    await sleep(integer_random(5000, 8000));
+    let response = await fetch(url);
+    return response;
+  }
+  return response;
+}
