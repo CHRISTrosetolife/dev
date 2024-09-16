14d452e363a0f0521ffc676b574d75b2490e753a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 16:15:00 -0400

    2024-09-16T20:14:59.703Z function_auto_after bible_books_each

--- public/src/bible_books_each.mjs
+++ public/src/bible_books_each.mjs
@@ -1,7 +1,9 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { log } from "./log.mjs";
 import { bible_books } from "./bible_books.mjs";
 import { each_async } from "./each_async.mjs";
 export async function bible_books_each(bible_folder, lambda) {
+  assert_arguments_length(arguments, 2);
   await each_async(await bible_books(bible_folder), async (book_name) => {
     log({
       bible_folder,
