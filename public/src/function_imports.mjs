62712b07377b7f0302787d822b77a5b398df022c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 09:31:49 -0400

    2024-09-17T13:31:48.997Z function_auto_after function_imports

--- public/src/function_imports.mjs
+++ public/src/function_imports.mjs
@@ -1,8 +1,8 @@
-import { function_name_to_file_name } from "./function_name_to_file_name.mjs";
+import { function_name_to_path } from "./function_name_to_path.mjs";
 import { file_js_parse } from "./file_js_parse.mjs";
 import { js_imports_existing } from "./js_imports_existing.mjs";
 export async function function_imports(function_name) {
-  let file_name = function_name_to_file_name(function_name);
+  let file_name = function_name_to_path(function_name);
   let ast = await file_js_parse(file_name);
   return js_imports_existing(ast);
 }
