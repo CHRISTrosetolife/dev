e345b72fad093cf8c57aa742dd196f498fda0019
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 09:25:13 -0400

    2024-09-17T13:25:12.734Z function_auto_after adsense_add

--- public/src/adsense_add.mjs
+++ public/src/adsense_add.mjs
@@ -1,6 +1,7 @@
+import { fn_name } from "./fn_name.mjs";
 import { each } from "./each.mjs";
 import { app_index_choices_pages } from "./app_index_choices_pages.mjs";
 export function adsense_add() {
   let fn_names = app_index_choices_pages();
-  each(list, (item) => {});
+  each(fn_names, (fn_name) => {});
 }
