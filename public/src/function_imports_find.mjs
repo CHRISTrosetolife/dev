f8fdbf6f28f77101a57127607bf6b0b31338e08b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 15:29:23 -0400

    2024-09-17T19:29:23.317Z function_auto_after function_imports_find

--- public/src/function_imports_find.mjs
+++ public/src/function_imports_find.mjs
@@ -2,5 +2,5 @@ import { each_object } from "./each_object.mjs";
 import { function_imports } from "./function_imports.mjs";
 export async function function_imports_find(function_name_from, name_to) {
   let map = await function_imports(function_name_from);
-  each_object();
+  each_object(object, (key, value) => {});
 }
