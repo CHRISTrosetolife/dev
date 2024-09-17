32f3064b937f197e4f920691dc57262e80b65753
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 15:24:42 -0400

    2024-09-17T19:24:36.568Z function_auto_after function_imports

--- public/src/function_imports.mjs
+++ public/src/function_imports.mjs
@@ -7,16 +7,15 @@ import { list_filter } from "./list_filter.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 export async function function_imports(function_name) {
   let found = {};
-  if (object_property_exists(found, function_name)) {
-    return;
+  if (!object_property_exists(found, function_name)) {
+    let ast = await function_parse(function_name);
+    let imports = js_imports_existing(ast);
+    let sources = js_imports_sources(ast);
+    sources = list_filter(sources, (s) => !string_starts_with(s, "."));
+    object_property_set(found, function_name, {
+      imports,
+      sources,
+    });
   }
-  let ast = await function_parse(function_name);
-  let imports = js_imports_existing(ast);
-  let sources = js_imports_sources(ast);
-  sources = list_filter(sources, (s) => !string_starts_with(s, "."));
-  object_property_set(found, function_name, {
-    imports,
-    sources,
-  });
   return found;
 }
