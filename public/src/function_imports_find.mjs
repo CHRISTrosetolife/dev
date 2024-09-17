648602a0f44901889a43956c29520f4aca5cd434
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 15:31:00 -0400

    2024-09-17T19:30:59.542Z function_auto_after function_imports_find

--- public/src/function_imports_find.mjs
+++ public/src/function_imports_find.mjs
@@ -1,10 +1,15 @@
+import { list_adder } from "./list_adder.mjs";
 import { each } from "./each.mjs";
 import { each_object } from "./each_object.mjs";
 import { function_imports } from "./function_imports.mjs";
 export async function function_imports_find(function_name_from, name_to) {
   let map = await function_imports(function_name_from);
+  list_adder((la) => {});
   each_object(map, (function_name, mapped) => {
     let { sources, imports } = mapped;
-    each([sources, imports], (item) => {});
+    each([sources, imports], (item) => {
+      if (item === name_to) {
+      }
+    });
   });
 }
