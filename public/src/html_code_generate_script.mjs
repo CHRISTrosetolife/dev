34bebe968ebe8b3b4f837fffce63849cec87dab5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 08:15:35 -0400

    2024-09-17T12:15:34.910Z function_auto_after html_code_generate_script

--- public/src/html_code_generate_script.mjs
+++ public/src/html_code_generate_script.mjs
@@ -3,6 +3,7 @@ import { html_code_generate } from "./html_code_generate.mjs";
 export async function html_code_generate_script(function_name_suffix, input) {
   await html_code_generate(
     "script",
+    input,
     string_combine_multiple(["html_script_", function_name_suffix]),
   );
 }
