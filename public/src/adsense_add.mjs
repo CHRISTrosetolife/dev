ca27079891982b1d79dfb73833d92161a8e722f9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 09:25:08 -0400

    2024-09-17T13:25:07.655Z function_auto_after adsense_add

--- public/src/adsense_add.mjs
+++ public/src/adsense_add.mjs
@@ -1,4 +1,6 @@
+import { each } from "./each.mjs";
 import { app_index_choices_pages } from "./app_index_choices_pages.mjs";
 export function adsense_add() {
   let fn_names = app_index_choices_pages();
+  each(list, (item) => {});
 }
