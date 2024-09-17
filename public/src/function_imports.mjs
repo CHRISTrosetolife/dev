dea8aa8f0c8eae74f9d61c301f991e553495f0f2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 09:32:30 -0400

    2024-09-17T13:32:30.462Z function_auto_after function_imports

--- public/src/function_imports.mjs
+++ public/src/function_imports.mjs
@@ -2,6 +2,7 @@ import { function_name_to_path } from "./function_name_to_path.mjs";
 import { file_js_parse } from "./file_js_parse.mjs";
 import { js_imports_existing } from "./js_imports_existing.mjs";
 export async function function_imports(function_name) {
+  let found = {};
   let file_name = function_name_to_path(function_name);
   let ast = await file_js_parse(file_name);
   let imports = js_imports_existing(ast);
