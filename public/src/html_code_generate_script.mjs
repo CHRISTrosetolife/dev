857180146a3df923cef2f06f4d020613c6005909
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 08:21:00 -0400

    2024-09-17T12:20:59.740Z function_auto_after html_code_generate_script

--- public/src/html_code_generate_script.mjs
+++ public/src/html_code_generate_script.mjs
@@ -1,3 +1,8 @@
+import { function_names_to_lookup } from "./function_names_to_lookup.mjs";
+import { file_overwrite } from "./file_overwrite.mjs";
+import { html_code_generate_parent } from "./html_code_generate_parent.mjs";
+import { html_css_generate } from "./html_css_generate.mjs";
+import { function_name_generated } from "./function_name_generated.mjs";
 import { file_write } from "./file_write.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
@@ -19,4 +24,20 @@ export async function html_code_generate_script(function_name_suffix, input) {
     file_write,
   );
   return function_name;
+  let body_string = await html_code_generate("link", html_css_code);
+  let fn_new_name = string_combine_multiple([
+    function_name_generated(html_css_generate),
+    "_",
+    name,
+  ]);
+  await function_new_generic(
+    fn_new_name,
+    html_code_generate_parent(),
+    body_string,
+    false,
+    [],
+    false,
+    file_overwrite,
+  );
+  return function_names_to_lookup([fn_new_name]);
 }
