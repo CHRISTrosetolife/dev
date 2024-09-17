5c65a9027c24c5202e1e00de2381ec94c8812207
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 09:39:43 -0400

    2024-09-17T13:39:42.893Z function_auto_after function_imports

--- public/src/function_imports.mjs
+++ public/src/function_imports.mjs
@@ -1,10 +1,10 @@
 import { function_parse } from "./function_parse.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { js_imports_existing } from "./js_imports_existing.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
 export async function function_imports(function_name) {
   let found = {};
-  if (object_property_get(found, function_name)) {
+  if (object_property_exists(found, function_name)) {
     return;
   }
   let ast = await function_parse(function_name);
