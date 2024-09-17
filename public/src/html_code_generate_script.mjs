c02b3f997576a800eb30505339b4df0c7449ad06
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 08:16:27 -0400

    2024-09-17T12:16:26.213Z function_auto_after html_code_generate_script

--- public/src/html_code_generate_script.mjs
+++ public/src/html_code_generate_script.mjs
@@ -1,3 +1,5 @@
+import { file_write } from "./file_write.mjs";
+import { function_new_generic } from "./function_new_generic.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_code_generate } from "./html_code_generate.mjs";
 export async function html_code_generate_script(function_name_suffix, input) {
@@ -6,4 +8,14 @@ export async function html_code_generate_script(function_name_suffix, input) {
     function_name_suffix,
   ]);
   let code = await html_code_generate("script", input, function_name);
+  let open = true;
+  await function_new_generic(
+    function_name,
+    "",
+    "",
+    open,
+    [],
+    false,
+    file_write,
+  );
 }
