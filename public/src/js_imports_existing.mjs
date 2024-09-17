63545526d48323c86616751d16abf054d99cbd05
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 09:39:19 -0400

    2024-09-17T13:39:18.694Z function_auto_after js_imports_existing

--- public/src/js_imports_existing.mjs
+++ public/src/js_imports_existing.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_imports_existing(ast) {
@@ -5,6 +6,7 @@ export function js_imports_existing(ast) {
     js_visit_node(ast, "ImportSpecifier", (v) => {
       let { node } = v;
       let { imported } = node;
+      log({});
       if (imported.type === "Identifier") {
         la(imported.name);
       }
