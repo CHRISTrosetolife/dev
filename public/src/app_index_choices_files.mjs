7651307af624836dfe35814d9547a96fd133030e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 08:31:28 -0400

    2024-09-17T12:31:28.209Z function_auto_after app_index_choices_files

--- public/src/app_index_choices_files.mjs
+++ public/src/app_index_choices_files.mjs
@@ -4,11 +4,12 @@ import { object_property_exists } from "./object_property_exists.mjs";
 import { each } from "./each.mjs";
 import { app_index_choices } from "./app_index_choices.mjs";
 export function app_index_choices_files() {
-  list_adder((la) => {});
-  let choices = app_index_choices();
-  each(choices, (choice) => {
-    if (object_property_exists(choice, "page")) {
-      let page = object_property_get(choice, "page");
-    }
+  list_adder((la) => {
+    let choices = app_index_choices();
+    each(choices, (choice) => {
+      if (object_property_exists(choice, "page")) {
+        let page = object_property_get(choice, "page");
+      }
+    });
   });
 }
