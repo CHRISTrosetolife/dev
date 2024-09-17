f576bea3cbf19d309eb709c879b6073b3b620fc5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 08:28:53 -0400

    2024-09-17T12:28:52.150Z function_auto_after app_index_choices

--- public/src/app_index.mjs
+++ public/src/app_index.mjs
@@ -1,5 +1,4 @@
-import { url_audio_bible } from "./url_audio_bible.mjs";
-import { html_replace_nb } from "./html_replace_nb.mjs";
+import { app_index_choices } from "./app_index_choices.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
@@ -7,58 +6,11 @@ import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered
 import { window_open } from "./window_open.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-import { list_add } from "./list_add.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 export function app_index() {
   let root = html_style_default_initialize();
   html_clear_scroll_top_centered(root);
-  let choices = [];
-  list_add(choices, {
-    text: "💻 development tools documentation",
-    page: "dev",
-  });
-  list_add(choices, {
-    text: string_combine_multiple([
-      "🧑‍💻️ learn to code ",
-      html_replace_nb("( javascript )"),
-    ]),
-    page: "learn_code",
-  });
-  list_add(choices, {
-    text: "🎓 learn language cebuano",
-    page: "ceb",
-  });
-  list_add(choices, {
-    text: string_combine_multiple([
-      "🎓 learn language english ",
-      html_replace_nb("( from cebuano )"),
-    ]),
-    page: "en",
-  });
-  list_add(choices, {
-    text: "📄 resume",
-    page: "resume",
-  });
-  list_add(choices, {
-    text: "📖 bible memorize",
-    page: "memorize",
-  });
-  list_add(choices, {
-    text: "🎮 game",
-    page: "gs",
-  });
-  list_add(choices, {
-    text: "⏺️ record bible",
-    page: "record",
-  });
-  list_add(choices, {
-    text: "🎥 audio bible videos",
-    href: url_audio_bible(),
-  });
-  list_add(choices, {
-    text: "📙 book syntagmatis theologiae christianae",
-    page: "yyy8Uu",
-  });
+  let choices = app_index_choices();
   each(choices, (choice) => {
     html_button_width_full_text_click(
       root,
---
+++ public/src/app_index_choices.mjs
@@ -0,0 +1,54 @@
+import { url_audio_bible } from "./url_audio_bible.mjs";
+import { html_replace_nb } from "./html_replace_nb.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { list_add } from "./list_add.mjs";
+export function app_index_choices() {
+  let choices = [];
+  list_add(choices, {
+    text: "💻 development tools documentation",
+    page: "dev",
+  });
+  list_add(choices, {
+    text: string_combine_multiple([
+      "🧑‍💻️ learn to code ",
+      html_replace_nb("( javascript )"),
+    ]),
+    page: "learn_code",
+  });
+  list_add(choices, {
+    text: "🎓 learn language cebuano",
+    page: "ceb",
+  });
+  list_add(choices, {
+    text: string_combine_multiple([
+      "🎓 learn language english ",
+      html_replace_nb("( from cebuano )"),
+    ]),
+    page: "en",
+  });
+  list_add(choices, {
+    text: "📄 resume",
+    page: "resume",
+  });
+  list_add(choices, {
+    text: "📖 bible memorize",
+    page: "memorize",
+  });
+  list_add(choices, {
+    text: "🎮 game",
+    page: "gs",
+  });
+  list_add(choices, {
+    text: "⏺️ record bible",
+    page: "record",
+  });
+  list_add(choices, {
+    text: "🎥 audio bible videos",
+    href: url_audio_bible(),
+  });
+  list_add(choices, {
+    text: "📙 book syntagmatis theologiae christianae",
+    page: "yyy8Uu",
+  });
+  return choices;
+}
