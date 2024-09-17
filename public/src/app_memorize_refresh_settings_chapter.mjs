853fc41cf521245d0862b2ccf272f2a8f43c72c4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-16 20:03:49 -0400

    2024-09-17T00:03:48.007Z function_auto_after app_memorize_refresh_settings_chapter

--- public/src/app_memorize_refresh_settings_chapter.mjs
+++ public/src/app_memorize_refresh_settings_chapter.mjs
@@ -1,3 +1,4 @@
+import { object_merge } from "./object_merge.mjs";
 import { app_memorize_book_code_get } from "./app_memorize_book_code_get.mjs";
 import { app_memorize_refresh_memorize } from "./app_memorize_refresh_memorize.mjs";
 import { app_memorize_chapter_set } from "./app_memorize_chapter_set.mjs";
@@ -24,6 +25,10 @@ export function app_memorize_refresh_settings_chapter(context) {
     book_code,
     async (context, book_code, chapter) => {
       app_memorize_chapter_set(context, chapter);
+      object_merge(save, {
+        token_index: 0,
+        verse_index: 0,
+      });
       await app_memorize_refresh_memorize(context);
     },
   );
