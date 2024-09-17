ef66b5bf063af55edeb67202e0bb25304b4f6e58
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 08:31:23 -0400

    2024-09-17T12:31:23.342Z function_auto_after app_index_choices_files

--- public/src/app_index_choices_files.mjs
+++ public/src/app_index_choices_files.mjs
@@ -1,9 +1,10 @@
+import { list_adder } from "./list_adder.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { each } from "./each.mjs";
 import { app_index_choices } from "./app_index_choices.mjs";
 export function app_index_choices_files() {
-  let pages = [];
+  list_adder((la) => {});
   let choices = app_index_choices();
   each(choices, (choice) => {
     if (object_property_exists(choice, "page")) {
