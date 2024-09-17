0a9d43edbecec2269d213f66ca5b2fd3d9965ce4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 16:02:18 -0400

    2024-09-17T20:02:17.911Z function_auto_after data_update_multiple_transform

--- public/src/data_update_multiple_transform.mjs
+++ public/src/data_update_multiple_transform.mjs
@@ -1,8 +1,9 @@
+import { function_paths } from "./function_paths.mjs";
 import { data_update_single } from "./data_update_single.mjs";
 import { file_js_parse } from "./file_js_parse.mjs";
 import { each_async } from "./each_async.mjs";
-export async function data_update_multiple_transform(fps, data) {
-  await each_async(fps, async (fp) => {
+export async function data_update_multiple_transform(function_paths, data) {
+  await each_async(function_paths, async (fp) => {
     let ast = await file_js_parse(fp);
     data_update_single(ast, data);
   });
