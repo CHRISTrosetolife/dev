169c07ac83fa38a9c203b051750df482e38f8cbc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 15:28:52 -0400

    2024-09-17T19:28:52.019Z function_auto_after function_imports_find

--- public/src/function_imports_find.mjs
+++ public/src/function_imports_find.mjs
@@ -1,4 +1,4 @@
 import { function_imports } from "./function_imports.mjs";
 export async function function_imports_find(function_name_from, name_to) {
-  await function_imports(function_name);
+  function_name_from = await function_imports(function_name);
 }
