3a3ef40018c11d3cb659ecf49d3cae508b80e7d5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 08:15:50 -0400

    2024-09-17T12:15:49.346Z function_auto_after html_code_generate_script

--- public/src/html_code_generate_script.mjs
+++ public/src/html_code_generate_script.mjs
@@ -1,9 +1,9 @@
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_code_generate } from "./html_code_generate.mjs";
 export async function html_code_generate_script(function_name_suffix, input) {
-  await html_code_generate(
-    "script",
-    input,
-    string_combine_multiple(["html_script_", function_name_suffix]),
-  );
+  let function_name = string_combine_multiple([
+    "html_script_",
+    function_name_suffix,
+  ]);
+  await html_code_generate("script", input, function_name);
 }
