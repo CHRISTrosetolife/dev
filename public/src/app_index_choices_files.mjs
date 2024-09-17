613c48b9890bd8483c13e77a21d65c75d708fe02
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 09:24:29 -0400

    2024-09-17T13:24:26.460Z function_auto_after app_index_choices_pages

--- public/src/app_index_choices_files.mjs
+++ public/src/app_index_choices_files.mjs
@@ -1,20 +1,6 @@
+import { app_index_choices_pages } from "./app_index_choices_pages.mjs";
 import { function_names_to_lookup } from "./function_names_to_lookup.mjs";
-import { app_prefix } from "./app_prefix.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { list_adder } from "./list_adder.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { object_property_exists } from "./object_property_exists.mjs";
-import { each } from "./each.mjs";
-import { app_index_choices } from "./app_index_choices.mjs";
 export function app_index_choices_files() {
-  let fn_names = list_adder((la) => {
-    let choices = app_index_choices();
-    each(choices, (choice) => {
-      if (object_property_exists(choice, "page")) {
-        let page = object_property_get(choice, "page");
-        la(string_combine_multiple([app_prefix(), page]));
-      }
-    });
-  });
+  let fn_names = app_index_choices_pages();
   return function_names_to_lookup(fn_names);
 }
