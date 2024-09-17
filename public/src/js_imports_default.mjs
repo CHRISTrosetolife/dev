e9e16685a62e265707a0ea9bdd13d65995b391af
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 09:46:50 -0400

    2024-09-17T13:46:49.686Z function_auto_after js_imports_default

--- public/src/js_imports_default.mjs
+++ public/src/js_imports_default.mjs
@@ -1,9 +1,11 @@
+import { log } from "./log.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_imports_default(ast) {
   let existing = list_adder((la) => {
     js_visit_node(ast, "ImportDefaultSpecifier", (v) => {
       let { node } = v;
+      log({});
       la(node);
     });
   });
