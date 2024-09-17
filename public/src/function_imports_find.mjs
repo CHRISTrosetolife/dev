6a7a132033a8f5a9f9306379ec31f0bcd73f3266
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 15:36:54 -0400

    2024-09-17T19:36:53.748Z function_auto_after function_imports_find

--- public/src/function_imports_find.mjs
+++ public/src/function_imports_find.mjs
@@ -24,8 +24,7 @@ export async function function_imports_find(function_name_from, name_to) {
     }
     let mapped = object_property_get(map, u);
     let { sources, imports } = mapped;
-    list_any([sources, imports]);
-    list_contains();
+    list_any([sources, imports], (i) => list_contains(i, v));
   });
   list_adder((la) => {
     each_object(map, (function_name, mapped) => {
