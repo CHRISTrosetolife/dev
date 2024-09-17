fff1d4bf674dc67ecc0d85d980cafec794229ec4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 15:31:27 -0400

    2024-09-17T19:31:27.381Z function_auto_after function_imports_find

--- public/src/function_imports_find.mjs
+++ public/src/function_imports_find.mjs
@@ -1,9 +1,11 @@
+import { graph_path_shortest } from "./graph_path_shortest.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { each } from "./each.mjs";
 import { each_object } from "./each_object.mjs";
 import { function_imports } from "./function_imports.mjs";
 export async function function_imports_find(function_name_from, name_to) {
   let map = await function_imports(function_name_from);
+  graph_path_shortest();
   list_adder((la) => {
     each_object(map, (function_name, mapped) => {
       let { sources, imports } = mapped;
