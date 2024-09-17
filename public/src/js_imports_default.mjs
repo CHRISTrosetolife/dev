6281300f198fe40f4455ba1504030bc4ac452a60
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 09:47:23 -0400

    2024-09-17T13:47:23.315Z function_auto_after js_imports_default

--- public/src/js_imports_default.mjs
+++ public/src/js_imports_default.mjs
@@ -3,7 +3,7 @@ import { list_adder } from "./list_adder.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_imports_default(ast) {
   let existing = list_adder((la) => {
-    js_visit_node(ast, "ImportDefaultSpecifier", (v) => {
+    js_visit_node(ast, "ImportDeclaration", (v) => {
       let { node } = v;
       log(node);
       la(node);
