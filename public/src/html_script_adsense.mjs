883ac8ce119c713936953fd5b73af235a20b8e04
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 08:19:26 -0400

    2024-09-17T12:19:25.613Z html_code_generate_script adsense '<script async src=https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6654470128757682      crossorigin=anonymous></script>'

---
+++ public/src/html_script_adsense.mjs
@@ -0,0 +1,12 @@
+import { html_attribute_set } from "./html_attribute_set.mjs";
+import { html_element } from "./html_element.mjs";
+export function html_script_adsense() {
+  let c = html_element(parent, "script");
+  html_attribute_set(c, "async", "");
+  html_attribute_set(
+    c,
+    "src",
+    "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6654470128757682",
+  );
+  html_attribute_set(c, "crossorigin", "anonymous");
+}
