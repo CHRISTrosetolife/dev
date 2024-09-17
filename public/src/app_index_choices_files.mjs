fc254d697637dfea33d69d1f9d6786833c170bae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-17 08:33:15 -0400

    2024-09-17T12:33:12.804Z function_auto_after app_index_choices_files

--- public/src/app_index_choices_files.mjs
+++ public/src/app_index_choices_files.mjs
@@ -1,3 +1,4 @@
+import { function_names_to_lookup } from "./function_names_to_lookup.mjs";
 import { app_prefix } from "./app_prefix.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { list_adder } from "./list_adder.mjs";
@@ -6,7 +7,7 @@ import { object_property_exists } from "./object_property_exists.mjs";
 import { each } from "./each.mjs";
 import { app_index_choices } from "./app_index_choices.mjs";
 export function app_index_choices_files() {
-  return list_adder((la) => {
+  let fn_names = list_adder((la) => {
     let choices = app_index_choices();
     each(choices, (choice) => {
       if (object_property_exists(choice, "page")) {
@@ -15,4 +16,5 @@ export function app_index_choices_files() {
       }
     });
   });
+  return function_names_to_lookup(fn_names);
 }
