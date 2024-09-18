cf36f742d520a0afb8bcf6a1453cf2c14edbd0d7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 15:08:37 -0400

    2024-09-18T19:08:37.280Z function_auto_after function_imports_path

--- public/src/function_imports_path.mjs
+++ public/src/function_imports_path.mjs
@@ -13,7 +13,7 @@ export async function function_imports_path(function_name_from, name_to) {
     each_object(map, (function_name, mapped) => {
       la(function_name);
       let { sources, imports } = mapped;
-      each([sources, imports], (list) => each(list, la));
+      each(list_combine_multiple([sources, imports]), (list) => each(list, la));
     });
   });
   return graph_path_shortest_directed(
