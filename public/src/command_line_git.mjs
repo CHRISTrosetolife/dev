42c8cf1432aaa3544a1703c70642ba05a73e7c34
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 15:44:35 -0400

    2024-09-18T19:44:34.792Z function_auto_after command_line_git

--- public/src/command_line_git.mjs
+++ public/src/command_line_git.mjs
@@ -1,5 +1,6 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { command_line_exec } from "./command_line_exec.mjs";
 export async function command_line_git(args) {
-  let command = `git ${args}`;
+  let command = string_combine_multiple(["git ", args]);
   return await command_line_exec(command);
 }
