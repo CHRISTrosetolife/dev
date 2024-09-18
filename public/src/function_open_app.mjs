f18883513d1b6099298cfc9a409d990592a578e0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 05:35:59 -0400

    2024-09-18T09:35:58.811Z function_auto_after function_open_app

--- public/src/function_open_app.mjs
+++ public/src/function_open_app.mjs
@@ -1,7 +1,6 @@
 import { app_identifier } from "./app_identifier.mjs";
-import { app_name } from "./app_name.mjs";
 import { function_open } from "./function_open.mjs";
-export async function function_open_app(app_name) {
-  let name_prefixed = app_identifier(app_name);
+export async function function_open_app(name) {
+  let name_prefixed = app_identifier(name);
   await function_open(name_prefixed);
 }
