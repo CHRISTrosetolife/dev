d28235d822218dd2fefd3e8006b0190805ffc0f5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 03:27:29 -0400

    2024-09-18T07:27:28.833Z function_auto_after graph_neighbors

--- public/src/graph_neighbors.mjs
+++ public/src/graph_neighbors.mjs
@@ -1,12 +1,6 @@
-import { list_without } from "./list_without.mjs";
-import { list_map } from "./list_map.mjs";
+import { graph_neighbors_generic } from "./graph_neighbors_generic.mjs";
 import { list_includes } from "./list_includes.mjs";
-import { list_filter } from "./list_filter.mjs";
-import { list_single } from "./list_single.mjs";
 export function graph_neighbors(edges, v) {
   let lambda_filter = (e) => list_includes(e, v);
-  let filtered = list_filter(edges, lambda_filter);
-  let mapped = list_map(filtered, (f) => list_without(f, v));
-  let mapped2 = list_map(mapped, list_single);
-  return mapped2;
+  return graph_neighbors_generic(edges, lambda_filter, v);
 }
---
+++ public/src/graph_neighbors_generic.mjs
@@ -0,0 +1,10 @@
+import { list_single } from "./list_single.mjs";
+import { list_without } from "./list_without.mjs";
+import { list_map } from "./list_map.mjs";
+import { list_filter } from "./list_filter.mjs";
+export function graph_neighbors_generic(edges, lambda_filter, v) {
+  let filtered = list_filter(edges, lambda_filter);
+  let mapped = list_map(filtered, (f) => list_without(f, v));
+  let mapped2 = list_map(mapped, list_single);
+  return mapped2;
+}
