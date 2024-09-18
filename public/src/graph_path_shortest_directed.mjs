d577a5d6424d58024b985679abf3f46cc5ba005e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 03:33:12 -0400

    2024-09-18T07:33:11.916Z function_auto_after graph_path_shortest_directed

--- public/src/graph_path_shortest_directed.mjs
+++ public/src/graph_path_shortest_directed.mjs
@@ -1,13 +1,12 @@
+import { graph_neighbors_directed } from "./graph_neighbors_directed.mjs";
 import { graph_path_shortest_generic } from "./graph_path_shortest_generic.mjs";
-import { graph_neighbors_undirected } from "./graph_neighbors_undirected.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function graph_path_shortest_directed(vertices, edge_lambda, from, to) {
   assert_arguments_length(arguments, 4);
-  let neighbors_lambda = graph_neighbors_undirected;
   return graph_path_shortest_generic(
     vertices,
     edge_lambda,
-    neighbors_lambda,
+    graph_neighbors_directed,
     from,
     to,
   );
