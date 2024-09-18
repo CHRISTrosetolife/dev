7a8da5553ffb9309c9735fe86315e2cfc700ce36
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 15:17:17 -0400

    2024-09-18T19:17:17.236Z function_auto_after function_dependencies

--- public/src/function_dependencies.mjs
+++ public/src/function_dependencies.mjs
@@ -1,18 +1,7 @@
-import { list_concat_multiple } from "./list_concat_multiple.mjs";
-import { assert } from "./assert.mjs";
-import { each } from "./each.mjs";
-import { list_adder_unique } from "./list_adder_unique.mjs";
-import { each_object } from "./each_object.mjs";
 import { function_imports } from "./function_imports.mjs";
-import { list_empty_is } from "./list_empty_is.mjs";
+import { object_properties } from "./object_properties.mjs";
 export async function function_dependencies(function_name) {
   let map = await function_imports(function_name);
-  let dependency_names = list_adder_unique((la) => {
-    each_object(map, (function_name, mapped) => {
-      let { sources, imports } = mapped;
-      assert(list_empty_is, [sources]);
-      each(list_concat_multiple([imports, [function_name]]), la);
-    });
-  });
+  let dependency_names = object_properties(map);
   return dependency_names;
 }
