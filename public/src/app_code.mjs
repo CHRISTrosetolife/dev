c12bea4e0d396f69ce276227f967d9a9fa5ca2fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 07:01:21 -0400

    2024-09-18T11:01:20.996Z function_auto_after app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,8 +1,9 @@
+import { function_names } from "./function_names.mjs";
 import { function_run_remote } from "./function_run_remote.mjs";
 import { html_scripts_load } from "./html_scripts_load.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_code() {
   let root = html_style_default_initialize();
   await html_scripts_load(root, ["axios", "acorn", "astring"]);
-  await function_run_remote();
+  await function_run_remote(function_names, []);
 }
