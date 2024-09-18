fffbca7c974eb730cc5be03a301df56b92c71743
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 06:13:50 -0400

    2024-09-18T10:13:49.268Z function_auto_after function_open

--- public/src/function_open.mjs
+++ public/src/function_open.mjs
@@ -1,13 +1,6 @@
-import { unawait } from "./unawait.mjs";
-import { assert_async } from "./assert_async.mjs";
-import { file_exists } from "./file_exists.mjs";
-import { file_open } from "./file_open.mjs";
+import { file_open_unawait } from "./file_open_unawait.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function function_open(function_name) {
   let file_path = function_name_to_path(function_name);
-   file_open_unawait(file_path);
-}
-
- function file_open_unawait(file_path) {
-    unawait(async () => await file_open(file_path));
+  file_open_unawait(file_path);
 }
