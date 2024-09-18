2a2c887419f3b48b9009ee38d214a154bfea9529
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 03:33:29 -0400

    2024-09-18T07:33:28.205Z function_auto_after function_imports_path

---
+++ public/src/function_imports_path.mjs
@@ -0,0 +1,45 @@
+import { graph_path_shortest_undirected } from "./graph_path_shortest_undirected.mjs";
+import { log } from "./log.mjs";
+import { list_any } from "./list_any.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
+import { list_adder_unique } from "./list_adder_unique.mjs";
+import { each } from "./each.mjs";
+import { each_object } from "./each_object.mjs";
+import { function_imports } from "./function_imports.mjs";
+import { list_includes } from "./list_includes.mjs";
+export async function function_imports_path(function_name_from, name_to) {
+  let map = await function_imports(function_name_from);
+  let vertices = list_adder_unique((la) => {
+    each_object(map, (function_name, mapped) => {
+      la(function_name);
+      let { sources, imports } = mapped;
+      each([sources, imports], la);
+    });
+  });
+  log({
+    t: map["function_new_generic"],
+  });
+  return graph_path_shortest_undirected(
+    vertices,
+    (u, v) => {
+      if (!object_property_exists(map, u)) {
+        return false;
+      }
+      let mapped = object_property_get(map, u);
+      let { sources, imports } = mapped;
+      let result = list_any([sources, imports], (i) => list_includes(i, v));
+      if (
+        v === "js_code_export_function_declare" &&
+        u === "function_new_generic"
+      ) {
+        log({
+          result,
+        });
+      }
+      return result;
+    },
+    function_name_from,
+    name_to,
+  );
+}
