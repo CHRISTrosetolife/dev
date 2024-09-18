41667c4d7090be857b273eb52f6034519a638792
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 07:01:38 -0400

    2024-09-18T11:01:36.512Z function_auto_after app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -7,4 +7,7 @@ export async function app_code() {
   let root = html_style_default_initialize();
   await html_scripts_load(root, ["axios", "acorn", "astring"]);
   let ns = await function_run_remote(function_names, []);
-  log({ns});
+  log({
+    ns,
+  });
+}
