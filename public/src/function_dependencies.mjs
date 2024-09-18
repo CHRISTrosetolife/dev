39f57dfa8c25effe1ebf231b4d26daa56b3c4003
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 15:03:48 -0400

    2024-09-18T19:03:47.882Z function_auto_after function_dependencies

--- public/src/function_dependencies.mjs
+++ public/src/function_dependencies.mjs
@@ -2,5 +2,5 @@ import { each_object } from "./each_object.mjs";
 import { function_imports } from "./function_imports.mjs";
 export async function function_dependencies(function_name) {
   let map = await function_imports(function_name);
-  each_object(object, (key, value) => {});
+  each_object(map, (function_name, m) => {});
 }
