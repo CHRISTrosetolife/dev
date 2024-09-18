85e0c9c560b7b32e91df3f54c546f84435e1e464
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 06:34:34 -0400

    2024-09-18T10:34:33.433Z function_auto_after js_template_to_function

--- public/src/js_template_to_function.mjs
+++ public/src/js_template_to_function.mjs
@@ -2,15 +2,14 @@ import { each } from "./each.mjs";
 import { js_code_call_args } from "./js_code_call_args.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { object_replace } from "./object_replace.mjs";
-import { log } from "./log.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_template_to_function(ast) {
   return js_visit_node(ast, "TemplateLiteral", (v) => {
     let { node } = v;
     let { quasis } = node;
+    each(list, (item) => {});
     let e = js_parse_expression(js_code_call_args(each.name, []));
     object_replace(node, e);
     object_replace(node);
-    log(node);
   });
 }
