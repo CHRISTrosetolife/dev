7f4d05990bb24b52d48dcddc3502f5ec7d711877
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 03:34:41 -0400

    2024-09-18T07:34:40.884Z function_auto_after list_shuffle

--- public/src/list_shuffle.mjs
+++ public/src/list_shuffle.mjs
@@ -1,6 +1,10 @@
 import { list_shuffle_from } from "./list_shuffle_from.mjs";
 import { list_index_last } from "./list_index_last.mjs";
+import { list_empty_is } from "./list_empty_is.mjs";
 export function list_shuffle(scrambled) {
+  if (list_empty_is(scrambled)) {
+    return;
+  }
   let low = 0;
   let high = list_index_last(scrambled);
   list_shuffle_from(low, high, scrambled);
