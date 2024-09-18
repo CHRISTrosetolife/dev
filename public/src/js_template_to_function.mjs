4fbd5a7f698e117d31cd2c6392eab79e335a8efd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 06:33:33 -0400

    2024-09-18T10:33:32.924Z function_auto_after js_template_to_function

--- public/src/js_template_to_function.mjs
+++ public/src/js_template_to_function.mjs
@@ -1,9 +1,11 @@
+import { object_replace } from "./object_replace.mjs";
 import { log } from "./log.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_template_to_function(ast) {
   return js_visit_node(ast, "TemplateLiteral", (v) => {
     let { node } = v;
     let { quasis } = node;
+    object_replace(node);
     log(node);
   });
 }
