1fb96f5543de7952b723c10c782002c9b09d5768
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 15:05:50 -0400

    2024-09-18T19:05:49.754Z function_auto_after function_dependencies

--- public/src/function_dependencies.mjs
+++ public/src/function_dependencies.mjs
@@ -1,3 +1,4 @@
+import { each } from "./each.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 import { each_object } from "./each_object.mjs";
 import { function_imports } from "./function_imports.mjs";
@@ -5,7 +6,13 @@ export async function function_dependencies(function_name) {
   let map = await function_imports(function_name);
   list_adder_unique((la) => {
     each_object(map, (function_name, m) => {
-      let { sources, imports } = m;
+      la();
+      let vertices = list_adder_unique((la) => {
+        each_object(map, (function_name, mapped) => {
+          let { sources, imports } = mapped;
+          each([function_name, sources, imports], la);
+        });
+      });
     });
   });
 }
