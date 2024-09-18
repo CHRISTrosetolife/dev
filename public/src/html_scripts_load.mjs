b057aa38196aabe786e653e72cd294373719c71b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 06:08:37 -0400

    2024-09-18T10:08:35.618Z function_auto_after html_scripts_load

--- public/src/html_scripts_load.mjs
+++ public/src/html_scripts_load.mjs
@@ -4,7 +4,7 @@ import { html_script_generate } from "./html_script_generate.mjs";
 import { function_name_generated_combine } from "./function_name_generated_combine.mjs";
 import { each_async } from "./each_async.mjs";
 export async function html_scripts_load(root, scripts) {
-  await each_async([scripts], async (script) => {
+  await each_async(scripts, async (script) => {
     let name = function_name_generated_combine(html_script_generate, script);
     log({
       name,
