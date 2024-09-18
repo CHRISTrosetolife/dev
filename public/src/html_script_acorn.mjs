68fe45fe0a5a74263a070840ad82707bbd2135ca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 05:43:07 -0400

    2024-09-18T09:43:05.459Z html_script_generate acorn '<script       src=https://cdnjs.cloudflare.com/ajax/libs/acorn/8.11.3/acorn.js       integrity=sha512-rlm77cSR5crHjxp0RrQwp9mCVada2Tp0GVzVm/M59Z45daZf7C24VNGVbg58swEnmIR2Oq65qHPr3mZqTn8iag==       crossorigin=anonymous       referrerpolicy=no-referrer     ></script>'

---
+++ public/src/html_script_acorn.mjs
@@ -0,0 +1,17 @@
+import { html_attribute_set } from "./html_attribute_set.mjs";
+import { html_element } from "./html_element.mjs";
+export function html_script_acorn(parent) {
+  let c = html_element(parent, "script");
+  html_attribute_set(
+    c,
+    "src",
+    "https://cdnjs.cloudflare.com/ajax/libs/acorn/8.11.3/acorn.js",
+  );
+  html_attribute_set(
+    c,
+    "integrity",
+    "sha512-rlm77cSR5crHjxp0RrQwp9mCVada2Tp0GVzVm/M59Z45daZf7C24VNGVbg58swEnmIR2Oq65qHPr3mZqTn8iag==",
+  );
+  html_attribute_set(c, "crossorigin", "anonymous");
+  html_attribute_set(c, "referrerpolicy", "no-referrer");
+}
