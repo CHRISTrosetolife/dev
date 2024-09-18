d2b310dbebf80dd40af8f40bbcd2ffd866fb4735
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 03:36:46 -0400

    2024-09-18T07:36:45.894Z function_auto_after log_error_write

--- public/src/log_error_write.mjs
+++ public/src/log_error_write.mjs
@@ -1,6 +1,5 @@
 import { log_write } from "./log_write.mjs";
 import { log_error_generic } from "./log_error_generic.mjs";
 export function log_error_write(message) {
-  let fn = log_write;
-  log_error_generic(fn, message);
+  log_error_generic(log_write, message);
 }
