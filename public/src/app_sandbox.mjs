aeee38e01cff3e734aab7727a916feb2fdac9145
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 17:09:43 -0400

    2024-09-18T21:09:42.305Z function_auto_after app_sandbox

--- public/src/app_sandbox.mjs
+++ public/src/app_sandbox.mjs
@@ -1,22 +1,4 @@
-import { html_document_body } from "./html_document_body.mjs";
-import { app_gs_tiles } from "./app_gs_tiles.mjs";
-import { html_style } from "./html_style.mjs";
-import { html_style_width_full } from "./html_style_width_full.mjs";
-import { html_style_centered } from "./html_style_centered.mjs";
-import { html_div } from "./html_div.mjs";
-export function app_sandbox() {
-  let root = html_document_body();
-  let container_outer = html_div(root);
-  html_style_centered(container_outer);
-  html_style_width_full(container_outer);
-  let container_inner = html_div(container_outer);
-  html_style(container_inner, {
-    position: "relative",
-  });
-  app_gs_tiles(container_inner, container_inner, {
-    player: {
-      x: 5,
-      y: 8,
-    },
-  });
+import { html_scripts_load } from "./html_scripts_load.mjs";
+export async function app_sandbox() {
+  await html_scripts_load(root, ["axios"]);
 }
