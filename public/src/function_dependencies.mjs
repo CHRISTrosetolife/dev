7655a82af2a66b94dd86eb6ebc0bdcd58d7042e4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 15:18:08 -0400

    2024-09-18T19:18:07.954Z function_auto_after function_dependencies

--- public/src/function_dependencies.mjs
+++ public/src/function_dependencies.mjs
@@ -1,7 +1,15 @@
+import { each } from "./each.mjs";
+import { list_adder_unique } from "./list_adder_unique.mjs";
 import { function_imports } from "./function_imports.mjs";
 import { object_properties } from "./object_properties.mjs";
 export async function function_dependencies(function_name) {
   let map = await function_imports(function_name);
   let dependency_names = object_properties(map);
+  let vertices = list_adder_unique((la) => {
+    each_object_values(map, (function_name, mapped) => {
+      let { sources } = mapped;
+      each(sources, la);
+    });
+  });
   return dependency_names;
 }
