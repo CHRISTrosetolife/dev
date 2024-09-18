2b4bec945f74aaeacccb00cd8a434d515f224fa0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 14:57:47 -0400

    2024-09-18T18:57:47.133Z function_auto_after git_fix

--- public/src/git_fix.mjs
+++ public/src/git_fix.mjs
@@ -2,7 +2,10 @@ import { command_line_git_multiple } from "./command_line_git_multiple.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function git_fix() {
   let commands = string_combine_multiple([
-    "\ncheckout master\nfetch\nrebase origin/master\npush origin master:master\n  ",
+    "checkout master",
+    "fetch",
+    "rebase origin/master",
+    "push origin master:master",
   ]);
   return await command_line_git_multiple(commands);
 }
