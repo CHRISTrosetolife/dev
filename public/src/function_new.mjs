4f4adba7936e965ea2a08484742bbab5e17102b9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 06:00:52 -0400

    2024-09-18T10:00:52.204Z function_auto_after function_new

--- public/src/function_new.mjs
+++ public/src/function_new.mjs
@@ -1,8 +1,14 @@
+import { function_open } from "./function_open.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
+import { file_exists } from "./file_exists.mjs";
 import { file_write } from "./file_write.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 export async function function_new(function_name) {
   assert_arguments_length(arguments, 1);
+  if (await file_exists(function_name)) {
+    await function_open(function_name);
+    return;
+  }
   let open = true;
   await function_new_generic(
     function_name,
