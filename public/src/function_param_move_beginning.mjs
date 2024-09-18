fb2735a2d6d9141cd78ca58cf27513c9ce2b394f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 06:05:42 -0400

    2024-09-18T10:05:42.159Z function_auto_after function_param_move_beginning

--- public/src/function_param_move_beginning.mjs
+++ public/src/function_param_move_beginning.mjs
@@ -1,1 +1,4 @@
-export function function_param_move_beginning() {}
+import { function_param_move } from "./function_param_move.mjs";
+export async function function_param_move_beginning() {
+  await function_param_move(function_name, param_index, param_index_new);
+}
