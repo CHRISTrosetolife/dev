376c27c3d773555cf615f02df94266210ca6cc72
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 17:47:48 -0400

    2024-09-18T21:47:47.493Z function_auto_after app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -9,12 +9,14 @@ import { html_scripts_load } from "./html_scripts_load.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { tautology } from "./tautology.mjs";
+import { html_div } from "./html_div.mjs";
 export async function app_code() {
   let root = html_style_default_initialize();
   await html_scripts_load(root, ["axios", "acorn", "astring"]);
   let ns = await function_run_remote(function_names.name, []);
   let input = html_input_width_full_focus(root);
   html_on_keydown(input, () => {});
+  let results = html_div(root);
   let filtered = list_filter(ns, tautology);
   let taken = list_take(filtered, 50);
   each(taken, (item) => {
