cbed154d3b1411aa3a76fd342a4928bc28e957b1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 05:35:17 -0400

    2024-09-18T09:35:17.007Z r app_identifiewr app_identifier

--- public/src/app_identifier.mjs
+++ public/src/app_identifier.mjs
@@ -1,6 +1,6 @@
 import { string_combine } from "./string_combine.mjs";
 import { app_prefix } from "./app_prefix.mjs";
-export function app_identifiewr(name) {
+export function app_identifier(name) {
   let prefix = app_prefix();
   let name_prefixed = string_combine(prefix, name);
   return name_prefixed;
--- public/src/app_new.mjs
+++ public/src/app_new.mjs
@@ -1,10 +1,10 @@
-import { app_identifiewr } from "./app_identifiewr.mjs";
+import { app_identifier } from "./app_identifier.mjs";
 import { file_write } from "./file_write.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 import { app_html_generate } from "./app_html_generate.mjs";
 export async function app_new(name) {
   await app_html_generate(name);
-  let name_prefixed = app_identifiewr(name);
+  let name_prefixed = app_identifier(name);
   let open = true;
   await function_new_generic(
     name_prefixed,
