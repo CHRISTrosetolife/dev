9f383900881062b2d178e260e8d8cce87236058f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 05:41:13 -0400

    2024-09-18T09:41:13.187Z function_auto_after function_delete_if_starts_with

--- public/src/function_delete_if_starts_with.mjs
+++ public/src/function_delete_if_starts_with.mjs
@@ -1,6 +1,10 @@
 import { function_names } from "./function_names.mjs";
 import { each_async } from "./each_async.mjs";
+import { list_filter } from "./list_filter.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
 export async function function_delete_if_starts_with(prefix) {
   let ns = await function_names();
-  await each_async(list, async (item) => {});
+  let starts_with = list_filter(ns, (n) => string_starts_with(n, prefix));
+  return starts_with;
+  await each_async(ns, async (n) => {});
 }
