bfb06999180c92de5a676b818fe8cac7dfc4b207
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 15:03:09 -0400

    2024-09-18T19:03:09.354Z function_auto_after function_dependencies

--- public/src/function_dependencies.mjs
+++ public/src/function_dependencies.mjs
@@ -1,1 +1,4 @@
-export function function_dependencies() {}
+import { function_imports } from "./function_imports.mjs";
+export async function function_dependencies(function_name) {
+  await function_imports(function_name);
+}
