8cfd23c04f243ba528b166a3a6b02bf2b5731732
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 15:37:40 -0400

    2024-09-18T19:37:39.470Z function_auto_after function_declaration

--- public/src/function_declaration.mjs
+++ public/src/function_declaration.mjs
@@ -2,5 +2,5 @@ import { js_declaration_single } from "./js_declaration_single.mjs";
 import { function_parse } from "./function_parse.mjs";
 export async function function_declaration(function_name) {
   let ast = await function_parse(function_name);
-  js_declaration_single(ast);
+  return js_declaration_single(ast);
 }
