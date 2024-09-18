97fe171055d83f847fed70c081fd75d270f54b35
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 06:12:29 -0400

    2024-09-18T10:12:25.853Z function_auto_after function_open_app_html

--- public/src/function_open_app_html.mjs
+++ public/src/function_open_app_html.mjs
@@ -1,2 +1,4 @@
+import { app_html_path } from "./app_html_path.mjs";
 export function function_open_app_html(name) {
-    let output_path = app_html_path(name);}
+  let output_path = app_html_path(name);
+}
