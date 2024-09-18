81b37b47468b55523941b2f433c1ab1c5242d777
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 06:39:28 -0400

    2024-09-18T10:39:27.955Z function_auto_after js_template_to_function

--- public/src/js_template_to_function.mjs
+++ public/src/js_template_to_function.mjs
@@ -8,15 +8,15 @@ import { js_visit_node } from "./js_visit_node.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function js_template_to_function(ast) {
   return js_visit_node(ast, "TemplateLiteral", (v) => {
+    let e = js_parse_expression(
+      js_code_call_args(string_combine_multiple.name, []),
+    );
+    let { arguments: a } = e;
     let { node } = v;
     let { quasis } = node;
     log(node);
     each(quasis, log);
     each_index(quasis, (q, qi) => {});
-    let e = js_parse_expression(
-      js_code_call_args(string_combine_multiple.name, []),
-    );
-    let { arguments: a } = e;
     log({
       e,
     });
