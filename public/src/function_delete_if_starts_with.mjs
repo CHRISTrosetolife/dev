febd81c7f8b08b5b2a7634b1f8b84e7763d2e2d6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 05:40:22 -0400

    2024-09-18T09:40:21.645Z function_auto_after function_delete_if_starts_with

--- public/src/function_delete_if_starts_with.mjs
+++ public/src/function_delete_if_starts_with.mjs
@@ -1,1 +1,4 @@
-export function function_delete_if_starts_with(prefix) {}
+import { each_async } from "./each_async.mjs";
+export async function function_delete_if_starts_with(prefix) {
+  await each_async(list, async (item) => {});
+}
