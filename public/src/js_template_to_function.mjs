30a74b8ffb324cdd768a9c414d0d8b495bec4202
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 06:32:47 -0400

    2024-09-18T10:32:46.361Z function_auto_after js_template_to_function

--- public/src/js_template_to_function.mjs
+++ public/src/js_template_to_function.mjs
@@ -3,6 +3,6 @@ import { js_visit_node } from "./js_visit_node.mjs";
 export function js_template_to_function(ast) {
   return js_visit_node(ast, "TemplateLiteral", (v) => {
     let { node } = v;
-    log({});
+    log(node);
   });
 }
