03b1ed607efd353b51d42bc8fc6e986b7262a40d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 06:40:33 -0400

    2024-09-18T10:40:33.167Z function_auto_after js_code_string

--- public/src/js_code_string.mjs
+++ public/src/js_code_string.mjs
@@ -1,7 +1,7 @@
 import { js_string } from "./js_string.mjs";
 import { js_unparse } from "./js_unparse.mjs";
 export function js_code_string(s) {
-  let code2 = js_string(s);
-  let unparsed = js_unparse(code2);
+  let code = js_string(s);
+  let unparsed = js_unparse(code);
   return unparsed;
 }
