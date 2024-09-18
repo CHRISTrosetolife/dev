571da0f943e5d5ab209669263734d98de9402615
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 06:23:38 -0400

    2024-09-18T10:23:38.167Z function_auto_after function_run

--- public/src/function_run.mjs
+++ public/src/function_run.mjs
@@ -1,15 +1,6 @@
-import { function_run_remote } from "./function_run_remote.mjs";
 import { function_import } from "./function_import.mjs";
-import { web_is } from "./web_is.mjs";
 export async function function_run(function_name, args) {
-  let result;
-  if (0) {
-    if (web_is()) {
-      result = await function_run_remote(function_name, args);
-      return;
-    }
-  }
   let imported_function = await function_import(function_name);
-  result = await imported_function(...args);
+  let result = await imported_function(...args);
   return result;
 }
