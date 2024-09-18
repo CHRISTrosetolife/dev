14bd86caa7a6005a7396160d011b84ee23c0da6e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 05:35:47 -0400

    2024-09-18T09:35:46.686Z function_auto_after function_open_app

--- public/src/function_open_app.mjs
+++ public/src/function_open_app.mjs
@@ -1,5 +1,5 @@
 import { app_identifier } from "./app_identifier.mjs";
 import { app_name } from "./app_name.mjs";
 export function function_open_app(app_name) {
-  let name_prefixed = app_identifier(name);
+  let name_prefixed = app_identifier(app_name);
 }
