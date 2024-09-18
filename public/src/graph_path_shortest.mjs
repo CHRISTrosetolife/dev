c5a07449237b757784fbc938155cb888b857d4d8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 03:31:36 -0400

    2024-09-18T07:31:36.215Z function_auto_after graph_path_shortest

--- public/src/graph_path_shortest.mjs
+++ public/src/graph_path_shortest.mjs
@@ -6,10 +6,6 @@ import { each_index } from "./each_index.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function graph_path_shortest(vertices, edge_lambda, from, to) {
   assert_arguments_length(arguments, 4);
-  let neighbors_get = (v) => {
-    let neighbors = graph_neighbors_undirected(edges, v);
-    return neighbors;
-  };
   let edges = list_adder((la) =>
     each_index(vertices, (a, ai) =>
       each_index(vertices, (b, bi) => {
@@ -22,6 +18,10 @@ export function graph_path_shortest(vertices, edge_lambda, from, to) {
       }),
     ),
   );
+  let neighbors_get = (v) => {
+    let neighbors = graph_neighbors_undirected(edges, v);
+    return neighbors;
+  };
   return graph_path_shortest_neighbors(
     (v) => list_index(vertices, v),
     neighbors_get,
