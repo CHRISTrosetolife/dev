8ded870bcb182debca22ed45d701713ba104c5e4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 06:13:39 -0400

    2024-09-18T10:13:38.263Z function_auto_after file_open

--- public/src/file_open.mjs
+++ public/src/file_open.mjs
@@ -1,9 +1,12 @@
+import { file_exists } from "./file_exists.mjs";
+import { assert_async } from "./assert_async.mjs";
 import { command_line_exec_run } from "./command_line_exec_run.mjs";
 import { web_is } from "./web_is.mjs";
 export async function file_open(file_name) {
   if (web_is()) {
     return;
   }
+  await assert_async(file_exists, [file_name]);
   await command_line_exec_run(`code ${file_name}`);
   return;
   await command_line_exec_run(`start notepad++ ${file_name}`);
