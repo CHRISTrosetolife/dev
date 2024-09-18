d8bd9c165335520bea6114af0c744c5eebe7e557
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 17:11:25 -0400

    2024-09-18T21:11:24.572Z function_auto_after app_sandbox

--- public/src/app_sandbox.mjs
+++ public/src/app_sandbox.mjs
@@ -2,7 +2,9 @@ import { server_port } from "./server_port.mjs";
 import { url_localhost } from "./url_localhost.mjs";
 import { http_post } from "./http_post.mjs";
 import { html_scripts_load } from "./html_scripts_load.mjs";
+import { html_document_body } from "./html_document_body.mjs";
 export async function app_sandbox() {
+  let root = html_document_body();
   await html_scripts_load(root, ["axios"]);
   await http_post(url_localhost(server_port()), {
     data: "test post",
