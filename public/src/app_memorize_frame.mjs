1019d73de7e15c42352eff08ac094fae4a5d2916
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 17:42:19 -0400

    2024-09-18T21:42:19.131Z function_auto_after app_memorize_frame

--- public/src/app_memorize_frame.mjs
+++ public/src/app_memorize_frame.mjs
@@ -1,13 +1,13 @@
+import { html_on_keydown } from "./html_on_keydown.mjs";
 import { html_on_remove } from "./html_on_remove.mjs";
 import { app_memorize_on_keydown } from "./app_memorize_on_keydown.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
-import { html_on } from "./html_on.mjs";
 import { app_memorize_refresh_memorize } from "./app_memorize_refresh_memorize.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 export async function app_memorize_frame(context) {
   await app_memorize_refresh_memorize(context);
   let component = html_document_body();
-  html_on(component, "keydown", on_keydown);
+  html_on_keydown(component, on_keydown);
   async function on_keydown(e) {
     let { keyCode } = e;
     let c = String.fromCharCode(keyCode);
---
+++ public/src/html_on_keydown.mjs
@@ -0,0 +1,4 @@
+import { html_on } from "./html_on.mjs";
+export function html_on_keydown(component, on_keydown) {
+  html_on(component, "keydown", on_keydown);
+}
