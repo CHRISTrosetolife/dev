bc2a2f72fabc5b811996a7fc588ff3b16f8f7065
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 05:45:14 -0400

    2024-09-18T09:45:12.950Z function_auto_after app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,4 +1,6 @@
+import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { html_script_acorn } from "./html_script_acorn.mjs";
 export async function app_code() {
+  let root = html_style_default_initialize();
   html_script_acorn(root);
 }
