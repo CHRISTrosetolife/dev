a2fd6a0b5a9118a7d143c0e2ec8aae0005d7ea5e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 06:14:01 -0400

    2024-09-18T10:14:01.280Z function_auto_after function_open_app_html

--- public/src/function_open_app_html.mjs
+++ public/src/function_open_app_html.mjs
@@ -1,4 +1,6 @@
+import { file_open_unawait } from "./file_open_unawait.mjs";
 import { app_html_path } from "./app_html_path.mjs";
 export function function_open_app_html(name) {
   let output_path = app_html_path(name);
+  file_open_unawait(output_path);
 }
