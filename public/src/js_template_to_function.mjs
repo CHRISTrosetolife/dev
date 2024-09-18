a3bc8b97cc3861ccf95a6a6f60591ffb85fb9ec0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 06:32:19 -0400

    2024-09-18T10:32:18.595Z function_auto_after js_template_to_function

--- public/src/js_template_to_function.mjs
+++ public/src/js_template_to_function.mjs
@@ -1,1 +1,14 @@
-export function js_template_to_function(ast) {}
+import { js_to_block_generic } from "./js_to_block_generic.mjs";
+import { js_visit_node } from "./js_visit_node.mjs";
+export function js_template_to_function(ast) {
+  return js_visit_node(ast, "IfStatement", (v) => {
+    let { node } = v;
+    let properties = ["consequent", "alternate"];
+    js_to_block_generic(node, properties, (type, p) => {
+      if (p === "alternate") {
+        return type === "IfStatement";
+      }
+      return false;
+    });
+  });
+}
