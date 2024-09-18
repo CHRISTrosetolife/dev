d856125a68a035774a9ee1a1f11c6f6896b48389
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-18 03:31:14 -0400

    2024-09-18T07:31:13.015Z pm graph_neighbors_generic 0 1

--- public/src/graph_neighbors_directed.mjs
+++ public/src/graph_neighbors_directed.mjs
@@ -2,5 +2,5 @@ import { list_first } from "./list_first.mjs";
 import { graph_neighbors_generic } from "./graph_neighbors_generic.mjs";
 export function graph_neighbors_directed(edges, v) {
   let lambda_filter = (e) => list_first(e) === v;
-  return graph_neighbors_generic(edges, lambda_filter, v);
+  return graph_neighbors_generic(lambda_filter, edges, v);
 }
--- public/src/graph_neighbors_undirected.mjs
+++ public/src/graph_neighbors_undirected.mjs
@@ -2,5 +2,5 @@ import { graph_neighbors_generic } from "./graph_neighbors_generic.mjs";
 import { list_includes } from "./list_includes.mjs";
 export function graph_neighbors_undirected(edges, v) {
   let lambda_filter = (e) => list_includes(e, v);
-  return graph_neighbors_generic(edges, lambda_filter, v);
+  return graph_neighbors_generic(lambda_filter, edges, v);
 }
