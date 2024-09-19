1dfe6f9ef66414c49b2d195d46192a532ad4e60f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-19 09:02:47 -0400

    2024-09-19T13:02:45.735Z sermon_format 1cor7

--- public/src/sermon_format.mjs
+++ public/src/sermon_format.mjs
@@ -1,6 +1,9 @@
 import { file_read } from "./file_read.mjs";
 import { sermon_path } from "./sermon_path.mjs";
+import { string_split_newline } from "./string_split_newline.mjs";
 export async function sermon_format(name) {
   let sermon_path_file = sermon_path(name);
   let text = await file_read(sermon_path_file);
+  let lines = string_split_newline(text);
+  return lines;
 }
