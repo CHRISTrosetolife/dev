42167a45047625d1761498315911ec1b24e1197a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-19 09:03:32 -0400

    2024-09-19T13:03:32.106Z function_auto_after sermon_format

--- public/src/sermon_format.mjs
+++ public/src/sermon_format.mjs
@@ -2,10 +2,12 @@ import { string_replace } from "./string_replace.mjs";
 import { file_read } from "./file_read.mjs";
 import { sermon_path } from "./sermon_path.mjs";
 import { string_split_newline } from "./string_split_newline.mjs";
+import { list_filter } from "./list_filter.mjs";
 export async function sermon_format(name) {
   let sermon_path_file = sermon_path(name);
   let text = await file_read(sermon_path_file);
   text = string_replace(text, "\r", "");
   let lines = string_split_newline(text);
+  list_filter(lines, () => {});
   return lines;
 }
