cc86be04699debea7ea005493e86ac6e0179ba14
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-19 13:54:22 -0400

    2024-09-19T17:54:21.498Z function_auto_after bible_book_prefix_to_number

--- public/src/bible_book_prefix_to_number.mjs
+++ public/src/bible_book_prefix_to_number.mjs
@@ -11,7 +11,7 @@ export async function bible_book_prefix_to_number(bible_folder, book_prefix) {
   let predicate = string_starts_with_curry(book_prefix);
   let mapper = string_case_lower;
   book_prefix = mapper(book_prefix);
-  let book = list_find_mapped(keys, mapper, predicate, book_prefix);
+  let book = list_find_mapped(keys, mapper, predicate);
   log({
     book,
   });
