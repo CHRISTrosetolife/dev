e333bae4cf689aa951c4f83035a9d4c8b5f23026
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-19 13:51:46 -0400

    2024-09-19T17:51:45.720Z function_auto_after list_find_mapped

---
+++ public/src/list_find_mapped.mjs
@@ -0,0 +1,10 @@
+import { list_find } from "./list_find.mjs";
+import { list_find_index } from "./list_find_index.mjs";
+import { list_map } from "./list_map.mjs";
+export function list_find_mapped(target_mapped, predicate, list, mapper) {
+  let keys_lower = list_map(matches, mapper);
+  target_mapped = mapper(target_mapped);
+  let index = list_find_index(keys_lower, predicate);
+  let book = list_find(list, index);
+  return book;
+}
