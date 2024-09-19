80129e52af56a540e0219212e9d01a2e2a09f283
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-19 13:20:25 -0400

    2024-09-19T17:20:24.788Z function_auto_after js_code_function_declare

---
+++ public/src/js_code_function_declare.mjs
@@ -0,0 +1,16 @@
+import { js_code_parenthesis } from "./js_code_parenthesis.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function js_code_function_declare(
+  function_name,
+  args_string,
+  body_string,
+) {
+  return string_combine_multiple([
+    "function ",
+    function_name,
+    js_code_parenthesis(args_string),
+    " {\n  ",
+    body_string,
+    "\n}",
+  ]);
+}
