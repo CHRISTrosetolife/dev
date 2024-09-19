7012fc339c9da6c5c1636186e15a718568d69ac8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-19 13:22:57 -0400

    2024-09-19T17:22:57.137Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -253,10 +253,13 @@ export function js_dollar(ast) {
       }
       if (remaining === "i") {
         if (list_is(parent)) {
+          let statement = js_parse_first(js_code_if());
+          log({
+            statement,
+          });
           let next = list_next(parent, node);
           list_remove(parent, next);
           let index = list_index(parent, node);
-          let statement = js_parse_first(js_code_if());
         }
       }
       if (remaining === "st") {
