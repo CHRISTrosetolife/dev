0cb2deaa22351bb9b64ecf6196b1560cbfb81ac8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-19 09:04:44 -0400

    2024-09-19T13:04:43.535Z function_auto_after sermon_format

---
+++ public/src/sermon_format.mjs
@@ -0,0 +1,20 @@
+import { string_first } from "./string_first.mjs";
+import { string_replace } from "./string_replace.mjs";
+import { file_read } from "./file_read.mjs";
+import { sermon_path } from "./sermon_path.mjs";
+import { string_split_newline } from "./string_split_newline.mjs";
+import { list_filter } from "./list_filter.mjs";
+import { string_empty_is } from "./string_empty_is.mjs";
+export async function sermon_format(name) {
+  let sermon_path_file = sermon_path(name);
+  let text = await file_read(sermon_path_file);
+  text = string_replace(text, "\r", "");
+  let lines = string_split_newline(text);
+  list_filter(lines, (line) => {
+    if (string_empty_is(line)) {
+      return true;
+    }
+    let first = string_first(line);
+  });
+  return lines;
+}
