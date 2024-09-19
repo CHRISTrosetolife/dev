b5f3f1e4f9fb9206bc34de90af0d20cb1bd81365
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-19 13:51:04 -0400

    2024-09-19T17:51:03.531Z function_auto_after bible_book_prefix_to_number

--- public/src/bible_book_prefix_to_number.mjs
+++ public/src/bible_book_prefix_to_number.mjs
@@ -1,9 +1,7 @@
-import { list_map } from "./list_map.mjs";
-import { list_find_index } from "./list_find_index.mjs";
+import { list_find_mapped } from "./list_find_mapped.mjs";
 import { log } from "./log.mjs";
 import { bible_book_name_to_number } from "./bible_book_name_to_number.mjs";
 import { string_starts_with_curry } from "./string_starts_with_curry.mjs";
-import { list_find } from "./list_find.mjs";
 import { bible_books_names_lookup_inverted } from "./bible_books_names_lookup_inverted.mjs";
 import { object_properties } from "./object_properties.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
@@ -12,18 +10,9 @@ export async function bible_book_prefix_to_number(bible_folder, book_prefix) {
   let keys = object_properties(inverted);
   let predicate = string_starts_with_curry(book_prefix);
   let mapper = string_case_lower;
-  let 
-  book= list_find_mapped(mapper, book_prefix, predicate, keys);
+  let book = list_find_mapped(mapper, book_prefix, predicate, keys);
   log({
     book,
   });
   return await bible_book_name_to_number(bible_folder, book);
 }
-function list_find_mapped(mapper, book_prefix, predicate, keys) {
-    let keys_lower = list_map(matches, mapper);
-    book_prefix = mapper(book_prefix);
-    let index = list_find_index(keys_lower, predicate);
-    let book = list_find(keys, index);
-    return book
-}
-
