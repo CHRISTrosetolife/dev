36812a7832ab539d70a330659ad4bd15b744c275
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-19 13:55:54 -0400

    2024-09-19T17:55:53.516Z function_auto_after list_find_mapped

--- public/src/list_find_mapped.mjs
+++ public/src/list_find_mapped.mjs
@@ -1,9 +1,9 @@
-import { list_find } from "./list_find.mjs";
+import { list_get } from "./list_get.mjs";
 import { list_find_index } from "./list_find_index.mjs";
 import { list_map } from "./list_map.mjs";
 export function list_find_mapped(list, mapper, predicate) {
   let keys_lower = list_map(list, mapper);
   let index = list_find_index(keys_lower, predicate);
-  let book = list_find(list, index);
+  let book = list_get(list, index);
   return book;
 }
