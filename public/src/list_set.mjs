b6912cea125c6e84898d2d1ee15a6f0f7e3bb960
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-19 13:31:50 -0400

    2024-09-19T17:31:49.500Z function_auto_after list_set

--- public/src/list_set.mjs
+++ public/src/list_set.mjs
@@ -1,3 +1,5 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function list_set(list, index, value) {
+  assert_arguments_length(arguments, 3);
   list[index] = value;
 }
