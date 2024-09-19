4f602a3ae4788ab720bb702024bd9f68863066f0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-19 09:01:33 -0400

    2024-09-19T13:01:32.932Z function_auto_after sermon_new

---
+++ public/src/sermon_new.mjs
@@ -0,0 +1,8 @@
+import { sermon_path } from "./sermon_path.mjs";
+import { file_open } from "./file_open.mjs";
+import { file_write } from "./file_write.mjs";
+export async function sermon_new(name) {
+  let sermon_path_file = sermon_path(name);
+  await file_write(sermon_path_file, "TODO");
+  await file_open(sermon_path_file);
+}
