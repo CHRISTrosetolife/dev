3435e04e515285a250970ff3dee69f73e890f73f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-19 13:26:29 -0400

    2024-09-19T17:26:29.126Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -256,12 +256,13 @@ export function js_dollar(ast) {
         let s1 = list_get_end_1(stack);
         if (list_is(s1)) {
           let statement = js_parse_first(js_code_if());
+          let next = list_next(parent, node);
+          let consequent = statement;
           log({
             statement,
             parent,
           });
           if (0) {
-            let next = list_next(parent, node);
             list_remove(parent, next);
             let index = list_index(parent, node);
           }
