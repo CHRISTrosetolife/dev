275c190a740ec9fa093e0be62bf9a988270e9240
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-19 13:29:58 -0400

    2024-09-19T17:29:57.991Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -261,10 +261,10 @@ export function js_dollar(ast) {
           let { consequent } = statement;
           let { body } = consequent;
           list_add(body, next);
+          let index = list_index(s1, parent);
           list_set(s1, next);
           if (0) {
             list_remove(parent, next);
-            let index = list_index(parent, node);
           }
         }
       }
