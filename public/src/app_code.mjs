e17441652a54d916f1898a02e02bd3252e6b318b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-19 12:57:38 -0400

    2024-09-19T16:57:37.437Z function_auto_after app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -23,6 +23,7 @@ export async function app_code() {
   let root = html_style_default_initialize();
   await html_scripts_load(root, ["axios", "acorn", "astring"]);
   let lookup = html_hash_lookup();
+  $llookup;
   let ns = await function_run_remote(function_names.name, []);
   let input = html_input_width_full_focus(root);
   let results = html_div(root);
