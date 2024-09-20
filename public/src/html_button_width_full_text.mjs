0605f0c7ec5985161e34fbe5cb1535d78af05324
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 12:47:28 -0400

    2024-09-20T16:47:28.098Z function_auto_after html_button_width_full_text

--- public/src/html_button_width_full_text.mjs
+++ public/src/html_button_width_full_text.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
 import { html_button_width_full } from "./html_button_width_full.mjs";
 export function html_button_width_full_text(parent, button_text) {
+  assert_arguments_length(arguments, 2);
   let button_previous = html_button_width_full(parent);
   html_inner_set(button_previous, button_text);
   return button_previous;
