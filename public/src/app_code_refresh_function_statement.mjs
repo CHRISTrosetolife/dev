af2a312272371cd4910878fd41871160d6fc2c96
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 13:27:19 -0400

    2024-09-20T17:27:18.604Z function_auto_after app_code_refresh_function_statement

--- public/src/app_code_refresh_function_statement.mjs
+++ public/src/app_code_refresh_function_statement.mjs
@@ -1,3 +1,4 @@
+import { html_div } from "./html_div.mjs";
 import { js_unparse } from "./js_unparse.mjs";
 import { equal } from "./equal.mjs";
 import { html_div_text } from "./html_div_text.mjs";
@@ -8,6 +9,7 @@ export function app_code_refresh_function_statement(root, statement) {
     let { type: type_e } = expression;
     if (equal(type_e, "AwaitExpression")) {
       let { argument } = expression;
+      html_div(root, "⌛ ");
       html_div_text(root, "⌛ ");
       app_code_refresh_function_statement(root, argument);
     }
