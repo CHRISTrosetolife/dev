a714f54051b222914bca24db8dd44ceae4b63cbb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 12:54:17 -0400

    2024-09-20T16:54:16.773Z function_auto_after js_declaration_single_body

--- public/src/js_declaration_single_body.mjs
+++ public/src/js_declaration_single_body.mjs
@@ -1,8 +1,7 @@
+import { js_declaration_to_body } from "./js_declaration_to_body.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
 export function js_declaration_single_body(ast) {
   let d = js_declaration_single(ast);
-  let { body: body_d } = d;
-  let { body: b } = body_d;
-  let body_block = b;
+  let body_block = js_declaration_to_body(d);
   return body_block;
 }
---
+++ public/src/js_declaration_to_body.mjs
@@ -0,0 +1,6 @@
+export function js_declaration_to_body(d) {
+  let { body: body_d } = d;
+  let { body: b } = body_d;
+  let body_block = b;
+  return body_block;
+}
