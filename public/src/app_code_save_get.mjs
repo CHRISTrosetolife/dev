ce086eb3bdf674384f445bbe699c6f4f57891589
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 19:11:03 -0400

    2024-09-20T23:11:03.315Z function_auto_after app_code_save_get

--- public/src/app_code_save_get.mjs
+++ public/src/app_code_save_get.mjs
@@ -1,6 +1,5 @@
 import { app_save_get } from "./app_save_get.mjs";
 import { app_code } from "./app_code.mjs";
 export function app_code_save_get() {
-  let app_fn = app_code;
-  return app_save_get(app_fn);
+  return app_save_get(app_code);
 }
