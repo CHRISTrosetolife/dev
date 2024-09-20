e35871c2aa8f29e0045523c55c9dc81b05e3c3f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 08:50:57 -0400

    2024-09-20T12:50:52.208Z function_auto_after retry_while_error

--- public/src/retry_while_error.mjs
+++ public/src/retry_while_error.mjs
@@ -1,5 +1,6 @@
 import { log_error } from "./log_error.mjs";
 export async function retry_while_error(lambda) {
+  let break_on_success = true;
   let success = false;
   while (!success) {
     try {
