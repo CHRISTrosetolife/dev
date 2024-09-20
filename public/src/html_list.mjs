86a38cc7139a5eeee7284e43e441e441799464b4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 11:54:29 -0400

    2024-09-20T15:54:29.234Z function_auto_after html_list

--- public/src/html_list.mjs
+++ public/src/html_list.mjs
@@ -1,9 +1,9 @@
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { html_div_text } from "./html_div_text.mjs";
 import { each } from "./each.mjs";
-export function html_list(root, ns) {
+export function html_list(root, list) {
   assert_arguments_length(arguments, 2);
-  each(ns, (item) => {
+  each(list, (item) => {
     html_div_text(root, item);
   });
 }
