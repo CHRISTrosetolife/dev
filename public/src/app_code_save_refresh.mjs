4e5e8ed8ea0f53bcc57241dad7f79e8c5a4674ad
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 19:11:55 -0400

    2024-09-20T23:11:55.210Z function_auto_after app_code_save_refresh

--- public/src/app_code_save_refresh.mjs
+++ public/src/app_code_save_refresh.mjs
@@ -2,8 +2,7 @@ import { app_save_change } from "./app_save_change.mjs";
 import { app_code_refresh } from "./app_code_refresh.mjs";
 import { app_code } from "./app_code.mjs";
 export async function app_code_save_refresh(root, lambda) {
-  let app_fn = app_code;
-  let save = app_save_change(app_fn, lambda);
+  let save = app_save_change(app_code, lambda);
   await app_code_refresh(root);
   return save;
 }
