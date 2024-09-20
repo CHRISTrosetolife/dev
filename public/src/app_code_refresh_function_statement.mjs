f50c697b4546df7ebb4bc68e1c08f04d714e2925
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 14:21:47 -0400

    2024-09-20T18:21:47.271Z function_auto_after app_code_refresh_function_statement

--- public/src/app_code_refresh_function_statement.mjs
+++ public/src/app_code_refresh_function_statement.mjs
@@ -1,10 +1,10 @@
-import { html_a_style } from "./html_a_style.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { html_list } from "./html_list.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { js_unparse } from "./js_unparse.mjs";
 import { equal } from "./equal.mjs";
 import { list_map } from "./list_map.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function app_code_refresh_function_statement(parent, statement) {
   let { type } = statement;
   if (type === "ExpressionStatement") {
@@ -21,8 +21,7 @@ export function app_code_refresh_function_statement(parent, statement) {
     let { type: type_c } = callee;
     if (equal(type_c, "Identifier")) {
       let { name } = callee;
-      let callee_name = html_span_text(parent, name);
-      html_a_style(callee_name);
+      html_button_width_full_text_click(parent, name, () => {});
       html_span_text(parent, " (");
       let as = object_property_get(statement, "arguments");
       html_list(parent, list_map(as, js_unparse));
