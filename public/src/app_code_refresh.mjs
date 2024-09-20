00b7727311a1b40487dda35a142c0a5e02dd8908
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 13:11:58 -0400

    2024-09-20T17:11:57.053Z function_auto_after app_code_refresh

--- public/src/app_code_refresh.mjs
+++ public/src/app_code_refresh.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { list_map } from "./list_map.mjs";
 import { js_declaration_single_body } from "./js_declaration_single_body.mjs";
 import { app_code_save_delete_refresh_button_back } from "./app_code_save_delete_refresh_button_back.mjs";
@@ -57,9 +56,7 @@ export async function app_code_refresh(root) {
           let { type } = b;
           if (type === "ExpressionStatement") {
             let { expression } = b;
-            log({
-              expression,
-            });
+            let { type: type_e } = expression;
           }
           return js_unparse(b);
         }),
