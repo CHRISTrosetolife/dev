a815a711a35b8101551ec46ad5f043da952d3dc2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 14:09:40 -0400

    2024-09-20T18:09:40.164Z function_auto_after http_server

--- public/src/http_server.mjs
+++ public/src/http_server.mjs
@@ -4,5 +4,5 @@ import { command_line_exec } from "./command_line_exec.mjs";
 export async function http_server() {
   log(http_server.name);
   await command_line_exec("http-server");
-  log(string_combine_multiple(["ended"]));
+  log(string_combine_multiple([http_server.name, " ended"]));
 }
