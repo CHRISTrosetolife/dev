55273ebf95a4e07a14e951f775337ffe974f7466
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 15:51:22 -0400

    2024-09-20T19:51:21.377Z function_auto_after html_style_border_size

--- public/src/html_style_border_size.mjs
+++ public/src/html_style_border_size.mjs
@@ -1,6 +1,6 @@
 import { html_style } from "./html_style.mjs";
 export function html_style_border_size(component, value) {
   html_style(component, {
-    "border-size": value,
+    "border-width": value,
   });
 }
