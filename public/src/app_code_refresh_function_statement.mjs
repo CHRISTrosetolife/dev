c95f25d6375c943e33311c343bd56c086d608987
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 15:44:05 -0400

    2024-09-20T19:44:03.929Z function_auto_after app_code_refresh_function_statement

--- public/src/app_code_refresh_function_statement.mjs
+++ public/src/app_code_refresh_function_statement.mjs
@@ -35,7 +35,7 @@ export function app_code_refresh_function_statement(parent, root, statement) {
   }
   let unparsed = js_unparse(statement);
   let s = html_span_text(parent, unparsed);
-  html_style_monospace(code);
+  html_style_monospace(s);
   html_style_background_color(
     parent,
     app_learn_code_style_code_color_background(),
