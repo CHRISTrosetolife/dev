990ba37417c89b7cc64f6e2d5ce486bb0638c1cf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 19:19:39 -0400

    2024-09-20T23:19:38.809Z function_auto_after html_scripts_load

--- public/src/html_scripts_load.mjs
+++ public/src/html_scripts_load.mjs
@@ -1,10 +1,12 @@
 import { function_run_arg } from "./function_run_arg.mjs";
 import { html_script_generate } from "./html_script_generate.mjs";
 import { function_name_generated_combine } from "./function_name_generated_combine.mjs";
-import { each_async } from "./each_async.mjs";
+import { promise_all } from "./promise_all.mjs";
+import { list_map } from "./list_map.mjs";
 export async function html_scripts_load(root, scripts) {
-  await each_async(scripts, async (script) => {
+  let mapped = list_map(scripts, async (script) => {
     let name = function_name_generated_combine(html_script_generate, script);
     await function_run_arg(name, root);
   });
+  await promise_all(mapped);
 }
