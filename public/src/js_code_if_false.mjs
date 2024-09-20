e5dc375a0107d99d793d59cb77148d81e8085bb3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 13:01:53 -0400

    2024-09-20T17:01:53.070Z function_auto_after js_code_if_false

--- public/src/js_code_if_false.mjs
+++ public/src/js_code_if_false.mjs
@@ -1,11 +1,5 @@
-import { js_code_braces } from "./js_code_braces.mjs";
-import { js_code_parenthesis } from "./js_code_parenthesis.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { js_code_if } from "./js_code_if.mjs";
 export function js_code_if_false() {
   let condition = false;
-  return string_combine_multiple([
-    "if",
-    js_code_parenthesis(condition),
-    js_code_braces(),
-  ]);
+  return js_code_if(condition);
 }
