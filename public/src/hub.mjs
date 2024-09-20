54461a44b6006e0f9436008fb2fa57270de1937d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 08:48:53 -0400

    2024-09-20T12:48:50.118Z function_auto_after hub

--- public/src/hub.mjs
+++ public/src/hub.mjs
@@ -1,4 +1,5 @@
+import { watch } from "./watch.mjs";
 import { retry_while_error } from "./retry_while_error.mjs";
 export async function hub() {
-  await retry_while_error();
+  await retry_while_error(watch);
 }
