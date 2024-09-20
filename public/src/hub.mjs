8b5d0a070ce924a243e55d9400f989e7ff6b6b03
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 12:31:02 -0400

    2024-09-20T16:31:02.162Z function_auto_after hub

--- public/src/hub.mjs
+++ public/src/hub.mjs
@@ -1,11 +1,10 @@
 import { unawait } from "./unawait.mjs";
 import { terminal } from "./terminal.mjs";
 import { server } from "./server.mjs";
-import { http_server } from "./http_server.mjs";
 import { watch } from "./watch.mjs";
 export async function hub() {
   await watch();
   await server();
-  unawait($a);
+  unawait(() => {});
   await terminal();
 }
