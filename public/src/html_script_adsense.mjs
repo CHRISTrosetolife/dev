88c70b66ef276e8645d5f7d97d464566a4164acc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 19:17:54 -0400

    2024-09-20T23:17:53.227Z function_auto_after html_script_adsense

--- public/src/html_script_adsense.mjs
+++ public/src/html_script_adsense.mjs
@@ -1,7 +1,8 @@
+import { html_load_wait } from "./html_load_wait.mjs";
 import { html_document_head } from "./html_document_head.mjs";
 import { html_attribute_set } from "./html_attribute_set.mjs";
 import { html_element } from "./html_element.mjs";
-export function html_script_adsense() {
+export async function html_script_adsense() {
   let head = html_document_head();
   let c = html_element(head, "script");
   html_attribute_set(c, "async", "");
@@ -11,4 +12,5 @@ export function html_script_adsense() {
     "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6654470128757682",
   );
   html_attribute_set(c, "crossorigin", "anonymous");
+  await html_load_wait(c);
 }
