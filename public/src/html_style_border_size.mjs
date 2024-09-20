96e782071a12e3bb81055d904a00350f02b4c7d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 15:49:38 -0400

    2024-09-20T19:49:36.857Z function_auto_after html_style_border_size

--- public/src/html_style_border_size.mjs
+++ public/src/html_style_border_size.mjs
@@ -1,6 +1,6 @@
 import { html_style } from "./html_style.mjs";
-export function html_style_border_size(answer_element, value) {
-  html_style(answer_element, {
-    "border-color": value,
+export function html_style_border_size(component, value) {
+  html_style(component, {
+    "border-size": value,
   });
 }
