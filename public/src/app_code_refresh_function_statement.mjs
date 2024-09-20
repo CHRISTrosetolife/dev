d7988fee6ec6249ffef1f83864a524e37b57c6d4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 15:55:34 -0400

    2024-09-20T19:55:33.662Z function_auto_after app_code_refresh_function_statement

--- public/src/app_code_refresh_function_statement.mjs
+++ public/src/app_code_refresh_function_statement.mjs
@@ -1,3 +1,7 @@
+import { html_style_default_border_margin } from "./html_style_default_border_margin.mjs";
+import { html_style_default_border_value } from "./html_style_default_border_value.mjs";
+import { html_style } from "./html_style.mjs";
+import { html_div } from "./html_div.mjs";
 import { app_code_source } from "./app_code_source.mjs";
 import { app_code_save_refresh_function_button } from "./app_code_save_refresh_function_button.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -7,6 +11,9 @@ import { js_unparse } from "./js_unparse.mjs";
 import { equal } from "./equal.mjs";
 import { list_map } from "./list_map.mjs";
 export function app_code_refresh_function_statement(parent, root, statement) {
+  let d = html_div(root);
+  html_style(d, html_style_default_border_value("lightblue"));
+  html_style(d, html_style_default_border_margin({}));
   let { type } = statement;
   if (type === "ExpressionStatement") {
     let { expression } = statement;
