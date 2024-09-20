310008c88bd9babd4da3a75b68ce1140de833139
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 19:08:16 -0400

    2024-09-20T23:08:16.070Z function_auto_after app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -2,8 +2,6 @@ import { object_property_initialize_get_async } from "./object_property_initiali
 import { app_code_save_get } from "./app_code_save_get.mjs";
 import { app_code_refresh } from "./app_code_refresh.mjs";
 import { global_get } from "./global_get.mjs";
-import { functions_source_get } from "./functions_source_get.mjs";
-import { function_run_remote } from "./function_run_remote.mjs";
 import { html_scripts_load } from "./html_scripts_load.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { object_merge } from "./object_merge.mjs";
@@ -11,8 +9,7 @@ export async function app_code() {
   let root = html_style_default_initialize();
   await html_scripts_load(root, ["axios", "acorn", "astring"]);
   let save = app_code_save_get();
-  await object_property_initialize_get_async();
-  let files = await function_run_remote(functions_source_get.name, []);
+  await object_property_initialize_get_async(save, "files", () => {});
   let g = global_get();
   object_merge(g, {
     files,
