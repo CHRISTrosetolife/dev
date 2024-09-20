7493f015ad80bfcad27a02d934d196da9be91732
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 13:12:51 -0400

    2024-09-20T17:12:50.865Z function_auto_after app_code_refresh

--- public/src/app_code_refresh.mjs
+++ public/src/app_code_refresh.mjs
@@ -58,7 +58,7 @@ export async function app_code_refresh(root) {
           if (type === "ExpressionStatement") {
             let { expression } = b;
             let { type: type_e } = expression;
-            if (equal(left, right)) {
+            if (equal(type_e, "AwaitExpression")) {
             }
           }
           return js_unparse(b);
