a65da75af6942d293be91511b572c9fb9e1dc6b8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 08:55:08 -0400

    2024-09-20T12:55:07.117Z function_auto_after hub

--- public/src/hub.mjs
+++ public/src/hub.mjs
@@ -1,6 +1,7 @@
+import { http_server } from "./http_server.mjs";
 import { list_map } from "./list_map.mjs";
 import { repeat } from "./repeat.mjs";
 import { watch } from "./watch.mjs";
 export async function hub() {
-  list_map([watch], async (lambda) => await repeat(lambda));
+  list_map([watch, http_server], async (lambda) => await repeat(lambda));
 }
