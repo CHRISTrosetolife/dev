101051eb32880219bea9d55de6ac04ce8c6ef67c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 19:11:20 -0400

    2024-09-20T23:11:19.828Z function_auto_after app_code_save_refresh

--- public/src/app_code_save_refresh.mjs
+++ public/src/app_code_save_refresh.mjs
@@ -1,9 +1,10 @@
+import { app_save_get } from "./app_save_get.mjs";
 import { app_code_refresh } from "./app_code_refresh.mjs";
 import { app_code } from "./app_code.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
-import { app_code_save_get } from "./app_code_save_get.mjs";
 export async function app_code_save_refresh(root, lambda) {
-  let save = app_code_save_get();
+  let app_fn = app_code;
+  let save = app_save_get(app_code);
   lambda(save);
   storage_local_set(app_code, "save", save);
   await app_code_refresh(root);
