d4eeb692774320b37a212c24e66c7e7fa4c24208
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 13:00:40 -0400

    2024-09-20T17:00:39.992Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -276,6 +276,15 @@ export function js_dollar(ast) {
           list_set(s1, index, statement);
         }
       }
+      if (remaining === "ibe") {
+        let { stack } = v;
+        let s1 = list_get_end_1(stack);
+        if (list_is(s1)) {
+          let statement = js_parse_first(js_code_if());
+          let index = list_index(s1, parent);
+          list_set(s1, index, statement);
+        }
+      }
       if (remaining === "st") {
         let e = js_parse_expression(
           js_code_call_args(object_property_set.name, [
