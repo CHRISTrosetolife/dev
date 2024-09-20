6eae578b922f39e1769713a70261c51e6fcf1465
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 10:57:34 -0400

    2024-09-20T14:57:25.141Z   a hub

--- public/src/hub.mjs
+++ public/src/hub.mjs
@@ -1,11 +1,11 @@
+import { unawait } from "./unawait.mjs";
 import { terminal } from "./terminal.mjs";
 import { server } from "./server.mjs";
 import { http_server } from "./http_server.mjs";
-import { repeat } from "./repeat.mjs";
 import { watch } from "./watch.mjs";
 export async function hub() {
   await watch();
   server();
-  await repeat(http_server);
+  unawait(http_server);
   await terminal();
 }
