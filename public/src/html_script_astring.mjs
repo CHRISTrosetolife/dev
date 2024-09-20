da1a35b85dc516a9bc1341851e177688ff3ae82d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 19:17:59 -0400

    2024-09-20T23:17:58.133Z function_auto_after html_script_astring

--- public/src/html_script_astring.mjs
+++ public/src/html_script_astring.mjs
@@ -1,10 +1,12 @@
+import { html_load_wait } from "./html_load_wait.mjs";
 import { html_attribute_set } from "./html_attribute_set.mjs";
 import { html_element } from "./html_element.mjs";
-export function html_script_astring(parent) {
+export async function html_script_astring(parent) {
   let c = html_element(parent, "script");
   html_attribute_set(
     c,
     "src",
     " https://cdn.jsdelivr.net/npm/astring@1.8.6/dist/astring.min.js ",
   );
+  await html_load_wait(c);
 }
