c88ff3c9fddf0f6be6e2f9fdeb3644be6e682364
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 21:40:45 -0400

    2024-09-21T01:40:45.199Z function_auto_after app_code_refresh_function_statement

--- public/src/app_code_refresh_function_statement.mjs
+++ public/src/app_code_refresh_function_statement.mjs
@@ -50,6 +50,7 @@ export function app_code_refresh_function_statement(
     });
   } else if (equal(type, "VariableDeclarator")) {
     let { id, init } = statement;
+    app_code_refresh_function_statement(parent, root, init, false);
   }
   app_code_source(parent, statement);
 }
