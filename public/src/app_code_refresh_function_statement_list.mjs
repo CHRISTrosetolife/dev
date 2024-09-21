9bb1f5bda7344bdc6d5f83ba0c7cdf099304a392
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-21 08:55:48 -0400

    2024-09-21T12:55:47.643Z function_auto_after app_code_refresh_function_statement_list

--- public/src/app_code_refresh_function_statement_list.mjs
+++ public/src/app_code_refresh_function_statement_list.mjs
@@ -1,18 +1,7 @@
-import { list_multiple_is } from "./list_multiple_is.mjs";
 import { app_code_refresh_function_statement } from "./app_code_refresh_function_statement.mjs";
 import { each } from "./each.mjs";
-export function app_code_refresh_function_statement_list(
-  list,
-  parent,
-  root,
-  border,
-) {
+export function app_code_refresh_function_statement_list(list, parent, root) {
   each(list, (item) => {
-    app_code_refresh_function_statement(
-      parent,
-      root,
-      item,
-      list_multiple_is(list),
-    );
+    app_code_refresh_function_statement(parent, root, item, false);
   });
 }
