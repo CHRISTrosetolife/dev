f204a859549ae9a79846723beb8c4bcdf2d692ee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-21 08:58:28 -0400

    2024-09-21T12:58:27.654Z function_auto_after app_code_refresh_function_statement_list

--- public/src/app_code_refresh_function_statement_list.mjs
+++ public/src/app_code_refresh_function_statement_list.mjs
@@ -1,7 +1,11 @@
+import { html_hr } from "./html_hr.mjs";
 import { each_index } from "./each_index.mjs";
 import { app_code_refresh_function_statement } from "./app_code_refresh_function_statement.mjs";
 export function app_code_refresh_function_statement_list(list, parent, root) {
-  each_index(list, (item) => {
+  each_index(list, (item, index) => {
+    if (false) {
+      html_hr(root);
+    }
     app_code_refresh_function_statement(parent, root, item, false);
   });
 }
