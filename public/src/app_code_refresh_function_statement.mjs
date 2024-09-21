e0bd5369071cfaab420339de44aae107fd1b7604
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-20 21:28:41 -0400

    2024-09-21T01:28:39.703Z function_auto_after app_code_refresh_function_statement

--- public/src/app_code_refresh_function_statement.mjs
+++ public/src/app_code_refresh_function_statement.mjs
@@ -1,12 +1,9 @@
 import { log } from "./log.mjs";
-import { each } from "./each.mjs";
 import { html_style_default_border_margin } from "./html_style_default_border_margin.mjs";
 import { html_style_default_border_value } from "./html_style_default_border_value.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_div } from "./html_div.mjs";
 import { app_code_source } from "./app_code_source.mjs";
-import { app_code_save_refresh_function_button } from "./app_code_save_refresh_function_button.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { equal } from "./equal.mjs";
 export function app_code_refresh_function_statement(
@@ -32,18 +29,7 @@ export function app_code_refresh_function_statement(
     }
     app_code_refresh_function_statement(parent, root, expression, false);
     return;
-  } else if (equal(type, "CallExpression")) {
-    let { callee } = statement;
-    let { type: type_c } = callee;
-    if (equal(type_c, "Identifier")) {
-      let as = object_property_get(statement, "arguments");
-      each(as, (item) => {
-        app_code_refresh_function_statement(parent, root, item, true);
-      });
-      let { name } = callee;
-      app_code_save_refresh_function_button(parent, root, name);
-      return;
-    }
+  } else if (false) {
   }
   log({
     type,
