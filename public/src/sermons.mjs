a4002574c4f4a2278330d36750dafe821c2cc8a5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-21 08:16:42 -0400

    2024-09-21T12:16:40.982Z function_auto_after sermons

--- public/src/sermons.mjs
+++ public/src/sermons.mjs
@@ -1,5 +1,8 @@
+import { path_parse_name } from "./path_parse_name.mjs";
+import { list_map } from "./list_map.mjs";
 import { folder_read } from "./folder_read.mjs";
 import { sermon_folder } from "./sermon_folder.mjs";
 export async function sermons() {
-  return await folder_read(sermon_folder(), ".txt");
+  let paths = await folder_read(sermon_folder(), ".txt");
+  return list_map(paths, path_parse_name);
 }
