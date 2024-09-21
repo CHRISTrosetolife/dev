5b9507f04ec8b0d8e3733d24e545d04f01b09ce0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-21 08:58:34 -0400

    2024-09-21T12:58:33.531Z function_auto_after app_code_refresh_function_statement_list

--- public/src/app_code_refresh_function_statement_list.mjs
+++ public/src/app_code_refresh_function_statement_list.mjs
@@ -3,7 +3,7 @@ import { each_index } from "./each_index.mjs";
 import { app_code_refresh_function_statement } from "./app_code_refresh_function_statement.mjs";
 export function app_code_refresh_function_statement_list(list, parent, root) {
   each_index(list, (item, index) => {
-    if (false) {
+    if (index >= 1) {
       html_hr(root);
     }
     app_code_refresh_function_statement(parent, root, item, false);
