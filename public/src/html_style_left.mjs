4c4e7a65b810960db047b2855b80bf922ef2a342
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-21 08:38:39 -0400

    2024-09-21T12:38:38.679Z function_auto_after html_style_left

--- public/src/html_style_left.mjs
+++ public/src/html_style_left.mjs
@@ -1,5 +1,5 @@
 import { html_style_text_align } from "./html_style_text_align.mjs";
 export function html_style_left(component) {
-  let value = "center";
+  let value = "left";
   html_style_text_align(component, value);
 }
