f472aa5f82bfb1f3bab3146974974a52f94c8dc5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-21 08:16:09 -0400

    2024-09-21T12:16:07.240Z function_auto_after sermons

--- public/src/sermons.mjs
+++ public/src/sermons.mjs
@@ -1,1 +1,5 @@
-export function sermons() {}
+import { folder_read } from "./folder_read.mjs";
+import { sermon_folder } from "./sermon_folder.mjs";
+export async function sermons() {
+  return await folder_read(sermon_folder(), ".txt");
+}
