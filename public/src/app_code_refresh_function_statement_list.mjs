47b6fc8f30c3d3dc5fb1848f19ddb5fce85d4951
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-21 08:58:18 -0400

    2024-09-21T12:58:17.779Z function_auto_after app_code_refresh_function_statement_list

--- public/src/app_code_refresh_function_statement_list.mjs
+++ public/src/app_code_refresh_function_statement_list.mjs
@@ -1,7 +1,7 @@
+import { each_index } from "./each_index.mjs";
 import { app_code_refresh_function_statement } from "./app_code_refresh_function_statement.mjs";
-import { each } from "./each.mjs";
 export function app_code_refresh_function_statement_list(list, parent, root) {
-  each(list, (item) => {
+  each_index(list, (item) => {
     app_code_refresh_function_statement(parent, root, item, false);
   });
 }
