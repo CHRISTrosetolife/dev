9f8aa55ec4f5f171a834e8c05dcc6f14bd08282c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 10:45:53 -0400

    2024-09-22T14:45:52.637Z function_auto_after js_node_type_visitor

--- public/src/js_node_type_visitor.mjs
+++ public/src/js_node_type_visitor.mjs
@@ -1,15 +1,11 @@
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 import { js_visit } from "./js_visit.mjs";
-import { js_node_is } from "./js_node_is.mjs";
 export function js_node_type_visitor(ast, type) {
   return list_adder_unique((la) => {
     js_visit(ast, lambda);
     function lambda(v) {
       let { node } = v;
-      if (!js_node_is(node)) {
-        return;
-      }
       if (!js_node_type_is(node, type)) {
         return;
       }
