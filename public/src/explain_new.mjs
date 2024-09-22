f451297d05447c2b450d250a79705c15172efe75
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 15:11:24 -0400

    2024-09-22T19:11:23.724Z function_auto_after explain_new

--- public/src/explain_new.mjs
+++ public/src/explain_new.mjs
@@ -1,6 +1,6 @@
+import { explain_folder } from "./explain_folder.mjs";
 import { written_new } from "./written_new.mjs";
-import { sermon_folder } from "./sermon_folder.mjs";
 export async function explain_new(name) {
-  let written_folder = sermon_folder();
+  let written_folder = explain_folder();
   await written_new(written_folder, name);
 }
