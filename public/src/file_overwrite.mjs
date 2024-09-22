f2855291b480c35fbb34758fb908df78c6a79a6c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 15:34:04 -0400

    2024-09-22T19:34:04.522Z function_auto_after file_overwrite

--- public/src/file_overwrite.mjs
+++ public/src/file_overwrite.mjs
@@ -1,4 +1,7 @@
+import { never } from "./never.mjs";
+import { assert } from "./assert.mjs";
 import { file_overwrite_generic } from "./file_overwrite_generic.mjs";
 export async function file_overwrite(file_name, data) {
+  assert(never, []);
   return await file_overwrite_generic(file_name, data, "utf-8");
 }
