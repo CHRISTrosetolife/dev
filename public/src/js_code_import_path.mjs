3b442c48f725b02bb0b8be787fe8f02d7a41429c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 15:27:52 -0400

    2024-09-22T19:27:50.846Z function_auto_after js_code_import_path

--- public/src/js_code_import_path.mjs
+++ public/src/js_code_import_path.mjs
@@ -1,9 +1,16 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { js_code_statement } from "./js_code_statement.mjs";
 import { string_combine } from "./string_combine.mjs";
 export function js_code_import_path(import_path, import_name) {
   let import_path_combined = string_combine(import_path, import_name);
   let import_code = js_code_statement(
-    `import { ${import_name} } from "${import_path_combined}.mjs"`,
+    string_combine_multiple([
+      "import { ",
+      import_name,
+      ' } from "',
+      import_path_combined,
+      '.mjs"',
+    ]),
   );
   return import_code;
 }
