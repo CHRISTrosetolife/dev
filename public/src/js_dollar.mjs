e46250f711ac2a63dce7f8fd2f73e65bf22abbf3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 10:47:03 -0400

    2024-09-22T14:47:03.167Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -171,10 +171,10 @@ export function js_dollar(ast) {
               if (js_node_type_is(init, "CallExpression")) {
                 let { callee } = init;
                 if (js_node_type_is(callee, "Identifier")) {
+                  log({
+                    init,
+                  });
                 }
-                log({
-                  init,
-                });
               }
             }
           }
