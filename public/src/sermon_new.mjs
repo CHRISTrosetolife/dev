2a753c53cbcbc6d36683761f53dee6aea1db49c2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 14:05:31 -0400

    2024-09-22T18:05:30.622Z s list,last

--- public/src/sermon_new.mjs
+++ public/src/sermon_new.mjs
@@ -1,12 +1,8 @@
-import { written_path } from "./written_path.mjs";
-import { sermon_folder } from "./sermon_folder.mjs";
 import { sermon_path } from "./sermon_path.mjs";
 import { file_open } from "./file_open.mjs";
 import { file_write } from "./file_write.mjs";
 export async function sermon_new(name) {
-  let written_folder = sermon_folder();
-  let sermon_path_file = written_path(written_folder, name);
-  return sermon_path_file;
+  $ex;
   let sermon_path_file = sermon_path(name);
   await file_write(sermon_path_file, "TODO");
   await file_open(sermon_path_file);
