c717589554b9bfe37d99fd105620ec7d1862cab8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 13:32:00 -0400

    2024-09-22T17:31:59.285Z function_auto_after js_identifier_rename_multiple

--- public/src/js_identifier_rename_multiple.mjs
+++ public/src/js_identifier_rename_multiple.mjs
@@ -4,14 +4,14 @@ import { each_index_only } from "./each_index_only.mjs";
 import { list_size_equal } from "./list_size_equal.mjs";
 import { assert } from "./assert.mjs";
 export function js_identifier_rename_multiple(
-  d,
+  ast,
   identifiers_from,
   identifiers_to,
 ) {
   assert(list_size_equal, [identifiers_from, identifiers_to]);
   each_index_only(identifiers_from, (index) => {
     js_identifier_rename(
-      d,
+      ast,
       list_get(identifiers_from, index),
       list_get(identifiers_to, index),
     );
