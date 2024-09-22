6c2f4aadf623cae903fa3db8edd231199b6b3bad
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 10:22:08 -0400

    2024-09-22T14:22:07.322Z pb written_path

--- public/src/sermon_path.mjs
+++ public/src/sermon_path.mjs
@@ -2,5 +2,5 @@ import { written_path } from "./written_path.mjs";
 import { sermon_folder } from "./sermon_folder.mjs";
 export function sermon_path(name) {
   let written_folder = sermon_folder();
-  return written_path(name, written_folder);
+  return written_path(written_folder, name);
 }
--- public/src/written_path.mjs
+++ public/src/written_path.mjs
@@ -1,6 +1,6 @@
 import { path_join } from "./path_join.mjs";
 import { string_combine } from "./string_combine.mjs";
-export function written_path(name, written_folder) {
+export function written_path(written_folder, name) {
   let file_name = string_combine(name, ".txt");
   let sermon_path_file = path_join([written_folder, file_name]);
   return sermon_path_file;
