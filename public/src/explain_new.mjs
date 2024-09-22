f384c45c291b22d03451afff088cbaa5a11955a6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 15:11:18 -0400

    2024-09-22T19:11:17.593Z c sermon_new explain_new

---
+++ public/src/explain_new.mjs
@@ -0,0 +1,6 @@
+import { written_new } from "./written_new.mjs";
+import { sermon_folder } from "./sermon_folder.mjs";
+export async function explain_new(name) {
+  let written_folder = sermon_folder();
+  await written_new(written_folder, name);
+}
