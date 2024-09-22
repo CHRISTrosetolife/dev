427299cdcbf0a50ac5e81a652a08353142f3aea2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 10:20:57 -0400

    2024-09-22T14:20:56.082Z function_auto_after sermon_path

--- public/src/sermon_path.mjs
+++ public/src/sermon_path.mjs
@@ -2,8 +2,8 @@ import { sermon_folder } from "./sermon_folder.mjs";
 import { path_join } from "./path_join.mjs";
 import { string_combine } from "./string_combine.mjs";
 export function sermon_path(name) {
-  let file_name = string_combine(name, ".txt");
   let explain_folder = sermon_folder();
+  let file_name = string_combine(name, ".txt");
   let sermon_path_file = path_join([explain_folder, file_name]);
   return sermon_path_file;
 }
