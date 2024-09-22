b0f5e9e1936d958d6646ace0f2fa699513d2e754
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 15:31:16 -0400

    2024-09-22T19:31:16.132Z function_auto_after function_dependencies

--- public/src/function_dependencies.mjs
+++ public/src/function_dependencies.mjs
@@ -1,7 +1,6 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { chalk } from "./chalk.mjs";
 import { js_code_import_generic } from "./js_code_import_generic.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { js_unparse } from "./js_unparse.mjs";
 import { list_map } from "./list_map.mjs";
@@ -11,10 +10,7 @@ import { each } from "./each.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 import { function_imports } from "./function_imports.mjs";
 import { object_properties } from "./object_properties.mjs";
-import { assert_message } from "./assert_message.mjs";
-import { list_empty_is } from "./list_empty_is.mjs";
 import { list_map_async } from "./list_map_async.mjs";
-import { list_join_comma_space } from "./list_join_comma_space.mjs";
 export async function function_dependencies(function_name) {
   let map = await function_imports(function_name);
   let externals = list_adder_unique((la) => {
@@ -27,12 +23,6 @@ export async function function_dependencies(function_name) {
     chalk: js_code_import_generic(chalk, chalk),
   };
   let e_code = list_map(externals, (n) => object_property_get(e, n));
-  assert_message(list_empty_is, [externals], () =>
-    string_combine_multiple([
-      "if this is non-empty, then refactor output to include external libraries: ",
-      list_join_comma_space(externals),
-    ]),
-  );
   let dependency_names = object_properties(map);
   let ds = await list_map_async(dependency_names, function_declaration);
   let us = list_map(ds, js_unparse);
