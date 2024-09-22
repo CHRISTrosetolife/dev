b1cc545f9091f85527c597ef7f093321099c3540
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 10:45:58 -0400

    2024-09-22T14:45:57.524Z function_auto_after js_node_type_is

--- public/src/js_node_type_is.mjs
+++ public/src/js_node_type_is.mjs
@@ -1,5 +1,9 @@
+import { js_node_is } from "./js_node_is.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function js_node_type_is(node, type) {
+  if (!js_node_is(node)) {
+    return;
+  }
   let result = object_property_get(node, "type") === type;
   return result;
 }
