48bd4bd2d108c1915f0d0e6798987b9a5bc21ae9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 08:08:53 -0400

    2024-09-22T12:08:49.031Z function_auto_after app_code_refresh

--- public/src/app_code_refresh.mjs
+++ public/src/app_code_refresh.mjs
@@ -52,16 +52,12 @@ export async function app_code_refresh(root) {
       html_inner_set(p, await function_read(name));
     } else {
       app_code_save_delete_refresh_button_back(root, "function");
-      each(list, (item) => {});
-      html_button_width_full_text_click(root, "source", async () => {
-        await app_code_save_refresh(root, (save) =>
-          object_property_set(save, "source", true),
-        );
-      });
-      html_button_width_full_text_click(root, "imports", async () => {
-        await app_code_save_refresh(root, (save) =>
-          object_property_set(save, "imports", true),
-        );
+      each(["source", "imports"], (item) => {
+        html_button_width_full_text_click(root, item, async () => {
+          await app_code_save_refresh(root, (save) =>
+            object_property_set(save, item, true),
+          );
+        });
       });
       let body = js_declaration_single_body(ast);
       html_div_text(root, name);
