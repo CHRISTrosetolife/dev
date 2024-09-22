204894e36ce55828cea45d4f6ebd2f2701ec30b6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 15:28:26 -0400

    2024-09-22T19:28:25.255Z function_auto_after js_code_import_path

--- public/src/js_code_import_path.mjs
+++ public/src/js_code_import_path.mjs
@@ -3,10 +3,11 @@ import { js_code_statement } from "./js_code_statement.mjs";
 import { string_combine } from "./string_combine.mjs";
 export function js_code_import_path(import_path, import_name) {
   let import_path_combined = string_combine(import_path, import_name);
+  let a = string_combine_multiple(["{ ", import_name, " }"]);
   let import_code = js_code_statement(
     string_combine_multiple([
       "import ",
-      string_combine_multiple(["{ ", import_name, " }"]),
+      a,
       ' from "',
       import_path_combined,
       '.mjs"',
