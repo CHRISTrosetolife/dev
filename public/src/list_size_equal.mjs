640f1450b545160b0e43e5ff5608c573b0229cb3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 11:56:48 -0400

    2024-09-22T15:56:48.628Z function_auto_after list_size_equal

--- public/src/list_size_equal.mjs
+++ public/src/list_size_equal.mjs
@@ -1,4 +1,4 @@
 import { lists_sizes_equal } from "./lists_sizes_equal.mjs";
 export function list_size_equal(params_names, args_names) {
-  lists_sizes_equal([params_names, args_names]);
+  return lists_sizes_equal([params_names, args_names]);
 }
