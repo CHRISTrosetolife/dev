bc74841362a534466438617dba67962d32e8f3b7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 15:32:30 -0400

    2024-09-22T19:32:30.049Z function_auto_after function_output_file

--- public/src/function_output_file.mjs
+++ public/src/function_output_file.mjs
@@ -6,11 +6,11 @@ import { date_string_iso_file } from "./date_string_iso_file.mjs";
 import { path_join } from "./path_join.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
 export async function function_output_file(function_name) {
-  let f_path = path_join([function_name, date_string_iso_file() + ".txt"]);
   let result = await function_run(function_name, []);
   if (!string_is(result)) {
     result = json_format_to(result);
   }
+  let f_path = path_join([function_name, date_string_iso_file() + ".txt"]);
   let output_path = folder_gitignore_path_function(
     function_output_file,
     f_path,
