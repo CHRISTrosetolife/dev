60ffcc298ab29c433642a68a90704c3763dd7afc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 11:31:49 -0400

    2024-09-22T15:31:49.015Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -179,7 +179,7 @@ export function js_dollar(ast) {
                   let d = js_declaration_single(ast_c);
                   let params = js_declaration_to_params(d);
                   let identifiers = js_identifiers(ast_c);
-                  let args = init;
+                  let { arguments: args } = init;
                   log({
                     params,
                     identifiers,
