7461ff8e20433dedac23e9d45a8e72a647b32687
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 13:53:27 -0400

    2024-09-22T17:53:26.235Z function_auto_after js_return_variablize

--- public/src/js_return_variablize.mjs
+++ public/src/js_return_variablize.mjs
@@ -4,11 +4,11 @@ export function js_return_variablize(ast) {
   let return_statements = js_node_type_visitor(ast, "ReturnStatement");
   for (let r of return_statements) {
     let { node } = r;
-    if (node.argument.type === "Identifier") {
+    if (r.node.argument.type === "Identifier") {
       continue;
     }
     let { parent } = r;
-    const init = node.argument;
+    let init = node.argument;
     let parsed2 = js_variablize(ast, parent, node, init);
     node.argument = parsed2;
   }
