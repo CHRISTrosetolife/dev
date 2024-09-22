d2366f13bdd2ddf25421d1339f5dd71356f53c39
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 14:05:52 -0400

    2024-09-22T18:05:50.168Z function_auto_after list_take_but

--- public/src/list_take_but.mjs
+++ public/src/list_take_but.mjs
@@ -1,5 +1,5 @@
 import { list_size } from "./list_size.mjs";
 import { list_take } from "./list_take.mjs";
-export function list_take_but(books, c) {
-  return list_take(books, list_size(books) - c);
+export function list_take_but(list, count) {
+  return list_take(list, list_size(list) - count);
 }
