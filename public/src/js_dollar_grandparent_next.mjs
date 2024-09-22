932c4ca99bd91746301300d8b6ff48082e710c4f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 13:44:01 -0400

    2024-09-22T17:44:00.866Z function_auto_after js_dollar_grandparent_next

--- public/src/js_dollar_grandparent_next.mjs
+++ public/src/js_dollar_grandparent_next.mjs
@@ -1,5 +1,5 @@
+import { list_get } from "./list_get.mjs";
 import { list_index_next } from "./list_index_next.mjs";
-import { list_next } from "./list_next.mjs";
 import { list_is } from "./list_is.mjs";
 import { list_get_end_1 } from "./list_get_end_1.mjs";
 export async function js_dollar_grandparent_next(v, lambda) {
@@ -8,7 +8,7 @@ export async function js_dollar_grandparent_next(v, lambda) {
   let s1 = list_get_end_1(stack);
   if (list_is(s1)) {
     let item_index_next = list_index_next(list, item);
-    let item_next = list_next(s1, parent);
+    let next = list_get(list, item_index_next);
     await lambda({
       next,
       s1,
