157ead945f5fdfed66d27009270bdc4da28f0f6d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-22 11:41:50 -0400

    2024-09-22T15:41:50.287Z function_auto_after js_imports_add_specified

---
+++ public/src/js_imports_add_specified.mjs
@@ -0,0 +1,20 @@
+import { js_identifiers_intersect_difference } from "./js_identifiers_intersect_difference.mjs";
+import { list_concat } from "./list_concat.mjs";
+import { list_add_beginning } from "./list_add_beginning.mjs";
+import { js_parse_first } from "./js_parse_first.mjs";
+import { js_code_import } from "./js_code_import.mjs";
+import { js_imports_existing } from "./js_imports_existing.mjs";
+import { js_declaration_single_name } from "./js_declaration_single_name.mjs";
+export function js_imports_add_specified(ast, specified) {
+  let name = js_declaration_single_name(ast);
+  let self = [name];
+  let existing = js_imports_existing(ast);
+  let combined = list_concat(existing, self);
+  let missing = js_identifiers_intersect_difference(ast, specified, combined);
+  let { body } = ast;
+  for (let m of missing) {
+    let code = js_code_import(m);
+    let first = js_parse_first(code);
+    list_add_beginning(body, first);
+  }
+}
