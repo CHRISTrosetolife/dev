69b73cd5139ec5602dc573f5b6d2f71245448b40
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-23 19:54:18 -0400

    2024-09-23T23:54:18.032Z function_auto_after app_list_choose

--- public/src/app_list_choose.mjs
+++ public/src/app_list_choose.mjs
@@ -30,6 +30,7 @@ export function app_list_choose(body) {
   let split = storage_local_get(app_list, "list");
   storage_local_initialize(app_list, "chosen", {});
   let chosen = storage_local_get(app_list, "chosen");
+  log({});
   each_index(split, (line, index) => {
     let b = html_button_width_full_text_click(body, line, () => {
       chosen[index] = !chosen[index];
