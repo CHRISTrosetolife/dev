afeed6eec1edf1d8ec387ecb08300d7727106023
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-24 19:42:43 -0400

    2024-09-24T23:42:42.432Z function_auto_after app_resume

--- public/src/app_resume.mjs
+++ public/src/app_resume.mjs
@@ -1,3 +1,4 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { app_resume_audio_bible } from "./app_resume_audio_bible.mjs";
 import { function_name_to_url_github } from "./function_name_to_url_github.mjs";
 import { noop } from "./noop.mjs";
@@ -35,7 +36,10 @@ export function app_resume() {
       html_style_link(function_name_to_url_github("app_memorize_keyboard")),
       noop,
     ],
-    'Transforms source code files through its abstract syntax tree. Includes a `terminal` for running functions, `aliases` for running commonly used functions and `$-prefixed "macros"` that transform code when a file is saved. For example r is an alias for function_rename that renames a function across all files, including fixing all references.',
+    string_combine_multiple([
+      'Transforms source code files through its abstract syntax tree. Includes a `terminal` for running functions, `aliases` for running commonly used functions and `$-prefixed "macros"` that transform code `when a file is saved`. For example r is an alias for `',
+      "` that renames a function across all files, including fixing all references.",
+    ]),
   );
   html_style_link("dev.html")(dev);
   html_cycle_p(root, [noop], "");
