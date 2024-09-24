06720d4a08fdb75764f1e0be1bd027675ac21afe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-24 19:59:31 -0400

    2024-09-24T23:59:28.683Z function_auto_after list_between_after

--- public/src/list_between_after.mjs
+++ public/src/list_between_after.mjs
@@ -1,5 +1,5 @@
 import { list_between } from "./list_between.mjs";
 import { list_concat } from "./list_concat.mjs";
 export function list_between_after(list, b) {
-  list_concat(list_between(list, b), b);
+  return list_concat(list_between(list, b), b);
 }
