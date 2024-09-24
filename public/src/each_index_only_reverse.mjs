1e1846ac6a5c870274559e02d9eaab16bd9d602f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-24 19:51:25 -0400

    2024-09-24T23:51:23.951Z function_auto_after each_index_only_reverse

--- public/src/each_index_only_reverse.mjs
+++ public/src/each_index_only_reverse.mjs
@@ -1,7 +1,6 @@
-import { list_insert } from "./list_insert.mjs";
 import { each_index_reverse } from "./each_index_reverse.mjs";
 export function each_index_only_reverse(list, lambda) {
   each_index_reverse(list, (item, index) => {
-    list_insert(list, index, b);
+    lambda(index);
   });
 }
