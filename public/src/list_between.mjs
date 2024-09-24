798a53b6d885c2ad59bef3c2bd4c3c77f5214a50
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-24 19:49:55 -0400

    2024-09-24T23:49:54.735Z function_auto_after list_between

--- public/src/list_between.mjs
+++ public/src/list_between.mjs
@@ -2,6 +2,6 @@ import { list_insert } from "./list_insert.mjs";
 import { each_index_reverse } from "./each_index_reverse.mjs";
 export function list_between(list, b) {
   each_index_reverse(list, (item, index) => {
-    list_insert();
+    list_insert(list, index, b);
   });
 }
