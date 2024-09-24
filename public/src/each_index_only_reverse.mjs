c7d21a5ce481ad4e718679b27b854f55393bf16d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-24 19:51:16 -0400

    2024-09-24T23:51:15.668Z function_auto_after each_index_only_reverse

--- public/src/each_index_only_reverse.mjs
+++ public/src/each_index_only_reverse.mjs
@@ -1,1 +1,7 @@
-export function each_index_only_reverse(list, lambda) {}
+import { list_insert } from "./list_insert.mjs";
+import { each_index_reverse } from "./each_index_reverse.mjs";
+export function each_index_only_reverse(list, lambda) {
+  each_index_reverse(list, (item, index) => {
+    list_insert(list, index, b);
+  });
+}
