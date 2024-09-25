ec90881bb23f634fd7150d8d23d935d0974d410e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 18:10:02 -0400

    2024-09-25T22:10:01.086Z function_auto_after log_json_format

--- public/src/log_json_format.mjs
+++ public/src/log_json_format.mjs
@@ -1,5 +1,5 @@
-import { json_to } from "./json_to.mjs";
+import { json_format_to } from "./json_format_to.mjs";
 import { log } from "./log.mjs";
 export function log_json_format(message) {
-  log(json_to(message));
+  log(json_format_to(message));
 }
