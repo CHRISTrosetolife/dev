50109a44c8f8a461bf891aa4ff653dcdd7dc7a63
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 17:44:22 -0400

    2024-09-25T21:44:22.028Z function_auto_after html_style_font_color

--- public/src/html_style_font_color.mjs
+++ public/src/html_style_font_color.mjs
@@ -1,6 +1,7 @@
+import { html_style_font_color_property } from "./html_style_font_color_property.mjs";
 import { html_style } from "./html_style.mjs";
 export function html_style_font_color(component, color) {
   html_style(component, {
-    ["color"]: color,
+    [html_style_font_color_property()]: color,
   });
 }
---
+++ public/src/html_style_font_color_property.mjs
@@ -0,0 +1,3 @@
+export function html_style_font_color_property() {
+  return "color";
+}
