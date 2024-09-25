33b175fd63e83753ca0bd06e7164d59140dd82e4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 17:39:28 -0400

    2024-09-25T21:39:27.479Z c html_style_default html_style_default_a

---
+++ public/src/html_style_default_a.mjs
@@ -0,0 +1,9 @@
+import { global_get } from "./global_get.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
+export function html_style_default_a() {
+  return object_property_initialize(
+    global_get(),
+    html_style_default_a.name,
+    {},
+  );
+}
