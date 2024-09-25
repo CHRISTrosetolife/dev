6b61103ed5e9c3b57f02870a42fbf95d98fe02b7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 17:54:53 -0400

    2024-09-25T21:54:53.238Z function_auto_after app_resume

--- public/src/app_resume.mjs
+++ public/src/app_resume.mjs
@@ -27,6 +27,8 @@ import { html_p } from "./html_p.mjs";
 export function app_resume() {
   let root = html_style_default_initialize();
   let print = true;
+  if (false) {
+  }
   html_style_default_font_size(1.2);
   html_style_default_a_merge({
     [html_style_text_decoration_property()]: "none",
