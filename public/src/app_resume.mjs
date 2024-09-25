4831c5be5f99a99e8e47aec85bb4f65ee574c9bd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 17:53:11 -0400

    2024-09-25T21:53:10.666Z function_auto_after app_resume

--- public/src/app_resume.mjs
+++ public/src/app_resume.mjs
@@ -105,17 +105,17 @@ export function app_resume() {
   app_resume_header_3(root, "Contractor");
   html_p_text(
     root,
-    "~2022: Programmer; Created debt collections software; Used JavaScript, C#/.NET, AngularJS and T-SQL",
+    "~2022: Programmer; Created debt collections software; Used JavaScript, C#, AngularJS and T-SQL",
   );
   app_resume_header_3(root, "Greenshades");
   html_p_text(
     root,
-    "~2020: Software Developer; Created tax software; Used C#/.NET and T-SQL",
+    "~2020: Software Developer; Created tax software; Used C# and T-SQL",
   );
   app_resume_header_3(root, "RF-SMART/ICS");
   html_p_text(
     root,
-    "~2017 - ~2019: Senior Software Engineer; Created warehouse software; Used C#/.NET, React, Knockout, EntityFramework, MySQL",
+    "~2017 - ~2019: Senior Software Engineer; Created warehouse software; Used C#, React, Knockout, EntityFramework, MySQL",
   );
   app_resume_header_3(
     root,
@@ -123,7 +123,7 @@ export function app_resume() {
   );
   html_p_text(
     root,
-    "~2009 - ~2017: From Intern to Team Lead, Senior Software Engineer; Created debt collections software; Used C#/.NET, AngularJS, EntityFramework, T-SQL and IIS",
+    "~2009 - ~2017: From Intern to Team Lead, Senior Software Engineer; Created debt collections software; Used C#, AngularJS, EntityFramework, T-SQL and IIS",
   );
   app_resume_header_2(root, "Education");
   app_resume_header_3(root, "University of North Florida");
