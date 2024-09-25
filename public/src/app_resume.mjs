b2dbb20ade5a742dbb43ae71350dd93052730bfe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-24 20:15:37 -0400

    2024-09-25T00:15:36.852Z function_auto_after app_resume

--- public/src/app_resume.mjs
+++ public/src/app_resume.mjs
@@ -106,7 +106,7 @@ export function app_resume() {
   app_resume_header_3(root, "RF-SMART/ICS");
   html_p_text(
     root,
-    "~2016 - ~2018: Senior Software Engineer; Created warehouse software. Used C#/.NET, React, Knockout, EntityFramework, MySQL",
+    "~2017 - ~2019: Senior Software Engineer; Created warehouse software. Used C#/.NET, React, Knockout, EntityFramework, MySQL",
   );
   app_resume_header_3(
     root,
@@ -114,7 +114,7 @@ export function app_resume() {
   );
   html_p_text(
     root,
-    "~2009 - ~2016: From Intern to Team Lead, Senior Software Engineer; Created debt collections software. Used C#/.NET, AngularJS, EntityFramework, T-SQL and IIS",
+    "~2009 - ~2017: From Intern to Team Lead, Senior Software Engineer; Created debt collections software. Used C#/.NET, AngularJS, EntityFramework, T-SQL and IIS",
   );
   app_resume_header_2(root, "Education");
   app_resume_header_3(root, "University of North Florida");
