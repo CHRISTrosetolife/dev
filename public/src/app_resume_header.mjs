a272108647c18fee061aa2e67ad94022fa8ccea1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 17:32:02 -0400

    2024-09-25T21:32:01.300Z function_auto_after app_resume_header

--- public/src/app_resume_header.mjs
+++ public/src/app_resume_header.mjs
@@ -10,13 +10,13 @@ import { html_p_text } from "./html_p_text.mjs";
 import { multiply } from "./multiply.mjs";
 export function app_resume_header(root, header) {
   let h = html_p_text(root, header);
+  let multiplier = 1.5;
   let d = object_property_get(
     html_style_default(),
     html_style_font_size_property(),
   );
   let p = html_style_numeric_parse(d);
   let { value } = p;
-  let multiplier = 1.5;
   let size_new = multiply(value, multiplier);
   html_style_font_size(h, size_new);
   html_style_bold(h);
