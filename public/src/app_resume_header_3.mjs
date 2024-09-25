5beb5de2097938a82bd52d5d8e829a755e3b1808
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 17:33:38 -0400

    2024-09-25T21:33:37.846Z function_auto_after app_resume_header_3

--- public/src/app_resume_header_3.mjs
+++ public/src/app_resume_header_3.mjs
@@ -3,9 +3,9 @@ import { html_style_underline } from "./html_style_underline.mjs";
 import { html_style_bold } from "./html_style_bold.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 export function app_resume_header_3(root, header) {
-  let c = html_p_text(root, header);
-  html_style_bold(c);
-  html_style_underline(c);
+  let p = html_p_text(root, header);
+  html_style_bold(p);
+  html_style_underline(p);
   html_style_font_size_default_multiplied(p, 1.1);
-  return c;
+  return p;
 }
