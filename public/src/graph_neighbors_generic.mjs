dabbdd7545a7fc4f15cf78bf802e6f7a74b6d1b4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 18:02:54 -0400

    2024-09-25T22:02:54.050Z function_auto_after graph_neighbors_generic

--- public/src/graph_neighbors_generic.mjs
+++ public/src/graph_neighbors_generic.mjs
@@ -6,7 +6,7 @@ import { list_filter } from "./list_filter.mjs";
 export function graph_neighbors_generic(lambda_filter, edges, v) {
   let filtered = list_filter(edges, lambda_filter);
   log({
-    filtered,
+    edges,
   });
   let mapped = list_map(filtered, (f) => list_without(f, v));
   let mapped2 = list_map(mapped, list_single);
