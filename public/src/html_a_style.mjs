291dc9212f2e2e293427e789e283931b6cc990d1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 17:40:44 -0400

    2024-09-25T21:40:44.021Z function_auto_after html_a_style

--- public/src/html_a_style.mjs
+++ public/src/html_a_style.mjs
@@ -1,6 +1,8 @@
+import { each } from "./each.mjs";
 import { html_style_default_a } from "./html_style_default_a.mjs";
 import { html_style_font_color } from "./html_style_font_color.mjs";
 export function html_a_style(a) {
   html_style_default_a();
+  each(list, (item) => {});
   html_style_font_color(a, "darkblue");
 }
