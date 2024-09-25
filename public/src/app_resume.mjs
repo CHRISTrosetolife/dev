00613cad98dfcb2903fce3f270c5817cbdb276b4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-24 20:07:29 -0400

    2024-09-25T00:07:28.741Z function_auto_after app_resume

--- public/src/app_resume.mjs
+++ public/src/app_resume.mjs
@@ -93,6 +93,8 @@ export function app_resume() {
     "JavaScript; Automation; Parsing and transforming code (writing code that writes code); Front-end programming; Back-end programming; Adapting; Communication; Working with difficult people",
   );
   app_resume_header_2(root, "Professional Experience");
+  app_resume_header_3(root, "Learn Cebuano Language");
+  html_p_text(root, "Genesys/Interactive Intelligence/Latitude Software");
   html_p_text(root, "Genesys/Interactive Intelligence/Latitude Software");
   html_p_text(root, "C#/.NET, AngularJS, EntityFramework, T-SQL");
   app_resume_header_2(root, "Education");
