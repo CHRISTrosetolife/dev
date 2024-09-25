c1829c0af56c170f28edf3b4cfe9966e2e3ec965
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 17:40:03 -0400

    2024-09-25T21:40:03.086Z function_auto_after html_a_style

--- public/src/html_a_style.mjs
+++ public/src/html_a_style.mjs
@@ -1,4 +1,6 @@
+import { html_style_default_a } from "./html_style_default_a.mjs";
 import { html_style_font_color } from "./html_style_font_color.mjs";
 export function html_a_style(a) {
+  html_style_default_a();
   html_style_font_color(a, "darkblue");
 }
