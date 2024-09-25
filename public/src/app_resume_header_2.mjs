f1019c242b98ae553484986540383b10307b6a30
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 17:33:02 -0400

    2024-09-25T21:33:01.432Z function_auto_after app_resume_header_2

--- public/src/app_resume_header_2.mjs
+++ public/src/app_resume_header_2.mjs
@@ -4,7 +4,7 @@ import { html_style_bold } from "./html_style_bold.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 export function app_resume_header_2(root, header) {
   let p = html_p_text(root, header);
-  html_style_font_size_default_multiplied(p, 1.25);
+  html_style_font_size_default_multiplied(p, 1.3);
   html_style_bold(p);
   html_style_centered(p);
 }
