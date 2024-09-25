2d56090debf6ca59bf27d2e1669a6d2bc48dc48b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 18:17:52 -0400

    2024-09-25T22:17:51.205Z function_auto_after app_dev_screen_function_new

--- public/src/app_dev_screen_function_new.mjs
+++ public/src/app_dev_screen_function_new.mjs
@@ -1,3 +1,4 @@
+import { fn_name } from "./fn_name.mjs";
 import { html_cycle_code_span } from "./html_cycle_code_span.mjs";
 import { html_cycle_code_multiple } from "./html_cycle_code_multiple.mjs";
 import { html_cycle_p } from "./html_cycle_p.mjs";
@@ -6,7 +7,6 @@ import { html_style_code } from "./html_style_code.mjs";
 import { app_dev_sandbox_command_line_result_remaining_first_function } from "./app_dev_sandbox_command_line_result_remaining_first_function.mjs";
 import { function_name_new } from "./function_name_new.mjs";
 import { app_dev_sandbox_command_line } from "./app_dev_sandbox_command_line.mjs";
-import { function_new } from "./function_new.mjs";
 import { html_style_function_name } from "./html_style_function_name.mjs";
 import { noop } from "./noop.mjs";
 import { app_dev_screen_img } from "./app_dev_screen_img.mjs";
@@ -19,13 +19,13 @@ export function app_dev_screen_function_new() {
       html_cycle_p(
         root,
         [html_style_function_name, noop, html_style_code],
-        function_new.name + "` adds a new `function",
+        fn_name("function_new") + "` adds a new `function",
       );
       let i = 1;
       app_dev_screen_img(app_dev_screen_function_new, root, i++);
       html_cycle_function_name(
         root,
-        "when `" + function_new.name + "` is ran :",
+        "when `" + fn_name("function_new") + "` is ran :",
       );
       html_cycle_code_multiple(root, [
         "a file is created for the `function`",
@@ -35,7 +35,7 @@ export function app_dev_screen_function_new() {
       ]);
       app_dev_screen_img(app_dev_screen_function_new, root, i++);
       let function_name = await function_name_new("greetings");
-      let args = [function_new.name, function_name];
+      let args = [fn_name("function_new"), function_name];
       let try_out_message = "creating a new `function`";
       await app_dev_sandbox_command_line(
         root,
