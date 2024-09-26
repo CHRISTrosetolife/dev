0ec58aabbdb245ef59a40686cbbde87f4d2f87de
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 15:33:01 -0400

    2024-09-26T19:33:00.601Z r bible_search bible_search_lookup

--- public/src/bible_search_lookup.mjs
+++ public/src/bible_search_lookup.mjs
@@ -2,7 +2,7 @@ import { bible_search_lookup_version } from "./bible_search_lookup_version.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { each_async } from "./each_async.mjs";
 import { bible_eng_versions } from "./bible_eng_versions.mjs";
-export async function bible_search() {
+export async function bible_search_lookup() {
   let r = {};
   let bible_folders = bible_eng_versions();
   await each_async(bible_folders, async (bible_folder) => {
