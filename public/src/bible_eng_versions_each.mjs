a5f0a4abc850608632fd9410a7ed5c49875d2648
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 17:38:34 -0400

    2024-09-26T21:38:28.748Z function_auto_after bible_eng_versions_each

--- public/src/bible_eng_versions_each.mjs
+++ public/src/bible_eng_versions_each.mjs
@@ -3,10 +3,10 @@ import { each_async } from "./each_async.mjs";
 import { bible_eng_versions } from "./bible_eng_versions.mjs";
 export async function bible_eng_versions_each(lambda) {
   let bible_folders = bible_eng_versions();
-  await each_async(bible_folders, (bible_folder) => {
+  await each_async(bible_folders, async (bible_folder) => {
     log({
       bible_folder,
     });
-    lambda(bible_folder);
+    await lambda(bible_folder);
   });
 }
