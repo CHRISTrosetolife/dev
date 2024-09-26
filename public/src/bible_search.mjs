5f4e317a45612522751c1b2b1f2c0f71a356967a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 17:14:01 -0400

    2024-09-26T21:14:00.747Z bible_search jesus

--- public/src/bible_search.mjs
+++ public/src/bible_search.mjs
@@ -6,7 +6,7 @@ import { string_split_comma } from "./string_split_comma.mjs";
 import { bible_search_lookup } from "./bible_search_lookup.mjs";
 export async function bible_search(words) {
   let s = string_split_comma(words);
-  let l = await bible_search_lookup();
+  let l = await bible_search_lookup_cache();
   each_object(l, (bible_folder, books) => {
     each_object_values(books, (chapter) => {
       log({
