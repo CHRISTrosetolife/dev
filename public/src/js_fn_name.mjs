bd1616fd5f177eb7d1adf630d6504de36aaa66c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 21:18:49 -0400

    2024-09-26T01:18:48.781Z function_auto_after js_fn_name

--- public/src/js_fn_name.mjs
+++ public/src/js_fn_name.mjs
@@ -1,6 +1,7 @@
 import { log } from "./log.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_fn_name(ast) {
+  log({});
   js_visit_node(ast, "MemberExpression", (v) => {
     log({
       v,
