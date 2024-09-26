4628750ab24c307483e26f6b388c3206e4cb580e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 18:47:24 -0400

    2024-09-26T22:47:23.729Z function_auto_after data_update_single

--- public/src/data_update_single.mjs
+++ public/src/data_update_single.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { js_visit_calls } from "./js_visit_calls.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { each } from "./each.mjs";
@@ -38,6 +39,7 @@ export function data_update_single(ast, data) {
     if (list_size_1(args)) {
       let a = list_single(a);
       if (js_node_type_is(a, "Literal")) {
+        object_property_get(object, "property_name");
       }
     }
   });
