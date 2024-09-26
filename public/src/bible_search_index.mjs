13a3eb7ec8b14464b43ade869374727d86fdf74b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 19:09:39 -0400

    2024-09-26T23:09:39.140Z function_auto_after bible_search

---
+++ public/src/bible_search_index.mjs
@@ -0,0 +1,36 @@
+import { list_add } from "./list_add.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
+import { list_unique } from "./list_unique.mjs";
+import { bible_words_map } from "./bible_words_map.mjs";
+import { each } from "./each.mjs";
+import { each_object } from "./each_object.mjs";
+import { bible_search_lookup_cache } from "./bible_search_lookup_cache.mjs";
+export async function bible_search_index() {
+  let symbols = "-–,;:!?.…·'‘’“”()[]{}¶*/&#%•`°|⌃►◄0123456789";
+  let i = {};
+  let lookup = await bible_search_lookup_cache();
+  each_object(lookup, (bible_folder, books) => {
+    each_object(books, (chapter_code, chapter_verses) => {
+      each(chapter_verses, (c) => {
+        let { tokens, verse_number } = c;
+        let mapped = bible_words_map(tokens, symbols);
+        let u = list_unique(mapped);
+        each(mapped, (m) => {
+          let chapter_codes = object_property_initialize(i, m, {});
+          let verse_numbers = object_property_initialize(
+            chapter_codes,
+            chapter_code,
+            {},
+          );
+          let folders = object_property_initialize(
+            verse_numbers,
+            verse_number,
+            [],
+          );
+          list_add(folders, bible_folder);
+        });
+      });
+    });
+  });
+  return i;
+}
