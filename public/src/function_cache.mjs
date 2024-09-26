e0ecb204637f38df77b19b7d391989a29e7b74e4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 17:26:16 -0400

    2024-09-26T21:26:13.097Z function_auto_after function_cache

--- public/src/function_cache.mjs
+++ public/src/function_cache.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { function_cache_path } from "./function_cache_path.mjs";
 import { file_write_json } from "./file_write_json.mjs";
 import { file_read_json } from "./file_read_json.mjs";
@@ -6,6 +7,7 @@ import { object_property_get } from "./object_property_get.mjs";
 export async function function_cache(fn, args) {
   let function_name = fn.name;
   let file_path = function_cache_path(function_name, args);
+  log({});
   let result;
   let object;
   if (await file_exists(file_path)) {
