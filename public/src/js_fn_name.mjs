cc7db60b7a4eb0d418e01ce9a19b3354d9259547
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 08:42:58 -0400

    2024-09-26T12:42:57.541Z function_auto_after js_fn_name

--- public/src/js_fn_name.mjs
+++ public/src/js_fn_name.mjs
@@ -1,5 +1,4 @@
 import { js_imports_add_specified } from "./js_imports_add_specified.mjs";
-import { log } from "./log.mjs";
 import { js_code_string } from "./js_code_string.mjs";
 import { js_code_call_args } from "./js_code_call_args.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -33,12 +32,8 @@ export async function js_fn_name(ast) {
       return;
     }
     let e = js_code_call_args(fn_name("fn_name"), [js_code_string(o_name)]);
-    log({
-      r,
-      e,
-    });
     let parsed = js_parse_expression(e);
     object_replace(node, parsed);
-    js_imports_add_specified();
+    js_imports_add_specified(ast, [fn_name.name]);
   });
 }
