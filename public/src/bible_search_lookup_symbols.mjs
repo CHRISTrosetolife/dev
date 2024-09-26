e90fb3f84958f0b9b1f9e0b844bc5d7e6c65a46a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 18:56:08 -0400

    2024-09-26T22:56:07.282Z function_auto_after bible_search_lookup_symbols

--- public/src/bible_search_lookup_symbols.mjs
+++ public/src/bible_search_lookup_symbols.mjs
@@ -1,14 +1,17 @@
+import { list_is } from "./list_is.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 import { each } from "./each.mjs";
 import { string_symbols_multiple } from "./string_symbols_multiple.mjs";
 import { each_object_values } from "./each_object_values.mjs";
 import { bible_search_lookup_cache } from "./bible_search_lookup_cache.mjs";
+import { assert_message } from "./assert_message.mjs";
 export async function bible_search_lookup_symbols() {
   let l = await bible_search_lookup_cache();
   let ts = list_adder_unique((la) => {
     each_object_values(l, (books) => {
       each_object_values(books, (chapter) => {
         let { tokens } = chapter;
+        assert_message(list_is, [tokens], () => {});
         each(tokens, la);
       });
     });
