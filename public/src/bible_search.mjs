a9e8119020c6b700e8b478cb89f3036410122fd9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 19:05:45 -0400

    2024-09-26T23:05:44.345Z function_auto_after bible_search

--- public/src/bible_search.mjs
+++ public/src/bible_search.mjs
@@ -1,7 +1,6 @@
 import { bible_words_map } from "./bible_words_map.mjs";
 import { each } from "./each.mjs";
 import { each_object } from "./each_object.mjs";
-import { list_adder_unique } from "./list_adder_unique.mjs";
 import { bible_search_lookup_cache } from "./bible_search_lookup_cache.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 export async function bible_search(words) {
@@ -9,14 +8,12 @@ export async function bible_search(words) {
   let symbols = "-–,;:!?.…·'‘’“”()[]{}¶*/&#%•`°|⌃►◄0123456789";
   let i = {};
   let lookup = await bible_search_lookup_cache();
-  let ts = list_adder_unique((la) => {
-    each_object(lookup, (bible_folder, books) => {
-      each_object(books, (chapter_code, chapter_verses) => {
-        each(chapter_verses, (c) => {
-          let { tokens } = c;
-          let mapped = bible_words_map(tokens, symbols);
-          each(mapped, (m) => {});
-        });
+  each_object(lookup, (bible_folder, books) => {
+    each_object(books, (chapter_code, chapter_verses) => {
+      each(chapter_verses, (c) => {
+        let { tokens } = c;
+        let mapped = bible_words_map(tokens, symbols);
+        each(mapped, (m) => {});
       });
     });
   });
