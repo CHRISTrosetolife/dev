27000f3348905c1d64ddb55ae1de3d704c21ca7d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 21:18:08 -0400

    2024-09-26T01:18:07.468Z function_auto_after js_fn_name

--- public/src/js_fn_name.mjs
+++ public/src/js_fn_name.mjs
@@ -1,4 +1,7 @@
+import { log } from "./log.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_fn_name(ast) {
-  js_visit_node(ast, "MemberExpression", (v) => {});
+  js_visit_node(ast, "MemberExpression", (v) => {
+    log({});
+  });
 }
