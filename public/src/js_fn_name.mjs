305e8e780aa753407c2aea22ac4cbed3daf46c74
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-25 21:25:19 -0400

    2024-09-26T01:25:18.632Z function_auto_after js_fn_name

--- public/src/js_fn_name.mjs
+++ public/src/js_fn_name.mjs
@@ -1,3 +1,4 @@
+import { function_exists } from "./function_exists.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
@@ -6,7 +7,7 @@ import { log } from "./log.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_fn_name(ast) {
   fn_name.name;
-  js_visit_node(ast, "MemberExpression", (v) => {
+  js_visit_node(ast, "MemberExpression", async (v) => {
     let { node } = v;
     let { object, property } = node;
     let r = false;
@@ -19,8 +20,10 @@ export function js_fn_name(ast) {
       r = true;
     }
     let o_name = object_property_get(object, "name");
-    if (r) {
-      return;
+    if (!(await function_exists(o_name))) {
+      r = true;
+    }
+    if (false) {
     }
     log(node);
   });
