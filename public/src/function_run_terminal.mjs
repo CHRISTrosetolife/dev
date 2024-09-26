a554243aeb96ce0fd9eda78778f028424bde16ca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 18:01:59 -0400

    2024-09-26T22:01:58.483Z bible_search_lookup_cache

--- public/src/function_run_terminal.mjs
+++ public/src/function_run_terminal.mjs
@@ -35,7 +35,7 @@ export async function function_run_terminal(function_name, args) {
           " ",
           file_path_output,
         ]);
-        if (0) {
+        if (10) {
           log({
             command,
           });
