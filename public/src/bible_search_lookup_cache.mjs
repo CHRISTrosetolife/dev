6d9459801fa3e85c69f2614875e61e011f10e469
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 18:11:55 -0400

    2024-09-26T22:11:54.337Z function_auto_after bible_search_lookup_cache

--- public/src/bible_search_lookup_cache.mjs
+++ public/src/bible_search_lookup_cache.mjs
@@ -1,7 +1,5 @@
-import { log } from "./log.mjs";
 import { bible_search_lookup } from "./bible_search_lookup.mjs";
 import { function_cache } from "./function_cache.mjs";
 export async function bible_search_lookup_cache() {
-  let c = await function_cache(bible_search_lookup, []);
-  return c;
+  return await function_cache(bible_search_lookup, []);
 }
