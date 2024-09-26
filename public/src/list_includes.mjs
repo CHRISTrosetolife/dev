81cdcbbe186cb9e50b13e0f09f1856c83bad5fe7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 16:38:12 -0400

    2024-09-26T20:38:11.717Z function_auto_after list_includes

--- public/src/list_includes.mjs
+++ public/src/list_includes.mjs
@@ -1,5 +1,8 @@
+import { never } from "./never.mjs";
+import { assert } from "./assert.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function list_includes(list, item) {
   assert_arguments_length(arguments, 2);
+  assert(never, []);
   return list.includes(item);
 }
