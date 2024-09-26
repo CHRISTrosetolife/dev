c2cf450fdc64d6554de77f315809100c4fbdc48a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 15:27:19 -0400

    2024-09-26T19:27:18.603Z function_auto_after bible_search

--- public/src/bible_search.mjs
+++ public/src/bible_search.mjs
@@ -4,11 +4,12 @@ import { bible_books_chapter_each } from "./bible_books_chapter_each.mjs";
 export async function bible_search() {
   let r = {};
   let vs = bible_eng_versions();
-  await each_async(vs, async (v) => {});
-  await bible_books_chapter_each(bible_folder, function lambda(chapter_code) {
-    la({
-      book_code,
-      chapter_code,
+  await each_async(vs, async (v) => {
+    await bible_books_chapter_each(bible_folder, function lambda(chapter_code) {
+      la({
+        book_code,
+        chapter_code,
+      });
     });
   });
 }
