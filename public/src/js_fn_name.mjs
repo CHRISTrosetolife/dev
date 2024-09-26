d1dd5d11a205b3e3acc9e458c3198f022871ad74
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 08:37:10 -0400

    2024-09-26T12:37:09.887Z function_auto_after js_fn_name

--- public/src/js_fn_name.mjs
+++ public/src/js_fn_name.mjs
@@ -6,6 +6,7 @@ import { each } from "./each.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
+import { js_parse_expression } from "./js_parse_expression.mjs";
 export function js_fn_name(ast) {
   js_visit_node(ast, "MemberExpression", async (v) => {
     let { node } = v;
@@ -27,5 +28,6 @@ export function js_fn_name(ast) {
       return;
     }
     let e = js_code_call_args(fn_name.name, [js_code_string(o_name)]);
+    js_parse_expression(e);
   });
 }
