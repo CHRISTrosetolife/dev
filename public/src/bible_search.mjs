f721459111b0823097f52d545a272450027820fc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 15:27:03 -0400

    2024-09-26T19:27:02.520Z function_auto_after bible_search

--- public/src/bible_search.mjs
+++ public/src/bible_search.mjs
@@ -1,15 +1,12 @@
 import { bible_eng_versions } from "./bible_eng_versions.mjs";
 import { bible_books_chapter_each } from "./bible_books_chapter_each.mjs";
-import { list_adder_async } from "./list_adder_async.mjs";
 export async function bible_search() {
   let r = {};
   let v = bible_eng_versions();
-  return await list_adder_async(async (la) => {
-    await bible_books_chapter_each(bible_folder, function lambda(chapter_code) {
-      la({
-        book_code,
-        chapter_code,
-      });
+  await bible_books_chapter_each(bible_folder, function lambda(chapter_code) {
+    la({
+      book_code,
+      chapter_code,
     });
   });
 }
