a6025c14fad9a1b01d151f210f02aa9a875b811d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 18:51:50 -0400

    2024-09-26T22:51:49.261Z function_auto_after bible_search

--- public/src/bible_search.mjs
+++ public/src/bible_search.mjs
@@ -6,7 +6,7 @@ export async function bible_search(words) {
   let s = string_split_comma(words);
   let i = {};
   let l = await bible_search_lookup_cache();
-  list_adder_multiple((la) => {
+  let ts = list_adder_multiple((la) => {
     each_object(l, (bible_folder, books) => {
       each_object(books, (chapter_code, chapter) => {
         let { verse_number, tokens } = chapter;
