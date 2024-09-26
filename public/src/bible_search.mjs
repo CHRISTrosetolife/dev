06a7c89f855b719bfdd412031848c1283ce42985
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 19:03:02 -0400

    2024-09-26T23:03:01.730Z function_auto_after bible_search

--- public/src/bible_search.mjs
+++ public/src/bible_search.mjs
@@ -1,8 +1,28 @@
-import { bible_search_lookup_symbols } from "./bible_search_lookup_symbols.mjs";
+import { list_is } from "./list_is.mjs";
+import { assert_message } from "./assert_message.mjs";
+import { each } from "./each.mjs";
+import { each_object } from "./each_object.mjs";
+import { list_adder_unique } from "./list_adder_unique.mjs";
+import { bible_search_lookup_cache } from "./bible_search_lookup_cache.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 export async function bible_search(words) {
   let s = string_split_comma(words);
   let symbols = "-–,;:!?.…·'‘’“”()[]{}¶*/&#%•`°|⌃►◄0123456789";
   let i = {};
-  return await bible_search_lookup_symbols();
+  let lookup = await bible_search_lookup_cache();
+  let ts = list_adder_unique((la) => {
+    each_object(lookup, (bible_folder, books) => {
+      each_object(books, (chapter_code, chapter_verses) => {
+        each(chapter_verses, (c) => {
+          let { tokens } = c;
+          assert_message(list_is, [tokens], () => ({
+            bible_folder,
+            chapter_code,
+            chapter,
+          }));
+          each(tokens, la);
+        });
+      });
+    });
+  });
 }
