76e310b4d854c8b2a5a77d47d9adbcd984904a4f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 17:12:49 -0400

    2024-09-26T21:12:49.158Z function_auto_after app_memorize_keyboard_reset

--- public/src/app_memorize_keyboard_reset.mjs
+++ public/src/app_memorize_keyboard_reset.mjs
@@ -2,7 +2,9 @@ import { app_memorize_button_keyboard_stylize } from "./app_memorize_button_keyb
 import { object_values } from "./object_values.mjs";
 import { each } from "./each.mjs";
 export function app_memorize_keyboard_reset(context) {
-  each(object_values(context.keyboard_buttons), (keyboard_button) => {
+  let o = context.keyboard_buttons;
+  let lambda = (keyboard_button) => {
     app_memorize_button_keyboard_stylize(context, keyboard_button);
-  });
+  };
+  each(object_values(o), lambda);
 }
