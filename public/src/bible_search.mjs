260a3ae5262b819135c648ab18ab62b34c53826b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 15:31:49 -0400

    2024-09-26T19:31:48.347Z function_auto_after bible_search

--- public/src/bible_search.mjs
+++ public/src/bible_search.mjs
@@ -1,22 +1,13 @@
-import { bible_chapter } from "./bible_chapter.mjs";
+import { bible_search_lookup_version } from "./bible_search_lookup_version.mjs";
 import { object_property_set } from "./object_property_set.mjs";
-import { object_property_initialize } from "./object_property_initialize.mjs";
 import { each_async } from "./each_async.mjs";
 import { bible_eng_versions } from "./bible_eng_versions.mjs";
-import { bible_books_chapter_each } from "./bible_books_chapter_each.mjs";
 export async function bible_search() {
   let r = {};
   let bible_folders = bible_eng_versions();
   await each_async(bible_folders, async (bible_folder) => {
-    let v = object_property_initialize(r, bible_folder, {});
-    await bible_books_chapter_each(
-      bible_folder,
-      async function lambda(chapter_code) {
-        let c = await bible_chapter(bible_folder, chapter_code);
-        object_property_set(v, chapter_code, c);
-      },
-    );
-    object_property_set(object, "property_name", value);
+    let v = await bible_search_lookup_version(bible_folder);
+    object_property_set(r, bible_folder, v);
   });
   return r;
 }
---
+++ public/src/bible_search_lookup_version.mjs
@@ -0,0 +1,14 @@
+import { object_property_set } from "./object_property_set.mjs";
+import { bible_chapter } from "./bible_chapter.mjs";
+import { bible_books_chapter_each } from "./bible_books_chapter_each.mjs";
+export async function bible_search_lookup_version(bible_folder) {
+  let v = {};
+  await bible_books_chapter_each(
+    bible_folder,
+    async function lambda(chapter_code) {
+      let c = await bible_chapter(bible_folder, chapter_code);
+      object_property_set(v, chapter_code, c);
+    },
+  );
+  return v;
+}
