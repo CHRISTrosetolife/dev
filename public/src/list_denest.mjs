fdb91542c96aabf68bd9a550770db55d84eea75a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 20:44:08 -0400

    2024-09-27T00:44:08.047Z function_auto_after list_denest

--- public/src/list_denest.mjs
+++ public/src/list_denest.mjs
@@ -1,7 +1,7 @@
 import { list_denest_recursive } from "./list_denest_recursive.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function list_denest(list) {
-  list_adder((la) => {
+  return list_adder((la) => {
     list_denest_recursive(list, la);
   });
 }
