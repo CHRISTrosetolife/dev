466a93eb559c2ef208b3ccdc55eec6cf162c6f38
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 20:13:23 -0400

    2024-09-27T00:13:22.632Z function_auto_after bible_search

--- public/src/bible_search.mjs
+++ public/src/bible_search.mjs
@@ -4,6 +4,7 @@ import { bible_search_symbols_map } from "./bible_search_symbols_map.mjs";
 import { bible_search_index_cache } from "./bible_search_index_cache.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 import { list_map } from "./list_map.mjs";
+import { list_first } from "./list_first.mjs";
 export async function bible_search(words) {
   let s = string_split_comma(words);
   let mapped = bible_search_symbols_map(s);
@@ -11,5 +12,6 @@ export async function bible_search(words) {
   let results = list_map(mapped, (m) => {
     return object_property_get(i, m);
   });
+  let r0 = list_first(results);
   return object_properties(results);
 }
