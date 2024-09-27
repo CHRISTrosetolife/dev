d74fdcc69ae8b6587a784e3f97ee74af7db112ae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-27 09:45:53 -0400

    2024-09-27T13:45:52.972Z function_auto_after bible_chapter_text

--- public/src/bible_chapter_text.mjs
+++ public/src/bible_chapter_text.mjs
@@ -1,15 +1,15 @@
+import { bible_reference_code } from "./bible_reference_code.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { bible_chapter } from "./bible_chapter.mjs";
 import { list_join_space } from "./list_join_space.mjs";
-import { bible_reference } from "./bible_reference.mjs";
 export async function bible_chapter_text(bible_folder, chapter_name) {
   let verses = await bible_chapter(bible_folder, chapter_name);
   let m = list_map(verses, (verse) => {
     let { verse_number, tokens } = verse;
     return string_combine_multiple([
-      bible_reference,
+      bible_reference_code(chapter_name),
       verse_number,
       " ",
       list_join_space(tokens),
