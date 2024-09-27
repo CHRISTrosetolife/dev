518c8d10b214d8fccf1e86d4d33c4102105f6761
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-27 09:44:19 -0400

    2024-09-27T13:44:18.938Z function_auto_after bible_chapter_text

--- public/src/bible_chapter_text.mjs
+++ public/src/bible_chapter_text.mjs
@@ -1,7 +1,6 @@
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
-import { list_concat_multiple } from "./list_concat_multiple.mjs";
 import { bible_chapter } from "./bible_chapter.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 export async function bible_chapter_text(bible_folder, chapter_name) {
@@ -14,5 +13,5 @@ export async function bible_chapter_text(bible_folder, chapter_name) {
       list_join_space(tokens),
     ]);
   });
-  return list_join_newline(list_concat_multiple(t));
+  return list_join_newline(m);
 }
