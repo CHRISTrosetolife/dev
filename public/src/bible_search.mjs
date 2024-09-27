76eb97fde108df9bf39634abb3bbf9fc5bef198e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 20:05:51 -0400

    2024-09-27T00:05:50.160Z function_auto_after bible_search

--- public/src/bible_search.mjs
+++ public/src/bible_search.mjs
@@ -1,6 +1,8 @@
+import { bible_words_map } from "./bible_words_map.mjs";
 import { bible_search_index_cache } from "./bible_search_index_cache.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 export async function bible_search(words) {
   let s = string_split_comma(words);
+  s = bible_words_map(s, symbols);
   let i = await bible_search_index_cache();
 }
