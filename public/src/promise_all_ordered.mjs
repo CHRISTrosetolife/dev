9a210c952643cee58d5a8706072b5c80c0321f07
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-27 09:29:07 -0400

    2024-09-27T13:29:07.476Z function_auto_after promise_all_ordered

--- public/src/promise_all_ordered.mjs
+++ public/src/promise_all_ordered.mjs
@@ -1,9 +1,5 @@
-import { each_async } from "./each_async.mjs";
 import { identity } from "./identity.mjs";
 import { list_map_async } from "./list_map_async.mjs";
 export async function promise_all_ordered(promises) {
   return await list_map_async(promises, identity);
-  await each_async(promises, async (promise) => {
-    await promise();
-  });
 }
