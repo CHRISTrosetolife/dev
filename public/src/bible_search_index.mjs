50b2cd509ffd37e21ddf30357d85803a35cc5629
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 20:06:58 -0400

    2024-09-27T00:06:55.953Z function_auto_after bible_search_index

--- public/src/bible_search_index.mjs
+++ public/src/bible_search_index.mjs
@@ -7,13 +7,13 @@ import { each } from "./each.mjs";
 import { each_object } from "./each_object.mjs";
 import { bible_search_lookup_cache } from "./bible_search_lookup_cache.mjs";
 export async function bible_search_index() {
-  let symbols = bible_search_symbols();
   let i = {};
   let lookup = await bible_search_lookup_cache();
   each_object(lookup, (bible_folder, books) => {
     each_object(books, (chapter_code, chapter_verses) => {
       each(chapter_verses, (c) => {
         let { tokens, verse_number } = c;
+        let symbols = bible_search_symbols();
         let mapped = bible_words_map(tokens, symbols);
         let us = list_unique(mapped);
         each(us, (u) => {
