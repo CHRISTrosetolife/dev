e71e915101cfda9e7525fdcb08aeeeaac7fe07c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-26 20:43:05 -0400

    2024-09-27T00:43:04.673Z function_auto_after list_denest

--- public/src/list_denest.mjs
+++ public/src/list_denest.mjs
@@ -1,8 +1,11 @@
+import { list_is } from "./list_is.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { each } from "./each.mjs";
 export function list_denest(list) {
   list_adder((la) => {
     each(list, (item) => {
+      if (list_is(item)) {
+      }
       la(item);
     });
   });
