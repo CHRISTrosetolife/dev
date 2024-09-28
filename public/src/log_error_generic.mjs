9ec0cd5230e50e6981269b283d4d4b3c2d83d923
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-27 20:29:19 -0400

    2024-09-28T00:29:14.529Z b listener,foreigner

--- public/src/log_error_generic.mjs
+++ public/src/log_error_generic.mjs
@@ -1,6 +1,7 @@
 import { chalk } from "./chalk.mjs";
 import { string_take } from "./string_take.mjs";
 export function log_error_generic(fn, message) {
+  return;
   message = string_take(message, 100);
   fn(chalk().redBright(message));
 }
