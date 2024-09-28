86acaad3eef7dfb034493685c3d537b6a453015e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 09:38:52 -0400

    2024-09-28T13:38:51.587Z function_auto_after sermon_format

--- public/src/sermon_format.mjs
+++ public/src/sermon_format.mjs
@@ -1,5 +1,4 @@
-import { sermon_overwrite } from "./sermon_overwrite.mjs";
-import { sermon_lines } from "./sermon_lines.mjs";
+import { sermon_transform } from "./sermon_transform.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { keyboard_keys } from "./keyboard_keys.mjs";
 import { each } from "./each.mjs";
@@ -10,22 +9,23 @@ import { string_replace } from "./string_replace.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { string_empty_is } from "./string_empty_is.mjs";
 export async function sermon_format(name) {
-  let lines = await sermon_lines(name);
-  let numbered = list_filter(lines, (line) => {
-    if (string_empty_is(line)) {
+  await sermon_transform(sermon_name, (lines) => {
+    let numbered = list_filter(lines, (line) => {
+      if (string_empty_is(line)) {
+        return false;
+      }
+      let first = string_first(line);
+      if (string_digits_is(first)) {
+        return true;
+      }
       return false;
-    }
-    let first = string_first(line);
-    if (string_digits_is(first)) {
-      return true;
-    }
-    return false;
-  });
-  let mapped = list_map(numbered, (n) => {
-    each(keyboard_keys(), (k) => {
-      n = string_replace(n, string_combine_multiple(["[", k, "]"]), "");
     });
-    return n;
+    let mapped = list_map(numbered, (n) => {
+      each(keyboard_keys(), (k) => {
+        n = string_replace(n, string_combine_multiple(["[", k, "]"]), "");
+      });
+      return n;
+    });
+    return mapped;
   });
-  await sermon_overwrite(name, mapped);
 }
