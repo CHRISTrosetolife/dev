6347aefc5a8576504968cd083f5293c4cc3affe5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 16:17:30 -0400

    2024-09-28T20:17:29.922Z function_auto_after app_todo

--- public/src/app_todo.mjs
+++ public/src/app_todo.mjs
@@ -1,6 +1,6 @@
-import { firebase_upload } from "./firebase_upload.mjs";
+import { firebase_download } from "./firebase_download.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_todo() {
   let body = html_style_default_initialize();
-  await firebase_upload("todo/list.json", []);
+  await firebase_download("todo/list.json", []);
 }
