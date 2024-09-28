17aeec3abab99b44244808df96db60ad9c905486
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-27 20:34:33 -0400

    2024-09-28T00:34:31.864Z b foreigner,speaker

--- public/src/log_error_generic.mjs
+++ public/src/log_error_generic.mjs
@@ -1,6 +1,4 @@
 import { chalk } from "./chalk.mjs";
-import { string_take } from "./string_take.mjs";
 export function log_error_generic(fn, message) {
-  message = string_take(message, 1);
   fn(chalk().redBright(message));
 }
