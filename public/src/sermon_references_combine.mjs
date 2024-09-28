58cb8b5a23650a7e14523c233637c61642bdd54a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 09:39:19 -0400

    2024-09-28T13:39:19.330Z function_auto_after sermon_references_combine

--- public/src/sermon_references_combine.mjs
+++ public/src/sermon_references_combine.mjs
@@ -1,5 +1,4 @@
 import { sermon_transform } from "./sermon_transform.mjs";
-import { identity } from "./identity.mjs";
 export async function sermon_references_combine(sermon_name) {
-  await sermon_transform(sermon_name, identity);
+  await sermon_transform(sermon_name, () => {});
 }
