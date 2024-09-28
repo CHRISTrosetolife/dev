03ae3f38df1d17b4cbe5876444c859c60dd2caf6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 16:20:55 -0400

    2024-09-28T20:20:54.201Z function_auto_after app_record

--- public/src/app_record.mjs
+++ public/src/app_record.mjs
@@ -20,36 +20,34 @@ export async function app_record() {
   let auth = firebase_auth();
   onAuthStateChanged(auth, async (user) => {
     if (user) {
-      let context = {};
-      context.mr = await html_recorder_media();
-      object_merge(context, {
-        root,
-      });
-      await app_context_books_bible(context);
-      let lookup = html_hash_lookup();
-      let hash_book = object_property_get_or(lookup, "book", null);
-      if (hash_book !== null) {
-        let hash_chapter = object_property_get_or(lookup, "chapter", null);
-        if (hash_chapter !== null) {
-          let hash_verse = object_property_get_or(lookup, "verse", null);
-          if (hash_verse !== null) {
-            await app_record_verse(
-              context,
-              hash_book,
-              hash_chapter,
-              hash_verse,
-            );
-          } else {
-            await app_record_chapter(context, hash_book, hash_chapter);
-          }
-        } else {
-          app_record_book(context, hash_book);
-        }
-      } else {
-        app_record_home(context);
-      }
+      await on_logged_in();
     } else {
       app_record_login(root);
     }
   });
+  async function on_logged_in() {
+    let context = {};
+    context.mr = await html_recorder_media();
+    object_merge(context, {
+      root,
+    });
+    await app_context_books_bible(context);
+    let lookup = html_hash_lookup();
+    let hash_book = object_property_get_or(lookup, "book", null);
+    if (hash_book !== null) {
+      let hash_chapter = object_property_get_or(lookup, "chapter", null);
+      if (hash_chapter !== null) {
+        let hash_verse = object_property_get_or(lookup, "verse", null);
+        if (hash_verse !== null) {
+          await app_record_verse(context, hash_book, hash_chapter, hash_verse);
+        } else {
+          await app_record_chapter(context, hash_book, hash_chapter);
+        }
+      } else {
+        app_record_book(context, hash_book);
+      }
+    } else {
+      app_record_home(context);
+    }
+  }
 }
