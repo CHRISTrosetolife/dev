38a422e496cf190d8e3f12a98673849285e77762
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 09:35:29 -0400

    2024-09-28T13:35:28.815Z function_auto_after sermon_format

--- public/src/sermon_format.mjs
+++ public/src/sermon_format.mjs
@@ -1,3 +1,4 @@
+import { sermon_lines } from "./sermon_lines.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { keyboard_keys } from "./keyboard_keys.mjs";
@@ -6,17 +7,12 @@ import { list_map } from "./list_map.mjs";
 import { string_digits_is } from "./string_digits_is.mjs";
 import { string_first } from "./string_first.mjs";
 import { string_replace } from "./string_replace.mjs";
-import { file_read } from "./file_read.mjs";
 import { sermon_path } from "./sermon_path.mjs";
-import { string_split_newline } from "./string_split_newline.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { string_empty_is } from "./string_empty_is.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 export async function sermon_format(name) {
-  let sermon_path_file = sermon_path(name);
-  let text = await file_read(sermon_path_file);
-  text = string_replace(text, "\r", "");
-  let lines = string_split_newline(text);
+  let lines = await sermon_lines(name);
   let numbered = list_filter(lines, (line) => {
     if (string_empty_is(line)) {
       return false;
@@ -33,6 +29,7 @@ export async function sermon_format(name) {
     });
     return n;
   });
-  text = list_join_space(mapped);
+  let text = list_join_space(mapped);
+  let sermon_path_file = sermon_path(name);
   await file_overwrite(sermon_path_file, text);
 }
---
+++ public/src/sermon_lines.mjs
@@ -0,0 +1,11 @@
+import { string_split_newline } from "./string_split_newline.mjs";
+import { string_replace } from "./string_replace.mjs";
+import { file_read } from "./file_read.mjs";
+import { sermon_path } from "./sermon_path.mjs";
+export async function sermon_lines(name) {
+  let sermon_path_file = sermon_path(name);
+  let text = await file_read(sermon_path_file);
+  text = string_replace(text, "\r", "");
+  let lines = string_split_newline(text);
+  return lines;
+}
