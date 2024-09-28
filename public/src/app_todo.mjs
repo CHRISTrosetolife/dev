b98520f076305975e40b1914efbf9a114f7346e5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 16:26:30 -0400

    2024-09-28T20:26:29.753Z function_auto_after app_todo

--- public/src/app_todo.mjs
+++ public/src/app_todo.mjs
@@ -4,7 +4,7 @@ import { log } from "./log.mjs";
 import { firebase_list } from "./firebase_list.mjs";
 export async function app_todo() {
   await app_firebase(async (context) => {
-    let root = context;
+    let { root } = context;
     html_clear_scroll_top(root);
     let r = await firebase_list("todo/list.json");
     log({
