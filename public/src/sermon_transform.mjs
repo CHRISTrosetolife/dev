af373a4ad4f079503d3d68c7594add53572ee19f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 10:02:35 -0400

    2024-09-28T14:02:35.362Z sermon_references_combine pastor_humility

--- public/src/sermon_transform.mjs
+++ public/src/sermon_transform.mjs
@@ -4,6 +4,5 @@ import { sermon_lines } from "./sermon_lines.mjs";
 export async function sermon_transform(sermon_name, transform) {
   let lines = await sermon_lines(sermon_name);
   let mapped = transform(lines);
-  let text = list_join_newline(mapped);
-  await sermon_overwrite(sermon_name, text);
+  await sermon_overwrite(sermon_name, mapped);
 }
