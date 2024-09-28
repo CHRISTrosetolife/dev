6cb86c46d3bdeb161d65b0bdd365f343013430a4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 16:17:08 -0400

    2024-09-28T20:17:05.631Z function_auto_after firebase_download

--- public/src/firebase_download.mjs
+++ public/src/firebase_download.mjs
@@ -4,7 +4,7 @@ import {
   ref,
   getDownloadURL,
 } from "https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.2/firebase-storage.min.js";
-export async function firebase_download(storage_path, blob) {
+export async function firebase_download(storage_path) {$aa
   let storage = getStorage();
   let storageRef = ref(storage, storage_path);
   let url = await getDownloadURL(storageRef);
