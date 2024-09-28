880b9b00a044a85fe4bcf122040959ee5850340c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 10:01:27 -0400

    2024-09-28T14:01:26.598Z function_auto_after sermon_transform

--- public/src/sermon_transform.mjs
+++ public/src/sermon_transform.mjs
@@ -1,7 +1,9 @@
+import { list_join_newline } from "./list_join_newline.mjs";
 import { sermon_overwrite } from "./sermon_overwrite.mjs";
 import { sermon_lines } from "./sermon_lines.mjs";
 export async function sermon_transform(sermon_name, transform) {
   let lines = await sermon_lines(sermon_name);
   let mapped = transform(lines);
+  let text = list_join_newline(mapped);
   await sermon_overwrite(sermon_name, mapped);
 }
