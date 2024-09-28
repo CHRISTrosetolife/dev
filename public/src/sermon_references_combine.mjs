b3c7733758ce16a0cb1546e95c90797874538c65
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 10:23:16 -0400

    2024-09-28T14:23:15.895Z function_auto_after sermon_references_combine

--- public/src/sermon_references_combine.mjs
+++ public/src/sermon_references_combine.mjs
@@ -49,7 +49,9 @@ export async function sermon_references_combine(sermon_name) {
     });
     list_adder((la) => {
       let previous_reference_is = false;
-      each(prepared, (p) => {});
+      each(prepared, (p) => {
+        let { line, reference_is } = p;
+      });
     });
     return lines;
   });
