301425030494e90faf781c7f0c420ce53fd241b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 16:28:38 -0400

    2024-09-28T20:28:37.808Z function_auto_after app_todo

--- public/src/app_todo.mjs
+++ public/src/app_todo.mjs
@@ -6,7 +6,7 @@ export async function app_todo() {
   await app_firebase(async (context) => {
     let { root } = context;
     html_clear_scroll_top(root);
-    let { items } = await firebase_list("en/to/ceb/group/");
+    let { items } = await firebase_list("todo/");
     log({
       items,
     });
