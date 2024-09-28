18d3ecd07f8a6d89b448b9bf48dcefff34606ab8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 16:15:07 -0400

    2024-09-28T20:15:04.989Z c firebase_upload firebase_download

---
+++ public/src/firebase_download.mjs
@@ -0,0 +1,10 @@
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+} from "https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.2/firebase-storage.min.js";
+export async function firebase_download(storage_path, blob) {
+  let storage = getStorage();
+  let storageRef = ref(storage, storage_path);
+  let snapshot = await uploadBytes(storageRef, blob);
+}
