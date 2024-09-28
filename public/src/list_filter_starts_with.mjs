65d2a3aeb99ab70c4821120f496906a1ee13c58e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 09:46:03 -0400

    2024-09-28T13:46:03.447Z function_auto_after list_filter_starts_with

--- public/src/list_filter_starts_with.mjs
+++ public/src/list_filter_starts_with.mjs
@@ -1,5 +1,5 @@
-import { string_starts_with } from "./string_starts_with.mjs";
+import { string_starts_with_curry } from "./string_starts_with_curry.mjs";
 import { list_filter } from "./list_filter.mjs";
 export function list_filter_starts_with(list, prefix) {
-  return list_filter(list, (item) => string_starts_with(item, prefix));
+  return list_filter(list, string_starts_with_curry(prefix));
 }
