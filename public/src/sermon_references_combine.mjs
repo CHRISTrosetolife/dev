6fd6955e97d65fe6981e989b6725080858cff6bb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 12:39:09 -0400

    2024-09-28T16:39:08.777Z function_auto_after sermon_references_combine

--- public/src/sermon_references_combine.mjs
+++ public/src/sermon_references_combine.mjs
@@ -1,3 +1,4 @@
+import { list_add } from "./list_add.mjs";
 import { integer_parse } from "./integer_parse.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { equal_by_property_multiple } from "./equal_by_property_multiple.mjs";
@@ -68,6 +69,7 @@ export async function sermon_references_combine(sermon_name) {
               verse_number = integer_parse(verse_number);
               previous_verse_number = integer_parse(previous_verse_number);
               if (previous_verse_number + 1 === verse_number) {
+                list_add(group, p);
                 let { book, chapter_name } = p;
               }
             }
