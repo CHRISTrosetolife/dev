bc304abb321be7c016310256f2499adc706dfb70
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 16:19:24 -0400

    2024-09-28T20:19:23.425Z function_auto_after firebase_list

--- public/src/firebase_list.mjs
+++ public/src/firebase_list.mjs
@@ -1,14 +1,13 @@
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
-import { http_get } from "./http_get.mjs";
 import {
   getStorage,
   ref,
-  getDownloadURL,
+  listAll,
 } from "https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.2/firebase-storage.min.js";
 export async function firebase_list(storage_path) {
   assert_arguments_length(arguments, 1);
   let storage = getStorage();
   let storageRef = ref(storage, storage_path);
-  let url = await getDownloadURL(storageRef);
-  return await http_get(url);
+  let r = await listAll(storageRef);
+  return r;
 }
