bca2343f19c1025674c8914366df67792982294f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 09:53:43 -0400

    2024-09-28T13:53:42.851Z function_auto_after bible_book_to_reference_prefix

--- public/src/bible_book_to_reference_prefix.mjs
+++ public/src/bible_book_to_reference_prefix.mjs
@@ -1,4 +1,4 @@
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function bible_book_to_reference_prefix(book) {
-  return string_combine_multiple([book, ":"]);
+  return string_combine_multiple([book, " "]);
 }
