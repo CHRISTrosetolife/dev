d32b123116369fc08007e1c2bae34615b55f29b6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 16:23:48 -0400

    2024-09-28T20:23:47.578Z function_auto_after app_firebase

--- public/src/app_firebase.mjs
+++ public/src/app_firebase.mjs
@@ -3,6 +3,7 @@ import { firebase_auth } from "./firebase_auth.mjs";
 import { firebase_initialize } from "./firebase_initialize.mjs";
 import { html_script_axios } from "./html_script_axios.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 export async function app_firebase(on_logged_in) {
   let root = html_style_default_initialize();
   await html_script_axios(root);
