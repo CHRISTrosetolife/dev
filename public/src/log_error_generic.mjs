e8a6b7ecca6bf995e76bd6455049aa8266f43441
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-27 20:22:53 -0400

    2024-09-28T00:22:52.464Z function_auto_after log_error_generic

--- public/src/log_error_generic.mjs
+++ public/src/log_error_generic.mjs
@@ -1,4 +1,6 @@
 import { chalk } from "./chalk.mjs";
+import { string_take } from "./string_take.mjs";
 export function log_error_generic(fn, message) {
+  message = string_take(message, 10000);
   fn(chalk().redBright(message));
 }
