403c4e38d72387340f14d5449fa5f34fd2a1a0a1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 12:34:34 -0400

    2024-09-28T16:34:34.261Z function_auto_after app_gs_xy_equal

--- public/src/app_gs_xy_equal.mjs
+++ public/src/app_gs_xy_equal.mjs
@@ -2,5 +2,6 @@ import { equal_by_property } from "./equal_by_property.mjs";
 import { list_all } from "./list_all.mjs";
 import { list_xy } from "./list_xy.mjs";
 export function app_gs_xy_equal(a, b) {
-  return list_all(list_xy(), (xy) => equal_by_property(a, b, xy));
+  let properties = list_xy();
+  return list_all(properties, (xy) => equal_by_property(a, b, xy));
 }
