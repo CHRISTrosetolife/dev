9b1a4ea5a5eca0eb4a28e0748e6b2b1f8b66fb0d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-29 14:41:57 -0400

    2024-09-29T18:41:56.948Z function_auto_after js_param_delete

--- public/src/js_param_delete.mjs
+++ public/src/js_param_delete.mjs
@@ -6,8 +6,8 @@ import { js_param_new_generic } from "./js_param_new_generic.mjs";
 export async function js_param_delete(ast, function_name, param_name) {
   let param_index = integer_parse(param_index);
   assert(number_is, [param_index]);
-  $ex;
   let lambda_add = (list, item) => list_insert(list, param_index, item);
+  $ex;
   await js_param_new_generic(
     ast,
     function_name,
