fd32712b610bfa572bb3a171565a56f90f339709
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-29 11:50:33 -0400

    2024-09-29T15:50:33.038Z function_auto_after app_todo_main

---
+++ public/src/app_todo_list.mjs
@@ -0,0 +1,31 @@
+import { app_todo_item_choices } from "./app_todo_item_choices.mjs";
+import { app_todo_button_back_main } from "./app_todo_button_back_main.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+import { each } from "./each.mjs";
+import { list_filter_property } from "./list_filter_property.mjs";
+import { html_hr } from "./html_hr.mjs";
+export function app_todo_list(
+  root,
+  items,
+  property_name,
+  value,
+  context,
+  properties,
+) {
+  html_hr(root);
+  let filtered = list_filter_property(items, property_name, value);
+  each(filtered, (item) => {
+    html_button_width_full_text_click(
+      root,
+      object_property_get(item, "name"),
+      () => {
+        app_todo_button_back_main(context);
+        each(properties, (p) => {
+          html_hr(root);
+          app_todo_item_choices(context, item, p);
+        });
+      },
+    );
+  });
+}
--- public/src/app_todo_main.mjs
+++ public/src/app_todo_main.mjs
@@ -1,15 +1,13 @@
+import { app_todo_list } from "./app_todo_list.mjs";
 import { app_todo_choices_initialize } from "./app_todo_choices_initialize.mjs";
 import { list_filter_property } from "./list_filter_property.mjs";
 import { each } from "./each.mjs";
 import { list_map_index } from "./list_map_index.mjs";
-import { app_todo_item_choices } from "./app_todo_item_choices.mjs";
 import { app_todo_button_back_main } from "./app_todo_button_back_main.mjs";
 import { app_todo_index_save } from "./app_todo_index_save.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { app_todo_item } from "./app_todo_item.mjs";
 import { string_is } from "./string_is.mjs";
 import { app_todo_items } from "./app_todo_items.mjs";
-import { html_hr } from "./html_hr.mjs";
 import { list_add } from "./list_add.mjs";
 import { html_value_get } from "./html_value_get.mjs";
 import { html_input_width_full_focus } from "./html_input_width_full_focus.mjs";
@@ -58,19 +56,5 @@ export async function app_todo_main(context) {
       await app_todo_main(context);
     });
   });
-  html_hr(root);
-  let filtered = list_filter_property(mapped, completed, nc);
-  each(filtered, (item) => {
-    html_button_width_full_text_click(
-      root,
-      object_property_get(item, "name"),
-      () => {
-        app_todo_button_back_main(context);
-        each(properties, (p) => {
-          html_hr(root);
-          app_todo_item_choices(context, item, p);
-        });
-      },
-    );
-  });
+  app_todo_list(root, mapped, completed, nc, context, properties);
 }
