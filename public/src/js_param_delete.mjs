6cd073d2da9b167d4a595b2dce5cfad8d31fec54
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-29 14:41:16 -0400

    2024-09-29T18:41:15.760Z function_auto_after js_param_delete

--- public/src/js_param_delete.mjs
+++ public/src/js_param_delete.mjs
@@ -1,15 +1,14 @@
+import { equal } from "./equal.mjs";
 import { number_is } from "./number_is.mjs";
 import { assert } from "./assert.mjs";
 import { integer_parse } from "./integer_parse.mjs";
 import { list_insert } from "./list_insert.mjs";
 import { js_param_new_generic } from "./js_param_new_generic.mjs";
-import { null_is } from "./null_is.mjs";
 export async function js_param_delete(ast, function_name, param_name) {
   let param_index = integer_parse(param_index);
   assert(number_is, [param_index]);
-  $ex;
   let b = null;
-  let a = null_is(b);
+  let a = equal(b, null);
   await js_param_new_generic(
     ast,
     function_name,
