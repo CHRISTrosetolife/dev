6efe959909091af40aaae98bb58788cc1b5e6fbd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-29 08:09:13 -0400

    2024-09-29T12:09:12.777Z function_auto_after app_todo_button_back_main

--- public/src/app_todo_button_back_main.mjs
+++ public/src/app_todo_button_back_main.mjs
@@ -2,8 +2,8 @@ import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { app_todo_main } from "./app_todo_main.mjs";
 import { html_button_width_full_text_click_back } from "./html_button_width_full_text_click_back.mjs";
 export function app_todo_button_back_main(context) {
-  html_clear_scroll_top(root);
   let { root } = context;
+  html_clear_scroll_top(root);
   html_button_width_full_text_click_back(root, async () => {
     await app_todo_main(context);
   });
