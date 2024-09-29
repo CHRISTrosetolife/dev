965b8204286d03190c7a9f434e58a870de61ffd8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-29 14:16:57 -0400

    2024-09-29T18:16:56.475Z c js_param_new_at js_param_delete

---
+++ public/src/js_param_delete.mjs
@@ -0,0 +1,20 @@
+import { number_is } from "./number_is.mjs";
+import { assert } from "./assert.mjs";
+import { integer_parse } from "./integer_parse.mjs";
+import { list_insert } from "./list_insert.mjs";
+import { js_param_new_generic } from "./js_param_new_generic.mjs";
+export async function js_param_new_at(
+  ast,
+  function_name,
+  param_name,
+) {
+  param_index = integer_parse(param_index);
+  assert(number_is, [param_index]);
+  await js_param_new_generic(
+    ast,
+    function_name,
+    param_name,
+    default_value_string,
+    (list, item) => list_insert(list, param_index, item),
+  );
+}
