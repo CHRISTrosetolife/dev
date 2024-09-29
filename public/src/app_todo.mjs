bd28d33aa8cd05779161c5823c658ca322168f1f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 20:56:40 -0400

    2024-09-29T00:56:40.256Z function_auto_after app_todo

--- public/src/app_todo.mjs
+++ public/src/app_todo.mjs
@@ -4,7 +4,6 @@ import { app_todo_firebase_path_combine } from "./app_todo_firebase_path_combine
 import { firebase_download } from "./firebase_download.mjs";
 import { firebase_upload_object } from "./firebase_upload_object.mjs";
 import { app_todo_firebase_path } from "./app_todo_firebase_path.mjs";
-import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { app_firebase } from "./app_firebase.mjs";
 import { log } from "./log.mjs";
@@ -17,14 +16,10 @@ export async function app_todo() {
     let { items } = await firebase_list(app_todo_firebase_path());
     let full_paths = list_map_property(items, "fullPath");
     if (!list_includes(full_paths, index)) {
-      await firebase_upload_object(test_path, {});
+      await firebase_upload_object(index, {});
     }
-    let test = "test.json";
-    let test_path = app_todo_firebase_path_combine(test);
-    html_button_width_full_text_click(root, "initialize", async () => {});
-    let d = await firebase_download(test_path);
+    let d = await firebase_download(index);
     log({
-      items,
       d,
     });
   });
