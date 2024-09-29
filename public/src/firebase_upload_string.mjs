1136286620c308ba217aa9a6f57e84d67e4e48a3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 20:47:06 -0400

    2024-09-29T00:47:04.689Z c firebase_upload_bytes firebase_upload_string

---
+++ public/src/firebase_upload_string.mjs
@@ -0,0 +1,12 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+} from "https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.2/firebase-storage.min.js";
+export async function firebase_upload_string(storage_path, blob) {
+  assert_arguments_length(arguments, 2);
+  let storage = getStorage();
+  let storageRef = ref(storage, storage_path);
+  let snapshot = await uploadBytes(storageRef, blob);
+}
