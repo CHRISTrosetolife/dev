168a6c9be4483cbe26672b53bc0070a28945cb8e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 21:04:22 -0400

    2024-09-29T01:04:21.534Z function_auto_after app_todo

--- public/src/app_todo.mjs
+++ public/src/app_todo.mjs
@@ -1,16 +1,16 @@
+import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
 import { firebase_download } from "./firebase_download.mjs";
 import { firebase_upload_object } from "./firebase_upload_object.mjs";
 import { list_includes } from "./list_includes.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 import { app_todo_firebase_path } from "./app_todo_firebase_path.mjs";
 import { firebase_list } from "./firebase_list.mjs";
-import { app_todo_firebase_path_combine } from "./app_todo_firebase_path_combine.mjs";
 import { app_todo_main } from "./app_todo_main.mjs";
 import { app_firebase } from "./app_firebase.mjs";
 import { object_merge } from "./object_merge.mjs";
 export async function app_todo() {
   await app_firebase(async (context) => {
-    let index_path = app_todo_firebase_path_combine("index.json");
+    let index_path = app_todo_firebase_path_index();
     let { items: firebase_items } = await firebase_list(
       app_todo_firebase_path(),
     );
---
+++ public/src/app_todo_firebase_path_index.mjs
@@ -0,0 +1,4 @@
+import { app_todo_firebase_path_combine } from "./app_todo_firebase_path_combine.mjs";
+export function app_todo_firebase_path_index() {
+  return app_todo_firebase_path_combine("index.json");
+}
