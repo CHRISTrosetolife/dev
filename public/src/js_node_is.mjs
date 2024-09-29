b20294a083d8fe4d2781493b136cbfde55b31020
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-29 14:37:30 -0400

    2024-09-29T18:37:29.469Z function_auto_after js_node_is

--- public/src/js_node_is.mjs
+++ public/src/js_node_is.mjs
@@ -1,7 +1,8 @@
 import { object_property_exists } from "./object_property_exists.mjs";
 import { null_is } from "./null_is.mjs";
+import { undefined_is } from "./undefined_is.mjs";
 export function js_node_is(candidate) {
-  if (null_is(candidate)) {
+  if (null_is(candidate) || undefined_is(candidate)) {
     return false;
   }
   return object_property_exists(candidate, "type");
