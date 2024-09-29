a4deb4a55f926723f1864b83ec84847f1c03a537
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 20:47:39 -0400

    2024-09-29T00:47:38.681Z function_auto_after firebase_upload_string

--- public/src/firebase_upload_string.mjs
+++ public/src/firebase_upload_string.mjs
@@ -2,11 +2,11 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import {
   getStorage,
   ref,
-  uploadBytes,
+  uploadString,
 } from "https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.2/firebase-storage.min.js";
 export async function firebase_upload_string(storage_path, blob) {
   assert_arguments_length(arguments, 2);
   let storage = getStorage();
   let storageRef = ref(storage, storage_path);
-  let snapshot = await uploadBytes(storageRef, blob);
+  let snapshot = await uploadString(storageRef, blob);
 }
