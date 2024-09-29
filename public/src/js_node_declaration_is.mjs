8008f2d7d19cd38b579a03d7d5bf6b824ec8c6f7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-29 11:22:40 -0400

    2024-09-29T15:22:40.331Z function_auto_after js_node_declaration_is

--- public/src/js_node_declaration_is.mjs
+++ public/src/js_node_declaration_is.mjs
@@ -1,9 +1,9 @@
 import { list_single } from "./list_single.mjs";
 import { list_size_1 } from "./list_size_1.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
-export async function js_node_declaration_is(next, lambda_inner) {
-  if (js_node_type_is(next, "VariableDeclaration")) {
-    let { declarations } = next;
+export async function js_node_declaration_is(node, lambda_inner) {
+  if (js_node_type_is(node, "VariableDeclaration")) {
+    let { declarations } = node;
     if (list_size_1(declarations)) {
       let d = list_single(declarations);
       await lambda_inner(d);
