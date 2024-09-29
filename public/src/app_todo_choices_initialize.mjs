8a846d4d27654d42ac75b078054a0fc991104551
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-29 10:42:53 -0400

    2024-09-29T14:42:49.733Z pm app_todo_choices_initialize 2 1

--- public/src/app_todo_choices_initialize.mjs
+++ public/src/app_todo_choices_initialize.mjs
@@ -1,7 +1,7 @@
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { list_first } from "./list_first.mjs";
-export function app_todo_choices_initialize(item, choices, property_name) {
+export function app_todo_choices_initialize(item, property_name, choices) {
   let choice_default = list_first(choices);
   if (!object_property_exists(item, property_name)) {
     object_property_set(item, property_name, choice_default);
