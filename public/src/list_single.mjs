1b093c5c7d9a5b1d8b2d351f2c4f4594abde0297
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 20:45:20 -0400

    2024-09-29T00:45:18.964Z function_auto_after list_single

--- public/src/list_single.mjs
+++ public/src/list_single.mjs
@@ -1,8 +1,10 @@
+import { fn_name } from "./fn_name.mjs";
 import { list_single_message } from "./list_single_message.mjs";
 import { json_to } from "./json_to.mjs";
 export function list_single(list) {
   let message_get = () =>
     json_to({
+      fn: fn_name("list_single"),
       list,
     });
   return list_single_message(list, message_get);
