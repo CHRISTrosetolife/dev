4b92bac485f77b7ba04274fcbaf774abd18f9cc2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-29 14:54:40 -0400

    2024-09-29T18:54:39.582Z function_auto_after js_param_delete

--- public/src/js_param_delete.mjs
+++ public/src/js_param_delete.mjs
@@ -1,30 +1,18 @@
-import { js_parse_expression } from "./js_parse_expression.mjs";
-import { js_param_generic } from "./js_param_generic.mjs";
-import { string_is } from "./string_is.mjs";
-import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { number_is } from "./number_is.mjs";
 import { assert } from "./assert.mjs";
 import { integer_parse } from "./integer_parse.mjs";
 import { list_insert } from "./list_insert.mjs";
+import { js_param_new_generic } from "./js_param_new_generic.mjs";
 export async function js_param_delete(ast, function_name, param_name) {
   let param_index = integer_parse(param_index);
   assert(number_is, [param_index]);
   let lambda_add = (list, item) => list_insert(list, param_index, item);
-  assert_arguments_length(arguments, 5);
-  assert2(string_is, [param_name2]);
-  assert2(string_is, [default_value_string2]);
-  let needs_imports_add = false;
-  js_param_generic(
-    ast2,
-    function_name2,
-    function lambda(args) {
-      let default_value = js_parse_expression(default_value_string2);
-      lambda_add2(args, default_value);
-      needs_imports_add = true;
-    },
-    (params) => {
-      let param_new = js_parse_expression(param_name2);
-      lambda_add2(params, param_new);
-    },
+  $ex;
+  await js_param_new_generic(
+    ast,
+    function_name,
+    param_name,
+    default_value_string,
+    lambda_add,
   );
 }
