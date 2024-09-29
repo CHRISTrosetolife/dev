ac02a4fb9c320bdb9fe756801ea4aa4478efbccf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-29 10:51:28 -0400

    2024-09-29T14:51:27.622Z function_auto_after app_todo_choices_initialize

--- public/src/app_todo_choices_initialize.mjs
+++ public/src/app_todo_choices_initialize.mjs
@@ -2,8 +2,9 @@ import { object_property_get } from "./object_property_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { list_first } from "./list_first.mjs";
-export function app_todo_choices_initialize(item, property, choices) {
-  let property_name = object_property_get(object, "property_name");
+export function app_todo_choices_initialize(item, property) {
+  let property_name = object_property_get(property, "name");
+  let choices = object_property_get(property, "choices");
   let choice_default = list_first(choices);
   if (!object_property_exists(item, property_name)) {
     object_property_set(item, property_name, choice_default);
