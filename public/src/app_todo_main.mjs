b995a898852772033fe6555a2014127ee1e80df8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-29 10:28:08 -0400

    2024-09-29T14:28:07.072Z function_auto_after app_todo_main

--- public/src/app_todo_main.mjs
+++ public/src/app_todo_main.mjs
@@ -41,7 +41,7 @@ export async function app_todo_main(context) {
         html_hr(root);
         app_todo_item_choices(context, item, "completed", [
           "✅ completed",
-          "✅ not completed",
+          "❎ not completed",
         ]);
         html_hr(root);
         app_todo_item_choices(context, item, "type", ["one-time", "daily"]);
