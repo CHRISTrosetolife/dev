3230d558de3763d2a95d25a59af8d5e241d940ac
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 21:02:22 -0400

    2024-09-29T01:02:22.242Z function_auto_after app_todo

--- public/src/app_todo.mjs
+++ public/src/app_todo.mjs
@@ -1,7 +1,5 @@
 import { app_todo_main } from "./app_todo_main.mjs";
 import { app_firebase } from "./app_firebase.mjs";
 export async function app_todo() {
-  await app_firebase(async (context) => {
-    await app_todo_main(context);
-  });
+  await app_firebase(app_todo_main);
 }
