111277874111f4a4f45f0caccdc4da17bbae909d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-29 14:18:33 -0400

    2024-09-29T18:18:32.769Z function_auto_after js_param_delete

--- public/src/js_param_delete.mjs
+++ public/src/js_param_delete.mjs
@@ -6,6 +6,7 @@ import { js_param_new_generic } from "./js_param_new_generic.mjs";
 export async function js_param_delete(ast, function_name, param_name) {
   let param_index = integer_parse(param_index);
   assert(number_is, [param_index]);
+  $ex;
   await js_param_new_generic(
     ast,
     function_name,
