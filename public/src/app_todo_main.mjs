65e3eb6a26c46d970c4939b899d5a0cb6fd2279a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 21:02:16 -0400

    2024-09-29T01:02:16.230Z function_auto_after app_todo

---
+++ public/src/app_todo_main.mjs
@@ -0,0 +1,34 @@
+import { list_add } from "./list_add.mjs";
+import { html_value_get } from "./html_value_get.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
+import { html_input_width_full_focus } from "./html_input_width_full_focus.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+import { firebase_download } from "./firebase_download.mjs";
+import { firebase_upload_object } from "./firebase_upload_object.mjs";
+import { list_includes } from "./list_includes.mjs";
+import { list_map_property } from "./list_map_property.mjs";
+import { app_todo_firebase_path } from "./app_todo_firebase_path.mjs";
+import { firebase_list } from "./firebase_list.mjs";
+import { app_todo_firebase_path_combine } from "./app_todo_firebase_path_combine.mjs";
+import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
+export async function app_todo_main(context) {
+  let { root } = context;
+  html_clear_scroll_top(root);
+  let index_path = app_todo_firebase_path_combine("index.json");
+  let { items: firebase_items } = await firebase_list(app_todo_firebase_path());
+  let full_paths = list_map_property(firebase_items, "fullPath");
+  if (!list_includes(full_paths, index_path)) {
+    await firebase_upload_object(index_path, {});
+  }
+  let index = await firebase_download(index_path);
+  html_button_width_full_text_click(root, "➕ add", () => {
+    html_clear_scroll_top(root);
+    let input = html_input_width_full_focus(root);
+    html_button_width_full_text_click(root, "➕ add", async () => {
+      let items = object_property_initialize(index, "items", []);
+      let value = html_value_get(input);
+      list_add(items, value);
+      await firebase_upload_object(index_path, index);
+    });
+  });
+}
