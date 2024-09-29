41268c9b3085421e3b509fa01c272a4226f2ef07
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-28 20:48:36 -0400

    2024-09-29T00:48:36.007Z function_auto_after firebase_upload_object

--- public/src/firebase_upload_object.mjs
+++ public/src/firebase_upload_object.mjs
@@ -1,4 +1,5 @@
+import { json_to } from "./json_to.mjs";
 import { firebase_upload_string } from "./firebase_upload_string.mjs";
 export async function firebase_upload_object(storage_path, o) {
-  await firebase_upload_string();
+  await firebase_upload_string(storage_path, json_to(o));
 }
