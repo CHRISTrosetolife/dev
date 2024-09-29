cb444ebdff669a96d303316a32caa8a2b869d94d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-29 11:48:57 -0400

    2024-09-29T15:48:56.603Z function_auto_after app_todo_main

--- public/src/app_todo_main.mjs
+++ public/src/app_todo_main.mjs
@@ -44,7 +44,7 @@ export async function app_todo_main(context) {
   });
   let add_dailies = "🌅 add dailies";
   html_button_width_full_text_click(root, add_dailies, () => {
-    let ds = list_filter_property(mapped, completed, daily);
+    let ds = list_filter_property(mapped, type, daily);
     each(mapped, (m) => {});
   });
   let add_item = "➕ add item";
