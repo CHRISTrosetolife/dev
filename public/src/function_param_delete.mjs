e7693949ebf5f6a12604a99859c5e77f04665c3c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-30 06:51:54 -0400

    2024-09-30T10:51:53.862Z function_auto_after function_param_delete

--- public/src/function_param_delete.mjs
+++ public/src/function_param_delete.mjs
@@ -1,16 +1,16 @@
-import { js_param_new_at } from "./js_param_new_at.mjs";
+import { js_param_delete_at } from "./js_param_delete_at.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { data_identifiers_each } from "./data_identifiers_each.mjs";
 import { function_transform_args_split_lambda } from "./function_transform_args_split_lambda.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
-export async function function_param_delete(function_name, param_name) {
+export async function function_param_delete(function_name, param_index) {
   assert_arguments_length(arguments, 2);
   await data_identifiers_each(function_name, lambda);
   async function lambda(fn_name) {
     await function_transform_args_split_lambda(
       fn_name,
-      [js_param_new_at],
-      [function_name, param_name],
+      [js_param_delete_at],
+      [function_name, param_index],
     );
   }
 }
