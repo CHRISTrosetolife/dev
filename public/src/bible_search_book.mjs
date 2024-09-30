2ca7c25878977a7f0155edd81f1c30b0164394dd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-09-30 12:56:01 -0400

    2024-09-30T16:56:01.094Z function_auto_after bible_search_book

--- public/src/bible_search_book.mjs
+++ public/src/bible_search_book.mjs
@@ -1,4 +1,4 @@
 import { bible_search_generic } from "./bible_search_generic.mjs";
 export async function bible_search_book(words, books) {
-  return await bible_search_generic(words, () => {});
+  return await bible_search_generic(words, (chapter_code, verse_number) => {});
 }
