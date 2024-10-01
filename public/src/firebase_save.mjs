dd5f0ea616a61de8c80475acc1b73272aae1d029
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 07:32:00 -0400

    2024-10-01T11:32:00.389Z function_auto_after firebase_save

--- public/src/firebase_save.mjs
+++ public/src/firebase_save.mjs
@@ -3,17 +3,17 @@ import { equal } from "./equal.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { firebase_download } from "./firebase_download.mjs";
 import { object_property_initialize_increment } from "./object_property_initialize_increment.mjs";
-export async function firebase_save(index_path, index) {
+export async function firebase_save(storage_path, object_data) {
   let version_property = "version";
   let version = object_property_initialize_increment(
-    index,
+    object_data,
     version_property,
     1,
   );
-  let existing = await firebase_download(index_path);
+  let existing = await firebase_download(storage_path);
   let version_previous = object_property_get(existing, version_property);
   if ((equal, [version, version_previous + 1])) {
-    await firebase_upload_object(index_path, index);
+    await firebase_upload_object(storage_path, object_data);
   } else {
     alert("data out of date");
   }
