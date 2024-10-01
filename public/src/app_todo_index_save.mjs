01b4fe1f672b1f6a0ac431d90d59df135b2ab14b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 06:27:37 -0400

    2024-10-01T10:27:37.343Z function_auto_after app_todo_index_save

--- public/src/app_todo_index_save.mjs
+++ public/src/app_todo_index_save.mjs
@@ -5,7 +5,7 @@ import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs
 export async function app_todo_index_save(context) {
   let { index } = context;
   let index_path = app_todo_firebase_path_index();
-  let value_new = object_property_initialize_increment(index, "version", 1);
+  let version = object_property_initialize_increment(index, "version", 1);
   let existing = await firebase_download(index_path);
   await firebase_upload_object(index_path, index);
 }
