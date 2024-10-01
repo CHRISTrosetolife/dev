48ef7cf7b258f7a7774baf35ed07034826bb9e32
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 07:11:57 -0400

    2024-10-01T11:11:56.863Z function_auto_after app_sleep

--- public/src/app_sleep.mjs
+++ public/src/app_sleep.mjs
@@ -3,6 +3,7 @@ import { app_firebase } from "./app_firebase.mjs";
 export async function app_sleep() {
   await app_firebase({
     on_logged_in: async (context) => {
+      return;
       let index = await firebase_storage_initialize("sleep/");
     },
   });
