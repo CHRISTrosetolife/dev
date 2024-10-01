336fd70971f5aceefe1b06636e7e0ada0a2d55b8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 07:06:07 -0400

    2024-10-01T11:06:07.175Z function_auto_after app_todo

--- public/src/app_todo.mjs
+++ public/src/app_todo.mjs
@@ -1,4 +1,3 @@
-import { noop } from "./noop.mjs";
 import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
 import { firebase_download } from "./firebase_download.mjs";
 import { firebase_upload_object } from "./firebase_upload_object.mjs";
@@ -10,19 +9,21 @@ import { app_todo_main } from "./app_todo_main.mjs";
 import { app_firebase } from "./app_firebase.mjs";
 import { object_merge } from "./object_merge.mjs";
 export async function app_todo() {
-  await app_firebase(async (context) => {
-    let index_path = app_todo_firebase_path_index();
-    let { items: firebase_items } = await firebase_list(
-      app_todo_firebase_path(),
-    );
-    let full_paths = list_map_property(firebase_items, "fullPath");
-    if (!list_includes(full_paths, index_path)) {
-      await firebase_upload_object(index_path, {});
-    }
-    let index = await firebase_download(index_path);
-    object_merge(context, {
-      index,
-    });
-    app_todo_main(context);
-  }, noop);
+  await app_firebase({
+    on_logged_in: async (context) => {
+      let index_path = app_todo_firebase_path_index();
+      let { items: firebase_items } = await firebase_list(
+        app_todo_firebase_path(),
+      );
+      let full_paths = list_map_property(firebase_items, "fullPath");
+      if (!list_includes(full_paths, index_path)) {
+        await firebase_upload_object(index_path, {});
+      }
+      let index = await firebase_download(index_path);
+      object_merge(context, {
+        index,
+      });
+      app_todo_main(context);
+    },
+  });
 }
