b1f4046f407fed53212cfdcce45cbdf6a1f6b71a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 07:17:46 -0400

    2024-10-01T11:17:45.278Z function_auto_after app_sleep

--- public/src/app_sleep.mjs
+++ public/src/app_sleep.mjs
@@ -1,8 +1,14 @@
+import { date_month_get } from "./date_month_get.mjs";
+import { date_year_get } from "./date_year_get.mjs";
+import { date_now } from "./date_now.mjs";
 import { firebase_storage_initialize } from "./firebase_storage_initialize.mjs";
 import { app_firebase } from "./app_firebase.mjs";
 export async function app_sleep() {
   await app_firebase({
     on_logged_in: async (context) => {
+      let n = date_now();
+      date_year_get(n);
+      date_month_get(n);
       return;
       let index = await firebase_storage_initialize("sleep/");
     },
