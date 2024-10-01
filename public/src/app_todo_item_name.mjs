a5a529ae5866a98f1e2d3665cbb12bee8424af3f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 05:25:04 -0400

    2024-10-01T09:25:03.612Z function_auto_after app_todo_item_name

--- public/src/app_todo_item_name.mjs
+++ public/src/app_todo_item_name.mjs
@@ -6,13 +6,17 @@ import { html_value_get } from "./html_value_get.mjs";
 import { html_input_width_full_focus } from "./html_input_width_full_focus.mjs";
 import { app_todo_button_back_main } from "./app_todo_button_back_main.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
-export function app_todo_item_name(context, button_text) {
+export function app_todo_item_name(
+  context,
+  button_text_first,
+  button_text_second,
+) {
   let { root } = context;
   let items = app_todo_items(context);
-  html_button_width_full_text_click(root, button_text, () => {
+  html_button_width_full_text_click(root, button_text_first, () => {
     app_todo_button_back_main(context);
     let input = html_input_width_full_focus(root);
-    html_button_width_full_text_click(root, button_text, async () => {
+    html_button_width_full_text_click(root, button_text_second, async () => {
       let value = html_value_get(input);
       list_add(items, app_todo_item(value));
       await app_todo_index_save_main(context);
