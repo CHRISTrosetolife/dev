cecaac6670bb04d77eb5030463627667a18403ac
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 06:28:56 -0400

    2024-10-01T10:28:55.585Z function_auto_after app_todo_index_save

--- public/src/app_todo_index_save.mjs
+++ public/src/app_todo_index_save.mjs
@@ -16,6 +16,6 @@ export async function app_todo_index_save(context) {
   );
   let existing = await firebase_download(index_path);
   let version_previous = object_property_get(existing, version_property);
-  assert(equal, [version, version + 1]);
+  assert(equal, [version, version_previous + 1]);
   await firebase_upload_object(index_path, index);
 }
