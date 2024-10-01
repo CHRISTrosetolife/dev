c346f9f9d4dfd422b03edb17e68f39446832d727
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 07:10:18 -0400

    2024-10-01T11:10:17.676Z function_auto_after app_todo

--- public/src/app_todo.mjs
+++ public/src/app_todo.mjs
@@ -1,10 +1,5 @@
+import { firebase_object_initialize } from "./firebase_object_initialize.mjs";
 import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
-import { firebase_download } from "./firebase_download.mjs";
-import { firebase_upload_object } from "./firebase_upload_object.mjs";
-import { list_includes } from "./list_includes.mjs";
-import { list_map_property } from "./list_map_property.mjs";
-import { app_todo_firebase_path } from "./app_todo_firebase_path.mjs";
-import { firebase_list } from "./firebase_list.mjs";
 import { app_todo_main } from "./app_todo_main.mjs";
 import { app_firebase } from "./app_firebase.mjs";
 import { object_merge } from "./object_merge.mjs";
@@ -12,14 +7,7 @@ export async function app_todo() {
   await app_firebase({
     on_logged_in: async (context) => {
       let index_path = app_todo_firebase_path_index();
-      let { items: firebase_items } = await firebase_list(
-        app_todo_firebase_path(),
-      );
-      let full_paths = list_map_property(firebase_items, "fullPath");
-      if (!list_includes(full_paths, index_path)) {
-        await firebase_upload_object(index_path, {});
-      }
-      let index = await firebase_download(index_path);
+      let index = await firebase_object_initialize(index_path);
       object_merge(context, {
         index,
       });
---
+++ public/src/firebase_object_initialize.mjs
@@ -0,0 +1,15 @@
+import { firebase_download } from "./firebase_download.mjs";
+import { firebase_upload_object } from "./firebase_upload_object.mjs";
+import { list_includes } from "./list_includes.mjs";
+import { list_map_property } from "./list_map_property.mjs";
+import { app_todo_firebase_path } from "./app_todo_firebase_path.mjs";
+import { firebase_list } from "./firebase_list.mjs";
+export async function firebase_object_initialize(index_path) {
+  let { items: firebase_items } = await firebase_list(app_todo_firebase_path());
+  let full_paths = list_map_property(firebase_items, "fullPath");
+  if (!list_includes(full_paths, index_path)) {
+    await firebase_upload_object(index_path, {});
+  }
+  let index = await firebase_download(index_path);
+  return index;
+}
