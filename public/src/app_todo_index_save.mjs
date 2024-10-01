bb14881b8e23502b2953892c74f07e16d0505ea1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 07:31:27 -0400

    2024-10-01T11:31:26.917Z function_auto_after app_todo_index_save

--- public/src/app_todo_index_save.mjs
+++ public/src/app_todo_index_save.mjs
@@ -1,23 +1,7 @@
-import { object_property_get } from "./object_property_get.mjs";
-import { firebase_download } from "./firebase_download.mjs";
-import { object_property_initialize_increment } from "./object_property_initialize_increment.mjs";
-import { firebase_upload_object } from "./firebase_upload_object.mjs";
+import { firebase_save } from "./firebase_save.mjs";
 import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
-import { equal } from "./equal.mjs";
 export async function app_todo_index_save(context) {
   let { index } = context;
   let index_path = app_todo_firebase_path_index();
-  let version_property = "version";
-  let version = object_property_initialize_increment(
-    index,
-    version_property,
-    1,
-  );
-  let existing = await firebase_download(index_path);
-  let version_previous = object_property_get(existing, version_property);
-  if ((equal, [version, version_previous + 1])) {
-    await firebase_upload_object(index_path, index);
-  } else {
-    alert("data out of date");
-  }
+  await firebase_save(index, index_path);
 }
---
+++ public/src/firebase_save.mjs
@@ -0,0 +1,20 @@
+import { firebase_upload_object } from "./firebase_upload_object.mjs";
+import { equal } from "./equal.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { firebase_download } from "./firebase_download.mjs";
+import { object_property_initialize_increment } from "./object_property_initialize_increment.mjs";
+export async function firebase_save(index, index_path) {
+  let version_property = "version";
+  let version = object_property_initialize_increment(
+    index,
+    version_property,
+    1,
+  );
+  let existing = await firebase_download(index_path);
+  let version_previous = object_property_get(existing, version_property);
+  if ((equal, [version, version_previous + 1])) {
+    await firebase_upload_object(index_path, index);
+  } else {
+    alert("data out of date");
+  }
+}
