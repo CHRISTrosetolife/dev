d555901e8f12b3d15f0164667084d74cbeab7fd3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 05:52:05 -0400

    2024-10-01T09:52:05.171Z function_auto_after object_property_initialize

--- public/src/object_property_initialize.mjs
+++ public/src/object_property_initialize.mjs
@@ -1,4 +1,4 @@
 import { object_property_initialize_get } from "./object_property_initialize_get.mjs";
-export function object_property_initialize(lookup, key, initial) {
-  return object_property_initialize_get(lookup, key, () => initial);
+export function object_property_initialize(object, key, initial) {
+  return object_property_initialize_get(object, key, () => initial);
 }
