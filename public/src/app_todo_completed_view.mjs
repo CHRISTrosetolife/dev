e0b8f08355d6cee4d3cb47f22e1b045705c695d7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 05:46:39 -0400

    2024-10-01T09:46:38.707Z function_auto_after app_todo_completed_view

---
+++ public/src/app_todo_completed_view.mjs
@@ -0,0 +1,25 @@
+import { noop } from "./noop.mjs";
+import { app_todo_completed_value } from "./app_todo_completed_value.mjs";
+import { app_todo_completed_property } from "./app_todo_completed_property.mjs";
+import { app_todo_list } from "./app_todo_list.mjs";
+import { app_todo_main } from "./app_todo_main.mjs";
+import { app_todo_not_completed } from "./app_todo_not_completed.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
+export function app_todo_completed_view(root, context, mapped) {
+  html_clear_scroll_top(root);
+  html_button_width_full_text_click(
+    root,
+    app_todo_not_completed(),
+    async () => {
+      app_todo_main(context);
+    },
+  );
+  app_todo_list(
+    context,
+    mapped,
+    app_todo_completed_property(),
+    app_todo_completed_value(),
+    noop,
+  );
+}
