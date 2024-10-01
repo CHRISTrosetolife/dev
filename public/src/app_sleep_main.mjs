8dcd076d54093f889b428be947220b865ed7c0c3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 07:37:26 -0400

    2024-10-01T11:37:25.998Z function_auto_after app_sleep_main

--- public/src/app_sleep_main.mjs
+++ public/src/app_sleep_main.mjs
@@ -3,8 +3,8 @@ import { html_hr } from "./html_hr.mjs";
 import { html_clear_scroll_top_context } from "./html_clear_scroll_top_context.mjs";
 export function app_sleep_main(context) {
   let root = html_clear_scroll_top_context(context);
-  let list_name = "wakeups";
+  $ag;
   let button_text = "woke up";
-  app_sleep_record(context, button_text, list_name);
+  app_sleep_record(context, button_text, "wakeups");
   html_hr(root);
 }
