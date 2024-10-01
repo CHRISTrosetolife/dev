30d98904b3e4fc5166fb76f4b15e615e63b30b9b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 05:47:59 -0400

    2024-10-01T09:47:59.021Z function_auto_after app_todo_items

--- public/src/app_todo_items.mjs
+++ public/src/app_todo_items.mjs
@@ -1,6 +1,22 @@
+import { app_todo_choices_initialize } from "./app_todo_choices_initialize.mjs";
+import { each } from "./each.mjs";
+import { list_set } from "./list_set.mjs";
+import { app_todo_item } from "./app_todo_item.mjs";
+import { string_is } from "./string_is.mjs";
+import { list_map_index } from "./list_map_index.mjs";
+import { app_todo_properties } from "./app_todo_properties.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 export function app_todo_items(context) {
   let { index } = context;
   let items = object_property_initialize(index, "items", []);
-  return items;
+  let properties = app_todo_properties();
+  let mapped = list_map_index(items, (item, ix) => {
+    if (string_is(item)) {
+      item = app_todo_item(item);
+      list_set(items, ix, item);
+    }
+    each(properties, (p) => app_todo_choices_initialize(item, p));
+    return item;
+  });
+  return mapped;
 }
