e07322baabdb4cb9793edd2f491ddc274950c857
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 07:11:02 -0400

    2024-10-01T11:11:02.087Z function_auto_after firebase_storage_initialize

--- public/src/firebase_storage_initialize.mjs
+++ public/src/firebase_storage_initialize.mjs
@@ -5,8 +5,8 @@ import { list_map_property } from "./list_map_property.mjs";
 import { app_todo_firebase_path } from "./app_todo_firebase_path.mjs";
 import { firebase_list } from "./firebase_list.mjs";
 export async function firebase_storage_initialize(storage_path) {
-  let { items: firebase_items } = await firebase_list(app_todo_firebase_path());
-  let full_paths = list_map_property(firebase_items, "fullPath");
+  let { items } = await firebase_list(app_todo_firebase_path());
+  let full_paths = list_map_property(items, "fullPath");
   if (!list_includes(full_paths, storage_path)) {
     await firebase_upload_object(storage_path, {});
   }
