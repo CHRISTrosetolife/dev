8395a9365098071ba806dc8f92ed9376a74564dc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-01 07:34:04 -0400

    2024-10-01T11:34:03.872Z function_auto_after html_clear_scroll_top_context

--- public/src/app_sleep_main.mjs
+++ public/src/app_sleep_main.mjs
@@ -10,15 +10,15 @@ import { date_year_get } from "./date_year_get.mjs";
 import { date_now } from "./date_now.mjs";
 export async function app_sleep_main(context) {
   html_clear_scroll_top_context(context);
-  let n = date_now();
-  let year = date_year_get(n);
-  let month = date_month_get(n);
-  let month_path = path_join(["sleep", year, month]);
-  let data = await firebase_storage_initialize(month_path);
-  let day = date_day_get(data);
-  let today = object_property_initialize(data, day, {});
   let { root } = context;
   html_button_width_full_text_click(root, "woke up", async () => {
+    let n = date_now();
+    let year = date_year_get(n);
+    let month = date_month_get(n);
+    let month_path = path_join(["sleep", year, month]);
+    let data = await firebase_storage_initialize(month_path);
+    let day = date_day_get(data);
+    let today = object_property_initialize(data, day, {});
     await firebase_save(index_path, index);
     await app_sleep_main(context);
   });
