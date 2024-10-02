503b4dcb18906ceacc8d906a857f1b5adfdf7ca6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-02 08:54:21 -0400

    2024-10-02T12:54:18.171Z function_auto_after sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { bible_words_greek_download } from "./bible_words_greek_download.mjs";
 import { app_ceb_upload } from "./app_ceb_upload.mjs";
 export async function sandbox() {
+  assert_arguments_length(arguments, 0);
   if (0) {
     await bible_words_greek_download();
   }
