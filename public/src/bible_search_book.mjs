0ce00f1a1426675d101db99db848bfa439edfe7d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-02 09:04:07 -0400

    2024-10-02T13:04:06.241Z bb acts man,heart

--- public/src/bible_search_book.mjs
+++ public/src/bible_search_book.mjs
@@ -4,9 +4,11 @@ import { bible_search_generic } from "./bible_search_generic.mjs";
 import { list_includes } from "./list_includes.mjs";
 import { string_case_upper } from "./string_case_upper.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
+import { string_take } from "./string_take.mjs";
 export async function bible_search_book(books, words) {
   let book = books;
   book = string_case_upper(book);
+  book=string_take(book, 3)
   let all = await bible_books("engbsb");
   assert(list_includes, [all, book]);
   return await bible_search_generic(words, (chapter_code, verse_number) => {
