8c246faae3058de340433b638eb9052be94ad3d7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-02 09:02:24 -0400

    2024-10-02T13:02:24.255Z function_auto_after bible_search_book

--- public/src/bible_search_book.mjs
+++ public/src/bible_search_book.mjs
@@ -1,9 +1,12 @@
+import { assert } from "./assert.mjs";
 import { bible_books } from "./bible_books.mjs";
 import { bible_search_generic } from "./bible_search_generic.mjs";
+import { list_includes } from "./list_includes.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 export async function bible_search_book(books, words) {
   let book = books;
   let all = await bible_books("engbsb");
+  assert(list_includes, [all, book]);
   return await bible_search_generic(words, (chapter_code, verse_number) => {
     return string_starts_with(chapter_code, book);
   });
