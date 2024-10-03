ebb1f073306002011c6e29abb4fcb30d8b59df5c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-03 09:23:11 -0400

    2024-10-03T13:23:08.822Z function_auto_after date_month_get

--- public/src/date_month_get.mjs
+++ public/src/date_month_get.mjs
@@ -3,6 +3,9 @@ export function date_month_get(date) {
   let month_index = date.getMonth();
   let month = month_index + 1;
   let month_padded = number_pad_2(month);
+  let month_name = date.toLocaleString("default", {
+    month: "long",
+  });
   return {
     month_index,
     month,
