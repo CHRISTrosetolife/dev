f9f78440ad4d3ca45cbe8cac335b0d5218e38267
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-03 09:20:23 -0400

    2024-10-03T13:20:22.957Z function_auto_after date_month_get

--- public/src/date_month_get.mjs
+++ public/src/date_month_get.mjs
@@ -3,4 +3,9 @@ export function date_month_get(date) {
   let month_index = date.getMonth();
   let month = month_index + 1;
   let month_padded = number_pad_2(month);
+  return {
+    month_index,
+    month,
+    month_padded,
+  };
 }
