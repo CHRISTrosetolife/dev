9c970b6a27f41f34ea1bba062f4b4e213154f27c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-03 09:42:31 -0400

    2024-10-03T13:42:31.431Z function_auto_after app_sleep_manual

--- public/src/app_sleep_manual.mjs
+++ public/src/app_sleep_manual.mjs
@@ -11,5 +11,5 @@ export function app_sleep_manual(context) {
   let { year, month_index, month, month_padded, month_name, day, now } =
     date_now_parts();
   let names = date_month_names();
-  month_index = html_list_chooser(root, names, month_index, context);
+  let month_chooser = html_list_chooser(root, names, month_index, context);
 }
