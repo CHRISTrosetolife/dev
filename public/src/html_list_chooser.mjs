bcf8407a0801ee523551aa4e65021566b3fa0be7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-03 09:42:57 -0400

    2024-10-03T13:42:57.171Z function_auto_after html_list_chooser

--- public/src/html_list_chooser.mjs
+++ public/src/html_list_chooser.mjs
@@ -1,3 +1,4 @@
+import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { equal } from "./equal.mjs";
 import { html_style_success } from "./html_style_success.mjs";
@@ -5,7 +6,6 @@ import { html_style_button_default } from "./html_style_button_default.mjs";
 import { list_map_index } from "./list_map_index.mjs";
 import { app_sleep_manual } from "./app_sleep_manual.mjs";
 import { html_button_width_full_text_click_back } from "./html_button_width_full_text_click_back.mjs";
-import { html_clear_scroll_top_context } from "./html_clear_scroll_top_context.mjs";
 import { list_get } from "./list_get.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function html_list_chooser(root, list, value, context) {
@@ -13,8 +13,8 @@ export function html_list_chooser(root, list, value, context) {
     root,
     list_get(list, value),
     () => {
-      $ex;
-      html_clear_scroll_top_context(context);
+      let { root } = context;
+      html_clear_scroll_top(root);
       html_button_width_full_text_click_back(root, () =>
         app_sleep_manual(context),
       );
