4d072999d2be4e21cb869aa37cdcbab8abcbb69e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-03 09:21:26 -0400

    2024-10-03T13:21:26.051Z function_auto_after app_sleep_today_download

--- public/src/app_sleep_today_download.mjs
+++ public/src/app_sleep_today_download.mjs
@@ -4,8 +4,8 @@ import { firebase_storage_initialize } from "./firebase_storage_initialize.mjs";
 import { path_join } from "./path_join.mjs";
 import { file_name_json } from "./file_name_json.mjs";
 export async function app_sleep_today_download() {
-  let { year, month, day, now } = date_now_parts();
-  let month_path = file_name_json(path_join(["sleep", year, month]));
+  let { year, month_padded, day, now } = date_now_parts();
+  let month_path = file_name_json(path_join(["sleep", year, month_padded]));
   let data = await firebase_storage_initialize(month_path);
   let today = object_property_initialize(data, day, {});
   return {
