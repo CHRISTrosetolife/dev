a67d40d9b4cd3ad01cc5d6dce69b46304716a1ca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-03 09:21:49 -0400

    2024-10-03T13:21:49.424Z function_auto_after app_sleep_main

--- public/src/app_sleep_main.mjs
+++ public/src/app_sleep_main.mjs
@@ -16,7 +16,8 @@ export function app_sleep_main(context) {
     string_combine_multiple([emoji_add(), " add manual"]),
     () => {
       html_clear_scroll_top_context(context);
-      let { year, month, day, now } = date_now_parts();
+      let { year, month_index, month, month_padded, day, now } =
+        date_now_parts();
       html_button_width_full_text_click(root);
     },
   );
