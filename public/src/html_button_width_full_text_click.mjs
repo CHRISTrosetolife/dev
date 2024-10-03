33cc35bd7e41082c8ffdd1e2af62b32d501c9fa3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-03 09:28:55 -0400

    2024-10-03T13:28:52.614Z function_auto_after html_button_width_full_text_click

--- public/src/html_button_width_full_text_click.mjs
+++ public/src/html_button_width_full_text_click.mjs
@@ -1,6 +1,7 @@
 import { html_on_click } from "./html_on_click.mjs";
 import { html_button_width_full_text } from "./html_button_width_full_text.mjs";
 export function html_button_width_full_text_click(parent, text, on_click) {
+  () => {};
   let b = html_button_width_full_text(parent, text);
   html_on_click(b, on_click);
   return b;
