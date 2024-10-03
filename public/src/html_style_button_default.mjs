c894e866acb8ba42f330bc9ab37f922fd4a669ff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-03 09:35:28 -0400

    2024-10-03T13:35:27.847Z function_auto_after html_style_button_default

--- public/src/html_style_button_default.mjs
+++ public/src/html_style_button_default.mjs
@@ -1,5 +1,5 @@
 import { html_style_button_default_value } from "./html_style_button_default_value.mjs";
 import { html_style } from "./html_style.mjs";
-export function html_style_button_default(result) {
-  html_style(result, html_style_button_default_value());
+export function html_style_button_default(component_button) {
+  html_style(component_button, html_style_button_default_value());
 }
