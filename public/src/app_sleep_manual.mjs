849c2c06b0e54bf9790a79bfc05227aea9503da1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-03 09:41:34 -0400

    2024-10-03T13:41:33.671Z function_auto_after app_sleep_manual

---
+++ public/src/app_sleep_manual.mjs
@@ -0,0 +1,15 @@
+import { html_list_chooser } from "./html_list_chooser.mjs";
+import { date_month_names } from "./date_month_names.mjs";
+import { date_now_parts } from "./date_now_parts.mjs";
+import { app_sleep_main } from "./app_sleep_main.mjs";
+import { html_button_width_full_text_click_back } from "./html_button_width_full_text_click_back.mjs";
+import { html_clear_scroll_top_context } from "./html_clear_scroll_top_context.mjs";
+export function app_sleep_manual(context) {
+  let { root } = context;
+  html_clear_scroll_top_context(context);
+  html_button_width_full_text_click_back(root, () => app_sleep_main(context));
+  let { year, month_index, month, month_padded, month_name, day, now } =
+    date_now_parts();
+  let names = date_month_names();
+  month_index = html_list_chooser(root, names, month_index, context);
+}
