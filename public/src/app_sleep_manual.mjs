666b4520646843e0e447e66f4cc26ab3a7d02776
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-03 09:31:42 -0400

    2024-10-03T13:31:41.843Z function_auto_after app_sleep_manual

--- public/src/app_sleep_manual.mjs
+++ public/src/app_sleep_manual.mjs
@@ -7,7 +7,7 @@ import { app_sleep_main } from "./app_sleep_main.mjs";
 import { html_button_width_full_text_click_back } from "./html_button_width_full_text_click_back.mjs";
 import { html_clear_scroll_top_context } from "./html_clear_scroll_top_context.mjs";
 export function app_sleep_manual(context) {
-  let root = context;
+  let { root } = context;
   html_clear_scroll_top_context(context);
   html_button_width_full_text_click_back(root, () => app_sleep_main(context));
   let { year, month_index, month, month_padded, month_name, day, now } =
