cd2ff1406ad061342a916cb9fb15ae883fec98c8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 10:08:05 -0400

    2024-10-04T14:08:03.816Z function_auto_after app_todo_index_save

--- public/src/app_todo_index_save.mjs
+++ public/src/app_todo_index_save.mjs
@@ -1,11 +1,7 @@
-import { log } from "./log.mjs";
 import { firebase_save } from "./firebase_save.mjs";
 import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
 export async function app_todo_index_save(context) {
   let { index } = context;
-  log({
-    index,
-  });
   let index_path = app_todo_firebase_path_index();
   await firebase_save(index_path, index);
 }
