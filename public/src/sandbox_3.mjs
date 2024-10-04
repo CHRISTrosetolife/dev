3ecbd73fdab615ddd4566f65139001503a6683de
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 19:16:52 -0400

    2024-10-04T23:16:51.889Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -3,10 +3,8 @@ import { list_size } from "./list_size.mjs";
 import { html_parse_a_href_starts_with } from "./html_parse_a_href_starts_with.mjs";
 import { http_cache_parse_parsed } from "./http_cache_parse_parsed.mjs";
 export async function sandbox_3() {
-  let url = string_combine_multiple([
-    "https://mechon-mamre.org/p/pt/",
-    "pt00.htm",
-  ]);
+  let prefix_url = "https://mechon-mamre.org/p/pt/";
+  let url = string_combine_multiple([prefix_url, "pt00.htm"]);
   let p = await http_cache_parse_parsed(url);
   let { root } = p;
   let links = html_parse_a_href_starts_with(root, "../../mp3/");
