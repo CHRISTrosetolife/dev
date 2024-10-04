fbc0c29d10b94b6f27aa1c22399e7ad33d89278d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 14:31:08 -0400

    2024-10-04T18:31:07.509Z function_auto_after app_record_chapter

--- public/src/app_record_chapter.mjs
+++ public/src/app_record_chapter.mjs
@@ -1,3 +1,6 @@
+import { object_replace } from "./object_replace.mjs";
+import { app_record } from "./app_record.mjs";
+import { app_save_change } from "./app_save_change.mjs";
 import { app_record_chapter_buttons } from "./app_record_chapter_buttons.mjs";
 import { app_record_verse } from "./app_record_verse.mjs";
 import { app_record_verses } from "./app_record_verses.mjs";
@@ -7,6 +10,12 @@ import { html_span_text } from "./html_span_text.mjs";
 import { each } from "./each.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export async function app_record_chapter(context, book_code, chapter) {
+  await app_save_change(app_record, (save) =>
+    object_replace(save, {
+      book: book_code,
+      chapter,
+    }),
+  );
   let { root } = context;
   let verses = await app_record_verses(book_code, chapter);
   app_record_chapter_buttons(context, book_code);
