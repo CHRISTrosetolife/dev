da741374a6e881f8f9e7d93d0bca1ce3c5d89ded
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 19:16:39 -0400

    2024-10-04T23:16:38.664Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,8 +1,12 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { list_size } from "./list_size.mjs";
 import { html_parse_a_href_starts_with } from "./html_parse_a_href_starts_with.mjs";
 import { http_cache_parse_parsed } from "./http_cache_parse_parsed.mjs";
 export async function sandbox_3() {
-  let url = "https://mechon-mamre.org/p/pt/pt00.htm";
+  let url = string_combine_multiple([
+    "https://mechon-mamre.org/p/pt/",
+    "pt00.htm",
+  ]);
   let p = await http_cache_parse_parsed(url);
   let { root } = p;
   let links = html_parse_a_href_starts_with(root, "../../mp3/");
