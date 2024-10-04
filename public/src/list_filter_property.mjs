021f2e1143b5b3830d4ce3391d54d73d832653e3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 13:56:49 -0400

    2024-10-04T17:56:46.347Z function_auto_after list_filter_property

--- public/src/list_filter_property.mjs
+++ public/src/list_filter_property.mjs
@@ -1,7 +1,9 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { equal } from "./equal.mjs";
 import { list_filter } from "./list_filter.mjs";
 export function list_filter_property(list, property_name, value) {
+  assert_arguments_length(arguments, 3);
   return list_filter(list, (m) =>
     equal(object_property_get(m, property_name), value),
   );
