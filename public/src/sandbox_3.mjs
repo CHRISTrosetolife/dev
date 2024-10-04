c5200f9c89726fdcce1b48ac77470df9d5457a36
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 19:15:30 -0400

    2024-10-04T23:15:29.149Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -3,6 +3,6 @@ import { http_cache_parse_parsed } from "./http_cache_parse_parsed.mjs";
 export async function sandbox_3() {
   let url = "https://mechon-mamre.org/p/pt/pt00.htm";
   let p = await http_cache_parse_parsed(url);
-  let root = p;
-  html_parse_a_href_starts_with();
+  let { root } = p;
+  html_parse_a_href_starts_with(root, "../../mp3/");
 }
