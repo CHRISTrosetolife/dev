ef90aa4174dc1e1aecbc417002889ed96841ae27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 14:31:20 -0400

    2024-10-04T18:31:19.123Z function_auto_after app_record_book

--- public/src/app_record_book.mjs
+++ public/src/app_record_book.mjs
@@ -1,7 +1,17 @@
+import { object_replace } from "./object_replace.mjs";
+import { app_record } from "./app_record.mjs";
+import { app_save_change } from "./app_save_change.mjs";
 import { app_record_home_button } from "./app_record_home_button.mjs";
 import { app_record_book_generic } from "./app_record_book_generic.mjs";
 import { app_record_chapter } from "./app_record_chapter.mjs";
-export function app_record_book(context, book_code) {
+export async function app_record_book(context, book_code) {
+  await app_save_change(app_record, (save) =>
+    object_replace(save, {
+      book: book_code,
+      chapter,
+      verse: verse_number,
+    }),
+  );
   let lambda_top = () => app_record_home_button(context);
   let app_record_book_on_click = app_record_chapter;
   app_record_book_generic(
