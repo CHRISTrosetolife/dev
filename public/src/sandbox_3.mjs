ee19fcc56984b4eb1eeca6558ddb023e180a0135
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 18:20:04 -0400

    2024-10-04T22:20:03.839Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,4 +1,6 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { ceb_bible_words_definitions_all_cache } from "./ceb_bible_words_definitions_all_cache.mjs";
 export async function sandbox_3() {
   let c = await ceb_bible_words_definitions_all_cache();
+  object_property_get(object, "property_name");
 }
