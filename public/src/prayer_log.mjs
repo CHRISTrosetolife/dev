40ba61ce30fc1b8a015d0ca4073ef00eab9190df
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 12:42:02 -0400

    2024-10-04T16:42:02.003Z function_auto_after prayer_log

--- public/src/prayer_log.mjs
+++ public/src/prayer_log.mjs
@@ -1,5 +1,11 @@
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { log } from "./log.mjs";
 export function prayer_log(prayer_body) {
-  log(string_combine_multiple(["God ", prayer_body, " amen"]));
+  log(
+    string_combine_multiple([
+      "God, according to the will of God, ",
+      prayer_body,
+      " amen",
+    ]),
+  );
 }
