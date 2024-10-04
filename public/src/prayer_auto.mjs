1b777f952b169e1b7474bfb04006fb956a664285
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 12:38:01 -0400

    2024-10-04T16:38:00.969Z function_auto_after prayer_auto

--- public/src/prayer_auto.mjs
+++ public/src/prayer_auto.mjs
@@ -1,6 +1,9 @@
+import { log } from "./log.mjs";
 import { each } from "./each.mjs";
 import { countries } from "./countries.mjs";
 export function prayer_auto() {
   let cs = countries();
-  each(list, (item) => {});
+  each(cs, (c) => {
+    log({});
+  });
 }
