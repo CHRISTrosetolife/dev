5c6423db76574c5f8075f7395f64517da6aa943b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 18:19:50 -0400

    2024-10-04T22:19:50.372Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,4 +1,4 @@
 import { ceb_bible_words_definitions_all_cache } from "./ceb_bible_words_definitions_all_cache.mjs";
 export async function sandbox_3() {
-  await ceb_bible_words_definitions_all_cache();
+  return await ceb_bible_words_definitions_all_cache();
 }
