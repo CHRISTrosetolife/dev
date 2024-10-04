942a1942086f220978e80c9e02e4b6c747896349
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 12:42:07 -0400

    2024-10-04T16:42:06.925Z function_auto_after prayer_auto

--- public/src/prayer_auto.mjs
+++ public/src/prayer_auto.mjs
@@ -6,7 +6,7 @@ import { countries } from "./countries.mjs";
 export function prayer_auto() {
   let cs = countries();
   each(cs, (c) => {
-    let requests = ["bless", "save each person"];
+    let requests = ["bless", "save each person in"];
     let request = "bless";
     let prayer_body = string_combine_multiple([
       request,
