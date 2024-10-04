77889150b80c48124e79dfd460b2ad86c0426e51
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 19:25:08 -0400

    2024-10-04T23:25:07.463Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -2,18 +2,18 @@ import { path_resolve } from "./path_resolve.mjs";
 import { log } from "./log.mjs";
 import { http_file } from "./http_file.mjs";
 import { each_async } from "./each_async.mjs";
-import { list_map } from "./list_map.mjs";
 import { html_parse_a_href_starts_with_hrefs } from "./html_parse_a_href_starts_with_hrefs.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { http_cache_parse_parsed } from "./http_cache_parse_parsed.mjs";
 import { string_combine } from "./string_combine.mjs";
+import { list_wait } from "./list_wait.mjs";
 export async function sandbox_3() {
   let prefix_url = "https://mechon-mamre.org/p/pt/";
   let url = string_combine_multiple([prefix_url, "pt00.htm"]);
   let p = await http_cache_parse_parsed(url);
   let { root } = p;
   let hrefs = html_parse_a_href_starts_with_hrefs(root, "../../mp3/");
-  let urls = list_map(
+  let urls = await list_wait(
     hrefs,
     async (h) => await path_resolve(string_combine(prefix_url, h)),
   );
