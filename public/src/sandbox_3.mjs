06cab75f0157f2cdba6883bca3f32957fc063780
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 18:20:32 -0400

    2024-10-04T22:20:31.177Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,6 +1,6 @@
-import { object_property_get } from "./object_property_get.mjs";
 import { ceb_bible_words_definitions_all_cache } from "./ceb_bible_words_definitions_all_cache.mjs";
+import { list_find_property } from "./list_find_property.mjs";
 export async function sandbox_3() {
   let c = await ceb_bible_words_definitions_all_cache();
-  object_property_get(object, "property_name");
+  list_find_property(c, "word", "atngal");
 }
