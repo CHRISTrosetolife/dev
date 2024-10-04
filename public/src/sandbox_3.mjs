387337842c10f03af261ed1708285a48a1674602
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 19:19:09 -0400

    2024-10-04T23:19:08.870Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,11 +1,14 @@
+import { list_map } from "./list_map.mjs";
 import { html_parse_a_href_starts_with_hrefs } from "./html_parse_a_href_starts_with_hrefs.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { http_cache_parse_parsed } from "./http_cache_parse_parsed.mjs";
+import { string_combine } from "./string_combine.mjs";
 export async function sandbox_3() {
   let prefix_url = "https://mechon-mamre.org/p/pt/";
   let url = string_combine_multiple([prefix_url, "pt00.htm"]);
   let p = await http_cache_parse_parsed(url);
   let { root } = p;
   let hrefs = html_parse_a_href_starts_with_hrefs(root, "../../mp3/");
+  list_map(hrefs, (h) => string_combine(url, h));
   return hrefs;
 }
