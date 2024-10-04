f5e01b2c10a428c559b94add7f83ad49cf624283
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 12:51:33 -0400

    2024-10-04T16:51:32.733Z function_auto_after prayer_auto

--- public/src/prayer_auto.mjs
+++ public/src/prayer_auto.mjs
@@ -19,6 +19,10 @@ export async function prayer_auto() {
     });
   });
   let verses = list_denest(chapters);
+  log({
+    verses,
+  });
+  return;
   let verse_texts = list_map(verses, (v) =>
     list_join_space(object_property_get(v, "tokens")),
   );
