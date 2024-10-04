03ff7216a7b0eba0e3414d5778c7f540e70b4625
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 14:40:13 -0400

    2024-10-04T18:40:04.714Z function_auto_after app_record_chapter_buttons

--- public/src/app_record_chapter_buttons.mjs
+++ public/src/app_record_chapter_buttons.mjs
@@ -1,7 +1,6 @@
+import { bible_book_name_text } from "./bible_book_name_text.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { app_record_book } from "./app_record_book.mjs";
-import { bible_book_name } from "./bible_book_name.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { app_record_home_button } from "./app_record_home_button.mjs";
 export function app_record_chapter_buttons(context, book_code) {
@@ -10,7 +9,7 @@ export function app_record_chapter_buttons(context, book_code) {
   app_record_home_button(context);
   html_button_width_full_text_click(
     root,
-    string_combine_multiple(["📖 book ", bible_book_name(book_code)]),
+    bible_book_name_text(book_code),
     async () => await app_record_book(context, book_code),
   );
 }
