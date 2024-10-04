e9ab24d9123399e9346f20d39a490257d6c4feee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 19:17:35 -0400

    2024-10-04T23:17:35.050Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,3 +1,5 @@
+import { html_parse_href } from "./html_parse_href.mjs";
+import { list_map } from "./list_map.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { list_size } from "./list_size.mjs";
 import { html_parse_a_href_starts_with } from "./html_parse_a_href_starts_with.mjs";
@@ -8,5 +10,6 @@ export async function sandbox_3() {
   let p = await http_cache_parse_parsed(url);
   let { root } = p;
   let links = html_parse_a_href_starts_with(root, "../../mp3/");
+  list_map(links, html_parse_href);
   return list_size(links);
 }
