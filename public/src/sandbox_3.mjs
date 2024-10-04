aaf16b9e2ee1a9b6516b428711559e2d21a2fdec
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 19:26:53 -0400

    2024-10-04T23:26:51.723Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,4 +1,4 @@
-import { path_resolve } from "./path_resolve.mjs";
+import { list_map } from "./list_map.mjs";
 import { log } from "./log.mjs";
 import { http_file } from "./http_file.mjs";
 import { each_async } from "./each_async.mjs";
@@ -6,20 +6,17 @@ import { html_parse_a_href_starts_with_hrefs } from "./html_parse_a_href_starts_
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { http_cache_parse_parsed } from "./http_cache_parse_parsed.mjs";
 import { string_combine } from "./string_combine.mjs";
-import { list_wait } from "./list_wait.mjs";
 export async function sandbox_3() {
   let prefix_url = "https://mechon-mamre.org/";
   let url = string_combine_multiple([prefix_url, "p/pt/pt00.htm"]);
   let p = await http_cache_parse_parsed(url);
   let { root } = p;
+  let prefix = "../../";
   let hrefs = html_parse_a_href_starts_with_hrefs(
     root,
-    string_combine_multiple(["../../", "mp3/"]),
+    string_combine_multiple([prefix, "mp3/"]),
   );
-  let urls = await list_wait(
-    hrefs,
-    async (h) => await path_resolve(string_combine(prefix_url, h)),
-  );
+  let urls = list_map(hrefs, (h) => string_combine(prefix_url, h));
   return urls;
   await each_async(urls, async (url) => {
     let location = await http_file(url);
