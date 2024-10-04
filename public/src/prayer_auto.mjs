3aa6c926c3edaea733543a4e733cd838d460f51e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 12:46:30 -0400

    2024-10-04T16:46:29.666Z function_auto_after prayer_auto

--- public/src/prayer_auto.mjs
+++ public/src/prayer_auto.mjs
@@ -1,12 +1,12 @@
+import { bible_books_chapter_each } from "./bible_books_chapter_each.mjs";
 import { log } from "./log.mjs";
-import { bible_books_chapters } from "./bible_books_chapters.mjs";
 import { prayer_log } from "./prayer_log.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { each } from "./each.mjs";
 import { countries } from "./countries.mjs";
 export async function prayer_auto() {
-  let chapters = await bible_books_chapters("engbsb");
+  let chapters = await bible_books_chapter_each("engbsb");
   log({
     chapters,
   });
