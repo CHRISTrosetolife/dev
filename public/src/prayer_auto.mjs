9f6893d0ccbbc46120e560ebd3ad7bd4568d620c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 12:44:39 -0400

    2024-10-04T16:44:39.240Z function_auto_after prayer_auto

--- public/src/prayer_auto.mjs
+++ public/src/prayer_auto.mjs
@@ -1,9 +1,11 @@
+import { bible_books_chapters } from "./bible_books_chapters.mjs";
 import { prayer_log } from "./prayer_log.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { each } from "./each.mjs";
 import { countries } from "./countries.mjs";
-export function prayer_auto() {
+export async function prayer_auto() {
+  await bible_books_chapters();
   let cs = countries();
   each(cs, (c) => {
     let requests = ["bless", "save each person in"];
