9590cfe42fd2b192f4b917c280dc70721807198b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 12:42:25 -0400

    2024-10-04T16:42:24.888Z function_auto_after prayer_auto

--- public/src/prayer_auto.mjs
+++ public/src/prayer_auto.mjs
@@ -7,13 +7,13 @@ export function prayer_auto() {
   let cs = countries();
   each(cs, (c) => {
     let requests = ["bless", "save each person in"];
-    each(requests, (item) => {});
-    let request = "bless";
-    let prayer_body = string_combine_multiple([
-      request,
-      " the country of ",
-      object_property_get(c, "name"),
-    ]);
-    prayer_log(prayer_body);
+    each(requests, (request) => {
+      let prayer_body = string_combine_multiple([
+        request,
+        " the country of ",
+        object_property_get(c, "name"),
+      ]);
+      prayer_log(prayer_body);
+    });
   });
 }
