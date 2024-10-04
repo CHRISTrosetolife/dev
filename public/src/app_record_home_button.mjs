647baa5dd7c21af7ff97ae855857db533da3e7a4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 14:33:54 -0400

    2024-10-04T18:33:53.231Z function_auto_after app_record_home_button

--- public/src/app_record_home_button.mjs
+++ public/src/app_record_home_button.mjs
@@ -2,5 +2,8 @@ import { app_record_home } from "./app_record_home.mjs";
 import { html_button_width_full_text_click_home } from "./html_button_width_full_text_click_home.mjs";
 export function app_record_home_button(context) {
   let { root } = context;
-  html_button_width_full_text_click_home(root, () => app_record_home(context));
+  html_button_width_full_text_click_home(
+    root,
+    async () => await app_record_home(context),
+  );
 }
