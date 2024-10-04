f25bbe9dfdfe41a7cc0601d3bf6ee96b55aa9545
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-04 19:12:41 -0400

    2024-10-04T23:12:39.654Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,6 +1,3 @@
-import { ceb_bible_words_definitions_all_cache } from "./ceb_bible_words_definitions_all_cache.mjs";
-import { list_find_property } from "./list_find_property.mjs";
 export async function sandbox_3() {
-  let c = await ceb_bible_words_definitions_all_cache();
-  return list_find_property(c, "word", "atngal");
+  let url = "https://mechon-mamre.org/p/pt/pt00.htm";
 }
