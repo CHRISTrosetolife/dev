2bd6be8f7b37b4f0265879b168d0790820e7d7b8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-05 19:45:30 -0400

    2024-10-05T23:45:29.835Z function_auto_after bible_storage_version_upload

--- public/src/bible_storage_version_upload.mjs
+++ public/src/bible_storage_version_upload.mjs
@@ -2,5 +2,5 @@ import { storage_upload_object_gitignore } from "./storage_upload_object_gitigno
 import { bible_storage_path_file_version } from "./bible_storage_path_file_version.mjs";
 export async function bible_storage_version_upload(bible_folder, key, data) {
   let destination = bible_storage_path_file_version(key, bible_folder);
-  await storage_upload_object_gitignore(data, destination);
+  await storage_upload_object_gitignore(destination, data);
 }
