eaeffa167c57929277c999e751a488c5087c167f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-05 19:32:34 -0400

    2024-10-05T23:32:34.075Z function_auto_after app_bible_search_upload

--- public/src/app_bible_search_upload.mjs
+++ public/src/app_bible_search_upload.mjs
@@ -1,4 +1,6 @@
+import { bible_search_index_cache } from "./bible_search_index_cache.mjs";
 import { storage_upload_object_gitignore } from "./storage_upload_object_gitignore.mjs";
 export async function app_bible_search_upload() {
+  let i = await bible_search_index_cache();
   await storage_upload_object_gitignore();
 }
