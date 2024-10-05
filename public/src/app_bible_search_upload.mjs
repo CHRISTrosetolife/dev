4c2fd3ed9a8c48203d275218781fd8074d181455
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-05 19:41:07 -0400

    2024-10-05T23:41:06.576Z function_auto_after app_bible_search_upload

--- public/src/app_bible_search_upload.mjs
+++ public/src/app_bible_search_upload.mjs
@@ -1,11 +1,9 @@
+import { bible_storage_version_upload } from "./bible_storage_version_upload.mjs";
 import { each_object_async } from "./each_object_async.mjs";
 import { bible_search_index_cache } from "./bible_search_index_cache.mjs";
-import { storage_upload_object_gitignore } from "./storage_upload_object_gitignore.mjs";
-import { object_properties } from "./object_properties.mjs";
 export async function app_bible_search_upload() {
   let i = await bible_search_index_cache();
   await each_object_async(i, async (word, value) => {
-    await storage_upload_object_gitignore();
+    await bible_storage_version_upload("search", word, value);
   });
-  
 }
