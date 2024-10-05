46293f8c0e18a15d8375246dc25b29d840de5603
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-05 19:43:42 -0400

    2024-10-05T23:43:40.965Z function_auto_after storage_upload_object_gitignore

--- public/src/storage_upload_object_gitignore.mjs
+++ public/src/storage_upload_object_gitignore.mjs
@@ -6,16 +6,13 @@ export async function storage_upload_object_gitignore(
   storage_path,
   result_new,
 ) {
-  log({
-    storage_path,
-    result_new,
-  });
-  return;
   await storage_upload_object(result_new, storage_path);
   let existing_path = folder_gitignore_path(storage_path);
   await file_overwrite_json(existing_path, result_new);
-  log({
-    existing_path,
-  });
+  if (false) {
+    log({
+      existing_path,
+    });
+  }
   return existing_path;
 }
