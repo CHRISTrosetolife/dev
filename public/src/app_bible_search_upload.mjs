5c3d03d3d9d0abbf8309a73c8951dd5cd491a075
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-06 19:18:16 -0400

    2024-10-06T23:18:15.649Z function_auto_after app_bible_search_upload

--- public/src/app_bible_search_upload.mjs
+++ public/src/app_bible_search_upload.mjs
@@ -1,4 +1,4 @@
-import { each } from "./each.mjs";
+import { each_async } from "./each_async.mjs";
 import { list_chunk_each } from "./list_chunk_each.mjs";
 import { each_object_async } from "./each_object_async.mjs";
 import { bible_search_index_cache } from "./bible_search_index_cache.mjs";
@@ -6,8 +6,8 @@ import { object_properties } from "./object_properties.mjs";
 export async function app_bible_search_upload() {
   let i = await bible_search_index_cache();
   let properties = object_properties(i);
-  await list_chunk_each(properties, (chunk) => {
-    each(list, (item) => {});
+  await list_chunk_each(properties, async (chunk) => {
+    await each_async(list, async (item) => {});
   });
   await each_object_async(i, async (word, value) => {});
 }
