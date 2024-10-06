c73eeb8c517e78180eb32533cdc9a20b0378dd37
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-06 19:14:18 -0400

    2024-10-06T23:14:17.692Z function_auto_after storage_upload_object_gitignore

--- public/src/storage_upload_object_gitignore.mjs
+++ public/src/storage_upload_object_gitignore.mjs
@@ -3,12 +3,14 @@ import { file_overwrite_json } from "./file_overwrite_json.mjs";
 import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 import { storage_upload_object } from "./storage_upload_object.mjs";
 import { file_exists } from "./file_exists.mjs";
+import { file_read_json } from "./file_read_json.mjs";
 export async function storage_upload_object_gitignore(
   storage_path,
   result_new,
 ) {
   let existing_path = folder_gitignore_path(storage_path);
   if (await file_exists(existing_path)) {
+    let result_existing = await file_read_json(existing_path);
   }
   await storage_upload_object(result_new, storage_path);
   await file_overwrite_json(existing_path, result_new);
