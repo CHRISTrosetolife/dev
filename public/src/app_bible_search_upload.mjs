02c7eb588d67a1ade7b8b1a4e856eb70c2161456
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-06 19:39:18 -0400

    2024-10-06T23:39:17.509Z function_auto_after app_bible_search_upload

--- public/src/app_bible_search_upload.mjs
+++ public/src/app_bible_search_upload.mjs
@@ -1,3 +1,4 @@
+import { string_symbols } from "./string_symbols.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_wait } from "./list_wait.mjs";
 import { list_map } from "./list_map.mjs";
@@ -8,6 +9,7 @@ import { object_properties } from "./object_properties.mjs";
 export async function app_bible_search_upload() {
   let i = await bible_search_index_cache();
   let properties = object_properties(i);
+  string_symbols();
   await list_chunk_each(properties, async (chunk) => {
     let mapped = list_map(chunk, async (word) => {
       let value = object_property_get(i, word);
