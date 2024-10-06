10e7270837077fc92f90c264821fcdb61feb0068
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-06 19:18:09 -0400

    2024-10-06T23:18:08.817Z function_auto_after app_bible_search_upload

--- public/src/app_bible_search_upload.mjs
+++ public/src/app_bible_search_upload.mjs
@@ -1,11 +1,13 @@
-import { bible_storage_version_upload } from "./bible_storage_version_upload.mjs";
+import { each } from "./each.mjs";
+import { list_chunk_each } from "./list_chunk_each.mjs";
 import { each_object_async } from "./each_object_async.mjs";
 import { bible_search_index_cache } from "./bible_search_index_cache.mjs";
 import { object_properties } from "./object_properties.mjs";
 export async function app_bible_search_upload() {
   let i = await bible_search_index_cache();
   let properties = object_properties(i);
-  await each_object_async(i, async (word, value) => {
-    await bible_storage_version_upload("search", word, value);
+  await list_chunk_each(properties, (chunk) => {
+    each(list, (item) => {});
   });
+  await each_object_async(i, async (word, value) => {});
 }
