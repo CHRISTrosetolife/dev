8ded44c79c2c023a1707491fdd7bdf35b80b9d74
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 09:03:43 -0400

    2024-10-07T13:03:42.847Z function_auto_after app_sleep_main

--- public/src/app_sleep_main.mjs
+++ public/src/app_sleep_main.mjs
@@ -1,13 +1,4 @@
-import { app_sleep_month_path } from "./app_sleep_month_path.mjs";
-import { firebase_save } from "./firebase_save.mjs";
-import { log } from "./log.mjs";
-import { object_property_exists } from "./object_property_exists.mjs";
-import { html_clear } from "./html_clear.mjs";
-import { html_p_text_centered } from "./html_p_text_centered.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { app_sleep_types } from "./app_sleep_types.mjs";
-import { each } from "./each.mjs";
-import { each_object } from "./each_object.mjs";
+import { app_sleep_delete_refresh } from "./app_sleep_delete_refresh.mjs";
 import { app_sleep_sleeps } from "./app_sleep_sleeps.mjs";
 import { app_sleep_wakeups } from "./app_sleep_wakeups.mjs";
 import { app_sleep_sleep } from "./app_sleep_sleep.mjs";
@@ -15,12 +6,10 @@ import { app_sleep_woke_up } from "./app_sleep_woke_up.mjs";
 import { app_sleep_manual_text } from "./app_sleep_manual_text.mjs";
 import { app_sleep_manual } from "./app_sleep_manual.mjs";
 import { html_hr } from "./html_hr.mjs";
-import { app_sleep_today_download } from "./app_sleep_today_download.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { app_sleep_record } from "./app_sleep_record.mjs";
 import { html_clear_scroll_top_context } from "./html_clear_scroll_top_context.mjs";
 import { html_div } from "./html_div.mjs";
-import { list_remove } from "./list_remove.mjs";
 export function app_sleep_main(context) {
   let root = html_clear_scroll_top_context(context);
   app_sleep_record(context, app_sleep_woke_up(), app_sleep_wakeups());
@@ -30,28 +19,7 @@ export function app_sleep_main(context) {
   });
   html_hr(root);
   let result = html_div(root);
-  html_button_width_full_text_click(root, "📃 data this month", async () => {
-    html_clear(result);
-    let { year, month_padded, data } = await app_sleep_today_download();
-    each_object(data, (day, data_day) => {
-      html_p_text_centered(result, day);
-      each(app_sleep_types(), (sleep_type) => {
-        let list_name = object_property_get(sleep_type, "list_name");
-        if (object_property_exists(data_day, list_name)) {
-          html_p_text_centered(result, list_name);
-          let list = object_property_get(data_day, list_name);
-          each(list, (item) => {
-            html_button_width_full_text_click(result, item, async () => {
-              list_remove(list, item);
-              let month_path = app_sleep_month_path(year, month_padded);
-              await firebase_save(month_path, data);
-            });
-          });
-        }
-      });
-    });
-    log({
-      data,
-    });
+  html_button_width_full_text_click(root, "delete", async () => {
+    await app_sleep_delete_refresh(result);
   });
 }
