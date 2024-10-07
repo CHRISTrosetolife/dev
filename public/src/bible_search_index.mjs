5f9e9a08baf9590b217310e32f13b67daaf99273
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-06 21:06:44 -0400

    2024-10-07T01:06:43.078Z function_auto_after bible_search_index

--- public/src/bible_search_index.mjs
+++ public/src/bible_search_index.mjs
@@ -1,5 +1,3 @@
-import { exit } from "./exit.mjs";
-import { log } from "./log.mjs";
 import { bible_search_symbols_map } from "./bible_search_symbols_map.mjs";
 import { list_add } from "./list_add.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
@@ -7,7 +5,6 @@ import { list_unique } from "./list_unique.mjs";
 import { each } from "./each.mjs";
 import { each_object } from "./each_object.mjs";
 import { bible_search_lookup_cache } from "./bible_search_lookup_cache.mjs";
-import { string_includes } from "./string_includes.mjs";
 export async function bible_search_index() {
   let i = {};
   let lookup = await bible_search_lookup_cache();
@@ -18,14 +15,6 @@ export async function bible_search_index() {
         let mapped = bible_search_symbols_map(tokens);
         let us = list_unique(mapped);
         each(us, (u) => {
-          if (string_includes(u, "?")) {
-            log({us,
-              tokens,
-              chapter_code,
-              bible_folder,u
-            });
-            exit();
-          }
           let chapter_codes = object_property_initialize(i, u, {});
           let verse_numbers = object_property_initialize(
             chapter_codes,
