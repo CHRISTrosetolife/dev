ee695cb5d4ed930f233469725cb65b84682e08e5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-06 21:03:39 -0400

    2024-10-07T01:03:38.272Z function_auto_after bible_words_map_generic

--- public/src/bible_words_map.mjs
+++
@@ -1,7 +0,0 @@
-import { bible_words_map_generic } from "./bible_words_map_generic.mjs";
-import { string_trim } from "./string_trim.mjs";
-export function bible_words_map(all, symbols) {
-  let lambda = string_trim;
-  all = bible_words_map_generic(lambda, all, symbols);
-  return all;
-}
