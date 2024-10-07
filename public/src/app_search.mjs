91f7a0fb47d72838b7cf898b6a38c3f777699c18
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 18:42:41 -0400

    2024-10-07T22:42:40.819Z function_auto_after app_search

--- public/src/app_search.mjs
+++ public/src/app_search.mjs
@@ -1,3 +1,4 @@
+import { app_search_folder } from "./app_search_folder.mjs";
 import { bible_storage_path_file_version } from "./bible_storage_path_file_version.mjs";
 import { firebase_download } from "./firebase_download.mjs";
 import { bible_search_results } from "./bible_search_results.mjs";
@@ -6,7 +7,10 @@ export async function app_search() {
   let body = html_style_default_initialize();
   let filtered = await bible_search_results(split, word_to_results, filter);
   async function word_to_results(word) {
-    let destination = bible_storage_path_file_version(key, bible_folder);
-    return await firebase_download(i, word);
+    let destination = bible_storage_path_file_version(
+      word,
+      app_search_folder(),
+    );
+    return await firebase_download(destination);
   }
 }
