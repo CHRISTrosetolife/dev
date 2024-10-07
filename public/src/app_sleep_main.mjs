d093e52dc912db07a256ac5b81c97eabd5d9e2b0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 08:50:50 -0400

    2024-10-07T12:50:49.959Z function_auto_after app_sleep_main

--- public/src/app_sleep_main.mjs
+++ public/src/app_sleep_main.mjs
@@ -31,6 +31,7 @@ export function app_sleep_main(context) {
       each(app_sleep_types(), (sleep_type) => {
         let list_name = object_property_get(sleep_type, "list_name");
         let list = object_property_get(data_day, list_name);
+        each(list2, (item) => {});
       });
     });
     html_inner_set(result, json_format_to(data));
