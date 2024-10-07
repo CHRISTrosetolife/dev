cbbd3424da499d13b5da1d4d10da110312404a70
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 09:17:51 -0400

    2024-10-07T13:17:51.125Z function_auto_after app_sleep_manual_refresh

--- public/src/app_sleep_manual_refresh.mjs
+++ public/src/app_sleep_manual_refresh.mjs
@@ -147,7 +147,7 @@ export function app_sleep_manual_refresh(context, parts) {
       }
     } else {
       if (equal(hours_12, 12)) {
-        log({});
+        log("here");
         hours = 0;
       } else {
         hours = hours_12;
