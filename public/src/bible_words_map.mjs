cc77fd220b333e3e1b0276458b37437ef0f2e9f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-06 21:03:24 -0400

    2024-10-07T01:03:23.209Z pb bible_words_map_genric

--- public/src/bible_words_map.mjs
+++ public/src/bible_words_map.mjs
@@ -2,6 +2,6 @@ import { bible_words_map_genric } from "./bible_words_map_genric.mjs";
 import { string_trim } from "./string_trim.mjs";
 export function bible_words_map(all, symbols) {
   let lambda = string_trim;
-  all = bible_words_map_genric(all, lambda, symbols);
+  all = bible_words_map_genric(lambda, all, symbols);
   return all;
 }
--- public/src/bible_words_map_genric.mjs
+++ public/src/bible_words_map_genric.mjs
@@ -2,7 +2,7 @@ import { string_empty_not_is } from "./string_empty_not_is.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { list_map } from "./list_map.mjs";
-export function bible_words_map_genric(all, lambda, symbols) {
+export function bible_words_map_genric(lambda, all, symbols) {
   all = list_map(all, (a) => lambda(a, symbols));
   all = list_map(all, string_case_lower);
   all = list_filter(all, string_empty_not_is);
