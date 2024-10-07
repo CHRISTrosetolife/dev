e6c81a2847c3a9574dcb0cd1bc530eccb718d4c5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 08:56:23 -0400

    2024-10-07T12:56:22.884Z function_auto_after app_sleep_main

--- public/src/app_sleep_main.mjs
+++ public/src/app_sleep_main.mjs
@@ -32,8 +32,8 @@ export function app_sleep_main(context) {
     each_object(data, (day, data_day) => {
       html_p_text_centered(result, day);
       each(app_sleep_types(), (sleep_type) => {
-        html_p_text_centered(result, sleep_type);
         let list_name = object_property_get(sleep_type, "list_name");
+        html_p_text_centered(result, list_name);
         if (object_property_exists(data_day, list_name)) {
           let list = object_property_get(data_day, list_name);
           each(list, (item) => {
