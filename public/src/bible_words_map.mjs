6f52ea9dc719731dd3984b5a3cc3594292a33178
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-06 21:02:57 -0400

    2024-10-07T01:02:56.688Z function_auto_after bible_words_map

--- public/src/bible_words_map.mjs
+++ public/src/bible_words_map.mjs
@@ -1,12 +1,7 @@
-import { string_empty_not_is } from "./string_empty_not_is.mjs";
-import { list_filter } from "./list_filter.mjs";
-import { string_case_lower } from "./string_case_lower.mjs";
+import { bible_words_map_genric } from "./bible_words_map_genric.mjs";
 import { string_trim } from "./string_trim.mjs";
-import { list_map } from "./list_map.mjs";
 export function bible_words_map(all, symbols) {
   let lambda = string_trim;
-  all = list_map(all, (a) => lambda(a, symbols));
-  all = list_map(all, string_case_lower);
-  all = list_filter(all, string_empty_not_is);
+  all = bible_words_map_genric(all, lambda, symbols);
   return all;
 }
