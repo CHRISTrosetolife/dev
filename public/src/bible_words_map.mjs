4bba29323c402fdb65209f399e5f8aebafbd5e4a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-06 21:02:25 -0400

    2024-10-07T01:02:24.908Z function_auto_after bible_words_map

--- public/src/bible_words_map.mjs
+++ public/src/bible_words_map.mjs
@@ -5,7 +5,7 @@ import { string_trim } from "./string_trim.mjs";
 import { list_map } from "./list_map.mjs";
 export function bible_words_map(all, symbols) {
   let lambda = string_trim;
-  all = list_map(all, (a) => string_trim(a, symbols));
+  all = list_map(all, (a) => lambda(a, symbols));
   all = list_map(all, string_case_lower);
   all = list_filter(all, string_empty_not_is);
   return all;
