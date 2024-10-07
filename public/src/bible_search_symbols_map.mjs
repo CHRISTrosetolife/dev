1ef6e8c4075b2ee7d7d1868b2ab61452e3405466
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-06 21:06:07 -0400

    2024-10-07T01:06:06.893Z function_auto_after bible_search_symbols_map

--- public/src/bible_search_symbols_map.mjs
+++ public/src/bible_search_symbols_map.mjs
@@ -1,10 +1,10 @@
-import { string_trim } from "./string_trim.mjs";
+import { string_replace_multiple } from "./string_replace_multiple.mjs";
 import { bible_words_map_generic } from "./bible_words_map_generic.mjs";
 import { bible_search_symbols } from "./bible_search_symbols.mjs";
 export function bible_search_symbols_map(tokens) {
   let symbols = bible_search_symbols();
   let mapped = bible_words_map_generic(
-    (a, symbols) => string_trim(a, symbols),
+    (a, symbols) => string_replace_multiple(a, symbols),
     tokens,
     symbols,
   );
