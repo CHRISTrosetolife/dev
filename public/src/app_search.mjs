41db00485bb2adf01b1ede5e724bb8d34743b286
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 19:45:11 -0400

    2024-10-07T23:45:10.337Z function_auto_after app_search

--- public/src/app_search.mjs
+++ public/src/app_search.mjs
@@ -1,5 +1,3 @@
-import { string_empty_not_is } from "./string_empty_not_is.mjs";
-import { list_filter } from "./list_filter.mjs";
 import { html_input_width_full_focus } from "./html_input_width_full_focus.mjs";
 import { html_script_axios } from "./html_script_axios.mjs";
 import { firebase_initialize } from "./firebase_initialize.mjs";
@@ -23,8 +21,11 @@ export async function app_search() {
   html_button_width_full_text_click(root, "search", async () => {
     let v = html_value_get(i);
     let split = string_split_space(v);
-    let f = list_filter(split, string_empty_not_is);
-    let filtered = await bible_search_results(f, word_to_results, tautology);
+    let filtered = await bible_search_results(
+      split,
+      word_to_results,
+      tautology,
+    );
   });
   async function word_to_results(word) {
     let destination = bible_storage_path_file_version(
