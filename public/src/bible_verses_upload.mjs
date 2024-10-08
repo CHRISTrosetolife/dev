320e7e0442dc872bf9778e243786d198cd96ce8b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 20:53:46 -0400

    2024-10-08T00:53:45.296Z function_auto_after bible_verses_upload

--- public/src/bible_verses_upload.mjs
+++ public/src/bible_verses_upload.mjs
@@ -4,6 +4,9 @@ import { bible_books_chapter_each } from "./bible_books_chapter_each.mjs";
 export async function bible_verses_upload() {
   await bible_books_chapter_each("engbsb", (chapter) => {
     let { book_code, chapter_code } = bible_chapter_name_parse(chapter);
-    log({});
+    log({
+      book_code,
+      chapter_code,
+    });
   });
 }
