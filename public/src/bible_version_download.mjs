13712e22e262afd28a81c7de029ec3430fdd8515
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 10:45:34 -0400

    2024-10-08T14:45:32.174Z function_auto_after bible_version_download

--- public/src/bible_version_download.mjs
+++ public/src/bible_version_download.mjs
@@ -1,1 +1,6 @@
-export function bible_version_download() {}
+import { bible_versions_download } from "./bible_versions_download.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
+export async function bible_version_download(bible_folder) {
+  assert_arguments_length(arguments, 1);
+  return await bible_versions_download([bible_folder]);
+}
