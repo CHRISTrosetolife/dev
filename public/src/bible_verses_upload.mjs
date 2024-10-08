600dce8f81e598960f207bd15e54595bb526c26f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 20:56:53 -0400

    2024-10-08T00:56:52.365Z function_auto_after bible_verses_upload

--- public/src/bible_verses_upload.mjs
+++ public/src/bible_verses_upload.mjs
@@ -1,14 +1,9 @@
-import { bible_chapter_unpadded } from "./bible_chapter_unpadded.mjs";
-import { log } from "./log.mjs";
+import { bible_chapter } from "./bible_chapter.mjs";
 import { bible_chapter_name_parse } from "./bible_chapter_name_parse.mjs";
 import { bible_books_chapter_each } from "./bible_books_chapter_each.mjs";
 export async function bible_verses_upload() {
-  await bible_books_chapter_each("engbsb", (chapter) => {
+  await bible_books_chapter_each("engbsb", async (chapter) => {
     let { book_code, chapter_code } = bible_chapter_name_parse(chapter);
-    chapter_code = bible_chapter_unpadded(chapter_code);
-    log({
-      book_code,
-      chapter_code,
-    });
+    await bible_chapter(book_code, cha);
   });
 }
