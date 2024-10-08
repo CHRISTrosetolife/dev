3bac5e52aaf4da51e063ccd57077bd1a6f0d79a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 20:59:54 -0400

    2024-10-08T00:59:53.738Z function_auto_after bible_verses_upload

--- public/src/bible_verses_upload.mjs
+++ public/src/bible_verses_upload.mjs
@@ -4,6 +4,7 @@ import { each_async } from "./each_async.mjs";
 import { bible_chapter } from "./bible_chapter.mjs";
 import { bible_chapter_name_parse } from "./bible_chapter_name_parse.mjs";
 import { bible_books_chapter_each } from "./bible_books_chapter_each.mjs";
+import { path_join } from "./path_join.mjs";
 export async function bible_verses_upload() {
   let bible_folder = "engbsb";
   await bible_books_chapter_each(bible_folder, async (chapter) => {
@@ -11,6 +12,7 @@ export async function bible_verses_upload() {
     let verses = await bible_chapter(bible_folder, chapter);
     await each_async(verses, async (verse) => {
       let { verse_number } = verse;
+      let key = path_join();
       let storage_path = bible_storage_path_file_version(key, bible_folder);
       await storage_upload_object_gitignore(storage_path, data);
     });
