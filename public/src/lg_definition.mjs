adb64ed91fdde171fd7d02928a71b2a7f93e791e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 19:10:55 -0400

    2024-10-08T23:10:55.032Z function_auto_after lg_definition

--- public/src/lg_definition.mjs
+++ public/src/lg_definition.mjs
@@ -1,5 +1,7 @@
+import { http_cache } from "./http_cache.mjs";
 import { url_secure } from "./url_secure.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-export function lg_definition() {
+export async function lg_definition() {
+  await http_cache();
   let u = string_combine_multiple([url_secure(), "learnluganda.com/concise"]);
 }
