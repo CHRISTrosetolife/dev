52dbade459c64765e5de8e80b320ba5ec180050a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 20:52:40 -0400

    2024-10-08T00:52:39.839Z function_auto_after bible_verses_upload

--- public/src/bible_verses_upload.mjs
+++ public/src/bible_verses_upload.mjs
@@ -1,1 +1,5 @@
-export function bible_verses_upload() {}
+import { log } from "./log.mjs";
+import { bible_books_chapter_each } from "./bible_books_chapter_each.mjs";
+export async function bible_verses_upload() {
+  await bible_books_chapter_each("engbsb", log({}));
+}
