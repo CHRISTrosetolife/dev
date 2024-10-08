f30ac845b3655e0c3065ef4685a76b43f95182b1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 10:31:23 -0400

    2024-10-08T14:31:20.829Z function_auto_after app_search

--- public/src/app_search.mjs
+++ public/src/app_search.mjs
@@ -1,3 +1,4 @@
+import { html_div } from "./html_div.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { bible_reference_code } from "./bible_reference_code.mjs";
 import { app_search_bible_folder } from "./app_search_bible_folder.mjs";
@@ -37,6 +38,7 @@ export async function app_search() {
     each(filtered, (f) => {
       let { chapter_code, verse_number } = f;
       let r = bible_reference_code(chapter_code, verse_number);
+      let result = html_div(root);
       let b = html_button_width_full_text_click(root, r, async () => {
         let storage_path = bible_verses_upload_path(
           app_search_bible_folder(),
