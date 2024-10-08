b7e17c7c1f308d0cee195173518227c773a37868
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 21:02:14 -0400

    2024-10-08T01:02:13.609Z function_auto_after bible_verses_upload

--- public/src/bible_verses_upload.mjs
+++ public/src/bible_verses_upload.mjs
@@ -1,17 +1,17 @@
 import { exit } from "./exit.mjs";
 import { storage_upload_object_gitignore } from "./storage_upload_object_gitignore.mjs";
 import { bible_storage_path_file_version } from "./bible_storage_path_file_version.mjs";
-import { each_async } from "./each_async.mjs";
 import { bible_chapter } from "./bible_chapter.mjs";
 import { bible_chapter_name_parse } from "./bible_chapter_name_parse.mjs";
 import { bible_books_chapter_each } from "./bible_books_chapter_each.mjs";
 import { path_join } from "./path_join.mjs";
+import { list_map } from "./list_map.mjs";
 export async function bible_verses_upload() {
   let bible_folder = "engbsb";
   await bible_books_chapter_each(bible_folder, async (chapter) => {
     let { book_code, chapter_code } = bible_chapter_name_parse(chapter);
     let verses = await bible_chapter(bible_folder, chapter);
-    await each_async(verses, async (verse) => {
+    let mapped = list_map(verses, async (verse) => {
       let { verse_number, tokens } = verse;
       let key = path_join([book_code, chapter_code, verse_number]);
       let storage_path = bible_storage_path_file_version(key, bible_folder);
