53ca30e195a3a563b66e640d7597b23b27c83bf0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 19:23:20 -0400

    2024-10-08T23:23:15.051Z function_auto_after lg_definition

--- public/src/lg_definition.mjs
+++ public/src/lg_definition.mjs
@@ -2,7 +2,6 @@ import { string_starts_with } from "./string_starts_with.mjs";
 import { html_parse_text } from "./html_parse_text.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
-import { log } from "./log.mjs";
 import { html_parse_visit_class_single } from "./html_parse_visit_class_single.mjs";
 import { http_cache_parse_parsed } from "./http_cache_parse_parsed.mjs";
 import { url_secure } from "./url_secure.mjs";
@@ -16,11 +15,10 @@ export async function lg_definition() {
   each(object_property_get(d, "children"), (c) => {
     let t = html_parse_text(c);
     if (string_starts_with(t, first)) {
-      log({
-        c,
-      });
+      first_found = true;
     }
-    if (false) {
+    if (!first_found) {
+      return;
     }
   });
 }
