64c79991db286e852601da14f9b7967d5854d37e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 19:11:09 -0400

    2024-10-08T23:11:08.680Z function_auto_after lg_definition

--- public/src/lg_definition.mjs
+++ public/src/lg_definition.mjs
@@ -2,6 +2,6 @@ import { http_cache } from "./http_cache.mjs";
 import { url_secure } from "./url_secure.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function lg_definition() {
-  await http_cache();
   let u = string_combine_multiple([url_secure(), "learnluganda.com/concise"]);
+  await http_cache(u);
 }
