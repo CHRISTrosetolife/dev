b3b64c632efcdbf56842abdd574ca76ba7f52fe2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 21:34:10 -0400

    2024-10-08T01:34:09.679Z function_auto_after bible_verses_upload_path

--- public/src/bible_verses_upload_path.mjs
+++ public/src/bible_verses_upload_path.mjs
@@ -1,11 +1,8 @@
+import { bible_chapter_name_parse } from "./bible_chapter_name_parse.mjs";
 import { bible_storage_path_file_version } from "./bible_storage_path_file_version.mjs";
 import { path_join } from "./path_join.mjs";
-export function bible_verses_upload_path(
-  book_code,
-  chapter_code,
-  verse_number,
-  bible_folder,
-) {
+export function bible_verses_upload_path(chapter, verse_number, bible_folder) {
+  let { book_code, chapter_code } = bible_chapter_name_parse(chapter);
   let key = path_join([book_code, chapter_code, verse_number]);
   let storage_path = bible_storage_path_file_version(key, bible_folder);
   return storage_path;
