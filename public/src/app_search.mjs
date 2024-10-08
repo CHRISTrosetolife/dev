9f101824fa6211c40531df07430949ccc2b1c642
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 21:29:30 -0400

    2024-10-08T01:29:29.151Z function_auto_after app_search

--- public/src/app_search.mjs
+++ public/src/app_search.mjs
@@ -1,7 +1,6 @@
 import { html_input_width_full_focus } from "./html_input_width_full_focus.mjs";
 import { html_script_axios } from "./html_script_axios.mjs";
 import { firebase_initialize } from "./firebase_initialize.mjs";
-import { log } from "./log.mjs";
 import { app_search_folder } from "./app_search_folder.mjs";
 import { bible_storage_path_file_version } from "./bible_storage_path_file_version.mjs";
 import { firebase_download } from "./firebase_download.mjs";
@@ -12,6 +11,7 @@ import { html_p_text_centered } from "./html_p_text_centered.mjs";
 import { html_value_get } from "./html_value_get.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { string_split_space } from "./string_split_space.mjs";
+import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 export async function app_search() {
   let root = html_style_default_initialize();
   await html_script_axios(root);
@@ -20,15 +20,13 @@ export async function app_search() {
   let i = html_input_width_full_focus(root);
   html_button_width_full_text_click(root, "🔎 search", async () => {
     let v = html_value_get(i);
+    html_clear_scroll_top(root);
     let split = string_split_space(v);
     let filtered = await bible_search_results(
       split,
       word_to_results,
       tautology,
     );
-    log({
-      filtered,
-    });
   });
   async function word_to_results(word) {
     let destination = bible_storage_path_file_version(
