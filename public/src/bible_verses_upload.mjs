3ae07f4bfc5c7a13f9195fbcf5a048a30f6ab81f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 20:53:14 -0400

    2024-10-08T00:53:13.862Z function_auto_after bible_verses_upload

--- public/src/bible_verses_upload.mjs
+++ public/src/bible_verses_upload.mjs
@@ -1,5 +1,7 @@
-import { log } from "./log.mjs";
+import { bible_chapter_name_parse } from "./bible_chapter_name_parse.mjs";
 import { bible_books_chapter_each } from "./bible_books_chapter_each.mjs";
 export async function bible_verses_upload() {
-  await bible_books_chapter_each("engbsb", log);
+  await bible_books_chapter_each("engbsb", (chapter_code) => {
+    bible_chapter_name_parse();
+  });
 }
