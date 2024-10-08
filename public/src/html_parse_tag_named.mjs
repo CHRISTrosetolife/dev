409d7de4e2db99d842e52093afb125e9621d8438
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 19:32:14 -0400

    2024-10-08T23:32:12.829Z function_auto_after html_parse_tag_named

--- public/src/html_parse_tag_named.mjs
+++ public/src/html_parse_tag_named.mjs
@@ -1,8 +1,9 @@
+import { html_parse_tag_is } from "./html_parse_tag_is.mjs";
 import { equal } from "./equal.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function html_parse_tag_named(parent, tag_name) {
   return (
-    parent.type === "tag" &&
+    html_parse_tag_is(parent) &&
     equal(object_property_get(parent, "name"), tag_name)
   );
 }
