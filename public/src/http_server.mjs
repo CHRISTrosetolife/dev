d63decf54320634637322600ea36d140727fe09a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-07 21:42:39 -0400

    2024-10-08T01:42:38.423Z function_auto_after http_server

--- public/src/http_server.mjs
+++ public/src/http_server.mjs
@@ -1,9 +1,12 @@
+import { command_line_exec } from "./command_line_exec.mjs";
 import { repeat } from "./repeat.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { log } from "./log.mjs";
 export async function http_server() {
   log(fn_name("http_server"));
-  await repeat(() => {});
+  await repeat(async () => {
+    await command_line_exec("http-server");
+  });
   log(string_combine_multiple([fn_name("http_server"), " ended"]));
 }
