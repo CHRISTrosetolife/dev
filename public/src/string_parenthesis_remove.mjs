eadef41678d76879ab070728ba05891636281a90
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 20:55:51 -0400

    2024-10-09T00:55:51.327Z function_auto_after string_parenthesis_remove

--- public/src/string_parenthesis_remove.mjs
+++ public/src/string_parenthesis_remove.mjs
@@ -1,15 +1,15 @@
+import { string_last_index } from "./string_last_index.mjs";
 import { string_size } from "./string_size.mjs";
 import { string_skip } from "./string_skip.mjs";
 import { string_take } from "./string_take.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { string_index_last } from "./string_index_last.mjs";
 import { string_index } from "./string_index.mjs";
 import { string_includes_multiple } from "./string_includes_multiple.mjs";
 export function string_parenthesis_remove(t) {
   if (string_includes_multiple(t, "()")) {
     let l = string_index(t, "(");
     let right = ")";
-    let r = string_index_last(t, right);
+    let r = string_last_index(t, right);
     t = string_combine_multiple([
       string_take(t, l),
       string_skip(t, r + string_size(right)),
