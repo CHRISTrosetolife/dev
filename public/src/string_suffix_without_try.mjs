a4355f4a0a66beb396b430522df4609ae5757f96
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 20:05:17 -0400

    2024-10-09T00:05:17.206Z function_auto_after string_suffix_without_try

--- public/src/string_suffix_without_try.mjs
+++ public/src/string_suffix_without_try.mjs
@@ -1,7 +1,7 @@
 import { string_suffix_without } from "./string_suffix_without.mjs";
 import { string_ends_with } from "./string_ends_with.mjs";
 export function string_suffix_without_try(t, suffix) {
-  if (string_ends_with(suffix)) {
+  if (string_ends_with(t, suffix)) {
     t = string_suffix_without(t, suffix);
   }
   return t;
