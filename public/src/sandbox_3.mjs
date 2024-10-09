167483e3653f6958cf27f8fcd0ad27ff41b2e05a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 20:37:55 -0400

    2024-10-09T00:37:54.560Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,6 +1,8 @@
+import { lg_definitions } from "./lg_definitions.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { bible_words_eng_score } from "./bible_words_eng_score.mjs";
 export async function sandbox_3() {
+  let dictionary = await lg_definitions();
   let words = await bible_words_eng_score();
   list_adder((la) => {});
 }
