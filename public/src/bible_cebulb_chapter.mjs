82b31462bfe0787750f7f2c2db531c4757fb50e4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-09 10:15:13 -0400

    2024-10-09T14:15:13.067Z function_auto_after bible_cebulb_chapter

--- public/src/bible_cebulb_chapter.mjs
+++ public/src/bible_cebulb_chapter.mjs
@@ -1,7 +1,9 @@
+import { bible_chapter } from "./bible_chapter.mjs";
 import { bible_verses_parse } from "./bible_verses_parse.mjs";
 import { html_parse_visit_class_list } from "./html_parse_visit_class_list.mjs";
 import { bible_chapter_parsed } from "./bible_chapter_parsed.mjs";
 export async function bible_cebulb_chapter(chapter_name) {
+  return await bible_chapter();
   let parsed_ceb = await bible_chapter_parsed("cebulb", chapter_name);
   let verses_ceb = html_parse_visit_class_list(parsed_ceb, "p");
   let ceb = bible_verses_parse(verses_ceb);
