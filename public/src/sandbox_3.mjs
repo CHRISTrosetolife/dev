27e61017947882aa7650eb380ffe545daa37fc90
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 20:38:06 -0400

    2024-10-09T00:38:05.789Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,8 +1,11 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { lg_definitions } from "./lg_definitions.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { bible_words_eng_score } from "./bible_words_eng_score.mjs";
+import { list_filter } from "./list_filter.mjs";
 export async function sandbox_3() {
   let dictionary = await lg_definitions();
   let words = await bible_words_eng_score();
+  list_filter(words, (w) => object_property_get(object, "property_name"));
   list_adder((la) => {});
 }
