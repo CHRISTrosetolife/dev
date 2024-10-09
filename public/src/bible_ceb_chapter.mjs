e1e6c24e4d41c8415c1484b9a9342e3f2f9b05db
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-09 10:24:24 -0400

    2024-10-09T14:24:23.016Z a bible_ceb_chapter

--- public/src/bible_ceb_chapter.mjs
+++ public/src/bible_ceb_chapter.mjs
@@ -1,6 +1,6 @@
+import { bible_chapter } from "./bible_chapter.mjs";
 import { list_size } from "./list_size.mjs";
 import { assert_message } from "./assert_message.mjs";
-import { bible_cebulb_chapter } from "./bible_cebulb_chapter.mjs";
 import { each } from "./each.mjs";
 import { each_object } from "./each_object.mjs";
 import { object_property_get_curry } from "./object_property_get_curry.mjs";
@@ -48,8 +48,7 @@ export async function bible_ceb_chapter(chapter_name) {
     list_sort_string(eng, object_property_get_curry("verse_number"));
   }
   let eng_verse_numbers = list_map_property(eng, "verse_number");
-  $ex;
-  let ceb = await bible_cebulb_chapter(chapter_name);
+  let ceb = await bible_chapter("cebulb", chapter_name);
   let ceb_verse_numbers = list_map_property(ceb, "verse_number");
   let ceb_verse_numbers_difference = list_difference(
     ceb_verse_numbers,
