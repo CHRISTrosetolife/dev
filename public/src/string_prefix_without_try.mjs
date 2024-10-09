c625b26f1dfe9cd75bcc9b08478aa91f63526199
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 20:20:25 -0400

    2024-10-09T00:20:24.507Z function_auto_after string_prefix_without_try

--- public/src/string_prefix_without_try.mjs
+++ public/src/string_prefix_without_try.mjs
@@ -1,8 +1,8 @@
-import { string_suffix_without } from "./string_suffix_without.mjs";
-import { string_ends_with } from "./string_ends_with.mjs";
-export function string_prefix_without_try(t, suffix) {
-  if (string_ends_with(t, suffix)) {
-    t = string_suffix_without(t, suffix);
+import { string_prefix_without } from "./string_prefix_without.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
+export function string_prefix_without_try(t, prefix) {
+  if (string_starts_with(t, prefix)) {
+    t = string_prefix_without(t, prefix);
   }
   return t;
 }
