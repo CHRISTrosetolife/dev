2bf7f5d4bbe5546c99a8e515b11d7b885cba4a9a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-09 10:31:02 -0400

    2024-10-09T14:31:01.935Z function_auto_after bible_ceb_chapter

--- public/src/bible_ceb_chapter.mjs
+++ public/src/bible_ceb_chapter.mjs
@@ -1,5 +1,4 @@
 import { bible_chapter_foreign } from "./bible_chapter_foreign.mjs";
 export async function bible_ceb_chapter(chapter_name) {
-  let bible_folder = "cebulb";
-  return await bible_chapter_foreign(chapter_name, bible_folder);
+  return await bible_chapter_foreign(chapter_name, "cebulb");
 }
