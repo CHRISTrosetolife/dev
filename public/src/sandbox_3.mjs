62210440f9d49d9b6b5ee70f89a09b9128102209
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 20:39:02 -0400

    2024-10-09T00:39:02.242Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,6 +1,5 @@
 import { list_map_property } from "./list_map_property.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { lg_definitions } from "./lg_definitions.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { bible_words_eng_score } from "./bible_words_eng_score.mjs";
@@ -9,8 +8,6 @@ export async function sandbox_3() {
   let dictionary = await lg_definitions();
   let words = await bible_words_eng_score();
   let ms = list_map_property(words, "word");
-  let fs = list_filter(words, (w) =>
-    object_property_exists(dictionary, object_property_get(w, "word")),
-  );
+  let fs = list_filter(ms, (w) => object_property_exists(dictionary, w));
   list_adder((la) => {});
 }
