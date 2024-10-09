d5460ab56d9e51cb56ceda725279e0a857d80dca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-09 10:35:54 -0400

    2024-10-09T14:35:53.506Z function_auto_after ceb_bible_words_5

--- public/src/ceb_bible_words_5.mjs
+++ public/src/ceb_bible_words_5.mjs
@@ -1,5 +1,4 @@
 import { bible_foreign_words } from "./bible_foreign_words.mjs";
 export async function ceb_bible_words_5(args) {
-  let bible_folder = "cebulb";
-  return await bible_foreign_words(args, bible_folder);
+  return await bible_foreign_words(args, "cebulb");
 }
