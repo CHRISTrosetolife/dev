4ef1893b39946633a2d60cc44089055cdeea0227
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-09 10:25:32 -0400

    2024-10-09T14:25:31.149Z pd js_dollar_expand_await 5

--- public/src/js_dollar_expand_await.mjs
+++ public/src/js_dollar_expand_await.mjs
@@ -1,18 +1,11 @@
 import { js_dollar_expand } from "./js_dollar_expand.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
-export async function js_dollar_expand_await(
-  e,
-  ast,
-  id,
-  a,
-  parent,
-  lambda_else,
-) {
+export async function js_dollar_expand_await(e, ast, id, a, parent) {
   if (js_node_type_is(e, "AwaitExpression")) {
     let arg = object_property_get(e, "argument");
     await js_dollar_expand(ast, arg, id, a, parent);
   } else {
-    await lambda_else();
+    await js_dollar_expand(ast, init, id, a, parent);
   }
 }
