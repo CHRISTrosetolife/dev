23cf02d05c995fef5f49eff0f801ae73f765efeb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 20:37:33 -0400

    2024-10-09T00:37:33.170Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,4 +1,6 @@
+import { list_adder } from "./list_adder.mjs";
 import { bible_words_eng_score } from "./bible_words_eng_score.mjs";
 export async function sandbox_3() {
   let words = await bible_words_eng_score();
+  list_adder((la) => {});
 }
