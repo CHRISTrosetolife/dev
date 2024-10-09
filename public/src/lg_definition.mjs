43cdece54257a47b2ecc3977c152d5341ab4cabb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 20:04:46 -0400

    2024-10-09T00:04:46.040Z pb string_suffix_without_try

--- public/src/lg_definition.mjs
+++ public/src/lg_definition.mjs
@@ -47,7 +47,7 @@ export async function lg_definition() {
     let symbols = ";!?.'\"()";
     let symbols_space = string_combine(symbols, " -");
     t = string_trim(t, symbols_space);
-    t = string_suffix_without_try("! int", t);
+    t = string_suffix_without_try(t, "! int");
     if (string_includes_multiple(t, symbols)) {
       log({
         t,
--- public/src/string_suffix_without_try.mjs
+++ public/src/string_suffix_without_try.mjs
@@ -1,6 +1,6 @@
 import { string_suffix_without } from "./string_suffix_without.mjs";
 import { string_ends_with } from "./string_ends_with.mjs";
-export function string_suffix_without_try(suffix, t) {
+export function string_suffix_without_try(t, suffix) {
   if (string_ends_with(suffix)) {
     t = string_suffix_without(t, suffix);
   }
