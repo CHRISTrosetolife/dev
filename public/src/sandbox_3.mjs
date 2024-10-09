7ab374dde434463943956284f424584a0bb7ae24
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-08 20:38:55 -0400

    2024-10-09T00:38:54.810Z function_auto_after sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,3 +1,4 @@
+import { list_map_property } from "./list_map_property.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { lg_definitions } from "./lg_definitions.mjs";
@@ -7,6 +8,7 @@ import { list_filter } from "./list_filter.mjs";
 export async function sandbox_3() {
   let dictionary = await lg_definitions();
   let words = await bible_words_eng_score();
+  let ms = list_map_property(words, "word");
   let fs = list_filter(words, (w) =>
     object_property_exists(dictionary, object_property_get(w, "word")),
   );
