5f905b524ca5b0e091206a4540a8d38374edb914
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-09 09:13:22 -0400

    2024-10-09T13:13:20.188Z function_auto_after string_take_find

--- public/src/string_take_find.mjs
+++ public/src/string_take_find.mjs
@@ -1,8 +1,8 @@
 import { string_take } from "./string_take.mjs";
 import { string_index } from "./string_index.mjs";
-export function string_take_find(n) {
+export function string_take_find(n, search) {
   let result;
-  let index = string_index(n, ".");
+  let index = string_index(n, search);
   if (index < 0) {
     result = n;
   } else {
