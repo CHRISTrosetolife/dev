ea59a1d6464abab4c48ad4b8e32b8dcbd9297a4a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-09 20:29:34 -0400

    2024-10-10T00:29:32.160Z r ceb_bible_words_100 ceb_bible_words_top_100

--- public/src/ceb_bible_words_top_100.mjs
+++ public/src/ceb_bible_words_top_100.mjs
@@ -1,4 +1,4 @@
 import { ceb_bible_words_top } from "./ceb_bible_words_top.mjs";
-export async function ceb_bible_words_100() {
+export async function ceb_bible_words_top_100() {
   return await ceb_bible_words_top(100);
 }
