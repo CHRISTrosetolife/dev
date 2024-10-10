94652d78fcfe89d546c97149b16b3d7de679cf06
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-09 20:46:46 -0400

    2024-10-10T00:46:45.613Z r ceb_bible_words_count bible_words_language_count

--- public/src/bible_words_language_count.mjs
+++ public/src/bible_words_language_count.mjs
@@ -1,6 +1,6 @@
 import { bible_words_language_cache } from "./bible_words_language_cache.mjs";
 import { bible_words_count_generic } from "./bible_words_count_generic.mjs";
-export async function ceb_bible_words_count(args) {
+export async function bible_words_language_count(args) {
   let all = await bible_words_language_cache(args);
   let symbols = "_-–—,;:!?.'‘’\"“”()[]}¶/`¯|€0123456789";
   return bible_words_count_generic(all, symbols);
--- public/src/ceb_bible_words_count_cache.mjs
+++ public/src/ceb_bible_words_count_cache.mjs
@@ -1,5 +1,5 @@
-import { ceb_bible_words_count } from "./ceb_bible_words_count.mjs";
+import { bible_words_language_count } from "./bible_words_language_count.mjs";
 import { function_cache } from "./function_cache.mjs";
 export async function ceb_bible_words_count_cache(args) {
-  return await function_cache(ceb_bible_words_count, [args]);
+  return await function_cache(bible_words_language_count, [args]);
 }
