0b870aecdeb2793113fcfb270be0acf49e0b877e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-09 20:33:24 -0400

    2024-10-10T00:33:23.366Z function_auto_after bible_words_language_rename

--- public/src/bible_words_language_rename.mjs
+++ public/src/bible_words_language_rename.mjs
@@ -3,5 +3,5 @@ import { string_underscore_trail } from "./string_underscore_trail.mjs";
 import { fn_name } from "./fn_name.mjs";
 export async function bible_words_language_rename() {
   let prefix = string_underscore_trail(fn_name("ceb_bible_words"));
-  await function_names_each(lambda);
+  await function_names_each(() => {});
 }
