28c7e5bc9a6058e387404296933a038ad6d1e366
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-09 20:30:30 -0400

    2024-10-10T00:30:29.322Z function_auto_after bible_words_language_rename

--- public/src/bible_words_language_rename.mjs
+++ public/src/bible_words_language_rename.mjs
@@ -1,1 +1,5 @@
-export function bible_words_language_rename() {}
+import { fn_name } from "./fn_name.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function bible_words_language_rename() {
+  let prefix = string_combine_multiple([fn_name("bible_words_language"), "_"]);
+}
