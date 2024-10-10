64699c976af97ed1f3f7ea85f6f14e60cef2bdae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-09 20:34:17 -0400

    2024-10-10T00:34:17.089Z function_auto_after bible_words_language_rename

--- public/src/bible_words_language_rename.mjs
+++ public/src/bible_words_language_rename.mjs
@@ -6,6 +6,8 @@ export async function bible_words_language_rename() {
   let prefix = string_underscore_trail(fn_name("ceb_bible_words"));
   await function_names_each((fn) => {
     if (string_starts_with(fn, prefix)) {
+      if (false) {
+      }
     }
   });
 }
