daf3e0aabc8fe609ba3f3715a018787a21a04027
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-09 20:45:12 -0400

    2024-10-10T00:45:11.719Z r ceb_bible_words_cache bible_words_language_cache

--- public/src/bible_words_language_cache.mjs
+++ public/src/bible_words_language_cache.mjs
@@ -1,5 +1,5 @@
 import { bible_words_language } from "./bible_words_language.mjs";
 import { function_cache } from "./function_cache.mjs";
-export async function ceb_bible_words_cache(args) {
+export async function bible_words_language_cache(args) {
   return await function_cache(bible_words_language, [args]);
 }
--- public/src/ceb_bible_words_count.mjs
+++ public/src/ceb_bible_words_count.mjs
@@ -1,7 +1,7 @@
+import { bible_words_language_cache } from "./bible_words_language_cache.mjs";
 import { bible_words_count_generic } from "./bible_words_count_generic.mjs";
-import { ceb_bible_words_cache } from "./ceb_bible_words_cache.mjs";
 export async function ceb_bible_words_count(args) {
-  let all = await ceb_bible_words_cache(args);
+  let all = await bible_words_language_cache(args);
   let symbols = "_-–—,;:!?.'‘’\"“”()[]}¶/`¯|€0123456789";
   return bible_words_count_generic(all, symbols);
 }
