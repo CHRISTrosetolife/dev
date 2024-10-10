131d23a8a2714f2837b3fe56b5c9686641833a64
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-09 20:32:49 -0400

    2024-10-10T00:32:49.362Z function_auto_after functions_rename_generic

--- public/src/functions_rename_generic.mjs
+++ public/src/functions_rename_generic.mjs
@@ -3,10 +3,11 @@ import { each_async } from "./each_async.mjs";
 import { function_names } from "./function_names.mjs";
 export async function functions_rename_generic(fn_new_get) {
   let fns = await function_names();
-  await each_async(fns, async (fn) => {
+  await each_async(fns, lambda);
+  async function lambda(fn) {
     let fn_new = await fn_new_get(fn);
     if (fn_new !== fn) {
       await function_rename(fn, fn_new);
     }
-  });
+  }
 }
