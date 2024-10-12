504a394fbd6ca6f4105cab2666497ebbee6c173d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-12 06:53:17 -0400

    2024-10-12T10:53:17.057Z function_auto_after app_good

--- public/src/app_good.mjs
+++ public/src/app_good.mjs
@@ -1,4 +1,5 @@
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_good() {
   let body = html_style_default_initialize();
+  html_button_width_full_click(body, "morning", () => {});
 }
