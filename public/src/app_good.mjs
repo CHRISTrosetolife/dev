5af7e8546563961602bfe1516c52d0bc28d2b30a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-12 06:53:26 -0400

    2024-10-12T10:53:26.099Z function_auto_after app_good

--- public/src/app_good.mjs
+++ public/src/app_good.mjs
@@ -1,5 +1,8 @@
+import { grace_good_morning } from "./grace_good_morning.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_good() {
   let body = html_style_default_initialize();
-  html_button_width_full_click(body, "morning", () => {});
+  html_button_width_full_click(body, "morning", () => {
+    grace_good_morning();
+  });
 }
