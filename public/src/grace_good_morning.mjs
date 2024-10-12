c24088786ccdfba907652a95fee13f5a3ac5775c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-12 06:42:02 -0400

    2024-10-12T10:42:01.295Z function_auto_after grace_good_morning

--- public/src/grace_good_morning.mjs
+++ public/src/grace_good_morning.mjs
@@ -14,6 +14,7 @@ export function grace_good_morning() {
   return list_join_newline(
     list_between(
       [
+        "Good morning love",
         range_map_join_empty(count, () => {
           return list_random_item(crosses);
         }),
