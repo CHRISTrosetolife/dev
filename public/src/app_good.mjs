08fe44684a1b36b3d72a89907a41a43d5383a020
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-12 06:55:00 -0400

    2024-10-12T10:54:59.802Z function_auto_after app_good

--- public/src/app_good.mjs
+++ public/src/app_good.mjs
@@ -1,9 +1,10 @@
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { clipboard_copy_web } from "./clipboard_copy_web.mjs";
 import { grace_good_morning } from "./grace_good_morning.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_good() {
   let body = html_style_default_initialize();
-  html_button_width_full_click(body, "morning", () => {
+  html_button_width_full_text_click(body, "morning", () => {
     let message = grace_good_morning();
     clipboard_copy_web(message);
   });
