1729fce19bf89488f03cb3156ea6e0b336baa4da
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-12 06:40:31 -0400

    2024-10-12T10:40:30.524Z function_auto_after grace_good_morning

---
+++ public/src/grace_good_morning.mjs
@@ -0,0 +1,17 @@
+import { range_map_join_empty } from "./range_map_join_empty.mjs";
+import { list_random_item } from "./list_random_item.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function grace_good_morning() {
+  let crosses = "✝☦";
+  let green = "💚";
+  let hugs = "🫂🤗";
+  let emojis = "💓♥️🤩💕💖💗💘💞🏩💝🫶🤟🏿💌💟🌷🌹❣️";
+  let faces = "😊😍😘😗😙😁🙂😀😇😇😇";
+  let colors = "❤️💙🧡🤍🤎🖤💚💛💜";
+  let family = "👫🫂👩🏾‍❤‍💋‍👨🏾💍👰🤵💋💒🏡🛏🤰👼👶🤱👨‍🍼🍼👨‍👩‍👦👨‍👩‍👧‍👦😇";
+  let count = 5;
+  let mapper = () => {
+    return list_random_item(crosses);
+  };
+  return string_combine_multiple([range_map_join_empty(count, mapper)]);
+}
