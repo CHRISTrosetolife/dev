3fc99817ac61ce94ee688beb3119060fd0266e30
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-12 07:12:38 -0400

    2024-10-12T11:12:38.253Z function_auto_after app_sleep_record

--- public/src/app_sleep_record.mjs
+++ public/src/app_sleep_record.mjs
@@ -1,3 +1,4 @@
+import { grace_good_generic_copy } from "./grace_good_generic_copy.mjs";
 import { app_sleep_save } from "./app_sleep_save.mjs";
 import { html_hr } from "./html_hr.mjs";
 import { app_sleep_today_download } from "./app_sleep_today_download.mjs";
@@ -9,6 +10,7 @@ export function app_sleep_record(context, button_text, list_name, message) {
   html_button_width_full_text_click(root, button_text, async () => {
     let { today, now, month_path, data } = await app_sleep_today_download();
     await app_sleep_save(today, list_name, now, month_path, data, result);
+    grace_good_generic_copy(message);
   });
   result = html_element(root, "pre");
   html_hr(root);
