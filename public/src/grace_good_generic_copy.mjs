73e109c0239e538d57e6e07a1a4ad642bd6e7e82
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-12 07:12:06 -0400

    2024-10-12T11:12:05.686Z function_auto_after grace_good_generic_copy

--- public/src/grace_good_generic_copy.mjs
+++ public/src/grace_good_generic_copy.mjs
@@ -1,6 +1,6 @@
 import { clipboard_copy_web } from "./clipboard_copy_web.mjs";
 import { grace_good_generic } from "./grace_good_generic.mjs";
-export function grace_good_generic_copy() {
-  let message = grace_good_generic(message);
-  clipboard_copy_web(message);
+export function grace_good_generic_copy(message) {
+  let m = grace_good_generic(message);
+  clipboard_copy_web(m);
 }
