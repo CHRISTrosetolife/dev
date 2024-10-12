92d773936114e9b27dce26e8235c18b534813447
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-12 06:50:30 -0400

    2024-10-12T10:50:29.692Z function_auto_after grace_good_morning

--- public/src/grace_good_morning.mjs
+++ public/src/grace_good_morning.mjs
@@ -1,3 +1,4 @@
+import { list_denest } from "./list_denest.mjs";
 import { list_shuffle } from "./list_shuffle.mjs";
 import { list_to } from "./list_to.mjs";
 import { list_size_randoms } from "./list_size_randoms.mjs";
@@ -14,18 +15,20 @@ export function grace_good_morning() {
   let family = "👫🫂👩🏾‍❤‍💋‍👨🏾💍👰🤵💋💒🏡🛏🤰👼👶🤱👨‍🍼🍼👨‍👩‍👦👨‍👩‍👧‍👦😇";
   let count = 5;
   let counts = [3, 5, 7];
-  return list_join_newline([
-    list_join_empty(
-      list_map([crosses, green, hugs], (items) =>
-        list_size_randoms(counts, items),
+  return list_join_newline(
+    list_denest([
+      list_join_empty(
+        list_map([crosses, green, hugs], (items) =>
+          list_size_randoms(counts, items),
+        ),
       ),
-    ),
-    "Good morning love 😇",
-    "I love you 💚",
-    list_map([emojis, faces, colors], (items) => {
-      let list = list_to(items);
-      list_shuffle(list);
-      return list_join_empty(list);
-    }),
-  ]);
+      "Good morning love 😇",
+      "I love you 💚",
+      list_map([emojis, faces, colors], (items) => {
+        let list = list_to(items);
+        list_shuffle(list);
+        return list_join_empty(list);
+      }),
+    ]),
+  );
 }
