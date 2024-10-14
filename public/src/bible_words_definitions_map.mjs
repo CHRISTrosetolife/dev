e7b921fb8b134e1da957c2dc68443faa9f74c188
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-14 16:31:59 -0400

    2024-10-14T20:31:59.185Z function_auto_after bible_words_definitions_map

--- public/src/bible_words_definitions_map.mjs
+++ public/src/bible_words_definitions_map.mjs
@@ -1,3 +1,4 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { bible_words_language_count_cache } from "./bible_words_language_count_cache.mjs";
 import { log } from "./log.mjs";
 import { each_index_async } from "./each_index_async.mjs";
@@ -7,6 +8,7 @@ import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { ceb_definition } from "./ceb_definition.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { list_add } from "./list_add.mjs";
+import { function_import } from "./function_import.mjs";
 export async function bible_words_definitions_map(language) {
   let words = await bible_words_language_count_cache({
     new: false,
@@ -14,6 +16,9 @@ export async function bible_words_definitions_map(language) {
   });
   let result = {};
   let size = list_size(words);
+  let definition_get = await function_import(
+    string_combine_multiple([language, "_definition"]),
+  );
   await each_index_async(words, async (word_language, index) => {
     if (index % 100 === 0) {
       log({
