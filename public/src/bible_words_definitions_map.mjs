0c2193665494d00eb698eef6248d93ec275bbce0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-14 16:28:59 -0400

    2024-10-14T20:28:59.191Z function_auto_after bible_words_definitions_map

---
+++ public/src/bible_words_definitions_map.mjs
@@ -0,0 +1,34 @@
+import { bible_words_language_count_cache } from "./bible_words_language_count_cache.mjs";
+import { log } from "./log.mjs";
+import { each_index_async } from "./each_index_async.mjs";
+import { list_size } from "./list_size.mjs";
+import { list_empty_not_is } from "./list_empty_not_is.mjs";
+import { object_property_exists_not } from "./object_property_exists_not.mjs";
+import { ceb_definition } from "./ceb_definition.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
+import { list_add } from "./list_add.mjs";
+export async function bible_words_definitions_map() {
+  let words_ceb = await bible_words_language_count_cache({
+    new: false,
+  });
+  let result = {};
+  let size = list_size(words_ceb);
+  await each_index_async(words_ceb, async (word_ceb, index) => {
+    if (index % 100 === 0) {
+      log({
+        size,
+        index,
+      });
+    }
+    let { word, definitions } = await ceb_definition(word_ceb.word);
+    if (object_property_exists_not(result, word)) {
+      if (list_empty_not_is(definitions)) {
+        if (word_ceb.word !== word) {
+          let words = object_property_initialize(result, word, []);
+          list_add(words, word_ceb.word);
+        }
+      }
+    }
+  });
+  return result;
+}
