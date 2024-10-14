bf0918090a244ead264948f987cfc71913bc47ed
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-14 16:11:31 -0400

    2024-10-14T20:11:31.211Z r ceb_bible_words_definitions_pairs bible_words_definitions_pairs

--- public/src/bible_words_definitions_pairs.mjs
+++ public/src/bible_words_definitions_pairs.mjs
@@ -9,7 +9,7 @@ import { object_property_exists } from "./object_property_exists.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { null_not_is } from "./null_not_is.mjs";
-export async function ceb_bible_words_definitions_pairs() {
+export async function bible_words_definitions_pairs() {
   let ceb_scores = await ceb_bible_words_score();
   let lookup = list_to_lookup_property(ceb_scores, "word");
   let pairs = await ceb_bible_words_definitions_pairs_compute();
