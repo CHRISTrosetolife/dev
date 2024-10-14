1084cfde93520c14b8fcad627a1bda1fc59047dd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-14 11:00:47 -0400

    2024-10-14T15:00:46.002Z r sermon_open written_open

--- public/src/written_open.mjs
+++ public/src/written_open.mjs
@@ -1,6 +1,6 @@
 import { file_open } from "./file_open.mjs";
 import { written_path } from "./written_path.mjs";
-export async function sermon_open(written_folder, name) {
+export async function written_open(written_folder, name) {
   let path_file = written_path(written_folder, name);
   await file_open(path_file);
 }
