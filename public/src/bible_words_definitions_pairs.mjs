c949394b4f3e61c8cee3b3244c5923fa01a80f46
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-14 16:15:35 -0400

    2024-10-14T20:15:35.424Z function_auto_after bible_words_definitions_pairs

--- public/src/bible_words_definitions_pairs.mjs
+++ public/src/bible_words_definitions_pairs.mjs
@@ -1,7 +1,7 @@
+import { bible_words_score } from "./bible_words_score.mjs";
 import { ceb_bible_words_definitions_pairs_compute } from "./ceb_bible_words_definitions_pairs_compute.mjs";
 import { list_to_lookup_property } from "./list_to_lookup_property.mjs";
 import { list_first } from "./list_first.mjs";
-import { ceb_bible_words_score } from "./ceb_bible_words_score.mjs";
 import { object_properties_new } from "./object_properties_new.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_map } from "./list_map.mjs";
@@ -10,7 +10,7 @@ import { object_property_set } from "./object_property_set.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { null_not_is } from "./null_not_is.mjs";
 export async function bible_words_definitions_pairs(language) {
-  let scores = await ceb_bible_words_score();
+  let scores = await bible_words_score();
   let lookup = list_to_lookup_property(scores, "word");
   let pairs = await ceb_bible_words_definitions_pairs_compute();
   let concise = list_map(pairs, (p) =>
