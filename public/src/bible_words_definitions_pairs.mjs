5c13c8feb438010062e9abc44cfcc7c5223d8fcf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-14 16:27:52 -0400

    2024-10-14T20:27:51.707Z function_auto_after bible_words_definitions_pairs

--- public/src/bible_words_definitions_pairs.mjs
+++ public/src/bible_words_definitions_pairs.mjs
@@ -1,6 +1,5 @@
 import { bible_words_definitions_pairs_compute_inner } from "./bible_words_definitions_pairs_compute_inner.mjs";
 import { bible_words_score_language } from "./bible_words_score_language.mjs";
-import { bible_words_definitions_pairs_compute } from "./bible_words_definitions_pairs_compute.mjs";
 import { list_to_lookup_property } from "./list_to_lookup_property.mjs";
 import { list_first } from "./list_first.mjs";
 import { object_properties_new } from "./object_properties_new.mjs";
@@ -12,9 +11,8 @@ import { list_filter } from "./list_filter.mjs";
 import { null_not_is } from "./null_not_is.mjs";
 export async function bible_words_definitions_pairs(language) {
   let scores = await bible_words_score_language(language);
-  await bible_words_definitions_pairs_compute_inner(scores_language);
   let lookup = list_to_lookup_property(scores, "word");
-  let pairs = await bible_words_definitions_pairs_compute(language);
+  let pairs = await bible_words_definitions_pairs_compute_inner(scores);
   let concise = list_map(pairs, (p) =>
     list_map(["ceb", "eng"], (language) =>
       object_property_get(p[language], "word"),
