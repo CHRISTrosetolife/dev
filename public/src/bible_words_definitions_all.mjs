7b427ab8cb1c9c84eb30a807700112c1f2ae0fe9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-14 16:37:11 -0400

    2024-10-14T20:37:10.444Z function_auto_after bible_words_definitions_all

--- public/src/bible_words_definitions_all.mjs
+++ public/src/bible_words_definitions_all.mjs
@@ -6,6 +6,6 @@ export async function bible_words_definitions_all(language) {
     new: false,
     language,
   });
-  let result = await bible_words_definitions_get(words, language);
+  let result = await bible_words_definitions_get(words, error());
   return result;
 }
