5285ab0f437bbad6d131177028fd0d9ceb2f0c65
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-14 16:35:42 -0400

    2024-10-14T20:35:41.172Z function_auto_after bible_words_definitions_all

--- public/src/bible_words_definitions_all.mjs
+++ public/src/bible_words_definitions_all.mjs
@@ -1,10 +1,10 @@
 import { bible_words_language_count_cache } from "./bible_words_language_count_cache.mjs";
 import { ceb_bible_words_definitions_get } from "./ceb_bible_words_definitions_get.mjs";
 export async function bible_words_definitions_all(language) {
-  let words_ceb = await bible_words_language_count_cache({
+  let words = await bible_words_language_count_cache({
     new: false,
     language,
   });
-  let result = await ceb_bible_words_definitions_get(words_ceb);
+  let result = await ceb_bible_words_definitions_get(words);
   return result;
 }
