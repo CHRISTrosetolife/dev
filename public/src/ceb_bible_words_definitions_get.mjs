18b5b61f6134db4f3178a2059737d8b67f121e04
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-14 16:35:31 -0400

    2024-10-14T20:35:28.427Z function_auto_after ceb_bible_words_definitions_get

--- public/src/ceb_bible_words_definitions_get.mjs
+++ public/src/ceb_bible_words_definitions_get.mjs
@@ -4,7 +4,6 @@ import { list_add } from "./list_add.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
-import { ceb_definition } from "./ceb_definition.mjs";
 export async function ceb_bible_words_definitions_get(list) {
   let existing = {};
   let result = [];
@@ -12,7 +11,7 @@ export async function ceb_bible_words_definitions_get(list) {
   await each_log_async(list, lambda);
   return result;
   async function lambda(item) {
-    let { word, definitions } = await ceb_definition(item.word);
+    let { word, definitions } = await definition_get(item.word);
     if (object_property_exists_not(existing, word)) {
       object_property_set(existing, word, true);
       if (list_empty_not_is(definitions)) {
