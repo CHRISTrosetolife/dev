4106ec21b369da5695b34c048fb1dd15cd9736a2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-15 19:34:29 -0400

    2024-10-15T23:34:28.134Z function_auto_after bible_audio_player_cebuano

--- public/src/bible_audio_player_cebuano.mjs
+++ public/src/bible_audio_player_cebuano.mjs
@@ -1,11 +1,6 @@
 import { bible_audio_player_language_copy } from "./bible_audio_player_language_copy.mjs";
 import { bible_audio_player_drive_replace } from "./bible_audio_player_drive_replace.mjs";
-import { bible_audio_player_generic_multiple } from "./bible_audio_player_generic_multiple.mjs";
-import { bible_audio_player_cebuano_path } from "./bible_audio_player_cebuano_path.mjs";
 export async function bible_audio_player_cebuano() {
-  let p = bible_audio_player_cebuano_path();
-  let folders = ["CEBRPVN1DA", "CEBRPVN2DA"];
-  await bible_audio_player_generic_multiple(p, folders);
   await bible_audio_player_drive_replace(lambda);
   async function lambda() {
     await bible_audio_player_language_copy("cebuano");
