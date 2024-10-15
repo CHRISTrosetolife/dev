38e87876948496b40766abbd4d4566cae539cbd6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-15 19:33:25 -0400

    2024-10-15T23:33:24.351Z function_auto_after bible_audio_player_cebuano

--- public/src/bible_audio_player_cebuano.mjs
+++ public/src/bible_audio_player_cebuano.mjs
@@ -1,8 +1,3 @@
-import { file_copy_closed } from "./file_copy_closed.mjs";
-import { file_copy_path } from "./file_copy_path.mjs";
-import { bible_hebrew_audio_download } from "./bible_hebrew_audio_download.mjs";
-import { each_async } from "./each_async.mjs";
-import { bible_audio_player_output_path } from "./bible_audio_player_output_path.mjs";
 import { bible_audio_player_language } from "./bible_audio_player_language.mjs";
 import { bible_audio_player_drive_replace } from "./bible_audio_player_drive_replace.mjs";
 import { bible_audio_player_generic_multiple } from "./bible_audio_player_generic_multiple.mjs";
@@ -14,13 +9,5 @@ export async function bible_audio_player_cebuano() {
   await bible_audio_player_drive_replace(lambda);
   async function lambda() {
     await bible_audio_player_language("cebuano");
-    let output = bible_audio_player_output_path();
-    await each_async(
-      await bible_hebrew_audio_download(),
-      async (hebrew_path) => {
-        let file_path_new = file_copy_path(hebrew_path, output);
-        await file_copy_closed(hebrew_path, file_path_new);
-      },
-    );
   }
 }
