b3542d10d3edb186f6e2872c5e00460e3a11231d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-15 19:14:25 -0400

    2024-10-15T23:14:23.999Z function_auto_after bible_audio_player_original

--- public/src/bible_audio_player_original.mjs
+++ public/src/bible_audio_player_original.mjs
@@ -4,7 +4,7 @@ import { bible_audio_player_generic_multiple } from "./bible_audio_player_generi
 export async function bible_audio_player_original() {
   let name = "greek";
   let p = bible_audio_player_path(name);
-  return await folder_read_shallow_names(p, '');
+  return await folder_read_shallow_names(p, "");
   let folders = ["CEBRPVN1DA", "CEBRPVN2DA"];
   await bible_audio_player_generic_multiple(p, folders);
 }
