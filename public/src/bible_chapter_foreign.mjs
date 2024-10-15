b48569df7eac6ab7d962ca9355a2e4671cc192bb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-15 15:37:28 -0400

    2024-10-15T19:37:27.606Z function_auto_after bible_chapter_foreign

--- public/src/bible_chapter_foreign.mjs
+++ public/src/bible_chapter_foreign.mjs
@@ -14,6 +14,10 @@ import { each } from "./each.mjs";
 import { each_object } from "./each_object.mjs";
 import { bible_engbsb_chapter } from "./bible_engbsb_chapter.mjs";
 export async function bible_chapter_foreign(bible_folder, chapter_name) {
+  log({
+    bible_folder,
+    chapter_name,
+  });
   let eng = await bible_engbsb_chapter(chapter_name);
   let missing_ceb = {
     MAT17: ["21"],
