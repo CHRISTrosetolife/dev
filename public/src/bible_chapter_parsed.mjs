2f2412c70519eb9afe43ac42dd9d92016b47ea3e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-15 15:12:29 -0400

    2024-10-15T19:12:27.225Z function_auto_after bible_chapter_parsed

--- public/src/bible_chapter_parsed.mjs
+++ public/src/bible_chapter_parsed.mjs
@@ -4,12 +4,13 @@ import { html_parse_visit_attribute_value } from "./html_parse_visit_attribute_v
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { list_remove } from "./list_remove.mjs";
 export async function bible_chapter_parsed(bible_folder, chapter_name) {
+  let root;
   let download_without_using_zip = false;
   if (download_without_using_zip) {
     let joined = bible_url_htm(bible_folder, chapter_name);
-    let root = await html_cache_parse(joined);
+    root = await html_cache_parse(joined);
   }
-  let root = await bible_file_html_parse(bible_folder, chapter_name);
+  root = await bible_file_html_parse(bible_folder, chapter_name);
   html_parse_visit_attribute_value(root, "class", "notemark", (v) => {
     let { node } = v;
     let { parent } = node;
