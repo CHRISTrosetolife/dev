512a2bd94ccf7a005ce185dcf638f5ffde840c96
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-15 15:28:39 -0400

    2024-10-15T19:28:38.565Z function_auto_after bible_words_foreign_book

--- public/src/bible_words_foreign_book.mjs
+++ public/src/bible_words_foreign_book.mjs
@@ -7,7 +7,7 @@ export async function bible_words_foreign_book(book_name, bible_folder, la) {
   log({
     book_name,
   });
-  await list_adder_async(async (la) => {
+  return await list_adder_async(async (la) => {
     await bible_chapters_each("engbsb", book_name, async (chapter_name) => {
       let r = await bible_chapter_foreign(chapter_name, bible_folder);
       let { foreign } = r;
