6092f13c6541e6171431aff901a9aa7a56a5fd48
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-15 19:33:54 -0400

    2024-10-15T23:33:52.497Z function_auto_after bible_audio_player_output_path

--- public/src/bible_audio_player_output_path.mjs
+++ public/src/bible_audio_player_output_path.mjs
@@ -1,6 +1,10 @@
+import { bible_audio_player_drive_letter } from "./bible_audio_player_drive_letter.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function bible_audio_player_output_path() {
-  let output = string_combine_multiple([":\\"]);
+  let output = string_combine_multiple([
+    bible_audio_player_drive_letter(),
+    ":\\",
+  ]);
   if (0) {
     output = "..\\bible\\english\\test";
   }
