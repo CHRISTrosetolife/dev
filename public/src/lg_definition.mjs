c52c6b5e94bf1eb916e0cd7875ee4292e5cc65e3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-15 15:53:54 -0400

    2024-10-15T19:53:52.644Z function_auto_after lg_definition

--- public/src/lg_definition.mjs
+++ public/src/lg_definition.mjs
@@ -1,10 +1,10 @@
+import { lg_definitions_cache } from "./lg_definitions_cache.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
-import { lg_definitions } from "./lg_definitions.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function lg_definition(word) {
   assert_arguments_length(arguments, 1);
-  let dictionary = await lg_definitions();
+  let dictionary = await lg_definitions_cache();
   let definitions;
   if (object_property_exists(dictionary, word)) {
     definitions = object_property_get(dictionary, word);
