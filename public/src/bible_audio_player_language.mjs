a4f559310f51ccbb4530041c926d8d8c532ec42b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-15 19:32:04 -0400

    2024-10-15T23:32:03.577Z function_auto_after bible_audio_player_original

---
+++ public/src/bible_audio_player_language.mjs
@@ -0,0 +1,8 @@
+import { bible_audio_player_generic_multiple } from "./bible_audio_player_generic_multiple.mjs";
+import { folder_read_shallow_names } from "./folder_read_shallow_names.mjs";
+import { bible_audio_player_path } from "./bible_audio_player_path.mjs";
+export async function bible_audio_player_language(language_folder_name) {
+  let p = bible_audio_player_path(language_folder_name);
+  let folders = await folder_read_shallow_names(p, "");
+  await bible_audio_player_generic_multiple(p, folders);
+}
--- public/src/bible_audio_player_original.mjs
+++ public/src/bible_audio_player_original.mjs
@@ -1,3 +1,4 @@
+import { bible_audio_player_language } from "./bible_audio_player_language.mjs";
 import { bible_hebrew_audio_download } from "./bible_hebrew_audio_download.mjs";
 import { drive_format } from "./drive_format.mjs";
 import { bible_audio_player_drive_letter } from "./bible_audio_player_drive_letter.mjs";
@@ -6,16 +7,11 @@ import { each_async } from "./each_async.mjs";
 import { bible_audio_player_output_path } from "./bible_audio_player_output_path.mjs";
 import { file_copy_closed } from "./file_copy_closed.mjs";
 import { file_copy_path } from "./file_copy_path.mjs";
-import { folder_read_shallow_names } from "./folder_read_shallow_names.mjs";
-import { bible_audio_player_path } from "./bible_audio_player_path.mjs";
-import { bible_audio_player_generic_multiple } from "./bible_audio_player_generic_multiple.mjs";
 export async function bible_audio_player_original() {
   let drive_letter = bible_audio_player_drive_letter();
   await drive_format(drive_letter);
   let name = "greek";
-  let p = bible_audio_player_path(name);
-  let folders = await folder_read_shallow_names(p, "");
-  await bible_audio_player_generic_multiple(p, folders);
+  await bible_audio_player_language(name);
   let output = bible_audio_player_output_path();
   await each_async(await bible_hebrew_audio_download(), async (hebrew_path) => {
     let file_path_new = file_copy_path(hebrew_path, output);
