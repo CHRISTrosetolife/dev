c848be65bbd074abdfaf2e5ca3de47163df54ddf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-15 19:22:51 -0400

    2024-10-15T23:22:50.531Z function_auto_after bible_audio_player_original

--- public/src/bible_audio_player_original.mjs
+++ public/src/bible_audio_player_original.mjs
@@ -1,3 +1,4 @@
+import { bible_hebrew_audio_download } from "./bible_hebrew_audio_download.mjs";
 import { drive_format } from "./drive_format.mjs";
 import { bible_audio_player_drive_letter } from "./bible_audio_player_drive_letter.mjs";
 import { drive_remove } from "./drive_remove.mjs";
@@ -16,12 +17,9 @@ export async function bible_audio_player_original() {
   let folders = await folder_read_shallow_names(p, "");
   await bible_audio_player_generic_multiple(p, folders);
   let output = bible_audio_player_output_path();
-  await each_async(
-    sandbox_bible_hebrew_audio_download(),
-    async (hebrew_path) => {
-      let file_path_new = file_copy_path(hebrew_path, output);
-      await file_copy_closed(hebrew_path, file_path_new);
-    },
-  );
+  await each_async(await bible_hebrew_audio_download(), async (hebrew_path) => {
+    let file_path_new = file_copy_path(hebrew_path, output);
+    await file_copy_closed(hebrew_path, file_path_new);
+  });
   await drive_remove(drive_letter);
 }
