e0820139aa75c82f6e2915de807c5030c247dc55
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-15 19:11:34 -0400

    2024-10-15T23:11:32.905Z function_auto_after bible_audio_player_cebuano

--- public/src/bible_audio_player_cebuano.mjs
+++ public/src/bible_audio_player_cebuano.mjs
@@ -5,8 +5,8 @@ import { each_async } from "./each_async.mjs";
 import { tautology } from "./tautology.mjs";
 export async function bible_audio_player_cebuano() {
   let p = bible_audio_player_cebuano_path();
-  await folder_unzip(p);
   let folders = ["CEBRPVN1DA", "CEBRPVN2DA"];
+  await folder_unzip(p);
   await each_async(folders, async (download_folder) => {
     await bible_audio_player_generic(p, download_folder, tautology);
   });
