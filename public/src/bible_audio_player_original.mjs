65708bf0338b6a3543ebdee9c5a885cfea5deb23
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-15 19:18:57 -0400

    2024-10-15T23:18:56.890Z function_auto_after bible_audio_player_original

--- public/src/bible_audio_player_original.mjs
+++ public/src/bible_audio_player_original.mjs
@@ -1,3 +1,5 @@
+import { file_copy_closed } from "./file_copy_closed.mjs";
+import { file_copy_path } from "./file_copy_path.mjs";
 import { folder_read_shallow_names } from "./folder_read_shallow_names.mjs";
 import { bible_audio_player_path } from "./bible_audio_player_path.mjs";
 import { bible_audio_player_generic_multiple } from "./bible_audio_player_generic_multiple.mjs";
@@ -6,4 +8,6 @@ export async function bible_audio_player_original() {
   let p = bible_audio_player_path(name);
   let folders = await folder_read_shallow_names(p, "");
   await bible_audio_player_generic_multiple(p, folders);
+  let file_path_new = file_copy_path(file_path, output_directory);
+  await file_copy_closed(from, to_mapped);
 }
