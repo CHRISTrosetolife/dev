026cb9c952b6fc9b4ad950c2263b62c5fe59b0f4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-15 20:38:45 -0400

    2024-10-16T00:38:44.671Z function_auto_after bible_words_definitions_all

--- public/src/bible_words_definitions_all.mjs
+++ public/src/bible_words_definitions_all.mjs
@@ -1,10 +1,7 @@
+import { bible_words_language_count_cache_all } from "./bible_words_language_count_cache_all.mjs";
 import { bible_words_definitions_get } from "./bible_words_definitions_get.mjs";
-import { bible_words_language_count_cache } from "./bible_words_language_count_cache.mjs";
 export async function bible_words_definitions_all(language) {
-  let words = await bible_words_language_count_cache({
-    new: false,
-    language,
-  });
+  let words = await bible_words_language_count_cache_all(language);
   let result = await bible_words_definitions_get(language, words);
   return result;
 }
