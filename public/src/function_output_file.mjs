6ab8f21e1682c2f7f344e37a86a8d1599efe4750
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-16 11:50:55 -0400

    2024-10-16T15:50:54.100Z function_auto_after function_output_file

--- public/src/function_output_file.mjs
+++ public/src/function_output_file.mjs
@@ -4,8 +4,8 @@ import { json_format_to } from "./json_format_to.mjs";
 import { string_is } from "./string_is.mjs";
 import { function_run } from "./function_run.mjs";
 export async function function_output_file(function_name) {
-  let args = [];
-  let args_list = string_split_comma("");
+  let args = "";
+  let args_list = string_split_comma(args);
   let result = await function_run(function_name, args);
   if (!string_is(result)) {
     result = json_format_to(result);
