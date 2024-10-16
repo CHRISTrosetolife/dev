8400647566e4688e091e88522223a2eab27f6738
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-16 12:02:51 -0400

    2024-10-16T16:02:51.510Z function_auto_after function_output_file_args_expression

--- public/src/function_output_file_args_expression.mjs
+++ public/src/function_output_file_args_expression.mjs
@@ -1,4 +1,4 @@
 import { string_split_comma } from "./string_split_comma.mjs";
 export function function_output_file_args_expression(function_name, args) {
-  string_split_comma(args);
+  let split = string_split_comma(args);
 }
