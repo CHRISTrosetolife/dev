0f447242c7968c3046be6ba1be31175dda5c1217
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-16 11:51:39 -0400

    2024-10-16T15:51:38.800Z function_auto_after function_output_file

--- public/src/function_output_file.mjs
+++ public/src/function_output_file.mjs
@@ -1,21 +1,5 @@
-import { string_split_comma } from "./string_split_comma.mjs";
-import { file_overwrite_gitignore } from "./file_overwrite_gitignore.mjs";
-import { json_format_to } from "./json_format_to.mjs";
-import { string_is } from "./string_is.mjs";
-import { function_run } from "./function_run.mjs";
+import { function_output_file_args } from "./function_output_file_args.mjs";
 export async function function_output_file(function_name) {
   let args = "";
-  let args_list;
-  if (args === "") {
-    args_list = [];
-  } else {
-    args_list = string_split_comma(args);
-  }
-  let result = await function_run(function_name, args);
-  if (!string_is(result)) {
-    result = json_format_to(result);
-  }
-  let fn = function_output_file;
-  let output_path = await file_overwrite_gitignore(function_name, fn, result);
-  return output_path;
+  return await function_output_file_args(args, function_name);
 }
---
+++ public/src/function_output_file_args.mjs
@@ -0,0 +1,21 @@
+import { file_overwrite_gitignore } from "./file_overwrite_gitignore.mjs";
+import { function_output_file } from "./function_output_file.mjs";
+import { json_format_to } from "./json_format_to.mjs";
+import { string_is } from "./string_is.mjs";
+import { function_run } from "./function_run.mjs";
+import { string_split_comma } from "./string_split_comma.mjs";
+export async function function_output_file_args(args, function_name) {
+  let args_list;
+  if (args === "") {
+    args_list = [];
+  } else {
+    args_list = string_split_comma(args);
+  }
+  let result = await function_run(function_name, args);
+  if (!string_is(result)) {
+    result = json_format_to(result);
+  }
+  let fn = function_output_file;
+  let output_path = await file_overwrite_gitignore(function_name, fn, result);
+  return output_path;
+}
