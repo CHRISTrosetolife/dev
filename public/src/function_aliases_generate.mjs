85a1d6b7d7258adfa3ebce536851ca0c50cd1454
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-16 11:52:18 -0400

    2024-10-16T15:52:17.369Z function_auto_after function_aliases_generate

--- public/src/function_aliases_generate.mjs
+++ public/src/function_aliases_generate.mjs
@@ -1,3 +1,4 @@
+import { function_output_file_args } from "./function_output_file_args.mjs";
 import { sermon_references_combine } from "./sermon_references_combine.mjs";
 import { bible_search_book } from "./bible_search_book.mjs";
 import { bible_chapter_text } from "./bible_chapter_text.mjs";
@@ -114,6 +115,7 @@ export async function function_aliases_generate() {
   alias_add(function_open_app, ["oa"]);
   alias_add(function_open_app_html, ["oh"]);
   alias_add(function_output_file, ["of"]);
+  alias_add(function_output_file_args, ["ofa"]);
   alias_add(function_param_new, ["p"]);
   alias_add(function_param_new_at, ["pn"]);
   alias_add(function_param_move_beginning, ["pb"]);
