388fce07c81517754277a2dee5ed249f9ecd3d7b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-16 12:02:45 -0400

    2024-10-16T16:02:44.247Z function_auto_after function_output_file_args_expression

--- public/src/function_output_file_args_expression.mjs
+++ public/src/function_output_file_args_expression.mjs
@@ -1,1 +1,4 @@
-export function function_output_file_args_expression(function_name, args) {}
+import { string_split_comma } from "./string_split_comma.mjs";
+export function function_output_file_args_expression(function_name, args) {
+  string_split_comma(args);
+}
