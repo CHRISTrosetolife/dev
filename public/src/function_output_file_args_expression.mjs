d3db6bf2aafcd8c6178bd159be5d8af1d60e203a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-16 12:12:16 -0400

    2024-10-16T16:12:16.151Z function_auto_after function_output_file_args_expression

--- public/src/function_output_file_args_expression.mjs
+++ public/src/function_output_file_args_expression.mjs
@@ -1,10 +1,14 @@
+import { function_output_file_args } from "./function_output_file_args.mjs";
 import { js_code_statement_return } from "./js_code_statement_return.mjs";
 import { list_map } from "./list_map.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
-export function function_output_file_args_expression(function_name, args) {
+export async function function_output_file_args_expression(
+  function_name,
+  args,
+) {
   let split = string_split_comma(args);
   let mapped = list_map(split, (s) =>
     new Function(js_code_statement_return(s))(),
   );
-  return mapped;
+  return await function_output_file_args(function_name, mapped);
 }
