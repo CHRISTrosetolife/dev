1fb230d6e609210caa2111dcaef37bebecb266cc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-16 12:10:48 -0400

    2024-10-16T16:10:47.066Z function_auto_after function_output_file_args_expression

--- public/src/function_output_file_args_expression.mjs
+++ public/src/function_output_file_args_expression.mjs
@@ -1,9 +1,12 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { log } from "./log.mjs";
 import { list_map } from "./list_map.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 export function function_output_file_args_expression(function_name, args) {
   let split = string_split_comma(args);
-  let mapped = list_map(split, (s) => eval("()"));
+  let mapped = list_map(split, (s) =>
+    eval(string_combine_multiple(["(", ")"])),
+  );
   let a = eval("({})");
   log({
     a,
