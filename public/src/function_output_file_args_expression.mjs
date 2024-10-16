224bdd3c76a6693fb784010d867db3124d94937e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-16 12:09:21 -0400

    2024-10-16T16:09:20.254Z function_output_file_args_expression a {},{a:1}

--- public/src/function_output_file_args_expression.mjs
+++ public/src/function_output_file_args_expression.mjs
@@ -4,7 +4,7 @@ import { string_split_comma } from "./string_split_comma.mjs";
 export function function_output_file_args_expression(function_name, args) {
   let split = string_split_comma(args);
   let mapped = list_map(split, eval);
-  let a = eval("{}");
+  let a = eval("({})");
   log({
     a,
   });
