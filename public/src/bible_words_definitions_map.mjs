ca54331eff68d3f200e780b8160a569816d7f767
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-15 20:36:49 -0400

    2024-10-16T00:36:47.652Z function_auto_after bible_words_definitions_map

--- public/src/bible_words_definitions_map.mjs
+++ public/src/bible_words_definitions_map.mjs
@@ -1,5 +1,5 @@
+import { bible_words_language_count_cache_all } from "./bible_words_language_count_cache_all.mjs";
 import { function_definition } from "./function_definition.mjs";
-import { bible_words_language_count_cache } from "./bible_words_language_count_cache.mjs";
 import { log } from "./log.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { list_size } from "./list_size.mjs";
@@ -8,10 +8,7 @@ import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { list_add } from "./list_add.mjs";
 export async function bible_words_definitions_map(language) {
-  let words = await bible_words_language_count_cache({
-    new: false,
-    language,
-  });
+  let words = await bible_words_language_count_cache_all(language);
   let result = {};
   let size = list_size(words);
   let definition_get = await function_definition(language);
