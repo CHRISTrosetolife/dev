8573a1ec3c517a881f225d89ce79187de81fe4ff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-16 12:10:52 -0400

    2024-10-16T16:10:51.806Z function_auto_after function_output_file_args_expression

--- public/src/function_output_file_args_expression.mjs
+++ public/src/function_output_file_args_expression.mjs
@@ -5,7 +5,7 @@ import { string_split_comma } from "./string_split_comma.mjs";
 export function function_output_file_args_expression(function_name, args) {
   let split = string_split_comma(args);
   let mapped = list_map(split, (s) =>
-    eval(string_combine_multiple(["(", ")"])),
+    eval(string_combine_multiple(["(", s, ")"])),
   );
   let a = eval("({})");
   log({
