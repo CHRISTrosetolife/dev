696a336fc1ed5a161798624035d67c13abb0dba2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-17 20:37:59 -0400

    2024-10-18T00:37:59.003Z function_auto_after sandbox_2

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -3,14 +3,16 @@ import { app_gs_bible_chapter_name } from "./app_gs_bible_chapter_name.mjs";
 import { each_async } from "./each_async.mjs";
 import { bible_chapter_video } from "./bible_chapter_video.mjs";
 export async function sandbox_2() {
-  let book_id = "JER";
-  let low = 18;
-  let high = 52;
   let as = [
     {
       book_id: "JER",
       low: 18,
       high: 52,
+    },
+    {
+      book_id: "LAM",
+      low: 1,
+      high: 5,
     },
   ];
   await each_async(as, async (a) => {
