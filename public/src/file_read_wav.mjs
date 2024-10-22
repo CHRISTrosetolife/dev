f2f5dc637879a63c62abd6d711ed7e5c9858fcfc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-22 10:17:27 -0400

    2024-10-22T14:17:25.754Z function_auto_after file_read_wav

--- public/src/file_read_wav.mjs
+++ public/src/file_read_wav.mjs
@@ -1,9 +1,13 @@
+import { log } from "./log.mjs";
 import { list_to } from "./list_to.mjs";
 import { file_read_binary } from "./file_read_binary.mjs";
 import { import_node } from "./import_node.mjs";
 export async function file_read_wav(input_path) {
   let wavefile = await import_node("wavefile");
   let { default: WaveFile } = wavefile;
+  log({
+    WaveFile,
+  });
   let w = new WaveFile(await file_read_binary(input_path));
   let { fmt } = w;
   let samples = list_to(w.getSamples());
