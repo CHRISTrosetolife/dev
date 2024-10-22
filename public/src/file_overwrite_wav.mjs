2269c214f24c405a1a4d7509f3ffeee6d2fdbf74
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-22 15:18:36 -0400

    2024-10-22T19:18:34.641Z function_auto_after file_overwrite_wav

--- public/src/file_overwrite_wav.mjs
+++ public/src/file_overwrite_wav.mjs
@@ -1,8 +1,10 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { import_node } from "./import_node.mjs";
 import { file_overwrite_binary } from "./file_overwrite_binary.mjs";
 export async function file_overwrite_wav(fmt, samples, path_output) {
   let wavefile = await import_node("wavefile");
-  let { WaveFile } = wavefile;
+  let d = object_property_get(wavefile, "default");
+  let { WaveFile } = d;
   let o = new WaveFile();
   o.fromScratch(fmt.numChannels, fmt.sampleRate, fmt.bitsPerSample, samples);
   await file_overwrite_binary(path_output, o.toBuffer());
