71407ac03908a3bf1594aa96224fda765e98f01f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-23 09:57:17 -0400

    2024-10-23T13:57:13.403Z function_auto_after bible_audio_player_original_greek

---
+++ public/src/bible_audio_player_language_copy_greek.mjs
@@ -0,0 +1,4 @@
+import { bible_audio_player_language_copy } from "./bible_audio_player_language_copy.mjs";
+export async function bible_audio_player_language_copy_greek() {
+  return await bible_audio_player_language_copy("greek");
+}
