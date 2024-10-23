14dcefde32d883dbbacc136914562f99886740e2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-23 09:57:59 -0400

    2024-10-23T13:57:58.180Z function_auto_after bible_audio_player_original_hebrew

--- public/src/bible_audio_player_original_hebrew.mjs
+++ public/src/bible_audio_player_original_hebrew.mjs
@@ -1,19 +1,7 @@
+import { bible_audio_player_language_copy_hebrew } from "./bible_audio_player_language_copy_hebrew.mjs";
 import { bible_audio_player_drive_replace } from "./bible_audio_player_drive_replace.mjs";
-import { bible_hebrew_audio_download } from "./bible_hebrew_audio_download.mjs";
-import { each_async } from "./each_async.mjs";
-import { bible_audio_player_output_path } from "./bible_audio_player_output_path.mjs";
-import { file_copy_closed } from "./file_copy_closed.mjs";
-import { file_copy_path } from "./file_copy_path.mjs";
 export async function bible_audio_player_original_hebrew() {
-  await bible_audio_player_drive_replace(lambda);
-  async function lambda() {
-    let output = bible_audio_player_output_path();
-    await each_async(
-      await bible_hebrew_audio_download(),
-      async (hebrew_path) => {
-        let file_path_new = file_copy_path(hebrew_path, output);
-        await file_copy_closed(hebrew_path, file_path_new);
-      },
-    );
-  }
+  await bible_audio_player_drive_replace(
+    bible_audio_player_language_copy_hebrew,
+  );
 }
