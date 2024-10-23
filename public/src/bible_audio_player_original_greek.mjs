18ba6f2f4997ecf3561e2265bb2e4cb2f4916563
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-22 21:43:53 -0400

    2024-10-23T01:43:51.361Z function_auto_after bible_audio_player_original_greek

--- public/src/bible_audio_player_original_greek.mjs
+++ public/src/bible_audio_player_original_greek.mjs
@@ -1,21 +1,8 @@
 import { bible_audio_player_language_copy } from "./bible_audio_player_language_copy.mjs";
 import { bible_audio_player_drive_replace } from "./bible_audio_player_drive_replace.mjs";
-import { bible_hebrew_audio_download } from "./bible_hebrew_audio_download.mjs";
-import { each_async } from "./each_async.mjs";
-import { bible_audio_player_output_path } from "./bible_audio_player_output_path.mjs";
-import { file_copy_closed } from "./file_copy_closed.mjs";
-import { file_copy_path } from "./file_copy_path.mjs";
 export async function bible_audio_player_original_greek() {
   await bible_audio_player_drive_replace(lambda);
   async function lambda() {
     await bible_audio_player_language_copy("greek");
-    let output = bible_audio_player_output_path();
-    await each_async(
-      await bible_hebrew_audio_download(),
-      async (hebrew_path) => {
-        let file_path_new = file_copy_path(hebrew_path, output);
-        await file_copy_closed(hebrew_path, file_path_new);
-      },
-    );
   }
 }
