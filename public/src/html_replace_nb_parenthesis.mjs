7e776721e8df6b01566e35be0770d65665a737bb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-24 11:26:20 -0400

    2024-10-24T15:26:18.819Z function_auto_after html_replace_nb_parenthesis

--- public/src/html_replace_nb_parenthesis.mjs
+++ public/src/html_replace_nb_parenthesis.mjs
@@ -1,7 +1,5 @@
 import { string_replace_nb } from "./string_replace_nb.mjs";
-import { string_replace } from "./string_replace.mjs";
 export function html_replace_nb_parenthesis(s) {
   s = string_replace_nb(s, " ");
-  s = string_replace(s, "-", "&#8209;");
   return s;
 }
