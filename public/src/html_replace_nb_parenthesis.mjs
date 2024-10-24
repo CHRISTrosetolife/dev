398691a4ad2a204a2add4e0559a6637c1305571c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-24 11:26:39 -0400

    2024-10-24T15:26:38.215Z function_auto_after html_replace_nb_parenthesis

--- public/src/html_replace_nb_parenthesis.mjs
+++ public/src/html_replace_nb_parenthesis.mjs
@@ -1,7 +1,7 @@
 import { each } from "./each.mjs";
 import { string_replace_nb } from "./string_replace_nb.mjs";
 export function html_replace_nb_parenthesis(s) {
-  each(list, (item) => {});
+  each(["(", ")"], (item) => {});
   s = string_replace_nb(s, " ");
   return s;
 }
