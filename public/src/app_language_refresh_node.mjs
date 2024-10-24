77cef7539e3e6b8c40b7f0d9f76882def64fcc60
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-24 12:21:07 -0400

    2024-10-24T16:21:05.690Z function_auto_after app_language_refresh_node

--- public/src/app_language_refresh_node.mjs
+++ public/src/app_language_refresh_node.mjs
@@ -155,7 +155,7 @@ export async function app_language_refresh_node(context) {
         group_pair_count,
         " words.",
       ]),
-      "After you complete a second group or beyond, it is recommended to review any previous groups.",
+      "After you complete a second group or beyond, it is recommended to review previous groups.",
       string_combine_multiple([
         "One way to go to a previous group is to choose: ",
         await app_language_text_group_previous(language_fluent),
