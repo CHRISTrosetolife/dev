c1070412892cf028708e58bfb3426d944d3b0b96
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-24 12:13:22 -0400

    2024-10-24T16:13:21.102Z function_auto_after app_language_refresh_node

--- public/src/app_language_refresh_node.mjs
+++ public/src/app_language_refresh_node.mjs
@@ -164,7 +164,7 @@ export async function app_language_refresh_node(context) {
         "Another way to go to a previous group is to keep choosing: ",
         html_button_up_text(),
       ]),
-      "Then you can choose from all groups",
+      "Then you can choose any group you want",
     ];
     each(paragraphs, (p) => {
       html_p_text(root, p);
