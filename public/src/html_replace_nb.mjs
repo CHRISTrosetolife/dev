3c4de830fbafd3186d3dc56e8e16bfb5baa3a8e0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-24 11:25:19 -0400

    2024-10-24T15:25:16.862Z function_auto_after html_replace_nb

--- public/src/html_replace_nb.mjs
+++ public/src/html_replace_nb.mjs
@@ -1,6 +1,7 @@
 import { string_replace } from "./string_replace.mjs";
 export function html_replace_nb(s) {
-  s = string_replace(s, " ", "&nbsp;");
+  let before = " ";
+  s = string_replace(s, before, "&nbsp;");
   s = string_replace(s, "-", "&#8209;");
   return s;
 }
