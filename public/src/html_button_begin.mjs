90e57f908d5364743d61f3631b35c60bf7526910
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-23 22:22:55 -0400

    2024-10-24T02:22:54.145Z function_auto_after html_button_begin

--- public/src/html_button_begin.mjs
+++ public/src/html_button_begin.mjs
@@ -1,13 +1,14 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_style_default_font_size_value } from "./html_style_default_font_size_value.mjs";
 import { html_style_bold } from "./html_style_bold.mjs";
 import { html_style_font_size } from "./html_style_font_size.mjs";
 import { multiply } from "./multiply.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
-export function html_button_begin(root, text, text, begin_onclick) {
+export function html_button_begin(root, text, begin_onclick) {
   let default_font_size = html_style_default_font_size_value();
   let begin = html_button_width_full_text_click(
     root,
-    "🚀 begin",
+    string_combine_multiple(["🚀 begin"]),
     begin_onclick,
   );
   let font_size_bigger = multiply(default_font_size, 1.2);
