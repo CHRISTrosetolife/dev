2ff855780e17e20bc4c52f89dc6f4229be149721
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 19:25:34 -0400

    2024-10-26T23:25:32.996Z function_auto_after app_bible

--- public/src/app_bible.mjs
+++ public/src/app_bible.mjs
@@ -1,4 +1,4 @@
-import { html_document_body } from "./html_document_body.mjs";
+import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_bible() {
-  let root = html_document_body();
+  let body = html_style_default_initialize();
 }
