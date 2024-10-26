d4d2dbf195e7ec614ecafbfb5e57d38c6be2218e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 19:35:01 -0400

    2024-10-26T23:35:00.998Z function_auto_after bible_interlinear_each_chapter

--- public/src/bible_interlinear_each_chapter.mjs
+++ public/src/bible_interlinear_each_chapter.mjs
@@ -1,8 +1,9 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
 export function bible_interlinear_each_chapter(books, chapter_each) {
   each(books, (book) => {
     each(book.chapters, (chapter) => {
-      chapter_each(chapter, book);
+      chapter_each(chapter, object_property_get(object, "property_name"));
     });
   });
 }
