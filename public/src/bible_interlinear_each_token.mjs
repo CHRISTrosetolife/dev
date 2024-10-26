6c675a6b43f1bb2b17bb219b3a398229b002713a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 19:33:23 -0400

    2024-10-26T23:33:23.039Z function_auto_after bible_interlinear_each_token

--- public/src/bible_interlinear_each_token.mjs
+++ public/src/bible_interlinear_each_token.mjs
@@ -2,7 +2,7 @@ import { bible_interlinear_each_chapter } from "./bible_interlinear_each_chapter
 import { each } from "./each.mjs";
 export function bible_interlinear_each_token(books, lambda) {
   bible_interlinear_each_chapter(books, chapter_each);
-  function chapter_each(chapter) {
+  function chapter_each(chapter, book) {
     each(chapter.verses, (verse) => {
       each(verse.tokens, (token) => {
         lambda(token);
