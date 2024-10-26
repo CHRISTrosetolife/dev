707b9895eebd99f6afe34f8a69eff715d1d5d351
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 19:28:15 -0400

    2024-10-26T23:28:15.110Z function_auto_after bible_interlinear_each_token

--- public/src/bible_interlinear_each_token.mjs
+++ public/src/bible_interlinear_each_token.mjs
@@ -2,11 +2,14 @@ import { each } from "./each.mjs";
 export function bible_interlinear_each_token(new_testament_books, lambda) {
   each(new_testament_books, (book) => {
     each(book.chapters, (chapter) => {
-      each(chapter.verses, (verse) => {
-        each(verse.tokens, (token) => {
-          lambda(token);
-        });
-      });
+      chapter_each(chapter);
     });
   });
+  function chapter_each(chapter) {
+    each(chapter.verses, (verse) => {
+      each(verse.tokens, (token) => {
+        lambda(token);
+      });
+    });
+  }
 }
