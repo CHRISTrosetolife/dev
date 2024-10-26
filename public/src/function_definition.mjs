a342c071f6bb71129dab3cb4cabf8054f80a332a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 06:59:09 -0400

    2024-10-26T10:59:08.823Z function_auto_after function_definition

--- public/src/function_definition.mjs
+++ public/src/function_definition.mjs
@@ -1,6 +1,9 @@
+import { string_is } from "./string_is.mjs";
+import { assert } from "./assert.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { function_import } from "./function_import.mjs";
 export async function function_definition(language) {
+  assert(string_is, [language]);
   return await function_import(
     string_combine_multiple([language, "_definition"]),
   );
