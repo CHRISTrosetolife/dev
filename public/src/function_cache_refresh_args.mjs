03a7671bc5b3ea800c0e1a2804d6c24c88c307fd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 07:01:11 -0400

    2024-10-26T11:01:11.414Z function_auto_after function_cache_refresh_args

--- public/src/function_cache_refresh_args.mjs
+++ public/src/function_cache_refresh_args.mjs
@@ -4,7 +4,11 @@ import { function_cache_name } from "./function_cache_name.mjs";
 import { function_run } from "./function_run.mjs";
 export async function function_cache_refresh_args(function_name, args_list) {
   let args = list_split_comma(args_list);
-  await function_cache_delete_generic(function_name, [], file_delete_if_exists);
+  await function_cache_delete_generic(
+    function_name,
+    args,
+    file_delete_if_exists,
+  );
   let cache_name = function_cache_name(function_name);
   return await function_run(cache_name, args);
 }
