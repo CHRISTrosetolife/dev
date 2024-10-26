2a6599b4787fbf249c0b48b529942eace6fcea33
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 19:31:00 -0400

    2024-10-26T23:30:58.710Z function_auto_after bible_interlinear_upload

--- public/src/bible_interlinear_upload.mjs
+++ public/src/bible_interlinear_upload.mjs
@@ -1,4 +1,6 @@
+import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
 import { bible_interlinear_each_chapter } from "./bible_interlinear_each_chapter.mjs";
-export function bible_interlinear_upload() {
+export async function bible_interlinear_upload() {
+  let books = await bible_interlinear_cache_new();
   bible_interlinear_each_chapter(books, chapter_each);
 }
