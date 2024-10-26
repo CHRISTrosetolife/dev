da0f6da27b18b1943cc4fa806bdab49e99d211b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 19:45:00 -0400

    2024-10-26T23:44:56.440Z function_auto_after app_bible

--- public/src/app_bible.mjs
+++ public/src/app_bible.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { bible_storage_interlinear_book_path } from "./bible_storage_interlinear_book_path.mjs";
 import { firebase_download_bible } from "./firebase_download_bible.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
@@ -7,4 +8,7 @@ export async function app_bible() {
     bible_storage_interlinear_book_path(book_name),
     "1",
   );
+  log({
+    chapter_interlinear,
+  });
 }
