753dedc2ae381df551906ceabbffe9cd958def96
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 06:56:40 -0400

    2024-10-26T10:56:39.926Z function_auto_after bible_words_definitions_get

--- public/src/bible_words_definitions_get.mjs
+++ public/src/bible_words_definitions_get.mjs
@@ -2,24 +2,18 @@ import { function_definition } from "./function_definition.mjs";
 import { each_log_async } from "./each_log_async.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
-import { object_property_set } from "./object_property_set.mjs";
-import { object_property_exists_not } from "./object_property_exists_not.mjs";
 export async function bible_words_definitions_get(language, list) {
-  let existing = {};
   let result = [];
   let definition_get = await function_definition(language);
   await each_log_async(list, lambda);
   return result;
   async function lambda(item) {
     let { word, definitions } = await definition_get(item.word);
-    list_add(result, {
-      word,
-      definitions,
-    });
-    if (object_property_exists_not(existing, word)) {
-      object_property_set(existing, word, true);
-      if (list_empty_not_is(definitions)) {
-      }
+    if (list_empty_not_is(definitions)) {
+      list_add(result, {
+        word,
+        definitions,
+      });
     }
   }
 }
