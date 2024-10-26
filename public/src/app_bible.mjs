23df20b56e6a52859baa20db386e0a5bc8cc483c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 19:47:15 -0400

    2024-10-26T23:47:07.915Z function_auto_after app_bible

--- public/src/app_bible.mjs
+++ public/src/app_bible.mjs
@@ -1,8 +1,8 @@
-import { firebase_initialize } from "./firebase_initialize.mjs";
 import { log } from "./log.mjs";
 import { bible_storage_interlinear_book_path } from "./bible_storage_interlinear_book_path.mjs";
 import { firebase_download_bible } from "./firebase_download_bible.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
+import { firebase_initialize } from "./firebase_initialize.mjs";
 export async function app_bible() {
   let body = html_style_default_initialize();
   firebase_initialize();
