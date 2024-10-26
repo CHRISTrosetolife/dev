94dd151f8c738d920643e14f5a0e1b85065245a6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 07:00:55 -0400

    2024-10-26T11:00:54.724Z function_auto_after function_cache_refresh_args

--- public/src/function_cache_refresh_args.mjs
+++ public/src/function_cache_refresh_args.mjs
@@ -1,9 +1,10 @@
-import { function_cache_delete_if_exists } from "./function_cache_delete_if_exists.mjs";
+import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
+import { function_cache_delete_generic } from "./function_cache_delete_generic.mjs";
 import { function_cache_name } from "./function_cache_name.mjs";
 import { function_run } from "./function_run.mjs";
 export async function function_cache_refresh_args(function_name, args_list) {
   let args = list_split_comma(args_list);
-  await function_cache_delete_if_exists(function_name);
+  await function_cache_delete_generic(function_name, [], file_delete_if_exists);
   let cache_name = function_cache_name(function_name);
   return await function_run(cache_name, args);
 }
