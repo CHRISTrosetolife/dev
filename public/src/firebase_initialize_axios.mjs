3f9b3aa221761ec8e940ff7ff0e6c4ce62fb56b1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 19:48:56 -0400

    2024-10-26T23:48:52.423Z function_auto_after firebase_initialize_axios

--- public/src/firebase_initialize_axios.mjs
+++ public/src/firebase_initialize_axios.mjs
@@ -1,7 +1,9 @@
+import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { firebase_initialize } from "./firebase_initialize.mjs";
 import { html_script_axios } from "./html_script_axios.mjs";
 export async function firebase_initialize_axios() {
-    let root = html_style_default_initialize();
+  let root = html_style_default_initialize();
   await html_script_axios(root);
-  firebase_initialize();$r,root
+  firebase_initialize();
+  return root;
 }
