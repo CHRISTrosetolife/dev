94fc08dd1b27f1a0d8c3718c56018f021523ed30
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 19:33:45 -0400

    2024-10-26T23:33:44.643Z function_auto_after bible_interlinear_upload

--- public/src/bible_interlinear_upload.mjs
+++ public/src/bible_interlinear_upload.mjs
@@ -1,10 +1,15 @@
 import { list_adder } from "./list_adder.mjs";
 import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
 import { bible_interlinear_each_chapter } from "./bible_interlinear_each_chapter.mjs";
+import { object_merge } from "./object_merge.mjs";
 export async function bible_interlinear_upload() {
   let books = await bible_interlinear_cache_new();
   let chapters = list_adder((la) => {
-    bible_interlinear_each_chapter(books, la);
+    bible_interlinear_each_chapter(books, (chapter, book) => {
+      object_merge(chapter, {
+        book,
+      });
+    });
   });
   return chapters;
 }
