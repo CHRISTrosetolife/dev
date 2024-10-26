ebe69f10a49e3f1e5164745f3e73af0decc4cf11
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 19:42:45 -0400

    2024-10-26T23:42:44.357Z function_auto_after app_bible

--- public/src/app_bible.mjs
+++ public/src/app_bible.mjs
@@ -1,4 +1,7 @@
+import { app_search_folder } from "./app_search_folder.mjs";
+import { firebase_download_bible } from "./firebase_download_bible.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-export function app_bible() {
+export async function app_bible() {
   let body = html_style_default_initialize();
+  let i = await firebase_download_bible(word, app_search_folder());
 }
