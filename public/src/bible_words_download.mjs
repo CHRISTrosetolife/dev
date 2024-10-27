697fb7f8dc09fc93d0e24a35a763c5651b1bd592
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-27 19:54:33 -0400

    2024-10-27T23:54:32.369Z function_auto_after bible_words_download

---
+++ public/src/bible_words_download.mjs
@@ -0,0 +1,12 @@
+import { bible_interlinear_strongs_cache } from "./bible_interlinear_strongs_cache.mjs";
+import { each_log_async } from "./each_log_async.mjs";
+import { function_run_plugin_string } from "./function_run_plugin_string.mjs";
+export async function bible_words_download(language) {
+  let strongs = await function_run_plugin_string(
+    "bible_interlinear_strongs",
+    language,
+  );
+  await each_log_async(strongs, async (strong) => {
+    await bible_interlinear_strongs_cache(language, strong);
+  });
+}
--- public/src/bible_words_greek_download.mjs
+++ public/src/bible_words_greek_download.mjs
@@ -1,13 +1,5 @@
-import { each_log_async } from "./each_log_async.mjs";
-import { function_run_plugin_string } from "./function_run_plugin_string.mjs";
-import { bible_interlinear_strongs_cache } from "./bible_interlinear_strongs_cache.mjs";
+import { bible_words_download } from "./bible_words_download.mjs";
 export async function bible_words_greek_download() {
   let language = "greek";
-  let strongs = await function_run_plugin_string(
-    "bible_interlinear_strongs",
-    language,
-  );
-  await each_log_async(strongs, async (strong) => {
-    await bible_interlinear_strongs_cache(language, strong);
-  });
+  await bible_words_download(language);
 }
