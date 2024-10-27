6dc7ac0ef690acbf64f7471e4c4718bb8b0e7f04
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-27 19:40:00 -0400

    2024-10-27T23:39:59.448Z c html_parse_visit_class_list html_parse_visit_id

---
+++ public/src/html_parse_visit_id.mjs
@@ -0,0 +1,4 @@
+import { html_parse_visit_attribute_value_list } from "./html_parse_visit_attribute_value_list.mjs";
+export function html_parse_visit_id(node, class_name) {
+  return html_parse_visit_attribute_value_list(node, "class", class_name);
+}
