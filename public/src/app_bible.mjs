2de7ad5054a2243210219d512ee5e09fb6d5ecef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-27 15:22:55 -0400

    2024-10-27T19:22:55.233Z function_auto_after app_bible

--- public/src/app_bible.mjs
+++ public/src/app_bible.mjs
@@ -3,10 +3,10 @@ import { app_context_books_bible } from "./app_context_books_bible.mjs";
 import { app_bible_verse } from "./app_bible_verse.mjs";
 import { html_style_a_plain } from "./html_style_a_plain.mjs";
 import { firebase_initialize_axios } from "./firebase_initialize_axios.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
 export async function app_bible() {
   let root = await firebase_initialize_axios();
   html_style_a_plain();
-  let book_code = "MAT";
   let chapter = "1";
   let verse_number = "1";
   let context = {
@@ -14,5 +14,6 @@ export async function app_bible() {
   };
   await app_context_books_bible(context);
   let save = app_save_get(app_bible);
+  let book_code = object_property_initialize(save, "book", "MAT");
   await app_bible_verse(context, book_code, chapter, verse_number);
 }
