f240be6b7277b2110f75e5798b502641ceaae167
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 21:19:17 -0400

    2024-10-27T01:19:16.988Z function_auto_after app_record_verses

--- public/src/app_record_verses.mjs
+++ public/src/app_record_verses.mjs
@@ -2,6 +2,5 @@ import { app_verses_generic } from "./app_verses_generic.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function app_record_verses(book_code, chapter) {
   assert_arguments_length(arguments, 2);
-  let app_fn = app_record_verses;
-  return await app_verses_generic(app_fn, book_code, chapter);
+  return await app_verses_generic(app_record_verses, book_code, chapter);
 }
