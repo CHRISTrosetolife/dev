6273460c94474894ee7b0b924428143c8f924bda
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-27 19:52:11 -0400

    2024-10-27T23:52:10.823Z function_auto_after function_run_plugin

--- public/src/function_run_plugin.mjs
+++ public/src/function_run_plugin.mjs
@@ -1,6 +1,7 @@
 import { function_run } from "./function_run.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function function_run_plugin(fn, plugin_suffix) {
-  let function_name = string_combine_multiple([fn.name, "_", plugin_suffix]);
+  let f_name = fn.name;
+  let function_name = string_combine_multiple([f_name, "_", plugin_suffix]);
   return await function_run(function_name, []);
 }
