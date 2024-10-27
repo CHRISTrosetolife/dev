9b6383d2b0576c7b348fe34a5ffb7217867d092f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 20:08:04 -0400

    2024-10-27T00:07:51.001Z function_auto_after html_style_italic

--- public/src/html_style_italic.mjs
+++ public/src/html_style_italic.mjs
@@ -1,6 +1,6 @@
 import { html_style } from "./html_style.mjs";
 export function html_style_italic(component) {
   return html_style(component, {
-    "font-weight": "bold",
+    "font-style": "italic",
   });
 }
