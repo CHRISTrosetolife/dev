f25d0cf6cadd2c2d510d053e58dd90d2768cd8f2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 21:18:55 -0400

    2024-10-27T01:18:53.616Z function_auto_after app_record_verses

--- public/src/app_record_verses.mjs
+++ public/src/app_record_verses.mjs
@@ -1,17 +1,7 @@
-import { app_gs_bible_chapter_name } from "./app_gs_bible_chapter_name.mjs";
-import { global_function_initialize } from "./global_function_initialize.mjs";
-import { bible_engbsb_storage_http_get } from "./bible_engbsb_storage_http_get.mjs";
-import { object_property_get_or_async } from "./object_property_get_or_async.mjs";
+import { app_verses_generic } from "./app_verses_generic.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function app_record_verses(book_code, chapter) {
   assert_arguments_length(arguments, 2);
   let app_fn = app_record_verses;
-  let chapter_code = app_gs_bible_chapter_name(book_code, chapter);
-  let a = await global_function_initialize(app_fn, () => ({}));
-  let verses = await object_property_get_or_async(
-    a,
-    chapter_code,
-    async () => await bible_engbsb_storage_http_get(chapter_code),
-  );
-  return verses;
+  return await app_verses_generic(book_code, chapter, app_fn);
 }
