98360f65e13badae0acffc631de7ca3182432a42
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-27 19:53:39 -0400

    2024-10-27T23:53:39.263Z function_auto_after bible_words_greek_download

--- public/src/bible_words_greek_download.mjs
+++ public/src/bible_words_greek_download.mjs
@@ -1,15 +1,15 @@
+import { each_log_async } from "./each_log_async.mjs";
 import { function_run_plugin_string } from "./function_run_plugin_string.mjs";
 import { bible_interlinear_strongs_cache } from "./bible_interlinear_strongs_cache.mjs";
 import { list_last } from "./list_last.mjs";
 import { log } from "./log.mjs";
-import { each_async } from "./each_async.mjs";
 export async function bible_words_greek_download() {
   let language = "greek";
   let strongs = await function_run_plugin_string(
     "bible_interlinear_strongs",
     language,
   );
-  await each_async(strongs, async (strong) => {
+  await each_log_async(strongs, async (strong) => {
     log({
       last: list_last(strongs),
       current: strong,
