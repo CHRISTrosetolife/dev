99d99a6cb39b6a2ad4cd1e62d2a1a3fbb9a63b7d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-27 19:52:46 -0400

    2024-10-27T23:52:44.465Z function_auto_after function_run_plugin

--- public/src/function_run_plugin.mjs
+++ public/src/function_run_plugin.mjs
@@ -1,4 +1,5 @@
 import { function_run_plugin_string } from "./function_run_plugin_string.mjs";
 export async function function_run_plugin(fn, plugin_suffix) {
   let prefix = fn.name;
-  return function_run_plugin_string(prefix, plugin_suffix);  
+  return await function_run_plugin_string(prefix, plugin_suffix);
+}
