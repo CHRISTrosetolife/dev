525e76ddd98fb2d9a4b8aecc470a161a054b3830
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-27 19:36:34 -0400

    2024-10-27T23:36:33.851Z function_auto_after bible_words_greek_download

---
+++ public/src/bible_words_greek_download.mjs
@@ -0,0 +1,18 @@
+import { bible_interlinear_strongs_cache } from "./bible_interlinear_strongs_cache.mjs";
+import { list_last } from "./list_last.mjs";
+import { log } from "./log.mjs";
+import { each_async } from "./each_async.mjs";
+import { bible_interlinear_strongs_greek } from "./bible_interlinear_strongs_greek.mjs";
+import { list_first } from "./list_first.mjs";
+export async function bible_words_greek_download() {
+  let strongs = await bible_interlinear_strongs_greek();
+  let language = "greek";
+  let strong = list_first(strongs);
+  await each_async(strongs, async (strong) => {
+    log({
+      last: list_last(strongs),
+      current: strong,
+    });
+    await bible_interlinear_strongs_cache(language, strong);
+  });
+}
