67e6389b2cd9f3761e3d453943af0e19f37df2c8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-27 19:44:29 -0400

    2024-10-27T23:44:27.917Z function_auto_after bible_words_greek_download

--- public/src/bible_words_greek_download.mjs
+++ public/src/bible_words_greek_download.mjs
@@ -18,7 +18,8 @@ export async function bible_words_greek_download() {
   let leftbox = html_parse_visit_id(p, "leftbox");
   let tophdg = html_parse_visit_class_list(leftbox, "tophdg");
   let definition = list_get(tophdg, 4);
-  log(html_parse_text(definition));
+  let definition_text = html_parse_text(definition);
+  log(definition_text);
   let { next } = definition;
   log(html_parse_text(next));
   return;
