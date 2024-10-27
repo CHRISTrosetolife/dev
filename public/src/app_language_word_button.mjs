04d2c59e7ec358660f8566d5ea51ebdfded01478
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 20:32:05 -0400

    2024-10-27T00:32:05.059Z function_auto_after app_language_word_button

--- public/src/app_language_word_button.mjs
+++ public/src/app_language_word_button.mjs
@@ -5,6 +5,8 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function app_language_word_button(root, language, text) {
   assert_arguments_length(arguments, 3);
   let button = app_language_word_button_audio_none(root, language, text);
-  unawait(async () => await app_language_audio(language, text));
+  if (false) {
+    unawait(async () => await app_language_audio(language, text));
+  }
   return button;
 }
