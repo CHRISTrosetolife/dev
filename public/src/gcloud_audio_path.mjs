dc21b1bde3d45487c4d0d356041b3eb2d4c499d3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-26 20:35:48 -0400

    2024-10-27T00:35:47.569Z function_auto_after gcloud_audio_path

--- public/src/gcloud_audio_path.mjs
+++ public/src/gcloud_audio_path.mjs
@@ -1,4 +1,13 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_encoded_to } from "./string_encoded_to.mjs";
 export function gcloud_audio_path(language_code, text, voice) {
-  return `audio/${language_code}/${string_encoded_to(text)}/${voice}.mp3`;
+  return string_combine_multiple([
+    "audio/",
+    language_code,
+    "/",
+    string_encoded_to(text),
+    "/",
+    voice,
+    ".mp3",
+  ]);
 }
