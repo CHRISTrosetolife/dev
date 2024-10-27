ac0b434076d87f80f71240c57da19dcc038eaf17
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-27 18:54:49 -0400

    2024-10-27T22:54:48.314Z function_auto_after app_bible

--- public/src/app_bible.mjs
+++ public/src/app_bible.mjs
@@ -6,6 +6,7 @@ import { app_bible_verse } from "./app_bible_verse.mjs";
 import { html_style_a_plain } from "./html_style_a_plain.mjs";
 import { firebase_initialize_axios } from "./firebase_initialize_axios.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
+import { object_merge } from "./object_merge.mjs";
 export async function app_bible() {
   let root = await firebase_initialize_axios();
   html_style_a_plain();
@@ -17,10 +18,12 @@ export async function app_bible() {
   log({
     save,
   });
+  object_merge(save, {
+    verse_number: "25",
+  });
   let book_code = object_property_initialize(save, "book", "MAT");
   let chapter = object_property_initialize(save, "chapter", "1");
   let verse_number = object_property_initialize(save, "verse", "1");
-  verse_number = "25";
   app_save_set(app_bible, save);
   await app_bible_verse(context, book_code, chapter, verse_number);
 }
