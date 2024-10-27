ac56917dac2d22b2c09b94f3d7457512518d83b6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-27 19:40:17 -0400

    2024-10-27T23:40:14.710Z function_auto_after html_parse_visit_id

--- public/src/html_parse_visit_id.mjs
+++ public/src/html_parse_visit_id.mjs
@@ -1,4 +1,6 @@
 import { html_parse_visit_attribute_value_list } from "./html_parse_visit_attribute_value_list.mjs";
-export function html_parse_visit_id(node, class_name) {
-  return html_parse_visit_attribute_value_list(node, "class", class_name);
+import { list_single } from "./list_single.mjs";
+export function html_parse_visit_id(node, id_value) {
+  let ids = html_parse_visit_attribute_value_list(node, "id", id_value);
+  return list_single(ids);
 }
