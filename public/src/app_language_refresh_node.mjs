99511ab4f648effe7baafa99654d3f29149876fe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-27 16:03:11 -0400

    2024-10-27T20:03:10.026Z function_auto_after app_language_refresh_node

--- public/src/app_language_refresh_node.mjs
+++ public/src/app_language_refresh_node.mjs
@@ -51,9 +51,11 @@ import { app_ceb } from "./app_ceb.mjs";
 import { storage_local_get } from "./storage_local_get.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 import { list_add } from "./list_add.mjs";
+import { html_p_text } from "./html_p_text.mjs";
 export async function app_language_refresh_node(context) {
   let { root, group, invert, language_fluent } = context;
   let group_index = app_language_group_index_get(context);
+  html_p_text(root, group_index);
   storage_local_set(context.app_fn, "screen", "node");
   let level_size = app_language_level_size();
   html_clear_scroll_top_centered(root);
