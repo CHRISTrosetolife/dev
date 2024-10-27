2cc440f137a4e7059117f4c2bf2f11653a13e87f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-27 19:49:36 -0400

    2024-10-27T23:49:36.059Z function_auto_after bible_interlinear_strongs_hebrew

--- public/src/bible_interlinear_strongs_hebrew.mjs
+++ public/src/bible_interlinear_strongs_hebrew.mjs
@@ -1,5 +1,5 @@
+import { bible_interlinear_cache_old } from "./bible_interlinear_cache_old.mjs";
 import { bible_interlinear_strongs_generic } from "./bible_interlinear_strongs_generic.mjs";
-import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
 export async function bible_interlinear_strongs_hebrew() {
-  return await bible_interlinear_strongs_generic(bible_interlinear_cache_new);
+  return await bible_interlinear_strongs_generic(bible_interlinear_cache_old);
 }
