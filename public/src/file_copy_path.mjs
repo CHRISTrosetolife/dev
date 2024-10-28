f6f4d667fbf835a883aac1c18a6c36384712d442
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 15:51:28 -0400

    2024-10-28T19:51:27.330Z function_auto_after file_copy_path

--- public/src/file_copy_path.mjs
+++ public/src/file_copy_path.mjs
@@ -1,7 +1,5 @@
-import { path_join } from "./path_join.mjs";
-import { path_parse_base } from "./path_parse_base.mjs";
+import { file_copy_path_generic } from "./file_copy_path_generic.mjs";
+import { identity } from "./identity.mjs";
 export function file_copy_path(file_path, output_directory) {
-  let file_name = path_parse_base(file_path);
-  let file_path_new = path_join([output_directory, file_name]);
-  return file_path_new;
+  return file_copy_path_generic(file_path, output_directory, identity);
 }
