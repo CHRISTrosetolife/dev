39b4bd6c3ee9750a184c97b69f1fbdcf87a65543
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 19:06:42 -0400

    2024-10-28T23:06:42.236Z function_auto_after app_todo_command_move

--- public/src/app_todo_command_move.mjs
+++ public/src/app_todo_command_move.mjs
@@ -1,8 +1,10 @@
 import { list_index } from "./list_index.mjs";
 import { list_get } from "./list_get.mjs";
 import { app_todo_command_items } from "./app_todo_command_items.mjs";
+import { list_size } from "./list_size.mjs";
 export async function app_todo_command_move(index_from, index_to) {
   let { filtered, items } = await app_todo_command_items();
+  let filtered_size = list_size(filtered);
   let item_from = list_get(filtered, index_from);
   let item_to = list_get(filtered, index_to);
   let items_index_from = list_index(items, item_from);
