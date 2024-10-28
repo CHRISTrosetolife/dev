185be3181b84ff67c31be0221c8f9d6d1a6fb630
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 15:34:56 -0400

    2024-10-28T19:34:53.500Z function_auto_after bible_audio_player_book

--- public/src/bible_audio_player_book.mjs
+++ public/src/bible_audio_player_book.mjs
@@ -1,5 +1,5 @@
 import { bible_audio_player_range } from "./bible_audio_player_range.mjs";
 export async function bible_audio_player_book(book_letters_prefix) {
   let book_to = book_letters_prefix;
-  await bible_audio_player_range(book_letters_prefix, book_to);
+  return await bible_audio_player_range(book_letters_prefix, book_to);
 }
