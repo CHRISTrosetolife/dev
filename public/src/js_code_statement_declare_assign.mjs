85707e0f8c6e130e131f725a85b75b5bff9853bf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 14:24:35 -0400

    2024-10-28T18:24:34.929Z de js_code_statement_declare_assign

--- public/src/js_code_statement_declare_assign.mjs
+++
@@ -1,5 +0,0 @@
-import { js_code_declare_assign } from "./js_code_declare_assign.mjs";
-import { js_code_statement } from "./js_code_statement.mjs";
-export function js_code_statement_declare_assign(name, value) {
-  return js_code_statement(js_code_declare_assign(name, value));
-}
