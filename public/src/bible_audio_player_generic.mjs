36fbb22eb56ffb55d0f190604ec836bfeaff5c86
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 15:40:43 -0400

    2024-10-28T19:40:42.721Z function_auto_after bible_audio_player_generic

--- public/src/bible_audio_player_generic.mjs
+++ public/src/bible_audio_player_generic.mjs
@@ -1,5 +1,4 @@
 import { folder_read_filter } from "./folder_read_filter.mjs";
-import { bible_audio_player_output_path } from "./bible_audio_player_output_path.mjs";
 import { uuid } from "./uuid.mjs";
 import { folder_files_copy_generic } from "./folder_files_copy_generic.mjs";
 import { path_join } from "./path_join.mjs";
@@ -14,7 +13,6 @@ export async function bible_audio_player_generic(
   let extension = ".mp3";
   let filtered = await folder_read_filter(from, extension, filter);
   return filtered;
-  let to = bible_audio_player_output_path();
   await folder_files_copy_generic(from, extension, to, filter, async (to) => {
     let without = string_suffix_without(to, extension);
     let with_id = string_combine_multiple([
