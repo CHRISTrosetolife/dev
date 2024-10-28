7fb6709059cff4a9afff3ddafbfc20a800e3a22c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 14:38:52 -0400

    2024-10-28T18:38:51.108Z function_auto_after app_todo_items

--- public/src/app_todo_items.mjs
+++ public/src/app_todo_items.mjs
@@ -1,22 +1,5 @@
-import { each_index } from "./each_index.mjs";
-import { app_todo_choices_initialize } from "./app_todo_choices_initialize.mjs";
-import { each } from "./each.mjs";
-import { list_set } from "./list_set.mjs";
-import { app_todo_item } from "./app_todo_item.mjs";
-import { string_is } from "./string_is.mjs";
-import { app_todo_properties } from "./app_todo_properties.mjs";
-import { object_property_initialize } from "./object_property_initialize.mjs";
+import { app_todo_items_object } from "./app_todo_items_object.mjs";
 export function app_todo_items(context) {
   let { index } = context;
-  let items = object_property_initialize(index, "items", []);
-  let properties = app_todo_properties();
-  each_index(items, (item, ix) => {
-    if (string_is(item)) {
-      item = app_todo_item(item);
-      list_set(items, ix, item);
-    }
-    each(properties, (p) => app_todo_choices_initialize(item, p));
-    return item;
-  });
-  return items;
+  return app_todo_items_object(index);
 }
---
+++ public/src/app_todo_items_object.mjs
@@ -0,0 +1,21 @@
+import { app_todo_choices_initialize } from "./app_todo_choices_initialize.mjs";
+import { each } from "./each.mjs";
+import { list_set } from "./list_set.mjs";
+import { app_todo_item } from "./app_todo_item.mjs";
+import { string_is } from "./string_is.mjs";
+import { each_index } from "./each_index.mjs";
+import { app_todo_properties } from "./app_todo_properties.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
+export function app_todo_items_object(index) {
+  let items = object_property_initialize(index, "items", []);
+  let properties = app_todo_properties();
+  each_index(items, (item, ix) => {
+    if (string_is(item)) {
+      item = app_todo_item(item);
+      list_set(items, ix, item);
+    }
+    each(properties, (p) => app_todo_choices_initialize(item, p));
+    return item;
+  });
+  return items;
+}
