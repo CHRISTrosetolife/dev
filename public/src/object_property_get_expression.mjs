e441bd151201d886e8c94c65aea50500b31fd904
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 14:28:24 -0400

    2024-10-28T18:28:23.814Z function_auto_after object_property_get_expression

--- public/src/object_property_get_expression.mjs
+++ public/src/object_property_get_expression.mjs
@@ -7,9 +7,10 @@ export function object_property_get_expression(
   object_variable_name,
   property_name_code,
 ) {
+  let object_variable_name_unique = js_name_unique(ast, object_variable_name);
   return js_parse_expression(
     js_code_call_args(fn_name("object_property_get"), [
-      js_name_unique(ast, object_variable_name),
+      object_variable_name_unique,
       property_name_code,
     ]),
   );
