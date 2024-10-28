7c58dfb6166e43db431320088221dd01c938fc9f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 18:32:11 -0400

    2024-10-28T22:32:10.686Z function_auto_after storage_file_download

--- public/src/storage_file_download.mjs
+++ public/src/storage_file_download.mjs
@@ -1,13 +1,9 @@
+import { storage_file_download_force } from "./storage_file_download_force.mjs";
 import { storage_file_download_path } from "./storage_file_download_path.mjs";
-import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
 import { file_exists_not } from "./file_exists_not.mjs";
 export async function storage_file_download(firebase_file) {
   let destination = storage_file_download_path(firebase_file);
   if (await file_exists_not(destination)) {
-    let destination = storage_file_download_path(firebase_file);
-    await folder_parent_exists_ensure(destination);
-    await firebase_file.download({
-      destination,
-    });
+    await storage_file_download_force(firebase_file);
   }
 }
---
+++ public/src/storage_file_download_force.mjs
@@ -0,0 +1,9 @@
+import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
+import { storage_file_download_path } from "./storage_file_download_path.mjs";
+export async function storage_file_download_force(firebase_file) {
+  let destination = storage_file_download_path(firebase_file);
+  await folder_parent_exists_ensure(destination);
+  await firebase_file.download({
+    destination,
+  });
+}
