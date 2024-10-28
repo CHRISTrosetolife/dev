7724c68ab4a0923f81612ac1e79c751932ec17bf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 15:43:59 -0400

    2024-10-28T19:43:58.282Z function_auto_after bible_audio_player_range

--- public/src/bible_audio_player_range.mjs
+++ public/src/bible_audio_player_range.mjs
@@ -119,7 +119,7 @@ export async function bible_audio_player_range(book_from, book_to) {
   let size = list_size(denested);
   await each_async(denested, async (d) => {
     let to_path = file_copy_path(d, to);
-    await file_copy_closed(d, to_mapped);
+    await file_copy_closed(d, to_path);
   });
   list_shuffle(paths);
   await invoke_multiple_async(paths);
