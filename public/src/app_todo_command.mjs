a479ad54c17896a086303352ea189680ab48c65b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 14:11:00 -0400

    2024-10-28T18:10:59.960Z function_auto_after app_todo_command

--- public/src/app_todo_command.mjs
+++ public/src/app_todo_command.mjs
@@ -11,4 +11,5 @@ export async function app_todo_command() {
   await storage_file_download(last);
   let path = storage_file_download_path(last);
   let i = await file_read_json(path);
+  let items = i;
 }
