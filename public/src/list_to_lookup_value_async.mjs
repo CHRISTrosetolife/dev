54bffadd8b9cefd0a06de73d0fda616acfa2c4a4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-27 20:06:14 -0400

    2024-10-28T00:06:13.523Z c list_to_lookup_value list_to_lookup_value_async

---
+++ public/src/list_to_lookup_value_async.mjs
@@ -0,0 +1,5 @@
+import { list_to_lookup_key_value } from "./list_to_lookup_key_value.mjs";
+import { identity } from "./identity.mjs";
+export function list_to_lookup_value_async(list, mapped_value) {
+  return list_to_lookup_key_value(list, identity, mapped_value);
+}
