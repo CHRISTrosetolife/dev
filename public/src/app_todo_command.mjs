8d4b026d2d31c3d0157729791f42013dc814fae5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 14:10:50 -0400

    2024-10-28T18:10:49.133Z function_auto_after app_todo_command

--- public/src/app_todo_command.mjs
+++ public/src/app_todo_command.mjs
@@ -1,14 +1,14 @@
-import { file_open } from "./file_open.mjs";
 import { list_single } from "./list_single.mjs";
 import { storage_file_download_path } from "./storage_file_download_path.mjs";
 import { storage_file_download } from "./storage_file_download.mjs";
 import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
 import { storage_files } from "./storage_files.mjs";
+import { file_read_json } from "./file_read_json.mjs";
 export async function app_todo_command() {
   let index_path = app_todo_firebase_path_index();
   let files = await storage_files(index_path);
   let last = list_single(files);
   await storage_file_download(last);
   let path = storage_file_download_path(last);
-  await file_open(path);
+  let i = await file_read_json(path);
 }
