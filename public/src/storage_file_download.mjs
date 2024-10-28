de4ae6b5a035d5664f3efc27bd0b44797fa1e0f4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 18:31:53 -0400

    2024-10-28T22:31:52.638Z function_auto_after storage_file_download

--- public/src/storage_file_download.mjs
+++ public/src/storage_file_download.mjs
@@ -4,6 +4,7 @@ import { file_exists_not } from "./file_exists_not.mjs";
 export async function storage_file_download(firebase_file) {
   let destination = storage_file_download_path(firebase_file);
   if (await file_exists_not(destination)) {
+    let destination = storage_file_download_path(firebase_file);
     await folder_parent_exists_ensure(destination);
     await firebase_file.download({
       destination,
