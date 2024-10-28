eb9ebb1f1d016da5d68d9d59d4c440dc111f1236
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 14:09:10 -0400

    2024-10-28T18:09:09.600Z function_auto_after app_todo_command

--- public/src/app_todo_command.mjs
+++ public/src/app_todo_command.mjs
@@ -1,4 +1,6 @@
+import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
 import { storage_files } from "./storage_files.mjs";
 export async function app_todo_command() {
-  await storage_files();
+  let index_path = app_todo_firebase_path_index();
+  let files = await storage_files(index_path);
 }
