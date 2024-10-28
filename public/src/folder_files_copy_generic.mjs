36df5b1fcb7b6e57700d1eb4fee64b35ada6f196
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 15:31:16 -0400

    2024-10-28T19:31:16.403Z function_auto_after folder_files_copy_generic

--- public/src/folder_files_copy_generic.mjs
+++ public/src/folder_files_copy_generic.mjs
@@ -1,5 +1,4 @@
 import { file_copy_closed } from "./file_copy_closed.mjs";
-import { log } from "./log.mjs";
 import { folder_files_move_generic } from "./folder_files_move_generic.mjs";
 export async function folder_files_copy_generic(
   input_directory,
@@ -10,11 +9,7 @@ export async function folder_files_copy_generic(
 ) {
   let lambda = async (from, to) => {
     let to_mapped = await map(to);
-    if (0) {
-      log(to_mapped);
-    } else {
-      await file_copy_closed(from, to_mapped);
-    }
+    await file_copy_closed(from, to_mapped);
   };
   await folder_files_move_generic(
     input_directory,
