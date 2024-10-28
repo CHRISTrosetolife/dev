e8b909c3840d6ab358e6e36de672323cf6836eb0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 19:11:30 -0400

    2024-10-28T23:11:30.340Z function_auto_after app_todo_command

--- public/src/app_todo_command.mjs
+++ public/src/app_todo_command.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { app_todo_command_log } from "./app_todo_command_log.mjs";
 import { app_todo_command_items } from "./app_todo_command_items.mjs";
 export async function app_todo_command() {
+  assert_arguments_length(arguments, 0);
   let { filtered } = await app_todo_command_items();
   app_todo_command_log(filtered);
 }
