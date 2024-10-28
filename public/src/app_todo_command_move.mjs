07bbb0a979423ae594b8996e6284d43a47d26975
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 19:08:13 -0400

    2024-10-28T23:08:13.390Z function_auto_after app_todo_command_move

--- public/src/app_todo_command_move.mjs
+++ public/src/app_todo_command_move.mjs
@@ -7,13 +7,11 @@ export async function app_todo_command_move(index_from, index_to) {
   let filtered_size = list_size(filtered);
   let item_from = list_get(filtered, index_from);
   let items_index_from = list_index(items, item_from);
-  let item_to;
   let items_index_to;
   if (index_to === filtered_size) {
-    item_to = null;
     items_index_to = list_size(items);
   } else {
-    item_to = list_get(filtered, index_to);
+    let item_to = list_get(filtered, index_to);
     items_index_to = list_index(items, item_to);
   }
 }
