c912424268d4fe560828b7dcaed28ed9f75e732c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 14:19:23 -0400

    2024-10-28T18:19:22.731Z function_auto_after js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -391,6 +391,7 @@ export function js_dollar(ast) {
       if (prefix_use(remaining, get_prefix, prefixes)) {
         remaining = string_prefix_without(remaining, get_prefix);
         let s = string_split_dollar(remaining);
+        name = s;
         log({
           s,
         });
