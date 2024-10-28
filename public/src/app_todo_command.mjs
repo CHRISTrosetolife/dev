7091087a20e1103bef4975e43087782379bca396
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-28 14:39:15 -0400

    2024-10-28T18:39:15.180Z function_auto_after app_todo_command

--- public/src/app_todo_command.mjs
+++ public/src/app_todo_command.mjs
@@ -1,4 +1,4 @@
-import { object_property_get } from "./object_property_get.mjs";
+import { app_todo_items_object } from "./app_todo_items_object.mjs";
 import { list_single } from "./list_single.mjs";
 import { storage_file_download_path } from "./storage_file_download_path.mjs";
 import { storage_file_download } from "./storage_file_download.mjs";
@@ -11,7 +11,7 @@ export async function app_todo_command() {
   let last = list_single(files);
   await storage_file_download(last);
   let path = storage_file_download_path(last);
-  let i = await file_read_json(path);
-  let items = object_property_get(i, "items");
+  let index = await file_read_json(path);
+  let items = app_todo_items_object(index);
   return items;
 }
