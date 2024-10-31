4ac12786c5223490e23006d213b150efe44bf4b8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-31 12:37:12 -0400

    2024-10-31T16:37:10.605Z function_auto_after app_share

--- public/src/app_share.mjs
+++ public/src/app_share.mjs
@@ -1,9 +1,10 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { html_hash_lookup } from "./html_hash_lookup.mjs";
 import { firebase_download_bible_verse } from "./firebase_download_bible_verse.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_share() {
   let body = html_style_default_initialize();
   let lookup = html_hash_lookup();
-  $glookup$verse;
+  let verse2 = object_property_get(lookup, "verse");
   let text = await firebase_download_bible_verse(chapter, verse);
 }
