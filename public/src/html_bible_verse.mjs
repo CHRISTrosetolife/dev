59228994f5cbdd02aeda73920a99c643bf4f31d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-31 18:00:26 -0400

    2024-10-31T22:00:23.900Z function_auto_after html_bible_verse

--- public/src/html_bible_verse.mjs
+++ public/src/html_bible_verse.mjs
@@ -2,8 +2,6 @@ import { list_single } from "./list_single.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { google_search_url } from "./google_search_url.mjs";
 import { html_a } from "./html_a.mjs";
-import { list_empty_not_is } from "./list_empty_not_is.mjs";
-import { bible_search_symbols_map_single } from "./bible_search_symbols_map_single.mjs";
 import { html_spacer } from "./html_spacer.mjs";
 import { each } from "./each.mjs";
 import { html_bible_verse_number } from "./html_bible_verse_number.mjs";
@@ -12,16 +10,13 @@ export function html_bible_verse(root, book_code, chapter, verse) {
   let vn = html_bible_verse_number(root, book_code, chapter, verse_number);
   each(tokens, (token) => {
     html_spacer(root);
-    let mapped = bible_search_symbols_map_single(token);
-    if (list_empty_not_is(mapped)) {
-      html_a(
-        root,
-        token,
-        google_search_url(
-          string_combine_multiple(["define: ", list_single(mapped)]),
-        ),
-      );
-    }
+    html_a(
+      root,
+      token,
+      google_search_url(
+        string_combine_multiple(["define: ", list_single(tokens)]),
+      ),
+    );
   });
   return vn;
 }
