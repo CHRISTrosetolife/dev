9247d12ce435ece434864ae08b2282c5c7e97495
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-31 15:32:48 -0400

    2024-10-31T19:32:45.113Z function_auto_after firebase_download_bible_verse

--- public/src/firebase_download_bible_verse.mjs
+++ public/src/firebase_download_bible_verse.mjs
@@ -1,13 +1,11 @@
 import { list_join_space } from "./list_join_space.mjs";
 import { firebase_download } from "./firebase_download.mjs";
 import { bible_verses_upload_path } from "./bible_verses_upload_path.mjs";
-import { bible_reference_code } from "./bible_reference_code.mjs";
 export async function firebase_download_bible_verse(
   bible_folder,
   chapter_code,
   verse_number,
 ) {
-  let reference = bible_reference_code(chapter_code, verse_number);
   let storage_path = bible_verses_upload_path(
     bible_folder,
     chapter_code,
