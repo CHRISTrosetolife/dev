6d30bcedfb54844c1bbaa2f1e038eceedadf7a20
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-31 14:41:17 -0400

    2024-10-31T18:41:14.616Z function_auto_after app_share

--- public/src/app_share.mjs
+++ public/src/app_share.mjs
@@ -76,9 +76,10 @@ export async function app_share() {
     let origin = object_property_get(location, "origin");
     let without_hash = string_combine_multiple([origin, pathname]);
     let properties = object_properties(lookup_next);
-    list_map(properties, (p) =>
+    let mapped = list_map(properties, (p) =>
       string_combine_multiple([p, "=", object_property_get(lookup_next, p)]),
     );
+    list_join_(mapped, ",");
     html_button_copy(result, text);
   });
 }
