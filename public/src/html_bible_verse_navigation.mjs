e19df0d31945bf766337b40950b9d9d9b826d6dc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-31 13:58:56 -0400

    2024-10-31T17:58:55.297Z function_auto_after html_bible_verse_navigation

--- public/src/html_bible_verse_navigation.mjs
+++ public/src/html_bible_verse_navigation.mjs
@@ -1,9 +1,6 @@
-import { list_next } from "./list_next.mjs";
-import { list_first } from "./list_first.mjs";
+import { html_bible_verse_navigation_next } from "./html_bible_verse_navigation_next.mjs";
 import { clipboard_copy_web } from "./clipboard_copy_web.mjs";
 import { bible_book_chapter_text } from "./bible_book_chapter_text.mjs";
-import { list_find_property_next_property } from "./list_find_property_next_property.mjs";
-import { list_last_is } from "./list_last_is.mjs";
 import { html_button_next_text } from "./html_button_next_text.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -52,45 +49,15 @@ export async function html_bible_verse_navigation(
     );
   }
   async function verse_next() {
-    let verse = list_find_property(verses, "verse_number", verse_number);
-    let verses = await app_verses_generic(app_fn, book_code, chapter);
-    if (list_last_is(verses, verse)) {
-      await chapter_next_go();
-    } else {
-      let verse_number_next = list_find_property_next_property(
-        verses,
-        "verse_number",
-        verse_number,
-      );
-      await verse_refresh(context, book_code, chapter, verse_number_next);
-    }
-    async function chapter_next_go() {
-      on_chapter_next();
-      let { books } = context;
-      let book = list_find_property(books, "book_code", book_code);
-      let { chapters } = book;
-      let chapter_next, book_next_code, book_next_book;
-      if (list_last_is(chapters, chapter)) {
-        if (list_last_is(books, book)) {
-          book_next_book = list_first(books);
-        } else {
-          book_next_book = list_next(books, book);
-        }
-        book_next_code = object_property_get(book_next_book, "book_code");
-        let { chapters } = book_next_book;
-        chapter_next = list_first(chapters);
-      } else {
-        book_next_code = book_code;
-        chapter_next = list_next(chapters, chapter);
-      }
-      let verses_next = await app_verses_generic(app_fn, book_code, chapter);
-      await verse_refresh(
-        context,
-        book_next_code,
-        chapter_next,
-        object_property_get(list_first(verses_next), "verse_number"),
-      );
-    }
+    await html_bible_verse_navigation_next(
+      verse_number,
+      app_fn,
+      book_code,
+      chapter,
+      verse_refresh,
+      context,
+      on_chapter_next,
+    );
   }
   return {
     previous,
---
+++ public/src/html_bible_verse_navigation_next.mjs
@@ -0,0 +1,56 @@
+import { object_property_get } from "./object_property_get.mjs";
+import { list_next } from "./list_next.mjs";
+import { list_first } from "./list_first.mjs";
+import { list_find_property_next_property } from "./list_find_property_next_property.mjs";
+import { list_last_is } from "./list_last_is.mjs";
+import { app_verses_generic } from "./app_verses_generic.mjs";
+import { list_find_property } from "./list_find_property.mjs";
+export async function html_bible_verse_navigation_next(
+  verse_number,
+  app_fn,
+  book_code,
+  chapter,
+  verse_refresh,
+  context,
+  on_chapter_next,
+) {
+  let verse = list_find_property(verses, "verse_number", verse_number);
+  let verses = await app_verses_generic(app_fn, book_code, chapter);
+  if (list_last_is(verses, verse)) {
+    await chapter_next_go();
+  } else {
+    let verse_number_next = list_find_property_next_property(
+      verses,
+      "verse_number",
+      verse_number,
+    );
+    await verse_refresh(context, book_code, chapter, verse_number_next);
+  }
+  async function chapter_next_go() {
+    on_chapter_next();
+    let { books } = context;
+    let book = list_find_property(books, "book_code", book_code);
+    let { chapters } = book;
+    let chapter_next, book_next_code, book_next_book;
+    if (list_last_is(chapters, chapter)) {
+      if (list_last_is(books, book)) {
+        book_next_book = list_first(books);
+      } else {
+        book_next_book = list_next(books, book);
+      }
+      book_next_code = object_property_get(book_next_book, "book_code");
+      let { chapters } = book_next_book;
+      chapter_next = list_first(chapters);
+    } else {
+      book_next_code = book_code;
+      chapter_next = list_next(chapters, chapter);
+    }
+    let verses_next = await app_verses_generic(app_fn, book_code, chapter);
+    await verse_refresh(
+      context,
+      book_next_code,
+      chapter_next,
+      object_property_get(list_first(verses_next), "verse_number"),
+    );
+  }
+}
