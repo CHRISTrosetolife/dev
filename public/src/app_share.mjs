0434211e5fd29164dcd546791e2ee52a36d4e69a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-31 14:15:22 -0400

    2024-10-31T18:15:19.073Z function_auto_after app_share

--- public/src/app_share.mjs
+++ public/src/app_share.mjs
@@ -39,6 +39,7 @@ export async function app_share() {
     });
     let verse_number_next = object_property_get(next, "verse_number_next");
     let location = object_property_get(window, "location");
+    let href = object_property_get(location, "href");
     html_button_copy(result, text);
   });
 }
