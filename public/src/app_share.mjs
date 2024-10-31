a14a77c3c8065c00c492e40b26642fdcc0ef24f2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-10-31 12:47:24 -0400

    2024-10-31T16:47:23.197Z function_auto_after app_share

--- public/src/app_share.mjs
+++ public/src/app_share.mjs
@@ -1,5 +1,5 @@
+import { each_async } from "./each_async.mjs";
 import { firebase_download_bible_verse } from "./firebase_download_bible_verse.mjs";
-import { app_search_bible_folder } from "./app_search_bible_folder.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { html_hash_lookup } from "./html_hash_lookup.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
@@ -8,6 +8,7 @@ export async function app_share() {
   let lookup = html_hash_lookup();
   let chapter = object_property_get(lookup, "c");
   let verse = object_property_get(lookup, "v");
-  let bible_folder = app_search_bible_folder();
+  let bible_folders = object_property_get(lookup, "f");
+  await each_async(list, async (item) => {});
   let text = await firebase_download_bible_verse(bible_folder, chapter, verse);
 }
