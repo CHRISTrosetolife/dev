af88afeb41f517750674030eda71122b4a1a36a2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-01 13:13:11 -0400

    2024-11-01T17:13:09.515Z function_auto_after bible_reference_code

--- public/src/bible_reference_code.mjs
+++ public/src/bible_reference_code.mjs
@@ -1,8 +1,11 @@
+import { list_last } from "./list_last.mjs";
+import { list_join_dash } from "./list_join_dash.mjs";
 import { bible_reference } from "./bible_reference.mjs";
 import { bible_book_name } from "./bible_book_name.mjs";
 import { bible_chapter_name_parse } from "./bible_chapter_name_parse.mjs";
 import { list_size_1 } from "./list_size_1.mjs";
 import { list_single } from "./list_single.mjs";
+import { list_first } from "./list_first.mjs";
 export function bible_reference_code(chapter_code, verse_numbers) {
   let { book_code, chapter_code: chapter_number } =
     bible_chapter_name_parse(chapter_code);
@@ -11,6 +14,7 @@ export function bible_reference_code(chapter_code, verse_numbers) {
   if (list_size_1(verse_numbers)) {
     vr = list_single(verse_numbers);
   } else {
+    vr = list_join_dash([list_first(verse_numbers), list_last(verse_numbers)]);
   }
   let r = bible_reference(book_name, chapter_number, verse_numbers);
   return r;
