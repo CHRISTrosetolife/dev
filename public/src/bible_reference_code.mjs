099caccdf4f1ef8419a0165307817172f4284298
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-01 13:11:39 -0400

    2024-11-01T17:11:38.905Z function_auto_after bible_reference_code

--- public/src/bible_reference_code.mjs
+++ public/src/bible_reference_code.mjs
@@ -1,10 +1,10 @@
 import { bible_reference } from "./bible_reference.mjs";
 import { bible_book_name } from "./bible_book_name.mjs";
 import { bible_chapter_name_parse } from "./bible_chapter_name_parse.mjs";
-export function bible_reference_code(chapter_code, verse_number) {
+export function bible_reference_code(chapter_code, verse_numbers) {
   let { book_code, chapter_code: chapter_number } =
     bible_chapter_name_parse(chapter_code);
   let book_name = bible_book_name(book_code);
-  let r = bible_reference(book_name, chapter_number, verse_number);
+  let r = bible_reference(book_name, chapter_number, verse_numbers);
   return r;
 }
