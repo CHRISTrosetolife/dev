aba91313f6f26171e2e20bf265dbf1278a77f886
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-01 14:06:39 -0400

    2024-11-01T18:06:38.698Z function_auto_after bible_chapter_verse_numbers

--- public/src/bible_chapter_verse_numbers.mjs
+++ public/src/bible_chapter_verse_numbers.mjs
@@ -1,6 +1,6 @@
-import { list_map } from "./list_map.mjs";
+import { list_map_property } from "./list_map_property.mjs";
 import { bible_chapter } from "./bible_chapter.mjs";
 export async function bible_chapter_verse_numbers(bible_folder, chapter_name) {
   let r = await bible_chapter(bible_folder, chapter_name);
-  return list_map(r, "verse_number");
+  return list_map_property(r, "verse_number");
 }
