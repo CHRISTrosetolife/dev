ef5b15850a2adaa4c1c67c470f52e7137ba8e678
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-01 12:48:30 -0400

    2024-11-01T16:48:27.871Z c html_button_copy html_button_copy_get

---
+++ public/src/html_button_copy_get.mjs
@@ -0,0 +1,7 @@
+import { clipboard_copy_web } from "./clipboard_copy_web.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+export function html_button_copy_get(result, text) {
+  html_button_width_full_text_click(result, "📋 copy", () => {
+    clipboard_copy_web(text);
+  });
+}
