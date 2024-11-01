24ac607e699aa92272dcc6290bf649e1ace2bcef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-01 13:09:09 -0400

    2024-11-01T17:09:08.692Z function_auto_after app_share_verse_download

--- public/src/app_share_verse_download.mjs
+++ public/src/app_share_verse_download.mjs
@@ -1,3 +1,5 @@
+import { list_add_beginning } from "./list_add_beginning.mjs";
+import { bible_reference_code } from "./bible_reference_code.mjs";
 import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
 import { firebase_download_bible_verse } from "./firebase_download_bible_verse.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
@@ -20,5 +22,7 @@ export async function app_share_verse_download(
   });
   list_add_multiple(texts, result);
   html_clear(verses_component);
+  let reference = bible_reference_code(chapter, verse_number_next);
+  list_add_beginning(texts, reference);
   html_p_text_multiple(verses_component, texts);
 }
