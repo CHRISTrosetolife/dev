7102398965770850cecf02621cc67c85f13ae700
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-01 14:06:14 -0400

    2024-11-01T18:06:11.965Z function_auto_after bible_chapter_verse_numbers

--- public/src/bible_chapter_verse_numbers.mjs
+++ public/src/bible_chapter_verse_numbers.mjs
@@ -1,4 +1,6 @@
+import { list_map } from "./list_map.mjs";
 import { bible_chapter } from "./bible_chapter.mjs";
 export async function bible_chapter_verse_numbers(bible_folder, chapter_name) {
-  await bible_chapter(bible_folder, chapter_name);
+  let r = await bible_chapter(bible_folder, chapter_name);
+  return list_map(r, "verse_number");
 }
