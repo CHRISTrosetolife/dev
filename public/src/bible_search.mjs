52663039b1cf74fb4d352c4974839349bc4d0454
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-02 08:21:09 -0400

    2024-11-02T12:21:06.847Z function_auto_after bible_search

--- public/src/bible_search.mjs
+++ public/src/bible_search.mjs
@@ -1,5 +1,7 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { bible_search_generic } from "./bible_search_generic.mjs";
 import { tautology } from "./tautology.mjs";
 export async function bible_search(words) {
+  assert_arguments_length(arguments, 1);
   return await bible_search_generic(words, tautology);
 }
