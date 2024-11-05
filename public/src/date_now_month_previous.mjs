23fe16ae1b749a36b41e5249d9a3c1e47b4953b7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-05 18:40:59 -0500

    2024-11-05T23:40:58.222Z function_auto_after date_now_month_previous

--- public/src/date_now_month_previous.mjs
+++ public/src/date_now_month_previous.mjs
@@ -1,5 +1,9 @@
 import { date_now } from "./date_now.mjs";
 export function date_now_month_previous() {
   let d = date_now();
-  return d.setMonth(d.getMonth() - 1);
+  let month = d.getMonth();
+  d.setMonth(d.getMonth() - 1);
+  while (d.getMonth() === month) {
+    d.setDate(d.getDate() - 1);
+  }
 }
