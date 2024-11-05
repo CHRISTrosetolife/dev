91212b42fd2d0cbb33b35ac81c0cd194f2f7104a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-05 18:41:53 -0500

    2024-11-05T23:41:52.775Z function_auto_after app_sleep_view_refresh

--- public/src/app_sleep_view_refresh.mjs
+++ public/src/app_sleep_view_refresh.mjs
@@ -7,15 +7,13 @@ import { app_sleep_types } from "./app_sleep_types.mjs";
 import { each } from "./each.mjs";
 import { html_p_text_centered } from "./html_p_text_centered.mjs";
 import { each_object } from "./each_object.mjs";
-import { app_sleep_today_download } from "./app_sleep_today_download.mjs";
 import { html_clear } from "./html_clear.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { json_to } from "./json_to.mjs";
 export async function app_sleep_view_refresh(parent) {
   html_clear(parent);
   let now = date_now();
-  await app_sleep_date_download(now);
-  let { month_path, data } = await app_sleep_today_download();
+  let { month_path, data } = await app_sleep_date_download(now);
   let items = list_adder((la) => {
     each_object(data, (day, data_day) => {
       html_p_text_centered(parent, day);
