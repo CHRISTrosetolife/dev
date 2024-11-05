986ad1b9c01788ca80623e7cfc13fc833227c473
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-05 18:29:09 -0500

    2024-11-05T23:29:07.191Z function_auto_after app_sleep_view_refresh

--- public/src/app_sleep_view_refresh.mjs
+++ public/src/app_sleep_view_refresh.mjs
@@ -1,3 +1,4 @@
+import { list_adder } from "./list_adder.mjs";
 import { firebase_save } from "./firebase_save.mjs";
 import { list_remove } from "./list_remove.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
@@ -12,6 +13,7 @@ import { html_clear } from "./html_clear.mjs";
 export async function app_sleep_view_refresh(parent) {
   html_clear(parent);
   let { month_path, data } = await app_sleep_today_download();
+  list_adder((la) => {});
   each_object(data, (day, data_day) => {
     html_p_text_centered(parent, day);
     each(app_sleep_types(), (sleep_type) => {
