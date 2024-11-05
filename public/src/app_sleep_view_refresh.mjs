496a275c2bc60627b2ea5dfcf15c340ddbb2c68c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-05 18:43:05 -0500

    2024-11-05T23:43:04.401Z function_auto_after app_sleep_view_refresh

--- public/src/app_sleep_view_refresh.mjs
+++ public/src/app_sleep_view_refresh.mjs
@@ -1,3 +1,4 @@
+import { date_month_previous } from "./date_month_previous.mjs";
 import { app_sleep_date_download } from "./app_sleep_date_download.mjs";
 import { date_now } from "./date_now.mjs";
 import { list_adder } from "./list_adder.mjs";
@@ -13,6 +14,7 @@ import { json_to } from "./json_to.mjs";
 export async function app_sleep_view_refresh(parent) {
   html_clear(parent);
   let now = date_now();
+  let p = date_month_previous(now);
   let { month_path, data } = await app_sleep_date_download(now);
   let items = list_adder((la) => {
     each_object(data, (day, data_day) => {
