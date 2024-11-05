11c7cdc7f5e0279e3fb268883521f7b2f39b1cda
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-05 18:37:47 -0500

    2024-11-05T23:37:46.940Z function_auto_after app_sleep_today_download

--- public/src/app_sleep_today_download.mjs
+++ public/src/app_sleep_today_download.mjs
@@ -2,5 +2,5 @@ import { app_sleep_date_download } from "./app_sleep_date_download.mjs";
 import { date_now } from "./date_now.mjs";
 export async function app_sleep_today_download() {
   let now = date_now();
-  return app_sleep_date_download(now);
+  return await app_sleep_date_download(now);
 }
