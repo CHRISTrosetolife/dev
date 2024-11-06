86a1db00f37fbe17269ab940c1d2ead9a5155c8a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-06 11:08:19 -0500

    2024-11-06T16:08:18.591Z function_auto_after string_prefix_without_multiple

--- public/src/string_prefix_without_multiple.mjs
+++ public/src/string_prefix_without_multiple.mjs
@@ -2,7 +2,7 @@ import { string_prefix_without } from "./string_prefix_without.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 export function string_prefix_without_multiple(chapter_name, prefix) {
   while (string_starts_with(chapter_name, prefix)) {
-    chapter_name = string_prefix_without(chapter_name, "0");
+    chapter_name = string_prefix_without(chapter_name, prefix);
   }
   return chapter_name;
 }
