5af286dd763c9f3f7cc3b80858e81af79d8edbe9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-06 16:03:34 -0500

    2024-11-06T21:03:33.819Z function_auto_after bible_books_chapters

--- public/src/bible_books_chapters.mjs
+++ public/src/bible_books_chapters.mjs
@@ -1,16 +1,15 @@
 import { bible_chapters_each } from "./bible_chapters_each.mjs";
 import { bible_books_each } from "./bible_books_each.mjs";
-import { bible_books_chapter_each } from "./bible_books_chapter_each.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
 export async function bible_books_chapters(bible_folder) {
   return await list_adder_async(async (la) => {
-    await bible_books_chapter_each(bible_folder);
     await bible_books_each(bible_folder, async (book_code) => {
       await bible_chapters_each(
         bible_folder,
         book_code,
         function lambda(chapter_code) {
           la({
+            book_code,
             chapter_code,
           });
         },
