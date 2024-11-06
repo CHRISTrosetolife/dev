88aa988172cefc7df94d0703214943bfa8353234
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-05 20:07:27 -0500

    2024-11-06T01:07:25.921Z function_auto_after app_sleep_view_refresh

--- public/src/app_sleep_view_refresh.mjs
+++ public/src/app_sleep_view_refresh.mjs
@@ -13,7 +13,6 @@ import { object_property_exists } from "./object_property_exists.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { app_sleep_types } from "./app_sleep_types.mjs";
 import { each } from "./each.mjs";
-import { html_p_text_centered } from "./html_p_text_centered.mjs";
 import { each_object } from "./each_object.mjs";
 import { html_clear } from "./html_clear.mjs";
 import { html_p_text } from "./html_p_text.mjs";
@@ -27,14 +26,12 @@ export async function app_sleep_view_refresh(parent) {
     await each_async(dates, async (d) => {
       let { month_path, data } = await app_sleep_date_download(d);
       each_object(data, (day, data_day) => {
-        html_p_text_centered(parent, day);
         each(app_sleep_types(), (sleep_type) => {
           let list_name = object_property_get(
             sleep_type,
             app_sleep_list_name(),
           );
           if (object_property_exists(data_day, list_name)) {
-            html_p_text_centered(parent, list_name);
             let list = object_property_get(data_day, list_name);
             each(list, (item) => {
               let entry = {
