ddedca9612a247a5664c8926e3d0294c6a3212f2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-06 11:10:03 -0500

    2024-11-06T16:10:03.123Z function_auto_after bible_chapter_folder

--- public/src/bible_chapter_folder.mjs
+++ public/src/bible_chapter_folder.mjs
@@ -1,9 +1,9 @@
-import { string_trim_front } from "./string_trim_front.mjs";
+import { bible_chapter_unpadded } from "./bible_chapter_unpadded.mjs";
 import { bible_chapter_name_parse } from "./bible_chapter_name_parse.mjs";
 import { path_join } from "./path_join.mjs";
 export function bible_chapter_folder(folder_name, chapter_name) {
   let { book_code, chapter_code } = bible_chapter_name_parse(chapter_name);
-  let chapter_number = string_trim_front(chapter_code, "0");
+  let chapter_number = bible_chapter_unpadded(chapter_code, "0");
   let p = path_join([folder_name, book_code, chapter_number, "/"]);
   return p;
 }
