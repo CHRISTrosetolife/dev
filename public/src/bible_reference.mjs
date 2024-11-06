813831f96085745e3c20759396cee9f75e83e496
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-06 11:08:55 -0500

    2024-11-06T16:08:54.928Z function_auto_after bible_reference

--- public/src/bible_reference.mjs
+++ public/src/bible_reference.mjs
@@ -1,6 +1,6 @@
-import { string_trim_front } from "./string_trim_front.mjs";
+import { string_prefix_without_multiple } from "./string_prefix_without_multiple.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function bible_reference(book_name, chapter, verse_number) {
-  chapter = string_trim_front(chapter, "0");
+  chapter = string_prefix_without_multiple(chapter, "0");
   return string_combine_multiple([book_name, " ", chapter, ":", verse_number]);
 }
