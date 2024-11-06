6998a7f59a61a9e4db880870edf4e2c1f80a937f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-05 19:58:21 -0500

    2024-11-06T00:58:20.626Z function_auto_after app_sleep_types

--- public/src/app_sleep_types.mjs
+++ public/src/app_sleep_types.mjs
@@ -1,3 +1,4 @@
+import { app_sleep_list_name } from "./app_sleep_list_name.mjs";
 import { app_sleep_sleeps } from "./app_sleep_sleeps.mjs";
 import { app_sleep_sleep } from "./app_sleep_sleep.mjs";
 import { app_sleep_wakeups } from "./app_sleep_wakeups.mjs";
@@ -6,11 +7,11 @@ export function app_sleep_types() {
   return [
     {
       label: app_sleep_woke_up(),
-      list_name: app_sleep_wakeups(),
+      [app_sleep_list_name()]: app_sleep_wakeups(),
     },
     {
       label: app_sleep_sleep(),
-      list_name: app_sleep_sleeps(),
+      [app_sleep_list_name()]: app_sleep_sleeps(),
     },
   ];
 }
