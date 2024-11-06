be06ab9681d719f940cbeb5d6a32665923b4b186
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-05 20:14:30 -0500

    2024-11-06T01:14:27.277Z function_auto_after app_sleep_view_refresh

--- public/src/app_sleep_view_refresh.mjs
+++ public/src/app_sleep_view_refresh.mjs
@@ -1,3 +1,4 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { round_2 } from "./round_2.mjs";
 import { date_now_month_previous } from "./date_now_month_previous.mjs";
 import { log } from "./log.mjs";
@@ -71,7 +72,7 @@ export async function app_sleep_view_refresh(parent) {
         html_p_text(parent, dateString);
         let hours = date_difference_hours(sleep_start, when);
         hours = round_2(hours);
-        html_p_text(parent, hours);
+        html_p_text(parent, string_combine_multiple([hours]));
         sleep_start = null;
       }
     }
