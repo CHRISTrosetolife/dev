13687f7eb749c8b95999416e1479a6111d392e66
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-05 20:02:30 -0500

    2024-11-06T01:02:29.822Z function_auto_after app_sleep_view_refresh

---
+++ public/src/app_sleep_view_refresh.mjs
@@ -0,0 +1,65 @@
+import { app_sleep_when_get } from "./app_sleep_when_get.mjs";
+import { app_sleep_when } from "./app_sleep_when.mjs";
+import { app_sleep_wakeups } from "./app_sleep_wakeups.mjs";
+import { app_sleep_list_name } from "./app_sleep_list_name.mjs";
+import { list_sort } from "./list_sort.mjs";
+import { list_adder_async } from "./list_adder_async.mjs";
+import { each_async } from "./each_async.mjs";
+import { date_month_previous } from "./date_month_previous.mjs";
+import { app_sleep_date_download } from "./app_sleep_date_download.mjs";
+import { date_now } from "./date_now.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { app_sleep_types } from "./app_sleep_types.mjs";
+import { each } from "./each.mjs";
+import { html_p_text_centered } from "./html_p_text_centered.mjs";
+import { each_object } from "./each_object.mjs";
+import { html_clear } from "./html_clear.mjs";
+import { html_p_text } from "./html_p_text.mjs";
+import { json_to } from "./json_to.mjs";
+import { app_sleep_sleeps } from "./app_sleep_sleeps.mjs";
+export async function app_sleep_view_refresh(parent) {
+  html_clear(parent);
+  let now = date_now();
+  let previous_month = date_month_previous(now);
+  let dates = [previous_month, now];
+  let items = await list_adder_async(async (la) => {
+    await each_async(dates, async (d) => {
+      let { month_path, data } = await app_sleep_date_download(d);
+      each_object(data, (day, data_day) => {
+        html_p_text_centered(parent, day);
+        each(app_sleep_types(), (sleep_type) => {
+          let list_name = object_property_get(
+            sleep_type,
+            app_sleep_list_name(),
+          );
+          if (object_property_exists(data_day, list_name)) {
+            html_p_text_centered(parent, list_name);
+            let list = object_property_get(data_day, list_name);
+            each(list, (item) => {
+              let entry = {
+                [app_sleep_when()]: item,
+                [app_sleep_list_name()]: list_name,
+              };
+              html_p_text(parent, json_to(entry));
+              la(entry);
+            });
+          }
+        });
+      });
+    });
+    list_sort(items, app_sleep_when_get);
+    let sleep_start = null;
+    each(items, (item) => {
+      let sleep_type = object_property_get(item, app_sleep_list_name());
+      if (sleep_start === null) {
+        if (sleep_type === app_sleep_sleeps()) {
+          sleep_start = when;
+        }
+      } else {
+        if (sleep_type === app_sleep_wakeups()) {
+        }
+      }
+    });
+  });
+}
