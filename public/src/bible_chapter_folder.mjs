7522fccb7fd6a9787b0f6f00c893d05537f539a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-06 11:10:09 -0500

    2024-11-06T16:10:08.640Z function_auto_after bible_chapter_folder

--- public/src/bible_chapter_folder.mjs
+++ public/src/bible_chapter_folder.mjs
@@ -3,7 +3,7 @@ import { bible_chapter_name_parse } from "./bible_chapter_name_parse.mjs";
 import { path_join } from "./path_join.mjs";
 export function bible_chapter_folder(folder_name, chapter_name) {
   let { book_code, chapter_code } = bible_chapter_name_parse(chapter_name);
-  let chapter_number = bible_chapter_unpadded(chapter_code, "0");
+  let chapter_number = bible_chapter_unpadded(chapter_code);
   let p = path_join([folder_name, book_code, chapter_number, "/"]);
   return p;
 }
