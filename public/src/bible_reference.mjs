2777f692779cc465eaecb2a7ce0ef187d63255e0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-06 11:09:52 -0500

    2024-11-06T16:09:51.652Z function_auto_after bible_reference

--- public/src/bible_reference.mjs
+++ public/src/bible_reference.mjs
@@ -1,6 +1,6 @@
-import { string_trim_front_0 } from "./string_trim_front_0.mjs";
+import { bible_chapter_unpadded } from "./bible_chapter_unpadded.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function bible_reference(book_name, chapter, verse_number) {
-  chapter = string_trim_front_0(chapter);
+  chapter = bible_chapter_unpadded(chapter);
   return string_combine_multiple([book_name, " ", chapter, ":", verse_number]);
 }
