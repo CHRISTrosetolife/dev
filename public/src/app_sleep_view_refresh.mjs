677ad4fe13c6bfccad75122aa85915ff6b858a77
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-05 20:09:02 -0500

    2024-11-06T01:09:00.627Z function_auto_after app_sleep_view_refresh

--- public/src/app_sleep_view_refresh.mjs
+++ public/src/app_sleep_view_refresh.mjs
@@ -63,6 +63,7 @@ export async function app_sleep_view_refresh(parent) {
           day: "numeric",
         };
         let dateString = today.toLocaleDateString("en-US", options);
+        html_p_text(parent, dateString);
         let hours = date_difference_hours(sleep_start, when);
         html_p_text(parent, hours);
         sleep_start = null;
