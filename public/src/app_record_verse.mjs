d2e538b52534e78b00e35d819d9fe8dc2285e513
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-07 06:21:36 -0500

    2024-11-07T11:21:35.267Z function_auto_after app_record_verse

--- public/src/app_record_verse.mjs
+++ public/src/app_record_verse.mjs
@@ -54,7 +54,7 @@ export async function app_record_verse(
     recording,
     recording_not,
     review;
-  let blob;
+  let blob = null;
   start = html_button_width_full_text_click(root, "⏺️ start recording", () => {
     each(recording_not, html_style_display_none);
     html_recorder_media_start(context.mr);
