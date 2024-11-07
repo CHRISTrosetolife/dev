664391f8740c0adf46027ccdb2a9cebaa021b46f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-07 06:42:01 -0500

    2024-11-07T11:41:58.148Z function_auto_after html_button_width_full_text_click_br

--- public/src/html_button_width_full_text_click_br.mjs
+++ public/src/html_button_width_full_text_click_br.mjs
@@ -1,6 +1,7 @@
 import { html_br } from "./html_br.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function html_button_width_full_text_click_br(parent, text, lambda) {
-  html_button_width_full_text_click(parent, text, lambda);
+  let result = html_button_width_full_text_click(parent, text, lambda);
   html_br(parent);
+  return result;
 }
