ebff602f3b87572dda4888ad6df824053d9e26b8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-07 16:01:00 -0500

    2024-11-07T21:00:59.506Z f

--- public/src/lesson_console_log_analogy_addition.mjs
+++ public/src/lesson_console_log_analogy_addition.mjs
@@ -4,14 +4,14 @@ import { html_p_text } from "./html_p_text.mjs";
 export function lesson_console_log_analogy_addition(parent) {
   html_p_text(
     parent,
-    "suppose two numbers are added together to get their sum",
+    "Suppose two numbers are added together to get their sum.",
   );
-  html_p_text(parent, "we could say :");
+  html_p_text(parent, "We could say:");
   let ol1 = html_ul(parent);
-  html_li_text(ol1, "the adding of the two numbers together is a process");
-  html_li_text(ol1, "the two numbers are the inputs to the addition process");
+  html_li_text(ol1, "The adding of the two numbers together is a process.");
+  html_li_text(ol1, "The two numbers are the inputs to the addition process.");
   html_li_text(
     ol1,
-    "the sum of the two numbers is the output of the addition process",
+    "The sum of the two numbers is the output of the addition process.",
   );
 }
