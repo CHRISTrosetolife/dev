40ab254b43de10c1a83acc4d1b18184355326574
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-09 19:20:40 -0500

    2024-11-10T00:20:38.352Z function_auto_after bible_audio_download_divinerevelations_esv

--- public/src/bible_audio_download_divinerevelations_esv.mjs
+++ public/src/bible_audio_download_divinerevelations_esv.mjs
@@ -9,7 +9,10 @@ import { string_prefix_without } from "./string_prefix_without.mjs";
 export async function bible_audio_download_divinerevelations_esv() {
   let prefix_url =
     "https://www.divinerevelations.info/documents/bible/english_mp3_bible/";
-  let url = string_combine_multiple([prefix_url, "p/pt/pt00.htm"]);
+  let url = string_combine_multiple([
+    prefix_url,
+    "english_standard_version_v2/",
+  ]);
   let p = await http_cache_parse_parsed(url);
   let { root } = p;
   let prefix = "../../";
