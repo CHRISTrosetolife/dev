235a4cf1353fb536078e280cc36b8318b4551a3b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-09 19:33:20 -0500

    2024-11-10T00:33:20.254Z function_auto_after html_parse_href

--- public/src/html_parse_href.mjs
+++ public/src/html_parse_href.mjs
@@ -1,8 +1,4 @@
 import { object_property_get } from "./object_property_get.mjs";
 export function html_parse_href(i) {
-  return object_property_get(
-    i.attribs,
-    object_property_get(object, "property_name"),
-    "href",
-  );
+  return object_property_get(object_property_get(i, "attribs"), "href");
 }
