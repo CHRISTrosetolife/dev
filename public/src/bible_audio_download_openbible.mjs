8fed96325e819f6a85187ff3ba9badef894d7cb1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 06:00:17 -0500

    2024-11-10T11:00:16.201Z c bible_audio_download_divinerevelations bible_audio_download_openbible

---
+++ public/src/bible_audio_download_openbible.mjs
@@ -0,0 +1,18 @@
+import { bible_audio_download_mp3 } from "./bible_audio_download_mp3.mjs";
+import { log } from "./log.mjs";
+import { each_async } from "./each_async.mjs";
+export async function bible_audio_download_openbible() {
+  let prefix_url =
+    "https://www.divinerevelations.info/documents/bible/english_mp3_bible/";
+  let versions = [
+    "english_standard_version_v2",
+    "english_new_revised_standard_version_drama",
+    "english_contemporary_english_version_drama",
+  ];
+  await each_async(versions, async (version) => {
+    let r = await bible_audio_download_mp3(prefix_url, version);
+    log({
+      r,
+    });
+  });
+}
