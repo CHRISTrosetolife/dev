babf28f59346f4197e0d895843431ccb1b30820c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 05:41:36 -0500

    2024-11-10T10:41:35.966Z function_auto_after bible_audio_download_divinerevelations_esv

--- public/src/bible_audio_download_divinerevelations_esv.mjs
+++ public/src/bible_audio_download_divinerevelations_esv.mjs
@@ -1,6 +1,5 @@
 import { bible_audio_download_divinerevelations_generic } from "./bible_audio_download_divinerevelations_generic.mjs";
 export async function bible_audio_download_divinerevelations_esv() {
-  return await bible_audio_download_divinerevelations_generic(
-    "english_standard_version_v2",
-  );
+  let version = "english_standard_version_v2";
+  return await bible_audio_download_divinerevelations_generic(version);
 }
