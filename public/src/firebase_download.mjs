721e460e31324e456d48873607f4404b2696b1d8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 14:41:36 -0500

    2024-11-10T19:41:34.685Z function_auto_after firebase_download

--- public/src/firebase_download.mjs
+++ public/src/firebase_download.mjs
@@ -4,17 +4,13 @@ import {
   getStorage,
   ref,
   getDownloadURL,
-  get,
 } from "https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.2/firebase-storage.min.js";
 export async function firebase_download(storage_path) {
   assert_arguments_length(arguments, 1);
   let storage = getStorage();
   let storageRef = ref(storage, storage_path);
-  let snapshot = await get(storageRef);
-  if (snapshot.exists()) {
-    return null;
-  }
-  return snapshot.val();
-  let url = await getDownloadURL(storageRef);
-  return await http_get(url);
+  try {
+    let url = await getDownloadURL(storageRef);
+    return await http_get(url);
+  } catch (e) {}
 }
