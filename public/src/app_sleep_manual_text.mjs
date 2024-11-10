a2bc0f29b3cab4337b0f3753f679697b99979341
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 14:27:07 -0500

    2024-11-10T19:27:06.530Z function_auto_after app_sleep_manual_text

--- public/src/app_sleep_manual_text.mjs
+++ public/src/app_sleep_manual_text.mjs
@@ -1,5 +1,6 @@
 import { emoji_add } from "./emoji_add.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_sleep_manual_text() {
-  return string_combine_multiple([emoji_add(), " add ", "manual"]);
+  let after = "manual";
+  return string_combine_multiple([emoji_add(), " add ", after]);
 }
