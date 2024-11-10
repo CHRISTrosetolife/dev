f6590ab30bf4bb9a85c787c9afdc9b4da170f4e2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 05:41:14 -0500

    2024-11-10T10:41:13.833Z function_auto_after bible_audio_download_divinerevelations_esv

--- public/src/bible_audio_download_divinerevelations_esv.mjs
+++ public/src/bible_audio_download_divinerevelations_esv.mjs
@@ -1,21 +1,5 @@
-import { bible_audio_download_generic } from "./bible_audio_download_generic.mjs";
-import { html_parse_a_href_ends_with_hrefs } from "./html_parse_a_href_ends_with_hrefs.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { string_combine } from "./string_combine.mjs";
+import { bible_audio_download_divinerevelations_generic } from "./bible_audio_download_divinerevelations_generic.mjs";
 export async function bible_audio_download_divinerevelations_esv() {
   let version = "english_standard_version_v2";
-  let prefix_url =
-    "https://www.divinerevelations.info/documents/bible/english_mp3_bible/";
-  let url = string_combine_multiple([prefix_url, version, "/"]);
-  return await bible_audio_download_generic(
-    url,
-    bible_audio_download_hrefs_get,
-    bible_audio_download_hrefs_map,
-  );
-  function bible_audio_download_hrefs_get(root) {
-    return html_parse_a_href_ends_with_hrefs(root, ".mp3");
-  }
-  function bible_audio_download_hrefs_map(href) {
-    return string_combine(url, href);
-  }
+  return bible_audio_download_divinerevelations_generic(version);
 }
