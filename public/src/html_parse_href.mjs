89091219768e6e9d43d3bfb94a103bc3bcf1959e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-09 19:33:13 -0500

    2024-11-10T00:33:12.676Z function_auto_after html_parse_href

--- public/src/html_parse_href.mjs
+++ public/src/html_parse_href.mjs
@@ -1,4 +1,8 @@
 import { object_property_get } from "./object_property_get.mjs";
 export function html_parse_href(i) {
-  return object_property_get(i.attribs, "href");
+  return object_property_get(
+    i.attribs,
+    object_property_get(object, "property_name"),
+    "href",
+  );
 }
