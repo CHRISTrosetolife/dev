eed95ceef30075a207500e20c733e04a50548e82
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 14:21:32 -0500

    2024-11-10T19:21:30.074Z function_auto_after app_mary

--- public/src/app_mary.mjs
+++ public/src/app_mary.mjs
@@ -6,12 +6,12 @@ import { clipboard_copy_web } from "./clipboard_copy_web.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_mary() {
   let body = html_style_default_initialize();
-  let text = await clipboard_paste_web();
+  let prayer = await clipboard_paste_web();
   let response =
     "Behold, I am an unworthy servant of the LORD. May the word of the LORD come true.";
   await clipboard_copy_web(response);
   html_p_text(body, "Response copied to clipboard:");
   html_p_text(body, response);
   let t = html_textarea(body);
-  html_value_set();
+  html_value_set(t, prayer);
 }
