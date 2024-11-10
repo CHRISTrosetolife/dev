06ce335e5a42bff2e8fc881f9fb74ecf75769690
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 14:18:48 -0500

    2024-11-10T19:18:48.114Z function_auto_after app_mary

--- public/src/app_mary.mjs
+++ public/src/app_mary.mjs
@@ -2,12 +2,12 @@ import { html_textarea } from "./html_textarea.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { clipboard_copy_web } from "./clipboard_copy_web.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-export function app_mary() {
+export async function app_mary() {
   let body = html_style_default_initialize();
-  const text =  navigator.clipboard.readText();
+  let text = navigator.clipboard.readText();
   let response =
     "Behold, I am an unworthy servant of the LORD. May the word of the LORD come true.";
-  clipboard_copy_web(response);
+  await clipboard_copy_web(response);
   html_p_text(body, "Response copied to clipboard:");
   html_p_text(body, response);
   html_textarea(body);
