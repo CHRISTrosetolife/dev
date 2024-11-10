3bbd4ba0ba54f938102dc780410f25f9a9c7a80c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 05:42:18 -0500

    2024-11-10T10:42:18.163Z r bible_audio_download_divinerevelations_esv bible_audio_download_divinerevelations_multiple

--- public/src/bible_audio_download_divinerevelations_multiple.mjs
+++ public/src/bible_audio_download_divinerevelations_multiple.mjs
@@ -1,6 +1,6 @@
 import { each_async } from "./each_async.mjs";
 import { bible_audio_download_divinerevelations_generic } from "./bible_audio_download_divinerevelations_generic.mjs";
-export async function bible_audio_download_divinerevelations_esv() {
+export async function bible_audio_download_divinerevelations_multiple() {
   let versions = ["english_standard_version_v2"];
   await each_async(versions, async (version) => {
     await bible_audio_download_divinerevelations_generic(version);
