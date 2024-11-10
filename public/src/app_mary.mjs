0aeac0b40ff5bf80ff66d306be1411980ee23b0d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 14:29:12 -0500

    2024-11-10T19:29:12.422Z function_auto_after app_mary

--- public/src/app_mary.mjs
+++ public/src/app_mary.mjs
@@ -1,10 +1,10 @@
+import { html_button_add } from "./html_button_add.mjs";
 import { html_textarea_width_full } from "./html_textarea_width_full.mjs";
 import { html_value_set } from "./html_value_set.mjs";
 import { clipboard_paste_web } from "./clipboard_paste_web.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { clipboard_copy_web } from "./clipboard_copy_web.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export async function app_mary() {
   let body = html_style_default_initialize();
   let prayer = await clipboard_paste_web();
@@ -15,5 +15,5 @@ export async function app_mary() {
   html_p_text(body, response);
   let t = html_textarea_width_full(body);
   html_value_set(t, prayer);
-  html_button_width_full_text_click();
+  html_button_add(root, "word", download);
 }
