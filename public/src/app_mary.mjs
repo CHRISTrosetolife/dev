d8ac606b02d3ef7ad1f56bdd8fbc675d06ce3580
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 14:30:48 -0500

    2024-11-10T19:30:48.145Z function_auto_after app_mary

--- public/src/app_mary.mjs
+++ public/src/app_mary.mjs
@@ -5,19 +5,17 @@ import { html_value_set } from "./html_value_set.mjs";
 import { clipboard_paste_web } from "./clipboard_paste_web.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { clipboard_copy_web } from "./clipboard_copy_web.mjs";
-import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_mary() {
   await app_firebase({
     on_logged_in: async (context) => {
-      let root = context;
-      let body = html_style_default_initialize();
+      let { root } = context;
       let prayer = await clipboard_paste_web();
       let response =
         "Behold, I am an unworthy servant of the LORD. May the word of the LORD come true.";
       await clipboard_copy_web(response);
-      html_p_text(body, "Response copied to clipboard:");
-      html_p_text(body, response);
-      let t = html_textarea_width_full(body);
+      html_p_text(root, "Response copied to clipboard:");
+      html_p_text(root, response);
+      let t = html_textarea_width_full(root);
       html_value_set(t, prayer);
       html_button_add(root, "word", () => {});
     },
