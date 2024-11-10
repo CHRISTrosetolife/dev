b17ef75cc38464bc79ae1d9587bfb62e0e762a7c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-09 19:21:49 -0500

    2024-11-10T00:21:48.742Z function_auto_after html_parse_a_href_starts_with

--- public/src/html_parse_a_href_starts_with.mjs
+++ public/src/html_parse_a_href_starts_with.mjs
@@ -1,13 +1,9 @@
-import { list_adder_visit } from "./list_adder_visit.mjs";
-import { html_parse_visit_tag_attribute } from "./html_parse_visit_tag_attribute.mjs";
-import { list_adder } from "./list_adder.mjs";
+import { html_parse_a_hrefs } from "./html_parse_a_hrefs.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { list_filter } from "./list_filter.mjs";
 export function html_parse_a_href_starts_with(element, prefix) {
-  let found = list_adder((la) =>
-    html_parse_visit_tag_attribute(element, "a", "href", list_adder_visit(la)),
-  );
+  let found = html_parse_a_hrefs(element);
   let filtered = list_filter(found, (i) => {
     let href = html_parse_href(i);
     return string_starts_with(href, prefix);
