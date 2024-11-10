db522b7d58a00dd4c950f1e1f80ff697900b7130
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 14:14:56 -0500

    2024-11-10T19:14:55.547Z function_auto_after app_mary

--- public/src/app_mary.mjs
+++ public/src/app_mary.mjs
@@ -2,7 +2,7 @@ import { clipboard_copy_web } from "./clipboard_copy_web.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_mary() {
   let body = html_style_default_initialize();
-  clipboard_copy_web(
-    "Behold, I am an unworthy servant of the LORD. May the word of the LORD come true.",
-  );
+  let response =
+    "Behold, I am an unworthy servant of the LORD. May the word of the LORD come true.";
+  clipboard_copy_web(response);
 }
