ffbebf5d609451a9cc570c11961ddab9c4ba49eb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 14:08:26 -0500

    2024-11-10T19:08:26.238Z function_auto_after bible_audio_download_divinerevelations

--- public/src/bible_audio_download_divinerevelations.mjs
+++ public/src/bible_audio_download_divinerevelations.mjs
@@ -9,7 +9,7 @@ export async function bible_audio_download_divinerevelations() {
     "english_contemporary_english_version_drama",
   ];
   await each_async(versions, async (version) => {
-    let r = await bible_audio_download_mp3(prefix_url, version);
-    await each_async(list, async (item) => {});
+    let chapter_paths = await bible_audio_download_mp3(prefix_url, version);
+    await each_async(chapter_paths, async (chapter_path) => {});
   });
 }
