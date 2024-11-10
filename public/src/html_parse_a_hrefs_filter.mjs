5a8274ba524b0209c85de7bda0bb48deb90f596f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-09 19:37:15 -0500

    2024-11-10T00:37:15.146Z function_auto_after html_parse_a_hrefs_filter

--- public/src/html_parse_a_hrefs_filter.mjs
+++ public/src/html_parse_a_hrefs_filter.mjs
@@ -1,4 +1,7 @@
+import { html_parse_href } from "./html_parse_href.mjs";
+import { list_map } from "./list_map.mjs";
 import { html_parse_a_hrefs_nodes_filter } from "./html_parse_a_hrefs_nodes_filter.mjs";
 export function html_parse_a_hrefs_filter(root, condition) {
   let nodes = html_parse_a_hrefs_nodes_filter(root, condition);
+  return list_map(nodes, html_parse_href);
 }
