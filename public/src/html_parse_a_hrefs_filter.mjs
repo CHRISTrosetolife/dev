9d89fbb5cfd95ea1299efd0ca8e8e4542fab1ebd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-09 19:23:07 -0500

    2024-11-10T00:23:05.985Z function_auto_after html_parse_a_href_starts_with

---
+++ public/src/html_parse_a_hrefs_filter.mjs
@@ -0,0 +1,18 @@
+import { html_parse_href } from "./html_parse_href.mjs";
+import { html_parse_visit_tag } from "./html_parse_visit_tag.mjs";
+import { list_adder } from "./list_adder.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
+export function html_parse_a_hrefs_filter(root, condition) {
+  return list_adder((la) =>
+    html_parse_visit_tag(root, "a", (v) => {
+      let { node } = v;
+      let { attribs } = node;
+      if (object_property_exists(attribs, "href")) {
+        let href = html_parse_href(node);
+        if (condition(href)) {
+          la(href);
+        }
+      }
+    }),
+  );
+}
