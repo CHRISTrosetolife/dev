23953f9726adfae595848911ce3aeefe9c10eaf4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-09 19:20:10 -0500

    2024-11-10T00:20:07.216Z c bible_hebrew_audio_download bible_audio_download_divinerevelations_esv

---
+++ public/src/bible_audio_download_divinerevelations_esv.mjs
@@ -0,0 +1,24 @@
+import { list_map_async } from "./list_map_async.mjs";
+import { list_map } from "./list_map.mjs";
+import { http_file } from "./http_file.mjs";
+import { html_parse_a_href_starts_with_hrefs } from "./html_parse_a_href_starts_with_hrefs.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { http_cache_parse_parsed } from "./http_cache_parse_parsed.mjs";
+import { string_combine } from "./string_combine.mjs";
+import { string_prefix_without } from "./string_prefix_without.mjs";
+export async function bible_audio_download_divinerevelations_esv() {
+  let prefix_url = "https://mechon-mamre.org/";
+  let url = string_combine_multiple([prefix_url, "p/pt/pt00.htm"]);
+  let p = await http_cache_parse_parsed(url);
+  let { root } = p;
+  let prefix = "../../";
+  let hrefs = html_parse_a_href_starts_with_hrefs(
+    root,
+    string_combine_multiple([prefix, "mp3/"]),
+  );
+  let urls = list_map(hrefs, (h) =>
+    string_combine(prefix_url, string_prefix_without(h, prefix)),
+  );
+  let locations = await list_map_async(urls, http_file);
+  return locations;
+}
