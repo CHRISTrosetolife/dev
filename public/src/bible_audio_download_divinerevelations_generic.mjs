5bd44b4cf38894030f14181f5669d1b849c9df33
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 05:55:34 -0500

    2024-11-10T10:55:33.633Z de bible_audio_download_divinerevelations_generic

--- public/src/bible_audio_download_divinerevelations_generic.mjs
+++
@@ -1,6 +0,0 @@
-import { bible_audio_download_mp3 } from "./bible_audio_download_mp3.mjs";
-export async function bible_audio_download_divinerevelations_generic(version) {
-  let prefix_url =
-    "https://www.divinerevelations.info/documents/bible/english_mp3_bible/";
-  return bible_audio_download_mp3(prefix_url, version);
-}
