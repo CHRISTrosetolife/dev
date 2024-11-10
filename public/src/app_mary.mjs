3baa8273760c4101cdbdcbb2e270f797dd08d2ae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 14:21:12 -0500

    2024-11-10T19:21:10.866Z function_auto_after app_mary

--- public/src/app_mary.mjs
+++ public/src/app_mary.mjs
@@ -1,3 +1,4 @@
+import { html_value_set } from "./html_value_set.mjs";
 import { clipboard_paste_web } from "./clipboard_paste_web.mjs";
 import { html_textarea } from "./html_textarea.mjs";
 import { html_p_text } from "./html_p_text.mjs";
@@ -11,5 +12,6 @@ export async function app_mary() {
   await clipboard_copy_web(response);
   html_p_text(body, "Response copied to clipboard:");
   html_p_text(body, response);
-  html_textarea(body);
+  let t = html_textarea(body);
+  html_value_set();
 }
