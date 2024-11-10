b06b84a60426126f7a4176eef0711bb21f6c1b39
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-09 19:26:25 -0500

    2024-11-10T00:26:24.689Z function_auto_after bible_audio_download_divinerevelations_esv

--- public/src/bible_audio_download_divinerevelations_esv.mjs
+++ public/src/bible_audio_download_divinerevelations_esv.mjs
@@ -5,7 +5,6 @@ import { http_file } from "./http_file.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { http_cache_parse_parsed } from "./http_cache_parse_parsed.mjs";
 import { string_combine } from "./string_combine.mjs";
-import { string_prefix_without } from "./string_prefix_without.mjs";
 export async function bible_audio_download_divinerevelations_esv() {
   let prefix_url =
     "https://www.divinerevelations.info/documents/bible/english_mp3_bible/";
@@ -15,11 +14,8 @@ export async function bible_audio_download_divinerevelations_esv() {
   ]);
   let p = await http_cache_parse_parsed(url);
   let { root } = p;
-  let prefix = "../../";
   let hrefs = html_parse_a_href_ends_with_hrefs(root, ".mp3");
-  let urls = list_map(hrefs, (h) =>
-    string_combine(prefix_url, string_prefix_without(h, prefix)),
-  );
+  let urls = list_map(hrefs, (h) => string_combine(url, h));
   let locations = await list_map_async(urls, http_file);
   return locations;
 }
