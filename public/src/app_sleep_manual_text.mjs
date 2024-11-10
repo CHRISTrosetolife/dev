8d9ae9f5d0908797aaa8ab162fcb9a975fbf3bb1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 14:27:41 -0500

    2024-11-10T19:27:40.226Z function_auto_after app_sleep_manual_text

--- public/src/app_sleep_manual_text.mjs
+++ public/src/app_sleep_manual_text.mjs
@@ -1,5 +1,4 @@
 import { button_add_text } from "./button_add_text.mjs";
-export function app_sleep_manual_text() {$ag
-  let after = "manual";
-  return button_add_text(after);
+export function app_sleep_manual_text() {
+  return button_add_text("manual");
 }
