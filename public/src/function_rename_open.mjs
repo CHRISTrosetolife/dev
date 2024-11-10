3ccd5504e1068a31b89b182062bae78e1dc1a3c5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 05:59:24 -0500

    2024-11-10T10:59:24.115Z function_auto_after function_rename_open

--- public/src/function_rename_open.mjs
+++ public/src/function_rename_open.mjs
@@ -1,7 +1,7 @@
-import { function_name_to_path } from "./function_name_to_path.mjs";
+import { function_open } from "./function_open.mjs";
 import { function_rename } from "./function_rename.mjs";
 export async function function_rename_open(fn_name_from, fn_name_to) {
   let r = await function_rename(fn_name_from, fn_name_to);
-  let fn_path_to = function_name_to_path(fn_name_to);
+  await function_open(fn_name_to);
   return r;
 }
