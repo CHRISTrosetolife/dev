688b6acf17b6c6ed05ef877f0cf743624f1bfd77
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 14:17:07 -0500

    2024-11-10T19:17:06.180Z function_auto_after app_mary

--- public/src/app_mary.mjs
+++ public/src/app_mary.mjs
@@ -1,3 +1,4 @@
+import { html_textarea } from "./html_textarea.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { clipboard_copy_web } from "./clipboard_copy_web.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
@@ -8,4 +9,5 @@ export function app_mary() {
   clipboard_copy_web(response);
   html_p_text(body, "Response copied to clipboard:");
   html_p_text(body, response);
+  html_textarea();
 }
