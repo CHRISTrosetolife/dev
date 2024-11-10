d4368ce41a4c9af9802e2c1e55eadf3d955c0bb9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-09 19:23:33 -0500

    2024-11-10T00:23:33.524Z function_auto_after html_parse_a_href_ends_with

--- public/src/html_parse_a_href_ends_with.mjs
+++ public/src/html_parse_a_href_ends_with.mjs
@@ -1,12 +1,12 @@
+import { string_ends_with } from "./string_ends_with.mjs";
 import { html_parse_a_hrefs } from "./html_parse_a_hrefs.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
-import { string_starts_with } from "./string_starts_with.mjs";
 import { list_filter } from "./list_filter.mjs";
-export function html_parse_a_href_ends_with(element, prefix) {
+export function html_parse_a_href_ends_with(element, suffix) {
   let found = html_parse_a_hrefs(element);
   let filtered = list_filter(found, (i) => {
     let href = html_parse_href(i);
-    return string_starts_with(href, prefix);
+    return string_ends_with(href, suffix);
   });
   return filtered;
 }
