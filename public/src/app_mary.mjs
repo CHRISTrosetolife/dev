c19c30c579d47c37c55417a50402f505089b17fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 14:19:51 -0500

    2024-11-10T19:19:49.852Z function_auto_after app_mary

--- public/src/app_mary.mjs
+++ public/src/app_mary.mjs
@@ -4,7 +4,7 @@ import { clipboard_copy_web } from "./clipboard_copy_web.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_mary() {
   let body = html_style_default_initialize();
-  let text = navigator.clipboard.readText();
+  let text = clipboard_past_web();
   let response =
     "Behold, I am an unworthy servant of the LORD. May the word of the LORD come true.";
   await clipboard_copy_web(response);
