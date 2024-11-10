1b2809246d6d3f917dae1590bbdd1b5858f61275
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-09 19:23:08 -0500

    2024-11-10T00:23:06.928Z function_auto_after html_parse_a_href_ends_with

--- public/src/html_parse_a_href_starts_with.mjs
+++ public/src/html_parse_a_href_starts_with.mjs
@@ -4,10 +4,14 @@ import { string_starts_with } from "./string_starts_with.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { tautology } from "./tautology.mjs";
 export function html_parse_a_href_starts_with(element, prefix) {
-  let found = html_parse_a_hrefs_filter(element, tautology);
+  let found = html_parse_a_hrefs(element);
   let filtered = list_filter(found, (i) => {
     let href = html_parse_href(i);
     return string_starts_with(href, prefix);
   });
   return filtered;
 }
+function html_parse_a_hrefs(element) {
+    return html_parse_a_hrefs_filter(element, tautology);
+}
+
