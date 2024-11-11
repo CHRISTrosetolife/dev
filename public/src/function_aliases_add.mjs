cc83f1b542561923428d1879875369dba80c7329
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 17:45:51 -0500

    2024-11-11T22:45:50.964Z function_auto_after function_aliases_add

--- public/src/function_aliases_add.mjs
+++ public/src/function_aliases_add.mjs
@@ -5,13 +5,16 @@ import { app_dev_screens } from "./app_dev_screens.mjs";
 import { app_list_add } from "./app_list_add.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
+import { string_delimit } from "./string_delimit.mjs";
 export async function function_aliases_add(aliases, function_name) {
   let split = string_split_comma(aliases);
   let list_code = js_code_array_string(split);
   await app_list_add(app_dev_screens, function inserted_code() {
     let c = js_code_statement_call_args(fn_name("function_aliases_register"), [
       "all",
-      js_code_statement_call_args(fn_name("fn_name"), [function_name]),
+      js_code_statement_call_args(fn_name("fn_name"), [
+        string_delimit(function_name),
+      ]),
       list_code,
     ]);
     log({
