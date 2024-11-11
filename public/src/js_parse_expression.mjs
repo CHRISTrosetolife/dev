2ca7cfedd85e23528c239a5bae1e18d1ae26adf5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 17:07:34 -0500

    2024-11-11T22:07:33.204Z function_auto_after js_parse_expression

--- public/src/js_parse_expression.mjs
+++ public/src/js_parse_expression.mjs
@@ -1,7 +1,10 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { js_parse_first } from "./js_parse_first.mjs";
 import { js_code_statement } from "./js_code_statement.mjs";
-export function js_parse_expression(variable_name) {
-  const identifier_code = js_code_statement(`(${variable_name})`);
+export function js_parse_expression(code) {
+  let identifier_code = js_code_statement(
+    string_combine_multiple(["(", code, ")"]),
+  );
   let parsed2 = js_parse_first(identifier_code);
   let { expression } = parsed2;
   return expression;
