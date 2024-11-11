446028fbff2a1e2e375717e0e7c6f03bfab14eb3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 16:55:58 -0500

    2024-11-11T21:55:57.492Z function_auto_after function_aliases_generate_upgrade

--- public/src/function_aliases_generate_upgrade.mjs
+++ public/src/function_aliases_generate_upgrade.mjs
@@ -3,5 +3,6 @@ import { function_transform_args_split_lambda } from "./function_transform_args_
 export async function function_aliases_generate_upgrade() {
   await function_transform_args_split_lambda(
     fn_name("function_aliases_generate"),
+    [(ast) => {}],
   );
 }
