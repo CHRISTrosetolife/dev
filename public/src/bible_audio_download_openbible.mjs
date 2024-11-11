776caca27f9f9294f2c279ae2ba014c6021ce849
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 10:37:49 -0500

    2024-11-11T15:37:47.090Z function_auto_after bible_audio_download_openbible

--- public/src/bible_audio_download_openbible.mjs
+++ public/src/bible_audio_download_openbible.mjs
@@ -1,14 +1,10 @@
+import { bible_audio_download_openbible_versions } from "./bible_audio_download_openbible_versions.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
 import { bible_audio_download_mp3_multiple } from "./bible_audio_download_mp3_multiple.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
 export async function bible_audio_download_openbible() {
   let prefix_url = "https://openbible.com/audio/";
-  let versions = [
-    "gilbert_books",
-    "hays_books",
-    "souer_books",
-    "gilbert_music_books",
-  ];
+  let versions = bible_audio_download_openbible_versions();
   await bible_audio_download_mp3_multiple(
     prefix_url,
     versions,
---
+++ public/src/bible_audio_download_openbible_versions.mjs
@@ -0,0 +1,3 @@
+export function bible_audio_download_openbible_versions() {
+  return ["gilbert_books", "hays_books", "souer_books", "gilbert_music_books"];
+}
