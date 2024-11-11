999ee2429d19be7c3e8a3611fcdf6d394b944c2e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 15:05:27 -0500

    2024-11-11T20:05:26.727Z function_auto_after functions_rename_identifier

--- public/src/functions_rename_identifier.mjs
+++ public/src/functions_rename_identifier.mjs
@@ -1,6 +1,6 @@
+import { function_transform_args_split_lambda } from "./function_transform_args_split_lambda.mjs";
 import { functions_identifier_list } from "./functions_identifier_list.mjs";
 import { each_async } from "./each_async.mjs";
-import { function_transform_args_split } from "./function_transform_args_split.mjs";
 import { fn_name } from "./fn_name.mjs";
 export async function functions_rename_identifier(
   identifier_from,
@@ -8,9 +8,10 @@ export async function functions_rename_identifier(
 ) {
   let matches = await functions_identifier_list(identifier_from);
   await each_async(matches, async (m) => {
-    await function_transform_args_split([fn_name("js_identifier_rename")], m, [
-      identifier_from,
-      identifier_to,
-    ]);
+    await function_transform_args_split_lambda(
+      [fn_name("js_identifier_rename")],
+      m,
+      [identifier_from, identifier_to],
+    );
   });
 }
