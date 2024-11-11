145b0f251913a349b02921e237dbd8aea073c66f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 14:53:13 -0500

    2024-11-11T19:53:12.769Z function_auto_after function_rename_identifier

--- public/src/function_rename_identifier.mjs
+++ public/src/function_rename_identifier.mjs
@@ -1,6 +1,8 @@
+import { function_transform_all_args } from "./function_transform_all_args.mjs";
 export async function function_rename_identifier(
   identifier_from,
   identifier_to,
 ) {
+  await function_transform_all_args();
   return r;
 }
