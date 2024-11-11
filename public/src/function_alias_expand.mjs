ed95dcbb126661dc9d0fbcb3ccc5f25ad604791e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 17:16:37 -0500

    2024-11-11T22:16:36.040Z function_auto_after function_alias_expand

--- public/src/function_alias_expand.mjs
+++ public/src/function_alias_expand.mjs
@@ -1,5 +1,5 @@
-import { function_aliases_generate } from "./function_aliases_generate.mjs";
+import { function_aliases } from "./function_aliases.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function function_alias_expand(alias) {
-  return object_property_get(function_aliases_generate(), alias);
+  return object_property_get(function_aliases(), alias);
 }
