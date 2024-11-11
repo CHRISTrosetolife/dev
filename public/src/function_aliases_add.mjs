50ec294527a5308f1823443691ee3f2a6981d039
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 17:42:56 -0500

    2024-11-11T22:42:55.292Z function_auto_after function_aliases_add

--- public/src/function_aliases_add.mjs
+++ public/src/function_aliases_add.mjs
@@ -1,3 +1,4 @@
+import { js_code_brackets_inside } from "./js_code_brackets_inside.mjs";
 import { list_map } from "./list_map.mjs";
 import { js_code_statement_call_args } from "./js_code_statement_call_args.mjs";
 import { app_dev_screens } from "./app_dev_screens.mjs";
@@ -6,12 +7,11 @@ import { fn_name } from "./fn_name.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 import { string_delimit } from "./string_delimit.mjs";
 import { list_join_comma_space } from "./list_join_comma_space.mjs";
-import { js_code_brackets } from "./js_code_brackets.mjs";
 export async function function_aliases_add(aliases, function_name) {
   let split = string_split_comma(aliases);
   let mapped = list_map(split, (s) => string_delimit(s));
   let joined = list_join_comma_space(mapped);
-  js_code_brackets()
+  js_code_brackets_inside();
   await app_list_add(app_dev_screens, function inserted_code() {
     return js_code_statement_call_args(fn_name("function_aliases_register"), [
       "all",
