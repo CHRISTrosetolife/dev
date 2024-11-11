ce75336f5236b8991baa10f5b5fef50fa9ec6a59
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 16:44:54 -0500

    2024-11-11T21:44:53.696Z function_auto_after object_property_change

--- public/src/object_property_change.mjs
+++ public/src/object_property_change.mjs
@@ -3,6 +3,6 @@ import { object_property_get } from "./object_property_get.mjs";
 export function object_property_change(object, property_name, change) {
   let existing = object_property_get(object, property_name);
   let changed = change(existing);
-  object_property_set(object, changed);
+  object_property_set(object, property_name, changed);
   return changed;
 }
