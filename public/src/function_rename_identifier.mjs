e2036a813ab1477e687f6177a3babf4a90336618
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 14:52:05 -0500

    2024-11-11T19:52:02.308Z c function_rename_open function_rename_identifier

---
+++ public/src/function_rename_identifier.mjs
@@ -0,0 +1,7 @@
+import { function_open } from "./function_open.mjs";
+import { function_rename } from "./function_rename.mjs";
+export async function function_rename_identifier(fn_name_from, fn_name_to) {
+  let r = await function_rename(fn_name_from, fn_name_to);
+  await function_open(fn_name_to);
+  return r;
+}
