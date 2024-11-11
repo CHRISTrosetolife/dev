96212cc1467421edb258dfe77a65f9982f15abc8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 16:54:53 -0500

    2024-11-11T21:54:52.851Z function_auto_after function_aliases_generate_upgrade

--- public/src/function_aliases_generate_upgrade.mjs
+++ public/src/function_aliases_generate_upgrade.mjs
@@ -1,1 +1,4 @@
-export function function_aliases_generate_upgrade() {}
+import { function_transform_args_split_split } from "./function_transform_args_split_split.mjs";
+export async function function_aliases_generate_upgrade() {
+  await function_transform_args_split_split();
+}
