d98b152f6f5ed7c8856690228cf29496046428f3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 21:36:53 -0500

    2024-11-11T02:36:45.045Z function_auto_after storage_url

--- public/src/storage_url.mjs
+++ public/src/storage_url.mjs
@@ -1,5 +1,11 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { string_slash_encoded } from "./string_slash_encoded.mjs";
 import { string_replace } from "./string_replace.mjs";
 export function storage_url(file_path) {
-  let replaced = string_replace(file_path, "/", "%2F");
-  return `https://firebasestorage.googleapis.com/v0/b/letjesusbeexalted.appspot.com/o/${replaced}?alt=media`;
+  let replaced = string_replace(file_path, "/", string_slash_encoded());
+  return string_combine_multiple([
+    "https://firebasestorage.googleapis.com/v0/b/letjesusbeexalted.appspot.com/o/",
+    replaced,
+    "?alt=media",
+  ]);
 }
---
+++ public/src/string_slash_encoded.mjs
@@ -0,0 +1,3 @@
+export function string_slash_encoded() {
+  return "%2F";
+}
