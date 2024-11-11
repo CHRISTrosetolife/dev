01f59607f87df229a40f4ef15378f2e3aa8d3d1c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 17:42:40 -0500

    2024-11-11T22:42:39.966Z function_auto_after js_code_brackets_inside

--- public/src/js_code_brackets_inside.mjs
+++ public/src/js_code_brackets_inside.mjs
@@ -1,3 +1,4 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function js_code_brackets_inside(inside) {
-  return `{ ${inside} }`;
+  return string_combine_multiple(["{ ", inside, " }"]);
 }
