7528543dde0afd652c9a76e63f750e3a6ce65cf8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 17:44:08 -0500

    2024-11-11T22:44:07.378Z function_auto_after js_code_array

--- public/src/js_code_array.mjs
+++ public/src/js_code_array.mjs
@@ -1,9 +1,5 @@
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { js_code_brackets_inside } from "./js_code_brackets_inside.mjs";
 import { list_join_comma_space } from "./list_join_comma_space.mjs";
 export function js_code_array(contents_list) {
-  return string_combine_multiple([
-    "[",
-    list_join_comma_space(contents_list, ", "),
-    "]",
-  ]);
+  return js_code_brackets_inside(list_join_comma_space(contents_list, ", "));
 }
