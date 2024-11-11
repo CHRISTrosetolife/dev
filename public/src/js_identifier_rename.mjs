7299d3e113fc48e4c22f766a5bdc4f6546336fc6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 14:53:52 -0500

    2024-11-11T19:53:50.826Z function_auto_after js_identifier_rename

--- public/src/js_identifier_rename.mjs
+++ public/src/js_identifier_rename.mjs
@@ -1,8 +1,10 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { js_visit_identifiers_named } from "./js_visit_identifiers_named.mjs";
 import { string_is } from "./string_is.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { assert } from "./assert.mjs";
 export function js_identifier_rename(ast, identifier_from, identifier_to) {
+  assert_arguments_length(arguments, 3);
   assert(string_is, [identifier_from]);
   assert(string_is, [identifier_to]);
   js_visit_identifiers_named(ast, identifier_from, lambda);
