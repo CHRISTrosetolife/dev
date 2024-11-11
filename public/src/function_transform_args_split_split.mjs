07867cbced4137cf73a571a4873d4009ee9caa5c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 16:55:25 -0500

    2024-11-11T21:55:24.870Z function_auto_after function_transform_args_split_split

--- public/src/function_transform_args_split_split.mjs
+++ public/src/function_transform_args_split_split.mjs
@@ -2,11 +2,11 @@ import { function_transform_args_split_lambda } from "./function_transform_args_
 import { function_import } from "./function_import.mjs";
 import { list_map_async } from "./list_map_async.mjs";
 export async function function_transform_args_split_split(
-  split,
+  lambda_names,
   function_name,
   args,
 ) {
-  let mapped = await list_map_async(split, function_import);
+  let mapped = await list_map_async(lambda_names, function_import);
   return await function_transform_args_split_lambda(
     function_name,
     mapped,
