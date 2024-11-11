e4f88a2291ffee9cf2ff545f5c44c5f195f14b3c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 17:15:05 -0500

    2024-11-11T22:15:05.498Z function_auto_after function_unalias

--- public/src/function_unalias.mjs
+++ public/src/function_unalias.mjs
@@ -1,8 +1,8 @@
+import { function_aliases } from "./function_aliases.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
-import { function_aliases_generate } from "./function_aliases_generate.mjs";
 export function function_unalias(first) {
-  let aliases = function_aliases_generate();
+  let aliases = function_aliases();
   if (object_property_exists(aliases, first)) {
     first = object_property_get(aliases, first);
   }
