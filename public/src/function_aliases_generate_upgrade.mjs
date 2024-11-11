525b09bc1a09f64ef75ddf0ff6bbe5a3ab4d6c6a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 16:56:50 -0500

    2024-11-11T21:56:49.801Z function_auto_after function_aliases_generate_upgrade

--- public/src/function_aliases_generate_upgrade.mjs
+++ public/src/function_aliases_generate_upgrade.mjs
@@ -1,9 +1,14 @@
+import { js_node_types } from "./js_node_types.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { function_transform_args_split_lambda } from "./function_transform_args_split_lambda.mjs";
 export async function function_aliases_generate_upgrade() {
   await function_transform_args_split_lambda(
     fn_name("function_aliases_generate"),
-    [(ast) => {}],
+    [
+      (ast) => {
+        js_node_types();
+      },
+    ],
     [],
   );
 }
