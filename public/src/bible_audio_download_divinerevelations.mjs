9efde0292557b3e4dcfddcf670bc828c7346f48e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 21:39:06 -0500

    2024-11-11T02:39:03.472Z function_auto_after bible_audio_download_divinerevelations

--- public/src/bible_audio_download_divinerevelations.mjs
+++ public/src/bible_audio_download_divinerevelations.mjs
@@ -7,6 +7,7 @@ import { file_copy_path } from "./file_copy_path.mjs";
 import { file_copy_closed } from "./file_copy_closed.mjs";
 import { bible_audio_download_mp3 } from "./bible_audio_download_mp3.mjs";
 import { each_async } from "./each_async.mjs";
+import { string_skip } from "./string_skip.mjs";
 export async function bible_audio_download_divinerevelations() {
   let prefix_url =
     "https://www.divinerevelations.info/documents/bible/english_mp3_bible/";
@@ -18,7 +19,9 @@ export async function bible_audio_download_divinerevelations() {
   await each_async(versions, async (version) => {
     let chapter_paths = await bible_audio_download_mp3(prefix_url, version);
     await each_async(chapter_paths, async (chapter_path) => {
-      string_last_index(chapter_path, string_slash_encoded());
+      let s = string_slash_encoded();
+      let i = string_last_index(chapter_path, s);
+      let remaining = string_skip(chapter_path, i + string_length(s));
       let p = file_copy_path(
         chapter_path,
         path_join([bible_audio_player_english_path(), version]),
