44bfdd526000b64404001bc222d6a092b1422b98
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 17:26:32 -0500

    2024-11-11T22:26:31.892Z function_auto_after function_alias_add

--- public/src/function_alias_add.mjs
+++ public/src/function_alias_add.mjs
@@ -1,1 +1,12 @@
-export function function_alias_add() {}
+import { js_code_statement_call_args } from "./js_code_statement_call_args.mjs";
+import { app_dev_screens } from "./app_dev_screens.mjs";
+import { app_list_add } from "./app_list_add.mjs";
+import { fn_name } from "./fn_name.mjs";
+export async function function_alias_add() {
+  await app_list_add(app_dev_screens, function inserted_code() {
+    return js_code_statement_call_args(fn_name("list_add"), [
+      "result",
+      screen_name,
+    ]);
+  });
+}
