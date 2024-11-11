cbe1db5f0714f762c7bba977df95f11580d1ca57
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-10 21:47:18 -0500

    2024-11-11T02:47:17.507Z function_auto_after bible_audio_download_divinerevelations

--- public/src/bible_audio_download_divinerevelations.mjs
+++ public/src/bible_audio_download_divinerevelations.mjs
@@ -1,13 +1,4 @@
-import { file_copy_closed_if_exists_not } from "./file_copy_closed_if_exists_not.mjs";
-import { string_size } from "./string_size.mjs";
-import { string_slash_encoded } from "./string_slash_encoded.mjs";
-import { string_last_index } from "./string_last_index.mjs";
-import { bible_audio_player_english_path } from "./bible_audio_player_english_path.mjs";
-import { path_join } from "./path_join.mjs";
-import { file_copy_path } from "./file_copy_path.mjs";
-import { bible_audio_download_mp3 } from "./bible_audio_download_mp3.mjs";
-import { each_async } from "./each_async.mjs";
-import { string_skip } from "./string_skip.mjs";
+import { bible_audio_download_mp3_multiple } from "./bible_audio_download_mp3_multiple.mjs";
 export async function bible_audio_download_divinerevelations() {
   let prefix_url =
     "https://www.divinerevelations.info/documents/bible/english_mp3_bible/";
@@ -16,17 +7,5 @@ export async function bible_audio_download_divinerevelations() {
     "english_new_revised_standard_version_drama",
     "english_contemporary_english_version_drama",
   ];
-  await each_async(versions, async (version) => {
-    let chapter_paths = await bible_audio_download_mp3(prefix_url, version);
-    await each_async(chapter_paths, async (chapter_path) => {
-      let s = string_slash_encoded();
-      let i = string_last_index(chapter_path, s);
-      let remaining = string_skip(chapter_path, i + string_size(s));
-      let p = file_copy_path(
-        remaining,
-        path_join([bible_audio_player_english_path(), version]),
-      );
-      await file_copy_closed_if_exists_not(p, chapter_path);
-    });
-  });
+  await bible_audio_download_mp3_multiple(versions, prefix_url);
 }
---
+++ public/src/bible_audio_download_mp3_multiple.mjs
@@ -0,0 +1,25 @@
+import { file_copy_closed_if_exists_not } from "./file_copy_closed_if_exists_not.mjs";
+import { bible_audio_player_english_path } from "./bible_audio_player_english_path.mjs";
+import { path_join } from "./path_join.mjs";
+import { file_copy_path } from "./file_copy_path.mjs";
+import { string_size } from "./string_size.mjs";
+import { string_skip } from "./string_skip.mjs";
+import { string_last_index } from "./string_last_index.mjs";
+import { string_slash_encoded } from "./string_slash_encoded.mjs";
+import { bible_audio_download_mp3 } from "./bible_audio_download_mp3.mjs";
+import { each_async } from "./each_async.mjs";
+export async function bible_audio_download_mp3_multiple(versions, prefix_url) {
+  await each_async(versions, async (version) => {
+    let chapter_paths = await bible_audio_download_mp3(prefix_url, version);
+    await each_async(chapter_paths, async (chapter_path) => {
+      let s = string_slash_encoded();
+      let i = string_last_index(chapter_path, s);
+      let remaining = string_skip(chapter_path, i + string_size(s));
+      let p = file_copy_path(
+        remaining,
+        path_join([bible_audio_player_english_path(), version]),
+      );
+      await file_copy_closed_if_exists_not(p, chapter_path);
+    });
+  });
+}
