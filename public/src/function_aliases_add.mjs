47eea6b48bba981e3c9a31974adc0425178f6ce9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 17:31:23 -0500

    2024-11-11T22:31:23.203Z function_param_new_at function_aliases_add all all 0

--- public/src/function_aliases_add.mjs
+++ public/src/function_aliases_add.mjs
@@ -2,7 +2,7 @@ import { object_property_set } from "./object_property_set.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { assert } from "./assert.mjs";
 import { each } from "./each.mjs";
-export function function_aliases_add(aliases, name) {
+export function function_aliases_add(all, aliases, name) {
   each(aliases, (a) => {
     assert(object_property_exists_not, [all, a]);
     object_property_set(all, a, name);
