b3ed0879782ceeb3d5cc9fbb66e35adcdfb4224e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 17:44:15 -0500

    2024-11-11T22:44:13.508Z function_auto_after function_aliases_add

--- public/src/function_aliases_add.mjs
+++ public/src/function_aliases_add.mjs
@@ -1,4 +1,4 @@
-import { js_code_brackets_inside } from "./js_code_brackets_inside.mjs";
+import { js_code_array } from "./js_code_array.mjs";
 import { list_map } from "./list_map.mjs";
 import { js_code_statement_call_args } from "./js_code_statement_call_args.mjs";
 import { app_dev_screens } from "./app_dev_screens.mjs";
@@ -6,7 +6,6 @@ import { app_list_add } from "./app_list_add.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 import { string_delimit } from "./string_delimit.mjs";
-import { list_join_comma_space } from "./list_join_comma_space.mjs";
 export async function function_aliases_add(aliases, function_name) {
   let split = string_split_comma(aliases);
   let mapped = list_map(split, (s) => string_delimit(s));
@@ -19,9 +18,3 @@ export async function function_aliases_add(aliases, function_name) {
     ]);
   });
 }
-function js_code_array(mapped) {
-    let joined = list_join_comma_space(mapped);
-    let list_code = js_code_brackets_inside(joined);
-    return list_code;
-}
-
