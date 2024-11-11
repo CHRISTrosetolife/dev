5e8fe5f71a51c3c3e4b7e8a9c5676199eb6eb8c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 17:43:05 -0500

    2024-11-11T22:43:03.869Z function_auto_after function_aliases_add

--- public/src/function_aliases_add.mjs
+++ public/src/function_aliases_add.mjs
@@ -11,7 +11,7 @@ export async function function_aliases_add(aliases, function_name) {
   let split = string_split_comma(aliases);
   let mapped = list_map(split, (s) => string_delimit(s));
   let joined = list_join_comma_space(mapped);
-  js_code_brackets_inside();
+  js_code_brackets_inside(joined);
   await app_list_add(app_dev_screens, function inserted_code() {
     return js_code_statement_call_args(fn_name("function_aliases_register"), [
       "all",
