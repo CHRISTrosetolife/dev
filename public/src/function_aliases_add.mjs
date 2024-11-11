8dc17f36f91fc01903c80f9b66950c8c9761348d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 17:42:38 -0500

    2024-11-11T22:42:37.626Z c js_code_braces_inside js_code_brackets_inside

--- public/src/function_aliases_add.mjs
+++ public/src/function_aliases_add.mjs
@@ -6,10 +6,12 @@ import { fn_name } from "./fn_name.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 import { string_delimit } from "./string_delimit.mjs";
 import { list_join_comma_space } from "./list_join_comma_space.mjs";
+import { js_code_brackets } from "./js_code_brackets.mjs";
 export async function function_aliases_add(aliases, function_name) {
   let split = string_split_comma(aliases);
   let mapped = list_map(split, (s) => string_delimit(s));
   let joined = list_join_comma_space(mapped);
+  js_code_brackets()
   await app_list_add(app_dev_screens, function inserted_code() {
     return js_code_statement_call_args(fn_name("function_aliases_register"), [
       "all",
