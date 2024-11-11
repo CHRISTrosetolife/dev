67063402ebd73819acbe2cb3690be078948257d3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 14:59:21 -0500

    2024-11-11T19:59:21.435Z function_auto_after functions_rename_identifier

--- public/src/functions_rename_identifier.mjs
+++ public/src/functions_rename_identifier.mjs
@@ -1,11 +1,12 @@
+import { list_join_comma } from "./list_join_comma.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { function_transform_all_args } from "./function_transform_all_args.mjs";
 export async function functions_rename_identifier(
   identifier_from,
   identifier_to,
 ) {
-  await function_transform_all_args(fn_name("js_identifier_rename"), [
-    identifier_from,
-    identifier_to,
-  ]);
+  await function_transform_all_args(
+    fn_name("js_identifier_rename"),
+    list_join_comma([identifier_from, identifier_to]),
+  );
 }
