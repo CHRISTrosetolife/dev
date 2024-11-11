3a6570af4f525bbe1a0f92f4b5cdf2f86adbaba9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-11 15:01:50 -0500

    2024-11-11T20:01:49.893Z function_auto_after functions_rename_identifier

--- public/src/functions_rename_identifier.mjs
+++ public/src/functions_rename_identifier.mjs
@@ -1,3 +1,4 @@
+import { function_transform_args_split } from "./function_transform_args_split.mjs";
 import { functions_identifier } from "./functions_identifier.mjs";
 import { function_names } from "./function_names.mjs";
 import { list_join_comma } from "./list_join_comma.mjs";
@@ -9,6 +10,11 @@ export async function functions_rename_identifier(
 ) {
   let function_names_result = await function_names();
   await functions_identifier(identifier_from);
+  let r = await function_transform_args_split(
+    lambda_names,
+    function_name,
+    args_split,
+  );
   await function_transform_all_args(
     fn_name("js_identifier_rename"),
     list_join_comma([identifier_from, identifier_to]),
