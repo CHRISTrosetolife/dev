b779244d9af196e487799b4a608f98b05388bbf4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-12 17:47:55 -0500

    2024-11-12T22:47:53.664Z function_auto_after app_pray

--- public/src/app_pray.mjs
+++ public/src/app_pray.mjs
@@ -1,4 +1,6 @@
+import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_pray() {
   let body = html_style_default_initialize();
+  html_p_text_multiple();
 }
