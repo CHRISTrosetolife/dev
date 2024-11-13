0d5b8f873d2ea8d905485471d075c115fc33efee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-12 19:06:29 -0500

    2024-11-13T00:06:28.646Z function_auto_after app_pray

--- public/src/app_pray.mjs
+++ public/src/app_pray.mjs
@@ -9,7 +9,7 @@ import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_pray() {
   let root = html_style_default_initialize();
-  html_clear_scroll_top_centered();
+  html_clear_scroll_top_centered(root);
   html_p_text_multiple(root, [
     "Heavenly Father,",
     "in the name of the Trinity:",
