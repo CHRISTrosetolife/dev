8abce24036e2a60277b5fd11ca42e6461305f0a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-13 09:23:53 -0500

    2024-11-13T14:23:51.383Z function_auto_after app_pray_music

--- public/src/app_pray_music.mjs
+++ public/src/app_pray_music.mjs
@@ -33,7 +33,7 @@ export function app_pray_music(index) {
     list_concat_multiple([
       [
         "Heavenly Father,",
-        "in the name of the Trinity:",
+        "In the name of the Trinity:",
         "Have mercy on me, a sinner;",
       ],
       list_get(prayers, index),
