41e3115552fe92e927b6d6989fe65928d6cf5680
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-12 19:09:27 -0500

    2024-11-13T00:09:26.291Z function_auto_after app_pray_music

---
+++ public/src/app_pray_music.mjs
@@ -0,0 +1,56 @@
+import { object_property_get } from "./object_property_get.mjs";
+import { list_previous } from "./list_previous.mjs";
+import { html_button_previous_text } from "./html_button_previous_text.mjs";
+import { html_button_next_text } from "./html_button_next_text.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+import { list_get } from "./list_get.mjs";
+import { list_concat_multiple } from "./list_concat_multiple.mjs";
+import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
+import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
+export function app_pray_music(root, index) {
+  let prayers = [
+    [
+      "Thank You for (previous);",
+      "Cause the word of God to be perfectly engrafted into my heart;",
+      "Cause me to perfectly worship You;",
+      "Cause me to be a perfect hearer of the word of God;",
+      "Cause my eyes and ears to perfectly be opened up to Scripture and truth;",
+      "Cause me to be a perfect doer of the word of God;",
+      "Cause me to sing perfectly;",
+      "Cause me to compose perfectly;",
+      "Cause me to play piano perfectly;",
+      "Help me video;",
+    ],
+  ];
+  html_clear_scroll_top_centered(root);
+  html_p_text_multiple(
+    root,
+    list_concat_multiple([
+      ["Heavenly Father,", "in the name of the Trinity:"],
+      list_get(prayers, index),
+      ["Amen"],
+    ]),
+  );
+  let next = html_button_width_full_text_click(
+    root,
+    string_combine_multiple([html_button_next_text(), " prayer"]),
+    () => {
+      index++;
+    },
+  );
+  let previous = html_button_width_full_text_click(
+    root,
+    string_combine_multiple([html_button_previous_text(), " prayer"]),
+    async () => {
+      let verse_previous = list_previous(verses, verse);
+      await verse_refresh(
+        context,
+        book_code,
+        chapter,
+        object_property_get(verse_previous, "verse_number"),
+      );
+    },
+  );
+  return index;
+}
