978d03daf4f23f22a5f7c5c2c7f7559f954fff1e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-14 13:16:49 -0500

    2024-11-14T18:16:48.550Z function_auto_after app_record_verse_buttons

--- public/src/app_record_verse_buttons.mjs
+++ public/src/app_record_verse_buttons.mjs
@@ -3,7 +3,8 @@ import { app_record_chapter } from "./app_record_chapter.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { bible_book_chapter_text } from "./bible_book_chapter_text.mjs";
 import { app_record_chapter_buttons } from "./app_record_chapter_buttons.mjs";
-export function app_record_verse_buttons(context, book_code, chapter, root) {
+export function app_record_verse_buttons(context, book_code, chapter) {
+  let { root } = context;
   app_record_chapter_buttons(context, book_code);
   let chapter_text = bible_book_chapter_text(book_code, chapter);
   html_button_width_full_text_click(
