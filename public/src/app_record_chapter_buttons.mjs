ee1fdc64759956e0f7ad8a1bad0171439652c28a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-15 12:19:32 -0500

    2024-11-15T17:19:31.946Z function_auto_after app_record_chapter_buttons

--- public/src/app_record_chapter_buttons.mjs
+++ public/src/app_record_chapter_buttons.mjs
@@ -1,4 +1,3 @@
-import { app_record_verse } from "./app_record_verse.mjs";
 import { bible_book_name_text } from "./bible_book_name_text.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { app_record_book } from "./app_record_book.mjs";
@@ -7,7 +6,7 @@ import { app_record_home_button } from "./app_record_home_button.mjs";
 export function app_record_chapter_buttons(context, book_code, on_verse) {
   let { root } = context;
   html_clear_scroll_top(root);
-  app_record_home_button(context, app_record_verse);
+  app_record_home_button(context, on_verse);
   html_button_width_full_text_click(
     root,
     bible_book_name_text(book_code),
