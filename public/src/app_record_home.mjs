f7f0459f730ad91a7d583df02a7c6a08b89f0090
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-15 12:18:25 -0500

    2024-11-15T17:18:25.148Z function_auto_after app_record_home

--- public/src/app_record_home.mjs
+++ public/src/app_record_home.mjs
@@ -6,6 +6,7 @@ import { app_record_home_generic } from "./app_record_home_generic.mjs";
 import { app_record_book } from "./app_record_book.mjs";
 export async function app_record_home(context, on_verse) {
   await app_save_change(app_record, (save) => object_replace(save, {}));
-  let app_record_home_on_click = app_record_book;
+  let app_record_home_on_click = async (context, book_code) =>
+    await app_record_book(context, book_code, on_verse);
   app_record_home_generic(context, noop, app_record_home_on_click, null);
 }
