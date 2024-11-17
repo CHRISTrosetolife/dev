813e9085df43dc920513fd7250940e6dab7bb32e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-17 15:22:57 -0500

    2024-11-17T20:22:57.464Z function_auto_after app_todo_command_move

--- public/src/app_todo_command_move.mjs
+++ public/src/app_todo_command_move.mjs
@@ -5,12 +5,13 @@ import { app_todo_command_log } from "./app_todo_command_log.mjs";
 import { app_todo_command_items_filter } from "./app_todo_command_items_filter.mjs";
 import { app_todo_command_items } from "./app_todo_command_items.mjs";
 export async function app_todo_command_move(index_from, index_to) {
+  let transform = (filtered, items) =>
+    app_todo_command_move_transform(filtered, index_to, index_from, items);
   if (false) {
     index_to = add_1_integer(index_to);
   }
   let { filtered, items, index, index_path } = await app_todo_command_items();
-  let transform = (filtered, items) =>
-    app_todo_command_move_transform(filtered, index_to, index_from, items);
+  transform(filtered, items);
   filtered = app_todo_command_items_filter(items);
   await storage_upload_object(index, index_path);
   app_todo_command_log(filtered);
