4c55ed356304cd4bfcb7f84be888e9abbeb0e709
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-17 15:23:50 -0500

    2024-11-17T20:23:48.998Z function_auto_after app_todo_command_move

--- public/src/app_todo_command_move.mjs
+++ public/src/app_todo_command_move.mjs
@@ -1,10 +1,11 @@
+import { add_1_integer } from "./add_1_integer.mjs";
 import { app_todo_command_transform } from "./app_todo_command_transform.mjs";
 import { app_todo_command_move_transform } from "./app_todo_command_move_transform.mjs";
 export async function app_todo_command_move(index_from, index_to) {
-    if (false) {
-      index_to = add_1_integer(index_to);
-    }
+  if (false) {
+    index_to = add_1_integer(index_to);
+  }
   let transform = (filtered, items) =>
     app_todo_command_move_transform(filtered, index_to, index_from, items);
-  index_to = await app_todo_command_transform(transform);
+   await app_todo_command_transform(transform);
 }
