7c5dc441b32920536c803e0c74b965bce2c7090a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-17 16:01:26 -0500

    2024-11-17T21:01:25.626Z watch   function_path_to_name

--- public/src/function_path_to_name.mjs
+++ public/src/function_path_to_name.mjs
@@ -4,8 +4,8 @@ import { string_prefix_without } from "./string_prefix_without.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
 export function function_path_to_name(function_path) {
   let prefix = function_path_prefix();
-  function_path = string_prefix_without(function_path, prefix);
   let suffix = function_path_suffix();
+  function_path = string_prefix_without(function_path, prefix);
   function_path = string_suffix_without(function_path, suffix);
   return function_path;
 }
