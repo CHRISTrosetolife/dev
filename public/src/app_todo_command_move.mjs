69df1ad3284bd7dda36e6df8ead648e025e5a00d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-17 15:20:39 -0500

    2024-11-17T20:20:37.743Z function_auto_after app_todo_command_move

--- public/src/app_todo_command_move.mjs
+++ public/src/app_todo_command_move.mjs
@@ -13,22 +13,25 @@ export async function app_todo_command_move(index_from, index_to) {
     index_to = add_1_integer(index_to);
   }
   let { filtered, items, index, index_path } = await app_todo_command_items();
-  let filtered_size = list_size(filtered);
-  let item_to;
-  if (index_to < filtered_size) {
-    item_to = list_get(filtered, index_to);
-  }
-  let item_from = list_get(filtered, index_from);
-  let items_index_from = list_index(items, item_from);
-  list_remove_at(items, items_index_from);
-  let items_index_to;
-  if (index_to >= filtered_size) {
-    items_index_to = list_size(items);
-  } else {
-    items_index_to = list_index(items, item_to);
-  }
-  list_insert(items, items_index_to, item_from);
+  app_todo_command_move_transform();
   filtered = app_todo_command_items_filter(items);
   await storage_upload_object(index, index_path);
   app_todo_command_log(filtered);
+  function app_todo_command_move_transform() {
+    let filtered_size = list_size(filtered);
+    let item_to;
+    if (index_to < filtered_size) {
+      item_to = list_get(filtered, index_to);
+    }
+    let item_from = list_get(filtered, index_from);
+    let items_index_from = list_index(items, item_from);
+    list_remove_at(items, items_index_from);
+    let items_index_to;
+    if (index_to >= filtered_size) {
+      items_index_to = list_size(items);
+    } else {
+      items_index_to = list_index(items, item_to);
+    }
+    list_insert(items, items_index_to, item_from);
+  }
 }
