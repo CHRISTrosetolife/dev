86abf49b04d103ff1e7355fcdca3190c02453d73
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-17 15:24:39 -0500

    2024-11-17T20:24:38.369Z function_auto_after app_todo_command_add

--- public/src/app_todo_command_add.mjs
+++ public/src/app_todo_command_add.mjs
@@ -1,6 +1,6 @@
 import { app_todo_command_transform } from "./app_todo_command_transform.mjs";
 import { app_todo_command_move_transform } from "./app_todo_command_move_transform.mjs";
-export async function app_todo_command_add(index_from, index_to) {
+export async function app_todo_command_add(description) {
   let transform = (filtered, items) =>
     app_todo_command_move_transform(filtered, index_to, index_from, items);
   await app_todo_command_transform(transform);
