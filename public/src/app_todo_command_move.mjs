1a21375fa85eb369cb7fe7234b0f80b5ebe5cd55
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-17 15:23:45 -0500

    2024-11-17T20:23:45.037Z function_auto_after app_todo_command_transform

--- public/src/app_todo_command_move.mjs
+++ public/src/app_todo_command_move.mjs
@@ -1,7 +1,10 @@
 import { app_todo_command_transform } from "./app_todo_command_transform.mjs";
 import { app_todo_command_move_transform } from "./app_todo_command_move_transform.mjs";
 export async function app_todo_command_move(index_from, index_to) {
+    if (false) {
+      index_to = add_1_integer(index_to);
+    }
   let transform = (filtered, items) =>
     app_todo_command_move_transform(filtered, index_to, index_from, items);
-  index_to = await app_todo_command_transform(index_to, transform);
+  index_to = await app_todo_command_transform(transform);
 }
--- public/src/app_todo_command_transform.mjs
+++ public/src/app_todo_command_transform.mjs
@@ -2,7 +2,7 @@ import { app_todo_command_log } from "./app_todo_command_log.mjs";
 import { storage_upload_object } from "./storage_upload_object.mjs";
 import { app_todo_command_items_filter } from "./app_todo_command_items_filter.mjs";
 import { app_todo_command_items } from "./app_todo_command_items.mjs";
-export async function app_todo_command_transform(index_to, transform) {
+export async function app_todo_command_transform(transform) {
   let { filtered, items, index, index_path } = await app_todo_command_items();
   transform(filtered, items);
   filtered = app_todo_command_items_filter(items);
