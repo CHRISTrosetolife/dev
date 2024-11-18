342acb1382ab8e007d353f0bdf6518c5941d692d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-17 21:13:05 -0500

    2024-11-18T02:13:04.715Z watch   html_prayer

---
+++ public/src/html_prayer.mjs
@@ -0,0 +1,16 @@
+import { list_concat_multiple } from "./list_concat_multiple.mjs";
+import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
+export function html_prayer(root, prayer) {
+  html_p_text_multiple(
+    root,
+    list_concat_multiple([
+      [
+        "Heavenly Father,",
+        "In the name of the Trinity:",
+        "Have mercy on me, a sinner;",
+      ],
+      prayer,
+      ["Amen"],
+    ]),
+  );
+}
