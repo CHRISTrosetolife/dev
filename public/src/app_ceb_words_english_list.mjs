18fd4b3cc7e1ceaddfe64ac30a18f8f19a5c8afa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-18 12:04:37 -0500

    2024-11-18T17:04:35.577Z watch   app_ceb_words_english_list

--- public/src/app_ceb_words_english_list.mjs
+++ public/src/app_ceb_words_english_list.mjs
@@ -1,7 +1,7 @@
 import { bible_words_definitions_atoms_cache } from "./bible_words_definitions_atoms_cache.mjs";
 import { object_properties } from "./object_properties.mjs";
 export async function app_ceb_words_english_list() {
-  let { inverted } = await bible_words_definitions_atoms_cache(from);
+  let { inverted } = await bible_words_definitions_atoms_cache("ceb");
   let english = object_properties(inverted);
   return english;
 }
