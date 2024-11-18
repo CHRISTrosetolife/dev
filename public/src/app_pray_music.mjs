52cf9c330ae92668c84925c1afa0567db816d4ea
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-17 21:13:37 -0500

    2024-11-18T02:13:36.944Z watch   app_pray_music

--- public/src/app_pray_music.mjs
+++ public/src/app_pray_music.mjs
@@ -12,9 +12,9 @@ export function app_pray_music(index) {
   let prayers = [
     [
       "Thank You for helping me (previous or:) upload;",
+      "Cause all to perfectly worship You;",
       "Cause the word of God to be perfectly engrafted into all hearts;",
       "Cause the planting and watering of the word of God to have perfectly maximized increase;",
-      "Cause all to perfectly worship You;",
       "Cause all to be a perfect hearer of the word of God;",
       "Cause the eyes and ears of all to perfectly be opened up to Scripture and truth;",
       "Cause all to be a perfect doer of the word of God;",
