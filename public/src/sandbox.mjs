65fc1992deefa2ac011d5a118c5b7289e0a27ce4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-28 07:22:44 -0500

    2024-11-28T12:22:32.449Z watch   sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,13 +1,11 @@
 import { Builder, Browser, By, Key, until } from "selenium-webdriver";
 export async function sandbox() {
-  (async function example() {
-    let driver = await new Builder().forBrowser(Browser.EDGE).build();
-    try {
-      await driver.get("https://www.google.com/ncr");
-      await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
-      await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
-    } finally {
-      await driver.quit();
-    }
-  })();
+  let driver = await new Builder().forBrowser(Browser.EDGE).build();
+  try {
+    await driver.get("https://www.google.com/ncr");
+    await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
+    await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
+  } finally {
+    await driver.quit();
+  }
 }
