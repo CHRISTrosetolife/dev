ea768450ce400507c3fd01894ac9814b36c98f7c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-28 07:35:52 -0500

    2024-11-28T12:35:50.210Z watch   storage_bucket

--- public/src/storage_bucket.mjs
+++ public/src/storage_bucket.mjs
@@ -1,3 +1,4 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { file_read_json } from "./file_read_json.mjs";
@@ -5,7 +6,9 @@ import { global_function_initialize } from "./global_function_initialize.mjs";
 import { import_node } from "./import_node.mjs";
 export async function storage_bucket() {
   let { default: admin } = await import_node("firebase-admin");
-  let service_account = await file_read_json("../firebase.json");
+  let service_account = await file_read_json(
+    string_combine_multiple(["../firebase.json"]),
+  );
   let g = global_function_initialize(storage_bucket, () => ({
     initialized: false,
   }));
