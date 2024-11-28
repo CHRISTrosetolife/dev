b640a839d889cfe07cafb6ebd4ec4024e720d26c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-28 14:20:40 -0500

    2024-11-28T19:20:40.145Z watch   sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,7 +1,6 @@
 import { file_transform } from "./file_transform.mjs";
 import { each_async } from "./each_async.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { file_read } from "./file_read.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_parse_parsed } from "./html_parse_parsed.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
@@ -19,10 +18,10 @@ export async function sandbox() {
   paths = list_map(paths, (q) => string_suffix_without(q, e));
   await each_async(paths, async (q) => {
     let test_path = string_combine_multiple([p, q, e]);
-    let html = await file_read(test_path);
-    let r = await html_parse_parsed(html);
-    let parsed = object_property_get(r, "parsed");
-    await file_transform();
+    await file_transform(async (text) => {
+      let r = await html_parse_parsed(html);
+      let parsed = object_property_get(r, "parsed");
+    }, test_path);
   });
   return paths;
 }
