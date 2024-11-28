1d4780ee98ec85fb7fb92dca956dd86825c0e106
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-28 07:26:17 -0500

    2024-11-28T12:26:15.897Z watch   sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -3,6 +3,7 @@ export async function sandbox() {
   let driver = await new Builder().forBrowser(Browser.EDGE).build();
   try {
     await driver.get("https://www.facebook.com/");
+    await driver.findElement(By.id("email")).sendKeys("webdriver");
     return;
     await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
     await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
