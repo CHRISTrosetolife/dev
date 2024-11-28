b9a9de2ac5501f7c5ff34ac7c43d859076534bd9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-28 14:06:22 -0500

    2024-11-28T19:06:22.291Z watch   sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,8 +1,9 @@
+import { string_prefix_without } from "./string_prefix_without.mjs";
 import { list_map } from "./list_map.mjs";
 import { folder_path_public } from "./folder_path_public.mjs";
 import { folder_read_shallow } from "./folder_read_shallow.mjs";
 export async function sandbox() {
   let p = folder_path_public();
   let paths = await folder_read_shallow(p, ".html");
-  return list_map(paths, (q) => string_prefix_remove(q, p));
+  return list_map(paths, (q) => string_prefix_without(q, p));
 }
