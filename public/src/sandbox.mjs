a16808de8a0a6d82c45dd5c90b29863569e4ae8c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-28 14:04:31 -0500

    2024-11-28T19:04:30.629Z watch   sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,5 @@
-import { folder_path_src } from "./folder_path_src.mjs";
+import { folder_path_public } from "./folder_path_public.mjs";
 import { folder_read_shallow } from "./folder_read_shallow.mjs";
 export async function sandbox() {
-  return await folder_read_shallow(folder_path_src(), ".html");
+  return await folder_read_shallow(folder_path_public(), ".html");
 }
