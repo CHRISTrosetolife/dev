dba328f1fea5862ce7016162c3b1ae0005e08b78
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-28 13:53:33 -0500

    2024-11-28T18:53:32.527Z watch   js_import_remove

--- public/src/js_import_remove.mjs
+++ public/src/js_import_remove.mjs
@@ -5,6 +5,8 @@ import { js_imports_existing_named } from "./js_imports_existing_named.mjs";
 export function js_import_remove(ast, e) {
   let { body } = ast;
   let existing2 = js_imports_existing_named(ast, e);
+  if (false) {
+  }
   let e2 = list_single(existing2);
   let { stack } = e2;
   let ancestor = list_get_end(stack, 1);
