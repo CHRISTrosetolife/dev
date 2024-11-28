f7999a3466c4e0e11e3c0585c239f19e1b5c7db5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-28 14:15:19 -0500

    2024-11-28T19:15:19.033Z watch   sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -6,8 +6,8 @@ import { list_map } from "./list_map.mjs";
 import { folder_path_public } from "./folder_path_public.mjs";
 import { folder_read_shallow } from "./folder_read_shallow.mjs";
 export async function sandbox() {
-  await html_parse_parsed();
   let p = folder_path_public();
+  await html_parse_parsed();
   let e = ".html";
   let paths = await folder_read_shallow(p, e);
   paths = list_map(paths, string_slash_normalize_right);
