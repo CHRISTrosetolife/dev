6393ea7ce02d8b8df3737dc65bec15213fe359a4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-28 14:21:33 -0500

    2024-11-28T19:21:32.863Z watch   sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -16,12 +16,14 @@ export async function sandbox() {
   paths = list_map(paths, string_slash_normalize_right);
   paths = list_map(paths, (q) => string_prefix_without(q, p));
   paths = list_map(paths, (q) => string_suffix_without(q, e));
+  paths = ["test"];
   await each_async(paths, async (q) => {
     let test_path = string_combine_multiple([p, q, e]);
     await file_transform(
       async (text) => {
-        let r = await html_parse_parsed(html);
+        let r = await html_parse_parsed(text);
         let parsed = object_property_get(r, "parsed");
+        return parsed.xml();
       },
       test_path,
       [],
