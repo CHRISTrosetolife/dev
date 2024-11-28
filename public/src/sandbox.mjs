1acc2ca8899b878b090f2fbb31f5f262dfe643ea
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-28 07:27:38 -0500

    2024-11-28T12:27:37.730Z watch   sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,9 +1,11 @@
+import { sleep } from "./sleep.mjs";
 import { Builder, Browser, By, Key, until } from "selenium-webdriver";
 export async function sandbox() {
   let driver = await new Builder().forBrowser(Browser.EDGE).build();
   try {
     await driver.get("https://www.facebook.com/");
     await driver.findElement(By.id("email")).sendKeys("9043144052");
+    await sleep(10000);
     return;
     await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
     await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
