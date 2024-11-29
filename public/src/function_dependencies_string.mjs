3bd13d38018e2a241c41d1e55c236fb64e1b7ad9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 14:45:43 -0500

    2024-11-29T19:45:43.031Z watch   function_dependencies_string

--- public/src/function_dependencies_string.mjs
+++ public/src/function_dependencies_string.mjs
@@ -1,4 +1,4 @@
-import { function_dependencies_string_froms } from "./function_dependencies_string_froms.mjs";
+import { function_dependencies_string_externals } from "./function_dependencies_string_externals.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { js_unparse } from "./js_unparse.mjs";
@@ -7,12 +7,6 @@ import { list_map_async } from "./list_map_async.mjs";
 import { object_properties } from "./object_properties.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_map } from "./list_map.mjs";
-import { object_property_set } from "./object_property_set.mjs";
-import { js_code_import_generic } from "./js_code_import_generic.mjs";
-import { list_join_comma_space } from "./list_join_comma_space.mjs";
-import { js_code_braces_inside } from "./js_code_braces_inside.mjs";
-import { list_is } from "./list_is.mjs";
-import { each_object } from "./each_object.mjs";
 import { object_values } from "./object_values.mjs";
 import { each } from "./each.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
@@ -25,22 +19,7 @@ export async function function_dependencies_string(function_name) {
       each(sources, la);
     });
   });
-  let e = {};
-  let froms = function_dependencies_string_froms();
-  each_object(froms, (from, identifiers) => {
-    let imported;
-    if (list_is(identifiers)) {
-      imported = js_code_braces_inside(list_join_comma_space(identifiers));
-    } else {
-      imported = identifiers;
-    }
-    let code = js_code_import_generic(imported, from);
-    object_property_set(e, from, code);
-  });
-  let standard = ["chalk"];
-  each(standard, (s) => {
-    object_property_set(e, s, js_code_import_generic(s, s));
-  });
+  let e = function_dependencies_string_externals();
   let e_code = list_map(externals, (n) => object_property_get(e, n));
   let dependency_names = object_properties(map);
   let ds = await list_map_async(dependency_names, function_declaration);
---
+++ public/src/function_dependencies_string_externals.mjs
@@ -0,0 +1,27 @@
+import { each } from "./each.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+import { js_code_import_generic } from "./js_code_import_generic.mjs";
+import { list_join_comma_space } from "./list_join_comma_space.mjs";
+import { js_code_braces_inside } from "./js_code_braces_inside.mjs";
+import { list_is } from "./list_is.mjs";
+import { each_object } from "./each_object.mjs";
+import { function_dependencies_string_froms } from "./function_dependencies_string_froms.mjs";
+export function function_dependencies_string_externals() {
+  let e = {};
+  let froms = function_dependencies_string_froms();
+  each_object(froms, (from, identifiers) => {
+    let imported;
+    if (list_is(identifiers)) {
+      imported = js_code_braces_inside(list_join_comma_space(identifiers));
+    } else {
+      imported = identifiers;
+    }
+    let code = js_code_import_generic(imported, from);
+    object_property_set(e, from, code);
+  });
+  let standard = ["chalk"];
+  each(standard, (s) => {
+    object_property_set(e, s, js_code_import_generic(s, s));
+  });
+  return e;
+}
