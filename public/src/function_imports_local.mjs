6e3e0814f5ae71cc8eb7a322fbe55da83c68d1f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 10:08:57 -0500

    2024-11-29T15:08:56.845Z watch   function_imports_local

--- public/src/function_imports_local.mjs
+++ public/src/function_imports_local.mjs
@@ -7,7 +7,7 @@ export async function function_imports_local(function_name) {
   let ast = await function_parse(function_name);
   let imports = js_imports_existing(ast);
   let sources = js_imports_sources(ast);
-  sources = list_filter(sources, (s) => js_import_external_is(s));
+  sources = list_filter(sources, js_import_external_is);
   return {
     imports,
     sources,
