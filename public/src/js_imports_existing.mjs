3f98b768997c26734e2b88313cff1162e55b258c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 10:05:48 -0500

    2024-11-29T15:05:48.180Z watch   js_imports_existing

--- public/src/js_imports_existing.mjs
+++ public/src/js_imports_existing.mjs
@@ -1,3 +1,4 @@
+import { list_get_end_1 } from "./list_get_end_1.mjs";
 import { log } from "./log.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
@@ -5,9 +6,9 @@ export function js_imports_existing(ast) {
   let existing = list_adder((la) => {
     js_visit_node(ast, "ImportSpecifier", (v) => {
       let { node } = v;
-      let { parent } = v;
+      let { stack } = v;
       log({
-        parent,
+        p: list_get_end_1(stack),
       });
       let { imported } = node;
       if (imported.type === "Identifier") {
