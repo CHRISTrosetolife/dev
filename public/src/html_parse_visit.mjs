3bf67985e88af105576073f534a9be3b013a9b8a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-28 20:25:00 -0500

    2024-11-29T01:25:00.230Z watch   html_parse_visit

--- public/src/html_parse_visit.mjs
+++ public/src/html_parse_visit.mjs
@@ -1,15 +1,9 @@
+import { html_parse_children } from "./html_parse_children.mjs";
 import { list_not_is } from "./list_not_is.mjs";
 import { assert } from "./assert.mjs";
-import { object_property_get_or } from "./object_property_get_or.mjs";
 import { tautology } from "./tautology.mjs";
 import { visit } from "./visit.mjs";
 export function html_parse_visit(root, lambda) {
   assert(list_not_is, [root]);
-  visit(
-    root,
-    (n) => object_property_get_or(n, "children", []),
-    tautology,
-    lambda,
-    [],
-  );
+  visit(root, html_parse_children, tautology, lambda, []);
 }
