1dc08a3b2f4b38ab25fad6bfe593b4007673392d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 10:09:42 -0500

    2024-11-29T15:09:42.239Z watch   js_imports_existing

--- public/src/js_imports_existing.mjs
+++ public/src/js_imports_existing.mjs
@@ -1,6 +1,5 @@
 import { js_import_declaration_source_value } from "./js_import_declaration_source_value.mjs";
 import { list_get_end_1 } from "./list_get_end_1.mjs";
-import { log } from "./log.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_imports_existing(ast) {
@@ -9,9 +8,7 @@ export function js_imports_existing(ast) {
       let { node } = v;
       let { stack } = v;
       let e = list_get_end_1(stack);
-      log({
-        p: js_import_declaration_source_value(e),
-      });
+      let sv = js_import_declaration_source_value(e);
       let { imported } = node;
       if (imported.type === "Identifier") {
         la(imported.name);
