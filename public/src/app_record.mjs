427856b068ad6432cbffc1a147a2a0477a4edf1e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 18:12:58 -0500

    2024-11-29T23:12:57.772Z watch   app_record

--- public/src/app_record.mjs
+++ public/src/app_record.mjs
@@ -2,6 +2,9 @@ import { app_record_generic } from "./app_record_generic.mjs";
 import { app_record_version_code } from "./app_record_version_code.mjs";
 import { app_record_verse } from "./app_record_verse.mjs";
 export async function app_record() {
-  let version_code = app_record_version_code();
-  await app_record_generic(version_code, app_record, app_record_verse);
+  await app_record_generic(
+    app_record_version_code(),
+    app_record,
+    app_record_verse,
+  );
 }
