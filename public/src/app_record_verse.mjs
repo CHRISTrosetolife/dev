d97e5f29291b3749ee653d2a476c062aa040fef7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 18:25:40 -0500

    2024-11-29T23:25:37.688Z watch   app_record_verse

--- public/src/app_record_verse.mjs
+++ public/src/app_record_verse.mjs
@@ -1,16 +1,8 @@
-import { html_div } from "./html_div.mjs";
-import { app_verses_generic } from "./app_verses_generic.mjs";
-import { app_record_verse_buttons } from "./app_record_verse_buttons.mjs";
+import { app_record_verse_generic } from "./app_record_verse_generic.mjs";
 import { html_button_width_full_text_click_br } from "./html_button_width_full_text_click_br.mjs";
 import { html_audio } from "./html_audio.mjs";
 import { app_record_project_name } from "./app_record_project_name.mjs";
-import { app_save_change_bible } from "./app_save_change_bible.mjs";
-import { html_bible_verse_navigation } from "./html_bible_verse_navigation.mjs";
-import { html_bible_verse } from "./html_bible_verse.mjs";
-import { html_style_green } from "./html_style_green.mjs";
-import { equal } from "./equal.mjs";
 import { app_record } from "./app_record.mjs";
-import { html_hr } from "./html_hr.mjs";
 import { firebase_upload_bytes } from "./firebase_upload_bytes.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { path_join } from "./path_join.mjs";
@@ -22,7 +14,6 @@ import { html_style_display_block } from "./html_style_display_block.mjs";
 import { html_style_display_none } from "./html_style_display_none.mjs";
 import { html_recorder_media_start } from "./html_recorder_media_start.mjs";
 import { html_recorder_media_stop } from "./html_recorder_media_stop.mjs";
-import { list_find_property } from "./list_find_property.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export async function app_record_verse(
   context,
@@ -32,26 +23,13 @@ export async function app_record_verse(
 ) {
   let app_lambda = app_record;
   let app_verse_lambda = app_record_verse;
-  await app_save_change_bible(app_lambda, book_code, chapter, verse_number);
-  let root = object_property_get(context, "root");
-  app_record_verse_buttons(context, book_code, chapter, app_verse_lambda);
-  let verses = await app_verses_generic(app_lambda, book_code, chapter);
-  let verse = list_find_property(verses, "verse_number", verse_number);
-  let vn = html_bible_verse(root, book_code, chapter, verse);
-  html_hr(root);
-  if (equal(verse_number, "1")) {
-    html_style_green(vn);
-  }
-  let middle = html_div(root);
-  let navigation = await html_bible_verse_navigation(
+  let { middle, navigation, root } = await app_record_verse_generic(
     app_lambda,
-    app_verse_lambda,
-    context,
     book_code,
     chapter,
     verse_number,
-    "reading and recording audio for audio Bible",
-    true,
+    context,
+    app_verse_lambda,
   );
   let start,
     previous,
---
+++ public/src/app_record_verse_generic.mjs
@@ -0,0 +1,46 @@
+import { html_bible_verse_navigation } from "./html_bible_verse_navigation.mjs";
+import { html_div } from "./html_div.mjs";
+import { html_style_green } from "./html_style_green.mjs";
+import { equal } from "./equal.mjs";
+import { html_hr } from "./html_hr.mjs";
+import { html_bible_verse } from "./html_bible_verse.mjs";
+import { list_find_property } from "./list_find_property.mjs";
+import { app_verses_generic } from "./app_verses_generic.mjs";
+import { app_record_verse_buttons } from "./app_record_verse_buttons.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { app_save_change_bible } from "./app_save_change_bible.mjs";
+export async function app_record_verse_generic(
+  app_lambda,
+  book_code,
+  chapter,
+  verse_number,
+  context,
+  app_verse_lambda,
+) {
+  await app_save_change_bible(app_lambda, book_code, chapter, verse_number);
+  let root = object_property_get(context, "root");
+  app_record_verse_buttons(context, book_code, chapter, app_verse_lambda);
+  let verses = await app_verses_generic(app_lambda, book_code, chapter);
+  let verse = list_find_property(verses, "verse_number", verse_number);
+  let vn = html_bible_verse(root, book_code, chapter, verse);
+  html_hr(root);
+  if (equal(verse_number, "1")) {
+    html_style_green(vn);
+  }
+  let middle = html_div(root);
+  let navigation = await html_bible_verse_navigation(
+    app_lambda,
+    app_verse_lambda,
+    context,
+    book_code,
+    chapter,
+    verse_number,
+    "reading and recording audio for audio Bible",
+    true,
+  );
+  return {
+    middle,
+    navigation,
+    root,
+  };
+}
