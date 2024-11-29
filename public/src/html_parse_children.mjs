451531bf27d5cc84b4f45fdc83590a8b24d4cee6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-28 20:24:54 -0500

    2024-11-29T01:24:53.498Z watch   html_parse_children

--- public/src/html_parse_children.mjs
+++ public/src/html_parse_children.mjs
@@ -1,4 +1,4 @@
-export function html_parse_children(main) {
-  let { children } = main;
-  return children;
+import { object_property_get_or } from "./object_property_get_or.mjs";
+export function html_parse_children(n) {
+  return object_property_get_or(n, "children", []);
 }
