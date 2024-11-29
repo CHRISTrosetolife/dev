db6c9cb38f3c3fcdef1215c59dc7a8ace4c58b3f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 18:36:57 -0500

    2024-11-29T23:36:57.321Z watch   app_bible

--- public/src/app_bible.mjs
+++ public/src/app_bible.mjs
@@ -1,3 +1,4 @@
+import { app_record_generic } from "./app_record_generic.mjs";
 import { log } from "./log.mjs";
 import { app_save_set } from "./app_save_set.mjs";
 import { app_save_get } from "./app_save_get.mjs";
@@ -8,6 +9,8 @@ import { firebase_initialize_axios } from "./firebase_initialize_axios.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { object_merge } from "./object_merge.mjs";
 export async function app_bible() {
+  await app_record_generic("engbsb", app_bible, app_bible_verse);
+  return;
   let root = await firebase_initialize_axios();
   html_style_a_plain();
   let context = {
