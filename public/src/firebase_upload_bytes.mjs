fe2df230536bc73f606badd47b1b5c5163f151e7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 10:17:59 -0500

    2024-11-29T15:17:58.539Z watch   firebase_list

--- public/src/firebase_upload_bytes.mjs
+++ public/src/firebase_upload_bytes.mjs
@@ -2,7 +2,7 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import {
   getStorage,
   ref,
-  uploadBytes,
+  uploadBytes,  
 } from "https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.2/firebase-storage.min.js";
 export async function firebase_upload_bytes(storage_path, blob) {
   assert_arguments_length(arguments, 2);
