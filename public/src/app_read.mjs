a9c401b428238c0c8b925a2e4e6c63c4bf8c2ff3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 18:13:14 -0500

    2024-11-29T23:13:14.383Z watch   app_read

--- public/src/app_read.mjs
+++ public/src/app_read.mjs
@@ -1,4 +1,11 @@
-import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-export function app_read() {
-  let body = html_style_default_initialize();
+import { app_record_verse } from "./app_record_verse.mjs";
+import { app_record } from "./app_record.mjs";
+import { app_record_version_code } from "./app_record_version_code.mjs";
+import { app_record_generic } from "./app_record_generic.mjs";
+export async function app_read() {
+  await app_record_generic(
+    app_record_version_code(),
+    app_record,
+    app_record_verse,
+  );
 }
