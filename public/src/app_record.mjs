dc88804e1efc1ababbc53ec80909336d94e82b4f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 18:10:52 -0500

    2024-11-29T23:10:52.302Z watch   app_record

--- public/src/app_record.mjs
+++ public/src/app_record.mjs
@@ -1,17 +1,10 @@
+import { app_record_lookup } from "./app_record_lookup.mjs";
 import { html_button_next } from "./html_button_next.mjs";
 import { app_record_version_code } from "./app_record_version_code.mjs";
-import { app_context_books_bible_generic } from "./app_context_books_bible_generic.mjs";
-import { app_save_get } from "./app_save_get.mjs";
 import { html_style_a_plain } from "./html_style_a_plain.mjs";
 import { app_firebase } from "./app_firebase.mjs";
-import { app_record_home } from "./app_record_home.mjs";
-import { app_record_book } from "./app_record_book.mjs";
-import { app_record_chapter } from "./app_record_chapter.mjs";
 import { app_record_verse } from "./app_record_verse.mjs";
-import { object_property_get_or } from "./object_property_get_or.mjs";
-import { html_hash_lookup } from "./html_hash_lookup.mjs";
 import { html_recorder_media } from "./html_recorder_media.mjs";
-import { object_merge } from "./object_merge.mjs";
 import { html_prayer } from "./html_prayer.mjs";
 import { app_pray_word } from "./app_pray_word.mjs";
 export async function app_record() {
@@ -27,36 +20,12 @@ export async function app_record() {
       let version_code = app_record_version_code();
       let app_lambda = app_record;
       let app_verse_lambda = app_record_verse;
-      await app_context_books_bible_generic(context, version_code);
-      let lookup = html_hash_lookup();
-      let save = app_save_get(app_lambda);
-      object_merge(lookup, save);
-      let hash_book = object_property_get_or(lookup, "book", null);
-      if (hash_book !== null) {
-        let hash_chapter = object_property_get_or(lookup, "chapter", null);
-        if (hash_chapter !== null) {
-          let hash_verse = object_property_get_or(lookup, "verse", null);
-          if (hash_verse !== null) {
-            await app_verse_lambda(
-              context,
-              hash_book,
-              hash_chapter,
-              hash_verse,
-            );
-          } else {
-            await app_record_chapter(
-              context,
-              hash_book,
-              hash_chapter,
-              app_verse_lambda,
-            );
-          }
-        } else {
-          await app_record_book(context, hash_book, app_verse_lambda);
-        }
-      } else {
-        await app_record_home(context, app_verse_lambda);
-      }
+      await app_record_lookup(
+        context,
+        version_code,
+        app_lambda,
+        app_verse_lambda,
+      );
     });
   }
 }
---
+++ public/src/app_record_lookup.mjs
@@ -0,0 +1,40 @@
+import { app_record_home } from "./app_record_home.mjs";
+import { app_record_book } from "./app_record_book.mjs";
+import { app_record_chapter } from "./app_record_chapter.mjs";
+import { object_property_get_or } from "./object_property_get_or.mjs";
+import { object_merge } from "./object_merge.mjs";
+import { app_save_get } from "./app_save_get.mjs";
+import { html_hash_lookup } from "./html_hash_lookup.mjs";
+import { app_context_books_bible_generic } from "./app_context_books_bible_generic.mjs";
+export async function app_record_lookup(
+  context,
+  version_code,
+  app_lambda,
+  app_verse_lambda,
+) {
+  await app_context_books_bible_generic(context, version_code);
+  let lookup = html_hash_lookup();
+  let save = app_save_get(app_lambda);
+  object_merge(lookup, save);
+  let hash_book = object_property_get_or(lookup, "book", null);
+  if (hash_book !== null) {
+    let hash_chapter = object_property_get_or(lookup, "chapter", null);
+    if (hash_chapter !== null) {
+      let hash_verse = object_property_get_or(lookup, "verse", null);
+      if (hash_verse !== null) {
+        await app_verse_lambda(context, hash_book, hash_chapter, hash_verse);
+      } else {
+        await app_record_chapter(
+          context,
+          hash_book,
+          hash_chapter,
+          app_verse_lambda,
+        );
+      }
+    } else {
+      await app_record_book(context, hash_book, app_verse_lambda);
+    }
+  } else {
+    await app_record_home(context, app_verse_lambda);
+  }
+}
