4343862e1990bd5ce31e777b67a859da1fd6be4d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 10:07:04 -0500

    2024-11-29T15:07:03.357Z watch   js_imports_sources

---
+++ public/src/js_import_declaration_source_value.mjs
@@ -0,0 +1,5 @@
+export function js_import_declaration_source_value(node) {
+  let { source } = node;
+  let { value } = source;
+  return value;
+}
--- public/src/js_imports_sources.mjs
+++ public/src/js_imports_sources.mjs
@@ -1,12 +1,11 @@
+import { js_import_declaration_source_value } from "./js_import_declaration_source_value.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_imports_sources(ast) {
   let existing = list_adder((la) => {
     js_visit_node(ast, "ImportDeclaration", (v) => {
       let { node } = v;
-      let { source } = node;
-      let { value } = source;
-      la(value);
+      la(js_import_declaration_source_value(node));
     });
   });
   return existing;
