b5c6013056abbbb63046adfd4dee46be4c188f7f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 08:30:39 -0500

    2024-11-29T13:30:39.094Z watch   url_repository

--- public/src/url_repository.mjs
+++ public/src/url_repository.mjs
@@ -3,6 +3,8 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function url_repository() {
   return string_combine_multiple([
     url_secure(),
-    "github.com/CHRISTrosetolife/dev/",
+    "github.com/",
+    "CHRISTrosetolife",
+    "/dev/",
   ]);
 }
