f8c8eff5fae732b23c03cf082d37b18f17623ff0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 18:12:19 -0500

    2024-11-29T23:12:18.733Z watch   app_record

---
+++ public/src/app_record_generic.mjs
@@ -0,0 +1,30 @@
+import { app_record_lookup } from "./app_record_lookup.mjs";
+import { html_recorder_media } from "./html_recorder_media.mjs";
+import { html_button_next } from "./html_button_next.mjs";
+import { app_pray_word } from "./app_pray_word.mjs";
+import { html_prayer } from "./html_prayer.mjs";
+import { html_style_a_plain } from "./html_style_a_plain.mjs";
+import { app_firebase } from "./app_firebase.mjs";
+export async function app_record_generic(
+  version_code,
+  app_lambda,
+  app_verse_lambda,
+) {
+  await app_firebase({
+    on_logged_in,
+    on_initialized: html_style_a_plain,
+  });
+  async function on_logged_in(context) {
+    let { root } = context;
+    html_prayer(root, app_pray_word());
+    html_button_next(root, async () => {
+      context.mr = await html_recorder_media();
+      await app_record_lookup(
+        context,
+        version_code,
+        app_lambda,
+        app_verse_lambda,
+      );
+    });
+  }
+}
