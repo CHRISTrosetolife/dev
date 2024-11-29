5296b2d71651a01da46516e8c8f780335d57db20
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 18:11:31 -0500

    2024-11-29T23:11:31.590Z watch   app_record

--- public/src/app_record.mjs
+++ public/src/app_record.mjs
@@ -8,6 +8,7 @@ import { html_recorder_media } from "./html_recorder_media.mjs";
 import { html_prayer } from "./html_prayer.mjs";
 import { app_pray_word } from "./app_pray_word.mjs";
 export async function app_record() {
+  let version_code = app_record_version_code();
   await app_firebase({
     on_logged_in,
     on_initialized: html_style_a_plain,
@@ -17,7 +18,6 @@ export async function app_record() {
     html_prayer(root, app_pray_word());
     html_button_next(root, async () => {
       context.mr = await html_recorder_media();
-      let version_code = app_record_version_code();
       let app_lambda = app_record;
       let app_verse_lambda = app_record_verse;
       await app_record_lookup(
