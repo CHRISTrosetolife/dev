7cfb09b445ab915499a08f8b3d65805622d004cd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-28 20:37:41 -0500

    2024-11-29T01:37:40.461Z watch   html_parse_tag_is

--- public/src/html_parse_tag_is.mjs
+++ public/src/html_parse_tag_is.mjs
@@ -1,6 +1,5 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { list_includes } from "./list_includes.mjs";
 export function html_parse_tag_is(parent) {
-  let type = object_property_get(parent, "type");
-  return list_includes([]);
+  return list_includes(["script", "tag"], object_property_get(parent, "type"));
 }
