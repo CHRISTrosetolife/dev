7c116481b2eaf4bf437eb632beda76aabe73686d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 18:38:42 -0500

    2024-11-29T23:38:42.129Z watch   app_read

--- public/src/app_read.mjs
+++ public/src/app_read.mjs
@@ -1,11 +1,5 @@
-import { app_record_verse } from "./app_record_verse.mjs";
-import { app_record } from "./app_record.mjs";
-import { app_record_version_code } from "./app_record_version_code.mjs";
+import { app_bible_verse } from "./app_bible_verse.mjs";
 import { app_record_generic } from "./app_record_generic.mjs";
 export async function app_read() {
-  await app_record_generic(
-    app_record_version_code(),
-    app_record,
-    app_record_verse,
-  );
+  await app_record_generic("engbsb", app_read, app_bible_verse);
 }
