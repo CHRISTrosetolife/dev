1accdd403316c3679cd3807c3eee5109d5212eba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 10:04:56 -0500

    2024-11-29T15:04:55.266Z watch   js_imports_existing

--- public/src/js_imports_existing.mjs
+++ public/src/js_imports_existing.mjs
@@ -4,6 +4,8 @@ export function js_imports_existing(ast) {
   let existing = list_adder((la) => {
     js_visit_node(ast, "ImportSpecifier", (v) => {
       let { node } = v;
+      let { parent } = v;
+      $_parent;
       let { imported } = node;
       if (imported.type === "Identifier") {
         la(imported.name);
