52d6c3cbd04d5b6da33cbbdd9e85bc64f553874b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 08:58:40 -0500

    2024-11-29T13:58:39.420Z watch   sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -3,13 +3,14 @@ import { list_filter } from "./list_filter.mjs";
 import { html_parse_tag_named } from "./html_parse_tag_named.mjs";
 import { html_parse_children } from "./html_parse_children.mjs";
 import { html_parse_visit_tag_single } from "./html_parse_visit_tag_single.mjs";
+import { list_empty_is } from "./list_empty_is.mjs";
 export async function sandbox() {
   return await apps_html_transform(transform);
   function transform(root, name) {
     let body = html_parse_visit_tag_single(root, "body");
     let cs = html_parse_children(body);
     let f = list_filter(cs, (c) => html_parse_tag_named(c, "script"));
-    if (false) {
+    if (list_empty_is(f)) {
     }
   }
 }
