f7daf5c3005cafacc2137d25df0023ec91c88880
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-28 20:37:12 -0500

    2024-11-29T01:37:11.354Z watch   html_parse_tag_is

--- public/src/html_parse_tag_is.mjs
+++ public/src/html_parse_tag_is.mjs
@@ -1,3 +1,6 @@
+import { object_property_get } from "./object_property_get.mjs";
+import { list_includes } from "./list_includes.mjs";
 export function html_parse_tag_is(parent) {
-  return parent.type === "tag";
+  let type = object_property_get(parent, "type");
+  return list_includes([]);
 }
