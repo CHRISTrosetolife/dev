340d6cac531ea8fee90f6c358eb7e28673b34823
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-29 10:21:04 -0500

    2024-11-29T15:21:04.268Z watch   js_code_array_string

--- public/src/js_code_array_string.mjs
+++ public/src/js_code_array_string.mjs
@@ -1,8 +1,8 @@
+import { js_code_string } from "./js_code_string.mjs";
 import { js_code_array } from "./js_code_array.mjs";
-import { string_delimit } from "./string_delimit.mjs";
 import { list_map } from "./list_map.mjs";
 export function js_code_array_string(split) {
-  let mapped = list_map(split, (s) => string_delimit(s));
+  let mapped = list_map(split, js_code_string);
   let list_code = js_code_array(mapped);
   return list_code;
 }
