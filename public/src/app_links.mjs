1b43d999618f424a4bc63b2e7f8a15da2ed0a02f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-30 18:02:17 -0500

    2024-11-30T23:02:17.127Z watch   app_links

--- public/src/app_links.mjs
+++ public/src/app_links.mjs
@@ -1,4 +1,6 @@
+import { app_index_choices_buttons } from "./app_index_choices_buttons.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_links() {
   let body = html_style_default_initialize();
+  app_index_choices_buttons(root, choices);
 }
