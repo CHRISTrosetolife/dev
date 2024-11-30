b8f66d9c2b8f265b3fe321a0448657c1e4d05878
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-30 18:03:20 -0500

    2024-11-30T23:03:20.079Z watch   app_links

--- public/src/app_links.mjs
+++ public/src/app_links.mjs
@@ -1,9 +1,11 @@
+import { apps_list } from "./apps_list.mjs";
 import { app_index_choices_buttons } from "./app_index_choices_buttons.mjs";
-import { apps_paths } from "./apps_paths.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { list_map } from "./list_map.mjs";
-export async function app_links() {
+export function app_links() {
   let body = html_style_default_initialize();
-  let choices = list_map(await apps_paths(), (p) => ({}));
+  let choices = list_map(apps_list(), (p) => ({
+    text: p,
+  }));
   app_index_choices_buttons(root, choices);
 }
