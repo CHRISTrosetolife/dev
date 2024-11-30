3bdc03d0271b99df08a6e732fb5284ca2993e4d1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-30 17:52:49 -0500

    2024-11-30T22:52:49.315Z watch   apps_html_transform

--- public/src/apps_html_transform.mjs
+++ public/src/apps_html_transform.mjs
@@ -1,22 +1,13 @@
+import { apps_paths } from "./apps_paths.mjs";
 import { app_extension_html } from "./app_extension_html.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { html_parse_parsed } from "./html_parse_parsed.mjs";
 import { file_transform } from "./file_transform.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { each_async } from "./each_async.mjs";
-import { string_suffix_without } from "./string_suffix_without.mjs";
-import { string_prefix_without } from "./string_prefix_without.mjs";
-import { string_slash_normalize_right } from "./string_slash_normalize_right.mjs";
-import { list_map } from "./list_map.mjs";
-import { folder_read_shallow } from "./folder_read_shallow.mjs";
 import { folder_path_public } from "./folder_path_public.mjs";
 export async function apps_html_transform(transform) {
-  let p = folder_path_public();
-  let e = app_extension_html();
-  let paths = await folder_read_shallow(p, e);
-  paths = list_map(paths, string_slash_normalize_right);
-  paths = list_map(paths, (q) => string_prefix_without(q, p));
-  paths = list_map(paths, (q) => string_suffix_without(q, e));
+  let paths = await apps_paths();
   await each_async(paths, async (name) => {
     let p = folder_path_public();
     let e = app_extension_html();
---
+++ public/src/apps_paths.mjs
@@ -0,0 +1,16 @@
+import { string_suffix_without } from "./string_suffix_without.mjs";
+import { string_prefix_without } from "./string_prefix_without.mjs";
+import { string_slash_normalize_right } from "./string_slash_normalize_right.mjs";
+import { list_map } from "./list_map.mjs";
+import { folder_read_shallow } from "./folder_read_shallow.mjs";
+import { app_extension_html } from "./app_extension_html.mjs";
+import { folder_path_public } from "./folder_path_public.mjs";
+export async function apps_paths() {
+  let p = folder_path_public();
+  let e = app_extension_html();
+  let paths = await folder_read_shallow(p, e);
+  paths = list_map(paths, string_slash_normalize_right);
+  paths = list_map(paths, (q) => string_prefix_without(q, p));
+  paths = list_map(paths, (q) => string_suffix_without(q, e));
+  return paths;
+}
