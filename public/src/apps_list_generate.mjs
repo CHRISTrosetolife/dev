7b74ddcc75e4952872674d84376f08b551db0ae0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-30 17:59:14 -0500

    2024-11-30T22:59:14.447Z watch   apps_list_generate

--- public/src/apps_list_generate.mjs
+++ public/src/apps_list_generate.mjs
@@ -1,6 +1,5 @@
-import { functions_list_generate } from "./functions_list_generate.mjs";
-import { function_names } from "./function_names.mjs";
+import { apps_paths } from "./apps_paths.mjs";
 import { generate_list_fn_name } from "./generate_list_fn_name.mjs";
 export async function apps_list_generate() {
-  await generate_list_fn_name(function_names, functions_list_generate);
+  await generate_list_fn_name(apps_paths, apps_list_generate);
 }
