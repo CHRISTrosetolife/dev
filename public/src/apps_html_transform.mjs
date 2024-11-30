dd0dc381b8290790c49afc1a14da2ac339ae8ca2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-30 17:52:33 -0500

    2024-11-30T22:52:32.510Z watch   apps_html_transform

--- public/src/apps_html_transform.mjs
+++ public/src/apps_html_transform.mjs
@@ -18,6 +18,8 @@ export async function apps_html_transform(transform) {
   paths = list_map(paths, (q) => string_prefix_without(q, p));
   paths = list_map(paths, (q) => string_suffix_without(q, e));
   await each_async(paths, async (name) => {
+    let p = folder_path_public();
+    let e = app_extension_html();
     let test_path = string_combine_multiple([p, name, e]);
     await file_transform(
       async (text) => {
