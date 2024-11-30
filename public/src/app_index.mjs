448c2de58a1112607780ff9b1496ffe492de003c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-30 18:01:55 -0500

    2024-11-30T23:01:55.101Z watch   app_index

--- public/src/app_index.mjs
+++ public/src/app_index.mjs
@@ -1,32 +1,10 @@
+import { app_index_choices_buttons } from "./app_index_choices_buttons.mjs";
 import { app_index_choices } from "./app_index_choices.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { each } from "./each.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
-import { window_open } from "./window_open.mjs";
-import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-import { object_property_exists } from "./object_property_exists.mjs";
 export function app_index() {
   let root = html_style_default_initialize();
   html_clear_scroll_top_centered(root);
   let choices = app_index_choices();
-  each(choices, (choice) => {
-    let url;
-    if (object_property_exists(choice, "page")) {
-      url = string_combine_multiple([
-        object_property_get(choice, "page"),
-        ".html",
-      ]);
-    } else {
-      url = object_property_get(choice, "href");
-    }
-    html_button_width_full_text_click(
-      root,
-      object_property_get(choice, "text"),
-      () => {
-        window_open(url);
-      },
-    );
-  });
+  app_index_choices_buttons(choices, root);
 }
---
+++ public/src/app_index_choices_buttons.mjs
@@ -0,0 +1,26 @@
+import { window_open } from "./window_open.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
+import { each } from "./each.mjs";
+export function app_index_choices_buttons(choices, root) {
+  each(choices, (choice) => {
+    let url;
+    if (object_property_exists(choice, "page")) {
+      url = string_combine_multiple([
+        object_property_get(choice, "page"),
+        ".html",
+      ]);
+    } else {
+      url = object_property_get(choice, "href");
+    }
+    html_button_width_full_text_click(
+      root,
+      object_property_get(choice, "text"),
+      () => {
+        window_open(url);
+      },
+    );
+  });
+}
