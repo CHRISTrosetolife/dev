f219b030bcdbf70e6e6bc47ef03bad3adb1c7c5a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-30 17:54:10 -0500

    2024-11-30T22:54:10.447Z watch   functions_list_generate

--- public/src/functions_list_generate.mjs
+++ public/src/functions_list_generate.mjs
@@ -1,7 +1,7 @@
 import { generate_list_generic } from "./generate_list_generic.mjs";
 import { function_names } from "./function_names.mjs";
 export async function functions_list_generate() {
-  let suffix = "-generate";
+  let suffix = "_generate";
   let name = "functions_list";
   let list = await function_names();
   await generate_list_generic(list, name);
