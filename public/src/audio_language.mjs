5d5c08ec2b6a33c6f1e490b7c1d62abb9e475ce9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 18:20:03 -0500

    2024-12-01T23:20:02.499Z watch   audio_language

--- public/src/audio_language.mjs
+++ public/src/audio_language.mjs
@@ -1,11 +1,11 @@
+import { list_find } from "./list_find.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { fn_name } from "./fn_name.mjs";
-import { list_filter } from "./list_filter.mjs";
 import { audio_language_list } from "./audio_language_list.mjs";
 export function audio_language(language) {
   let list = audio_language_list();
   let target = function_name_combine(fn_name("audio_language"), language);
-  list_filter(list, (e) => object_property_get(e, "name") === target);
+  let match = list_find(list, (e) => object_property_get(e, "name") === target);
   return list;
 }
