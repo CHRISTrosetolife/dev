729f644b37153d7d36b04bc13efae5cd6e2c836e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 18:24:38 -0500

    2024-12-01T23:24:37.965Z watch   audio_language

--- public/src/audio_language.mjs
+++ public/src/audio_language.mjs
@@ -1,6 +1,5 @@
 import { generate_list_prefix_plugin } from "./generate_list_prefix_plugin.mjs";
 import { audio_language_list } from "./audio_language_list.mjs";
 export function audio_language(language) {
-  let fn = audio_language_list;
-  return generate_list_prefix_plugin(fn, language);
+  return generate_list_prefix_plugin(audio_language_list, language);
 }
