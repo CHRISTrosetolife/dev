e2c5214aed02f9d4b9d39ace9be7cd60a2048d9c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 17:07:21 -0500

    2024-12-01T22:07:20.451Z watch   audio_language

--- public/src/audio_language.mjs
+++ public/src/audio_language.mjs
@@ -1,4 +1,6 @@
+import { audio_language_list } from "./audio_language_list.mjs";
 import { function_run_plugin } from "./function_run_plugin.mjs";
 export async function audio_language(language) {
+  let list = audio_language_list();
   return await function_run_plugin(audio_language, language);
 }
