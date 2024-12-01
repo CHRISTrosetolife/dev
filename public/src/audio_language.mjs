c93e4695280a7e8248fa2babce3288c0eb24cdfb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 18:22:04 -0500

    2024-12-01T23:22:03.290Z watch   audio_language

--- public/src/audio_language.mjs
+++ public/src/audio_language.mjs
@@ -1,11 +1,15 @@
 import { list_find } from "./list_find.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
-import { fn_name } from "./fn_name.mjs";
 import { audio_language_list } from "./audio_language_list.mjs";
+import { string_suffix_without } from "./string_suffix_without.mjs";
 export function audio_language(language) {
-  let list = audio_language_list();
-  let target = function_name_combine(fn_name("audio_language"), language);
+  let fn = audio_language_list;
+  let list = fn();
+  let target = function_name_combine(
+    string_suffix_without(object_property_get(fn, "name")),
+    language,
+  );
   let match = list_find(list, (e) => object_property_get(e, "name") === target);
   return match();
 }
