7c00fb0cfd7a7b6172a411554fe4bb263ae8e17e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-30 21:00:40 -0500

    2024-12-01T02:00:40.036Z watch   sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { each_async } from "./each_async.mjs";
 import { list_size } from "./list_size.mjs";
 import { apps_html_transform } from "./apps_html_transform.mjs";
 import { list_filter } from "./list_filter.mjs";
@@ -5,6 +6,7 @@ import { html_parse_tag_named } from "./html_parse_tag_named.mjs";
 import { html_parse_children } from "./html_parse_children.mjs";
 import { html_parse_visit_tag_single } from "./html_parse_visit_tag_single.mjs";
 export async function sandbox() {
+  await each_async(list, async (item) => {});
   return await apps_html_transform(transform);
   function transform(root, name) {
     let body = html_parse_visit_tag_single(root, "body");
