6d431a851c913b5f74abe12433ebcef097c8373f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 18:15:09 -0500

    2024-12-01T23:15:09.414Z watch   audio_language

--- public/src/audio_language.mjs
+++ public/src/audio_language.mjs
@@ -1,3 +1,4 @@
+import { audio_language_list } from "./audio_language_list.mjs";
 import { generate_list_prefix_prefix } from "./generate_list_prefix_prefix.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { generate_list_prefix_name } from "./generate_list_prefix_name.mjs";
@@ -5,7 +6,7 @@ import { fn_name } from "./fn_name.mjs";
 export function audio_language(language) {
   let name = generate_list_prefix_name(fn_name("audio_language"));
   let name_fn = eval(name);
-  let list = name_fn();
+  let list = audio_language_list();
   let target = string_combine_multiple([
     generate_list_prefix_prefix(prefix),
     language,
