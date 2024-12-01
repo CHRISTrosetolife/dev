7e51f3eac786bd35479af58bbd587702183d234e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 18:22:48 -0500

    2024-12-01T23:22:48.391Z watch   generate_list_prefix_name

--- public/src/generate_list_prefix_name.mjs
+++ public/src/generate_list_prefix_name.mjs
@@ -1,5 +1,8 @@
 import { function_name_separator } from "./function_name_separator.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function generate_list_prefix_name(prefix) {
-  return string_combine_multiple([prefix, function_name_separator(), "list"]);
+  return string_combine_multiple([
+    prefix,
+    string_combine_multiple([function_name_separator(), "list"]),
+  ]);
 }
