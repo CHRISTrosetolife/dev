48a2ec5a597ac57f1ddb4257211805bde6768a97
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 13:21:12 -0500

    2024-12-01T18:21:11.704Z watch   app_html_generate_global

--- public/src/app_html_generate_global.mjs
+++ public/src/app_html_generate_global.mjs
@@ -1,3 +1,5 @@
+import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
 export function app_html_generate_global() {
+  return js_code_statement_let_assign();
   return "let global = {};";
 }
