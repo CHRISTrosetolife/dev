f8531fc34aef33ddfbd4f56af45d0e31c1b4c78e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 17:01:47 -0500

    2024-12-01T22:01:47.437Z watch   function_run_plugin_string

--- public/src/function_run_plugin_string.mjs
+++ public/src/function_run_plugin_string.mjs
@@ -1,6 +1,11 @@
+import { function_name_separator } from "./function_name_separator.mjs";
 import { function_run } from "./function_run.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function function_run_plugin_string(prefix, plugin_suffix) {
-  let function_name = string_combine_multiple([prefix, "_", plugin_suffix]);
+  let function_name = string_combine_multiple([
+    prefix,
+    function_name_separator(),
+    plugin_suffix,
+  ]);
   return await function_run(function_name, []);
 }
