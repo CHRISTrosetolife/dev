0d39f6391135b7b54791f7fc057a48094e35041c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-30 20:47:49 -0500

    2024-12-01T01:47:48.473Z watch   apps_paths

--- public/src/apps_paths.mjs
+++ public/src/apps_paths.mjs
@@ -5,6 +5,7 @@ import { list_map } from "./list_map.mjs";
 import { folder_read_shallow } from "./folder_read_shallow.mjs";
 import { app_extension_html } from "./app_extension_html.mjs";
 import { folder_path_public } from "./folder_path_public.mjs";
+import { list_filter } from "./list_filter.mjs";
 export async function apps_paths() {
   let p = folder_path_public();
   let e = app_extension_html();
@@ -12,5 +13,6 @@ export async function apps_paths() {
   paths = list_map(paths, string_slash_normalize_right);
   paths = list_map(paths, (q) => string_prefix_without(q, p));
   paths = list_map(paths, (q) => string_suffix_without(q, e));
+  paths = list_filter(paths, (p) => p !== "404");
   return paths;
 }
