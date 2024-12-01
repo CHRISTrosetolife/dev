f938d12780ae28e3c095352831938b29aca739fe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 18:19:46 -0500

    2024-12-01T23:19:46.289Z watch   audio_language

--- public/src/audio_language.mjs
+++ public/src/audio_language.mjs
@@ -1,10 +1,11 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { audio_language_list } from "./audio_language_list.mjs";
 export function audio_language(language) {
   let list = audio_language_list();
-  let prefix = function_name_combine(fn_name("audio_language"), language);
-  list_filter(list);
+  let target = function_name_combine(fn_name("audio_language"), language);
+  list_filter(list, (e) => object_property_get(object, "property_name"));
   return list;
 }
