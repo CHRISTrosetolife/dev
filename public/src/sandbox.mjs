7a87ac0a0872749dd2cba30f5178c76715a848f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 13:14:29 -0500

    2024-12-01T18:14:28.878Z watch   sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -7,7 +7,7 @@ import { html_parse_children } from "./html_parse_children.mjs";
 import { html_parse_visit_tag_single } from "./html_parse_visit_tag_single.mjs";
 export async function sandbox() {
   return await apps_html_transform(transform);
-  function transform(root, name) {
+  function transform(root, name, parsed) {
     let body = html_parse_visit_tag_single(root, "body");
     let cs = html_parse_children(body);
     let f = list_filter(cs, (c) => html_parse_tag_named(c, "script"));
