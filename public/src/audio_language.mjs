f22fd029043239b67a8e15bc5a838fe86bae1b6e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 17:08:58 -0500

    2024-12-01T22:08:57.543Z watch   audio_language

--- public/src/audio_language.mjs
+++ public/src/audio_language.mjs
@@ -1,9 +1,9 @@
 import { generate_list_prefix_name } from "./generate_list_prefix_name.mjs";
 import { fn_name } from "./fn_name.mjs";
-import { audio_language_list } from "./audio_language_list.mjs";
 import { function_run_plugin } from "./function_run_plugin.mjs";
 export async function audio_language(language) {
   let name = generate_list_prefix_name(fn_name("audio_language"));
-  let list = audio_language_list();
+  let name_fn = eval(name);
+  let list = name_fn();
   return await function_run_plugin(audio_language, language);
 }
