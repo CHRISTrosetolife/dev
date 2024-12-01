18cd8647f54c3c3578360467b4ef0706f88a08d1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 17:00:51 -0500

    2024-12-01T22:00:50.772Z watch   generate_list_prefix

--- public/src/generate_list_prefix.mjs
+++ public/src/generate_list_prefix.mjs
@@ -1,1 +1,4 @@
-export function generate_list_prefix(prefix) {}
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function generate_list_prefix(prefix) {
+  prefix = string_combine_multiple([prefix, "_"]);
+}
