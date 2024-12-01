3e650cf389c6bfd487faa4c8e3d3f092961fd29f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 17:03:08 -0500

    2024-12-01T22:03:07.814Z watch   generate_list_prefix

--- public/src/generate_list_prefix.mjs
+++ public/src/generate_list_prefix.mjs
@@ -1,5 +1,7 @@
 import { function_name_separator } from "./function_name_separator.mjs";
+import { function_names } from "./function_names.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-export function generate_list_prefix(prefix) {
+export async function generate_list_prefix(prefix) {
   prefix = string_combine_multiple([prefix, function_name_separator()]);
+  let ns = await function_names();
 }
