6450695a2d26b13215137ee5d5b037f2ad14ee8a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 15:48:59 -0500

    2024-12-01T20:48:58.698Z watch   html_style_button_default_value

--- public/src/html_style_button_default_value.mjs
+++ public/src/html_style_button_default_value.mjs
@@ -1,5 +1,8 @@
 import { global_function_initialize } from "./global_function_initialize.mjs";
 let d = {};
 export function html_style_button_default_value() {
-  return global_function_initialize();
+  return global_function_initialize(
+    html_style_button_default_value,
+    () => ({}),
+  );
 }
