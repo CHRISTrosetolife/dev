8d6a75bcefb3f3500e369259494b9c1b166cb324
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-11-30 21:08:54 -0500

    2024-12-01T02:08:53.552Z watch   app_new

--- public/src/app_new.mjs
+++ public/src/app_new.mjs
@@ -1,9 +1,11 @@
+import { app_html_generate_dev } from "./app_html_generate_dev.mjs";
 import { app_identifier } from "./app_identifier.mjs";
 import { file_write } from "./file_write.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 import { app_html_generate } from "./app_html_generate.mjs";
 export async function app_new(name) {
   await app_html_generate(name);
+  await app_html_generate_dev(name);
   let name_prefixed = app_identifier(name);
   let open = true;
   await function_new_generic(
