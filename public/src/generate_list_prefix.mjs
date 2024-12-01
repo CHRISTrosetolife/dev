1ea047295730543c0afc2374f08b95ba8c9a998a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 18:13:43 -0500

    2024-12-01T23:13:42.829Z watch   generate_list_prefix

---
+++ public/src/generate_list_prefix.mjs
@@ -0,0 +1,14 @@
+import { generate_list_string } from "./generate_list_string.mjs";
+import { generate_list_prefix_prefix } from "./generate_list_prefix_prefix.mjs";
+import { generate_list_prefix_name } from "./generate_list_prefix_name.mjs";
+import { list_filter_starts_with } from "./list_filter_starts_with.mjs";
+import { function_names } from "./function_names.mjs";
+import { list_filter } from "./list_filter.mjs";
+export async function generate_list_prefix(prefix) {
+  prefix = generate_list_prefix_prefix(prefix);
+  let ns = await function_names();
+  let list = list_filter_starts_with(ns, prefix);
+  let name = generate_list_prefix_name(prefix);
+  list = list_filter(list, (l) => l !== name);
+  return await generate_list_string(list, name);
+}
