416da0843290eb817c9c6ec3a41a8f42be2a955e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 07:21:12 -0500

    2024-12-01T12:21:12.064Z watch   function_imports

--- public/src/function_imports.mjs
+++ public/src/function_imports.mjs
@@ -4,7 +4,8 @@ import { function_imports_recursive } from "./function_imports_recursive.mjs";
 export async function function_imports(function_name) {
   let function_names = [function_name];
   let found = {};
-  await each_async(list, async (item) => {});
-  await function_imports_recursive(found, function_name);
+  await each_async(function_names, async (function_name) => {
+    await function_imports_recursive(found, function_name);
+  });
   return found;
 }
