51397adb653581aff4d8a8993ec66ae003668c22
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 18:18:28 -0500

    2024-12-01T23:18:27.734Z watch   audio_language

--- public/src/audio_language.mjs
+++ public/src/audio_language.mjs
@@ -1,11 +1,7 @@
+import { list_filter } from "./list_filter.mjs";
 import { audio_language_list } from "./audio_language_list.mjs";
-import { generate_list_prefix_prefix } from "./generate_list_prefix_prefix.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function audio_language(language) {
   let list = audio_language_list();
-  let target = string_combine_multiple([
-    generate_list_prefix_prefix(prefix),
-    language,
-  ]);
+  list_filter(list);
   return list;
 }
