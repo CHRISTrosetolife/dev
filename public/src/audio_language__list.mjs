109baa395eee08bba1aa58598b41888a9e090489
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 18:14:54 -0500

    2024-12-01T23:14:53.161Z generate_list_prefix audio_language

---
+++ public/src/audio_language__list.mjs
@@ -0,0 +1,14 @@
+import { audio_language_list } from "./audio_language_list.mjs";
+import { audio_language_he } from "./audio_language_he.mjs";
+import { audio_language_en } from "./audio_language_en.mjs";
+import { audio_language_el } from "./audio_language_el.mjs";
+import { audio_language_ceb } from "./audio_language_ceb.mjs";
+export function audio_language__list() {
+  return [
+    audio_language_ceb,
+    audio_language_el,
+    audio_language_en,
+    audio_language_he,
+    audio_language_list,
+  ];
+}
