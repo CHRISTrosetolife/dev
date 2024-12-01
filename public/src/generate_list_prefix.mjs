8eee6c26f68287cadf86054ffcc29ec563baee4b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-01 17:05:12 -0500

    2024-12-01T22:05:11.615Z watch   generate_list_prefix

--- public/src/generate_list_prefix.mjs
+++ public/src/generate_list_prefix.mjs
@@ -3,7 +3,7 @@ import { list_filter_starts_with } from "./list_filter_starts_with.mjs";
 import { function_name_separator } from "./function_name_separator.mjs";
 import { function_names } from "./function_names.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-export async function generate_list_prefix(prefix, name) {
+export async function generate_list_prefix(prefix) {
   prefix = string_combine_multiple([prefix, function_name_separator()]);
   let ns = await function_names();
   let list = list_filter_starts_with(ns, prefix);
