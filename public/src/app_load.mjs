1a6ae25064d5bb892888a148760ba01da82b7221
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-02 16:22:08 -0500

    2024-12-02T21:22:07.125Z watch   app_load

--- public/src/app_load.mjs
+++ public/src/app_load.mjs
@@ -1,2 +1,5 @@
+import { app_prefix } from "./app_prefix.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_load(name) {
-    let name_prefixed = string_combine_multiple([app_prefix(), name]);}
+  let name_prefixed = string_combine_multiple([app_prefix(), name]);
+}
