bd5a9534d72cc1e330e1afc6e083372b4512388b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-02 15:36:50 -0500

    2024-12-02T20:36:50.143Z watch   function_deploy

--- public/src/function_deploy.mjs
+++ public/src/function_deploy.mjs
@@ -1,6 +1,6 @@
-import { storage_upload_object } from "./storage_upload_object.mjs";
+import { storage_upload_string } from "./storage_upload_string.mjs";
 import { function_dependencies_string } from "./function_dependencies_string.mjs";
 export async function function_deploy(name) {
   let code = await function_dependencies_string(name);
-  await storage_upload_object(code);
+  await storage_upload_string(code);
 }
