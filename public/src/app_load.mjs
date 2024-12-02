cec1f467a184dd5627299a494c8df910ec8ae7a0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-02 16:31:18 -0500

    2024-12-02T21:31:18.149Z watch   app_load

--- public/src/app_load.mjs
+++ public/src/app_load.mjs
@@ -1,5 +1,7 @@
+import { http_storage_get } from "./http_storage_get.mjs";
 import { app_prefix } from "./app_prefix.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-export function app_load(name) {
+export async function app_load(name) {
   let name_prefixed = string_combine_multiple([app_prefix(), name]);
+  await http_storage_get();
 }
