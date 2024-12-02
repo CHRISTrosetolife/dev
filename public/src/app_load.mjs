673e58d2f06e585f625c6e62c786dc4f3abb43eb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-02 16:33:24 -0500

    2024-12-02T21:33:23.642Z watch   app_load

--- public/src/app_load.mjs
+++ public/src/app_load.mjs
@@ -1,3 +1,4 @@
+import { html_document_body } from "./html_document_body.mjs";
 import { html_script_axios } from "./html_script_axios.mjs";
 import { log } from "./log.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -6,7 +7,7 @@ import { http_storage_get } from "./http_storage_get.mjs";
 import { app_prefix } from "./app_prefix.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function app_load(name) {
-  await html_script_axios();
+  await html_script_axios(html_document_body());
   let name_prefixed = string_combine_multiple([app_prefix(), name]);
   let v = await http_storage_get(function_deploy_path_version(name));
   let version = object_property_get(v, "version");
