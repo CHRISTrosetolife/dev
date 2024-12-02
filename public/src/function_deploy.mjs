df1428eee5e07f8718b03813938a2a3b85ef9f75
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-02 15:35:00 -0500

    2024-12-02T20:35:00.134Z watch   function_deploy

--- public/src/function_deploy.mjs
+++ public/src/function_deploy.mjs
@@ -1,1 +1,5 @@
-export function function_deploy(name) {}
+import { app_prefix } from "./app_prefix.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function function_deploy(name) {
+  let name_prefixed = string_combine_multiple([app_prefix(), name]);
+}
