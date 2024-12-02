4ec429135bc11bd3359c169e7b3e58bf8a73aad3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-02 15:43:46 -0500

    2024-12-02T20:43:45.507Z watch   file_name_json

--- public/src/file_name_json.mjs
+++ public/src/file_name_json.mjs
@@ -1,4 +1,4 @@
 import { string_combine } from "./string_combine.mjs";
-export function file_name_json(without_extension) {
-  return string_combine(without_extension, ".json");
+export function file_name_json(name) {
+  return string_combine(name, ".json");
 }
