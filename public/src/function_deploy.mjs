7506911bc578532c0489f485b2ed570541e77e9c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-02 15:36:42 -0500

    2024-12-02T20:36:41.478Z watch   function_deploy

--- public/src/function_deploy.mjs
+++ public/src/function_deploy.mjs
@@ -1,9 +1,6 @@
 import { storage_upload_object } from "./storage_upload_object.mjs";
 import { function_dependencies_string } from "./function_dependencies_string.mjs";
-import { app_prefix } from "./app_prefix.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function function_deploy(name) {
-  let name_prefixed = string_combine_multiple([app_prefix(), name]);
-  let code = await function_dependencies_string(name_prefixed);
-  await storage_upload_object();
+  let code = await function_dependencies_string(name);
+  await storage_upload_object(code);
 }
