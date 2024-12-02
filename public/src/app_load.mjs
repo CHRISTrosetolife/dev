3290bb006fc3637779c63eb70da3cd2763a88156
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-02 16:32:41 -0500

    2024-12-02T21:32:40.664Z watch   app_load

--- public/src/app_load.mjs
+++ public/src/app_load.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { function_deploy_path_version } from "./function_deploy_path_version.mjs";
 import { http_storage_get } from "./http_storage_get.mjs";
@@ -7,4 +8,7 @@ export async function app_load(name) {
   let name_prefixed = string_combine_multiple([app_prefix(), name]);
   let v = await http_storage_get(function_deploy_path_version(name));
   let version = object_property_get(v, "version");
+  log({
+    version,
+  });
 }
