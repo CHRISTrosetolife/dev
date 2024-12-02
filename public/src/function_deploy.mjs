3436a72ff6e0ca70f5e0463ef0fe7c3a4557b182
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-02 15:37:46 -0500

    2024-12-02T20:37:45.880Z watch   function_deploy

--- public/src/function_deploy.mjs
+++ public/src/function_deploy.mjs
@@ -1,6 +1,11 @@
+import { date_string_iso_file } from "./date_string_iso_file.mjs";
+import { path_join } from "./path_join.mjs";
 import { storage_upload_string } from "./storage_upload_string.mjs";
 import { function_dependencies_string } from "./function_dependencies_string.mjs";
 export async function function_deploy(name) {
   let code = await function_dependencies_string(name);
-  await storage_upload_string(code);
+  await storage_upload_string(
+    code,
+    path_join(["function", name, date_string_iso_file()]),
+  );
 }
