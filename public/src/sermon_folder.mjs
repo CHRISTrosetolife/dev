ace0041c9c04e0f9364974bf44f86a71ce3c2199
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-04 16:03:14 -0500

    2024-12-04T21:03:13.797Z watch   sermon_folder

--- public/src/sermon_folder.mjs
+++ public/src/sermon_folder.mjs
@@ -1,3 +1,6 @@
+import { folder_path_public } from "./folder_path_public.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function sermon_folder() {
-  return "./sermons/";
+  let a = string_combine_multiple([folder_path_public(), "sermons", "/"]);
+  return a;
 }
