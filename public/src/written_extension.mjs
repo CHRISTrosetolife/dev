6cd64066d58e9e69b5d3409d39732728aff9ff7a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-04 16:06:47 -0500

    2024-12-04T21:06:43.875Z watch   written_path

---
+++ public/src/written_extension.mjs
@@ -0,0 +1,3 @@
+export function written_extension() {
+  return ".txt";
+}
--- public/src/written_path.mjs
+++ public/src/written_path.mjs
@@ -1,7 +1,8 @@
+import { written_extension } from "./written_extension.mjs";
 import { path_join } from "./path_join.mjs";
 import { string_combine } from "./string_combine.mjs";
 export function written_path(written_folder, name) {
-  let file_name = string_combine(name, ".txt");
+  let file_name = string_combine(name, written_extension());
   let sermon_path_file = path_join([written_folder, file_name]);
   return sermon_path_file;
 }
