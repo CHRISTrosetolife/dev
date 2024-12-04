d1bb7c31c2b7682ac6a1d4d31049ea45f098d734
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-04 16:03:48 -0500

    2024-12-04T21:03:48.424Z watch   app_preach

--- public/src/app_preach.mjs
+++ public/src/app_preach.mjs
@@ -1,9 +1,11 @@
+import { html_script_axios } from "./html_script_axios.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { each } from "./each.mjs";
 import { sermons_list } from "./sermons_list.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-export function app_preach() {
+export async function app_preach() {
   let body = html_style_default_initialize();
+  await html_script_axios(body);
   let ss = sermons_list();
   each(ss, (s) => {
     html_button_width_full_text_click(body, s, () => {});
