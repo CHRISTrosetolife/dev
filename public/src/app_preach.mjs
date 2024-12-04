8bc3eb6747aabceb7fc1fea1d2230908b0a8d796
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-04 16:11:42 -0500

    2024-12-04T21:11:41.194Z watch   app_preach

--- public/src/app_preach.mjs
+++ public/src/app_preach.mjs
@@ -1,31 +1,8 @@
-import { sermon_folder_name } from "./sermon_folder_name.mjs";
-import { sermon_path_suffix } from "./sermon_path_suffix.mjs";
-import { log } from "./log.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { path_join } from "./path_join.mjs";
-import { http_get } from "./http_get.mjs";
-import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
+import { app_preach_main } from "./app_preach_main.mjs";
 import { html_script_axios } from "./html_script_axios.mjs";
-import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
-import { each } from "./each.mjs";
-import { sermons_list } from "./sermons_list.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_preach() {
   let root = html_style_default_initialize();
   await html_script_axios(root);
-  let ss = sermons_list();
-  each(ss, (s) => {
-    html_button_width_full_text_click(root, s, async () => {
-      html_clear_scroll_top(root);
-      let t = await http_get(
-        path_join([
-          string_combine_multiple(["/", sermon_folder_name()]),
-          string_combine_multiple([s, sermon_path_suffix()]),
-        ]),
-      );
-      log({
-        t,
-      });
-    });
-  });
+  app_preach_main(root);
 }
---
+++ public/src/app_preach_main.mjs
@@ -0,0 +1,26 @@
+import { html_p_text } from "./html_p_text.mjs";
+import { sermon_path_suffix } from "./sermon_path_suffix.mjs";
+import { sermon_folder_name } from "./sermon_folder_name.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { path_join } from "./path_join.mjs";
+import { http_get } from "./http_get.mjs";
+import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+import { each } from "./each.mjs";
+import { sermons_list } from "./sermons_list.mjs";
+export function app_preach_main(root) {
+  let ss = sermons_list();
+  each(ss, (s) => {
+    html_button_width_full_text_click(root, s, async () => {
+      html_clear_scroll_top(root);
+      html_button_width_full_text_click(root, s, async () => {});
+      let t = await http_get(
+        path_join([
+          string_combine_multiple(["/", sermon_folder_name()]),
+          string_combine_multiple([s, sermon_path_suffix()]),
+        ]),
+      );
+      html_p_text(t);
+    });
+  });
+}
