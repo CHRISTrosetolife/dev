26cf87d95d49b7fff79465953bfe624a26a3b280
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-05 18:21:38 -0500

    2024-12-05T23:21:38.415Z watch   sermons_list_generate

--- public/src/sermons_list_generate.mjs
+++ public/src/sermons_list_generate.mjs
@@ -1,5 +1,5 @@
 import { sermons } from "./sermons.mjs";
 import { generate_list_fn } from "./generate_list_fn.mjs";
 export async function sermons_list_generate() {
-  await generate_list_fn(sermons, sermons_list_generate);
+  return await generate_list_fn(sermons, sermons_list_generate);
 }
