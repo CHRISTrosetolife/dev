d6a008b0024aa9c5bc750f858466a7c4ec1f67f1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-05 18:22:37 -0500

    2024-12-05T23:22:36.947Z watch   app_grocery

--- public/src/app_grocery.mjs
+++ public/src/app_grocery.mjs
@@ -1,4 +1,17 @@
-import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-export function app_grocery() {
-  let body = html_style_default_initialize();
+import { app_todo_main } from "./app_todo_main.mjs";
+import { object_merge } from "./object_merge.mjs";
+import { firebase_storage_initialize } from "./firebase_storage_initialize.mjs";
+import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
+import { app_firebase } from "./app_firebase.mjs";
+export async function app_grocery() {
+  await app_firebase({
+    on_logged_in: async (context) => {
+      let index_path = app_todo_firebase_path_index();
+      let index = await firebase_storage_initialize(index_path);
+      object_merge(context, {
+        index,
+      });
+      app_todo_main(context);
+    },
+  });
 }
