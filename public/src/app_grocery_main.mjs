edc8d421ef12956144e94ade4a6a1bf98d74ec1e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-05 18:27:26 -0500

    2024-12-05T23:27:25.463Z watch   app_grocery_main

--- public/src/app_grocery_main.mjs
+++ public/src/app_grocery_main.mjs
@@ -2,6 +2,6 @@ import { object_property_get } from "./object_property_get.mjs";
 import { json_to } from "./json_to.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 export function app_grocery_main(context) {
-  let root2 = object_property_get(context, "root");
+  let root = object_property_get(context, "root");
   html_p_text(root, json_to(object_property_get(context, "index")));
 }
