784cdb5453a5fcbc3ab84e26deb5f173e1908ff3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-05 14:00:25 -0500

    2024-12-05T19:00:21.219Z watch   sermon_new

--- public/src/sermon_new.mjs
+++ public/src/sermon_new.mjs
@@ -1,6 +1,8 @@
+import { sermons_list_generate } from "./sermons_list_generate.mjs";
 import { written_new } from "./written_new.mjs";
 import { sermon_folder } from "./sermon_folder.mjs";
 export async function sermon_new(name) {
   let written_folder = sermon_folder();
   await written_new(written_folder, name);
+  await sermons_list_generate();
 }
