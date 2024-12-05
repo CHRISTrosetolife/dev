b6fdaf877b7a101cc799ace6607268c2cd6bb434
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-05 18:26:36 -0500

    2024-12-05T23:26:35.523Z watch   app_grocery

--- public/src/app_grocery.mjs
+++ public/src/app_grocery.mjs
@@ -1,17 +1,16 @@
-import { app_todo_main } from "./app_todo_main.mjs";
+import { app_grocery_main } from "./app_grocery_main.mjs";
 import { object_merge } from "./object_merge.mjs";
 import { firebase_storage_initialize } from "./firebase_storage_initialize.mjs";
-import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
 import { app_firebase } from "./app_firebase.mjs";
 export async function app_grocery() {
   await app_firebase({
     on_logged_in: async (context) => {
-      let index_path = 'grocery/index.json'
+      let index_path = "grocery/index.json";
       let index = await firebase_storage_initialize(index_path);
       object_merge(context, {
         index,
       });
-      app_todo_main(context);
+      app_grocery_main(context);
     },
   });
 }
