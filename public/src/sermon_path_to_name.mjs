2a917b97af142373f4027e1361b82f0249de2a82
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-10 17:16:26 -0500

    2024-12-10T22:16:25.986Z   s prefix

--- public/src/sermon_path_to_name.mjs
+++ public/src/sermon_path_to_name.mjs
@@ -1,6 +1,10 @@
+import { log } from "./log.mjs";
 import { sermon_path_suffix } from "./sermon_path_suffix.mjs";
 import { sermon_folder } from "./sermon_folder.mjs";
 import { string_without_surround } from "./string_without_surround.mjs";
 export function sermon_path_to_name(path) {
+  log({
+    path,
+  });
   return string_without_surround(path, sermon_folder(), sermon_path_suffix());
 }
