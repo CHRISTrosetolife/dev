a03f833882e330a0a7d2ff8f42564939b243504a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-10 14:36:29 -0500

    2024-12-10T19:36:28.943Z watch   app_todo_button_back_main

--- public/src/app_todo_button_back_main.mjs
+++ public/src/app_todo_button_back_main.mjs
@@ -2,6 +2,7 @@ import { html_button_back } from "./html_button_back.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { app_todo_main } from "./app_todo_main.mjs";
 export function app_todo_button_back_main(context) {
+  let app_main_lambda = app_todo_main;
   let { root } = context;
   html_clear_scroll_top(root);
   html_button_back(root, async () => {
