211aa3aded820f1c443b1fd9ce44074abea5d2fc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-10 14:40:51 -0500

    2024-12-10T19:40:49.436Z watch   app_todo_item_name

--- public/src/app_todo_item_name.mjs
+++ public/src/app_todo_item_name.mjs
@@ -1,11 +1,6 @@
-import { html_button_back_main } from "./html_button_back_main.mjs";
-import { firebase_save_index } from "./firebase_save_index.mjs";
+import { html_item_add } from "./html_item_add.mjs";
 import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
 import { app_todo_main } from "./app_todo_main.mjs";
-import { html_value_set } from "./html_value_set.mjs";
-import { html_value_get } from "./html_value_get.mjs";
-import { html_input_width_full_focus } from "./html_input_width_full_focus.mjs";
-import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function app_todo_item_name(
   context,
   button_text_first,
@@ -16,16 +11,14 @@ export function app_todo_item_name(
 ) {
   let app_main_lambda = app_todo_main;
   let index_path = app_todo_firebase_path_index();
-  let { root } = context;
-  html_button_width_full_text_click(root, button_text_first, () => {
-    html_button_back_main(context, app_main_lambda);
-    let input = html_input_width_full_focus(root);
-    html_value_set(input, value_initial);
-    html_button_width_full_text_click(root, button_text_second, async () => {
-      let value = html_value_get(input);
-      on_complete(value);
-      await firebase_save_index(context, index_path);
-      on_save();
-    });
-  });
+  html_item_add(
+    context,
+    button_text_first,
+    app_main_lambda,
+    value_initial,
+    button_text_second,
+    on_complete,
+    index_path,
+    on_save,
+  );
 }
---
+++ public/src/html_item_add.mjs
@@ -0,0 +1,29 @@
+import { firebase_save_index } from "./firebase_save_index.mjs";
+import { html_value_get } from "./html_value_get.mjs";
+import { html_value_set } from "./html_value_set.mjs";
+import { html_input_width_full_focus } from "./html_input_width_full_focus.mjs";
+import { html_button_back_main } from "./html_button_back_main.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+export function html_item_add(
+  context,
+  button_text_first,
+  app_main_lambda,
+  value_initial,
+  button_text_second,
+  on_complete,
+  index_path,
+  on_save,
+) {
+  let { root } = context;
+  html_button_width_full_text_click(root, button_text_first, () => {
+    html_button_back_main(context, app_main_lambda);
+    let input = html_input_width_full_focus(root);
+    html_value_set(input, value_initial);
+    html_button_width_full_text_click(root, button_text_second, async () => {
+      let value = html_value_get(input);
+      on_complete(value);
+      await firebase_save_index(context, index_path);
+      on_save();
+    });
+  });
+}
