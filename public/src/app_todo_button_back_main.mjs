ddbf8e24334a20e16c06b3b2ded07c989b868c6c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-10 14:37:01 -0500

    2024-12-10T19:37:00.548Z watch   app_todo_button_back_main

--- public/src/app_todo_button_back_main.mjs
+++ public/src/app_todo_button_back_main.mjs
@@ -1,11 +1,6 @@
-import { html_button_back } from "./html_button_back.mjs";
-import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
+import { app_button_back_main } from "./app_button_back_main.mjs";
 import { app_todo_main } from "./app_todo_main.mjs";
 export function app_todo_button_back_main(context) {
   let app_main_lambda = app_todo_main;
-  let { root } = context;
-  html_clear_scroll_top(root);
-  html_button_back(root, () => {
-    app_main_lambda(context);
-  });
+  app_button_back_main(context, app_main_lambda);
 }
