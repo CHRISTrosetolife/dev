64d9868fe3aca43627c33641f91b30c8e287db5c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-10 17:16:55 -0500

    2024-12-10T22:16:54.393Z watch   sermon_path_to_name

--- public/src/sermon_path_to_name.mjs
+++ public/src/sermon_path_to_name.mjs
@@ -1,10 +1,8 @@
-import { log } from "./log.mjs";
+import { string_prefix_without_try } from "./string_prefix_without_try.mjs";
 import { sermon_path_suffix } from "./sermon_path_suffix.mjs";
 import { sermon_folder } from "./sermon_folder.mjs";
 import { string_without_surround } from "./string_without_surround.mjs";
 export function sermon_path_to_name(path) {
-  log({
-    path,
-  });
+  path = string_prefix_without_try(path, "./");
   return string_without_surround(path, sermon_folder(), sermon_path_suffix());
 }
