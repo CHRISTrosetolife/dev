46919a650aaf91a2821a3866f221e2ca328fdedb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-10 14:42:32 -0500

    2024-12-10T19:42:30.392Z watch   app_todo_main

--- public/src/app_todo_main.mjs
+++ public/src/app_todo_main.mjs
@@ -1,7 +1,8 @@
+import { html_item_add } from "./html_item_add.mjs";
+import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
 import { string_add_item } from "./string_add_item.mjs";
 import { app_todo_item_add } from "./app_todo_item_add.mjs";
 import { app_todo_completed_view } from "./app_todo_completed_view.mjs";
-import { app_todo_item_name } from "./app_todo_item_name.mjs";
 import { app_todo_index_save_main } from "./app_todo_index_save_main.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { app_todo_daily } from "./app_todo_daily.mjs";
@@ -39,19 +40,26 @@ export function app_todo_main(context) {
     await app_todo_index_save_main(context);
   });
   let add_item = string_add_item();
-  app_todo_item_name(
+  let value_initial = "";
+  let app_main_lambda = app_todo_main;
+  let index_path = app_todo_firebase_path_index();
+  html_item_add(
     context,
     add_item,
+    app_main_lambda,
+    value_initial,
     add_item,
-    "",
-    function on_complete(value) {
-      let items = app_todo_items(context);
-      app_todo_item_add(items, value);
-    },
-    function on_save() {
-      app_todo_main(context);
-    },
+    on_complete,
+    index_path,
+    on_save,
   );
+  function on_complete(value) {
+    let items = app_todo_items(context);
+    app_todo_item_add(items, value);
+  }
+  function on_save() {
+    app_todo_main(context);
+  }
   app_todo_list(
     context,
     items,
