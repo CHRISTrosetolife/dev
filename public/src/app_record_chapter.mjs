de32e84e978d4d13e9bdfb2a57de44c5657d89ab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-14 12:59:17 -0500

    2024-12-14T17:59:12.924Z watch   app_record_chapter

--- public/src/app_record_chapter.mjs
+++ public/src/app_record_chapter.mjs
@@ -1,4 +1,3 @@
-import { app_record_verse } from "./app_record_verse.mjs";
 import { bible_book_chapter_text } from "./bible_book_chapter_text.mjs";
 import { html_p_text_centered } from "./html_p_text_centered.mjs";
 import { object_replace } from "./object_replace.mjs";
@@ -25,7 +24,7 @@ export async function app_record_chapter(
   );
   let { root } = context;
   let verses = await app_record_verses(book_code, chapter);
-  app_record_chapter_buttons(context, book_code, app_record_verse);
+  app_record_chapter_buttons(context, book_code, on_verse);
   html_p_text_centered(root, bible_book_chapter_text(book_code, chapter));
   each(verses, (verse) => {
     let { tokens, verse_number } = verse;
