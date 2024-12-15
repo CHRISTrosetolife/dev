4f4a310311c7be98b1c5fafba63bd696846f68a2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-15 14:52:59 -0500

    2024-12-15T19:52:59.141Z watch   app_grocery_main

---
+++ public/src/html_item_add.mjs
@@ -0,0 +1,17 @@
+import { html_item_add_generic } from "./html_item_add_generic.mjs";
+import { string_add_item } from "./string_add_item.mjs";
+export function html_item_add(app_main_lambda, context, index_path) {
+  let add_item = string_add_item();
+  let refresh = () => app_main_lambda(context);
+  let value_initial = "";
+  html_item_add_generic(
+    context,
+    add_item,
+    app_main_lambda,
+    value_initial,
+    add_item,
+    on_complete,
+    index_path,
+    refresh,
+  );
+}
