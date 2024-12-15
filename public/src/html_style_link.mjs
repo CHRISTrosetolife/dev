9cf287369f771ba2bc098f75e257cdba41a0396c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-15 11:11:36 -0500

    2024-12-15T16:11:35.616Z watch   html_style_link

--- public/src/html_style_link.mjs
+++ public/src/html_style_link.mjs
@@ -1,6 +1,7 @@
 import { html_a_blank } from "./html_a_blank.mjs";
 import { html_clear_inner_get } from "./html_clear_inner_get.mjs";
 export function html_style_link(url) {
+  let html_a_lambda = html_a_blank;
   return (c) => {
     let text = html_clear_inner_get(c);
     html_a_blank(c, text, url);
