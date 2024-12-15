81d3704e9c16e1ca91fcc2226a1fd066acfa6d0a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-15 14:39:15 -0500

    2024-12-15T19:39:14.265Z watch   app_todo_list_filtered

---
+++ public/src/app_todo_list.mjs
@@ -0,0 +1,36 @@
+import { app_todo_item_choices } from "./app_todo_item_choices.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+import { emoji_edit_combine } from "./emoji_edit_combine.mjs";
+import { app_todo_item_name } from "./app_todo_item_name.mjs";
+import { html_hr } from "./html_hr.mjs";
+import { app_todo_button_back_main } from "./app_todo_button_back_main.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { each } from "./each.mjs";
+import { app_todo_properties } from "./app_todo_properties.mjs";
+export function app_todo_list(filtered, root, context, refresh) {
+  let properties = app_todo_properties();
+  each(filtered, (item) => {
+    let name = object_property_get(item, "name");
+    html_button_width_full_text_click(root, name, () => {
+      app_todo_button_back_main(context);
+      html_hr(root);
+      app_todo_item_name(
+        context,
+        emoji_edit_combine(name),
+        emoji_edit_combine("save"),
+        name,
+        function on_complete(value) {
+          object_property_set(item, "name", value);
+        },
+        function on_save() {
+          refresh(context);
+        },
+      );
+      each(properties, (p) => {
+        html_hr(root);
+        app_todo_item_choices(context, item, p);
+      });
+    });
+  });
+}
--- public/src/app_todo_list_filtered.mjs
+++ public/src/app_todo_list_filtered.mjs
@@ -1,12 +1,4 @@
-import { object_property_set } from "./object_property_set.mjs";
-import { emoji_edit_combine } from "./emoji_edit_combine.mjs";
-import { app_todo_item_name } from "./app_todo_item_name.mjs";
-import { app_todo_properties } from "./app_todo_properties.mjs";
-import { app_todo_item_choices } from "./app_todo_item_choices.mjs";
-import { app_todo_button_back_main } from "./app_todo_button_back_main.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
-import { each } from "./each.mjs";
+import { app_todo_list } from "./app_todo_list.mjs";
 import { list_filter_property } from "./list_filter_property.mjs";
 import { html_hr } from "./html_hr.mjs";
 export function app_todo_list_filtered(
@@ -19,28 +11,5 @@ export function app_todo_list_filtered(
   let { root } = context;
   html_hr(root);
   let filtered = list_filter_property(items, property_name, value);
-  let properties = app_todo_properties();
-  each(filtered, (item) => {
-    let name = object_property_get(item, "name");
-    html_button_width_full_text_click(root, name, () => {
-      app_todo_button_back_main(context);
-      html_hr(root);
-      app_todo_item_name(
-        context,
-        emoji_edit_combine(name),
-        emoji_edit_combine("save"),
-        name,
-        function on_complete(value) {
-          object_property_set(item, "name", value);
-        },
-        function on_save() {
-          refresh(context);
-        },
-      );
-      each(properties, (p) => {
-        html_hr(root);
-        app_todo_item_choices(context, item, p);
-      });
-    });
-  });
+  app_todo_list(filtered, root, context, refresh);
 }
