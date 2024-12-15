1bd2ad3b75eaab8e9c533341e00a105045818550
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-15 15:17:40 -0500

    2024-12-15T20:17:39.648Z watch   app_grocery_main

--- public/src/app_grocery_main.mjs
+++ public/src/app_grocery_main.mjs
@@ -1,1 +1,6 @@
-export function app_grocery_main(context) {}
+import { object_property_get } from "./object_property_get.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+export function app_grocery_main(context) {
+  let root = object_property_get(context, "root");
+  html_button_width_full_text_click(root);
+}
