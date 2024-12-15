b131174290999bc3959d8ce5d295988de31879fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-15 15:11:24 -0500

    2024-12-15T20:11:22.766Z watch   app_grocery_main

--- public/src/app_grocery_main.mjs
+++ public/src/app_grocery_main.mjs
@@ -5,14 +5,11 @@ import { app_grocery_firebase_path_index } from "./app_grocery_firebase_path_ind
 import { html_item_add } from "./html_item_add.mjs";
 import { app_todo_list } from "./app_todo_list.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { json_to } from "./json_to.mjs";
-import { html_p_text } from "./html_p_text.mjs";
 export function app_grocery_main(context) {
   let root = object_property_get(context, "root");
   html_clear_scroll_top(root);
   let items = app_grocery_items(context);
   let index = object_property_get(context, "index");
-  html_p_text(root, json_to(index));
   html_item_add(
     context,
     app_grocery_main,
