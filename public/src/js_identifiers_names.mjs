4876504995a3a1b9e61171b80cca1c9d5efad84a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-15 14:45:12 -0500

    2024-12-15T19:45:11.890Z watch   js_identifiers_names

--- public/src/js_identifiers_names.mjs
+++ public/src/js_identifiers_names.mjs
@@ -1,5 +1,4 @@
 import { list_map_property } from "./list_map_property.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 export function js_identifiers_names(args) {
-  return list_map_property(args, (a) => object_property_get(a, "name"));
+  return list_map_property(args, "name");
 }
