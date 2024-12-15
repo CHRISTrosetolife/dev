a8053f240a2c531caf0b427cb233fdfd7c7855fd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-15 11:32:30 -0500

    2024-12-15T16:32:29.432Z watch   html_style_link

--- public/src/html_style_link.mjs
+++ public/src/html_style_link.mjs
@@ -1,5 +1,5 @@
+import { html_a } from "./html_a.mjs";
 import { html_style_link_generic } from "./html_style_link_generic.mjs";
-import { html_a_blank } from "./html_a_blank.mjs";
 export function html_style_link(url) {
-  return html_style_link_generic(html_a_blank, url);
+  return html_style_link_generic(html_a, url);
 }
