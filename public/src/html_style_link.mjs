c706ab7629dd2a19c8af40d7afc99dd33d33eeb9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-15 11:12:47 -0500

    2024-12-15T16:12:47.066Z watch   html_style_link

--- public/src/html_style_link.mjs
+++ public/src/html_style_link.mjs
@@ -1,6 +1,5 @@
 import { html_style_link_generic } from "./html_style_link_generic.mjs";
 import { html_a_blank } from "./html_a_blank.mjs";
 export function html_style_link(url) {
-  let html_a_lambda = html_a_blank;
-  return html_style_link_generic(html_a_lambda, url);
+  return html_style_link_generic(html_a_blank, url);
 }
