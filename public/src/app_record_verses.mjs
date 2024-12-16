d033ad3865b93191aedc54229296c55a1b79c4bb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-16 16:52:54 -0500

    2024-12-16T21:52:53.185Z watch   app_record_chapter

--- public/src/app_record_verses.mjs
+++ public/src/app_record_verses.mjs
@@ -2,6 +2,6 @@ import { app_record } from "./app_record.mjs";
 import { app_verses_generic } from "./app_verses_generic.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function app_record_verses(context, book_code, chapter) {
-  assert_arguments_length(arguments, 2);
+  assert_arguments_length(arguments, 3);
   return await app_verses_generic(context, app_record, book_code, chapter);
 }
