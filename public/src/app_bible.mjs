ca664921b16100526414eda39e2655ab70506c05
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-24 18:52:49 -0500

    2024-12-24T23:52:48.737Z watch   app_bible

--- public/src/app_bible.mjs
+++ public/src/app_bible.mjs
@@ -1,5 +1,5 @@
 import { app_record_generic } from "./app_record_generic.mjs";
 import { app_bible_verse } from "./app_bible_verse.mjs";
 export async function app_bible() {
-  await app_record_generic("engbsb", app_bible, app_bible_verse, true);
+  await app_record_generic("engbsb", app_bible, app_bible_verse, false);
 }
