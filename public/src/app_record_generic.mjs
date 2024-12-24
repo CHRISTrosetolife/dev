52cf484d7a74b9693cb4181753c26e4b6bea0705
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-24 18:52:19 -0500

    2024-12-24T23:52:19.528Z watch   app_record_generic

--- public/src/app_record_generic.mjs
+++ public/src/app_record_generic.mjs
@@ -21,7 +21,9 @@ export async function app_record_generic(
     object_property_set(context, "app_lambda", app_lambda);
     html_prayer(root, app_pray_word());
     html_button_next(root, async () => {
-      context.mr = await html_recorder_media();
+      if (false) {
+        context.mr = await html_recorder_media();
+      }
       object_property_set(context, "version_code", version_code);
       await app_record_lookup(context, app_verse_lambda);
     });
