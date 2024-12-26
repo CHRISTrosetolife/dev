0825a287bbff75dcb44c6372e2e3d7ea0fa7427c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-26 13:59:13 -0500

    2024-12-26T18:59:12.933Z watch   app_grocery_items_list

--- public/src/app_grocery_items_list.mjs
+++ public/src/app_grocery_items_list.mjs
@@ -1,10 +1,10 @@
+import { app_todo_list_generic } from "./app_todo_list_generic.mjs";
 import { app_grocery_main } from "./app_grocery_main.mjs";
 import { html_button_back_main } from "./html_button_back_main.mjs";
 import { app_grocery_items } from "./app_grocery_items.mjs";
 import { app_todo_item_add } from "./app_todo_item_add.mjs";
 import { app_grocery_firebase_path_index } from "./app_grocery_firebase_path_index.mjs";
 import { html_item_add } from "./html_item_add.mjs";
-import { app_todo_list } from "./app_todo_list.mjs";
 export function app_grocery_items_list(context) {
   html_button_back_main(context, app_grocery_main);
   html_item_add(
@@ -15,7 +15,7 @@ export function app_grocery_items_list(context) {
   );
   let refresh = () => app_grocery_items_list(context);
   let items = app_grocery_items(context);
-  app_todo_list(context, items, refresh);
+  app_todo_list_generic(context, items, refresh, []);
   function on_complete(value) {
     let items = app_grocery_items(context);
     app_todo_item_add(items, value);
