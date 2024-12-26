238c63f8e3152c29283d2a65f71772aead63b85e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-26 13:59:00 -0500

    2024-12-26T18:58:59.833Z watch   app_todo_list

---
+++ public/src/app_todo_list_generic.mjs
@@ -0,0 +1,36 @@
+import { app_todo_item_choices } from "./app_todo_item_choices.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+import { emoji_edit_combine } from "./emoji_edit_combine.mjs";
+import { app_todo_item_name } from "./app_todo_item_name.mjs";
+import { html_button_back_main } from "./html_button_back_main.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { each } from "./each.mjs";
+import { html_hr } from "./html_hr.mjs";
+export function app_todo_list_generic(context, filtered, refresh, properties) {
+  let { root } = context;
+  html_hr(root);
+  each(filtered, (item) => {
+    let name = object_property_get(item, "name");
+    html_button_width_full_text_click(root, name, () => {
+      html_button_back_main(context, refresh);
+      html_hr(root);
+      app_todo_item_name(
+        context,
+        emoji_edit_combine(name),
+        emoji_edit_combine("save"),
+        name,
+        function on_complete(value) {
+          object_property_set(item, "name", value);
+        },
+        function on_save() {
+          refresh(context);
+        },
+      );
+      each(properties, (p) => {
+        html_hr(root);
+        app_todo_item_choices(context, item, p);
+      });
+    });
+  });
+}
