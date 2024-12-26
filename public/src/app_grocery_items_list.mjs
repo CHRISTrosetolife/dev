3f0fceeb857c0f7ef800b474b4d76b4dc74f3621
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-26 14:05:11 -0500

    2024-12-26T19:05:10.659Z watch   app_grocery_items_list

--- public/src/app_grocery_items_list.mjs
+++ public/src/app_grocery_items_list.mjs
@@ -1,4 +1,3 @@
-import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
 import { app_todo_list_generic } from "./app_todo_list_generic.mjs";
 import { app_grocery_main } from "./app_grocery_main.mjs";
 import { html_button_back_main } from "./html_button_back_main.mjs";
@@ -12,13 +11,7 @@ export function app_grocery_items_list(context) {
   html_item_add(context, app_grocery_items_list, index_path, on_complete);
   let refresh = () => app_grocery_items_list(context);
   let items = app_grocery_items(context);
-  app_todo_list_generic(
-    context,
-    items,
-    refresh,
-    [],
-    app_todo_firebase_path_index(),
-  );
+  app_todo_list_generic(context, items, refresh, [], index_path);
   function on_complete(value) {
     let items = app_grocery_items(context);
     app_todo_item_add(items, value);
