a98a1a5d655eb722c2a52233c54c6c1be5001845
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-28 11:31:46 -0500

    2024-12-28T16:31:45.808Z watch   app_grocery_need

--- public/src/app_grocery_need.mjs
+++ public/src/app_grocery_need.mjs
@@ -1,1 +1,4 @@
-export function app_grocery_need(context) {}
+import { html_clear_scroll_top_context } from "./html_clear_scroll_top_context.mjs";
+export function app_grocery_need(context) {
+  let root = html_clear_scroll_top_context(context);
+}
