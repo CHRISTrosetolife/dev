4d3ae76316db6065c43f5ab5409e89800a19f94a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-28 11:38:23 -0500

    2024-12-28T16:38:23.103Z watch   app_grocery_need

--- public/src/app_grocery_need.mjs
+++ public/src/app_grocery_need.mjs
@@ -1,7 +1,6 @@
-import { app_grocery_main } from "./app_grocery_main.mjs";
 import { html_button_back_main } from "./html_button_back_main.mjs";
 import { app_grocery_items } from "./app_grocery_items.mjs";
 export function app_grocery_need(context) {
-  html_button_back_main(context, app_grocery_main);
+  html_button_back_main(context, app_grocery_need);
   let items = app_grocery_items(context);
 }
