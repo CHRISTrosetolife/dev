256032067af79898407409f85f162d22011fd2a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-28 11:51:52 -0500

    2024-12-28T16:51:51.773Z watch   app_grocery_need

--- public/src/app_grocery_need.mjs
+++ public/src/app_grocery_need.mjs
@@ -1,3 +1,4 @@
+import { object_property_initialize } from "./object_property_initialize.mjs";
 import { app_grocery_index_need } from "./app_grocery_index_need.mjs";
 import { app_grocery_index_items } from "./app_grocery_index_items.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
@@ -10,6 +11,7 @@ export function app_grocery_need(context) {
   let need = app_grocery_index_need(context);
   html_hr_each(root, items, (item) => {
     let name = object_property_get(item, "name");
+    let count = object_property_initialize(index, "need", {});
     html_button_width_full_text_click(root, name, () => {});
   });
 }
