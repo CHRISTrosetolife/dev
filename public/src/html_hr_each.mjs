414028ffc0dbd5134fa7994b801ebf0442f02023
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-28 11:46:08 -0500

    2024-12-28T16:46:07.252Z watch   html_hr_each

--- public/src/html_hr_each.mjs
+++ public/src/html_hr_each.mjs
@@ -1,1 +1,6 @@
-export function html_hr_each(parent, list, lambda) {}
+import { each } from "./each.mjs";
+import { html_hr } from "./html_hr.mjs";
+export function html_hr_each(parent, list, lambda) {
+  html_hr(parent);
+  each(list, lambda);
+}
