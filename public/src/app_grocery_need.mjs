441feb46f8c54c1c901c24801940f1b1caf1a756
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-28 11:49:28 -0500

    2024-12-28T16:49:27.846Z watch   app_grocery_need

--- public/src/app_grocery_need.mjs
+++ public/src/app_grocery_need.mjs
@@ -1,3 +1,4 @@
+import { app_grocery_index_need } from "./app_grocery_index_need.mjs";
 import { app_grocery_index_items } from "./app_grocery_index_items.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -6,6 +7,7 @@ import { html_button_back_main } from "./html_button_back_main.mjs";
 export function app_grocery_need(context) {
   let root = html_button_back_main(context, app_grocery_need);
   let items = app_grocery_index_items(context);
+  let need = app_grocery_index_need(context);
   html_hr_each(root, items, (item) => {
     let name = object_property_get(item, "name");
     html_button_width_full_text_click(root, name, () => {});
