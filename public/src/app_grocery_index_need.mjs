f19be078302085517cd310e13c46e961c365fe34
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-28 11:49:13 -0500

    2024-12-28T16:49:12.752Z watch   app_grocery_index_need

--- public/src/app_grocery_index_need.mjs
+++ public/src/app_grocery_index_need.mjs
@@ -2,6 +2,6 @@ import { object_property_initialize } from "./object_property_initialize.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function app_grocery_index_need(context) {
   let index = object_property_get(context, "index");
-  let items = object_property_initialize(index, "items", []);
+  let items = object_property_initialize(index, "need", {});
   return items;
 }
