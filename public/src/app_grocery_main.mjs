2516549ab80b691f507c4a62bdd5cdd8138088ca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2024-12-28 11:30:51 -0500

    2024-12-28T16:30:46.151Z watch   app_grocery_main

--- public/src/app_grocery_main.mjs
+++ public/src/app_grocery_main.mjs
@@ -1,10 +1,8 @@
-import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
+import { html_clear_scroll_top_context } from "./html_clear_scroll_top_context.mjs";
 import { app_grocery_items_list } from "./app_grocery_items_list.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function app_grocery_main(context) {
-  let root = object_property_get(context, "root");
-  html_clear_scroll_top(root);
+  let root = html_clear_scroll_top_context(context);
   html_button_width_full_text_click(root, "items", () => {
     app_grocery_items_list(context);
   });
