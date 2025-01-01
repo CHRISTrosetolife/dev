1e6d3f18378bd574c37e7b39af327fc10ae3c4ac
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-01 11:33:54 -0500

    2025-01-01T16:33:51.525Z watch   bible_verses_upload_path

--- public/src/app_context_books_bible_generic.mjs
+++ public/src/app_context_books_bible_generic.mjs
@@ -1,7 +1,7 @@
 import { object_merge } from "./object_merge.mjs";
 import { bible_storage_version_http_get } from "./bible_storage_version_http_get.mjs";
 export async function app_context_books_bible_generic(context, version_code) {
-  let { books } = await bible_storage_version_http_get("books", version_code);
+  let { books } = await bible_storage_version_http_get(version_code, "books");
   object_merge(context, {
     books,
   });
---
+++ public/src/bible_verses_upload_path.mjs
@@ -0,0 +1,9 @@
+import { bible_chapter_name_parse } from "./bible_chapter_name_parse.mjs";
+import { bible_storage_path_file_version } from "./bible_storage_path_file_version.mjs";
+import { path_join } from "./path_join.mjs";
+export function bible_verses_upload_path(bible_folder, chapter, verse_number) {
+  let { book_code, chapter_code } = bible_chapter_name_parse(chapter);
+  let key = path_join([book_code, chapter_code, verse_number]);
+  let storage_path = bible_storage_path_file_version(bible_folder, key);
+  return storage_path;
+}
