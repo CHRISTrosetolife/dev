2fbe3aa16e4dde5416c0a503e50a35c89320b180
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-03 20:59:12 -0500

    2025-01-04T01:59:11.267Z watch   lesson_console_log_javascript

--- public/src/lesson_console_log_javascript.mjs
+++ public/src/lesson_console_log_javascript.mjs
@@ -1,7 +1,6 @@
 import { html_cycle_list } from "./html_cycle_list.mjs";
 import { html_style_code } from "./html_style_code.mjs";
 import { html_p } from "./html_p.mjs";
-import { html_style_monospace } from "./html_style_monospace.mjs";
 import { noop } from "./noop.mjs";
 import { html_style_bold } from "./html_style_bold.mjs";
 export function lesson_console_log_javascript(parent) {
@@ -30,6 +29,7 @@ export function lesson_console_log_javascript(parent) {
     [noop, html_style_code],
     ["at the end of a statement there should be a semicolon ( ", ";", " )"],
   );
+  return;
   html_cycle_list(
     parent,
     html_p,
