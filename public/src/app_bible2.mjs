fba9a83002a26d57b5151adbbbc397f5a86f4548
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-05 20:45:10 -0500

    2025-01-06T01:45:09.186Z watch   app_bible2

--- public/src/app_bible2.mjs
+++ public/src/app_bible2.mjs
@@ -1,6 +1,5 @@
 import { app_bible_verse } from "./app_bible_verse.mjs";
-import { app_bible } from "./app_bible.mjs";
 import { app_record_generic } from "./app_record_generic.mjs";
 export async function app_bible2() {
-  await app_record_generic("engbsb", app_bible, app_bible_verse, false);
+  await app_record_generic("engbsb", app_bible2, app_bible_verse, false);
 }
