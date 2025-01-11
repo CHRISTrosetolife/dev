efbfe00d3a16669d18c3523c78064ec89f1e9f1b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-11 16:10:51 -0500

    2025-01-11T21:10:51.007Z watch   object_property_get_or_async

--- public/src/object_property_get_or_async.mjs
+++ public/src/object_property_get_or_async.mjs
@@ -1,5 +1,6 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
+import { object_property_set } from "./object_property_set.mjs";
 export async function object_property_get_or_async(
   n,
   property_name,
@@ -8,8 +9,8 @@ export async function object_property_get_or_async(
   let result;
   if (object_property_exists_not(n, property_name)) {
     result = await default_value_get();
-  } else {
-    result = object_property_get(n, property_name);
+    object_property_set(n, property_name, result);
   }
+  result = object_property_get(n, property_name);
   return result;
 }
