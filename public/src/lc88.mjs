a34c713ebc7dd4b6fc27012fddd596adcbe7a6b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-12 16:46:19 -0500

    2025-01-12T21:46:17.613Z watch   lc88

--- public/src/lc88.mjs
+++ public/src/lc88.mjs
@@ -1,3 +1,4 @@
+import { list_set } from "./list_set.mjs";
 import { list_get } from "./list_get.mjs";
 export function lc88(nums1, m, nums2, n) {
   let index_1 = m - 1;
@@ -5,5 +6,6 @@ export function lc88(nums1, m, nums2, n) {
   let value_1 = list_get(nums1, index_1);
   let value_2 = list_get(nums2, index_2);
   if (value_1 > value_2) {
+    list_set();
   }
 }
