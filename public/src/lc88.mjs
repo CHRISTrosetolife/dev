162e77379603b22ef18520dd304873d1da8b78ff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-12 16:42:31 -0500

    2025-01-12T21:42:29.759Z watch   lc88

--- public/src/lc88.mjs
+++ public/src/lc88.mjs
@@ -1,4 +1,5 @@
 import { list_index_last } from "./list_index_last.mjs";
 export function lc88(nums1, m, nums2, n) {
-  list_index_last(nums1);
+  let pointer_1 = list_index_last(nums1);
+  let pointer_2 = list_index_last(nums2);
 }
