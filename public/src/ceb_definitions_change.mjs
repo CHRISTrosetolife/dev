2de15cc48449865b39224a5b07a8bf35dd50dd83
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-12 13:12:40 -0500

    2025-01-12T18:12:38.922Z watch   ceb_definitions_change

--- public/src/ceb_definitions_change.mjs
+++ public/src/ceb_definitions_change.mjs
@@ -1,8 +1,8 @@
+import { function_cache_refresh_args } from "./function_cache_refresh_args.mjs";
 import { app_language_upload } from "./app_language_upload.mjs";
 import { fn_name } from "./fn_name.mjs";
-import { function_cache_refresh } from "./function_cache_refresh.mjs";
 export async function ceb_definitions_change() {
-  await function_cache_refresh(fn_name("ceb_bible_words_definitions_all"));
-  await function_cache_refresh(fn_name("ceb_bible_words_definitions_atoms"));
+  await function_cache_refresh_args(fn_name("bible_words_definitions_all"));
+  await function_cache_refresh_args(fn_name("bible_words_definitions_atoms"));
   await app_language_upload("ceb");
 }
