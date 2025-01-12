68bdf8efcbae98b3754501cf7ddc81718fa0730a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-12 16:45:22 -0500

    2025-01-12T21:45:20.570Z watch   lc88

--- public/src/lc88.mjs
+++ public/src/lc88.mjs
@@ -1,6 +1,7 @@
-import { each_range_reverse } from "./each_range_reverse.mjs";
+import { list_get } from "./list_get.mjs";
 export function lc88(nums1, m, nums2, n) {
   let index_1 = m - 1;
   let index_2 = n - 1;
-  each_range_reverse(count, (item) => {});
+  let value_1 = list_get(nums1, index_1);
+  let value_2 = list_get(nums2, index_2);
 }
