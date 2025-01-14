ff285e0e090ad6fc198c5b55087f67539e579936
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-13 21:00:19 -0500

    2025-01-14T02:00:17.254Z watch   app_good

--- public/src/app_good.mjs
+++ public/src/app_good.mjs
@@ -4,7 +4,8 @@ import { html_button_width_full_text_click } from "./html_button_width_full_text
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_good() {
   let body = html_style_default_initialize();
-  let messages = ["morning", "night"];
+  let morning = "morning";
+  let messages = [morning, "night"];
   each(messages, (message) => {
     html_button_width_full_text_click(body, message, () => {
       grace_good_generic_copy(message);
