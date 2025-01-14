5ac592b0d56eba576d1ed7baa7a113ebd0789d06
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-13 21:08:57 -0500

    2025-01-14T02:08:55.686Z watch   app_good

--- public/src/app_good.mjs
+++ public/src/app_good.mjs
@@ -14,7 +14,7 @@ export function app_good() {
       if (message === morning) {
         html_p_text(
           body,
-          "Our Father, Who art in heaven, hallowed be Thy Name; Thy kingdom come; Thy will be done on earth as it is in heaven; give us this day our daily bread; and forgive us our trespasses as we forgive those who trespass against us; Lead us not into temptation, but deliver us from evil",
+          "Our Father, Who art in heaven, hallowed be Thy Name; Thy kingdom come; Thy will be done on earth as it is in heaven; give us this day our daily bread; And forgive us our trespasses as we forgive those who trespass against us; Lead us not into temptation, but deliver us from evil",
         );
         html_prayer(body, [
           "Cause me to perfectly obey the Father;",
