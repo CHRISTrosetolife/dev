ab6f0224d7d2db2616f3ad87f160ec2b3ede29cf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-13 21:03:54 -0500

    2025-01-14T02:03:49.262Z watch   app_good

--- public/src/app_good.mjs
+++ public/src/app_good.mjs
@@ -1,3 +1,4 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { grace_good_generic_copy } from "./grace_good_generic_copy.mjs";
 import { each } from "./each.mjs";
@@ -11,7 +12,10 @@ export function app_good() {
     html_button_width_full_text_click(body, message, () => {
       grace_good_generic_copy(message);
       if (message === morning) {
-        html_p_text();
+        let lines = string_combine_multiple([
+          "Our Father, Who art in heaven, hallowed be Thy Name; Thy kingdom come; Thy will be done on earth as it is in heaven; give us this day our daily bread; and forgive us our trespasses as we forgive those who trespass against us; lead us not into temptation, but deliver us from evil|",
+        ]);
+        html_p_text(body);
       }
     });
   });
