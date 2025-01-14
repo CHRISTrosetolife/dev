4ab201ed6d0c432244412587e38d7d4ebbccc35f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-14 12:43:33 -0500

    2025-01-14T17:43:31.519Z watch   app_share

--- public/src/app_share.mjs
+++ public/src/app_share.mjs
@@ -15,8 +15,7 @@ export async function app_share() {
   object_property_set(context, "version_code", "engbsb");
   await app_context_books_bible(context);
   let lookup = html_hash_lookup();
-  let chapter_next;
-  chapter_next = object_property_get(lookup, app_share_chapter());
+  let chapter_next = object_property_get(lookup, app_share_chapter());
   let verse_number_next;
   verse_number_next = object_property_get(lookup, app_share_verse());
   let p = bible_chapter_name_parse(chapter_next);
