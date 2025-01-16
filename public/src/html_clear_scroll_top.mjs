0754bb5540305cd4546aebe80facf0493cebc168
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 16:36:03 -0500

    2025-01-16T21:36:02.961Z watch   html_clear_scroll_top

--- public/src/html_clear_scroll_top.mjs
+++ public/src/html_clear_scroll_top.mjs
@@ -2,5 +2,7 @@ import { html_scroll_top } from "./html_scroll_top.mjs";
 import { html_clear } from "./html_clear.mjs";
 export function html_clear_scroll_top(root) {
   html_clear(root);
-  html_scroll_top();
+  if (false) {
+    html_scroll_top();
+  }
 }
