e9dd6fadef111bb302eebd9d912ceb0585a63d09
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 12:34:05 -0500

    2025-01-16T17:34:05.444Z pb object_properties_select

--- public/src/object_properties_select.mjs
+++ public/src/object_properties_select.mjs
@@ -1,5 +1,5 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { list_map } from "./list_map.mjs";
-export function object_properties_select(properties, c) {
+export function object_properties_select(c, properties) {
   return list_map(properties, (p) => object_property_get(c, p));
 }
