f031eea02dc373b52654945001a27091eb78fd5e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 11:57:59 -0500

    2025-01-16T16:57:59.222Z restart

--- public/src/app_language2.mjs
+++ public/src/app_language2.mjs
@@ -4,7 +4,7 @@ export async function app_language2(language_learn, language_fluent) {
   let body = html_style_default_initialize();
   let context = {
     language_learn,
-    language_fluent,
+    language_fluent
   };
   await app_language_group_index_changed_inner(context, 0);
 }
