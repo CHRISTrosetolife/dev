0bce7235ae4c5e78871f0c07e5763f64fe105b3c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 17:54:55 -0500

    2025-01-16T22:54:55.107Z watch   app_language2

--- public/src/app_language2.mjs
+++ public/src/app_language2.mjs
@@ -70,6 +70,9 @@ export async function app_language2(app_fn, language_learn, language_fluent) {
     );
   });
   let words = storage_local_get(app_fn, "words", {});
+  log({
+    words,
+  });
   if (object_properties_empty_is(words)) {
     let word = list_first(concated);
     object_property_set(words, object_property_get(word, "key"), {
