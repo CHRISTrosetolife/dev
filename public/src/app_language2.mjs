98f95f67d4ebd73ff5dec3aad862e195c5d609d0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 17:32:33 -0500

    2025-01-16T22:32:33.376Z watch   app_language2

--- public/src/app_language2.mjs
+++ public/src/app_language2.mjs
@@ -27,6 +27,7 @@ import { list_map } from "./list_map.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { app_language_group_index_changed_inner } from "./app_language_group_index_changed_inner.mjs";
 import { list_concat } from "./list_concat.mjs";
+import { list_without } from "./list_without.mjs";
 export async function app_language2(app_fn, language_learn, language_fluent) {
   let root = await app_language_initialize();
   let context = {
@@ -103,6 +104,7 @@ export async function app_language2(app_fn, language_learn, language_fluent) {
     let language = object_property_get(word, "language");
     let question = object_property_get(word, "question");
     let answer = object_property_get(word, "answer");
+    let choices = list_without(values, answer);
     app_language2_word(root, language, question);
     let quiz_container;
     await app_language_button_ready(
