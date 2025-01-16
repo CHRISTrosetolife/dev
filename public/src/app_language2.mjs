374cca63e43fb145c582bf269ad7554097d5e055
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 11:57:01 -0500

    2025-01-16T16:57:00.905Z watch   app_language2

--- public/src/app_language2.mjs
+++ public/src/app_language2.mjs
@@ -1,4 +1,4 @@
-import { app_language_group_get } from "./app_language_group_get.mjs";
+import { app_language_group_index_changed_inner } from "./app_language_group_index_changed_inner.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_language2(language_learn, language_fluent) {
   let body = html_style_default_initialize();
@@ -6,5 +6,5 @@ export async function app_language2(language_learn, language_fluent) {
     language_learn,
     language_fluent,
   };
-  let words = await app_language_group_get(context, 0);
+  await app_language_group_index_changed_inner(context, 0);
 }
