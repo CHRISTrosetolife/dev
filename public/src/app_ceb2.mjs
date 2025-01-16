9e29dc7d8ba2173f9ed6c6e7444751ad75bd6b41
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 11:50:37 -0500

    2025-01-16T16:50:35.078Z r app_language_2 app_language2

--- public/src/app_ceb2.mjs
+++ public/src/app_ceb2.mjs
@@ -1,6 +1,6 @@
-import { app_language_2 } from "./app_language_2.mjs";
+import { app_language2 } from "./app_language2.mjs";
 export async function app_ceb2() {
   let language_learn = "ceb";
   let language_fluent = "en";
-  await app_language_2(language_learn, language_fluent);
+  await app_language2(language_learn, language_fluent);
 }
--- public/src/app_language2.mjs
+++ public/src/app_language2.mjs
@@ -1,6 +1,6 @@
 import { app_language_group_get } from "./app_language_group_get.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-export async function app_language_2(language_learn, language_fluent) {
+export async function app_language2(language_learn, language_fluent) {
   let body = html_style_default_initialize();
   let context = {
     app_fn,
