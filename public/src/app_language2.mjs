5fe4e41ccb227981930d54ede3b042b054d46683
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 12:04:49 -0500

    2025-01-16T17:04:48.180Z watch   app_language2

--- public/src/app_language2.mjs
+++ public/src/app_language2.mjs
@@ -1,6 +1,8 @@
+import { list_map } from "./list_map.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { app_language_group_index_changed_inner } from "./app_language_group_index_changed_inner.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
+import { list_concat } from "./list_concat.mjs";
 export async function app_language2(language_learn, language_fluent) {
   let body = html_style_default_initialize();
   let context = {
@@ -9,4 +11,10 @@ export async function app_language2(language_learn, language_fluent) {
   };
   await app_language_group_index_changed_inner(context, 0);
   let group = object_property_get(context, "group");
+  list_concat(
+    list_map(group, (pair) => ({
+      pair,
+      language: language_learn,
+    })),
+  );
 }
