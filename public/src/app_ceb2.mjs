e41684c03816fa336090977f18c694d0001c19ce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 12:16:04 -0500

    2025-01-16T17:16:03.627Z watch   app_ceb2

--- public/src/app_ceb2.mjs
+++ public/src/app_ceb2.mjs
@@ -1,6 +1,7 @@
+import { fn_name } from "./fn_name.mjs";
 import { app_language2 } from "./app_language2.mjs";
 export async function app_ceb2() {
   let language_learn = "ceb";
   let language_fluent = "en";
-  await app_language2(language_learn, language_fluent);
+  await app_language2(fn_name("app_ceb2"), language_learn, language_fluent);
 }
