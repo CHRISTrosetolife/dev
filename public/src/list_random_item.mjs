82ca2af395e6a00f7c22e4bd34b8f12fd57bb41f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 17:51:59 -0500

    2025-01-16T22:51:58.530Z watch   list_random_item

--- public/src/list_random_item.mjs
+++ public/src/list_random_item.mjs
@@ -1,4 +1,5 @@
+import { list_get } from "./list_get.mjs";
 import { list_random_index } from "./list_random_index.mjs";
 export function list_random_item(list) {
-  return list[list_random_index(list)];
+  return list_get(list, list_random_index(list));
 }
