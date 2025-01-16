6edfa13765604eadabed24233778afabe03b88ac
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 15:52:27 -0500

    2025-01-16T20:52:27.105Z watch   html_flag_language

--- public/src/html_flag_language.mjs
+++ public/src/html_flag_language.mjs
@@ -2,10 +2,10 @@ import { object_property_get } from "./object_property_get.mjs";
 import { html_flag_us } from "./html_flag_us.mjs";
 import { html_flag_ph } from "./html_flag_ph.mjs";
 import { html_flag } from "./html_flag.mjs";
-export function html_flag_language(language) {
+export function html_flag_language(parent, language) {
   let languages_to_flags = {
     ceb: html_flag_ph(),
     en: html_flag_us(),
   };
-  html_flag(button, object_property_get(languages_to_flags, language));
+  html_flag(parent, object_property_get(languages_to_flags, language));
 }
