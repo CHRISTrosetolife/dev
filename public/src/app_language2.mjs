2b32a7d21eebf4dc915762ff0a99269e42b612a1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 12:09:26 -0500

    2025-01-16T17:09:25.882Z watch   app_language2

--- public/src/app_language2.mjs
+++ public/src/app_language2.mjs
@@ -1,13 +1,13 @@
+import { app_language_initialize } from "./app_language_initialize.mjs";
 import { log } from "./log.mjs";
 import { list_second } from "./list_second.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_map } from "./list_map.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { app_language_group_index_changed_inner } from "./app_language_group_index_changed_inner.mjs";
-import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { list_concat } from "./list_concat.mjs";
 export async function app_language2(language_learn, language_fluent) {
-  let body = html_style_default_initialize();
+  let body = await app_language_initialize();
   let context = {
     language_learn,
     language_fluent,
