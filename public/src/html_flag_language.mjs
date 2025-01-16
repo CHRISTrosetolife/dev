fe866071cdb0d9d3e45c16c1bc6eadb9ba25c067
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 17:19:18 -0500

    2025-01-16T22:19:17.503Z watch   html_flag_language

--- public/src/html_flag_language.mjs
+++ public/src/html_flag_language.mjs
@@ -1,12 +1,6 @@
-import { object_property_get } from "./object_property_get.mjs";
-import { html_flag_us } from "./html_flag_us.mjs";
-import { html_flag_ph } from "./html_flag_ph.mjs";
+import { html_flag_languages } from "./html_flag_languages.mjs";
 import { html_flag } from "./html_flag.mjs";
 export function html_flag_language(parent, language) {
-  let languages_to_flags = {
-    ceb: html_flag_ph(),
-    en: html_flag_us(),
-  };
-  let g = object_property_get(languages_to_flags, language);
+  let g = html_flag_languages(language);
   html_flag(parent, g);
 }
---
+++ public/src/html_flag_languages.mjs
@@ -0,0 +1,11 @@
+import { object_property_get } from "./object_property_get.mjs";
+import { html_flag_us } from "./html_flag_us.mjs";
+import { html_flag_ph } from "./html_flag_ph.mjs";
+export function html_flag_languages(language) {
+  let languages_to_flags = {
+    ceb: html_flag_ph(),
+    en: html_flag_us(),
+  };
+  let g = object_property_get(languages_to_flags, language);
+  return g;
+}
