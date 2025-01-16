ecd451b64063c1af1b12bbaa4d621020ac870cd5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 10:22:37 -0500

    2025-01-16T15:22:36.857Z watch   bible_words_definitions_atoms

--- public/src/bible_words_definitions_atoms.mjs
+++ public/src/bible_words_definitions_atoms.mjs
@@ -1,3 +1,5 @@
+import { list_size } from "./list_size.mjs";
+import { object_property_set } from "./object_property_set.mjs";
 import { list_add } from "./list_add.mjs";
 import { app_language_atom_size } from "./app_language_atom_size.mjs";
 import { bible_words_definitions_pairs } from "./bible_words_definitions_pairs.mjs";
@@ -14,14 +16,21 @@ export async function bible_words_definitions_atoms(language) {
     "definitions",
   );
   let inverted = object_list_invert(definitions);
+  let lefts = {};
+  let rights = {};
   let waiting = [];
+  let count = 0;
   let pair_index = 0;
   let atoms = [];
-  let group = [];
+  let atom = [];
   let pair = pairs[pair_index];
-  list_add(group, pair);
-  if (false) {
+  list_add(atom, pair);
+  object_property_set(object, "property_name", value);
+  count++;
+  if (list_size(atom) === atom_count) {
+    list_add(atoms, atom);
   }
+  pair_index++;
   return {
     atoms,
     definitions,
