e3739f9fc8ba6994c06082c4faebcf10f0893585
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 17:44:59 -0500

    2025-01-16T22:44:58.414Z watch   app_language_button_ready

--- public/src/app_language_button_ready.mjs
+++ public/src/app_language_button_ready.mjs
@@ -1,3 +1,4 @@
+import { html_div } from "./html_div.mjs";
 import { app_language_refresh_quiz_text_ready } from "./app_language_refresh_quiz_text_ready.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_span_text } from "./html_span_text.mjs";
@@ -5,12 +6,8 @@ import { html_flag } from "./html_flag.mjs";
 import { html_style_display_block } from "./html_style_display_block.mjs";
 import { html_style_display_none } from "./html_style_display_none.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
-export async function app_language_button_ready(
-  root,
-  f,
-  language_fluent,
-) {
-    let quiz_container;
+export async function app_language_button_ready(root, f, language_fluent) {
+  let quiz_container;
   let button_ready = html_button_width_full_text_click(root, "", () => {
     html_style_display_none(button_ready);
     html_style_display_block(quiz_container_get());
@@ -23,7 +20,7 @@ export async function app_language_button_ready(
       await app_language_refresh_quiz_text_ready(language_fluent),
     ]),
   );
-    quiz_container = html_div(root);
-    html_style_display_none(quiz_container);
+  quiz_container = html_div(root);
+  html_style_display_none(quiz_container);
   return button_ready;
 }
