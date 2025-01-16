bb88764d86b9b1379d82da86dd46499afb9c0b21
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 11:48:56 -0500

    2025-01-16T16:48:55.596Z watch   app_ceb2

--- public/src/app_ceb2.mjs
+++ public/src/app_ceb2.mjs
@@ -1,6 +1,14 @@
 import { app_language_group_get } from "./app_language_group_get.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_ceb2() {
+  let language_learn = "ceb";
+  let language_fluent = "en";
   let body = html_style_default_initialize();
+  let context = {
+    app_fn,
+    language_learn,
+    language_fluent,
+    invert,
+  };
   await app_language_group_get();
 }
