4efb86d543dd0392829bc9b3098d780ecc8f6539
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 11:49:45 -0500

    2025-01-16T16:49:44.710Z watch   app_ceb2

--- public/src/app_ceb2.mjs
+++ public/src/app_ceb2.mjs
@@ -1,14 +1,6 @@
-import { app_language_group_get } from "./app_language_group_get.mjs";
-import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
+import { app_language_2 } from "./app_language_2.mjs";
 export async function app_ceb2() {
   let language_learn = "ceb";
   let language_fluent = "en";
-  let body = html_style_default_initialize();
-  let context = {
-    app_fn,
-    language_learn,
-    language_fluent,
-    invert,
-  };
-  await app_language_group_get();
+  await app_language_2(language_learn, language_fluent);
 }
---
+++ public/src/app_language_2.mjs
@@ -0,0 +1,12 @@
+import { app_language_group_get } from "./app_language_group_get.mjs";
+import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
+export async function app_language_2(language_learn, language_fluent) {
+  let body = html_style_default_initialize();
+  let context = {
+    app_fn,
+    language_learn,
+    language_fluent,
+    invert,
+  };
+  await app_language_group_get();
+}
