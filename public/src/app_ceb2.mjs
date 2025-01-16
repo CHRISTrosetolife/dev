80498b0a7c6d80aadb52e7b31e30af30c8d9395b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 11:48:37 -0500

    2025-01-16T16:48:36.826Z watch   app_ceb2

--- public/src/app_ceb2.mjs
+++ public/src/app_ceb2.mjs
@@ -1,4 +1,6 @@
+import { app_language_group_get } from "./app_language_group_get.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-export function app_ceb2() {
+export async function app_ceb2() {
   let body = html_style_default_initialize();
+  await app_language_group_get();
 }
