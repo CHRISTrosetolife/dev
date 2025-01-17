b1b447e1b66ae5caab34ca739247e69f029f0de1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 19:13:12 -0500

    2025-01-17T00:13:11.774Z watch   app_language2_refresh

--- public/src/app_language2_refresh.mjs
+++ public/src/app_language2_refresh.mjs
@@ -92,6 +92,9 @@ export async function app_language2_refresh(context) {
             gap = ceiling(gap * factor);
             object_property_set(v, "gap", gap);
             object_property_set(v, "wait", gap);
+            log({
+              v,
+            });
           }
           storage_local_set(app_fn, "words", words);
           await app_language2_refresh(context);
