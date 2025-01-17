d08632cc535bcdadcbce0d04173d39dae04e399d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-17 14:34:36 -0500

    2025-01-17T19:34:36.346Z watch   app_language2_upload

--- public/src/app_language2_upload.mjs
+++ public/src/app_language2_upload.mjs
@@ -1,5 +1,5 @@
 import { bible_words_definitions_atoms_cache } from "./bible_words_definitions_atoms_cache.mjs";
-export async function app_language2_upload() {
+export async function app_language2_upload(from) {
   let { atoms, definitions, inverted } =
     await bible_words_definitions_atoms_cache(from);
 }
