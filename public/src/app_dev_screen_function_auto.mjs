56f672c8ed588dc3a31f84e39e55930a7100b17b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-17 16:25:00 -0500

    2025-01-17T21:24:59.907Z watch   app_dev_screen_function_auto

--- public/src/app_dev_screen_function_auto.mjs
+++ public/src/app_dev_screen_function_auto.mjs
@@ -3,8 +3,6 @@ import { js_code_statement_return } from "./js_code_statement_return.mjs";
 import { html_hr } from "./html_hr.mjs";
 import { app_dev_screen_if_to_block_if_else } from "./app_dev_screen_if_to_block_if_else.mjs";
 import { js_code_statement_assign } from "./js_code_statement_assign.mjs";
-import { sleep } from "./sleep.mjs";
-import { log } from "./log.mjs";
 import { js_code_import } from "./js_code_import.mjs";
 import { function_auto_results } from "./function_auto_results.mjs";
 import { string_delimit_backtick } from "./string_delimit_backtick.mjs";
@@ -15,8 +13,6 @@ import { js_code_format } from "./js_code_format.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { js_code_export_function_declare } from "./js_code_export_function_declare.mjs";
 import { js_code_call_args } from "./js_code_call_args.mjs";
-import { add } from "./add.mjs";
-import { multiply } from "./multiply.mjs";
 import { js_code_statement_call_args } from "./js_code_statement_call_args.mjs";
 import { js_code_function_declare } from "./js_code_function_declare.mjs";
 import { function_name_new } from "./function_name_new.mjs";
@@ -64,12 +60,12 @@ export function app_dev_screen_function_auto() {
       let contents_c = js_code_function_declare(
         fn_c,
         "a,b",
-        js_code_statement_call_args(multiply.name, ["a", "b"]),
+        js_code_statement_call_args(fn_name("multiply"), ["a", "b"]),
       );
       let contents_b = js_code_function_declare(
         fn_b,
         "a,b,c",
-        js_code_statement_call_args(add.name, [
+        js_code_statement_call_args(fn_name("add"), [
           "a",
           js_code_call_args(fn_c, ["b", "c"]),
         ]),
@@ -79,14 +75,14 @@ export function app_dev_screen_function_auto() {
         "",
         string_combine_multiple([
           js_code_statement_assign("c", 4),
-          js_code_statement_call_args(sleep.name, [100]),
+          js_code_statement_call_args(fn_name("sleep"), [100]),
           app_dev_screen_if_to_block_if_else(),
           js_code_statement_return(js_code_call_args(fn_b, ["1", "2", "3"])),
         ]),
         false,
       );
       let code = string_combine_multiple([
-        js_code_import(log.name),
+        js_code_import(fn_name("log")),
         contents_a,
         contents_b,
         contents_c,
