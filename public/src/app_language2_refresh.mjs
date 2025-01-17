52deeb1535e491f53afa88adf5707170d9fb1c5f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-17 15:19:26 -0500

    2025-01-17T20:19:25.374Z watch   app_language2_refresh

--- public/src/app_language2_refresh.mjs
+++ public/src/app_language2_refresh.mjs
@@ -81,6 +81,7 @@ export async function app_language2_refresh(context) {
         app_language2_word_index(v) < index &&
         object_property_get(word, "question") === question,
     );
+    let mapped = list_map_property(matches, "answer");
     app_language2_word(root, language, question);
     html_br2(root);
     let quiz_container = await app_language_button_ready(
