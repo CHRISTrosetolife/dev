bebb650fb3d80aa4de0504e5177457056abb3118
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-17 15:11:00 -0500

    2025-01-17T20:10:59.200Z watch   app_language2_upload

--- public/src/app_language2_upload.mjs
+++ public/src/app_language2_upload.mjs
@@ -1,3 +1,4 @@
+import { list_first } from "./list_first.mjs";
 import { bible_words_definitions_atoms } from "./bible_words_definitions_atoms.mjs";
 import { app_language_upload_result } from "./app_language_upload_result.mjs";
 import { app_language_group_upload } from "./app_language_group_upload.mjs";
@@ -5,7 +6,7 @@ export async function app_language2_upload(from) {
   let to = "en";
   let { atoms, definitions, inverted } =
     await bible_words_definitions_atoms(from);
-  let result_new = app_language_upload_result(atoms, {
+  let result_new = app_language_upload_result([list_first(atoms)], {
     definitions,
   });
   let existing_path = await app_language_group_upload(
