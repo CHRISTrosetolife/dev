3fcc6bb4bc1ab8f92ce502d947350ee6b0a83453
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-17 14:34:47 -0500

    2025-01-17T19:34:46.414Z watch   app_language2_upload

--- public/src/app_language2_upload.mjs
+++ public/src/app_language2_upload.mjs
@@ -1,5 +1,11 @@
+import { app_language_group_upload } from "./app_language_group_upload.mjs";
 import { bible_words_definitions_atoms_cache } from "./bible_words_definitions_atoms_cache.mjs";
 export async function app_language2_upload(from) {
   let { atoms, definitions, inverted } =
     await bible_words_definitions_atoms_cache(from);
+  let existing_path = await app_language_group_upload(
+    profile,
+    group_index,
+    result_new,
+  );
 }
