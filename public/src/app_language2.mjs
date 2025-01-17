f290a46b5654204856580b2acd0dfec88f3809c4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-16 19:13:48 -0500

    2025-01-17T00:13:48.433Z watch   app_language2

--- public/src/app_language2.mjs
+++ public/src/app_language2.mjs
@@ -58,7 +58,7 @@ export async function app_language2(app_fn, language_learn, language_fluent) {
   each(concated, (word) => {
     let k = object_property_get(word, "key");
     if (object_property_exists(words, k)) {
-      return;
+      $ireturn;
     }
     let w = {
       learning: false,
