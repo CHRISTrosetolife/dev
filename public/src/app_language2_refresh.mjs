a647e2f532192dc46cfd2c77b719a3fd9951cbb6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-18 10:41:13 -0500

    2025-01-18T15:41:12.569Z watch   app_language2_refresh

--- public/src/app_language2_refresh.mjs
+++ public/src/app_language2_refresh.mjs
@@ -66,6 +66,7 @@ export async function app_language2_refresh(context) {
     let question = object_property_get(word, "question");
     let mapped = app_language2_answers(values_all, u);
     let answer_text = list_join_comma_space(mapped);
+    lislt_size(list_filter_property(values, "learning", true));
     html_progress(root, "Word pair");
     html_p_text(root, "Remember this:");
     app_language2_word_titled(root, "Question", language, question);
