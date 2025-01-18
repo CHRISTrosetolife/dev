4ace72bdec751f98ef592663420330410b648070
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-18 11:08:01 -0500

    2025-01-18T16:07:59.784Z watch   app_language2_answers

--- public/src/app_language2_answers.mjs
+++ public/src/app_language2_answers.mjs
@@ -11,13 +11,16 @@ import { identity } from "./identity.mjs";
 export function app_language2_answers(values_all, v) {
   let word = object_property_get(v, "word");
   let question = object_property_get(word, "question");
+  let language = object_property_get(word, "language");
   let index = object_property_get(word, "index");
   let matches = list_filter(
     values_all,
     (v) =>
       app_language2_word_index(v) < index &&
       object_property_get(object_property_get(v, "word"), "question") ===
-        question,
+        question &&
+      object_property_get(object_property_get(v, "word"), "language") ===
+        language,
   );
   log({
     matches,
