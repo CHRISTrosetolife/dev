022c365df87f427988fcdc138c79ac0d71bcc4dd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-18 10:24:18 -0500

    2025-01-18T15:24:18.296Z watch   app_language2

--- public/src/app_language2.mjs
+++ public/src/app_language2.mjs
@@ -34,7 +34,7 @@ export async function app_language2(app_fn, language_learn, language_fluent) {
   await app_language_group_index_changed_inner(context, "all");
   let group = object_property_get(context, "group");
   let flat = list_flatten(group);
-  flat = list_take(flat, 10);
+  flat = list_take(flat, 5);
   let chunked = list_chunk(flat, 2);
   let mapped = list_map(chunked, (chunk) =>
     list_concat(
