3bf4886e4003b86976a478b370487051027abbde
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-18 19:27:46 -0500

    2025-01-19T00:27:45.480Z watch   app_language2_refresh_home

--- public/src/app_language2_refresh_home.mjs
+++ public/src/app_language2_refresh_home.mjs
@@ -1,1 +1,5 @@
-export function app_language2_refresh_home() {}
+import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
+export function app_language2_refresh_home(context) {
+  let { root } = context;
+  html_clear_scroll_top_centered(root);
+}
