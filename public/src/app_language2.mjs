51b6c53d37f79ba190220692d219c0fc4b733eee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-18 19:36:47 -0500

    2025-01-19T00:36:47.511Z watch   app_language2

--- public/src/app_language2.mjs
+++ public/src/app_language2.mjs
@@ -89,6 +89,7 @@ export async function app_language2(app_fn, language_learn, language_fluent) {
     object_property_set(w, "gap", null);
   });
   storage_local_set(app_fn, "words", words);
-  app_language2_gaps_set(context, 1.2);
+  let factor = storage_local_initialize(app_fn, "factor", 1.2);
+  app_language2_gaps_set(context, factor);
   await app_language2_refresh_learn(context);
 }
