2e3d141e6e36fbe07bafc5b326b76220d2689bfa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-18 19:29:34 -0500

    2025-01-19T00:29:33.776Z watch   app_language2_refresh_home

--- public/src/app_language2_refresh_home.mjs
+++ public/src/app_language2_refresh_home.mjs
@@ -1,9 +1,10 @@
+import { app_language2_refresh_learn } from "./app_language2_refresh_learn.mjs";
 import { html_button_back } from "./html_button_back.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 export function app_language2_refresh_home(context) {
   let { root } = context;
   html_clear_scroll_top_centered(root);
-  html_button_back(root, () => {
-    app_language2_refresh_home(context);
+  html_button_back(root, async () => {
+    await app_language2_refresh_learn(context);
   });
 }
