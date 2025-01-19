45de920b1434acd57ecce4d15edbc7fb1aef260b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-18 19:32:46 -0500

    2025-01-19T00:32:45.634Z watch   app_language2_gaps_set

--- public/src/app_language2_gaps_set.mjs
+++ public/src/app_language2_gaps_set.mjs
@@ -1,9 +1,11 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { list_add } from "./list_add.mjs";
 import { ceiling } from "./ceiling.mjs";
 import { object_properties } from "./object_properties.mjs";
 import { list_size } from "./list_size.mjs";
 export function app_language2_gaps_set(context, factor) {
+  let words2 = object_property_get(context, "words");
   let w_size = list_size(object_properties(words));
   let gap = 0;
   let gaps = [0];
