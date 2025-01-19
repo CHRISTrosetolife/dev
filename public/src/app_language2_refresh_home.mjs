e1eca02c023c3d2c51caa50408c9485b617a6494
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-18 19:35:21 -0500

    2025-01-19T00:35:21.150Z watch   app_language2_refresh_home

--- public/src/app_language2_refresh_home.mjs
+++ public/src/app_language2_refresh_home.mjs
@@ -4,6 +4,7 @@ import { app_language2_refresh_learn } from "./app_language2_refresh_learn.mjs";
 import { html_button_back } from "./html_button_back.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 import { list_map } from "./list_map.mjs";
+import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function app_language2_refresh_home(context) {
   let { root } = context;
   html_clear_scroll_top_centered(root);
@@ -12,5 +13,7 @@ export function app_language2_refresh_home(context) {
   });
   let n = range_from(11, 30);
   let factors = list_map(n, (i) => i / 10);
-  each(factors, (factor) => {});
+  each(factors, (factor) => {
+    html_button_width_full_text_click(root, factor, () => {});
+  });
 }
