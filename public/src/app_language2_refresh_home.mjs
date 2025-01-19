c9e641f788817ceb9424197505762a9426da630e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-19 10:53:08 -0500

    2025-01-19T15:53:07.141Z watch   app_language2_refresh_home

--- public/src/app_language2_refresh_home.mjs
+++ public/src/app_language2_refresh_home.mjs
@@ -1,3 +1,4 @@
+import { list_take } from "./list_take.mjs";
 import { json_to } from "./json_to.mjs";
 import { html_div_text } from "./html_div_text.mjs";
 import { app_language2_word_index } from "./app_language2_word_index.mjs";
@@ -29,7 +30,7 @@ export function app_language2_refresh_home(context) {
     let values_all = object_values(words);
     let values = list_filter_property(values_all, "skip", false);
     list_sort(values, app_language2_word_index);
-    each(list, (item) => {
+    each(list_take(values, 200), (item) => {
       html_div_text(root, json_to(item));
     });
   });
