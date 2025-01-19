4391b4b8f30a55882c42b8325299fc910502150b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-18 19:34:33 -0500

    2025-01-19T00:34:32.749Z watch   app_language2_refresh_home

--- public/src/app_language2_refresh_home.mjs
+++ public/src/app_language2_refresh_home.mjs
@@ -3,6 +3,7 @@ import { each } from "./each.mjs";
 import { app_language2_refresh_learn } from "./app_language2_refresh_learn.mjs";
 import { html_button_back } from "./html_button_back.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
+import { list_map } from "./list_map.mjs";
 export function app_language2_refresh_home(context) {
   let { root } = context;
   html_clear_scroll_top_centered(root);
@@ -10,5 +11,6 @@ export function app_language2_refresh_home(context) {
     await app_language2_refresh_learn(context);
   });
   let n = range_from(11, 30);
+  let mapped = list_map(n, (i) => i / 10);
   each(list, (item) => {});
 }
