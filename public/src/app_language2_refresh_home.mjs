891beba66846b1f6ae9653a40a67eccfa9c10ea9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-18 19:28:32 -0500

    2025-01-19T00:28:31.980Z watch   app_language2_refresh_home

--- public/src/app_language2_refresh_home.mjs
+++ public/src/app_language2_refresh_home.mjs
@@ -1,5 +1,7 @@
+import { html_button_back } from "./html_button_back.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 export function app_language2_refresh_home(context) {
   let { root } = context;
   html_clear_scroll_top_centered(root);
+  html_button_back();
 }
