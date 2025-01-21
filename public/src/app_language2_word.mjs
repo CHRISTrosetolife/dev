5bd44cb8571756cd22b99032acacdfd42df6fd40
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-21 11:08:13 -0500

    2025-01-21T16:08:13.031Z watch   app_language2_word

--- public/src/app_language2_word.mjs
+++ public/src/app_language2_word.mjs
@@ -1,3 +1,4 @@
+import { html_style_default_border_value } from "./html_style_default_border_value.mjs";
 import { html_span } from "./html_span.mjs";
 import { html_style_bold } from "./html_style_bold.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
@@ -6,7 +7,8 @@ import { html_flag_language } from "./html_flag_language.mjs";
 export function app_language2_word(body, language, rememeber, boxed) {
   let q = html_span(body);
   let box = html_span(q);
-  if (false) {
+  if (boxed) {
+    html_style_default_border_value(box, "black");
   }
   html_flag_language(box, language);
   html_span_text(q, string_combine_multiple([" ", rememeber]));
