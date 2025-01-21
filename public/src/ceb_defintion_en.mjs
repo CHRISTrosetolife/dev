6691c37edd337c6ce3c6c0c22a4cf375af996eab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-21 16:18:01 -0500

    2025-01-21T21:18:00.673Z watch   ceb_defintion_en

--- public/src/ceb_defintion_en.mjs
+++ public/src/ceb_defintion_en.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { ceb_html_cache_parse_form1 } from "./ceb_html_cache_parse_form1.mjs";
 import { string_to_url } from "./string_to_url.mjs";
 import { ceb_definition_prefix_en } from "./ceb_definition_prefix_en.mjs";
@@ -6,9 +5,6 @@ import { string_combine } from "./string_combine.mjs";
 export async function ceb_defintion_en(d) {
   let url = string_combine(ceb_definition_prefix_en(), d);
   url = string_to_url(url);
-  log({
-    url,
-  });
   let { children: children2_ } = await ceb_html_cache_parse_form1(url);
   let children2 = children2_;
   return children2;
