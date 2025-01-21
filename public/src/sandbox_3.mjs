533297691473f0b598cb0c1b345c72111b831676
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-21 16:13:51 -0500

    2025-01-21T21:13:50.705Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,13 +1,16 @@
+import { list_map } from "./list_map.mjs";
 import { string_split_newline } from "./string_split_newline.mjs";
 import { http_cache } from "./http_cache.mjs";
 import { log } from "./log.mjs";
 import { ceb_defintion_en } from "./ceb_defintion_en.mjs";
+import { string_trim_whitespace } from "./string_trim_whitespace.mjs";
 export async function sandbox_3() {
   let text = await http_cache(
     "https://raw.githubusercontent.com/first20hours/google-10000-english/refs/heads/master/google-10000-english-usa.txt",
   );
   await ceb_defintion_en("god");
   let lines = string_split_newline(text);
+  let trimmed = list_map(lines, string_trim_whitespace);
   log({
     text,
   });
