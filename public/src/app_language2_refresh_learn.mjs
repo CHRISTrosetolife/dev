514a79e7843b07a0313c586c1efdedf0faebafbf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-21 11:21:32 -0500

    2025-01-21T16:21:31.545Z watch   app_language2_refresh_learn

--- public/src/app_language2_refresh_learn.mjs
+++ public/src/app_language2_refresh_learn.mjs
@@ -119,7 +119,7 @@ export async function app_language2_refresh_learn(context) {
       language_other,
     );
     let va_filtered2 = list_filter_property(va_filtered, "learning", true);
-    let questions = list_map_property(va_filtered, "question");
+    let questions = list_map_property(va_filtered2, "question");
     let boxed = list_includes(questions, question);
     log({
       language_other,
