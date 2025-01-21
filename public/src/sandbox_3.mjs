a64c38bd481fca52c041a828dccf91686067457a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-21 16:09:57 -0500

    2025-01-21T21:09:57.210Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,8 +1,9 @@
+import { html_parse_text } from "./html_parse_text.mjs";
 import { ceb_defintion_en } from "./ceb_defintion_en.mjs";
 import { log } from "./log.mjs";
 export async function sandbox_3() {
   let d = await ceb_defintion_en("god");
   log({
-    d,
+    d: html_parse_text(d),
   });
 }
