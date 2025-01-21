08138dd72adfd76b8ea008697af1f361df321a2e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-21 16:07:10 -0500

    2025-01-21T21:07:09.931Z watch   ceb_definition

--- public/src/ceb_definition.mjs
+++ public/src/ceb_definition.mjs
@@ -347,7 +347,8 @@ export async function ceb_definition(word) {
     }
     let url = string_combine(ceb_definition_prefix_en(), d);
     url = string_to_url(url);
-    let { children: children2 } = await ceb_html_cache_parse_form1(url);
+    let { children: children2_ } = await ceb_html_cache_parse_form1(url);
+    let children2 = children2_;
     let as = list_filter(children2, (c) => html_parse_tag_named(c, "a"));
     let mapped5 = list_map(as, html_parse_href);
     let filtered5 = list_filter_starts_with(mapped5, prefix_1);
