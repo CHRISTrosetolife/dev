6f4176c8f90b42a8f54b5aeae41974ff6dc8633d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-21 16:12:57 -0500

    2025-01-21T21:12:57.344Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -2,7 +2,7 @@ import { http_cache } from "./http_cache.mjs";
 import { log } from "./log.mjs";
 import { ceb_defintion_en } from "./ceb_defintion_en.mjs";
 export async function sandbox_3() {
-  await http_cache(
+  let text = await http_cache(
     "https://raw.githubusercontent.com/first20hours/google-10000-english/refs/heads/master/google-10000-english-usa.txt",
   );
   await ceb_defintion_en("god");
