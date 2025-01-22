14bb6bc9af8744d0d2c76c124c0b64400fa94828
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-22 10:38:31 -0500

    2025-01-22T15:38:27.859Z c bible_interlinear_strongs_cache bible_interlinear_strongs_cache_hub

---
+++ public/src/bible_interlinear_strongs_cache_hub.mjs
@@ -0,0 +1,14 @@
+import { http_cache } from "./http_cache.mjs";
+import { url_secure_w3 } from "./url_secure_w3.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export async function bible_interlinear_strongs_cache_hub(language, strong) {
+  let url = string_combine_multiple([
+    url_secure_w3(),
+    "openbible.com/strongs/",
+    language,
+    "/",
+    strong,
+    ".htm",
+  ]);
+  return await http_cache(url);
+}
