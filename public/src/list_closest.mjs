05787573a890e811234f4b0fb4076ee783aaebe0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-22 17:44:42 -0500

    2025-01-22T22:44:41.721Z watch   list_closest

--- public/src/list_closest.mjs
+++ public/src/list_closest.mjs
@@ -2,6 +2,8 @@ import { abs } from "./abs.mjs";
 import { each } from "./each.mjs";
 import { list_first } from "./list_first.mjs";
 export function list_closest(list, item) {
+  list = [1, 3, 6, 8];
+  item = 5;
   let closest = list_first(list);
   let best = Number.POSITIVE_INFINITY;
   each(list, (e) => {
