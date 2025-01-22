7c3dc27f61c4440e161d51ced3bdee846fd04e53
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-22 17:44:52 -0500

    2025-01-22T22:44:51.254Z watch   list_closest

--- public/src/list_closest.mjs
+++ public/src/list_closest.mjs
@@ -3,7 +3,7 @@ import { each } from "./each.mjs";
 import { list_first } from "./list_first.mjs";
 export function list_closest(list, item) {
   list = [1, 3, 6, 8];
-  item = 5;
+  item = 2;
   let closest = list_first(list);
   let best = Number.POSITIVE_INFINITY;
   each(list, (e) => {
