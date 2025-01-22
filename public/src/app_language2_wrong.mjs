32dec0dc128628fdadac6aac78c816c7e9d3b14a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-22 17:41:17 -0500

    2025-01-22T22:41:16.610Z pm app_language2_wrong 3 0

--- public/src/app_language2_wrong.mjs
+++ public/src/app_language2_wrong.mjs
@@ -1,5 +1,5 @@
 import { object_property_set } from "./object_property_set.mjs";
-export function app_language2_wrong(v, gap_initial, wait_initial, context) {
+export function app_language2_wrong(context, v, gap_initial, wait_initial) {
   object_property_set(v, "gap", gap_initial);
   object_property_set(v, "wait", wait_initial);
 }
