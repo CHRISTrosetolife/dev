500c32549b0d829154a9e726836320aba08f55a5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-22 17:42:57 -0500

    2025-01-22T22:42:57.149Z watch   list_closest

--- public/src/list_closest.mjs
+++ public/src/list_closest.mjs
@@ -1,1 +1,6 @@
-export function list_closest() {}
+import { each } from "./each.mjs";
+import { list_first } from "./list_first.mjs";
+export function list_closest(list, item) {
+  let closest = list_first(list);
+  each(list2, (item2) => {});
+}
