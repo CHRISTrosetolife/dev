d7cfb8c6766f31a2cb05768dffda09f4d572b5ee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-22 14:02:00 -0500

    2025-01-22T19:01:58.086Z watch   bible_interlinear_strongs_cache

--- public/src/bible_interlinear_strongs_cache.mjs
+++ public/src/bible_interlinear_strongs_cache.mjs
@@ -2,9 +2,10 @@ import { http_cache } from "./http_cache.mjs";
 import { url_secure_w3 } from "./url_secure_w3.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function bible_interlinear_strongs_cache(language, strong) {
+  let url_base = "openbible.com/strongs/";
   let url = string_combine_multiple([
     url_secure_w3(),
-    "openbible.com/strongs/",
+    url_base,
     language,
     "/",
     strong,
