00c281353cc92a0e4741806c30d43fc4a6dce13e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-22 17:49:39 -0500

    2025-01-22T22:49:39.173Z watch   app_language2_refresh_home

--- public/src/app_language2_refresh_home.mjs
+++ public/src/app_language2_refresh_home.mjs
@@ -25,17 +25,19 @@ export function app_language2_refresh_home(context) {
   html_button_back(root, async () => {
     await app_language2_refresh_learn(context);
   });
-  html_button_width_full_text_click(root, "stats", () => {
-    let { root, words } = context;
-    html_clear_scroll_top_centered(root);
-    html_button_back(root, async () => {
-      app_language2_refresh_home(context);
+  if (false) {
+    html_button_width_full_text_click(root, "stats", () => {
+      let { root, words } = context;
+      html_clear_scroll_top_centered(root);
+      html_button_back(root, async () => {
+        app_language2_refresh_home(context);
+      });
+      let stats = storage_local_get(app_fn, "stats");
+      each(list_take(values, 200), (item) => {
+        html_div_text(root, json_format_to(item));
+      });
     });
-    let stats = storage_local_get(app_fn, "stats");
-    each(list_take(values, 200), (item) => {
-      html_div_text(root, json_format_to(item));
-    });
-  });
+  }
   html_button_width_full_text_click(root, "values", () => {
     let { root, words } = context;
     html_clear_scroll_top_centered(root);
