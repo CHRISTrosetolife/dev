3c28e7a19e4c2ce75cff8e4c7f905fe39885f50a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-22 17:45:47 -0500

    2025-01-22T22:45:46.580Z watch   app_language2_wrong

--- public/src/app_language2_wrong.mjs
+++ public/src/app_language2_wrong.mjs
@@ -1,5 +1,10 @@
+import { list_closest } from "./list_closest.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 export function app_language2_wrong(context, v, gap_initial, wait_initial) {
   object_property_set(v, "gap", gap_initial);
   object_property_set(v, "wait", wait_initial);
+  let gap = object_property_get(v, "gap");
+  let gaps = object_property_get(context, "gaps");
+  let closest = list_closest(gaps, gap);
 }
