9b40ef7af4a5d6729872375571c4d9a367b039f4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-22 14:03:31 -0500

    2025-01-22T19:03:31.049Z watch   bible_interlinear_strongs_cache_hub

--- public/src/bible_interlinear_strongs_cache_hub.mjs
+++ public/src/bible_interlinear_strongs_cache_hub.mjs
@@ -1,14 +1,8 @@
-import { http_cache } from "./http_cache.mjs";
-import { url_secure_w3 } from "./url_secure_w3.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { bible_interlinear_strongs_cache_generic } from "./bible_interlinear_strongs_cache_generic.mjs";
 export async function bible_interlinear_strongs_cache_hub(language, strong) {
-  let url = string_combine_multiple([
-    url_secure_w3(),
+  return await bible_interlinear_strongs_cache_generic(
     "biblehub.com/",
     language,
-    "/",
     strong,
-    ".htm",
-  ]);
-  return await http_cache(url);
+  );
 }
