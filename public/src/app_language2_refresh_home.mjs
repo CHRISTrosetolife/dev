f3b6fd747b157895141b065783b9a98a112611a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-23 14:20:02 -0500

    2025-01-23T19:20:01.336Z watch   app_language2_refresh_home

--- public/src/app_language2_refresh_home.mjs
+++ public/src/app_language2_refresh_home.mjs
@@ -37,11 +37,11 @@ export function app_language2_refresh_home(context) {
       });
       let stats = storage_local_get(app_fn, "stats");
       let right = object_property_get(stats, "right");
-      let wrong = object_property_get(stats, "wrong");
       html_p_text(root, "right");
       each_object(right, (key, value) => {
         html_div_text(root, string_combine_multiple([key, ": ", value]));
       });
+      let wrong = object_property_get(stats, "wrong");
       html_p_text(root, "wrong");
       each_object(wrong, (key, value) => {
         html_div_text(root, string_combine_multiple([key, ": ", value]));
