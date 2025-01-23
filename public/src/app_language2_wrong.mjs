eb1f2f4bf7da3aa4d3712728701cc3e0376fd1d5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-23 14:53:59 -0500

    2025-01-23T19:53:59.120Z watch   app_language2_wrong

--- public/src/app_language2_wrong.mjs
+++ public/src/app_language2_wrong.mjs
@@ -6,7 +6,6 @@ import { object_property_get } from "./object_property_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 export function app_language2_wrong(context, v, gap_initial, wait_initial) {
   let gap = object_property_get(v, "gap");
-  let gaps = object_property_get(context, "gaps");
   let rounded = round(gap);
   let { app_fn } = context;
   let stats = app_language2_stats_get(app_fn);
