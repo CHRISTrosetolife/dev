e83885058443952bc3a7c403a250cb5a78f7c3d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-24 15:43:05 -0500

    2025-01-24T20:43:03.808Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,8 +1,8 @@
+import { string_split_space } from "./string_split_space.mjs";
 import { list_map } from "./list_map.mjs";
 import { string_split_newline } from "./string_split_newline.mjs";
 import { http_cache } from "./http_cache.mjs";
 import { ceb_defintion_en } from "./ceb_defintion_en.mjs";
-import { string_trim_whitespace } from "./string_trim_whitespace.mjs";
 import { each_async } from "./each_async.mjs";
 export async function sandbox_3() {
   let text2 = await http_cache(
@@ -10,8 +10,8 @@ export async function sandbox_3() {
   );
   let text = await http_cache("https://norvig.com/ngrams/count_1w.txt");
   let lines = string_split_newline(text);
-  let trimmed = list_map(lines, string_trim_whitespace);
-  await each_async(trimmed, async (t) => {
+  let split = list_map(lines, string_split_space);
+  let firsts = await each_async(trimmed, async (t) => {
     await ceb_defintion_en(t);
   });
 }
