3978dd1c272af67a7f3444a6de05995d9ab5a986
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-24 15:54:02 -0500

    2025-01-24T20:54:00.846Z watch   sandbox_3

---
+++ public/src/list_difference_lower.mjs
@@ -0,0 +1,9 @@
+import { list_difference } from "./list_difference.mjs";
+import { string_case_lower } from "./string_case_lower.mjs";
+import { list_map } from "./list_map.mjs";
+export function list_difference_lower(c, firsts) {
+  let c_lower = list_map(c, string_case_lower);
+  let firsts_lower = list_map(firsts, string_case_lower);
+  let filtered = list_difference(firsts_lower, c_lower);
+  return filtered;
+}
--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,4 +1,4 @@
-import { string_case_lower } from "./string_case_lower.mjs";
+import { list_difference_lower } from "./list_difference_lower.mjs";
 import { http_cache_split_trim } from "./http_cache_split_trim.mjs";
 import { log } from "./log.mjs";
 import { list_first } from "./list_first.mjs";
@@ -8,7 +8,6 @@ import { http_cache } from "./http_cache.mjs";
 import { ceb_defintion_en } from "./ceb_defintion_en.mjs";
 import { each_async } from "./each_async.mjs";
 import { curse_words } from "./curse_words.mjs";
-import { list_difference } from "./list_difference.mjs";
 export async function sandbox_3() {
   let text2 = await http_cache(
     "https://raw.githubusercontent.com/first20hours/google-10000-english/refs/heads/master/google-10000-english-usa.txt",
@@ -19,9 +18,7 @@ export async function sandbox_3() {
   );
   let split = list_map(trimmed, string_split_space);
   let firsts = list_map(split, list_first);
-  let c_lower = list_map(c, string_case_lower);
-  let firsts_lower = list_map(firsts, string_case_lower);
-  let filtered = list_difference(firsts_lower, c_lower);
+  let filtered = list_difference_lower(c, firsts);
   log({
     filtered,
   });
