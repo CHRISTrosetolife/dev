6303be76662a03ee3df8fb0a5d8cb999d72c6bdd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-24 15:50:10 -0500

    2025-01-24T20:50:09.262Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,3 +1,4 @@
+import { string_case_lower } from "./string_case_lower.mjs";
 import { http_cache_split_trim } from "./http_cache_split_trim.mjs";
 import { log } from "./log.mjs";
 import { list_first } from "./list_first.mjs";
@@ -7,16 +8,18 @@ import { http_cache } from "./http_cache.mjs";
 import { ceb_defintion_en } from "./ceb_defintion_en.mjs";
 import { each_async } from "./each_async.mjs";
 import { curse_words } from "./curse_words.mjs";
+import { list_filter } from "./list_filter.mjs";
 export async function sandbox_3() {
   let text2 = await http_cache(
     "https://raw.githubusercontent.com/first20hours/google-10000-english/refs/heads/master/google-10000-english-usa.txt",
   );
   let c = await curse_words();
+  let c_lower = list_map(c, string_case_lower);
   let url = "https://norvig.com/ngrams/count_1w.txt";
   let trimmed = await http_cache_split_trim(url);
   let split = list_map(trimmed, string_split_space);
   let firsts = list_map(split, list_first);
-  filtered;
+  let filtered = list_filter(firsts);
   log({
     firsts,
   });
