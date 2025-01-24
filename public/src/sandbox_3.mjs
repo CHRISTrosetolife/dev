2833ea58831d97c93c9477e712405f8854571b47
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-24 15:50:53 -0500

    2025-01-24T20:50:52.679Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -8,20 +8,21 @@ import { http_cache } from "./http_cache.mjs";
 import { ceb_defintion_en } from "./ceb_defintion_en.mjs";
 import { each_async } from "./each_async.mjs";
 import { curse_words } from "./curse_words.mjs";
-import { list_filter } from "./list_filter.mjs";
+import { list_difference } from "./list_difference.mjs";
 export async function sandbox_3() {
   let text2 = await http_cache(
     "https://raw.githubusercontent.com/first20hours/google-10000-english/refs/heads/master/google-10000-english-usa.txt",
   );
   let c = await curse_words();
-  let c_lower = list_map(c, string_case_lower);
   let url = "https://norvig.com/ngrams/count_1w.txt";
   let trimmed = await http_cache_split_trim(url);
   let split = list_map(trimmed, string_split_space);
   let firsts = list_map(split, list_first);
-  let filtered = list_filter(firsts);
+  let c_lower = list_map(c, string_case_lower);
+  let firsts_lower = list_map(split, string_case_lower);
+  let filtered = list_difference(firsts_lower, c_lower);
   log({
-    firsts,
+    filtered,
   });
   return;
   await each_async(firsts, async (t) => {
