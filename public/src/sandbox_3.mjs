d13644c8a213be04c8e3d7c83e1f947d6d1b3b7f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-24 16:06:26 -0500

    2025-01-24T21:06:25.678Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,17 +1,14 @@
 import { object_property_exists } from "./object_property_exists.mjs";
 import { list_filter } from "./list_filter.mjs";
-import { list_chunk } from "./list_chunk.mjs";
 import { string_split_tab } from "./string_split_tab.mjs";
 import { list_difference_lower } from "./list_difference_lower.mjs";
 import { http_cache_split_trim } from "./http_cache_split_trim.mjs";
-import { log } from "./log.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_map } from "./list_map.mjs";
 import { http_cache } from "./http_cache.mjs";
 import { ceb_defintion_en } from "./ceb_defintion_en.mjs";
 import { each_async } from "./each_async.mjs";
 import { curse_words } from "./curse_words.mjs";
-import { list_last } from "./list_last.mjs";
 import { json_from } from "./json_from.mjs";
 export async function sandbox_3() {
   let text2 = await http_cache(
@@ -31,12 +28,7 @@ export async function sandbox_3() {
   let filtered2 = list_filter(filtered, (f) =>
     object_property_exists(dictionary, f),
   );
-  let cs = list_chunk(filtered2, 20);
-  log({
-    take: list_last(cs),
-  });
-  return;
-  await each_async(firsts, async (t) => {
+  await each_async(filtered2, async (t) => {
     await ceb_defintion_en(t);
   });
 }
