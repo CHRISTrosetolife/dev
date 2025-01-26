e53c106292f6c65d0ba6bf72772a4ce630deb941
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-26 10:49:28 -0500

    2025-01-26T15:49:27.106Z watch   app_language2_refresh_home

--- public/src/app_language2_refresh_home.mjs
+++ public/src/app_language2_refresh_home.mjs
@@ -90,8 +90,7 @@ export function app_language2_refresh_home(context) {
       each_object(closest, (gap, rw) => {
         gap = integer_parse(gap);
         let total2 = summation((c) => {
-          each(ss, (s) => {
-            let v = object_property_get(rw, s);
+          each_object(ss, (s, v) => {
             c(v);
           });
         });
