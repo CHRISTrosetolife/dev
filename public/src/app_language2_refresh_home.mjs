32707de1cf3a194430b9eaedc8be4bb2fece3066
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-26 10:41:34 -0500

    2025-01-26T15:41:34.261Z watch   app_language2_refresh_home

--- public/src/app_language2_refresh_home.mjs
+++ public/src/app_language2_refresh_home.mjs
@@ -87,9 +87,12 @@ export function app_language2_refresh_home(context) {
         });
       });
       each_object(closest, (gap, rw) => {
-        each(ss, (s) => {
-          let v = object_property_get(rw, s);
-        });
+        total = (c) => {
+          each(ss, (s) => {
+            let v = object_property_get(rw, s);
+            c(v);
+          });
+        };
       });
       html_button_width_full_text_click(root, "reset", () => {
         storage_local_set(app_fn, "stats", app_language2_stats_initial());
