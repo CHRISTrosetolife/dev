217168c7aa2413f21adc8e901f79cceceaa039cb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 17:11:21 -0500

    2025-01-28T22:11:19.329Z watch   app_language_2_refresh_learn

--- public/src/app_language_2_refresh_learn.mjs
+++ public/src/app_language_2_refresh_learn.mjs
@@ -123,10 +123,12 @@ export async function app_language_2_refresh_learn(context) {
       answer_text,
     );
     html_br2(root);
-    html_p_text(
-      root,
-      "You will be quizzed on this. You will be given the question. You will need to remember the answer.",
-    );
+    if (false) {
+      html_p_text(
+        root,
+        "You will be quizzed on this. You will be given the question. You will need to remember the answer.",
+      );
+    }
     html_button_next(root, async () => {
       decrease_wait();
       object_property_set(u, "learning", true);
