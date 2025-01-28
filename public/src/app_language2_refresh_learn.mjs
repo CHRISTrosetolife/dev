39d4e244ad082378e633a6abcd46e60efbad6532
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 16:35:36 -0500

    2025-01-28T21:35:35.566Z watch   app_language2_refresh_learn

--- public/src/app_language2_refresh_learn.mjs
+++ public/src/app_language2_refresh_learn.mjs
@@ -81,12 +81,12 @@ export async function app_language2_refresh_learn(context) {
   });
   let flattened = list_flatten(mapped2);
   let unique = list_unique(flattened);
-  let diffed = list_difference(values_skip_manual, unique);
+  let values = list_difference(values_skip_manual, unique);
   list_sort(values, app_language2_word_index);
   log({
     values,
   });
-  let values = list_copy(values);
+  values = list_copy(values);
   list_sort_property(values, "wait");
   let wait_initial = 0;
   let gap_initial = 0;
