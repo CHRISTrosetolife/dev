b0c20239ad490e56d94602ee385eb5423aa49f03
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 15:12:37 -0500

    2025-01-28T20:12:35.370Z watch   html_spacer_vertical_2

--- public/src/html_spacer_vertical_2.mjs
+++ public/src/html_spacer_vertical_2.mjs
@@ -1,5 +1,4 @@
 import { html_spacer_vertical_n } from "./html_spacer_vertical_n.mjs";
 export function html_spacer_vertical_2(root) {
-  let count = 2;
-  html_spacer_vertical_n(root, count);
+  html_spacer_vertical_n(root, 2);
 }
