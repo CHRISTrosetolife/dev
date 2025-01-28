8d1a73f04a02dbeacc77527e4fa1b011a3ddcedd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 17:54:24 -0500

    2025-01-28T22:54:23.940Z watch   app_language_2_answers_matches

--- public/src/app_language_2_answers_matches.mjs
+++ public/src/app_language_2_answers_matches.mjs
@@ -8,16 +8,16 @@ export function app_language_2_answers_matches(values_all, v) {
   let question = object_property_get(word, "question");
   let language = object_property_get(word, "language");
   let index = object_property_get(word, "index");
-  let matches = list_filter(
-    values_all,
-    (v) =>
+  let matches = list_filter(values_all, (v) => {
+    return (
       object_property_get(v, "learning") === true &&
       app_language_2_word_index(v) < index &&
       object_property_get(object_property_get(v, "word"), "question") ===
         question &&
       object_property_get(object_property_get(v, "word"), "language") ===
-        language,
-  );
+        language
+    );
+  });
   if (list_empty_not_is(matches)) {
     log({
       matches,
