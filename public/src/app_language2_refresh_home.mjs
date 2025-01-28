12584245a82f1ad7703dffb5b2c2e2aaab103bd9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 12:47:14 -0500

    2025-01-28T17:47:11.533Z watch   app_language2_refresh_home

--- public/src/app_language2_refresh_home.mjs
+++ public/src/app_language2_refresh_home.mjs
@@ -156,30 +156,24 @@ export function app_language2_refresh_home(context) {
       html_button_back(root, async () => {
         app_language2_refresh_home(context);
       });
-      let values_all = object_values(words);
-      let values = list_filter_property(values_all, "skip", false);
-      list_sort(values, app_language2_word_index);
-      each(list_take(values, 200), (item) => {
-        html_div_text(root, json_format_to(item));
+      html_p_text(
+        root,
+        "The gap of a word pair relates to the number of other word pairs you see before seeing the same word pair again. The factor is the rate at which a gap increases. The lower the factor, the easier the app is. The higher the factor, the more difficult the app is. Choose a factor:",
+      );
+      let n = range_from(11, 30);
+      let factors = list_map(n, (i) => i / 10);
+      let factor = app_language2_factor_get(context);
+      each(factors, (f) => {
+        let b = html_button_width_full_text_click(root, f, () => {
+          let app_fn = object_property_get(context, "app_fn");
+          storage_local_set(app_fn, "factor", f);
+          app_language2_gaps_update(context);
+          app_language2_refresh_home(context);
+        });
+        if (f === factor) {
+          html_style_success(b);
+        }
       });
     },
   );
-  html_p_text(
-    root,
-    "The gap of a word pair relates to the number of other word pairs you see before seeing the same word pair again. The factor is the rate at which a gap increases. The lower the factor, the easier the app is. The higher the factor, the more difficult the app is. Choose a factor:",
-  );
-  let n = range_from(11, 30);
-  let factors = list_map(n, (i) => i / 10);
-  let factor = app_language2_factor_get(context);
-  each(factors, (f) => {
-    let b = html_button_width_full_text_click(root, f, () => {
-      let app_fn = object_property_get(context, "app_fn");
-      storage_local_set(app_fn, "factor", f);
-      app_language2_gaps_update(context);
-      app_language2_refresh_home(context);
-    });
-    if (f === factor) {
-      html_style_success(b);
-    }
-  });
 }
