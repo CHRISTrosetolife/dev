ab7f507d154386c65edfaa42cff3ec9374cce550
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 18:21:07 -0500

    2025-01-28T23:21:06.281Z watch   html_button_me_email

--- public/src/html_button_me_email.mjs
+++ public/src/html_button_me_email.mjs
@@ -1,9 +1,9 @@
 import { me_email } from "./me_email.mjs";
 import { html_style_email } from "./html_style_email.mjs";
 import { html_button_width_full_text } from "./html_button_width_full_text.mjs";
-export function html_button_me_email(root) {
+export function html_button_me_email(parent) {
   let email_button = html_button_width_full_text(
-    root,
+    parent,
     "📨 Contact developer by email",
   );
   html_style_email(me_email())(email_button);
