dbb20696cb78b6f34e6fda6e19485622b46ea4e0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 15:12:23 -0500

    2025-01-28T20:12:22.805Z pb html_spacer_vertical_n

--- public/src/html_spacer_vertical_2.mjs
+++ public/src/html_spacer_vertical_2.mjs
@@ -1,5 +1,5 @@
 import { html_spacer_vertical_n } from "./html_spacer_vertical_n.mjs";
 export function html_spacer_vertical_2(root) {
   let count = 2;
-  html_spacer_vertical_n(count, root);
+  html_spacer_vertical_n(root, count);
 }
--- public/src/html_spacer_vertical_n.mjs
+++ public/src/html_spacer_vertical_n.mjs
@@ -1,6 +1,6 @@
 import { html_spacer_vertical } from "./html_spacer_vertical.mjs";
 import { each_range } from "./each_range.mjs";
-export function html_spacer_vertical_n(count, root) {
+export function html_spacer_vertical_n(root, count) {
   each_range(count, () => {
     html_spacer_vertical(root);
   });
