6213c2affc658bf0a193f3a2f59061df1111464f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 18:21:25 -0500

    2025-01-28T23:21:24.679Z watch   app_language_2

--- public/src/app_language_2.mjs
+++ public/src/app_language_2.mjs
@@ -75,7 +75,6 @@ export async function app_language_2(app_fn, language_learn, language_fluent) {
   let words = app_language_2_words_get(context);
   if (html_localhost_is()) {
     if (true) {
-      words = {};
     }
   }
   object_property_set(context, "words", words);
