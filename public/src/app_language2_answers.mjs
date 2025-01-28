be856db0e9858511d9e5e8afcc819eee6190a1c9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 16:27:12 -0500

    2025-01-28T21:27:11.068Z watch   app_language2_answers

--- public/src/app_language2_answers.mjs
+++ public/src/app_language2_answers.mjs
@@ -1,11 +1,11 @@
-import { app_language2_matches } from "./app_language2_matches.mjs";
+import { app_language2_answers_matches } from "./app_language2_answers_matches.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_add } from "./list_add.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_sort_string } from "./list_sort_string.mjs";
 import { identity } from "./identity.mjs";
 export function app_language2_answers(values_all, v) {
-  let matches = app_language2_matches(values_all, v);
+  let matches = app_language2_answers_matches(values_all, v);
   list_add(matches, v);
   let mapped = list_map(matches, (m) =>
     object_property_get(object_property_get(m, "word"), "answer"),
--- public/src/app_language2_answers_matches.mjs
+++ public/src/app_language2_answers_matches.mjs
@@ -2,7 +2,7 @@ import { log } from "./log.mjs";
 import { app_language2_word_index } from "./app_language2_word_index.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-export function app_language2_matches(values_all, v) {
+export function app_language2_answers_matches(values_all, v) {
   let word = object_property_get(v, "word");
   let question = object_property_get(word, "question");
   let language = object_property_get(word, "language");
