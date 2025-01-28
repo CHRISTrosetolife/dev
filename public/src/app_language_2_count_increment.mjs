efcb658c54442303ca859b8877e61888e7a59a78
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 17:32:51 -0500

    2025-01-28T22:32:50.040Z watch   app_language_2_count_increment

--- public/src/app_language_2_count_increment.mjs
+++ public/src/app_language_2_count_increment.mjs
@@ -1,5 +1,5 @@
 import { storage_local_increment } from "./storage_local_increment.mjs";
 export function app_language_2_count_increment(context) {
   let { app_fn } = context;
-  storage_local_increment(context, "count");
+  storage_local_increment(app_fn, "count");
 }
