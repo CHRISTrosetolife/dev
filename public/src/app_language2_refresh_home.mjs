e08b4c432c5b2be1d23d3ad2eebc08b74349e85e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 12:47:43 -0500

    2025-01-28T17:47:42.555Z watch   app_language2_refresh_home

--- public/src/app_language2_refresh_home.mjs
+++ public/src/app_language2_refresh_home.mjs
@@ -151,7 +151,7 @@ export function app_language2_refresh_home(context) {
     root,
     string_combine_multiple([emoji_chart(), " Factor"]),
     () => {
-      let { root, words } = context;
+      let { root } = context;
       html_clear_scroll_top_centered(root);
       html_button_back(root, async () => {
         app_language2_refresh_home(context);
