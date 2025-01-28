79f70e7413415103a9d3a56c23e07096b7e3892c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 17:32:41 -0500

    2025-01-28T22:32:39.305Z watch   app_language_2_count_increment

--- public/src/app_language_2_count_increment.mjs
+++ public/src/app_language_2_count_increment.mjs
@@ -1,4 +1,5 @@
 import { storage_local_increment } from "./storage_local_increment.mjs";
 export function app_language_2_count_increment(context) {
+  let { app_fn } = context;
   storage_local_increment(context, "count");
 }
--- public/src/storage_local_increment.mjs
+++ public/src/storage_local_increment.mjs
@@ -1,7 +1,6 @@
 import { storage_local_set } from "./storage_local_set.mjs";
 import { storage_local_initialize } from "./storage_local_initialize.mjs";
-export function storage_local_increment(context, name) {
-  let { app_fn } = context;
+export function storage_local_increment(app_fn, name) {
   let cnt = storage_local_initialize(app_fn, name, 0);
   cnt++;
   storage_local_set(app_fn, name, cnt);
