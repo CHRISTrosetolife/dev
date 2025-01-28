9789776246dd489c1ab1aa2d9983e4db12e08c48
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 16:45:11 -0500

    2025-01-28T21:45:09.399Z watch   app_language_2_answers_matches

--- public/src/app_language_2_answers_matches.mjs
+++ public/src/app_language_2_answers_matches.mjs
@@ -2,6 +2,7 @@ import { app_language_2_word_index } from "./app_language_2_word_index.mjs";
 import { log } from "./log.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { object_property_get } from "./object_property_get.mjs";
+import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export function app_language_2_answers_matches(values_all, v) {
   let word = object_property_get(v, "word");
   let question = object_property_get(word, "question");
@@ -17,7 +18,7 @@ export function app_language_2_answers_matches(values_all, v) {
       object_property_get(object_property_get(v, "word"), "language") ===
         language,
   );
-  if (false) {
+  if (list_empty_not_is(matches)) {
     log({
       matches,
     });
