4ce7d94a2efb80160cd0f3c26431c00096f2bed8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 17:04:04 -0500

    2025-01-28T22:04:04.118Z watch   app_language_2_refresh_learn

--- public/src/app_language_2_refresh_learn.mjs
+++ public/src/app_language_2_refresh_learn.mjs
@@ -177,8 +177,8 @@ export async function app_language_2_refresh_learn(context) {
     let answers = list_map_property(v_filtered2, "answer");
     let others = list_difference(answers, mapped);
     let others_unique = list_unique(others);
-    list_shuffle(others);
-    let other_taken = list_take(others, list_size(mapped));
+    list_shuffle(others_unique);
+    let other_taken = list_take(others_unique, list_size(mapped));
     let answer_text = list_join_comma_space(mapped);
     let choices = [answer_text, other];
     list_shuffle(choices);
