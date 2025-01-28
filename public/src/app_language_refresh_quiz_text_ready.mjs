17614c1dd0c15269a4579beaa39e8b73b4e39f39
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 13:04:17 -0500

    2025-01-28T18:04:17.190Z watch   app_language_refresh_quiz_text_ready

--- public/src/app_language_refresh_quiz_text_ready.mjs
+++ public/src/app_language_refresh_quiz_text_ready.mjs
@@ -1,4 +1,4 @@
 import { app_language_text } from "./app_language_text.mjs";
 export async function app_language_refresh_quiz_text_ready(language_fluent) {
-  return "👌 " + (await app_language_text(language_fluent, "Ready"));
+  return await app_language_text(language_fluent, "Ready");
 }
