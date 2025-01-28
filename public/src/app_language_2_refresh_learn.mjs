b0e652f3c2e811f65056edc81f967e540c271e80
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 17:20:32 -0500

    2025-01-28T22:20:31.436Z watch   app_language_2_refresh_learn

--- public/src/app_language_2_refresh_learn.mjs
+++ public/src/app_language_2_refresh_learn.mjs
@@ -219,7 +219,7 @@ export async function app_language_2_refresh_learn(context) {
         string_combine_multiple([
           "If this is the answer you thought of, choose this:",
         ]),
-        root,
+        quiz_container,
       );
       let b = html_button_width_full_text_click(
         quiz_container,
