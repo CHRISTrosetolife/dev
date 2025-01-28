b6923aeef6015d51a6426611601914a39e6f514e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 18:27:07 -0500

    2025-01-28T23:27:06.642Z watch   app_ceb

--- public/src/app_ceb.mjs
+++ public/src/app_ceb.mjs
@@ -1,4 +1,6 @@
+import { html_style_link_blank } from "./html_style_link_blank.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_ceb() {
   let body = html_style_default_initialize();
+  html_style_link_blank();
 }
