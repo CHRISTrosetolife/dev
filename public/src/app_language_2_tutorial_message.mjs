e64c9129652c34b623c699f70acfefd6f8f15139
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 17:13:38 -0500

    2025-01-28T22:13:37.771Z watch   app_language_2_tutorial_message

--- public/src/app_language_2_tutorial_message.mjs
+++ public/src/app_language_2_tutorial_message.mjs
@@ -1,10 +1,12 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { storage_local_initialize } from "./storage_local_initialize.mjs";
 export function app_language_2_tutorial_message(
-  app_fn,
+  app_fn2,
   root,
   tutorial_message,
 ) {
+  let app_fn3 = object_property_get(contetx, "app_fn");
   if (storage_local_initialize(app_fn, "tutorial", true) === true) {
     html_p_text(root, tutorial_message);
   }
