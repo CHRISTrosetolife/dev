a3131a96fcac468ef388e8e4bc301b5fc7f46c4a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 17:34:29 -0500

    2025-01-28T22:34:28.553Z watch   app_language_2

--- public/src/app_language_2.mjs
+++ public/src/app_language_2.mjs
@@ -33,6 +33,7 @@ export async function app_language_2(app_fn, language_learn, language_fluent) {
   await app_language_group_index_changed_inner(context, "all");
   let group = object_property_get(context, "group");
   if (html_localhost_is()) {
+    storage_local_set(app_fn, "count", 0);
     group = [
       [
         ["o", "oh"],
@@ -72,8 +73,8 @@ export async function app_language_2(app_fn, language_learn, language_fluent) {
   });
   let words = app_language_2_words_get(context);
   if (html_localhost_is()) {
-    words = {};
     if (false) {
+      words = {};
     }
   }
   object_property_set(context, "words", words);
