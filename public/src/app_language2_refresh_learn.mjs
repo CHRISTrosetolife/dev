00de037e28110b8deb1db423a0522a22f05fcac6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-28 13:55:04 -0500

    2025-01-28T18:55:04.282Z watch   app_language2_refresh_learn

--- public/src/app_language2_refresh_learn.mjs
+++ public/src/app_language2_refresh_learn.mjs
@@ -229,7 +229,11 @@ export async function app_language2_refresh_learn(context) {
     html_spacer_vertical_2(root);
     html_button_width_full_text_click(root, "Skip", () => {
       let skip_manual = app_language2_skip_manual_get(app_fn);
-      object_property_set(object, "property_name", value);
+      object_property_set(
+        object,
+        object_property_get(object2, "property_name"),
+        value,
+      );
     });
     if (false) {
       html_p_text(root, round_2(v_gap));
