5bccb53af98097c5802adf359307ad83793cada4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-29 16:23:58 -0500

    2025-01-29T21:23:58.505Z watch   app_language_2

--- public/src/app_language_2.mjs
+++ public/src/app_language_2.mjs
@@ -75,7 +75,7 @@ export async function app_language_2(app_fn, language_learn, language_fluent) {
   list_sort(flat2, (f) => {
     let key = object_property_get(f, "key");
     return (object_property_exists(words, key) &&
-      object_property_get(object_property_get(words, f), "learning") ===
+      object_property_get(object_property_get(words, key), "learning") ===
         true) ||
       object_property_exists(skip_manual, key)
       ? 1
