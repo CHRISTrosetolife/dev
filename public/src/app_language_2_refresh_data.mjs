ed44107db2bf2e0d2c39de55500807516b9b7095
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-29 16:36:20 -0500

    2025-01-29T21:36:18.420Z watch   app_language_2_refresh_data

--- public/src/app_language_2_refresh_data.mjs
+++ public/src/app_language_2_refresh_data.mjs
@@ -2,7 +2,6 @@ import { app_language_2_word_index } from "./app_language_2_word_index.mjs";
 import { app_language_2_button_back_home } from "./app_language_2_button_back_home.mjs";
 import { json_format_to } from "./json_format_to.mjs";
 import { html_div_text } from "./html_div_text.mjs";
-import { list_take } from "./list_take.mjs";
 import { each } from "./each.mjs";
 import { list_sort } from "./list_sort.mjs";
 import { object_values } from "./object_values.mjs";
@@ -11,7 +10,7 @@ export function app_language_2_refresh_data(context) {
   let { root, words } = context;
   let values_all = object_values(words);
   list_sort(values_all, app_language_2_word_index);
-  each(list_take(values_all, 200), (item) => {
+  each(values_all, (item) => {
     html_div_text(root, json_format_to(item));
   });
 }
