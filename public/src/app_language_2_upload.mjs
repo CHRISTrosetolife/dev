56a19f9c65cd1e6abd6c84e067a76c0d228edc07
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-29 07:00:28 -0500

    2025-01-29T12:00:26.184Z watch   app_language_2_upload

--- public/src/app_language_2_upload.mjs
+++ public/src/app_language_2_upload.mjs
@@ -8,7 +8,7 @@ export async function app_language_2_upload(from) {
   let result_new = app_language_upload_result(atoms, {
     definitions,
   });
-  return result_new;
+  return result_new.group;
   let existing_path = await app_language_group_upload(
     {
       from,
