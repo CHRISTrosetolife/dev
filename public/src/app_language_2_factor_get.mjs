e687a71a63f094662fcfcb3f54e23192bb1e030f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-30 14:13:38 -0500

    2025-01-30T19:13:37.803Z watch   app_language_2_factor_get

--- public/src/app_language_2_factor_get.mjs
+++ public/src/app_language_2_factor_get.mjs
@@ -1,3 +1,4 @@
+import { app_language_2_factor_new } from "./app_language_2_factor_new.mjs";
 import { app_language_2_factor_sort } from "./app_language_2_factor_sort.mjs";
 import { storage_local_initialize } from "./storage_local_initialize.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -7,12 +8,7 @@ export function app_language_2_factor_get(context) {
   let factors = storage_local_initialize(app_fn, "factors", 1.5);
   if (number_is(factors)) {
     let threshold = 0;
-    factors = [
-      {
-        threshold,
-        factor: factors,
-      },
-    ];
+    factors = [app_language_2_factor_new(threshold, factors)];
   }
   app_language_2_factor_sort(factors);
   return factors;
---
+++ public/src/app_language_2_factor_new.mjs
@@ -0,0 +1,6 @@
+export function app_language_2_factor_new(threshold, factor) {
+  return {
+    threshold,
+    factor,
+  };
+}
