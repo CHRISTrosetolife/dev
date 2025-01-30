709f29dd69971645e6e49b88c2f43ee3d5d316f2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-30 13:30:03 -0500

    2025-01-30T18:30:01.112Z watch   app_language_2_factor_get

--- public/src/app_language_2_factor_get.mjs
+++ public/src/app_language_2_factor_get.mjs
@@ -1,14 +1,16 @@
 import { storage_local_initialize } from "./storage_local_initialize.mjs";
 import { object_property_get } from "./object_property_get.mjs";
+import { number_is } from "./number_is.mjs";
 export function app_language_2_factor_get(context) {
   let app_fn = object_property_get(context, "app_fn");
-  let factors = storage_local_initialize(app_fn, "factors", [
-    {
-      threshold: 0,
-      factor: 1.5,
-    },
-  ]);
-  if (false) {
+  let factors = storage_local_initialize(app_fn, "factors", 1.5);
+  if (number_is(factors)) {
+    factors = [
+      {
+        threshold: 0,
+        factor: factors,
+      },
+    ];
   }
   return factors;
 }
