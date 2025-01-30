d0ba5019aa79a4bc6e08bc15d1c0791bc152d5e7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-30 12:23:40 -0500

    2025-01-30T17:23:35.947Z watch   app_language_2_refresh_factor

--- public/src/app_language_2_refresh_factor.mjs
+++ public/src/app_language_2_refresh_factor.mjs
@@ -9,7 +9,7 @@ export function app_language_2_refresh_factor(context) {
   let { root } = context;
   html_p_text(
     root,
-    "Lower factors are easier. Higher factors are more difficult. A gap relates to the number of other word pairs you see before seeing the same word pair again. A factor is the rate at which a gap increases. A difficulty of 2 means that the gap approximately doubles each time a word pair is correctly answered. The lower the factor, the more words are reviewed before introducing new words. The higher the factor, the more often new words are introduced. Factors may have thresholds. With thresholds, the gap and the threshold determine which factor is used. Configure your factor(s) and any thresholds:",
+    "Lower factors are easier. Higher factors are more difficult. A gap relates to the number of other word pairs you see before seeing the same word pair again. A factor is the rate at which a gap increases. A factor of 2 means that the gap approximately doubles each time a word pair is correctly answered. The lower the factor, the more words are reviewed before introducing new words. The higher the factor, the more often new words are introduced. Factors may have thresholds. With thresholds, the gap and the threshold determine which factor is used. Configure your factor(s) and any thresholds:",
   );
   let gaps = app_language_2_gaps_get(context);
   html_p_text(
