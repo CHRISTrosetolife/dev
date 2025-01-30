be4a4ae02bcf10d2f0542d85e5dd094fa0064ad1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-30 14:42:24 -0500

    2025-01-30T19:42:22.862Z watch   html_button_delete

--- public/src/html_button_delete.mjs
+++ public/src/html_button_delete.mjs
@@ -1,10 +1,10 @@
 import { emoji_delete } from "./emoji_delete.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
-export function html_button_delete(root, lambda) {
+export function html_button_delete(root, after, lambda) {
   html_button_width_full_text_click(
     root,
-    string_combine_multiple([emoji_delete(), " Delete"]),
+    string_combine_multiple([emoji_delete(), " Delete ", after]),
     lambda,
   );
 }
