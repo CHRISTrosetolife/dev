9eca7a7a7341b05763ed8476b9ad294b08a89ea0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-30 12:41:40 -0500

    2025-01-30T17:41:39.972Z watch   app_language_2_factor_get

--- public/src/app_language_2_factor_get.mjs
+++ public/src/app_language_2_factor_get.mjs
@@ -2,10 +2,11 @@ import { storage_local_initialize } from "./storage_local_initialize.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function app_language_2_factor_get(context) {
   let app_fn = object_property_get(context, "app_fn");
-  return storage_local_initialize(app_fn, "factors", [
+  let factors = storage_local_initialize(app_fn, "factors", [
     {
       threshold: 0,
       factor: 1.5,
     },
   ]);
+  return factors;
 }
