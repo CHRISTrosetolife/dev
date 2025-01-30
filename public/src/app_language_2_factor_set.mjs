a9ae13433de4bf2b6f19ed5974e23dba0bdf1540
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-30 14:27:57 -0500

    2025-01-30T19:27:55.729Z watch   app_language_2_factor_set

--- public/src/app_language_2_factor_set.mjs
+++ public/src/app_language_2_factor_set.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { app_language_2_gaps_update } from "./app_language_2_gaps_update.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
 import { app_language_2_factor_sort } from "./app_language_2_factor_sort.mjs";
@@ -5,6 +6,9 @@ import { object_property_get } from "./object_property_get.mjs";
 export function app_language_2_factor_set(context, factors) {
   let app_fn = object_property_get(context, "app_fn");
   app_language_2_factor_sort(factors);
+  log({
+    factors,
+  });
   storage_local_set(app_fn, "factor", factors);
   app_language_2_gaps_update(context);
 }
