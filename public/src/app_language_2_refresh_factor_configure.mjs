baddfa8905d7036d11bb82ba22816eef64a05a52
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-30 14:42:47 -0500

    2025-01-30T19:42:44.450Z watch   app_language_2_refresh_factor_configure

--- public/src/app_language_2_refresh_factor_configure.mjs
+++ public/src/app_language_2_refresh_factor_configure.mjs
@@ -70,9 +70,9 @@ export function app_language_2_refresh_factor_configure(
     });
   });
   if (threshold_value >= threshold_min) {
-    html_button_delete(root, 'factor', () => {
+    html_button_delete(root, "factor", () => {
       list_remove(factors, f);
-      app_language_2_factor_set(context, factors);
+      app_language_2_factor_set(context, factors);app_language_2_refresh_factor(context)
     });
   }
 }
