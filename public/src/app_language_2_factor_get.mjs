7f6becec0e876d2804611f77b4a4a553d70b2692
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-30 14:30:39 -0500

    2025-01-30T19:30:38.005Z watch   app_language_2_factor_get

--- public/src/app_language_2_factor_get.mjs
+++ public/src/app_language_2_factor_get.mjs
@@ -10,7 +10,7 @@ export function app_language_2_factor_get(context) {
   let app_fn = object_property_get(context, "app_fn");
   let factors = storage_local_initialize(
     app_fn,
-    "factors",
+    "factor",
     app_language_2_factor_value_default(),
   );
   if (number_is(factors)) {
