6b6aa4000649e876a3408f09ad7bef8215fbba51
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-30 13:56:46 -0500

    2025-01-30T18:56:46.070Z watch   app_language_2_refresh_factor_configure

--- public/src/app_language_2_refresh_factor_configure.mjs
+++ public/src/app_language_2_refresh_factor_configure.mjs
@@ -1,3 +1,4 @@
+import { object_property_set } from "./object_property_set.mjs";
 import { app_language_2_factor_sort } from "./app_language_2_factor_sort.mjs";
 import { html_style_button_default } from "./html_style_button_default.mjs";
 import { html_style_success } from "./html_style_success.mjs";
@@ -10,16 +11,20 @@ import { list_adder } from "./list_adder.mjs";
 import { list_map } from "./list_map.mjs";
 import { range_from } from "./range_from.mjs";
 import { app_language_2_button_back_home } from "./app_language_2_button_back_home.mjs";
-export function app_language_2_refresh_factor_configure(context, f, factors) {
+export function app_language_2_refresh_factor_configure(
+  context,
+  factor,
+  factors,
+) {
   app_language_2_button_back_home(context);
   let { root } = context;
   let n = range_from(11, 40);
   let factor_choices = list_map(n, (i) => i / 10);
-  let factor = object_property_get(object, "property_name");
   let buttons = list_adder((la) => {
     each(factor_choices, (f) => {
       let b = html_button_width_full_text_click(root, f, () => {
         let app_fn = object_property_get(context, "app_fn");
+        object_property_set(factor, "factor", f);
         app_language_2_factor_sort(factors);
         storage_local_set(app_fn, "factor", factors);
         app_language_2_gaps_update(context);
@@ -32,7 +37,7 @@ export function app_language_2_refresh_factor_configure(context, f, factors) {
         });
       });
       la(b);
-      if (object_property_get(f, "factor") === factors) {
+      if (object_property_get(factor, "factor") === factors) {
         html_style_success(b);
       }
     });
