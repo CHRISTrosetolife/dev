2f727b37fed5a9c912655ab729efad6387a94b5a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-30 13:35:06 -0500

    2025-01-30T18:35:05.878Z watch   app_language_2_gaps_update

--- public/src/app_language_2_gaps_update.mjs
+++ public/src/app_language_2_gaps_update.mjs
@@ -11,7 +11,7 @@ export function app_language_2_gaps_update(context) {
   let gap = 0;
   let gaps = [0];
   while (gap < w_size * w_size) {
-    list_filter(factors, (f) => object_property_get(object, "property_name"));
+    list_filter(factors, (f) => object_property_get(f, "threshold") >= gap);
     gap = ceiling(gap * factor) + 1;
     list_add(gaps, gap);
   }
