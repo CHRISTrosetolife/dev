bc2d6ed6e15dab58c619bb2860d4e91a38957eb8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-30 14:18:46 -0500

    2025-01-30T19:18:45.237Z watch   app_language_2_factor_string

--- public/src/app_language_2_factor_string.mjs
+++ public/src/app_language_2_factor_string.mjs
@@ -1,6 +1,10 @@
+import { log } from "./log.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_language_2_factor_string(f) {
+  log({
+    f,
+  });
   return string_combine_multiple([
     object_property_get(f, "factor"),
     "@",
