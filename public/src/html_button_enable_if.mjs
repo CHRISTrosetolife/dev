32d876ca77658be03600ee5425316f022a2b83a7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-31 16:19:01 -0500

    2025-01-31T21:18:58.354Z watch   app_language_2_refresh_home

---
+++ public/src/html_button_enable_if.mjs
@@ -0,0 +1,9 @@
+import { html_button_disable } from "./html_button_disable.mjs";
+import { html_button_enable } from "./html_button_enable.mjs";
+export function html_button_enable_if(condition) {
+  if (condition) {
+    html_button_enable(button);
+  } else {
+    html_button_disable(button);
+  }
+}
