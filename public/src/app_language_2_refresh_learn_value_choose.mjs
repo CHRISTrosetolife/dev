7312cf873c0a9753ce2e00d98acf0ae7415ecb98
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-31 18:11:29 -0500

    2025-01-31T23:11:28.584Z watch   app_language_2_refresh_learn_value_choose

--- public/src/app_language_2_refresh_learn_value_choose.mjs
+++ public/src/app_language_2_refresh_learn_value_choose.mjs
@@ -4,12 +4,7 @@ import { list_filter_property } from "./list_filter_property.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_sort_property } from "./list_sort_property.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-export function app_language_2_refresh_learn_value_choose(
-  values,
-  values_skip_manual,
-  max_indexes,
-  values_all,
-) {
+export function app_language_2_refresh_learn_value_choose(values, values_all) {
   list_sort_property(values, "wait");
   let wait_initial = 0;
   let gap_initial = 0;
@@ -20,12 +15,8 @@ export function app_language_2_refresh_learn_value_choose(
   let u = list_first_or(unlearning, null);
   return {
     v,
-    values,
-    values_skip_manual,
-    max_indexes,
     wait_initial,
     gap_initial,
-    values_all,
     learn_new,
     u,
   };
