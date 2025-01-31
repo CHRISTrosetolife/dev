ac36aab4d9168b6867a215a76f9184ce8c614072
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-31 18:28:38 -0500

    2025-01-31T23:28:36.806Z watch   app_language_2_reset_search

--- public/src/app_language_words_set.mjs
+++ public/src/app_language_words_set.mjs
@@ -1,5 +1,6 @@
 import { storage_local_set } from "./storage_local_set.mjs";
 export function app_language_words_set(context) {
   let { app_fn } = context;
+        let words = app_language_2_words_get(context);
   storage_local_set(app_fn, "words", words);
 }
