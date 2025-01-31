346bb80ec9ef0095bc2539f33ee85d7817439cb6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-31 16:04:51 -0500

    2025-01-31T21:04:51.287Z watch   app_language_2_refresh_home

--- public/src/app_language_2_refresh_home.mjs
+++ public/src/app_language_2_refresh_home.mjs
@@ -1,14 +1,8 @@
 import { app_language_2_words_sort } from "./app_language_2_words_sort.mjs";
-import { html_style_display_none } from "./html_style_display_none.mjs";
-import { storage_local_set } from "./storage_local_set.mjs";
-import { object_property_delete } from "./object_property_delete.mjs";
-import { app_language_2_word_key } from "./app_language_2_word_key.mjs";
-import { emoji_check } from "./emoji_check.mjs";
 import { app_language_2_other } from "./app_language_2_other.mjs";
 import { app_language_2_word_p } from "./app_language_2_word_p.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { html_hr } from "./html_hr.mjs";
-import { html_div } from "./html_div.mjs";
 import { object_values } from "./object_values.mjs";
 import { each } from "./each.mjs";
 import { html_p_text } from "./html_p_text.mjs";
@@ -31,6 +25,7 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { storage_local_exists } from "./storage_local_exists.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+import { html_button } from "./html_button.mjs";
 export function app_language_2_refresh_home(context) {
   let { root, app_fn } = context;
   html_clear_scroll_top_centered(root);
@@ -86,7 +81,7 @@ export function app_language_2_refresh_home(context) {
       let values_all = object_values(words);
       app_language_2_words_sort(values_all);
       each(values_all, (v) => {
-        let entry = html_div(root);
+        let entry = html_button(root);
         html_hr(entry);
         let word = object_property_get(v, "word");
         let language = object_property_get(word, "language");
@@ -97,16 +92,6 @@ export function app_language_2_refresh_home(context) {
           entry,
           app_language_2_other(language, language_learn, language_fluent),
           answer,
-        );
-        html_button_width_full_text_click(
-          entry,
-          string_combine_multiple([emoji_check(), "Unskip"]),
-          () => {
-            let word_key = app_language_2_word_key(v);
-            object_property_delete(skip_manual, word_key);
-            storage_local_set(app_fn, "skip_manual", skip_manual);
-            html_style_display_none(entry);
-          },
         );
       });
     });
