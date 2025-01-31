8fcd2250f52dce3f8aa0fef481550da194294c5d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-31 16:19:24 -0500

    2025-01-31T21:19:22.955Z watch   html_button_enable_if

--- public/src/html_button_enable_if.mjs
+++ public/src/html_button_enable_if.mjs
@@ -1,6 +1,6 @@
 import { html_button_disable } from "./html_button_disable.mjs";
 import { html_button_enable } from "./html_button_enable.mjs";
-export function html_button_enable_if(condition) {
+export function html_button_enable_if(button, condition) {
   if (condition) {
     html_button_enable(button);
   } else {
