73c6225ec5f19a789e06fa9e1fd275fd8e156292
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-31 15:17:32 -0500

    2025-01-31T20:17:31.987Z watch   app_language_2_refresh_learn

--- public/src/app_language_2_refresh_learn.mjs
+++ public/src/app_language_2_refresh_learn.mjs
@@ -1,3 +1,4 @@
+import { list_add_beginning } from "./list_add_beginning.mjs";
 import { list_find } from "./list_find.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
@@ -189,7 +190,8 @@ export async function app_language_2_refresh_learn(context) {
       [],
     );
     let questions_recent_limit = 15;
-    list_add(questions_recent, word_key);
+    list_add_beginning(questions_recent, word_key);
+    questions_recent = list_take(questions_recent, questions_recent_limit);
     let v_gap = object_property_get(v, "gap");
     let language = object_property_get(word, "language");
     let question = object_property_get(word, "question");
