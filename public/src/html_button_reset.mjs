defddb2c2b28e1db660273c969a827cacd5b7f91
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-31 16:21:45 -0500

    2025-01-31T21:21:40.656Z watch   html_button_reset

--- public/src/html_button_reset.mjs
+++ public/src/html_button_reset.mjs
@@ -2,7 +2,7 @@ import { emoji_danger } from "./emoji_danger.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function html_button_reset(root, lambda) {
-  html_button_width_full_text_click(
+  return html_button_width_full_text_click(
     root,
     string_combine_multiple([emoji_danger(), " Reset"]),
     lambda,
