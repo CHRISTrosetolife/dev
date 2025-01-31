1c61a83df5d008a49685e4c0dc19012bb158c4fd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-31 15:14:58 -0500

    2025-01-31T20:14:57.436Z watch   app_language_2_refresh_learn

--- public/src/app_language_2_refresh_learn.mjs
+++ public/src/app_language_2_refresh_learn.mjs
@@ -181,6 +181,7 @@ export async function app_language_2_refresh_learn(context) {
     app_language_2_skip(context, word);
   } else {
     let word = object_property_get(v, "word");
+    let word_key = app_language_2_word_key(v);
     let v_gap = object_property_get(v, "gap");
     let language = object_property_get(word, "language");
     let question = object_property_get(word, "question");
