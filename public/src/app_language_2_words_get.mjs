f33fe1afb7c8beab3d4057ba8f318217ecffb783
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-31 18:26:30 -0500

    2025-01-31T23:26:29.596Z watch   app_language_2_words_get

--- public/src/app_language_2_words_get.mjs
+++ public/src/app_language_2_words_get.mjs
@@ -2,7 +2,8 @@ import { object_property_exists } from "./object_property_exists.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { storage_local_initialize } from "./storage_local_initialize.mjs";
 export function app_language_2_words_get(context) {
-  if (object_property_exists(object, "property_name")) {
+  if (object_property_exists(context, "words")) {
+    return object_property_get(context, "words");
   }
   let app_fn = object_property_get(context, "app_fn");
   return storage_local_initialize(app_fn, "words", {});
