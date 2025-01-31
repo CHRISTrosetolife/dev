4ca65ab05591e34285dd4ad39533bdba77edde85
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-31 16:04:21 -0500

    2025-01-31T21:04:16.722Z watch   app_language_2_words_sort

---
+++ public/src/app_language_2_words_sort.mjs
@@ -0,0 +1,5 @@
+import { app_language_2_word_index } from "./app_language_2_word_index.mjs";
+import { list_sort } from "./list_sort.mjs";
+export function app_language_2_words_sort(values) {
+  list_sort(values, app_language_2_word_index);
+}
