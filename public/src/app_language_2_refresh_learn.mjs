2ba684db7ee30ad967dbce0d88576519af608201
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-01-31 15:15:56 -0500

    2025-01-31T20:15:56.249Z watch   app_language_2_refresh_learn

--- public/src/app_language_2_refresh_learn.mjs
+++ public/src/app_language_2_refresh_learn.mjs
@@ -188,6 +188,7 @@ export async function app_language_2_refresh_learn(context) {
       "questions_recent",
       [],
     );
+    let questions_recent_limit = 15;
     let v_gap = object_property_get(v, "gap");
     let language = object_property_get(word, "language");
     let question = object_property_get(word, "question");
