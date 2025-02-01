12d8cf173d86364e06902ce3c506370c6652b810
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 16:17:17 -0500

    2025-02-01T21:17:16.080Z watch   app_ceb2

--- public/src/app_ceb2.mjs
+++ public/src/app_ceb2.mjs
@@ -1,31 +1,9 @@
-import { storage_local_exists_not } from "./storage_local_exists_not.mjs";
-import { string_skip } from "./string_skip.mjs";
-import { storage_local_set } from "./storage_local_set.mjs";
-import { storage_local_remove } from "./storage_local_remove.mjs";
-import { storage_local_get } from "./storage_local_get.mjs";
-import { string_starts_with } from "./string_starts_with.mjs";
-import { each } from "./each.mjs";
-import { storage_local_keys } from "./storage_local_keys.mjs";
+import { app_ceb2_migrate } from "./app_ceb2_migrate.mjs";
 import { app_language_2 } from "./app_language_2.mjs";
-import { string_size } from "./string_size.mjs";
 export async function app_ceb2() {
   let language_learn = "ceb";
   let language_fluent = "en";
   let app_fn = app_ceb2;
-  let other = {
-    name: "",
-  };
-  let keys = storage_local_keys();
-  each(keys, (k) => {
-    let prefix = " ";
-    if (string_starts_with(k, prefix)) {
-      let k_old = string_skip(k, string_size(prefix));
-      if (storage_local_exists_not(app_fn, k_old)) {
-        let migration = storage_local_get(other, k_old);
-        storage_local_remove(other, k_old);
-        storage_local_set(app_fn, k_old, migration);
-      }
-    }
-  });
+  app_ceb2_migrate(app_fn);
   await app_language_2(app_fn, language_learn, language_fluent);
 }
---
+++ public/src/app_ceb2_migrate.mjs
@@ -0,0 +1,26 @@
+import { storage_local_set } from "./storage_local_set.mjs";
+import { storage_local_remove } from "./storage_local_remove.mjs";
+import { storage_local_get } from "./storage_local_get.mjs";
+import { storage_local_exists_not } from "./storage_local_exists_not.mjs";
+import { string_size } from "./string_size.mjs";
+import { string_skip } from "./string_skip.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
+import { each } from "./each.mjs";
+import { storage_local_keys } from "./storage_local_keys.mjs";
+export function app_ceb2_migrate(app_fn) {
+  let other = {
+    name: "",
+  };
+  let keys = storage_local_keys();
+  each(keys, (k) => {
+    let prefix = " ";
+    if (string_starts_with(k, prefix)) {
+      let k_old = string_skip(k, string_size(prefix));
+      if (storage_local_exists_not(app_fn, k_old)) {
+        let migration = storage_local_get(other, k_old);
+        storage_local_remove(other, k_old);
+        storage_local_set(app_fn, k_old, migration);
+      }
+    }
+  });
+}
