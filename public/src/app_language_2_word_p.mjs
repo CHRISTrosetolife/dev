861710ee456df23b547efe251a870a385d4c52ce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 10:32:11 -0500

    2025-02-01T15:32:02.079Z watch   app_language_2_word_p

--- public/src/app_language_2_word_p.mjs
+++ public/src/app_language_2_word_p.mjs
@@ -2,5 +2,5 @@ import { app_language_2_word } from "./app_language_2_word.mjs";
 import { html_p } from "./html_p.mjs";
 export function app_language_2_word_p(body, language, rememeber) {
   let p = html_p(body);
-  app_language_2_word(body, language, rememeber, false);
+  app_language_2_word(p, language, rememeber, false);
 }
