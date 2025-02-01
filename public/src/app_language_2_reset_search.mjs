8a6a9da3fadc7304bb2b3dd3af8f2babf9bedfa4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 10:30:10 -0500

    2025-02-01T15:30:09.402Z watch   app_language_2_reset_search

--- public/src/app_language_2_reset_search.mjs
+++ public/src/app_language_2_reset_search.mjs
@@ -3,7 +3,6 @@ import { app_language_2_refresh_learn_value_choose_initial } from "./app_languag
 import { app_language_words_set } from "./app_language_words_set.mjs";
 import { app_language_2_on_learn } from "./app_language_2_on_learn.mjs";
 import { app_language_2_decrease_wait } from "./app_language_2_decrease_wait.mjs";
-import { log } from "./log.mjs";
 import { app_language_2_refresh_learn } from "./app_language_2_refresh_learn.mjs";
 import { app_language_2_word_key } from "./app_language_2_word_key.mjs";
 import { app_language_2_refresh_learn_value_choose } from "./app_language_2_refresh_learn_value_choose.mjs";
@@ -48,10 +47,8 @@ export function app_language_2_reset_search(context, v) {
       let { values, values_skip_manual, max_indexes } =
         app_language_2_refresh_learn_value_choose_initial(context);
       while (true) {
-        log(1);
         let { v, wait_initial, gap_initial, learn_new, u } =
           app_language_2_refresh_learn_value_choose(values);
-        log(2);
         if (learn_new) {
           let key = app_language_2_word_key(u);
           if (key === key2) {
@@ -67,13 +64,9 @@ export function app_language_2_reset_search(context, v) {
             wait_initial,
             gap_initial,
           );
-          log(3);
         } else {
-          log(4);
           app_language_2_decrease_wait(values);
-          log(5);
           app_language_2_learn_success(context, v, false);
-          log(6);
         }
       }
     });
