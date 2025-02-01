8b551c948043a66d28215f8d3d7570e562decb0c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 13:11:27 -0500

    2025-02-01T18:11:26.711Z pd app_language_word_pair 2

--- public/src/app_language_word_pair.mjs
+++ public/src/app_language_word_pair.mjs
@@ -2,12 +2,7 @@ import { app_language_2_other } from "./app_language_2_other.mjs";
 import { html_spacer } from "./html_spacer.mjs";
 import { app_language_2_word } from "./app_language_2_word.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-export function app_language_word_pair(
-  word,
-  entry,
-  language_learn2,
-  language_fluent2,
-) {
+export function app_language_word_pair(word, entry, language_fluent2) {
   let { language_learn, language_fluent } = context;
   let language = object_property_get(word, "language");
   let question = object_property_get(word, "question");
