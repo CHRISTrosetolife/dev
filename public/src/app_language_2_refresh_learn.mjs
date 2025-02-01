3e4401d4b145825ba3bef24d955b225ce065a4d5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 11:39:17 -0500

    2025-02-01T16:39:16.666Z watch   app_language_2_refresh_learn

--- public/src/app_language_2_refresh_learn.mjs
+++ public/src/app_language_2_refresh_learn.mjs
@@ -1,3 +1,4 @@
+import { number_min } from "./number_min.mjs";
 import { app_language_2_refresh_learn_value_choose_initial } from "./app_language_2_refresh_learn_value_choose_initial.mjs";
 import { app_language_words_set } from "./app_language_words_set.mjs";
 import { app_language_2_decrease_wait } from "./app_language_2_decrease_wait.mjs";
@@ -131,7 +132,7 @@ export async function app_language_2_refresh_learn(context) {
     questions_recent_keys = list_unique(questions_recent_keys);
     let answer_count_max = 4;
     let answer_choice_word_count_max = 10;
-    let questions_recent_limit = 15;
+    let questions_recent_limit = number_min(15);
     questions_recent_keys = list_take_soft(
       questions_recent_keys,
       questions_recent_limit,
