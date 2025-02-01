00b09c6890aaf289cd3ddfbde2e1d74abc04e39a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 15:43:01 -0500

    2025-02-01T20:43:00.350Z watch   storage_local_get

--- public/src/storage_local_get.mjs
+++ public/src/storage_local_get.mjs
@@ -1,7 +1,4 @@
-import { json_from } from "./json_from.mjs";
-import { storage_local_key } from "./storage_local_key.mjs";
+import { storage_local_get_checkless } from "./storage_local_get_checkless.mjs";
 export function storage_local_get(fn_namespace, key) {
-  let json = localStorage.getItem(storage_local_key(fn_namespace, key));
-  let item = json_from(json);
-  return item;
+  return storage_local_get_checkless(fn_namespace, key);
 }
