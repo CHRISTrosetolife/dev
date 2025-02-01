c81adf4f20fd5cc416a3b4f92ab9fb1599729912
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 11:25:52 -0500

    2025-02-01T16:25:51.586Z watch   html_progress

--- public/src/html_progress.mjs
+++ public/src/html_progress.mjs
@@ -7,16 +7,16 @@ import { add_1 } from "./add_1.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 export function html_progress(root, progress_text, index, count) {
-  let progress = html_button_width_full(root);
-  html_span_text(progress, string_combine_multiple([progress_text, " "]));
-  let text_index = html_span_text(progress, number_locale(add_1(index)));
+  let button = html_button_width_full(root);
+  html_span_text(button, string_combine_multiple([progress_text, " "]));
+  let text_index = html_span_text(button, number_locale(add_1(index)));
   html_span_text(
-    progress,
+    button,
     string_combine_multiple([" of ", number_locale(count)]),
   );
-  html_style_success(progress);
+  html_style_success(button);
   let progress_percent = (index / count) * 100;
-  html_style(progress, {
+  html_style(button, {
     background: string_combine_multiple([
       "linear-gradient(90deg, ",
       html_style_success_background(),
@@ -28,6 +28,7 @@ export function html_progress(root, progress_text, index, count) {
     ]),
   });
   return {
+    button,
     text_index,
   };
 }
