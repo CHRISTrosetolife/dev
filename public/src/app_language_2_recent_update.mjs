3e774f52fa6b177f5061f437047f68de3ea44d0a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 12:41:52 -0500

    2025-02-01T17:41:51.896Z watch   app_language_2_recent_update

--- public/src/app_language_2_recent_update.mjs
+++ public/src/app_language_2_recent_update.mjs
@@ -7,28 +7,28 @@ import { list_unique } from "./list_unique.mjs";
 import { list_add_beginning } from "./list_add_beginning.mjs";
 export function app_language_2_recent_update(
   context,
-  questions_recent_keys_stored,
+  questions_recent_stored,
   word_key,
   recent_count,
   language,
 ) {
   let app_fn = object_property_get(context, "app_fn");
-  list_add_beginning(questions_recent_keys_stored, word_key);
-  questions_recent_keys_stored = list_unique(questions_recent_keys_stored);
+  list_add_beginning(questions_recent_stored, word_key);
+  questions_recent_stored = list_unique(questions_recent_stored);
   let answer_count_max = 4;
   let answer_choice_word_count_max = 10;
   let questions_recent_limit = number_max(
     recent_count,
     answer_count_max * answer_choice_word_count_max,
   );
-  questions_recent_keys_stored = list_take_soft(
-    questions_recent_keys_stored,
+  questions_recent_stored = list_take_soft(
+    questions_recent_stored,
     questions_recent_limit,
   );
   object_property_set(
     questions_recent_keys_by_language,
     language,
-    questions_recent_keys_stored,
+    questions_recent_stored,
   );
   storage_local_set(
     app_fn,
