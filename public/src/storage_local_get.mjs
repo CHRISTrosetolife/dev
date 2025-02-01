20372d0c6d39f9e32db971cb85d1cbef51528195
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 16:02:11 -0500

    2025-02-01T21:02:10.939Z watch   storage_local_get_checkless

--- public/src/storage_local_get.mjs
+++ public/src/storage_local_get.mjs
@@ -1,6 +1,5 @@
 import { storage_local_get_migration } from "./storage_local_get_migration.mjs";
 import { storage_local_get_checkless } from "./storage_local_get_checkless.mjs";
 export function storage_local_get(fn_namespace, key) {
-  storage_local_get_migration(fn_namespace, key);
   return storage_local_get_checkless(fn_namespace, key);
 }
