c1bafb232a573ea64d63a36b1390a7b9e19049c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 12:04:09 -0500

    2025-02-01T17:04:08.153Z watch   app_language_2_on_answer

--- public/src/app_language_2_on_answer.mjs
+++ public/src/app_language_2_on_answer.mjs
@@ -2,7 +2,14 @@ import { app_language_2_recent_update } from "./app_language_2_recent_update.mjs
 import { app_language_2_refresh_learn } from "./app_language_2_refresh_learn.mjs";
 import { app_language_2_answer_delay } from "./app_language_2_answer_delay.mjs";
 import { app_language_words_update } from "./app_language_words_update.mjs";
-export async function app_language_2_on_answer(context) {
+export async function app_language_2_on_answer(
+  context,
+  questions_recent_keys_stored,
+  word_key,
+  recent_count,
+  questions_recent_keys_by_language,
+  language,
+) {
   app_language_words_update(context);
   app_language_2_recent_update(
     context,
