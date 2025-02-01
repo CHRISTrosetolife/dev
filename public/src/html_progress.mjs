241904a70162e3de749359093a2170506f1ed0ee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 11:22:47 -0500

    2025-02-01T16:22:45.887Z watch   html_progress

--- public/src/html_progress.mjs
+++ public/src/html_progress.mjs
@@ -1,21 +1,23 @@
+import { html_inner_set } from "./html_inner_set.mjs";
+import { html_button_width_full } from "./html_button_width_full.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { number_locale } from "./number_locale.mjs";
 import { html_style_success_background } from "./html_style_success_background.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_style_success } from "./html_style_success.mjs";
 import { add_1 } from "./add_1.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { html_button_width_full_text } from "./html_button_width_full_text.mjs";
 export function html_progress(root, progress_text, index, count) {
-  let progress = html_button_width_full_text(
-    root,
-    string_combine_multiple([
-      progress_text,
-      " ",
-      number_locale(add_1(index)),
-      " of ",
-      number_locale(count),
-    ]),
-  );
+  let text = string_combine_multiple([
+    progress_text,
+    " ",
+    number_locale(add_1(index)),
+    " of ",
+    number_locale(count),
+  ]);
+  assert_arguments_length(arguments, 2);
+  let progress = html_button_width_full(root);
+  html_inner_set(progress, text);
   html_style_success(progress);
   let progress_percent = (index / count) * 100;
   html_style(progress, {
