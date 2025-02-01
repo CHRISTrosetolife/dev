f6fce066fb8f317b112429a0276f5d592a8b9d02
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 10:27:40 -0500

    2025-02-01T15:27:39.249Z watch   app_language_group_index_changed_inner

--- public/src/app_language_group_index_changed_inner.mjs
+++ public/src/app_language_group_index_changed_inner.mjs
@@ -1,3 +1,4 @@
+import { object_properties_new } from "./object_properties_new.mjs";
 import { object_merge } from "./object_merge.mjs";
 import { app_language_group_get } from "./app_language_group_get.mjs";
 export async function app_language_group_index_changed_inner(
@@ -5,6 +6,7 @@ export async function app_language_group_index_changed_inner(
   group_index,
 ) {
   let result = await app_language_group_get(context, group_index);
+  let chosen = object_properties_new();
   let { group, definitions, inverted } = result;
   object_merge(context, {
     group,
