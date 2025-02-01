a648e6db4199bfc250ab1565eabc6db4f9106b10
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 15:50:36 -0500

    2025-02-01T20:50:33.004Z watch   storage_local_initialize_lambda

--- public/src/storage_local_initialize_lambda.mjs
+++ public/src/storage_local_initialize_lambda.mjs
@@ -1,8 +1,9 @@
+import { storage_local_get_migration } from "./storage_local_get_migration.mjs";
 import { storage_local_get } from "./storage_local_get.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
 import { storage_local_exists_not } from "./storage_local_exists_not.mjs";
 export function storage_local_initialize_lambda(fn_namespace, key, value_get) {
-    storage_local_get_migration(fn_namespace, key);
+  storage_local_get_migration(fn_namespace, key);
   if (storage_local_exists_not(fn_namespace, key)) {
     let value = value_get();
     storage_local_set(fn_namespace, key, value);
