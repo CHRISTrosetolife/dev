879c9a3948badb4e735f2d25058eeb1205f652c5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 11:34:49 -0500

    2025-02-01T16:34:43.845Z watch   html_progress_index_update

--- public/src/html_progress_index_update.mjs
+++ public/src/html_progress_index_update.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { html_progress_style_update } from "./html_progress_style_update.mjs";
 import { html_progress_index_set } from "./html_progress_index_set.mjs";
-export function html_progress_index_update(count) {
+export function html_progress_index_update(result, count) {
+  assert_arguments_length(arguments, 2);
   html_progress_index_set(result, count);
   html_progress_style_update(result);
 }
