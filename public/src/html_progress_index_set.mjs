c86c45117033bfc4aab8c5159f7ccf803e952abe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 11:32:44 -0500

    2025-02-01T16:32:43.278Z watch   html_progress_index_set

--- public/src/html_progress_index_set.mjs
+++ public/src/html_progress_index_set.mjs
@@ -1,8 +1,12 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { add_1 } from "./add_1.mjs";
 import { number_locale } from "./number_locale.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
 export function html_progress_index_set(result, index) {
-  html_inner_set(number_locale(add_1(index)));
+  html_inner_set(
+    object_property_get(object, "property_name"),
+    number_locale(add_1(index)),
+  );
   object_property_set(result, "index", index);
 }
