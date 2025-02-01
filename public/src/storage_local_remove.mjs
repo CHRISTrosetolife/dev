bf01cdf98405bbfe63beac37ce3bf2f401546e93
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 16:15:17 -0500

    2025-02-01T21:15:15.311Z watch   storage_local_remove

--- public/src/storage_local_remove.mjs
+++ public/src/storage_local_remove.mjs
@@ -1,4 +1,6 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { storage_local_key } from "./storage_local_key.mjs";
 export function storage_local_remove(fn_namespace, key) {
+  assert_arguments_length(arguments, 2);
   localStorage.removeItem(storage_local_key(fn_namespace, key));
 }
