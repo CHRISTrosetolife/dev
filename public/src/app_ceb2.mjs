e4388f67ca076794486b44bd374573a7e3f0105e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 16:05:28 -0500

    2025-02-01T21:05:27.550Z watch   app_ceb2

--- public/src/app_ceb2.mjs
+++ public/src/app_ceb2.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { each_range } from "./each_range.mjs";
 import { log } from "./log.mjs";
@@ -6,7 +7,7 @@ export async function app_ceb2() {
   let language_learn = "ceb";
   let language_fluent = "en";
   let keys = list_adder((la) => {
-    each_range(localStorage.length, (i) => {
+    each_range(object_property_get(object, "property_name"), (i) => {
       la(localStorage.key(i));
     });
   });
