8433a7493b37deaad3e86c7d6ae664e274f604cf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 16:08:40 -0500

    2025-02-01T21:08:39.718Z watch   app_ceb2

--- public/src/app_ceb2.mjs
+++ public/src/app_ceb2.mjs
@@ -1,13 +1,11 @@
+import { each } from "./each.mjs";
 import { storage_local_keys } from "./storage_local_keys.mjs";
-import { log } from "./log.mjs";
 import { app_language_2 } from "./app_language_2.mjs";
 export async function app_ceb2() {
   let language_learn = "ceb";
   let language_fluent = "en";
   let keys = storage_local_keys();
-  log({
-    keys,
-  });
+  each(list, (item) => {});
   return;
   await app_language_2(app_ceb2, language_learn, language_fluent);
 }
