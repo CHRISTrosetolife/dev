b7ea2a00b8a193070ab95345a98a22911e591cbc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 11:28:17 -0500

    2025-02-01T16:28:16.734Z pb html_progress_index_set

---
+++ public/src/html_progress.mjs
@@ -0,0 +1,38 @@
+import { html_progress_index_set } from "./html_progress_index_set.mjs";
+import { html_span } from "./html_span.mjs";
+import { html_button_width_full } from "./html_button_width_full.mjs";
+import { number_locale } from "./number_locale.mjs";
+import { html_style_success_background } from "./html_style_success_background.mjs";
+import { html_style } from "./html_style.mjs";
+import { html_style_success } from "./html_style_success.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { html_span_text } from "./html_span_text.mjs";
+export function html_progress(root, progress_text, index, count) {
+  let button = html_button_width_full(root);
+  let result = {
+    button,
+    text_index,
+    count,
+  };
+  html_span_text(button, string_combine_multiple([progress_text, " "]));
+  let text_index = html_span(button);
+  html_progress_index_set(result, index);
+  html_span_text(
+    button,
+    string_combine_multiple([" of ", number_locale(count)]),
+  );
+  html_style_success(button);
+  let progress_percent = (index / count) * 100;
+  html_style(button, {
+    background: string_combine_multiple([
+      "linear-gradient(90deg, ",
+      html_style_success_background(),
+      " ",
+      progress_percent,
+      "%, white ",
+      progress_percent,
+      "%)",
+    ]),
+  });
+  return result;
+}
--- public/src/html_progress_index_set.mjs
+++ public/src/html_progress_index_set.mjs
@@ -2,7 +2,7 @@ import { object_property_set } from "./object_property_set.mjs";
 import { add_1 } from "./add_1.mjs";
 import { number_locale } from "./number_locale.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
-export function html_progress_index_set(index, result) {
+export function html_progress_index_set(result, index) {
   html_inner_set(number_locale(add_1(index)));
   object_property_set(result, "index", index);
 }
