9154907a7a3440f58f6ebd926eb417e604854b95
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 11:33:52 -0500

    2025-02-01T16:33:48.311Z watch   html_progress_style_update

--- public/src/html_progress_style_update.mjs
+++ public/src/html_progress_style_update.mjs
@@ -4,7 +4,7 @@ import { html_style } from "./html_style.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function html_progress_style_update(result) {
   let progress_percent =
-    (object_property_get(result, "index") /
+    ((object_property_get(result, "index") + 1) /
       object_property_get(result, "count")) *
     100;
   html_style(object_property_get(result, "button"), {
