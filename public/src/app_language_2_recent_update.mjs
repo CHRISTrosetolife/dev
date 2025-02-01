b5902b610e5e707d46f58d5f87a57dc995f5622f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 12:47:34 -0500

    2025-02-01T17:47:33.838Z watch   app_language_2_recent_update

--- public/src/app_language_2_recent_update.mjs
+++ public/src/app_language_2_recent_update.mjs
@@ -1,3 +1,4 @@
+import { app_language_2_questions_recent_get } from "./app_language_2_questions_recent_get.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
 import { object_property_set } from "./object_property_set.mjs";
@@ -9,8 +10,9 @@ export function app_language_2_recent_update(context, word, recent_count) {
   let app_fn = object_property_get(context, "app_fn");
   let language_learn = object_property_get(context, "language_learn");
   let language_fluent = object_property_get(context, "language_fluent");
+  let questions_recent_stored = app_language_2_questions_recent_get(context);
   list_add_beginning(questions_recent_stored, word_key);
-  let questions_recent_stored = list_unique(questions_recent_stored);
+  questions_recent_stored = list_unique(questions_recent_stored);
   let answer_count_max = 4;
   let answer_choice_word_count_max = 10;
   let questions_recent_limit = number_max(
