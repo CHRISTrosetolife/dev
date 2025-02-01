b03bf3ef5806332fb4ded96d61431fe5b2c97c2c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 16:04:19 -0500

    2025-02-01T21:04:17.668Z watch   app_ceb2

--- public/src/app_ceb2.mjs
+++ public/src/app_ceb2.mjs
@@ -1,6 +1,10 @@
+import { log } from "./log.mjs";
 import { app_language_2 } from "./app_language_2.mjs";
 export async function app_ceb2() {
   let language_learn = "ceb";
   let language_fluent = "en";
+  for (let i = 0, len = localStorage.length; i < len; ++i) {
+    console.log(localStorage.getItem(localStorage.key(i)));
+  }
   await app_language_2(app_ceb2, language_learn, language_fluent);
 }
