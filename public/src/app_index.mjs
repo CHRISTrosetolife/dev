892c4e595843c5eb619e20de37906a1ef1386cd3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 12:17:14 -0500

    2025-02-01T17:17:14.351Z watch   app_index

--- public/src/app_index.mjs
+++ public/src/app_index.mjs
@@ -1,3 +1,4 @@
+import { html_button_me_email } from "./html_button_me_email.mjs";
 import { app_index_choices_buttons } from "./app_index_choices_buttons.mjs";
 import { app_index_choices } from "./app_index_choices.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
@@ -5,6 +6,7 @@ import { html_style_default_initialize } from "./html_style_default_initialize.m
 export function app_index() {
   let root = html_style_default_initialize();
   html_clear_scroll_top_centered(root);
+  html_button_me_email(root);
   let choices = app_index_choices();
   app_index_choices_buttons(root, choices);
 }
