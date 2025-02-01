69f0048b844016347309e03ef909bdf2f41ff462
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 16:12:37 -0500

    2025-02-01T21:12:36.298Z watch   app_ceb2

--- public/src/app_ceb2.mjs
+++ public/src/app_ceb2.mjs
@@ -1,3 +1,4 @@
+import { string_skip } from "./string_skip.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
 import { storage_local_remove } from "./storage_local_remove.mjs";
 import { storage_local_get } from "./storage_local_get.mjs";
@@ -8,6 +9,7 @@ import { string_starts_with } from "./string_starts_with.mjs";
 import { each } from "./each.mjs";
 import { storage_local_keys } from "./storage_local_keys.mjs";
 import { app_language_2 } from "./app_language_2.mjs";
+import { string_size } from "./string_size.mjs";
 export async function app_ceb2() {
   let language_learn = "ceb";
   let language_fluent = "en";
@@ -16,6 +18,7 @@ export async function app_ceb2() {
     let prefix = " ";
     if (string_starts_with(k, prefix)) {
       let k_new = string_combine_multiple([fn_name("app_ceb2"), k]);
+      let k_old = string_skip(k, string_size(prefix));
       let other = {
         name: "",
       };
