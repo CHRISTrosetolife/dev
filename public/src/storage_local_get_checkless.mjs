50638d3248b59122146c98291e2813b7fadd8479
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 16:02:05 -0500

    2025-02-01T21:02:04.132Z watch   storage_local_get_checkless

--- public/src/storage_local_get_checkless.mjs
+++ public/src/storage_local_get_checkless.mjs
@@ -1,7 +1,1 @@
-import { json_from } from "./json_from.mjs";
-import { storage_local_key } from "./storage_local_key.mjs";
-export function storage_local_get_checkless(fn_namespace, key) {
-  let json = localStorage.getItem(storage_local_key(fn_namespace, key));
-  let item = json_from(json);
-  return item;
-}
+export function storage_local_get_checkless(fn_namespace, key) {}
