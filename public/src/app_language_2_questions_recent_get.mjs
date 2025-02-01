f0d17654f4806a7c84d0ecbac6ed61d4db2f1501
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 12:46:28 -0500

    2025-02-01T17:46:28.095Z watch   app_language_2_questions_recent_get

--- public/src/app_language_2_questions_recent_get.mjs
+++ public/src/app_language_2_questions_recent_get.mjs
@@ -1,4 +1,6 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { storage_local_initialize } from "./storage_local_initialize.mjs";
 export function app_language_2_questions_recent_get(context) {
+  let app_fn2 = object_property_get(context, "app_fn");
   return storage_local_initialize(app_fn, "questions_recent", []);
 }
