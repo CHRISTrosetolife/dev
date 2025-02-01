d9a199150482699d18952bc11ba8399d918cd373
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 13:13:02 -0500

    2025-02-01T18:13:02.476Z watch   app_language_2_skipped

--- public/src/app_language_2_skipped.mjs
+++ public/src/app_language_2_skipped.mjs
@@ -1,7 +1,6 @@
-import { app_language_2_word } from "./app_language_2_word.mjs";
+import { app_language_word_pair } from "./app_language_word_pair.mjs";
 import { app_language_2_word_key } from "./app_language_2_word_key.mjs";
 import { app_language_2_skip_manual_get } from "./app_language_2_skip_manual_get.mjs";
-import { app_language_2_other } from "./app_language_2_other.mjs";
 import { app_language_2_button_back_home } from "./app_language_2_button_back_home.mjs";
 import { html_style_display_none } from "./html_style_display_none.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
@@ -17,7 +16,6 @@ import { html_p_text } from "./html_p_text.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { object_values } from "./object_values.mjs";
-import { html_spacer } from "./html_spacer.mjs";
 export function app_language_2_skipped(context) {
   app_language_2_button_back_home(context);
   let { root, words, language_learn, language_fluent, app_fn } = context;
@@ -31,16 +29,7 @@ export function app_language_2_skipped(context) {
     let entry = html_div(root);
     html_hr(entry);
     let word = object_property_get(v, "word");
-    let language = object_property_get(word, "language");
-    let question = object_property_get(word, "question");
-    app_language_2_word(entry, language, question);
-    html_spacer(entry);
-    let answer = object_property_get(word, "answer");
-    app_language_2_word(
-      entry,
-      app_language_2_other(language, language_learn, language_fluent),
-      answer,
-    );
+    app_language_word_pair(context, entry, word);
     html_button_width_full_text_click(
       entry,
       string_combine_multiple([emoji_check(), "Unskip"]),
