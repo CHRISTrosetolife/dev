7a395683048e163ef45455add073bbb411edb97d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 16:16:12 -0500

    2025-02-01T21:16:09.050Z watch   storage_local_exists_not

--- public/src/storage_local_exists_not.mjs
+++ public/src/storage_local_exists_not.mjs
@@ -1,4 +1,6 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { storage_local_exists } from "./storage_local_exists.mjs";
 export function storage_local_exists_not(fn_namespace, key) {
+  assert_arguments_length(arguments, 2);
   return !storage_local_exists(fn_namespace, key);
 }
