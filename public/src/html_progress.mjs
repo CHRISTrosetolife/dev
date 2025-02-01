5199f937a14835a2af7b2de79e246ed038bae303
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 11:23:06 -0500

    2025-02-01T16:23:06.229Z watch   html_progress

--- public/src/html_progress.mjs
+++ public/src/html_progress.mjs
@@ -1,6 +1,5 @@
 import { html_inner_set } from "./html_inner_set.mjs";
 import { html_button_width_full } from "./html_button_width_full.mjs";
-import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { number_locale } from "./number_locale.mjs";
 import { html_style_success_background } from "./html_style_success_background.mjs";
 import { html_style } from "./html_style.mjs";
@@ -8,7 +7,6 @@ import { html_style_success } from "./html_style_success.mjs";
 import { add_1 } from "./add_1.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function html_progress(root, progress_text, index, count) {
-  assert_arguments_length(arguments, 4);
   let progress = html_button_width_full(root);
   html_inner_set(
     progress,
