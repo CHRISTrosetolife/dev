2f9ba7d874b4e301141f8899188f207516cce156
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-01 12:53:22 -0500

    2025-02-01T17:53:21.749Z watch   list_copy_reverse

--- public/src/list_copy_reverse.mjs
+++ public/src/list_copy_reverse.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { list_reverse } from "./list_reverse.mjs";
 import { list_copy } from "./list_copy.mjs";
 export function list_copy_reverse(list) {
+  assert_arguments_length(arguments, 1);
   let result = list_copy(list);
   list_reverse(result);
   return result;
