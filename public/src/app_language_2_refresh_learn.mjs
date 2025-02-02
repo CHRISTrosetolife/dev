184f30dd783aee63f33d4d86d48f39321110c4a3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-02 15:45:51 -0500

    2025-02-02T20:45:50.924Z watch   app_language_2_refresh_learn

--- public/src/app_language_2_refresh_learn.mjs
+++ public/src/app_language_2_refresh_learn.mjs
@@ -272,7 +272,7 @@ export async function app_language_2_refresh_learn(context) {
     });
     app_language_2_skip(context, word);
   }
-  function app_language_2_answers_take(v_filtered2, mapped) {
+  function app_language_2_answers_take(v_filtered2, mapped, choices_count) {
     let answers = list_map_property(v_filtered2, "answer");
     let others = list_difference(answers, mapped);
     let others_unique = list_unique(others);
