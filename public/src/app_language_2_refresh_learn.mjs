a6f910b42f8489ec0c287156a3937d6bb6e3fad2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-02 14:18:17 -0500

    2025-02-02T19:18:16.783Z watch   app_language_2_refresh_learn

--- public/src/app_language_2_refresh_learn.mjs
+++ public/src/app_language_2_refresh_learn.mjs
@@ -1,3 +1,4 @@
+import { app_language_2_font_size } from "./app_language_2_font_size.mjs";
 import { html_p } from "./html_p.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_button_text_click } from "./html_button_text_click.mjs";
@@ -257,7 +258,9 @@ export async function app_language_2_refresh_learn(context) {
         }
         await app_language_2_on_answer(context, word, recent_count);
       });
-      each(list2, (item) => {});
+      each([b, b2], (bi) => {
+        app_language_2_font_size();
+      });
       html_spacer_vertical_2(quiz_container);
     });
     app_language_2_skip(context, word);
