b835e6e86a551a5cbf5760da8808af9e74c5cafb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 16:05:11 -0500

    2025-02-03T21:05:11.492Z watch   list_all_or

--- public/src/list_all_or.mjs
+++ public/src/list_all_or.mjs
@@ -1,7 +1,7 @@
 import { each_or } from "./each_or.mjs";
 import { list_all } from "./list_all.mjs";
 export function list_all_or(list, conditions) {
-  list_all(list, (l) => {
+  return list_all(list, (l) => {
     return each_or(l, conditions);
   });
 }
