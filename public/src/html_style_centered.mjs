b8b709ef0c0727f703b544dc35bd4c99a7ec580f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 17:14:06 -0500

    2025-02-03T22:14:05.877Z watch   html_style_centered

--- public/src/html_style_centered.mjs
+++ public/src/html_style_centered.mjs
@@ -1,5 +1,4 @@
 import { html_style_text_align } from "./html_style_text_align.mjs";
 export function html_style_centered(component) {
-  let value = "center";
-  html_style_text_align(component, value);
+  html_style_text_align(component, "center");
 }
