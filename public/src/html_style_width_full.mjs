a3f2af1d0108a32870f35db85be0296b0f3bc591
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 17:31:40 -0500

    2025-02-03T22:31:38.658Z watch   html_style_width_full

--- public/src/html_style_width_full.mjs
+++ public/src/html_style_width_full.mjs
@@ -1,5 +1,4 @@
 import { html_style_width } from "./html_style_width.mjs";
 export function html_style_width_full(component) {
-  let value = "100%";
-  html_style_width(component, value);
+  html_style_width(component, "100%");
 }
