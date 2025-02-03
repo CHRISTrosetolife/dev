a72677bc05c9d8e421bae22800cd69cc4f561531
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 14:00:23 -0500

    2025-02-03T19:00:22.488Z watch   app_code_local_upload

--- public/src/app_code_local_upload.mjs
+++ public/src/app_code_local_upload.mjs
@@ -1,5 +1,6 @@
+import { storage_upload_object } from "./storage_upload_object.mjs";
 import { functions_source_get } from "./functions_source_get.mjs";
 export async function app_code_local_upload() {
   let s = await functions_source_get();
-  return s;
+  await storage_upload_object("code/functions.json", s);
 }
