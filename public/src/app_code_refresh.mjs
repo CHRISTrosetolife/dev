de4937d2c804382a065a4d0ef494af59f3d79a83
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 14:49:14 -0500

    2025-02-03T19:49:13.164Z watch   app_code_refresh

--- public/src/app_code_refresh.mjs
+++ public/src/app_code_refresh.mjs
@@ -1,3 +1,4 @@
+import { html_button_back } from "./html_button_back.mjs";
 import { html_list } from "./html_list.mjs";
 import { function_path_to_name } from "./function_path_to_name.mjs";
 import { list_map } from "./list_map.mjs";
@@ -11,10 +12,9 @@ export function app_code_refresh(root) {
   html_clear_scroll_top_centered();
   html_button_width_full_text_click(root, "Functions", async () => {
     html_clear_scroll_top_centered();
+    html_button_back(root, () => {app_code_refresh(root)});
     let files = await http_storage_get(app_code_local_functions_path());
     global_files_initialize(files);
     let file_paths = object_properties(files);
     let mapped2 = list_map(file_paths, function_path_to_name);
-    html_list(root, mapped2);
-  });
-}
+    html_list(ro
