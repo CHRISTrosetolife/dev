6aaa3778efaa8d26b63b796b934831b6a3b0ada8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 15:42:55 -0500

    2025-02-03T20:42:55.327Z watch   html_style_display_block_or_none

--- public/src/html_style_display_block_or_none.mjs
+++ public/src/html_style_display_block_or_none.mjs
@@ -1,9 +1,9 @@
 import { html_style_display_none } from "./html_style_display_none.mjs";
 import { html_style_display_block } from "./html_style_display_block.mjs";
-export function html_style_display_block_or_none(condition, p_error_message) {
+export function html_style_display_block_or_none(condition, component) {
   if (condition) {
-    html_style_display_block(p_error_message);
+    html_style_display_block(component);
   } else {
-    html_style_display_none(p_error_message);
+    html_style_display_none(component);
   }
 }
