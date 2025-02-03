0f3dda6ab49838a884165f4f2a7f396bd4d96627
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 15:30:45 -0500

    2025-02-03T20:30:44.352Z watch   app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -26,8 +26,7 @@ export async function app_code() {
     html_input_width_full_placeholder(parent, "Username");
     function username_valid(username) {
       let fns = [string_letter_is, string_underscore_is, string_digit_is];
-      let list = string_all_or(username, fns);
-      return list_all(list, (l) => string_letter_is(l));
+      return string_all_or(username, fns);
     }
   } else {
     app_code_refresh(context);
