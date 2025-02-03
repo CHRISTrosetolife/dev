756a633c56dd429f7f14d0e3b562c10bbdbe6524
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 15:45:46 -0500

    2025-02-03T20:45:45.552Z watch   html_on_input_initial

--- public/src/html_on_input_initial.mjs
+++ public/src/html_on_input_initial.mjs
@@ -1,5 +1,5 @@
 import { html_on_input } from "./html_on_input.mjs";
-export function html_on_input_initial(search_input, lambda) {
-  html_on_input(search_input, lambda);
+export function html_on_input_initial(component, lambda) {
+  html_on_input(component, lambda);
   lambda();
 }
