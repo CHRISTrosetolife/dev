ecdb4d6795c648a4fe355e920f5f648174a066be
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 14:50:38 -0500

    2025-02-03T19:50:37.978Z watch   app_code_refresh

--- public/src/app_code_refresh.mjs
+++ public/src/app_code_refresh.mjs
@@ -5,7 +5,7 @@ import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered
 export function app_code_refresh(context) {
   html_clear_scroll_top_centered();
   let root = object_property_get(context, "root");
-  html_button_width_full_text_click(context, "Functions", async () => {
+  html_button_width_full_text_click(root, "Functions", async () => {
     await app_code_refresh_functions(context);
   });
 }
