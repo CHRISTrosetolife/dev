eb790d550dee9c99469ae3928ec8ce294dc67c9b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 14:51:50 -0500

    2025-02-03T19:51:48.980Z watch   app_code_refresh_functions

--- public/src/app_code_refresh_functions.mjs
+++ public/src/app_code_refresh_functions.mjs
@@ -1,4 +1,4 @@
-import { object_property_get } from "./object_property_get.mjs";
+import { html_clear_scroll_top_centered_context } from "./html_clear_scroll_top_centered_context.mjs";
 import { html_list } from "./html_list.mjs";
 import { function_path_to_name } from "./function_path_to_name.mjs";
 import { list_map } from "./list_map.mjs";
@@ -8,10 +8,8 @@ import { app_code_local_functions_path } from "./app_code_local_functions_path.m
 import { http_storage_get } from "./http_storage_get.mjs";
 import { app_code_refresh } from "./app_code_refresh.mjs";
 import { html_button_back } from "./html_button_back.mjs";
-import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 export async function app_code_refresh_functions(context) {
-  html_clear_scroll_top_centered();
-  let root = object_property_get(context, "root");
+  let root = html_clear_scroll_top_centered_context(context);
   html_button_back(root, () => {
     app_code_refresh(root);
   });
