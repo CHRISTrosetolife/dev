fbe5a71deed6e01c1d119c69c826bb7e39bce1b8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 15:30:36 -0500

    2025-02-03T20:30:36.336Z watch   string_all_or

--- public/src/string_all_or.mjs
+++ public/src/string_all_or.mjs
@@ -2,6 +2,5 @@ import { list_all_or } from "./list_all_or.mjs";
 import { string_split_empty } from "./string_split_empty.mjs";
 export function string_all_or(username, fns) {
   let list = string_split_empty(username);
-  list_all_or(list, fns);
-  return list;
+  return list_all_or(list, fns);
 }
