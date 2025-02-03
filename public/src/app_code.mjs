ec0ad5e1843cab2d241561c691848fda9c4651c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 15:15:21 -0500

    2025-02-03T20:15:20.729Z watch   app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,3 +1,4 @@
+import { html_clear_scroll_top_centered_context } from "./html_clear_scroll_top_centered_context.mjs";
 import { storage_local_exists_not } from "./storage_local_exists_not.mjs";
 import { app_code_refresh } from "./app_code_refresh.mjs";
 import { html_scripts_load } from "./html_scripts_load.mjs";
@@ -9,6 +10,7 @@ export async function app_code() {
     root,
   };
   if (storage_local_exists_not(app_code, "username")) {
+    let root = html_clear_scroll_top_centered_context(context);
   } else {
     app_code_refresh(context);
   }
