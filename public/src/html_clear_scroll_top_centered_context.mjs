fe86c6facb3380481393aca7ee470afb5acfd868
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 14:53:24 -0500

    2025-02-03T19:53:22.631Z watch   html_clear_scroll_top_centered_context

--- public/src/html_clear_scroll_top_centered_context.mjs
+++ public/src/html_clear_scroll_top_centered_context.mjs
@@ -1,7 +1,7 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 export function html_clear_scroll_top_centered_context(context) {
-  html_clear_scroll_top_centered();
   let root = object_property_get(context, "root");
+  html_clear_scroll_top_centered(root);
   return root;
 }
