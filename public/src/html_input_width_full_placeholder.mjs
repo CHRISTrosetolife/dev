1b333094d1817c90c59deeeb612d2cc0f40c087e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 17:14:34 -0500

    2025-02-03T22:14:31.182Z watch   html_input_width_full_placeholder

--- public/src/html_input_width_full_placeholder.mjs
+++ public/src/html_input_width_full_placeholder.mjs
@@ -1,3 +1,4 @@
+import { html_style_left } from "./html_style_left.mjs";
 import { html_style_font_size } from "./html_style_font_size.mjs";
 import { html_div_text } from "./html_div_text.mjs";
 import { html_placeholder } from "./html_placeholder.mjs";
@@ -5,6 +6,7 @@ import { html_input_width_full } from "./html_input_width_full.mjs";
 export function html_input_width_full_placeholder(parent, placeholder) {
   let title = html_div_text(parent, placeholder);
   html_style_font_size(title, 2.5);
+  html_style_left(title);
   let input = html_input_width_full(parent);
   html_placeholder(input, placeholder);
   return input;
