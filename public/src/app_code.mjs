6343fad110d27bc3e918c240c90476cf904e92f7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 13:31:50 -0500

    2025-02-03T18:31:49.285Z watch   app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,27 +1,6 @@
-import { app_save_change } from "./app_save_change.mjs";
-import { functions_source_get } from "./functions_source_get.mjs";
-import { function_run_remote } from "./function_run_remote.mjs";
-import { object_property_initialize_get_async } from "./object_property_initialize_get_async.mjs";
-import { app_code_refresh } from "./app_code_refresh.mjs";
-import { global_get } from "./global_get.mjs";
 import { html_scripts_load } from "./html_scripts_load.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-import { object_merge } from "./object_merge.mjs";
 export async function app_code() {
   let root = html_style_default_initialize();
   await html_scripts_load(root, ["axios", "acorn", "astring"]);
-  await app_save_change(app_code, async (save) => {
-    let files = await object_property_initialize_get_async(
-      save,
-      "files",
-      async () => {
-        return await function_run_remote(functions_source_get.name, []);
-      },
-    );
-    let g = global_get();
-    object_merge(g, {
-      files,
-    });
-  });
-  await app_code_refresh(root);
 }
