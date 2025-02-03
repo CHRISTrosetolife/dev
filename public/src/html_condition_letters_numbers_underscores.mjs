6f082c92e295f761848908f95f6c5ef8ef6825f4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 16:10:37 -0500

    2025-02-03T21:10:37.323Z watch   html_condition_letters_numbers_underscores

--- public/src/html_condition_letters_numbers_underscores.mjs
+++ public/src/html_condition_letters_numbers_underscores.mjs
@@ -5,11 +5,12 @@ import { string_all_or } from "./string_all_or.mjs";
 export function html_condition_letters_numbers_underscores() {
   return {
     message: (value) => "contain only letters, numbers or underscores",
-    condition: (u) =>
+    condition: function valid_is(u) {
       string_all_or(u, [
         string_letter_is,
         string_underscore_is,
         string_digit_is,
-      ]),
+      ]);
+    },
   };
 }
