8fcdd896889fde543446ebd4ee722a9cb56d2d00
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 14:18:53 -0500

    2025-02-03T19:18:52.718Z de folder_path_src_prefixed

--- public/src/folder_path_src_prefixed.mjs
+++
@@ -1,6 +0,0 @@
-import { folder_path_src } from "./folder_path_src.mjs";
-import { folder_current_prefix } from "./folder_current_prefix.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
-export function folder_path_src_prefixed() {
-  return string_combine_multiple([folder_current_prefix(), folder_path_src()]);
-}
