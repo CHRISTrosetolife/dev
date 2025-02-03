2b47b7f4faf494b7b6f53ffe626c4195ddc04188
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 13:33:04 -0500

    2025-02-03T18:33:03.613Z watch   app_language_group_get

--- public/src/app_language_group_get.mjs
+++ public/src/app_language_group_get.mjs
@@ -1,7 +1,7 @@
+import { http_storage_get } from "./http_storage_get.mjs";
 import { app_language_group_path } from "./app_language_group_path.mjs";
-import { http_storage } from "./http_storage.mjs";
 export async function app_language_group_get(context, group_index) {
-  return await http_storage(
+  return await http_storage_get(
     app_language_group_path(
       context.language_learn,
       context.language_fluent,
