8f09ccda15d1485daf66950e2561df6282578170
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 13:56:32 -0500

    2025-02-03T18:56:32.140Z watch   app_code_local_upload

--- public/src/app_code_local_upload.mjs
+++ public/src/app_code_local_upload.mjs
@@ -1,1 +1,4 @@
-export function app_code_local_upload() {}
+import { functions_source_get } from "./functions_source_get.mjs";
+export async function app_code_local_upload() {
+  await functions_source_get();
+}
