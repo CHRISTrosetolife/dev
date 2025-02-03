3636725daf55c7ec23006c1d79f60d7faf441fbf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 14:48:44 -0500

    2025-02-03T19:48:43.831Z watch   app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,24 +1,8 @@
-import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
-import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
-import { function_path_to_name } from "./function_path_to_name.mjs";
-import { list_map } from "./list_map.mjs";
-import { html_list } from "./html_list.mjs";
-import { global_files_initialize } from "./global_files_initialize.mjs";
-import { app_code_local_functions_path } from "./app_code_local_functions_path.mjs";
-import { http_storage_get } from "./http_storage_get.mjs";
+import { app_code_refresh } from "./app_code_refresh.mjs";
 import { html_scripts_load } from "./html_scripts_load.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-import { object_properties } from "./object_properties.mjs";
 export async function app_code() {
   let root = html_style_default_initialize();
   await html_scripts_load(root, ["axios", "acorn", "astring"]);
-  html_clear_scroll_top_centered();
-  html_button_width_full_text_click(root, "Functions", async () => {
-    html_clear_scroll_top_centered();
-    let files = await http_storage_get(app_code_local_functions_path());
-    global_files_initialize(files);
-    let file_paths = object_properties(files);
-    let mapped2 = list_map(file_paths, function_path_to_name);
-    html_list(root, mapped2);
-  });
+  app_code_refresh(root);
 }
