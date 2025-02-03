1cddbaf564006d0b9c22c27c221732f068e704fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 17:23:53 -0500

    2025-02-03T22:23:52.317Z watch   app_code_refresh_functions

--- public/src/app_code_refresh_functions.mjs
+++ public/src/app_code_refresh_functions.mjs
@@ -1,3 +1,6 @@
+import { global_files } from "./global_files.mjs";
+import { global_get } from "./global_get.mjs";
+import { object_property_initialize_get_async } from "./object_property_initialize_get_async.mjs";
 import { global_files_set } from "./global_files_set.mjs";
 import { app_code_refresh_main } from "./app_code_refresh_main.mjs";
 import { html_clear_scroll_top_centered_context } from "./html_clear_scroll_top_centered_context.mjs";
@@ -15,6 +18,11 @@ export async function app_code_refresh_functions(context) {
   });
   let files = await http_storage_get(app_code_local_functions_path());
   global_files_set(files);
+  await object_property_initialize_get_async(
+    global_get(),
+    global_files(),
+    () => {},
+  );
   let file_paths = object_properties(files);
   let mapped2 = list_map(file_paths, function_path_to_name);
   html_list(root, mapped2);
