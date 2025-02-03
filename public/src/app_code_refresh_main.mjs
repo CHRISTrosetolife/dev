ead48a63c743542fe81e9b19520835de5b0147ef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 17:29:55 -0500

    2025-02-03T22:29:54.432Z watch   app_code_refresh_main

--- public/src/app_code_refresh_main.mjs
+++ public/src/app_code_refresh_main.mjs
@@ -1,7 +1,10 @@
+import { html_style_background_color_transparent } from "./html_style_background_color_transparent.mjs";
 import { html_clear_scroll_top_centered_context } from "./html_clear_scroll_top_centered_context.mjs";
 import { app_code_refresh_functions } from "./app_code_refresh_functions.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+import { html_document_body } from "./html_document_body.mjs";
 export function app_code_refresh_main(context) {
+  html_style_background_color_transparent(html_document_body(), "black", 75);
   let root = html_clear_scroll_top_centered_context(context);
   html_button_width_full_text_click(root, "Functions", async () => {
     await app_code_refresh_functions(context);
