9502736fe757c3698afc169c73f6e6a825d1313b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 14:02:40 -0500

    2025-02-03T19:02:40.068Z watch   app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,8 +1,9 @@
+import { app_code_local_functions_path } from "./app_code_local_functions_path.mjs";
 import { http_storage_get } from "./http_storage_get.mjs";
 import { html_scripts_load } from "./html_scripts_load.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_code() {
   let root = html_style_default_initialize();
   await html_scripts_load(root, ["axios", "acorn", "astring"]);
-  await http_storage_get();
+  await http_storage_get(app_code_local_functions_path());
 }
