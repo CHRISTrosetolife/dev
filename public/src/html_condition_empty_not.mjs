bce4baea1824995d164d267c6fe464350e1c748f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 16:10:00 -0500

    2025-02-03T21:10:00.108Z watch   html_condition_empty_not

--- public/src/html_condition_empty_not.mjs
+++ public/src/html_condition_empty_not.mjs
@@ -1,7 +1,7 @@
 import { string_empty_not_is } from "./string_empty_not_is.mjs";
 export function html_condition_empty_not() {
   return {
-    message: (c) => "not be empty",
+    message: (value) => "not be empty",
     condition: string_empty_not_is,
   };
 }
