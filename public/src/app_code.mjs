4f731d8876c8a69d2da050c8acb1a1dec3c69bab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 15:13:44 -0500

    2025-02-03T20:13:42.232Z watch   app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,4 +1,4 @@
-import { storage_local_exists } from "./storage_local_exists.mjs";
+import { storage_local_exists_not } from "./storage_local_exists_not.mjs";
 import { app_code_refresh } from "./app_code_refresh.mjs";
 import { html_scripts_load } from "./html_scripts_load.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
@@ -8,7 +8,7 @@ export async function app_code() {
   let context = {
     root,
   };
-  if (storage_local_exists(app_code)) {
+  if (storage_local_exists_not(app_code)) {
   }
   app_code_refresh(context);
 }
