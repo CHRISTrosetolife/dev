ec18fa7af3457c46579500323309e8158044e4f0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 17:13:25 -0500

    2025-02-03T22:13:22.237Z watch   html_input_width_full_placeholder

--- public/src/html_input_width_full_placeholder.mjs
+++ public/src/html_input_width_full_placeholder.mjs
@@ -4,7 +4,7 @@ import { html_placeholder } from "./html_placeholder.mjs";
 import { html_input_width_full } from "./html_input_width_full.mjs";
 export function html_input_width_full_placeholder(parent, placeholder) {
   let title = html_div_text(parent, placeholder);
-  html_style_font_size(title, 2);
+  html_style_font_size(title, 3);
   let input = html_input_width_full(parent);
   html_placeholder(input, placeholder);
   return input;
