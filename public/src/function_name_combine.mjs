ab1038b7d3f30a317807fffb6df2c2954cc14b91
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 15:22:10 -0500

    2025-02-03T20:22:10.072Z watch   function_name_combine

--- public/src/function_name_combine.mjs
+++ public/src/function_name_combine.mjs
@@ -1,9 +1,5 @@
 import { function_name_separator } from "./function_name_separator.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-export function function_name_combine(function_name_base, j) {
-  return string_combine_multiple([
-    function_name_base,
-    function_name_separator(),
-    j,
-  ]);
+export function function_name_combine(left, right) {
+  return string_combine_multiple([left, function_name_separator(), right]);
 }
