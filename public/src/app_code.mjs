1862671edcab50ca5c59b671c6bc1d5acc760701
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 14:17:25 -0500

    2025-02-03T19:17:24.419Z watch   app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,9 +1,7 @@
+import { folder_path_src_prefixed } from "./folder_path_src_prefixed.mjs";
 import { function_path_suffix } from "./function_path_suffix.mjs";
 import { list_map_prefix_suffix_without } from "./list_map_prefix_suffix_without.mjs";
 import { html_list } from "./html_list.mjs";
-import { folder_path_src } from "./folder_path_src.mjs";
-import { folder_current_prefix } from "./folder_current_prefix.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { global_files_initialize } from "./global_files_initialize.mjs";
 import { app_code_local_functions_path } from "./app_code_local_functions_path.mjs";
 import { http_storage_get } from "./http_storage_get.mjs";
@@ -16,10 +14,7 @@ export async function app_code() {
   let files = await http_storage_get(app_code_local_functions_path());
   global_files_initialize(files);
   let file_paths = object_properties(files);
-  let prefix = string_combine_multiple([
-    folder_current_prefix(),
-    folder_path_src(),
-  ]);
+  let prefix = folder_path_src_prefixed();
   let suffix = function_path_suffix();
   let mapped2 = list_map_prefix_suffix_without(file_paths, prefix, suffix);
   html_list(root, mapped2);
