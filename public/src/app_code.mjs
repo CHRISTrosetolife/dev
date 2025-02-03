6abcb1ae5284a40d723c5ff0e44a91e878c4748c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 14:19:02 -0500

    2025-02-03T19:19:01.121Z watch   app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,7 +1,5 @@
 import { function_path_to_name } from "./function_path_to_name.mjs";
 import { list_map } from "./list_map.mjs";
-import { folder_path_src_prefixed } from "./folder_path_src_prefixed.mjs";
-import { function_path_suffix } from "./function_path_suffix.mjs";
 import { html_list } from "./html_list.mjs";
 import { global_files_initialize } from "./global_files_initialize.mjs";
 import { app_code_local_functions_path } from "./app_code_local_functions_path.mjs";
@@ -15,8 +13,6 @@ export async function app_code() {
   let files = await http_storage_get(app_code_local_functions_path());
   global_files_initialize(files);
   let file_paths = object_properties(files);
-  let prefix = folder_path_src_prefixed();
-  let suffix = function_path_suffix();
   let mapped2 = list_map(file_paths, function_path_to_name);
   html_list(root, mapped2);
 }
