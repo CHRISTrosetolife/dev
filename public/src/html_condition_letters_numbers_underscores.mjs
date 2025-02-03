f1977af1723435e37fb78478e91e9089efcc5778
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 16:11:55 -0500

    2025-02-03T21:11:54.756Z watch   html_condition_letters_numbers_underscores

--- public/src/html_condition_letters_numbers_underscores.mjs
+++ public/src/html_condition_letters_numbers_underscores.mjs
@@ -1,13 +1,17 @@
+import { list_unique } from "./list_unique.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_digit_is } from "./string_digit_is.mjs";
 import { string_underscore_is } from "./string_underscore_is.mjs";
 import { string_letter_is } from "./string_letter_is.mjs";
 import { string_all_or } from "./string_all_or.mjs";
+import { list_filter } from "./list_filter.mjs";
+import { string_split_empty } from "./string_split_empty.mjs";
 export function html_condition_letters_numbers_underscores() {
   return {
     message: (value) =>
       string_combine_multiple([
         "contain only letters, numbers or underscores; contains: ",
+        list_unique(list_filter(string_split_empty(value), valid_is)),
       ]),
     condition: function valid_is(u) {
       string_all_or(u, [
