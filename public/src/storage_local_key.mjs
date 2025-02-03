3e5973021e427af3dd0f5126bf746b568ced6c16
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 14:55:54 -0500

    2025-02-03T19:55:53.375Z watch   storage_local_key

--- public/src/storage_local_key.mjs
+++ public/src/storage_local_key.mjs
@@ -1,5 +1,7 @@
+import { assert } from "./assert.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 export function storage_local_key(fn_namespace, key) {
+  assert(function_is, [fn_namespace]);
   return list_join_space([object_property_get(fn_namespace, "name"), key]);
 }
