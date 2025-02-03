1424d3d94c2669729e8040c984d6b62884f95c69
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 14:02:28 -0500

    2025-02-03T19:02:27.489Z watch   app_code_local_upload

---
+++ public/src/app_code_local_functions_path.mjs
@@ -0,0 +1,3 @@
+export function app_code_local_functions_path() {
+  return "code/functions/local.json";
+}
--- public/src/app_code_local_upload.mjs
+++ public/src/app_code_local_upload.mjs
@@ -1,6 +1,7 @@
+import { app_code_local_functions_path } from "./app_code_local_functions_path.mjs";
 import { storage_upload_object } from "./storage_upload_object.mjs";
 import { functions_source_get } from "./functions_source_get.mjs";
 export async function app_code_local_upload() {
   let s = await functions_source_get();
-  await storage_upload_object("code/functions/local.json", s);
+  await storage_upload_object(app_code_local_functions_path(), s);
 }
