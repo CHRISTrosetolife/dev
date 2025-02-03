e14845d030a3495deb17e10754c225913527b0d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 14:08:59 -0500

    2025-02-03T19:08:58.046Z watch   app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -5,10 +5,12 @@ import { html_scripts_load } from "./html_scripts_load.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { json_to } from "./json_to.mjs";
+import { object_properties } from "./object_properties.mjs";
 export async function app_code() {
   let root = html_style_default_initialize();
   await html_scripts_load(root, ["axios", "acorn", "astring"]);
   let files = await http_storage_get(app_code_local_functions_path());
   global_files_initialize(files);
+  let file_paths = object_properties(files);
   html_p_text(root, json_to(files));
 }
