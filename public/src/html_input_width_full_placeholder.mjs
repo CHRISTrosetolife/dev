c4dd933d69f62dcb97af6ac9be3ac153ee8381de
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 17:12:19 -0500

    2025-02-03T22:12:14.851Z watch   html_input_width_full_placeholder

--- public/src/html_input_width_full_placeholder.mjs
+++ public/src/html_input_width_full_placeholder.mjs
@@ -1,8 +1,10 @@
+import { html_style_font_size } from "./html_style_font_size.mjs";
 import { html_div_text } from "./html_div_text.mjs";
 import { html_placeholder } from "./html_placeholder.mjs";
 import { html_input_width_full } from "./html_input_width_full.mjs";
 export function html_input_width_full_placeholder(parent, placeholder) {
   let title = html_div_text(parent, placeholder);
+  html_style_font_size(title, 1);
   let input = html_input_width_full(parent);
   html_placeholder(input, placeholder);
   return input;
