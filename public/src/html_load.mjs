cd703c731d44d3105e6f4b13a76e19462caa11ab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 19:45:45 -0500

    2025-02-04T00:45:44.860Z watch   html_load

--- public/src/html_load.mjs
+++ public/src/html_load.mjs
@@ -1,8 +1,8 @@
+import { global_function } from "./global_function.mjs";
 import { sleep_0 } from "./sleep_0.mjs";
 import { html_loader } from "./html_loader.mjs";
-import { global_get } from "./global_get.mjs";
 export async function html_load(lambda) {
-  global_get();
+  let g = global_function(html_load);
   let o = {};
   let overlay = html_loader();
   await sleep_0();
