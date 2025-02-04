d0ec6d4f7c7e372b18862cd861d0c0603aedef08
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 13:49:31 -0500

    2025-02-04T18:49:30.097Z watch   app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,3 +1,5 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { app_code_screen_prefix_get } from "./app_code_screen_prefix_get.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_code_screen_suffix_get } from "./app_code_screen_suffix_get.mjs";
 import { storage_local_initialize } from "./storage_local_initialize.mjs";
@@ -18,6 +20,8 @@ export async function app_code() {
     context,
     fn_name("app_code_refresh_main"),
   );
+  let prefix = app_code_screen_prefix_get(context);
   let screen = storage_local_initialize(app_fn, "screen", suffix);
+  string_combine_multiple([prefix, screen]);
   await function_run(f, [context]);
 }
