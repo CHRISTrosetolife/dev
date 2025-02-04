a71f41ac6f489379eb293ee5d317ae955670d225
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 13:29:42 -0500

    2025-02-04T18:29:41.642Z function_name_combine a b

--- public/src/function_name_combine.mjs
+++ public/src/function_name_combine.mjs
@@ -1,9 +1,8 @@
 import { function_name_separator } from "./function_name_separator.mjs";
 import { list_between } from "./list_between.mjs";
 import { list_join_empty } from "./list_join_empty.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function function_name_combine(left, right) {
   let b = list_between([left, right], function_name_separator());
   let joined = list_join_empty(b);
-  return string_combine_multiple(joined);
+  return joined;
 }
