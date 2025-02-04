ae1850899b1739fb5e5d4c1a66bf2ba8397f72d7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 15:01:52 -0500

    2025-02-04T20:01:49.128Z watch   app_code_refresh_function

--- public/src/app_code_refresh_function.mjs
+++ public/src/app_code_refresh_function.mjs
@@ -1,3 +1,4 @@
+import { html_element } from "./html_element.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { storage_local_get_context } from "./storage_local_get_context.mjs";
@@ -5,7 +6,7 @@ import { app_code_files_get } from "./app_code_files_get.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_code_screen_set } from "./app_code_screen_set.mjs";
 import { html_load } from "./html_load.mjs";
-import { html_p_text } from "./html_p_text.mjs";
+import { html_inner_set } from "./html_inner_set.mjs";
 export async function app_code_refresh_function(context) {
   await html_load(async () => {
     let root = app_code_screen_set(
@@ -19,6 +20,7 @@ export async function app_code_refresh_function(context) {
     );
     let p = function_name_to_path(function_selected);
     let contents = object_property_get(files, p);
-    html_p_text(root, contents);
+    html_element(root, "pre");
+    html_inner_set(contents);
   });
 }
