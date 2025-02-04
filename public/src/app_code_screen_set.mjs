16e271c081c494922e50c695a30f94d4823bf6e2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 13:46:41 -0500

    2025-02-04T18:46:40.307Z watch   app_code_screen_set

--- public/src/app_code_screen_set.mjs
+++ public/src/app_code_screen_set.mjs
@@ -1,10 +1,14 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { app_code_screen_suffix_get } from "./app_code_screen_suffix_get.mjs";
 import { html_clear_scroll_top_centered_context } from "./html_clear_scroll_top_centered_context.mjs";
-import { app_code } from "./app_code.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
 export function app_code_screen_set(context, this_fn_name) {
   let suffix = app_code_screen_suffix_get(context, this_fn_name);
-  storage_local_set(app_code, "screen", suffix);
+  storage_local_set(
+    object_property_get(object, "property_name"),
+    "screen",
+    suffix,
+  );
   let root = html_clear_scroll_top_centered_context(context);
   return root;
 }
