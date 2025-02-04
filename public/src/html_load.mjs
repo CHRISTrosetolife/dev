b33e7a3e542a5dfdd4ec2d413d68748eb173e159
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 19:46:27 -0500

    2025-02-04T00:46:26.595Z watch   html_load

--- public/src/html_load.mjs
+++ public/src/html_load.mjs
@@ -1,11 +1,11 @@
 import { set_new } from "./set_new.mjs";
-import { object_property_set } from "./object_property_set.mjs";
 import { global_function } from "./global_function.mjs";
 import { sleep_0 } from "./sleep_0.mjs";
 import { html_loader } from "./html_loader.mjs";
+import { object_property_initialize_get } from "./object_property_initialize_get.mjs";
 export async function html_load(lambda) {
   let g = global_function(html_load);
-  object_property_set(g, "set", set_new());
+  let s = object_property_initialize_get(g, "set", () => set_new());
   let o = {};
   let overlay = html_loader();
   await sleep_0();
