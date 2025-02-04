48d04e5641aa446a3e9fb2b54e400258b1474e41
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 13:27:07 -0500

    2025-02-04T18:27:05.344Z watch   function_name_combine

--- public/src/function_name_combine.mjs
+++ public/src/function_name_combine.mjs
@@ -1,5 +1,7 @@
 import { function_name_separator } from "./function_name_separator.mjs";
+import { list_join } from "./list_join.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function function_name_combine(left, right) {
+  let joined = list_join([left, right], function_name_separator());
   return string_combine_multiple([left, function_name_separator(), right]);
 }
