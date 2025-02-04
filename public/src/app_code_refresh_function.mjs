cf4a887c3e197b4d4bd5a3e500ce2469cb600b46
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 14:50:58 -0500

    2025-02-04T19:50:57.513Z watch   app_code_refresh_function

--- public/src/app_code_refresh_function.mjs
+++ public/src/app_code_refresh_function.mjs
@@ -1,3 +1,4 @@
+import { app_code_files_get } from "./app_code_files_get.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_code_screen_set } from "./app_code_screen_set.mjs";
 import { html_load } from "./html_load.mjs";
@@ -7,5 +8,6 @@ export async function app_code_refresh_function() {
       context,
       fn_name("app_code_refresh_functions"),
     );
+    let files = await app_code_files_get();
   });
 }
