124e52bf91d6ca789f1de8f226a9690b8e225c2a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 19:51:12 -0500

    2025-02-04T00:51:12.337Z watch   html_load

--- public/src/html_load.mjs
+++ public/src/html_load.mjs
@@ -1,3 +1,5 @@
+import { html_remove } from "./html_remove.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 import { set_remove } from "./set_remove.mjs";
 import { set_add } from "./set_add.mjs";
 import { object_property_set } from "./object_property_set.mjs";
@@ -19,4 +21,8 @@ export async function html_load(lambda) {
   set_add(o);
   await lambda();
   set_remove(o);
+  if (set_empty_is(s)) {
+    let overlay = object_property_get(g, "overlay");
+    html_remove(overlay);
+  }
 }
