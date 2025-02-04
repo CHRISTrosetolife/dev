639a2ebdede1a5f56b09f5ccd3d445a3c95fcd47
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 19:47:41 -0500

    2025-02-04T00:47:40.337Z watch   set_size

--- public/src/set_size.mjs
+++ public/src/set_size.mjs
@@ -1,6 +1,6 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
-export function set_size(list) {
+export function set_size(set) {
   assert_arguments_length(arguments, 1);
-  return object_property_get(object, "property_name");
+  return object_property_get(set, "size");
 }
