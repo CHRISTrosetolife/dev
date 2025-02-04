cb2cf03cb78ca8c8b9396256f8c0cec840a0a025
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 13:51:40 -0500

    2025-02-04T18:51:33.850Z watch   app_code_refresh_main

--- public/src/app_code_refresh_main.mjs
+++ public/src/app_code_refresh_main.mjs
@@ -1,10 +1,7 @@
-import { app_code_screen_set } from "./app_code_screen_set.mjs";
-import { fn_name } from "./fn_name.mjs";
 import { app_code_username_exists } from "./app_code_username_exists.mjs";
 import { app_code_refresh_menu } from "./app_code_refresh_menu.mjs";
 import { app_code_refresh_username } from "./app_code_refresh_username.mjs";
 export function app_code_refresh_main(context) {
-  app_code_screen_set(context, fn_name("app_code_refresh_main"));
   if (app_code_username_exists()) {
     app_code_refresh_menu(context);
   } else {
