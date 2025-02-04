7203da84e082111bdec63f36c8d19640674fd746
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 11:50:21 -0500

    2025-02-04T16:50:20.738Z watch   html_input_validated

--- public/src/html_input_validated.mjs
+++ public/src/html_input_validated.mjs
@@ -5,12 +5,12 @@ import { html_div } from "./html_div.mjs";
 import { html_input_width_full_placeholder } from "./html_input_width_full_placeholder.mjs";
 export function html_input_validated(root, placeholder) {
   let input_username = html_input_width_full_placeholder(root, placeholder);
-  let p_error_message = html_div(root);
-  html_style_rounded_padded(p_error_message);
+  let error_message = html_div(root);
+  html_style_rounded_padded(error_message);
   let error_color = html_input_validated_error_color();
-  html_style_font_color(p_error_message, error_color);
+  html_style_font_color(error_message, error_color);
   return {
-    p_error_message,
+    error_message,
     error_color,
     input_username,
   };
