24836612b56eada718f9ea3318ffd360846cfa18
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 14:13:12 -0500

    2025-02-04T19:13:11.719Z watch   html_condition_letters_numbers_underscores_spaces

--- public/src/html_condition_letters_numbers_underscores_spaces.mjs
+++ public/src/html_condition_letters_numbers_underscores_spaces.mjs
@@ -1,8 +1,11 @@
 import { html_condition_letters_numbers_underscores_conditions } from "./html_condition_letters_numbers_underscores_conditions.mjs";
 import { html_condition_string } from "./html_condition_string.mjs";
+import { list_concat_multiple } from "./list_concat_multiple.mjs";
 export function html_condition_letters_numbers_underscores_spaces() {
   return html_condition_string(
     "letters, numbers or underscores",
-    html_condition_letters_numbers_underscores_conditions(),
+    list_concat_multiple([
+      html_condition_letters_numbers_underscores_conditions(),
+    ]),
   );
 }
