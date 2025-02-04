491493ebc5c3ba8d11b55ca2a42245a418295503
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 13:45:03 -0500

    2025-02-04T18:44:59.650Z watch   app_code_screen_set

--- public/src/app_code_screen_set.mjs
+++ public/src/app_code_screen_set.mjs
@@ -1,16 +1,9 @@
+import { app_code_screen_suffix_get } from "./app_code_screen_suffix_get.mjs";
 import { html_clear_scroll_top_centered_context } from "./html_clear_scroll_top_centered_context.mjs";
 import { app_code } from "./app_code.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
-import { string_prefix_without } from "./string_prefix_without.mjs";
-import { fn_name } from "./fn_name.mjs";
-import { function_name_combine_multiple } from "./function_name_combine_multiple.mjs";
 export function app_code_screen_set(context, this_fn_name) {
-  let prefix = function_name_combine_multiple([
-    fn_name("app_code"),
-    "refresh",
-    "",
-  ]);
-  let suffix = string_prefix_without(this_fn_name, prefix);
+  let suffix = app_code_screen_suffix_get(this_fn_name);
   storage_local_set(app_code, "screen", suffix);
   let root = html_clear_scroll_top_centered_context(context);
   return root;
---
+++ public/src/app_code_screen_suffix_get.mjs
@@ -0,0 +1,12 @@
+import { string_prefix_without } from "./string_prefix_without.mjs";
+import { fn_name } from "./fn_name.mjs";
+import { function_name_combine_multiple } from "./function_name_combine_multiple.mjs";
+export function app_code_screen_suffix_get(this_fn_name) {
+  let prefix = function_name_combine_multiple([
+    fn_name("app_code"),
+    "refresh",
+    "",
+  ]);
+  let suffix = string_prefix_without(this_fn_name, prefix);
+  return suffix;
+}
