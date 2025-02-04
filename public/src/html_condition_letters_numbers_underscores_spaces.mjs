ba0b673da0fc31953c62baac1c30bad94719f623
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 14:14:08 -0500

    2025-02-04T19:14:08.065Z watch   html_condition_letters_numbers_underscores_spaces

--- public/src/html_condition_letters_numbers_underscores_spaces.mjs
+++ public/src/html_condition_letters_numbers_underscores_spaces.mjs
@@ -4,7 +4,7 @@ import { html_condition_string } from "./html_condition_string.mjs";
 import { list_concat_multiple } from "./list_concat_multiple.mjs";
 export function html_condition_letters_numbers_underscores_spaces() {
   return html_condition_string(
-    "letters, numbers or underscores",
+    "letters, underscores, numbers or spaces",
     list_concat_multiple([
       html_condition_letters_numbers_underscores_conditions(),
       [string_space_is],
