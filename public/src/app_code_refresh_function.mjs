d877ca86538d8152805e325f7c87152f972c47a1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 14:53:33 -0500

    2025-02-04T19:53:33.010Z watch   app_code_refresh_function

--- public/src/app_code_refresh_function.mjs
+++ public/src/app_code_refresh_function.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { app_code_files_get } from "./app_code_files_get.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_code_screen_set } from "./app_code_screen_set.mjs";
@@ -9,5 +10,6 @@ export async function app_code_refresh_function() {
       fn_name("app_code_refresh_function"),
     );
     let files = await app_code_files_get();
+    let app_fn = object_property_get(context, "app_fn");
   });
 }
