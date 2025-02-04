1cd7288e620c15d130a50c7f03868cfe95a3ced0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 12:23:00 -0500

    2025-02-04T17:22:59.675Z watch   html_input_validated

--- public/src/html_input_validated.mjs
+++ public/src/html_input_validated.mjs
@@ -20,6 +20,7 @@ import { html_input_validated_error_color } from "./html_input_validated_error_c
 import { html_style_rounded_padded } from "./html_style_rounded_padded.mjs";
 import { html_div } from "./html_div.mjs";
 import { html_input_width_full_placeholder } from "./html_input_width_full_placeholder.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
 export function html_input_validated(root, placeholder) {
   let input_username = html_input_width_full_placeholder(root, placeholder);
   let error_message = html_div(root);
@@ -62,7 +63,7 @@ export function html_input_validated(root, placeholder) {
       valid ? "green" : html_input_validated_error_color(),
     );
     html_style(input_username, border_color);
-    if (false) {
+    if (object_property_exists(button_save, "on_input")) {
       html_button_enable_if(button_save, valid);
     }
   });
