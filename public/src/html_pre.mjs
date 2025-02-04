db0463d820340659454afda5fbadf2683ccb0597
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 15:02:57 -0500

    2025-02-04T20:02:37.753Z watch   html_pre_text

---
+++ public/src/html_pre.mjs
@@ -0,0 +1,4 @@
+import { html_element } from "./html_element.mjs";
+export function html_pre(root) {
+  return html_element(root, "pre");
+}
--- public/src/html_pre_text.mjs
+++ public/src/html_pre_text.mjs
@@ -1,6 +1,6 @@
+import { html_pre } from "./html_pre.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
-import { html_element } from "./html_element.mjs";
 export function html_pre_text(root, contents) {
-  let e = html_element(root, "pre");
+  let e = html_pre(root);
   html_inner_set(e, contents);
 }
