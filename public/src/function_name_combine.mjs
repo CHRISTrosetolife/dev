3c98d722f6986e11abe4bfd11cf50c666ddb78c9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 13:32:44 -0500

    2025-02-04T18:32:41.250Z watch   function_name_combine

--- public/src/function_name_combine.mjs
+++ public/src/function_name_combine.mjs
@@ -1,6 +1,7 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { function_name_combine_multiple } from "./function_name_combine_multiple.mjs";
 export function function_name_combine(left, right) {
-  $aa
+  assert_arguments_length(arguments, 2);
   let terms = [left, right];
   return function_name_combine_multiple(terms);
 }
