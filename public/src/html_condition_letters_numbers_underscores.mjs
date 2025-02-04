6ad9d334a58f021a02800b64b6714fba10963bbd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 14:10:36 -0500

    2025-02-04T19:10:36.129Z watch   html_condition_letters_numbers_underscores

--- public/src/html_condition_letters_numbers_underscores.mjs
+++ public/src/html_condition_letters_numbers_underscores.mjs
@@ -8,13 +8,15 @@ import { string_all_or } from "./string_all_or.mjs";
 import { string_split_empty } from "./string_split_empty.mjs";
 export function html_condition_letters_numbers_underscores() {
   let requirements = [string_letter_is, string_underscore_is, string_digit_is];
+  let message = "letters, numbers or underscores";
   return {
     message: (value) => {
       let value_as_list = string_split_empty(value);
       let characters_invalid = list_filter_not(value_as_list, valid_is);
       let unique = list_unique(characters_invalid);
       return string_combine_multiple([
-        "contain only letters, numbers or underscores; invalid character(s): ",
+        "contain only ",
+        "; invalid character(s): ",
         unique,
       ]);
     },
