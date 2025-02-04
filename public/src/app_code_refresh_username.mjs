8437aca3e566b52bf0e0070d9261c429379824b1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 13:35:37 -0500

    2025-02-04T18:35:34.731Z watch   app_code_refresh_username

--- public/src/app_code_refresh_username.mjs
+++ public/src/app_code_refresh_username.mjs
@@ -11,11 +11,9 @@ import { storage_local_set } from "./storage_local_set.mjs";
 import { html_hr } from "./html_hr.mjs";
 import { html_value_get } from "./html_value_get.mjs";
 import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
-import { html_clear_scroll_top_centered_context } from "./html_clear_scroll_top_centered_context.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function app_code_refresh_username(context) {
-  app_code_screen_set(fn_name("app_code_refresh_username"));
-  let root = html_clear_scroll_top_centered_context(context);
+  let root = app_code_screen_set(context, fn_name("app_code_refresh_username"));
   html_p_text_multiple(root, [
     "In order to store data for later, you need a username",
     "If you do not want to store data for later, you may skip this",
