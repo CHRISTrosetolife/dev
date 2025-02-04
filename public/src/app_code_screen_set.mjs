a521d644f42c7f139b3df4b66380b333edac67f3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 13:35:41 -0500

    2025-02-04T18:35:39.712Z watch   app_code_screen_set

--- public/src/app_code_screen_set.mjs
+++ public/src/app_code_screen_set.mjs
@@ -4,7 +4,7 @@ import { storage_local_set } from "./storage_local_set.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { function_name_combine_multiple } from "./function_name_combine_multiple.mjs";
-export function app_code_screen_set(context,this_fn_name) {
+export function app_code_screen_set(context, this_fn_name) {
   let prefix = function_name_combine_multiple([
     fn_name("app_code"),
     "refresh",
