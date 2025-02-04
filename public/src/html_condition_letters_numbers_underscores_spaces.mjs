77ea684e20136229e3989167ae089f19a13667ef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 14:12:58 -0500

    2025-02-04T19:12:56.844Z c html_condition_letters_numbers_underscores html_condition_letters_numbers_underscores_spaces

---
+++ public/src/html_condition_letters_numbers_underscores_spaces.mjs
@@ -0,0 +1,8 @@
+import { html_condition_letters_numbers_underscores_conditions } from "./html_condition_letters_numbers_underscores_conditions.mjs";
+import { html_condition_string } from "./html_condition_string.mjs";
+export function html_condition_letters_numbers_underscores_spaces() {
+  return html_condition_string(
+    "letters, numbers or underscores",
+    html_condition_letters_numbers_underscores_conditions(),
+  );
+}
