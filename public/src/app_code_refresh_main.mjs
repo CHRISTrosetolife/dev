073b4f84c8ae6e3bfab90999656bba71ddd7ff07
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 12:31:16 -0500

    2025-02-04T17:31:15.659Z watch   app_code_refresh_main

--- public/src/app_code_refresh_main.mjs
+++ public/src/app_code_refresh_main.mjs
@@ -1,3 +1,5 @@
+import { app_code } from "./app_code.mjs";
+import { storage_local_get } from "./storage_local_get.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_clear_scroll_top_centered_context } from "./html_clear_scroll_top_centered_context.mjs";
 import { app_code_refresh_functions } from "./app_code_refresh_functions.mjs";
@@ -9,7 +11,10 @@ export function app_code_refresh_main(context) {
   });
   html_button_width_full_text_click(
     root,
-    string_combine_multiple(["Log out "]),
+    string_combine_multiple([
+      "Log out ",
+      storage_local_get(app_code, "username"),
+    ]),
     async () => {
       await app_code_refresh_functions(context);
     },
