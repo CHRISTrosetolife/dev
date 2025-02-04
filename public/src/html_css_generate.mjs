41981a36e3daa0059338ec0183644cb6875d74a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 19:38:25 -0500

    2025-02-04T00:38:23.834Z watch   html_css_generate

--- public/src/html_css_generate.mjs
+++ public/src/html_css_generate.mjs
@@ -1,5 +1,5 @@
 import { html_code_generate_generic } from "./html_code_generate_generic.mjs";
-export async function html_css_generate(name, html_css_code) {
+export async function html_css_generate(name, input) {
   return await html_code_generate_generic(
     "link",
     input,
