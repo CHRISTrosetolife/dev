bed90b4f59ce0dbbe150707ed7ede138cfa9a25a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 13:29:15 -0500

    2025-02-04T18:29:14.583Z watch   list_between

--- public/src/list_between.mjs
+++ public/src/list_between.mjs
@@ -1,11 +1,11 @@
 import { each_index_only_reverse } from "./each_index_only_reverse.mjs";
 import { list_insert } from "./list_insert.mjs";
-export function list_between(list, b) {
+export function list_between(list, item) {
   each_index_only_reverse(list, (index) => {
     if (index === 0) {
       return;
     }
-    list_insert(list, index, b);
+    list_insert(list, index, item);
   });
   return list;
 }
