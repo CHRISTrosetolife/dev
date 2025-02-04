3940e8ba242feaf6b831d1c42b517cbfe6254651
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 19:52:28 -0500

    2025-02-04T00:52:26.373Z watch   object_property_initialize_get

--- public/src/object_property_initialize_get.mjs
+++ public/src/object_property_initialize_get.mjs
@@ -1,9 +1,9 @@
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
-export function object_property_initialize_get(lookup, key, initial) {
+export function object_property_initialize_get(lookup, key, initial_get) {
   if (object_property_exists_not(lookup, key)) {
-    object_property_set(lookup, key, initial());
+    object_property_set(lookup, key, initial_get());
   }
   return object_property_get(lookup, key);
 }
