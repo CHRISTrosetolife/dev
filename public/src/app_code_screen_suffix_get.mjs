794e1490fe0cc51b5044ba78d1277f2d13522be9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 13:47:37 -0500

    2025-02-04T18:47:35.932Z watch   app_code_screen_suffix_get

--- public/src/app_code_screen_suffix_get.mjs
+++ public/src/app_code_screen_suffix_get.mjs
@@ -1,10 +1,10 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
-import { fn_name } from "./fn_name.mjs";
 import { function_name_combine_multiple } from "./function_name_combine_multiple.mjs";
 export function app_code_screen_suffix_get(context, this_fn_name) {
-    const app_fn = object_property_get(context, "app_fn");
+  let app_fn = object_property_get(context, "app_fn");
   let prefix = function_name_combine_multiple([
-    $g,
+    object_property_get(object, "property_name"),
     "refresh",
     "",
   ]);
