90840587d07c8e9d0e262033f6496178f5c4c799
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 12:29:11 -0500

    2025-02-04T17:29:10.669Z watch   object_property_set_exists_not

--- public/src/object_property_set_exists_not.mjs
+++ public/src/object_property_set_exists_not.mjs
@@ -1,11 +1,7 @@
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { assert } from "./assert.mjs";
-export function object_property_set_exists_not(
-  property_name,
-  input_username,
-  value,
-) {
+export function object_property_set_exists_not(property_name, object, value) {
   assert(object_property_exists_not, property_name);
-  object_property_set(input_username, property_name, value);
+  object_property_set(object, property_name, value);
 }
