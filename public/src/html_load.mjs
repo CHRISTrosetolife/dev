5217e3fac409ed324ba2dbcd19ebbc987e753ed8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 19:44:30 -0500

    2025-02-04T00:44:29.538Z watch   html_load

--- public/src/html_load.mjs
+++ public/src/html_load.mjs
@@ -1,1 +1,7 @@
-export function html_load() {}
+import { sleep_0 } from "./sleep_0.mjs";
+import { html_loader } from "./html_loader.mjs";
+export async function html_load(lambda) {
+  let o = {};
+  let overlay = html_loader();
+  await sleep_0();
+}
