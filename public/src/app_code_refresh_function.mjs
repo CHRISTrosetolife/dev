44bdfebe55656b3886ed235165adc8634d3f6c7e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 15:02:19 -0500

    2025-02-04T20:02:17.996Z watch   html_pre_text

--- public/src/app_code_refresh_function.mjs
+++ public/src/app_code_refresh_function.mjs
@@ -1,4 +1,4 @@
-import { html_element } from "./html_element.mjs";
+import { html_pre_text } from "./html_pre_text.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { storage_local_get_context } from "./storage_local_get_context.mjs";
@@ -6,7 +6,6 @@ import { app_code_files_get } from "./app_code_files_get.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_code_screen_set } from "./app_code_screen_set.mjs";
 import { html_load } from "./html_load.mjs";
-import { html_inner_set } from "./html_inner_set.mjs";
 export async function app_code_refresh_function(context) {
   await html_load(async () => {
     let root = app_code_screen_set(
@@ -20,7 +19,6 @@ export async function app_code_refresh_function(context) {
     );
     let p = function_name_to_path(function_selected);
     let contents = object_property_get(files, p);
-    let e = html_element(root, "pre");
-    html_inner_set(e, contents);
+    html_pre_text(root, contents);
   });
 }
