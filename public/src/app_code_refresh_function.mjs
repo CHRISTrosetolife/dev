27264612146184d73604742a207bb104a1b44b4b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 14:54:21 -0500

    2025-02-04T19:54:20.489Z watch   app_code_refresh_function

--- public/src/app_code_refresh_function.mjs
+++ public/src/app_code_refresh_function.mjs
@@ -1,5 +1,4 @@
-import { storage_local_get } from "./storage_local_get.mjs";
-import { object_property_get } from "./object_property_get.mjs";
+import { storage_local_get_context } from "./storage_local_get_context.mjs";
 import { app_code_files_get } from "./app_code_files_get.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_code_screen_set } from "./app_code_screen_set.mjs";
@@ -11,7 +10,7 @@ export async function app_code_refresh_function() {
       fn_name("app_code_refresh_function"),
     );
     let files = await app_code_files_get();
-    let app_fn = object_property_get(context, "app_fn");
-    storage_local_get(app_fn, "function_selected", value);
+    let property_name = "function_selected";
+    storage_local_get_context(property_name);
   });
 }
