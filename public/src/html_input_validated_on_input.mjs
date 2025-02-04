4e23a57f1df7e703c5bd57612a409f52227b0a95
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 12:50:29 -0500

    2025-02-04T17:50:28.411Z watch   html_input_validated_on_input

--- public/src/html_input_validated_on_input.mjs
+++ public/src/html_input_validated_on_input.mjs
@@ -1,3 +1,4 @@
-export function html_input_validated_on_input() {
-  return "on_input";
+import { object_property_get } from "./object_property_get.mjs";
+export function html_input_validated_on_input(input_username) {
+  object_property_get(input_username, "on_input")();
 }
