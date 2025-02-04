cc9da9b45052f3cf7a2932bb19fe319f4f712d0c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 19:41:58 -0500

    2025-02-04T00:41:57.642Z watch   app_code_refresh_main

--- public/src/app_code_refresh_main.mjs
+++ public/src/app_code_refresh_main.mjs
@@ -3,7 +3,7 @@ import { html_clear_scroll_top_centered_context } from "./html_clear_scroll_top_
 import { app_code_refresh_functions } from "./app_code_refresh_functions.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function app_code_refresh_main(context) {
-  html_loader();
+  let overlay = html_loader();
   return;
   let root = html_clear_scroll_top_centered_context(context);
   html_button_width_full_text_click(root, "Functions", async () => {
