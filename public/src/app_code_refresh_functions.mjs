3859fa3097bcb158c2574e1df2dc158ea5e08a20
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 13:17:49 -0500

    2025-02-04T18:17:48.549Z watch   app_code_refresh_functions

--- public/src/app_code_refresh_functions.mjs
+++ public/src/app_code_refresh_functions.mjs
@@ -1,3 +1,4 @@
+import { each_range } from "./each_range.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { html_button_width_full_click } from "./html_button_width_full_click.mjs";
 import { app_code_refresh_menu } from "./app_code_refresh_menu.mjs";
@@ -91,6 +92,7 @@ export async function app_code_refresh_functions(context) {
           let index = 0;
           each(v_list, (vi) => {
             let m;
+            each_range(count, (item) => {});
             while (true) {
               m = list_get(mapped, index);
               if (object_property_get(m, "symbol") === vi) {
