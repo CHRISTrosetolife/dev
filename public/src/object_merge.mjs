2c8e3a484511a32afb21a006d9df3a4d7f511074
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 11:56:17 -0500

    2025-02-04T16:56:14.447Z pb object_merge_generic

--- public/src/object_merge.mjs
+++ public/src/object_merge.mjs
@@ -2,5 +2,5 @@ import { object_merge_generic } from "./object_merge_generic.mjs";
 import { object_merge_properties } from "./object_merge_properties.mjs";
 export function object_merge(to, from) {
   let merge_fn = object_merge_properties;
-  return object_merge_generic(from, merge_fn, to);
+  return object_merge_generic(merge_fn, from, to);
 }
--- public/src/object_merge_generic.mjs
+++ public/src/object_merge_generic.mjs
@@ -1,5 +1,5 @@
 import { object_properties } from "./object_properties.mjs";
-export function object_merge_generic(from, merge_fn, to) {
+export function object_merge_generic(merge_fn, from, to) {
   let from_properties = object_properties(from);
   merge_fn(to, from, from_properties);
   return to;
