4d8938e74eb9254d6c042a3a0e2a8d418924f862
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 11:56:52 -0500

    2025-02-04T16:56:52.065Z watch   object_merge_strict

--- public/src/object_merge_strict.mjs
+++ public/src/object_merge_strict.mjs
@@ -1,6 +1,6 @@
+import { object_merge_properties_strict } from "./object_merge_properties_strict.mjs";
 import { object_merge_generic } from "./object_merge_generic.mjs";
-import { object_merge_properties } from "./object_merge_properties.mjs";
 export function object_merge_strict(to, from) {
-  let merge_fn = object_merge_properties;
+  let merge_fn = object_merge_properties_strict;
   return object_merge_generic(merge_fn, from, to);
 }
