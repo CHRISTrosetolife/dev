d9fb1d262ed2ab275be725cad8b96c7853b493d3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 12:49:02 -0500

    2025-02-04T17:49:00.212Z watch   html_input_validated_on_input_lambda

--- public/src/html_input_validated_on_input_lambda.mjs
+++ public/src/html_input_validated_on_input_lambda.mjs
@@ -1,3 +1,4 @@
-export function html_input_validated_on_input_lambda() {
-  return "on_input_lambda";
+import { object_property_set } from "./object_property_set.mjs";
+export function html_input_validated_on_input_lambda(input_username, lambda) {
+  object_property_set(input_username, "on_input_lambda", lambda);
 }
