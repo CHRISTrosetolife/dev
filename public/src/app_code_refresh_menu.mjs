e0264d0c9406f40ad3e4c25ec927221419e5ae15
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 13:23:29 -0500

    2025-02-04T18:23:28.425Z watch   app_code_refresh_menu

--- public/src/app_code_refresh_menu.mjs
+++ public/src/app_code_refresh_menu.mjs
@@ -7,7 +7,9 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_clear_scroll_top_centered_context } from "./html_clear_scroll_top_centered_context.mjs";
 import { app_code_refresh_functions } from "./app_code_refresh_functions.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
+import { storage_local_set } from "./storage_local_set.mjs";
 export function app_code_refresh_menu(context) {
+  storage_local_set(app_code, "screen", suffix);
   let root = html_clear_scroll_top_centered_context(context);
   html_button_width_full_text_click(root, "Functions", async () => {
     await app_code_refresh_functions(context);
