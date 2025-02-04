8285b8705e5fd94954f0b65b37f20ec84a4934a6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 11:51:28 -0500

    2025-02-04T16:51:27.963Z watch   html_input_validated

--- public/src/html_input_validated.mjs
+++ public/src/html_input_validated.mjs
@@ -3,15 +3,17 @@ import { html_input_validated_error_color } from "./html_input_validated_error_c
 import { html_style_rounded_padded } from "./html_style_rounded_padded.mjs";
 import { html_div } from "./html_div.mjs";
 import { html_input_width_full_placeholder } from "./html_input_width_full_placeholder.mjs";
+import { object_merge } from "./object_merge.mjs";
 export function html_input_validated(root, placeholder) {
   let input_username = html_input_width_full_placeholder(root, placeholder);
   let error_message = html_div(root);
   html_style_rounded_padded(error_message);
   let error_color = html_input_validated_error_color();
   html_style_font_color(error_message, error_color);
-  return {
-    error_message,
-    error_color,
+  return object_merge(
+    {
+      error_message,
+    },
     input_username,
-  };
+  );
 }
