e1abe5c4416b82225342f57626a66d45f0e9f077
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 11:41:29 -0500

    2025-02-04T16:41:26.329Z watch   list_between_surround

--- public/src/list_between_surround.mjs
+++ public/src/list_between_surround.mjs
@@ -1,5 +1,5 @@
 import { list_between_after } from "./list_between_after.mjs";
 import { list_concat } from "./list_concat.mjs";
-export function list_between_surround(list, b) {
-  return list_concat([b], list_between_after(list, b));
+export function list_between_surround(list, item) {
+  return list_concat([item], list_between_after(list, item));
 }
