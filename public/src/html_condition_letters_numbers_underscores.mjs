ae5501a5f03985eba586332acd9f8aec7810beae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 14:11:22 -0500

    2025-02-04T19:11:20.859Z watch   html_condition_letters_numbers_underscores

--- public/src/html_condition_letters_numbers_underscores.mjs
+++ public/src/html_condition_letters_numbers_underscores.mjs
@@ -3,8 +3,7 @@ import { string_digit_is } from "./string_digit_is.mjs";
 import { string_underscore_is } from "./string_underscore_is.mjs";
 import { string_letter_is } from "./string_letter_is.mjs";
 export function html_condition_letters_numbers_underscores() {
-  let message = "letters, numbers or underscores";
-  return html_condition_string(message, [
+  return html_condition_string("letters, numbers or underscores", [
     string_letter_is,
     string_underscore_is,
     string_digit_is,
