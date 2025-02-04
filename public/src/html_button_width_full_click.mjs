e6d536d2f2062cb545c46846398684849beb9354
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 13:59:52 -0500

    2025-02-04T18:59:50.478Z watch   html_button_width_full_click

--- public/src/html_button_width_full_click.mjs
+++ public/src/html_button_width_full_click.mjs
@@ -1,4 +1,4 @@
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function html_button_width_full_click(results, lambda) {
-  html_button_width_full_text_click(results, "", lambda);
+  return html_button_width_full_text_click(results, "", lambda);
 }
