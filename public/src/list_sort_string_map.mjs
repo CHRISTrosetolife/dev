6da3f38a240a5dbf25a1b92afd4991a829d3662f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 11:11:24 -0500

    2025-02-04T16:11:22.887Z watch   app_language_2_answers

--- public/src/list_sort_string_map.mjs
+++ public/src/list_sort_string_map.mjs
@@ -1,5 +1,5 @@
 export function list_sort_string_map(list, element_mapper) {
-  var collator = new Intl.Collator([], {
+  let collator = new Intl.Collator([], {
     numeric: true,
   });
   list.sort((a, b) => collator.compare(element_mapper(a), element_mapper(b)));
