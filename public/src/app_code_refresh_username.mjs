aec4954afd5c04ccf34d7eb9c4bd1f11e75456b0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 14:18:47 -0500

    2025-02-04T19:18:45.203Z watch   app_code_refresh_username

--- public/src/app_code_refresh_username.mjs
+++ public/src/app_code_refresh_username.mjs
@@ -28,12 +28,11 @@ export function app_code_refresh_username(context) {
     let value = html_value_get(input_username);
     storage_local_set(app_code, "username", value);
     next();
-  });$ag
-  let lambda = (valid) => {
+  });
+  html_input_validated_on_input_lambda_initial(input_username, (valid) => {
     log({});
     html_button_enable_if(button_save, valid);
-  };
-  html_input_validated_on_input_lambda_initial(input_username, lambda);
+  });
   html_hr(root);
   html_button_width_full_text_click(root, "Skip", () => {
     next();
