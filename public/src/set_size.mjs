2bcf2fbbd7aca509760bfab94679f724d2ad9af5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 19:47:18 -0500

    2025-02-04T00:47:17.756Z c list_size set_size

---
+++ public/src/set_size.mjs
@@ -0,0 +1,8 @@
+import { assert } from "./assert.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
+import { list_is } from "./list_is.mjs";
+export function set_size(list) {
+  assert_arguments_length(arguments, 1);
+  assert(list_is, [list]);
+  return list.length;
+}
