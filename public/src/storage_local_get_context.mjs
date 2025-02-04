001c71d8053761709bfc8e483e13fbb3f159dd02
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 14:55:22 -0500

    2025-02-04T19:55:21.021Z watch   storage_local_get_context

--- public/src/storage_local_get_context.mjs
+++ public/src/storage_local_get_context.mjs
@@ -2,5 +2,5 @@ import { storage_local_get } from "./storage_local_get.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function storage_local_get_context(context, property_name) {
   let app_fn = object_property_get(context, "app_fn");
-  storage_local_get(app_fn, property_name, value);
+  return storage_local_get(app_fn, property_name);
 }
