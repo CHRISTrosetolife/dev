cf8303636d395e86959a8942c8555f6731aa6325
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-03 19:58:26 -0500

    2025-02-04T00:58:24.937Z watch   app_code

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,17 +1,19 @@
+import { html_div } from "./html_div.mjs";
 import { app_code_refresh_main } from "./app_code_refresh_main.mjs";
 import { app_code_refresh_username } from "./app_code_refresh_username.mjs";
 import { storage_local_exists_not } from "./storage_local_exists_not.mjs";
 import { html_scripts_load } from "./html_scripts_load.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_code() {
-  let root = html_style_default_initialize();
-  await html_scripts_load(root, ["axios", "acorn", "astring"]);
+  let body = html_style_default_initialize();
+  let root = html_div(body);
+  await html_scripts_load(body, ["axios", "acorn", "astring"]);
   let context = {
     root,
   };
   if (storage_local_exists_not(app_code, "username")) {
     app_code_refresh_username(context);
   } else {
-    app_code_refresh_main(context);
+    await app_code_refresh_main(context);
   }
 }
