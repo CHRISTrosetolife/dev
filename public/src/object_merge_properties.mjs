d716cadc5c74b9dc7c16e2a664d377c3046eb5a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-04 11:53:24 -0500

    2025-02-04T16:53:23.473Z watch   object_merge_properties

--- public/src/object_merge_properties.mjs
+++ public/src/object_merge_properties.mjs
@@ -5,10 +5,11 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { assert } from "./assert.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
 export function object_merge_properties(to, from, properties) {
+  let strict = false;
   assert_arguments_length(arguments, 3);
   each(properties, (p) => {
     let value = object_property_get(from, p);
-    if (false) {
+    if (strict) {
       assert(object_property_exists_not, [from, p]);
     }
     object_property_set(to, p, value);
