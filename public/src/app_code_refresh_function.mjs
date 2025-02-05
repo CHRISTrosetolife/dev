2e544be363534168cf37176721ddd4a90fa03fe7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-05 18:58:39 -0500

    2025-02-05T23:58:38.549Z watch   app_code_refresh_function

--- public/src/app_code_refresh_function.mjs
+++ public/src/app_code_refresh_function.mjs
@@ -30,6 +30,7 @@ export async function app_code_refresh_function(context) {
       let type = object_property_get(b, "type");
       if (type === "ImportDeclaration") {
         let source = object_property_get(b, "source");
+        let type_source = object_property_get(source, "type");
         let specifiers = object_property_get(b, "specifiers");
         assert(list_size_1, [specifiers]);
         let s = list_first(specifiers);
