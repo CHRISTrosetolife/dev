a3f73dc45d5e92633e94ea84993fdb6b225e1cbe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-05 18:52:03 -0500

    2025-02-05T23:52:01.483Z watch   app_code_refresh_function

--- public/src/app_code_refresh_function.mjs
+++ public/src/app_code_refresh_function.mjs
@@ -1,5 +1,5 @@
+import { log } from "./log.mjs";
 import { js_parse } from "./js_parse.mjs";
-import { html_pre_text } from "./html_pre_text.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { storage_local_get_context } from "./storage_local_get_context.mjs";
@@ -20,7 +20,9 @@ export async function app_code_refresh_function(context) {
     );
     let p = function_name_to_path(function_selected);
     let contents = object_property_get(files, p);
-    html_pre_text(root, contents);
     p = js_parse(contents);
+    log({
+      p,
+    });
   });
 }
