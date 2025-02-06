543e49a44fecd0db44aeac46f3462192398b959e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 13:17:16 -0500

    2025-02-06T18:17:15.742Z watch   app_code_refresh_function_node

--- public/src/app_code_refresh_function_node.mjs
+++ public/src/app_code_refresh_function_node.mjs
@@ -55,6 +55,7 @@ export function app_code_refresh_function_node(parent, node) {
     let body2 = object_property_get(node, "body");
     let id = object_property_get(node, "id");
     let name3 = object_property_get(id, "name");
+    html_code_identifier_fn(parent, name3);
     app_code_refresh_function_node(parent, declaration);
   } else {
     log({
