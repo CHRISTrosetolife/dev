563467c95bce6e7c4f8ea7c0d13573c9f095362f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 13:24:24 -0500

    2025-02-06T18:24:09.375Z watch   html_span_text_font_color

--- public/src/html_span_text_font_color.mjs
+++ public/src/html_span_text_font_color.mjs
@@ -1,6 +1,6 @@
 import { html_style_font_color } from "./html_style_font_color.mjs";
 import { html_span_text } from "./html_span_text.mjs";
-export function html_span_text_font_color(section, keyword, color) {
-  let kw = html_span_text(section, keyword);
-  html_style_font_color(kw, color);
+export function html_span_text_font_color(parent, text, font_color) {
+  let kw = html_span_text(parent, text);
+  html_style_font_color(kw, font_color);
 }
