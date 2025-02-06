082aa3768417b286d79691211d032317ea11397f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-05 22:13:21 -0500

    2025-02-06T03:13:18.306Z watch   html_code_string

--- public/src/html_code_string.mjs
+++ public/src/html_code_string.mjs
@@ -1,7 +1,7 @@
+import { string_delimit_double } from "./string_delimit_double.mjs";
 import { html_style_font_color } from "./html_style_font_color.mjs";
-import { string_delimit } from "./string_delimit.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 export function html_code_string(section, value) {
-  let st = html_span_text(section, string_delimit(value));
+  let st = html_span_text(section, string_delimit_double(value));
   html_style_font_color(st, "chocolate");
 }
