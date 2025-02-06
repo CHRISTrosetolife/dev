fea723057c34bb6daeef1fe7427c5d1e3cef52db
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-05 21:18:00 -0500

    2025-02-06T02:17:57.293Z watch   html_span_text

--- public/src/html_span_text.mjs
+++ public/src/html_span_text.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { html_span } from "./html_span.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
 export function html_span_text(parent, text) {
+  assert_arguments_length(arguments, 2);
   let a = html_span(parent);
   html_inner_set(a, text);
   return a;
