b0f408dff8696ec056f6ea96124da4468b6ee4a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-05 21:21:46 -0500

    2025-02-06T02:21:42.173Z watch   html_textarea_code

---
+++ public/src/html_style_code_dark.mjs
@@ -0,0 +1,8 @@
+import { app_learn_code_code_background_set } from "./app_learn_code_code_background_set.mjs";
+import { app_learn_code_style_code } from "./app_learn_code_style_code.mjs";
+import { app_learn_code_style_code_color } from "./app_learn_code_style_code_color.mjs";
+export function html_style_code_dark(textarea) {
+  app_learn_code_style_code_color(textarea);
+  app_learn_code_style_code(textarea);
+  app_learn_code_code_background_set(textarea);
+}
