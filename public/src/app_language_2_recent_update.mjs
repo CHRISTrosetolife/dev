0a329b53aa209930147eef121e04abe04499b147
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 10:45:14 -0500

    2025-02-06T15:45:11.257Z watch   storage_local_recent_add

--- public/src/app_language_2_recent_update.mjs
+++ public/src/app_language_2_recent_update.mjs
@@ -18,7 +18,7 @@ export function app_language_2_recent_update(context, word, recent_count) {
   let questions_recent_limit = number_max(
     recent_count,
     (answer_count_max - 1) * answer_choice_word_count_max,
-  );
+  );$ag
   let property_name = "questions_recent";
   storage_local_recent_add(
     context,
