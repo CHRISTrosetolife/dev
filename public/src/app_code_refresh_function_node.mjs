33b8f81d54b4570c7e33b75a88395a0a9b74346c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 12:47:50 -0500

    2025-02-06T17:47:47.922Z watch   app_code_refresh_function_node

--- public/src/app_code_refresh_function_node.mjs
+++ public/src/app_code_refresh_function_node.mjs
@@ -43,7 +43,8 @@ export function app_code_refresh_function_node(parent, node) {
     let f_name = html_span_text(parent, name);
     html_style_font_color(f_name, "forestgreen");
     html_span_text(parent, string_combine_multiple([" } "]));
-    html_code_keyword(parent, js_keyword_from());
+    let kw = js_keyword_from();
+    html_code_keyword(parent, kw);
     html_span_text(parent, " ");
     html_code_string(parent, value);
     html_span_text(parent, js_code_statement_end());
