d4c828d26e2fb6f0d061cf1d031b5754eccbf96b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 11:02:13 -0500

    2025-02-06T16:02:12.773Z watch   app_code_refresh_functions_recent

--- public/src/app_code_refresh_functions_recent.mjs
+++ public/src/app_code_refresh_functions_recent.mjs
@@ -1,7 +1,11 @@
+import { each } from "./each.mjs";
+import { app_code_functions_recent_get } from "./app_code_functions_recent_get.mjs";
 import { app_code_button_menu_app } from "./app_code_button_menu_app.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_code_screen_set } from "./app_code_screen_set.mjs";
 export function app_code_refresh_functions_recent(context) {
   let root = app_code_screen_set(context, fn_name("app_code_refresh_function"));
   app_code_button_menu_app(context);
+  let functions_recent = app_code_functions_recent_get(context);
+  each(list, (item) => {});
 }
