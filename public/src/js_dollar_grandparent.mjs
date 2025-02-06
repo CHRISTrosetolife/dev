dedd161e0f1de27f67f4b93931a6235f97ef8944
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 13:59:21 -0500

    2025-02-06T18:59:18.026Z c js_dollar_grandparent_next js_dollar_grandparent

---
+++ public/src/js_dollar_grandparent.mjs
@@ -0,0 +1,19 @@
+import { list_get } from "./list_get.mjs";
+import { list_index_next } from "./list_index_next.mjs";
+import { list_is } from "./list_is.mjs";
+import { list_get_end_1 } from "./list_get_end_1.mjs";
+export async function js_dollar_grandparent(v, lambda) {
+  let { parent } = v;
+  let { stack } = v;
+  let s1 = list_get_end_1(stack);
+  if (list_is(s1)) {
+    let index = list_index_next(s1, parent);
+    let next = list_get(s1, index);
+    await lambda({
+      index,
+      next,
+      s1,
+    });
+  }
+  return s1;
+}
