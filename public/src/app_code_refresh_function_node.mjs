f1090bd2cd720f2da846ffc675d797a29b88bf8d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 12:13:32 -0500

    2025-02-06T17:13:31.659Z watch   app_code_refresh_function_node

--- public/src/app_code_refresh_function_node.mjs
+++ public/src/app_code_refresh_function_node.mjs
@@ -1,3 +1,4 @@
+import { js_node_type_is } from "./js_node_type_is.mjs";
 import { js_code_statement_end } from "./js_code_statement_end.mjs";
 import { html_code_string } from "./html_code_string.mjs";
 import { js_keyword_from } from "./js_keyword_from.mjs";
@@ -17,8 +18,7 @@ import { object_property_get } from "./object_property_get.mjs";
 import { html_div } from "./html_div.mjs";
 export function app_code_refresh_function_node(parent, b) {
   let section = html_div(parent);
-  let type = object_property_get(b, "type");
-  if (type === "ImportDeclaration") {
+  if (js_node_type_is(b, "ImportDeclaration")) {
     let source = object_property_get(b, "source");
     let type_source = object_property_get(source, "type");
     assert(equal, [type_source, "Literal"]);
