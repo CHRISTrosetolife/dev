7967ef2eb3714bd325d9a6e90c6154e5f4c804b4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-05 22:18:15 -0500

    2025-02-06T03:18:14.842Z watch   app_code_button_functions_search

--- public/src/app_code_button_functions_search.mjs
+++ public/src/app_code_button_functions_search.mjs
@@ -2,7 +2,7 @@ import { object_property_get } from "./object_property_get.mjs";
 import { app_code_refresh_functions } from "./app_code_refresh_functions.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function app_code_button_functions_search(context) {
-  let root2 = object_property_get(context, "root");
+  let root = object_property_get(context, "root");
   html_button_width_full_text_click(root, "Search Functions", async () => {
     await app_code_refresh_functions(context);
   });
