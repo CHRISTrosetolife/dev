69ea1a8d2436c052df6463a512621bdaf06949d5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 16:31:03 -0500

    2025-02-06T21:30:54.544Z watch   app_code_refresh_function_node

--- public/src/app_code_refresh_function_node.mjs
+++ public/src/app_code_refresh_function_node.mjs
@@ -86,7 +86,7 @@ export function app_code_refresh_function_node(parent, node) {
     html_span_text(parent, js_code_statement_end());
   } else if (js_node_type_is(node, "VariableDeclarator")) {
     let id2 = object_property_get(node, "id");
-    assert(js_node_type_is, [node, "Identifier"]);
+    assert(js_node_type_is, [id2, "Identifier"]);
     let name4 = object_property_get(id2, "name");
     html_code_identifier(parent, name4);
     html_span_text(parent, " = ");
