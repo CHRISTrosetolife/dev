bdaae939b3a5fb0169f4140ea86ca90ae09db96c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 10:42:36 -0500

    2025-02-06T15:42:28.844Z watch   git_logout_extra

--- public/src/git_logout_extra.mjs
+++ public/src/git_logout_extra.mjs
@@ -1,4 +1,4 @@
 import { command_line_git } from "./command_line_git.mjs";
 export async function git_logout_extra() {
-  await command_line_git();
+  await command_line_git("credential-manager github logout 158495363");
 }
