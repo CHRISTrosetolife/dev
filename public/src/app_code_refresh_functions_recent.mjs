7fc6d6fc0f305ff2334407a99c172109fb30d791
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 12:03:03 -0500

    2025-02-06T17:02:58.557Z watch   app_code_refresh_functions_recent

--- public/src/app_code_refresh_functions_recent.mjs
+++ public/src/app_code_refresh_functions_recent.mjs
@@ -1,11 +1,10 @@
+import { each_index_1 } from "./each_index_1.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { each } from "./each.mjs";
 import { app_code_functions_recent_get } from "./app_code_functions_recent_get.mjs";
 import { app_code_button_menu_app } from "./app_code_button_menu_app.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_code_screen_set } from "./app_code_screen_set.mjs";
 import { html_button } from "./html_button.mjs";
-import { each_index } from "./each_index.mjs";
 export function app_code_refresh_functions_recent(context) {
   let root = app_code_screen_set(
     context,
@@ -13,7 +12,6 @@ export function app_code_refresh_functions_recent(context) {
   );
   app_code_button_menu_app(context);
   let functions_recent = app_code_functions_recent_get(context);
-  each_index_1(functions_recent, (fr) => {
+  each_index_1(functions_recent, (fr,index) => {
     html_button(root, string_combine_multiple([fr]), () => {});
-  });
-}
+  
