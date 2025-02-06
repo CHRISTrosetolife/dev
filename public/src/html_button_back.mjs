b1a2d293ac15fcd9c50a3f5c4f445694a40bd6d6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-05 22:19:43 -0500

    2025-02-06T03:19:42.486Z watch   html_button_back

--- public/src/html_button_back.mjs
+++ public/src/html_button_back.mjs
@@ -1,10 +1,11 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_button_back_text } from "./html_button_back_text.mjs";
 import { html_button_width_full_text_click } from "./html_button_width_full_text_click.mjs";
 export function html_button_back(parent, back_on_click) {
   let after = "";
   html_button_width_full_text_click(
     parent,
-    html_button_back_text(),
+    string_combine_multiple([html_button_back_text()]),
     back_on_click,
   );
 }
