6344bdc7ed0c4f0a4fde66754349be6ffbaf294c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 11:47:38 -0500

    2025-02-06T16:47:37.249Z watch   js_fn_name_rename

--- public/src/js_fn_name_rename.mjs
+++ public/src/js_fn_name_rename.mjs
@@ -1,5 +1,4 @@
 import { js_value_string_set } from "./js_value_string_set.mjs";
-import { log } from "./log.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_visit_calls_fn_name } from "./js_visit_calls_fn_name.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
@@ -9,9 +8,6 @@ export function js_fn_name_rename(ast, name_from, name_to) {
     let v = object_property_get(arg, "value");
     if (v === name_from) {
       js_value_string_set(arg, name_to);
-      log({
-        arg,
-      });
     }
   });
 }
