aa84e124214addcf0507db8ec2af532a07699ce1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 12:55:41 -0500

    2025-02-06T17:55:41.079Z watch   app_code_refresh_function_node

--- public/src/app_code_refresh_function_node.mjs
+++ public/src/app_code_refresh_function_node.mjs
@@ -52,7 +52,6 @@ export function app_code_refresh_function_node(parent, node) {
     app_code_refresh_function_node(parent, declaration);
   } else if (js_node_type_is(node, "FunctionDeclaration")) {
     html_code_keyword_space(parent, js_keyword_function());
-    let declaration = object_property_get(node, "declaration");
     app_code_refresh_function_node(parent, declaration);
   } else {
     log({
