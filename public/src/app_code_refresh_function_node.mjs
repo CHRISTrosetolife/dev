af2ea464c2478b7fab4acc36a151436e870238a5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 13:54:12 -0500

    2025-02-06T18:54:06.582Z watch   app_code_refresh_function_node

--- public/src/app_code_refresh_function_node.mjs
+++ public/src/app_code_refresh_function_node.mjs
@@ -66,7 +66,7 @@ export function app_code_refresh_function_node(parent, node) {
     });
     html_span_text(parent, ") {");
     let body3 = object_property_get(node, "body");
-    app_code_refresh_function_node_section(parent, body2);
+    app_code_refresh_function_node_section(parent, body3);
     html_span_text(parent, "}");
   } else {
     log({
