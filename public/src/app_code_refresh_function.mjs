cbef50bc9e1f532e6c6c7ba522b321a9fb01da41
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-05 21:23:25 -0500

    2025-02-06T02:23:24.741Z watch   app_code_refresh_function

--- public/src/app_code_refresh_function.mjs
+++ public/src/app_code_refresh_function.mjs
@@ -36,8 +36,8 @@ export async function app_code_refresh_function(context) {
     );
     let path = function_name_to_path(function_selected);
     let contents = object_property_get(files, path);
-    html_style_code_dark(container);
     let container = html_div(root);
+    html_style_code_dark(container);
     let p = js_parse(contents);
     let body = object_property_get(p, "body");
     each(body, (b) => {
