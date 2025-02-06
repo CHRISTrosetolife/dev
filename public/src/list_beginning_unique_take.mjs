d1f31172843beb2cf5b2a2232503850c126af67c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 10:27:55 -0500

    2025-02-06T15:27:47.030Z watch   list_beginning_unique_take

--- public/src/list_beginning_unique_take.mjs
+++ public/src/list_beginning_unique_take.mjs
@@ -1,16 +1,9 @@
 import { list_take_soft } from "./list_take_soft.mjs";
 import { list_unique_json } from "./list_unique_json.mjs";
 import { list_add_beginning } from "./list_add_beginning.mjs";
-export function list_beginning_unique_take(
-  questions_recent_stored,
-  recent_new,
-  questions_recent_limit,
-) {
-  list_add_beginning(questions_recent_stored, recent_new);
-  questions_recent_stored = list_unique_json(questions_recent_stored);
-  questions_recent_stored = list_take_soft(
-    questions_recent_stored,
-    questions_recent_limit,
-  );
-  return questions_recent_stored;
+export function list_beginning_unique_take(list, item_new, limit) {
+  list_add_beginning(list, item_new);
+  list = list_unique_json(list);
+  list = list_take_soft(list, limit);
+  return list;
 }
