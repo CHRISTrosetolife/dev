f55da0fd4cf6778c38e5df1e1150913ab0c895c6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 11:29:54 -0500

    2025-02-06T16:29:53.482Z watch   test2

--- public/src/test1.mjs
+++ public/src/test1.mjs
@@ -1,4 +1,4 @@
 import { fn_name } from "./fn_name.mjs";
 export function test1() {
-  fn_name("test1a");
+  fn_name("test1");
 }
--- public/src/test2.mjs
+++ public/src/test2.mjs
@@ -1,4 +1,4 @@
 import { fn_name } from "./fn_name.mjs";
 export function test2() {
-  fn_name("test1a");
+  fn_name("test1");
 }
