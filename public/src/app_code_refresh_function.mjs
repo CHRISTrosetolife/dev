94dc91d6c524881a22efb7fd2cc2eb4882a491d6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-05 21:33:46 -0500

    2025-02-06T02:33:45.786Z watch   app_code_refresh_function

--- public/src/app_code_refresh_function.mjs
+++ public/src/app_code_refresh_function.mjs
@@ -65,7 +65,7 @@ export async function app_code_refresh_function(context) {
         html_span_text(section, string_combine_multiple([" } "]));
         html_code_keyword(section, js_keyword_from());
         html_span_text(section, " ");
-        s = html_span_text(section, string_delimit(value));
+        let st = html_span_text(section, string_delimit(value));
         html_span_text(section, js_code_statement_end());
       }
     });
