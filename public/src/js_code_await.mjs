cfb27df18d82d995577a2c6941584c4d3fb99c03
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-05 19:05:59 -0500

    2025-02-06T00:05:58.196Z watch   js_code_await

--- public/src/js_code_await.mjs
+++ public/src/js_code_await.mjs
@@ -1,3 +1,4 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function js_code_await(awaited_code) {
-  return `await ${awaited_code}`;
+  return string_combine_multiple(["await ", awaited_code]);
 }
