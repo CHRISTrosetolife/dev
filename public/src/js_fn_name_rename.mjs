3cad47f804ef453bcde63490f914ac50f1e4c8a5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 11:10:51 -0500

    2025-02-06T16:10:50.138Z watch   js_fn_name_rename

--- public/src/js_fn_name_rename.mjs
+++ public/src/js_fn_name_rename.mjs
@@ -1,4 +1,6 @@
+import { js_visit_calls_fn_name } from "./js_visit_calls_fn_name.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function js_fn_name_rename(ast, name_from, name_to) {
   assert_arguments_length(arguments, 3);
+  js_visit_calls_fn_name(ast);
 }
