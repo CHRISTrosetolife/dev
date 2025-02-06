60a8455d3c19eccaa48cb3641dc07752c97d64a6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 11:57:40 -0500

    2025-02-06T16:57:36.548Z watch   app_code_refresh_function_menu

--- public/src/app_code_refresh_function_menu.mjs
+++ public/src/app_code_refresh_function_menu.mjs
@@ -1,11 +1,15 @@
+import { fn_name } from "./fn_name.mjs";
+import { app_code_screen_set } from "./app_code_screen_set.mjs";
 import { app_code_button_menu_app } from "./app_code_button_menu_app.mjs";
 import { app_code_refresh_function } from "./app_code_refresh_function.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_button_back_after } from "./html_button_back_after.mjs";
 import { storage_local_get_context } from "./storage_local_get_context.mjs";
-import { html_clear_scroll_top_centered_context } from "./html_clear_scroll_top_centered_context.mjs";
 export function app_code_refresh_function_menu(context) {
-  let root = html_clear_scroll_top_centered_context(context);
+  let root = app_code_screen_set(
+    context,
+    fn_name("app_code_refresh_function_menu"),
+  );
   let function_selected = storage_local_get_context(
     context,
     "function_selected",
