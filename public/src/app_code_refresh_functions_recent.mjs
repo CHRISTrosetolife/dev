cadda4528e437d878a3eefa435923627ddfff063
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 11:02:29 -0500

    2025-02-06T16:02:28.826Z watch   app_code_refresh_functions_recent

--- public/src/app_code_refresh_functions_recent.mjs
+++ public/src/app_code_refresh_functions_recent.mjs
@@ -3,9 +3,12 @@ import { app_code_functions_recent_get } from "./app_code_functions_recent_get.m
 import { app_code_button_menu_app } from "./app_code_button_menu_app.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_code_screen_set } from "./app_code_screen_set.mjs";
+import { html_button } from "./html_button.mjs";
 export function app_code_refresh_functions_recent(context) {
   let root = app_code_screen_set(context, fn_name("app_code_refresh_function"));
   app_code_button_menu_app(context);
   let functions_recent = app_code_functions_recent_get(context);
-  each(list, (item) => {});
+  each(functions_recent, (fr) => {
+    html_button(root, fr, () => {});
+  });
 }
