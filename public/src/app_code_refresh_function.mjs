05c050d343a2b56aba2924b3d9f6134423dbd47e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-06 12:11:53 -0500

    2025-02-06T17:11:50.250Z watch   app_code_refresh_function

--- public/src/app_code_refresh_function.mjs
+++ public/src/app_code_refresh_function.mjs
@@ -1,19 +1,9 @@
+import { app_code_refresh_function_node } from "./app_code_refresh_function_node.mjs";
 import { html_style_left } from "./html_style_left.mjs";
 import { app_code_refresh_function_menu } from "./app_code_refresh_function_menu.mjs";
 import { storage_local_recent_add } from "./storage_local_recent_add.mjs";
 import { html_button } from "./html_button.mjs";
-import { html_code_string } from "./html_code_string.mjs";
-import { html_code_keyword } from "./html_code_keyword.mjs";
 import { html_style_monospace } from "./html_style_monospace.mjs";
-import { log } from "./log.mjs";
-import { js_keyword_from } from "./js_keyword_from.mjs";
-import { js_keyword_import } from "./js_keyword_import.mjs";
-import { js_code_statement_end } from "./js_code_statement_end.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { equal } from "./equal.mjs";
-import { list_first } from "./list_first.mjs";
-import { list_size_1 } from "./list_size_1.mjs";
-import { assert } from "./assert.mjs";
 import { each } from "./each.mjs";
 import { js_parse } from "./js_parse.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -23,11 +13,7 @@ import { app_code_files_get } from "./app_code_files_get.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_code_screen_set } from "./app_code_screen_set.mjs";
 import { html_load } from "./html_load.mjs";
-import { string_starts_with } from "./string_starts_with.mjs";
-import { folder_current_prefix } from "./folder_current_prefix.mjs";
 import { html_div } from "./html_div.mjs";
-import { html_span_text } from "./html_span_text.mjs";
-import { html_style_font_color } from "./html_style_font_color.mjs";
 export async function app_code_refresh_function(context) {
   await html_load(async () => {
     let root = app_code_screen_set(
@@ -57,35 +43,7 @@ export async function app_code_refresh_function(context) {
     let p = js_parse(contents);
     let body = object_property_get(p, "body");
     each(body, (b) => {
-      let section = html_div(container);
-      let type = object_property_get(b, "type");
-      if (type === "ImportDeclaration") {
-        let source = object_property_get(b, "source");
-        let type_source = object_property_get(source, "type");
-        assert(equal, [type_source, "Literal"]);
-        let value = object_property_get(source, "value");
-        assert(string_starts_with, [value, folder_current_prefix()]);
-        let specifiers = object_property_get(b, "specifiers");
-        assert(list_size_1, [specifiers]);
-        let s = list_first(specifiers);
-        let imported = object_property_get(s, "imported");
-        let name2 = object_property_get(imported, "name");
-        let local = object_property_get(s, "local");
-        let name = object_property_get(local, "name");
-        assert(equal, [name, name2]);
-        html_code_keyword(section, js_keyword_import());
-        html_span_text(section, " { ");
-        let f_name = html_span_text(section, name);
-        html_style_font_color(f_name, "forestgreen");
-        log({
-          local,
-        });
-        html_span_text(section, string_combine_multiple([" } "]));
-        html_code_keyword(section, js_keyword_from());
-        html_span_text(section, " ");
-        html_code_string(section, value);
-        html_span_text(section, js_code_statement_end());
-      }
+      app_code_refresh_function_node(container, b);
     });
   });
 }
---
+++ public/src/app_code_refresh_function_node.mjs
@@ -0,0 +1,48 @@
+import { js_code_statement_end } from "./js_code_statement_end.mjs";
+import { html_code_string } from "./html_code_string.mjs";
+import { js_keyword_from } from "./js_keyword_from.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { log } from "./log.mjs";
+import { html_style_font_color } from "./html_style_font_color.mjs";
+import { html_span_text } from "./html_span_text.mjs";
+import { js_keyword_import } from "./js_keyword_import.mjs";
+import { html_code_keyword } from "./html_code_keyword.mjs";
+import { list_first } from "./list_first.mjs";
+import { list_size_1 } from "./list_size_1.mjs";
+import { folder_current_prefix } from "./folder_current_prefix.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
+import { equal } from "./equal.mjs";
+import { assert } from "./assert.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { html_div } from "./html_div.mjs";
+export function app_code_refresh_function_node(parent, b) {
+  let section = html_div(parent);
+  let type = object_property_get(b, "type");
+  if (type === "ImportDeclaration") {
+    let source = object_property_get(b, "source");
+    let type_source = object_property_get(source, "type");
+    assert(equal, [type_source, "Literal"]);
+    let value = object_property_get(source, "value");
+    assert(string_starts_with, [value, folder_current_prefix()]);
+    let specifiers = object_property_get(b, "specifiers");
+    assert(list_size_1, [specifiers]);
+    let s = list_first(specifiers);
+    let imported = object_property_get(s, "imported");
+    let name2 = object_property_get(imported, "name");
+    let local = object_property_get(s, "local");
+    let name = object_property_get(local, "name");
+    assert(equal, [name, name2]);
+    html_code_keyword(section, js_keyword_import());
+    html_span_text(section, " { ");
+    let f_name = html_span_text(section, name);
+    html_style_font_color(f_name, "forestgreen");
+    log({
+      local,
+    });
+    html_span_text(section, string_combine_multiple([" } "]));
+    html_code_keyword(section, js_keyword_from());
+    html_span_text(section, " ");
+    html_code_string(section, value);
+    html_span_text(section, js_code_statement_end());
+  }
+}
