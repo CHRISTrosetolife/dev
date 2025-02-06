d4d6b42980a8a90eae3bf859c6a412151676e2bc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-05 21:25:43 -0500

    2025-02-06T02:25:42.936Z watch   html_textarea_code

--- public/src/html_textarea_code.mjs
+++ public/src/html_textarea_code.mjs
@@ -1,9 +1,13 @@
-import { html_style_code_dark } from "./html_style_code_dark.mjs";
+import { app_learn_code_code_background_set } from "./app_learn_code_code_background_set.mjs";
+import { app_learn_code_style_code } from "./app_learn_code_style_code.mjs";
+import { app_learn_code_style_code_color } from "./app_learn_code_style_code_color.mjs";
 import { html_textarea_width_full } from "./html_textarea_width_full.mjs";
 import { html_attribute_set } from "./html_attribute_set.mjs";
 export function html_textarea_code(root) {
   let textarea = html_textarea_width_full(root);
-  html_style_code_dark(textarea);
+  app_learn_code_style_code_color(textarea);
+  app_learn_code_style_code(textarea);
+  app_learn_code_code_background_set(textarea);
   html_attribute_set(textarea, "spellcheck", "false");
   return textarea;
 }
