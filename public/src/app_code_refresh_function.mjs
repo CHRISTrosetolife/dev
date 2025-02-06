9a212a34f40ef34884e86c05dd51400c047cf40b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-05 19:04:06 -0500

    2025-02-06T00:04:05.684Z watch   app_code_refresh_function

--- public/src/app_code_refresh_function.mjs
+++ public/src/app_code_refresh_function.mjs
@@ -16,6 +16,7 @@ import { string_starts_with } from "./string_starts_with.mjs";
 import { folder_current_prefix } from "./folder_current_prefix.mjs";
 import { html_div } from "./html_div.mjs";
 import { html_span_text } from "./html_span_text.mjs";
+import { string_delimit } from "./string_delimit.mjs";
 export async function app_code_refresh_function(context) {
   await html_load(async () => {
     let root = app_code_screen_set(
@@ -48,7 +49,10 @@ export async function app_code_refresh_function(context) {
         assert(equal, [imported, local]);
         html_span_text(section, "import { ");
         html_span_text(section, local);
-        html_span_text(section, string_combine_multiple(["} from "]));
+        html_span_text(
+          section,
+          string_combine_multiple(["} from ", string_delimit()]),
+        );
       }
     });
   });
