f3b5b02e2c85ca03aebea78f1df9dc9d783c3694
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 18:22:07 -0500

    2025-02-07T23:22:07.158Z watch   app_code_local_upload

--- public/src/app_code_local_upload.mjs
+++ public/src/app_code_local_upload.mjs
@@ -1,5 +1,4 @@
-import { object_property_set } from "./object_property_set.mjs";
-import { each_object } from "./each_object.mjs";
+import { object_map } from "./object_map.mjs";
 import { app_code_local_files_path } from "./app_code_local_files_path.mjs";
 import { app_code_batch_name } from "./app_code_batch_name.mjs";
 import { storage_upload_object } from "./storage_upload_object.mjs";
@@ -8,10 +7,6 @@ export async function app_code_local_upload() {
   let batch_name = await app_code_batch_name();
   let s = await functions_source_get();
   let mapper = (contents, file_path) => {};
-  let result = {};
-  each_object(s, (file_path, contents) => {
-    let value = mapper(contents, file_path);
-    object_property_set(object, file_path, value);
-  });
+  let result = object_map(s, mapper);
   await storage_upload_object(app_code_local_files_path(batch_name), result);
 }
---
+++ public/src/object_map.mjs
@@ -0,0 +1,10 @@
+import { object_property_set } from "./object_property_set.mjs";
+import { each_object } from "./each_object.mjs";
+export function object_map(s, mapper) {
+  let result = {};
+  each_object(s, (key, value) => {
+    let value_new = mapper(value, key);
+    object_property_set(object, key, value_new);
+  });
+  return result;
+}
