ca92a48ea357c7831ebd3162ca1e18ce01fc71c3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 18:14:34 -0500

    2025-02-07T23:14:30.721Z watch   app_code_local_functions_path

--- public/src/app_code_local_functions_path.mjs
+++ public/src/app_code_local_functions_path.mjs
@@ -1,7 +1,5 @@
-import { app_code_batch_name } from "./app_code_batch_name.mjs";
 import { app_code_repo_name } from "./app_code_repo_name.mjs";
 import { path_join } from "./path_join.mjs";
-export async function app_code_local_functions_path() {
-  let batch_name = await app_code_batch_name();
+export function app_code_local_functions_path(batch_name) {
   return path_join(["code", "local", app_code_repo_name(), batch_name]);
 }
