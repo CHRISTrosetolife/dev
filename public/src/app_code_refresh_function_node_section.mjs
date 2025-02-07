4bbd0e22ef9d37b6cf9fdf7c01a0086508d6ff79
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 13:22:48 -0500

    2025-02-07T18:22:46.316Z watch   app_code_refresh_function_node_section

--- public/src/app_code_refresh_function_node_section.mjs
+++ public/src/app_code_refresh_function_node_section.mjs
@@ -4,10 +4,11 @@ import { html_div } from "./html_div.mjs";
 import { html_span } from "./html_span.mjs";
 import { html_style_font_color_white } from "./html_style_font_color_white.mjs";
 export function app_code_refresh_function_node_section(parent, b, indent) {
+  indent++;
   let section = html_div(parent);
   let left = html_span(section);
   each_range(indent, (i) => {
-    let s = html_span(parent, "a");
+    let s = html_span(left, "a");
     html_style_font_color_white(s);
   });
   let right = html_span(section);
