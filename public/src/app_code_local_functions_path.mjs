902037f96a12ed33a6784a7afdc558969223a717
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 14:08:53 -0500

    2025-02-07T19:08:53.090Z watch   app_code_local_functions_path

--- public/src/app_code_local_functions_path.mjs
+++ public/src/app_code_local_functions_path.mjs
@@ -4,9 +4,6 @@ import { app_code_repo_name } from "./app_code_repo_name.mjs";
 import { path_join } from "./path_join.mjs";
 export async function app_code_local_functions_path() {
   let batch_name = path_join([
-    "code",
-    "local",
-    app_code_repo_name(),
     "batch",
     date_string_iso_file(),
     await uuid(),
