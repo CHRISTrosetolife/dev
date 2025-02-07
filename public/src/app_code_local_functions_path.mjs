e11ba39079acc277eb4f52c6a8d8b6d08a08c364
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 14:02:15 -0500

    2025-02-07T19:02:15.391Z watch   app_code_local_functions_path

--- public/src/app_code_local_functions_path.mjs
+++ public/src/app_code_local_functions_path.mjs
@@ -1,7 +1,9 @@
+import { string_replace_multiple } from "./string_replace_multiple.mjs";
 import { date_string_iso_file } from "./date_string_iso_file.mjs";
 import { app_code_repo_name } from "./app_code_repo_name.mjs";
 import { path_join } from "./path_join.mjs";
 export function app_code_local_functions_path() {
+  string_replace_multiple();
   return path_join([
     "code",
     "local",
