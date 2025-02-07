97bef17f88abd1e65e4fd3fc375e7972265809ce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 14:09:02 -0500

    2025-02-07T19:09:01.890Z watch   app_code_local_functions_path

--- public/src/app_code_local_functions_path.mjs
+++ public/src/app_code_local_functions_path.mjs
@@ -9,13 +9,5 @@ export async function app_code_local_functions_path() {
     await uuid(),
     ".json",
   ]);
-  return path_join([
-    "code",
-    "local",
-    app_code_repo_name(),
-    "batch",
-    date_string_iso_file(),
-    await uuid(),
-    ".json",
-  ]);
+  return path_join(["code", "local", app_code_repo_name(), batch_name]);
 }
