6786405b5560f053130a403f2a8c1ec9ac8af879
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 13:37:16 -0500

    2025-02-07T18:37:16.445Z watch   app_code_refresh_function_node_section

--- public/src/app_code_refresh_function_node_section.mjs
+++ public/src/app_code_refresh_function_node_section.mjs
@@ -5,11 +5,13 @@ import { app_code_refresh_function_node } from "./app_code_refresh_function_node
 import { html_div } from "./html_div.mjs";
 import { html_style_width } from "./html_style_width.mjs";
 export function app_code_refresh_function_node_section(parent, b, indent) {
+  if (false) {
+  }
   let section = html_div(parent);
   html_style_flex_row_centered(section);
   let left = html_div(section);
   html_style_width(left, string_combine_multiple(["1ch"]));
-  let right = html_div(section);
-  html_style_flex_1(right);
-  app_code_refresh_function_node(right, b);
+  parent = html_div(section);
+  html_style_flex_1(parent);
+  app_code_refresh_function_node(parent, b);
 }
