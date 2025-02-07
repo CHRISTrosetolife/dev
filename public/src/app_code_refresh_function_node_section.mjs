409c8368ea1d8d24c9e7e856d6626822609b475f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 13:31:11 -0500

    2025-02-07T18:31:10.589Z watch   app_code_refresh_function_node_section

--- public/src/app_code_refresh_function_node_section.mjs
+++ public/src/app_code_refresh_function_node_section.mjs
@@ -1,16 +1,14 @@
-import { each_range } from "./each_range.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { html_style_flex_row_centered } from "./html_style_flex_row_centered.mjs";
 import { app_code_refresh_function_node } from "./app_code_refresh_function_node.mjs";
 import { html_div } from "./html_div.mjs";
-import { html_span } from "./html_span.mjs";
 import { html_style_width } from "./html_style_width.mjs";
 export function app_code_refresh_function_node_section(parent, b, indent) {
   indent++;
   let section = html_div(parent);
-  let left = html_span(section);
-  each_range(indent, () => {
-    let s = html_div(left);
-    html_style_width(s, "1ch");
-  });
+  html_style_flex_row_centered(section);
+  let left = html_div(section);
+  html_style_width(s, string_combine_multiple(["ch"]));
   let right = html_div(section);
   app_code_refresh_function_node(right, b, indent);
 }
