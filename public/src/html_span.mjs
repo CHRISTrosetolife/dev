e2150ca9f8718b6b0be7c8f41f05f37afb2084a0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 13:24:20 -0500

    2025-02-07T18:24:19.002Z watch   html_span

--- public/src/html_span.mjs
+++ public/src/html_span.mjs
@@ -1,4 +1,6 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { html_element } from "./html_element.mjs";
 export function html_span(parent) {
+  assert_arguments_length(arguments, 1);
   return html_element(parent, "span");
 }
