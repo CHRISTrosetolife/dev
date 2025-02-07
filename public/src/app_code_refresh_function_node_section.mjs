665b8fdd76d5f78e6b1090f3e084eb17e8b8d618
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 13:21:29 -0500

    2025-02-07T18:21:28.265Z watch   app_code_refresh_function_node_section

--- public/src/app_code_refresh_function_node_section.mjs
+++ public/src/app_code_refresh_function_node_section.mjs
@@ -2,11 +2,13 @@ import { each_range } from "./each_range.mjs";
 import { app_code_refresh_function_node } from "./app_code_refresh_function_node.mjs";
 import { html_div } from "./html_div.mjs";
 import { html_span } from "./html_span.mjs";
+import { html_style_font_color_white } from "./html_style_font_color_white.mjs";
 export function app_code_refresh_function_node_section(parent, b, indent) {
   let section = html_div(parent);
   let left = html_span(section);
   each_range(indent, (i) => {
-    html_span(parent, "a");
+    let s = html_span(parent, "a");
+    html_style_font_color_white(i);
   });
   let right = html_span(section);
   app_code_refresh_function_node(right, b, indent);
