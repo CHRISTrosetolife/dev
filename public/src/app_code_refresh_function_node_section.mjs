5ab5d22c30c9efbba352f08234c2887a847b3a90
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 13:20:58 -0500

    2025-02-07T18:20:56.964Z watch   app_code_refresh_function_node_section

--- public/src/app_code_refresh_function_node_section.mjs
+++ public/src/app_code_refresh_function_node_section.mjs
@@ -1,11 +1,11 @@
-import { each } from "./each.mjs";
+import { each_range } from "./each_range.mjs";
 import { app_code_refresh_function_node } from "./app_code_refresh_function_node.mjs";
 import { html_div } from "./html_div.mjs";
 import { html_span } from "./html_span.mjs";
 export function app_code_refresh_function_node_section(parent, b, indent) {
   let section = html_div(parent);
   let left = html_span(section);
-  each(list, (item) => {});
+  each_range(count, (item) => {});
   let right = html_span(section);
   app_code_refresh_function_node(right, b, indent);
 }
