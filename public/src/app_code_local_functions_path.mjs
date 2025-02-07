1af23d7ba820ba8c3941f29eda5165fc2aeeadbb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 14:01:32 -0500

    2025-02-07T19:01:32.174Z watch   app_code_local_functions_path

--- public/src/app_code_local_functions_path.mjs
+++ public/src/app_code_local_functions_path.mjs
@@ -3,5 +3,12 @@ import { app_code_repo_name } from "./app_code_repo_name.mjs";
 import { path_join } from "./path_join.mjs";
 export function app_code_local_functions_path() {
   date_string_iso_file();
-  return path_join(["code", "local", app_code_repo_name(), "batch", ".json"]);
+  return path_join([
+    "code",
+    "local",
+    app_code_repo_name(),
+    "batch",
+    date_string_iso_file(),
+    ".json",
+  ]);
 }
