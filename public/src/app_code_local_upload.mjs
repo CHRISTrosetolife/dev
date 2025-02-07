74a775d979af2310ad9108e2db450c27a1a3c343
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 18:24:44 -0500

    2025-02-07T23:24:44.316Z watch   app_code_local_upload

--- public/src/app_code_local_upload.mjs
+++ public/src/app_code_local_upload.mjs
@@ -4,9 +4,9 @@ import { app_code_batch_name } from "./app_code_batch_name.mjs";
 import { storage_upload_object } from "./storage_upload_object.mjs";
 import { functions_source_get } from "./functions_source_get.mjs";
 export async function app_code_local_upload() {
+  let s = await functions_source_get();
   let batch_name = await app_code_batch_name();
   let storage_path = app_code_local_files_path(batch_name);
-  let s = await functions_source_get();
   let files = object_map(s, (contents) => ({
     contents,
   }));
