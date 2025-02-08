daa198b5ccf188ce1f53bf1b1aba89d660bc333b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 16:44:12 -0500

    2025-02-08T21:44:11.856Z watch   app_code_local_upload

--- public/src/app_code_local_upload.mjs
+++ public/src/app_code_local_upload.mjs
@@ -9,7 +9,10 @@ import { app_code_batch_name } from "./app_code_batch_name.mjs";
 import { storage_upload_object } from "./storage_upload_object.mjs";
 import { functions_source_get } from "./functions_source_get.mjs";
 export async function app_code_local_upload() {
-  await folder_read_shallow(folder_path_public(), app_extension_html());
+  let htmls = await folder_read_shallow(
+    folder_path_public(),
+    app_extension_html(),
+  );
   await files_contents_lookup();
   let s = await functions_source_get();
   let batch_name = await app_code_batch_name();
