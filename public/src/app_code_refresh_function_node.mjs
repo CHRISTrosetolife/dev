2a3337e813880cd3196460a04911bbb2f3d78aa8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 11:14:27 -0500

    2025-02-08T16:14:26.925Z watch   app_code_refresh_function_node

--- public/src/app_code_refresh_function_node.mjs
+++ public/src/app_code_refresh_function_node.mjs
@@ -74,7 +74,7 @@ export function app_code_refresh_function_node(args) {
     html_code_keyword_space(parent, js_keyword_function());
     let id = object_property_get(node, "id");
     let name3 = object_property_get(id, "name");
-    html_code_identifier_fn(parent, name3);
+    app_code_refresh_function_node_identifier_add_fn(parent, name3, args);
     let params = object_property_get(node, "params");
     html_span_text_list_comma_parenthesis(parent, params, lambda);
     function lambda(param) {
