cc0821671d0c13745f478fdfc717d7a5c495b8ce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 11:13:50 -0500

    2025-02-08T16:13:49.871Z watch   app_code_refresh_function_node

---
+++ public/src/app_code_refresh_function_node_identifier_add_fn.mjs
@@ -0,0 +1,10 @@
+import { app_code_refresh_function_node_identifier_add_generic } from "./app_code_refresh_function_node_identifier_add_generic.mjs";
+import { html_code_identifier_fn } from "./html_code_identifier_fn.mjs";
+export function app_code_refresh_function_node_identifier_add_fn(
+  parent,
+  name,
+  args,
+) {
+  let i = html_code_identifier_fn(parent, name);
+  app_code_refresh_function_node_identifier_add_generic(args, name, i);
+}
