0c2f012a377927533fec03232c1aa90a201d8358
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 13:58:34 -0500

    2025-02-08T18:58:29.212Z watch   retry_if

--- public/src/retry_if.mjs
+++ public/src/retry_if.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { list_any_true } from "./list_any_true.mjs";
 import { string_to } from "./string_to.mjs";
 import { string_includes } from "./string_includes.mjs";
@@ -6,9 +5,6 @@ import { list_map } from "./list_map.mjs";
 export function retry_if(list) {
   return function retry_if_inner(e) {
     let s = string_to(e);
-    log({
-      s,
-    });
     let m = list_map(list, (i) => string_includes(s, i));
     return list_any_true(m);
   };
