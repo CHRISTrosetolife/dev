fa2655c8889488296d7d37a81137dc9c3fa2dcaa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 10:55:03 -0500

    2025-02-08T15:55:01.357Z watch   app_code_refresh_function_node_block

--- public/src/app_code_refresh_function_node_block.mjs
+++ public/src/app_code_refresh_function_node_block.mjs
@@ -1,10 +1,16 @@
+import { object_merge } from "./object_merge.mjs";
 import { app_code_refresh_function_node_section } from "./app_code_refresh_function_node_section.mjs";
 import { each } from "./each.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 export function app_code_refresh_function_node_block(parent, body2) {
   html_span_text(parent, " {");
   each(body2, (b) => {
-    app_code_refresh_function_node_section(parent, b, true);
+    app_code_refresh_function_node_section(
+      object_merge(args, {
+        node: b,
+      }),
+      true,
+    );
   });
   html_span_text(parent, "}");
 }
