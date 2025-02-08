4b0bf0e642e543713ba97bbcdfc5104644c7f30d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 12:27:37 -0500

    2025-02-08T17:27:31.254Z watch   html_code_string

--- public/src/html_code_string.mjs
+++ public/src/html_code_string.mjs
@@ -1,4 +1,4 @@
 import { html_span_text_font_color } from "./html_span_text_font_color.mjs";
 export function html_code_string(section, delimited) {
-  html_span_text_font_color(section, delimited, "chocolate");
+  html_span_text_font_color(section, delimited, "orange");
 }
