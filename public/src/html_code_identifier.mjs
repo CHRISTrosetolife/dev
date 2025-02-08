0f6b2fc5de87a5365691467dd02f1817e28c281f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 12:31:49 -0500

    2025-02-08T17:31:40.621Z watch   html_code_identifier

--- public/src/html_code_identifier.mjs
+++ public/src/html_code_identifier.mjs
@@ -1,5 +1,5 @@
 import { html_span_text_font_color } from "./html_span_text_font_color.mjs";
 export function html_code_identifier(parent, param) {
-  let f_name = html_span_text_font_color(parent, param, "steelblue");
+  let f_name = html_span_text_font_color(parent, param, "lightskyblue");
   return f_name;
 }
