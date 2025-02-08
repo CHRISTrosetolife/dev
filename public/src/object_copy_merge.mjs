1713f44c43562384786194210cd6629b99f037cc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 11:06:17 -0500

    2025-02-08T16:06:17.021Z watch   object_copy_merge

--- public/src/object_copy_merge.mjs
+++ public/src/object_copy_merge.mjs
@@ -1,4 +1,6 @@
 import { object_copy } from "./object_copy.mjs";
+import { object_merge } from "./object_merge.mjs";
 export function object_copy_merge(to, from) {
   let to_copy = object_copy(to);
+  return object_merge(to_copy, from);
 }
