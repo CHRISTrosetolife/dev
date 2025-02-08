539d03208c85a6c77a925fffbcbd61f0952f13c6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 16:42:01 -0500

    2025-02-08T21:42:00.285Z watch   functions_contents

--- public/src/functions_contents.mjs
+++ public/src/functions_contents.mjs
@@ -2,5 +2,5 @@ import { files_contents } from "./files_contents.mjs";
 import { functions_paths } from "./functions_paths.mjs";
 export async function functions_contents() {
   let paths = await functions_paths();
-  return files_contents(paths);
+  return await files_contents(paths);
 }
