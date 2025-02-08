02c18c718b9baa1801b5efa7c584e2136bcedbd4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 19:22:34 -0500

    2025-02-08T00:22:33.710Z watch   app_code_refresh_function_node

--- public/src/app_code_refresh_function_node.mjs
+++ public/src/app_code_refresh_function_node.mjs
@@ -118,6 +118,7 @@ export function app_code_refresh_function_node(parent, node) {
     html_code_identifier(parent, name6);
   } else if (js_node_type_is(node, "ObjectExpression")) {
     let properties = object_property_get(node, "properties");
+    html_span_text_list_comma();
   } else if (js_node_type_is(node, "ArrayExpression")) {
     let elements = object_property_get(node, "elements");
     html_span_text_list_comma(parent, elements, lambda);
