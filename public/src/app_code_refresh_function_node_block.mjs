9bb169f1842507853ecd40c606b187ba7b9f8fa9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 12:30:43 -0500

    2025-02-08T17:30:40.229Z watch   app_code_refresh_function_node_block

--- public/src/app_code_refresh_function_node_block.mjs
+++ public/src/app_code_refresh_function_node_block.mjs
@@ -1,10 +1,10 @@
+import { html_span_text_gray } from "./html_span_text_gray.mjs";
 import { object_copy_merge } from "./object_copy_merge.mjs";
 import { app_code_refresh_function_node_section } from "./app_code_refresh_function_node_section.mjs";
 import { each } from "./each.mjs";
-import { html_span_text } from "./html_span_text.mjs";
 export function app_code_refresh_function_node_block(args) {
   let { parent, node } = args;
-  html_span_text(parent, " {");
+  html_span_text_gray(parent, " {");
   each(node, (n) => {
     app_code_refresh_function_node_section(
       object_copy_merge(args, {
@@ -13,5 +13,5 @@ export function app_code_refresh_function_node_block(args) {
       true,
     );
   });
-  html_span_text(parent, "}");
+  html_span_text_gray(parent, "}");
 }
