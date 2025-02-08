2f2dab0bfee77a9cba0a748756dd23dd6e05d507
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 16:42:10 -0500

    2025-02-08T21:42:09.875Z watch   functions_source_get

--- public/src/functions_source_get.mjs
+++ public/src/functions_source_get.mjs
@@ -1,6 +1,8 @@
+import { files_contents } from "./files_contents.mjs";
+import { functions_paths } from "./functions_paths.mjs";
 import { list_to_lookup_key_value_property } from "./list_to_lookup_key_value_property.mjs";
-import { functions_contents } from "./functions_contents.mjs";
 export async function functions_source_get() {
-  let mapped = await functions_contents();
+  let paths = await functions_paths();
+  let mapped = await files_contents(paths);
   return list_to_lookup_key_value_property(mapped, "file_path", "contents");
 }
