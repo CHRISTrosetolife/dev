eaa03db7b763c23e92ef033756875f09977edaaa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 12:34:39 -0500

    2025-02-08T17:34:35.926Z watch   html_code_identifier

--- public/src/html_code_identifier.mjs
+++ public/src/html_code_identifier.mjs
@@ -1,5 +1,5 @@
 import { html_span_text_font_color } from "./html_span_text_font_color.mjs";
 export function html_code_identifier(parent, param) {
-  let f_name = html_span_text_font_color(parent, param, "cadetblue");
+  let f_name = html_span_text_font_color(parent, param, "dodgerblue");
   return f_name;
 }
