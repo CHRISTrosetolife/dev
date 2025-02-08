9aca28f64042d441b6da58d8e1c11c5ab989b854
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 11:16:34 -0500

    2025-02-08T16:16:34.388Z watch   app_code_refresh_function_node_identifier_add_generic

--- public/src/app_code_refresh_function_node_identifier_add_generic.mjs
+++ public/src/app_code_refresh_function_node_identifier_add_generic.mjs
@@ -9,4 +9,5 @@ export function app_code_refresh_function_node_identifier_add_generic(
   let ids = object_property_get(args, "identifiers");
   let id_list = object_property_initialize(ids, name, []);
   list_add(id_list, i);
+  return id_list;
 }
