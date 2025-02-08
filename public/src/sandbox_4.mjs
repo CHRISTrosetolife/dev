5570fe69033dafffc66c2bc05d2509df074d5fa0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 14:07:45 -0500

    2025-02-08T19:07:43.631Z watch   sandbox_4

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -1,6 +1,18 @@
+import { object_property_exists } from "./object_property_exists.mjs";
+import { list_filter } from "./list_filter.mjs";
+import { json_from } from "./json_from.mjs";
+import { english_words } from "./english_words.mjs";
 import { http_cache } from "./http_cache.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function sandbox_4() {
+  let filtered = await english_words();
+  let dictionary_json = await http_cache(
+    "https://raw.githubusercontent.com/matthewreagan/WebstersEnglishDictionary/refs/heads/master/dictionary_compact.json",
+  );
+  let dictionary = json_from(dictionary_json);
+  let filtered2 = list_filter(filtered, (f) =>
+    object_property_exists(dictionary, f),
+  );
   let url = string_combine_multiple([
     "https://www.bohol.ph/diksyunaryo.php?sw=god&lang=English",
   ]);
