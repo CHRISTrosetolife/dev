b1570d9bb55fbdb87b8be4cfcbb609fcb4c1b954
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 11:17:20 -0500

    2025-02-08T16:17:19.519Z watch   app_code_refresh_function_node_identifier_add_generic

--- public/src/app_code_refresh_function_node_identifier_add_generic.mjs
+++ public/src/app_code_refresh_function_node_identifier_add_generic.mjs
@@ -1,3 +1,4 @@
+import { html_on_click } from "./html_on_click.mjs";
 import { list_add } from "./list_add.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -9,5 +10,5 @@ export function app_code_refresh_function_node_identifier_add_generic(
   let ids = object_property_get(args, "identifiers");
   let id_list = object_property_initialize(ids, name, []);
   list_add(id_list, i);
-  return id_list;
+  html_on_click(i, () => {});
 }
