2dd9179f1d257a4233b75ce14c19a6c5b7d78d17
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 19:17:54 -0500

    2025-02-08T00:17:53.279Z watch   app_code_refresh_function

--- public/src/app_code_refresh_function.mjs
+++ public/src/app_code_refresh_function.mjs
@@ -36,7 +36,8 @@ export async function app_code_refresh_function(context) {
       functions_recent_limit,
     );
     let path = function_name_to_path(function_selected);
-    let contents = object_property_get(files, path);
+    let file = object_property_get(files, path);
+    let contents2 = object_property_get(file, "contents");
     let node = js_parse(contents);
     let container = html_div(root);
     html_style_monospace(container);
