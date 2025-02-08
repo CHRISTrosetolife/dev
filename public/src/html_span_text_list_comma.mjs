7c96d50fb56bcb304fe4272078271b1f6bdc8aa3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 12:39:12 -0500

    2025-02-08T17:39:10.379Z watch   html_span_text_list_comma

--- public/src/html_span_text_list_comma.mjs
+++ public/src/html_span_text_list_comma.mjs
@@ -1,11 +1,11 @@
-import { html_span_text } from "./html_span_text.mjs";
+import { html_span_text_gray } from "./html_span_text_gray.mjs";
 import { list_last_is } from "./list_last_is.mjs";
 import { each } from "./each.mjs";
 export function html_span_text_list_comma(parent, list, lambda) {
   each(list, (item) => {
     lambda(item);
     if (!list_last_is(list, item)) {
-      html_span_text(parent, ", ");
+      html_span_text_gray(parent, ", ");
     }
   });
 }
