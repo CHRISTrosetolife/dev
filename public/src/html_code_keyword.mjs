67b9211c18adbdcd93427b1db072686e45501f78
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 12:20:39 -0500

    2025-02-08T17:20:26.624Z watch   html_code_keyword

--- public/src/html_code_keyword.mjs
+++ public/src/html_code_keyword.mjs
@@ -1,5 +1,5 @@
 import { html_span_text_font_color } from "./html_span_text_font_color.mjs";
 export function html_code_keyword(section, keyword) {
-  let color = "purple";
+  let color = "DarkOrchid";
   html_span_text_font_color(section, keyword, color);
 }
