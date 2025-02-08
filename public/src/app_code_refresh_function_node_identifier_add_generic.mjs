369fdbf1115902dcd44a95cd64a1a936500c9260
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 12:11:04 -0500

    2025-02-08T17:11:04.121Z watch   app_code_refresh_function_node_identifier_add_generic

--- public/src/app_code_refresh_function_node_identifier_add_generic.mjs
+++ public/src/app_code_refresh_function_node_identifier_add_generic.mjs
@@ -1,3 +1,4 @@
+import { html_style_background_color } from "./html_style_background_color.mjs";
 import { html_style_background_color_yellow } from "./html_style_background_color_yellow.mjs";
 import { each } from "./each.mjs";
 import { html_on_click } from "./html_on_click.mjs";
@@ -16,7 +17,7 @@ export function app_code_refresh_function_node_identifier_add_generic(
   html_on_click(i, () => {
     each(id_list, (i) =>
       toggled
-        ? html_style_background_color_yellow(i)
+        ? html_style_background_color(i, "none")
         : html_style_background_color_yellow(i),
     );
   });
