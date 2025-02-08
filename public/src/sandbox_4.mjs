6d8d00c42f01c0cc42e01b3dfa665ecc07be3eff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 14:04:30 -0500

    2025-02-08T19:04:29.272Z watch   sandbox_4

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -1,1 +1,6 @@
-export function sandbox_4() {}
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function sandbox_4() {
+  let url = string_combine_multiple([
+    "https://www.bohol.ph/diksyunaryo.php?sw=me&lang=English",
+  ]);
+}
