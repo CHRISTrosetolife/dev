220c8031eedc939470f606da98ae88b4d6ae32e7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 19:05:02 -0500

    2025-02-08T00:05:02.146Z watch   app_code_files_get

--- public/src/app_code_files_get.mjs
+++ public/src/app_code_files_get.mjs
@@ -1,4 +1,4 @@
-import { app_code_local_files_path } from "./app_code_local_files_path.mjs";
+import { app_code_local_files_path_latest } from "./app_code_local_files_path_latest.mjs";
 import { http_storage_get } from "./http_storage_get.mjs";
 import { global_files } from "./global_files.mjs";
 import { object_property_initialize_get_async } from "./object_property_initialize_get_async.mjs";
@@ -9,7 +9,7 @@ export async function app_code_files_get() {
     g,
     global_files(),
     async () => {
-      return await http_storage_get(app_code_local_files_path());
+      return await http_storage_get(app_code_local_files_path_latest());
     },
   );
   return files;
