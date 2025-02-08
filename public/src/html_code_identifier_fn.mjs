55472055c308bb8f4e3cfeabb8608b77e5320b79
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 12:26:34 -0500

    2025-02-08T17:26:31.545Z watch   html_code_identifier_fn

--- public/src/html_code_identifier_fn.mjs
+++ public/src/html_code_identifier_fn.mjs
@@ -1,5 +1,5 @@
 import { html_span_text_font_color } from "./html_span_text_font_color.mjs";
 export function html_code_identifier_fn(parent, name) {
-  let s = html_span_text_font_color(parent, name, "springgreen");
+  let s = html_span_text_font_color(parent, name, "palegreen");
   return s;
 }
