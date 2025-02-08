496df8c7e90912fd3c3044cd8955ba71b67d9ccb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 19:16:14 -0500

    2025-02-08T00:16:13.643Z watch   html_script_acorn

--- public/src/html_script_acorn.mjs
+++ public/src/html_script_acorn.mjs
@@ -6,12 +6,12 @@ export async function html_script_acorn(parent) {
   html_attribute_set(
     c,
     "src",
-    "https://cdnjs.cloudflare.com/ajax/libs/acorn/8.11.3/acorn.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/acorn/8.14.0/acorn.js",
   );
   html_attribute_set(
     c,
     "integrity",
-    "sha512-rlm77cSR5crHjxp0RrQwp9mCVada2Tp0GVzVm/M59Z45daZf7C24VNGVbg58swEnmIR2Oq65qHPr3mZqTn8iag==",
+    "sha512-LmDbUhyR0zKgMllqAf67Ye09qA/UXZ87QiqvJQnKAi9bxkKu42lbiaPaWIvWP3Jj8Qs8BfZKKP6YMXDFGxdv3w==",
   );
   html_attribute_set(c, "crossorigin", "anonymous");
   html_attribute_set(c, "referrerpolicy", "no-referrer");
