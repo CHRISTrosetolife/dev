fc0c32cb4f8cf26b4cabe4619b0f9da4771cd200
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 19:02:00 -0500

    2025-02-08T00:01:59.719Z watch   object_map

--- public/src/object_map.mjs
+++ public/src/object_map.mjs
@@ -4,7 +4,7 @@ export function object_map(s, mapper) {
   let result = {};
   each_object(s, (key, value) => {
     let value_new = mapper(value, key);
-    object_property_set(object, key, value_new);
+    object_property_set(result, key, value_new);
   });
   return result;
 }
