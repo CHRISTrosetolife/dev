df2665bb7b86bfac2de6646ae1adae95047ed9f9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-07 19:06:01 -0500

    2025-02-08T00:06:00.619Z watch   app_code_files_get

--- public/src/app_code_files_get.mjs
+++ public/src/app_code_files_get.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { app_code_local_files_path_latest } from "./app_code_local_files_path_latest.mjs";
 import { http_storage_get } from "./http_storage_get.mjs";
 import { global_files } from "./global_files.mjs";
@@ -10,6 +11,7 @@ export async function app_code_files_get() {
     global_files(),
     async () => {
       let v = await http_storage_get(app_code_local_files_path_latest());
+      let batch_path = object_property_get(v, "batch_path");
       return v;
     },
   );
