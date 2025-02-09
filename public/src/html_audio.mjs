726d2293cd943720381730d3b91b9f917cc49e3a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 17:04:40 -0500

    2025-02-09T22:04:36.815Z watch   html_audio

--- public/src/html_audio.mjs
+++ public/src/html_audio.mjs
@@ -1,8 +1,11 @@
+import { html_button_wait } from "./html_button_wait.mjs";
+import { html_overlay } from "./html_overlay.mjs";
+import { html_interacted } from "./html_interacted.mjs";
 export async function html_audio(src) {
-    if (!html_interacted()) {
-        let overlay = html_overlay();
-        await html_button_wait(overlay, "Press this button to enable audio");
-      }
+  if (!html_interacted()) {
+    let overlay = html_overlay();
+    await html_button_wait(overlay, "Press this button to enable audio");
+  }
   let a = new Audio(src);
   let result = new Promise((resolve) => {
     a.addEventListener("ended", function () {
