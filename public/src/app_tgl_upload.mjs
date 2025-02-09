49ccac64081427d78e4837fa856028d5b0d46e3a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 12:20:53 -0500

    2025-02-09T17:20:52.199Z watch   app_tgl_upload

--- public/src/app_tgl_upload.mjs
+++ public/src/app_tgl_upload.mjs
@@ -1,1 +1,6 @@
-export function app_tgl_upload() {}
+import { app_language_upload_result } from "./app_language_upload_result.mjs";
+export function app_tgl_upload() {
+  let result_new = app_language_upload_result(atoms, {
+    definitions,
+  });
+}
