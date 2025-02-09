8feb1805f1195a4a12b4140920bf861cb9d64dc0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 14:07:29 -0500

    2025-02-09T19:07:23.972Z watch   app_tgl_upload

--- public/src/app_tgl_upload.mjs
+++ public/src/app_tgl_upload.mjs
@@ -15,7 +15,6 @@ import { html_parse_visit_tag_list } from "./html_parse_visit_tag_list.mjs";
 import { html_parse_visit_attribute_value_single } from "./html_parse_visit_attribute_value_single.mjs";
 import { html_parse_visit_id } from "./html_parse_visit_id.mjs";
 import { html_parse_visit_class_single } from "./html_parse_visit_class_single.mjs";
-import { log } from "./log.mjs";
 import { definition_bohol } from "./definition_bohol.mjs";
 import { app_language_group_upload } from "./app_language_group_upload.mjs";
 import { app_language_upload_result } from "./app_language_upload_result.mjs";
@@ -75,10 +74,7 @@ export async function app_tgl_upload() {
       });
     });
   });
-  log({
-    words,
-  });
-  return;
+  return words;
   let from = "tgl";
   let to = "en";
   let result_new = app_language_upload_result(atoms, {
