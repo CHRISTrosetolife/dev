36fdbd86f910f29a097b2dc5b8ce0e0a19e6e74a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 04:23:32 -0500

    2025-02-09T09:23:31.958Z watch   app_code_app_new

--- public/src/app_code_app_new.mjs
+++ public/src/app_code_app_new.mjs
@@ -1,1 +1,5 @@
-export function app_code_app_new(context) {}
+import { fn_name } from "./fn_name.mjs";
+import { app_code_screen_set } from "./app_code_screen_set.mjs";
+export function app_code_app_new(context) {
+  let root = app_code_screen_set(context, fn_name("app_code_app_new"));
+}
