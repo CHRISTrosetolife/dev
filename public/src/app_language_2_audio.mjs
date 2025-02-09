54c9b4dbb743590b75903ccf6d09e6b164e69d31
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 17:17:53 -0500

    2025-02-09T22:17:52.383Z watch   app_language_2_audio

--- public/src/app_language_2_audio.mjs
+++ public/src/app_language_2_audio.mjs
@@ -3,13 +3,13 @@ import { app_language_audio } from "./app_language_audio.mjs";
 import { html_on_click } from "./html_on_click.mjs";
 import { html_span_text_emoji_ear } from "./html_span_text_emoji_ear.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-export function app_language_2_audio(word, language, text_word) {
+export function app_language_2_audio(word, language, text) {
   if (language === "tgl") {
-    let container2 = object_property_get(word, "container");
-    html_span_text_emoji_ear(container2);
-    html_on_click(container2, async () => {
-      await app_language_audio(language, text_word);
+    let container = object_property_get(word, "container");
+    html_span_text_emoji_ear(container);
+    html_on_click(container, async () => {
+      await app_language_audio(language, text);
     });
-    app_language_audio_unawait(language, text_word);
+    app_language_audio_unawait(language, text);
   }
 }
