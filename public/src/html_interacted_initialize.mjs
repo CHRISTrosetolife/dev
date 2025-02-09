6e9a527228ed7cc08cc117db7344f6dbbf806e6d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 16:55:01 -0500

    2025-02-09T21:54:58.505Z watch   html_interacted_initialize

--- public/src/html_interacted_initialize.mjs
+++ public/src/html_interacted_initialize.mjs
@@ -1,3 +1,4 @@
+import { html_on_keydown } from "./html_on_keydown.mjs";
 import { each } from "./each.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { html_document_get } from "./html_document_get.mjs";
@@ -9,7 +10,7 @@ export function html_interacted_initialize() {
   let g = global_function(html_interacted);
   object_property_initialize(g, "initialized", false);
   let start = true;
-  each(list, (item) => {});
+  each([html_on_keydown, html_on_click], (item) => {});
   html_on_click(html_document_get(), () => {
     if (start) {
       object_property_set(g, "initialized", true);
