2dcffc50be7c888ad5b82a9f61cf48a8f1349898
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 15:01:54 -0500

    2025-02-09T20:01:54.150Z watch   bible_words_definitions_atoms

--- public/src/bible_words_definitions_atoms.mjs
+++ public/src/bible_words_definitions_atoms.mjs
@@ -1,6 +1,5 @@
-import { bible_words_spaceify } from "./bible_words_spaceify.mjs";
+import { bible_words_definitions_pairs_to_atoms_inverted } from "./bible_words_definitions_pairs_to_atoms_inverted.mjs";
 import { bible_words_definitions_pairs } from "./bible_words_definitions_pairs.mjs";
-import { object_list_invert } from "./object_list_invert.mjs";
 import { list_to_lookup_key_value_property } from "./list_to_lookup_key_value_property.mjs";
 export async function bible_words_definitions_atoms(language) {
   let { pairs, definitions: definitions_list } =
@@ -10,11 +9,5 @@ export async function bible_words_definitions_atoms(language) {
     "word",
     "definitions",
   );
-  let inverted = object_list_invert(definitions);
-  let atoms = bible_words_spaceify(pairs);
-  return {
-    atoms,
-    definitions,
-    inverted,
-  };
+  return bible_words_definitions_pairs_to_atoms_inverted(definitions, pairs);
 }
