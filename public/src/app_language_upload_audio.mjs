874b700b49b896e54e6d25cb267955a30a2ea605
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 15:47:04 -0500

    2025-02-09T20:47:01.524Z watch   app_language_upload_audio

--- public/src/app_language_upload_audio.mjs
+++ public/src/app_language_upload_audio.mjs
@@ -4,17 +4,17 @@ import { list_wait } from "./list_wait.mjs";
 import { list_any_created } from "./list_any_created.mjs";
 import { audio_upload } from "./audio_upload.mjs";
 import { list_first } from "./list_first.mjs";
-import { list_map_async } from "./list_map_async.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_chunk_each } from "./list_chunk_each.mjs";
 export async function app_language_upload_audio(group, from) {
   await list_chunk_each(group, async function each_chunk(chunk) {
     let mapped = list_map(chunk, async (atom) => {
-      let createds = await list_map_async(atom, async (pair) => {
+      let mapped2 = list_map(atom, async (pair) => {
         let b = list_first(pair);
         let r = await audio_upload(from, b);
         return r;
       });
+      let createds = await list_wait(mapped2);
       return list_any_created(createds);
     });
     let createds = await list_wait(mapped);
