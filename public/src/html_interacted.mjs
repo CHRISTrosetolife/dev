6ad172b3cea83c5730b6897b3b39a081b1e60bd7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 16:56:57 -0500

    2025-02-09T21:56:56.475Z watch   html_interacted

--- public/src/html_interacted.mjs
+++ public/src/html_interacted.mjs
@@ -1,6 +1,7 @@
-import { object_property_initialize } from "./object_property_initialize.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 import { global_function } from "./global_function.mjs";
 export function html_interacted() {
   let g = global_function(html_interacted);
-  let interacted = object_property_initialize(g, "interacted", false);
+  let interacted = object_property_get(g, "interacted");
+  return interacted;
 }
