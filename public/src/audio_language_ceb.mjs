04411f17d1220b5460f0048bbc3f67b78b6f51bd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 15:17:13 -0500

    2025-02-09T20:17:10.700Z watch   audio_language_ceb

--- public/src/audio_language_ceb.mjs
+++ public/src/audio_language_ceb.mjs
@@ -1,23 +1,4 @@
+import { audio_language_tgl } from "./audio_language_tgl.mjs";
 export function audio_language_ceb() {
-  return {
-    language_code: "fil-PH",
-    voices: [
-      {
-        code: "Standard-D",
-        male: true,
-      },
-      {
-        code: "Standard-A",
-        male: false,
-      },
-      {
-        code: "Standard-C",
-        male: true,
-      },
-      {
-        code: "Standard-B",
-        male: false,
-      },
-    ],
-  };
+  return audio_language_tgl();
 }
