e3d3712bc3901ed88a1fa4a1a4312496b8275b02
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 17:06:33 -0500

    2025-02-09T22:06:32.556Z watch   html_document_body

--- public/src/html_document_body.mjs
+++ public/src/html_document_body.mjs
@@ -1,8 +1,9 @@
 import { html_document_get } from "./html_document_get.mjs";
 import { html_id } from "./html_id.mjs";
 export function html_document_body() {
+  let b = html_document_get().body;
   let result = {
-    element: html_document_get().body,
+    element: b,
   };
   html_id(result);
   return result;
