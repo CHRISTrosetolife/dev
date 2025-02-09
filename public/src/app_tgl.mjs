75a0e31059f0b25f8c97f48fc7bc84b67a0be72a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 12:18:24 -0500

    2025-02-09T17:18:23.877Z watch   app_tgl

--- public/src/app_tgl.mjs
+++ public/src/app_tgl.mjs
@@ -1,4 +1,6 @@
-import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-export function app_tgl() {
-  let root = html_style_default_initialize();
+import { app_language_2 } from "./app_language_2.mjs";
+export async function app_tgl() {
+  let language_learn = "tgl";
+  let language_fluent = "en";
+  await app_language_2(app_tgl, language_learn, language_fluent);
 }
