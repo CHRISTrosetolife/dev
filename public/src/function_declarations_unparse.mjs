55dd878773cb47269052e93a1f22db5e905a3360
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 11:11:26 -0500

    2025-02-09T16:11:24.430Z watch   function_declarations_unparse

--- public/src/function_declarations_unparse.mjs
+++ public/src/function_declarations_unparse.mjs
@@ -1,9 +1,10 @@
 import { js_unparse } from "./js_unparse.mjs";
 import { list_map } from "./list_map.mjs";
 import { function_declaration } from "./function_declaration.mjs";
-import { list_map_async } from "./list_map_async.mjs";
+import { list_wait } from "./list_wait.mjs";
 export async function function_declarations_unparse(dependency_names) {
-  let ds = await list_map_async(dependency_names, function_declaration);
-  let us = list_map(ds, js_unparse);
+  let ds = list_map(dependency_names, function_declaration);
+  let ws = await list_wait(ds);
+  let us = list_map(ws, js_unparse);
   return us;
 }
