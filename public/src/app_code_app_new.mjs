c5af7c7ec64bcad73d5a006b9a45e9d9377f9bd3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 04:31:39 -0500

    2025-02-09T09:31:37.356Z watch   app_code_app_new

--- public/src/app_code_app_new.mjs
+++ public/src/app_code_app_new.mjs
@@ -1,3 +1,8 @@
+import { app_code } from "./app_code.mjs";
+import { storage_local_set } from "./storage_local_set.mjs";
+import { html_condition_letters_numbers_underscores } from "./html_condition_letters_numbers_underscores.mjs";
+import { html_condition_empty_not } from "./html_condition_empty_not.mjs";
+import { html_input_validated_button } from "./html_input_validated_button.mjs";
 import { html_condition_letters_numbers_underscores_spaces } from "./html_condition_letters_numbers_underscores_spaces.mjs";
 import { html_input_validated_focus } from "./html_input_validated_focus.mjs";
 import { fn_name } from "./fn_name.mjs";
@@ -7,4 +12,14 @@ export function app_code_app_new(context) {
   let st = html_input_validated_focus(root, "Search query", [
     html_condition_letters_numbers_underscores_spaces(),
   ]);
+  html_input_validated_button(
+    root,
+    "Username",
+    [html_condition_empty_not(), html_condition_letters_numbers_underscores()],
+    "Save",
+    function on_submit(value) {
+      storage_local_set(app_code, "username", value);
+      next();
+    },
+  );
 }
