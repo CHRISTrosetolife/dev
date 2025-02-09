c932018c159d6dac99a061387caf419007dad817
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 20:43:27 -0500

    2025-02-09T01:43:27.369Z watch   string_html_escape

--- public/src/string_html_escape.mjs
+++ public/src/string_html_escape.mjs
@@ -1,1 +1,8 @@
-export function string_html_escape(s) {}
+export function string_html_escape(s) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
