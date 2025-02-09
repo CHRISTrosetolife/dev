43fd4e6946a49d11ea68eb2908e428dcc576bdde
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 12:33:46 -0500

    2025-02-09T17:33:45.122Z watch   app_tgl_upload

--- public/src/app_tgl_upload.mjs
+++ public/src/app_tgl_upload.mjs
@@ -1,9 +1,11 @@
+import { html_parse_visit_class_single } from "./html_parse_visit_class_single.mjs";
 import { log } from "./log.mjs";
 import { definition_bohol } from "./definition_bohol.mjs";
 import { app_language_group_upload } from "./app_language_group_upload.mjs";
 import { app_language_upload_result } from "./app_language_upload_result.mjs";
 export async function app_tgl_upload() {
   let p = await definition_bohol("the");
+  html_parse_visit_class_single();
   log({
     p,
   });
