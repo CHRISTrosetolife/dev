87d61bf6b31007a96c87c8f6ba8bf4a56c75a80a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 11:03:27 -0500

    2025-02-09T16:03:27.315Z watch   app_code_refresh_app_new

--- public/src/app_code_refresh_app_new.mjs
+++ public/src/app_code_refresh_app_new.mjs
@@ -13,7 +13,7 @@ import { list_concat } from "./list_concat.mjs";
 export async function app_code_refresh_app_new(context) {
   let root = app_code_screen_set(context, fn_name("app_code_refresh_app_new"));
   let f_name = fn_name("apps_list");
-  let d = await function_declarations_unparse(f_name);
+  let f_source = await function_declarations_unparse([f_name]);
   let existing = apps_list();
   let message = "not be the name of an existing app";
   let ib = html_input_validated_button(
