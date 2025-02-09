4a9b46382c83aa585edc3496fb046066a15193ca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 20:45:16 -0500

    2025-02-09T01:45:13.390Z watch   app_code_refresh_html

--- public/src/app_code_refresh_html.mjs
+++ public/src/app_code_refresh_html.mjs
@@ -9,7 +9,7 @@ import { app_code_screen_set } from "./app_code_screen_set.mjs";
 import { html_load } from "./html_load.mjs";
 export async function app_code_refresh_html(context) {
   await html_load(async () => {
-    let root = app_code_screen_set(context, fn_name("app_code_refresh_htmls"));
+    let root = app_code_screen_set(context, fn_name("app_code_refresh_html"));
     await app_code_button_html_files(context);
     let html_selected = storage_local_get_context(context, "html_selected");
     let contents = await app_code_file_contents(html_selected);
