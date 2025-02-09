33961a05461678023d22fa89e272171138ce0d18
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 17:10:31 -0500

    2025-02-09T22:10:28.436Z watch   html_document_body

--- public/src/html_document_body.mjs
+++ public/src/html_document_body.mjs
@@ -1,7 +1,7 @@
 import { html_element_wrap } from "./html_element_wrap.mjs";
 import { html_document_get } from "./html_document_get.mjs";
 export function html_document_body() {
-  let b = html_document_get().body;
+  let b = html_document_get().element.body;
   let result = html_element_wrap(b);
   return result;
 }
