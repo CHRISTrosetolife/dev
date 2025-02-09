85dbab30457598c423234aeb1a432e07e2b42ed7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 20:36:31 -0500

    2025-02-09T01:36:30.548Z watch   app_code_refresh_html

--- public/src/app_code_refresh_html.mjs
+++ public/src/app_code_refresh_html.mjs
@@ -1,9 +1,7 @@
+import { html_pre_text } from "./html_pre_text.mjs";
+import { app_code_file_contents } from "./app_code_file_contents.mjs";
 import { storage_local_get_context } from "./storage_local_get_context.mjs";
-import { app_code_files_get } from "./app_code_files_get.mjs";
 import { app_code_button_html_files } from "./app_code_button_html_files.mjs";
-import { storage_local_set_context } from "./storage_local_set_context.mjs";
-import { html_button } from "./html_button.mjs";
-import { each } from "./each.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_code_screen_set } from "./app_code_screen_set.mjs";
 import { html_load } from "./html_load.mjs";
@@ -11,11 +9,8 @@ export async function app_code_refresh_html(context) {
   await html_load(async () => {
     let root = app_code_screen_set(context, fn_name("app_code_refresh_htmls"));
     await app_code_button_html_files();
-    let files = await app_code_files_get();
-    storage_local_set_context(context, "html_selected", h);
     let html_selected = storage_local_get_context(context, "html_selected");
-    each(file_paths_html, (h) => {
-      html_button(root, h, () => {});
-    });
+    let contents = await app_code_file_contents(html_selected);
+    html_pre_text(root, contents);
   });
 }
