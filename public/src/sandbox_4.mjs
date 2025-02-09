e22d04595de873d67a6e3e35c59cf3108d1de83b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 12:26:16 -0500

    2025-02-09T17:26:16.173Z watch   sandbox_4

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -1,13 +1,9 @@
 import { definition_bohol } from "./definition_bohol.mjs";
-import { log } from "./log.mjs";
 import { english_words_dictionary } from "./english_words_dictionary.mjs";
 import { each_log_async } from "./each_log_async.mjs";
 export async function sandbox_4() {
   let filtered2 = await english_words_dictionary();
   await each_log_async(filtered2, async (word) => {
     let result = await definition_bohol(word);
-    log({
-      result,
-    });
   });
 }
