166fac4e5931623eedd2cb5d285e790211d1fe89
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 12:58:39 -0500

    2025-02-09T17:58:39.354Z watch   app_tgl_upload

--- public/src/app_tgl_upload.mjs
+++ public/src/app_tgl_upload.mjs
@@ -49,6 +49,7 @@ export async function app_tgl_upload() {
     let td_en = list_get(row_tds, en_index);
     let a_en = html_parse_visit_tag_single(td_en, "a");
     let word_en = html_parse_text(a_en);
+    word_en = string_trim_whitespace(word_en);
     let td_tgl = list_get(row_tds, tgl_index);
     let as_tgl = html_parse_visit_tag_list(td_tgl, "a");
     list_map(as_tgl, (a_tgl) => {
