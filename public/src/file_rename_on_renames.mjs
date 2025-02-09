ff934c7d0c1a8289283690bb14092e2102232127
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 10:53:52 -0500

    2025-02-09T15:53:48.344Z watch   file_rename_on_renames

--- public/src/file_rename_on_renames.mjs
+++ public/src/file_rename_on_renames.mjs
@@ -1,7 +1,7 @@
 import { file_rename } from "./file_rename.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { global_function_initialize } from "./global_function_initialize.mjs";
-export function function_rename_on_renames() {
+export function file_rename_on_renames() {
   let g = global_function_initialize(file_rename, () => ({
     on_renames: [],
   }));
