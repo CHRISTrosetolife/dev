bb9b656b9c880019744d203c09b7d9e88322666a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 16:52:48 -0500

    2025-02-09T21:52:45.856Z watch   html_document_body

--- public/src/html_document_body.mjs
+++ public/src/html_document_body.mjs
@@ -1,3 +1,4 @@
+import { html_document_get } from "./html_document_get.mjs";
 import { html_id } from "./html_id.mjs";
 export function html_document_body() {
   let result = {
--- public/src/html_interacted_initialize.mjs
+++ public/src/html_interacted_initialize.mjs
@@ -1,7 +1,9 @@
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { global_function } from "./global_function.mjs";
 import { html_interacted } from "./html_interacted.mjs";
+import { html_on_click } from "./html_on_click.mjs";
+import { html_document_body } from "./html_document_body.mjs";
 export function html_interacted_initialize() {
   let g = global_function(html_interacted);
-  object_property_initialize(g, "initialized", false);
+  object_property_initialize(g, "initialized", false);html_on_click(html_document_get())
 }
