74d56d18ed4fca1d0a168159371f090adbdbdc9c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 15:44:58 -0500

    2025-02-09T20:44:57.705Z watch   audio_upload

--- public/src/gcloud_tts.mjs
+++ public/src/gcloud_tts.mjs
@@ -11,6 +11,7 @@ export async function gcloud_tts(
   text,
   output_path,
 ) {
+    assert(string_empty_not_is, [text]);
   if (await file_exists(output_path)) {
     return {
       created: false,
