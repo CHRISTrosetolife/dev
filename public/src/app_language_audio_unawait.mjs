3fcc4b3f0fdc7d65fbbb253e8d3c6677b148263f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 16:36:01 -0500

    2025-02-09T21:35:58.240Z watch   app_language_word_button

---
+++ public/src/app_language_audio_unawait.mjs
@@ -0,0 +1,5 @@
+import { app_language_audio } from "./app_language_audio.mjs";
+import { unawait } from "./unawait.mjs";
+export function app_language_audio_unawait(language, text) {
+  unawait(async () => await app_language_audio(language, text));
+}
--- public/src/app_language_word_button.mjs
+++ public/src/app_language_word_button.mjs
@@ -1,12 +1,11 @@
-import { unawait } from "./unawait.mjs";
+import { app_language_audio_unawait } from "./app_language_audio_unawait.mjs";
 import { app_language_word_button_audio_none } from "./app_language_word_button_audio_none.mjs";
-import { app_language_audio } from "./app_language_audio.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function app_language_word_button(root, language, text) {
   assert_arguments_length(arguments, 3);
   let button = app_language_word_button_audio_none(root, language, text);
   if (false) {
-    unawait(async () => await app_language_audio(language, text));
+    app_language_audio_unawait(language, text);
   }
   return button;
 }
