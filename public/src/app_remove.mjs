bd1674c2c8cc287f1a72f32fd641fed463bb7caf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 12:14:16 -0500

    2025-02-09T17:14:14.589Z watch   app_remove

--- public/src/app_remove.mjs
+++ public/src/app_remove.mjs
@@ -1,4 +1,6 @@
+import { function_delete } from "./function_delete.mjs";
 import { app_identifier } from "./app_identifier.mjs";
-export function app_remove(name) {
+export async function app_remove(name) {
   let name_prefixed = app_identifier(name);
+  await function_delete(name_prefixed);
 }
