e8ed5a1e3b87614de96ae92b82ee8b0ced811f8d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 20:30:31 -0500

    2025-02-09T01:30:31.037Z watch   app_code_refresh_htmls

--- public/src/app_code_refresh_htmls.mjs
+++ public/src/app_code_refresh_htmls.mjs
@@ -1,3 +1,5 @@
+import { html_button } from "./html_button.mjs";
+import { each } from "./each.mjs";
 import { app_code_button_menu_app } from "./app_code_button_menu_app.mjs";
 import { app_code_html_file_paths_get } from "./app_code_html_file_paths_get.mjs";
 import { fn_name } from "./fn_name.mjs";
@@ -8,5 +10,7 @@ export async function app_code_refresh_htmls(context) {
     let root = app_code_screen_set(context, fn_name("app_code_refresh_htmls"));
     app_code_button_menu_app(context);
     let file_paths_html = await app_code_html_file_paths_get();
+    each(list, (item) => {});
+    html_button(parent, "HTML files", () => {});
   });
 }
