eacc8cecb1c4a9f755c0d18a49a2eb0347c1f972
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 19:30:54 -0500

    2025-02-09T00:30:52.580Z watch   app_code_refresh_menu

--- public/src/app_code_refresh_menu.mjs
+++ public/src/app_code_refresh_menu.mjs
@@ -1,3 +1,5 @@
+import { app_code_files_get } from "./app_code_files_get.mjs";
+import { html_load } from "./html_load.mjs";
 import { app_code_refresh_functions_recent } from "./app_code_refresh_functions_recent.mjs";
 import { app_code_functions_recent_get } from "./app_code_functions_recent_get.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
@@ -12,27 +14,30 @@ import { storage_local_remove } from "./storage_local_remove.mjs";
 import { app_code } from "./app_code.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { app_code_refresh_username } from "./app_code_refresh_username.mjs";
-export function app_code_refresh_menu(context) {
-  let root = app_code_screen_set(context, fn_name("app_code_refresh_menu"));
-  app_code_button_functions_search(context);
-  let functions_recent = app_code_functions_recent_get(context);
-  if (list_empty_not_is(functions_recent)) {
-    html_button(root, "Recent functions", () => {
-      app_code_refresh_functions_recent(context);
-    });
-  }
-  if (app_code_username_exists()) {
-    html_button(
-      root,
-      string_combine_multiple(["Log out: ", app_code_username_get()]),
-      () => {
-        storage_local_remove(app_code, "username");
-        app_code_refresh_main(context);
-      },
-    );
-  } else {
-    html_button(root, "Log in", () => {
-      app_code_refresh_username(context);
-    });
-  }
+export async function app_code_refresh_menu(context) {
+  await html_load(async () => {
+    let root = app_code_screen_set(context, fn_name("app_code_refresh_menu"));
+    app_code_button_functions_search(context);
+    let functions_recent = app_code_functions_recent_get(context);
+    if (list_empty_not_is(functions_recent)) {
+      html_button(root, "Recent functions", () => {
+        app_code_refresh_functions_recent(context);
+      });
+    }
+    if (app_code_username_exists()) {
+      html_button(
+        root,
+        string_combine_multiple(["Log out: ", app_code_username_get()]),
+        () => {
+          storage_local_remove(app_code, "username");
+          app_code_refresh_main(context);
+        },
+      );
+    } else {
+      html_button(root, "Log in", () => {
+        app_code_refresh_username(context);
+      });
+    }
+    let files = await app_code_files_get();
+  });
 }
