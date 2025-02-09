2010bc6dfa2847e0e803d1432bb161206c3511b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 12:27:54 -0500

    2025-02-09T17:27:52.524Z watch   app_tgl_upload

--- public/src/app_tgl_upload.mjs
+++ public/src/app_tgl_upload.mjs
@@ -2,7 +2,7 @@ import { definition_bohol } from "./definition_bohol.mjs";
 import { app_language_group_upload } from "./app_language_group_upload.mjs";
 import { app_language_upload_result } from "./app_language_upload_result.mjs";
 export async function app_tgl_upload() {
-  return await definition_bohol("the");
+  let p = await definition_bohol("the");
   return;
   let from = "tgl";
   let to = "en";
