a872ec27165d4a384612a465d321ae0ffbdbdc35
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 18:14:51 -0500

    2025-02-09T23:14:50.818Z watch   app_language_2_refresh_learn

--- public/src/app_language_2_refresh_learn.mjs
+++ public/src/app_language_2_refresh_learn.mjs
@@ -287,7 +287,7 @@ export async function app_language_2_refresh_learn(context) {
           correct = false;
           html_style_wrong(b);
           await app_language_2_answer_delay();
-          html_remove();
+          html_remove(choice_div);
         }
       });
       each([b, b2], (bi) => {
