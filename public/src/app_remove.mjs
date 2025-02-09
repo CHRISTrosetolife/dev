81048608078837963eeb46d9e42b62efd6f9d318
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 12:16:02 -0500

    2025-02-09T17:16:01.625Z watch   app_remove

--- public/src/app_remove.mjs
+++ public/src/app_remove.mjs
@@ -1,3 +1,4 @@
+import { each_async } from "./each_async.mjs";
 import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
 import { app_html_path } from "./app_html_path.mjs";
 import { function_delete } from "./function_delete.mjs";
@@ -5,5 +6,6 @@ import { app_identifier } from "./app_identifier.mjs";
 export async function app_remove(name) {
   let name_prefixed = app_identifier(name);
   await function_delete(name_prefixed);
+  await each_async(list, async (item) => {});
   await file_delete_if_exists(app_html_path(name));
 }
