d11a346530a350816c35a1312adb1966f04c3429
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-08 20:38:55 -0500

    2025-02-09T01:38:51.689Z watch   app_code_button_html_files

--- public/src/app_code_button_html_files.mjs
+++ public/src/app_code_button_html_files.mjs
@@ -1,3 +1,4 @@
+import { app_code_refresh_htmls } from "./app_code_refresh_htmls.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { html_button } from "./html_button.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
@@ -6,6 +7,8 @@ export async function app_code_button_html_files(context) {
   let root = object_property_get(context, "root");
   let file_paths_html = await app_code_html_file_paths_get();
   if (list_empty_not_is(file_paths_html)) {
-    html_button(root, "HTML files", () => {});
+    html_button(root, "HTML files", async () => {
+      await app_code_refresh_htmls(context);
+    });
   }
 }
