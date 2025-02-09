6d21a4d8a6e756e76e5f805f243eef46856a185e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 04:31:20 -0500

    2025-02-09T09:31:19.842Z watch   app_code_refresh_username

--- public/src/app_code_refresh_username.mjs
+++ public/src/app_code_refresh_username.mjs
@@ -1,12 +1,7 @@
-import { html_input_validated_button } from "./html_input_validated_button.mjs";
 import { html_button } from "./html_button.mjs";
-import { html_condition_letters_numbers_underscores } from "./html_condition_letters_numbers_underscores.mjs";
-import { html_condition_empty_not } from "./html_condition_empty_not.mjs";
 import { app_code_screen_set } from "./app_code_screen_set.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_code_refresh_menu } from "./app_code_refresh_menu.mjs";
-import { app_code } from "./app_code.mjs";
-import { storage_local_set } from "./storage_local_set.mjs";
 import { html_hr } from "./html_hr.mjs";
 import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
 export function app_code_refresh_username(context) {
@@ -16,16 +11,6 @@ export function app_code_refresh_username(context) {
     "If you do not want to store data for later, you may skip this",
     "At this time there are no passwords. You should choose a unique username or someone could overwrite your data.",
   ]);
-  html_input_validated_button(
-    root,
-    "Username",
-    [html_condition_empty_not(), html_condition_letters_numbers_underscores()],
-    "Save",
-    function on_submit(value) {
-      storage_local_set(app_code, "username", value);
-      next();
-    },
-  );
   html_hr(root);
   html_button(root, "Skip", () => {
     next();
