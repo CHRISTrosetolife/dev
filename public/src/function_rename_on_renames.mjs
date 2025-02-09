cab225780942f65510de1b61ac89be9376058522
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 10:53:25 -0500

    2025-02-09T15:53:24.484Z watch   function_rename_on_renames

--- public/src/function_rename_on_renames.mjs
+++ public/src/function_rename_on_renames.mjs
@@ -1,8 +1,8 @@
+import { file_rename } from "./file_rename.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { function_rename } from "./function_rename.mjs";
 import { global_function_initialize } from "./global_function_initialize.mjs";
 export function function_rename_on_renames() {
-  let g = global_function_initialize(function_rename, () => ({
+  let g = global_function_initialize(file_rename, () => ({
     on_renames: [],
   }));
   let on_renames = object_property_get(g, "on_renames");
