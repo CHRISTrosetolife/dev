53bab4f587297a665a16b7fa9661208c2f55198b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 10:16:11 -0500

    2025-02-09T15:16:10.630Z watch   file_overwrite_generic

--- public/src/file_overwrite_generic.mjs
+++ public/src/file_overwrite_generic.mjs
@@ -7,7 +7,7 @@ export async function file_overwrite_generic(file_name, data, encoding) {
   if (web_is()) {
     let { files } = global_get();
     let existing = object_property_initialize(files, file_name, {});
-    object_property_set(files, file_name, data);
+    object_property_set(existing, "contents", data);
   } else {
     let fs = await import("fs");
     await folder_parent_exists_ensure(file_name);
