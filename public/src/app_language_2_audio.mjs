ed470992de4d30b8280d578ca70a55ea87ca9f31
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 17:19:15 -0500

    2025-02-09T22:19:12.690Z pd app_language_2_audio 1

--- public/src/app_language_2_audio.mjs
+++ public/src/app_language_2_audio.mjs
@@ -3,7 +3,7 @@ import { app_language_audio } from "./app_language_audio.mjs";
 import { html_on_click } from "./html_on_click.mjs";
 import { html_span_text_emoji_ear } from "./html_span_text_emoji_ear.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-export function app_language_2_audio(word, langua2ge) {
+export function app_language_2_audio(word) {
   let text = object_property_get(word, "text");
   let language = object_property_get(word, "language");
   if (language === "tgl") {
