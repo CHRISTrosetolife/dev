4869c498fd00491f71ebfac8431c50b1640542a5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 12:41:52 -0500

    2025-02-09T17:41:51.780Z watch   app_tgl_upload

--- public/src/app_tgl_upload.mjs
+++ public/src/app_tgl_upload.mjs
@@ -1,6 +1,6 @@
+import { list_first_remaining } from "./list_first_remaining.mjs";
 import { assert } from "./assert.mjs";
 import { list_map } from "./list_map.mjs";
-import { list_first } from "./list_first.mjs";
 import { html_parse_visit_tag_list } from "./html_parse_visit_tag_list.mjs";
 import { html_parse_visit_attribute_value_single } from "./html_parse_visit_attribute_value_single.mjs";
 import { html_parse_visit_id } from "./html_parse_visit_id.mjs";
@@ -22,7 +22,7 @@ export async function app_tgl_upload() {
     "100%",
   );
   let trs = html_parse_visit_tag_list(table, "tr");
-  let tr_first = list_first(trs);
+  let tr_first = list_first_remaining(trs);
   let td = html_parse_visit_tag_list(tr_first, "td");
   let languages = list_map(td, html_parse_text);
   let en = "English";
