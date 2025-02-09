9a4a52e7419d4e95ba2f84f261d0cac45c234f00
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 18:22:29 -0500

    2025-02-09T23:22:26.844Z watch   app_language_2_refresh_learn

--- public/src/app_language_2_refresh_learn.mjs
+++ public/src/app_language_2_refresh_learn.mjs
@@ -282,7 +282,6 @@ export async function app_language_2_refresh_learn(context) {
         html_style_wrong(button);
         await app_language_2_answer_delay();
         html_remove(choice_div);
-        html_remove(b);
       }
       async function on_answer_match(button) {
         decrease_wait();
