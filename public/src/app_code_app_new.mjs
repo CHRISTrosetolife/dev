d1e8c41b64505095c1d65d4d077ea4bec3a00538
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 04:32:20 -0500

    2025-02-09T09:32:17.058Z watch   app_code_app_new

--- public/src/app_code_app_new.mjs
+++ public/src/app_code_app_new.mjs
@@ -1,18 +1,15 @@
+import { object_property_get } from "./object_property_get.mjs";
+import { html_focus } from "./html_focus.mjs";
 import { app_code } from "./app_code.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
 import { html_condition_letters_numbers_underscores } from "./html_condition_letters_numbers_underscores.mjs";
 import { html_condition_empty_not } from "./html_condition_empty_not.mjs";
 import { html_input_validated_button } from "./html_input_validated_button.mjs";
-import { html_condition_letters_numbers_underscores_spaces } from "./html_condition_letters_numbers_underscores_spaces.mjs";
-import { html_input_validated_focus } from "./html_input_validated_focus.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_code_screen_set } from "./app_code_screen_set.mjs";
 export function app_code_app_new(context) {
   let root = app_code_screen_set(context, fn_name("app_code_app_new"));
-  let st = html_input_validated_focus(root, "Search query", [
-    html_condition_letters_numbers_underscores_spaces(),
-  ]);
-  html_input_validated_button(
+  let ib = html_input_validated_button(
     root,
     "Username",
     [html_condition_empty_not(), html_condition_letters_numbers_underscores()],
@@ -22,4 +19,5 @@ export function app_code_app_new(context) {
       next();
     },
   );
+  html_focus(object_property_get(object, "property_name"));
 }
