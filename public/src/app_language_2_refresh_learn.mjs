942927a3f3005258be04904be747d8c35af6b412
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 16:42:17 -0500

    2025-02-09T21:42:15.809Z watch   app_language_2_refresh_learn

--- public/src/app_language_2_refresh_learn.mjs
+++ public/src/app_language_2_refresh_learn.mjs
@@ -1,3 +1,4 @@
+import { app_language_audio } from "./app_language_audio.mjs";
 import { html_on_click } from "./html_on_click.mjs";
 import { html_span_text_emoji_ear } from "./html_span_text_emoji_ear.mjs";
 import { app_language_audio_unawait } from "./app_language_audio_unawait.mjs";
@@ -290,7 +291,9 @@ export async function app_language_2_refresh_learn(context) {
     if (language === "tgl") {
       let container2 = object_property_get(word, "container");
       html_span_text_emoji_ear(container2);
-      html_on_click(container2, () => {});
+      html_on_click(container2, async () => {
+        await app_language_audio(language, text_word);
+      });
       app_language_audio_unawait(language, text_word);
     }
   }
