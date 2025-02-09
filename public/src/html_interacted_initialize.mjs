b78e70364dcbd426eedcec05b3ed77cee658865b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 16:53:02 -0500

    2025-02-09T21:53:01.538Z watch   html_interacted_initialize

--- public/src/html_interacted_initialize.mjs
+++ public/src/html_interacted_initialize.mjs
@@ -6,5 +6,5 @@ import { html_on_click } from "./html_on_click.mjs";
 export function html_interacted_initialize() {
   let g = global_function(html_interacted);
   object_property_initialize(g, "initialized", false);
-  html_on_click(html_document_get());
+  html_on_click(html_document_get(), () => {});
 }
