a1c54dbd51b02970a53ed80b273284e3e50ca1a1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 18:29:20 -0500

    2025-02-10T23:29:18.852Z watch   app_el_upload

--- public/src/app_el_upload.mjs
+++ public/src/app_el_upload.mjs
@@ -1,10 +1,12 @@
+import { bible_interlinear_each_token_strong_number } from "./bible_interlinear_each_token_strong_number.mjs";
 import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
 import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
 import { app_el_alphabet_pairs } from "./app_el_alphabet_pairs.mjs";
 export async function app_el_upload() {
   let from = "el";
   let to = "en";
-  await bible_interlinear_cache_new();
+  let books = await bible_interlinear_cache_new();
+  bible_interlinear_each_token_strong_number(books, la);
   let pairs = app_el_alphabet_pairs();
   return await app_language_2_upload_pairs(pairs, from, to);
 }
