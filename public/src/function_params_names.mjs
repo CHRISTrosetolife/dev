eac3465aec107908148021e4903bd87d6fd1a010
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 13:30:44 -0500

    2025-02-10T18:30:43.349Z watch   function_params_names

--- public/src/function_params_names.mjs
+++ public/src/function_params_names.mjs
@@ -2,6 +2,6 @@ import { function_params } from "./function_params.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 export async function function_params_names(function_name) {
   let params = await function_params(function_name);
-  let param_names = list_map_property(params, "limit");
+  let param_names = list_map_property(params, "name");
   return param_names;
 }
