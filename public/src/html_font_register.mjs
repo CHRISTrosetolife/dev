bf3e28d778af00f5ce1a085bcff58df1e7f9e4ef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 17:31:51 -0500

    2025-02-10T22:31:49.962Z watch   html_font_register

--- public/src/html_font_register.mjs
+++ public/src/html_font_register.mjs
@@ -1,1 +1,16 @@
-export function html_font_register() {}
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function html_font_register() {
+  let style = document.createElement("style");
+  style.type = "text/css";
+  let fontFaceRule = string_combine_multiple([
+    "\n      @font-face {\n        font-family: '",
+    fontName,
+    "';\n        src: url('",
+    fontPath,
+    ".woff2') format('woff2'),\n             url('",
+    fontPath,
+    ".woff') format('woff');\n        font-weight: normal;\n        font-style: normal;\n      }\n    ",
+  ]);
+  style.innerHTML = fontFaceRule;
+  document.head.appendChild(style);
+}
