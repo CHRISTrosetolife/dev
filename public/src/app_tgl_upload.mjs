fc846d552e2e6d321d5661eebe30d6e49b0cc19e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 15:42:18 -0500

    2025-02-10T20:42:17.563Z watch   app_tgl_upload

--- public/src/app_tgl_upload.mjs
+++ public/src/app_tgl_upload.mjs
@@ -17,5 +17,6 @@ export async function app_tgl_upload() {
   );
   let atoms = bible_words_spaceify(pairs);
   await app_language_upload_audio(atoms, from);
+  return;
   await app_language_2_upload_atoms_definitions(atoms, definitions, from, to);
 }
