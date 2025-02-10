d01396813b845f014aaab3174a04c7f1cfb9cdff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 18:42:42 -0500

    2025-02-10T23:42:40.522Z watch   bible_interlinear_definition

--- public/src/bible_interlinear_definition.mjs
+++ public/src/bible_interlinear_definition.mjs
@@ -1,6 +1,6 @@
 import { bible_interlinear_strongs_cache } from "./bible_interlinear_strongs_cache.mjs";
 import { bible_interlinear_definition_generic } from "./bible_interlinear_definition_generic.mjs";
-import { list_map_property } from "./list_map_property.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 export async function bible_interlinear_definition(language, strong) {
   let label = "Short Definition:";
   let html_lambda = bible_interlinear_strongs_cache;
@@ -10,5 +10,5 @@ export async function bible_interlinear_definition(language, strong) {
     strong,
     [label],
   );
-  return list_map_property(ds, "definition");
+  return object_property_get(ds, "definition");
 }
