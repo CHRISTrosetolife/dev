981847afbde217057ee974b1cb2280c84f0c6a55
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 18:35:53 -0500

    2025-02-10T23:35:52.299Z watch   app_el_upload

--- public/src/app_el_upload.mjs
+++ public/src/app_el_upload.mjs
@@ -1,3 +1,5 @@
+import { list_map } from "./list_map.mjs";
+import { list_take } from "./list_take.mjs";
 import { list_sort_map } from "./list_sort_map.mjs";
 import { list_second } from "./list_second.mjs";
 import { list_adder } from "./list_adder.mjs";
@@ -7,6 +9,7 @@ import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
 import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
 import { object_property_increment } from "./object_property_increment.mjs";
 import { list_reverse } from "./list_reverse.mjs";
+import { list_first } from "./list_first.mjs";
 export async function app_el_upload() {
   let from = "el";
   let to = "en";
@@ -22,6 +25,8 @@ export async function app_el_upload() {
   });
   list_sort_map(pairs, (p) => list_second(p));
   list_reverse(pairs);
+  let taken = list_take(pairs, 50);
+  let mapped = list_map(taken, list_first);
   return pairs;
   return await app_language_2_upload_pairs(pairs, from, to);
 }
