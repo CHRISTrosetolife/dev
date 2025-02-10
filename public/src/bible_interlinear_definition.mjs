142b76060956d9d40f4d836aec58bf71fa1d5ed7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 18:20:05 -0500

    2025-02-10T23:20:04.436Z watch   bible_interlinear_definition

--- public/src/bible_interlinear_definition.mjs
+++ public/src/bible_interlinear_definition.mjs
@@ -1,26 +1,12 @@
-import { list_find } from "./list_find.mjs";
-import { equal } from "./equal.mjs";
-import { assert } from "./assert.mjs";
-import { string_trim_whitespace } from "./string_trim_whitespace.mjs";
-import { html_parse_text } from "./html_parse_text.mjs";
-import { html_parse_visit_class_list } from "./html_parse_visit_class_list.mjs";
-import { html_parse_visit_id } from "./html_parse_visit_id.mjs";
-import { html_parse } from "./html_parse.mjs";
-import { bible_interlinear_strongs_cache } from "./bible_interlinear_strongs_cache.mjs";
+import { bible_interlinear_definition_generic } from "./bible_interlinear_definition_generic.mjs";
+import { bible_interlinear_strongs_cache_hub } from "./bible_interlinear_strongs_cache_hub.mjs";
 export async function bible_interlinear_definition(language, strong) {
-  let html = await bible_interlinear_strongs_cache(language, strong);
-  let p = await html_parse(html);
-  let leftbox = html_parse_visit_id(p, "leftbox");
-  let tophdg = html_parse_visit_class_list(leftbox, "tophdg");
-  let definition = list_find(tophdg, (t) => {
-    let t_text = html_parse_text(t);
-    t_text = string_trim_whitespace(t_text);
-    return equal(t_text, "Short Definition:");
-  });
-  let definition_text = html_parse_text(definition);
-  definition_text = string_trim_whitespace(definition_text);
-  assert(equal, [definition_text, "Short Definition:"]);
-  let { next } = definition;
-  let next_text = html_parse_text(next);
-  return next_text;
+  let label = "Short Definition:";
+  let html_lambda = bible_interlinear_strongs_cache_hub;
+  return await bible_interlinear_definition_generic(
+    html_lambda,
+    language,
+    strong,
+    label,
+  );
 }
