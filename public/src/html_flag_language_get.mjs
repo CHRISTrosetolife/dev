d77f593a499d13958eea0764efa209b95a5726e7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 17:18:34 -0500

    2025-02-10T22:18:31.800Z watch   html_flag_language_get

--- public/src/html_flag_language_get.mjs
+++ public/src/html_flag_language_get.mjs
@@ -1,7 +1,8 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { html_flag_us } from "./html_flag_us.mjs";
 import { html_flag_ph } from "./html_flag_ph.mjs";
-export function html_flag_language_get(language) {'https://flagicons.lipis.dev/'
+export function html_flag_language_get(language) {
+  "https://flagicons.lipis.dev/";
   let languages_to_flags = {
     ceb: html_flag_ph(),
     tgl: html_flag_ph(),
