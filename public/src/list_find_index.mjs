965ad86f3ca6eadeb001145ac4b6bd4b5c28dcc9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 12:39:36 -0500

    2025-02-10T17:39:33.214Z watch   list_find_index

--- public/src/list_find_index.mjs
+++ public/src/list_find_index.mjs
@@ -1,7 +1,7 @@
 import { list_index } from "./list_index.mjs";
 import { list_find } from "./list_find.mjs";
-export function list_find_index(keys_lower, predicate) {
-  let book_lower = list_find(keys_lower, predicate);
-  let index = list_index(keys_lower, book_lower);
+export function list_find_index(list, predicate) {
+  let item = list_find(list, predicate);
+  let index = list_index(list, item);
   return index;
 }
