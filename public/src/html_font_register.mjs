73964480edadf3647a2fc5b6d7bf0a1989e381e1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 17:33:45 -0500

    2025-02-10T22:33:45.268Z watch   html_font_register

--- public/src/html_font_register.mjs
+++ public/src/html_font_register.mjs
@@ -3,16 +3,16 @@ import { html_document_head } from "./html_document_head.mjs";
 import { html_element } from "./html_element.mjs";
 import { html_inner_set } from "./html_inner_set.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-export function html_font_register() {
+export function html_font_register(font_name, font_path) {
   let style = html_element(html_document_head(), "style");
   html_attribute_set(style, "type", "text/css");
   let value = string_combine_multiple([
     "\n      @font-face {\n        font-family: '",
-    fontName,
+    font_name,
     "';\n        src: url('",
-    fontPath,
+    font_path,
     ".woff2') format('woff2'),\n             url('",
-    fontPath,
+    font_path,
     ".woff') format('woff');\n        font-weight: normal;\n        font-style: normal;\n      }\n    ",
   ]);
   html_inner_set(style, value);
