3c985ca8480468824f1abe3998e02b3642263827
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 17:40:30 -0500

    2025-02-10T22:40:19.884Z watch   html_style_sans_serif

--- public/src/html_style_sans_serif.mjs
+++ public/src/html_style_sans_serif.mjs
@@ -1,5 +1,5 @@
 import { html_style_font_family } from "./html_style_font_family.mjs";
 export function html_style_sans_serif(root) {
   let value = "sans-serif";
-  html_style_font_family(component, value);
+  html_style_font_family(root, value);
 }
