49697cd36f568756d67d5645f93f410d5f900db1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 17:05:06 -0500

    2025-02-10T22:05:05.618Z watch   app_el_upload

--- public/src/app_el_upload.mjs
+++ public/src/app_el_upload.mjs
@@ -1,4 +1,6 @@
+import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
 import { app_el_alphabet_pairs } from "./app_el_alphabet_pairs.mjs";
-export function app_el_upload() {
+export async function app_el_upload() {
   return app_el_alphabet_pairs();
+  return await app_language_2_upload_pairs(pairs, from, to);
 }
