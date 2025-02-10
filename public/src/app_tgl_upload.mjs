c19cf1cfadd087c49c9ea05aec8aabe9be804b36
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 15:22:29 -0500

    2025-02-10T20:22:28.236Z watch   app_tgl_upload

--- public/src/app_tgl_upload.mjs
+++ public/src/app_tgl_upload.mjs
@@ -1,3 +1,5 @@
+import { list_includes } from "./list_includes.mjs";
+import { list_filter_index } from "./list_filter_index.mjs";
 import { app_tgl_upload_pairs_cache } from "./app_tgl_upload_pairs_cache.mjs";
 import { app_language_upload_audio } from "./app_language_upload_audio.mjs";
 import { app_language_2_upload_atoms_definitions } from "./app_language_2_upload_atoms_definitions.mjs";
@@ -5,13 +7,18 @@ import { bible_words_spaceify } from "./bible_words_spaceify.mjs";
 import { list_to_lookup_key_value_list } from "./list_to_lookup_key_value_list.mjs";
 import { list_second } from "./list_second.mjs";
 import { list_first } from "./list_first.mjs";
-import { list_index_first } from "./list_index_first.mjs";
 export async function app_tgl_upload() {
   let from = "tgl";
   let to = "en";
   let limit = 6355;
   let pairs = await app_tgl_upload_pairs_cache(limit);
-  list_index_first(pairs, () => {});
+  return (
+    pairs.length +
+    " " +
+    list_filter_index(pairs, (p) => {
+      $r, list_includes(p, "paglipat");
+    })
+  );
   return;
   let definitions = list_to_lookup_key_value_list(
     pairs,
