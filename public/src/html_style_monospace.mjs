e711d3f45c87a8ffcedc1c07364e11d771295578
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 17:38:38 -0500

    2025-02-10T22:38:36.743Z watch   html_style_monospace

--- public/src/html_style_monospace.mjs
+++ public/src/html_style_monospace.mjs
@@ -1,6 +1,7 @@
 import { html_style } from "./html_style.mjs";
 export function html_style_monospace(component) {
+  let value = "monospace";
   html_style(component, {
-    "font-family": "monospace",
+    "font-family": value,
   });
 }
