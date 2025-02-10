a2e5afea060c3a746271afe2913a76e0f77f2485
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 17:33:01 -0500

    2025-02-10T22:33:00.042Z watch   html_font_register

--- public/src/html_font_register.mjs
+++ public/src/html_font_register.mjs
@@ -1,9 +1,10 @@
+import { html_attribute_set } from "./html_attribute_set.mjs";
 import { html_document_head } from "./html_document_head.mjs";
 import { html_element } from "./html_element.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function html_font_register() {
   let style = html_element(html_document_head(), "style");
-  style.type = "text/css";
+  html_attribute_set(style, "type", "text/css");
   let fontFaceRule = string_combine_multiple([
     "\n      @font-face {\n        font-family: '",
     fontName,
