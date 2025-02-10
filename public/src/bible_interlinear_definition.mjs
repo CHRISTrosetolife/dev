bf4d18a62a966122562666bc22ea8cb01033174e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 18:24:19 -0500

    2025-02-10T23:24:18.582Z watch   bible_interlinear_definition

--- public/src/bible_interlinear_definition.mjs
+++ public/src/bible_interlinear_definition.mjs
@@ -1,8 +1,8 @@
+import { bible_interlinear_strongs_cache } from "./bible_interlinear_strongs_cache.mjs";
 import { bible_interlinear_definition_generic } from "./bible_interlinear_definition_generic.mjs";
-import { bible_interlinear_strongs_cache_hub } from "./bible_interlinear_strongs_cache_hub.mjs";
 export async function bible_interlinear_definition(language, strong) {
   let label = "Short Definition:";
-  let html_lambda = bible_interlinear_strongs_cache_hub;
+  let html_lambda = bible_interlinear_strongs_cache;
   return await bible_interlinear_definition_generic(
     html_lambda,
     language,
