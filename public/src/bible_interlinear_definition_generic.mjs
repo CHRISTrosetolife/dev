6627f9d7c69357a2934e9b13b01182a9fc0ebad8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 18:22:11 -0500

    2025-02-10T23:22:11.016Z watch   bible_interlinear_definition_generic

--- public/src/bible_interlinear_definition_generic.mjs
+++ public/src/bible_interlinear_definition_generic.mjs
@@ -1,3 +1,4 @@
+import { list_map } from "./list_map.mjs";
 import { assert } from "./assert.mjs";
 import { equal } from "./equal.mjs";
 import { string_trim_whitespace } from "./string_trim_whitespace.mjs";
@@ -6,25 +7,29 @@ import { list_find } from "./list_find.mjs";
 import { html_parse_visit_class_list } from "./html_parse_visit_class_list.mjs";
 import { html_parse_visit_id } from "./html_parse_visit_id.mjs";
 import { html_parse } from "./html_parse.mjs";
+import { list_join_comma_space } from "./list_join_comma_space.mjs";
 export async function bible_interlinear_definition_generic(
   html_lambda,
   language,
   strong,
-  label,
+  labels,
 ) {
   let html = await html_lambda(language, strong);
   let p = await html_parse(html);
   let leftbox = html_parse_visit_id(p, "leftbox");
   let tophdg = html_parse_visit_class_list(leftbox, "tophdg");
-  let definition = list_find(tophdg, (t) => {
-    let t_text = html_parse_text(t);
-    t_text = string_trim_whitespace(t_text);
-    return equal(t_text, label);
+  let mapped = list_map(labels, (label) => {
+    let definition = list_find(tophdg, (t) => {
+      let t_text = html_parse_text(t);
+      t_text = string_trim_whitespace(t_text);
+      return equal(t_text, label);
+    });
+    let definition_text = html_parse_text(definition);
+    definition_text = string_trim_whitespace(definition_text);
+    assert(equal, [definition_text, label]);
+    let { next } = definition;
+    let next_text = html_parse_text(next);
+    return next_text;
   });
-  let definition_text = html_parse_text(definition);
-  definition_text = string_trim_whitespace(definition_text);
-  assert(equal, [definition_text, label]);
-  let { next } = definition;
-  let next_text = html_parse_text(next);
-  return next_text;
+  return list_join_comma_space(mapped);
 }
--- public/src/bible_interlinear_definition_hub.mjs
+++ public/src/bible_interlinear_definition_hub.mjs
@@ -1,12 +1,11 @@
 import { bible_interlinear_definition_generic } from "./bible_interlinear_definition_generic.mjs";
 import { bible_interlinear_strongs_cache_hub } from "./bible_interlinear_strongs_cache_hub.mjs";
 export async function bible_interlinear_definition_hub(language, strong) {
-  let label = "Definition:";
   let html_lambda = bible_interlinear_strongs_cache_hub;
   return await bible_interlinear_definition_generic(
     html_lambda,
     language,
     strong,
-    label,
+    ["Definition:",'Meaning:'],
   );
 }
