bd3ef19985c5aac25b685f374c692b96a3293efc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 17:17:09 -0500

    2025-02-10T22:17:06.342Z watch   html_flag_language_get

--- public/src/html_flag_language_get.mjs
+++ public/src/html_flag_language_get.mjs
@@ -1,12 +1,12 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { html_flag_us } from "./html_flag_us.mjs";
 import { html_flag_ph } from "./html_flag_ph.mjs";
-export function html_flag_language_get(language) {
+export function html_flag_language_get(language) {'https://flagicons.lipis.dev/'
   let languages_to_flags = {
     ceb: html_flag_ph(),
     tgl: html_flag_ph(),
     en: html_flag_us(),
-    el: "el",
+    el: "gr",
   };
   let g = object_property_get(languages_to_flags, language);
   return g;
