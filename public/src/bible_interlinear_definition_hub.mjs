c1fcdbcc533547d60a7f6505759b8d7eeaf3b690
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 18:18:13 -0500

    2025-02-10T23:18:07.900Z watch   bible_interlinear_definition_hub

--- public/src/bible_interlinear_definition_hub.mjs
+++ public/src/bible_interlinear_definition_hub.mjs
@@ -12,14 +12,15 @@ export async function bible_interlinear_definition_hub(language, strong) {
   let p = await html_parse(html);
   let leftbox = html_parse_visit_id(p, "leftbox");
   let tophdg = html_parse_visit_class_list(leftbox, "tophdg");
+  let label = "Definition: ";
   let definition = list_find(tophdg, (t) => {
     let t_text = html_parse_text(t);
     t_text = string_trim_whitespace(t_text);
-    return equal(t_text, "Short Definition:");
+    return equal(t_text, label);
   });
   let definition_text = html_parse_text(definition);
   definition_text = string_trim_whitespace(definition_text);
-  assert(equal, [definition_text, "Short Definition:"]);
+  assert(equal, [definition_text, label]);
   let { next } = definition;
   let next_text = html_parse_text(next);
   return next_text;
