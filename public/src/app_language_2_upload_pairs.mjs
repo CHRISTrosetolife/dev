39f0b8d749f1f5579997f1c0b16a8ea7148b6389
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 17:04:55 -0500

    2025-02-10T22:04:55.072Z watch   app_language_2_upload_pairs

---
+++ public/src/app_language_2_upload_pairs.mjs
@@ -0,0 +1,17 @@
+import { app_language_2_upload_atoms_definitions } from "./app_language_2_upload_atoms_definitions.mjs";
+import { app_language_upload_audio } from "./app_language_upload_audio.mjs";
+import { bible_words_spaceify } from "./bible_words_spaceify.mjs";
+import { list_second } from "./list_second.mjs";
+import { list_first } from "./list_first.mjs";
+import { list_to_lookup_key_value_list } from "./list_to_lookup_key_value_list.mjs";
+export async function app_language_2_upload_pairs(pairs, from, to) {
+  let definitions = list_to_lookup_key_value_list(
+    pairs,
+    list_first,
+    list_second,
+  );
+  let atoms = bible_words_spaceify(pairs);
+  await app_language_upload_audio(atoms, from);
+  return;
+  await app_language_2_upload_atoms_definitions(atoms, definitions, from, to);
+}
--- public/src/app_tgl_upload.mjs
+++ public/src/app_tgl_upload.mjs
@@ -1,22 +1,9 @@
+import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
 import { app_tgl_upload_pairs_cache } from "./app_tgl_upload_pairs_cache.mjs";
-import { app_language_upload_audio } from "./app_language_upload_audio.mjs";
-import { app_language_2_upload_atoms_definitions } from "./app_language_2_upload_atoms_definitions.mjs";
-import { bible_words_spaceify } from "./bible_words_spaceify.mjs";
-import { list_to_lookup_key_value_list } from "./list_to_lookup_key_value_list.mjs";
-import { list_second } from "./list_second.mjs";
-import { list_first } from "./list_first.mjs";
 export async function app_tgl_upload() {
   let from = "tgl";
   let to = "en";
   let limit = 1000;
   let pairs = await app_tgl_upload_pairs_cache(limit);
-  let definitions = list_to_lookup_key_value_list(
-    pairs,
-    list_first,
-    list_second,
-  );
-  let atoms = bible_words_spaceify(pairs);
-  await app_language_upload_audio(atoms, from);
-  return;
-  await app_language_2_upload_atoms_definitions(atoms, definitions, from, to);
+  return await app_language_2_upload_pairs(pairs, from, to);
 }
