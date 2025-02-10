9958709949d321eed399f69cc7872eb80e8fd61a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 17:35:28 -0500

    2025-02-10T22:35:27.802Z watch   html_font_register

--- public/src/html_font_register.mjs
+++ public/src/html_font_register.mjs
@@ -1,4 +1,5 @@
-import { object_property_exists } from "./object_property_exists.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { json_to } from "./json_to.mjs";
 import { global_function } from "./global_function.mjs";
 import { html_attribute_set } from "./html_attribute_set.mjs";
@@ -9,18 +10,19 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function html_font_register(font_name, font_path) {
   let g = global_function(html_font_register);
   let key = json_to(font_name, font_path);
-  if (object_property_exists(object, "property_name")) {
+  if (object_property_exists_not(g, key)) {
+    let style = html_element(html_document_head(), "style");
+    html_attribute_set(style, "type", "text/css");
+    let value = string_combine_multiple([
+      "\n      @font-face {\n        font-family: '",
+      font_name,
+      "';\n        src: url('",
+      font_path,
+      ".woff2') format('woff2'),\n             url('",
+      font_path,
+      ".woff') format('woff');\n        font-weight: normal;\n        font-style: normal;\n      }\n    ",
+    ]);
+    html_inner_set(style, value);
+    object_property_set(g, key, true);
   }
-  let style = html_element(html_document_head(), "style");
-  html_attribute_set(style, "type", "text/css");
-  let value = string_combine_multiple([
-    "\n      @font-face {\n        font-family: '",
-    font_name,
-    "';\n        src: url('",
-    font_path,
-    ".woff2') format('woff2'),\n             url('",
-    font_path,
-    ".woff') format('woff');\n        font-weight: normal;\n        font-style: normal;\n      }\n    ",
-  ]);
-  html_inner_set(style, value);
 }
