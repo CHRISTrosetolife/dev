75ec89de5cbb2c8907c3755dd4d8df630150cb62
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 16:52:02 -0500

    2025-02-10T21:52:02.110Z watch   app_el_upload

--- public/src/app_el_upload.mjs
+++ public/src/app_el_upload.mjs
@@ -1,6 +1,7 @@
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_el_upload() {
-  string_combine_multiple([
+  let alphabet_string = string_combine_multiple([
     "\nΑα\tAlpha\tΝν\tNu\nΒβ\tBeta\tΞξ\tXi\nΓγ\tGamma\tΟο\tOmicron\nΔδ\tDelta\tΠπ\tPi\nΕε\tEpsilon\tΡρ\tRho\nΖζ\tZeta\tΣσς\tSigma\nΗη\tEta\tΤτ\tTau\nΘθ\tTheta\tΥυ\tUpsilon\nΙι\tIota\tΦφ\tPhi\nΚκ\tKappa\tΧχ\tChi\nΛλ\tLambda\tΨψ\tPsi\nΜμ\tMu\tΩω\tOmega\n    ",
   ]);
+  return alphabet_string;
 }
