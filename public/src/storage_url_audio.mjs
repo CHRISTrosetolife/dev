fb18855c6c47de4dcc344976716551a224cf1626
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-09 19:00:55 -0500

    2025-02-10T00:00:52.681Z watch   storage_url_audio

--- public/src/storage_url_audio.mjs
+++ public/src/storage_url_audio.mjs
@@ -1,7 +1,9 @@
 import { storage_url } from "./storage_url.mjs";
 import { audio_path } from "./audio_path.mjs";
+import { string_replace } from "./string_replace.mjs";
 export async function storage_url_audio(language, count, text) {
   let file_path = await audio_path(language, count, text);
+  file_path = string_replace(file_path, "%20", "%2520");
   let url = storage_url(file_path);
   return url;
 }
