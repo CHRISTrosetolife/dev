cf83047b66fc4519fb4c371bc90d26a54842fc43
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 16:51:37 -0500

    2025-02-10T21:51:36.047Z watch   app_el_upload

--- public/src/app_el_upload.mjs
+++ public/src/app_el_upload.mjs
@@ -1,1 +1,6 @@
-export function app_el_upload() {}
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function app_el_upload() {
+  string_combine_multiple([
+    "\nΑα\tAlpha\tΝν\tNu\nΒβ\tBeta\tΞξ\tXi\nΓγ\tGamma\tΟο\tOmicron\nΔδ\tDelta\tΠπ\tPi\nΕε\tEpsilon\tΡρ\tRho\nΖζ\tZeta\tΣσς\tSigma\nΗη\tEta\tΤτ\tTau\nΘθ\tTheta\tΥυ\tUpsilon\nΙι\tIota\tΦφ\tPhi\nΚκ\tKappa\tΧχ\tChi\nΛλ\tLambda\tΨψ\tPsi\nΜμ\tMu\tΩω\tOmega\n    ",
+  ]);
+}
