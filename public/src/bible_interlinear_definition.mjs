300196a7ad0fc406a36ef1993caa05f5d8686402
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 18:41:55 -0500

    2025-02-10T23:41:52.757Z watch   bible_interlinear_definition

--- public/src/bible_interlinear_definition.mjs
+++ public/src/bible_interlinear_definition.mjs
@@ -1,12 +1,14 @@
 import { bible_interlinear_strongs_cache } from "./bible_interlinear_strongs_cache.mjs";
 import { bible_interlinear_definition_generic } from "./bible_interlinear_definition_generic.mjs";
+import { list_map_property } from "./list_map_property.mjs";
 export async function bible_interlinear_definition(language, strong) {
   let label = "Short Definition:";
   let html_lambda = bible_interlinear_strongs_cache;
-  return await bible_interlinear_definition_generic(
+  let ds = await bible_interlinear_definition_generic(
     html_lambda,
     language,
     strong,
     [label],
   );
+  return list_map_property(ds, "definition");
 }
