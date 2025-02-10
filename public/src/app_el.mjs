508dbd6a1781dcfae03ba22539254d9d878e6048
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 16:49:36 -0500

    2025-02-10T21:49:36.236Z watch   app_el

--- public/src/app_el.mjs
+++ public/src/app_el.mjs
@@ -1,7 +1,6 @@
-import { app_tgl } from "./app_tgl.mjs";
 import { app_language_2 } from "./app_language_2.mjs";
 export async function app_el() {
   let language_learn = "el";
   let language_fluent = "en";
-  await app_language_2(app_tgl, language_learn, language_fluent);
+  await app_language_2(app_el, language_learn, language_fluent);
 }
