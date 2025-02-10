398d37666b5813a2a9f2e49329946450a462b433
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 16:55:05 -0500

    2025-02-10T21:55:05.024Z watch   app_el_upload

--- public/src/app_el_upload.mjs
+++ public/src/app_el_upload.mjs
@@ -4,6 +4,7 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_trim_whitespace } from "./string_trim_whitespace.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { string_empty_not_is } from "./string_empty_not_is.mjs";
+import { list_chunk } from "./list_chunk.mjs";
 export function app_el_upload() {
   let alphabet_string = string_combine_multiple([
     "\nΑα\tAlpha\tΝν\tNu\nΒβ\tBeta\tΞξ\tXi\nΓγ\tGamma\tΟο\tOmicron\nΔδ\tDelta\tΠπ\tPi\nΕε\tEpsilon\tΡρ\tRho\nΖζ\tZeta\tΣσς\tSigma\nΗη\tEta\tΤτ\tTau\nΘθ\tTheta\tΥυ\tUpsilon\nΙι\tIota\tΦφ\tPhi\nΚκ\tKappa\tΧχ\tChi\nΛλ\tLambda\tΨψ\tPsi\nΜμ\tMu\tΩω\tOmega\n    ",
@@ -11,5 +12,6 @@ export function app_el_upload() {
   let split = string_split_multiple(alphabet_string, ["\n", "\t"]);
   let mapped = list_map(split, string_trim_whitespace);
   let filtered = list_filter(mapped, string_empty_not_is);
-  return filtered;
+  let chunks = list_chunk(filtered, 2);
+  return chunks;
 }
