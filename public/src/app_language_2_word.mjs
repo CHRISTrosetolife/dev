58b5bb2c4993070ae50ce01d572f126df31253c8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 18:11:24 -0500

    2025-02-10T23:11:19.210Z watch   app_language_2_word

--- public/src/app_language_2_word.mjs
+++ public/src/app_language_2_word.mjs
@@ -11,6 +11,7 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { html_flag_language } from "./html_flag_language.mjs";
 import { html_style } from "./html_style.mjs";
+import { html_style_font_color } from "./html_style_font_color.mjs";
 export function app_language_2_word(parent, language, text, boxed) {
   let container = html_span(parent);
   let box = html_span(container);
@@ -33,6 +34,7 @@ export function app_language_2_word(parent, language, text, boxed) {
     html_font_register("GentiumPlus-Regular", "font/GentiumPlus-Regular");
     html_style_font_family(text_span, "GentiumPlus-Regular");
     html_style_font_size_default_multiplied(text_span, 1.2);
+    html_style_font_color(text_span, "darkblue");
   }
   html_style_bold(container);
   let result = {
