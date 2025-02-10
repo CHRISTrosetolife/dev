fca01cd5066fe7fc6a749a26256e70064d7200fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 18:29:50 -0500

    2025-02-10T23:29:49.509Z watch   object_property_increment

--- public/src/object_property_increment.mjs
+++ public/src/object_property_increment.mjs
@@ -1,7 +1,7 @@
 import { add_1 } from "./add_1.mjs";
 import { object_property_change } from "./object_property_change.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
-export function object_property_increment(right, closest) {
-  object_property_initialize(right, closest, 0);
-  object_property_change(right, closest, add_1);
+export function object_property_increment(object, property_name) {
+  object_property_initialize(object, property_name, 0);
+  object_property_change(object, property_name, add_1);
 }
