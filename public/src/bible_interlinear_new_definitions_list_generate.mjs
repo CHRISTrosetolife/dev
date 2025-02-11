d79fb9004aaa158a64e348db9e71396f008fa650
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 19:08:42 -0500

    2025-02-11T00:08:38.093Z watch   bible_interlinear_new_definitions_list_generate

--- public/src/bible_interlinear_new_definitions_list_generate.mjs
+++ public/src/bible_interlinear_new_definitions_list_generate.mjs
@@ -1,5 +1,8 @@
 import { bible_interlinear_new_definitions } from "./bible_interlinear_new_definitions.mjs";
 import { generate_list_fn } from "./generate_list_fn.mjs";
 export async function bible_interlinear_new_definitions_list_generate() {
-  await generate_list_fn(bible_interlinear_new_definitions);
+  await generate_list_fn(
+    bible_interlinear_new_definitions,
+    bible_interlinear_new_definitions_list_generate,
+  );
 }
