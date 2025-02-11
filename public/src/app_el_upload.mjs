ebbdbecad21c5760f9e7f8d01d33d47fc95bb8d7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 07:08:34 -0500

    2025-02-11T12:08:34.269Z watch   app_el_upload

--- public/src/app_el_upload.mjs
+++ public/src/app_el_upload.mjs
@@ -1,11 +1,15 @@
+import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
+import { bible_interlinear_definitions_generic } from "./bible_interlinear_definitions_generic.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { bible_interlinear_new_definitions } from "./bible_interlinear_new_definitions.mjs";
 import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
 import { list_map } from "./list_map.mjs";
 export async function app_el_upload() {
   let from = "el";
   let to = "en";
-  let ds = await bible_interlinear_new_definitions();
+  let ds = await bible_interlinear_definitions_generic(
+    bible_interlinear_cache_new,
+    "greek",
+  );
   let pairs = list_map(ds, (d) => [
     object_property_get(d, "word"),
     object_property_get(d, "definition"),
