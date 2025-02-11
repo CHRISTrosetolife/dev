af9dd7587adce2921eec990a28fb0db59af5b0b7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 19:34:30 -0500

    2025-02-11T00:34:24.301Z watch   app_save_change

--- public/src/app_save_change.mjs
+++ public/src/app_save_change.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { app_save_set } from "./app_save_set.mjs";
 import { app_save_get } from "./app_save_get.mjs";
 export async function app_save_change(app_fn, lambda) {
+  assert_arguments_length(arguments, 2);
   let save = app_save_get(app_fn);
   await lambda(save);
   app_save_set(app_fn, save);
