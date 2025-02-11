6586e96d005362475924b61f80fbaac4eb88366d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 10:37:48 -0500

    2025-02-11T15:37:48.444Z watch   bible_ceb_upload

--- public/src/bible_ceb_upload.mjs
+++ public/src/bible_ceb_upload.mjs
@@ -16,7 +16,6 @@ export async function bible_ceb_upload() {
   let bible_version = "cebulb";
   let chapters = await bible_books_chapters(bible_version);
   await each_async(chapters, async (chapter_info) => {
-    let book_code = object_property_get(chapter_info, "book_code");
     let chapter_code = object_property_get(chapter_info, "chapter_code");
     let chapter = await bible_chapter(bible_version, chapter_code);
     let tokens = list_adder_unique((la) => {
