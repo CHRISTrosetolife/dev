b155113d482b92ec4504fcd561f4b14d701f726b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 07:41:07 -0500

    2025-02-11T12:41:05.744Z watch   html_font_register_generic

--- public/src/app_language_2_word.mjs
+++ public/src/app_language_2_word.mjs
@@ -35,7 +35,7 @@ export function app_language_2_word(parent, language, text, boxed) {
     html_style_font_family(text_span, "GentiumPlus-Regular");
     html_style_font_size_default_multiplied(text_span, 1.2);
     html_style_font_color(text_span, "darkblue");
-  }
+  }$el
   html_style_bold(container);
   let result = {
     container,
