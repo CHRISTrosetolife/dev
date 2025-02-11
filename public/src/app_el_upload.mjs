3db8806aef50bab679a88222382f5a610d0e66e0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 07:09:59 -0500

    2025-02-11T12:09:58.698Z watch   app_language_2_bible_upload

--- public/src/app_el_upload.mjs
+++ public/src/app_el_upload.mjs
@@ -1,20 +1,12 @@
+import { app_language_2_bible_upload } from "./app_language_2_bible_upload.mjs";
 import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
-import { bible_interlinear_definitions_generic } from "./bible_interlinear_definitions_generic.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
-import { list_map } from "./list_map.mjs";
 export async function app_el_upload() {
   let from = "el";
   let from_long = "greek";
   let bible_interlinear_cache_fn = bible_interlinear_cache_new;
-  let ds = await bible_interlinear_definitions_generic(
+  return await app_language_2_bible_upload(
     bible_interlinear_cache_fn,
     from_long,
+    from,
   );
-  let to = "en";
-  let pairs = list_map(ds, (d) => [
-    object_property_get(d, "word"),
-    object_property_get(d, "definition"),
-  ]);
-  return await app_language_2_upload_pairs(pairs, from, to);
 }
---
+++ public/src/app_language_2_bible_upload.mjs
@@ -0,0 +1,20 @@
+import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { list_map } from "./list_map.mjs";
+import { bible_interlinear_definitions_generic } from "./bible_interlinear_definitions_generic.mjs";
+export async function app_language_2_bible_upload(
+  bible_interlinear_cache_fn,
+  from_long,
+  from,
+) {
+  let ds = await bible_interlinear_definitions_generic(
+    bible_interlinear_cache_fn,
+    from_long,
+  );
+  let to = "en";
+  let pairs = list_map(ds, (d) => [
+    object_property_get(d, "word"),
+    object_property_get(d, "definition"),
+  ]);
+  return await app_language_2_upload_pairs(pairs, from, to);
+}
