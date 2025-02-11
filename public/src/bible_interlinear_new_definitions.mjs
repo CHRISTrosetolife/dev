15da1121dfa0d4a9cb9e8d011c483a725b4de168
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 07:08:19 -0500

    2025-02-11T12:08:19.257Z watch   bible_interlinear_new_definitions

--- public/src/bible_interlinear_new_definitions.mjs
+++ public/src/bible_interlinear_new_definitions.mjs
@@ -1,9 +1,8 @@
 import { bible_interlinear_definitions_generic } from "./bible_interlinear_definitions_generic.mjs";
 import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
 export async function bible_interlinear_new_definitions() {
-  let language = "greek";
   return await bible_interlinear_definitions_generic(
     bible_interlinear_cache_new,
-    language,
+    "greek",
   );
 }
