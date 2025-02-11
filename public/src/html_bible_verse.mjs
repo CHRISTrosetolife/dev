0991deb400cdab9e5e95e3ef165c0c502330b91b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 12:58:51 -0500

    2025-02-11T17:58:47.425Z watch   html_bible_verse

--- public/src/html_bible_verse.mjs
+++ public/src/html_bible_verse.mjs
@@ -4,11 +4,12 @@ import { google_search_url } from "./google_search_url.mjs";
 import { html_spacer } from "./html_spacer.mjs";
 import { each } from "./each.mjs";
 import { html_bible_verse_number } from "./html_bible_verse_number.mjs";
-export function html_bible_verse(root, book_code, chapter, verse) {
+export function html_bible_verse(root, book_code, chapter, verse, each_token) {
   let { tokens, verse_number } = verse;
   let vn = html_bible_verse_number(root, book_code, chapter, verse_number);
   each(tokens, (token) => {
     html_spacer(root);
+    each_token(root, token);
     html_a_blank(
       root,
       token,
