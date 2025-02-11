76787425f7ce79736dcbd1e657b04d4527eb4e5d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 19:15:20 -0500

    2025-02-11T00:15:18.802Z watch   app_app_el_definitions

--- public/src/app_app_el_definitions.mjs
+++ public/src/app_app_el_definitions.mjs
@@ -1,6 +1,8 @@
+import { bible_interlinear_new_definitions_list } from "./bible_interlinear_new_definitions_list.mjs";
 import { each } from "./each.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_app_el_definitions() {
   let root = html_style_default_initialize();
+  let list = bible_interlinear_new_definitions_list();
   each(list, (item) => {});
 }
