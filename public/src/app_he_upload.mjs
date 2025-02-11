bc74b5b9a44477a7a44ccc637804b1658c5a74bd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 07:12:25 -0500

    2025-02-11T12:12:25.064Z watch   app_he_upload

--- public/src/app_he_upload.mjs
+++ public/src/app_he_upload.mjs
@@ -1,9 +1,9 @@
+import { bible_interlinear_cache_old } from "./bible_interlinear_cache_old.mjs";
 import { app_language_2_bible_upload } from "./app_language_2_bible_upload.mjs";
-import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
 export async function app_he_upload() {
-  let from = "el";
-  let from_long = "greek";
-  let bible_interlinear_cache_fn = bible_interlinear_cache_new;
+  let from = "he";
+  let from_long = "hebrew";
+  let bible_interlinear_cache_fn = bible_interlinear_cache_old;
   return await app_language_2_bible_upload(
     bible_interlinear_cache_fn,
     from_long,
