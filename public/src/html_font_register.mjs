5a69654646477c5a09a53e544074660e358405e8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 07:36:52 -0500

    2025-02-11T12:36:51.716Z watch   html_font_register

--- public/src/html_font_register.mjs
+++ public/src/html_font_register.mjs
@@ -16,11 +16,13 @@ export function html_font_register(font_name, font_path) {
     let value = string_combine_multiple([
       "@font-face { font-family: '",
       font_name,
-      "'; src: url('",
+      "'; src: ",
+      "url('",
       font_path,
       ".woff2') format('woff2'), url('",
       font_path,
-      ".woff') format('woff'); font-weight: normal; font-style: normal; }",
+      ".woff') format('woff')",
+      "; font-weight: normal; font-style: normal; }",
     ]);
     html_inner_set(style, value);
     object_property_set(g, key, true);
