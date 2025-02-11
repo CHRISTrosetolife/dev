ef0cdd757473b779d2caa58c713d73a1ce5209c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 13:19:36 -0500

    2025-02-11T18:19:34.471Z ss transparent

--- public/src/html_style_background_color_select.mjs
+++ public/src/html_style_background_color_select.mjs
@@ -2,7 +2,7 @@ import { html_style_background_color } from "./html_style_background_color.mjs";
 import { html_style_background_color_yellow } from "./html_style_background_color_yellow.mjs";
 export function html_style_background_color_select(row_selected, row) {
   if (row_selected !== null) {
-    html_style_background_color(row_selected, "none");
+    html_style_background_color(row_selected, "transparent");
   }
   row_selected = row;
   html_style_background_color_yellow(row_selected);
