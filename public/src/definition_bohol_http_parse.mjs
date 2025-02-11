3634da63e35cb3c8882ed03e29655d1249e1e13c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 11:47:03 -0500

    2025-02-11T16:47:02.307Z watch   definition_bohol_http_parse

--- public/src/definition_bohol_http_parse.mjs
+++ public/src/definition_bohol_http_parse.mjs
@@ -1,10 +1,12 @@
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-export async function definition_bohol_http_parse(word) {
+export async function definition_bohol_http_parse(word, language_search) {
+  language_search = "English";
   let url = string_combine_multiple([
     "https://www.bohol.ph/diksyunaryo.php?sw=",
     word,
-    "&lang=English",
+    "&lang=",
+    language_search,
   ]);
   let result = await html_cache_parse(url);
   return result;
