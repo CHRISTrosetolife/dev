bab69058f320b756815d0234a4784aaf6cbf4373
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 11:05:17 -0500

    2025-02-11T16:05:15.801Z watch   app_bible_ceb_verse

--- public/src/app_bible_ceb_verse.mjs
+++ public/src/app_bible_ceb_verse.mjs
@@ -1,3 +1,6 @@
+import { html_div } from "./html_div.mjs";
+import { each } from "./each.mjs";
+import { bible_verses_to_verse_tokens } from "./bible_verses_to_verse_tokens.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { bible_storage_interlinear_chapter_definitions_name } from "./bible_storage_interlinear_chapter_definitions_name.mjs";
 import { bible_storage_ceb_definitions_path } from "./bible_storage_ceb_definitions_path.mjs";
@@ -6,7 +9,6 @@ import { list_find_verse_number } from "./list_find_verse_number.mjs";
 import { html_bible_verse } from "./html_bible_verse.mjs";
 import { html_bible_word } from "./html_bible_word.mjs";
 import { html_hr } from "./html_hr.mjs";
-import { bible_verses_to_verse_tokens_each } from "./bible_verses_to_verse_tokens_each.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { app_gs_bible_chapter_name } from "./app_gs_bible_chapter_name.mjs";
@@ -37,12 +39,11 @@ export async function app_bible_ceb_verse(
   let verse_ceb = list_find_verse_number(verses_ceb, verse_number);
   html_bible_verse(middle, book_code, chapter, verse_ceb);
   html_hr(middle);
-  bible_verses_to_verse_tokens_each(
-    verses_ceb,
-    verse_number,
-    middle,
-    lambda_each,
-  );
+  let tokens = bible_verses_to_verse_tokens(verses_ceb, verse_number);
+  each(tokens, (token) => {
+    let row = html_div(middle);
+    lambda_each(token, row);
+  });
   html_hr(middle);
   let definitions = await global_function_property_async(
     app_lambda,
