76d4548e333bf0297192986881a963720131cc6b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 07:49:43 -0500

    2025-02-11T12:49:42.483Z p app_language_2_word_p context context

--- public/src/app_language_2_word_p.mjs
+++ public/src/app_language_2_word_p.mjs
@@ -1,6 +1,6 @@
 import { app_language_2_word } from "./app_language_2_word.mjs";
 import { html_p } from "./html_p.mjs";
-export function app_language_2_word_p(body, language, text) {
+export function app_language_2_word_p(body, language, text, context) {
   let p = html_p(body);
   let result = app_language_2_word(p, language, text, false, context);
   return result;
