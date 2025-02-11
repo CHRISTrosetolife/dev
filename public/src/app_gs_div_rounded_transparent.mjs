10773c70f20dc315b34f9dd86c6ba0aa29e67ede
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 13:20:36 -0500

    2025-02-11T18:20:32.218Z watch   html_overlay

---
+++ public/src/app_gs_div_rounded_transparent.mjs
@@ -0,0 +1,16 @@
+import { html_style_background_color_transparent_mix } from "./html_style_background_color_transparent_mix.mjs";
+import { html_style_rounded_padded } from "./html_style_rounded_padded.mjs";
+import { game_tile_units_css } from "./game_tile_units_css.mjs";
+import { html_style } from "./html_style.mjs";
+import { html_div } from "./html_div.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
+export function app_gs_div_rounded_transparent(parent, color, percent) {
+  assert_arguments_length(arguments, 3);
+  let div = html_div(parent);
+  html_style(div, {
+    padding: game_tile_units_css(0.25),
+  });
+  html_style_background_color_transparent_mix(div, color, percent);
+  html_style_rounded_padded(div);
+  return div;
+}
