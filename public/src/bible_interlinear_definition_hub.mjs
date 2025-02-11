ec0694423a5224e8c2f11359a3e7abcdaea77a28
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 20:34:11 -0500

    2025-02-11T01:34:10.799Z watch   bible_interlinear_definition_hub

--- public/src/bible_interlinear_definition_hub.mjs
+++ public/src/bible_interlinear_definition_hub.mjs
@@ -2,10 +2,11 @@ import { bible_interlinear_definition_generic } from "./bible_interlinear_defini
 import { bible_interlinear_strongs_cache_hub } from "./bible_interlinear_strongs_cache_hub.mjs";
 export async function bible_interlinear_definition_hub(language, strong) {
   let html_lambda = bible_interlinear_strongs_cache_hub;
+  let old = ["Definition:", "Meaning:"];
   return await bible_interlinear_definition_generic(
     html_lambda,
     language,
     strong,
-    ["Definition:", "Meaning:"],
+    ["Definition:"],
   );
 }
