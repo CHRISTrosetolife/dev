ebb19bd9589733221ba741bd88d798fcfb7cb0ef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 11:47:11 -0500

    2025-02-11T16:47:10.684Z watch   definition_bohol_http_parse

--- public/src/definition_bohol_http_parse.mjs
+++ public/src/definition_bohol_http_parse.mjs
@@ -1,7 +1,6 @@
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function definition_bohol_http_parse(word, language_search) {
-  language_search = "English";
   let url = string_combine_multiple([
     "https://www.bohol.ph/diksyunaryo.php?sw=",
     word,
