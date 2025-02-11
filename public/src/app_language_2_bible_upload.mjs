72b6a640e1b861dad7751312ba6a1d1d64301dc1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 07:12:40 -0500

    2025-02-11T12:12:40.177Z watch   app_language_2_bible_upload

--- public/src/app_language_2_bible_upload.mjs
+++ public/src/app_language_2_bible_upload.mjs
@@ -16,5 +16,6 @@ export async function app_language_2_bible_upload(
     object_property_get(d, "word"),
     object_property_get(d, "definition"),
   ]);
+  return pairs;
   return await app_language_2_upload_pairs(pairs, from, to);
 }
