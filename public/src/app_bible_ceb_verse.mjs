9129b0003a1355ac51138e07d71724da91864f17
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 09:51:11 -0500

    2025-02-11T14:51:11.177Z watch   app_bible_ceb_verse

--- public/src/app_bible_ceb_verse.mjs
+++ public/src/app_bible_ceb_verse.mjs
@@ -1,3 +1,4 @@
+import { app_gs_bible_chapter_name } from "./app_gs_bible_chapter_name.mjs";
 import { bible_storage_version_http_get } from "./bible_storage_version_http_get.mjs";
 import { global_function_property_async } from "./global_function_property_async.mjs";
 import { app_bible_verse_common } from "./app_bible_verse_common.mjs";
@@ -13,6 +14,7 @@ export async function app_bible_ceb_verse(
     chapter,
     verse_number,
   );
+  let chapter_code = app_gs_bible_chapter_name(book_code, chapter);
   let verses = await global_function_property_async(
     app_fn,
     chapter_code,
