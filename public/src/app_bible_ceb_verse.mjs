54e95b5b92f3373f73053f96b513554480f98ca4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 09:52:21 -0500

    2025-02-11T14:52:20.714Z watch   app_bible_ceb_verse

--- public/src/app_bible_ceb_verse.mjs
+++ public/src/app_bible_ceb_verse.mjs
@@ -1,3 +1,4 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { list_find_verse_number } from "./list_find_verse_number.mjs";
 import { app_gs_bible_chapter_name } from "./app_gs_bible_chapter_name.mjs";
 import { bible_storage_version_http_get } from "./bible_storage_version_http_get.mjs";
@@ -16,10 +17,11 @@ export async function app_bible_ceb_verse(
     verse_number,
   );
   let chapter_code = app_gs_bible_chapter_name(book_code, chapter);
+  let ceb_version = "cebulb";
   let verses_ceb = await global_function_property_async(
     app_fn,
-    chapter_code,
-    async () => await bible_storage_version_http_get("cebulb", chapter_code),
+    string_combine_multiple([chapter_code]),
+    async () => await bible_storage_version_http_get(ceb_version, chapter_code),
   );
   let verse_interlinear = list_find_verse_number(verses_ceb, verse_number);
 }
