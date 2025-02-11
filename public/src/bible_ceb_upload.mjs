a60abe5bdb989d0c3e09fa3ac574abf2998e4fb5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 10:22:52 -0500

    2025-02-11T15:22:52.000Z watch   bible_ceb_upload

--- public/src/bible_ceb_upload.mjs
+++ public/src/bible_ceb_upload.mjs
@@ -1,5 +1,4 @@
 import { bible_books_chapters } from "./bible_books_chapters.mjs";
-import { object_merge_strict } from "./object_merge_strict.mjs";
 import { bible_storage_interlinear_chapter_definitions_property } from "./bible_storage_interlinear_chapter_definitions_property.mjs";
 import { bible_storage_interlinear_chapter_definitions_name } from "./bible_storage_interlinear_chapter_definitions_name.mjs";
 import { bible_storage_interlinear_chapter_definitions_path } from "./bible_storage_interlinear_chapter_definitions_path.mjs";
@@ -12,25 +11,10 @@ import { each_async } from "./each_async.mjs";
 import { bible_storage_interlinear_book_path } from "./bible_storage_interlinear_book_path.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { bible_storage_version_upload } from "./bible_storage_version_upload.mjs";
-import { list_adder } from "./list_adder.mjs";
-import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
-import { bible_interlinear_each_chapter } from "./bible_interlinear_each_chapter.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 export async function bible_ceb_upload() {
   let chapters = await bible_books_chapters("cebulb");
-  let language = "greek";
-  let books = await bible_interlinear_cache_new();
-  let chapters = list_adder((la) => {
-    bible_interlinear_each_chapter(books, (chapter, book_name) => {
-      let r = object_merge_strict(
-        {
-          book_name,
-        },
-        chapter,
-      );
-      la(r);
-    });
-  });
+  return chapters;
   await each_async(chapters, async (chapter) => {
     let book_name = object_property_get(chapter, "book_name");
     let chapter_name = object_property_get(chapter, "chapter_name");
