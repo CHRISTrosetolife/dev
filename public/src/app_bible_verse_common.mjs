ecfc710d092844f307b1c4be1706ec0a2de5df05
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 11:38:35 -0500

    2025-02-11T16:38:33.110Z watch   app_bible_verse_common

--- public/src/app_bible_verse_common.mjs
+++ public/src/app_bible_verse_common.mjs
@@ -1,6 +1,5 @@
 import { html_hr } from "./html_hr.mjs";
 import { html_bible_verse_navigation } from "./html_bible_verse_navigation.mjs";
-import { app_bible_verse } from "./app_bible_verse.mjs";
 import { app_record_verse_generic } from "./app_record_verse_generic.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function app_bible_verse_common(
@@ -8,6 +7,7 @@ export async function app_bible_verse_common(
   book_code,
   chapter,
   verse_number,
+  app_verse_lambda,
 ) {
   let app_lambda = object_property_get(context, "app_lambda");
   let { middle: m } = await app_record_verse_generic(
@@ -16,12 +16,12 @@ export async function app_bible_verse_common(
     chapter,
     verse_number,
     context,
-    app_bible_verse,
+    app_verse_lambda,
   );
   let middle = m;
   await html_bible_verse_navigation(
     app_lambda,
-    app_bible_verse,
+    app_verse_lambda,
     context,
     book_code,
     chapter,
