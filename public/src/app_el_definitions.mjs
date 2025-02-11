2d05044f660d92ceaf76150a2ed805b68388e335
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 19:18:36 -0500

    2025-02-11T00:18:33.893Z watch   app_el_definitions

--- public/src/app_el_definitions.mjs
+++ public/src/app_el_definitions.mjs
@@ -1,3 +1,4 @@
+import { html_hr } from "./html_hr.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { json_to } from "./json_to.mjs";
 import { html_p_text } from "./html_p_text.mjs";
@@ -8,8 +9,9 @@ export function app_el_definitions() {
   let root = html_style_default_initialize();
   let list = bible_interlinear_new_definitions_list();
   each(list, (item) => {
-    html_p_text(root, json_to(item));
     let definition = object_property_get(item, "definition");
     let word = object_property_get(item, "word");
+    html_p_text(root, json_to(item));
+    html_hr(root);
   });
 }
