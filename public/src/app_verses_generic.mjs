294a9baeb3b4701f8c5a16ff0df30fffe1dc290f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 08:57:00 -0500

    2025-02-11T13:56:59.574Z watch   app_verses_generic

--- public/src/app_verses_generic.mjs
+++ public/src/app_verses_generic.mjs
@@ -5,10 +5,10 @@ import { object_property_get_or_async } from "./object_property_get_or_async.mjs
 import { app_gs_bible_chapter_name } from "./app_gs_bible_chapter_name.mjs";
 export async function app_verses_generic(context, app_fn, book_code, chapter) {
   let chapter_code = app_gs_bible_chapter_name(book_code, chapter);
-  let a = await global_function(app_fn);
   let version_code = object_property_get(context, "version_code");
   let lambda = async () =>
     await bible_storage_version_http_get(version_code, chapter_code);
+  let a = await global_function(app_fn);
   let verses = await object_property_get_or_async(a, chapter_code, lambda);
   return verses;
 }
