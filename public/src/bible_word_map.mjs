4c57eb4499c21f8955645c319562eabb7a114c2a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 10:43:53 -0500

    2025-02-11T15:43:52.895Z watch   bible_word_map

--- public/src/bible_word_map.mjs
+++ public/src/bible_word_map.mjs
@@ -1,10 +1,12 @@
 import { bible_words_map } from "./bible_words_map.mjs";
 import { bible_words_count_generic_symbols } from "./bible_words_count_generic_symbols.mjs";
 import { list_single } from "./list_single.mjs";
+import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export function bible_word_map(word) {
   let symbols = bible_words_count_generic_symbols();
   let mapped = bible_words_map([word], symbols);
-  if (false) {
+  if (list_empty_not_is(mapped)) {
     return list_single(mapped);
   }
+  return null;
 }
