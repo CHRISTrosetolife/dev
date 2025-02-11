b62fa3dcb873321d64c43b9a943f754433406b7a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 19:12:07 -0500

    2025-02-11T00:12:00.870Z bible_interlinear_new_definitions_list_generate

--- public/src/bible_interlinear_new_definitions_list.mjs
+++ public/src/bible_interlinear_new_definitions_list.mjs
@@ -1,54 +1,273 @@
 export function bible_interlinear_new_definitions_list() {
   return [
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
-    "[object Object]",
+    {
+      strong: "3588",
+      definition: "the, the, the definite article.",
+      word: "ὁ, ἡ, τό",
+    },
+    {
+      strong: "2532",
+      definition: "and, also, even, indeed, but, and, even, also, namely.",
+      word: "καί",
+    },
+    {
+      strong: "846",
+      definition:
+        "he, she, it, they, them, himself, herself, itself, themselves, same, he, she, it, they, them, same.",
+      word: "αὐτός",
+    },
+    {
+      strong: "4771",
+      definition: "you, you.",
+      word: "σύ",
+    },
+    {
+      strong: "1161",
+      definition:
+        "but, and, now, moreover, a weak adversative particle, generally placed second in its clause; but, on the other hand, and.",
+      word: "δέ",
+    },
+    {
+      strong: "1722",
+      definition: "in, on, at, by, with, among, in, on, among.",
+      word: "ἐν",
+    },
+    {
+      strong: "1473",
+      definition: "I, me, I, the first-person pronoun.",
+      word: "ἐγώ",
+    },
+    {
+      strong: "1510",
+      definition: "to be, to exist, to happen, to be present, I am, exist.",
+      word: "εἰμί",
+    },
+    {
+      strong: "1519",
+      definition:
+        "into, to, towards, for, in order to, into, in, unto, to, upon, towards, for, among.",
+      word: "εἰς",
+    },
+    {
+      strong: "3756",
+      definition: "no, not, no, not.",
+      word: "οὐ, οὐκ, οὐχ",
+    },
+    {
+      strong: "3739",
+      definition: "who, which, that, who, which, what, that.",
+      word: "ὅς, ἥ, ὅ",
+    },
+    {
+      strong: "3778",
+      definition: "This, these, he, she, it, this; he, she, it.",
+      word: "οὗτος, αὕτη, τοῦτο",
+    },
+    {
+      strong: "2316",
+      definition: "God, god, (a) God, (b) a god, generally.",
+      word: "θεός",
+    },
+    {
+      strong: "3754",
+      definition:
+        "that, because, since, for, that, since, because; may introduce direct discourse.",
+      word: "ὅτι",
+    },
+    {
+      strong: "3956",
+      definition: "All, every, whole, entire, all, the whole, every kind of.",
+      word: "πᾶς",
+    },
+    {
+      strong: "3004",
+      definition:
+        "To say, to speak, to tell, (denoting speech in progress), (a) I say, speak; I mean, mention, tell, (b) I call, name, especially in the pass., (c) I tell, command.",
+      word: "λέγω",
+    },
+    {
+      strong: "2036",
+      definition: "said, answer, bid, bring word, command",
+      word: "εἶπεν",
+    },
+    {
+      strong: "3361",
+      definition: "not, lest, not, lest.",
+      word: "μή",
+    },
+    {
+      strong: "1063",
+      definition: "for, because, since, for.",
+      word: "γάρ",
+    },
+    {
+      strong: "2424",
+      definition:
+        "Jesus, Jesus; the Greek form of Joshua; Jesus, son of Eliezer; Jesus, surnamed Justus.",
+      word: "Ἰησοῦς",
+    },
+    {
+      strong: "1537",
+      definition:
+        "from, out of, by, away from, from out, out from among, from, suggesting from the interior outwards.",
+      word: "ἐκ or ἐξ",
+    },
+    {
+      strong: "1909",
+      definition:
+        "on, upon, over, at, by, before, across, against, among, beside, in, on the basis of, on, to, against, on the basis of, at.",
+      word: "ἐπί",
+    },
+    {
+      strong: "2962",
+      definition: "Lord, master, owner, lord, master, sir; the Lord.",
+      word: "κύριος",
+    },
+    {
+      strong: "2192",
+      definition: "To have, to hold, to possess, I have, hold, possess.",
+      word: "ἔχω",
+    },
+    {
+      strong: "4314",
+      definition: "to, towards, with, for, to, towards, with.",
+      word: "πρός",
+    },
+    {
+      strong: "2443",
+      definition: "that, in order that, so that, in order that, so that.",
+      word: "ἵνα",
+    },
+    {
+      strong: "1096",
+      definition:
+        "to become, to come into being, to happen, to be made, to be done, I come into being, am born, become, come about, happen.",
+      word: "γίνομαι",
+    },
+    {
+      strong: "1223",
+      definition:
+        "through, by, because of, for the sake of, (a) gen: through, throughout, by the instrumentality of, (b) acc: through, on account of, by reason of, for the sake of, because of.",
+      word: "διά",
+    },
+    {
+      strong: "575",
+      definition: "from, away from, of, by, from, away from.",
+      word: "ἀπό",
+    },
+    {
+      strong: "235",
+      definition: "but, rather, on the contrary, but, except, however.",
+      word: "ἀλλά",
+    },
+    {
+      strong: "2064",
+      definition: "To come, to go, I come, go.",
+      word: "ἔρχομαι",
+    },
+    {
+      strong: "5101",
+      definition:
+        "who, which, what, why, someone, anyone, who, which, what, why.",
+      word: "τίς",
+    },
+    {
+      strong: "4160",
+      definition:
+        "To make, to do, to act, to cause, to work, (a) I make, manufacture, construct, (b) I do, act, cause.",
+      word: "ποιέω",
+    },
+    {
+      strong: "444",
+      definition: "Man, human, person, mankind, a man, one of the human race.",
+      word: "ἄνθρωπος",
+    },
+    {
+      strong: "5547",
+      definition:
+        "Christ, Anointed One, Messiah, Anointed One; the Messiah, the Christ.",
+      word: "Χριστός",
+    },
+    {
+      strong: "5100",
+      definition:
+        "someone, anyone, certain one, some, a certain thing, any one, some one, a certain one or thing.",
+      word: "τις",
+    },
+    {
+      strong: "5613",
+      definition:
+        "as, like, when, while, since, because, as, like as, about, as it were, according as, how, when, while, as soon as, so that.",
+      word: "ὡς",
+    },
+    {
+      strong: "3767",
+      definition: "therefore, then, so, consequently, therefore, then.",
+      word: "οὖν",
+    },
+    {
+      strong: "1487",
+      definition: "if, whether, if.",
+      word: "εἰ",
+    },
+    {
+      strong: "2596",
+      definition:
+        "according to, down, against, throughout, by, gen: against, down from, throughout, by; acc: over against, among, daily, day-by-day, each day, according to, by way of.",
+      word: "κατά",
+    },
+    {
+      strong: "3326",
+      definition:
+        "with, after, among, (a) gen: with, in company with, (b) acc: (1) behind, beyond, after, of place, (2) after, of time, with nouns, neut. of adjectives.",
+      word: "μετά",
+    },
+    {
+      strong: "3708",
+      definition:
+        "To see, perceive, attend to, I see, look upon, experience, perceive, discern, beware.",
+      word: "ὁράω",
+    },
+    {
+      strong: "191",
+      definition:
+        "To hear, to listen, to understand, I hear, listen, comprehend by hearing; pass: is heard, reported.",
+      word: "ἀκούω",
+    },
+    {
+      strong: "1325",
+      definition: "To give, to grant, to bestow, I offer, give; I put, place.",
+      word: "δίδωμι",
+    },
+    {
+      strong: "3962",
+      definition: "Father, father, (Heavenly) Father, ancestor, elder, senior.",
+      word: "πατήρ",
+    },
+    {
+      strong: "2250",
+      definition: "Day, a day, the period from sunrise to sunset.",
+      word: "ἡμέρα",
+    },
+    {
+      strong: "4151",
+      definition: "Spirit, wind, breath, wind, breath, spirit.",
+      word: "πνεῦμα",
+    },
+    {
+      strong: "5207",
+      definition: "Son, a son, descendent.",
+      word: "υἱός",
+    },
+    {
+      strong: "4183",
+      definition: "many, much, great, much, many; often.",
+      word: "πολύς",
+    },
+    {
+      strong: "2228",
+      definition: "or, than, or, than.",
+      word: "ἤ",
+    },
   ];
 }
