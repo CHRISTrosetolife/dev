dec692aa564ae7ad97b46f8618f9ba9f6af5f5ab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 19:11:39 -0500

    2025-02-11T00:11:32.535Z watch   generate_list_string

--- public/src/generate_list_string.mjs
+++ public/src/generate_list_string.mjs
@@ -1,7 +1,7 @@
+import { json_to } from "./json_to.mjs";
 import { generate_list_generic } from "./generate_list_generic.mjs";
-import { string_delimit } from "./string_delimit.mjs";
 import { list_map } from "./list_map.mjs";
 export async function generate_list_string(list, name) {
-  let delimited = list_map(list, string_delimit);
+  let delimited = list_map(list, json_to);
   await generate_list_generic(delimited, name);
 }
