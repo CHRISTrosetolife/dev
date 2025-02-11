219f1a0e3f4d9db0a12d08a207086dc8ba68b54f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 11:17:27 -0500

    2025-02-11T16:17:25.939Z watch   html_a_blank

--- public/src/html_a_blank.mjs
+++ public/src/html_a_blank.mjs
@@ -1,7 +1,7 @@
 import { html_a } from "./html_a.mjs";
 import { html_attribute_set } from "./html_attribute_set.mjs";
-export function html_a_blank(e, text, url) {
-  let a = html_a(e, text, url);
+export function html_a_blank(parent, text, url) {
+  let a = html_a(parent, text, url);
   html_attribute_set(a, "target", "_blank");
   return a;
 }
