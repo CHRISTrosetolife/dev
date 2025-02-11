8ba31028025d02a0649a176bffac9aa1df655cb3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 20:35:30 -0500

    2025-02-11T01:35:29.625Z watch   app_el_upload

--- public/src/app_el_upload.mjs
+++ public/src/app_el_upload.mjs
@@ -1,8 +1,14 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { bible_interlinear_new_definitions } from "./bible_interlinear_new_definitions.mjs";
 import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
+import { list_map } from "./list_map.mjs";
 export async function app_el_upload() {
   let from = "el";
   let to = "en";
   let ds = await bible_interlinear_new_definitions();
+  list_map(ds, (d) => [
+    object_property_get(object, "property_name"),
+    object_property_get(object2, "property_name"),
+  ]);
   return await app_language_2_upload_pairs(pairs2, from, to);
 }
