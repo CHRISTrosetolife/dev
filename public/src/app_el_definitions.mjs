052553978892a978d2eab8e2296d5365a76fc475
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 19:19:51 -0500

    2025-02-11T00:19:48.551Z watch   app_el_definitions

--- public/src/app_el_definitions.mjs
+++ public/src/app_el_definitions.mjs
@@ -1,6 +1,6 @@
+import { string_split_multiple } from "./string_split_multiple.mjs";
 import { html_hr } from "./html_hr.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { json_to } from "./json_to.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { each } from "./each.mjs";
 import { bible_interlinear_new_definitions_list } from "./bible_interlinear_new_definitions_list.mjs";
@@ -10,8 +10,9 @@ export function app_el_definitions() {
   let list = bible_interlinear_new_definitions_list();
   each(list, (item) => {
     let definition = object_property_get(item, "definition");
+    let split = string_split_multiple(definition, [",", " ", ";"]);
     let word = object_property_get(item, "word");
-    html_p_text(root, json_to(item));
+    html_p_text(root, word);
     html_hr(root);
   });
 }
