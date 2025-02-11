809bfe5a8a419353181d23e4f1d57a3563a6179f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 09:48:16 -0500

    2025-02-11T14:48:08.581Z watch   app_bible_ceb

--- public/src/app_bible_ceb.mjs
+++ public/src/app_bible_ceb.mjs
@@ -1,5 +1,5 @@
-import { app_bible_verse } from "./app_bible_verse.mjs";
+import { app_bible_ceb_verse } from "./app_bible_ceb_verse.mjs";
 import { app_record_generic } from "./app_record_generic.mjs";
 export async function app_bible_ceb() {
-  await app_record_generic("engbsb", app_bible_ceb, app_bible_verse, false);
+  await app_record_generic("engbsb", app_bible_ceb, app_bible_ceb_verse, false);
 }
