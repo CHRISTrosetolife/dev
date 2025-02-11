48b67616f285f3815acac5f8e045d58cfea736c2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 11:48:24 -0500

    2025-02-11T16:48:24.096Z watch   app_tgl_upload_pairs

--- public/src/app_tgl_upload_pairs.mjs
+++ public/src/app_tgl_upload_pairs.mjs
@@ -6,12 +6,13 @@ import { list_adder_async } from "./list_adder_async.mjs";
 import { english_words_dictionary } from "./english_words_dictionary.mjs";
 import { list_take_soft } from "./list_take_soft.mjs";
 export async function app_tgl_upload_pairs(limit) {
-  let language = "Tagalog";
+  let language_to = "Tagalog";
   let filtered2 = await english_words_dictionary();
   let taken = list_take_soft(filtered2, limit);
   let pairs = await list_adder_async(async (la) => {
     await each_log_async(taken, async (w) => {
-      let inner = await definition_bohol(w, language);
+      let language_from = "English";
+      let inner = await definition_bohol(w, language_from, language_to);
       each(inner, la);
     });
   });
