f32ae30a197c0f1abae901ddffc12262d702b823
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 10:36:07 -0500

    2025-02-11T15:36:06.692Z watch   bible_ceb_upload

--- public/src/bible_ceb_upload.mjs
+++ public/src/bible_ceb_upload.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { bible_chapter } from "./bible_chapter.mjs";
 import { bible_books_chapters } from "./bible_books_chapters.mjs";
 import { bible_storage_interlinear_chapter_definitions_property } from "./bible_storage_interlinear_chapter_definitions_property.mjs";
@@ -16,6 +17,9 @@ export async function bible_ceb_upload() {
   let bible_version = "cebulb";
   let chapters = await bible_books_chapters(bible_version);
   await each_async(chapters, async (chapter) => {
+    log({
+      chapter,
+    });
     let book_name = object_property_get(chapter, "book_name");
     let chapter_name = object_property_get(chapter, "chapter_name");
     chapter = await bible_chapter(bible_version, chapter_name);
