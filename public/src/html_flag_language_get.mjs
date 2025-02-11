92a4a94bab42edec8da98183fbd15c394e9d4154
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 07:27:58 -0500

    2025-02-11T12:27:55.007Z watch   html_flag_language_get

--- public/src/html_flag_language_get.mjs
+++ public/src/html_flag_language_get.mjs
@@ -8,6 +8,7 @@ export function html_flag_language_get(language) {
     tgl: html_flag_ph(),
     en: html_flag_us(),
     el: "gr",
+    he: "il",
   };
   let g = object_property_get(languages_to_flags, language);
   return g;
