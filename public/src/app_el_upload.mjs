17f671db8e0eebf940943712e9031630586799b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 20:35:53 -0500

    2025-02-11T01:35:52.876Z watch   app_el_upload

--- public/src/app_el_upload.mjs
+++ public/src/app_el_upload.mjs
@@ -6,9 +6,10 @@ export async function app_el_upload() {
   let from = "el";
   let to = "en";
   let ds = await bible_interlinear_new_definitions();
-  list_map(ds, (d) => [
-    object_property_get(object, "property_name"),
-    object_property_get(object2, "property_name"),
+  let pairs = list_map(ds, (d) => [
+    object_property_get(d, "word"),
+    object_property_get(d, "definition"),
   ]);
+  return pairs;
   return await app_language_2_upload_pairs(pairs2, from, to);
 }
