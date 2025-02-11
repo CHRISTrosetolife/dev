9c207b386d5b278615cc7c9ee841347984b73865
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 19:08:02 -0500

    2025-02-11T00:07:58.722Z watch   app_el_upload

--- public/src/app_el_upload.mjs
+++ public/src/app_el_upload.mjs
@@ -1,8 +1,10 @@
+import { generate_list_fn } from "./generate_list_fn.mjs";
 import { bible_interlinear_new_definitions } from "./bible_interlinear_new_definitions.mjs";
 import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
 export async function app_el_upload() {
   let from = "el";
   let to = "en";
   return await bible_interlinear_new_definitions();
+  await generate_list_fn(bible_interlinear_new_definitions);
   return await app_language_2_upload_pairs(pairs2, from, to);
 }
