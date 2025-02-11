52ee3bbd3b52fdf1b7b2c22f76ead4543b3cbac7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 09:49:11 -0500

    2025-02-11T14:49:08.608Z watch   app_bible_ceb_verse

--- public/src/app_bible_ceb_verse.mjs
+++ public/src/app_bible_ceb_verse.mjs
@@ -1,1 +1,9 @@
-export function app_bible_ceb_verse() {}
+import { app_bible_verse_common } from "./app_bible_verse_common.mjs";
+export async function app_bible_ceb_verse() {
+  let middle = await app_bible_verse_common(
+    context,
+    book_code,
+    chapter,
+    verse_number,
+  );
+}
