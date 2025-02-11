13a815cb62e3c5d59d3f169244c4ac1ca31e69c9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-10 19:27:22 -0500

    2025-02-11T00:27:19.851Z watch   app_el_definitions

--- public/src/app_el_definitions.mjs
+++ public/src/app_el_definitions.mjs
@@ -1,3 +1,4 @@
+import { html_button_add } from "./html_button_add.mjs";
 import { log } from "./log.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_merge } from "./object_merge.mjs";
@@ -12,7 +13,6 @@ import { html_p_text } from "./html_p_text.mjs";
 import { each } from "./each.mjs";
 import { bible_interlinear_new_definitions_list } from "./bible_interlinear_new_definitions_list.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-import { html_button_next } from "./html_button_next.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 export function app_el_definitions() {
@@ -32,7 +32,7 @@ export function app_el_definitions() {
     each(split, (s) => {
       html_button_text_click(root, s, () => {});
     });
-    html_button_next(root, () => {});
+    html_button_add(root, () => {});
     async function word_reset() {
       if (list_is(word_builder) && list_empty_not_is(word_builder)) {
         list_add(definitions_word, list_join_space(word_builder));
