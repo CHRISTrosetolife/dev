49342d2be684816f6d09e15d4abf190eef7ad57e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 18:00:50 -0500

    2025-02-12T23:00:49.828Z watch   html_span_text_list_comma_parenthesis_params

--- public/src/html_span_text_list_comma_parenthesis_params.mjs
+++ public/src/html_span_text_list_comma_parenthesis_params.mjs
@@ -1,4 +1,3 @@
-import { list_map_property } from "./list_map_property.mjs";
 import { app_code_refresh_function_node_identifier_add } from "./app_code_refresh_function_node_identifier_add.mjs";
 import { html_span_text_list_comma_parenthesis } from "./html_span_text_list_comma_parenthesis.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -8,8 +7,7 @@ export function html_span_text_list_comma_parenthesis_params(
   args,
 ) {
   let params = object_property_get(node, "params");
-  let names = list_map_property(params, "name");
-  html_span_text_list_comma_parenthesis(parent, names, lambda);
+  html_span_text_list_comma_parenthesis(parent, params, lambda);
   function lambda(param) {
     app_code_refresh_function_node_identifier_add(parent, param, args);
   }
