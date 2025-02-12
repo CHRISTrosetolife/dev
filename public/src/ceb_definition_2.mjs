840d493344b222bfcbd8bacec64cad4a8b99efd4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 20:10:20 -0500

    2025-02-12T01:10:19.958Z watch   ceb_definition_2

--- public/src/ceb_definition_2.mjs
+++ public/src/ceb_definition_2.mjs
@@ -6,7 +6,7 @@ import { ceb_html_cache_parse_form1 } from "./ceb_html_cache_parse_form1.mjs";
 import { ceb_definition_url } from "./ceb_definition_url.mjs";
 export async function ceb_definition_2(word) {
   let url = ceb_definition_url(word);
-  let { parsed, children } = await ceb_html_cache_parse_form1(url);
+  let { parsed, children, form1 } = await ceb_html_cache_parse_form1(url);
   if (ceb_definition_no_results(children)) {
     return {
       word,
@@ -14,7 +14,7 @@ export async function ceb_definition_2(word) {
     };
   }
   let prefix_1 = ceb_definition_prefix_1();
-  let a_href_lefts = html_parse_a_href_starts_with(parsed, prefix_1);
+  let a_href_lefts = html_parse_a_href_starts_with(form1, prefix_1);
   let mapped3 = html_parse_map_text_trim(a_href_lefts);
   return mapped3;
 }
