7ec30b544d18683cfbf42d17bed4111b0a365fb8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 16:16:56 -0500

    2025-02-12T21:16:52.844Z watch   function_name_generated

--- public/src/function_name_generated.mjs
+++ public/src/function_name_generated.mjs
@@ -1,4 +1,4 @@
 import { string_suffix_without } from "./string_suffix_without.mjs";
-export function function_name_generated(fn) {
+export function function_name_generated(f_name) {
   return string_suffix_without(f_name, "_generate");
 }
