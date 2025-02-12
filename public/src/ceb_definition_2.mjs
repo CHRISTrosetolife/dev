72089f258b9f37b6525976b9875e0229bae8cc02
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 20:12:40 -0500

    2025-02-12T01:12:38.407Z watch   ceb_definition_2

--- public/src/ceb_definition_2.mjs
+++ public/src/ceb_definition_2.mjs
@@ -1,3 +1,4 @@
+import { html_parse_visit_tag } from "./html_parse_visit_tag.mjs";
 import { html_parse_visit_tag_attribute_value } from "./html_parse_visit_tag_attribute_value.mjs";
 import { html_parse_map_text_trim } from "./html_parse_map_text_trim.mjs";
 import { html_parse_a_href_starts_with } from "./html_parse_a_href_starts_with.mjs";
@@ -20,6 +21,7 @@ export async function ceb_definition_2(word) {
     "style",
     "border-top:none;color:black",
   );
+  html_parse_visit_tag(table, "tr");
   let prefix_1 = ceb_definition_prefix_1();
   let a_href_lefts = html_parse_a_href_starts_with(table, prefix_1);
   let mapped3 = html_parse_map_text_trim(a_href_lefts);
