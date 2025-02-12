533cc9f9140b862e2d48042181780e799b23a0ee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 14:22:29 -0500

    2025-02-12T19:22:28.371Z   a function_new_constant_string

--- public/src/js_return_argument_type.mjs
+++ public/src/js_return_argument_type.mjs
@@ -1,8 +1,8 @@
 import { object_property_get } from "./object_property_get.mjs";
 export function js_return_argument_type(node) {
-  let argument2 = object_property_get(node, "argument");
-  if (argument2 === null) {
+  let argument = object_property_get(node, "argument");
+  if (argument === null) {
     return null;
   }
-  return node.argument.type;
+  return argument.type;
 }
