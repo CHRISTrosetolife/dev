68a10c8a415f0e4646b3bc920f21830fd8eef27f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 14:16:08 -0500

    2025-02-12T19:16:05.441Z watch   function_new_constant_string

--- public/src/function_new_constant_string.mjs
+++ public/src/function_new_constant_string.mjs
@@ -1,1 +1,4 @@
-export function function_new_constant_string(f_name, value) {}
+import { function_new } from "./function_new.mjs";
+export async function function_new_constant_string(f_name, value) {
+  await function_new();
+}
