65f626a0159d6face58c1445f7151e611cec262d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 20:03:00 -0500

    2025-02-12T01:02:59.707Z watch   ceb_definition_2

--- public/src/ceb_definition_2.mjs
+++ public/src/ceb_definition_2.mjs
@@ -1,6 +1,13 @@
+import { ceb_definition_no_results } from "./ceb_definition_no_results.mjs";
 import { ceb_html_cache_parse_form1 } from "./ceb_html_cache_parse_form1.mjs";
 import { ceb_definition_url } from "./ceb_definition_url.mjs";
 export async function ceb_definition_2(word) {
   let url = ceb_definition_url(word);
   let { parsed, children } = await ceb_html_cache_parse_form1(url);
+  if (ceb_definition_no_results(children)) {
+    return {
+      word,
+      definitions: [],
+    };
+  }
 }
