60ebda87046add59144af92b9b31017a768cb35f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 15:33:07 -0500

    2025-02-12T20:33:04.829Z watch   js_visit_identifiers_named

--- public/src/js_visit_identifiers_named.mjs
+++ public/src/js_visit_identifiers_named.mjs
@@ -1,9 +1,11 @@
 import { equal } from "./equal.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_visit_identifiers } from "./js_visit_identifiers.mjs";
+import { js_node_is } from "./js_node_is.mjs";
 export function js_visit_identifiers_named(ast, search, lambda) {
   js_visit_identifiers(ast, (v) => {
     let { node } = v;
+    let identifier_is = js_node_is("Identifier");
     let name = object_property_get(node, "name");
     if (equal(name, search)) {
       lambda(node);
