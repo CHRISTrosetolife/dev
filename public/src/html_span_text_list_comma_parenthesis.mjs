248fedfad18266b04a76cfdd5ed0cf5f88c67f05
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 17:21:03 -0500

    2025-02-12T22:21:02.613Z watch   html_span_text_list_comma_parenthesis

--- public/src/html_span_text_list_comma_parenthesis.mjs
+++ public/src/html_span_text_list_comma_parenthesis.mjs
@@ -1,9 +1,7 @@
-import { html_span_text_gray } from "./html_span_text_gray.mjs";
+import { html_span_text_wrap_parenthesis } from "./html_span_text_wrap_parenthesis.mjs";
 import { html_span_text_list_comma } from "./html_span_text_list_comma.mjs";
 export function html_span_text_list_comma_parenthesis(parent, list, lambda) {
-  html_span_text_gray(parent, "( ");
-  lambda(parent);
-  html_span_text_gray(parent, " )");
+  html_span_text_wrap_parenthesis(parent, lambda);
   function lambda(parent) {
     html_span_text_list_comma(parent, list, lambda);
   }
---
+++ public/src/html_span_text_wrap_parenthesis.mjs
@@ -0,0 +1,6 @@
+import { html_span_text_gray } from "./html_span_text_gray.mjs";
+export function html_span_text_wrap_parenthesis(parent, lambda) {
+  html_span_text_gray(parent, "( ");
+  lambda(parent);
+  html_span_text_gray(parent, " )");
+}
