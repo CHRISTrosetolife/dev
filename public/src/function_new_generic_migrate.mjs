6bd3ca075c0a33fd57faa2d6fb600be2e32d625d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 15:21:00 -0500

    2025-02-12T20:21:00.052Z watch   function_new_generic_migrate

--- public/src/function_new_generic_migrate.mjs
+++ public/src/function_new_generic_migrate.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { js_visit_calls } from "./js_visit_calls.mjs";
 import { each_object_async } from "./each_object_async.mjs";
@@ -8,7 +9,9 @@ export async function function_new_generic_migrate() {
   let results = await functions_identifier(f_name);
   await each_object_async(results, async (f_name_result) => {
     await function_transform(f_name_result, async (ast) => {
-      js_visit_calls(ast, f_name, (v) => {});
+      js_visit_calls(ast, f_name, (v) => {
+        let node = object_property_get(v, "node");
+      });
     });
   });
   return results;
