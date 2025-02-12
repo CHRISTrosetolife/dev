84ee5d15809ad595080658c0f8df70d6c949db27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 20:18:04 -0500

    2025-02-12T01:18:02.393Z watch   ceb_definition_2

--- public/src/ceb_definition_2.mjs
+++ public/src/ceb_definition_2.mjs
@@ -7,6 +7,8 @@ import { ceb_definition_prefix_1 } from "./ceb_definition_prefix_1.mjs";
 import { ceb_definition_no_results } from "./ceb_definition_no_results.mjs";
 import { ceb_html_cache_parse_form1 } from "./ceb_html_cache_parse_form1.mjs";
 import { ceb_definition_url } from "./ceb_definition_url.mjs";
+import { assert } from "./assert.mjs";
+import { list_size_2 } from "./list_size_2.mjs";
 export async function ceb_definition_2(word) {
   let url = ceb_definition_url(word);
   let { parsed, children, form1 } = await ceb_html_cache_parse_form1(url);
@@ -25,6 +27,7 @@ export async function ceb_definition_2(word) {
   let rows = html_parse_visit_tag_list(table, "tr");
   each(rows, (row) => {
     let columns = html_parse_visit_tag_list(table, "td");
+    assert(list_size_2, [columns]);
   });
   let prefix_1 = ceb_definition_prefix_1();
   let a_href_lefts = html_parse_a_href_starts_with(table, prefix_1);
