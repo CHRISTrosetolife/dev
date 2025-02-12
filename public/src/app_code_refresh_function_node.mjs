311c84a9b581ba30d45c842df22b1e2168a34ee5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 13:43:08 -0500

    2025-02-12T18:43:05.065Z watch   app_code_refresh_function_node

--- public/src/app_code_refresh_function_node.mjs
+++ public/src/app_code_refresh_function_node.mjs
@@ -42,8 +42,9 @@ export function app_code_refresh_function_node(args) {
     html_style_display_none(imports_container);
     let imports = html_div(imports_container);
     html_style_display_none(imports);
-    html_button(imports_container, "Show imports", () => {
+    let imports_show = html_button(imports_container, "Show imports", () => {
       html_style_display_block(imports);
+      html_style_display_none(imports);
     });
     let only_imports = true;
     each(body, async (b) => {
