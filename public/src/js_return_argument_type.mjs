3a31a439f8f64d033385bc1c4345761c4c81a4bf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 14:21:21 -0500

    2025-02-12T19:21:20.926Z   a js_return_argument_type

--- public/src/js_return_argument_type.mjs
+++ public/src/js_return_argument_type.mjs
@@ -1,5 +1,7 @@
 import { object_property_get } from "./object_property_get.mjs";
 export function js_return_argument_type(node) {
   let argument2 = object_property_get(node, "argument");
+  if (false) {
+  }
   return node.argument.type;
 }
