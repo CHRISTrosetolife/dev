92478aa498afe660eedfe6f4517c188afbc0cd8a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 14:50:36 -0500

    2025-02-12T19:50:35.731Z function_new_generic_migrate

--- public/src/function_new_generic_migrate.mjs
+++ public/src/function_new_generic_migrate.mjs
@@ -4,6 +4,6 @@ import { functions_identifier } from "./functions_identifier.mjs";
 export async function function_new_generic_migrate() {
   let f_name = fn_name("function_new_generic");
   let results = await functions_identifier(f_name);
-  await each_object_async(object, async (key, value) => {});
+  await each_object_async(results, async (key, value) => {});
   return results;
 }
