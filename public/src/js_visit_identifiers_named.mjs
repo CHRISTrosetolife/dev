4c8c88857f1dfaec512898b126809502631d4839
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 15:33:40 -0500

    2025-02-12T20:33:39.879Z watch   js_visit_identifiers_named

--- public/src/js_visit_identifiers_named.mjs
+++ public/src/js_visit_identifiers_named.mjs
@@ -5,9 +5,9 @@ import { js_node_is } from "./js_node_is.mjs";
 export function js_visit_identifiers_named(ast, search, lambda) {
   js_visit_identifiers(ast, (v) => {
     let { node } = v;
-    let identifier_is = js_node_is("Identifier");
-    let name = object_property_get(node, "name");
-    if (equal(name, search)) {
+    let identifier_is = js_node_is(node, "Identifier");
+    let e = identifier_is && equal(object_property_get(node, "name"), search);
+    if (e) {
       lambda(node);
     }
   });
