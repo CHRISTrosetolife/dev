c8ddf5dcf1c2291c45a28146d460276ab8638101
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 16:16:00 -0500

    2025-02-12T21:15:59.296Z watch   html_scripts_load

--- public/src/html_scripts_load.mjs
+++ public/src/html_scripts_load.mjs
@@ -1,11 +1,14 @@
+import { fn_name } from "./fn_name.mjs";
 import { list_wait } from "./list_wait.mjs";
 import { function_run_arg } from "./function_run_arg.mjs";
-import { html_script_generate } from "./html_script_generate.mjs";
 import { function_name_generated_combine } from "./function_name_generated_combine.mjs";
 import { list_map } from "./list_map.mjs";
 export async function html_scripts_load(root, scripts) {
   let mapped = list_map(scripts, async (script) => {
-    let name = function_name_generated_combine(html_script_generate, script);
+    let name = function_name_generated_combine(
+      fn_name("html_script_generate"),
+      script,
+    );
     await function_run_arg(name, root);
   });
   await list_wait(mapped);
