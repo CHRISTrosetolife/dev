0eb3cff6dba2d18fd4defc334bd44fccb7bfa67a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 15:38:06 -0500

    2025-02-12T20:38:04.970Z watch   function_new_generic_migrate

--- public/src/function_new_generic_migrate.mjs
+++ public/src/function_new_generic_migrate.mjs
@@ -3,7 +3,6 @@ import { list_last } from "./list_last.mjs";
 import { lists_sizes_equal } from "./lists_sizes_equal.mjs";
 import { function_params_names } from "./function_params_names.mjs";
 import { assert } from "./assert.mjs";
-import { log } from "./log.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { js_visit_calls } from "./js_visit_calls.mjs";
 import { each_object_async } from "./each_object_async.mjs";
@@ -16,9 +15,6 @@ export async function function_new_generic_migrate() {
   let results = await functions_identifier(f_name);
   let param_names = await function_params_names(f_name);
   await each_object_async(results, async (f_name_result) => {
-    log({
-      f_name_result,
-    });
     await function_transform(f_name_result, async (ast) => {
       js_visit_calls(ast, f_name, (params) => {
         assert(lists_sizes_equal, [[params, param_names]]);
