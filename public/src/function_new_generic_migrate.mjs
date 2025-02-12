1f2a886c1e82fb83f3883b8249d5bac1341cb4ef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 14:49:51 -0500

    2025-02-12T19:49:50.137Z watch   function_new_generic_migrate

--- public/src/function_new_generic_migrate.mjs
+++ public/src/function_new_generic_migrate.mjs
@@ -2,6 +2,7 @@ import { fn_name } from "./fn_name.mjs";
 import { functions_identifier } from "./functions_identifier.mjs";
 export async function function_new_generic_migrate() {
   let f_name = fn_name("function_new_generic");
-  let results = await functions_identifier(f_name);$eoa
+  let results = await functions_identifier(f_name);
+  $eoa;
   return results;
 }
