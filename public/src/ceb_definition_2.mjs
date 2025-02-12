dfd68eaeaf5e80d9d214e39ae71aa5730c5b6577
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 20:21:23 -0500

    2025-02-12T01:21:22.607Z watch   ceb_definition_2

--- public/src/ceb_definition_2.mjs
+++ public/src/ceb_definition_2.mjs
@@ -11,6 +11,8 @@ import { ceb_html_cache_parse_form1 } from "./ceb_html_cache_parse_form1.mjs";
 import { ceb_definition_url } from "./ceb_definition_url.mjs";
 import { assert } from "./assert.mjs";
 import { list_size_2 } from "./list_size_2.mjs";
+import { object_merge_strict } from "./object_merge_strict.mjs";
+import { list_single } from "./list_single.mjs";
 export async function ceb_definition_2(word) {
   let url = ceb_definition_url(word);
   let { parsed, children, form1 } = await ceb_html_cache_parse_form1(url);
@@ -26,15 +28,19 @@ export async function ceb_definition_2(word) {
     "style",
     "border-top:none;color:black",
   );
+  let result = {};
+  let prefix_1 = ceb_definition_prefix_1();
   let rows = html_parse_visit_tag_list(table, "tr");
   each(rows, (row) => {
     let columns = html_parse_visit_tag_list(row, "td");
     assert(list_size_2, [columns]);
     let f = list_first(columns);
+    let a_href_fs = html_parse_a_href_starts_with(f, prefix_1);
+    let word_cebs = html_parse_map_text_trim(a_href_fs);
+    object_merge_strict(result, {
+      [list_single(word_cebs)]: [],
+    });
     let s = list_second(columns);
   });
-  let prefix_1 = ceb_definition_prefix_1();
-  let a_href_lefts = html_parse_a_href_starts_with(table, prefix_1);
-  let mapped3 = html_parse_map_text_trim(a_href_lefts);
   return mapped3;
 }
