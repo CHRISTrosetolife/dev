c38346b3e1a0dd3ee72a158240d5c5f2f6281461
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 14:59:15 -0500

    2025-02-12T19:59:14.991Z a functions_transform

--- public/src/functions_transform.mjs
+++ public/src/functions_transform.mjs
@@ -1,19 +1,8 @@
+import { function_transform } from "./function_transform.mjs";
 import { functions_list } from "./functions_list.mjs";
-import { file_js_transform_regular } from "./file_js_transform_regular.mjs";
-import { log } from "./log.mjs";
-import { function_name_to_path } from "./function_name_to_path.mjs";
 import { each_async } from "./each_async.mjs";
 export async function functions_transform(functions_list, lambda) {
   await each_async(functions_list, async (e) => {
     await function_transform(e, lambda);
   });
 }
-async function function_transform(e, lambda) {
-    let file_path = function_name_to_path(e);
-    log({
-        file_path,
-        e,
-    });
-    await file_js_transform_regular(file_path, lambda);
-}
-
