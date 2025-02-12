d9583501a8e9eea9216e0c710b573d2acbb99cec
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 15:05:06 -0500

    2025-02-12T20:05:05.806Z c string_trail_underscore string_trail_dollar

---
+++ public/src/string_trail_dollar.mjs
@@ -0,0 +1,4 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function string_trail_dollar(log_prefix_start) {
+  return string_combine_multiple([log_prefix_start, "_"]);
+}
