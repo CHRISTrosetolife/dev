de80a117c1d4c433ad1daaa4bc4ace63ee15bd5a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 20:09:41 -0500

    2025-02-12T01:09:38.870Z watch   ceb_form1

--- public/src/ceb_form1.mjs
+++ public/src/ceb_form1.mjs
@@ -3,12 +3,15 @@ export function ceb_form1(root) {
   let tag_name = "form";
   let attribute_name = "name";
   let attribute_value = "form1";
-  let q_single = html_parse_visit_tag_attribute_value(
+  let form1 = html_parse_visit_tag_attribute_value(
     root,
     tag_name,
     attribute_name,
     attribute_value,
   );
-  let { childNodes: children } = q_single;
-  return children;
+  let { childNodes: children } = form1;
+  return {
+    children,
+    form1,
+  };
 }
