efc86961b88b545767745e799dd30a53b990c116
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 15:40:18 -0500

    2025-02-12T20:40:17.556Z watch   js_node_identifier_named

--- public/src/js_node_identifier_named.mjs
+++ public/src/js_node_identifier_named.mjs
@@ -1,8 +1,8 @@
+import { js_node_type_is } from "./js_node_type_is.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { equal } from "./equal.mjs";
-import { js_node_is } from "./js_node_is.mjs";
 export function js_node_identifier_named(node, name) {
-  let identifier_is = js_node_is(node, "Identifier");
+  let identifier_is = js_node_type_is(node, "Identifier");
   let e = identifier_is && equal(object_property_get(node, "name"), name);
   return e;
 }
