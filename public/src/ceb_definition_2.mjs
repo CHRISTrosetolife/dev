377b325c450f160f843ebe2230e77cd40730830e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 20:11:40 -0500

    2025-02-12T01:11:39.252Z watch   ceb_definition_2

--- public/src/ceb_definition_2.mjs
+++ public/src/ceb_definition_2.mjs
@@ -1,3 +1,4 @@
+import { html_parse_visit_tag_attribute_value } from "./html_parse_visit_tag_attribute_value.mjs";
 import { html_parse_map_text_trim } from "./html_parse_map_text_trim.mjs";
 import { html_parse_a_href_starts_with } from "./html_parse_a_href_starts_with.mjs";
 import { ceb_definition_prefix_1 } from "./ceb_definition_prefix_1.mjs";
@@ -13,9 +14,14 @@ export async function ceb_definition_2(word) {
       definitions: [],
     };
   }
-  ("border-top:none;color:black");
+  let table = html_parse_visit_tag_attribute_value(
+    form1,
+    "tbody",
+    "style",
+    "border-top:none;color:black",
+  );
   let prefix_1 = ceb_definition_prefix_1();
-  let a_href_lefts = html_parse_a_href_starts_with(form1, prefix_1);
+  let a_href_lefts = html_parse_a_href_starts_with(table, prefix_1);
   let mapped3 = html_parse_map_text_trim(a_href_lefts);
   return mapped3;
 }
