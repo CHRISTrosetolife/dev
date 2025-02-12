8f30d6270c2fde91990e1cda070e505f0939ef80
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 20:04:47 -0500

    2025-02-12T01:04:46.690Z watch   ceb_definition_2

--- public/src/ceb_definition_2.mjs
+++ public/src/ceb_definition_2.mjs
@@ -1,3 +1,6 @@
+import { html_parse_map_text_trim } from "./html_parse_map_text_trim.mjs";
+import { html_parse_a_href_starts_with } from "./html_parse_a_href_starts_with.mjs";
+import { ceb_definition_prefix_1 } from "./ceb_definition_prefix_1.mjs";
 import { ceb_definition_no_results } from "./ceb_definition_no_results.mjs";
 import { ceb_html_cache_parse_form1 } from "./ceb_html_cache_parse_form1.mjs";
 import { ceb_definition_url } from "./ceb_definition_url.mjs";
@@ -10,4 +13,7 @@ export async function ceb_definition_2(word) {
       definitions: [],
     };
   }
+  let prefix_1 = ceb_definition_prefix_1();
+  let a_href_lefts = html_parse_a_href_starts_with(parsed, prefix_1);
+  let mapped3 = html_parse_map_text_trim(a_href_lefts);
 }
