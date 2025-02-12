582529d8f9a7f083b2f737c20393feabe43ed8dc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 16:01:32 -0500

    2025-02-12T21:01:31.955Z watch   function_new

--- public/src/function_new.mjs
+++ public/src/function_new.mjs
@@ -1,13 +1,7 @@
-import { fn_name } from "./fn_name.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { log_error } from "./log_error.mjs";
-import { function_exists } from "./function_exists.mjs";
-import { function_open } from "./function_open.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
 export async function function_new(function_name) {
   assert_arguments_length(arguments, 1);
-  fn_name("function_new_generic"), " might overwrite a function";
   let open = true;
   await function_new_generic(function_name, "", "", open, [], false, false);
 }
