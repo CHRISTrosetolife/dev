9316f02d2bad75aa70c792d37d968097473bfa01
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 15:30:45 -0500

    2025-02-12T20:30:41.412Z watch   bible_books_prefix_to_name_generate

--- public/src/generate_list_generic.mjs
+++
@@ -1,8 +0,0 @@
-import { file_overwrite } from "./file_overwrite.mjs";
-import { js_code_array } from "./js_code_array.mjs";
-import { js_code_statement_return } from "./js_code_statement_return.mjs";
-import { function_new_generic } from "./function_new_generic.mjs";
-export async function generate_list_generic(delimited, name) {
-  let r = js_code_statement_return(js_code_array(delimited));
-  await function_new_generic(name, "", r, false, [], false, file_overwrite);
-}
