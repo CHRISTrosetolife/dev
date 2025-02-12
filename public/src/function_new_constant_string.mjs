a41712b5f95ee57ab6e10a92b965c05191dcea04
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 16:04:31 -0500

    2025-02-12T21:04:30.061Z watch   function_new_constant_string

--- public/src/function_new_constant_string.mjs
+++ public/src/function_new_constant_string.mjs
@@ -1,5 +1,15 @@
+import { js_code_string } from "./js_code_string.mjs";
 import { function_new_generic } from "./function_new_generic.mjs";
+import { js_code_statement_return } from "./js_code_statement_return.mjs";
 export async function function_new_constant_string(f_name, value) {
   let open = true;
-  await function_new_generic(f_name, "", "", open, [], false, false);
+  await function_new_generic(
+    f_name,
+    "",
+    js_code_statement_return(js_code_string(value)),
+    open,
+    [],
+    false,
+    false,
+  );
 }
