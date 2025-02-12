0580a302e0a986429dda4dfdd75d5bf35462ed0b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-11 20:09:55 -0500

    2025-02-12T01:09:53.765Z watch   ceb_html_cache_parse_form1

--- public/src/ceb_html_cache_parse_form1.mjs
+++ public/src/ceb_html_cache_parse_form1.mjs
@@ -2,9 +2,10 @@ import { ceb_form1 } from "./ceb_form1.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 export async function ceb_html_cache_parse_form1(url) {
   let parsed = await html_cache_parse(url);
-  let children = ceb_form1(parsed);
+  let { children, form1 } = ceb_form1(parsed);
   return {
     parsed,
     children,
+    form1,
   };
 }
