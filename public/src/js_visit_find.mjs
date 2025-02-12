395737114a5b4d9a30c6dc7dc2cad52f8ff226e7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-12 18:09:39 -0500

    2025-02-12T23:09:38.505Z watch   js_visit_find

--- public/src/js_visit_find.mjs
+++ public/src/js_visit_find.mjs
@@ -1,6 +1,6 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { js_visit } from "./js_visit.mjs";
-export function js_visit_find(identifier) {
+export function js_visit_find(ast, identifier) {
   let found = null;
   js_visit(ast, (v) => {
     let node = object_property_get(v, "node");
