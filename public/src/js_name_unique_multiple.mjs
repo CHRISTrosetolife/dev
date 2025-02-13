353e66c09428b7effc763524cee53f22f461000f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 12:02:15 -0500

    2025-02-13T17:02:13.194Z watch   js_name_unique_multiple

--- public/src/js_name_unique_multiple.mjs
+++ public/src/js_name_unique_multiple.mjs
@@ -3,9 +3,11 @@ import { string_combine } from "./string_combine.mjs";
 import { js_identifiers } from "./js_identifiers.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_concat_multiple } from "./list_concat_multiple.mjs";
+import { list_add_multiple } from "./list_add_multiple.mjs";
 export function js_name_unique_multiple(asts, prefix) {
   let identifiers = list_concat_multiple(list_map(asts, js_identifiers));
-  let keywords = "arguments";
+  let keywords = ["arguments"];
+  list_add_multiple(identifiers, keywords);
   let i = 1;
   let variable_name;
   while (true) {
