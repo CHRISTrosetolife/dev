906888225c1b2e213b802c774301ac7d71ef18f8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 16:43:16 -0500

    2025-02-13T21:43:15.534Z watch   html_child_nodes

--- public/src/html_child_nodes.mjs
+++ public/src/html_child_nodes.mjs
@@ -1,5 +1,7 @@
+import { object_property_get } from "./object_property_get.mjs";
 export function html_child_nodes(parent) {
   let { childNodes } = parent;
+  object_property_get(object, "property_name");
   let v = childNodes;
   return v;
 }
