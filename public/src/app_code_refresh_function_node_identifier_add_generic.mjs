01f04fe08244c40796ad19c574e3ba24f7747188
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 17:46:28 -0500

    2025-02-13T22:46:27.884Z watch   app_code_refresh_function_node_identifier_add_generic

--- public/src/app_code_refresh_function_node_identifier_add_generic.mjs
+++ public/src/app_code_refresh_function_node_identifier_add_generic.mjs
@@ -44,7 +44,7 @@ export function app_code_refresh_function_node_identifier_add_generic(
   html_on_click(component, function () {
     let selected = object_property_initialize(data, "selected", map_new());
     if (object_property_toggle(toggled, name)) {
-      if (map_size(selected) >= 3) {
+      if (map_size(selected) >= 2) {
         let v3;
         return v3;
       }
