44d6363d29968cab8a018c81e42590a347f9c385
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 11:54:00 -0500

    2025-02-13T16:53:59.143Z watch   js_function_expression_simplify

--- public/src/js_function_expression_simplify.mjs
+++ public/src/js_function_expression_simplify.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { js_node_type_statement_block_is } from "./js_node_type_statement_block_is.mjs";
 import { assert } from "./assert.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -7,6 +8,8 @@ export function js_function_expression_simplify(ast) {
     let { node } = v;
     let body = object_property_get(node, "body");
     assert(js_node_type_statement_block_is, [body]);
-    $l$body;
+    log({
+      body,
+    });
   });
 }
