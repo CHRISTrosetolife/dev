6c3c2bd1d213c6c1e4112a76d4aa52e562830e48
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 18:07:24 -0500

    2025-02-13T23:07:22.232Z watch   html_loader

--- public/src/html_loader.mjs
+++ public/src/html_loader.mjs
@@ -1,3 +1,4 @@
+import { fn_name } from "./fn_name.mjs";
 import { html_overlay } from "./html_overlay.mjs";
 import { html_style_font_size_default_multiplied } from "./html_style_font_size_default_multiplied.mjs";
 import { html_style_centered } from "./html_style_centered.mjs";
@@ -5,7 +6,7 @@ import { html_spacer_vertical_n } from "./html_spacer_vertical_n.mjs";
 import { html_style_font_color_white } from "./html_style_font_color_white.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 export function html_loader() {
-  let overlay = html_overlay($t);
+  let overlay = html_overlay(fn_name("html_loader"));
   let label = html_p_text(overlay, "Loading...");
   html_style_font_color_white(label);
   html_spacer_vertical_n(label, 10);
