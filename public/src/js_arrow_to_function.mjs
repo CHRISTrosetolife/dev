51aa64beb563628399284eca25ec426ec85b66fd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 11:43:15 -0500

    2025-02-13T16:43:14.025Z watch   js_arrow_to_function

--- public/src/js_arrow_to_function.mjs
+++ public/src/js_arrow_to_function.mjs
@@ -1,5 +1,6 @@
+import { object_replace } from "./object_replace.mjs";
+import { js_block_statement } from "./js_block_statement.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { log } from "./log.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
@@ -8,10 +9,9 @@ export function js_arrow_to_function(ast) {
     let { node } = v;
     object_property_set(node, "type", "FunctionExpression");
     let body = object_property_get(node, "body");
-    if (js_node_type_is(body, "BlockStatement")) {
+    if (!js_node_type_is(body, "BlockStatement")) {
+      let wrapped = js_block_statement(body);
+      object_replace(body, wrapped);
     }
-    log({
-      node,
-    });
   });
 }
