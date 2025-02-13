b32dd4a830288c1378435401a17cd6c9b011d5d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 18:48:32 -0500

    2025-02-13T23:48:32.235Z watch   app_new_bible_ceb

--- public/src/app_new_bible_ceb.mjs
+++ public/src/app_new_bible_ceb.mjs
@@ -1,13 +1,16 @@
+import { string_unique } from "./string_unique.mjs";
 import { js_identifier_rename } from "./js_identifier_rename.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { function_parse } from "./function_parse.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_new_generic } from "./app_new_generic.mjs";
 export async function app_new_bible_ceb(name) {
-  let ast = await function_parse(fn_name("app_bible_ceb"));
+  let a = fn_name("app_bible_ceb");
+  let ast = await function_parse(a);
   let fnns = function_names();
   await function_transform(ast, function (ast) {
-    js_identifier_rename();
+    let a_new = string_unique(a, fnns);
+    js_identifier_rename(ast, a, a_new);
   });
   let body_string = "let root = html_style_default_initialize();";
   let v = await app_new_generic(name, body_string);
