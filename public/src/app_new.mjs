24057f824cdc85af795c7935edf740da91570e77
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 18:43:58 -0500

    2025-02-13T23:43:56.919Z watch   app_new

--- public/src/app_new.mjs
+++ public/src/app_new.mjs
@@ -1,23 +1,6 @@
-import { apps_list_generate } from "./apps_list_generate.mjs";
-import { app_html_generate_dev } from "./app_html_generate_dev.mjs";
-import { app_identifier } from "./app_identifier.mjs";
-import { function_new_generic } from "./function_new_generic.mjs";
-import { app_html_generate } from "./app_html_generate.mjs";
+import { app_new_generic } from "./app_new_generic.mjs";
 export async function app_new(name) {
   let body_string = "let root = html_style_default_initialize();";
-  await app_html_generate(name);
-  await app_html_generate_dev(name);
-  let name_prefixed = app_identifier(name);
-  let open = true;
-  await function_new_generic(
-    name_prefixed,
-    "",
-    body_string,
-    open,
-    [],
-    false,
-    false,
-  );
-  await apps_list_generate();
-  return name_prefixed;
+  let v = await app_new_generic(name, body_string);
+  return v;
 }
---
+++ public/src/app_new_generic.mjs
@@ -0,0 +1,22 @@
+import { apps_list_generate } from "./apps_list_generate.mjs";
+import { function_new_generic } from "./function_new_generic.mjs";
+import { app_identifier } from "./app_identifier.mjs";
+import { app_html_generate_dev } from "./app_html_generate_dev.mjs";
+import { app_html_generate } from "./app_html_generate.mjs";
+export async function app_new_generic(name, body_string) {
+  await app_html_generate(name);
+  await app_html_generate_dev(name);
+  let name_prefixed = app_identifier(name);
+  let open = true;
+  await function_new_generic(
+    name_prefixed,
+    "",
+    body_string,
+    open,
+    [],
+    false,
+    false,
+  );
+  await apps_list_generate();
+  return name_prefixed;
+}
