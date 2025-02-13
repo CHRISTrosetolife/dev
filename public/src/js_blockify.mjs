a3494178a2c011d3449c62f8762dbd425d939bb8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 12:52:20 -0500

    2025-02-13T17:52:19.754Z watch   js_blockify

--- public/src/js_blockify.mjs
+++ public/src/js_blockify.mjs
@@ -1,3 +1,4 @@
+import { js_parse_first_function } from "./js_parse_first_function.mjs";
 import { js_code_statement_return } from "./js_code_statement_return.mjs";
 import { js_node_type_statement_block_is } from "./js_node_type_statement_block_is.mjs";
 import { object_replace } from "./object_replace.mjs";
@@ -6,6 +7,7 @@ import { js_block_statement } from "./js_block_statement.mjs";
 export function js_blockify(body) {
   if (!js_node_type_statement_block_is(body)) {
     let r = js_code_statement_return("");
+    js_parse_first_function(r);
     let wrapped = js_block_statement([object_copy_shallow(body)]);
     object_replace(body, wrapped);
   }
