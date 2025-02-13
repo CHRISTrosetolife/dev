71746381ea0fe30fc0c4126d792bd85c03360349
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 11:47:42 -0500

    2025-02-13T16:47:42.006Z watch   js_function_expression_simplify

--- public/src/js_function_expression_simplify.mjs
+++ public/src/js_function_expression_simplify.mjs
@@ -1,1 +1,12 @@
-export function js_function_expression_simplify(ast) {}
+import { js_blockify } from "./js_blockify.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+import { js_visit_node } from "./js_visit_node.mjs";
+export function js_function_expression_simplify(ast) {
+  js_visit_node(ast, "FunctionExpression", (v) => {
+    let { node } = v;
+    object_property_set(node, "type", "FunctionExpression");
+    let body = object_property_get(node, "body");
+    js_blockify(body);
+  });
+}
