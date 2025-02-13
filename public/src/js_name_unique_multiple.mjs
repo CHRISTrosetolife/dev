e781be769f481d0abf81565719d982c845ffbccc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 18:46:57 -0500

    2025-02-13T23:46:56.778Z watch   js_name_unique_multiple

--- public/src/js_name_unique_multiple.mjs
+++ public/src/js_name_unique_multiple.mjs
@@ -1,9 +1,9 @@
 import { list_includes } from "./list_includes.mjs";
-import { string_combine } from "./string_combine.mjs";
 import { js_identifiers } from "./js_identifiers.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_concat_multiple } from "./list_concat_multiple.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function js_name_unique_multiple(asts, prefix) {
   let identifiers = list_concat_multiple(list_map(asts, js_identifiers));
   let keywords = ["arguments"];
@@ -11,7 +11,8 @@ export function js_name_unique_multiple(asts, prefix) {
   let i = 1;
   let variable_name;
   while (true) {
-    variable_name = i === 1 ? prefix : string_combine(prefix, i);
+    variable_name =
+      i === 1 ? prefix : string_combine_multiple([prefix, "_", i]);
     if (!list_includes(identifiers, variable_name)) {
       break;
     }
