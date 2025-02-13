f9b40730efcbb80e7767231e23675531d8df819f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 11:42:01 -0500

    2025-02-13T16:42:00.874Z watch   js_arrow_to_function

--- public/src/js_arrow_to_function.mjs
+++ public/src/js_arrow_to_function.mjs
@@ -2,12 +2,13 @@ import { object_property_get } from "./object_property_get.mjs";
 import { log } from "./log.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
 export function js_arrow_to_function(ast) {
   js_visit_node(ast, "ArrowFunctionExpression", (v) => {
     let { node } = v;
     object_property_set(node, "type", "FunctionExpression");
     let body = object_property_get(node, "body");
-    if (false) {
+    if (js_node_type_is(body, "BlockStatement")) {
     }
     log({
       node,
