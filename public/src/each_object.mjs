4c083dcedfbcc43c5a7014e63df0a23b83221ff9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 12:35:13 -0500

    2025-02-13T17:35:13.306Z watch   each_object

--- public/src/each_object.mjs
+++ public/src/each_object.mjs
@@ -1,6 +1,7 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { object_properties } from "./object_properties.mjs";
 export function each_object(object, lambda) {
+  let keys_get = object_properties;
   let keys = object_properties(object);
   for (let key of keys) {
     let value = object_property_get(object, key);
