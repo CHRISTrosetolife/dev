d74d804a13e1132c6c0a8879c43bf4da76829348
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 12:20:18 -0500

    2025-02-13T17:20:17.205Z watch   assert_multiple

--- public/src/assert_multiple.mjs
+++ public/src/assert_multiple.mjs
@@ -1,6 +1,6 @@
 import { assert } from "./assert.mjs";
 import { each } from "./each.mjs";
-export function assert_multiple(fn, params) {
+export function assert_multiple(params, fn) {
   each(params, (param) => {
     assert(fn, [param]);
   });
