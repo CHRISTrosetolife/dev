3ca69e66610f830b5d0b5026649f6e6374db4301
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 09:44:39 -0500

    2025-02-13T14:44:31.951Z watch   object_property_toggle

--- public/src/object_property_toggle.mjs
+++ public/src/object_property_toggle.mjs
@@ -1,5 +1,7 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 export function object_property_toggle(toggled, name) {
-  object_property_set(toggled, name, !object_property_get(toggled, name));
+  let value_new = !object_property_get(toggled, name);
+  object_property_set(toggled, name, value_new);
+  return value_new;
 }
