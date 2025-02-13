f86c07708a5eda832e0b88268d9d0c639cf821f2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 15:18:04 -0500

    2025-02-13T20:18:03.360Z watch   app_code_refresh_function_node_identifier_add_generic

--- public/src/app_code_refresh_function_node_identifier_add_generic.mjs
+++ public/src/app_code_refresh_function_node_identifier_add_generic.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { app_code_refresh_function_node_nodes } from "./app_code_refresh_function_node_nodes.mjs";
 import { list_get } from "./list_get.mjs";
 import { list_index } from "./list_index.mjs";
@@ -63,9 +62,7 @@ export function app_code_refresh_function_node_identifier_add_generic(
             let next = list_get(v, index_next);
             let m = map_get(nodes, next);
             let parent = object_property_get(m, "parent");
-            log({
-              parent,
-            });
+            html_style_background_color(parent, "Maroon");
           });
         }
       }
