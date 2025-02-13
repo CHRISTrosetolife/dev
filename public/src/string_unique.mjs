61bcc4b18302ed886a2d7949a12b37ab68036786
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 18:55:30 -0500

    2025-02-13T23:55:30.271Z watch   string_unique

--- public/src/string_unique.mjs
+++ public/src/string_unique.mjs
@@ -4,8 +4,7 @@ export function string_unique(prefix, existing) {
   let i = 1;
   let variable_name;
   while (true) {
-    variable_name =
-      i === 1 ? prefix : string_combine_multiple([prefix, "_", i]);
+    variable_name = i === 1 ? prefix : string_combine_multiple([prefix, i]);
     if (!list_includes(existing, variable_name)) {
       break;
     }
