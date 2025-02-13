e1b13f540f80bf14965e59bc0f8c2efe8ba88d2f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 11:58:41 -0500

    2025-02-13T16:58:38.793Z watch   js_function_expression_simplify

--- public/src/js_function_expression_simplify.mjs
+++ public/src/js_function_expression_simplify.mjs
@@ -5,6 +5,7 @@ import { object_property_get } from "./object_property_get.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 import { list_size_1 } from "./list_size_1.mjs";
 import { list_first } from "./list_first.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
 export function js_function_expression_simplify(ast) {
   js_visit_node(ast, "FunctionExpression", (v) => {
     let { node } = v;
@@ -13,7 +14,8 @@ export function js_function_expression_simplify(ast) {
     let body2 = object_property_get(body, "body");
     if (list_size_1(body2)) {
       let f = list_first(body2);
-      if (false) {
+      if (js_node_type_is(f, "ExpressionStatement")) {
+        let expression = object_property_get(f, "expression");
       }
       log({
         f,
