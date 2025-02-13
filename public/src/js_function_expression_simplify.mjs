d8c395596ceff88ba41bcdb86c52bc1ac6622b49
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 11:49:33 -0500

    2025-02-13T16:49:32.560Z watch   js_function_expression_simplify

--- public/src/js_function_expression_simplify.mjs
+++ public/src/js_function_expression_simplify.mjs
@@ -1,10 +1,11 @@
-import { js_blockify } from "./js_blockify.mjs";
+import { never } from "./never.mjs";
+import { assert } from "./assert.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_function_expression_simplify(ast) {
   js_visit_node(ast, "FunctionExpression", (v) => {
     let { node } = v;
     let body = object_property_get(node, "body");
-    js_blockify(body);
+    assert(never, []);
   });
 }
