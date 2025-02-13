54070169eb0c8970a8654a5a6e2190ae2c72a559
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 11:49:41 -0500

    2025-02-13T16:49:40.473Z watch   js_function_expression_simplify

--- public/src/js_function_expression_simplify.mjs
+++ public/src/js_function_expression_simplify.mjs
@@ -1,4 +1,4 @@
-import { never } from "./never.mjs";
+import { js_node_type_statement_block_is } from "./js_node_type_statement_block_is.mjs";
 import { assert } from "./assert.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
@@ -6,6 +6,6 @@ export function js_function_expression_simplify(ast) {
   js_visit_node(ast, "FunctionExpression", (v) => {
     let { node } = v;
     let body = object_property_get(node, "body");
-    assert(never, []);
+    assert(js_node_type_statement_block_is, [body]);
   });
 }
