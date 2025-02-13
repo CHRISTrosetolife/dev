22e07c5d87872ab6387579d3ae2700994501896a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 18:50:57 -0500

    2025-02-13T23:50:57.293Z watch   app_new_bible_ceb

--- public/src/app_new_bible_ceb.mjs
+++ public/src/app_new_bible_ceb.mjs
@@ -6,19 +6,18 @@ import { function_transform } from "./function_transform.mjs";
 import { function_parse } from "./function_parse.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_new_generic } from "./app_new_generic.mjs";
-export async function app_new_bible_ceb(name) {
+export async function app_new_bible_ceb() {
   let a = fn_name("app_bible_ceb");
   let ast = await function_parse(a);
   let fnns = function_names();
+  let a_new = string_unique(a, fnns);
   await function_transform(ast, function (ast) {
-    let a_new = string_unique(a, fnns);
     js_identifier_rename(ast, a, a_new);
     todo("this should also rename", fn_name);
   });
   let b = js_declaration_single_body(ast);
   let v2 = b;
   return v2;
-  let body_string = "let root = html_style_default_initialize();";
-  let v = await app_new_generic(name, body_string);
+  let v = await app_new_generic(a_new, body_string);
   return v;
 }
