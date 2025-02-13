70aa54e840e6dab278496b56eddc072f224c450f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 18:07:00 -0500

    2025-02-13T23:06:59.400Z watch   html_overlay_z

--- public/src/html_overlay_z.mjs
+++ public/src/html_overlay_z.mjs
@@ -1,6 +1,11 @@
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { global_function } from "./global_function.mjs";
+import { fn_name } from "./fn_name.mjs";
 export function html_overlay_z() {
   let g = global_function(html_overlay_z);
-  object_property_initialize(g, "property_name", null);
+  object_property_initialize(g, "list", [
+    fn_name("html_audio"),
+    fn_name("html_loader"),
+    fn_name("app_code_refresh_function"),
+  ]);
 }
