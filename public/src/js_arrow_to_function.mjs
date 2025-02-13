b194e7a25e28d39de59cdc174782cfce76c27d0d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 11:41:30 -0500

    2025-02-13T16:41:29.526Z watch   js_arrow_to_function

--- public/src/js_arrow_to_function.mjs
+++ public/src/js_arrow_to_function.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { log } from "./log.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
@@ -5,6 +6,7 @@ export function js_arrow_to_function(ast) {
   js_visit_node(ast, "ArrowFunctionExpression", (v) => {
     let { node } = v;
     object_property_set(node, "type", "FunctionExpression");
+    let body = object_property_get(node, "body");
     log({
       node,
     });
