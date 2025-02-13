2adb0468647fa28205d511f3d65d754f6708fd1a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 18:08:33 -0500

    2025-02-13T23:08:32.416Z watch   html_overlay

--- public/src/html_overlay.mjs
+++ public/src/html_overlay.mjs
@@ -1,10 +1,12 @@
+import { html_overlay_z } from "./html_overlay_z.mjs";
+import { list_index } from "./list_index.mjs";
 import { html_style_background_color_transparent_mix } from "./html_style_background_color_transparent_mix.mjs";
 import { html_style } from "./html_style.mjs";
 import { html_style_height_full } from "./html_style_height_full.mjs";
 import { html_style_width_full } from "./html_style_width_full.mjs";
 import { html_div } from "./html_div.mjs";
 import { html_document_body } from "./html_document_body.mjs";
-export function html_overlay() {
+export function html_overlay(overlay_level_name) {
   let body = html_document_body();
   let overlay = html_div(body);
   html_style_width_full(overlay);
@@ -13,7 +15,7 @@ export function html_overlay() {
     position: "fixed",
     top: 0,
     left: 0,
-    "z-index": 9999,
+    "z-index": 9999 - list_index(html_overlay_z(), overlay_level_name),
   });
   html_style_background_color_transparent_mix(overlay, "black", 85);
   return overlay;
