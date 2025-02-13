57268a014ac2a33355e632c46f400ae7fad1d9ff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 18:07:16 -0500

    2025-02-13T23:07:15.244Z watch   html_audio

--- public/src/html_audio.mjs
+++ public/src/html_audio.mjs
@@ -1,20 +1,22 @@
+import { fn_name } from "./fn_name.mjs";
 import { html_remove } from "./html_remove.mjs";
 import { html_button_wait } from "./html_button_wait.mjs";
 import { html_overlay } from "./html_overlay.mjs";
 import { html_interacted } from "./html_interacted.mjs";
 export async function html_audio(src) {
   if (!html_interacted()) {
-    let overlay = html_overlay();
+    let overlay = html_overlay(fn_name("html_audio"));
     await html_button_wait(overlay, "Press this button to enable audio");
     html_remove(overlay);
   }
   let a = new Audio(src);
-  let result = new Promise((resolve) => {
+  let result = new Promise(function (resolve) {
     a.addEventListener("ended", function () {
       a.currentTime = 0;
       resolve();
     });
   });
   await a.play();
-  return await result;
+  let v = await result;
+  return v;
 }
--- public/src/html_loader.mjs
+++ public/src/html_loader.mjs
@@ -5,7 +5,7 @@ import { html_spacer_vertical_n } from "./html_spacer_vertical_n.mjs";
 import { html_style_font_color_white } from "./html_style_font_color_white.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 export function html_loader() {
-  let overlay = html_overlay();
+  let overlay = html_overlay($t);
   let label = html_p_text(overlay, "Loading...");
   html_style_font_color_white(label);
   html_spacer_vertical_n(label, 10);
