2ee26c7c1482fcdfb6a43d21fb93e7f73db33f51
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 14:46:14 -0500

    2025-02-13T19:46:13.645Z watch   js_dollar

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -325,18 +325,18 @@ export function js_dollar(ast) {
           remaining = string_prefix_without(remaining, first_second);
           let { stack } = v;
           let s1 = list_get_end(stack, 1);
-          if (false) {
+          if (list_is(s1)) {
+            list_insert(
+              s1,
+              index,
+              js_code_statement_let_assign(
+                js_name_unique(ast, "first"),
+                js_code_call_args(fn_name("list_first"), [
+                  js_code_array([remaining]),
+                ]),
+              ),
+            );
           }
-          list_insert(
-            s1,
-            index,
-            js_code_statement_let_assign(
-              js_name_unique(ast, "first"),
-              js_code_call_args(fn_name("list_first"), [
-                js_code_array([remaining]),
-              ]),
-            ),
-          );
         }
       }
       if (remaining === "v") {
