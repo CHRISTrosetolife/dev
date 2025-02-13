3c2047fd26bb3e4ea2ecb4149ae72fc339d28d15
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 12:16:46 -0500

    2025-02-13T17:16:45.751Z watch   js_function_expression_simplify

--- public/src/js_parse.mjs
+++ public/src/js_parse.mjs
@@ -1,14 +1,17 @@
+import { import_multi } from "./import_multi.mjs";
+import { import_node } from "./import_node.mjs";
 import { js_parse_options } from "./js_parse_options.mjs";
+import { log } from "./log.mjs";
 import { web_is } from "./web_is.mjs";
 let a = await import_node("acorn");
 export function js_parse(code) {
-  if (web_is()) {
-    let a = acorn;
-  }
-  try {
-    return a.parse(code, js_parse_options());
-  } catch (e) {
-    $L$code;
-    throw e;
-  }
-}
+    if (web_is()) {
+        a = acorn;
+      }
+      try {
+        return a.parse(code, js_parse_options());
+      } catch (e) {
+        log(code)
+        throw e;
+      }
+} 
