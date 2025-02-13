bceba449adaaf569bc28d364a6b7f5762c87e7dc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 17:41:57 -0500

    2025-02-13T22:41:56.591Z watch   map_size

--- public/src/map_size.mjs
+++ public/src/map_size.mjs
@@ -1,5 +1,6 @@
 import { map_properties } from "./map_properties.mjs";
 import { list_size } from "./list_size.mjs";
 export function map_size(selected) {
-  return list_size(map_properties(selected));
+  let v = list_size(map_properties(selected));
+  return v;
 }
