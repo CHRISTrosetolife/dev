1ca18b809d2ba22aba22a528ae3600f865aa3548
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 18:05:11 -0500

    2025-02-13T23:05:10.355Z watch   html_overlay_z

--- public/src/html_overlay_z.mjs
+++ public/src/html_overlay_z.mjs
@@ -1,4 +1,6 @@
+import { object_property_initialize } from "./object_property_initialize.mjs";
 import { global_function } from "./global_function.mjs";
 export function html_overlay_z() {
   let g = global_function(html_overlay_z);
+  object_property_initialize(object, "property_name", null);
 }
