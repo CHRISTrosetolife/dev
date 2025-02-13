e5e1651ed5012d06ed6ac1a2b66e273c0383cf27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 12:01:46 -0500

    2025-02-13T17:01:45.200Z watch   js_name_unique_multiple

--- public/src/js_name_unique_multiple.mjs
+++ public/src/js_name_unique_multiple.mjs
@@ -5,6 +5,7 @@ import { list_map } from "./list_map.mjs";
 import { list_concat_multiple } from "./list_concat_multiple.mjs";
 export function js_name_unique_multiple(asts, prefix) {
   let identifiers = list_concat_multiple(list_map(asts, js_identifiers));
+  let keywords = "arguments";
   let i = 1;
   let variable_name;
   while (true) {
