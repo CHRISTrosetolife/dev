8ed503c5d6aec5101c46e97ea93824859f8187c2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 12:18:29 -0500

    2025-02-13T17:18:28.167Z watch   js_node_identifier_named

--- public/src/js_node_identifier_named.mjs
+++ public/src/js_node_identifier_named.mjs
@@ -1,8 +1,8 @@
-import { js_node_type_is } from "./js_node_type_is.mjs";
+import { js_node_type_identifier_is } from "./js_node_type_identifier_is.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { equal } from "./equal.mjs";
 export function js_node_identifier_named(node, name) {
-  let identifier_is = js_node_type_is(node, "Identifier");
+  let identifier_is = js_node_type_identifier_is(node);
   let e = identifier_is && equal(object_property_get(node, "name"), name);
   return e;
 }
---
+++ public/src/js_node_type_identifier_is.mjs
@@ -0,0 +1,4 @@
+import { js_node_type_is } from "./js_node_type_is.mjs";
+export function js_node_type_identifier_is(node) {
+  return js_node_type_is(node, "Identifier");
+}
