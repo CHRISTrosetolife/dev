38e7b9261727cef0985725d7a3ed85af891f97ba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 18:52:01 -0500

    2025-02-13T23:52:01.085Z watch   app_new_bible_ceb

--- public/src/app_new_bible_ceb.mjs
+++ public/src/app_new_bible_ceb.mjs
@@ -3,7 +3,6 @@ import { js_declaration_single_body } from "./js_declaration_single_body.mjs";
 import { todo } from "./todo.mjs";
 import { string_unique } from "./string_unique.mjs";
 import { js_identifier_rename } from "./js_identifier_rename.mjs";
-import { function_transform } from "./function_transform.mjs";
 import { function_parse } from "./function_parse.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { app_new_generic } from "./app_new_generic.mjs";
@@ -12,10 +11,8 @@ export async function app_new_bible_ceb() {
   let ast = await function_parse(a);
   let fnns = await functions_names();
   let a_new = string_unique(a, fnns);
-  await function_transform(ast, function (ast) {
-    js_identifier_rename(ast, a, a_new);
-    todo("this should also rename", fn_name);
-  });
+  js_identifier_rename(ast, a, a_new);
+  todo("this should also rename", fn_name);
   let b = js_declaration_single_body(ast);
   let v2 = b;
   return v2;
