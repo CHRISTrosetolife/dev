140fae905fb0a0d6696332330b3a52ded1b5d217
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 12:39:18 -0500

    2025-02-13T17:39:17.981Z watch   app_code_refresh_function_node_identifier_add_generic

--- public/src/app_code_refresh_function_node_identifier_add_generic.mjs
+++ public/src/app_code_refresh_function_node_identifier_add_generic.mjs
@@ -37,7 +37,7 @@ export function app_code_refresh_function_node_identifier_add_generic(
       map_set(selected, identifier, {
         visitor,
       });
-      each_map(selected, (property, value) => {
+      each_map(selected, (component2, value) => {
         let selected2 = object_property_get(value, "selected");
       });
     } else {
