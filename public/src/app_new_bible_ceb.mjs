621fb9cdfbf8373d3b433169df84227439b8d38b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 18:44:48 -0500

    2025-02-13T23:44:48.143Z watch   app_new_bible_ceb

--- public/src/app_new_bible_ceb.mjs
+++ public/src/app_new_bible_ceb.mjs
@@ -1,5 +1,8 @@
+import { function_parse } from "./function_parse.mjs";
+import { fn_name } from "./fn_name.mjs";
 import { app_new_generic } from "./app_new_generic.mjs";
 export async function app_new_bible_ceb(name) {
+  await function_parse(fn_name("app_bible_ceb"));
   let body_string = "let root = html_style_default_initialize();";
   let v = await app_new_generic(name, body_string);
   return v;
