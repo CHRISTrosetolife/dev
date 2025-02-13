026971f9aa7d33a47398b651caa42ad3eacc6d3e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 09:42:40 -0500

    2025-02-13T14:42:23.538Z watch   bible_ceb_upload_chapter

--- public/src/bible_ceb_upload_chapter.mjs
+++ public/src/bible_ceb_upload_chapter.mjs
@@ -4,7 +4,6 @@ import { object_property_initialize } from "./object_property_initialize.mjs";
 import { ceb_definition_2 } from "./ceb_definition_2.mjs";
 import { definition_bohol } from "./definition_bohol.mjs";
 import { bible_storage_ceb_definitions_path } from "./bible_storage_ceb_definitions_path.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { null_not_is } from "./null_not_is.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { bible_word_map } from "./bible_word_map.mjs";
@@ -16,7 +15,6 @@ import { list_to_lookup_value_async } from "./list_to_lookup_value_async.mjs";
 import { list_unique } from "./list_unique.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 import { bible_chapter } from "./bible_chapter.mjs";
-import { object_merge_strict } from "./object_merge_strict.mjs";
 import { list_first } from "./list_first.mjs";
 export async function bible_ceb_upload_chapter(bible_version, chapter_code) {
   let chapter = await bible_chapter(bible_version, chapter_code);
@@ -32,14 +30,6 @@ export async function bible_ceb_upload_chapter(bible_version, chapter_code) {
     list_add_multiple(word_definitions, mapped2);
     list_uniqueify(word_definitions);
     let result = d2;
-    if (false) {
-      let word = object_property_get(d, "word");
-      if (word !== u) {
-        object_merge_strict(result, {
-          root: word,
-        });
-      }
-    }
     return result;
   });
   await bible_storage_version_upload(
