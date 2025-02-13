f1d8e87cd28057ac603fc661e029577bae62f784
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 18:47:23 -0500

    2025-02-13T23:47:22.705Z watch   js_name_unique_multiple

--- public/src/js_name_unique_multiple.mjs
+++ public/src/js_name_unique_multiple.mjs
@@ -1,22 +1,12 @@
-import { list_includes } from "./list_includes.mjs";
+import { string_unique } from "./string_unique.mjs";
 import { js_identifiers } from "./js_identifiers.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_concat_multiple } from "./list_concat_multiple.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function js_name_unique_multiple(asts, prefix) {
   let identifiers = list_concat_multiple(list_map(asts, js_identifiers));
   let keywords = ["arguments"];
   list_add_multiple(identifiers, keywords);
-  let i = 1;
-  let variable_name;
-  while (true) {
-    variable_name =
-      i === 1 ? prefix : string_combine_multiple([prefix, "_", i]);
-    if (!list_includes(identifiers, variable_name)) {
-      break;
-    }
-    i++;
-  }
-  return variable_name;
+  let v = string_unique(prefix, identifiers);
+  return v;
 }
---
+++ public/src/string_unique.mjs
@@ -0,0 +1,15 @@
+import { list_includes } from "./list_includes.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function string_unique(prefix, existing) {
+  let i = 1;
+  let variable_name;
+  while (true) {
+    variable_name =
+      i === 1 ? prefix : string_combine_multiple([prefix, "_", i]);
+    if (!list_includes(existing, variable_name)) {
+      break;
+    }
+    i++;
+  }
+  return variable_name;
+}
