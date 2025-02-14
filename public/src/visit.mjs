b6f942b0642c32f94ad6de7cca13998416269b31
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 18:26:29 -0500

    2025-02-14T23:26:29.068Z watch   visit

--- public/src/visit.mjs
+++ public/src/visit.mjs
@@ -1,6 +1,11 @@
 import { each } from "./each.mjs";
 import { list_concat } from "./list_concat.mjs";
 export function visit(root, children_get, filter, each, stack) {
+  each({
+    node,
+    parent: root,
+    stack,
+  });
   let children = children_get(root);
   for (let node of children) {
     if (filter(node)) {
