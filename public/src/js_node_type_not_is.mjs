1dd0ba499150225201c6f59298d6d8c1426cca27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 18:47:48 -0500

    2025-02-14T23:47:48.077Z watch   js_node_type_not_is

--- public/src/js_node_type_not_is.mjs
+++ public/src/js_node_type_not_is.mjs
@@ -1,1 +1,5 @@
-export function js_node_type_not_is() {}
+import { js_node_type_is } from "./js_node_type_is.mjs";
+export function js_node_type_not_is(node, type) {
+  let v = !js_node_type_is(node, type);
+  return v;
+}
