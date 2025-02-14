86ccace546d3f8c4706565385132aa66e890cfd5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 19:56:08 -0500

    2025-02-14T00:56:07.212Z watch   app_code_button_menu_app_migrate

--- public/src/app_code_button_menu_app_migrate.mjs
+++ public/src/app_code_button_menu_app_migrate.mjs
@@ -1,3 +1,4 @@
+import { object_property_set } from "./object_property_set.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_second } from "./list_second.mjs";
 import { list_first } from "./list_first.mjs";
@@ -10,7 +11,8 @@ export async function app_code_button_menu_app_migrate() {
   function migrate(params) {
     let first = list_first(params);
     let second = list_second(params);
-    if (object_property_get(object, "property_name")) {
+    if (object_property_get(first, "name") === "root") {
+      object_property_set(first, "name", "context");
     }
   }
 }
