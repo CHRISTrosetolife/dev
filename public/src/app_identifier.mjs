99ed6958ce59017d08f5a95494bfb8644e176fd8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 19:00:05 -0500

    2025-02-14T00:00:04.842Z watch   app_identifier

--- public/src/app_identifier.mjs
+++ public/src/app_identifier.mjs
@@ -1,7 +1,10 @@
+import { string_starts_with_not } from "./string_starts_with_not.mjs";
+import { assert } from "./assert.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { app_prefix } from "./app_prefix.mjs";
 export function app_identifier(name) {
   let prefix = app_prefix();
+  assert(string_starts_with_not);
   let name_prefixed = string_combine(prefix, name);
   return name_prefixed;
 }
