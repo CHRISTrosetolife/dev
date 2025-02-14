bb93060914584de62e50922de057f3447be29d20
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 17:45:27 -0500

    2025-02-14T22:45:25.944Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -2,13 +2,13 @@ import { log } from "./log.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_identifiers_scoped } from "./js_identifiers_scoped.mjs";
 import { fn_name } from "./fn_name.mjs";
-import { function_parse } from "./function_parse.mjs";
 import { js_visit } from "./js_visit.mjs";
 import { js_unparse } from "./js_unparse.mjs";
 import { js_node_is } from "./js_node_is.mjs";
+import { function_declaration } from "./function_declaration.mjs";
 export async function sandbox_5() {
-  let ast = await function_parse(fn_name("sandbox_5"));
-  js_visit(ast, function (v) {
+  let d = await function_declaration(fn_name("sandbox_5"));
+  js_visit(d, function (v) {
     let node = object_property_get(v, "node");
     let names = js_identifiers_scoped(v);
     if (js_node_is(node)) {
