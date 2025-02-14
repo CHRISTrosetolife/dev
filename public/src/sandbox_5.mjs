0cf945170c1c471faa57cfb5ebe48b5610cc7452
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 17:38:37 -0500

    2025-02-14T22:38:35.750Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,5 +1,7 @@
 import { fn_name } from "./fn_name.mjs";
 import { function_parse } from "./function_parse.mjs";
+import { js_visit } from "./js_visit.mjs";
 export async function sandbox_5() {
   let ast = await function_parse(fn_name("sandbox_5"));
+  js_visit(ast, function (v) {});
 }
