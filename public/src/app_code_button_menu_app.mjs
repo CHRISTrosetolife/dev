fc741dd09559a5d9d552a0a7c798152d2270f2d5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 19:51:54 -0500

    2025-02-14T00:51:53.624Z watch   app_code_button_menu_app

--- public/src/app_code_button_menu_app.mjs
+++ public/src/app_code_button_menu_app.mjs
@@ -1,7 +1,8 @@
 import { app_code_refresh_menu } from "./app_code_refresh_menu.mjs";
 import { html_button } from "./html_button.mjs";
-export function app_code_button_menu_app(parent, context, on_click) {
+export function app_code_button_menu_app(context, parent, on_click) {
   html_button(parent, "App menu", async function () {
     await app_code_refresh_menu(context);
+    on_click();
   });
 }
