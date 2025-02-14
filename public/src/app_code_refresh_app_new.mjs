43922a0fb5a1da3e1d3655ab5810fc7e9f2fb5ac
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 19:34:29 -0500

    2025-02-14T00:34:28.728Z watch   app_code_refresh_functions_recent

---
+++ public/src/app_code_refresh_app_new.mjs
@@ -0,0 +1,32 @@
+import { app_code_button_menu_app } from "./app_code_button_menu_app.mjs";
+import { app_code_invoke } from "./app_code_invoke.mjs";
+import { app_code_refresh_function_change } from "./app_code_refresh_function_change.mjs";
+import { app_new } from "./app_new.mjs";
+import { html_condition_includes_not } from "./html_condition_includes_not.mjs";
+import { html_condition_identifier_fn_part } from "./html_condition_identifier_fn_part.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { html_focus } from "./html_focus.mjs";
+import { html_input_validated_button } from "./html_input_validated_button.mjs";
+import { fn_name } from "./fn_name.mjs";
+import { app_code_screen_set } from "./app_code_screen_set.mjs";
+import { list_concat } from "./list_concat.mjs";
+export async function app_code_refresh_app_new(context) {
+  let root = app_code_screen_set(context, fn_name("app_code_refresh_app_new"));
+  app_code_button_menu_app(context);
+  let f_name = fn_name("apps_list");
+  let existing = await app_code_invoke(f_name);
+  let message = "not be the name of an existing app";
+  let ib = html_input_validated_button(
+    root,
+    "App name",
+    list_concat(html_condition_identifier_fn_part(), [
+      html_condition_includes_not(existing, message),
+    ]),
+    "New app",
+    async function on_submit(value) {
+      let f_name = await app_new(value);
+      await app_code_refresh_function_change(context, f_name);
+    },
+  );
+  html_focus(object_property_get(ib, "input"));
+}
---
+++ public/src/app_code_refresh_htmls.mjs
@@ -0,0 +1,22 @@
+import { app_code_refresh_html } from "./app_code_refresh_html.mjs";
+import { storage_local_set_context } from "./storage_local_set_context.mjs";
+import { html_button } from "./html_button.mjs";
+import { each } from "./each.mjs";
+import { app_code_button_menu_app } from "./app_code_button_menu_app.mjs";
+import { app_code_html_file_paths_get } from "./app_code_html_file_paths_get.mjs";
+import { fn_name } from "./fn_name.mjs";
+import { app_code_screen_set } from "./app_code_screen_set.mjs";
+import { html_load } from "./html_load.mjs";
+export async function app_code_refresh_htmls(context) {
+  await html_load(async function () {
+    let root = app_code_screen_set(context, fn_name("app_code_refresh_htmls"));
+    app_code_button_menu_app(context);
+    let file_paths_html = await app_code_html_file_paths_get();
+    each(file_paths_html, function (h) {
+      html_button(root, h, async function () {
+        storage_local_set_context(context, "html_selected", h);
+        await app_code_refresh_html(context);
+      });
+    });
+  });
+}
