21a72c315cc8ea791eb386a9d29995d73bc3300e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 18:49:05 -0500

    2025-02-14T23:49:04.187Z watch   js_unparsable

--- public/src/js_unparsable.mjs
+++ public/src/js_unparsable.mjs
@@ -1,5 +1,6 @@
 import { js_node_type_not_is } from "./js_node_type_not_is.mjs";
 import { js_node_is } from "./js_node_is.mjs";
 export function js_unparsable(node) {
-  return js_node_is(node) && js_node_type_not_is(node, "ImportSpecifier");
+  let v = js_node_is(node) && js_node_type_not_is(node, "ImportSpecifier");
+  return v;
 }
