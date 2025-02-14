419d123d17cdfdc94457c26a76c91a676b1781dd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 14:36:40 -0500

    2025-02-14T19:36:39.369Z watch   list_remove_multiple_from

---
+++ public/src/list_remove_multiple_from.mjs
@@ -0,0 +1,4 @@
+import { list_remove_multiple_at } from "./list_remove_multiple_at.mjs";
+export function list_remove_multiple_from(ancestor_common, low, high) {
+  list_remove_multiple_at(ancestor_common, low, high - low);
+}
