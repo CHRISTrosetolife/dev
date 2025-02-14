688318fcfc66e2f05eca52a49db75b5f081c5f77
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 16:33:13 -0500

    2025-02-14T21:33:13.103Z watch   js_visit_find

--- public/src/js_visit_find.mjs
+++ public/src/js_visit_find.mjs
@@ -1,10 +1,10 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { js_visit } from "./js_visit.mjs";
-export function js_visit_find(ast, identifier) {
+export function js_visit_find(ast, node_target) {
   let found = null;
-  js_visit(ast, (v) => {
+  js_visit(ast, function (v) {
     let node = object_property_get(v, "node");
-    if (node === identifier) {
+    if (node === node_target) {
       found = v;
     }
   });
