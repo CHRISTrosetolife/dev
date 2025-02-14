b19cacc1dddd1c8c4f9927ad8d8f03a2b80075c0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 19:54:11 -0500

    2025-02-14T00:54:10.857Z watch   app_code_button_menu_app_migrate

--- public/src/app_code_button_menu_app_migrate.mjs
+++ public/src/app_code_button_menu_app_migrate.mjs
@@ -1,13 +1,13 @@
+import { log } from "./log.mjs";
 import { functions_calls_migrate } from "./functions_calls_migrate.mjs";
-import { object_property_set } from "./object_property_set.mjs";
 import { fn_name } from "./fn_name.mjs";
-import { list_single } from "./list_single.mjs";
 export async function app_code_button_menu_app_migrate() {
   let f_name = fn_name("app_code_button_menu_app");
   let v = await functions_calls_migrate(f_name, migrate);
   return v;
   function migrate(params) {
-    let p = list_single(params);
-    object_property_set(p, "name", "root");
+    log({
+      params,
+    });
   }
 }
