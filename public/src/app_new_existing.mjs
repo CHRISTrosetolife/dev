04f1e702af83b94fe9a0082df9b80e0e9c97c63c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 19:04:38 -0500

    2025-02-14T00:04:37.714Z watch   app_new_existing

--- public/src/app_new_existing.mjs
+++ public/src/app_new_existing.mjs
@@ -27,6 +27,6 @@ export async function app_new_existing(a) {
   let lines = list_map(body, js_unparse);
   let body_string = list_join_semicolon(lines);
   let a_new_unprefixed = string_prefix_without(a_new, app_prefix());
-  let v = await app_new_generic(a_new, body_string, async_is);
+  let v = await app_new_generic(a_new_unprefixed, body_string, async_is);
   return v;
 }
