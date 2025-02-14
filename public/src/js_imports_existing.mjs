d63982039751ef6f1cdc304c4e687788e49fb172
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 18:54:40 -0500

    2025-02-14T23:54:39.769Z watch   js_imports_existing

--- public/src/js_imports_existing.mjs
+++ public/src/js_imports_existing.mjs
@@ -4,14 +4,15 @@ import { list_get_end_1 } from "./list_get_end_1.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_imports_existing(ast) {
-  let existing = list_adder((la) => {
-    js_visit_node(ast, "ImportSpecifier", (v) => {
+  let existing = list_adder(function (la) {
+    js_visit_node(ast, "ImportSpecifier", function (v) {
       let { node } = v;
       let { stack } = v;
       let e = list_get_end_1(stack);
       let sv = js_import_declaration_source_value(e);
       if (js_import_external_is(sv)) {
-        return;
+        let v2;
+        return v2;
       }
       let { imported } = node;
       if (imported.type === "Identifier") {
