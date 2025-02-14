30f717434b30ae00075cd72e5d10590f93435763
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 19:01:26 -0500

    2025-02-14T00:01:24.999Z watch   app_identifier

--- public/src/app_identifier.mjs
+++ public/src/app_identifier.mjs
@@ -3,8 +3,10 @@ import { assert } from "./assert.mjs";
 import { string_combine } from "./string_combine.mjs";
 import { app_prefix } from "./app_prefix.mjs";
 export function app_identifier(name) {
-  let prefix = app_prefix();$i
-  assert(string_starts_with_not, [name, prefix]);
+  let prefix = app_prefix();
+  if (false) {
+    assert(string_starts_with_not, [name, prefix]);
+  }
   let name_prefixed = string_combine(prefix, name);
   return name_prefixed;
 }
