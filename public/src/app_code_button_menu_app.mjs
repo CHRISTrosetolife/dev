0700f544ba15d1df4f9b5328322d5ef298f15bd7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-13 19:44:05 -0500

    2025-02-14T00:44:05.148Z watch   app_code_button_menu_app

--- public/src/app_code_button_menu_app.mjs
+++ public/src/app_code_button_menu_app.mjs
@@ -1,8 +1,6 @@
 import { app_code_refresh_menu } from "./app_code_refresh_menu.mjs";
 import { html_button } from "./html_button.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-export function app_code_button_menu_app(context) {
-  let root = object_property_get(context, "root");
+export function app_code_button_menu_app(root) {
   html_button(root, "App menu", async function () {
     await app_code_refresh_menu(context);
   });
