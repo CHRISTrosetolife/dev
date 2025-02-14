ed21575f2b1d9ec8ca1ff68274324fd75bd0345e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 18:55:30 -0500

    2025-02-14T23:55:29.235Z watch   js_imports_existing_names

--- public/src/js_imports_existing_names.mjs
+++ public/src/js_imports_existing_names.mjs
@@ -1,5 +1,7 @@
 import { js_imports_existing } from "./js_imports_existing.mjs";
+import { list_map_property } from "./list_map_property.mjs";
 export function js_imports_existing_names(ast) {
   let v = js_imports_existing(ast);
-  return v;
+  let v2 = list_map_property(v, "name");
+  return v2;
 }
