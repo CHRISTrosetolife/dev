29b52aebe983ec637df5e28f37a3fd4082652809
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 18:55:15 -0500

    2025-02-14T23:55:14.422Z watch   js_imports_existing_names

--- public/src/js_imports_existing_names.mjs
+++ public/src/js_imports_existing_names.mjs
@@ -1,23 +1,5 @@
-import { js_import_external_is } from "./js_import_external_is.mjs";
-import { js_import_declaration_source_value } from "./js_import_declaration_source_value.mjs";
-import { list_get_end_1 } from "./list_get_end_1.mjs";
-import { list_adder } from "./list_adder.mjs";
-import { js_visit_node } from "./js_visit_node.mjs";
+import { js_imports_existing } from "./js_imports_existing.mjs";
 export function js_imports_existing_names(ast) {
-  let existing = list_adder((la) => {
-    js_visit_node(ast, "ImportSpecifier", (v) => {
-      let { node } = v;
-      let { stack } = v;
-      let e = list_get_end_1(stack);
-      let sv = js_import_declaration_source_value(e);
-      if (js_import_external_is(sv)) {
-        return;
-      }
-      let { imported } = node;
-      if (imported.type === "Identifier") {
-        la(imported.name);
-      }
-    });
-  });
-  return existing;
+  let v = js_imports_existing(ast);
+  return v;
 }
