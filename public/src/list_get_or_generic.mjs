397136fd3f0e7d75b6adbc78889fa0efff4080ad
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 17:26:52 -0500

    2025-02-14T22:26:52.291Z watch   list_get_or_generic

--- public/src/list_get_or_generic.mjs
+++ public/src/list_get_or_generic.mjs
@@ -1,12 +1,11 @@
+import { list_index_is } from "./list_index_is.mjs";
 import { list_get } from "./list_get.mjs";
-import { list_index_last } from "./list_index_last.mjs";
-import { greater_than } from "./greater_than.mjs";
 export function list_get_or_generic(fn, list, index) {
   let result;
-  if (greater_than(index, list_index_last(list))) {
-    result = fn(list);
-  } else {
+  if (list_index_is(list, index)) {
     result = list_get(list, index);
+  } else {
+    result = fn(list);
   }
   return result;
 }
