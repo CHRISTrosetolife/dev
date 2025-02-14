87936cf1410bccd4d8e166c4d24f4192f3e79560
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 16:31:34 -0500

    2025-02-14T21:31:33.617Z watch   app_code_refresh_function

--- public/src/app_code_refresh_function.mjs
+++ public/src/app_code_refresh_function.mjs
@@ -1,3 +1,4 @@
+import { list_first } from "./list_first.mjs";
 import { js_identifiers_scoped } from "./js_identifiers_scoped.mjs";
 import { app_code_refresh_function_selection_remove } from "./app_code_refresh_function_selection_remove.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
@@ -53,6 +54,7 @@ export async function app_code_refresh_function(context) {
         html_button(overlay, "Selection functionize", async function () {
           let removed =
             app_code_refresh_function_selection_remove(selection_result);
+          let f = list_first(removed);
           js_identifiers_scoped(v, node);
           refresh_overlay_remove();
         });
