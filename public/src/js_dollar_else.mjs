0b99ebb8bec008c8b1c519b54f1f0e7302fe9c01
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:29:12 -0500

    2025-02-15T15:29:11.083Z watch   js_dollar_else

--- public/src/js_dollar_else.mjs
+++ public/src/js_dollar_else.mjs
@@ -1,11 +1,10 @@
-import { js_stack_find_list } from "./js_stack_find_list.mjs";
+import { js_stack_find_list_visitor } from "./js_stack_find_list_visitor.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_remove } from "./list_remove.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { js_node_is } from "./js_node_is.mjs";
 export function js_dollar_else(v, value_new) {
-  let { stack } = v;
-  let { current, list, item } = js_stack_find_list(stack);
+  let { current, list, item } = js_stack_find_list_visitor(v);
   let previous = null;
   while (js_node_is(current) && current.type === "IfStatement") {
     previous = current;
