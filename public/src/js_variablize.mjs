4c5630b4ac413c03cdc2c53171b860a6ae090bee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 14:39:23 -0500

    2025-02-15T19:39:22.739Z watch   js_variablize

--- public/src/js_variablize.mjs
+++ public/src/js_variablize.mjs
@@ -4,14 +4,14 @@ import { list_first } from "./list_first.mjs";
 import { list_index } from "./list_index.mjs";
 import { list_is } from "./list_is.mjs";
 import { assert } from "./assert.mjs";
-export function js_variablize(ast, parent, node, init) {
-  assert(list_is, [parent]);
-  let index = list_index(parent, node);
+export function js_variablize(ast, parent_list, list_item, expression) {
+  assert(list_is, [parent_list]);
+  let index = list_index(parent_list, list_item);
   let { parsed, variable_name } = js_name_unique_v_parsed(ast);
-  parent.splice(index, 0, parsed);
+  parent_list.splice(index, 0, parsed);
   let { declarations } = parsed;
   let first = list_first(declarations);
-  first.init = init;
+  first.init = expression;
   let parsed2 = js_parse_expression(variable_name);
   return parsed2;
 }
