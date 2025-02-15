8798c97daef3bfcf10790afaf6861846f29bad59
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 14:08:06 -0500

    2025-02-15T19:08:05.599Z watch   js_identifiers_duplicates

--- public/src/js_identifiers_duplicates.mjs
+++ public/src/js_identifiers_duplicates.mjs
@@ -1,19 +1,14 @@
+import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { js_identifiers_defines } from "./js_identifiers_defines.mjs";
-import { log } from "./log.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_visit } from "./js_visit.mjs";
-import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export function js_identifiers_duplicates(ast) {
   list_adder(function (la) {
     js_visit(ast, function (v) {
       let node = object_property_get(v, "node");
       let identifiers = js_identifiers_defines(node);
-      if (list_empty_not_is(identifiers)) {
-        log({
-          identifiers,
-        });
-      }
+      each(list, function (item) {});
     });
   });
 }
