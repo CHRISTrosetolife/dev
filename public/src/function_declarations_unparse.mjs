67ed2e4791e34f6ebd35e50f6c83079a8b384441
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 16:07:17 -0500

    2025-02-15T21:07:17.372Z watch   function_declarations_unparse

--- public/src/function_declarations_unparse.mjs
+++ public/src/function_declarations_unparse.mjs
@@ -5,7 +5,11 @@ import { list_map } from "./list_map.mjs";
 import { function_declaration } from "./function_declaration.mjs";
 import { file_read } from "./file_read.mjs";
 export async function function_declarations_unparse(dependency_names) {
-  todo("maybe this should use ", file_read, "to return the file contents?");
+  todo(
+    "maybe this should instead use ",
+    file_read,
+    "to return the file contents?",
+  );
   let ds = await list_map_unordered(dependency_names, function_declaration);
   let us = list_map(ds, js_unparse);
   return us;
