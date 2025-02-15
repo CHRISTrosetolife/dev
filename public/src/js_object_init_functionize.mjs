a6572082ff5116668a64ab8c164c9636b26ae5aa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:38:54 -0500

    2025-02-15T15:38:53.901Z watch   js_object_init_functionize

--- public/src/js_object_init_functionize.mjs
+++ public/src/js_object_init_functionize.mjs
@@ -1,3 +1,4 @@
+import { fn_name } from "./fn_name.mjs";
 import { js_identifier_to_expression } from "./js_identifier_to_expression.mjs";
 import { js_call } from "./js_call.mjs";
 import { each_reverse } from "./each_reverse.mjs";
@@ -8,7 +9,6 @@ import { list_is } from "./list_is.mjs";
 import { assert } from "./assert.mjs";
 import { list_get_end } from "./list_get_end.mjs";
 import { object_property_set } from "./object_property_set.mjs";
-import { object_new } from "./object_new.mjs";
 export function js_object_init_functionize(ast) {
   let vs = js_variable_declarators(ast, "ObjectExpression");
   for (let v of vs) {
@@ -17,16 +17,20 @@ export function js_object_init_functionize(ast) {
     let { id } = node;
     let { init } = node;
     let { properties } = init;
-    each_reverse(properties, (p) => {
+    each_reverse(properties, function (p) {
       let { key, value } = p;
       let key_string = js_identifier_to_expression(key);
-      let call = js_call(object_property_set.name, [id, key_string, value]);
+      let call = js_call(fn_name("object_property_set"), [
+        id,
+        key_string,
+        value,
+      ]);
       let { stack } = v;
-      let variable_declaration_parent = list_get_end(stack, 2);
+      let variable_declaration_parent = list_get_end(stack, 3);
       assert(list_is, [variable_declaration_parent]);
       list_insert(variable_declaration_parent, index_insert, call);
     });
-    const init_new = js_call(object_new.name, []);
+    let init_new = js_call(fn_name("object_new"), []);
     object_property_set(node, "init", init_new);
   }
 }
