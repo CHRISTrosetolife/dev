39439e9c399e169f7efa0886877af31a45e52410
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:48:16 -0500

    2025-02-15T15:48:14.210Z watch   app_bible_ceb_upload

--- public/src/app_bible_ceb_upload.mjs
+++ public/src/app_bible_ceb_upload.mjs
@@ -5,8 +5,9 @@ import { object_property_get } from "./object_property_get.mjs";
 export async function app_bible_ceb_upload() {
   let bible_version = "cebulb";
   let chapters = await bible_books_chapters(bible_version);
-  await each_log_async(chapters, async (chapter_info) => {
+  await each_log_async(chapters, async function (chapter_info) {
     let chapter_code = object_property_get(chapter_info, "chapter_code");
-    return await bible_ceb_upload_chapter(bible_version, chapter_code);
+    let v = await bible_ceb_upload_chapter(bible_version, chapter_code);
+    return v;
   });
 }
