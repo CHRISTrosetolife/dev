84d9aaf4ef7c93a2fe734d3df95c6e0bb2e97dc1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:55:40 -0500

    2025-02-15T15:55:39.093Z watch   app_code_refresh_function_node_nodes

--- public/src/app_code_refresh_function_node_nodes.mjs
+++ public/src/app_code_refresh_function_node_nodes.mjs
@@ -3,6 +3,9 @@ import { map_new } from "./map_new.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function app_code_refresh_function_node_nodes(args) {
   let data = object_property_get(args, "data");
-  let nodes = object_property_initialize_get(data, "nodes", map_new());
+  let nodes = object_property_initialize_get(data, "nodes", function () {
+    let v = map_new();
+    return v;
+  });
   return nodes;
 }
