559b76cf17eab84c808019fc26d46dd98798764e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 21:51:55 -0500

    2025-02-15T02:51:54.861Z watch   app_weights

--- public/src/app_weights.mjs
+++ public/src/app_weights.mjs
@@ -5,7 +5,8 @@ import { each } from "./each.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_weights() {
   let root = html_style_default_initialize();
-  each([1, 2, 5, 10, 15, 20, 25, 45, 50], function (value) {
+  let weights = [1, 2, 5, 10, 15, 20, 25, 45, 50];
+  each(weights, function (value) {
     let selected = false;
     let b = html_button_text_click(root, string_to(value), function () {
       selected = !selected;
