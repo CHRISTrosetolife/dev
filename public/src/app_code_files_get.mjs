d5c3466258f8bce3d2a5a98e9b5c734ae59ee1da
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 16:47:34 -0500

    2025-02-15T21:47:34.050Z watch   app_code_files_get

--- public/src/app_code_files_get.mjs
+++ public/src/app_code_files_get.mjs
@@ -6,19 +6,20 @@ import { global_files } from "./global_files.mjs";
 import { object_property_initialize_get_async } from "./object_property_initialize_get_async.mjs";
 import { global_get } from "./global_get.mjs";
 export async function app_code_files_get() {
-  return await html_load(async () => {
+  let v2 = await html_load(async function () {
     let g = global_get();
     let files = await object_property_initialize_get_async(
       g,
       global_files(),
-      async () => {
+      async function () {
         let v = await http_storage_get(app_code_local_files_path_latest());
         let batch_path = object_property_get(v, "batch_path");
-        let v2 = await http_storage_get(batch_path);
-        let files2 = object_property_get(v2, "files");
+        let batch = await http_storage_get(batch_path);
+        let files2 = object_property_get(batch, "files");
         return files2;
       },
     );
     return files;
   });
+  return v2;
 }
