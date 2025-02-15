059cb080f692de7ecf7ae2fef46b9353f94ad75f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 14:06:15 -0500

    2025-02-15T19:06:14.776Z watch   js_identifiers_duplicates

--- public/src/js_identifiers_duplicates.mjs
+++ public/src/js_identifiers_duplicates.mjs
@@ -6,7 +6,7 @@ import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export function js_identifiers_duplicates(ast) {
   js_visit(ast, function (v) {
     let node = object_property_get(v, "node");
-    let identifiers = js_identifiers_defines(v, node);
+    let identifiers = js_identifiers_defines(node);
     if (list_empty_not_is(identifiers)) {
       log({
         identifiers,
