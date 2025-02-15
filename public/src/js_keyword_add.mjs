14ac8e85b1aa3391e03abca8596541264fa2019e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 11:15:40 -0500

    2025-02-15T16:15:39.211Z s combine

--- public/src/js_keyword_add.mjs
+++ public/src/js_keyword_add.mjs
@@ -1,7 +1,6 @@
 import { string_suffix_without } from "./string_suffix_without.mjs";
 import { function_new_constant_string } from "./function_new_constant_string.mjs";
 export async function js_keyword_add(kw) {
-  await function_new_constant_string(
-    string_suffix_without(js_keyword_add, "_add"),
-  );
+  let prefix = string_suffix_without(js_keyword_add, "_add");
+  await function_new_constant_string(prefix, kw);
 }
