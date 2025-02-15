cb850f8d1ee9fd099ce1f079d474de8c50abee66
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 13:35:09 -0500

    2025-02-15T18:35:09.338Z watch   js_identifiers_duplicates

--- public/src/js_identifiers_duplicates.mjs
+++ public/src/js_identifiers_duplicates.mjs
@@ -1,7 +1,10 @@
+import { js_identifiers_scoped_each } from "./js_identifiers_scoped_each.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_visit } from "./js_visit.mjs";
 export function js_identifiers_duplicates(ast) {
   js_visit(ast, function (v) {
     let node = object_property_get(v, "node");
+    let stack = object_property_get(node, "stack");
+    js_identifiers_scoped_each();
   });
 }
