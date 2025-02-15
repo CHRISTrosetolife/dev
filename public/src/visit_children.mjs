a44ed636fd5d90f374acf9336e1ab319f30a6a77
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 02:31:00 -0500

    2025-02-15T07:30:59.712Z watch   visit_children

--- public/src/visit_children.mjs
+++ public/src/visit_children.mjs
@@ -1,10 +1,15 @@
 import { visit_each_children } from "./visit_each_children.mjs";
 import { each } from "./each.mjs";
 export function visit_children(parent, children_get, filter, each, stack) {
+  let result = false;
   let children = children_get(parent);
   for (let node of children) {
     if (filter(node)) {
-      visit_each_children(parent, children_get, filter, each, stack, node);
+      result =
+        visit_each_children(parent, children_get, filter, each, stack, node) ||
+        result;
     }
   }
+  let v = result;
+  return v;
 }
