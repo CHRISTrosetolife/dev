b5f48afdc2902ef774f976c3030e4b48a0699ed7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:47:29 -0500

    2025-02-15T15:47:28.288Z watch   log_error

--- public/src/log_error.mjs
+++ public/src/log_error.mjs
@@ -1,6 +1,8 @@
+import { string_take } from "./string_take.mjs";
 import { log_error_generic } from "./log_error_generic.mjs";
 import { log } from "./log.mjs";
 export function log_error(message) {
+  let truncated = string_take(message, 1000);
   let fn = log;
   log_error_generic(fn, message);
 }
