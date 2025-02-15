f5f1cb134e08e0060d8f593d9249e84424d170cb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 19:06:39 -0500

    2025-02-15T00:06:38.692Z watch   js_identifiers_scoped

--- public/src/js_identifiers_scoped.mjs
+++ public/src/js_identifiers_scoped.mjs
@@ -2,7 +2,7 @@ import { js_identifiers_scoped_each } from "./js_identifiers_scoped_each.mjs";
 import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function js_identifiers_scoped(visitor) {
-  let { stack, node } = visitor;
+  let { stack } = visitor;
   let names = list_adder(function (la) {
     each(stack, function (stack_item) {
       let stack_item_names = js_identifiers_scoped_each(stack_item, visitor);
