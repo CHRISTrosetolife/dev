a3a37b250d3fdefb34d20103eb10e4df3eb08c0e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 11:14:17 -0500

    2025-02-15T16:14:16.205Z watch   js_keyword_add

--- public/src/js_keyword_add.mjs
+++ public/src/js_keyword_add.mjs
@@ -1,1 +1,4 @@
-export function js_keyword_add() {}
+import { function_new_constant_string } from "./function_new_constant_string.mjs";
+export async function js_keyword_add(kw) {
+  await function_new_constant_string();
+}
