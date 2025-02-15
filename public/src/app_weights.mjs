9afe642642f155058cac8a83e173c64e66c00612
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 21:48:23 -0500

    2025-02-15T02:48:23.081Z watch   app_weights

--- public/src/app_weights.mjs
+++ public/src/app_weights.mjs
@@ -1,4 +1,4 @@
-import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
+import { html_input_width_full_placeholder } from "./html_input_width_full_placeholder.mjs";
 export function app_weights() {
-  let root = html_style_default_initialize();
+  html_input_width_full_placeholder(parent, placeholder);
 }
