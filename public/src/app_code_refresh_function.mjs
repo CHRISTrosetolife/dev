a42c782056b547069693fa1f4e2f5f369a587280
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 15:42:48 -0500

    2025-02-15T20:42:47.985Z watch   app_code_refresh_function

--- public/src/app_code_refresh_function.mjs
+++ public/src/app_code_refresh_function.mjs
@@ -112,6 +112,8 @@ export async function app_code_refresh_function(context) {
       } else if (object_property_exists(selection_result, "one")) {
         html_button(overlay, "Selection variablize", async function () {
           let stack = object_property_get(selection_result, "one");
+          let visitor = object_property_get(m, "visitor");
+          let node2 = object_property_get(visitor, "node");
           let b = list_find_last(stack, js_node_type_statement_block_is);
           let list = list_next(stack, b);
           js_variablize();
