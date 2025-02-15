1d3107b262d38d2ff05797cca06a20dea9a32b87
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 13:36:07 -0500

    2025-02-15T18:36:06.605Z watch   js_identifiers_duplicates

--- public/src/js_identifiers_duplicates.mjs
+++ public/src/js_identifiers_duplicates.mjs
@@ -2,11 +2,12 @@ import { log } from "./log.mjs";
 import { js_identifiers_scoped_each } from "./js_identifiers_scoped_each.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_visit } from "./js_visit.mjs";
+import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export function js_identifiers_duplicates(ast) {
   js_visit(ast, function (v) {
     let node = object_property_get(v, "node");
     let identifiers = js_identifiers_scoped_each(v, node);
-    if (false) {
+    if (list_empty_not_is(identifiers)) {
       log({
         identifiers,
       });
