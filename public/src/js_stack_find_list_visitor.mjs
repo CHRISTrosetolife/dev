2dfd7ed266197c53e89321476745918670acb4f8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:28:50 -0500

    2025-02-15T15:28:49.754Z watch   js_stack_find_list_visitor

--- public/src/js_stack_find_list_visitor.mjs
+++ public/src/js_stack_find_list_visitor.mjs
@@ -1,1 +1,6 @@
-export function js_stack_find_list_visitor() {}
+import { js_stack_find_list } from "./js_stack_find_list.mjs";
+export function js_stack_find_list_visitor(v) {
+  let { stack } = v;
+  let v2 = js_stack_find_list(stack);
+  return v2;
+}
