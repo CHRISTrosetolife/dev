013c8613563689319550fdb3a0d2cb94071c386e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 16:57:59 -0500

    2025-02-15T21:57:59.317Z watch   app_code_batch_path_get

--- public/src/app_code_batch_path_get.mjs
+++ public/src/app_code_batch_path_get.mjs
@@ -2,7 +2,6 @@ import { app_code_data_get } from "./app_code_data_get.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function app_code_batch_path_get() {
   let data = await app_code_data_get();
-  let batch2 = object_property_get(data, "batch");
-  let files2 = object_property_get(batch2, "files");
-  return files2;
+  let batch_path = object_property_get(data, "batch_path");
+  return batch_path;
 }
