d99f9b6fc1c28dc432d09143a4cc3c57dbfccbc5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:40:05 -0500

    2025-02-15T15:40:05.296Z watch   list_find_last_index

--- public/src/list_find_last_index.mjs
+++ public/src/list_find_last_index.mjs
@@ -1,7 +1,7 @@
 import { list_index } from "./list_index.mjs";
 import { list_find_last } from "./list_find_last.mjs";
-export function list_find_last_index(stack, predicate) {
-  let last = list_find_last(stack, predicate);
-  let v = list_index(stack, last);
+export function list_find_last_index(list, predicate) {
+  let last = list_find_last(list, predicate);
+  let v = list_index(list, last);
   return v;
 }
