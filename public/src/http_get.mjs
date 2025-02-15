fa07167daefd8c59b036594413566bb5577b22b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 16:16:01 -0500

    2025-02-15T21:15:59.280Z watch   http_get

--- public/src/http_get.mjs
+++ public/src/http_get.mjs
@@ -1,4 +1,4 @@
-import { web_not_is } from "./web_not_is.mjs";
+import { web_is } from "./web_is.mjs";
 import { http_data_get } from "./http_data_get.mjs";
 import { import_multi } from "./import_multi.mjs";
 export async function http_get(url) {
