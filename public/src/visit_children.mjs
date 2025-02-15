1475d0d1ffaa776d4312498c1eb3011a03a68d37
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:11:13 -0500

    2025-02-15T15:11:12.592Z watch   visit_children

--- public/src/visit_children.mjs
+++ public/src/visit_children.mjs
@@ -1,6 +1,11 @@
 import { visit_each_children } from "./visit_each_children.mjs";
-import { each } from "./each.mjs";
-export function visit_children(parent, children_get, filter, each, stack) {
+export function visit_children(
+  parent,
+  children_get,
+  filter,
+  lambda_each,
+  stack,
+) {
   let children = children_get(parent);
   for (let node of children) {
     if (filter(node)) {
@@ -8,7 +13,7 @@ export function visit_children(parent, children_get, filter, each, stack) {
         parent,
         children_get,
         filter,
-        each,
+        lambda_each,
         stack,
         node,
       );
