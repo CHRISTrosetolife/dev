ce9624ece44b892bfabb12a44b28cf2ef189a729
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:52:27 -0500

    2025-02-15T15:52:23.474Z watch   object_property_get

---
+++ public/src/assert_get_result.mjs
@@ -0,0 +1,12 @@
+import { undefined_not_is } from "./undefined_not_is.mjs";
+import { assert_message } from "./assert_message.mjs";
+export function assert_get_result(result, key, value) {
+  assert_message(undefined_not_is, [result], function () {
+    let v = {
+      key,
+      value,
+    };
+    return v;
+  });
+  return result;
+}
--- public/src/object_property_get.mjs
+++ public/src/object_property_get.mjs
@@ -1,10 +1,6 @@
-import { assert_message } from "./assert_message.mjs";
-import { undefined_not_is } from "./undefined_not_is.mjs";
+import { assert_get_result } from "./assert_get_result.mjs";
 export function object_property_get(object, property_name) {
   let result = object[property_name];
-  assert_message(undefined_not_is, [result], () => ({
-    property_name,
-    object,
-  }));
-  return result;
+  let v = assert_get_result(result, property_name, object);
+  return v;
 }
