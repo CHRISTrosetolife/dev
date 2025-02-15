c2122a28b8c212004cb88bf7ff54347a9dde47c6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 12:56:54 -0500

    2025-02-15T17:56:53.742Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -4,5 +4,7 @@ import { function_parse } from "./function_parse.mjs";
 export async function sandbox_5() {
   let ast = await function_parse(fn_name("sandbox_5b"));
   let v3 = js_identifiers_shadowed_names(ast);
-  return v3;
+  let y, z;
+  let x = y + z;
+  return x;
 }
