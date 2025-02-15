9b76e6d7a8a5d2d4c58d457e4fe2de00c0419565
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 16:58:51 -0500

    2025-02-15T21:58:50.817Z watch   app_code_file_contents

--- public/src/app_code_file_contents.mjs
+++ public/src/app_code_file_contents.mjs
@@ -1,8 +1,7 @@
+import { app_code_file_get } from "./app_code_file_get.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { app_code_files_get } from "./app_code_files_get.mjs";
 export async function app_code_file_contents(path) {
-  let files = await app_code_files_get();
-  let file = object_property_get(files, path);
+  let file = await app_code_file_get(path);
   let contents = object_property_get(file, "contents");
   return contents;
 }
