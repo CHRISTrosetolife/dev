afb077f293f72ccc7275c4592991a90dd8f140c8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 11:16:05 -0500

    2025-02-15T16:16:03.127Z watch   js_keyword_add

--- public/src/js_keyword_add.mjs
+++ public/src/js_keyword_add.mjs
@@ -3,5 +3,5 @@ import { string_suffix_without } from "./string_suffix_without.mjs";
 import { function_new_constant_string } from "./function_new_constant_string.mjs";
 export async function js_keyword_add(kw) {
   let prefix = string_suffix_without(js_keyword_add, "_add");
-  await function_new_constant_string(function_name_combine(prefix), kw);
+  await function_new_constant_string(function_name_combine(prefix, kw), kw);
 }
