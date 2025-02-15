d9fe8f370632e3dd6b34f2ada08c928b1256ac65
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:31:58 -0500

    2025-02-15T15:31:57.770Z watch   list_get_end_2

--- public/src/list_get_end_2.mjs
+++ public/src/list_get_end_2.mjs
@@ -1,5 +1,5 @@
 import { list_get_end } from "./list_get_end.mjs";
-export function list_get_end_2(stack) {
-  let v = list_get_end(stack, 1);
+export function list_get_end_2(list) {
+  let v = list_get_end(list, 2);
   return v;
 }
