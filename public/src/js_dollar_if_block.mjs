500d49cd2e53d787024365e286f9c41b521b2c85
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:35:20 -0500

    2025-02-15T15:35:19.471Z watch   js_dollar_if_block

--- public/src/js_dollar_if_block.mjs
+++ public/src/js_dollar_if_block.mjs
@@ -1,16 +1,16 @@
+import { list_get_end_2 } from "./list_get_end_2.mjs";
 import { list_set } from "./list_set.mjs";
 import { list_index } from "./list_index.mjs";
 import { js_code_if } from "./js_code_if.mjs";
 import { js_parse_first } from "./js_parse_first.mjs";
 import { list_is } from "./list_is.mjs";
-import { list_get_end_1 } from "./list_get_end_1.mjs";
 export function js_dollar_if_block(v, condition) {
   let { parent } = v;
   let { stack } = v;
-  let s1 = list_get_end_1(stack);
-  if (list_is(s1)) {
+  let s2 = list_get_end_2(stack);
+  if (list_is(s2)) {
     let statement = js_parse_first(js_code_if(condition));
-    let index = list_index(s1, parent);
-    list_set(s1, index, statement);
+    let index = list_index(s2, parent);
+    list_set(s2, index, statement);
   }
 }
