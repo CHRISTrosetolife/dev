62669a4421475a0957a29ea66695291496601d6c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 16:19:06 -0500

    2025-02-15T21:19:06.047Z watch   http_get

--- public/src/http_get.mjs
+++ public/src/http_get.mjs
@@ -1,24 +1,21 @@
+import { invoke_async } from "./invoke_async.mjs";
 import { html_load } from "./html_load.mjs";
 import { web_is } from "./web_is.mjs";
 import { http_data_get } from "./http_data_get.mjs";
 import { import_multi } from "./import_multi.mjs";
 export async function http_get(url) {
   let axios = await import_multi("axios", "axios");
-  let get;
+  let wrapper;
   if (web_is()) {
-    get = async function () {
-      let v3 = await html_load(result_get);
-      return v3;
-    };
+    wrapper = html_load;
   } else {
     axios = axios.default;
-    get = result_get;
+    wrapper = invoke_async;
   }
-  let result = await get();
-  let v = http_data_get(result);
-  return v;
-  async function result_get() {
+  let result = await wrapper(async function () {
     let v2 = await axios.get(url);
     return v2;
-  }
+  });
+  let v = http_data_get(result);
+  return v;
 }
