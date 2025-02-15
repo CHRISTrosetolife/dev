86fbe718eeea4d0a8df3f9beb65425a824a08ae5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 21:49:13 -0500

    2025-02-15T02:49:13.308Z watch   app_weights

--- public/src/app_weights.mjs
+++ public/src/app_weights.mjs
@@ -1,4 +1,6 @@
 import { each } from "./each.mjs";
 export function app_weights() {
-  each([1, 2, 5, 10, 15, 20, 25, 45, 50], function (item) {});
+  each([1, 2, 5, 10, 15, 20, 25, 45, 50], function (item) {
+    html_button_small;
+  });
 }
