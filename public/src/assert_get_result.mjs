5008fa713e68592dcf5486f47f38c8aa4c156c19
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:53:47 -0500

    2025-02-15T15:53:45.592Z watch   assert_get_result

--- public/src/assert_get_result.mjs
+++ public/src/assert_get_result.mjs
@@ -1,10 +1,10 @@
 import { undefined_not_is } from "./undefined_not_is.mjs";
 import { assert_message } from "./assert_message.mjs";
-export function assert_get_result(result, key, value) {
+export function assert_get_result(result, key, object) {
   assert_message(undefined_not_is, [result], function () {
     let v = {
       key,
-      value,
+      object,
     };
     return v;
   });
