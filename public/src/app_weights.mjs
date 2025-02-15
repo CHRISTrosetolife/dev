a003fd066d178d83ee6aefd2ab1c9b031a3c2dd3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-14 21:55:47 -0500

    2025-02-15T02:55:46.567Z watch   app_weights

--- public/src/app_weights.mjs
+++ public/src/app_weights.mjs
@@ -1,16 +1,10 @@
-import { html_style_success_if } from "./html_style_success_if.mjs";
-import { string_to } from "./string_to.mjs";
-import { html_button_text_click } from "./html_button_text_click.mjs";
-import { each } from "./each.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
+import { html_input_width_full_placeholder } from "./html_input_width_full_placeholder.mjs";
 export function app_weights() {
   let root = html_style_default_initialize();
-  let weights = [1, 2, 5, 10, 15, 20, 25, 45, 50];
-  each(weights, function (value) {
-    let selected = false;
-    let b = html_button_text_click(root, string_to(value), function () {
-      selected = !selected;
-      html_style_success_if(b, selected);
-    });
-  });
+  let add_weight = html_input_width_full_placeholder(
+    root,
+    "Add available weight",
+  );
+  let weights = [];
 }
