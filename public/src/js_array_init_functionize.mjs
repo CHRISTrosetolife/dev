352950463eaf3e99f5b5cf8aafae078c68ac056b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:14:29 -0500

    2025-02-15T15:14:29.340Z watch   js_array_init_functionize

--- public/src/js_array_init_functionize.mjs
+++ public/src/js_array_init_functionize.mjs
@@ -1,3 +1,4 @@
+import { fn_name } from "./fn_name.mjs";
 import { js_call } from "./js_call.mjs";
 import { each_reverse } from "./each_reverse.mjs";
 import { js_init_index_insert } from "./js_init_index_insert.mjs";
@@ -5,7 +6,6 @@ import { js_variable_declarators } from "./js_variable_declarators.mjs";
 import { list_insert } from "./list_insert.mjs";
 import { list_get_end } from "./list_get_end.mjs";
 import { object_property_set } from "./object_property_set.mjs";
-import { list_add } from "./list_add.mjs";
 export function js_array_init_functionize(ast) {
   let vs = js_variable_declarators(ast, "ArrayExpression");
   for (let v of vs) {
@@ -14,13 +14,13 @@ export function js_array_init_functionize(ast) {
     let { id } = node;
     let { init } = node;
     let { elements } = init;
-    each_reverse(elements, (e) => {
-      let call = js_call(list_add.name, [id, e]);
+    each_reverse(elements, function (e) {
+      let call = js_call(fn_name("list_add"), [id, e]);
       let { stack } = v;
-      let variable_declaration_parent = list_get_end(stack, 2);
+      let variable_declaration_parent = list_get_end(stack, 3);
       list_insert(variable_declaration_parent, index_insert, call);
     });
-    const init_new = js_call(object_new.name, []);
+    let init_new = js_call(fn_name("object_new"), []);
     object_property_set(node, "init", init_new);
   }
 }
