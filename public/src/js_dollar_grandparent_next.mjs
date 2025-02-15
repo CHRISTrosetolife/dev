f047037cd458723f38bcaf1c7882fe0315d19d83
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:33:39 -0500

    2025-02-15T15:33:39.063Z watch   js_dollar_grandparent_next

--- public/src/js_dollar_grandparent_next.mjs
+++ public/src/js_dollar_grandparent_next.mjs
@@ -3,8 +3,8 @@ import { list_get } from "./list_get.mjs";
 import { object_merge_strict } from "./object_merge_strict.mjs";
 export async function js_dollar_grandparent_next(v, lambda) {
   await js_dollar_grandparent(v, async function (a) {
-    let { s1, index } = a;
-    let next = list_get(s1, index);
+    let { s2, index } = a;
+    let next = list_get(s2, index);
     await lambda(
       object_merge_strict(a, {
         next,
