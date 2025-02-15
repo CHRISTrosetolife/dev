da6d62e2b0fd2a923caad7624b9c691ebf46fdc5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 15:55:03 -0500

    2025-02-15T20:55:02.494Z watch   js_variablize

--- public/src/js_variablize.mjs
+++ public/src/js_variablize.mjs
@@ -1,9 +1,11 @@
+import { object_replace } from "./object_replace.mjs";
 import { js_name_unique_v_parsed } from "./js_name_unique_v_parsed.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_index } from "./list_index.mjs";
 import { list_is } from "./list_is.mjs";
 import { assert } from "./assert.mjs";
+import { object_copy } from "./object_copy.mjs";
 export function js_variablize(ast, parent_list, list_item, expression) {
   assert(list_is, [parent_list]);
   let index = list_index(parent_list, list_item);
@@ -11,7 +13,8 @@ export function js_variablize(ast, parent_list, list_item, expression) {
   parent_list.splice(index, 0, parsed);
   let { declarations } = parsed;
   let first = list_first(declarations);
-  first.init = expression;
+  first.init = object_copy(expression);
   let parsed2 = js_parse_expression(variable_name);
+  object_replace(expression, parsed2);
   return parsed2;
 }
