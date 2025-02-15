bed7bf8cb3beaae49a37540d18fd6b548ca52ef8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 16:16:26 -0500

    2025-02-15T21:16:23.459Z watch   http_get

--- public/src/http_get.mjs
+++ public/src/http_get.mjs
@@ -1,9 +1,11 @@
+import { html_load } from "./html_load.mjs";
 import { web_is } from "./web_is.mjs";
 import { http_data_get } from "./http_data_get.mjs";
 import { import_multi } from "./import_multi.mjs";
 export async function http_get(url) {
   let axios = await import_multi("axios", "axios");
   if (web_is()) {
+    await html_load();
   } else {
     axios = axios.default;
   }
