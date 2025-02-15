9f3ff640229a2b8636dd11cd05ab338154c7f14b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 11:15:19 -0500

    2025-02-15T16:15:19.168Z watch   js_keyword_add

--- public/src/js_keyword_add.mjs
+++ public/src/js_keyword_add.mjs
@@ -1,4 +1,7 @@
+import { string_suffix_without } from "./string_suffix_without.mjs";
 import { function_new_constant_string } from "./function_new_constant_string.mjs";
 export async function js_keyword_add(kw) {
-  await function_new_constant_string();
+  await function_new_constant_string(
+    string_suffix_without(js_keyword_add, "_add"),
+  );
 }
