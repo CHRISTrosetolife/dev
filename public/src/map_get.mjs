9b16ad68b6730f98bc74921828e57ad37e01e176
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:54:22 -0500

    2025-02-15T15:54:11.064Z watch   map_get

--- public/src/map_get.mjs
+++ public/src/map_get.mjs
@@ -1,4 +1,6 @@
+import { assert_get_result } from "./assert_get_result.mjs";
 export function map_get(map, key) {
   let v = map.get(key);
-  return v;
+  let v2 = assert_get_result(v, key, map);
+  return v2;
 }
