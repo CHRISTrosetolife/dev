c71b0e938652c6825b85166c4e060afea7b520cf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 10:10:54 -0500

    2025-02-15T15:10:53.581Z watch   visit

--- public/src/visit.mjs
+++ public/src/visit.mjs
@@ -1,5 +1,4 @@
 import { visit_each_children } from "./visit_each_children.mjs";
-import { each } from "./each.mjs";
-export function visit(root, children_get, filter, each, stack) {
-  visit_each_children(null, children_get, filter, each, stack, root);
+export function visit(root, children_get, filter, lambda_each, stack) {
+  visit_each_children(null, children_get, filter, lambda_each, stack, root);
 }
