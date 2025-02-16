72875b9c927174510cb1c751180aa594a67610d9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 10:33:09 -0500

    2025-02-16T15:33:06.242Z watch   function_transform_return

--- public/src/function_transform_return.mjs
+++ public/src/function_transform_return.mjs
@@ -1,15 +1,11 @@
-import { list_size } from "./list_size.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { function_transform_args_return } from "./function_transform_args_return.mjs";
-import { list_to } from "./list_to.mjs";
-import { equal } from "./equal.mjs";
-import { assert } from "./assert.mjs";
 export async function function_transform_return(lambda_names, function_name) {
-  let args = list_to(arguments);
-  let length = list_size(args);
-  assert(equal, [length, 2]);
-  return await function_transform_args_return(
+  assert_arguments_length(arguments, 2);
+  let v = await function_transform_args_return(
     lambda_names,
     function_name,
     null,
   );
+  return v;
 }
