54dfdd89dff07f2ce46619e7e01385b07247d3e5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 15:04:44 -0500

    2025-02-16T20:04:44.131Z watch   app_code_local_download

--- public/src/app_code_local_download.mjs
+++ public/src/app_code_local_download.mjs
@@ -7,12 +7,13 @@ import { storage_file_download } from "./storage_file_download.mjs";
 import { app_code_file_name_latest } from "./app_code_file_name_latest.mjs";
 import { app_code_local_user_path } from "./app_code_local_user_path.mjs";
 export async function app_code_local_download(username) {
-  let latest_path_local = app_code_local_files_path_latest();
   let latest_path_user = app_code_local_user_path(
     username,
     app_code_file_name_latest(),
   );
   let latest_user = await storage_file_download(latest_path_user);
+  let latest_path_local = app_code_local_files_path_latest();
+  let latest_local = await storage_file_download(latest_path_local);
   let batch_path = object_property_get(latest_user, "batch_path");
   let batches = await app_code_batches(batch_path, username);
   await each_async(batches, async function (batch) {
