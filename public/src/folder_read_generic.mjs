9747ab62d7e13b20ac6d1ad538c0a4a45669da99
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 11:39:13 -0500

    2025-02-16T16:39:12.962Z watch   folder_read_generic

--- public/src/folder_read_generic.mjs
+++ public/src/folder_read_generic.mjs
@@ -1,22 +1,22 @@
+import { list_is } from "./list_is.mjs";
 import { folder_path_slash_forward } from "./folder_path_slash_forward.mjs";
 import { list_sort_string_map } from "./list_sort_string_map.mjs";
 import { folder_is } from "./folder_is.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
 import { each_async } from "./each_async.mjs";
 import { identity } from "./identity.mjs";
-import { string_is } from "./string_is.mjs";
 import { assert } from "./assert.mjs";
 import { import_node } from "./import_node.mjs";
 import { string_ends_with } from "./string_ends_with.mjs";
-export async function folder_read_generic(folder, file_extension, recursive) {
+export async function folder_read_generic(folder, file_extensions, recursive) {
   let fs = await import_node("fs");
   let path = await import_node("path");
-  assert(string_is, [file_extension]);
+  assert(list_is, [file_extension]);
   folder = folder_path_slash_forward(folder);
   folder += "/";
   async function traverse(dir, lambda_each) {
     let files = await fs.promises.readdir(dir);
-    await each_async(files, async (file) => {
+    await each_async(files, async function (file) {
       let path_full = path.join(dir, file);
       if (recursive) {
         if (await folder_is(path_full)) {
@@ -28,7 +28,7 @@ export async function folder_read_generic(folder, file_extension, recursive) {
       }
     });
   }
-  let files = await list_adder_async(async (la) => {
+  let files = await list_adder_async(async function (la) {
     await traverse(folder, la);
   });
   list_sort_string_map(files, identity);
