f5ee451d11c20354d5e93af6236e4ea3aa707e86
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 10:04:54 -0500

    2025-02-16T15:04:52.998Z watch   html_on

--- public/src/html_on.mjs
+++ public/src/html_on.mjs
@@ -1,9 +1,12 @@
+import { html_event_listener_remove } from "./html_event_listener_remove.mjs";
 import { html_event_listener_add } from "./html_event_listener_add.mjs";
 export function html_on(component, type, lambda) {
   let { element } = component;
   html_event_listener_add(element, type, lambda);
   let v = {
-    remove: function () {html_event_listener_remove()},
+    remove: function () {
+      html_event_listener_remove(element, type, lambda);
+    },
   };
   return v;
 }
