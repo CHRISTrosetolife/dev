f8e1dc25d1d43e329465e3c74f1318f5bb5d64a1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 10:27:58 -0500

    2025-02-16T15:27:57.721Z watch   function_transform

--- public/src/function_transform.mjs
+++ public/src/function_transform.mjs
@@ -1,6 +1,6 @@
 import { file_js_transform_regular } from "./file_js_transform_regular.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
-export async function function_transform(e, lambda) {
-  let file_path = function_name_to_path(e);
+export async function function_transform(function_name, lambda) {
+  let file_path = function_name_to_path(function_name);
   await file_js_transform_regular(file_path, lambda);
 }
