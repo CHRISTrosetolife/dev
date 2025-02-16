92e6faccb29f7e31f9b0da8eb7f96b05aa24a267
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 11:40:04 -0500

    2025-02-16T16:40:04.075Z watch   folder_read_generic

--- public/src/folder_read_generic.mjs
+++ public/src/folder_read_generic.mjs
@@ -1,3 +1,4 @@
+import { list_any } from "./list_any.mjs";
 import { list_is } from "./list_is.mjs";
 import { folder_path_slash_forward } from "./folder_path_slash_forward.mjs";
 import { list_sort_string_map } from "./list_sort_string_map.mjs";
@@ -11,7 +12,7 @@ import { string_ends_with } from "./string_ends_with.mjs";
 export async function folder_read_generic(folder, file_extensions, recursive) {
   let fs = await import_node("fs");
   let path = await import_node("path");
-  assert(list_is, [file_extension]);
+  assert(list_is, [file_extensions]);
   folder = folder_path_slash_forward(folder);
   folder += "/";
   async function traverse(dir, lambda_each) {
@@ -23,7 +24,12 @@ export async function folder_read_generic(folder, file_extensions, recursive) {
           await traverse(path_full, lambda_each);
         }
       }
-      if (string_ends_with(path_full, file_extension)) {
+      if (
+        list_any(file_extensions, function (file_extension) {
+          let v = string_ends_with(path_full, file_extension);
+          return v;
+        })
+      ) {
         lambda_each(path_full);
       }
     });
