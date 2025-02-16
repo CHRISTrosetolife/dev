b9bf546a834ba1bc98bc1bc56ab092e9af156914
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 15:05:27 -0500

    2025-02-16T20:05:26.406Z watch   date_string_iso

--- public/src/date_string_iso.mjs
+++ public/src/date_string_iso.mjs
@@ -1,4 +1,6 @@
 import { date_now } from "./date_now.mjs";
 export function date_string_iso() {
-  return date_now().toISOString();
+  let d = date_now();
+  let v = d.toISOString();
+  return v;
 }
