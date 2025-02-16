ed47d01cf4f236cbfbd88a5d341abb9dbc3bc471
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 12:38:34 -0500

    2025-02-16T17:38:33.233Z watch   file_js_parse

--- public/src/file_js_parse.mjs
+++ public/src/file_js_parse.mjs
@@ -2,6 +2,6 @@ import { file_read } from "./file_read.mjs";
 import { js_parse } from "./js_parse.mjs";
 export async function file_js_parse(file_name) {
   let code = await file_read(file_name);
-  let parsed = js_parse(code);
-  return parsed;
+  let ast = js_parse(code);
+  return ast;
 }
