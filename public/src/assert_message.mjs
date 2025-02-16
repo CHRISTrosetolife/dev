0f8772397dcd2ccbb93139399a90d8f22287d4ef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 21:35:44 -0500

    2025-02-16T02:35:43.606Z watch   assert_message

--- public/src/assert_message.mjs
+++ public/src/assert_message.mjs
@@ -3,12 +3,14 @@ import { assert_message_string } from "./assert_message_string.mjs";
 import { json_to } from "./json_to.mjs";
 import { string_take } from "./string_take.mjs";
 export function assert_message(fn, args, message_get) {
-  return assert_message_string(fn, args, () => {
+  let v = assert_message_string(fn, args, function () {
     let j = json_to(message_get());
     let limit = 1000;
-    if (string_size(j )> limit) {
-      return string_take(j, limit);
+    if (string_size(j) > limit) {
+      let v2 = string_take(j, limit);
+      return v2;
     }
     return j;
   });
+  return v;
 }
