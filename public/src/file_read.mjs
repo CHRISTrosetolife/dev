a92b9d1694db33017af3f33ac579f288dad1d5de
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 09:30:47 -0500

    2025-02-16T14:30:45.964Z watch   file_read

--- public/src/file_read.mjs
+++ public/src/file_read.mjs
@@ -1,3 +1,4 @@
+import { global_files } from "./global_files.mjs";
 import { file_read_generic } from "./file_read_generic.mjs";
 import { global_get } from "./global_get.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -5,10 +6,12 @@ import { web_is } from "./web_is.mjs";
 export async function file_read(file_name) {
   if (web_is()) {
     let g = global_get();
-    let { files } = g;
+    let { [global_files()]: files } = g;
     let existing = object_property_get(files, file_name);
-    return object_property_get(existing, "contents");
+    let v = object_property_get(existing, "contents");
+    return v;
   }
   let encoding = "utf-8";
-  return await file_read_generic(file_name, encoding);
+  let v2 = await file_read_generic(file_name, encoding);
+  return v2;
 }
