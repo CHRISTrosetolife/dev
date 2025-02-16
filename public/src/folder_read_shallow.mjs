bf29012963d5d15ed98ee353b3b3bd959b1b6bda
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 11:38:54 -0500

    2025-02-16T16:38:53.880Z watch   folder_read_shallow

--- public/src/folder_read_shallow.mjs
+++ public/src/folder_read_shallow.mjs
@@ -1,6 +1,6 @@
 import { folder_read_generic } from "./folder_read_generic.mjs";
 export async function folder_read_shallow(folder, file_extension) {
   let recursive = false;
-  let files = await folder_read_generic(folder, file_extension, recursive);
+  let files = await folder_read_generic(folder, [file_extension], recursive);
   return files;
 }
