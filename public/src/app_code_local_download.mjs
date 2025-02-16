5f833f9ef64d2f59b595970be638d77f7c1478f9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 12:33:23 -0500

    2025-02-16T17:33:20.806Z watch   app_code_local_download

--- public/src/app_code_local_download.mjs
+++ public/src/app_code_local_download.mjs
@@ -1,8 +1,10 @@
+import { storage_file_download } from "./storage_file_download.mjs";
 import { app_code_file_name_latest } from "./app_code_file_name_latest.mjs";
 import { app_code_local_user_path } from "./app_code_local_user_path.mjs";
-export function app_code_local_download(username) {
+export async function app_code_local_download(username) {
   let file_path_latest = app_code_local_user_path(
     username,
     app_code_file_name_latest(),
   );
+  await storage_file_download();
 }
