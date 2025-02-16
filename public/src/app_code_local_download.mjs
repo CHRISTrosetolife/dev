d83d70b32ab65dd97d162f4eb5d16995841f6ec2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 15:13:52 -0500

    2025-02-16T20:13:51.619Z watch   app_code_local_download

--- public/src/app_code_local_download.mjs
+++ public/src/app_code_local_download.mjs
@@ -19,6 +19,7 @@ export async function app_code_local_download(username) {
   let when_local = object_property_get(latest_local, "when");
   let when_local_date = new Date(when_local);
   if (when_local_date > when_user_date) {
+    return;
   }
   let batch_path = object_property_get(latest_user, "batch_path");
   let batches = await app_code_batches(batch_path, username);
