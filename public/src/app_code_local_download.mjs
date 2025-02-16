684827ff6e8244dfbdd6be4bc369f58b25952e22
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 13:02:30 -0500

    2025-02-16T18:02:29.623Z watch   app_code_local_download

--- public/src/app_code_local_download.mjs
+++ public/src/app_code_local_download.mjs
@@ -4,7 +4,6 @@ import { object_property_get } from "./object_property_get.mjs";
 import { storage_file_download } from "./storage_file_download.mjs";
 import { app_code_file_name_latest } from "./app_code_file_name_latest.mjs";
 import { app_code_local_user_path } from "./app_code_local_user_path.mjs";
-import { list_add } from "./list_add.mjs";
 export async function app_code_local_download(username) {
   let file_path_latest = app_code_local_user_path(
     username,
@@ -13,11 +12,12 @@ export async function app_code_local_download(username) {
   let latest_object = await storage_file_download(file_path_latest);
   let batch_path = object_property_get(latest_object, "batch_path");
   let batch = await current_get(batch_path);
-  await list_adder_async(async function (la) {});
-  while (next_exists(batch)) {
-    batch = await next_get(batch);
-    list_add(batches, batch);
-  }
+  let batches = await list_adder_async(async function (la) {
+    while (next_exists(batch)) {
+      batch = await next_get(batch);
+      la(batch);
+    }
+  });
   async function current_get(batch_path) {
     let file_path_batch = app_code_local_user_path(username, batch_path);
     let batch = await storage_file_download(file_path_batch);
