90957bc4c7520262c9f095334ee194b415d32d5d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 21:36:39 -0500

    2025-02-16T02:36:38.391Z watch   assert_message

---
+++ public/src/assert_message.mjs
@@ -0,0 +1,12 @@
+import { string_take_soft } from "./string_take_soft.mjs";
+import { assert_message_string } from "./assert_message_string.mjs";
+import { json_to } from "./json_to.mjs";
+export function assert_message(fn, args, message_get) {
+  let v = assert_message_string(fn, args, function () {
+    let j = json_to(message_get());
+    let limit = 1000;
+    j = string_take_soft(j, limit);
+    return j;
+  });
+  return v;
+}
