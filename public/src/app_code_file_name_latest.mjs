081e0e5f1b2fc7c6ed23a9a175bfdbf8f15b8588
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 12:14:59 -0500

    2025-02-16T17:14:58.212Z watch   app_code_file_name_latest

---
+++ public/src/app_code_file_name_latest.mjs
@@ -0,0 +1,5 @@
+import { file_name_json } from "./file_name_json.mjs";
+export function app_code_file_name_latest() {
+  let v = file_name_json("latest");
+  return v;
+}
--- public/src/app_code_local_files_path_latest.mjs
+++
@@ -1,5 +0,0 @@
-import { file_name_json } from "./file_name_json.mjs";
-import { app_code_local_files_path } from "./app_code_local_files_path.mjs";
-export function app_code_local_files_path_latest() {
-  return app_code_local_files_path(file_name_json("latest"));
-}
