93dcf2c3e056f479e1d6729cfd697e7a6772fd5e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 12:54:30 -0500

    2025-02-16T17:54:29.755Z watch   app_code_local_download

--- public/src/app_code_local_download.mjs
+++ public/src/app_code_local_download.mjs
@@ -1,3 +1,4 @@
+import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { storage_file_download } from "./storage_file_download.mjs";
 import { app_code_file_name_latest } from "./app_code_file_name_latest.mjs";
@@ -12,7 +13,7 @@ export async function app_code_local_download(username) {
   while (true) {
     let file_path_batch = app_code_local_user_path(username, batch_path);
     let batch = await storage_file_download(file_path_batch);
-    if (false) {
+    if (object_property_exists_not(batch, "batch_path_previous")) {
     }
   }
 }
