bd371c5a66e1a5b824d7a4ca606f25cf7fa1d2bb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 12:27:06 -0500

    2025-02-16T17:27:03.832Z r app_code_local_user_path app_code_local_user_path_web

--- public/src/app_code_user_upload.mjs
+++ public/src/app_code_user_upload.mjs
@@ -1,7 +1,7 @@
+import { app_code_local_user_path_web } from "./app_code_local_user_path_web.mjs";
 import { firebase_upload_object } from "./firebase_upload_object.mjs";
-import { app_code_local_user_path } from "./app_code_local_user_path.mjs";
 export async function app_code_user_upload(context, file_name, object) {
-  let file_path = app_code_local_user_path(context, file_name);
+  let file_path = app_code_local_user_path_web(context, file_name);
   await firebase_upload_object(file_path, object);
   let v = file_path;
   return v;
