73df8fa57243ebbb0d94f6999d8ef5fcb36ec6e2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 13:13:46 -0500

    2025-02-16T18:13:44.241Z watch   app_code_local_download

---
+++ public/src/app_code_local_download.mjs
@@ -0,0 +1,18 @@
+import { app_code_batches } from "./app_code_batches.mjs";
+import { each_async } from "./each_async.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { storage_file_download } from "./storage_file_download.mjs";
+import { app_code_file_name_latest } from "./app_code_file_name_latest.mjs";
+import { app_code_local_user_path } from "./app_code_local_user_path.mjs";
+export async function app_code_local_download(username) {
+  let file_path_latest = app_code_local_user_path(
+    username,
+    app_code_file_name_latest(),
+  );
+  let latest_object = await storage_file_download(file_path_latest);
+  let batch_path = object_property_get(latest_object, "batch_path");
+  let batches = await app_code_batches(batch_path, username);
+  await each_async(batches, async function (batch) {
+    let files = object_property_get(batch, "files");
+  });
+}
