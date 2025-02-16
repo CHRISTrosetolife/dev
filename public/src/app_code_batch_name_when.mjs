616b813ac0e3910ab7f7247248c3effb7914e074
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 15:09:00 -0500

    2025-02-16T20:09:00.059Z watch   app_code_batch_name_when

--- public/src/app_code_batch_name_when.mjs
+++ public/src/app_code_batch_name_when.mjs
@@ -1,8 +1,15 @@
+import { date_string_iso_to_file } from "./date_string_iso_to_file.mjs";
+import { date_to_string_iso } from "./date_to_string_iso.mjs";
 import { file_name_json } from "./file_name_json.mjs";
 import { path_join } from "./path_join.mjs";
 import { uuid } from "./uuid.mjs";
 export async function app_code_batch_name_when(when) {
   let id = await uuid();
-  let path = path_join(["batch", when, file_name_json(id)]);
+  let iso = date_to_string_iso(when);
+  let path = path_join([
+    "batch",
+    date_string_iso_to_file(iso),
+    file_name_json(id),
+  ]);
   return path;
 }
