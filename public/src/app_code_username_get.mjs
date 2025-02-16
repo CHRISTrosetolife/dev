ebaf4ca9653f56f3e3e6f42f6dba316fc5728643
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 11:32:58 -0500

    2025-02-16T16:32:57.542Z watch   app_code_username_get

--- public/src/app_code_username_get.mjs
+++ public/src/app_code_username_get.mjs
@@ -1,5 +1,7 @@
-import { app_code } from "./app_code.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 import { storage_local_get } from "./storage_local_get.mjs";
 export function app_code_username_get() {
-  return storage_local_get(app_code, "username");
+  let app_fn = object_property_get(context, "app_fn");
+  let v = storage_local_get(app_fn, "username");
+  return v;
 }
