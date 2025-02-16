e0f11b95bd9dedab07324cad4c24e54a3a8432fa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 11:31:51 -0500

    2025-02-16T16:31:47.346Z watch   app_code_refresh_main

--- public/src/app_code_refresh_main.mjs
+++ public/src/app_code_refresh_main.mjs
@@ -1,9 +1,9 @@
 import { app_code_username_exists } from "./app_code_username_exists.mjs";
 import { app_code_refresh_menu } from "./app_code_refresh_menu.mjs";
 import { app_code_refresh_username } from "./app_code_refresh_username.mjs";
-export function app_code_refresh_main(context) {
+export async function app_code_refresh_main(context) {
   if (app_code_username_exists(context)) {
-    app_code_refresh_menu(context);
+    await app_code_refresh_menu(context);
   } else {
     app_code_refresh_username(context);
   }
