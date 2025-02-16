d9a7c25365ce55fabf9695ec8f9969113ba4402d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 09:36:06 -0500

    2025-02-16T14:36:03.110Z watch   function_name_new

--- public/src/function_name_new.mjs
+++ public/src/function_name_new.mjs
@@ -1,3 +1,4 @@
+import { global_files } from "./global_files.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { add_1 } from "./add_1.mjs";
 import { function_import } from "./function_import.mjs";
@@ -10,7 +11,7 @@ export async function function_name_new(function_name_base) {
   while (true) {
     try {
       let g = global_get();
-      let { files } = g;
+      let { [global_files()]: files } = g;
       let function_path = function_name_to_path(function_name);
       if (object_property_exists_not(files, function_path)) {
         await function_import(function_name);
