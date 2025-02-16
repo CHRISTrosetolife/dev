61facd97ad56f3c69b7eeb28ce3d278eddc419d5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 11:22:52 -0500

    2025-02-16T16:22:51.574Z watch   app_code_refresh_html

--- public/src/app_code_refresh_html.mjs
+++ public/src/app_code_refresh_html.mjs
@@ -1,7 +1,7 @@
+import { app_code_file_get } from "./app_code_file_get.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { html_style_pre_wrap } from "./html_style_pre_wrap.mjs";
 import { string_html_escape } from "./string_html_escape.mjs";
-import { app_code_file_contents } from "./app_code_file_contents.mjs";
 import { storage_local_get_context } from "./storage_local_get_context.mjs";
 import { app_code_button_html_files } from "./app_code_button_html_files.mjs";
 import { fn_name } from "./fn_name.mjs";
@@ -10,11 +10,11 @@ import { html_load } from "./html_load.mjs";
 import { html_style_left } from "./html_style_left.mjs";
 import { html_style_monospace } from "./html_style_monospace.mjs";
 export async function app_code_refresh_html(context) {
-  await html_load(async () => {
+  await html_load(async function () {
     let root = app_code_screen_set(context, fn_name("app_code_refresh_html"));
     await app_code_button_html_files(context);
     let html_selected = storage_local_get_context(context, "html_selected");
-    let contents = await app_code_file_contents(html_selected);
+    let contents = await app_code_file_get(html_selected);
     let e = string_html_escape(contents);
     let t = html_p_text(root, e);
     html_style_left(t);
