45b7aa3e22c731db4b5e62b88638803256f6dde1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 12:40:30 -0500

    2025-02-16T17:40:29.566Z watch   file_js_unparse

--- public/src/file_js_unparse.mjs
+++ public/src/file_js_unparse.mjs
@@ -4,6 +4,8 @@ import { file_overwrite } from "./file_overwrite.mjs";
 import { js_unparse } from "./js_unparse.mjs";
 export async function file_js_unparse(file_name, ast) {
   let before = null;
+  if (false) {
+  }
   let code = js_unparse(ast);
   let prettied = await js_code_format(code);
   await file_overwrite(file_name, prettied);
