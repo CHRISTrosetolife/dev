ead5777aafc53c6f74e7daeef47b16776ca05b85
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 21:28:56 -0500

    2025-02-16T02:28:55.533Z watch   file_overwrite_generic

--- public/src/file_overwrite_generic.mjs
+++ public/src/file_overwrite_generic.mjs
@@ -7,6 +7,7 @@ import { global_function } from "./global_function.mjs";
 export async function file_overwrite_generic(file_name, contents, encoding) {
   if (web_is()) {
     let g = global_function(file_overwrite_generic);
+    let fcs = object_property_initialize(g, "files_changes", []);
     let { files } = global_get();
     let existing = object_property_initialize(files, file_name, {});
     object_property_set(existing, "contents", contents);
