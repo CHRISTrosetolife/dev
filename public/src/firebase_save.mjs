b4bbd1e1d2efb972c5bd86ce4d4fdae82f5eb52c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 21:22:40 -0500

    2025-02-16T02:22:38.776Z watch   firebase_save

--- public/src/firebase_save.mjs
+++ public/src/firebase_save.mjs
@@ -1,16 +1,12 @@
+import { object_property_increment } from "./object_property_increment.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { firebase_upload_object } from "./firebase_upload_object.mjs";
 import { equal } from "./equal.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { firebase_download } from "./firebase_download.mjs";
-import { object_property_initialize_increment } from "./object_property_initialize_increment.mjs";
 export async function firebase_save(storage_path, object_data) {
   let version_property = "version";
-  let version = object_property_initialize_increment(
-    object_data,
-    version_property,
-    1,
-  );
+  let version = object_property_increment(object_data, version_property);
   let existing = await firebase_download(storage_path);
   let version_previous;
   if (existing !== null && object_property_exists(existing, version_property)) {
