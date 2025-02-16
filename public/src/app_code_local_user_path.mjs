1600daf2e10705d34cefde31e1fedafc7a8c54c4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 11:28:24 -0500

    2025-02-16T16:28:24.188Z watch   app_code_local_user_path

--- public/src/app_code_local_user_path.mjs
+++ public/src/app_code_local_user_path.mjs
@@ -1,5 +1,6 @@
 import { git_repo_name } from "./git_repo_name.mjs";
 import { path_join } from "./path_join.mjs";
 export function app_code_local_user_path(batch_name) {
-  return path_join(["code", "local", git_repo_name(), batch_name]);
+  let v = path_join(["code", "local", git_repo_name(), batch_name]);
+  return v;
 }
