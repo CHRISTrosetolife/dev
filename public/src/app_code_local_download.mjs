67ca1cc47798f029766246dd77cf4016271cce5c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 13:04:46 -0500

    2025-02-16T18:04:46.211Z watch   app_code_local_download

--- public/src/app_code_local_download.mjs
+++ public/src/app_code_local_download.mjs
@@ -13,16 +13,6 @@ export async function app_code_local_download(username) {
   let batch_path = object_property_get(latest_object, "batch_path");
   let batch = await current_get(batch_path);
   let batches = await list_linked_traverse(batch, next_get, next_exists);
-  async function list_linked_traverse(first, next_get, next_exists) {
-    let current = first;
-    let v3 = await list_adder_async(async function (la) {
-      while (next_exists(current)) {
-        current = await next_get(current);
-        la(current);
-      }
-    });
-    return v3;
-  }
   async function current_get(batch_path) {
     let file_path_batch = app_code_local_user_path(username, batch_path);
     let batch = await storage_file_download(file_path_batch);
@@ -36,5 +26,15 @@ export async function app_code_local_download(username) {
     let batch_path = object_property_get(batch, "batch_path_previous");
     let v2 = await current_get(batch_path);
     return v2;
+  }
+  async function list_linked_traverse(first, next_get, next_exists) {
+    let current = first;
+    let v3 = await list_adder_async(async function (la) {
+      while (next_exists(current)) {
+        current = await next_get(current);
+        la(current);
+      }
+    });
+    return v3;
   }
 }
