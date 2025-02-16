b7d9e52a8328138b0a576c42f0882938b759afd0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 12:48:52 -0500

    2025-02-16T17:48:51.610Z watch   file_js_unparse

--- public/src/file_js_unparse.mjs
+++ public/src/file_js_unparse.mjs
@@ -1,5 +1,11 @@
+import { todo } from "./todo.mjs";
 import { file_js_unparse_code } from "./file_js_unparse_code.mjs";
 export async function file_js_unparse(file_name, ast) {
+  todo(
+    "maybe this function should not exist to modify ast's and all code should use ",
+    file_js_unparse_code,
+    " so that file operations are only when there are changes",
+  );
   let before = null;
   let v = await file_js_unparse_code(ast, file_name, before);
   return v;
