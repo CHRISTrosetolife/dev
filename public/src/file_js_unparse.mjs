470fe8e0650b92a290064d613fd414d45b575167
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 12:42:45 -0500

    2025-02-16T17:42:44.074Z pm file_js_unparse_code 2 1

--- public/src/file_js_unparse.mjs
+++ public/src/file_js_unparse.mjs
@@ -1,6 +1,6 @@
 import { file_js_unparse_code } from "./file_js_unparse_code.mjs";
 export async function file_js_unparse(file_name, ast) {
   let before = null;
-  let v = await file_js_unparse_code(ast, before, file_name);
+  let v = await file_js_unparse_code(ast, file_name, before);
   return v;
 }
--- public/src/file_js_unparse_code.mjs
+++ public/src/file_js_unparse_code.mjs
@@ -2,7 +2,7 @@ import { js_data } from "./js_data.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
 import { js_code_format } from "./js_code_format.mjs";
 import { js_unparse } from "./js_unparse.mjs";
-export async function file_js_unparse_code(ast, before, file_name) {
+export async function file_js_unparse_code(ast, file_name, before) {
   let code = js_unparse(ast);
   let result;
   if (code === before) {
