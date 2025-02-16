fe6acea7e9ca2db3e44adfab555c1838c391c3fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 15:08:18 -0500

    2025-02-16T20:08:16.484Z watch   date_string_iso_file

--- public/src/date_string_iso.mjs
+++ public/src/date_string_iso.mjs
@@ -1,7 +1,7 @@
-import { date_string_iso_to } from "./date_string_iso_to.mjs";
+import { date_to_string_iso } from "./date_to_string_iso.mjs";
 import { date_now } from "./date_now.mjs";
 export function date_string_iso() {
   let d = date_now();
-  let v = date_string_iso_to(d);
+  let v = date_to_string_iso(d);
   return v;
 }
--- public/src/date_string_iso_file.mjs
+++ public/src/date_string_iso_file.mjs
@@ -1,9 +1,7 @@
-import { string_replace } from "./string_replace.mjs";
-import { string_replace_multiple } from "./string_replace_multiple.mjs";
+import { date_string_iso_to_file } from "./date_string_iso_to_file.mjs";
 import { date_string_iso } from "./date_string_iso.mjs";
 export function date_string_iso_file() {
   let d = date_string_iso();
-  d = string_replace_multiple(d, [":", "."], "_");
-  d = string_replace(d, "Z", "");
+  d = date_string_iso_to_file(d);
   return d;
 }
