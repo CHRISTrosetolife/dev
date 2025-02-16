d1d4f890692989586f5149493c47e3e51e361eff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 12:57:46 -0500

    2025-02-16T17:57:46.251Z watch   app_code_local_download

--- public/src/app_code_local_download.mjs
+++ public/src/app_code_local_download.mjs
@@ -13,12 +13,24 @@ export async function app_code_local_download(username) {
   let batch_path = object_property_get(latest_object, "batch_path");
   let batches = [];
   while (true) {
-    let file_path_batch = app_code_local_user_path(username, batch_path);
-    let batch = await storage_file_download(file_path_batch);
+    let batch = await current_get(batch_path);
     list_add(batches, batch);
-    if (object_property_exists_not(batch, "batch_path_previous")) {
+    if (next_exists(batch)) {
       break;
     }
-    batch_path = object_property_get(batch, "batch_path_previous");
+    batch_path = next_get(batch);
+  }
+  async function current_get(batch_path) {
+    let file_path_batch = app_code_local_user_path(username, batch_path);
+    let batch = await storage_file_download(file_path_batch);
+    return batch;
+  }
+  function next_exists(batch) {
+    let v = object_property_exists_not(batch, "batch_path_previous");
+    return v;
+  }
+  function next_get(batch) {
+    let v2 = object_property_get(batch, "batch_path_previous");
+    return v2;
   }
 }
