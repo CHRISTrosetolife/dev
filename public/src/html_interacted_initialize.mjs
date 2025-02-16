567ca05f5aef1d3b838cc006715a05efc3a450d4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 10:06:25 -0500

    2025-02-16T15:06:24.099Z watch   html_interacted_initialize

--- public/src/html_interacted_initialize.mjs
+++ public/src/html_interacted_initialize.mjs
@@ -1,4 +1,3 @@
-import { html_on_remove } from "./html_on_remove.mjs";
 import { html_on_keydown } from "./html_on_keydown.mjs";
 import { each } from "./each.mjs";
 import { object_property_set } from "./object_property_set.mjs";
@@ -7,17 +6,20 @@ import { object_property_initialize } from "./object_property_initialize.mjs";
 import { global_function } from "./global_function.mjs";
 import { html_interacted } from "./html_interacted.mjs";
 import { html_on_click } from "./html_on_click.mjs";
+import { list_map } from "./list_map.mjs";
 export function html_interacted_initialize() {
   let g = global_function(html_interacted);
   object_property_initialize(g, "interacted", false);
   let start = true;
-  each([html_on_keydown, html_on_click], function (on) {
+  let mapped = list_map([html_on_keydown, html_on_click], function (on) {
     let result = on(html_document_get(), function () {
       if (start) {
         object_property_set(g, "interacted", true);
         start = false;
-        html_on_remove(result);
+        each(list, function (item) {});
       }
     });
+    let v = result;
+    return v;
   });
 }
