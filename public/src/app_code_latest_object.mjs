3952829db1f514a49efeaf9dbc2836814dbb502f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 15:10:00 -0500

    2025-02-16T20:09:59.491Z watch   app_code_latest_object

--- public/src/app_code_latest_object.mjs
+++ public/src/app_code_latest_object.mjs
@@ -1,7 +1,9 @@
+import { date_to_string_iso } from "./date_to_string_iso.mjs";
 export function app_code_latest_object(batch_path, when) {
+  let iso = date_to_string_iso(when);
   let v = {
     batch_path,
-    when,
+    when: iso,
   };
   return v;
 }
