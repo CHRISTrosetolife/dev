0903f163acc15b111218dc4e816e50d31cdb05e0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 10:01:08 -0500

    2025-02-16T15:01:06.443Z watch   html_on

--- public/src/html_on.mjs
+++ public/src/html_on.mjs
@@ -2,4 +2,8 @@ import { html_event_listener } from "./html_event_listener.mjs";
 export function html_on(component, type, lambda) {
   let { element } = component;
   html_event_listener(element, type, lambda);
+  let v = {
+    lambda,
+  };
+  return v;
 }
