f0f4f214862aa32cb219bed27d3a94db66978e7e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 09:26:15 -0500

    2025-02-16T14:26:15.009Z watch   app_code_files_get

--- public/src/app_code_files_get.mjs
+++ public/src/app_code_files_get.mjs
@@ -1,24 +1,20 @@
 import { global_files } from "./global_files.mjs";
 import { object_property_initialize_get_async } from "./object_property_initialize_get_async.mjs";
 import { global_get } from "./global_get.mjs";
-import { html_load } from "./html_load.mjs";
 import { app_code_data_get } from "./app_code_data_get.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function app_code_files_get() {
-  let files3 = await html_load(async function () {
-    let g = global_get();
-    let files = await object_property_initialize_get_async(
-      g,
-      global_files(),
-      async function () {
-        let data = await app_code_data_get();
-        let batch2 = object_property_get(data, "batch");
-        let files2 = object_property_get(batch2, "files");
-        let v2 = files2;
-        return v2;
-      },
-    );
-    return files;
-  });
-  return files3;
+  let g = global_get();
+  let files = await object_property_initialize_get_async(
+    g,
+    global_files(),
+    async function () {
+      let data = await app_code_data_get();
+      let batch2 = object_property_get(data, "batch");
+      let files2 = object_property_get(batch2, "files");
+      let v2 = files2;
+      return v2;
+    },
+  );
+  return files;
 }
