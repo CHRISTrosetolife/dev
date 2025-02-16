b8c31a973d19e628bcda73514157d9e1d249433e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 09:24:23 -0500

    2025-02-16T14:24:22.860Z watch   app_code_files_get

--- public/src/app_code_files_get.mjs
+++ public/src/app_code_files_get.mjs
@@ -1,31 +1,7 @@
-import { app_code_local_files_path_latest } from "./app_code_local_files_path_latest.mjs";
-import { http_storage_get } from "./http_storage_get.mjs";
-import { object_property_initialize_get_async } from "./object_property_initialize_get_async.mjs";
-import { html_load } from "./html_load.mjs";
 import { app_code_data_get } from "./app_code_data_get.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { global_function } from "./global_function.mjs";
 export async function app_code_files_get() {
   let data = await app_code_data_get();
-  let v = await html_load(async function () {
-    let g = global_function(app_code_data_get);
-    let data = await object_property_initialize_get_async(
-      g,
-      "data",
-      async function () {
-        let latest = await http_storage_get(app_code_local_files_path_latest());
-        let batch_path = object_property_get(latest, "batch_path");
-        let batch = await http_storage_get(batch_path);
-        let data = {
-          batch,
-          batch_path,
-        };
-        return data;
-      },
-    );
-    return data;
-  });
-  return v;
   let batch2 = object_property_get(data, "batch");
   let files2 = object_property_get(batch2, "files");
   return files2;
