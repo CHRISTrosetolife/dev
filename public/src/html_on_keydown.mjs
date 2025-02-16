9b20241a854d3d5d81040e5523e0e2de2809620e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 10:01:44 -0500

    2025-02-16T15:01:44.095Z watch   html_on_keydown

--- public/src/html_on_keydown.mjs
+++ public/src/html_on_keydown.mjs
@@ -1,4 +1,5 @@
 import { html_on } from "./html_on.mjs";
 export function html_on_keydown(component, on_keydown) {
-  html_on(component, "keydown", on_keydown);
+  let v = html_on(component, "keydown", on_keydown);
+  return v;
 }
