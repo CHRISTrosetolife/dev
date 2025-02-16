0d3f145b3d0323fbf84206f0a988ef0550a2cfd2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 11:30:59 -0500

    2025-02-16T16:30:57.199Z watch   app_code_refresh_menu

--- public/src/app_code_refresh_menu.mjs
+++ public/src/app_code_refresh_menu.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { app_code_refresh_app_new } from "./app_code_refresh_app_new.mjs";
 import { app_code_button_html_files } from "./app_code_button_html_files.mjs";
 import { html_load } from "./html_load.mjs";
@@ -12,16 +13,15 @@ import { app_code_username_get } from "./app_code_username_get.mjs";
 import { app_code_username_exists } from "./app_code_username_exists.mjs";
 import { app_code_refresh_main } from "./app_code_refresh_main.mjs";
 import { storage_local_remove } from "./storage_local_remove.mjs";
-import { app_code } from "./app_code.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { app_code_refresh_username } from "./app_code_refresh_username.mjs";
 export async function app_code_refresh_menu(context) {
-  await html_load(async () => {
+  await html_load(async function () {
     let root = app_code_screen_set(context, fn_name("app_code_refresh_menu"));
     app_code_button_functions_search(context);
     let functions_recent = app_code_functions_recent_get(context);
     if (list_empty_not_is(functions_recent)) {
-      html_button(root, "Recent functions", () => {
+      html_button(root, "Recent functions", function () {
         app_code_refresh_functions_recent(context);
       });
     }
@@ -29,19 +29,20 @@ export async function app_code_refresh_menu(context) {
       html_button(
         root,
         string_combine_multiple(["Log out: ", app_code_username_get()]),
-        () => {
-          storage_local_remove(app_code, "username");
+        function () {
+          let app_fn = object_property_get(context, "app_fn");
+          storage_local_remove(app_fn, "username");
           app_code_refresh_main(context);
         },
       );
     } else {
-      html_button(root, "Log in", () => {
+      html_button(root, "Log in", function () {
         app_code_refresh_username(context);
       });
     }
     await app_code_button_html_files(context);
-    html_button(root, "New app", () => {
-      app_code_refresh_app_new(context);
+    html_button(root, "New app", async function () {
+      await app_code_refresh_app_new(context);
     });
   });
 }
