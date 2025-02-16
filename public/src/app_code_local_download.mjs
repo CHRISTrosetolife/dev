6fd01f0da34feb00081c90b9c82f908e3d7f8b60
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-16 12:29:46 -0500

    2025-02-16T17:29:45.095Z watch   app_code_local_download

--- public/src/app_code_local_download.mjs
+++ public/src/app_code_local_download.mjs
@@ -1,4 +1,6 @@
+import { app_code_file_name_latest } from "./app_code_file_name_latest.mjs";
 import { app_code_local_user_path } from "./app_code_local_user_path.mjs";
 export function app_code_local_download(username) {
+  app_code_file_name_latest();
   app_code_local_user_path(username);
 }
