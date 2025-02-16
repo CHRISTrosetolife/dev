1db1c7f590ec706335a89e619a2a0856db5c344b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-15 21:36:50 -0500

    2025-02-16T02:36:48.404Z watch   assert_message

--- public/src/assert_message.mjs
+++ public/src/assert_message.mjs
@@ -5,8 +5,8 @@ export function assert_message(fn, args, message_get) {
   let v = assert_message_string(fn, args, function () {
     let j = json_to(message_get());
     let limit = 1000;
-    j = string_take_soft(j, limit);
-    return j;
+    let v2 = string_take_soft(j, limit);
+    return v2;
   });
   return v;
 }
