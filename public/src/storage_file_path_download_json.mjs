5738ecdd760a97f11563e4f66cbf18ee901f89ba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 18:28:40 -0500

    2025-02-17T23:28:36.927Z r storage_file_path_download storage_file_path_download_json

--- public/src/storage_file_path_download_json.mjs
+++ public/src/storage_file_path_download_json.mjs
@@ -3,7 +3,7 @@ import { storage_file_download } from "./storage_file_download.mjs";
 import { storage_file } from "./storage_file.mjs";
 import { file_read } from "./file_read.mjs";
 import { json_from } from "./json_from.mjs";
-export async function storage_file_path_download(latest_path_user) {
+export async function storage_file_path_download_json(latest_path_user) {
   let f = await storage_file(latest_path_user);
   await storage_file_download(f);
   let destination = storage_file_download_path(f);
