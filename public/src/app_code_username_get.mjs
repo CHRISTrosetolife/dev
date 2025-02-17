c562055679ddff6879cf608ba7891e043110783e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 18:21:29 -0500

    2025-02-17T23:21:26.772Z watch   app_code_username_get

--- public/src/app_code_username_get.mjs
+++ public/src/app_code_username_get.mjs
@@ -3,5 +3,6 @@ import { storage_local_get } from "./storage_local_get.mjs";
 export function app_code_username_get(context) {
   let app_fn = object_property_get(context, "app_fn");
   let v = storage_local_get(app_fn, "username");
+  let username = object_property_get(v, "username");
   return v;
 }
