8422d364022edd141cb1f642860e0acd60337d7e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 18:29:38 -0500

    2025-02-17T23:29:37.115Z watch   storage_file_path_download_json

---
+++ public/src/storage_file_path_download.mjs
@@ -0,0 +1,11 @@
+import { file_read } from "./file_read.mjs";
+import { storage_file_download_path } from "./storage_file_download_path.mjs";
+import { storage_file_download } from "./storage_file_download.mjs";
+import { storage_file } from "./storage_file.mjs";
+export async function storage_file_path_download(latest_path_user) {
+  let f = await storage_file(latest_path_user);
+  await storage_file_download(f);
+  let destination = storage_file_download_path(f);
+  let v = await file_read(destination);
+  return v;
+}
--- public/src/storage_file_path_download_json.mjs
+++ public/src/storage_file_path_download_json.mjs
@@ -1,13 +1,7 @@
-import { storage_file_download_path } from "./storage_file_download_path.mjs";
-import { storage_file_download } from "./storage_file_download.mjs";
-import { storage_file } from "./storage_file.mjs";
-import { file_read } from "./file_read.mjs";
+import { storage_file_path_download } from "./storage_file_path_download.mjs";
 import { json_from } from "./json_from.mjs";
 export async function storage_file_path_download_json(latest_path_user) {
-  let f = await storage_file(latest_path_user);
-  await storage_file_download(f);
-  let destination = storage_file_download_path(f);
-  let v = await file_read(destination);
+  let v = await storage_file_path_download(latest_path_user);
   let p = json_from(v);
   return p;
 }
