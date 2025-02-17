3c77a02711ff6382e0b5254c50bec7b65aa6007e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 18:04:54 -0500

    2025-02-17T23:04:52.634Z watch   storage_local_set

--- public/src/storage_local_set.mjs
+++ public/src/storage_local_set.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { json_to } from "./json_to.mjs";
 import { storage_local_key } from "./storage_local_key.mjs";
 export function storage_local_set(fn_namespace, key, value) {
+  assert_arguments_length(arguments, 3);
   let json = json_to(value);
   localStorage.setItem(storage_local_key(fn_namespace, key), json);
 }
