7dea5a3146c4242bc68699170fbb47292de6fc21
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 15:11:19 -0500

    2025-02-17T20:11:18.369Z watch   file_overwrite

--- public/src/file_overwrite.mjs
+++ public/src/file_overwrite.mjs
@@ -1,7 +1,8 @@
 import { string_is } from "./string_is.mjs";
 import { assert } from "./assert.mjs";
 import { file_overwrite_generic } from "./file_overwrite_generic.mjs";
-export async function file_overwrite(file_name, data) {
-  assert(string_is, [data]);
-  return await file_overwrite_generic(file_name, data, "utf-8");
+export async function file_overwrite(file_path, data_string) {
+  assert(string_is, [data_string]);
+  let v = await file_overwrite_generic(file_path, data_string, "utf-8");
+  return v;
 }
