70e8b8da3f2f5db2a6f5664c085b8e67f784c128
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 18:18:35 -0500

    2025-02-17T23:18:30.872Z watch   storage_local_exists

--- public/src/storage_local_get.mjs
+++ public/src/storage_local_get.mjs
@@ -1,7 +1,7 @@
+import { storage_local_get_unparsed } from "./storage_local_get_unparsed.mjs";
 import { json_from } from "./json_from.mjs";
-import { storage_local_key } from "./storage_local_key.mjs";
 export function storage_local_get(fn_namespace, key) {
-  let json = localStorage.getItem(storage_local_key(fn_namespace, key));
+  let json = storage_local_get_unparsed(fn_namespace, key);
   let v = json_from(json);
   return v;
 }
---
+++ public/src/storage_local_get_unparsed.mjs
@@ -0,0 +1,4 @@
+import { storage_local_key } from "./storage_local_key.mjs";
+export function storage_local_get_unparsed(fn_namespace, key) {
+  return localStorage.getItem(storage_local_key(fn_namespace, key));
+}
