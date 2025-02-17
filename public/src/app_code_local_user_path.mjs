5e01fbb9f99ecca9e1d67ae6107be8f3b4327a62
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 18:05:18 -0500

    2025-02-17T23:05:14.577Z watch   app_code_local_user_path

--- public/src/app_code_local_user_path.mjs
+++ public/src/app_code_local_user_path.mjs
@@ -1,5 +1,8 @@
+import { never } from "./never.mjs";
+import { assert } from "./assert.mjs";
 import { path_join } from "./path_join.mjs";
 export function app_code_local_user_path(username, file_name) {
+  assert(never, []);
   let v = path_join(["code", "user", username, file_name]);
   return v;
 }
