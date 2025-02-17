9180722bcccb2cfe7a02a10ba87d71785d5118c3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 17:08:15 -0500

    2025-02-17T22:08:14.643Z watch   app_code_batches

--- public/src/app_code_batches.mjs
+++ public/src/app_code_batches.mjs
@@ -9,17 +9,16 @@ export async function app_code_batches(batch_path, username) {
   let batches = await list_linked_traverse(
     batch,
     previous_get,
-    previous_exists,
+    function previous_exists(batch) {
+      let v = app_code_batch_previous_exists(batch);
+      return v;
+    },
   );
   list_reverse(batches);
   async function current_get(batch_path) {
     let file_path_batch = app_code_local_user_path(username, batch_path);
     let batch = await storage_file_download(file_path_batch);
     return batch;
-  }
-  function previous_exists(batch) {
-    let v = app_code_batch_previous_exists(batch);
-    return v;
   }
   async function previous_get(batch) {
     let batch_path = object_property_get(batch, "batch_path_previous");
