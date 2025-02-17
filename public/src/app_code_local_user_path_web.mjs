4c3a61d58645d1d902cf8d4a5c7f3b68dc55f985
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 18:22:54 -0500

    2025-02-17T23:22:53.601Z watch   app_code_local_user_path_web

--- public/src/app_code_local_user_path_web.mjs
+++ public/src/app_code_local_user_path_web.mjs
@@ -1,9 +1,9 @@
-import { storage_local_get } from "./storage_local_get.mjs";
+import { app_code_username_get } from "./app_code_username_get.mjs";
 import { app_code_local_user_path } from "./app_code_local_user_path.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function app_code_local_user_path_web(context, file_name) {
   let app_fn = object_property_get(context, "app_fn");
-  let { username } = storage_local_get(app_fn, "username");
+  let { username } = app_code_username_get(app_fn, "username");
   let v = app_code_local_user_path(username, file_name);
   return v;
 }
