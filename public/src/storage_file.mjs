a7d6530cae6ee616b9591bb1a8fb75f30c8230e5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 18:00:50 -0500

    2025-02-17T23:00:44.752Z watch   storage_file

--- public/src/storage_file.mjs
+++ public/src/storage_file.mjs
@@ -1,6 +1,6 @@
 import { storage_bucket_prefix } from "./storage_bucket_prefix.mjs";
-export async function storage_file(prefix) {
-  let bucket = await storage_bucket_prefix(prefix);
-  let data = bucket.file(prefix);
+export async function storage_file(file_path) {
+  let bucket = await storage_bucket_prefix(file_path);
+  let data = bucket.file(file_path);
   return data;
 }
