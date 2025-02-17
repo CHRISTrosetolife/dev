c229fd0919678af4563dbaceaea158b4c2e2a6c3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 15:24:11 -0500

    2025-02-17T20:24:08.721Z watch   each_object_unordered_async

--- public/src/each_object_unordered_async.mjs
+++ public/src/each_object_unordered_async.mjs
@@ -1,18 +1,7 @@
-import { log } from "./log.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { object_properties } from "./object_properties.mjs";
-import { list_index } from "./list_index.mjs";
-import { list_size } from "./list_size.mjs";
+import { list_map_unordered } from "./list_map_unordered.mjs";
 export async function each_object_unordered_async(object, lambda) {
   let keys = object_properties(object);
-  for (let key of keys) {
-    if (false) {
-      log({
-        i: list_index(keys, key),
-        s: list_size(keys),
-      });
-    }
-    let value = object_property_get(object, key);
-    await lambda(key, value);
-  }
+  let v = await list_map_unordered(keys, $A$KEY);
+  return v;
 }
