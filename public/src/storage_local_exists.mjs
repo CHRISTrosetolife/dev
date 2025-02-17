57cf07071fb08704ed841f426d401fa97763516d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 18:14:46 -0500

    2025-02-17T23:14:41.963Z watch   storage_local_exists

--- public/src/storage_local_exists.mjs
+++ public/src/storage_local_exists.mjs
@@ -1,4 +1,5 @@
-import { storage_local_get_checkless } from "./storage_local_get_checkless.mjs";
+import { storage_local_get } from "./storage_local_get.mjs";
 export function storage_local_exists(fn_namespace, key) {
-  return storage_local_get_checkless(fn_namespace, key) !== null;
+  let v = storage_local_get(fn_namespace, key) !== null;
+  return v;
 }
--- public/src/storage_local_get.mjs
+++ public/src/storage_local_get.mjs
@@ -1,4 +1,4 @@
 import { storage_local_get_checkless } from "./storage_local_get_checkless.mjs";
-export function storage_local_get(fn_namespace, key) {
+export function storage_local_get(fn_namespace, key) {$ex
   return storage_local_get_checkless(fn_namespace, key);
 }
