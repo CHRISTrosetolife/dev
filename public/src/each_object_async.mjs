f446c2b7f1e38212f909c3b9616bb42856557551
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 15:27:13 -0500

    2025-02-17T20:27:08.260Z watch   each_object_async

--- public/src/each_object_async.mjs
+++ public/src/each_object_async.mjs
@@ -1,11 +1,6 @@
-import { object_property_get } from "./object_property_get.mjs";
+import { each_object_generic_async } from "./each_object_generic_async.mjs";
 import { each_async } from "./each_async.mjs";
-import { object_properties } from "./object_properties.mjs";
 export async function each_object_async(object, lambda_each) {
   let lambda_fn = each_async;
-  let keys = object_properties(object);
-  await lambda_fn(keys, async function (key) {
-    let value = object_property_get(object, key);
-    await lambda_each(key, value);
-  });
+  await each_object_generic_async(object, lambda_fn, lambda_each);
 }
---
+++ public/src/each_object_generic_async.mjs
@@ -0,0 +1,13 @@
+import { object_property_get } from "./object_property_get.mjs";
+import { object_properties } from "./object_properties.mjs";
+export async function each_object_generic_async(
+  object,
+  lambda_fn,
+  lambda_each,
+) {
+  let keys = object_properties(object);
+  await lambda_fn(keys, async function (key) {
+    let value = object_property_get(object, key);
+    await lambda_each(key, value);
+  });
+}
