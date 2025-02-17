c933d067c908517cb586ae8cebebf40d03710eab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 18:00:18 -0500

    2025-02-17T23:00:17.485Z watch   storage_file

--- public/src/storage_file.mjs
+++ public/src/storage_file.mjs
@@ -1,12 +1,6 @@
 import { storage_bucket_prefix } from "./storage_bucket_prefix.mjs";
-import { list_sort_string_map_property } from "./list_sort_string_map_property.mjs";
-import { list_single } from "./list_single.mjs";
 export async function storage_file(prefix) {
   let bucket = await storage_bucket_prefix(prefix);
-  let data = await bucket.getFiles({
-    prefix,
-  });
-  let files = list_single(data);
-  list_sort_string_map_property(files, "name");
-  return files;
+  let data = bucket.file(prefix);
+  return data;
 }
