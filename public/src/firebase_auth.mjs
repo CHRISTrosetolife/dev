198f0857db5feac54312a0cd3e6971cda383b971
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 15:34:37 -0500

    2025-02-17T20:34:32.263Z watch   firebase_auth

--- public/src/firebase_auth.mjs
+++ public/src/firebase_auth.mjs
@@ -1,6 +1,7 @@
-import { global_get } from "./global_get.mjs";
+import { firebase_initialize } from "./firebase_initialize.mjs";
+import { global_function } from "./global_function.mjs";
 export function firebase_auth() {
-  let { firebase } = global_get();
-  let { auth } = firebase;
+  let g = global_function(firebase_initialize);
+  let { auth } = g;
   return auth;
 }
