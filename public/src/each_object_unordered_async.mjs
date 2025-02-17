bc4d507d317a68b96ed447e0779c05a8d8f4a8b4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 15:27:42 -0500

    2025-02-17T20:27:40.745Z watch   each_object_unordered_async

--- public/src/each_object_unordered_async.mjs
+++ public/src/each_object_unordered_async.mjs
@@ -1,9 +1,8 @@
-import { each_async } from "./each_async.mjs";
 import { each_object_async_lambda } from "./each_object_async_lambda.mjs";
 import { object_properties } from "./object_properties.mjs";
 import { list_map_unordered } from "./list_map_unordered.mjs";
 export async function each_object_unordered_async(object, lambda) {
-  let lambda_fn = each_async;
+  let lambda_fn = list_map_unordered;
   await list_map_unordered(object, lambda_fn, lambda_each);
   let keys = object_properties(object);
   let v = await list_map_unordered(keys, async function (key) {
