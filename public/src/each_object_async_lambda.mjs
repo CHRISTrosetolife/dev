8d8f49d63b45f612ac5c6cf452cae09a6566aefb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 15:28:09 -0500

    2025-02-17T20:28:08.898Z de each_object_async_lambda

--- public/src/each_object_async_lambda.mjs
+++
@@ -1,5 +0,0 @@
-import { object_property_get } from "./object_property_get.mjs";
-export async function each_object_async_lambda(object, key, lambda) {
-  let value = object_property_get(object, key);
-  await lambda(key, value);
-}
