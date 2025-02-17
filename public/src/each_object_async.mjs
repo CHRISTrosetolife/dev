ea3c156da3486b628704c4bc7f7672b8187ec32a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 15:25:19 -0500

    2025-02-17T20:25:14.792Z watch   each_object_async

--- public/src/each_object_async.mjs
+++ public/src/each_object_async.mjs
@@ -1,3 +1,4 @@
+import { each_async } from "./each_async.mjs";
 import { each_object_async_lambda } from "./each_object_async_lambda.mjs";
 import { log } from "./log.mjs";
 import { object_properties } from "./object_properties.mjs";
@@ -5,6 +6,7 @@ import { list_index } from "./list_index.mjs";
 import { list_size } from "./list_size.mjs";
 export async function each_object_async(object, lambda) {
   let keys = object_properties(object);
+  await each_async(list, async function (item) {});
   for (let key of keys) {
     if (false) {
       log({
