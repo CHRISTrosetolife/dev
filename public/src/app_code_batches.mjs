c1f7f72da7028ceadba40e18535a920b767b2bfd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 17:07:40 -0500

    2025-02-17T22:07:39.056Z watch   app_code_batches

--- public/src/app_code_batches.mjs
+++ public/src/app_code_batches.mjs
@@ -6,18 +6,22 @@ import { list_reverse } from "./list_reverse.mjs";
 import { list_linked_traverse } from "./list_linked_traverse.mjs";
 export async function app_code_batches(batch_path, username) {
   let batch = await current_get(batch_path);
-  let batches = await list_linked_traverse(batch, next_get, next_exists);
+  let batches = await list_linked_traverse(
+    batch,
+    previous_get,
+    previous_exists,
+  );
   list_reverse(batches);
   async function current_get(batch_path) {
     let file_path_batch = app_code_local_user_path(username, batch_path);
     let batch = await storage_file_download(file_path_batch);
     return batch;
   }
-  function next_exists(batch) {
+  function previous_exists(batch) {
     let v = object_property_exists(batch, "batch_path_previous");
     return v;
   }
-  async function next_get(batch) {
+  async function previous_get(batch) {
     let batch_path = object_property_get(batch, "batch_path_previous");
     let v2 = await current_get(batch_path);
     return v2;
