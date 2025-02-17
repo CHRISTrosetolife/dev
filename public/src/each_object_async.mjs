2fa44af8ce83a194929fb0813160b38e74206ecd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 15:26:31 -0500

    2025-02-17T20:26:29.495Z watch   each_object_async

--- public/src/each_object_async.mjs
+++ public/src/each_object_async.mjs
@@ -2,8 +2,9 @@ import { each_async } from "./each_async.mjs";
 import { each_object_async_lambda } from "./each_object_async_lambda.mjs";
 import { object_properties } from "./object_properties.mjs";
 export async function each_object_async(object, lambda_each) {
+  let lambda_fn = each_async;
   let keys = object_properties(object);
-  await each_async(keys, async function (key) {
+  await lambda_fn(keys, async function (key) {$ex
     await each_object_async_lambda(object, key, lambda_each);
   });
 }
