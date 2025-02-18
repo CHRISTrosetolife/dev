68f5685b9908f9090f8f912e3c54663b6928bf34
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 16:18:28 -0500

    2025-02-18T21:18:27.806Z watch   function_aliases_add

--- public/src/function_aliases_add.mjs
+++ public/src/function_aliases_add.mjs
@@ -1,3 +1,4 @@
+import { object_property_exists } from "./object_property_exists.mjs";
 import { assert } from "./assert.mjs";
 import { function_aliases } from "./function_aliases.mjs";
 import { js_code_call_args } from "./js_code_call_args.mjs";
@@ -7,18 +8,16 @@ import { app_list_add } from "./app_list_add.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 import { string_delimit } from "./string_delimit.mjs";
-import { object_property_exists_not } from "./object_property_exists_not.mjs";
-import { list_all } from "./list_all.mjs";
+import { list_filter } from "./list_filter.mjs";
+import { list_empty_is } from "./list_empty_is.mjs";
 export async function function_aliases_add(aliases, function_name) {
   let existing = function_aliases();
   let split = string_split_comma(aliases);
-  assert(list_all, [
-    split,
-    function (s) {
-      let v = object_property_exists_not(existing, s);
-      return v;
-    },
-  ]);
+  let duplicates = list_filter(split, function (s) {
+    let v = object_property_exists(existing, s);
+    return v;
+  });
+  assert(list_empty_is, [duplicates]);
   let list_code = js_code_array_string(split);
   await app_list_add(function_aliases, function inserted_code() {
     let c = js_code_statement_call_args(fn_name("function_aliases_register"), [
