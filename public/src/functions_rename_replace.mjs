00cff45376c1f6c443e56bd06e692313e63407d8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 10:27:31 -0500

    2025-02-18T15:27:31.208Z ft js_watch_lockify functions_rename_replace

--- public/src/functions_rename_replace.mjs
+++ public/src/functions_rename_replace.mjs
@@ -6,13 +6,15 @@ export async function functions_rename_replace(
   replacement_before,
   replacement_after,
 ) {
-  assert_arguments_length(arguments, 2);
-  await functions_rename_generic(fn_new_get);
-  function fn_new_get(fn) {
-    let fn_new = fn;
-    if (string_includes(fn, replacement_before)) {
-      fn_new = string_replace(fn, replacement_before, replacement_after);
+  return await watch_lock(async function () {
+    assert_arguments_length(arguments, 2);
+    await functions_rename_generic(fn_new_get);
+    function fn_new_get(fn) {
+      let fn_new = fn;
+      if (string_includes(fn, replacement_before)) {
+        fn_new = string_replace(fn, replacement_before, replacement_after);
+      }
+      return fn_new;
     }
-    return fn_new;
-  }
+  });
 }
