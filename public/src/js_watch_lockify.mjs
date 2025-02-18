48ea3e5987d28b3d8861ae559d15211556603198
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 10:30:41 -0500

    2025-02-18T15:30:39.974Z watch   js_watch_lockify

--- public/src/js_watch_lockify.mjs
+++ public/src/js_watch_lockify.mjs
@@ -1,3 +1,4 @@
+import { js_imports_add_specified } from "./js_imports_add_specified.mjs";
 import { js_body_body_get } from "./js_body_body_get.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { js_code_function_declare } from "./js_code_function_declare.mjs";
@@ -16,8 +17,10 @@ export function js_watch_lockify(ast) {
   let b2 = js_body_body_get(f);
   list_add_multiple(b2, b);
   list_clear(b);
-  let e = js_expression_call_args(fn_name("watch_lock"), [f]);
+  let w = fn_name("watch_lock");
+  let e = js_expression_call_args(w, [f]);
   let argument = js_expression_await(e);
   let r = js_statement_return(argument);
   list_add(b, r);
+  js_imports_add_specified(ast, [w]);
 }
