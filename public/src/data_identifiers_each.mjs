9a1bf6b1878feded8ca307c264b7451d559d3fb6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 14:39:22 -0500

    2025-02-18T19:39:22.261Z watch   data_identifiers_each

--- public/src/data_identifiers_each.mjs
+++ public/src/data_identifiers_each.mjs
@@ -4,7 +4,5 @@ import { data_identifiers } from "./data_identifiers.mjs";
 export async function data_identifiers_each(function_name, lambda) {
   let dis = await data_identifiers();
   let fns = object_property_get(dis, function_name);
-  await each_async(fns, async function (f_name) {
-    await lambda(f_name);
-  });
+  await each_async(fns, lambda);
 }
