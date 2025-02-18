86b475bd8a1f34f1e4d8891d52a7f3752a6d9f4a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 14:11:54 -0500

    2025-02-18T19:11:54.166Z watch   function_params_objectify

--- public/src/function_params_objectify.mjs
+++ public/src/function_params_objectify.mjs
@@ -1,3 +1,4 @@
+import { js_code_braces_inside } from "./js_code_braces_inside.mjs";
 import { js_identifiers_duplicates } from "./js_identifiers_duplicates.mjs";
 import { js_declaration_to_body } from "./js_declaration_to_body.mjs";
 import { js_identifiers_to_names } from "./js_identifiers_to_names.mjs";
@@ -9,6 +10,7 @@ import { noop } from "./noop.mjs";
 import { list_intersect } from "./list_intersect.mjs";
 import { assert } from "./assert.mjs";
 import { list_empty_is } from "./list_empty_is.mjs";
+import { list_join_comma_space } from "./list_join_comma_space.mjs";
 export async function function_params_objectify(function_name) {
   assert_arguments_length(arguments, 1);
   await data_identifiers_each(function_name, async function lambda(f_name) {
@@ -23,6 +25,7 @@ export async function function_params_objectify(function_name) {
           let i = list_intersect(params_names, duplicates);
           assert(list_empty_is, [i]);
           let body = js_declaration_to_body(declaration);
+          js_code_braces_inside(list_join_comma_space(params_names));
         },
       );
     });
