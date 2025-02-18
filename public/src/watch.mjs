98fb3729d83bb4497574363ce9817bfee037f770
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 21:49:05 -0500

    2025-02-18T02:49:03.671Z watch   watch

--- public/src/watch.mjs
+++ public/src/watch.mjs
@@ -1,3 +1,4 @@
+import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
 import { folder_read_shallow } from "./folder_read_shallow.mjs";
 import { sermon_path_to_name } from "./sermon_path_to_name.mjs";
 import { function_auto_after_path } from "./function_auto_after_path.mjs";
@@ -44,7 +45,7 @@ export async function watch() {
     if (event !== "change") {
       return;
     }
-    await folder_read_shallow();
+    await folder_read_shallow(folder_gitignore_path(), "");
     path = string_replace(path, "\\", "/");
     path = folder_current_prefix_combine(path);
     object_property_initialize(cache, path, {});
