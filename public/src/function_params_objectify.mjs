f7407a16636405d505b9eaeac9d1f54c4c5a945d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 15:03:32 -0500

    2025-02-18T20:03:31.859Z watch   function_params_objectify

--- public/src/function_params_objectify.mjs
+++ public/src/function_params_objectify.mjs
@@ -1,3 +1,4 @@
+import { list_map_index } from "./list_map_index.mjs";
 import { noop } from "./noop.mjs";
 import { data_identifiers_each_transform_params } from "./data_identifiers_each_transform_params.mjs";
 import { list_add_first } from "./list_add_first.mjs";
@@ -12,6 +13,7 @@ import { assert } from "./assert.mjs";
 import { list_empty_is } from "./list_empty_is.mjs";
 import { js_parse_first } from "./js_parse_first.mjs";
 import { list_is } from "./list_is.mjs";
+import { list_size } from "./list_size.mjs";
 export async function function_params_objectify(function_name) {
   assert_arguments_length(arguments, 1);
   let params_names;
@@ -34,6 +36,8 @@ export async function function_params_objectify(function_name) {
   await data_identifiers_each_transform_params(
     function_name,
     function on_call(params, declaration, ast) {
+      assert(list_size_is, [params, list_size(params_names)]);
+      list_map_index();
       let params_names = js_identifiers_to_names(params);
       let duplicates = js_identifiers_duplicates(ast);
       let i = list_intersect(params_names, duplicates);
