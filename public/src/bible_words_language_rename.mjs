af835a3658b49056f4922fd28dcdedec63b50673
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 10:23:07 -0500

    2025-02-18T15:23:04.192Z ft js_watch_lockify bible_words_language_rename

--- public/src/bible_words_language_rename.mjs
+++ public/src/bible_words_language_rename.mjs
@@ -5,14 +5,18 @@ import { fn_name } from "./fn_name.mjs";
 import { function_rename } from "./function_rename.mjs";
 import { string_combine } from "./string_combine.mjs";
 export async function bible_words_language_rename() {
-  let prefix_old = string_trail_underscore(fn_name("ceb_bible_words"));
-  let prefix_new = string_trail_underscore(fn_name("bible_words_language_ceb"));
-  await function_names_prefix_number_each(prefix_old, lambda);
-  async function lambda(without, fn_old) {
-    let fn_new = string_combine(prefix_new, without);
-    log({
-      fn_new,
-    });
-    await function_rename(fn_old, fn_new);
-  }
+  return await watch_lock(async function () {
+    let prefix_old = string_trail_underscore(fn_name("ceb_bible_words"));
+    let prefix_new = string_trail_underscore(
+      fn_name("bible_words_language_ceb"),
+    );
+    await function_names_prefix_number_each(prefix_old, lambda);
+    async function lambda(without, fn_old) {
+      let fn_new = string_combine(prefix_new, without);
+      log({
+        fn_new,
+      });
+      await function_rename(fn_old, fn_new);
+    }
+  });
 }
