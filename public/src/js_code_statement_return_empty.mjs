89f273a681389b0b344176e59086f7fe6dc4ea82
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 09:56:02 -0500

    2025-02-18T14:56:01.356Z watch   js_statement_return

---
+++ public/src/js_code_statement_return_empty.mjs
@@ -0,0 +1,4 @@
+import { js_code_statement_return } from "./js_code_statement_return.mjs";
+export function js_code_statement_return_empty() {
+  return js_code_statement_return("");
+}
--- public/src/js_statement_return.mjs
+++ public/src/js_statement_return.mjs
@@ -1,8 +1,8 @@
+import { js_code_statement_return_empty } from "./js_code_statement_return_empty.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { js_parse_first_function } from "./js_parse_first_function.mjs";
-import { js_code_statement_return } from "./js_code_statement_return.mjs";
 export function js_statement_return(argument) {
-  let r_code = js_code_statement_return("");
+  let r_code = js_code_statement_return_empty();
   let r = js_parse_first_function(r_code);
   object_property_set(r, "argument", argument);
   return r;
