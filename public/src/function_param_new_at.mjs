28ed2de043a386f4779c8d13f386055c60beb292
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 15:10:24 -0500

    2025-02-18T20:10:22.880Z watch   function_param_new_at

--- public/src/function_param_new_at.mjs
+++ public/src/function_param_new_at.mjs
@@ -1,3 +1,5 @@
+import { fn_name } from "./fn_name.mjs";
+import { function_params_names_assert_calls_params_size } from "./function_params_names_assert_calls_params_size.mjs";
 import { data_identifiers_each_lock } from "./data_identifiers_each_lock.mjs";
 import { js_param_new_at } from "./js_param_new_at.mjs";
 import { function_transform_args_split_lambda } from "./function_transform_args_split_lambda.mjs";
@@ -9,6 +11,7 @@ export async function function_param_new_at(
   param_index,
 ) {
   assert_arguments_length(arguments, 4);
+  await function_params_names_assert_calls_params_size(function_name);
   await data_identifiers_each_lock(function_name, lambda);
   async function lambda(fn_name) {
     await function_transform_args_split_lambda(
