f920d689da334c2fec0fd8acaca24f7fac556e15
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 13:49:30 -0500

    2025-02-18T18:49:29.315Z watch   function_params_objectify

--- public/src/function_params_objectify.mjs
+++ public/src/function_params_objectify.mjs
@@ -1,5 +1,4 @@
-import { fn_name } from "./fn_name.mjs";
-import { log } from "./log.mjs";
+import { js_identifiers_to_names } from "./js_identifiers_to_names.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { js_param_generic } from "./js_param_generic.mjs";
 import { data_identifiers_each } from "./data_identifiers_each.mjs";
@@ -7,12 +6,10 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { noop } from "./noop.mjs";
 export async function function_params_objectify(function_name) {
   assert_arguments_length(arguments, 1);
-  await data_identifiers_each(function_name, async function lambda(fn_name) {
-    await function_transform(fn_name, function js_params_objectify(ast) {
+  await data_identifiers_each(function_name, async function lambda(f_name) {
+    await function_transform(f_name, function js_params_objectify(ast) {
       js_param_generic(ast, function_name, noop, function (params) {
-        log({
-          params,
-        });
+        js_identifiers_to_names(params);
       });
     });
   });
