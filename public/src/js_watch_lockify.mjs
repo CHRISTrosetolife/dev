02c558c47f3bbce61d08796345f0534ac9cd10bc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 09:37:17 -0500

    2025-02-18T14:37:15.838Z watch   js_watch_lockify

--- public/src/js_watch_lockify.mjs
+++ public/src/js_watch_lockify.mjs
@@ -1,4 +1,8 @@
+import { log } from "./log.mjs";
 import { js_declaration_single_body } from "./js_declaration_single_body.mjs";
 export function js_watch_lockify(ast) {
   let b = js_declaration_single_body(ast);
+  log({
+    b,
+  });
 }
