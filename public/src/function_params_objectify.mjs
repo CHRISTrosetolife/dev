c29c8b755b3e4e9380b179402118bbd7d3e717df
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 13:46:59 -0500

    2025-02-18T18:46:59.054Z watch   function_params_objectify

--- public/src/function_params_objectify.mjs
+++ public/src/function_params_objectify.mjs
@@ -4,16 +4,12 @@ import { js_params_objectify } from "./js_params_objectify.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { data_identifiers_each } from "./data_identifiers_each.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
+import { noop } from "./noop.mjs";
 export async function function_params_objectify(function_name) {
   assert_arguments_length(arguments, 1);
   await data_identifiers_each(function_name, async function lambda(fn_name) {
     await function_transform(fn_name, function js_params_objectify(ast) {
-      js_param_generic(
-        ast,
-        function_name,
-        function () {},
-        function () {},
-      );
+      js_param_generic(ast, function_name, noop, function () {});
     });
   });
 }
