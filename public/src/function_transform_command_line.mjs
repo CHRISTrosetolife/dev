77cf24e1411dc72c8867a63bf988121a25598f03
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 10:29:19 -0500

    2025-02-18T15:29:17.797Z watch   function_transform_command_line

--- public/src/function_transform_command_line.mjs
+++ public/src/function_transform_command_line.mjs
@@ -1,13 +1,15 @@
+import { watch_lock } from "./watch_lock.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { function_transform_return } from "./function_transform_return.mjs";
 export async function function_transform_command_line(
   lambda_names,
   function_name,
 ) {
-  return await watch_lock(async function () {
+  let v = await watch_lock(async function () {
     assert_arguments_length(arguments, 2);
     let r = await function_transform_return(lambda_names, function_name);
     let { results } = r;
     return results;
   });
+  return v;
 }
