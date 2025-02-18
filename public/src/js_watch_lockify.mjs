c88f38efa8989c301bfeb59eacfbc3b1ced3bd7b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 09:41:36 -0500

    2025-02-18T14:41:33.997Z watch   js_watch_lockify

--- public/src/js_watch_lockify.mjs
+++ public/src/js_watch_lockify.mjs
@@ -1,3 +1,4 @@
+import { js_statement_return } from "./js_statement_return.mjs";
 import { list_clear } from "./list_clear.mjs";
 import { list_copy } from "./list_copy.mjs";
 import { log } from "./log.mjs";
@@ -6,6 +7,7 @@ export function js_watch_lockify(ast) {
   let b = js_declaration_single_body(ast);
   let copy = list_copy(b);
   list_clear(b);
+  let r = js_statement_return(argument);
   log({
     b,
   });
