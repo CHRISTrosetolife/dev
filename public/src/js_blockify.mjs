dc459f8657c72e64d4b0fbafc39a69f77daaf8fc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 09:39:56 -0500

    2025-02-18T14:39:54.360Z watch   js_blockify

--- public/src/js_blockify.mjs
+++ public/src/js_blockify.mjs
@@ -7,9 +7,10 @@ import { object_copy_shallow } from "./object_copy_shallow.mjs";
 import { js_block_statement } from "./js_block_statement.mjs";
 export function js_blockify(body) {
   if (!js_node_type_statement_block_is(body)) {
+    let argument = object_copy_shallow(body);
     let r_code = js_code_statement_return("");
     let r = js_parse_first_function(r_code);
-    object_property_set(r, "argument", object_copy_shallow(body));
+    object_property_set(r, "argument", argument);
     let wrapped = js_block_statement([r]);
     object_replace(body, wrapped);
   }
