59fb1e8616257db3e3fad7ef37acabf689cf564f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 21:45:31 -0500

    2025-02-18T02:45:30.594Z watch   run

--- public/src/run.mjs
+++ public/src/run.mjs
@@ -2,10 +2,11 @@ import { run_tokens } from "./run_tokens.mjs";
 import { process_argv_remaining } from "./process_argv_remaining.mjs";
 import { uuid_file } from "./uuid_file.mjs";
 export async function run() {
-  let v2 = await uuid_file(run, async function () {
+  let v2 = await uuid_file(run, async function (file_path) {
     let r = process_argv_remaining();
     let v = await run_tokens(r);
-    $r, v;
+    let v3 = v;
+    return v3;
   });
   return v2;
 }
