6ef0d5a2a1f6609e68449118eaf3b93f0e98a79e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 10:11:32 -0500

    2025-02-18T15:11:30.678Z watch   js_code_export_function_declare

--- public/src/js_code_export_function_declare.mjs
+++ public/src/js_code_export_function_declare.mjs
@@ -1,3 +1,4 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { js_code_function_declare } from "./js_code_function_declare.mjs";
 export function js_code_export_function_declare(
   function_name,
@@ -5,13 +6,13 @@ export function js_code_export_function_declare(
   body_string,
   async_is,
 ) {
-  let space = async_is ? ` async ` : " ";
+  let space = async_is ? string_combine_multiple([" async "]) : " ";
   let declare = js_code_function_declare(
     function_name,
     args_string,
     body_string,
-    false,
+    async_is,
   );
-  let contents_function = `export${space}${declare}`;
+  let contents_function = string_combine_multiple(["export", space, declare]);
   return contents_function;
 }
