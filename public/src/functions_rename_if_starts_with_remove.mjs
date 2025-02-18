21aa0c15ec8a94e3e733f9772a56e2f16d12d27e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 10:25:46 -0500

    2025-02-18T15:25:45.687Z ft js_watch_lockify functions_rename_if_starts_with_remove

--- public/src/functions_rename_if_starts_with_remove.mjs
+++ public/src/functions_rename_if_starts_with_remove.mjs
@@ -3,14 +3,16 @@ import { string_includes } from "./string_includes.mjs";
 import { string_replace } from "./string_replace.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 export async function functions_rename_if_starts_with_remove(prefix, removal) {
-  await functions_rename_generic(fn_new_get);
-  function fn_new_get(fn) {
-    let fn_new = fn;
-    if (string_starts_with(fn, prefix)) {
-      if (string_includes(fn, removal)) {
-        fn_new = string_replace(fn, removal, "");
+  return await watch_lock(async function () {
+    await functions_rename_generic(fn_new_get);
+    function fn_new_get(fn) {
+      let fn_new = fn;
+      if (string_starts_with(fn, prefix)) {
+        if (string_includes(fn, removal)) {
+          fn_new = string_replace(fn, removal, "");
+        }
+        return fn_new;
       }
-      return fn_new;
     }
-  }
+  });
 }
