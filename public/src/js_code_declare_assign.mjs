616895e2233971cf3f7c4ea03df855ae441c12d0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 14:20:27 -0500

    2025-02-18T19:20:26.372Z watch   js_code_declare_assign

--- public/src/js_code_declare_assign.mjs
+++ public/src/js_code_declare_assign.mjs
@@ -1,7 +1,7 @@
 import { js_code_statement_let } from "./js_code_statement_let.mjs";
 import { js_code_assign } from "./js_code_assign.mjs";
-export function js_code_declare_assign(variable_name, value) {
-  let declare = js_code_assign(variable_name, value);
+export function js_code_declare_assign(left, value) {
+  let declare = js_code_assign(left, value);
   let code = js_code_statement_let(declare);
   return code;
 }
