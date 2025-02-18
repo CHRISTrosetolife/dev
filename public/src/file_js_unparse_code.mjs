ba99c526d941ae36d32a45a0db4889c1ea0ac9b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 10:04:51 -0500

    2025-02-18T15:04:48.960Z watch   file_js_unparse_code

--- public/src/file_js_unparse_code.mjs
+++ public/src/file_js_unparse_code.mjs
@@ -1,12 +1,13 @@
-import { log } from "./log.mjs";
 import { js_data } from "./js_data.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
 import { js_code_format } from "./js_code_format.mjs";
 import { js_unparse } from "./js_unparse.mjs";
+import { assert } from "./assert.mjs";
+import { string_is } from "./string_is.mjs";
+import { js_node_is } from "./js_node_is.mjs";
 export async function file_js_unparse_code(ast, file_path, before) {
-  log({
-    file_path,
-  });
+  assert(js_node_is, [ast]);
+  assert(string_is, [file_path]);
   let code = js_unparse(ast);
   let prettied = await js_code_format(code);
   let result;
