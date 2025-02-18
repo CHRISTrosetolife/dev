6a6f09749e3df064480fa2a4ad629c0a9db4cc3e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 14:39:55 -0500

    2025-02-18T19:39:55.000Z watch   data_identifiers_each

--- public/src/data_identifiers_each.mjs
+++ public/src/data_identifiers_each.mjs
@@ -3,9 +3,10 @@ import { each_async } from "./each_async.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { data_identifiers } from "./data_identifiers.mjs";
 export async function data_identifiers_each(function_name, lambda) {
-  return await watch_lock(async function () {
+  let v = await watch_lock(async function () {
     let dis = await data_identifiers();
     let fns = object_property_get(dis, function_name);
     await each_async(fns, lambda);
   });
+  return v;
 }
