421389005d240a2231f9960757ea6b9dffabb467
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 19:08:44 -0500

    2025-02-18T00:08:44.185Z js_variablize bible_interlinear_upload : v2 to v2

--- public/src/bible_interlinear_upload.mjs
+++ public/src/bible_interlinear_upload.mjs
@@ -17,14 +17,15 @@ import { bible_interlinear_each_chapter } from "./bible_interlinear_each_chapter
 import { list_map_property } from "./list_map_property.mjs";
 export async function bible_interlinear_upload() {
   let language = "greek";
-  let books = await bible_interlinear_cache_new();
+  let v2 = bible_interlinear_cache_new;
+  let books = await v2();
   let chapters = list_adder(function (la) {
     bible_interlinear_each_chapter(books, function (chapter, book_name) {
       let r = object_merge_strict(
         {
           book_name,
         },
-        chapter,
+        chapter
       );
       la(r);
     });
@@ -35,30 +36,31 @@ export async function bible_interlinear_upload() {
     await bible_storage_version_upload(
       bible_storage_interlinear_book_path(book_name),
       chapter_name,
-      chapter,
+      chapter
     );
     let tokens = list_adder_unique(function (la) {
       bible_interlinear_chapter_each_token(chapter, la);
     });
     let strongs = list_map_property(tokens, "strong");
-     strongs = list_unique(strongs);
-    let definitions = await list_to_lookup_value_async(
-      strongs,
-      async function (s) {
-        let v = {
-          [bible_storage_interlinear_chapter_definitions_property()]:
-            await bible_interlinear_definition(language, s),
-        };
-        return v;
-      },
-    );
+    strongs = list_unique(strongs);
+    let definitions = await list_to_lookup_value_async(strongs, async function (
+      s
+    ) {
+      let v = {
+        [bible_storage_interlinear_chapter_definitions_property()]: await bible_interlinear_definition(
+          language,
+          s
+        ),
+      };
+      return v;
+    });
     await bible_storage_version_upload(
       bible_storage_interlinear_chapter_definitions_path(
         book_name,
-        chapter_name,
+        chapter_name
       ),
       bible_storage_interlinear_chapter_definitions_name(),
-      definitions,
+      definitions
     );
   });
 }
