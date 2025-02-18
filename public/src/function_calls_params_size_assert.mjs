8f51fe16e7daa8b62cd6d3a866afd0999d65c400
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 15:27:13 -0500

    2025-02-18T20:27:13.154Z watch   function_calls_params_size_assert

--- public/src/function_calls_params_size_assert.mjs
+++ public/src/function_calls_params_size_assert.mjs
@@ -1,15 +1,15 @@
 import { noop } from "./noop.mjs";
 import { list_size_equal } from "./list_size_equal.mjs";
-import { assert } from "./assert.mjs";
 import { data_identifiers_each_transform_params } from "./data_identifiers_each_transform_params.mjs";
+import { assert_message } from "./assert_message.mjs";
 export async function function_calls_params_size_assert(
   function_name,
   expected,
 ) {
   await data_identifiers_each_transform_params(
     function_name,
-    function on_call(params) {
-      assert(list_size_equal, [params, expected]);
+    function on_call(params, function_name) {
+      assert_message(list_size_equal, [params, expected], function () {});
     },
     noop,
   );
