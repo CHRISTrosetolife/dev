22278fb565fb2d860e8a7fdb34c01c26c14e7c14
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 14:33:07 -0500

    2025-02-18T19:33:02.571Z watch   function_params_objectify

--- public/src/function_params_objectify.mjs
+++ public/src/function_params_objectify.mjs
@@ -1,3 +1,4 @@
+import { noop } from "./noop.mjs";
 import { data_identifiers_each_transform_params } from "./data_identifiers_each_transform_params.mjs";
 import { list_add_first } from "./list_add_first.mjs";
 import { js_code_destructure_assign } from "./js_code_destructure_assign.mjs";
@@ -14,18 +15,17 @@ export async function function_params_objectify(function_name) {
   assert_arguments_length(arguments, 1);
   await data_identifiers_each_transform_params(
     function_name,
-    on_define,
-    on_call,
+    function on_define(params, declaration, ast) {
+      let params_names = js_identifiers_to_names(params);
+      let duplicates = js_identifiers_duplicates(ast);
+      let i = list_intersect(params_names, duplicates);
+      assert(list_empty_is, [i]);
+      let body = js_declaration_to_body(declaration);
+      let arg_name = js_name_unique(ast, "arg");
+      let destructure_code = js_code_destructure_assign(params_names, arg_name);
+      let destructure = js_parse_first(destructure_code);
+      list_add_first(body, destructure);
+    },
+    noop,
   );
-  function on_define(params, declaration, ast) {
-    let params_names = js_identifiers_to_names(params);
-    let duplicates = js_identifiers_duplicates(ast);
-    let i = list_intersect(params_names, duplicates);
-    assert(list_empty_is, [i]);
-    let body = js_declaration_to_body(declaration);
-    let arg_name = js_name_unique(ast, "arg");
-    let destructure_code = js_code_destructure_assign(params_names, arg_name);
-    let destructure = js_parse_first(destructure_code);
-    list_add_first(body, destructure);
-  }
 }
