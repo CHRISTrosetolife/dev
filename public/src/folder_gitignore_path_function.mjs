8e08ce461acb9e3395de676a0f4f6509afd2b0d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 21:51:19 -0500

    2025-02-18T02:51:17.487Z watch   folder_gitignore_path_function_parent

--- public/src/folder_gitignore_path_function.mjs
+++
@@ -1,6 +0,0 @@
-import { path_join } from "./path_join.mjs";
-import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
-export function folder_gitignore_path_function(fn, f_path) {
-  let v = path_join([folder_gitignore_path(fn.name), f_path]);
-  return v;
-}
---
+++ public/src/folder_gitignore_path_function_parent.mjs
@@ -0,0 +1,5 @@
+import { folder_gitignore_path } from "./folder_gitignore_path.mjs";
+export function folder_gitignore_path_function_parent(fn) {
+  let v = folder_gitignore_path(fn.name);
+  return v;
+}
