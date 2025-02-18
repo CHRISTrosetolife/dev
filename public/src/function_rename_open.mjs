04bc8d220981e51508566f8f5bc6ab2121ccba41
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 09:29:19 -0500

    2025-02-18T14:29:18.280Z watch   function_rename_open

--- public/src/function_rename_open.mjs
+++ public/src/function_rename_open.mjs
@@ -1,10 +1,10 @@
+import { watch_lock } from "./watch_lock.mjs";
 import { file_write_empty } from "./file_write_empty.mjs";
-import { run } from "./run.mjs";
 import { uuid_file } from "./uuid_file.mjs";
 import { function_open } from "./function_open.mjs";
 import { function_rename } from "./function_rename.mjs";
 export async function function_rename_open(fn_name_from, fn_name_to) {
-  let v2 = await uuid_file(run, async function (file_path) {
+  let v2 = await uuid_file(watch_lock, async function (file_path) {
     await file_write_empty(file_path);
     let r = await function_rename(fn_name_from, fn_name_to);
     await function_open(fn_name_to);
