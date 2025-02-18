9f0f815dc82abd019357c77e7272e81a84379387
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 14:20:19 -0500

    2025-02-18T19:20:18.804Z watch   js_code_assign

--- public/src/js_code_assign.mjs
+++ public/src/js_code_assign.mjs
@@ -1,3 +1,5 @@
-export function js_code_assign(variable_name, value) {
-  return `${variable_name} = ${value}`;
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function js_code_assign(left, value) {
+  let v = string_combine_multiple([left, " = ", value]);
+  return v;
 }
