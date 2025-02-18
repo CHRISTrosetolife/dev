90579bbf8fb21a87b2557a6a44af88f1628ce7ea
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 16:36:46 -0500

    2025-02-18T21:36:44.812Z watch   html_style_default

--- public/src/html_style_default.mjs
+++ public/src/html_style_default.mjs
@@ -1,5 +1,11 @@
+import { fn_name } from "./fn_name.mjs";
 import { global_get } from "./global_get.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 export function html_style_default() {
-  return object_property_initialize(global_get(), html_style_default.name, {});
+  let v = object_property_initialize(
+    global_get(),
+    fn_name("html_style_default"),
+    {},
+  );
+  return v;
 }
