867bcafc268c448d5019b069c158baf86a4a0e6a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 15:31:25 -0500

    2025-02-18T20:31:24.984Z watch   function_params_objectify

--- public/src/function_params_objectify.mjs
+++ public/src/function_params_objectify.mjs
@@ -25,20 +25,20 @@ import { js_parse_expression } from "./js_parse_expression.mjs";
 import { error } from "./error.mjs";
 export async function function_params_objectify(function_name) {
   assert_arguments_length(arguments, 1);
-  let params_names;
+  let params_names_fn;
   if (false) {
     await data_identifiers_each_transform_params(
       function_name,
       noop,
       function on_define(params, declaration, ast) {
-        params_names = js_identifiers_to_names(params);
+        params_names_fn = js_identifiers_to_names(params);
         let duplicates = js_identifiers_duplicates(ast);
-        let i = list_intersect(params_names, duplicates);
+        let i = list_intersect(params_names_fn, duplicates);
         assert(list_empty_is, [i]);
         let body = js_declaration_to_body(declaration);
         let arg_name = js_name_unique(ast, "arg");
         let destructure_code = js_code_destructure_assign(
-          params_names,
+          params_names_fn,
           arg_name,
         );
         let destructure = js_parse_first(destructure_code);
@@ -46,15 +46,15 @@ export async function function_params_objectify(function_name) {
       },
     );
   }
-  params_names = await function_params_names(function_name);
-  assert(list_is, [params_names]);
-  await function_calls_params_size_assert_list(function_name, params_names);
+  params_names_fn = await function_params_names(function_name);
+  assert(list_is, [params_names_fn]);
+  await function_calls_params_size_assert_list(function_name, params_names_fn);
   await data_identifiers_each_transform_params(
     function_name,
     function on_call(params, declaration, ast) {
       let c = js_code_object_properties(
-        params_names,
-        list_map(range(list_size(params_names)), function () {
+        params_names_fn,
+        list_map(range(list_size(params_names_fn)), function () {
           let v2 = "0";
           return v2;
         }),
