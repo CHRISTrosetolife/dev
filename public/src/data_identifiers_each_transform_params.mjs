90fce8fcb26b1ded00c8877650fc4fd8d7cc1fa0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 14:31:43 -0500

    2025-02-18T19:31:41.272Z watch   data_identifiers_each_transform_params

---
+++ public/src/data_identifiers_each_transform_params.mjs
@@ -0,0 +1,14 @@
+import { noop } from "./noop.mjs";
+import { js_param_generic } from "./js_param_generic.mjs";
+import { data_identifiers_each_transform } from "./data_identifiers_each_transform.mjs";
+export async function data_identifiers_each_transform_params(
+  function_name,
+  on_define,
+) {
+  await data_identifiers_each_transform(
+    function_name,
+    function js_params_objectify(ast) {
+      js_param_generic(ast, function_name, noop, on_define);
+    },
+  );
+}
