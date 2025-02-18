ce9ecd3b944174b6dc53b1eae0302a2801861d52
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 15:15:08 -0500

    2025-02-18T20:15:08.129Z watch   js_code_object_properties

--- public/src/js_code_object_properties.mjs
+++ public/src/js_code_object_properties.mjs
@@ -1,12 +1,13 @@
 import { each_two } from "./each_two.mjs";
-import { list_join } from "./list_join.mjs";
 import { js_code_braces_inside } from "./js_code_braces_inside.mjs";
 import { js_code_key_value } from "./js_code_key_value.mjs";
 import { list_add } from "./list_add.mjs";
+import { list_join_comma_space } from "./list_join_comma_space.mjs";
 export function js_code_object_properties(names, values) {
   let result = [];
   each_two(names, values, lambda);
-  return js_code_braces_inside(list_join(result, ", "));
+  let v = js_code_braces_inside(list_join_comma_space(result));
+  return v;
   function lambda(name, value) {
     list_add(result, js_code_key_value(name, value));
   }
