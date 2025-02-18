4bbd3eab13e1e0f9c42874f007bed597af003f11
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 10:05:52 -0500

    2025-02-18T15:05:50.761Z watch   file_js_transform_regular

--- public/src/file_js_transform_regular.mjs
+++ public/src/file_js_transform_regular.mjs
@@ -3,5 +3,5 @@ import { file_js_parse_code } from "./file_js_parse_code.mjs";
 export async function file_js_transform_regular(file_path, lambda) {
   let { ast, code } = await file_js_parse_code(file_path);
   lambda(ast);
-  await file_js_unparse_code(file_path, ast, code);
+  await file_js_unparse_code(ast, file_path, code);
 }
