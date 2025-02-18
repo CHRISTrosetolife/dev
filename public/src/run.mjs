1e5e2892897bab5a97f14ee9dd6a19f89bc03454
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-17 21:55:01 -0500

    2025-02-18T02:55:00.944Z watch   run

--- public/src/run.mjs
+++ public/src/run.mjs
@@ -3,10 +3,14 @@ import { run_tokens } from "./run_tokens.mjs";
 import { process_argv_remaining } from "./process_argv_remaining.mjs";
 import { uuid_file } from "./uuid_file.mjs";
 import { global_function } from "./global_function.mjs";
+import { object_merge_strict } from "./object_merge_strict.mjs";
 export async function run() {
   let v2 = await uuid_file(run, async function (file_path) {
     await file_write_empty(file_path);
     let g = global_function(run);
+    object_merge_strict(g, {
+      file_path,
+    });
     let r = process_argv_remaining();
     let v = await run_tokens(r);
     let v3 = v;
