92bc26c6a4b827f79c0d1f52df79c92f3e0b459c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 15:12:55 -0500

    2025-02-18T20:12:54.367Z watch   function_params_objectify

--- public/src/function_params_objectify.mjs
+++ public/src/function_params_objectify.mjs
@@ -37,7 +37,7 @@ export async function function_params_objectify(function_name) {
   await data_identifiers_each_transform_params(
     function_name,
     function on_call(params, declaration, ast) {
-      list_map_index();
+      list_map_index(params, function (p) {});
       let params_names = js_identifiers_to_names(params);
       let duplicates = js_identifiers_duplicates(ast);
       let i = list_intersect(params_names, duplicates);
