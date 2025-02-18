b5403efd510997e3f25e6fc866d2adee96933845
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 09:56:09 -0500

    2025-02-18T14:56:07.372Z watch   js_code_statement_return_empty

--- public/src/js_code_statement_return_empty.mjs
+++ public/src/js_code_statement_return_empty.mjs
@@ -1,4 +1,5 @@
 import { js_code_statement_return } from "./js_code_statement_return.mjs";
 export function js_code_statement_return_empty() {
-  return js_code_statement_return("");
+  let v = js_code_statement_return("");
+  return v;
 }
