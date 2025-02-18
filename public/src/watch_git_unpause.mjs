2781dbfb00c8adab453824c67079baf49d01f0c5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 16:14:42 -0500

    2025-02-18T21:14:42.187Z watch   watch_git_unpause

--- public/src/watch_git_unpause.mjs
+++ public/src/watch_git_unpause.mjs
@@ -1,6 +1,6 @@
-import { file_delete } from "./file_delete.mjs";
+import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
 import { watch_git_pause_path } from "./watch_git_pause_path.mjs";
 export async function watch_git_unpause() {
   let p = watch_git_pause_path();
-  await file_delete(p);
+  await file_delete_if_exists(p);
 }
