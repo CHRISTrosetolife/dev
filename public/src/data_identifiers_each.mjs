a9c6dab57a581edb70b91a01a3068db52c2caf3a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 14:39:14 -0500

    2025-02-18T19:39:13.744Z watch   data_identifiers_each

--- public/src/data_identifiers_each.mjs
+++ public/src/data_identifiers_each.mjs
@@ -1,13 +1,10 @@
-import { list_map_async } from "./list_map_async.mjs";
+import { each_async } from "./each_async.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { data_identifiers } from "./data_identifiers.mjs";
 export async function data_identifiers_each(function_name, lambda) {
   let dis = await data_identifiers();
   let fns = object_property_get(dis, function_name);
-  let v = await list_map_async(fns, async function (f_name) {
-    let m = await lambda(f_name);
-    let v2 = m;
-    return v2;
+  await each_async(fns, async function (f_name) {
+    await lambda(f_name);
   });
-  return v;
 }
