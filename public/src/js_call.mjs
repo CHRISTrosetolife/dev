a372639c53e0b16e4116af0e0f1fb368143f4ab8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 09:47:46 -0500

    2025-02-18T14:47:45.278Z watch   js_call

--- public/src/js_call.mjs
+++ public/src/js_call.mjs
@@ -1,10 +1,8 @@
-import { js_parse_expression } from "./js_parse_expression.mjs";
+import { js_expression_call } from "./js_expression_call.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { list_add } from "./list_add.mjs";
-import { js_code_call } from "./js_code_call.mjs";
 export function js_call(fn_name, args) {
-  let code_call = js_code_call(fn_name);
-  let expression = js_parse_expression(code_call);
+  let expression = js_expression_call(fn_name);
   let { arguments: args2 } = expression;
   for (let a of args) {
     list_add(args2, a);
