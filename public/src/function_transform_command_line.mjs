f0549e58f2fe361c242c5cc38e7663600bc16fa5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 10:28:15 -0500

    2025-02-18T15:28:14.707Z ft js_watch_lockify ft

--- public/src/function_transform_command_line.mjs
+++ public/src/function_transform_command_line.mjs
@@ -4,8 +4,10 @@ export async function function_transform_command_line(
   lambda_names,
   function_name,
 ) {
-  assert_arguments_length(arguments, 2);
-  let r = await function_transform_return(lambda_names, function_name);
-  let { results } = r;
-  return results;
+  return await watch_lock(async function () {
+    assert_arguments_length(arguments, 2);
+    let r = await function_transform_return(lambda_names, function_name);
+    let { results } = r;
+    return results;
+  });
 }
