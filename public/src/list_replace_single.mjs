c0e722c4a110e05860984c556a561e11cc5f408a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 16:21:28 -0500

    2025-02-18T21:21:27.210Z watch   list_replace_single

--- public/src/list_replace_single.mjs
+++ public/src/list_replace_single.mjs
@@ -2,5 +2,5 @@ import { list_add } from "./list_add.mjs";
 import { list_clear } from "./list_clear.mjs";
 export function list_replace_single(params, p) {
   list_clear(params);
-  list_add(p);
+  list_add(params, p);
 }
