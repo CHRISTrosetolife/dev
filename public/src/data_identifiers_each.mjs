10809103bbe634d34ab3458bc442aed8a11b1aa3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 14:39:51 -0500

    2025-02-18T19:39:51.112Z ft js_watch_lockify data_identifiers_each

--- public/src/data_identifiers_each.mjs
+++ public/src/data_identifiers_each.mjs
@@ -1,8 +1,11 @@
+import { watch_lock } from "./watch_lock.mjs";
 import { each_async } from "./each_async.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { data_identifiers } from "./data_identifiers.mjs";
 export async function data_identifiers_each(function_name, lambda) {
-  let dis = await data_identifiers();
-  let fns = object_property_get(dis, function_name);
-  await each_async(fns, lambda);
+  return await watch_lock(async function () {
+    let dis = await data_identifiers();
+    let fns = object_property_get(dis, function_name);
+    await each_async(fns, lambda);
+  });
 }
