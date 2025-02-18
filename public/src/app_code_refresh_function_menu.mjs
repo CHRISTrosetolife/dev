55d09c29e1ba0ec75ee81d8c80e954095e9f9e8e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 16:58:10 -0500

    2025-02-18T21:58:03.026Z watch   app_code_refresh_function_menu

--- public/src/app_code_refresh_function_menu.mjs
+++ public/src/app_code_refresh_function_menu.mjs
@@ -114,7 +114,7 @@ export function app_code_refresh_function_menu(arg) {
       let selection_rename_text = "Selection rename";
       html_button(overlay, selection_rename_text, async function () {
         html_clear(overlay);
-        html_button_back(overlay);
+        html_button_back(overlay, menu_refresh);
         let { node } =
           app_code_refresh_function_selection_one_get(selection_result);
         let before = object_property_get(node, "name");
