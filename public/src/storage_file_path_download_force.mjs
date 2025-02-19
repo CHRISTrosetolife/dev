b261a5fdd2a3930375dfda6f4f4eddeeff297fdb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 21:15:32 -0500

    2025-02-19T02:15:30.897Z watch   storage_file_path_download_force

--- public/src/storage_file_path_download_force.mjs
+++ public/src/storage_file_path_download_force.mjs
@@ -1,11 +1,10 @@
-import { file_read } from "./file_read.mjs";
-import { storage_file_download_path } from "./storage_file_download_path.mjs";
-import { storage_file_download } from "./storage_file_download.mjs";
-import { storage_file } from "./storage_file.mjs";
+import { storage_file_path_download_generic } from "./storage_file_path_download_generic.mjs";
+import { storage_file_download_force } from "./storage_file_download_force.mjs";
 export async function storage_file_path_download_force(latest_path_user) {
-  let f = await storage_file(latest_path_user);
-  await storage_file_download(f);
-  let destination = storage_file_download_path(f);
-  let v = await file_read(destination);
+  let download_lambda = storage_file_download_force;
+  let v = await storage_file_path_download_generic(
+    latest_path_user,
+    download_lambda,
+  );
   return v;
 }
