d39abede57412f6f6c007581ec4bf80e6c6abb2f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 21:07:54 -0500

    2025-02-19T02:07:51.617Z watch   app_code_local_download

--- public/src/app_code_local_download.mjs
+++ public/src/app_code_local_download.mjs
@@ -46,6 +46,8 @@ export async function app_code_local_download(username) {
           log({
             file_path,
           });
+          if (false) {
+          }
           let contents = object_property_get(
             value,
             app_code_property_contents(),
