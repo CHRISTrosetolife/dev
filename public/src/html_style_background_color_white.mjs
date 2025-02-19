f6c0082d1fc9e22a4a816e43c3066ef7f8b123ce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 20:36:43 -0500

    2025-02-19T01:36:42.194Z watch   app_code_refresh_function_menu

---
+++ public/src/html_style_background_color_white.mjs
@@ -0,0 +1,4 @@
+import { html_style_background_color } from "./html_style_background_color.mjs";
+export function html_style_background_color_white(d) {
+  html_style_background_color(d, "white");
+}
