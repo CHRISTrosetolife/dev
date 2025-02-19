eb273fe81e3db99f2e39a2dbf62fdb5bee5a838f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 20:58:05 -0500

    2025-02-19T01:58:04.994Z watch   app_code_refresh_function_menu

--- public/src/app_code_refresh_function_menu.mjs
+++ public/src/app_code_refresh_function_menu.mjs
@@ -1,3 +1,5 @@
+import { html_style_units } from "./html_style_units.mjs";
+import { html_style_margin } from "./html_style_margin.mjs";
 import { html_style_rounded_padded } from "./html_style_rounded_padded.mjs";
 import { html_style_background_color_white } from "./html_style_background_color_white.mjs";
 import { html_div } from "./html_div.mjs";
@@ -121,6 +123,7 @@ export function app_code_refresh_function_menu(arg) {
         let d = html_div(overlay);
         html_style_background_color_white(d);
         html_style_rounded_padded(d);
+        html_style_margin(d, html_style_units(2));
         html_button_back(overlay, menu_refresh);
         let { node } =
           app_code_refresh_function_selection_one_get(selection_result);
