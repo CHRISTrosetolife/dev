497e7c6d397d6baaedc91173008d5f636e1978d3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-19 14:59:01 -0500

    2025-02-19T19:58:59.653Z watch   app_preach

--- public/src/app_preach.mjs
+++ public/src/app_preach.mjs
@@ -1,7 +1,6 @@
+import { html_style_default_initialize_axios } from "./html_style_default_initialize_axios.mjs";
 import { app_preach_main } from "./app_preach_main.mjs";
-import { html_script_axios } from "./html_script_axios.mjs";
-import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_preach() {
-    let root = await html_style_default_initialize_axios();
+  let root = await html_style_default_initialize_axios();
   app_preach_main(root);
 }
--- public/src/app_supper.mjs
+++ public/src/app_supper.mjs
@@ -1,6 +1,6 @@
 import { app_preach_sermon_contents } from "./app_preach_sermon_contents.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_supper() {
-  let root = html_style_default_initialize();
+    let root = await html_style_default_initialize_axios();
   await app_preach_sermon_contents(root, "supper");
 }
