bf15d734904b668aa76a7042d53f34eaf673061c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-19 17:33:39 -0500

    2025-02-19T22:33:38.703Z watch   function_params_move_name

--- public/src/function_params_move_name.mjs
+++ public/src/function_params_move_name.mjs
@@ -4,12 +4,14 @@ import { list_sort } from "./list_sort.mjs";
 import { function_param_move_index } from "./function_param_move_index.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { list_index } from "./list_index.mjs";
+import { function_params_names } from "./function_params_names.mjs";
 export async function function_params_move_name(
   function_name,
   name_old,
   name_new,
 ) {
   assert_arguments_length(arguments, 3);
+  let params_names = await function_params_names(function_name);
   let index_old = list_index(params_names, name_old);
   let index_new = list_index(params_names, name_new);
   let indices = [index_old, index_new];
