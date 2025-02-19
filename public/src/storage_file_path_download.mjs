4dacf550d557ee0e0602128656a50687c061ae7c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 21:14:53 -0500

    2025-02-19T02:14:52.596Z watch   storage_file_path_download

--- public/src/storage_file_path_download.mjs
+++ public/src/storage_file_path_download.mjs
@@ -3,8 +3,9 @@ import { storage_file_download_path } from "./storage_file_download_path.mjs";
 import { storage_file_download } from "./storage_file_download.mjs";
 import { storage_file } from "./storage_file.mjs";
 export async function storage_file_path_download(latest_path_user) {
+  let download_lambda = storage_file_download;
   let f = await storage_file(latest_path_user);
-  await storage_file_download(f);
+  await download_lambda(f);
   let destination = storage_file_download_path(f);
   let v = await file_read(destination);
   return v;
