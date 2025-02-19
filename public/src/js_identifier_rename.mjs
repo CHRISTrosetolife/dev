f143da5b3299f08fe9b4f5fb0f3723fd028066a7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 20:40:15 -0500

    2025-02-19T01:40:14.081Z watch   js_identifier_rename

--- public/src/js_identifier_rename.mjs
+++ public/src/js_identifier_rename.mjs
@@ -3,6 +3,7 @@ import { js_visit_identifiers_named } from "./js_visit_identifiers_named.mjs";
 import { string_is } from "./string_is.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { assert } from "./assert.mjs";
+import { js_identifiers_names } from "./js_identifiers_names.mjs";
 export function js_identifier_rename(
   ast,
   identifier_from_name,
@@ -11,6 +12,7 @@ export function js_identifier_rename(
   assert_arguments_length(arguments, 3);
   assert(string_is, [identifier_from_name]);
   assert(string_is, [identifier_to_name]);
+  let existing = js_identifiers_names(ast);
   js_visit_identifiers_named(ast, identifier_from_name, lambda);
   function lambda(node) {
     object_property_set(node, "name", identifier_to_name);
