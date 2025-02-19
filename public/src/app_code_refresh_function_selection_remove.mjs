88e54ee6c41d524e6787e7dd138665916ca5f1fa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-19 17:45:54 -0500

    2025-02-19T22:45:53.533Z watch   app_code_refresh_function_selection_remove

--- public/src/app_code_refresh_function_selection_remove.mjs
+++ public/src/app_code_refresh_function_selection_remove.mjs
@@ -1,9 +1,8 @@
 import { app_code_refresh_function_selection_removals } from "./app_code_refresh_function_selection_removals.mjs";
 import { list_remove_multiple_from } from "./list_remove_multiple_from.mjs";
 export function app_code_refresh_function_selection_remove(result) {
-  let { ancestor_common, low, high, removed } =
-    app_code_refresh_function_selection_removals(result);
+  let v = app_code_refresh_function_selection_removals(result);
+  let { ancestor_common, low, high, removed } = v;
   list_remove_multiple_from(ancestor_common, low, high);
-  let v2 = removed;
-  return v2;
+  return v;
 }
