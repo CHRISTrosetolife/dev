9052031241c87b57c462a2b0ff21bf9e3d11123e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-18 20:43:59 -0500

    2025-02-19T01:43:58.700Z watch   js_identifier_exists_not_assert

--- public/src/js_identifier_exists_not_assert.mjs
+++ public/src/js_identifier_exists_not_assert.mjs
@@ -1,9 +1,8 @@
-import { list_includes_not } from "./list_includes_not.mjs";
-import { assert } from "./assert.mjs";
+import { assert_boolean } from "./assert_boolean.mjs";
 import { js_identifiers_names } from "./js_identifiers_names.mjs";
 import { list_includes } from "./list_includes.mjs";
 export function js_identifier_exists_not_assert(ast, identifier_to_name) {
   let existing = js_identifiers_names(ast);
   let exists = list_includes(existing, identifier_to_name);
-  assert(list_includes_not, [existing, identifier_to_name]);
+  assert_boolean(exists);
 }
