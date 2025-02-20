3baffd00cfbbe2c3f78d07c3d892dafa83cd27de
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 14:32:19 -0500

    2025-02-20T19:32:16.907Z watch   app_code_refresh_function_menu

---
+++ public/src/app_code_button_copy_generic.mjs
@@ -0,0 +1,36 @@
+import { app_code_refresh_function_change } from "./app_code_refresh_function_change.mjs";
+import { list_concat } from "./list_concat.mjs";
+import { list_between } from "./list_between.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { functions_names } from "./functions_names.mjs";
+import { noop } from "./noop.mjs";
+import { app_code_button_rename_generic } from "./app_code_button_rename_generic.mjs";
+export function app_code_button_copy_generic(
+  overlay,
+  button_text,
+  menu_refresh,
+  fn,
+  fn_args,
+  ast_change_finish,
+  overlay_remove,
+) {
+  app_code_button_rename_generic(
+    overlay,
+    button_text,
+    menu_refresh,
+    noop,
+    functions_names,
+    "Function name new",
+    "Function already exists",
+    async function (function_selected, after_value) {
+      await fn(...fn_args);
+      await ast_change_finish(
+        string_combine_multiple(
+          list_between(list_concat([fn.name], fn_args), " "),
+        ),
+      );
+      await app_code_refresh_function_change(after_value);
+      overlay_remove();
+    },
+  );
+}
--- public/src/app_code_refresh_function_menu.mjs
+++ public/src/app_code_refresh_function_menu.mjs
@@ -1,10 +1,5 @@
-import { list_concat } from "./list_concat.mjs";
-import { app_code_refresh_function_change } from "./app_code_refresh_function_change.mjs";
-import { list_between } from "./list_between.mjs";
+import { app_code_button_copy_generic } from "./app_code_button_copy_generic.mjs";
 import { function_copy } from "./function_copy.mjs";
-import { functions_names } from "./functions_names.mjs";
-import { noop } from "./noop.mjs";
-import { app_code_button_rename_generic } from "./app_code_button_rename_generic.mjs";
 import { file_read } from "./file_read.mjs";
 import { file_js_unparse_code } from "./file_js_unparse_code.mjs";
 import { app_code_user_upload_batch } from "./app_code_user_upload_batch.mjs";
@@ -77,24 +72,14 @@ export function app_code_refresh_function_menu(arg) {
     let fn = function_copy;
     let fn_args = [selection_result, after_value];
     let button_text = "Function copy";
-    app_code_button_rename_generic(
+    app_code_button_copy_generic(
       overlay,
       button_text,
       menu_refresh,
-      noop,
-      functions_names,
-      "Function name new",
-      "Function already exists",
-      async function (function_selected, after_value) {
-        await fn(...fn_args);
-        await ast_change_finish(
-          string_combine_multiple(
-            list_between(list_concat([fn.name], fn_args), " "),
-          ),
-        );
-        await app_code_refresh_function_change(after_value);
-        overlay_remove();
-      },
+      fn,
+      fn_args,
+      ast_change_finish,
+      overlay_remove,
     );
   }
   app_code_button_menu_app(context, overlay, overlay_remove);
