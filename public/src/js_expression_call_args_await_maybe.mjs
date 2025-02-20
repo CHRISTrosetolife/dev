b0e86c9bcaf01583169df98523992b755d8f5dc6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 17:06:32 -0500

    2025-02-20T22:06:31.153Z watch   js_expression_call_args_await_maybe

--- public/src/js_expression_call_args_await_maybe.mjs
+++ public/src/js_expression_call_args_await_maybe.mjs
@@ -3,16 +3,15 @@ import { list_map } from "./list_map.mjs";
 import { js_expression_call_args } from "./js_expression_call_args.mjs";
 import { identity } from "./identity.mjs";
 import { js_expression_await } from "./js_expression_await.mjs";
-import { function_name_new } from "./function_name_new.mjs";
 export function js_expression_call_args_await_maybe(
-  function_name_new,
+  function_name_called,
   args_names,
   async_is,
 ) {
   let wrapper = async_is ? js_expression_await : identity;
   let e = wrapper(
     js_expression_call_args(
-      function_name_new,
+      function_name_called,
       list_map(args_names, js_parse_expression),
     ),
   );
--- public/src/js_expression_call_args_await_maybe_insert.mjs
+++ public/src/js_expression_call_args_await_maybe_insert.mjs
@@ -2,14 +2,14 @@ import { list_insert } from "./list_insert.mjs";
 import { js_expression_call_args_await_maybe } from "./js_expression_call_args_await_maybe.mjs";
 export function js_expression_call_args_await_maybe_insert(
   function_name_called,
-  param_names,
+  args_names,
   async_is,
   list,
   index_insert,
 ) {
   let e = js_expression_call_args_await_maybe(
     function_name_called,
-    param_names,
+    args_names,
     async_is,
   );
   list_insert(list, index_insert, e);
