1f90f9af60c785a3a7e220dbcb29cb265f367d80
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 11:52:10 -0500

    2025-02-20T16:52:09.546Z r functions_params_delete_nam functions_params_delete_name

--- public/src/functions_params_delete_name.mjs
+++ public/src/functions_params_delete_name.mjs
@@ -1,6 +1,6 @@
 import { function_params_delete_name } from "./function_params_delete_name.mjs";
 import { each_async } from "./each_async.mjs";
-export async function functions_params_delete_nam(function_names) {
+export async function functions_params_delete_name(function_names) {
   await each_async(function_names, async function (function_name) {
     await function_params_delete_name();
   });
