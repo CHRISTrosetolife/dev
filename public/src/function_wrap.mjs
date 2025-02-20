ca381fc32a4024fb807c539336e4dc8d2dff2d4c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 16:53:30 -0500

    2025-02-20T21:53:29.273Z watch   function_wrap

--- public/src/function_wrap.mjs
+++ public/src/function_wrap.mjs
@@ -3,4 +3,6 @@ import { function_declaration } from "./function_declaration.mjs";
 export async function function_wrap(function_name, function_name_wrapped) {
   let d = await function_declaration(function_name);
   let params = js_declaration_to_params(d);
+  let v = d;
+  return v;
 }
