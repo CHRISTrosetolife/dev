7676525fd3e06f0f2030a8e49e580fba5508e69b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 15:01:25 -0500

    2025-02-20T20:01:23.438Z watch   file_delete

--- public/src/file_delete.mjs
+++ public/src/file_delete.mjs
@@ -3,12 +3,12 @@ import { global_files } from "./global_files.mjs";
 import { web_is } from "./web_is.mjs";
 import { import_node } from "./import_node.mjs";
 import { object_property_delete } from "./object_property_delete.mjs";
-export async function file_delete(file_name) {
+export async function file_delete(file_path) {
   if (web_is()) {
     let { [global_files()]: files } = global_get();
-    object_property_delete(files, file_name);
+    object_property_delete(files, file_path);
   }
   let fs = await import_node("fs");
-  let v = await fs.promises.rm(file_name);
+  let v = await fs.promises.rm(file_path);
   return v;
 }
