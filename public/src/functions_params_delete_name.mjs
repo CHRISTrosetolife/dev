f80683672c1f65f84bb51873a8fb58e2baf51249
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 11:55:00 -0500

    2025-02-20T16:55:00.220Z watch   functions_params_delete_name

--- public/src/functions_params_delete_name.mjs
+++ public/src/functions_params_delete_name.mjs
@@ -1,10 +1,11 @@
+import { string_split_comma } from "./string_split_comma.mjs";
 import { function_params_delete_name } from "./function_params_delete_name.mjs";
 import { each_async } from "./each_async.mjs";
 export async function functions_params_delete_name(
   function_names,
   param_name_to_delete,
 ) {
-  let split = list_split_comma(function_names);
+  let split = string_split_comma(function_names);
   await each_async(split, async function (function_name) {
     await function_params_delete_name(function_name, param_name_to_delete);
   });
