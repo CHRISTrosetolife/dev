8a3b5e3eda3bb7ec7f570d5ce5462dcd087f4763
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 15:03:33 -0500

    2025-02-20T20:03:30.397Z watch   file_delete

--- public/src/file_delete.mjs
+++ public/src/file_delete.mjs
@@ -1,3 +1,5 @@
+import { list_add } from "./list_add.mjs";
+import { global_file_changes } from "./global_file_changes.mjs";
 import { global_get } from "./global_get.mjs";
 import { global_files } from "./global_files.mjs";
 import { web_is } from "./web_is.mjs";
@@ -7,6 +9,11 @@ export async function file_delete(file_path) {
   if (web_is()) {
     let { [global_files()]: files } = global_get();
     object_property_delete(files, file_path);
+    let fcs = global_file_changes();
+    list_add(fcs, {
+      file_path,
+      deleted: true,
+    });
     return;
   }
   let fs = await import_node("fs");
