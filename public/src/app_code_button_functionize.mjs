3ff7f64270348e2068a867a717eff994f6024ad2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 11:40:57 -0500

    2025-02-20T16:40:56.881Z watch   app_code_button_functionize

--- public/src/app_code_button_functionize.mjs
+++ public/src/app_code_button_functionize.mjs
@@ -118,13 +118,17 @@ export function app_code_button_functionize(
       ancestor_common,
       low,
       high,
+      param_names,
     };
     return v5;
   }
   async function on_click(on_overlay_result, function_name_new) {
-    let { ancestor_common, low, high } = on_overlay_result;
+    let { ancestor_common, low, high, param_names } = on_overlay_result;
     list_remove_multiple_from(ancestor_common, low, high);
-    js_code_export_function_declare(function_name_new);
+    js_code_export_function_declare(
+      function_name_new,
+      list_join_comma_space(param_names),
+    );
     await ast_change_finish(
       string_combine_multiple([
         error("todo"),
