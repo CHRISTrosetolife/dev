621e98abdcfde8326c8846442ecdb08af784b0d0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 13:22:04 -0500

    2025-02-20T18:22:03.721Z watch   js_imports_fix

--- public/src/js_imports_fix.mjs
+++ public/src/js_imports_fix.mjs
@@ -1,8 +1,6 @@
-import { log } from "./log.mjs";
 import { js_imports_remove } from "./js_imports_remove.mjs";
 import { js_imports_add } from "./js_imports_add.mjs";
 export async function js_imports_fix(ast) {
   await js_imports_add(ast);
   js_imports_remove(ast);
-  log("here");
 }
