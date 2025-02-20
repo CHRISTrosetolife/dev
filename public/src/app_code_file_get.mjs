ebb3b1296443e2ff44a2f05f01b18a1fdcd508f2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 13:01:04 -0500

    2025-02-20T18:01:03.717Z watch   app_code_file_get

--- public/src/app_code_file_get.mjs
+++ public/src/app_code_file_get.mjs
@@ -1,8 +1,11 @@
+import { object_property_exists } from "./object_property_exists.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { app_code_files_get } from "./app_code_files_get.mjs";
 export async function app_code_file_get(path) {
   let files = await app_code_files_get();
-  if (false) {
+  if (!object_property_exists(files, path)) {
+    let v = null;
+    return v;
   }
   let file = object_property_get(files, path);
   return file;
