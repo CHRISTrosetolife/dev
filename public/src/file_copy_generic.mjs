4cb150e06c7dc3592d017a47c56392a5e32baa02
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 15:07:53 -0500

    2025-02-20T20:07:50.161Z watch   file_copy_generic

--- public/src/file_copy_generic.mjs
+++ public/src/file_copy_generic.mjs
@@ -1,18 +1,15 @@
 import { file_overwrite } from "./file_overwrite.mjs";
-import { global_files } from "./global_files.mjs";
 import { file_open } from "./file_open.mjs";
 import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
 import { import_node } from "./import_node.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { global_get } from "./global_get.mjs";
 import { web_is } from "./web_is.mjs";
 import { file_exists } from "./file_exists.mjs";
 import { assert_not_async } from "./assert_not_async.mjs";
+import { file_read } from "./file_read.mjs";
 export async function file_copy_generic(file_name_from, file_name_to, open) {
   await assert_not_async(file_exists, [file_name_to]);
   if (web_is()) {
-    let { [global_files()]: files } = global_get();
-    let existing = object_property_get(files, file_name_from);
+    let existing = await file_read(file_name_from);
     await file_overwrite(file_name_to, existing);
   } else {
     let fs = await import_node("fs");
