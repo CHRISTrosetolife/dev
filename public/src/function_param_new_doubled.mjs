d55b98ad4e2943d32c7f66f1fd5a6b7cf006797d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 17:12:48 -0500

    2025-02-20T22:12:47.360Z watch   function_param_new_doubled

--- public/src/function_param_new_doubled.mjs
+++ public/src/function_param_new_doubled.mjs
@@ -1,8 +1,4 @@
 import { function_param_new } from "./function_param_new.mjs";
-export async function function_param_new_doubled(
-  function_name,
-  param_name,
-  default_value_string,
-) {
-  await function_param_new(function_name, param_name, default_value_string);
+export async function function_param_new_doubled(function_name, param_name) {
+  await function_param_new(function_name, param_name, param_name);
 }
