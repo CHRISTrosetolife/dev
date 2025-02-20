8d1273ba64c1115522074c7bbd607a173d523443
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 15:43:04 -0500

    2025-02-20T20:43:03.184Z function_rename bible_interlinear_upload bible_interlinear_upload_greek

--- public/src/bible_interlinear_upload_greek.mjs
+++ public/src/bible_interlinear_upload_greek.mjs
@@ -1,6 +1,6 @@
 import { bible_interlinear_upload_generic } from "./bible_interlinear_upload_generic.mjs";
 import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
-export async function bible_interlinear_upload() {
+export async function bible_interlinear_upload_greek() {
   let language = "greek";
   let books_get = bible_interlinear_cache_new;
   await bible_interlinear_upload_generic(language, books_get);
