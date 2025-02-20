e3a131e3f5e1ecc2398c6b9ef5820b47f2d3bf0e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 15:30:51 -0500

    2025-02-20T20:30:48.528Z c function_param_delete_name function_param_move_first_name

---
+++ public/src/function_param_move_first_name.mjs
@@ -0,0 +1,13 @@
+import { function_param_delete_index } from "./function_param_delete_index.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
+import { list_index } from "./list_index.mjs";
+import { function_params_names } from "./function_params_names.mjs";
+export async function function_param_move_first_name(
+  function_name,
+  param_name_to_delete,
+) {
+  assert_arguments_length(arguments, 2);
+  let params_names = await function_params_names(function_name);
+  let index = list_index(params_names, param_name_to_delete);
+  await function_param_delete_index(function_name, index);
+}
