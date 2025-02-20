17974959390de074434f743c4a7919740e05a481
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 13:00:40 -0500

    2025-02-20T18:00:40.385Z watch   app_code_file_get

--- public/src/app_code_file_get.mjs
+++ public/src/app_code_file_get.mjs
@@ -2,6 +2,8 @@ import { object_property_get } from "./object_property_get.mjs";
 import { app_code_files_get } from "./app_code_files_get.mjs";
 export async function app_code_file_get(path) {
   let files = await app_code_files_get();
+  if (false) {
+  }
   let file = object_property_get(files, path);
   return file;
 }
