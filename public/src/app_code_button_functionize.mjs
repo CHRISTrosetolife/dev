b4df947a8b7dad14e58b69b7d418218e251f2306
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 11:30:16 -0500

    2025-02-20T16:30:16.319Z watch   app_code_button_functionize

--- public/src/app_code_button_functionize.mjs
+++ public/src/app_code_button_functionize.mjs
@@ -1,3 +1,7 @@
+import { app_code_button_rename_generic } from "./app_code_button_rename_generic.mjs";
+import { app_code_refresh_function_selection_one_get } from "./app_code_refresh_function_selection_one_get.mjs";
+import { fn_name } from "./fn_name.mjs";
+import { js_identifier_rename } from "./js_identifier_rename.mjs";
 import { error } from "./error.mjs";
 import { js_code_export_function_declare } from "./js_code_export_function_declare.mjs";
 import { list_remove_multiple_from } from "./list_remove_multiple_from.mjs";
@@ -33,6 +37,46 @@ export function app_code_button_functionize(
   menu_refresh,
   ast_change_finish,
 ) {
+  let selection_rename_text = "Selection rename";
+  let input_placeholder = "Name after";
+  let error_message = "Identifier already exists";
+  let invalid_list_get = function () {
+    let existing = js_identifiers_names(ast);
+    let v = existing;
+    return v;
+  };
+  async function on_click(on_overlay_result, after_value) {
+    js_identifier_rename(ast, on_overlay_result, after_value);
+    await ast_change_finish(
+      string_combine_multiple([
+        fn_name("js_identifier_rename"),
+        " ",
+        function_selected,
+        ' : "',
+        on_overlay_result,
+        '" to "',
+        after_value,
+        '"',
+      ]),
+    );
+  }
+  function on_overlay(d) {
+    let { node } =
+      app_code_refresh_function_selection_one_get(selection_result);
+    let before = object_property_get(node, "name");
+    html_p_text_multiple(d, ["Name before:", before]);
+    return before;
+  }
+  app_code_button_rename_generic(
+    overlay,
+    selection_rename_text,
+    menu_refresh,
+    on_overlay,
+    invalid_list_get,
+    input_placeholder,
+    error_message,
+    on_click,
+  );
   html_button(overlay, "Selection functionize", async function () {
     let s = js_identifiers_shadowed_names(ast);
     if (list_empty_not_is(s)) {
