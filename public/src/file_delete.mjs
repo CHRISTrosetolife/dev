9c6436813a436d0097e783bb2d37d09dec414341
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 15:00:09 -0500

    2025-02-20T20:00:06.955Z watch   file_delete

--- public/src/file_delete.mjs
+++ public/src/file_delete.mjs
@@ -1,5 +1,9 @@
+import { web_is } from "./web_is.mjs";
 import { import_node } from "./import_node.mjs";
 export async function file_delete(file_name) {
+  if (web_is()) {
+  }
   let fs = await import_node("fs");
-  return await fs.promises.rm(file_name);
+  let v = await fs.promises.rm(file_name);
+  return v;
 }
