7cd2bd6ac8da8ab36a9e0d3321dc1c1393e2356e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 16:45:14 -0500

    2025-02-20T21:45:12.503Z watch   app_code_button_copy_generic

--- public/src/app_code_button_copy_generic.mjs
+++ public/src/app_code_button_copy_generic.mjs
@@ -1,11 +1,12 @@
+import { app_code_function_selected_get } from "./app_code_function_selected_get.mjs";
 import { function_run } from "./function_run.mjs";
 import { app_code_refresh_function_change } from "./app_code_refresh_function_change.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { list_between } from "./list_between.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { functions_names } from "./functions_names.mjs";
-import { noop } from "./noop.mjs";
 import { app_code_button_rename_generic } from "./app_code_button_rename_generic.mjs";
+import { html_p_text } from "./html_p_text.mjs";
 export function app_code_button_copy_generic(
   context,
   overlay,
@@ -16,15 +17,21 @@ export function app_code_button_copy_generic(
   ast_change_finish,
   overlay_remove,
 ) {
+  let function_selected = app_code_function_selected_get(context);
   app_code_button_rename_generic(
     overlay,
     button_text,
     menu_refresh,
-    noop,
+    function (d) {
+      html_p_text(
+        d,
+        string_combine_multiple(["Function: ", function_selected]),
+      );
+    },
     functions_names,
     "Function name new",
     "Function already exists",
-    async function (function_selected, after_value) {
+    async function (on_overlay_result, after_value) {
       let fn_args = fn_args_get(after_value);
       await function_run(f_name, fn_args);
       await ast_change_finish(
