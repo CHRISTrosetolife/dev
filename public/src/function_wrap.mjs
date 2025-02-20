aecccfe1f300f084a267f7b10dee55d4ec5e1171
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 16:54:19 -0500

    2025-02-20T21:54:18.144Z watch   function_wrap

--- public/src/function_wrap.mjs
+++ public/src/function_wrap.mjs
@@ -1,8 +1,10 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { js_declaration_to_params } from "./js_declaration_to_params.mjs";
 import { function_declaration } from "./function_declaration.mjs";
 export async function function_wrap(function_name, function_name_wrapped) {
   let d = await function_declaration(function_name);
   let params = js_declaration_to_params(d);
+  let async = object_property_get(d, "async");
   let v = d;
   return v;
 }
