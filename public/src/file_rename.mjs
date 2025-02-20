8cded66af5c998701e015ba9d25a7c715b1b37e0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 14:58:44 -0500

    2025-02-20T19:58:42.816Z watch   file_rename

--- public/src/file_rename.mjs
+++ public/src/file_rename.mjs
@@ -1,3 +1,4 @@
+import { file_copy } from "./file_copy.mjs";
 import { web_is } from "./web_is.mjs";
 import { file_rename_on_renames } from "./file_rename_on_renames.mjs";
 import { each_async } from "./each_async.mjs";
@@ -5,7 +6,7 @@ import { import_node } from "./import_node.mjs";
 import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
 export async function file_rename(file_name_from, file_name_to) {
   if (web_is()) {
-    return;
+    await file_copy();
   } else {
     let fs = await import_node("fs");
     await folder_parent_exists_ensure(file_name_to);
