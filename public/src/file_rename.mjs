cc0627ca96006a6f6e69b5febe8417aa454be99b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 14:57:59 -0500

    2025-02-20T19:57:55.920Z watch   file_rename

--- public/src/file_rename.mjs
+++ public/src/file_rename.mjs
@@ -6,6 +6,7 @@ import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
 export async function file_rename(file_name_from, file_name_to) {
   if (web_is()) {
     return;
+  } else {
   }
   let fs = await import_node("fs");
   await folder_parent_exists_ensure(file_name_to);
