7cf6dba7270a138cf39e67dedc54f0c19498836f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 17:39:50 -0500

    2025-02-20T22:39:47.301Z watch   watch_lock

--- public/src/watch_lock.mjs
+++ public/src/watch_lock.mjs
@@ -1,6 +1,8 @@
 import { file_write_empty } from "./file_write_empty.mjs";
 import { uuid_file } from "./uuid_file.mjs";
 export async function watch_lock(lambda) {
+  if (false) {
+  }
   let v2 = await uuid_file(watch_lock, async function (file_path) {
     await file_write_empty(file_path);
     let v = await lambda();
