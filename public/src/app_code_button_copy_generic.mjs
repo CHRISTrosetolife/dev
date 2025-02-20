807a83f8dc94219a5125a46f55501254499d5ed0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 15:28:56 -0500

    2025-02-20T20:28:55.076Z watch   app_code_refresh_function_menu

--- public/src/app_code_button_copy_generic.mjs
+++ public/src/app_code_button_copy_generic.mjs
@@ -32,7 +32,8 @@ export function app_code_button_copy_generic(
           list_between(list_concat([f_name], fn_args), " "),
         ),
       );
-      await app_code_refresh_function_change(after_value);
+      await app_code_refresh_function_change(
+        context,after_value);
       overlay_remove();
     },
   );
