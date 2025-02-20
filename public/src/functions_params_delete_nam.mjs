15d401974769c7c2c071cbab41f7f56ea989decd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 11:51:58 -0500

    2025-02-20T16:51:58.067Z watch   functions_params_delete_nam

--- public/src/functions_params_delete_nam.mjs
+++ public/src/functions_params_delete_nam.mjs
@@ -1,7 +1,7 @@
 import { function_params_delete_name } from "./function_params_delete_name.mjs";
 import { each_async } from "./each_async.mjs";
-export async function functions_params_delete_nam() {
-  await each_async(list, async function (item) {
+export async function functions_params_delete_nam(function_names) {
+  await each_async(function_names, async function (function_name) {
     await function_params_delete_name();
   });
 }
