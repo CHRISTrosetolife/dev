b0975f00817d666a62fd172573954542cc7765c0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 14:48:02 -0500

    2025-02-20T19:47:58.197Z watch   folder_parent_exists_ensure

--- public/src/folder_parent_exists_ensure.mjs
+++ public/src/folder_parent_exists_ensure.mjs
@@ -1,6 +1,8 @@
 import { folder_parent } from "./folder_parent.mjs";
 import { folder_exists_ensure } from "./folder_exists_ensure.mjs";
 export async function folder_parent_exists_ensure(file_name) {
+  if (false) {
+  }
   let parent = await folder_parent(file_name);
   await folder_exists_ensure(parent);
 }
