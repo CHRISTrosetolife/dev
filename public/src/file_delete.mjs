3ee49af73f7e1852b35f44b3de39766d3aeb9ae0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 15:26:05 -0500

    2025-02-20T20:26:03.513Z watch   file_delete

--- public/src/file_delete.mjs
+++ public/src/file_delete.mjs
@@ -1,8 +1,4 @@
-import { app_code_property_path } from "./app_code_property_path.mjs";
-import { app_code_property_deleted } from "./app_code_property_deleted.mjs";
-import { app_code_property_contents } from "./app_code_property_contents.mjs";
-import { list_add } from "./list_add.mjs";
-import { global_file_changes } from "./global_file_changes.mjs";
+import { global_file_changes_add } from "./global_file_changes_add.mjs";
 import { global_get } from "./global_get.mjs";
 import { global_files } from "./global_files.mjs";
 import { web_is } from "./web_is.mjs";
@@ -12,12 +8,7 @@ export async function file_delete(file_path) {
   if (web_is()) {
     let { [global_files()]: files } = global_get();
     object_property_delete(files, file_path);
-    let fcs = global_file_changes();
-    list_add(fcs, {
-      [app_code_property_path()]: file_path,
-      [app_code_property_contents()]: null,
-      [app_code_property_deleted()]: true,
-    });
+    global_file_changes_add(file_path, null, true);
     return;
   }
   let fs = await import_node("fs");
