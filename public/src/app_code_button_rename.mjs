baf5f9307438416c9ce9324f01da2a69d5d17e2f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 11:27:28 -0500

    2025-02-20T16:27:28.553Z watch   app_code_button_rename

--- public/src/app_code_button_rename.mjs
+++ public/src/app_code_button_rename.mjs
@@ -1,16 +1,11 @@
-import { html_focus } from "./html_focus.mjs";
+import { app_code_button_rename_generic } from "./app_code_button_rename_generic.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { js_identifier_rename } from "./js_identifier_rename.mjs";
-import { html_value_get } from "./html_value_get.mjs";
-import { html_conditions_letters_numbers_underscores_spaces_list_includes_not } from "./html_conditions_letters_numbers_underscores_spaces_list_includes_not.mjs";
-import { html_input_validated_button } from "./html_input_validated_button.mjs";
 import { js_identifiers_names } from "./js_identifiers_names.mjs";
 import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { app_code_refresh_function_selection_one_get } from "./app_code_refresh_function_selection_one_get.mjs";
-import { html_overlay_container } from "./html_overlay_container.mjs";
-import { html_button } from "./html_button.mjs";
 export function app_code_button_rename(
   overlay,
   menu_refresh,
@@ -27,27 +22,16 @@ export function app_code_button_rename(
     let v = existing;
     return v;
   };
-  html_button(overlay, selection_rename_text, async function () {
-    let d = html_overlay_container(overlay, menu_refresh);
-    let on_overlay_result = on_overlay(d);
-    let invalid = invalid_list_get();
-    let input;
-    let after = html_input_validated_button(
-      d,
-      input_placeholder,
-      html_conditions_letters_numbers_underscores_spaces_list_includes_not(
-        error_message,
-        invalid,
-      ),
-      selection_rename_text,
-      async function () {
-        let after_value = html_value_get(input);
-        await on_click(on_overlay_result, after_value);
-      },
-    );
-    input = object_property_get(after, "input");
-    html_focus(input);
-  });
+  app_code_button_rename_generic(
+    overlay,
+    selection_rename_text,
+    menu_refresh,
+    on_overlay,
+    invalid_list_get,
+    input_placeholder,
+    error_message,
+    on_click,
+  );
   async function on_click(on_overlay_result, after_value) {
     js_identifier_rename(ast, on_overlay_result, after_value);
     await ast_change_finish(
---
+++ public/src/app_code_button_rename_generic.mjs
@@ -0,0 +1,39 @@
+import { html_focus } from "./html_focus.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { html_value_get } from "./html_value_get.mjs";
+import { html_conditions_letters_numbers_underscores_spaces_list_includes_not } from "./html_conditions_letters_numbers_underscores_spaces_list_includes_not.mjs";
+import { html_input_validated_button } from "./html_input_validated_button.mjs";
+import { html_overlay_container } from "./html_overlay_container.mjs";
+import { html_button } from "./html_button.mjs";
+export function app_code_button_rename_generic(
+  overlay,
+  selection_rename_text,
+  menu_refresh,
+  on_overlay,
+  invalid_list_get,
+  input_placeholder,
+  error_message,
+  on_click,
+) {
+  html_button(overlay, selection_rename_text, async function () {
+    let d = html_overlay_container(overlay, menu_refresh);
+    let on_overlay_result = on_overlay(d);
+    let invalid = invalid_list_get();
+    let input;
+    let after = html_input_validated_button(
+      d,
+      input_placeholder,
+      html_conditions_letters_numbers_underscores_spaces_list_includes_not(
+        error_message,
+        invalid,
+      ),
+      selection_rename_text,
+      async function () {
+        let after_value = html_value_get(input);
+        await on_click(on_overlay_result, after_value);
+      },
+    );
+    input = object_property_get(after, "input");
+    html_focus(input);
+  });
+}
