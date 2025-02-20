14550f3b665fa7250376a5483d4ca5e298b2e2ef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 14:55:59 -0500

    2025-02-20T19:55:55.291Z watch   file_rename

--- public/src/file_rename.mjs
+++ public/src/file_rename.mjs
@@ -3,11 +3,13 @@ import { each_async } from "./each_async.mjs";
 import { import_node } from "./import_node.mjs";
 import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
 export async function file_rename(file_name_from, file_name_to) {
+  if (false) {
+  }
   let fs = await import_node("fs");
   await folder_parent_exists_ensure(file_name_to);
   await fs.promises.rename(file_name_from, file_name_to);
   let on_renames = file_rename_on_renames();
-  await each_async(on_renames, async (on_renames) => {
+  await each_async(on_renames, async function (on_renames) {
     await on_renames(file_name_from, file_name_to);
   });
 }
