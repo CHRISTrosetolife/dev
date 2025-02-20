f2c733b00b4e20cea96a373029bdaed6926cd51a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 15:32:31 -0500

    2025-02-20T20:32:28.439Z o al

--- public/src/function_param_move_first_name.mjs
+++ public/src/function_param_move_first_name.mjs
@@ -1,5 +1,5 @@
+import { function_param_move_first_index } from "./function_param_move_first_index.mjs";
 import { function_param_index } from "./function_param_index.mjs";
-import { function_param_delete_index } from "./function_param_delete_index.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function function_param_move_first_name(
   function_name,
@@ -7,4 +7,5 @@ export async function function_param_move_first_name(
 ) {
   assert_arguments_length(arguments, 2);
   let index = await function_param_index(function_name, param_name_to_move);
-  await function_param_move_first_index(function_name, index)
+  await function_param_move_first_index(function_name, index);
+}
