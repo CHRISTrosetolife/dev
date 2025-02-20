9e20bda19c4090120022fd5c21dc859c7616f132
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 12:55:40 -0500

    2025-02-20T17:55:39.826Z watch   app_code_refresh_function_menu

--- public/src/app_code_refresh_function_menu.mjs
+++ public/src/app_code_refresh_function_menu.mjs
@@ -1,14 +1,8 @@
+import { app_code_user_upload_batch } from "./app_code_user_upload_batch.mjs";
 import { app_code_function_selected_get } from "./app_code_function_selected_get.mjs";
 import { app_code_button_variablize } from "./app_code_button_variablize.mjs";
 import { app_code_button_functionize } from "./app_code_button_functionize.mjs";
 import { app_code_button_rename } from "./app_code_button_rename.mjs";
-import { app_code_latest_object } from "./app_code_latest_object.mjs";
-import { app_code_file_name_latest } from "./app_code_file_name_latest.mjs";
-import { app_code_user_upload } from "./app_code_user_upload.mjs";
-import { app_code_batch_name_when } from "./app_code_batch_name_when.mjs";
-import { date_now } from "./date_now.mjs";
-import { app_code_property_message } from "./app_code_property_message.mjs";
-import { app_code_batch_path_get } from "./app_code_batch_path_get.mjs";
 import { app_code_property_contents } from "./app_code_property_contents.mjs";
 import { object_properties_new } from "./object_properties_new.mjs";
 import { object_property_set } from "./object_property_set.mjs";
@@ -93,20 +87,3 @@ export function app_code_refresh_function_menu(arg) {
     overlay_remove();
   }
 }
-async function app_code_user_upload_batch(files, batch_message, context) {
-    let batch_path_previous = await app_code_batch_path_get();
-    let batch_new = {
-        files,
-        batch_path_previous,
-        [app_code_property_message()]: batch_message,
-    };
-    let when = date_now();
-    let batch_name = await app_code_batch_name_when(when);
-    let batch_path = await app_code_user_upload(context, batch_name, batch_new);
-    await app_code_user_upload(
-        context,
-        app_code_file_name_latest(),
-        app_code_latest_object(batch_path, when)
-    );
-}
-
