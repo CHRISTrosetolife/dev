a1580ed728e76d9b60ef8abe1d7d68d1dc9b4d23
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-20 16:52:25 -0500

    2025-02-20T21:52:24.245Z watch   function_wrap

--- public/src/function_wrap.mjs
+++ public/src/function_wrap.mjs
@@ -1,1 +1,4 @@
-export function function_wrap(function_name, function_name_wrapped) {}
+import { function_params } from "./function_params.mjs";
+export async function function_wrap(function_name, function_name_wrapped) {
+  let params = await function_params(function_name);
+}
