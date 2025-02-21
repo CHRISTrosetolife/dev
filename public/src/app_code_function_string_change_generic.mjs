c9bc7343b83b443d9baaf8ea4d482502d380a6d8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-21 15:54:35 -0500

    2025-02-21T20:54:34.152Z watch   app_code_function_string_change_generic

--- public/src/app_code_function_string_change_generic.mjs
+++ public/src/app_code_function_string_change_generic.mjs
@@ -5,7 +5,6 @@ import { fn_name } from "./fn_name.mjs";
 import { js_unparse } from "./js_unparse.mjs";
 import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
 import { app_code_function_action } from "./app_code_function_action.mjs";
-import { html_button } from "./html_button.mjs";
 export function app_code_function_string_change_generic(
   context,
   overlay,
@@ -17,32 +16,30 @@ export function app_code_function_string_change_generic(
   input_placeholder,
   node,
 ) {
-  html_button(overlay, button_text, async function () {
-    app_code_function_action(
-      context,
-      overlay,
-      button_text,
-      menu_refresh,
-      function on_overlay(node) {
-        html_p_text_multiple(overlay, ["Old value:", js_unparse(node)]);
-        let value_old = node_to_input_value(node);
-        let v4 = {
-          [fn_name("app_code_function_action")]: {
-            input_value: value_old,
-          },
-        };
-        return v4;
-      },
-      list_empty_arrow(),
-      input_placeholder,
-      async function (on_overlay_result, value_new) {
-        let node_new = input_value_to_node(value_new);
-        let node_code_old = js_unparse(node);
-        object_replace(node, node_new);
-        await ast_change_finish(
-          list_join_space([node_code_old, "to", js_unparse(node_new)]),
-        );
-      },
-    );
-  });
+  app_code_function_action(
+    context,
+    overlay,
+    button_text,
+    menu_refresh,
+    function on_overlay(node) {
+      html_p_text_multiple(overlay, ["Old value:", js_unparse(node)]);
+      let value_old = node_to_input_value(node);
+      let v4 = {
+        [fn_name("app_code_function_action")]: {
+          input_value: value_old,
+        },
+      };
+      return v4;
+    },
+    list_empty_arrow(),
+    input_placeholder,
+    async function (on_overlay_result, value_new) {
+      let node_new = input_value_to_node(value_new);
+      let node_code_old = js_unparse(node);
+      object_replace(node, node_new);
+      await ast_change_finish(
+        list_join_space([node_code_old, "to", js_unparse(node_new)]),
+      );
+    },
+  );
 }
