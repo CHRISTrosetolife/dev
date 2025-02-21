0408ef0aa242cc489941c8f1c3cd50fdd6c0650c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-21 14:12:31 -0500

    2025-02-21T19:12:29.107Z watch   js_string

--- public/src/js_string.mjs
+++ public/src/js_string.mjs
@@ -1,8 +1,8 @@
+import { js_code_string } from "./js_code_string.mjs";
 import { js_value_string_set } from "./js_value_string_set.mjs";
-import { string_delimit } from "./string_delimit.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 export function js_string(s) {
-  let code2 = js_parse_expression(string_delimit(""));
+  let code2 = js_parse_expression(js_code_string(""));
   js_value_string_set(code2, s);
   return code2;
 }
