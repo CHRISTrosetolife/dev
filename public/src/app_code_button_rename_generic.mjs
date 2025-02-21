576345dc66ace48fcb12ae1f2efe41dae2bfe778
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-21 14:36:27 -0500

    2025-02-21T19:36:26.380Z watch   app_code_button_rename_generic

--- public/src/app_code_button_rename_generic.mjs
+++ public/src/app_code_button_rename_generic.mjs
@@ -1,11 +1,5 @@
-import { html_input_validated_button_focus } from "./html_input_validated_button_focus.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { html_p_text } from "./html_p_text.mjs";
-import { app_code_function_selected_get } from "./app_code_function_selected_get.mjs";
-import { html_value_get } from "./html_value_get.mjs";
+import { app_code_function_action } from "./app_code_function_action.mjs";
 import { html_conditions_letters_numbers_underscores_spaces_list_includes_not } from "./html_conditions_letters_numbers_underscores_spaces_list_includes_not.mjs";
-import { html_overlay_container } from "./html_overlay_container.mjs";
-import { html_button } from "./html_button.mjs";
 export function app_code_button_rename_generic(
   context,
   overlay,
@@ -17,32 +11,16 @@ export function app_code_button_rename_generic(
   error_message,
   on_click,
 ) {
-  html_button(overlay, button_text, async function () {
-    let function_selected = app_code_function_selected_get(context);
-    let d = html_overlay_container(overlay, menu_refresh);
-    html_p_text(
-      d,
-      string_combine_multiple([
-        "Function: ",
-        function_selected,
-        " : ",
-        button_text,
-      ]),
-    );
-    let on_overlay_result = on_overlay(d);
-    let conditions = await conditions_get();
-    let input = html_input_validated_button_focus(
-      d,
-      input_placeholder,
-      conditions,
-      button_text,
-      on_submit,
-    );
-    async function on_submit() {
-      let after_value = html_value_get(input);
-      await on_click(on_overlay_result, after_value);
-    }
-  });
+  app_code_function_action(
+    overlay,
+    button_text,
+    context,
+    menu_refresh,
+    on_overlay,
+    conditions_get,
+    input_placeholder,
+    on_click,
+  );
   async function conditions_get() {
     let invalid = await invalid_list_get();
     let conditions =
---
+++ public/src/app_code_function_action.mjs
@@ -0,0 +1,44 @@
+import { html_value_get } from "./html_value_get.mjs";
+import { html_input_validated_button_focus } from "./html_input_validated_button_focus.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { html_p_text } from "./html_p_text.mjs";
+import { html_overlay_container } from "./html_overlay_container.mjs";
+import { app_code_function_selected_get } from "./app_code_function_selected_get.mjs";
+import { html_button } from "./html_button.mjs";
+export function app_code_function_action(
+  overlay,
+  button_text,
+  context,
+  menu_refresh,
+  on_overlay,
+  conditions_get,
+  input_placeholder,
+  on_click,
+) {
+  html_button(overlay, button_text, async function () {
+    let function_selected = app_code_function_selected_get(context);
+    let d = html_overlay_container(overlay, menu_refresh);
+    html_p_text(
+      d,
+      string_combine_multiple([
+        "Function: ",
+        function_selected,
+        " : ",
+        button_text,
+      ]),
+    );
+    let on_overlay_result = on_overlay(d);
+    let conditions = await conditions_get();
+    let input = html_input_validated_button_focus(
+      d,
+      input_placeholder,
+      conditions,
+      button_text,
+      on_submit,
+    );
+    async function on_submit() {
+      let after_value = html_value_get(input);
+      await on_click(on_overlay_result, after_value);
+    }
+  });
+}
