9ee576c41038a97ce5cdb075b352ed5318152412
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-21 16:10:57 -0500

    2025-02-21T21:10:54.416Z watch   bible_interlinear_definition_generic

--- public/src/bible_interlinear_definition_generic.mjs
+++ public/src/bible_interlinear_definition_generic.mjs
@@ -1,3 +1,4 @@
+import { string_empty_not_is } from "./string_empty_not_is.mjs";
 import { list_map } from "./list_map.mjs";
 import { assert } from "./assert.mjs";
 import { equal } from "./equal.mjs";
@@ -14,24 +15,28 @@ export async function bible_interlinear_definition_generic(
   strong,
   labels,
 ) {
+  assert(string_empty_not_is, [strong]);
   let html = await html_lambda(language, strong);
   let p = await html_parse(html);
   let leftbox = html_parse_visit_id(p, "leftbox");
   let tophdg = html_parse_visit_class_list(leftbox, "tophdg");
-  let mapped = list_map(labels, (label) => {
-    return label_to_text(label);
+  let mapped = list_map(labels, function (label) {
+    let v = label_to_text(label);
+    return v;
   });
   let definition = list_join_comma_space(mapped);
   let word = label_to_text("Original Word:");
-  return {
+  let v2 = {
     definition,
     word,
   };
+  return v2;
   function label_to_text(label) {
-    let definition = list_find(tophdg, (t) => {
+    let definition = list_find(tophdg, function (t) {
       let t_text = html_parse_text(t);
       t_text = string_trim_whitespace(t_text);
-      return equal(t_text, label);
+      let v3 = equal(t_text, label);
+      return v3;
     });
     let definition_text = html_parse_text(definition);
     definition_text = string_trim_whitespace(definition_text);
