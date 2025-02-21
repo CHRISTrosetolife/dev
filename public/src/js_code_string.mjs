30b56b0356f23b022a10a9da2ed818fe2c45cfd8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-21 14:10:35 -0500

    2025-02-21T19:10:34.201Z watch   js_code_string

--- public/src/js_code_string.mjs
+++ public/src/js_code_string.mjs
@@ -1,7 +1,10 @@
-import { js_string } from "./js_string.mjs";
+import { js_value_string_set } from "./js_value_string_set.mjs";
+import { string_delimit } from "./string_delimit.mjs";
+import { js_parse_expression } from "./js_parse_expression.mjs";
 import { js_unparse } from "./js_unparse.mjs";
 export function js_code_string(s) {
-  let code = js_string(s);
+  let code = js_parse_expression(string_delimit(""));
+  js_value_string_set(code, s);
   let unparsed = js_unparse(code);
   return unparsed;
 }
