cb24dad3d2e3b7ee8f0105bbf698e152a5987f8e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-21 16:08:00 -0500

    2025-02-21T21:07:57.161Z bible_interlinear_upload_hebrew Imports fix

--- public/src/bible_interlinear_upload_hebrew.mjs
+++ public/src/bible_interlinear_upload_hebrew.mjs
@@ -1,5 +1,5 @@
+import { bible_interlinear_cache_old } from "./bible_interlinear_cache_old.mjs";
 import { bible_interlinear_upload_generic } from "./bible_interlinear_upload_generic.mjs";
-import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
 export async function bible_interlinear_upload_hebrew() {
   let language = "hebrew";
   let books_get = bible_interlinear_cache_old;
