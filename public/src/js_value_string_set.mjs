f4115001f62221b2bfe2e32257d6a5719600b524
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-21 13:45:40 -0500

    2025-02-21T18:45:39.165Z watch   js_value_string_set

--- public/src/js_value_string_set.mjs
+++ public/src/js_value_string_set.mjs
@@ -1,6 +1,6 @@
 import { object_property_delete } from "./object_property_delete.mjs";
-export function js_value_string_set(expression, value) {
-  expression.value = value;
+export function js_value_string_set(node, value) {
+  node.value = value;
   let property = "raw";
-  object_property_delete(expression, property);
+  object_property_delete(node, property);
 }
