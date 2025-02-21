49ab1053c0bfd8aeec527b3029929548daa0ff7a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-21 14:13:10 -0500

    2025-02-21T19:13:05.730Z watch   js_string

---
+++ public/src/js_code_string_empty.mjs
@@ -0,0 +1,4 @@
+import { js_code_string } from "./js_code_string.mjs";
+export function js_code_string_empty() {
+  return js_code_string("");
+}
--- public/src/js_string.mjs
+++ public/src/js_string.mjs
@@ -1,8 +1,8 @@
-import { js_code_string } from "./js_code_string.mjs";
+import { js_code_string_empty } from "./js_code_string_empty.mjs";
 import { js_value_string_set } from "./js_value_string_set.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 export function js_string(s) {
-  let code2 = js_parse_expression(js_code_string(""));
+  let code2 = js_parse_expression(js_code_string_empty());
   js_value_string_set(code2, s);
   return code2;
 }
