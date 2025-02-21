78d942e460774c4c37f4a7f92f600e75f39884c2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-21 14:14:59 -0500

    2025-02-21T19:14:45.825Z bible_interlinear_upload_hebrew js_value_string_set greek to hebrew

--- public/src/bible_interlinear_upload_hebrew.mjs
+++ public/src/bible_interlinear_upload_hebrew.mjs
@@ -1,7 +1,7 @@
 import { bible_interlinear_upload_generic } from "./bible_interlinear_upload_generic.mjs";
 import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
 export async function bible_interlinear_upload_hebrew() {
-  let language = "greek";
+  let language = "hebrew";
   let books_get = bible_interlinear_cache_new;
   await bible_interlinear_upload_generic(language, books_get);
 }
