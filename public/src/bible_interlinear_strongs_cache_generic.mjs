2a1a17454b3b6e2a23320243101bf545a3b0610c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-21 16:09:50 -0500

    2025-02-21T21:09:48.776Z watch   bible_interlinear_strongs_cache_generic

--- public/src/bible_interlinear_strongs_cache_generic.mjs
+++ public/src/bible_interlinear_strongs_cache_generic.mjs
@@ -1,3 +1,5 @@
+import { never } from "./never.mjs";
+import { assert } from "./assert.mjs";
 import { http_cache } from "./http_cache.mjs";
 import { url_secure_w3 } from "./url_secure_w3.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
@@ -6,6 +8,7 @@ export async function bible_interlinear_strongs_cache_generic(
   language,
   strong,
 ) {
+  assert(never, []);
   let url = string_combine_multiple([
     url_secure_w3(),
     url_base,
@@ -14,5 +17,6 @@ export async function bible_interlinear_strongs_cache_generic(
     strong,
     ".htm",
   ]);
-  return await http_cache(url);
+  let v = await http_cache(url);
+  return v;
 }
