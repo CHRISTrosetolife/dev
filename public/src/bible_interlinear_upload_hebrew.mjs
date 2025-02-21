9d5dfd4e8b8f434a941ccd26adea9e51c03d05f2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-21 16:03:05 -0500

    2025-02-21T21:03:04.435Z bible_interlinear_upload_hebrew bible_interlinear_cache_new to bible_interlinear_cache_old

--- public/src/bible_interlinear_upload_hebrew.mjs
+++ public/src/bible_interlinear_upload_hebrew.mjs
@@ -2,6 +2,6 @@ import { bible_interlinear_upload_generic } from "./bible_interlinear_upload_gen
 import { bible_interlinear_cache_new } from "./bible_interlinear_cache_new.mjs";
 export async function bible_interlinear_upload_hebrew() {
   let language = "hebrew";
-  let books_get = bible_interlinear_cache_new;
+  let books_get = bible_interlinear_cache_old;
   await bible_interlinear_upload_generic(language, books_get);
 }
