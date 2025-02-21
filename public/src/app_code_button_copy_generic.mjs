c5a8b3a1e860338aade75abd933315f753b826ee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-21 13:58:05 -0500

    2025-02-21T18:58:04.775Z watch   app_code_button_copy_generic

--- public/src/app_code_button_copy_generic.mjs
+++ public/src/app_code_button_copy_generic.mjs
@@ -2,10 +2,9 @@ import { noop } from "./noop.mjs";
 import { function_run } from "./function_run.mjs";
 import { app_code_refresh_function_change } from "./app_code_refresh_function_change.mjs";
 import { list_concat } from "./list_concat.mjs";
-import { list_between } from "./list_between.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { functions_names } from "./functions_names.mjs";
 import { app_code_button_rename_generic } from "./app_code_button_rename_generic.mjs";
+import { list_join_space } from "./list_join_space.mjs";
 export function app_code_button_copy_generic(
   context,
   overlay,
@@ -29,9 +28,7 @@ export function app_code_button_copy_generic(
       let fn_args = fn_args_get(after_value);
       await function_run(f_name, fn_args);
       await ast_change_finish(
-        string_combine_multiple(
-          list_between(list_concat([f_name], fn_args), " "),
-        ),
+        list_join_space(list_concat([f_name], fn_args)),
         after_value,
       );
       await app_code_refresh_function_change(context, after_value);
