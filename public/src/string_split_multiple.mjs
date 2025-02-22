f3ee59621c65396b928eedeeb20ae7b597217f8c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 12:11:40 -0500

    2025-02-22T17:11:36.509Z watch   string_split_multiple

--- public/src/string_split_multiple.mjs
+++ public/src/string_split_multiple.mjs
@@ -3,10 +3,10 @@ import { each } from "./each.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_multiple_is } from "./list_multiple_is.mjs";
 import { list_skip } from "./list_skip.mjs";
-export function string_split_multiple(input, tokens) {
-  assert(list_multiple_is, [tokens]);
-  let temp_character = list_first(tokens);
-  each(list_skip(tokens, 1), (token) => {
+export function string_split_multiple(input, splits) {
+  assert(list_multiple_is, [splits]);
+  let temp_character = list_first(splits);
+  each(list_skip(splits, 1), function (token) {
     input = input.split(token).join(temp_character);
   });
   input = input.split(temp_character);
