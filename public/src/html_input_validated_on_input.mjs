6fa2d3792e6a6b7faf35a1e08c98ca0949dd9963
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 17:02:33 -0500

    2025-02-22T22:02:32.709Z watch   html_input_validated_on_input

--- public/src/html_input_validated_on_input.mjs
+++ public/src/html_input_validated_on_input.mjs
@@ -1,4 +1,4 @@
 import { object_property_get } from "./object_property_get.mjs";
-export function html_input_validated_on_input(input_username) {
-  object_property_get(input_username, "on_input")();
+export function html_input_validated_on_input(component_input) {
+  object_property_get(component_input, "on_input")();
 }
