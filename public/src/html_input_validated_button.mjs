37dde619408beda58ec782865afe3e70d665b02d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 17:06:36 -0500

    2025-02-22T22:06:34.189Z watch   html_input_validated_button

--- public/src/html_input_validated_button.mjs
+++ public/src/html_input_validated_button.mjs
@@ -5,6 +5,7 @@ import { html_input_validated_on_input_lambda_initial } from "./html_input_valid
 import { html_value_get } from "./html_value_get.mjs";
 import { html_button } from "./html_button.mjs";
 import { html_input_validated } from "./html_input_validated.mjs";
+import { map_new } from "./map_new.mjs";
 export function html_input_validated_button(
   root,
   placeholder,
@@ -18,6 +19,7 @@ export function html_input_validated_button(
     html_on_keydown_enter(input, on_click);
   });
   let button = html_button(root, button_text, on_click);
+  let m = map_new();
   html_input_validated_on_input_lambda_initial(input, function (valid) {
     html_button_enable_if(button, valid);
   });
