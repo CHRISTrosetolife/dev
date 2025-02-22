c1d7c61006c0d01967e9336247773404d27d349a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 13:14:34 -0500

    2025-02-22T18:14:31.579Z watch   string_case_upper_first

--- public/src/string_case_upper_first.mjs
+++ public/src/string_case_upper_first.mjs
@@ -1,10 +1,11 @@
+import { string_first } from "./string_first.mjs";
 import { string_skip } from "./string_skip.mjs";
-import { string_take } from "./string_take.mjs";
 import { string_case_upper } from "./string_case_upper.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function string_case_upper_first(hv_name) {
-  return string_combine_multiple([
-    string_case_upper(string_take(hv_name, 1)),
+  let v = string_combine_multiple([
+    string_case_upper(string_first(hv_name)),
     string_skip(hv_name, 1),
   ]);
+  return v;
 }
