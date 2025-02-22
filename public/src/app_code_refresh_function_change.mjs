414515165ff6f898667ac7566f2d3f24f4bacc9e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 15:19:42 -0500

    2025-02-22T20:19:40.592Z watch   app_code_refresh_function_change

--- public/src/app_code_refresh_function_change.mjs
+++ public/src/app_code_refresh_function_change.mjs
@@ -1,9 +1,9 @@
-import { never } from "./never.mjs";
+import { string_is } from "./string_is.mjs";
 import { assert } from "./assert.mjs";
 import { app_code_refresh_function } from "./app_code_refresh_function.mjs";
 import { storage_local_set_context } from "./storage_local_set_context.mjs";
 export async function app_code_refresh_function_change(context, function_name) {
-  assert(never, []);
+  assert(string_is, [function_name]);
   storage_local_set_context(context, "function_selected", function_name);
   await app_code_refresh_function(context);
 }
