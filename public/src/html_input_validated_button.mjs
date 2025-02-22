69086cdf3c76ac426cc2a6ec630d285de4806bda
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 17:37:12 -0500

    2025-02-22T22:37:12.128Z watch   html_input_validated_button

--- public/src/html_input_validated_button.mjs
+++ public/src/html_input_validated_button.mjs
@@ -1,13 +1,5 @@
-import { map_exists } from "./map_exists.mjs";
-import { each } from "./each.mjs";
-import { html_on_keydown_enter } from "./html_on_keydown_enter.mjs";
-import { html_button_enable_if } from "./html_button_enable_if.mjs";
-import { html_input_validated_on_input_lambda_initial } from "./html_input_validated_on_input_lambda_initial.mjs";
-import { html_value_get } from "./html_value_get.mjs";
-import { html_button } from "./html_button.mjs";
+import { html_inputs_validated } from "./html_inputs_validated.mjs";
 import { html_input_validated } from "./html_input_validated.mjs";
-import { map_new } from "./map_new.mjs";
-import { map_set } from "./map_set.mjs";
 import { map_get } from "./map_get.mjs";
 export function html_input_validated_button(
   root,
@@ -18,38 +10,12 @@ export function html_input_validated_button(
 ) {
   let input = html_input_validated(root, placeholder, conditions);
   let inputs = [input];
-  each(inputs, function (input) {
-    html_on_keydown_enter(input, on_click);
-  });
-  let button = html_button(root, button_text, on_click);
-  let m = map_new();
-  each(inputs, function (input) {
-    html_input_validated_on_input_lambda_initial(input, function (valid) {
-      map_set(m, input, valid);
-      let valid_all = true;
-      each(inputs, function (input) {
-        if (map_exists(input)) {
-          if (map_get(m, input) === false) {
-            valid_all = false;
-          }
-        } else {
-          valid_all = false;
-        }
-        if (!valid_all) {
-          let v2 = true;
-          return v2;
-        }
-      });
-      html_button_enable_if(button, valid_all);
-    });
-  });
-  function on_click() {
-    let values = map_new();
-    each(inputs, function (input) {
-      map_set(values, html_value_get(input));
-    });
-    on_click_lambda(values);
-  }
+  let button = html_inputs_validated(
+    inputs,
+    root,
+    button_text,
+    on_click_lambda,
+  );
   let v = {
     input,
     button,
---
+++ public/src/html_inputs_validated.mjs
@@ -0,0 +1,50 @@
+import { html_value_get } from "./html_value_get.mjs";
+import { html_button_enable_if } from "./html_button_enable_if.mjs";
+import { map_get } from "./map_get.mjs";
+import { map_exists } from "./map_exists.mjs";
+import { map_set } from "./map_set.mjs";
+import { html_input_validated_on_input_lambda_initial } from "./html_input_validated_on_input_lambda_initial.mjs";
+import { map_new } from "./map_new.mjs";
+import { html_button } from "./html_button.mjs";
+import { html_on_keydown_enter } from "./html_on_keydown_enter.mjs";
+import { each } from "./each.mjs";
+export function html_inputs_validated(
+  inputs,
+  root,
+  button_text,
+  on_click_lambda,
+) {
+  each(inputs, function (input) {
+    html_on_keydown_enter(input, on_click);
+  });
+  let button = html_button(root, button_text, on_click);
+  let m = map_new();
+  each(inputs, function (input) {
+    html_input_validated_on_input_lambda_initial(input, function (valid) {
+      map_set(m, input, valid);
+      let valid_all = true;
+      each(inputs, function (input) {
+        if (map_exists(input)) {
+          if (map_get(m, input) === false) {
+            valid_all = false;
+          }
+        } else {
+          valid_all = false;
+        }
+        if (!valid_all) {
+          let v2 = true;
+          return v2;
+        }
+      });
+      html_button_enable_if(button, valid_all);
+    });
+  });
+  function on_click() {
+    let values = map_new();
+    each(inputs, function (input) {
+      map_set(values, html_value_get(input));
+    });
+    on_click_lambda(values);
+  }
+  return button;
+}
