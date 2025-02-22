38d2670ce13d08f83ab054b4fc089b772ff0374c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 15:20:53 -0500

    2025-02-22T20:20:53.025Z watch   function_new

--- public/src/function_new.mjs
+++ public/src/function_new.mjs
@@ -3,7 +3,7 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function function_new(function_name) {
   assert_arguments_length(arguments, 1);
   let open = true;
-  await function_new_generic_code(
+  let v = await function_new_generic_code(
     function_name,
     "",
     "",
@@ -12,4 +12,5 @@ export async function function_new(function_name) {
     false,
     false,
   );
+  return v;
 }
