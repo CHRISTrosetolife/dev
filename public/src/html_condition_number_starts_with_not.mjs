cba6084c079738636318000d2db436950e17a571
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 13:11:50 -0500

    2025-02-22T18:11:50.205Z watch   html_condition_number_starts_with_not

--- public/src/html_condition_number_starts_with_not.mjs
+++ public/src/html_condition_number_starts_with_not.mjs
@@ -1,11 +1,16 @@
-import { string_empty_not_is } from "./string_empty_not_is.mjs";
+import { string_to } from "./string_to.mjs";
+import { list_map } from "./list_map.mjs";
+import { range } from "./range.mjs";
 export function html_condition_number_starts_with_not() {
   let v = {
     message: function (value) {
-      let v2 = "not be empty";
+      let v2 = "not start with a number";
       return v2;
     },
-    condition: string_empty_not_is,
+    condition: function (value) {
+      let v3 = list_map(range(10), string_to);
+      return v3;
+    },
   };
   return v;
 }
