4152e332064b61d8c745c4c7ce5a922ab06a385f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 12:35:09 -0500

    2025-02-22T17:35:08.247Z watch   app_name

--- public/src/app_name.mjs
+++ public/src/app_name.mjs
@@ -1,5 +1,7 @@
+import { app_prefix } from "./app_prefix.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
 export function app_name(app_function) {
-  let prefix = "app_";
-  return string_prefix_without(app_function.name, prefix);
+  let prefix = app_prefix();
+  let v = string_prefix_without(app_function.name, prefix);
+  return v;
 }
