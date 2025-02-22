205561324612f7dee7a47f09ec0b023f81f62861
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 14:41:19 -0500

    2025-02-22T19:41:18.438Z watch   app_code_button_functions_search

--- public/src/app_code_button_functions_search.mjs
+++ public/src/app_code_button_functions_search.mjs
@@ -1,3 +1,4 @@
+import { emoji_search } from "./emoji_search.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { app_code_refresh_functions_search } from "./app_code_refresh_functions_search.mjs";
 import { html_button } from "./html_button.mjs";
@@ -6,7 +7,7 @@ export function app_code_button_functions_search(context) {
   let root = object_property_get(context, "root");
   html_button(
     root,
-    string_combine_multiple(["Search functions"]),
+    string_combine_multiple([emoji_search(), " Search functions"]),
     async function () {
       await app_code_refresh_functions_search(context);
     },
