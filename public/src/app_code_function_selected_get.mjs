cee41affa53b9e68920a92d2de7a3a99a37b2812
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 15:13:32 -0500

    2025-02-22T20:13:31.640Z watch   app_code_function_selected_get

--- public/src/app_code_function_selected_get.mjs
+++ public/src/app_code_function_selected_get.mjs
@@ -1,9 +1,12 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { storage_local_exists } from "./storage_local_exists.mjs";
 import { storage_local_get_context } from "./storage_local_get_context.mjs";
 export function app_code_function_selected_get(context) {
-  if (false) {
-    storage_local_exists();
+  let key = "function_selected";
+  let app_fn = object_property_get(context, "app_fn");
+  let e = storage_local_exists(app_fn, key);
+  if (e) {
   }
-  let v = storage_local_get_context(context, "function_selected");
+  let v = storage_local_get_context(context, key);
   return v;
 }
