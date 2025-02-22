f2fae1ae1850ca9b9fdbbb0a6a3c96a895cd3c82
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 12:12:21 -0500

    2025-02-22T17:12:19.036Z watch   function_search

--- public/src/function_search.mjs
+++ public/src/function_search.mjs
@@ -7,6 +7,7 @@ import { function_search_terms_match } from "./function_search_terms_match.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function function_search(query) {
   assert_arguments_length(arguments, 1);
+  ("split on comma or period in case the user made a typo");
   let terms = string_split_multiple(query, [",", "."]);
   let fn_names = await functions_names();
   let matches = list_filter(fn_names, function (fn_name) {
