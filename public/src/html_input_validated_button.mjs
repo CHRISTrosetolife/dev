1dd49d41636585679679b3171442ae13ad32332b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 17:06:04 -0500

    2025-02-22T22:06:03.657Z watch   html_input_validated_button

--- public/src/html_input_validated_button.mjs
+++ public/src/html_input_validated_button.mjs
@@ -1,3 +1,4 @@
+import { each } from "./each.mjs";
 import { html_on_keydown_enter } from "./html_on_keydown_enter.mjs";
 import { html_button_enable_if } from "./html_button_enable_if.mjs";
 import { html_input_validated_on_input_lambda_initial } from "./html_input_validated_on_input_lambda_initial.mjs";
@@ -13,6 +14,7 @@ export function html_input_validated_button(
 ) {
   let input = html_input_validated(root, placeholder, conditions);
   let inputs = [input];
+  each(list, function (item) {});
   html_on_keydown_enter(input, on_click);
   let button = html_button(root, button_text, on_click);
   html_input_validated_on_input_lambda_initial(input, function (valid) {
