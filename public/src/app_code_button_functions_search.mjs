08b68b467ae8525ebfcdb4bda5133c125133b529
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 14:41:09 -0500

    2025-02-22T19:41:09.215Z watch   app_code_button_functions_search

--- public/src/app_code_button_functions_search.mjs
+++ public/src/app_code_button_functions_search.mjs
@@ -1,9 +1,14 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { app_code_refresh_functions_search } from "./app_code_refresh_functions_search.mjs";
 import { html_button } from "./html_button.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function app_code_button_functions_search(context) {
   let root = object_property_get(context, "root");
-  html_button(root, "Search functions", async () => {
-    await app_code_refresh_functions_search(context);
-  });
+  html_button(
+    root,
+    string_combine_multiple(["Search functions"]),
+    async function () {
+      await app_code_refresh_functions_search(context);
+    },
+  );
 }
