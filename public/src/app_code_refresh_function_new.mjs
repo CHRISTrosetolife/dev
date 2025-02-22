7edac2f1ec6babff5b1de049eb75ce458f1fb684
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 13:08:36 -0500

    2025-02-22T18:08:35.979Z watch   app_code_refresh_function_new

--- public/src/app_code_refresh_function_new.mjs
+++ public/src/app_code_refresh_function_new.mjs
@@ -7,10 +7,10 @@ import { fn_name } from "./fn_name.mjs";
 import { list_concat } from "./list_concat.mjs";
 export async function app_code_refresh_function_new(context) {
   let existing = await functions_names();
-  let message = "not be the name of an existing app";
-  let input_placeholder = "App name";
-  let button_text = "New app";
-  let function_this = fn_name("app_code_refresh_app_new");
+  let message = "not be the name of an existing function";
+  let input_placeholder = "Function name";
+  let button_text = "New function";
+  let function_this = fn_name("app_code_refresh_function_new");
   let conditions = list_concat(html_condition_identifier_fn_part(), [
     html_condition_includes_not(existing, message),
   ]);
