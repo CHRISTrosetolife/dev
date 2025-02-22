fc3dbe790e1bfd280a2206d295566cc0014e3ffd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 12:34:25 -0500

    2025-02-22T17:34:22.445Z watch   app_code_refresh_app_new

--- public/src/app_code_refresh_app_new.mjs
+++ public/src/app_code_refresh_app_new.mjs
@@ -1,3 +1,4 @@
+import { functions_names } from "./functions_names.mjs";
 import { noop } from "./noop.mjs";
 import { app_code_button_menu_app } from "./app_code_button_menu_app.mjs";
 import { app_code_invoke } from "./app_code_invoke.mjs";
@@ -12,9 +13,9 @@ import { fn_name } from "./fn_name.mjs";
 import { app_code_screen_set } from "./app_code_screen_set.mjs";
 import { list_concat } from "./list_concat.mjs";
 export async function app_code_refresh_app_new(context) {
-  let fns_all = function_names();
   let function_this = fn_name("app_code_refresh_app_new");
   let root = app_code_screen_set(context, function_this);
+  let fns_all = await functions_names();
   app_code_button_menu_app(context, root, noop);
   let existing = await app_code_invoke(fn_name("apps_list"));
   let message = "not be the name of an existing app";
