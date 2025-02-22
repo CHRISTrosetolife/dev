f4e03f62a11458f3bdc70692bce305261392d2b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 13:19:06 -0500

    2025-02-22T18:19:02.794Z watch   app_code_refresh_function_new

---
+++ public/src/app_code_refresh_function_new.mjs
@@ -0,0 +1,27 @@
+import { html_condition_identifier_fn } from "./html_condition_identifier_fn.mjs";
+import { function_new } from "./function_new.mjs";
+import { app_code_refresh_app_new_generic } from "./app_code_refresh_app_new_generic.mjs";
+import { functions_names } from "./functions_names.mjs";
+import { html_condition_includes_not } from "./html_condition_includes_not.mjs";
+import { fn_name } from "./fn_name.mjs";
+import { list_concat } from "./list_concat.mjs";
+export async function app_code_refresh_function_new(context) {
+  let existing = await functions_names();
+  let message = "not be the name of an existing function";
+  let input_placeholder = "Function name";
+  let button_text = "New function";
+  let function_this = fn_name("app_code_refresh_function_new");
+  let conditions = list_concat(
+    html_condition_identifier_fn(),
+    html_condition_includes_not(existing, message),
+  );
+  let on_submit = function_new;
+  app_code_refresh_app_new_generic(
+    context,
+    function_this,
+    input_placeholder,
+    conditions,
+    button_text,
+    on_submit,
+  );
+}
