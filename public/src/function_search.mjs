39b55e1d9534985dbacbded62b632ebb37a977b8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 12:11:03 -0500

    2025-02-22T17:11:00.336Z watch   function_search

--- public/src/function_search.mjs
+++ public/src/function_search.mjs
@@ -1,16 +1,18 @@
+import { string_split_multiple } from "./string_split_multiple.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { functions_names } from "./functions_names.mjs";
 import { function_names_to_lookup } from "./function_names_to_lookup.mjs";
-import { string_split_comma } from "./string_split_comma.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { function_search_terms_match } from "./function_search_terms_match.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function function_search(query) {
   assert_arguments_length(arguments, 1);
-  let terms = string_split_comma(query);
+  let terms = string_split_multiple(query);
   let fn_names = await functions_names();
-  let matches = list_filter(fn_names, (fn_name) => {
-    return function_search_terms_match(fn_name, terms);
+  let matches = list_filter(fn_names, function (fn_name) {
+    let v = function_search_terms_match(fn_name, terms);
+    return v;
   });
-  return function_names_to_lookup(matches);
+  let v2 = function_names_to_lookup(matches);
+  return v2;
 }
