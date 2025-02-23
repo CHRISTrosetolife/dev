9a01bc581b00f09f5a61c7983c72e2bc076d45db
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 20:25:07 -0500

    2025-02-23T01:25:06.794Z watch   map_exists

--- public/src/map_exists.mjs
+++ public/src/map_exists.mjs
@@ -1,5 +1,6 @@
 import { map_properties } from "./map_properties.mjs";
 import { list_includes } from "./list_includes.mjs";
-export function map_exists(input) {
-  return list_includes(map_properties(selected), input);
+export function map_exists(map, key) {
+  let v = list_includes(map_properties(map), key);
+  return v;
 }
