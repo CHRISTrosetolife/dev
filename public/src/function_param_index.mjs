919e62722314f093726d9355de5c9cbf0304c128
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-22 20:07:08 -0500

    2025-02-23T01:07:07.817Z watch   function_param_index

--- public/src/function_param_index.mjs
+++ public/src/function_param_index.mjs
@@ -1,7 +1,13 @@
+import { log } from "./log.mjs";
 import { list_index } from "./list_index.mjs";
 import { function_params_names } from "./function_params_names.mjs";
 export async function function_param_index(function_name, param_name) {
   let params_names = await function_params_names(function_name);
+  log({
+    function_name,
+    param_name,
+    params_names,
+  });
   let index = list_index(params_names, param_name);
   return index;
 }
