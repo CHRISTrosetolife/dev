ef29e4c3d224a32156e0742afef8b8b7c09d4e5d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-25 15:05:22 -0500

    2025-02-25T20:05:20.499Z watch   app_good

--- public/src/app_good.mjs
+++ public/src/app_good.mjs
@@ -1,3 +1,4 @@
+import { prayers_bible } from "./prayers_bible.mjs";
 import { prayers_unity } from "./prayers_unity.mjs";
 import { prayers_family } from "./prayers_family.mjs";
 import { html_button } from "./html_button.mjs";
@@ -69,26 +70,7 @@ export function app_good() {
             "Help me fast",
             "Help all Christians fast",
             "Let there be harvest workers",
-            "Give all Christians the Spirit of wisdom and of revelation in the knowledge of God",
-            "Enlighten the eyes of the hearts of every Christian",
-            "Let all Christians know the hope God called us",
-            "Let all Christians know the riches of God's glorious inheritance in the saints",
-            "Let all Christians know the immeasurable greatness of God's power toward us who believe",
-            "May the God of peace himself sanctify all Christians completely",
-            "May the whole spirits and souls and bodies of all Christians be kept blameless at the coming of our Lord Jesus Christ",
-            "Make every Christian worthy of God's calling",
-            "Fulfill every resolve for good and every work of faith of every Christian by God's power",
-            "Comfort the hearts of every Christian",
-            "Establish the hearts of all Christans in every good work and word",
-            "Deliver all Christians from wicked and evil men",
-            "Direct the hearts of every Christian to the love of God and to the steadfastness of Christ",
-            "Give every Christian peace at all times in every way",
-            "Equip every Christian with everything good to do the will of God",
-            "Work in every Christian that which is pleasing in God's sight, through Jesus Christ",
-            "May grace and peace be multiplied to every Christian in the knowledge of God and of Jesus our Lord",
-            "May all go well with every Christian",
-            "May every Christian be in good health",
-            "May it go well with the soul of every Christian",
+            prayers_bible(),
             "Let there be no pride",
             "Let there be no adultery",
             "Let there be no murder",
---
+++ public/src/prayers_bible.mjs
@@ -0,0 +1,24 @@
+export function prayers_bible() {
+  return [
+    "Give all Christians the Spirit of wisdom and of revelation in the knowledge of God",
+    "Enlighten the eyes of the hearts of every Christian",
+    "Let all Christians know the hope God called us",
+    "Let all Christians know the riches of God's glorious inheritance in the saints",
+    "Let all Christians know the immeasurable greatness of God's power toward us who believe",
+    "May the God of peace himself sanctify all Christians completely",
+    "May the whole spirits and souls and bodies of all Christians be kept blameless at the coming of our Lord Jesus Christ",
+    "Make every Christian worthy of God's calling",
+    "Fulfill every resolve for good and every work of faith of every Christian by God's power",
+    "Comfort the hearts of every Christian",
+    "Establish the hearts of all Christans in every good work and word",
+    "Deliver all Christians from wicked and evil men",
+    "Direct the hearts of every Christian to the love of God and to the steadfastness of Christ",
+    "Give every Christian peace at all times in every way",
+    "Equip every Christian with everything good to do the will of God",
+    "Work in every Christian that which is pleasing in God's sight, through Jesus Christ",
+    "May grace and peace be multiplied to every Christian in the knowledge of God and of Jesus our Lord",
+    "May all go well with every Christian",
+    "May every Christian be in good health",
+    "May it go well with the soul of every Christian",
+  ];
+}
