a48fd03d8be6db111e91a7d8703af6695deddddd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-02-25 15:14:17 -0500

    2025-02-25T20:14:13.016Z watch   prayers_sins

---
+++ public/src/prayers_sins.mjs
@@ -0,0 +1,26 @@
+export function prayers_sins() {
+  let v = [
+    "Let there be no pride",
+    "Let there be no adultery",
+    "Let there be no murder",
+    "Let there be no sin against children",
+    "Let there be no sin against babies",
+    "Let there be no sin against unborn babies",
+    "Let there be no thievery",
+    "Let there be no doubt",
+    "Let there be no jealousy or envy",
+    "Let there be no sinful anger or wrath",
+    "Let there be no pornography",
+    "Let there be no rape",
+    "Let there be no sexual sin",
+    "Let there be no sexual sin against children",
+    "Let there be no fornication",
+    "Let there be no kidnapping",
+    "Let there be no hostages",
+    "Let there be no unnecessary war",
+    "Let there be no violence",
+    "Let there be no drugs",
+    "Let there be no greed",
+  ];
+  return v;
+}
