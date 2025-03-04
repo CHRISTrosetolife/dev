dc2140ef2ea69f3edd67eca8bde2bd924af8e5f0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-04 11:33:50 -0500

    2025-03-04T16:33:49.244Z watch   html_on_keydown_enter

--- public/src/html_on_keydown_enter.mjs
+++ public/src/html_on_keydown_enter.mjs
@@ -1,10 +1,10 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { html_on_keydown } from "./html_on_keydown.mjs";
 export function html_on_keydown_enter(input, on_click) {
-  html_on_keydown(input, function (event) {
+  html_on_keydown(input, async function (event) {
     let key = object_property_get(event, "key");
     if (key === "Enter") {
-      on_click();
+      await on_click();
     }
   });
 }
