36f70354e6bb3b25c55ec3d866183d6588d79b68
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-05 21:37:34 -0500

    2025-03-06T02:37:33.942Z watch   html_prayer

--- public/src/html_prayer.mjs
+++ public/src/html_prayer.mjs
@@ -1,9 +1,9 @@
-import { list_concat_multiple } from "./list_concat_multiple.mjs";
+import { list_denest } from "./list_denest.mjs";
 import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
 export function html_prayer(root, prayer) {
   html_p_text_multiple(
     root,
-    list_concat_multiple([
+    list_denest([
       [
         "Heavenly Father,",
         "In the name of Jesus:",
