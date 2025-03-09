8e5898cee546b85de4ebdfa51238e4cc056770ff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-09 13:28:36 -0400

    2025-03-09T17:28:35.513Z watch   app_chapter

--- public/src/app_chapter.mjs
+++ public/src/app_chapter.mjs
@@ -19,6 +19,7 @@ export async function app_chapter() {
   let chapter_name = object_property_get(lookup, app_share_chapter());
   let verses = await bible_storage_version_http_get(version_code, chapter_name);
   let { book_code, chapter_code } = bible_chapter_name_parse(chapter_name);
-  each(list, function (item) {});
-  html_bible_verse_define(root);
+  each(verses, function (verse) {
+    html_bible_verse_define(root, book_code, chapter_code, verse);
+  });
 }
