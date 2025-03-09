a7b90d082688c2625bb09c981b09f7fb9e091059
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-09 13:21:31 -0400

    2025-03-09T17:21:30.583Z watch   app_chapter

--- public/src/app_chapter.mjs
+++ public/src/app_chapter.mjs
@@ -1,6 +1,13 @@
+import { app_share_chapter } from "./app_share_chapter.mjs";
+import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { html_style_default_initialize_axios } from "./html_style_default_initialize_axios.mjs";
 import { html_hash_lookup } from "./html_hash_lookup.mjs";
+import { html_p_text } from "./html_p_text.mjs";
 export async function app_chapter() {
   let root = await html_style_default_initialize_axios();
   let lookup = html_hash_lookup();
+  if (object_property_exists_not(lookup, app_share_chapter())) {
+    html_p_text("Needs chapter in URL");
+    return;
+  }
 }
