6b7e4b57f741a5d1dbd8409fe43e15922ec23c9e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-09 16:01:35 -0400

    2025-03-09T20:01:34.585Z watch   app_search

--- public/src/app_search.mjs
+++ public/src/app_search.mjs
@@ -1,3 +1,4 @@
+import { html_style_flex_row_centered } from "./html_style_flex_row_centered.mjs";
 import { window_open } from "./window_open.mjs";
 import { log } from "./log.mjs";
 import { invoke_once } from "./invoke_once.mjs";
@@ -70,6 +71,7 @@ export async function app_search() {
           verse_number,
         );
         let text = string_combine_multiple([reference, " ", joined]);
+        let row = html_style_flex_row_centered(search_result_component);
         html_button(
           search_result_component,
           string_combine_multiple([emoji_book(), " open chapter"]),
