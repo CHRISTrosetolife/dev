a2778f5df83e7e5a017d21ad5b58e29b1011be97
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-09 16:43:08 -0400

    2025-03-09T20:43:07.275Z watch   app_chapter

--- public/src/app_chapter.mjs
+++ public/src/app_chapter.mjs
@@ -1,3 +1,4 @@
+import { html_style_background_color_yellow } from "./html_style_background_color_yellow.mjs";
 import { app_share_verse } from "./app_share_verse.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { html_style_a_plain } from "./html_style_a_plain.mjs";
@@ -29,10 +30,11 @@ export async function app_chapter() {
   let verses = await bible_storage_version_http_get(version_code, chapter_name);
   let { book_code, chapter_code } = bible_chapter_name_parse(chapter_name);
   each(verses, function (verse) {
-    let { verse_number } = verse;
-    if (false) {
-    }
     let v_component = html_div(root);
+    let { verse_number } = verse;
+    if (verse_number === verse_number_selected) {
+      html_style_background_color_yellow(v_component);
+    }
     html_bible_verse_define(v_component, book_code, chapter_code, verse);
   });
 }
