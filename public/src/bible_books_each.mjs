0ed8ff94d3f76a804b5adc8f12f6f9551a9def85
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-09 12:19:38 -0400

    2025-03-09T16:19:37.909Z watch   bible_books_each

--- public/src/bible_books_each.mjs
+++ public/src/bible_books_each.mjs
@@ -1,11 +1,23 @@
+import { equal_json } from "./equal_json.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { log } from "./log.mjs";
 import { bible_books } from "./bible_books.mjs";
 import { each_async } from "./each_async.mjs";
 export async function bible_books_each(bible_folder, lambda) {
   assert_arguments_length(arguments, 2);
-  await each_async(await bible_books(bible_folder), async (book_name) => {
-    if (false) {
+  await each_async(await bible_books(bible_folder), async function (book_name) {
+    if (
+      equal_json(
+        {
+          bible_folder,
+          book_name,
+        },
+        {
+          bible_folder,
+          book_name,
+        },
+      )
+    ) {
       log({
         bible_folder,
         book_name,
