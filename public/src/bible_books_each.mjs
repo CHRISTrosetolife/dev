c2ac9524c9a38113c364af702cf9640c7090a17d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-09 12:20:46 -0400

    2025-03-09T16:20:45.682Z watch   bible_books_each

--- public/src/bible_books_each.mjs
+++ public/src/bible_books_each.mjs
@@ -1,15 +1,11 @@
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
-import { log } from "./log.mjs";
 import { bible_books } from "./bible_books.mjs";
 import { each_async } from "./each_async.mjs";
 export async function bible_books_each(bible_folder, lambda) {
   assert_arguments_length(arguments, 2);
   await each_async(await bible_books(bible_folder), async function (book_name) {
     if (bible_folder === "frasbl" && book_name === "PS2") {
-      log({
-        bible_folder,
-        book_name,
-      });
+      return;
     }
     await lambda(book_name);
   });
