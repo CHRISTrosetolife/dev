1b1375bf14f23a2cfe5cc80024a3ad20a77455b9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 10:53:22 -0400

    2025-03-10T14:53:21.213Z pm app_language_2_upload_pairs_bohol language_to limit

--- public/src/app_language_2_upload_pairs_bohol.mjs
+++ public/src/app_language_2_upload_pairs_bohol.mjs
@@ -7,8 +7,8 @@ import { list_take_soft } from "./list_take_soft.mjs";
 import { english_words_dictionary } from "./english_words_dictionary.mjs";
 export async function app_language_2_upload_pairs_bohol(
   language_from,
-  limit,
   language_to,
+  limit,
 ) {
   let filtered2 = await english_words_dictionary();
   let taken = list_take_soft(filtered2, limit);
--- public/src/app_tgl_upload_pairs.mjs
+++ public/src/app_tgl_upload_pairs.mjs
@@ -4,8 +4,8 @@ export async function app_tgl_upload_pairs(limit) {
   let language_from = "English";
   let v = await app_language_2_upload_pairs_bohol(
     language_from,
-    limit,
     language_to,
+    limit,
   );
   return v;
 }
