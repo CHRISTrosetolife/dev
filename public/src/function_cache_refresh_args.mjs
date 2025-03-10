ed29d66671bbf43406cd281e8d69cd274c3b201e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 11:31:27 -0400

    2025-03-10T15:31:26.963Z function_wrap function_cache_refresh_args_generic function_cache_refresh_args

---
+++ public/src/function_cache_refresh_args.mjs
@@ -0,0 +1,4 @@
+import { function_cache_refresh_args_generic } from "./function_cache_refresh_args_generic.mjs";
+export async function function_cache_refresh_args(function_name, args) {
+  await function_cache_refresh_args_generic(function_name, args);
+}
