17dbfe74ad104b1c451104c159dec10e0bf554d1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 13:47:29 -0400

    2025-03-10T17:47:28.515Z watch   sandbox_4

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -2,7 +2,6 @@ import { each } from "./each.mjs";
 import { html_parse_text } from "./html_parse_text.mjs";
 import { html_parse_children } from "./html_parse_children.mjs";
 import { list_first } from "./list_first.mjs";
-import { list_map } from "./list_map.mjs";
 import { log } from "./log.mjs";
 import { html_parse_visit_tag_list } from "./html_parse_visit_tag_list.mjs";
 import { html_parse_visit_attribute_value_single } from "./html_parse_visit_attribute_value_single.mjs";
@@ -19,14 +18,10 @@ export async function sandbox_4() {
   let root = await html_cache_parse(url);
   let table = html_parse_visit_attribute_value_single(root, "cellpadding", "3");
   let rows = html_parse_visit_tag_list(table, "tr");
-  each(list, function (item) {
-    log(
-      list_map(rows, function (row) {
-        let v = list_first(html_parse_children(row));
-        let v2 = html_parse_text(v);
-        return v;
-      }),
-    );
+  each(rows, function (row) {
+    let v = list_first(html_parse_children(row));
+    let v2 = html_parse_text(v);
+    log(v2);
   });
   return;
   let filtered2 = await english_words_dictionary();
