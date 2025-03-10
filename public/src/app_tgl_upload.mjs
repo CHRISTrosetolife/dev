431e0e142674a63afa9eb1edf7bf28036aec89c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 10:58:15 -0400

    2025-03-10T14:58:14.517Z watch   app_tgl_upload

--- public/src/app_tgl_upload.mjs
+++ public/src/app_tgl_upload.mjs
@@ -1,9 +1,16 @@
+import { app_language_2_upload_pairs_bohol_cache } from "./app_language_2_upload_pairs_bohol_cache.mjs";
 import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
-import { app_tgl_upload_pairs_cache } from "./app_tgl_upload_pairs_cache.mjs";
 export async function app_tgl_upload() {
   let from = "tgl";
   let to = "en";
   let limit = 1000;
-  let pairs = await app_tgl_upload_pairs_cache(limit);
-  return await app_language_2_upload_pairs(pairs, from, to);
+  let language_to = "Tagalog";
+  let language_from = "English";
+  let pairs = await app_language_2_upload_pairs_bohol_cache(
+    language_from,
+    language_to,
+    limit,
+  );
+  let v = await app_language_2_upload_pairs(pairs, from, to);
+  return v;
 }
