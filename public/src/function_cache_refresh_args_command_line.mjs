bd7a42fe11689afe3fef16d40f3002bd886fd88b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 11:30:42 -0400

    2025-03-10T15:30:39.241Z r function_cache_refresh_args function_cache_refresh_args_generic

--- public/src/function_cache_refresh_args_command_line.mjs
+++ public/src/function_cache_refresh_args_command_line.mjs
@@ -1,10 +1,10 @@
-import { function_cache_refresh_args } from "./function_cache_refresh_args.mjs";
+import { function_cache_refresh_args_generic } from "./function_cache_refresh_args_generic.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 export async function function_cache_refresh_args_command_line(
   function_name,
   args_list,
 ) {
   let args = string_split_comma(args_list);
-  let v = await function_cache_refresh_args(function_name, args);
+  let v = await function_cache_refresh_args_generic(function_name, args);
   return v;
 }
--- public/src/function_cache_refresh_args_generic.mjs
+++ public/src/function_cache_refresh_args_generic.mjs
@@ -1,7 +1,7 @@
 import { function_run_cache } from "./function_run_cache.mjs";
 import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
 import { function_cache_delete_generic } from "./function_cache_delete_generic.mjs";
-export async function function_cache_refresh_args(function_name, args) {
+export async function function_cache_refresh_args_generic(function_name, args) {
   await function_cache_delete_generic(
     function_name,
     args,
