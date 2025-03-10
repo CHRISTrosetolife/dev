854ce7a8445f6d045ce9d3448e39d030023b2c2a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 13:42:03 -0400

    2025-03-10T17:42:02.969Z watch   sandbox_4

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -1,4 +1,4 @@
-import { html_parse_visit_tag_attribute_value } from "./html_parse_visit_tag_attribute_value.mjs";
+import { html_parse_visit_attribute_value_single } from "./html_parse_visit_attribute_value_single.mjs";
 import { html_cache_parse } from "./html_cache_parse.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { english_words_dictionary } from "./english_words_dictionary.mjs";
@@ -10,7 +10,7 @@ export async function sandbox_4() {
     word,
   ]);
   let root = await html_cache_parse(url);
-  html_parse_visit_tag_attribute_value();
+  html_parse_visit_attribute_value_single();
   return;
   let filtered2 = await english_words_dictionary();
   await each_log_async(filtered2, function () {});
