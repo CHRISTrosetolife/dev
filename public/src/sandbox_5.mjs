2c0b25ce19dc67842e072930ab452166b3af3e7c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 18:10:45 -0400

    2025-03-10T22:10:44.970Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,16 +1,14 @@
+import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { defintion_en_ur } from "./defintion_en_ur.mjs";
 import { each_log_async } from "./each_log_async.mjs";
 import { english_words_dictionary_take_soft } from "./english_words_dictionary_take_soft.mjs";
-import { list_first } from "./list_first.mjs";
 import { audio_upload } from "./audio_upload.mjs";
 export async function sandbox_5() {
   let words = await english_words_dictionary_take_soft(1000);
-  let w = list_first(words);
-  let v = await audio_upload("en", w);
-  await each_log_async(words, async function (word) {
-    let definitions = await defintion_en_ur(word);
-    if (false) {
+  await each_log_async(words, async function (w) {
+    let definitions = await defintion_en_ur(w);
+    if (list_empty_not_is(definitions)) {
+      await audio_upload("en", w);
     }
   });
-  return v;
 }
