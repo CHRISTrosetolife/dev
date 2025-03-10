3dd7f1aaeeaa86d380600468753f03a588d437b7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 15:52:51 -0400

    2025-03-10T19:52:51.150Z watch   bible_interlinear_words_hebrew_audio_upload

--- public/src/bible_interlinear_words_hebrew_audio_upload.mjs
+++ public/src/bible_interlinear_words_hebrew_audio_upload.mjs
@@ -1,3 +1,4 @@
+import { bible_interlinear_words_audio_upload_map } from "./bible_interlinear_words_audio_upload_map.mjs";
 import { each } from "./each.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 import { string_symbols_multiple } from "./string_symbols_multiple.mjs";
@@ -13,8 +14,9 @@ export async function bible_interlinear_words_hebrew_audio_upload() {
     return v;
   };
   let words = await bible_interlinear_words(bible_interlinear_cache_old);
+  let m2 = bible_interlinear_words_audio_upload_map(words, filter);
   let letters = list_adder_unique(function (la) {
-    each(words, function (word) {
+    each(m2, function (word) {
       let letters = word.match(/([\u05D0-\u05EA][\u0591-\u05C7]*)/g);
       each(letters, la);
     });
