761ed6f452ca928e89762819dc0930545108ea9d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 13:37:07 -0400

    2025-03-10T17:37:05.387Z watch   sandbox_4

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -3,11 +3,10 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { english_words_dictionary } from "./english_words_dictionary.mjs";
 import { each_log_async } from "./each_log_async.mjs";
 export async function sandbox_4() {
+  let word = "system";
   let url = string_combine_multiple([
-    "https://www.bohol.ph/diksyunaryo.php?sw=",
+    "https://www.urduword.com/search.php?English=",
     word,
-    "&lang=",
-    language_search,
   ]);
   let result = await html_cache_parse(url);
   let filtered2 = await english_words_dictionary();
