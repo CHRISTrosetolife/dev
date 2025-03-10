a04ace66fe2c2d078e4f08746cd3291abe8edd67
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 14:03:35 -0400

    2025-03-10T18:03:35.389Z watch   sandbox_4

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -1,10 +1,13 @@
+import { log } from "./log.mjs";
 import { defintion_en_ur } from "./defintion_en_ur.mjs";
 import { english_words_dictionary } from "./english_words_dictionary.mjs";
 import { each_log_async } from "./each_log_async.mjs";
 export async function sandbox_4() {
-  return pairs;
-  let filtered2 = await english_words_dictionary();
-  await each_log_async(filtered2, async function () {
+  let words = await english_words_dictionary();
+  await each_log_async(words, async function (word) {
     let pairs = await defintion_en_ur(word);
+    log({
+      pairs,
+    });
   });
 }
