801b4bc5d71afe50ce7846332a25963f6d0aa323
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 11:05:56 -0400

    2025-03-10T15:05:55.190Z watch   app_language_2_upload_bohol

--- public/src/app_language_2_upload_bohol.mjs
+++ public/src/app_language_2_upload_bohol.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
 import { app_language_2_upload_pairs_bohol_cache } from "./app_language_2_upload_pairs_bohol_cache.mjs";
 export async function app_language_2_upload_bohol(
@@ -12,5 +13,9 @@ export async function app_language_2_upload_bohol(
     language_to,
     limit,
   );
+  log({
+    pairs,
+  });
+  return;
   await app_language_2_upload_pairs(pairs, from, to);
 }
