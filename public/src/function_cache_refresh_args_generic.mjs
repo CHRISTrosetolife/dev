f2a2bae25bb1444b7a7c022b2f36de4d3fd4aae4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 11:37:40 -0400

    2025-03-10T15:37:38.074Z watch   function_cache_refresh_args_generic

--- public/src/function_cache_refresh_args_generic.mjs
+++ public/src/function_cache_refresh_args_generic.mjs
@@ -1,11 +1,14 @@
+import { log } from "./log.mjs";
 import { function_run_cache } from "./function_run_cache.mjs";
 import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
 import { function_cache_delete_generic } from "./function_cache_delete_generic.mjs";
 export async function function_cache_refresh_args_generic(function_name, args) {
+  log("function_cache_refresh_args_generic");
   await function_cache_delete_generic(
     function_name,
     args,
     file_delete_if_exists,
   );
-  return await function_run_cache(function_name, args);
+  let v = await function_run_cache(function_name, args);
+  return v;
 }
