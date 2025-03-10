c9adb40281f497a06cce690e6e858b3a05bfd668
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 11:01:03 -0400

    2025-03-10T15:01:02.625Z watch   app_tgl_upload

---
+++ public/src/app_language_2_upload_bohol.mjs
@@ -0,0 +1,16 @@
+import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
+import { app_language_2_upload_pairs_bohol_cache } from "./app_language_2_upload_pairs_bohol_cache.mjs";
+export async function app_language_2_upload_bohol(
+  language_from,
+  language_to,
+  limit,
+  from,
+  to,
+) {
+  let pairs = await app_language_2_upload_pairs_bohol_cache(
+    language_from,
+    language_to,
+    limit,
+  );
+  await app_language_2_upload_pairs(pairs, from, to);
+}
--- public/src/app_tgl_upload.mjs
+++ public/src/app_tgl_upload.mjs
@@ -1,15 +1,15 @@
-import { app_language_2_upload_pairs_bohol_cache } from "./app_language_2_upload_pairs_bohol_cache.mjs";
-import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
+import { app_language_2_upload_bohol } from "./app_language_2_upload_bohol.mjs";
 export async function app_tgl_upload() {
   let from = "tgl";
   let to = "en";
   let limit = 1000;
   let language_to = "Tagalog";
   let language_from = "English";
-  let pairs = await app_language_2_upload_pairs_bohol_cache(
+  await app_language_2_upload_bohol(
     language_from,
     language_to,
     limit,
+    from,
+    to,
   );
-  await app_language_2_upload_pairs(pairs, from, to);
 }
