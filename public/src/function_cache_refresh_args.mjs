24c9398d08a9c54d9792eaa41f53740c6b037790
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 11:32:06 -0400

    2025-03-10T15:32:05.096Z watch   function_cache_refresh_args

--- public/src/function_cache_refresh_args.mjs
+++ public/src/function_cache_refresh_args.mjs
@@ -1,4 +1,4 @@
 import { function_cache_refresh_args_generic } from "./function_cache_refresh_args_generic.mjs";
-export async function function_cache_refresh_args(function_name, args) {
-  await function_cache_refresh_args_generic(function_name, args);
+export async function function_cache_refresh_args(fn, args) {
+  await function_cache_refresh_args_generic(fn.name, args);
 }
