8c64b89d938b30c9a23d199fc7e66fa42b346246
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 10:54:31 -0400

    2025-03-10T14:54:30.828Z fcn app_language_2_upload_pairs_bohol

---
+++ public/src/app_language_2_upload_pairs_bohol_cache.mjs
@@ -0,0 +1,13 @@
+import { app_language_2_upload_pairs_bohol } from "./app_language_2_upload_pairs_bohol.mjs";
+import { function_cache } from "./function_cache.mjs";
+export async function app_language_2_upload_pairs_bohol_cache(
+  language_from,
+  language_to,
+  limit,
+) {
+  return await function_cache(app_language_2_upload_pairs_bohol, [
+    language_from,
+    language_to,
+    limit,
+  ]);
+}
