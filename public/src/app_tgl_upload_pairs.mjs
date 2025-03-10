53b4563b0db1f668ccc29bb4c04fd862a43bb173
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 10:49:14 -0400

    2025-03-10T14:49:13.814Z watch   app_tgl_upload_pairs

--- public/src/app_tgl_upload_pairs.mjs
+++ public/src/app_tgl_upload_pairs.mjs
@@ -7,11 +7,11 @@ import { english_words_dictionary } from "./english_words_dictionary.mjs";
 import { list_take_soft } from "./list_take_soft.mjs";
 export async function app_tgl_upload_pairs(limit) {
   let language_to = "Tagalog";
+  let language_from = "English";
   let filtered2 = await english_words_dictionary();
   let taken = list_take_soft(filtered2, limit);
-  let pairs = await list_adder_async(async (la) => {
-    await each_log_async(taken, async (w) => {
-      let language_from = "English";
+  let pairs = await list_adder_async(async function (la) {
+    await each_log_async(taken, async function (w) {
       let inner = await definition_bohol(w, language_from, language_to);
       each(inner, la);
     });
