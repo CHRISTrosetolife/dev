07a69dc92494df598f150e3b56e1438250a9d0bd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 11:04:40 -0400

    2025-03-10T15:04:39.306Z c app_tgl_upload app_ceb2_upload

---
+++ public/src/app_ceb2_upload.mjs
@@ -0,0 +1,15 @@
+import { app_language_2_upload_bohol } from "./app_language_2_upload_bohol.mjs";
+export async function app_ceb2_upload() {
+  let from = "tgl";
+  let to = "en";
+  let limit = 1000;
+  let language_to = "Tagalog";
+  let language_from = "English";
+  await app_language_2_upload_bohol(
+    language_from,
+    language_to,
+    limit,
+    from,
+    to,
+  );
+}
