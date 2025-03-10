c6820dc8d7eac60c6711e1eaeff23b20d79f7f9f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 14:03:17 -0400

    2025-03-10T18:03:16.602Z watch   sandbox_4

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -2,9 +2,9 @@ import { defintion_en_ur } from "./defintion_en_ur.mjs";
 import { english_words_dictionary } from "./english_words_dictionary.mjs";
 import { each_log_async } from "./each_log_async.mjs";
 export async function sandbox_4() {
-  let word = "system";
-  let pairs = await defintion_en_ur(word);
   return pairs;
   let filtered2 = await english_words_dictionary();
-  await each_log_async(filtered2, function () {});
+  await each_log_async(filtered2, async function () {
+    let pairs = await defintion_en_ur(word);
+  });
 }
