9e74c77d28c46bd59616f748186a2a72fb713f63
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 11:50:37 -0400

    2025-03-10T15:50:37.056Z watch   app_language_2_upload_pairs_bohol_cache

--- public/src/app_language_2_upload_pairs_bohol_cache.mjs
+++ public/src/app_language_2_upload_pairs_bohol_cache.mjs
@@ -1,3 +1,5 @@
+import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
+import { function_cache_delete_generic } from "./function_cache_delete_generic.mjs";
 import { app_language_2_upload_pairs_bohol } from "./app_language_2_upload_pairs_bohol.mjs";
 import { function_cache } from "./function_cache.mjs";
 export async function app_language_2_upload_pairs_bohol_cache(
@@ -7,6 +9,7 @@ export async function app_language_2_upload_pairs_bohol_cache(
 ) {
   let fn = app_language_2_upload_pairs_bohol;
   let args = [language_from, language_to, limit];
+  await function_cache_delete_generic(fn.name, args, file_delete_if_exists);
   let v = await function_cache(fn, args);
   return v;
 }
