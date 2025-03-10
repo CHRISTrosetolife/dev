7ceb7e8253ecfa0b0e052b492ac42d8b90e649b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 14:12:48 -0400

    2025-03-10T18:12:47.731Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,5 +1,9 @@
+import { list_first } from "./list_first.mjs";
+import { english_words_dictionary } from "./english_words_dictionary.mjs";
 import { audio_upload } from "./audio_upload.mjs";
 export async function sandbox_5() {
+  let words = await english_words_dictionary();
+  let w = list_first(words);
   let url = "";
   await audio_upload();
 }
