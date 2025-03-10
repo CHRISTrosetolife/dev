246b274c2998aa6d6f611883cb5213d8c2a22886
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 10:50:09 -0400

    2025-03-10T14:50:08.694Z watch   app_tgl_upload_pairs

---
+++ public/src/app_language_2_upload_pairs_bohol.mjs
@@ -0,0 +1,23 @@
+import { list_unique_json } from "./list_unique_json.mjs";
+import { each } from "./each.mjs";
+import { definition_bohol } from "./definition_bohol.mjs";
+import { each_log_async } from "./each_log_async.mjs";
+import { list_adder_async } from "./list_adder_async.mjs";
+import { list_take_soft } from "./list_take_soft.mjs";
+import { english_words_dictionary } from "./english_words_dictionary.mjs";
+export async function app_language_2_upload_pairs_bohol(
+  limit,
+  language_from,
+  language_to,
+) {
+  let filtered2 = await english_words_dictionary();
+  let taken = list_take_soft(filtered2, limit);
+  let pairs = await list_adder_async(async function (la) {
+    await each_log_async(taken, async function (w) {
+      let inner = await definition_bohol(w, language_from, language_to);
+      each(inner, la);
+    });
+  });
+  pairs = list_unique_json(pairs);
+  return pairs;
+}
--- public/src/app_tgl_upload_pairs.mjs
+++ public/src/app_tgl_upload_pairs.mjs
@@ -1,21 +1,11 @@
-import { definition_bohol } from "./definition_bohol.mjs";
-import { list_unique_json } from "./list_unique_json.mjs";
-import { each } from "./each.mjs";
-import { each_log_async } from "./each_log_async.mjs";
-import { list_adder_async } from "./list_adder_async.mjs";
-import { english_words_dictionary } from "./english_words_dictionary.mjs";
-import { list_take_soft } from "./list_take_soft.mjs";
+import { app_language_2_upload_pairs_bohol } from "./app_language_2_upload_pairs_bohol.mjs";
 export async function app_tgl_upload_pairs(limit) {
   let language_to = "Tagalog";
   let language_from = "English";
-  let filtered2 = await english_words_dictionary();
-  let taken = list_take_soft(filtered2, limit);
-  let pairs = await list_adder_async(async function (la) {
-    await each_log_async(taken, async function (w) {
-      let inner = await definition_bohol(w, language_from, language_to);
-      each(inner, la);
-    });
-  });
-  pairs = list_unique_json(pairs);
-  return pairs;
+  let v = await app_language_2_upload_pairs_bohol(
+    limit,
+    language_from,
+    language_to,
+  );
+  return v;
 }
