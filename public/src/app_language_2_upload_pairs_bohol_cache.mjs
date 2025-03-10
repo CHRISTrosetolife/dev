9bf8f9e9fcf05480f1a1276f7878b71cce564367
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-10 11:32:19 -0400

    2025-03-10T15:32:19.008Z watch   app_language_2_upload_pairs_bohol_cache

--- public/src/app_language_2_upload_pairs_bohol_cache.mjs
+++ public/src/app_language_2_upload_pairs_bohol_cache.mjs
@@ -1,13 +1,14 @@
-import { function_cache_refresh_args_generic } from "./function_cache_refresh_args_generic.mjs";
+import { function_cache_refresh_args } from "./function_cache_refresh_args.mjs";
 import { app_language_2_upload_pairs_bohol } from "./app_language_2_upload_pairs_bohol.mjs";
 export async function app_language_2_upload_pairs_bohol_cache(
   language_from,
   language_to,
   limit,
 ) {
-  let v = await function_cache_refresh_args_generic(
-    app_language_2_upload_pairs_bohol,
-    [language_from, language_to, limit],
-  );
+  let v = await function_cache_refresh_args(app_language_2_upload_pairs_bohol, [
+    language_from,
+    language_to,
+    limit,
+  ]);
   return v;
 }
