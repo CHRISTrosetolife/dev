fcf50d8ce349f32b24c621278a3332ed1313b3f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 17:28:46 -0400

    2025-03-11T21:28:44.241Z watch   html_button_noload

--- public/src/html_button_noload.mjs
+++ public/src/html_button_noload.mjs
@@ -1,9 +1,9 @@
+import { html_on_click_noload } from "./html_on_click_noload.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
-import { html_on_click } from "./html_on_click.mjs";
 import { html_button_width_full_text } from "./html_button_width_full_text.mjs";
 export function html_button_noload(parent, text, on_click) {
   assert_arguments_length(arguments, 3);
   let b = html_button_width_full_text(parent, text);
-  html_on_click(b, on_click);
+  html_on_click_noload(b, on_click);
   return b;
 }
