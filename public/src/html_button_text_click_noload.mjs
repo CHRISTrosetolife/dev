bec934097b308e80236205bcf47f9e73a7420e9d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 17:24:38 -0400

    2025-03-11T21:24:34.203Z watch   html_button_text_click_noload

--- public/src/html_button_text_click_noload.mjs
+++ public/src/html_button_text_click_noload.mjs
@@ -1,7 +1,7 @@
-import { html_on_click } from "./html_on_click.mjs";
+import { html_on_click_noload } from "./html_on_click_noload.mjs";
 import { html_button_text } from "./html_button_text.mjs";
 export function html_button_text_click_noload(container, text, lambda) {
   let b = html_button_text(container, text);
-  html_on_click(b, lambda);
+  html_on_click_noload(b, lambda);
   return b;
 }
