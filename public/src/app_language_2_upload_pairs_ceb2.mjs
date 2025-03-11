42a5f2f9aa87a0cc25266b2162e5385b0606d502
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 15:57:55 -0400

    2025-03-11T19:57:55.248Z watch   app_language_2_upload_pairs_ceb2

--- public/src/app_language_2_upload_pairs_ceb2.mjs
+++ public/src/app_language_2_upload_pairs_ceb2.mjs
@@ -1,6 +1,5 @@
+import { object_property_exists } from "./object_property_exists.mjs";
 import { log } from "./log.mjs";
-import { list_multiple_is } from "./list_multiple_is.mjs";
-import { object_properties } from "./object_properties.mjs";
 import { ceb_definition_en_2 } from "./ceb_definition_en_2.mjs";
 import { each_async } from "./each_async.mjs";
 import { english_words_dictionary } from "./english_words_dictionary.mjs";
@@ -8,8 +7,7 @@ export async function app_language_2_upload_pairs_ceb2() {
   let filtered = await english_words_dictionary();
   await each_async(filtered, async function (t) {
     let r = await ceb_definition_en_2(t);
-    let p = object_properties(r);
-    if (list_multiple_is(p)) {
+    if (object_property_exists(object, "property_name")) {
       log({
         r,
       });
