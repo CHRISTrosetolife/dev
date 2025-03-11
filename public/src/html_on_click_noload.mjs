e66abbae4e6fcf9417a34b244f7749f9b132c1a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 17:23:49 -0400

    2025-03-11T21:23:48.747Z watch   html_on_click_noload

--- public/src/html_on_click_noload.mjs
+++ public/src/html_on_click_noload.mjs
@@ -1,7 +1,7 @@
+import { invoke } from "./invoke.mjs";
 import { html_on_click_generic } from "./html_on_click_generic.mjs";
-import { html_load } from "./html_load.mjs";
 export function html_on_click_noload(component, lambda) {
-  let lambda2 = html_load;
+  let lambda2 = invoke;
   let v = html_on_click_generic(component, lambda2, lambda);
   return v;
 }
