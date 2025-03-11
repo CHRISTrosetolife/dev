b67d3362b93ec7c934facc52c07455e9c1040c80
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 16:28:43 -0400

    2025-03-11T20:28:42.552Z watch   app_language_2_upload_pairs_ceb2

--- public/src/app_language_2_upload_pairs_ceb2.mjs
+++ public/src/app_language_2_upload_pairs_ceb2.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
@@ -11,10 +10,6 @@ export async function app_language_2_upload_pairs_ceb2(limit) {
   let pairs = await list_adder_async(async function (la) {
     await each_async(words, async function (word) {
       let r = await ceb_definition_en_2(word);
-      log({
-        r,
-        word,
-      });
       if (object_property_exists(r, word)) {
         let ds = object_property_get(r, word);
         each(ds, function (d) {
