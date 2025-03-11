a237f4c36636993e0652b54888774b6fe1e8b3bf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 12:59:17 -0400

    2025-03-11T16:59:16.003Z watch   app_language_2_upload_pairs_en_ur_cache

--- public/src/app_language_2_upload_pairs_en_ur_cache.mjs
+++ public/src/app_language_2_upload_pairs_en_ur_cache.mjs
@@ -1,5 +1,8 @@
+import { function_cache_force } from "./function_cache_force.mjs";
 import { app_language_2_upload_pairs_en_ur } from "./app_language_2_upload_pairs_en_ur.mjs";
-import { function_cache } from "./function_cache.mjs";
 export async function app_language_2_upload_pairs_en_ur_cache(limit) {
-  return await function_cache(app_language_2_upload_pairs_en_ur, [limit]);
+  let v = await function_cache_force(app_language_2_upload_pairs_en_ur, [
+    limit,
+  ]);
+  return v;
 }
