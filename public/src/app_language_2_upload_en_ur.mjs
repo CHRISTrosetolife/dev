c5c5c85bf2dffb36de7af04d11f49a1fd9ad423b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 12:51:18 -0400

    2025-03-11T16:51:16.480Z watch   app_language_2_upload_en_ur

--- public/src/app_language_2_upload_en_ur.mjs
+++ public/src/app_language_2_upload_en_ur.mjs
@@ -1,16 +1,6 @@
+import { app_language_2_upload_pairs_en_ur_cache } from "./app_language_2_upload_pairs_en_ur_cache.mjs";
 import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
-import { app_language_2_upload_pairs_bohol_cache } from "./app_language_2_upload_pairs_bohol_cache.mjs";
-export async function app_language_2_upload_en_ur(
-  language_from,
-  language_to,
-  limit,
-  from,
-  to,
-) {
-  let pairs = await app_language_2_upload_pairs_bohol_cache(
-    language_from,
-    language_to,
-    limit,
-  );
+export async function app_language_2_upload_en_ur(limit, from, to) {
+  let pairs = await app_language_2_upload_pairs_en_ur_cache(limit);
   await app_language_2_upload_pairs(pairs, from, to);
 }
---
+++ public/src/app_language_2_upload_pairs_en_ur_cache.mjs
@@ -0,0 +1,5 @@
+import { app_language_2_upload_pairs_en_ur } from "./app_language_2_upload_pairs_en_ur.mjs";
+import { function_cache } from "./function_cache.mjs";
+export async function app_language_2_upload_pairs_en_ur_cache(limit) {
+  return await function_cache(app_language_2_upload_pairs_en_ur, [limit]);
+}
