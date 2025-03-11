8fe12a300f3957466f6f8b40e2244e9cf9a38752
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 12:48:52 -0400

    2025-03-11T16:48:49.249Z watch   app_language_2_upload_pairs_en_ur

--- public/src/app_language_2_upload_pairs_en_ur.mjs
+++ public/src/app_language_2_upload_pairs_en_ur.mjs
@@ -1,10 +1,6 @@
 import { defintion_en_ur } from "./defintion_en_ur.mjs";
 import { app_language_2_upload_pairs_get } from "./app_language_2_upload_pairs_get.mjs";
-export async function app_language_2_upload_pairs_en_ur(
-  language_from,
-  language_to,
-  limit,
-) {
+export async function app_language_2_upload_pairs_en_ur(limit) {
   let definitions_get = async function (w) {
     let v = await defintion_en_ur(w);
     return v;
