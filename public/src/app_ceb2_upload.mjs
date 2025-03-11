11bc655b8f8589cc8e2cad9bf23565311468e833
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 16:02:56 -0400

    2025-03-11T20:02:53.284Z watch   app_ceb2_upload

--- public/src/app_ceb2_upload.mjs
+++ public/src/app_ceb2_upload.mjs
@@ -1,15 +1,9 @@
+import { app_language_2_upload_pairs_ceb2 } from "./app_language_2_upload_pairs_ceb2.mjs";
 import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
-import { app_language_2_upload_pairs_bohol_cache } from "./app_language_2_upload_pairs_bohol_cache.mjs";
 export async function app_ceb2_upload() {
   let from = "ceb";
   let to = "en";
-  let limit = 1000;
-  let language_to = "Cebuano";
-  let language_from = "English";
-  let pairs = await app_language_2_upload_pairs_bohol_cache(
-    language_from,
-    language_to,
-    limit,
-  );
+  let limit = 2000;
+  let pairs = await app_language_2_upload_pairs_ceb2(limit);
   await app_language_2_upload_pairs(pairs, from, to);
 }
