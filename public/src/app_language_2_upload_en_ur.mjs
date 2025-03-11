0117e7343e17ae0c0695d184c0e229060e545e85
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 12:53:39 -0400

    2025-03-11T16:53:36.873Z watch   app_language_2_upload_en_ur

--- public/src/app_language_2_upload_en_ur.mjs
+++ public/src/app_language_2_upload_en_ur.mjs
@@ -2,5 +2,7 @@ import { app_language_2_upload_pairs_en_ur_cache } from "./app_language_2_upload
 import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
 export async function app_language_2_upload_en_ur(limit, from, to) {
   let pairs = await app_language_2_upload_pairs_en_ur_cache(limit);
+  let v = pairs;
+  return v;
   await app_language_2_upload_pairs(pairs, from, to);
 }
