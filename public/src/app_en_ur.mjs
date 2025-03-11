6af0bf1c2660d59f09a9e2593fa72ecc2dcfe839
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 13:22:57 -0400

    2025-03-11T17:22:56.379Z watch   apps_list

--- public/src/app_en_ur.mjs
+++ public/src/app_en_ur.mjs
@@ -1,4 +1,6 @@
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_en_ur() {
-  let root = html_style_default_initialize();
+  let language_learn = "en";
+  let language_fluent = "ur";
+   app_language_2(app_tgl, language_learn, language_fluent);
 }
