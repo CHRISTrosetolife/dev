6e96d0ff92c0902eb2b4be4ede462cbbe2b7392d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 12:53:15 -0400

    2025-03-11T16:53:14.916Z watch   app_en_ur_upload

--- public/src/app_en_ur_upload.mjs
+++ public/src/app_en_ur_upload.mjs
@@ -1,15 +1,7 @@
 import { app_language_2_upload_en_ur } from "./app_language_2_upload_en_ur.mjs";
 export async function app_en_ur_upload() {
-  let from = "tgl";
-  let to = "en";
+  let from = "en";
+  let to = "ur";
   let limit = 1000;
-  let language_to = "Tagalog";
-  let language_from = "English";
-  await app_language_2_upload_en_ur(
-    language_from,
-    language_to,
-    limit,
-    from,
-    to,
-  );
+  await app_language_2_upload_en_ur(limit, from, to);
 }
