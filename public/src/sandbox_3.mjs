0511f3b8876b989f461d294a5b7ed97333e90199
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 15:14:18 -0400

    2025-03-11T19:14:14.535Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,8 +1,8 @@
+import { each_async } from "./each_async.mjs";
 import { log } from "./log.mjs";
 import { list_multiple_is } from "./list_multiple_is.mjs";
 import { ceb_definition_en_2 } from "./ceb_definition_en_2.mjs";
 import { english_words_dictionary } from "./english_words_dictionary.mjs";
-import { each_log_async } from "./each_log_async.mjs";
 import { http_cache } from "./http_cache.mjs";
 import { object_properties } from "./object_properties.mjs";
 export async function sandbox_3() {
@@ -10,7 +10,7 @@ export async function sandbox_3() {
     "https://raw.githubusercontent.com/first20hours/google-10000-english/refs/heads/master/google-10000-english-usa.txt",
   );
   let filtered = await english_words_dictionary();
-  await each_log_async(filtered, async function (t) {
+  await each_async(filtered, async function (t) {
     let r = await ceb_definition_en_2(t);
     let p = object_properties(r);
     if (list_multiple_is(p)) {
