a53165dcd9e5423091ddc0c5bd1a6133db8fc622
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 15:59:23 -0400

    2025-03-11T19:59:23.216Z watch   app_language_2_upload_pairs_ceb2

--- public/src/app_language_2_upload_pairs_ceb2.mjs
+++ public/src/app_language_2_upload_pairs_ceb2.mjs
@@ -1,16 +1,14 @@
+import { each } from "./each.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
-import { log } from "./log.mjs";
 import { ceb_definition_en_2 } from "./ceb_definition_en_2.mjs";
 import { each_async } from "./each_async.mjs";
 import { english_words_dictionary } from "./english_words_dictionary.mjs";
 export async function app_language_2_upload_pairs_ceb2() {
-  let filtered = await english_words_dictionary();
-  await each_async(filtered, async function (t) {
-    let r = await ceb_definition_en_2(t);
-    if (object_property_exists(object, "property_name")) {
-      log({
-        r,
-      });
+  let words = await english_words_dictionary();
+  await each_async(words, async function (word) {
+    let r = await ceb_definition_en_2(word);
+    if (object_property_exists(object, word)) {
+      each(list, function (item) {});
     }
   });
 }
