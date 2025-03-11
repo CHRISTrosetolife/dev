d3baa99b0d39b2b4f195a793ca8c8204d046c353
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 16:07:07 -0400

    2025-03-11T20:07:06.401Z watch   app_ceb2_upload

--- public/src/app_ceb2_upload.mjs
+++ public/src/app_ceb2_upload.mjs
@@ -1,9 +1,9 @@
-import { app_language_2_upload_pairs_ceb2 } from "./app_language_2_upload_pairs_ceb2.mjs";
+import { app_language_2_upload_pairs_ceb2_cache } from "./app_language_2_upload_pairs_ceb2_cache.mjs";
 import { app_language_2_upload_pairs } from "./app_language_2_upload_pairs.mjs";
 export async function app_ceb2_upload() {
   let from = "ceb";
   let to = "en";
   let limit = 2000;
-  let pairs = await app_language_2_upload_pairs_ceb2(limit);
+  let pairs = await app_language_2_upload_pairs_ceb2_cache(limit);
   await app_language_2_upload_pairs(pairs, from, to);
 }
