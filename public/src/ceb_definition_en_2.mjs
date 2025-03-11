e983e6c833fa858bb8308c7381bebff45a3073f4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 15:10:56 -0400

    2025-03-11T19:10:54.639Z watch   ceb_definition_en_2

--- public/src/ceb_definition_en_2.mjs
+++ public/src/ceb_definition_en_2.mjs
@@ -1,4 +1,4 @@
-import { list_adder } from "./list_adder.mjs";
+import { null_not_is } from "./null_not_is.mjs";
 import { html_parse_text_trim_whitespace } from "./html_parse_text_trim_whitespace.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
 import { ceb_definition_prefix_en } from "./ceb_definition_prefix_en.mjs";
@@ -7,19 +7,21 @@ import { each } from "./each.mjs";
 import { html_parse_tag_named_a_list_filter } from "./html_parse_tag_named_a_list_filter.mjs";
 import { ceb_definition_en } from "./ceb_definition_en.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
+import { assert } from "./assert.mjs";
 export async function ceb_definition_en_2(d) {
   let children = await ceb_definition_en(d);
   let as = html_parse_tag_named_a_list_filter(children);
   let prefix_1 = ceb_definition_prefix_ceb();
   let prefix_2 = ceb_definition_prefix_en();
   let en = null;
-  list_adder(function (la) {});
+  let result = {};
   each(as, function (a) {
     let h = html_parse_href(a);
     let t = html_parse_text_trim_whitespace(a);
     if (string_starts_with(h, prefix_2)) {
       en = t;
     } else if (string_starts_with(h, prefix_1)) {
+      assert(null_not_is, [en]);
     }
   });
 }
