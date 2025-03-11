0900341c71c4cc9904f72893e32b2a5f92bff145
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 16:06:55 -0400

    2025-03-11T20:06:54.755Z fcn app_language_2_upload_pairs_ceb2

---
+++ public/src/app_language_2_upload_pairs_ceb2_cache.mjs
@@ -0,0 +1,5 @@
+import { app_language_2_upload_pairs_ceb2 } from "./app_language_2_upload_pairs_ceb2.mjs";
+import { function_cache } from "./function_cache.mjs";
+export async function app_language_2_upload_pairs_ceb2_cache(limit) {
+  return await function_cache(app_language_2_upload_pairs_ceb2, [limit]);
+}
