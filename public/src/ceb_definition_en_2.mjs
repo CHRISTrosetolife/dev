a3274313dd05fcfd119a1b02ddbbc95ec9a2c090
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 15:01:24 -0400

    2025-03-11T19:01:21.374Z watch   ceb_definition_en_2

--- public/src/ceb_definition_en_2.mjs
+++ public/src/ceb_definition_en_2.mjs
@@ -4,5 +4,5 @@ import { ceb_definition_en } from "./ceb_definition_en.mjs";
 export async function ceb_definition_en_2(d) {
   let children = await ceb_definition_en(d);
   let as = html_parse_tag_named_a_list_filter(children);
-  each(list, function (item) {});
+  each(as, function (a) {});
 }
