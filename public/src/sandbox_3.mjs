6cf927f52501f1f55da24d97b80450cc08c03b53
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 16:02:44 -0400

    2025-03-11T20:02:44.361Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -4,5 +4,5 @@ export async function sandbox_3() {
   let text2 = await http_cache(
     "https://raw.githubusercontent.com/first20hours/google-10000-english/refs/heads/master/google-10000-english-usa.txt",
   );
-  await app_language_2_upload_pairs_ceb2();
+  await app_language_2_upload_pairs_ceb2(2000);
 }
