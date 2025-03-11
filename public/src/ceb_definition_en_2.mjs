1a10267b334fed6200ba0b582f201f40725d926b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 15:05:48 -0400

    2025-03-11T19:05:46.765Z watch   ceb_definition_en_2

--- public/src/ceb_definition_en_2.mjs
+++ public/src/ceb_definition_en_2.mjs
@@ -4,6 +4,7 @@ import { ceb_definition_prefix_ceb } from "./ceb_definition_prefix_ceb.mjs";
 import { each } from "./each.mjs";
 import { html_parse_tag_named_a_list_filter } from "./html_parse_tag_named_a_list_filter.mjs";
 import { ceb_definition_en } from "./ceb_definition_en.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
 export async function ceb_definition_en_2(d) {
   let children = await ceb_definition_en(d);
   let as = html_parse_tag_named_a_list_filter(children);
@@ -11,7 +12,8 @@ export async function ceb_definition_en_2(d) {
   let prefix_2 = ceb_definition_prefix_en();
   each(as, function (a) {
     let h = html_parse_href(a);
-    if (false) {
+    if (string_starts_with(h, prefix_1)) {
+    } else {
     }
   });
 }
