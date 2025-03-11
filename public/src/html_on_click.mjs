f8ae96ad2943277df518a159a2e0afbe284944be
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 17:22:57 -0400

    2025-03-11T21:22:56.463Z watch   html_on_click

--- public/src/html_on_click.mjs
+++ public/src/html_on_click.mjs
@@ -1,8 +1,9 @@
 import { html_load } from "./html_load.mjs";
 import { html_on } from "./html_on.mjs";
 export function html_on_click(component, lambda) {
+  let lambda2 = html_load;
   let v = html_on(component, "click", async function on_click() {
-    await html_load(lambda);
+    await lambda2(lambda);
   });
   return v;
 }
