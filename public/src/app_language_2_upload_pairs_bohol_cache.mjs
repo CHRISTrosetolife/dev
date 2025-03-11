0472000dee58f0603aba6495d589295b84c6e351
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 12:42:34 -0400

    2025-03-11T16:42:33.131Z watch   app_language_2_upload_pairs_bohol_cache

--- public/src/app_language_2_upload_pairs_bohol_cache.mjs
+++ public/src/app_language_2_upload_pairs_bohol_cache.mjs
@@ -1,4 +1,4 @@
-import { function_cache_force } from "./function_cache_force.mjs";
+import { function_cache } from "./function_cache.mjs";
 import { app_language_2_upload_pairs_bohol } from "./app_language_2_upload_pairs_bohol.mjs";
 export async function app_language_2_upload_pairs_bohol_cache(
   language_from,
@@ -7,6 +7,6 @@ export async function app_language_2_upload_pairs_bohol_cache(
 ) {
   let fn = app_language_2_upload_pairs_bohol;
   let args = [language_from, language_to, limit];
-  let v = await function_cache_force(fn, args);
+  let v = await function_cache(fn, args);
   return v;
 }
