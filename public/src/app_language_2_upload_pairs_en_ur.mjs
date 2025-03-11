8b5c401b74d329d61bfb29c5d7ded522009c3431
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 12:48:27 -0400

    2025-03-11T16:48:26.597Z watch   app_language_2_upload_pairs_en_ur

--- public/src/app_language_2_upload_pairs_en_ur.mjs
+++ public/src/app_language_2_upload_pairs_en_ur.mjs
@@ -1,12 +1,12 @@
+import { defintion_en_ur } from "./defintion_en_ur.mjs";
 import { app_language_2_upload_pairs_get } from "./app_language_2_upload_pairs_get.mjs";
-import { definition_bohol } from "./definition_bohol.mjs";
 export async function app_language_2_upload_pairs_en_ur(
   language_from,
   language_to,
   limit,
 ) {
   let definitions_get = async function (w) {
-    let v = await definition_bohol(w, language_from, language_to);
+    let v = await defintion_en_ur(w);
     return v;
   };
   let v2 = await app_language_2_upload_pairs_get(definitions_get, limit);
