b509f0223ad913cda8cf750cbc74b7f10a956a26
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-11 20:23:57 -0400

    2025-03-12T00:23:55.018Z watch   bible_interlinear_definition

--- public/src/bible_interlinear_definition.mjs
+++ public/src/bible_interlinear_definition.mjs
@@ -2,17 +2,15 @@ import { string_empty_not_is } from "./string_empty_not_is.mjs";
 import { assert } from "./assert.mjs";
 import { bible_interlinear_strongs_cache } from "./bible_interlinear_strongs_cache.mjs";
 import { bible_interlinear_definition_generic } from "./bible_interlinear_definition_generic.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 export async function bible_interlinear_definition(language, strong) {
   assert(string_empty_not_is, [strong]);
   let label = "Short Definition:";
   let html_lambda = bible_interlinear_strongs_cache;
-  let ds = await bible_interlinear_definition_generic(
+  let d = await bible_interlinear_definition_generic(
     html_lambda,
     language,
     strong,
     [label],
   );
-  let v = object_property_get(ds, "definition");
-  return v;
+  return d;
 }
