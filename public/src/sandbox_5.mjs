8ffa2e222b1a61725ee88fb7b937609f74618841
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-13 16:08:33 -0400

    2025-03-13T20:08:32.678Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,9 +1,7 @@
-import { list_empty_not_is } from "./list_empty_not_is.mjs";
-import { defintion_en_ur } from "./defintion_en_ur.mjs";
-import { each_log_async } from "./each_log_async.mjs";
-import { english_words_dictionary_take_soft } from "./english_words_dictionary_take_soft.mjs";
-import { audio_upload } from "./audio_upload.mjs";
+import { prayers_countries } from "./prayers_countries.mjs";
+import { list_join_newline } from "./list_join_newline.mjs";
 import { prayers_list } from "./prayers_list.mjs";
 export async function sandbox_5() {
-  $r,list_join_newline(prayers_list(prayers_countries()))
+  let v = list_join_newline(prayers_list(prayers_countries()));
+  return v;
 }
