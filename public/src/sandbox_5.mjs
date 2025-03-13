060be1e0326df787aa7a2ed2b8f7cc1617e5653a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-13 16:08:28 -0400

    2025-03-13T20:08:28.309Z watch   prayers

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -3,12 +3,7 @@ import { defintion_en_ur } from "./defintion_en_ur.mjs";
 import { each_log_async } from "./each_log_async.mjs";
 import { english_words_dictionary_take_soft } from "./english_words_dictionary_take_soft.mjs";
 import { audio_upload } from "./audio_upload.mjs";
+import { prayers_list } from "./prayers_list.mjs";
 export async function sandbox_5() {
-  let words = await english_words_dictionary_take_soft(1000);
-  await each_log_async(words, async function (w) {
-    let definitions = await defintion_en_ur(w);
-    if (list_empty_not_is(definitions)) {
-      await audio_upload("en", w);
-    }
-  });
+  $r,list_join_newline(prayers_list(prayers_countries()))
 }
