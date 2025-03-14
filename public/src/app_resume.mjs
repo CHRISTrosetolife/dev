81d6e76fffc46af4e0ef31cf256709985e9e1030
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-14 12:15:10 -0400

    2025-03-14T16:15:08.212Z watch   app_resume

--- public/src/app_resume.mjs
+++ public/src/app_resume.mjs
@@ -1,3 +1,5 @@
+import { list_map } from "./list_map.mjs";
+import { list_between_surround } from "./list_between_surround.mjs";
 import { function_name_to_url_github_style_link_list_bewteen_noop } from "./function_name_to_url_github_style_link_list_bewteen_noop.mjs";
 import { me_phone } from "./me_phone.mjs";
 import { html_style_link_blank } from "./html_style_link_blank.mjs";
@@ -73,15 +75,21 @@ export function app_resume() {
   let ceb = app_resume_header_3(root, "Language Vocabularly Learning");
   html_cycle_p(
     root,
-    function_name_to_url_github_style_link_list_bewteen_noop([
-      fn_name("bible_words_language_count"),
-      fn_name("english_words_dictionary"),
-      html_style_link_blank("ceb2.html"),
-      html_style_link_blank("tgl.html"),
-      html_style_link_blank("el.html"),
-      html_style_link_blank("he.html"),
-      html_style_link_blank("en_ur.html"),
-    ]),
+    list_between_surround(
+      list_map(
+        [
+          fn_name("bible_words_language_count"),
+          fn_name("english_words_dictionary"),
+          html_style_link_blank("ceb2.html"),
+          html_style_link_blank("tgl.html"),
+          html_style_link_blank("el.html"),
+          html_style_link_blank("he.html"),
+          html_style_link_blank("en_ur.html"),
+        ],
+        function_name_to_url_github_style_link,
+      ),
+      noop,
+    ),
     'Allows user to learn most common words in a language; Most frequent words in English are `computed from either the Bible` or the `most common words found in an English dictionary`; User is presented with words in one language and must remember choose the corresponding word or words in another language; Sometimes user is presented with words from the language they already know and sometimes the user is presented with words from the language they are learning; "both directions" are taught. There are apps for: `English to Cebuano`, `English to Tagalog (with audio)`, `English to Biblical Greek (with Audio)`, `English to Biblical Hebrew` and `Urdu to English`.',
   );
   if (false) {
