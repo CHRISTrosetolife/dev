2dd5451fb0ebadcfb37c9101b428b9e34a01a4af
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-14 12:09:41 -0400

    2025-03-14T16:09:41.042Z watch   app_resume

--- public/src/app_resume.mjs
+++ public/src/app_resume.mjs
@@ -76,8 +76,9 @@ export function app_resume() {
     root,
     function_name_to_url_github_style_link_list_bewteen_noop([
       fn_name("bible_words_language_count"),
+      fn_name("english_words_dictionary"),
     ]),
-    "Allows user to learn most common words in a language; Most frequent words in English are computed from either the `Bible` or the most common words found in an English dictionary -- Most frequent words in English and Cebuano are computed from Bible word frequencies; User is presented Cebuano words and their English translations; Then user chooses English translation for each Cebuano word; then user chooses Cebuano word for each English translation; Then user review new and old words until 96 words are learned; Then user chooses the next group of 96 words to study, beginning with the first 6 words in the group; When quizzing, user chooses between choosing an entire word at once or 1, 2 or 3 letters at a time",
+    'Allows user to learn most common words in a language; Most frequent words in English are `computed from either the Bible` or the `most common words found in an English dictionary`; User is presented with words in one language and must remember choose the corresponding word or words in another language; Sometimes user is presented with words from the language they already know and sometimes the user is presented with words from the language they are learning; "both directions" are taught. There are apps for: English to Cebuano, English to Tagalog, English to Ancient Greek, English to Ancient Hebrew and Urdu to English.',
   );
   if (false) {
     app_resume_audio_bible(root);
