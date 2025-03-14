74b263aac6a29da41ccacaea14c10f66bddf0dca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-14 17:26:55 -0400

    2025-03-14T21:26:54.454Z de countries_names_list_generate

--- public/src/countries_names_list_generate.mjs
+++
@@ -1,5 +0,0 @@
-import { countries_names } from "./countries_names.mjs";
-import { generate_list_fn } from "./generate_list_fn.mjs";
-export async function countries_names_list_generate() {
-  await generate_list_fn(countries_names, countries_names_list_generate);
-}
