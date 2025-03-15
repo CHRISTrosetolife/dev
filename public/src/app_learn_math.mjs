6a7ae580aa24a0b8e03d4dcca5171ea0ed50a06a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-15 14:30:17 -0400

    2025-03-15T18:30:16.653Z watch   app_learn_math

--- public/src/app_learn_math.mjs
+++ public/src/app_learn_math.mjs
@@ -1,3 +1,4 @@
+import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { html_button } from "./html_button.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_learn_math() {
@@ -5,5 +6,7 @@ export function app_learn_math() {
   if (false) {
     html_button(root, "Factoring whole numbers", function () {});
   }
-  html_button(root, "Factoring polynomials", function () {});
+  html_button(root, "Factoring polynomials", function () {
+    html_clear_scroll_top(root);
+  });
 }
