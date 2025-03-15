e2639b0efc8ef0e218f647537dfb5f5541a9884b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-15 14:31:15 -0400

    2025-03-15T18:31:14.882Z watch   app_learn_math

--- public/src/app_learn_math.mjs
+++ public/src/app_learn_math.mjs
@@ -8,7 +8,7 @@ export function app_learn_math() {
   }
   html_button(root, "Factoring polynomials", function () {
     html_clear_scroll_top(root);
-    let answer = 1;
+    let answer_1 = 1;
     let answer_2 = 1;
   });
 }
