25f2a9834df09e5a246b96ebc800ad5614eaa404
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-16 17:34:05 -0400

    2025-03-16T21:34:01.862Z watch   each_range_1_nested

--- public/src/each_range_1_nested.mjs
+++ public/src/each_range_1_nested.mjs
@@ -1,8 +1,9 @@
+import { each_range_1 } from "./each_range_1.mjs";
 import { each } from "./each.mjs";
 export function each_range_1_nested(max, each) {
   each_range_1(max, function (a) {
     each_range_1(max, function (b) {
-      each(a, b); 
+      each(a, b);
     });
   });
 }
