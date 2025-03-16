6329f2f77dd54ebe00db0f60dbc76d8eed23966d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-16 17:13:40 -0400

    2025-03-16T21:13:39.662Z watch   html_button_next

--- public/src/html_button_next.mjs
+++ public/src/html_button_next.mjs
@@ -1,5 +1,7 @@
 import { html_button } from "./html_button.mjs";
 import { html_button_next_text } from "./html_button_next_text.mjs";
 export function html_button_next(root, next_on_click) {
-  return html_button(root, html_button_next_text(), next_on_click);
+  let after = "";
+  let v = html_button(root, html_button_next_text(), next_on_click);
+  return v;
 }
