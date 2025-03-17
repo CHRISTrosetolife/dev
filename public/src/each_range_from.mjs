15bc5f4ffa57496e7024395f674a5093f0471c8a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-17 17:17:36 -0400

    2025-03-17T21:17:36.138Z watch   each_range_from

--- public/src/each_range_from.mjs
+++ public/src/each_range_from.mjs
@@ -1,5 +1,5 @@
 import { range_from } from "./range_from.mjs";
 import { each } from "./each.mjs";
-export function each_range_from(n, lambda) {
-  each(range_from(n), lambda);
+export function each_range_from(from, to, lambda) {
+  each(range_from(from, to), lambda);
 }
