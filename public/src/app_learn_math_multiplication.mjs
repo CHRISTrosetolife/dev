33c539ee5905320e822255f5e17def3f7cd3a4e6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-17 18:27:42 -0400

    2025-03-17T22:27:41.449Z watch   app_learn_math_multiplication

--- public/src/app_learn_math_multiplication.mjs
+++ public/src/app_learn_math_multiplication.mjs
@@ -6,28 +6,32 @@ import { each_range } from "./each_range.mjs";
 import { html_button } from "./html_button.mjs";
 export function app_learn_math_multiplication(root) {
   let multiplication_choices = [];
-  html_button(root, "Multiplication", function () {
-    function refill() {
-      each_range(11, function (a) {
-        let b = 0;
-        list_add(multiplication_choices, {
-          left: a,
-          right: b,
+  html_button(
+    root,
+    "Multiplication",
+    function app_learn_math_multiplication_quiz() {
+      function refill() {
+        each_range(11, function (a) {
+          let b = 0;
+          list_add(multiplication_choices, {
+            left: a,
+            right: b,
+          });
+          list_add(multiplication_choices, {
+            left: b,
+            right: a,
+          });
         });
-        list_add(multiplication_choices, {
-          left: b,
-          right: a,
-        });
-      });
-    }
-    multiplication_choices = list_unique_json_shuffle(multiplication_choices);
-    app_learn_math_quiz(
-      root,
-      digits_10(),
-      function () {},
-      problem_next,
-      refill,
-      multiplication_choices,
-    );
-  });
+      }
+      multiplication_choices = list_unique_json_shuffle(multiplication_choices);
+      app_learn_math_quiz(
+        root,
+        digits_10(),
+        function () {},
+        app_learn_math_multiplication_quiz,
+        refill,
+        multiplication_choices,
+      );
+    },
+  );
 }
