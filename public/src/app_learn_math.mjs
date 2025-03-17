66b5f133073bb4dfea6c6d5f143468b15b2f8bed
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-17 17:22:15 -0400

    2025-03-17T21:22:15.067Z watch   app_learn_math

--- public/src/app_learn_math.mjs
+++ public/src/app_learn_math.mjs
@@ -1,8 +1,6 @@
 import { app_learn_math_main } from "./app_learn_math_main.mjs";
-import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_learn_math() {
   let root = html_style_default_initialize();
-  html_clear_scroll_top_centered(root);
   app_learn_math_main(root);
 }
