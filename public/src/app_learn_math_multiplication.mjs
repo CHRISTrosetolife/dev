cbde07d0e521cb0a3a5f589a85ac285a52966994
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-17 17:49:14 -0400

    2025-03-17T21:49:13.936Z watch   app_learn_math_multiplication

---
+++ public/src/app_learn_math_multiplication.mjs
@@ -0,0 +1,29 @@
+import { app_learn_math_quiz } from "./app_learn_math_quiz.mjs";
+import { list_unique_json_shuffle } from "./list_unique_json_shuffle.mjs";
+import { list_add } from "./list_add.mjs";
+import { each_range } from "./each_range.mjs";
+import { html_button } from "./html_button.mjs";
+export function app_learn_math_multiplication(root) {
+  let multiplication_choices;
+  html_button(root, "Multiplication", function () {
+    each_range(11, function (a) {
+      let b = 0;
+      list_add(multiplication_choices, {
+        left: a,
+        right: b,
+      });
+      list_add(multiplication_choices, {
+        left: b,
+        right: a,
+      });
+    });
+    multiplication_choices = list_unique_json_shuffle(multiplication_choices);
+    app_learn_math_quiz(
+      root,
+      question,
+      button_strings,
+      expecteds,
+      problem_next,
+    );
+  });
+}
