8309415f1c1a81c8111f3651b3163b7951b42cc7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-17 17:45:00 -0400

    2025-03-17T21:44:59.703Z watch   app_learn_math_polynomials_factoring

---
+++ public/src/app_learn_math_polynomials_factoring.mjs
@@ -0,0 +1,106 @@
+import { app_learn_math_quiz } from "./app_learn_math_quiz.mjs";
+import { string_split_empty } from "./string_split_empty.mjs";
+import { digits_10 } from "./digits_10.mjs";
+import { list_concat_multiple } from "./list_concat_multiple.mjs";
+import { list_reverse } from "./list_reverse.mjs";
+import { list_second } from "./list_second.mjs";
+import { list_first } from "./list_first.mjs";
+import { html_sup_text } from "./html_sup_text.mjs";
+import { html_span_text } from "./html_span_text.mjs";
+import { html_span } from "./html_span.mjs";
+import { list_pop } from "./list_pop.mjs";
+import { list_empty_is } from "./list_empty_is.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { html_button } from "./html_button.mjs";
+import { list_shuffle } from "./list_shuffle.mjs";
+import { list_unique_json } from "./list_unique_json.mjs";
+import { list_add } from "./list_add.mjs";
+import { each_range_1 } from "./each_range_1.mjs";
+import { each_range_from } from "./each_range_from.mjs";
+export function app_learn_math_polynomials_factoring(root) {
+  let factoring_threshold = 7;
+  factoring_threshold, " value is seven because older son learned this";
+  factoring_button(1, factoring_threshold, "easier");
+  let factoring_max_upper = 12;
+  factoring_max_upper,
+    " value because that is the max number learned in times tables in school";
+  factoring_button(factoring_threshold + 1, factoring_max_upper, "harder");
+  function factoring_button(factoring_min, factoring_max, description) {
+    let factor_polynomials_choices = [];
+    function factor_polynomials_choices_refill(factoring_min, factoring_max) {
+      each_range_from(factoring_min, factoring_max, function (answer_1) {
+        each_range_1(factoring_max, function (answer_2) {
+          list_add(factor_polynomials_choices, {
+            answer_1,
+            answer_2,
+          });
+        });
+      });
+      factor_polynomials_choices = list_unique_json(factor_polynomials_choices);
+      list_shuffle(factor_polynomials_choices);
+    }
+    html_button(
+      root,
+      string_combine_multiple([
+        "Factoring polynomials (",
+        factoring_min,
+        " - ",
+        factoring_max,
+        ") (",
+        description,
+        ")",
+      ]),
+      function factor_polynomials() {
+        let variable = "x";
+        let problem_next = factor_polynomials;
+        let button_strings = list_concat_multiple([
+          digits_10(),
+          string_split_empty("()+"),
+          [variable],
+        ]);
+        if (list_empty_is(factor_polynomials_choices)) {
+          factor_polynomials_choices_refill(factoring_min, factoring_max);
+        }
+        let { answer_1, answer_2 } = list_pop(factor_polynomials_choices);
+        let sum = answer_1 + answer_2;
+        let product = answer_1 * answer_2;
+        function question(root) {
+          let equation = html_span(root);
+          html_span_text(equation, variable);
+          html_sup_text(equation, "2");
+          html_span_text(equation, "+");
+          html_span_text(equation, sum);
+          html_span_text(equation, variable);
+          html_span_text(equation, "+");
+          html_span_text(equation, product);
+        }
+        let choices = [answer_1, answer_2];
+        let expecteds = [];
+        function expected_add(choices2) {
+          let expected = string_combine_multiple([
+            "(",
+            variable,
+            "+",
+            list_first(choices2),
+            ")(",
+            variable,
+            "+",
+            list_second(choices2),
+            ")",
+          ]);
+          list_add(expecteds, expected);
+        }
+        expected_add(choices);
+        list_reverse(choices);
+        expected_add(choices);
+        app_learn_math_quiz(
+          root,
+          question,
+          button_strings,
+          expecteds,
+          problem_next,
+        );
+      },
+    );
+  }
+}
