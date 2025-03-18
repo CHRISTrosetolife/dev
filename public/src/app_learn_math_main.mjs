11ff818b132425dc8a7f4fe409ca7db16d01d188
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-18 17:08:58 -0400

    2025-03-18T21:08:57.784Z watch   app_learn_math_main

--- public/src/app_learn_math_main.mjs
+++ public/src/app_learn_math_main.mjs
@@ -1,3 +1,4 @@
+import { app_learn_math_division_choices } from "./app_learn_math_division_choices.mjs";
 import { app_learn_math_multiplication_choices } from "./app_learn_math_multiplication_choices.mjs";
 import { app_learn_math_polynomials_factoring } from "./app_learn_math_polynomials_factoring.mjs";
 import { html_clear_scroll_top_centered } from "./html_clear_scroll_top_centered.mjs";
@@ -8,5 +9,6 @@ export function app_learn_math_main(root) {
     html_button(root, "Factoring whole numbers", function () {});
   }
   app_learn_math_multiplication_choices(root);
+  app_learn_math_division_choices(root);
   app_learn_math_polynomials_factoring(root);
 }
