05ef845603547b755057550658d6e7c196fff452
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-18 16:57:53 -0400

    2025-03-18T20:57:52.590Z watch   app_learn_math_multiplication_choices_generic

--- public/src/app_learn_math_multiplication_choices_generic.mjs
+++ public/src/app_learn_math_multiplication_choices_generic.mjs
@@ -1,5 +1,4 @@
 import { app_learn_math_multiplication_choices_max_ranged } from "./app_learn_math_multiplication_choices_max_ranged.mjs";
-import { app_learn_math_multiplication_choices_max } from "./app_learn_math_multiplication_choices_max.mjs";
 import { app_learn_math_multiplication_choices } from "./app_learn_math_multiplication_choices.mjs";
 import { digits_10 } from "./digits_10.mjs";
 import { app_learn_math_quiz } from "./app_learn_math_quiz.mjs";
@@ -23,8 +22,8 @@ export function app_learn_math_multiplication_choices_generic(
   html_button_back(root, function () {
     app_learn_math_main(root);
   });
-  let max = app_learn_math_multiplication_choices_max();
-  let max_ranged = app_learn_math_multiplication_choices_max_ranged(max);
+  let max = 10;
+  let max_ranged = max + 1;
   html_span_text(root, "Do each of these in order");
   each_range(max_ranged, function (by) {
     html_button(
