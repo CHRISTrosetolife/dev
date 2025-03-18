c9ae2be92506cbeba105afdbec3b60554b696a08
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-18 15:08:28 -0400

    2025-03-18T19:08:27.105Z c app_learn_math_multiplication_choices app_learn_math_division_choices

---
+++ public/src/app_learn_math_division_choices.mjs
@@ -0,0 +1,21 @@
+import { app_learn_math_multiplication_choices_generic } from "./app_learn_math_multiplication_choices_generic.mjs";
+import { tautology } from "./tautology.mjs";
+export function app_learn_math_division_choices(root) {
+  let filter = tautology;
+  function symbol_get() {
+    let v2 = "×";
+    return v2;
+  }
+  let operation_word = "Multiply";
+  function operation(left, right) {
+    let v3 = left * right;
+    return v3;
+  }
+  app_learn_math_multiplication_choices_generic(
+    root,
+    operation_word,
+    filter,
+    symbol_get,
+    operation,
+  );
+}
