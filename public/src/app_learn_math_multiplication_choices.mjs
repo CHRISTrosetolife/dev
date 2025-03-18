ada8651bfffd25ce3a1b1af9c6175834b932cc32
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-18 15:07:59 -0400

    2025-03-18T19:07:58.364Z watch   app_learn_math_multiplication_choices_generic

--- public/src/app_learn_math_multiplication_choices.mjs
+++ public/src/app_learn_math_multiplication_choices.mjs
@@ -21,73 +21,77 @@ export function app_learn_math_multiplication_choices(root) {
     let v3 = left * right;
     return v3;
   }
-  html_clear_scroll_top_centered(root);
-  html_button_back(root, function () {
-    app_learn_math_main(root);
-  });
-  let max = 10;
-  let max_ranged = max + 1;
-  html_span_text(root, "Do each of these in order");
-  each_range(max_ranged, function (by) {
-    html_button(
-      root,
-      string_combine_multiple([operation_word, " by ", by]),
-      function () {
-        function refill(multiplication_choices) {
-          each_range(max_ranged, function (other) {
-            list_add_left_right(multiplication_choices, by, other);
-          });
-        }
-        app_learn_math_multiplication_quiz(refill);
-      },
-    );
-  });
-  html_button(
-    root,
-    string_combine_multiple([operation_word, " by 1-", max]),
-    function () {
-      function refill(choices) {
-        each_range(max_ranged, function (a) {
-          each_range(max_ranged, function (b) {
-            list_add_left_right(choices, a, b);
-          });
-        });
-      }
-      app_learn_math_multiplication_quiz(refill);
-    },
-  );
-  function list_add_left_right(choices, a, b) {
-    let lrs = [
-      {
-        left: b,
-        right: a,
-      },
-      {
-        left: b,
-        right: a,
-      },
-    ];
-    each(lrs, function (lr) {
-      if (filter(lr)) {
-        list_add(choices, lr);
-      }
-    });
-  }
-  function app_learn_math_multiplication_quiz(refill) {
-    app_learn_math_quiz(
-      root,
-      function (question, popped) {
-        let { left, right } = popped;
-        html_span_text(question, left);
-        html_span_text(question, symbol_get());
-        html_span_text(question, right);
-        html_span_text(question, "=?");
-        let v = [operation(left, right)];
-        return v;
-      },
-      digits_10(),
-      refill,
-      app_learn_math_multiplication_choices,
-    );
-  }
+  app_learn_math_multiplication_choices_generic(root, operation_word, filter, symbol_get, operation);
 }
+function app_learn_math_multiplication_choices_generic(root, operation_word, filter, symbol_get, operation) {
+    html_clear_scroll_top_centered(root);
+    html_button_back(root, function () {
+        app_learn_math_main(root);
+    });
+    let max = 10;
+    let max_ranged = max + 1;
+    html_span_text(root, "Do each of these in order");
+    each_range(max_ranged, function (by) {
+        html_button(
+            root,
+            string_combine_multiple([operation_word, " by ", by]),
+            function () {
+                function refill(multiplication_choices) {
+                    each_range(max_ranged, function (other) {
+                        list_add_left_right(multiplication_choices, by, other);
+                    });
+                }
+                app_learn_math_multiplication_quiz(refill);
+            }
+        );
+    });
+    html_button(
+        root,
+        string_combine_multiple([operation_word, " by 1-", max]),
+        function () {
+            function refill(choices) {
+                each_range(max_ranged, function (a) {
+                    each_range(max_ranged, function (b) {
+                        list_add_left_right(choices, a, b);
+                    });
+                });
+            }
+            app_learn_math_multiplication_quiz(refill);
+        }
+    );
+    function list_add_left_right(choices, a, b) {
+        let lrs = [
+            {
+                left: b,
+                right: a,
+            },
+            {
+                left: b,
+                right: a,
+            },
+        ];
+        each(lrs, function (lr) {
+            if (filter(lr)) {
+                list_add(choices, lr);
+            }
+        });
+    }
+    function app_learn_math_multiplication_quiz(refill) {
+        app_learn_math_quiz(
+            root,
+            function (question, popped) {
+                let { left, right } = popped;
+                html_span_text(question, left);
+                html_span_text(question, symbol_get());
+                html_span_text(question, right);
+                html_span_text(question, "=?");
+                let v = [operation(left, right)];
+                return v;
+            },
+            digits_10(),
+            refill,
+            app_learn_math_multiplication_choices
+        );
+    }
+}
+
