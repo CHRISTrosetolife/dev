04ee5cf696103084e84d5d66d2dbca54a1c7431b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-03-18 15:09:56 -0400

    2025-03-18T19:09:55.854Z watch   app_learn_math_division_choices

--- public/src/app_learn_math_division_choices.mjs
+++ public/src/app_learn_math_division_choices.mjs
@@ -2,7 +2,7 @@ import { object_property_get } from "./object_property_get.mjs";
 import { app_learn_math_multiplication_choices_generic } from "./app_learn_math_multiplication_choices_generic.mjs";
 export function app_learn_math_division_choices(root) {
   let filter = function (lr) {
-    let v = object_property_get(object, "property_name");
+    let v = object_property_get(lr, "right") !== 0;
     return v;
   };
   function symbol_get() {
