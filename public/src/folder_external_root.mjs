d8c9e437e811feb94c519ddb2fec7457616d7f01
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-06 19:06:32 -0400

    2025-04-06T23:06:31.389Z watch   folder_external_root

--- public/src/folder_external_root.mjs
+++ public/src/folder_external_root.mjs
@@ -1,5 +1,6 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { path_join } from "./path_join.mjs";
 export function folder_external_root(folder) {
-  let v = path_join(["D:", folder]);
+  let v = path_join([string_combine_multiple(["D", ":"]), folder]);
   return v;
 }
