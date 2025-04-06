a7dc803158dd17521e77998073f0f894d385376d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-06 18:50:23 -0400

    2025-04-06T22:50:22.637Z watch   bible_audio_player_path

--- public/src/bible_audio_player_path.mjs
+++ public/src/bible_audio_player_path.mjs
@@ -1,13 +1,8 @@
 import { folder_external_root } from "./folder_external_root.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { bible_audio_player_drive_letter } from "./bible_audio_player_drive_letter.mjs";
 import { path_join } from "./path_join.mjs";
 export function bible_audio_player_path(name) {
   let drive_letter = bible_audio_player_drive_letter();
-  let v = path_join([
-    folder_external_root(string_combine_multiple([drive_letter, ":"])),
-    "bible",
-    name,
-  ]);
+  let v = path_join([folder_external_root("bible"), name]);
   return v;
 }
