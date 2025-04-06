f5fcd3bd608c14d2a82cdab3c41259a25c725222
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-06 09:21:57 -0400

    2025-04-06T13:21:56.302Z watch   emoji_wrap_music

---
+++ public/src/emoji_wrap_music.mjs
@@ -0,0 +1,5 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function emoji_wrap_music(praise) {
+  let v = string_combine_multiple(["🎵 ", praise, " 🎶"]);
+  return v;
+}
