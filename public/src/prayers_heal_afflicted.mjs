693e88d5c3064370a2362157059f5ffac439c21a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-06 09:37:40 -0400

    2025-04-06T13:37:39.950Z watch   prayers_heal_afflicted

--- public/src/prayers_heal_afflicted.mjs
+++ public/src/prayers_heal_afflicted.mjs
@@ -1,7 +1,7 @@
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function prayers_heal_afflicted(cough) {
   let v = string_combine_multiple([
-    "Heal me from the ",
+    "Heal me from any ",
     cough,
     " that afflicted me",
   ]);
