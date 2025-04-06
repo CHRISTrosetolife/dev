abd9ef5f1e440baa5bbc468ca0c7fe6c42fbd61a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-06 09:24:40 -0400

    2025-04-06T13:24:40.111Z watch   date_age

--- public/src/date_age.mjs
+++ public/src/date_age.mjs
@@ -1,1 +1,13 @@
-export function date_age() {}
+export function date_age() {
+  let birthDate = new Date(birthday);
+  let today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  let monthDifference = today.getMonth() - birthDate.getMonth();
+  if (
+    monthDifference < 0 ||
+    (monthDifference === 0 && today.getDate() < birthDate.getDate())
+  ) {
+    age--;
+  }
+  return age;
+}
