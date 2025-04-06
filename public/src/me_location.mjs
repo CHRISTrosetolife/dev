838e299a8234b6f42ee9d85c8148ac7b7cf1a8f9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-06 09:29:00 -0400

    2025-04-06T13:28:59.495Z watch   me_location

--- public/src/me_location.mjs
+++ public/src/me_location.mjs
@@ -1,1 +1,5 @@
-export function me_location() {}
+export function me_location() {
+  let v =
+    "I live in the USA: the United States of America, in the state of Florida, in the city of Jacksonville";
+  return v;
+}
