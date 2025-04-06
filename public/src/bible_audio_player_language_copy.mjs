0a4dfbf4470e28d0ea587993a56aa91d7a671259
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-06 19:00:23 -0400

    2025-04-06T23:00:22.575Z watch   bible_audio_player_language_copy

--- public/src/bible_audio_player_language_copy.mjs
+++ public/src/bible_audio_player_language_copy.mjs
@@ -1,3 +1,5 @@
+import { string_starts_with } from "./string_starts_with.mjs";
+import { list_filter } from "./list_filter.mjs";
 import { log } from "./log.mjs";
 import { bible_audio_player_generic_multiple } from "./bible_audio_player_generic_multiple.mjs";
 import { bible_audio_player_path } from "./bible_audio_player_path.mjs";
@@ -5,6 +7,10 @@ import { folder_read_shallow } from "./folder_read_shallow.mjs";
 export async function bible_audio_player_language_copy(language_folder_name) {
   let p = bible_audio_player_path(language_folder_name);
   let folders = await folder_read_shallow(p, "");
+  folders = list_filter(folders, function (f) {
+    let v = !string_starts_with(f, "zip");
+    return v;
+  });
   log({
     p,
     folders,
