af393ef12652f4c01f727ee574f5b9060ce01c85
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-07 07:56:32 -0400

    2025-04-07T11:56:31.490Z watch   app_share_verse_refresh

--- public/src/app_share_verse_refresh.mjs
+++ public/src/app_share_verse_refresh.mjs
@@ -145,9 +145,9 @@ export async function app_share_verse_refresh(
     {
       button_text: "💵 Money",
       value: string_combine_multiple([
-        "May you delight yourself in the LORD our God ",
+        "May you delight 😃 yourself in the LORD our God ",
         emojis_trinity(),
-        "and may the LORD grant you the desires of your heart 💓 ! 😭 I'm sorry to hear that you are in need of money 😟 ! I have no money to give to you 😞 .",
+        "and may the LORD grant you the desires of your heart 💓😊 ! 😭 I'm sorry to hear that you are in need of money 😟 ! I have no money to give to you 😞 .",
       ]),
     },
   ];
