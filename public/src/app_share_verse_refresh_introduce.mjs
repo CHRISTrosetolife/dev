44fd002a4f5aeb140038bc30f5ecc1842cdfb745
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-07 07:26:24 -0400

    2025-04-07T11:26:21.757Z watch   app_share_verse_refresh_introduce

--- public/src/app_share_verse_refresh_introduce.mjs
+++ public/src/app_share_verse_refresh_introduce.mjs
@@ -8,7 +8,7 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_share_verse_refresh_introduce() {
   let v = string_combine_multiple([
     app_share_verse_refresh_greet(),
-    " I believe Jesus died ✝️ , was buried 🪨 and rose ⬆️ to life ⛪ . I am a servant of God. My parents named me Jared Mathis. I have a wife. I preach 🗣 the gospel ✝️.",
+    " I believe Christ died ✝️ , was buried 🪨 and rose ⬆️ to life ⛪ . I am a servant of God. My parents named me Jared Mathis. I have a wife. I preach 🗣 the gospel ✝️.",
     emoji_wrap_music("I sing 🎤️ and play 🎹 Bible songs"),
     " . I am ",
     me_age(),
