db595823fa1aab0b1d1dc600d47c79ea2af58c13
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-08 16:12:10 -0400

    2025-04-08T20:12:09.735Z watch   lesson_array_three

--- public/src/lesson_array_three.mjs
+++ public/src/lesson_array_three.mjs
@@ -1,6 +1,5 @@
 import { lesson_array_two_indices } from "./lesson_array_two_indices.mjs";
 import { html_cycle_code_multiple } from "./html_cycle_code_multiple.mjs";
-import { html_span_text } from "./html_span_text.mjs";
 import { lesson_array_source_get } from "./lesson_array_source_get.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
 import { string_combine } from "./string_combine.mjs";
@@ -17,11 +16,12 @@ export function lesson_array_three() {
   );
   return v;
   function example_before(parent) {
-    html_span_text(parent, "Remember:");
     html_cycle_code_multiple(parent, [
       "Remember:",
       lesson_array_two_indices(),
-      "here is some example code :",
+      "Threfore:",
+      "the number `2` refers to the third element",
+      ,
     ]);
   }
 }
