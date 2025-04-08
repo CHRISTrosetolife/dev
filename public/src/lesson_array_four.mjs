6ebe73564fbfbbf226452bcc94c481e1a7028f20
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-08 16:23:09 -0400

    2025-04-08T20:23:08.904Z watch   lesson_array_four

--- public/src/lesson_array_four.mjs
+++ public/src/lesson_array_four.mjs
@@ -1,10 +1,8 @@
-import { lesson_array_two_indices } from "./lesson_array_two_indices.mjs";
-import { html_cycle_code_multiple } from "./html_cycle_code_multiple.mjs";
 import { lesson_array_source_get } from "./lesson_array_source_get.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
 import { string_combine } from "./string_combine.mjs";
 export function lesson_array_four() {
-  let description = "array with three items";
+  let description = "array with four items";
   let example_message = string_combine("with an ", description);
   let count = 3;
   let v = lesson_simple(
@@ -15,12 +13,5 @@ export function lesson_array_four() {
     [],
   );
   return v;
-  function example_before(parent) {
-    html_cycle_code_multiple(parent, [
-      "Remember:",
-      lesson_array_two_indices(),
-      "Threfore:",
-      "the number `2` refers to the third element",
-    ]);
-  }
+  function example_before(parent) {}
 }
