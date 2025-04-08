ffeaaec9232bd81b3ce0e837b6cbe9b2ec96fcf7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-08 16:27:53 -0400

    2025-04-08T20:27:51.718Z watch   lesson_new_modules_add

--- public/src/lesson_new_modules_add.mjs
+++ public/src/lesson_new_modules_add.mjs
@@ -1,14 +1,17 @@
+import { fn_name } from "./fn_name.mjs";
 import { app_list_add } from "./app_list_add.mjs";
 import { lesson_new_name } from "./lesson_new_name.mjs";
-import { list_add } from "./list_add.mjs";
 import { js_code_statement_call_args } from "./js_code_statement_call_args.mjs";
 import { app_learn_code_modules } from "./app_learn_code_modules.mjs";
 export async function lesson_new_modules_add(name) {
-  let lesson_name = lesson_new_name(name);
+  if (false) {
+    let lesson_name = lesson_new_name(name);
+  }
   await app_list_add(app_learn_code_modules, function inserted_code() {
-    return js_code_statement_call_args(list_add.name, [
+    let v = js_code_statement_call_args(fn_name("list_add"), [
       "module_current",
-      lesson_name,
+      name,
     ]);
+    return v;
   });
 }
