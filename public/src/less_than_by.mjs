dd8b346bac8f4572a73c720c078701e75a52951d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-08 16:18:44 -0400

    2025-04-08T20:18:40.230Z watch   less_than_by

--- public/src/less_than_by.mjs
+++ public/src/less_than_by.mjs
@@ -1,1 +1,5 @@
-export function less_than_by(a, b, mapper) {}
+import { less_than } from "./less_than.mjs";
+export function less_than_by(a, b, mapper) {
+  let v = less_than(mapper(a), mapper(b));
+  return v;
+}
