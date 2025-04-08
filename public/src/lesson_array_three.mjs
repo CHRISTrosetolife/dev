d010374fef5cab137070d2fa84fe90a46a7d2906
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-08 15:50:55 -0400

    2025-04-08T19:50:54.471Z watch   lesson_array_three

--- public/src/lesson_array_three.mjs
+++ public/src/lesson_array_three.mjs
@@ -1,3 +1,4 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_cycle_code_multiple } from "./html_cycle_code_multiple.mjs";
 import { html_cycle_bold_multiple } from "./html_cycle_bold_multiple.mjs";
 import { app_learn_code_log_combined } from "./app_learn_code_log_combined.mjs";
@@ -18,22 +19,27 @@ export function lesson_array_three() {
   let description = "array with two items";
   let example_message = string_combine("with an ", description);
   let count = 2;
-  return lesson_simple(
+  let v = lesson_simple(
     function () {
       let a = app_learn_code_random_identifier();
       let values = integer_random_digit_single_positive_multiple_sorted(count);
-      let outputs = list_map(range(count), (i) => js_code_array_get(a, i));
-      const list = [
+      let outputs = list_map(range(count), function (i) {
+        let v2 = js_code_array_get(a, i);
+        return v2;
+      });
+      let list = [
         js_code_statement_let_assign(a, js_code_array(values)),
         app_learn_code_log_combined(outputs),
       ];
-      return list_join_newline(list);
+      let v3 = list_join_newline(list);
+      return v3;
     },
     example_before,
     description,
     example_message,
     [screen1, screen2, screen3],
   );
+  return v;
   function example_before(parent) {}
   function screen1(parent) {
     let a = app_learn_code_random_identifier();
@@ -68,8 +74,14 @@ export function lesson_array_three() {
       parent,
       list_join_newline([
         js_code_statement_let_assign(a, js_code_array(values)),
-        `${js_code_statement(js_code_array_get(a, 0))} // use 0 for the first element , 3.2`,
-        `${js_code_statement(js_code_array_get(a, 1))} // use 1 for the first element , 4.5`,
+        string_combine_multiple([
+          js_code_statement(js_code_array_get(a, 0)),
+          " // use 0 for the first element , 3.2",
+        ]),
+        string_combine_multiple([
+          js_code_statement(js_code_array_get(a, 1)),
+          " // use 1 for the first element , 4.5",
+        ]),
       ]),
     );
     html_cycle_code_multiple(parent, [
