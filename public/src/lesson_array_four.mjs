bfe06c4d198791a5a9dfa7d1d2b2ea75a6a08598
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-08 16:22:56 -0400

    2025-04-08T20:22:55.835Z l lesson_array_three lesson_array_four

---
+++ public/src/lesson_array_four.mjs
@@ -0,0 +1,26 @@
+import { lesson_array_two_indices } from "./lesson_array_two_indices.mjs";
+import { html_cycle_code_multiple } from "./html_cycle_code_multiple.mjs";
+import { lesson_array_source_get } from "./lesson_array_source_get.mjs";
+import { lesson_simple } from "./lesson_simple.mjs";
+import { string_combine } from "./string_combine.mjs";
+export function lesson_array_four() {
+  let description = "array with three items";
+  let example_message = string_combine("with an ", description);
+  let count = 3;
+  let v = lesson_simple(
+    lesson_array_source_get(count),
+    example_before,
+    description,
+    example_message,
+    [],
+  );
+  return v;
+  function example_before(parent) {
+    html_cycle_code_multiple(parent, [
+      "Remember:",
+      lesson_array_two_indices(),
+      "Threfore:",
+      "the number `2` refers to the third element",
+    ]);
+  }
+}
