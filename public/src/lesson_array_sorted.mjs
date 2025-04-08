e94d8efb323a37f848c890b61fc031d0ba4a8b91
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-08 16:41:29 -0400

    2025-04-08T20:41:29.225Z watch   lesson_array_sorted

--- public/src/lesson_array_sorted.mjs
+++ public/src/lesson_array_sorted.mjs
@@ -1,3 +1,4 @@
+import { list_copy_reverse } from "./list_copy_reverse.mjs";
 import { range_1 } from "./range_1.mjs";
 import { js_code_array } from "./js_code_array.mjs";
 import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
@@ -5,20 +6,23 @@ import { app_learn_code_code_part_contrast } from "./app_learn_code_code_part_co
 import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
 import { app_learn_code_random_identifier } from "./app_learn_code_random_identifier.mjs";
 export function lesson_array_sorted(count) {
-  function lesson_array_sorted_inner(parent) {
+  let v = function lesson_array_sorted_inner(parent) {
     let a = app_learn_code_random_identifier();
     html_p_text_multiple(parent, [
       "in this lesson numbers will appear from smallest to largest",
       "for example , this is allowed :",
     ]);
+    let forwards = range_1(count);
+    let backwards = list_copy_reverse(forwards);
     app_learn_code_code_part_contrast(
       parent,
-      js_code_statement_let_assign(a, js_code_array(range_1(count))),
+      js_code_statement_let_assign(a, js_code_array(forwards)),
     );
     html_p_text_multiple(parent, ["however , this is not allowed :"]);
     app_learn_code_code_part_contrast(
       parent,
-      js_code_statement_let_assign(a, js_code_array([2, 1])),
+      js_code_statement_let_assign(a, js_code_array(backwards)),
     );
-  }
+  };
+  return v;
 }
