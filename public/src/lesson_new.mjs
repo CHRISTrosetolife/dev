46a0cb915d739e5af7c2fa100c4f0d17f8f813d3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-08 16:22:31 -0400

    2025-04-08T20:22:30.749Z watch   lesson_new

--- public/src/lesson_new.mjs
+++ public/src/lesson_new.mjs
@@ -1,8 +1,10 @@
 import { lesson_new_name } from "./lesson_new_name.mjs";
 import { lesson_new_modules_add } from "./lesson_new_modules_add.mjs";
 import { function_copy } from "./function_copy.mjs";
-export async function lesson_new(name, previous) {
-  let lesson_name = lesson_new_name(name);
-  await function_copy(previous, lesson_name);
+export async function lesson_new(previous, name) {
+  if (false) {
+    let lesson_name = lesson_new_name(name);
+  }
+  await function_copy(previous, name);
   await lesson_new_modules_add(name);
 }
