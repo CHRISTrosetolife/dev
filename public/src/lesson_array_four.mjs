7f354a6a3e2fe9a5d1fd83d9f78e8de2b3cc54eb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-08 16:26:28 -0400

    2025-04-08T20:26:26.941Z watch   lesson_array_four

--- public/src/lesson_array_four.mjs
+++ public/src/lesson_array_four.mjs
@@ -3,9 +3,9 @@ import { lesson_array_source_get } from "./lesson_array_source_get.mjs";
 import { lesson_simple } from "./lesson_simple.mjs";
 import { string_combine } from "./string_combine.mjs";
 export function lesson_array_four() {
+  let count = 3;
   let description = string_combine_multiple(["array with four items"]);
   let example_message = string_combine("with an ", description);
-  let count = 3;
   let v = lesson_simple(
     lesson_array_source_get(count),
     example_before,
