7e75d27df2d4562b0280babca10786b65f4fa87e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-08 16:09:02 -0400

    2025-04-08T20:09:01.860Z watch   html_cycle_code_multiple

--- public/src/html_cycle_code_multiple.mjs
+++ public/src/html_cycle_code_multiple.mjs
@@ -1,5 +1,7 @@
+import { list_denest } from "./list_denest.mjs";
 import { html_cycle_code } from "./html_cycle_code.mjs";
 export function html_cycle_code_multiple(parent, messages) {
+  messages = list_denest(messages);
   for (let message of messages) {
     html_cycle_code(parent, message);
   }
