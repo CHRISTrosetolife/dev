bf0505dba2ab1406326befd6efc4d6169f7ee5c3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-09 09:04:52 -0400

    2025-04-09T13:04:51.384Z watch   lesson_array_source_get

--- public/src/lesson_array_source_get.mjs
+++ public/src/lesson_array_source_get.mjs
@@ -1,11 +1,18 @@
-import { js_code_dot } from "./js_code_dot.mjs";
+import { list_join } from "./list_join.mjs";
 import { app_learn_code_log } from "./app_learn_code_log.mjs";
 import { lesson_array_source_get_generic } from "./lesson_array_source_get_generic.mjs";
+import { js_code_array_get } from "./js_code_array_get.mjs";
+import { range } from "./range.mjs";
+import { list_map } from "./list_map.mjs";
 export function lesson_array_source_get(count) {
   let v = lesson_array_source_get_generic(count, second_get);
   return v;
   function second_get(list_identifier, count) {
-    let second = app_learn_code_log(js_code_dot(list_identifier, "length"));
+    let outputs = list_map(range(count), function (i) {
+      let v2 = js_code_array_get(list_identifier, i);
+      return v2;
+    });
+    let second = app_learn_code_log(list_join(outputs, " + ' ' + "));
     return second;
   }
 }
