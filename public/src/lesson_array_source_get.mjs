1fe089efcc9ac2e3a18fa61556ac2f5df2dc13b6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-09 09:00:31 -0400

    2025-04-09T13:00:30.065Z watch   lesson_array_source_get

--- public/src/lesson_array_source_get.mjs
+++ public/src/lesson_array_source_get.mjs
@@ -1,24 +1,17 @@
-import { list_join_newline } from "./list_join_newline.mjs";
+import { lesson_array_source_get_generic } from "./lesson_array_source_get_generic.mjs";
 import { app_learn_code_log_combined } from "./app_learn_code_log_combined.mjs";
-import { js_code_array } from "./js_code_array.mjs";
-import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
 import { js_code_array_get } from "./js_code_array_get.mjs";
 import { range } from "./range.mjs";
 import { list_map } from "./list_map.mjs";
-import { integer_random_digit_single_positive_multiple_sorted } from "./integer_random_digit_single_positive_multiple_sorted.mjs";
-import { app_learn_code_random_identifier } from "./app_learn_code_random_identifier.mjs";
 export function lesson_array_source_get(count) {
-  let v = function () {
-    let a = app_learn_code_random_identifier();
-    let values = integer_random_digit_single_positive_multiple_sorted(count);
+  let v = lesson_array_source_get_generic(count, second_get);
+  return v;
+  function second_get(count, a) {
     let outputs = list_map(range(count), function (i) {
       let v2 = js_code_array_get(a, i);
       return v2;
     });
     let second = app_learn_code_log_combined(outputs);
-    let list = [js_code_statement_let_assign(a, js_code_array(values)), second];
-    let v3 = list_join_newline(list);
-    return v3;
-  };
-  return v;
+    return second;
+  }
 }
---
+++ public/src/lesson_array_source_get_generic.mjs
@@ -0,0 +1,16 @@
+import { list_join_newline } from "./list_join_newline.mjs";
+import { js_code_array } from "./js_code_array.mjs";
+import { js_code_statement_let_assign } from "./js_code_statement_let_assign.mjs";
+import { integer_random_digit_single_positive_multiple_sorted } from "./integer_random_digit_single_positive_multiple_sorted.mjs";
+import { app_learn_code_random_identifier } from "./app_learn_code_random_identifier.mjs";
+export function lesson_array_source_get_generic(count, second_get) {
+  let v = function () {
+    let a = app_learn_code_random_identifier();
+    let values = integer_random_digit_single_positive_multiple_sorted(count);
+    let second = second_get(count, a);
+    let list = [js_code_statement_let_assign(a, js_code_array(values)), second];
+    let v3 = list_join_newline(list);
+    return v3;
+  };
+  return v;
+}
