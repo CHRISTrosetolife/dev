de937f8860202c32151fee429d3e097e4737ac29
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-09 16:48:58 -0400

    2025-04-09T20:48:56.212Z de lesson_array_source_get_count

--- public/src/lesson_array_source_get_count.mjs
+++
@@ -1,7 +0,0 @@
-import { lesson_array_source_get } from "./lesson_array_source_get.mjs";
-export function lesson_array_source_get_count(count) {
-  let v = lesson_array_source_get(function () {
-    return count;
-  });
-  return v;
-}
