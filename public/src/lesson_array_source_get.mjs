091f57d501132ef359c239dc473ef44a0d47c981
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-09 09:04:16 -0400

    2025-04-09T13:04:15.782Z watch   lesson_array_source_get

--- public/src/lesson_array_source_get.mjs
+++ public/src/lesson_array_source_get.mjs
@@ -1,5 +1,6 @@
+import { list_join } from "./list_join.mjs";
+import { app_learn_code_log } from "./app_learn_code_log.mjs";
 import { lesson_array_source_get_generic } from "./lesson_array_source_get_generic.mjs";
-import { app_learn_code_log_combined } from "./app_learn_code_log_combined.mjs";
 import { js_code_array_get } from "./js_code_array_get.mjs";
 import { range } from "./range.mjs";
 import { list_map } from "./list_map.mjs";
@@ -11,7 +12,7 @@ export function lesson_array_source_get(count) {
       let v2 = js_code_array_get(list_identifier, i);
       return v2;
     });
-    let second = app_learn_code_log_combined(outputs);
+    let second = app_learn_code_log(list_join(outputs, " + ' ' + "));
     return second;
   }
 }
