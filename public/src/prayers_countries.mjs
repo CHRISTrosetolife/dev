9d684455b8701713e954bb4c2dbef86efc106219
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-10 05:51:33 -0400

    2025-04-10T09:51:31.751Z watch   prayers_countries

--- public/src/prayers_countries.mjs
+++ public/src/prayers_countries.mjs
@@ -1,9 +1,12 @@
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { countries_names } from "./countries_names.mjs";
 import { list_map } from "./list_map.mjs";
+import { string_split } from "./string_split.mjs";
 export function prayers_countries() {
   let c = countries_names();
-  let m = list_map(c, function () {});
+  let m = list_map(c, function () {
+    let s = string_split(c, ",");
+  });
   let v = list_map(c, function (a) {
     let v2 = string_combine_multiple(["Save and bless ", a]);
     return v2;
