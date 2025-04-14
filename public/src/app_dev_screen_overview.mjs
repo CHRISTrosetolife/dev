abba57c1aa606a02aa19210af8433e0a2a59aad0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-14 18:05:29 -0400

    2025-04-14T22:05:28.666Z watch   app_dev_screen_overview

--- public/src/app_dev_screen_overview.mjs
+++ public/src/app_dev_screen_overview.mjs
@@ -1,12 +1,14 @@
 import { html_cycle_code_multiple } from "./html_cycle_code_multiple.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 export function app_dev_screen_overview() {
-  return {
+  let v = {
     name: function overview_name(root) {
-      return html_span_text(root, "overview");
+      let v2 = html_span_text(root, "overview");
+      return v2;
     },
     screen: function overview_screen(root) {
       html_cycle_code_multiple(root, [
+        "This is an introduction to the development tools I",
         "one goal was that a recent college or bootcamp graduate could get started quickly",
         "rather than experts-only , be beginner-friendly",
         "with a larger candidate pool , an employer can find developers easier",
@@ -15,4 +17,5 @@ export function app_dev_screen_overview() {
       ]);
     },
   };
+  return v;
 }
