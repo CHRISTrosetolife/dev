281bca6aa7614c2f3dfe5204d171040532929db8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-14 18:09:45 -0400

    2025-04-14T22:09:44.043Z watch   app_dev_screen_overview

--- public/src/app_dev_screen_overview.mjs
+++ public/src/app_dev_screen_overview.mjs
@@ -9,12 +9,10 @@ export function app_dev_screen_overview() {
     screen: function overview_screen(root) {
       html_cycle_code_multiple(root, [
         "This is an introduction to the development tools I've created for myself",
-        "The primary goal is speed: what tools can speed up repetitive development tasks? For example I have a script that automatically adds the let JavaScript keyword so I do not have to type it out anymore",
-        "one goal was that a recent college or bootcamp graduate could get started quickly",
-        "rather than experts-only , be beginner-friendly",
-        "with a larger candidate pool , an employer can find developers easier",
-        "therefore , the code should favor simplicity :",
-        "to keep the programming simple , `function`s are the basic building block , rather than classes or object-oriented programming",
+        "The primary goal is speed: what tools can speed up repetitive development tasks? For example, I have a script that automatically adds the `let` JavaScript keyword so I do not have to type it out anymore",
+        "This tutorial starts with the basics of the development tools and then gives an introduction to give you an idea",
+        "Another goal is so others, even programming beginners can use it. Therefore simplicity is a design goal.",
+        "For example to keep things simple for anyone, `function`s are the basic building block, rather than classes or object-oriented programming",
       ]);
     },
   };
