27cfa052d04b8283932e1adf06e6ac5c5715d57c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-14 17:56:18 -0400

    2025-04-14T21:56:16.084Z watch   html_style_font_size_object_value

---
+++ public/src/html_style_font_size_object.mjs
@@ -0,0 +1,6 @@
+import { html_style_font_size_property } from "./html_style_font_size_property.mjs";
+export function html_style_font_size_object(v) {
+  return {
+    [html_style_font_size_property()]: v,
+  };
+}
--- public/src/html_style_font_size_object_value.mjs
+++ public/src/html_style_font_size_object_value.mjs
@@ -1,12 +1,7 @@
-import { html_style_font_size_property } from "./html_style_font_size_property.mjs";
+import { html_style_font_size_object } from "./html_style_font_size_object.mjs";
 import { html_style_default_font_size_value_get } from "./html_style_default_font_size_value_get.mjs";
 export function html_style_font_size_object_value(default_font_size) {
-    const v = html_style_default_font_size_value_get(default_font_size);
-  return html_style_font_size_object(v);
+  let v = html_style_default_font_size_value_get(default_font_size);
+  let v2 = html_style_font_size_object(v);
+  return v2;
 }
-function html_style_font_size_object(v) {
-    return {
-        [html_style_font_size_property()]: v,
-    };
-}
-
