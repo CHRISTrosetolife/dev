ff627752c45df48d18558cbe35065812b0cf8042
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-14 17:50:17 -0400

    2025-04-14T21:50:17.074Z watch   html_style_link_blank

--- public/src/html_style_link_blank.mjs
+++ public/src/html_style_link_blank.mjs
@@ -1,5 +1,6 @@
 import { html_style_link_generic } from "./html_style_link_generic.mjs";
 import { html_a_blank } from "./html_a_blank.mjs";
 export function html_style_link_blank(url) {
-  return html_style_link_generic(html_a_blank, url);
+  let v = html_style_link_generic(html_a_blank, url);
+  return v;
 }
