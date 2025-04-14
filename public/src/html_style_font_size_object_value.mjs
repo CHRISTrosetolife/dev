e0908087efd4d13ce4e82bc64dd4e0ea5276ef2c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-14 17:55:59 -0400

    2025-04-14T21:55:57.816Z watch   html_style_link_blank

--- public/src/html_style_font_size_object_value.mjs
+++ public/src/html_style_font_size_object_value.mjs
@@ -1,8 +1,12 @@
 import { html_style_font_size_property } from "./html_style_font_size_property.mjs";
 import { html_style_default_font_size_value_get } from "./html_style_default_font_size_value_get.mjs";
 export function html_style_font_size_object_value(default_font_size) {
-  return {
-    [html_style_font_size_property()]:
-      html_style_default_font_size_value_get(default_font_size),
-  };
+    const v = html_style_default_font_size_value_get(default_font_size);
+  return html_style_font_size_object(v);
 }
+function html_style_font_size_object(v) {
+    return {
+        [html_style_font_size_property()]: v,
+    };
+}
+
