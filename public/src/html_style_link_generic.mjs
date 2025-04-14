b98fba0c13bb083fad1dd640ec23ec1a1d45f91d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-14 17:59:16 -0400

    2025-04-14T21:59:15.550Z watch   html_style_link_generic

--- public/src/html_style_link_generic.mjs
+++ public/src/html_style_link_generic.mjs
@@ -5,7 +5,7 @@ export function html_style_link_generic(html_a_lambda, url) {
   let v2 = function (c) {
     let text = html_clear_inner_get(c);
     let a = html_a_lambda(c, text, url);
-    html_style(v, html_style_font_size_object("inherit"));
+    html_style(a, html_style_font_size_object("inherit"));
   };
   return v2;
 }
