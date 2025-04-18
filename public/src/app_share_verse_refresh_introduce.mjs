bd72aa918469cc983931ac5f2340bedab9874ca4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-18 10:36:13 -0400

    2025-04-18T14:36:12.589Z watch   app_share_verse_refresh_introduce

--- public/src/app_share_verse_refresh_introduce.mjs
+++ public/src/app_share_verse_refresh_introduce.mjs
@@ -6,19 +6,21 @@ import { emoji_wrap_music } from "./emoji_wrap_music.mjs";
 import { app_share_verse_refresh_greet } from "./app_share_verse_refresh_greet.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_share_verse_refresh_introduce() {
-  let v = string_combine_multiple([
-    app_share_verse_refresh_greet(),
-    " I believe Christ died ✝️ , was buried 🪨 and rose ⬆️ to life ⛪ . I am a servant of God. My parents named me Jared Mathis. I have a wife. God called me to preach 🗣 the gospel ✝️.",
-    emoji_wrap_music("I sing 🎤️ and play 🎹 Bible songs"),
-    " . I am ",
-    me_age(),
-    " years old. ",
-    me_location(),
-    ". ",
-    app_share_verse_refresh_phone(),
-    " 📬 Here is my email: ",
-    me_email(),
-    " 📧 ",
-  ]);
+  let v = [
+    string_combine_multiple([
+      app_share_verse_refresh_greet(),
+      " I believe Christ died ✝️ , was buried 🪨 and rose ⬆️ to life ⛪ . I am a servant of God. My parents named me Jared Mathis. I have a wife. God called me to preach 🗣 the gospel ✝️.",
+      emoji_wrap_music("I sing 🎤️ and play 🎹 Bible songs"),
+      " . I am ",
+      me_age(),
+      " years old. ",
+      me_location(),
+      ". ",
+      app_share_verse_refresh_phone(),
+      " 📬 Here is my email: ",
+      me_email(),
+      " 📧 ",
+    ]),
+  ];
   return v;
 }
