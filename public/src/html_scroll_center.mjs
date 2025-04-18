e5196b53f36fedabe6550d88e93a6929000466f5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-18 08:09:04 -0400

    2025-04-18T12:09:04.078Z watch   html_scroll_center

--- public/src/html_scroll_center.mjs
+++ public/src/html_scroll_center.mjs
@@ -1,4 +1,4 @@
 import { html_scroll_center_generic } from "./html_scroll_center_generic.mjs";
-export function html_scroll_center(verse_word) {
-  html_scroll_center_generic(verse_word, {});
+export function html_scroll_center(component) {
+  html_scroll_center_generic(component, {});
 }
