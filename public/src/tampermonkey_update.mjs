c63378f564616f9153bc55603eedd20706fcaa96
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-19 16:33:10 -0400

    2025-04-19T20:33:10.384Z watch   tampermonkey_update

--- public/src/tampermonkey_update.mjs
+++ public/src/tampermonkey_update.mjs
@@ -1,7 +1,8 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { path_join } from "./path_join.mjs";
 export function tampermonkey_update(name) {
   let id = "tampermonkey";
   let combined = function_name_combine(id, name);
-  let output_path = path_join([id, name]);
+  let output_path = path_join([id, string_combine_multiple([name])]);
 }
