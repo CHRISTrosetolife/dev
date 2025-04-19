058a1f1d2215dbbfe7f0444a88dc3e633d7d7355
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-19 16:40:40 -0400

    2025-04-19T20:40:39.872Z watch   tampermonkey_update

--- public/src/tampermonkey_update.mjs
+++ public/src/tampermonkey_update.mjs
@@ -4,11 +4,12 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { path_join } from "./path_join.mjs";
 import { function_dependencies_string } from "./function_dependencies_string.mjs";
+import { list_join_newline } from "./list_join_newline.mjs";
 export async function tampermonkey_update(name) {
   let id = "tampermonkey";
   let combined = function_name_combine(id, name);
   let functions = await function_dependencies_string(combined);
-  let contents = string_combine_multiple([
+  let contents = list_join_newline([
     js_code_statement_call(combined),
     functions,
   ]);
