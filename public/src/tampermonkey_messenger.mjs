ea48baf6d0836c4b212b9cf870a75b60a21f8c83
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-19 16:56:19 -0400

    2025-04-19T20:56:18.573Z watch   tampermonkey_messenger

--- public/src/tampermonkey_messenger.mjs
+++ public/src/tampermonkey_messenger.mjs
@@ -1,5 +1,7 @@
+import { html_element_wait } from "./html_element_wait.mjs";
 export function tampermonkey_messenger() {
   alert("working");
+  html_element_wait;
   window.addEventListener("load", function () {
     alert("It's loaded!");
   });
