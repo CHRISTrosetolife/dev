91b48a5faddecde5642a40236f0a98a4bd701ff2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-19 15:05:34 -0400

    2025-04-19T19:05:28.211Z s5

--- public/src/http_storage_get.mjs
+++ public/src/http_storage_get.mjs
@@ -1,24 +1,6 @@
 import { storage_url } from "./storage_url.mjs";
 import { http_get } from "./http_get.mjs";
 export async function http_storage_get(file_path) {
-  return await http_get(storage_url(file_path));
+  let v = await http_get(storage_url(file_path));
+  return v;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
