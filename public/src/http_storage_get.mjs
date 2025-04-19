bc53a4126bb7a7734dd2075d3df3bae3b9f71e12
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-19 15:05:21 -0400

    2025-04-19T19:05:19.805Z watch   sandbox_5

--- public/src/http_storage_get.mjs
+++ public/src/http_storage_get.mjs
@@ -3,3 +3,22 @@ import { http_get } from "./http_get.mjs";
 export async function http_storage_get(file_path) {
   return await http_get(storage_url(file_path));
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,7 +1,8 @@
 import { storage_upload_string } from "./storage_upload_string.mjs";
 export async function sandbox_5() {
   await storage_upload_string(
-    '// ==UserScript==\n// ==UserScript==\n\n(function(){alert("jesus");});',
+    '// ==UserScript==\n// ==UserScript==\n\n(function(){alert("jesus");})();',
     "tampermonkey/jesus.js",
   );
+  s;
 }
