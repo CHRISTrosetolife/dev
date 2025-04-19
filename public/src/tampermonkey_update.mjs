a108690ddf221d84a103d9b2ddb901e8e62617b6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-19 16:39:10 -0400

    2025-04-19T20:39:09.916Z watch   tampermonkey_update

--- public/src/tampermonkey_update.mjs
+++ public/src/tampermonkey_update.mjs
@@ -6,7 +6,7 @@ import { function_dependencies_string } from "./function_dependencies_string.mjs
 export async function tampermonkey_update(name) {
   let id = "tampermonkey";
   let combined = function_name_combine(id, name);
-  let contents = await function_dependencies_string(combined);
+  let functions = await function_dependencies_string(combined);
   let output_path = path_join([id, string_combine_multiple([name, ".js"])]);
-  await file_overwrite_open(output_path, contents);
+  await file_overwrite_open(output_path, functions);
 }
