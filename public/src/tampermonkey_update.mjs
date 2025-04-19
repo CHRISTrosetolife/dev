d1fc915a49a40a2a4e0525f92b4f9f72a9a9d192
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-19 16:31:46 -0400

    2025-04-19T20:31:45.985Z watch   tampermonkey_update

--- public/src/tampermonkey_update.mjs
+++ public/src/tampermonkey_update.mjs
@@ -1,4 +1,4 @@
 import { function_name_combine } from "./function_name_combine.mjs";
 export function tampermonkey_update(name) {
-  name = function_name_combine("tampermonkey_", name);
+  let combined = function_name_combine("tampermonkey_", name);
 }
