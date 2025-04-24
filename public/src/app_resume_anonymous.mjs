76fa91268132f6702b87dd321430c93d7515e847
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-24 15:19:12 -0400

    2025-04-24T19:19:12.193Z watch   app_resume_anonymous

--- public/src/app_resume_anonymous.mjs
+++ public/src/app_resume_anonymous.mjs
@@ -1,6 +1,6 @@
 import { app_resume_generic } from "./app_resume_generic.mjs";
 export function app_resume_anonymous() {
   let anonymous = true;
-  let print = false;
+  let print = true;
   app_resume_generic(print, anonymous);
 }
