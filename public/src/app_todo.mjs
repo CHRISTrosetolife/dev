70571426ef9b3bdd9678e88e6db8860d12192cb8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-24 13:31:45 -0400

    2025-04-24T17:31:45.388Z watch   app_todo

--- public/src/app_todo.mjs
+++ public/src/app_todo.mjs
@@ -1,11 +1,15 @@
+import { html_overlay_z } from "./html_overlay_z.mjs";
+import { list_add } from "./list_add.mjs";
+import { fn_name } from "./fn_name.mjs";
 import { firebase_storage_initialize } from "./firebase_storage_initialize.mjs";
 import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
 import { app_todo_main } from "./app_todo_main.mjs";
 import { app_firebase } from "./app_firebase.mjs";
 import { object_merge } from "./object_merge.mjs";
 export async function app_todo() {
+  list_add(html_overlay_z(), fn_name("app_todo"));
   await app_firebase({
-    on_logged_in: async (context) => {
+    on_logged_in: async function (context) {
       let index_path = app_todo_firebase_path_index();
       let index = await firebase_storage_initialize(index_path);
       object_merge(context, {
