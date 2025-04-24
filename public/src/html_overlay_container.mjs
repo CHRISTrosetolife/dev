8f09fcc89b52e5fb3cf2eabd2763d96a194a0017
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-24 13:32:52 -0400

    2025-04-24T17:32:52.186Z watch   html_overlay_container

--- public/src/html_overlay_container.mjs
+++ public/src/html_overlay_container.mjs
@@ -7,10 +7,10 @@ import { html_div } from "./html_div.mjs";
 import { html_clear } from "./html_clear.mjs";
 export function html_overlay_container(overlay, menu_refresh) {
   html_clear(overlay);
-  let d = html_div(overlay);
-  html_style_background_color_white(d);
-  html_style_rounded_padded(d);
-  html_style_margin(d, html_style_units(2));
-  html_button_back(d, menu_refresh);
-  return d;
+  let container = html_div(overlay);
+  html_style_background_color_white(container);
+  html_style_rounded_padded(container);
+  html_style_margin(container, html_style_units(2));
+  html_button_back(container, menu_refresh);
+  return container;
 }
