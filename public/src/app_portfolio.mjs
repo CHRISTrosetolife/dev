776d9ddfca404eedda13dd0d05c3cbae53f927de
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-27 14:13:56 -0400

    2025-04-27T18:13:56.131Z watch   app_portfolio

--- public/src/app_portfolio.mjs
+++ public/src/app_portfolio.mjs
@@ -1,3 +1,4 @@
+import { app_portfolio_app_learn_to_code_name } from "./app_portfolio_app_learn_to_code_name.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { me_name } from "./me_name.mjs";
@@ -12,4 +13,9 @@ export function app_portfolio() {
       "!",
     ]),
   );
+  let apps = [
+    {
+      name: app_portfolio_app_learn_to_code_name(),
+    },
+  ];
 }
