0c1072451da237a457f239f93618b31451349001
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 12:13:26 -0400

    2025-04-28T16:13:25.893Z watch   app_ceb

--- public/src/app_ceb.mjs
+++ public/src/app_ceb.mjs
@@ -1,8 +1,6 @@
 import { html_button_width_full_text_link } from "./html_button_width_full_text_link.mjs";
 import { html_spacer_vertical_2 } from "./html_spacer_vertical_2.mjs";
 import { html_style_a_plain } from "./html_style_a_plain.mjs";
-import { html_button_width_full_text } from "./html_button_width_full_text.mjs";
-import { html_style_link_blank } from "./html_style_link_blank.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 export async function app_ceb() {
@@ -15,7 +13,9 @@ export async function app_ceb() {
     "New Cebuano Language Learn App",
   );
   html_spacer_vertical_2(body);
-  html_style_link_blank("ceb1.html")(
-    html_button_width_full_text(body, "Original Cebuano Language Learn App"),
+  html_button_width_full_text_link(
+    body,
+    "ceb1.html",
+    "Original Cebuano Language Learn App",
   );
 }
