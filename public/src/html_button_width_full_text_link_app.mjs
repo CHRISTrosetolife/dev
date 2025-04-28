135bb8583dc5f8c6be7cefa2e21f394240feb260
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 12:24:25 -0400

    2025-04-28T16:24:24.322Z watch   html_button_width_full_text_link_app

--- public/src/html_button_width_full_text_link_app.mjs
+++ public/src/html_button_width_full_text_link_app.mjs
@@ -1,4 +1,4 @@
-import { html_button_width_full_text_link } from "./html_button_width_full_text_link.mjs";
+import { html_button_window_open } from "./html_button_window_open.mjs";
 import { app_extension_html_combine } from "./app_extension_html_combine.mjs";
 import { app_name_string } from "./app_name_string.mjs";
 export function html_button_width_full_text_link_app(
@@ -8,5 +8,5 @@ export function html_button_width_full_text_link_app(
 ) {
   let a_name = app_name_string(a_name_prefixed);
   let url = app_extension_html_combine(a_name);
-  html_button_width_full_text_link(body, url, text);
+  html_button_window_open(body, url, text);
 }
