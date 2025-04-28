92b7dde3837bed319d4c6c722b56f7407d980d06
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 16:17:24 -0400

    2025-04-28T20:17:24.082Z watch   app_index_choices_migrate

--- public/src/app_index_choices_migrate.mjs
+++ public/src/app_index_choices_migrate.mjs
@@ -1,4 +1,5 @@
+import { fn_name } from "./fn_name.mjs";
 import { function_transform } from "./function_transform.mjs";
 export async function app_index_choices_migrate() {
-  await function_transform();
+  await function_transform(fn_name("app_index_choices"));
 }
