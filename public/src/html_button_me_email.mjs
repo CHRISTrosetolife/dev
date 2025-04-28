01862cd544236deb888f79033009c9514a30a929
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 19:06:10 -0400

    2025-04-28T23:06:09.684Z watch   html_button_me_email

--- public/src/html_button_me_email.mjs
+++ public/src/html_button_me_email.mjs
@@ -1,7 +1,6 @@
 import { html_button_email } from "./html_button_email.mjs";
 import { me_email } from "./me_email.mjs";
 export function html_button_me_email(parent) {
-  let email = me_email();
   let text = "📨 Contact developer by email";
-  html_button_email(parent, email, text);
+  html_button_email(parent, me_email(), text);
 }
