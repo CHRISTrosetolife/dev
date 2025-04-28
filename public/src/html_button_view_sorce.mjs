d44481a68e42e12ef9c779d3b83aee4b736dffa2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 12:23:19 -0400

    2025-04-28T16:23:19.145Z watch   html_button_view_sorce

--- public/src/html_button_view_sorce.mjs
+++ public/src/html_button_view_sorce.mjs
@@ -1,10 +1,7 @@
-import { html_button } from "./html_button.mjs";
-import { window_open } from "./window_open.mjs";
+import { html_button_window_open } from "./html_button_window_open.mjs";
 import { function_name_to_url_github } from "./function_name_to_url_github.mjs";
 export function html_button_view_sorce(root, name) {
   let url = function_name_to_url_github(name);
   let text = "💻 view the source of this screen";
-  html_button(root, text, function () {
-    window_open(url);
-  });
+  html_button_window_open(root, text, url);
 }
---
+++ public/src/html_button_window_open.mjs
@@ -0,0 +1,7 @@
+import { window_open } from "./window_open.mjs";
+import { html_button } from "./html_button.mjs";
+export function html_button_window_open(root, text, url) {
+  html_button(root, text, function () {
+    window_open(url);
+  });
+}
