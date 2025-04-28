eb10cfb425ae003cf0af86564cfa1a8c3d6cc708
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 19:03:11 -0400

    2025-04-28T23:03:10.550Z watch   html_style_email

---
+++ public/src/html_mailto.mjs
@@ -0,0 +1,4 @@
+export function html_mailto() {
+  let v = "mailto";
+  return v;
+}
--- public/src/html_style_email.mjs
+++ public/src/html_style_email.mjs
@@ -1,4 +1,6 @@
+import { html_mailto } from "./html_mailto.mjs";
 import { html_style_link_prefix } from "./html_style_link_prefix.mjs";
 export function html_style_email(email_address) {
-  return html_style_link_prefix("mailto", email_address);
+  let v = html_style_link_prefix(html_mailto(), email_address);
+  return v;
 }
