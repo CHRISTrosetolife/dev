e1a7236f6d43c39c96a2e1d088674550dc72a35b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 16:29:26 -0400

    2025-04-28T20:29:25.777Z watch   js_node_type_first

--- public/src/js_node_type_first.mjs
+++ public/src/js_node_type_first.mjs
@@ -1,7 +1,10 @@
+import { log } from "./log.mjs";
 import { js_node_type } from "./js_node_type.mjs";
 import { list_first } from "./list_first.mjs";
 export function js_node_type_first(ast, type) {
   let r = js_node_type(ast, type);
   let v = list_first(r);
-  return v;
+  log({
+    v,
+  });
 }
