832b865d3570bb555014c379fba830b7485fb38c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 16:17:18 -0400

    2025-04-28T20:17:17.428Z watch   app_index_choices_migrate

--- public/src/app_index_choices_migrate.mjs
+++ public/src/app_index_choices_migrate.mjs
@@ -1,1 +1,4 @@
-export function app_index_choices_migrate() {}
+import { function_transform } from "./function_transform.mjs";
+export async function app_index_choices_migrate() {
+  await function_transform();
+}
