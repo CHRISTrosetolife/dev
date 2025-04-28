304e418f8aaa91f570740389dd363f54357a4880
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 19:02:38 -0400

    2025-04-28T23:02:37.937Z watch   html_style_link_prefix

--- public/src/html_style_link_prefix.mjs
+++ public/src/html_style_link_prefix.mjs
@@ -1,7 +1,6 @@
 import { html_style_link_blank } from "./html_style_link_blank.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-export function html_style_link_prefix(prefix, phone_number_text) {
-  return html_style_link_blank(
-    string_combine_multiple([prefix, ":", phone_number_text]),
-  );
+export function html_style_link_prefix(prefix, link) {
+  let v = html_style_link_blank(string_combine_multiple([prefix, ":", link]));
+  return v;
 }
