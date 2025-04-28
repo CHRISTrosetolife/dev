98f3da089753d0bd61f996c48a8a1b235cd0358a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 18:59:48 -0400

    2025-04-28T22:59:48.009Z watch   html_hash_set

--- public/src/html_hash_set.mjs
+++ public/src/html_hash_set.mjs
@@ -1,4 +1,6 @@
+import { object_property_set } from "./object_property_set.mjs";
 import { window_location } from "./window_location.mjs";
 export function html_hash_set(value) {
+  object_property_set(object, "property_name", value2);
   window_location().hash = value;
 }
