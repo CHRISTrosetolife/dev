edcf56791f8d333f4066ff3cfea08ed54fea33bb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 12:23:49 -0400

    2025-04-28T16:23:48.522Z pm html_button_window_open text url

--- public/src/html_button_view_sorce.mjs
+++ public/src/html_button_view_sorce.mjs
@@ -3,5 +3,5 @@ import { function_name_to_url_github } from "./function_name_to_url_github.mjs";
 export function html_button_view_sorce(root, name) {
   let url = function_name_to_url_github(name);
   let text = "💻 view the source of this screen";
-  html_button_window_open(root, text, url);
+  html_button_window_open(root, url, text);
 }
--- public/src/html_button_window_open.mjs
+++ public/src/html_button_window_open.mjs
@@ -1,6 +1,6 @@
 import { window_open } from "./window_open.mjs";
 import { html_button } from "./html_button.mjs";
-export function html_button_window_open(root, text, url) {
+export function html_button_window_open(root, url, text) {
   html_button(root, text, function () {
     window_open(url);
   });
