fb9f5013ca5b2c38159414f0e8d4d9ecde0318e7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 16:43:33 -0400

    2025-04-28T20:43:32.148Z watch   app_index_choices_buttons

--- public/src/app_index_choices_buttons.mjs
+++ public/src/app_index_choices_buttons.mjs
@@ -1,22 +1,18 @@
-import { app_extension_html } from "./app_extension_html.mjs";
+import { app_extension_html_combine } from "./app_extension_html_combine.mjs";
 import { html_button } from "./html_button.mjs";
 import { window_open } from "./window_open.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { each } from "./each.mjs";
 export function app_index_choices_buttons(root, choices) {
-  each(choices, (choice) => {
+  each(choices, function (choice) {
     let url;
     if (object_property_exists(choice, "page")) {
-      url = string_combine_multiple([
-        object_property_get(choice, "page"),
-        app_extension_html(),
-      ]);
+      url = app_extension_html_combine(object_property_get(choice, "page"));
     } else {
       url = object_property_get(choice, "href");
     }
-    html_button(root, object_property_get(choice, "text"), () => {
+    html_button(root, object_property_get(choice, "text"), function () {
       window_open(url);
     });
   });
