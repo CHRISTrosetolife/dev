0ca88f0f43aebd3b0e76cc6601f12de367fbe187
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 19:05:51 -0400

    2025-04-28T23:05:50.631Z pmi html_button_email 2 1

--- public/src/html_button_me_email.mjs
+++ public/src/html_button_me_email.mjs
@@ -3,5 +3,5 @@ import { me_email } from "./me_email.mjs";
 export function html_button_me_email(parent) {
   let email = me_email();
   let text = "📨 Contact developer by email";
-  html_button_email(parent, text, email);
+  html_button_email(parent, email, text);
 }
