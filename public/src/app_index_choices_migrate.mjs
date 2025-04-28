3ca1f703fd19b48f9bf4d16bb98487ce90973fd9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 16:31:25 -0400

    2025-04-28T20:31:24.726Z watch   app_index_choices_migrate

--- public/src/app_index_choices_migrate.mjs
+++ public/src/app_index_choices_migrate.mjs
@@ -1,8 +1,11 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export async function app_index_choices_migrate() {
   await function_transform(fn_name("app_index_choices"), function (ast) {
-    js_visit_node(ast, "ObjectExpression", function (v) {});
+    js_visit_node(ast, "ObjectExpression", function (v) {
+      let node = object_property_get(v, "node");
+    });
   });
 }
