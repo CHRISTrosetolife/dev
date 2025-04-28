9e66e462880bdb54c5c78d9e3491460663e4d8fd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 11:46:42 -0400

    2025-04-28T15:46:41.826Z watch   string_trim

--- public/src/string_trim.mjs
+++ public/src/string_trim.mjs
@@ -1,12 +1,7 @@
+import { string_trim_end } from "./string_trim_end.mjs";
 import { string_trim_front } from "./string_trim_front.mjs";
-import { list_to } from "./list_to.mjs";
-import { string_take_less_1 } from "./string_take_less_1.mjs";
-import { string_ends_with_multiple } from "./string_ends_with_multiple.mjs";
 export function string_trim(t, symbols) {
   t = string_trim_front(t, symbols);
-  symbols = list_to(symbols);
-  while (string_ends_with_multiple(t, symbols)) {
-    t = string_take_less_1(t);
-  }
+  t = string_trim_end(symbols, t);
   return t;
 }
