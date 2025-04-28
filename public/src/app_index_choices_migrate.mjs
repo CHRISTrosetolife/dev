f1ef2a5bf3dae75c1187bf601b5f84187fedafe1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 16:31:41 -0400

    2025-04-28T20:31:41.296Z watch   app_index_choices_migrate

--- public/src/app_index_choices_migrate.mjs
+++ public/src/app_index_choices_migrate.mjs
@@ -6,6 +6,7 @@ export async function app_index_choices_migrate() {
   await function_transform(fn_name("app_index_choices"), function (ast) {
     js_visit_node(ast, "ObjectExpression", function (v) {
       let node = object_property_get(v, "node");
+      let properties = object_property_get(node, "properties");
     });
   });
 }
