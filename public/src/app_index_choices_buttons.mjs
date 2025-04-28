99434b558f16a553778181de7fe6fb24e1422b89
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 16:50:34 -0400

    2025-04-28T20:50:34.019Z watch   app_index_choices_buttons

--- public/src/app_index_choices_buttons.mjs
+++ public/src/app_index_choices_buttons.mjs
@@ -1,3 +1,4 @@
+import { app_name_string } from "./app_name_string.mjs";
 import { app_extension_html_combine } from "./app_extension_html_combine.mjs";
 import { html_button } from "./html_button.mjs";
 import { window_open } from "./window_open.mjs";
@@ -8,7 +9,8 @@ export function app_index_choices_buttons(root, choices) {
   each(choices, function (choice) {
     let url;
     if (object_property_exists(choice, "app")) {
-      url = app_extension_html_combine(object_property_get(choice, "app"));
+      let a = object_property_get(choice, "app");
+      url = app_extension_html_combine(app_name_string(a));
     } else {
       url = object_property_get(choice, "href");
     }
