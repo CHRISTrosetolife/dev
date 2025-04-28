5f2ec7d9e8d1a0fdd3505545bd74e1386c3124f8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 16:52:33 -0400

    2025-04-28T20:52:32.647Z watch   app_index_choices_pages

--- public/src/app_index_choices_pages.mjs
+++ public/src/app_index_choices_pages.mjs
@@ -1,18 +1,17 @@
-import { app_prefix } from "./app_prefix.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { each } from "./each.mjs";
 import { app_index_choices } from "./app_index_choices.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function app_index_choices_pages() {
-  return list_adder((la) => {
+  let v = list_adder(function (la) {
     let choices = app_index_choices();
-    each(choices, (choice) => {
-      if (object_property_exists(choice, "page")) {
-        let page = object_property_get(choice, "page");
-        la(string_combine_multiple([app_prefix(), page]));
+    each(choices, function (choice) {
+      if (object_property_exists(choice, "app")) {
+        let a = object_property_get(choice, "app");
+        la(a);
       }
     });
   });
+  return v;
 }
