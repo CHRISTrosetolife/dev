6e1bb12114762e5abe09a615d6260910041d52b1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 12:14:50 -0400

    2025-04-28T16:14:50.131Z watch   app_ceb

--- public/src/app_ceb.mjs
+++ public/src/app_ceb.mjs
@@ -1,3 +1,4 @@
+import { fn_name } from "./fn_name.mjs";
 import { html_button_width_full_text_link } from "./html_button_width_full_text_link.mjs";
 import { html_spacer_vertical_2 } from "./html_spacer_vertical_2.mjs";
 import { html_style_a_plain } from "./html_style_a_plain.mjs";
@@ -7,6 +8,7 @@ export async function app_ceb() {
   let body = html_style_default_initialize();
   html_style_a_plain();
   html_p_text(body, "It is recommended to use the new app");
+  let a_name = fn_name("app_ceb2");
   let url = "ceb2.html";
   let text = "New Cebuano Language Learn App";
   html_button_width_full_text_link(body, url, text);
