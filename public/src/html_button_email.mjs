c5926b29445a1f28011a84da9eff76141bb9378e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 19:05:33 -0400

    2025-04-28T23:05:32.771Z watch   html_button_email

---
+++ public/src/html_button_email.mjs
@@ -0,0 +1,10 @@
+import { window_location_href_set } from "./window_location_href_set.mjs";
+import { html_mailto } from "./html_mailto.mjs";
+import { html_link_prefix } from "./html_link_prefix.mjs";
+import { html_button } from "./html_button.mjs";
+export function html_button_email(parent, text, email) {
+  html_button(parent, text, function () {
+    let href = html_link_prefix(html_mailto(), email);
+    window_location_href_set(href);
+  });
+}
--- public/src/html_button_me_email.mjs
+++ public/src/html_button_me_email.mjs
@@ -4,8 +4,14 @@ import { html_link_prefix } from "./html_link_prefix.mjs";
 import { html_button } from "./html_button.mjs";
 import { me_email } from "./me_email.mjs";
 export function html_button_me_email(parent) {
-  html_button(parent, "📨 Contact developer by email", function () {
-    let href = html_link_prefix(html_mailto(), me_email());
-    window_location_href_set(href);
-  });
+    const email = me_email();
+    const text = "📨 Contact developer by email";
+  html_button_email(parent, text, email);
 }
+function html_button_email(parent, text, email) {
+    html_button(parent, text, function () {
+        let href = html_link_prefix(html_mailto(), email);
+        window_location_href_set(href);
+    });
+}
+
