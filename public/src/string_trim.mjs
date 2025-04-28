2cdf06b5b46a68f02763e817c048102318445c2c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 11:46:57 -0400

    2025-04-28T15:46:56.219Z pb string_trim_end

--- public/src/string_trim.mjs
+++ public/src/string_trim.mjs
@@ -2,6 +2,6 @@ import { string_trim_end } from "./string_trim_end.mjs";
 import { string_trim_front } from "./string_trim_front.mjs";
 export function string_trim(t, symbols) {
   t = string_trim_front(t, symbols);
-  t = string_trim_end(symbols, t);
+  t = string_trim_end(t, symbols);
   return t;
 }
--- public/src/string_trim_end.mjs
+++ public/src/string_trim_end.mjs
@@ -1,7 +1,7 @@
 import { string_take_less_1 } from "./string_take_less_1.mjs";
 import { string_ends_with_multiple } from "./string_ends_with_multiple.mjs";
 import { list_to } from "./list_to.mjs";
-export function string_trim_end(symbols, t) {
+export function string_trim_end(t, symbols) {
   symbols = list_to(symbols);
   while (string_ends_with_multiple(t, symbols)) {
     t = string_take_less_1(t);
