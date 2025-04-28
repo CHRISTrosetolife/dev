3d311293261660db62de65f12e84ce37b5afe4de
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-28 19:04:13 -0400

    2025-04-28T23:04:12.911Z watch   html_button_me_email

--- public/src/html_button_me_email.mjs
+++ public/src/html_button_me_email.mjs
@@ -1,8 +1,11 @@
+import { window_location_href_set } from "./window_location_href_set.mjs";
 import { html_mailto } from "./html_mailto.mjs";
 import { html_link_prefix } from "./html_link_prefix.mjs";
 import { html_button } from "./html_button.mjs";
 import { me_email } from "./me_email.mjs";
 export function html_button_me_email(parent) {
-  html_button(parent, "📨 Contact developer by email", function () {});
-  let href = html_link_prefix(html_mailto(), me_email());
+  html_button(parent, "📨 Contact developer by email", function () {
+    let href = html_link_prefix(html_mailto(), me_email());
+    window_location_href_set(href);
+  });
 }
