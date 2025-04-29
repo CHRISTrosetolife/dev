8d315b4aeea3f7083d768ea2c34e0a3856f5de06
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-04-29 05:42:07 -0400

    2025-04-29T09:42:07.111Z watch   app_portfolio

--- public/src/app_portfolio.mjs
+++ public/src/app_portfolio.mjs
@@ -11,10 +11,9 @@ import { each } from "./each.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { me_name } from "./me_name.mjs";
-import { html_p_text } from "./html_p_text.mjs";
 export function app_portfolio() {
   let root = html_style_default_initialize();
-  html_p_text(
+  html_p_text_centered(
     root,
     string_combine_multiple([
       "Welcome to the online portfolio of apps created by ",
