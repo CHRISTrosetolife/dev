183fab6d83274219b4bd7806639561c0e33d31dc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-02 14:12:54 -0400

    2025-05-02T18:12:53.847Z watch   app_ceb3

--- public/src/app_ceb3.mjs
+++ public/src/app_ceb3.mjs
@@ -1,6 +1,12 @@
-import { app_language_2 } from "./app_language_2.mjs";
+import { app_ceb3_group_index } from "./app_ceb3_group_index.mjs";
+import { app_language_2_generic } from "./app_language_2_generic.mjs";
 export async function app_ceb3() {
   let language_learn = "ceb";
   let language_fluent = "en";
-  await app_language_2_generic(app_ceb3, language_learn, language_fluent, app_ceb3_group_index());
+  await app_language_2_generic(
+    app_ceb3,
+    language_learn,
+    language_fluent,
+    app_ceb3_group_index(),
+  );
 }
