e607b82f4030b47bdc084f8004497ba248cb413b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-02 14:40:40 -0400

    2025-05-02T18:40:39.274Z watch   app_language_2_upload_pairs_ceb2

--- public/src/app_language_2_upload_pairs_ceb2.mjs
+++ public/src/app_language_2_upload_pairs_ceb2.mjs
@@ -1,3 +1,5 @@
+import { list_first } from "./list_first.mjs";
+import { string_size } from "./string_size.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -6,6 +8,7 @@ import { object_property_exists } from "./object_property_exists.mjs";
 import { ceb_definition_en_2 } from "./ceb_definition_en_2.mjs";
 import { each_async } from "./each_async.mjs";
 import { english_words_dictionary_take_soft } from "./english_words_dictionary_take_soft.mjs";
+import { list_sort_map } from "./list_sort_map.mjs";
 export async function app_language_2_upload_pairs_ceb2(limit) {
   let words = await english_words_dictionary_take_soft(limit);
   let pairs = await list_adder_async(async function (la) {
@@ -13,12 +16,16 @@ export async function app_language_2_upload_pairs_ceb2(limit) {
       let r = await ceb_definition_en_2(word);
       if (object_property_exists(r, word)) {
         let ds = object_property_get(r, word);
-        list_map(ds, function (d) {
+        let mapped = list_map(ds, function (d) {
           let v = [d, word];
           return v;
         });
-        each(ds, function (d) {
-          la([d, word]);
+        list_sort_map(mapped, function (p) {
+          let v2 = string_size(list_first(p));
+          return v2;
+        });
+        each(mapped, function (n) {
+          la([n, word]);
         });
       }
     });
