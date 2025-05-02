1e563df459a11f6ee42e67d0bb902220700893d1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-02 14:20:26 -0400

    2025-05-02T18:20:25.984Z watch   app_links

--- public/src/app_links.mjs
+++ public/src/app_links.mjs
@@ -1,3 +1,4 @@
+import { app_prefix_combine } from "./app_prefix_combine.mjs";
 import { apps_list } from "./apps_list.mjs";
 import { app_index_choices_buttons } from "./app_index_choices_buttons.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
@@ -7,7 +8,7 @@ export function app_links() {
   let choices = list_map(apps_list(), function (p) {
     let v = {
       text: p,
-      app: p,
+      app: app_prefix_combine(p),
     };
     return v;
   });
