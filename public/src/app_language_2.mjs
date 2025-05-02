0141a5ef67480eeda1fd2c38bab0017b7692979e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-02 14:11:47 -0400

    2025-05-02T18:11:46.776Z watch   app_language_2

--- public/src/app_language_2.mjs
+++ public/src/app_language_2.mjs
@@ -7,6 +7,7 @@ import { html_style_default_font_size } from "./html_style_default_font_size.mjs
 import { app_language_initialize } from "./app_language_initialize.mjs";
 import { app_language_group_index_changed_inner } from "./app_language_group_index_changed_inner.mjs";
 export async function app_language_2(app_fn, language_learn, language_fluent) {
+  let name = "all";
   let root = await app_language_initialize();
   html_style_default_font_size(app_language_2_font_size());
   html_style_a_plain();
@@ -16,7 +17,7 @@ export async function app_language_2(app_fn, language_learn, language_fluent) {
     language_fluent,
     root,
   };
-  await app_language_group_index_changed_inner(context, "all");
+  await app_language_group_index_changed_inner(context, name);
   let { words, group_flat } = app_language_2_group_get(context);
   app_language_2_words_initialize(context, words, group_flat);
   await app_language_2_refresh_learn(context);
