484384838f793d1e5b52343380682908792bb435
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-02 14:39:53 -0400

    2025-05-02T18:39:52.901Z watch   app_language_2_upload_pairs_ceb2

--- public/src/app_language_2_upload_pairs_ceb2.mjs
+++ public/src/app_language_2_upload_pairs_ceb2.mjs
@@ -1,3 +1,4 @@
+import { list_map } from "./list_map.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
@@ -12,6 +13,10 @@ export async function app_language_2_upload_pairs_ceb2(limit) {
       let r = await ceb_definition_en_2(word);
       if (object_property_exists(r, word)) {
         let ds = object_property_get(r, word);
+        list_map(ds, function (d) {
+          let v = [d, word];
+          return v;
+        });
         each(ds, function (d) {
           la([d, word]);
         });
