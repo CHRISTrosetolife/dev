18dad18b42733318017739b8bfd5c405bc54f7a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-04 11:17:18 -0400

    2025-05-04T15:17:17.455Z watch   app_ceb3_upload

--- public/src/app_ceb3_upload.mjs
+++ public/src/app_ceb3_upload.mjs
@@ -4,7 +4,7 @@ import { app_ceb3_group_index } from "./app_ceb3_group_index.mjs";
 export async function app_ceb3_upload() {
   let from = "ceb";
   let to = "en";
-  let limit = 1000;
+  let limit = 10000;
   let name = app_ceb3_group_index();
   let pairs = await app_language_2_upload_pairs_ceb2(limit);
   await app_language_2_upload_pairs(pairs, from, to, name);
