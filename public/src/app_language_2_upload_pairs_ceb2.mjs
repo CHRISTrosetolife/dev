265fe86bbd58db6155fae10c0e658d1136200d99
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-04 11:03:00 -0400

    2025-05-04T15:02:59.570Z watch   app_language_2_upload_pairs_ceb2

--- public/src/app_language_2_upload_pairs_ceb2.mjs
+++ public/src/app_language_2_upload_pairs_ceb2.mjs
@@ -1,7 +1,5 @@
 import { list_to_lookup_key_value_property } from "./list_to_lookup_key_value_property.mjs";
 import { bible_words_score_language } from "./bible_words_score_language.mjs";
-import { list_first } from "./list_first.mjs";
-import { string_size } from "./string_size.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -31,10 +29,11 @@ export async function app_language_2_upload_pairs_ceb2(limit) {
         if (false) {
           list_sort_map(mapped, function (m) {
             if (object_property_exists(scores_lookup, m)) {
-              object_property_get(object, "property_name");
+              let v2 = object_property_get(scores_lookup, m);
+              return v2;
             }
-            let v2 = string_size(list_first(m));
-            return v2;
+            let v3 = 0;
+            return v3;
           });
         }
         each(mapped, function (n) {
