7257abc4cead21a2e7c5c6882ba4de6888b4db3a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-05 12:41:15 -0400

    2025-05-05T16:41:14.490Z watch   app_pray

--- public/src/app_pray.mjs
+++ public/src/app_pray.mjs
@@ -1,9 +1,10 @@
-import { prayers_list } from "./prayers_list.mjs";
 import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
+import { prayers_list } from "./prayers_list.mjs";
 import { prayers } from "./prayers.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_pray() {
   let body = html_style_default_initialize();
   let ps = prayers();
-  html_p_text_multiple(body, prayers_list(ps));
+  let pl = prayers_list(ps);
+  html_p_text_multiple(body, pl);
 }
