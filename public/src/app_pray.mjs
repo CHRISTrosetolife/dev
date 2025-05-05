57a733e6f49d14f2625862ecfeb706845688a426
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-05 18:37:27 -0400

    2025-05-05T22:37:27.180Z watch   app_pray

--- public/src/app_pray.mjs
+++ public/src/app_pray.mjs
@@ -1,6 +1,7 @@
+import { html_style_background_color_transparent } from "./html_style_background_color_transparent.mjs";
+import { list_map_index } from "./list_map_index.mjs";
 import { storage_local_get } from "./storage_local_get.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
-import { each_index } from "./each_index.mjs";
 import { html_on_click } from "./html_on_click.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { prayers_list } from "./prayers_list.mjs";
@@ -10,14 +11,17 @@ export function app_pray() {
   let body = html_style_default_initialize();
   let ps = prayers();
   let pl = prayers_list(ps);
-  each_index(pl, function lambda_each(prayer, index) {
+  let rows = list_map_index(pl, function lambda_each(prayer, index) {
     let p = html_p_text(body, prayer);
     let selected = false;
     html_on_click(p, function () {
       selected = !selected;
       let index_old = storage_local_get(app_pray, "prayer_index");
+      html_style_background_color_transparent();
       let value = selected ? null : index;
       storage_local_set(app_pray, "prayer_index", value);
     });
+    let v = p;
+    return v;
   });
 }
