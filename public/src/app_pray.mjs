7489f3935b1b72c721cb6da59c751dc03e9ea8c9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-05 12:40:36 -0400

    2025-05-05T16:40:36.327Z watch   app_pray

--- public/src/app_pray.mjs
+++ public/src/app_pray.mjs
@@ -3,5 +3,6 @@ import { html_prayer } from "./html_prayer.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_pray() {
   let body = html_style_default_initialize();
-  html_prayer(body, prayers());
+  let ps = prayers();
+  html_prayer(body, ps);
 }
