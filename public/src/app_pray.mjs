7045fbaebf807c16936f92a81ba619ac8089f487
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-05 14:22:00 -0400

    2025-05-05T18:21:59.567Z watch   app_pray

--- public/src/app_pray.mjs
+++ public/src/app_pray.mjs
@@ -1,6 +1,6 @@
+import { each_index } from "./each_index.mjs";
 import { html_on_click } from "./html_on_click.mjs";
 import { html_p_text } from "./html_p_text.mjs";
-import { each } from "./each.mjs";
 import { prayers_list } from "./prayers_list.mjs";
 import { prayers } from "./prayers.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
