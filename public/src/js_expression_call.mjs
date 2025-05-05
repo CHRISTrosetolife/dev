40b8de44d4a2a9d971fa84d9482dffc7d2580769
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-05 12:51:35 -0400

    2025-05-05T16:51:34.549Z watch   js_expression_call

--- public/src/js_expression_call.mjs
+++ public/src/js_expression_call.mjs
@@ -1,8 +1,7 @@
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { js_code_call } from "./js_code_call.mjs";
-import { fn_name } from "./fn_name.mjs";
-export function js_expression_call(fn_name) {
-  let code_call = js_code_call(fn_name);
+export function js_expression_call(f_name) {
+  let code_call = js_code_call(f_name);
   let expression = js_parse_expression(code_call);
   return expression;
 }
