2bc0433ccca39e28d55f6e5db54e094a70284e72
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-05 12:51:17 -0400

    2025-05-05T16:51:17.444Z watch   js_expression_call_args

--- public/src/js_expression_call_args.mjs
+++ public/src/js_expression_call_args.mjs
@@ -1,11 +1,9 @@
 import { js_expression_call } from "./js_expression_call.mjs";
 import { fn_name } from "./fn_name.mjs";
-import { list_add } from "./list_add.mjs";
+import { list_add_multiple } from "./list_add_multiple.mjs";
 export function js_expression_call_args(fn_name, args) {
   let expression = js_expression_call(fn_name);
   let { arguments: args2 } = expression;
-  for (let a of args) {
-    list_add(args2, a);
-  }
+  list_add_multiple(args2, args);
   return expression;
 }
