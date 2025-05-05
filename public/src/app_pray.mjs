dcbf132e1f6c766877021d176c53f679dacd3837
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-05 14:26:35 -0400

    2025-05-05T18:26:35.179Z watch   app_pray

--- public/src/app_pray.mjs
+++ public/src/app_pray.mjs
@@ -1,6 +1,6 @@
-import { each_index } from "./each_index.mjs";
 import { html_on_click } from "./html_on_click.mjs";
 import { html_p_text } from "./html_p_text.mjs";
+import { each } from "./each.mjs";
 import { prayers_list } from "./prayers_list.mjs";
 import { prayers } from "./prayers.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
@@ -9,7 +9,7 @@ export function app_pray() {
   let ps = prayers();
   let pl = prayers_list(ps);
   $2i;
-  each_index(pl, function lambda_each(prayer, index) {
+  each(pl, function lambda_each(prayer) {
     let p = html_p_text(body, prayer);
     html_on_click(p, function () {});
   });
