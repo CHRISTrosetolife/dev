acce73f563bb420a4fcdab31fad24fbafed6699d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-05 12:44:52 -0400

    2025-05-05T16:44:52.184Z watch   js_for_to_each

--- public/src/js_for_to_each.mjs
+++ public/src/js_for_to_each.mjs
@@ -1,10 +1,11 @@
-import { never } from "./never.mjs";
-import { js_to_block_generic } from "./js_to_block_generic.mjs";
+import { log } from "./log.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
 export function js_for_to_each(ast) {
-  let v2 = js_visit_node(ast, "WhileStatement", function (v) {
+  let v2 = js_visit_node(ast, "ForOfStatement", function (v) {
     let { node } = v;
-    js_to_block_generic(node, ["body"], never);
+    log({
+      node,
+    });
   });
   return v2;
 }
