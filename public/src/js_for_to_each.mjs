050217552d6cdace789f358cee6b7b6639b6b95d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-05 12:47:19 -0400

    2025-05-05T16:47:19.317Z watch   js_for_to_each

--- public/src/js_for_to_each.mjs
+++ public/src/js_for_to_each.mjs
@@ -6,6 +6,7 @@ export function js_for_to_each(ast) {
     let { node } = v;
     let left = object_property_get(node, "left");
     let right = object_property_get(node, "right");
+    let declarations = object_property_get(left, "declarations");
     log({
       node,
     });
