05492fd7c23586aeae8fb6c4d734e26e9ef7be23
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-05 13:10:03 -0400

    2025-05-05T17:10:02.767Z ft js_for_to_each html_p_text_multiple

--- public/src/html_p_text_multiple.mjs
+++ public/src/html_p_text_multiple.mjs
@@ -1,6 +1,6 @@
 import { html_p_text } from "./html_p_text.mjs";
 export function html_p_text_multiple(parent, paragraphs) {
-  for (let p of paragraphs) {
+  each(paragraphs, function lambda_each(p) {
     html_p_text(parent, p);
-  }
+  });
 }
