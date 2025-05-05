84c3c6b5704f09f8b02d11d461e4ec9296ded6bd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-05 13:07:03 -0400

    2025-05-05T17:07:03.362Z watch   js_for_to_each

--- public/src/js_for_to_each.mjs
+++ public/src/js_for_to_each.mjs
@@ -1,5 +1,4 @@
-import { object_property_set } from "./object_property_set.mjs";
-import { js_code_function_declare } from "./js_code_function_declare.mjs";
+import { js_function_expression } from "./js_function_expression.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { js_name_unique } from "./js_name_unique.mjs";
@@ -9,8 +8,6 @@ import { js_node_type_not_is } from "./js_node_type_not_is.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { log } from "./log.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
-import { js_parse_expression } from "./js_parse_expression.mjs";
-import { list_add_multiple } from "./list_add_multiple.mjs";
 export function js_for_to_each(ast) {
   let v2 = js_visit_node(ast, "ForOfStatement", function (v) {
     let { node } = v;
@@ -43,12 +40,3 @@ export function js_for_to_each(ast) {
   });
   return v2;
 }
-function js_function_expression(lambda_name, async_is, params, body) {
-    let lambda_code = js_code_function_declare(lambda_name, "", "", async_is);
-    let lambda = js_parse_expression(lambda_code);
-    let params2 = object_property_get(lambda, "params");
-    list_add_multiple(params2, params);
-    object_property_set(lambda, "body", body);
-    return lambda;
-}
-
