577940e1bdb9677bee1d0fc5a5d1d79098b9f8ab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-05 12:41:09 -0400

    2025-05-05T16:41:09.266Z watch   html_prayer

--- public/src/html_prayer.mjs
+++ public/src/html_prayer.mjs
@@ -1,5 +1,6 @@
 import { prayers_list } from "./prayers_list.mjs";
 import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
 export function html_prayer(root, prayer) {
-  html_p_text_multiple(root, prayers_list(prayer));
+  let pl = prayers_list(prayer);
+  html_p_text_multiple(root, pl);
 }
