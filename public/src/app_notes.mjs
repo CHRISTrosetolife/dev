4c2626ecda9323fe7d77de5e464c16d340d2b83b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-06 11:24:11 -0400

    2025-05-06T15:24:10.754Z watch   app_notes

--- public/src/app_notes.mjs
+++ public/src/app_notes.mjs
@@ -1,6 +1,5 @@
+import { firebase_storage_initialize_context } from "./firebase_storage_initialize_context.mjs";
 import { app_todo_main } from "./app_todo_main.mjs";
-import { object_merge } from "./object_merge.mjs";
-import { firebase_storage_initialize } from "./firebase_storage_initialize.mjs";
 import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
 import { app_firebase } from "./app_firebase.mjs";
 import { html_button_add } from "./html_button_add.mjs";
@@ -11,10 +10,7 @@ export async function app_notes() {
   await app_firebase({
     on_logged_in: async function (context) {
       let index_path = app_todo_firebase_path_index();
-      let index = await firebase_storage_initialize(index_path);
-      object_merge(context, {
-        index,
-      });
+      await firebase_storage_initialize_context(index_path, context);
       app_todo_main(context);
     },
   });
