2c39a5c93fd50fb27fe02f6ffb1a5291f466900c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-06 11:20:04 -0400

    2025-05-06T15:20:03.437Z watch   html_button_add

--- public/src/html_button_add.mjs
+++ public/src/html_button_add.mjs
@@ -1,5 +1,5 @@
 import { html_button } from "./html_button.mjs";
 import { button_add_text } from "./button_add_text.mjs";
-export function html_button_add(root, text, download) {
-  html_button(root, button_add_text(text), download);
+export function html_button_add(root, text, on_click) {
+  html_button(root, button_add_text(text), on_click);
 }
