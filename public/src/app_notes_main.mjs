e58aacc339f8f6ac39635aeefbe485011dfa8923
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-06 18:56:21 -0400

    2025-05-06T22:56:20.992Z watch   app_notes_main

--- public/src/app_notes_main.mjs
+++ public/src/app_notes_main.mjs
@@ -1,9 +1,11 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { app_todo_main } from "./app_todo_main.mjs";
 import { html_item_add } from "./html_item_add.mjs";
 import { app_notes_firebase_path_index } from "./app_notes_firebase_path_index.mjs";
 export function app_notes_main(context) {
   let index_path = app_notes_firebase_path_index();
   html_item_add(context, app_todo_main, index_path, on_complete);
-  (function (on_complete) {});
-  function on_complete() {}
+  function on_complete(value) {
+    let index = object_property_get(context, "index");
+  }
 }
