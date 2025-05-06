84bc7d00c02193e1084133c8973cc19523f85648
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-06 19:01:18 -0400

    2025-05-06T23:01:18.511Z watch   app_notes

--- public/src/app_notes.mjs
+++ public/src/app_notes.mjs
@@ -2,11 +2,9 @@ import { app_notes_main } from "./app_notes_main.mjs";
 import { app_notes_firebase_path_index } from "./app_notes_firebase_path_index.mjs";
 import { firebase_storage_initialize_context } from "./firebase_storage_initialize_context.mjs";
 import { app_firebase } from "./app_firebase.mjs";
-import { html_button_add } from "./html_button_add.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_notes() {
   let root = html_style_default_initialize();
-  html_button_add(root, "note");
   await app_firebase({
     on_logged_in: async function (context) {
       let index_path = app_notes_firebase_path_index();
