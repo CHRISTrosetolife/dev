36577103673ccb6d59158ecf61ac13f8cc9db87b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-06 11:19:32 -0400

    2025-05-06T15:19:30.076Z watch   app_notes

--- public/src/app_notes.mjs
+++ public/src/app_notes.mjs
@@ -1,4 +1,6 @@
+import { html_button_add } from "./html_button_add.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_notes() {
   let root = html_style_default_initialize();
+  html_button_add();
 }
