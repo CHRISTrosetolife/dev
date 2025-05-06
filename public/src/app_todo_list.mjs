d6814f747bc27cdf1c351dcb71cd7270ddbcb660
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-06 19:49:57 -0400

    2025-05-06T23:49:57.301Z watch   app_todo_list

--- public/src/app_todo_list.mjs
+++ public/src/app_todo_list.mjs
@@ -1,5 +1,5 @@
 import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
-import { app_todo_list_generic } from "./app_todo_list_generic.mjs";
+import { app_todo_list_generic } from "./app_todo_list_generic.1.mjs";
 import { app_todo_properties } from "./app_todo_properties.mjs";
 export function app_todo_list(context, filtered, refresh, items_all) {
   let properties = app_todo_properties();
