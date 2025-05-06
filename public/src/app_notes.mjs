46c6e3e9dc058891a5a6bc43d3bb1d3fc0f0dfe0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-06 11:25:12 -0400

    2025-05-06T15:25:12.322Z watch   app_notes

--- public/src/app_notes.mjs
+++ public/src/app_notes.mjs
@@ -1,6 +1,5 @@
 import { firebase_storage_initialize_context } from "./firebase_storage_initialize_context.mjs";
 import { app_todo_main } from "./app_todo_main.mjs";
-import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
 import { app_firebase } from "./app_firebase.mjs";
 import { html_button_add } from "./html_button_add.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
@@ -9,7 +8,7 @@ export async function app_notes() {
   html_button_add(root, "note");
   await app_firebase({
     on_logged_in: async function (context) {
-      let index_path = app_todo_firebase_path_index();
+      let index_path = "notes/index.json";
       await firebase_storage_initialize_context(index_path, context);
       app_todo_main(context);
     },
