9e3fabbbad934d9315be0101bce12e616c4f0b8e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-11 14:48:20 -0400

    2025-05-11T18:48:20.300Z c html_parse_a_href_ends_with html_parse_a_href_surrounded

---
+++ public/src/html_parse_a_href_surrounded.mjs
@@ -0,0 +1,12 @@
+import { string_ends_with } from "./string_ends_with.mjs";
+import { html_parse_a_hrefs } from "./html_parse_a_hrefs.mjs";
+import { html_parse_href } from "./html_parse_href.mjs";
+import { list_filter } from "./list_filter.mjs";
+export function html_parse_a_href_surrounded(element, suffix) {
+  let found = html_parse_a_hrefs(element);
+  let filtered = list_filter(found, (i) => {
+    let href = html_parse_href(i);
+    return string_ends_with(href, suffix);
+  });
+  return filtered;
+}
