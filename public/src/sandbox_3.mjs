b2ca329f642be304954f0bd87b38c9c607afda16
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-11 14:51:23 -0400

    2025-05-11T18:51:22.136Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,12 +1,13 @@
-import { html_parse_a_href_starts_with_hrefs } from "./html_parse_a_href_starts_with_hrefs.mjs";
+import { html_parse_a_href_surrounded_hrefs } from "./html_parse_a_href_surrounded_hrefs.mjs";
 import { http_cache_parse_parsed } from "./http_cache_parse_parsed.mjs";
 export async function sandbox_3() {
   let u =
     "https://bible.catholicgallery.org/world-english-bible-with-deuterocanon/";
   let { root } = await http_cache_parse_parsed(u);
-  let v = await html_parse_a_href_starts_with_hrefs(
+  let v = await html_parse_a_href_surrounded_hrefs(
     root,
     "https://bible.catholicgallery.org/english/",
+    "-1/",
   );
   return v;
 }
