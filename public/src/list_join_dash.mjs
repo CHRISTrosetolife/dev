8b87db876959323dcf02d106d2c753662e51f458
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-11 16:00:48 -0400

    2025-05-11T20:00:47.650Z watch   list_join_dash

--- public/src/list_join_dash.mjs
+++ public/src/list_join_dash.mjs
@@ -1,4 +1,5 @@
 import { list_join } from "./list_join.mjs";
-export function list_join_dash(m) {
-  return list_join(m, "-");
+export function list_join_dash(list) {
+  let v = list_join(list, "-");
+  return v;
 }
