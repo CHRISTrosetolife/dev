c5fe4ef1575566f3a3595520bf4d7b15f076c147
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-11 14:40:49 -0400

    2025-05-11T18:40:48.233Z s3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,7 +1,8 @@
 import { html_cache_parse_hrefs } from "./html_cache_parse_hrefs.mjs";
+import { tautology } from "./tautology.mjs";
 export async function sandbox_3() {
   let u =
     "https://bible.catholicgallery.org/world-english-bible-with-deuterocanon/";
-  let v = await html_cache_parse_hrefs(u);
+  let v = await html_cache_parse_hrefs(u, tautology);
   return v;
 }
