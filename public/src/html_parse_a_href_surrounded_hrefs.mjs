896cc07273e32b4c9306ab85b6cb11568ff849b7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-11 14:49:47 -0400

    2025-05-11T18:49:45.925Z c html_parse_a_href_starts_with_hrefs html_parse_a_href_surrounded_hrefs

---
+++ public/src/html_parse_a_href_surrounded_hrefs.mjs
@@ -0,0 +1,6 @@
+import { html_parse_a_href_starts_with_hrefs_generic } from "./html_parse_a_href_starts_with_hrefs_generic.mjs";
+import { html_parse_a_href_starts_with } from "./html_parse_a_href_starts_with.mjs";
+export function html_parse_a_href_surrounded_hrefs(root, prefix) {
+  let links_get = html_parse_a_href_starts_with;
+  return html_parse_a_href_starts_with_hrefs_generic(links_get, root, prefix);
+}
