b9f00bad9772d1cedf54fe2497c814bd348d04f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-11 14:37:08 -0400

    2025-05-11T18:37:07.831Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,8 +1,3 @@
-import { app_language_2_upload_pairs_ceb2 } from "./app_language_2_upload_pairs_ceb2.mjs";
-import { http_cache } from "./http_cache.mjs";
 export async function sandbox_3() {
-  let text2 = await http_cache(
-    "https://raw.githubusercontent.com/first20hours/google-10000-english/refs/heads/master/google-10000-english-usa.txt",
-  );
-  await app_language_2_upload_pairs_ceb2(2000);
+  "https://bible.catholicgallery.org/world-english-bible-with-deuterocanon/";
 }
