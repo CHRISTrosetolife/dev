11ab9292fd78ac6f51f6ae1bfb0b7fd4e70721d8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-11 16:02:02 -0400

    2025-05-11T20:01:59.866Z watch   range_generic

--- public/src/range_generic.mjs
+++ public/src/range_generic.mjs
@@ -1,9 +1,13 @@
+import { never } from "./never.mjs";
+import { assert } from "./assert.mjs";
 import { add } from "./add.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function range_generic(count, offset) {
-  return list_adder((la) => {
+  assert(never, []);
+  let v = list_adder(function (la) {
     for (let i = 0; i < count; i++) {
       la(add(i, offset));
     }
   });
+  return v;
 }
