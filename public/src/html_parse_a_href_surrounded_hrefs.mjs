4d6b3b13c964205f3eed2c494488c2705a9fc0a2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-11 14:50:34 -0400

    2025-05-11T18:50:34.063Z watch   html_parse_a_href_surrounded_hrefs

--- public/src/html_parse_a_href_surrounded_hrefs.mjs
+++ public/src/html_parse_a_href_surrounded_hrefs.mjs
@@ -1,7 +1,9 @@
+import { html_parse_href } from "./html_parse_href.mjs";
+import { list_map } from "./list_map.mjs";
 import { html_parse_a_href_surrounded } from "./html_parse_a_href_surrounded.mjs";
-import { html_parse_a_href_starts_with_hrefs_generic } from "./html_parse_a_href_starts_with_hrefs_generic.mjs";
 export function html_parse_a_href_surrounded_hrefs(root, prefix) {
   let links_get = html_parse_a_href_surrounded;
-  let v = html_parse_a_href_starts_with_hrefs_generic(links_get, root, prefix);
+  let links = links_get(root, prefix);
+  let v = list_map(links, html_parse_href);
   return v;
 }
