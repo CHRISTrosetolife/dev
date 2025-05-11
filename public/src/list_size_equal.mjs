ed3a999640838edfcb398a918746c7ea951a24f8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-11 15:36:03 -0400

    2025-05-11T19:36:02.517Z watch   list_size_equal

--- public/src/list_size_equal.mjs
+++ public/src/list_size_equal.mjs
@@ -1,4 +1,5 @@
 import { lists_sizes_equal } from "./lists_sizes_equal.mjs";
-export function list_size_equal(params_names, args_names) {
-  return lists_sizes_equal([params_names, args_names]);
+export function list_size_equal(list1, list2) {
+  let v = lists_sizes_equal([list1, list2]);
+  return v;
 }
