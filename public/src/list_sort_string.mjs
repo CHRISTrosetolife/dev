0e0319d2cce6ed2783c7dab17182c1f9ad4a1b2b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-11 20:18:06 -0400

    2025-05-12T00:18:04.091Z watch   list_sort_string

--- public/src/list_sort_string.mjs
+++ public/src/list_sort_string.mjs
@@ -1,5 +1,5 @@
 import { identity } from "./identity.mjs";
 import { list_sort_string_map } from "./list_sort_string_map.mjs";
-export function list_sort_string(taken) {
-  list_sort_string_map(taken, identity);
+export function list_sort_string(list) {
+  list_sort_string_map(list, identity);
 }
