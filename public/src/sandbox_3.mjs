076ff42bf95d6b052d99e2852f44c7a9f43d9c87
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-11 20:03:19 -0400

    2025-05-12T00:03:18.524Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,8 +1,10 @@
+import { bible_chapter_parsed } from "./bible_chapter_parsed.mjs";
 import { log } from "./log.mjs";
 import { bible_books_chapter_each } from "./bible_books_chapter_each.mjs";
 export async function sandbox_3() {
   let bible_folder = "engwebu";
-  await bible_books_chapter_each(bible_folder, function (chapter) {
+  await bible_books_chapter_each(bible_folder, async function (chapter) {
+    let root = await bible_chapter_parsed(bible_folder, chapter_name);
     log({
       chapter,
     });
