fdb8b0f56bf34a2480703e4973d7df13b9cb2fd0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-11 20:15:30 -0400

    2025-05-12T00:15:28.009Z watch   html_parse_visit_attribute

--- public/src/html_parse_visit_attribute.mjs
+++ public/src/html_parse_visit_attribute.mjs
@@ -1,7 +1,9 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { html_parse_visit } from "./html_parse_visit.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { undefined_not_is } from "./undefined_not_is.mjs";
 export function html_parse_visit_attribute(root, attribute_name, lambda) {
+  assert_arguments_length(arguments, 3);
   function lambda_inner(v) {
     let { node } = v;
     let { attribs } = node;
