2ef4cefd16dd5598b302ccf6ea53b16b7d65ce0e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-13 14:35:10 -0400

    2025-05-13T18:35:09.813Z watch   ebible_classes

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -11,30 +11,34 @@ import { bible_books_chapter_each } from "./bible_books_chapter_each.mjs";
 import { html_parse_text } from "./html_parse_text.mjs";
 import { object_properties } from "./object_properties.mjs";
 export async function sandbox_3() {
-  let found = {};
-  let bible_folder = "engwebu";
-  await bible_books_chapter_each(bible_folder, async function (chapter_name) {
-    let root = await bible_chapter_parsed(bible_folder, chapter_name);
-    let main = html_parse_visit_class_single(root, "main");
-    html_parse_visit_attribute(main, "class", function (v) {
-      let { node } = v;
-      let { attribs } = node;
-      let class2 = object_property_get(attribs, "class");
-      if (object_property_exists_not(found, class2)) {
-        object_property_set(found, class2, {
-          chapter_name,
-          class: class2,
-          text: html_parse_text(node),
-        });
-      }
-    });
-  });
-  let ps = object_properties(found);
-  list_sort_string(ps);
-  each(ps, function (p) {
-    log({
-      property: p,
-      value: object_property_get(found, p),
-    });
-  });
+    let bible_folder = "engwebu";
+    await ebible_classes(bible_folder);
 }
+async function ebible_classes(bible_folder) {
+    let found = {};
+    await bible_books_chapter_each(bible_folder, async function (chapter_name) {
+        let root = await bible_chapter_parsed(bible_folder, chapter_name);
+        let main = html_parse_visit_class_single(root, "main");
+        html_parse_visit_attribute(main, "class", function (v) {
+            let { node } = v;
+            let { attribs } = node;
+            let class2 = object_property_get(attribs, "class");
+            if (object_property_exists_not(found, class2)) {
+                object_property_set(found, class2, {
+                    chapter_name,
+                    class: class2,
+                    text: html_parse_text(node),
+                });
+            }
+        });
+    });
+    let ps = object_properties(found);
+    list_sort_string(ps);
+    each(ps, function (p) {
+        log({
+            property: p,
+            value: object_property_get(found, p),
+        });
+    });
+}
+
