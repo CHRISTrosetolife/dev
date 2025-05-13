9c77991c9533aaae91f372a6b41a0d859e95b55d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-13 18:41:42 -0400

    2025-05-13T22:41:40.227Z pb folder_gitignore_path_function_json

--- public/src/folder_gitignore_path_function_json.mjs
+++ public/src/folder_gitignore_path_function_json.mjs
@@ -1,6 +1,6 @@
 import { folder_gitignore_path_function } from "./folder_gitignore_path_function.mjs";
 import { file_name_json } from "./file_name_json.mjs";
-export function folder_gitignore_path_function_json(u, fn) {
+export function folder_gitignore_path_function_json(fn, u) {
   let file_name = file_name_json(u);
   let file_path = folder_gitignore_path_function(fn, file_name);
   return file_path;
--- public/src/uuid_file.mjs
+++ public/src/uuid_file.mjs
@@ -5,7 +5,7 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function uuid_file(fn, lambda) {
   assert_arguments_length(arguments, 2);
   let u = await uuid();
-  let file_path = folder_gitignore_path_function_json(u, fn);
+  let file_path = folder_gitignore_path_function_json(fn, u);
   try {
     let v = await lambda(file_path);
     return v;
