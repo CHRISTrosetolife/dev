959315b7bcfca222982273f380d2722fc4fdde49
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-13 14:31:48 -0400

    2025-05-13T18:31:46.197Z watch   bible_books

--- public/src/bible_books.mjs
+++ public/src/bible_books.mjs
@@ -1,18 +1,9 @@
+import { bible_books_generic } from "./bible_books_generic.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
-import { bible_books_html_element_to_prefix } from "./bible_books_html_element_to_prefix.mjs";
 import { bible_books_html_elements } from "./bible_books_html_elements.mjs";
-import { list_map } from "./list_map.mjs";
 export async function bible_books(bible_folder) {
   assert_arguments_length(arguments, 1);
   let elements_get = bible_books_html_elements;
-  return await bible_books_generic(elements_get, bible_folder);
+  let v = await bible_books_generic(elements_get, bible_folder);
+  return v;
 }
-async function bible_books_generic(elements_get, bible_folder) {
-    let book_elements = await elements_get(bible_folder);
-    let book_prefixes = list_map(
-        book_elements,
-        bible_books_html_element_to_prefix
-    );
-    return book_prefixes;
-}
-
