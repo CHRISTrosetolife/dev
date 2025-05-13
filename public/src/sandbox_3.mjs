73a8f718b57124fc1458efbc3ef32343257fe1c8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-13 14:38:26 -0400

    2025-05-13T18:38:25.421Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,14 +1,20 @@
+import { list_map_property } from "./list_map_property.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { bible_chapter } from "./bible_chapter.mjs";
 import { bible_books_apocrypha } from "./bible_books_apocrypha.mjs";
 import { list_first } from "./list_first.mjs";
+import { list_map } from "./list_map.mjs";
+import { list_join_space } from "./list_join_space.mjs";
 export async function sandbox_3() {
   let bible_folder = "engwebu";
   let books = await bible_books_apocrypha(bible_folder);
   let first = list_first(books);
-  let v = await bible_chapter(
+  let verses = await bible_chapter(
     bible_folder,
     string_combine_multiple([first, "01"]),
   );
-  return v;
+  list_join_space(
+    list_map(list_map_property(verses, "tokens"), list_join_space),
+  );
+  return verses;
 }
