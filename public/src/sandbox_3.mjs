7f7381da8db63bc600d5b8e8915bcc40ce5b7c5a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-13 19:13:55 -0400

    2025-05-13T23:13:54.518Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,3 +1,9 @@
+import { command_line } from "./command_line.mjs";
+import { log } from "./log.mjs";
+import { path_join } from "./path_join.mjs";
+import { folder_external_root } from "./folder_external_root.mjs";
+import { file_write } from "./file_write.mjs";
+import { uuid_file } from "./uuid_file.mjs";
 import { each_index_async } from "./each_index_async.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
@@ -16,5 +22,25 @@ export async function sandbox_3() {
     let text = list_join_space(
       list_map(list_map_property(verses, "tokens"), list_join_space),
     );
+    await uuid_file(sandbox_3, async function (file_path) {
+      await file_write(file_path, text);
+      let output_path = folder_external_root(
+        path_join(["bible\\english", bible_folder, "apocrypha"]),
+      );
+      let program = folder_external_root(
+        "programs\\WPy64-312100\\scripts\\python.bat",
+      );
+      let command = list_join_space([
+        program,
+        "py/tts.py",
+        file_path,
+        output_path,
+      ]);
+      log({
+        command,
+      });
+      let result = await command_line(command);
+      log(result);
+    });
   });
 }
