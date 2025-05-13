cd3a804b7073d2aa4c701f95f58aca1aef25e084
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-13 14:36:06 -0400

    2025-05-13T18:36:05.196Z watch   sandbox_3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,7 +1,9 @@
+import { bible_chapter } from "./bible_chapter.mjs";
 import { bible_books_apocrypha } from "./bible_books_apocrypha.mjs";
 import { list_first } from "./list_first.mjs";
 export async function sandbox_3() {
   let bible_folder = "engwebu";
   let books = await bible_books_apocrypha(bible_folder);
   let first = list_first(books);
+  await bible_chapter();
 }
