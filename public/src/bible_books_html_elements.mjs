8262a453d72ccb2bc3737f127db1a8110c619f8e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-13 14:33:33 -0400

    2025-05-13T18:33:30.394Z watch   ebible_class_apocrypha

--- public/src/bible_books_html_elements.mjs
+++ public/src/bible_books_html_elements.mjs
@@ -1,6 +1,7 @@
+import { ebible_class_apocrypha } from "./ebible_class_apocrypha.mjs";
 import { bible_books_html_elements_generic } from "./bible_books_html_elements_generic.mjs";
 export async function bible_books_html_elements(bible_folder) {
-  let classes = ["oo", "aa", "nn"];
+  let classes = ["oo", ebible_class_apocrypha(), "nn"];
   let v = await bible_books_html_elements_generic(bible_folder, classes);
   return v;
 }
