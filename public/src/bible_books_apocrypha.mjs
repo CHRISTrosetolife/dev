df2bfe75912fb57acd94990a83fae8241add5c14
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-13 14:32:39 -0400

    2025-05-13T18:32:35.789Z c bible_books bible_books_apocrypha

---
+++ public/src/bible_books_apocrypha.mjs
@@ -0,0 +1,9 @@
+import { bible_books_generic } from "./bible_books_generic.mjs";
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
+import { bible_books_html_elements } from "./bible_books_html_elements.mjs";
+export async function bible_books_apocrypha(bible_folder) {
+  assert_arguments_length(arguments, 1);
+  let elements_get = bible_books_html_elements;
+  let v = await bible_books_generic(bible_folder, elements_get);
+  return v;
+}
