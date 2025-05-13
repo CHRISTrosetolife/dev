98258b72b128624de4265c71edc5bc23a357bd6d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-13 14:31:42 -0400

    2025-05-13T18:31:41.394Z watch   bible_books_generic

--- public/src/bible_books.mjs
+++ public/src/bible_books.mjs
@@ -5,10 +5,14 @@ import { list_map } from "./list_map.mjs";
 export async function bible_books(bible_folder) {
   assert_arguments_length(arguments, 1);
   let elements_get = bible_books_html_elements;
-  let book_elements = await elements_get(bible_folder);
-  let book_prefixes = list_map(
-    book_elements,
-    bible_books_html_element_to_prefix,
-  );
-  return book_prefixes;
+  return await bible_books_generic(elements_get, bible_folder);
 }
+async function bible_books_generic(elements_get, bible_folder) {
+    let book_elements = await elements_get(bible_folder);
+    let book_prefixes = list_map(
+        book_elements,
+        bible_books_html_element_to_prefix
+    );
+    return book_prefixes;
+}
+
---
+++ public/src/bible_books_generic.mjs
@@ -0,0 +1,10 @@
+import { bible_books_html_element_to_prefix } from "./bible_books_html_element_to_prefix.mjs";
+import { list_map } from "./list_map.mjs";
+export async function bible_books_generic(elements_get, bible_folder) {
+  let book_elements = await elements_get(bible_folder);
+  let book_prefixes = list_map(
+    book_elements,
+    bible_books_html_element_to_prefix,
+  );
+  return book_prefixes;
+}
