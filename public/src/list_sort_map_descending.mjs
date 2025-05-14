8440e87e13eecd19f646453ecdc093e66d0c499e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-14 17:13:55 -0400

    2025-05-14T21:13:49.081Z watch   list_sort_map_descending

--- public/src/list_sort_map_descending.mjs
+++ public/src/list_sort_map_descending.mjs
@@ -1,6 +1,6 @@
 export function list_sort_map_descending(list, element_mapper) {
   list.sort(function (a, b) {
-    let v = element_mapper(a) - element_mapper(b);
+    let v = element_mapper(b) - element_mapper(a);
     return v;
   });
 }
