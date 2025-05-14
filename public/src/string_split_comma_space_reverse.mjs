c1fc37b37cba8b1925fcd3edaf74fb61946a5098
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-14 12:25:58 -0400

    2025-05-14T16:25:57.620Z watch   string_split_comma_space_reverse

--- public/src/string_split_comma_space_reverse.mjs
+++ public/src/string_split_comma_space_reverse.mjs
@@ -1,13 +1,9 @@
+import { string_split_comma_space } from "./string_split_comma_space.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 import { list_reverse } from "./list_reverse.mjs";
-import { string_split } from "./string_split.mjs";
 export function string_split_comma_space_reverse(d) {
   let s = string_split_comma_space(d);
   list_reverse(s);
   let v3 = list_join_space(s);
   return v3;
 }
-function string_split_comma_space(d) {
-    return string_split(d, ", ");
-}
-
