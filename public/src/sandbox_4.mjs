12ba278164bfb88432e966f5a4f8c6d6268afe39
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-15 19:49:15 -0400

    2025-05-15T23:49:14.566Z watch   sandbox_4

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -3,8 +3,9 @@ import { catholic_bishops_letter } from "./catholic_bishops_letter.mjs";
 import { each_async } from "./each_async.mjs";
 import { keyboard_keys_sorted } from "./keyboard_keys_sorted.mjs";
 export async function sandbox_4() {
-  await list_adder_async(async function (la) {});
-  await each_async(keyboard_keys_sorted(), async function (letter) {
-    let bishops = await catholic_bishops_letter(letter);
+  await list_adder_async(async function (la) {
+    await each_async(keyboard_keys_sorted(), async function (letter) {
+      let bishops = await catholic_bishops_letter(letter);
+    });
   });
 }
