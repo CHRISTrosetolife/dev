ae5ad0d7355364b56fd27be968e407a3a58cb8ae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-15 19:50:34 -0400

    2025-05-15T23:50:33.491Z watch   sandbox_4

---
+++ public/src/catholic_bishops_generate.mjs
@@ -0,0 +1,12 @@
+import { catholic_bishops_letter } from "./catholic_bishops_letter.mjs";
+import { keyboard_keys_sorted } from "./keyboard_keys_sorted.mjs";
+import { each_async } from "./each_async.mjs";
+import { list_adder_multiple_async } from "./list_adder_multiple_async.mjs";
+export async function catholic_bishops_generate() {
+  let all = await list_adder_multiple_async(async function (la) {
+    await each_async(keyboard_keys_sorted(), async function (letter) {
+      let bishops = await catholic_bishops_letter(letter);
+      la(bishops);
+    });
+  });
+}
--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -1,12 +1,4 @@
-import { list_adder_multiple_async } from "./list_adder_multiple_async.mjs";
-import { catholic_bishops_letter } from "./catholic_bishops_letter.mjs";
-import { each_async } from "./each_async.mjs";
-import { keyboard_keys_sorted } from "./keyboard_keys_sorted.mjs";
+import { catholic_bishops_generate } from "./catholic_bishops_generate.mjs";
 export async function sandbox_4() {
-  let all = await list_adder_multiple_async(async function (la) {
-    await each_async(keyboard_keys_sorted(), async function (letter) {
-      let bishops = await catholic_bishops_letter(letter);
-      la(bishops);
-    });
-  });
+  await catholic_bishops_generate();
 }
