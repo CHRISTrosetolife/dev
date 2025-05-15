3b921a20dae6708c436f5b058fd45c156623f02c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-15 19:40:35 -0400

    2025-05-15T23:40:35.064Z watch   catholic_bishops_letter

--- public/src/catholic_bishops_letter.mjs
+++ public/src/catholic_bishops_letter.mjs
@@ -1,8 +1,9 @@
+import { equal } from "./equal.mjs";
+import { assert } from "./assert.mjs";
 import { html_parse_tag_named } from "./html_parse_tag_named.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { html_parse_children } from "./html_parse_children.mjs";
 import { list_size } from "./list_size.mjs";
-import { log } from "./log.mjs";
 import { html_parse_visit_tag_single } from "./html_parse_visit_tag_single.mjs";
 import { http_cache_parse_parsed } from "./http_cache_parse_parsed.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
@@ -19,5 +20,5 @@ export async function catholic_bishops_letter(letter) {
     let v = html_parse_tag_named(c, "ul");
     return v;
   });
-  log(list_size(uls));
+  assert(equal, [list_size(uls), 2]);
 }
