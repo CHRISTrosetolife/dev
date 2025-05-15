791357eec1df9f3cbb49c3a8e0eda1dbbe729577
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-15 19:39:46 -0400

    2025-05-15T23:39:43.988Z watch   catholic_bishops_letter

--- public/src/catholic_bishops_letter.mjs
+++ public/src/catholic_bishops_letter.mjs
@@ -1,6 +1,8 @@
+import { html_parse_tag_named } from "./html_parse_tag_named.mjs";
+import { list_filter } from "./list_filter.mjs";
+import { html_parse_children } from "./html_parse_children.mjs";
 import { list_size } from "./list_size.mjs";
 import { log } from "./log.mjs";
-import { html_parse_visit_tag_list } from "./html_parse_visit_tag_list.mjs";
 import { html_parse_visit_tag_single } from "./html_parse_visit_tag_single.mjs";
 import { http_cache_parse_parsed } from "./http_cache_parse_parsed.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
@@ -12,6 +14,10 @@ export async function catholic_bishops_letter(letter) {
   ]);
   let { root } = await http_cache_parse_parsed(url);
   let body = html_parse_visit_tag_single(root, "body");
-  let uls = html_parse_visit_tag_list(body, "ul");
+  let children = html_parse_children(body);
+  let uls = list_filter(children, function (c) {
+    let v = html_parse_tag_named(c, "ul");
+    return v;
+  });
   log(list_size(uls));
 }
