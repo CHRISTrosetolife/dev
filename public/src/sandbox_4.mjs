8879d24cd76989a569cc7366856ed6a6f9c4ce6b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-15 19:34:58 -0400

    2025-05-15T23:34:56.885Z watch   sandbox_4

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -1,6 +1,5 @@
+import { http_cache_parse_parsed } from "./http_cache_parse_parsed.mjs";
 import { each_async } from "./each_async.mjs";
-import { log } from "./log.mjs";
-import { http_cache } from "./http_cache.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { keyboard_keys_sorted } from "./keyboard_keys_sorted.mjs";
 export async function sandbox_4() {
@@ -10,9 +9,6 @@ export async function sandbox_4() {
       letter,
       ".html",
     ]);
-    let s = await http_cache(url);
-    log({
-      s,
-    });
+    let { root } = await http_cache_parse_parsed(url);
   });
 }
