b1868e45f59c52cf003dd3c4d91c66c4fcdf72d4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-15 19:52:09 -0400

    2025-05-15T23:52:08.881Z watch   generate_list_generic

--- public/src/generate_list_generic.mjs
+++ public/src/generate_list_generic.mjs
@@ -3,5 +3,5 @@ import { js_code_array } from "./js_code_array.mjs";
 import { js_code_statement_return } from "./js_code_statement_return.mjs";
 export async function generate_list_generic(delimited, name) {
   let r = js_code_statement_return(js_code_array(delimited));
-  await function_new_generic_code(name, "", r, false, [], false, true);
+  await function_new_generic_code(name, "", r, true, [], false, true);
 }
