76e013e13c97d6d7f4d7541a1e2c8eaa78e55c10
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-15 19:51:20 -0400

    2025-05-15T23:51:20.546Z watch   catholic_bishops_generate

--- public/src/catholic_bishops_generate.mjs
+++ public/src/catholic_bishops_generate.mjs
@@ -4,11 +4,13 @@ import { keyboard_keys_sorted } from "./keyboard_keys_sorted.mjs";
 import { each_async } from "./each_async.mjs";
 import { list_adder_multiple_async } from "./list_adder_multiple_async.mjs";
 export async function catholic_bishops_generate() {
-  let all = await list_adder_multiple_async(async function (la) {
-    await each_async(keyboard_keys_sorted(), async function (letter) {
-      let bishops = await catholic_bishops_letter(letter);
-      la(bishops);
+  await generate_list_fn(async function () {
+    let v = await list_adder_multiple_async(async function (la) {
+      await each_async(keyboard_keys_sorted(), async function (letter) {
+        let bishops = await catholic_bishops_letter(letter);
+        la(bishops);
+      });
     });
+    return v;
   });
-  await generate_list_fn(function () {});
 }
