91bbbfd238b98d2af8b1f3d689efdcfa6e0c5cb6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-15 19:55:47 -0400

    2025-05-15T23:55:46.533Z watch   html_parse_parsed

--- public/src/html_parse_parsed.mjs
+++ public/src/html_parse_parsed.mjs
@@ -3,7 +3,9 @@ import { import_node } from "./import_node.mjs";
 export async function html_parse_parsed(input) {
   'import * as cheerio from "cheerio";';
   let cheerio = await import_node("cheerio");
-  let parsed = cheerio.load(input);
+  let parsed = cheerio.load(input, {
+    xml: true,
+  });
   let root = object_property_get(parsed.root(), "0");
   let result = {
     parsed,
