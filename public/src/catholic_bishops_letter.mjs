1848b2833e9c90eb68819c0f0ca6218d3ff7489a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-15 19:45:24 -0400

    2025-05-15T23:45:23.994Z catholic_bishops_letter a

--- public/src/catholic_bishops_letter.mjs
+++ public/src/catholic_bishops_letter.mjs
@@ -29,7 +29,7 @@ export async function catholic_bishops_letter(letter) {
   let s = list_second(uls);
   let s_children = html_parse_children(s);
   let v2 = list_map(s_children, function (li) {
-    log(li.name);
+    log(li.type);
     let v3 = html_parse_text(html_parse_visit_tag_first(li, "a"));
     return v3;
   });
