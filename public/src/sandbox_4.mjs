c58af3215b0d33c4b73f2ac2490437b1bf319b68
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-15 19:23:25 -0400

    2025-05-15T23:23:22.263Z watch   sandbox_4

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -1,12 +1,18 @@
+import { log } from "./log.mjs";
+import { http_cache } from "./http_cache.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { keyboard_keys_sorted } from "./keyboard_keys_sorted.mjs";
 import { each } from "./each.mjs";
 export async function sandbox_4() {
-  each(keyboard_keys_sorted(), function (letter) {
+  each(keyboard_keys_sorted(), async function (letter) {
     let url = string_combine_multiple([
       "https://www.catholic-hierarchy.org/bishop/la",
       letter,
       ".html",
     ]);
+    let s = await http_cache(url);
+    log({
+      s,
+    });
   });
 }
