e8e334dce847bf07f981f36f6b4a8a70572fee45
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-16 09:06:50 -0400

    2025-05-16T13:06:48.564Z watch   http_buffer

--- public/src/http_buffer.mjs
+++ public/src/http_buffer.mjs
@@ -1,6 +1,6 @@
 import { http_response } from "./http_response.mjs";
 export async function http_buffer(url) {
   let response = await http_response(url);
-  let body = await response.text();
+  let body = await response.buffer();
   return body;
 }
