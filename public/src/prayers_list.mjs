d417d2d1b17c97ac65e8253f2eb889bf9ea194cc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-16 18:52:59 -0400

    2025-05-16T22:52:59.186Z watch   prayers_list_begin

--- public/src/prayers_list.mjs
+++ public/src/prayers_list.mjs
@@ -1,13 +1,17 @@
 import { list_denest } from "./list_denest.mjs";
 export function prayers_list(prayer) {
   let v = list_denest([
-    [
-      "Heavenly Father,",
-      "In the name of Jesus:",
-      "Have mercy on me, a sinner;",
-    ],
+    prayers_list_begin(),
     prayer,
     "Amen",
   ]);
   return v;
+}
+
+function prayers_list_begin() {
+    return [
+        "Heavenly Father,",
+        "In the name of Jesus:",
+        "Have mercy on me, a sinner;",
+    ];
 }
---
+++ public/src/prayers_list_begin.mjs
@@ -0,0 +1,8 @@
+export function prayers_list_begin() {
+  let v = [
+    "Heavenly Father,",
+    "In the name of Jesus:",
+    "Have mercy on me, a sinner;",
+  ];
+  return v;
+}
