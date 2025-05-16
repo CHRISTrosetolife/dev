3470d1998a90c0b06654f2b54da1095ae4381494
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-16 09:09:17 -0400

    2025-05-16T13:09:17.071Z watch   http_iso_8859_1

--- public/src/http_iso_8859_1.mjs
+++ public/src/http_iso_8859_1.mjs
@@ -1,4 +1,5 @@
 import { http_buffer } from "./http_buffer.mjs";
+import iconv from "iconv-lite";
 export async function http_iso_8859_1(url) {
   let b = await http_buffer(url);
   let html = iconv.decode(b, "ISO-8859-1");
