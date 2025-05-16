196c9100f543eeee73de6f62cf578a1353f504c2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-16 12:24:43 -0400

    2025-05-16T16:24:42.730Z watch   sandbox_4

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -2,7 +2,10 @@ import { http_iso_8859_1_cache_parse_parsed } from "./http_iso_8859_1_cache_pars
 import { file_extension_html } from "./file_extension_html.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function sandbox_4() {
-  let prefix = "https://www.catholic-hierarchy.org/bishop/ll";
+  let prefix = string_combine_multiple([
+    "https://www.catholic-hierarchy.org/bishop/",
+    "ll",
+  ]);
   let url = string_combine_multiple([prefix, file_extension_html()]);
   let p = await http_iso_8859_1_cache_parse_parsed(url);
 }
