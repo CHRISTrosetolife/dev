ff0da4f33733646286ba62bfed5ddb15c92dd91d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-16 14:20:11 -0400

    2025-05-16T18:20:09.893Z watch   app_new_from

--- public/src/app_new_from.mjs
+++ public/src/app_new_from.mjs
@@ -1,7 +1,7 @@
 import { app_identifier } from "./app_identifier.mjs";
 import { app_new_generic } from "./app_new_generic.mjs";
 export async function app_new_from(name_new, name_existing) {
-  let name_new_prefixed = app_identifier(name_new);
+  let name_existing_prefixed = app_identifier(name_existing);
   let body_string = "let root = html_style_default_initialize();";
   let v = await app_new_generic(name_new, body_string, false);
   return v;
