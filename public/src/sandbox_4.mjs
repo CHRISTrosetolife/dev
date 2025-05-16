b23b41b05325cf568ef861499c6670db774e3fce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-16 12:48:58 -0400

    2025-05-16T16:48:57.582Z watch   sandbox_4

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -1,5 +1,5 @@
-import { http_iso_8859_1_cache } from "./http_iso_8859_1_cache.mjs";
-import { each_async } from "./each_async.mjs";
+import { catholic_bishops_page } from "./catholic_bishops_page.mjs";
+import { list_map_async } from "./list_map_async.mjs";
 import { html_parse_a_href_starts_with_hrefs } from "./html_parse_a_href_starts_with_hrefs.mjs";
 import { http_iso_8859_1_cache_parse_parsed } from "./http_iso_8859_1_cache_parse_parsed.mjs";
 import { file_extension_html } from "./file_extension_html.mjs";
@@ -13,9 +13,12 @@ export async function sandbox_4() {
     file_extension_html(),
   ]);
   let { root } = await http_iso_8859_1_cache_parse_parsed(url);
-  let list = html_parse_a_href_starts_with_hrefs(root, page_prefix);
-  await each_async(list, async function (item) {
-    await http_iso_8859_1_cache(string_combine_multiple([folder, item]));
+  let pages = html_parse_a_href_starts_with_hrefs(root, page_prefix);
+  await list_map_async(pages, async function (item) {
+    let page_url = string_combine_multiple([folder, item]);
+    let v = await catholic_bishops_page(page_url);
+    let v2 = v;
+    return v2;
   });
-  return list;
+  return pages;
 }
