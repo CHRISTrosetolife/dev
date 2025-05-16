052c8cadbfc68edf9bd4b1545aa79d06da6a7d7f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-16 12:51:25 -0400

    2025-05-16T16:51:24.321Z watch   sandbox_4

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -1,3 +1,4 @@
+import { list_without } from "./list_without.mjs";
 import { catholic_bishops_page } from "./catholic_bishops_page.mjs";
 import { list_map_async } from "./list_map_async.mjs";
 import { html_parse_a_href_starts_with_hrefs } from "./html_parse_a_href_starts_with_hrefs.mjs";
@@ -14,6 +15,7 @@ export async function sandbox_4() {
   let url = string_combine_multiple([folder, page_file_name]);
   let { root } = await http_iso_8859_1_cache_parse_parsed(url);
   let pages = html_parse_a_href_starts_with_hrefs(root, page_prefix);
+  pages = list_without(pages, page_file_name);
   let result = await list_map_async(pages, async function (item) {
     let page_url = string_combine_multiple([folder, item]);
     let v = await catholic_bishops_page(page_url);
