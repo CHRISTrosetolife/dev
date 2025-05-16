d9371be197e69c13efdecfc1709e31d4aee28bd5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-16 12:52:55 -0400

    2025-05-16T16:52:54.728Z watch   catholic_bishops_generate

--- public/src/catholic_bishops_generate.mjs
+++ public/src/catholic_bishops_generate.mjs
@@ -1,16 +1,28 @@
+import { catholic_bishops_page } from "./catholic_bishops_page.mjs";
+import { list_map_async } from "./list_map_async.mjs";
+import { list_without } from "./list_without.mjs";
+import { html_parse_a_href_starts_with_hrefs } from "./html_parse_a_href_starts_with_hrefs.mjs";
+import { http_iso_8859_1_cache_parse_parsed } from "./http_iso_8859_1_cache_parse_parsed.mjs";
+import { file_extension_html } from "./file_extension_html.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { generate_list_fn } from "./generate_list_fn.mjs";
-import { catholic_bishops_letter } from "./catholic_bishops_letter.mjs";
-import { keyboard_keys_sorted } from "./keyboard_keys_sorted.mjs";
-import { each_async } from "./each_async.mjs";
-import { list_adder_multiple_async } from "./list_adder_multiple_async.mjs";
 export async function catholic_bishops_generate() {
   await generate_list_fn(async function () {
-    let v = await list_adder_multiple_async(async function (la) {
-      await each_async(keyboard_keys_sorted(), async function (letter) {
-        let bishops = await catholic_bishops_letter(letter);
-        la(bishops);
-      });
+    let page_prefix = "ll";
+    let page_file_name = string_combine_multiple([
+      page_prefix,
+      file_extension_html(),
+    ]);
+    let folder = "https://www.catholic-hierarchy.org/bishop/";
+    let url = string_combine_multiple([folder, page_file_name]);
+    let { root } = await http_iso_8859_1_cache_parse_parsed(url);
+    let pages = html_parse_a_href_starts_with_hrefs(root, page_prefix);
+    pages = list_without(pages, page_file_name);
+    let result = await list_map_async(pages, async function (item) {
+      let page_url = string_combine_multiple([folder, item]);
+      let v = await catholic_bishops_page(page_url);
+      return v;
     });
-    return v;
+    return result;
   }, catholic_bishops_generate);
 }
--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -6,20 +6,5 @@ import { http_iso_8859_1_cache_parse_parsed } from "./http_iso_8859_1_cache_pars
 import { file_extension_html } from "./file_extension_html.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function sandbox_4() {
-  let page_prefix = "ll";
-  let page_file_name = string_combine_multiple([
-    page_prefix,
-    file_extension_html(),
-  ]);
-  let folder = "https://www.catholic-hierarchy.org/bishop/";
-  let url = string_combine_multiple([folder, page_file_name]);
-  let { root } = await http_iso_8859_1_cache_parse_parsed(url);
-  let pages = html_parse_a_href_starts_with_hrefs(root, page_prefix);
-  pages = list_without(pages, page_file_name);
-  let result = await list_map_async(pages, async function (item) {
-    let page_url = string_combine_multiple([folder, item]);
-    let v = await catholic_bishops_page(page_url);
-    return v;
-  });
-  return result;
+
 }
