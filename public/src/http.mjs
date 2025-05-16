dad22b2d6d02d9a17a0080808cca5cdadfee96b7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-16 09:06:27 -0400

    2025-05-16T13:06:25.417Z watch   http

--- public/src/http.mjs
+++ public/src/http.mjs
@@ -1,8 +1,5 @@
-import { string_is } from "./string_is.mjs";
-import { assert } from "./assert.mjs";
 import { http_response } from "./http_response.mjs";
 export async function http(url) {
-  assert(string_is, [url]);
   let response = await http_response(url);
   let body = await response.text();
   return body;
