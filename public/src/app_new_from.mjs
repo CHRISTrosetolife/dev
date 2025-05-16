abdfed1bcd1c444821000d30d6920d83316912ac
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-16 14:23:39 -0400

    2025-05-16T18:23:39.125Z watch   app_new_from

--- public/src/app_new_from.mjs
+++ public/src/app_new_from.mjs
@@ -1,3 +1,5 @@
+import { list_map } from "./list_map.mjs";
+import { list_join_newline } from "./list_join_newline.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { function_declaration } from "./function_declaration.mjs";
 import { app_identifier } from "./app_identifier.mjs";
@@ -8,9 +10,8 @@ export async function app_new_from(name_existing, name_new) {
   let d = await function_declaration(name_existing_prefixed);
   let body = object_property_get(d, "body");
   let body2 = object_property_get(body, "body");
-  let v2 = d;
-  let v3 = js_unparse(body);
-  return v3;
+  let statements = list_join_newline(list_map(body2, js_unparse));
+  return statements;
   let body_string = "let root = html_style_default_initialize();";
   let v = await app_new_generic(name_new, body_string, false);
   return v;
