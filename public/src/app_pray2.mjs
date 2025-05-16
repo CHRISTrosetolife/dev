d1dd52312bc2f72f6c030a5a20e372178c3f0905
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-16 09:20:02 -0400

    2025-05-16T13:20:01.976Z watch   app_pray2

--- public/src/app_pray2.mjs
+++ public/src/app_pray2.mjs
@@ -1,6 +1,8 @@
 import { catholic_bishops } from "./catholic_bishops.mjs";
 import { app_pray_generic } from "./app_pray_generic.mjs";
+import { list_map } from "./list_map.mjs";
 export async function app_pray2() {
   let bishops = catholic_bishops();
+  list_map(bishops, function () {});
   await app_pray_generic(ps);
 }
