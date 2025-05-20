0b5a0e4da8999f63cbb342fecac039883afd6481
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-20 11:08:35 -0400

    2025-05-20T15:08:35.392Z watch   prayers_sabbwa

--- public/src/prayers_sabbwa.mjs
+++ public/src/prayers_sabbwa.mjs
@@ -1,5 +1,6 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { prayers_sabbw } from "./prayers_sabbw.mjs";
 export function prayers_sabbwa(s) {
-  let v = prayers_sabbw($s, "all ", s);
+  let v = prayers_sabbw(string_combine_multiple(["all ", s]));
   return v;
 }
