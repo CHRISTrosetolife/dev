3cc3f1103787fd5e3a798a6aec2fa69b2f32d3c0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-20 11:32:08 -0400

    2025-05-20T15:32:07.557Z watch   prayers_sabbw

--- public/src/prayers_sabbw.mjs
+++ public/src/prayers_sabbw.mjs
@@ -1,4 +1,5 @@
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function prayers_sabbw(a) {
-  return string_combine_multiple(["Save and bless beyond words ", a]);
+  let v = string_combine_multiple(["Save and ", "bless beyond words ", a]);
+  return v;
 }
