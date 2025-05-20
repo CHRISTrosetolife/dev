ddcb0b20e08240b2c2e3cee3ef9ca2102e4bf12e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-20 11:05:48 -0400

    2025-05-20T15:05:47.659Z watch   app_pray2

--- public/src/app_pray2.mjs
+++ public/src/app_pray2.mjs
@@ -1,3 +1,4 @@
+import { prayers_sabbw } from "./prayers_sabbw.mjs";
 import { list_map_index } from "./list_map_index.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { catholic_bishops } from "./catholic_bishops.mjs";
@@ -8,8 +9,7 @@ export async function app_pray2() {
     let v = string_combine_multiple([
       index + 1,
       ": ",
-      "Save and bless beyond words ",
-      b,
+      prayers_sabbw(b),
       " and his family, friends, fans and the same for any successors and offspring; Lead him and them to always love and obey God perfectly, including speaking truth in love about unity, head coverings, homosexuality, transgender, race, and men and women regarding marriage and ministry;",
     ]);
     return v;
