077c208983bcd6faea76b0bb9156e91db3b45e75
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-21 19:26:25 -0400

    2025-05-21T23:26:24.756Z watch   html_scroll_top

--- public/src/html_scroll_top.mjs
+++ public/src/html_scroll_top.mjs
@@ -1,7 +1,11 @@
+import { log } from "./log.mjs";
 export function html_scroll_top(component) {
   let { element } = component;
   element.scrollTo({
     top: 0,
     behavior: "auto",
+  });
+  log({
+    element,
   });
 }
