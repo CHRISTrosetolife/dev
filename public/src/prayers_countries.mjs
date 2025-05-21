af0836c52116ef0d239125e518fef6774352dfb5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-21 19:39:21 -0400

    2025-05-21T23:39:20.576Z watch   prayers_countries

--- public/src/prayers_countries.mjs
+++ public/src/prayers_countries.mjs
@@ -7,10 +7,13 @@ export function prayers_countries() {
   let c = countries_names();
   let m = list_map(c, string_split_comma_space_reverse);
   let v = list_map(m, function (a) {
-    let v2 = string_combine_multiple([
-      prayers_sabbw(a),
-      " and may the people and their offspring love perfectly",
-    ]);
+    let v2 = prayers_sabbw(
+      string_combine_multiple([
+        "the people of ",
+        a,
+        " and their offspring and may they all love perfectly",
+      ]),
+    );
     return v2;
   });
   return v;
