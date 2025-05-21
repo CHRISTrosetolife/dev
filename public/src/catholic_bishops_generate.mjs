46101b13d0a247432cb0e258ec74f4c442686dba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-21 14:07:54 -0400

    2025-05-21T18:07:53.789Z watch   catholic_bishops_generate

--- public/src/catholic_bishops_generate.mjs
+++ public/src/catholic_bishops_generate.mjs
@@ -1,25 +1,13 @@
+import { catholic_bishops_generate_pages } from "./catholic_bishops_generate_pages.mjs";
 import { catholic_bishops_generate_folder } from "./catholic_bishops_generate_folder.mjs";
 import { list_adder_multiple_async } from "./list_adder_multiple_async.mjs";
 import { each_async } from "./each_async.mjs";
 import { catholic_bishops_page } from "./catholic_bishops_page.mjs";
-import { list_without } from "./list_without.mjs";
-import { html_parse_a_href_starts_with_hrefs } from "./html_parse_a_href_starts_with_hrefs.mjs";
-import { http_iso_8859_1_cache_parse_parsed } from "./http_iso_8859_1_cache_parse_parsed.mjs";
-import { file_extension_html } from "./file_extension_html.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { generate_list_fn } from "./generate_list_fn.mjs";
 export async function catholic_bishops_generate() {
   await generate_list_fn(async function () {
-    let page_prefix = "ll";
-    let page_file_name = string_combine_multiple([
-      page_prefix,
-      file_extension_html(),
-    ]);
-    let folder = catholic_bishops_generate_folder();
-    let url = string_combine_multiple([folder, page_file_name]);
-    let { root } = await http_iso_8859_1_cache_parse_parsed(url);
-    let pages = html_parse_a_href_starts_with_hrefs(root, page_prefix);
-    pages = list_without(pages, page_file_name);
+    let pages = await catholic_bishops_generate_pages();
     let v2 = await list_adder_multiple_async(async function (la) {
       let folder = catholic_bishops_generate_folder();
       await each_async(pages, async function (item) {
---
+++ public/src/catholic_bishops_generate_pages.mjs
@@ -0,0 +1,19 @@
+import { list_without } from "./list_without.mjs";
+import { html_parse_a_href_starts_with_hrefs } from "./html_parse_a_href_starts_with_hrefs.mjs";
+import { http_iso_8859_1_cache_parse_parsed } from "./http_iso_8859_1_cache_parse_parsed.mjs";
+import { catholic_bishops_generate_folder } from "./catholic_bishops_generate_folder.mjs";
+import { file_extension_html } from "./file_extension_html.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export async function catholic_bishops_generate_pages() {
+  let page_prefix = "ll";
+  let page_file_name = string_combine_multiple([
+    page_prefix,
+    file_extension_html(),
+  ]);
+  let folder = catholic_bishops_generate_folder();
+  let url = string_combine_multiple([folder, page_file_name]);
+  let { root } = await http_iso_8859_1_cache_parse_parsed(url);
+  let pages = html_parse_a_href_starts_with_hrefs(root, page_prefix);
+  pages = list_without(pages, page_file_name);
+  return pages;
+}
