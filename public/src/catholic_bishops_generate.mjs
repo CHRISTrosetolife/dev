ccd5b1dc2e7d10ce420ba34cf1b77b11daa73d7f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-21 14:16:10 -0400

    2025-05-21T18:16:10.230Z watch   catholic_bishops_generate

--- public/src/catholic_bishops_generate.mjs
+++ public/src/catholic_bishops_generate.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { string_includes_not } from "./string_includes_not.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { catholic_bishops_generate_pages } from "./catholic_bishops_generate_pages.mjs";
@@ -6,6 +7,7 @@ import { each_async } from "./each_async.mjs";
 import { catholic_bishops_page } from "./catholic_bishops_page.mjs";
 import { generate_list_fn } from "./generate_list_fn.mjs";
 import { list_filter } from "./list_filter.mjs";
+import { list_difference } from "./list_difference.mjs";
 export async function catholic_bishops_generate() {
   await generate_list_fn(async function () {
     let pages = await catholic_bishops_generate_pages();
@@ -15,6 +17,10 @@ export async function catholic_bishops_generate() {
         v = list_filter(v, function (item) {
           let lower = string_case_lower(item);
           let v3 = string_includes_not(lower, "sister");
+          let difference = list_difference(v, v3);
+          log({
+            difference,
+          });
           return v3;
         });
         la(v);
