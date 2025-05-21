5986a13d2e45e7efa8f081e3a3334de8f2267de4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-21 19:22:00 -0400

    2025-05-21T23:21:59.412Z watch   html_scroll_top

--- public/src/html_scroll_top.mjs
+++ public/src/html_scroll_top.mjs
@@ -1,4 +1,3 @@
-import { html_scroll_center_generic } from "./html_scroll_center_generic.mjs";
 export function html_scroll_top(component) {
-  html_scroll_center_generic(component, {});
+  let { element } = component;
 }
