daaf00ce34b99177bafe5f90ab389efbf4e2d2bd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-21 14:13:40 -0400

    2025-05-21T18:13:39.378Z watch   catholic_bishops_generate

--- public/src/catholic_bishops_generate.mjs
+++ public/src/catholic_bishops_generate.mjs
@@ -1,3 +1,4 @@
+import { string_includes_not } from "./string_includes_not.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { catholic_bishops_generate_pages } from "./catholic_bishops_generate_pages.mjs";
 import { list_adder_multiple_async } from "./list_adder_multiple_async.mjs";
@@ -11,9 +12,9 @@ export async function catholic_bishops_generate() {
       await each_async(pages, async function (item) {
         let v = await catholic_bishops_page(item);
         let lower = string_case_lower(v);
-        if (false) {
+        if (string_includes_not(lower, "sister")) {
+          la(v);
         }
-        la(v);
       });
     });
     return v2;
