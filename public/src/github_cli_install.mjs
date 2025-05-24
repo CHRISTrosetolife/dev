ddc0de1658c4ccd28c1a7dcdf8953cd0aeabc890
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-23 20:44:10 -0400

    2025-05-24T00:44:09.512Z watch   github_cli_install

--- public/src/github_cli_install.mjs
+++ public/src/github_cli_install.mjs
@@ -2,5 +2,10 @@ import { command_line } from "./command_line.mjs";
 export async function github_cli_install() {
   try {
     await command_line("gh --version");
-  } catch (e) {}
+  } catch (e) {
+    let v = true;
+    return v;
+  }
+  let v2 = false;
+  return v2;
 }
