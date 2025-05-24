89a7bf14e5b94719beb9bcf664566e7cd188fa18
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-23 21:09:10 -0400

    2025-05-24T01:09:10.366Z watch   command_line_errors

--- public/src/command_line_errors.mjs
+++ public/src/command_line_errors.mjs
@@ -1,11 +1,10 @@
 import { command_line } from "./command_line.mjs";
 export async function command_line_errors(command) {
-  let v;
+  let v = false;
   try {
     await command_line(command);
   } catch (e) {
     v = true;
   }
-  v = false;
   return v;
 }
