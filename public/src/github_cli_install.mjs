1fe70ad22bb384aec6bea2405847c25597900820
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-23 20:47:46 -0400

    2025-05-24T00:47:46.428Z watch   github_cli_install

--- public/src/github_cli_install.mjs
+++ public/src/github_cli_install.mjs
@@ -3,9 +3,10 @@ import { command_line_errors } from "./command_line_errors.mjs";
 export async function github_cli_install() {
   let command = "gh --version";
   if (await command_line_errors(command)) {
-    await command_line(
+    let v2 = await command_line(
       "winget install --id GitHub.cli -e --accept-package-agreements --accept-source-agreements",
     );
+    return v2;
   }
   let v = await command_line(command);
   return v;
