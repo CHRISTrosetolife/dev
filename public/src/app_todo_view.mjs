7a3799baa14d3f558f05e198d9d0ec6216d46287
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-24 14:23:54 -0400

    2025-05-24T18:23:53.521Z watch   app_todo_view

--- public/src/app_todo_view.mjs
+++ public/src/app_todo_view.mjs
@@ -2,7 +2,6 @@ import { html_button } from "./html_button.mjs";
 import { app_todo_list_filtered } from "./app_todo_list_filtered.mjs";
 import { app_todo_items } from "./app_todo_items.mjs";
 import { noop } from "./noop.mjs";
-import { app_todo_completed_value } from "./app_todo_completed_value.mjs";
 import { app_todo_completed_property } from "./app_todo_completed_property.mjs";
 import { app_todo_main } from "./app_todo_main.mjs";
 import { app_todo_not_completed } from "./app_todo_not_completed.mjs";
@@ -11,14 +10,14 @@ export function app_todo_view(context, value) {
   let { root } = context;
   html_clear_scroll_top(root);
   let items = app_todo_items(context);
-  html_button(root, app_todo_not_completed(), async () => {
+  html_button(root, app_todo_not_completed(), async function () {
     app_todo_main(context);
   });
   app_todo_list_filtered(
     context,
     items,
     app_todo_completed_property(),
-    app_todo_completed_value(),
+    value,
     noop,
   );
 }
