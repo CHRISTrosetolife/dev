1f436c9ff745e25b154099f238ff70dc91a9dc27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-24 15:43:34 -0400

    2025-05-24T19:43:33.623Z watch   js_identifiers_names_duplicates

--- public/src/js_identifiers_names_duplicates.mjs
+++ public/src/js_identifiers_names_duplicates.mjs
@@ -1,7 +1,7 @@
+import { list_adder } from "./list_adder.mjs";
 import { js_identifiers_names_generic } from "./js_identifiers_names_generic.mjs";
-import { list_adder_unique } from "./list_adder_unique.mjs";
 export function js_identifiers_names_duplicates(ast) {
-  let la_fn = list_adder_unique;
+  let la_fn = list_adder;
   let v = js_identifiers_names_generic(la_fn, ast);
   return v;
 }
