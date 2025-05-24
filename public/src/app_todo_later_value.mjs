bc514bb232b280062d586b622ac24d7a69395b5c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-24 14:21:27 -0400

    2025-05-24T18:21:26.114Z watch   app_todo_later_value

---
+++ public/src/app_todo_later_value.mjs
@@ -0,0 +1,4 @@
+export function app_todo_later_value() {
+  let v = "⏳️ Later";
+  return v;
+}
--- public/src/app_todo_properties.mjs
+++ public/src/app_todo_properties.mjs
@@ -1,17 +1,23 @@
+import { app_todo_later_value } from "./app_todo_later_value.mjs";
 import { app_todo_completed_value } from "./app_todo_completed_value.mjs";
 import { app_todo_completed_property } from "./app_todo_completed_property.mjs";
 import { app_todo_daily } from "./app_todo_daily.mjs";
 import { app_todo_type } from "./app_todo_type.mjs";
 import { app_todo_not_completed } from "./app_todo_not_completed.mjs";
 export function app_todo_properties() {
-  return [
+  let v = [
     {
       name: app_todo_completed_property(),
-      choices: [app_todo_not_completed(), app_todo_completed_value()],
+      choices: [
+        app_todo_not_completed(),
+        app_todo_completed_value(),
+        app_todo_later_value(),
+      ],
     },
     {
       name: app_todo_type(),
       choices: ["one-time", app_todo_daily()],
     },
   ];
+  return v;
 }
