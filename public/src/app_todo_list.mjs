c0b0e2d7f1c052b18b93d280aba4282c1e329d76
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-24 15:51:20 -0400

    2025-05-24T19:51:19.014Z watch   app_todo_list

--- public/src/app_todo_list.mjs
+++ public/src/app_todo_list.mjs
@@ -1,11 +1,24 @@
+import { app_todo_item_choices } from "./app_todo_item_choices.mjs";
+import { html_hr } from "./html_hr.mjs";
+import { each } from "./each.mjs";
+import { app_todo_properties } from "./app_todo_properties.mjs";
+import { app_todo_list_inner } from "./app_todo_list_inner.mjs";
 import { app_todo_firebase_path_index } from "./app_todo_firebase_path_index.mjs";
-import { app_todo_list_generic } from "./app_todo_list_generic.mjs";
 export function app_todo_list(context, filtered, refresh, items_all) {
-  app_todo_list_generic(
+  app_todo_list_inner(
     context,
     filtered,
     refresh,
-    app_todo_firebase_path_index(),
+    app_todo_firebase_path_index,
+    middle,
     items_all,
   );
+  function middle(context, item) {
+    let properties = app_todo_properties();
+    let { root } = context;
+    each(properties, function (p) {
+      html_hr(root);
+      app_todo_item_choices(context, item, p);
+    });
+  }
 }
