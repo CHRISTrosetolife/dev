6f195ab23a9683a8f61f81b2614242d2495a6a82
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-24 15:43:04 -0400

    2025-05-24T19:43:02.445Z watch   js_identifiers_names

--- public/src/js_identifiers_names.mjs
+++ public/src/js_identifiers_names.mjs
@@ -1,16 +1,7 @@
-import { js_visit_identifiers } from "./js_visit_identifiers.mjs";
+import { js_identifiers_names_generic } from "./js_identifiers_names_generic.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 export function js_identifiers_names(ast) {
   let la_fn = list_adder_unique;
-  return js_identifiers_names_generic(la_fn, ast);
+  let v = js_identifiers_names_generic(la_fn, ast);
+  return v;
 }
-function js_identifiers_names_generic(la_fn, ast) {
-    let v2 = la_fn(function (la) {
-        js_visit_identifiers(ast, function (v) {
-            let { node } = v;
-            la(node.name);
-        });
-    });
-    return v2;
-}
-
