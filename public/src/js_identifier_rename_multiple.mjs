a3990e8f5efd37d4e0d2adf97da7acd033fd9160
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-24 15:39:45 -0400

    2025-05-24T19:39:45.280Z si app_todo_list_generic

--- public/src/js_identifier_rename_multiple.mjs
+++ public/src/js_identifier_rename_multiple.mjs
@@ -2,24 +2,14 @@ import { list_get } from "./list_get.mjs";
 import { js_identifier_rename } from "./js_identifier_rename.mjs";
 import { each_index_only } from "./each_index_only.mjs";
 import { list_size_equal } from "./list_size_equal.mjs";
-import { assert_message } from "./assert_message.mjs";
+import { assert } from "./assert.mjs";
 export function js_identifier_rename_multiple(
   ast,
   identifiers_from,
   identifiers_to,
 ) {
-  assert_message(
-    list_size_equal,
-    [identifiers_from, identifiers_to],
-    function () {
-      let v = {
-        identifiers_from,
-        identifiers_to,
-      };
-      return v;
-    },
-  );
-  each_index_only(identifiers_from, function (index) {
+  assert(list_size_equal, [identifiers_from, identifiers_to]);
+  each_index_only(identifiers_from, (index) => {
     js_identifier_rename(
       ast,
       list_get(identifiers_from, index),
