d71ae9cb3dced9d6535f9a34d134ebcaaafd3b5b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-24 15:49:58 -0400

    2025-05-24T19:49:56.333Z a app_grocery_items_list

--- public/src/app_grocery_items_list.mjs
+++ public/src/app_grocery_items_list.mjs
@@ -1,5 +1,9 @@
+import { app_todo_item_choices } from "./app_todo_item_choices.mjs";
+import { html_hr } from "./html_hr.mjs";
+import { each } from "./each.mjs";
+import { app_todo_properties } from "./app_todo_properties.mjs";
+import { app_todo_list_inner } from "./app_todo_list_inner.mjs";
 import { app_grocery_index_items } from "./app_grocery_index_items.mjs";
-import { app_todo_list_generic } from "./app_todo_list_generic.mjs";
 import { app_grocery_main } from "./app_grocery_main.mjs";
 import { html_button_back_main } from "./html_button_back_main.mjs";
 import { app_todo_item_add } from "./app_todo_item_add.mjs";
@@ -9,9 +13,20 @@ export function app_grocery_items_list(context) {
   html_button_back_main(context, app_grocery_main);
   let index_path = app_grocery_firebase_path_index();
   html_item_add(context, app_grocery_items_list, index_path, on_complete);
-  let refresh = () => app_grocery_items_list(context);
-  let items = app_grocery_index_items(context);$ex    
-  app_todo_list_generic(context, items, refresh, index_path, items);
+  let refresh = function () {
+    let v = app_grocery_items_list(context);
+    return v;
+  };
+  let items = app_grocery_index_items(context);
+  app_todo_list_inner(context, items, refresh, index_path, middle, items);
+  function middle(context, item) {
+    let properties = app_todo_properties();
+    let { root } = context;
+    each(properties, function (p) {
+      html_hr(root);
+      app_todo_item_choices(context, item, p);
+    });
+  }
   function on_complete(value) {
     let items = app_grocery_index_items(context);
     app_todo_item_add(items, value);
