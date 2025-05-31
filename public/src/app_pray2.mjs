e3feb95314fa1b03c58077fb4f948edcee9c9089
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-31 16:20:03 -0400

    2025-05-31T20:20:03.187Z watch   app_pray2

--- public/src/app_pray2.mjs
+++ public/src/app_pray2.mjs
@@ -11,25 +11,16 @@ export async function app_pray2() {
     let mod = index % count;
     let v;
     if (mod === 0) {
-      v = string_combine_multiple([
-        index + 1,
-        ": ",
-        prayers_sabbw(b),
-        " and his family, friends, fans and the same for any successors and offspring; Lead him and them to always love and obey God perfectly, including speaking truth in love about unity, head coverings, homosexuality, transgender, race, and men and women regarding marriage and ministry;",
-      ]);
+      v = string_combine_multiple([index + 1, ": ", prayers_sabbw(b), ", "]);
     } else if (mod === count - 1) {
-      v = string_combine_multiple([
-        index + 1,
-        ": ",
-        prayers_sabbw(b),
-        " and his family, friends, fans and the same for any successors and offspring; Lead him and them to always love and obey God perfectly, including speaking truth in love about unity, head coverings, homosexuality, transgender, race, and men and women regarding marriage and ministry;",
-      ]);
+      v = string_combine_multiple([index + 1, ": ", b, ", "]);
     } else {
       v = string_combine_multiple([
         index + 1,
         ": ",
-        prayers_sabbw(b),
-        " and his family, friends, fans and the same for any successors and offspring; Lead him and them to always love and obey God perfectly, including speaking truth in love about unity, head coverings, homosexuality, transgender, race, and men and women regarding marriage and ministry;",
+        b,
+        ", ",
+        " and their families, friends, fans and the same for any successors and offspring; Lead them to always love and obey God perfectly, including speaking truth in love about unity, head coverings, homosexuality, transgender, race, and men and women regarding marriage and ministry;",
       ]);
     }
     return v;
