c0a2ec43424df20e8c01ac482ac1d4c53953e7f2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-05-31 16:22:08 -0400

    2025-05-31T20:22:07.664Z watch   app_pray2

--- public/src/app_pray2.mjs
+++ public/src/app_pray2.mjs
@@ -13,8 +13,6 @@ export async function app_pray2() {
     if (mod === 0) {
       v = string_combine_multiple([index + 1, ": ", prayers_sabbw(b), ", "]);
     } else if (mod === count - 1) {
-      v = string_combine_multiple([index + 1, ": ", b, ", "]);
-    } else {
       v = string_combine_multiple([
         index + 1,
         ": ",
@@ -22,6 +20,8 @@ export async function app_pray2() {
         ", ",
         " and their families, friends, fans and the same for any successors and offspring; Lead them to always love and obey God perfectly, including speaking truth in love about unity, head coverings, homosexuality, transgender, race, and men and women regarding marriage and ministry;",
       ]);
+    } else {
+      v = string_combine_multiple([index + 1, ": ", b, ", "]);
     }
     return v;
   });
