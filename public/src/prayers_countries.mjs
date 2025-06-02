2d313c29073b6e2d827c5336142694448e3d00d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-02 13:48:52 -0400

    2025-06-02T17:48:51.827Z watch   prayers_countries

--- public/src/prayers_countries.mjs
+++ public/src/prayers_countries.mjs
@@ -1,3 +1,4 @@
+import { prayers_list_generic } from "./prayers_list_generic.mjs";
 import { prayers_sabbw } from "./prayers_sabbw.mjs";
 import { string_split_comma_space_reverse } from "./string_split_comma_space_reverse.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
@@ -7,14 +8,17 @@ export function prayers_countries() {
   let c = countries_names();
   let m = list_map(c, string_split_comma_space_reverse);
   let v = list_map(m, function (a) {
-    let v2 = prayers_sabbw(
-      string_combine_multiple([
-        "the people of ",
-        a,
-        " and their offspring and may they all love perfectly",
-      ]),
-    );
     return v2;
   });
-  return v;
+  let on_first = prayers_sabbw;
+  function on_last(item) {
+    let v3 = string_combine_multiple([
+      "the people of ",
+      item,
+      " and their offspring and may they all love perfectly",
+    ]);
+    return v3;
+  }
+  let v4 = prayers_list_generic(bishops, on_first, on_last);
+  return v4;
 }
