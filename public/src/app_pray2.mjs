fb29548eeab370f587466d52627d2a75127554c5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-02 13:38:49 -0400

    2025-06-02T17:38:48.599Z watch   app_pray2

--- public/src/app_pray2.mjs
+++ public/src/app_pray2.mjs
@@ -1,29 +1,6 @@
-import { list_index_last_is } from "./list_index_last_is.mjs";
-import { mod } from "./mod.mjs";
-import { prayers_sabbw } from "./prayers_sabbw.mjs";
-import { list_map_index } from "./list_map_index.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { catholic_bishops } from "./catholic_bishops.mjs";
+import { catholic_bishops_prayers } from "./catholic_bishops_prayers.mjs";
 import { app_pray_generic } from "./app_pray_generic.mjs";
 export async function app_pray2() {
-  let bishops = catholic_bishops();
-  let on_first = prayers_sabbw;
-  let last_after =
-    " and their families, friends, fans and the same for any successors and offspring; Lead them to always love and obey God perfectly, including speaking truth in love about unity, head coverings, homosexuality, transgender, race, and men and women regarding marriage and ministry;";
-  let ps = list_map_index(bishops, function (b, index) {
-    let count = 10;
-    let mod = index % count;
-    let v;
-    let index_1 = index + 1;
-    let index_1_colon = string_combine_multiple([index_1, ": "]);
-    if (mod === 0) {
-      v = string_combine_multiple([index_1_colon, on_first(b), ", "]);
-    } else if (mod === count - 1 || list_index_last_is(bishops, index)) {
-      v = string_combine_multiple([index_1_colon, b, ", ", last_after]);
-    } else {
-      v = string_combine_multiple([index_1_colon, b, ", "]);
-    }
-    return v;
-  });
+  let ps = catholic_bishops_prayers();
   await app_pray_generic(app_pray2, ps);
 }
---
+++ public/src/catholic_bishops_prayers.mjs
@@ -0,0 +1,28 @@
+import { list_index_last_is } from "./list_index_last_is.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { mod } from "./mod.mjs";
+import { list_map_index } from "./list_map_index.mjs";
+import { prayers_sabbw } from "./prayers_sabbw.mjs";
+import { catholic_bishops } from "./catholic_bishops.mjs";
+export function catholic_bishops_prayers() {
+  let bishops = catholic_bishops();
+  let on_first = prayers_sabbw;
+  let last_after =
+    " and their families, friends, fans and the same for any successors and offspring; Lead them to always love and obey God perfectly, including speaking truth in love about unity, head coverings, homosexuality, transgender, race, and men and women regarding marriage and ministry;";
+  let ps = list_map_index(bishops, function (b, index) {
+    let count = 10;
+    let mod = index % count;
+    let v;
+    let index_1 = index + 1;
+    let index_1_colon = string_combine_multiple([index_1, ": "]);
+    if (mod === 0) {
+      v = string_combine_multiple([index_1_colon, on_first(b), ", "]);
+    } else if (mod === count - 1 || list_index_last_is(bishops, index)) {
+      v = string_combine_multiple([index_1_colon, b, ", ", last_after]);
+    } else {
+      v = string_combine_multiple([index_1_colon, b, ", "]);
+    }
+    return v;
+  });
+  return ps;
+}
