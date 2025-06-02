242ea30fa94e5f5c01073aaed6adae163dc6a0d6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-02 13:34:43 -0400

    2025-06-02T17:34:42.802Z watch   list_last_is

---
+++ public/src/list_index_last_is.mjs
@@ -0,0 +1,4 @@
+import { list_index_last } from "./list_index_last.mjs";
+export function list_index_last_is(index, list) {
+  return index === list_index_last(list);
+}
--- public/src/list_last_is.mjs
+++ public/src/list_last_is.mjs
@@ -1,5 +1,7 @@
-import { list_index_last } from "./list_index_last.mjs";
+import { list_index_last_is } from "./list_index_last_is.mjs";
 import { list_index } from "./list_index.mjs";
 export function list_last_is(list, item) {
-  return list_index(list, item) === list_index_last(list);
+  let index = list_index(list, item);
+  let v = list_index_last_is(index, list);
+  return v;
 }
