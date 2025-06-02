433f70a379456148618beff12bd823b30279027c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-02 14:48:27 -0400

    2025-06-02T18:48:25.523Z watch   catholic_bishops_prayers

--- public/src/catholic_bishops_prayers.mjs
+++ public/src/catholic_bishops_prayers.mjs
@@ -2,15 +2,18 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { prayers_list_generic } from "./prayers_list_generic.mjs";
 import { prayers_sabbw } from "./prayers_sabbw.mjs";
 import { catholic_bishops } from "./catholic_bishops.mjs";
+import { list_map } from "./list_map.mjs";
+import { identity } from "./identity.mjs";
 export function catholic_bishops_prayers() {
   let bishops = catholic_bishops();
-  let on_first = prayers_sabbw;
+  let m = list_map(bishops, prayers_sabbw);
+  let on_first = identity;
   function on_last(item) {
     let v2 =
       " and their families, friends, fans and the same for any successors and offspring; Lead them to always love and obey God perfectly, including speaking truth in love about unity, head coverings, homosexuality, transgender, race, and men and women regarding marriage and ministry;";
     let v3 = string_combine_multiple([item, v2]);
     return v3;
   }
-  let v = prayers_list_generic(bishops, on_first, on_last);
+  let v = prayers_list_generic(m, on_first, on_last);
   return v;
 }
