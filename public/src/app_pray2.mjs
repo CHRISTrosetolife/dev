e4cd156906066cc66234084bc4ea42b7290f4d32
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-02 13:38:16 -0400

    2025-06-02T17:38:15.472Z watch   app_pray2

--- public/src/app_pray2.mjs
+++ public/src/app_pray2.mjs
@@ -8,7 +8,6 @@ import { app_pray_generic } from "./app_pray_generic.mjs";
 export async function app_pray2() {
   let bishops = catholic_bishops();
   let on_first = prayers_sabbw;
-  let index_1_colon = string_combine_multiple([index_1, ": "]);
   let last_after =
     " and their families, friends, fans and the same for any successors and offspring; Lead them to always love and obey God perfectly, including speaking truth in love about unity, head coverings, homosexuality, transgender, race, and men and women regarding marriage and ministry;";
   let ps = list_map_index(bishops, function (b, index) {
@@ -16,6 +15,7 @@ export async function app_pray2() {
     let mod = index % count;
     let v;
     let index_1 = index + 1;
+    let index_1_colon = string_combine_multiple([index_1, ": "]);
     if (mod === 0) {
       v = string_combine_multiple([index_1_colon, on_first(b), ", "]);
     } else if (mod === count - 1 || list_index_last_is(bishops, index)) {
