595df9442e4e8ab62ac3e40b1eadae68d7762f09
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-02 13:37:21 -0400

    2025-06-02T17:37:21.248Z watch   app_pray2

--- public/src/app_pray2.mjs
+++ public/src/app_pray2.mjs
@@ -13,22 +13,18 @@ export async function app_pray2() {
     let mod = index % count;
     let v;
     let index_1 = index + 1;
+    let index_1_colon = string_combine_multiple([index_1, ": "]);
     if (mod === 0) {
-      v = string_combine_multiple([
-        string_combine_multiple([index_1, ": "]),
-        on_first(b),
-        ", ",
-      ]);
+      v = string_combine_multiple([index_1_colon, on_first(b), ", "]);
     } else if (mod === count - 1 || list_index_last_is(bishops, index)) {
       v = string_combine_multiple([
-        index_1,
-        ": ",
+        index_1_colon,
         b,
         ", ",
         " and their families, friends, fans and the same for any successors and offspring; Lead them to always love and obey God perfectly, including speaking truth in love about unity, head coverings, homosexuality, transgender, race, and men and women regarding marriage and ministry;",
       ]);
     } else {
-      v = string_combine_multiple([index_1, ": ", b, ", "]);
+      v = string_combine_multiple([index_1_colon, b, ", "]);
     }
     return v;
   });
