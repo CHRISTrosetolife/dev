04619ccdb1b4b0f1e52740608823498d7b3b3d66
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-04 13:33:17 -0400

    2025-06-04T17:33:16.720Z watch   prayers_countries

--- public/src/prayers_countries.mjs
+++ public/src/prayers_countries.mjs
@@ -14,7 +14,7 @@ export function prayers_countries() {
   function on_last(item) {
     let v3 = string_combine_multiple([
       item,
-      " and their offspring and ancestors and may they all love perfectly",
+      " and their offspring and ancestors and according to Your desire may they all love perfectly",
     ]);
     return v3;
   }
