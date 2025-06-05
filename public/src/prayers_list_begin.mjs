456190e5ac8fcc6e8bd79df596cf496fbc8f6464
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-05 19:55:35 -0400

    2025-06-05T23:55:34.910Z watch   prayers_list_begin

--- public/src/prayers_list_begin.mjs
+++ public/src/prayers_list_begin.mjs
@@ -1,7 +1,7 @@
 export function prayers_list_begin() {
   let v = [
     "Heavenly Father,",
-    "In the name of Jesus:",
+    "In the name of Father, and of the Son and of the Holy Spirit:",
     "Have mercy on me, a sinner;",
   ];
   return v;
