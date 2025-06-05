6389d80921358bc5812e07270084f6217a55cc08
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-05 19:47:45 -0400

    2025-06-05T23:47:44.871Z watch   prayers_music

--- public/src/prayers_music.mjs
+++ public/src/prayers_music.mjs
@@ -1,3 +1,4 @@
+import { prayers_bbw } from "./prayers_bbw.mjs";
 import { prayers_sabbw } from "./prayers_sabbw.mjs";
 import { string_prefix_and } from "./string_prefix_and.mjs";
 import { prayers_list_generic } from "./prayers_list_generic.mjs";
@@ -26,7 +27,7 @@ export function prayers_music() {
     "Vertical Worship",
     "NEEDTOBREATHE",
   ];
-  let v2 = prayers_list_generic(v, string_prefix_and);
+  let v2 = prayers_list_generic(v, prayers_bbw, string_prefix_and);
   let v3 = [prayers_sabbw(":"), v2];
   return v3;
 }
