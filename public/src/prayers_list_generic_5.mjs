556a13228f846980f5971134b049f015c3acfb79
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-05 19:51:14 -0400

    2025-06-05T23:51:14.016Z watch   prayers_list_generic_5

--- public/src/prayers_list_generic_5.mjs
+++ public/src/prayers_list_generic_5.mjs
@@ -1,6 +1,6 @@
 import { prayers_list_generic } from "./prayers_list_generic.mjs";
 export function prayers_list_generic_5(list, on_first, on_last) {
-  let count = 10;
+  let count = 5;
   let v = prayers_list_generic(list, count, on_first, on_last);
   return v;
 }
