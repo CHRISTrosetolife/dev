73ec685d94ae642814d2aad207021106dbc22044
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-06 12:18:09 -0400

    2025-06-06T16:18:06.775Z watch   prayers

--- public/src/prayers.mjs
+++ public/src/prayers.mjs
@@ -99,8 +99,7 @@ export function prayers() {
     "If possible, may all seminary and religious education be free and online",
     "May the voice of every Christian including mine lead all including listeners to Christ",
     prayers_bbw(
-      "all worship, all worship singing voices, my singing voice, my singing voice and my speaking voice",
-      list_join_comma_space([]),
+      "all worship, all worship singing voices, my singing voice, and my speaking voice",
     ),
     prayers_bbw(
       "all Christian songwriting, Christian speaking, Christian writing, and Christian prayer",
