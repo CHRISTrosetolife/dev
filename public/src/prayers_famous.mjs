0006eccc6cd4a0f6fa2a7e198013c1e916dcd9a2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-06 12:21:57 -0400

    2025-06-06T16:21:55.867Z watch   prayers_famous

--- public/src/prayers_famous.mjs
+++ public/src/prayers_famous.mjs
@@ -2,27 +2,12 @@ import { prayers_sabbw } from "./prayers_sabbw.mjs";
 import { list_map } from "./list_map.mjs";
 export function prayers_famous() {
   let v = [
-    "the president and any future presidents",
-    "[the Pope and] any future Popes",
-    "the Ecumenical Patriarch and any future Ecumenical Patriarchs",
-    "Elon and let him be generous",
-    "Zuckerberg and let him be generous",
-    "Bezos and let him be generous",
-    "Altman and let him be generous",
-    "Gates and let him be generous",
-    "Rowling and let her be generous",
-    "Putin and let him be generous",
-    "Larry Page and let him be generous",
-    "Warren Buffet and let him be generous",
-    "Taylor Swift and let her be generous",
-    "Kanye and let him be generous",
-    "MrBeast and let him be generous",
-    "Bieber, let him be generous and thank you for his Christian faith",
+    "the president and any future presidents, [the Pope and] any future Popes, and the Ecumenical Patriarch and any future Ecumenical Patriarchs",
+    "Elon, Zuckerberg, Bezos, Altman, and Gates and let them be generous",
+    "Rowling, Putin, Larry Page, Warren Buffet, Taylor Swift and Kanye and let them be generous",
+    "MrBeast, Michael Jordan, Katy Perry, Joe Rogan and JD Vance and let them be generous",
     "Shaquille O'Neal, let him be generous and thank You for his generosity",
-    "Michael Jordan and let him be generous",
-    "Katy Perry and let her be generous",
-    "Joe Rogan and let him be generous",
-    "JD Vance",
+    "Bieber and let him be generous and thank you for his Christian faith",
   ];
   let v2 = list_map(v, function (e) {
     let v3 = prayers_sabbw(e);
