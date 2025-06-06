7fa2a263d02ece24333ee525776e305693401701
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-06 12:04:21 -0400

    2025-06-06T16:04:16.858Z watch   prayers_unity

---
+++ public/src/prayers_unity.mjs
@@ -0,0 +1,17 @@
+import { prayers_bbw } from "./prayers_bbw.mjs";
+export function prayers_unity() {
+  let v = [
+    "Unite the church",
+    "May the church perfectly agree about God, Jesus, the Holy Spirit and  the Trinity",
+    "May the church perfectly agree about the Bible canon, tradition, head coverings, spiritual gifts and transgender",
+    "May the church perfectly agree about homosexuality, women in leadership, marriage roles, copyrighting the Bible and believer-to-believer lawsuits",
+    "May the church perfectly agree about abortion",
+    "Unite Roman Catholics, Eastern Orthodox, Oriental Orthodox, the Church of the East, Anglicans, Lutherans and Presbyterians with the rest of the church",
+    "Unite Methodists, Pentecostals, Baptists, Assemblies of God, Seventh-Day Adventists, Mormons and Jehovah's Witnesses with the rest of the church",
+    "Unite Quakers, the Amish, Southern Baptists, Northern Baptists and Evangelicals with the rest of the church",
+    "Unite Protestants and Charismatics with the rest of the church",
+    "May all shepherds be in communion with each other",
+    prayers_bbw("every member of the body of Christ"),
+  ];
+  return v;
+}
