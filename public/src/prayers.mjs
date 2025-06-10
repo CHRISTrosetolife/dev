020e1d36162d30ae72c00d2a66662bb4c2eff7d6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-10 18:47:31 -0400

    2025-06-10T22:47:30.985Z watch   prayers

--- public/src/prayers.mjs
+++ public/src/prayers.mjs
@@ -171,7 +171,7 @@ export function prayers() {
     ),
     "If it means anyone is saved, may there be miracles, wonders, signs, healing",
     "May every thought be as if an optimal prayer is prayed",
-    prayers_bbw("the bodies and any tools of elect angels"),
+    prayers_bbw("the bodies and any instruments and tools of elect angels"),
     prayers_bbw("M"),
     "For each challenge God gave Job, if God desires, may I fulfill",
     "Predestine all my remaining choices",
