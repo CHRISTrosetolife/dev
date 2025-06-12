202407f4ce72d25a1ff04cc7e74ab37ec56920f9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-12 13:41:19 -0400

    2025-06-12T17:41:19.158Z watch   prayers

--- public/src/prayers.mjs
+++ public/src/prayers.mjs
@@ -163,14 +163,16 @@ export function prayers() {
       prayers_bbw,
       string_prefix_and,
     ),
-    "If it means anyone is saved, may there be miracles, wonders, signs, healing",
-    "May every thought be as if an optimal prayer is prayed",
-    prayers_bbw("the bodies and any instruments and tools of elect angels"),
-    prayers_bbw("M"),
-    "For each challenge God gave Job, if God desires, may I fulfill",
-    "Predestine all my remaining choices",
-    "May God choose all my remaining choices, not me",
-    "Not only me but anyone who would want this",
+    [
+      "If it means anyone is saved, may there be miracles, wonders, signs, healing",
+      "May every thought be as if an optimal prayer is prayed",
+      prayers_bbw("the bodies and any instruments and tools of elect angels"),
+      prayers_bbw("M"),
+      "For each challenge God gave Job, if God desires, may I fulfill",
+      "Predestine all my remaining choices",
+      "May God choose all my remaining choices, not me",
+      "Not only me but anyone who would want this",
+    ],
   ]);
   let v2 = list_map(v, string_case_upper_first);
   return v2;
