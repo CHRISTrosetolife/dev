689945c7132c0a3554184c7874133bfd405618c7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-12 13:57:17 -0400

    2025-06-12T17:57:16.789Z watch   prayers_sins

--- public/src/prayers_sins.mjs
+++ public/src/prayers_sins.mjs
@@ -1,9 +1,9 @@
 export function prayers_sins() {
   let v = [
-    "Let there be no pride, adultery, murder, stealing, wage theft or rebellion against God",
+    "Let there be no pride, no adultery, no murder, no stealing, no wage theft and no rebellion against God",
     "Let there be no divination, no witchcraft, no suicide and no sin against children",
     "Let there be no sin against babies, including unborn",
-    "Let there be no thievery, no doubt, no jealousy or envy and no wrath or sinful anger",
+    "Let there be no thievery, no doubt, no jealousy nor envy and no wrath nor sinful anger",
     "Let there be no pornography, no rape, no sexual sin and no sexual sin against children",
     "Let there be no fornication, no kidnapping, no hostages, no unnecessary war, no violence, no drugs and no excessive alcohol",
     "Let there be no greed, no idolatry, no racism, no prejudice, and no partiality",
