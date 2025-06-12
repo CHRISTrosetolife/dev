ab9c768711c343e30a1627fe51ce074988f409df
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-12 13:55:54 -0400

    2025-06-12T17:55:53.587Z watch   prayers

--- public/src/prayers.mjs
+++ public/src/prayers.mjs
@@ -1,3 +1,4 @@
+import { prayers_me } from "./prayers_me.mjs";
 import { prayers_needy } from "./prayers_needy.mjs";
 import { prayers_religions } from "./prayers_religions.mjs";
 import { prayers_sanctify } from "./prayers_sanctify.mjs";
@@ -6,9 +7,6 @@ import { prayers_church_leadership } from "./prayers_church_leadership.mjs";
 import { prayers_heal_me } from "./prayers_heal_me.mjs";
 import { prayers_big } from "./prayers_big.mjs";
 import { prayers_love } from "./prayers_love.mjs";
-import { prayers_list_generic_5 } from "./prayers_list_generic_5.mjs";
-import { string_prefix_and } from "./string_prefix_and.mjs";
-import { prayers_bbw } from "./prayers_bbw.mjs";
 import { string_case_upper_first } from "./string_case_upper_first.mjs";
 import { list_map } from "./list_map.mjs";
 import { prayers_countries } from "./prayers_countries.mjs";
@@ -46,28 +44,7 @@ export function prayers() {
     prayers_famous(),
     prayers_countries(),
     prayers_religions(),
-    [
-      "May my muscles grow",
-      prayers_list_generic_5(
-        [
-          "the works of Christians, including mine",
-          "my word of God singing and speaking",
-          "my programming, including speed",
-          "Christian websites, apps and programming, including mine",
-          "my teaching",
-          "my proofs",
-          "my thinking",
-          "my work, including any job search",
-          "any organization or business of mine",
-          "any leadership I have",
-        ],
-        prayers_bbw,
-        string_prefix_and,
-      ),
-      prayers_bbw(
-        "my music, inlcuding songs, composing, improvising and piano",
-      ),
-    ],
+    prayers_me(),
     prayers_needy(),
     prayers_big(),
   ]);
---
+++ public/src/prayers_me.mjs
@@ -0,0 +1,25 @@
+import { string_prefix_and } from "./string_prefix_and.mjs";
+import { prayers_bbw } from "./prayers_bbw.mjs";
+import { prayers_list_generic_5 } from "./prayers_list_generic_5.mjs";
+export function prayers_me() {
+  return [
+    "May my muscles grow",
+    prayers_list_generic_5(
+      [
+        "the works of Christians, including mine",
+        "my word of God singing and speaking",
+        "my programming, including speed",
+        "Christian websites, apps and programming, including mine",
+        "my teaching",
+        "my proofs",
+        "my thinking",
+        "my work, including any job search",
+        "any organization or business of mine",
+        "any leadership I have",
+      ],
+      prayers_bbw,
+      string_prefix_and,
+    ),
+    prayers_bbw("my music, inlcuding songs, composing, improvising and piano"),
+  ];
+}
