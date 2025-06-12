05fdfb4b0b8111523506292fc2ec2e9ad50a6682
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-12 13:44:44 -0400

    2025-06-12T17:44:43.607Z watch   prayers

--- public/src/prayers.mjs
+++ public/src/prayers.mjs
@@ -135,7 +135,7 @@ export function prayers() {
       string_prefix_and,
     ),
     prayers_bbw("my music, inlcuding songs, composing, improvising and piano"),
-    prayers_bbw("Christian websites and programming"),
+    prayers_bbw("Christian websites, apps and programming, including mine"),
     prayers_list_generic_5(
       [
         "orphans",
