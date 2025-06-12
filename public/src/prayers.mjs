7689c27752e2f65a2624326c7cd8f562aa598fe0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-12 13:52:17 -0400

    2025-06-12T17:52:16.792Z watch   prayers

--- public/src/prayers.mjs
+++ public/src/prayers.mjs
@@ -1,3 +1,4 @@
+import { prayers_sanctify } from "./prayers_sanctify.mjs";
 import { prayers_voices } from "./prayers_voices.mjs";
 import { prayers_church_leadership } from "./prayers_church_leadership.mjs";
 import { prayers_heal_me } from "./prayers_heal_me.mjs";
@@ -34,17 +35,7 @@ export function prayers() {
     "May the coming of the day of God be hastened",
     prayers_love(),
     prayers_family(),
-    "Cause all Christians including love and I to love, to eagerly and joyfully do the desire of God, be holy, perfect in mercy, humble, gentle, patient, diligent, to be wise, generous, obedient to Scripture, perfect in prayer, to hope, to have pure hearts, to be faithful in marriage if married, to be faithful in singleness if single, to choose good, including not vowing",
-    "Cause all vows of all Christians including mine to be fulfilled",
-    "Cause love and I to have zero negative emotions and be pure in heart",
-    "Cause my family to willingly listen in parallel to audio bibles",
-    "Maximize the faith of all Christians including love and I",
-    "Cause me to have no sinful or angry tone",
-    "Cause Christian wives, including love, to submit to their husbands, according to the word of God",
-    "Cause Christian husbands, including Jared, to love their wife as Christ loved the church",
-    "Banish all sinful desire in all Christians including me for anyone that You have not given as a spouse",
-    "Let children obey their parents",
-    "Direct all our footsteps and thoughts",
+    prayers_sanctify(),
     "Answer all the prayer requests every Christian including me should be praying and help any others You desire to pray like this",
     "Bless, help and provide for each person who has asked me to pray for them or give materially",
     prayers_heal_me(),
---
+++ public/src/prayers_sanctify.mjs
@@ -0,0 +1,15 @@
+export function prayers_sanctify() {
+  return [
+    "Cause all Christians including love and I to love, to eagerly and joyfully do the desire of God, be holy, perfect in mercy, humble, gentle, patient, diligent, to be wise, generous, obedient to Scripture, perfect in prayer, to hope, to have pure hearts, to be faithful in marriage if married, to be faithful in singleness if single, to choose good, including not vowing",
+    "Cause all vows of all Christians including mine to be fulfilled",
+    "Cause love and I to have zero negative emotions and be pure in heart",
+    "Cause my family to willingly listen in parallel to audio bibles",
+    "Maximize the faith of all Christians including love and I",
+    "Cause me to have no sinful or angry tone",
+    "Cause Christian wives, including love, to submit to their husbands, according to the word of God",
+    "Cause Christian husbands, including Jared, to love their wife as Christ loved the church",
+    "Banish all sinful desire in all Christians including me for anyone that You have not given as a spouse",
+    "Let children obey their parents",
+    "Direct all our footsteps and thoughts",
+  ];
+}
