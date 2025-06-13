c6174cbc4d7cc2c24bc150827a0d93f78728e776
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-13 19:02:43 -0400

    2025-06-13T23:02:42.681Z watch   list_includes_multiple_not

--- public/src/list_includes_multiple_not.mjs
+++ public/src/list_includes_multiple_not.mjs
@@ -1,4 +1,11 @@
-import { list_includes_multiple } from "./list_includes_multiple.mjs";
+import { list_includes } from "./list_includes.mjs";
 export function list_includes_multiple_not(split, items_to_exclude) {
-  return !list_includes_multiple(split, items_to_exclude);
+  for (let item of items_to_exclude) {
+    if (list_includes(list, item)) {
+      let v = false;
+      return v;
+    }
+  }
+  let v2 = true;
+  return v2;
 }
