79d5ead72a14da995f5c42e0e3fd72ce1384e798
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-13 18:57:20 -0400

    2025-06-13T22:57:19.864Z watch   catholic_bishops_generate

--- public/src/catholic_bishops_generate.mjs
+++ public/src/catholic_bishops_generate.mjs
@@ -1,4 +1,3 @@
-import { list_includes_not } from "./list_includes_not.mjs";
 import { string_split_space } from "./string_split_space.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { catholic_bishops_generate_pages } from "./catholic_bishops_generate_pages.mjs";
@@ -7,7 +6,6 @@ import { each_async } from "./each_async.mjs";
 import { catholic_bishops_page } from "./catholic_bishops_page.mjs";
 import { generate_list_fn } from "./generate_list_fn.mjs";
 import { list_filter } from "./list_filter.mjs";
-import { list_difference } from "./list_difference.mjs";
 export async function catholic_bishops_generate() {
   await generate_list_fn(async function () {
     let pages = await catholic_bishops_generate_pages();
@@ -17,10 +15,8 @@ export async function catholic_bishops_generate() {
         let v4 = list_filter(v, function (item) {
           let lower = string_case_lower(item);
           let split = string_split_space(lower);
-          let v3 = list_includes_not(split, "sister");
-          return v3;
+          return split;
         });
-        let difference = list_difference(v, v4);
         la(v4);
       });
     });
