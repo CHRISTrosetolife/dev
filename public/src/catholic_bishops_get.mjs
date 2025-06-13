84bc7df4e9a062561445dd6b390512580c2012dd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-13 19:09:11 -0400

    2025-06-13T23:09:10.462Z watch   catholic_bishops_get

--- public/src/catholic_bishops_get.mjs
+++ public/src/catholic_bishops_get.mjs
@@ -1,13 +1,22 @@
+import { string_split_space } from "./string_split_space.mjs";
+import { string_case_lower } from "./string_case_lower.mjs";
+import { list_filter } from "./list_filter.mjs";
 import { catholic_bishops_page } from "./catholic_bishops_page.mjs";
 import { each_async } from "./each_async.mjs";
 import { list_adder_multiple_async } from "./list_adder_multiple_async.mjs";
 import { catholic_bishops_generate_pages } from "./catholic_bishops_generate_pages.mjs";
-export async function catholic_bishops_get() {
+export async function catholic_bishops_get(include_split) {
   let pages = await catholic_bishops_generate_pages();
   let v2 = await list_adder_multiple_async(async function (la) {
     await each_async(pages, async function (item) {
       let v = await catholic_bishops_page(item);
-      la(v);
+      let v4 = list_filter(v, function (item) {
+        let lower = string_case_lower(item);
+        let split = string_split_space(lower);
+        let v3 = include_split(split);
+        return v3;
+      });
+      la(v4);
     });
   });
   return v2;
