34be2626c7561e3040c060bdab6735a798446bfd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-13 19:05:04 -0400

    2025-06-13T23:05:04.157Z watch   list_excludes

--- public/src/list_excludes.mjs
+++ public/src/list_excludes.mjs
@@ -2,10 +2,10 @@ import { list_includes } from "./list_includes.mjs";
 export function list_excludes(split, items_to_exclude) {
   for (let item of items_to_exclude) {
     if (list_includes(split, item)) {
-      let v = false;
+      let v = true;
       return v;
     }
   }
-  let v2 = true;
+  let v2 = false;
   return v2;
 }
