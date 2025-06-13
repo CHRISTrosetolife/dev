2b998a6010c4eb1502028b39cc4d188913f567f0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-13 19:05:50 -0400

    2025-06-13T23:05:49.703Z r list_includes_multiple list_includes_all

--- public/src/list_includes_all.mjs
+++ public/src/list_includes_all.mjs
@@ -1,5 +1,5 @@
 import { list_includes } from "./list_includes.mjs";
-export function list_includes_multiple(list, items_to_include) {
+export function list_includes_all(list, items_to_include) {
   for (let item of items_to_include) {
     if (!list_includes(list, item)) {
       return false;
