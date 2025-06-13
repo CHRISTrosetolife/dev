6cfc74684654be91d6f6462fd33d398b41b79d3b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-13 19:15:14 -0400

    2025-06-13T23:15:12.459Z watch   app_pray_catholic_bishops_not

--- public/src/app_pray_catholic_bishops_not.mjs
+++ public/src/app_pray_catholic_bishops_not.mjs
@@ -1,9 +1,8 @@
-import { app_pray_catholic_bishops } from "./app_pray_catholic_bishops.mjs";
 import { app_pray_generic } from "./app_pray_generic.mjs";
 import { catholic_bishops_prayers } from "./catholic_bishops_prayers.mjs";
 import { catholic_bishops_not } from "./catholic_bishops_not.mjs";
 export async function app_pray_catholic_bishops_not() {
   let names = catholic_bishops_not();
   let ps = catholic_bishops_prayers(names);
-  await app_pray_generic(app_pray_catholic_bishops, ps);
+  await app_pray_generic(app_pray_catholic_bishops_not, ps);
 }
