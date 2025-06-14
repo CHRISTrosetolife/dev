a43e23c23e44342d5c52a5b21a093d779154edc5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 15:59:00 -0400

    2025-06-14T19:59:00.185Z watch   catholic_bishops_prayers

--- public/src/catholic_bishops_prayers.mjs
+++ public/src/catholic_bishops_prayers.mjs
@@ -1,3 +1,5 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { storage_local_get } from "./storage_local_get.mjs";
 import { prayers_list_generic } from "./prayers_list_generic.mjs";
 import { catholic_bishops_prayers_wrap } from "./catholic_bishops_prayers_wrap.mjs";
 import { list_map } from "./list_map.mjs";
@@ -14,7 +16,11 @@ export function catholic_bishops_prayers(fn, names) {
     let v4 = catholic_bishops_prayers_wrap(fn, "first", n);
     return v4;
   }
-  let count = 10;
-  let v = prayers_list_generic(m, count, on_first, on_last);
+  let v = prayers_list_generic(
+    m,
+    storage_local_get(fn, string_combine_multiple(["group_count"])),
+    on_first,
+    on_last,
+  );
   return v;
 }
