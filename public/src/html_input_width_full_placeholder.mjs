47f30ae83319ac12b21880c0c443ec161577e591
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 15:24:46 -0400

    2025-06-14T19:24:45.988Z watch   html_input_width_full_placeholder

--- public/src/html_input_width_full_placeholder.mjs
+++ public/src/html_input_width_full_placeholder.mjs
@@ -5,15 +5,17 @@ import { html_div_text } from "./html_div_text.mjs";
 import { html_placeholder } from "./html_placeholder.mjs";
 import { html_input_width_full } from "./html_input_width_full.mjs";
 export function html_input_width_full_placeholder(parent, placeholder) {
+  let fn = html_input_width_full;
   let title = html_div_text(parent, placeholder);
   html_style_font_size_default_multiplied(title, 0.8);
   html_style_left(title);
-  let input = html_input_width_full(parent);
+  let input = fn(parent);
   html_placeholder(input, placeholder);
-  return object_merge_strict(
+  let v = object_merge_strict(
     {
       title,
     },
     input,
   );
+  return v;
 }
