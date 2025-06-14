a947904a8d87573da6875cb6e47fcd7eb2edb07a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 15:43:59 -0400

    2025-06-14T19:43:59.176Z watch   app_pray_catholic_bishops

--- public/src/app_pray_catholic_bishops.mjs
+++ public/src/app_pray_catholic_bishops.mjs
@@ -1,8 +1,6 @@
-import { app_pray_catholic_bishops_main } from "./app_pray_catholic_bishops_main.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { app_pray_catholic_bishops_settings } from "./app_pray_catholic_bishops_settings.mjs";
 export async function app_pray_catholic_bishops() {
   let body = html_style_default_initialize();
   app_pray_catholic_bishops_settings(body);
-  await app_pray_catholic_bishops_main(body);
 }
