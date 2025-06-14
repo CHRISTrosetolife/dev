86ae352e6e8bf09f595eac346c7afe6361377062
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 15:21:45 -0400

    2025-06-14T19:21:44.845Z watch   app_pray_catholic_bishops

--- public/src/app_pray_catholic_bishops.mjs
+++ public/src/app_pray_catholic_bishops.mjs
@@ -1,3 +1,4 @@
+import { html_on_input_value } from "./html_on_input_value.mjs";
 import { prayers_sabbw } from "./prayers_sabbw.mjs";
 import { storage_local_initialize } from "./storage_local_initialize.mjs";
 import { html_value_set } from "./html_value_set.mjs";
@@ -10,19 +11,21 @@ import { catholic_bishops } from "./catholic_bishops.mjs";
 import { html_clear_scroll_top } from "./html_clear_scroll_top.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 export async function app_pray_catholic_bishops() {
+  let fn = app_pray_catholic_bishops;
   let root = html_document_body();
   html_button_settings(root, function () {
     html_clear_scroll_top(root);
     html_span_text(root, "Text to display before each name");
     let t = html_textarea_width_full(root);
     let name_each_before = storage_local_initialize(
-      app_pray_catholic_bishops,
+      fn,
       "name_each_before",
       prayers_sabbw(""),
     );
     html_value_set(t, name_each_before);
+    html_on_input_value(t, function (value_new) {});
   });
   let names = catholic_bishops();
   let ps = catholic_bishops_prayers(names);
-  await app_pray_generic(app_pray_catholic_bishops, ps);
+  await app_pray_generic(fn, ps);
 }
