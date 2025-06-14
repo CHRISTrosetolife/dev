cf20e4b1a580b2bfc0a04b2f883d4811b277a86e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 16:11:26 -0400

    2025-06-14T20:11:25.578Z watch   app_pray_catholic_bishops_main

--- public/src/app_pray_catholic_bishops_main.mjs
+++ public/src/app_pray_catholic_bishops_main.mjs
@@ -5,12 +5,17 @@ import { catholic_bishops } from "./catholic_bishops.mjs";
 import { app_pray_catholic_bishops_settings } from "./app_pray_catholic_bishops_settings.mjs";
 import { html_button_settings } from "./html_button_settings.mjs";
 import { app_pray_catholic_bishops } from "./app_pray_catholic_bishops.mjs";
+import { html_p_text } from "./html_p_text.mjs";
 export async function app_pray_catholic_bishops_main(root) {
   html_clear_scroll_top(root);
   let fn = app_pray_catholic_bishops;
   html_button_settings(root, function () {
     app_pray_catholic_bishops_settings(root);
   });
+  html_p_text(
+    root,
+    "Tap or click on a name to highlight it. If you refresh this page, the page will remember the last name you selected",
+  );
   let names = catholic_bishops();
   let ps = catholic_bishops_prayers(fn, names);
   await app_pray_generic(root, fn, ps);
