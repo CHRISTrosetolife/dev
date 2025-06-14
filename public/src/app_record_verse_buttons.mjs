31a91fe1834c914c3e4d09db79a18ba35fbe2f31
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 16:34:57 -0400

    2025-06-14T20:34:57.422Z watch   app_record_verse_buttons

--- public/src/app_record_verse_buttons.mjs
+++ public/src/app_record_verse_buttons.mjs
@@ -1,3 +1,4 @@
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { html_button } from "./html_button.mjs";
 import { html_hr } from "./html_hr.mjs";
 import { app_record_chapter } from "./app_record_chapter.mjs";
@@ -12,10 +13,9 @@ export function app_record_verse_buttons(
   let { root } = context;
   app_record_chapter_buttons(context, book_code, on_verse);
   let chapter_text = bible_book_chapter_text(book_code, chapter);
-  html_button(
-    root,
-    chapter_text,
-    async () => await app_record_chapter(context, book_code, chapter, on_verse),
-  );
+  html_button(root, string_combine_multiple([chapter_text]), async function () {
+    let v = await app_record_chapter(context, book_code, chapter, on_verse);
+    return v;
+  });
   html_hr(root);
 }
