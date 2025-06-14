2ee22850686d66c5aa2182e6baa30097cee85f06
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 16:08:04 -0400

    2025-06-14T20:08:03.833Z watch   html_input_width_full_placeholder_storage_local

--- public/src/html_input_width_full_placeholder_storage_local.mjs
+++ public/src/html_input_width_full_placeholder_storage_local.mjs
@@ -1,7 +1,6 @@
+import { html_input_type_number_on_input } from "./html_input_type_number_on_input.mjs";
 import { html_input_width_full_placeholder } from "./html_input_width_full_placeholder.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
-import { html_on_input_value } from "./html_on_input_value.mjs";
-import { html_value_set } from "./html_value_set.mjs";
 import { storage_local_initialize } from "./storage_local_initialize.mjs";
 export function html_input_width_full_placeholder_storage_local(
   fn,
@@ -12,10 +11,14 @@ export function html_input_width_full_placeholder_storage_local(
 ) {
   let t = html_input_width_full_placeholder(root, placeholder);
   let value_before = storage_local_initialize(fn, key, value_initial);
-  html_value_set(t, value_before);
-  html_on_input_value(t, function (value_new) {
-    storage_local_set(fn, key, value_new);
-  });
+  html_input_type_number_on_input(
+    t,
+    value_before,
+    1,
+    function on_threshold_change(value_new) {
+      storage_local_set(fn, key, value_new);
+    },
+  );
   let v = t;
   return v;
 }
