c648379a30721c5a5de628cb7dafa5ab1dcd4eb1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 15:49:22 -0400

    2025-06-14T19:49:21.588Z watch   app_pray_catholic_bishops_settings

---
+++ public/src/html_textarea_width_full_placeholder_storage_local.mjs
@@ -0,0 +1,19 @@
+import { storage_local_set } from "./storage_local_set.mjs";
+import { html_on_input_value } from "./html_on_input_value.mjs";
+import { html_value_set } from "./html_value_set.mjs";
+import { storage_local_initialize } from "./storage_local_initialize.mjs";
+import { html_textarea_width_full_placeholder } from "./html_textarea_width_full_placeholder.mjs";
+export function html_textarea_width_full_placeholder_storage_local(
+  root,
+  placeholder,
+  fn,
+  key,
+  value_initial,
+) {
+  let t = html_textarea_width_full_placeholder(root, placeholder);
+  let value_before = storage_local_initialize(fn, key, value_initial);
+  html_value_set(t, value_before);
+  html_on_input_value(t, function (value_new) {
+    storage_local_set(fn, key, value_new);
+  });
+}
