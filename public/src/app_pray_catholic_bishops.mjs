2a58090aad6f70a44e597503c64cd44644c2a641
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 15:43:10 -0400

    2025-06-14T19:43:10.417Z watch   app_pray_catholic_bishops

--- public/src/app_pray_catholic_bishops.mjs
+++ public/src/app_pray_catholic_bishops.mjs
@@ -1,16 +1,16 @@
+import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { app_pray_catholic_bishops_settings } from "./app_pray_catholic_bishops_settings.mjs";
 import { app_pray_generic } from "./app_pray_generic.mjs";
 import { html_button_settings } from "./html_button_settings.mjs";
-import { html_document_body } from "./html_document_body.mjs";
 import { catholic_bishops_prayers } from "./catholic_bishops_prayers.mjs";
 import { catholic_bishops } from "./catholic_bishops.mjs";
 export async function app_pray_catholic_bishops() {
+  let body = html_style_default_initialize();
   let fn = app_pray_catholic_bishops;
-  let root = html_document_body();
-  html_button_settings(root, function () {
-    app_pray_catholic_bishops_settings(root);
+  html_button_settings(body, function () {
+    app_pray_catholic_bishops_settings(body);
   });
   let names = catholic_bishops();
   let ps = catholic_bishops_prayers(names);
-  await app_pray_generic(root, fn, ps);
+  await app_pray_generic(body, fn, ps);
 }
