768e9a727fefce20580acc6fd44c889093a1fde4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 15:40:41 -0400

    2025-06-14T19:40:40.452Z pm app_pray_generic app_fn body

--- public/src/app_pray_generic_initialize.mjs
+++ public/src/app_pray_generic_initialize.mjs
@@ -2,5 +2,5 @@ import { app_pray_generic } from "./app_pray_generic.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export async function app_pray_generic_initialize(app_fn, ps) {
   let body = html_style_default_initialize();
-  await app_pray_generic(body, ps, app_fn);
+  await app_pray_generic(app_fn, body, ps);
 }
