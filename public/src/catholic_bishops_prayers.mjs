48924f9b6f7706df2c8713cec10561b790c2d313
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 15:56:17 -0400

    2025-06-14T19:56:16.613Z watch   catholic_bishops_prayers

--- public/src/catholic_bishops_prayers.mjs
+++ public/src/catholic_bishops_prayers.mjs
@@ -1,15 +1,17 @@
 import { catholic_bishops_prayers_wrap } from "./catholic_bishops_prayers_wrap.mjs";
 import { prayers_list_generic_10 } from "./prayers_list_generic_10.mjs";
 import { list_map } from "./list_map.mjs";
-import { identity } from "./identity.mjs";
 export function catholic_bishops_prayers(fn, names) {
   let m = list_map(names, function (n) {
     let v4 = catholic_bishops_prayers_wrap(fn, "each", n);
     return v4;
   });
-  let on_first = identity;
   function on_last(n) {
     let v4 = catholic_bishops_prayers_wrap(fn, "last", n);
+    return v4;
+  }
+  function on_first(n) {
+    let v4 = catholic_bishops_prayers_wrap(fn, "first", n);
     return v4;
   }
   let v = prayers_list_generic_10(m, on_first, on_last);
