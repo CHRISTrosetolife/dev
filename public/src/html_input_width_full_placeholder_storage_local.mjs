522a55b41a506240512ec5cc85c1e7a2a5275723
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 15:59:56 -0400

    2025-06-14T19:59:56.136Z watch   html_input_width_full_placeholder_storage_local

--- public/src/html_input_width_full_placeholder_storage_local.mjs
+++ public/src/html_input_width_full_placeholder_storage_local.mjs
@@ -1,8 +1,8 @@
+import { html_input_width_full_placeholder } from "./html_input_width_full_placeholder.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
 import { html_on_input_value } from "./html_on_input_value.mjs";
 import { html_value_set } from "./html_value_set.mjs";
 import { storage_local_initialize } from "./storage_local_initialize.mjs";
-import { html_textarea_width_full_placeholder } from "./html_textarea_width_full_placeholder.mjs";
 export function html_input_width_full_placeholder_storage_local(
   fn,
   root,
@@ -10,7 +10,7 @@ export function html_input_width_full_placeholder_storage_local(
   placeholder,
   value_initial,
 ) {
-  let t = html_textarea_width_full_placeholder(root, placeholder);
+  let t = html_input_width_full_placeholder(root, placeholder);
   let value_before = storage_local_initialize(fn, key, value_initial);
   html_value_set(t, value_before);
   html_on_input_value(t, function (value_new) {
