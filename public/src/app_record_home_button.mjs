ecef2602f0c8ebb5fd30eb3169e67a2d3bf26e0a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 16:37:48 -0400

    2025-06-14T20:37:48.324Z watch   app_record_home_button

--- public/src/app_record_home_button.mjs
+++ public/src/app_record_home_button.mjs
@@ -2,7 +2,7 @@ import { html_button } from "./html_button.mjs";
 import { app_record_home } from "./app_record_home.mjs";
 export function app_record_home_button(context, on_verse) {
   let { root } = context;
-  html_button(root, "🏠 Home", on_click);
+  html_button(root, "📖 Choose book ", on_click);
   async function on_click() {
     let v = await app_record_home(context, on_verse);
     return v;
