8702b808783141bdf18b6322833958cd73941758
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 15:14:46 -0400

    2025-06-14T19:14:45.469Z watch   app_pray_catholic_bishops

--- public/src/app_pray_catholic_bishops.mjs
+++ public/src/app_pray_catholic_bishops.mjs
@@ -1,3 +1,4 @@
+import { html_textarea_width_full } from "./html_textarea_width_full.mjs";
 import { html_button_settings } from "./html_button_settings.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { app_pray_generic } from "./app_pray_generic.mjs";
@@ -5,7 +6,9 @@ import { catholic_bishops_prayers } from "./catholic_bishops_prayers.mjs";
 import { catholic_bishops } from "./catholic_bishops.mjs";
 export async function app_pray_catholic_bishops() {
   let root = html_document_body();
-  html_button_settings(root, function () {});
+  html_button_settings(root, function () {
+    html_textarea_width_full();
+  });
   let names = catholic_bishops();
   let ps = catholic_bishops_prayers(names);
   await app_pray_generic(app_pray_catholic_bishops, ps);
