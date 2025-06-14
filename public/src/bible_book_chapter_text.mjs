5c739aaf8039de77131d2cc8a50b79a4a07a8470
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 16:38:02 -0400

    2025-06-14T20:38:01.536Z watch   bible_book_name_text

--- public/src/bible_book_chapter_text.mjs
+++ public/src/bible_book_chapter_text.mjs
@@ -2,7 +2,7 @@ import { bible_book_name } from "./bible_book_name.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function bible_book_chapter_text(book_code, chapter) {
   return string_combine_multiple([
-    "📖 chapter ",
+    "📖 Chapter ",
     bible_book_name(book_code),
     " ",
     chapter,
--- public/src/bible_book_name_text.mjs
+++ public/src/bible_book_name_text.mjs
@@ -1,5 +1,6 @@
 import { bible_book_name } from "./bible_book_name.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function bible_book_name_text(book_code) {
-  return string_combine_multiple(["📖 book ", bible_book_name(book_code)]);
+  let v = string_combine_multiple(["📖 Book ", bible_book_name(book_code)]);
+  return v;
 }
