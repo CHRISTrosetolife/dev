39d20b640debd24a539ef6f276ecf10118a4c185
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-14 16:36:59 -0400

    2025-06-14T20:36:59.318Z watch   app_record_home_button

--- public/src/app_record_home_button.mjs
+++ public/src/app_record_home_button.mjs
@@ -2,5 +2,8 @@ import { html_button_home } from "./html_button_home.mjs";
 import { app_record_home } from "./app_record_home.mjs";
 export function app_record_home_button(context, on_verse) {
   let { root } = context;
-  html_button_home(root, async () => await app_record_home(context, on_verse));
+  html_button_home(root, async function () {
+    let v = await app_record_home(context, on_verse);
+    return v;
+  });
 }
