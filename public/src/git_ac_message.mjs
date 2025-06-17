ea3b9afeadcd373080d138eff86c8eea4d67a8c1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 13:20:45 -0400

    2025-06-17T17:20:44.660Z watch   git_ac_message

--- public/src/git_ac_message.mjs
+++ public/src/git_ac_message.mjs
@@ -1,5 +1,5 @@
+import { command_line_git_multiple_options } from "./command_line_git_multiple_options.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { command_line_git_multiple } from "./command_line_git_multiple.mjs";
 import { git_ac_message_commit } from "./git_ac_message_commit.mjs";
 import { git_ac_message_add } from "./git_ac_message_add.mjs";
 import { date_string_iso } from "./date_string_iso.mjs";
@@ -15,6 +15,7 @@ export async function git_ac_message(commit_message) {
       '"',
     ]),
   ];
-  let v = await command_line_git_multiple(commands);
+  let options = {};
+  let v = await command_line_git_multiple_options(commands, options);
   return v;
 }
