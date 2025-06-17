a597f61909494dd36a10a9b4493393cefcb2473e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 16:43:52 -0400

    2025-06-17T20:43:52.093Z watch   storage_local_toggle

--- public/src/storage_local_toggle.mjs
+++ public/src/storage_local_toggle.mjs
@@ -4,4 +4,6 @@ export function storage_local_toggle(fn, name) {
   let value = storage_local_get(fn, name);
   value = !value;
   storage_local_set(fn, name, value);
+  let v = value;
+  return v;
 }
