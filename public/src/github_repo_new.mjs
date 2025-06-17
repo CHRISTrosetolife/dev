1b28aefb20202194f6916eb44626a8093292c216
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 13:04:48 -0400

    2025-06-17T17:04:47.975Z watch   github_repo_new

--- public/src/github_repo_new.mjs
+++ public/src/github_repo_new.mjs
@@ -1,3 +1,4 @@
+import { folder_new } from "./folder_new.mjs";
 import { assert_not_async } from "./assert_not_async.mjs";
 import { file_exists } from "./file_exists.mjs";
 import { folder_previous } from "./folder_previous.mjs";
@@ -5,6 +6,7 @@ import { path_join } from "./path_join.mjs";
 export async function github_repo_new(name) {
   let path = path_join([folder_previous(), name]);
   await assert_not_async(file_exists, [path]);
+  await folder_new(path);
   let v = path;
   return v;
 }
