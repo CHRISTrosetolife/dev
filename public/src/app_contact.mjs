545d46bd84f4ce5e589cfda3a4ace1f802a4b1ef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 17:00:33 -0400

    2025-06-17T21:00:32.304Z watch   app_contact

--- public/src/app_contact.mjs
+++ public/src/app_contact.mjs
@@ -1,14 +1,16 @@
+import { html_rows_set } from "./html_rows_set.mjs";
 import { html_textarea_width_full_placeholder_storage_local } from "./html_textarea_width_full_placeholder_storage_local.mjs";
 import { app_contact_instructions } from "./app_contact_instructions.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_contact() {
   let root = html_style_default_initialize();
   app_contact_instructions(root);
-  html_textarea_width_full_placeholder_storage_local(
+  let t = html_textarea_width_full_placeholder_storage_local(
     app_contact,
     root,
     "message",
     "Enter the message you want to send to me:",
     "",
   );
+  html_rows_set(t, 8);
 }
