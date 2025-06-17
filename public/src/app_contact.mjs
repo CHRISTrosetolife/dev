830edd51960c4b4a93e11f5a54b728985cd7a27c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 16:58:30 -0400

    2025-06-17T20:58:29.688Z watch   app_contact

--- public/src/app_contact.mjs
+++ public/src/app_contact.mjs
@@ -1,6 +1,8 @@
+import { html_textarea_width_full_placeholder_storage_local } from "./html_textarea_width_full_placeholder_storage_local.mjs";
 import { app_contact_instructions } from "./app_contact_instructions.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_contact() {
   let root = html_style_default_initialize();
   app_contact_instructions(root);
+  html_textarea_width_full_placeholder_storage_local();
 }
