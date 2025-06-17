2338a087251e595b247dce870b2aaabc276814f2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 12:53:58 -0400

    2025-06-17T16:53:58.070Z watch   github_repo_new

--- public/src/github_repo_new.mjs
+++ public/src/github_repo_new.mjs
@@ -1,3 +1,4 @@
+import { assert_async } from "./assert_async.mjs";
 import { file_exists } from "./file_exists.mjs";
 import { folder_previous } from "./folder_previous.mjs";
 import { path_join } from "./path_join.mjs";
@@ -5,4 +6,5 @@ export async function github_repo_new(name) {
   let path = path_join([folder_previous(), name]);
   if (await file_exists(path)) {
   }
+  await assert_async();
 }
