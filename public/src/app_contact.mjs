40fafddb67ba093e322be0e2515f9640a10c3daf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 17:08:18 -0400

    2025-06-17T21:08:18.136Z watch   app_contact

--- public/src/app_contact.mjs
+++ public/src/app_contact.mjs
@@ -26,7 +26,7 @@ export function app_contact() {
     if (value === "hi") {
       html_inner_set(response, app_share_verse_refresh_greet());
       html_value_set(t, "");
-      html_scroll_center();
+      html_scroll_center(response);
     }
   });
   response = html_p(root);
