e048971704fb346c7d5200e447f1daec8fe3bf6f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 16:30:22 -0400

    2025-06-17T20:30:22.243Z watch   app_contact

--- public/src/app_contact.mjs
+++ public/src/app_contact.mjs
@@ -12,8 +12,8 @@ export function app_contact() {
     "If my computer program cannot answer your message, then your message will be sent to me and Lord-willing I will answer later",
     "Refresh the page to see if I have answered",
   ]);
-  html_style_display_none(instructions);
   html_button(root, "Hide instructions", function () {
+    html_style_display_none(instructions);
     html_style_hidden(instructions);
   });
 }
