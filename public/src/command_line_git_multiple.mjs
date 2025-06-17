89bfdcf629e5efb47bf44ff265f776995eba2522
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 13:19:37 -0400

    2025-06-17T17:19:36.910Z watch   command_line_git_multiple

--- public/src/command_line_git_multiple.mjs
+++ public/src/command_line_git_multiple.mjs
@@ -1,9 +1,10 @@
 import { command_line_git } from "./command_line_git.mjs";
 import { list_map_async } from "./list_map_async.mjs";
 export async function command_line_git_multiple(commands) {
-  let result = await list_map_async(
-    commands,
-    async (command) => await command_line_git(command),
-  );
+  let options = {};
+  let result = await list_map_async(commands, async function (command) {
+    let v = await command_line_git(command);
+    return v;
+  });
   return result;
 }
