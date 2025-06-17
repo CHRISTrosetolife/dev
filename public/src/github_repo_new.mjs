00b39b7753e21d7a531f764dc8d41805c742238f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 12:52:51 -0400

    2025-06-17T16:52:51.240Z watch   github_repo_new

--- public/src/github_repo_new.mjs
+++ public/src/github_repo_new.mjs
@@ -2,4 +2,6 @@ import { folder_previous } from "./folder_previous.mjs";
 import { path_join } from "./path_join.mjs";
 export function github_repo_new(name) {
   let path = path_join([folder_previous(), name]);
+  if (false) {
+  }
 }
