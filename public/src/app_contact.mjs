4ef0ffd9cdd834141192645393efb83c8e140237
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 16:28:00 -0400

    2025-06-17T20:27:59.702Z watch   app_contact

--- public/src/app_contact.mjs
+++ public/src/app_contact.mjs
@@ -1,9 +1,10 @@
+import { html_p_text_multiple } from "./html_p_text_multiple.mjs";
 import { html_style_hidden } from "./html_style_hidden.mjs";
 import { html_button } from "./html_button.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 export function app_contact() {
   let root = html_style_default_initialize();
-  let instructions = html_p_text_mutliple(root, [
+  let instructions = html_p_text_multiple(root, [
     "This website will let you contact me",
     "I write computer programs",
     "First my computer program will try to answer your message for me",
