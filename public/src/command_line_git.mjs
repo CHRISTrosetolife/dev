9289d390273b458bb894ab038b792c62fdb68fd4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 13:07:58 -0400

    2025-06-17T17:07:58.288Z watch   command_line_git

--- public/src/command_line_git.mjs
+++ public/src/command_line_git.mjs
@@ -1,7 +1,7 @@
+import { command_line_exec_generic } from "./command_line_exec_generic.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { command_line_exec } from "./command_line_exec.mjs";
 export async function command_line_git(args) {
   let command = string_combine_multiple(["git ", args]);
-  let v = await command_line_exec(command);
+  let v = await command_line_exec_generic(command, {});
   return v;
 }
