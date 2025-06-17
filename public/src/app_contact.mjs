7ae5e2ac5bdd8eaf3c46296a685ada0ce3ca5b1f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 16:24:20 -0400

    2025-06-17T20:24:19.919Z watch   app_contact

--- public/src/app_contact.mjs
+++ public/src/app_contact.mjs
@@ -2,7 +2,7 @@ import { html_style_default_initialize } from "./html_style_default_initialize.m
 export function app_contact() {
   let root = html_style_default_initialize();
   html_p_text_mutliple(root, [
-    "This app will let you contact me",
+    "This website will let you contact me",
     "I write computer programs",
     "First my computer program will try to answer your message for me",
     "If my computer program cannot answer your message, then your message will be sent to me and Lord-willing I will answer later",
