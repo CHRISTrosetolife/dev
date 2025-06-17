39af77b0535ab98c6c78a6dc78f6d44f0c5b638f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 16:28:54 -0400

    2025-06-17T20:28:54.019Z watch   html_p_text_multiple

--- public/src/html_p_text_multiple.mjs
+++ public/src/html_p_text_multiple.mjs
@@ -1,7 +1,11 @@
 import { each } from "./each.mjs";
+import { html_p } from "./html_p.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 export function html_p_text_multiple(parent, paragraphs) {
+  let container = html_p(parent);
   each(paragraphs, function lambda_each(p) {
-    html_p_text(parent, p);
+    html_p_text(container, p);
   });
+  let v = container;
+  return v;
 }
