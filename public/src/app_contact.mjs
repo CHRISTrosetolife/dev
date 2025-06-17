11402460ddea60318f840b61dcd94da23959c9b1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-17 17:02:21 -0400

    2025-06-17T21:02:20.936Z watch   app_contact

--- public/src/app_contact.mjs
+++ public/src/app_contact.mjs
@@ -2,6 +2,7 @@ import { html_rows_set } from "./html_rows_set.mjs";
 import { html_textarea_width_full_placeholder_storage_local } from "./html_textarea_width_full_placeholder_storage_local.mjs";
 import { app_contact_instructions } from "./app_contact_instructions.mjs";
 import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
+import { html_button } from "./html_button.mjs";
 export function app_contact() {
   let root = html_style_default_initialize();
   app_contact_instructions(root);
@@ -13,4 +14,5 @@ export function app_contact() {
     "",
   );
   html_rows_set(t, 8);
+  html_button(root, "Send message to me", function () {});
 }
