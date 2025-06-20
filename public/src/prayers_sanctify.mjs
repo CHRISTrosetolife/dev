151d9392c6f700c5ea0f25ca0992ac6d578b93f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-20 17:56:10 -0400

    2025-06-20T21:56:09.899Z watch   prayers_sanctify

--- public/src/prayers_sanctify.mjs
+++ public/src/prayers_sanctify.mjs
@@ -2,7 +2,7 @@ import { prayers_sins } from "./prayers_sins.mjs";
 export function prayers_sanctify() {
   let v = [
     "Cause all Christians including love and I to love, to eagerly and joyfully do the desire of God, be holy, perfect in mercy, humble, gentle, patient, diligent, to be wise, generous, obedient to Scripture, perfect in prayer, to hope, to have pure hearts, to be faithful in marriage if married, to be faithful in singleness if single, to choose good, including not vowing",
-    "Let no Christian fall into judgment because of their vow",
+    "Let no Christian fall into judgment because of any of their vows",
     "Cause love and I to have zero negative emotions and be pure in heart",
     "Cause my family to willingly listen in parallel to audio bibles",
     "Maximize the faith of all Christians including love and I",
