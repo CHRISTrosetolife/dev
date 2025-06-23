2cec6509a4b1dfa86c64d1ba5c05f7d8783b1428
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-23 17:12:05 -0400

    2025-06-23T21:12:04.807Z watch   app_contact_respond

--- public/src/app_contact_respond.mjs
+++ public/src/app_contact_respond.mjs
@@ -1,4 +1,21 @@
+import { html_scroll_center } from "./html_scroll_center.mjs";
+import { html_value_set } from "./html_value_set.mjs";
+import { html_style_bold } from "./html_style_bold.mjs";
+import { html_p_text } from "./html_p_text.mjs";
 import { app_share_verse_refresh_greet } from "./app_share_verse_refresh_greet.mjs";
 export function app_contact_respond(value) {
-  return app_share_verse_refresh_greet();
+  if (value === "hi") {
+    if (false) {
+      html_p_text(response, "💻 Computer program answered for me:");
+    }
+    let output = app_contact_respond(value);
+    let r = html_p_text(response, output);
+    if (false) {
+      html_style_bold(r);
+    }
+    html_value_set(t, "");
+    html_scroll_center(response);
+  }
+  let v = app_share_verse_refresh_greet();
+  return v;
 }
