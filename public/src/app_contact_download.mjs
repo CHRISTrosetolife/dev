cf4bba96f370232a56246c190b50167e83947dad
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-23 17:06:13 -0400

    2025-06-23T21:06:10.957Z watch   app_contact_download

--- public/src/app_contact_download.mjs
+++ public/src/app_contact_download.mjs
@@ -1,6 +1,6 @@
-import { app_contact_firebase_folder_combine } from "./app_contact_firebase_folder_combine.mjs";
+import { app_contact_firebase_folder_prefix } from "./app_contact_firebase_folder_prefix.mjs";
 import { storage_files_download } from "./storage_files_download.mjs";
 export async function app_contact_download() {
-  let prefix = app_contact_firebase_folder_combine("");
+  let prefix = app_contact_firebase_folder_prefix();
   await storage_files_download();
 }
---
+++ public/src/app_contact_firebase_folder_prefix.mjs
@@ -0,0 +1,4 @@
+import { app_contact_firebase_folder_combine } from "./app_contact_firebase_folder_combine.mjs";
+export function app_contact_firebase_folder_prefix() {
+  return app_contact_firebase_folder_combine("");
+}
