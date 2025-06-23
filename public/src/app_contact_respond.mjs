b5a14f62a048bbd05503859123bbc4c4d7b38311
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-23 17:39:08 -0400

    2025-06-23T21:39:07.786Z watch   app_contact_respond

--- public/src/app_contact_respond.mjs
+++ public/src/app_contact_respond.mjs
@@ -2,7 +2,7 @@ import { list_includes } from "./list_includes.mjs";
 import { app_share_verse_refresh_greet } from "./app_share_verse_refresh_greet.mjs";
 export function app_contact_respond(input) {
   let output = null;
-  if (list_includes(["hi"], input)) {
+  if (list_includes(["hi", "hii"], input)) {
     output = app_share_verse_refresh_greet();
   }
   return output;
