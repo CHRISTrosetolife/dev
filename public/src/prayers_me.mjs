19f56a5d423e0ac71e5085df91bd8602c1c6efc9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-23 13:33:47 -0400

    2025-06-23T17:33:46.512Z watch   prayers

--- public/src/prayers_me.mjs
+++ public/src/prayers_me.mjs
@@ -4,6 +4,9 @@ import { prayers_bbw } from "./prayers_bbw.mjs";
 import { prayers_list_generic_5 } from "./prayers_list_generic_5.mjs";
 export function prayers_me() {
   let v = [
+      "If it means not sinning, take anything away, do anything",
+      "Cause me to never be hospitalized again, never be in therapy again and desire your desire for my lifespan",
+      "Cause me to either speak perfectly or be silent",
     prayers_heal_me(),
     "May my muscles grow",
     prayers_list_generic_5(
