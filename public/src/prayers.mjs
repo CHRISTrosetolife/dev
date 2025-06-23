e525bd1dd4dd933071df5327338a9dd93b79b972
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-23 13:36:22 -0400

    2025-06-23T17:36:21.969Z watch   prayers

--- public/src/prayers.mjs
+++ public/src/prayers.mjs
@@ -1,3 +1,4 @@
+import { prayers_intro } from "./prayers_intro.mjs";
 import { prayers_orphans_widows_unstained_world } from "./prayers_orphans_widows_unstained_world.mjs";
 import { prayers_religions } from "./prayers_religions.mjs";
 import { prayers_big } from "./prayers_big.mjs";
@@ -10,12 +11,7 @@ import { prayers_family } from "./prayers_family.mjs";
 import { list_denest } from "./list_denest.mjs";
 export function prayers() {
   let v = list_denest([
-    [
-      "Cause me to perfectly and maximally glorify and obey the Father, Jesus and the Holy Spirit",
-      "May the words of my mouth and the meditation of my heart be pleasing in your sight, O Lord, my Rock and my Redeemer",
-      "May I fulfill my ministry",
-      "Thank You",
-    ],
+    prayers_intro(),
     prayers_love(),
     prayers_family(),
     prayers_countries(),
---
+++ public/src/prayers_intro.mjs
@@ -0,0 +1,9 @@
+export function prayers_intro() {
+  let v = [
+    "Cause me to perfectly and maximally glorify and obey the Father, Jesus and the Holy Spirit",
+    "May the words of my mouth and the meditation of my heart be pleasing in your sight, O Lord, my Rock and my Redeemer",
+    "May I fulfill my ministry",
+    "Thank You",
+  ];
+  return v;
+}
