1dad7b95d188be89aa309843c90c4a620e766bfb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-23 17:05:50 -0400

    2025-06-23T21:05:49.597Z watch   app_contact_download

--- public/src/app_contact_download.mjs
+++ public/src/app_contact_download.mjs
@@ -1,4 +1,6 @@
+import { app_contact_firebase_folder_combine } from "./app_contact_firebase_folder_combine.mjs";
 import { storage_files_download } from "./storage_files_download.mjs";
 export async function app_contact_download() {
+  app_contact_firebase_folder_combine("");
   await storage_files_download();
 }
