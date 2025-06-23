023724dd34127e2ac4657e97737ce40cd69ccf96
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-23 17:08:43 -0400

    2025-06-23T21:08:42.469Z watch   storage_files_download

--- public/src/storage_files_download.mjs
+++ public/src/storage_files_download.mjs
@@ -1,9 +1,10 @@
+import { list_map_async } from "./list_map_async.mjs";
 import { storage_file_download } from "./storage_file_download.mjs";
-import { each_async } from "./each_async.mjs";
 import { storage_files } from "./storage_files.mjs";
 export async function storage_files_download(prefix) {
   let files = await storage_files(prefix);
-  await each_async(files, async (firebase_file) => {
+  let v = await list_map_async(files, async function (firebase_file) {
     await storage_file_download(firebase_file);
   });
+  return v;
 }
