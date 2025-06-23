c43ac33f5aebf7949657249e5c22f20a94920915
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-23 17:14:28 -0400

    2025-06-23T21:14:28.282Z watch   app_contact_download

--- public/src/app_contact_download.mjs
+++ public/src/app_contact_download.mjs
@@ -1,6 +1,9 @@
+import { list_wait } from "./list_wait.mjs";
 import { app_contact_firebase_folder_prefix } from "./app_contact_firebase_folder_prefix.mjs";
+import { file_read_json } from "./file_read_json.mjs";
 import { storage_files_download } from "./storage_files_download.mjs";
 export async function app_contact_download() {
   let prefix = app_contact_firebase_folder_prefix();
-  let files = await storage_files_download(prefix);
+  let file_paths = await storage_files_download(prefix);
+  let read = await list_wait(file_paths, file_read_json);
 }
