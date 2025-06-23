86584afbffb19c9fc74b9c455cb59a033ce0fb53
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-23 18:14:52 -0400

    2025-06-23T22:14:52.036Z watch   app_contact

--- public/src/app_contact.mjs
+++ public/src/app_contact.mjs
@@ -1,4 +1,6 @@
+import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
 import { app_load } from "./app_load.mjs";
 export async function app_contact() {
+  html_style_default_initialize();
   await app_load("contact_main");
 }
