3d0e54e29ac8f712e5970cfee89e93ef15d3469c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-23 17:05:42 -0400

    2025-06-23T21:05:41.494Z watch   app_contact_download

--- public/src/app_contact_download.mjs
+++ public/src/app_contact_download.mjs
@@ -1,1 +1,4 @@
-export function app_contact_download() {}
+import { storage_files_download } from "./storage_files_download.mjs";
+export async function app_contact_download() {
+  await storage_files_download();
+}
