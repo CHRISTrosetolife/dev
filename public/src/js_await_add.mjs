f6b3dd4f9212cdcce17af8228305e586baaf651c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 13:48:05 -0400

    2025-06-24T17:48:04.210Z watch   js_await_add

--- public/src/js_await_add.mjs
+++ public/src/js_await_add.mjs
@@ -1,3 +1,4 @@
+import { data_functions_async_is } from "./data_functions_async_is.mjs";
 import { data_importing } from "./data_importing.mjs";
 import { functions_data_get_list } from "./functions_data_get_list.mjs";
 import { js_imports_existing_names } from "./js_imports_existing_names.mjs";
@@ -11,9 +12,7 @@ import { js_parent_replace } from "./js_parent_replace.mjs";
 import { data_functions } from "./data_functions.mjs";
 import { list_before } from "./list_before.mjs";
 import { each_reverse } from "./each_reverse.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { js_visit_node } from "./js_visit_node.mjs";
-import { object_property_exists } from "./object_property_exists.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { equal } from "./equal.mjs";
 import { list_add } from "./list_add.mjs";
@@ -30,39 +29,35 @@ export async function js_await_add(ast) {
     let { callee } = node;
     if (callee.type === "Identifier") {
       let { name } = callee;
-      if (object_property_exists(functions, name)) {
-        let f = object_property_get(functions, name);
-        let { async } = f;
-        if (async) {
-          let { parent } = v;
-          let { type: parent_type } = parent;
-          if (parent_type !== "AwaitExpression") {
-            let a = js_expression_await(node);
-            js_parent_replace(v, node, a);
-            let { stack } = v;
-            let found = false;
-            each_reverse(stack, function (s) {
-              if (found) {
-                return;
+      if (data_functions_async_is(functions, name)) {
+        let { parent } = v;
+        let { type: parent_type } = parent;
+        if (parent_type !== "AwaitExpression") {
+          let a = js_expression_await(node);
+          js_parent_replace(v, node, a);
+          let { stack } = v;
+          let found = false;
+          each_reverse(stack, function (s) {
+            if (found) {
+              return;
+            }
+            let { type } = s;
+            let asyncable;
+            if (equal(type, "BlockStatement")) {
+              let after = list_before(stack, s);
+              let { type: after_type } = after;
+              if (js_function_types_is(after_type)) {
+                asyncable = after;
               }
-              let { type } = s;
-              let asyncable;
-              if (equal(type, "BlockStatement")) {
-                let after = list_before(stack, s);
-                let { type: after_type } = after;
-                if (js_function_types_is(after_type)) {
-                  asyncable = after;
-                }
-              }
-              if (type === "ArrowFunctionExpression") {
-                asyncable = s;
-              }
-              if (undefined_not_is(asyncable)) {
-                object_property_set(asyncable, "async", true);
-                found = true;
-              }
-            });
-          }
+            }
+            if (type === "ArrowFunctionExpression") {
+              asyncable = s;
+            }
+            if (undefined_not_is(asyncable)) {
+              object_property_set(asyncable, "async", true);
+              found = true;
+            }
+          });
         }
       }
     }
