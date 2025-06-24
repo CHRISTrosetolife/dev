1db469ad8d5d248f2a6e76f764d3520d662f0523
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 13:07:19 -0400

    2025-06-24T17:07:19.032Z watch   data_identifiers

--- public/src/data_identifiers.mjs
+++ public/src/data_identifiers.mjs
@@ -1,7 +1,7 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { data_path } from "./data_path.mjs";
 import { file_read_json } from "./file_read_json.mjs";
 export async function data_identifiers() {
   let data = await file_read_json(data_path());
-  let { identifiers } = data;
-  return identifiers;
+  let identifiers = object_property_get(data, "identifiers");
 }
