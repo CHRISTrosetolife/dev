b038be9ac58c4e78f4835aece089ad613178e7fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 13:08:26 -0400

    2025-06-24T17:08:24.680Z watch   data_get

---
+++ public/src/data_get.mjs
@@ -0,0 +1,9 @@
+import { object_property_get } from "./object_property_get.mjs";
+import { data_path } from "./data_path.mjs";
+import { file_read_json } from "./file_read_json.mjs";
+export async function data_get(property_name) {
+  let data = await file_read_json(data_path());
+  let identifiers = object_property_get(data, property_name);
+  let v = identifiers;
+  return v;
+}
