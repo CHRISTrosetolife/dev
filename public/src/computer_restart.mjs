42f730e634347f5dfb9aeddcf02a493d27924745
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 14:53:06 -0400

    2025-06-24T18:53:05.390Z watch   computer_restart

--- public/src/computer_restart.mjs
+++ public/src/computer_restart.mjs
@@ -1,5 +1,4 @@
 import { computer_shutdown_generic } from "./computer_shutdown_generic.mjs";
 export async function computer_restart() {
-  let flag = "r";
-  await computer_shutdown_generic(flag);
+  await computer_shutdown_generic("r");
 }
