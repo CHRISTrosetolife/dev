57474e871d9583e8ccd5dc3a3ad2ab29013f7569
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 14:34:28 -0400

    2025-06-24T18:34:25.638Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,1 +1,9 @@
-export async function sandbox_5() {}
+import { log } from "./log.mjs";
+import * as vscode from "vscode";
+export async function sandbox_5() {
+  let openFiles = vscode.window.visibleTextEditors.map(function (editor) {
+    let v = editor.document.fileName;
+    return v;
+  });
+  console.log(openFiles);
+}
