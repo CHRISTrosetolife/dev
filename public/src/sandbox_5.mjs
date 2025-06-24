e8e375ddeb80418c4f2eb102b42bb03f65c565bc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 14:50:05 -0400

    2025-06-24T18:50:04.769Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,9 +1,1 @@
-import { log } from "./log.mjs";
-import * as vscode from "vscode";
-export async function sandbox_5() {
-  let openFiles = vscode.window.visibleTextEditors.map(function (editor) {
-    let v = editor.document.fileName;
-    return v;
-  });
-  console.log(openFiles);
-}
+export async function sandbox_5() {}
