c05498675015f4d27fcba4f7bbf6096c366b27f0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 13:13:35 -0400

    2025-06-24T17:13:35.138Z watch   functions_string

--- public/src/functions_string.mjs
+++ public/src/functions_string.mjs
@@ -1,19 +1,8 @@
-import { object_property_exists } from "./object_property_exists.mjs";
+import { functions_data_get } from "./functions_data_get.mjs";
 import { data_literal_strings } from "./data_literal_strings.mjs";
-import { function_names_to_lookup } from "./function_names_to_lookup.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function functions_string(name) {
   assert_arguments_length(arguments, 1);
-  return functions_data_get(data_literal_strings, name);
+  let v = functions_data_get(data_literal_strings, name);
+  return v;
 }
-async function functions_data_get(data_get_fn, name) {
-  let literal_strings = await data_get_fn();
-    let result = [];
-    if (object_property_exists(literal_strings, name)) {
-        let matches = object_property_get(literal_strings, name);
-        result = function_names_to_lookup(matches);
-    }
-    return result;
-}
-
