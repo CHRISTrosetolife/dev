cd0c54fdba7e83ce47783df06e60465019b652d8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 13:18:48 -0400

    2025-06-24T17:18:47.528Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,10 +1,4 @@
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { storage_upload_string } from "./storage_upload_string.mjs";
+import { functions_data_get } from "./functions_data_get.mjs";
 export async function sandbox_5() {
-  await storage_upload_string(
-    string_combine_multiple([
-      "// ==UserScript==\n// @name         New Userscript\n// @namespace    http://tampermonkey.net/\n// @version      2025-04-19\n// @description  try to take over the world!\n// @author       You\n// @match        http://*/*\n// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==\n// @grant        none\n// ==/UserScript==\n\n(function() {\n    'use strict';\n    console.log('here');alert('here')\n    // Your code here...\n})();",
-    ]),
-    "tampermonkey/jesus.js",
-  );
+  functions_data_get;
 }
