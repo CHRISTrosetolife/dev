2ab7ccef5be74a95a538bd7158d9c6e3b025e626
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 12:24:23 -0400

    2025-06-24T16:24:21.945Z watch   js_await_add

--- public/src/js_await_add.mjs
+++ public/src/js_await_add.mjs
@@ -1,3 +1,4 @@
+import { js_declaration_single_name } from "./js_declaration_single_name.mjs";
 import { function_imports_recursive } from "./function_imports_recursive.mjs";
 import { js_declaration_single_async_is } from "./js_declaration_single_async_is.mjs";
 import { js_expression_await } from "./js_expression_await.mjs";
@@ -60,7 +61,7 @@ export async function js_await_add(ast) {
   let async_after = js_declaration_single_async_is(ast);
   if (!async_before && async_after) {
     let found = {};
-    js_declaration_name(ast);
+    js_declaration_single_name(ast);
     await function_imports_recursive(found);
   }
 }
