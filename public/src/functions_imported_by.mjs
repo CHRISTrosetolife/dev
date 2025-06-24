7a110370b8a6d76dff106bf3e8f3d64d2be88a3e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 13:14:27 -0400

    2025-06-24T17:14:26.190Z watch   functions_imported_by

--- public/src/functions_imported_by.mjs
+++ public/src/functions_imported_by.mjs
@@ -3,6 +3,6 @@ import { data_literal_strings } from "./data_literal_strings.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function functions_imported_by(name) {
   assert_arguments_length(arguments, 1);
-  let v = functions_data_get(data_literal_strings, name);
+  let v = await functions_data_get(data_literal_strings, name);
   return v;
 }
