60121e12cfef656d001a4ee19424e5b75ece5ea4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 13:44:15 -0400

    2025-06-24T17:44:14.718Z watch   functions_data_get

--- public/src/functions_data_get.mjs
+++ public/src/functions_data_get.mjs
@@ -3,10 +3,10 @@ import { object_property_get } from "./object_property_get.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 export async function functions_data_get(data_get_fn, name) {
   let literal_strings = await data_get_fn();
-  let result = [];
+  let matches = [];
   if (object_property_exists(literal_strings, name)) {
-    let matches = object_property_get(literal_strings, name);
-    result = function_names_to_lookup(matches);
+    matches = object_property_get(literal_strings, name);
   }
+  let result = function_names_to_lookup(matches);
   return result;
 }
