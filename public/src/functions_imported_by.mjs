126af4d7d90c8bbebba570b251990a5bd1e412a3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 13:15:00 -0400

    2025-06-24T17:14:59.742Z watch   functions_imported_by

--- public/src/functions_imported_by.mjs
+++ public/src/functions_imported_by.mjs
@@ -1,8 +1,8 @@
+import { data_imported_by } from "./data_imported_by.mjs";
 import { functions_data_get } from "./functions_data_get.mjs";
-import { data_literal_strings } from "./data_literal_strings.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function functions_imported_by(name) {
   assert_arguments_length(arguments, 1);
-  let v = await functions_data_get(data_literal_strings, name);
+  let v = await functions_data_get(data_imported_by, name);
   return v;
 }
