07134fe0cd9e47ebbbc7e3f30b21e4d1c917dfb9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-25 14:24:44 -0400

    2025-06-25T18:24:43.434Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,8 +1,10 @@
+import { list_wait } from "./list_wait.mjs";
 import { html_parse } from "./html_parse.mjs";
 import { list_map } from "./list_map.mjs";
 import { grace_facebook_data_messages_jared_cache } from "./grace_facebook_data_messages_jared_cache.mjs";
 export async function sandbox_5() {
   let v4 = await grace_facebook_data_messages_jared_cache();
   let v = list_map(v4, html_parse);
-  return v;
+  let v2 = await list_wait(v);
+  return v2;
 }
