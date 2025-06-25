6ee739d6a902fa80fa8d3e6a7c1abaa552b7987c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 23:17:29 -0400

    2025-06-25T03:17:28.803Z watch   grace_facebook_data_htmls

--- public/src/grace_facebook_data_htmls.mjs
+++ public/src/grace_facebook_data_htmls.mjs
@@ -1,5 +1,6 @@
+import { folder_external_root } from "./folder_external_root.mjs";
 import { folder_read } from "./folder_read.mjs";
 export async function grace_facebook_data_htmls() {
-  let v = await folder_read("D:/g", ".html");
+  let v = await folder_read(folder_external_root("g"), ".html");
   return v;
 }
