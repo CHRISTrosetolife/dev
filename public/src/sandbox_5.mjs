03cefd3ba25bc69bcba185879ace672e626decc0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 23:28:35 -0400

    2025-06-25T03:28:34.660Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -24,7 +24,7 @@ export async function sandbox_5() {
     return v5;
   });
   let v2 = list_filter(v, function (s) {
-    let v3 = string_includes_multiple(s, mapped);
+    let v3 = string_includes_multiple(s, mapped) && string_includes_multiple();
     return v3;
   });
   return v2;
