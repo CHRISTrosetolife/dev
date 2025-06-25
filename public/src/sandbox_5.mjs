0fcd9bd62e27cce5d7a1a130b475b8daf19b1b50
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 23:20:12 -0400

    2025-06-25T03:20:12.098Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -2,11 +2,15 @@ import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { string_includes } from "./string_includes.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { grace_facebook_data_htmls_cache } from "./grace_facebook_data_htmls_cache.mjs";
+import { list_any } from "./list_any.mjs";
 export async function sandbox_5() {
   let folders = ["your_activity_off_meta_technologies"];
   let v = await grace_facebook_data_htmls_cache();
   let v2 = list_filter(folders, function (s) {
-    let v3 = string_includes(string_combine_multiple(["\\", s, "\\"]));
+    let v3 = list_any(folders, function (f) {
+      let v4 = string_includes(string_combine_multiple(["\\", s, "\\"]));
+      return v4;
+    });
     return v3;
   });
   return v2;
