b1f16bf326d8153c648f1a910aa081067b43e8d5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-25 14:28:44 -0400

    2025-06-25T18:28:44.376Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,3 +1,4 @@
+import { html_parse } from "./html_parse.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each_async } from "./each_async.mjs";
 import { log } from "./log.mjs";
@@ -6,6 +7,7 @@ export async function sandbox_5() {
   let v4 = await grace_facebook_data_messages_jared_cache();
   await each_async(v4, async function (item) {
     let contents = object_property_get(item, "contents");
+    let p = await html_parse(contents);
   });
   log({
     v2,
