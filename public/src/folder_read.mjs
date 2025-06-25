44f7deb9cfa921e24b699056028eac3099f0ad08
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-24 23:11:40 -0400

    2025-06-25T03:11:40.148Z watch   folder_read

--- public/src/folder_read.mjs
+++ public/src/folder_read.mjs
@@ -1,5 +1,8 @@
+import { never } from "./never.mjs";
+import { assert } from "./assert.mjs";
 import { folder_read_generic } from "./folder_read_generic.mjs";
 export async function folder_read(folder, file_extension) {
+  assert(never, []);
   let recursive = true;
   let files = await folder_read_generic(folder, [file_extension], recursive);
   return files;
