6709ac1162977ab95f2fb062a0579a13b03bd868
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-25 20:30:48 -0400

    2025-06-26T00:30:47.523Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,3 +1,4 @@
+import { html_parse_children } from "./html_parse_children.mjs";
 import { html_parse_visit_tag_single } from "./html_parse_visit_tag_single.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { html_parse } from "./html_parse.mjs";
@@ -18,5 +19,6 @@ export async function sandbox_5() {
     let root = await html_parse(contents);
     object_property_set(item, "parsed", root);
     let main = html_parse_visit_tag_single(root, "main");
+    html_parse_children();
   }
 }
