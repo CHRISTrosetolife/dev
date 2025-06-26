b2a949a224789f510efdc9a4583d186b4b68d1da
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-26 13:28:50 -0400

    2025-06-26T17:28:49.679Z pm object_merge_list_generic strict to

--- public/src/object_merge_list_generic.mjs
+++ public/src/object_merge_list_generic.mjs
@@ -3,7 +3,7 @@ import { object_property_initialize } from "./object_property_initialize.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { assert } from "./assert.mjs";
 import { each_object } from "./each_object.mjs";
-export function object_merge_list_generic(from, strict, to) {
+export function object_merge_list_generic(from, to, strict) {
   each_object(from, function (key, value) {
     if (strict) {
       assert(object_property_exists_not, [to, key]);
--- public/src/object_merge_list_strict.mjs
+++ public/src/object_merge_list_strict.mjs
@@ -3,6 +3,6 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function object_merge_list_strict(from, to) {
   assert_arguments_length(arguments, 2);
   let strict = true;
-  let v = object_merge_list_generic(from, strict, to);
+  let v = object_merge_list_generic(from, to, strict);
   return v;
 }
