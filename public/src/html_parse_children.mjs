a0521d3646608963ea5011456a150566cb69a2a1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-25 20:31:09 -0400

    2025-06-26T00:31:08.593Z watch   html_parse_children

--- public/src/html_parse_children.mjs
+++ public/src/html_parse_children.mjs
@@ -1,4 +1,5 @@
 import { object_property_get_or } from "./object_property_get_or.mjs";
-export function html_parse_children(n) {
-  return object_property_get_or(n, "children", []);
+export function html_parse_children(node) {
+  let v = object_property_get_or(node, "children", []);
+  return v;
 }
