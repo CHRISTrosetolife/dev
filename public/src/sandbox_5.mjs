ad6127812689745a33312b4b446b4f658ea8fe4f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-25 20:48:50 -0400

    2025-06-26T00:48:49.963Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,3 +1,4 @@
+import { html_parse_tag_named } from "./html_parse_tag_named.mjs";
 import { html_parse_tag_name } from "./html_parse_tag_name.mjs";
 import { list_map } from "./list_map.mjs";
 import { html_parse_text } from "./html_parse_text.mjs";
@@ -14,6 +15,7 @@ import { grace_facebook_data_messages_jared_cache } from "./grace_facebook_data_
 import { list_first } from "./list_first.mjs";
 import { assert } from "./assert.mjs";
 import { list_size } from "./list_size.mjs";
+import { list_filter } from "./list_filter.mjs";
 export async function sandbox_5() {
   let v4 = await grace_facebook_data_messages_jared_cache();
   let f = list_first(v4);
@@ -35,7 +37,9 @@ export async function sandbox_5() {
     });
     assert(equal, [list_size(c), 3]);
     let h2 = html_parse_visit_tag_single(f, "h2");
-    let div = html_parse_visit_tag_single(f, "div");
+    let div = list_filter(c, function (child) {
+      return html_parse_tag_named(child, "div");
+    });
     let footer = html_parse_visit_tag_single(f, "footer");
     log({
       user: html_parse_text(h2),
