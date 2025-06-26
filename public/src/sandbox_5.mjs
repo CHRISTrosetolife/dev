c923fa27576167b9230c6acdd66825f2d5a3d1c6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-26 13:17:06 -0400

    2025-06-26T17:17:06.039Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,11 +1,10 @@
-import { object_property_string_empty_not_is } from "./object_property_string_empty_not_is.mjs";
+import { list_filter_property_string_empty_not_is } from "./list_filter_property_string_empty_not_is.mjs";
 import { facebook_data_messages_date_get } from "./facebook_data_messages_date_get.mjs";
 import { list_to_groups } from "./list_to_groups.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
 import { grace_facebook_data_messages_jared_parsed_cache } from "./grace_facebook_data_messages_jared_parsed_cache.mjs";
 import { list_first } from "./list_first.mjs";
-import { list_filter } from "./list_filter.mjs";
 export async function sandbox_5() {
   let threads = await grace_facebook_data_messages_jared_parsed_cache();
   let thread = list_first(threads);
@@ -18,11 +17,3 @@ export async function sandbox_5() {
   each(threads, function (item) {});
   return threads;
 }
-function list_filter_property_string_empty_not_is(list, property_name) {
-    filtered = list_filter(list, function (item) {
-        let v2 = object_property_string_empty_not_is(item, property_name);
-        return v2;
-    });
-    return filtered;
-}
-
