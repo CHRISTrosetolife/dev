c94832090d3faff22a8e88d011197c7a802d13e5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-26 15:37:09 -0400

    2025-06-26T19:37:09.001Z ar fv_messages

--- public/src/app_fv_messages.mjs
+++
@@ -1,4 +0,0 @@
-import { html_style_default_initialize } from "./html_style_default_initialize.mjs";
-export function app_fv_messages() {
-  let root = html_style_default_initialize();
-}
--- public/src/apps_list.mjs
+++ public/src/apps_list.mjs
@@ -1,5 +1,5 @@
 export function apps_list() {
-  let v = [
+  return [
     "bible_ceb",
     "bible_ceb2",
     "bible_ceb3",
@@ -18,7 +18,6 @@ export function apps_list() {
     "el",
     "en_ur",
     "en",
-    "fv_messages",
     "good",
     "grocery",
     "gs",
@@ -51,5 +50,4 @@ export function apps_list() {
     "todo",
     "yyy8Uu",
   ];
-  return v;
 }
