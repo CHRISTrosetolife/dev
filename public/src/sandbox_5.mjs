193c77dc997da5ae292b840867b96e59bd6ad342
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-25 20:35:31 -0400

    2025-06-26T00:35:30.720Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,3 +1,4 @@
+import { html_parse_visit_tag_list } from "./html_parse_visit_tag_list.mjs";
 import { log } from "./log.mjs";
 import { html_parse_tag_name } from "./html_parse_tag_name.mjs";
 import { html_parse_children } from "./html_parse_children.mjs";
@@ -21,6 +22,7 @@ export async function sandbox_5() {
     let root = await html_parse(contents);
     object_property_set(item, "parsed", root);
     let main = html_parse_visit_tag_single(root, "main");
+    html_parse_visit_tag_list(main);
     let c = html_parse_children(main);
     f = list_first(c);
     let n = html_parse_tag_name(f);
