69eecd156fafa73f7f4bc6d5251ed152c298ad98
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-26 14:03:17 -0400

    2025-06-26T18:03:16.348Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -29,6 +29,9 @@ export async function sandbox_5() {
   let result = [];
   let predicate = function (key, value) {
     let d = date_to(key);
+    let cutoff = new Date("2025-03-22");
+    let v4 = d < cutoff;
+    return v4;
   };
   each_object(merged, function (date, messages) {});
   let v = merged;
