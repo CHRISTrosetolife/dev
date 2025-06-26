5fcf4d2b1f869eb4638ebfd319575d97dc4d9e7d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-25 22:07:57 -0400

    2025-06-26T02:07:55.112Z watch   list_wait

--- public/src/list_wait.mjs
+++ public/src/list_wait.mjs
@@ -1,3 +1,6 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function list_wait(promises_list) {
-  return await Promise.all(promises_list);
+  assert_arguments_length(arguments, 1);
+  let v = await Promise.all(promises_list);
+  return v;
 }
