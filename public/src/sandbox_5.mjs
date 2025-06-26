0d8027d04d6afb49bae6a5b8612a96149d0af5fa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-26 13:53:37 -0400

    2025-06-26T17:53:36.632Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -19,7 +19,7 @@ export async function sandbox_5() {
   let merged = groups_merge(mapped);
   each_object(merged, function (date, messages) {
     each(list, function (item) {
-      let messages2 = object_property_get(item, "messages");
+      let message = object_property_get(item, "message");
     });
   });
   let v = merged;
