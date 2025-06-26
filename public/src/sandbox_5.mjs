831ec979d165283e3a12834976b5ef6397e192de
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-25 22:59:11 -0400

    2025-06-26T02:59:10.913Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,6 +1,5 @@
 import { facebook_data_messages_date_get } from "./facebook_data_messages_date_get.mjs";
 import { list_to_groups } from "./list_to_groups.mjs";
-import { string_empty_not_is } from "./string_empty_not_is.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
 import { grace_facebook_data_messages_jared_parsed_cache } from "./grace_facebook_data_messages_jared_parsed_cache.mjs";
@@ -10,7 +9,7 @@ export async function sandbox_5() {
   let threads = await grace_facebook_data_messages_jared_parsed_cache();
   let thread = list_first(threads);
   let messages = object_property_get(thread, "messages");
-  messages = list_filter(messages, string_empty_not_is);
+  messages = list_filter(messages, function (m) {});
   let result = list_to_groups(messages, facebook_data_messages_date_get);
   let v = result;
   return v;
