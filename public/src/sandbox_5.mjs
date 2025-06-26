cdcaf222ba363363e46d8977801ea0ec36e7d868
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-26 13:24:55 -0400

    2025-06-26T17:24:54.762Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -8,11 +8,14 @@ import { grace_facebook_data_messages_jared_parsed_cache } from "./grace_faceboo
 import { list_first } from "./list_first.mjs";
 export async function sandbox_5() {
   let threads = await grace_facebook_data_messages_jared_parsed_cache();
-  list_map(threads, function (thread) {});
+  list_map(threads, function (thread) {
+    let messages = object_property_get(thread, "messages");
+    messages = list_filter_property_string_empty_not_is(messages, "message");
+    let result = list_to_groups(messages, facebook_data_messages_date_get);
+    let v2 = result;
+    return v2;
+  });
   let thread = list_first(threads);
-  let messages = object_property_get(thread, "messages");
-  messages = list_filter_property_string_empty_not_is(messages, "message");
-  let result = list_to_groups(messages, facebook_data_messages_date_get);
   let merged = {};
   let v = result;
   return v;
