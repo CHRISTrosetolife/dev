c29071110ccab83b84f1288b0938755a6beb47b6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-26 13:54:18 -0400

    2025-06-26T17:54:18.259Z watch   object_merge_list

--- public/src/object_merge_list.mjs
+++ public/src/object_merge_list.mjs
@@ -2,7 +2,7 @@ import { object_merge_list_generic } from "./object_merge_list_generic.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export function object_merge_list(from, to) {
   assert_arguments_length(arguments, 2);
-  let strict = true;
+  let strict = false;
   let v = object_merge_list_generic(from, to, strict);
   return v;
 }
