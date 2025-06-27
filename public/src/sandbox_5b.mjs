fee666ab2373608069004e229535c8fa2dc01739
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-27 14:29:08 -0400

    2025-06-27T18:29:07.559Z watch   grace_facebook_data_messages_jared_before

--- public/src/sandbox_5b.mjs
+++ public/src/sandbox_5b.mjs
@@ -6,15 +6,19 @@ import { list_random_item } from "./list_random_item.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { grace_facebook_data_messages_jared_parsed_list } from "./grace_facebook_data_messages_jared_parsed_list.mjs";
 export async function sandbox_5b() {
-  let parsed = await grace_facebook_data_messages_jared_parsed_list();
-  let before = list_filter(parsed, before_trip);
-  let messages = list_random_item(before);
-  await facebook_data_messages_to_html_open(messages);
-  function before_trip(item) {
-    let day = object_property_get(item, "day");
-    let d = date_to(day);
-    let cutoff = new Date("2025-03-22");
-    let v4 = d < cutoff && string_includes(day, "2025");
-    return v4;
-  }
+  await grace_facebook_data_messages_jared_before();
 }
+async function grace_facebook_data_messages_jared_before() {
+    let parsed = await grace_facebook_data_messages_jared_parsed_list();
+    let before = list_filter(parsed, before_trip);
+    let messages = list_random_item(before);
+    await facebook_data_messages_to_html_open(messages);
+    function before_trip(item) {
+        let day = object_property_get(item, "day");
+        let d = date_to(day);
+        let cutoff = new Date("2025-03-22");
+        let v4 = d < cutoff && string_includes(day, "2025");
+        return v4;
+    }
+}
+
