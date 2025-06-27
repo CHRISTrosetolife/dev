0d2ed08c1cd55baae4955e5759573272530033fd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-27 14:47:27 -0400

    2025-06-27T18:47:25.935Z watch   string_includes

--- public/src/string_includes.mjs
+++ public/src/string_includes.mjs
@@ -1,7 +1,7 @@
-import { never } from "./never.mjs";
 import { assert } from "./assert.mjs";
+import { string_is } from "./string_is.mjs";
 export function string_includes(string, value) {
-  assert(never, []);
+  assert(string_is, [string]);
   let v = string.includes(value);
   return v;
 }
