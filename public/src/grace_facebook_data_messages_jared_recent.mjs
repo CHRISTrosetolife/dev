6b839e6050096aa11280ac4a54edb6a6f43542bb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-27 14:31:50 -0400

    2025-06-27T18:31:49.683Z watch   grace_facebook_data_messages_jared_recent

--- public/src/grace_facebook_data_messages_jared_recent.mjs
+++ public/src/grace_facebook_data_messages_jared_recent.mjs
@@ -1,19 +1,12 @@
-import { date_to } from "./date_to.mjs";
+import { list_first } from "./list_first.mjs";
+import { list_take_reverse } from "./list_take_reverse.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { facebook_data_messages_to_html_open } from "./facebook_data_messages_to_html_open.mjs";
-import { list_random_item } from "./list_random_item.mjs";
-import { list_filter } from "./list_filter.mjs";
 import { grace_facebook_data_messages_jared_parsed_list } from "./grace_facebook_data_messages_jared_parsed_list.mjs";
 export async function grace_facebook_data_messages_jared_recent() {
   let parsed = await grace_facebook_data_messages_jared_parsed_list();
-  let after = list_filter(parsed, after_trip);
-  let messages = list_random_item(after);
+  let recents = list_take_reverse(parsed, 5);
+  let recent = list_first(recents);
+  let messages = object_property_get(recent, "messages");
   await facebook_data_messages_to_html_open(messages);
-  function after_trip(item) {
-    let day = object_property_get(item, "day");
-    let d = date_to(day);
-    let cutoff = new Date("2025-04-04");
-    let v4 = d >= cutoff;
-    return v4;
-  }
 }
