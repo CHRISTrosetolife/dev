3c86e4ec21c7c5834b2f3fca092d12165c621b52
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-27 14:25:02 -0400

    2025-06-27T18:25:01.698Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -97,14 +97,15 @@ export async function sandbox_5() {
   messages = list_random_item(after);
   await facebook_data_messages_to_html_open(messages);
   function before_trip(item) {
-    let day2 = object_property_get(item, "day");
-    let d = date_to(key);
+    let day = object_property_get(item, "day");
+    let d = date_to(day);
     let cutoff = new Date("2025-03-22");
-    let v4 = d < cutoff && string_includes(key, "2025");
+    let v4 = d < cutoff && string_includes(day, "2025");
     return v4;
   }
   function after_trip(item) {
-    let d = date_to(key);
+    let day = object_property_get(item, "day");
+    let d = date_to(day);
     let cutoff = new Date("2025-04-04");
     let v4 = d >= cutoff;
     return v4;
