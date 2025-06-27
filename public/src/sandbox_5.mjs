5e49ee9932d18bbc9caacae7a4feb62f7fd886b0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-27 14:49:21 -0400

    2025-06-27T18:49:20.844Z s5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -15,11 +15,7 @@ export async function sandbox_5() {
     each(parsed, function (day) {
       let messages = object_property_get(day, "messages");
       let mapped = list_map_property(messages, "message");
-      log({
-        mapped,
-      });
       let match = list_any(mapped, function (m) {
-        log(m);
         let v = string_includes(m, search_term);
         return v;
       });
