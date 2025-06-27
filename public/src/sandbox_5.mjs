a60232dddd5c43d9704549c795a240cce6b5b417
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-27 14:43:14 -0400

    2025-06-27T18:43:12.551Z watch   sandbox_5

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,3 +1,4 @@
+import { list_map_property } from "./list_map_property.mjs";
 import { list_concat_multiple } from "./list_concat_multiple.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { each } from "./each.mjs";
@@ -5,7 +6,6 @@ import { object_property_get } from "./object_property_get.mjs";
 import { grace_facebook_data_messages_jared_parsed_list } from "./grace_facebook_data_messages_jared_parsed_list.mjs";
 import { facebook_data_messages_to_html_open } from "./facebook_data_messages_to_html_open.mjs";
 import { string_includes } from "./string_includes.mjs";
-import { list_map } from "./list_map.mjs";
 import { list_includes } from "./list_includes.mjs";
 export async function sandbox_5() {
   let search_term = "wedding";
@@ -13,7 +13,7 @@ export async function sandbox_5() {
   let list = list_adder(function (la) {
     each(parsed, function (day) {
       let messages = object_property_get(day, "messages");
-      let mapped = list_map(messages, "message");
+      let mapped = list_map_property(messages, "message");
       let match = list_includes(mapped, function (m) {
         let v = string_includes(m, search_term);
         return v;
