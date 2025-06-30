72600511694e509c2599452e208bb7b71d53744a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-30 19:46:47 -0400

    2025-06-30T23:46:47.074Z watch   app_load

--- public/src/app_load.mjs
+++ public/src/app_load.mjs
@@ -1,5 +1,4 @@
 import { list_join_newline } from "./list_join_newline.mjs";
-import { log } from "./log.mjs";
 import { app_prefix_combine } from "./app_prefix_combine.mjs";
 import { js_code_call } from "./js_code_call.mjs";
 import { function_deploy_path } from "./function_deploy_path.mjs";
@@ -16,6 +15,5 @@ export async function app_load(name) {
   let script = await http_storage_get(
     function_deploy_path(name_prefixed, version),
   );
-  log(script);
   await eval(list_join_newline([js_code_call(name_prefixed), script]));
 }
