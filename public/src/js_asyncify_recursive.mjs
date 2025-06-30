20662b12f4ce29784b7f33fa2bdbee60db15f680
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-30 19:23:33 -0400

    2025-06-30T23:23:31.256Z watch   js_asyncify_recursive

--- public/src/js_asyncify_recursive.mjs
+++ public/src/js_asyncify_recursive.mjs
@@ -23,7 +23,10 @@ export async function js_asyncify_recursive(this_name, stack, functions) {
     if (!async_is) {
       let ast_importing = await function_parse(importing);
       await js_await_add_recursive(ast_importing, stack);
-      await function_transform(importing, function (ast) {});
+      await function_transform(importing, async function (ast) {
+        let v = await js_await_add_recursive(ast_importing, stack);
+        return v;
+      });
     }
   });
 }
