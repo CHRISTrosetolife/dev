5f46290905d692c1ced9c0a5b9d95d3704fe7dc2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-30 18:29:23 -0400

    2025-06-30T22:29:22.974Z watch   js_asyncify

--- public/src/js_asyncify.mjs
+++ public/src/js_asyncify.mjs
@@ -1,5 +1,7 @@
+import { data_functions } from "./data_functions.mjs";
 import { js_asyncify_recursive } from "./js_asyncify_recursive.mjs";
-export async function js_asyncify(this_name, stack, functions) {
-  stack = [];
+export async function js_asyncify(this_name) {
+  let functions = await data_functions();
+  let stack = [];
   await js_asyncify_recursive(this_name, stack, functions);
 }
