a99ce30dfec719996e1c0f0af7ca476be6bc9e59
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-30 19:32:53 -0400

    2025-06-30T23:32:52.532Z watch   js_await_add

--- public/src/js_await_add.mjs
+++ public/src/js_await_add.mjs
@@ -1,5 +1,7 @@
+import { data_functions } from "./data_functions.mjs";
 import { js_await_add_recursive } from "./js_await_add_recursive.mjs";
 export async function js_await_add(ast) {
   let stack = [];
-  await js_await_add_recursive(stack, ast);
+  let functions = await data_functions();
+  await js_await_add_recursive(stack, ast, functions);
 }
