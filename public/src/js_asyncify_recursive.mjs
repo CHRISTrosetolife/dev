2e58cef9f336e3352d619faebf8f18a782880cf1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-06-30 19:41:11 -0400

    2025-06-30T23:41:11.100Z watch   js_asyncify_recursive

--- public/src/js_asyncify_recursive.mjs
+++ public/src/js_asyncify_recursive.mjs
@@ -1,7 +1,6 @@
 import { function_transform } from "./function_transform.mjs";
 import { each_async } from "./each_async.mjs";
 import { js_await_add_recursive } from "./js_await_add_recursive.mjs";
-import { data_functions_async_is } from "./data_functions_async_is.mjs";
 import { list_difference } from "./list_difference.mjs";
 import { data_importing } from "./data_importing.mjs";
 import { functions_data_get_list } from "./functions_data_get_list.mjs";
@@ -9,7 +8,6 @@ export async function js_asyncify_recursive(this_name, stack, functions) {
   let importings = await functions_data_get_list(data_importing, this_name);
   let difference = list_difference(importings, stack);
   await each_async(difference, async function (importing) {
-    let async_is = data_functions_async_is(functions, importing);
     await function_transform(importing, async function (ast) {
       let v = await js_await_add_recursive(ast, stack, functions);
       return v;
