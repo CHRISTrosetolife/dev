196a3fee6e26837c94b424109f60f1888c66b763
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-02 19:58:46 -0400

    2025-07-02T23:58:45.600Z watch   app_share_book_random

--- public/src/app_share_book_random.mjs
+++ public/src/app_share_book_random.mjs
@@ -3,7 +3,6 @@ import { app_share_verse } from "./app_share_verse.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { app_share_chapter } from "./app_share_chapter.mjs";
 import { object_property_set } from "./object_property_set.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { html_hash_lookup } from "./html_hash_lookup.mjs";
 export function app_share_book_random(context) {
   let lookup = html_hash_lookup();
