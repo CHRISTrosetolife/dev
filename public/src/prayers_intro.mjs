3f38be2203fdada48048c608e2eca13cc858ce9d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-02 15:39:23 -0400

    2025-07-02T19:39:22.816Z watch   prayers_intro

--- public/src/prayers_intro.mjs
+++ public/src/prayers_intro.mjs
@@ -2,7 +2,7 @@ export function prayers_intro() {
   let v = [
     "Cause me to perfectly and maximally glorify and obey the Father, Jesus and the Holy Spirit",
     "May the words of my mouth and the meditation of my heart be pleasing in your sight, O Lord, my Rock and my Redeemer",
-    "May I fulfill my ministry",
+    "May I fulfill any ministry you've commanded",
     "Thank You",
   ];
   return v;
