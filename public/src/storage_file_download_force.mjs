2720e8e28af3190280df72b989ec20fc584a0208
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-02 14:00:36 -0400

    2025-07-02T18:00:35.682Z watch   storage_file_download_force

--- public/src/storage_file_download_force.mjs
+++ public/src/storage_file_download_force.mjs
@@ -1,5 +1,7 @@
+import { storage_file_download_path } from "./storage_file_download_path.mjs";
 import { folder_parent_exists_ensure } from "./folder_parent_exists_ensure.mjs";
 export async function storage_file_download_force(firebase_file) {
+  let destination = storage_file_download_path(firebase_file);
   await folder_parent_exists_ensure(destination);
   await firebase_file.download({
     destination,
