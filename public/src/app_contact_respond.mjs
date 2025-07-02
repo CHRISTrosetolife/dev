1041fbe1d7059f9d6c5cf16aa9e1355f330cd804
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-02 14:20:01 -0400

    2025-07-02T18:20:01.210Z watch   app_contact_respond

--- public/src/app_contact_respond.mjs
+++ public/src/app_contact_respond.mjs
@@ -1,23 +1,10 @@
-import { each_index } from "./each_index.mjs";
 import { string_split_space } from "./string_split_space.mjs";
-import { log } from "./log.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { list_includes } from "./list_includes.mjs";
 import { app_share_verse_refresh_greet } from "./app_share_verse_refresh_greet.mjs";
 export function app_contact_respond(input) {
   let lower = string_case_lower(input);
   let tokens = string_split_space(lower);
-  function app_contact_respond_nice_to_meet_you() {
-    let v = ["great", "to", "meet", "with", app_contact_respond_you];
-    return v;
-  }
-  function app_contact_respond_you() {
-    let v2 = ["you", [[""], ["servant", "of", "the", "most", "high", "god"]]];
-    return v2;
-  }
-  log({
-    split: tokens,
-  });
   let output = null;
   if (list_includes(["hi", "hii"], input)) {
     output = app_share_verse_refresh_greet();
@@ -27,10 +14,15 @@ export function app_contact_respond(input) {
     "to",
     "meet",
     "with",
-    app_contact_respond_you,
+    "you",
+    "servant",
+    "of",
+    "the",
+    "most",
+    "high",
+    "god",
   ];
   let index = 0;
-  each_index(list, function (item, index) {});
   if (false) {
   }
   return output;
