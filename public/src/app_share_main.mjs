3600724b7ff7819564e68db33c8545ed2b46f0b8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-02 19:43:19 -0400

    2025-07-02T23:43:19.315Z watch   app_share_main

--- public/src/app_share_main.mjs
+++ public/src/app_share_main.mjs
@@ -1,17 +1,12 @@
+import { app_share_book_random } from "./app_share_book_random.mjs";
 import { html_hash_set_lookup } from "./html_hash_set_lookup.mjs";
-import { list_take_bible_books_new_context } from "./list_take_bible_books_new_context.mjs";
 import { html_button } from "./html_button.mjs";
 import { html_clear_scroll_top_context } from "./html_clear_scroll_top_context.mjs";
 import { app_share_verse_refresh } from "./app_share_verse_refresh.mjs";
 import { app_share_bible_folders } from "./app_share_bible_folders.mjs";
-import { app_share_verse } from "./app_share_verse.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
-import { app_share_chapter } from "./app_share_chapter.mjs";
 import { object_property_set } from "./object_property_set.mjs";
-import { list_random_item } from "./list_random_item.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
-import { html_hash_lookup } from "./html_hash_lookup.mjs";
 export function app_share_main(context) {
   let root = html_clear_scroll_top_context(context);
   let folders = [
@@ -70,7 +65,8 @@ export function app_share_main(context) {
   ];
   each(folders, function (folder) {
     html_button(root, object_property_get(folder, "text"), async function () {
-      let { lookup, book_code, chapter_code, verse_number_next } = app_share_book_random(context);
+      let { lookup, book_code, chapter_code, verse_number_next } =
+        app_share_book_random(context);
       let f = object_property_get(folder, "code");
       object_property_set(lookup, app_share_bible_folders(), f);
       html_hash_set_lookup(lookup);
@@ -84,19 +80,3 @@ export function app_share_main(context) {
     });
   });
 }
-function app_share_book_random(context) {
-    let lookup = html_hash_lookup();
-    let books_new = list_take_bible_books_new_context(context);
-    let book = list_random_item(books_new);
-    let book_code = object_property_get(book, "book_code");
-    let chapter_code = "01";
-    object_property_set(
-        lookup,
-        app_share_chapter(),
-        string_combine_multiple([book_code, chapter_code])
-    );
-    let verse_number_next = "1";
-    object_property_set(lookup, app_share_verse(), verse_number_next);
-    return { lookup, book_code, chapter_code, verse_number_next };
-}
-
