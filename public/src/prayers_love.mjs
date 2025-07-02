bf1b543d0b59991aa828d455b550eb7cacf39709
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-02 15:41:28 -0400

    2025-07-02T19:41:28.088Z watch   prayers_love

--- public/src/prayers_love.mjs
+++ public/src/prayers_love.mjs
@@ -16,7 +16,7 @@ export function prayers_love() {
     "May love feel and be strong during period and childbirth, without pain",
     "Protect the teeth of love and I",
     "May I speak and understand Cebuano",
-    "Banish my desire for soda and sinful pleasure",
+    "Banish my desire for soda and pleasure",
     "May my face be clean-shaven without any cuts",
     "May the desire of the Father be done, including regarding...",
     prayers_me(),
