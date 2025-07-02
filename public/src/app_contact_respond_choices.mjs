524e2fec420f163930d865cd05f2456aa52e02fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-02 18:36:58 -0400

    2025-07-02T22:36:58.121Z watch   app_contact_respond_choices

--- public/src/app_contact_respond_choices.mjs
+++ public/src/app_contact_respond_choices.mjs
@@ -47,7 +47,7 @@ export function app_contact_respond_choices() {
     {
       choice: ["keep", "sending", "us", "such", "scriptures"],
       response:
-        "If you send me a message here 📨, then you will receive a scripture 📖",
+        "If you send me a message here 📨, then you will receive a scripture 📖.",
     },
     {
       choice: ["and"],
