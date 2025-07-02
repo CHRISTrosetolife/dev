2e69ca1f399b9c790006ff204d2c9bc51efc30f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-02 18:04:46 -0400

    2025-07-02T22:04:46.218Z watch   app_contact_respond

--- public/src/app_contact_respond.mjs
+++ public/src/app_contact_respond.mjs
@@ -1,6 +1,5 @@
 import { each } from "./each.mjs";
 import { app_contact_respond_choice } from "./app_contact_respond_choice.mjs";
-import { and } from "./and.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { app_share_thanks } from "./app_share_thanks.mjs";
 import { string_split_space } from "./string_split_space.mjs";
@@ -11,59 +10,60 @@ export function app_contact_respond(input) {
   let lower = string_case_lower(input);
   let tokens = string_split_space(lower);
   let output = null;
-  let choices = [];
+  let choices = [
+    {
+      choice: [
+        "great",
+        "to",
+        "meet",
+        "with",
+        "you",
+        "servant",
+        "of",
+        "the",
+        "most",
+        "high",
+        "god",
+      ],
+      response: app_share_verse_refresh_greet(),
+    },
+    {
+      choice: [
+        "keep",
+        "on",
+        "praying",
+        "for",
+        "us",
+        "so",
+        "that",
+        "god's",
+        "will",
+        "be",
+        "done",
+        "on",
+        "us",
+        "and",
+        "keep",
+        "sending",
+        "us",
+        "such",
+        "scriptures",
+      ],
+      response: string_combine_multiple([
+        app_share_thanks(),
+        " Great to meet you, too!",
+      ]),
+    },
+    {
+      choice: ["keep", "sending", "us", "such", "scriptures"],
+      response: "",
+    },
+    {
+      choice: ["and"],
+      response: "",
+    },
+  ];
   each(list, function (item) {});
-  let nice_to_meet_you = {
-    choice: [
-      "great",
-      "to",
-      "meet",
-      "with",
-      "you",
-      "servant",
-      "of",
-      "the",
-      "most",
-      "high",
-      "god",
-    ],
-    response: app_share_verse_refresh_greet(),
-  };
-  let pray = {
-    choice: [
-      "keep",
-      "on",
-      "praying",
-      "for",
-      "us",
-      "so",
-      "that",
-      "god's",
-      "will",
-      "be",
-      "done",
-      "on",
-      "us",
-      "and",
-      "keep",
-      "sending",
-      "us",
-      "such",
-      "scriptures",
-    ],
-    response: string_combine_multiple([
-      app_share_thanks(),
-      " Great to meet you, too!",
-    ]),
-  };
-  let scripture = {
-    choice: ["keep", "sending", "us", "such", "scriptures"],
-    response: "",
-  };
-  let and = {
-    choice: ["and"],
-    response: "",
-  };
   let offset = 0;
   offset = app_contact_respond_choice(
     tokens,
