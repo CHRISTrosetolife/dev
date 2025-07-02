9fa6e077ff65f417dbe96697954552298fd71caa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-02 14:03:48 -0400

    2025-07-02T18:03:47.873Z watch   app_contact_respond

--- public/src/app_contact_respond.mjs
+++ public/src/app_contact_respond.mjs
@@ -1,9 +1,13 @@
+import { log } from "./log.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { list_includes } from "./list_includes.mjs";
 import { app_share_verse_refresh_greet } from "./app_share_verse_refresh_greet.mjs";
 export function app_contact_respond(input) {
   let lower = string_case_lower(input);
   let split = list_split_space(lower);
+  log({
+    split,
+  });
   let output = null;
   if (list_includes(["hi", "hii"], input)) {
     output = app_share_verse_refresh_greet();
