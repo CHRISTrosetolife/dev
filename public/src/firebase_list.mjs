a743e98a0a6793665e7d08751e738f17d4ee3110
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-01 20:41:19 -0400

    2025-07-02T00:41:18.947Z watch   firebase_list

--- public/src/firebase_list.mjs
+++ public/src/firebase_list.mjs
@@ -1,7 +1,7 @@
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 export async function firebase_list(storage_path) {
   assert_arguments_length(arguments, 1);
-  let { getStorage, ref, listAll } = await import(
+  let { ref, listAll } = await import(
     "https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.2/firebase-storage.min.js"
   );
   let storageRef = ref(storage, storage_path);
