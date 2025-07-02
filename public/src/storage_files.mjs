23710774c71613ff208a393b7bfe9795d27ca547
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-02 14:32:14 -0400

    2025-07-02T18:32:12.801Z watch   storage_files

--- public/src/storage_files.mjs
+++ public/src/storage_files.mjs
@@ -1,3 +1,5 @@
+import { log } from "./log.mjs";
+import { list_size } from "./list_size.mjs";
 import { exit } from "./exit.mjs";
 import { list_sort_string_property } from "./list_sort_string_property.mjs";
 import { storage_bucket_prefix } from "./storage_bucket_prefix.mjs";
@@ -9,7 +11,11 @@ export async function storage_files(prefix) {
   });
   let files = list_single(data);
   list_sort_string_property(files, "name");
-  if (file_paths_size >= 1000) {
+  let files_size = list_size(files);
+  log({
+    file_paths_size: files_size,
+  });
+  if (files_size >= 1000) {
     ("this needs pagination");
     exit();
   }
