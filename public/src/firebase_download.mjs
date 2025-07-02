8d060f5eee592614907ee54f39f1c39c841a4a22
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-01 20:40:01 -0400

    2025-07-02T00:40:00.180Z watch   firebase_download

--- public/src/firebase_download.mjs
+++ public/src/firebase_download.mjs
@@ -1,24 +1,24 @@
 import { log } from "./log.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { http_get } from "./http_get.mjs";
-import {
-  getStorage,
-  ref,
-  getDownloadURL,
-} from "https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.2/firebase-storage.min.js";
 export async function firebase_download(storage_path) {
   assert_arguments_length(arguments, 1);
+  let { getStorage, ref, getDownloadURL } = await import(
+    "https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.2/firebase-storage.min.js"
+  );
   let storage = getStorage();
   let storageRef = ref(storage, storage_path);
   try {
     let url = await getDownloadURL(storageRef);
-    return await http_get(url);
+    let v = await http_get(url);
+    return v;
   } catch (e) {
     log({
       e,
     });
     if (e.code === "storage/object-not-found") {
-      return null;
+      let v2 = null;
+      return v2;
     } else {
       throw e;
     }
