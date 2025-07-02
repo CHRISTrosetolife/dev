f7c80d5e925edc3d7070d32c779f84f4206a60b7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-02 17:53:49 -0400

    2025-07-02T21:53:49.295Z watch   app_contact_respond

--- public/src/app_contact_respond.mjs
+++ public/src/app_contact_respond.mjs
@@ -58,6 +58,7 @@ export function app_contact_respond(input) {
   let scripture = ["keep", "sending", "us", "such", "scriptures"];
   let and = ["and"];
   let match = true;
+  let offset = 0;
   each_index(nice_to_meet_you, function (item, index) {
     if (list_index_is(tokens, index)) {
       let current = list_get(tokens, index);
