201e8a1db0990775ee8b6bc192728e2be154f8a0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-02 19:17:42 -0400

    2025-07-02T23:17:41.533Z watch   app_contact_respond_choices

--- public/src/app_contact_respond_choices.mjs
+++ public/src/app_contact_respond_choices.mjs
@@ -45,7 +45,7 @@ export function app_contact_respond_choices() {
       response: "God let there be prayer 🙏 amen.",
     },
     {
-      tokens: ["keep", "sending", "us", "such", "scriptures", "test"],
+      tokens: ["keep", "sending", "us", "such", "scriptures"],
       response:
         "If you send me a message here 📨, then you will receive a scripture 📖.",
     },
