ff65d47bc81d00463ca1b9d6cb8161f3da668ff5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-02 13:23:16 -0400

    2025-07-02T17:23:15.991Z watch   app_share_verse_refresh

--- public/src/app_share_verse_refresh.mjs
+++ public/src/app_share_verse_refresh.mjs
@@ -166,7 +166,7 @@ export async function app_share_verse_refresh(
         "Please contact me using this link: ",
         app_url("contact"),
         "Please do not send me a message here.",
-        "I have received too many messages to reply to them all in this app.",
+        "I have received too many messages to quickly reply to them all in this app.",
       ],
     },
   ];
