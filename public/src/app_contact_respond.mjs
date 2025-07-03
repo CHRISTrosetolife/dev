0e316bd23e009ff941006bea84b058974d3fd002
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-03 14:53:35 -0400

    2025-07-03T18:53:35.117Z watch   app_contact_respond

--- public/src/app_contact_respond.mjs
+++ public/src/app_contact_respond.mjs
@@ -1,3 +1,4 @@
+import { string_empty_not_is } from "./string_empty_not_is.mjs";
 import { log } from "./log.mjs";
 import { while_change } from "./while_change.mjs";
 import { app_contact_respond_choices } from "./app_contact_respond_choices.mjs";
@@ -7,10 +8,12 @@ import { string_split_space } from "./string_split_space.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 import { list_size } from "./list_size.mjs";
+import { list_filter } from "./list_filter.mjs";
 export function app_contact_respond(input) {
   let outputs = [];
   let lower = string_case_lower(input);
   let tokens = string_split_space(lower);
+  let filtered = list_filter(tokens, string_empty_not_is);
   let choices = app_contact_respond_choices();
   let offset = 0;
   while_change(lambda);
