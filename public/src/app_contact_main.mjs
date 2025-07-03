e53da7f9ac1c7c83c72f6f28387953b2218e1d0a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-02 22:04:24 -0400

    2025-07-03T02:04:24.094Z watch   app_contact_main

--- public/src/app_contact_main.mjs
+++ public/src/app_contact_main.mjs
@@ -1,3 +1,4 @@
+import { firebase_download_bible_verse } from "./firebase_download_bible_verse.mjs";
 import { list_take_bible_books_new_context_random_vc } from "./list_take_bible_books_new_context_random_vc.mjs";
 import { app_context_books_bible } from "./app_context_books_bible.mjs";
 import { html_clear } from "./html_clear.mjs";
@@ -47,6 +48,11 @@ export async function app_contact_main() {
   html_rows_set(t, 8);
   let response;
   html_button(root, "Send message to me", async function () {
+    let text = await firebase_download_bible_verse(
+      bible_folder,
+      chapter,
+      verse_number,
+    );
     html_clear(response);
     try {
       let value = html_value_get(t);
