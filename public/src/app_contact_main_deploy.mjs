3b2b6bb44c288deceb1a1f82e132b45ee9118a25
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-04 15:10:07 -0400

    2025-07-04T19:10:07.002Z watch   app_contact_main_deploy

--- public/src/app_contact_main_deploy.mjs
+++ public/src/app_contact_main_deploy.mjs
@@ -1,5 +1,6 @@
 import { fn_name } from "./fn_name.mjs";
 import { function_deploy } from "./function_deploy.mjs";
 export async function app_contact_main_deploy() {
-  await function_deploy(fn_name("app_contact_main"));
+  let v = await function_deploy(fn_name("app_contact_main"));
+  return v;
 }
