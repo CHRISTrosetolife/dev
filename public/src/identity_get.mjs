cbdd84697f4a924ab6a1106ae9f28c98b9d07797
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 19:16:21 -0400

    2025-07-05T23:16:20.141Z watch   identity_get

--- public/src/identity_get.mjs
+++ public/src/identity_get.mjs
@@ -1,1 +1,6 @@
-export function identity_get() {}
+import { identity } from "./identity.mjs";
+import { function_get } from "./function_get.mjs";
+export function identity_get() {
+  let v = function_get(identity);
+  return v;
+}
