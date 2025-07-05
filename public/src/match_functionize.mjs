813d1092c29fefe2fa191a81c1a55ecd8ae2fa9e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 16:33:17 -0400

    2025-07-05T20:33:16.859Z watch   match_functionize

--- public/src/match_functionize.mjs
+++ public/src/match_functionize.mjs
@@ -1,13 +1,14 @@
-import { each } from "./each.mjs";
+import { list_map } from "./list_map.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { match_string } from "./match_string.mjs";
 import { string_is } from "./string_is.mjs";
 export function match_functionize(a, item, lambda) {
   assert_arguments_length(arguments, 3);
-  each(list, function (item2) {});
-  if (string_is(item)) {
-    item = match_string(item);
-  }
-  let c = item(a);
+  list_map(a, function (ai) {
+    if (string_is(item)) {
+      item = match_string(item);
+    }
+    let c = item(a);
+  });
   return c;
 }
