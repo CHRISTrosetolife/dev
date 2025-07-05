dc5de9a48859a5b92c511af44a8385e03a017900
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 12:29:44 -0400

    2025-07-05T16:29:43.675Z watch   app_contact_respond

--- public/src/app_contact_respond.mjs
+++ public/src/app_contact_respond.mjs
@@ -1,9 +1,6 @@
 import { match_fill } from "./match_fill.mjs";
 import { string_empty_not_is } from "./string_empty_not_is.mjs";
-import { while_change } from "./while_change.mjs";
 import { app_contact_respond_choices } from "./app_contact_respond_choices.mjs";
-import { each } from "./each.mjs";
-import { app_contact_respond_choice } from "./app_contact_respond_choice.mjs";
 import { string_split_space } from "./string_split_space.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { list_join_space } from "./list_join_space.mjs";
@@ -18,25 +15,6 @@ export function app_contact_respond(input) {
   let result = match_fill(tokens, choices);
   let offset = 0;
   let valid = true;
-  while_change(lambda);
-  function lambda(change) {
-    each(choices, function (choice) {
-      let {
-        offset: offset_next,
-        valid: choice_valid,
-        match,
-      } = app_contact_respond_choice(filtered, offset, outputs, choice);
-      if (match && !choice_valid) {
-        valid = choice_valid;
-      }
-      if (offset_next !== offset) {
-        change = true;
-        offset = offset_next;
-      }
-    });
-    let v = change;
-    return v;
-  }
   let finished = offset === list_size(filtered);
   let output = !finished && valid ? null : list_join_space(outputs);
   let v3 = {
