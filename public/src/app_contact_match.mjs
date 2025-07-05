2f3952a86fe61f9e93c87cbc5600bbd12a44d869
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 08:07:21 -0400

    2025-07-05T12:07:21.478Z watch   app_contact_match_generic

--- public/src/app_contact_match.mjs
+++ public/src/app_contact_match.mjs
@@ -2,14 +2,18 @@ import { list_add } from "./list_add.mjs";
 import { object_copy } from "./object_copy.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function app_contact_match(message) {
-  let v = function app_contact_match_inner(result) {
-    let data = object_property_get(result, "data");
-    let copy = object_copy(data);
-    let outputs = object_property_get(copy, "outputs");
-    lambda(outputs);
-  };
-  return v;
+  return app_contact_match_generic(lambda);
   function lambda(outputs) {
     list_add(outputs, message);
   }
 }
+function app_contact_match_generic(lambda) {
+    let v = function app_contact_match_inner(result) {
+        let data = object_property_get(result, "data");
+        let copy = object_copy(data);
+        let outputs = object_property_get(copy, "outputs");
+        lambda(outputs);
+    };
+    return v;
+}
+
---
+++ public/src/app_contact_match_generic.mjs
@@ -0,0 +1,11 @@
+import { object_copy } from "./object_copy.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+export function app_contact_match_generic(lambda) {
+  let v = function app_contact_match_inner(result) {
+    let data = object_property_get(result, "data");
+    let copy = object_copy(data);
+    let outputs = object_property_get(copy, "outputs");
+    lambda(outputs);
+  };
+  return v;
+}
