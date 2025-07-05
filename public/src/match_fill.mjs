eca9c14b6c970996c80035aa15172afdef7aadfc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 11:42:10 -0400

    2025-07-05T15:42:09.737Z watch   match_fill

--- public/src/match_fill.mjs
+++ public/src/match_fill.mjs
@@ -1,1 +1,14 @@
-export function match_fill() {}
+import { match_on } from "./match_on.mjs";
+import { match_functionize } from "./match_functionize.mjs";
+import { list_map } from "./list_map.mjs";
+export function match_fill(choices) {
+  let v = function match_choice_inner(a) {
+    let mapped = list_map(choices, function (choice) {
+      let result = match_functionize(a, choice);
+      return result;
+    });
+    let filtered = match_on(mapped, on_match);
+    return filtered;
+  };
+  return v;
+}
