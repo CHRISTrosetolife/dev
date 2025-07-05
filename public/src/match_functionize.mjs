eb8b63f2eb61a7838361e6ee797c2f957fe6a79f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 14:25:47 -0400

    2025-07-05T18:25:46.656Z watch   match_functionize

--- public/src/match_functionize.mjs
+++ public/src/match_functionize.mjs
@@ -1,6 +1,10 @@
+import { log } from "./log.mjs";
 import { match_string } from "./match_string.mjs";
 import { string_is } from "./string_is.mjs";
 export function match_functionize(a, item) {
+  log({
+    a,
+  });
   if (string_is(item)) {
     item = match_string(item);
   }
