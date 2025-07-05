fb9387512d0266e3fd80205840fec723d7798c27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 07:56:11 -0400

    2025-07-05T11:56:10.624Z c match_choice match_sequence

---
+++ public/src/match_sequence.mjs
@@ -0,0 +1,16 @@
+import { each } from "./each.mjs";
+import { list_map } from "./list_map.mjs";
+import { list_filter_property } from "./list_filter_property.mjs";
+export function match_sequence(choices) {
+  let v = function match_choice_inner(input, index, on_match) {
+    let mapped = list_map(choices, function (choice) {
+      let result = choice(input, index);
+      return result;
+    });
+    let filtered = list_filter_property(mapped, "match", true);
+    each(filtered, on_match);
+    let v2 = filtered;
+    return v2;
+  };
+  return v;
+}
