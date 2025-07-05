53215bca516c8b68568194147f639f37c63083b7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 12:31:40 -0400

    2025-07-05T16:31:39.638Z watch   app_contact_match_invalid

--- public/src/app_contact_match_invalid.mjs
+++ public/src/app_contact_match_invalid.mjs
@@ -1,9 +1,11 @@
+import { object_property_set } from "./object_property_set.mjs";
 import { app_contact_match_generic } from "./app_contact_match_generic.mjs";
 import { list_clear } from "./list_clear.mjs";
 export function app_contact_match_invalid(message) {
   let v = app_contact_match_generic(lambda, message);
   return v;
-  function lambda(outputs) {
+  function lambda(outputs, data) {
+    object_property_set(object, "property_name", value);
     list_clear(outputs);
   }
 }
