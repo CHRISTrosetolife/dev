140bacf2d534f95a36b506466a371823763580ef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 11:54:29 -0400

    2025-07-05T15:54:28.626Z watch   match_fill

--- public/src/match_fill.mjs
+++ public/src/match_fill.mjs
@@ -4,13 +4,13 @@ import { match_on } from "./match_on.mjs";
 import { match_functionize } from "./match_functionize.mjs";
 export function match_fill(choices) {
   let v = function match_choice_inner(a) {
+    let final = null;
     each(choices, function (choice) {
       let result = match_functionize(a, choice);
       let filtered = match_on([result], on_match);
       if (list_empty_not_is(filtered)) {
         match_choice_inner(result);
       }
-      return result;
     });
     return filtered;
   };
