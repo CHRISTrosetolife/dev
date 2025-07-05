ebcc5a1cbf7dcbdb5f47cdeedd764bb2ed25843f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 07:51:18 -0400

    2025-07-05T11:51:18.393Z watch   app_contact_match

--- public/src/app_contact_match.mjs
+++ public/src/app_contact_match.mjs
@@ -1,7 +1,9 @@
+import { object_copy } from "./object_copy.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function app_contact_match(message) {
   let v = function app_contact_match_inner(result) {
     let data = object_property_get(result, "data");
+    let copy = object_copy();
   };
   return v;
 }
