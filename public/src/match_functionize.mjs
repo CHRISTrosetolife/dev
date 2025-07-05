59c2b3559d42b1e0072c7b6ba48a1d620d0cfca7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 16:35:46 -0400

    2025-07-05T20:35:46.446Z watch   match_functionize

--- public/src/match_functionize.mjs
+++ public/src/match_functionize.mjs
@@ -2,8 +2,8 @@ import { list_map } from "./list_map.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { match_string } from "./match_string.mjs";
 import { string_is } from "./string_is.mjs";
-export function match_functionize(a, item, lambda) {
-  assert_arguments_length(arguments, 3);
+export function match_functionize(a, item) {
+  assert_arguments_length(arguments, 2);
   let v = list_map(a, function (ai) {
     if (string_is(item)) {
       item = match_string(item);
