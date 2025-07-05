180463700bf704e7374f67624738f437847931b6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 07:46:07 -0400

    2025-07-05T11:46:06.611Z watch   match_choice

--- public/src/match_choice.mjs
+++ public/src/match_choice.mjs
@@ -1,12 +1,14 @@
+import { each } from "./each.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_filter_property } from "./list_filter_property.mjs";
 export function match_choice(choices) {
-  let v = function match_choice_inner(input, index) {
+  let v = function match_choice_inner(input, index, on_match) {
     let mapped = list_map(choices, function (choice) {
       let result = choice(input, index);
       return result;
     });
     let filtered = list_filter_property(mapped, "match", true);
+    each(list, function (item) {});
     let v2 = filtered;
     return v2;
   };
