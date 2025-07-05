069ad31710ddf30a72544c61a329f38c0caea53c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 16:32:43 -0400

    2025-07-05T20:32:43.411Z watch   match_functionize

--- public/src/match_functionize.mjs
+++ public/src/match_functionize.mjs
@@ -1,12 +1,8 @@
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
-import { log } from "./log.mjs";
 import { match_string } from "./match_string.mjs";
 import { string_is } from "./string_is.mjs";
 export function match_functionize(a, item, lambda) {
   assert_arguments_length(arguments, 3);
-  log({
-    a,
-  });
   if (string_is(item)) {
     item = match_string(item);
   }
