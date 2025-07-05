4a6e1756c71c16e274530905c8d02d8549350f56
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-04 20:08:31 -0400

    2025-07-05T00:08:30.687Z watch   app_contact_main_open

--- public/src/app_contact_main_open.mjs
+++ public/src/app_contact_main_open.mjs
@@ -1,6 +1,6 @@
 import { function_open } from "./function_open.mjs";
 import { fn_name } from "./fn_name.mjs";
 export async function app_contact_main_open() {
-  let v = await function_open(fn_name("app_contact_main"));
+  let v = await function_open(fn_name("app_contact_respond_choices"));
   return v;
 }
