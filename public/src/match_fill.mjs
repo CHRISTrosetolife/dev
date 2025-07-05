1a917ca2dcd5ec850d6833b1b5782d26be47010f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 11:43:13 -0400

    2025-07-05T15:43:13.463Z watch   match_fill

--- public/src/match_fill.mjs
+++ public/src/match_fill.mjs
@@ -1,8 +1,10 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { match_on } from "./match_on.mjs";
 import { match_functionize } from "./match_functionize.mjs";
 import { list_map } from "./list_map.mjs";
 export function match_fill(choices) {
   let v = function match_choice_inner(a) {
+    let input = object_property_get(a, "input");
     let mapped = list_map(choices, function (choice) {
       let result = match_functionize(a, choice);
       return result;
