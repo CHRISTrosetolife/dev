f23b6c90a6092e5079456a6241b7bb13a64b954e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 07:42:35 -0400

    2025-07-05T11:42:35.416Z watch   match_choice

--- public/src/match_choice.mjs
+++ public/src/match_choice.mjs
@@ -2,7 +2,9 @@ import { list_filter } from "./list_filter.mjs";
 export function match_choice(choices) {
   let v = function match_choice_inner(input, index) {
     list_filter(choices, function (choice) {
-      choice(input, index);
+      let result = choice(input, index);
+      let { match } = result;
+      return;
     });
   };
   return v;
