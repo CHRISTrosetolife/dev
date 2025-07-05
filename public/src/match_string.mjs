1341dd974ad1b6e56df7d8a18b8af478be06164d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 16:40:13 -0400

    2025-07-05T20:40:13.249Z watch   match_string

--- public/src/match_string.mjs
+++ public/src/match_string.mjs
@@ -3,8 +3,11 @@ import { object_property_increment } from "./object_property_increment.mjs";
 import { list_get } from "./list_get.mjs";
 import { object_copy } from "./object_copy.mjs";
 import { object_property_get } from "./object_property_get.mjs";
+import { list_map } from "./list_map.mjs";
 export function match_string(s) {
   let v = function match_string_inner(a) {
+    let v3 = list_map(a, $a$ai);
+    return v3;
     let copy = object_copy(a);
     let tokens = object_property_get(copy, "tokens");
     let index = object_property_get(copy, "index");
