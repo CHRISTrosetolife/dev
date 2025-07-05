ffa207d2fca1e5d565b4be029eba2fd2675b085d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 12:21:25 -0400

    2025-07-05T16:21:24.644Z watch   app_contact_match_generic

--- public/src/app_contact_match_generic.mjs
+++ public/src/app_contact_match_generic.mjs
@@ -1,6 +1,5 @@
-import { object_property_set } from "./object_property_set.mjs";
+import { app_contact_match_data_replace } from "./app_contact_match_data_replace.mjs";
 import { list_add } from "./list_add.mjs";
-import { object_copy } from "./object_copy.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function app_contact_match_generic(lambda, message) {
   let v = function app_contact_match_inner(result) {
@@ -11,10 +10,3 @@ export function app_contact_match_generic(lambda, message) {
   };
   return v;
 }
-function app_contact_match_data_replace(result) {
-    let data = object_property_get(result, "data");
-    let copy = object_copy(data);
-    object_property_set(result, "data", copy);
-    return copy;
-}
-
