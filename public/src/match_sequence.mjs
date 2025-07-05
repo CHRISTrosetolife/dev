b7111cce062d3c97f906c51319f3f573dd99541c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 07:56:19 -0400

    2025-07-05T11:56:18.651Z watch   match_sequence

--- public/src/match_sequence.mjs
+++ public/src/match_sequence.mjs
@@ -2,7 +2,7 @@ import { each } from "./each.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_filter_property } from "./list_filter_property.mjs";
 export function match_sequence(choices) {
-  let v = function match_choice_inner(input, index, on_match) {
+  let v = function match_sequence_inner(input, index, on_match) {
     let mapped = list_map(choices, function (choice) {
       let result = choice(input, index);
       return result;
