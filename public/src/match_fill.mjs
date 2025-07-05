9bfbef70b5d7c7b291018b7272326a939538ee97
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 12:29:11 -0400

    2025-07-05T16:29:10.446Z watch   match_fill

--- public/src/match_fill.mjs
+++ public/src/match_fill.mjs
@@ -6,31 +6,28 @@ import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { each } from "./each.mjs";
 import { match_on } from "./match_on.mjs";
 import { match_functionize } from "./match_functionize.mjs";
-export function match_fill(choices) {
-  let v = function match_choice_inner(tokens) {
-    let a = {
-      tokens,
-      index: 0,
-    };
-    object_property_set(a, "match", false);
-    let result = a;
-    let final = result;
-    each(choices, function (choice) {
-      result = match_functionize(result, choice);
-      let filtered = match_on([result], noop);
-      let filtered_empty_not = list_empty_not_is(filtered);
-      if (filtered_empty_not) {
-        match_choice_inner(result);
-        let tokens = object_property_get(result, "tokens");
-        let index = object_property_get(result, "index");
-        let tokens_size = list_size(tokens);
-        if (index === tokens_size + 1) {
-          object_property_set(result, "match", true);
-          final = result;
-        }
-      }
-    });
-    return final;
+export function match_fill(choices, tokens) {
+  let a = {
+    tokens,
+    index: 0,
   };
-  return v;
+  object_property_set(a, "match", false);
+  let result = a;
+  let final = result;
+  each(choices, function (choice) {
+    result = match_functionize(result, choice);
+    let filtered = match_on([result], noop);
+    let filtered_empty_not = list_empty_not_is(filtered);
+    if (filtered_empty_not) {
+      match_choice_inner(result);
+      let tokens = object_property_get(result, "tokens");
+      let index = object_property_get(result, "index");
+      let tokens_size = list_size(tokens);
+      if (index === tokens_size + 1) {
+        object_property_set(result, "match", true);
+        final = result;
+      }
+    }
+  });
+  return final;
 }
