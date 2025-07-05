1a94d0a0440f333cce1c5aa9a708d357f3401226
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 16:35:30 -0400

    2025-07-05T20:35:29.928Z watch   match_choice

--- public/src/match_choice.mjs
+++ public/src/match_choice.mjs
@@ -1,12 +1,9 @@
-import { list_concat } from "./list_concat.mjs";
+import { list_concat_multiple } from "./list_concat_multiple.mjs";
 import { log } from "./log.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
-import { list_single } from "./list_single.mjs";
 import { match_functionize } from "./match_functionize.mjs";
 import { match_on } from "./match_on.mjs";
 import { list_map } from "./list_map.mjs";
-import { list_filter } from "./list_filter.mjs";
-import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export function match_choice(choices, on_match) {
   assert_arguments_length(arguments, 2);
   let v = function match_choice_inner(a) {
@@ -14,9 +11,8 @@ export function match_choice(choices, on_match) {
       let result = match_functionize(a, choice);
       return result;
     });
-    let filtered2 = list_filter(mapped, list_empty_not_is);
-    let mapped2 = list_concat(filtered2, list_single);
-    let filtered = match_on(mapped2, on_match);
+    let c = list_concat_multiple(mapped);
+    let filtered = match_on(c, on_match);
     log({
       filtered,
       choices,
