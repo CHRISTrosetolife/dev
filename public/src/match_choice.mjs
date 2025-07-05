61db6810da489962267b1a6013517dd6fb127ba8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 08:11:52 -0400

    2025-07-05T12:11:52.405Z watch   match_choice

--- public/src/match_choice.mjs
+++ public/src/match_choice.mjs
@@ -1,9 +1,10 @@
+import { string_is } from "./string_is.mjs";
 import { match_on } from "./match_on.mjs";
 import { list_map } from "./list_map.mjs";
 export function match_choice(choices) {
   let v = function match_choice_inner(a, on_match) {
     let mapped = list_map(choices, function (choice) {
-      if (false) {
+      if (string_is(choice)) {
       }
       let result = choice(a);
       return result;
