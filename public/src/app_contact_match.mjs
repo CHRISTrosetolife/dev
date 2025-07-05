d804388af15db86f21025bf8c1d7bd4874cc34b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 08:07:27 -0400

    2025-07-05T12:07:27.211Z watch   app_contact_match

--- public/src/app_contact_match.mjs
+++ public/src/app_contact_match.mjs
@@ -1,19 +1,9 @@
+import { app_contact_match_generic } from "./app_contact_match_generic.mjs";
 import { list_add } from "./list_add.mjs";
-import { object_copy } from "./object_copy.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 export function app_contact_match(message) {
-  return app_contact_match_generic(lambda);
+  let v = app_contact_match_generic(lambda);
+  return v;
   function lambda(outputs) {
     list_add(outputs, message);
   }
 }
-function app_contact_match_generic(lambda) {
-    let v = function app_contact_match_inner(result) {
-        let data = object_property_get(result, "data");
-        let copy = object_copy(data);
-        let outputs = object_property_get(copy, "outputs");
-        lambda(outputs);
-    };
-    return v;
-}
-
