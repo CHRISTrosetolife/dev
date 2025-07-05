06ba2cde581efb2ba257d84789dd0af73dd94064
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 13:49:31 -0400

    2025-07-05T17:49:30.600Z watch   match_sequence

--- public/src/match_sequence.mjs
+++ public/src/match_sequence.mjs
@@ -2,12 +2,13 @@ import { match_functionize } from "./match_functionize.mjs";
 import { match_on } from "./match_on.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
+import { list_empty_is } from "./list_empty_is.mjs";
 export function match_sequence(steps, on_match) {
   let v = function match_sequence_inner(a) {
     let result = a;
     each(steps, function (step) {
       result = match_functionize(result, step);
-      if (!match) {
+      if (list_empty_is(result)) {
         let v3 = true;
         return v3;
       }
