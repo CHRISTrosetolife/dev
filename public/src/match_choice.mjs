48ebd822f89d9d1fa0b02fb6ddea8cac775753ce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 07:43:40 -0400

    2025-07-05T11:43:40.406Z watch   match_choice

--- public/src/match_choice.mjs
+++ public/src/match_choice.mjs
@@ -1,11 +1,12 @@
-import { list_filter } from "./list_filter.mjs";
+import { list_map } from "./list_map.mjs";
+import { list_filter_property } from "./list_filter_property.mjs";
 export function match_choice(choices) {
   let v = function match_choice_inner(input, index) {
-    list_filter(choices, function (choice) {
+    let mapped = list_map(choices, function (choice) {
       let result = choice(input, index);
-      let { match } = result;
-      return match;
+      return result;
     });
+    let filtered = list_filter_property(mapped);
   };
   return v;
 }
