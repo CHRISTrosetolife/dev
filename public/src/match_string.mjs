2f5e33f550872d0d0d7f37c9a0a6d2b83112b92a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 08:59:10 -0400

    2025-07-05T12:59:10.171Z watch   match_string

--- public/src/match_string.mjs
+++ public/src/match_string.mjs
@@ -1,6 +1,8 @@
+import { object_copy } from "./object_copy.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function match_string(s) {
   let v = function match_string_inner(a) {
+    let copy = object_copy(a);
     let input = object_property_get(a, "input");
     let index = object_property_get(a, "index");
     return filtered;
