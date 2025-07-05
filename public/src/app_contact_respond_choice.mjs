b536bf434a65434e2baf923dbdce4cadf8c2a6d3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-04 20:11:32 -0400

    2025-07-05T00:11:31.985Z watch   app_contact_respond_choice

--- public/src/app_contact_respond_choice.mjs
+++ public/src/app_contact_respond_choice.mjs
@@ -3,12 +3,14 @@ import { list_add } from "./list_add.mjs";
 import { list_get } from "./list_get.mjs";
 import { list_index_is } from "./list_index_is.mjs";
 import { each_index } from "./each_index.mjs";
+import { string_case_lower } from "./string_case_lower.mjs";
 export function app_contact_respond_choice(tokens, offset, outputs, choice) {
   let choice_tokens = object_property_get(choice, "tokens");
   let choice_response = object_property_get(choice, "response");
   let match = true;
   let offset_next = null;
   each_index(choice_tokens, function (item, index) {
+    item = string_case_lower(item);
     let ti = offset + index;
     if (list_index_is(tokens, ti)) {
       let current = list_get(tokens, ti);
