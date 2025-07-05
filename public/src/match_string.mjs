4710e04f3835cbcb51f9ec8c4d72cada0c8719fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 17:44:15 -0400

    2025-07-05T21:44:13.626Z watch   match_string

--- public/src/match_string.mjs
+++ public/src/match_string.mjs
@@ -4,7 +4,9 @@ import { list_get } from "./list_get.mjs";
 import { object_copy } from "./object_copy.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_map } from "./list_map.mjs";
+import { string_case_lower } from "./string_case_lower.mjs";
 export function match_string(s) {
+  s = string_case_lower(s);
   let v = function match_string_inner(a) {
     let v3 = list_map(a, function (ai) {
       let copy = object_copy(ai);
