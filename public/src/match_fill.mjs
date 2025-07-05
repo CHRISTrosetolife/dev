883e604e82392d7cbfc351d5315cfd5eee58144c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 11:50:00 -0400

    2025-07-05T15:49:59.439Z watch   match_fill

--- public/src/match_fill.mjs
+++ public/src/match_fill.mjs
@@ -1,14 +1,10 @@
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { each } from "./each.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { match_on } from "./match_on.mjs";
 import { match_functionize } from "./match_functionize.mjs";
 import { object_copy } from "./object_copy.mjs";
 export function match_fill(choices) {
   let v = function match_choice_inner(a) {
-    let copy = object_copy(a);
-    let input = object_property_get(copy, "input");
-    let index = object_property_get(copy, "index");
     each(choices, function (choice) {
       let result = match_functionize(a, choice);
       let filtered = match_on([result], on_match);
