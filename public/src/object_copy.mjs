a03ec0f5bbdd3f4eab6f5299ba27572e80d9ada9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 19:28:23 -0400

    2025-07-05T23:28:22.558Z watch   object_copy

--- public/src/object_copy.mjs
+++ public/src/object_copy.mjs
@@ -1,5 +1,7 @@
+import { json_from } from "./json_from.mjs";
 import { json_to } from "./json_to.mjs";
 export function object_copy(object) {
-  let v = json_to(copy);
+  let json = json_to(copy);
+  let v = json_from(json);
   return v;
 }
