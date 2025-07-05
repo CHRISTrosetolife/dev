669f074ed73e2f68f599968b3f23aa1c0c92e6a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 12:26:41 -0400

    2025-07-05T16:26:41.209Z watch   match_string

--- public/src/match_string.mjs
+++ public/src/match_string.mjs
@@ -5,9 +5,9 @@ import { object_property_get } from "./object_property_get.mjs";
 export function match_string(s) {
   let v = function match_string_inner(a) {
     let copy = object_copy(a);
-    let input = object_property_get(copy, "input");
+    let tokens = object_property_get(copy, "tokens");
     let index = object_property_get(copy, "index");
-    let i = list_get(input, index);
+    let i = list_get(tokens, index);
     let match = i === s;
     if (match) {
       object_property_increment(copy, "index");
