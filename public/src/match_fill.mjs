f11aefc4ce3353c7c17d9d2fb42afa1ce48606f9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 11:54:59 -0400

    2025-07-05T15:54:58.901Z watch   match_fill

--- public/src/match_fill.mjs
+++ public/src/match_fill.mjs
@@ -4,7 +4,7 @@ import { match_on } from "./match_on.mjs";
 import { match_functionize } from "./match_functionize.mjs";
 export function match_fill(choices) {
   let v = function match_choice_inner(a) {
-    let final = null;
+    let result;
     each(choices, function (choice) {
       let result = match_functionize(a, choice);
       let filtered = match_on([result], on_match);
