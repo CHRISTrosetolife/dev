f151ae829ace8b329303235eed296e1e43148783
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 11:44:36 -0400

    2025-07-05T15:44:35.573Z watch   match_fill

--- public/src/match_fill.mjs
+++ public/src/match_fill.mjs
@@ -1,3 +1,6 @@
+import { app_contact_respond_choice } from "./app_contact_respond_choice.mjs";
+import { each } from "./each.mjs";
+import { while_change } from "./while_change.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { match_on } from "./match_on.mjs";
 import { match_functionize } from "./match_functionize.mjs";
@@ -6,6 +9,25 @@ export function match_fill(choices) {
   let v = function match_choice_inner(a) {
     let input = object_property_get(a, "input");
     let index = object_property_get(a, "index");
+    while_change(lambda);
+    function lambda(change) {
+      each(choices, function (choice) {
+        let {
+          offset: offset_next,
+          valid: choice_valid,
+          match,
+        } = app_contact_respond_choice(filtered, offset, outputs, choice);
+        if (match && !choice_valid) {
+          let valid = choice_valid;
+        }
+        if (offset_next !== offset) {
+          change = true;
+          let offset = offset_next;
+        }
+      });
+      let v = change;
+      return v;
+    }
     let mapped = list_map(choices, function (choice) {
       let result = match_functionize(a, choice);
       return result;
