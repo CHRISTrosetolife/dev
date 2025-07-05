04e26bf316da294bbf29e778feef60345da9c5c0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 08:12:34 -0400

    2025-07-05T12:12:33.815Z c match_choice match_string

---
+++ public/src/match_string.mjs
@@ -0,0 +1,17 @@
+import { string_is } from "./string_is.mjs";
+import { match_on } from "./match_on.mjs";
+import { list_map } from "./list_map.mjs";
+export function match_string(choices) {
+  let v = function match_choice_inner(a, on_match) {
+    let mapped = list_map(choices, function (choice) {
+      if (string_is(choice)) {
+        choice = match_string(choice);
+      }
+      let result = choice(a);
+      return result;
+    });
+    let filtered = match_on(mapped, on_match);
+    return filtered;
+  };
+  return v;
+}
