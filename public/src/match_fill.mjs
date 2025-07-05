0f228fe68eccfb93dc2b86b05ad58e14e64ebf69
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 11:49:54 -0400

    2025-07-05T15:49:54.177Z watch   match_fill

--- public/src/match_fill.mjs
+++ public/src/match_fill.mjs
@@ -1,3 +1,4 @@
+import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { each } from "./each.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { match_on } from "./match_on.mjs";
@@ -11,7 +12,7 @@ export function match_fill(choices) {
     each(choices, function (choice) {
       let result = match_functionize(a, choice);
       let filtered = match_on([result], on_match);
-      if (false) {
+      if (list_empty_not_is(filtered)) {
       }
       return result;
     });
