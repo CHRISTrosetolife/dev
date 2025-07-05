df047cbeaee6c14c8a09f23966c0f3df6ede29dd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 08:13:03 -0400

    2025-07-05T12:13:02.927Z watch   match_string

--- public/src/match_string.mjs
+++ public/src/match_string.mjs
@@ -1,8 +1,8 @@
 import { string_is } from "./string_is.mjs";
 import { match_on } from "./match_on.mjs";
 import { list_map } from "./list_map.mjs";
-export function match_string(choices) {
-  let v = function match_choice_inner(a, on_match) {
+export function match_string(s) {
+  let v = function match_string_inner(a, on_match) {
     let mapped = list_map(choices, function (choice) {
       if (string_is(choice)) {
         choice = match_string(choice);
