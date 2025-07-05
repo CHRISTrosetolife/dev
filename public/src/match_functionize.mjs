24681eb96fb94dda9f3d4cc2ee553c74cf4c6cf4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 12:45:18 -0400

    2025-07-05T16:45:17.568Z watch   match_functionize

--- public/src/match_functionize.mjs
+++ public/src/match_functionize.mjs
@@ -1,5 +1,3 @@
-import { log } from "./log.mjs";
-import { list_single } from "./list_single.mjs";
 import { match_string } from "./match_string.mjs";
 import { string_is } from "./string_is.mjs";
 export function match_functionize(a, item) {
@@ -7,10 +5,5 @@ export function match_functionize(a, item) {
     item = match_string(item);
   }
   let c = item(a);
-  log({
-    item,
-    c,
-  });
-  let result = list_single(c);
-  return result;
+  return c;
 }
