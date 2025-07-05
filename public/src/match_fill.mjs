60b9a4e13d3e355d83c3a03ff37a16e333a56600
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-05 17:45:02 -0400

    2025-07-05T21:45:01.389Z watch   match_fill

--- public/src/match_fill.mjs
+++ public/src/match_fill.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { list_size } from "./list_size.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -26,9 +25,6 @@ export function match_fill(tokens, choices) {
     each(choices, function (choice) {
       let c = match_functionize(a, choice);
       let filtered = match_on(c, noop);
-      log({
-        filtered,
-      });
       each(filtered, function (f) {
         let tokens = object_property_get(f, "tokens");
         let index = object_property_get(f, "index");
