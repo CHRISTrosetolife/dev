9915d8825149571e486bfe6f42473b7e20a9b70b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 15:36:53 -0400

    2025-07-06T19:36:52.946Z watch   js_terminal_to_string_split

--- public/src/js_terminal_to_string_split.mjs
+++ public/src/js_terminal_to_string_split.mjs
@@ -1,12 +1,7 @@
+import { string_split_space } from "./string_split_space.mjs";
 import { js_terminal_to_string } from "./js_terminal_to_string.mjs";
-import { string_delimit } from "./string_delimit.mjs";
-import { string_double } from "./string_double.mjs";
-import { terminal_tokens_quote } from "./terminal_tokens_quote.mjs";
-import { string_replace } from "./string_replace.mjs";
 export async function js_terminal_to_string_split() {
   let value = await js_terminal_to_string();
-  let q = terminal_tokens_quote();
-  let replaced = string_replace(value, q, string_double(q));
-  let v = string_delimit(replaced);
+  string_split_space();
   return v;
 }
