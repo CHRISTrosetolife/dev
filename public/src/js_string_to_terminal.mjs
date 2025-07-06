3269bc9e75a5062eaf420751ec5c569a1a61d892
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 14:01:01 -0400

    2025-07-06T18:01:01.213Z watch   js_string_to_terminal

--- public/src/js_string_to_terminal.mjs
+++ public/src/js_string_to_terminal.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { string_delimit } from "./string_delimit.mjs";
 import { string_double } from "./string_double.mjs";
@@ -5,6 +6,7 @@ import { terminal_tokens_quote } from "./terminal_tokens_quote.mjs";
 import { string_replace } from "./string_replace.mjs";
 export function js_string_to_terminal(code) {
   let parsed = js_parse_expression(code);
+  let value = object_property_get(parsed, "value");
   let q = terminal_tokens_quote();
   let replaced = string_replace(code, q, string_double(q));
   let v = string_delimit(replaced);
