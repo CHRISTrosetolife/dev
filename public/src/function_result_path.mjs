0a2207c5a16f3e8a2ae9dc741f618df6dd6c019e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 18:38:29 -0400

    2025-07-06T22:38:27.124Z r function_deploy_result function_result_path

--- public/src/function_result_path.mjs
+++ public/src/function_result_path.mjs
@@ -1,7 +1,16 @@
-import { file_name_json } from "./file_name_json.mjs";
-import { storage_path_function } from "./storage_path_function.mjs";
-import { path_join } from "./path_join.mjs";
-export function function_result_path(f_name) {
-  let v = path_join([storage_path_function("result"), file_name_json(f_name)]);
-  return v;
+import { function_run } from "./function_run.mjs";
+import { storage_upload_object_force } from "./storage_upload_object_force.mjs";
+import { function_deploy_path_version } from "./function_deploy_path_version.mjs";
+import { function_deploy_path } from "./function_deploy_path.mjs";
+import { storage_upload_string } from "./storage_upload_string.mjs";
+export async function function_result_path(f_name) {
+  await function_run();
+  await storage_upload_string(code, function_deploy_path(f_name, version));
+  await storage_upload_object_force(
+    {
+      name: f_name,
+      version,
+    },
+    function_deploy_path_version(f_name),
+  );
 }
