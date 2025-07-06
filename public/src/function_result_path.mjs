fb754994abf729d0a47eab78a9dd58c515c7c711
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 18:41:37 -0400

    2025-07-06T22:41:35.456Z watch   function_result_path

--- public/src/function_result_path.mjs
+++ public/src/function_result_path.mjs
@@ -1,7 +1,7 @@
+import { file_name_json } from "./file_name_json.mjs";
 import { storage_path_function } from "./storage_path_function.mjs";
-import { file_name_js } from "./file_name_js.mjs";
 import { path_join } from "./path_join.mjs";
 export function function_result_path(name) {
-  let v = path_join([storage_path_function("result"), file_name_js(name)]);
+  let v = path_join([storage_path_function("result"), file_name_json(name)]);
   return v;
 }
