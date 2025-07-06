0bb4b64c3c72b02879095adf0bf8edfc07d20e72
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 18:35:51 -0400

    2025-07-06T22:35:51.083Z watch   function_deploy_result

--- public/src/function_deploy_result.mjs
+++ public/src/function_deploy_result.mjs
@@ -1,18 +1,14 @@
 import { storage_upload_object_force } from "./storage_upload_object_force.mjs";
 import { function_deploy_path_version } from "./function_deploy_path_version.mjs";
 import { function_deploy_path } from "./function_deploy_path.mjs";
-import { date_string_iso_file } from "./date_string_iso_file.mjs";
 import { storage_upload_string } from "./storage_upload_string.mjs";
-import { function_dependencies_string } from "./function_dependencies_string.mjs";
-export async function function_deploy_result(name) {
-  let code = await function_dependencies_string(name);
-  let version = date_string_iso_file();
-  await storage_upload_string(code, function_deploy_path(name, version));
+export async function function_deploy_result(f_name) {
+  await storage_upload_string(code, function_deploy_path(f_name, version));
   await storage_upload_object_force(
     {
-      name,
+      name: f_name,
       version,
     },
-    function_deploy_path_version(name),
+    function_deploy_path_version(f_name),
   );
 }
