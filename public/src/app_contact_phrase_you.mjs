33bc01fbf0fa8ee09dd9df7ee77e1c1f0e55bc79
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 17:39:51 -0400

    2025-07-06T21:39:51.133Z watch   app_contact_phrase_you

--- public/src/app_contact_phrase_you.mjs
+++ public/src/app_contact_phrase_you.mjs
@@ -1,10 +1,11 @@
 import { match_optional } from "./match_optional.mjs";
 import { match_sequence } from "./match_sequence.mjs";
 export function app_contact_phrase_you() {
-  return match_sequence([
+  let v = match_sequence([
     "you",
-    match_optional([
+    match_optional(
       match_sequence(["servant", "of", "the", "most", "high", "god"]),
-    ]),
+    ),
   ]);
+  return v;
 }
