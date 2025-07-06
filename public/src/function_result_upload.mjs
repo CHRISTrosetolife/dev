119f585dc64c0c252bded9a2764b78af9fe93ffb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 18:41:53 -0400

    2025-07-06T22:41:52.678Z watch   function_result_upload

--- public/src/function_result_upload.mjs
+++ public/src/function_result_upload.mjs
@@ -1,7 +1,7 @@
+import { function_result_path } from "./function_result_path.mjs";
 import { function_run } from "./function_run.mjs";
 import { storage_upload_object_force } from "./storage_upload_object_force.mjs";
-import { function_deploy_path } from "./function_deploy_path.mjs";
 export async function function_result_upload(f_name) {
   let result = await function_run(f_name);
-  await storage_upload_object_force(result, function_deploy_path(f_name));
+  await storage_upload_object_force(result, function_result_path(f_name));
 }
