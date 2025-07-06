8a752d37d421510265cb8684e15464bb0f96bbf6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 17:22:42 -0400

    2025-07-06T21:22:42.028Z watch   match_on

--- public/src/match_on.mjs
+++ public/src/match_on.mjs
@@ -1,6 +1,6 @@
 import { each } from "./each.mjs";
 export function match_on(match_fn, on_match) {
-  let v = function (a) {
+  let v = function match_on_inner(a) {
     a = match_fn(a);
     each(filtered, on_match);
   };
