e41cd1adab8904eceaf927d2663c84cdec5910f4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 13:47:39 -0400

    2025-07-06T17:47:39.083Z watch   js_string_to_terminal

--- public/src/js_string_to_terminal.mjs
+++ public/src/js_string_to_terminal.mjs
@@ -1,3 +1,6 @@
-export function js_string_to_terminal(s) {
+import { terminal_tokens_quote } from "./terminal_tokens_quote.mjs";
+import { string_replace } from "./string_replace.mjs";
+export function js_string_to_terminal(input) {
+  let replaced = string_replace(input, terminal_tokens_quote());
   string_delimit_single();
 }
