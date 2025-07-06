7ae444d39a977d557133f924175ada6bd116a099
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 13:48:11 -0400

    2025-07-06T17:48:11.233Z watch   js_string_to_terminal

--- public/src/js_string_to_terminal.mjs
+++ public/src/js_string_to_terminal.mjs
@@ -2,10 +2,8 @@ import { string_double } from "./string_double.mjs";
 import { terminal_tokens_quote } from "./terminal_tokens_quote.mjs";
 import { string_replace } from "./string_replace.mjs";
 export function js_string_to_terminal(input) {
-  let replaced = string_replace(
-    input,
-    terminal_tokens_quote(),
-    string_double(terminal_tokens_quote()),
-  );
-  string_delimit_single();
+  let q = terminal_tokens_quote();
+  let replaced = string_replace(input, q, string_double(q));
+  let v = string_delimit_single(replaced);
+  return v;
 }
