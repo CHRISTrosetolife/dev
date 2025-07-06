eeb0867df2c58269460daf658534002a71c8ca10
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 18:36:55 -0400

    2025-07-06T22:36:55.091Z watch   function_deploy_result

--- public/src/function_deploy_result.mjs
+++ public/src/function_deploy_result.mjs
@@ -1,8 +1,10 @@
+import { function_run } from "./function_run.mjs";
 import { storage_upload_object_force } from "./storage_upload_object_force.mjs";
 import { function_deploy_path_version } from "./function_deploy_path_version.mjs";
 import { function_deploy_path } from "./function_deploy_path.mjs";
 import { storage_upload_string } from "./storage_upload_string.mjs";
 export async function function_deploy_result(f_name) {
+  await function_run();
   await storage_upload_string(code, function_deploy_path(f_name, version));
   await storage_upload_object_force(
     {
