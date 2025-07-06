453872ca03a5daa1df7ba988ca387151f1cdf6ea
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 17:25:43 -0400

    2025-07-06T21:25:42.409Z watch   app_contact_phrase_jesus

--- public/src/app_contact_phrase_jesus.mjs
+++ public/src/app_contact_phrase_jesus.mjs
@@ -1,7 +1,6 @@
-import { noop } from "./noop.mjs";
 import { match_optional } from "./match_optional.mjs";
 import { match_sequence } from "./match_sequence.mjs";
 export function app_contact_phrase_jesus() {
-  let v = match_sequence(["Jesus", match_optional("Christ")], noop);
+  let v = match_sequence(["Jesus", match_optional("Christ")]);
   return v;
 }
