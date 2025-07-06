53577eeb37a929f9c9535472e67426305f43a6c7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 16:33:03 -0400

    2025-07-06T20:33:02.555Z watch   app_contact_respond

--- public/src/app_contact_respond.mjs
+++ public/src/app_contact_respond.mjs
@@ -7,9 +7,11 @@ import { string_split_space } from "./string_split_space.mjs";
 import { string_case_lower } from "./string_case_lower.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 import { list_filter } from "./list_filter.mjs";
+import { list_add } from "./list_add.mjs";
 export function app_contact_respond(input) {
   let lower = string_case_lower(input);
   let letters = keyboard_keys();
+  list_add(letters, "'");
   let tokens = string_split_space(lower);
   let filtered = list_filter(tokens, string_empty_not_is);
   let choices = app_contact_respond_choices();
