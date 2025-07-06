e2f8c72ed1e1dbff6e3cd92b00e29d49d3a178bf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 15:35:10 -0400

    2025-07-06T19:35:09.519Z watch   js_string_to_terminal

--- public/src/js_string_to_terminal.mjs
+++ public/src/js_string_to_terminal.mjs
@@ -1,3 +1,4 @@
+import { line_read } from "./line_read.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { string_delimit } from "./string_delimit.mjs";
@@ -6,20 +7,7 @@ import { terminal_tokens_quote } from "./terminal_tokens_quote.mjs";
 import { string_replace } from "./string_replace.mjs";
 import readline from "readline";
 export async function js_string_to_terminal() {
-  function waitForInput() {
-    let result = new Promise(function (resolve) {
-      let rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-      });
-      rl.question("", function (answer) {
-        rl.close();
-        resolve(answer);
-      });
-    });
-    return result;
-  }
-  let code = await waitForInput();
+  let code = await line_read();
   let parsed = js_parse_expression(code);
   let value = object_property_get(parsed, "value");
   let q = terminal_tokens_quote();
---
+++ public/src/line_read.mjs
@@ -0,0 +1,13 @@
+export function line_read() {
+  let result = new Promise(function (resolve) {
+    let rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl.question("", function (answer) {
+      rl.close();
+      resolve(answer);
+    });
+  });
+  return result;
+}
