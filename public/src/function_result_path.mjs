5c790401f156cd855c0c5287e0b6cd3893f41458
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 18:38:52 -0400

    2025-07-06T22:38:51.183Z watch   function_result_path

--- public/src/function_result_path.mjs
+++ public/src/function_result_path.mjs
@@ -1,16 +1,7 @@
 import { function_run } from "./function_run.mjs";
 import { storage_upload_object_force } from "./storage_upload_object_force.mjs";
-import { function_deploy_path_version } from "./function_deploy_path_version.mjs";
 import { function_deploy_path } from "./function_deploy_path.mjs";
-import { storage_upload_string } from "./storage_upload_string.mjs";
 export async function function_result_path(f_name) {
-  await function_run();
-  await storage_upload_string(code, function_deploy_path(f_name, version));
-  await storage_upload_object_force(
-    {
-      name: f_name,
-      version,
-    },
-    function_deploy_path_version(f_name),
-  );
+  let result = await function_run(f_name);
+  await storage_upload_object_force(result, function_deploy_path(f_name));
 }
