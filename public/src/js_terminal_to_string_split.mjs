7da8d9ccaf3c5db40501bd747d2b57fdd88e826d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 15:37:02 -0400

    2025-07-06T19:37:01.468Z watch   js_terminal_to_string_split

--- public/src/js_terminal_to_string_split.mjs
+++ public/src/js_terminal_to_string_split.mjs
@@ -2,6 +2,6 @@ import { string_split_space } from "./string_split_space.mjs";
 import { js_terminal_to_string } from "./js_terminal_to_string.mjs";
 export async function js_terminal_to_string_split() {
   let value = await js_terminal_to_string();
-  string_split_space();
+  let s = string_split_space(value);
   return v;
 }
