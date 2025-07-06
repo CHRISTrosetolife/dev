26817fc2d963cf237ca5081181f9ab599f6db6f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 18:44:36 -0400

    2025-07-06T22:44:36.258Z watch   english_words_dictionary_object

--- public/src/english_words_dictionary_object.mjs
+++ public/src/english_words_dictionary_object.mjs
@@ -1,4 +1,5 @@
 import { english_words_dictionary } from "./english_words_dictionary.mjs";
 export async function english_words_dictionary_object() {
-  return await english_words_dictionary();
+  let v = await english_words_dictionary();
+  return v;
 }
