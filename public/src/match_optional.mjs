621bf328ab82e87c28653c386573141bfb6c0afc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 17:39:23 -0400

    2025-07-06T21:39:23.319Z watch   match_optional

--- public/src/match_optional.mjs
+++ public/src/match_optional.mjs
@@ -1,7 +1,9 @@
+import { never } from "./never.mjs";
+import { assert } from "./assert.mjs";
 import { identity } from "./identity.mjs";
 import { match_choice } from "./match_choice.mjs";
 export function match_optional(input) {
-  $asn;
+  assert(never, []);
   let v = match_choice([input, identity]);
   return v;
 }
