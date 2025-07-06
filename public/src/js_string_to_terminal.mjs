7040b019807dd34d2d398bf7976607232a1c580d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 13:54:44 -0400

    2025-07-06T17:54:43.324Z watch   js_string_to_terminal

--- public/src/js_string_to_terminal.mjs
+++ public/src/js_string_to_terminal.mjs
@@ -4,7 +4,7 @@ import { string_double } from "./string_double.mjs";
 import { terminal_tokens_quote } from "./terminal_tokens_quote.mjs";
 import { string_replace } from "./string_replace.mjs";
 export function js_string_to_terminal(code) {
-  js_parse_expression(code);
+  let parsed = js_parse_expression(code);
   let q = terminal_tokens_quote();
   let replaced = string_replace(code, q, string_double(q));
   let v = string_delimit(replaced);
