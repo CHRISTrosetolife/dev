fbe1a1557becd3d2785700c951a2a18830af75f5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 17:27:38 -0400

    2025-07-06T21:27:37.624Z watch   list_add_multiple

--- public/src/list_add_multiple.mjs
+++ public/src/list_add_multiple.mjs
@@ -1,8 +1,10 @@
+import { each } from "./each.mjs";
 import { never } from "./never.mjs";
 import { assert } from "./assert.mjs";
 import { list_add } from "./list_add.mjs";
 export function list_add_multiple(list, other) {
   assert(never, []);
+  each(list2, function (item) {});
   for (let o of other) {
     list_add(list, o);
   }
