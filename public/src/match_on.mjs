dad132ea460c9769be042ca3ba5b41a68e0cf105
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 17:21:56 -0400

    2025-07-06T21:21:55.888Z watch   match_on

--- public/src/match_on.mjs
+++ public/src/match_on.mjs
@@ -1,6 +1,8 @@
 import { match_filter } from "./match_filter.mjs";
 import { each } from "./each.mjs";
-export function match_on(mapped, on_match) {
+export function match_on(match_fn, on_match) {
+  let v = $a;
+  return v; 
   let filtered = match_filter(mapped);
   each(filtered, on_match);
   return filtered;
