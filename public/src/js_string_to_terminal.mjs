ce3dc701a353ce415ce6e8bf6c686f6057b17bc1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 15:29:02 -0400

    2025-07-06T19:29:01.698Z watch   js_string_to_terminal

--- public/src/js_string_to_terminal.mjs
+++ public/src/js_string_to_terminal.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { string_delimit } from "./string_delimit.mjs";
@@ -23,9 +22,6 @@ export async function js_string_to_terminal() {
   let code = await waitForInput();
   let parsed = js_parse_expression(code);
   let value = object_property_get(parsed, "value");
-  log({
-    value,
-  });
   let q = terminal_tokens_quote();
   let replaced = string_replace(value, q, string_double(q));
   let v = string_delimit(replaced);
