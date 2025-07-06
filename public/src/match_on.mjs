50677a450ddc62ddbb404d0b468716a595c2196e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 17:25:55 -0400

    2025-07-06T21:25:54.568Z watch   match_on

--- public/src/match_on.mjs
+++ public/src/match_on.mjs
@@ -2,7 +2,7 @@ import { each } from "./each.mjs";
 export function match_on(match_fn, on_match) {
   let v = function match_on_inner(a) {
     a = match_fn(a);
-    each(filtered, on_match);
+    each(a, on_match);
   };
   return v;
 }
