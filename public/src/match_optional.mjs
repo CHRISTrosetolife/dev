1c8d06c645a8647ff3c4440de9f04a25b098ad2b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 16:40:31 -0400

    2025-07-06T20:40:30.945Z watch   match_optional

--- public/src/match_optional.mjs
+++ public/src/match_optional.mjs
@@ -1,6 +1,7 @@
 import { noop } from "./noop.mjs";
 import { identity } from "./identity.mjs";
 import { match_choice } from "./match_choice.mjs";
-export function match_optional(newLocal) {
-  return match_choice([newLocal, identity], noop);
+export function match_optional(input) {
+  let v = match_choice([input, identity], noop);
+  return v;
 }
