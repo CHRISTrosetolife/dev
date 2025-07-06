4f52ed59cfc8e5842f5da94abb1f39981ec5576a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 13:54:35 -0400

    2025-07-06T17:54:34.654Z watch   js_string_to_terminal

--- public/src/js_string_to_terminal.mjs
+++ public/src/js_string_to_terminal.mjs
@@ -1,10 +1,12 @@
+import { js_parse_expression } from "./js_parse_expression.mjs";
 import { string_delimit } from "./string_delimit.mjs";
 import { string_double } from "./string_double.mjs";
 import { terminal_tokens_quote } from "./terminal_tokens_quote.mjs";
 import { string_replace } from "./string_replace.mjs";
-export function js_string_to_terminal(input) {
+export function js_string_to_terminal(code) {
+  js_parse_expression(code);
   let q = terminal_tokens_quote();
-  let replaced = string_replace(input, q, string_double(q));
+  let replaced = string_replace(code, q, string_double(q));
   let v = string_delimit(replaced);
   return v;
 }
