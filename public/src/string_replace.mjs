0d32d59026796081ec66e8fd384f9c04d1cca8e3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 18:45:27 -0400

    2025-07-06T22:45:25.186Z watch   string_replace

--- public/src/string_replace.mjs
+++ public/src/string_replace.mjs
@@ -1,6 +1,9 @@
+import { never } from "./never.mjs";
+import { assert } from "./assert.mjs";
 import { string_split } from "./string_split.mjs";
 import { list_join } from "./list_join.mjs";
 export function string_replace(input, from, to) {
+  assert(never, []);
   let split = string_split(input, from);
   let joined = list_join(split, to);
   return joined;
