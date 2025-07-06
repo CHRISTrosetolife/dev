c62ae38fe6cbcc6530b632e5075300ca25b5ed10
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 18:47:16 -0400

    2025-07-06T22:47:15.819Z watch   function_result_upload

--- public/src/function_result_upload.mjs
+++ public/src/function_result_upload.mjs
@@ -3,5 +3,5 @@ import { function_result_path } from "./function_result_path.mjs";
 import { function_run } from "./function_run.mjs";
 export async function function_result_upload(f_name) {
   let result = await function_run(f_name, []);
-  await storage_upload_object(result, function_result_path(f_name));
+  await storage_upload_object(function_result_path(f_name), result);
 }
