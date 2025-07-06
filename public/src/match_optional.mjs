57287f9aad703d7276c0fab423b1e15ed5924913
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 17:39:37 -0400

    2025-07-06T21:39:36.636Z watch   match_optional

--- public/src/match_optional.mjs
+++ public/src/match_optional.mjs
@@ -1,9 +1,9 @@
-import { never } from "./never.mjs";
-import { assert } from "./assert.mjs";
+import { assert_not } from "./assert_not.mjs";
 import { identity } from "./identity.mjs";
 import { match_choice } from "./match_choice.mjs";
+import { list_is } from "./list_is.mjs";
 export function match_optional(input) {
-  assert(never, []);
+  assert_not(list_is, [input]);
   let v = match_choice([input, identity]);
   return v;
 }
