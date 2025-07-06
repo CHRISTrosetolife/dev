636cb33b96f1ea9ec1d4f7dcb2930f27acbe98f8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 18:34:37 -0400

    2025-07-06T22:34:36.526Z watch   function_result_path

--- public/src/function_result_path.mjs
+++ public/src/function_result_path.mjs
@@ -1,11 +1,7 @@
+import { file_name_json } from "./file_name_json.mjs";
 import { storage_path_function } from "./storage_path_function.mjs";
-import { file_name_js } from "./file_name_js.mjs";
 import { path_join } from "./path_join.mjs";
-export function function_result_path(name, version) {
-  let v = path_join([
-    storage_path_function("deploy"),
-    name,
-    file_name_js(version),
-  ]);
+export function function_result_path(f_name) {
+  let v = path_join([storage_path_function("deploy"), file_name_json(version)]);
   return v;
 }
