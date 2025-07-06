aa5a9fd2120c7c3d541c417c7e2843b7683a418f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 17:28:02 -0400

    2025-07-06T21:28:01.953Z watch   each

--- public/src/each.mjs
+++ public/src/each.mjs
@@ -1,6 +1,9 @@
+import { assert } from "./assert.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
+import { list_is } from "./list_is.mjs";
 export function each(list, lambda) {
   assert_arguments_length(arguments, 2);
+  assert(list_is, [list]);
   for (let item of list) {
     if (lambda(item) === true) {
       return;
