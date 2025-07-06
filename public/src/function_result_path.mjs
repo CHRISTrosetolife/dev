5056d41626756b4caf602d4b2f2f241257ce6230
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 18:34:00 -0400

    2025-07-06T22:33:59.871Z c function_deploy_path function_result_path

---
+++ public/src/function_result_path.mjs
@@ -0,0 +1,11 @@
+import { storage_path_function } from "./storage_path_function.mjs";
+import { file_name_js } from "./file_name_js.mjs";
+import { path_join } from "./path_join.mjs";
+export function function_result_path(name, version) {
+  let v = path_join([
+    storage_path_function("deploy"),
+    name,
+    file_name_js(version),
+  ]);
+  return v;
+}
