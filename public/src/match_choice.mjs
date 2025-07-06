9c6135e86d31de6fc2bddf50c4ca46b5d3ccc63c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 17:20:18 -0400

    2025-07-06T21:20:16.942Z watch   match_choice

--- public/src/match_choice.mjs
+++ public/src/match_choice.mjs
@@ -3,15 +3,14 @@ import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { match_functionize } from "./match_functionize.mjs";
 import { match_on } from "./match_on.mjs";
 import { list_map } from "./list_map.mjs";
-export function match_choice(choices, on_match) {
-  assert_arguments_length(arguments, 2);
+export function match_choice(choices) {
+  assert_arguments_length(arguments, 1);
   let v = function match_choice_inner(a) {
     let mapped = list_map(choices, function (choice) {
       let result = match_functionize(a, choice);
       return result;
     });
     let c = list_concat_multiple(mapped);
-    let filtered = match_on(c, on_match);
     return filtered;
   };
   return v;
