2661c8a3e8bde2729d5488785f0c067dff2da6dd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 18:43:04 -0400

    2025-07-06T22:43:02.929Z watch   function_run

--- public/src/function_run.mjs
+++ public/src/function_run.mjs
@@ -1,5 +1,7 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { function_import } from "./function_import.mjs";
 export async function function_run(function_name, args) {
+  assert_arguments_length(arguments, 2);
   let imported_function = await function_import(function_name);
   let result = await imported_function(...args);
   return result;
