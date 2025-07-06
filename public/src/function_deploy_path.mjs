d7929299ae40410998a916787f6ca3741b630b14
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 18:31:17 -0400

    2025-07-06T22:31:16.909Z watch   function_deploy_path

--- public/src/function_deploy_path.mjs
+++ public/src/function_deploy_path.mjs
@@ -1,9 +1,10 @@
+import { storage_path_function } from "./storage_path_function.mjs";
 import { file_name_js } from "./file_name_js.mjs";
 import { path_join } from "./path_join.mjs";
 export function function_deploy_path(name, version) {
   let folder = "deploy";
   let v = path_join([
-    path_join(["function", folder]),
+    storage_path_function(folder),
     name,
     file_name_js(version),
   ]);
---
+++ public/src/storage_path_function.mjs
@@ -0,0 +1,4 @@
+import { path_join } from "./path_join.mjs";
+export function storage_path_function(folder) {
+  return path_join(["function", folder]);
+}
