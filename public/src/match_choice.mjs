7287814d0839ce5b69b4be7a17ac0daeeb3695ae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 17:21:13 -0400

    2025-07-06T21:21:12.637Z watch   match_choice

--- public/src/match_choice.mjs
+++ public/src/match_choice.mjs
@@ -1,8 +1,8 @@
 import { list_concat_multiple } from "./list_concat_multiple.mjs";
 import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { match_functionize } from "./match_functionize.mjs";
-import { match_on } from "./match_on.mjs";
 import { list_map } from "./list_map.mjs";
+import { match_filter } from "./match_filter.mjs";
 export function match_choice(choices) {
   assert_arguments_length(arguments, 1);
   let v = function match_choice_inner(a) {
@@ -11,6 +11,7 @@ export function match_choice(choices) {
       return result;
     });
     let c = list_concat_multiple(mapped);
+    let filtered = match_filter(c);
     return filtered;
   };
   return v;
