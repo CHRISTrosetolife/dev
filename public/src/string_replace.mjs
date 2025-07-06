e9f2491927628e015bc71a3dc6badad2998311d5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 18:45:36 -0400

    2025-07-06T22:45:36.112Z watch   string_replace

--- public/src/string_replace.mjs
+++ public/src/string_replace.mjs
@@ -1,9 +1,9 @@
-import { never } from "./never.mjs";
+import { string_is } from "./string_is.mjs";
 import { assert } from "./assert.mjs";
 import { string_split } from "./string_split.mjs";
 import { list_join } from "./list_join.mjs";
 export function string_replace(input, from, to) {
-  assert(never, []);
+  assert(string_is, [input]);
   let split = string_split(input, from);
   let joined = list_join(split, to);
   return joined;
