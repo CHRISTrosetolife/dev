af1de5ed8af13c113f496ef6501facc5594112ba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 21:52:37 -0400

    2025-07-07T01:52:36.117Z watch   list_to_lookup_key_value

--- public/src/list_to_lookup_key_value.mjs
+++ public/src/list_to_lookup_key_value.mjs
@@ -1,9 +1,9 @@
 import { object_property_set } from "./object_property_set.mjs";
 import { each } from "./each.mjs";
-export function list_to_lookup_key_value(list, mapper_key, mapped_value) {
+export function list_to_lookup_key_value(list, mapper_key, value_mapper) {
   let lookup = {};
-  each(list, (w) => {
-    object_property_set(lookup, mapper_key(w), mapped_value(w, lookup));
+  each(list, function (w) {
+    object_property_set(lookup, mapper_key(w), value_mapper(w, lookup));
   });
   return lookup;
 }
--- public/src/list_to_lookup_value.mjs
+++ public/src/list_to_lookup_value.mjs
@@ -1,5 +1,5 @@
 import { list_to_lookup_key_value } from "./list_to_lookup_key_value.mjs";
 import { identity } from "./identity.mjs";
-export function list_to_lookup_value(list, mapped_value) {
-  return list_to_lookup_key_value(list, identity, mapped_value);
+export function list_to_lookup_value(list, value_mapper) {
+  return list_to_lookup_key_value(list, identity, value_mapper);
 }
