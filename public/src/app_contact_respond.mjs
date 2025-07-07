d56ed1c7197622ff5c4aa96946524d6e28ff261d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 21:57:28 -0400

    2025-07-07T01:57:27.522Z watch   app_contact_respond

--- public/src/app_contact_respond.mjs
+++ public/src/app_contact_respond.mjs
@@ -18,7 +18,7 @@ export async function app_contact_respond(input) {
     function_result_path(fn_name("english_words_dictionary_object")),
   );
   let { words } = json_from(json);
-  list_to_lookup_keys(words);
+  let words_lookup = list_to_lookup_keys(words);
   let lower = string_case_lower(input);
   let alphabet = app_contact_alphabet();
   let joined = string_only(lower, alphabet);
