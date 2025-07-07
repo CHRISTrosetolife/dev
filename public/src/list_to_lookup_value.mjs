27f55d88d6a52c801bb5fbaf34569ae35ad82bfe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-06 21:52:44 -0400

    2025-07-07T01:52:43.344Z watch   list_to_lookup_value

--- public/src/list_to_lookup_value.mjs
+++ public/src/list_to_lookup_value.mjs
@@ -1,5 +1,6 @@
 import { list_to_lookup_key_value } from "./list_to_lookup_key_value.mjs";
 import { identity } from "./identity.mjs";
 export function list_to_lookup_value(list, value_mapper) {
-  return list_to_lookup_key_value(list, identity, value_mapper);
+  let v = list_to_lookup_key_value(list, identity, value_mapper);
+  return v;
 }
