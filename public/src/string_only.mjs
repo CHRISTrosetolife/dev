e102856a9da6ca9850e487608dbb28333f6bd187
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-12 19:04:28 -0400

    2025-07-12T23:04:28.305Z watch   string_only

--- public/src/string_only.mjs
+++ public/src/string_only.mjs
@@ -1,10 +1,10 @@
+import { list_map } from "./list_map.mjs";
 import { list_join_empty } from "./list_join_empty.mjs";
 import { list_includes } from "./list_includes.mjs";
-import { list_filter } from "./list_filter.mjs";
 import { string_split_empty } from "./string_split_empty.mjs";
 export function string_only(lower, letters) {
   let split = string_split_empty(lower);
-  let filtered2 = list_filter(split, function (c) {
+  let filtered2 = list_map(split, function (c) {
     let v = list_includes(letters, c);
     return v;
   });
