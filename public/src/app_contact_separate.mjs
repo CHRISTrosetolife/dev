131282ac6d3ebcfd99a6d052dd3866726582c961
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-12 18:46:12 -0400

    2025-07-12T22:46:12.086Z watch   app_contact_separate

--- public/src/app_contact_separate.mjs
+++ public/src/app_contact_separate.mjs
@@ -2,11 +2,11 @@ import { string_prefix_without } from "./string_prefix_without.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { each } from "./each.mjs";
-export function app_contact_separate(result, words_lower, w) {
+export function app_contact_separate(result, words_lower, next) {
   each(words_lower, function (l) {
-    if (string_starts_with(w, l)) {
+    if (string_starts_with(next, l)) {
       let c = list_concat(result, [l]);
-      let remaining = string_prefix_without(w, l);
+      let remaining = string_prefix_without(next, l);
     }
   });
 }
