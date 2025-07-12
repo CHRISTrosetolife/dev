fde8bfc340038c757a4e9081c1081b36a0134efb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-12 18:47:32 -0400

    2025-07-12T22:47:31.592Z watch   app_contact_separate

--- public/src/app_contact_separate.mjs
+++ public/src/app_contact_separate.mjs
@@ -3,6 +3,8 @@ import { list_concat } from "./list_concat.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { each } from "./each.mjs";
 export function app_contact_separate(dictionary, remaining, result) {
+  if (false) {
+  }
   each(dictionary, function (d) {
     if (string_starts_with(remaining, d)) {
       let result_new = list_concat(result, [d]);
