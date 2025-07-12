e93da5bf755477eae5c23b044f487be45de24246
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-12 19:29:32 -0400

    2025-07-12T23:29:32.319Z watch   app_contact_phrase_god

--- public/src/app_contact_phrase_god.mjs
+++ public/src/app_contact_phrase_god.mjs
@@ -1,4 +1,6 @@
+import { match_optional } from "./match_optional.mjs";
 import { match_sequence } from "./match_sequence.mjs";
 export function app_contact_phrase_god() {
-  return match_sequence(["lord", "God"]);
+  let v = match_sequence([match_optional("lord"), "God"]);
+  return v;
 }
