49ddec9c973768f29983786d6ed82c5c25063add
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-12 18:46:56 -0400

    2025-07-12T22:46:56.402Z watch   app_contact_separate

--- public/src/app_contact_separate.mjs
+++ public/src/app_contact_separate.mjs
@@ -2,11 +2,12 @@ import { string_prefix_without } from "./string_prefix_without.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { each } from "./each.mjs";
-export function app_contact_separate(result, words_lower, next) {
-  each(words_lower, function (l) {
-    if (string_starts_with(next, l)) {
-      let c = list_concat(result, [l]);
-      let remaining = string_prefix_without(next, l);
+export function app_contact_separate(result, dictionary, remaining) {
+  each(dictionary, function (d) {
+    if (string_starts_with(remaining, d)) {
+      let result_new = list_concat(result, [d]);
+      let remaining_new = string_prefix_without(remaining, d);
+      app_contact_separate(result_new, dictionary, remaining_new);
     }
   });
 }
