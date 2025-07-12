094f10780c321acedd3faf93ab63a3c2dedb0d94
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-12 18:45:41 -0400

    2025-07-12T22:45:41.173Z pm app_contact_separate result words_lower

--- public/src/app_contact_separate.mjs
+++ public/src/app_contact_separate.mjs
@@ -2,7 +2,7 @@ import { string_prefix_without } from "./string_prefix_without.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { each } from "./each.mjs";
-export function app_contact_separate(words_lower, w, result) {
+export function app_contact_separate(result, words_lower, w) {
   each(words_lower, function (l) {
     if (string_starts_with(w, l)) {
       let c = list_concat(result, [l]);
