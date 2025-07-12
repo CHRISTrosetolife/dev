33fb002cf3dce2314ee84e9238a01133bfd8aec5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-12 19:16:17 -0400

    2025-07-12T23:16:16.517Z watch   app_contact_download

--- public/src/app_contact_download.mjs
+++ public/src/app_contact_download.mjs
@@ -13,7 +13,7 @@ export async function app_contact_download() {
     let file = await file_read_json(file_path);
     let message = object_property_get(file, "message");
     let { lookup, list } = await app_contact_dictionary();
-    let { output } = await app_contact_respond(message);
+    let { output } = await app_contact_respond(lookup, list, message);
     let output_null = output === null;
     let v = {
       file_path,
