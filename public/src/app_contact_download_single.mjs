67ddf36654d509a024cee7fc89b33ce1f9155134
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-12 19:51:49 -0400

    2025-07-12T23:51:49.202Z watch   app_contact_download_single

--- public/src/app_contact_download_single.mjs
+++ public/src/app_contact_download_single.mjs
@@ -1,9 +1,9 @@
 import { string_includes } from "./string_includes.mjs";
 import { app_contact_download_file_paths } from "./app_contact_download_file_paths.mjs";
-import { app_contact_download_file } from "./app_contact_download_file.mjs";
 import { app_contact_dictionary } from "./app_contact_dictionary.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { object_property_get } from "./object_property_get.mjs";
+import { list_single } from "./list_single.mjs";
 export async function app_contact_download_single(query) {
   let file_paths = await app_contact_download_file_paths();
   let d = await app_contact_dictionary();
@@ -11,7 +11,7 @@ export async function app_contact_download_single(query) {
     let v4 = string_includes(f, query);
     return v4;
   });
-  let v = await app_contact_download_file(d, file_path);
+  let m = list_single(mapped);
   let filtered = list_filter(mapped, function (m) {
     let v2 = object_property_get(m, "output_null");
     return v2;
