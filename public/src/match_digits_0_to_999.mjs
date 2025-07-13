72905de4db43ab5a3e9e9a0425a6ca1628de27a8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 12:57:00 -0400

    2025-07-13T16:56:59.458Z watch   match_digits_0_to_999

--- public/src/match_digits_0_to_999.mjs
+++ public/src/match_digits_0_to_999.mjs
@@ -1,3 +1,4 @@
+import { match_digit_9 } from "./match_digit_9.mjs";
 import { match_digit_10 } from "./match_digit_10.mjs";
 import { match_optional } from "./match_optional.mjs";
 import { match_repeat } from "./match_repeat.mjs";
@@ -5,7 +6,7 @@ import { match_sequence } from "./match_sequence.mjs";
 export function match_digits_0_to_999() {
   let times = 3;
   let v = match_sequence([
-    match_digit_10(),
+    match_digit_9(),
     match_repeat(times - 1, match_optional(match_digit_10())),
   ]);
   return v;
