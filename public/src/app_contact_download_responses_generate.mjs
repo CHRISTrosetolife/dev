84bafbffd6d88ab0e1c0a5c0c9cbcc934dbfe142
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 19:07:52 -0400

    2025-07-13T23:07:51.450Z watch   app_contact_download_responses_generate

--- public/src/app_contact_download_responses_generate.mjs
+++ public/src/app_contact_download_responses_generate.mjs
@@ -1,13 +1,8 @@
-import { app_contact_download_all } from "./app_contact_download_all.mjs";
-import { object_properties_new } from "./object_properties_new.mjs";
-import { list_map } from "./list_map.mjs";
+import { app_contact_download_responses_get } from "./app_contact_download_responses_get.mjs";
 import { generate_list_fn } from "./generate_list_fn.mjs";
 export async function app_contact_download_responses_generate() {
   await generate_list_fn(async function () {
-    let r = await app_contact_download_all();
-    let v = list_map(r, async function (ri) {
-      object_properties_new(ri, ["outputs", "valid", "message"]);
-    });
+    let v = await app_contact_download_responses_get();
     return v;
   }, fn);
 }
---
+++ public/src/app_contact_download_responses_get.mjs
@@ -0,0 +1,10 @@
+import { object_properties_new } from "./object_properties_new.mjs";
+import { list_map } from "./list_map.mjs";
+import { app_contact_download_all } from "./app_contact_download_all.mjs";
+export async function app_contact_download_responses_get() {
+  let r = await app_contact_download_all();
+  let v = list_map(r, async function (ri) {
+    object_properties_new(ri, ["outputs", "valid", "message"]);
+  });
+  return v;
+}
