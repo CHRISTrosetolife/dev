cf358d2549d50c48af0c3fbbce72c2d241fca1c4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 19:06:34 -0400

    2025-07-13T23:06:33.717Z watch   app_contact_download_responses_generate

--- public/src/app_contact_download_responses_generate.mjs
+++ public/src/app_contact_download_responses_generate.mjs
@@ -3,8 +3,8 @@ import { list_map } from "./list_map.mjs";
 import { generate_list_fn } from "./generate_list_fn.mjs";
 export async function app_contact_download_responses_generate() {
   await generate_list_fn(function () {
-    list_map(r, function () {
-      object_properties_new();
+    list_map(r, function (ri) {
+      object_properties_new(ri, ["outputs", "valid", "message"]);
     });
   }, fn);
 }
