f79c0a7badc99eaabec713df3f9a7d189e7d8919
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 19:16:14 -0400

    2025-07-13T23:16:13.968Z watch   app_contact_download_responses_test

--- public/src/app_contact_download_responses_test.mjs
+++ public/src/app_contact_download_responses_test.mjs
@@ -1,8 +1,10 @@
+import { list_to_lookup_property } from "./list_to_lookup_property.mjs";
 import { each } from "./each.mjs";
 import { app_contact_download_responses_get } from "./app_contact_download_responses_get.mjs";
 import { app_contact_download_responses } from "./app_contact_download_responses.mjs";
 export async function app_contact_download_responses_test() {
   let existing = app_contact_download_responses();
+  let lookup = list_to_lookup_property(existing, "message");
   let responses_new = await app_contact_download_responses_get();
   each(responses_new, function (r) {});
 }
