f06cabfadbcc9ffb90cda02033ce512ef4b089d8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 12:56:38 -0400

    2025-07-13T16:56:34.895Z watch   match_digit_9

--- public/src/match_digit_9.mjs
+++ public/src/match_digit_9.mjs
@@ -1,6 +1,6 @@
-import { digits_10 } from "./digits_10.mjs";
+import { digits_9 } from "./digits_9.mjs";
 import { match_choice } from "./match_choice.mjs";
 export function match_digit_9() {
-  let v = match_choice(digits_10());
+  let v = match_choice(digits_9());
   return v;
 }
