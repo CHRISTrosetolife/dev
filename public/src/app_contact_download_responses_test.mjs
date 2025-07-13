52767567a150c05bd258fffdb8c7fee4512c7531
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 19:13:50 -0400

    2025-07-13T23:13:49.181Z watch   app_contact_download_responses_test

--- public/src/app_contact_download_responses_test.mjs
+++ public/src/app_contact_download_responses_test.mjs
@@ -1,1 +1,4 @@
-export function app_contact_download_responses_test() {}
+import { app_contact_download_responses } from "./app_contact_download_responses.mjs";
+export function app_contact_download_responses_test() {
+  let r = app_contact_download_responses();
+}
