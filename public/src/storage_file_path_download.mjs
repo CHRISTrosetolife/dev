9c0d06bb32ef7d8f5f7e07f6ad9c65373e77ef8d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 19:27:42 -0400

    2025-07-13T23:27:42.127Z watch   storage_file_path_download

--- public/src/storage_file_path_download.mjs
+++ public/src/storage_file_path_download.mjs
@@ -1,7 +1,11 @@
+import { firebase_download } from "./firebase_download.mjs";
 import { storage_file_path_download_generic } from "./storage_file_path_download_generic.mjs";
 import { storage_file_download } from "./storage_file_download.mjs";
+import { web_is } from "./web_is.mjs";
 export async function storage_file_path_download(storage_path) {
-  if (false) {
+  if (web_is()) {
+    let v2 = await firebase_download(storage_path);
+    return v2;
   }
   let download_lambda = storage_file_download;
   let v = await storage_file_path_download_generic(
