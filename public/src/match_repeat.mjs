271395be0a037abe36cdaa890960f7d3304890cc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 13:54:19 -0400

    2025-07-13T17:54:18.731Z watch   match_repeat

--- public/src/match_repeat.mjs
+++ public/src/match_repeat.mjs
@@ -2,6 +2,10 @@ import { range } from "./range.mjs";
 import { list_map } from "./list_map.mjs";
 import { match_sequence } from "./match_sequence.mjs";
 export function match_repeat(repeat_times, repeated_item) {
-  let v = match_sequence(list_map(range(repeat_times), repeated_item));
+  let v = match_sequence(
+    list_map(range(repeat_times), function () {
+      return repeated_item;
+    }),
+  );
   return v;
 }
