48e5c1d9b604c86ae3cb14f489362149139e2b7a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 11:50:08 -0400

    2025-07-13T15:50:07.536Z watch   app_contact_adds

--- public/src/app_contact_adds.mjs
+++ public/src/app_contact_adds.mjs
@@ -1,7 +1,11 @@
+import { log } from "./log.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { bible_storage_version_http_get } from "./bible_storage_version_http_get.mjs";
 export async function app_contact_adds() {
   let { books } = await bible_storage_version_http_get("engbsb", "books");
+  log({
+    books,
+  });
   let v = [
     "god",
     "greetings",
