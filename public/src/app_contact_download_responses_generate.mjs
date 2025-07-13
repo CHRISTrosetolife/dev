f8d77910ebdf8870189e518ac2916645a02c902b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 19:23:22 -0400

    2025-07-13T23:23:21.507Z watch   app_contact_download_responses_generate

--- public/src/app_contact_download_responses_generate.mjs
+++ public/src/app_contact_download_responses_generate.mjs
@@ -1,10 +1,10 @@
+import { function_get } from "./function_get.mjs";
 import { app_contact_download_responses_test } from "./app_contact_download_responses_test.mjs";
-import { app_contact_download_responses_get } from "./app_contact_download_responses_get.mjs";
 import { generate_list_fn } from "./generate_list_fn.mjs";
 export async function app_contact_download_responses_generate() {
-  await app_contact_download_responses_test();
+  let responses_new = await app_contact_download_responses_test();
   await generate_list_fn(
-    app_contact_download_responses_get,
+    function_get(responses_new),
     app_contact_download_responses_generate,
   );
 }
