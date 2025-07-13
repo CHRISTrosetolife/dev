b0ccdb7fb4d061fd64c3c9489918b28721713599
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 19:15:43 -0400

    2025-07-13T23:15:41.619Z watch   list_to_lookup_property

--- public/src/list_to_lookup_property.mjs
+++ public/src/list_to_lookup_property.mjs
@@ -1,8 +1,11 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { list_to_lookup } from "./list_to_lookup.mjs";
 export function list_to_lookup_property(list, property_name) {
   let lookup = list_to_lookup(list, mapper);
   function mapper(w) {
-    return w[property_name];
+    object_property_get(object, "property_name");
+    let v = w[property_name];
+    return v;
   }
   return lookup;
 }
