472f140fd24a1fc7e2256547e09c41cb75779e62
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 12:54:47 -0400

    2025-07-13T16:54:46.888Z watch   digits_9

--- public/src/digits_9.mjs
+++ public/src/digits_9.mjs
@@ -1,7 +1,7 @@
+import { range_1 } from "./range_1.mjs";
 import { list_map_string } from "./list_map_string.mjs";
-import { range } from "./range.mjs";
 export function digits_9() {
-  let d = range(10);
+  let d = range_1(9);
   let v = list_map_string(d);
   return v;
 }
