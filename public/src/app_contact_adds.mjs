3602d22226de4e9ed1c8fe8c1a4d045386d8bc4c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 14:14:12 -0400

    2025-07-13T18:14:11.518Z r global_function_property_or global_function_property_initialize

--- public/src/app_contact_adds.mjs
+++ public/src/app_contact_adds.mjs
@@ -1,4 +1,4 @@
-import { global_function_property_or } from "./global_function_property_or.mjs";
+import { global_function_property_initialize } from "./global_function_property_initialize.mjs";
 import { bible_reference_separator } from "./bible_reference_separator.mjs";
 import { digits_10 } from "./digits_10.mjs";
 import { list_map_property } from "./list_map_property.mjs";
@@ -7,7 +7,7 @@ import { list_concat_multiple } from "./list_concat_multiple.mjs";
 export async function app_contact_adds() {
   let { books } = await bible_storage_version_http_get("engbsb", "books");
   let bible_books_names = list_map_property(books, "book_name");
-  global_function_property_or(
+  global_function_property_initialize(
     app_contact_adds,
     "bible_books_names",
     function () {
