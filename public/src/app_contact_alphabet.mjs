1d388a738bd587570818395660fd697059d8fbc8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 14:04:16 -0400

    2025-07-13T18:04:15.719Z watch   app_contact_alphabet

--- public/src/app_contact_alphabet.mjs
+++ public/src/app_contact_alphabet.mjs
@@ -1,3 +1,5 @@
+import { bible_reference_separator } from "./bible_reference_separator.mjs";
+import { list_add } from "./list_add.mjs";
 import { string_split_empty } from "./string_split_empty.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
 import { keyboard_keys } from "./keyboard_keys.mjs";
@@ -6,5 +8,6 @@ export function app_contact_alphabet() {
   let letters = keyboard_keys();
   list_add_multiple(letters, string_split_empty("' "));
   list_add_multiple(letters, digits_10());
+  list_add(letters, bible_reference_separator());
   return letters;
 }
