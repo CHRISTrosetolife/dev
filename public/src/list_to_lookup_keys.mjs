f601faeab68ff9e1d02a2f80343bf4843e8134a0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 19:22:00 -0400

    2025-07-13T23:21:59.381Z watch   list_to_lookup_keys

--- public/src/list_to_lookup_keys.mjs
+++ public/src/list_to_lookup_keys.mjs
@@ -1,6 +1,8 @@
+import { list_unique } from "./list_unique.mjs";
 import { null_get } from "./null_get.mjs";
 import { list_to_lookup_value } from "./list_to_lookup_value.mjs";
 export function list_to_lookup_keys(words) {
+  words = list_unique(words);
   let v = list_to_lookup_value(words, null_get);
   return v;
 }
