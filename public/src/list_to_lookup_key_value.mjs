795e4ba51720995ea15837930fa90ab52d128715
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 19:19:40 -0400

    2025-07-13T23:19:39.437Z watch   list_to_lookup_key_value

--- public/src/list_to_lookup_key_value.mjs
+++ public/src/list_to_lookup_key_value.mjs
@@ -1,8 +1,11 @@
+import { never } from "./never.mjs";
+import { assert } from "./assert.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { each } from "./each.mjs";
 export function list_to_lookup_key_value(list, mapper_key, value_mapper) {
   let lookup = {};
   each(list, function (w) {
+    assert(never, []);
     object_property_set(lookup, mapper_key(w), value_mapper(w, lookup));
   });
   return lookup;
