c4c58ef8e7436feef9bd192bc657bd66dc037011
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 19:20:02 -0400

    2025-07-13T23:20:02.272Z watch   list_to_lookup_key_value

--- public/src/list_to_lookup_key_value.mjs
+++ public/src/list_to_lookup_key_value.mjs
@@ -1,12 +1,13 @@
-import { never } from "./never.mjs";
 import { assert } from "./assert.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { each } from "./each.mjs";
+import { object_property_exists_not } from "./object_property_exists_not.mjs";
 export function list_to_lookup_key_value(list, mapper_key, value_mapper) {
   let lookup = {};
   each(list, function (w) {
-    assert(never, []);
-    object_property_set(lookup, mapper_key(w), value_mapper(w, lookup));
+    let k = mapper_key(w);
+    assert(object_property_exists_not, [lookup, k]);
+    object_property_set(lookup, k, value_mapper(w, lookup));
   });
   return lookup;
 }
