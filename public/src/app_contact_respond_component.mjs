669796a9264c16ec61078a69d7c8f737828c775e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 19:35:25 -0400

    2025-07-13T23:35:24.233Z watch   app_contact_respond_component

--- public/src/app_contact_respond_component.mjs
+++ public/src/app_contact_respond_component.mjs
@@ -1,9 +1,8 @@
-import { app_contact_dictionary } from "./app_contact_dictionary.mjs";
 import { log } from "./log.mjs";
 import { html_clear } from "./html_clear.mjs";
 import { app_contact_respond } from "./app_contact_respond.mjs";
 import { html_value_get } from "./html_value_get.mjs";
-export  function app_contact_respond_component(t, response_p,d) {
+export async function app_contact_respond_component(t, response_p, d) {
   html_clear(response_p);
   let value = html_value_get(t);
   let response = app_contact_respond(d, value);
