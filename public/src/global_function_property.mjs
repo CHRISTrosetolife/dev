27329ac7c1c2fad5955b0eb9c19f2208057e380e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 14:10:08 -0400

    2025-07-13T18:10:07.026Z watch   global_function_property

--- public/src/global_function_property.mjs
+++ public/src/global_function_property.mjs
@@ -1,7 +1,7 @@
-import { object_property_get_or } from "./object_property_get_or.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 import { global_function } from "./global_function.mjs";
-export function global_function_property(fn, property_name, value_get) {
+export function global_function_property(fn, property_name) {
   let a = global_function(fn);
-  let result = object_property_get_or(a, property_name, value_get);
+  let result = object_property_get(a, property_name);
   return result;
 }
