a13add32c428217f67c57c4c757d2b453d662316
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 14:02:49 -0400

    2025-07-13T18:02:48.811Z watch   app_contact_alphabet

--- public/src/app_contact_alphabet.mjs
+++ public/src/app_contact_alphabet.mjs
@@ -1,8 +1,10 @@
 import { string_split_empty } from "./string_split_empty.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
 import { keyboard_keys } from "./keyboard_keys.mjs";
+import { digits_10 } from "./digits_10.mjs";
 export function app_contact_alphabet() {
   let letters = keyboard_keys();
   list_add_multiple(letters, string_split_empty("' "));
+  list_add_multiple(letters, digits_10());
   return letters;
 }
