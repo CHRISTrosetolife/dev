d43ce3e1800d53564ea58e90db83f8915363ab20
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 11:38:01 -0400

    2025-07-13T15:38:00.594Z r js_terminal_to_string_split line_read_split_space

--- public/src/function_aliases.mjs
+++ public/src/function_aliases.mjs
@@ -119,9 +119,7 @@ export function function_aliases() {
   function_aliases_register(all, fn_name("app_contact_main_deploy"), ["cy"]);
   function_aliases_register(all, fn_name("app_contact_main_open"), ["ac"]);
   function_aliases_register(all, fn_name("bible_verses_upload"), ["bd"]);
-  function_aliases_register(all, fn_name("js_terminal_to_string_split"), [
-    "ts",
-  ]);
+  function_aliases_register(all, fn_name("line_read_split_space"), ["ts"]);
   function_aliases_register(all, fn_name("app_contact_download_single"), [
     "cds",
   ]);
--- public/src/line_read_split_space.mjs
+++ public/src/line_read_split_space.mjs
@@ -1,7 +1,7 @@
 import { line_read } from "./line_read.mjs";
 import { string_split_space } from "./string_split_space.mjs";
 import { json_to } from "./json_to.mjs";
-export async function js_terminal_to_string_split() {
+export async function line_read_split_space() {
   let value = await line_read();
   let s = string_split_space(value);
   let v = json_to(s);
