867550313228176545f562fb5182bec6bd04bf5a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 19:07:01 -0400

    2025-07-13T23:07:00.243Z watch   app_contact_download_responses_generate

--- public/src/app_contact_download_responses_generate.mjs
+++ public/src/app_contact_download_responses_generate.mjs
@@ -1,9 +1,11 @@
+import { app_contact_download_all } from "./app_contact_download_all.mjs";
 import { object_properties_new } from "./object_properties_new.mjs";
 import { list_map } from "./list_map.mjs";
 import { generate_list_fn } from "./generate_list_fn.mjs";
 export async function app_contact_download_responses_generate() {
   await generate_list_fn(function () {
-    list_map(r, function (ri) {
+    list_map(r, async function (ri) {
+      await app_contact_download_all();
       object_properties_new(ri, ["outputs", "valid", "message"]);
     });
   }, fn);
