dc6f7ab748a951e28391e284b039869d65911594
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 19:09:34 -0400

    2025-07-13T23:09:34.021Z watch   app_contact_download_responses_get

--- public/src/app_contact_download_responses_get.mjs
+++ public/src/app_contact_download_responses_get.mjs
@@ -4,7 +4,7 @@ import { list_map } from "./list_map.mjs";
 import { app_contact_download_all } from "./app_contact_download_all.mjs";
 export async function app_contact_download_responses_get() {
   let r = await app_contact_download_all();
-  let v = list_map(r, async function (ri) {
+  let v = list_map(r, function (ri) {
     let v2 = object_properties_new(ri, ["outputs", "valid", "message"]);
     log({
       v2,
