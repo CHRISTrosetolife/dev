b5b81a8461faf3f598fa99f88a74ef4d762a8fbd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 13:57:42 -0400

    2025-07-13T17:57:41.639Z watch   app_contact_match_invalid_edit

--- public/src/app_contact_match_invalid_edit.mjs
+++ public/src/app_contact_match_invalid_edit.mjs
@@ -1,12 +1,14 @@
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { app_contact_match_invalid } from "./app_contact_match_invalid.mjs";
 export function app_contact_match_invalid_edit(what_you_did, edit_to_say) {
-  return app_contact_match_invalid(
+  let v = app_contact_match_invalid(
     string_combine_multiple([
       "You ",
       what_you_did,
       ". Please edit your message to say what ",
       edit_to_say,
+      ".",
     ]),
   );
+  return v;
 }
