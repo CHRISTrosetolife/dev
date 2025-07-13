d28fe777c3db31a08026da30e6f953541974f656
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 11:36:29 -0400

    2025-07-13T15:36:29.035Z watch   app_contact_respond_choices

--- public/src/app_contact_respond_choices.mjs
+++ public/src/app_contact_respond_choices.mjs
@@ -150,6 +150,15 @@ export function app_contact_respond_choices() {
         ]),
       ),
     ),
+    match_on(
+      match_sequence(["only", "we", "have", "Urdu", "translation", "bible"]),
+      app_contact_match(
+        string_combine_multiple([
+          "God, thanks for Bible ",
+          app_contact_phrase_amen(),
+        ]),
+      ),
+    ),
     match_on(match_sequence(["verse"]), app_contact_match()),
   ];
   return v;
