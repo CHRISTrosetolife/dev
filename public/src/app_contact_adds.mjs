6a1564076bf23141f514f5ec088f1ca349ac5cc9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 12:40:33 -0400

    2025-07-13T16:40:33.194Z watch   app_contact_adds

--- public/src/app_contact_adds.mjs
+++ public/src/app_contact_adds.mjs
@@ -1,6 +1,6 @@
 import { list_map_property } from "./list_map_property.mjs";
-import { list_concat } from "./list_concat.mjs";
 import { bible_storage_version_http_get } from "./bible_storage_version_http_get.mjs";
+import { list_concat_multiple } from "./list_concat_multiple.mjs";
 export async function app_contact_adds() {
   let { books } = await bible_storage_version_http_get("engbsb", "books");
   let books_names = list_map_property(books, "book_name");
@@ -15,6 +15,6 @@ export async function app_contact_adds() {
     "how're",
     "hi",
   ];
-  let v2 = list_concat(v, books_names);
+  let v2 = list_concat_multiple([v, books_names]);
   return v2;
 }
