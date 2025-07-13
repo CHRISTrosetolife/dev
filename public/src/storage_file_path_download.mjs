be183a13aea75205b7aafe3fc29aacbed71d37e1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 19:27:30 -0400

    2025-07-13T23:27:29.370Z watch   storage_file_path_download

--- public/src/storage_file_path_download.mjs
+++ public/src/storage_file_path_download.mjs
@@ -1,6 +1,8 @@
 import { storage_file_path_download_generic } from "./storage_file_path_download_generic.mjs";
 import { storage_file_download } from "./storage_file_download.mjs";
 export async function storage_file_path_download(storage_path) {
+  if (false) {
+  }
   let download_lambda = storage_file_download;
   let v = await storage_file_path_download_generic(
     storage_path,
