499e089682a8d3997466d531687986858228ca9c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-13 12:43:04 -0400

    2025-07-13T16:43:03.381Z c global_function_property_async global_function_property

---
+++ public/src/global_function_property.mjs
@@ -0,0 +1,11 @@
+import { object_property_get_or_async } from "./object_property_get_or_async.mjs";
+import { global_function } from "./global_function.mjs";
+export async function global_function_property(
+  app_fn,
+  property_name,
+  value_get,
+) {
+  let a = global_function(app_fn);
+  let result = await object_property_get_or_async(a, property_name, value_get);
+  return result;
+}
