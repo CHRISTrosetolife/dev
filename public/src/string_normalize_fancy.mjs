eda05deaa375613944748875523eaf80f71c3b7b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 18:18:05 -0400

    2025-07-14T22:18:03.761Z watch   string_normalize_fancy

--- public/src/string_normalize_fancy.mjs
+++ public/src/string_normalize_fancy.mjs
@@ -4,9 +4,9 @@ export function string_normalize_fancy(str) {
   for (let char of str) {
     let code = char.codePointAt(0);
     if (code >= 0x1d5a0 && code <= 0x1d5b9) {
-      result += String.fromCharCode(code - 0x1d5a0 + 65);
+      result += String.fromCodePoint(code - 0x1d5a0 + 65);
     } else if (code >= 0x1d5ba && code <= 0x1d5d3) {
-      result += String.fromCharCode(code - 0x1d5ba + 97);
+      result += String.fromCodePoint(code - 0x1d5ba + 97);
     } else {
       result += char;
     }
