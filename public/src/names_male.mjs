643701f5e40e5244104ca332660b0b8bbab740a7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 13:42:56 -0400

    2025-07-14T17:42:54.831Z watch   names_male

--- public/src/names_male.mjs
+++ public/src/names_male.mjs
@@ -1,4 +1,6 @@
 import { http_cache } from "./http_cache.mjs";
-export function names_male() {
-  http_cache;
+export async function names_male() {
+  await http_cache(
+    "https://www.cs.cmu.edu/afs/cs/project/ai-repository/ai/areas/nlp/corpora/names/male.txt",
+  );
 }
