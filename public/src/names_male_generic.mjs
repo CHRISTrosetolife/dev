1808f2a5182b4b5fe38c21cd214421a75ee4181b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 13:55:20 -0400

    2025-07-14T17:55:19.186Z watch   names_male_generic

--- public/src/names_male_generic.mjs
+++ public/src/names_male_generic.mjs
@@ -1,9 +1,10 @@
+import { generate_list_string_generated } from "./generate_list_string_generated.mjs";
 import { list_filter_string_empty_not_is } from "./list_filter_string_empty_not_is.mjs";
 import { list_filter_starts_with_not } from "./list_filter_starts_with_not.mjs";
 import { string_split_newline } from "./string_split_newline.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { http_cache } from "./http_cache.mjs";
-export async function names_male_generic(prefix) {
+export async function names_male_generic(fn, prefix) {
   let txt = await http_cache(
     string_combine_multiple([
       "https://www.cs.cmu.edu/afs/cs/project/ai-repository/ai/areas/nlp/corpora/names/",
@@ -14,5 +15,6 @@ export async function names_male_generic(prefix) {
   let lines = string_split_newline(txt);
   let filtered = list_filter_starts_with_not(lines, "#");
   let filtered2 = list_filter_string_empty_not_is(filtered);
+  await generate_list_string_generated(fn, filtered2);
   return filtered2;
 }
