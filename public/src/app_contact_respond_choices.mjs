f32f673ffc37f89e7a426a285a896980913bbc1a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 18:08:57 -0400

    2025-07-14T22:08:56.785Z watch   app_contact_respond_choices

--- public/src/app_contact_respond_choices.mjs
+++ public/src/app_contact_respond_choices.mjs
@@ -215,9 +215,7 @@ export function app_contact_respond_choices() {
         match_choice(countries_names()),
       ]),
       app_contact_match(
-        string_combine_multiple([
-          "Thank God that you are a Christian! If we confess Jesus before men, then Jesus will confess us before the Father!",
-        ]),
+        "Thank God that you are a Christian! If we confess Jesus before men, then Jesus will confess us before the Father!",
       ),
     ),
     match_on(
@@ -228,9 +226,7 @@ export function app_contact_respond_choices() {
         match_choice(list_map(range_from(2, 20), number_to_words)),
         "children",
       ]),
-      app_contact_match(
-        string_combine_multiple(["Thank God for your marriage and children!"]),
-      ),
+      app_contact_match("Thank God for your marriage and children!"),
     ),
   ];
   return v;
