9e7d02a94817d8b48e171d7b3282b87632fe47bf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 13:44:29 -0400

    2025-07-14T17:44:28.555Z watch   names_male

--- public/src/names_male.mjs
+++ public/src/names_male.mjs
@@ -1,9 +1,15 @@
+import { list_filter_starts_with } from "./list_filter_starts_with.mjs";
 import { http_cache } from "./http_cache.mjs";
 import { string_split_newline } from "./string_split_newline.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
 export async function names_male() {
   let txt = await http_cache(
     "https://www.cs.cmu.edu/afs/cs/project/ai-repository/ai/areas/nlp/corpora/names/male.txt",
   );
   let lines = string_split_newline(txt);
+  list_filter_starts_with(lines, function (l) {
+    let v = string_starts_with();
+    return v;
+  });
   return txt;
 }
