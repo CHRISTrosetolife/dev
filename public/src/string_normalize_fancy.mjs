b19e3f42b3c8d24debe57e0ff71baa4c47de348d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 18:21:09 -0400

    2025-07-14T22:21:09.017Z watch   string_normalize_fancy

--- public/src/string_normalize_fancy.mjs
+++ public/src/string_normalize_fancy.mjs
@@ -7,6 +7,10 @@ export function string_normalize_fancy(str) {
       result += String.fromCodePoint(code - 0x1d5a0 + 65);
     } else if (code >= 0x1d5ba && code <= 0x1d5d3) {
       result += String.fromCodePoint(code - 0x1d5ba + 97);
+    } else if (code >= 0x1d63c && code <= 0x1d655) {
+      result += String.fromCodePoint(code - 0x1d63c + 65);
+    } else if (code >= 0x1d656 && code <= 0x1d66f) {
+      result += String.fromCodePoint(code - 0x1d656 + 97);
     } else {
       result += char;
     }
