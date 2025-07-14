8d61411ad13a550a7027d87d28ef5f90295ab625
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 13:49:32 -0400

    2025-07-14T17:49:31.297Z watch   names_male_generic

--- public/src/names_male.mjs
+++ public/src/names_male.mjs
@@ -5,15 +5,19 @@ import { http_cache } from "./http_cache.mjs";
 import { string_split_newline } from "./string_split_newline.mjs";
 export async function names_male() {
   let prefix = "";
-  let txt = await http_cache(
-    string_combine_multiple([
-      "https://www.cs.cmu.edu/afs/cs/project/ai-repository/ai/areas/nlp/corpora/names/",
-      prefix,
-      "male.txt",
-    ]),
-  );
-  let lines = string_split_newline(txt);
-  let filtered = list_filter_starts_with_not(lines, "#");
-  let filtered2 = list_filter_string_empty_not_is(filtered);
-  return filtered2;
+  return await names_male_generic(prefix);
 }
+async function names_male_generic(prefix) {
+    let txt = await http_cache(
+        string_combine_multiple([
+            "https://www.cs.cmu.edu/afs/cs/project/ai-repository/ai/areas/nlp/corpora/names/",
+            prefix,
+            "male.txt",
+        ])
+    );
+    let lines = string_split_newline(txt);
+    let filtered = list_filter_starts_with_not(lines, "#");
+    let filtered2 = list_filter_string_empty_not_is(filtered);
+    return filtered2;
+}
+
