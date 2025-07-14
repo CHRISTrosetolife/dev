0783764c5a8f0b4d243e86585a21efb5ecd5c084
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 18:10:30 -0400

    2025-07-14T22:10:29.741Z watch   string_normalize_fancy

--- public/src/string_normalize_fancy.mjs
+++ public/src/string_normalize_fancy.mjs
@@ -1,1 +1,14 @@
-export function string_normalize_fancy(s) {}
+export function string_normalize_fancy(s) {
+  let result = "";
+  for (let char of str) {
+    let code = char.codePointAt(0);
+    if (code >= 0x1d5a0 && code <= 0x1d5b9) {
+      result += String.fromCharCode(code - 0x1d5a0 + 65);
+    } else if (code >= 0x1d5ba && code <= 0x1d5d3) {
+      result += String.fromCharCode(code - 0x1d5ba + 97);
+    } else {
+      result += char;
+    }
+  }
+  return result;
+}
