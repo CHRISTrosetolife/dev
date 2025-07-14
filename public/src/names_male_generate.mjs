09e06cd1722e0f4b93d1b3ee4c2087879561ab05
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 13:55:51 -0400

    2025-07-14T17:55:50.904Z watch   names_male_generate

--- public/src/names_male_generate.mjs
+++ public/src/names_male_generate.mjs
@@ -1,4 +1,3 @@
-import { generate_list_string_generated } from "./generate_list_string_generated.mjs";
 import { names_male_generic } from "./names_male_generic.mjs";
 export async function names_male_generate() {
   let prefix = "";
