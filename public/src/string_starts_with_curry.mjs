d6be30299bf465072c0e5959decf20727d4671e9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 13:45:14 -0400

    2025-07-14T17:45:11.684Z watch   string_starts_with_curry

--- public/src/string_starts_with_curry.mjs
+++ public/src/string_starts_with_curry.mjs
@@ -1,7 +1,8 @@
 import { string_starts_with } from "./string_starts_with.mjs";
 export function string_starts_with_curry(prefix) {
-  let v = function (h) {
-    return string_starts_with(h, prefix);
+  let v = function string_starts_with_curry_inner(h) {
+    let v2 = string_starts_with(h, prefix);
+    return v2;
   };
   return v;
 }
