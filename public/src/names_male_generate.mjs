fba37f03a091403df23d8018fbab4b73885a9d78
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 13:54:01 -0400

    2025-07-14T17:54:01.329Z watch   names_male_generate

--- public/src/names_male_generate.mjs
+++ public/src/names_male_generate.mjs
@@ -1,6 +1,7 @@
+import { generate_list_string_generated } from "./generate_list_string_generated.mjs";
 import { names_male_generic } from "./names_male_generic.mjs";
 export async function names_male_generate() {
   let prefix = "";
-  let v = await names_male_generic(prefix);
-  return v;
+  let list = await names_male_generic(prefix);
+  await generate_list_string_generated(names_male_generate, list);
 }
