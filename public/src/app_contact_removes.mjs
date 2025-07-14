6ddc8b76fe2714fdd8499e88b93b760a968473b6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 13:31:54 -0400

    2025-07-14T17:31:53.873Z watch   app_contact_removes

--- public/src/app_contact_removes.mjs
+++ public/src/app_contact_removes.mjs
@@ -1,7 +1,7 @@
 import { string_split_empty } from "./string_split_empty.mjs";
 import { list_concat } from "./list_concat.mjs";
 export function app_contact_removes() {
-  return list_concat(string_split_empty("bdefghmnsw"), [
+  let v = list_concat(string_split_empty("bdefghmnswy"), [
     "ut",
     "ur",
     "re",
@@ -11,4 +11,5 @@ export function app_contact_removes() {
     "sen",
     "ing",
   ]);
+  return v;
 }
