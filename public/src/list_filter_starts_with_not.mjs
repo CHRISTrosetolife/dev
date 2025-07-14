6282c3ebe6adedeae93f8be06ee7970fba46a8ca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 13:46:34 -0400

    2025-07-14T17:46:32.621Z watch   list_filter_starts_with_not

--- public/src/list_filter_starts_with_not.mjs
+++ public/src/list_filter_starts_with_not.mjs
@@ -1,7 +1,10 @@
-import { string_starts_with_curry } from "./string_starts_with_curry.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
 import { list_filter } from "./list_filter.mjs";
 export function list_filter_starts_with_not(list, prefix) {
-  let filter = string_starts_with_curry(prefix);
+  let filter = function string_starts_with_curry_inner(h) {
+    let v2 = string_starts_with(h, prefix);
+    return v2;
+  };
   let v = list_filter(list, filter);
   return v;
 }
