de327c71b46024a749a456596f40b2239ee39ab0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 13:52:15 -0400

    2025-07-14T17:52:13.974Z r names_male names_male_generate

--- public/src/names_male_generate.mjs
+++ public/src/names_male_generate.mjs
@@ -1,5 +1,5 @@
 import { names_male_generic } from "./names_male_generic.mjs";
-export async function names_male() {
+export async function names_male_generate() {
   let prefix = "";
   let v = await names_male_generic(prefix);
   return v;
