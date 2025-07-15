1cbf560b9001c66c716b6b6670ee813b20fc3d25
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 22:00:52 -0400

    2025-07-15T02:00:51.361Z watch   names_last_generate

--- public/src/names_last_generate.mjs
+++ public/src/names_last_generate.mjs
@@ -1,1 +1,11 @@
-export function names_last_generate() {}
+import { names_generate_generic } from "./names_generate_generic.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export async function names_last_generate() {
+  let url = string_combine_multiple([
+    "https://www.cs.cmu.edu/afs/cs/project/ai-repository/ai/areas/nlp/corpora/names/",
+    prefix,
+    "male.txt",
+  ]);
+  let filtered2 = await names_generate_generic(url, fn);
+  return filtered2;
+}
