335d4f6dc3ad9943d5aaa22fca450ef05d411f52
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 22:00:57 -0400

    2025-07-15T02:00:56.658Z watch   names_last_generate

--- public/src/names_last_generate.mjs
+++ public/src/names_last_generate.mjs
@@ -2,9 +2,7 @@ import { names_generate_generic } from "./names_generate_generic.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export async function names_last_generate() {
   let url = string_combine_multiple([
-    "https://www.cs.cmu.edu/afs/cs/project/ai-repository/ai/areas/nlp/corpora/names/",
-    prefix,
-    "male.txt",
+    "https://gist.githubusercontent.com/craigmartin97/e98a9e2a267c379e47be1191d9431de2/raw/c09c7356e85e39e41faa92a665b7ef0b3b840b6a/last-names.txt",
   ]);
   let filtered2 = await names_generate_generic(url, fn);
   return filtered2;
