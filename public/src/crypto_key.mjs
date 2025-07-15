e0767a503bbbf8d88070b18b9bc683c0e8edcdd3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 17:26:43 -0400

    2025-07-15T21:26:42.658Z watch   crypto_key

--- public/src/crypto_key.mjs
+++ public/src/crypto_key.mjs
@@ -1,5 +1,7 @@
+import { function_run_save_parent_path } from "./function_run_save_parent_path.mjs";
 import { file_read_json } from "./file_read_json.mjs";
 export async function crypto_key() {
-  let v = await file_read_json();
+  let p = function_run_save_parent_path("key");
+  let v = await file_read_json(p);
   return v;
 }
