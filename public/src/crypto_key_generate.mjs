6bf7e126a314e4bb677f31c1bd90bc01b15ffc68
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 16:13:13 -0400

    2025-07-15T20:13:13.039Z watch   crypto_key_generate

--- public/src/crypto_key_generate.mjs
+++ public/src/crypto_key_generate.mjs
@@ -1,1 +1,13 @@
-export function crypto_key_generate() {}
+import { log } from "./log.mjs";
+export async function crypto_key_generate() {
+  let keyPair = await window.crypto.subtle.generateKey(
+    {
+      name: "ECDH",
+      namedCurve: "P-256",
+    },
+    true,
+    ["deriveKey", "deriveBits"],
+  );
+  console.log("Public Key:", keyPair.publicKey);
+  console.log("Private Key:", keyPair.privateKey);
+}
