57e5d692587a9d17fc51379686d14b42fa66d8bf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 18:24:48 -0400

    2025-07-15T22:24:48.056Z watch   crypto_key_test

--- public/src/crypto_key_test.mjs
+++ public/src/crypto_key_test.mjs
@@ -7,5 +7,7 @@ export async function crypto_key_test() {
   let message = "hi";
   let public_key = object_property_get(key, "public_key");
   let encrypted = await crypto_encrypt(public_key, message);
-  await crypto_key_decrypt(encrypted);
+  await crypto_key_decrypt(
+    "svuhy1ac3Kpp14/7Yq27lgduKeyjnUrv2K2vBuicgEujQfKx5R9bnD8Krx+OBFx/UhN5Mab5SLEWybys0CN5ARQdk4uV9qr5XTacWDJSzn1qcsLbvbG7bm8uNwqPcqKimS3g/pNPcgbnpGTXEdlR99to1I408dIyW2Z9G+6GsJBP6RICrY5sNXZmoluAQ1SirSAkSir7Ze2zftz7TVUZIEpPfN/HG9yrRq9AM7LC8fVgJ34toXX7/tGgbQRlDSnob7biD9c42DPch0z0Ci0lx8Zh2IWxGFoNF4H5V9cVkzEn9KH+mfFmygAVPPL6AUeuGbEQUfFQY52qYa3B74RIyg==",
+  );
 }
