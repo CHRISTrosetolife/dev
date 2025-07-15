0fbd10369c819809db2c5ee2d2651ff185473bd1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 16:24:02 -0400

    2025-07-15T20:24:02.340Z watch   string_snake_to

--- public/src/string_snake_to.mjs
+++ public/src/string_snake_to.mjs
@@ -1,1 +1,7 @@
-export function string_snake_to(s) {}
+export function string_snake_to(s) {
+  let v = str
+    .replace(/([a-z0-9])([A-Z])/g, "$1_$2")
+    .replace(/([A-Z])([A-Z][a-z])/g, "$1_$2")
+    .toLowerCase();
+  return v;
+}
