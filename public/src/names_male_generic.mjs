e5628f4bcdd8eb2a01e1d2b1f42205aac25b8da6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 21:59:50 -0400

    2025-07-15T01:59:49.737Z watch   names_male_generic

--- public/src/names_male_generic.mjs
+++ public/src/names_male_generic.mjs
@@ -5,13 +5,12 @@ import { string_split_newline } from "./string_split_newline.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { http_cache } from "./http_cache.mjs";
 export async function names_male_generic(fn, prefix) {
-  let txt = await http_cache(
-    string_combine_multiple([
-      "https://www.cs.cmu.edu/afs/cs/project/ai-repository/ai/areas/nlp/corpora/names/",
-      prefix,
-      "male.txt",
-    ]),
-  );
+  let url = string_combine_multiple([
+    "https://www.cs.cmu.edu/afs/cs/project/ai-repository/ai/areas/nlp/corpora/names/",
+    prefix,
+    "male.txt",
+  ]);
+  let txt = await http_cache(url);
   let lines = string_split_newline(txt);
   let filtered = list_filter_starts_with_not(lines, "#");
   let filtered2 = list_filter_string_empty_not_is(filtered);
