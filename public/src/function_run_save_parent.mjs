e94637320a1897378d25331bd8b9c2888ab5a757
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 17:20:23 -0400

    2025-07-15T21:20:22.308Z watch   function_run_save_parent

--- public/src/function_run_save_parent.mjs
+++ public/src/function_run_save_parent.mjs
@@ -1,4 +1,6 @@
+import { file_write_json } from "./file_write_json.mjs";
 import { function_run } from "./function_run.mjs";
 export async function function_run_save_parent(f_name, parent_file_name) {
   let result = await function_run(f_name);
+  await file_write_json(parent_file_name, result);
 }
