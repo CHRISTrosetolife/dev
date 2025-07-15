711df5b32bfb50754ffef5ce8c4b4c9df303e4da
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 22:31:59 -0400

    2025-07-15T02:31:56.924Z watch   app_contact_respond_choices

--- public/src/app_contact_respond_choices.mjs
+++ public/src/app_contact_respond_choices.mjs
@@ -100,7 +100,7 @@ export function app_contact_respond_choices() {
         "need",
         "bible",
       ]),
-      app_contact_match_invalid_edit("asked for help", "kind of help you need"),
+      app_cosntact_match_invalid_edit_help(),
     ),
     match_on(
       match_sequence([
