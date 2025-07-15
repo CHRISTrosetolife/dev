2313648982c41fc5dff6500b70be26469d8b4655
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 18:13:53 -0400

    2025-07-15T22:13:52.521Z watch   crypto_key_test

--- public/src/crypto_key_test.mjs
+++ public/src/crypto_key_test.mjs
@@ -1,6 +1,5 @@
 import { crypto_decrypt } from "./crypto_decrypt.mjs";
 import { crypto_key } from "./crypto_key.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { log } from "./log.mjs";
 import { buffer_to } from "./buffer_to.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -12,12 +11,6 @@ export async function crypto_key_test() {
   });
   let message = "Hello, secure world!";
   let encrypted = publicEncrypt(public_key, buffer_to(message));
-  log(
-    string_combine_multiple([
-      "🔐 Encrypted (base64):",
-      encrypted.toString("base64"),
-    ]),
-  );
   let key = await crypto_key();
   let public_key = object_property_get(key, "public_key");
   let private_key = object_property_get(key, "private_key");
