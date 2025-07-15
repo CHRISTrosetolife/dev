68d76de77bffdb5bdef45d51406ae2cf49a3013a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 18:02:16 -0400

    2025-07-15T22:02:14.494Z watch   app_contact_respond_component

--- public/src/app_contact_respond_component.mjs
+++ public/src/app_contact_respond_component.mjs
@@ -2,7 +2,7 @@ import { log } from "./log.mjs";
 import { html_clear } from "./html_clear.mjs";
 import { app_contact_respond } from "./app_contact_respond.mjs";
 import { html_value_get } from "./html_value_get.mjs";
-export async function app_contact_respond_component(t, response_p, d) {
+export function app_contact_respond_component(t, response_p, d) {
   html_clear(response_p);
   let value = html_value_get(t);
   let response = app_contact_respond(d, value);
