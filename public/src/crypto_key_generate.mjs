74dadcc13bb01d54f452529d4c3c4e95b45505d2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 17:34:34 -0400

    2025-07-15T21:34:33.287Z watch   crypto_key_generate

--- public/src/crypto_key_generate.mjs
+++ public/src/crypto_key_generate.mjs
@@ -1,11 +1,6 @@
-import { buffer_to } from "./buffer_to.mjs";
 import { object_snake_to } from "./object_snake_to.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { log } from "./log.mjs";
 import {
   generateKeyPairSync,
-  publicEncrypt,
-  privateDecrypt,
 } from "node:crypto";
 export async function crypto_key_generate() {
   let k = generateKeyPairSync("rsa", {
@@ -21,11 +16,4 @@ export async function crypto_key_generate() {
   });
   let result = object_snake_to(k);
   return result;
-  let public_key = object_property_get(result, "public_key");
-  let private_key = object_property_get(result, "private_key");
-  let message = "Hello, secure world!";
-  let encrypted = publicEncrypt(public_key, buffer_to(message));
-  console.log("🔐 Encrypted (base64):", encrypted.toString("base64"));
-  let decrypted = privateDecrypt(private_key, encrypted);
-  console.log("🔓 Decrypted:", decrypted.toString());
 }
--- public/src/crypto_key_test.mjs
+++ public/src/crypto_key_test.mjs
@@ -1,1 +1,12 @@
-export function crypto_key_test() {}
+
+import {
+  publicEncrypt,
+  privateDecrypt,
+} from "node:crypto";export function crypto_key_test() {
+  let public_key = object_property_get(result, "public_key");
+  let private_key = object_property_get(result, "private_key");
+  let message = "Hello, secure world!";
+  let encrypted = publicEncrypt(public_key, buffer_to(message));
+  log("🔐 Encrypted (base64):", encrypted.toString("base64"));
+  let decrypted = privateDecrypt(private_key, encrypted);
+  log("🔓 Decrypted:", decrypted.toString());}
