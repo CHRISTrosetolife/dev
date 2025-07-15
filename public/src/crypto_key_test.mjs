695826463bf18f8d6f32003b4657feabfa6b8b8d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 18:19:45 -0400

    2025-07-15T22:19:44.949Z crypto_key_test

--- public/src/crypto_key_test.mjs
+++ public/src/crypto_key_test.mjs
@@ -4,7 +4,7 @@ import { crypto_key } from "./crypto_key.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function crypto_key_test() {
   let key = await crypto_key();
-  let message = "Hello, secure world!";
+  let message = "hi";
   let public_key = object_property_get(key, "public_key");
   let encrypted = await crypto_encrypt(public_key, message);
   await crypto_key_decrypt(encrypted);
