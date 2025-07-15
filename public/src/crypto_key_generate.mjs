77d7fe2affcb26613485e928dfd629a10a01982c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 16:28:44 -0400

    2025-07-15T20:28:43.306Z watch   crypto_key_generate

--- public/src/crypto_key_generate.mjs
+++ public/src/crypto_key_generate.mjs
@@ -32,8 +32,8 @@ export async function crypto_key_generate() {
   let public_key = object_property_get(result, "public_key");
   let private_key = object_property_get(result, "private_key");
   let message = "Hello, secure world!";
-  let encrypted = publicEncrypt(publicKey, buffer_to(message));
+  let encrypted = publicEncrypt(public_key, buffer_to(message));
   log("🔐 Encrypted (base64):", encrypted.toString("base64"));
-  let decrypted = privateDecrypt(privateKey, encrypted);
+  let decrypted = privateDecrypt(private_key, encrypted);
   log("🔓 Decrypted:", decrypted.toString());
 }
