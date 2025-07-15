77792839888f187fd358cbc3acf058ad653b80af
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 13:44:30 -0400

    2025-07-15T17:44:29.392Z watch   list_flatten

--- public/src/list_flatten.mjs
+++ public/src/list_flatten.mjs
@@ -1,7 +1,9 @@
+import { fn_name } from "./fn_name.mjs";
 import { list_is } from "./list_is.mjs";
 import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function list_flatten(list) {
+  "see also", fn_name("list_denest");
   let v = list_adder(function (la) {
     each(list, function (item) {
       if (list_is(item)) {
