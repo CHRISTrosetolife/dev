12a7125f5b247e4800f31b9e3642f52d1a4f917c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 17:34:40 -0400

    2025-07-15T21:34:38.997Z watch   crypto_key_test

--- public/src/crypto_key_test.mjs
+++ public/src/crypto_key_test.mjs
@@ -1,12 +1,12 @@
-
-import {
-  publicEncrypt,
-  privateDecrypt,
-} from "node:crypto";export function crypto_key_test() {
+import { log } from "./log.mjs";
+import { buffer_to } from "./buffer_to.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+export function crypto_key_test() {
   let public_key = object_property_get(result, "public_key");
   let private_key = object_property_get(result, "private_key");
   let message = "Hello, secure world!";
   let encrypted = publicEncrypt(public_key, buffer_to(message));
   log("🔐 Encrypted (base64):", encrypted.toString("base64"));
   let decrypted = privateDecrypt(private_key, encrypted);
-  log("🔓 Decrypted:", decrypted.toString());}
+  log("🔓 Decrypted:", decrypted.toString());
+}
