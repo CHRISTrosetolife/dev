a17031a3eee8ff08c615dc0b6b8bec647e823658
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 17:33:20 -0400

    2025-07-15T21:33:19.615Z crypto_key_public_generate

---
+++ public/src/crypto_key_public.mjs
@@ -0,0 +1,3 @@
+export function crypto_key_public() {
+  return "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwlApvfPPtXxfMBX6rWGp\nzxE4YJCzPisGbpw2j96vPTEpyAz1mZJ4DuIhUFB8chppZ5qQvTGYxbBqcu+HhFur\nyY7Nq65bRgkqGtVeOdWJyWkFX/sIlTJerK3+L+fVAXnAySG5oeJjFZeaIACKNflp\nFfpyo1oGpv3q4Rx20IDcipJDYpqqyzPyqTVdOc/nyrx+rzE5bMw98q/Rvb438UlF\n1G6x41+BhElOclxe/zcuL5NgCmSOhbEwJZ13CrIYMesm+jc6kp18fO5Vt+lZeLQE\niTZluTeMLNfQU5iNheYzu82hnqlg0X30BhUAVWRamBnOKbnRgHfpLGgWpbklz9VM\nrwIDAQAB\n-----END PUBLIC KEY-----\n";
+}
