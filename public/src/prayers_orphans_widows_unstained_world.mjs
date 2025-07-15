d2cdf7e8e846beb92da45fc293ffc88877ffd33f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 19:49:08 -0400

    2025-07-15T23:49:06.991Z watch   prayers_orphans_widows_unstained_world

--- public/src/prayers_orphans_widows_unstained_world.mjs
+++ public/src/prayers_orphans_widows_unstained_world.mjs
@@ -1,5 +1,6 @@
 import { prayers_church } from "./prayers_church.mjs";
 import { prayers_needy } from "./prayers_needy.mjs";
 export function prayers_orphans_widows_unstained_world() {
-  return [prayers_needy(), prayers_church()];
+  let v = [prayers_needy(), "Exalt the lowly", prayers_church()];
+  return v;
 }
