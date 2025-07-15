3260bf7893c4277fa0aed15b93aa88e4bb766ae0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 14:12:12 -0400

    2025-07-15T18:12:11.620Z watch   app_contact_download_unhandled

--- public/src/app_contact_download_unhandled.mjs
+++ public/src/app_contact_download_unhandled.mjs
@@ -1,3 +1,4 @@
+import { app_contact_download_responses_test_inner } from "./app_contact_download_responses_test_inner.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { app_contact_download_responses_lookup } from "./app_contact_download_responses_lookup.mjs";
 import { app_contact_download_all } from "./app_contact_download_all.mjs";
@@ -6,6 +7,7 @@ import { object_property_get } from "./object_property_get.mjs";
 export async function app_contact_download_unhandled() {
   let lookup = app_contact_download_responses_lookup();
   let mapped = await app_contact_download_all();
+  app_contact_download_responses_test_inner(mapped, lookup);
   let filtered = list_filter(mapped, function (m) {
     let on = object_property_get(m, "output_null");
     let message = object_property_get(m, "message");
