4bf9618fd7fbdae306f0eb98875c66ec7563d102
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 18:27:40 -0400

    2025-07-15T22:27:39.954Z watch   crypto_decrypt

--- public/src/crypto_decrypt.mjs
+++ public/src/crypto_decrypt.mjs
@@ -1,7 +1,9 @@
+import { string_base64_from } from "./string_base64_from.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 import { log } from "./log.mjs";
 import { privateDecrypt } from "node:crypto";
 export function crypto_decrypt(private_key, encrypted) {
+  string_base64_from(encrypted);
   let decrypted = privateDecrypt(private_key, encrypted);
   log(string_combine_multiple(["🔓 Decrypted:", decrypted.toString()]));
   let v = decrypted;
