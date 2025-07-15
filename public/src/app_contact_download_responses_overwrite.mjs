9f2551d90c93bac65165383ce2771ce5e95cb4b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 14:04:55 -0400

    2025-07-15T18:04:54.158Z watch   app_contact_download_responses_overwrite

--- public/src/app_contact_download_responses_overwrite.mjs
+++ public/src/app_contact_download_responses_overwrite.mjs
@@ -1,3 +1,4 @@
+import { app_contact_download_responses_generate } from "./app_contact_download_responses_generate.mjs";
 import { app_contact_download_responses_get } from "./app_contact_download_responses_get.mjs";
 import { function_get } from "./function_get.mjs";
 import { generate_list_fn } from "./generate_list_fn.mjs";
@@ -5,6 +6,6 @@ export async function app_contact_download_responses_overwrite() {
   let responses_new = await app_contact_download_responses_get();
   await generate_list_fn(
     function_get(responses_new),
-    app_contact_download_responses_overwrite,
+    app_contact_download_responses_generate,
   );
 }
