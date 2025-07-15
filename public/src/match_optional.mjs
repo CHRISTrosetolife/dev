b8b33d887ef978d3e2f6d8b376ff879e7a2b3061
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 21:54:21 -0400

    2025-07-15T01:54:21.027Z watch   match_optional

--- public/src/match_optional.mjs
+++ public/src/match_optional.mjs
@@ -4,6 +4,6 @@ import { match_choice } from "./match_choice.mjs";
 import { list_is } from "./list_is.mjs";
 export function match_optional(input) {
   assert_not(list_is, [input]);
-  let v = match_choice([input, identity]);
+  let v = match_choice([identity, input]);
   return v;
 }
