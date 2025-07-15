8a0661d4e5be8eeccf9bd3af7b7f32eb4d58da1e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 13:43:48 -0400

    2025-07-15T17:43:48.081Z watch   list_flatten

--- public/src/list_flatten.mjs
+++ public/src/list_flatten.mjs
@@ -1,9 +1,15 @@
+import { list_is } from "./list_is.mjs";
 import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
-export function list_flatten(group) {
-  return list_adder((la) => {
-    each(group, (atom) => {
-      each(atom, la);
+export function list_flatten(list) {
+  let v = list_adder(function (la) {
+    each(list, function (item) {
+      if (list_is(item)) {
+        each(atom, la);
+      } else {
+        la(item);
+      }
     });
   });
+  return v;
 }
