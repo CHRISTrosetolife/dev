669ecaf8e1057bd82be89c74345016414a86a237
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 18:34:08 -0400

    2025-07-15T22:34:07.654Z watch   crypto_decrypt

--- public/src/crypto_decrypt.mjs
+++ public/src/crypto_decrypt.mjs
@@ -1,10 +1,17 @@
-import { privateDecrypt } from "node:crypto";
+import { constants, privateDecrypt } from "node:crypto";
 import { string_is } from "./string_is.mjs";
 export function crypto_decrypt(private_key, buffer) {
   if (string_is(buffer)) {
     buffer = Buffer.from(buffer, "base64");
   }
-  let decrypted = privateDecrypt(private_key, buffer);
+  let decrypted = privateDecrypt(
+    {
+      key: privateKey,
+      padding: constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: "sha256",
+    },
+    buffer,
+  );
   let v = decrypted;
   return v;
 }
