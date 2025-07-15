9fca0e37587550e12c703ac5dc5c3a16e63bc027
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 13:44:49 -0400

    2025-07-15T17:44:49.256Z watch   list_denest

--- public/src/list_denest.mjs
+++ public/src/list_denest.mjs
@@ -1,7 +1,10 @@
+import { fn_name } from "./fn_name.mjs";
 import { list_denest_recursive } from "./list_denest_recursive.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function list_denest(list) {
-  return list_adder((la) => {
+  "see also", fn_name("list_flatten");
+  let v = list_adder(function (la) {
     list_denest_recursive(list, la);
   });
+  return v;
 }
