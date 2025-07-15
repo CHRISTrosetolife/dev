478c1d27e87a1d31404e7f74557bf375292f5f86
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 17:32:44 -0400

    2025-07-15T21:32:43.341Z watch   crypto_key_public_generate

--- public/src/crypto_key_public_generate.mjs
+++ public/src/crypto_key_public_generate.mjs
@@ -1,6 +1,8 @@
+import { crypto_key } from "./crypto_key.mjs";
 import { function_new_constant_string } from "./function_new_constant_string.mjs";
 import { generate_suffix_without } from "./generate_suffix_without.mjs";
 export async function crypto_key_public_generate() {
+  let k = await crypto_key();
   let name = generate_suffix_without(crypto_key_public_generate);
-  await function_new_constant_string();
+  await function_new_constant_string(name);
 }
