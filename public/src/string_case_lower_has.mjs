bc8c9fef0d78baefbe00900b57ff9cc4a23d56ee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 16:21:45 -0400

    2025-07-15T20:21:44.834Z watch   string_case_lower_has

--- public/src/string_case_lower_has.mjs
+++ public/src/string_case_lower_has.mjs
@@ -1,6 +1,6 @@
+import { string_case_upper } from "./string_case_upper.mjs";
 import { equal_not } from "./equal_not.mjs";
-import { string_case_lower } from "./string_case_lower.mjs";
 export function string_case_lower_has(s) {
-  let v = equal_not(string_case_lower(s), s);
+  let v = equal_not(string_case_upper(s), s);
   return v;
 }
