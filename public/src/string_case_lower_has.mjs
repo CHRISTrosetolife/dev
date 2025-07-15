2892bebac91ed7de665862abcd1e9627375f015d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 16:21:32 -0400

    2025-07-15T20:21:32.220Z c string_case_upper_has string_case_lower_has

---
+++ public/src/string_case_lower_has.mjs
@@ -0,0 +1,6 @@
+import { equal_not } from "./equal_not.mjs";
+import { string_case_lower } from "./string_case_lower.mjs";
+export function string_case_lower_has(s) {
+  let v = equal_not(string_case_lower(s), s);
+  return v;
+}
