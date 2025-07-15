06bd25e6a90c7ec9028cbc110c2a0e7bd5c1d5b9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 22:02:17 -0400

    2025-07-15T02:02:17.157Z r names_last_generate names_last_list_generate

--- public/src/names_last_list_generate.mjs
+++ public/src/names_last_list_generate.mjs
@@ -1,9 +1,9 @@
 import { names_generate_generic } from "./names_generate_generic.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
-export async function names_last_generate() {
+export async function names_last_list_generate() {
   let url = string_combine_multiple([
     "https://gist.githubusercontent.com/craigmartin97/e98a9e2a267c379e47be1191d9431de2/raw/c09c7356e85e39e41faa92a665b7ef0b3b840b6a/last-names.txt",
   ]);
-  let result = await names_generate_generic(url, names_last_generate);
+  let result = await names_generate_generic(url, names_last_list_generate);
   return result;
 }
