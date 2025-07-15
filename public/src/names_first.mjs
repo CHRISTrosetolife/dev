95a5b63c0c1c6b988f17e897d193cc95de4a727a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 20:18:33 -0400

    2025-07-15T00:18:32.844Z watch   names_first

--- public/src/names_first.mjs
+++ public/src/names_first.mjs
@@ -1,3 +1,4 @@
+import { list_add } from "./list_add.mjs";
 import { list_concat_multiple } from "./list_concat_multiple.mjs";
 import { names_female } from "./names_female.mjs";
 import { names_male } from "./names_male.mjs";
@@ -5,5 +6,6 @@ export function names_first() {
   let m = names_male();
   let f = names_female();
   let na = list_concat_multiple([m, f]);
+  list_add(na, "saloni");
   return na;
 }
