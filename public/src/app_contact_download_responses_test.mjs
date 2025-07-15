c8cd43770478cfd404136c1ec0dbd1dbfcc6863f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 14:11:58 -0400

    2025-07-15T18:11:57.773Z watch   app_contact_download_responses_test

--- public/src/app_contact_download_responses_test.mjs
+++ public/src/app_contact_download_responses_test.mjs
@@ -1,9 +1,5 @@
+import { app_contact_download_responses_test_inner } from "./app_contact_download_responses_test_inner.mjs";
 import { app_contact_download_responses_lookup } from "./app_contact_download_responses_lookup.mjs";
-import { object_property_exists } from "./object_property_exists.mjs";
-import { equal_json } from "./equal_json.mjs";
-import { assert } from "./assert.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { each } from "./each.mjs";
 import { app_contact_download_responses_get } from "./app_contact_download_responses_get.mjs";
 export async function app_contact_download_responses_test() {
   let lookup = app_contact_download_responses_lookup();
@@ -12,13 +8,3 @@ export async function app_contact_download_responses_test() {
   let v = responses_new;
   return v;
 }
-function app_contact_download_responses_test_inner(responses_new, lookup) {
-    each(responses_new, function (n) {
-        let message = object_property_get(n, "message");
-        if (object_property_exists(lookup, message)) {
-            let e = object_property_get(lookup, message);
-            assert(equal_json, [e, n]);
-        }
-    });
-}
-
