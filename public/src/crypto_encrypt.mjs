57dcfe9aa1fe1950398f5e187b2b820205e26f7a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 18:11:25 -0400

    2025-07-15T22:11:24.229Z watch   crypto_encrypt

--- public/src/crypto_encrypt.mjs
+++ public/src/crypto_encrypt.mjs
@@ -1,18 +1,21 @@
 import { log } from "./log.mjs";
 import { crypto_key_public_import } from "./crypto_key_public_import.mjs";
+import { web_is } from "./web_is.mjs";
 export async function crypto_encrypt(publicKeyPem, plaintext) {
-  if (false) {
+  if (web_is()) {
+    let key = await crypto_key_public_import(publicKeyPem);
+    let enc = new TextEncoder().encode(plaintext);
+    let encrypted = await crypto.subtle.encrypt(
+      {
+        name: "RSA-OAEP",
+      },
+      key,
+      enc,
+    );
+    let encryptedBase64 = btoa(
+      String.fromCharCode(...new Uint8Array(encrypted)),
+    );
+    console.log("🔐 Encrypted (base64):", encryptedBase64);
+    return encryptedBase64;
   }
-  let key = await crypto_key_public_import(publicKeyPem);
-  let enc = new TextEncoder().encode(plaintext);
-  let encrypted = await crypto.subtle.encrypt(
-    {
-      name: "RSA-OAEP",
-    },
-    key,
-    enc,
-  );
-  let encryptedBase64 = btoa(String.fromCharCode(...new Uint8Array(encrypted)));
-  console.log("🔐 Encrypted (base64):", encryptedBase64);
-  return encryptedBase64;
 }
