db542e6a55fb953c10e1470283ee284e38e5d962
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-14 22:01:50 -0400

    2025-07-15T02:01:50.250Z watch   names_last_generate

--- public/src/names_last_generate.mjs
+++ public/src/names_last_generate.mjs
@@ -4,6 +4,6 @@ export async function names_last_generate() {
   let url = string_combine_multiple([
     "https://gist.githubusercontent.com/craigmartin97/e98a9e2a267c379e47be1191d9431de2/raw/c09c7356e85e39e41faa92a665b7ef0b3b840b6a/last-names.txt",
   ]);
-  let filtered2 = await names_generate_generic(url, fn);
+  let filtered2 = await names_generate_generic(url, names_last_generate);
   return filtered2;
 }
