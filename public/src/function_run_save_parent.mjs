f6b266301efc695d9137537d25e2aee443ee5323
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 17:14:04 -0400

    2025-07-15T21:14:03.563Z watch   function_run_save_parent

--- public/src/function_run_save_parent.mjs
+++ public/src/function_run_save_parent.mjs
@@ -1,1 +1,4 @@
-export function function_run_save_parent(f_name, parent_file_name) {function_run(f_name)} 
+import { function_run } from "./function_run.mjs";
+export async function function_run_save_parent(f_name, parent_file_name) {
+  await function_run(f_name);
+}
