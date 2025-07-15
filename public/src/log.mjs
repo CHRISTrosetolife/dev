b089e034e2645fd4178312a1173b0c7896f7b482
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-15 17:37:01 -0400

    2025-07-15T21:36:59.331Z crypto_key_test

--- public/src/log.mjs
+++ public/src/log.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments_length } from "./assert_arguments_length.mjs";
 import { log_logs } from "./log_logs.mjs";
 import { list_add } from "./list_add.mjs";
 export function log(message) {
+  assert_arguments_length(arguments, 1);
   let logs = log_logs();
   list_add(logs, message);
   console.log(message);
