c9ac754894254d3c58bfbc37c994c6346784013c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-16 13:36:46 -0400

    2025-07-16T17:36:44.985Z watch   prayers_needy

--- public/src/prayers_needy.mjs
+++ public/src/prayers_needy.mjs
@@ -3,14 +3,7 @@ import { string_prefix_and } from "./string_prefix_and.mjs";
 import { prayers_list_generic_5 } from "./prayers_list_generic_5.mjs";
 export function prayers_needy() {
   let v = prayers_list_generic_5(
-    [
-      "children",
-      "foreigners",
-      "the poor",
-      "the elderly",
-      "prisoners",
-      "the innocent accused",
-    ],
+    ["children", "foreigners", "the elderly", "the innocent accused"],
     prayers_sabbw,
     string_prefix_and,
   );
