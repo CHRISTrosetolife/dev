ba9dab3373238d36c4d7021f48519fc901861165
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-16 12:13:21 -0400

    2025-07-16T16:13:15.366Z watch   app_pray_generic

--- public/src/app_pray_generic.mjs
+++ public/src/app_pray_generic.mjs
@@ -61,12 +61,25 @@ export async function app_pray_generic(body, app_fn, ps) {
   }
   function colorize(d, index) {
     let choices = [
-      "Salmon",
-      "SandyBrown",
-      "Yellow",
-      "lightgreen",
-      "lightblue",
-      "MediumPurple",
+      "forestgreen",
+      "darkgreen",
+      "seagreen",
+      "mediumseagreen",
+      "mediumspringgreen",
+      "springgreen",
+      "green",
+      "darkolivegreen",
+      "olive",
+      "olivedrab",
+      "teal",
+      "mediumaquamarine",
+      "cadetblue",
+      "lightseagreen",
+      "darkslategray",
+      "palegreen",
+      "viridian",
+      "huntergreen",
+      "myrtlegreen",
     ];
     if (d.selected) {
       html_style_background_color(d, list_get_wrap(choices, index));
