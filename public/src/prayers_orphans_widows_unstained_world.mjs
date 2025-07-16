9e1e9fd349ee5ce0f51c9eeeedcb33d5e5732fe8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-16 13:33:56 -0400

    2025-07-16T17:33:52.627Z watch   prayers_orphans_widows_unstained_world

--- public/src/prayers_orphans_widows_unstained_world.mjs
+++ public/src/prayers_orphans_widows_unstained_world.mjs
@@ -5,6 +5,7 @@ export function prayers_orphans_widows_unstained_world() {
     prayers_needy(),
     "Feed the hungry and starve",
     "give drink to the thirsty and dehydrated",
+    "clothe the unclothed",
     "Exalt the lowly",
     "Let all Christians excel in their gifts",
     "Let no gifted Christian do slavish, menial or trivial tasks",
