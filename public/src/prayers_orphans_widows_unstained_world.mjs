946692d474b265af51a892f805dfc84d18f92f27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-16 13:34:26 -0400

    2025-07-16T17:34:24.975Z watch   prayers_orphans_widows_unstained_world

--- public/src/prayers_orphans_widows_unstained_world.mjs
+++ public/src/prayers_orphans_widows_unstained_world.mjs
@@ -3,9 +3,9 @@ import { prayers_needy } from "./prayers_needy.mjs";
 export function prayers_orphans_widows_unstained_world() {
   let v = [
     prayers_needy(),
-    "Feed the hungry and starve",
-    "give drink to the thirsty and dehydrated",
-    "clothe the unclothed",
+    "Save, bless and feed the hungry and starving",
+    "Save, bless and give drink to the thirsty and dehydrated",
+    "Save, bless and clothe the unclothed",
     "Exalt the lowly",
     "Let all Christians excel in their gifts",
     "Let no gifted Christian do slavish, menial or trivial tasks",
