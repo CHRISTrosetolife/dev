297db9c09f5f892519fcf3c7dbfc35169c319584
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-19 17:53:28 -0400

    2025-07-19T21:53:27.920Z watch   app_url

--- public/src/app_url.mjs
+++ public/src/app_url.mjs
@@ -1,9 +1,10 @@
+import { app_extension_html } from "./app_extension_html.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_url(a_name) {
   let v = string_combine_multiple([
     "https://letjesusbeexalted.web.app/",
     a_name,
-    ".html",
+    app_extension_html(),
   ]);
   return v;
 }
