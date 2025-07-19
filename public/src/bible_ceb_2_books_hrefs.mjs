c8f74281b08e24372da234ffe5f977e122a19a57
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-19 17:58:33 -0400

    2025-07-19T21:58:30.058Z watch   bible_ceb_2_books_hrefs

--- public/src/bible_ceb_2_books_hrefs.mjs
+++ public/src/bible_ceb_2_books_hrefs.mjs
@@ -1,4 +1,4 @@
-import { app_extension_html } from "./app_extension_html.mjs";
+import { file_extension_html } from "./file_extension_html.mjs";
 import { html_cache_parse_hrefs } from "./html_cache_parse_hrefs.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_find_properties } from "./list_find_properties.mjs";
@@ -19,9 +19,13 @@ export async function bible_ceb_2_books_hrefs() {
   let url = string_combine_multiple([url_base, "bible.html"]);
   let book_hrefs = await html_cache_parse_hrefs(url, condition);
   function condition(href) {
-    return string_ends_with(href, app_extension_html());
+    let v = string_ends_with(href, file_extension_html());
+    return v;
   }
-  book_hrefs = list_map(book_hrefs, (href) => string_combine(url_base, href));
+  book_hrefs = list_map(book_hrefs, function (href) {
+    let v2 = string_combine(url_base, href);
+    return v2;
+  });
   let columns = 7;
   let rows = 10;
   let book_hrefs_size = list_size(book_hrefs);
@@ -29,10 +33,10 @@ export async function bible_ceb_2_books_hrefs() {
   let columns_full_count = columns_full * rows;
   let rows_full = book_hrefs_size - columns_full_count;
   let rows_missing = rows - rows_full;
-  let indexed = list_adder((la) => {
+  let indexed = list_adder(function (la) {
     let index = 0;
-    each_range(rows_full, (row) => {
-      each_range(columns, (column) => {
+    each_range(rows_full, function (row) {
+      each_range(columns, function (column) {
         la({
           row,
           column,
@@ -41,8 +45,8 @@ export async function bible_ceb_2_books_hrefs() {
         index++;
       });
     });
-    each_range(rows_missing, (row_missing) => {
-      each_range(columns_full, (column) => {
+    each_range(rows_missing, function (row_missing) {
+      each_range(columns_full, function (column) {
         la({
           row: row_missing + rows_full,
           column,
@@ -52,9 +56,9 @@ export async function bible_ceb_2_books_hrefs() {
       });
     });
   });
-  book_hrefs = list_adder((la) => {
-    each_range(columns, (column) => {
-      each_range(rows, (row) => {
+  book_hrefs = list_adder(function (la) {
+    each_range(columns, function (column) {
+      each_range(rows, function (row) {
         if (column === columns - 1 && row >= rows_full) {
           return;
         }
