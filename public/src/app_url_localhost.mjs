0266f38e48104ace64b312ef984d17ec6699bcf3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-19 18:08:03 -0400

    2025-07-19T22:08:02.582Z watch   app_url_localhost

--- public/src/app_url_localhost.mjs
+++ public/src/app_url_localhost.mjs
@@ -1,10 +1,7 @@
+import { app_path } from "./app_path.mjs";
 import { url_localhost_client } from "./url_localhost_client.mjs";
-import { app_extension_html_combine } from "./app_extension_html_combine.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function app_url_localhost(a_name) {
-  let v = string_combine_multiple([
-    url_localhost_client(),
-    app_extension_html_combine(a_name),
-  ]);
+  let v = string_combine_multiple([url_localhost_client(), app_path(a_name)]);
   return v;
 }
