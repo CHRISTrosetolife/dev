70b70cefb6a77b5ef87d3fa74dfb1d7c56c62a04
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-19 17:00:45 -0400

    2025-07-19T21:00:44.501Z watch   app_contact_download_file

--- public/src/app_contact_download_file.mjs
+++ public/src/app_contact_download_file.mjs
@@ -1,3 +1,4 @@
+import { crypto_key_decrypt } from "./crypto_key_decrypt.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { object_merge_strict } from "./object_merge_strict.mjs";
 import { file_read_json } from "./file_read_json.mjs";
@@ -7,7 +8,8 @@ export async function app_contact_download_file(d, file_path) {
   let file = await file_read_json(file_path);
   let message;
   if (object_property_exists(file, "encrypted")) {
-    message = object_property_get(file, "encrypted");
+    let encrypted = object_property_get(file, "encrypted");
+    message = await crypto_key_decrypt(encrypted);
   } else {
     message = object_property_get(file, "message");
   }
