61140698e94a7e06f028e1b50a95f8d970becd85
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-19 17:38:29 -0400

    2025-07-19T21:38:27.385Z watch   app_gs

--- public/src/app_gs.mjs
+++ public/src/app_gs.mjs
@@ -1,9 +1,9 @@
+import { html_localhost_is } from "./html_localhost_is.mjs";
 import { html_script_axios } from "./html_script_axios.mjs";
 import { app_gs_game_delete } from "./app_gs_game_delete.mjs";
 import { app_gs_load } from "./app_gs_load.mjs";
 import { object_merge } from "./object_merge.mjs";
 import { html_style_button_default_value } from "./html_style_button_default_value.mjs";
-import { html_hostname } from "./html_hostname.mjs";
 import { html_scale_none } from "./html_scale_none.mjs";
 import { app_gs_style_default_initialize } from "./app_gs_style_default_initialize.mjs";
 export async function app_gs() {
@@ -19,7 +19,7 @@ export async function app_gs() {
     "margin-left": "0",
     "margin-right": "0",
   });
-  if (html_hostname() !== "localhost") {
+  if (!html_localhost_is()) {
     html_scale_none();
   }
   if (0) {
