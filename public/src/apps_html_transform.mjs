daf46f347676aff5bf64f4bde24b061685910577
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-19 17:57:37 -0400

    2025-07-19T21:57:36.339Z watch   apps_html_transform

--- public/src/apps_html_transform.mjs
+++ public/src/apps_html_transform.mjs
@@ -1,6 +1,6 @@
+import { app_extension_html_combine } from "./app_extension_html_combine.mjs";
 import { html_parse_unparse } from "./html_parse_unparse.mjs";
 import { apps_paths } from "./apps_paths.mjs";
-import { app_extension_html } from "./app_extension_html.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { html_parse_parsed } from "./html_parse_parsed.mjs";
 import { file_transform } from "./file_transform.mjs";
@@ -9,12 +9,13 @@ import { each_async } from "./each_async.mjs";
 import { folder_path_public } from "./folder_path_public.mjs";
 export async function apps_html_transform(transform) {
   let paths = await apps_paths();
-  await each_async(paths, async (name) => {
+  await each_async(paths, async function (name) {
     let p = folder_path_public();
-    let e = app_extension_html();
-    let test_path = string_combine_multiple([p, name, e]);
+    let test_path = app_extension_html_combine(
+      string_combine_multiple([p, name]),
+    );
     await file_transform(
-      async (text) => {
+      async function (text) {
         let r = await html_parse_parsed(text);
         let parsed = object_property_get(r, "parsed");
         let root = object_property_get(r, "root");
