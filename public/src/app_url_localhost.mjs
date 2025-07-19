83bbe10621875ee1a9ce2c7b28ead1ab136c18b0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-19 17:59:49 -0400

    2025-07-19T21:59:46.827Z c app_url app_url_localhost

---
+++ public/src/app_url_localhost.mjs
@@ -0,0 +1,9 @@
+import { app_extension_html_combine } from "./app_extension_html_combine.mjs";
+import { string_combine_multiple } from "./string_combine_multiple.mjs";
+export function app_url_localhost(a_name) {
+  let v = string_combine_multiple([
+    "https://letjesusbeexalted.web.app/",
+    app_extension_html_combine(a_name),
+  ]);
+  return v;
+}
