919d8e99effe90ecf7acbaa243740fb6156e1e34
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-19 17:11:00 -0400

    2025-07-19T21:10:59.218Z watch   app_contact_removes

--- public/src/app_contact_removes.mjs
+++ public/src/app_contact_removes.mjs
@@ -2,6 +2,7 @@ import { string_split_empty } from "./string_split_empty.mjs";
 import { list_concat } from "./list_concat.mjs";
 export function app_contact_removes() {
   let v = list_concat(string_split_empty("bdefghlmnpswy"), [
+    "fa",
     "ut",
     "ur",
     "re",
