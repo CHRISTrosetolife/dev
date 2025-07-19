c8127bc7b262e6a0ac6e80738c78e288bc2fd57f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-19 17:43:54 -0400

    2025-07-19T21:43:51.224Z watch   sandbox_selenium

--- public/src/html_localhost_is.mjs
+++ public/src/html_localhost_is.mjs
@@ -1,4 +1,8 @@
 import { html_hostname } from "./html_hostname.mjs";
 export function html_localhost_is() {
-  return html_hostname() === "localhost";
+  return html_hostname() === string_localhost();
 }
+function string_localhost() {
+    return "localhost";
+}
+
--- public/src/sandbox_selenium.mjs
+++ public/src/sandbox_selenium.mjs
@@ -1,3 +1,4 @@
+import { url_unsecure } from "./url_unsecure.mjs";
 import { each_object_async } from "./each_object_async.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { file_read_json_previous } from "./file_read_json_previous.mjs";
@@ -8,8 +9,8 @@ export async function sandbox_selenium() {
   let facebook = object_property_get(secret, "facebook");
   let driver = await new Builder().forBrowser(Browser.EDGE).build();
   try {
-    await driver.get("https://www.facebook.com/");
-    await each_object_async(facebook, async (key, value) => {
+    await driver.get(url_unsecure(string_localhost()));
+    await each_object_async(facebook, async function (key, value) {
       await driver.findElement(By.id(key)).sendKeys(value);
     });
     await driver.findElement(By.name("email")).sendKeys(Key.RETURN);
