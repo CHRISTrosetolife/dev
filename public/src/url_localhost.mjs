bfc99ca0b2001ff44aed6defa611fde27f1b012c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-19 17:48:03 -0400

    2025-07-19T21:48:02.464Z watch   url_localhost

--- public/src/url_localhost.mjs
+++ public/src/url_localhost.mjs
@@ -1,5 +1,12 @@
 import { url_unsecure } from "./url_unsecure.mjs";
 import { string_combine_multiple } from "./string_combine_multiple.mjs";
 export function url_localhost(port) {
-  return string_combine_multiple([url_unsecure(), "localhost:", port, "/"]);
+  let v = string_combine_multiple([
+    url_unsecure(),
+    "localhost",
+    ":",
+    port,
+    "/",
+  ]);
+  return v;
 }
