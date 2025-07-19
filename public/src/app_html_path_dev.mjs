f1684d8f8016e7254ed5037c21a36fd08eab2878
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-07-19 17:57:10 -0400

    2025-07-19T21:57:09.115Z watch   app_html_path_dev

--- public/src/app_html_path_dev.mjs
+++ public/src/app_html_path_dev.mjs
@@ -1,13 +1,13 @@
-import { app_extension_html } from "./app_extension_html.mjs";
-import { string_combine_multiple } from "./string_combine_multiple.mjs";
+import { app_extension_html_combine } from "./app_extension_html_combine.mjs";
 import { folder_name_public } from "./folder_name_public.mjs";
 import { folder_current } from "./folder_current.mjs";
 import { path_join } from "./path_join.mjs";
 export function app_html_path_dev(name) {
-  return path_join([
+  let v = path_join([
     folder_current(),
     folder_name_public(),
     "dev",
-    string_combine_multiple([name, app_extension_html()]),
+    app_extension_html_combine(name),
   ]);
+  return v;
 }
